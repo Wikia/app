d29845c32f930def4b42cc72512fbe0c793634b5
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-08-28 13:27:09 +0000

    use external storage instead of sayid database
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@1297 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Editcount/SpecialEditcount_body.php
+++ extensions/wikia/Editcount/SpecialEditcount_body.php
@@ -132,9 +132,9 @@ class Editcount extends SpecialPage {
 		$fname = 'Editcount::editsByNsAll';
 		$nscount = array();
 
-		$dbs =& wfGetDBStats();
+		$dbs = wfGetDBExt(DB_SLAVE);
 		$res = $dbs->select(
-			array( 'city_user_edits' ),
+			array( '`dbstats`.`city_user_edits`' ),
 			array( 'ue_edit_namespace as namespace', 'ue_edit_count as count' ),
 			array(
 				'ue_user_id' => $uid
