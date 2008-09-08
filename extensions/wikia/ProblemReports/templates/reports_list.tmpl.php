3ff8d8f8009d01b899b415d2ed58f6ee3cb31f3a
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-09-08 08:23:38 +0000

    ProblemReports: proper indentation
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@1529 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ProblemReports/templates/reports_list.tmpl.php
+++ extensions/wikia/ProblemReports/templates/reports_list.tmpl.php
@@ -1,7 +1,5 @@
 <?php
-    
-    global $wgServer, $wgLang, $wgContLang, $wgOut, $wgExtensionsPath, $wgCityId;
-
+	global $wgServer, $wgLang, $wgContLang, $wgOut, $wgExtensionsPath, $wgCityId;
 ?>
 
 <?= $problems_selector ?>
@@ -25,16 +23,16 @@
 
 <div class="problemReportsListWrapper"><table class="problemReportsList" width="100%">
 <thead>
-    <tr>
+	<tr>
 <?php foreach($th as $header): ?>
-	<th><?= htmlspecialchars(wfMsg($header)) ?></th>
+		<th><?= htmlspecialchars(wfMsg($header)) ?></th>
 <?php endforeach ?>
-    </tr>		
+	</tr>		
 </thead>
 
 <tbody>
 <?php if (count($reports) == 0): ?>
-    <tr><td colspan="9" style="text-align: center; font-weight: bold"><?= wfMsg('pr_no_reports') ?></td></tr>
+	<tr><td colspan="9" style="text-align: center; font-weight: bold"><?= wfMsg('pr_no_reports') ?></td></tr>
 
 <?php endif; ?>
 <?php if (count($reports) > 0): ?>
@@ -43,42 +41,42 @@
 
 <?php
 
-    // change hostname to the one set in pr_server
-    $url = Title::makeTitle(NS_MAIN,$problem['title'])->getFullURL();
-    $url = str_replace($wgServer, $problem['server'], $url);
+	// change hostname to the one set in pr_server
+	$url = Title::makeTitle(NS_MAIN,$problem['title'])->getFullURL();
+	$url = str_replace($wgServer, $problem['server'], $url);
     
-    // comments page - red links only for not existing local project_talk pages (use parser for this)
-    if ($wgServer == $problem['server']) {
+	// comments page - red links only for not existing local project_talk pages (use parser for this)
+	if ($wgServer == $problem['server']) {
 		// link to local page -> use $wgOut->parse
 		$comments_url = $wgOut->parse('[[Project_talk:ProblemReports/'.$problem['id'].'|'.wfMsg('pr_table_comments').']]');
-    }
-    else {
+	}
+	else {
 		$comments_url = $problem['server'] . Title::makeTitle(NS_MAIN, 'Project_talk:ProblemReports/'.$problem['id'])->escapeLocalURL();
 		$comments_url = '<a href="'.htmlspecialchars($comments_url).'">'. htmlspecialchars(wfMsg('pr_table_comments')) .'</a>';
-    }
+	}
 			
-    // make link to: user page (for logged-in users) / contributions list of certain IP + entered username (for anons)
-    if ($problem['anon'] == 1) {
+	// make link to: user page (for logged-in users) / contributions list of certain IP + entered username (for anons)
+	if ($problem['anon'] == 1) {
 		// Special:Contributions/72.134.123.98
 		$user_url = Title::makeTitle(NS_SPECIAL, 'Contributions/'.$problem['ip'])->escapeLocalURL();
 		$user_name = $problem['ip'] . (empty($problem['reporter']) ? '' :  ' ("'.$problem['reporter'].'")');								
-    }
-    else {
+	}
+	else {
 		// User:foo
 		$user_url = Title::makeTitle(NS_USER, $problem['reporter'])->escapeLocalURL();
 		$user_name = $problem['reporter'];
-    }
+	}
 			
-    // change link to point to wikia where problem was reported (#3566)
-    if ($wgCityId != $problem['city']) {
+	// change link to point to wikia where problem was reported (#3566)
+	if ($wgCityId != $problem['city']) {
 		$user_url = $problem['server'] . $user_url;
-    }
+	}
 		
-    // format date (calculate time for UTC per #2214)
-    $date = $wgLang->sprintfDate('j\-M\-y\<\b\r \/\>H:i', $problem['date']) . ' UTC';
+	// format date (calculate time for UTC per #2214)
+	$date = $wgLang->sprintfDate('j\-M\-y\<\b\r \/\>H:i', $problem['date']) . ' UTC';
 			
-    // more link
-    $more_url = Title::makeTitle(NS_SPECIAL,'ProblemReports')->escapeLocalURL('city='.$problem['city']);
+	// more link
+	$more_url = Title::makeTitle(NS_SPECIAL,'ProblemReports')->escapeLocalURL('city='.$problem['city']);
 ?>
 
 <!-- Problem #<?= $problem['id'] ?> -->
@@ -89,43 +87,41 @@
 <?php if( !empty($problem['summary']) ): ?>    <a onclick="reportProblemToogleSummary(<?= $problem['id'] ?>)" class="problemReportsInfo" title="<?= wfMsg('pr_table_description')?>">&nbsp;</a><?php endif; ?>
 	<a href="<?= Title::newFromText('ProblemReports/'.$problem['id'], NS_SPECIAL)->escapeLocalURL() ?>" title="<?= htmlspecialchars(wfMsg('moredotdotdot')) ?>">#<?= number_format($problem['id'], 0, '.', '.') ?></a>&nbsp;<?php if (!empty($problem['summary'])):?>
 <?php endif ?></td>
-    <td><a href="<?= $more_url ?>" title="<?= wfMsg('pr_raports_from_this_wikia') ?>"><?= str_replace(array('http://', '.wikia.com', '.wikia-inc.com'), '', $problem['server']) ?></a>&nbsp;<sup><a href="<?= Title::newFromText('ProblemReports', NS_SPECIAL)->escapeLocalURL('showall=1&lang='.$problem['lang']) ?>"><?= $problem['lang'] ?></a></sup></td>
-    <td style="text-align: center; width: 40px"><?= htmlspecialchars($problemTypes[$problem['type']]) ?></td>
-    <td><a href="<?= $url ?>"><?= wordwrap(htmlspecialchars($problem['title']), 30, ' <br />', true) ?></a></td>
-    <td style="text-align: center"><?= $date ?></td>
-    <td><a href="<?= htmlspecialchars($user_url) ?>"><?= htmlspecialchars(shortenText($user_name,30)) ?></a>
-<?php if (!empty($problem['email']) && $can_do_actions):?>
- <a href="<?= Title::newFromText('ProblemReports/'.$problem['id'], NS_SPECIAL)->escapeLocalURL() ?>" title="<?= wfMsg('email')?>" style="text-decoration: none"><img src="<?= $wgExtensionsPath ?>/wikia/ProblemReports/images/mail_icon.gif" alt="@" width="16" height="16"/></a>
-<?php endif ?><?php if ($isStaff && $problem['anon'] == 0) :?>
- (<?= $problem['ip'] ?>)
-<?php endif ?></td>
-    <td style="text-align: center; width: 65px" id="problemReportsList-problem-<?= $problem['id'] ?>-status"><em class="reportProblemStatus<?= $problem['status'] ?>"><?= wfMsg('pr_status_'.$problem['status']) ?></em></td>
+	<td><a href="<?= $more_url ?>" title="<?= wfMsg('pr_raports_from_this_wikia') ?>"><?= str_replace(array('http://', '.wikia.com', '.wikia-inc.com'), '', $problem['server']) ?></a>&nbsp;<sup><a href="<?= Title::newFromText('ProblemReports', NS_SPECIAL)->escapeLocalURL('showall=1&lang='.$problem['lang']) ?>"><?= $problem['lang'] ?></a></sup></td>
+	<td style="text-align: center; width: 40px"><?= htmlspecialchars($problemTypes[$problem['type']]) ?></td>
+	<td><a href="<?= $url ?>"><?= wordwrap(htmlspecialchars($problem['title']), 30, ' <br />', true) ?></a></td>
+	<td style="text-align: center"><?= $date ?></td>
+	<td><a href="<?= htmlspecialchars($user_url) ?>"><?= htmlspecialchars(shortenText($user_name,30)) ?></a>
+	<?php if (!empty($problem['email']) && $can_do_actions):?><a href="<?= Title::newFromText('ProblemReports/'.$problem['id'], NS_SPECIAL)->escapeLocalURL() ?>" title="<?= wfMsg('email')?>" style="text-decoration: none"><img src="<?= $wgExtensionsPath ?>/wikia/ProblemReports/images/mail_icon.gif" alt="@" width="16" height="16"/></a>
+<?php endif ?><?php if ($isStaff && $problem['anon'] == 0) :?>(<?= $problem['ip'] ?>)
+<?php endif ?>	</td>
+	<td style="text-align: center; width: 65px" id="problemReportsList-problem-<?= $problem['id'] ?>-status"><em class="reportProblemStatus<?= $problem['status'] ?>"><?= wfMsg('pr_status_'.$problem['status']) ?></em></td>
 <?php if ( $can_do_actions ) {
 
-	echo '<td style="width: 120px" id="problemReportsActions-'.$problem['id'].'">';
+	echo "\t".'<td style="width: 120px" id="problemReportsActions-'.$problem['id'].'">';
 
 	foreach($action_icons as $id => $name) {
 	
 		if ($id == 10 && !$can_remove)
 			continue;	// you can't remove reports
 
-		echo "\n\t".'<a id="problemReportsActions-'.$problem['id'].'-'.$id.'" class="problemReportsActions '.$name. ($problem['status'] == $id ? ' problemReportsActionGreyedOut' : '') .'" title="'.wfMsg('pr_status_'.$id).'" onclick="reportProblemAction(this, '.$problem['id'].', '.$id.', \''.wfMsg('pr_status_wait').'\', \''.wfMsg( ($id == 10) ? 'pr_remove_ask' : 'pr_status_ask').'\')">&nbsp;</a>';
+		echo "\n\t\t".'<a id="problemReportsActions-'.$problem['id'].'-'.$id.'" class="problemReportsActions '.$name. ($problem['status'] == $id ? ' problemReportsActionGreyedOut' : '') .'" title="'.wfMsg('pr_status_'.$id).'" onclick="reportProblemAction(this, '.$problem['id'].', '.$id.', \''.wfMsg('pr_status_wait').'\', \''.wfMsg( ($id == 10) ? 'pr_remove_ask' : 'pr_status_ask').'\')">&nbsp;</a>';
 	}
 	
-	echo "\n".'</td>';
+	echo "\n\t</td>";
 }
 ?>
 
 </tr>
 
 <?php if (!empty($problem['summary'])): ?><tr id="problemReportsList-problem-<?= $problem['id'] ?>-summary" style="<?php if (!$single_report) { ?>display: none; <?php } ?>border-left: solid 4px #<?= $colors[$problem['type']] ?>"<?= ($count % 2 ? '' : ' class="odd"') ?>>
-    <td colspan="<?= count($th) ?>" class="problemReportsDescription"><div><strong><?= wfMsg('pr_table_description') ?></strong>: <?= strip_tags($wgOut->parse($problem['summary']), '<p><a><b><i><u><pre><div><img>') ?></div>
-    <?php if (!empty( $problem['browser']  )): ?>
+	<td colspan="<?= count($th) ?>" class="problemReportsDescription"><div><strong><?= wfMsg('pr_table_description') ?></strong>: <?= strip_tags($wgOut->parse($problem['summary']), '<p><a><b><i><u><pre><div><img>') ?></div>
+	 <?php if (!empty( $problem['browser']  )): ?>
 
-        <h5><?= htmlspecialchars($problem['browser']) ?></h5>
-    <?php endif ?>
+	<h5><?= htmlspecialchars($problem['browser']) ?></h5>
+	<?php endif ?>
 
-    <?= $comments_url ?>
+	<?= $comments_url ?>
 </td></tr>
 <?php endif ?>
 <?php endforeach ?>
