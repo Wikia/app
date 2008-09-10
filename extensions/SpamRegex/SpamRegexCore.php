6919f07832de8a69aa9634768084b3c2b3da3fa8
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-09-10 12:50:44 +0000

    merged r1650. SpamRegex fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@1654 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/SpamRegex/SpamRegexCore.php
+++ extensions/SpamRegex/SpamRegexCore.php
@@ -1,86 +1,115 @@
 <?php
 
-/*
+/* 
 	fetch phrases to block, and fill $wgSpamRegex with them, rather than scribble that into the variable at startup
 */
 
 global $wgHooks;
 /* initialize hook, FilterEdit is too far in code */
-$wgHooks['AlternateEdit'][] = 'wfGetSpamRegex';
-$wgHooks['EditFilter'][] = 'wfGetSummarySpamRegex';
-
-function wfGetSpamRegex () {
-	global $wgSpamRegex;
-	if (!wfSimplifiedRegexCheckSharedDB())
-		return true;
-
-	/* get here only the phrases for blocking in textbox */
-	$phrases = wfFetchSpamRegexData (SPAMREGEX_TEXTBOX);
-	("" != $phrases) ? $wgSpamRegex =  "/".$phrases."/i" : $wgSpamRegex = false;
-	return true;
-}
+$wgHooks['EditFilter'][] = 'wfGetSummarySpamRegex' ;
+$wgHooks['SpecialMovepageBeforeMove'][] = 'wfGetMoveSpamRegex' ;
 
 function wfGetSummarySpamRegex ($editpage) {
-	global $wgOut;
-	if (!wfSimplifiedRegexCheckSharedDB())
-		return true;
+	wfProfileIn( __METHOD__ );
+	wfLoadExtensionMessages ('SpamRegex') ;
+	global $wgOut, $wgTitle ;
 
-	$matches = array();
-	/* here we get only the phrases for blocking in summaries... */
-	$phrases = wfFetchSpamRegexData (SPAMREGEX_SUMMARY);
-	("" != $phrases) ? $m_phrases =  "/".$phrases."/i" : $m_phrases = false;
-	if ( $m_phrases && ($editpage->summary != '') && preg_match( $m_phrases, $editpage->summary, $matches ) ) {
-		/*	...so let's rock with our custom spamPage to indicate that
-			(since some phrases can be safely in the text and not in a summary
-			and we do not want to confuse the good users, right?)
-		*/
-		$wgOut->setPageTitle( wfMsg( 'spamprotectiontitle' ) );
-		$wgOut->setRobotPolicy( 'noindex,nofollow' );
-		$wgOut->setArticleRelated( false );
+	// here we get only the phrases for blocking in summaries...	
+	$s_phrases = array () ;
+	$s_phrases = wfFetchSpamRegexData (SPAMREGEX_SUMMARY) ;
+	if ( $s_phrases && ($editpage->summary != '')) {
+		//	...so let's rock with our custom spamPage to indicate that
+		//	(since some phrases can be safely in the text and not in a summary
+		//	and we do not want to confuse the good users, right?)
+		
+		foreach ($s_phrases as $s_phrase) {
+			if (preg_match ($s_phrase, $editpage->summary, $s_matches)) {
+				$wgOut->setPageTitle( wfMsg( 'spamprotectiontitle' ) );
+				$wgOut->setRobotPolicy( 'noindex,nofollow' );
+				$wgOut->setArticleRelated( false );
 
-		$wgOut->addWikiText( wfMsg( 'spamprotectiontext' ) );
-		if ( $matches[0] ) {
-			$wgOut->addWikiText( wfMsg( 'spamprotectionmatch', "<nowiki>{$matches[0]}</nowiki>" ) );
+				$wgOut->addWikiText( wfMsg( 'spamprotectiontext' ) );
+				$wgOut->addWikiText( wfMsg( 'spamprotectionmatch', "<nowiki>{$s_matches[0]}</nowiki>" ) );
+				$wgOut->addWikiText ( wfMsg ('spamregex_summary') ) ; 
+
+				$wgOut->returnToMain( false, $wgTitle );
+				wfProfileOut( __METHOD__ );
+				return false ;
+			}
 		}
-		$wgOut->addWikiText ( wfMsg ('spamregex_summary') );
-
-		$wgOut->returnToMain( false );
-
-		return false;
 	}
-	return true;
+
+	$t_phrases = array () ;
+	// and here we check for phrases within the text itself
+	$t_phrases = wfFetchSpamRegexData (SPAMREGEX_TEXTBOX) ;
+	if ($t_phrases && is_array($t_phrases)) {
+		foreach ($t_phrases as $t_phrase) {
+			if (preg_match ($t_phrase, $editpage->textbox1, $t_matches)) {
+				$editpage->spamPage ($t_matches[0]) ;
+				wfProfileOut( __METHOD__ );
+				return false ;
+			}
+		}
+	}	
+	wfProfileOut( __METHOD__ );
+	return true ;
 }
 
-function wfFetchSpamRegexData ($mode) {
-	global $wgMemc, $wgUser, $wgSpamRegex, $wgSharedDB;
 
-	$phrases = "" ;
+// this is for page move
+function wfGetMoveSpamRegex ($movepage) {
+	wfProfileIn( __METHOD__ );
+	wfLoadExtensionMessages ('SpamRegex') ;
+
+	// here we get only the phrases for blocking in summaries...	
+	$s_phrases = array () ;
+	$s_phrases = wfFetchSpamRegexData (SPAMREGEX_SUMMARY) ;
+	if ( $s_phrases && ($movepage->reason != '')) {		
+		foreach ($s_phrases as $s_phrase) {
+			if (preg_match ($s_phrase, $movepage->reason, $s_matches)) {				
+				$spammessage = wfMsg ('spamregex-move') ;
+				$spammessage .= wfMsg ('spamprotectionmatch', "<nowiki>{$s_matches[0]}</nowiki>") ;
+				$movepage->showForm ('hookaborted', $spammessage) ;
+
+				wfProfileOut( __METHOD__ );
+				return false ;
+			}
+		}
+	}
+	wfProfileOut( __METHOD__ );
+	return true ;
+}
+
+
+function wfFetchSpamRegexData ($mode) {
+	wfProfileIn( __METHOD__ );
+	global $wgMemc, $wgUser, $wgSpamRegex, $wgSharedDB ;
+
+	$phrases = array () ;
 	$first = true ;
 
 	/* first, check if regex string is already stored in memcache */
-	( $mode == SPAMREGEX_SUMMARY ) ? $key_clause = ":Summary" : $key_clause = ":Textbox";
-	$key = "$wgSharedDB:spamRegexCore:spamRegex" . $key_clause;
-	$cached = $wgMemc->get($key);
+	( $mode == SPAMREGEX_SUMMARY ) ? $key_clause = ":Summary" : $key_clause = ":Textbox" ;
+	$key = wfSpamRegexGetMemcDB () . ":spamRegexCore:spamRegex" . $key_clause ;	
+	$cached = $wgMemc->get ($key) ;
 	if ( !$cached ) {
 		/* fetch data from db, concatenate into one string, then fill cache */
-		( $mode == SPAMREGEX_SUMMARY ) ? $clause = " WHERE spam_summary = 1" : $clause = " WHERE spam_textbox = 1";
-		$dbr =& wfGetDB( DB_SLAVE );
-		$query = "SELECT spam_text FROM " . wfSpamRegexGetTable() . $clause;
-		$res = $dbr->query($query);
+		( $mode == SPAMREGEX_SUMMARY ) ? $clause = " WHERE spam_summary = 1" : $clause = " WHERE spam_textbox = 1" ;
+		$dbr =& wfGetDB( DB_SLAVE ) ;
+		$query = "SELECT spam_text FROM " . wfSpamRegexGetTable() . $clause ;
+		$res = $dbr->query ($query) ;
 		while ( $row = $dbr->fetchObject( $res ) ) {
-			$concat = $row->spam_text;
-			if (!$first) {
-				$phrases .= "|".$concat;
-			} else {
-				$phrases .= $concat;
-				$first = false;
-			}
+			$concat = $row->spam_text ;
+			$phrases [] = "/" . $concat . "/i" ;
 		}
-		$wgMemc->set($key, $phrases, 0);
-		$dbr->freeResult($res);
+	
+		$wgMemc->set ($key, $phrases, 0) ; 
+		$dbr->freeResult ($res) ;
 	} else {
 		/* take from cache */
-		$phrases = $cached;
-	}
-	return $phrases;
+		$phrases = $cached ;
+	}	
+	wfProfileOut( __METHOD__ );
+	return $phrases ;
 }
+
--- extensions/SpamRegex/SpecialSpamRegex.php
+++ extensions/SpamRegex/SpecialSpamRegex.php
@@ -11,76 +11,66 @@ if(!defined('MEDIAWIKI'))
 $wgAvailableRights[] = 'spamregex';
 $wgGroupPermissions['staff']['spamregex'] = true;
 
-$wgExtensionFunctions[] = 'wfSpamRegexSetup';
 $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'Regular Expression Spam Block',
 	'author' => 'Bartek Łapiński',
-	'url' => 'http://www.mediawiki.org/wiki/Extension:SpamRegex',
-	'description' => 'filters out unwanted phrases in edited pages, based on regular expressions',
-	'descriptionmsg' => 'spamregex-desc',
+	'description' => 'filters out unwanted phrases in edited pages, based on regular expressions'
 );
 
-/* special page init */
-function wfSpamRegexSetup() {
-	global $IP, $wgMessageCache;
-	if (!wfSimplifiedRegexCheckSharedDB())
-		return;
-	require_once($IP. '/includes/SpecialPage.php');
-	wfLoadExtensionMessages( 'Spamregex' );
-	SpecialPage::addPage(new SpecialPage('Spamregex', 'spamregex', true, 'wfSpamRegexSpecial', false));
-}
+$wgSpecialPages ['spamregex'] = array('SpecialPage', 'Spamregex', 'spamregex', true, 'wfSpamRegexSpecial', false) ;
 
 /* wrapper for GET values */
 function wfSpamRegexGetListBits () {
-	global $wgRequest;
-	$pieces = array();
-	list( $limit, $offset ) = $wgRequest->getLimitOffset();
-	$pieces[] = 'limit=' . $limit;
-	$pieces[] = 'offset=' . $offset;
-	$bits = implode( '&', $pieces );
-	return $bits;
+	global $wgRequest ;
+	$pieces = array() ;
+	list( $limit, $offset ) = $wgRequest->getLimitOffset() ;
+	$pieces[] = 'limit=' . $limit ;
+	$pieces[] = 'offset=' . $offset ;
+	$bits = implode( '&', $pieces ) ;
+	return $bits ;
 }
 
 /* the core */
 function wfSpamRegexSpecial( $par ) {
-	global $wgOut, $wgUser, $wgRequest;
+	global $wgOut, $wgUser, $wgRequest ;
+	wfLoadExtensionMessages( 'SpamRegex' );
 	$wgOut->setPageTitle(wfMsgHtml('spamregex-page-title'));
-	$sRF = new spamRegexForm($par);
-	$sRL = new spamRegexList($par);
+	$sRF = new spamRegexForm ($par) ;
+	$sRL = new spamRegexList ($par) ;
 
 	$action = $wgRequest->getVal( 'action' );
 	if ( 'success_block' == $action ) {
-		$sRF->showSuccess();
-		$sRF->showForm('');
+		$sRF->showSuccess() ;
+		$sRF->showForm ('') ;
 	} else if ( 'success_unblock' == $action ) {
-		$sRL->showSuccess();
-		$sRF->showForm('');
+		$sRL->showSuccess() ;
+		$sRF->showForm ('') ;
 	} else if ( 'failure_unblock' == $action ) {
-		$text = htmlspecialchars ($wgRequest->getVal('text'));
-		$sRF->showForm ("Error unblocking \"{$text}\". Probably there is no such pattern.");
+		$text = htmlspecialchars ($wgRequest->getVal ('text')) ;
+		$sRF->showForm ("Error unblocking \"{$text}\". Probably there is no such pattern.") ;
 	} else if ( $wgRequest->wasPosted() && 'submit' == $action &&
 		$wgUser->matchEditToken( $wgRequest->getVal ('wpEditToken') ) ) {
-		$sRF->doSubmit();
+		$sRF->doSubmit () ;
 	} else if ('delete' == $action) {
-		$sRL->deleteFromList();
+		$sRL->deleteFromList () ;
 	} else {
-		$sRF->showForm('');
+		$sRF->showForm ('') ;
 	}
 		$sRL->showList ('') ;
 }
 
 /* useful for cleaning the memcached keys */
 function wfSpamRegexUnsetKeys () {
-	global $wgMemc, $wgSharedDB;
-	$wgMemc->delete ("$wgSharedDB:spamRegexCore:spamRegex:Textbox");
-	$wgMemc->delete ("$wgSharedDB:spamRegexCore:spamRegex:Summary");
-	$wgMemc->delete ("$wgSharedDB:spamRegexCore:numResults");
+	global $wgMemc, $wgSharedDB ;
+	$wgMemc->delete (wfSpamRegexGetMemcDB () . ":spamRegexCore:spamRegex:Textbox") ;
+	$wgMemc->delete (wfSpamRegexGetMemcDB () . ":spamRegexCore:spamRegex:Summary") ;
+	$wgMemc->delete (wfSpamRegexGetMemcDB () . ":spamRegexCore:numResults") ;
 }
 
 /* the list of blocked names/addresses */
 class spamRegexList {
-	var $mRegexUnblockedAddress;
-	var $numResults = 0;
+	var $mRegexUnblockedAddress ;
+	var $numResults = 0 ;
 
 	/* constructor */
 	function regexBlockList ( $par ) {
@@ -88,115 +78,122 @@ class spamRegexList {
 
 	/* output list */
 	function showList ( $err ) {
-		global $wgOut, $wgRequest, $wgMemc, $wgLang;
+		wfProfileIn( __METHOD__ );
+		global $wgOut, $wgRequest, $wgMemc, $wgLang ;
 
 		/* on error, display error */
 		if ( "" != $err ) {
-			$wgOut->addHTML ("<p class='error'>{$err}</p>\n");
+			$wgOut->addHTML ("<p class='error'>{$err}</p>\n") ;
 		}
 		$titleObj = Title::makeTitle( NS_SPECIAL, 'Spamregex' );
-		$action = $titleObj->escapeLocalURL("") ."?".wfSpamRegexGetListBits();
-		$action_unblock = $titleObj->escapeLocalURL("action=delete") ."&".wfSpamRegexGetListBits();
-		list( $limit, $offset ) = $wgRequest->getLimitOffset();
+		$action = $titleObj->escapeLocalURL("") ."?".wfSpamRegexGetListBits() ;
+		$action_unblock = $titleObj->escapeLocalURL("action=delete") ."&".wfSpamRegexGetListBits() ;
+		list( $limit, $offset ) = $wgRequest->getLimitOffset() ;
 
-		$wgOut->addWikiText('<br /><br />'.wfMsg('spamregex-currently-blocked'));
-		$this->showPrevNext($wgOut);
+		$wgOut->addWikiText('<br/><br/>'.wfMsg('spamregex-currently-blocked')) ;
+		$this->showPrevNext ($wgOut) ;
 
 		if (0 == $this->fetchNumResults ()) {
-			$wgOut->addWikiText('<br />'.wfMsg('spamregex-no-currently-blocked').'<br /><br />');
+			$wgOut->addWikiText('<br/>'.wfMsg('spamregex-no-currently-blocked').'<br/><br/>') ;
 		}
 
-		$wgOut->addHTML ("<form name=\"spamregexlist\" method=\"get\" action=\"{$action}\">");
+		$wgOut->addHTML ("<form name=\"spamregexlist\" method=\"get\" action=\"{$action}\">") ;
 		/* get data and play with data */
-		$dbr = &wfGetDB (DB_SLAVE);
-		$query = "SELECT * FROM ".wfSpamRegexGetTable();
-		$query .= " order by spam_text";
+		$dbr = &wfGetDB (DB_SLAVE) ;
+		$query = "SELECT * FROM ".wfSpamRegexGetTable() ;
+		$query .= " order by spam_text" ;
 
-		$query = $dbr->limitResult ($query, $limit, $offset);
-		$res = $dbr->query($query);
-		$wgOut->addHTML("<ul>");
+		$query = $dbr->limitResult ($query, $limit, $offset) ;
+		$res = $dbr->query ($query) ;
+		$wgOut->addHTML ("<ul>") ;
 		while ( $row = $dbr->fetchObject( $res ) ) {
-			$time = $wgLang->timeanddate( wfTimestamp( TS_MW, $row->spam_timestamp ), true );
-			$ublock_ip = urlencode ($row->spam_text);
-			$desc = "";
+			$time = $wgLang->timeanddate( wfTimestamp( TS_MW, $row->spam_timestamp ), true ) ;
+			$ublock_ip = urlencode ($row->spam_text) ;
+			$desc = "" ;
 			if ($row->spam_textbox == 1) {
-				$desc .= wfMsg('spamregex-text');
+				$desc .= wfMsg('spamregex-text') ;
 			}
 			if ($row->spam_summary == 1) {
 				if ($row->spam_textbox == 1) {
 					$desc .= " " ;
 				}
-				$desc .= wfMsg('spamregex-summary-log');
+				$desc .= wfMsg('spamregex-summary-log') ;
 			}
-			$wgOut->addWikiText (wfMsg('spamregex-log-1', $row->spam_text, $desc, $action_unblock, $ublock_ip) .$row->spam_user . wfMsg('spamregex-log-2', $time));
+			$wgOut->addWikiText (wfMsg('spamregex-log-1',$row->spam_text,$desc, $action_unblock, $ublock_ip) .$row->spam_user . wfMsg('spamregex-log-2', $time));
 		}
-		$dbr->freeResult($res);
-		$wgOut->addHTML("</ul></form>");
-		$this->showPrevNext($wgOut);
+		$dbr->freeResult ($res) ;
+		$wgOut->addHTML ("</ul></form>") ;
+		$this->showPrevNext ($wgOut) ;
+		wfProfileOut( __METHOD__ );
 	}
 
 	/* remove from list - without confirmation */
 	function deleteFromList () {
-		global $wgOut, $wgRequest, $wgMemc, $wgUser;
+		wfProfileIn( __METHOD__ );
+		global $wgOut, $wgRequest, $wgMemc, $wgUser ;
 		$text = urldecode ($wgRequest->getVal('text'));
 		/* delete */
 		$dbw =& wfGetDB( DB_MASTER );
-		$query = "DELETE FROM ".wfSpamRegexGetTable()." WHERE spam_text = ".$dbw->addQuotes($text);
-		$dbw->query($query);
-		$titleObj = Title::makeTitle( NS_SPECIAL, 'Spamregex' );
+		$query = "DELETE FROM ".wfSpamRegexGetTable()." WHERE spam_text = ".$dbw->addQuotes($text) ;
+		$dbw->query ($query) ;
+		$titleObj = Title::makeTitle( NS_SPECIAL, 'Spamregex' ) ;
 		if ( $dbw->affectedRows() ) {
 			/* success  */
-			wfSpamRegexUnsetKeys();
-			$wgOut->redirect( $titleObj->getFullURL( 'action=success_unblock&text='.urlencode($text).'&'.wfSpamRegexGetListBits() ) );
+			wfSpamRegexUnsetKeys () ;
+			wfProfileOut( __METHOD__ );
+			$wgOut->redirect( $titleObj->getFullURL( 'action=success_unblock&text='.urlencode($text).'&'.wfSpamRegexGetListBits() ) ) ;
 		} else {
-			$wgOut->redirect( $titleObj->getFullURL( 'action=failure_unblock&text='.urlencode($text).'&'.wfSpamRegexGetListBits() ) );
+			wfProfileOut( __METHOD__ );
+			$wgOut->redirect( $titleObj->getFullURL( 'action=failure_unblock&text='.urlencode($text).'&'.wfSpamRegexGetListBits() ) ) ;
 		}
 	}
 
 	/* fetch number of all rows */
 	function fetchNumResults () {
-		global $wgMemc, $wgSharedDB;
+		wfProfileIn( __METHOD__ );
+		global $wgMemc, $wgSharedDB ;
 
 		/* we use memcached here */
-		$key = "$wgSharedDB:spamRegexCore:numResults";
-		$cached = $wgMemc->get($key);
-		$results = 0;
+		$key = wfSpamRegexGetMemcDB () . ":spamRegexCore:numResults" ;
+		$cached = $wgMemc->get ($key) ;
+		$results = 0 ;
 		if (is_null ($cached)) {
-			$dbr = &wfGetDB (DB_SLAVE);
-			$query_count = "SELECT COUNT(*) as n FROM ".wfSpamRegexGetTable();
-			$res_count = $dbr->query($query_count);
-			$row_count = $dbr->fetchObject($res_count);
-			$results = $row_count->n;
-			$wgMemc->set($key, $results, REGEXBLOCK_EXPIRE);
-			$dbr->freeResult($res_count);
+			$dbr = &wfGetDB (DB_SLAVE) ;
+			$query_count = "SELECT COUNT(*) as n FROM ".wfSpamRegexGetTable() ;
+			$res_count = $dbr->query($query_count) ;
+			$row_count = $dbr->fetchObject ($res_count);
+			$results = $row_count->n ;
+			$wgMemc->set ($key, $results, REGEXBLOCK_EXPIRE) ;
+			$dbr->freeResult ($res_count) ;
 		} else {
-			$results = $cached;
+			$results = $cached ;
 		}
-		$this->numResults = $results;
-		return $results;
+		$this->numResults = $results ;
+		wfProfileOut( __METHOD__ );
+		return $results ;
 	}
 
 	/* draws one option for select */
 	function makeOption ($blocker, $current) {
-		global $wgOut;
+		global $wgOut ;
 		if ($blocker == $current) {
-			$wgOut->addHTML ("<option selected=\"selected\" value=\"{$blocker}\">{$blocker}</option>");
+			$wgOut->addHTML ("<option selected=\"selected\" value=\"{$blocker}\">{$blocker}</option>") ;
 		} else {
-			$wgOut->addHTML ("<option value=\"{$blocker}\">{$blocker}</option>");
+			$wgOut->addHTML ("<option value=\"{$blocker}\">{$blocker}</option>") ;
 		}
 	}
 
 	/* on success */
 	function showSuccess () {
-		global $wgOut, $wgRequest;
-		$wgOut->setPageTitle(wfMsg('spamregex-page-title-1'));
-		$wgOut->setSubTitle(wfMsg('spamregex-unblock-success'));
-		$wgOut->addWikiText(wfMsg('spamregex-unblock-message', $wgRequest->getVal('text', $par)));
+		global $wgOut, $wgRequest ;
+		$wgOut->setPageTitle(wfMsg('spamregex-page-title-1')) ;
+		$wgOut->setSubTitle(wfMsg('spamregex-unblock-success')) ;
+		$wgOut->addWikiText(wfMsg('spamregex-unblock-message', $wgRequest->getVal('text', $par))) ;
 	}
 
 	/* init for showprevnext */
 	function showPrevNext( &$out ) {
-		global $wgContLang, $wgRequest;
+		global $wgContLang,$wgRequest;
 		list( $limit, $offset ) = $wgRequest->getLimitOffset();
 		$html = wfViewPrevNext(
 				$offset,
@@ -211,34 +208,34 @@ class spamRegexList {
 
 /* the form for blocking names and addresses */
 class spamRegexForm {
-	var $mBlockedPhrase;
-	var $mBlockedText;
-	var $mBlockedSummary;
+	var $mBlockedPhrase ;
+	var $mBlockedText ;
+	var $mBlockedSummary ;
 
 	/* constructor */
 	function spamRegexForm ( $par ) {
 		global $wgRequest ;
 		$this->mBlockedPhrase = $wgRequest->getVal( 'wpBlockedPhrase',  $wgRequest->getVal( 'text', $par ) );
-		($wgRequest->getVal ('wpBlockedTextbox') ) ? $this->mBlockedTextbox = 1 : $this->mBlockedTextbox = 0;
-		($wgRequest->getVal ('wpBlockedSummary') ) ? $this->mBlockedSummary = 1 : $this->mBlockedSummary = 0;
+		($wgRequest->getVal ('wpBlockedTextbox') ) ? $this->mBlockedTextbox = 1 : $this->mBlockedTextbox = 0 ;
+		($wgRequest->getVal ('wpBlockedSummary') ) ? $this->mBlockedSummary = 1 : $this->mBlockedSummary = 0 ;
 	}
 
 	/* output */
 	function showForm ( $err ) {
-		global $wgOut, $wgUser, $wgRequest;
+		global $wgOut, $wgUser, $wgRequest ;
 
 		$token = htmlspecialchars( $wgUser->editToken() );
 		$titleObj = Title::makeTitle( NS_SPECIAL, 'Spamregex' );
-		$action = $titleObj->escapeLocalURL( "action=submit" )."&".wfSpamRegexGetListBits();
+		$action = $titleObj->escapeLocalURL( "action=submit" )."&".wfSpamRegexGetListBits() ;
 
 		if ( "" != $err ) {
 			$wgOut->setSubtitle( wfMsgHtml( 'formerror' ) );
 			$wgOut->addHTML( "<p class='error'>{$err}</p>\n" );
 		}
 
-		$wgOut->addWikiText(wfMsg('spamregex-intro'));
+		$wgOut->addWikiText(wfMsg('spamregex-intro')) ;
 
-		( 'submit' == $wgRequest->getVal( 'action' )) ? $scBlockedPhrase = htmlspecialchars ($this->mBlockedPhrase) : $scBlockedPhrase = '';
+		( 'submit' == $wgRequest->getVal( 'action' )) ? $scBlockedPhrase = htmlspecialchars ($this->mBlockedPhrase) : $scBlockedPhrase = '' ;
 
 		$wgOut->addScript("
 			<script type=\"text/javascript\">
@@ -265,12 +262,12 @@ class spamRegexForm {
 
 				addOnloadHook (SpamRegexEnhanceControls) ;
 			</script>"
-		);
+		) ;
 		$phraseblock = wfMsg('spamregex-phrase-block');
 		$phraseblocktext = wfMsg('spamregex-phrase-block-text');
-		$phraseblocksummary = wfMsg('spamregex-phrase-block-summary');
-		$blockphrase = wfMsg('spamregex-block-submit');
-		$wgOut->addHTML("
+		$phraseblocksummary= wfMsg('spamregex-phrase-block-summary');
+		$blockphrase=wfMsg('spamregex-block-submit');
+		$wgOut->addHtml("
 <form name=\"spamregex\" method=\"post\" action=\"{$action}\">
 	<table border=\"0\">
 		<tr>
@@ -306,37 +303,41 @@ class spamRegexForm {
 
 	/* on success */
 	function showSuccess () {
-		global $wgOut, $wgRequest;
-		$wgOut->setPageTitle(wfMsg('spamregex-page-title-2'));
-		$wgOut->setSubTitle(wfMsg('spamregex-block-success'));
+		global $wgOut, $wgRequest ;
+		$wgOut->setPageTitle(wfMsg('spamregex-page-title-2')) ;
+		$wgOut->setSubTitle(wfMsg('spamregex-block-success')) ;
 
-		$wgOut->addWikiText(wfMsg('spamregex-block-message', $this->mBlockedPhrase));
+		$wgOut->addWikiText(wfMsg('spamregex-block-message',$this->mBlockedPhrase)) ;
 	}
 
 	/* on submit */
 	function doSubmit () {
-		global $wgOut, $wgUser, $wgMemc;
+		wfProfileIn( __METHOD__ );
+		global $wgOut, $wgUser, $wgMemc ;
 
 		/* empty name */
 		if ( strlen($this->mBlockedPhrase) == 0 ) {
-			$this->showForm (wfMsgHtml('spamregex-warning-1'));
+			$this->showForm (wfMsgHtml('spamregex-warning-1')) ;
+			wfProfileOut( __METHOD__ );
 			return ;
 		}
 		/* validate expression */
 		if (!$simple_regex = wfValidRegex ($this->mBlockedPhrase) ) {
-			$this->showForm (wfMsgHtml('spamregex-error-1'));
+			$this->showForm (wfMsgHtml('spamregex-error-1')) ;
+			wfProfileOut( __METHOD__ );
 			return ;
 		}
 
 		/* make insert */
 		$dbw =& wfGetDB( DB_MASTER );
-		$name = $wgUser->getName();
-		$timestamp =  wfTimestampNow();
+		$name = $wgUser->getName () ;
+		$timestamp =  wfTimestampNow() ;
 
 		/* we need at least one block mode specified... we can have them both, of course */
 		if ( ($this->mBlockedTextbox == 0) && ($this->mBlockedSummary == 0) ) {
-			$this->showForm (wfMsgHtml('spamregex-warning-2'));
-			return;
+			$this->showForm (wfMsgHtml('spamregex-warning-2')) ;
+			wfProfileOut( __METHOD__ );
+			return ;
 		}
 
 		$query = "INSERT IGNORE INTO ".wfSpamRegexGetTable()
@@ -347,17 +348,19 @@ class spamRegexForm {
 				  {$dbw->addQuotes($name)},
 				  {$this->mBlockedTextbox},
 				  {$this->mBlockedSummary}
-				 )";
-		$dbw->query($query);
+				 )" ;
+		$dbw->query ($query) ;
 
 		/* duplicate entry */
 		if (!$dbw->affectedRows()) {
-			$this->showForm (wfMsgHtml('spamregex-already-blocked', $this->mBlockedPhrase));
-			return;
+			$this->showForm (wfMsgHtml('spamregex-already-blocked',$this->mBlockedPhrase)) ;
+			wfProfileOut( __METHOD__ );
+			return ;
 		}
-		wfSpamRegexUnsetKeys ($name);
+		wfSpamRegexUnsetKeys ($name) ;
 		/* redirect */
-		$titleObj = Title::makeTitle( NS_SPECIAL, 'Spamregex' );
-		$wgOut->redirect( $titleObj->getFullURL( 'action=success_block&text=' .urlencode( $this->mBlockedPhrase )."&".wfSpamRegexGetListBits() ) );
+		$titleObj = Title::makeTitle( NS_SPECIAL, 'Spamregex' ) ;
+		wfProfileOut( __METHOD__ );
+		$wgOut->redirect( $titleObj->getFullURL( 'action=success_block&text=' .urlencode( $this->mBlockedPhrase )."&".wfSpamRegexGetListBits() ) ) ;
 	}
 }
