5aeb0c3dc3df18e311a9541f364966b34293b83e
tor <tor@wikia-inc.com>
2008-09-10 12:50:44 +0000

    merged r1650. SpamRegex fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@1654 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/SpamRegex/SpamRegex.php
+++ extensions/SpamRegex/SpamRegex.php
@@ -1,22 +1,29 @@
 <?php
 
 /* help displayed on the special page  */
-define ('SPAMREGEX_PATH', '/');
+define ('SPAMREGEX_PATH', '/') ;
 
 /* for memcached - expiration time */
-define ('SPAMREGEX_EXPIRE', 0);
+define ('SPAMREGEX_EXPIRE', 0) ;
 
 /* two modes for two kinds of blocks */
-define ('SPAMREGEX_TEXTBOX', 0);
-define ('SPAMREGEX_SUMMARY', 1);
+define ('SPAMREGEX_TEXTBOX', 0) ;
+define ('SPAMREGEX_SUMMARY', 1) ;
 
 /* return the name of the table  */
 function wfSpamRegexGetTable() {
-	global $wgSharedDB;
-	if ("" != $wgSharedDB) {
-		return "{$wgSharedDB}.spam_regex";
+	return wfSharedTable('spam_regex', false);
+}
+
+/* return the proper db key for Memc */
+function wfSpamRegexGetMemcDB () {
+	global $wgSharedDB, $wgExternalSharedDB, $wgDBname ;
+	if (!empty( $wgExternalSharedDB )) {
+		return $wgExternalSharedDB ;
+	} elseif (!empty( $wgSharedDB )) {
+		return $wgSharedDB ;
 	} else {
-		return "spam_regex";
+		return $wgDBname ;	
 	}
 }
 
@@ -27,9 +34,9 @@ function wfSpamRegexGetTable() {
 if (!defined('MEDIAWIKI')) die();
 
 $dir = dirname(__FILE__) . '/';
-$wgExtensionMessagesFiles['Spamregex'] = $dir . 'SpamRegex.i18n.php';
+$wgExtensionMessagesFiles['SpamRegex'] = $dir . 'SpamRegex.i18n.php';
 
-require_once ($IP.SPAMREGEX_PATH."extensions/SpamRegex/SpecialSpamRegex.php");
+require_once ($IP.SPAMREGEX_PATH."extensions/SpamRegex/SpecialSpamRegex.php") ;
 //will need more, maybe Core?
-require_once ($IP.SPAMREGEX_PATH."extensions/SpamRegex/SpamRegexCore.php");
-require_once ($IP.SPAMREGEX_PATH."extensions/SimplifiedRegex/SimplifiedRegex.php");
+require_once ($IP.SPAMREGEX_PATH."extensions/SpamRegex/SpamRegexCore.php") ;
+require_once ($IP.SPAMREGEX_PATH."extensions/SimplifiedRegex/SimplifiedRegex.php") ;
