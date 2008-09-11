a2db44b8845d51fe32b203a110bf043f9d64dd76
macbre <macbre@wikia-inc.com>
2008-09-11 09:41:44 +0000

    StaffSig: use $wgStylePath
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@1692 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaffSig/StaffSig.php
+++ extensions/wikia/StaffSig/StaffSig.php
@@ -3,12 +3,13 @@
 $wgExtensionFunctions[] = 'wfSigSetup';
 
 function wfSigSetup() {
-  global $wgParser;
-  $wgParser->setHook( 'staff', 'wfMakeSignature' );
+	global $wgParser;
+	$wgParser->setHook( 'staff', 'wfMakeSignature' );
 }
 
 function wfMakeSignature( $contents, $attributes, $parser ) {
-  return '<a href="http://www.wikia.com/wiki/Wikia_Staff" title="This user is a member of Wikia staff" class="staffSigLink"><img src="/skins/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
+	global $wgStylePath;
+	return '<a href="http://www.wikia.com/wiki/Wikia_Staff" title="This user is a member of Wikia staff" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
 }
 
 ?>
