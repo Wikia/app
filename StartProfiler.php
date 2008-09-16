8706dc80b44c4201f43f77430624b3fe402e0bb7
emil <emil@wikia-inc.com>
2008-09-16 08:41:48 +0000

    enable profiler
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@1814 e846bd91-c042-42a7-a218-4b65e2c1904d

--- StartProfiler.php
+++ StartProfiler.php
@@ -1,22 +1,36 @@
 <?php
 
-require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
+$rand = mt_rand(0, 0x7fffffff);
+$host = @$_SERVER['HTTP_HOST'];
 
-/**
- * To use a profiler, delete the line above and add something like this:
- *
- *   require_once(  dirname(__FILE__).'/includes/Profiler.php' );
- *   $wgProfiler = new Profiler;
- *
- * Or for a sampling profiler:
- *   if ( !mt_rand( 0, 100 ) ) {
- *       require_once(  dirname(__FILE__).'/includes/Profiler.php' );
- *       $wgProfiler = new Profiler;
- *   } else {
- *       require_once(  dirname(__FILE__).'/includes/ProfilerStub.php' );
- *   }
- * 
- * Configuration of the profiler output can be done in LocalSettings.php
- */
-
+if ( @$_SERVER['REQUEST_URI'] == '/index.php?title=Anakin_Skywalker&action=submit' ) {
+	require_once( dirname(__FILE__).'/includes/ProfilerSimpleUDP.php' );
+	$wgProfiler = new ProfilerSimpleUDP;
+	$wgProfiler->setProfileID( 'bigpage' );
+} elseif (!empty($_GET['forceprofile'])) {
+	require_once( dirname(__FILE__).'/includes/ProfilerSimpleText.php' );
+	$wgProfiler = new ProfilerSimpleText;
+	$wgProfiler->setProfileID( 'forced' );
+} elseif ( !( $rand % 50 ) ) {
+	require_once( dirname(__FILE__).'/includes/ProfilerSimpleUDP.php' );
+	$wgProfiler = new ProfilerSimpleUDP;
+	if ( $host == 'www.wowwiki.com' ) {
+		$wgProfiler->setProfileID( 'wow' );
+	} elseif ( $host == 'starwars.wikia.com' ) {
+		$wgProfiler->setProfileID( 'starwars' );
+	} elseif ( $host == 'yugioh.wikia.com' ) {
+		$wgProfiler->setProfileID( 'yugioh' );
+	} elseif ( $host == 'muppet.wikia.com' ) {
+		$wgProfiler->setProfileID( 'muppet' );
+	} else {
+		$wgProfiler->setProfileID( 'wikia' );
+	}
+	#$wgProfiler->setProfileID( 'all' );
+	#$wgProfiler->setMinimum(5 /* seconds */);
+} elseif ( defined( 'MW_FORCE_PROFILE' ) ) {
+	require_once( dirname(__FILE__).'/includes/Profiler.php' );
+	$wgProfiler = new Profiler;
+} else {
+	require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
+}
 
