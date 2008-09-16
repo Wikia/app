c29c83780688dcfc4be9c8f6f00481820fcbebd0
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-09-16 13:02:59 +0000

    CreateAPage: increase version number
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@1835 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreateAPage/SpecialCreatePage.php
+++ extensions/wikia/CreateAPage/SpecialCreatePage.php
@@ -23,7 +23,7 @@ $wgExtensionCredits['specialpage'][] = array(
    'name' => 'CreatePage',
    'author' => 'Bartek Łapiński, Lucas \'TOR\' Garczewski, Przemek Piotrowski'  ,
    'url' => 'http://help.wikia.com/wiki/Help:CreatePage' ,
-   'version' => '3.57' ,
+   'version' => '3.58' ,
    'description' => 'easy to use interface for creating new articles'
 );
 
