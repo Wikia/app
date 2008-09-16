26fb4008904de24e8ba8e069411472ddc55e87d1
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-09-16 21:24:29 +0000

    merged r1722
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@1861 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/TaskManager/Tasks/CreateWikiTask.php
+++ extensions/wikia/TaskManager/Tasks/CreateWikiTask.php
@@ -46,13 +46,17 @@ class CreateWikiTask extends BatchTask {
 	 */
 	function execute( $params = null ) {
 		global $IP, $wgDevelEnvironment;
-		global $wgWikiaLocalSettingsPath, $wgWikiaAdminSettingsPath;
+		global $wgWikiaLocalSettingsPath, $wgWikiaAdminSettingsPath; // $wgExtensionMessagesFiles;
 
 		if( !isset( $wgWikiaAdminSettingsPath ) ) {
 			$wgWikiaAdminSettingsPath = dirname( $wgWikiaLocalSettingsPath ) . "/../AdminSettings.php";
 		}
-		wfLoadExtensionMessages( "CreateWikiTask" );
 
+		/* we don't need it actually, messages are kept on messaging.wikia (fixes #3592)
+		$wgExtensionMessagesFiles["CreateWikiTask"] = dirname(__FILE__) . '/CreateWikiTask/CreateWikiTask.i18n.php';
+		wfLoadExtensionMessages( "CreateWikiTask" );
+		*/
+		
 		$this->mData = $params;
 
 		/**
