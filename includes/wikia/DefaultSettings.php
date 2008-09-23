39c0cca6a507265ac8fb85086096f7fbadb74280
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-09-23 22:16:35 +0000

    $wgStyleVersion++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2051 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -27,9 +27,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
  * Use revision number
  */
 
-$wgStyleVersion         = '1874';
+$wgStyleVersion         = '2050';
 $wgMergeStyleVersionJS  = '1595';
-$wgMergeStyleVersionCSS = '1874';
+$wgMergeStyleVersionCSS = '2050';
 
 /**
  * @name $wgCityId
