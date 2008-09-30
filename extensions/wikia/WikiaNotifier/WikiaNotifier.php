8cc1f9d2746b8e2b83b3263a3c424a79624ea2b5
eloy <eloy@wikia-inc.com>
2008-09-30 11:11:47 +0000

    wfNotifyOnPageChangeComplete use external storage as well
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2244 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaNotifier/WikiaNotifier.php
+++ extensions/wikia/WikiaNotifier/WikiaNotifier.php
@@ -3,8 +3,6 @@
 Author: Piotr Molski (moli@wikia.com)
 */
 
-define ('NOTIFY_TABLE', "CREATE TABLE `_wikialogs_`.`notify_log` (`nl_id` int(11) NOT NULL auto_increment,`nl_city` int(11) NOT NULL, `nl_type` char(32) NOT NULL,`nl_editor` int(6) NOT NULL,`nl_watcher` int(6) NOT NULL,`nl_title` varchar(255) NOT NULL,`nl_namespace` int(11) NOT NULL,`nl_timestamp` char(14) default NULL,PRIMARY KEY (`nl_id`),KEY `nl_watcher` (`nl_watcher`),KEY `nl_editor` (`nl_editor`),KEY `nl_title` (`nl_title`, `nl_namespace`),KEY `nl_type` (`nl_type`, `nl_timestamp`), KEY `nl_city` (`nl_city`)) ENGINE=InnoDB DEFAULT CHARSET=latin1");
-
 $wgHooks['NotifyOnPageChangeComplete'][] = 'wfNotifyOnPageChangeComplete';
 
 function wfNotifyOnPageChangeComplete($title, $timestamp, $watcher) {
@@ -37,22 +35,23 @@ function wfNotifyOnPageChangeComplete($title, $timestamp, $watcher) {
 	}
 
 	$notify_type = "notifyonpagechange";
-	$dbw =& wfGetDB( DB_MASTER );
-	if (!$dbw->tableExists("`_wikialogs_`.`notify_log`")) {
-		$dbw->query(NOTIFY_TABLE);
-	}
+	$dbw = wfGetDBExt( DB_MASTER );
 
 	$nl_id = $dbw->nextSequenceValue( 'notify_log_nl_id_seq' );
-	$dbw->insert("`_wikialogs_`.`notify_log`", array(
-		'nl_id' 		=> $nl_id,
-		'nl_city'		=> intval($wgCityId),
-		'nl_type'		=> $notify_type,
-		'nl_editor' 	=> $editor_id,
-		'nl_watcher'	=> $watcher_id,
-		'nl_title' 		=> $title->getDBkey(),
-		'nl_namespace' 	=> $namespace,
-		'nl_timestamp'	=> $dbw->timestamp($timestamp)
-	), __METHOD__);
+	$dbw->insert(
+		array( "notify_log"),
+		array(
+			'nl_id' 		=> $nl_id,
+			'nl_city'		=> intval($wgCityId),
+			'nl_type'		=> $notify_type,
+			'nl_editor' 	=> $editor_id,
+			'nl_watcher'	=> $watcher_id,
+			'nl_title' 		=> $title->getDBkey(),
+			'nl_namespace' 	=> $namespace,
+			'nl_timestamp'	=> $dbw->timestamp($timestamp)
+		),
+		__METHOD__
+	);
 	wfProfileOut( __METHOD__ );
 	return true;
 }
