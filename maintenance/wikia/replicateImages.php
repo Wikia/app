65f38390ff28936dad39b2434a009d5119ffe6dc
eloy <eloy@wikia-inc.com>
2008-09-30 10:51:40 +0000

    change hook, now it use external storage and dataware database
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2241 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/replicateImages.php
+++ maintenance/wikia/replicateImages.php
@@ -1,64 +1,91 @@
 <?php
 /**
  * Replicate images through file servers
- * 
+ *
  * @addto maintenance
  * @author Adrian 'ADi' Wieczorek <adi(at)wikia.com>
- * 
+ * @author Krzysztof Krzyżaniak <eloy@wikia-inc.com>
+ *
  */
 
 ini_set( "include_path", dirname(__FILE__)."/.." );
 require_once( 'commandLine.inc' );
 
-$sLogsDb = '_wikialogs_';
-$aFileServers = isset($options['server']) ? explode(',', $options['server']) : array();
-$iImageLimit = isset($options['limit']) ? $options['limit'] : 10; 
-$sUserLogin = isset($options['u']) ? $options['u'] : 'cron';
-$bIsDryRun = isset($options['dryrun']) ? true : false;
+class WikiaReplicateImages {
 
-$dbr = wfGetDB(DB_SLAVE);
-$dbw = wfGetDB(DB_MASTER);
+	private $mServers = array(
+		"file3" => array(
+			"address" => "file3.sjc.wikia-inc.com",
+			"transform" => array( "!^/images/(.)!", "/raid/images/by_id/$1/$1" ),
+		),
+		"willow" => array(
+			"address" => "willow.sjc.wikia-inc.com",
+			"transform" => false
+		)
+	);
 
-$dbr->selectDB($sLogsDb);
-$dbw->selectDB($sLogsDb);
+	/**
+	 * main entry point for class
+	 *
+	 * @access public
+	 *
+	 * @param	array	$options	commandline options
+	 */
+	public function execute( $options ) {
+		$sLogsDb = '_wikialogs_';
+		$aFileServers = isset($options['server']) ? explode(',', $options['server']) : array();
+		$iImageLimit = isset($options['limit']) ? $options['limit'] : 10;
+		$sUserLogin = isset($options['u']) ? $options['u'] : 'cron';
+		$bIsDryRun = isset($options['dryrun']) ? true : false;
 
-$oResource = $dbr->query("SELECT up_id, up_path FROM upload_path WHERE up_sent<>'y' ORDER BY up_created ASC" . ($iImageLimit ? " LIMIT " . addslashes($iImageLimit) : ""));
+		$dbr = wfGetDB(DB_SLAVE);
+		$dbw = wfGetDB(DB_MASTER);
 
-if($oResource) {
- while($oResultRow = $dbr->fetchObject($oResource)) {
- 	$bFileCopied = true;
- 	$sDestPath = $oResultRow->up_path;
- 	foreach($aFileServers as $sServerName) {
- 		if($sServerName == 'file3') {
- 			$sDestPath = preg_replace("!^/images/(.)!", "/raid/images/by_id/$1/$1", $sDestPath);
- 		}
- 		$sScpCommand = '/usr/bin/scp -p ' . $oResultRow->up_path . ' ' . $sUserLogin . '@' . $sServerName . ':' . $sDestPath;
- 		$sScpCommand.= ' >/dev/null 2>&1';
- 		
- 		if($bIsDryRun) {
- 			print($sScpCommand . "\n");
- 		}
- 		else {
-	 		$aOutput = null;
-	 		$iReturnValue = 1;
-	 		@exec($sScpCommand, $aOutput, $iReturnValue);
-	
-	 		if($iReturnValue > 0) {
-	 			print("ERROR: $sScpCommand - command failed.\n\n");
-	 			$bFileCopied = false;
-	 			break;
-	 		}
- 		}
- 	}
- 	
- 	if($bFileCopied && !$bIsDryRun) {
- 		// update flag in db
- 		$dbw->query("UPDATE upload_path SET up_sent='y' WHERE up_id='" . $oResultRow->up_id . "'");
-		 $dbw->immediateCommit();
- 	}
- }
- 
-}
-else {
-	print("No new images to be replicated.\n");
-}
+		$dbr->selectDB($sLogsDb);
+		$dbw->selectDB($sLogsDb);
+
+		$oResource = $dbr->query("SELECT up_id, up_path FROM upload_path WHERE up_sent<>'y' ORDER BY up_created ASC" . ($iImageLimit ? " LIMIT " . addslashes($iImageLimit) : ""));
+
+		if($oResource) {
+			while($oResultRow = $dbr->fetchObject($oResource)) {
+				$bFileCopied = true;
+				$sDestPath = $oResultRow->up_path;
+				foreach($aFileServers as $sServerName) {
+					if($sServerName == 'file3') {
+						$sDestPath = preg_replace("!^/images/(.)!", "/raid/images/by_id/$1/$1", $sDestPath);
+					}
+					$sScpCommand = '/usr/bin/scp -p ' . $oResultRow->up_path . ' ' . $sUserLogin . '@' . $sServerName . ':' . $sDestPath;
+					$sScpCommand.= ' >/dev/null 2>&1';
+
+					if( 1 ) {
+						print($sScpCommand . "\n");
+					}
+					else {
+						$aOutput = null;
+						$iReturnValue = 1;
+						@exec($sScpCommand, $aOutput, $iReturnValue);
+
+						if($iReturnValue > 0) {
+							print("ERROR: $sScpCommand - command failed.\n\n");
+							$bFileCopied = false;
+							break;
+						}
+					}
+				}
+
+				if($bFileCopied && !$bIsDryRun) {
+					// update flag in db
+					$dbw->query("UPDATE upload_path SET up_sent='y' WHERE up_id='" . $oResultRow->up_id . "'");
+					 $dbw->immediateCommit();
+				}
+			}
+		}
+		else {
+			print("No new images to be replicated.\n");
+		}
+	}
+};
+
+
+$replicate = new WikiaReplicateImages();
+$replicate->execute( $options );
