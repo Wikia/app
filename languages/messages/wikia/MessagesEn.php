4cd9a757c0cf0f493f4e4f0b77abdf322850cba1
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-10-01 16:56:38 +0000

    removed the word "wiki" from SharedHelp text per Kirk
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2311 e846bd91-c042-42a7-a218-4b65e2c1904d

--- languages/messages/wikia/MessagesEn.php
+++ languages/messages/wikia/MessagesEn.php
@@ -548,7 +548,7 @@ Please change [[Special:Preferences#prefsection-1|your preferences]] to use this
 
 Any changes that apply to \'\'all\'\' wikis should be made to the Wikia Help copy. [[w:c:help:Help_talk:$1|Suggest changes here]].
 
-Text should be placed on this page if you wish to explain usage, style and policy guidelines which apply only to {{SITENAME}} wiki. Text added in this edit box will be displayed below the boxed help text.',
+Text should be placed on this page if you wish to explain usage, style and policy guidelines which apply only to {{SITENAME}}. Text added in this edit box will be displayed below the boxed help text.',
 'shared_help_search_info' => 'To search for help with editing, please visit the [http://help.wikia.com/wiki/Special:Search?search=$1 Help Wikia]',
 'multiplefileuploadsummary' => 'Summary:',
 'uploadtext-ext' => 'A full list of allowed extensions is available on the [[{{ns:Special}}:Version|wiki version page]].',
