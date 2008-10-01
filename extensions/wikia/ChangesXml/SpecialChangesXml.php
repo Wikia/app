6a5b67b93bdcbc73f972e789531178c8dd36659c
andrewy <andrewy@wikia-inc.com>
2008-10-01 18:50:03 +0000

    #first working version
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2330 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ChangesXml/SpecialChangesXml.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * This extension will keep trak of all changes over N characters in MAIN_NS
+ * This data creates changes.xml file
+ * Author Andrew Yasinky andrewy at wikia.com
+ */
+
+if (!defined('MEDIAWIKI')) {
+        echo <<<EOT
+To install this extension, put the following line in LocalSettings.php:
+require_once( "$IP/extensions/wikia/ChangesXml/SpecialChangesXml.php" );
+EOT;
+    exit( 1 );
+}
+
+$wgExtensionFunctions[] = 'wfSpecialChangesXml';
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'ChangesXml',
+	'author' => 'Andrew Yasinsky',
+	'description' => 'ChagesXml feed',
+);
+
+function wfSpecialChangesXml() {
+  //init
+}
+
+function wfChangesXml( $rc ) {
+
+	//get old/new sizes
+	extract($rc->mExtra);
+
+	if ( isset( $oldSize ) && isset( $newSize ) ) {
+	 //we only look at certain size in main namespace
+	  if( abs( $newSize - $oldSize ) < 100 ){
+	  	return true;
+	  }
+	}
+
+	$titleObj = $rc->getTitle();
+	if(	$titleObj->getNamespace() != NS_MAIN ){
+ 	 //we only want content
+	 return;
+	}	
+
+	$title = $titleObj->getText();
+	$title = str_replace(array("\n", "\r", '_'), array("", "",""), $title);
+	$url = $titleObj->getFullURL();
+
+	//store change
+	$dbw = wfGetDBExt(DB_MASTER) ;
+	
+	$sql = "CREATE TABLE IF NOT EXISTS `_ext_changesxml` ( " .           
+                   " `id` bigint(20) NOT NULL auto_increment, " . 
+                   " `title` varchar(100) default NULL, " .       
+                   " `url` varchar(200) default NULL, " .         
+                   " `timestamp` bigint(20) default NULL, " .      
+                   " PRIMARY KEY  (`id`) " .                      
+                 " ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ";
+	
+	$dbw->query( $sql );
+	
+	$res = $dbw->insert(
+			'_ext_changesxml',
+			array(
+				'title'		=> $title,
+				'url'		=> $url,
+				'timestamp'	=> time(),
+			),
+			__METHOD__
+		);
+
+	if ($dbw->getFlag(DBO_TRX)) {
+		$dbw->commit();
+	}
+	
+  return true;	
+}
+
+if( !empty( $wgEnableSpecialChangesXml ) ){
+	$wgHooks['RecentChange_save'][] = 'wfChangesXml';
+}
