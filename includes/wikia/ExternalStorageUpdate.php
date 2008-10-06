e2cff9cf813cdbf81f0659d6e12a31c889e8534f
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-10-06 11:32:25 +0000

    merge changes back from 200810.1 (r2425:2436)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2437 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/ExternalStorageUpdate.php
+++ includes/wikia/ExternalStorageUpdate.php
@@ -4,37 +4,6 @@
  * in one table.
  */
 
-/**+ tables definition
-
-CREATE TABLE `blobs` (
-  `blob_id` int(10) NOT NULL auto_increment,
-  `rev_wikia_id` int(8) unsigned NOT NULL,
-  `rev_id` int(10) unsigned default NULL,
-  `rev_page_id` int(10) unsigned NOT NULL,
-  `rev_namespace` int(10) unsigned NOT NULL default '0',
-  `rev_user` int(10) unsigned NOT NULL default '0',
-  `rev_user_text` varchar(255) character set latin1 collate latin1_bin NOT NULL default '',
-  `rev_timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `blob_text` mediumtext NOT NULL,
-  `rev_flags` tinyblob default NULL
-  PRIMARY KEY  (`blob_id`),
-  KEY `rev_page_id` (`rev_wikia_id`,`rev_page_id`,`rev_id`),
-  KEY `rev_namespace` (`rev_wikia_id`,`rev_page_id`,`rev_namespace`),
-  KEY `rev_user` (`rev_wikia_id`,`rev_user`,`rev_timestamp`),
-  KEY `rev_user_text` (`rev_wikia_id`,`rev_user_text`,`rev_timestamp`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `pages` (
-  `page_wikia_id` int(8) unsigned NOT NULL,
-  `page_id` int(10) unsigned NOT NULL,
-  `page_namespace` int(10) unsigned NOT NULL default '0',
-  `page_title` varchar(255) NOT NULL,
-  `page_counter` int(8) unsigned NOT NULL default '0',
-  `page_edits` int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY (`page_wikia_id`,`page_id`),
-  KEY `page_namespace` (`page_wikia_id`,`page_namespace`,`page_title`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-**/
 
 $wgHooks[ "RevisionInsertComplete" ][] = "ExternalStorageUpdate::addDeferredUpdate";
 
@@ -65,14 +34,16 @@ class ExternalStorageUpdate {
 		$path = explode( "/", $this->mUrl );
 		$store    = $path[0];
 		$cluster  = $path[2];
-		$id	      = $path[3];
+		$id       = $path[3];
 
 		wfProfileIn( __METHOD__ );
 		if( $this->mRevision instanceof Revision ) {
 			$this->mPageId = $this->mRevision->getPage();
-			$Title = Title::newFromID( $this->mPageId );
+			$Title = Title::newFromID( $this->mPageId, GAID_FOR_UPDATE );
 			if( ! $Title  ) {
-				wfDebug( __METHOD__.": title is null, page id = {$this->mPageId}" );
+				global $wgDBname;
+				wfDebug( __METHOD__.": title is null, page_id={$this->mPageId}; city_id={$wgCityId}, dbname={$wgDBname}" );
+				error_log( __METHOD__.": title is null, page_id={$this->mPageId}; city_id={$wgCityId}, dbname={$wgDBname}" );
 				wfProfileOut( __METHOD__ );
 				return false;
 			}
