336e4e27287df53640916f2604e6ac3942e2693d
andrewy <andrewy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-10-13 21:08:56 +0000

    #appropriate changes to atom
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2582 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ChangesXml/SpecialChangesXml.php
+++ extensions/wikia/ChangesXml/SpecialChangesXml.php
@@ -26,7 +26,7 @@ function wfSpecialChangesXml() {
 }
 
 function wfChangesXml( $rc ) {
-    global $wgEnableSpecialChangesXmlToFeedUrl;
+    global $wgEnableSpecialChangesXmlToFeedUrl, $wgContLang;
 
 	//get old/new sizes
 	extract($rc->mExtra);
@@ -49,14 +49,20 @@ function wfChangesXml( $rc ) {
 	$title = $titleObj->getText();
 	$title = str_replace(array("\n", "\r", '_'), array("", "",""), $title);
 	$url = $titleObj->getFullURL();
+	$base = parse_url( $url );
+	
+	$wiki_url = 'http://' . $base['host'] . '/';
+	$wiki_atom = $wiki_url . 'index.php?title=Special:RecentChanges&amp;feed=atom';	
+	
+	$categories = $titleObj->getParentCategories();
+	$category_string = $wgContLang->getNSText( NS_CATEGORY ).':';
 	
 	//PUT feed
 	if( !empty($wgEnableSpecialChangesXmlToFeedUrl) ){
-		
 			$a_data =  '<feed xmlns="http://www.w3.org/2005/Atom" >' . "\n";
 			$a_data .= '  <title type="text">'. $title .'</title>' . "\n";
-			$a_data .= '  <link href="' . $url .'" />' . "\n";
-			$a_data .= '  <link rel="self" type="application/atom+xml" href="" />' . "\n" ;
+			$a_data .= '  <link href="' . $wiki_url .'" />' . "\n";
+			$a_data .= '  <link rel="self" type="application/atom+xml" href="' . $wiki_atom .'" />' . "\n" ;
 			$a_data .= "  <author><name>" . $rc_user_text . "</name></author>" . "\n";
 			$a_data .= "  <entry>" . "\n";
 			$a_data .= "    <title>" . $title . "</title>" . "\n";
@@ -65,7 +71,9 @@ function wfChangesXml( $rc ) {
 			$a_data .= "    <updated>" . date( DATE_ATOM ) . "</updated>" . "\n";
 			$a_data .= '      <content type="html">' . "\n";
 			$a_data .= "      </content>" . "\n";
-			$a_data .= '    <category term="" />' . "\n";
+		foreach($categories as $key=>$value){	
+			$a_data .= '    <category term="' . str_replace( '_', ' ', str_replace( $category_string, '', $key) ) . '" />' . "\n";
+		}	
 			$a_data .= "  </entry>" . "\n";
 			$a_data .= "</feed>" . "\n";
 		
