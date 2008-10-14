3cac8d9bd1c78450938839ee81dfab585029f9d7
emil <emil@wikia-inc.com>
2008-10-14 09:49:48 +0000

    fixed indenting and formatting
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2595 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ChangesXml/SpecialChangesXml.php
+++ extensions/wikia/ChangesXml/SpecialChangesXml.php
@@ -7,89 +7,89 @@
  */
 
 if (!defined('MEDIAWIKI')) {
-        echo <<<EOT
+	echo <<<EOT
 To install this extension, put the following line in LocalSettings.php:
 require_once( "$IP/extensions/wikia/ChangesXml/SpecialChangesXml.php" );
 EOT;
-    exit( 1 );
+	exit( 1 );
 }
 
 $wgExtensionFunctions[] = 'wfSpecialChangesXml';
 $wgExtensionCredits['specialpage'][] = array(
-	'name' => 'ChangesXml',
-	'author' => 'Andrew Yasinsky',
-	'description' => 'ChagesXml feed',
+		'name' => 'ChangesXml',
+		'author' => 'Andrew Yasinsky',
+		'description' => 'ChagesXml feed',
 );
 
 function wfSpecialChangesXml() {
-  //init
+	//init
 }
 
 function wfChangesXml( $rc ) {
-    global $wgEnableSpecialChangesXmlToFeedUrl, $wgContLang;
+	global $wgEnableSpecialChangesXmlToFeedUrl, $wgContLang;
 
 	//get old/new sizes
-	extract($rc->mExtra);
-    extract($rc->mAttribs); //only to get user text
+	extract( $rc->mExtra );
+	extract( $rc->mAttribs ); //only to get user text
 	
-	if ( isset( $oldSize ) && isset( $newSize ) ) {
-	 //we only look at certain size in main namespace
-	  if( abs( $newSize - $oldSize ) < 100 ){
-	  	return true;
-	  }
+	if( isset( $oldSize ) && isset( $newSize ) ) {
+		//we only look at certain size in main namespace
+		if( abs( $newSize - $oldSize ) < 100 ) {
+			return true;
+		}
 	}
 
 	$titleObj = $rc->getTitle();
 
-	if(	$titleObj->getNamespace() != NS_MAIN ){
-	 //we only want content
-	 return true;
+	if( $titleObj->getNamespace() != NS_MAIN ) {
+		//we only want content
+		return true;
 	}	
 
 	$title = $titleObj->getText();
-	$title = str_replace(array("\n", "\r", '_'), array("", "",""), $title);
+	$title = str_replace( array( "\n", "\r", '_' ), array( "", "","" ), $title );
 	$url = $titleObj->getFullURL();
 	$base = parse_url( $url );
-	
+
 	$wiki_url = 'http://' . $base['host'] . '/';
 	$wiki_atom = $wiki_url . 'index.php?title=Special:RecentChanges&amp;feed=atom';	
-	
+
 	$categories = $titleObj->getParentCategories();
-	$category_string = $wgContLang->getNSText( NS_CATEGORY ).':';
-	
+	$category_string = $wgContLang->getNSText( NS_CATEGORY ) . ':';
+
 	//PUT feed
-	if( !empty($wgEnableSpecialChangesXmlToFeedUrl) ){
-			$a_data =  '<feed xmlns="http://www.w3.org/2005/Atom" >' . "\n";
-			$a_data .= '  <title type="text">'. $title .'</title>' . "\n";
-			$a_data .= '  <link href="' . $wiki_url .'" />' . "\n";
-			$a_data .= '  <link rel="self" type="application/atom+xml" href="' . $wiki_atom .'" />' . "\n" ;
-			$a_data .= "  <author><name>" . $rc_user_text . "</name></author>" . "\n";
-			$a_data .= "  <entry>" . "\n";
-			$a_data .= "    <title>" . $title . "</title>" . "\n";
-			$a_data .= '    <link href="' . $url . '" />' . "\n";
-			$a_data .= "    <published>" . date( DATE_ATOM ) . "</published>" . "\n";
-			$a_data .= "    <updated>" . date( DATE_ATOM ) . "</updated>" . "\n";
-			$a_data .= '      <content type="html">' . "\n";
-			$a_data .= "      </content>" . "\n";
-		foreach($categories as $key=>$value){	
+	if( !empty( $wgEnableSpecialChangesXmlToFeedUrl ) ) {
+		$a_data =  '<feed xmlns="http://www.w3.org/2005/Atom" >' . "\n";
+		$a_data .= '  <title type="text">'. $title .'</title>' . "\n";
+		$a_data .= '  <link href="' . $wiki_url .'" />' . "\n";
+		$a_data .= '  <link rel="self" type="application/atom+xml" href="' . $wiki_atom .'" />' . "\n" ;
+		$a_data .= "  <author><name>" . $rc_user_text . "</name></author>" . "\n";
+		$a_data .= "  <entry>" . "\n";
+		$a_data .= "    <title>" . $title . "</title>" . "\n";
+		$a_data .= '    <link href="' . $url . '" />' . "\n";
+		$a_data .= "    <published>" . date( DATE_ATOM ) . "</published>" . "\n";
+		$a_data .= "    <updated>" . date( DATE_ATOM ) . "</updated>" . "\n";
+		$a_data .= '      <content type="html">' . "\n";
+		$a_data .= "      </content>" . "\n";
+		foreach( $categories as $key=>$value ) {
 			$a_data .= '    <category term="' . str_replace( '_', ' ', str_replace( $category_string, '', $key) ) . '" />' . "\n";
-		}	
-			$a_data .= "  </entry>" . "\n";
-			$a_data .= "</feed>" . "\n";
-		
-			$ch = curl_init();
-	    	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-	    	curl_setopt($ch, CURLOPT_URL, $wgEnableSpecialChangesXmlToFeedUrl );
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Length: ".strlen($a_data)));
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $a_data);
-			$r = curl_exec($ch);
-		
-	}	
-	
-  return true;	
+		}
+		$a_data .= "  </entry>" . "\n";
+		$a_data .= "</feed>" . "\n";
+
+		$ch = curl_init();
+		curl_setopt( $ch, CURLOPT_TIMEOUT, 5 );
+		curl_setopt( $ch, CURLOPT_URL, $wgEnableSpecialChangesXmlToFeedUrl );
+		curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "PUT" );
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, array( "Content-Length: " . strlen( $a_data ) ) );
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, $a_data);
+		$r = curl_exec( $ch );
+
+	}
+
+	return true;
 }
 
-if( !empty( $wgEnableSpecialChangesXmlToFeedUrl ) ){
+if( !empty( $wgEnableSpecialChangesXmlToFeedUrl ) ) {
 	$wgHooks['RecentChange_save'][] = 'wfChangesXml';
 }
