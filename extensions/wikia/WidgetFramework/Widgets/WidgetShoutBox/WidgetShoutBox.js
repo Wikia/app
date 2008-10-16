f8d19057b6b7a4f24ba9396a6526b164994bf546
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-10-16 19:07:01 +0000

    #3691 - removing messages from shoutbox
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2731 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WidgetFramework/Widgets/WidgetShoutBox/WidgetShoutBox.js
+++ extensions/wikia/WidgetFramework/Widgets/WidgetShoutBox/WidgetShoutBox.js
@@ -1,12 +1,12 @@
 function WidgetShoutBoxSend(widgetId) {
 
-    var messageBox = YAHOO.util.Dom.get(widgetId + '_message');
-    
-    if (!messageBox || messageBox.value == '') {
+	var messageBox = YAHOO.util.Dom.get(widgetId + '_message');
+
+	if (!messageBox || messageBox.value == '') {
 	return false;
-    }
-    
-    var callback = {
+	}
+
+	var callback = {
 		success: function(o) {
 			id = o.argument;
 			res = YAHOO.Tools.JSONParse(o.responseText);
@@ -16,10 +16,10 @@ function WidgetShoutBoxSend(widgetId) {
 				if(res.title) {
 					YAHOO.util.Dom.get(id+'_header').innerHTML = res.title;
 				}
-				
+
 				// focus on message input
 				YAHOO.util.Dom.get(id + '_message').focus();
-			} 
+			}
 			else {
 				// ..should never occur
 			}
@@ -28,37 +28,73 @@ function WidgetShoutBoxSend(widgetId) {
 			// ..should never occur
 		},
 		argument: widgetId
-    }
-    
-    message = encodeURIComponent( messageBox.value );
+	}
 
-    YAHOO.util.Connect.asyncRequest('GET', wgScriptPath + '/index.php?action=ajax&rs=WidgetFrameworkAjax&actionType=configure&id='+widgetId+'&skin='+skin+'&message='+message, callback);
-    YAHOO.util.Dom.get(widgetId+'_content').innerHTML = '';
-    YAHOO.util.Dom.addClass(widgetId+'_content', 'widget_loading');
-    
-    return true;
+	message = encodeURIComponent( messageBox.value );
+
+	YAHOO.util.Connect.asyncRequest('GET', wgScriptPath + '/index.php?action=ajax&rs=WidgetFrameworkAjax&actionType=configure&id='+widgetId+'&skin='+skin+'&message='+message, callback);
+	YAHOO.util.Dom.get(widgetId+'_content').innerHTML = '';
+	YAHOO.util.Dom.addClass(widgetId+'_content', 'widget_loading');
+
+	return true;
 }
 
 
 function WidgetShoutboxTabToogle(widgetId, tab) {
 
-	    var currentTab = (tab == 0) ? 'online' : 'chat';
-	    var onlineTab = YAHOO.util.Dom.get(widgetId+"_online");
-	    var chatTab   = YAHOO.util.Dom.get(widgetId+"_chat");
+		var currentTab = (tab == 0) ? 'online' : 'chat';
+		var onlineTab = YAHOO.util.Dom.get(widgetId+"_online");
+		var chatTab   = YAHOO.util.Dom.get(widgetId+"_chat");
 
-	    // switch tabs and save current state to cookie
-	    switch(currentTab)
-	    {
+		// switch tabs and save current state to cookie
+		switch(currentTab)
+		{
 		case "online":
-		    onlineTab.style.display = "none";
-		    chatTab.style.display   = "";
-		    YAHOO.Tools.setCookie(widgetId + "_showChat", 1, new Date(2030, 0, 1));
-		    break;
+			onlineTab.style.display = "none";
+			chatTab.style.display   = "";
+			YAHOO.Tools.setCookie(widgetId + "_showChat", 1, new Date(2030, 0, 1));
+			break;
 
 		case "chat":
-		    onlineTab.style.display = "";
-		    chatTab.style.display   = "none";
-		    YAHOO.Tools.setCookie(widgetId + "_showChat", 0, new Date(2030, 0, 1));
-		    break;
-	    }
+			onlineTab.style.display = "";
+			chatTab.style.display   = "none";
+			YAHOO.Tools.setCookie(widgetId + "_showChat", 0, new Date(2030, 0, 1));
+			break;
+		}
+}
+
+function WidgetShoutBoxRemoveMsg(widgetId, aElem) {
+	var chatTab = YAHOO.util.Dom.get(widgetId+"_chat");
+	var msgId = aElem.parentNode.getAttribute('msgid');
+
+	if (!chatTab || !msgId) {
+		return false;
+	}
+
+	var callback = {
+		success: function(o) {
+			id = o.argument;
+			res = YAHOO.Tools.JSONParse(o.responseText);
+			if(res.success) {
+				YAHOO.util.Dom.removeClass(id+'_content', 'widget_loading');
+				YAHOO.util.Dom.get(id+'_content').innerHTML = res.body;
+				if(res.title) {
+					YAHOO.util.Dom.get(id+'_header').innerHTML = res.title;
+				}
+				// focus on message input
+				YAHOO.util.Dom.get(id + '_message').focus();
+			}
+			else {
+				// ..should never occur
+			}
+		},
+		failure: function(o) {
+			// ..should never occur
+		},
+		argument: widgetId
+	}
+
+	YAHOO.util.Connect.asyncRequest('GET', wgScriptPath + '/index.php?action=ajax&rs=WidgetFrameworkAjax&actionType=configure&id='+widgetId+'&msgid='+msgId, callback);
+	YAHOO.util.Dom.get(widgetId+'_content').innerHTML = '';
+	YAHOO.util.Dom.addClass(widgetId+'_content', 'widget_loading');
 }
