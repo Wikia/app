555584ab6e9714989ee68466bac6d62e309b43aa
macbre <macbre@wikia-inc.com>
2008-10-17 12:33:10 +0000

    WikiaBotJS: insert JS code after <link> tags and add support for console.log()
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2749 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaBotJS/WikiaBotJS.php
+++ extensions/wikia/WikiaBotJS/WikiaBotJS.php
@@ -12,8 +12,8 @@ $wgExtensionFunctions[] = 'wfWikiaBotJS';
 $wgExtensionCredits['other'][] = array(
 	'name' => 'WikiaBotJS',
 	'author' => 'Maciej Brencz',
-	'description' => 'Allow inclusion of JS code inside <head> section.',
-	'version' => 0.1
+	'description' => 'Allow inclusion of JS code inside <head> section',
+	'version' => 0.2
 );
 
 // extension setup
@@ -22,34 +22,37 @@ function wfWikiaBotJS() {
 	// show JS to limited number of users
 	$allowedUsers = array('WikiaBot', 'Macbre', 'Galezewski', 'TOR', 'BartL', 'Adi3ek');
 	if (in_array($wgUser->getName(), $allowedUsers)) {
-		$wgHooks['BeforePageDisplay'][] = 'wfWikiaBotJSInsert';
+		$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'wfWikiaBotJSInsert';
 	}
 }
 
 // insert JS code
-function wfWikiaBotJSInsert(&$out, &$skin) {
+function wfWikiaBotJSInsert(&$skin, &$tpl) {
 
 	$JS = <<<EOD
+		<script type="text/javascript">/*<![CDATA[*/
+			// WikiaBotJS
+			window.onerror = function (msg, src, lno) {
+				url = 'ael_msg=' + encodeURIComponent(msg);
+				url += '&ael_url=' + encodeURIComponent(src);
+				url += '&ael_lno=' + encodeURIComponent(lno);
+				url += '&location=' + encodeURIComponent(document.location);
+				url += '&rand=' + Math.random(); // prevent caching
 
-<script type="text/javascript">/*<![CDATA[*/
-// WikiaBotJS
-window.onerror = function (msg, src, lno) {
- var url = "";
- url += ((url != "") ? "&" : "") + encodeURIComponent('ael_msg') + "=" + encodeURIComponent(msg);
- url += ((url != "") ? "&" : "") + encodeURIComponent('ael_url') + "=" + encodeURIComponent(src);
- url += ((url != "") ? "&" : "") + encodeURIComponent('ael_lno') + "=" + encodeURIComponent(lno);
- url += '&location=' + encodeURIComponent(document.location);
- url += '&rand=' + Math.random(); // prevent caching
- 
- img = new Image();
- img.src = 'http://ws2.poz.wikia-inc.com/~lukasz/ael.php?' + url;
+				img = new Image();
+				img.src = 'http://ws2.poz.wikia-inc.com/~lukasz/ael.php?' + url;
 
- return false;
-}	
-/*]]>*/</script>
+				if (typeof console != "undefined") {
+					console.log('WikiaBotJS: error "' + msg + '" @ ' + src  + ':' + lno  + ' reported to our QA team');
+				}
+
+				return false;
+			}
+			if (typeof console != "undefined") console.log('WikiaBotJS: ok');
+		/*]]>*/</script>
 
 EOD;
 
-	$out->addHeadItem('wikiaBotJS', $JS);
+	$tpl->data['headlinks'] .= $JS;
 	return true;
 }
