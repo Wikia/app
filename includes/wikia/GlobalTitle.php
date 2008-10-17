24728fa941e6b27fcabcfc61da43921c91e70b9c
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-10-17 14:33:28 +0000

    getFullUrl work
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2763 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/GlobalTitle.php
+++ includes/wikia/GlobalTitle.php
@@ -11,28 +11,31 @@ class GlobalTitle {
 	/**
 	 * public, used in static constructor
 	 */
-	public $mDefaultText = false;
+	public $mText = false;
 	public $mNamespace = false;
 	public $mCityId = false;
 	public $mTextform = false;
+	public $mUrlform = false;
 
 	/**
 	 * others, private
 	 */
 	private $mServer = false;
 	private $mContLang = false;
+	private $mArticlePath = false;
 
 	/**
 	 * static constructor, Create new Title from name of page
 	 */
-	public static function newFromText( $text, $defaultNamespace = NS_MAIN, $city_id = false ) {
+	public static function newFromText( $text, $namespace, $city_id ) {
 
 		$filteredText = Sanitizer::decodeCharReferences( $text );
 		$title = new GlobalTitle();
 
-		$title->mDefaultText = $filteredText;
-		$title->mTextform = str_replace( '_', ' ', $title->mDefaultText );
-		$title->mNamespace = $defaultNamespace;
+		$title->mText = $filteredText;
+		$title->mUrlform = $filteredText;
+		$title->mTextform = str_replace( '_', ' ', $title->mText );
+		$title->mNamespace = $namespace;
 		$title->mCityId = $city_id;
 
 		return $title;
@@ -86,7 +89,16 @@ class GlobalTitle {
 
 		if( ! $this->mServer ) {
 			$this->loadServer();
+			$this->loadArticlePath();
 		}
+		$namespace = wfUrlencode( $this->getNsText() );
+		/**
+		 * replace $1 with article title with namespace
+		 */
+		$url = str_replace( '$1', $namespace . $this->mUrlform, $this->mArticlePath );
+		$url = wfAppendQuery( $this->mServer . $url, $query );
+
+		return $url;
 	}
 
 
@@ -95,7 +107,7 @@ class GlobalTitle {
 	 *
 	 * Determine wgServer value from WikiFactory variables
 	 *
-	 * @return string, $wgServer for $city_id
+	 * @return string
 	 */
 	private function loadServer() {
 		/**
@@ -125,5 +137,53 @@ class GlobalTitle {
 		}
 
 		return false;
+	}
+
+	/**
+	 * loadArticlePath
+	 *
+	 * Determine wgArticlePath value from WikiFactory variables
+	 *
+	 * @return string
+	 */
+	private function loadArticlePath() {
+		global $wgArticlePath;
+
+		/**
+		 * don't do this twice
+		 */
+		if( $this->mArticlePath ) {
+			return $this->mArticlePath;
+		}
+
+		/**
+		 * get value from city_variables
+		 */
+		$path = WikiFactory::getVarValueByName( "wgArticlePath", $this->mCityId );
+		if( ! $path ) {
+			/**
+			 * it's 100% true but it's at least something
+			 */
+			$path = $wgArticlePath;
+		}
+
+		/**
+		 * replace all variables with proper values (for example wgScriptPath)
+		 */
+		preg_match_all( '/(\$\w+)[^\w]*/', $path, $vars );
+		if( is_array( $vars[1] ) ) {
+			foreach( $vars[1] as $var ) {
+				$key = ltrim( $var, '$' );
+				if( ! is_numeric( $key) ) {
+					$replace = WikiFactory::getVarValueByName( $key, $this->mCityId );
+					if( !$replace ) {
+						$replace = $$key;
+					}
+					$path = str_replace( $var, $replace, $path  );
+				}
+			}
+		}
+		$this->mArticlePath = $path;
+		return $path;
 	}
 }
--- includes/wikia/t/GlobalTitleTest.php
+++ includes/wikia/t/GlobalTitleTest.php
@@ -15,26 +15,31 @@ require_once( "simpletest/reporter.php" );
 
 class GlobalTitleCase extends UnitTestCase {
 
-	function testNewFromText1Param() {
-		$title = GlobalTitle::newFromText( "Test" );
+	function testNewFromText1() {
+		$title = GlobalTitle::newFromText( "Test", NS_MAIN, 177 );
 		$this->assertTrue( $title->getNamespace() === NS_MAIN );
 		$this->assertTrue( $title->getNsText() === "" ) ;
 		$this->assertTrue( $title->getText() === "Test" );
 
-		$title = GlobalTitle::newFromText( "Test_Ze_Spacjami" );
+		$title = GlobalTitle::newFromText( "Test_Ze_Spacjami", NS_MAIN, 177 );
 		$this->assertTrue( $title->getText() === "Test Ze Spacjami", "Underscores, spaces expected" );
 
 	}
 
-	function testNewFromText2Param() {
-		$title = GlobalTitle::newFromText( "Test", NS_TALK );
+	function testNewFromText2() {
+		$title = GlobalTitle::newFromText( "Test", NS_TALK, 177 );
 		$this->assertTrue( $title->getNamespace() === NS_TALK );
 		$this->assertTrue( $title->getNsText() === "Talk" );
 		$this->assertTrue( $title->getText() === "Test" );
 
-		$title = GlobalTitle::newFromText( "Test_Ze_Spacjami", NS_TALK );
+		$title = GlobalTitle::newFromText( "Test_Ze_Spacjami", NS_TALK, 177 );
 		$this->assertTrue( $title->getText() === "Test Ze Spacjami", "Underscores, spaces expected" );
 
+	}
+
+	function testUrls() {
+		$title = GlobalTitle::newFromText( "Timeline", NS_MAIN, 113 ); # memory-alpha
+		$this->assertTrue( $title->getFullURL() === "http://memory-alpha.org/en/wiki/Timeline" );
 	}
 
 };
