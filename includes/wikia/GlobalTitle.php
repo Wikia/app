63b5102846bd5f7c2440b9eb2d80f67cec687335
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-10-17 12:33:27 +0000

    loadServer for GlobalTitle
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2750 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/GlobalTitle.php
+++ includes/wikia/GlobalTitle.php
@@ -8,9 +8,19 @@
 
 class GlobalTitle {
 
+	/**
+	 * public, used in static constructor
+	 */
 	public $mDefaultText = false;
 	public $mNamespace = false;
 	public $mCityId = false;
+	public $mTextform = false;
+
+	/**
+	 * others, private
+	 */
+	private $mServer = false;
+	private $mContLang = false;
 
 	/**
 	 * static constructor, Create new Title from name of page
@@ -21,6 +31,7 @@ class GlobalTitle {
 		$title = new GlobalTitle();
 
 		$title->mDefaultText = $filteredText;
+		$title->mTextform = str_replace( '_', ' ', $title->mDefaultText );
 		$title->mNamespace = $defaultNamespace;
 		$title->mCityId = $city_id;
 
@@ -32,15 +43,15 @@ class GlobalTitle {
 	}
 
 	/**
-     * Get the namespace text
-     * @return string
-     */
-    public function getNsText() {
-        global $wgContLang, $wgCanonicalNamespaceNames;
+	 * Get the namespace text
+	 * @return string
+	 */
+	public function getNsText() {
+		global $wgContLang, $wgCanonicalNamespaceNames;
 
-        if( isset( $wgCanonicalNamespaceNames[ $this->mNamespace ] ) ) {
-            return $wgCanonicalNamespaceNames[ $this->mNamespace ];
-        }
+		if( isset( $wgCanonicalNamespaceNames[ $this->mNamespace ] ) ) {
+			return $wgCanonicalNamespaceNames[ $this->mNamespace ];
+		}
 
 		/**
 		 * get extra namespaces for city_id, they have to be defined in
@@ -48,10 +59,71 @@ class GlobalTitle {
 		 */
 		$localNamespaces = WikiFactory::getVarValueByName( "wgExtraNamespacesLocal", $this->mCityId );
 
-        if( isset( $localNamespaces[ $this->mNamespace ] ) ) {
-            return $localNamespaces[ $this->mNamespace ];
-        }
+		if( isset( $localNamespaces[ $this->mNamespace ] ) ) {
+			return $localNamespaces[ $this->mNamespace ];
+		}
 
 		return $wgContLang->getNsText( $this->mNamespace );
-    }
+	}
+
+	/**
+	 * Get the text form (spaces not underscores) of the main part
+	 * @return string
+	 */
+	public function getText() {
+		return $this->mTextform;
+	}
+
+	/**
+	 * Get a real URL referring to this title
+	 *
+	 *
+	 * @param string $query an optional query string
+	 * @param string $variant language variant of url (for sr, zh..)
+	 * @return string the URL
+	 */
+	public function getFullURL( $query = '', $variant = false ) {
+
+		if( ! $this->mServer ) {
+			$this->loadServer();
+		}
+	}
+
+
+	/**
+	 * loadServer
+	 *
+	 * Determine wgServer value from WikiFactory variables
+	 *
+	 * @return string, $wgServer for $city_id
+	 */
+	private function loadServer() {
+		/**
+		 * don't do this twice
+		 */
+		if( $this->mServer ) {
+			return $this->mServer;
+		}
+
+		/**
+		 * get value from city_variables
+		 */
+		$server = WikiFactory::getVarValueByName( "wgServer", $this->mCityId );
+		if( $server ) {
+			$this->mServer = $server;
+			return $server;
+		}
+
+		/**
+		 * get value from city_list.city_url
+		 */
+		$city = WikiFactory::getWikiByID( $city_id );
+		if( $city ) {
+			$server = rtrim( $server->city_url, "/" );
+			$this->mServer = $server;
+			return $server;
+		}
+
+		return false;
+	}
 }
