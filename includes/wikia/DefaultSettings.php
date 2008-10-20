0f10b9fde5c2e35b575863f9011f74c15a4d1fee
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-10-20 12:31:19 +0000

    load GlobalTitle when needed
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@2809 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -60,6 +60,7 @@ global $wgAutoloadClasses;
  * custom wikia classes
  */
 $wgAutoloadClasses["EasyTemplate"]  =  $GLOBALS["IP"]."/includes/wikia/EasyTemplate.php";
+$wgAutoloadClasses["GlobalTitle"]  =  $GLOBALS["IP"]."/includes/wikia/GlobalTitle.php";
 $wgAutoloadClasses["Wikia"] = "includes/wikia/Wikia.php";
 $wgAutoloadClasses["WikiFactory"] = $GLOBALS["IP"]."/extensions/wikia/WikiFactory/WikiFactory.php";
 $wgAutoloadClasses["WikiMover"] = $GLOBALS["IP"]."/extensions/wikia/WikiFactory/Mover/WikiMover.php";
@@ -157,8 +158,8 @@ include_once( "$IP/extensions/wikia/MostPopularCategories/SpecialMostPopularCate
 include_once( "$IP/extensions/wikia/MostPopularArticles/SpecialMostPopularArticles.php" );
 include_once( "$IP/extensions/wikia/MostVisitedPages/SpecialMostVisitedPages.php" );
 include_once( "$IP/extensions/wikia/WidgetSpecialPage/WidgetsSpecialPage.php" );
-
 include_once( "$IP/extensions/ParserDiffTest/ParserDiffTest.php" );
+
 
 /**
  * Wikia libmemcached client
