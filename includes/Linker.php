656b72f0d88bb4477eb9389cc21363f28f546776
macbre <macbre@wikia-inc.com>
2008-11-04 11:15:08 +0000

    Linker: kill notice related to refId
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3190 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/Linker.php
+++ includes/Linker.php
@@ -730,7 +730,8 @@ class Linker {
 			$s = $thumb->toHtml( $attrArr );
 		}
 		if ( '' != $fp['align'] ) {
-			$s = "<div{$fp['refid']} class=\"float{$fp['align']}\"><span>{$s}</span></div>";
+			$refId = isset($fp['refid']) ? " refid=\"{$fp['refid']}\"" : '';
+			$s = "<div$refId class=\"float{$fp['align']}\"><span>{$s}</span></div>";
 		}
 		return str_replace("\n", ' ', $prefix.$s.$postfix);
 	}
