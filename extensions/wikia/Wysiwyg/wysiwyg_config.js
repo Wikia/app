946f31bb8cbde68690ee588278fc35768d838689
macbre <macbre@wikia-inc.com>
2008-11-05 10:07:47 +0000

    Wysiwyg: turn FF spell checker on
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3228 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Wysiwyg/wysiwyg_config.js
+++ extensions/wikia/Wysiwyg/wysiwyg_config.js
@@ -20,6 +20,7 @@ FCKConfig.FormatOutput = false;
 
 FCKConfig.AutoDetectLanguage = false;
 FCKConfig.DefaultLanguage = window.parent.wgUserLanguage;
+FCKConfig.FirefoxSpellChecker = true;
 
 FCKConfig.BackgroundBlockerColor = '#000';
 FCKConfig.BackgroundBlockerOpacity = '0.6';
