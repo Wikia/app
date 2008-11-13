b9adb2c12d26037071eddb6771c53aeed82495cd
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-11-13 16:05:51 +0000

    Special:CreateBlogPost added
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3397 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Blogs/templates/createPostConfirm.tmpl.php
@@ -0,0 +1,6 @@
+<!-- s:<?php echo __FILE__ ?> -->
+<strong><?php echo wfMsg( "create-blog-confirm-title" ) ?></strong><br />
+<br />
+<?php echo wfMsg( "create-blog-confirm-info", array($title->getFullURL()) ); ?>&nbsp;
+<!-- e:<?= __FILE__ ?> -->
+
