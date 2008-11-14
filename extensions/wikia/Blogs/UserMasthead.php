a5aa402b4cf13c230ab2ec4edd61043c98f95364
eloy <eloy@wikia-inc.com>
2008-11-14 15:12:16 +0000

    css path
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3438 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/UserMasthead.php
+++ extensions/wikia/Blogs/UserMasthead.php
@@ -2,12 +2,11 @@
 $wgHooks['MonacoBeforePageBar'][] = 'userMasthead';
 
 function userMasthead() {
-	global $wgTitle, $wgUser, $userMasthead, $wgOut;
+	global $wgTitle, $wgUser, $userMasthead, $wgOut, $wgExtensionsPath, $wgStyleVersion;
 
 	$namespace = $wgTitle->getNamespace();
 	if ( $namespace == NS_BLOG_ARTICLE || $namespace == NS_USER || $namespace == NS_USER_TALK || $namespace == NS_SPECIAL && ($wgTitle->getDBkey() == 'Watchlist' || $wgTitle->getDBkey() == 'EmailUser' || $wgTitle->getDBkey() == 'WidgetDashboard' || $wgTitle->getDBkey() == 'Preferences')) {
-
-		$wgOut->addScript('<link rel="stylesheet" type="text/css" href="{$wgExtensionsPath}/wikia/Blogs/css/UserMasthead.css?{$wgStyleVersion}" />');
+		$wgOut->addScript("<link rel=\"stylesheet\" type=\"text/css\" href=\"{$wgExtensionsPath}/wikia/Blogs/css/UserMasthead.css?{$wgStyleVersion}\" />");
 
 		$userMasthead = true; //hides article/talk tabs in Monaco.php
 		$out = array();
