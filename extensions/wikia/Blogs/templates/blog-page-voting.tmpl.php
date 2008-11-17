ecb1eda12729d5aaf4bb4e533d47b593654cb31c
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-11-17 09:31:24 +0000

    added function to display number of votes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3459 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Blogs/templates/blog-page-voting.tmpl.php
@@ -0,0 +1,1 @@
+<div id="star-rating" class="star-rating"><div class="star-rating" style="background-position:left center;position:absolute;text-indent:-9000px;z-index:1;width: <?= $ratingPx ?>px;"></div></div>
