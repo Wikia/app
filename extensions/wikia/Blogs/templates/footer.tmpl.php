cbfddda826ab70a73b8ac84ee3abace1bffba86c
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-11-18 14:27:38 +0000

    rate article
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3524 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/footer.tmpl.php
+++ extensions/wikia/Blogs/templates/footer.tmpl.php
@@ -4,7 +4,18 @@
 			<?php echo wfMsg("blog-last-edited", array( $edited ) ) ?>
 		</li>
 		<li>
-			<!-- stars rating -->
+			<!-- stars rating / copied from Monaco skin -->
+			<div id="star-rating-wrapper">
+				<ul id="star-rating" class="star-rating">
+					<li style="width: <?php echo $ratingPx ?>px;" id="current-rating" class="current-rating"><span><?= $rating ?>/5</span></li>
+					<li><a class="one-star" id="star1" title="1/5"<?=$hidden_star?>><span>1</span></a></li>
+					<li><a class="two-stars" id="star2" title="2/5"<?=$hidden_star?>><span>2</span></a></li>
+					<li><a class="three-stars" id="star3" title="3/5"<?=$hidden_star?>><span>3</span></a></li>
+					<li><a class="four-stars" id="star4" title="4/5"<?=$hidden_star?>><span>4</span></a></li>
+					<li><a class="five-stars" id="star5" title="5/5"<?=$hidden_star?>><span>5</span></a></li>
+				</ul>
+				<span style="<?= ($voted ? '' : 'display: none;') ?>" id="unrateLink"><a id="unrate" href="#"><?= wfMsg( 'unrate_it' ) ?></a></span>
+			</div>
 		</li>
 	</ul>
 </div>
