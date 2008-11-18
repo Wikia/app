19991de3ee1b1848d77abbd1d3ed24dbfdab882e
nick <nick@wikia-inc.com>
2008-11-18 07:20:16 +0000

    Checkpoint
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3505 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/Athena.js
+++ extensions/wikia/AdEngine/Athena.js
@@ -6,24 +6,20 @@
 
 Athena = {};
 Athena.pageVars = new Array();
-Athena.configUrl = "/athena/AthenaConfig.php";
+Athena.configUrl = "http://athena.dev.wikia-inc.com/athena/configApi.php";
 Athena.setup = function (){
-	// Allow for the debug level to be set in the url
-	var athenaUrlDebug = document.location.search.match(/athena_debug=[A-Za-z0-9_\-]+/);
-	if (athenaUrlDebug != null ){ 
-		Athena.debugLevel = athenaUrlDebug.toString().substr(13);
-		Athena.debug("Debug level set to " + Athena.debugLevel);
-	} else {
-		Athena.debugLevel = 0;
-	}
-
+	Athena.debugLevel = Athena.getRequestVal('athena_debug', 0);
 };
 
 /* Pull the current configuration from our web servers via json 
  */
 Athena.pullConfig = function (){
 	Athena.debug("pullConfig() called");
-	Athena.loadScript(Athena.configUrl, true);
+	var passVars = new Array();
+	passVars['pageVars'] = Athena.json_encode(Athena.pageVars);
+	var myConfigUrl = Athena.configUrl + '?athena_debug=' + Athena.debugLevel;
+	myConfigUrl += '&' + Athena.buildQueryString(passVars);
+	Athena.loadScript(myConfigUrl, true);
 };
 
 Athena.callAd = function (slotname){
@@ -54,7 +50,6 @@ Athena.hop = function (slotname){
 /* Send a beacon back to our server so we know if it worked */
 Athena.sendBeacon = function (success, network, networkInfo, geography, site, page){
 	Athena.debug("sendBeacon() called");
-
 };
 
 
@@ -125,6 +120,101 @@ Athena.print_r = function (arr,level) {
 		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
 	}
 	return dumped_text;
+};
+
+/*
+ * Converts the given data structure to a JSON string.
+ * Argument: arr - The data structure that must be converted to JSON
+ * http://www.openjs.com/scripts/data/json_encode.php
+ */
+Athena.json_encode = function (arr) {
+    var parts = [];
+    var is_list = (Object.prototype.toString.apply(arr) === '[object Array]');
+
+    for(var key in arr) {
+    	var value = arr[key];
+        if(typeof value == "object") { //Custom handling for arrays
+            if(is_list) parts.push(Athena.json_encode(value)); /* :RECURSION: */
+            else parts[key] = Athena.json_encode(value); /* :RECURSION: */
+        } else {
+            var str = "";
+            if(!is_list) str = '"' + key + '":';
+
+            //Custom handling for multiple data types
+            if(typeof value == "number") str += value; //Numbers
+            else if(value === false) str += 'false'; //The booleans
+            else if(value === true) str += 'true';
+            else str += '"' + value + '"'; //All other things
+            // :TODO: Is there any more datatype we should be in the lookout for? (Functions?)
+
+            parts.push(str);
+        }
+    }
+    var json = parts.join(",");
+    
+    if(is_list) return '[' + json + ']';//Return numerical JSON
+    return '{' + json + '}';//Return associative JSON
+};
+
+
+/* Nick wrote: This code looks at the query string supplied in the url and parses it.
+ * It returns an associative array of url decoded name value pairs
+ */
+Athena.parseQueryString = function (qstring){
+  var ret=new Array();
+  var varName, varValue, nvpairs, intIndex, i;
+  
+  if (qstring.charAt(0) == '?') qstring = qstring.substr(1);
+  if (qstring.length == 0) return ret;
+  
+  qstring=qstring.replace(/\;/g, '&', qstring);
+  
+  nvpairs=qstring.split('&');
+
+  for (i=0; i < nvpairs.length; i++){
+    if (nvpairs[i].length == 0){
+      continue;
+    }
+
+    if ((intIndex = nvpairs[i].indexOf('=')) != -1) {
+      varName = decodeURIComponent(nvpairs[i].substr(0, intIndex));
+      varValue = decodeURIComponent(nvpairs[i].substr(intIndex + 1)); 
+    } else {
+      varName = decodeURIComponent(nvpairs[i].substr(0, intIndex));
+      varValue = '';
+    }
+
+    if (varName=='' || varValue==''){
+      continue;
+    }
+
+    ret[varName]=varValue;
+  }
+
+  return ret;
+};
+
+/* Look for a particular value in the query string, and return it. Optional second arg is a default. */
+Athena.getRequestVal = function(varname, defaultval){
+	nvpairs = Athena.parseQueryString(document.location.search);
+	if (nvpairs[varname] !== undefined){
+		return nvpairs[varname];
+	} else {
+		return defaultval;
+	}
+};
+
+
+/* Take an array of name value pairs and return a url encoded string */
+Athena.buildQueryString = function (nvpairs){
+	var ret='';
+	for (name in nvpairs){
+		if (ret !=''){
+			ret += "&";
+		}
+		ret += escape(name) + '=' + escape(nvpairs[name]);
+	}
+	return ret;
 };
 
 // Init
