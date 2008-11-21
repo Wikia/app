997a27cbafde992e0b31705e92627cb3a68d68c4
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-11-21 10:56:00 +0000

    * Wysiwyg: Parser improvements
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3667 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/parser/Preprocessor_DOM.php
+++ includes/parser/Preprocessor_DOM.php
@@ -503,7 +503,7 @@ class Preprocessor_DOM implements Preprocessor {
 					if(!empty($wgWysiwygParserEnabled)) {
 						global $wgWikitext;
 						$wgWikitext[] = $element;
-						$element .= "start-".(count($wgWikitext)-1)."-stop";
+						$element .= "\x7f-start-".(count($wgWikitext)-1)."-stop";
 					}
 				} else {
 					# Create XML element
