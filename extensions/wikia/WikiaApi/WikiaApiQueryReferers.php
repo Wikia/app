e582eec497e6e45142b993ee72065cffebd0e7c3
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-11-24 11:27:09 +0000

    next version of api for referrers widget
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3700 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaApi/WikiaApiQueryReferers.php
+++ extensions/wikia/WikiaApi/WikiaApiQueryReferers.php
@@ -5,131 +5,79 @@
  *
  * @author Piotr Molski (moli) <moli@wikia.com>
  *
- * @todo
- *
- */
-
-/** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- *
- * PLEASE SET UP GLOBALS :
- *
- * $wgDBuser,
- * $wgDBpassword,
- * $wgDBStatsServer,
- * $wgDBStats;
- *
- * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  */
 
 class WikiaApiQueryReferers extends WikiaApiQuery {
-    /**
-     * constructor
-     */
-	public function __construct($query, $moduleName) {
-		parent :: __construct($query, $moduleName);
-	}
 
-    /**
-     * main function
-     */
+	public function __construct($query, $moduleName) { parent :: __construct($query, $moduleName); }
+
 	public function execute() {
 		global $wgUser;
 
 		switch ($this->getActionName()) {
-			case parent::INSERT :
-								{
-									// needed?
-									break;
-								}
-			case parent::UPDATE :
-								{
-									// needed?
-									break;
-								}
-			case parent::DELETE :
-								{
-									// needed?
-									break;
-								}
-			default:
-								{
-									$this->getWikiReferers();
-									break;
-								}
+			case parent::INSERT : break;
+			case parent::UPDATE : break;
+			case parent::DELETE : break;
+			default: $this->getWikiReferers(); break;
 		}
 	}
 
-	protected function getDB() {
-		return wfGetDBExt(DB_SLAVE);
-	}
+	protected function getDB() { return wfGetDBExt(DB_SLAVE); }
 
-	/*
-	 * Get votes of articles
-	 */
-	private function getWikiReferers ()
-	{
-		global $wgDBStats, $wikiaCityId;
-	    global $wgServerName, $wgDBname;
-
-        #--- initial parameters (dbname, limit, offset ...)
+	private function getWikiReferers () {
+		global $wgDBStats, $wgCityId;
+		global $wgServerName, $wgDBname;
+		
+		#--- initial parameters (dbname, limit, offset ...)
 		extract($this->getInitialParams());
-
-        #--- request parameters ()
+		
+		#--- request parameters ()
 		extract($this->extractRequestParams());
-
+		
 		$this->initCacheKey($lcache_key, __METHOD__);
-        #--- blank variables
-
+		
 		#---
 		$user_id = $this->getUser()->getId();
 		$ip = wfGetIP();
 		$browserId = $this->getBrowser();
-
+		
 		#---
-		$where_derived = " and ref_domain != '".$wgServerName."' and ref_domain not like '%$wgDBname.wikia%' and ref_domain not like '%wikia-inc.com%'";
+		$aTmp = $aData = array();
+		$where_derived = " ref_domain != '' and ref_domain != '".$wgServerName."' and ref_domain not like '%$wgDBname.wikia%' and ref_domain not like '%wikia-inc.com%'";
 		try {
 			#--- database instance - DB_SLAVE
 			$dbs = $this->getDB();
+			
+			if ( is_null($dbs) ) throw new WikiaApiQueryError(0); 
 
-			if ( is_null($dbs) ) {
-				throw new WikiaApiQueryError(0);
-				//throw new DBConnectionError($db, 'Connection error');
-			}
-
-			#--- identifier of city
+			#--- identifier of wikia
+			$city = ( !is_null($city) ) ? intval($city) : intval($wgCityId);
 			if ( !is_null($city) ) {
 				$where_derived .= " and ref_city_id = '".intval($city)."' ";
-				$this->setCacheKey ($lcache_key, 'C', $city);
+				$this->setCacheKey( $lcache_key, 'C', $city );
 			}
 
-			#--- identifier of date
-			if ( !empty($fromdate) ) {
-				if ( !$this->isCorrectDate($fromdate) ) {
-					throw new WikiaApiQueryError(1);
-				}
-				$this->setCacheKey ($lcache_key, 'F', $fromdate);
-				$where_derived .= " and date_format(ref_timestamp, '%Y-%m-%d') > '{$fromdate}' ";
-			} else {
-                $ts = wfTimestamp( TS_MW, strtotime( "-1 months") );
-                $where_derived .= " and ref_timestamp >= '$ts' ";
-            }
-
-			#--- identifier of date
-			if ( !empty($todate) ) {
-				if ( !$this->isCorrectDate($todate) ) {
-					throw new WikiaApiQueryError(1);
-				}
-				$this->setCacheKey ($lcache_key, 'T', $todate);
-				$where_derived .= " and date_format(ref_timestamp, '%Y-%m-%d') < '{$todate}' ";
+			#--- number of months 
+			$fromdate = intval($fromdate);
+			switch ($fromdate) { 
+				// last month 
+				case "1" : $where_derived .= " and ref_type = 2 "; break;
+				// last 3 months 
+				case "3" : $where_derived .= " and ref_type = 3 "; break;
+				// last 6 months 
+				case "6" : $where_derived .= " and ref_type = 4 "; break;
+				// default (all referrers)
+				default  : $where_derived .= " and ref_type = 1 "; break;
 			}
+			$this->setCacheKey( $lcache_key, 'FM', $fromdate );
 
-			#--- identifier of domain
-			$define_table = "city_wikireferer_domains";
+			#--- use full name of domain
+			$define_table = "city_wikireferer_domain_views";
 			if ( !empty($usefulldomain) ) {
 				$this->setCacheKey ($lcache_key, 'FD', $usefulldomain);
-				$define_table = "city_wikireferer";
+				$define_table = "city_wikireferer_views";
 			}
-			#---
+			#--- name of domain
 			if ( !empty($domain) ) {
 				$this->setCacheKey ($lcache_key, 'D', $domain);
 				$where_derived .= " and ref_domain = '{$domain}' ";
@@ -138,12 +86,11 @@ class WikiaApiQueryReferers extends WikiaApiQuery {
 				$part_domain = explode(".",$domain);
 				if (count($part_domain) == 1) {
                     $this->setCacheKey ($lcache_key, 'WCD', 1);
-					$define_table = "city_wikireferer_domains";
+					$define_table = "city_wikireferer_domain_views";
 				} else {
                     $this->setCacheKey ($lcache_key, 'WC', 1);
-					$define_table = "city_wikireferer";
+					$define_table = "city_wikireferer_views";
 				}
-				$db_where = "";
 			}
 
 			#--- useext
@@ -157,89 +104,63 @@ class WikiaApiQueryReferers extends WikiaApiQuery {
 
 			#--- use notdomain
 			if ( !empty($notdomain) ) {
-                $this->setCacheKey ($lcache_key, 'NAD', str_replace(" ", "_", $notdomain));
-                $notdomain_where = $this->getNotAllowedDomains($notdomain);
+				$this->setCacheKey ($lcache_key, 'NAD', str_replace(" ", "_", $notdomain));
+				$notdomain_where = $this->getNotAllowedDomains($notdomain);
 				if (!empty($notdomain_where)) {
 					$where_derived .= " and {$notdomain_where} ";
 				}
-            }
-
-			$table = "(select ref_domain, sum(ref_count) as ref_count ";
-			$table .= "from `{$wgDBStats}`.`{$define_table}` ";
-			$table .= "where ref_domain != '' {$where_derived} ";
-			$table .= "GROUP BY ref_domain) as ref";
-
-			#--- useurl option
-			if ( !empty($useurl) ) {
-				$table = "`{$wgDBStats}`.`city_wikireferer_urls_top`";
-				$db_where = " where ref_domain != '' {$where_derived} ";
-				if (is_null($city)) {
-					$db_where .= " and ref_city_id = '".$wikiaCityId."' ";
-				}
-				$this->setCacheKey ($lcache_key, 'UU', '1');
-			}
-			else {
-			    $db_where = '';
 			}
 
+			$table = "`{$wgDBStats}`.`{$define_table}`";
 			$select = " ref_domain, ref_count ";
-			$order = "ORDER BY ref_count DESC";
+			$order = ""; #"ORDER BY ref_count DESC";
 
 			#---
 			if ( !empty($ctime) ) {
-				if ( !$this->isInt($ctime) ) {
-					throw new WikiaApiQueryError(1);
-				}
+				if ( !$this->isInt($ctime) ) throw new WikiaApiQueryError(1);
 			}
 
+			// check data from cache ...
+			$cached = $this->getDataFromCache($lcache_key);
+			if (!is_array($cached)) {
+				$res = $dbs->query("select {$select} from {$table} where {$where_derived} {$order} ");
+				$loop = 0;
+				while ($row = $dbs->fetchObject($res)) {
+					$aTmp[$row->ref_count] = array (
+						"domain"	=> $row->ref_domain,
+						"count"		=> $row->ref_count,
+					);
+					$loop++;
+				}
+				$dbs->freeResult($res);
+				$this->saveCacheData($lcache_key, $aTmp, $ctime);
+			} else {
+				$aTmp = $cached;
+			}
+			
+			$iLimit = $iOffset = 0;
 			#--- limit
 			if ( !empty($limit)  ) {
 				if ( !$this->isInt($limit) ) {
 					throw new WikiaApiQueryError(1);
 				}
-				if (empty($uservote)) {
-					$this->setCacheKey ($lcache_key, 'L', $limit);
-				}
-				$db_limit = "LIMIT {$limit}";
-			}
-			else {
-			    $db_limit = '';
-			}
-
+				$iLimit = $limit;
+			} 
+			
 			#--- offset
 			if ( !empty($offset)  ) {
 				if ( !$this->isInt($offset) ) {
 					throw new WikiaApiQueryError(1);
 				}
-				if (empty($uservote)) {
-					$this->setCacheKey ($lcache_key, 'OF', $offset);
+				$iOffset = $offset;
+			} 
+			
+			if (!empty($aTmp)) {
+				krsort($aTmp);
+				$aData = array_slice($aTmp, $iOffset, $iLimit);
+				foreach ($aData as $i => $aDomain) {
+					ApiResult::setContent( $aData[$i], $aDomain['domain'] );
 				}
-				$db_offset = "OFFSET {$offset}";
-			}
-			else {
-			    $db_offset = '';
-			}
-
-			$data = array();
-			// check data from cache ...
-			$cached = $this->getDataFromCache($lcache_key);
-			if (!is_array($cached)) {
-				#$res = $this->select(__METHOD__);
-				$res = $dbs->query("select {$select} from {$table} {$db_where} {$order} {$db_limit} {$db_offset}");
-				$loop = 0;
-				while ($row = $dbs->fetchObject($res)) {
-					$data[$loop] = array(
-						"domain"	=> $row->ref_domain,
-						"count"		=> $row->ref_count,
-					);
-					ApiResult :: setContent( $data[$loop], $row->ref_domain );
-					$loop++;
-				}
-				$dbs->freeResult($res);
-				$this->saveCacheData($lcache_key, $data, $ctime);
-			} else {
-				// ... cached
-				$data = $cached;
 			}
 		} catch (WikiaApiQueryError $e) {
 			// getText();
@@ -253,159 +174,99 @@ class WikiaApiQueryReferers extends WikiaApiQuery {
 
 		// is exception
 		if ( isset($e) ) {
-			$data = $e->getText();
-			$this->getResult()->setIndexedTagName($data, 'fault');
+			$sData = $e->getText();
+			$this->getResult()->setIndexedTagName($sData, 'fault');
+		} else { 
+			$this->getResult()->setIndexedTagName($aData, 'item');
 		}
-		else
-		{
-			$this->getResult()->setIndexedTagName($data, 'item');
-		}
-		$this->getResult()->addValue('query', $this->getModuleName(), $data);
+		$this->getResult()->addValue('query', $this->getModuleName(), $aData);
 	}
 
-	#
-	#
-	function getWhereInternalDomains( $table ) {
+	private function getWhereInternalDomains( $table ) {
 		$where = "";
-	    $city_domains = WikiFactory::getDomains();
-
+		$city_domains = WikiFactory::getDomains();
 		if (!empty($city_domains) && is_array($city_domains)) {
 			$domain_cond = array();
 			foreach ( $city_domains as $domain ) {
-			    $domain = WikiFactory::getDomainHash($domain);
+				$domain = WikiFactory::getDomainHash($domain);
 				$_ = explode(".", $domain);
 				$domain_where = "";
 				switch ($table) {
-					case "city_wikireferer_domains" :
-						if (count($_) > 1 && !empty($_[count($_)-2])) {
-							$domain_where = $_[count($_)-2]; #i.e. wikia, wikicites etc ..
-						}
+					case "city_wikireferer_domain_views":
+						if (count($_) > 1 && !empty($_[count($_)-2])) $domain_where = $_[count($_)-2]; #i.e. wikia, wikicites etc ..
 						break;
-
-					case "city_wikireferer" :
-					case "city_wikireferer_urls" :
-						if (count($_) > 1 && !empty($_[count($_)-2])) {
-							$domain_where = $_[count($_)-2].".".$_[count($_)-1];
-						}
+					case "city_wikireferer_views" 		:
+						if (count($_) > 1 && !empty($_[count($_)-2])) $domain_where = $_[count($_)-2].".".$_[count($_)-1];
 						break;
 				}
-				if (!empty($domain_where))
-				{
+				if (!empty($domain_where)) 
 					if (empty($domain_cond["(ref_domain not like '%".$domain_where."%')"]))
-					{
 						$domain_cond["(ref_domain not like '%".$domain_where."%')"] = 1;
-					}
-				}
 			}
 
 			#---
 			$where = implode (" and ", array_keys($domain_cond));
 		}
-
 		return $where;
 	}
 
-    #
-    #
-    private function getNotAllowedDomains($pagename)
-    {
-        $where = "";
-        $domain_cond = array();
-        $templateTitle = Title::newFromText ($pagename, NS_MEDIAWIKI);
-        if( $templateTitle->exists() )
-        {
-            $templateArticle = new Article ($templateTitle);
-            $templateContent = $templateArticle->getContent();
-            $lines = explode( "\n", $templateContent );
-            foreach( $lines as $line )
-            {
-                $line = trim($line);
-                $domain_cond["(ref_domain not like '%".$line."%')"] = 1;
-            }
+	private function getNotAllowedDomains($pagename) {
+		$where = "";
+		$domain_cond = array();
+		$templateTitle = Title::newFromText ($pagename, NS_MEDIAWIKI);
+		if ( $templateTitle->exists() ) {
+			$templateArticle = new Article ($templateTitle);
+			$templateContent = $templateArticle->getContent();
+			$lines = explode( "\n", $templateContent );
+			foreach( $lines as $line ) {
+				$line = trim($line);
+				$domain_cond["(ref_domain not like '%".$line."%')"] = 1;
+			}
 			$where = implode (" and ", array_keys($domain_cond));
-        }
+		}
+		return $where;
+	}
 
-        return $where;
-    }
-
-	/**
-	 *
-	 * Description functions
-	 *
-	 */
 	protected function getQueryDescription() {
 		return 'Get counting of HTTP referers for Wiki';
 	}
 
-	/**
-	 *
-	 * Description parameters
-	 *
-	 */
 	protected function getParamQueryDescription() {
 		return array (
 			'city'			=> 'Identifier of Wiki page',
-			'fromdate' 		=> 'Start date period that user want to generate statistics. Format: YYYY-MM-DD',
-			'todate' 		=> 'End of date period that user want to generate statistics. Format: YYYY-MM-DD',
+			'fromdate' 		=> 'last 1, 3 or 6 months. Default value: 0 (all referrers)',
 			'domain'		=> 'Domain name. It can be one of following values: "domain.com" (i.e. google.com), "domain.com.jp" (i.e. google.com.pl) or "domain" (i.e. google)',
-			'useurl'		=> 'Get statistics for domains with full urls (i.e. google.com?search=wikia)',
 			'usefulldomain' => 'Use full name of domains (i.e. www.google.com (not "google"))',
-			'useext'		=> 'Use external referrers',
-			'notdomain'     => 'Mediawiki article with list of urls which should not visible in result'
+			'useext'		=> 'Show only external referrers',
+			'notdomain'     => 'List of domains which should not be visible in result'
 		);
 	}
 
-	/*
-	 *
-	 * Allowed parameters
-	 *
-	 */
-
-	#---
 	protected function getAllowedQueryParams() {
 		return array (
-			"city" => array ( ApiBase :: PARAM_TYPE => 'integer' ),
-			"fromdate" => array ( ApiBase :: PARAM_TYPE => 'string', ApiBase :: PARAM_DFLT => 0 ),
-			"todate" => array ( ApiBase :: PARAM_TYPE => 'string' ),
-			"domain" => array ( ApiBase :: PARAM_TYPE => 'string' ),
-			"useurl" => array ( ApiBase :: PARAM_TYPE => 'integer', ApiBase :: PARAM_DFLT => 0 ),
+			"city" 			=> array ( ApiBase :: PARAM_TYPE => 'integer' ),
+			"fromdate" 		=> array ( ApiBase :: PARAM_TYPE => 'integer', ApiBase :: PARAM_DFLT => 0 ),
+			"domain" 		=> array ( ApiBase :: PARAM_TYPE => 'string' ),
 			"usefulldomain" => array ( ApiBase :: PARAM_TYPE => 'integer', ApiBase :: PARAM_DFLT => 0 ),
-			"useext" => array ( ApiBase :: PARAM_TYPE => 'integer', ApiBase :: PARAM_DFLT => 0 ),
-			"notdomain" => array ( ApiBase :: PARAM_TYPE => 'string' ),
+			"useext" 		=> array ( ApiBase :: PARAM_TYPE => 'integer', ApiBase :: PARAM_DFLT => 0 ),
+			"notdomain" 	=> array ( ApiBase :: PARAM_TYPE => 'string' ),
 		);
 	}
 
-	/*
-	 *
-	 * Examples
-	 *
-	 */
-
-	#---
 	protected function getQueryExamples() {
 		return array (
 			'api.php?action=query&list=wkreferer',
 			'api.php?action=query&list=wkreferer&wkdomain=google',
-			'api.php?action=query&list=wkreferer&wkfromdate=2007-07-01&wkdomain=wikia',
-			'api.php?action=query&list=wkreferer&wkfromdate=2007-07-01&wkdomain=yahoo&wktodate=2007-08-01',
-			'api.php?action=query&list=wkreferer&wkfromdate=2007-07-01&wkdomain=wikia&wkuseurl=0',
-			'api.php?action=query&list=wkreferer&wkfromdate=2007-07-01&wkdomain=wikia&wkusefulldomain=1',
+			'api.php?action=query&list=wkreferer&wkfromdate=1&wkdomain=wikia',
+			'api.php?action=query&list=wkreferer&wkfromdate=3&wkdomain=yahoo',
+			'api.php?action=query&list=wkreferer&wkfromdate=6&wkdomain=wikia&wkuseurl=0',
+			'api.php?action=query&list=wkreferer&wkfromdate=0&wkdomain=wikia&wkusefulldomain=1',
 			'api.php?action=query&list=wkreferer&wkuseurl=0&wkuseext=1',
 			'api.php?action=query&list=wkreferer&wknotdomain=Not_allowed_urls',
 		);
 	}
 
-	/*
-	 *
-	 * Version
-	 *
-	 */
-
-	#---
 	public function getVersion() {
 		return __CLASS__ . ': $Id: '.__CLASS__.'.php '.filesize(dirname(__FILE__)."/".__CLASS__.".php").' '.strftime("%Y-%m-%d %H:%M:%S", time()).'Z wikia $';
 	}
-
 };
-
-?>
