f51468bf061d6500382c792c821018683b7e2c11
eloy <eloy@wikia-inc.com>
2008-11-25 15:54:34 +0000

    ajax comments (no refreshing yet)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3758 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/comment-post.tmpl.php
+++ extensions/wikia/Blogs/templates/comment-post.tmpl.php
@@ -1,13 +1,13 @@
-<div class="blog-comment-input">
-	<form action="#" method="post" id="blog-comment-form-<?php echo $position ?>">
+<div class="blog-comm-input">
+	<form action="#" method="post" id="blog-comm-form-<?php echo $position ?>">
 		<!-- avatar -->
 		<?php
 			echo $avatar->getImageTag( 50, 50 );
 			//  onclick="YAHOO.Wikia.Blogs.submit( 'blog-comment-form-{ echo $position }' )"
 		?>
 		<!-- textarea -->
-		<textarea width="80%" name="wpBlogComment" id="blog-comment-<?php echo $position ?>"></textarea>
+		<textarea width="80%" name="wpBlogComment" id="blog-comm-<?php echo $position ?>"></textarea>
 		<!-- submit -->
-		<input type="submit" name="wpBlogSubmit" value="<?php echo wfMsg("blog-comment-post") ?>" />
+		<input type="submit" name="wpBlogSubmit" id="blog-comm-submit-<?php echo $position ?>" value="<?php echo wfMsg("blog-comment-post") ?>" />
 	</form>
 </div>
