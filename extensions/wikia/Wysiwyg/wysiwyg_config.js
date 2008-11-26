4748a8d1543d044ea94c7d4e7229680e110857bb
inez <inez@wikia-inc.com>
2008-11-26 14:20:15 +0000

    * Wysiwyg: Prototype of Insert template dropdown - config
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3787 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Wysiwyg/wysiwyg_config.js
+++ extensions/wikia/Wysiwyg/wysiwyg_config.js
@@ -2,7 +2,7 @@ FCKConfig.FormatIndentator = '';
 FCKConfig.FontFormats = 'p;h2;h3;pre' ;
 
 FCKConfig.ToolbarSets["Default"] = [
-	['Source','-','Undo','Redo','-','Bold','Italic','Underline','StrikeThrough','Link','Unlink','-','FontFormat','-','OrderedList','UnorderedList','Outdent','Indent','-','AddImage','Table','Tildes','Rule','-','Find','Replace']
+	['Source','-','Undo','Redo','-','Bold','Italic','Underline','StrikeThrough','Link','Unlink','-','FontFormat','-','OrderedList','UnorderedList','Outdent','Indent','-','AddImage','Table','Tildes','Rule','-','Find','Replace','InsertTemplate']
 ];
 
 FCKConfig.StyleVersion = window.parent.wgStyleVersion;
