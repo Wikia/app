c746d4a99f23a1f5ad34f9dcc66b3dcc74be2d58
moli <moli@wikia-inc.com>
2008-11-27 13:25:35 +0000

    next changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3844 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/blog-page-voting.tmpl.php
+++ extensions/wikia/Blogs/templates/blog-page-voting.tmpl.php
@@ -1,1 +1,1 @@
-<span><ul class="star-rating" id="star-rating"><li class="current-rating" id="current-rating" style="padding:0px;width: <?= $ratingPx ?>px;"><span>0/5</span></li></ul></span>
+<span><ul class="star-rating"><li class="current-rating" style="padding:0px;width: <?= $ratingPx ?>px;"><span>0/5</span></li></ul></span>
