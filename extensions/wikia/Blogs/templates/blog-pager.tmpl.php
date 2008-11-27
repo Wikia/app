88e3842a0dcde9d2708c93ae452260d6117138b4
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-11-27 12:02:53 +0000

    bug fixed
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3832 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/blog-pager.tmpl.php
+++ extensions/wikia/Blogs/templates/blog-pager.tmpl.php
@@ -3,13 +3,13 @@
 <td align="center" width="70%">
 <?
 $i = 0;
-if ($iPageCount > 1 || $bShowNext) {
+if ($iPageCount > 1) {
 	if ($iPage!=0) {
 ?>		
 <a href="<?=$wgTitle->getFullURL($pageOffsetName."=".($iPage - 1))?>">&#171; <?=wfMsg('blog-newerposts')?></a>&nbsp;&nbsp;
 <?
 	}
-	if (( $iPage - $iNbrShow) < 0) {
+	if ( ($iPage - $iNbrShow) < 0 ) {
 		$i = 0;
 	} else {
 		$i = $iPage - $iNbrShow;
@@ -33,7 +33,7 @@ if ($iPageCount > 1 || $bShowNext) {
 <b><?=($iPage+1)?></b>&nbsp;&nbsp;
 <?
 	$to = $iPage + $iNbrShow + 1;
-	if ( ( $iPage + $iNbrShow + 1 ) > $iPageCount ) {
+	if ( $to > $iPageCount ) {
 		$to = $iPageCount;
 	} 
 	
@@ -53,7 +53,7 @@ if ($iPageCount > 1 || $bShowNext) {
 <a href="<?=$wgTitle->getFullURL($pageOffsetName."=".($iPageCount-1))?>"><?=$iPageCount?></a>
 <?
 	}
-	if ( ($iPage + 1) != $iPageCount || $bShowNext == 1) {
+	if ( ($iPage + 1) != $iPageCount ) {
 ?>
 <a href="<?=$wgTitle->getFullURL($pageOffsetName."=".($iPage+1))?>"><?=wfMsg('blog-olderposts')?> &#187;</a>
 <?
