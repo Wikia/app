79d109c488fc7c938548cedeae891670b7f31c30
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-11-28 15:54:49 +0000

    change event from click to change
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3902 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/js/Blogs.js
+++ extensions/wikia/Blogs/js/Blogs.js
@@ -36,4 +36,4 @@ YAHOO.Wikia.Blogs.submit = function( event, id ) {
 
 YAHOO.util.Event.addListener( "blog-comm-submit-top", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-top" );
 YAHOO.util.Event.addListener( "blog-comm-submit-bottom", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-bottom" );
-YAHOO.util.Event.addListener( "blog-comm-form-select", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-select" );
+YAHOO.util.Event.addListener( "blog-comm-form-select", "change", YAHOO.Wikia.Blogs.submit, "blog-comm-form-select" );
