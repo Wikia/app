95e337e65e8b22b34a4df9c6c9ec4866d1c72d46
eloy <eloy@wikia-inc.com>
2008-11-28 14:56:52 +0000

    ajaxify comments
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3896 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/js/Blogs.js
+++ extensions/wikia/Blogs/js/Blogs.js
@@ -1,16 +1,38 @@
 YAHOO.namespace("Wikia.Blogs");
 
+YAHOO.Wikia.Blogs.callback = {
+    success: function( oResponse ) {
+        var data = YAHOO.Tools.JSONParse( oResponse.responseText );
+		YAHOO.Wikia.Blogs.callback.add( data );
+    },
+    failure: function( oResponse ) {
+    },
+    timeout: 500000
+};
+
+YAHOO.Wikia.Blogs.callback.add = function( data ) {
+	var li = document.createElement( "li" );
+	li.innerHTML = data["text"];
+	YAHOO.util.Dom.get("blog-comments-ul").appendChild( li );
+};
 
 /**
  * so far simply submit of form
  */
 YAHOO.Wikia.Blogs.submit = function( event, id ) {
+
 	var oForm = YAHOO.util.Dom.get( id );
-	oForm.submit();
+
+	if( id == "blog-comm-form-select" ) {
+		oForm.submit();
+	}
+	else {
+		YAHOO.util.Event.preventDefault( event );
+		YAHOO.util.Connect.setForm( oForm, false );
+		YAHOO.util.Connect.asyncRequest( "POST", wgServer + wgScriptPath + wgScript + "?action=ajax&rs=BlogComments::axPost&title=" + wgTitle, YAHOO.Wikia.Blogs.callback );
+	}
 };
 
 YAHOO.util.Event.addListener( "blog-comm-submit-top", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-top" );
 YAHOO.util.Event.addListener( "blog-comm-submit-bottom", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-bottom" );
 YAHOO.util.Event.addListener( "blog-comm-form-select", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-select" );
-YAHOO.util.Dom.get("blog-comm-top").disabled=false;
-YAHOO.util.Dom.get("blog-comm-bottom").disabled=false;
