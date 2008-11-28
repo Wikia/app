1d0dba0e936ff0913c77c3c4facfbfae23587219
eloy <eloy@wikia-inc.com>
2008-11-28 11:57:46 +0000

    working on comment
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3883 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/js/Blogs.js
+++ extensions/wikia/Blogs/js/Blogs.js
@@ -12,3 +12,5 @@ YAHOO.Wikia.Blogs.submit = function( event, id ) {
 YAHOO.util.Event.addListener( "blog-comm-submit-top", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-top" );
 YAHOO.util.Event.addListener( "blog-comm-submit-bottom", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-bottom" );
 YAHOO.util.Event.addListener( "blog-comm-form-select", "click", YAHOO.Wikia.Blogs.submit, "blog-comm-form-select" );
+YAHOO.util.Dom.get("blog-comm-top").disabled=false;
+YAHOO.util.Dom.get("blog-comm-bottom").disabled=false;
--- extensions/wikia/Blogs/templates/comment.tmpl.php
+++ extensions/wikia/Blogs/templates/comment.tmpl.php
@@ -1,3 +1,31 @@
+<?php
+if( isset( $justLi ) ):
+/**
+ * this is exactly the same comment schema as below between <li>/<li>
+ * it's used for ajaxed inserting new child. So if you want to change anything
+ * you have to change in both cases
+ */
+?>
+<div class="blog-comment">
+	<a name="<?php echo isset( $comment["anchor"][2] ) ? $comment["anchor"][2] : "" ?>"></a>
+	<div class="comment_avatar">
+		<?php echo $comment["avatar"] ?>
+	</div>
+	<div class="comment">
+		<div class="details">
+			<strong><?php echo $comment["sig"] ?></strong>
+			<span class="timestamp"><?php echo $comment["timestamp"] ?></span>
+		</div>
+		<?php
+			echo $comment["text"];
+		?>
+	</div>
+</div>
+<br style="clear: both;" />
+<?php
+else:
+
+?>
 <a name="comments"></a>
 <h2 class="wikia_header">
 <?php echo wfMsg("blog-comments") ?>
@@ -14,7 +42,7 @@
 		// show top input
 ?>
 <div class="blog-comm-input reset clearfix">
-	<form action="<?php $title->getFullURL() ?>" method="post" id="blog-comm-form-top">
+	<form action="<?php $title->getFullURL() ?>" method="post" id="blog-comm-form-top" disabled="disabled">
 		<!-- avatar -->
 		<div class="blog-comm-input-avatar">
 		<?php
@@ -25,7 +53,7 @@
 		<div class="blog-comm-input-text">
 		<textarea name="wpBlogComment" id="blog-comm-top"></textarea><br />
 		<!-- submit -->
-		<a href="#" name="wpBlogSubmit" id="blog-comm-submit-top" class="bigButton">
+		<a href="<?php $title->getFullURL() ?>" name="wpBlogSubmit" id="blog-comm-submit-top" class="bigButton">
 			<big><? echo wfMsg("blog-comment-post") ?></big>
 			<small></small>
 		</a>
@@ -76,9 +104,9 @@
 		</div>
 		<!-- textarea -->
 		<div class="blog-comm-input-text">
-		<textarea name="wpBlogComment" id="blog-comm-bottom"></textarea><br />
+		<textarea name="wpBlogComment" id="blog-comm-bottom" disabled="disabled"></textarea><br />
 		<!-- submit -->
-		<a href="#" name="wpBlogSubmit" id="blog-comm-submit-bottom" class="bigButton">
+		<a href="<?php $title->getFullURL() ?>" name="wpBlogSubmit" id="blog-comm-submit-bottom" class="bigButton">
 			<big><? echo wfMsg("blog-comment-post") ?></big>
 			<small></small>
 		</a>
@@ -87,5 +115,6 @@
 </div>
 <?php
 	endif;
+	echo "</div>";
+endif; # $justLi
 ?>
-</div>
