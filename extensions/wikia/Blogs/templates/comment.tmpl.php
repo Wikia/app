9ca0ed9be31a9aa3d0d26bc917854f864ed4b9f1
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-12-01 16:28:55 +0000

    show selector when are comments
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3948 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/comment.tmpl.php
+++ extensions/wikia/Blogs/templates/comment.tmpl.php
@@ -29,12 +29,14 @@ else:
 <h2 class="wikia_header">
 <?php echo wfMsg("blog-comments") ?>
 </h2>
+<?php if( count( $comments ) > 10 && isset( $props[ "commenting" ] ) && $props[ "commenting" ] == 1 ): ?>
 <form action="<?php echo $title->getFullURL() ?>" method="get" id="blog-comm-form-select">
 <select name="order">
 	<option value="asc" <?php if ($order=="asc") echo 'selected="selected"' ?>><?php echo wfMsg("blog-comments-asc") ?></option>
 	<option value="desc" <?php if ($order=="desc") echo 'selected="selected"' ?>><?php echo wfMsg("blog-comments-dsc") ?></option>
 </select>
 </form>
+<?php endif ?>
 <div id="blog-comments" class="reset clearfix">
 <?php
 	if( count( $comments ) > 10 && isset( $props[ "commenting" ] ) && $props[ "commenting" ] == 1 ):
