0f550e8c085183f304edfe5ed6f77318fdabdd3a
bartek <bartek@wikia-inc.com>
2008-12-01 14:38:35 +0000

    CreateAPage: also fix issues with pipes upon preview
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3934 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreateAPage/SpecialCreatePage.php
+++ extensions/wikia/CreateAPage/SpecialCreatePage.php
@@ -23,7 +23,7 @@ $wgExtensionCredits['specialpage'][] = array(
    'name' => 'CreatePage',
    'author' => 'Bartek Łapiński, Lucas \'TOR\' Garczewski, Przemek Piotrowski'  ,
    'url' => 'http://help.wikia.com/wiki/Help:CreatePage' ,
-   'version' => '3.71' ,
+   'version' => '3.72' ,
    'description' => 'easy to use interface for creating new articles'
 );
 
