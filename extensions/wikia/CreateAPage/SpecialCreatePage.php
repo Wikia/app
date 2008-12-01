3fc5fdc5b8a41d861c34b009e4e040893b32f4b6
bartek <bartek@wikia-inc.com>
2008-12-01 11:07:26 +0000

    CreateAPage: fix for pipes inside infobox createplate in rt1046
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@3918 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreateAPage/SpecialCreatePage.php
+++ extensions/wikia/CreateAPage/SpecialCreatePage.php
@@ -23,7 +23,7 @@ $wgExtensionCredits['specialpage'][] = array(
    'name' => 'CreatePage',
    'author' => 'Bartek Łapiński, Lucas \'TOR\' Garczewski, Przemek Piotrowski'  ,
    'url' => 'http://help.wikia.com/wiki/Help:CreatePage' ,
-   'version' => '3.69' ,
+   'version' => '3.71' ,
    'description' => 'easy to use interface for creating new articles'
 );
 
