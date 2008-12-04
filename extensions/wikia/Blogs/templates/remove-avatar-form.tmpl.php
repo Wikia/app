22f13f451bb01edf279e85e6060d213a3880b8da
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-12-04 16:12:51 +0000

    text & style changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4093 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/remove-avatar-form.tmpl.php
+++ extensions/wikia/Blogs/templates/remove-avatar-form.tmpl.php
@@ -45,7 +45,7 @@ if (isset($status) && ($status == "WMSG_REMOVE_ERROR"))
 }
 ?>
 <div id="wba-upload">
-    <div style="float:left;padding:2px;text-align:center;">
+    <div style="float:left;padding:5px 2px;text-align:left;">
     	<?= wfMsg("blog-avatar-remove-info") ?> "<?= wfmsg("blog-avatar-getuser") ?>"
     </div>	
 	<div style="clear: both;"></div>    
@@ -60,12 +60,12 @@ if (isset($status) && ($status == "WMSG_REMOVE_ERROR"))
 <div style="clear: both;"></div>    
 <? if ($user && $is_posted) { ?>
 <div id="wba-upload">
-    <div style="text-align: center;padding: 1em;">
-        <div id="wba-upload-progress">&nbsp;</div>
+    <div style="text-align: center;padding: 5px;">
+        <div style="text-align:center;padding: 5px;"><?= $user->getName() ?></div>
         <div style="text-align: center;"><?= $avatar->getImageTag( 50, 50 )?></div>
     </div>
 	<div style="clear: both;"></div>
-    <div style="text-align: center;padding: 1em;">
+    <div style="text-align: center;padding: 5px;">
 <?if (!$avatar->isDefault()) { ?>
         <form id="wba-remove-form" enctype="multipart/form-data" action="<?= $title->getLocalUrl() ?>" method="post">
             <input type="hidden" name="action" value="remove_avatar" />
