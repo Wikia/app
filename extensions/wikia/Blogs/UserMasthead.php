6e69b82cc861e217fa09e35a059bc481f19e7371
moli <moli@wikia-inc.com>
2008-12-04 11:27:54 +0000

    small changes & tweaks
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4067 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/UserMasthead.php
+++ extensions/wikia/Blogs/UserMasthead.php
@@ -13,86 +13,95 @@ function userMasthead() {
 	$dbKey = $wgTitle->getDBkey();
 	$isAnon = $wgUser->isAnon();
 
-	if( $namespace == NS_BLOG_ARTICLE ||
-		$namespace == NS_BLOG_LISTING ||
-		$namespace == NS_BLOG_LISTING_TALK ||
-		$namespace == NS_USER ||
-		$namespace == NS_USER_TALK ||
-		$namespace == NS_SPECIAL && (
-			$dbKey == 'Watchlist' ||
-			$dbKey == 'WidgetDashboard' ||
-			$dbKey == 'Preferences' ||
-			$dbKey == 'Contributions'
-		)
-	){
-
+	$allowedNamespaces = array (
+		0 => array(NS_BLOG_ARTICLE, NS_USER, NS_USER_TALK),
+		1 => array(NS_BLOG_LISTING, NS_BLOG_LISTING_TALK)
+	);
+	$allowedPages = array (
+		0 => array('Watchlist', 'WidgetDashboard', 'Preferences'), 
+		1 => array('Contributions')
+	);
+	
+	if ( in_array( $namespace, array_merge($allowedNamespaces[0], $allowedNamespaces[1]) ) || 
+		 ( ($namespace == NS_SPECIAL) && ( in_array( $dbKey, array_merge($allowedPages[0],$allowedPages[1]) ) ) ) 
+	) {
 		/**
 		 * change dbkey for nonspecial articles, in this case we use NAMESPACE
 		 * as key
 		 */
-		if( $namespace != NS_SPECIAL ) {
+		if ( $namespace != NS_SPECIAL ) {
 			$dbKey = $namespace;
 		}
-		$userMasthead = true; //hides article/talk tabs in Monaco.php
+		
+		/* hides article/talk tabs in Monaco.php */
+		$userMasthead = true; 
+		$Avatar = null;
+		$userspace = "";
 		$out = array();
-		//DEFINE USERSPACE - THE USERNAME THAT BELONGS ON THE MASTHEAD
-		if ( in_array( $namespace, array( NS_USER, NS_USER_TALK, NS_BLOG_ARTICLE ) ) ) {
-			if( strpos( $wgTitle->getDBkey(), "/") ) {
-				list ( $userspace, $title ) = explode( "/", $wgTitle->getDBkey(), 2 );
-			}
-			else {
-				$userspace = $wgTitle->getDBkey();
-			}
+		/* check conditions */
+		if ( in_array( $namespace, $allowedNamespaces[0] ) ) {
+			#---
+			$userspace = $wgTitle->getBaseText();
 			$Avatar = BlogAvatar::newFromUserName( $userspace );
-		}
-		if(	in_array( $namespace, array( NS_BLOG_LISTING, NS_BLOG_LISTING_TALK ) ) ||
-		   $dbKey == 'Watchlist' ||
-		   $dbKey == 'WidgetDashboard' ||
-		   $dbKey == 'Preferences' ) {
+		} 
+		if ( in_array( $namespace, $allowedNamespaces[1] ) || in_array( $dbKey, $allowedPages[0] ) ) {
+			#---
 			$userspace = $wgUser->getName();
 			$Avatar = BlogAvatar::newFromUser( $wgUser );
 		}
 
-		if( $dbKey == 'Contributions' ) {
+		if ( in_array( $dbKey, $allowedPages[1] ) ) {
 			$reqTitle = $wgRequest->getText("title", false);
-			if( strpos( $reqTitle, "/") !== false ) {
-				list ( $title, $userspace ) = explode( "/", $reqTitle, 2 );
+			if ( strpos( $reqTitle, "/") !== false ) {
+				list ( , $userspace ) = explode( "/", $reqTitle, 2 );
 				$Avatar = BlogAvatar::newFromUserName( $userspace );
-			}
-			else {
+			} else {
 				$userspace = $wgUser->getName();
 				$Avatar = BlogAvatar::newFromUser( $wgUser );
 			}
 		}
-		$out['userspace'] = $userspace;
+		if ($userspace != "") {
+			$out['userspace'] = $userspace;
 
-		/**
-		 * get avatar for this user
-		 */
+			$oTitle = Title::newFromText( $userspace, NS_USER ); 
+			if ($oTitle instanceof Title) {
+				$out['nav_links'][] = array('text' => wfMsg('nstab-user'), 'href' => $oTitle->getLocalUrl(), "dbkey" => NS_USER );
+			}
+			$oTitle = Title::newFromText( $userspace, NS_USER_TALK ); 
+			if ($oTitle instanceof Title) {
+				$out['nav_links'][] = array('text' => wfMsg('talkpage'), 'href' => $oTitle->getLocalUrl(), "dbkey" => NS_USER_TALK );
+			}
+			$oTitle = Title::newFromText( $userspace, NS_BLOG_ARTICLE ); 
+			if ($oTitle instanceof Title) {
+				$out['nav_links'][] = array('text' => wfMsg('blog-page'), 'href' => $oTitle->getLocalUrl(), "dbkey" => NS_BLOG_ARTICLE );
+			}
+			if( !$isAnon ) {
+				$oTitle = Title::newFromText( "Contributions/{$userspace}", NS_SPECIAL ); 
+				if ($oTitle instanceof Title) {
+					$out['nav_links'][] = array('text' => wfMsg('contris'), 'href' => $oTitle->getLocalUrl(), "dbkey" => "Contributions" );
+				}
+			}
 
-		$out['nav_links'][] = array('text' => wfMsg('nstab-user'), 'href' => Title::newFromText( $userspace, NS_USER )->getLocalUrl(), "dbkey" => NS_USER );
-		$out['nav_links'][] = array('text' => wfMsg('talkpage'), 'href' => Title::newFromText( $userspace, NS_USER_TALK )->getLocalUrl(), "dbkey" => NS_USER_TALK );
-		$out['nav_links'][] = array('text' => wfMsg('blog-page'), 'href' => Title::newFromText( $userspace, NS_BLOG_ARTICLE )->getLocalUrl(), "dbkey" => NS_BLOG_ARTICLE );
-		if( !$isAnon ) {
-			$out['nav_links'][] = array('text' => wfMsg('contris'), 'href' => Title::newFromText( "Contributions/{$userspace}", NS_SPECIAL )->getLocalUrl(), "dbkey" => "Contributions" );
+			if ( $wgUser->isLoggedIn() && $wgUser->getName() == $userspace ) {
+				$out['nav_links'][] = array('text' => wfMsg('prefs-watchlist'), 'href' => Title::newFromText("Watchlist", NS_SPECIAL )->getLocalUrl(), "dbkey" => "Watchlist" );
+				$out['nav_links'][] = array('text' => wfMsg('blog-widgets-label'), 'href' => Title::newFromText("WidgetDashboard", NS_SPECIAL )->getLocalUrl(), "dbkey" => "WidgetDashboard" );
+				$out['nav_links'][] = array('text' => wfMsg('preferences'), 'href' => Title::newFromText("Preferences", NS_SPECIAL )->getLocalUrl(), "dbkey" => "Preferences" );
+			} elseif ( !$isAnon ) {
+				$oTitle = Title::newFromText( "EmailUser/{$userspace}", NS_SPECIAL ); 
+				if ($oTitle instanceof Title) {
+					$out['nav_links'][] = array('text' => wfMsg("emailpage"), 'href' => $oTitle->getLocalUrl(), "dbkey" => "EmailUser" );
+				}
+			}
+
+			$tmpl = new EasyTemplate( dirname( __FILE__ ) . '/templates/' );
+			$tmpl->set_vars( array(
+				'data' => $out,
+				"avatar" => $Avatar,
+				"userspace" => $userspace,
+				"current" => $dbKey
+			));
+			echo $tmpl->execute('UserMasthead');
 		}
-
-		if ( $wgUser->isLoggedIn() && $wgUser->getName() == $userspace ) {
-			$out['nav_links'][] = array('text' => wfMsg('prefs-watchlist'), 'href' => Title::newFromText("Watchlist", NS_SPECIAL )->getLocalUrl(), "dbkey" => "Watchlist" );
-			$out['nav_links'][] = array('text' => wfMsg('blog-widgets-label'), 'href' => Title::newFromText("WidgetDashboard", NS_SPECIAL )->getLocalUrl(), "dbkey" => "WidgetDashboard" );
-			$out['nav_links'][] = array('text' => wfMsg('preferences'), 'href' => Title::newFromText("Preferences", NS_SPECIAL )->getLocalUrl(), "dbkey" => "Preferences" );
-		} elseif ( !$isAnon ) {
-			$out['nav_links'][] = array('text' => wfMsg("emailpage"), 'href' => Title::newFromText( "EmailUser/{$userspace}", NS_SPECIAL )->getLocalUrl(), "dbkey" => "EmailUser" );
-		}
-
-		$tmpl = new EasyTemplate( dirname( __FILE__ ) . '/templates/' );
-		$tmpl->set_vars( array(
-			'data' => $out,
-			"avatar" => $Avatar,
-			"userspace" => $userspace,
-			"current" => $dbKey
-		));
-		echo $tmpl->execute('UserMasthead');
 	}
 	return true;
 }
