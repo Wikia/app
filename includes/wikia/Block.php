1273503cc2921f92849a17203c7f3df082fc13c4
bartek <bartek@wikia-inc.com>
2008-12-08 11:28:28 +0000

    Block.php: merging r4105 into trunk, enhancement for #747
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4138 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ includes/wikia/Block.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * a hook to prevent autoblocks from RT #747 to be made
+ */
+
+$wgHooks['AbortAutoblock'][] = 'wfWikiaAbortAutoblock';
+
+function wfWikiaAbortAutoblock( $autoblockip, $block ) {
+	if ( 0 == strpos( $autoblockip, '10.' ) ) {
+		wfDebug( "IP $autoblockip was prevented from being autoblocked by 10.* autoblock" );
+		return false;                 
+	}
+}
+
