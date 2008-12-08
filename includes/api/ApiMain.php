a30f03df24c8dd02b8bebd8a86c92e0503f5f5e1
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-12-08 12:02:36 +0000

    Api: merge r4122:4124 into trunk, fixes rt1576
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4145 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/api/ApiMain.php
+++ includes/api/ApiMain.php
@@ -348,7 +348,14 @@ class ApiMain extends ApiBase {
 		$this->mAction = $params['action'];
 
 		// Instantiate the module requested by the user
-		$module = new $this->mModules[$this->mAction] ($this, $this->mAction);
+
+		// RT #1576 5.12.2008 Bartek
+		if ( class_exists( $this->mModules[$this->mAction] ) )  {
+			$module = new $this->mModules[$this->mAction] ($this, $this->mAction);
+		} else {
+			ApiBase :: dieUsage( "Trying to load a nonexistant or undefined classname" );
+			return;
+		}
 
 		if( $module->shouldCheckMaxlag() && isset( $params['maxlag'] ) ) {
 			// Check for maxlag
