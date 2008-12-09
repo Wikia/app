eb1499fe0da9afdb3f18fcf1647538c0f89e7bad
tor <tor@wikia-inc.com>
2008-12-09 14:36:32 +0000

    Blogs: removed hardcoded msgs (closes #2594)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4194 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/createPostCategoryCloud.tmpl.php
+++ extensions/wikia/Blogs/templates/createPostCategoryCloud.tmpl.php
@@ -10,8 +10,8 @@
 		<?php $xnum++; endforeach; ?>
 	</div>
 	<textarea accesskey="," name="wpCategoryTextarea<?=$cloudNo;?>" id="wpCategoryTextarea<?=$cloudNo;?>" class="wpCategoryTextarea" rows='3' cols='<?=$cols?>'><?=$textCategories?></textarea>
-	Add a new category&nbsp;<input type="text" name="wpCategoryInput<?=$cloudNo;?>" class="wpCategoryInput" id="wpCategoryInput<?=$cloudNo;?>" value="" />
-	<input type="button" name="wpCategoryButton" id="wpCategoryButton" class="button color1" value="Submit" onclick="cloudInputAdd(<?=$cloudNo;?>, 'wpCategoryTextarea<?=$cloudNo;?>', 'categoryCloudSection<?=$cloudNo;?>', 'wpCategoryInput<?=$cloudNo;?>'); return false ;" />
+	<?=wfMsg('create-blog-categories-textinput')?>&nbsp;<input type="text" name="wpCategoryInput<?=$cloudNo;?>" class="wpCategoryInput" id="wpCategoryInput<?=$cloudNo;?>" value="" />
+	<input type="button" name="wpCategoryButton" id="wpCategoryButton" class="button color1" value="<?=wfMsg('create-blog-categories-submit')?>" onclick="cloudInputAdd(<?=$cloudNo;?>, 'wpCategoryTextarea<?=$cloudNo;?>', 'categoryCloudSection<?=$cloudNo;?>', 'wpCategoryInput<?=$cloudNo;?>'); return false ;" />
 </div>
 
 <script type="text/javascript">
