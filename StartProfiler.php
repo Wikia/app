169dfee51dbc552a3b7f9a45df70fd354e7143e9
emil <emil@wikia-inc.com>
2008-12-11 09:59:01 +0000

    enable forceprofile parameter
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4272 e846bd91-c042-42a7-a218-4b65e2c1904d

--- StartProfiler.php
+++ StartProfiler.php
@@ -1,22 +1,10 @@
 <?php
 
-require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
-
-/**
- * To use a profiler, delete the line above and add something like this:
- *
- *   require_once(  dirname(__FILE__).'/includes/Profiler.php' );
- *   $wgProfiler = new Profiler;
- *
- * Or for a sampling profiler:
- *   if ( !mt_rand( 0, 100 ) ) {
- *       require_once(  dirname(__FILE__).'/includes/Profiler.php' );
- *       $wgProfiler = new Profiler;
- *   } else {
- *       require_once(  dirname(__FILE__).'/includes/ProfilerStub.php' );
- *   }
- * 
- * Configuration of the profiler output can be done in LocalSettings.php
- */
-
+if( !empty( $_GET['forceprofile'] ) ) {
+	require_once( dirname(__FILE__).'/includes/ProfilerSimpleText.php' );
+	$wgProfiler = new ProfilerSimpleText;
+	$wgProfiler->setProfileID( 'forced' );
+} else {
+	require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
+}
 
