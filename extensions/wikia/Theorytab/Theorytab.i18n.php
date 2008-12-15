c1a588fc15bcd50dd8d1d01e71df2afd9b62d32f
tor <tor@wikia-inc.com>
2008-12-15 14:39:16 +0000

    added Theorytab extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4409 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Theorytab/Theorytab.i18n.php
@@ -0,0 +1,43 @@
+<?php
+
+if (!defined('MEDIAWIKI')) {
+        echo "This is MediaWiki extension named Theorytab.\n";
+        exit(1) ;
+}
+
+$messages = array(
+	'en' => array (
+		"theorytab-subpage" => "/Theories",
+		"theorytab-title" => "Theory",
+		"theorytab-parent-title" => "Parent article"),
+
+        'de' => array (
+		"theorytab-subpage" => "/Theories",
+		"theorytab-title" => "Theorien",
+		"theorytab-parent-title" => "Hauptartikel" ),
+
+        'es' => array (
+		"theorytab-subpage" => "/Theories",
+		"theorytab-title" => "Teorias",
+		"theorytab-parent-title" => "Articulo Principal" ),
+
+	'fr' => array (
+		"theorytab-subpage" => "/Theories",
+		"theorytab-title" => "Theories",
+		"theorytab-parent-title" => "Article Principal" ),
+
+	'it' => array (
+		"theorytab-subpage" => "/Theories",
+		"theorytab-title" => "Teorie",
+		"theorytab-parent-title" => "Articolo principale" ),
+
+        'nl' => array (
+		"theorytab-subpage" => "/Theories",
+		"theorytab-title" => "Theorieen",
+		"theorytab-parent-title" => "Hoofd Artikel" ),
+
+	'pt-br' => array (
+		"theorytab-subpage" => "/Theories",
+		"theorytab-title" => "Teorias",
+		"theorytab-parent-title" => "Artigo Princdipal" )
+);
