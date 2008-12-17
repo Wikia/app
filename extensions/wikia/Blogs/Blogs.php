12e8a5c1eaaf298a2b03a3feecd90f7448681b51
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-12-17 11:05:11 +0000

    German l10n of blog ns. Closes RT#2590.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4479 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/Blogs.php
+++ extensions/wikia/Blogs/Blogs.php
@@ -29,6 +29,25 @@ $wgContentNamespaces[] = NS_BLOG_ARTICLE;
 $wgContentNamespaces[] = NS_BLOG_LISTING;
 
 /**
+ * localized blog ns - proof of concept aka quick hack
+ *
+ * @see SMW includes/SMW_GlobalFunctions.php::smwfInitNamespaces
+ * FIXME generalize
+ */
+if ('de' == $wgLanguageCode) {
+	// make en ns point (alias) to "main ns" - at this point themselves
+	foreach (array(500, 501, 502, 503) as $ns) {
+		$wgNamespaceAliases[$wgExtraNamespaces[$ns]] = $ns;
+	}
+	// translate "main ns" into de
+	$wgExtraNamespaces[500] = 'Benutzer_Blog';
+	$wgExtraNamespaces[501] = 'Benutzer_Blog_Kommentare';
+	$wgExtraNamespaces[502] = 'Blog';
+	$wgExtraNamespaces[503] = 'Blog_Diskussion';
+	// the end: de name is the "main ns", en is just an alias (redirect)
+}
+
+/**
  * setup function
  */
 $wgAutoloadClasses[ "BlogListPage" ] = dirname(__FILE__) . '/BlogListPage.php';
