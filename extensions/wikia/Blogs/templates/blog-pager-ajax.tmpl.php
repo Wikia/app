c705760a194ebd5f589702f0dd8b4099f36576fe
moli <moli@wikia-inc.com>
2008-12-17 11:08:38 +0000

    new version of templates
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4481 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/blog-pager-ajax.tmpl.php
+++ extensions/wikia/Blogs/templates/blog-pager-ajax.tmpl.php
@@ -1,15 +1,15 @@
 <!-- s:<?= __FILE__ ?> -->
+<? 
+$i = 0; $prevPage = $nextPage = "";
+if ($iPageCount > 1) {
+	if ( $iPage != 0 ) { 
+		$prevPage = '<a href="#" id="wk-blog-prev-link" onclick="wkBlogShowPage(-1);">&#171; '.wfMsg('blog-newerposts').'</a>';
+	} 
+	if ( ($iPage + 1) != $iPageCount ) {
+		$nextPage = '<a href="#" id="wk-blog-next-link" onclick="wkBlogShowPage(1);">'.wfMsg('blog-olderposts').' &#187;</a>';
+	}	
+?>	
 <script type="text/javascript" src="<?=$wgExtensionsPath?>/wikia/Blogs/js/BlogsPager.js?<?=$wgStyleVersion?>"></script>
-<table border="0"><tr><td width="15%">&nbsp;</td>
-<td align="center" width="70%">
-<?
-$i = 0; if ($iPageCount > 1) { 	if ( $iPage != 0 ) {
-?>		
-<a href="#" id="wk-blog-prev-link" onclick="wkBlogShowPage(-1);">&#171; <?=wfMsg('blog-newerposts')?></a>
-<? } ?>	
-&nbsp;&nbsp;<strong id="wk-blog-current-page"><?=($iPage+1)?></strong>&nbsp;&nbsp;
-<? if ( ($iPage + 1) != $iPageCount ) { ?>
-<a href="#" id="wk-blog-next-link" onclick="wkBlogShowPage(1);"><?=wfMsg('blog-olderposts')?> &#187;</a>
-<? } } ?>
-</td><td width="15%">&nbsp;</td></tr></table>
+<div style="text-align:right; padding:2px 5px;"><?=$prevPage?>&nbsp;&nbsp;<strong id="wk-blog-current-page"><?=($iPage+1)?></strong>&nbsp;&nbsp;<?=$nextPage?></div>
+<? } ?>
 <!-- e:<?= __FILE__ ?> -->
