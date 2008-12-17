f65bed4cf33c1ed330408bdf7598b00a2a56fccb
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-12-17 11:23:21 +0000

    credits
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4485 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/Blogs.php
+++ extensions/wikia/Blogs/Blogs.php
@@ -12,7 +12,7 @@ $wgExtensionCredits['other'][] = array(
 	"name" => "BlogArticle",
 	"description" => "Blog Articles",
 	"version" => preg_replace( '/^.* (\d\d\d\d-\d\d-\d\d).*$/', '\1', '$Id: WikiFactory.php 13985 2008-06-16 15:20:38Z eloy $' ),
-	"author" => "[http://www.wikia.com/wiki/User:Eloy.wikia Krzysztof Krzyżaniak (eloy)]"
+	"author" => "[http://www.wikia.com/wiki/User:Eloy.wikia Krzysztof Krzyżaniak (eloy)], Piotr Molski, Adrian Wieczorek"
 );
 
 define( "NS_BLOG_ARTICLE", 500 );
