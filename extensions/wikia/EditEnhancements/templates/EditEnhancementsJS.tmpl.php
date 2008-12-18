8d4f73df98e8a1aa126fbf6a25eb7e8250760b6e
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2008-12-18 21:29:14 +0000

    IE compatibility for edit enhancements extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4568 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditEnhancements/templates/EditEnhancementsJS.tmpl.php
+++ extensions/wikia/EditEnhancements/templates/EditEnhancementsJS.tmpl.php
@@ -7,17 +7,17 @@ EditEnhancements = function() {
 
 	this.calculate = function() {
 		if (document.getElementById('wpTextbox1___Frame')) {
-			textbox = 'wpTextbox1___Frame';
+			var textbox = 'wpTextbox1___Frame';
 		} else {
-			textbox = 'wpTextbox1';
+			var textbox = 'wpTextbox1';
 		}
 
-		viewport = Dom.getViewportHeight();
-		textareaTop = jQuery("#" + textbox).offset().top;
-		toolbar = jQuery("#edit_enhancements_toolbar").outerHeight(true);
-		heightDiff = jQuery("#" + textbox).outerHeight(true) - jQuery("#" + textbox).height();
+		var viewport = Dom.getViewportHeight();
+		var textareaTop = jQuery("#" + textbox).offset().top;
+		var toolbar = jQuery("#edit_enhancements_toolbar").outerHeight();
+		var heightDiff = jQuery("#" + textbox).outerHeight(true) - jQuery("#" + textbox).height();
 		
-		targetHeight = viewport - textareaTop - toolbar - heightDiff + 'px';
+		var targetHeight = viewport - textareaTop - toolbar - heightDiff + 'px';
 		if (parseInt(targetHeight) > 200) {
 			Dom.setStyle(textbox, 'height', targetHeight);
 		}
--- extensions/wikia/EditEnhancements/templates/EditEnhancementsPreviewJS.tmpl.php
+++ extensions/wikia/EditEnhancements/templates/EditEnhancementsPreviewJS.tmpl.php
@@ -1,21 +1,14 @@
 <script type="text/javascript">
 
 EditEnhancementsPreview = function() {
-	var Dom = YAHOO.util.Dom;
-	var Event = YAHOO.util.Event;
 	jQuery.noConflict();
 
 	this.calculate = function() {
 		bodyContentLeft = jQuery("#bodyContent").offset().left;
-		bodyContentWidth = Dom.getStyle('bodyContent', 'width');
+		bodyContentWidth = jQuery("#bodyContent").width();
 		widthDiff = jQuery("#edit_enhancements_toolbar").outerWidth(true) - jQuery("#edit_enhancements_toolbar").width();
 
-		Dom.setStyle('edit_enhancements_toolbar', 'left', bodyContentLeft + 'px');
-		Dom.setStyle('edit_enhancements_toolbar', 'width', parseInt(bodyContentWidth) - parseInt(widthDiff) + 'px');
-		Dom.setStyle('edit_enhancements_toolbar', 'position', 'fixed');
-		Dom.setStyle('edit_enhancements_toolbar', 'bottom', '0px');
-		Dom.setStyle('edit_enhancements_toolbar', 'margin', '0px');
-		Dom.setStyle('edit_enhancements_toolbar', 'z-index', '500');
+		jQuery("#edit_enhancements_toolbar").css({'left' : bodyContentLeft + 'px', 'width' : parseInt(bodyContentWidth) - parseInt(widthDiff) + 'px', 'position' : 'fixed', 'bottom' : '0px', 'margin' : '0px', 'z-index' : '500'});
 	}
 	
 	this.calculate();
