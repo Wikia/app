ef22b215af8b40032b60982bb7ade1af3e7370af
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-08 07:37:12 +0000

    Due to reorganization of central. Closes RT#9155
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4841 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaffSig/StaffSig.php
+++ extensions/wikia/StaffSig/StaffSig.php
@@ -9,7 +9,7 @@ function wfSigSetup() {
 
 function wfMakeSignature( $contents, $attributes, $parser ) {
 	global $wgStylePath;
-	return '<a href="http://www.wikia.com/wiki/Wikia_Staff" title="This user is a member of Wikia staff" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
+	return '<a href="http://www.wikia.com/wiki/Wikia:Staff" title="This user is a member of Wikia staff" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
 }
 
 ?>
