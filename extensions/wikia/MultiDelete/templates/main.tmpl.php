9d737a74c159ff6aea9fc7435cd5d5ae9a4bc94a
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-09 02:46:39 +0000

    brand new shiny version of MultiDelete: * handling thousands of wikis by using external storage (one connection to DB instead of thousands) * optimization to task (no need to create Title object for every deleted page) * getting rid of unused features like deletion using file * TODO: remove unused entries in i18n file * minus: less information about task (right now there is no separation into 'single' and 'multi' task which causes easier handling of deletion but makes hard to display detailed infos like what was deleted in that very task)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4877 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/MultiDelete/templates/main.tmpl.php
@@ -0,0 +1,90 @@
+<!-- s:<?= __FILE__ ?> -->
+<style type="text/css">
+#mainForm table tr {vertical-align: top}
+.right {text-align: right}
+</style>
+<div id="PaneCompose">
+	<div id="PaneError"><?= !empty($formData['errMsg']) ? Wikia::errormsg($formData['errMsg']) : '' ?></div>
+	<div id="PaneInfo"><?= wfMsg('multidelete-help') ?></div>
+	<form method="post" id="mainForm" action="<?= $title->getLocalUrl() ?>">
+		<table>
+			<tr>
+				<td class="right">
+					<?= wfMsg('multidelete-as') ?>
+				</td>
+				<td>
+					<select name="mMode" id="mMode" style="width:350px">
+					<?php
+					foreach ($formData['modes'] as $val => $text) {
+						$selected = $val == $formData['mode'] ? ' selected="selected"' : '';
+						echo "<option value=\"$val\"$selected>$text</option>\n";
+					}
+					?>
+					</select>
+				</td>
+			</tr>
+
+			<tr>
+				<td class="right">
+					<?= wfMsg('multidelete-on') ?>
+				</td>
+				<td>
+					<select name="mRange" id="mRange" style="width:350px">
+					<?php
+					foreach ($formData['ranges'] as $val => $text) {
+						$selected = $val == $formData['range'] ? ' selected="selected"' : '';
+						echo "<option value=\"$val\"$selected>$text</option>\n";
+					}
+					?>
+					</select>
+				</td>
+			</tr>
+
+			<tr id="wikiInboxRow" style="<?= $formData['rangeHidden'] ?>">
+				<td class="right">
+					<?= wfMsg('multidelete-inbox-caption') ?>
+				</td>
+				<td>
+					<textarea name="mWikiInbox" id="mWikiInbox" cols="40" rows="2"><?= empty($formData['wikiInbox']) ? '' : $formData['wikiInbox'] ?></textarea>
+				</td>
+			</tr>
+
+			<tr>
+				<td class="right">
+					<?= wfMsg('multidelete-page') ?>
+				</td>
+				<td>
+					<textarea name="mPages" id="mPages" cols="40" rows="10"><?= empty($formData['page']) ? '' : $formData['page'] ?></textarea>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+				</td>
+				<td>
+					<input name="mAction" type="submit" value="<?= wfMsg('multidelete-button') ?>"/>
+				</td>
+			</tr>
+		</table>
+
+		<input type='hidden' name='mEditToken' value="<?= $formData['editToken'] ?>" />
+	</form>
+</div>
+
+<script type="text/javascript">
+function $(id) {
+	return document.getElementById(id);
+}
+
+function showWikiInbox () {
+	var wikiInboxRow = $('wikiInboxRow');
+
+	if ((this.options[this.selectedIndex].value) == 'selected') {
+		wikiInboxRow.style.display = '';
+	} else {
+		wikiInboxRow.style.display = 'none' ;
+	}
+}
+YAHOO.util.Event.addListener('mRange', 'change', showWikiInbox);
+</script>
+<!-- e:<?= __FILE__ ?> -->
