31520047721a1983b00fc35846ec3da54f8c4a7f
macbre <macbre@wikia-inc.com>
2009-01-13 15:28:45 +0000

    EditEnhancements: CSS for *#%$ IE6
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@4976 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditEnhancements/templates/EditEnhancementsToolbar.tmpl.php
+++ extensions/wikia/EditEnhancements/templates/EditEnhancementsToolbar.tmpl.php
@@ -9,6 +9,13 @@
 		margin: 0px !important;
 		z-index: 500;
 	}
+
+	* html .edit_enhancements_toolbar_fixed {
+		position: absolute;
+		top: expression( ((e=document.documentElement.scrollTop+document.documentElement.clientHeight)?e:document.body.scrollTop+document.documentElement.clientHeight) - 100  +'px') !important;
+		left: 0 !important;
+	}
+
 	.edit_enhancements_toolbar_static {
 		position: static;
 		width: auto;
