942789ef04036c133e325cb76cb850dbb23299d1
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-16 09:08:17 +0000

    EditEnhancements: JS changes for preview mode
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5036 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditEnhancements/templates/EditEnhancementsPreviewJS.tmpl.php
+++ extensions/wikia/EditEnhancements/templates/EditEnhancementsPreviewJS.tmpl.php
@@ -1,35 +1,67 @@
 <script type="text/javascript">
-EditEnhancementsPreview = function() {
-	jQuery.noConflict();
+EditEnhancementsPreview = {
 
-	this.calculate = function() {
+	timestamp: 0,
 
+	calculate: function() {
+		jQuery.noConflict();
+
+		editBarHeight = jQuery('#edit_enhancements_toolbar').outerHeight();
 		scrollBottomOffset = YAHOO.util.Dom.getDocumentScrollTop() + YAHOO.util.Dom.getViewportHeight();
 
 		if (typeof FCK == 'undefined') {
-			textAreaOffset = jQuery('#wpTextbox1').offset().top + jQuery('#wpTextbox1').height();
+			textAreaOffset = jQuery('#wpTextbox1').offset().top + jQuery('#wpTextbox1').outerHeight();
 		}
 		else {
-			textAreaOffset = jQuery('#wpTextbox1___Frame').offset().top + jQuery('#wpTextbox1___Frame').height();
+			textAreaOffset = jQuery('#wpTextbox1___Frame').offset().top + jQuery('#wpTextbox1___Frame').outerHeight();
 		}
 
 		// choose positioning method
-		if (scrollBottomOffset < textAreaOffset + 50) {
+		if ( scrollBottomOffset < (textAreaOffset + editBarHeight + 8) ) {
 			bodyContentLeft = jQuery("#bodyContent").offset().left;
 			bodyContentWidth = jQuery("#bodyContent").width();
 			widthDiff = jQuery("#edit_enhancements_toolbar").outerWidth(true) - jQuery("#edit_enhancements_toolbar").width();
 
 			jQuery("#edit_enhancements_toolbar").css({'left' : bodyContentLeft + 'px', 'width' : parseInt(bodyContentWidth) - parseInt(widthDiff) + 'px'}).removeClass('edit_enhancements_toolbar_static').addClass('edit_enhancements_toolbar_fixed');
+
+			// add margin-top to #editpage-copywarn to keep the same scroll height
+			jQuery('#editpage-copywarn').css('marginTop', editBarHeight + 10 + 'px');
 		}
 		else {
 			jQuery("#edit_enhancements_toolbar").removeClass('edit_enhancements_toolbar_fixed').addClass('edit_enhancements_toolbar_static');
+
+			// remove margin-top from #editpage-copywarn to keep the same scroll height
+			jQuery('#editpage-copywarn').css('marginTop', '0px');
+		}
+	},
+
+	onEvent: function() {
+		// update timestamp - do interval for next x miliseconds
+		this.timestamp = (new Date()).getTime() + 1*1000
+
+		// recalculate position
+		this.calculate();
+
+		// start interval for smooth scrolling
+		if (!this.interval) {
+			this.interval = setInterval(this.loop, 25);
+		}
+	},
+
+	interval: false,
+
+	loop: function() {
+		EditEnhancementsPreview.calculate();
+
+		// we're done, clear interval
+		if ( EditEnhancementsPreview.timestamp < (new Date()).getTime() ) {
+			clearInterval(EditEnhancementsPreview.interval);
+			EditEnhancementsPreview.interval = false;
 		}
 	}
-	
-	this.calculate();
-}
+};
 
-YAHOO.util.Event.on(window, 'load',   EditEnhancementsPreview);
-YAHOO.util.Event.on(window, 'resize', EditEnhancementsPreview);
-YAHOO.util.Event.on(window, 'scroll', EditEnhancementsPreview);
+YAHOO.util.Event.on(window, 'load',   function() {EditEnhancementsPreview.onEvent();});
+YAHOO.util.Event.on(window, 'resize', function() {EditEnhancementsPreview.onEvent();});
+YAHOO.util.Event.on(window, 'scroll', function() {EditEnhancementsPreview.onEvent();});
 </script>
