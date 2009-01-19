80b15f20901d7652e42ec47a9106e143a1922f96
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-19 15:46:11 +0000

    next changes in Listusers - RT#5956
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5078 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Listusers/templates/main-form.tmpl.php
+++ extensions/wikia/Listusers/templates/main-form.tmpl.php
@@ -34,7 +34,7 @@
 	border:1px solid black;	
 }
 .lu_filter {
-	padding: 2px;
+	padding: 3px 2px 7px 2px;
 }
 .lu_first {
 	padding-left: 5px;
@@ -45,6 +45,12 @@
 	overflow-y:scroll;
 	padding:1px 5px;
 	margin:1px;
+}
+.lu_fieldset {
+	line-height:10px;
+	margin: 1px 0px;
+	padding: 0px 2px 2px;
+	width: 60%;
 }
 </style>
 <script type="text/javascript">
@@ -64,7 +70,7 @@ function __makeParamValue(f) {
 	return target;
 }
 
-function wfJSPager(total,link,page,limit,func) {
+function wfJSPager(total,link,page,limit,func,order,desc) {
 	var lNEXT = "<?=wfMsg('listusersnext')?>";
 	var lPREVIOUS = "<?=wfMsg('listusersprevious')?>";
 	var lR_ARROW = ">";
@@ -81,8 +87,8 @@ function wfJSPager(total,link,page,limit,func) {
 
 	if ((!total) || (total <= 0)) return "";
 
-	function __makeClickFunc(jsFunc, func, limit, offset) {
-		return " " + jsFunc + "=\"" + func + "(" + limit + "," + offset + "); return false;\" ";
+	function __makeClickFunc(jsFunc, func, limit, offset, order, desc) {
+		return " " + jsFunc + "=\"" + func + "(" + limit + "," + offset + ", '" + order + "'," + desc + "); return false;\" ";
 	}
 
 	var page_count = Math.ceil(total/limit);
@@ -90,7 +96,7 @@ function wfJSPager(total,link,page,limit,func) {
 	var to = 0;
 
 	var nbr_result = "<select id=\"wcLUselect\" style=\"" + selectStyle + "\" ";
-	nbr_result += __makeClickFunc("onChange", func, "this.value", 0);
+	nbr_result += __makeClickFunc("onChange", func, "this.value", 0, order, desc);
 	for (k = 0; k <= 9; k++) {
 		selected = (limit == (5*(parseInt(k)+1))) ? "selected" : "";
 		nbr_result += "<option " + selected + " value=\""+(5*(parseInt(k)+1))+"\">" + (5*(parseInt(k)+1)) + "</option>";
@@ -104,7 +110,7 @@ function wfJSPager(total,link,page,limit,func) {
 
 	if (page_count > 1) {
 		if (page != 0) {
-			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, (parseInt(page)-1));
+			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, (parseInt(page)-1), order, desc);
 			pager += "href=\"" + link + "&page=" + (parseInt(page)-1) + "\">" + lL_ARROW + " " + lPREVIOUS + "</a>&nbsp;&nbsp;";
 		}
 
@@ -115,12 +121,12 @@ function wfJSPager(total,link,page,limit,func) {
 		}
 
 		if ( i > 0 ) {
-			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, 0) + " href=\"" + link + "&page=0\">1</a>&nbsp;";
+			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, 0, order, desc) + " href=\"" + link + "&page=0\">1</a>&nbsp;";
 			if ( i != 1) pager += "&nbsp;...&nbsp;&nbsp;";
 		}
 
 		for (k = i; k < page; k++) {
-			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, parseInt(k));
+			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, parseInt(k), order, desc);
 			pager += " href=\"" + link + "&page=" + parseInt(k) + "\">" + (parseInt(k)+1) + "</a>&nbsp;&nbsp;";
 		}
 
@@ -133,18 +139,18 @@ function wfJSPager(total,link,page,limit,func) {
 		}
 
 		for (i = parseInt(page)+1; i < to; i++) {
-			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, parseInt(i));
+			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, parseInt(i), order, desc);
 			pager += " href=\"" + link + "&page=" + parseInt(i) + "\">" + (parseInt(i)+1) + "</a>&nbsp;&nbsp;";
 		}
 
 		if ( to < page_count ) {
 			if ( to != page_count-1 ) pager += "&nbsp;...&nbsp;&nbsp;";
-			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, parseInt(page_count)-1);
+			pager += "<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, parseInt(page_count)-1, order, desc);
 			pager += "href=\"" + link + "&page=" + (parseInt(page_count)-1) + "\">" + page_count + "</a>";
 		}
 
 		if ( (parseInt(page) + 1) != parseInt(page_count) ) {
-			pager += "&nbsp;&nbsp;<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, (parseInt(page)+1));
+			pager += "&nbsp;&nbsp;<a style=\"" + linkStyle + "\" " + __makeClickFunc("onclick", func, limit, (parseInt(page)+1), order, desc);
 			pager += " href=\"" + link + "&page=" + (parseInt(page)+1) + "\">" + lNEXT + " " + lR_ARROW + "</a>";
 		}
 	} else {
@@ -156,7 +162,7 @@ function wfJSPager(total,link,page,limit,func) {
 	return pager;
 }
 
-function wkLUshowDetails(limit, offset) 
+function wkLUshowDetails(limit, offset, ord, desc) 
 {
 	limit = typeof(limit) != 'undefined' ?limit : 30;
 	offset = typeof(offset) != 'undefined' ? offset : 0;
@@ -181,37 +187,44 @@ function wkLUshowDetails(limit, offset)
 			} else { 
 				page = resData['page'];
 				limit = resData['limit'];
+				order = resData['order'];
+				desc = resData['desc'];
 				//
 				div_details.innerHTML = foundText.replace("CNT", resData['nbr_records']);
 				//
-				pager = wfJSPager(resData['nbr_records'],"/index.php?title=Special:Listusers", page, limit, 'wkLUshowDetails');
+				pager = wfJSPager(resData['nbr_records'],"/index.php?title=Special:Listusers", page, limit, 'wkLUshowDetails', order, desc);
 				var _tmp = "<div style=\"clear:both\">";
 				_tmp += pager;
- 				_tmp += "<br /><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" valign=\"top\">";
-				var oneRow = "<tr><th class=\"lu_row\">#</th>";
-				oneRow += "<th class=\"lu_row\"><?=wfMsg('listusers-username')?></th>";
-				oneRow += "<th class=\"lu_row\"><?=wfMsg('listusers-groups')?></th>";
-				oneRow += "<th class=\"lu_row\"><?=wfMsg('listusersrev-cnt')?></th>";
-				oneRow += "<th class=\"lu_row\"><?=wfMsg('listusers-loggedin')?></th>";
-				oneRow += "<th class=\"lu_row\"><?=wfMsg('listusers-edited')?></th></tr>";
+ 				_tmp += "<table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" valign=\"top\" class=\"TablePager\">";
+				var oneRow = "<tr><th>#</th>";
+				var _th = (order == 'username') ? ((desc == -1) ? "&darr;" : "&uarr;") : "";
+				oneRow += "<th><a id=\"TablePager_username\" style=\"cursor:pointer;" + ((order == 'username') ? "color:#006400;" : "") + "\">" + _th + " <?=wfMsg('listusers-username')?></a></th>";
+				_th = (order == 'groups') ? ((desc == -1) ? "&darr;" : "&uarr;") : "";
+				oneRow += "<th><a id=\"TablePager_groups\" style=\"cursor:pointer;" + ((order == 'groups') ? "color:#006400;" : "") + "\">" + _th + " <?=wfMsg('listusers-groups')?></a></th>";
+				_th = (order == 'revcnt') ? ((desc == -1) ? "&darr;" : "&uarr;") : "";
+				oneRow += "<th><a id=\"TablePager_revcnt\" style=\"cursor:pointer;" + ((order == 'revcnt') ? "color:#006400;" : "") + "\">" + _th + " <?=wfMsg('listusersrev-cnt')?></a></th>";
+				_th = (order == 'loggedin') ? ((desc == -1) ? "&darr;" : "&uarr;") : "";
+				oneRow += "<th><a id=\"TablePager_loggedin\" style=\"cursor:pointer;" + ((order == 'loggedin') ? "color:#006400;" : "") + "\">" + _th + " <?=wfMsg('listusers-loggedin')?></a></th>";
+				oneRow += "<th><?=wfMsg('listusers-edited')?></th></tr>";
 				_tmp += oneRow;
  				loop = limit * offset;
  				if (resData['data']) {
 					for (i in resData['data']) {
 						loop++;
 						var blocked = (resData['data'][i]['blcked'] == 1) ? "style=\"color:#DF2930\"" : "";
-						oneRow = "<tr><td class=\"lu_row\" " + blocked + " >" + loop  + ".</td>";
-						oneRow += "<td class=\"lu_row\" " + blocked + " ><span style=\"font-size:90%;font-weight:bold;\">" +resData['data'][i]['user_link']+ "</span> <span style=\"font-size:77%; padding-left:8px;\">" + resData['data'][i]['links'] + "</span></td>";
-						oneRow += "<td class=\"lu_row\" " + blocked + " >" +resData['data'][i]['groups']+ "</td>";
-						oneRow += "<td class=\"lu_row\" " + blocked + " >" +resData['data'][i]['rev_cnt']+ "</td>";
-						oneRow += "<td class=\"lu_row\" " + blocked + " >" + ((resData['data'][i]['last_login']) ? resData['data'][i]['last_login'] : "-") + "</td>";
-						oneRow += "<td class=\"lu_row\" " + blocked + " >" + ((resData['data'][i]['last_edited']) ? resData['data'][i]['last_edited'] : "-") + "</td>";
+						oneRow = "<tr><td " + blocked + " >" + loop  + ".</td>";
+						oneRow += "<td " + blocked + " ><span style=\"font-size:90%;font-weight:bold;\">" +resData['data'][i]['user_link']+ "</span> <span style=\"font-size:77%; padding-left:8px;\">" + resData['data'][i]['links'] + "</span></td>";
+						oneRow += "<td " + blocked + " >" +resData['data'][i]['groups']+ "</td>";
+						oneRow += "<td " + blocked + " >" +resData['data'][i]['rev_cnt']+ "</td>";
+						oneRow += "<td " + blocked + " >" + ((resData['data'][i]['last_login']) ? resData['data'][i]['last_login'] : "-") + "</td>";
+						oneRow += "<td " + blocked + " >" + ((resData['data'][i]['last_edited']) ? resData['data'][i]['last_edited'] : "-") + "</td>";
 						oneRow += "</tr>";
 						_tmp += oneRow;
 					}
 				}
 				_tmp += "</table><br />";
 				records.innerHTML = _tmp + pager + "</div>";
+				_addEvents(order, desc);
 				if (typeof TieDivLibrary != "undefined" ) {
 					TieDivLibrary.calculate();
 				};
@@ -238,6 +251,8 @@ function wkLUshowDetails(limit, offset)
 		params += "&rsargs[2]=" + contributed.value;
 		params += "&rsargs[3]=" + limit;
 		params += "&rsargs[4]=" + offset;
+		params += "&rsargs[5]=" + ord;
+		params += "&rsargs[6]=" + desc;
 		//---
 		YAHOO.util.Event.preventDefault(e);
 		div_details.innerHTML="<img src=\"<?=$wgExtensionsPath?>/wikia/Listusers/images/ajax-loader-s.gif\" />";
@@ -247,28 +262,42 @@ function wkLUshowDetails(limit, offset)
 	}
 }
 
-YAHOO.util.Event.onDOMReady(function () {
-	YAHOO.namespace("Wikia.ListUsers");
-	wkLUshowDetails();
-	
-	YAHOO.Wikia.ListUsers.ShowUsers = function(e) {
-		var select_pages = document.getElementById('wcLUselect');
-		var cnt = (select_pages) ? select_pages.value : 30;
-		wkLUshowDetails(cnt, 0);
-	};
+__ShowUsers = function(e, args) {
+	var _id = this.id;
+	var _desc = args[0];
+	var _order = "username";
+	if (_id.indexOf('TablePager_', 0) !== -1) {
+		_order = _id.replace('TablePager_', '');
+	} 
+	var select_pages = document.getElementById('wcLUselect');
+	var cnt = (select_pages) ? select_pages.value : 30;
+	wkLUshowDetails(cnt, 0, _order, _desc);
+};
 
-	YAHOO.util.Event.addListener("lu-showusers", "click", YAHOO.Wikia.ListUsers.ShowUsers);
+function _addEvents(f, desc) {
+	YAHOO.util.Event.addListener("TablePager_username", "click", __ShowUsers, [(f == 'username')?desc*-1:desc]);
+	YAHOO.util.Event.addListener("TablePager_groups", "click", __ShowUsers, [(f == 'groups')?desc*-1:desc]);
+	YAHOO.util.Event.addListener("TablePager_revcnt", "click", __ShowUsers, [(f == 'revcnt')?desc*-1:desc]);
+	YAHOO.util.Event.addListener("TablePager_loggedin", "click", __ShowUsers, [(f == 'loggedin')?desc*-1:desc]);
+}
+
+//http://images.wikia.com/common/releases_200901.3/skins/common/images/Arr_u.png
+//http://images.wikia.com/common/releases_200901.3/skins/common/images/Arr_d.png
+
+YAHOO.util.Event.onDOMReady(function () {
+	wkLUshowDetails(30, 0, 'username', -1);
+	var desc = -1;
+	YAHOO.util.Event.addListener("lu-showusers", "click", __ShowUsers, [desc]);
 });
 
 /*]]>*/
 </script>
 <p class='error'><?=$error?></p>
-<div style="clear:both">
 <form method="post" action="<?=$action?>" id="lu-form">
-<table style="width:100%;" cellpadding="0" cellspacing="0"><tr>
 <? $found = 0; ?>	
-<td valign="middle" class="lu_left" style="border-bottom:0px;">
 <? if ( !empty($groupList) && (!empty($aGroups)) ) { ?>
+<fieldset class="lu_fieldset">
+<legend><?=wfMsg('listusers-groups')?></legend>
 	<table><tr>
 	<? $i = 0; foreach ($aGroups as $groupName => $userGroupName) { ?>
 		<? if ($i % 3 == 0) { ?>
@@ -282,27 +311,20 @@ YAHOO.util.Event.onDOMReady(function () {
 		<? $i++; ?>
 	<? } ?>
 	</tr></table>
+</fieldset>	
 <? } ?>
-</td>
-<td valign="middle" class="lu_result" rowspan="2">
-	<div style="font-size:85%;" id="listusers-result"><?=wfMsg('listusersfound', $found)?></div>
-</td>
-</tr>
-<tr>
-<td class="lu_header">
+<fieldset class="lu_fieldset">
+<legend><?=wfMsg('listusers-options')?></legend>
 	<div class="lu_filter">
 		<span class="lu_filter lu_first"><?= wfMsg('listusersstartingtext') ?></span>
 		<span class="lu_filter"><input type="text" name="lu_search" id="lu_search" size="5"></span>
 		<span class="lu_filter lu_first"><?= wfMsg('listuserscontributed') ?></span>
 		<span class="lu_filter"><select name="lu_contributed" id="lu_contributed" ><? foreach ($contributed as $val => $text) { ?><option <?= ($val == $selContrib) ? "selected" : "" ?> value="<?=$val?>"><?=$text?><? } ?></select></span>
 		<span class="lu_filter"><input type="button" value="<?=wfMsg('listusersdetails')?>" id="lu-showusers"></span>
-	</div>	
-</td>
-</tr>
-<tr>
-<td valign="top" colspan="3" class="lu_left" id="lu-result" style="font-size:100%"></td>
-</tr>
-</table>
+	</div>
+</fieldset>
+<div style="font-size:85%;text-align:right;height:20px;" id="listusers-result"><?=wfMsg('listusersfound', $found)?></div>
+<div id="lu-result" style="font-size:100%;clear:both;"></div>
 </form>
 </div>
 <!-- END OF DISTRIBUTION TABLE -->
