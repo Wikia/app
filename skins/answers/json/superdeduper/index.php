75c1a82c2bb3320405cd9f40673fb7df7ac4fd91
nick <nick@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-21 23:02:09 +0000

    This won't work here, because of apache rewrite rules to images.wikia.com. Moving to /extensions/wikia/SuperDeduper
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5180 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/answers/json/superdeduper/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Simple JSON interface to the super deduper for type-ahead completion */
-header('Content-Type: text/plain');
-require '/usr/wikia/source//answers/SuperDeduper.php';
-$SuperDeduper = new SuperDeduper();
-echo json_encode($SuperDeduper->getRankedmatches(@$_GET['q']));
-?>
