75d88ef7858f85b8f0f9df7e9b8def4946801ccd
nick <nick@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-22 18:33:50 +0000

    Add demo, clean up interface
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5217 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SuperDeduper/index.php
+++ extensions/wikia/SuperDeduper/index.php
@@ -3,5 +3,15 @@
 header('Content-Type: text/plain');
 require '/usr/wikia/source//answers/SuperDeduper.php';
 $SuperDeduper = new SuperDeduper();
-echo json_encode($SuperDeduper->getRankedmatches(@$_GET['q']));
+if (empty($_GET['limit'])){
+	$_GET['limit'] = 10;
+}
+$results = $SuperDeduper->getRankedmatches(@$_GET['query'], $_GET['limit']);
+
+$out = array();
+foreach($results as $title => $rank){
+	$out["ResultSet"]["Result"][] = array("title" => $title, "rank" => $rank);
+}
+echo json_encode($out);
+
 ?>
