27d36b74a99d75d10c9dbeaef0f6200a5b9f908d
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-27 03:43:19 +0000

    New special page for staff to easily build flash slideshow widgets for promotion. Code reviewed by Nick
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5374 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaWidget/WikiaWidget.php
@@ -0,0 +1,30 @@
+<?php
+if (!defined('MEDIAWIKI')) {
+        exit( 1 );
+}
+
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'WikiaWidget',
+	'author' => 'Christian Williams',
+	'description' => 'Create Wikia Widgets for use on external sites or for inclusion on your wiki',
+	'version' => '1.0',
+);
+ 
+$dir = dirname(__FILE__) . '/';
+
+/*
+$wgAutoloadClasses['WikiaWidget'] = $dir . 'WikiaWidget_body.php'; # Tell MediaWiki to load the extension body.
+$wgSpecialPages['WikiaWidget'] = 'WikiaWidget'; # Let MediaWiki know about your new special page.
+*/
+
+
+
+/**
+ * permissions
+ */
+$wgAvailableRights[] = 'wikiawidget';
+$wgGroupPermissions['staff']['wikiawidget'] = true;
+$wgGroupPermissions['wikiawidget']['wikiawidget'] = true;
+
+extAddSpecialPage( dirname(__FILE__) . '/WikiaWidget_body.php', 'WikiaWidget', 'WikiaWidget' );
+$wgSpecialPageGroups['WikiaWidget'] = 'wikia';
