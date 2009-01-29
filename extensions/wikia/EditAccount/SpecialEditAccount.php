0b98204989615bb2065ecae4d19aac22456b024e
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-29 22:26:28 +0000

    applied patch from JackPhoenix (init, standard compliance & cosmetics; closes #10252)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5546 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditAccount/SpecialEditAccount.php
+++ extensions/wikia/EditAccount/SpecialEditAccount.php
@@ -1,40 +1,37 @@
 <?php
-
 /**
  * EditAccount
  *
  * This extension is used by Wikia Staff to manage essential user account information
  * in the case of a lost password and/or invalid e-mail submitted during registration.
  *
+ * @file
+ * @ingroup Extensions
  * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
  * @date 2008-09-17
- * @copyright Copyright (C) 2008 Łukasz Garczewski, Wikia Inc.
+ * @copyright Copyright © 2008 Łukasz Garczewski, Wikia Inc.
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- * @package MediaWiki
- * @subpackage SpecialPage
  */
 
-if (!defined('MEDIAWIKI')) {
-        echo "This is MediaWiki extension named EditAccount.\n";
-        exit(1) ;
+if( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension named EditAccount.\n";
+	exit( 1 );
 }
 
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['specialpage'][] = array(
-        'name' => 'EditAccount',
-        'author' => "[http://www.wikia.com/wiki/User:TOR Lucas 'TOR' Garczewski]",
-        'description' => 'Enables Wikia Staff members to manage user account information.'
+	'name' => 'EditAccount',
+	'version' => '1.0',
+	'author' => "[http://www.wikia.com/wiki/User:TOR Łukasz 'TOR' Garczewski]",
+	'description' => 'Enables Wikia Staff members to manage user account information.'
 );
 
-$wgExtensionMessagesFiles['EditAccount'] = dirname(__FILE__) . '/SpecialEditAccount.i18n.php';
-
-$wgSpecialPageGroups['EditAccount'] = 'users';
-
-//Allow group STAFF to use this extension.
+// New user right, required to use the extension.
 $wgAvailableRights[] = 'editaccount';
 $wgGroupPermissions['*']['editaccount'] = false;
 $wgGroupPermissions['staff']['editaccount'] = true;
 
-//Log deffinition
+// Log definition
 $wgLogTypes[] = 'editaccnt';
 $wgLogNames['editaccnt'] = 'editaccount-log';
 $wgLogHeaders['editaccnt'] = 'editaccount-log-header';
@@ -42,8 +39,10 @@ $wgLogActions['editaccnt/mailchange'] = 'editaccount-log-entry-email';
 $wgLogActions['editaccnt/passchange'] = 'editaccount-log-entry-pass';
 $wgLogActions['editaccnt/closeaccnt'] = 'editaccount-log-entry-close';
 
-//Register special page
-if (!function_exists('extAddSpecialPage')) {
-        require("$IP/extensions/ExtensionFunctions.php");
-}
-extAddSpecialPage(dirname(__FILE__) . '/SpecialEditAccount_body.php', 'EditAccount', 'EditAccount');
+// Set up the new special page
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['EditAccount'] = $dir . 'SpecialEditAccount.i18n.php';
+$wgAutoloadClasses['EditAccount'] = $dir. 'SpecialEditAccount_body.php';
+$wgSpecialPages['EditAccount'] = 'EditAccount';
+// Special page group for MW 1.13+
+$wgSpecialPageGroups['EditAccount'] = 'users';
--- extensions/wikia/EditAccount/SpecialEditAccount_body.php
+++ extensions/wikia/EditAccount/SpecialEditAccount_body.php
@@ -1,22 +1,21 @@
 <?php
-
 /**
  * EditAccount
  *
  * This extension is used by Wikia Staff to manage essential user account information
  * in the case of a lost password and/or invalid e-mail submitted during registration.
  *
+ * @file
+ * @ingroup Extensions
  * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
  * @date 2008-09-17
- * @copyright Copyright (C) 2008 Łukasz Garczewski, Wikia Inc.
+ * @copyright Copyright © 2008 Łukasz Garczewski, Wikia Inc.
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- * @package MediaWiki
- * @subpackage SpecialPage
  */
 
-if (!defined('MEDIAWIKI')) {
-	echo "This is MediaWiki extension named EditAccount.\n";
-	exit(1) ;
+if( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension named EditAccount.\n";
+	exit( 1 );
 }
 
 class EditAccount extends SpecialPage {
@@ -24,45 +23,67 @@ class EditAccount extends SpecialPage {
 	var $mStatus = null;
 	var $mStatusMsg;
 
-	function EditAccount() {
-                SpecialPage::SpecialPage('EditAccount', 'editaccount');
-                wfLoadExtensionMessages('EditAccount');
-        }
-          
-	function execute() {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct( 'EditAccount'/*class*/, 'editaccount'/*restriction*/ );
+		wfLoadExtensionMessages( 'EditAccount' ); // Load internationalization messages
+	}
+
+	/**
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the page or null
+	 */
+	public function execute( $par ) {
 		global $wgOut, $wgUser, $wgRequest;
+
+		// Set page title and other stuff
 		$this->setHeaders();
 
-		// quit early if user is not permited to access
-		if(!$wgUser->isAllowed('editaccount')) {
-                        $wgOut->permissionRequired('editaccount');
-                        return;
+		# If the user isn't permitted to access this special page, display an error
+		if( !$wgUser->isAllowed( 'editaccount' ) ) {
+			$wgOut->permissionRequired( 'editaccount' );
+			return;
 		}
 
-		$action = $wgRequest->getVal('wpAction');
-		$userName = $wgRequest->getVal('wpUserName');
+		# Show a message if the database is in read-only mode
+		if ( wfReadOnly() ) {
+			$wgOut->readOnlyPage();
+			return;
+		}
+
+		# If user is blocked, s/he doesn't need to access this page
+		if ( $wgUser->isBlocked() ) {
+			$wgOut->blockedPage();
+			return;
+		}
+
+		$action = $wgRequest->getVal( 'wpAction' );
+		$userName = $wgRequest->getVal( 'wpUserName' );
 
 		// check if user name is an existing user
-		if (!empty($userName)) {
+		if( !empty( $userName ) ) {
 			$this->mUser = User::newFromName( $userName );
-			if ($this->mUser->idFromName( $userName ) === 0) {
+			if( $this->mUser->idFromName( $userName ) === 0 ) {
 				$this->mStatus = false;
-				$this->mStatusMsg = wfMsg('editaccount-nouser', $userName);
+				$this->mStatusMsg = wfMsg( 'editaccount-nouser', $userName );
 				$action = '';
 			}
 		} else {
 			$action = '';
 		}
 
-		switch ($action) {
+		switch( $action ) {
 			case 'setemail':
-				$newEmail = $wgRequest->getVal('wpNewEmail');
-				$this->mStatus = $this->setEmail($newEmail);
+				$newEmail = $wgRequest->getVal( 'wpNewEmail' );
+				$this->mStatus = $this->setEmail( $newEmail );
 				$template = $this->mStatus ? 'selectuser' : 'displayuser';
 				break;
 			case 'setpass':
-				$newPass = $wgRequest->getVal('wpNewPass');
-				$this->mStatus = $this->setPassword($newPass);
+				$newPass = $wgRequest->getVal( 'wpNewPass' );
+				$this->mStatus = $this->setPassword( $newPass );
 				$template = $this->mStatus ? 'selectuser' : 'displayuser';
 				break;
 			case 'closeaccount':
@@ -76,86 +97,106 @@ class EditAccount extends SpecialPage {
 				$template = 'selectuser';
 		}	
 
-		$wgOut->setPageTitle(wfMsg('editaccount-title'));
+		$wgOut->setPageTitle( wfMsg( 'editaccount-title' ) );
 
 		$oTmpl = new EasyTemplate(dirname( __FILE__ ) . '/templates/');
 		$oTmpl->set_Vars( array(
-				'status'    => $this->mStatus,
+				'status'	=> $this->mStatus,
 				'statusMsg' => $this->mStatusMsg,
-				'user'      => $userName,
+				'user'	  => $userName,
 				'userEmail' => is_object($this->mUser) ? $this->mUser->getEmail() : null
 			));
-		$wgOut->addHTML($oTmpl->execute($template));
+		// HTML output
+		$wgOut->addHTML( $oTmpl->execute( $template ) );
 	}
 
-	function setEmail($email) {
+	/**
+	 * Set a user's email
+	 * @param $email Mixed: email address to set to the user
+	 * @return Boolean: true on success, false on failure (i.e. if we were given an invalid email address)
+	 */
+	function setEmail( $email ) {
 		$oldEmail = $this->mUser->getEmail();
-		if ($this->mUser->isValidEmailAddr( $email )) {
+		if( $this->mUser->isValidEmailAddr( $email ) ) {
 			$this->mUser->setEmail( $email );
 			$this->mUser->saveSettings();
 
 			// Check if everything went through OK, just in case
-			if ($this->mUser->getEmail() == $email) {
+			if( $this->mUser->getEmail() == $email ) {
 				global $wgUser, $wgTitle;
 
-				$log = new LogPage('editaccnt');
-				$log->addEntry('mailchange', $wgTitle, '', array($this->mUser->getUserPage()));
+				$log = new LogPage( 'editaccnt' );
+				$log->addEntry( 'mailchange', $wgTitle, '', array( $this->mUser->getUserPage() ) );
 
-				$this->mStatusMsg = wfMsg('editaccount-success-email', $this->mUser->mName, $email);
+				$this->mStatusMsg = wfMsg( 'editaccount-success-email', $this->mUser->mName, $email );
 				return true;
 			} else {
-				$this->mStatusMsg = wfMsg('editaccount-error-email', $this->mUser->mName);
+				$this->mStatusMsg = wfMsg( 'editaccount-error-email', $this->mUser->mName );
 				return false;
 			}
 		} else {
-			$this->mStatusMsg = wfMsg('editaccount-invalid-email', $email);
+			$this->mStatusMsg = wfMsg( 'editaccount-invalid-email', $email );
 			return false;
 		}
 	}
 
-	function setPassword($pass) {
-		if ($this->mUser->setPassword($pass)) {
+	/**
+	 * Set a user's password
+	 * @param $pass Mixed: password to set to the user
+	 * @return Boolean: true on success, false on failure
+	 */
+	function setPassword( $pass ) {
+		if( $this->mUser->setPassword( $pass ) ) {
 			global $wgUser, $wgTitle;
 
+			// Save the new settings
 			$this->mUser->saveSettings();
 
-			$log = new LogPage('editaccnt');
-			$log->addEntry('passchange', $wgTitle, '', array($this->mUser->getUserPage()));
+			// Log what was done
+			$log = new LogPage( 'editaccnt' );
+			$log->addEntry( 'passchange', $wgTitle, '', array( $this->mUser->getUserPage() ) );
 
-			$this->mStatusMsg = wfMsg('editaccount-success-pass', $this->mUser->mName);
+			// And finally, inform the user that everything went as planned
+			$this->mStatusMsg = wfMsg( 'editaccount-success-pass', $this->mUser->mName );
 			return true;
 		} else {
-			$this->mStatusMsg = wfMsg('editaccount-error-pass', $this->mUser->mName);
+			// We have errors, let's inform the user about those
+			$this->mStatusMsg = wfMsg( 'editaccount-error-pass', $this->mUser->mName );
 			return false;
 		}
 	}
 
-	/* 
-	 * closeAccount
+	/**
+	 * Scrambles the user's password, sets an empty e-mail and marks as disabled
 	 *
-	 * Scrables user's password, sets an empty e-mail and marks as disabled
+	 * @return Boolean: true on success, false on failure
 	 */
 	function closeAccount() {
 		# Set flag for Special:Contributions
 		# NOTE: requires FlagClosedAccounts.php to be included separately
-		if (defined('CLOSED_ACCOUNT_FLAG')) {
+		if( defined( 'CLOSED_ACCOUNT_FLAG' ) ) {
 			$this->mUser->setRealName( CLOSED_ACCOUNT_FLAG );
 		}
 
-		$this->mUser->setEmail('');
+		// Remove e-mail address
+		$this->mUser->setEmail( '' );
 
 		if ( $this->mUser->setPassword( wfGenerateToken() ) ) {
-			global $wgUser, $wgtitle;
+			global $wgUser, $wgTitle;
 
+			// Save the new settings
 			$this->mUser->saveSettings();
 
-			$log = new LogPage('editaccnt');
-			$log->addEntry('closeaccnt', $wgTitle, '', array($this->mUser->getUserPage()));
+			// Log what was done
+			$log = new LogPage( 'editaccnt' );
+			$log->addEntry( 'closeaccnt', $wgTitle, '', array( $this->mUser->getUserPage() ) );
 
-			$this->mStatusMsg = wfMsg('editaccount-success-close', $this->mUser->mName);
+			// All clear!
+			$this->mStatusMsg = wfMsg( 'editaccount-success-close', $this->mUser->mName );
 			return true;	
-		} else {	
-			$this->mStatusMsg = wfMsg('editaccount-error-close', $this->mUser->mName);
+		} else {
+			// There were errors...inform the user about those
+			$this->mStatusMsg = wfMsg( 'editaccount-error-close', $this->mUser->mName );
 			return false;
 		}
 	}
