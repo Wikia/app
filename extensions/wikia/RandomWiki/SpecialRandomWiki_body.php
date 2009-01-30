d11ece35dc250727684b67263a8d8b370d667878
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-01-30 12:25:00 +0000

    RandomWiki: applied patch from Jack Phoenix (init & cosmetics, closes #10253)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5556 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RandomWiki/SpecialRandomWiki_body.php
+++ extensions/wikia/RandomWiki/SpecialRandomWiki_body.php
@@ -1,32 +1,40 @@
 <?php
-
 /**
  * RandomWiki
  *
+ * @file
+ * @ingroup Extensions
  * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
  * @author Maciej Błaszkowski (Marooned) <marooned@wikia-inc.com>
- * @date 2008-10-14
- * @copyright Copyright (C) 2008 Łukasz Garczewski, Wikia Inc.
+ * @date 2009-01-30
+ * @copyright Copyright © 2008 Łukasz Garczewski, Wikia Inc.
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- * @package MediaWiki
- * @subpackage SpecialPage
  */
 
-if (!defined('MEDIAWIKI')) {
-	echo "This is MediaWiki extension named RandomWiki.\n";
-	exit(1) ;
+if( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension named RandomWiki.\n";
+	exit( 1 );
 }
 
 class RandomWiki extends SpecialPage {
 
-	function RandomWiki() {
-                SpecialPage::SpecialPage('RandomWiki');
-        }
-          
-	function execute() {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct( 'RandomWiki' );
+	}
+
+	/**
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the page or null
+	 */
+	public function execute( $par ) {
 		global $wgOut, $wgSharedDB;
 
 		$dbr = wfGetDB( DB_SLAVE );
+		// Make sure that we're in the shared database so that the query will work
 		$dbr->selectDB( $wgSharedDB );
 
 		$res = $dbr->select( 'city_list', array( 'city_url' ), array( 'city_public' => 1 ) );
@@ -39,6 +47,7 @@ class RandomWiki extends SpecialPage {
 
 		$targetWiki = $dbr->fetchObject( $res );
 
+		// Redirect the user to a randomly-chosen wiki
 		$wgOut->redirect( $targetWiki->city_url );
 	}
 
