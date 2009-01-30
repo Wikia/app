0b9d72c3832ac1bc18f923dac4d00d8da920eaaf
tor <tor@wikia-inc.com>
2009-01-30 12:25:00 +0000

    RandomWiki: applied patch from Jack Phoenix (init & cosmetics, closes #10253)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5556 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RandomWiki/SpecialRandomWiki.php
+++ extensions/wikia/RandomWiki/SpecialRandomWiki.php
@@ -1,35 +1,35 @@
 <?php
-
 /**
  * RandomWiki
  *
+ * @file
+ * @ingroup Extensions
  * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
  * @author Maciej Błaszkowski (Marooned) <marooned@wikia-inc.com>
- * @date 2008-10-14
- * @copyright Copyright (C) 2008 Łukasz Garczewski, Wikia Inc.
+ * @date 2009-01-30
+ * @version 1.0
+ * @copyright Copyright © 2008 Łukasz Garczewski, Wikia Inc.
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- * @package MediaWiki
- * @subpackage SpecialPage
  */
 
-if (!defined('MEDIAWIKI')) {
-        echo "This is MediaWiki extension named RandomWiki.\n";
-        exit(1) ;
+if( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension named RandomWiki.\n";
+	exit (1 );
 }
 
 $wgExtensionCredits['specialpage'][] = array(
-        'name' => 'RandomWiki',
-        'author' => array(
-			"[http://www.wikia.com/wiki/User:TOR Lucas 'TOR' Garczewski]",
-			'[http://www.wikia.com/wiki/User:Marooned Maciej Błaszkowski (Marooned)]'
-		 ),
-        'description' => 'Lets users explore a random wiki.'
+	'name' => 'RandomWiki',
+	'version' => '1.0',
+	'author' => array(
+		"[http://www.wikia.com/wiki/User:TOR Łukasz 'TOR' Garczewski]",
+		'[http://www.wikia.com/wiki/User:Marooned Maciej Błaszkowski (Marooned)]'
+	),
+	'description' => 'Lets users explore a random wiki.'
 );
 
+// Set up the new special page
+$dir = dirname(__FILE__) . '/';
+$wgAutoloadClasses['RandomWiki'] = $dir. 'SpecialRandomWiki_body.php';
+$wgSpecialPages['RandomWiki'] = 'RandomWiki';
+// Special page group for MW 1.13+
 $wgSpecialPageGroups['RandomWiki'] = 'wikia';
-
-//Register special page
-if (!function_exists('extAddSpecialPage')) {
-        require("$IP/extensions/ExtensionFunctions.php");
-}
-extAddSpecialPage(dirname(__FILE__) . '/SpecialRandomWiki_body.php', 'RandomWiki', 'RandomWiki');
