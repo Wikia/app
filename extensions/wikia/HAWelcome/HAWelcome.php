52e75b7db0ed50e06f5ab0cf1bf77c26f3ad4283
eloy <eloy@wikia-inc.com>
2009-02-02 16:31:42 +0000

    added hook after revision complete
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5638 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/HAWelcome/HAWelcome.php
+++ extensions/wikia/HAWelcome/HAWelcome.php
@@ -1,8 +1,15 @@
 <?php
 
 /**
+ * Welcome user after first edit
+ *
+ * @copyright Copyright © Krzysztof Krzyżaniak for Wikia Inc.
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
  * @ingroup JobQueue
  */
+
+$wgHooks[ "RevisionInsertComplete" ][]	= "HAWelcome::revisionInsertComplete";
+
 class HAWelcome extends Job {
 
 	/**
@@ -16,6 +23,37 @@ class HAWelcome extends Job {
 	}
 
 	public function run() {
+		return true;
+	}
+
+	/**
+	 * revisionInsertComplete
+	 *
+	 * static method called as hook
+	 *
+	 * @static
+	 * @access public
+	 *
+	 * @param Revision	$revision	revision object
+	 * @param string	$url		url to external object
+	 * @param string	$flags		flags for this revision
+	 *
+	 * @return true means process other hooks
+	 */
+	public static function revisionInsertComplete( &$revision, &$url, &$flags ) {
+		global $wgTitle, $wgUser;
+
+		/**
+		 * check if talk page for wgUser exists
+		 */
+		$talkPage = $wgUser->getUserPage()->getTalkPage();
+		if( $talkPage ) {
+			$talkArticle = new Article( $talkPage, 0 );
+			if( !$talkArticle->exits( ) ) {
+				$welcomeJob = new HAWelcome( $wgTitle, array( "user" => $wgUser ) );
+				$welcomeJob->insert();
+			}
+		}
 		return true;
 	}
 }
