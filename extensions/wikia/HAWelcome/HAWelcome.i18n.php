60ef924fd941a5bda6f9dc42b1dcd76461884e5f
eloy <eloy@wikia-inc.com>
2009-02-06 09:54:57 +0000

    language file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5762 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/HAWelcome/HAWelcome.i18n.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *  extension message file
+ */
+
+$messages = array();
+$messages['en'] = array(
+	"hawelcome" => "@latest",
+	"hawelcome-message-user" => "Hi, welcome to {{SITENAME}}! Thanks for your edit to the [[$1]] page.
+
+Please leave a message on [[$2|my talk page]] if I can help with anything! -- ~~~~",
+	"hawelcome-message-anon" => "Hi, welcome to {{SITENAME}}! Thanks for your edit to the [[$1]] page.
+
+'''[[Special:Userlogin|Please sign in and create a user name]]'''. It's an easy way to keep track of your contributions and helps you communicate with the rest of the community.
+
+Please leave a message on [[$2|my talk page]] if I can help with anything! -- ~~~~",
+	"hawelcome-message-log" => "Friendly welcome"
+);
