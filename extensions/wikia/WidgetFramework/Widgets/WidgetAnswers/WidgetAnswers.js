b13e0ec53b11c25a1a392ed5c1c50097234e49ce
christian <christian@wikia-inc.com>
2009-02-07 00:19:30 +0000

    js file for wikianswers widget
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5787 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WidgetFramework/Widgets/WidgetAnswers/WidgetAnswers.js
@@ -0,0 +1,22 @@
+var widget_answers_placeholder = 'Ask a question';
+function answers_widget_handler(e) { 
+	if (e.type == 'focus') {
+		if (YAHOO.util.Event.getTarget(e).value == widget_answers_placeholder) {
+			YAHOO.util.Dom.removeClass('answers_ask_field', 'alt');	
+			$('answers_ask_field').value = '';
+		}	
+	} else if (e.type == 'blur') {
+		if (YAHOO.util.Event.getTarget(e).value == '') {
+			YAHOO.util.Dom.addClass('answers_ask_field', 'alt');
+			$('answers_ask_field').value = widget_answers_placeholder;
+		}
+	} else if (e.type == 'keypress') {
+		var keycode = e.which || window.event.keyCode;
+		if (keycode == 13 && $('answers_ask_field').value != '' ) {
+			window.open('http://answers.wikia.com/index.php?title=Special:CreateQuestionPage&questiontitle=' + $('answers_ask_field').value + '&categories=' + wgSitename, 'wikianswers');
+		}
+	}
+} 
+YAHOO.util.Event.addListener('answers_ask_field', "focus", answers_widget_handler); 
+YAHOO.util.Event.addListener('answers_ask_field', "blur", answers_widget_handler);
+YAHOO.util.Event.addListener('answers_ask_field', "keypress", answers_widget_handler);
