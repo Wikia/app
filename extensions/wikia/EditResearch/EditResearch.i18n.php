103768c07c632d2ea53c34853b971f52ccbd3d56
pean <pean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-02-11 22:19:10 +0000

    new extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5916 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/EditResearch/EditResearch.i18n.php
@@ -0,0 +1,7 @@
+<?php
+$messages = array();
+$messages['en'] = array(
+	'search_wikipedia' => 'Search Wikipedia',
+	'previous' => 'previous',
+	'research_no_results' => 'No pages found.  Please search again.'
+);
---
+++ extensions/wikia/EditResearch/EditResearch.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Author: David Pean
+ */
+
+$wgExtensionCredits['other'][] = array(
+    'name' => 'EditResearch',
+    'author' => 'David Pean',
+);
+
+$wgExtensionMessagesFiles['EditResearch'] = dirname(__FILE__).'/'.'EditResearch.i18n.php';
+
+$wgHooks['EditForm:BeforeDisplayingTextbox'][] = 'AddEditResearch';
+function AddEditResearch($o) {
+	global $wgUser, $wgOut, $wgHooks,$wgStylePath, $wgStyleVersion, $wgExtensionsPath ;
+	
+	wfLoadExtensionMessages('EditResearch');
+	
+	$wgOut->addScript('<link rel="stylesheet" type="text/css" href="'.$wgExtensionsPath.'/wikia/EditResearch/EditResearch.css?'.$wgStyleVersion.'"/>');
+	$wgOut->addScript('<script type="text/javascript" src="'.$wgExtensionsPath.'/wikia/EditResearch/EditResearch.js?'.$wgStyleVersion.'"></script>');
+
+	$script = '
+	<script type="text/javascript">
+		var wgNextMsg = "' . wfMsg('next')  . '";
+		var wgPrevMsg = "' . wfMsg('previous')  . '";
+		var wgNoResultsMsg = "' . wfMsg('research_no_results')  . '";
+	</script>';
+	$wgOut->addHtml( $script );
+	
+	$wgOut->addHtml( "<div id='research-container'>");
+	$wgOut->addHtml( "<h3>" . wfMsg("reasearch_wikipedia_title") . "</h3>" );
+	$wgOut->addHtml( "<div id='research-inner'>");
+	$wgOut->addHtml( "<div id='research-search'>");					
+	$wgOut->addHtml(wfMsg("search_wikipedia") . ' <input type="text" id="search_input"> <input id="search_button" type="button" value="' . wfMsg("go") . '" onclick="research()">');
+	$wgOut->addHtml('</div>');
+	$wgOut->addHtml('<div id="research_box" ></div></div></div>');
+	
+	return true;
+}
+
+
