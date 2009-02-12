2552842f9c9736dd59be3df54c5a2f68ba8e2c72
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-02-12 12:43:26 +0000

    * Support for Video namespace in Parser
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@5944 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -250,3 +250,10 @@ $wgExtensionsPath = false; /// defaults to "{$wgScriptPath}/extensions"
  */
 $wgHubCreationVariables = array();
 $wgLangCreationVariables = array();
+
+/**
+ * Define Video namespace (used by WikiaVideo extensions)
+ * Can not be define directly in extension since it is used in Parser.php and extension is not always enabled
+ */
+define('NS_VIDEO', '400');
+$wgExtraNamespaces[NS_VIDEO] = 'Video';
