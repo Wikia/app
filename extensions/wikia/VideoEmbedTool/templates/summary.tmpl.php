b71e577a4e77eb5c8e45ca13ffbe0bfc3a2ba9df
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-02-19 14:48:17 +0000

    * Merge WikiaVideo changes upto r6244
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6250 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
+++ extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
@@ -5,4 +5,5 @@
 	<div id="VideoEmbedTag" style="border-bottom: 1px solid #CCC; padding: 15px 0; margin: 15px;"><?= $tag ?></div>
 	<div id="VideoEmbedPageSuccess" style="display:none;"><?= wfMsg( 'vet-page-success' ) ?></br><br/></div>
 	<input onclick="VET_close(event);" type="button" value="<?= wfMsg( 'vet-return' ) ?>" />
+	<div id="VideoEmbedCode" style="display: none;" ><?= $code ?></div>
 </div>
