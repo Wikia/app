6a3ef0e9eb16c44aa6345481d92812f4d1038f4c
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-02-20 16:14:31 +0000

    * Inc
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6300 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -26,9 +26,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '5552';
-$wgMergeStyleVersionJS  = '5552';
-$wgMergeStyleVersionCSS = '5552';
+$wgStyleVersion         = '5553';
+$wgMergeStyleVersionJS  = '5553';
+$wgMergeStyleVersionCSS = '5553';
 
 /**
  * @name $wgCityId
