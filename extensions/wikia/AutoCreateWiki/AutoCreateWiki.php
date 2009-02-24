b0dafd4e12a2de29d75c3024df900d996f6bb946
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-02-24 09:52:07 +0000

    schema for new extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6372 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AutoCreateWiki/AutoCreateWiki.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Lazy loader for Special:AutoCreateWiki
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Krzysztof Krzyżaniak <eloy@wikia-inc.com> for Wikia.com
+ * @copyright © 2009, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @version 1.0
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension and cannot be used standalone.\n";
+	exit( 1 );
+}
+
+/**
+ * Extension credits that will show up on Special:Version
+ */
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'AutoCreateWiki',
+	'version' => '1.0',
+	'author' => 'Krzysztof Krzyźaniak',
+	'description' => 'Create wiki in WikiFactory by user requests'
+);
+
+/**
+ * Set up the new special page
+ */
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles[ "AutoCreateWiki" ] = $dir . "AutoCreateWiki.i18n.php";
+$wgAutoloadClasses[ "AutoCreateWiki" ] = $dir. "AutoCreateWiki_body.php";
+$wgSpecialPages[ "AutoCreateWiki" ] = "AutoCreateWikiPage";
