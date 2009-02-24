1922df06565a55748e3c35b4cd54551c3e04c5b1
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-02-24 11:05:11 +0000

    AutoCreateWiki: test suite
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6380 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/AutoCreateWiki_body.php
+++ extensions/wikia/AutoCreateWiki/AutoCreateWiki_body.php
@@ -17,13 +17,47 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 
 class Our404HandlerPage extends SpecialPage {
 
-	public $mTitle, $mAction, $mSubpage;
+	private
+		$mTitle,
+		$mAction,
+		$mSubpage,
+		$mWikiData,
+		$mMYSQLdump,
+		$mMYSQLbin,
+		$mPHPbin,
+		$mImagesDir;
+
+	/**
+	 * test database, CAUTION! content will be destroyed during tests
+	 */
+	const TESTDB = "testdb";
 
 	/**
 	 * constructor
 	 */
 	public function  __construct() {
 		parent::__construct( "AutoCreateWiki" /*class*/ );
+
+		/**
+		 * initialize some data
+		 */
+		$this->mWikiData = array();
+		$this->mImagesDir = "/images/";
+
+		/**
+		 * set paths for external tools
+		 */
+		$this->mPHPbin =
+			( file_exists("/usr/bin/php") && is_executable( "/usr/bin/php" ))
+			? "/usr/bin/php" : "/opt/wikia/php/bin/php";
+
+		$this->mMYSQLdump =
+			( file_exists("/usr/bin/mysqldump") && is_executable( "/usr/bin/mysqldump" ))
+			? "/usr/bin/mysqldump" : "/opt/wikia/bin/mysqldump";
+
+		$this->mMYSQLbin =
+			( file_exists("/usr/bin/mysql") && is_executable("/usr/bin/mysql") )
+			? "/usr/bin/mysql" : "/opt/wikia/bin/mysql";
 	}
 
 	/**
@@ -42,5 +76,46 @@ class Our404HandlerPage extends SpecialPage {
 		$this->mAction = $wgRequest->getVal( "action", false );
 		$this->mSubpage = $subpage;
 
+	}
+
+	/**
+	 * main function for extension -- create wiki in wikifactory cluster
+	 */
+	private function create() {
+
+		/**
+		 * this will clean test database and fill mWikiData with test data
+		 */
+		$this->prepareTest();
+
+	}
+
+	/**
+	 * prepareTest, clear test database
+	 */
+	private function prepareTest() {
+
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->query( sprintf( "DROP DATABSE %s", self::TESTDB ) );
+
+		$this->mWikiData[ "hub" ]		= 1;
+        $this->mWikiData[ "name"]       = strtolower( trim( "testdb" ) );
+        $this->mWikiData[ "title" ]     = trim( "TestDB Wiki" );
+        $this->mWikiData[ "language" ]  = trim( "en" );
+        $this->mWikiData[ "subdomain" ] = $this->mWikiData[ "name"];
+        $this->mWikiData[ "redirect"]   = $this->mWikiData[ "name"];
+		$this->mWikiData[ "dir_part"]   = $this->mWikiData[ "name"];
+		$this->mWikiData[ "dbname"]     = substr( $this->mWikiData[ "name"], 0, 64);
+		$this->mWikiData[ "path"]       = "/usr/wikia/docroot/wiki.factory";
+        $this->mWikiData[ "images"]     = $this->mImagesDir . $this->mWikiData[ "name"];
+        $this->mWikiData[ "testWiki"]   = true;
+
+        if( isset( $this->mWikiData[ "language" ] ) && $this->mWikiData[ "language" ] !== "en" ) {
+			$this->mWikiData[ "subdomain"] = strtolower( $this->mWikiData[ "subdomain"] ) . "." . $this->mWikiData[ "name"];
+			$this->mWikiData[ "redirect"] = strtolower( $this->mWikiData[ "language" ] ) . "." . ucfirst( $this->mWikiData[ "name"] );
+			$this->mWikiData[ "images"] .= "/" . strtolower( $this->mWikiData[ "language" ] );
+			$this->mWikiData[ "dbname"] = strtolower(str_replace("-","", $this->mWikiData[ "language" ] ). $this->mWikiData[ "dbname"] );
+			$this->mWikiData[ "dir_part"] .= "/".strtolower( $this->mWikiData[ "language" ] );
+		}
 	}
 }
