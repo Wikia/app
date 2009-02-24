193775b26e3e30aef129e6bde952ce21636fa0f2
eloy <eloy@wikia-inc.com>
2009-02-24 16:17:32 +0000

    AutoCreateWiki: populate city_list, city_domains
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6402 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/AutoCreateWiki_body.php
+++ extensions/wikia/AutoCreateWiki/AutoCreateWiki_body.php
@@ -22,6 +22,7 @@ class AutoCreateWikiPage extends SpecialPage {
 		$mAction,
 		$mSubpage,
 		$mWikiData,
+		$mWikiId,
 		$mMYSQLdump,
 		$mMYSQLbin,
 		$mPHPbin,
@@ -35,6 +36,9 @@ class AutoCreateWikiPage extends SpecialPage {
 	const GAMING = 2;
 	const ENTERTAINMENT = 3;
 	const LOG = "autocreatewiki";
+    const CREATEWIKI_LOGO = "/images/central/images/2/22/Wiki_Logo_Template.png";
+    const CREATEWIKI_ICON = "/images/central/images/6/64/Favicon.ico";
+
 
 	/**
 	 * constructor
@@ -85,11 +89,13 @@ class AutoCreateWikiPage extends SpecialPage {
 
 	/**
 	 * main function for extension -- create wiki in wikifactory cluster
+	 * we are assumming that data is valid!
+	 *
 	 */
 	private function create() {
 
-		global $wgDebugLogGroups;
-		$wgDebugLogGroups[ self::LOG ] = "/tmp/autocreatewiki.log";
+		global $wgDebugLogGroups, $wgOut, $wgUser;
+		# $wgDebugLogGroups[ self::LOG ] = "/tmp/autocreatewiki.log";
 
 		wfProfileIn( __METHOD__ );
 
@@ -97,9 +103,85 @@ class AutoCreateWikiPage extends SpecialPage {
 		 * this will clean test database and fill mWikiData with test data
 		 */
 		$this->prepareTest();
-		print_pre( $this->mWikiData );
+		$wgOut->addHtml( print_pre( $this->mWikiData, 1 ) );
 
 		$this->mCurrTime = wfTime();
+
+		/**
+		 * create image folder
+		 */
+		wfMkdirParents( $this->mWikiData[ "images"] );
+		$this->log( "Create {$this->mWikiData[ "images"]} folder" );
+
+		/**
+		 * check and create database
+		 */
+		$dbw = wfGetDB( DB_MASTER );
+		$Row = $dbw->selectRow(
+			wfSharedTable("city_list"),
+			array( "count(*) as count" ),
+			array( "city_dbname" => $this->mWikiData[ "dbname"] ),
+			__METHOD__
+		);
+		$this->log( "Checking if database {$this->mWikiData[ "dbname"]} already exists");
+		if( $Row->count > 0 ) {
+			#error
+		}
+		$dbw->query( sprintf( "CREATE DATABASE %s", $this->mWikiData[ "dbname"]) );
+		$this->log( "Creating database {$this->mWikiData[ "dbname"]}" );
+
+		/**
+		 * create position in wiki.factory
+		 * (I like sprintf construction, so sue me)
+		 */
+		$dbw->insert(
+			wfSharedTable("city_list"),
+			array(
+				'city_title'          => $this->mWikiData[ "title" ],
+				'city_dbname'         => $this->mWikiData[ "dbname"],
+				'city_url'            => sprintf( "http://%s.wikia.com/", $this->mWikiData[ "subdomain" ] ),
+				'city_founding_user'  => $wgUser->getID(),
+				'city_founding_email' => $wgUser->getEmail(),
+				'city_path'           => $this->mWikiData[ "path" ],
+				'city_description'    => $this->mWikiData[ "title" ],
+				'city_lang'           => $this->mWikiData[ "language" ],
+				'city_created'        => wfTimestamp( TS_DB, time() ),
+			),
+			__METHOD__
+		);
+		$this->mWikiId = $dbw->insertId();
+		$this->log( "Creating row in city_list table, city_id = {$this->mWikiId}" );
+
+		$dbw->insert(
+			wfSharedTable("city_domains"),
+			array(
+				array(
+					'city_id'     =>  $this->mWikiId,
+					'city_domain' => sprintf("%s.wikia.com", $this->mWikiData[ "subdomain" ] )
+				),
+				array(
+					'city_id'     =>  $this->mWikiId,
+					'city_domain' => sprintf("www.%s.wikia.com", $this->mWikiData[ "subdomain" ] )
+				)
+			),
+			__METHOD__
+		);
+		$this->log( "Populating city_domains" );
+
+
+		/**
+		 * copy defaul logo & favicon
+		 */
+		wfMkdirParents("{$this->mWikiData[ "images" ]}/images/b/bc");
+		wfMkdirParents("{$this->mWikiData[ "images" ]}/images/6/64");
+
+		if (file_exists(self::CREATEWIKI_LOGO)) {
+			copy(self::CREATEWIKI_LOGO, "{$this->mWikiData[ "images" ]}/images/b/bc/Wiki.png");
+		}
+		if (file_exists(self::CREATEWIKI_ICON)) {
+			copy(self::CREATEWIKI_ICON, "{$this->mWikiData[ "images" ]}/images/6/64/Favicon.ico");
+		}
+		$this->log( "Coping favicon and logo" );
 
 		wfProfileOut( __METHOD__ );
 	}
@@ -114,9 +196,6 @@ class AutoCreateWikiPage extends SpecialPage {
 		$languages = array( "de", "en", "pl", "fr", "es" );
 		shuffle( $languages );
 
-		$dbw = wfGetDB( DB_MASTER );
-		$dbw->query( sprintf( "DROP DATABASE IF EXISTS %s", self::TESTDB ) );
-
 		$this->mWikiData[ "hub" ]		= rand( 1, 19 );
         $this->mWikiData[ "name"]       = strtolower( trim( self::TESTDB ) );
         $this->mWikiData[ "title" ]     = trim( $wgContLang->ucfirst( self::TESTDB ) . " Wiki" );
@@ -124,18 +203,24 @@ class AutoCreateWikiPage extends SpecialPage {
         $this->mWikiData[ "subdomain" ] = $this->mWikiData[ "name"];
         $this->mWikiData[ "redirect"]   = $this->mWikiData[ "name"];
 		$this->mWikiData[ "dir_part"]   = $this->mWikiData[ "name"];
-		$this->mWikiData[ "dbname"]     = substr( $this->mWikiData[ "name"], 0, 64);
+		$this->mWikiData[ "dbname"]     = substr( str_replace( "-", "", $this->mWikiData[ "name"] ), 0, 64);
 		$this->mWikiData[ "path"]       = "/usr/wikia/docroot/wiki.factory";
         $this->mWikiData[ "images"]     = $this->mImagesDir . $this->mWikiData[ "name"];
         $this->mWikiData[ "testWiki"]   = true;
 
         if( isset( $this->mWikiData[ "language" ] ) && $this->mWikiData[ "language" ] !== "en" ) {
-			$this->mWikiData[ "subdomain"] = strtolower( $this->mWikiData[ "language"] ) . "." . $this->mWikiData[ "name"];
-			$this->mWikiData[ "redirect"] = strtolower( $this->mWikiData[ "language" ] ) . "." . ucfirst( $this->mWikiData[ "name"] );
-			$this->mWikiData[ "images"] .= "/" . strtolower( $this->mWikiData[ "language" ] );
-			$this->mWikiData[ "dbname"] = strtolower(str_replace("-","", $this->mWikiData[ "language" ] ). $this->mWikiData[ "dbname"] );
-			$this->mWikiData[ "dir_part"] .= "/".strtolower( $this->mWikiData[ "language" ] );
+			$this->mWikiData[ "subdomain" ] = strtolower( $this->mWikiData[ "language"] ) . "." . $this->mWikiData[ "name"];
+			$this->mWikiData[ "redirect" ]  = strtolower( $this->mWikiData[ "language" ] ) . "." . ucfirst( $this->mWikiData[ "name"] );
+			$this->mWikiData[ "dbname" ]    = strtolower( str_replace( "-", "", $this->mWikiData[ "language" ] ). $this->mWikiData[ "dbname"] );
+			$this->mWikiData[ "images" ]   .= "/" . strtolower( $this->mWikiData[ "language" ] );
+			$this->mWikiData[ "dir_part" ] .= "/" . strtolower( $this->mWikiData[ "language" ] );
 		}
+
+		/**
+		 * drop test table
+		 */
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->query( sprintf( "DROP DATABASE IF EXISTS %s", self::TESTDB ) );
 
 		/**
 		 * clear wikifactory tables: city_list, city_variables, city_domains
@@ -159,7 +244,13 @@ class AutoCreateWikiPage extends SpecialPage {
 				__METHOD__
 			);
 			$dbw->commit();
+		}
 
+		/**
+		 * remove image directory
+		 */
+		if( file_exists( $this->mWikiData[ "images" ] ) && is_dir( $this->mWikiData[ "images" ] ) ) {
+			exec( "rm -rf {$this->mWikiData[ "images" ]}" );
 		}
 	}
 
@@ -170,8 +261,8 @@ class AutoCreateWikiPage extends SpecialPage {
 		global $wgOut;
 
 		$info = sprintf( "%s: %F", $info, wfTime() - $this->mCurrTime );
-		wfDebugLog( self::log, $info );
-		Wikia::log( self::log, $info );
+		wfDebugLog( self::LOG, $info );
+		Wikia::log( self::LOG, $info );
 		$wgOut->addHtml( $info ."<br />" );
 
 		$this->mCurrTime = wfTime();
