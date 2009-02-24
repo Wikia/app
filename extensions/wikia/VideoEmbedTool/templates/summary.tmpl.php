1cc24e8400f4b4f085dfd2b4943724223ae3bea5
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-02-24 11:22:19 +0000

    VideoEmbedTool: better formatting the summary message
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6381 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
+++ extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
@@ -2,8 +2,9 @@
 <?= wfMsg('vet-success') ?>
 </div>
 <div style="text-align: center;">
-	<div id="VideoEmbedTag" style="border-bottom: 1px solid #CCC; padding: 15px 0; margin: 15px;"><?= $tag ?></div>
+	<div id="VideoEmbedVisualTag" style="border-bottom: 1px solid #CCC; padding: 15px 0; margin: 15px;"><?= htmlspecialchars( $tag ) ?></div>
 	<div id="VideoEmbedPageSuccess" style="display:none;"><?= wfMsg( 'vet-page-success' ) ?></br><br/></div>
 	<input onclick="VET_close(event);" type="button" value="<?= wfMsg( 'vet-return' ) ?>" />
 	<div id="VideoEmbedCode" style="display: none;" ><?= $code ?></div>
+	<div id="VideoEmbedTag" style="border-bottom: 1px solid #CCC; padding: 15px 0; margin: 15px; display:none;"><?= $tag ?></div>
 </div>
