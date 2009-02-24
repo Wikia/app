8b85e4e31f789270a384783cbd8246f0394fe203
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-02-24 15:10:10 +0000

    VideoEmbedTool: small rewrite
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6389 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
+++ extensions/wikia/VideoEmbedTool/templates/summary.tmpl.php
@@ -6,5 +6,5 @@
 	<div id="VideoEmbedPageSuccess" style="display:none;"><?= wfMsg( 'vet-page-success' ) ?></br><br/></div>
 	<input onclick="VET_close(event);" type="button" value="<?= wfMsg( 'vet-return' ) ?>" />
 	<div id="VideoEmbedCode" style="display: none;" ><?= $code ?></div>
-	<div id="VideoEmbedTag" style="border-bottom: 1px solid #CCC; padding: 15px 0; margin: 15px; display:none;"><?= $tag ?></div>
+	<input type="hidden" id="VideoEmbedTag" value="<?= htmlspecialchars( $tag ) ?>" />
 </div>
