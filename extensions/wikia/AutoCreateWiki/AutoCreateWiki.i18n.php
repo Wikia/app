c4b269b599f8631caa07bb09c9223ad53f4332bf
eloy <eloy@wikia-inc.com>
2009-02-24 09:52:07 +0000

    schema for new extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6372 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AutoCreateWiki/AutoCreateWiki.i18n.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Internationalization file for AutoCreateWiki extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array();
---
+++ extensions/wikia/AutoCreateWiki/AutoCreateWiki_body.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Main part of Special:AutoCreateWiki
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Krzysztof Krzyżaniak <eloy@wikia-inc.com> for Wikia.com
+ * @copyright © 2009, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @version 1.0
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension and cannot be used standalone.\n";
+	exit( 1 );
+}
+
+class Our404HandlerPage extends SpecialPage {
+
+	public $mTitle, $mAction, $mSubpage;
+
+	/**
+	 * constructor
+	 */
+	public function  __construct() {
+		parent::__construct( "AutoCreateWiki" /*class*/ );
+	}
+
+	/**
+	 * Main entry point
+	 *
+	 * @access public
+	 *
+	 * @param $subpage Mixed: subpage of SpecialPage
+	 */
+	public function execute( $subpage ) {
+		global $wgRequest;
+		wfLoadExtensionMessages( "AutoCreateWiki" );
+
+		$this->setHeaders();
+		$this->mTitle = Title::makeTitle( NS_SPECIAL, "AutoCreateWiki" );
+		$this->mAction = $wgRequest->getVal( "action", false );
+		$this->mSubpage = $subpage;
+
+	}
+}
