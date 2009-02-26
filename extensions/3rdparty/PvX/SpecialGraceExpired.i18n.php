bd6b642b98d013d07f7fddaeaf4f8077756de5ce
tor <tor@wikia-inc.com>
2009-02-26 15:48:12 +0000

    PvX: GraceExpired custom extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6498 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/3rdparty/PvX/SpecialGraceExpired.i18n.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Internationalisation file for MostPopularCategories extension.
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+*/
+
+$messages['en'] = array(
+	'graceexpired' => 'Grace expired',
+);
