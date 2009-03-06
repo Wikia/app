bc32394e10ba512f11d67262653e0a66aef71141
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-06 15:20:47 +0000

    helper functions
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6675 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AutoCreateWiki/AutoCreateWiki_helper.php
@@ -0,0 +1,288 @@
+<?php
+
+/**
+ * @package MediaWiki
+ * @subpackage RequestWiki
+ * @author Krzysztof Krzyżaniak <eloy@wikia.com> for Wikia.com
+ * @author Piotr Molski <moli@wikia-inc.com> for Wikia.com
+ * @version: 0.1
+ *
+ * helper classes & functions
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+    echo "This is MediaWiki extension and cannot be used standalone.\n";
+    exit( 1 ) ;
+}
+
+class AutoCreateWiki {
+	/**
+	 * isDomainExists
+	 *
+	 * check in city_domains if we have such wikia in city_domains
+	 *
+	 * @param string $name: domain name
+	 * @param string $language default null - choosen language
+	 *
+	 * @return integer - 0 or 1
+	 */
+	public static function domainExists( $name, $language = null  ) {
+		$sDomain = Wikia::fixDomainName($name, $language);
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$oRow = $dbr->selectRow(
+			wfSharedTable("city_domains"),
+			array( "count(*) as count" ),
+			array( "city_domain" => $sDomain ),
+			__METHOD__
+		);
+		return $oRow->count;
+	}
+	
+	/**
+	 * getDomainsLikeOrExact
+	 *
+	 * check if name is similar or the same, using sql like queries
+	 *
+	 * @access public
+	 * @author Krzysztof Krzyżaniak <eloy@wikia.com>
+	 *
+	 * @param string $name: name to check
+	 * @param string $language default null - choosen language
+	 *
+	 * @return array with matches
+	 */
+	function getDomainsLikeOrExact( $name, $language = null ) {
+
+		$dbr = wfGetDB( DB_SLAVE );
+
+		$domains = array();
+		$unique = array();
+
+		$name = self::deleteCommonPostfix($name);
+		
+		$condition = $conditionSimilar = "";
+
+		/**
+		 * don't check short names
+		 */
+		if ( strlen( $name ) > 3 ) {
+			$names = explode(" ", $name);
+			$skip = false;
+			$tmp_array = array();
+
+			if ( is_array( $names ) ) {
+				foreach( $names as $n ) {
+					if ( !preg_match("/^[\w\.]+$/",$n) ) continue;
+					$tmp_array['exact'][] = "city_domain like '{$n}.%'";
+					$tmp_array['similar'][] = "city_domain like '%{$n}%'";
+				}
+				if ( sizeof( $tmp_array ) ) {
+					$condition = implode(" or ", $tmp_array['exact']);
+					$conditionSimilar = implode(" or ", $tmp_array['similar']);
+				} else {
+					$skip = true;
+				}
+			} else {
+				$condition = "city_domain like '{$name}.%'";
+				$conditionSimilar = "city_domain like '%{$name}%'";
+			}
+
+			if ( $skip === false ) {
+				#--- exact (but with language prefixes)
+				list ($city_domains, $city_list) = array(wfSharedTable("city_domains"), wfSharedTable("city_list"));
+				
+				$oRes = $dbr->select (
+					array ( $city_domains, $city_list ),
+					array ( "*" ),
+					array (
+						$condition,
+						"{$city_domains}.city_id = {$city_list}.city_id",
+						"{$city_list}.city_public" => 1
+					),
+					__METHOD__,
+					array( "limit" => 20 )
+				);
+
+				while ( $oRow = $dbr->fetchObject( $oRes ) ) {
+					if ( preg_match( "/^www\./", strtolower( $oRow->city_domain ) ) )
+						continue;
+					$unique[ strtolower( $oRow->city_domain ) ] = 1;
+					$domains["exact"][] = $oRow;
+				}
+				$dbr->freeResult($oRes);
+
+				#-- 
+				# Similar domains
+				$oRes = $dbr->select(
+					array ( $city_domains, $city_list ),
+					array ( "*" ),
+					array (
+						$conditionSimilar,
+						"{$city_domains}.city_id = {$city_list}.city_id",
+						"{$city_list}.city_public" => 1
+					),
+					__METHOD__,
+					array( "limit" => 20 )
+				);
+
+				while ( $oRow = $dbr->fetchObject( $oRes ) ) {
+					if ( preg_match( "/^www\./", strtolower( $oRow->city_domain ) ) )
+						continue;
+					if ( array_key_exists( strtolower($oRow->city_domain), $unique)
+						&& $unique[ strtolower($oRow->city_domain) ] == 1 )
+						continue;
+					$domains["like"][] = $oRow;
+				}
+				$dbr->freeResult($oRes);
+			}
+		}
+		return $domains;
+	}
+	
+	/**
+	 * wfRequestDeleteCommonPostfix
+	 *
+	 * deletes common postfixes like 'pedia' or 'wiki'
+	 *
+	 * @access public
+	 * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+	 *
+	 * @param string $name: name process
+	 *
+	 * @return string processed name
+	 */
+	function deleteCommonPostfix($name) {
+		$commonPostfixes = array('pedia', 'wikia', 'wiki');
+		$regexp = array('/(?:^|(?<!\s))(?:' . implode('|', $commonPostfixes) . ')(?:\s|$)/');
+		return preg_replace($regexp, '', $name);
+	}
+	
+	
+	/**
+	 * wfRequestDeleteCommonPostfix
+	 *
+	 * deletes common postfixes like 'pedia' or 'wiki'
+	 *
+	 * @access public
+	 * @author Maciej Błaszkowski <marooned at wikia-inc.com>
+	 *
+	 * @param string $name: name process
+	 *
+	 * @return string processed name
+	 */
+	public static function checkDomainIsCorrect($sName, $sLang) {
+		wfProfileIn(__METHOD__);
+
+		$sResponse = "";
+		
+		if ( strlen($sName) === 0 ) {
+			#-- empty field
+			$sResponse = wfMsg('autocreatewiki-empty-field');
+		} elseif ( strlen( $sName ) < 3 ) {
+			#-- too short
+			$sResponse = wfMsg('autocreatewiki-name-too-short');
+		} elseif (preg_match('/[^a-z0-9-]/i', $sName)) {
+			#-- invalid name
+			$sResponse = wfMsg('autocreatewiki-bad-name');
+		} elseif ( in_array( $sName, array_keys( Language::getLanguageNames() )) ) {
+			#-- invalid name
+			$sResponse = wfMsg('autocreatewiki-violate-policy');
+		} else {
+			$iExists = AutoCreateWiki::domainExists($sName, $sLang);
+			if (!empty($iExists)) {
+				#--- domain exists
+				$sResponse = wfMsg('autocreatewiki-name-taken');
+			} 
+		}
+		
+		wfProfileOut(__METHOD__);
+		return $sResponse;
+	}
+	
+	public static function checkUsernameIsCorrect($sValue) {
+		wfProfileIn(__METHOD__);
+		$sResponse = "";
+		if ($sValue == "") {
+			$sResponse = wfMsg('autocreatewiki-empty-username');
+		} else {
+			$u = User::newFromName( $sValue );
+			if( is_null( $u ) || !User::isUsableName( $u->getName() ) ) {
+				$sResponse = wfMsg('autocreatewiki-invalid-username');
+			} else {
+				$iId = User::idFromName( $sValue );
+				if ( !empty($iId) ) {
+					$sResponse = wfMsg('autocreatewiki-busy-username');
+				}
+			}
+		}
+		wfProfileOut(__METHOD__);
+		return $sResponse;
+	}
+		
+	public static function checkEmailIsCorrect($sValue) {
+		wfProfileIn(__METHOD__);
+		$sResponse = "";
+		if ($sValue != "") {
+			if ( !User::isValidEmailAddr( $sValue ) ) {
+				$sResponse = wfMsg('autocreatewiki-invalid-email');
+			}
+		}
+		wfProfileOut(__METHOD__);
+		return $sResponse;
+	}
+	
+	public static function checkPasswordIsCorrect($sUsername, $sValue) {
+		global $wgMinimalPasswordLength;
+		
+		wfProfileIn(__METHOD__);
+		$sResponse = "";
+		if ($sUsername == "") {
+			$sResponse = wfMsg('autocreatewiki-set-username');
+		} else {
+			$u = User::newFromName( $sUsername );
+			if ( is_null( $u ) ) {
+				$u = new StubUser();
+			}
+			if ( !$u->isValidPassword( $sValue ) ) {
+				$sResponse = wfMsg('autocreatewiki-invalid-passwd');
+			}
+		}
+		wfProfileOut(__METHOD__);
+		return $sResponse;
+	}
+
+	public static function checkRetypePasswordIsCorrect($sPass, $sValue) {	
+		wfProfileIn(__METHOD__);
+		$sResponse = "";
+		if ( strcmp( $sPass, $sValue ) != 0 ) {
+			$sResponse = wfMsg('autocreatewiki-invalid-retype-passwd');
+		}
+		wfProfileOut(__METHOD__);
+		return $sResponse;
+	}	
+	
+	public static function checkBirthdayIsCorrect ($sYear, $sMonth, $sDay) {
+		wfProfileIn(__METHOD__);
+		$sResponse = "";
+		if ($sYear == -1 || $sMonth == -1 || $sDay == -1) {
+			$sResponse = wfMsg('autocreatewiki-invalid-birthday');
+		} elseif ( empty($sYear) || empty($sMonth) || empty($sDay) ) {
+			$sResponse = wfMsg('autocreatewiki-invalid-birthday');
+		} else {
+			$sYear = sprintf("%0d", $sYear);
+			$sMonth = sprintf("%0d", $sMonth);
+			$sDay = sprintf("%0d", $sDay);
+			if ( preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', "{$sYear}-{$sMonth}-{$sDay}") === false ) {
+				$sResponse = wfMsg('autocreatewiki-invalid-birthday');
+			} else {
+				$userBirthDay = strtotime($sYear . '-' . $sMonth . '-' . $sDay);
+				if ($userBirthDay > strtotime('-13 years')) {
+					$sResponse = wfMsg('autocreatewiki-invalid-birthday');
+				} 
+			}
+		}
+		wfProfileOut(__METHOD__);
+		return $sResponse;
+	}
+}
---
+++ extensions/wikia/AutoCreateWiki/js/autocreatewiki.js
@@ -0,0 +1,265 @@
+YAHOO.namespace("ACWikiRequest");
+var YC = YAHOO.util.Connect;
+var YD = YAHOO.util.Dom;
+var YE = YAHOO.util.Event;
+var WR = YAHOO.WRequest;
+var wgAjaxPath = wgScriptPath + wgScript;
+
+YE.preventDefault('highlightform');
+
+function canAcceptForm() {
+	var cnt = 0;
+	for (i in divErrors) { cnt++; }	
+	if (cnt == 0) {
+		YD.get( "wiki-submit" ).disabled = false;
+	}
+}
+
+YAHOO.util.Event.onAvailable("moving", function() {
+	var aBodyXY = YAHOO.util.Dom.getXY('highlightform');
+	var aDivSel = YAHOO.util.Dom.getElementsByClassName('formblock', 'div');
+	var height, width;
+	var curDiv = null;
+	if (aDivSel) {
+		height = YAHOO.util.Dom.getStyle(aDivSel[0], 'height').replace("px", "");
+		width = YAHOO.util.Dom.getStyle(aDivSel[0], 'width').replace("px", "");
+
+		function findDiv(target) {
+			var cDiv = null;			
+			while (cDiv == null) { 
+				if (target.nodeName.toUpperCase() == 'DIV') {
+					cDiv = target;
+				} else {
+					target = target.parentNode; 
+				}
+			}
+			return cDiv;
+		}
+		
+		function onblurFormElem(event) {
+			curDiv = null;
+		}
+		
+		function onfocusFormElem(event) {
+			if (!curDiv) {
+				var target = YAHOO.util.Event.getTarget(event, true);
+				curDiv = findDiv(target);
+			}
+			
+			var selectedDivs = YAHOO.util.Dom.getElementsByClassName('selected', 'div');
+			if (selectedDivs.length == 0) {
+				YAHOO.util.Dom.setStyle("moving", 'display', 'none');
+				if (curDiv) {
+					YAHOO.util.Dom.addClass(curDiv, 'selected'); 
+				}
+			} else {
+				if (selectedDivs.length > 0) {
+					var prevDiv = selectedDivs[0];
+					if (prevDiv != curDiv) {
+						height = curDiv.offsetHeight;
+						var prevHeight = prevDiv.offsetHeight;
+						width = YAHOO.util.Dom.getStyle(curDiv, 'width').replace("px", "");
+						if (prevDiv) YAHOO.util.Dom.removeClass(prevDiv, 'selected'); 
+						var move = new YAHOO.util.Anim("moving", {
+							top: {
+								from: (prevDiv) ? (YAHOO.util.Dom.getXY(prevDiv)[1] - aBodyXY[1]) : 0, 
+								to: (prevDiv) ? (YAHOO.util.Dom.getXY(curDiv)[1] - aBodyXY[1]) : 0
+							},
+							height: { from: prevHeight, to: height },
+							width: { from: width, to: width }
+						}, 1);
+						move.duration = 0.5;
+						move.onComplete.subscribe(function() {
+							YAHOO.util.Dom.addClass(curDiv, 'selected'); 
+							YAHOO.util.Dom.setStyle("moving", 'display', 'none');
+						}); 
+						YAHOO.util.Dom.setStyle("moving", 'display', 'block');
+						move.animate();
+					}
+				}
+			}
+		}
+			
+		var oF = document.forms['highlightform'];
+		var oElm = oF.getElementsByTagName('INPUT');
+		var els = oElm.length;
+		for(i = 0; i < els; i++) {
+			if (oElm[i].type != 'hidden' && oElm[i].type != 'submit' && oElm[i].type != 'reset') {
+				YAHOO.util.Event.addListener(oElm[i], "focus", onfocusFormElem);
+				YAHOO.util.Event.addListener(oElm[i], "blur", onblurFormElem);
+			}
+		}
+		var oEls = oF.getElementsByTagName('SELECT');
+		var elss = oEls.length;
+		for(i = 0; i < elss; i++) {
+			YAHOO.util.Event.addListener(oEls[i], "focus", onfocusFormElem);
+			YAHOO.util.Event.addListener(oEls[i], "blur", onblurFormElem);
+		}
+
+		YAHOO.util.Dom.setStyle("moving", 'display', 'none');
+		YAHOO.util.Dom.addClass(aDivSel[0], 'selected'); 
+	}
+});
+
+YAHOO.util.Event.onDOMReady(function () {
+	canAcceptForm()
+});
+
+YAHOO.ACWikiRequest.NameCallback = {
+    success: function( oResponse ) {
+    	var res = oResponse.responseText;
+    	if (res) {
+			var divData = YAHOO.Tools.JSONParse(res);
+			var div = YD.get( divData["div-name"] );
+			var error = divData["div-error"];
+			var status = YD.get(divData["div-name"] + "-status");
+			var msg = divData["div-body"];
+			if (error) {
+				YD.get( "wiki-submit" ).disabled = true;
+				status.innerHTML = "";
+				divErrors["'" + divData["div-name"] + "'"] = divData["div-name"];
+			}
+			else {
+				status.innerHTML = "<img src='" + stylepath + "/wikia/img/ok.png' />";
+				delete divErrors["'" + divData["div-name"] + "'"];
+				canAcceptForm();
+			}
+			if ( msg ) {
+				div.innerHTML = msg;
+				YAHOO.util.Dom.setStyle(div, 'display', 'block'); 
+			} else {
+				YAHOO.util.Dom.setStyle(div, 'display', 'none'); 
+			}
+		} else {
+			YAHOO.util.Dom.setStyle(div, 'display', 'none'); 
+		}
+    },
+    failure: function( oResponse ) {
+        YAHOO.log( "simple replace failure " + oResponse.responseText );
+        YAHOO.util.Dom.setStyle(div, 'display', 'none'); 
+    },
+    timeout: 50000
+};
+
+YAHOO.ACWikiRequest.checkDomain = function(e) {
+	var err = YD.get("wiki-domain-error");
+	var name = YD.get("wiki-domain").value;
+    var lang = YD.get("wiki-language").value;
+	
+	YAHOO.util.Dom.setStyle(err, 'display', 'block');
+	err.innerHTML = '<img src="http://images.wikia.com/common/progress_bar.gif" width="70" height="11" alt="Wait..." border="0" />';
+	
+    // to lowercase
+    name = name.toLowerCase();
+    YD.get("wiki-domain").value = name;
+
+    YC.asyncRequest( "GET", wgAjaxPath + "?action=ajax&rs=axACWRequestCheckName&name=" + escape(name) + "&lang=" + escape(lang), YAHOO.ACWikiRequest.NameCallback);
+}
+
+YAHOO.ACWikiRequest.wikiLanguageChange = function(e) {
+	var prefixDiv = YD.get("prefixedAddress");
+	var value = prefixDiv.innerHTML;
+	if (this.value != 'en' && prefixDiv) {
+		value.replace("http://", "");
+		value = "http://" + this.value + ".";
+	} else {
+		value = "http://";
+	}
+	prefixDiv.innerHTML = value;
+	YAHOO.ACWikiRequest.checkDomain(e);	
+}
+
+YAHOO.ACWikiRequest.wikiBirthdayCheck = function(e) {
+	var year = YD.get("wiki-user-year");
+	var month = YD.get("wiki-user-month");
+    var day = YD.get("wiki-user-day");
+	var err = YD.get("wiki-birthday-error");
+
+	if ( (year.value > 0) && (month.value > 0) && (day.value > 0) ) {
+		YAHOO.util.Dom.setStyle(err, 'display', 'block');
+		err.innerHTML = '<img src="http://images.wikia.com/common/progress_bar.gif" width="70" height="11" alt="Wait..." border="0" />';
+		params = "&year=" + escape(year.value) + "&month=" + escape(month.value) + "&day=" + escape(day.value);
+		YC.asyncRequest( "GET", wgAjaxPath + "?action=ajax&rs=axACWRequestCheckBirthday" + params, YAHOO.ACWikiRequest.NameCallback);
+	}
+}
+
+YAHOO.ACWikiRequest.wikiDomainKeyUp = function(e) {
+	var value = this.value;
+		
+	var func = function() { 
+		if (value) { 
+			if (e) {
+				YE.preventDefault(e);
+			}
+			YAHOO.ACWikiRequest.checkDomain(e);
+		};
+	};
+
+	if ( this.zid ) {
+		clearTimeout(this.zid);
+	}
+	this.zid = setTimeout(func,1000);
+}
+
+YAHOO.ACWikiRequest.checkAccount = function(e, fid) {
+	if ( fid ) {
+		var err = YD.get(fid + "-error");
+		var name = YD.get(fid);
+		var lang = YD.get("wiki-language").value;
+		//---
+		YAHOO.util.Dom.setStyle(err, 'display', 'block');
+		err.innerHTML = '<img src="http://images.wikia.com/common/progress_bar.gif" width="70" height="11" alt="Wait..." border="0" />';
+		//---
+		fid = fid.replace("wiki-", "");
+		var params = "";
+		if ( fid == "username" ) {
+			YD.get("wiki-retype-password").value = "";
+			delete divErrors["'wiki-retype-password-error'"];
+			YAHOO.util.Dom.setStyle("wiki-retype-password-error", 'display', 'none');
+			YD.get("wiki-retype-password-error-status").innerHTML = "";
+			//---
+			YD.get("wiki-password").value = "";
+			delete divErrors["'wiki-retype-password'"];
+			YAHOO.util.Dom.setStyle("wiki-password-error", 'display', 'none');
+			YD.get("wiki-password-error-status").innerHTML = "";
+			canAcceptForm();
+		} else if (fid == "retype-password") {
+			params = "&pass=" + escape(YD.get("wiki-password").value);
+		} else if ( fid == "password" ) {
+			params = "&username=" + escape(YD.get("wiki-username").value);
+			YD.get("wiki-retype-password").value = "";
+			delete divErrors["'wiki-retype-password-error'"];
+			YAHOO.util.Dom.setStyle("wiki-retype-password-error", 'display', 'none');
+			YD.get("wiki-retype-password-error-status").innerHTML = "";
+		}
+		var req = wgAjaxPath + "?action=ajax&rs=axACWRequestCheckAccount&name=" + escape(fid) + "&lang=" + escape(lang) + "&value=" + escape(name.value);
+		YC.asyncRequest( "GET", req + params, YAHOO.ACWikiRequest.NameCallback);
+	}
+}
+
+YAHOO.ACWikiRequest.wikiAccountKeyUp = function(e) {
+	var id = this.id;
+	var func = function() { 
+		if (id && this.value) { 
+			if (e) {
+				YE.preventDefault(e);
+			}
+			YAHOO.ACWikiRequest.checkAccount(e, id);
+		};
+	};
+	if ( this.zid ) {
+		clearTimeout(this.zid);
+	}
+	this.zid = setTimeout(func,1000);
+}
+
+YE.addListener("wiki-domain", "keyup", YAHOO.ACWikiRequest.wikiDomainKeyUp );
+YE.addListener("wiki-username", "keyup", YAHOO.ACWikiRequest.wikiAccountKeyUp );
+YE.addListener("wiki-email", "keyup", YAHOO.ACWikiRequest.wikiAccountKeyUp );
+YE.addListener("wiki-password", "keyup", YAHOO.ACWikiRequest.wikiAccountKeyUp );
+YE.addListener("wiki-retype-password", "keyup", YAHOO.ACWikiRequest.wikiAccountKeyUp );
+
+YE.addListener("wiki-language", "change", YAHOO.ACWikiRequest.wikiLanguageChange );
+YE.addListener("wiki-user-year", "change", YAHOO.ACWikiRequest.wikiBirthdayCheck );
+YE.addListener("wiki-user-month", "change", YAHOO.ACWikiRequest.wikiBirthdayCheck );
+YE.addListener("wiki-user-day", "change", YAHOO.ACWikiRequest.wikiBirthdayCheck );
