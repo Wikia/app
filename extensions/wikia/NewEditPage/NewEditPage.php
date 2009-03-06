827ea4ce0f89a10dbe6c723444317e2b16a43f69
macbre <macbre@wikia-inc.com>
2009-03-06 09:49:38 +0000

    NewEditPage: fixes for preview mode and not existing article (RT #11788)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6658 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewEditPage/NewEditPage.php
+++ extensions/wikia/NewEditPage/NewEditPage.php
@@ -2,7 +2,7 @@
 $wgExtensionCredits['other'][] = array(
         'name' => 'New Edit Page',
 	'description' => 'Applies edit page changes',
-        'version' => 0.11,
+        'version' => 0.2,
         'author' => '[http://pl.wikia.com/wiki/User:Macbre Maciej Brencz]'
 );
 
@@ -28,7 +28,10 @@ function wfNewEditPageInit() {
 // add custom CSS to page of not existing articles
 function wfNewEditPageArticleView($title) {
 
+	global $wgNewEditPageNewArticle;
+
 	if (!$title->exists()) {
+		$wgNewEditPageNewArticle = true;
 		wfNewEditPageAddCSS();
 	}
 
@@ -37,7 +40,7 @@ function wfNewEditPageArticleView($title) {
 
 // add CSS to edit pages
 function wfNewEditPageAddCSS() {
-	global $wgWysiwygEdit, $wgOut, $wgUser, $wgExtensionsPath, $wgStyleVersion;
+	global $wgWysiwygEdit, $wgOut, $wgUser, $wgExtensionsPath, $wgStyleVersion, $wgNewEditPageNewArticle;
 
 	// do not touch monobook
 	$skinName = get_class($wgUser->getSkin());
@@ -45,10 +48,16 @@ function wfNewEditPageAddCSS() {
 		return true;
 	}
 
-	if (!empty($wgWysiwygEdit)) {
+	if (!empty($wgNewEditPageNewArticle)) {
+		// new article notice
+		$cssFile = 'NewEditPageNewArticle.css';
+	}
+	else if (!empty($wgWysiwygEdit)) {
+		// edit mode in wysiwyg
 		$cssFile = 'NewEditPageWysiwyg.css';
 	}
 	else {
+		// edit mode in old MW editor
 		$cssFile = 'NewEditPage.css';
 	}
 
@@ -64,7 +73,7 @@ function wfNewEditPageAddCSS() {
 
 // add red preview notice in old editor
 function wfNewEditPageAddPreviewBar($editPage) {
-	global $wgOut, $wgUser;
+	global $wgOut, $wgUser, $wgTitle, $wgHooks;
 
 	// do not touch monobook
 	$skinName = get_class($wgUser->getSkin());
@@ -72,9 +81,27 @@ function wfNewEditPageAddPreviewBar($editPage) {
 		return true;
 	}
 
+	// we're in preview mode
 	if ($editPage->formtype == 'preview') {
 		wfLoadExtensionMessages('NewEditPage');
 		$wgOut->addHTML('<div id="new_edit_page_preview_notice">' . wfMsg('new-edit-page-preview-notice') . '</div>');
+
+		// add page title before preview HTML
+		$wgHooks['OutputPageBeforeHTML'][] = 'wfNewEditPageAddPreviewTitle';
+	}
+
+	return true;
+}
+
+// add page title before preview HTML
+function wfNewEditPageAddPreviewTitle($wgOut, $text) {
+	global $wgTitle;
+	$wgOut->addHTML('<h1 id="new_edit_page_preview_title">' . $wgTitle->getPrefixedText() . '</h1>');
+
+	// find first closing </h2> and remove preview notice
+	$pos = strpos($text, '</h2>');
+	if ($pos !== false) {
+		$text = substr($text, $pos+5);
 	}
 
 	return true;
