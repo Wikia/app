1c23979a81c7d32ff1c546542a9fa405b2551580
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-09 22:23:43 +0000

    new skin for Warhammer on WoW
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6724 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdSkin/AdSkin.php
+++ extensions/wikia/AdSkin/AdSkin.php
@@ -4,7 +4,9 @@
  */
 
 $wgHooks['GetHTMLAfterBody'][] = 'RenderAdSkin';
-		
+$wgHooks['SpecialFooterAfterWikia'][] = 'RenderAdSkinJS';
+$wgHooks['MonacoAdLink'][] = 'RenderAdLink';
+
 function RenderAdSkin() {
 	global $wgAdSkin, $wgExtensionsPath, $wgUser, $wgStyleVersion, $wgWikiaLogo;
 
@@ -35,8 +37,50 @@ function RenderAdSkin() {
 			}';
 			echo '</style>';
 			break;
+		case "warhammer":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/warhammer.css?'. $wgStyleVersion .'" />';
+			echo '<style type="text/css">';
+			echo 'body.mainpage #wikia_header {
+				background: #252525 url('. $wgWikiaLogo .') 10px 2px no-repeat;
+			}';
+			echo '</style>';
+			
+			break;
 		}	
 	}
 
+	return true;
+}
+
+function RenderAdSkinJS() {
+	global $wgUser, $wgExtensionsPath, $wgStyleVersion, $wgAdSkin;
+	
+	if (is_object($wgUser) && $wgUser->isLoggedIn() ){
+		return true;
+	}
+
+	if (isset($wgAdSkin)) {
+		switch ($wgAdSkin) {
+		case "warhammer":
+			echo '<script type="text/javascript" src="'. $wgExtensionsPath .'/wikia/AdSkin/js/warhammer.js?'. $wgStyleVersion .'"></script>';
+			echo '<A HREF="http://ad.doubleclick.net/jump/N2790.Wikia/B3436947.10;sz=1x1;ord='. time() .'?"><IMG SRC="http://ad.doubleclick.net/ad/N2790.Wikia/B3436947.10;sz=1x1;ord='. time() .'?" BORDER=0 WIDTH=1 HEIGHT=1 ALT="Click Here"></A>'; 
+			break;
+		}
+	}
+	
+	return true;
+}
+
+function RenderAdLink() {
+	global $wgAdSkin, $wgStyleVersion, $wgExtensionsPath;
+
+	if (isset($wgAdSkin)) {
+		switch ($wgAdSkin) {
+		case "warhammer_link":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/warhammer_link.css?'. $wgStyleVersion .'" />';
+			echo '<a href="#" id="warhammer_link">Download the Free 10-Day Trial</a>';
+			break;
+		}
+	}
 	return true;
 }
