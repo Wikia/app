2184d84bc429d672550aaadcfc450b47f429e2ca
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-09 10:05:48 +0000

    VideoEmbedTool: adding alts and titles with accompanying messages fixes RT #11769
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6695 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoEmbedTool/templates/details.tmpl.php
+++ extensions/wikia/VideoEmbedTool/templates/details.tmpl.php
@@ -45,11 +45,11 @@ if('' == $props['oname']) {
 		<th><?= wfMsg('vet-layout') ?></th>
 		<td>
 			<input type="radio" id="VideoEmbedLayoutLeft" name="layout" onclick="VET_toggleSizing( true );" />
-			<label for="VideoEmbedLayoutLeft"><img src="<?= $wgExtensionsPath.'/wikia/VideoEmbedTool/images/image_upload_left.png' ?>" /></label>
+			<label for="VideoEmbedLayoutLeft"><img src="<?= $wgExtensionsPath.'/wikia/VideoEmbedTool/images/image_upload_left.png' ?>" alt="<?= wfMsg( 'vet-left' ) ?>" title="<?= wfMsg( 'vet-left' ) ?>" /></label>
 			<input type="radio" id="VideoEmbedLayoutRight" name="layout" checked="checked" onclick="VET_toggleSizing( true );" />
-			<label for="VideoEmbedLayoutRight"><img src="<?= $wgExtensionsPath.'/wikia/VideoEmbedTool/images/image_upload_right.png' ?>" /></label>
+			<label for="VideoEmbedLayoutRight"><img src="<?= $wgExtensionsPath.'/wikia/VideoEmbedTool/images/image_upload_right.png' ?>" alt="<?=wfMsg( 'vet-right' ) ?>" title="<?= wfMsg( 'vet-right' ) ?>" /></label>
 			<input type="radio" id="VideoEmbedLayoutGallery" name="layout" onclick="VET_toggleSizing( false );" />
-			<label for="VideoEmbedLayoutGallery"><img src="<?= $wgExtensionsPath.'/wikia/VideoEmbedTool/images/image_upload_gallery.png' ?>" /></label>
+			<label for="VideoEmbedLayoutGallery"><img src="<?= $wgExtensionsPath.'/wikia/VideoEmbedTool/images/image_upload_gallery.png' ?>" alt="<?= wfMsg( 'vet-gallery' ) ?>" title="<?= wfMsg( 'vet-gallery' ) ?>" /></label>
 		</td>
 	</tr>
 	<tr>
