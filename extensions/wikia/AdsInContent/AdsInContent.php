0895b07c6f2e1ce77a80c145607812c5b8cb721a
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-11 14:48:22 +0000

    AdsInContent: margin for floating ads + general cealnup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6776 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdsInContent/AdsInContent.php
+++ extensions/wikia/AdsInContent/AdsInContent.php
@@ -14,7 +14,7 @@ if(!defined('MEDIAWIKI')) {
 
 $wgExtensionCredits['other'][] = array(
 	'name' => 'AdsInContent',
-	'author' => '[http://www.wikia.com/wiki/User:Macbre Maciej Brencz], [http://www.wikia.com/wiki/User:Adi3ek Adrian \'ADi\' Wieczorek]',
+	'author' => array('[http://www.wikia.com/wiki/User:Macbre Maciej Brencz]', '[http://www.wikia.com/wiki/User:Adi3ek Adrian \'ADi\' Wieczorek]'),
 	'description' => 'Displays ad boxes inside article content',
 	'version' => 1.1
 );
@@ -24,23 +24,41 @@ $wgAdsInContentExtensionConfig = array(
 	'bottomAdUnit' => false,
 	'insideAdUnit' => array( /* secttions range => array of section numbers */
 		'3-7' => array(2,5),
-		'8-*' => array(2,5,8)
+		'8-*' => array(2,5,8),
 //		'9-*' => array(3,6,8)
 	),
- 'insideAdUnitConfig' => array(
- 	'google'     => array(
-																			0 => array( 'width' => 468, 'height' => 60, 'align' => 'left', 'googleAdChannel' => '9100000015'), // 011
-																			1 => array( 'width' => 200, 'height' => 200, 'align' => 'left', 'googleAdChannel' => '9100000016') // 012
+	'insideAdUnitConfig' => array(
+ 		'google'     => array(
+			0 => array(
+				'width' => 468,
+				'height' => 60,
+				'align' => 'left',
+				'float' => false,
+				'googleAdChannel' => '9100000015'), // 011
+			1 => array(
+				'width' => 200,
+				'height' => 200,
+				'align' => 'left',
+				'float' => true,
+				'googleAdChannel' => '9100000016') // 012
  																	),
-/* 	'yieldbuild' => array(
-																			0 => array( 'width' => 468, 'height' => 200, 'align' => 'left', 'yieldbuildLocation' => 'left_first_section'),
-																			1 => array( 'width' => 468, 'height' => 200, 'align' => 'left', 'yieldbuildLocation' => 'right_first_section')
+/* 		'yieldbuild' => array(
+			0 => array(
+				'width' => 468,
+				'height' => 200,
+				'align' => 'left',
+				'yieldbuildLocation' => 'left_first_section'),
+			1 => array(
+				'width' => 468,
+				'height' => 200,
+				'align' => 'left',
+				'yieldbuildLocation' => 'right_first_section')
  	                ) */
- ),
- 'limit' => array(
- 	'google' => 4,
-/* 	'yieldbuild' => 0 */
- )
+	),
+	'limit' => array(
+		'google' => 4,
+//		'yieldbuild' => 0
+	)
 );
 
 $wgExtensionFunctions[] = 'wfAdsInContentSetup';
@@ -76,4 +94,3 @@ function wfAdsInContentHook(&$out, &$text) {
 	}
 	return true;
 }
-
