7486e953e5512739260d9c4423a6529a77dfbf1e
pean <pean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-16 18:27:09 +0000

    add params for language and db
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6910 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SuperDeduper/index.php
+++ extensions/wikia/SuperDeduper/index.php
@@ -1,8 +1,8 @@
 <?php
 /* Simple JSON interface to the super deduper for type-ahead completion */
 header('Content-Type: text/plain');
-require '/usr/wikia/source//answers/SuperDeduper.php';
-$SuperDeduper = new SuperDeduper();
+require '/usr/wikia/source/answers/SuperDeduper.php';
+$SuperDeduper = new SuperDeduper( $_GET['lang'], $_GET['db'] );
 if (empty($_GET['limit'])){
 	$_GET['limit'] = 10;
 }
