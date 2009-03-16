41bc9cdebdac73ff147abf2967b423463aa81cf5
christian <christian@wikia-inc.com>
2009-03-16 22:42:42 +0000

    superpages skin and fixes to warhammer
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6920 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdSkin/AdSkin.php
+++ extensions/wikia/AdSkin/AdSkin.php
@@ -3,12 +3,14 @@
  * Author: Christian Williams 
  */
 
+$wgAdSkinVersion = 1;
+
 $wgHooks['GetHTMLAfterBody'][] = 'RenderAdSkin';
 $wgHooks['SpecialFooterAfterWikia'][] = 'RenderAdSkinJS';
 $wgHooks['MonacoAdLink'][] = 'RenderAdLink';
 
 function RenderAdSkin() {
-	global $wgAdSkin, $wgExtensionsPath, $wgUser, $wgStyleVersion, $wgWikiaLogo;
+	global $wgAdSkin, $wgExtensionsPath, $wgUser, $wgWikiaLogo, $wgAdSkinVersion;
 
 	// Disable for logged in users
 	if (is_object($wgUser) && $wgUser->isLoggedIn() ){
@@ -18,19 +20,19 @@ function RenderAdSkin() {
 	if (isset($wgAdSkin)) {
 		switch ($wgAdSkin) {
 		case "wow_lich_king":
-			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king.css?'. $wgStyleVersion .'" />';
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king.css?'. $wgAdSkinVersion .'" />';
 			break;
 		case "wow_lich_king_warhammer":
-			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king_warhammer.css?'. $wgStyleVersion .'" />';
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/wow_lich_king_warhammer.css?'. $wgAdSkinVersion .'" />';
 			break;
 		case "dragonball_origins":
-			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/dragonball_origins.css?'. $wgStyleVersion .'" />';
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/dragonball_origins.css?'. $wgAdSkinVersion .'" />';
 			break;
 		case "dnd":
-			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/dnd.css?'. $wgStyleVersion .'" />';
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/dnd.css?'. $wgAdSkinVersion .'" />';
 			break;
 		case "underworld":
-			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/underworld.css?'. $wgStyleVersion .'" />';
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/underworld.css?'. $wgAdSkinVersion .'" />';
 			echo '<style type="text/css">';
 			echo 'body.mainpage #wikia_header {
 				background: url('. $wgWikiaLogo .') 10px 2px no-repeat;
@@ -38,13 +40,15 @@ function RenderAdSkin() {
 			echo '</style>';
 			break;
 		case "warhammer":
-			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/warhammer.css?'. $wgStyleVersion .'" />';
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/warhammer.css?'. $wgAdSkinVersion .'" />';
 			echo '<style type="text/css">';
 			echo 'body.mainpage #wikia_header {
 				background: #252525 url('. $wgWikiaLogo .') 10px 2px no-repeat;
 			}';
 			echo '</style>';
-			
+			break;
+		case "superpages":
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/superpages.css?'. $wgAdSkinVersion .'" />';
 			break;
 		}	
 	}
@@ -53,7 +57,7 @@ function RenderAdSkin() {
 }
 
 function RenderAdSkinJS() {
-	global $wgUser, $wgExtensionsPath, $wgStyleVersion, $wgAdSkin;
+	global $wgUser, $wgExtensionsPath, $wgAdSkin, $wgAdSkinVersion;
 	
 	if (is_object($wgUser) && $wgUser->isLoggedIn() ){
 		return true;
@@ -62,7 +66,7 @@ function RenderAdSkinJS() {
 	if (isset($wgAdSkin)) {
 		switch ($wgAdSkin) {
 		case "warhammer":
-			echo '<script type="text/javascript" src="'. $wgExtensionsPath .'/wikia/AdSkin/js/warhammer.js?'. $wgStyleVersion .'"></script>';
+			echo '<script type="text/javascript" src="'. $wgExtensionsPath .'/wikia/AdSkin/js/warhammer.js?'. $wgAdSkinVersion .'"></script>';
 			echo '<A HREF="http://ad.doubleclick.net/jump/N2790.Wikia/B3436947.10;sz=1x1;ord='. time() .'?"><IMG SRC="http://ad.doubleclick.net/ad/N2790.Wikia/B3436947.10;sz=1x1;ord='. time() .'?" BORDER=0 WIDTH=1 HEIGHT=1 ALT="Click Here"></A>'; 
 			break;
 		}
@@ -72,12 +76,12 @@ function RenderAdSkinJS() {
 }
 
 function RenderAdLink() {
-	global $wgAdSkin, $wgStyleVersion, $wgExtensionsPath;
+	global $wgAdSkin, $wgAdSkinVersion, $wgExtensionsPath;
 
 	if (isset($wgAdSkin)) {
 		switch ($wgAdSkin) {
 		case "warhammer_link":
-			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/warhammer_link.css?'. $wgStyleVersion .'" />';
+			echo '<link rel="stylesheet" type="text/css" href="'. $wgExtensionsPath .'/wikia/AdSkin/css/warhammer_link.css?'. $wgAdSkinVersion .'" />';
 			echo '<a href="http://www.warhammeronline.com/trial?WHK33J-U7C8Q-HWPEJ-9QAIJ-4Y8JK" id="warhammer_link">Download the Free 10-Day Trial</a>';
 			break;
 		}
