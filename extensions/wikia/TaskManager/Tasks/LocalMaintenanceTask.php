a656ba98f935c01bbf801d122f5656086fbe4437
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-17 11:54:46 +0000

    generic task for local maintenance
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6943 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/TaskManager/Tasks/LocalMaintenanceTask.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * @file
+ * @package MediaWiki
+ * @ingroup BatchTask
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
+ * @copyright Copyright © Krzysztof Krzyżaniak for Wikia Inc.
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
+ * @date 2009-03-17
+ * @version 1.0
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+/**
+ * Generic Task, will run maintenance task for specified city_id
+ */
+class LocalMaintenanceTask extends BatchTask {
+
+	private $mParams;
+
+	/**
+	 * contructor
+	 * @access public
+	 */
+	public function  __construct() {
+
+		parent::__construct();
+		$this->mType = "local-maintenance";
+		$this->mVisible = false;
+		$this->mTTL = 1800;
+		$this->mDebug = false;
+	}
+
+	/**
+	 * execute
+	 *
+	 * entry point for TaskExecutor
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @param mixed $params default null - task data from wikia_tasks table
+	 *
+	 * @return boolean - status of operation
+	 */
+	public function execute( $params = null ) {
+		global $IP, $wgWikiaLocalSettingsPath, $wgWikiaAdminSettingsPath;
+
+		$this->mTaskID = $params->task_id;
+		$this->mParams = unserialize( $params->task_arguments );
+
+		$city_id = $this->mParams[ "city_id" ];
+		$command = $this->mParams[ "command" ];
+		$arguments = $this->mParams[ "arguments" ];
+
+		if( $city_id && $command ) {
+			/**
+			 * execute maintenance script
+			 */
+			$cmd = sprintf( "SERVER_ID={$city_id} php {$IP}/{$command} {$arguments} --conf {$wgWikiaLocalSettingsPath} --aconf {$wgWikiaAdminSettingsPath}" );
+			$this->addLog( "Running {$cmd}" );
+			$retval = wfShellExec( $cmd, $status );
+			$this->addLog( $retval );
+		}
+
+		return true;
+	}
+
+	/**
+	 * getForm
+	 *
+	 * this task is not visible in selector so it doesn't have real HTML form
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @param Title $title: Title struct
+	 * @param mixes $data: params from HTML form
+	 *
+	 * @return false
+	 */
+	public function getForm( $title, $data = false ) {
+		return false;
+	}
+
+	/**
+	 * getType
+	 *
+	 * return string with codename type of task
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @return string: unique name
+	 */
+	public function getType() {
+		return $this->mType;
+	}
+
+	/**
+	 * isVisible
+	 *
+	 * check if class is visible in TaskManager from dropdown
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @return boolean: visible or not
+	 */
+	public function isVisible() {
+		return $this->mVisible;
+	}
+
+	/**
+	 * submitForm
+	 *
+	 * since this task is invisible for form selector we use this method for
+	 * saving request data in database
+	 *
+	 * @access public
+	 * @author eloy@wikia
+	 *
+	 * @return true
+	 */
+	public function submitForm() {
+		return true;
+	}
+}
