1d52026d0b8b0bcc27f3a566cdc2dc9ac0218d3f
eloy <eloy@wikia-inc.com>
2009-03-17 10:53:09 +0000

    rt#12106
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@6933 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/HAWelcome/HAWelcome.i18n.php
+++ extensions/wikia/HAWelcome/HAWelcome.i18n.php
@@ -20,10 +20,10 @@ $messages['en'] = array(
 * Favorite page #2
 * Favorite page #3
 ",
-	"welcome-message-user" => "Hi, welcome to {{SITENAME}}! Thanks for your edit to the [[$1]] page.
+	"welcome-message-user" => "Hi, welcome to {{SITENAME}}! Thanks for your edit to the [[:$1]] page.
 
 Please leave a message on [[$2|my talk page]] if I can help with anything! $3",
-	"welcome-message-anon" => "Hi, welcome to {{SITENAME}}! Thanks for your edit to the [[$1]] page.
+	"welcome-message-anon" => "Hi, welcome to {{SITENAME}}! Thanks for your edit to the [[:$1]] page.
 
 '''[[Special:Userlogin|Please sign in and create a user name]]'''. It's an easy way to keep track of your contributions and helps you communicate with the rest of the community.
 
