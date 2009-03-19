002ff7b9f4de8de6c9a1c77910c9376a27df31e3
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-19 10:10:18 +0000

    Wysiwyg: update FCK to 2.6.4 in trunk
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7021 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Wysiwyg/fckeditor/editor/_source/classes/fckenterkey.js
+++ extensions/wikia/Wysiwyg/fckeditor/editor/_source/classes/fckenterkey.js
@@ -1,6 +1,6 @@
 /*
  * FCKeditor - The text editor for Internet - http://www.fckeditor.net
- * Copyright (C) 2003-2008 Frederico Caldeira Knabben
+ * Copyright (C) 2003-2009 Frederico Caldeira Knabben
  *
  * == BEGIN LICENSE ==
  *
@@ -541,7 +541,27 @@ FCKEnterKey.prototype._ExecuteEnterBlock = function( blockTag, range )
 		}
 
 		if ( FCKBrowserInfo.IsGeckoLike )
-			FCKDomTools.ScrollIntoView( eNextBlock || eNewBlock, false ) ;
+		{
+			if ( eNextBlock )
+			{
+				// If we have split the block, adds a temporary span at the
+				// range position and scroll relatively to it.
+				var tmpNode = this.Window.document.createElement( 'span' ) ;
+
+				// We need some content for Safari.
+				tmpNode.innerHTML = '&nbsp;';
+
+				oRange.InsertNode( tmpNode ) ;
+				FCKDomTools.ScrollIntoView( tmpNode, false ) ;
+				oRange.DeleteContents() ;
+			}
+			else
+			{
+				// We may use the above scroll logic for the new block case
+				// too, but it gives some weird result with Opera.
+				FCKDomTools.ScrollIntoView( eNextBlock || eNewBlock, false ) ;
+			}
+		}
 
 		oRange.Select() ;
 	}
