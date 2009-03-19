5f359e521166579e45191b2d8ae6da72f71806d9
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-19 15:28:17 +0000

    RandomWiki: added cache
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7046 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RandomWiki/SpecialRandomWiki_body.php
+++ extensions/wikia/RandomWiki/SpecialRandomWiki_body.php
@@ -18,7 +18,8 @@ if( !defined( 'MEDIAWIKI' ) ) {
 
 class RandomWiki extends SpecialPage {
 
-	var $limit = 500;
+	const limit = 500;
+	const cachekey = 'wikicities:RandomWiki:list';
 
 	/**
 	 * Constructor
@@ -33,36 +34,43 @@ class RandomWiki extends SpecialPage {
 	 * @param $par String: page name on target wiki
 	 */
 	public function execute( $par ) {
-		global $wgOut, $wgSharedDB;
+		global $wgOut, $wgSharedDB, $wgMemc;
 
 		// Quit early if we don't have access to the central wiki DB
 		if ( empty( $wgSharedDB ) )
 			return;
 
-		$options = array(
-			'GROUP BY' => 'cw_city_id',
-			'ORDER BY' => 'sum(cw_article_count_link) DESC',
-			'LIMIT' => $this->limit,
-		);
+		$topWikis = $wgMemc->get( self::cachekey );
+
+		if ( empty( $topWikis ) ) {
+
+			$options = array(
+				'GROUP BY' => 'cw_city_id',
+				'ORDER BY' => 'sum(cw_article_count_link) DESC',
+				'LIMIT' => self::limit,
+			);
 
 
-		$dbs = wfGetDBExt( DB_SLAVE );
-		$dbs->selectDB( 'dbstats' );
+			$dbs = wfGetDBExt( DB_SLAVE );
+			$dbs->selectDB( 'dbstats' );
 
-		$res = $dbs->select( 'city_stats_full', array( 'cw_city_id' ), '', __METHOD__, $options );
+			$res = $dbs->select( 'city_stats_full', array( 'cw_city_id' ), '', __METHOD__, $options );
 
-		$totalWikis = $dbs->numRows( $res );
-		$randomNum = mt_rand( 0, $totalWikis - 1 );
-		$dbs->dataSeek( $res, $randomNum );
+			while ( $row = $dbs->fetchObject( $res ) )
+				$topWikis[] = $row->cw_city_id;
+			
+			$wgMemc->set( self::cachekey, $topWikis, 2592000 /* 30 days */ );
+		}
 
-		$targetWiki = $dbs->fetchObject( $res );
+		$rand = array_rand( $topWikis );
+		$targetWiki = $topWikis[$rand];
 
-		$wgServerRemote = WikiFactory::getVarByName( 'wgServer', $targetWiki->cw_city_id );
+		$wgServerRemote = WikiFactory::getVarByName( 'wgServer', $targetWiki );
 		$url = unserialize( $wgServerRemote->cv_value );
 
 		// When a param is given, add it to the URL as a wiki page
 		if ( !empty( $par ) ) {
-			$wgArticlePathRemote = WikiFactory::getVarByName( 'wgArticlePath', $targetWiki->cw_city_id );
+			$wgArticlePathRemote = WikiFactory::getVarByName( 'wgArticlePath', $targetWiki );
 			$url .= str_replace( '$1', $par, unserialize( $wgArticlePathRemote->cv_value ) );
 		}
 
