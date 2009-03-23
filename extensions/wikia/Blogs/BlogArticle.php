c830aa127b70f0765f552e9c55fcf00649d9826d
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-03-23 09:24:48 +0000

    display section of # blog pages in category (rt#10728)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7123 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/BlogArticle.php
+++ extensions/wikia/Blogs/BlogArticle.php
@@ -408,18 +408,38 @@ class BlogArticle extends Article {
 
 		$dbcnt = $cat->getPageCount() - $cat->getSubcatCount() - $cat->getFileCount();
 		$rescnt = count( $catView->blogs );
-		#	$countmsg = $catView->getCountMessage( $rescnt, $dbcnt, 'article' );
+		$countmsg = self::getCountMessage( $catView, $rescnt, $dbcnt, 'article' );
 
 		if( $rescnt > 0 ) {
 			$r = "<div id=\"mw-pages\">\n";
 			$r .= '<h2>' . wfMsg( "blog-header", $ti ) . "</h2>\n";
-			#	$r .= $countmsg;
+			$r .= $countmsg;
 			$r .= $catView->formatList( $catView->blogs, $catView->blogs_start_char );
 			$r .= "\n</div>";
 		}
 		$output = $r;
 
 		return true;
+	}
+
+	/*
+	 * static method to get number of pages in caetgory
+	 */ 
+	static public function getCountMessage( &$catView, $rescnt, $dbcnt, $type ) {
+		global $wgLang;
+		# See CategoryPage->getCountMessage() function
+		$totalrescnt = count( $catView->blogs ) + count( $catView->children ) + ($catView->showGallery ? $catView->gallery->count() : 0);
+		if ($dbcnt == $rescnt || (($totalrescnt == $this->limit || $this->from || $this->until) && $dbcnt > $rescnt)) {
+			# Case 1: seems sane.
+			$totalcnt = $dbcnt;
+		} elseif ( $totalrescnt < $catView->limit && !$catView->from && !$catView->until ) {
+			# Case 2: not sane, but salvageable.
+			$totalcnt = $rescnt;
+		} else {
+			# Case 3: hopeless.  Don't give a total count at all.
+			return wfMsgExt("blog-subheader-all", 'parse', $wgLang->formatNum( $rescnt ) );
+		}
+		return wfMsgExt( "blog-subheader-all", 'parse', $wgLang->formatNum( $rescnt ), $wgLang->formatNum( $totalcnt ) );
 	}
 
 	/**
