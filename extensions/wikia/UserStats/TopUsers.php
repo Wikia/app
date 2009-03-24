44e5a4d9be60c3399c91e0eb3f42244e6821e8c2
moli <moli@wikia-inc.com>
2009-03-24 14:36:51 +0000

    check user is blocked rt#9698
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7166 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserStats/TopUsers.php
+++ extensions/wikia/UserStats/TopUsers.php
@@ -28,12 +28,13 @@ function wfSpecialTopUsers(){
 			
 			$wgOut->setPagetitle( wfMsg( "user_stats_alltime_title") );
 			
-			$count = 50;
+			$count = 100;
+			$realcount = 50;
 			
 			$user_list = array();
 			
 			//try cache
-			$key = wfMemcKey( 'user_stats', 'top', 'points', $count );
+			$key = wfMemcKey( 'user_stats', 'top', 'points', $realcount );
 			$data = $wgMemc->get( $key );
 			if( $data != ""){
 				wfDebug("Got top users by points ({$count}) from cache\n");
@@ -50,19 +51,25 @@ function wfSpecialTopUsers(){
 					array('stats_user_id <> 0'), __METHOD__, 
 					$params
 				);
+				$loop = 0;
 				while( $row = $dbr->fetchObject($res) ){
-					$user_list[] = array(  
+					$oUser = User::newFromId($row->stats_user_id);
+					if (! $oUser->isBlocked() ) {
+						$user_list[] = array(  
 							"user_id" => $row->stats_user_id,
 							"user_name" => $row->stats_user_name,
 							"points" => $row->stats_total_points
-							);
+						);
+						$loop++;		
+					}
+					if ($loop >= 50) break;
 				}
 				$wgMemc->set( $key, $user_list, 60 * 5);
 			}
 			
 			$recent_title = Title::makeTitle( NS_SPECIAL  , "TopUsersRecent"  );
 			
-			$out .= "<div class=\"top-fan-nav\">
+			$out = "<div class=\"top-fan-nav\">
 				<h1>" . wfMsg("top_fans_by_points_nav_header") . "</h1>
 				<p><b>" . wfMsg("top_fans_total_points_link") . "</b></p>";
 				
@@ -74,14 +81,14 @@ function wfSpecialTopUsers(){
 			}
 				
 			//Build Nav of Stats by Category based on Mediawiki:topfans_by_category
+			$by_category_title = Title::makeTitle( NS_SPECIAL, "TopFansByStatistic");
 			
+			$lines = explode( "\n", wfMsgForContent( 'topfans_by_category' ) );
+
 			if (count($lines)>0) {
 				$out .= "<h1 style=\"margin-top:15px !important;\">" . wfMsg("top_fans_by_category_nav_header") . "</h1>";
 			}
 			
-			$by_category_title = Title::makeTitle( NS_SPECIAL, "TopFansByStatistic");
-			
-			$lines = explode( "\n", wfMsgForContent( 'topfans_by_category' ) );
 			foreach ($lines as $line) {
 				
 				if (strpos($line, '*') !== 0){
