befd4ce4f0868bf08d82d34bd0e4dcb0b9a35cb3
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-04-04 14:26:20 +0000

    forcetrace=1
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7640 e846bd91-c042-42a7-a218-4b65e2c1904d

--- StartProfiler.php
+++ StartProfiler.php
@@ -4,6 +4,9 @@ if( !empty( $_GET['forceprofile'] ) ) {
 	require_once( dirname(__FILE__).'/includes/ProfilerSimpleText.php' );
 	$wgProfiler = new ProfilerSimpleText;
 	$wgProfiler->setProfileID( 'forced' );
+} elseif( !empty( $_GET['forcetrace'] ) ) {
+	require_once( dirname(__FILE__).'/includes/ProfilerSimpleTrace.php' );
+	$wgProfiler = new ProfilerSimpleTrace;
 } else {
 	require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
 }
