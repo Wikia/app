724d2f4a072d5ff69509f14ba77bfa2e4c03b36a
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-04-06 13:47:24 +0000

    Multi RC not used.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7668 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -85,7 +85,6 @@ $wgAutoloadClasses["ApiFeaturedContent"] = "extensions/wikia/FeaturedContent/Api
 $wgAutoloadClasses["ApiPartnerWikiConfig"] = "extensions/wikia/FeaturedContent/ApiPartnerWikiConfig.php";
 $wgAutoloadClasses["WikiaApiAjaxLogin"] = "extensions/wikia/WikiaApi/WikiaApiAjaxLogin.php";
 $wgAutoloadClasses["ApiImageThumb"] = $GLOBALS["IP"]."/extensions/wikia/Our404Handler/ApiImageThumb.php";
-//$wgAutoloadClasses["ApiRecentChangesCombined"] = "extensions/wikia/RecentChangesCombined/ApiRecentChangesCombined.php";
 $wgAutoloadClasses["WikiaApiQuerySiteInfo"] = "extensions/wikia/WikiaApi/WikiaApiQuerySiteinfo.php";
 
 
@@ -127,7 +126,6 @@ global $wgAPIModules;
 $wgAPIModules["insert"] = "WikiaApiQueryWrite";
 $wgAPIModules["update"] = "WikiaApiQueryWrite";
 $wgAPIModules["delete"] = "WikiaApiQueryWrite";
-//$wgAPIModules["recentchangescombined"] = "ApiRecentChangesCombined";
 $wgAPIModules["featuredcontent"] = "ApiFeaturedContent";
 $wgAPIModules["partnerwikiconfig"] = "ApiPartnerWikiConfig";
 $wgAPIModules["ajaxlogin"] = "WikiaApiAjaxLogin";
