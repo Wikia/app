c48c8c31711ef86438d39585332d2bd2cdf060ad
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-04-07 14:19:43 +0000

    clear PHP notices
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7703 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewUsersList/NewUsersList.php
+++ extensions/wikia/NewUsersList/NewUsersList.php
@@ -6,56 +6,50 @@ function wfGetNewUsers() {
     $wgParser->setHook( "newusers", "GetNewUsers" );
 }
 
-function GetNewUsers( $input, $args, &$parser ){
+function GetNewUsers( $input, $args, &$parser ) {
 	global $wgUser, $wgParser, $wgTitle, $wgOut, $wgUploadDirectory, $wgMemc;
-
 	$parser->disableCache();
-
-	$count = $args["count"];
-	$per_row = $args["row"];
-	if(!is_numeric($count) ) $count = 10;
-	if(!is_numeric($per_row) ) $per_row = 5;
-	
+	$count = (isset($args["count"]) && is_numeric($args["count"])) ? intval($args["count"]) : 10;
+	$per_row = (isset($args["row"]) && is_numeric($args["row"])) ? intval($args["row"]) : 5;
 	//try cache
 	$key = wfMemcKey( 'users', 'new', $count );
 	$data = $wgMemc->get( $key );
 		
-	if(!$data){
+	if ( !$data ) {
 		$dbr =& wfGetDB( DB_SLAVE );
-		
-		$params['ORDER BY'] = 'ur_date';
-		$params['LIMIT'] = $count;
-		
-		$res = $dbr->select( '`user_register_track`', array('ur_user_id','ur_user_name'), 
-			"", __METHOD__, 
-			$params
+		$res = $dbr->select( 
+			"`user_register_track`", 
+			array('ur_user_id','ur_user_name'), 
+			null, 
+			__METHOD__, 
+			array('ORDER BY' => 'ur_date', 'LIMIT' => $count)
 		);
-		
 		$list = array();
 		while ($row = $dbr->fetchObject( $res ) ) {
-			 $list[] = array(
-				 "user_id"=>$row->ur_user_id,"user_name"=>$row->ur_user_name);
+			$list[] = array(
+				"user_id" => $row->ur_user_id,
+				"user_name" => $row->ur_user_name
+			);
 		}
 		$wgMemc->set( $key, $list, 60 * 10 );
-	}else{
+	} else {
 		wfDebug( "Got new users from cache\n" );
 		$list = $data;
 	}
 	
-	$output .= "<div class=\"new-users\">";
+	$output = "<div class=\"new-users\">";
 
 	$x=1;
 	foreach ($list as $user) {
-		
-		$avatar = new wAvatar( $user["user_id"] ,"ml");
+		$avatar = new wAvatar( $user["user_id"] ,"ml" );
 		$user_link = Title::makeTitle(NS_USER, $user["user_name"] );
 		
 		$output .= "<a href=\"".$user_link->escapeFullURL()."\" rel=\"nofollow\">{$avatar->getAvatarURL()}</a>";
 		
-		if($x==$count || $x!=1 && $x%$per_row ==0)$output .= "<div class=\"cleared\"></div>";
+		if ( ($x == $count) || ($x != 1) && ($x % $per_row == 0) ) {
+			$output .= "<div class=\"cleared\"></div>";
+		}
 		$x++;
-		
-		 
 	}
 	
 	$output .= "<div class=\"cleared\"></div>
@@ -63,5 +57,3 @@ function GetNewUsers( $input, $args, &$parser ){
 	
 	return $output;
 }
-
-?>
