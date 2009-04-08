ba9dfb4ee95e30dfd03564a2a19b59274b5087e9
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-04-08 13:44:50 +0000

    WysiwygInterface: add credits
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7743 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WysiwygInterface/WysiwygInterface.php
+++ extensions/wikia/WysiwygInterface/WysiwygInterface.php
@@ -3,6 +3,13 @@ if(!defined('MEDIAWIKI')) {
 	exit(1);
 }
 
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'WysiwygInterface',
+	'description' => 'Wysiwyg modified parser development tool',
+	'version' => '1.0',
+	'author' => array('Inez Korczyński', 'Maciej Brencz', 'Maciej Błaszkowski (Marooned)'),
+);
+
 $dir = dirname(__FILE__) . '/';
 
 $wgAutoloadClasses['WysiwygInterface'] = $dir . 'WysiwygInterface_body.php';
