fc0a35cce333dee1aab80df22c2e0f92510980dd
macbre <macbre@wikia-inc.com>
2009-04-09 09:28:07 +0000

    GoogleMaps: properly encode messages in JS
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7754 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/GoogleMaps/GoogleMaps.body.php
+++ extensions/3rdparty/GoogleMaps/GoogleMaps.body.php
@@ -849,7 +849,7 @@ JAVASCRIPT;
 	function getMessageJS ( ) {
 		$translation = "var _ = { ";
 		foreach( array_keys( $this->mMessages["en"] ) as $key ) {
-			$translation .= "'$key':'" . addslashes( $this->translateMessage( $key ) ) . "', ";
+			$translation .= "'$key':'" . Xml::encodeJsVar( $this->translateMessage( $key ) ) . "', ";
 		}
 		$translation = preg_replace( "/, $/", '', $translation );
 		$translation .= " };";
