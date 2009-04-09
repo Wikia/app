b42c8ae75c688c3ed3193f12d8415cdd3bcca8e4
macbre <macbre@wikia-inc.com>
2009-04-09 09:48:08 +0000

    StaffSig: add credits
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7756 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaffSig/StaffSig.php
+++ extensions/wikia/StaffSig/StaffSig.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Adds <staff /> parser hook rendering as staff signature
+ *
+ * @author Emil Podlaszewski
+ */
+
 //Avoid unstubbing $wgParser on setHook() too early on modern (1.12+) MW versions, as per r35980
 if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
 	$wgHooks['ParserFirstCallInit'][] = 'wfSigSetup';
@@ -17,5 +23,3 @@ function wfMakeSignature( $contents, $attributes, $parser ) {
 	global $wgStylePath;
 	return '<a href="http://www.wikia.com/wiki/Wikia:Staff" title="This user is a member of Wikia staff" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
 }
-
-?>
