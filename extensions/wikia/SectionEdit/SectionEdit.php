d9072d72c051848bb18f1c041b8a7b2d77974f60
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-04-22 11:37:34 +0000

    SectionEdit: work on RT #10623
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@7966 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SectionEdit/SectionEdit.php
+++ extensions/wikia/SectionEdit/SectionEdit.php
@@ -1,20 +1,43 @@
-<?
+<?php
 /**
  * Adds green "edit" section buttons for anons
  *
  * @author Christian Williams <christian@wikia-inc.com>
+ * @author Maciej Brencz <macbre@wikia-inc.com> - small fixes and enhancements
  */
+$wgExtensionCredits['other'][] = array(
+	'name' => 'SectionEdit',
+	'description' => 'Adds green section edit buttons',
+	'version' => '1.1',
+	'author' => array('Christian Williams', 'Maciej Brencz')
+);
 
-$wgHooks['SpecialFooterAfterWikia'][] = 'SectionEditStyle';
+$wgHooks['SpecialFooterAfterWikia'][] = 'SectionEditJS';
 
-function SectionEditStyle() {
-	global $wgUser;
-	if (is_object($wgUser) && $wgUser->isLoggedIn() ){
+function SectionEditJS() {
+	global $wgUser, $wgTitle, $wgDisableAnonymousEditig, $wgJsMimeType;
+
+	// add JS only for anons
+	if (is_object($wgUser) && $wgUser->isLoggedIn()) {
 		return true;
 	}
-	
-	echo '
-<script type="text/javascript">
+
+	// RT #10623: add green buttons also when anon editing is disabled
+	if ( !empty($wgDisableAnonymousEditig)) {
+		$editMsg = Xml::escapeJsString(wfMsg('editsection'));
+		$editUrl = Xml::escapeJsString($wgTitle->getEditUrl());
+
+		$script = '
+if (skin == "monaco") {
+	jQuery.noConflict();
+	jQuery("h2 span.mw-headline").each(function(i) {
+		//add elements and styling
+		jQuery(this).parent().prepend("<span class=\"editsection\"><a href=\"'.$editUrl.'\">'.$editMsg.'</a></span>").find("a").addClass("bigButton").contents().wrap("<big></big>").end().append("<small></small>").parent().css("position", "relative").css("top", "-3px").css("margin-bottom", "-2px");
+	});
+}';
+	}
+	else {
+		$script = '
 if (skin == "monaco") {
 	jQuery.noConflict();
 	jQuery("h2 span.editsection").each(function(i) {
@@ -23,7 +46,9 @@ if (skin == "monaco") {
 		//add elements and styling
 		jQuery(this).find("a").addClass("bigButton").contents().wrap("<big></big>").end().append("<small></small>").parent().css("position", "relative").css("top", "-3px").css("margin-bottom", "-2px");
 	});
-}
-</script>';
+}';
+	}
+
+	echo "<script type=\"$wgJsMimeType\">/*<![CDATA[*/$script\n/*]]>*/</script>";
 	return true;
 }
