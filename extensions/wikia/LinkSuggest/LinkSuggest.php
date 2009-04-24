6f389e86f70e8b4e530bf30548d7f220bb0a6161
inez <inez@wikia-inc.com>
2009-04-24 10:24:18 +0000

    * Minor fixes for LinkSuggest
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8050 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/LinkSuggest/LinkSuggest.php
+++ extensions/wikia/LinkSuggest/LinkSuggest.php
@@ -52,7 +52,7 @@ $wgHooks['EditForm::MultiEdit:Form'][] = 'AddLinkSuggest';
 function AddLinkSuggest($a, $b, $c, $d) {
 	global $wgOut, $wgExtensionsPath, $wgStyleVersion, $wgUser;
 	if($wgUser->getOption('disablelinksuggest') != true) {
-		$wgOut->addHTML('<div id="LS_imagePreview" style="visibility: hidden; position: absolute; z-index: 999; background-color: white;"></div>');
+		$wgOut->addHTML('<div id="LS_imagePreview" style="visibility: hidden; position: absolute; z-index: 999; width: 180px; text-align: right;"></div>');
 		$wgOut->addHTML('<div id="wpTextbox1_container" class="yui-ac-container"></div>');
 		$wgOut->addScript('<script type="text/javascript" src="'.$wgExtensionsPath.'/wikia/LinkSuggest/LinkSuggest.js?'.$wgStyleVersion.'"></script>');
 	}
