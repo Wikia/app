85428783afc00bed42e0173c6ae39a3cd4229faa
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-04-24 14:28:37 +0000

    now splitted by status
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8078 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/templates/shortstats.tmpl.php
+++ extensions/wikia/WikiFactory/templates/shortstats.tmpl.php
@@ -1,12 +1,21 @@
+<h2>
+	Wikis created daily:
+</h2>
 <table class="filehistory">
 	<tr>
 		<th>date</th>
-		<th>how many</th>
+		<th>total</th>
+		<th>active</th>
+		<th>disabled</th>
+		<th>redirected</th>
 	</tr>
-<?php foreach( $stats as $row ): ?>
+<?php foreach( $stats as $date => $row ): ?>
 	<tr>
-		<td><?php echo $row->date ?></td>
-		<td><?php echo $row->count ?></td>
+		<td><?php echo $date ?></td>
+		<td><?php echo isset( $row->total ) ? $row->total : 0; ?></td>
+		<td><?php echo isset( $row->active ) ? $row->active : 0; ?></td>
+		<td><?php echo isset( $row->disabled ) ? $row->disabled : 0; ?></td>
+		<td><?php echo isset( $row->redirected ) ? $row->redirected : 0; ?></td>
 	</tr>
 <?php endforeach; ?>
 </table>
