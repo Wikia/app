5489780dc61c552bb3cf23c331356036726be764
macbre <macbre@wikia-inc.com>
2009-04-27 14:50:30 +0000

    Curse/ABC cobranding: remove unused files
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8125 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/monaco/abc/footer.tmpl.php
+++
@@ -1,4 +0,0 @@
-<div id="special_footer">
-	<!-- <img src="/skins/monaco/abc/footer2.jpg"/> -->
-	<iframe width="100%" height="97" scrolling="no" frameborder="0" src="http://abc.go.com/static/gnav/footer_fall2007_external.html"></iframe>
-</div>
