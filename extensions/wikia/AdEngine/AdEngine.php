3c8ea2534368620c2a210856a4cb606029afd73c
adi <adi@wikia-inc.com>
2009-04-29 14:21:52 +0000

    small fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8187 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdEngine.php
+++ extensions/wikia/AdEngine/AdEngine.php
@@ -159,6 +159,7 @@ class AdEngine {
 				WHERE skin='".$db->strencode($skin_name)."'";
 
 		$res = $db->query($sql);
+		$this->slots = array();
 
 		while($row = $db->fetchObject($res)){
 			$this->slots[$row->slot] = array(
@@ -268,7 +269,7 @@ class AdEngine {
 		// Special case for this type of ad. Not in Athena
 		} else if ($slotname == 'RIGHT_SKYSCRAPER_1'){
 			return $this->getProviderFromId($this->slots[$slotname]['provider_id']);
-			
+
 		// All of the errors and toggles are handled, now switch based on language
 		} else {
 
