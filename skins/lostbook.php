29f15e0fcadf92e8a8f9797990b88462f3842500
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-04-30 13:18:44 +0000

    Lostbook: update the skin for MW1.14
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8208 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/lostbook.php
+++ skins/lostbook.php
@@ -6,29 +6,95 @@
  * dependency on PHPTAL.
  *
  * @todo document
- * @package MediaWiki
- * @subpackage Skins
+ * @file
+ * @ingroup Skins
  */
 
 if( !defined( 'MEDIAWIKI' ) )
 	die( -1 );
 
-/** */
-require_once('includes/SkinTemplate.php');
-
 /**
- * Inherit main code from SkinTemplate, set the CSS and template filter.
+ * Inherit main code from MonoBookTemplate, set the CSS and custom template elements.
  * @todo document
- * @package MediaWiki
- * @subpackage Skins
+ * @ingroup Skins
  */
-class Skinlostbook extends SkinTemplate {
+
+require_once("skins/MonoBook.php");
+
+class SkinLostbook extends SkinMonoBook {
 	/** Using lostbook. */
 	function initPage( &$out ) {
-		SkinTemplate::initPage( $out );
+		parent::initPage( $out );
 		$this->skinname  = 'lostbook';
 		$this->stylename = 'lostbook';
 		$this->template  = 'lostbookTemplate';
+
+		// register hook to add "Digg This Story" button to toolbox portlet
+		global $wgHooks;
+		$wgHooks['MonoBookTemplateToolboxEnd'][] = array(&$this, addDiggButton);
+	}
+
+	function setupSkinUserCss( OutputPage $out ) {
+		// Append to the default screen common & print styles...
+		WikiaSkinMonobook::setupSkinUserCss( $out );
+
+		$out->addStyle( 'lostbook/main.css', 'screen' );
+
+		$out->addStyle( 'lostbook/IE50Fixes.css', 'screen', 'lt IE 5.5000' );
+		$out->addStyle( 'lostbook/IE55Fixes.css', 'screen', 'IE 5.5000' );
+		$out->addStyle( 'lostbook/IE60Fixes.css', 'screen', 'IE 6' );
+		$out->addStyle( 'lostbook/IE70Fixes.css', 'screen', 'IE 7' );
+
+		$out->addStyle( 'lostbook/rtl.css', 'screen', '', 'rtl' );
+
+		// load YUI (moved from r5702)
+/*
+		$out->addScriptFile('http://yui.yahooapis.com/2.6.0/build/yahoo-dom-event/yahoo-dom-event.js');
+		$out->addScriptFile('http://yui.yahooapis.com/2.6.0/build/connection/connection-min.js');
+		$out->addScriptFile('http://yui.yahooapis.com/2.6.0/build/json/json-min.js');
+*/
+	}
+
+	// don't return "wikia" toolbox
+	protected function buildWikiaToolbox() {
+		return '';
+	}
+
+	public function addWikiaCss(&$out) {
+		return true;
+	}
+
+	public function addWikiaVars(&$obj, &$tpl) {
+		wfProfileIn(__METHOD__);
+
+		parent::addWikiaVars($obj, $tpl);
+
+		$tpl->set('copyright', '<a href="http://www.gnu.org/copyleft/fdl.html" class="external " title="http://www.gnu.org/copyleft/fdl.html" rel="nofollow">GFDL</a>');
+		$tpl->set('privacy', $this->privacyLink());
+		$tpl->set('about', $this->aboutLink());
+		$tpl->set('disclaimer', $this->disclaimerLink());
+
+		wfProfileOut(__METHOD__);
+		return true;
+	}
+
+	function addDiggButton($tpl) {
+?>
+
+				<div id="t-digglink">
+<?php
+		if($tpl->data['notspecialpage']) { 
+			global $wgTitle;
+			$url = htmlspecialchars($wgTitle->getFullURL());
+?>
+					<a href="http://digg.com/submit?phase=2&amp;topic=television&amp;url=<?= $url ?>"><img src="http://digg.com/img/badges/91x17-digg-button-alt.png" width="91" height="17" alt="Digg!" /></a>
+<?php
+		}
+?>
+				</div>
+<?php
+		return true;
+	
 	}
 }
 
@@ -37,272 +103,9 @@ class Skinlostbook extends SkinTemplate {
  * @package MediaWiki
  * @subpackage Skins
  */
-class lostbookTemplate extends QuickTemplate {
-	/**
-	 * Template filter callback for MonoBook skin.
-	 * Takes an associative array of data set from a SkinTemplate-based
-	 * class, and a wrapper for MediaWiki's localization database, and
-	 * outputs a formatted page.
-	 *
-	 * @access private
-	 */
-	function execute() {
-		// Suppress warnings to prevent notices about missing indexes in $this->data
-		wfSuppressWarnings();
+class lostbookTemplate extends MonoBookTemplate {
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="<?php $this->text('xhtmldefaultnamespace') ?>" <?php
-	foreach($this->data['xhtmlnamespaces'] as $tag => $ns) {
-		?>xmlns:<?php echo "{$tag}=\"{$ns}\" ";
-	} ?>xml:lang="<?php $this->text('lang') ?>" lang="<?php $this->text('lang') ?>" dir="<?php $this->text('dir') ?>">
-	<head>
-		<meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
-		<?php $this->html('headlinks') ?>
-		<title><?php $this->text('pagetitle') ?></title>
-		<style type="text/css" media="screen,projection">/*<![CDATA[*/ @import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/main.css?<?php echo $GLOBALS['wgStyleVersion'] ?>"; /*]]>*/</style>
-		<link rel="stylesheet" type="text/css" <?php if(empty($this->data['printable']) ) { ?>media="print"<?php } ?> href="<?php $this->text('stylepath') ?>/common/commonPrint.css?<?php echo $GLOBALS['wgStyleVersion'] ?>" />
-		<link rel="stylesheet" type="text/css" media="handheld" href="<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/handheld.css?<?php echo $GLOBALS['wgStyleVersion'] ?>" />
-		<!--[if lt IE 5.5000]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE50Fixes.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";</style><![endif]-->
-		<!--[if IE 5.5000]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE55Fixes.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";</style><![endif]-->
-		<!--[if IE 6]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE60Fixes.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";</style><![endif]-->
-		<!--[if IE 7]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE70Fixes.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";</style><![endif]-->
-		<!--[if lt IE 7]><script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath') ?>/common/IEFixes.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"></script>
-		<meta http-equiv="imagetoolbar" content="no" /><![endif]-->
-		
-<?php if( isset( $GLOBALS['wgTitle'] ) && defined( "NS_BLOG_ARTICLE" ) && $GLOBALS['wgTitle']->getNamespace() == NS_BLOG_ARTICLE ): ?>
-		<script type="<?php $this->text('jsmimetype') ?>" src="http://yui.yahooapis.com/2.6.0/build/yahoo-dom-event/yahoo-dom-event.js"></script>
-		<script type="<?php $this->text('jsmimetype') ?>" src="http://yui.yahooapis.com/2.6.0/build/connection/connection-min.js"></script>
-		<script type="<?php $this->text('jsmimetype') ?>" src="http://yui.yahooapis.com/2.6.0/build/json/json-min.js"></script>
-<?php endif; print Skin::makeGlobalVariablesScript( $this->data ); ?>
-
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/common/wikibits.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"><!-- wikibits js --></script>
-<?php	if($this->data['jsvarurl'  ]) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('jsvarurl'  ) ?>"><!-- site js --></script>
-<?php	} ?>
-<?php	if($this->data['pagecss'   ]) { ?>
-		<style type="text/css"><?php $this->html('pagecss'   ) ?></style>
-<?php	}
-		if($this->data['usercss'   ]) { ?>
-		<style type="text/css"><?php $this->html('usercss'   ) ?></style>
-<?php	}
-		if($this->data['userjs'    ]) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('userjs' ) ?>"></script>
-<?php	}
-		if($this->data['userjsprev']) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>"><?php $this->html('userjsprev') ?></script>
-<?php	}
-		if($this->data['trackbackhtml']) print $this->data['trackbackhtml']; ?>
-		<!-- Head Scripts -->
-		<?php $this->html('headscripts') ?>
-	</head>
-<body <?php if($this->data['body_ondblclick']) { ?>ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
-<?php if($this->data['body_onload'    ]) { ?>onload="<?php     $this->text('body_onload')     ?>"<?php } ?>
- class="mediawiki <?php $this->text('nsclass') ?> <?php $this->text('dir') ?> <?php $this->text('pageclass') ?>">
-	<div id="globalWrapper">
-		<div id="column-content">
-	<div id="content">
-		<a name="top" id="top"></a>
-		<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
-		<h1 class="firstHeading"><?php $this->data['displaytitle']!=""?$this->html('title'):$this->text('title') ?></h1>
-		<div id="bodyContent">
-			<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
-			<div id="contentSub"><?php $this->html('subtitle') ?></div>
-			<?php if($this->data['undelete']) { ?><div id="contentSub2"><?php     $this->html('undelete') ?></div><?php } ?>
-			<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
-			<?php if($this->data['showjumplinks']) { ?><div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div><?php } ?>
-			<!-- start content -->
-			<?php $this->html('bodytext') ?>
-			<?php if($this->data['catlinks']) { ?><div id="catlinks"><?php       $this->html('catlinks') ?></div><?php } ?>
-			<!-- end content -->
-			<div class="visualClear"></div>
-		</div>
-	</div>
-		</div>
-		<div id="column-one">
-	<div id="p-cactions" class="portlet">
-		<h5><?php $this->msg('views') ?></h5>
-		<div class="pBody">
-			<ul>
-	<?php			foreach($this->data['content_actions'] as $key => $tab) { ?>
-					 <li id="ca-<?php echo Sanitizer::escapeId($key) ?>"<?php
-					 	if($tab['class']) { ?> class="<?php echo htmlspecialchars($tab['class']) ?>"<?php }
-					 ?>><a href="<?php echo htmlspecialchars($tab['href']) ?>"><?php
-					 echo htmlspecialchars($tab['text']) ?></a></li>
-	<?php			 } ?>
-			</ul>
-		</div>
-	</div>
-	<div class="portlet" id="p-personal">
-		<h5><?php $this->msg('personaltools') ?></h5>
-		<div class="pBody">
-			<ul>
-<?php 			foreach($this->data['personal_urls'] as $key => $item) { ?>
-				<li id="pt-<?php echo Sanitizer::escapeId($key) ?>"<?php
-					if ($item['active']) { ?> class="active"<?php } ?>><a href="<?php
-				echo htmlspecialchars($item['href']) ?>"<?php
-				if(!empty($item['class'])) { ?> class="<?php
-				echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
-				echo htmlspecialchars($item['text']) ?></a></li>
-<?php			} ?>
-			</ul>
-		</div>
-		<div id="p-search" class="portlet">
-		<h5><label for="searchInput"><?php $this->msg('search') ?></label></h5>
-		<div id="searchBody" class="pBody">
-			<form action="<?php $this->text('searchaction') ?>" id="searchform"><div>
-				<input id="searchInput" name="search" type="text" <?php
-					if($this->haveMsg('accesskey-search')) {
-						?>accesskey="<?php $this->msg('accesskey-search') ?>"<?php }
-					if( isset( $this->data['search'] ) ) {
-						?> value="<?php $this->text('search') ?>"<?php } ?> />
-				<input type='submit' name="go" class="searchButton" id="searchGoButton"	value="<?php $this->msg('searcharticle') ?>" />&nbsp;
-				<input type='submit' name="fulltext" class="searchButton" id="mw-searchButton" value="<?php $this->msg('searchbutton') ?>" />
-			</div></form>
-		</div>
-	</div>
-		<?php include ('googlesearch.php') ?>
-	</div>
-	<div class="portlet" id="p-logo">
-		<a style="background-image: url(<?php $this->text('logopath') ?>);" <?php
-			?>href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>" <?php
-			?>title="<?php $this->msg('mainpage') ?>"></a>
-	</div>
-	<script type="<?php $this->text('jsmimetype') ?>"> if (window.isMSIE55) fixalpha(); </script>
-	<?php foreach ($this->data['sidebar'] as $bar => $cont) { ?>
-	<div class='portlet' id='p-<?php echo Sanitizer::escapeId($bar) ?>'>
-		<h5><?php $out = wfMsg( $bar ); if (wfEmptyMsg($bar, $out)) echo $bar; else echo $out; ?></h5>
-		<div class='pBody'>
-			<ul>
-<?php 			foreach($cont as $key => $val) { ?>
-				<li id="<?php echo Sanitizer::escapeId($val['id']) ?>"<?php
-					if ( $val['active'] ) { ?> class="active" <?php }
-				?>><a href="<?php echo htmlspecialchars($val['href']) ?>"><?php echo htmlspecialchars($val['text']) ?></a></li>
-<?php			} ?>
-			</ul>
-		</div>
-	</div>
-	<?php } ?>
-
-	<div class="portlet" id="p-tb">
-		<h5><?php $this->msg('toolbox') ?></h5>
-		<div class="pBody">
-			<ul>
-<?php
-		if($this->data['notspecialpage']) { ?>
-				<li id="t-whatlinkshere"><a href="<?php
-				echo htmlspecialchars($this->data['nav_urls']['whatlinkshere']['href'])
-				?>"><?php $this->msg('whatlinkshere') ?></a></li>
-<?php
-			if( $this->data['nav_urls']['recentchangeslinked'] ) { ?>
-				<li id="t-recentchangeslinked"><a href="<?php
-				echo htmlspecialchars($this->data['nav_urls']['recentchangeslinked']['href'])
-				?>"><?php $this->msg('recentchangeslinked') ?></a></li>
-<?php 		}
-		}
-		if(isset($this->data['nav_urls']['trackbacklink'])) { ?>
-			<li id="t-trackbacklink"><a href="<?php
-				echo htmlspecialchars($this->data['nav_urls']['trackbacklink']['href'])
-				?>"><?php $this->msg('trackbacklink') ?></a></li>
-<?php 	}
-		if($this->data['feeds']) { ?>
-			<li id="feedlinks"><?php foreach($this->data['feeds'] as $key => $feed) {
-					?><span id="feed-<?php echo Sanitizer::escapeId($key) ?>"><a href="<?php
-					echo htmlspecialchars($feed['href']) ?>"><?php echo htmlspecialchars($feed['text'])?></a>&nbsp;</span>
-					<?php } ?></li><?php
-		}
-
-		foreach( array('contributions', 'blockip', 'emailuser', 'upload', 'specialpages') as $special ) {
-
-			if($this->data['nav_urls'][$special]) {
-				?><li id="t-<?php echo $special ?>"><a href="<?php echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
-				?>"><?php $this->msg($special) ?></a></li>
-<?php		}
-		}
-
-		if(!empty($this->data['nav_urls']['print']['href'])) { ?>
-				<li id="t-print"><a href="<?php echo htmlspecialchars($this->data['nav_urls']['print']['href'])
-				?>"><?php $this->msg('printableversion') ?></a></li><?php
-		}
-
-		if(!empty($this->data['nav_urls']['permalink']['href'])) { ?>
-				<li id="t-permalink"><a href="<?php echo htmlspecialchars($this->data['nav_urls']['permalink']['href'])
-				?>"><?php $this->msg('permalink') ?></a></li><?php
-		} elseif ($this->data['nav_urls']['permalink']['href'] === '') { ?>
-				<li id="t-ispermalink"><?php $this->msg('permalink') ?></li><?php
-		}
-
-		wfRunHooks( 'lostbookTemplateToolboxEnd', array( &$this ) );
-?>
-<div id="t-digglink">
-<?php
-		if($this->data['notspecialpage']) { ?>
-				<a href="http://digg.com/submit?phase=2&topic=television&url=http://<?php echo $_SERVER["HTTP_HOST"] ?><?php echo $_SERVER["REQUEST_URI"] ?>">
-				<img src="http://digg.com/img/badges/91x17-digg-button-alt.png" width="91" height="17" alt="Digg!" />
-				</a>
-				<?php
-		}
-?>
-</div>
-			</ul>
-		</div>
-	</div>
-
-<div id="p-sidesearch" class="portlet">
-  <h5><label for="searchInput">Search</label></h5>
-  <div id="sidesearchBody" class="pBody">
-  <form action="/wiki/Special:Search" id="searchformside"><div>
-	  			<input id="searchInput" name="search" type="text" accesskey="f" value="" />
-		  		<input type='submit' name="go" class="searchButton" id="searchGoButton"	value="Go" />&nbsp;
-			  	<input type='submit' name="fulltext" class="searchButton" id="mw-searchButton" value="Search" />
-			  </div></form>
-  </div>
-</div>
-
-<?php
-		if( $this->data['language_urls'] ) { ?>
-	<div id="p-lang" class="portlet">
-		<h5><?php $this->msg('otherlanguages') ?></h5>
-		<div class="pBody">
-			<ul>
-<?php		foreach($this->data['language_urls'] as $langlink) { ?>
-				<li class="<?php echo htmlspecialchars($langlink['class'])?>"><?php
-				?><a href="<?php echo htmlspecialchars($langlink['href']) ?>"><?php echo $langlink['text'] ?></a></li>
-<?php		} ?>
-			</ul>
-		</div>
-	</div>
-<?php	} ?>
-
-
-<!-- Google AdSense Side -->
-<?php
-if (strstr($_SERVER['REQUEST_URI'],"Special:")) {include ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"search=")) {include  ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"action=")) {include ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"printable=")) {include ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"category:")) {include ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"MediaWiki")) {include ("nogoogleads.inc");}
-else {include ("adsense-side.php");}
-?>
-<!-- End Google Adsense Side -->
-
-</div><!-- end of the left (by default at least) column -->
-
-<!-- Google AdSense Bottom-->
-<?php
-if (strstr($_SERVER['REQUEST_URI'],"Special:")) {include ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"search=")) {include  ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"action=")) {include ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"printable=")) {include ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"category:")) {include ("nogoogleads.inc");}
-else if (strstr($_SERVER['REQUEST_URI'],"MediaWiki")) {include ("nogoogleads.inc");}
-else {include ("adsense.php");}
-?>
-<!-- End Google Adsense Bottom -->
-
-			<div class="visualClear"></div>
-			<div id="footer">
-<?php
+	function footer() {
 		if($this->data['poweredbyico']) { ?>
 				<div id="f-poweredbyico"><?php $this->html('poweredbyico') ?></div>
 <?php 	}
@@ -327,36 +130,8 @@ else {include ("adsense.php");}
 <li id="footer-amung"><a href="http://whos.amung.us/show/mcrbmil5"><img src="http://whos.amung.us/swidget/mcrbmil5.gif" alt="tracker" width="80" height="15" border="0" /></a></li>
 <li id="footer-coolness"><a href="http://www.coolness.com">Coolness Domains</a></li>
 <li id="footer-logoby">Logo by Placid Azylum</li>
-<li id="footer-cssby">CSS by jabberwock & plkrtn</li>
+<li id="footer-cssby">CSS by jabberwock &amp; plkrtn</li>
 			</ul>
-		</div>
-	<?php $this->html('bottomscripts'); /* JS call to runBodyOnloadHook */ ?>
-</div>
+<?php	}
 
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-87598-1";
-urchinTracker();
-</script>
-
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-87598-7";
-urchinTracker();
-</script>
-
-<?php $this->html('reporttime') ?>
-<?php if ( $this->data['debug'] ): ?>
-<!-- Debug output:
-<?php $this->text( 'debug' ); ?>
-
--->
-<?php endif; ?>
-</body></html>
-<?php
-	wfRestoreWarnings();
-	} // end of execute() method
-} // end of class
-?>
+}
