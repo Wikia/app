afea970259def85153d4b48777cdac7dd0400904
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-04-30 14:19:38 +0000

    cssing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8213 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/Close/templates/index-html.tmpl.php
+++ extensions/wikia/WikiFactory/Close/templates/index-html.tmpl.php
@@ -5,19 +5,30 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="description" content="">
 	<meta name="keywords" content="">
+	<style language="text/css">
+	.error{background-color: red;}
+        { background: white; }
+        a { color: navy; text-decoration: none; }
+        a:hover { color: blue; }
+        .project { border: solid 1px black; padding: 5px; width: 500px; margin: 20px; background-color: silver; }
+        .projname { font-size: xx-large; color: white; }
+        .site { border: dotted 1px black; padding: 3px; width: 600px; margin: 3px; background-color: white;  }
+        .auxtables { font-size: small; }
+        .meta { color: gray; font-size: small; }
+    </style>
 </head>
 <body>
 	<ol>
 <?php if( $haveXml ): ?>
 		<li>
 			<a href="full.xml.gz">full.xml.gz</a>
-			<em>(last changed: <?php echo date("F d Y H:i:s.", filectime( "{$directory}/full.xml.gz" ) ) ?>)</em>
+			<em class="meta">(last changed: <?php echo date("F d Y H:i:s.", filectime( "{$directory}/full.xml.gz" ) ) ?>)</em>
 		</li>
 <?php endif ?>
 <?php if( $haveZip ): ?>
 		<li>
 			<a href="images.zip">images.zip</a>
-			<em>(last changed: <?php echo date("F d Y H:i:s.", filectime( "{$directory}/images.zip" ) ) ?>)</em>
+			<em class="meta">(last changed: <?php echo date("F d Y H:i:s.", filectime( "{$directory}/images.zip" ) ) ?>)</em>
 		</li>
 <?php endif ?>
 	</ol>
