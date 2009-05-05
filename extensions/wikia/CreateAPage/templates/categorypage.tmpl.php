585983facc1fa6c0a218b20a4bdf2b0c4b428551
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-05 08:21:00 +0000

    (bug 14893) Allow [Add Category] button to be translated.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8253 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreateAPage/templates/categorypage.tmpl.php
+++ extensions/wikia/CreateAPage/templates/categorypage.tmpl.php
@@ -24,7 +24,7 @@ $xnum++;
 ?>
 </div>
 <textarea accesskey="," name="wpCategoryTextarea" id="wpCategoryTextarea" rows='3' cols='<?=$cols?>'<?$ew?>><?=$text_category?></textarea>
-<input type="button" name="wpCategoryButton" id="wpCategoryButton" class="button color1" value="Add Category" onclick="cloudInputAdd(); return false ;" />
+<input type="button" name="wpCategoryButton" id="wpCategoryButton" class="button color1" value="<?= wfMsg ('createpage_addcategory') ?>" onclick="cloudInputAdd(); return false ;" />
 <input type="text" name="wpCategoryInput" id="wpCategoryInput" value="" />
 </div>
 <script type="text/javascript">
