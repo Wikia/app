4156935cc670f31b5034e8f345105ed738bac3d8
macbre <macbre@wikia-inc.com>
2009-05-05 17:27:57 +0000

    AjaxLogin: add AJAX dispatcher function to get ajax login
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8284 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AjaxLogin.php
+++ extensions/wikia/AjaxLogin/AjaxLogin.php
@@ -13,10 +13,33 @@ $wgHooks['GetHTMLAfterBody'][] = 'GetAjaxLoginForm';
 
 function GetAjaxLoginForm($skin) {
 	global $wgTitle, $wgUser;
-	if($wgUser->isAnon() && $wgTitle->getNamespace() != 8 && $wgTitle->getDBkey() != 'Userlogin') {
+	if(get_class($skin) != 'AwesomeTemplate' && $wgUser->isAnon() && $wgTitle->getNamespace() != 8 && $wgTitle->getDBkey() != 'Userlogin') {
 		$tmpl = new EasyTemplate(dirname( __FILE__ ));
 		echo $tmpl->execute('AjaxLogin');
 	}
 	return true;
 }
-?>
+
+$wgAjaxExportList[] = 'GetAjaxLogin';
+function GetAjaxLogin() {
+	$tmpl = new EasyTemplate(dirname( __FILE__ ));
+
+	$ret = $tmpl->execute('AjaxLogin');
+
+	// retunr just the <form>
+	$start = strpos($ret, '<form');
+	$end = strpos($ret, '</form>') + 7;
+	$ret = substr($ret, $start, $end-$start);
+
+	// make me modal
+	$ret = '
+	<div id="AjaxLogin" title="'.htmlspecialchars(wfMsg('login')).'">
+		'.$ret.'
+	</div>
+
+	<script type="text/javascript">
+		$("#AjaxLogin").makeModal();
+	</script>';
+
+	return new AjaxResponse( $ret );
+}
