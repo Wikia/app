546745a9e83aa43e5bd9b5f9d2e343cac8c58c32
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-06 11:58:02 +0000

    added city_id and img_path (without directory)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8314 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaSpecialUploadInfo/WikiaSpecialUploadInfo.php
+++ extensions/wikia/WikiaSpecialUploadInfo/WikiaSpecialUploadInfo.php
@@ -14,7 +14,7 @@ $wgHooks['UploadComplete'][]  = 'wfSpecialUploadPutInformation';
 
 
 function wfSpecialUploadPutInformation( $uploadForm ) {
-	global $wgRequest;
+	global $wgRequest, $wgCityId;
 
 	$title = $uploadForm->mLocalFile->getTitle();
 	if( !($title instanceof Title) ) {
@@ -24,17 +24,20 @@ function wfSpecialUploadPutInformation( $uploadForm ) {
 		return true;
 	}
 	$mTitle = $title->getText();
+	$relPath = $uploadForm->mLocalFile->getRel();
 	$fullPath = $uploadForm->mLocalFile->getFullPath();
 
 	$dbw = wfGetDBExt( DB_MASTER );
 	$dbw->insert(
 		"upload_log",
 		array(
-			"up_id" => $title->getArticleId(),
-			"up_path" => $fullPath,
+			"up_id" 	=> $title->getArticleId(),
+			"up_path" 	=> $fullPath,
+			"up_imgpath"=> $relPath,
 			"up_flags"	=> 0,
-			"up_title" => $mTitle,
-			"up_created" => wfTimestampNow()
+			"up_title" 	=> $mTitle,
+			"up_created"=> wfTimestampNow(),
+			"up_city_id"=> $wgCityId
 		),
 		__METHOD__
 	);
