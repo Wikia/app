15465599907463adf6d887e7228e876459d2a8e4
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-06 21:46:36 +0000

    Lean Monaco: remove HTML for hub menu (will be fetch via AJAX)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8338 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/Awesome.php
+++ skins/Awesome.php
@@ -1447,43 +1447,6 @@ if( $custom_user_data ) {
 
 	<div class="monaco_shrinkwrap" style="position: absolute; top: 0; z-index: 20">
 <?php
-	if(!empty($categorylist)) {
-?>
-
-			<div id="headerMenuHub" class="headerMenu color1 reset">
-				<table cellspacing="0">
-				<tr>
-					<td>
-<?php
-		for($i = 0; $i < ceil(count($categorylist['nodes']) / 2); $i++) {
-?>
-						<a rel="nofollow" href="<?= htmlspecialchars($categorylist['nodes'][$i]['href']) ?>" id="headerMenuHub-<?= $i ?>"><?= $categorylist['nodes'][$i]['text'] ?></a><br />
-<?php
-		}
-?>
-					</td>
-					<td>
-<?php
-		for($i = ceil(count($categorylist['nodes']) / 2); $i < count($categorylist['nodes']); $i++) {
-?>
-						<a rel="nofollow" href="<?= htmlspecialchars($categorylist['nodes'][$i]['href']) ?>" id="headerMenuHub-<?= $i ?>"><?= $categorylist['nodes'][$i]['text'] ?></a><br />
-<?php
-		}
-?>
-					</td>
-				</tr>
-				</table>
-<?php
-		if($categorylist['cat']['text']) {
-?>
-				<a rel="nofollow" href="<?= htmlspecialchars($categorylist['cat']['href']) ?>" id="goToHub"><?= wfMsg('seemoredotdotdot') ?></a>
-<?php
-		}
-?>
-			</div>
-<?php
-	}
-
 	if(isset($this->data['userlinks']['more'])) {
 ?>
 			<div id="headerMenuUser" class="headerMenu color1 reset">
