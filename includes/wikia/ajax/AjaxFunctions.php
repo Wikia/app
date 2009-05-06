5cc919d95f90dc05a7d8dcfdff6d42a0f36c4041
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-06 23:27:06 +0000

    Lean Monaco: add GetUserMenu ajax method to fetch HTML of user menu
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8347 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/ajax/AjaxFunctions.php
+++ includes/wikia/ajax/AjaxFunctions.php
@@ -13,3 +13,42 @@ function GetHubMenu() {
 
 	return $response;
 }
+
+$wgAjaxExportList[] = 'GetUserMenu';
+function GetUserMenu($userName = '') {
+	$links = array();
+
+	$links['userpage'] = array(
+		'text' => wfMsg('mypage'),
+		'href' => Title::newFromText($userName, NS_USER)->getLocalURL()
+		);
+
+	$links['mycontris'] = array(
+		'text' => wfMsg('mycontris'),
+		'href' => Title::newFromText('Contributions/'.$userName, NS_SPECIAL)->getLocalURL()
+		);
+
+	$links['widgets'] = array(
+		'text' => wfMsg('manage_widgets'),
+		'href' => '#'
+		);
+
+	$links['preferences'] = array(
+		'text' => wfMsg('preferences'),
+		'href' => Title::newFromText('Preferences', NS_SPECIAL)->getLocalURL()
+		);
+
+	$html = '<div class="headerMenu color1 reset" id="headerMenuUser"><ul>';
+	foreach($links as $id => $link) {
+		$tooltip = !isMsgEmpty('tooltip-pt-'.$id) ?  htmlspecialchars(wfMsg('tooltip-pt-'.$id)) : '';
+		$accesskey = !isMsgEmpty('accesskey-pt-'.$id) ?  wfMsg('accesskey-pt-'.$id) : '';
+		$html .= '<li id="userMenu-'.$id.'"><a href="'.$link['href'].'" title="'.$tooltip.'" accesskey="'.$accesskey.'">'.htmlspecialchars($link['text']).'</a></li>';
+	}
+	$html .= '</ul></div>';
+
+	$response = new AjaxResponse();
+	$response->addText( $html );
+	$response->setCacheDuration( 3600 * 24 * 365 * 10); // 10 years
+
+	return $response;
+}
