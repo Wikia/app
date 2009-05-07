2a077954850755b5c6896f6eaa1f809ddb303b4f
bartek <bartek@wikia-inc.com>
2009-05-07 09:33:06 +0000

    PictureGame: fix fatal typo for #15034
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8357 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/PictureGame/AjaxUploadForm.php
+++ extensions/wikia/PictureGame/AjaxUploadForm.php
@@ -43,7 +43,7 @@ class AjaxUploadForm extends UploadForm{
 				$this->mainUploadForm( wfMsgHtml( 'emptyfile' ) );
 			    return;
 
-			case self::MIN_LENGHT_PARTNAME:
+			case self::MIN_LENGTH_PARTNAME:
 				$this->mainUploadForm( wfMsgHtml( 'minlength1' ) );
 			    return;
 
