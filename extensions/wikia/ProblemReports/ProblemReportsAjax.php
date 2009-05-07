e45f9871ef4a09dc96d5bc77d323eda26e75ee45
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-07 19:54:54 +0000

    ProblemReports: cache AJAX response
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8390 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ProblemReports/ProblemReportsAjax.php
+++ extensions/wikia/ProblemReports/ProblemReportsAjax.php
@@ -50,8 +50,9 @@ function wfProblemReportsAjaxGetDialog() {
 	}
 
 	// create AJAX response
-	$response = new AjaxResponse( $text );
-	$response->setContentType('text/plain; charset=utf-8');
+	$response = new AjaxResponse();
+	$response->addText($text);
+	$response->setCacheDuration( 3600 * 24 * 365 * 10); // 10 years
 
 	wfProfileOut(__METHOD__);
     
