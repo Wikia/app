0e7f97ce14bc67020c45903d0deb427ed79d5f8e
adi <adi@wikia-inc.com>
2009-05-07 11:17:26 +0000

    OutboundScreen ext (interstitial ad test): whitelist support added + delayed redirect
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8360 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/OutboundScreen/SpecialOutboundScreen_body.php
+++ extensions/wikia/OutboundScreen/SpecialOutboundScreen_body.php
@@ -1,6 +1,8 @@
 <?php
 
 class Outbound extends UnlistedSpecialPage {
+	private $redirectDelay = 5; // in seconds
+
 	/**
 	 * Constructor
 	 */
@@ -12,24 +14,31 @@ class Outbound extends UnlistedSpecialPage {
 	function execute ( $par ) {
 		?>
 <html>
-<body>
-<div style="height: 100%; width: 100%; text-align: center">
-	<div style="margin: 0 auto">
-		<img src='http://staff.wikia-inc.com/images/2/25/Wordmark_wikia_gradient.png' alt="Wikia logo" />
-	</div>
-	<div style="margin-top: 3em">
-		<p>You are leaving Wikia for another page on the Internet.</p>
+<script type="text/javascript">
+<!--
+function doRedirect(){
+	window.location = "<?=$par;?>";
+}
+//-->
+</script>
+<body onLoad="setTimeout('doRedirect()', <?=($this->redirectDelay * 1000);?>)">
+	<div style="height: 100%; width: 100%; text-align: center">
+		<div style="margin: 0 auto">
+			<img src='http://staff.wikia-inc.com/images/2/25/Wordmark_wikia_gradient.png' alt="Wikia logo" />
+		</div>
+		<div style="margin-top: 3em">
+			<p>You are leaving Wikia for another page on the Internet.</p>
 
-		<p>Watch out. There be dragons! Seriously.</p>
+			<p>Watch out. There be dragons! Seriously.</p>
 
-		<p><a href="<?= $par ?>">Click here to continue...</a></p>
+			<p><a href="<?= $par ?>">Click here to continue...</a></p>
+		</div>
+		<div style="vertical-align: middle;">
+			<!-- Begin: AdBrite, Generated: 2009-05-04 17:07:52  -->
+			<script type="text/javascript" src="http://ads.adbrite.com/mb/text_group.php?sid=1157756&br=1"></script>
+			<!-- End: AdBrite -->
+		</div>
 	</div>
-	<div style="vertical-align: middle;">
-		<!-- Begin: AdBrite, Generated: 2009-05-04 17:07:52  -->
-		<script type="text/javascript" src="http://ads.adbrite.com/mb/text_group.php?sid=1157756&br=1"></script>
-		<!-- End: AdBrite -->
-	</div>
-</div>
 </body>
 </html>
 		<?php
