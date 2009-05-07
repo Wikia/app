65dc0f0fbabf3e70ae0674df6c668c3d83fc639b
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-07 11:17:26 +0000

    OutboundScreen ext (interstitial ad test): whitelist support added + delayed redirect
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8360 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/OutboundScreen/OutboundScreen.php
+++ extensions/wikia/OutboundScreen/OutboundScreen.php
@@ -1,17 +1,51 @@
 <?php
+/**
+ * OutboundScreen - redirects external links to special page
+ *
+ * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
+ * @author Adrian 'ADi' Wieczorek <adi(at)wikia.com>
+ *
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ */
 
 $wgHooks['LinkerMakeExternalLink'][] = 'efOutboundScreen';
+$wgAutoloadClasses['Outbound'] = dirname( __FILE__ ) . '/SpecialOutboundScreen_body.php';
+$wgSpecialPages['Outbound'] = 'Outbound';
+$wgExtensionMessagesFiles['Outbound'] = dirname(__FILE__) . '/OutboundScreen.i18n.php';
 
 function efOutboundScreen ( $url, $text, $link ) {
-	$special = Title::newFromText( 'Special:Outbound/' . $url );
+	static $whiteList;
 
-	if($special instanceof Title) {
-		$url = $special->getFullURL();
+	if(!is_array($whiteList)) {
+		$whiteList = array();
+		$whiteListContent = wfMsgForContent('outbound-screen-whitelist');
+		if(!empty($whiteListContent)) {
+			$lines = explode("\n", $whiteListContent);
+			foreach($lines as $line) {
+				if(strpos($line, '* ') === 0 ) {
+					$whiteList[] = trim($line, '* ');
+				}
+			}
+		}
+	}
+
+	$isWhitelisted = false;
+	foreach($whiteList as $whiteListedUrl) {
+		$matches = null;
+		preg_match('/'.$whiteListedUrl.'/i', $url, $matches);
+		if(isset($matches[0])) {
+			$isWhitelisted = true;
+			break;
+		}
+	}
+
+	if(!$isWhitelisted) {
+		$special = Title::newFromText( 'Special:Outbound/' . $url );
+		if($special instanceof Title) {
+			$url = $special->getFullURL();
+		}
 	}
 
 	return true;
 }
-
-$wgAutoloadClasses['Outbound'] = dirname( __FILE__ ) . '/SpecialOutboundScreen_body.php';
-$wgSpecialPages['Outbound'] = 'Outbound';
-$wgExtensionMessagesFiles['Outbound'] = dirname(__FILE__) . '/OutboundScreen.i18n.php';
