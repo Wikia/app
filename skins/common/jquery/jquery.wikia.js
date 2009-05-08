871702c084b5d522b291537690ea14567a6b7a78
macbre <macbre@wikia-inc.com>
2009-05-08 20:37:38 +0000

    LeanMonaco: add makeModal() as jQuery plugin
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8454 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/jquery/jquery.wikia.js
+++ skins/common/jquery/jquery.wikia.js
@@ -4,3 +4,26 @@ jQuery.fn.log = function (msg) {
 	}
 	return this;
 };
+
+jQuery.fn.getModal = function(url, id, options) {
+	// get modal plugin
+	$.getScript(stylepath + '/common/jquery/jquery.wikia.modal.js?' + wgStyleVersion, function() {
+		$().log('getModal: plugin loaded');
+
+		// get modal content via AJAX
+		$.get(url, function(html) {
+			$("#positioned_elements").append(html);
+
+			// makeModal() if requested
+			if (typeof id == 'string') {
+				$(id).makeModal(options);
+				$().log('getModal: ' + id + ' modal made');
+			}
+
+			// fire callback if provided
+			if (typeof options == 'object' && typeof options.callback == 'function') {
+				options.callback();
+			}
+		});
+	});
+}
