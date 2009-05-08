7953eee70a7a4e98e4770bfa3e074e46fd2d1505
eloy <eloy@wikia-inc.com>
2009-05-08 08:44:00 +0000

    CloseWiki: renaming files
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8412 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/Close/maintenance.php
+++ extensions/wikia/WikiFactory/Close/maintenance.php
@@ -5,9 +5,9 @@
  * @addtopackage maintenance
  */
 
-ini_set( "include_path", dirname(__FILE__)."/../../../../../maintenance/" );
+ini_set( "include_path", dirname(__FILE__)."/../../../../maintenance/" );
 require_once( "commandLine.inc" );
-require_once( "closeWiki.inc" );
+require_once( "CloseWikiMaintenace.php" );
 
 $setup = isset( $options[ "setup" ] ) ? true : false;
 $maintenance = new CloseWikiMaintenace( );
