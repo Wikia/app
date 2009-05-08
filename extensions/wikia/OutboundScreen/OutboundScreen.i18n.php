dc12b8744342e883b79060491b81a169fbb49c4b
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-08 16:47:10 +0000

    OutboundScreen: moved text to message file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8448 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/OutboundScreen/OutboundScreen.i18n.php
+++ extensions/wikia/OutboundScreen/OutboundScreen.i18n.php
@@ -1,4 +1,11 @@
 <?php
 
 $messages = array();
-$messages["en"] = array();
+$messages["en"] = array(
+	'outbound-screen-text' => '<p>You are leaving Wikia for another page on the Internet.</p>
+
+                        <p>Watch out. There be dragons! Seriously.</p>
+
+                        <p><a href="$1">Click here to continue...</a></p>',
+
+);
--- extensions/wikia/OutboundScreen/SpecialOutboundScreen_body.php
+++ extensions/wikia/OutboundScreen/SpecialOutboundScreen_body.php
@@ -26,13 +26,7 @@ function doRedirect(){
 		<div style="margin: 0 auto">
 			<img src='http://staff.wikia-inc.com/images/2/25/Wordmark_wikia_gradient.png' alt="Wikia logo" />
 		</div>
-		<div style="margin-top: 3em">
-			<p>You are leaving Wikia for another page on the Internet.</p>
-
-			<p>Watch out. There be dragons! Seriously.</p>
-
-			<p><a href="<?= $par ?>">Click here to continue...</a></p>
-		</div>
+		<div style="margin-top: 3em"><?=wfMsgForContent( 'outbound-screen-text', $par );?></div>
 		<div style="vertical-align: middle;">
 			<!-- Begin: AdBrite, Generated: 2009-05-04 17:07:52  -->
 			<script type="text/javascript" src="http://ads.adbrite.com/mb/text_group.php?sid=1157756&br=1"></script>
