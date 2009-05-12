3e5bcd9c1b20a36d9aa61dde8f64b187949bd288
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-12 15:37:28 +0000

    Answers: Make widget answers works correctly
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8531 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WidgetFramework/Widgets/WidgetAnswers/WidgetAnswers.js
+++ extensions/wikia/WidgetFramework/Widgets/WidgetAnswers/WidgetAnswers.js
@@ -1,25 +1,44 @@
+function WidgetAnswers_init(id) {
+	jQuery.getScript(WidgetAnswers_url);
+}
+
+function WidgetAnswers_load(data) {
+	if(data.query.wkpagesincat) {
+		html = '';
+		for(var recent_q in data.query.wkpagesincat ) {
+			var page = data.query.wkpagesincat[recent_q];
+			var text = page.title.replace(/_/g," ") + "?";
+			if(text.length > 100) {
+				text = text.substring(0,100) + "...";
+			}
+			html += "<li><a href=\"" + page.url + "\" target=\"_blank\">" + text + "</a></li>";
+		}
+		jQuery("#recent_unanswered_questions").prepend(html);
+	}
+}
+
 var widget_answers_placeholder = 'Ask a question';
-function answers_widget_handler(e) { 
+function answers_widget_handler(e) {
 	if (e.type == 'focus') {
 		if (YAHOO.util.Event.getTarget(e).value == widget_answers_placeholder) {
-			YAHOO.util.Dom.removeClass('answers_ask_field', 'alt');	
-			$('answers_ask_field').value = '';
-		}	
+			YAHOO.util.Dom.removeClass('answers_ask_field', 'alt');
+			$G('answers_ask_field').value = '';
+		}
 	} else if (e.type == 'blur') {
 		if (YAHOO.util.Event.getTarget(e).value == '') {
 			YAHOO.util.Dom.addClass('answers_ask_field', 'alt');
-			$('answers_ask_field').value = widget_answers_placeholder;
+			$G('answers_ask_field').value = widget_answers_placeholder;
 		}
 	} else if (e.type == 'keypress') {
 		var keycode = e.which || window.event.keyCode;
-		if (keycode == 13 && $('answers_ask_field').value != '' ) {
-			var widget_answers_question = $('answers_ask_field').value;
+		if (keycode == 13 && $G('answers_ask_field').value != '' ) {
+			var widget_answers_question = $G('answers_ask_field').value;
 			window.open('http://answers.wikia.com/index.php?title=Special:CreateQuestionPage&questiontitle=' + widget_answers_question + '&categories=' + wgSitename, 'wikianswers');
 			YAHOO.util.Dom.addClass('answers_ask_field', 'alt');
-			$('answers_ask_field').value = widget_answers_placeholder;
+			$G('answers_ask_field').value = widget_answers_placeholder;
 		}
 	}
-} 
-YAHOO.util.Event.addListener('answers_ask_field', "focus", answers_widget_handler); 
+}
+YAHOO.util.Event.addListener('answers_ask_field', "focus", answers_widget_handler);
 YAHOO.util.Event.addListener('answers_ask_field', "blur", answers_widget_handler);
 YAHOO.util.Event.addListener('answers_ask_field', "keypress", answers_widget_handler);
