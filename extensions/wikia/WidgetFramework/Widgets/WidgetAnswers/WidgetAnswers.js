93ab143b38cdd7bf84809f6de2b2cc9fe8b3b0a4
inez <inez@wikia-inc.com>
2009-05-12 16:12:21 +0000

    Answers: Allow to have more then one Widget on the page
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8533 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WidgetFramework/Widgets/WidgetAnswers/WidgetAnswers.js
+++ extensions/wikia/WidgetFramework/Widgets/WidgetAnswers/WidgetAnswers.js
@@ -1,44 +1,48 @@
-function WidgetAnswers_init(id) {
-	jQuery.getScript(WidgetAnswers_url);
+var widget_answers_placeholder = 'Ask a question';
+function WidgetAnswers_handler(e) {
+	if(e.type == 'focus') {
+		if(e.target.value == widget_answers_placeholder) {
+			e.target.value = '';
+			e.target.style.color = '#000';
+		}
+	} else if (e.type == 'blur') {
+		if(e.target.value == '') {
+			e.target.value = widget_answers_placeholder;
+			e.target.style.color = '#999';
+		}
+	} else if (e.type == 'keypress') {
+		var keycode = e.which || window.event.keyCode;
+		if(keycode == 13 && e.target.value != '' ) {
+			window.open('http://answers.wikia.com/index.php?title=Special:CreateQuestionPage&questiontitle=' + e.target.valuel + '&categories=' + wgSitename, 'wikianswers');
+			e.target.value = widget_answers_placeholder;
+			e.target.style.color = '#999';
+		}
+	}
 }
 
+var WidgetAnswers_html = false;
+function WidgetAnswers_init(id) {
+	if(WidgetAnswers_html === false) {
+		WidgetAnswers_html = '';
+		jQuery.getScript(WidgetAnswers_url);
+	} else {
+		$('#'+id+'_content').children('div:last').children('ul').prepend(WidgetAnswers_html);
+	}
+
+	$('#'+WidgetAnswers_ids[i]+'_content').children('form').children('input').val(widget_answers_placeholder).focus(WidgetAnswers_handler).blur(WidgetAnswers_handler).keypress(WidgetAnswers_handler);
+}
 function WidgetAnswers_load(data) {
 	if(data.query.wkpagesincat) {
-		html = '';
 		for(var recent_q in data.query.wkpagesincat ) {
 			var page = data.query.wkpagesincat[recent_q];
 			var text = page.title.replace(/_/g," ") + "?";
 			if(text.length > 100) {
 				text = text.substring(0,100) + "...";
 			}
-			html += "<li><a href=\"" + page.url + "\" target=\"_blank\">" + text + "</a></li>";
+			WidgetAnswers_html += "<li><a href=\"" + page.url + "\" target=\"_blank\">" + text + "</a></li>";
 		}
-		jQuery("#recent_unanswered_questions").prepend(html);
-	}
-}
-
-var widget_answers_placeholder = 'Ask a question';
-function answers_widget_handler(e) {
-	if (e.type == 'focus') {
-		if (YAHOO.util.Event.getTarget(e).value == widget_answers_placeholder) {
-			YAHOO.util.Dom.removeClass('answers_ask_field', 'alt');
-			$G('answers_ask_field').value = '';
-		}
-	} else if (e.type == 'blur') {
-		if (YAHOO.util.Event.getTarget(e).value == '') {
-			YAHOO.util.Dom.addClass('answers_ask_field', 'alt');
-			$G('answers_ask_field').value = widget_answers_placeholder;
-		}
-	} else if (e.type == 'keypress') {
-		var keycode = e.which || window.event.keyCode;
-		if (keycode == 13 && $G('answers_ask_field').value != '' ) {
-			var widget_answers_question = $G('answers_ask_field').value;
-			window.open('http://answers.wikia.com/index.php?title=Special:CreateQuestionPage&questiontitle=' + widget_answers_question + '&categories=' + wgSitename, 'wikianswers');
-			YAHOO.util.Dom.addClass('answers_ask_field', 'alt');
-			$G('answers_ask_field').value = widget_answers_placeholder;
+		for(var i = 0; i < WidgetAnswers_ids.length; i++) {
+			$('#'+WidgetAnswers_ids[i]+'_content').children('div:last').children('ul').prepend(WidgetAnswers_html);
 		}
 	}
 }
-YAHOO.util.Event.addListener('answers_ask_field', "focus", answers_widget_handler);
-YAHOO.util.Event.addListener('answers_ask_field', "blur", answers_widget_handler);
-YAHOO.util.Event.addListener('answers_ask_field', "keypress", answers_widget_handler);
