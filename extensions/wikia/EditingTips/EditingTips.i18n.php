3e3d6a21461f0adb5c7dadf872358d48b0cb7ca2
tor <tor@wikia-inc.com>
2009-05-17 17:09:34 +0000

    added FI i18n for Finnish (#15659)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8723 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditingTips/EditingTips.i18n.php
+++ extensions/wikia/EditingTips/EditingTips.i18n.php
@@ -3,9 +3,18 @@ $messages = array();
 $messages['en'] = array(
 	'tog-disableeditingtips' => 'Do not show editing tips',
 	'tog-widescreeneditingtips' => 'Use the Widescreen editing',	
-	'editingtips_enter_widescreen' => 'Enter Widescreen' ,
-	'editingtips_exit_widescreen' => 'Exit Widescreen' ,	
-	'editingtips_hide' => 'Hide Editing Tips' ,
-	'editingtips_show' => 'Show Editing Tips' ,
+	'editingtips_enter_widescreen' => 'Enter Widescreen',
+	'editingtips_exit_widescreen' => 'Exit Widescreen',
+	'editingtips_hide' => 'Hide Editing Tips',
+	'editingtips_show' => 'Show Editing Tips',
 	'editingTips' => 'Add your tips [[MediaWiki:EditingTips|here]]',
+);
+$messages['fi'] = array(
+       'tog-disableeditingtips' => 'Älä näytä muokkausvinkkejä',
+       'tog-widescreeneditingtips' => 'Käytä laajakuvaeditoria',
+       'editingtips_enter_widescreen' => 'Siirry laajakuvaeditoriin',
+       'editingtips_exit_widescreen' => 'Poistu laajakuvaeditorista',
+       'editingtips_hide' => 'Piilota muokkausvinkit',
+       'editingtips_show' => 'Näytä muokkausvinkit',
+       'editingTips' => 'Lisää vinkkisi [[MediaWiki:EditingTips|tänne]]',
 );
