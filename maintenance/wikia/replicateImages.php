a8c9c10bec633587d66c73011bfbe46897c25c9d
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-18 08:37:33 +0000

    replicate images: most recent firts
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8727 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/replicateImages.php
+++ maintenance/wikia/replicateImages.php
@@ -78,7 +78,7 @@ class WikiaReplicateImages {
 			),
 			__METHOD__,
 			array(
-				  "ORDER BY" => "up_id ASC",
+				  "ORDER BY" => "up_id DESC",
 				  "LIMIT" => $limit
 			)
 		);
@@ -91,7 +91,7 @@ class WikiaReplicateImages {
 				 * just uploaded file
 				 */
 
-				Wikia::log( __CLASS__, "start", "====== copy up_id:{$Row->up_id} city_id:{$Row->up_city_id} path:{$Row->up_imgpath} created:{$Row->up_created} flags:{$Row->up_flags}" );
+				Wikia::log( __CLASS__, "start", "****** copy up_id:{$Row->up_id} city_id:{$Row->up_city_id} path:{$Row->up_imgpath} created:{$Row->up_created} flags:{$Row->up_flags}" );
 
 				foreach( $this->mServers as $name => $server ) {
 					/**
