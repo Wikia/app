c2cede647bb5df1d289fdac265404936d00e9ad4
eloy <eloy@wikia-inc.com>
2009-05-18 10:54:51 +0000

    CloseWiki: short script for comparing values of dbName
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8736 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiFactory/Close/comparevals.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @package MediaWiki
+ * @addtopackage maintenance
+ */
+
+ini_set( "include_path", dirname(__FILE__)."/../../../../maintenance/" );
+require_once( "commandLine.inc" );
+
+/**
+ * compare values stored in city_list and city_variables
+ */
+
+$dbr = wfGetDB( DB_SLAVE );
+$sth = $dbr->select(
+	WikiFactory::table("city_list"),
+	array( "city_dbname", "city_id" ),
+	false,
+	__FILE__,
+	array( "ORDER BY" => "city_id" )
+);
+
+while( $row = $dbr->fetchObject( $sth ) ) {
+	$variable = $dbr->selectRow(
+		WikiFactory::table( "city_variables" ),
+		array( "cv_value" ),
+		array(
+			"cv_city_id" => $row->city_id,
+			"cv_variable_id = (SELECT cv_id FROM city_variables_pool WHERE cv_name='wgDBname')"
+		),
+		__FILE__
+	);
+
+	if( !empty( $variable->cv_value ) ) {
+		if( unserialize( $variable->cv_value ) !== $row->city_dbname ) {
+			print "wgDBname different than city_dbname in city_id={$row->city_id}\n";
+		}
+	}
+	else {
+		print "wgDBname is not defined in city_id={$row->city_id}\n";
+	}
+}
