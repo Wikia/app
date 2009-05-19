aaaf385d0c3534bff85dc2130e89db6b211944ab
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-19 13:07:36 +0000

    WikiaIrcGateway: added optgroup handling and finishing touches (#544)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8798 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaIrcGateway/WikiaIrcGateway.php
+++ extensions/wikia/WikiaIrcGateway/WikiaIrcGateway.php
@@ -1,5 +1,28 @@
 <?php
+/**
+ * WikiaIrcGateway
+ *
+ * Allows users to add an IRC gateway login form to any article
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
+ * @date 2009-05-19
+ * @copyright Copyright © 2009 Łukasz Garczewski, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
 
+if( !defined( 'MEDIAWIKI' ) ) {
+        echo "This is a MediaWiki extension named WikiaIrcGateway.\n";
+        exit( 1 );
+}
+
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['parserhook'][] = array(
+	'name' => 'Wikia IRC Gateway',
+	'author' => "[http://www.wikia.com/wiki/User:TOR Łukasz 'TOR' Garczewski]",
+	'description' => 'Lets users insert IRC login form on any page'
+);
 
 $wgExtensionFunctions[] = "wfWikiaIrcGateway";
 
@@ -13,9 +36,8 @@ function printWikiaIrcGatewayLoginForm( $input, $argv ) {
 
 	wfLoadExtensionMessages('WikiaIrcGateway');
 
-$output = 
-'
-<form method="post" action="http://irc.wikia.com/irc.cgi" name="loginform" onsubmit="setjs();return nickvalid()">
+	$output = '<div id="ircform_container">
+<form id="ircform" method="post" action="http://irc.wikia.com/irc.cgi" name="loginform">
 	<input type="hidden" name="interface" value="nonjs">
 	<input type="hidden" name="Server" value="irc.freenode.net" disabled="1">
 	<table>
@@ -31,19 +53,25 @@ $output =
 			<td>
 				<select name="Channel">';
 
-$array = explode( "\n* ", wfMsgForContent('ircgate-channellist') );
+	$array = explode( "\n*", wfMsgForContent('ircgate-channellist') );
 
-foreach ( $array as $channel ) {
-	$output .= '<option>' . $channel . "</option>\n";
-}
+	foreach ( $array as $line ) {
+		if ( strpos( ltrim( $line, '* ' ), 'group: ' ) === 0 ) {
+			$output .= '<optgroup label="' . substr( ltrim( $line, '* ' ), 7 ) . '">';
+		} elseif ( strpos( ltrim( $line, '* ' ), 'group-end' ) === 0 ) {
+			$output .= "</optgroup>\n";
+		} else {
+			$output .= '<option>' . ltrim( $line, '* ') . "</option>\n";
+		}
+	}
 
-$output .= '				</select>
+	$output .= '			</select>
 			</td>
 		</tr>
 	</table>   
-</form>';
+</form>
+</div>';
 
-return $output;
+	return $output;
 
 }
-
