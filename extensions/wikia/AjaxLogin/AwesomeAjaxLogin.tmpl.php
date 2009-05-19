661cd7fd88ce5ff7f24a4266b9c82d3c8b0bc7e5
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-19 13:18:10 +0000

    AjaxLogin: change elements ID
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8802 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
@@ -1,18 +1,18 @@
 <?php
-global $wgAuth, $wgUser, $wgEnableEmail, $wgExtensionsPath, $wgStyleVersion;
+global $wgAuth, $wgUser, $wgEnableEmail;
 $titleObj = SpecialPage::getTitleFor( 'Userlogin' );
 $link = $titleObj->getLocalUrl('type=signup');
 ?>
 <div id="AjaxLogin" title="<?= wfMsg('login') ?>">
-	<form action="" method="post" name="userajaxloginform" id="userajaxloginform" style="margin:5px">
+	<form action="" method="post" style="margin:5px">
 		<div id="wpError"></div>
-		<label for="wpName1"><?= wfMsg("yourname") ?></label><br/>
-		<input type="text" class="loginText" name="wpName" id="wpName1" tabindex="101" size="20" /><br/>
-		<label for="wpPassword1"><?= wfMsg("yourpassword") ?></label><br/>
-		<input type="password" class="loginPassword" name="wpPassword" id="wpPassword1" tabindex="102" size="20" /><br/>
+		<label for="wpNameAjaxLogin"><?= wfMsg("yourname") ?></label><br/>
+		<input type="text" class="loginText" name="wpName" id="wpNameAjaxLogin" tabindex="101" size="20" /><br/>
+		<label for="wpPasswordAjaxLogin"><?= wfMsg("yourpassword") ?></label><br/>
+		<input type="password" class="loginPassword" name="wpPassword" id="wpPasswordAjaxLogin" tabindex="102" size="20" /><br/>
 		<div style="padding-bottom:3px">
-			<input type="checkbox" name="wpRemember" tabindex="104" value="1" id="wpRemember1" <?php if( $wgUser->getOption( 'rememberpassword' ) ) { ?>checked="checked"<?php } ?> />
-			<label for="wpRemember1"><?= wfMsg('remembermypassword') ?></label><br/>
+			<input type="checkbox" name="wpRemember" tabindex="104" value="1" id="wpRememberAjaxLogin" <?php if( $wgUser->getOption( 'rememberpassword' ) ) { ?>checked="checked"<?php } ?> />
+			<label for="wpRememberAjaxLogin"><?= wfMsg('remembermypassword') ?></label><br/>
 		</div>
 		<input style="margin:0;padding:0 .25em;width:auto;overflow:visible;" type="submit" name="wpLoginattempt" id="wpLoginattempt" tabindex="105" value="<?= wfMsg("login") ?>" onclick="AjaxLogin.action='login'" />
 <?php if( $wgEnableEmail && $wgAuth->allowPasswordChange() ) { ?>
@@ -22,9 +22,9 @@ $link = $titleObj->getLocalUrl('type=signup');
 	</form>
 </div>
 <script type="text/javascript">
-	$.getScript('<?= $wgExtensionsPath ?>/wikia/AjaxLogin/AwesomeAjaxLogin.js?<?= $wgStyleVersion ?>', function() {
+	$.getScript(wgExtensionsPath + '/wikia/AjaxLogin/AwesomeAjaxLogin.js?' + wgStyleVersion, function() {
 		AjaxLogin.init( $('#AjaxLogin form') );
 		$('#AjaxLogin').makeModal({width: 275});
-		$('#AjaxLogin').find('#wpName1').focus();
+		$('#wpNameAjaxLogin').focus();
 	});
 </script>
