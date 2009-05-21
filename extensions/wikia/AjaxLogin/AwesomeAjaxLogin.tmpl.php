a47e7bb0bfb3f2586858e105f040ac7b463964d5
macbre <macbre@wikia-inc.com>
2009-05-21 11:56:42 +0000

    AjaxLogin: revert r8890 as $.getScript is now fixed
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8898 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
@@ -1,5 +1,5 @@
 <?php
-global $wgAuth, $wgUser, $wgEnableEmail, $wgExtensionsPath, $wgStyleVersion;
+global $wgAuth, $wgUser, $wgEnableEmail;
 $titleObj = SpecialPage::getTitleFor( 'Userlogin' );
 $link = $titleObj->getLocalUrl('type=signup');
 ?>
@@ -18,7 +18,3 @@ $link = $titleObj->getLocalUrl('type=signup');
 	<br/><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>"><?= wfMsg('nologinlink') ?></a>
 	</form>
 </div>
-<script type="text/javascript" src="<?= $wgExtensionsPath ?>/wikia/AjaxLogin/AwesomeAjaxLogin.js?<?= $wgStyleVersion ?>"></script>
-<script type="text/javascript">
-	AjaxLogin.init($("#AjaxLoginBox form"));
-</script> 
