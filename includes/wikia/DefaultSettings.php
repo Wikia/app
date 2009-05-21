888af4b0b346e73ea9ae79d023e2a257679449f5
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-21 18:20:14 +0000

    (rt 15139) Remoe old skins from SkinChooser: it's a go.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8928 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -195,12 +195,12 @@ $wgSkipSkins = array(
 		'uncyclopedia',
 		'wowwiki',
 		'lostbook',
-#		'quartz',		# wait for rt#15139
-#		'cologneblue',	# wait for rt#15139
-#		'modern',		# wait for rt#15139
-#		'myskin',		# wait for rt#15139
-#		'simple',		# wait for rt#15139
-#		'standard',		# wait for rt#15139
+		'quartz',
+		'cologneblue',
+		'modern',
+		'myskin',
+		'simple',
+		'standard',
 		'awesome',
 );
 
