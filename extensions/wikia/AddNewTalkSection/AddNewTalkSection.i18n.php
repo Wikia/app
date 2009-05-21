a9826ad2192c93ad667adeb2244e65338af18a6c
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-21 13:11:26 +0000

    * AddNewTalkSection - new extension * change calling of CustomArticleFooter hook * changes in CustomArticleFooter.php to proper handling of new calling method
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8910 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AddNewTalkSection/AddNewTalkSection.i18n.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * AddNewTalkSection
+ *
+ * A AddNewTalkSection extension for MediaWiki
+ * Make long talk pages easier to use, by adding an "add new section" link to the page end.
+ *
+ * @author Maciej Błaszkowski (Marooned) <marooned at wikia-inc.com>
+ * @date 2009-05-19
+ * @copyright Copyright (C) 2009 Maciej Błaszkowski, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @package MediaWiki
+ *
+ * To activate this functionality, place this file in your extensions/
+ * subdirectory, and add the following line to LocalSettings.php:
+ *     require_once("$IP/extensions/wikia/AddNewTalkSection/AddNewTalkSection.php");
+ */
+
+if (!defined('MEDIAWIKI')) {
+	echo "This is MediaWiki extension named AddNewTalkSection.\n";
+	exit(1) ;
+}
+
+$messages = array(
+	'en' => array(
+		'addnewtalksection-link' => 'Add new section',
+//		'tog-disableaddnewtalksection' => 'Disable Add ...'
+	)
+);
