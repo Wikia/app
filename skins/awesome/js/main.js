2ccf80a5a023056933ba450245dcc04cc0b3504e
macbre <macbre@wikia-inc.com>
2009-05-21 09:18:19 +0000

    AjaxLogin: move JS to template to fix Opera problems with $.getScript()
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8890 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/awesome/js/main.js
+++ skins/awesome/js/main.js
@@ -202,17 +202,7 @@ function openLogin(event) {
 	}
 	else {
 		// make modal persistent, so it won't be removed from DOM
-		$().getModal(window.wgScript + '?action=ajax&rs=GetAjaxLogin&uselang=' + window.wgUserLanguage + '&cb=' + wgMWrevId + '-' + wgStyleVersion, '#AjaxLoginBox', 
-		{
-			width: 300,
-			persistent: true,
-			callback: function() {
-				$.getScript(wgExtensionsPath + '/wikia/AjaxLogin/AwesomeAjaxLogin.js?' + wgStyleVersion, function() {
-					$().log( AjaxLogin );
-					AjaxLogin.init( $('#AjaxLoginBox form') );
-				});
-			}
-		});
+		$().getModal(window.wgScript + '?action=ajax&rs=GetAjaxLogin&uselang=' + window.wgUserLanguage + '&cb=' + wgMWrevId + '-' + wgStyleVersion, '#AjaxLoginBox', {width: 300, persistent: true});
 	}
 }
 
