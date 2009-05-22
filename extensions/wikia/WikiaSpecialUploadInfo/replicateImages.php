b7d0b1bf71f66033ca66b302bbcb1e606eb75cae
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-22 13:18:41 +0000

    replicate images: maintenance script moved to extensions/wikia/WikiaSpecialUploadInfo directory
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8942 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaSpecialUploadInfo/replicateImages.php
+++ extensions/wikia/WikiaSpecialUploadInfo/replicateImages.php
@@ -8,7 +8,7 @@
  *
  */
 
-ini_set( "include_path", dirname(__FILE__)."/.." );
+ini_set( "include_path", dirname(__FILE__)."/../../../maintenance/" );
 require_once( 'commandLine.inc' );
 
 class WikiaReplicateImages {
