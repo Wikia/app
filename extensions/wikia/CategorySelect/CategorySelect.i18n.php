6727a2fd5b25377e6d0d59964e5e6101cc96dfaa
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-23 23:19:07 +0000

    (rt 15976) .fi messages for category select.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@8965 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CategorySelect/CategorySelect.i18n.php
+++ extensions/wikia/CategorySelect/CategorySelect.i18n.php
@@ -38,5 +38,21 @@ $messages = array(
 		'categoryselect-button-save' => 'Save',
 		'categoryselect-button-cancel' => 'Cancel',
 		'tog-disablecategoryselect' => 'Disable Category Tagging'
+	),
+	'fi' => array(
+		'categoryselect-code-view' => 'Näytä koodi',
+		'categoryselect-visual-view' => 'Näytä visuaalisena',
+		'categoryselect-infobox-caption' => 'Luokan asetukset',
+		'categoryselect-infobox-category' => 'Syötä luokan nimi:',
+		'categoryselect-infobox-sortkey' => 'Aakkosta tämä artikkeli "$1" luokkasivulle nimellä:',
+		'categoryselect-addcategory-button' => 'Lisää luokka',
+		'categoryselect-suggest-hint' => 'Paina Enter, kun olet valmis',
+		'categoryselect-tooltip' => "'''Uutuus!''' Luokan lisäystyökalurivi. Testaa sitä tai katso [[Ohje:CategorySelect|ohje]] saadaksesi lisätietoa.",
+		'categoryselect-unhandled-syntax' => 'Käsittelemätön syntaksi havaittu - visuaalisen moodin takaisin kytkentä on mahdotonta.',
+		'categoryselect-edit-summary' => 'Luokkien lisääminen',
+		'categoryselect-empty-name' => 'Syötä luokan nimi (osaa ennen |)',
+		'categoryselect-button-save' => 'Tallenna',
+		'categoryselect-button-cancel' => 'Peruuta',
+		'tog-disablecategoryselect' => 'peru luokkien lisäys'
 	)
 );
