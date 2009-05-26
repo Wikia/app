5dd1014f8baabbb125d81a95b3328b7e29e97720
macbre <macbre@wikia-inc.com>
2009-05-26 10:49:31 +0000

    Widgets: use WidgetFramework.update
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@9043 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WidgetFramework/Widgets/WidgetShoutBox/WidgetShoutBox.js
+++ extensions/wikia/WidgetFramework/Widgets/WidgetShoutBox/WidgetShoutBox.js
@@ -1,26 +1,12 @@
 function WidgetShoutBoxSend(widgetId) {
-
 	var messageBox = $('#widget_' + widgetId + '_message');
 
 	if (!messageBox.length || messageBox.attr('value') == '') {
 		return false;
 	}
 
-	// get message
-	message = encodeURIComponent( messageBox.attr('value') );
-
-	$('#widget_' + widgetId + '_content').html('').addClass('widget_loading').log('msg: ' + message, 'WidgetSlideshow');
-
-	$.getJSON(wgScript + '?action=ajax&rs=WidgetFrameworkAjax&actionType=configure&id='+widgetId+'&skin='+skin+'&message='+message, function(res) {
-		if(res.success) {
-			$('#widget_' + res.id +'_content').removeClass('widget_loading').html(res.body);
-			if(res.title) {
-				$('#widget_' + res.id +'_header').html(res.title);
-			}
-
-			// focus on message input
-			$('#widget_' + res.id + '_message').focus();
-		}
+	WidgetFramework.update(widgetId, {message: messageBox.attr('value')}, function(id, widget) {
+		$('#widget_' + id + '_message').focus();
 	});
 
 	return true;
@@ -34,17 +20,7 @@ function WidgetShoutBoxRemoveMsg(widgetId, aElem) {
 		return false;
 	}
 
-	$('#widget_' + widgetId + '_content').html('').addClass('widget_loading').log('removing msg #' + msgId, 'WidgetSlideshow');
-
-	$.getJSON(wgScript + '?action=ajax&rs=WidgetFrameworkAjax&actionType=configure&id='+widgetId+'&skin='+skin+'&msgid='+msgId, function(res) {
-		if(res.success) {
-			$('#widget_' + res.id +'_content').removeClass('widget_loading').html(res.body);
-			if(res.title) {
-				$('#widget_' + res.id +'_header').html(res.title);
-			}
-
-			// focus on message input
-			$('#widget_' + res.id + '_message').focus();
-		}
+	WidgetFramework.update(widgetId, {msgId: msgId}, function(id, widget) {
+		$('#widget_' + id + '_message').focus();
 	});
 }
