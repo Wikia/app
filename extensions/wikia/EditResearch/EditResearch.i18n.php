2e6374cdb05de1be959ce87efd720aee78eb83a5
emil <emil@wikia-inc.com>
2009-05-27 11:25:29 +0000

    move some messages from Answers.i18n.php
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@9099 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditResearch/EditResearch.i18n.php
+++ extensions/wikia/EditResearch/EditResearch.i18n.php
@@ -3,5 +3,7 @@ $messages = array();
 $messages['en'] = array(
 	'search_wikipedia' => 'Search Wikipedia',
 	'previous' => 'previous',
-	'research_no_results' => 'No pages found.  Please search again.'
+	'research_no_results' => 'No pages found.  Please search again.',
+	'research_wikipedia_title' => 'Research your answer on Wikipedia',
+	'research_this' => 'Research this',
 );
