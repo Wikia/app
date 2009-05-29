5bea44f4644d08a5709c04440060775c14a6cddf
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-05-29 21:45:01 +0000

    added category field to answers skin
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@9217 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/Answers.php
+++ skins/Answers.php
@@ -138,7 +138,8 @@ wfRunHooks('GetHTMLAfterBody', array (&$this));
 	
 		<div id="answers_ask">
 			<form method="get" action="" onsubmit="return false" name="ask_form" id="ask_form">
-				<input type="text" id="answers_ask_field" value="<?=htmlentities(wfMsg("ask_a_question"))?>" class="alt" /><span>?</span>
+				<input type="text" id="answers_ask_field" value="<?=htmlentities(wfMsg("ask_a_question"))?>" class="header_field alt" /><span>?</span>
+				<input type="text" id="answers_category_field" value="<?=htmlentities(wfMsg("in_category"))?>" class="header_field alt" />
 				<a href="javascript:void(0);" id="ask_button" class="huge_button huge_button_green"><div></div><?= wfMsg("ask_button") ?></a>
 			</form>
 		</div><?/*answers_ask*/?>
