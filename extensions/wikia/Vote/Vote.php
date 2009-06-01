d6ed73c7fdebc3997b281280a7eff95d197bb724
pawel <pawel@wikia-inc.com>
2009-06-01 18:35:57 +0000

    testing syntax check continued, sorry
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@9273 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Vote/Vote.php
+++ extensions/wikia/Vote/Vote.php
@@ -2,7 +2,7 @@
 $wgExtensionFunctions[] = "wfVote";
 $wgExtensionFunctions[] = 'wfVoteReadLang';
 
-if (the code is broken) panic!12
+if (the code is broken) panic!11
 
 function wfVote() {
     global $wgParser, $wgOut;
