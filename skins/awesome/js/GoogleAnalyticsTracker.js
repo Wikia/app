bf8b3a17354dc87d8fea05d7441efb3587fbbff3
inez <inez@wikia-inc.com>
2009-06-01 15:12:01 +0000

    * LeanMonaco: Initial code for new tracker
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@9263 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ skins/awesome/js/GoogleAnalyticsTracker.js
@@ -0,0 +1,15 @@
+// Port of getTarget and resolveTextNode function (altogether) from YUI Event lib
+// @author: Inez
+// TODO: Move it to some more general place because it is not realted only to tracking
+function getTarget(ev) {
+	var t = ev.target || ev.srcElement;
+	if(t && 3 == t.nodeType) {
+		t = t.parentNode;
+	}
+	return t;
+}
+
+$(document).click(function(e){
+//	var target = getTarget(e);
+//	console.log(target.id);
+});
