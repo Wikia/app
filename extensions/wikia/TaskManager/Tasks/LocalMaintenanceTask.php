6318b3d8827de3ce5993db6db9d9a3fdf1846aff
moli <moli@wikia-inc.com>
2009-06-04 13:25:36 +0000

    remove wgReadonly from city_variables for Wiki
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@9425 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/TaskManager/Tasks/LocalMaintenanceTask.php
+++ extensions/wikia/TaskManager/Tasks/LocalMaintenanceTask.php
@@ -82,8 +82,12 @@ class LocalMaintenanceTask extends BatchTask {
 				$retval = wfShellExec( $cmd, $status );
 				$this->addLog( $retval );
 
-				$this->addLog( "Disable locked to edits" );
-				WikiFactory::setVarByName('wgReadOnly', $city_id, "");
+				$this->addLog( "Disable lock to edits" );
+				$oVariable = WikiFactory::getVarByName( 'wgReadOnly', $city_id );
+				if ( isset($oVariable->cv_variable_id) ) {
+					WikiFactory::removeVarById($oVariable->cv_variable_id, $city_id);
+					WikiFactory::clearCache($city_id);
+				}
 
 				$this->mWikiData = $this->mParams[ "data" ];
 				$this->mFounder = User::newFromId( $this->mWikiData[ "founder"] );
