5b0829338c1ab6b6fd97cfc8977399f5b3112f6a
marooned <marooned@wikia-inc.com>
2009-06-10 10:54:57 +0000

    ForceSubjectForNewSection: rt#16474: new extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@9537 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ForceSubjectForNewSection/ForceSubjectForNewSection.i18n.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * ForceSubjectForNewSection
+ *
+ * A ForceSubjectForNewSection extension for MediaWiki
+ * Force subject on adding new section to talk page
+ *
+ * @author Maciej Błaszkowski (Marooned) <marooned at wikia-inc.com>
+ * @date 2009-06-09
+ * @copyright Copyright (C) 2009 Maciej Błaszkowski, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @package MediaWiki
+ *
+ * To activate this functionality, place this file in your extensions/
+ * subdirectory, and add the following line to LocalSettings.php:
+ *     require_once("$IP/extensions/wikia/ForceSubjectForNewSection/ForceSubjectForNewSection.php");
+ */
+
+if (!defined('MEDIAWIKI')) {
+	echo "This is MediaWiki extension named ForceSubjectForNewSection.\n";
+	exit(1) ;
+}
+
+$messages = array(
+	'en' => array(
+		'force-subject-for-new-section-message' => 'Please enter a headline for the section'
+	),
+	'pl' => array(
+		'force-subject-for-new-section-message' => 'Proszę wpisać temat dla sekcji'
+	)
+);
