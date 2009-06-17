a2bb2c1deb67d17d2db39cb6e1422e32c8c0dd2c
macbre <macbre@wikia-inc.com>
2009-06-17 07:59:05 +0000

    Wysiwyg: merge r9641 and r9686 to trunk
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@9706 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Wysiwyg/SpecialEnableRichTextEditor.php
@@ -0,0 +1,39 @@
+<?php
+
+$dir = dirname(__FILE__).'/';
+$wgExtensionMessagesFiles['Wysiwyg'] = $dir.'Wysiwyg.i18n.php';
+
+/*
+ * Special page to enable rich text editor
+*/
+$wgSpecialPages['EnableRichTextEditor'] = 'EnableRichTextEditor';
+class EnableRichTextEditor extends SpecialPage {
+	function __construct() {
+		parent::__construct('EnableRichTextEditor');
+		wfLoadExtensionMessages('Wysiwyg');
+	}
+
+	function execute( $par ) {
+		global $wgOut, $wgUser;
+
+		$this->setHeaders();
+
+		if ($wgUser->isAnon()) {
+			$this->displayRestrictionError();
+			return;
+		}
+
+		$wgOut->setPageTitle('Enable Rich Text Editor');
+
+		// set user option
+		$wgUser->setOption('enablerichtext', true);
+		$wgUser->saveSettings();
+
+		// commit
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->commit();
+
+		// show message
+		$wgOut->addWikiText( wfMsg('wysiwyg-enablerichtexteditormessage') );
+	}
+}
