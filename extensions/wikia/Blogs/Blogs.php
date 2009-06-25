63d68ebca142e0ad63a79796fc420b1694643988
adi <adi@wikia-inc.com>
2009-06-25 18:44:33 +0000

    small fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10050 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/Blogs.php
+++ extensions/wikia/Blogs/Blogs.php
@@ -114,7 +114,6 @@ include( dirname( __FILE__ ) . "/BlogTemplate.php");
 include( dirname( __FILE__ ) . "/BlogArticle.php");
 include( dirname( __FILE__ ) . "/BlogComments.php");
 include( dirname( __FILE__ ) . "/BlogLockdown.php");
-include_once( dirname( __FILE__ ) . "/BlogAvatar.php");
 
 /**
  * add task
