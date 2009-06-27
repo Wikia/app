30af1688ddaa0c5a499a678f392ad48fda1a8502
emil <emil@wikia-inc.com>
2009-06-27 09:00:35 +0000

    MediaWiki implementation of SuperDeduper (rt #17491)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10101 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SuperDeduper/SuperDeduper.php
@@ -0,0 +1,24 @@
+<?php
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
+	die( 1 );
+}
+
+$wgAutoloadClasses['SuperDeduper'] = '/usr/wikia/source/answers/SuperDeduper.php';
+$wgAutoloadClasses['AwesomeDeduper'] = '/usr/wikia/source/answers/AwesomeDeduper.php';
+$wgAutoloadClasses['ApiSuperDeduper'] = dirname(__FILE__) . '/ApiSuperDeduper.php';
+
+$wgAPIModules['superdeduper'] = 'ApiSuperDeduper';
+$wgAjaxExportList[] = 'efGetRankedMatches';
+
+function efGetRankedMatches() {
+	global $wgRequest;
+	$sd = new AwesomeDeduper( $wgRequest->getVal('lang'), $wgRequest->getVal('db') );
+	$results = $sd->getRankedmatches( $wgRequest->getVal('query'), $wgRequest->getVal('limit',10) );
+	$out = array();
+	foreach( $results as $title => $rank ) {
+		$out['ResultSet']['Result'][] = array( 'title' => $title, 'rank' => $rank );
+	}
+	return new AjaxResponse( json_encode( $out ) );
+}
