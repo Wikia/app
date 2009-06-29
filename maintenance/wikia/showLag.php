4b8f24fbfe34645c9703e0d61ac4934801c2ce3f
eloy <eloy@wikia-inc.com>
2009-06-29 21:45:41 +0000

    show socket data
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10155 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/showLag.php
+++ maintenance/wikia/showLag.php
@@ -3,4 +3,5 @@
 ini_set( "include_path", dirname(__FILE__)."/../" );
 require_once( 'commandLine.inc' );
 
+print_r( $wgMemc->get_sock( "db:lag_times:db4" ) );
 print_r( $wgMemc->get( "db:lag_times:db4" ) );
