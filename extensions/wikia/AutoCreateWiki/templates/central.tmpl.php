2d23dc9c3a85aec32580293305ead46682d4632f
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-06-29 12:35:32 +0000

    use $data['images_url'] instead of dir_part
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10129 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/templates/central.tmpl.php
+++ extensions/wikia/AutoCreateWiki/templates/central.tmpl.php
@@ -5,7 +5,7 @@
 | wiki title = <?php echo "{$data["title"]}\n" ?>
 | request name = <?php echo "{$data["subdomain"]}\n" ?>
 | wiki url = http://<?= $data["subdomain"] ?>.wikia.com
-| wiki logo = http://images.wikia.com/<?= $data["dir_part"] ?>/images/b/bc/Wiki.png
+| wiki logo = <?= $data[ "images_url" ] ?>/b/bc/Wiki.png
 | request description = <?php echo "{$data["title"]}\n" ?>
 | request created = <?=  $timestamp."\n" ?>
 | request category = <?php echo "{$category}\n" ?>
