e5b76959f58e3e721e7abd17a1daf9955e444706
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-06-29 10:53:30 +0000

    rt#18204: Prevent RandomWiki from showing closed wiki.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10124 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RandomWiki/SpecialRandomWiki_body.php
+++ extensions/wikia/RandomWiki/SpecialRandomWiki_body.php
@@ -34,27 +34,55 @@ class RandomWiki extends SpecialPage {
 	 * @param $par String: page name on target wiki
 	 */
 	public function execute( $par ) {
-		global $wgExternalStatsDB, $wgOut, $wgMemc;
+		global $wgExternalSharedDB, $wgExternalStatsDB, $wgOut, $wgMemc;
 
 		$topWikis = $wgMemc->get( self::cachekey );
 
 		if ( empty( $topWikis ) ) {
 
+			$two_days_ago = time() - 2 * 24 * 3600; // allow a day or two in case stats generation will be disrupted (or yikes! month ends)
+			$conds = array(
+				'cw_stats_date' => date('Ym00000000', $two_days_ago), // pick wiki with recent stats present => more likely to be live (but no guarantee till the end of the month)
+			);
 			$options = array(
-				'GROUP BY' => 'cw_city_id',
-				'ORDER BY' => 'sum(cw_article_count_link) DESC',
+				'ORDER BY' => 'cw_article_count_link DESC',
 				'LIMIT' => self::limit,
 			);
 
 
 			$dbs = wfGetDB( DB_SLAVE, array(), $wgExternalStatsDB );
 
-			$res = $dbs->select( 'city_stats_full', array( 'cw_city_id' ), '', __METHOD__, $options );
+			$res = $dbs->select( 'city_stats_full', array( 'cw_city_id' ), $conds, __METHOD__, $options );
 
+			$topWikis = array();
 			while ( $row = $dbs->fetchObject( $res ) )
 				$topWikis[] = $row->cw_city_id;
 			
-			$wgMemc->set( self::cachekey, $topWikis, 2592000 /* 30 days */ );
+			if (count($topWikis)) {
+				$db2 = wfGetDB( DB_SLAVE, array(), $wgExternalSharedDB );
+
+				// check if any wiki from that list above has been closed
+				// removing from list should be quicker than reproducing it,
+				// hence public=0 and array_diff
+				$conds2 = array(
+					'city_public' => 0,
+					'city_id IN (' . join(',', $topWikis) . ')',
+				);
+				$options2 = array(
+					'ORDER BY' => 'city_id',
+				);
+				$res2 = $db2->select('city_list', array('city_id'), $conds2, __METHOD__, $options2);
+				$closedWikis = array();
+				while ($row = $db2->fetchObject($res2)) {
+					$closedWikis[] = $row->city_id;
+				}
+
+				if (count($closedWikis)) {
+					$topWikis = array_diff($topWikis, $closedWikis);
+				}
+			}
+
+			$wgMemc->set( self::cachekey, $topWikis, 24 * 3600 ); // cache it for 24h only, wiki can be closed at any time
 		}
 
 		$rand = array_rand( $topWikis );
