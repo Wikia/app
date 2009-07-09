cb2cf5d6d9691a792db9c41fef811cd458419a40
macbre <macbre@wikia-inc.com>
2009-07-09 10:10:27 +0000

    Wysiwyg: disable Opera/Chrome/Safari
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10466 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Wysiwyg/fckeditor/fckeditor_php5.php
+++ extensions/wikia/Wysiwyg/fckeditor/fckeditor_php5.php
@@ -49,7 +49,7 @@ function FCKeditor_IsCompatibleBrowser()
 
 	if ( strpos($sAgent, 'Chrome') !== false )
 	{
-		return true;
+		return false;
 	}
 	else if ( strpos($sAgent, 'MSIE') !== false && strpos($sAgent, 'mac') === false && strpos($sAgent, 'Opera') === false )
 	{
@@ -61,6 +61,7 @@ function FCKeditor_IsCompatibleBrowser()
 		$iVersion = (int)substr($sAgent, strpos($sAgent, 'Gecko/') + 6, 8) ;
 		return ($iVersion >= 20030210) ;
 	}
+/*
 	else if ( strpos($sAgent, 'Opera/') !== false )
 	{
 		$fVersion = (float)substr($sAgent, strpos($sAgent, 'Opera/') + 6, 4) ;
@@ -71,6 +72,7 @@ function FCKeditor_IsCompatibleBrowser()
 		$iVersion = $matches[1] ;
 		return ( $matches[1] >= 522 ) ;
 	}
+*/
 	else
 		return false ;
 }
