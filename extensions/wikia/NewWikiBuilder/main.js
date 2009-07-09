76f7f822705e6ddd92cd14555c2daa37277ddec1
nick <nick@wikia-inc.com>
2009-07-09 06:19:13 +0000

    Preview works
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10461 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewWikiBuilder/main.js
+++ extensions/wikia/NewWikiBuilder/main.js
@@ -7,4 +7,33 @@ NewWikiBuilder.handleError = function(e){
 	alert(Mediawiki.print_r(e));
 };
 
+NewWikiBuilder.iframeFormUpload = function(iframe){
+	var d;
+	// Different browsers have different ways of getting the iframe's document
+	if (iframe.contentDocument) {
+		d = iframe.contentDocument;
+	} else if (iframe.contentWindow) {
+		d = iframe.contentWindow.document;
+	} else {
+		d = window.frames[iframe.id].document;
+	}
+
+	// Bail if it loaded
+	if (d.location.href == "about:blank") {
+		return;
+	}
+
+	Mediawiki.updateStatus('Image Uploaded');
+
+	// Fill in the preview or the current depending on what was clicked
+	var url;
+	if ($("#logo_article").val() == "Wiki-Preview.png"){
+		url = Mediawiki.getImageUrl("Wiki-Preview.png") + '?' + Math.random();
+		$("#logo_preview").css("backgroundImage", "url(" + url + ")");
+	} else {
+		url = Mediawiki.getImageUrl("Wiki.png") + '?' + Math.random();
+		$("#logo_current").css("backgroundImage", "url(" + url + ")");
+	}
+};
+
 NewWikiBuilder.updateStatus = Mediawiki.updateStatus;
