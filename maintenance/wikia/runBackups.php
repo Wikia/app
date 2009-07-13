d21090e962d06c33ca864a8805cd88c3be622797
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-13 13:26:19 +0000

    full/current backup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10568 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/runBackups.php
+++ maintenance/wikia/runBackups.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * migrateUploadPath -- migrate images to new directory layout
- *
  * @addto maintenance
  * @author Krzysztof Krzyżaniak (eloy)
  *
@@ -9,48 +7,106 @@
 ini_set( "include_path", dirname(__FILE__)."/.." );
 require_once('commandLine.inc');
 
+
 /**
- * connect to WF db
+ * run backup for range of wikis
  */
-$variable = "wgServer";
-$dbw = Wikifactory::db( DB_MASTER );
-$sth = $dbw->select(
-		    array( "city_variables", "city_list" ),
-		array( "*" ),
-		array(
-			"cv_variable_id = (SELECT cv_id FROM city_variables_pool WHERE cv_name = '{$variable}')",
-			"city_public=1",
-			"city_id = cv_city_id"
-		),
-		__METHOD__
-);
-while( $row = $dbw->fetchObject( $sth ) ) {
-	$before = unserialize( $row->cv_value );
+function runBackups( $from, $to, $full ) {
 
-	$parts = explode( "/", $before );
-	array_shift( $parts ); array_shift( $parts );
-	$domain = array_shift( $parts );
+	global $IP, $wgWikiaLocalSettingsPath, $wgWikiaAdminSettingsPath;
 
-	$match = $dbw->selectRow(
-		array( "city_domains" ),
-		array( "count(*) as count" ),
-		array(
-			"city_id" => $row->cv_city_id,
-			"city_domain" => $domain
-		),
-		__METHOD__
+	$range = array();
+	if( $from !== false && $to !== false ) {
+		$range = array( sprintf( "city_id >= %d AND city_id < %d", $from, $to ) );
+		Wikia::log( __METHOD__, "info", "Running from {$from} to {$to}" );
+	}
+	else {
+		Wikia::log( __METHOD__, "info", "Running for all wikis" );
+	}
+	$dbw = Wikifactory::db( DB_MASTER );
+
+	$sth = $dbw->select(
+			array( "city_list" ),
+			array( "city_id", "city_dbname" ), # , "city_cluster"
+			array( "city_public=1" ) + $range ,
+			__METHOD__,
+			array( "ORDER BY" => "city_id" )
 	);
+	while( $row = $dbw->fetchObject( $sth ) ) {
+		Wikia::log( __METHOD__, "info", "{$row->city_id} {$row->city_dbname}");
 
-	if( empty( $match->count ) ) {
-		print "{$row->cv_city_id} {$before} is wrong ";
-		$parts = explode( ".", $domain );
-		$tmp = $parts[ 0 ];
-		$parts[ 0 ] = $parts[ 1 ];
-		$parts[ 1 ] = $tmp;
-		$after = implode( ".", $parts );
-		$after = "http://" . $after;
-		print "it should be {$after}\n";
-#		WikiFactory::setVarByName( $variable, $row->cv_city_id, $after );
-#		WikiFactory::clearCache( $row->city_id );
+		/**
+		 * build command
+		 */
+		$status = false;
+		if( $full ) {
+			$path = sprintf("%s/pages_full.xml.gz", getDirectory( $row->city_dbname ) );
+			$cmd = wfEscapeShellArg(
+				"SERVER_ID={$row->city_id}",
+				"php",
+				"{$IP}/maintenance/dumpBackup.php",
+				"--conf {$wgWikiaLocalSettingsPath}",
+				"--aconf {$wgWikiaAdminSettingsPath}",
+				"--full",
+				"--xml",
+				"--quiet",
+				"--output=gzip:{$path}"
+			);
+			 wfShellExec( $cmd, $status );
+		}
+		else {
+			$path = sprintf("%s/pages_current.xml.gz", getDirectory( $row->city_dbname ) );
+			$cmd = "SERVER_ID={$row->city_id}" . " " .wfEscapeShellArg(
+				"php",
+				"{$IP}/maintenance/dumpBackup.php",
+				"--conf {$wgWikiaLocalSettingsPath}",
+				"--aconf {$wgWikiaAdminSettingsPath}",
+				"--current",
+				"--xml",
+				"--quiet",
+				"--output=gzip:{$path}"
+			);
+			wfShellExec( $cmd, $status );
+		}
+		if( $status ) {
+			/**
+			 * update city_list table
+			 */
+		}
 	}
 }
+
+/**
+ * dump directory is created as
+ *
+ * <root>/<first letter>/<two first letters>/<database>
+ */
+function getDirectory( $database ) {
+	global $wgDevelEnvironment;
+	$dumpDirectory = empty( $wgDevelEnvironment ) ?  "/opt/dbdumps" : "/tmp/dumps";
+	$database = strtolower( $database );
+	$directory = sprintf(
+		"%s/%s/%s/%s",
+		$dumpDirectory,
+		substr( $database, 0, 1),
+		substr( $database, 0, 2),
+		$database
+	);
+	if( !is_dir( $directory ) ) {
+		Wikia::log( __METHOD__ , "dir", "create {$directory}" );
+		wfMkdirParents( $directory );
+	}
+
+	return $directory;
+}
+
+
+/**
+ * main part
+ */
+$optionsWithArgs = array( 'from', 'to' );
+$from = isset( $options[ "from" ] ) ? $options[ "from" ] : false;
+$to   = isset( $options[ "to" ] ) ? $options[ "to" ] : false;
+$full = isset( $options[ "full" ] ) ? true : false;
+
+runBackups( $from, $to, $full );
