e3e33071b1a7eeeb6d892bff7dcd71c850f7b872
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-14 13:55:55 +0000

    NewWikiBuilder: correct user permissions check
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10616 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewWikiBuilder/NewWikiBuilder.php
+++ extensions/wikia/NewWikiBuilder/NewWikiBuilder.php
@@ -1,6 +1,13 @@
 <?php
 // TODO extension credits
 
+// New user right, required to use the extension.
+$wgAvailableRights[] = 'newwikibuilder';
+$wgGroupPermissions['*']['newwikibuilder'] = false;
+$wgGroupPermissions['sysop']['newwikibuilder'] = true;
+$wgGroupPermissions['bureaucrat']['newwikibuilder'] = true;
+$wgGroupPermissions['staff']['newwikibuilder'] = true;
+
 $wgAutoloadClasses['ApiUploadLogo'] = dirname(__FILE__) . '/ApiUploadLogo.php';
 $wgAPIModules['uploadlogo'] = 'ApiUploadLogo';
 
