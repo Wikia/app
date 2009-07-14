935fa4477dc7ba97ef9043892bff3aefc27262f0
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-14 11:48:28 +0000

    * Add display event tracking for WError and MWException
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10608 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/Exception.php
+++ includes/Exception.php
@@ -212,6 +212,20 @@ class MWException extends Exception {
 	 * print the end of the html page if not using $wgOut.
 	 */
 	function htmlFooter() {
+		$js = <<<EOD
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-9713682-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<script type="text/javascript">
+pageTracker._trackEvent("MWException", "Display" , "{$_SERVER['REQUEST_URI']}");
+</script>
+EOD;
 		echo "</body></html>";
 	}
 }
