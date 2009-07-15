676d7938bda3863c65fddd517213f7a1436fe5f3
nick <nick@wikia-inc.com>
2009-07-15 21:49:09 +0000

    Temporary hack to support awesome on the pages on the live site that hve it cached
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10691 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaticChute/index.php
+++ extensions/wikia/StaticChute/index.php
@@ -2,6 +2,8 @@
 require 'StaticChute.php';
 
 $StaticChute = new StaticChute($_GET['type']);
+// Temporary hack for transition
+$_GET['packages'] = preg_replace('/awesome/', 'monaco', @$_GET['packages']);
 $files = $StaticChute->getFileList($_GET);
 if (empty($files)){
 	header('HTTP/1.0 400 Bad Request');
