cc0a8ada07a932cc1e4ec98e2a96671006e2f302
macbre <macbre@wikia-inc.com>
2009-07-16 09:34:36 +0000

    StaticChute: use faster str_replace instead of preg_replace
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10707 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaticChute/index.php
+++ extensions/wikia/StaticChute/index.php
@@ -2,8 +2,12 @@
 require 'StaticChute.php';
 
 $StaticChute = new StaticChute($_GET['type']);
+
 // Temporary hack for transition
-$_GET['packages'] = preg_replace('/awesome/', 'monaco', @$_GET['packages']);
+if (isset($_GET['packages'])) {
+	$_GET['packages'] = str_replace('awesome', 'monaco', $_GET['packages']);
+}
+
 $files = $StaticChute->getFileList($_GET);
 if (empty($files)){
 	header('HTTP/1.0 400 Bad Request');
