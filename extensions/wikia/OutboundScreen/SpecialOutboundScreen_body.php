850912b4244ce81bf416fd3f4edb9eb7383fa4fa
macbre <macbre@wikia-inc.com>
2009-07-17 13:17:01 +0000

    OutboundScreen:  * fix RT #18845  * general HTML/CSS/JS and code cleanup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10783 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/OutboundScreen/SpecialOutboundScreen_body.php
+++ extensions/wikia/OutboundScreen/SpecialOutboundScreen_body.php
@@ -26,17 +26,53 @@ class Outbound extends UnlistedSpecialPage {
 			return true;
 		}
 
+		// output only template content
+		$wgOut->setArticleBodyOnly(true);
+
+		$skin = $wgUser->getSkin();
+		$skinName = get_class($skin);
+
+		// this may not be set yet
+		if (empty($skin->skinname)) {
+			$skin->skinname = substr($skinName, 4);
+		}
+
+		// add MW CSS
+		$skin->setupUserCss($wgOut);
+
+		// Monaco themes
+		if ($skinName == 'SkinMonaco' && !empty($skin->themename)) {
+			switch($skin->themename) {
+				case 'custom':
+					break;
+
+				case 'sapphire':
+					$wgOut->addStyle('monaco/css/root.css');
+					break;
+
+				default:
+					$wgOut->addStyle('monaco/' . $skin->themename . '/css/main.css');
+					break;
+			}
+		}
+
+		// render <link> tags
+		$css = $wgOut->buildCssLinks();
+
+		// render template
 		$oTmpl = new EasyTemplate( dirname( __FILE__ ) . "/templates/" );
 
 		$oTmpl->set_vars(
 				array(
 					'url' => $url,
+					'css' => $css,
 					'redirectDelay' => $this->redirectDelay,
-					'imagesPath' => ($wgExtensionsPath . '/wikia/OutboundScreen/images')
-					)
-			);
-		echo $oTmpl->execute('page');
-		exit;
-	}
+					'imagesPath' => $wgExtensionsPath . '/wikia/OutboundScreen/images',
+				)
+		);
 
+		// just output content of template
+		$wgOut->clearHTML();
+		$wgOut->addHTML( $oTmpl->execute('page') );
+	}
 }
