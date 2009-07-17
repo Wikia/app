6e735a706c6f2341e3ded8d9c63b25bda6ded911
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-17 13:17:01 +0000

    OutboundScreen:  * fix RT #18845  * general HTML/CSS/JS and code cleanup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10783 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/OutboundScreen/templates/page.tmpl.php
+++ extensions/wikia/OutboundScreen/templates/page.tmpl.php
@@ -1,64 +1,56 @@
-<!-- s:<?= __FILE__ ?> -->
-<html>
-<?php if($redirectDelay > 0): ?>
-<script type="text/javascript">
-<!--
-function doRedirect(){
-	window.location = "<?=$url;?>";
-}
-//-->
-</script>
-<body onLoad="setTimeout('doRedirect()', <?=($redirectDelay * 1000);?>)" style="margin-top: 0px;">
-<?php else: ?>
-<body style="margin-top: 0px;">
-<?php endif; ?>
-<style type="text/css">
-<!--
-#pageTop {
-	width: 100%;
-	height: 100%;
-	height: 24px;
-	background: #303030;
-	text-align: center;
-}
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xml:lang="en" lang="en" dir="ltr">
+	<head>
+		<title></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<style type="text/css">
+			body {
+				margin: 0;
+				padding: 0;
+			}
+			#pageTop {
+				background-image: url('<?=$imagesPath?>/wikia_logo_tiny.png');
+				background-repeat: no-repeat;
+				background-position: 5px 50%;
+				padding: 5px;
+				text-align: center;
+			}
 
-#exitLinkText {
-	padding-top: 6px;
-}
+			#exitLink {
+				color: white;
+				text-decoration: none;
+				font-size: 12px;
+				font-family: Verdana, Arial, sans-serif;
+			}
 
-#exitLink {
-	color: white;
-	text-decoration: none;
-	font-size: 12px;
-	font-family: verdana;
-}
+			#exitPageAd {
+				text-align: center;
+				padding: 5px;
+			}
+		</style>
+		<?= $css ?>
 
-#exitPageAd {
-	text-align: center;
-	padding: 5px;
-}
--->
-</style>
-	<div>
-		<div id="pageTop">
-			<img src="<?=$imagesPath?>/wikia_logo_tiny.png" align="left"/>
-			<span id="exitLinkText"><a href="<?=$url;?>" id="exitLink" rel="nofollow"><?=($redirectDelay > 0) ? wfMsgForContent('outbound-screen-text-with-redirect', $redirectDelay) : wfMsgForContent('outbound-screen-text');?></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+		<script type="text/javascript">/*<![CDATA[*/
+			function doRedirect() {
+				window.location = <?= Xml::encodeJSvar($url) ?>;
+			}
+		/*]]>*/</script>
+	</head>
+	<body class="color2"<?php if($redirectDelay > 0): ?> onLoad="setTimeout(doRedirect, <?=($redirectDelay * 1000);?>)"<?php endif?>>
+		<div>
+			<div id="pageTop" class="color1"><a href="<?= htmlspecialchars($url); ?>" id="exitLink" rel="nofollow"><?=($redirectDelay > 0) ? wfMsgForContent('outbound-screen-text-with-redirect', $redirectDelay) : wfMsgForContent('outbound-screen-text');?></a></div>
+			<div id="exitPageAd">
+				<script type="text/javascript">/*<![CDATA[*/
+					google_ad_client = "pub-4086838842346968";
+					/* Exit page ad */
+					google_ad_slot = "0549965769";
+					google_ad_width = 336;
+					google_ad_height = 280;
+					google_page_url = "http://gamergear.wikia.com/wiki/GamerGear_Wiki";
+				/*]]>*/</script>
+				<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+				</script>
+			</div>
 		</div>
-		<div id="exitPageAd">
-			<script type="text/javascript"><!--
-			google_ad_client = "pub-4086838842346968";
-			/* Exit page ad */
-			google_ad_slot = "0549965769";
-			google_ad_width = 336;
-			google_ad_height = 280;
-			google_page_url = "http://gamergear.wikia.com/wiki/GamerGear_Wiki";
-			//-->
-			</script>
-			<script type="text/javascript"
-			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-			</script>
-		</div>
-	</div>
-</body>
+	</body>
 </html>
-<!-- e:<?= __FILE__ ?> -->
