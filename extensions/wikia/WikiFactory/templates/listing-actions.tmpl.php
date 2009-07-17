71149d6797955ef3c7ea986225f28bdbd2c4148d
eloy <eloy@wikia-inc.com>
2009-07-17 15:17:18 +0000

    result listing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10792 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/templates/listing-actions.tmpl.php
+++
@@ -1,6 +0,0 @@
-<form action="<?php echo $closeTitle->getFullUrl() ?>" method="post">
-<div style="padding: 8pt;">
-	<input type="submit" name="submit0" value="Close" />
-	<input type="submit" name="submit1" value="Close and Redirect" />
-	<input type="submit" name="submit2" value="Close and Delete" />
-</div>
