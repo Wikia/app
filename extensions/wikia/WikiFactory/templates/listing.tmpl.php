4e0457cc08bf1753522da5ad2bdeb56e24a11778
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-20 12:45:55 +0000

    add language to listing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10829 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/templates/listing.tmpl.php
+++ extensions/wikia/WikiFactory/templates/listing.tmpl.php
@@ -12,6 +12,7 @@ foreach( range( 1, $limit) as $cnt ):
 <div class="wf-list">
 	<h4><a href="<?php echo $title->getFullUrl() . "/{$row->wiki->city_id}" ?>">
 		<?php CityListPager::bold( $row->wiki->city_title, $part )?>
+		<span style="font-size: smaller">language: <?php echo $row->wiki->city_lang ?></span>
 	</a></h4>
 	<?php echo empty( $row->wiki->city_description ) ? "no description" : $row->wiki->city_description ?>
 	<br />
