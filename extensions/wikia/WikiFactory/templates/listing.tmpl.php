05f86170ea07f6adc8e24bbe5b23c0d3ce885792
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-20 12:37:04 +0000

    WF listing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10828 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/templates/listing.tmpl.php
+++ extensions/wikia/WikiFactory/templates/listing.tmpl.php
@@ -1,14 +1,27 @@
+<!-- s:<?= __FILE__ ?> -->
+<style type="text/css">
+/*<![CDATA[*/
+div.wf-list { text-align: left; margin-bottom: 1em;}
+/*]]>*/
+</style>
 <form action="" method="get">
-<?php foreach( range( 1, $limit) as $cnt ):
+<?php
+foreach( range( 1, $limit) as $cnt ):
 	$row = array_shift( $data )
 ?>
-<div>
+<div class="wf-list">
 	<h4><a href="<?php echo $title->getFullUrl() . "/{$row->wiki->city_id}" ?>">
-		<?php echo $row->wiki->city_title ?>
+		<?php CityListPager::bold( $row->wiki->city_title, $part )?>
 	</a></h4>
-	<?php echo empty( $row->wiki->city_description) ? "no description" : $row->wiki->city_description ?>
+	<?php echo empty( $row->wiki->city_description ) ? "no description" : $row->wiki->city_description ?>
 	<br />
-	<em>Used domains: <?php echo implode ( ", ", $row->domains ) ?></em>
+	<em>Used domains: <?php echo CityListPager::bold( implode ( ", ", $row->domains ), $part ) ?></em>
+	<br />
+	<span style="color: darkgreen;"><?php CityListPager::bold( $row->wiki->city_url, $part ) ?></a></span>
+	 - <a href="<?php echo $title->getFullUrl() . "/{$row->wiki->city_id}" ?>">wikifactory</a>
+	 - <a href="<?php echo $row->wiki->city_url ?>">link</a>
 </div
-<?php endforeach ?>
+<?php
+endforeach
+?>
 </form
