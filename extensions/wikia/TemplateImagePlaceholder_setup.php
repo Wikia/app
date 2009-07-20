135e5ab2a8e09b41908f1cc701a49d455a5571b0
bartek <bartek@wikia-inc.com>
2009-07-20 09:15:06 +0000

    TIP: adding the file here
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10814 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/TemplateImagePlaceholder_setup.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * @author Bartek Łapiński
+ */
+
+if(!defined('MEDIAWIKI')) {
+        exit(1);
+}
+
+$wgExtensionCredits['other'][] = array(
+        'name' => 'Template Image Placeholder',
+        'author' => 'Bartek Łapiński',
+        'version' => '0.11',
+);
+
