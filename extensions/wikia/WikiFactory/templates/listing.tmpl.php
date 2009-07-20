5ca55abcfe31acceb1f4f3cb9a1f5eb849f8f598
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-20 11:04:58 +0000

    WF: listing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10826 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/templates/listing.tmpl.php
+++ extensions/wikia/WikiFactory/templates/listing.tmpl.php
@@ -1,3 +1,14 @@
 <form action="" method="get">
-<?php print_pre( $data ) ?>
+<?php foreach( range( 1, $limit) as $cnt ):
+	$row = array_shift( $data )
+?>
+<div>
+	<h4><a href="<?php echo $title->getFullUrl() . "/{$row->wiki->city_id}" ?>">
+		<?php echo $row->wiki->city_title ?>
+	</a></h4>
+	<?php echo empty( $row->wiki->city_description) ? "no description" : $row->wiki->city_description ?>
+	<br />
+	<em>Used domains: <?php echo implode ( ", ", $row->domains ) ?></em>
+</div
+<?php endforeach ?>
 </form
