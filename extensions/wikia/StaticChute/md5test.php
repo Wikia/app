a9f6e9174d7fa0f1425feb95314fe77d627b478c
nick <nick@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-22 20:50:42 +0000

    No longer needed, bug found
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10928 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaticChute/md5test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'StaticChute.php';
-
-$StaticChute = new StaticChute('css');
-foreach(array('monaco_css', 'monaco_css_print') as $package){
-	$files = $StaticChute->getFileList(array('packages'=> $package));
-	echo "checksum for $package is " . $StaticChute->getChecksum($files) . ", files are \n";
-}
