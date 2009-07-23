e970d2325a91146ee34d3a7cf283b7b439b2509d
nick <nick@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-23 01:06:35 +0000

    Minor i18n work
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10938 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewWikiBuilder/NewWikiBuilder.i18n.php
+++ extensions/wikia/NewWikiBuilder/NewWikiBuilder.i18n.php
@@ -20,7 +20,8 @@ $messages['en'] = array(
 	  "logging-in" => "Logging in...",
 	  "api-error" => "There was a problem: ",
 	  "no-more-pages" => "No more pages can be created",
-	  "must-be-logged-in" => "You must be logged in for this action"
+	  "must-be-logged-in" => "You must be logged in for this action",
+	  "skip-this-step" => "Skip this step"
 );
 
 // Note that this variable is referenced in the NewWikiBuilder.html.php file
