a0ea52b7c48b14d9458e861feab8b9d1c2bd35ec
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-23 08:48:21 +0000

    rt#19518: Analytics: Fixed re-broken masthead tracking.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10945 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/UserMasthead.tmpl.php
+++ extensions/wikia/Blogs/templates/UserMasthead.tmpl.php
@@ -29,12 +29,8 @@ if ($wgTitle == 'Special:Watchlist') {
 			<?
 			global $wgStylePath;
 			foreach( $data['nav_links_head'] as $navLink ) {
-				$tracker = $navLink['href'];
-				$tracker = preg_replace('|^/wiki/(.*)[:/][^:/]+$|', '\1', $tracker); // /wiki/User:REMOVE but /wiki/Special:Contributions/REMOVE
-				$tracker = preg_replace('/[^a-z]/i', '', $tracker);
-				$tracker = strtolower($tracker);
 				$id = strtolower($navLink['dbkey']);
-				echo '<li id="mt_' . $id . '"><a id="mt_' . $id . '_icon" href="'. $navLink['href'] .'" onclick="WET.byStr(\'usermasthead/' . $tracker . '\')"><img id="mt_' . $id . '_img" class="sprite" src="' . $wgStylePath . '/monobook/blank.gif" alt="'. $navLink['text'] .'"/></a> <div><a id="mt_' . $id . '_link" href="'. $navLink['href'] .'" onclick="WET.byStr(\'usermasthead/' . $tracker . '\')">'. $navLink['text'] .'</a></div></li>';
+				echo '<li id="mt_' . $id . '"><a id="mt_' . $id . '_icon" href="'. $navLink['href'] .'" onclick="WET.byStr(\'usermasthead/' . $navLink['tracker'] . '\')"><img id="mt_' . $id . '_img" class="sprite" src="' . $wgStylePath . '/monobook/blank.gif" alt="'. $navLink['text'] .'"/></a> <div><a id="mt_' . $id . '_link" href="'. $navLink['href'] .'" onclick="WET.byStr(\'usermasthead/' . $navLink['tracker'] . '\')">'. $navLink['text'] .'</a></div></li>';
 			}
 			?>
 		</ul>
@@ -42,11 +38,7 @@ if ($wgTitle == 'Special:Watchlist') {
 	<ul class="nav_links">
 		<?
 		foreach( $data['nav_links'] as $navLink ) {
-			$tracker = $navLink['href'];
-			$tracker = preg_replace('|^/wiki/(.*)[:/][^:/]+$|', '\1', $tracker); // /wiki/User:REMOVE but /wiki/Special:Contributions/REMOVE
-			$tracker = preg_replace('/[^a-z]/i', '', $tracker);
-			$tracker = strtolower($tracker);
-			echo "<li ". ( ( $current  == $navLink[ "dbkey" ]) ? 'class="selected">' : ">" ) . '<a href="'. $navLink['href'] .'" onclick="WET.byStr(\'usermasthead/' . $tracker . '\')">'. $navLink['text'] .'</a></li>';
+			echo "<li ". ( ( $current  == $navLink[ "dbkey" ]) ? 'class="selected">' : ">" ) . '<a href="'. $navLink['href'] .'" onclick="WET.byStr(\'usermasthead/' . $navLink['tracker'] . '\')">'. $navLink['text'] .'</a></li>';
 		}
 		?>
 	</ul>
