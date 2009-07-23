f495f4b43356fea47ac558ed1e1466fb3ade1656
eloy <eloy@wikia-inc.com>
2009-07-23 14:31:24 +0000

    remove JSONProfile from dependency
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10969 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserBulletin/UserBulletin_AjaxFunctions.php
+++ extensions/wikia/UserBulletin/UserBulletin_AjaxFunctions.php
@@ -2,35 +2,29 @@
 /*
  * Ajax Functions used by Wikia extensions
  */
- 
-
 
 $wgAjaxExportList [] = 'wfDeleteUserBulletin';
-function wfDeleteUserBulletin($ub_id){  
+$wgAjaxExportList [] = 'wfGetUserBulletinsJSON';
+
+function wfDeleteUserBulletin($ub_id){
 	$b = new UserBulletin();
 	$b->deleteBulletin($ub_id);
-	
-	return "ok";
 
+	return "ok";
 }
 
-$wgAjaxExportList [] = 'wfGetUserBulletinsJSON';
-function wfGetUserBulletinsJSON($user_name, $count=25){  
-	global $IP;
-	
-	require_once( "$IP/extensions/wikia/JSONProfile/JSON.php" );
-	$json = new Services_JSON();
-	
+function wfGetUserBulletinsJSON($user_name, $count=25){
+
+	wfProfileIn( __METHOD__ );
 	$b = new UserBulletinList( $user_name );
 	$bulletins = $b->getList("",$count);
-				
+
 	$profile_JSON_array["activity"] = array(
 			"time" => time(),
 			"title"=>"Recent Activity",
 			"activity"=>$bulletins,
-	);	
-	
-	return "var json_bulletins=" . $json->encode($profile_JSON_array) . ";\n\nwrite_activity(json_bulletins);";
+	);
+	wfProfileOut( __METHOD__ );
 
+	return "var json_bulletins=" . Wikia::json_encode( $profile_JSON_array ) . ";\n\nwrite_activity(json_bulletins);";
 }
-?>
