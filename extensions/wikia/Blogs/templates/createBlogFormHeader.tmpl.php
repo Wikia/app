d39b738210bf34b285c03af150274b54fd27fe01
adi <adi@wikia-inc.com>
2009-07-23 10:58:22 +0000

    full Wysiwyg + Category Select + Edit Enhancements support for Blogs (#18224)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10956 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/createBlogFormHeader.tmpl.php
+++ extensions/wikia/Blogs/templates/createBlogFormHeader.tmpl.php
@@ -1,10 +1,9 @@
 <!-- s:<?php echo __FILE__ ?> -->
-
 <strong><?php echo wfMsg( "create-blog-form-title" ) ?></strong><br />
 <?php echo wfMsg( "create-blog-form-info" ) ?>
 <br />
 <br />
-<form name="blogPostForm" id="blogPostForm" method="post" action="<?php echo $title->getLocalUrl();?>">
+<div id="blogPostForm">
 	<input type="hidden" name="articleEditAllowed" value="<?php echo isset($formData['isExistingArticleEditAllowed']) ? $formData['isExistingArticleEditAllowed'] : "0"; ?>" />
 	<?php if(!empty($preview)): ?>
 		<h2>Preview</h2>
@@ -35,28 +34,5 @@
 			<input type="checkbox" name="blogPostIsCommentingEnabled" value="1" <?php echo (!empty($formData['isCommentingEnabled']) || !isset($formData['isCommentingEnabled'])) ? "checked" : ""; ?> /><?php echo wfMsg("blog-comments-label") ?>
 		</div>
 	</div>
-	<div class="formBlock" id="editform">
-		<label><?php echo wfMsg( "create-blog-form-post-text" ) ?></label>
-		<div class="display: block">
-			<?php
-				echo EditPage::getEditToolbar();
-			?>
-			<textarea id="wpTextbox1" class="yui-ac-input" cols="80" rows="15" name="wpTextbox1" accesskey="," autocomplete="off"><?php echo (isset($formData['postBody']) ? $formData['postBody'] : ""); ?></textarea>
-		</div>
-	</div>
-
-	<div class="formBlock">
-		<?php echo $categoryCloud; ?>
-	</div>
-
-	<div class="formLastBlock">
-		<div class="editButtons">
-			<input id="wpSave" type="submit" title="<?=wfMsg( 'tooltip-save' ).' ['.wfMsg( 'accesskey-save' ).']'?>" accesskey="s" value="<?=wfMsg('create-blog-save')?>" name="wpSave"/>
-			<input id="wpPreview" type="submit" title="<?=wfMsg( 'tooltip-preview' ).' ['.wfMsg( 'accesskey-preview' ).']'?>" accesskey="p" value="<?=wfMsg('create-blog-preview')?>" name="wpPreview"/>
-			<span class="editHelp">
-				<a id="wpCancel" title="<?php echo $title->getText(); ?>" href="<?php echo $title->getLocalUrl();?>"><?=wfMsgExt('cancel', array('parseinline'))?></a> |
-				<a id="wpEdithelp" href="<?=Skin::makeInternalOrExternalUrl( wfMsgForContent( 'edithelppage' ))?>" target="helpwindow"><?=htmlspecialchars( wfMsg('edithelp') )?></a> <?=htmlspecialchars( wfMsg( 'newwindow' ) )?>
-			</span>
-		</div>
-	</div>
+</div>
 <!-- e:<?= __FILE__ ?> -->
