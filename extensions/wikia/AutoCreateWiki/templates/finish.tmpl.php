f8dc897b244b54e6d508a9ee73605bc366cf1fa0
nick <nick@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-24 00:52:26 +0000

    Remove trailing slash
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@10994 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/templates/finish.tmpl.php
+++ extensions/wikia/AutoCreateWiki/templates/finish.tmpl.php
@@ -12,5 +12,5 @@
 <!-- Link to the New Wiki Builder. Make it trixy for now, it's not ready for everyone to see -->
 <p>
 <p>
-<span style="font-size:8pt; color: grey; text-decoration:none"><a href="<?=$domain?>/wiki/Special:NewWikiBuilder">.</a></span>
+<span style="font-size:8pt; color: grey; text-decoration:none"><a href="<?=$domain?>wiki/Special:NewWikiBuilder">.</a></span>
 <!-- e:<?= __FILE__ ?> -->
