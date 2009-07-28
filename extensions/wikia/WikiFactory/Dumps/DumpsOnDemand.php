a768d33df58c340d924e8bc3244b8c48315ffe95
eloy <eloy@wikia-inc.com>
2009-07-28 10:25:45 +0000

    typo fixed, dynamic url
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@11101 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/Dumps/DumpsOnDemand.php
+++ extensions/wikia/WikiFactory/Dumps/DumpsOnDemand.php
@@ -7,23 +7,53 @@
  */
 $wgHooks[ "CustomSpecialStatistics" ][] = "DumpsOnDemads::customSpecialStatistics";
 
-$wgExtensionMessagesFiles[ "WikiFactoryDoD" ] =  dirname( __FILE__ ) . '/DumpsOnDemand.i18n.php';
+$wgExtensionMessagesFiles[ "DumpsOnDemand" ] =  dirname( __FILE__ ) . '/DumpsOnDemand.i18n.php';
 
-class DumpsOnDemads {
+class DumpsOnDemand {
+
+	const BASEURL = "http://wiki-stats.wikia.com";
 
 	/**
 	 * @access public
 	 * @static
 	 */
 	static public function customSpecialStatistics( &$specialpage, &$text ) {
-		global $wgOut;
+		global $wgOut, $wgDBname;
 
-		wfLoadExtensionMessages( "WikiFactoryDoD" );
+		wfLoadExtensionMessages( "DumpsOnDemads" );
+
+		/**
+		 * read json file with dumps information
+		 */
 
 		$tmpl = new EasyTemplate( dirname( __FILE__ ) . "/templates/" );
-		$tmpl->set( "urlDumpFull", "http://wikistats.wikia.com/dbdumps/t/te/techteamtest/" );
-		$tmpl->set( "urlDumpCurr", "http://wikistats.wikia.com/dbdumps/t/te/techteamtest/" );
+		$index = Wikia::json_decode( file_get_contents( self::getUrl( $wgDBname, "index.json" ) ) );
+
+		$tmpl->set( "urlDumpFull", self::getUrl( $wgDBname, "pages_current.xml.gz" ) );
+		$tmpl->set( "urlDumpCurr", self::getUrl( $wgDBname, "pages_full.xml.gz" ) );
+		$tmpl->set( "index", $index );
 		$text .= $tmpl->render( "dod" );
 		return true;
+	}
+
+	/**
+	 * return url to place where dumps are stored
+	 *
+	 * @static
+	 * @access public
+	 *
+	 * @return String
+	 */
+	static public function getUrl( $database, $file, $baseurl = false ) {
+		$database = strtolower( $database );
+
+		return sprintf(
+			"%s/%s/%s/%s/%s",
+			( $baseurl === false ) ? self::BASEURL : $baseurl,
+			substr( $database, 0, 1),
+			substr( $database, 0, 2),
+			$database,
+			$file
+		);
 	}
 }
