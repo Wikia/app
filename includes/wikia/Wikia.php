daf8b7fcabc4ad448594aed7fa0b2b379964840d
eloy <eloy@wikia-inc.com>
2009-07-30 11:01:18 +0000

    readable time duration
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@11259 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/Wikia.php
+++ includes/wikia/Wikia.php
@@ -511,4 +511,54 @@ class Wikia {
 		return $wgMemc->delete( $lockKey );
 	}
 
+
+	/**
+	 * A function for making time periods readable
+	 *
+	 * @author      Aidan Lister <aidan@php.net>
+	 * @version     2.0.0
+	 * @link        http://aidanlister.com/2004/04/making-time-periods-readable/
+	 * @param       int     number of seconds elapsed
+	 * @param       string  which time periods to display
+	 * @param       bool    whether to show zero time periods
+	 */
+	static public function timeDuration($seconds, $use = null, $zeros = false) {
+		// Define time periods
+		$periods = array (
+			'years'     => 31556926,
+			'Months'    => 2629743,
+			'weeks'     => 604800,
+			'days'      => 86400,
+			'hours'     => 3600,
+			'minutes'   => 60,
+			'seconds'   => 1
+			);
+
+		// Break into periods
+		$seconds = (float) $seconds;
+		foreach ($periods as $period => $value) {
+			if ($use && strpos($use, $period[0]) === false) {
+				continue;
+			}
+			$count = floor($seconds / $value);
+			if ($count == 0 && !$zeros) {
+				continue;
+			}
+			$segments[strtolower($period)] = $count;
+			$seconds = $seconds % $value;
+		}
+
+		// Build the string
+		foreach ($segments as $key => $value) {
+			$segment_name = substr($key, 0, -1);
+			$segment = $value . ' ' . $segment_name;
+			if ($value != 1) {
+				$segment .= 's';
+			}
+			$array[] = $segment;
+		}
+
+		$str = implode(', ', $array);
+		return $str;
+	}
 }
