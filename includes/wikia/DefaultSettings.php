8bcab695838aaa87062890bcf6ce4fbcbef89041
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-07-31 15:35:21 +0000

    styleversion++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@11319 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -26,9 +26,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '11115';
-$wgMergeStyleVersionJS  = '11115';
-$wgMergeStyleVersionCSS = '11115';
+$wgStyleVersion         = '11319';
+$wgMergeStyleVersionJS  = '11319';
+$wgMergeStyleVersionCSS = '11319';
 
 /**
  * @name $wgCityId
