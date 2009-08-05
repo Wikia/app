16fa208ee17c145efca2fb3b578992301cf1e7b7
nick <nick@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-08-05 15:53:01 +0000

    Clean up text
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@11521 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/templates/finish.tmpl.php
+++ extensions/wikia/AutoCreateWiki/templates/finish.tmpl.php
@@ -7,12 +7,9 @@
 
 <div class="awc-title"><?=wfMsg('autocreatewiki-success-title')?></div>
 <br />
-<div class="awc-subtitle"><?=wfMsg('autocreatewiki-success-subtitle')?></div>
 
 
 <div style="font-style: normal;" class="clearfix" id="nwb_link">
-	<div class="awc-title"><?=$domain?></div>
-	<div class="awc-subtitle"><?=wfMsg('autocreatewiki-success-has-been-created')?></div>
 
 	<div style="position: absolute; left: 50%; margin-top: 20px;">
 		<a href="<?=$domain?>wiki/Special:NewWikiBuilder" class="bigButton" style="margin-left: -50%;"><big><?=wfMsg('autocreatewiki-success-get-started')?></big><small></small></a>
