5ec6272ca7d4d7500ecdae7d3d0c50be71bbf3ee
moli <moli@wikia-inc.com>
2009-08-13 11:32:51 +0000

    MultiWikiEdit and MultiDelete in one place
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@11832 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/MultiTasks/SpecialMultiDelete.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ * @author Piotr Molski <moli@wikia-inc.com> for Wikia.com
+ * @copyright (C) 2008, Wikia Inc.
+ * @licence GNU General Public Licence 2.0 or later
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+    echo "This is MediaWiki extension named MultiWikiDelete.\n";
+    exit( 1 ) ;
+}
+
+require_once ( dirname(__FILE__) . '/MultiTasksCore.php' );
+
+$wgExtensionCredits['specialpage'][] = array(
+    "name" => "Multi Wiki Delete",
+    "description" => "Special Multi Wiki Delete",
+    "author" => "Bartek Łapiński, Piotr Molski"
+);
+
+$wgExtensionMessagesFiles["Multidelete"] = dirname(__FILE__) . '/MultiTasks.i18n.php';
+
+$wgAvailableRights[] = 'multidelete';
+$wgGroupPermissions['staff']['multidelete'] = true;
+
+extAddSpecialPage( dirname(__FILE__) . '/SpecialMultiDelete_body.php', 'Multidelete', 'Multidelete' );
+
+require_once( $IP . "/extensions/wikia/TaskManager/BatchTask.php" );
+extAddBatchTask( dirname(__FILE__)."/Tasks/MultiDeleteTask.php", "multidelete", "MultiDeleteTask" );
+
+$wgSpecialPageGroups['Multidelete'] = 'pagetools';
---
+++ extensions/wikia/MultiTasks/templates/row-delete-list.tmpl.php
@@ -0,0 +1,17 @@
+<!-- s:<?= __FILE__ ?> -->
+<li>
+<div style="padding:6px;">
+	<div>Multi-wiki delete mode: <b><?=$modeText?></b></div>
+	<div>Article: <b><?=$sTitle?></b> </div>
+	<div>Deleted by user: <a href="<?=$wgUser->getUserPage()->getLocalUrl()?>"><?=$mTaskParams['admin']?></a></div>
+	<div>Reason: <b><?=$mTaskParams['reason']?></b> </div>
+	<div>Number of Wikis to delete: <b><?=$countWikis?></b></div>
+	<div>Language of Wikis: <b><?= ( $lang ) ? $lang : " - " ?></b></div>
+<? if ( $error ) { ?> 
+	<div style="color:#BF0000"><strong><?=wfMsg ('multidelete_task_error' )?></strong></div>
+<? } else { ?>
+	<div style="color:#2AAF00"><strong><?=wfMsg ('multidelete_task_added', $submit_id )?></strong></div>
+<? } ?>
+</div>	
+</li>
+<!-- e:<?= __FILE__ ?> -->
