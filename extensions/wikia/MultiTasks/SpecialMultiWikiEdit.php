cb677720b46f23b3ad065959418226436fc77128
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-08-13 11:32:51 +0000

    MultiWikiEdit and MultiDelete in one place
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@11832 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/MultiTasks/SpecialMultiWikiEdit.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ * @author Bartek Lapinski for Wikia.com
+ * @author Piotr Molski <moli@wikia-inc.com> for Wikia.com
+ * @copyright (C) 2008, Wikia Inc.
+ * @licence GNU General Public Licence 2.0 or later
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+    echo "This is MediaWiki extension named MultiWikiEdit.\n";
+    exit( 1 ) ;
+}
+$wgExtensionCredits['specialpage'][] = array(
+    "name" => "Multi Wiki Edit",
+    "description" => "Special Multi Wiki Edit",
+    "author" => "Bartek Łapiński, Piotr Molski"
+);
+
+$wgExtensionMessagesFiles["Multiwikiedit"] = dirname(__FILE__) . '/MultiTasks.i18n.php';
+
+$wgAvailableRights[] = 'multiwikiedit';
+$wgGroupPermissions['staff']['multiwikiedit'] = true;
+$wgGroupPermissions['helper']['multiwikiedit'] = true;
+
+require_once( $IP . "/extensions/wikia/TaskManager/BatchTask.php" );
+extAddBatchTask( dirname(__FILE__)."/Tasks/MultiWikiEditTask.php", "multiwikiedit", "MultiWikiEditTask" );
+
+extAddSpecialPage( dirname(__FILE__) . '/SpecialMultiWikiEdit_body.php', 'Multiwikiedit', 'Multiwikiedit' );
+$wgSpecialPageGroups['Multiwikiedit'] = 'pagetools';
