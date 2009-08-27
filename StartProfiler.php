cbb6e1811d802b8f8835f906430ec2fa698973b0
emil <emil@wikia-inc.com>
2009-08-27 11:00:54 +0000

    report service time via stomp using mw profiler
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@12261 e846bd91-c042-42a7-a218-4b65e2c1904d

--- StartProfiler.php
+++ StartProfiler.php
@@ -8,6 +8,8 @@ if( !empty( $_GET['forceprofile'] ) ) {
 	require_once( dirname(__FILE__).'/includes/ProfilerSimpleTrace.php' );
 	$wgProfiler = new ProfilerSimpleTrace;
 } else {
-	require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
+	//require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
+	require_once( dirname(__FILE__).'/includes/ProfilerSimpleStomp.php' );
+	$wgProfiler = new ProfilerSimpleStomp;
 }
 
