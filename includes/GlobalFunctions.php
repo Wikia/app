661285a45c1883c9d58dca07744995e486b68a53
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-08-27 11:00:54 +0000

    report service time via stomp using mw profiler
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@12261 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -878,27 +878,6 @@ function wfReportTime() {
 	}
 	if( $wgShowHostnames) header( sprintf( "X-Served-By-Backend: %s", wfHostname() ) );
 
-	// WIKIA: send a message to the MQ
-	global $wgReportTimeViaStomp;
-	if( $wgReportTimeViaStomp ) {
-		wfProfileIn( __METHOD__ . '-stomp' );
-		global $wgStompServer, $wgStompUser, $wgStompPassword;
-		$stomp = new Stomp( $wgStompServer );
-		$stomp->connect( $wgStompUser, $wgStompPassword );
-		$stomp->sync = false;
-		$key = 'wikia.apache.service_time.'.wfHostname();
-		$stomp->send( $key,
-			Wikia::json_encode( array(
-				'key' => $key,
-				'real' => $elapsed,
-				'cpu' => $elapsedcpu,
-				'url' => 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
-			) ),
-			array( 'exchange' => 'amq.topic', 'bytes_message' => 1 )
-		);
-		wfProfileOut( __METHOD__ . '-stomp' );
-	}
-
 	wfProfileOut( __METHOD__ );
 	return $wgShowHostnames
 		? sprintf( "<!-- Served by %s in %01.3f secs. cpu: %01.3f -->", wfHostname(), $elapsed, $elapsedcpu )
---
+++ includes/ProfilerSimpleStomp.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @file
+ * @ingroup Profiler
+ */
+
+require_once( dirname( __FILE__ ) . '/ProfilerSimple.php' );
+
+/**
+ * @ingroup Profiler
+ */
+class ProfilerSimpleStomp extends ProfilerSimple {
+
+	public $mFiltered = true;
+	public $mProfiledMethods = array(
+		'memcached::get',
+		'Database::query',
+		'Database::open',
+		'Database::query-master',
+		'Setup.php-extensions',
+		'MediaWiki::performAction',
+		'Output-skin',
+		);
+
+	function profileIn($functionname) {
+		if( !$this->mFiltered || in_array( $functionname, $this->mProfiledMethods ) ) {
+			parent::profileIn($functionname);
+		}
+	}
+
+	function profileOut($functionname) {
+		if( $functionname=='close' || !$this->mFiltered || in_array( $functionname, $this->mProfiledMethods ) ) {
+			parent::profileOut($functionname);
+		}
+	}
+
+	function getFunctionReport() {
+		global $wgStompServer, $wgStompUser, $wgStompPassword;
+
+		$key = 'wikia.apache.service_time.'.wfHostname();
+		$body = array(
+				'key' => $key,
+				'url' => strpos($_SERVER['REQUEST_URI'], 'http')===false ? 
+						'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] :
+						$_SERVER['REQUEST_URI'],
+			     );
+		foreach( $this->mCollated as $k=>$v ) {
+			if( $k=='-total' || !$this->mFiltered || in_array( $k, $this->mProfiledMethods ) ) {
+				$body['profiles'][$k] = $v;
+			}
+		}
+		try {
+			$stomp = new Stomp( $wgStompServer );
+			$stomp->connect( $wgStompUser, $wgStompPassword );
+			$stomp->sync = false;
+			$stomp->send(
+					$key,
+					Wikia::json_encode( $body ),
+					array( 'exchange' => 'amq.topic', 'bytes_message' => 1 )
+				    );
+		}
+		catch( Stomp_Exception $e ) {
+			Wikia::log( __METHOD__, 'exception', $e->getMessage() );
+		}
+	}
+
+}
+
