91fbda443e20a8a2bc30f5eee3e5ede2755f872c
tor <tor@wikia-inc.com>
2009-09-01 14:28:51 +0000

    Variables: updated to newest version for LyricWiki
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@12404 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/Variables/Variables.php
+++ extensions/3rdparty/Variables/Variables.php
@@ -1,47 +1,61 @@
 <?php
-
+ 
 if ( !defined( 'MEDIAWIKI' ) ) {
     die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
-
+ 
 $wgExtensionFunctions[] = 'wfSetupVariables';
-
+ 
 $wgExtensionCredits['parserhook'][] = array(
         'name' => 'Variables',
+        'version' => '1.2.2',
         'url' => 'http://www.mediawiki.org/wiki/Extension:VariablesExtension',
-        'author' => 'Rob Adams',
-        'description' => 'Define page-scoped variables'
+        'author' => 'Rob Adams, Tom Hempel and Daniel Werner',
+        'description' => 'Define page-scoped variables',
 );
-
+ 
 $wgHooks['LanguageGetMagic'][]       = 'wfVariablesLanguageGetMagic';
-
+ 
 class ExtVariables {
-    var $mVariables;
-
+    var $mVariables = array();
+ 
     function vardefine( &$parser, $expr = '', $value = '' ) {
         $this->mVariables[$expr] = $value;
         return '';
     }
-
-    function varf( &$parser, $expr = '' ) {
-        return $this->mVariables[$expr];
+ 
+    function vardefineecho( &$parser, $expr = '', $value = '' ) {
+        $this->mVariables[$expr] = $value;
+        return $value;
+    }
+ 
+    function varf( &$parser, $expr = '', $defaultVal = '' ) {        
+        if ( $this->mVariables[$expr] != '' ) {
+            return $this->mVariables[$expr];
+        } else {
+            return $defaultVal;
+        }
+    }
+ 
+    function varexists( &$parser, $expr = '' ) {
+        return array_key_exists($expr, $this->mVariables);
     }
 }
-
+ 
 function wfSetupVariables() {
-    global $wgParser, $wgMessageCache, $wgExtVariables, $wgMessageCache, $wgHooks;
-
+    global $wgParser, $wgExtVariables, $wgHooks;
+ 
     $wgExtVariables = new ExtVariables;
-
+ 
     $wgParser->setFunctionHook( 'vardefine', array( &$wgExtVariables, 'vardefine' ) );
+    $wgParser->setFunctionHook( 'vardefineecho', array( &$wgExtVariables, 'vardefineecho' ) );
     $wgParser->setFunctionHook( 'var', array( &$wgExtVariables, 'varf' ) );
+    $wgParser->setFunctionHook( 'varexists', array( &$wgExtVariables, 'varexists' ) );
 }
-
+ 
 function wfVariablesLanguageGetMagic( &$magicWords, $langCode = 0 ) {
         require_once( dirname( __FILE__ ) . '/Variables.i18n.php' );
         foreach( efVariablesWords( $langCode ) as $word => $trans )
-                $magicWords[$word] = $trans;
+            $magicWords[$word] = $trans;
         return true;
 }
-
-?>
