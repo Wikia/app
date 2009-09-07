7b6400db1f9a1f46f8204e4a09177d23d7fbcd8a
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-09-07 11:28:56 +0000

    ShareFeature: add main file and credits
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@12532 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ShareFeature/ShareFeature_setup.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * @author Bartek Łapiński 
+ */
+
+if(!defined('MEDIAWIKI')) {
+        exit(1);
+}
+
+$wgExtensionCredits['other'][] = array(
+        'name' => 'ShareFeature',
+        'author' => 'Bartek Łapiński',
+        'version' => '0.10',
+);
+
