4be766955a10e81a366cc58de909e61ebf8b04fa
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-09-07 15:46:51 +0000

    ShareFeature: a little cleanup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@12554 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ShareFeature/ShareFeature_setup.php
+++ extensions/wikia/ShareFeature/ShareFeature_setup.php
@@ -1,6 +1,16 @@
 <?php
-/*
- * @author Bartek Łapiński 
+
+/**
+ * Share Feature extension
+ *
+ * Extension allows users/anons to share a link to the page with popular sites
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ *
+ * @author Bartek Łapiński <bartek@wikia-inc.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * 
  */
 
 if(!defined('MEDIAWIKI')) {
@@ -21,12 +31,14 @@ $wgHooks['MonacoAfterArticleLinks'][] = 'SFMonacoAfterArticleLinks';
 
 // display the links for the feature in the page controls bar
 function SFMonacoAfterArticleLinks() {
-	echo "<li id=\"control_share_feature\"><a href=\"#\">" . wfMsg('sf-link') . "</a></li>";
+	$function = '';
+	echo "<li id=\"control_share_feature\"><a href=\"#\" onclick=\"" . $function . "\">" . wfMsg('sf-link') . "</a></li>";
 	return true;
 }
 
+// initialize the extension
 function ShareFeature_init() {
-        global $wgAutoloadClasses, $wgExtensionMessagesFiles;
+        global $wgExtensionMessagesFiles;
         $wgExtensionMessagesFiles['ShareFeature'] = dirname(__FILE__).'/ShareFeature.i18n.php';
         wfLoadExtensionMessages('ShareFeature');
 }
