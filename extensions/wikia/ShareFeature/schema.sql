62f5780d821125e9d697c7b9475b5961a0cffb81
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-09-15 12:55:33 +0000

    ShareFeature: initial values for those
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@12772 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ShareFeature/schema.sql
+++ extensions/wikia/ShareFeature/schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE /*$wgDBprefix*/ `share_feature` (
   `sf_clickcount` int(11) default '0',
   PRIMARY KEY (sf_user_id, sf_provider_id)
 );
+
+INSERT INTO `share_feature` VALUES (0, 0, 1000);
+INSERT INTO `share_feature` VALUES (0, 1, 900);
+INSERT INTO `share_feature` VALUES (0, 2, 800);
+INSERT INTO `share_feature` VALUES (0, 3, 700);
+INSERT INTO `share_feature` VALUES (0, 4, 600);
+INSERT INTO `share_feature` VALUES (0, 5, 500);
+INSERT INTO `share_feature` VALUES (0, 6, 400);
+INSERT INTO `share_feature` VALUES (0, 7, 300);
+
