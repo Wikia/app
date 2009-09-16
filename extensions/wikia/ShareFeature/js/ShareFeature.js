e33c281043bfa4f5fa1b7ae7e1bfc0c753c208fb
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-09-16 14:41:25 +0000

    ShareFeature: add functionality for provider
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@12830 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ShareFeature/js/ShareFeature.js
+++ extensions/wikia/ShareFeature/js/ShareFeature.js
@@ -1,7 +1,15 @@
 
-// open dialog on clicking
+var ShareFeature = {}
+
+ShareFeature.ajax = function( provider ) {
+	$.getJSON( wgScript + '?action=ajax&rs=wfShareFeatureAjaxUpdateStats', {
+		 'provider' : provider,
+		}, function() {
+		});
+}
 
 $(function() {
+		// open dialog on clicking
 		$('#ca-share_feature').click(function() {
 			$().getModal(
 				wgScript + '?action=ajax&rs=wfShareFeatureAjaxGetDialog&title=' + encodeURIComponent(wgPageName) + '&wiki=' + wgCityId,
