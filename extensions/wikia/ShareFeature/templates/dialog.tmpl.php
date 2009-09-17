d2ab55629f20b6900501b0ed874c8f7ed7f6eb14
bartek <bartek@wikia-inc.com>
2009-09-17 13:35:26 +0000

    ShareFeature: rough adding of images (need to be styled still)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@12871 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ShareFeature/templates/dialog.tmpl.php
+++ extensions/wikia/ShareFeature/templates/dialog.tmpl.php
@@ -3,9 +3,10 @@
 		<div>
 			<ul>
 			<?php
+				global $wgExtensionsPath;
 				foreach( $sites as $site) {
 			?>
-				<li><a href="<?= $site['url'] ?>" target="_blank" onclick="ShareFeature.ajax( <?= $site['id'] ?>  )"><?= $site['name'] ?></a></li>
+				<li><img src="<?= $wgExtensionsPath ?>/wikia/ShareFeature/images/<?= strtolower( $site['name'] ) ?>.png" alt="<?= $site['name'] ?>"/><a href="<?= $site['url'] ?>" target="_blank" onclick="ShareFeature.ajax( <?= $site['id'] ?>  )"><?= $site['name'] ?></a></li>
 			<?php
 				}
 			?>
