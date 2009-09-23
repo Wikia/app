2f53fdac116bbc9be996c3d07b3f3315670c1488
bartek <bartek@wikia-inc.com>
2009-09-23 15:13:22 +0000

    ShareFeature: add ids
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13065 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ShareFeature/templates/dialog.tmpl.php
+++ extensions/wikia/ShareFeature/templates/dialog.tmpl.php
@@ -6,7 +6,7 @@
 				global $wgExtensionsPath;
 				foreach( $sites as $site) {
 			?>
-				<li><a href="<?= $site['url'] ?>" target="_blank" onmousedown="ShareFeature.mouseDown( <?= $site['id'] ?> )"><img src="<?= $wgExtensionsPath ?>/wikia/ShareFeature/images/<?= strtolower( $site['name'] ) ?>.png" alt="<?= $site['name'] ?>"/></a><a href="<?= $site['url'] ?>" target="_blank" onmousedown="ShareFeature.mouseDown( <?= $site['id'] ?>  )"><?= $site['name'] ?></a></li>
+				<li><a href="<?= $site['url'] ?>" target="_blank" onmousedown="ShareFeature.mouseDown( <?= $site['id'] ?> )"><img src="<?= $wgExtensionsPath ?>/wikia/ShareFeature/images/<?= strtolower( $site['name'] ) ?>.png" alt="<?= $site['name'] ?>"/></a><a href="<?= $site['url'] ?>" id="sf_prv_<?= strtolower( $site['name'] ) ?>" target="_blank" onmousedown="ShareFeature.mouseDown( <?= $site['id'] ?>  )"><?= $site['name'] ?></a></li>
 			<?php
 				}
 			?>
