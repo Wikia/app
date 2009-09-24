a4a29bff15ad0b7a607a8ccb20888db9d0e39eec
moli <moli@wikia-inc.com>
2009-09-24 12:52:08 +0000

    to test MWE and MWD
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13098 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/MultiWikiDeleteMaintenance.php
+++ maintenance/wikia/MultiWikiDeleteMaintenance.php
@@ -7,7 +7,7 @@
 
 ini_set( "include_path", dirname(__FILE__)."/.." );
 require_once( "commandLine.inc" );
-require_once( $GLOBALS["IP"]."/extensions/wikia/TaskManager/Tasks/MultiWikiEditTask.php" );
+require_once( $GLOBALS["IP"]."/extensions/wikia/MultiTasks/Tasks/MultiDeleteTask.php" );
 
 global $wgMaxShellTime, $wgMaxShellFileSize;
 $wgMaxShellTime = 0;
@@ -18,6 +18,5 @@ $dbr = wfGetDB( DB_MASTER, array(), $wgExternalSharedDB );
 $aCondition = array("task_id" => $options['TASK_ID']);
 $oTask = $dbr->selectRow( "wikia_tasks", "*", $aCondition, __METHOD__, array( "ORDER BY" => "task_id") );
 
-$Maintenance = new MultiWikiEditTask();
+$Maintenance = new MultiDeleteTask();
 $Maintenance->execute($oTask);
-
