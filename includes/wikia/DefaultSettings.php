875c91044dad7768957f49ef28522f9fd5725b80
macbre <macbre@wikia-inc.com>
2009-09-25 15:37:12 +0000

    wgStyleVersion++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13183 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -17,7 +17,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '13176';
+$wgStyleVersion         = '13183';
 $wgMergeStyleVersionJS  = '11321';
 $wgMergeStyleVersionCSS = '11321';
 
