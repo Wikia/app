463519e61c508deb4688a967d17d4b2d269b182e
macbre <macbre@wikia-inc.com>
2009-09-25 14:42:09 +0000

    Resolve conflict on ap8
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13180 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -17,7 +17,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '13154';
+$wgStyleVersion         = '13176';
 $wgMergeStyleVersionJS  = '11321';
 $wgMergeStyleVersionCSS = '11321';
 
