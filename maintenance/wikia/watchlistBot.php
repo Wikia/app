2022711d4a97b0620a94c9d004adccb83c13f372
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-09-30 08:51:15 +0000

    send mail if weekly digest finish
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13308 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/watchlistBot.php
+++ maintenance/wikia/watchlistBot.php
@@ -27,11 +27,11 @@ if(class_exists('GlobalWatchlistBot')) {
 	}
 	else {
 		$oWatchlistBot->run();
+		//	
+		$oUser = User::newFromId(115748); //Moli.wikia
+		$oUser->load();
+		$oUser->sendMail( 'Global watchlist is finished', 'Global watchlist is finished', 'Wikia <community@wikia.com>', null, 'GlobalWatchlist' );
 	}
-	//	
-	$oUser = User::newFromId(115748); //Moli.wikia
-	$oUser->load();
-	$oUser->sendMail( 'Global watchlist is finished', 'Global watchlist is finished', 'Wikia <community@wikia.com>', null, 'GlobalWatchlist' );
 }
 else {
 	print "GlobalWatchlist extension is not installed.\n";
