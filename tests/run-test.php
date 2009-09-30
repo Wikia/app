5b952591710ea382592ae619ad52fd61ee65d847
emil <emil@wikia-inc.com>
2009-09-30 13:26:42 +0000

    Make phpunit tests work for Wikia. A sample usage: SERVER_ID=43967 php run-test.php --conf /usr/wikia/conf/current/qa9.wiki.factory/LocalSettings.php ReverseParserTest.php
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13345 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/run-test.php
+++ tests/run-test.php
@@ -3,5 +3,6 @@
 require_once( dirname(__FILE__) . '/../maintenance/commandLine.inc' );
 ini_set( 'include_path', get_include_path() . PATH_SEPARATOR . /*$_SERVER['PHP_PEAR_INSTALL_DIR']*/ 'C:\php\pear' );
 require( 'PHPUnit/TextUI/Command.php' );
-
+define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
+PHPUnit_TextUI_Command::main();
 
