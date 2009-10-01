5fa23ed211572b49d19c119f6a74815e9f769f4c
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-01 13:11:13 +0000

    Answers: wikipedia re-search bar: quotes are not escaped (RT #22290)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13399 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditResearch/EditResearch.js
+++ extensions/wikia/EditResearch/EditResearch.js
@@ -8,7 +8,7 @@ jQuery("#research_box").ready(function() {
 		sel = getSelection()
 		if(sel){
 			insertTags("","", sel)
-			
+
 			if( document.getElementById("wpTextbox1").value.indexOf("{{wikipedia|" + current_title + "}}") == -1 ){
 				document.getElementById("wpTextbox1").value = document.getElementById("wpTextbox1").value + "\n{{wikipedia|" + current_title + "}}"
 			}
@@ -21,7 +21,7 @@ jQuery("#research_box").ready(function() {
 		}
 	});
 });
-	
+
 jQuery("#firstHeading").ready(function() {
 	jQuery("#firstHeading").mouseup(function() {
 		sel = getSelection()
@@ -37,7 +37,7 @@ function getSelection(){
 	var sel = "";
 	if(document.getSelection) {
 		sel = document.getSelection();
-	} else if(document.selection) { 
+	} else if(document.selection) {
 		sel = document.selection.createRange().text;
 	} else if(window.getSelection) {
 		sel = window.getSelection();
@@ -48,17 +48,17 @@ function getSelection(){
 function research(){
 	search = document.getElementById("search_input").value;
 	if( !search ) return;
-		
+
 	jQuery("#research_box").css("overflow", "");
 	jQuery("#research-inner").css("background-color", "#FFF").css("background-image", "none");
 	url = wikipedia_server + "/w/api.php?action=query&list=search&srsearch=" + search + "&sroffset=" + (research_page * research_page_limit) + "&format=json&callback=?";
-	jQuery.getJSON( url, "", function( data ){	
-		
+	jQuery.getJSON( url, "", function( data ){
+
 		if( data.query ){
 			search_html = ""
 			for(x=0; x<=data.query.search.length-1;x++){
 				title = data.query.search[x].title;
-				search_html += "<div class='research-search-result'><a href=javascript:research_wikipedia_article('" + title.replace(/\s/g,"_") + "')>" + title + "</a></div>"
+				search_html += "<div class='research-search-result'><a href='#' onclick='research_wikipedia_article(this)'>" + title + "</a></div>"
 			}
 			if( search_html ){
 				search_html += "<div id='research-search-nav'>" + ((research_page>0)?"<a href=javascript:research_paginate(-1)>" + wgPrevMsg + "</a>":"") + " <a href=javascript:research_paginate(1)>" + wgNextMsg + "</a></div>"
@@ -75,23 +75,25 @@ function research_paginate(dir){
 	research();
 }
 
-function research_wikipedia_article( article ){
-	
+function research_wikipedia_article(node) {
+
+	var article = jQuery(node).html().replace(/\s/g, '_');
+
 	injectSpinner( document.getElementById("research_box"), "wikipedia");
-	
+
 	//sometimes Wikipedia links have a hash to point to a section...need to strip that out for the API
 	article = article.replace(/#.*/g,"")
-	
+
 	//save for attribution
 	current_title = article;
-	
+
 	document.getElementById("research_box").innerHTML = ""
 	document.getElementById("research_box").style.overflow="";
 	url = wikipedia_server + "/w/api.php?action=parse&page=" + article + "&format=json&callback=?";
-	
+
 	jQuery.getJSON( url, "", function( data ){
 		html = data.parse.text["*"];
-		
+
 		//replace all the wiki links so they point to this function
 		//<a href=\"\/wiki\/Eli_Manning\"
 		html = html.replace(/<a\shref=\"(\/wiki\/(.*?))\".*?>(.*?)<\/a>/g,"<a href=\"javascript:research_wikipedia_article('$2')\">$3</a>" )
@@ -103,5 +105,4 @@ function research_wikipedia_article( article ){
 		document.getElementById("research_box").style.overflow="scroll";
 		document.getElementById("research_box").innerHTML = html;
 	});
-			
 }
