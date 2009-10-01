b0926c9e10493f25889bf8640493cce7a14ff856
eloy <eloy@wikia-inc.com>
2009-10-01 13:05:48 +0000

    table and params definition
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13396 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaApi/WikiaApiReportEmail.php
+++ extensions/wikia/WikiaApi/WikiaApiReportEmail.php
@@ -15,17 +15,22 @@ $wgAPIModules['theschwartz'] = 'WikiaApiReportEmail';
 use these tables on dataware database:
 
 CREATE TABLE emails (
-	send_date TIMESTAMP NOT NULL INDEX,
-	from VARCHAR NOT NULL,
-	to VARCHAR NOT NULL INDEX
-	success integer  not null  / set 1 for success 0 for failure
-	failure_reason tinytext,
-	type INTEGER references email_types( id )
+	send_date TIMESTAMP NOT NULL,
+	send_from tinytext NOT NULL,
+	send_to tinytext NOT NULL,
+	user_id int(5) unsigned NOT NULL,
+	city_id int(9) unsigned  NOT NULL,
+	success tinyint unsigned not null,
+	failure_reason tinytext default '',
+	type_id tinyint unsigned references email_types( id ),
+	key emails_send_to( send_to(255) ),
+	key emails_send_date( send_date ),
+	key emails_user_id( user_id )
 );
 
 CREATE TABLE email_types (
-	id int primary key
-	type varchar not null
+	id tinyint unsigned not null primary key,
+	type varchar(64) not null
 )
 **/
 
@@ -49,21 +54,41 @@ class WikiaApiReportEmail extends ApiBase {
 	 */
 	public function execute() {
 
-		global $wgUser;
+		global $wgExternalDatawareDB, $wgTheSchwartzToken;
+
+		$wgTheSchwartzToken = "test";
 
 		$params = $this->extractRequestParams();
 		$result = array();
 
-		if( $wgUser->isAllowed( "theschwartz-report" ) || 1 ) { // enabled for testing
+		if( isset($params[ "token" ] ) && $params[ "token" ] === $wgTheSchwartzToken ) {
 			if( is_null( $params[ "from" ] ) ) {
 				$this->dieUsageMsg( array( "missingparam", "from" ) );
 			}
 			if( is_null( $params[ "to" ] ) ) {
 				$this->dieUsageMsg( array( "missingparam", "to" ) );
 			}
-			if( is_null( $params[ "type" ] ) ) {
-				$this->dieUsageMsg( array( "missingparam", "type" ) );
+			if( is_null( $params[ "type_id" ] ) ) {
+				$this->dieUsageMsg( array( "missingparam", "type_id" ) );
 			}
+			if( is_null( $params[ "user_id" ] ) ) {
+				$this->dieUsageMsg( array( "missingparam", "user_id" ) );
+			}
+			if( is_null( $params[ "city_id" ] ) ) {
+				$this->dieUsageMsg( array( "missingparam", "city_id" ) );
+			}
+			if( is_null( $params[ "type_id" ] ) ) {
+				$this->dieUsageMsg( array( "missingparam", "type_id" ) );
+			}
+			if( is_null( $params[ "success" ] ) ) {
+				$this->dieUsageMsg( array( "missingparam", "success" ) );
+			}
+			if( is_null( $params[ "reason" ] ) ) {
+				$this->dieUsageMsg( array( "missingparam", "reason" ) );
+			}
+
+			$dbw = wfGetDB( DB_MASTER, array(), $wgExternalDatawareDB );
+			//$dbw->insert();
 		}
 		else {
 			$this->dieUsageMsg( array( "sessionfailure" ) );
@@ -91,10 +116,14 @@ class WikiaApiReportEmail extends ApiBase {
 	 */
 	public function getAllowedParams() {
 		return array(
-			"from"    => array(),
-			"to"      => array(),
-			"type"    => array(),
-			"success" => array( APIBASE::PARAM_TYPE => "integer" ) 
+			"from"     => array(),
+			"to"       => array(),
+			"token"    => array(),
+			"success"  => array( APIBASE::PARAM_TYPE => "integer" ),
+			"city_id"  => array( APIBASE::PARAM_TYPE => "integer" ),
+			"user_id"  => array( APIBASE::PARAM_TYPE => "integer" ),
+			"type_id"  => array( APIBASE::PARAM_TYPE => "integer" ),
+			"reason"   => array( )
 		);
 	}
 
@@ -105,10 +134,14 @@ class WikiaApiReportEmail extends ApiBase {
 	 */
 	public function getParamDescription() {
 		return array(
-			"from"    => "From: email address",
-			"to"      => "To: email address",
-			"type"    => "Type of email e.g. watchlist",
-			"success" => "Operation succeded or not"
+			"from"     => "From: email address",
+			"to"       => "To: email address",
+			"user_id"  => "user_id from user table",
+			"city_id"  => "city_id from city_list table",
+			"type_id"  => "Type of email e.g. watchlist",
+			"success"  => "Operation succeded or not",
+			"token"    => "Secret token",
+			"reason"   => "Tells reason whe operation was not succeded"
 		);
 	}
 
