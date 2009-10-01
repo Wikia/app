9eaa544472060a73152b64cfc57c628bc7c1f0fd
macbre <macbre@wikia-inc.com>
2009-10-01 13:58:47 +0000

    EditResearch: more fixes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13405 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditResearch/EditResearch.js
+++ extensions/wikia/EditResearch/EditResearch.js
@@ -5,12 +5,12 @@ var current_title = "";
 
 jQuery("#research_box").ready(function() {
 	jQuery("#research_box").mouseup(function() {
-		sel = getSelection()
+		var sel = getSelection()
 		if(sel){
-			insertTags("","", sel)
+			insertTags("","", sel);
 
 			if( document.getElementById("wpTextbox1").value.indexOf("{{wikipedia|" + current_title + "}}") == -1 ){
-				document.getElementById("wpTextbox1").value = document.getElementById("wpTextbox1").value + "\n{{wikipedia|" + current_title + "}}"
+				document.getElementById("wpTextbox1").value = document.getElementById("wpTextbox1").value + "\n{{wikipedia|" + current_title + "}}";
 			}
 		}
 	});
@@ -24,9 +24,9 @@ jQuery("#research_box").ready(function() {
 
 jQuery("#firstHeading").ready(function() {
 	jQuery("#firstHeading").mouseup(function() {
-		sel = getSelection()
+		var sel = getSelection();
 		if(sel){
-			document.getElementById("search_input").value = sel
+			document.getElementById("search_input").value = sel;
 			document.getElementById("search_input").focus();
 			research();
 		}
@@ -46,32 +46,34 @@ function getSelection(){
 }
 
 function research(){
-	search = document.getElementById("search_input").value;
-	if( !search ) return;
+	var search = document.getElementById("search_input").value;
+	if( !search ) {
+		return;
+	}
 
 	jQuery("#research_box").css("overflow", "");
 	jQuery("#research-inner").css("background-color", "#FFF").css("background-image", "none");
-	url = wikipedia_server + "/w/api.php?action=query&list=search&srsearch=" + search + "&sroffset=" + (research_page * research_page_limit) + "&format=json&callback=?";
+	url = wikipedia_server + "/w/api.php?action=query&list=search&srsearch=" + encodeURIComponent(search) + "&sroffset=" + (research_page * research_page_limit) + "&format=json&callback=?";
 	jQuery.getJSON( url, "", function( data ){
 
 		if( data.query ){
-			search_html = ""
+			var search_html = ""
 			for(x=0; x<=data.query.search.length-1;x++){
 				title = data.query.search[x].title;
-				search_html += "<div class='research-search-result'><a href='#research-search' onclick='research_wikipedia_article(this)'>" + title + "</a></div>"
+				search_html += "<div class='research-search-result'><a href='#research-search' onclick='research_wikipedia_article(this)'>" + title + "</a></div>";
 			}
 			if( search_html ){
-				search_html += "<div id='research-search-nav'>" + ((research_page>0)?"<a href=javascript:research_paginate(-1)>" + wgPrevMsg + "</a>":"") + " <a href=javascript:research_paginate(1)>" + wgNextMsg + "</a></div>"
+				search_html += "<div id='research-search-nav'>" + ((research_page>0)?"<a href=javascript:research_paginate(-1)>" + wgPrevMsg + "</a>":"") + " <a href=javascript:research_paginate(1)>" + wgNextMsg + "</a></div>";
 			}else{
-				search_html = wgNoResultsMsg
+				search_html = wgNoResultsMsg;
 			}
-			document.getElementById("research_box").innerHTML = search_html
+			document.getElementById("research_box").innerHTML = search_html;
 		}
 	} );
 }
 
 function research_paginate(dir){
-	research_page = research_page + dir
+	research_page = research_page + dir;
 	research();
 }
 
@@ -82,26 +84,28 @@ function research_wikipedia_article(node) {
 	injectSpinner( document.getElementById("research_box"), "wikipedia");
 
 	//sometimes Wikipedia links have a hash to point to a section...need to strip that out for the API
-	article = article.replace(/#.*/g,"")
+	article = article.replace(/#.*/g,"");
 
 	//save for attribution
 	current_title = article;
 
-	document.getElementById("research_box").innerHTML = ""
-	document.getElementById("research_box").style.overflow="";
-	url = wikipedia_server + "/w/api.php?action=parse&page=" + article + "&format=json&callback=?";
+	document.getElementById("research_box").innerHTML = "";
+	document.getElementById("research_box").style.overflow = "";
+
+	var url = wikipedia_server + "/w/api.php?action=parse&page=" + encodeURIComponent(article) + "&format=json&callback=?";
 
 	jQuery.getJSON( url, "", function( data ){
 		html = data.parse.text["*"];
 
 		//replace all the wiki links so they point to this function
 		//<a href=\"\/wiki\/Eli_Manning\"
-		html = html.replace(/<a\shref=\"(\/wiki\/(.*?))\".*?>(.*?)<\/a>/g,"<a href=\"javascript:research_wikipedia_article('$2')\">$3</a>" )
+		html = html.replace(/<a\shref=\"(\/wiki\/(.*?))\".*?>(.*?)<\/a>/g,"<a href='#research-search' onclick='research_wikipedia_article(this)'>$3</a>" );
 
 		//we should also remove red links and edit links
-		html = html.replace(/<a\shref=\"(\/w\/index\.php\?title=(.*?)&.*?)\".*?>(.*?)<\/a>/g,"$3" )
+		html = html.replace(/<a\shref=\"(\/w\/index\.php\?title=(.*?)&.*?)\".*?>(.*?)<\/a>/g,"$3" );
 
-		removeSpinner("wikipedia")
+		removeSpinner("wikipedia");
+
 		document.getElementById("research_box").style.overflow="scroll";
 		document.getElementById("research_box").innerHTML = html;
 	});
