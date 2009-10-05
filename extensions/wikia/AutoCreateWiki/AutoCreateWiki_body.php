a82a66ed73e6f097725c9d57888c7a3e111da8dd
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-05 09:29:52 +0000

    activated NWB for es wikis (#23937)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13488 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/AutoCreateWiki_body.php
+++ extensions/wikia/AutoCreateWiki/AutoCreateWiki_body.php
@@ -710,7 +710,7 @@ class AutoCreateWikiPage extends SpecialPage {
 		$oTmpl->set_vars( array( "domain" => $this->mWikiData[ "url" ] ) );
 
 		// New Wiki Builder isn't supported by all languages yet
-		$NewWikiBuilderLanguages = array('en', 'de');
+		$NewWikiBuilderLanguages = array('en', 'de', 'es');
 		if (in_array($this->mWikiData[ "language" ], $NewWikiBuilderLanguages)){
 			$sFinishText = $oTmpl->execute("finish");
 		} else {
