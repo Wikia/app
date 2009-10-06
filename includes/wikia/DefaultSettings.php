8e6d92b9ff42d4da6b1dae124386072b735d3ab9
macbre <macbre@wikia-inc.com>
2009-10-06 14:21:46 +0000

    MagCloud: use images.wikia.com/common/...
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13567 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -17,7 +17,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '13565';
+$wgStyleVersion         = '13566';
 $wgMergeStyleVersionJS  = '11321';
 $wgMergeStyleVersionCSS = '11321';
 
