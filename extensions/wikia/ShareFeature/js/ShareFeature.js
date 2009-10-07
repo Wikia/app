a24f6c3eca600758fa5113ca527ce5aa9b3bd65f
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-07 17:01:46 +0000

    ShareFeature: more changes for RT #24161
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13640 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ShareFeature/js/ShareFeature.js
+++ extensions/wikia/ShareFeature/js/ShareFeature.js
@@ -11,6 +11,26 @@ ShareFeature.ajax = function( provider ) {
 	);
 };
 
+ShareFeature.openDialog = function() {
+	if( false == ShareFeatureEnabled ) {
+		ShareFeatureEnabled = true;
+		$().getModal(
+			wgScript + '?action=ajax&rs=wfShareFeatureAjaxGetDialog&title=' + encodeURIComponent(wgPageName) + '&wiki=' + wgCityId,
+				'#shareFeatureInside',
+			{
+					width: 300,
+					callback: function() {
+						ShareFeatureEnabled = false;
+						ShareFeature.track( 'open' );
+					},
+				onClose: function() {
+					ShareFeature.track('close');
+				}
+			}
+		);
+	}
+}
+
 ShareFeature.mouseDown = function( provider ) {
 	var event = $.getEvent();
 	switch( event.button ) {
@@ -38,24 +58,7 @@ $(function() {
 		if( $( '#control_share_feature' ).exists() && $( '#ca-share_feature' ).exists() ) {
 			$('#control_share_feature').removeClass( 'disabled' );
 			// open dialog on clicking
-			$('#ca-share_feature').click(function() {
-				if( false == ShareFeatureEnabled ) {
-					ShareFeatureEnabled = true;
-					$().getModal(
-						wgScript + '?action=ajax&rs=wfShareFeatureAjaxGetDialog&title=' + encodeURIComponent(wgPageName) + '&wiki=' + wgCityId,
-						'#shareFeatureInside',
-						{
-							width: 300,
-							callback: function() {
-								ShareFeatureEnabled = false;
-								ShareFeature.track( 'open' );
-							},
-							onClose: function() {
-								ShareFeature.track('close');
-							}
-						}
-					);
-				}
-			});
+			$('#ca-share_feature').click( ShareFeature.openDialog );
+			$('#fe_sharefeature_link').click( ShareFeature.openDialog );
 		}
 });
