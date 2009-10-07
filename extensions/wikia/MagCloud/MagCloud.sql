4f208ec283388cc59f03b00e3b2d3ecf52159834
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-07 05:36:17 +0000

    MagCloud: DB error logging.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13594 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MagCloud/MagCloud.sql
+++ extensions/wikia/MagCloud/MagCloud.sql
@@ -6,3 +6,13 @@ CREATE TABLE magcloud_collection (
  mco_articles TEXT DEFAULT NULL,
  mco_cover TEXT DEFAULT NULL
 );
+
+CREATE TABLE magcloud_collection_log (
+	mcl_publish_hash VARCHAR(32),
+	mcl_publish_timestamp INT,
+	mcl_publish_token TEXT,
+	mcl_publish_code INT,
+	mcl_publish_msg TEXT,
+	mcl_publish_raw_result TEXT,
+	mcl_timestamp INT
+);
