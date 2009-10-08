a42897a02d54591455f364646d7dd1309b9fc35f
tor <tor@wikia-inc.com>
2009-10-08 12:47:16 +0000

    StaffPowers: improved unblockableness, now with a msg
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13658 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/StaffPowers/StaffPowers.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalization file for StaffPowers extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
+ */
+$messages['en'] = array(
+	'staffpowers-ipblock-aborted' => 'Blocking Wikia staff is not possible. Please use [[Special:Contact]] to report any issues or problems with our staff.'
+);
