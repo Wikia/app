e9124a53f806f37585d1e167efe94d834bdc17b4
nick <nick@wikia-inc.com>
2009-10-12 20:02:31 +0000

    UPdate the hubs for quantcast per Gil
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13740 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AnalyticsEngine/AnalyticsProviderQuantServe.php
+++ extensions/wikia/AnalyticsEngine/AnalyticsProviderQuantServe.php
@@ -16,9 +16,16 @@ class AnalyticsProviderQuantServe implements iAnalyticsProvider {
 			"<script type=\"text/javascript\">/*<![CDATA[*/
 			try {
 				_qoptions = { qacct: '{$this->account}' };
-				_qoptions.labels = Athena.getPageVar('hub');
-				for (var i = 0; i < ProviderValues.list.length; i++){
-					_qoptions.labels += ',' + Athena.getPageVar('hub') + '.' + ProviderValues.list[i].value;
+				// Hard code some hosts per Gil
+				if (window.location.hostname == 'answers.wikia.com') { 
+					_qoptions.labels = 'lifestyles,mom';
+				} else if (window.location.hostname == 'recipes.wikia.com'){
+					_qoptions.labels = 'lifestyles,mom';
+				} else {
+					_qoptions.labels = Athena.getPageVar('hub');
+					for (var i = 0; i < ProviderValues.list.length; i++){
+						_qoptions.labels += ',' + Athena.getPageVar('hub') + '.' + ProviderValues.list[i].value;
+					}
 				}
 			} catch (e){
 				// Fall back to old way.
