4fc40552953b65a1a2a5257f190042b80304bfbf
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-13 14:39:59 +0000

    simple page for settings cookies, will be removed later
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13783 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ includes/wikia/SpecialIowa.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * switch colocation cookie
+ */
+
+
+$wgSpecialPages[ "Iowa" ] = "SpecialIowa";
+$wgAutoloadClasses[ "Iowa" ] = dirname(__FILE__) . '/SpecialIowa.php';
+
+class SpecialIowa extends UnlistedSpecialPage {
+
+	private
+		$mTitle,
+		$mCookie,
+		$mCookieName;
+
+	public function __construct() {
+		parent::__construct( 'Iowa', 'wikifactory' );
+		$this->mCookieName = "wikiaColocation";
+		$this->mCookie = false;
+	}
+
+	public function execute( $subpage ) {
+		global $wgOut, $wgUser, $wgRequest, $wgMessageCache;
+
+		$wgMessageCache->addMessages( array( "iowa" => "Iowa" ), "en" );
+
+		if( !$wgUser->isAllowed( 'wikifactory' ) ) {
+			$this->displayRestrictionError();
+			return;
+		}
+		wfProfileIn( __METHOD__ );
+
+		$this->setHeaders();
+		$this->mTitle = SpecialPage::getTitleFor( "Iowa" );
+
+		/**
+		 * show current value of cookie
+		 */
+		if ( isset( $_COOKIE[ $this->mCookieName ] ) ) {
+			$this->mCookie = $_COOKIE[ $this->mCookieName ];
+		}
+		$wgOut->addHTML( "Current value of cookie: <em>" . ( $this->mCookie ? $this->mCookie : "not set" ) . "</em>" );
+
+		/**
+		 * show input chooser
+		 */
+		$wgOut->addHTML( Xml::openElement( "form", array( "action" => $this->mTitle->getFullURL(), "method" => "post" ) ) );
+		$select = new XMLSelect( "iowacookie", "iowacookie" );
+		$select->addOption( "set cookie to Iowa", 1 );
+		$select->addOption( "remove cookie at all", 1 );
+		$wgOut->addHTML( $select->getHTML() );
+		$wgOut->addHTML( Xml::submitButton( "submit" ) );
+		$wgOut->addHTML( Xml::closeElement( "form" ) );
+
+		/**
+		 * if posted change cookie value
+		 */
+		if( $wgRequest->wasPosted() ) {
+			print_pre( $wgRequest->getVal( "iowacookie" ) );
+		}
+		//$wgRequest->response()->setcookie();
+		wfProfileOut( __METHOD__ );
+	}
+}
