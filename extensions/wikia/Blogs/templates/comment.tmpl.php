4c9c3bdefc7f540cbaa663cbc73359ddb9ee35e4
moli <moli@wikia-inc.com>
2009-10-13 13:13:21 +0000

    blog specific "edit" link (RT#24791)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@13776 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/comment.tmpl.php
+++ extensions/wikia/Blogs/templates/comment.tmpl.php
@@ -18,7 +18,7 @@
 			<a href="#" class="blog-comm-hide" id="<?php echo $comment[ "title" ]->getArticleId() ?>"><?=wfMsg('blog-comment-hide')?></a>
 		<?php endif; ?>
 		<?php if( $canEdit ): ?>
-			<a name="<?php echo $comment[ "title" ]->getArticleId() ?>" href="#<?php echo $comment[ "title" ]->getArticleId() ?>" class="blog-comm-edit" id="<?php echo $comment[ "title" ]->getArticleId() ?>"><?=wfMsg('editlink')?></a>
+			<a name="<?php echo $comment[ "title" ]->getArticleId() ?>" href="#<?php echo $comment[ "title" ]->getArticleId() ?>" class="blog-comm-edit" id="<?php echo $comment[ "title" ]->getArticleId() ?>"><?=wfMsg('blog-comment-edit')?></a>
 			<?php if ( !empty($showHistory) && !$comment[ "title" ]->isNewPage() ) : ?>
 				<?= $sk->makeKnownLinkObj( $comment[ "title" ], wfMsgHtml( 'blog-comment-history' ), 'action=history' ) ?>
 			<?php endif; ?>
