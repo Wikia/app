bf6160959639dbf3cf972948be3266c9582a1aa9
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-27 14:27:10 +0000

    urlencode parameters
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14244 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/AutoCreateWikiLocalJob.php
+++ extensions/wikia/AutoCreateWiki/AutoCreateWikiLocalJob.php
@@ -443,7 +443,7 @@ class AutoCreateWikiLocalJob extends Job {
 	private function queueReminderMail() {
 		global $wgServer, $wgTheSchwartzSecretToken;
 
-		$backurl = sprintf( "%s/api.php?action=awcreminder&user_id=%d&token=%s",
+		$backurl = sprintf( "%s/api.php?action=awcreminder%26user_id=%d%26token=%s",
 			$wgServer, $this->mFounder->getId(), $wgTheSchwartzSecretToken );
 
 		Http::post( self::REMINDER_URL, 'default', array (
