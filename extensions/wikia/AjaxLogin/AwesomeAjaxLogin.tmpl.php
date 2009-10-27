6e9d7a001776a23388384fdd862fd902c5b23b9f
bartek <bartek@wikia-inc.com>
2009-10-27 16:00:07 +0000

    LoginFriction: bold the messages
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14260 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
@@ -6,8 +6,8 @@ $link = $titleObj->getLocalUrl('type=signup');
 <div id="AjaxLoginBox" title="<?= wfMsg('login') ?>">
 	<form action="" method="post" style="margin:5px; font-size: 0.95em" name="userajaxloginform">
 		<div id="wpError"></div>
-		<label for="wpName1" style="display: block"><?= wfMsg("yourname") ?></label>
-		<label for="wpPassword1" style="display: block"><?= wfMsg("yourpassword") ?></label>
+		<label for="wpName1" style="display: block; font-weight: bold"><?= wfMsg("yourname") ?></label>
+		<label for="wpPassword1" style="display: block; font-weight: bold"><?= wfMsg("yourpassword") ?></label>
 		<div style="padding: 3px">
 			<label for="wpRemember1" style="padding-left: 5px"><?= wfMsg('remembermypassword') ?></label>
 		</div>
