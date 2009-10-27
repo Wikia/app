86758bc7ff8279713f1bafe942c0722fcb59c3c0
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-27 17:02:15 +0000

    LoginFriction: move two links and fire up form on one of them
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14264 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
@@ -6,15 +6,15 @@ $link = $titleObj->getLocalUrl('type=signup');
 <div id="AjaxLoginBox" title="<?= wfMsg('login') ?>">
 	<form action="" method="post" style="margin:5px; font-size: 0.95em" name="userajaxloginform">
 		<div id="wpError"></div>
-		<label for="wpName1" style="display: block; font-weight: bold"><?= wfMsg("yourname") ?></label>
-		<label for="wpPassword1" style="display: block; font-weight: bold"><?= wfMsg("yourpassword") ?></label>
+		<label for="wpName1" style="display: block; font-weight: bold"><?= wfMsg("yourname") ?></label><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>" style="margin: 3px;"><?= wfMsg('nologinlink') ?></a>
+		<label for="wpPassword1" style="display: block; font-weight: bold"><?= wfMsg("yourpassword") ?></label><a id="wpMailmypassword" href="#" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();" style="margin: 3px;"><?= wfMsg('mailmypassword') ?></a>
 		<div style="padding: 3px">
 			<label for="wpRemember1" style="padding-left: 5px"><?= wfMsg('remembermypassword') ?></label>
 		</div>
 		<input style="margin:0;padding:0 .25em;width:auto;overflow:visible;" type="submit" name="wpLoginattempt" id="wpLoginattempt" tabindex="105" value="<?= wfMsg("login") ?>" onclick="AjaxLogin.action='login'" />
 <?php if( $wgEnableEmail && $wgAuth->allowPasswordChange() ) { ?>
-		<br /><input style="margin:3px 0;padding:0 .25em;width:auto;overflow:visible;font-size:0.9em" type="submit" name="wpMailmypassword" id="wpMailmypassword" tabindex="106" value="<?= wfMsg('mailmypassword') ?>" onclick="AjaxLogin.action='password'" />
+		<br />
 <?php } ?>
-	<br/><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>"><?= wfMsg('nologinlink') ?></a>
+	<br/>
 	</form>
 </div>
