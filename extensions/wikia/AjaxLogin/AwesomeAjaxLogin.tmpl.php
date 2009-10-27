1796fc59f11888c258ec333656f0e9600deb3dcb
bartek <bartek@wikia-inc.com>
2009-10-27 17:40:12 +0000

    LoginFriction: change
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14266 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
@@ -7,14 +7,14 @@ $link = $titleObj->getLocalUrl('type=signup');
 	<form action="" method="post" style="margin:5px; font-size: 0.95em" name="userajaxloginform">
 		<div id="wpError"></div>
 		<label for="wpName1" style="display: block; font-weight: bold"><?= wfMsg("yourname") ?></label><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>" style="margin: 3px;"><?= wfMsg('nologinlink') ?></a>
-		<label for="wpPassword1" style="display: block; font-weight: bold"><?= wfMsg("yourpassword") ?></label><a id="wpMailmypassword" href="#" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();" style="margin: 3px;"><?= wfMsg('mailmypassword') ?></a>
+		<label for="wpPassword1" style="display: block; font-weight: bold"><?= wfMsg("yourpassword") ?></label>
+		<?php if( $wgEnableEmail && $wgAuth->allowPasswordChange() ) { ?>
+		<a id="wpMailmypassword" href="#" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();" style="margin: 3px;"><?= wfMsg('mailmypassword') ?></a>
+		<?php } ?>
 		<div style="padding: 3px">
 			<label for="wpRemember1" style="padding-left: 5px"><?= wfMsg('remembermypassword') ?></label>
 		</div>
 		<a id="wpLoginattempt" class="wikia_button" href="#" onclick="AjaxLogin.action='login'; AjaxLogin.form.submit();" ><span><?= wfMsg("login") ?></span></a>
-<?php if( $wgEnableEmail && $wgAuth->allowPasswordChange() ) { ?>
-		<br />
-<?php } ?>
 	<br/>
 	</form>
 </div>
