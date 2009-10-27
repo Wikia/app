0a73c587533a58b0c974a10665f30eaf71d0106f
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-27 18:17:29 +0000

    LoginFriction: make padding as in mockup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14272 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
@@ -4,14 +4,14 @@ $titleObj = SpecialPage::getTitleFor( 'Userlogin' );
 $link = $titleObj->getLocalUrl('type=signup');
 ?>
 <div id="AjaxLoginBox" title="<?= wfMsg('login') ?>">
-	<form action="" method="post" style="margin:5px; font-size: 0.95em" name="userajaxloginform">
-		<div id="wpError"></div>
-		<label for="wpName1" style="display: block; font-weight: bold"><?= wfMsg("yourname") ?></label><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>" style="margin: 3px;"><?= wfMsg('nologinlink') ?></a>
-		<label for="wpPassword1" style="display: block; font-weight: bold"><?= wfMsg("yourpassword") ?></label>
+	<form action="" method="post" style="margin:5px; font-size: 0.95em;" name="userajaxloginform">
+<div id="wpError"></div>
+		<label for="wpName1" style="display: block; font-weight: bold;"><?= wfMsg("yourname") ?></label><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>" style="margin: 3px;"><?= wfMsg('nologinlink') ?></a>
+		<label for="wpPassword1" style="display: block; font-weight: bold; margin-top: 8px"><?= wfMsg("yourpassword") ?></label>
 		<?php if( $wgEnableEmail && $wgAuth->allowPasswordChange() ) { ?>
 		<a id="wpMailmypassword" href="#" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();" style="margin: 3px;"><?= wfMsg('mailmypassword') ?></a>
 		<?php } ?>
-		<div style="padding: 3px">
+		<div style="padding: 3px; margin-top: 8px; margin-bottom: 4px;">
 			<label for="wpRemember1" style="padding-left: 5px"><?= wfMsg('remembermypassword') ?></label>
 		</div>
 	</form>
