80ee28abe69c3c07df4b86e175c112a38b16bfcb
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-10-27 13:49:42 +0000

    some tests
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14232 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/eloystest.php
+++ maintenance/wikia/eloystest.php
@@ -8,44 +8,66 @@
 ini_set( "include_path", dirname(__FILE__)."/.." );
 require_once( "commandLine.inc" );
 
-$from      = "community@wikia-inc.com";
-$recipient = "eloy@wikia-inc.com";
-$body      = "Test email";
-$headers   = array(
-	"X-Msg-Category" => "Test"
-);
+function testDelayedEmails() {
+	$from      = "community@wikia-inc.com";
+	$recipient = "eloy@wikia-inc.com";
+	$body      = "Test email";
+	$headers   = array(
+		"X-Msg-Category" => "Test"
+	);
 
-/**
- * normal email
- */
-$headers[ "Subject" ] = wfQuotedPrintable( "Test email normal: " . wfTimestampNow() );
-$textHeaders = "";
-foreach( $headers as $header => $value ) {
-	$textHeaders .= "{$header}: $value\n";
+	/**
+	 * normal email
+	 */
+	$headers[ "Subject" ] = wfQuotedPrintable( "Test email normal: " . wfTimestampNow() );
+	$textHeaders = "";
+	foreach( $headers as $header => $value ) {
+		$textHeaders .= "{$header}: $value\n";
+	}
+	Http::post("http://theschwartz/theschwartz/inject", 'default', array (
+		CURLOPT_POSTFIELDS => array (
+			"rcpt" => $recipient,
+			"env_from" => $from,
+			"msg" => "$textHeaders" . "\n\n" . "$body"
+		)
+	) );
+
+	/**
+	 * delayed email
+	 */
+	global $wgTheSchwartzSecretToken;
+	$url = sprintf( "%s/api.php?action=awcreminder&user_id=%d&token=%s",
+		51098,
+		$wgTheSchwartzSecretToken
+	);
+	Http::post("http://theschwartz/theschwartz/function/TheSchwartz::Worker::SendEmail", 'default', array (
+		CURLOPT_POSTFIELDS => array (
+			"theschwartz_run_after" => time() + 300,
+			"url" => $url
+		)
+	) );
 }
-Http::post("http://theschwartz/theschwartz/inject", 'default', array (
-	CURLOPT_POSTFIELDS => array (
-		"rcpt" => $recipient,
-		"env_from" => $from,
-		"msg" => "$textHeaders" . "\n\n" . "$body"
-	)
-) );
 
-/**
- * delayed email
- */
+function testStomp() {
+	global $wgStompServer, $wgStompUser, $wgStompPassword, $wgCityId;
 
-$headers[ "Subject" ] = wfQuotedPrintable( "Test email delay: " . wfTimestampNow() );
-$textHeaders = "";
-foreach( $headers as $header => $value ) {
-	$textHeaders .= "{$header}: $value\n";
+	try {
+		$stomp = new Stomp( $wgStompServer );
+		$stomp->connect( $wgStompUser, $wgStompPassword );
+		$stomp->sync = false;
+		$stomp->send(
+			"/test/eloy/stomp",
+			Wikia::json_encode( array( "value" => "test" ) ),
+			array( 'exchange' => 'amq.topic', 'bytes_message' => 1 )
+		);
+	}
+	catch( Stomp_Exception $e ) {
+		Wikia::log( __METHOD__, 'stomp_exception', $e->getMessage() );
+	}
 }
-#Http::post("http://theschwartz/theschwartz/function/TheSchwartz::Worker::SendEmail", 'default', array (
-Http::post("http://theschwartz/theschwartz/inject", 'default', array (
-	CURLOPT_POSTFIELDS => array (
-		"theschwartz_run_after" => time() + 300,
-		"rcpt" => $recipient,
-		"env_from" => $from,
-		"msg" => "$textHeaders" . "\n\n" . "$body"
-	)
-) );
+
+#
+# main
+#
+testDelayedEmails();
+testStomp();
