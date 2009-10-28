7cfe0a80b8f1bd328d673d10fc4196236f2c4431
christian <christian@wikia-inc.com>
2009-10-28 00:39:37 +0000

    styling for new ajaxlogin plus colors for all themes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14278 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
@@ -4,17 +4,18 @@ $titleObj = SpecialPage::getTitleFor( 'Userlogin' );
 $link = $titleObj->getLocalUrl('type=signup');
 ?>
 <div id="AjaxLoginBox" title="<?= wfMsg('login') ?>">
-	<form action="" method="post" style="margin:5px; font-size: 0.95em;" name="userajaxloginform">
+	<form action="" method="post" style="margin: 10px;" name="userajaxloginform">
 <div id="wpError"></div>
-		<label for="wpName1" style="display: block; font-weight: bold;"><?= wfMsg("yourname") ?></label><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>" style="margin: 3px;"><?= wfMsg('nologinlink') ?></a>
+		<label for="wpName1" style="display: block; font-weight: bold;"><?= wfMsg("yourname") ?></label> <a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>" style="font-size: 9pt;"><?= wfMsg('nologinlink') ?></a>
 		<label for="wpPassword1" style="display: block; font-weight: bold; margin-top: 8px"><?= wfMsg("yourpassword") ?></label>
 		<?php if( $wgEnableEmail && $wgAuth->allowPasswordChange() ) { ?>
-		<a id="wpMailmypassword" href="#" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();" style="margin: 3px;"><?= wfMsg('mailmypassword') ?></a>
+		<a id="wpMailmypassword" href="#" style="font-size: 9pt;" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();"><?= wfMsg('mailmypassword') ?></a>
 		<?php } ?>
-		<div style="padding: 3px; margin-top: 8px; margin-bottom: 4px;">
+		<div style="margin: 15px 0;">
 			<label for="wpRemember1" style="padding-left: 5px"><?= wfMsg('remembermypassword') ?></label>
 		</div>
 	</form>
-	<hr/>
-	<a id="wpLoginattempt" class="wikia_button" href="#" onclick="AjaxLogin.action='login'; AjaxLogin.form.submit();" ><span><?= wfMsg("login") ?></span></a><br/>
+	<div class="modalToolbar neutral">
+		<a id="wpLoginattempt" class="wikia_button" href="#" onclick="AjaxLogin.action='login'; AjaxLogin.form.submit();" ><span><?= wfMsg("login") ?></span></a>
+	</div>
 </div>
