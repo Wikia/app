9cfbe94a8025023a4593300f18c9fd7693ddd309
marooned <marooned@wikia-inc.com>
2009-11-02 11:44:32 +0000

    ActivityFeedTag: * add `uselang` parameter handling * add getUrlVars(), getUrlVar() to jQuery
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14436 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/jquery/jquery.wikia.js
+++ skins/common/jquery/jquery.wikia.js
@@ -157,3 +157,21 @@ $.getEvent = function(e, boundEl) {
 
 	return ev;
 };
+
+//@see http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html
+$.extend({
+	getUrlVars: function(){
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++)
+		{
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	},
+	getUrlVar: function(name){
+		return $.getUrlVars()[name];
+	}
+});
