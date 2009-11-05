5113fba07c06c08667d226c9e78c06a222a61157
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-05 00:40:12 +0000

    group these with other gift pages, even though most are unlisted (for now)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14627 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SystemGifts/SystemGifts.php
+++ extensions/wikia/SystemGifts/SystemGifts.php
@@ -7,18 +7,23 @@ $wgGroupPermissions['sysop']['awardsmanage'] = true;
 //Special Pages
 $wgAutoloadClasses['TopAwards'] = "{$wgSystemGiftsDirectory}/TopAwards.php";
 $wgSpecialPages['TopAwards'] = 'TopAwards';
+$wgSpecialPageGroups['TopAwards'] = 'users';
 
 $wgAutoloadClasses['ViewSystemGifts'] = "{$wgSystemGiftsDirectory}/SpecialViewSystemGifts.php";
 $wgSpecialPages['ViewSystemGifts'] = 'ViewSystemGifts';
+$wgSpecialPageGroups['ViewSystemGifts'] = 'users';
 
 $wgAutoloadClasses['ViewSystemGift'] = "{$wgSystemGiftsDirectory}/SpecialViewSystemGift.php";
 $wgSpecialPages['ViewSystemGift'] = 'ViewSystemGift';
+$wgSpecialPageGroups['ViewSystemGift'] = 'users';
 
 $wgAutoloadClasses['SystemGiftManager'] = "{$wgSystemGiftsDirectory}/SpecialSystemGiftManager.php";
 $wgSpecialPages['SystemGiftManager'] = 'SystemGiftManager';
+$wgSpecialPageGroups['SystemGiftManager'] = 'users';
 
 $wgAutoloadClasses['SystemGiftManagerLogo'] = "{$wgSystemGiftsDirectory}/SpecialSystemGiftManagerLogo.php";
 $wgSpecialPages['SystemGiftManagerLogo'] = 'SystemGiftManagerLogo';
+$wgSpecialPageGroups['SystemGiftManagerLogo'] = 'users';
 
 include_once( "{$wgSystemGiftsDirectory}/SpecialPopulateAwards.php" );
 ?>
