7ec0d75b20fa82d463e5cd8f2d2ce85b06bea2c9
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-07 12:42:06 +0000

    MultiDelete: update instructions wording
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14726 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MultiTasks/MultiTasks.i18n.php
+++ extensions/wikia/MultiTasks/MultiTasks.i18n.php
@@ -57,7 +57,7 @@ $messages['en'] = array(
 /*multidelete*/
 	'multidelete' => 'Delete batch of pages',
 	'multidelete_button' => 'DELETE',
-	'multidelete_help' => 'Delete a batch of pages. You can either perform a single delete, or delete pages listed in a file. You can do it for this wiki, all wikis from shared database or selected wikis (from a text file, each in a single new line). Choose a user that will be shown in deletion logs. Uploaded page list file should contain page name and optional reason separated by | character in each line.',
+	'multidelete_help' => 'Delete a batch of pages via a background task. You can do it for this wiki, all wikis from shared database, selected wikis, or selected languages. Can choose to delete as YOU, or a preset script account.',
 	'multidelete_caption' => 'Page list',
 	'multidelete_title' => 'Multi Delete',
 	'multidelete_both_modes' => 'Please choose either one specified page or a given list of pages.',
@@ -87,7 +87,7 @@ $messages['en'] = array(
 	'multidelete_omitting_invalid' => 'Omitting invalid page $1.',
 	'multidelete_file_bad_format' => 'The file should be plain text',
 	'multidelete_file_missing' => 'Unable to read given file',
-	'multidelete_select_script' => 'delete page script',
+	'multidelete_select_script' => 'designated script account',
 	'multidelete_no_page' => 'Please specify at least one page to delete OR choose a file containing page list.',
 	'multidelete_split_results' => "Your article count was too high to be done in a single task. It has been split into '''$1''' tasks instead, with a maximum chunk size of '''$2''' articles." ,
 	'multidelete_max_display_size' => "Multidelete has currently a maximum number of articles in the confirm phase. Your query has been capped at '''$1''' articles. Choose your articles and save task. After that, consider running it again to catch all the remaining articles." ,
