d5209796cd304b07a4273ee9ca59fcb017e478cd
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-09 12:37:51 +0000

    per rt#26362
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14741 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/syncClusterWithCityList.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set( "include_path", dirname(__FILE__)."/.." );
+require_once( "commandLine.inc" );
+
+$dbw = WikiFactory::db( DB_MASTER );
+
+$sth = $dbw->select(
+	array( "city_list" ),
+	array( "*" ),
+	array( "city_public" => 1 ),
+	__METHOD__,
+	array( "ORDER BY" => "city_id" )
+);
+
+while( $row = $dbw->fetchObject( $sth ) ) {
+	/**
+	 * get cluster
+	 */
+	$cluster = WikiFactory::getVarValueByName( "wgDBcluster", $row->city_id );
+	if( $cluster !== $row->city_cluster ) {
+		echo "Difference in {$row->city_id}: {$cluster} vs. {$row->city_cluster}\n";
+	}
+}
