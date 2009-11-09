b9c7b5875cd46ffc6f72899478eaa409585fca0d
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-09 09:45:14 +0000

    NewEditPage: refactor the code - prepare it for RT #22760
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14730 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewEditPage/NewEditPage.php
+++ extensions/wikia/NewEditPage/NewEditPage.php
@@ -2,163 +2,32 @@
 $wgExtensionCredits['other'][] = array(
 	'name' => 'New Edit Page',
 	'description' => 'Applies edit page changes',
-	'version' => 0.666,
+	'version' => 0.7,
 	'author' => array('[http://pl.wikia.com/wiki/User:Macbre Maciej Brencz]', '[http://www.wikia.com/wiki/User:Marooned Maciej Błaszkowski (Marooned)]')
 );
 
-$wgExtensionFunctions[] = 'wfNewEditPageInit';
+$dir = dirname(__FILE__) . '/';
 
-function wfNewEditPageInit() {
-	global $wgHooks, $wgExtensionMessagesFiles;
+// register extension class
+$wgAutoloadClasses['NewEditPage'] = $dir . 'NewEditPage.class.php';
 
-	// i18n
-	$wgExtensionMessagesFiles['NewEditPage'] = dirname(__FILE__).'/NewEditPage.i18n.php';
+// i18n
+$wgExtensionMessagesFiles['NewEditPage'] = $dir . 'NewEditPage.i18n.php';
 
-	// edit page
-	$wgHooks['EditPage::showEditForm:initial2'][] = 'wfNewEditPageAddCSS';
+// add CSS for view / edit page
+$wgHooks['EditPage::showEditForm:initial2'][] = 'NewEditPage::addCSS';
 
-	// not existing articles
-	$wgHooks['ArticleFromTitle'][] = 'wfNewEditPageArticleView';
+// handle not existing articles
+$wgHooks['ArticleFromTitle'][] = 'NewEditPage::articleView';
 
-	// add red preview notice
-	$wgHooks['EditPage::showEditForm:initial'][] = 'wfNewEditPageAddPreviewBar';
+// add red preview notice
+$wgHooks['EditPage::showEditForm:initial'][] = 'NewEditPage::addPreviewBar';
 
-	// add brown old revision notice
-	$wgHooks['EditPage::showEditForm:oldRevisionNotice'][] = 'wfNewEditPageAddOldRevisionBar';
+// add brown old revision notice
+$wgHooks['EditPage::showEditForm:oldRevisionNotice'][] = 'NewEditPage::addOldRevisionBar';
 
-	// undo success (RT #22732)
-	$wgHooks['EditPage::getContent::end'][] = 'wfNewEditPageUndoSuccess';
-	return true;
-}
+// undo success (RT #22732)
+$wgHooks['EditPage::getContent::end'][] = 'NewEditPage::undoSuccess';
 
-// add custom CSS to page of not existing articles
-function wfNewEditPageArticleView($title) {
-
-	global $wgNewEditPageNewArticle, $wgRequest;
-
-	// limit to not existing articles and view mode
-	if (!$title->exists() && $wgRequest->getVal('action', 'view') == 'view') {
-		$wgNewEditPageNewArticle = true;
-		wfNewEditPageAddCSS();
-	}
-
-	return $title;
-}
-
-// add CSS to edit pages
-function wfNewEditPageAddCSS() {
-	global $wgWysiwygEdit, $wgOut, $wgUser, $wgExtensionsPath, $wgStyleVersion, $wgNewEditPageNewArticle;
-
-	// do not touch skins other than Monaco (RT #13061)
-	$skinName = get_class($wgUser->getSkin());
-	if ($skinName != 'SkinMonaco') {
-		return true;
-	}
-
-	if (!empty($wgNewEditPageNewArticle)) {
-		// new article notice
-		$cssFile = 'NewEditPageNewArticle.css';
-	}
-	else if (!empty($wgWysiwygEdit)) {
-		// edit mode in wysiwyg
-		$cssFile = 'NewEditPageWysiwyg.css';
-	}
-	else {
-		// edit mode in old MW editor
-		$cssFile = 'NewEditPage.css';
-	}
-
-	// add static CSS file
-	$wgOut->addLink(array(
-		'rel' => 'stylesheet',
-		'href' => "{$wgExtensionsPath}/wikia/NewEditPage/{$cssFile}?{$wgStyleVersion}",
-		'type' => 'text/css'
-	));
-
-	return true;
-}
-
-// add red preview notice in old editor
-function wfNewEditPageAddPreviewBar($editPage) {
-	global $wgOut, $wgUser, $wgHooks, $wgRequest;
-
-	// do not touch skins other than Monaco (RT #13061)
-	$skinName = get_class($wgUser->getSkin());
-	if ($skinName != 'SkinMonaco') {
-		return true;
-	}
-
-	// we're in preview mode
-	// extra check for categories - they have `formtype` always set to 'preview' (rt#15017)
-	if ($editPage->formtype == 'preview' && !($editPage->mTitle->mNamespace == NS_CATEGORY && ($wgRequest->getVal('action') != 'submit' || !$wgRequest->wasPosted()))) {
-		wfLoadExtensionMessages('NewEditPage');
-		$wgOut->addHTML('<div class="new_edit_page_notice" id="new_edit_page_preview_notice">' . wfMsg('new-edit-page-preview-notice') . '</div>');
-
-		// add page title before preview HTML
-		$wgHooks['OutputPageBeforeHTML'][] = 'wfNewEditPageAddPreviewTitle';
-
-		// hide page title in preview mode
-		$wgOut->addHTML('<style type="text/css">.firstHeading {display: none}</style>');
-	}
-
-	return true;
-}
-
-/**
- * add brown old revision notice in old editor
- *
- * @author Maciej Błaszkowski <marooned at wikia-inc.com>
- */
-function wfNewEditPageAddOldRevisionBar($editPage) {
-	global $wgOut, $wgUser, $wgHooks, $wgRequest;
-
-	// do not touch skins other than Monaco (RT #13061)
-	$skinName = get_class($wgUser->getSkin());
-	if ($skinName != 'SkinMonaco') {
-		return true;
-	}
-
-	if (!$editPage->mArticle->isCurrent()) {
-		wfLoadExtensionMessages('NewEditPage');
-		$wgOut->addHTML('<div class="new_edit_page_notice" id="new_edit_page_old_revision_notice">' . wfMsgExt('editingold', 'parseinline') . '</div>');
-
-		// add page title before preview HTML
-		$wgHooks['OutputPageBeforeHTML'][] = 'wfNewEditPageAddPreviewTitle';
-
-		// hide page title in preview mode
-		$wgOut->addHTML('<style type="text/css">.firstHeading {display: none}</style>');
-	}
-	//false to supress original warning
-	return false;
-}
-
-// add page title before preview HTML
-function wfNewEditPageAddPreviewTitle($wgOut, $text) {
-	global $wgTitle, $wgCustomTitle;
-	$wgOut->addHTML('<h1 id="new_edit_page_preview_title">' . ( isset($wgCustomTitle) ? $wgCustomTitle->getPrefixedText() : $wgTitle->getPrefixedText() ) . '</h1>');
-
-	// find first closing </h2> and remove preview notice
-	$pos = strpos($text, '</h2>');
-	if ($pos !== false) {
-		$text = substr($text, $pos+5);
-	}
-
-	return true;
-}
-
-// move edit page title after undo success message box (RT #22732)
-function wfNewEditPageUndoSuccess($editPage, $text) {
-	if ( strpos($editPage->editFormPageTop, '<div class="mw-undo-success">') !== false ) {
-		global $wgOut, $wgTitle, $wgCustomTitle;
-
-		// hide page title in preview mode
-		$wgOut->addHTML('<style type="text/css">.firstHeading {display: none}</style>');
-
-		// add title
-		$editPage->editFormPageTop .= Xml::element('h1',
-			array('class' => 'firstHeading', 'style' => 'display: block'),
-			(isset($wgCustomTitle) ? $wgCustomTitle->getPrefixedText() : $wgTitle->getPrefixedText())
-		);
-	}
-	return true;
-}
+// shorten edit page 'transcluded pages' list using JS (RT #22760)
+$wgHooks['LinkerFormatTemplates'][] = 'NewEditPage::formatTemplates';
