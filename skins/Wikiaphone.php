f3ccc2a4416ec6416ff97727e0acea40da1851c7
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-10 19:13:35 +0000

    * Merge r14850
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14851 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/Wikiaphone.php
+++ skins/Wikiaphone.php
@@ -32,6 +32,8 @@ class SkinWikiaphone extends SkinTemplate {
 		$out->addStyle( 'wikiaphone/IE50Fixes.css', 'screen,handheld', 'lt IE 5.5000' );
 		$out->addStyle( 'wikiaphone/IE55Fixes.css', 'screen,handheld', 'IE 5.5000' );
 		$out->addStyle( 'wikiaphone/IE60Fixes.css', 'screen,handheld', 'IE 6' );
+		$out->addScript( '<script type="text/javascript" src="http://www.google-analytics.com/urchin.js"></script>' );
+		$out->addScriptFile( '../wikiaphone/main.js' );
 	}
 }
 
