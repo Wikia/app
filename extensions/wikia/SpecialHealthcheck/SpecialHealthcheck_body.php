4312e075a07074ec0d7b6ca8c878c75c6a4998fa
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-10 01:36:47 +0000

    new Special:Healthcheck extension (#26679)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14789 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialHealthcheck/SpecialHealthcheck_body.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * HealthCheck
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
+ * @date 2009-11-10
+ * @copyright Copyright © 2009 Łukasz Garczewski, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension named HealthCheck.\n";
+	exit( 1 );
+}
+
+class HealthCheck extends UnlistedSpecialPage {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct( 'HealthCheck'/*class*/ );
+	}
+
+	/**
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the page or null
+	 */
+	public function execute( $par ) {
+		global $wgSpecialHealth, $wgOut, $wgCityId;
+
+		// Set page title and other stuff
+		$this->setHeaders();
+		$wgOut->setPageTitle( 'Special:Healthcheck' );
+
+
+		if ( empty( $wgSpecialHealth ) ) {
+			# failure!
+			$wgOut->setStatusCode( 503 );
+			$wgOut->addHTML( 'Server status is: NOT OK' );
+		} else {
+			# success!
+			$wgOut->setStatusCode( 200 );
+			$wgOut->addWikiText( 'Server status is: OK' );
+		}
+	}
+}
