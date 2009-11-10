7fbda0eec18dd5ad3e8f85f54e941ea522312ba6
inez <inez@wikia-inc.com>
2009-11-10 17:28:24 +0000

    * Initial import of Wikiaphone skin (?useskin=wikiaphone) based on chick skin. Should be not visible in preferences at all.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14846 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ skins/Wikiaphone.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * See docs/skin.txt
+ *
+ * @todo document
+ * @file
+ * @ingroup Skins
+ */
+
+if( !defined( 'MEDIAWIKI' ) )
+	die( -1 );
+
+/** */
+require_once( dirname(__FILE__) . '/MonoBook.php' );
+
+/**
+ * @todo document
+ * @ingroup Skins
+ */
+class SkinWikiaphone extends SkinTemplate {
+	function initPage( OutputPage $out ) {
+		SkinTemplate::initPage( $out );
+		$this->skinname  = 'wikiaphone';
+		$this->stylename = 'wikiaphone';
+		$this->template  = 'MonoBookTemplate';
+	}
+
+	function setupSkinUserCss( OutputPage $out ){
+		parent::setupSkinUserCss( $out );
+		// Append to the default screen common & print styles...
+		$out->addStyle( 'wikiaphone/main.css', 'screen,handheld' );
+		$out->addStyle( 'wikiaphone/IE50Fixes.css', 'screen,handheld', 'lt IE 5.5000' );
+		$out->addStyle( 'wikiaphone/IE55Fixes.css', 'screen,handheld', 'IE 5.5000' );
+		$out->addStyle( 'wikiaphone/IE60Fixes.css', 'screen,handheld', 'IE 6' );
+	}
+}
+
+
