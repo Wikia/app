6b462acb4d513d36edf19aceaffebce4e4f2111e
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-14 17:52:43 +0000

    RegexBlock: more tweaks to interface
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@14999 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RegexBlock/templates/page-stats.tmpl.php
+++ extensions/wikia/RegexBlock/templates/page-stats.tmpl.php
@@ -3,7 +3,13 @@
 $blocker_link = $skin->makeKnownLinkObj( $titleObj, $blockInfo->blckby_blocker, 'filter=' . urlencode($blockInfo->blckby_blocker) );
 $blockername_link = $skin->makeKnownLinkObj( $titleObj, $blockInfo->blckby_name, 'rfilter=' . urlencode($blockInfo->blckby_name) );
 ?>
-<h5><?=wfMsg('regexblock_stats_title')?> <strong> <?=$blockername_link?></strong> (<?=wfMsg('regexblock_blocker_name')?>: <b><?=$blocker_link?></b>, <i><?= ($blockInfo->blckby_reason) ? wfMsg('regexblock_reason') . $blockInfo->blckby_reason : wfMsg('regexblock_generic_reason');?></i>)</h5>
+<h5><?=wfMsg('regexblock_stats_title')?> <strong> <?=$blockername_link?></strong></h5><br />
+<ul>
+<li><?=wfMsg('regexblock_blocker_name')?>: <b><?=$blocker_link?></b></li>
+<li><?=wfMsg('regexblock_reason')?> <?=($blockInfo->blckby_reason) ?  ('<b>' . $blockInfo->blckby_reason . '</b>') : ('<i>' . wfMsg('regexblock_generic_reason') . '</i>' ); ?></li>
+<li><?=wgMsg('regexblock_block_date_from')?>: <b><?=$lang->timeanddate( wfTimestamp( TS_MW, $blockInfo->blckby_timestamp ), true )?></b></li>
+<li><?=wfMsg('regexblock_block_date_to')?>: <?=($blockInfo->blckby_expire != 'infinite') ?  ('<b>' . $lang->timeanddate( wfTimestamp( TS_MW, $blockInfo->blckby_expire ), true ) . '</b>') : ('<i>infinite</i>'); ?></li>
+</ul>
 <br />
 <? if (!empty($stats_list)) { ?>
 <p><?=$pager?></p>
