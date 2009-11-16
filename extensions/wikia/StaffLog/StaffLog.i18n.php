d1377c5b30b6ae4c679230bc93ea2e7d8233403b
tomek <tomek@wikia-inc.com>
2009-11-16 09:53:04 +0000

    add central logs(StaffLog) for piggyback
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15013 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/StaffLog/StaffLog.i18n.php
@@ -0,0 +1,9 @@
+<?php
+$messages = array();
+ 
+$messages['en'] = array( 
+	"stafflog" => "StaffLog",
+	"stafflog-blockmsg" => '$1 $2 try to block staff user $3. On wiki : $4. Reason: $5',
+	"stafflog-piggybackloginmsg" => '$1 Piggyback - user $2 login as $3 ',
+	"stafflog-piggybacklogoutmsg" => '$1 Piggyback - user $2 logout from $3 acount'
+);
