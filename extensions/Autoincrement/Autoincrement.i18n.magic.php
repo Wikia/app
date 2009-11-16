5955c31b5af173e8a38566a050cea466f41de63a
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-16 11:31:39 +0000

    missing files
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15023 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/Autoincrement/Autoincrement.i18n.magic.php
@@ -0,0 +1,27 @@
+<?php
+
+$magicWords = array();
+
+$magicWords['en'] = array(
+	'autoincrement' => array( 0, 'AUTOINCREMENT' ),
+);
+
+$magicWords['ar'] = array(
+	'autoincrement'   => array( '0', 'زيادة_تلقائية', 'AUTOINCREMENT' ),
+);
+
+$magicWords['mr'] = array(
+	'autoincrement'   => array( '0', 'स्वयंवाढ', 'AUTOINCREMENT' ),
+);
+
+$magicWords['nds-nl'] = array(
+	'autoincrement'   => array( '0', 'AUTEMATISCHOPHOGEN', 'AUTOMATISCHOPHOGEN', 'AUTOINCREMENT' ),
+);
+
+$magicWords['nl'] = array(
+	'autoincrement'   => array( '0', 'AUTOMATISCHOPHOGEN', 'AUTOINCREMENT' ),
+);
+
+$magicWords['ru'] = array(
+	'autoincrement'   => array( '0', 'АВТОИНКРЕМЕНТ', 'AUTOINCREMENT' ),
+);
