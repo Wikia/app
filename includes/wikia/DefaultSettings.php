af5b8cafde1a4f9ad6d0d8c85e0aed25dbd6a297
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-17 14:04:01 +0000

    Special:Signup - top align both sides
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15084 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -17,9 +17,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '15080';
-$wgMergeStyleVersionJS  = '15080';
-$wgMergeStyleVersionCSS = '15080';
+$wgStyleVersion         = '15081';
+$wgMergeStyleVersionJS  = '15081';
+$wgMergeStyleVersionCSS = '15081';
 
 /**
  * @name $wgCityId
