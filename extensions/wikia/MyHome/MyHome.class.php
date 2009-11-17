ec9c7e38def289b3eb0c6dfcfff337503e6c423b
bartek <bartek@wikia-inc.com>
2009-11-17 17:56:37 +0000

    LoginFriction: science finction (this will get succession rate)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15105 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MyHome/MyHome.class.php
+++ extensions/wikia/MyHome/MyHome.class.php
@@ -188,6 +188,7 @@ class MyHome {
 
 		// do not redirect for skins different then monaco
 		if(get_class($wgUser->getSkin()) == 'SkinMonaco') {
+			$_SESSION['Signup_AccountCreated'] = 'created';			
 			$wgOut->redirect(Skin::makeSpecialUrl('MyHome'));
 		}
 
