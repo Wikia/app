f8a711adde4499f03c1af7176dc75c2923788fee
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-19 13:06:20 +0000

    ActionBox: add file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15197 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ActionBox/ActionBox.php
@@ -0,0 +1,26 @@
+
+<?php
+
+if (!defined('MEDIAWIKI')) {
+        exit( 1 );
+}
+
+$wgExtensionCredits['specialpage'][] = array(
+        'name' => 'Action Box',
+        'author' => 'Bartek Lapinski',
+        'url' => 'http://www.mediawiki.org/wiki/Extension:MyExtension',
+        'description' => 'The brand new user Action Box!',
+        'descriptionmsg' => 'The extension that will allow users to take ACTION!',
+        'version' => '0.1.0',
+);
+
+$dir = dirname(__FILE__) . '/';
+
+// special page
+$wgAutoloadClasses['ActionBox'] = $dir.'ActionBox.class.php';
+$wgSpecialPages['ActionBox'] = 'ActionBox';
+$wgSpecialPageGroups['ActionBox'] = 'users';
+$wgExtensionAliasesFiles['ActionBox'] = $dir . 'ActionBox.alias.php';
+$wgExtensionMessagesFiles['ActionBox'] = $dir . 'ActionBox.i18n.php';
+
+
