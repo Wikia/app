537e28326eb71cdda1159c07885ef73636c299e8
bartek <bartek@wikia-inc.com>
2009-11-20 15:05:30 +0000

    LoginFriction: extend the mainLoginForm
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15261 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialSignup/SpecialSignup.class.php
+++ extensions/wikia/SpecialSignup/SpecialSignup.class.php
@@ -13,9 +13,144 @@ class Signup extends SpecialPage {
 			wfSetupSession();
 		}
 		$wgHooks['MakeGlobalVariablesScript'][] = 'wfSpecialUserloginSetupVars';
-		$form = new LoginForm( $wgRequest, 'signup' );
+		$form = new ExtendedLoginForm( $wgRequest, 'signup' );
 		$form->execute();
 	}	
+}
+
+class ExtendedLoginForm extends LoginForm {
+	var $mActionType;
+
+	function processLogin() {
+                $this->mActionType = 'login';
+		return parent::processLogin();		
+	}	
+
+	// extended to allow to cater better for things here
+        function mainLoginForm( $msg, $msgtype = 'error' ) {
+                global $wgUser, $wgOut, $wgAllowRealName, $wgEnableEmail;
+                global $wgCookiePrefix, $wgLoginLanguageSelector;
+                global $wgAuth, $wgEmailConfirmToEdit, $wgCookieExpiration;
+
+                $titleObj = SpecialPage::getTitleFor( 'Userlogin' );
+
+                if ( ('' != $msg) && ('error' == $msgtype) && ('signup' == $this->mType) ) {
+                        $wgOut->addScript('<script type="text/javascript">WET.byStr(\'signupActions/signup/createaccount/failure\');</script>');
+                }
+
+                if ( $this->mType == 'signup' ) {
+                        // Block signup here if in readonly. Keeps user from
+                        // going through the process (filling out data, etc)
+                        // and being informed later.
+                        if ( wfReadOnly() ) {
+                                $wgOut->readOnlyPage();
+                                return;
+                        } elseif ( $wgUser->isBlockedFromCreateAccount() ) {
+                                $this->userBlockedMessage();
+                                return;
+                        } elseif ( count( $permErrors = $titleObj->getUserPermissionsErrors( 'createaccount', $wgUser, true ) )>0 ) {
+                                $wgOut->showPermissionsErrorPage( $permErrors, 'createaccount' );
+                                return;
+                        }
+                }
+
+                if ( '' == $this->mName ) {
+                        if ( $wgUser->isLoggedIn() ) {
+                                $this->mName = $wgUser->getName();
+                        } else {
+                                $this->mName = isset( $_COOKIE[$wgCookiePrefix.'UserName'] ) ? $_COOKIE[$wgCookiePrefix.'UserName'] : null;
+                        }
+                }
+
+                $titleObj = SpecialPage::getTitleFor( 'Userlogin' );
+
+                if ( $this->mType == 'signup' ) {
+                        $template = new UsercreateTemplate();
+                        $q = 'action=submitlogin&type=signup';
+                        $q2 = 'action=submitlogin&type=login';
+                        $linkq = 'type=login';
+                        $linkmsg = 'gotaccount';
+
+                        // ADi: marketing opt-in/out checkbox added
+                        $template->addInputItem( 'wpMarketingOptIn', 1, 'checkbox', 'tog-marketingallowed');
+                } else {
+                        $template = new UserloginTemplate();
+                        $q = 'action=submitlogin&type=login';
+                        $linkq = 'type=signup';
+                        $linkmsg = 'nologin';
+                }
+
+                if ( !empty( $this->mReturnTo ) ) {
+                        $returnto = '&returnto=' . wfUrlencode( $this->mReturnTo );
+                        if ( !empty( $this->mReturnToQuery ) )
+                                $returnto .= '&returntoquery=' .
+                                        wfUrlencode( $this->mReturnToQuery );
+                        $q .= $returnto;
+                        $linkq .= $returnto;
+                        $q2 .= $returnto;
+                        $linkq2 .= $returnto;
+                }
+
+                # Pass any language selection on to the mode switch link
+                if( $wgLoginLanguageSelector && $this->mLanguage )
+                        $linkq .= '&uselang=' . $this->mLanguage;
+
+                $link = '<a href="' . htmlspecialchars ( $titleObj->getLocalUrl( $linkq ) ) . '">';
+                $link .= wfMsgHtml( $linkmsg . 'link' ); # Calling either 'gotaccountlink' or 'nologinlink'
+                $link .= '</a>';
+
+                # Don't show a "create account" link if the user can't
+                if( $this->showCreateOrLoginLink( $wgUser ) )
+                        $template->set( 'link', wfMsgHtml( $linkmsg, $link ) );
+                else
+                        $template->set( 'link', '' );
+
+                $template->set( 'header', '' );
+                $template->set( 'name', $this->mName );
+                $template->set( 'password', $this->mPassword );
+                $template->set( 'retype', $this->mRetype );
+                $template->set( 'email', $this->mEmail );
+                $template->set( 'realname', $this->mRealName );
+                $template->set( 'domain', $this->mDomain );
+
+                $template->set( 'actioncreate', $titleObj->getLocalUrl( $q ) );
+                $template->set( 'actionlogin', $titleObj->getLocalUrl( $q2 ) );
+
+                $template->set( 'message', $msg );
+                $template->set( 'messagetype', $msgtype );
+                $template->set( 'createemail', $wgEnableEmail && $wgUser->isLoggedIn() );
+                $template->set( 'userealname', $wgAllowRealName );
+                $template->set( 'useemail', $wgEnableEmail );
+                $template->set( 'emailrequired', $wgEmailConfirmToEdit );
+                $template->set( 'canreset', $wgAuth->allowPasswordChange() );
+                $template->set( 'canremember', ( $wgCookieExpiration > 0 ) );
+                $template->set( 'remember', $wgUser->getOption( 'rememberpassword' ) or $this->mRemember  );
+
+                $template->set( 'birthyear', $this->wpBirthYear );
+                $template->set( 'birthmonth', $this->wpBirthMonth );
+                $template->set( 'birthday', $this->wpBirthDay );
+
+                # Prepare language selection links as needed
+                if( $wgLoginLanguageSelector ) {
+                        $template->set( 'languages', $this->makeLanguageSelector() );
+                        if( $this->mLanguage )
+                                $template->set( 'uselang', $this->mLanguage );
+                }
+
+                // Give authentication and captcha plugins a chance to modify the form
+                $wgAuth->modifyUITemplate( $template );
+                if ( $this->mType == 'signup' ) {
+                        wfRunHooks( 'UserCreateForm', array( &$template ) );
+                } else {
+                        wfRunHooks( 'UserLoginForm', array( &$template ) );
+                }
+
+                $wgOut->setPageTitle( wfMsg( 'userlogin' ) );
+                $wgOut->setRobotPolicy( 'noindex,nofollow' );
+                $wgOut->setArticleRelated( false );
+                $wgOut->disallowUserJs();  // just in case...
+                $wgOut->addTemplate( $template );
+       }
 }
 
 
