d5c274aa92d875cc09cdbbfc9f1ac5544ffffb67
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-23 19:42:05 +0000

    MyHome: corrected empty message display (#28674)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15335 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MyHome/templates/feed.tmpl.php
+++ extensions/wikia/MyHome/templates/feed.tmpl.php
@@ -1,7 +1,7 @@
 <?php
 	if (isset($emptyMessage)) {
 ?>
-	<h3 class="myhome-empty-message"><?= htmlspecialchars($emptyMessage) ?></h3>
+	<h3 class="myhome-empty-message"><?= $emptyMessage ?></h3>
 <?php
 	}
 	else {
