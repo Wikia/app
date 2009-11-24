12c21726fd0ce0ad0b5742e007f4f676af8530a9
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-24 14:17:48 +0000

    ActionBox: Newpages feed, basic rough version
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15356 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ActionBox/ActionBox.class.php
+++ extensions/wikia/ActionBox/ActionBox.class.php
@@ -1,6 +1,7 @@
 <?php
 
 class ActionBox extends SpecialPage {
+
         function __construct() {
                 parent::__construct('ActionBox');
         }
@@ -8,6 +9,38 @@ class ActionBox extends SpecialPage {
         function execute() {
                 global $wgRequest, $wgHooks, $wgOut;
                 $this->setHeaders();
-        }
-}
 
+
+		// get the Two Tools
+
+		$this->getNewpagesFeed( 5 );
+
+
+		// get the Three Feeds
+
+        }
+
+
+	// fetch the feed for SpecialNewpages
+	function getNewpagesFeed( $limit ) {
+		wfProfileIn( __METHOD__ );
+
+		$oFauxRequest = new FauxRequest(
+				array(
+					'action'        => 'query',
+					'list'		=> 'recentchanges',
+					'prop'          => array('title'),					
+					'namespace'	=> 0,
+					'limit'		=> intval($limit),
+				     )
+				);
+
+		$oApi = new ApiMain($oFauxRequest);
+                $oApi->execute();
+                $aResult =& $oApi->GetResultData();
+
+		// todo nicely format and return
+
+		wfProfileOut( __METHOD__ ); 		
+	}
+}
