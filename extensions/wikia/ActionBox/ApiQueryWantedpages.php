7b04c2e08bb523ed4324c3c5fb711ca5e9f4713f
bartek <bartek@wikia-inc.com>
2009-11-24 17:01:07 +0000

    ActionBox: rise skeleton
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15376 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ActionBox/ApiQueryWantedpages.php
+++ extensions/wikia/ActionBox/ApiQueryWantedpages.php
@@ -9,6 +9,35 @@ if (!defined('MEDIAWIKI')) {
 
 class ApiQueryWantedpages extends ApiQueryGeneratorBase {
 
+
+	public function __construct( $query ) {
+		
+	}
+
+
+	private function run( $resultPageSet = null ) {
+
+		$this->addTables( 'querycache' );
+		
+
+	}
+
+
+	public function getParamDescription() {
+		return array(
+			'limit' => 'Limit how many random pages will be returned'
+		);
+	}
+
+
+	public function getDescription() {
+		return "Returns given number of Wantedpages";
+	}
+
+
+
+
+
 	//todo fill
 
 }
