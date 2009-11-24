3401f1a0a6d5b83c22ac384b6ad9e3a6887999d9
bartek <bartek@wikia-inc.com>
2009-11-24 17:25:17 +0000

    ActionBox: more stuff for the Wantedpages query
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15380 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ActionBox/ApiQueryWantedpages.php
+++ extensions/wikia/ActionBox/ApiQueryWantedpages.php
@@ -9,24 +9,41 @@ if (!defined('MEDIAWIKI')) {
 
 class ApiQueryWantedpages extends ApiQueryGeneratorBase {
 
-
-	public function __construct( $query ) {
-		
+	public function __construct($query, $moduleName) {
+		parent :: __construct($query, $moduleName, 'wn');
 	}
 
+	public function execute() {
+		$this->run();
+	}
+
+	protected function prepareQuery( $limit, &$resultPageSet ) {
+
+		$this->resetQueryParams();
+		$this->addTables( 'querycache' );
+		$this->addOption( 'LIMIT', $limit );		
+		$this->addWhereFld( 'qc_type', 'Wantedpages' );
+
+	}
+
+	protected function runQuery(&$resultPageSet) {
+		$db = $this->getDB();
+		$res = $this->select(__METHOD__);
+		$count = 0;
+
+	}
 
 	private function run( $resultPageSet = null ) {
 
-		$this->addTables( 'querycache' );
-		
+		$this->prepareQuery();	
+		$count = $this->runQuery($resultPageSet);
 
 	}
 
-
 	public function getParamDescription() {
 		return array(
-			'limit' => 'Limit how many random pages will be returned'
-		);
+				'limit' => 'Limit how many random pages will be returned'
+			    );
 	}
 
 
@@ -34,11 +51,23 @@ class ApiQueryWantedpages extends ApiQueryGeneratorBase {
 		return "Returns given number of Wantedpages";
 	}
 
+	public function getAllowedParams() {
+		return array (
+				'limit' => array (
+					ApiBase :: PARAM_TYPE => 'limit',
+					ApiBase :: PARAM_DFLT => 5,
+					ApiBase :: PARAM_MIN => 1,
+					ApiBase :: PARAM_MAX => 10,
+					ApiBase :: PARAM_MAX2 => 20
+					),
+			     );
+	}
+
+	protected function getExamples() {
+		return 'api.php?action=query&list=wantedpages&wnlimit=5';
+	}
 
 
-
-
-	//todo fill
 
 }
 
