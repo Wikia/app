34dde0421595855644f7627aebe40299ab502c08
bartek <bartek@wikia-inc.com>
2009-11-24 16:18:34 +0000

    ActionBox: add new API query file (here, for now)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15374 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ActionBox/ApiQueryWantedpages.php
@@ -0,0 +1,15 @@
+<?
+
+if (!defined('MEDIAWIKI')) {
+        // Eclipse helper - will be ignored in production
+        require_once ('ApiQueryBase.php');
+}
+
+
+
+class ApiQueryWantedpages extends ApiQueryGeneratorBase {
+
+	//todo fill
+
+}
+
