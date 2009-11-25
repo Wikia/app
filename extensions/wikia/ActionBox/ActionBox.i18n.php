d907fc3c36c7007129755b7454dc8f62f15ad7d9
bartek <bartek@wikia-inc.com>
2009-11-25 12:37:06 +0000

    ActionBox: added this file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15424 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ActionBox/ActionBox.i18n.php
@@ -0,0 +1,11 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'actionbox' => 'Action Box',
+	'actionbox-newpages-hd' => 'Add information to a new page',
+	'actionbox-images-hd' => 'Add a new picture',
+	'actionbox-wantedpages-hd' => 'Start a new page',
+	'actionbox-logo-hd' => 'Upload a logo',		
+);
