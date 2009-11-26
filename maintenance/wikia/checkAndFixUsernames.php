38cc12e72a4453391eb80303a62448f1f07dca1a
eloy <eloy@wikia-inc.com>
2009-11-26 13:39:48 +0000

    just inform about inconsistency
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15468 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/checkAndFixUsernames.php
+++ maintenance/wikia/checkAndFixUsernames.php
@@ -4,12 +4,12 @@ ini_set( "include_path", dirname(__FILE__)."/.." );
 require_once( "commandLine.inc" );
 
 $tables = array(
-	"revision"      => "rev_user_text",
-	"image"         => "img_user_text",
-	"recentchanges" => "rc_user_text",
-	"archive"       => "ar_user_text",
-	"filearchive"   => "fa_user_text",
-	"oldimage"      => "oi_user_text"
+	"revision"      => array( "rev_user_text", "rev_user" ),
+	"image"         => array( "img_user_text", "img_user" ),
+	"recentchanges" => array( "rc_user_text", "rc_user" ),
+	"archive"       => array( "ar_user_text", "ar_user" ),
+	"filearchive"   => array( "fa_user_text", "fa_user" ),
+	"oldimage"      => array( "oi_user_text", "oi_user" )
 );
 
 /**
@@ -22,7 +22,7 @@ $dbr = wfGetDB( DB_SLAVE, array(), $wgExternalSharedDB );
  * local database handler
  * @todo do not harcode, use commandline option
  */
-$dbw = $dbr = wfGetDB( DB_MASTER, array(), "lyrics" );
+$dbw = $dbr = wfGetDB( DB_MASTER, array(), "firefly" ); # "lyrics"
 
 /**
  * not very optimal but we cant use join between db clusters
@@ -30,17 +30,32 @@ $dbw = $dbr = wfGetDB( DB_MASTER, array(), "lyrics" );
  * we probably could assume that username which is bad in one table is bad
  * in other as well
  */
-foreach( $tables as $table => $column ) {
+$cachedUsers = array();
+foreach( $tables as $table => $columns ) {
 
 	$sth = $dbr->select(
 		array( $table ),
-		array( $column ),
+		$columns,
 		false,
 		__METHOD__
 	);
-	while( $row = $dbw->fetchObject( $sth ) ) {
+	while( $row = $dbw->fetchRow( $sth ) ) {
 		/**
 		 * for this username check user_id in external shared
 		 */
+		if( empty( $cachedUsers[ $row[ 0 ] ] ) ) {
+			$user = $dbr->selectRow(
+				array( "user" ),
+				array( "user_id", "user_name"),
+				array( "user_name" => $row[ 0 ] ),
+				__METHOD__
+			);
+			if( !empty( $user ) ) {
+				$cachedUsers[ $user->user_name ] = $user->user_id;
+				if( $user->user_id != $row[ 1 ] ) {
+					Wikia::log( "log", false, "inconsistency in $table, for {$user->user_name} local = {$row[ 1 ]}, global = {$user->user_id}\n" );
+				}
+			}
+		}
 	}
 }
