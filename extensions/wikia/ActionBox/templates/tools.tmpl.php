3880e07df389b8a0ebf35eafb3e800171d721a7b
bartek <bartek@wikia-inc.com>
2009-11-26 14:37:08 +0000

    ActionBox: better
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15479 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ActionBox/templates/tools.tmpl.php
+++ extensions/wikia/ActionBox/templates/tools.tmpl.php
@@ -1,3 +1,6 @@
+
+<?php global $wgAdminSkin; ?>
+
 <ul>
 <!-- ############## Add a logo ############ -->
 <li id="step2" class="step">
@@ -35,3 +38,53 @@
 </div>
 </li>
 </ul>
+
+<!-- ############## Pick Theme ############## -->
+
+<li id="step3" class="step">
+<h1 class="headline"><?php echo wfMsg("nwb-step3-headline")?></h1>
+<div class="wrapper clearfix">
+        <?php echo wfMsg("nwb-step3-text")?>
+        <div id="theme_template" style="display:none" class="theme_selekction">
+                <label for="theme_radio_$theme"><img id="theme_preview_image_$theme" /></label>
+                <input onclick="NWB.changeTheme('monaco-$theme', false)" type="radio" name="theme" value="monaco-$theme" id="theme_radio_$theme"> <label for="theme_radio_$theme">$Theme</label>
+        </div>
+        <div id="theme_scroller" class="accent">
+                <table><tr></tr></table>
+        </div>
+
+
+<script>
+var wgAdminSkin = '<?php echo $wgAdminSkin?>';
+
+var themes = ['Sapphire', 'Jade', 'Slate', 'Smoke', 'Beach', 'Brick', 'Gaming'];
+for (var i = 0; i < themes.length; i++){
+        // Copy the template, search and replace the values
+        var ltheme = themes[i].toLowerCase();
+        var thtml = $("#theme_template").html();
+        thtml = thtml.replace(/\$Theme/g, themes[i]);
+        thtml = thtml.replace(/\$theme/g, ltheme);
+
+        // Create element with that preview and append it
+        $("#theme_scroller tr").append("<td>" + thtml + "</td>");
+        $("#theme_preview_image_" + ltheme).attr("src", "http://images.wikia.com/common/skins/monaco/" + ltheme + "/images/preview.png");
+
+        // Check the box with the current theme ($wgAdminSkin)
+        if (wgAdminSkin.replace(/monaco-/, '')  == ltheme) {
+                // Check the box and change the theme 
+                $("#theme_radio_" + ltheme).attr("checked", true);
+                NWB.changeTheme(ltheme, false);
+        }
+}
+</script>
+</div>
+<div class="nav">
+        <span class="nav_reverse">
+                <button class="secondary" onclick="WET.byStr('nwb/step3back');NWB.gotostep(2);"><span><?php echo wfMsg("nwb-back-to-step-2")?></span></button>
+        </span>
+        <a href="#step4" id="skip_step_3" onclick="WET.byStr('nwb/step3skip');"><?php echo wfMsg("nwb-skip-this-step")?></a> <?php echo wfMsg("nwb-or")?><button onclick="NWB.changeTheme($('input[name=theme]:checked').val(), true);WET.byStr('nwb/step3save');NWB.gotostep(4);"><span><?php echo wfMsg("nwb-save-theme")?></span></button>
+        <input onclick="NWB.changeTheme($('input[name=theme]:checked').val(), true);WET.byStr('nwb/step3save');NWB.gotostep(4);" type="button" id="hidden_theme_submit" style="display:none"><!-- For selenium tests -->
+</div>
+</li>
+
+
