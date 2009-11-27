d6380b14e5d58d45943ce835494859410fa474ac
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-27 16:25:22 +0000

    WikiStickies: more
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15548 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ActionBox/ActionBox.i18n.php
+++ extensions/wikia/ActionBox/ActionBox.i18n.php
@@ -5,7 +5,7 @@ $messages = array();
 $messages['en'] = array(
 	'actionbox' => 'Action Box',
 	'actionbox-newpages-hd' => 'Add information to a new page',
-	'actionbox-images-hd' => 'Add a new picture',
+	'actionbox-wantedimages-hd' => 'Add a new picture',
 	'actionbox-wantedpages-hd' => 'Start a new page',
 	'actionbox-logo-hd' => 'Upload a logo',		
 );
