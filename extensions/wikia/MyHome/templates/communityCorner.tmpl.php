2100bb5f76a0df6486e309e693908564cf72f296
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-11-30 13:53:44 +0000

    MyHome: fixed bad HTML (#31909)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15581 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MyHome/templates/communityCorner.tmpl.php
+++ extensions/wikia/MyHome/templates/communityCorner.tmpl.php
@@ -1,6 +1,6 @@
 <div id="myhome-community-corner">
 	<h2 class="dark_text_2"><?=wfMsg('myhome-community-corner-header')?></h2>
-	<div id="myhome-community-corner-content"><?=wfMsgExt('community-corner', array('parseinline'))?></div>
+	<div id="myhome-community-corner-content"><?=wfMsgExt('community-corner', array('parse'))?></div>
 <?php if($isAdmin) { ?>
 	<div id="myhome-community-corner-edit"><a href="<?=Title::newFromText('community-corner', NS_MEDIAWIKI)->getLocalURL('action=edit')?>" rel="nofollow"><?=wfMsg('myhome-community-corner-edit')?></a></div>
 <?php } ?>
