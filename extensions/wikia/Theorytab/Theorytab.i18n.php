a8d56765348eb4524a59c582f23a616d8d2004b6
tor <tor@wikia-inc.com>
2009-12-01 13:57:06 +0000

    i18n oops :P
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15609 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Theorytab/Theorytab.i18n.php
+++ extensions/wikia/Theorytab/Theorytab.i18n.php
@@ -2,43 +2,43 @@
 
 $messages = array();
 
-$messages['en'] => array (
+$messages['en'] = array (
 	"theorytab-subpage" => "/Theories",
 	"theorytab-title" => "Theory",
 	"theorytab-parent-title" => "Parent article"
 );
 
-$messages['de'] => array (
+$messages['de'] = array (
 	"theorytab-subpage" => "/Theories",
 	"theorytab-title" => "Theorien",
 	"theorytab-parent-title" => "Hauptartikel"
 );
 
-$messages['es'] => array (
+$messages['es'] = array (
 	"theorytab-subpage" => "/Theories",
 	"theorytab-title" => "Teorias",
 	"theorytab-parent-title" => "Articulo Principal" 
 );
 
-$messages['fr'] => array (
+$messages['fr'] = array (
 	"theorytab-subpage" => "/Theories",
 	"theorytab-title" => "Theories",
 	"theorytab-parent-title" => "Article Principal"
 );
 
-$messages['it'] => array (
+$messages['it'] = array (
 	"theorytab-subpage" => "/Theories",
 	"theorytab-title" => "Teorie",
 	"theorytab-parent-title" => "Articolo principale"
 );
 
-$messages['nl'] => array (
+$messages['nl'] = array (
 	"theorytab-subpage" => "/Theories",
 	"theorytab-title" => "Theorieen",
 	"theorytab-parent-title" => "Hoofd Artikel"
 );
 
-$messages['pt-br'] => array (
+$messages['pt-br'] = array (
 	"theorytab-subpage" => "/Theories",
 	"theorytab-title" => "Teorias",
 	"theorytab-parent-title" => "Artigo Princdipal"
