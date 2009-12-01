f06fbe4a84309fb1f2ee6cf18930908cc03c48d4
macbre <macbre@wikia-inc.com>
2009-12-01 11:58:23 +0000

    RTE: add dragdrop plugin to trigger custom "dropped" event
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15602 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RTE/js/RTE.js
+++ extensions/wikia/RTE/js/RTE.js
@@ -36,6 +36,7 @@ window.RTE = {
 	// list of our RTE custom plugins (stored in js/plugins) to be loaded on editor init
 	plugins: [
 		'comment',
+		'dragdrop',
 		'entities',
 		'image',
 		'justify',
@@ -230,9 +231,6 @@ window.RTE = {
 
 		// set ID, so CSS rules from MW can be applied
 		body.attr('id', RTE.instance.config.bodyId);
-
-		// setup drag&drop
-		RTE.setupDragNDrop();
 	},
 
 	// reposition of #RTEStuff div when Widescreen button is pressed
@@ -245,44 +243,6 @@ window.RTE = {
 		});
 	},
 
-	// setup drag&drop support
-	setupDragNDrop: function() {
-		// fire "dropped" custom event when element is drag&drop-ed
-		// mark dragged element with _rte_dragged attribute
-		RTE.getEditor().unbind('.dnd').bind('dragdrop.dnd', function(ev) {
-			setTimeout(function() {
-				RTE.log(ev);
-
-				// get dragged element
-				var draggedElement = RTE.getEditor().find('[_rte_dragged]');
-
-				RTE.log('drag&drop: dropped');
-				RTE.log(draggedElement);
-
-				// get coordinates from "dragdrop" event and send it with "dropped" event
-				// @see http://www.quirksmode.org/js/events_properties.html#position
-				var extra = {
-					pageX: (ev.pageX ? ev.pageX : ev.clientX),
-					pageY: (ev.pageY ? ev.pageY : ev.clientY)
-				};
-
-				// remove "marking" attribute and trigger event handler
-				RTE.log('drag&drop: triggering "dropped" event...');
-				draggedElement.removeAttr('_rte_dragged').trigger('dropped', extra);
-			}, 500);
-		}).bind('mousedown.dnd', function(ev) {
-			var target = $(ev.target);
-
-			// "mark" dragged element
-			target.attr('_rte_dragged', true);
-
-		}).bind('mouseup.dnd', function(ev) {
-			var target = $(ev.target);
-
-			// remove "marking" attribute
-			target.removeAttr('_rte_dragged');
-		});
-	},
 
 	// get jQuery object wrapping body of editor' iframe
 	getEditor: function() {
