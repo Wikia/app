0d02a4f10dcc5fc37255169802b13036ab121d57
meitar <meitar@wikia-inc.com>
2009-12-01 01:04:10 +0000

    Remove all PHP short tags in Ajax login template; they're non-portable.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15590 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.tmpl.php
@@ -3,32 +3,32 @@ global $wgAuth, $wgUser, $wgEnableEmail;
 $titleObj = SpecialPage::getTitleFor( 'Userlogin' );
 $link = $titleObj->getLocalUrl('type=signup');
 ?>
-<div id="AjaxLoginBox" title="<?= wfMsg('login') ?>">
+<div id="AjaxLoginBox" title="<?php echo wfMsg('login') ?>">
 	<form action="" method="post" style="margin: 10px;" name="userajaxloginform">
 		<div id="wpError"></div>
-		<label for="wpName1" style="display: block; font-weight: bold;"><?= wfMsg("yourname") ?></label> 
+		<label for="wpName1" style="display: block; font-weight: bold;"><?php echo wfMsg("yourname") ?></label> 
 		<table>
 		<tr>
 			<td id="ajaxlogin_username_cell"></td>
-			<td><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>" style="font-size: 9pt;"><?= wfMsg('nologinlink') ?></a></td>
+			<td><a id="wpAjaxRegister" href="<?php echo htmlspecialchars($link) ?>" style="font-size: 9pt;"><?php echo wfMsg('nologinlink') ?></a></td>
 		</tr>
 		</table>
-		<label for="wpPassword1" style="display: block; font-weight: bold; margin-top: 8px"><?= wfMsg("yourpassword") ?></label>
+		<label for="wpPassword1" style="display: block; font-weight: bold; margin-top: 8px"><?php echo wfMsg("yourpassword") ?></label>
 		<table>
 		<tr>
 			<td id="ajaxlogin_password_cell"></td>
 		<?php if( $wgEnableEmail && $wgAuth->allowPasswordChange() ) { ?>
-			<td><a id="wpMailmypassword" href="#" style="font-size: 9pt;" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();"><?= wfMsg('mailmypassword') ?></a></td>
+			<td><a id="wpMailmypassword" href="#" style="font-size: 9pt;" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();"><?php echo wfMsg('mailmypassword') ?></a></td>
 			</td>
 		<?php } ?>
 		</tr>
 		</table>
 		<div style="margin: 15px 0;">
-			<label for="wpRemember1" style="padding-left: 5px"><?= wfMsg('remembermypassword') ?></label>
+			<label for="wpRemember1" style="padding-left: 5px"><?php echo wfMsg('remembermypassword') ?></label>
 		</div>
 		<input type="submit" value="Login" style="position: absolute; left: -10000px; width: 0" />
 	</form>
 	<div class="modalToolbar neutral">
-		<a id="wpLoginattempt" class="wikia_button" href="#" onclick="AjaxLogin.action='login'; AjaxLogin.form.submit();" tabindex="105"><span><?= wfMsg("login") ?></span></a>
+		<a id="wpLoginattempt" class="wikia_button" href="#" onclick="AjaxLogin.action='login'; AjaxLogin.form.submit();" tabindex="105"><span><?php echo wfMsg("login") ?></span></a>
 	</div>
 </div>
