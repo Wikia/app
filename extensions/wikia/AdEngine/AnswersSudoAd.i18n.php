5bf61a75ac0a571ebb287a0aea860eb821b1f86c
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-02 12:48:37 +0000

    rt#28908: Magic, msg key was too long.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15669 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AnswersSudoAd.i18n.php
+++ extensions/wikia/AdEngine/AnswersSudoAd.i18n.php
@@ -4,6 +4,6 @@ $messages = array();
 
 $messages["en"] = array(
 	"asa-leaderboard" => "leaderboard with ASA::HOMEPAGE_NEW_QUESTIONS ASA::HOMEPAGE_RECENTLY_ANSWERED_QUESTIONS",
-	"asa-leaderboard-list-of-images" => "http://image1.jpeg http://image2.jpeg",
+	"asa-leaderboard-list" => "http://image1.jpeg http://image2.jpeg",
 	"asa-leaderboard-image" => '$1',
 );
