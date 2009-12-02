67a2e448fcb71daa48a6ce2282eabc9ca6e3c928
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-02 14:09:00 +0000

    WikiStickies: more mockup related changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15685 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiStickies/WikiStickies.i18n.php
+++ extensions/wikia/WikiStickies/WikiStickies.i18n.php
@@ -14,5 +14,6 @@ $messages['en'] = array(
 	'wikistickies-more' => 'see more',
 	'wikistickies-admins-hd' => 'Admins only',
 	'wikistickies-admins-msg' => 'These aren\'t your typical WikiStickies. These are things that can only be done by administrators of the wiki that influence how everyone sees the wiki.',
-	'wikistickies-logo-hd' => 'Upload a logo',		
+	'wikistickies-logo-hd' => 'Logo',		
+	'wikistickies-theme-hd' => 'Theme',
 );
