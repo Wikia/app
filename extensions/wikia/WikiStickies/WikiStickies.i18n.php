4b50a314cc221e450ef743eb215af0c00c4d037e
bartek <bartek@wikia-inc.com>
2009-12-02 10:47:10 +0000

    WikiStickies: typo
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15646 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiStickies/WikiStickies.i18n.php
+++ extensions/wikia/WikiStickies/WikiStickies.i18n.php
@@ -9,6 +9,6 @@ $messages['en'] = array(
 	'wikistickies-wantedpages-hd' => 'Can you start these pages?',
 	'wikistickies-more' => 'see more',
 	'wikistickies-admins-hd' => 'Admins only',
-	'wikistickies-admins-msg' => 'These aren\'t your typical WikiStickies. These are things that can only be done by administrators of the wiki that influence how everyone sees the wiki.'
+	'wikistickies-admins-msg' => 'These aren\'t your typical WikiStickies. These are things that can only be done by administrators of the wiki that influence how everyone sees the wiki.',
 	'wikistickies-logo-hd' => 'Upload a logo',		
 );
