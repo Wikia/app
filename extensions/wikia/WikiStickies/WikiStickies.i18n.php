123672bd9e3b475a9fcb334834816d3463525835
bartek <bartek@wikia-inc.com>
2009-12-02 10:19:16 +0000

    WikiStickies: new, modified messages from newest mockup plus getting 11 instead of 3 results (will be apparent later)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15640 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiStickies/WikiStickies.i18n.php
+++ extensions/wikia/WikiStickies/WikiStickies.i18n.php
@@ -4,8 +4,11 @@ $messages = array();
 
 $messages['en'] = array(
 	'wikistickies' => 'WikiStickies',
-	'wikistickies-newpages-hd' => 'Add information to a new page',
-	'wikistickies-wantedimages-hd' => 'Add a new picture',
-	'wikistickies-wantedpages-hd' => 'Start a new page',
+	'wikistickies-newpages-hd' => 'Can you add to these short pages?',
+	'wikistickies-wantedimages-hd' => 'Cab you add images to these pages?',
+	'wikistickies-wantedpages-hd' => 'Can you start these pages?',
+	'wikistickies-more' => 'see more',
+	'wikistickies-admins-hd' => 'Admins only',
+	'wikistickies-admins-msg' => 'These aren\'t your typical WikiStickies. These are things that can only be done by administrators of the wiki that influence how everyone sees the wiki.'
 	'wikistickies-logo-hd' => 'Upload a logo',		
 );
