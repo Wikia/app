8b4a5c320b1fc5b455803ea362a0875260290201
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-02 12:03:44 +0000

    WikiStickies: more changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15662 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiStickies/templates/tools.tmpl.php
+++ extensions/wikia/WikiStickies/templates/tools.tmpl.php
@@ -3,45 +3,12 @@
 
 <div class="wikiheadertools">
 <ul>
-<!-- ############## Add a logo ############ -->
-<li id="step2" class="step">
-<div class="wikistickiesheader"><?php echo wfMsg("nwb-step2-headline")?></div>
-<div class="wrapper clearfix">
-        <?php echo wfMsg("nwb-step2-text")?>
-        <!-- Hidden iframe to handle the file upload -->
-        <iframe id="hidden_iframe" src="about:blank" style="display:none" name="hidden_iframe" onLoad="NWB.iframeFormUpload(this)"></iframe>
-
-        <div style="float: left;">
-        <form action="/api.php" method="post" enctype="multipart/form-data" target="hidden_iframe" onSubmit='return NWB.iframeFormInit(this)' id="logo_form">
-                <input type="hidden" name="action" value="uploadlogo">
-                <input type="hidden" name="format" value="xml">
-                <input id="logo_article" type="hidden" name="title" value="Wiki.png">
-                <label><?php echo wfMsg("nwb-choose-logo")?>:</label><input type="file" name="logo_file" id="logo_file" onclick="WET.byStr('nwb/step2browse');"/> <input type="submit" value="<?php echo wfMsg("nwb-preview")?>" onclick="WET.byStr('nwb/step2preview');this.form.title.value='Wiki-Preview.png'"/>
-                <!--<input type="submit" value="Save" onclick="this.form.title.value='Wiki.png'"/>-->
-        </form>
-
-        <div id="logo_preview_wrapper">
-                <label><?php echo wfMsg("nwb-logo-preview")?>:</label>
-                <div id="logo_preview"></div>
-        </div>
-
-        </div><!--float-->
-        <div class="accent note">
-                <img src="/extensions/wikia/NewWikiBuilder/sample_logo.jpg" id="sample_logo" /><br />
-                <?php echo wfMsg("nwb-step2-example")?>
-        </div>
-</div>
-<div class="nav">
-        <button onclick="WET.byStr('nwb/step2save');NWB.uploadLogo();"><span><?php echo wfMsg("nwb-save-logo")?></span></button>
-</div>
-</li>
 
 <!-- ############## Pick Theme ############## -->
 
 <li id="step3" class="step">
 <div class="wikistickiesheader"><?php echo wfMsg("nwb-step3-headline")?></div>
 <div class="wrapper clearfix">
-        <?php echo wfMsg("nwb-step3-text")?>
         <div id="theme_template" style="display:none" class="theme_selekction">
                 <label for="theme_radio_$theme"><img id="theme_preview_image_$theme" /></label>
                 <input onclick="NWB.changeTheme('monaco-$theme', false)" type="radio" name="theme" value="monaco-$theme" id="theme_radio_$theme"> <label for="theme_radio_$theme">$Theme</label>
@@ -80,5 +47,35 @@ for (var i = 0; i < themes.length; i++){
         <input onclick="NWB.changeTheme($('input[name=theme]:checked').val(), true);WET.byStr('nwb/step3save');NWB.gotostep(4);" type="button" id="hidden_theme_submit" style="display:none"><!-- For selenium tests -->
 </div>
 </li>
+
+<!-- ############## Add a logo ############ -->
+<li id="step2" class="step">
+<div class="wikistickiesheader"><?php echo wfMsg("nwb-step2-headline")?></div>
+<div class="wrapper clearfix">
+        <!-- Hidden iframe to handle the file upload -->
+        <iframe id="hidden_iframe" src="about:blank" style="display:none" name="hidden_iframe" onLoad="NWB.iframeFormUpload(this)"></iframe>
+
+        <div style="float: left;">
+        <form action="/api.php" method="post" enctype="multipart/form-data" target="hidden_iframe" onSubmit='return NWB.iframeFormInit(this)' id="logo_form">
+                <input type="hidden" name="action" value="uploadlogo">
+                <input type="hidden" name="format" value="xml">
+                <input id="logo_article" type="hidden" name="title" value="Wiki.png">
+                <input type="file" name="logo_file" id="logo_file" onclick="WET.byStr('nwb/step2browse');"/> <input type="submit" value="<?php echo wfMsg("nwb-preview")?>" onclick="WET.byStr('nwb/step2preview');this.form.title.value='Wiki-Preview.png'"/>
+                <!--<input type="submit" value="Save" onclick="this.form.title.value='Wiki.png'"/>-->
+        </form>
+
+        <div id="logo_preview_wrapper">
+                <label><?php echo wfMsg("nwb-logo-preview")?>:</label>
+                <div id="logo_preview"></div>
+        </div>
+
+        </div><!--float-->
+</div>
+<div class="nav">
+        <button onclick="WET.byStr('nwb/step2save');NWB.uploadLogo();"><span><?php echo wfMsg("nwb-save-logo")?></span></button>
+</div>
+</li>
+
+
 </ul>
 </div>
