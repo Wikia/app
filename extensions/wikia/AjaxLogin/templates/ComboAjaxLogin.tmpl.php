7d7f432e4c280c73908e5234bda6a9d4c76eaa51
tomek <tomek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-02 13:24:01 +0000

    Login Friction Part C
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15678 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AjaxLogin/templates/ComboAjaxLogin.tmpl.php
@@ -0,0 +1,54 @@
+<?php
+global $wgAuth, $wgUser, $wgEnableEmail;
+
+$titleObj = SpecialPage::getTitleFor( 'Userlogin' );
+$link = $titleObj->getLocalUrl('type=signup');
+?>
+<div id="AjaxLoginBox" title="<?= wfMsg('login') ?>">
+	<div>TEXT</div>
+	<div id="AjaxLoginButtons" title="<?= wfMsg('login') ?>">
+			<a id="wpGoLogina" class="wikia_button ajaxregister_button" href="#" onclick="AjaxLogin.showLogin(this);" >
+				<span><?= wfMsg("login") ?></span>
+			</a>
+			<a id="wpGoRegister" class="wikia_button ajaxregister_button" href="#" onclick="AjaxLogin.showRegister(this);" >
+				<span><?= wfMsg("login") ?></span>
+			</a>
+	</div>
+	<div id="AjaxLoginLoginForm" style="display:none" title="<?= wfMsg('login') ?>">
+		<form action="" method="post" style="margin: 10px;" name="userajaxloginform">
+			<div id="wpError"></div>
+			<label for="wpName1" style="display: block; font-weight: bold;"><?= wfMsg("yourname") ?></label> 
+			<table>
+			<tr style="width:350px" >
+				<td id="ajaxlogin_username_cell">
+					
+				</td>
+				<td><a id="wpAjaxRegister" href="<?= htmlspecialchars($link) ?>" style="font-size: 9pt;"><?= wfMsg('nologinlink') ?></a></td>
+			</tr>
+			</table>
+			<label for="wpPassword1" style="display: block; font-weight: bold; margin-top: 8px"><?= wfMsg("yourpassword") ?></label>
+			<table>
+			<tr>
+				<td id="ajaxlogin_password_cell">
+					
+				</td>
+			<?php if( $wgEnableEmail && $wgAuth->allowPasswordChange() ) { ?>
+				<td><a id="wpMailmypassword" href="#" style="font-size: 9pt;" onclick="AjaxLogin.action='password'; AjaxLogin.form.submit();"><?= wfMsg('mailmypassword') ?></a></td>
+				</td>
+			<?php } ?>
+			</tr>
+			</table>
+			<div style="margin: 15px 0;">
+				
+				<label for="wpRemember1" style="padding-left: 5px"><?= wfMsg('remembermypassword') ?></label>
+			</div>
+			<input type="submit" value="Login" style="position: absolute; left: -10000px; width: 0" />
+		</form>
+		<div class="modalToolbar neutral">
+			<a id="wpLoginattempt" class="wikia_button" href="#" onclick="AjaxLogin.action='login'; AjaxLogin.form.submit();" ><span><?= wfMsg("login") ?></span></a>
+		</div>
+	</div>
+	<div id="AjaxLoginRegisterForm" style="display:none" title="<?= wfMsg('login') ?>">
+		<?php echo $registerAjax; ?>
+	</div>	
+</div>
