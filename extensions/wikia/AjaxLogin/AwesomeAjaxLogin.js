8f9aab9a94b3fed2f68913c32b8b9c872c6d0179
tomek <tomek@wikia-inc.com>
2009-12-02 13:38:24 +0000

    revert Login Friction Part C
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15681 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/AwesomeAjaxLogin.js
+++ extensions/wikia/AjaxLogin/AwesomeAjaxLogin.js
@@ -1,6 +1,5 @@
 /*
  * Author: Maciej Brencz (based on code from Inez Korczynski)
- * Mod by: Tomasz Odrobny
  */
 var AjaxLogin = {
 	init: function(form) {
@@ -8,12 +7,12 @@ var AjaxLogin = {
 
 		// move login/password/remember fields from hidden form to AjaxLogin
 		var labels = this.form.find('label');
-		$('#wpName1Ajax').clone().attr("id","wpName2Ajax").appendTo("#ajaxlogin_username_cell");
-		$('#wpPassword1Ajax').clone().attr("id","wpPassword2Ajax").appendTo("#ajaxlogin_password_cell");
-		$('#wpRemember1Ajax').clone().attr("id","wpRemember2Ajax").insertBefore(labels[2]);
+		$('#wpName1Ajax').appendTo("#ajaxlogin_username_cell");
+		$('#wpPassword1Ajax').appendTo("#ajaxlogin_password_cell");
+		$('#wpRemember1Ajax').insertBefore(labels[2]);
 
 		// remove hidden form
-		$('#userajaxloginform').attr("id","");
+		$('#userajaxloginform').remove();
 
 		this.form.attr('id', 'userajaxloginform');
 
@@ -26,42 +25,6 @@ var AjaxLogin = {
 		$('#wpAjaxRegister').click(this.ajaxRegisterConfirm);
 		// get proper returnto/returntoquery param
 		$('#wpAjaxRegister').attr('href', $('#register').attr('href'));
-	},
-	
-	doSuccess: function(openwindow) {
-		// macbre: call custom function (if provided by any extension)
-		if (typeof window.wgAjaxLoginOnSuccess == 'function') {
-			// let's update wgUserName
-			window.wgUserName = response.ajaxlogin.lgusername;
-
-			// close AjaxLogin form
-			$('#AjaxLoginBoxWrapper').closeModal();
-
-			$().log('AjaxLogin: calling custom function');
-			window.wgAjaxLoginOnSuccess();
-			return;
-		}
-
-		// we're on edit page
-		if($('#wpPreview').exists() && $('#wpLogin').exists()) {
-			if ($('#wikiDiff').children().exists()) {  
-				$('#wpDiff').click();
-			} else {
-				if (!$('#wikiPreview').children().exists()) {
-					$('#wpLogin').attr('value', 1);
-				}
-				$('#wpPreview').click();
-			}
-		} else {
-			if(wgCanonicalSpecialPageName == "Userlogout") {
-				window.location.href = wgServer + wgScriptPath;
-			} else {
-				AjaxLogin.doReload();
-			}
-		}
-	},
-	doReload: function() {
-		window.location.reload(true);
 	},
 	formSubmitHandler: function(ev) {
 		// Prevent the default action for event (submit of form)
@@ -84,6 +47,7 @@ var AjaxLogin = {
 
 		// Let's block login form (disable buttons and input boxes)
 		AjaxLogin.blockLoginForm(true);
+
 		$.postJSON(window.wgScriptPath + '/api.php?' + params.join('&'), function(response) {
 					var responseResult = response.ajaxlogin.result;
 					switch(responseResult) {
@@ -116,8 +80,38 @@ var AjaxLogin = {
 						WET.byStr('signupActions/popup/emailpassword/success');
 					} else {
 						WET.byStr('signupActions/popup/login/success');
+					}					
+					
+					// macbre: call custom function (if provided by any extension)
+					if (typeof window.wgAjaxLoginOnSuccess == 'function') {
+						// let's update wgUserName
+						window.wgUserName = response.ajaxlogin.lgusername;
+
+						// close AjaxLogin form
+						$('#AjaxLoginBoxWrapper').closeModal();
+
+						$().log('AjaxLogin: calling custom function');
+						window.wgAjaxLoginOnSuccess();
+						return;
 					}
-						AjaxLogin.doSuccess();
+
+					// we're on edit page
+					if($('#wpPreview').exists() && $('#wpLogin').exists()) {
+						if ($('#wikiDiff').children().exists()) {
+							$('#wpDiff').click();
+						} else {
+							if (!$('#wikiPreview').children().exists()) {
+								$('#wpLogin').attr('value', 1);
+							}
+							$('#wpPreview').click();
+						}
+					} else {
+						if(wgCanonicalSpecialPageName == "Userlogout") {
+							window.location.href = wgServer + wgScriptPath;
+						} else {
+							window.location.reload(true);
+						}
+					}
 					break;
 
 					case 'NotExists':
@@ -166,127 +160,5 @@ var AjaxLogin = {
 			}
 		}
 		WET.byStr('signupActions/popup/switchtocreateaccount');
-	},
-	showFromDOM: function() {
-		$('#AjaxLoginBox').showModal();
-		$('#wpName1Ajax').focus();
-		return true
 	}
 };
-
-if ( (typeof wgComboAjaxLogin != 'undefined') && wgComboAjaxLogin ) {
-	/* clear repted names */ 
-	
-	/* expend AjaxLogin object for combo ajax login */
-	$.extend(AjaxLogin,{
-				clicked : 0,
-				placeholderID : null,
-				getNewTop: function(newHeight){
-					var modalTop = (($(window).height() - newHeight) / 2) + $(window).scrollTop();
-					if (modalTop < $(window).scrollTop() + 20) {
-						return $(window).scrollTop() + 20;
-					} else {
-						return modalTop;
-					}
-				},
-				getNewMarginLeft: function(newWidth){
-					return  -newWidth / 2;
-				},
-				showRegister: function() {
-					$('#wpGoRegister').addClass('ajaxregister_button_enable');
-					$('#wpGoLogina').removeClass('ajaxregister_button_enable');
-					$('#AjaxLoginLoginForm').hide();
-			        $("#AjaxLoginBoxWrapper").animate({ 
-			        	'width': "700px",
-			        	'marginLeft': parseInt(this.getNewMarginLeft(709))+"px",  
-			        	'top':  parseInt(this.getNewTop(390))+"px"
-			        }, 500 );
-					$('#AjaxLoginRegisterForm').show("fast");
-				},
-				showLogin : function() {
-					$('#wpGoLogina').addClass('ajaxregister_button_enable');
-					$('#wpGoRegister').removeClass('ajaxregister_button_enable');
-					$('#AjaxLoginRegisterForm').hide();
-					$("#AjaxLoginBoxWrapper").animate({ 
-				          'width': "320px",
-				          'marginLeft': parseInt(this.getNewMarginLeft(329))+"px",
-				          'top':  parseInt(this.getNewTop(240))+"px"    
-						}, 500 );
-					$('#AjaxLoginLoginForm').show("fast");
-				},
-				showFromDOM: function() {	
-					if ($('#AjaxLoginButtons').length > 0)
-					{
-						$('#AjaxLoginBoxWrapper').remove();
-						return false;
-					}
-					$('#AjaxLoginBox').showModal();
-					$('#wpName1Ajax').focus();
-					return true;
-				},
-				showComboFromDOM: function() {
-					if ($('#AjaxLoginButtons').length > 0)
-					{
-						$('#AjaxLoginBox').showModal();
-						$('#wpName1Ajax').focus();
-						return true;						
-					}
-					/* remove normal ajax login to show combo (id)*/
-					$('#AjaxLoginBoxWrapper').remove();
-					return false;
-				},
-				setPlaceHolder: function(id) {
-					AjaxLogin.placeholderID = id;
-				},
-				doReload: function(){
-					params = "";
-					if( AjaxLogin.placeholderID !== null){
-						params = "placeholder=" + AjaxLogin.placeholderID;
-					}
-					
-					if (window.location.href.indexOf("?") > 0) {
-						window.location.href = window.location.href.replace("#","") + ("&" + params);
-						return;
-					}
-					window.location.href = window.location.href.replace("#","") + ("?" + params);
-					
-				}
-			}); 
-	//override submitForm for submitForm
-	if (typeof UserRegistration != 'undefined') 
-	{
-		UserRegistration.submitForm = function() {
-			if (UserRegistration.checkForm()) {
-				$("#userloginErrorBox").hide();
-				 $.ajax({
-					   type: "POST",
-					   dataType: "json",
-					   url: window.wgScriptPath  + "/index.php",
-					   data: $("#userajaxregisterform").serialize() + "&action=ajax&rs=createUserLogin",
-					   success: function(msg){
-					 		/* post data to normal form if age < 13 */
-					 		if (msg.type === "redirectQuery") {
-					 			$('#userajaxregisterform').submit();
-					 			return ;
-					 		}
-					 
-					 		if( msg.type != "error" ) {
-					 			AjaxLogin.doSuccess();
-					 			return ;
-					 		}
-					 		
-					 		$('#userloginInnerErrorBox').empty().append(msg.msg);
-					 		$("#userloginErrorBox").show();
-					 		$(".captcha img").attr("src",msg.captchaUrl);
-					 		$("#wpCaptchaId").val(msg.captcha);
-					   }
-					 });
-			} else {
-				$("#userloginErrorBox").show();
-				WET.byStr('signupActions/signup/createaccount/failure');
-			}
-		}
-	}
-}
-
-
