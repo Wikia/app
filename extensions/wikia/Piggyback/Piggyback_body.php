6d3172c8fabafa8c36a5293981e49b7ec40ed632
emil <emil@wikia-inc.com>
2009-12-03 08:47:17 +0000

    make the special page unlisted code formatting
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15722 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Piggyback/Piggyback_body.php
+++ extensions/wikia/Piggyback/Piggyback_body.php
@@ -1,14 +1,13 @@
 <?php
 
-class Piggyback extends SpecialPage
-{
+class Piggyback extends SpecialPage {
 	var $mAction;
 
-	function __construct(){
+	function __construct() {
 		global $wgRequest;
-		parent::__construct( 'Piggyback','piggyback' );
+		parent::__construct( 'Piggyback', 'piggyback', false );
 		$this->mAction = $wgRequest->getVal( 'action' );
-		wfLoadExtensionMessages('Piggyback');
+		wfLoadExtensionMessages( 'Piggyback' );
 	}
 
 	function execute( $par ){
@@ -19,16 +18,16 @@ class Piggyback extends SpecialPage
 			return;
 		}
 
-		if (PBLoginForm::isPiggyback()) {
+		if( PBLoginForm::isPiggyback() ) {
 			$wgOut->setPageTitle( wfMsg( 'badaccess' ) );
-			$wgOut->addHtml(wfMsg('piggyback-firstlogout'));
+			$wgOut->addHtml( wfMsg( 'piggyback-firstlogout') );
 			$wgOut->returnToMain();
-			return  ;
+			return;
 		}
 
 		$this->setHeaders();
-		$LoginForm = new PBLoginForm($wgRequest);
-		if ($this->mAction == 'submitlogin' && ($wgRequest->wasPosted())) {
+		$LoginForm = new PBLoginForm( $wgRequest );
+		if( $this->mAction == 'submitlogin' && $wgRequest->wasPosted() ) {
 			$LoginForm->validPiggyback();
 		}
 
@@ -41,28 +40,27 @@ class Piggyback extends SpecialPage
  * overload LoginForm to not re-implement user verification
  */
 
-class PBLoginForm extends LoginForm
-{
-	var $mOtherName,$exTemplate,$plugin;
+class PBLoginForm extends LoginForm {
+	var $mOtherName, $exTemplate, $plugin;
 
-	function __construct(&$request) {
+	function __construct( &$request ) {
 		global $wgUser;
 
-		$this->titleObj = SpecialPage::getTitleFor( 'Piggyback');
+		$this->titleObj = SpecialPage::getTitleFor( 'Piggyback' );
 		$this->plugin = new PiggybackTemplate();
 		$this->exTemplate =  new UserloginTemplate();
 		$this->exTemplate->set( 'actionlogin', $this->titleObj->getLocalUrl( 'action=submitlogin' ) );
 
-		$this->mOtherName = $request->getVal('wpOtherName');
-		parent::LoginForm(&$request);
+		$this->mOtherName = $request->getVal( 'wpOtherName' );
+		parent::LoginForm( &$request );
 		$this->mType = "login";
 		/* fake to don't change remember password */
-		$this->mRemember = (bool)$wgUser->getOption( 'rememberpassword' );
+		$this->mRemember = (bool) $wgUser->getOption( 'rememberpassword' );
 	}
 
-	function mainLoginForm($msg, $msgtype = 'error' ) {
-		$this->exTemplate->set('messagetype', $msgtype );
-		$this->exTemplate->set('message',$msg);
+	function mainLoginForm( $msg, $msgtype = 'error' ) {
+		$this->exTemplate->set( 'messagetype', $msgtype );
+		$this->exTemplate->set( 'message', $msg );
 		$this->exTemplate->set( 'name', $this->mName );
 		$this->exTemplate->set( 'password', $this->mPassword );
 		$this->plugin->set( 'otherName', $this->mOtherName );
@@ -75,33 +73,27 @@ class PBLoginForm extends LoginForm
 		$u = User::newFromName( $this->mOtherName );
 
 		if (  $u->getId()  == 0 ) {
-			$this->mainLoginForm(wfMsg( 'piggyback-nosuchuser', htmlspecialchars( $this->mOtherName ) ));
+			$this->mainLoginForm( wfMsg( 'piggyback-nosuchuser', htmlspecialchars( $this->mOtherName ) ) );
 			return ;
 		}
 
 		if ( $u->getId()  == $wgUser->getId() ) {
-			$this->mainLoginForm(wfMsg( 'piggyback-itisyou'));
+			$this->mainLoginForm( wfMsg( 'piggyback-itisyou') );
 			return ;
 		}
 
-		/*		if (  $u->isAllowed('piggyback') )
-		 {
-			$this->mainLoginForm(wfMsg( 'issysopto', htmlspecialchars( $this->mOtherName ) ));
-			return ;
-			} */
-
-		$wgRequest->setSessionData("PgParentUser",$wgUser->getID());
+		$wgRequest->setSessionData( "PgParentUser", $wgUser->getID() );
 		
-		wfRunHooks('PiggybackLogIn',array($wgUser,$u));
+		wfRunHooks( 'PiggybackLogIn',array( $wgUser, $u) );
 		
 		$log = new LogPage( 'piggyback' );
 		$log->addEntry( 'piggyback', SpecialPage::getTitleFor( 'Piggyback') , "login ".$wgUser->getName()." to ".$u->getName(),  array() );
 
-		$this->switchUser($u);
+		$this->switchUser( $u );
 	}
 
 	function switchUser($u) {
-		global $wgUser, $wgAuth,$wgOut,$wgRequest,$wgLang;
+		global $wgUser, $wgAuth, $wgOut, $wgRequest, $wgLang;
 		$oldUserlang = $wgUser->getOption( 'language' );
 		$wgAuth->updateUser( $u );
 		$wgUser = $u;
@@ -113,55 +105,51 @@ class PBLoginForm extends LoginForm
 			 *
 			 */
 			if( $this->hasSessionCookie() || $this->mSkipCookieCheck ) {
-				$code = $wgRequest->getVal( 'uselang',$oldUserlang );
+				$code = $wgRequest->getVal( 'uselang', $oldUserlang );
 				$wgLang = Language::factory( $code );
-			} else
-			{
+			} else {
 				return $this->cookieRedirectCheck( 'login' );
 			}
 		}
 
-		$userpage = Title::makeTitle( NS_USER, $wgUser->getName());
+		$userpage = Title::makeTitle( NS_USER, $wgUser->getName() );
 		$wgOut->redirect( $userpage->getLocalUrl() );
 	}
 
 	function validPiggyback() {
 		global $wgUser;
 		/* pre valid */
-		$cUserId = User :: idFromName($this->mName);
-		if (($this->mName != "") && $wgUser->getID() != $cUserId) {
-			$this->mainLoginForm(wfMsg( 'piggyback-wronguser', htmlspecialchars( $this->mName ) ));
+		$cUserId = User::idFromName( $this->mName );
+		if( $this->mName != "" && $wgUser->getID() != $cUserId ) {
+			$this->mainLoginForm( wfMsg( 'piggyback-wronguser', htmlspecialchars( $this->mName ) ) );
 			return ;
 		}
 		
-		if( ($this->mName != "") && ($cUserId == 0)) {
-			$this->mainLoginForm(wfMsg( 'piggyback-nosuchuser' ));
+		if( $this->mName != "" && $cUserId == 0 ) {
+			$this->mainLoginForm( wfMsg( 'piggyback-nosuchuser' ) );
 		}
 		
 		$this->processLogin();
 	}
 
 	function render() {
-		global $wgOut,$wgScriptPath;
-		$this->exTemplate->set("header",$this->plugin);
-		$wgOut->addStyle("$wgScriptPath/extensions/wikia/Piggyback/Resources/Piggyback.css");
-		$wgOut->addTemplate($this->exTemplate);
+		global $wgOut, $wgExtensionsPath;
+		$this->exTemplate->set( "header", $this->plugin );
+		$wgOut->addStyle( "$wgExtensionsPath/wikia/Piggyback/Piggyback.css" );
+		$wgOut->addTemplate( $this->exTemplate );
 	}
 
 	static function isPiggyback() {
 		global $wgRequest;
-		return $wgRequest->getSessionData("PgParentUser") != null;
+		return $wgRequest->getSessionData( "PgParentUser" ) != null;
 	}
 
-	function goToParent($oldName) {
+	function goToParent( $oldName ) {
 		global $wgRequest;
-		$u = User::newFromId( $wgRequest->getSessionData("PgParentUser") );
+		$u = User::newFromId( $wgRequest->getSessionData( "PgParentUser" ) );
 		$log = new LogPage( 'piggyback' );
-		$this->switchUser($u);
-		/*	$log->addEntry( 'piggyback', SpecialPage::getTitleFor( 'Piggyback') ,"logout ".$u->getName()." from ".$oldName, array() ); */
-		$wgRequest->setSessionData("PgParentUser",null);
+		$this->switchUser( $u );
+		//$log->addEntry( 'piggyback', SpecialPage::getTitleFor( 'Piggyback' ), "logout ".$u->getName()." from ".$oldName, array() );
+		$wgRequest->setSessionData( "PgParentUser" , null );
 	}
 }
-
-
-
