7a67fff7c6ce594b7346d097689df0178d21b1f8
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-03 23:27:55 +0000

    if there is more than 20 images on the page count it as no-images
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15794 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiStickies/ApiQueryWantedimages.php
+++ extensions/wikia/WikiStickies/ApiQueryWantedimages.php
@@ -11,54 +11,75 @@ class ApiQueryWantedimages extends ApiQueryBase {
 		parent :: __construct($query, $moduleName, 'wi');
 	}
 
+	/*
+	 * returns true if the given article contains less then $threshold articles
+	 */
+	private function containsImages( $page, $threshold ) {
+		return $this->getImageCount( $page ) > 0 && $this->getImageCount( $page ) < $threshold;
+	}
+
+	private function getImageCount( $page ) {
+		if( isset( $this->image_cnt[$page]) ) {
+			return $this->image_cnt[$page];
+		}
+
+		global $wgMemc;
+
+		$memcKey = wfMemcKey( 'ws_image_cnt', $page );
+		$this->image_cnt[$page] = $wgMemc->get( $memcKey );
+		if( $this->image_cnt[$page] === null ) {
+			$dbr = wfGetDB( DB_SLAVE, 'api' );
+			$this->image_cnt[$page] = $dbr->selectField(
+					'imagelinks',
+					'count(*) as cnt',
+					array( 'il_from' => $page ),
+					__METHOD__ );
+			$wgMemc->set( $memcKey, $this->image_cnt[$page], 60*60*12 );
+		}
+		return $this->image_cnt[$page];
+	}
+
 	public function execute() {
 		global $wgContentNamespaces;
 
 		$db = $this->getDB();
 		$params = $this->extractRequestParams();
 
-                $this->addFields( array( 'page_title', 'page_namespace' ) );
-                $this->addTables( 'querycache' );
-                $this->addTables( 'page' );
-                $this->addTables( 'imagelinks' );
+		$this->addFields( array( 'page_id', 'page_title', 'page_namespace' ) );
+		$this->addTables( array( 'querycache', 'page' ) );
 		$this->addJoinConds( array(
 			'page' => array( 'JOIN', array(
 						'qc_title = page_title',
 						'qc_namespace = page_namespace' ) ),
-			'imagelinks' => array( 'LEFT JOIN', 'page_id = il_from' )
 			) );
-		$this->addWhere( array( 
-			"qc_type = 'Mostlinked'",
-			"il_to is NULL",
-			"page_namespace IN (" . implode( ",", $wgContentNamespaces ) . ")",
-			"page_is_redirect = 0",
-			) );
+		$this->addWhereFld( 'qc_type', 'Mostlinked' );
+		$this->addWhereFld( 'page_namespace', $wgContentNamespaces );
+		$this->addWhereFld( 'page_is_redirect', 0 );
 		$this->addOption( 'ORDER BY', 'qc_value DESC' );
-                $this->addOption( 'LIMIT', $params['limit'] + 1 );
 
-                $res = $this->select(__METHOD__);
-                $count = 0;
-                $result = $this->getResult();
+		$res = $this->select(__METHOD__);
+		$count = 0;
+		$result = $this->getResult();
 
-                while( $row = $db->fetchObject( $res ) ) {
-                        if (++$count > $params['limit']) {
-                                // We've reached the one extra which shows that
-                                // there are additional pages to be had. Stop here...
-                                $this->setContinueEnumParameter('continue', $row->page_title );
-                                break;
-                        }
+		while( $row = $db->fetchObject( $res ) ) {
+			if( !$this->containsImages( $row->page_id, 20 ) ) {
+				if (++$count > $params['limit']) {
+					// We've reached the one extra which shows that
+					// there are additional pages to be had. Stop here...
+					$this->setContinueEnumParameter('continue', $row->page_title );
+					break;
+				}
+				$vals['title'] = $row->page_title;
+				$vals['namespace'] = $row->page_namespace;
 
-                        $title = Title::makeTitle($row->page_namespace, $row->page_title);
-                        $vals['title'] = $row->page_title;
-                        $vals['namespace'] = $row->page_namespace;
-
-                        $fit = $this->getResult()->addValue(array('query', $this->getModuleName()), null, $vals);
-                        if(!$fit) {
-                                break;
-                        }
-                }
-                $db->freeResult( $res );
-                $this->getResult()->setIndexedTagName_internal(array('query', $this->getModuleName()), 'page');
+				$fit = $this->getResult()->addValue(array('query', $this->getModuleName()), null, $vals);
+				if(!$fit) {
+					break;
+				}
+			}
+		}
+		$db->freeResult( $res );
+		$this->getResult()->setIndexedTagName_internal(array('query', $this->getModuleName()), 'page');
 	}
 
 	public function getParamDescription() {
