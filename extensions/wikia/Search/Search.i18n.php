20bb2c90452d15a22502a533bf29505d39a57ba9
adi <adi@wikia-inc.com>
2009-12-04 16:36:28 +0000

    CentralSearch: last-minute changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@15849 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Search/Search.i18n.php
+++ extensions/wikia/Search/Search.i18n.php
@@ -5,7 +5,6 @@ $messages = array();
 $messages['en'] = array(
 	'wikiasearch-titles-only' => 'Search only in page titles',
 	'wikiasearch-system-error-msg' => 'Due to a system error, your search could not be completed',
-	'wikiasearch-search-this-wiki' => 'Search $1',
-	'wikiasearch-search-wikia' => 'Search Wikia',
-	'wikiasearch-button' => 'Search all Wikia'
+	'wikiasearch-search-this-wiki' => 'Search Wikia Central Only',
+	'wikiasearch-search-wikia' => 'Search Wikia'
 );
