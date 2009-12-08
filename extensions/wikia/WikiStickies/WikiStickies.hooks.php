2535dc5f51edbb5d02895f1da2cc71a257e0e124
emil <emil@wikia-inc.com>
2009-12-08 16:53:18 +0000

    invalidated querycache
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16009 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiStickies/WikiStickies.hooks.php
@@ -0,0 +1,79 @@
+<?php
+
+function efAddWikiSticky( &$html ) {
+	global $wgOut, $wgExtensionsPath, $wgStyleVersion;
+
+	wfLoadExtensionMessages( 'WikiStickies' );
+
+	WikiStickies::addWikiStickyResources();
+	// Special:MyHome page-specific resources
+	$wgOut->addExtensionStyle("{$wgExtensionsPath}/wikia/WikiStickies/css/WikiStickiesMyHome.css?{$wgStyleVersion}");
+
+	// fetch the feeds and rotate them
+	$feedWithoutimages = WikiStickies::getWithoutimagesFeed( WikiStickies::INITIAL_FEED_LIMIT );
+	$feedNewpages = WikiStickies::getNewpagesFeed( WikiStickies::INITIAL_FEED_LIMIT );
+	$feedWantedpages = WikiStickies::getWantedpagesFeed( WikiStickies::INITIAL_FEED_LIMIT );
+
+	$feeds = array();
+	for( $i = 0; $i < max( count( $feedWithoutimages ), count( $feedNewpages ), count( $feedWantedpages ) ); $i++ ) {
+		if( isset( $feedWithoutimages[$i] ) ) {
+			$feeds[] = array(
+					'prefix' => wfMsg( 'wikistickies-withoutimages-st' ),
+					'title' => $feedWithoutimages[$i] );
+		}
+		if( isset( $feedNewpages[$i] ) ) {
+			$feeds[] = array(
+					'prefix' => wfMsg( 'wikistickies-newpages-st' ),
+					'title' => $feedNewpages[$i] );
+		}
+		if( isset( $feedWantedpages[$i] ) ) {
+			$feeds[] = array(
+					'prefix' => wfMsg( 'wikistickies-wantedpages-st' ),
+					'title' => $feedWantedpages[$i] );
+		}
+	}
+
+	$js = "WIKIA.WikiStickies.stickies = [\n";
+	foreach( $feeds as $f ) {
+		$js .= "'" . WikiStickies::renderWikiStickyContent( $f['title'], $f['prefix'] ) . "',\n";
+	}
+	$js .= "];\n";
+	$wgOut->addInlineScript( $js );
+	$wgOut->addScriptFile("{$wgExtensionsPath}/wikia/WikiStickies/js/WikiStickiesMyHome.js");
+
+	$html = WikiStickies::renderWikiSticky( $feeds[0]['title'], $feeds[0]['prefix'] );
+
+	return true;
+}
+
+function efRemoveFromSpecialWithoutimages( &$uploadForm ) {
+	if( !($uploadForm->mLocalFile instanceof LocalFile) ) {
+		return true;
+	}
+
+	$title = $uploadForm->mLocalFile->getTitle();
+	if( !($title instanceof Title) ) {
+		return true;
+	}
+
+	$dbw = wfGetDB( DB_MASTER );
+	$dbw->delete( 'querycache', array( 
+		'qc_type'      => 'Withoutimages',
+		'qc_title'     => $title->getDBkey(),
+		'qc_namespace' => $title->getNamespace(),
+	) );
+
+	return true;
+}
+
+function efRemoveFromSpecialWantpages( &$rc ) {
+	if( $rc->mAttribs['rc_type'] == RC_NEW ) {
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->delete( 'querycache', array( 
+			'qc_type'      => 'Wantedpages',
+			'qc_title'     => $rc->mAttribs['rc_title'],
+			'qc_namespace' => $rc->mAttribs['rc_namespace'],
+		) );
+	}
+	return true;
+}
--- extensions/wikia/WikiStickies/WikiStickies.php
+++ extensions/wikia/WikiStickies/WikiStickies.php
@@ -34,52 +34,9 @@ $wgAPIListModules['wantedpages'] = 'ApiQueryWantedpages';
 $wgAPIListModules['withoutimages'] = 'ApiQueryWithoutimages';
 
 // Hooks
+include( $dir . 'WikiStickies.hooks.php' );
 $wgHooks['MyHome::sidebarBeforeContent'][] = 'efAddWikiSticky';
-
-function efAddWikiSticky( &$html ) {
-	global $wgOut, $wgExtensionsPath, $wgStyleVersion;
-
-	wfLoadExtensionMessages( 'WikiStickies' );
-
-	WikiStickies::addWikiStickyResources();
-	// Special:MyHome page-specific resources
-	$wgOut->addExtensionStyle("{$wgExtensionsPath}/wikia/WikiStickies/css/WikiStickiesMyHome.css?{$wgStyleVersion}");
-
-	// fetch the feeds and rotate them
-	$feedWithoutimages = WikiStickies::getWithoutimagesFeed( WikiStickies::INITIAL_FEED_LIMIT );
-	$feedNewpages = WikiStickies::getNewpagesFeed( WikiStickies::INITIAL_FEED_LIMIT );
-	$feedWantedpages = WikiStickies::getWantedpagesFeed( WikiStickies::INITIAL_FEED_LIMIT );
-
-	$feeds = array();
-	for( $i = 0; $i < max( count( $feedWithoutimages ), count( $feedNewpages ), count( $feedWantedpages ) ); $i++ ) {
-		if( isset( $feedWithoutimages[$i] ) ) {
-			$feeds[] = array(
-					'prefix' => wfMsg( 'wikistickies-withoutimages-st' ),
-					'title' => $feedWithoutimages[$i] );
-		}
-		if( isset( $feedNewpages[$i] ) ) {
-			$feeds[] = array(
-					'prefix' => wfMsg( 'wikistickies-newpages-st' ),
-					'title' => $feedNewpages[$i] );
-		}
-		if( isset( $feedWantedpages[$i] ) ) {
-			$feeds[] = array(
-					'prefix' => wfMsg( 'wikistickies-wantedpages-st' ),
-					'title' => $feedWantedpages[$i] );
-		}
-	}
-
-	$js = "WIKIA.WikiStickies.stickies = [\n";
-	foreach( $feeds as $f ) {
-		$js .= "'" . WikiStickies::renderWikiStickyContent( $f['title'], $f['prefix'] ) . "',\n";
-	}
-	$js .= "];\n";
-	$wgOut->addInlineScript( $js );
-	$wgOut->addScriptFile("{$wgExtensionsPath}/wikia/WikiStickies/js/WikiStickiesMyHome.js");
-
-	$html = WikiStickies::renderWikiSticky( $feeds[0]['title'], $feeds[0]['prefix'] );
-
-	return true;
-}
+$wgHooks['UploadComplete'][]  = 'efRemoveFromSpecialWithoutimages';
+$wgHooks['RecentChange_save'][] = 'efRemoveFromSpecialWantedpages';
 
 include( $dir . 'SpecialWithoutimages.php' );
