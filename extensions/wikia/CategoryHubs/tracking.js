b3bf4875cbdff9f2f4c87e3cca1eb961f3bce036
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-09 09:06:13 +0000

    CategoryHubs: click tracking.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16039 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CategoryHubs/tracking.js
+++ extensions/wikia/CategoryHubs/tracking.js
@@ -1,18 +1,20 @@
+var cathub_state = "unanswered";
+
 $(document).ready(function() {
 	$("#topContribsAllTime > ul.cathub-contributors-list-wide   a").click(function() { WET.byStr("cathub/alltime/top3"); });
 	$("#topContribsAllTime > ul.cathub-contributors-list-narrow a").click(function() { WET.byStr("cathub/alltime/top11"); });
 	$("#topContribsRecent  > ul.cathub-contributors-list-wide   a").click(function() { WET.byStr("cathub/last7/top3"); });
 	$("#topContribsRecent  > ul.cathub-contributors-list-narrow a").click(function() { WET.byStr("cathub/last7/top11"); });
 
-	$("#cathub-tablink-unanswered").click(function() { WET.byStr("cathub/list/tabUnanswered"); });
-	$("#cathub-tablink-answered  ").click(function() { WET.byStr("cathub/list/tabAnswered"); });
+	$("#cathub-tablink-unanswered").click(function() { cathub_state = "unanswered"; WET.byStr("cathub/list/tabUnanswered"); });
+	$("#cathub-tablink-answered  ").click(function() { cathub_state = "answered";   WET.byStr("cathub/list/tabAnswered"); });
 
-	$(".cathub-article-link > a").click(function() { WET.byStr("cathub/list/question"); });
-	$(".cathub-asked        > a").click(function() { WET.byStr("cathub/list/user"); });
+	$(".cathub-article-link > a").click(function() { WET.byStr("cathub/list/" + cathub_state + "/question"); });
+	$(".cathub-asked        > a").click(function() { WET.byStr("cathub/list/" + cathub_state + "/user"); });
 
-	$(".cathub-button-answer").click(        function() { WET.byStr("cathub/list/buttonAnswer"); });
-	$(".cathub-button-save  ").live("click", function() { WET.byStr("cathub/list/buttonSave"); });
-	$(".cathub-button-cancel").live("click", function() { WET.byStr("cathub/list/buttonCancel"); });
+	$(".cathub-button-answer").click(        function() { WET.byStr("cathub/list/" + cathub_state + "/btnAnswer"); });
+	$(".cathub-button-save  ").live("click", function() { WET.byStr("cathub/list/" + cathub_state + "/btnSave"); });
+	$(".cathub-button-cancel").live("click", function() { WET.byStr("cathub/list/" + cathub_state + "/btnCancel"); });
 
 	$("#mw-subcategories > a").click(function() { WET.byStr("cathub/subcategories"); });
 });
