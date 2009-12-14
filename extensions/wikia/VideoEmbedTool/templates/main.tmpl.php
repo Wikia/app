1cedb06fae89229faa3006f8fefde74940feed5e
tomek <tomek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-14 10:05:43 +0000

    #32014: Videogalleries with non-existent videos show broken gametrailers
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16236 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoEmbedTool/templates/main.tmpl.php
+++ extensions/wikia/VideoEmbedTool/templates/main.tmpl.php
@@ -5,17 +5,32 @@
 
 ?>
 
-<table cellspacing="0" style="width: 100%;" id="VideoEmbedInputTable">
-	<tr id="VideoEmbedTitle">
-		<td colspan="2">
-			<div id="VideoEmbedTitle"><h1><?= wfMsg( 'vet-title' ) ?></h1></div>
-		</td>
-	</tr>
 <?php
 	global $wgStylePath, $wgUser, $wgScriptPath, $wgExtensionsPath;
 ?>
-	<tr id="VideoEmbedAdd">
+
+<table cellspacing="0" style="width: 100%;" id="VideoEmbedInputTable">
+	<tr id="VideoEmbedTitle">
 		<td>
+			<div id="VideoEmbedTitle" ><h1><?= wfMsg( 'vet-title' ) ?></h1></div>
+		</td>
+		<td id="VideoEmbedSupported" width="200" rowspan="2">
+			<a href="http://www.youtube.com" class="image" title="YouTube" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/youtube.jpg" height="42" width="60" border="0"></a>
+			<a href="http://www.5min.com" class="image" title="5min" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/5min.gif" height="23" width="60" border="0"></a>
+			<a href="http://gamevideos.1up.com/" class="image" title="Gamevideos"  target="_blank"><img alt="Gamevideos" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/gamevideos.jpg" height="26" width="60" border="0"></a>
+			<a href="http://www.myvideo.de" class="image" title="Myvideo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/myvideo.jpg" height="17" width="60" border="0"></a>
+			<a href="http://en.sevenload.com" class="image" title="Sevenload" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/sevenload.jpg" height="11" width="60" border="0"></a>
+			<a href="http://www.vimeo.com" class="image" title="Vimeo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/vimeo.png" height="36" width="60" border="0"></a>
+			<a href="http://www.dailymotion.com/" class="image" title="dailymotion" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/dailymotion-logo-june08.png" width="50" border="0"></a>
+			<a href="http://www.gametrailers.com/" class="image" title="GameTrailers" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/GameTrailers_logo.png" width="60" border="0"></a>
+			<a href="http://blip.tv" class="image" title="blip.tv" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/blip-tv1.png" width="60" border="0"></a>
+			<a href="http://www.viddler.com" class="image" title="Viddler" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/viddler_logo.png" width="60" border="0"></a>
+			<p>	<a href="http://help.wikia.com/wiki/Help:Video_Embed_Tool" target="_blank"><?= wfMsg( 'vet-see-all' ) ?></a></p>	
+		<td>
+	</tr>
+
+	<tr id="VideoEmbedAdd">
+		<td  colspan="2">
 			<img src="<?= $wgStylePath; ?>/common/images/ajax.gif" id="VideoEmbedProgress2" style="visibility: hidden;"/>
 			<?php
 			if( !$wgUser->isAllowed( 'upload' ) ) {
@@ -40,23 +55,8 @@
 ?>
 <div id="VideoEmbedMainMesg"><?= wfMsg( 'vet-main-info' ) ?></div>
 </td>
-<td id="VideoEmbedSupported" width="200">
-<a href="http://www.youtube.com" class="image" title="YouTube" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/youtube.jpg" height="42" width="60" border="0"></a>
-<a href="http://www.5min.com" class="image" title="5min" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/5min.gif" height="23" width="60" border="0"></a>
-<a href="http://gamevideos.1up.com/" class="image" title="Gamevideos"  target="_blank"><img alt="Gamevideos" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/gamevideos.jpg" height="26" width="60" border="0"></a>
-<a href="http://www.myvideo.de" class="image" title="Myvideo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/myvideo.jpg" height="17" width="60" border="0"></a>
-<a href="http://en.sevenload.com" class="image" title="Sevenload" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/sevenload.jpg" height="11" width="60" border="0"></a>
-<a href="http://www.vimeo.com" class="image" title="Vimeo" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/vimeo.png" height="36" width="60" border="0"></a>
-<a href="http://www.dailymotion.com/" class="image" title="dailymotion" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/dailymotion-logo-june08.png" width="60" border="0"></a>
-<a href="http://www.gametrailers.com/" class="image" title="GameTrailers" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/GameTrailers_logo.png" width="60" border="0"></a>
-<a href="http://blip.tv" class="image" title="blip.tv" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/blip-tv1.png" width="60" border="0"></a>
-<a href="http://www.viddler.com" class="image" title="Viddler" target="_blank"><img alt="" src="<?= $wgExtensionsPath; ?>/wikia/VideoEmbedTool/images/viddler_logo.png" width="60" border="0"></a>
-
-	<p>	<a href="http://help.wikia.com/wiki/Help:Video_Embed_Tool" target="_blank"><?= wfMsg( 'vet-see-all' ) ?></a></p>	
-<td>
 
 </table>
-<br/>
 <div id="VET_results_0">
 	<?= $result ?>
 </div>
