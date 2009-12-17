8778bd7f5fe0549642a7dc7415717a9d795ded68
moli <moli@wikia-inc.com>
2009-12-17 12:05:35 +0000

    other params
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16390 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaStats/WikiaWebStats.php
+++ extensions/wikia/WikiaStats/WikiaWebStats.php
@@ -8,7 +8,7 @@ $wgHooks['SkinAfterBottomScripts'][] = 'wfWikiaWebStatsScript';
 function wfWikiaWebStatsScript($this, $bottomScriptText) {
 	global $wgUser, $wgArticle, $wgTitle, $wgCityId, $wgDBname, $wgDBcluster, $wgDotDisplay, $wgAdServerTest, $wgReadOnly;
 	if ( !empty($wgCityId) && !empty($wgDotDisplay) && empty($wgReadOnly) ) {
-		$url = 'http://wikia-ads.wikia.com/onedot_old.php?c='.$wgCityId.'&amp;x='.$wgDBname.'&amp;y='.$wgDBcluster.'&amp;u='.$wgUser->getID().'&amp;a='.(is_object($wgArticle) ? $wgArticle->getID() : null).'&amp;n='.$wgTitle->getNamespace().(!empty($wgAdServerTest) ? '&amp;db_test=1' : '');
+		$url = 'http://wikia-ads.wikia.com/onedot.php?c='.$wgCityId.'&amp;x='.$wgDBname.'&amp;y='.$wgDBcluster.'&amp;u='.$wgUser->getID().'&amp;a='.(is_object($wgArticle) ? $wgArticle->getID() : null).'&amp;n='.$wgTitle->getNamespace().(!empty($wgAdServerTest) ? '&amp;db_test=1' : '');
 		$bottomScriptText .= '<script type="text/javascript">/*<![CDATA[*/document.write("<img src=\"'.$url.'"+((typeof document.referrer != "undefined") ? "&amp;r="+escape(document.referrer) : "")+"&amp;cb="+(new Date).valueOf()+"\" width=\"1\" height=\"1\" border=\"0\" alt=\"\" />");/*]]>*/</script><noscript><img src="'.$url.'" width="1" height="1" border="0" alt="" /></noscript>';
 		$bottomScriptText .= "\n";
 	}
