3fe5b8c551434f244f5a1f2be85446f0ff912dc8
meitar <meitar@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-18 20:24:39 +0000

    Format code according to code conventions. No functional change.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16455 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MyHome/templates/feed.simple.tmpl.php
+++ extensions/wikia/MyHome/templates/feed.simple.tmpl.php
@@ -1,36 +1,33 @@
 <?php
-	if (isset($emptyMessage)) {
+if ( isset( $emptyMessage ) ) {
 ?>
-	<h3 class="myhome-empty-message shortlist"><?= $emptyMessage ?></h3>
+	<h3 class="myhome-empty-message shortlist"><?php print $emptyMessage ?></h3>
 <?php
-	}
-	else {
+} else {
 ?>
-
-	<ul class="activityfeed reset shortlist" id="<?= $tagid ?>">
+	<ul class="activityfeed reset shortlist" id="<?php print $tagid ?>">
 <?php
-		foreach($data as $row) {
+	foreach( $data as $row ) {
 ?>
-		<li class="activity-type-<?= FeedRenderer::getIconType($row) ?> activity-ns-<?= $row['ns'] ?>">
+		<li class="activity-type-<?php print FeedRenderer::getIconType($row) ?> activity-ns-<?php print $row['ns'] ?>">
 			<?php print FeedRenderer::getSprite( $row, $assets['blank'] ) ?>
 <?php
-			if (isset($row['url'])) {
+			if( isset( $row['url'] ) ) {
 ?>
-			<strong><a href="<?= htmlspecialchars($row['url']) ?>"><?= htmlspecialchars($row['title'])  ?></a></strong>
+			<strong><a href="<?php print htmlspecialchars($row['url']) ?>"><?php print htmlspecialchars($row['title'])  ?></a></strong>
+<?php
+			} else {
+?>
+			<span class="title"><?php print htmlspecialchars($row['title']) ?></span>
 <?php
 			}
-			else {
 ?>
-			<span class="title"><?= htmlspecialchars($row['title'])  ?></span>
-<?php
-			}
-?>
-			<cite><?= ActivityFeedRenderer::formatTimestamp($row['timestamp']); ?><?= FeedRenderer::getDiffLink($row); ?></cite>
+			<cite><?php print ActivityFeedRenderer::formatTimestamp($row['timestamp']); ?><?php print FeedRenderer::getDiffLink($row); ?></cite>
 		</li>
 <?php
-		}
+	}
 ?>
 	</ul>
 <?php
-	}
+}
 ?>
--- extensions/wikia/MyHome/templates/feed.widget.tmpl.php
+++ extensions/wikia/MyHome/templates/feed.widget.tmpl.php
@@ -1,36 +1,33 @@
 <?php
-	if (isset($emptyMessage)) {
+if( isset( $emptyMessage ) ) {
 ?>
-	<h3 class="myhome-empty-message widgetfeed"><?= $emptyMessage ?></h3>
+	<h3 class="myhome-empty-message widgetfeed"><?php print $emptyMessage ?></h3>
 <?php
-	}
-	else {
+} else {
 ?>
-
-	<ul class="activityfeed widgetfeed" id="<?= $tagid ?>-recently-edited">
+	<ul class="activityfeed widgetfeed" id="<?php print $tagid ?>-recently-edited">
 <?php
-		foreach($data as $row) {
+	foreach( $data as $row) {
 ?>
-		<li class="activity-type-<?= FeedRenderer::getIconType($row) ?> activity-ns-<?= $row['ns'] ?>">
+		<li class="activity-type-<?php print FeedRenderer::getIconType( $row ) ?> activity-ns-<?php print $row['ns'] ?>">
 			<?php print FeedRenderer::getSprite( $row, $assets['blank'] ) ?>
 <?php
-			if (isset($row['url'])) {
+		if (isset($row['url'])) {
 ?>
-			<strong><a href="<?= htmlspecialchars($row['url']) ?>"><?= htmlspecialchars($row['title'])  ?></a></strong>
+			<strong><a href="<?php print htmlspecialchars( $row['url'] ) ?>"><?php print htmlspecialchars( $row['title'] )  ?></a></strong>
 <?php
-			}
-			else {
+		} else {
 ?>
-			<span class="title"><?= htmlspecialchars($row['title'])  ?></span>
-<?php
-			}
-?>
-			<cite><?= ActivityFeedRenderer::formatTimestamp($row['timestamp']); ?> <?= wfMsg("myhome-feed-by", FeedRenderer::getUserPageLink($row)) ?></cite>
-		</li>
+			<span class="title"><?php print htmlspecialchars( $row['title'] )  ?></span>
 <?php
 		}
 ?>
-	</ul>
+			<cite><?php print ActivityFeedRenderer::formatTimestamp( $row['timestamp'] ); ?> <?php print wfMsg( "myhome-feed-by", FeedRenderer::getUserPageLink( $row ) ) ?></cite>
+		</li>
 <?php
 	}
+?>
+	</ul>
+<?php
+}
 ?>
