0e71d65d069e8c93b72011e17a43fde13425f548
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2009-12-19 22:47:28 +0000

    typo
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16462 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
+++ extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
@@ -3,5 +3,5 @@
 $messages = array();
 $messages["en"] = array(
 	"comboajaxlogin-createlog" => "Log in or create an account",
-	"comboajaxlogin-actionmsg" => "To performer this action you first need to log in or create an account"
+	"comboajaxlogin-actionmsg" => "To perform this action you first need to log in or create an account"
 );
