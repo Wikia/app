9740b41f10b430f811a96af4a2e79bda7c9151f3
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-01-08 11:34:45 +0000

    Newsite: english special page name
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16743 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/newsite/NeueWebsite.php
+++ extensions/wikia/newsite/NeueWebsite.php
@@ -13,5 +13,6 @@ include('neue_website.php');
 $dir = dirname(__FILE__);
 
 $wgAutoloadClasses['NeueWebsite'] = $dir . '/NeueWebsite.body.php';
-$wgSpecialPages['NeueWebsite'] = 'NeueWebsite';
+$wgSpecialPages['NewWebsite'] = 'NeueWebsite';
 $wgExtensionMessagesFiles['Newsite'] = $dir . '/NeueWebsite.i18n.php';
+$wgExtensionAliasesFiles['Newsite'] = $dir . '/NeueWebsite.alias.php';
