fca3b4d9920d08fe896602e2e3ea96dbf6019b65
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-01-11 08:27:10 +0000

    The DevBoxPanel now has a GUI for pulling databases and has been generally prettied up a bit.  It is still lacking a GUI for setting which local dbs can override and it still has a problem allowing database overrides because page-writing triggers the BatchTask::createTask function which tries to write to wikicities which isn't allowed since it's in readOnly mode.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16787 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Development/SpecialDevBoxPanel/Special_DevBoxPanel.i18n.php
+++ extensions/wikia/Development/SpecialDevBoxPanel/Special_DevBoxPanel.i18n.php
@@ -7,28 +7,33 @@ $messages["en"] = Array(
 	                                 "To enable this panel please add this line to LocalSettings.php:<br/><strong><code>\$wgDevelEnvironment = true;</code></strong>",
 	"devbox-intro"                => "This panel is designed to help manage your dev-box (ie: on Wikia servers, <strong>not intended for local installations</strong>). Since this SpecialPage is only designed for dev-boxes, it is recommended NOT to use this on other types of servers.",
 	
-	"devbox-vital-settings"       => "Vital Settings",
+	"devbox-heading-vital"        => "Vital Settings",
 	"devbox-setting-name"         => "Setting Name",
 	"devbox-setting-value"        => "Current Value",
 
+	"devbox-heading-pull-dbs"     => "Pull Databases From Prod",
 	"devbox-dbs-on-devbox"        => "Databases On Dev Box",
 	"devbox-dbs-in-production"    => "Available From Production Slaves",
 
-	"devbox-section-all"          => "All databases alphabetically (including those from Summary)",
+	"devbox-pull-by-domain"       => "Pull any wiki's database by wiki domain: ",
+
+	//"devbox-section-all"          => "All databases alphabetically (including those from Summary)", // TODO: REMOVE IF WE DECIDE TO CHUCK THIS SECTION
 	"devbox-section-existing"     => "Existing Databases",
-	"devbox-section-summary"      => "Summary",
+	"devbox-section-recommended"  => "Commonly Used",
+	"devbox-no-dbs-in-list"       => "(None found)",
 
 	"devbox-recommended"          => "(recommended)",
 	"devbox-get-db"               => "Get it",
-	"devbox-reload-db"            => "Reload it",
+	"devbox-reload-db"            => "Re-pull it",
 
-	"devbox-change-wiki-heading"  => "Change Active Wiki",
+	"devbox-heading-change-wiki"  => "Change Active Wiki",
 	"devbox-change-wiki-intro"    => "Allows you to set the active wiki instead of modifying hosts files.  To use the hosts-file method, leave this field blank.",
 	"devbox-change-wiki-label"    => "Wiki to use (eg: \"lyrics.wikia.com\"): ",
 	"devbox-change-wiki-submit"   => "Update",
 	"devbox-change-wiki-success"  => "This MediaWiki will now run as <strong><code>\$1</code></strong><br/><em>Note: this change won't be apparent until the next page-load.</em>",
 	"devbox-default-wiki"         => "whatever wiki is in the url",
 	"devbox-change-wiki-fileerror"=> "There was an error writing to \"<em>\$1</em>\".  Make sure you have appropriate write-permissions.",
+
 
 	"devbox-footer"               => "<br/><br/><small>Want to change something?<br/>The code for this extension is located in <code>\$1</code></small>",
 );
