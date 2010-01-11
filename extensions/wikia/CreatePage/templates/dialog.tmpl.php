c3cace63c934efe815c10dd8f556c7944278fb6d
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-01-11 16:49:13 +0000

    CreatePage: template without styling
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16828 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreatePage/templates/dialog.tmpl.php
+++ extensions/wikia/CreatePage/templates/dialog.tmpl.php
@@ -1,5 +1,25 @@
 <div id="CreatePageDialog" title="<?= wfMsg( 'createpage-dialog-title' ) ?>" >
-	<div>
-		I'm CreatePage Dialog !
+	<div class="createpage_mesg_norm">
+		<?= wfMsg( 'createpage-dialog-message1' ) ?>
 	</div>
+	<div class="createpage_msg_bold">
+		<?= wfMsg( 'createpage-dialog-message2' ) ?>
+	</div>
+	<input id="wpCreatepageDialogTitle" name="wpCreatepageDialogTitle" type="text" />
+	<div>
+	</div>
+	        <div class="createpage_title_desc">
+                <?= wfMsg( 'createpage-dialog-choose' ) ?>
+	</div>
+	<div>
+		<input type="radio" name="wpCreatePageChoices" id="CreatePageDialogFormat" value="format" selected="selected" />
+		<label for="CreatePageDialogFormat"><?= wfMsg( 'createpage-dialog-format' ) ?>
+		<img src="/extensions/wikia/CreatePage/images/plc.png" /></label>
+		<input type="radio" name="wpCreatePageChoices" id="CreatePageDialogBlank" value="format" />
+		<label for="CreatePageDialogBlank"><?= wfMsg( 'createpage-dialog-blank' ) ?>
+		<img src="/extensions/wikia/CreatePage/images/plc.png" /></label>
+	</div>
+	<div class="modalToolbar neutral">
+                <a id="wpLoginattempt" class="wikia_button" href="#"><span><?= wfMsg("createpage-dialog-title") ?></span></a>
+        </div>
 </div>
