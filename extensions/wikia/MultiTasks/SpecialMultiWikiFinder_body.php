a6a59486077c7330173dcf12e685fb21e3916c1e
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-01-12 14:21:58 +0000

    multiwiki title finder
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@16913 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/MultiTasks/SpecialMultiWikiFinder_body.php
@@ -0,0 +1,229 @@
+<?php
+
+class MultiwikifinderSpecialPage extends SpecialPage {
+    private $mfp = null;
+    var $mName = 'Multiwikifinder';
+
+	function __construct() {
+		parent::__construct( $this->mName );
+	}
+
+	function execute($page = null, $limit = "", $offset = "", $show = true) {
+		global $wgRequest;
+		wfLoadExtensionMessages("Multiwikifinder");
+
+		$page = $wgRequest->getVal('target', $page);
+		if (empty($limit) && empty($offset)) { 
+            list( $limit, $offset ) = wfCheckLimits();
+        }
+        $this->mfp = new MultiwikifinderPage($this->mName, $page, $limit, $offset);
+        
+		if (!empty($show)) {
+            $this->setHeaders();
+        } else {
+            // return data as array - not like <LI> list
+            $this->mfp->setShow(false);
+        }
+        $this->mfp->showForm();
+    }
+    
+    function getResult() { return $this->mpp->getResult(); }
+}
+
+class MultiwikifinderPage {
+	private $data = array();
+	private $mShow = true;
+	const ORDER_ROWS = 200;
+	var $mPage = null;
+	var $mPageTitle = "";
+	var $mPageNS = 0;
+	var $mTitle = "";
+
+	function __construct($name, $page, $limit = 50, $offset = 0) { 
+
+		$this->mName = $name;
+
+		if ( !empty($page) ) {
+			$this->mPage = Title::newFromText($page);
+			if ( $this->mPage instanceof Title ) {
+				$this->mPageTitle = $this->mPage->getDBkey();
+				$this->mPageNS = $this->mPage->getNamespace();
+			} else {
+				$this->mPageTitle = $page;
+				$this->mPageNS = 0;
+			}
+		}
+		$this->offset = $offset;
+		$this->limit =  $limit;
+		$this->mTitle = Title::makeTitle( NS_SPECIAL, $this->mName );
+	}
+
+	function linkParameters() { 
+		$res = array();
+		if ( isset($this->mPageTitle) && isset($this->mPageNS) ) {
+			$res = array('target' => $this->mPage->getFullText());	
+		}
+		return $res; 
+	}
+	function setShow( $bool ) { $this->mShow = $bool; }
+
+	function getPageHeader() { 
+        wfProfileIn( __METHOD__ );
+        
+        if (empty($this->mShow)) {
+        	wfProfileOut( __METHOD__ );
+        	return "";
+		}
+		
+		$action = $this->mTitle->escapeLocalURL("");
+
+        $oTmpl = new EasyTemplate( dirname( __FILE__ ) . "/templates/" );
+        $oTmpl->set_vars( array(
+            "action"	=> $action,
+            "mPage"		=> ( is_object($this->mPage) ) ? $this->mPage->getFullText() : "",
+        ));
+        
+        wfProfileOut( __METHOD__ );
+        return $oTmpl->execute("main-finder-form");
+	}
+
+	public function getResult() { return $this->data; }
+
+	function showForm() {
+		global $wgOut;
+
+        wfProfileIn( __METHOD__ );
+		$wgOut->addHTML( $this->getPageHeader() );
+		
+		if ( isset($this->mPageTitle) && isset($this->mPageNS) ) {
+			$this->showResults();
+		}
+        wfProfileOut( __METHOD__ );
+	}
+	
+	function showResults( ) {
+		global $wgUser, $wgOut, $wgLang, $wgContLang, $wgMemc, $wgExternalDatawareDB;
+
+        wfProfileIn( __METHOD__ );
+		$wgOut->setSyndicated( false );
+
+		$num = 0;
+		$key = wfMemcKey( "MTF", $this->mPageTitle, $this->mPageNS, $this->limit, $this->offset );
+		$data = $wgMemc->get( $key );
+		
+		if ( empty($data) || 
+			( isset($data) && ( 0 == intval($data['numrec'] ) ) ) 
+		) {
+			$dbr = wfGetDB( DB_SLAVE, 'blobs', $wgExternalDatawareDB ); 
+			$oRes = $dbr->select(
+				'pages',
+				array( 'page_wikia_id, page_id, page_title, page_namespace, page_latest' ),
+				array( 
+					'page_title_lower'	=> strtolower($this->mPageTitle),
+					'page_namespace' 	=> $this->mPageNS,
+					'page_status'		=> 0
+				),
+				__METHOD__ 
+			);
+
+			$num = $dbr->numRows($oRes);
+
+			$data = array( 'numrec' => $num, 'rows' => array() );
+			while ( $oRow = $dbr->fetchObject( $oRes ) ) {
+				$oGTitle = GlobalTitle::newFromText( $this->mPageTitle, $this->mPageNS, $oRow->page_wikia_id );
+				$data['rows'][ $oRow->page_wikia_id ] = array(
+					$oRow->page_id,
+					$oGTitle->getFullURL()
+				);
+				#- get last edit TS or not
+				if ( $num <= self::ORDER_ROWS ) {
+					$data['order'][ $oRow->page_wikia_id ] = $oGTitle->getLatestRevTS();
+				} else {
+					$data['order'][ $oRow->page_wikia_id ] = $oRow->page_latest;
+				}
+			}
+			
+			if ( $data['numrec'] > 0 ) {
+				arsort($data['order']);
+				$data = array_slice($data, $this->offset, $this->limit);
+			}
+			
+			$dbr->freeResult( $oRes );
+			$wgMemc->set( $key, $data, 60 * 60 );
+		} 
+
+		$num = $data['numrec'] ;
+		$wgOut->addHTML( XML::openElement( 'div', array('class' => 'mw-spcontent') ) );
+		
+		# Top header and navigation
+		if( $this->mShow ) {
+			$wgOut->addHTML( '<p>' . wfMsgExt('multiwikirecords', array(), $num) . '</p>' );
+			if( $num > 0 ) {
+				$wgOut->addHTML( '<p>' . wfShowingResults( $this->offset, $num ) . '</p>' );
+				# Disable the "next" link when we reach the end
+				$paging = wfViewPrevNext( 
+					$this->offset, 
+					$this->limit, 
+					$wgContLang->specialPage( $this->mName ), 
+					wfArrayToCGI( $this->linkParameters() ), 
+					( $num < $this->limit ) 
+				);
+				$wgOut->addHTML( '<p>' . $paging . '</p>' );
+			} else {
+				$wgOut->addHTML( XML::closeElement( 'div' ) );
+				return;
+			}
+		}
+
+		# The actual results; specialist subclasses will want to handle this
+		# with more than a straight list, so we hand them the info, plus
+		# an OutputPage, and let them get on with it
+		$this->outputResults( 
+			$wgOut,
+			$wgUser->getSkin(),
+			$data
+		);
+
+		# Repeat the paging links at the bottom
+		if( $this->mShow ) {
+			$wgOut->addHTML( '<p>' . $paging . '</p>' );
+		}
+
+		$wgOut->addHTML( XML::closeElement( 'div' ) );
+
+        wfProfileOut( __METHOD__ );
+		return $num;
+	}
+	
+	public function outputResults( $out, $skin, $data ) {
+		global $wgContLang;
+
+		if( isset($data) && $data['numrec'] > 0 ) {
+			$html = array();
+			if ( $this->mShow ) {
+				$html[] = XML::openElement( 'ol', array('start' => $this->offset + 1, 'class' => 'special' ) );
+			}
+
+			foreach ( $data['order'] as $city_id => $ordered ) {
+				$res = "";
+				list ($page_id, $page_url) = $data['rows'][$city_id];
+				if (empty($this->mShow)) {
+					$this->data[$city_id] = array('city_id' => $city_id, 'page_id' => $page_id, 'url' => $page_url);
+				} else {
+					$res = Xml::openElement( 'a', array('href' => $page_url) ) . $page_url . Xml::closeElement( 'a' );
+				}
+				
+				$html[] = $this->mShow ? Xml::openElement( 'li' ) . $res . Xml::closeElement( 'li' ) : "";
+			}
+
+			if( $this->mShow ) {
+				$html[] = XML::closeElement( 'ol' );
+			}
+
+			$html = $this->mShow ? implode( '', $html ) : $wgContLang->listToText( $html );
+
+			$out->addHTML( $html );
+		}
+	}
+	
+}
