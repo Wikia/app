32402812f3bf5ae2469094becc8dd855c76ef104
tor <tor@wikia-inc.com>
2010-01-13 16:14:51 +0000

    FastCat: moved from Website to stand-alone, refactored
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17008 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/Websitewiki/SpecialFastCat.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * imported from Websitewiki
- * modified by TOR
- */
-
-$wgAutoloadClasses['FastCat'] = dirname( __FILE__ ). '/SpecialFastCat.body.php';
-$wgSpecialPages['FastCat'] = 'FastCat';
