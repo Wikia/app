bc17d08f987b654eb5c26a7879afa891112bc7e9
nick <nick@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-01-19 23:57:52 +0000

    Answers is ok too
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17350 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CategorySelect/CategorySelect.js
+++ extensions/wikia/CategorySelect/CategorySelect.js
@@ -43,6 +43,8 @@ function extractSortkey(text) {
 		result['name'] = text.slice(0, pipePos);
 		result['sort'] = text.slice(pipePos + 1);
 	}
+	console.log("result in extractSortKey");
+	console.dir(result);
 	return result;
 }
 
@@ -190,6 +192,8 @@ function inputBlur() {
 }
 
 function addCategory(category, params, index) {
+	console.log("addCategory called");
+	console.dir(addCaegory.arguments);
 	if (params == undefined) {
 		params = {'namespace': csDefaultNamespace, 'outerTag': '', 'sortkey': ''};
 	}
