d23d078034f051fbfc05ad6d4d5eb2cbf4de6321
eloy <eloy@wikia-inc.com>
2010-01-27 12:39:10 +0000

    reserve space for project :>
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17670 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiFactory/Tags/WikiFactoryTags.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @package MediaWiki
+ * @ingroup WikiFactory
+ *
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia.com> for Wikia Inc.
+ */
+
+# use tables
+#
+#CREATE TABLE `city_tag` (
+#  `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+#  `name` varchar(255) DEFAULT NULL,
+#  PRIMARY KEY (`id`),
+#  UNIQUE KEY `city_tag_name_uniq` (`name`)
+#) ENGINE=InnoDB DEFAULT;
+#
+#
+#CREATE TABLE `city_tag_map` (
+#  `city_id` int(9) NOT NULL,
+#  `tag_id` int(8) unsigned NOT NULL,
+#  PRIMARY KEY (`city_id`,`tag_id`),
+#  KEY `tag_id` (`tag_id`),
+#  CONSTRAINT `city_tag_map_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city_list` (`city_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+#  CONSTRAINT `city_tag_map_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `city_tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+#) ENGINE=InnoDB
