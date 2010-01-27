f20d6aa232d601c7723141111bd135baf9ed8a8d
tor <tor@wikia-inc.com>
2010-01-27 17:00:03 +0000

    added wfWaitForSlaves
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17708 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/migrate_users.php
+++ maintenance/wikia/migrate_users.php
@@ -278,6 +278,7 @@ function alterTable( $table, $column, $from_val, $to_val, $where_column = false,
 	}
 
 	if( !$options['dryrun'] ) {
+		wfWaitForSlaves( 4 );
 		$db->query("UPDATE low_priority $table SET $column=$to_val where $where_column=$from_val");
 	}
 }
