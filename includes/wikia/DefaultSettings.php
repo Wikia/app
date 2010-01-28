1de979ae57a1c4424bbd049f6f5b7edf1440dddd
christian <christian@wikia-inc.com>
2010-01-28 02:39:21 +0000

    cross-skin font sizing for signup page, remove js for globalnav, styleversion++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17736 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -17,7 +17,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '17717';
+$wgStyleVersion         = '17718';
 $wgMergeStyleVersionJS  = '16743';
 $wgMergeStyleVersionCSS = '16743';
 
