de49081f02dcf23fa5be02f1a8e68183da7200b6
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-01-28 20:32:25 +0000

    Made Interstitials use all of the associated WikiFactory settings now.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17792 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Interstitials/Interstitials.js
+++ extensions/wikia/Interstitials/Interstitials.js
@@ -1,23 +1,20 @@
 if((typeof wgAdsInterstitialsEnabled != 'undefined') && wgAdsInterstitialsEnabled){
 	const COOKIE_NAME = 'IntPgCounter';
 	var count = $.cookies.get(COOKIE_NAME);
-	count = ((!count)?1:count);
+	count = parseInt((!count)?1:count);
 
-	var nextCount = parseInt(count) + 1;
-
-	// TODO: Use these globals.
-	//wgAdsInterstitialsPagesBeforeFirstAd
-	//wgAdsInterstitialsPagesBetweenAds
-	//wgAdsInterstitialsDurationInSeconds
-	//wgAdsInterstitialsCampaignCode
-
-	$.cookies.set(COOKIE_NAME, nextCount, {
+	$.cookies.set(COOKIE_NAME, (count + 1), {
 		path: wgCookiePath,
 		domain: wgCookieDomain
 	});
 
-	if((count % 2) == 0){
-		$('#interstitial_foreground').css('display', 'block');
-		$('#interstitial_background').css('display', 'block');
+	if((wgAdsInterstitialsPagesBeforeFirstAd == count) || ((count > wgAdsInterstitialsPagesBeforeFirstAd) && ((count % wgAdsInterstitialsPagesBetweenAds) == 0))){
+		var adMillis = 1000 * wgAdsInterstitialsDurationInSeconds;
+		$('#interstitial_foreground').show();
+		$('#interstitial_background').show();
+		setTimeout(function(){
+			$('#interstitial_foreground').hide();
+			$('#interstitial_background').hide();
+		}, adMillis);
 	}
 }
