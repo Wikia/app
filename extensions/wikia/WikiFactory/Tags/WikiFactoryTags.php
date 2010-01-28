f5648ce20a94111c3ea7f552a6cd643986da9e23
eloy <eloy@wikia-inc.com>
2010-01-28 12:38:22 +0000

    getTags method
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17762 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/Tags/WikiFactoryTags.php
+++ extensions/wikia/WikiFactory/Tags/WikiFactoryTags.php
@@ -23,6 +23,86 @@
 #  `tag_id` int(8) unsigned NOT NULL,
 #  PRIMARY KEY (`city_id`,`tag_id`),
 #  KEY `tag_id` (`tag_id`),
-#  CONSTRAINT `city_tag_map_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city_list` (`city_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-#  CONSTRAINT `city_tag_map_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `city_tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+#  CONSTRAINT FOREIGN KEY (`city_id`) REFERENCES `city_list` (`city_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+#  CONSTRAINT FOREIGN KEY (`tag_id`) REFERENCES `city_tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 #) ENGINE=InnoDB
+#
+# http://www.pui.ch/phred/archives/2005/04/tags-database-schemas.html
+
+class WikiFactoryTags {
+
+	private $mCityId;
+
+	/**
+	 * public constructor
+	 *
+	 * @access public
+	 * @param integer $city_id	city_id value from city_list table
+	 */
+	public function __construct( $city_id ) {
+		$this->mCityId = $city_id;
+	}
+
+	/**
+	 *
+	 * @access private
+	 *
+	 * @return string  key used for caching
+	 */
+	private function cacheKey() {
+		return sprintf( "wikifactory:tags:v1:%d", $this->mCityId );
+	}
+
+
+	/**
+	 * getTags -- get all tags defined from database
+	 *
+	 * @access public
+	 */
+	public function getTags( $skipcache = false ) {
+
+		global $wgMemc;
+
+		wfProfileIn( __METHOD__ );
+
+		$result = array();
+
+		/**
+		 * try cache first
+		 */
+		if( !$skipcache ) {
+			/**
+			 * check if cache has any values stored
+			 */
+			$result = $wgMemc->get( $this->cacheKey() );
+			if( !is_array( $result ) ) {
+				$usedb = true;
+			}
+		}
+		else {
+			$usedb = true;
+		}
+
+		if( $usedb ) {
+			$dbr = WikiFactory::db( DB_SLAVE );
+			$sth = $dbr->select(
+				array( "city_tag", "city_tag_map" ),
+				array( "tag_id", "name" ),
+				array( "tag_id = id", "city_id" => $this->mCityId ),
+				__METHOD__
+			);
+			while( $row = $dbr->fetchObject( $sth ) ) {
+				$result[ $row->tag_id ] = $row->name;
+			}
+
+			/**
+			 * set in cache for future use
+			 */
+			$wgMemc->set( $this->cacheKey(), $result, 60*60*24 );
+		}
+
+		wfProfileOut( __METHOD__ );
+
+		return $result;
+	}
+}
