bd944a4e7053cd04fdf42912403c15e2980f379c
christian <christian@wikia-inc.com>
2010-01-28 23:06:54 +0000

    styleversion++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17802 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -17,7 +17,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '17720';
+$wgStyleVersion         = '17721';
 $wgMergeStyleVersionJS  = '16743';
 $wgMergeStyleVersionCSS = '16743';
 
