8e472ef4181a01779db4055de2882bdb2923d6c7
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-01-29 16:27:32 +0000

    fix parse error
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17845 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/Websitewiki/domainhook.i18n.php
+++ extensions/3rdparty/Websitewiki/domainhook.i18n.php
@@ -5,7 +5,7 @@ $messages = array();
 $messages['en'] = array(
 	'websiteinfo-summary' => 'Website info for $1',
 	'websiteinfo-erotik-alt' => 'Erotica',
-)
+);
 
 $messages['de'] = array(
 	'websiteinfo-summary' => 'Website-Infos f&uuml;r $1',
