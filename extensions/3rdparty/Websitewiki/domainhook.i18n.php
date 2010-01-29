0a2f6fa792454eeb833c7eb70e31ddbcd4ad7ff3
tor <tor@wikia-inc.com>
2010-01-29 13:55:15 +0000

    Websitewiki: fixed infobox vanishing on edit issue, started i18n
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17829 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/3rdparty/Websitewiki/domainhook.i18n.php
@@ -0,0 +1,13 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'websiteinfo-summary' => 'Website info for $1',
+	'websiteinfo-erotik-alt' => 'Erotica',
+)
+
+$messages['de'] = array(
+	'websiteinfo-summary' => 'Website-Infos f&uuml;r $1',
+	'websiteinfo-erotik-alt' => 'Erotik',
+);
