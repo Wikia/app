ef4f5760018f5352134050011bbb7e6da9ee0a66
sean <sean@wikia-inc.com>
2010-01-31 05:53:32 +0000

    Made Interstitial background the height of the window, not just the viewport (so it looks alright even if the user scrolls).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17873 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Interstitials/Interstitials.js
+++ extensions/wikia/Interstitials/Interstitials.js
@@ -11,6 +11,7 @@ if((typeof wgAdsInterstitialsEnabled != 'undefined') && wgAdsInterstitialsEnable
 	if((wgAdsInterstitialsPagesBeforeFirstAd == count) || ((count > wgAdsInterstitialsPagesBeforeFirstAd) && ((count % wgAdsInterstitialsPagesBetweenAds) == 0))){
 		$('#interstitial_fg').css('border-color', $('#wikia_page').css('border-left-color'));
 		$('#interstitial_fg').css('background-color', $('#wikia_page').css('background-color'));
+		$('#interstitial_bg').css('height', $('html').css('height')); // make bg 100% of page, not just viewport.
 
 		var adMillis = 1000 * wgAdsInterstitialsDurationInSeconds;
 		$('#interstitial_fg').show();
