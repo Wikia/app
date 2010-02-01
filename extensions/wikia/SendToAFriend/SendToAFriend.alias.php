62ad6b83e3d560d492fa801cf2ee72fcc934ce1b
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-01 21:29:20 +0000

    * add aliases file for special page localisation * add description messages in $wgExtensionCredits to allow extension description localisation * remove "Click here" reference in 'stf_after_reg' * add plural support for 'stf_throttle' and 'stf_ctx_invite' * remove hard coded leading space in 'stf_we_dont_keep' and use 'word-separator'
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17931 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SendToAFriend/SendToAFriend.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Aliases for special pages of SendToAFriend extension.
+ */
+
+$aliases = array();
+
+/** English
+ */
+$aliases['en'] = array(
+	'InviteSpecialPage' => array( 'InviteSpecialPage' ),
+);
