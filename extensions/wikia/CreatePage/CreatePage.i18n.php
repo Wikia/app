7611ac1737a32b6869e40b54bf2e189626d10824
siebrand <siebrand@wikia-inc.com>
2010-02-01 21:08:51 +0000

    * split sentence in two sentences and end with proper interpunction. * add line breaks between sentences for easier localisation * normalise file formatting (exactly a single newline between sections)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17926 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreatePage/CreatePage.i18n.php
+++ extensions/wikia/CreatePage/CreatePage.i18n.php
@@ -4,6 +4,7 @@
  */
 
 $messages = array();
+
 $messages['en'] = array(
 	'createpage-sp-title' => 'Create a new article',
 	'createpage_title' => 'Create a new article',
@@ -12,7 +13,8 @@ $messages['en'] = array(
 	'createpage_empty_title_error' => 'Title required',
 	'createpage_empty_article_body_error' => 'Article text required',
 	'createpage_invalid_title_error' => 'Invalid title',
-	'createpage_article_already_exists' => 'Article with that name already exists, please select different name',
+	'createpage_article_already_exists' => 'Article with that name already exists.
+Please select different name.',
 	'createpage_spam' => 'Sorry, your edit could not be saved',
 	'createpage_cant_edit' => 'Could not perform edit',
 	'createpage-dialog-title' => 'Create a New Article',
@@ -22,9 +24,12 @@ $messages['en'] = array(
 	'createpage-dialog-format' => 'Standard layout',
 	'createpage-dialog-blank' => 'Blank page',
 	'createpage-error-empty-title' => 'Please write a title for your article',
-	'createpage-error-invalid-title' => 'Sorry, the article title was invalid. Please use a different title.',
-	'createpage-error-article-exists' => 'An article with that title already exists. You can go to <a href="$1">$2</a>, or rename your page',
-	'createpage-error-article-spam' => 'Sorry, the article title was rejected by our spam filter. Please use a different title.',
+	'createpage-error-invalid-title' => 'Sorry, the article title was invalid.
+Please use a different title.',
+	'createpage-error-article-exists' => 'An article with that title already exists.
+You can go to <a href="$1">$2</a>, or rename your page',
+	'createpage-error-article-spam' => 'Sorry, the article title was rejected by our spam filter.
+Please use a different title.',
 	'createpage-error-article-blocked' => 'Sorry, you are unable to create that article at this time.',
 	'tog-createpagedefaultblank' => 'Use blank page as default for creating a new page',
 	'createpage-newpagelayout' => '',
@@ -36,14 +41,12 @@ $messages['de'] = array(
 	'createpage_title_caption' => 'Seitentitel:',
 );
 
-
 $messages['es'] = array(
 	'createpage-sp-title' => 'Crea un nuevo artículo',
 	'createpage_title' => 'Crear un nuevo artículo',
 	'createpage_title_caption' => 'Título:',
 	'createpage_enter_text' => 'Escribe el contenido aquí:',
 );
-
 
 $messages['fa'] = array(
 	'createpage-sp-title' => 'ایجاد مقاله جدید',
@@ -52,14 +55,12 @@ $messages['fa'] = array(
 	'createpage_enter_text' => 'متن',
 );
 
-
 $messages['fi'] = array(
 	'createpage-sp-title' => 'Luo uusi artikkeli',
 	'createpage_title' => 'Luo uusi artikkeli',
 	'createpage_title_caption' => 'Otsikko:',
 	'createpage_enter_text' => 'Kirjoita tekstiä tähän:',
 );
-
 
 $messages['ja'] = array(
 	'createpage-sp-title' => '新規記事を作成',
@@ -68,14 +69,12 @@ $messages['ja'] = array(
 	'createpage_enter_text' => '本文:',
 );
 
-
 $messages['zh'] = array(
 	'createpage-sp-title' => '新增文章',
 	'createpage_title' => '發表新文章',
 	'createpage_title_caption' => '文章標題',
 	'createpage_enter_text' => '輸入文字',
 );
-
 
 $messages['zh-cn'] = array(
 	'createpage-sp-title' => '新增文章',
@@ -84,14 +83,12 @@ $messages['zh-cn'] = array(
 	'createpage_enter_text' => '输入文字',
 );
 
-
 $messages['zh-hans'] = array(
 	'createpage-sp-title' => '新增文章',
 	'createpage_title' => '发表新文章',
 	'createpage_title_caption' => '文章标题',
 	'createpage_enter_text' => '输入文字',
 );
-
 
 $messages['zh-hant'] = array(
 	'createpage-sp-title' => '新增文章',
@@ -100,14 +97,12 @@ $messages['zh-hant'] = array(
 	'createpage_enter_text' => '輸入文字',
 );
 
-
 $messages['zh-hk'] = array(
 	'createpage-sp-title' => '新增文章',
 	'createpage_title' => '發表新文章',
 	'createpage_title_caption' => '文章標題',
 	'createpage_enter_text' => '輸入文字',
 );
-
 
 $messages['zh-tw'] = array(
 	'createpage-sp-title' => '新增文章',
@@ -116,11 +111,9 @@ $messages['zh-tw'] = array(
 	'createpage_enter_text' => '輸入文字',
 );
 
-
 $messages['fr'] = array(
 	'createpage_enter_text' => 'Texte :',
 );
-
 
 $messages['zh-sg'] = array(
 	'createpage_title' => '发表新文章',
