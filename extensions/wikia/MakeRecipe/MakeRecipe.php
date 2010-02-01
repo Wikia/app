9285d28276144873f1b9933681dc8cc6c6aa5849
bartek <bartek@wikia-inc.com>
2010-02-01 17:58:22 +0000

    MakeRecipe: add skeleton extension for contest ticket
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17919 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/MakeRecipe/MakeRecipe.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Adds contest button (RT #38367)
+ *
+ * @author Bartek Lapinski <bartek@wikia-inc.com>
+ */
+$wgExtensionCredits['other'][] = array(
+                'name' => 'MakeRecipe',
+                'description' => 'Adds contest button',
+                'version' => '0.1',
+                'author' => array('Bartek Lapinski')
+                );
+
+//$wgExtensionMessagesFiles['MakeRecipe'] = dirname(__FILE__) . '/MakeRecipe.i18n.php';
+
+
