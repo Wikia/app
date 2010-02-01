bef51e897d7bd5a684b97f3446da335254273983
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-01 21:33:44 +0000

    * add aliases file for special page localisation * add description messages in $wgExtensionCredits to allow extension description localisation * update 'interwikidispatcher' (display name on Special:SpecialPages and special page title) to look better
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17932 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/InterwikiDispatcher/SpecialInterwikiDispatcher.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Aliases for special pages of SpecialInterwikiDispatcher extension.
+ */
+
+$aliases = array();
+
+/** English
+ */
+$aliases['en'] = array(
+	'InterwikiDispatcher' => array( 'InterwikiDispatcher' ),
+);
