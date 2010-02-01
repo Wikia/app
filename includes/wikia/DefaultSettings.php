760593dd4744968d5cadffb1247587e070bd996f
macbre <macbre@wikia-inc.com>
2010-02-01 15:32:57 +0000

    wgStyleVersion++ (use fresh messages for RTE)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17903 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -17,7 +17,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '17723';
+$wgStyleVersion         = '17900';
 $wgMergeStyleVersionJS  = '16743';
 $wgMergeStyleVersionCSS = '16743';
 
