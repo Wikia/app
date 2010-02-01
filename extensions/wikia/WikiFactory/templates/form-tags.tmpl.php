84636513b561e55487691188d2d315a0ec2ebc7f
eloy <eloy@wikia-inc.com>
2010-02-01 15:43:41 +0000

    tags with autocompletion
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17906 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/templates/form-tags.tmpl.php
+++ extensions/wikia/WikiFactory/templates/form-tags.tmpl.php
@@ -5,6 +5,15 @@ function deletechecked() {
     var answer = confirm("Delete tag?")
 	return answer;
 }
+
+$.getScript(stylepath+'/common/jquery/jquery.autocomplete.js', function() {
+	$('#wftaginput').autocomplete({
+		serviceUrl: wgServer+wgScript+'?action=ajax&rs=WikiFactoryTags::axQuery',
+		minChars:3,
+		deferRequestBy: 0
+	});
+});
+
 /*]]>*/
 </script>
 <h2>Tags</h2>
@@ -14,22 +23,39 @@ function deletechecked() {
 		echo $info;
 	endif;
 ?>
-	<br />
-	Tags defined for this wiki:
+<form action="<?php echo $title->getFullUrl() ?>" method="post">
+<fieldset>
+	<legend>Tags management  </legend>
+	<table>
+	<tr>
+	<td class="mw-label">
+		Tags defined for this wiki
+	</td>
+	<td class="mw-input">
+		&nbsp;
 <?php
-	foreach( $tags as $id => $tag ):
-		echo " <strong>{$tag}</strong><sup><a href=\"";
-		echo $title->getFullUrl( array( "wpTagId" => $id, "wpTagName" => $tag ) );
-		echo "\" class=\"wfTagRemove\" onclick=\"return deletechecked()\" >remove</a></sup> ";
-	endforeach;
+	if( is_array( $tags ) ):
+		foreach( $tags as $id => $tag ):
+			echo " <strong>{$tag}</strong><sup><a href=\"";
+			echo $title->getFullUrl( array( "wpTagId" => $id, "wpTagName" => $tag ) );
+			echo "\" class=\"wfTagRemove\" onclick=\"return deletechecked()\" >remove</a></sup> ";
+		endforeach;
+	endif;
 ?>
-	<br />
-	<div>
-	Add new tag:
-	<form action="<?php echo $title->getFullUrl() ?>" method="post">
-		<input type="text" name="wpTag" />
-		<input type="submit" value="Add" />
-	</form>
-	</div>
+	</td>
+	</tr>
+	<tr>
+		<td class="mw-label">
+			Add new tag
+		</td>
+		<td class="mw-input">
+			&nbsp;
+			<input type="text" name="wpTag" id="wftaginput" />
+			<input type="submit" value="Add" />
+		</td>
+	</tr>
+	</table>
+</fieldset>
+</form
 </div>
 <!-- e:<?= __FILE__ ?> -->
