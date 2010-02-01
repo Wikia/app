dd6383b96a212f2468c0902033f31c5119c55213
tor <tor@wikia-inc.com>
2010-02-01 12:33:16 +0000

    CreatePage: removed unused messages (#38802)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17889 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreatePage/CreatePage.i18n.php
+++ extensions/wikia/CreatePage/CreatePage.i18n.php
@@ -6,16 +6,9 @@
 $messages = array();
 $messages['en'] = array(
 	'createpage-sp-title' => 'Create a new article',
-	'createpage_help' => '',
-	'createpage_caption' => 'title',
-	'createpage_button_caption' => 'Create!',
 	'createpage_title' => 'Create a new article',
 	'createpage_title_caption' => 'Title:',
 	'createpage_enter_text' => 'Text:',
-	'createpage_here' => 'here',
-	'createpagetext' => '',
-	'createpage_alternate_creation' => 'or click $1 to use original editor',
-	'createpage_categories_help' => 'Categories help organize information in this wiki. Please choose from the list below or type a new one.',
 	'createpage_empty_title_error' => 'Title required',
 	'createpage_empty_article_body_error' => 'Article text required',
 	'createpage_invalid_title_error' => 'Invalid title',
@@ -39,166 +32,98 @@ $messages['en'] = array(
 
 $messages['de'] = array(
 	'createpage-sp-title' => 'Neue Seite anlegen',
-	'createpage_caption' => 'Seitentitel',
-	'createpage_button_caption' => 'Seite speichern',
 	'createpage_title' => 'Neue Seite anlegen',
 	'createpage_title_caption' => 'Seitentitel:',
-	'createpage_here' => 'hier',
-	'createpage_alternate_creation' => 'oder klicke $1, um den herkömmlichen Editor zu verwenden',
-	'createpage_categories_help' => 'Kategorien sind hilfreich beim Organisieren der Wiki-Inhalte. Bitte wähle die passende Kategorie aus der Vorschlagsliste oder gib eine neue Kategorie ein (mehrere Kategorien bitte durch Kommata trennen).',
 );
 
 
 $messages['es'] = array(
 	'createpage-sp-title' => 'Crea un nuevo artículo',
-	'createpage_caption' => 'título',
-	'createpage_button_caption' => '¡Crear!',
 	'createpage_title' => 'Crear un nuevo artículo',
 	'createpage_title_caption' => 'Título:',
 	'createpage_enter_text' => 'Escribe el contenido aquí:',
-	'createpage_here' => 'aquí',
-	'createpage_alternate_creation' => 'o haz clic $1 para utilizar el editor básico',
-	'createpage_categories_help' => 'Las categorías ayudan a organizar la información de este wiki. Por favor, selecciona al menos una de la lista de más abajo o escribe una nueva.',
 );
 
 
 $messages['fa'] = array(
 	'createpage-sp-title' => 'ایجاد مقاله جدید',
-	'createpage_caption' => 'عنوان',
-	'createpage_button_caption' => 'ایجاد کن!',
 	'createpage_title' => 'ایجاد مقالۀ جدید',
 	'createpage_title_caption' => 'نام',
 	'createpage_enter_text' => 'متن',
-	'createpage_categories_help' => 'رده‌ها به شما کمک می‌کنند که مطالب این ویکی را سازماندهی کنید. لطفا یک رده از رده‌های زیر را انتخاب کنید یا ردۀ جدیدی در جعبه زیر اضافه کنید..',
 );
 
 
 $messages['fi'] = array(
 	'createpage-sp-title' => 'Luo uusi artikkeli',
-	'createpage_caption' => 'otsikko',
-	'createpage_button_caption' => 'Luo artikkeli',
 	'createpage_title' => 'Luo uusi artikkeli',
 	'createpage_title_caption' => 'Otsikko:',
 	'createpage_enter_text' => 'Kirjoita tekstiä tähän:',
-	'createpage_here' => 'tästä',
-	'createpage_alternate_creation' => 'tai paina $1 käyttääksesi alkuperäistä editoria',
-	'createpage_categories_help' => 'Luokat auttavat järjestämään tietoa tässä wikissä. Alapuolella on lista suosituista luokista tässä wikissä. Mitä isompi sana, sitä suositumpi luokka on. Klikkaa luokkia, jotka haluat lisätä ja ne ilmestyvät laatikkoon alapuolella.',
 );
 
 
 $messages['ja'] = array(
 	'createpage-sp-title' => '新規記事を作成',
-	'createpage_caption' => '項目名',
-	'createpage_button_caption' => '作成する',
 	'createpage_title' => '新規記事を作成',
 	'createpage_title_caption' => 'タイトル:',
 	'createpage_enter_text' => '本文:',
-	'createpage_here' => 'ここ',
-	'createpage_alternate_creation' => '従来のエディタを使うには、 $1 をクリックしてください',
-	'createpage_categories_help' => 'カテゴリを使うことで、このウィキアの情報を整理できます。是非とも、下のリストから選ぶか、新しいカテゴリを作成してみてください。',
 );
 
 
 $messages['zh'] = array(
 	'createpage-sp-title' => '新增文章',
-	'createpage_caption' => '分類：',
-	'createpage_button_caption' => '發布！',
 	'createpage_title' => '發表新文章',
 	'createpage_title_caption' => '文章標題',
 	'createpage_enter_text' => '輸入文字',
-	'createpage_here' => '這裡',
-	'createpage_alternate_creation' => '原始編輯模式請點選 $1',
-	'createpage_categories_help' => '將文章加註分類，可加強這個站上的文章的組織。你可以在下方挑選一個適當的分類，或是直接輸入一個新的分類。',
 );
 
 
 $messages['zh-cn'] = array(
 	'createpage-sp-title' => '新增文章',
-	'createpage_caption' => '分类：',
-	'createpage_button_caption' => '发布！',
 	'createpage_title' => '发表新文章',
 	'createpage_title_caption' => '文章标题',
 	'createpage_enter_text' => '输入文字',
-	'createpage_here' => '这里',
-	'createpage_alternate_creation' => '原始编辑模式请点选 $1',
-	'createpage_categories_help' => '将文章加註分类，可加強这个站上的文章的组织。你可以在下方挑选一个适当的分类，或是直接输入一个新的分类。',
 );
 
 
 $messages['zh-hans'] = array(
 	'createpage-sp-title' => '新增文章',
-	'createpage_caption' => '分类：',
-	'createpage_button_caption' => '发布！',
 	'createpage_title' => '发表新文章',
 	'createpage_title_caption' => '文章标题',
 	'createpage_enter_text' => '输入文字',
-	'createpage_here' => '这里',
-	'createpage_alternate_creation' => '原始编辑模式请点选 $1',
-	'createpage_categories_help' => '将文章加註分类，可加強这个站上的文章的组织。你可以在下方挑选一个适当的分类，或是直接输入一个新的分类。',
 );
 
 
 $messages['zh-hant'] = array(
 	'createpage-sp-title' => '新增文章',
-	'createpage_caption' => '分類：',
-	'createpage_button_caption' => '發布！',
 	'createpage_title' => '發表新文章',
 	'createpage_title_caption' => '文章標題',
 	'createpage_enter_text' => '輸入文字',
-	'createpage_here' => '這裡',
-	'createpage_alternate_creation' => '原始編輯模式請點選 $1',
-	'createpage_categories_help' => '將文章加註分類，可加強這個站上的文章的組織。你可以在下方挑選一個適當的分類，或是直接輸入一個新的分類。',
 );
 
 
 $messages['zh-hk'] = array(
 	'createpage-sp-title' => '新增文章',
-	'createpage_caption' => '分類：',
-	'createpage_button_caption' => '發布！',
 	'createpage_title' => '發表新文章',
 	'createpage_title_caption' => '文章標題',
 	'createpage_enter_text' => '輸入文字',
-	'createpage_here' => '這裡',
-	'createpage_alternate_creation' => '原始編輯模式請點選 $1',
-	'createpage_categories_help' => '將文章加註分類，可加強這個站上的文章的組織。你可以在下方挑選一個適當的分類，或是直接輸入一個新的分類。',
 );
 
 
 $messages['zh-tw'] = array(
 	'createpage-sp-title' => '新增文章',
-	'createpage_caption' => '分類：',
-	'createpage_button_caption' => '發布！',
 	'createpage_title' => '發表新文章',
 	'createpage_title_caption' => '文章標題',
 	'createpage_enter_text' => '輸入文字',
-	'createpage_here' => '這裡',
-	'createpage_alternate_creation' => '原始編輯模式請點選 $1',
-	'createpage_categories_help' => '將文章加註分類，可加強這個站上的文章的組織。你可以在下方挑選一個適當的分類，或是直接輸入一個新的分類。',
 );
 
 
 $messages['fr'] = array(
-	'createpage_caption' => 'titre',
-	'createpage_button_caption' => 'Créer !',
 	'createpage_enter_text' => 'Texte :',
-	'createpage_here' => 'ici',
-	'createpage_alternate_creation' => 'ou cliquez $1 pour utiliser l\'éditeur original',
-	'createpage_categories_help' => 'Les catégories sont bien pour mettre un structure au wiki. Choissisez une catégorie des examples ou mettez une nouvelle catégorie (séparer plusieures catégories avec des virgules).',
 );
 
 
 $messages['zh-sg'] = array(
-	'createpage_caption' => '分类：',
-	'createpage_button_caption' => '发布！',
 	'createpage_title' => '发表新文章',
 	'createpage_title_caption' => '文章标题',
 	'createpage_enter_text' => '输入文字',
-	'createpage_here' => '这里',
-	'createpage_alternate_creation' => '原始编辑模式请点选 $1',
-	'createpage_categories_help' => '将文章加註分类，可加強这个站上的文章的组织。你可以在下方挑选一个适当的分类，或是直接输入一个新的分类。',
-);
-
-
-$messages['bg'] = array(
-	'createpage_button_caption' => 'Създаване!',
 );
