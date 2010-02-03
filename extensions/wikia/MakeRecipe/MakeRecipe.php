940118e5f5bc3beb19fbac50613b380f7b6b9d01
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-03 09:39:43 +0000

    MakeRecipe: display the button when needed
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@17994 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MakeRecipe/MakeRecipe.php
+++ extensions/wikia/MakeRecipe/MakeRecipe.php
@@ -4,13 +4,39 @@
  *
  * @author Bartek Lapinski <bartek@wikia-inc.com>
  */
-$wgExtensionCredits['other'][] = array(
+$wgExtensionCredits['parserhook'][] = array(
                 'name' => 'MakeRecipe',
                 'description' => 'Adds contest button',
-                'version' => '0.1',
+                'version' => '0.2',
                 'author' => array('Bartek Lapinski')
                 );
 
 $wgExtensionMessagesFiles['MakeRecipe'] = dirname(__FILE__) . '/MakeRecipe.i18n.php';
+
+$wgHooks['ParserFirstCallInit'][] = 'wfMakeRecipe';
+
+function wfMakeRecipe( &$parser ) {
+        $parser->setHook( 'makerecipe', 'wfMakeRecipeButton' );
+        return true;
+}
+
+function wfMakeRecipeButton( $input, $argv, &$parser ) {
+        wfLoadExtensionMessages( 'MakeRecipe' );
+        global $wgRequest, $wgScript;
+
+	$link = '';
+	$onclick = '';
+	$output = Xml::openElement( 'a', array(
+						'class' => 'wikia_button',
+						'href' => $link,
+						'onclick' => $onclick
+						 ) )
+		.Xml::openElement( 'span' )
+		.wfMsg( 'makerecipe' )	
+		.Xml::closeElement( 'span' )
+		.Xml::closeElement( 'a' );
+
+	return $parser->replaceVariables( $output );	
+}
 
 
