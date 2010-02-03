9acac596d02bd34bc9096fd4e5f090c3b4c87b7a
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-03 14:15:58 +0000

    CSS: move Error box on Special:Signup to proper file (will be use also for other skins)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18020 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -17,7 +17,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion         = '17901';
+$wgStyleVersion         = '17902';
 $wgMergeStyleVersionJS  = '16743';
 $wgMergeStyleVersionCSS = '16743';
 
