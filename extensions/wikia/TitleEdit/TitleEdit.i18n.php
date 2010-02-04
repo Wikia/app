a03dc8538dae2dd4b68d15d848597fa85c4d8524
siebrand <siebrand@wikia-inc.com>
2010-02-04 13:21:03 +0000

    * Update and add description message * Format i18n file * Whitespace updates
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18068 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/TitleEdit/TitleEdit.i18n.php
+++ extensions/wikia/TitleEdit/TitleEdit.i18n.php
@@ -1,15 +1,24 @@
 <?php
+/**
+ * Internationalisation file for extension TitleEdit.
+ *
+ * @addtogroup Extensions
+ */
 
 $messages = array();
 
+/** English
+ * @author Bartek Lapinski
+ */
 $messages['en'] = array(
-        'titleedit' => 'edit',
-);
-
-$messages['pl'] = array(
-	'titleedit' => 'edytuj',
+	'titleedit-desc' => 'Adds title edit button to top of pages',
+	'titleedit' => 'edit',
 );
 
 $messages['qqq'] = array(
 	'titleedit' => 'This is shown next to the H1 element (article title) and opens the edit form (full article edit) when clicked',
+);
+
+$messages['pl'] = array(
+	'titleedit' => 'edytuj',
 );
