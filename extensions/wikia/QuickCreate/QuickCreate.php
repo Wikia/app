6674767a7e14e1d5a962707eb0ed9944b6265a75
siebrand <siebrand@wikia-inc.com>
2010-02-04 13:14:46 +0000

    * Update and add description message * Format i18n file * Update indentation and remove trailing whitespace
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18067 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/QuickCreate/QuickCreate.php
+++ extensions/wikia/QuickCreate/QuickCreate.php
@@ -4,41 +4,41 @@
  *
  * @author Bartek Lapinski <bartek@wikia-inc.com>
  */
+
 $wgExtensionCredits['parserhook'][] = array(
-                'name' => 'QuickCreate',
-                'description' => 'Adds a create new page button with ability to log in for anons',
-                'version' => '1,0',
-                'author' => array('Bartek Lapinski')
-                );
+	'name' => 'QuickCreate',
+	'description' => 'Adds a create new page button with ability to log in for anons',
+	'descriptionmsg' => 'quickcreate-desc',
+	'version' => '1,0',
+	'author' => array( 'Bartek Lapinski' ),
+);
 
 $wgExtensionMessagesFiles['QuickCreate'] = dirname(__FILE__) . '/QuickCreate.i18n.php';
 
 $wgHooks['ParserFirstCallInit'][] = 'wfQuickCreate';
 
 function wfQuickCreate( &$parser ) {
-        $parser->setHook( 'quickcreate', 'wfQuickCreateButton' );
-        return true;
+	$parser->setHook( 'quickcreate', 'wfQuickCreateButton' );
+	return true;
 }
 
 function wfQuickCreateButton( $input, $argv, &$parser ) {
-        wfLoadExtensionMessages( 'QuickCreate' );
-        global $wgRequest, $wgScript;
-
+	wfLoadExtensionMessages( 'QuickCreate' );
+	global $wgRequest, $wgScript;
 
 	$title = Title::makeTitle( NS_SPECIAL, "CreatePage");
 	$link = $title->getFullUrl();
 	$onclick = ''; // todo track or not to track? not until feedback
 	$output = Xml::openElement( 'a', array(
-						'class' => 'wikia_button',
-						'id'	=> 'mr-submit',
-						'href' => $link,
-						'onclick' => $onclick
-						 ) )
+			'class' => 'wikia_button',
+			'id'	=> 'mr-submit',
+			'href' => $link,
+			'onclick' => $onclick
+		) )
 		.Xml::openElement( 'span' )
 		.wfMsg( 'quickcreate' )	
 		.Xml::closeElement( 'span' )
 		.Xml::closeElement( 'a' );
 
-	return $parser->replaceVariables( $output );	
+	return $parser->replaceVariables( $output );
 }
-
