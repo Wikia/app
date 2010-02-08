e9485f48bfe5d8a83ba7753ec69cc0bace0390b7
nick <nick@wikia-inc.com>
2010-02-08 19:39:20 +0000

    Updated comscore tag
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18200 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AnalyticsEngine/AnalyticsProviderComscore.php
+++ extensions/wikia/AnalyticsEngine/AnalyticsProviderComscore.php
@@ -12,13 +12,24 @@ class AnalyticsProviderComscore implements iAnalyticsProvider {
 		switch ($event){
 		  case AnalyticsEngine::EVENT_PAGEVIEW : return '
 <!-- Begin comScore Tag -->
-<script type="text/javascript">/*<![CDATA[*/
-setTimeout(function(){ var url = "http" + (/^https:/.test(document.location.href) ? "s" : "") + "://beacon.scorecardresearch.com/scripts/beacon.dll" + "?c1=2&c2=6177433&c3=&c4=&c5=&c6=&c7=" + escape(document.location.href) + "&c8=" + escape(document.title) + "&c9=" + escape(document.referrer) + "&c10=" + escape(screen.width+\'x\'+screen.height) + "&rn=" + (new Date()).getTime(); var i = new Image(); i.src = url; }, 1);
-/*]]>*/</script>
+<script>
+document.write(unescape("%3Cscript src=\'" + (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js\' %3E%3C/script%3E"));
+</script>
+<script>
+COMSCORE.beacon({
+  c1:2,
+  c2:6177433,
+  c3:"",
+  c4:"",
+  c5:"",
+  c6:"",
+  c15:""
+});
+</script>
 <noscript>
-<img src="http://beacon.scorecardresearch.com/scripts/beacon.dll?c1=2&amp;c2=6177433&amp;c3=&amp;c4=&amp;c5=&amp;c6=&amp;c7=&amp;x=NOJAVASCRIPT" alt="" />
+<img src="http://b.scorecardresearch.com/p?c1=2&c2=6177433&c3=&c4=&c5=&c6=&c15=&cj=1" />
 </noscript>
-<!-- End comScore Tag -->'; 
+<!-- End comScore Tag -->';
 			break;
                   default: return '<!-- Unsupported event for ' . __CLASS__ . ' -->';
 		}
