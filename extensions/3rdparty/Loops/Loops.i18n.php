13a4137bec0cd6f1a27c6f868907ba0fdc6307aa
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-09 21:29:24 +0000

    update ext to latest from mediawiki.org (0.3.0)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18283 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/Loops/Loops.i18n.php
+++ extensions/3rdparty/Loops/Loops.i18n.php
@@ -1,21 +1,42 @@
 <?php
 
-class Loops_i18n {
-    private static $words = array(
+class Loops_i18n
+{
+    private $words = array(
     // English
         'en' => array(
-            'dowhile'         => array( 0, 'dowhile' ),
-            'while'           => array( 0, 'while' ),
-            'foreachnamedarg' => array( 0, 'foreachnamedarg' ),
+            'dowhile'    => array( 0, 'dowhile' ),
+            'while'      => array( 0, 'while' ),
+            'forargs'    => array( 0, 'forargs' ),
+            'fornumargs' => array( 0, 'fornumargs' ),
+            'loop'       => array( 0, 'loop' ),
         ),
     );
 
-    private static $messages = array(
+    private $messages = array(
     // English
         'en' => array(
             'loops_max' => "Maximum number of loops have been performed",
         )
     );
+
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        // create the singleton if needed
+        if ( self::$instance === null )
+            self::$instance = new self();
+
+        return self::$instance;
+    }
+
+    /**
+     * limited-access constructor to insure singleton
+     */
+    protected function __construct()
+    {
+    }
 
     /**
      * Get translated magic words, if available
@@ -23,15 +44,17 @@ class Loops_i18n {
      * @param string $lang Language code
      * @return array
      */
-    public static function magicWords( $lang ) {
+    public function magicWords( $lang )
+    {
         // English is used as a fallback, and the English synonyms are
         // used if a translation has not been provided for a given word
-        return ( $lang == 'en' || !isset( self::$words[$lang] ) ) ?
-            self::$words['en'] :
-            array_merge( self::$words['en'], self::$words[$lang] );
+        return ( $lang == 'en' || !isset( $this->words[ $lang ] ) ) ?
+            $this->words[ 'en' ] :
+            array_merge( $this->words[ 'en' ], $this->words[ $lang ] );
     }
 
-    public static function getMessages() {
-        return self::$messages;
+    public function getMessages()
+    {
+        return $this->messages;
     }
 }
--- extensions/3rdparty/Loops/Loops.php
+++ extensions/3rdparty/Loops/Loops.php
@@ -14,77 +14,113 @@
  *     initial creation.
  * @version 0.2.0
  *     #foreachnamedarg added
+ * @version 0.3.0
+ *     technical change from static members and methods to singleton
+ *     #loop added
+ *     #fornumargs added
+ *     #foreachnamedarg renamed to #forargs expaned to include indexed template
+ *         arguments as well as named arguments
  *
- * @todo allow other functions in the extension to work without
- *     VariablesExtension.
- * @todo #foreacharg and #foreachnumarg
+ * @todo message notifying the need for the VariablesExtension extension for
+ *     certain functions
  * @todo inline documentation
  */
 
-if ( !defined( "MEDIAWIKI" ) ) {
+if ( !defined( "MEDIAWIKI" ) )
+{
     die( "This file is a MediaWiki extension, it is not a valid entry point" );
 }
 
-$wgHooks['ParserFirstCallInit'][] = "ExtLoops::setup";
-$wgExtensionCredits['parserhook'][] = array(
-    'author'      => 'David M. Sledge',
-    'name'        => 'Loops',
-    'version'     => ExtLoops::VERSION,
-    'description' => 'Parser functions for performing loops',
-    'url'         => 'http://www.mediawiki.org/wiki/Extension:Loops',
+$wgHooks[ 'ParserFirstCallInit' ][] = $wgHooks[ 'LanguageGetMagic' ][]
+    = $wgHooks[ 'ParserLimitReport' ][] = ExtLoops::getInstance();
+$wgExtensionCredits[ 'parserhook' ][] = array(
+    'path'        => __FILE__,
+    "author"      => "David M. Sledge",
+    "name"        => "Loops",
+    "version"     => ExtLoops::VERSION,
+    "description" => "Parser functions for performing loops",
+    "url"         => "http://www.mediawiki.org/wiki/Extension:Loops",
 );
 
-$wgHooks['LanguageGetMagic'][]  = 'ExtLoops::languageGetMagic';
-$wgHooks['ParserLimitReport'][] = 'ExtLoops::parserLimitReport';
+class ExtLoops
+{
+    const VERSION = "0.3.0";
 
-class ExtLoops {
-    const VERSION = "0.2.0";
-    private static $parserFunctions = array(
-        'dowhile'         => 'dowhile',
-        'while'           => 'whileHook',
-        'foreachnamedarg' => 'foreachNamedArg',
-    );
     public static $maxLoops = 100;  // maximum number of loops allowed
-                                    // (-1 = no limit).  #foreachnamedarg is
+                                    // (-1 = no limit).  #forargs is
                                     // not limited by this.
+    private static $instance = null;
 
-    public static function setup( &$parser ) {
+    private $parserFunctions = array(
+        'dowhile'    => array( 'dowhile', SFH_OBJECT_ARGS ),
+        'while'      => array( 'whileHook', SFH_OBJECT_ARGS ),
+        'loop'       => array( 'loop', SFH_OBJECT_ARGS ),
+        'forargs'    => array( 'forArgs', SFH_OBJECT_ARGS ),
+        'fornumargs' => array( 'forNumArgs', SFH_OBJECT_ARGS ),
+
+    );
+    private $loopCount = 0;
+
+    public static function getInstance()
+    {
+        // create the singleton if needed
+        if ( self::$instance === null )
+            self::$instance = new self();
+
+        return self::$instance;
+    }
+
+    /**
+     * limited-access constructor to insure singleton
+     */
+    protected function __construct()
+    {
+    }
+
+    public function onParserFirstCallInit( &$parser )
+    {
         global $wgMessageCache, $wgHooks;
 
         // These functions accept DOM-style arguments
-        foreach( self::$parserFunctions as $hook => $function )
-            $parser->setFunctionHook( $hook,
-                array( __CLASS__, $function ), SFH_OBJECT_ARGS );
+        foreach( $this->parserFunctions as $hook => $callAndFlags )
+            $parser->setFunctionHook( $hook, array( $this, $callAndFlags[ 0 ] ),
+                $callAndFlags[ 1 ] );
 
         require_once( dirname( __FILE__ ) . '/Loops.i18n.php' );
 
-        foreach( Loops_i18n::getMessages() as $lang => $messages )
+        foreach( Loops_i18n::getInstance()->getMessages()
+            as $lang => $messages )
             $wgMessageCache->addMessages( $messages, $lang );
 
-        $wgHooks['ParserClearState'][] = __CLASS__ . '::parserClearState';
+        $wgHooks[ 'ParserClearState' ][] = $this;
 
         return true;
     }
 
-    public static function languageGetMagic( &$magicWords, $langCode ) {
+    public function onLanguageGetMagic( &$magicWords, $langCode )
+    {
         require_once( dirname( __FILE__ ) . '/Loops.i18n.php' );
 
-        foreach( Loops_i18n::magicWords( $langCode ) as $word => $trans )
-            $magicWords[$word] = $trans;
+        foreach( Loops_i18n::getInstance()->magicWords( $langCode )
+            as $word => $trans )
+            $magicWords[ $word ] = $trans;
 
         return true;
     }
 
-    public static function parserLimitReport( $parser, &$report ) {
-        if ( isset( $parser->loops_count ) ) {
-            $report .= "#(do)while count: {$parser->loops_count}/" .
-                self::$maxLoops . "\n";
+    public function onParserLimitReport( $parser, &$report )
+    {
+        if ( isset( $this->loopCount ) )
+        {
+            $report .= "ExtLoops count: {$this->loopCount}/" . self::$maxLoops
+                . "\n";
         }
 
         return true;
     }
 
-    public static function whileHook( &$parser, $frame, $args ) {
+    public function whileHook( &$parser, $frame, $args )
+    {
         // bug 12842:  first argument is automatically
         //   expanded, so we ignore this one
         array_shift( $args );
@@ -92,20 +128,22 @@ class ExtLoops {
         $loopStatement = array_shift( $args );
         $output = '';
 
-        while ( isset( $test ) && trim( $frame->expand( $test ) ) !== '' ) {
+        while ( isset( $test ) && trim( $frame->expand( $test ) ) !== '' )
+        {
             if ( self::$maxLoops >= 0 &&
-                ++$parser->loops_count > self::$maxLoops )
-                return wfMsgForContent( 'loops_max' );
+                ++$this->loopCount > self::$maxLoops )
+                return $output . wfMsgForContent( 'loops_max' );
 
             $output .= isset( $loopStatement ) ?
                 trim( $frame->expand( $loopStatement ) ) : '';
         }
 
-        //return '<pre><nowiki>'. $output . '</nowiki></pre>';
+        //return '<pre><nowiki>'. $output . '</nowiki></'. 'pre>';
         return $output;
     }
 
-    public static function dowhile( &$parser, $frame, $args ) {
+    public function dowhile( &$parser, $frame, $args )
+    {
         // bug 12842:  first argument is automatically
         //   expanded, so we ignore this one
         array_shift( $args );
@@ -113,21 +151,42 @@ class ExtLoops {
         $loopStatement = array_shift( $args );
         $output = '';
 
-        do {
+        do
+        {
             if ( self::$maxLoops >= 0 &&
-                ++$parser->loops_count > self::$maxLoops )
-                return wfMsgForContent( 'loops_max' );
+                ++$this->loopCount > self::$maxLoops )
+                return $output . wfMsgForContent( 'loops_max' );
 
             $output .= isset( $loopStatement ) ?
                 trim( $frame->expand( $loopStatement ) ) : '';
         }
         while ( isset( $test ) && trim( $frame->expand( $test ) ) !== '' );
 
-        //return '<pre><nowiki>'. $output . '</nowiki></pre>';
+        //return '<pre><nowiki>'. $output . '</nowiki></'. 'pre>';
         return $output;
     }
 
-    public static function foreachNamedArg( &$parser, $frame, $args ) {
+    public function forArgs( &$parser, $frame, $args )
+    {
+        if ( !( $frame instanceof PPTemplateFrame_DOM ) )
+        {
+            $arg = array_shift( $args );
+            $arg = isset( $arg ) ? $frame->expand( $arg ) : '';
+
+            // TODO: get the synonym for the content language
+            $out = "{{#forargs:$arg";
+
+            // expand and display each argument
+            while ( ( $arg = array_shift( $args ) ) !== null )
+            {
+                $out .= '|' . $frame->expand( $arg );
+            }
+
+            $out .= '}}';
+
+            return $out;
+        }
+
         global $wgExtVariables;
 
         // The first arg is already expanded, but this is a good habit to have.
@@ -142,14 +201,17 @@ class ExtLoops {
         $loopStatement = isset( $args[3] ) ? $args[3] : '';
         $output = '';
 
-        foreach ( array_keys( $frame->namedArgs ) as $argName ) {
+        $tArgs = preg_match( '/^([1-9][0-9]*)?$/', $filter ) > 0
+            ? $frame->getArguments() : $frame->getNamedArguments();
+
+        foreach ( $tArgs as $argName => $argVal )
+        {
             if ( $filter == '' || strpos( $argName, $filter ) === 0 )
             {
                 if ( $keyVarName !== $valueVarName )
                     $wgExtVariables->vardefine( $parser, $keyVarName,
                         substr( $argName, strlen( $filter ) ) );
 
-                $argVal = $frame->getNamedArgument( $argName );
                 $wgExtVariables->vardefine( $parser, $valueVarName, $argVal );
                 $output .= trim( $frame->expand( $loopStatement ) );
             }
@@ -158,8 +220,101 @@ class ExtLoops {
         return $output;
     }
 
-    public static function parserClearState( &$parser ) {
-        $parser->loops_count = 0;
+    public function forNumArgs( &$parser, $frame, $args )
+    {
+        if ( !( $frame instanceof PPTemplateFrame_DOM ) )
+        {
+            $arg = array_shift( $args );
+            $arg = isset( $arg ) ? $frame->expand( $arg ) : '';
+
+            // TODO: get the synonym for the content language
+            $out = "{{#forargs:$arg";
+
+            // expand and display each argument
+            while ( ( $arg = array_shift( $args ) ) !== null )
+            {
+                $out .= '|' . $frame->expand( $arg );
+            }
+
+            $out .= '}}';
+
+            return $out;
+        }
+
+        global $wgExtVariables;
+
+        // The first arg is already expanded, but this is a good habit to have.
+        // name of the variable to store the argument name.  this
+        // will be accessed in the loop by using {{#var:}}
+        $keyVarName = isset( $args[0] ) ?
+            trim( $frame->expand( $args[0] ) ) : '';
+        // name of the variable to store the argument value.
+        $valueVarName = isset( $args[1] ) ?
+            trim( $frame->expand( $args[1] ) ) : '';
+        $loopStatement = isset( $args[2] ) ? $args[2] : '';
+        $output = '';
+        $numArgs = $frame->getArguments();
+        ksort( $numArgs );
+
+        foreach ( $numArgs as $argNumber => $argVal )
+        {
+            if ( is_string( $argNumber ) )
+                continue;
+
+            if ( $keyVarName !== $valueVarName )
+                $wgExtVariables->vardefine( $parser, $keyVarName, $argNumber );
+
+            $wgExtVariables->vardefine( $parser, $valueVarName, $argVal );
+            $output .= trim( $frame->expand( $loopStatement ) );
+        }
+
+        return $output;
+    }
+
+    public function loop( &$parser, $frame, $args )
+    {
+        global $wgExtVariables;
+        // snag the variable name
+        $varName = array_shift( $args );
+        $varName = $varName === null ? '' : trim( $frame->expand( $varName ) );
+        // grab the intitial value for the variable (default to 0)
+        $startVal = array_shift( $args );
+        $startVal = $startVal === null
+            ? 0 : intval( trim( $frame->expand( $startVal ) ) );
+        // How many times are we gonna loop?
+        $count = array_shift( $args );
+
+        if ( $count === null )
+            return '';
+
+        $endVal = $startVal +  intval( trim( $frame->expand( $count ) ) );
+
+        if ( $endVal == $startVal )
+            return '';
+
+        // grab the unexpanded loop statement
+        $loopStatement = array_shift( $args );
+        $output = '';
+
+        for ( ; $startVal != $endVal;
+            $startVal < $endVal ? $startVal++ : $startVal-- )
+        {
+            if ( self::$maxLoops >= 0 &&
+                ++$this->loopCount > self::$maxLoops )
+                return $output . wfMsgForContent( 'loops_max' );
+
+            $wgExtVariables->vardefine( $parser, $varName, $startVal );
+
+            $output .= isset( $loopStatement ) ?
+                trim( $frame->expand( $loopStatement ) ) : '';
+        }
+
+        return $output;
+    }
+
+    public function onParserClearState( &$parser )
+    {
+        $this->loopCount = 0;
 
         return true;
     }
