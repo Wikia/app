575425c144c0f06ed3ed8b05ee89fc0a6f6b0707
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-10 12:10:30 +0000

    actually isEmailConfirmed is better in that place
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18310 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/WikiaApiCreatorReminderEmail.php
+++ extensions/wikia/AutoCreateWiki/WikiaApiCreatorReminderEmail.php
@@ -59,7 +59,7 @@ class WikiaApiCreatorReminderEmail extends ApiBase {
 			}
 
 			Wikia::log( __METHOD__, "user", $founder->getName() );
-			if( $founder && $founder->canReceiveEmail() ) {
+	      			if( $founder && $founder->isEmailConfirmed() ) {
 				if( $founder->sendMail(
 					wfMsg( "autocreatewiki-reminder-subject" ),
 					wfMsg( "autocreatewiki-reminder-body", array( $founder->getName(), $wgServer ) ),
