ada7446b6e15230d4e936dbe161c459f4b801fd5
emil <emil@wikia-inc.com>
2010-02-10 14:28:14 +0000

    merged r18320
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18321 e846bd91-c042-42a7-a218-4b65e2c1904d

--- StartProfiler.php
+++ StartProfiler.php
@@ -1,6 +1,6 @@
 <?php
 
-$rand = mt_rand(0, 0x7fffffff);
+$rand_mod_10 = mt_rand(0, 0x7fffffff) % 10;
 
 if( !empty( $_GET['forceprofile'] ) ) {
 	require_once( dirname(__FILE__).'/includes/ProfilerSimpleText.php' );
@@ -9,10 +9,13 @@ if( !empty( $_GET['forceprofile'] ) ) {
 } elseif( !empty( $_GET['forcetrace'] ) ) {
 	require_once( dirname(__FILE__).'/includes/ProfilerSimpleTrace.php' );
 	$wgProfiler = new ProfilerSimpleTrace;
-} elseif( !( $rand % 10 ) ) {
-	// every 10th request gets reported via stomp
+} elseif( $rand_mod_10 == 1 ) {
 	require_once( dirname(__FILE__).'/includes/ProfilerSimpleStomp.php' );
 	$wgProfiler = new ProfilerSimpleStomp;
+} elseif( $rand_mod_10 == 2 ) {
+	require_once( dirname(__FILE__).'/includes/ProfilerSimpleUDP.php' );
+	$wgProfiler = new ProfilerSimpleUDP;
+	$wgProfiler->setProfileId( 'wikia' );
 } else {
 	require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
 }
