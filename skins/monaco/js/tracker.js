fa4ce6f806ec05fdd222919e5c47d324c9b7c1cf
tomek <tomek@wikia-inc.com>
2010-02-12 11:26:17 +0000

    homepage: change track url
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18403 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/monaco/js/tracker.js
+++ skins/monaco/js/tracker.js
@@ -146,19 +146,3 @@ var initTracker = function() {
 		$.tracker.byStr(str);
 	});
 };
-
-jQuery.tracker.track = function(fakeurl) {     
-    fakeurlArray = fakeurl.split('/');
-    if(typeof urchinTracker != 'undefined') {
-        _uacct = "UA-2871474-1";
-        var username = wgUserName == null ? 'anon' : 'user';
-        var fake = '/1_home/' + username + '/' + fakeurl;
-        $().log('tracker: ' + fake);
-        urchinTracker(fake);
-        if(wgPrivateTracker) {
-            fake = '/1_home/' + wgDB + '/' + username + '/' + fakeurl;
-            $().log('tracker: ' + fake);
-            urchinTracker(fake);
-        }
-    }
-};
