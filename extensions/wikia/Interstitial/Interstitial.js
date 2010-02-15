7606208ba447f404738cd8e07f6cc2743dc749f2
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-15 05:16:19 +0000

    Interstitials: tweaks to the edge-cases to make sure everything is smooth prior to the branch tonight.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18435 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Interstitial/Interstitial.js
+++ extensions/wikia/Interstitial/Interstitial.js
@@ -7,12 +7,13 @@ console.log("Int: The Interstitials Code is loded.");
 console.log("Int: count: " + count);
 	count += 1;
 console.log("Int: next: " + count);
-	$.cookies.set(COOKIE_NAME, count, {
+	$.cookies.set(COOKIE_NAME, count + '', {
 		path: wgCookiePath,
 		domain: wgCookieDomain
 	});
+console.log("Int: now set to: " + $.cookies.get(COOKIE_NAME) + " (should be same as 'next')");
 
-	if((wgAdsInterstitialsPagesBeforeFirstAd == count) || ((count > wgAdsInterstitialsPagesBeforeFirstAd) && ((count % (wgAdsInterstitialsPagesBetweenAds+1)) == 0))){
+	if((wgAdsInterstitialsPagesBeforeFirstAd == count - 1) || ((count > wgAdsInterstitialsPagesBeforeFirstAd) && (((count - wgAdsInterstitialsPagesBeforeFirstAd) % (wgAdsInterstitialsPagesBetweenAds+1)) == 0))){
 console.log("Int: Decided to display interstitial on next page.");
 
 			// If it's about to be time for an interstitial, re-write all INTERNAL links to go through the Interstitial.
@@ -29,6 +30,5 @@ console.log("Int: Decided NOT to display interstitial on next page.");
 console.log("Int: Settings...");
 console.log("Int: wgAdsInterstitialsPagesBeforeFirstAd: " + wgAdsInterstitialsPagesBeforeFirstAd);
 console.log("Int: wgAdsInterstitialsPagesBetweenAds:    " + wgAdsInterstitialsPagesBetweenAds);
-console.log("Int: wgAdsInterstitialsDurationInSeconds: " + wgAdsInterstitialsDurationInSeconds);
 
 }
