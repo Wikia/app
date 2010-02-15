902c6a6eb9da6e81b7686e95a1493c9d5f1f5c9c
sean <sean@wikia-inc.com>
2010-02-15 06:40:44 +0000

    Removed the Javascript logging from Interstitials.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18439 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Interstitial/Interstitial.js
+++ extensions/wikia/Interstitial/Interstitial.js
@@ -1,35 +1,22 @@
 if((typeof wgAdsInterstitialsEnabled != 'undefined') && wgAdsInterstitialsEnabled){
-console.log("Int: The Interstitials Code is loded.");
 	var COOKIE_NAME = 'IntPgCounter';
 	var count = $.cookies.get(COOKIE_NAME);
 	count = parseInt((!count)?1:count);
 	
-console.log("Int: count: " + count);
 	count += 1;
-console.log("Int: next: " + count);
 	$.cookies.set(COOKIE_NAME, count + '', {
 		path: wgCookiePath,
 		domain: wgCookieDomain
 	});
-console.log("Int: now set to: " + $.cookies.get(COOKIE_NAME) + " (should be same as 'next')");
 
 	var numToSkip = 2; // skip the interstitial and the page it was blocking as candidates
 	if((wgAdsInterstitialsPagesBeforeFirstAd == count - 1) || ((count > wgAdsInterstitialsPagesBeforeFirstAd) && (((count - wgAdsInterstitialsPagesBeforeFirstAd -1) % (wgAdsInterstitialsPagesBetweenAds + numToSkip)) == 0))){
-console.log("Int: Decided to display interstitial on next page.");
-
-			// If it's about to be time for an interstitial, re-write all INTERNAL links to go through the Interstitial.
-			$('a[href]').each(function(index, elem){
-				if($(elem).attr('hostname') == location.hostname){
-					var link = wgInterstitialPath + encodeURIComponent($(elem).attr('href'));
-//console.log("LINK: " + link);
-					$(elem).attr('href', link);
-				}
-			});
-        } else {
-console.log("Int: Decided NOT to display interstitial on next page.");
-        }
-console.log("Int: Settings...");
-console.log("Int: wgAdsInterstitialsPagesBeforeFirstAd: " + wgAdsInterstitialsPagesBeforeFirstAd);
-console.log("Int: wgAdsInterstitialsPagesBetweenAds:    " + wgAdsInterstitialsPagesBetweenAds);
-
+		// If it's about to be time for an interstitial, re-write all INTERNAL links to go through the Interstitial.
+		$('a[href]').each(function(index, elem){
+			if($(elem).attr('hostname') == location.hostname){
+				var link = wgInterstitialPath + encodeURIComponent($(elem).attr('href'));
+				$(elem).attr('href', link);
+			}
+		});
+	}
 }
