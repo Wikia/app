baae756a63c9b774ce60427ac845f39485fa9ee2
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-15 16:30:05 +0000

    rt#36195: Automate table setup for wgEnableNewsiteExt.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18462 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/WikiFactoryOnVarChange.php
+++ extensions/wikia/WikiFactory/WikiFactoryOnVarChange.php
@@ -22,8 +22,25 @@ class WikiFactoryOnVarChange {
 	static public function SetupTablesForWebsitewiki($name, $city_id, $value) {
 #trigger_error("WikiFactoryOnVarChange: (3) " . print_r(array(__METHOD__, $name, $city_id, $value), true));
 
-		if ($name == "wgEnableNewsiteExt") {
+		global $IP;
+		$dbname = WikiFactory::IDtoDB($city_id);
+
+		# liberal amout of c&p from AutoCreateWikiPage::createWiki
+		# TODO: refactor
+
+		$dbw_local = wfGetDB( DB_MASTER, array(), $dbname );
+
+		$sqlfiles = array(
+			"{$IP}/extensions/3rdparty/Websitewiki/keywords.sql",
+			"{$IP}/extensions/3rdparty/Websitewiki/ratings.sql",
+			"{$IP}/extensions/wikia/newsite/related.sql",
+		);
+
+		foreach ($sqlfiles as $file) {
+			$error = $dbw_local->sourceFile( $file );
 		}
+
+		$dbw_local->commit();
 
 		return true;
 	}
