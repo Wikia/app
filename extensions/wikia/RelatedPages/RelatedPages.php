2a808d7d2d5217d5601bb6d99710cf6979d0c48a
inez <inez@wikia-inc.com>
2010-02-16 16:21:26 +0000

    * Merge r18515:18525 from releases/201002.2
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18528 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedPages/RelatedPages.php
+++ extensions/wikia/RelatedPages/RelatedPages.php
@@ -23,7 +23,7 @@ function RelatedPages_Compare($a, $b) {
 }
 
 function RelatedPages_Display( &$template, &$templateEngine ) {
-	global $wgUser, $wgArticle, $wgTitle, $wgRequest;
+	global $wgUser, $wgArticle, $wgTitle, $wgRequest, $wgMemc;
 
 	if(!$wgUser->isAllowed('wikifactory')) {
 		return true;
@@ -45,62 +45,70 @@ function RelatedPages_Display( &$template, &$templateEngine ) {
 	if($templateEngine->data['catlinks']) {
 		$categories = Wikia::getVar('InCategories');
 
-		$dbr = wfGetDB(DB_SLAVE);
+		if($categories) {
+			sort($categories);
 
-		$out = array();
-		$results = array();
+			$cacheKey = wfMemcKey(__CLASS__, join('-', $categories));
 
-		$query1 = 'SELECT /* RelatedPages Query 1 */ cl_from, COUNT(cl_to) AS count FROM categorylinks USE KEY(cl_from) WHERE cl_to IN ("'.join('","', $categories).'") GROUP BY 1';
+			$content = $wgMemc->get($cacheKey);
 
-		if(count($categories) > 1) {
-			$query1 .= ' HAVING COUNT(cl_to) > 1';
-		} else {
-			$query1 .= ' LIMIT 100';
-		}
-
-		$res1 = $dbr->query($query1);
-		while($row1 = $dbr->fetchObject($res1)) {
-			if($row1->cl_from != $wgArticle->getID()) {
-				$results[$row1->cl_from] = $row1->count;
-			}
-		}
-
-		if(count($categories) > 1) {
-			arsort($results);
-			uasort($results, 'RelatedPages_Compare');
-			$out = array_slice(array_keys($results), 0, 5);
-		} else {
-			$out = array_rand($results, 5);
-		}
-
-		if(count($categories) > 1 && count($out) < 5) {
-			$results = array();
-			$query2 = 'SELECT /* RelatedPages Query 2 */ cl_from FROM categorylinks USE KEY(cl_from) WHERE cl_to IN ("'.join('","', $categories).'") GROUP BY 1 HAVING COUNT(cl_to) = 1 LIMIT 100';
-			$res2 = $dbr->query($query2);
-			while($row2 = $dbr->fetchObject($res2)) {
-				if($row2->cl_from != $wgArticle->getID()) {
-					if(!in_array($row2->cl_from, $out)) {
-						$results[] = $row2->cl_from;
+			if(empty($content)) {
+				$dbr = wfGetDB(DB_SLAVE);
+				$out = array();
+				$results = array();
+				$query1 = 'SELECT /* RelatedPages Query 1 */ cl_from, COUNT(cl_to) AS count FROM categorylinks USE KEY(cl_from) WHERE cl_to IN ("'.join('","', $categories).'") GROUP BY 1';
+				if(count($categories) > 1) {
+					$query1 .= ' HAVING COUNT(cl_to) > 1';
+				} else {
+					$query1 .= ' LIMIT 100';
+				}
+				$res1 = $dbr->query($query1);
+				while($row1 = $dbr->fetchObject($res1)) {
+					if($row1->cl_from != $wgArticle->getID()) {
+						$results[$row1->cl_from] = $row1->count;
 					}
 				}
-			}
-			if(!empty($results)) {
-				$out = array_merge($out, array_rand($results, 5 - count($out)));
-			}
-		}
-
-		if(count($out) > 0) {
-			$templateEngine->data['bodytext'] .= '<style>.RelatedPages li { font-weight: bold; float: left; background: transparent url("http://images.wikia.com/common/skins/common/bullet.gif") no-repeat 0px 50%; padding-left: 21px; margin-right: 16px; }</style>';
-			$templateEngine->data['bodytext'] .= '<div style="clear:both;"></div><div class="widget" style="margin-top: 10px;"><div class="accent" style="padding: 6px; font-weight: bold;">Check out these related pages:</div><div style="padding: 10px;"><ul class="reset clearfix RelatedPages" style="margin: 0">';
-			$i = 0;
-			foreach($out as $item) {
-				$title = Title::newFromId($item);
-				if(!empty($title) && $title->exists()) {
-					$templateEngine->data['bodytext'] .= '<li'.($i == 0 ? ' style="background: none; padding-left: 0;"' : '').'><a href="'.htmlspecialchars($title->getFullUrl()).'">'.htmlspecialchars($title->getPrefixedText()).'</a></li>';
-					$i++;
+				if(count($categories) > 1) {
+					arsort($results);
+					uasort($results, 'RelatedPages_Compare');
+					$out = array_slice(array_keys($results), 0, 5);
+				} else {
+					$out = array_rand($results, 5);
 				}
+				if(count($categories) > 1 && count($out) < 5) {
+					$results = array();
+					$query2 = 'SELECT /* RelatedPages Query 2 */ cl_from FROM categorylinks USE KEY(cl_from) WHERE cl_to IN ("'.join('","', $categories).'") GROUP BY 1 LIMIT 100';
+					$res2 = $dbr->query($query2);
+					while($row2 = $dbr->fetchObject($res2)) {
+						if($row2->cl_from != $wgArticle->getID()) {
+							if(!in_array($row2->cl_from, $out)) {
+								$results[] = $row2->cl_from;
+							}
+						}
+					}
+					if(!empty($results)) {
+						$out = array_merge($out, array_rand(array_flip($results), 5 - count($out)));
+					}
+				}
+				if(count($out) > 0) {
+					$content = '<style>.RelatedPages li { font-weight: bold; float: left; background: transparent url("http://images.wikia.com/common/skins/common/bullet.gif") no-repeat 0px 50%; padding-left: 21px; margin-right: 16px; }</style>';
+					$content .= '<div style="clear:both;"></div><div id="RelatedPages" class="widget" style="margin-top: 10px;"><div class="accent" style="padding: 6px; font-weight: bold;">Check out these related pages:</div><div style="padding: 10px;"><ul class="reset clearfix RelatedPages" style="margin: 0">';
+					$i = 0;
+					foreach($out as $item) {
+						$title = Title::newFromId($item);
+						if(!empty($title) && $title->exists()) {
+							$content .= '<li'.($i == 0 ? ' style="background: none; padding-left: 0;"' : '').'><a href="'.htmlspecialchars($title->getFullUrl()).'">'.htmlspecialchars($title->getPrefixedText()).'</a></li>';
+							$i++;
+						}
+					}
+					$content .= '</ul></div></div>';
+				} else {
+					$content = '<!-- no related pages -->';
+				}
+
+				$wgMemc->set($cacheKey, $content, 60 * 60 * 6);
 			}
-			$templateEngine->data['bodytext'] .= '</ul></div></div>';
+			$templateEngine->data['bodytext'] .= $content;
 		}
 	}
 
