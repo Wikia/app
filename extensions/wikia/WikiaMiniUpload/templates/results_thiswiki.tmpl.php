631cd82ad55e89ef84e7ba6dc8687faa4b693e23
siebrand <siebrand@wikia-inc.com>
2010-02-22 00:06:02 +0000

    * Add plural support for 'wmu-flickr2' and 'wmu-thiswiki2' * Remove Title Case usage
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18766 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMiniUpload/templates/results_thiswiki.tmpl.php
+++ extensions/wikia/WikiaMiniUpload/templates/results_thiswiki.tmpl.php
@@ -18,7 +18,7 @@ if($results['page'] < $results['pages']) {
 }
 ?>
 </div>
-<?= wfMsg('wmu-thiswiki2', $results['total']) ?>
+<?= wfMsgExt( 'wmu-thiswiki2', 'parsemag', $results['total'] ) ?>
 </div>
 
 <table cellspacing="0" id="ImageUploadFindTable">
