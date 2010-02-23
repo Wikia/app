4103bb9c419aa18f0bb32cccc1378803884072de
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-02-23 18:32:52 +0000

    rt#36740: Interwiki map script per wiki, not global.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18913 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/updateCentralInterwiki.php
+++ maintenance/wikia/updateCentralInterwiki.php
@@ -9,20 +9,19 @@
 
 $oldCwd = getcwd();
 
-$optionsWithArgs = array( "o", "target" );
+$optionsWithArgs = array( "o" );
 
 include_once( "../commandLine.inc" );
 include_once( "updateCentralInterwiki.inc" );
 
 if ( isset( $options['help'] ) || isset( $options['h']) ) {
 die( "Produces an SQL file from Central\'s Interwiki_map article.\n
-		  Usage: php updateCentralInterwiki.php [--o='file'|--target='id'|--global] [--verbose] [--force]
+		  Usage: php updateCentralInterwiki.php [--o='file'|--p] [--verbose] [--force]
 
 		  Prints to stdout by default.
 
 		  --o        print to output file
-		  --target   update interwikis for specified wiki (ID)
-		  --global   do a global interwiki update for all wikis in city_list
+		  --p        print to stdout
 		  --force    proceed, even if there were no changes to interwiki_map since last update
 		  --verbose  print out information on each operation\n\n");
 }
@@ -30,7 +29,6 @@ die( "Produces an SQL file from Central\'s Interwiki_map article.\n
 chdir( $oldCwd );
 
 $verbose = (bool) (isset( $options['verbose'] ) || isset( $options['v'] ));
-$global = (bool) (isset( $options ['global'] ) || isset( $options ['g'] ));
 $force = (bool) (isset( $options ['force'] ) || isset( $options ['f'] ));
 
 $sql = getRebuildInterwikiSQL();
@@ -40,39 +38,29 @@ if ( isset( $options['o'] ) ) {
 	$file = fopen( $options['o'], "w" );
 	fwrite( $file, $sql );
 	fclose( $file );
-} elseif ( $global || !empty( $options ['target'] ) ) {
+} elseif ( isset( $options ['p'] ) ) {
+	# Output to stdout
+	print $sql;
+} else {
 	# Update interwiki tables in all wikias
 
-	$dbr = wfGetDB( DB_SLAVE );
-	$dbr->selectDB('wikicities');
+	$dbr = wfGetDB( DB_SLAVE, array(), 'wikicities');
 	$lastmod = $dbr->selectField('page', 'page_latest', "page_title = 'Interwiki_map' && page_namespace = 0");
 
-		if (!empty($options['target'])) {
-			$res = $dbr->select('city_list', array('city_id', 'city_dbname'), "city_id = ". $options['target']);
-		} else {
-			$res = $dbr->select('city_list', array('city_id', 'city_dbname'));
-		}
-		while ( $row = $dbr->fetchRow( $res ) ) {
 	# Check if update is needed
-	$lastupdate = WikiFactory::getVarValueByName('wgLastInterwikiUpdate', $row['city_id']);
+	$lastupdate = $wgLastInterwikiUpdate;
 	if ( $lastupdate !== $lastmod || $force ) {
 			wfWaitForSlaves( 100 );
-			$dbw = wfGetDB( DB_MASTER, array(), $row['city_dbname'] );
+			$dbw = wfGetDB( DB_MASTER );
 			if ( $dbw != false ) {
 				if ( $verbose )
-					print "Updating database: ". $row['city_dbname']. "...\n";
+					print "Updating database...\n";
 				$dbw->doQuery($sql);
 			}
 		# Update $wgLastInterwikiUpdate
-		WikiFactory::SetVarByName('wgLastInterwikiUpdate', $row['city_id'], $lastmod);
+		WikiFactory::SetVarByName('wgLastInterwikiUpdate', $wgCityId, $lastmod);
 			if (isset( $verbose ))
 				print "Done.\n";
 	}
-		}
 
-		$dbr->freeResult( $res );
-
-} else {
-	# Output to stdout
-	print $sql;
 }
