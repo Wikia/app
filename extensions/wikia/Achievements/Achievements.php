7a50148ef2de8f7edc0eeda1debe768c46c83676
inez <inez@wikia-inc.com>
2010-02-24 11:58:07 +0000

    * Changes to Achievements extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@18994 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Achievements/Achievements.php
+++ extensions/wikia/Achievements/Achievements.php
@@ -4,7 +4,6 @@ define('ACHIEVEMENT_NEW_ARTICLE', 2);
 define('ACHIEVEMENT_IMAGE_ADDED_TO_ARTICLE', 3);
 define('ACHIEVEMENT_VIDEO_ADDED_TO_ARTICLE', 4);
 define('ACHIEVEMENT_CATEGORY_ADDED_TO_ARTICLE', 5);
-
 define('ACHIEVEMENT_EDIT_MY_USERPAGE', 6);
 define('ACHIEVEMENT_EDIT_SOMEONE_USERPAGE', 7);
 define('ACHIEVEMENT_EDIT_FOR_5_DAYS', 8);
@@ -19,43 +18,43 @@ $wgExtensionFunctions[] = 'Achievements_Setup';
  * @author: Inez Korczyński
  */
 function Achievements_Setup() {
-  global $wgHooks, $achievementTypes;
-  $wgHooks['ArticleSaveComplete'][] = 'Achievements_ArticleSaveComplete';
-  $wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'Achievements_Display';
+	global $wgHooks, $achievementTypes;
+	$wgHooks['ArticleSaveComplete'][] = 'Achievements_ArticleSaveComplete';
+	$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'Achievements_Display';
 
-  $achievementTypes = array();
+	$achievementTypes = array();
 
-  // be very careful with this array editing
-  $achievementTypes[ACHIEVEMENT_EDIT_ARTICLE] = array(
-  	'name' => 'edits',
-  	'type' => 'repeat',
-  	'levels' => array(1, 5, 10, 25, 50, 100, 200)
-  );
-  $achievementTypes[ACHIEVEMENT_IMAGE_ADDED_TO_ARTICLE] = array(
-  	'name' => 'pictures',
-  	'type' => 'repeat',
-  	'levels' => array(1, 5, 10, 20, 40)
-  );
-  $achievementTypes[ACHIEVEMENT_NEW_ARTICLE] = array(
-  	'name' => 'newpage',
-  	'type' => 'repeat',
-  	'levels' => array(1, 5, 10, 20, 40)
-  );
-  $achievementTypes[ACHIEVEMENT_CATEGORY_ADDED_TO_ARTICLE] = array(
-  	'name' => 'category',
-  	'type' => 'repeat',
-  	'levels' => array(1, 5, 10, 20, 40)
-  );
-  $achievementTypes[ACHIEVEMENT_VIDEO_ADDED_TO_ARTICLE] = array(
-  	'name' => 'videos',
-  	'type' => 'repeat',
-  	'levels' => array(1, 5, 10, 20, 40)
-  );
+	// be very careful with this array editing
+	$achievementTypes[ACHIEVEMENT_EDIT_ARTICLE] = array(
+		'name' => 'edits',
+		'type' => 'repeat',
+		'levels' => array(1, 5, 10, 25, 50, 100, 200)
+	);
+	$achievementTypes[ACHIEVEMENT_IMAGE_ADDED_TO_ARTICLE] = array(
+		'name' => 'pictures',
+		'type' => 'repeat',
+		'levels' => array(1, 5, 10, 20, 40)
+	);
+	$achievementTypes[ACHIEVEMENT_NEW_ARTICLE] = array(
+		'name' => 'newpage',
+		'type' => 'repeat',
+		'levels' => array(1, 5, 10, 20, 40)
+	);
+	$achievementTypes[ACHIEVEMENT_CATEGORY_ADDED_TO_ARTICLE] = array(
+		'name' => 'category',
+		'type' => 'repeat',
+		'levels' => array(1, 5, 10, 20, 40)
+	);
+	$achievementTypes[ACHIEVEMENT_VIDEO_ADDED_TO_ARTICLE] = array(
+		'name' => 'videos',
+		'type' => 'repeat',
+		'levels' => array(1, 5, 10, 20, 40)
+	);
 
-  $achievementTypes[ACHIEVEMENT_EDIT_MY_USERPAGE] = array('name' => 'userpage', 'type' => 'onetime');
-  $achievementTypes[ACHIEVEMENT_EDIT_SOMEONE_USERPAGE] = array('name' => 'makingfriends', 'type' => 'onetime');
-  $achievementTypes[ACHIEVEMENT_EDIT_10_ARTICLES] = array('name' => 'busyday', 'type' => 'onetime');
-  $achievementTypes[ACHIEVEMENT_EDIT_FOR_5_DAYS] = array('name' => 'keytothewiki', 'type' => 'onetime');
+	$achievementTypes[ACHIEVEMENT_EDIT_MY_USERPAGE] = array('name' => 'userpage', 'type' => 'onetime');
+	$achievementTypes[ACHIEVEMENT_EDIT_SOMEONE_USERPAGE] = array('name' => 'makingfriends', 'type' => 'onetime');
+	$achievementTypes[ACHIEVEMENT_EDIT_10_ARTICLES] = array('name' => 'busyday', 'type' => 'onetime');
+	$achievementTypes[ACHIEVEMENT_EDIT_FOR_5_DAYS] = array('name' => 'keytothewiki', 'type' => 'onetime');
 }
 
 /**
@@ -88,21 +87,32 @@ function Achievements_Display(&$template, &$templateEngine) {
 			$src = $wgExtensionsPath . '/wikia/Achievements/images/'.$achievement['name'].'/'.$achievement['name'].'-bw.jpg';
 			$text .= wfMsg('achievement-'.$achievement['name'].'-info');
 		} else {
-			$src = $wgExtensionsPath . '/wikia/Achievements/images/'.$achievement['name'].'/'.$achievement['name'].($userBadges[$achievementTypeId] > 4 ? 'x' : $userBadges[$achievementTypeId] + 1).'.jpg';
-			$text .= '<strong>' . wfMsg('achievement-level', $userBadges[$achievementTypeId] + 1) . '</strong>';
-			$text .= wfMsg('achievement-'.$achievement['name'].'-summary', $userCounters[$achievementTypeId]);
 
-			if(isset($achievement['levels'][$userBadges[$achievementTypeId] + 1])) {
-				$next = $achievement['levels'][$userBadges[$achievementTypeId] + 1];
+			if($achievementTypes[$achievementTypeId]['type'] == 'repeat') {
+
+				$src = $wgExtensionsPath . '/wikia/Achievements/images/'.$achievement['name'].'/'.$achievement['name'].($userBadges[$achievementTypeId] > 4 ? 'x' : $userBadges[$achievementTypeId] + 1).'.jpg';
+
+				$text .= '<strong>' . wfMsg('achievement-level', $userBadges[$achievementTypeId] + 1) . '</strong>';
+				$text .= wfMsg('achievement-'.$achievement['name'].'-summary', $userCounters[$achievementTypeId]);
+
+				if(isset($achievement['levels'][$userBadges[$achievementTypeId] + 1])) {
+					$next = $achievement['levels'][$userBadges[$achievementTypeId] + 1];
+				} else {
+					$valuesNo = count($achievement['levels']);
+					$maxValue = $achievement['levels'][$valuesNo-1];
+					$secondMaxValue = $achievement['levels'][$valuesNo-2];
+					$diff = $maxValue - $secondMaxValue;
+					$next = $diff * ($userBadges[$achievementTypeId] + 2);
+				}
+
+				$text .= '<em>'.wfMsg('achievement-'.$achievement['name'].'-next', $next).'</em>';
+
 			} else {
-				$valuesNo = count($achievement['levels']);
-				$maxValue = $achievement['levels'][$valuesNo-1];
-				$secondMaxValue = $achievement['levels'][$valuesNo-2];
-				$diff = $maxValue - $secondMaxValue;
-				$next = $diff * ($userBadges[$achievementTypeId] + 2);
-			}
 
-			$text .= '<em>'.wfMsg('achievement-'.$achievement['name'].'-next', $next).'</em>';
+				$src = $wgExtensionsPath . '/wikia/Achievements/images/'.$achievement['name'].'/'.$achievement['name'].'.jpg';
+				$text .= wfMsg('achievement-'.$achievement['name'].'-summary');
+
+			}
 		}
 
 		$badgesDisplay .= <<<EOT
@@ -113,7 +123,7 @@ function Achievements_Display(&$template, &$templateEngine) {
 EOT;
 	}
 
-	$dbr = wfGetDB(DB_MASTER);
+	$dbr = wfGetDB(DB_SLAVE);
 	$noofbadges = $dbr->selectField('achievements_badges', 'count(*) as cnt', array('user_id' => $user->getID()), 'Achievements_Display');
 	$username = $user->getName();
 
@@ -203,136 +213,175 @@ function Achievements_GetUserTopBadges($userId) {
  * @author: Inez Korczyński
  */
 function Achievements_ArticleSaveComplete(&$article, &$user, $text, $summary, &$minoredit, $watchthis, $sectionanchor, &$flags, $revision, &$status, $baseRevId ) {
-  if(!$user->isLoggedIn()) {
-    return true;
-  }
+	global $wgContentNamespaces;
 
-  // determine for which achievement counters should be increased and how much
+	if(!$user->isLoggedIn()) {
+		return true;
+	}
 
-  $achievementCountersToIncrease = array();
+	if(!($status instanceof Status) || $status->ok != true || count($status->errors) != 0) {
+		return true;
+	}
 
-  if($status instanceof Status) {
-    if($status->ok == true && count($status->errors) == 0) {
+	if(!$article) {
+		return true;
+	}
 
-      // new article && edit article
-      if($status->value['new'] == true) {
-        $achievementCountersToIncrease[ACHIEVEMENT_NEW_ARTICLE] = 1;
-      } else {
-        $achievementCountersToIncrease[ACHIEVEMENT_EDIT_ARTICLE] = 1;
-      }
+	$achievementCountersToIncrease = array();
 
-      // images inserts && video inserts
-      $mediaInserts = Wikia::getVar('imageInserts');
-      if(!empty($mediaInserts) && is_array($mediaInserts)) {
-        foreach($mediaInserts as $insert) {
-          if($insert['il_to']{0} == ':') {
-            $type = ACHIEVEMENT_VIDEO_ADDED_TO_ARTICLE;
-          } else {
-            $type = ACHIEVEMENT_IMAGE_ADDED_TO_ARTICLE;
-          }
+	$title = $article->getTitle();
 
-          if(!isset($achievementCountersToIncrease[$type])) {
-            $achievementCountersToIncrease[$type] = 0;
-          }
-          $achievementCountersToIncrease[$type]++;
-        }
-      }
+	$userBadges = Achievements_GetUserTopBadges($user->getID());
 
-    }
-  }
+	if(!isset($userBadges[ACHIEVEMENT_EDIT_MY_USERPAGE])) {
+		if($title->getNamespace() == NS_USER && $title->getText() == $user->getName()) {
+			$achievementCountersToIncrease[ACHIEVEMENT_EDIT_MY_USERPAGE] = 1;
+		}
+	}
 
-  // if there are any achievement counters to increase then let's do that and if needed add badges
+	if(!isset($userBadges[ACHIEVEMENT_EDIT_SOMEONE_USERPAGE])) {
+		if($title->getNamespace() == NS_USER_TALK && $title->getText() != $user->getName()) {
+			$achievementCountersToIncrease[ACHIEVEMENT_EDIT_SOMEONE_USERPAGE] = 1;
+		}
+	}
 
-  if(count($achievementCountersToIncrease) > 0) {
+	if(in_array($title->getNamespace(), $wgContentNamespaces)) {
 
-    $currentCounters = Achievements_GetCurrentCounters($user->getId(), array_keys($achievementCountersToIncrease));
+		if(!isset($userBadges[ACHIEVEMENT_EDIT_10_ARTICLES])) {
+			$dbr = wfGetDB(DB_SLAVE);
+			$res = $dbr->query('SELECT count(*) as cnt FROM (SELECT rc_user FROM recentchanges WHERE rc_user_text = '.$dbr->addQuotes($user->getName()).' AND rc_timestamp >= date_sub(now(), interval 24 hour) AND rc_namespace IN ('.join(',', $wgContentNamespaces).') LIMIT 10) AS c;');
+			if($res->fetchObject()->cnt == 10) {
+				$achievementCountersToIncrease[ACHIEVEMENT_EDIT_10_ARTICLES] = 1;
+			}
+		}
 
-    $newCounters = array();
+		if(!isset($userBadges[ACHIEVEMENT_EDIT_FOR_5_DAYS])) {
+		}
 
-    foreach($achievementCountersToIncrease as $achievementTypeId => $counter) {
-      $newCounters[$achievementTypeId] = $currentCounters[$achievementTypeId] + $counter;
-    }
+		// article edit and article creation
+		if($status->value['new'] == true) {
+			$achievementCountersToIncrease[ACHIEVEMENT_NEW_ARTICLE] = 1;
+		} else {
+			$achievementCountersToIncrease[ACHIEVEMENT_EDIT_ARTICLE] = 1;
+		}
 
-    Achievements_UpdateCounters($user->getId(), $newCounters);
+		// adding video or image to an article
+		$mediaInserts = Wikia::getVar('imageInserts');
+		if(!empty($mediaInserts) && is_array($mediaInserts)) {
+			foreach($mediaInserts as $insert) {
+				if($insert['il_to']{0} == ':') {
+					$type = ACHIEVEMENT_VIDEO_ADDED_TO_ARTICLE;
+				} else {
+					$type = ACHIEVEMENT_IMAGE_ADDED_TO_ARTICLE;
+				}
 
-    Achievements_AddBadgesIfNeeded($user->getId(), $newCounters);
+				if(!isset($achievementCountersToIncrease[$type])) {
+					$achievementCountersToIncrease[$type] = 0;
+				}
 
-  }
+				$achievementCountersToIncrease[$type]++;
+			}
+		}
 
-  return true;
+		// adding category to an article
+		$categoryInserts = Wikia::getVar('categoryInserts');
+		if(!empty($categoryInserts) && is_array($categoryInserts)) {
+			foreach($categoryInserts as $insert) {
+				if(!isset($achievementCountersToIncrease[ACHIEVEMENT_CATEGORY_ADDED_TO_ARTICLE])) {
+					$achievementCountersToIncrease[ACHIEVEMENT_CATEGORY_ADDED_TO_ARTICLE] = 0;
+				}
+				$achievementCountersToIncrease[ACHIEVEMENT_CATEGORY_ADDED_TO_ARTICLE]++;
+			}
+		}
+
+	}
+
+	// if there are any achievement counters to increase then let's do that and if needed add badges
+	if(count($achievementCountersToIncrease) > 0) {
+		$currentCounters = Achievements_GetCurrentCounters($user->getId(), array_keys($achievementCountersToIncrease));
+		$newCounters = array();
+
+		foreach($achievementCountersToIncrease as $achievementTypeId => $counter) {
+			$newCounters[$achievementTypeId] = $currentCounters[$achievementTypeId] + $counter;
+		}
+
+		Achievements_UpdateCounters($user->getId(), $newCounters);
+		Achievements_AddBadgesIfNeeded($user->getId(), $newCounters);
+	}
+
+	return true;
 }
 
 /**
  * @author: Inez Korczyński
  */
 function Achievements_AddBadgesIfNeeded($userId, $counters /* achievementTypeId => counter */) {
-  global $achievementTypes;
+	global $achievementTypes;
 
-  $rows = array();
+	$rows = array();
 
-  foreach($counters as $achievementTypeId => $counter) {
-    $level = array_search($counter, $achievementTypes[$achievementTypeId]['levels']);
+	foreach($counters as $achievementTypeId => $counter) {
+		$level = array_search($counter, $achievementTypes[$achievementTypeId]['levels']);
 
-    if($level === false) {
-      $valuesNo = count($achievementTypes[$achievementTypeId]['levels']);
-      $maxValue = $achievementTypes[$achievementTypeId]['levels'][$valuesNo-1];
-      $secondMaxValue = $achievementTypes[$achievementTypeId]['levels'][$valuesNo-2];
-      if($counter > $maxValue) {
-        $diff = $maxValue - $secondMaxValue;
-        if($counter % $diff == 0) {
-          $level = ($counter - $maxValue) /  $diff + $valuesNo - 1;
-        }
-      }
-    }
+		if($level === false) {
+			$valuesNo = count($achievementTypes[$achievementTypeId]['levels']);
+			$maxValue = $achievementTypes[$achievementTypeId]['levels'][$valuesNo-1];
+			$secondMaxValue = $achievementTypes[$achievementTypeId]['levels'][$valuesNo-2];
+			if($counter > $maxValue) {
+				$diff = $maxValue - $secondMaxValue;
+				if($counter % $diff == 0) {
+					$level = ($counter - $maxValue) /  $diff + $valuesNo - 1;
+				}
+			}
+		}
 
-    if($level !== false) {
-	    $rows[] = array('user_id' => $userId,
-			            'achievement_type_id' => $achievementTypeId,
-			            'level' => $level);
-    }
-  }
+		if($level !== false) {
+			$rows[] = array('user_id' => $userId,
+							'achievement_type_id' => $achievementTypeId,
+							'level' => $level);
+		}
+	}
 
-  $dbw = wfGetDB(DB_MASTER);
-  $o = $dbw->insert('achievements_badges', $rows, 'Achievements_AddBadgesIfNeeded');
+	$dbw = wfGetDB(DB_MASTER);
+	$o = $dbw->insert('achievements_badges', $rows, 'Achievements_AddBadgesIfNeeded');
 }
 
 /**
  * @author: Inez Korczyński
  */
 function Achievements_GetCurrentCounters($userId, $achievementTypeIds) {
-  $dbw = wfGetDB(DB_MASTER);
+	$dbw = wfGetDB(DB_MASTER);
 
-  // this query is intentionally perform at master database because it uses SELECT .. FOR UPDATE,
-  // which sets the locks on selected rows for later update
-  $res = $dbw->select('achievements_counters',
-			            array('achievement_type_id', 'counter'),
-			            array('user_id' => $userId, 'achievement_type_id IN ('.join(',', $achievementTypeIds).')'),
-			            'Achievements_GetCounters',
-			            array('FOR UPDATE'));
+	// this query is intentionally perform at master database because it uses SELECT .. FOR UPDATE,
+	// which sets the locks on selected rows for later update
+	$res = $dbw->select('achievements_counters',
+						array('achievement_type_id', 'counter'),
+						array('user_id' => $userId, 'achievement_type_id IN ('.join(',', $achievementTypeIds).')'),
+						'Achievements_GetCounters',
+						array('FOR UPDATE'));
 
-  $counters = array_fill_keys($achievementTypeIds, 0);
+	$counters = array_fill_keys($achievementTypeIds, 0);
 
-  while($row = $dbw->fetchObject($res)) {
-    $counters[$row->achievement_type_id] = $row->counter;
-  }
+	while($row = $dbw->fetchObject($res)) {
+		$counters[$row->achievement_type_id] = $row->counter;
+	}
 
-  return $counters;
+	return $counters;
 }
 
 /**
  * @author: Inez Korczyński
  */
 function Achievements_UpdateCounters($userId, $newCounters /* achievementTypeId => counter */) {
+	$rows = array();
 
-  $rows = array();
+	// prepare query data for sql replace statement
+	foreach($newCounters as $achievementTypeId => $counter) {
+		$rows[] = array('user_id' => $userId,
+						'achievement_type_id' => $achievementTypeId,
+						'counter' => $counter);
+	}
 
-  // prepare query data for sql replace statement
-  foreach($newCounters as $achievementTypeId => $counter) {
-    $rows[] = array('user_id' => $userId,
-		            'achievement_type_id' => $achievementTypeId,
-		            'counter' => $counter);
-  }
-
-  $dbw = wfGetDB(DB_MASTER);
-  $dbw->replace('achievements_counters', null, $rows, 'Achievements_UpdateCounters');
+	$dbw = wfGetDB(DB_MASTER);
+	$dbw->replace('achievements_counters', null, $rows, 'Achievements_UpdateCounters');
 }
