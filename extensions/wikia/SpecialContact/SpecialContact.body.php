30808889f2093b84f564ca5b32cdb60d4c2e26af
uberfuzzy <uberfuzzy@wikia-inc.com>
2010-03-01 09:50:58 +0000

    lockdown fixes for #41549. rewrite error handling to detect and prevent blank emails. rewrite message building to adapt to missing data (subject/realname/username). no longer send cc message if sending primary fails. contactintro is now wiki parsed. shifted form messages around to make clearer (and for prep for future changes)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19293 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialContact/SpecialContact.body.php
+++ extensions/wikia/SpecialContact/SpecialContact.body.php
@@ -8,6 +8,7 @@ class ContactForm extends SpecialPage {
 	var $mName, $mPassword, $mRetype, $mReturnto, $mCookieCheck, $mPosted;
 	var $mAction, $mCreateaccount, $mCreateaccountMail, $mMailmypassword;
 	var $mLoginattempt, $mRemember, $mEmail, $mBrowser;
+	var $err;
 
 	function  __construct() {
 		parent::__construct( "Contact" , '' /*restriction*/);
@@ -29,17 +30,25 @@ class ContactForm extends SpecialPage {
 		$this->mBrowser = $wgRequest->getText( 'wpBrowser' );
 		$this->mCCme = $wgRequest->getCheck( 'wgCC' );
 
-		if( $this->mPosted ) {
-			if ('submit' == $this->mAction ) {
-				if (!User::isValidEmailAddr($this->mEmail)) {
-					$this->mainContactForm(wfMsg('contactpage-email-failed'));
-					return;
-				} else {
-					return $this->processCreation();
-				}
+		if( $this->mPosted && ('submit' == $this->mAction ) ) {
+			if (!User::isValidEmailAddr($this->mEmail)) {
+				$this->err .= "\n" . wfMsg('contactpage-email-failed');
+			}
+		
+			//no message text?
+			if( empty($this->err) && empty($this->mProblemDesc) ) {
+				$this->err .= "\n" . wfMsg('contactnomessage');
+			}
+
+			//no errors?
+			if( empty($this->err) )
+			{
+				return $this->processCreation();
 			}
 		}
-		$this->mainContactForm( '' );
+		
+		$this->mainContactForm();
+		return htmlspecialchars($errors);
 	}
 	/**
 	 * @access private
@@ -55,40 +64,53 @@ class ContactForm extends SpecialPage {
 		$wgOut->addHTML(wfMsg( 'contactsubmitcomplete' ));
 		$wgOut->addHTML('<br/>' . wfMsgExt( 'returnto', 'parse', "[[" . $mp->getText() . "]]" ));
 
+		//build common top of both emails
+		$m_shared = '';
+		$m_shared .= ( !empty($this->mRealName) ) ? $this->mRealName : $this->mName;
+		$m_shared .= " ({$this->mEmail})";
+		$m_shared .= " {$this->mWhichWiki}";
+		$m_shared .= ( !empty($this->mName) ) ? "/wiki/User:" . urlencode(str_replace(" ", "_", $this->mName)) : '';
+		$m_shared .= " contacted Wikia";
+		$m_shared .= ( !empty($this->mProblem) ) ? " about {$this->mProblem}" : '';
+		$m_shared .= ".\n";
+
+		//wikia debug info
+		$info = array();
+		$info[] = '' . $this->mBrowser;
+		$info[] = 'wkID:' . $wgCityId;
+		
 		$uid = $wgUser->getID();
 		if( !empty($uid) ) {
-			$uid = 'UID: ' . $uid . '; ';
-		} else {
-			$uid = '';
+			$info[] = 'uID:' . $uid;
 		}
-		$m = "$this->mRealName";
-		$m .= " ({$this->mEmail}) {$this->mWhichWiki}/wiki/User:" . str_replace(" ", "_", $this->mName) . " contacted Wikia about ";
-		$m .= "$this->mProblem.\n";
-		$m .= "User browser data: {$this->mBrowser}; wkID: {$wgCityId}; {$uid}IP: " . wfGetIP() . "\n\n";
-		$m .= "$this->mProblemDesc\n";
+		$info[] = 'IP:' . wfGetIP();
+		$info = implode("; ", $info) . "\n";
+		//end wikia debug data
+		
+		$m = $m_shared . $info . "\n{$this->mProblemDesc}\n";
 		
 		if($this->mCCme) {
 			$mcc = wfMsg('contactccheader') . "\n\n";
-			$mcc .= "$this->mRealName";
-			$mcc .= " ({$this->mEmail}) {$this->mWhichWiki}/wiki/User:" . str_replace(" ", "_", $this->mName) . " contacted Wikia about ";
-			$mcc .= "$this->mProblem.\n\n";
-			$mcc .= "$this->mProblemDesc\n";
+			$mcc .= $m_shared . "\n{$this->mProblemDesc}\n";
 		}
 		
 	#	exec("/bin/echo '$m' >> /home/wikicities/contactmails.log");
 
-		$from = $this->mEmail;
-		#if (class_exists("MailAddress"))
-		$from = new MailAddress($from);
+		$mail_user = new MailAddress($this->mEmail);
+		$mail_community = new MailAddress("community@wikia.com");
 
 		$errors = '';
-		$error = UserMailer::send( new MailAddress("community@wikia.com"), $from, wfMsg('contactmailsub') . ' - ' . $this->mProblem, $m, $from, null, 'SpecialContact' );
+
+		//to us, from user
+		$subject = wfMsg('contactmailsub') . (( !empty($this->mProblem) )? ' - ' . $this->mProblem : '');
+		$error = UserMailer::send( $mail_community, $mail_user, $subject, $m, $mail_user, null, 'SpecialContact' );
 		if (WikiError::isError($error)) {
 			$errors .= "\n" . $error->getMessage();
 		}
 
-		if( $this->mCCme && $wgUser->getEmailAuthenticationTimestamp() != null ) {
-			$error = UserMailer::send( $from, new MailAddress("community@wikia.com"), wfMsg('contactmailsub') . ' - ' . $this->mProblem, $mcc, $from, null, 'SpecialContactCC' );
+		//to user, from us
+		if( empty($errors) && $this->mCCme && $wgUser->getEmailAuthenticationTimestamp() != null ) {
+			$error = UserMailer::send( $mail_user, $mail_community, wfMsg('contactmailsubcc'), $mcc, $mail_user, null, 'SpecialContactCC' );
 			if (WikiError::isError($error)) {
 				$errors .= "\n" . $error->getMessage();
 			}
@@ -100,19 +122,10 @@ class ContactForm extends SpecialPage {
 	/**
 	 * @access private
 	 */
-	function mainContactForm( $err ) {
+	function mainContactForm( ) {
 		global $wgUser, $wgOut, $wgLang;
 		global $wgDBname, $wgAllowRealName;
 		global $wgServer, $wgSitename;
-
-		$yn = wfMsg( 'contactusername' );
-		$intro = wfMsg( 'contactintro' );
-		$cwtt = wfMsg( 'contactproblem' );
-		$cwn = wfMsg( 'contactproblemdesc' );
-		$nvt = wfMsg( 'contactrealname' );
-		$wwn = wfMsg( 'contactwikiname' );
-		$rcw = wfMsg( 'contactmail' );
-		$ye = wfMsg( 'contactyourmail' );
 
 		if ( '' == $this->mName ) {
 			if ( 0 != $wgUser->getID() ) {
@@ -124,23 +137,26 @@ class ContactForm extends SpecialPage {
 
 		if( $wgUser->isAnon() == false ) {
 			//user mode
-			
-			if ( $this->mRealName == '') {
-				$this->mRealName = $wgUser->getRealName();
-			}
 
-			if ( $this->mEmail == '' ) {
-				$this->mEmail = $wgUser->getEmail();
-			}
+			//we have user data, so use it, overriding any passed in from url
+			$this->mRealName = $wgUser->getRealName();
+			$this->mEmail = $wgUser->getEmail();
 
 			$user_readonly = 'readonly="readonly" ';
-			$user_autofill_marker = '<span class="autofilled" style="color:blue; cursor: help;" title="'. wfMsg('contactfilledin') .'">+</span>';
+			$autofill_marker = '<span class="autofilled" style="color:blue; cursor: help;" title="'. wfMsg('contactfilledin') .'">+</span>';
 		}
 		else
 		{
 			//anon mode
 			$user_readonly = ''; //dont lock
-			$user_autofill_marker = ''; //no lock, no marker
+			$autofill_marker = ''; //no lock, no marker
+		}
+		
+		$name_readonly = $mail_readonly = $user_readonly;  //mirror
+		
+		if( empty($this->mEmail) ) {
+			//user has blank email, so dont lock it
+			$mail_readonly = '';
 		}
 
 		$wgOut->setPageTitle( wfMsg( 'contactpagetitle' ) );
@@ -158,12 +174,13 @@ class ContactForm extends SpecialPage {
 		$encProblem = htmlspecialchars( $this->mProblem );
 		$encProblemDesc = htmlspecialchars( $this->mProblemDesc );
 
-		if ($err != '') {
-			$wgOut->addHTML("$err<br/><br/>");
+		#$this->err = 'foo';
+		if ( !empty($this->err) ) {
+			$wgOut->addHTML("<div class='errorbox' style='float:none;'>" . $this->err . "</div>\n");
 		}
 
 		// add intro text
-		$wgOut->addHTML( $intro );
+		$wgOut->addHTML( wfMsgExt( 'contactintro', array('parse', 'content' )) );
 		
 		$ti = 1;
 		//setup form and javascript
@@ -175,9 +192,9 @@ class ContactForm extends SpecialPage {
 				info = 'Browser: ' + YAHOO.Tools.getBrowserEngine().ua;
 				//flash
 				flashVer = parseInt(YAHOO.Tools.checkFlash()) ? YAHOO.Tools.checkFlash() : 'none';
-				info += ' Flash: ' + flashVer;
+				info += '; Flash: ' + flashVer;
 				//skin and theme
-				info += ' Skin: ' + skin;
+				info += '; Skin: ' + skin;
 				if (typeof themename != 'undefined') {
 					info += '-' + themename;
 				}
@@ -190,7 +207,7 @@ class ContactForm extends SpecialPage {
 			$wgOut->addHTML( "
 			<table border='0'>
 				<tr>
-					<td align='right'>$wwn:</td>
+					<td align='right'>" . wfMsg( 'contactwikiname' ) . "</td>
 					<td align='left'>
 						<input tabindex='" . ($ti++) . "' type='text' name=\"wpContactWikiName\" value=\"{$wgServer}\" size='40' />
 					</td>
@@ -202,34 +219,34 @@ class ContactForm extends SpecialPage {
 			<input type=\"hidden\" id=\"wpContactWikiName\" name=\"wpContactWikiName\" value=\"{$wgServer}\" />
 			<table border='0'>
 				<tr>
-					<td align='right'>$yn:</td>
+					<td align='right'>". wfMsg( 'contactusername' ) .":</td>
 					<td align='left'>
-						<input tabindex='" . ($ti++) . "' type='text' name=\"wpName\" {$user_readonly}value=\"{$encName}\" size='40' /> {$user_autofill_marker}
+						<input tabindex='" . ($ti++) . "' type='text' name=\"wpName\" value=\"{$encName}\" size='40' {$user_readonly}/> {$autofill_marker}
 					</td>
 				</tr>\n");
 		}
 
 		$wgOut->addHTML( "
 				<tr>
-					<td align='right'>$nvt:</td>
+					<td align='right'>". wfMsg( 'contactrealname' ) .":</td>
 					<td align='left'>
-						<input tabindex='" . ($ti++) . "' type='text' name=\"wpContactRealName\" {$user_readonly}value=\"{$encRealName}\" size='40' /> {$user_autofill_marker}
+						<input tabindex='" . ($ti++) . "' type='text' name=\"wpContactRealName\" value=\"{$encRealName}\" size='40' {$name_readonly}/> {$autofill_marker}
 					</td>
 				</tr>
 				<tr>
-					<td align='right'>$ye:</td>
+					<td align='right'>" . wfMsg( 'contactyourmail' ) .":</td>
 					<td align='left'>
-						<input tabindex='" . ($ti++) . "' type='text' name=\"wpEmail\" {$user_readonly}value=\"{$encEmail}\" size='40' /> {$user_autofill_marker}
+						<input tabindex='" . ($ti++) . "' type='text' name=\"wpEmail\" value=\"{$encEmail}\" size='40' {$mail_readonly}/> {$autofill_marker}
 					</td>
 				</tr>
 				<tr>
-					<td align='right'>$cwtt:</td>
+					<td align='right'>". wfMsg( 'contactproblem' ) .":</td>
 					<td align='left'>
 						<input tabindex='" . ($ti++) . "' type='text' name=\"wpContactProblem\" value=\"{$encProblem}\" size='80' />
 					</td>
 				</tr>
 				<tr>
-					<td align='right' valign='top'>$cwn:</td>
+					<td align='right' valign='top'>".  wfMsg( 'contactproblemdesc' ) . ":</td>
 					<td align='left'>
 						<textarea tabindex='" . ($ti++) . "' name=\"wpContactProblemDesc\" rows=\"10\" cols=\"60\">{$encProblemDesc}</textarea>
 					</td>
@@ -237,12 +254,14 @@ class ContactForm extends SpecialPage {
 				<tr>
 					<td></td>
 					<td align='left'>
-						<input tabindex='" . ($ti++) . "' type='submit' name=\"wpContactattempt\" value=\"{$rcw}\" />
+						<input tabindex='" . ($ti++) . "' type='submit' name=\"wpContactattempt\" value=\"". wfMsg( 'contactmail' ) ."\" />
 					</td>
 				</tr>\n");
 
 		if( !$wgUser->isAnon() && $wgUser->getEmail() != '') {
+			//is user, has email, but is verified?
 			if( $wgUser->getEmailAuthenticationTimestamp() != null ) {
+				//yes!
 				$wgOut->addHtml("
 				<tr>
 					<td></td>
@@ -253,11 +272,12 @@ class ContactForm extends SpecialPage {
 			}
 			else
 			{
+				//not
 				$wgOut->addHtml("
 				<tr>
 					<td></td>
 					<td align='left'>
-						<input tabindex='" . ($ti++) . "' type='checkbox' name=\"wgCC\" value=\"0\" disabled=\"disabled\" readonly=\"readonly\" />".
+						<input tabindex='" . ($ti++) . "' type='checkbox' value=\"0\" disabled=\"disabled\" readonly=\"readonly\" />".
 						"<s><i>" . wfMsg('contactccme') . "</i></s><br/> ". wfMsg('contactccdisabled'). "
 					</td>
 				</tr>\n");
