6f3c1fb86f24da9d4a8bb9ec8427a9a0c21aab36
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-04 15:51:34 +0000

    FEMonaco: more stuff for VET spriting
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19474 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoEmbedTool/VideoEmbedTool_body.php
+++ extensions/wikia/VideoEmbedTool/VideoEmbedTool_body.php
@@ -30,8 +30,8 @@ class VideoEmbedTool {
 		$out = '<div class="reset" id="VideoEmbed">';
                 $out .= '<div id="VideoEmbedBorder"></div>';
                 $out .= '<div id="VideoEmbedProgress1" class="VideoEmbedProgress"></div>';
-                $out .= '<div id="VideoEmbedBack"><div></div><a href="#">' . wfMsg( 'vet-back' ) . '</a></div>';
-		$out .= '<img src="'.$wgStylePath.'/common/blank.gif" id="fe_vetclose_img" class="sprite share" alt="'.wfMsg('vet-close').'" />';
+		$out .= '<img src="'.$wgStylePath.'/common/blank.gif" id="fe_vetback_img" class="sprite back" alt="'.wfMsg('vet-back').'" />';
+		$out .= '<img src="'.$wgStylePath.'/common/blank.gif" id="fe_vetclose_img" class="sprite close" alt="'.wfMsg('vet-close').'" />';
                 $out .= '<div id="VideoEmbedBody">';
                 $out .= '<div id="VideoEmbedError"></div>';
                 $out .= '<div id="VideoEmbedMain">' . $this->loadMain() . '</div>';
