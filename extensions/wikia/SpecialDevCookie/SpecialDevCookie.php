5aa09d2ad523d41eff362df40524ffc6ec7490b3
marooned <marooned@wikia-inc.com>
2010-03-04 11:48:01 +0000

    new special page for setting development cookie
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19449 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialDevCookie/SpecialDevCookie.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * switch cookie used for development
+ */
+
+
+$wgSpecialPages['DevCookie'] = 'SpecialDevCookie';
+
+class SpecialDevCookie extends UnlistedSpecialPage {
+
+	private
+		$mTitle,
+		$mCookie,
+		$mCookieName;
+
+	public function __construct() {
+		parent::__construct( 'DevCookie' );
+		$this->mCookieName = "-wikia-development";
+		$this->mCookie = null;
+	}
+
+	public function execute( $subpage ) {
+		global $wgOut, $wgUser, $wgRequest, $wgMessageCache, $wgCookiePrefix;
+
+		$wgMessageCache->addMessages( array( 'devcookie' => 'DevCookie' ), 'en' );
+
+		wfProfileIn( __METHOD__ );
+
+		$this->setHeaders();
+		$this->mTitle = SpecialPage::getTitleFor( 'DevCookie' );
+
+		/**
+		 * if posted change cookie value
+		 */
+		if( $wgRequest->wasPosted() ) {
+			$val = $wgRequest->getVal( 'cookie', 0 );
+			if( $val == 1 ) {
+				$wgOut->addHTML( Wikia::successbox( 'cookie set' ) );
+				$wgRequest->response()->setcookie( $this->mCookieName, '1' );
+				$this->mCookie = '1';
+			}
+			elseif( $val == 0 ) {
+				$wgOut->addHTML( Wikia::successbox( 'cookie set' ) );
+				$wgRequest->response()->setcookie( $this->mCookieName, '0' );
+				$this->mCookie = '0';
+			}
+			else {
+				$wgOut->addHTML( Wikia::successbox( 'cookie removed' ) );
+				$wgRequest->response()->setcookie( $this->mCookieName, '-1', 1 );
+				$this->mCookie = null;
+			}
+
+		}
+		else {
+			/**
+			 * show current value of cookie
+			 */
+			if ( isset( $_COOKIE[ $wgCookiePrefix . $this->mCookieName ] ) ) {
+				$this->mCookie = $_COOKIE[ $wgCookiePrefix . $this->mCookieName ];
+			}
+		}
+		$wgOut->addHTML(
+			"Current value of cookie {$wgCookiePrefix}{$this->mCookieName}: <em>" .
+			( is_null($this->mCookie) ? 'not set' : ($this->mCookie ? 'true' : 'false') ) . '</em>'
+		);
+
+		/**
+		 * show input chooser
+		 */
+		$wgOut->addHTML( Xml::openElement( 'form', array( 'action' => $this->mTitle->getFullURL(), 'method' => 'post' ) ) );
+		$select = new XMLSelect( 'cookie', 'cookie' );
+		$select->addOption( 'Set cookie to true', 1 );
+		$select->addOption( 'Set cookie to false', 0 );
+		$select->addOption( 'Remove cookie', -1 );
+		$wgOut->addHTML( $select->getHTML() );
+		$wgOut->addHTML( Xml::submitButton( 'submit' ) );
+		$wgOut->addHTML( Xml::closeElement( 'form' ) );
+
+		wfProfileOut( __METHOD__ );
+	}
+}
