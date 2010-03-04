fc18a7bf619b2a18897c0269504e9d2c93883857
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-04 05:37:54 +0000

    add extension (#41960)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19423 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/3rdparty/ListTransclusions/ListTransclusions.alias.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * ListTransclusions extension, alias file
+ *
+ * @author Patrick Westerhoff [poke]
+ */
+$aliases       = array();
+$aliases['en'] = array(
+    'ListTransclusions' => array( 'ListTransclusions' ),
+);
