d72428a279b9fb952480ededaa71887c059ee03f
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-05 15:12:25 +0000

    Monaco: cleanup and minify WikiaScriptLoader and use it as inline script in Monaco
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19540 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/monaco/js/WikiaScriptLoader.js
+++ skins/monaco/js/WikiaScriptLoader.js
@@ -1,14 +1,5 @@
 // @see http://www.stevesouders.com/blog/2009/04/27/loading-scripts-without-blocking/
 var WikiaScriptLoader = function() {
-	// benchmarking load time
-	var self = this;
-	this.start = window.wgNow.getTime() || this.getTime();
-
-	this.addHandler(window, 'load', function() {self.onPageLoad.call(self)});
-
-	// ID for script tags
-	this.scriptId = 0;
-
 	// detect Firefox / Opera / Chrome4 and use script DOM node injection for them
 	var userAgent = navigator.userAgent.toLowerCase();
 	this.useDOMInjection = (userAgent.indexOf('opera') != -1) ||
@@ -20,45 +11,30 @@ var WikiaScriptLoader = function() {
 
 	// get reference to <head> tag
 	this.headNode = document.getElementsByTagName('HEAD')[0];
-
-	// debug
-	this.log('init() after ' + (this.getTime() - this.start) + ' ms - ' + (this.useDOMInjection ? 'using script DOM node injection' : 'using document.write of <script> tag'));
 };
 
 WikiaScriptLoader.prototype = {
 	// load script from provided URL and don't block another downloads
 	// see pages 57 and 58 of "Even Faster Web Sites"
 	loadScript: function(url, onloadCallback) {
-		var id = 'wsl-' + (++this.scriptId);
-
 		if (this.useDOMInjection) {
-			this.loadScriptDOMInjection(id, url, onloadCallback);
+			this.loadScriptDOMInjection(url, onloadCallback);
 		}
 		else {
-			this.loadScriptDocumentWrite(id, url, onloadCallback);
+			this.loadScriptDocumentWrite(url, onloadCallback);
 		}
-
-		this.log('"' + url + '" is loading');
 	},
 
 	// use script DOM node injection method
-	loadScriptDOMInjection: function(id, url, onloadCallback) {
-		var self = this;
-
-		// bencharking
-		var start = this.getTime();
-
+	loadScriptDOMInjection: function(url, onloadCallback) {
 		// add <script> tag to <head> node
 		var scriptNode = document.createElement('script');
 		scriptNode.type = "text/javascript";
 		scriptNode.src = url;
-		scriptNode.id = id;
 
 		// handle script onload event
 		var scriptOnLoad = function() {
 			scriptNode.onloadDone = true;
-
-			self.log('"' + url + '" loaded in ' + (self.getTime() - start) + ' ms');
 
 			if (typeof onloadCallback == 'function') {
 				onloadCallback();
@@ -74,27 +50,15 @@ WikiaScriptLoader.prototype = {
 			}
 		}
 
-		// handle HTTP 404
-		scriptNode.onerror = function() {
-			self.log('loading "' + url + '" failed');
-		};
-
 		this.headNode.appendChild(scriptNode);
 	},
 
 	// use document.write method to add script tag
-	loadScriptDocumentWrite: function(id, url, onloadCallback) {
-		var self = this;
-
-		// bencharking
-		var start = this.getTime();
-
-		document.write('<scr' + 'ipt id="' + id + '"src="' + url + '" type="text/javascript"></scr' + 'ipt>');
+	loadScriptDocumentWrite: function(url, onloadCallback) {
+		document.write('<scr' + 'ipt src="' + url + '" type="text/javascript"></scr' + 'ipt>');
 
 		// handle script onload event
 		var scriptOnLoad = function() {
-			self.log('"' + url + '" loaded in ' + (self.getTime() - start) + ' ms');
-
 			if (typeof onloadCallback == 'function') {
 				onloadCallback();
 			}
@@ -109,15 +73,10 @@ WikiaScriptLoader.prototype = {
 	loadScriptAjax: function(url, onloadCallback) {
 		var self = this;
 		var xhr = this.getXHRObject();
-		var start = this.getTime();
 
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4) {
-				self.log('"' + url + '" loaded via AJAX in ' + (self.getTime() - start)  + ' ms');
 				var jsCode = xhr.responseText;
-
-				// execute downloaded JS
-				var evalStart = self.getTime();
 
 				// evaluate JS via eval() / inline <script> tag
 				if (self.isIE) {
@@ -132,9 +91,6 @@ WikiaScriptLoader.prototype = {
 					self.headNode.appendChild(scriptNode);
 				}
 
-				// debug
-				self.log('"' + url + '" executed in ' + (self.getTime() - evalStart)  + ' ms');
-
 				if (typeof onloadCallback == 'function') {
 					onloadCallback();
 				}
@@ -143,18 +99,6 @@ WikiaScriptLoader.prototype = {
 
 		xhr.open("GET", url, true);
 		xhr.send('');
-
-		this.log('"' + url + '" is loading via AJAX');
-	},
-
-	// log page loading time
-	onPageLoad: function() {
-		this.log('page loaded in ' + (this.getTime() - this.start) + ' ms');
-	},
-
-	// get current time in miliseconds
-	getTime: function() {
-		return (new Date()).getTime();
 	},
 
 	// add event handler
@@ -190,16 +134,6 @@ WikiaScriptLoader.prototype = {
 		}
 
 		return xhrObj;
-	},
-
-	// logging for Firefox/IE8/Opera
-	log: function(msg) {
-		if (typeof window.console != 'undefined') {
-			window.console.log('WSL: ' + msg);
-		}
-		else if (typeof window.opera != 'undefined') {
-			window.opera.postError('WSL: ' + msg);
-		}
 	}
 }
 
