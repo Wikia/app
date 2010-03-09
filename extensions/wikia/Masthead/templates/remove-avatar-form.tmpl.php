48780cfed0e8a77cced776bc43ffe82cfd485190
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-09 13:45:46 +0000

    loading YUI on demand
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19635 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Masthead/templates/remove-avatar-form.tmpl.php
+++ extensions/wikia/Masthead/templates/remove-avatar-form.tmpl.php
@@ -9,27 +9,28 @@
 <!-- js part -->
 <script type="text/javascript">
 /*<![CDATA[*/
-YAHOO.namespace("Blog.Avatar");
+$.loadYUI( function() {
+	YAHOO.namespace("Blog.Avatar");
 
-var YC = YAHOO.util.Connect;
-var YD = YAHOO.util.Dom;
-var YE = YAHOO.util.Event;
-var WA = YAHOO.Blog.Avatar;
+	var YC = YAHOO.util.Connect;
+	var YD = YAHOO.util.Dom;
+	var YE = YAHOO.util.Event;
+	var WA = YAHOO.Blog.Avatar;
 
-YAHOO.Blog.Avatar.Submit = function(e) {
-    YAHOO.util.Dom.get("wba-search-form").submit();
-}
-
-YAHOO.Blog.Avatar.SubmitRemove = function(e) {
-	if (confirm("<?= wfMsg("blog-avatar-remove-confirm") ?>")) {
-		YAHOO.util.Dom.get("wba-remove-form").submit();
-	} else {
-		YAHOO.util.Event.stopEvent(e);
+	YAHOO.Blog.Avatar.Submit = function(e) {
+	    YAHOO.util.Dom.get("wba-search-form").submit();
 	}
-}
-YE.addListener("wba-search-user", "click", YAHOO.Blog.Avatar.Submit);
-YE.addListener("wba-remove-user", "click", YAHOO.Blog.Avatar.SubmitRemove);
 
+	YAHOO.Blog.Avatar.SubmitRemove = function(e) {
+		if (confirm("<?= wfMsg("blog-avatar-remove-confirm") ?>")) {
+			YAHOO.util.Dom.get("wba-remove-form").submit();
+		} else {
+			YAHOO.util.Event.stopEvent(e);
+		}
+	}
+	YE.addListener("wba-search-user", "click", YAHOO.Blog.Avatar.Submit);
+	YE.addListener("wba-remove-user", "click", YAHOO.Blog.Avatar.SubmitRemove);
+});
 /*]]>*/
 </script>
 <?
@@ -39,7 +40,7 @@ if (isset($status) && ($status == "WMSG_REMOVE_ERROR"))
 <div id="wba-upload">
     <div style="float:left;padding:2px;text-align:center;">
     	<?= wfMsg("blog-avatar-cannot-remove") ?>
-    </div>	
+    </div>
 </div>
 <?
 }
@@ -47,17 +48,17 @@ if (isset($status) && ($status == "WMSG_REMOVE_ERROR"))
 <div id="wba-upload">
     <div style="float:left;padding:5px 2px;text-align:left;">
     	<?= wfMsg("blog-avatar-remove-info") ?>
-    </div>	
-	<div style="clear: both;"></div>    
+    </div>
+	<div style="clear: both;"></div>
 	<div>
 		<form id="wba-search-form" enctype="multipart/form-data" action="<?= $title->getLocalUrl() ?>" method="post">
 			<input type="hidden" name="action" value="search_user" />
 			<input type="text" name="av_user" size="25" value="<?= $search_user ?>">
 			<input type="submit" name="wpSubmit" id="wba-search-user" value="<?= wfmsg("blog-avatar-getuser") ?>">
-		</form>            
+		</form>
 	</div>
 </div>
-<div style="clear: both;"></div>    
+<div style="clear: both;"></div>
 <? if ($user && $is_posted) { ?>
 <div id="wba-upload">
     <div style="text-align: center;padding: 5px;">
@@ -71,23 +72,23 @@ if (isset($status) && ($status == "WMSG_REMOVE_ERROR"))
             <input type="hidden" name="action" value="remove_avatar" />
             <input type="hidden" name="av_user" value="<?= $user->getName() ?>">
             <input type="submit" name="wpSubmit" id="wba-remove-user" value="<?= wfMsg("blog-avatar-removeavatar") ?>">
-<? } ?>    
+<? } ?>
     		<input type="button" value="<?= wfMsg("blog-avatar-goto-userpage", $user->getName()) ?>" onclick="window.location='<?= Title::makeTitle( NS_USER, $user->getName() )->getLocalUrl(); ?>'" />
 <?if (!$avatar->isDefault()) { ?>
-		</form> 
-<? } ?>    
+		</form>
+<? } ?>
 	</div>
-	<div style="clear: both;"></div>    
+	<div style="clear: both;"></div>
 </div>
-<? } else if ($is_posted) { ?>	
+<? } else if ($is_posted) { ?>
 <div>
     <span style="float:left;padding:5px;text-align:center;">
     	<strong><?= wfMsg("blog-avatar-nouser") ?></strong>
     </span>
-</div>    
+</div>
 <? } ?>
 
-<div style="clear: both;"></div>    
+<div style="clear: both;"></div>
 <div id="wba-result">
 </div>
 <!-- e:<?= __FILE__ ?> -->
