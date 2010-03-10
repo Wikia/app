dd33f252ff79e689d8a260056ba9a7811ebb5c41
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-10 17:08:38 +0000

    AWC: corrected user existance check (#40647)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19697 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoCreateWiki/AutoCreateWiki_helper.php
+++ extensions/wikia/AutoCreateWiki/AutoCreateWiki_helper.php
@@ -280,7 +280,7 @@ class AutoCreateWiki {
 			$sResponse = wfMsg('autocreatewiki-empty-username');
 		} else {
 			$u = User::newFromName( $sValue );
-			if( is_null( $u ) || !User::isUsableName( $u->getName() ) ) {
+			if( is_null( $u ) || $u->getId() == 0 ) {
 				$sResponse = wfMsg('autocreatewiki-invalid-username');
 			} else {
 				$iId = User::idFromName( $sValue );
