10622b36bf194e31ba28123dfc00e6514b8c8446
moli <moli@wikia-inc.com>
2010-03-11 13:12:54 +0000

    number of words
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19721 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CorporatePage/HomePageStatistic.class.php
+++ extensions/wikia/CorporatePage/HomePageStatistic.class.php
@@ -16,36 +16,9 @@ class HomePageStatistic
 	public static function getWordsAddedLastWeek(){
 		global $wgMemc;
         wfProfileIn( __METHOD__ );          
-		$key = wfMemcKey( "hp_stats", "words_added_week" );
-		$result = $wgMemc->get( $key, null);
 
-		if ($result != null){
-			return number_format($result);
-		}
-		
-		$months = array(); 
-		$thisMonth =  date('Y-m',strtotime($i.'-1 day'));
-		for ($i = -1; $i >= -6; $i-- ){
-			$time = strtotime($i.' day');
-			$month = date('Y-m', $time);
-			$months[$month]['count'] ++;
-			if (empty($months[$month]['number_of_day'])){
-				if ( $thisMonth == $month ){
-					$months[$month]['number_of_day'] = date('j', $time);	
-				} else {
-					$months[$month]['number_of_day'] = date('t', $time);	
-				}
-			}
-			if ( $i == -1 ){
-				$months[$month]['count'] += date('G')/24;
-			}
-		}
-		$result = 0;
-		foreach ( $months as $key => $value ){
-			$factor = $value['count']/$value['number_of_day'];
-			$result += (int) (WikiaGlobalStats::getCountWordsInMonth($key)*$factor);
-		}
-		$wgMemc->set( $key, $result, 60*60);
+		$result = (int) (WikiaGlobalStats::countWordsInLastDays());
+
         wfProfileOut( __METHOD__ );          
 		return number_format($result);
 	}
