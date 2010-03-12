b7b8d6e3b28fe71f0e2d7a4f0719991d8b1f2f2a
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-12 10:28:18 +0000

    * Bump MonacoSidebar version
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19781 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/Monaco.php
+++ skins/Monaco.php
@@ -18,7 +18,7 @@ $wgHooks['MessageCacheReplace'][] = 'MonacoSidebar::invalidateCache';
 
 class MonacoSidebar {
 
-	const version = '0.04';
+	const version = '0.05';
 
 	static function invalidateCache() {
 		global $wgMemc;
