5468d9573a6eccc5fcc44b34ab7f62d73e32f63d
tomek <tomek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-15 23:29:02 +0000

    remove return to from short text
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@19919 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaStats/WikiaStatsAutoHubsConsumerDB.php
+++ extensions/wikia/WikiaStats/WikiaStatsAutoHubsConsumerDB.php
@@ -382,7 +382,7 @@ class WikiaStatsAutoHubsConsumerDB {
 		while ( $value = $this->dbs->fetchRow($res) ) {
 			if( !empty($city_array[$value['city_id']]) ) {
 				$city_array[$value['city_id']] = array_merge( $value, $city_array[$value['city_id']]);	
-				$city_array[$value['city_id']]['city_description'] = $this->shortenText( $city_array[$value['city_id']]['city_description'],0 ,120 );
+				$this->shortenText( $city_array[$value['city_id']]['city_description'],0 ,120 );
 			}
 		}
 
