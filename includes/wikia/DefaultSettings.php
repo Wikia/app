54fde2efb7f9666c682ae939096dd3305627229a
sean <sean@wikia-inc.com>
2010-03-17 21:20:05 +0000

    Bump wgStyleVersion
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20067 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20036';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20066';
 
 /**
  * @name $wgCityId
