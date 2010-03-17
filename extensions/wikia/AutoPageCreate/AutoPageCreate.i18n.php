7b4f43e2b4df96e9db3078a2681b9ab98f328ea8
emil <emil@wikia-inc.com>
2010-03-17 13:39:19 +0000

    css changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20032 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoPageCreate/AutoPageCreate.i18n.php
+++ extensions/wikia/AutoPageCreate/AutoPageCreate.i18n.php
@@ -8,12 +8,12 @@
 $messages = array();
 
 $messages['en'] = array(
-	"autopagecreate-newpage-notice-content"	=> "<p style=\"text-align: center\">'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
-	"autopagecreate-newpage-notice-talk"	=> "<p style=\"text-align: center\">'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
-	"autopagecreate-newpage-notice-user"	=> "<p style=\"text-align: center\">'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
-	"autopagecreate-newpage-notice-project"	=> "<p style=\"text-align: center\">'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
-	"autopagecreate-newpage-notice-template"	=> "<p style=\"text-align: center\">'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
-	"autopagecreate-newpage-notice-forum"	=> "<p style=\"text-align: center\">'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
-	"autopagecreate-newpage-notice-blog"	=> "<p style=\"text-align: center\">'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
-	"autopagecreate-newpage-notice-other"	=> "<p style=\"text-align: center\">'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
+	"autopagecreate-newpage-notice-content"	=> "<p>'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
+	"autopagecreate-newpage-notice-talk"	=> "<p>'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
+	"autopagecreate-newpage-notice-user"	=> "<p>'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
+	"autopagecreate-newpage-notice-project"	=> "<p>'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
+	"autopagecreate-newpage-notice-template"	=> "<p>'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
+	"autopagecreate-newpage-notice-forum"	=> "<p>'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
+	"autopagecreate-newpage-notice-blog"	=> "<p>'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
+	"autopagecreate-newpage-notice-other"	=> "<p>'''This article is just starting. You can help by adding a sentence or a photo!'''</p>",
 );
