a9d1561888f6c13ed42f9210a3b1adc82d28ef55
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-19 16:23:11 +0000

    Bump style version to try to get all of the broken CSS flushed out more quickly.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20210 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20085';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20209';
 
 /**
  * @name $wgCityId
