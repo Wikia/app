73dbddf7d242201a5ed594e5d008dce0924b1f1b
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-23 15:47:15 +0000

    AutoPageCreate: for user page, but without condition so far
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20327 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoPageCreate/AutoPageCreate.php
+++ extensions/wikia/AutoPageCreate/AutoPageCreate.php
@@ -22,10 +22,19 @@ function wfAutoPageCreateInit() {
 }
 
 function wfAutoPageCreateEditPage( $editpage ) {
-	if( !$editpage->mTitle->exists() && $editpage->mTitle->isContentPage() ) {
-		$editpage->textbox1 = wfMsgForContent( 'newpagelayout' );
+	if( !$editpage->mTitle->exists() ) {
+		if( $editpage->mTitle->isContentPage() ) {
+			$editpage->textbox1 = wfMsgForContent( 'newpagelayout' );
+		} else if( ( $editpage->mTitle->getNamespace() == NS_USER ) && !wfAutoPageCreateIsAnonUserpage( $editpage->mTitle  ) ) {
+			$editpage->textbox1 = wfMsgForContent( 'welcome-user-page' );
+		}
 	}
 	return true;
+}
+
+function wfAutoPageCreateIsAnonUserpage( $title  ) {
+
+	return false;
 }
 
 function wfAutoPageCreateSetupVars( $vars ) {
@@ -57,7 +66,12 @@ function wfAutoPageCreateViewPage( $article, $out, &$text, &$return404 ) {
 				$out->setRobotPolicy( 'noindex,nofollow' );
 			} else {
 				switch( $article->mTitle->getNamespace() ) {
-					case NS_USER:		$overlayMsgKey = "autopagecreate-newpage-notice-user"; break;
+					case NS_USER:
+						if( !wfAutoPageCreateIsAnonUserpage( $article->mTitle ) ) {
+							$text = wfMsgForContent( "welcome-user-page" );
+							$overlayMsgKey = "autopagecreate-newpage-notice-user";
+						}
+						break;
 					case NS_PROJECT:	$overlayMsgKey = "autopagecreate-newpage-notice-project"; break;
 					case NS_TEMPLATE:	$overlayMsgKey = "autopagecreate-newpage-notice-template"; break;
 					case 110 /* NS_FORUM */:	$overlayMsgKey = "autopagecreate-newpage-notice-forum"; break;
