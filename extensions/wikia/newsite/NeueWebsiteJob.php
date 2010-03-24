31b653fe7b88505f5071da89e380d4b89c3db47c
eloy <eloy@wikia-inc.com>
2010-03-24 13:09:20 +0000

    makeRelated copied and little adjusted
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20418 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/newsite/NeueWebsiteJob.php
+++ extensions/wikia/newsite/NeueWebsiteJob.php
@@ -39,4 +39,40 @@ class NeueWebsiteJob extends Job {
 		wfProfileOut( __METHOD__ );
 	}
 
+	/**
+	 * make related pages data using google related info
+	 *
+	 * @access private
+	 */
+	private function makeRelated( $dom, $domdom ) {
+		global $exDomainList;
+
+		$gourl = "http://www.google.de/ie?safe=off&q=related%3A$dom&hl=de&start=0&num=30&sa=N";
+		$go = Http::get( $gourl );
+
+		if( !strstr( $go, "keine mit Ihrer Suchanfrage" ) ) {
+			$matches = array();
+			$newmatches = array();
+			preg_match_all( '|http://([^/]+)/|', $go, $matches );
+
+			foreach( $matches as $match ) {
+				$n = strtolower($match);
+				if(!strncmp($n, "www.", 4)) {
+					$n = substr($n, 4);
+					if(ereg($exDomainList, $n) && stripos($n, "google") === false)
+						$newmatches[] = $n;
+					}
+				}
+
+			$umatches = array_unique($newmatches);
+
+			foreach($umatches as $match) {
+				// echo "match: $dom $match ---";
+				wfWaitForSlaves( 5 );
+				$dbw = wfGetDB( DB_MASTER );
+				$qr = $dbw->query("insert into related set name1='$domdom', name2='$match'");
+				// hope it works
+			}
+		}
+	}
 }
