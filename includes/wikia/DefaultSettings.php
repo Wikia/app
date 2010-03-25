faa265699ab5acb72518642ca8f99a3117995a73
bartek <bartek@wikia-inc.com>
2010-03-25 18:41:43 +0000

    StyleVersion++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20496 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20455';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20457';
 
 /**
  * @name $wgCityId
