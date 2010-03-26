1300866bc41b55e54e9f618c5db356e413f8c507
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-26 15:59:48 +0000

    related as parser hook
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20536 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/newsite/NeueWebsite.php
+++ extensions/wikia/newsite/NeueWebsite.php
@@ -1,18 +1,29 @@
 <?php
-# Not a valid entry point, skip unless MEDIAWIKI is defined
-if (!defined('MEDIAWIKI')) 
-{
-        echo <<<EOT
-  ... Not installed ...
-EOT;
-        exit( 1 );
+
+if( !defined( 'MEDIAWIKI' ) ) {
+    echo "This is MediaWiki extension and cannot be used standalone.\n";
+    exit( 1 ) ;
 }
 
 include('neue_website.php');
 
 $dir = dirname(__FILE__);
 
+/**
+ * initialize parse hooks
+ */
+// $wgHooks['ParserFirstCallInit'][] = 'efNeueWebsiteHooks';
+// $wgAutoloadClasses['NeueWebsiteHooks'] = $dir . '/NeueWebsite.hooks.php';
+
 $wgAutoloadClasses['NeueWebsite'] = $dir . '/NeueWebsite.body.php';
 $wgSpecialPages['NewWebsite'] = 'NeueWebsite';
 $wgExtensionMessagesFiles['Newsite'] = $dir . '/NeueWebsite.i18n.php';
 $wgExtensionAliasesFiles['Newsite'] = $dir . '/NeueWebsite.alias.php';
+
+/**
+ * register <nws:related /> tag
+ */
+function efNeueWebsiteHooks( &$parser ) {
+	$parser->setHook( "nws:related", array( "NeueWebsiteHooks", "renderRelated" ) );
+	return true;
+}
