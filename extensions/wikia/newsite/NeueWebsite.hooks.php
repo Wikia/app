edf3f03a730ae9fec6e25b9d372cae663d5c00d4
eloy <eloy@wikia-inc.com>
2010-03-26 15:59:48 +0000

    related as parser hook
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20536 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/newsite/NeueWebsite.hooks.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * parser hooks definition
+ * @author Krzysztof Krzyżaniak (eloy)
+ */
+
+
+class NeueWebsiteHooks {
+
+	/**
+	 * renderRelated
+	 *
+	 * static constructor/callback function -- render values from related table
+	 *
+	 * @access public
+	 * @static
+	 * @author Krzysztof Krzyżaniak <eloy@wikia.com>
+	 *
+	 * @param string $input: Text from tag
+	 * @param array $params: atrributions
+	 * @param Object $parser: Wiki Parser object
+	 */
+	static public function renderRelated( $input, $params, &$parser ) {
+		global $wgTitle;
+
+		wfProfileIn( __METHOD__ );
+
+		$key = strtolower( $wgTitle->getDBkey() );
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$sth = $dbr->select(
+			array( "related" ),
+			array( "name1", "name2" ),
+			array( $dbr->makeList( array( "name1" => $key, "name2" => $key ), LIST_OR ) )
+		);
+
+		$results = array();
+		$output  = "";
+
+		while( $row = $dbr->fetchObject( $sth ) ) {
+			if( $row->name1 !== $key ) {
+				$results[] = $row->name1;
+			}
+			if( $row->name2 !== $key ) {
+				$results[] = $row->name2;
+			}
+		}
+		$results = array_unique( $results );
+		if( count( $results ) ) {
+			wfLoadExtensionMessages( 'Newsite' );
+			$output .= wfMsgForContent( 'newsite-output-related' );
+			$output .= Xml::openElement( "span", array( "class" => "small" ) );
+			foreach( $results as $site ) {
+				$output .= sprintf("[[%s]] ", ucfirst( strtolower( $site ) ) );
+			}
+			$output .= Xml::closeElement( "span" );
+		}
+
+		wfProfileOut( __METHOD__ );
+
+		return $output;
+	}
+
+}
