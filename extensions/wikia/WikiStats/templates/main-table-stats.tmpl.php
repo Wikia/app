41c52885ce74ade52eec0e478216c07e34d3e04f
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-26 11:05:39 +0000

    commit changes (show only 11 cols)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20531 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiStats/templates/main-table-stats.tmpl.php
+++ extensions/wikia/WikiStats/templates/main-table-stats.tmpl.php
@@ -8,40 +8,29 @@
 <caption></caption>
 <thead>
 <tr>
-	<th rowspan="4"><b><?= ucfirst(wfMsg('wikistats_date')) ?></b></th>
-	<th colspan="8"><b><a href="#wikians"><?= wfMsg('wikistats_wikians') ?></a></b></th>
-	<th colspan="5"><b><a href="#articles"><?= wfMsg('wikistats_articles') ?></a></b></th>
+	<th rowspan="3"><b><?= ucfirst(wfMsg('wikistats_date')) ?></b></th>
+	<th colspan="4"><b><a href="#wikians"><?= wfMsg('wikistats_wikians') ?></a></b></th>
+	<th colspan="3"><b><a href="#articles"><?= wfMsg('wikistats_articles') ?></a></b></th>
 	<th colspan="4"><b><a href="#media"><?= wfMsg('wikistats_media') ?></a></b></th>
 </tr>
 <tr>
-	<th colspan="5"><?=wfMsg('wikistats_lifetime_editors')?></th>
-	<th colspan="3"><?=wfMsg('wikistats_months_edits')?></th>
-	<th rowspan="3"><?=wfMsg('wikistats_total')?></th>
-	<th rowspan="3"><?=wfMsg('wikistats_new_per_day')?></th>
-	<th rowspan="3"><?=wfMsg('size-kilobytes', 0.5)?></th>
-	<th rowspan="3"><?=wfMsg('wikistats_edits')?></th>
-	<th rowspan="3"><?=wfMsg('wikistats_words')?></th>
+	<th colspan="2"><?=wfMsg('wikistats_namespaces')?></th>
+	<th colspan="2"><?=wfMsg('wikistats_edits')?></th>
+	<th rowspan="2"><?=wfMsg('wikistats_total')?></th>
+	<th rowspan="2"><?=wfMsg('wikistats_new_per_day')?></th>
+	<th rowspan="2"><?=wfMsg('wikistats_edits')?></th>
 	<th colspan="2"><?=wfMsg('wikistats_images')?></th>
 	<th colspan="2"><?=wfMsg('wikistats_video')?></th>
 </tr>
 <tr>
-	<th rowspan="2"><?=wfMsg('wikistats_total')?></th>
-	<th colspan="2"><?=wfMsg('wikistats_namespaces')?></th>
-	<th colspan="2"><?=wfMsg('wikistats_edits')?></th>
-	<th colspan="3"><?=wfMsg('wikistats_namespaces')?></th>
-	<th rowspan="2"><?=wfMsg('wikistats_links')?></th>
-	<th rowspan="2"><?=wfMsg('wikistats_uploaded_images')?></th>
-	<th rowspan="2"><?=wfMsg('wikistats_video_embeded')?></th>
-	<th rowspan="2"><?=wfMsg('wikistats_uploaded_images')?></th>
-</tr>
-<tr>
-	<th><?=wfMsg('wikistats_content')?></th>
-	<th><?=wfMsg('wikistats_userns')?></th>
-	<th>&gt;5</th>
-	<th>&gt;100</th>
 	<th><?=wfMsg('wikistats_total')?></th>
 	<th><?=wfMsg('wikistats_content')?></th>
-	<th><?=wfMsg('wikistats_userns')?></th>
+	<th>&gt;5</th>
+	<th>&gt;100</th>
+	<th><?=wfMsg('wikistats_links')?></th>
+	<th><?=wfMsg('wikistats_uploaded_images')?></th>
+	<th><?=wfMsg('wikistats_video_embeded')?></th>
+	<th><?=wfMsg('wikistats_uploaded_images')?></th>
 </tr>
 </thead>
 <tbody>
@@ -55,24 +44,16 @@
 		if (empty($out) || ($out === 0)) {
 			$out = "&nbsp;";
 		} else {
-			switch ($column) {
-				case 'date': 
-					$stamp = mktime(23, 59, 59, substr($out, 4, 2), 1, substr($out, 0, 4));
-					if ( $out == $today ) {
-						$stamp = ( !empty($today_day) ) ? $today_day : $stamp;
-						$out = $wgLang->sprintfDate( $mStats->dateFormat(0), wfTimestamp(TS_MW, $stamp));
-					} else {
-						$out = $wgLang->sprintfDate("M Y", wfTimestamp(TS_MW, $stamp));
-					}
-					$row = "th";
-					$column = "&nbsp;";
-					break;
-				case 'K' : # percent of articles > 0.5 kB
-					$out = $mStats->numberFormat( $columns['I'] ? (100*$out)/$columns['I'] : 0, "%0d%%" ); 
-					break;
-				default: 
-					$out = $mStats->numberFormat( $out, "%0.1f" ); 
-					break;
+			if ($column == 'date' ) { 
+				$stamp = mktime(23, 59, 59, substr($out, 4, 2), 1, substr($out, 0, 4));
+				if ( $out == $today ) {
+					$stamp = ( !empty($today_day) ) ? $today_day : $stamp;
+					$out = $wgLang->sprintfDate( $mStats->dateFormat(0), wfTimestamp(TS_MW, $stamp));
+				} else {
+					$out = $wgLang->sprintfDate("M Y", wfTimestamp(TS_MW, $stamp));
+				}
+				$row = "th";
+				$column = "&nbsp;";
 			}
 		}
 		$cols[] = $column;
@@ -120,39 +101,28 @@ foreach ($diffData as $date => $columns) {
 }
 ?>
 <tr>
-	<th rowspan="4"><b><?= ucfirst(wfMsg('wikistats_date')) ?></b></th>
-	<th rowspan="2"><?=wfMsg('wikistats_total')?></th>
-	<th><?=wfMsg('wikistats_content')?></th>
-	<th><?=wfMsg('wikistats_userns')?></th>
-	<th>&gt;5</th>
-	<th>&gt;100</th>
+	<th rowspan="3"><b><?= ucfirst(wfMsg('wikistats_date')) ?></b></th>
 	<th><?=wfMsg('wikistats_total')?></th>
 	<th><?=wfMsg('wikistats_content')?></th>
-	<th><?=wfMsg('wikistats_userns')?></th>
-	<th rowspan="3"><?=wfMsg('wikistats_total')?></th>
-	<th rowspan="3"><?=wfMsg('wikistats_new_per_day')?></th>
-	<th rowspan="3"><?=wfMsg('size-kilobytes', 0.5)?></th>
-	<th rowspan="3"><?=wfMsg('wikistats_edits')?></th>
-	<th rowspan="3"><?=wfMsg('wikistats_words')?></th>
-	<th rowspan="2"><?=wfMsg('wikistats_links')?></th>
-	<th rowspan="2"><?=wfMsg('wikistats_uploaded_images')?></th>
-	<th rowspan="2"><?=wfMsg('wikistats_video_embeded')?></th>
-	<th rowspan="2"><?=wfMsg('wikistats_uploaded_images')?></th>
-</tr>
+	<th>&gt;5</th>
+	<th>&gt;100</th>
+	<th rowspan="2"><?=wfMsg('wikistats_total')?></th>
+	<th rowspan="2"><?=wfMsg('wikistats_new_per_day')?></th>
+	<th rowspan="2"><?=wfMsg('wikistats_edits')?></th>
+	<th><?=wfMsg('wikistats_links')?></th>
+	<th><?=wfMsg('wikistats_uploaded_images')?></th>
+	<th><?=wfMsg('wikistats_video_embeded')?></th>
+	<th><?=wfMsg('wikistats_uploaded_images')?></th>
+</tr>	
 <tr>
 	<th colspan="2"><?=wfMsg('wikistats_namespaces')?></th>
 	<th colspan="2"><?=wfMsg('wikistats_edits')?></th>
-	<th colspan="3"><?=wfMsg('wikistats_namespaces')?></th>
-</tr>
-<tr>
-	<th colspan="5"><?=wfMsg('wikistats_lifetime_editors')?></th>
-	<th colspan="3"><?=wfMsg('wikistats_months_edits')?></th>
 	<th colspan="2"><?=wfMsg('wikistats_images')?></th>
 	<th colspan="2"><?=wfMsg('wikistats_video')?></th>
-</tr>
+</tr>	
 <tr>
-	<th colspan="8"><b><a href="#wikians"><?= wfMsg('wikistats_wikians') ?></a></b></th>
-	<th colspan="5"><b><a href="#articles"><?= wfMsg('wikistats_articles') ?></a></b></th>
+	<th colspan="4"><b><a href="#wikians"><?= wfMsg('wikistats_wikians') ?></a></b></th>
+	<th colspan="3"><b><a href="#articles"><?= wfMsg('wikistats_articles') ?></a></b></th>
 	<th colspan="4"><b><a href="#media"><?= wfMsg('wikistats_media') ?></a></b></th>
 </tr>
 </tfoot>
