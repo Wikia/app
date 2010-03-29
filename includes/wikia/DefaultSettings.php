94281dab709dba5afd854cceb350e3c307eb9f92
tomek <tomek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-29 17:13:05 +0000

    style version ++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20588 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20569';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20570';
 
 /**
  * @name $wgCityId
