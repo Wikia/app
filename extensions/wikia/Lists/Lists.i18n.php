ac9f00318949c7bc948af8101c202ff2a2c51dd2
sean <sean@wikia-inc.com>
2010-03-31 01:48:14 +0000

    Added Lists which is very much in-progress.  It's not recommended to enable it in production.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20634 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Lists/Lists.i18n.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Lists
+ *
+ * This extension will allow easy creation and sharing of lists
+ * either as an addition to an existing wiki or as a standalone wiki.
+ * https://contractor.wikia-inc.com/wiki/Lists
+ *
+ * @author Sean Colombo <sean at wikia-inc dot com>
+ * @date 20100330
+ * @copyright Copyright (C) 2010 Sean Colombo, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @package MediaWiki
+ *
+ * To activate this functionality, place this file in your extensions/
+ * subdirectory, and add the following line to LocalSettings.php:
+ *     require_once("$IP/extensions/wikia/Lists/Lists.php");
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'lists-desc' => 'Allows existing wikis or standalone wikis to easily create and share lists.',
+	'lists-default-category-prefix' => 'Lists/',
+	'lists-default-item-category-prefix' => 'Lists Containing/',
+
+
+);
+
+
+/** Message documentation (Message documentation)
+ */
+$messages['qqq'] = array(
+);
+
