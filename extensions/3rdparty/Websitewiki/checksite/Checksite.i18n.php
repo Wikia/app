9e32ed85e40e4e3099a44a4e553cd35f4e8f7088
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-03-31 15:35:53 +0000

    RT#43764: WSW: Enable i18n for Special:Checksite and Special:Keyword
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20654 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/Websitewiki/checksite/Checksite.i18n.php
+++ extensions/3rdparty/Websitewiki/checksite/Checksite.i18n.php
@@ -1,12 +1,27 @@
 <?php
-$allMessages = array(
-        'en' => array( 
-                'checksite' => 'CheckSite'
+$messages = array(
+        'en' => array(
+                'checksite' => 'CheckSite',
                 'neuewebsite' => 'CheckSite'
         ),
-        'de' => array( 
-                'checksite' => 'Website überprüfen'
-                'checksite' => 'Website überprüfen'
+        'de' => array(
+                'checksite' => 'Website überprüfen',
+				'checksite-description' =>
+'* Mit dieser Spezialseite kann der Screenshot aktualisiert werden. Die Screenshots werden in der Reihenfolge der Anforderung aktualisiert - bei vielen Screenshots kann das etwas dauern. Mehrfache Anforderung beschleunigt den Vorgang nicht.
+* Ausserdem wird anhand von robots.txt überprüft, ob der Inhaber einer Website diese für ungeeignet hält, im WebsiteWiki diskutiert zu werden.',
+				'checksite-label' => "'''Website überprüfen:'''",
+				'checksite-submit' => 'überprüfen',
+				'checksite-cant-check' => '$1 kann nicht überprüft werden.',
+				'checksite-not-found' => '$1 kann nicht als Wikipage gefunden werden.',
+				'checksite-not-exist' => 'Die Website <a href="$1">$1</a> existiert nicht im WebsiteWiki.',
+				'checksite-url-not-found' => 'Die Website $1 wurde nicht gefunden.',
+				'checksite-robots' => 'http://$1/robots.txt empfiehlt, $2 nicht im Websitewiki aufzuführen.',
+				'checksite-redirect-url' => 'Ungeeignet',
+				'checksite-redirect-comment' => 'Redirect: Ungeeignet',
+				'checksite-screenshot-error' => 'Der Screenshot kann nicht aktualisiert werden.',
+				'checksite-screenshot-updating' => 'Der Screenshot von $1 wird aktualisiert.',
+
+				'newmark-submit' => 'anlegen',
+				'newmark-header' => "'''Neue Website:'''",
         ),
 );
-?>
--- extensions/3rdparty/Websitewiki/keyword/Keyword.body.php
+++ extensions/3rdparty/Websitewiki/keyword/Keyword.body.php
@@ -1,198 +1,176 @@
 <?php
 
-function kwlink($kw)
-{
-  $kwu = $kw;
-  $kwu{0} = strtoupper($kwu{0});
-  return "<a href=\"/Spezial:Keyword/" . htmlspecialchars($kw) . "\">$kwu</a>";
+class Keyword extends SpecialPage {
+
+	static $specialKeywordUrl;
+
+	function  __construct() {
+		parent::__construct( 'Keyword' , '' /*restriction*/);
+		wfLoadExtensionMessages('Keyword');
+		self::$specialKeywordUrl = Skin::makeSpecialUrl('Keyword');
+	}
+
+	function kwlink($kw) {
+		$kwu = $kw;
+		$kwu{0} = strtoupper($kwu{0});
+		return '<a href="' . self::$specialKeywordUrl . '/' . htmlspecialchars($kw) . "\">$kwu</a>";
+	}
+
+	function execute($par) {
+		global $wgRequest, $wgOut;
+
+		$action = Skin::makeSpecialUrl('Keyword');
+
+		wfLoadExtensionMessages('Keyword');
+		$label = wfMsgExt('keyword-label', array('parseinline'));
+		$submit = wfMsg('keyword-submit');
+		$specialKeywordCommonUrl = Skin::makeSpecialUrl('Keyword', 'target=common');
+		$list = wfMsg('keyword-keywords-list', $specialKeywordCommonUrl);
+
+		$post = '<p />
+			<form action="' . self::$specialKeywordUrl . "\" method=\"get\">
+			$label
+			<input type=\"text\" name=\"kw\" size=\"40\" maxlength=\"80\" />
+			<input type=\"submit\" value=\"$submit\" />
+			</form><p />
+			$list<p />";
+
+		$output = '';
+		$this->setHeaders();
+		$wgOut->setRobotPolicy('index,follow');
+
+		// Google
+		//TODO: "summary" is hardcoded in German, should this whole block be moved to message?
+		$google = '<table border="1" cellspacing="0" cellpadding="4" style="float:right; margin:0 0 .5em 1em;
+		 width:260px; background:#fff; border-collapse:collapse; border:1px solid #999;
+		 font-size:smaller; line-height:1.5; " summary="Anzeigen"><tr><td align="center" height="250" style="background:#ffffff;">';
+		$google .= AdEngine::getInstance()->getAd( 'WEBSITEWIKI_KEYWORDS' );
+		$google .= '</td></tr></table>';
+
+		# Get request data from, e.g.
+		$kwp = $wgRequest->getText('kw');
+		$tar = $wgRequest->getText('target');
+		$doall = $wgRequest->getText('all');
+		$kwt = substr(strstr($wgRequest->getVal('title'), '/'), 1);
+		$kw = $kwp ? $kwp : $kwt;
+
+		# Do stuff
+
+		$dbr = wfGetDB(DB_SLAVE, 'vslow');
+
+		if ($tar == 'common') {
+			$wgOut->setPagetitle(wfMsg('keyword-common-page-title'));
+			$label = wfMsg('keyword-common-label');
+			$wgOut->addHTML("$label<ol>\n");
+
+			$res = $dbr->doQuery('SELECT kw_word, kw_count FROM kw_keywords ORDER BY kw_count DESC LIMIT 100');
+			while ($res && $row = mysql_fetch_row($res)) {
+				$kword = $row[0];
+				$kcount = $row[1];
+				$output .= '<li>' . kwlink($kword) . " ($kcount)</li>\n";
+			}
+			$output .= "</ol>\n";
+			$wgOut->addHTML($google . $output);
+
+			return;
+		}
+
+		if (!isset($kw) || strlen($kw) < 2) {
+			$wgOut->addHTML($post);
+			return;
+		}
+
+		$keyword = strtolower(trim(str_replace('_', ' ', $kw)));
+		$gnkeyword = $keyword;
+		$keyword{0} = strtoupper($keyword{0});
+
+		$wgOut->setPagetitle(wfMsg('keyword-page-title', $keyword));
+
+		$kw = $dbr->strencode(strtolower(trim(str_replace('_', ' ', $kw))));
+
+		$res = $dbr->doQuery('SELECT COUNT(*) FROM kw_keywords');
+		if ($res && $row = mysql_fetch_row($res)) {
+			$diffkeys = $row[0];
+		} else {
+			$wgOut->addWikiMsg('keyword-bad-length');
+			return;
+		}
+
+		$res = $dbr->doQuery('SELECT kw_count FROM kw_keywords ORDER BY kw_count DESC LIMIT 1');
+		if ($res && $row = mysql_fetch_row($res)) {
+			$maxkeycount = $row[0];
+		} else {
+			$wgOut->addWikiMsg('keyword-bad-length');
+			return;
+		}
+
+		$res = $dbr->doQuery("SELECT kw_count, kw_id FROM kw_keywords WHERE kw_word='$kw'");
+		if ($res && $row = mysql_fetch_row($res)) {
+			$thiskeycount = $row[0];
+			$thiskeyid = $row[1];
+		} else {
+			$wgOut->addWikiMsg('keyword-not-used', $keyword);
+			return;
+		}
+
+		$thisrelcomm = round(($thiskeycount / $maxkeycount * 100), 2);
+
+		$output .= '<ul>';
+		$output .= wfMsg('keyword-description', $keyword, $gnkeyword, $specialKeywordCommonUrl);
+
+		$res = $dbr->doQuery("SELECT kw_page,kw_ptype FROM kw_page WHERE kw_key = $thiskeyid LIMIT 50;");  // 50 was 100
+		$similpages = array();
+		$porncount = 0;
+		while ($res && $row = mysql_fetch_row($res)) {
+			$similpages[] = $row[0];
+			$porncount += $row[1];
+		}
+		// $output .= "<li>npc = $porncount</li>\n";
+
+		$similstring = '(';
+		foreach( $similpages as $spage)
+			$similstring .= $spage . ",";
+
+		$similstring .= '0)';
+
+		$res = $dbr->doQuery("SELECT kw_word FROM kw_keywords,kw_page WHERE kw_page IN $similstring AND kw_key=kw_id GROUP BY kw_key ORDER BY COUNT(kw_key) DESC LIMIT 15 OFFSET 1");
+		while ($res && $row = mysql_fetch_row($res)) {
+			$sameword = $row[0];
+			$output .= '<li>' . kwlink($sameword) . "</li>\n";
+		}
+		$output .= "</ul></li>\n";
+
+		if (!$doall && $thiskeycount > 25) {
+			srand($thiskeyid);
+			$off = rand(0, $thiskeycount - 25);
+			$ua = wfMsg('keyword-incl');
+			$alleanz = ' [<a href="?all=1"' . wfMsg('keyword-show-all') . '</a>]';
+			$limi = "LIMIT 25 OFFSET $off";
+		} else {
+			$ua = '';
+			$off = 0;
+			$alleanz = '';
+			$limi = '';
+		}
+
+		// randomized, slow $res = $dbr->doQuery("select page_title from kw_page,page where kw_key=$thiskeyid and page_id=kw_page order by rand($thiskeyid) limit 25");
+
+		$res = $dbr->doQuery("SELECT page_title FROM kw_page, page WHERE kw_key=$thiskeyid AND page_id=kw_page $limi");
+		$usedOn = wfMsg('keyword-used-on', $keyword, $alleanz);
+		$output .= "<li>$usedOn</li>\n</ul>\n<ul>\n";
+		while ($res && $row = mysql_fetch_row($res)) {
+			$pgtitle = $row[0];
+			$output .= '<li><a href="/' . htmlspecialchars($pgtitle) . "\">$pgtitle</a></li>\n";
+		}
+		$output .= "</ul></li>\n";
+		$output .= "</ul>\n";
+
+//		$output .= "KW: $kw num $diffkeys this $thiskeycount id $thiskeyid max $maxkeycount\n";
+
+		if ($porncount == 0) {
+			$wgOut->addHTML( $google.$output );
+		} else {
+			$wgOut->addHTML( $output );
+		}
+	}
 }
-
-class Keyword extends SpecialPage
-{
-  function Keyword()
-  {
-    SpecialPage::SpecialPage("Keyword");
-  }
-
-  function execute($par)
-  {
-    global $wgRequest, $wgOut;
-//    sleep(1);  // throttle ...
-
-
-    $post = "<p />
-      <form action=\"/Spezial:Keyword\" method=\"get\">
-      <b>Keyword: </b>
-      <input type=\"text\" name=\"kw\" size=\"40\" maxlength=\"80\" />
-      <input type=\"submit\" value=\" suchen \" />
-      </form><p />
-      <b>Liste der <a href=\"/Spezial:Keyword?target=common\">h&auml;ufigsten Keywords</a></b><p />";
-
-    $output= "";
-    $this->setHeaders();
-    $wgOut->setRobotPolicy('index,follow');
-
-    // Google
-      $google = '<table border="1" cellspacing="0" cellpadding="4" style="float:right; margin:0 0 .5em 1em;
-     width:260px; background:#fff; border-collapse:collapse; border:1px solid #999;
-     font-size:smaller; line-height:1.5; " summary="Anzeigen"><tr><td align="center" height="250" style="background:#ffffff;">';
-      $google .= AdEngine::getInstance()->getAd( 'WEBSITEWIKI_KEYWORDS' );
-      $google .= '</td></tr></table>';
-
-    # Get request data from, e.g.
-    $kwp = $wgRequest->getText('kw');
-    $tar = $wgRequest->getText('target');
-    $doall = $wgRequest->getText('all');
-    $kwt = substr(strstr($wgRequest->getVal('title'), "/"), 1);
-    $kw = $kwp ? $kwp : $kwt;
-
-    # Do stuff
-
-    $dbr = wfGetDB( DB_SLAVE, "vslow" );
-
-    if($tar == "common")
-    {
-      $wgOut->setPagetitle( "Häufigste Keywords" );
-      $wgOut->addHTML("H&auml;ufigste Keywords:<p><ol>\n");
-
-      $res = $dbr->doQuery("select kw_word, kw_count from kw_keywords order by kw_count desc limit 100");
-      while($res && $row = mysql_fetch_row($res))
-      {
-	$kword = $row[0];
-	$kcount = $row[1];
-        $output .= "<li>" . kwlink($kword) . " ($kcount)</li>\n";
-      }
-      $output .= "</ol>\n";
-      $wgOut->addHTML( $google.$output );
-
-      return;
-    }
-
-    if(!isset($kw) || strlen($kw) < 2)
-    {
-      $wgOut->addHTML("$post");
-      return;
-    }
-
-    $keyword = strtolower(trim(str_replace("_", " ", $kw)));
-    $gnkeyword = $keyword;
-    $keyword{0} = strtoupper($keyword{0});
-
-    $wgOut->setPagetitle( "Keyword $keyword" );
-
-    $kw = $dbr->strencode(strtolower(trim(str_replace("_", " ", $kw))));
-
-    $res = $dbr->doQuery("select count(*) from kw_keywords");
-    if($res && $row = mysql_fetch_row($res))
-      $diffkeys = $row[0];
-    else
-    {
-      $wgOut->addHTML("Fehler bei der Keywordausz&auml;hlung.");
-      return;
-    }
-
-    $res = $dbr->doQuery("select kw_count from kw_keywords order by kw_count desc limit 1");
-    if($res && $row = mysql_fetch_row($res))
-      $maxkeycount = $row[0];
-    else
-    {
-      $wgOut->addHTML("Fehler bei der Keywordausz&auml;hlung.");
-      return;
-    }
-
-    $res = $dbr->doQuery("select kw_count, kw_id from kw_keywords where kw_word='$kw'");
-    if($res && $row = mysql_fetch_row($res))
-    {
-      $thiskeycount = $row[0];
-      $thiskeyid = $row[1];
-    }
-    else
-    {
-      $wgOut->addHTML("Das Keyword <i>$keyword</i> wird von keiner Seite im WebsiteWiki verwendet.");
-      return;
-    }
-
-    $thisrelcomm = round(($thiskeycount / $maxkeycount * 100), 2);
-
-    $output .= "<ul>\n";
-    $output .= "<li>Das Keyword <i>$keyword</i> wird $thiskeycount mal auf Websites im WebsiteWiki verwendet.</li>\n";
-    $output .= "<li>Suchen von <i><a href=\"http://www.gnomit.com/search/$gnkeyword\">$keyword</a></i> bei <a href=\"http://www.gnomit.com/\">Gnomit</a>.</li>\n";
-    $output .= "<li>Seine relative <a href=\"/Spezial:Keyword?target=common\">H&auml;ufigkeit</a> betr&auml;gt $thisrelcomm %.</li>\n";
-
-    $output .= "<li>Verwandte Keywords:</li>\n<li><ul>\n";
-
-    $res = $dbr->doQuery("select kw_page,kw_ptype from kw_page where kw_key = $thiskeyid limit 50;");  // 50 was 100
-    $similpages = array();
-    $porncount = 0;
-    while($res && $row = mysql_fetch_row($res))
-    {
-      $similpages[] = $row[0];
-      $porncount += $row[1];
-    }
-    // $output .= "<li>npc = $porncount</li>\n";
-
-    $similstring = '(';
-    foreach( $similpages as $spage)
-      $similstring .= $spage . ",";
-
-    $similstring .= '0)';
-
-    $res = $dbr->doQuery("select kw_word from kw_keywords,kw_page where kw_page in $similstring  and kw_key=kw_id group by kw_key order by count(kw_key) desc limit 15 offset 1");
-    while($res && $row = mysql_fetch_row($res))
-    {
-      $sameword = $row[0];
-      $output .= "<li>" . kwlink($sameword) . "</li>\n";
-    }
-    $output .= "</ul></li>\n";
-
-    if(!$doall && $thiskeycount > 25)
-    {
-      srand($thiskeyid);
-      $off = rand(0, $thiskeycount - 25);
-      $ua = "u.a.";
-      $alleanz = " [<a href=\"?all=1\">alle anzeigen</a>]";
-      $limi = "limit 25 offset $off";
-//      sleep(2);  // throttle more ...
-    }
-    else
-    {
-      $ua = "";
-      $off = 0;
-      $alleanz = "";
-      $limi = "";
-    }
-
-    // randomized, slow $res = $dbr->doQuery("select page_title from kw_page,page where kw_key=$thiskeyid and page_id=kw_page order by rand($thiskeyid) limit 25");
-
-    $res = $dbr->doQuery("select page_title from kw_page,page where kw_key=$thiskeyid and page_id=kw_page $limi");
-    $output .= "<li><i>$keyword</i> wird $ua auf folgenden Websites verwendet:$alleanz</li>\n<li><ul>\n";
-    while($res && $row = mysql_fetch_row($res))
-    {
-      $pgtitle = $row[0];
-      $output .= "<li><a href=\"/" . htmlspecialchars($pgtitle) . "\">$pgtitle</a></li>\n";
-    }
-    $output .= "</ul></li>\n";
-    $output .= "</ul>\n";
-
-//     $output .= "KW: $kw num $diffkeys this $thiskeycount id $thiskeyid max $maxkeycount\n";
-
-    if($porncount == 0)
-      $wgOut->addHTML( $google.$output );
-    else
-      $wgOut->addHTML( $output );
-  }
-
-  function loadMessages()
-  {
-	  static $messagesLoaded = false;
-	  global $wgMessageCache;
-	  if ( $messagesLoaded ) return;
-	  $messagesLoaded = true;
-
-	  require( dirname( __FILE__ ) . '/Keyword.i18n.php' );
-	  foreach ( $kwallMessages as $lang => $langMessages ) {
-		  $wgMessageCache->addMessages( $langMessages, $lang );
-	  }
-  }
-}
-?>
