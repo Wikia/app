6a6ba8a7b3c966e114ef0a86cdf699f68b0a627c
tor <tor@wikia-inc.com>
2010-04-01 18:49:31 +0000

    Sponsoring: removed section edit link
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20706 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialSponsorPage/Advertisements.php
+++ extensions/wikia/SpecialSponsorPage/Advertisements.php
@@ -241,9 +241,7 @@ Class AdDisplay
   public static function OutputAdvertisementOutputHook( &$out, &$text ){
 		if(!self::ArticleCanShowAd()) return true;
 		global $wgParser, $wgTitle;
-		$wtext = "==External Sponsored Links==\n";
-		$parser = $wgParser->parse($wtext, $wgTitle, new ParserOptions());
-		$text.= $parser->getText();
+		$text.= "\n<h2>External Sponsored Links</h2>\n";
 		$text.= self::OutputAdvertisement();
 		return true;
   }
