cab250a5aaff36c3988aa63f7e1d993e09a4b9f6
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-01 15:24:13 +0000

    wgStyle++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20699 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20653';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20697';
 
 /**
  * @name $wgCityId
