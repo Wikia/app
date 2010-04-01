f72b335a33854309f9c6e29192fdabe7c1b20f0e
inez <inez@wikia-inc.com>
2010-04-01 14:19:37 +0000

    * Fix for RT#37695
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20692 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RTE/js/plugins/link/plugin.js
+++ extensions/wikia/RTE/js/plugins/link/plugin.js
@@ -42,6 +42,35 @@ CKEDITOR.plugins.add('rte-link',
 	}
 });
 
+CKEDITOR.plugins.link =
+{
+	/**
+	 *  Get the surrounding link element of current selection.
+	 * @param editor
+	 * @example CKEDITOR.plugins.link.getSelectedLink( editor );
+	 * @since 3.2.1
+	 * The following selection will all return the link element.
+	 *	 <pre>
+	 *  <a href="#">li^nk</a>
+	 *  <a href="#">[link]</a>
+	 *  text[<a href="#">link]</a>
+	 *  <a href="#">li[nk</a>]
+	 *  [<b><a href="#">li]nk</a></b>]
+	 *  [<a href="#"><b>li]nk</b></a>
+	 * </pre>
+	 */
+	getSelectedLink : function( editor )
+	{
+		var range;
+		try { range  = editor.getSelection().getRanges()[ 0 ]; }
+		catch( e ) { return null; }
+
+		range.shrink( CKEDITOR.SHRINK_TEXT );
+		var root = range.getCommonAncestor();
+		return root.getAscendant( 'a', true );
+	}
+};
+
 CKEDITOR.unlinkCommand = function(){};
 CKEDITOR.unlinkCommand.prototype =
 {
