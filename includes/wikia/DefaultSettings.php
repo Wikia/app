e6e83d083366e5fbee22f88ad8aec7a94a1f8ce8
sean <sean@wikia-inc.com>
2010-04-01 16:46:00 +0000

    Bump wgStyleVersion.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20702 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20697.3';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20701';
 
 /**
  * @name $wgCityId
