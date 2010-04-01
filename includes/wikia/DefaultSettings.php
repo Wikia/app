b1fc339490c36d5fca257f0c9e7a7e3c23060ca9
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-01 15:59:07 +0000

    wgStyle++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20701 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20697';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20697.3';
 
 /**
  * @name $wgCityId
