889975c4762e6cd13095c8332af8bd976ddce56d
macbre <macbre@wikia-inc.com>
2010-04-02 11:24:09 +0000

    IE6PhaseOut: and a bit of credits
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20723 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/IE6PhaseOut/IE6PhaseOut_setup.php
+++ extensions/wikia/IE6PhaseOut/IE6PhaseOut_setup.php
@@ -1,7 +1,20 @@
 <?php
 /**
+ * IE6PhaseOut
+ *
  * This extension shows message for IE6 users (RT #44794)
+ *
+ * @author Maciej Brencz (Macbre) <macbre at wikia-inc.com>
+ *
+ * To activate this functionality, place this file in your extensions/
+ * subdirectory, and add the following line to LocalSettings.php:
+ *     require_once("$IP/extensions/wikia/IE6PhaseOut/IE6PhaseOut_setup.php");
  */
+$wgExtensionCredits['other'][] = array(
+	'name' => 'IE6PhaseOut',
+	'author' => 'Maciej Brencz',
+	'description' => 'Shows message for Internet Explorer 6 users',
+);
 
 $dir = dirname(__FILE__) . '/';
 
