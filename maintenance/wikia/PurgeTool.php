2c3a5acc81708b0429cc3b78213633086b1f831d
sean <sean@wikia-inc.com>
2010-04-02 22:25:12 +0000

    Typofix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20742 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/PurgeTool.php
+++ maintenance/wikia/PurgeTool.php
@@ -5,7 +5,7 @@ ini_set( "include_path", dirname(__FILE__)."/../.." );
 require_once( 'commandLine.inc' );
 
 $urls = array(
-	"http://swfanon.wikia.com/wiki/File:IchiGo.jpg";
+	"http://swfanon.wikia.com/wiki/File:IchiGo.jpg",
 	"http://images1.wikia.nocookie.net/__cb20100331234651/swfanon/images/6/60/LinkFA-star.png",
 	"http://swfanon.wikia.com/wiki/File:LinkFA-star.png?action=purge",
 	"http://images1.wikia.nocookie.net/__cb20100402220156/swfanon/images/d/da/IchiGo.jpg"
