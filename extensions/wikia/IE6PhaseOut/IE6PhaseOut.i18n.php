3b3eaef818a9e0e243db83880436bfa592e0bd8b
macbre <macbre@wikia-inc.com>
2010-04-02 10:00:28 +0000

    IE6PhaseOut: add message for Internet Explorer 6 users (RT #44794) - first commit
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20720 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/IE6PhaseOut/IE6PhaseOut.i18n.php
@@ -0,0 +1,7 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'ie6-phaseout-message' => 'We are phasing out support of your browser, IE 6. Continuing to use this browser may result in some a lack of functionality on Wikia. Please consider upgrading to one of these browsers.. [http://foo.bar link], link, link',
+);
