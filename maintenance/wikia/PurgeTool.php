1ec3b5e995023378dd5ac55db309981f7e245f36
sean <sean@wikia-inc.com>
2010-04-02 22:23:19 +0000

    Emergency maintenance script... may be a duplicate of other code.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20741 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/PurgeTool.php
@@ -0,0 +1,24 @@
+<?php
+
+
+ini_set( "include_path", dirname(__FILE__)."/../.." );
+require_once( 'commandLine.inc' );
+
+$urls = array(
+	"http://swfanon.wikia.com/wiki/File:IchiGo.jpg";
+	"http://images1.wikia.nocookie.net/__cb20100331234651/swfanon/images/6/60/LinkFA-star.png",
+	"http://swfanon.wikia.com/wiki/File:LinkFA-star.png?action=purge",
+	"http://images1.wikia.nocookie.net/__cb20100402220156/swfanon/images/d/da/IchiGo.jpg"
+);
+
+
+include 'includes/SquidUpdate.php';
+
+for($cnt=0; $cnt< count($urls); $cnt++){
+	$url = $urls[$cnt];
+	print "Purging $url...\n";
+	SquidUpdate::StompPurge($url);
+}
+
+print "Done.\n";
+
