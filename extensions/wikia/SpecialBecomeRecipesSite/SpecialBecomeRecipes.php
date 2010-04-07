017eac803ce93131262392c9c9cee0b81ad08839
sean <sean@wikia-inc.com>
2010-04-07 05:54:30 +0000

    Code complete but completely untested code for converting a normal wiki into a recipes wiki.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20790 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialBecomeRecipesSite/SpecialBecomeRecipes.php
+++ extensions/wikia/SpecialBecomeRecipesSite/SpecialBecomeRecipes.php
@@ -33,14 +33,87 @@ function wfBecomeRecipesSpecial( $par ) {
 	global $wgOut, $wgUser, $wgRequest ;
    	$wgOut->setPageTitle ("Become a Recipes Wiki");
 
+	// Detect if this is a recipes site...
+	$isAlreadyRecipes = false;
+	$dbr = wfGetDB(DB_MASTER)->;
+	$queryString = "SHOW COLUMNS FROM watchlist";
+	$result = $dbr->query($queryString, __METHOD__);
+	if (!$result) {
+		echo 'Could not run query: ' . mysql_error();
+	} else if (mysql_num_rows($result) > 0) {
+		while ($row = mysql_fetch_assoc($result)) {
+			if($row['Field'] == "wl_wikia_addedtimestamp"){
+				$isAlreadyRecipes = true;
+			}
+		}
+	}
 	
-	// TODO: Detect if this is a recipes site...
-	
-	
-	// TODO: If this is not yet a recipes site, perform the database upgrades
-	// TODO: If this is not yet a recipes site, change the messages (as the current user).
-	
-	// TODO: Display a message that either says that the conversion is now done, or that it was ALREADY done and doesn't need to be done again.
-	
-	
-}
+	if($isAlreadyRecipes){
+		$wgOut->addHTML("<div style='border:#0f0 solid 1px;background-color:#cfc'>This database is already set up as a recipes site.  You're good to go!</div>");
+	} else {
+		$numErrors = 0;
+		// Perform database alterations.
+		if(!$dbr->query("ALTER TABLE `watchlist` ADD `wl_wikia_addedtimestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP", __METHOD__)){
+			$wgOut->addHTML("ERROR: Error adding wl_wikia_addedtimestamp column to watchlist table.<br/>\n");
+			$numErrors++;
+		}
+		if(!$dbr->query("ALTER TABLE `watchlist` ADD INDEX ( `wl_wikia_addedtimestamp` )", __METHOD__)){
+			$wgOut->addHTML("ERROR: Error adding INDEX to wl_wikia_addedtimestamp column in watchlist table.<br/>\n");
+			$numErrors++;
+		}
+		$wgOut->addHTML("Database alterations complete.<br/>\n");
+
+		// Change certain messages.
+		$numErrors += setMessage("MediaWiki:Watch", "Save this page");
+		$numErrors += setMessage("MediaWiki:Unwatch", "Remove");
+		$numErrors += setMessage("MediaWiki:watching", "Saving");
+		$numErrors += setMessage("MediaWiki:unwatching", "Removing");
+		$numErrors += setMessage("MediaWiki:addedwatchtext", "The page \"[[$1]]\" has been added to your Saved Pages");
+		$numErrors += setMessage("MediaWiki:removedwatchtext", "The page \"[[$1]]\" has been removed from your Saved Pages");
+		$numErrors += setMessage("MediaWiki:sf-link", "Share this page");
+		$numErrors += setMessage("MediaWiki:Talkpage", "Messages");
+		$numErrors += setMessage("MediaWiki:Article-comments-comments", "Add a Tip or Comment");
+		$numErrors += setMessage("MediaWiki:Article-comments-post", "Post");
+		$numErrors += setMessage("MediaWiki:Widget-title-community", "New recipes");
+		$wgOut->addHTML("Messages updated.<br/>\n");
+
+		// Success xor error message.
+		if($numErrors == 0){
+			$wgOut->addHTML("<div style='border:#0f0 solid 1px;background-color:#cfc'>Setup complete.  This wiki will now function as a recipes site.</div>");
+		} else {
+			$wgOut->addHTML("<div style='border:#f00 solid 1px;background-color:#fcc'>There were errors while trying to become a recipes wiki.  Please see above.</div>");
+		}
+	}
+
+} // end wfBecomeRecipesSpecial()
+
+/**
+ * Given the name of the message (with preceding "MediaWiki:"), updates it's contents
+ * to be the content passed in.  This happens as an edit from the currently logged in user.
+ *
+ * Returns an integer that is the number of errors (so 0 on success and one on failure).
+ */
+function setMessage($msgName, $content){
+	global $wgOut;
+	$numErrors = 0;
+	$wgTitle = Title::newFromText( $msgName );
+	if ( !$wgTitle ) {
+		$wgOut->addHTML("ERROR: Invalid title: \"$msgName\"<br/>\n");
+		$numErrors++;
+	} else {
+		$wgArticle = new Article( $wgTitle );
+
+		# Do the edit
+		$flags = 0;
+		$summary = "Switching to Recipes messaging.";
+		$success = $wgArticle->doEdit( $text, $summary, $flags);
+		if ( $success ) {
+			$wgOut->addHTML("Sucessfully updated \"$msgName\".<br/>\n");
+		} else {
+			$wgOut->addHTML("ERROR: Unable to update \"$msgName\".<br/>\n");
+			$numErrors++;
+		}
+	}
+
+	return $numErrors;
+} // end setMessage()
