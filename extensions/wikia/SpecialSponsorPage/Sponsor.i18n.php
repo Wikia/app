2c10016c4bffef1ddcc5af6ddd752a61d744e233
tor <tor@wikia-inc.com>
2010-04-07 11:49:27 +0000

    Sponsors: added messages
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20811 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialSponsorPage/Sponsor.i18n.php
@@ -0,0 +1,11 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'sponsor-header' => '<h2>External Sponsor Links</h2>',
+	'sponsor-msg' => '<div class="sponsormsg" style="border:1px black solid; padding:2em;">
+<p><a href="$1">Sponsor this page</a></p>
+</div>',
+	'sponsor-template' => '<p><a href="$1">$2</a><br />$3</p>',
+);
