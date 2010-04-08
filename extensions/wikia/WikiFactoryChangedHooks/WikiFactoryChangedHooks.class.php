bb8bd5b28196eeea918a0b342f178a256172a807
sean <sean@wikia-inc.com>
2010-04-08 18:49:25 +0000

    trunk: Pulled in the changes from release branch which fix rt#45538 and the errant "bullet" that was showing up on recipes sites.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20885 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactoryChangedHooks/WikiFactoryChangedHooks.class.php
+++ extensions/wikia/WikiFactoryChangedHooks/WikiFactoryChangedHooks.class.php
@@ -23,33 +23,78 @@ Class WikiFactoryChangedHooks {
 			// Detect if this is a recipes site...
 			$isAlreadyRecipes = false;
 			$dbName = WikiFactory::IDtoDB($city_id);
-			$dbr = wfGetDB(DB_MASTER, array(), $dbName);
+			$dbr = &wfGetDB(DB_MASTER, 'wikifactory', $dbName);
 			$queryString = "SHOW COLUMNS FROM watchlist";
-			$result = $dbr->query($queryString, __METHOD__);
-			if (!$result) {
-				print 'Could not run query: ' . mysql_error() . "<br/>\nQuery: $queryString";
-			} else if (mysql_num_rows($result) > 0) {
-				while ($row = mysql_fetch_assoc($result)) {
-					if($row['Field'] == "wl_wikia_addedtimestamp"){
-						$isAlreadyRecipes = true;
-					}
+			$res = $dbr->query($queryString, __METHOD__);
+			foreach( $res as $row ) {
+				if($row->Field == "wl_wikia_addedtimestamp"){
+					$isAlreadyRecipes = true;
 				}
 			}
 
-			if($isAlreadyRecipes){
-				$wgOut->addHTML("<div style='border:#0f0 solid 1px;background-color:#cfc'>This database is already set up as a recipes site.  You're good to go!</div>");
-			} else {
+			if(!$isAlreadyRecipes){
+				$numErrors = 0;
+				// Perform database alterations. - For the moment, these will throw exceptions if they don't work.  To change that, pass 'true' as third parameter.
+				if(!$dbr->query("ALTER TABLE `watchlist` ADD `wl_wikia_addedtimestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP", __METHOD__)){
+					$numErrors++;
+				}
+				if(!$dbr->query("ALTER TABLE `watchlist` ADD INDEX ( `wl_wikia_addedtimestamp` )", __METHOD__)){
+					$numErrors++;
+				}
 
+				// Change certain messages.
+				$numErrors += self::setMessage("MediaWiki:Watch", "Save this page");
+				$numErrors += self::setMessage("MediaWiki:Unwatch", "Remove");
+				$numErrors += self::setMessage("MediaWiki:watching", "Saving");
+				$numErrors += self::setMessage("MediaWiki:unwatching", "Removing");
+				$numErrors += self::setMessage("MediaWiki:addedwatchtext", "The page \"[[$1]]\" has been added to your Saved Pages");
+				$numErrors += self::setMessage("MediaWiki:removedwatchtext", "The page \"[[$1]]\" has been removed from your Saved Pages");
+				$numErrors += self::setMessage("MediaWiki:sf-link", "Share this page");
+				$numErrors += self::setMessage("MediaWiki:Talkpage", "Messages");
+				$numErrors += self::setMessage("MediaWiki:Article-comments-comments", "Add a Tip or Comment");
+				$numErrors += self::setMessage("MediaWiki:Article-comments-post", "Post");
+				$numErrors += self::setMessage("MediaWiki:Widget-title-community", "New recipes");
 
-// TODO: Once this part is tested, start merging in the changes from SpecialBecomeRecipes.
-$wgOut->addHTML("Not a recipes site yet... would perform upgrades here.");
-
-
+				// Success xor error message.
+				//if($numErrors == 0){
+				//} else {
+				//}
 			}
-
 		}
+
 		wfProfileOut( __METHOD__ );
 		return true;
 	}
+
+	/**
+	 * Given the name of the message (with preceding "MediaWiki:"), updates it's contents
+	 * to be the content passed in.  This happens as an edit from the currently logged in user.
+	 *
+	 * Returns an integer that is the number of errors (so 0 on success and one on failure).
+	 */
+	private static function setMessage($msgName, $content){
+		global $wgOut;
+		$numErrors = 0;
+		$wgTitle = Title::newFromText( $msgName );
+		if ( !$wgTitle ) {
+			$wgOut->addHTML("ERROR: Invalid title: \"$msgName\"<br/>\n");
+			$numErrors++;
+		} else {
+			$wgArticle = new Article( $wgTitle );
+
+			# Do the edit
+			$flags = 0;
+			$summary = "Switching to Recipes messaging.";
+			$success = $wgArticle->doEdit( $text, $summary, $flags);
+			if ( $success ) {
+				$wgOut->addHTML("Sucessfully updated \"$msgName\".<br/>\n");
+			} else {
+				$wgOut->addHTML("ERROR: Unable to update \"$msgName\".<br/>\n");
+				$numErrors++;
+			}
+		}
+
+		return $numErrors;
+	} // end setMessage()
 
 }
