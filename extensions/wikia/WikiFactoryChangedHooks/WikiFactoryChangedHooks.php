19d1694e577a506737e8bc670dd143d8c8a02b9e
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-08 18:49:25 +0000

    trunk: Pulled in the changes from release branch which fix rt#45538 and the errant "bullet" that was showing up on recipes sites.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20885 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactoryChangedHooks/WikiFactoryChangedHooks.php
+++ extensions/wikia/WikiFactoryChangedHooks/WikiFactoryChangedHooks.php
@@ -6,5 +6,7 @@
  * when the extensions that they pertain to aren't available) they have all been aggregated here.
  */
 
+$wgAutoloadClasses['WikiFactoryChangedHooks'] = dirname(__FILE__)."/WikiFactoryChangedHooks.class.php";
+
 // Alphabetical
 $wgHooks['WikiFactoryChanged'][] = 'WikiFactoryChangedHooks::recipesTweaks';
