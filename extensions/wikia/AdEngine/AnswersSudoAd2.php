7088215b35f52e7c4b162e16b3f20c56bb0f7534
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-09 02:47:09 +0000

    rt#45716: hack in google code @three answers sites
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20891 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AdEngine/AnswersSudoAd2.php
@@ -0,0 +1,24 @@
+<?php
+
+$wgHooks["fillInAdPlaceholder"][] = "wfAnswersSudoAd2";
+
+function wfAnswersSudoAd2($placeholdertype, $slotname, $AdEngine, $html) {
+	if (("HOME_TOP_LEADERBOARD" != $slotname) && ("TOP_LEADERBOARD" != $slotname)) return true;
+
+	global $wgUser;
+	if (empty($_GET["showads"]) && is_object($wgUser) && $wgUser->isLoggedIn() && !$wgUser->getOption("showAds")) return true;
+					 
+	$html = '<div id="' . htmlspecialchars($slotname) . '" class="noprint">
+		<script type="text/javascript"><!--
+			google_ad_client = "pub-1541662546603203";
+			/* Wikia 728x90 */
+			google_ad_slot = "1429918703";
+			google_ad_width = 728;
+			google_ad_height = 90;
+			//-->
+		</script>
+		<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+	</div>';
+
+	return true;
+}
