55b8836d3d7630bd3a80a72e609d25ab64e83cf3
macbre <macbre@wikia-inc.com>
2010-04-09 11:40:09 +0000

    IE6PhaseOut: add texts and links to IE6 phase out messages (RT #45768)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20909 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/IE6PhaseOut/IE6PhaseOut.i18n.php
+++ extensions/wikia/IE6PhaseOut/IE6PhaseOut.i18n.php
@@ -3,5 +3,56 @@
 $messages = array();
 
 $messages['en'] = array(
-	'ie6-phaseout-message' => 'We are phasing out support of your browser, IE 6. Continuing to use this browser may result in some a lack of functionality on Wikia. Please consider upgrading to one of these browsers.. [http://foo.bar link], link, link',
+	'ie6-phaseout-message' => 'We are phasing out support of your browser, Internet Explorer 6. Continuing to use this browser may result in some a lack of functionality on Wikia. Please consider upgrading to one of these browsers:
+* Firefox: http://www.firefox.com
+* Chrome: http://www.google.com/chrome/
+* Safari: http://www.apple.com/safari/
+* Opera: http://www.opera.com/
+* Internet Explorer 8: http://www.microsoft.com/windows/Internet-explorer',
+);
+
+$messages['es'] = array(
+	'ie6-phaseout-message' => 'Usted está usando un navegador obsoleto. Para navegar mejor por este sitio, por favor, actualice su navegador.
+* Firefox: http://www.mozilla-europe.org/es/firefox/
+* Chrome: http://www.google.com/chrome?hl=es
+* Safari: http://www.apple.com/es/safari/download/
+* Opera: http://www.opera.com/download/
+* Internet Explorer: http://www.microsoft.com/downloads/details.aspx?FamilyID=341c2ad5-8c3d-4347-8c03-08cdecd8852b&DisplayLang=es',
+);
+
+
+$messages['de'] = array(
+	'ie6-phaseout-message' => 'Es wird Zeit... wir lassen den Support für den Internet Explorer 6 endgültig auslaufen. Eine weitere Benutzung dieses Programms führt vermutlich zu einem Verlust von Funktionen auf Wikia. Bitte prüfe, ob du nicht auf einen der folgenden Browser upgraden kannst:
+* Firefox: http://www.mozilla-europe.org/de/firefox/
+* Chrome: http://www.google.com/chrome/?hl=de
+* Safari: http://www.apple.com/de/safari/
+* Opera: http://www.opera.com/
+* Internet Explorer: http://www.microsoft.com/germany/windows/internet-explorer/',
+);
+
+$messages['pl'] = array(
+	'ie6-phaseout-message' => 'Zdecydowaliśmy się wycofać wsparcie dla przeglądarki Internet Explorer 6. Dalsze korzystanie z niej może spowodować, że niektóre funkcje serwisu Wikia będą niedostępne. Prosimy rozważyć uaktualnienie do jednej z poniższych przeglądarek:
+* Firefox: http://www.mozilla-europe.org/pl/firefox/
+* Chrome: http://www.google.com/chrome/?hl=pl
+* Safari: http://www.apple.com/pl/safari/
+* Opera: http://www.opera.com/download/
+* Internet Explorer: http://www.microsoft.com/poland/windows/internet-explorer/',
+);
+
+$messages['it'] = array(
+	'ie6-phaseout-message' => 'Stai usando un browser obsoleto. Per una migliore navigazione su questo sito, per cortesia passa ad un browser di ultima generazione.
+* Firefox: http://www.mozilla-europe.org/it/firefox/
+* Chrome: http://www.google.com/chrome/?hl=it
+* Safari: http://www.apple.com/it/safari/
+* Opera: http://www.opera.com/
+* Internet Explorer: http://www.microsoft.com/italy/windows/internet-explorer/',
+);
+
+$messages['fr'] = array(
+	'ie6-phaseout-message' => 'Vous utilisez un navigateur dépassé depuis près de 8 ans! Pour une meilleure expérience web, prenez le temps de mettre votre navigateur à jour.
+* Firefox: http://fr.www.mozilla.com/fr/
+* Chrome: http://www.google.com/chrome?hl=fr
+* Safari: http://www.apple.com/fr/safari/download/
+* Opera: http://www.opera.com/download/
+* Internet Explorer: http://www.microsoft.com/downloads/details.aspx?FamilyID=341c2ad5-8c3d-4347-8c03-08cdecd8852b&DisplayLang=fr',
 );
