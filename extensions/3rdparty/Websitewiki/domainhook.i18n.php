0904b6d020c9a4679739cb2c55bbe00b77dd730e
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-12 19:42:47 +0000

    Websitewiki: message fix (#46099)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20956 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/Websitewiki/domainhook.i18n.php
+++ extensions/3rdparty/Websitewiki/domainhook.i18n.php
@@ -4,7 +4,7 @@ $messages = array();
 
 $messages['en'] = array(
 	'websiteinfo-title' => 'Website info for $1',
-	'websiteinfo-summary-link' => '<a href=\"/Website_info\">Website info</a> for $1',
+	'websiteinfo-summary-link' => '<a href="/Website_info">Website info</a> for $1',
 	'websiteinfo-screenshot-alt' => 'Screenshot of $1',
 	'websiteinfo-bookmarks' => 'Bookmarken using...',
 	'websiteinfo-verify' => 'Verify',
@@ -15,7 +15,7 @@ $messages['en'] = array(
 
 $messages['de'] = array(
 	'websiteinfo-title' => 'Website-Infos f&uuml;r $1',
-	'websiteinfo-summary-link' => '<a href=\"/Website-Infos\">Website-Infos</a> f&uuml;r $1',
+	'websiteinfo-summary-link' => '<a href="/Website-Infos">Website-Infos</a> f&uuml;r $1',
 	'websiteinfo-screenshot-alt' => 'Screenshot $1',
 	'websiteinfo-bookmarks' => 'Bookmarken bei ...',
 	'websiteinfo-verify' => '&Uuml;berpr&uuml;fen',
