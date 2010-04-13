dce05807bfea7b525e62b20caafa54a37d635f15
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-13 14:50:38 +0000

    preliminary version of isValidDomain method
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@20999 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/Wikia.php
+++ includes/wikia/Wikia.php
@@ -1059,7 +1059,7 @@ class Wikia {
 		wfProfileOut(__METHOD__);
 		return $domains;
 	}
-	
+
 	/**
 	 * fixed answers sitenames
 	 */
@@ -1078,6 +1078,22 @@ class Wikia {
 		}
 		wfProfileOut(__METHOD__);
 		return $result;
+	}
+
+	/**
+	 * check if domain is valid according to some known standards
+	 * (it is not very strict checking)
+	 *
+	 * @author Krzysztof Krzyżaniak (eloy)
+	 * @access public
+	 * @static
+	 *
+	 * @param String $domain -- domain name for checking
+	 *
+	 * @return Boolean  -- true if valid, false otherwise
+	 */
+	public static function isValidDomain( $domain ) {
+		return (bool )preg_match("/^([a-z0-9]([-a-z0-9]*[a-z0-9])?\\.)+((a[cdefgilmnoqrstuwxz]|aero|arpa)|(b[abdefghijmnorstvwyz]|biz)|(c[acdfghiklmnorsuvxyz]|cat|com|coop)|d[ejkmoz]|(e[ceghrstu]|edu)|f[ijkmor]|(g[abdefghilmnpqrstuwy]|gov)|h[kmnrtu]|(i[delmnoqrst]|info|int)|(j[emop]|jobs)|k[eghimnprwyz]|l[abcikrstuvy]|(m[acdghklmnopqrstuvwxyz]|mil|mobi|museum)|(n[acefgilopruz]|name|net)|(om|org)|(p[aefghklmnrstwy]|pro)|qa|r[eouw]|s[abcdeghijklmnortvyz]|(t[cdfghjklmnoprtvwz]|travel)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])$/i", $domain );
 	}
 
 }
