5e0b4bf70b8d7c8480487354cf135edaab1a76f8
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-21 16:44:21 +0000

    not used table
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21303 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/newsite/related.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `related` (
-  `name1` varchar(128) NOT NULL default '',
-  `name2` varchar(128) NOT NULL default '',
-  KEY `name1` (`name1`),
-  KEY `name2` (`name2`)
-);
