6538a6ba8ae9b075ca0b2521e85c2a27f38f38d8
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-22 14:03:46 +0000

    Checksite: added English messages (#46877)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21345 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/Websitewiki/checksite/Checksite.i18n.php
+++ extensions/3rdparty/Websitewiki/checksite/Checksite.i18n.php
@@ -2,26 +2,42 @@
 $messages = array(
         'en' => array(
                 'checksite' => 'CheckSite',
-                'neuewebsite' => 'CheckSite'
+                'neuewebsite' => 'CheckSite',
+		'checksite-description' => "* This special page allows to update the screen shot thumbnail. Screen shots are created ordered by requests - this may need some time if many requests are in the queue. Requesting an update for one website several times does '''not''' expedite the creation!
+* Also robots.txt will be checked if the owner of the website intends to exclude the site from discussions about it.",
+		'checksite-label' => "'''Check website:'''",
+		'checksite-submit' => 'check',
+		'checksite-cant-check' => '$1 cannot be checked.',
+		'checksite-not-found' => 'Cannot find wiki page $1.',
+		'checksite-not-exist' => 'Website <a href="$1">$1</a> does not exist in WebsiteWiki.',
+		'checksite-url-not-found' => 'Cannot find website $1.',
+		'checksite-robots' => 'http://$1/robots.txt suggested $2, which cannot found in Websitewiki.',
+		'checksite-redirect-url' => 'Redirect error',
+		'checksite-redirect-comment' => 'Redirection error',
+		'checksite-screenshot-error' => 'The screenshot could not be refreshed.',
+		'checksite-screenshot-updating' => 'The screenshot for $1 was refreshed.',
+
+                'newmark-submit' => 'submit',
+                'newmark-header' => "'''New Website:'''",
         ),
         'de' => array(
                 'checksite' => 'Website überprüfen',
-				'checksite-description' =>
+		'checksite-description' =>
 '* Mit dieser Spezialseite kann der Screenshot aktualisiert werden. Die Screenshots werden in der Reihenfolge der Anforderung aktualisiert - bei vielen Screenshots kann das etwas dauern. Mehrfache Anforderung beschleunigt den Vorgang nicht.
 * Ausserdem wird anhand von robots.txt überprüft, ob der Inhaber einer Website diese für ungeeignet hält, im WebsiteWiki diskutiert zu werden.',
-				'checksite-label' => "'''Website überprüfen:'''",
-				'checksite-submit' => 'überprüfen',
-				'checksite-cant-check' => '$1 kann nicht überprüft werden.',
-				'checksite-not-found' => '$1 kann nicht als Wikipage gefunden werden.',
-				'checksite-not-exist' => 'Die Website <a href="$1">$1</a> existiert nicht im WebsiteWiki.',
-				'checksite-url-not-found' => 'Die Website $1 wurde nicht gefunden.',
-				'checksite-robots' => 'http://$1/robots.txt empfiehlt, $2 nicht im Websitewiki aufzuführen.',
-				'checksite-redirect-url' => 'Ungeeignet',
-				'checksite-redirect-comment' => 'Redirect: Ungeeignet',
-				'checksite-screenshot-error' => 'Der Screenshot kann nicht aktualisiert werden.',
-				'checksite-screenshot-updating' => 'Der Screenshot von $1 wird aktualisiert.',
+		'checksite-label' => "'''Website überprüfen:'''",
+		'checksite-submit' => 'überprüfen',
+		'checksite-cant-check' => '$1 kann nicht überprüft werden.',
+		'checksite-not-found' => '$1 kann nicht als Wikipage gefunden werden.',
+		'checksite-not-exist' => 'Die Website <a href="$1">$1</a> existiert nicht im WebsiteWiki.',
+		'checksite-url-not-found' => 'Die Website $1 wurde nicht gefunden.',
+		'checksite-robots' => 'http://$1/robots.txt empfiehlt, $2 nicht im Websitewiki aufzuführen.',
+		'checksite-redirect-url' => 'Ungeeignet',
+		'checksite-redirect-comment' => 'Redirect: Ungeeignet',
+		'checksite-screenshot-error' => 'Der Screenshot kann nicht aktualisiert werden.',
+		'checksite-screenshot-updating' => 'Der Screenshot von $1 wird aktualisiert.',
 
-				'newmark-submit' => 'anlegen',
-				'newmark-header' => "'''Neue Website:'''",
+		'newmark-submit' => 'anlegen',
+		'newmark-header' => "'''Neue Website:'''",
         ),
 );
