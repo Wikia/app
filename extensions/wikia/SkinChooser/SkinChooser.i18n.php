e3fcec87911a545199c7c8088e12f270a4ef86c0
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-22 11:17:29 +0000

    SkinChooser: move message to separate i18n  file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21330 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SkinChooser/SkinChooser.i18n.php
@@ -0,0 +1,7 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'skinchooser-customcss' => 'For custom themes, select the custom option in the menu above and specify custom CSS in [[MediaWiki:Monaco.css]].',
+);
