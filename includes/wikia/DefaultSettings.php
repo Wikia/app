aa09d98eae1210a3520fdad7a82f9f4566a3660e
adi <adi@wikia-inc.com>
2010-04-22 08:21:52 +0000

    wgStyleVersion++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21317 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '20738';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '21316';
 
 /**
  * @name $wgCityId
