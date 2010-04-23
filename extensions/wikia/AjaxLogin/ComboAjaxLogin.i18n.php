56718b9eb9b06b8b5a0596627e79e5f050a766b6
tor <tor@wikia-inc.com>
2010-04-23 10:23:12 +0000

    merged r21392 from 04.4
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21393 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
+++ extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
@@ -3,5 +3,10 @@
 $messages = array();
 $messages["en"] = array(
 	"comboajaxlogin-createlog" => "Log in or create an account",
-	"comboajaxlogin-actionmsg" => "To perform this action you first need to log in or create an account"
+	"comboajaxlogin-actionmsg" => "To perform this action you first need to log in or create an account",
+);
+
+$messages["pl"] = array(
+	"comboajaxlogin-createlog" => "Zaloguj się lub utwórz konto",
+	"comboajaxlogin-actionmsg" => "Aby wykonać tę akcję musisz się najpierw zalogować lub zarejestrować konto",
 );
