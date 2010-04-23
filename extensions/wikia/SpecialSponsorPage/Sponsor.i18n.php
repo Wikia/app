ad2ac3b5d0e8c282ea6be558cbb09bd3ee536ff7
tor <tor@wikia-inc.com>
2010-04-23 15:50:30 +0000

    Sponsor msg fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21441 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialSponsorPage/Sponsor.i18n.php
+++ extensions/wikia/SpecialSponsorPage/Sponsor.i18n.php
@@ -5,5 +5,5 @@ $messages = array();
 $messages['en'] = array(
 	'sponsor-header' => '<h2>External Sponsor Links</h2>',
 	'sponsor-msg' => '<li><a href="$1">Sponsor this page</a></li>',
-	'sponsor-template' => '<li><a href="$1">$2</a><br />$3</li>',
+	'sponsor-template' => '<li><a href="http://$1">$2</a><br />$3</li>',
 );
