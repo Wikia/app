c5c5246a97cd9e44d4c0245ade04078d8ca23e28
tomek <tomek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-26 13:22:58 +0000

    sigle column on follow in user page
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21499 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Follow/templates/followedUserPage.tmpl.php
+++ extensions/wikia/Follow/templates/followedUserPage.tmpl.php
@@ -15,13 +15,6 @@
 			<li><a href="<?php echo $value['url']; ?>" ><?php echo $value['wl_title']; ?></a> </li>
 		<?php endforeach; ?>
 	</ul>
-	<?php if (!empty($data2)): ?>
-		<ul class="followedList" >
-			<?php foreach($data2 as $value): ?>
-				<li><a href="<?php echo $value['url']; ?>" ><?php echo $value['wl_title']; ?></a> </li>	
-			<?php endforeach; ?>
-		</ul> 
-	<?php endif; ?>
 	<div style="clear: both;"></div>
 	<a id="follow_more_link" class="wikia-button" id="ShowMoreFollowed" href="<?php echo $moreUrl; ?>" rel="nofollow"><?php echo wfMsg('wikiafollowedpages-userpage-more'); ?></a>
 <?php else: ?>
