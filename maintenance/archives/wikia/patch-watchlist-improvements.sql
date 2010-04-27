4d5bc65480fc5d74189f0254318277c4ca06246c
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-27 09:35:23 +0000

    updaters for wikia
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21537 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/archives/wikia/patch-watchlist-improvements.sql
+++ maintenance/archives/wikia/patch-watchlist-improvements.sql
@@ -1,12 +1,8 @@
---relation for blogs pages
 
-CREATE TABLE `blog_listing_relation` ( 
-  `blr_relation`  VARBINARY(255) NOT NULL DEFAULT '',
-  `blr_title` VARBINARY(255) NOT NULL DEFAULT '',
-  `blr_type` ENUM('cat','user'),
-  UNIQUE KEY `wl_user` (`blr_relation`,`blr_title`,`blr_type`),
-  KEY `type_relation` (`blr_relation`,`blr_type`)
-) ENGINE=INNODB DEFAULT CHARSET=BINARY;
 
--- time stamp for watch list 
-alter table watchlist add wl_wikia_addedtimestamp TIMESTAMP DEFAULT NOW();
+-- time stamp for watch list
+ALTER TABLE /*$wgDBprefix*/watchlist
+	ADD `wl_wikia_addedtimestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE /*$wgDBprefix*/watchlist
+	ADD INDEX ( `wl_wikia_addedtimestamp` );
