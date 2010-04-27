a4f634bc37196f972479e2b21fcff1693101281d
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-27 10:05:23 +0000

    name index
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21542 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/archives/wikia/patch-watchlist-improvements.sql
+++ maintenance/archives/wikia/patch-watchlist-improvements.sql
@@ -5,4 +5,4 @@ ALTER TABLE /*$wgDBprefix*/watchlist
 	ADD `wl_wikia_addedtimestamp` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;
 
 ALTER TABLE /*$wgDBprefix*/watchlist
-	ADD INDEX ( `wl_wikia_addedtimestamp` );
+	ADD INDEX wl_wikia_addedtimestamp ( wl_wikia_addedtimestamp );
