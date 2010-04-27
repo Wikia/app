1962b211a102cbb620e0edf853ef95043a06c751
adi <adi@wikia-inc.com>
2010-04-27 13:15:12 +0000

    rss format fixes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21566 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/solr2rss/templates/results.tmpl.php
+++ extensions/wikia/solr2rss/templates/results.tmpl.php
@@ -1,11 +1,11 @@
 <?php echo "<?xml version=\"1.0\"?>\n"; ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
  <channel>
   <title>RSS format of Wikia Solr search results</title>
-  <description>Feed from: <?php echo $url; ?></description>
-  <link><?php echo $url; ?></link>
+  <description>Feed from: <?php echo htmlspecialchars($url); ?></description>
+  <atom:link href="<?php echo htmlspecialchars($url); ?>" rel="self" type="application/rss+xml" />
   <pubDate><?php echo date($dateFormat); ?></pubDate>
-  <generator><?php echo $url; ?></generator>
+  <generator><?php echo htmlspecialchars($url); ?></generator>
   <?php foreach ($docs as $doc): ?>
    <item>
     <title><?php echo htmlspecialchars($doc->title); ?></title>
