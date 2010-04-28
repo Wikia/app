9fe42f430391229bf3d4ac6063f25bd53db9238f
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-28 09:42:45 +0000

    Q&A: move some filtering functionality from WidgetCategoryCloud to global, and then apply it to Q&A project
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21697 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WidgetFramework/Widgets/WidgetCategoryCloud/WidgetCategoryCloud.php
+++ extensions/wikia/WidgetFramework/Widgets/WidgetCategoryCloud/WidgetCategoryCloud.php
@@ -72,55 +72,16 @@ function WidgetCategoryCloudGetData() {
 
 function WidgetCategoryCloudSkipRow($name) {
 	/* TODO remove when cat_hidden is fixed */
-	if (in_array($name, WidgetCategoryCloudGetHiddenCategories())) return true;
+	if (in_array($name, Wikia::categoryCloudGetHiddenCategories())) return true;
 
 	global $wgBiggestCategoriesBlacklist;
 	if (in_array($name, $wgBiggestCategoriesBlacklist)) return true;
 
 	/* FIXME don't read each time, cache in static var (or class method...) */
-	if (in_array($name, WidgetCategoryCloudMsgToArray("widget-categorycloud-blacklist-global"))) return true;
-	if (in_array($name, WidgetCategoryCloudMsgToArray("widget-categorycloud-blacklist"))) return true;
+	if (in_array($name, Wikia::categoryCloudMsgToArray("widget-categorycloud-blacklist-global"))) return true;
+	if (in_array($name, Wikia::categoryCloudMsgToArray("widget-categorycloud-blacklist"))) return true;
 
 	return false;
-}
-
-/* TODO remove when cat_hidden is fixed */
-function WidgetCategoryCloudGetHiddenCategories() {
-	$data = array();
-
-	wfProfileIn( __METHOD__ );
-
-	global $wgMemc;
-	$key = wfMemcKey("WidgetCategoryCloud", "hidcats");
-	$data = $wgMemc->get($key);
-	if (is_null($data)) {
-		$dbr = wfGetDB( DB_SLAVE );
-		$res = $dbr->select(
-			array("page", "page_props"),
-			array("page_title"),
-			array("page_id = pp_page", "page_namespace" => NS_CATEGORY, "pp_propname" => "hiddencat"),
-			__METHOD__
-		);
-		while ($row = $res->fetchObject()) {
-			$data[] = $row->page_title;
-		}
-		$wgMemc->set($key, $data, 300);
-	}
-
-	wfProfileOut(__METHOD__);
-	return $data;
-}
-
-// FIXME move to global functions
-function WidgetCategoryCloudMsgToArray($key) {
-	$data = array();
-
-	$msg = wfMsg($key);
-	if (!wfEmptyMsg($msg, $key)) {
-		$data = preg_split("/[*\s,]+/", $msg, null, PREG_SPLIT_NO_EMPTY); 
-	}
-
-	return $data;
 }
 
 function WidgetCategoryCloudCloudizeData($data) {
