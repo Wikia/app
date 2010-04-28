b4cd6c1b68698747a7bc7b6719663b17cb63f276
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-28 08:18:33 +0000

    Q&A: answers CT codes added
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21666 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/FounderEmails/js/FounderEmails.js
+++ extensions/wikia/FounderEmails/js/FounderEmails.js
@@ -24,6 +24,27 @@ FounderEmails.track = function( code ) {
 	codes.FE19 = '0days/forums';
 	codes.FE20 = 'editanon/myhome';
 
+	codes.AFE01 = 'answers/userregistered/userpage';
+	codes.AFE02 = 'answers/edit/userpage';
+	codes.AFE03 = 'answers/0days/unsubscribe';
+	codes.AFE04 = 'answers/userregistered/unsubscribe';
+	codes.AFE05 = 'answers/edit/unsubscribe';
+	codes.AFE06 = 'answers/editfirst/userpage';
+	codes.AFE07 = 'answers/editfirst/unsubscribe';
+	codes.AFE08 = 'answers/3days/unsubscribe';
+	codes.AFE09 = 'answers/10days/unsubscribe';
+	codes.AFE10 = 'answers/10days/logoupload';
+	codes.AFE11 = 'answers/10days/logoinstructions';
+	codes.AFE12 = 'answers/10days/changeskins';
+	codes.AFE13 = 'answers/10days/gettingstarted';
+	codes.AFE14 = 'answers/10days/forums';
+	codes.AFE15 = 'answers/3days/myhome';
+	codes.AFE16 = 'answers/3days/gettingstarted';
+	codes.AFE17 = 'answers/3days/forums';
+	codes.AFE18 = 'answers/0days/gettingstarted';
+	codes.AFE19 = 'answers/0days/forums';
+	codes.AFE20 = 'answers/editanon/myhome';
+
 	WET.byStr( '1_emails/founder/' + codes[code] );
 };
 
