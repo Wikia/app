8065c71dd754ee22d62a6a214d8e52b8784026d4
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-28 11:44:21 +0000

    new thumbnailer based on cutycapt
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21708 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WebsiteWiki/thumbnail.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#
+# create thumbnail using cutycapt and imagemagick convert
+#
+#
+# usage thumbnail.sh <url> <target.png>
+
+TMPFILE="`tempfile`".png
+URL="$1"
+TARGET="$2"
+#echo $TMPFILE;
+echo "Getting $URL"
+/usr/bin/cutycapt --url=$URL --out=$TMPFILE --delay=5000
+if [ -f $TMPFILE ];
+then
+	/usr/bin/convert -thumbnail "250x188^" -crop 250x188+0+0 -antialias +repage $TMPFILE $TARGET
+	rm $TMPFILE
+fi
