8c045959ffd5fe4b5ca467e159d7a09911b1f0bf
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-28 14:07:26 +0000

    Q&A: quick change
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21727 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CategoryHubs/CategoryHubs.php
+++ extensions/wikia/CategoryHubs/CategoryHubs.php
@@ -33,12 +33,14 @@ if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
         $wgExtensionFunctions[] = 'CategoryHubs_initParserHook';
 }
 
+
+$wgHooks['BeforePageDisplay'][] = 'categoryHubAdditionalScripts';
+
 if( $wgEnableCategoryHubsExt ) {
 	///// BEGIN - SETUP HOOKS /////
 	$wgHooks['LanguageGetMagic'][] = 'categoryHubAddMagicWords'; // setup names for parser functions (needed here)
 	$wgHooks['ParserAfterStrip'][] = 'categoryHubCheckForMagicWords';
 	// TODO: FIXME: Can this be put somewhere better so that the scripts are only included on CategoryHubs rather than all Answers pages?
-	$wgHooks['BeforePageDisplay'][] = 'categoryHubAdditionalScripts';
 	$wgHooks['MakeGlobalVariablesScript'][] = 'categoryHubJsGlobalVariables';
 
 	// Allows us to define a special order for the various sections on the page.
