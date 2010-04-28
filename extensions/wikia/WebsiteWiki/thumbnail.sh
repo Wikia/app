07dd90ecb05c1550fbdadca040e051057fb0c711
eloy <eloy@wikia-inc.com>
2010-04-28 12:04:33 +0000

    use xvfb-run for cutycapt
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21712 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WebsiteWiki/thumbnail.sh
+++ extensions/wikia/WebsiteWiki/thumbnail.sh
@@ -11,7 +11,7 @@ URL="$1"
 TARGET="$2"
 #echo $TMPFILE;
 echo "Getting $URL"
-/usr/bin/cutycapt --url=$URL --out=$TMPFILE --delay=5000
+ /usr/bin/xvfb-run --server-args="-screen 0, 1024x768x24" /usr/bin/cutycapt --url=$URL --out=$TMPFILE --delay=5000
 if [ -f $TMPFILE ];
 then
 	/usr/bin/convert -thumbnail "250x188^" -crop 250x188+0+0 -antialias +repage $TMPFILE $TARGET
