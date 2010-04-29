60a69f72ba0a5701096f991d9bb725533e996f91
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-29 10:40:16 +0000

    UserProfile: kill notice
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21770 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserProfile_NY/UserProfileClass.php
+++ extensions/wikia/UserProfile_NY/UserProfileClass.php
@@ -15,9 +15,9 @@ class UserProfile {
 	var $user_id;           	# Text form (spaces not underscores) of the main part
 	var $user_name;			# Text form (spaces not underscores) of the main part
 	var $profile;           	# Text form (spaces not underscores) of the main part
-	
+
 	var $profile_fields_count;
-	
+
 	var $profile_fields = array(
 					"real_name","location_city","hometown_city","birthday", "gender",
 					"about","places_lived","websites","occupation","schools",
@@ -26,7 +26,7 @@ class UserProfile {
 					);
 	var $profile_missing = array();
 	static $profile_version = "006";
-	
+
 	/**
 	 * Constructor
 	 * @private
@@ -36,20 +36,20 @@ class UserProfile {
 		$this->user_name = $title1->getText();
 		$this->user_id = User::idFromName($this->user_name);
 	}
-	
-	 
+
+
 	static function clearCache( $user_id ){
 		global $wgMemc;
-		
+
 		$key = wfMemcKey( 'user', 'profile',self::$profile_version, 'work', $user_id );
 		$wgMemc->delete( $key );
 		$key = wfMemcKey( 'user', 'profile',self::$profile_version, 'info', $user_id );
 		$wgMemc->delete( $key );
 	}
-	
+
 	public function getProfile(){
 		global $wgMemc, $wgUserProfileDisplay;
-		
+
 		//try cache first
 		$key = wfMemcKey( 'user', 'profile', self::$profile_version,  'info', $this->user_id );
 		//$wgMemc->delete( $key );
@@ -62,23 +62,23 @@ class UserProfile {
 			wfDebug( "Got user profile info for {$this->user_name} from db\n" );
 			$dbr =& wfGetDB( DB_SLAVE );
 			$params['LIMIT'] = "5";
-			$row = $dbr->selectRow( 'user_profile', 
-				"*", 
+			$row = $dbr->selectRow( 'user_profile',
+				"*",
 				array( 'up_user_id' => $this->user_id ), __METHOD__,
 				$params
 				);
-		
+
 			$profile["user_id"]= $this->user_id;
 			if($row){
 				$profile["gender"]= (isset($row->up_gender)) ? $row->up_gender : 0;
-				$profile["location_city"]= $row->up_location_city;	
-				$profile["location_state"]= $row->up_location_state;	
+				$profile["location_city"]= $row->up_location_city;
+				$profile["location_state"]= $row->up_location_state;
 				$profile["location_country"]= $row->up_location_country;
-				$profile["hometown_city"]= $row->up_hometown_city;	
-				$profile["hometown_state"]= $row->up_hometown_state;	
+				$profile["hometown_city"]= $row->up_hometown_city;
+				$profile["hometown_state"]= $row->up_hometown_state;
 				$profile["hometown_country"]= $row->up_hometown_country;
-				$profile["birthday"]= $this->formatBirthday($row->up_birthday);	
-				$profile["birthyear"]= $this->getBirthdayYear($row->up_birthday);	
+				$profile["birthday"]= $this->formatBirthday($row->up_birthday);
+				$profile["birthyear"]= $this->getBirthdayYear($row->up_birthday);
 				$profile["about"]= $row->up_about;
 				$profile["places_lived"]= $row->up_places_lived;
 				$profile["websites"]= $row->up_websites;
@@ -106,7 +106,7 @@ class UserProfile {
 		}
 
 		//get work information
-		
+
 		if( isset($wgUserProfileDisplay['work']) && $wgUserProfileDisplay['work'] == true ){
 			//try cache first
 			if( $profile["user_id"] ){
@@ -120,28 +120,28 @@ class UserProfile {
 					wfDebug( "Got user profile work for {$this->user_name} from db\n" );
 					$dbr =& wfGetDB( DB_SLAVE );
 					$params['ORDER_BY'] = "w_emp_from";
-					$res = $dbr->select( 'user_profile_work', 
-						"*", 
+					$res = $dbr->select( 'user_profile_work',
+						"*",
 						array( 'w_user_id' => $this->user_id ), __METHOD__,
 						$params
 						);
-				
+
 					while( $row = $dbr->fetchObject($res) ) {
-						
+
 						$from = strtotime( $row->w_emp_from );
-						
+
 						if( $from !== false ){
 							$from_month = date( "n", $from );
 							$from_year = date("Y", $from);
 						}
-						
+
 						$to = strtotime( $row->w_emp_to );
-						
+
 						if( $to !== false ){
 							$to_month = date( "n", $to );
 							$to_year = date( "Y", $to );
 						}
-						
+
 						$work[] = array(
 							"city" => $row->w_location_city,
 							"state" => $row->w_location_state,
@@ -157,7 +157,7 @@ class UserProfile {
 							"to" => $row->w_emp_to,
 							"is_current" => $row->w_is_current,
 							"id" => $row->w_id
-						);		
+						);
 					}
 					$profile["user_id"]= $this->user_id;
 					$wgMemc->set($key, $work);
@@ -165,12 +165,12 @@ class UserProfile {
 				$profile["work"] = $work;
 			}
 		}
-		
+
 		$user = User::newFromId($this->user_id);
 		$user->loadFromId();
-		$profile["real_name"]= $user->getRealName();	
-		$profile["email"]= $user->getEmail();	
-		 
+		$profile["real_name"]= $user->getRealName();
+		$profile["email"]= $user->getEmail();
+
 		return $profile;
 	}
 
@@ -180,6 +180,9 @@ class UserProfile {
 			$month = $dob[1];
 			$day = $dob[2];
 			$birthday_date = date("F jS", mktime(0,0,0,$month,$day));
+		}
+		else {
+			$birthday_date = '';
 		}
 		return $birthday_date;
 	}
@@ -191,58 +194,58 @@ class UserProfile {
 		}
 		return "00";
 	}
-	
+
 	public function getProfileComplete(){
 		global $wgUser, $wgSitename;
-		
+
 		$complete_count = 0;
-		
+
 		//check all profile fields
 		$profile = $this->getProfile();
 		foreach($this->profile_fields as $field){
 			if($profile[$field]){
-				$complete_count++;	
+				$complete_count++;
 			}
 			$this->profile_fields_count++;
 		}
-	
+
 		//check if avatar
 		$this->profile_fields_count++;
 		$avatar = new wAvatar($wgUser->getID(),"l");
-		if (strpos($avatar->getAvatarImage(), 'default_') === false)$complete_count++;	
-		
+		if (strpos($avatar->getAvatarImage(), 'default_') === false)$complete_count++;
+
 		//if ArmchairGM, check if they have a favorite team/sport
 		if($wgSitename == "ArmchairGM"){
 			$this->profile_fields_count++;
 			$favs = SportsTeams::getUserFavorites($wgUser->getID());
 			if(count($favs) > 0)$complete_count++;
 		}
-		
+
 		return round($complete_count / $this->profile_fields_count * 100);
 	}
-	
+
 	static function getEditProfileNav( $current_nav ){
 		$lines = explode( "\n", wfMsgForContent( 'update_profile_nav' ) );
 		$output = "<div class=\"profile-tab-bar\">";
 		foreach ($lines as $line) {
-			
+
 			if (strpos($line, '*') !== 0){
 				continue;
 			}else{
 				$line = explode( '|' , trim($line, '* '), 2 );
 				$page = Title::newFromText($line[0]);
 				$link_text = $line[1];
-				
+
 				$output .= "<div class=\"profile-tab" . (($current_nav==$link_text)?"-on":"") . "\"><a href=\"" . $page->escapeFullURL() . "\">{$link_text}</a></div>";
 			}
 		}
 		$output .= "<div class=\"cleared\"></div></div>";
-		
+
 		return $output;
 	}
 
 }
-	
+
 
 
 ?>
