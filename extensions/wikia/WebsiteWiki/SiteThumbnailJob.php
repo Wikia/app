ba316521a973b3108e02f2fe73ed5a1500d4e88f
eloy <eloy@wikia-inc.com>
2010-04-29 19:38:03 +0000

    prepare thumbnailer as separate job
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21799 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WebsiteWiki/SiteThumbnailJob.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * SiteThumbnailJob -- create thumbnail for domain
+ *
+ * @file
+ * @ingroup JobQueue
+ *
+ * @copyright Copyright © Krzysztof Krzyżaniak for Wikia Inc.
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
+ * @date 2010-03-15
+ * @version 1.0
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+class SiteThumbnailJob extends Job {
+
+	private
+		$mDomain,
+		$mTargetUrl,
+		$mParams,
+		$mThumbnailer;
+
+	/**
+	 * constructor
+	 *
+	 * @access public
+	 */
+	public function __construct( $title, $params, $id = 0 ) {
+
+		global $wgContLang;
+
+		parent::__construct( "newsite", $title, $params, $id );
+
+		/**
+		 * init for local stuffs
+		 */
+		$this->mDomain      = $wgContLang->lc( $this->title->getDBKey() );
+		$this->mTargetUrl   = sprintf( "http://%s/", $this->mDomain );
+		$this->mParams      = $params;
+		$this->mThumbnailer = dirname( __FILE__ ) . "/thumbnailer.sh";
+	}
+
+	/**
+	 * main entry point
+	 *
+	 * @access public
+	 */
+	public function run() {
+		global $wgUser, $wgOut, $wgContLang, $wgTitle;
+
+		/**
+		 * overwrite $wgTitle
+		 */
+		$wgTitle = $this->title;
+	}
+
+	/**
+	 * create thumbnail for web site
+	 */
+	private function makeThumbnail() {
+		global $wgEnableUploadInfoExt;
+
+		wfProfileIn( __METHOD__ );
+
+		$imagePath = NewWebsite::getThumbnailDirectory( $this->mDomain, 'png' ); ;
+
+		/**
+		 * create PNG
+		 */
+		$cmd = sprintf( "%s %s %s", $this->mThumbnailer, $this->mTargetUrl , $imagePath );
+		Wikia::log( __METHOD__, "cmd", $cmd );
+
+		$out = wfShellExec( $cmd, $result );
+		if ( $result !== 0 ) {
+			/**
+			 * log error
+			 */
+		}
+		wfProfileOut( __METHOD__ );
+
+		return true;
+	}
+}
