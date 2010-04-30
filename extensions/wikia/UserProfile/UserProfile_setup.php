d527a6cb2700ad5b1ecf2200c3ce87f6d99fb11b
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-04-30 16:17:18 +0000

    * Fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21885 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserProfile/UserProfile_setup.php
+++ extensions/wikia/UserProfile/UserProfile_setup.php
@@ -1,47 +1,3 @@
-<<<<<<< .working
-<?php
-
-$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'UserProfile_handler';
-
-function UserProfile_handler(&$skin, &$tpl) {
-	global $wgTitle,$wgOut, $wgJsMimeType, $wgExtensionsPath, $wgStyleVersion, $wgRequest;
-	
-	if ( $wgRequest->getVal('action','view') != 'view' ) {
-		return true;	
-	}
-	
-	$wgOut->addStyle( "common/userpage_sidebar.css" );
-	
-	wfProfileIn(__METHOD__);
-	if ( $wgTitle->getNamespace() != NS_USER) {
-		return true;	
-	};
-	$html = '';
-
-	wfRunHooks('AddToUserProfile', array(&$out));
-
-	if(count($out) > 0) {
-		$html .= "<div id='profile-content' >";
-		$html .= "<div id='profile-content-inner' >";
-		$html .= $tpl->data['bodytext'];
-		$html .= "</div>";
-		$html .= "</div>";
-	
-		$html .= '<div id="profile-sidebar">';
-		if(isset($out['UserProfile1'])) {
-			$html .= $out['UserProfile1'];
-		}
-		if(isset($out['followedPages'])) {
-			$html .= $out['followedPages'];
-		}
-		$html .= '</div>';		
-	
-		$tpl->data['bodytext'] = $html;
-	}	
-	wfProfileOut(__METHOD__);
-	return true;
-}
-=======
 <?php
 
 $wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'UserProfile_handler';
@@ -77,4 +33,4 @@ function UserProfile_handler(&$skin, &$tpl) {
 	}
 	wfProfileOut(__METHOD__);
 	return true;
-}>>>>>>> .merge-right.r21880
+}
