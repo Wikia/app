7988c5fadca5126ffdbe0c5e44ef3fc51f3500af
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-04 14:03:26 +0000

    Fix typo.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21996 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GraceExpired/SpecialGraceExpired.i18n.php
+++ extensions/wikia/GraceExpired/SpecialGraceExpired.i18n.php
@@ -10,7 +10,7 @@ $messages = array();
 
 $messages['en'] = array(
 	'graceexpired' => 'Grace expired',
-	'graceexpired-row' => '$1 (in $2, last editied at $3)',
+	'graceexpired-row' => '$1 (in $2, last edited at $3)',
 	'graceexpired-header' => "This page lists all builds that are either [[:Category:Abandoned|abandoned]], [[:Category:Trash_builds|trash]], [[:Category:Build stubs|stubs]] or [[:Category:Trial Builds|trial]], and have not been edited for at least two weeks.
 
 Activity on rating pages is ''not'' taken into account here.",
