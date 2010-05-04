6f8c9a14f4f945194a0871b9f6dfbf47c597dfca
eloy <eloy@wikia-inc.com>
2010-05-04 15:35:58 +0000

    enable more than one xvfb-run
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22013 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WebsiteWiki/thumbnail.sh
+++ extensions/wikia/WebsiteWiki/thumbnail.sh
@@ -14,7 +14,7 @@ fi
 TMPFILE="`tempfile`".png
 URL="$1"
 TARGET="$2"
-/usr/bin/xvfb-run --server-args="-screen 0, 1024x768x24" /usr/bin/cutycapt --url=$URL --out=$TMPFILE --delay=5000
+/usr/bin/xvfb-run --auto-servernum --server-args="-screen 0, 1024x768x24" /usr/bin/cutycapt --url=$URL --out=$TMPFILE --delay=5000
 if [ -f $TMPFILE ];
 then
 	/usr/bin/convert -thumbnail "250x188^" -crop "250x188+0+0" +repage $TMPFILE $TARGET
