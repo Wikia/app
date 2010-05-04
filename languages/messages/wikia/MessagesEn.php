88780d154c5906968d8a189fbea232a8281336d0
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-04 16:39:26 +0000

    rt#48074: MagCloud widget(s) reorganization.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22017 e846bd91-c042-42a7-a218-4b65e2c1904d

--- languages/messages/wikia/MessagesEn.php
+++ languages/messages/wikia/MessagesEn.php
@@ -705,8 +705,8 @@ hu',
 'widget-shoutbox-disabled' => 'The Shoutbox widget is disabled on this wiki.',
 
 # MagCloud widget
-'magcloud-widget-title' => 'Check out these user-created Wikia magazines',
-'magcloud-widget-desc' => 'Check out these user-created Wikia magazines',
+'magcloud-widget-title' => 'Magazine Creator',
+'magcloud-widget-desc' => 'Create a magazine from content on this wiki',
 
 #Wikia Widgets tooltips
 'wt_help_startup'  => '|Not familiar with widgets?||Open user menu and click "Manage widgets"...',
