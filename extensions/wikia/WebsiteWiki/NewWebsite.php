03a7f894364aa1fd300ae601aa3b7ea64a2f67fc
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-04 14:34:55 +0000

    one delete too far
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@21998 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WebsiteWiki/NewWebsite.php
+++ extensions/wikia/WebsiteWiki/NewWebsite.php
@@ -11,8 +11,8 @@ $dir = dirname(__FILE__);
  * initialize parse hooks
  */
 $wgAutoloadClasses[ 'NewWebsiteJob'    ] = $dir . '/NewWebsiteJob.php';
-$wgAutoloadClasses[ 'NewWebsiteJob'    ] = $dir . '/NewWebsiteJob.php';
 $wgAutoloadClasses[ 'SiteThumbnailJob' ] = $dir . '/SiteThumbnailJob.php';
+$wgAutoloadClasses[ 'NewWebsite'       ] = $dir . '/NewWebsite.body.php';
 
 $wgExtensionMessagesFiles[ 'Newsite'   ] = $dir . '/NewWebsite.i18n.php';
 $wgExtensionAliasesFiles[ 'Newsite'    ] = $dir . '/NewWebsite.alias.php';
