07c6df5c8a3d26f5d456f2b7aab5318b6042068c
macbre <macbre@wikia-inc.com>
2010-05-05 08:09:06 +0000

    Answers: don't load core JS files twice
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22045 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -958,7 +958,7 @@ class OutputPage {
 		if ( $wgUseAjax ) {
 			// macbre: following files are part of merged JS for following skins - don't load them from here
 			$skinName = get_class($sk);
-			$skipWikiaSkins = array('SkinMonaco', 'SkinCorporate', 'SkinCorporateHome', 'SkinCorporateHubs');
+			$skipWikiaSkins = array('SkinMonaco', 'SkinAnswers', 'SkinCorporate', 'SkinCorporateHome', 'SkinCorporateHubs');
 
 			if (!in_array($skinName, $skipWikiaSkins)) {
 				$this->addScriptFile( 'ajax.js' );
