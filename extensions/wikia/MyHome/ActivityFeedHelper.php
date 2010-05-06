e69097f45a7447a5a8a21bb1f8252cf5cea8b5c9
moli <moli@wikia-inc.com>
2010-05-06 11:39:20 +0000

    kill warning
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22179 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MyHome/ActivityFeedHelper.php
+++ extensions/wikia/MyHome/ActivityFeedHelper.php
@@ -17,104 +17,106 @@ class ActivityFeedHelper {
 		$parameters['flags'] = array();
 		$parameters['includeNamespaces'] = null;
 
-		foreach ($parametersIn as $parameter => $value) {
-			if (is_int($parameter)) {	//ajax
-				@list($var, $val) = explode('=', $value);
-			} else {	//parser tag
-				$var = $parameter;
-				$val = $value;
-			}
+		if ( is_array($parametersIn) && !empty($parametersIn) ) { 
+			foreach ($parametersIn as $parameter => $value) {
+				if (is_int($parameter)) {	//ajax
+					@list($var, $val) = explode('=', $value);
+				} else {	//parser tag
+					$var = $parameter;
+					$val = $value;
+				}
 
-			switch (trim($var)) {
-				case 'size':
-					if (!empty($val)) $parameters['maxElements'] = intval($val);
-					break;
-				case 'hideimages':
-					if (!isset($val) || $val != 'false') $parameters['flags'][] = 'hideimages';
-					break;
-				case 'hidevideos':
-					if (!isset($val) || $val != 'false') $parameters['flags'][] = 'hidevideos';
-					break;
-				case 'hidecategories':
-					if (!isset($val) || $val != 'false') $parameters['flags'][] = 'hidecategories';
-					break;
-				case 'shortlist':
-					if (!isset($val) || $val != 'false') $parameters['flags'][] = 'shortlist';
-					break;
-				case 'hidenewpages':
-					if (!isset($val) || $val != 'false') $parameters['flags'][] = 'hidenewpages';
-					break;
-				case 'exclude':	//only from tag
-					if (!empty($val)) {
-						$namespaces = explode(',', $val);
-						$blankNamespace = wfMsgForContent('blanknamespace');
-						$blankNamespace = wfEmptyMsg('blanknamespace', $blankNamespace) ? null : $wgContLang->lc($blankNamespace);
-						foreach ($namespaces as $namespace) {
-							$namespace = trim($namespace);
-							if (($namespaceIndex = $wgContLang->getNsIndex($namespace)) !== false && $namespaceIndex >= 0) {
-								$excludeNamespaces[] = $namespaceIndex;
-							} else {
-								//check for main namespace which doesn't have formal name - try hardcoded 'main' and 'blanknamespace' message used in many places
-								$namespaceLC = $wgContLang->lc($namespace);
-								if ($namespaceLC == 'main' || $namespaceLC == $blankNamespace) {
-									$excludeNamespaces[] = 0;
+				switch (trim($var)) {
+					case 'size':
+						if (!empty($val)) $parameters['maxElements'] = intval($val);
+						break;
+					case 'hideimages':
+						if (!isset($val) || $val != 'false') $parameters['flags'][] = 'hideimages';
+						break;
+					case 'hidevideos':
+						if (!isset($val) || $val != 'false') $parameters['flags'][] = 'hidevideos';
+						break;
+					case 'hidecategories':
+						if (!isset($val) || $val != 'false') $parameters['flags'][] = 'hidecategories';
+						break;
+					case 'shortlist':
+						if (!isset($val) || $val != 'false') $parameters['flags'][] = 'shortlist';
+						break;
+					case 'hidenewpages':
+						if (!isset($val) || $val != 'false') $parameters['flags'][] = 'hidenewpages';
+						break;
+					case 'exclude':	//only from tag
+						if (!empty($val)) {
+							$namespaces = explode(',', $val);
+							$blankNamespace = wfMsgForContent('blanknamespace');
+							$blankNamespace = wfEmptyMsg('blanknamespace', $blankNamespace) ? null : $wgContLang->lc($blankNamespace);
+							foreach ($namespaces as $namespace) {
+								$namespace = trim($namespace);
+								if (($namespaceIndex = $wgContLang->getNsIndex($namespace)) !== false && $namespaceIndex >= 0) {
+									$excludeNamespaces[] = $namespaceIndex;
+								} else {
+									//check for main namespace which doesn't have formal name - try hardcoded 'main' and 'blanknamespace' message used in many places
+									$namespaceLC = $wgContLang->lc($namespace);
+									if ($namespaceLC == 'main' || $namespaceLC == $blankNamespace) {
+										$excludeNamespaces[] = 0;
+									}
 								}
 							}
+							if (count($excludeNamespaces)) {
+								global $wgCanonicalNamespaceNames;
+								$allNS[0] = 'Main';	//hack to add main namespace
+								$allNS += $wgCanonicalNamespaceNames;
+								unset($allNS[-1]);
+								unset($allNS[-2]);
+								$parameters['includeNamespaces'] = implode('|', array_diff(array_keys($allNS), $excludeNamespaces));
+							}
 						}
-						if (count($excludeNamespaces)) {
-							global $wgCanonicalNamespaceNames;
-							$allNS[0] = 'Main';	//hack to add main namespace
-							$allNS += $wgCanonicalNamespaceNames;
-							unset($allNS[-1]);
-							unset($allNS[-2]);
-							$parameters['includeNamespaces'] = implode('|', array_diff(array_keys($allNS), $excludeNamespaces));
+						break;
+					case 'style':	//only from tag
+						if (!empty($val)) {
+							$style = Sanitizer::checkCss($val);
+							if ($style) {
+								$parameters['style'] = $style;
+							}
 						}
-					}
-					break;
-				case 'style':	//only from tag
-					if (!empty($val)) {
-						$style = Sanitizer::checkCss($val);
-						if ($style) {
-							$parameters['style'] = $style;
+						break;
+					case 'uselang':	//only from ajax
+						if (!empty($val)) {
+							$parameters['uselang'] = $val;
 						}
-					}
-					break;
-				case 'uselang':	//only from ajax
-					if (!empty($val)) {
-						$parameters['uselang'] = $val;
-					}
-					break;
-				case 'ns':	//only from ajax
-					if (!empty($val)) {
-						$parameters['includeNamespaces'] = $val;
-					}
-					break;
-				case 'flags':	//only from ajax
-					if (!empty($val)) {
-						$flags = explode('|', $val);
-						if (in_array('hideimages', $flags)) $parameters['flags'][] = 'hideimages';
-						if (in_array('hidevideos', $flags)) $parameters['flags'][] = 'hidevideos';
-						if (in_array('hidecategories', $flags)) $parameters['flags'][] = 'hidecategories';
-						if (in_array('shortlist', $flags)) $parameters['flags'][] = 'shortlist';
-						if (in_array('hidenewpages', $flags)) $parameters['flags'][] = 'hidenewpages';
-					}
-					break;
-				case 'tagid':	//only from ajax
-					if (!empty($val)) {
-						$parameters['tagid'] = $val;
-					}
-					break;
-				case 'type':	//pick proper template for tag or widget
-					if (!empty($val)) {
-						$parameters['type'] = $val;
-					}
-					break;
+						break;
+					case 'ns':	//only from ajax
+						if (!empty($val)) {
+							$parameters['includeNamespaces'] = $val;
+						}
+						break;
+					case 'flags':	//only from ajax
+						if (!empty($val)) {
+							$flags = explode('|', $val);
+							if (in_array('hideimages', $flags)) $parameters['flags'][] = 'hideimages';
+							if (in_array('hidevideos', $flags)) $parameters['flags'][] = 'hidevideos';
+							if (in_array('hidecategories', $flags)) $parameters['flags'][] = 'hidecategories';
+							if (in_array('shortlist', $flags)) $parameters['flags'][] = 'shortlist';
+							if (in_array('hidenewpages', $flags)) $parameters['flags'][] = 'hidenewpages';
+						}
+						break;
+					case 'tagid':	//only from ajax
+						if (!empty($val)) {
+							$parameters['tagid'] = $val;
+						}
+						break;
+					case 'type':	//pick proper template for tag or widget
+						if (!empty($val)) {
+							$parameters['type'] = $val;
+						}
+						break;
+				}
 			}
-		}
 
-		if (in_array('shortlist', $parameters['flags'])) {
-			global $wgContentNamespaces;
-			$parameters['includeNamespaces'] = implode('|', array_diff($wgContentNamespaces, $excludeNamespaces));
+			if (in_array('shortlist', $parameters['flags'])) {
+				global $wgContentNamespaces;
+				$parameters['includeNamespaces'] = implode('|', array_diff($wgContentNamespaces, $excludeNamespaces));
+			}
 		}
 
 		wfProfileOut(__METHOD__);
