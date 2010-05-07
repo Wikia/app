0e64016a23f313095691d6ee51cba96707428ebb
bartek <bartek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-07 08:44:01 +0000

    Q&A: answers saves differently
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22223 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewWikiBuilder/ApiCreateMultiplePages.php
+++ extensions/wikia/NewWikiBuilder/ApiCreateMultiplePages.php
@@ -4,6 +4,63 @@
  *
  */
 
+class PrefilledDefaultQuestion extends DefaultQuestion {
+	function create( $text ) {
+		global $wgOut, $wgUser, $wgContLang, $wgAnswersEnableSocial;
+
+		if ( wfReadOnly() ) {
+			return false;
+		}
+
+		if ( $this->badWordsTest() ) {
+			return false;
+		}
+
+		if ( !wfRunHooks( 'CreateDefaultQuestionPageFilter', array($this->title) ) ) {
+			return false;
+		}
+
+		if ( !$this->title->userCan( 'edit' ) || !$this->title->userCan( 'create' ) ) {
+			return false;
+		}
+
+		if ( $this->searchTest() ) {
+			return false;
+		}
+
+		$default_text = $text . Answer::getSpecialCategoryTag("unanswered");
+
+		//add default category tags passed in
+		if( $this->categories ){
+			$categories_array = explode("|", $this->categories);
+			foreach( $categories_array as $category ){
+				$default_text .= "\n[[" . $wgContLang->getNsText( NS_CATEGORY ) . ":" . ucfirst( $category ) . "]]";
+			}
+		}
+
+		$flags = EDIT_NEW;
+		$article = new Article( $this->title );
+		$article->doEdit( $default_text, wfMsgForContent("new_question_comment"), $flags );
+
+		if( $wgUser->isLoggedIn() ){
+			if( $wgAnswersEnableSocial ){
+				$stats = new UserStatsTrack($wgUser->getID(), $wgUser->getName());
+				$stats->incStatField("quiz_created"); //we'll use this to track how many questions a user created
+			}
+			$wgUser->addWatch( $this->title );
+		}
+
+		//store question in session so we can give attribution if they create an account afterwards
+		$_SESSION['wsQuestionAsk'] = "";
+		if( $wgUser->isAnon() ){
+			$_SESSION['wsQuestionAsk'] = $this->question;
+		}
+
+		return true;
+	}
+}
+
+
 class ApiCreateMultiplePages extends ApiBase {
 	protected $maxpages = 100;
 
@@ -30,7 +87,7 @@ class ApiCreateMultiplePages extends ApiBase {
 		$createType = 'createPage';
                 if ($params['type'] == 'answers') {
                         $createType .= $params['type'];
-			$pagetexts = explode( '|', $params['pagetext']  );
+			$pagetexts = explode( '|', $params['pagetext'] );
                 }
 
 		$categories = explode('|', $params['category']);
@@ -95,14 +152,14 @@ class ApiCreateMultiplePages extends ApiBase {
 		}
 	}
 
-	private function createPageAnswers( $page, $category = null ) {
-		$q = new DefaultQuestion( $page );
+	private function createPageAnswers( $page, $category = null, $text = null ) {
+		$q = new PrefilledDefaultQuestion( $page );
 		if ( !is_object( $q ) ) {
 			return false;
 		}
 		$q->categories = $category;
 
-		$res = $q->create();
+		$res = $q->create( $text );
 
 		return $res;
 	}
