d9b007605ee3f7ff81c091f8714d3ea1016da30c
bartek <bartek@wikia-inc.com>
2010-05-07 09:52:26 +0000

    Q&A: move this
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22224 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewWikiBuilder/ApiCreateMultiplePages.php
+++ extensions/wikia/NewWikiBuilder/ApiCreateMultiplePages.php
@@ -4,63 +4,6 @@
  *
  */
 
-class PrefilledDefaultQuestion extends DefaultQuestion {
-	function create( $text ) {
-		global $wgOut, $wgUser, $wgContLang, $wgAnswersEnableSocial;
-
-		if ( wfReadOnly() ) {
-			return false;
-		}
-
-		if ( $this->badWordsTest() ) {
-			return false;
-		}
-
-		if ( !wfRunHooks( 'CreateDefaultQuestionPageFilter', array($this->title) ) ) {
-			return false;
-		}
-
-		if ( !$this->title->userCan( 'edit' ) || !$this->title->userCan( 'create' ) ) {
-			return false;
-		}
-
-		if ( $this->searchTest() ) {
-			return false;
-		}
-
-		$default_text = $text . Answer::getSpecialCategoryTag("unanswered");
-
-		//add default category tags passed in
-		if( $this->categories ){
-			$categories_array = explode("|", $this->categories);
-			foreach( $categories_array as $category ){
-				$default_text .= "\n[[" . $wgContLang->getNsText( NS_CATEGORY ) . ":" . ucfirst( $category ) . "]]";
-			}
-		}
-
-		$flags = EDIT_NEW;
-		$article = new Article( $this->title );
-		$article->doEdit( $default_text, wfMsgForContent("new_question_comment"), $flags );
-
-		if( $wgUser->isLoggedIn() ){
-			if( $wgAnswersEnableSocial ){
-				$stats = new UserStatsTrack($wgUser->getID(), $wgUser->getName());
-				$stats->incStatField("quiz_created"); //we'll use this to track how many questions a user created
-			}
-			$wgUser->addWatch( $this->title );
-		}
-
-		//store question in session so we can give attribution if they create an account afterwards
-		$_SESSION['wsQuestionAsk'] = "";
-		if( $wgUser->isAnon() ){
-			$_SESSION['wsQuestionAsk'] = $this->question;
-		}
-
-		return true;
-	}
-}
-
-
 class ApiCreateMultiplePages extends ApiBase {
 	protected $maxpages = 100;
 
@@ -153,6 +96,7 @@ class ApiCreateMultiplePages extends ApiBase {
 	}
 
 	private function createPageAnswers( $page, $category = null, $text = null ) {
+		require_once("$IP/extensions/wikia/NewWikiBuilder/PrefilledDefaultQuestion.php");
 		$q = new PrefilledDefaultQuestion( $page );
 		if ( !is_object( $q ) ) {
 			return false;
