ec14d031a5ba3412800b08c17c6588fce7ed4ab2
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-07 14:47:26 +0000

    Fixing GetThemeCSS when $skin->themename == 'sapphire'
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22250 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/Wikia.php
+++ includes/wikia/Wikia.php
@@ -222,13 +222,18 @@ class WikiaAssets {
 	}
 
 	public static function GetThemeCSS($skin) {
-		global $wgDevelEnvironment;
+		global $wgDevelEnvironment, $wgStylePath;
+		
+		if($skin->themename == 'custom' || $skin->themename == 'sapphire') {
+				return null;
+		}
+		
 		if(empty($wgDevelEnvironment)) {
 			$start = "http://images1.wikia.nocookie.net/common";
 		} else {
 			$start = "";
 		}
-		return "\n".'<!-- GetThemeCSS --><link rel="stylesheet" type="text/css" href="'. $start .'/skins/'. $skin->skinname .'/' . $skin->themename . '/css/main.css" />';
+		return "\n".'<!-- GetThemeCSS --><link rel="stylesheet" type="text/css" href="'. $wgStylePath .'/'. $skin->skinname .'/' . $skin->themename . '/css/main.css" />';
 	}
 
 	public static function GetCoreCSS($themename, $isRTL, $isAllInOne) {
