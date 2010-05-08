5b947264b9a27c343eced6b8d1b3514acf06cd7c
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-08 02:03:46 +0000

    replacing magnify-clip icon with details icon and renaming CSS class to match
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22266 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageLightbox/ImageLightbox.tmpl.php
+++ extensions/wikia/ImageLightbox/ImageLightbox.tmpl.php
@@ -1,7 +1,7 @@
 <div id="lightbox-image" title="<?= htmlspecialchars($name) ?>" style="text-align: center">
 	<img alt="" height="<?= $height ?>" width="<?= $width ?>" src="<?= $wgBlankImgUrl ?>" style="background: 50% 50% no-repeat url('<?= $thumbUrl ?>')" />
 	<div id="lightbox-caption" class="neutral clearfix" style="padding: 5px">
-		<a id="lightbox-link" href="<?= htmlspecialchars($href) ?>" title="<?= wfMsg('lightbox_details_tooltip') ?>" style="float: right"><img class="sprite magnify-clip" width="16" height="16" src="<?= $wgBlankImgUrl ?>" alt="" /></a>
+		<a id="lightbox-link" href="<?= htmlspecialchars($href) ?>" title="<?= wfMsg('lightbox_details_tooltip') ?>" style="float: right"><img class="sprite details" width="16" height="16" src="<?= $wgBlankImgUrl ?>" alt="" /></a>
 		<div id="lightbox-caption-content" style="margin-right: 25px; text-align: left"></div>
 	</div>
 </div>
