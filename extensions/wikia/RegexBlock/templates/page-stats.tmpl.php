ca54605258d96baa92336f1a5b4d53e823a5821c
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-08 02:11:16 +0000

    add unblock link to stats view (by request of VSTF)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22267 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RegexBlock/templates/page-stats.tmpl.php
+++ extensions/wikia/RegexBlock/templates/page-stats.tmpl.php
@@ -1,30 +1,33 @@
-<!-- s:<?= __FILE__ ?> -->
-<? 
+<!-- s:<?php print __FILE__; ?> -->
+<?php
 $blocker_link = $skin->makeKnownLinkObj( $titleObj, $blockInfo->blckby_blocker, 'filter=' . urlencode($blockInfo->blckby_blocker) );
 $blockername_link = $skin->makeKnownLinkObj( $titleObj, $blockInfo->blckby_name, 'rfilter=' . urlencode($blockInfo->blckby_name) );
 ?>
-<h5><?=wfMsg('regexblock_stats_title')?> <strong> <?=$blockername_link?></strong></h5>
+<h5><?php print wfMsg('regexblock_stats_title'); ?> <strong> <?php print $blockername_link; ?></strong></h5>
 <ul>
-<li><?=wfMsg('regexblock_blocker_name')?>: <b><?=$blocker_link?></b></li>
-<li><?=wfMsg('regexblock_reason')?> <?=($blockInfo->blckby_reason) ?  ('<b>' . $blockInfo->blckby_reason . '</b>') : ('<i>' . wfMsg('regexblock_generic_reason') . '</i>' ); ?></li>
-<li>Type: <?= (($blockInfo->blckby_exact) ? wfMsg('regexblock_exact_match') : wfMsg('regexblock_regex_match')) ?></li>
-<li><?=wfMsg('regexblock_block_date_from')?>: <b><?=$lang->timeanddate( wfTimestamp( TS_MW, $blockInfo->blckby_timestamp ), true )?></b></li>
-<li><?=wfMsg('regexblock_block_date_to')?>: <?=($blockInfo->blckby_expire != 'infinite') ?  ('<b>' . $lang->timeanddate( wfTimestamp( TS_MW, $blockInfo->blckby_expire ), true ) . '</b>') : ('<i>infinite</i>'); ?></li>
+<li><?php print wfMsg('regexblock_blocker_name'); ?>: <b><?php print $blocker_link ?></b></li>
+<li><?php print wfMsg('regexblock_reason'); ?> <?php print ($blockInfo->blckby_reason) ?  ('<b>' . $blockInfo->blckby_reason . '</b>') : ('<i>' . wfMsg('regexblock_generic_reason') . '</i>' ); ?></li>
+<li>Type: <?php print (($blockInfo->blckby_exact) ? wfMsg('regexblock_exact_match') : wfMsg('regexblock_regex_match')) ?></li>
+<li><?php print wfMsg('regexblock_block_date_from')?>: <b><?php print $lang->timeanddate( wfTimestamp( TS_MW, $blockInfo->blckby_timestamp ), true )?></b></li>
+<li><?php print wfMsg('regexblock_block_date_to')?>: <?php print ($blockInfo->blckby_expire != 'infinite') ?  ('<b>' . $lang->timeanddate( wfTimestamp( TS_MW, $blockInfo->blckby_expire ), true ) . '</b>') : ('<i>infinite</i>'); ?></li>
 </ul>
+<?php
+print "<a href=\"" . $titleObj->getFullURL( array('action'=>'delete', 'blckid'=>'6428') ) . "\">".wfMsg('regexblock_unblock')."</a><br/>\n";
+?>
 <br />
-<? if (!empty($stats_list)) { ?>
-<p><?=$pager?></p>
+<?php if (!empty($stats_list)) { ?>
+<p><?php print $pager?></p>
 <br />
 <ul id="regexblock_triggers">
-<? foreach ($stats_list as $id => $row) { ?>
-<li><?
+<?php foreach ($stats_list as $id => $row) { ?>
+<li><?php
 print wfMsg('regexblock_match_stats_record', array($row->stats_match, $row->stats_user, htmlspecialchars($row->stats_dbname), $lang->timeanddate( wfTimestamp( TS_MW, $row->stats_timestamp ), true ), $row->stats_ip) );
 ?></li>
-<? } ?>
+<?php } ?>
 </ul>
 <br />
-<p><?=$pager?></p>
-<? } else { ?>
-<?= wfMsg('regexblock_nodata_found') ?>
-<? } ?>
-<!-- e:<?= __FILE__ ?> -->
+<p><?php print $pager?></p>
+<?php } else { ?>
+<?php print wfMsg('regexblock_nodata_found') ?>
+<?php } ?>
+<!-- e:<?php print __FILE__; ?> -->
