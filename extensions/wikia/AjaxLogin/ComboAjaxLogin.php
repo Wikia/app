297bd8ded2ac4355a23deec0d2d4b661d8eea205
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-13 02:12:47 +0000

    First things first, need this return value.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22402 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/ComboAjaxLogin.php
+++ extensions/wikia/AjaxLogin/ComboAjaxLogin.php
@@ -34,6 +34,7 @@ $wgExtensionMessagesFiles['ComboAjaxLogin'] = dirname(__FILE__) . '/ComboAjaxLog
 
 	// Bind the buttons on the skin to call the ComboAjaxLogin.  This needs to be on every page when this extension is enabled.
 	$out->addScript("<script type='text/javascript' src='$wgExtensionsPath/wikia/AjaxLogin/AjaxLoginBindings.js?$wgStyleVersion'></script>\n");
+	return true;
  } // end ajaxLoginAdditionalScripts()
 
 /**
