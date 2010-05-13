1da1ab5ddbb52e6e3bcad608f03ce203114bc810
sean <sean@wikia-inc.com>
2010-05-13 02:03:16 +0000

    Moved AjaxLogin binding code out of wikibits.js and into its own js file included by the extension. This is probably less efficient but is the correct way to do it, otherwise we've added a dependency between the extension and the main part of the app in an ugly and unintuitive way.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22401 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '22332';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '22400';
 
 /**
  * @name $wgCityId
