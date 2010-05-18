Wikia/app

commit 3753e73385b5e6f5f34aba715c1cec45dc9945eb

MERGE fbconnect BRANCH INTO trunk.
git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22146 e846bd91-c042-42a7-a218-4b65e2c1904d

extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
@@ -4,9 +4,14 @@ $messages = array();
 $messages["en"] = array(
 	"comboajaxlogin-createlog" => "Log in or create an account",
 	"comboajaxlogin-actionmsg" => "To perform this action you first need to log in or create an account",
+	"comboajaxlogin-connectmarketing" => "<h1>Connect your accounts</h1><ul><li>Keep your current username, history, edits... nothing changes except how you log in</li><li>Share your activity on Wikia with your friends on Facebook</li><li>Complete control of what's published</li></ul>",
+	"comboajaxlogin-connectmarketing-back" => "&laquo; Back",
+	"comboajaxlogin-connectmarketing-forward" => "Get started &raquo;",
+	"comboajaxlogin-connectdirections" => "<p>Enter your Wikia username and password here - we will magically connect your Wikia and Facebook accounts in the background.</p><p>Once you're done, you can log in easily using any Facebook Connect button.",
+	"comboajaxlogin-post-not-understood" => "There was an error in the way this form was constructed.  Please try again or report this to [[Special:Contact]].",
 );
 
 $messages["pl"] = array(
-	"comboajaxlogin-createlog" => "Zaloguj się lub utwórz konto",
-	"comboajaxlogin-actionmsg" => "Aby wykonać tę akcję musisz się najpierw zalogować lub zarejestrować konto",
+	"comboajaxlogin-createlog" => "Zaloguj sie lub utw�rz konto",
+	"comboajaxlogin-actionmsg" => "Aby wykonac te akcje musisz sie najpierw zalogowac lub zarejestrowac konto",
 );

commit 6d964fa16fd309375b2b9a578f7d5d50dd5f71bf

fixed PL encoding broken in r22146
git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22545 e846bd91-c042-42a7-a218-4b65e2c1904d

extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
@@ -1,6 +1,7 @@
 <?php
 
 $messages = array();
+
 $messages["en"] = array(
 	"comboajaxlogin-createlog" => "Log in or create an account",
 	"comboajaxlogin-actionmsg" => "To perform this action you first need to log in or create an account",
@@ -11,7 +12,7 @@ $messages["en"] = array(
 	"comboajaxlogin-post-not-understood" => "There was an error in the way this form was constructed.  Please try again or report this to [[Special:Contact]].",
 );
 
-$messages["pl"] = array(
-	"comboajaxlogin-createlog" => "Zaloguj sie lub utw�rz konto",
-	"comboajaxlogin-actionmsg" => "Aby wykonac te akcje musisz sie najpierw zalogowac lub zarejestrowac konto",
+$messages['pl'] = array(
+	'comboajaxlogin-createlog' => 'Zaloguj się lub utwórz konto',
+	'comboajaxlogin-actionmsg' => 'Aby wykonać tę akcję musisz się najpierw zalogować lub utworzyć konto.',
 );