2e3b7a4a70376f906a8c6a2018154bb17e3bb9e7
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-18 12:26:05 +0000

    fill blanks with code
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22528 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ includes/wikia/RiakSessionsHandler.php
@@ -0,0 +1,93 @@
+<?php
+
+
+/**
+ * This file gets included if $wgSessionsInRiak is set in the config.
+ * It redirects session handling functions to store their data in riak
+ * instead of the local filesystem.
+ *
+ * @file
+ * @ingroup Cache
+ * @author Krzysztof Krzyżaniak (eloy)
+ */
+
+class RiakSessionHandler {
+
+	static public $mBucket = "session";
+
+	/**
+	 * return proper key for session
+	 *
+	 * if $wgWikiaCentralAuthDatabase is set it means that we use WikiaCentralAuth
+	 * and we want to set prefix for $wgWikiaCentralAuthDatabase
+	 *
+	 * if $wgSharedDB is set it means that we use global user table on 1st cluster
+	 *
+	 * if nothing from above is set we have local user table (for example
+	 * uncyclopedia)
+	 */
+	static public function key( $id ) {
+		global $wgSharedDB, $wgDBname, $wgWikiaCentralAuthDatabase;
+
+		/**
+		 * default key
+		 */
+		$key = sprintf( "%s:session:%s", $wgDBname, $id );
+
+		if( !empty( $wgWikiaCentralAuthDatabase ) ) {
+			$key = sprintf( "%s:session:%s", $wgWikiaCentralAuthDatabase, $id );
+		}
+		elseif( !empty( $wgSharedDB ) ) {
+			$key = sprintf( "%s:session:%s", $wgSharedDB, $id );
+		}
+
+		return $key;
+	}
+
+	static public function open() {
+		#
+		# NOP, riak is connectless protocol (HTTP)
+		#
+		return true;
+	}
+
+	static public function close() {
+		#
+		# NOP, riak is connectless protocol (HTTP)
+		#
+		return true;
+	}
+
+	static public function read( $id ) {
+		$riak = wfGetRiakClient();
+		$bucket = $riak->RiakBucket( self::$mBucket );
+		print_r( $bucket->get( self::key( $id ) ) ) ;
+
+	}
+
+	static public function write( $id, $data ) {
+		$riak = wfGetRiakClient();
+		$bucket = $riak->RiakBucket( self::$mBucket );
+		$bucket->newObject( self::key( $id ), $data );
+	}
+
+	static public function destroy() {
+		$riak = wfGetRiakClient();
+		$bucket = $riak->RiakBucket( self::$mBucket );
+
+	}
+
+	static public function gc( $maxlifetime ) {
+		$riak = wfGetRiakClient();
+	}
+
+}
+
+session_set_save_handler(
+	array( "RiakSessionHandler", "open" ),
+	array( "RiakSessionHandler", "close" ),
+	array( "RiakSessionHandler", "read" ),
+	array( "RiakSessionHandler", "write" ),
+	array( "RiakSessionHandler", "destroy" ),
+	array( "RiakSessionHandler", "gc" )
+);
