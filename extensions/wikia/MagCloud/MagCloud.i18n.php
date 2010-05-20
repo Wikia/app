0e286b9070c2f95d626939edb6a2d4c9c8539c79
siebrand <siebrand@wikia-inc.com>
2010-05-20 19:35:40 +0000

    Some i18n and language consistency updates.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22651 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MagCloud/MagCloud.i18n.php
+++ extensions/wikia/MagCloud/MagCloud.i18n.php
@@ -4,15 +4,15 @@ $messages = array();
 
 $messages['en'] = array(
 	'wikiacollection' => 'Make a magazine!',
-	'magcloud-intro-get-started' => 'Collect articles',
-	'magcloud-intro-create-magazine' => 'Create a magazine from articles on this Wiki!',
+	'magcloud-intro-get-started' => 'Collect wiki pages',
+	'magcloud-intro-create-magazine' => 'Create a magazine from pages on this wiki!',
 	'magcloud-intro-check-out' => 'Click the magazine to check out an example from our community!',
-	'magcloud-intro-hint' => 'Just use the toolbar at the top of the page to select the articles you\'d like to compile into your magazine. Click "Go to your magazine" when you\'re done!',
-	'magcloud-intro-view-my-magazines' => 'View my Magazines',
+	'magcloud-intro-hint' => 'Just use the toolbar at the top of the page to select the wiki pages you would like to compile into your magazine. Click "{{int:Magcloud-toolbar-go-to-magazine}}" when you are done!',
+	'magcloud-intro-view-my-magazines' => 'View my magazines',
 
-	'magcloud-load-magazine-title' => 'Load a Magazine',
+	'magcloud-load-magazine-title' => 'Load a magazine',
 	'magcloud-load-magazine-load' => 'Load this magazine',
-	'magcloud-load-magazine-empty' => 'You haven\'t saved any magazines yet. Try creating one and save it to see them in this list!',
+	'magcloud-load-magazine-empty' => 'You have not saved any magazines yet. Try creating one and save it to see them in this list!',
 
 	'magcloud-add-article-success' => "'''$1''' has been added to your magazine",
 	'magcloud-add-article-already' => "'''$1''' already added",
@@ -21,54 +21,54 @@ $messages['en'] = array(
 	'magcloud-toolbar-article-in-collection' => "'''$1''' is already in your magazine",
 	'magcloud-toolbar-article-add' => "Add '''$1''' to your magazine?",
 	'magcloud-toolbar-add' => 'Add',
-	'magcloud-toolbar-articles-count' => '<big>$1</big> {{PLURAL:$1|article|articles}}',
+	'magcloud-toolbar-articles-count' => '<big>$1</big> wiki {{PLURAL:$1|page|pages}}',
 	'magcloud-toolbar-go-to-magazine' => 'Go to your magazine',
 
 	'magcloud-discard-magazine-title' => 'Save your collection?',
-	'magcloud-discard-magazine-msg' => 'You are working on an unsaved magazine. Would you like to save it? (You\'ll need to be logged in to save your collection.)',
+	'magcloud-discard-magazine-msg' => 'You are working on an unsaved magazine. Would you like to save it? (You will need to be logged in to save your collection.)',
 	'magcloud-discard-magazine-discard' => 'Discard and exit',
 	'magcloud-discard-magazine-save' => 'Save and exit',
 
 	'magcloud-special-collection-title' => 'Make a magazine!',
-	'magcloud-special-collection-empty' => 'You haven\'t selected any articles yet! Head back into the wiki, and select \'Add\' in the toolbar above to compile your magazine.',
-	'magcloud-special-collection-review-list-info' => 'Great, you have selected $1 {{PLURAL:$1|article|articles}} for your magazine. If you want to add more, just browse to more pages and hit "Add" in the toolbar above!',
+	'magcloud-special-collection-empty' => 'You have not selected any wiki pages yet! Head back into the wiki, and select "{{int:Magcloud-toolbar-add}}" in the toolbar above to compile your magazine.',
+	'magcloud-special-collection-review-list-info' => 'Great, you have selected $1 wiki {{PLURAL:$1|page|pages}} for your magazine. If you want to add more, just browse to more wiki pages and hit "{{int:Magcloud-toolbar-add}}" in the toolbar above!',
 	'magcloud-special-collection-review-list-tips' => "<big>Tips for creating a Great Magazine</big>
 
 # Try choosing articles that follow a theme, like comic villans or unique swords.
-# Your final magazine needs to be less than 100 pages in length, which is probably about 8 - 10 articles, depending on which articles you're adding.
+# Your final magazine needs to be less than 100 pages in length, which is probably about 8 to 10 wiki pages, depending on which wiki pages you are adding.
 
 If you're stuck for inspiration, check out the examples below!",
 	'magcloud-order-your-articles' => 'Order your articles',
 
-	'magcloud-button-design-a-cover' => 'Design a Cover',
+	'magcloud-button-design-a-cover' => 'Design a cover',
 
 	'magcloud-design-default-title' => 'My $1 magazine',
 	'magcloud-design-default-subtitle' => 'Subtitle',
 
-	'magcloud-design-review-list' => 'Back to Review List',
+	'magcloud-design-review-list' => 'Back to Review list',
 	'magcloud-design-preview' => 'Preview',
 	'magcloud-design-license-policy' => 'Only upload an image that you have rights to print and that adheres to this wiki\'s policies.',
 	'magcloud-design-image-selected' => '$1 selected',
 
 	'magcloud-preview-done' => 'Your magazine has been generated.',
-	'magcloud-preview-back-to-cover' => 'Back to Design Cover',
-	'magcloud-preview-save-magazine' => 'Save your Magazine',
-	'magcloud-preview-publish' => 'Publish your Magazine',
+	'magcloud-preview-back-to-cover' => 'Back to Design cover',
+	'magcloud-preview-save-magazine' => 'Save your magazine',
+	'magcloud-preview-publish' => 'Publish your magazine',
 	'magcloud-preview-generating-pdf' => 'Creating your magazine...',
 
 	'magcloud-publish-status-title' => 'Publishing your magazine',
 	'magcloud-publish-status' => 'Now uploading your magazine to MagCloud...',
-	'magcloud-publish-error' => 'We encountered an error while trying to send your magazine to MagCloud - we logged it to try and figure out what happened. Best try again later - hopefully we\'ll have figured out the problem. Please accept our apologies!',
+	'magcloud-publish-error' => 'We encountered an error while trying to send your magazine to MagCloud - we logged it to try and figure out what happened. Best try again later - hopefully we will have figured out the problem. Please accept our apologies!',
 	'magcloud-publish-done' => 'All done! Redirecting you to MagCloud...',
 
-	'magcloud-publish-status' => "Great! We're going to pipe your magazine over.",
-	'magcloud-publish-ajax-initialize' => "We're logging into MagCloud...",
+	'magcloud-publish-status' => "Great! We are going to send your magazine over.",
+	'magcloud-publish-ajax-initialize' => "We are logging into MagCloud...",
 	'magcloud-publish-ajax-login' => "...now setting up the publication...",
 	'magcloud-publish-ajax-publication' => "...setting up the issue...",
 	'magcloud-publish-ajax-issue' => " ...just finishing up the upload...",
 	'magcloud-publish-ajax-upload' => "...and tidying up, like our parents told us to...",
 	'magcloud-publish-ajax-processing' => "...now where is the big red PUBLISH button...",
-	'magcloud-publish-done' => "...and we're done! Redirecting to MagCloud...",
+	'magcloud-publish-done' => "...and we are done! Redirecting to MagCloud...",
 );
 
 /** Message documentation (Message documentation)
