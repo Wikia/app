642fece1e3743c97216f569de19d68bc058e0735
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-05-25 00:06:18 +0000

    Added inclusion of DevBoxSettings.php on DevBoxes and a note about what it should be used for.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@22770 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Development/Configs_DevBox/LocalSettings.php
+++ extensions/wikia/Development/Configs_DevBox/LocalSettings.php
@@ -1,4 +1,12 @@
 <?php
+
+// NOTE: SINCE DevBoxes ARE A SPECIAL CASE, THIS FILE IS CONTAINED IN /extensions/wikia/Development/Configs_DevBox AND
+// IS COPIED OVER TO THE DOCROOT WHEN updateDevBox.sh IS RUN.
+
+// IT IS NOT RECOMMENDED TO MODIFY THIS FILE DIRECTLY BECAUSE IT WILL BE OVERWRITTEN THE NEXT TIME YOU RUN updateDevBox.sh
+// INSTEAD, MODIFY THE DevBoxSettings.php WHICH IS IN THE DOCROOT DIRECTORY. IT IS DESIGNED FOR MAKING CHANGES JUST ON YOUR
+// DEVBOX.
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 $wgShowExceptionDetails = true;
@@ -31,6 +39,8 @@ include("$IP/extensions/wikia/Development/SpecialDevBoxPanel/Special_DevBoxPanel
 #
 $oWiki = new WikiFactoryLoader();
 $wgCityId = $oWiki->execute();
+
+require_once( dirname( $wgWikiaLocalSettingsPath ) . '/../DevBoxSettings.php' );
 
 ####
 # To fake any Wiki Factory settings, put them here:
