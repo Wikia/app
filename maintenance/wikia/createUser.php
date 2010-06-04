b728cc565a36ce1ee0846c2ec1d30489a3b5d976
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-06-04 10:54:07 +0000

    create user from commandline
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23170 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/createUser.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Maintenance script to create an account and grant it administrator rights
+ *
+ * @file
+ * @ingroup Maintenance
+ * @author Rob Church <robchur@gmail.com>
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
+ */
+
+ini_set( "include_path", dirname(__FILE__)."/../" );
+$options = array( 'help' );
+require_once( '/../commandLine.inc' );
+
+if( isset( $options['help'] ) ) {
+	showHelp();
+	exit( 1 );
+}
+
+if( count( $args ) < 3 ) {
+	echo( "Please provide a username, password and email for the new account.\n" );
+	die( 1 );
+}
+
+$username = $args[0];
+$password = $args[1];
+$email    = $args[3];
+
+echo( wfWikiID() . ": Creating User:{$username}..." );
+
+# Validate username and check it doesn't exist
+$user = User::newFromName( $username );
+if( !is_object( $user ) ) {
+	echo( "invalid username.\n" );
+	die( 1 );
+} elseif( 0 != $user->idForName() ) {
+	echo( "account exists.\n" );
+	die( 1 );
+}
+
+# Insert the account into the database
+$user->addToDatabase();
+$user->setEmail( $email );
+$user->setPassword( $password );
+$user->saveSettings();
+
+
+# Increment site_stats.ss_users
+$ssu = new SiteStatsUpdate( 0, 0, 0, 0, 1 );
+$ssu->doUpdate();
+
+echo( "done.\n" );
+
+function showHelp() {
+	echo( <<<EOT
+Create a new user account
+USAGE: php createUser.php [--help] <username> <password> <email>
+
+	--help
+		Show this help information
+
+EOT
+	);
+}
