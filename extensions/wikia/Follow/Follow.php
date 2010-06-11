b96e35155dc5d013feced69879673a0942e01173
siebrand <siebrand@wikia-inc.com>
2010-06-11 23:05:41 +0000

    * update messages and trailing whitespace removed from i18n file * svn:eol-style native for i18n file * i18n for extension credits description
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23559 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Follow/Follow.php
+++ extensions/wikia/Follow/Follow.php
@@ -5,8 +5,7 @@ if ( ! defined( 'MEDIAWIKI' ) )
 $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'Watchlist improvements',
 	'author' => 'Tomasz Odrobny',
-	'url' => '',
-	'description' => 'improvements of watchlist',
+	'descriptionmsg' => 'follow-desc',
 	'version' => '1.0.0',
 );
 
@@ -17,7 +16,8 @@ $wgAutoloadClasses['FollowedPages']  = $dir . 'FollowedPagesSpecial.php';
 $wgAutoloadClasses['FollowModel'] = $dir . 'FollowModel.class.php';
 $wgExtensionMessagesFiles['Follow'] = $dir . 'Follow.i18n.php';
 $wgExtensionAliasesFiles['Follow'] = $dir . 'Follow.alias.php';
-/*Hoks setup */
+
+/* Hooks setup */
 if ( !empty($wgEnableWikiaFollowedPages) && $wgEnableWikiaFollowedPages ) {
 	$wgHooks['RecentChangeNotify'][] = 'FollowHelper::watchersList';
 	$wgHooks['AfterCategoriesUpdate'][] = 'FollowHelper::watchCategories';
@@ -28,13 +28,9 @@ if ( !empty($wgEnableWikiaFollowedPages) && $wgEnableWikiaFollowedPages ) {
 	$wgHooks['AddToUserProfile'][] = 'FollowHelper::renderUserProfile';
 	$wgSpecialPages['Following'] = 'FollowedPages'; # Let MediaWiki know about your new special page.
 	$wgSpecialPageGroups['Following'] = 'changes';
-	$wgAjaxExportList[] = 'FollowHelper::showAll';	
-	$wgHooks['MailNotifyBuildKeys'][] = 'FollowHelper::mailNotifyBuildKeys'; 
+	$wgAjaxExportList[] = 'FollowHelper::showAll';
+	$wgHooks['MailNotifyBuildKeys'][] = 'FollowHelper::mailNotifyBuildKeys';
 }
+
 $wgHooks['UserToggles'][] = 'FollowHelper::addExtraToggles';
 $wgHooks['beforeRenderPrefsWatchlist'][] = 'FollowHelper::renderFollowPrefs';
-
-
- 
-
-
