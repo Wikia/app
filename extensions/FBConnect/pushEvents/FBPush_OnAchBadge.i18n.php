5f3055da85f9692923f9b6b8a6404a6b35b27d9a
shahid <shahid@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-06-11 00:34:00 +0000

    updates to FB Connect messages
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23487 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/FBConnect/pushEvents/FBPush_OnAchBadge.i18n.php
+++ extensions/FBConnect/pushEvents/FBPush_OnAchBadge.i18n.php
@@ -1,7 +1,7 @@
 <?php
 
 $messages['en'] = array(
-	'tog-fbconnect-push-allow-OnAchBadge' => 'Post to my Facebook News Feed when I got bage.',
+	'tog-fbconnect-push-allow-OnAchBadge' => 'Post to my Facebook News Feed when I earn an Achievement',
 	'fbconnect-msg-OnWatchArticle' => '$FB_NAME is now following the 
 \"$ARTICLENAME\" article on $WIKINAME: $ARTICLE_URL'
 );
--- extensions/FBConnect/pushEvents/FBPush_OnWatchArticle.i18n.php
+++ extensions/FBConnect/pushEvents/FBPush_OnWatchArticle.i18n.php
@@ -1,10 +1,10 @@
 <?php
 
 $messages['en'] = array(
-	'tog-fbconnect-push-allow-OnWatchArticle' => 'Post to my Facebook News Feed when I follow (watchlist) an article.',
+	'tog-fbconnect-push-allow-OnWatchArticle' => 'Post to my Facebook News Feed when I follow (watchlist) an article',
 	'tog-fbconnect-push-allow-OnWatchArticle-short' => 'Follow (watchlist) an article',
 
 	'fbconnect-msg-OnWatchArticle-link' => '$ARTICLENAME',
-	'fbconnect-msg-OnWatchArticle-short' => 'Follow (watchlist) an article',
+	'fbconnect-msg-OnWatchArticle-short' => 'is following an article on $WIKINAME!',
 	'fbconnect-msg-OnWatchArticle' => '$TEXT'
 );
