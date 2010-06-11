7eced0a37279114b712fb6f5bf10a7dff826cca7
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-06-11 23:05:41 +0000

    * update messages and trailing whitespace removed from i18n file * svn:eol-style native for i18n file * i18n for extension credits description
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23559 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Follow/Follow.i18n.php
+++ extensions/wikia/Follow/Follow.i18n.php
@@ -3,9 +3,10 @@
 $messages = array();
 
 $messages['en'] = array(
+	'follow-desc' => 'Improvements for the watchlist functionality',
 	'wikiafollowedpages-special-heading-category' => "Categories ($1) ",
 	'wikiafollowedpages-special-heading-article' => "Articles ($1)",
-	'wikiafollowedpages-special-heading-blogs' => "Blogs and Posts ($1)",
+	'wikiafollowedpages-special-heading-blogs' => "Blogs and posts ($1)",
 	'wikiafollowedpages-special-heading-forum' => 'Forum threads ($1)',
 	'wikiafollowedpages-special-heading-project' => 'Project pages ($1)',
 	'wikiafollowedpages-special-heading-user' => 'User pages ($1)',
@@ -13,34 +14,36 @@ $messages['en'] = array(
 	'wikiafollowedpages-special-heading-mediawiki' => 'MediaWiki pages ($1)',
 	'wikiafollowedpages-special-heading-media' => 'Images and videos ($1)',
 	'wikiafollowedpages-special-namespace' => "($1 page) ",
-	'wikiafollowedpages-special-empty' => "This user's followed pages list is empty. (Add pages to this list by clicking \"Follow\" at the top of an article). ",
+	'wikiafollowedpages-special-empty' => "This user's followed pages list is empty.
+Add pages to this list by clicking \"Follow\" at the top of a page.",
 	'wikiafollowedpages-special-anon' => 'Please [[Special:Signup|log in]] to create or view your followed pages list.',
 
-	'wikiafollowedpages-special-showall' => 'Show All',
+	'wikiafollowedpages-special-showall' => 'Show all',
 	'wikiafollowedpages-special-title' => 'Followed pages',
 	'wikiafollowedpages-special-delete-tooltip' => 'Remove this page',
 
-	'wikiafollowedpages-special-hidden' => 'This user has chosen to hide their followed pages list from public view.',  
+	'wikiafollowedpages-special-hidden' => 'This user has chosen to hide their followed pages list from public view.',
 	'wikiafollowedpages-special-hidden-unhide' => 'Unhide this list.',
 	'wikiafollowedpages-special-blog-by' => 'by $1',
-	'wikiafollowedpages-masthead' => 'Followed pages',  
+	'wikiafollowedpages-masthead' => 'Followed pages',
 	'wikiafollowedpages-following' => 'Following',
 	'wikiafollowedpages-special-title-userbar' => 'Followed pages',
 
-	'tog-enotiffollowedpages' => 'E-mail me when a page I\'m following is changed',
-	'tog-enotiffollowedminoredits' => 'E-mail me for minor edits',
+	'tog-enotiffollowedpages' => 'E-mail me when a page I am following is changed',
+	'tog-enotiffollowedminoredits' => 'E-mail me for minor edits to pages I am following',
 
-	'wikiafollowedpages-prefs-advanced' => 'Advanced options',  
-	'wikiafollowedpages-prefs-watchlist' => 'Special:Watchlist only',
- 
-	'tog-hidefollowedpages' => 	'Make my followed pages lists private', 
-	'follow-categoryadd-summary' => "Page added to category", //TODO check this 
+	'wikiafollowedpages-prefs-advanced' => 'Advanced options',
+	'wikiafollowedpages-prefs-watchlist' => '[[Special:Watchlist|Watchlist]] only',
+
+	'tog-hidefollowedpages' => 'Make my followed pages lists private',
+	'follow-categoryadd-summary' => "Page added to category", //TODO check this
 	'follow-bloglisting-summary' => "Blog posted on blog page",
 
-	'wikiafollowedpages-userpage-heading' => "Pages I'm Following",
-	'wikiafollowedpages-userpage-more'  =>  'More',
-	'wikiafollowedpages-userpage-hide'  =>  'hide', 
-	'wikiafollowedpages-userpage-empty' => "This user's followed pages list is empty. (Add pages to this list by clicking \"Follow\" at the top of an article).",
+	'wikiafollowedpages-userpage-heading' => "Pages I am following",
+	'wikiafollowedpages-userpage-more' => 'More',
+	'wikiafollowedpages-userpage-hide' => 'hide',
+	'wikiafollowedpages-userpage-empty' => "This user's followed pages list is empty.
+Add pages to this list by clicking \"Follow\" at the top of a page.",
 
 	'enotif_subject_categoryadd' => '{{SITENAME}} page $PAGETITLE has been added to $CATEGORYNAME by $PAGEEDITOR',
 	'enotif_body_categoryadd' => 'Dear $WATCHINGUSERNAME,
@@ -56,7 +59,7 @@ Please visit and edit often...
 ___________________________________________
 * Check out our featured wikis! http://www.wikia.com
 
-* Want to control which emails you receive? 
+* Want to control which e-mails you receive?
 Go to: {{fullurl:{{ns:special}}:Preferences}}.',
 
 	'enotif_body_categoryadd-html' => '<p>
@@ -72,10 +75,10 @@ Please visit and edit often...
 <br /><hr />
 <ul>
 <li><a href="http://www.wikia.com">Check out our featured wikis!</a></li>
-<li>Want to control which emails you receive? Go to <a href="{{fullurl:{{ns:special}}:Preferences}}">User Preferences</a></li>
+<li>Want to control which e-mails you receive? Go to <a href="{{fullurl:{{ns:special}}:Preferences}}">User preferences</a></li>
 </ul>
 </p>',
- 
+
 	'enotif_subject_blogpost' => '{{SITENAME}} page $PAGETITLE has been posted to $BLOGLISTINGNAME by $PAGEEDITOR',
 	'enotif_body_blogpost' => 'Dear $WATCHINGUSERNAME,
 
@@ -90,7 +93,7 @@ Please visit and edit often...
 ___________________________________________
 * Check out our featured wikis! http://www.wikia.com
 
-* Want to control which emails you receive? 
+* Want to control which e-mails you receive?
 Go to: {{fullurl:{{ns:special}}:Preferences}}.',
 	'enotif_body_blogpost-HTML' => '<p>
 Dear $WATCHINGUSERNAME,
@@ -105,7 +108,7 @@ Please visit and edit often...
 <br /><hr />
 <ul>
 <li><a href="http://www.wikia.com">Check out our featured wikis!</a></li>
-<li>Want to control which emails you receive? Go to <a href="{{fullurl:{{ns:special}}:Preferences}}">User Preferences</a></li>
+<li>Want to control which e-mails you receive? Go to <a href="{{fullurl:{{ns:special}}:Preferences}}">User preferences</a></li>
 </ul>
 </p>'
 );
