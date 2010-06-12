732067cdc8c9afba6def7dcffe382aa3c4ebe441
tomek <tomek@wikia-inc.com>
2010-06-12 04:38:35 +0000

    FBcommit: image feed fix for all case of image upload
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23561 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/FBConnect/pushEvents/FBPush_OnAddImage.i18n.php
+++ extensions/FBConnect/pushEvents/FBPush_OnAddImage.i18n.php
@@ -4,7 +4,7 @@ $messages['en'] = array(
 	'tog-fbconnect-push-allow-OnAddImage' => "Post to my Facebook News Feed when I add an image",
 	'tog-fbconnect-push-allow-OnAddImage-short' => "Add an image",
 
-	'fbconnect-msg-OnAddImage-link' => '$ARTICLENAME',
+	'fbconnect-msg-OnAddImage-link' => '$IMGNAME',
 	'fbconnect-msg-OnAddImage-short' => 'has added an image to $WIKINAME!',
 	'fbconnect-msg-OnAddImage' => 'Check out the rest of the wiki!' 
 );
