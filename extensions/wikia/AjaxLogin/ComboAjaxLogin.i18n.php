46428b18289f8e8451733bad317c63fb95c61d98
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-06-14 20:10:00 +0000

    * tweak messages * add i18n for credits description
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23641 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
+++ extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
@@ -3,13 +3,15 @@
 $messages = array();
 
 $messages["en"] = array(
+	'comboajaxlogin-desc' => 'Dynamic box which allow users to login, remind password and register users',
 	"comboajaxlogin-createlog" => "Log in or create an account",
 	"comboajaxlogin-actionmsg" => "To perform this action you first need to log in or create an account",
-	"comboajaxlogin-connectmarketing" => "<h1>Connect your accounts</h1><ul><li>Keep your current username, history, edits... nothing changes except how you log in</li><li>Share your activity on Wikia with your friends on Facebook</li><li>Complete control of what's published</li></ul>",
+	"comboajaxlogin-connectmarketing" => "<h1>Connect your accounts</h1><ul><li>Keep your current username, history, edits... Nothing changes except how you log in</li><li>Share your activity on Wikia with your friends on Facebook</li><li>Complete control of what is published</li></ul>",
 	"comboajaxlogin-connectmarketing-back" => "&laquo; Back",
 	"comboajaxlogin-connectmarketing-forward" => "Get started &raquo;",
-	"comboajaxlogin-connectdirections" => "<p>Enter your Wikia username and password here - we will magically connect your Wikia and Facebook accounts in the background.</p><p>Once you're done, you can log in easily using any Facebook Connect button.",
-	"comboajaxlogin-post-not-understood" => "There was an error in the way this form was constructed.  Please try again or report this to [[Special:Contact]].",
+	"comboajaxlogin-connectdirections" => "<p>Enter your Wikia username and password here - we will magically connect your Wikia and Facebook accounts in the background.</p><p>Once you are done, you can log in easily using any Facebook Connect button.",
+	"comboajaxlogin-post-not-understood" => "There was an error in the way this form was constructed.
+Please try again or [[Special:Contact|report this]].",
 );
 
 $messages['pl'] = array(
