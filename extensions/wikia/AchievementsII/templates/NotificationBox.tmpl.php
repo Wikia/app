58b8d15af9ef82452722031244b54829183c6a8e
christian <christian@wikia-inc.com>
2010-06-14 07:25:22 +0000

    reworked achievement notifications. class-based approach, more accurate naming conventions, reduced css complexity
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23603 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AchievementsII/templates/NotificationBox.tmpl.php
+++ extensions/wikia/AchievementsII/templates/NotificationBox.tmpl.php
@@ -1,39 +1,40 @@
 <?php
 global $wgExtensionsPath, $wgStyleVersion;
-?>
+?>	
 <link href="<?= $wgExtensionsPath.'/wikia/AchievementsII/css/notification.css?'.$wgStyleVersion ?>" type="text/css" rel="stylesheet">
 <?php
 $badgeName = htmlspecialchars($badge->getName());
 ?>
-<div id="wikia-alert" class="accent" style="display: none;">
+<div class="AchievementsNotification accent reset" style="display: none;">
 	<img src="<?=$badge->getPictureUrl(90);?>" alt="<?=$badgeName;?>" width="90" height="90" />
-	<div id="alert">
+	<div class="text">
 		<h1><?=wfMsg('achievements-notification-title', $user->getName());?></h1>
-		<h2><?=wfMsg('achievements-notification-subtitle', $badge->getName(), $badge->getPersonalGivenFor());?></h2>
+		<p><?=wfMsg('achievements-notification-subtitle', $badge->getName(), $badge->getPersonalGivenFor());?></p>
 		<p><?=wfMsgExt('achievements-notification-link', array('parse'), $user->getName());?></p>
 	</div>
-	<h3 class="<?=AchConfig::getInstance()->getLevelMsgKeyPart($badge->getLevel());?>">
-		<span id="points"><?=wfMsg( 'achievements-points-with-break', AchConfig::getInstance()->getLevelScore( $badge->getLevel() ) );?></span>
-	</h3>
+	<div class="points <?=AchConfig::getInstance()->getLevelMsgKeyPart($badge->getLevel());?>">
+		<span><?=wfMsg( 'achievements-points-with-break', AchConfig::getInstance()->getLevelScore( $badge->getLevel() ) );?></span>
+	</div>
 </div>
 
 <script>
 	wgAfterContentAndJS.push(
-		function() {
+		function() {			
 			$(function() {
+			
 				var trackFunc = function(e) {
 					window.jQuery.tracker.byStr('Achievements/notification/yourprofile');
 				};
-				
-				$('#alert').find('a').click(trackFunc);
 
-				$('#wikia-alert').click(function(e) {
+				$('.AchievementsNotification').find('a').click(trackFunc);
+
+				$('.AchievementsNotification').click(function(e) {
 					trackFunc();
-					window.location.href = $('#alert').find('a').attr('href');
+					window.location.href = $('.AchievementsNotification').find('a').attr('href');
 				});
-				
-				$('#wikia-alert').slideDown('slow');
-				setTimeout(function() {$('#wikia-alert').slideUp('slow');}, 10000);
+
+				$('.AchievementsNotification').slideDown('slow');
+				setTimeout(function() {$('.AchievementsNotification').slideUp('slow');}, 10000);				
 				window.jQuery.tracker.byStr('Achievements/notification/appears');
 			});
 		}
