5cba7479d7d362286d465dec9c02fadac23294d5
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-06-14 20:13:50 +0000

    Remove markup from "comboajaxlogin-connectdirections" and parse on output. Also fixes unclose <p> in "comboajaxlogin-connectdirections".
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23642 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
+++ extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
@@ -9,7 +9,9 @@ $messages["en"] = array(
 	"comboajaxlogin-connectmarketing" => "<h1>Connect your accounts</h1><ul><li>Keep your current username, history, edits... Nothing changes except how you log in</li><li>Share your activity on Wikia with your friends on Facebook</li><li>Complete control of what is published</li></ul>",
 	"comboajaxlogin-connectmarketing-back" => "&laquo; Back",
 	"comboajaxlogin-connectmarketing-forward" => "Get started &raquo;",
-	"comboajaxlogin-connectdirections" => "<p>Enter your Wikia username and password here - we will magically connect your Wikia and Facebook accounts in the background.</p><p>Once you are done, you can log in easily using any Facebook Connect button.",
+	"comboajaxlogin-connectdirections" => "Enter your Wikia username and password here - we will magically connect your Wikia and Facebook accounts in the background.
+
+Once you are done, you can log in easily using any Facebook Connect button.",
 	"comboajaxlogin-post-not-understood" => "There was an error in the way this form was constructed.
 Please try again or [[Special:Contact|report this]].",
 );
