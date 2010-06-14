5b6f27214615c5ebec599fd6b2c4e2e9c1a16b17
siebrand <siebrand@wikia-inc.com>
2010-06-14 20:17:52 +0000

    Add newlines to "comboajaxlogin-connectmarketing".
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23643 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
+++ extensions/wikia/AjaxLogin/ComboAjaxLogin.i18n.php
@@ -6,7 +6,12 @@ $messages["en"] = array(
 	'comboajaxlogin-desc' => 'Dynamic box which allow users to login, remind password and register users',
 	"comboajaxlogin-createlog" => "Log in or create an account",
 	"comboajaxlogin-actionmsg" => "To perform this action you first need to log in or create an account",
-	"comboajaxlogin-connectmarketing" => "<h1>Connect your accounts</h1><ul><li>Keep your current username, history, edits... Nothing changes except how you log in</li><li>Share your activity on Wikia with your friends on Facebook</li><li>Complete control of what is published</li></ul>",
+	"comboajaxlogin-connectmarketing" => "<h1>Connect your accounts</h1>
+<ul>
+<li>Keep your current username, history, edits... Nothing changes except how you log in</li>
+<li>Share your activity on Wikia with your friends on Facebook</li>
+<li>Complete control of what is published</li>
+</ul>",
 	"comboajaxlogin-connectmarketing-back" => "&laquo; Back",
 	"comboajaxlogin-connectmarketing-forward" => "Get started &raquo;",
 	"comboajaxlogin-connectdirections" => "Enter your Wikia username and password here - we will magically connect your Wikia and Facebook accounts in the background.
