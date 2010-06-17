5c50d502a170fd1dea63a703290a549dd1cf11f8
moli <moli@wikia-inc.com>
2010-06-17 13:00:57 +0000

    commit changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23759 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/archiveStatsData.php
+++ maintenance/wikia/archiveStatsData.php
@@ -18,15 +18,23 @@ $fname = 'archiveStatsData';
 # Do an initial scan for inactive accounts and report the result
 echo( "Start script ...\n" );
 
+$help = isset($options['help']);
+$wikia = $options['wikia'];
+$events = isset($options['events']);
+$sleep = $options['sleep'];
+$sloop = $options['sloop'];
+#echo print_r($options, true);
+
 function runArchiveStats($city_id) {
 	global $IP, $wgWikiaLocalSettingsPath;
+	global $sleep, $sloop;
 	$script_path = $_SERVER['PHP_SELF'];
-	$path = "SERVER_ID={$city_id} php {$script_path} --events=1 --conf {$wgWikiaLocalSettingsPath}";
-	echo $path . " \n";
-	#$return = wfShellExec( $path );
+	$path = "SERVER_ID={$city_id} php {$script_path} --events=1 --conf {$wgWikiaLocalSettingsPath} --sleep=$sleep --sloop=$sloop";
+	#echo $path . " \n";
+	$return = wfShellExec( $path );
 }
 
-function callEditHook(&$articles, $oRow, $sleep = 0) {
+function callEditHook(&$articles, $oRow) {
 	global $wgCityId, $wgServer;
 	$wgServer = WikiFactory::getVarValueByName( "wgServer", $wgCityId );
 	# flags
@@ -46,25 +54,29 @@ function callEditHook(&$articles, $oRow, $sleep = 0) {
 	$articles[$oRow->page_id] = 1;
 	# call function
 	$res = ScribeProducer::saveComplete( &$oArticle, &$oUser, null, null, null, null, null, &$flags, $oRevision, &$status, 0 );
-	if ( $sleep ) {
-		usleep(500000);
-	}
 }
 
-$main = isset($options['main']);
-$events = isset($options['events']);
-$sleep = isset($options['sleep']);
-echo print_r($options, true);
-exit;
-if ( $main ) {
-	$dbr = wfGetDB( DB_SLAVE, 'stats', $wgExternalSharedDB );
-	$res = $dbr->select( 'wikicities', array( '*' ), 'city_public = 1', $fname );
-	while( $row = $dbr->fetchObject( $res ) ) {
-		runArchiveStats($row->city_id);
-	}
-	$dbr->freeResult( $res );
+if ( $help ) {
+	echo <<<TEXT
+Usage:
+    php archiveStatsData.php --help
+    php archiveStatsData.php [--sleep=X] [--sloop=Y]
+
+    --help         : This help message
+    --wikia=S      : Run script for Wikia (city_dbname)
+    --sleep=X      : Sleep time in seconds - number of seconds after the execution of sloop=Y tasks
+    --sloop=Y      : How many tasks the script should run to to sleep X seconds
+TEXT;
+	exit(0);
 }
 elseif ( $events ) {
+	$loop = 1;
+	if ( empty($sleep) ) {
+		$sleep = 3;
+	}
+	if ( empty($sloop) ) {
+		$sloop = 50;
+	}
 	$articles = array();
 	$dbr = wfGetDB( DB_SLAVE, 'stats' );
 	$oRes = $dbr->select(
@@ -72,17 +84,32 @@ elseif ( $events ) {
 		array( 'page_id, page_namespace, rev_id, rev_user' ), 
 		array( 'page_id = rev_page' ), 
 		$fname,
-		array( 'ORDER BY' => 'rev_id') 
+		array( 'ORDER BY' => 'rev_id', 'LIMIT' => 100) 
 	);
 
 	if ( $oRes ) {
 		while( $oRow = $dbr->fetchObject( $oRes ) ) {
-			callEditHook(&$articles, $oRow, $sleep);
+			if ( ( $loop % $sloop ) == 0 ) {
+				echo "$loop. sleep: " . $sleep . "\n";
+				sleep($sleep);
+			}
+			callEditHook(&$articles, $oRow);
+			$loop++;
 		}
 		$dbr->freeResult( $oRes );
 	} 
-} else {
-	echo "Invalid options \n";
+} 
+else {
+	$dbr = wfGetDB( DB_SLAVE, 'stats', $wgExternalSharedDB );
+	$where = array( 'city_public' => 1 );
+	if ( !empty($wikia) ) {
+		$where['city_dbname'] = $wikia;
+	}
+	$res = $dbr->select( 'city_list', array( '*' ), $where, $fname );
+	while( $row = $dbr->fetchObject( $res ) ) {
+		runArchiveStats($row->city_id);
+	}
+	$dbr->freeResult( $res );
 }
 
 echo "end \n";
