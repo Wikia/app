67e84d7aa9a53df7d262a131311e193c1189e1e0
eloy <eloy@wikia-inc.com>
2010-06-23 14:30:52 +0000

    eol
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@23972 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Sitemap/SpecialSitemap_body.php
+++ extensions/wikia/Sitemap/SpecialSitemap_body.php
@@ -142,7 +142,7 @@ class SitemapPage extends UnlistedSpecialPage {
 
 		header( "Content-type: application/xml; charset=UTF-8" );
 		$out = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-		$out .= sprintf( "<!-- generated on fly by %s -->", $this->mTitle->getFullURL() );
+		$out .= sprintf( "<!-- generated on fly by %s -->\n", $this->mTitle->getFullURL() );
 		$out .= "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
 		foreach( $this->mNamespaces as $namespace ) {
 			$out .= "\t<sitemap>\n";
@@ -169,7 +169,7 @@ class SitemapPage extends UnlistedSpecialPage {
 		header( "Content-type: application/x-gzip" );
 
 		$out = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-		$out .= sprintf( "<!-- generated on fly by %s -->", $this->mTitle->getFullURL() );
+		$out .= sprintf( "<!-- generated on fly by %s -->\n", $this->mTitle->getFullURL() );
 		$sth = $dbr->select(
 			'page',
             array(
