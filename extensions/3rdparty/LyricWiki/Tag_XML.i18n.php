a576bac15d765680ce6fbc73717ad7d001373772
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-06-23 22:47:16 +0000

    Fix error message.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24010 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/LyricWiki/Tag_XML.i18n.php
+++ extensions/3rdparty/LyricWiki/Tag_XML.i18n.php
@@ -2,7 +2,7 @@
 
 $messages = Array();
 $messages['en'] = Array(
-	'xml-feedfailed' => "Unable to fetch RSS feed.  Verify that your feed URL is correct. If this problem persists, please contact [[User:Sean Colombo]] or [[User:Teknomunk]]",
+	'xml-feedfailed' => "Unable to fetch RSS feed.  Verify that your feed URL is correct. If this problem persists, please contact [[User_talk:Sean Colombo|Sean Colombo]]",
 	'xml-emptyresult' => "The RSS feed returned an empty result.  Please check to make sure the source of the feed allows their feed to be fetched using 'curl'.",
 	'xml-nocurl' => "'''ERROR! curl_init does not exist.  Please ensure that php-curl module is installed on the server.'''",
 	'xml-pathnotfound' => "ERROR! Path not found: \$1<br/>",
