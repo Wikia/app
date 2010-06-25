a7d8af238b01905181a28b1cec26a404157ba7bd
macbre <macbre@wikia-inc.com>
2010-06-25 11:22:31 +0000

    wgStyleVersion++
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24087 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Use revision number
  */
-$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '23790';
+$wgStyleVersion = $wgMergeStyleVersionJS = $wgMergeStyleVersionCSS = '24086';
 
 /**
  * @name wgAkamaiGlobalVersion
