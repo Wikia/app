511b1380fca4c35c88f01e22c98a17bcc9cb5157
moli <moli@wikia-inc.com>
2010-07-01 15:10:16 +0000

    commit changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24271 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/archiveStatsData.php
+++ maintenance/wikia/archiveStatsData.php
@@ -8,52 +8,87 @@
  * @author Piotr Molski <moli@wikia-inc.com>
  */
 
+ini_set( 'memory_limit', '512M' );
 ini_set( "include_path", dirname(__FILE__)."/.." );
 $IP = $GLOBALS["IP"];
 require_once( "commandLine.inc" );
 include_once("$IP/extensions/wikia/Scribe/ScribeClient.php");
 include_once("$IP/extensions/wikia/Scribe/ScribeProducer.php");
-$fname = 'archiveStatsData';
 
 # Do an initial scan for inactive accounts and report the result
 echo( "Start script ...\n" );
 
 $help = isset($options['help']);
-$wikia = $options['wikia'];
+$wikia = isset($options['wikia']) ? $options['wikia'] : '';
 $events = isset($options['events']);
-$sleep = $options['sleep'];
-$sloop = $options['sloop'];
+$sleep = isset($options['sleep']) ? $options['sleep'] : '';
+$sloop = isset($options['sloop']) ? $options['sloop'] : '';
+$scribe = isset($options['scribe']) ? $options['scribe'] : '';
 #echo print_r($options, true);
 
 function runArchiveStats($city_id) {
 	global $IP, $wgWikiaLocalSettingsPath;
+	global $wgMaxShellMemory;
+	
 	global $sleep, $sloop;
 	$script_path = $_SERVER['PHP_SELF'];
+	$wgMaxShellMemory = $wgMaxShellMemory * 3;
 	$path = "SERVER_ID={$city_id} php {$script_path} --events=1 --conf {$wgWikiaLocalSettingsPath} --sleep=$sleep --sloop=$sloop";
 	#echo $path . " \n";
 	$return = wfShellExec( $path );
+	echo $return;
 }
 
-function callEditHook(&$articles, $oRow) {
+function callScribeProducer($page_id, $page_ns, $rev_id, $user_id) {
+	global $IP, $wgWikiaLocalSettingsPath, $wgCityId;
+	$script_path = $_SERVER['PHP_SELF'];
+	$path = "SERVER_ID={$wgCityId} php {$script_path} --scribe=1 --conf {$wgWikiaLocalSettingsPath} --page_id=$page_id --page_ns=$page_ns --rev_id=$rev_id --user_id=$user_id";
+	#echo $path . " \n";
+	$return = wfShellExec( $path );
+	echo $return;
+}
+
+function checkIsNew($page_id, $rev_id) {
+	wfProfileIn( __METHOD__ );
+
+	$dbr = wfGetDB( DB_SLAVE, 'stats' );
+	$oRow = $dbr->selectRow( 
+		array( 'revision' ), 
+		array( 'rev_id' ), 
+		array( 
+			"rev_id < " . intval($rev_id),
+			'rev_page' => $page_id,
+		),
+		'archiveStatsData',
+		array( 'ORDER BY' => 'rev_id') 
+	);
+
+	$return = ( isset( $oRow->rev_id ) ) ? false : true; 
+}
+
+function callEditHook($page_id, $rev_id, $page_ns, $user_id) {
 	global $wgCityId, $wgServer;
 	$wgServer = WikiFactory::getVarValueByName( "wgServer", $wgCityId );
 	# flags
 	$flags = "";
 	# article
-	$oArticle = Article::newFromID($oRow->page_id);
+	$oArticle = Article::newFromID($page_id);
 	# user
-	$oUser = User::newFromId($oRow->rev_user);
+	$oUser = User::newFromId($user_id);
 	# revision
-	$oRevision = Revision::newFromId($oRow->rev_id);
+	$oRevision = Revision::newFromId($rev_id);
 	# status - new or edit
 	$status = Status::newGood( array() );
 	# check is new
-	if ( !isset($articles[$oRow->page_id]) ) {
-		$status->value['new'] = true;
-	}
-	$articles[$oRow->page_id] = 1;
+	$status->value['new'] = checkIsNew($page_id, $rev_id);
 	# call function
 	$res = ScribeProducer::saveComplete( &$oArticle, &$oUser, null, null, null, null, null, &$flags, $oRevision, &$status, 0 );
+	#
+	unset($oArticle);
+	unset($oUser);
+	unset($oRevision);
+	unset($status);
+	unset($res);
 }
 
 if ( $help ) {
@@ -77,35 +112,49 @@ elseif ( $events ) {
 	if ( empty($sloop) ) {
 		$sloop = 50;
 	}
-	$articles = array();
 	$dbr = wfGetDB( DB_SLAVE, 'stats' );
 	$oRes = $dbr->select(
 		array( 'page', 'revision' ), 
 		array( 'page_id, page_namespace, rev_id, rev_user' ), 
 		array( 'page_id = rev_page' ), 
-		$fname,
-		array( 'ORDER BY' => 'rev_id', 'LIMIT' => 100) 
+		'archiveStatsData',
+		array( 'ORDER BY' => 'rev_id') 
 	);
 
 	if ( $oRes ) {
-		while( $oRow = $dbr->fetchObject( $oRes ) ) {
-			if ( ( $loop % $sloop ) == 0 ) {
-				echo "$loop. sleep: " . $sleep . "\n";
-				sleep($sleep);
+		$count = $dbr->numRows( $oRes );
+		echo "$count records found \n";
+		if ( $count ) {
+			while( $oRow = $dbr->fetchObject( $oRes ) ) {
+				#echo (" $loop % $sloop = " . $loop % $sloop . " \n");
+				if ( ( $loop % $sloop ) == 0 ) {
+					echo ("$loop. sleep: " . $sleep . "\n");
+					sleep($sleep);
+				}
+				#callScribeProducer($oRow->page_id, $oRow->page_namespace, $oRow->rev_id, $oRow->rev_user);
+				callEditHook($oRow->page_id, $oRow->rev_id, $oRow->page_namespace, $oRow->rev_user);
+				$loop++;
 			}
-			callEditHook(&$articles, $oRow);
-			$loop++;
+			$dbr->freeResult( $oRes );
 		}
-		$dbr->freeResult( $oRes );
-	} 
+	}
 } 
+elseif ( $scribe ) {
+	$page_id = isset($options['page_id']) ? $options['page_id'] : '';
+	$rev_id = isset($options['rev_id']) ? $options['rev_id'] : '';
+	$user_id = isset($options['user_id']) ? $options['user_id'] : '';
+	$page_ns = isset($options['page_ns']) ? $options['page_ns'] : '';
+	if ( $page_id && $rev_id ) {
+		callEditHook($page_id, $rev_id, $page_ns, $user_id);
+	}
+}
 else {
 	$dbr = wfGetDB( DB_SLAVE, 'stats', $wgExternalSharedDB );
 	$where = array( 'city_public' => 1 );
 	if ( !empty($wikia) ) {
 		$where['city_dbname'] = $wikia;
 	}
-	$res = $dbr->select( 'city_list', array( '*' ), $where, $fname );
+	$res = $dbr->select( 'city_list', array( '*' ), $where, 'archiveStatsData' );
 	while( $row = $dbr->fetchObject( $res ) ) {
 		runArchiveStats($row->city_id);
 	}
