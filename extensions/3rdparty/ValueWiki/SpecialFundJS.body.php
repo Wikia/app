29387714a1c51b9be8128ee8a5de33857c039ffd
tor <tor@wikia-inc.com>
2010-07-19 13:40:18 +0000

    typo
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24562 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/ValueWiki/SpecialFundJS.body.php
+++ extensions/3rdparty/ValueWiki/SpecialFundJS.body.php
@@ -136,14 +136,14 @@ echo "//" . $url . chr(10);
 
 global $wgHTTPProxy;
 
-$ch = curl_init();    // Starts the curl handler
+$c = curl_init();    // Starts the curl handler
 curl_setopt($c, CURLOPT_URL, $url); // Sets the paypal address for curl
 curl_setopt($c, CURLOPT_FAILONERROR, 1);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, 1); // Returns result to a variable instead of echoing
 curl_setopt($c, CURLOPT_TIMEOUT, 3); // Sets a time limit for curl in seconds (do not set too low)
 curl_setopt($c, CURLOPT_PROXY, $wgHTTPProxy); // set HTTP proxy
 $info = curl_exec($c); // run the curl process (and return the result to $result
-curl_close($ch);
+curl_close($c);
 
 
 $info	= str_ireplace('"', '', $info);
