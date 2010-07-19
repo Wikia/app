09dbd0f4030c1846204246bea775e4fea2fcb6a4
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-07-19 16:03:12 +0000

    * fix rt#56250: monaco-$theme getting stored in wgAdminSkin
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24571 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewWikiBuilder/NewWikiBuilder.html.php
+++ extensions/wikia/NewWikiBuilder/NewWikiBuilder.html.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+global $wgAdminSkin, $wgSkinTheme, $wgCdnStylePath;
+$skinName = 'monaco';
 $numSteps = 5;
 require dirname(__FILE__) . '/header.php';
 ?>
@@ -35,15 +37,15 @@ require dirname(__FILE__) . '/header.php';
 <li id="step2" class="step">
 <h1 class="headline"><?php echo wfMsgForContent("nwb-step2-headline")?></h1>
 <div class="wrapper clearfix">
-	<?php echo wfMsgForContent("nwb-step2-text")?>	
+	<?php echo wfMsgForContent("nwb-step2-text")?>
 	<!-- Hidden iframe to handle the file upload -->
 	<iframe id="hidden_iframe" src="about:blank" style="display:none" name="hidden_iframe" onLoad="NWB.iframeFormUpload(this)"></iframe>
 
 	<div style="float: left;">
 	<form action="/api.php" method="post" enctype="multipart/form-data" target="hidden_iframe" onSubmit='return NWB.iframeFormInit(this)' id="logo_form">
-		<input type="hidden" name="action" value="uploadlogo">	
-		<input type="hidden" name="format" value="xml">	
-		<input id="logo_article" type="hidden" name="title" value="Wiki.png">	
+		<input type="hidden" name="action" value="uploadlogo">
+		<input type="hidden" name="format" value="xml">
+		<input id="logo_article" type="hidden" name="title" value="Wiki.png">
 		<label><?php echo wfMsgForContent("nwb-choose-logo")?>:</label><input type="file" name="logo_file" id="logo_file" onclick="WET.byStr('nwb/step2browse');"/> <input type="submit" value="<?php echo wfMsgForContent("nwb-preview")?>" onclick="WET.byStr('nwb/step2preview');this.form.title.value='Wiki-Preview.png'"/>
 	</form>
 
@@ -51,7 +53,7 @@ require dirname(__FILE__) . '/header.php';
 		<label><?php echo wfMsgForContent("nwb-logo-preview")?>:</label>
 		<div id="logo_preview"></div>
 	</div>
-	
+
 	</div><!--float-->
 	<div class="accent note">
 		<img src="/extensions/wikia/NewWikiBuilder/sample_logo.jpg" id="sample_logo" /><br />
@@ -72,40 +74,24 @@ require dirname(__FILE__) . '/header.php';
 <h1 class="headline"><?php echo wfMsgForContent("nwb-step3-headline")?></h1>
 <div class="wrapper clearfix">
 	<?php echo wfMsgForContent("nwb-step3-text")?>
-	<div id="theme_template" style="display:none" class="theme_selekction">
-		<label for="theme_radio_$theme"><img id="theme_preview_image_$theme" /></label>
-		<input onclick="NWB.changeTheme('monaco-$theme', false)" type="radio" name="theme" value="monaco-$theme" id="theme_radio_$theme"> <label for="theme_radio_$theme">$Theme</label>
-	</div>
-	<div id="theme_scroller" class="accent">
-		<table><tr></tr></table>
-	</div>
-
-
-<script type="text/javascript">
-var wgAdminSkin = '<?php echo $wgAdminSkin?>';
-
-// TODO: Pull this list from wgSkinTheme? 
-// TODO: convert this to a loop in PHP?
-var themes = ['Sapphire','Jade','Sky','Spring','Beach','Forest','Moonlight','Carbon','Obsession'];
-for (var i = 0; i < themes.length; i++){
-	// Copy the template, search and replace the values
-	var ltheme = themes[i].toLowerCase();
-	var thtml = $("#theme_template").html();
-	thtml = thtml.replace(/\$Theme/g, themes[i]);
-	thtml = thtml.replace(/\$theme/g, ltheme);
-
-	// Create element with that preview and append it
-	$("#theme_scroller tr").append("<td>" + thtml + "</td>");
-	$("#theme_preview_image_" + ltheme).attr("src", "http://images.wikia.com/common/skins/monaco/" + ltheme + "/images/preview.png");
-
-	// Check the box with the current theme ($wgAdminSkin)
-	if (wgAdminSkin.replace(/monaco-/, '')  == ltheme) {
-		// Check the box and change the theme 
-		$("#theme_radio_" + ltheme).attr("checked", true);
-		NWB.changeTheme(ltheme, false);
-	}
-}
-</script>
+	<?if(is_array($wgSkinTheme[$skinName])):?>
+		<div id="theme_scroller" class="accent">
+			<table>
+				<tr>
+					<?foreach($wgSkinTheme[$skinName] as $theme):?>
+						<?if($theme == 'custom') continue;?>
+						<td>
+							<label for="theme_radio_<?= $theme ;?>">
+								<img id="theme_preview_image_<?= $theme ;?>" src="<?= $wgCdnStylePath ;?>/skins/<?= $skinName ;?>/<?= $theme ;?>/images/preview.png"/>
+							</label>
+							<input onclick="NWB.changeTheme('<?= $skinName ;?>-<?= $theme ;?>', false)" type="radio" name="theme" value="<?= $skinName ;?>-<?= $theme ;?>" id="theme_radio_<?= $theme ;?>"<?= ($wgAdminSkin == "{$skinName}-{$theme}") ? 'checked' : null ;?>>
+							<label for="theme_radio_<?= $theme ;?>"><?= ucfirst($theme) ;?></label>
+						</td>
+					<?endforeach;?>
+				</tr>
+			</table>
+		</div>
+	<?endif;?>
 </div>
 <div class="nav">
 	<span class="nav_reverse">
