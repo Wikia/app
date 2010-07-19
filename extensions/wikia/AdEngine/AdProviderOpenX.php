3ed940931f2168cbd979c22348862a49673d2f9a
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-07-19 10:57:54 +0000

    OpenX
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24553 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdProviderOpenX.php
+++ extensions/wikia/AdEngine/AdProviderOpenX.php
@@ -13,31 +13,12 @@ class AdProviderOpenX implements iAdProvider {
 
 	// TODO, get these out of code and configurable
 	private $zoneIds = array(
-		'HOME_TOP_LEADERBOARD' => 626,
-		'HOME_TOP_RIGHT_BOXAD' => 627,
-		'HOME_LEFT_SKYSCRAPER_1' => 628,
-		'HOME_LEFT_SKYSCRAPER_2' => 629,
-		'TOP_LEADERBOARD' => 630,
-		'TOP_RIGHT_BOXAD' => 631,
-		'LEFT_SKYSCRAPER_1' => 632,
-		'LEFT_SKYSCRAPER_2' => 633,
-		'LEFT_SKYSCRAPER_3' => 652, 
-		'FOOTER_BOXAD' => 634,
-		'RIGHT_SKYSCRAPER_1' => 452 // Monobook
+		'FOOTER_SPOTLIGHT_LEFT' => 3,
+		'FOOTER_SPOTLIGHT_MIDDLE' => 3,
+		'FOOTER_SPOTLIGHT_RIGHT' => 3,
+		'default' => 0, // FIXME
 	);
 
-	private $spotlightCategoryZones = array(
-		'2' => 635, // Gaming
-		'3' => 636, // Entertainment
-		'5' => 637,
-		'9' => 637,
-		'12' => 637,
-		'15' => 637,
-		'16' => 637,
-		'18' => 637,
-		'19' => 637,
-		'default' => 638
-	);
 
         private $slotsToCall = array();
 
@@ -51,10 +32,7 @@ class AdProviderOpenX implements iAdProvider {
 
 	public function getAd($slotname, $slot) {
 		$cat=AdEngine::getCachedCategory();
-		$zoneId = $this->getZoneId($slotname, $cat['id']);
-
-$cat = array("id" => 1);
-$zoneId = 3;
+		$zoneId = $this->getZoneId($slotname);
 
 		if(empty($zoneId)){
 			$nullAd = new AdProviderNull("Invalid slotname, no zoneid for $slotname in " . __CLASS__);
@@ -65,11 +43,6 @@ $zoneId = 3;
 <!-- AdProviderOpenX slot: $slotname zoneid: $zoneId  -->
 <script type='text/javascript'>/*<![CDATA[*/
 
-	var source = Array();
-	source.push('slot=$slotname');
-	source.push('catid={$cat['id']}');
-	source.push('lang=' + wgContentLanguage);
-
 	document.write('<scr'+'ipt type="text/javascript">');
 	document.write('var base_url = "http://spotlights.wikia.com/ajs.php";');
 	document.write('base_url += "?loc=" + escape(window.location);');
@@ -79,7 +52,12 @@ $zoneId = 3;
 	document.write('base_url += "&zoneid=$zoneId";');
 	document.write('base_url += "&cb=" + AdsCB;');
 	document.write('if(typeof document.MAX_used != "undefined" && document.MAX_used != ",") base_url += "&exclude=" + document.MAX_used;');
-	document.write('base_url += "&source='+source.join(';')+'";');
+	document.write('base_url += "&hub={$cat['name']}";');
+	document.write('base_url += "&skin_name=" + skin;');
+	document.write('base_url += "&cont_lang=" + wgContentLanguage;');
+	document.write('base_url += "&user_lang=" + wgUserLanguage;');
+	document.write('base_url += "&dbname=" + wgDB;');
+	document.write('base_url += "&slotname={$slotname}";');
 	document.write('base_url += "&block=1";');
 	document.write('</scr'+'ipt>');
 	document.write('<scr'+'ipt type="text/javascript" src="'+base_url+'"></scr'+'ipt>');
@@ -90,20 +68,12 @@ EOT;
 
 	}
 
-	// Logic for zoneids documented here: http://staff.wikia-inc.com/wiki/Ad_Slots
-	public function getZoneId($slotname, $catid){
+	public function getZoneId($slotname){
 		if(isset($this->zoneIds[$slotname])){
 			return $this->zoneIds[$slotname];
-		} else if (AdEngine::getInstance()->getAdType($slotname) == 'spotlight'){
-			// For spotlights, they all have the same zoneid, determined by category.
-			if(isset($this->spotlightCategoryZones[$catid])){
-				return $this->spotlightCategoryZones[$catid];
 			} else {
-				return $this->spotlightCategoryZones['default'];
+				return $this->zoneIds['default'];
 			}
-		} else {
-			return null;
-		}
 	}
 
 }
