6fdef4e66673577d7a13a7d6905ec1006b580520
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-07-20 17:46:02 +0000

    * merge -c24620 https://svn.wikia-code.com/wikia/releases/201007.3
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24621 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/LyricWiki/server.php
+++ extensions/3rdparty/LyricWiki/server.php
@@ -2134,8 +2134,8 @@ function logSoapFailure($origArtistSql, $origSongSql, $lookedForSql){
 
 		// Spool a certain number of updates in memcache before writing to db.
 		// macbre: added str_replace (RT #59011)
-		// eloy, changed to md4 (faster hash algorithm)
-		$memkey = wfMemcKey( 'lw_soap_failure', md4( sprintf( "%s:%s", $origArtistSql, $origSongSql ) ) );
+		// eloy, changed to md5
+		$memkey = wfMemcKey( 'lw_soap_failure', md5( sprintf( "%s:%s", $origArtistSql, $origSongSql ) ) );
 		$numFails = $wgMemc->get( $memkey );
 		if(empty($numFails)){
 			wfDebug("LWSOAP: Setting $memkey to 1\n");
