3c9b177176f3728fee7a66ee80908336b66bce8a
moli <moli@wikia-inc.com>
2010-07-20 07:27:53 +0000

    blogs follow - final version of job
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24580 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/BlogsFolllowJob.php
+++ extensions/wikia/Blogs/BlogsFolllowJob.php
@@ -43,9 +43,28 @@ class BlogsFolllowJob extends Job {
 	 * @access public
 	 */
 	public function run() {
+		global $wgUseEnotif;
 
 		wfProfileIn( __METHOD__ );
 
+		if ( ! $wgUseEnotif ) {
+			wfProfileOut( __METHOD__ );
+			return true;
+		}
+
+		if ( $this->mParams['user_id'] ) {
+			/* registered User */
+			$editor = User::newFromID( $this->mParams['user_id'] );
+		} elseif ( $this->mParams['user_name'] ) {
+			/* anons */
+			$editor = User::newFromName( $this->mParams['user_name'], false );
+		} else {
+			/* invalid user */
+			wfProfileOut( __METHOD__ );
+			return true;
+		}
+
+		
 		/**
 		 * get title of page, take main part from this title which will be
 		 * main page for user blogs (listing)
@@ -65,21 +84,38 @@ class BlogsFolllowJob extends Job {
 			),
 			__METHOD__
 		);
+		
+		while ($row = $dbw->fetchObject( $res ) ) {
+			$watchers[] = intval( $row->wl_user );
+		}		
 
-		while( $row = $dbr->fetchObject( $sth ) ) {
-			$watchingUser = User::newFromId( $row->wl_user );
-
-			/**
-			 * check if user want to be emailed
-			 */
-			if( $watchingUser->getOption('enotifminoredits') && $watchingUser->isEmailConfirmed() ) {
-				/**
-				 * send info about creating new post, compose mail
-				 */
-				// $watchingUser->sendMail();
-			}
+		if ( !empty($watchers) ) {
+			$enotif = new EmailNotification();
+			$title = Title::makeTitle( $ownerTitle->getNamespace(), $ownerTitle->getDBKey() );
+			$enotif->actuallyNotifyOnPageChange( 
+				$editor, 
+				$title, 
+				$this->params['timestamp'],
+				$this->params['comment'],
+				$this->params['minor'],
+				0,
+				$watchers,
+				$this->params['log_action'] 
+			);
+							
+			/* Update wl_notificationtimestamp for all watching users except the editor */
+			$dbw = wfGetDB( DB_MASTER );
+			$dbw->begin();
+			$dbw->update( 'watchlist',
+				array( 'wl_notificationtimestamp' => $dbw->timestamp( $timestamp ) ), 
+				array( 
+					'wl_title' 		=> $ownerTitle->getDBkey(),
+					'wl_namespace'	=> $ownerTitle->getNamespace(),
+					'wl_user' 		=> $watchers
+				), __METHOD__
+			);
+			$dbw->commit();			
 		}
-
 
 		wfProfileOut( __METHOD__ );
 
