8639a09ff3cee8867cc1a5d4635532e47ad88572
wladek <wladek@wikia-inc.com>
2010-07-21 11:05:26 +0000

    RT#53308: Add German aliases
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24631 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/Blogs.alias.php
+++ extensions/wikia/Blogs/Blogs.alias.php
@@ -11,3 +11,10 @@ $aliases['en'] = array(
 	'CreateBlogPage'        => array( 'CreateBlogPage' ),
 	'CreateBlogListingPage' => array( 'CreateBlogListingPage' ),
 );
+
+/** German(Deutsch)
+ */
+$aliases['de'] = array(
+	'CreateBlogPage' => array( 'Blog-Beitrag_erstellen' , 'CreateBlogPage' ),
+	'CreateBlogListingPage' => array( 'Blog-Aggregation_erstellen', 'CreateBlogListingPage' ),
+);
