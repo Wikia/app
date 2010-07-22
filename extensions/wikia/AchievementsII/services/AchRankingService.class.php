9e9ae89043653fcb15a43fcc7e9b48ea12e47ab7
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-07-22 11:52:58 +0000

    * fix rt#57044: Banned users' badges should be removed
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24651 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AchievementsII/services/AchRankingService.class.php
+++ extensions/wikia/AchievementsII/services/AchRankingService.class.php
@@ -7,22 +7,29 @@ class AchRankingService {
 		$this->mRecentAwardedUsers = null;
 	}
 
-	public function getUsersRanking($limit = 100) {
+	public function getUsersRanking($limit = null, $compareToSnapshot = false) {
 		wfProfileIn(__METHOD__);
 
 		global $wgCityId, $wgWikiaBotLikeUsers, $wgExternalSharedDB;
 		$ranking = array();
+		$rules = array('ORDER BY' => 'score desc');
 
+		if($limit > 0)
+			$rules['LIMIT'] = $limit * 2;//bots and blocked users are filtered after the query has been run, let's admit that ratio is 2:1
+		
 		$dbr = wfGetDB(DB_SLAVE, array(), $wgExternalSharedDB);
-		$res = $dbr->select('ach_user_score', 'user_id, score', array('wiki_id' => $wgCityId), __METHOD__, array('ORDER BY' => 'score desc', 'LIMIT' => $limit));
-		$rankingSnapshot = $this->loadFromSnapshot();
+		$res = $dbr->select('ach_user_score', 'user_id, score', array('wiki_id' => $wgCityId), __METHOD__, $rules);
+		$rankingSnapshot = ($compareToSnapshot) ? $this->loadFromSnapshot() : null;
 		$positionCounter = 0;
 		
 		while($row = $dbr->fetchObject($res)) {
 		    $user = User::newFromId($row->user_id);
+
 		    if($user && !$user->isBlocked() && !in_array( $user->getName(), $wgWikiaBotLikeUsers ) ) {
 			$ranking[] = new AchRankedUser($user, $row->score, $positionCounter++, ($rankingSnapshot != null && isset($rankingSnapshot[$user->getId()])) ? $rankingSnapshot[$user->getId()] : null);
 		    }
+
+		    if($limit > 0 && $positionCounter == $limit) break;
 		}
 		
 		$dbr->freeResult($res);
@@ -32,8 +39,22 @@ class AchRankingService {
 		return $ranking;
 	}
 
+	public function getUserRankingPosition(User $user) {
+		if($user) {
+			$ranking = $this->getUsersRanking();
+
+			foreach($ranking as $position => $rankedUser) {
+				if($rankedUser->getId() == $user->getId()) return ++$position;
+			}
+
+			return count($ranking) + 1;
+		}
+		else
+			return false;
+	}
+
 	function serialize(){
-	    $ranking = $this->getUsersRanking(20);
+	    $ranking = $this->getUsersRanking();
 
 	    $result = array();
 
@@ -82,13 +103,18 @@ class AchRankingService {
 			$conds[] = 'badge_type_id NOT IN (' . implode($blackList) . ')';
 		
 		if($limit != null)
-			$rules['LIMIT'] = $limit;
+			$rules['LIMIT'] = $limit * 2; //bots and blocked users are filtered after the query hs been run, let's admit that ratio is 2:1
 		
 		$res = $dbr->select('ach_user_badges', 'user_id, badge_type_id, badge_lap, date', $conds, __METHOD__, $rules);
 		
 		while($row = $dbr->fetchObject($res)) {
-			if(AchConfig::getInstance()->isInRecents($row->badge_type_id))
-				$badges[] = array('user' => User::newFromId($row->user_id), 'badge' => new AchBadge($row->badge_type_id, $row->badge_lap), 'date' => $row->date);
+			if(AchConfig::getInstance()->isInRecents($row->badge_type_id)) {
+				$user = User::newFromId($row->user_id);
+				
+				if($user && !$user->isBlocked() && !in_array( $user->getName(), $wgWikiaBotLikeUsers ) ) {
+					$badges[] = array('user' => $user, 'badge' => new AchBadge($row->badge_type_id, $row->badge_lap), 'date' => $row->date);
+				}
+			}
 		}
 
 		$dbr->freeResult($res);
