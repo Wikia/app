26824f3d9967b3607846bb527d892746eeffcad7
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-07-22 17:14:17 +0000

    added nl custom magic footer (#58038)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24660 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MagicFooterLinksLoader/CustomMagicFooterLinks.php
+++ extensions/wikia/MagicFooterLinksLoader/CustomMagicFooterLinks.php
@@ -21,6 +21,7 @@ function wfAnswersMagicFooterLinks($results) {
 		"de" => "Besuche das [[w:c:websitewiki|WebsiteWiki]] und [[w:c:frag|Wikianswers: Frag Wikia!]]",
 		"es" => "[[w:c:respuestas|WikiRespuestas]]",
 		"fr" => "[http://www.wikia.com/Hiring/French-speaking_helpers Helper Team]", /* it's not really answers... */
+		"nl" = "Nieuws & Updates - [[w:c:nl|Centrale Wiki]] | Hulp Nodig? - [[w:c:hulp|Hulp Wiki]] | Vraag & Antwoord - [[w:c:antwoorden|Wikiantwoorden]]",
 	);
 
 	global $wgContLang;
