c75a5e75628689af4de9244b8cd8a8eede194561
wladek <wladek@wikia-inc.com>
2010-07-23 14:54:41 +0000

    RT#53307: Add German aliases for Achievements special pages
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24683 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AchievementsII/AchievementsII.alias.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English */
+$aliases['en'] = array(
+	'Leaderboard' => array( 'Leaderboard' ),
+	'AchievementsCustomize' => array( 'AchievementsCustomize' ),
+);
+
+/** German(Deutsch) */
+$aliases['de'] = array(
+	'Leaderboard' => array( 'Rangliste' ),
+	'AchievementsCustomize' => array( 'HerausforderungenAnpassen' ),
+);
