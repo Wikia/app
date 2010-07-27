e551b6f5c598fd51ffddae2e919c9685fec566e1
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-07-27 12:49:12 +0000

    time elapsing count
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24733 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Our404Handler/404handler.pl
+++ extensions/wikia/Our404Handler/404handler.pl
@@ -438,6 +438,8 @@ while( $request->Accept() >= 0 || $test ) {
 						my $h = $y2 - $y1;
 						my $geometry = sprintf( "%dx%d+%d+%d", $w, $h, $x1, $y1 );
 						$image->Crop( $geometry );
+						$t_elapsed = tv_interval( $t_start, [ gettimeofday() ] );
+						print STDERR "Cropping into $geometry, time: $t_elapsed\n" if $debug;
 					}
 
 					my $origw  = $image->Get( 'width' );
