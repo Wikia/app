a80649e35be475c0daa2821b38735749c547d5f0
siebrand <siebrand@wikia-inc.com>
2010-07-28 13:53:41 +0000

    * add i18n for description message * bump version
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24784 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SharedHelp/SharedHelp.i18n.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Internationalisation file for SharedHelp extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ */
+$messages['en'] = array(
+	'sharedhelp-desc' => 'Takes pages from [[w:c:Help|Help Wikia]] and inserts them into the {{ns:help}} namespace on this wiki',
+);
