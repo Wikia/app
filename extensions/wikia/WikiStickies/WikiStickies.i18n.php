2b391b77c3da0fd2a3d8597e34329199d224b417
siebrand <siebrand@wikia-inc.com>
2010-07-28 17:38:47 +0000

    Fix strange fsckup in r24797
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24800 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiStickies/WikiStickies.i18n.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @ingroup Wikia
+ * @package WikiStickies
+ * @file WikiStickies.i18n.php
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'wikistickies' => 'WikiStickies',
+	'wikistickies-desc' => 'The big, sticky Wiki Stickies to capture your eye!',
+	'wikistickies-newpages-hd' => 'Can you add information to one of these new pages?',
+	'wikistickies-withoutimages-hd' => 'Can you add a picture to one of these pages?',
+	'wikistickies-wantedpages-hd' => 'Can you start one of the articles that we need?',
+	'wikistickies-newpages-st' => '<span class="fake_newpages">Can you add information to the page about</span> $1?',
+	'wikistickies-wantedpages-st' => '<span class="fake_wantedpages">Can you start an article about</span> $1?',
+	'wikistickies-withoutimages-st' => '<span class="fake_withoutimages">Can you add a picture to the page about</span> $1?',
+	'wikistickies-newpages-st-short' => '<span>Can you add information to</span> $1?',
+	'wikistickies-wantedpages-st-short' => '<span>Can you start an article about</span> $1?',
+	'wikistickies-withoutimages-st-short' => '<span>Can you add a picture to</span> $1?',
+	'wikistickies-save-changes' => 'Save changes',
+	'wikistickies-more' => 'see more',
+	'wikistickies-admins-hd' => 'Admins only',
+	'wikistickies-admins-msg' => 'These are not your typical WikiStickies.
+These are things that can only be done by administrators of the wiki to influence how everyone sees the wiki.',
+	'wikistickies-logo-hd' => 'Upload a logo',
+	'wikistickies-theme-hd' => 'Choose a theme',
+	'wikistickies-custom-hd' => 'Create your own WikiStickies',
+	'wikistickies-custom-msg' => 'You can edit the [[MediaWiki:CommunityStickies|CommunityStickies page]] to add a new WikiStickies note!
+Add items to the list, and they will appear on My Home and on this page.',
+	'wikistickies-next' => 'Next ▒!',
+	'wikistickies-hide' => 'hide',
+	'wikistickies-see-more' => 'See more pages that need help',
+);
