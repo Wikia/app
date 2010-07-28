a7442a415f9b3dc980c40743af5173c7617319dd
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-07-28 13:28:14 +0000

    * tweak messages, add L10n for description message * format i18n file properly * add FIXME for incorrect message key * MEDIAWIKI check and extension author added
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24782 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RecipesTweaks/RecipesTweaks.i18n.php
+++ extensions/wikia/RecipesTweaks/RecipesTweaks.i18n.php
@@ -1,19 +1,21 @@
 <?php
+
 $messages = array();
+
 $messages['en'] = array(
-	'savedpages' => 'Saved Pages',
+	'recipestweaks-desc' => 'Design and functionality tweaks for Recipes Wiki',
+	'savedpages' => 'Saved pages', // FIXME: should have 'recipestweaks-' key prefix.
 	'recipes-add-new-recipe' => 'Add a recipe',
 	'recipes-browse' => 'Explore Recipes Wiki',
 	'recipes-contributed-by' => 'Contributed by $1 $2',
 	'recipes-contributed-by-anon' => 'A Recipes Wiki user',
 	'recipes-mainpage-title' => 'Welcome to Recipes Wiki',
 	'recipes-more' => 'More',
-	'recipes-my-saved-pages' => 'My Saved Pages',
+	'recipes-my-saved-pages' => 'My saved pages',
 	'recipes-my-saved-pages-empty' => 'No saved pages yet',
-	'recipes-rating' => 'This Recipe\'s Rating:',
+	'recipes-rating' => 'This recipe\'s rating:',
 	'recipes-search-label' => 'Search for a recipe',
-	'recipes-savedpages-empty' => 'You have no items in your saved pages collection',
+	'recipes-savedpages-empty' => 'You have no items in your saved pages collection.',
 	'recipes-savedpages-by' => 'by $1',
 	'recipes-history' => 'See changes to this recipe',
-
 );
