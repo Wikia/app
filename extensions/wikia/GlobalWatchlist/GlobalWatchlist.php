30f67733a659be680a0109244070a7ab154a2b96
siebrand <siebrand@wikia-inc.com>
2010-07-31 18:33:05 +0000

    * add L10n for description message * stylize.php
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24898 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GlobalWatchlist/GlobalWatchlist.php
+++ extensions/wikia/GlobalWatchlist/GlobalWatchlist.php
@@ -8,25 +8,25 @@
  *
  */
 
-if (!defined('MEDIAWIKI')) {
-	echo("This file is an extension to the MediaWiki software and cannot be used standalone.\n");
-	exit(1) ;
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
+	exit( 1 ) ;
 }
 
 $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'GlobalWatchlist',
 	'author' => '[http://www.wikia.com/wiki/User:Adi3ek Adrian \'ADi\' Wieczorek]',
-	'description' => 'Sending weekly digest email witch watchlisted pages on all wikis'
+	'descriptionmsg' => 'globalwatchlist-desc',
 );
 
 // configuration
 $wgGlobalWatchlistMaxDigestedArticlesPerWiki = 50;
 
 // message file
-$wgExtensionMessagesFiles['GlobalWatchlist'] = dirname(__FILE__) . '/GlobalWatchlist.i18n.php';
+$wgExtensionMessagesFiles['GlobalWatchlist'] = dirname( __FILE__ ) . '/GlobalWatchlist.i18n.php';
 
 // classes
-$wgAutoloadClasses['GlobalWatchlistBot'] = dirname(__FILE__) . '/GlobalWatchlistBot.class.php';
+$wgAutoloadClasses['GlobalWatchlistBot'] = dirname( __FILE__ ) . '/GlobalWatchlistBot.class.php';
 
 // hooks
 $wgHooks['getWatchlistPreferencesCustomHtml'][] = 'wfGlobalWatchlistPrefsCustomHtml';
@@ -34,16 +34,16 @@ $wgHooks['getUserProfilePreferencesCustomEmailToggles'][] = 'wfGlobalWatchlistPr
 
 // user toggles
 $wgHooks['UserToggles'][] = 'wfGlobalWatchlistToggle';
-//$wgHooks['WatchArticleComplete'][] = 'wfGlobalWatchArticleComplete';
-//$wgHooks['UnwatchArticleComplete'][] = 'wfGlobalUnwatchArticleComplete';
+// $wgHooks['WatchArticleComplete'][] = 'wfGlobalWatchArticleComplete';
+// $wgHooks['UnwatchArticleComplete'][] = 'wfGlobalUnwatchArticleComplete';
 
-function wfGlobalWatchlistToggle($extraToggles) {
+function wfGlobalWatchlistToggle( $extraToggles ) {
 	$extraToggles['watchlistdigest'] = 'watchlistdigest';
 	$extraToggles['watchlistdigestclear'] = 'watchlistdigestclear';
 	return true;
 }
 
-function wfGlobalWatchlistPrefsCustomHtml($prefsForm) {
+function wfGlobalWatchlistPrefsCustomHtml( $prefsForm ) {
 	global $wgOut, $wgUser, $wgExternalSharedDB;
 
 	/* Now extension is enabled for all users so I think the check is not necessary --- wladek
@@ -55,30 +55,29 @@ function wfGlobalWatchlistPrefsCustomHtml($prefsForm) {
 	if($oResultRow->count) {
 		// only for staff members at the moment
 	*/
-		wfLoadExtensionMessages('GlobalWatchlist');
+		wfLoadExtensionMessages( 'GlobalWatchlist' );
 
 		$tname = 'watchlistdigestclear';
 		$prefsForm->mUsedToggles[$tname] = true;
 
-		$wgOut->addHtml( $prefsForm->getToggle($tname) );
+		$wgOut->addHtml( $prefsForm->getToggle( $tname ) );
 	/*
 	}
 	*/
-		
+
 	return true;
 }
 
-function wfGlobalWatchlistPrefsEmailToggle($prefsForm, $toggleHtml) {
+function wfGlobalWatchlistPrefsEmailToggle( $prefsForm, $toggleHtml ) {
 	global $wgUser;
 
 	// only for staff members at the moment
-	wfLoadExtensionMessages('GlobalWatchlist');
+	wfLoadExtensionMessages( 'GlobalWatchlist' );
 
 	$tname = 'watchlistdigest';
 	$prefsForm->mUsedToggles[$tname] = true;
 
-	$toggleHtml .= $prefsForm->getToggle($tname) . '<br />';
+	$toggleHtml .= $prefsForm->getToggle( $tname ) . '<br />';
 
 	return true;
 }
-
