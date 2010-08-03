221a17eef277380985975c41feb7618627357338
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-08-03 09:30:16 +0000

    enable scribe onedot for some wikis
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24932 e846bd91-c042-42a7-a218-4b65e2c1904d

--- onedot.php
+++ onedot.php
@@ -4,27 +4,51 @@ set_include_path(get_include_path().PATH_SEPARATOR.dirname( __FILE__ )."/lib".PA
 require "./includes/api/ApiFormatJson_json.php";
 require_once "Stomp.php";
 
-
+$IP = dirname( __FILE__ );
 $key = "wikia.apache.stats.";
 $params = $_GET;
 if ( empty($_GET['c']) || empty($_GET['x']) ) {
 	exit(0);
 }
-$queue = (empty($_GET['db_test'])) ? $key : "";
-$queue .= "www-stats";
-try {
-	$json = new Services_JSON;
-	$stomp = new Stomp( (!empty($_GET['db_test'])) ? 'tcp://10.10.10.150:61613' : 'tcp://10.8.2.221:61613' );
-	$stomp->connect( 'guest', 'guest' );
-	$stomp->sync = false;
-	$stomp->send(
-		$queue,
-		$json->encode( $params ),
-		array( 'exchange' => 'amq.topic', 'bytes_message' => 1 )
-	);
-}
-catch( StompException $e ) {
-	error_log ('stomp_exception: ' . $e->getMessage());
+
+if ( in_array( $_GET['c'], array( 1657 /*ffxi*/, 147 /*starwars*/, 490 /*wowwiki*/ ) ) ) {
+	include_once $IP . "/extensions/wikia/Scribe/ScribeClient.php";
+
+	if ( !function_exists('wfProfileIn') ) 			{ function wfProfileIn() 	{ }; }
+	if ( !function_exists('wfProfileOut') ) 		{ function wfProfileOut() 	{ }; }
+	if ( !function_exists('wfDebug') ) 				{ function wfDebug() 		{ }; }
+
+	$params = $_GET;
+	try {
+		$wgScribeHost = "";
+		if ( $params['test'] == 1 ) {
+			$wgScribeHost = "10.10.10.150";
+		}
+		$params['lv'] = date("Y-m-d H:i:s");
+		$json = new Services_JSON;
+		$data = $json->encode( $params );
+		WScribeClient::singleton( 'log_view' )->send( $data );
+	}
+	catch( TException $e ) {
+		Wikia::log( __FILE__, 'scribeClient exception', $e->getMessage() );
+	}	
+} else {
+	$queue = (empty($_GET['db_test'])) ? $key : "";
+	$queue .= "www-stats";
+	try {
+		$json = new Services_JSON;
+		$stomp = new Stomp( (!empty($_GET['db_test'])) ? 'tcp://10.10.10.150:61613' : 'tcp://10.8.2.221:61613' );
+		$stomp->connect( 'guest', 'guest' );
+		$stomp->sync = false;
+		$stomp->send(
+			$queue,
+			$json->encode( $params ),
+			array( 'exchange' => 'amq.topic', 'bytes_message' => 1 )
+		);
+	}
+	catch( StompException $e ) {
+		error_log ('stomp_exception: ' . $e->getMessage());
+	}
 }
 
 /**
