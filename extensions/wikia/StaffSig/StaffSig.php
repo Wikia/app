a89f51cacd9bcd26e8d00147a0fdb96283a583b3
uberfuzzy <uberfuzzy@wikia-inc.com>
2010-08-03 19:46:30 +0000

    add a sig tag for helpers
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24982 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaffSig/StaffSig.php
+++ extensions/wikia/StaffSig/StaffSig.php
@@ -11,10 +11,16 @@ $wgHooks['ParserFirstCallInit'][] = 'wfSigSetup';
 
 function wfSigSetup(&$parser) {
 	$parser->setHook( 'staff', 'wfMakeSignature' );
+	$parser->setHook( 'helper', 'wfMakeHSignature' );
 	return true;
 }
 
 function wfMakeSignature( $contents, $attributes, $parser ) {
 	global $wgStylePath;
 	return '<a href="http://community.wikia.com/wiki/Project:Staff" title="This user is a member of Wikia staff" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
+}
+
+function wfMakeHSignature( $contents, $attributes, $parser ) {
+	global $wgStylePath;
+	return '<a href="http://community.wikia.com/wiki/Help:Helper_Group" title="This user is a Wikia Helper" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
 }
