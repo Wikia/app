efb1e26fd2ba70ba0899d8e7dd69e5dc3337831f
moli <moli@wikia-inc.com>
2010-08-03 14:14:26 +0000

    don't use it anymore
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24951 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiStats/templates/latestview.tmpl.php
+++ extensions/wikia/WikiStats/templates/latestview.tmpl.php
@@ -2,7 +2,6 @@
 <!-- LATEST VIEWS -->
 <div id="ws-latestview-stats">
 <div><?= wfMsg('wikistats_latest_pageviews') ?> <input type="button" id="ws-latestview-btn" value="<?= wfMsg('wikiastast_refresh_data') ?>" /></div>
-<div class="small" style="padding:0"><?= wfMsgExt('wikistats_latest_pageviews_subtitle', 'parsemag', 3) ?></div>
 <div>
 <?php if ( !empty($data) ) { ?>
 <ol class='special'>	
