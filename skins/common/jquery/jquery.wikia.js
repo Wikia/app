f45acb7588bb3a61da52523e7ddcdfa70f1ac374
marooned <marooned@wikia-inc.com>
2010-08-04 10:43:45 +0000

    use `search` instead of `href` so hash is not included in last param
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@24998 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/jquery/jquery.wikia.js
+++ skins/common/jquery/jquery.wikia.js
@@ -326,18 +326,17 @@ $.getEvent = function(e, boundEl) {
 
 //see http://jquery-howto.blogspot.com/2009/09/get-url-parameters-values-with-jquery.html
 $.extend({
-	getUrlVars: function(){
+	getUrlVars: function() {
 		var vars = [], hash;
-		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-		for(var i = 0; i < hashes.length; i++)
-		{
+		var hashes = window.location.search.slice(window.location.search.indexOf('?') + 1).split('&');
+		for (var i = 0; i < hashes.length; i++) {
 			hash = hashes[i].split('=');
 			vars.push(hash[0]);
 			vars[hash[0]] = hash[1];
 		}
 		return vars;
 	},
-	getUrlVar: function(name){
+	getUrlVar: function(name) {
 		return $.getUrlVars()[name];
 	}
 });
