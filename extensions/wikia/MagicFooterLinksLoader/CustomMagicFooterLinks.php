8bdbb2db0b76fe463c75b09551ed9ead2235df5e
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-08-19 10:55:28 +0000

    removed fr footer (#64174)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25549 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MagicFooterLinksLoader/CustomMagicFooterLinks.php
+++ extensions/wikia/MagicFooterLinksLoader/CustomMagicFooterLinks.php
@@ -20,7 +20,6 @@ function wfAnswersMagicFooterLinks($results) {
 	$links = array(
 		"de" => "Besuche das [[w:c:websitewiki|WebsiteWiki]] und [[w:c:frag|Wikianswers: Frag Wikia!]]",
 		"es" => "[[w:c:respuestas|WikiRespuestas]]",
-		"fr" => "[http://www.wikia.com/Hiring/French-speaking_helpers Helper Team]", /* it's not really answers... */
 		"nl" => "Nieuws & Updates - [[w:c:nl|Centrale Wiki]] | Hulp Nodig? - [[w:c:hulp|Hulp Wiki]] | Vraag & Antwoord - [[w:c:antwoorden|Wikiantwoorden]]",
 	);
 
