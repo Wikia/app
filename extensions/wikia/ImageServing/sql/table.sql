bcae5d36a3c8e250cb404776d912774e3d8e80fc
eloy <eloy@wikia-inc.com>
2010-08-20 12:42:04 +0000

    changed table definition
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25614 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageServing/sql/table.sql
+++ extensions/wikia/ImageServing/sql/table.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `page_wikia_props` (  
+CREATE TABLE /*$wgDBprefix*/page_wikia_props (  
 	`page_id` int(10) NOT NULL,
-	`propname` varchar(60) NOT NULL,
+	`propname`  int(10) NOT NULL,
 	`props` blob NOT NULL,
 PRIMARY KEY (`page_id`,`propname`) );
