08e4ddbc93f5cc05697916e81e13594fd5820ba1
eloy <eloy@wikia-inc.com>
2010-08-20 13:12:55 +0000

    alter table on page_wikia_props
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25618 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageServing/sql/table.sql
+++ extensions/wikia/ImageServing/sql/table.sql
@@ -2,4 +2,6 @@ CREATE TABLE /*$wgDBprefix*/page_wikia_props (
 	`page_id` int(10) NOT NULL,
 	`propname`  int(10) NOT NULL,
 	`props` blob NOT NULL,
-PRIMARY KEY (`page_id`,`propname`) );
+	PRIMARY KEY (`page_id`,`propname`) 
+) ENGINE=InnoDB;
+
