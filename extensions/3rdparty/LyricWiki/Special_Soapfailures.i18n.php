4bf0becd20ed26761fe92b44feef80cc463c652a
sean <sean@wikia-inc.com>
2010-08-26 23:39:31 +0000

    I can has English.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25815 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/LyricWiki/Special_Soapfailures.i18n.php
+++ extensions/3rdparty/LyricWiki/Special_Soapfailures.i18n.php
@@ -8,7 +8,7 @@ $messages['en'] = array(
 	'soapfailures-artist' => 'Artist:',
 	'soapfailures-song' => 'Song:',
 	'soapfailures-intro' => "<em>Once you have created a missing page, made a redirect for it, or otherwise fixed it so that it should 
-			no longer be a failed request... type the artist and song name into the form at the top of the page and click \"Fixed\" link next to the title and the SOAP webservice will test the song again. 
+			no longer be a failed request... type the artist and song name into the form at the top of the page and click the \"Fixed\" button and the SOAP webservice will test the song again. 
 			If the song is then retrieved successfully, it will be removed from the failures list and the cache will be cleared 
 			so that you can see the updated list right away.</em><br/>
 			<br/>
