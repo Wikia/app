475f9e174014a8b52f10e07e148cb4f123e235ed
hyun <hyun@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-08-30 17:27:35 +0000

    fixed styling in oasis
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25899 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/Search_Index.php
+++ skins/oasis/modules/templates/Search_Index.php
@@ -1,5 +1,5 @@
 <form id="WikiaSearch" class="WikiaSearch" action="index.php?title=Special:Search" method="get">
-	<input type="search" name="search" placeholder="<?= $placeholder ?>" autocomplete="off" accesskey="f">
+	<input type="text" name="search" placeholder="<?= $placeholder ?>" autocomplete="off" accesskey="f">
 	<input type="hidden" name="fulltext" value="<?= $fulltext ?>">
 	<input type="submit">
 	<button class="wikia-chiclet-button"><img src="<?= $wgBlankImgUrl ?>"></button>
