b8c6b3024e51cfa760f544d966ddb6c9746bc5e4
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-08-31 08:22:13 +0000

    ArticleCategories: use HTML rendered by MW
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25919 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/ArticleCategoriesModule.class.php
+++ skins/oasis/modules/ArticleCategoriesModule.class.php
@@ -1,27 +1,9 @@
 <?php
 class ArticleCategoriesModule extends Module {
-
-	var $categories;
-	var $categoriesLink;
+	var $catlinks;
 
 	public function executeIndex() {
 		wfProfileIn(__METHOD__);
-
-		global $wgOut;
-
-		// get list of categories for current article
-		$this->categories = $wgOut->getCategoryLinks();
-
-		if (!empty($this->categories['normal'])) {
-			$this->categories = $this->categories['normal'];
-		}
-		else {
-			$this->categories = array();
-		}
-
-		// render "Categories:" link
-		$msg = wfMsgExt('pagecategories', array('parsemag'), count($this->categories));
-		$this->categoriesLink = View::link(Title::newFromText(wfMsgForContent('pagecategorieslink')), $msg);
 
 		wfProfileOut(__METHOD__);
 	}
