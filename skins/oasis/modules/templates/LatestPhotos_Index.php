0dc9166d50429a1a96432b624bfc3ad6d1bf074c
bernhard <bernhard@wikia-inc.com>
2010-08-31 02:40:20 +0000

    latest photos update for angie
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25916 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/LatestPhotos_Index.php
+++ skins/oasis/modules/templates/LatestPhotos_Index.php
@@ -1,6 +1,3 @@
-<!--
-Owens backend
--->
 <section class="LatestPhotosModule">
 		<?= View::specialPageLink('Upload', 'oasis-add-photo', 'wikia-button'); ?>
 	<details class="tally counter">
@@ -25,13 +22,14 @@ else {
 		?>
 		<a href="#" class="previous<?= $class ?>"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-left"></a>
 		<a href="#" class="next <?= $class ?>"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-right"></a>
-	<ul class="carousel">		
+	<div class="carousel-container">
+		<div>
+			<ul class="carousel">	
 	<?php
 	$count = 1;
-	foreach ($thumbUrls as $url) {
-		echo sprintf('<li class="%s"><a href="%s"><img src="%s" /></a></li>',
-				$count, $url["file_url"], $url["thumb_url"]);
-
+	foreach ($thumbUrls as $url) {?>
+		<li><a href="<?= $url["file_url"] ?>"><img src="<?= $url["thumb_url"] ?>" /></a></li>
+	<?php
 	}
 	?>
 	<?php 
@@ -45,11 +43,47 @@ else {
 
 	}?>
 
-	</ul>
-
+			</ul>
+		</div>
+	</div>
 	<?= View::specialPageLink('NewFiles', 'oasis-latest-photos-more', array('class' => 'more')) ?>
 	
 	
 <?php }
 	?>
 </section>
+
+
+<!--
+Bernhard's test front end
+-->
+<!--
+<section class="LatestPhotosModule">
+	<a class="wikia-button">Add a Photo</a>
+	<details class="counter">
+		<em>9,173</em><?= wfMsg('oasis-latest-photos-header') ?>
+	</details>
+	
+		<a href="#" class="previous"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-left"></a>
+		<a href="#" class="next"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-right"></a>
+		<div class="carousel-container">
+			<div>
+				<ul class="carousel">
+					<li class="1"><a href="#"><img src="/skins/oasis/images/latestphotos/1.jpg" /></a></li>
+					<li class="2"><a href="#"><img src="/skins/oasis/images/latestphotos/2.jpg" /></a></li>
+					
+					<li class="3"><a href="#"><img src="/skins/oasis/images/latestphotos/3.jpg" /></a></li>
+					<li class="4"><a href="#"><img src="/skins/oasis/images/latestphotos/4.jpg" /></a></li>
+					<li class="5"><a href="#"><img src="/skins/oasis/images/latestphotos/5.jpg" /></a></li>
+					<li class="6"><a href="#"><img src="/skins/oasis/images/latestphotos/6.jpg" /></a></li>	
+					<li class="7"><a href="#"><img src="/skins/oasis/images/latestphotos/7.jpg" /></a></li>	
+					<li class="8"><a href="#"><img src="/skins/oasis/images/latestphotos/8.jpg" /></a></li>		
+					<li class="9"><a href="#"><img src="/skins/oasis/images/latestphotos/9.jpg" /></a></li>
+					<li class="10"><a href="#"><img src="/skins/oasis/images/latestphotos/10.jpg" /></a></li>
+					<li class="see-all"><a href="#">See All Photos</a></li>		
+				</ul>
+			</div>
+		</div>
+	<a class="more">See All Photos in This Gallery ></a>
+</section>
+-->
