bf2323361d25df067da225ea643b13afbdc364d0
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-08-31 10:11:03 +0000

    LatestPhotos: HTML cleanup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25932 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/LatestPhotos_Index.php
+++ skins/oasis/modules/templates/LatestPhotos_Index.php
@@ -3,7 +3,7 @@
 	<details class="tally counter">
 		<em>9,173</em><?= wfMsg('oasis-latest-photos-header') ?>
 	</details>
-	
+
 <?php
 if ($enableEmptyGallery == true) { ?>
 	<details class="empty-photos">
@@ -21,10 +21,10 @@ else {
 	}
 		?>
 		<a href="#" class="previous<?= $class ?>"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-left"></a>
-		<a href="#" class="next <?= $class ?>"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-right"></a>
+		<a href="#" class="next<?= $class ?>"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-right"></a>
 	<div class="carousel-container">
 		<div>
-			<ul class="carousel">	
+			<ul class="carousel">
 	<?php
 	$count = 1;
 	foreach ($thumbUrls as $url) {?>
@@ -32,14 +32,14 @@ else {
 	<?php
 	}
 	?>
-	<?php 
+	<?php
 	if (count($thumbUrls) > 2) { ?>
 		<li class="see-all"><?= View::specialPageLink('NewFiles', 'oasis-latest-photos-inner-message') ?></li>
-	<?php 
+	<?php
 	}
 	else {?>
-		<li class="add-more"><?= View::specialPageLink('Upload', 'oasis-latest-photos-single') ?></li>	
-		<?php 
+		<li class="add-more"><?= View::specialPageLink('Upload', 'oasis-latest-photos-single') ?></li>
+		<?php
 
 	}?>
 
@@ -47,8 +47,8 @@ else {
 		</div>
 	</div>
 	<?= View::specialPageLink('NewFiles', 'oasis-latest-photos-more', array('class' => 'more')) ?>
-	
-	
+
+
 <?php }
 	?>
 </section>
@@ -63,7 +63,7 @@ Bernhard's test front end
 	<details class="counter">
 		<em>9,173</em><?= wfMsg('oasis-latest-photos-header') ?>
 	</details>
-	
+
 		<a href="#" class="previous"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-left"></a>
 		<a href="#" class="next"><img src="<?= $wgBlankImgUrl; ?>" class="latest-images-right"></a>
 		<div class="carousel-container">
@@ -71,16 +71,16 @@ Bernhard's test front end
 				<ul class="carousel">
 					<li class="1"><a href="#"><img src="/skins/oasis/images/latestphotos/1.jpg" /></a></li>
 					<li class="2"><a href="#"><img src="/skins/oasis/images/latestphotos/2.jpg" /></a></li>
-					
+
 					<li class="3"><a href="#"><img src="/skins/oasis/images/latestphotos/3.jpg" /></a></li>
 					<li class="4"><a href="#"><img src="/skins/oasis/images/latestphotos/4.jpg" /></a></li>
 					<li class="5"><a href="#"><img src="/skins/oasis/images/latestphotos/5.jpg" /></a></li>
-					<li class="6"><a href="#"><img src="/skins/oasis/images/latestphotos/6.jpg" /></a></li>	
-					<li class="7"><a href="#"><img src="/skins/oasis/images/latestphotos/7.jpg" /></a></li>	
-					<li class="8"><a href="#"><img src="/skins/oasis/images/latestphotos/8.jpg" /></a></li>		
+					<li class="6"><a href="#"><img src="/skins/oasis/images/latestphotos/6.jpg" /></a></li>
+					<li class="7"><a href="#"><img src="/skins/oasis/images/latestphotos/7.jpg" /></a></li>
+					<li class="8"><a href="#"><img src="/skins/oasis/images/latestphotos/8.jpg" /></a></li>
 					<li class="9"><a href="#"><img src="/skins/oasis/images/latestphotos/9.jpg" /></a></li>
 					<li class="10"><a href="#"><img src="/skins/oasis/images/latestphotos/10.jpg" /></a></li>
-					<li class="see-all"><a href="#">See All Photos</a></li>		
+					<li class="see-all"><a href="#">See All Photos</a></li>
 				</ul>
 			</div>
 		</div>
