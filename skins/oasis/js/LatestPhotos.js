0ecbc14daaf1a60da1989c3ecf53edba080f7970
bernhard <bernhard@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-08-31 02:40:20 +0000

    latest photos update for angie
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25916 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/LatestPhotos.js
+++ skins/oasis/js/LatestPhotos.js
@@ -4,46 +4,73 @@ $(function() {
 
 var LatestPhotos = {
 	browsing: false,
-	transition_speed: 200,
+	transition_speed: 500,
 	init: function() {
 		this.attachListeners();
 	},
 	attachListeners: function() {
+		LatestPhotos.attachBlindImages();
+	
 		$(".LatestPhotosModule").find(".next").click(LatestPhotos.nextImage);
 		$(".LatestPhotosModule").find(".previous").click(LatestPhotos.previousImage);
 	},
+	
+	attachBlindImages: function() {
+		if ($('.carousel li').length == 5) {
+			$('.carousel').append("<li class='blind'></li>");
+		}
+		else if ($('.carousel li').length == 4) {
+			$('.carousel').append("<li class='blind'></li>");
+			$('.carousel').append("<li class='blind'></li>");
+		}
+		else {
+			$('.carousel').append("<li class='blind'></li>");
+		}
+	},
+	
 	nextImage: function() {
-		var visibles = $(".LatestPhotosModule li:visible");
-		var invisbles = $(".LatestPhotosModule li:hidden");
-		var next = $(visibles).last().next();		
+		var width = LatestPhotos.setCarouselWidth();
+		
 		if (LatestPhotos.browsing == false) {
-			if (!$(next).exists()) {
-				invisbles.remove();
-				$(visibles).parent().append(invisbles);
-				next = $(visibles).last().next();
-			}
 			LatestPhotos.browsing = true;
-			$(visibles).first().hide(LatestPhotos.transition_speed);
-			$(next).show(LatestPhotos.transition_speed, function() {
+			$(".carousel-container div").animate({
+				left: '-' + width
+			}, LatestPhotos.transition_speed, function() {
+				LatestPhotos.removeFirstPhotos();
 				LatestPhotos.browsing = false;
 			});
 		}
 		return false;
 	},
+	
+	removeFirstPhotos: function() {
+		var old = $('.carousel li').slice(0,3);
+		$('.carousel-container div').css('left', '0px');
+		$('.carousel li').slice(0,3).remove();
+		$('.carousel').append(old);
+		
+	},
+	
+	setCarouselWidth: function() {
+		var width = $(".carousel li").outerWidth() * 3 + 6;
+		$('.carousel').css('width', width * $(".carousel li").length + 'px'); // all li's in one line
+		return width;
+	},
+	
 	previousImage: function() {
-		var visibles = $(".LatestPhotosModule li:visible");
-		var invisibles = $(".LatestPhotosModule li:hidden");
-		var prev = $(visibles).first().prev();
+		var width = LatestPhotos.setCarouselWidth();
+		
 		if (LatestPhotos.browsing == false) {
-			$(".LatestPhotosModule li:visible").last().hide(LatestPhotos.transition_speed);
-			if (!$(prev).exists()) {
-				var last = $(invisibles).last();
-				$(invisibles).last().remove();
-				$(visibles).parent().prepend(last);
-				$(".LatestPhotosModule li").first().hide();
-			}
 			LatestPhotos.browsing = true;
-			$(visibles).prev().first().show(LatestPhotos.transition_speed, function() {
+			var images = $('.carousel li').length;
+			for (i=0; i < 3; i++) {
+				$('.carousel').prepend( $('.carousel li').eq(images -1) ) ;
+			}
+			$(".carousel-container div").css('left', - width + 'px');
+			
+			$(".carousel-container div").animate({
+				left:  '0px'
+			}, LatestPhotos.transition_speed, function() {
 				LatestPhotos.browsing = false;
 			});
 		}
