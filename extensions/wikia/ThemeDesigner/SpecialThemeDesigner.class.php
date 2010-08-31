3d71aef288ffd39d67d06a744d7c5555d1616ad6
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-08-31 11:30:40 +0000

    Theme designer: add user right for special page and check for it
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25937 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/SpecialThemeDesigner.class.php
+++ extensions/wikia/ThemeDesigner/SpecialThemeDesigner.class.php
@@ -7,8 +7,20 @@ class SpecialThemeDesigner extends UnlistedSpecialPage {
 	}
 
 	public function execute() {
+		wfProfileIn(__METHOD__);
+		global $wgUser;
+
+		// check rights
+		if (!$wgUser->isAllowed('themedesigner')) {
+			$this->displayRestrictionError();
+			wfProfileOut(__METHOD__);
+			return;
+		}
+
 		Wikia::setVar('OasisEntryModuleName', 'ThemeDesigner');
 		
 		wfLoadExtensionMessages('ThemeDesigner');
+
+		wfProfileOut(__METHOD__);
 	}
 }
