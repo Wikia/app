15ba7ded638cc9a0faca97fb037e97ad2054737c
macbre <macbre@wikia-inc.com>
2010-09-01 08:54:52 +0000

    NotificationsModule: add unit test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25976 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/ModuleDataTests.php
+++ tests/ModuleDataTests.php
@@ -26,7 +26,7 @@ class ModuleDataTests extends PHPUnit_Framework_TestCase {
 	function testRailModule() {
 		global $wgTitle;
 		$wgTitle = Title::newFromText('Dev_Box_Trick_Wiki');
-		
+
 		$moduleData = Module::get('Rail')->getData();
 
 		$this->assertType("array",
@@ -53,8 +53,36 @@ class ModuleDataTests extends PHPUnit_Framework_TestCase {
 
 		// boxad is 300 wide
 		$this->assertEquals(
-				'300',
-				$moduleData['imgWidth']
-				);
+			'300',
+			$moduleData['imgWidth']
+		);
+	}
+
+	function testNotificationsModule() {
+		// add notification
+		NotificationsModule::addNotification('Notification about something very important', array('data' => 'bar'));
+
+		$moduleData = Module::get('Notifications')->getData();
+
+		$this->assertEquals(
+			array(
+				array(
+					'message' => 'Notification about something very important',
+					'data' => array('data' => 'bar'),
+					'type' => 1,
+				)
+			),
+			$moduleData['notifications']
+		);
+
+		// add confirmation
+		NotificationsModule::addConfirmation('Confirmation of something done');
+
+		$moduleData = Module::get('Notifications', 'Confirmation')->getData();
+
+		$this->assertEquals(
+			'Confirmation of something done',
+			$moduleData['confirmation']
+		);
 	}
 }
