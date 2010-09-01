52a4daee21fd6a157e114bfd6e34b676bec5b6ba
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-01 08:22:14 +0000

    ThemeDesigner: add images revisions to settings structure
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@25974 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/js/ThemeDesigner.js
+++ extensions/wikia/ThemeDesigner/js/ThemeDesigner.js
@@ -15,10 +15,12 @@ var ThemeDesigner = {
 		"wordmark-font": "Orbitron",
 		"wordmark-font-size": "",
 		"wordmark-image": false,
+		"wordmark-image-revision": false,
 		"banner-image": false,
-		"background-color": "",
+		"banner-image-revision": false,
 		"background-image": false,
-		"background-tiled": false	
+		"background-image-revision": false,
+		"background-tiled": false
 	},
 
 	init: function() {
@@ -26,35 +28,37 @@ var ThemeDesigner = {
 		$.extend(ThemeDesigner.settings, themeSettings);
 		ThemeDesigner.applySettings();
 
+		$().log(ThemeDesigner.settings, 'ThemeDesigner');
+
 		// iframe resizing
 		$(window).resize(ThemeDesigner.resizeIframe).resize();
-		
+
 		// handle navigation clicks
 		$("#Navigation a").click(ThemeDesigner.navigationClick);
-		
+
 		// init theme tab
 		ThemeDesigner.ThemeTabInit();
-		
+
 		// init wordmark tab
 		ThemeDesigner.WordmarkTabInit();
-		
+
 		// click first tab
-		$("#Navigation a:first").click();				
+		$("#Navigation a:first").click();
 	},
-	
+
 	applySettings: function() {
 		$("#swatch-color-background").css("background-color", ThemeDesigner.settings["color-body"]);
 		$("#swatch-color-buttons").css("background-color", ThemeDesigner.settings["color-buttons"]);
 		$("#swatch-color-links").css("background-color", ThemeDesigner.settings["color-links"]);
 		$("#swatch-color-page").css("background-color", ThemeDesigner.settings["color-page"]);
 	},
-	
+
 	navigationClick: function(event) {
 		event.preventDefault();
-		
+
 		var clickedLink = $(this);
 		var command = clickedLink.attr("rel");
-		
+
 		clickedLink.parent().addClass("selected").siblings().removeClass("selected");
 		$("#" + command).show().siblings("section").hide();
 	},
@@ -68,7 +72,7 @@ var ThemeDesigner = {
 	ThemeTabInit: function() {
 		$("#ThemeTab .previous, #ThemeTab .next").click(ThemeDesigner.themeSlider);
 	},
-	
+
 	themeSlider: function(event) {
 		event.preventDefault();
 		var list = $("#ThemeTab .slider ul");
@@ -78,17 +82,17 @@ var ThemeDesigner = {
 		if (arrow.hasClass("disabled")) {
 			return;
 		}
-		
+
 		var slideTo = (arrow.hasClass("previous")) ? -760 : 0;
 		list.animate({
 			marginLeft: slideTo
 		}, "slow");
 		$("#ThemeTab .next, #ThemeTab .previous").toggleClass("disabled");
 	},
-	
+
 	WordmarkTabInit: function() {
 		var fonts = ["Helvetica", "Capralba", "Fontin", "IM Fell", "Josefin Sans", "Orbitron", "Prociono", "Reenie Beanie", "Tangerine", "Titillium", "Yanone Kaffeesatz"];
-		
+
 		// populate font menu
 		var options = '';
 		for(i=0; i<fonts.length; i++) {
@@ -99,9 +103,9 @@ var ThemeDesigner = {
 			options += '<option value="' + fonts[i] + '"' + selected +'>' + fonts[i] + '</option>';
 		}
 		$("#wordmark-font").html(options);
-		
+
 		// populate wordmark text
 		$("#wordmark").html(ThemeDesigner.settings["wordmark-text"]);
 	}
-	
+
 };
