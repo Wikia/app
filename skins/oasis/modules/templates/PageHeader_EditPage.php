61bade9735cd2a3647843aeaf0d32a3f8e72daea
bernhard <bernhard@wikia-inc.com>
2010-09-01 18:14:13 +0000

    adding jump from edit button to editor
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26016 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/PageHeader_EditPage.php
+++ skins/oasis/modules/templates/PageHeader_EditPage.php
@@ -1,4 +1,5 @@
 <div id="WikiaPageHeader" class="WikiaPageHeader">
+	<a name="EditPage"></a>
 	<h1><?= $displaytitle != "" ? $title : htmlspecialchars($title) ?></h1>
 	<p><?= $subtitle ?></p>
 </div>
