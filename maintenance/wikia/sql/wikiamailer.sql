07d416bf2e22f381b7f7ad6be47d6c62232c17aa
artur <artur@wikia-inc.com>
2010-09-01 20:52:24 +0000

    store generated emails in the database so we dont loose em
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26026 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/sql/wikiamailer.sql
@@ -0,0 +1,28 @@
+
+CREATE DATABASE wikia_mailer;
+use wikia_mailer;
+
+CREATE TABLE mail  IF NOT EXISTS (
+       id INT PRIMARY KEY AUTO_INCREMENT,
+       created DATETIME NOT NULL DEFAULT NOW(), # when we sent it from apache
+       src VARCHAR(255) NOT NULL,            
+       dst VARCHAR(255) NOT NULL,
+       hdr TEXT NOT NULL,
+       msg TEXT NOT NULL,
+       city_id INT NOT NULL,
+       locked_by VARCHAR(255),  # which sender has it    
+       locked DATETIME,         # when the sender grabbed it
+       transmitted DATETIME,    # when we sent it to the outsideworld
+       is_bounce BOOL NOT NULL DEFAULT FALSE,
+       is_error BOOL NOT NULL DEFAULT FALSE,
+       is_spam BOOL NOT NULL DEFAULT FALSE,
+       error_status VARCHAR(255),
+       error_msg VARCHAR(255),
+       opened DATETIME,
+       clicked DATETIME,
+       INDEX(dst),
+       INDEX(city_id),
+       INDEX(locked_by)
+);
+
+
