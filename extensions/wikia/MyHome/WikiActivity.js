b9ea0b5d03a3feb3f724db38350b4cdfca6047aa
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-02 09:05:28 +0000

    WikiActivity: move some code from MyHome.js, so only WikiActivity.js has to be loaded
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26075 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MyHome/WikiActivity.js
+++ extensions/wikia/MyHome/WikiActivity.js
@@ -7,11 +7,18 @@ var WikiActivity = {
 	init: function() {
 		WikiActivity.track('view');
 
+		// handle clicks on "more"
+		$('.activity-feed-more').children('a').click(WikiActivity.fetchMore);
+
 		// track clicks within activity feed
 		$('#myhome-activityfeed').click(WikiActivity.trackClick);
 
 		// track clicks on link to Special:RecentChanges (Oasis specific)
 		$('#WikiaPageHeader').find('a').trackClick('wikiactivity/recentchanges');
+	},
+
+	ajax: function(method, params, callback) {
+		$.getJSON(wgScript + '?action=ajax&rs=MyHomeAjax&method=' + method, params, callback);
 	},
 
 	track: function(fakeUrl) {
@@ -22,7 +29,7 @@ var WikiActivity = {
 		var node = $(ev.target);
 
 		// fix for images inside links
-		if (node.is('img')) {
+		if (node.is('img') || node.is('span') /* video play icon */) {
 			node = node.parent();
 		}
 
@@ -61,5 +68,39 @@ var WikiActivity = {
 					break;
 			}
 		}
+	},
+
+	// load next x entries when "more" is clicked
+	fetchMore: function(ev) {
+		ev.preventDefault();
+
+		var node = $(ev.target);
+
+		var feedContent = $('#myhome-activityfeed');
+		var fetchSince = node.attr('data-since');
+
+		$().log('fetching feed since ' + fetchSince, 'WikiActivity');
+		WikiActivity.track('more');
+
+		// get the data
+		WikiActivity.ajax('getFeed', {
+			'type': 'activity',
+			'since': fetchSince
+		}, function(data) {
+			if (data.html) {
+				// add rows
+				$(data.html).insertAfter(feedContent);
+
+				// show more?
+				if (data.fetchSince) {
+					// store new timestamp to fetch from
+					node.attr('data-since', data.fetchSince);
+				}
+				else {
+					// remove "more" link
+					node.parent().remove();
+				}
+			}
+		});
 	}
 };
