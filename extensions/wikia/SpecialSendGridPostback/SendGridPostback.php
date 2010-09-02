fa97387a616191a5bf6eb6f3986e798540633b5e
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-02 00:25:34 +0000

    Skeleton of special page for SendGrid to postback to.  We don't know exactly what they're sending yet, so we'll log it for now.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26039 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialSendGridPostback/SendGridPostback.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * A SpecialPage which SendGrid will post data to when an email is opened, clicked, bounced back, unsubscribed, marked as spam, etc..
+ *
+ * @author Sean Colombo
+ */
+
+$wgSpecialPages['SendGridPostback'] = 'SendGridPostback';
+/*$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'SendGridPostback',
+	'url' => 'http://lyrics.wikia.com/User:Sean_Colombo', // FIXME: please point to a location where to get more information on this extension (presumably mediawiki.org/wiki/Extension/BatchUserRights
+	'author' => '[http://www.seancolombo.com Sean Colombo]',
+	'descriptionmsg' => 'sendgridpostback-desc',
+	'version' => '1.0',
+);*/
+
+/**
+ * @ingroup SpecialPage
+ */
+class SendGridPostback extends SpecialPage {
+
+	public function __construct() {
+		parent::__construct( 'SendGridPostback' );
+	}
+
+	public function isRestricted() {
+		return false;
+	}
+
+	/**
+	 * @param $par Mixed: string if any subpage provided, else null
+	 */
+	function execute( $par ) {
+		
+		// Just to see what they're sending us.
+		ob_start();
+		global $_POST;
+		print_r($_POST);
+		$data = ob_get_clean();
+		wfErrorLog($data);
+		
+		global $wgOut;
+		$wgOut->addHMTL("Post-data logged to error log");
+		
+	}
+
+} // end class SendGridPostback
