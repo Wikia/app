61c6fcbd5ba393c48d526c9f2cfb14d3dddbabb7
christian <christian@wikia-inc.com>
2010-09-02 02:02:34 +0000

    theme designer work
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26052 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/templates/ThemeDesigner_WordmarkTab.php
+++ extensions/wikia/ThemeDesigner/templates/ThemeDesigner_WordmarkTab.php
@@ -1,18 +1,49 @@
 <section id="WordmarkTab" class="WordmarkTab">
 	<fieldset class="text">
 		<h1><?= wfMsg('themedesigner-text-wordmark') ?></h1>
-		<button class="secondary"><img src="<?= $wgStylePath ?>/common/blank.gif"> Edit</button>
-		<select id="wordmark-font">
-			<option value="">Default</option>
-		</select>
-		<select id="wordmark-size">
-			<option value="small"><?= wfMsg('themedesigner-small') ?></option>
-			<option value="medium"><?= wfMsg('themedesigner-medium') ?></option>
-			<option value="large"><?= wfMsg('themedesigner-large') ?></option>
-		</select>
-		<span id="wordmark"></span>
+		
+		<ul class="controls">
+			<li>
+				<button class="secondary" id="wordmark-edit-button"><img src="<?= $wgStylePath ?>/common/blank.gif"> Edit</button>
+			</li>
+			<li>
+				<h2>font</h2>
+				<select id="wordmark-font">
+					<option value="default">Default</option>
+					<option value="fontin">Fontin</option>
+					<option value="garton">Garton</option>
+					<option value="gondola">Gondola</option>
+					<option value="idolwild">Idolwild</option>
+					<option value="imfell">IM Fell</option>
+					<option value="josefin">Josefin</option>
+					<option value="orbitron">Orbitron</option>
+					<option value="prociono">Prociono</option>
+					<option value="speakeasy">Speakeasy</option>
+					<option value="tangerine">Tangerine</option>
+					<option value="titillium">Titillium</option>
+					<option value="yanone">Yanone</option>
+				</select>
+			</li>
+			<li>
+				<h2>size</h2>
+				<select id="wordmark-size">
+					<option value="small"><?= wfMsg('themedesigner-small') ?></option>
+					<option value="medium"><?= wfMsg('themedesigner-medium') ?></option>
+					<option value="large"><?= wfMsg('themedesigner-large') ?></option>
+				</select>
+			</li>
+		</ul>
+				
+		<div id="wordmark"></div>
+		
+		<div id="wordmark-edit">
+			<input type="text">
+			<button>Save</button>
+		</div>
+	
 	</fieldset>
 	<fieldset class="graphic">
 		<h1><?= wfMsg('themedesigner-graphic-wordmark') ?></h1>
+		
 	</fieldset>
 </section>
