e74d151577728b3c04bb5b75d565451a01090a69
wladek <wladek@wikia-inc.com>
2010-09-03 14:51:20 +0000

    fixed imageserving pull from cache bug
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26156 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageServing/imageServing.class.php
+++ extensions/wikia/ImageServing/imageServing.class.php
@@ -11,13 +11,13 @@ class imageServing{
 	private $proportion;
 	private $deltaY = 0;
 	private $db;
-	
+
 	/**
 	 * @param $articles \type{\arrayof{\int}} List of articles ids to get images
 	 * @param $articles \type{\arrayof{\int}} List of articles ids to get images
 	 * @param $width \int image width
-	 * @param $width \int 
-	 */ 
+	 * @param $width \int
+	 */
 	function __construct($articles, $width = 100, $proportion = array("w" => 1, "h" => 1), $db = null){
 		$this->articles = $articles;
 		$this->width = $width;
@@ -25,18 +25,18 @@ class imageServing{
 		$this->deltaY = (round($proportion['w']/$proportion['h']) - 1)*0.1;
 		$this->db = $db;
 	}
-	
+
 	/**
-	 * getImages - get array with list of top images for all article pass to construct 
+	 * getImages - get array with list of top images for all article pass to construct
 	 *
 	 * @access public
-	 * 
-	 * @param $n \type{\arrayof{\int}} number of images get of each of article 
-	 * @param $article_lp \int lp number of article pass to construct for 0 get all 
-	 * 
+	 *
+	 * @param $n \type{\arrayof{\int}} number of images get of each of article
+	 * @param $article_lp \int lp number of article pass to construct for 0 get all
+	 *
 	 * @return  \type{\arrayof{\topImage}}
 	 */
-		
+
 	public function getImages($n = 5, $article_lp = 0) {
 		global $wgMemc;
 		$articles = $this->articles;
@@ -45,39 +45,39 @@ class imageServing{
 		foreach($articles as $key => $value) {
 			$mcKey = wfMemcKey("imageserving", $this->width, $n, $this->proportion["w"], $this->proportion["h"], $value);
 			$mcOut = $wgMemc->get($mcKey, null);
-			
+
 			if($mcOut != null) {
 				unset($articles[$key]);
-				$cache_return[] = $mcOut;
+				$cache_return[$value] = $mcOut;
 			}
 		}
-		
+
 		if(count($articles) < 1) {
 			return $cache_return;
 		}
-		
-		
+
+
 		if($this->db == null) {
-			$db = wfGetDB(DB_SLAVE, array());	
+			$db = wfGetDB(DB_SLAVE, array());
 		} else {
 			$db = $this->db;
 		}
-		
-		
+
+
 		$res = $db->select(
 	            array( 'page_wikia_props' ),
-	            array(	'page_id', 
+	            array(	'page_id',
 	            		'props'
 	            	 ),
-	            array(		
+	            array(
 					'page_id in('.implode(",", $articles).')',
 					"propname = 'imageOrder' or propname =  0"),
 	            __METHOD__
 		);
-		
+
 		$image_list = array();
 		$images_name = array();
-		
+
 		/* build list of images to get info about it */
 		while ($row =  $db->fetchRow( $res ) ) {
 			$props = unserialize( $row['props'] );
@@ -86,26 +86,26 @@ class imageServing{
 					if( empty($image_list[$value]) ) {
 						$images_name[] = $db->addQuotes( $value );
 					}
-					$image_list[$value][$row['page_id']] = $key;	
+					$image_list[$value][$row['page_id']] = $key;
 				}
 			}
 		}
-		
+
 		if (count($image_list) == 0) {
 			wfProfileOut(__METHOD__);
 			return $cache_return;
 		}
-		
+
 		$res = $db->select(
 	            array( 'imagelinks LEFT JOIN image on il_to = img_name ' ),
-	            array(	'count(*) cnt', 
+	            array(	'count(*) cnt',
 	            		'il_to',
 	            		'img_width',
 	            		'img_height',
 	            		'img_minor_mime'
 	            	 ),
 				array(),
-	           	__METHOD__,	 
+	           	__METHOD__,
 	            array(
 	            	"GROUP BY" => "il_to",
 	            	"HAVING" => implode(' and ',array(
@@ -116,19 +116,19 @@ class imageServing{
 	            		))
 	            )
 		);
-		
+
 		$db_out = array();
 		while ($row =  $db->fetchRow( $res ) ) {
 			if(!in_array($row['img_minor_mime'], array( "svg+xml","svg"))) {
-				$db_out[$row['il_to']] = $row;	
+				$db_out[$row['il_to']] = $row;
 			}
 		}
-		
+
 		if (count($db_out) == 0) {
 			wfProfileOut(__METHOD__);
 			return $cache_return;
 		}
-		
+
 		$out = array();
 
 		foreach( $image_list as $key => $value  ) {
@@ -142,70 +142,70 @@ class imageServing{
 				}
 			}
 		}
-		
+
 		foreach ($out as $key => $value) {
 			$mcKey = wfMemcKey("imageserving", $this->width, $n, $this->proportion["w"], $this->proportion["h"], $key);
-			$wgMemc->set($mcKey, $value, 60*60);	
+			$wgMemc->set($mcKey, $value, 60*60);
 		}
-		
-		wfProfileOut(__METHOD__);		
+
+		wfProfileOut(__METHOD__);
 		return $out + $cache_return;
 	}
-	
+
 	/**
 	 * getUrl - generate url for cut images
 	 *
 	 * @access public
-	 * 
-	 * @param $name \string dbkey of image  
-	 * @param $width \int 
-	 * @param $height \int  
-	 *  
+	 *
+	 * @param $name \string dbkey of image
+	 * @param $width \int
+	 * @param $height \int
+	 *
 	 * @return  \string url for image
 	 */
-	
+
 	private function getUrl($name, $width = 0, $height = 0) {
 		$file_title = Title::newFromText($name ,NS_FILE );
 		$img = wfFindFile( $file_title  );
 		if($img == null) {
 			return "";
-		} 
+		}
 		return wfReplaceImageServer($img->getThumbUrl( $this->getCut($width, $height)."-".$img->getName()));
 	}
-	
+
 	/**
 	 * getUrl - generate cut frame for  Thumb
-	 * 
-	 * @param $width \int 
+	 *
+	 * @param $width \int
 	 * @param $height \int
-	 * 
-	 * 
-	 * @return \string prefix for thumb image 
+	 *
+	 *
+	 * @return \string prefix for thumb image
 	 */
-	
+
 	public function getCut($width, $height) {
 		$pHeight = round(($width)*($this->proportion['h']/$this->proportion['w']));
-		
+
 		if($pHeight >= $height) {
 			$pWidth =  round($height*($this->proportion['w']/$this->proportion['h']));
-			$top = 0; 
+			$top = 0;
 			$left = round($width/2 - $pWidth/2) + 1;
 			$right = $left + $pWidth + 1;
 			$bottom = $height;
 		} else {
 			$deltaYpx = round($height*$this->deltaY);
-			
+
 			$bottom = $pHeight + $deltaYpx;
 			$top = $deltaYpx;
-			
+
 			if( $bottom > $height ) {
 				$bottom = $pHeight;
 				$top = 0;
-			}	
-			
-			$left = 0; 
+			}
+
+			$left = 0;
 			$right = $width;
-			
+
 		}
 		return "{$this->width}px-$left,$right,$top,$bottom";
 	}
