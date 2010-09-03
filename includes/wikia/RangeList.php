69c97f508414777747ba97bc4363c30f653ccd91
sean <sean@wikia-inc.com>
2010-09-03 16:55:33 +0000

    Commit old fixes to RangList class. After these fixes it passed fairly extensive unit-testing.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26163 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/RangeList.php
+++ includes/wikia/RangeList.php
@@ -11,12 +11,20 @@
  * NOTE: For now this is class is only designed for positive-integers.  It would not work with decimals,
  * and while it might work as-is for negative-integers, it was not designed with them in mind, nor was that
  * functionality tested so it is very unlikely it would work perfectly for negative integers.
+ *
+ * TODO: Add a call to this function to our UnitTesting system somewhere.
+ * RangeList::runUnitTests();
  */
- 
+
 class RangeList{
 	// An array of ranges where the lower-bound of the range is the key and the upper-bound of the range is the value.
 	// To be clear, when a single entry is put in, it would be both the lower-bound and the upper-bound.
 	var $ranges = array();
+	
+	// Convenience wrapper for addRange on a single number.
+	public function addPoint($number){
+		$this->addRange($number, $number);
+	}
 
 	/**
 	 * Adds a new value or range to the RangeList.
@@ -32,8 +40,8 @@ class RangeList{
 	 *
 	 */
 	public function addRange($min, $max, $debug=false){
+		ksort($this->ranges);
 		$lowerBounds = array_keys($this->ranges);
-		asort($lowerBounds);
 
 		// Try to find the first lower-bound which is greater than or equal to 'min'
 		$numRanges = count($lowerBounds);
@@ -62,13 +70,17 @@ class RangeList{
 					if($lower != $min){
 						unset($this->ranges[$lower]);
 					}
+
+					if($debug){print "\tOverwrote $lower-$upper with $min-$max\n";}
 				} else if($max < $lower){
 					// If the max of the new range is lower than the lower of the old range, then this range must
 					// fall completely between two existing ranges, so just add the new range.
 					$this->ranges[$min] = $max;
+					if($debug){print "\tAdded new range $min-$max\n";}
 				} else if($lower != $min){
 					$this->ranges[$min] = $upper;
 					unset($this->ranges[$lower]);
+					if($debug){print "\tSet new lower bound so that $lower-$upper was replaced with $min-$max\n";}
 				}
 
 				// Handle when the previous range had an upper greater than this min.
@@ -78,7 +90,7 @@ class RangeList{
 				}
 			}
 
-			if($debug){print "\tPreparing to examine next range.\n";}
+			//if($debug){print "\tPreparing to examine next range.\n";}
 			$prevLower = $lower;
 			$prevUpper = $upper;
 			$index++;
@@ -105,22 +117,23 @@ class RangeList{
 		if($debug){print "\tDone adding ranges... assuring that the keys are still ordered.\n";}
 		ksort($this->ranges); // we may have added another range, assure it is in the correct order now.
 		if($debug){print "\tAbout to merge any overlapping/adjacent ranges.\n";}
-		if($debug){var_dump($this->ranges);}
+		if($debug){print "\t".$this->toString()."\n";}
 
 		// Clean up overlapping and adjacent ranges.
-		$this->mergeRanges();
+		$this->mergeRanges($debug);
 		if($debug){print "\tDone merging ranges.\n";}
+		if($debug){print "\t".$this->toString()."\n";}
 	} // end addRange()
 
 	/**
 	 * Goes through the ranges and reduces them to be the fewest number of entries
 	 * possible by merging overlapping and adjacent ranges.
 	 */
-	public function mergeRanges(){
+	public function mergeRanges($debug=false){
 		// Need to recreate the lowerBounds array now that there have been some new assignments.
+		ksort($this->ranges); // there were changes, need to re-order
 		$lowerBounds = array_keys($this->ranges);
 		$numRanges = count($lowerBounds);
-		asort($lowerBounds); // keys will not always be in order
 
 		$somethingWasChanged = false;
 		for($cnt=0; $cnt < $numRanges; $cnt++){
@@ -132,7 +145,9 @@ class RangeList{
 				$currUpper = $this->ranges[$currLower];
 
 				// If this range overlaps the previous, combine them.
+				if($debug){print "Testing if $lastLower-$lastUpper should merge with $currLower - $currUpper\n";}
 				if(self::rangesShouldBeMerged($lastLower, $lastUpper, $currLower, $currUpper)){
+					if($debug){print "MERGING\n";}
 					$min = min($lastLower, $currLower);
 					$max = max($lastUpper, $currUpper);
 
@@ -154,7 +169,14 @@ class RangeList{
 		
 		// Keep calling until everything is merged.
 		if($somethingWasChanged){
+			if($debug){
+				print "Something was changed.  About to merge-again to make sure it all worked.\n";
+				print "State after this merge and before the next: \n";
+				print $this->toString()."\n";
+			}
 			$this->mergeRanges();
+		} else if($debug){
+			print "Nothing was merged.\n";
 		}
 	} // end mergeRanges()
 	
@@ -206,7 +228,7 @@ class RangeList{
 	 * Returns the number of distinct ranges in the list.
 	 */
 	public function numRanges(){
-		return count($this->ranges());
+		return count($this->ranges);
 	}
 	
 	/**
@@ -217,9 +239,28 @@ class RangeList{
 		// make sure to sort before returning the ranges.
 		ksort($this->ranges);
 
-		return $this->ranges();
-	}
+		return $this->ranges;
+	} // end getRanges()
 	
+	public function setRanges($rangeListArray){
+		$this->ranges = $rangeListArray;
+	} // end setRanges()
+
+	/**
+	 * Returns all of the integers that are in any of the ranges (the ranges are inclusive).
+	 * This may be an extremely large result, so this function is recommended mainly for testing.
+	 */
+	public function getAllValues(){
+		$retVal = array();
+		ksort($this->ranges);
+		foreach($this->ranges as $min=>$max){
+			for($cnt=$min; $cnt<=$max; $cnt++){
+				$retVal[] = $cnt;
+			}
+		}
+		return $retVal;
+	} // end getAllValues()
+
 	/**
 	 * Returns the sum of all of the numbers contained in the ranges.
 	 */
@@ -255,7 +296,27 @@ class RangeList{
 		}
 		return $numFails;
 	}
-	
+
+	/**
+	 * Outputs a human-readable version of the RangeList.
+	 */
+	public function toString(){
+		ksort($this->ranges);
+
+		$retVal = "";
+		foreach($this->ranges as $min => $max){
+			$retVal .= ($retVal==""?"":", ");
+			if($min == $max){
+				$retVal .= $min;
+			} else {
+				$retVal .= "$min-$max";
+			}
+		}
+		$retVal = "($retVal)";
+
+		return $retVal;
+	}
+
 	// From 'bishop' on PHP man page comments for comparators.
 	public static function array_identical($a, $b) {
 		return (is_array($a) && is_array($b) && array_diff_assoc($a, $b) === array_diff_assoc($b, $a));
@@ -310,11 +371,76 @@ class RangeList{
 		} else {
 			print "$numFails TESTS FAILED! SEE DETAILS ABOVE!\n";
 		}
-	} // end test_addRange();
+	} // end test_addRange()
+	
+	/**
+	 * This function is to help find problems in a dataset which has known-errors when being added to a RangeList.
+	 */
+	public static function test_exampleData(){
+		$rangeList = new RangeList();
+		$exampleData = array(
+			7236, 242748, 298844, 460970, 474758, 480648, 501229, 512169, 518695,
+			525959, 526874, 764498, 765675, 779359, 779361, 939362, 941241, 958663,
+			1810544, 435681, 436434, 752887, 781363, 1822767, 1823062, 1823064, 1823069,
+			1823074, 1823076, 1823081, 1823664, 1826151, 1826155, 1826162, 1826163,
+			469566, 767737, 767741, 768008, 768047, 768060, 768064, 768069, 1519524,
+			1814601, 1814608, 1814609, 1814611, 1814670, 1814671, 1814675, 1814680,
+			1814684, 1814685, 1814686, 1814687, 1814688, 1814696, 1814601, 1814608,
+			1814609, 1814611, 1814670, 1814671, 1814675, 1814680, 1814684, 1814685,
+			1814686, 1814687, 1814688, 1814696, 1148148, 1148526, 1148533, 1149317,
+			1149323, 1328278, 1382047, 1382048, 1434440, 1778992, 1779032, 1782040,
+			1782302, 1784875, 1784878, 1778992, 1779032, 1782040, 1782302, 1784875,
+			1784878, 1778992, 1779032, 1782040, 1782302, 1784875, 1784878, 1494721,
+			1494722, 1778992, 1779032, 1782040, 1782302, 1784875, 1784878, 1494721,
+			1494722, 1778992, 1779032, 1782040, 1782302, 1784875, 1784878, 1490732,
+			1166101, 1440234, 1166101, 1440234, 1166101, 1440234, 1166101, 1440234,
+			1166101, 1440234, 1166101, 1440234, 1166101, 1440234, 1166101, 1166101,
+			1166101, 1166101, 1166101, 1166101, 525936, 1335358, 1522670, 1821834,
+			880785, 1779032, 1778992, 1784875, 236640, 746473, 746475, 746478, 746481,
+			746483, 746484, 746947, 761452, 769691, 773565, 781677, 781679, 789891,
+			800303, 810903, 828358, 839866, 846610, 860057, 880928, 902117, 918220,
+			934960, 952786, 973754, 995975, 1124800, 1147200, 1160679, 1162432, 1195372,
+			1208913, 1436094, 1442521, 1517640, 1625563, 1800838, 1812148, 1826497,
+			1444577, 1444631, 1128540, 1826364, 1826364, 242610, 1435182, 1830579,
+			1493326, 788529, 788680, 1829623, 1826371, 766494, 1799531
+		);
+
+		$valuesAdded = array();
+
+		$failed = false;
+		foreach($exampleData as $currentId){
+			$valuesAdded[] = $currentId;
+			$rangeList->addPoint($currentId);
+			asort($valuesAdded);
+
+			//if($valuesAdded != $rangeList->getAllValues()){
+			if(count(array_diff($rangeList->getAllValues(), $valuesAdded)) > 0){
+				$failed = true;
+				print "ERROR OCCURRED WHEN ADDING $currentId\n";
+				print "RAW IDS ADDED:\n(".implode(", ", $valuesAdded).")\n";
+				//print "ALL VALUES:\n(".implode(", ", $rangeList->getAllValues()).")\n";
+				
+				$prevRangeList = new RangeList();
+				$prevRangeList->setRanges($prevRanges);
+				print "PREV-RANGELIST:\n".$prevRangeList->toString()."\n";
+				
+				print "RANGELIST:\n".$rangeList->toString()."\n";
+				
+				print "Repeating last operation but in debug mode...\n";
+				$prevRangeList->addRange($currentId, $currentId, false);
+				print "RE-CALCULATED RANGELIST:\n".$prevRangeList->toString()."\n";
+				break;
+			}
+			$prevRanges = $rangeList->getRanges();
+		}
+
+		if(!$failed){
+			print "TEST PASSED.\n";
+		}
+	} // end test_exampleData()
 
 } // end class RangeList
 
 
-
-// TODO: REMOVE
-RangeList::runUnitTests();
+// TODO: REMOVE AFTER FIXING.
+//RangeList::test_exampleData();
