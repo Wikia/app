aa8ef26fa2154e65c23d5843320f9d0df2d6e1fc
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-03 08:29:31 +0000

    * some HTML5 placeholder love for Oasis and forms mixin (thx Macbre)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26132 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/jquery/jquery.wikia.js
+++ skins/common/jquery/jquery.wikia.js
@@ -418,3 +418,57 @@ $(window).bind('load', function() {
 		$().log('window onload after ' + loadTime + ' ms', window.skin);
 	}
 });
+
+
+
+/**
+ * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
+ *
+ * HTML5 placeholder attribute fallback for HTML5-disabled browsers.
+ * A placeholder CSS class should be defined (use the forms mixin in Oasis)
+ */
+
+$(function() {
+	$('input[placeholder]').placeholder();
+});
+
+jQuery.fn.placeholder = function() {
+	 //feature detection
+	var hasNativeSupport = 'placeholder' in document.createElement('input');
+
+	if(!hasNativeSupport){
+		this.each(function() {
+			var input = $(this);
+			var text = input.attr('placeholder');
+
+			if(input.val() == ''){
+				input
+					.addClass('placeholder')
+					.val(text);
+			}
+
+			input.focus(function(){
+				if(input.val() == text){
+					input.val('');
+				}
+
+				input.removeClass('placeholder');
+			});
+
+			input.blur(function(){
+				if(input.val() == ''){
+					input
+						.addClass('placeholder')
+						.val(text);
+				}
+			});
+
+			//clear the field is a submit event is fired somewhere around here
+			input.closest('form').submit(function(){
+				if(input.val() == text){
+					input.val('');
+				}
+			});
+		});
+	}
+}
