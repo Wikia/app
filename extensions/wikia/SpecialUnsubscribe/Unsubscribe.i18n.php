37299abc52d05ef7469bbc2b6af68615d1ffc964
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-05 17:58:03 +0000

    * add description message which was defined but missing * update L10n and i18n * add translator hints/message documentation * svn eol-style:native * run stylize.php
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26208 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialUnsubscribe/Unsubscribe.i18n.php
+++ extensions/wikia/SpecialUnsubscribe/Unsubscribe.i18n.php
@@ -9,26 +9,41 @@
 $messages = array();
 
 /** English
- * @author 
+ * @author
  */
 $messages['en'] = array(
+	'unsubscribe-desc' => 'Provides a [[Special:Unsubscribe|single e-mail unsubscribe]] point',
 	'unsubscribe' => 'Unsubscribe',
 
-	'unsubscribe-badaccess' => 'Sorry, this page cannot be used in this manor. Please follow the link from your email.',
-	'unsubscribe-badtoken' => 'Sorry, there was a problem with the security token.',
-	'unsubscribe-bademail' => 'Sorry, there was a problem with the email.',
+	'unsubscribe-badaccess' => 'Sorry, this page cannot be used in this manor.
+Please follow the link from your e-mail.',
+	'unsubscribe-badtoken' => 'Sorry, there was a problem with the security token.
+Please try again.',
+	'unsubscribe-bademail' => 'Sorry, there was a problem with the e-mail address.
+Please try again.',
 
-	#user info list
-	'unsubscribe-already' => 'Already unsubscribed', 
+	# user info list
+	'unsubscribe-already' => 'Already unsubscribed',
 
-	#confirm form
+	# confirm form
 	'unsubscribe-confirm-legend' => 'Confirm unsubscribe',
-	'unsubscribe-confirm-text' => 'Unsubscribe all accounts tied to <tt>$1</tt>?',
+	'unsubscribe-confirm-text' => 'Unsubscribe all accounts using the e-mail address <code>$1</code>?',
 	'unsubscribe-confirm-button' => 'Yes, I\'m sure',
-	
-	#working page
-	'unsubscribe-working' => 'Unsubscribing {{PLURAL:$1|account|accounts}} for $2',
-	'unsubscribe-working-problem' => 'problem loading user info for: $1',
-	'unsubscribe-working-done' => 'Complete',
-	
+
+	# working page
+	'unsubscribe-working' => 'Unsubscribing {{PLURAL:$1|account|$1 accounts}} for $2',
+	'unsubscribe-working-problem' => '* Problem loading user info for: $1',
+	'unsubscribe-working-done' => 'The e-mail address has been unsubscribed.',
+);
+
+$messages['qqq'] = array(
+	'unsubscribe-desc' => '{{desc}}',
+	'unsubscribe' => 'Special page description on [[Special:SpecialPages]] and special page title.',
+	'unsubscribe-confirm-text' => 'Parameters:
+* $1 is an e-mail address.',
+	'unsubscribe-working' => 'Parameters:
+* $1 is the number of users for which the given e-mail address is used.
+* $1 is an e-mail address.',
+	'unsubscribe-working-problem' => 'This is an error message. The "*" character is wiki markup for a list. Parameters:
+* $1 is a username.',
 );
