ac865da3b270788f5d47ea92ec9e07bf732935d4
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-06 14:00:45 +0000

    rt#56125: remove MagCloud code from svn
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26222 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MagCloud/MagCloudCallback.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$dir = dirname(__FILE__);
-
-$wgAutoloadClasses["MagCloudCallback"] = "{$dir}/SpecialMagCloudCallback.class.php";
-$wgSpecialPages["MagCloudCallback"]    = "MagCloudCallback";
--- maintenance/wikia/WidgetFrameworkGenerator.php
+++ maintenance/wikia/WidgetFrameworkGenerator.php
@@ -37,7 +37,6 @@ $wgWidgetsOrderedList = array(
 	'WidgetWikiPage',
 	'WidgetNewPages',
 	'WidgetAnswers',
-	'WidgetMagCloud',
 );
 
 global $wgWidgets, $wgMessageCache;
