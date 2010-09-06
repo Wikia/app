8fb76320405bb4956fd8ebdf4bff7d1f8438bdbb
tor <tor@wikia-inc.com>
2010-09-06 14:58:37 +0000

    removed Recipes Box from selected wikis (rt#65841)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26225 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RecipesTweaks/RecipesTweaks_setup.php
+++ extensions/wikia/RecipesTweaks/RecipesTweaks_setup.php
@@ -23,7 +23,10 @@ $wgHooks['SkinGetPageClasses'][] = 'RecipesTweaks::addBodyClass';
 $wgHooks['CustomUserData'][] = 'RecipesTweaks::replaceWatchlistUserLink';
 $wgHooks['MonacoBeforeWikiaPage'][] = 'RecipesTweaks::renderArticleHeaderTabs';
 $wgHooks['MonacoBeforePageBar'][] = 'RecipesTweaks::renderArticleHeaderStripe';
-$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'RecipesTweaks::renderUserSavedPagesBox';
+if ( !empty( $wgEnableRecipiesTweaksSavedPages ) ) {
+	// requested to be disabled on certain wikis, defaults to true (enabled) in CommonSettings
+	$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'RecipesTweaks::renderUserSavedPagesBox';
+}
 
 // i18n
 $wgExtensionMessagesFiles['RecipesTweaks'] = $dir . 'RecipesTweaks.i18n.php';
