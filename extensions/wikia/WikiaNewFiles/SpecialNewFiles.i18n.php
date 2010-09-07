dd370aaacc605b8420687bcfb22c3404519a9137
tor <tor@wikia-inc.com>
2010-09-07 12:06:52 +0000

    rt#65564: New Special:NewFiles - improve formatting with long page titles
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26250 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaNewFiles/SpecialNewFiles.i18n.php
+++ extensions/wikia/WikiaNewFiles/SpecialNewFiles.i18n.php
@@ -11,6 +11,6 @@ $messages['en'] = array(
 	'wikianewfiles-desc'    => 'Extends a [[Special:NewFiles|special page]] to override some of the header formatting',
 	'wikianewfiles-title'   => 'New photos on this wiki',
 	'wikianewfiles-uploadby' => 'by $1',
-	'wikianewfiles-postedin' => 'Posted in',
+	'wikianewfiles-postedin' => 'Posted&nbsp;in',
 	'wikianewfiles-more'     => 'more...',
 );
