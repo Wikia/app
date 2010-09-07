64e32e837ddbfe2cb6455eefc1123758e8b2008d
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-07 10:00:03 +0000

    RelatedPages: 1) more scalable article matching algorith implemented 2) caching
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26244 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedPages/RelatedPages.class.php
+++ extensions/wikia/RelatedPages/RelatedPages.class.php
@@ -4,6 +4,9 @@ class RelatedPages {
 
 	private $pages = null;
 	private $categories = null;
+	private $categoryCacheTTL = 3; // in hours
+	private $categoryRankCacheTTL = 24; // in hours
+	private $categoriesLimit = 6;
 	static private $instance = null;
 
 	private function __construct( ) {
@@ -43,169 +46,223 @@ class RelatedPages {
 	 */
 	public function get( $articleId, $limit = 3 ) {
 		global $wgContentNamespaces;
+		wfProfileIn( __METHOD__ );
 
 		// prevent from calling this function more than one, use reset() to omit
 		if( is_array( $this->pages ) ) {
+			wfProfileOut( __METHOD__ );
 			return $this->pages;
 		}
 
 		$this->pages = array();
-
 		$categories = $this->getCategories();
-		if ( $categories ) {
-			sort( $categories );
 
-			// caching tmp off
-			//$cacheKey = wfMemcKey(__CLASS__, join(':', $categories), 1);
-			//$out = $wgMemc->get($cacheKey);
-			$out = null;
-
-			if(!is_array($out)) {
-
-				$dbr = wfGetDB(DB_SLAVE, 'stats');
-
-				$tables = array( "categorylinks" );
-
-				if(count($wgContentNamespaces) > 0) {
-					$joinSql = array( "page" =>
-						array(
-							"JOIN",
-							implode(
-								" AND ",
-								array(
-									"page_id = cl_from",
-									( count($wgContentNamespaces) == 1)
-										? "page_namespace = " . intval(array_shift($wgContentNamespaces))
-										: "page_namespace in ( " . $dbr->makeList( $wgContentNamespaces ) . " )",
-								)
-							)
-						)
-					);
-					$tables[] = "page";
-				} else {
-					$joinSql = array();
-				}
-
-				$options = array("GROUP BY" => "cl_from");
-				if (count($categories) > 1) {
-					$options["HAVING"] = "COUNT(cl_to) > 1";
-				} else {
-					$options["LIMIT"] = 100;
-				}
-
-				$res1 = $dbr->select(
-					$tables,
-					array( "cl_from", "COUNT(cl_to) AS count" ),
-					array(
-						"cl_to in ( " . $dbr->makeList( $categories ) . " )",
-					),
-					"RelatedPages Query 1",
-					$options,
-					$joinSql
-				);
-				$out = array();
-				$results = array();
-				while($row1 = $dbr->fetchObject($res1)) {
-					$results[$row1->cl_from] = $row1->count;
-				}
-
-				if ( count($categories) > 1 ) {
-					arsort($results);
-					//uasort($results, 'RelatedPages_Compare');
-					$out = array_slice(array_keys($results), 0, 5);
-				} else {
-					if ( count($results) > 0 ) {
-						$out = array_rand($results, min(count($results), 5));
-						if ( !is_array($out) ) {
-							$out = array($out);
-						}
-					}
-				}
-
-				if ( count($categories) > 1 && count($out) < 5 ) {
-					$results = array();
-
-					$res2 = $dbr->select(
-						$tables,
-						array( "cl_from" ),
-						array(
-							"cl_to in ( " . $dbr->makeList( $categories ) . " )",
-						),
-						"RelatedPages Query 2",
-						array(
-							"GROUP BY" => "cl_from",
-							"LIMIT" => 100
-						),
-						$joinSql
-					);
-
-					while($row2 = $dbr->fetchObject($res2)) {
-						if(!in_array($row2->cl_from, $out)) {
-							$results[] = $row2->cl_from;
-						}
-					}
-
-					if ( !empty($results) ) {
-						$randOut = array_rand(array_flip($results), min(count($results), 5 - count($out)));
-						if ( !is_array($randOut) ) { // array_rand will return a single element instead of an array of size 1
-							$randOut = array($randOut);
-						}
-						$out = array_merge($out, $randOut);
-					}
-				}
-
-				//$wgMemc->set($cacheKey, $out, 60 * 60 * 6);
+		if ( count($categories) > 0 ) {
+			$categories = $this->getCategoriesByRank( $categories );
+			if( count( $categories ) > $this->categoriesLimit ) {
+				// limit the number of categories to look for
+				$categories = array_slice( $categories, 0, $this->categoriesLimit );
 			}
 
-			if ( count($out) > 0 ) {
-				unset($out[array_search($articleId, $out)]);
+			$pagesPerCategory = array();
+			$allPages = array();
+			$counters = array();
 
-				// quit if we have no data after removing current page
-				if ( empty( $out ) ) {
-					return array();
-				}
+			foreach( $categories as $category ) {
+				$pages = $this->getPagesForCategory( $category );
+				$pagesPerCategory[$category] = $pages;
+				$allPages = array_merge( $allPages, $pages );
+			}
+			$allPages = array_unique( $allPages );
+			unset( $allPages[array_search($articleId, $allPages)] );
+			sort( $allPages );
 
-				$count = 0;
-				foreach ( $out as $item ) {
-					$title = Title::newFromId($item);
-					if(!empty($title) && $title->exists() && $count < $limit) {
-						$this->pages[ $title->getArticleID() ] = array( 'url' => $title->getFullUrl(), 'title' => $title->getPrefixedText() );
-					}
-					$count++;
-				}
-
-				if( class_exists('imageServing') ) {
-					// ImageServing extension enabled, get images
-					$imageServing = new imageServing( array_keys( $this->pages ), 200, array( 'w' => 2, 'h' => 1 ) );
-					$images = $imageServing->getImages(1); // get just one image per article
-
-					// TMP: always remove last article to get a text snippeting working example
-					$images = array_slice($images, 0, $limit-1, true);
-
-					foreach( $this->pages as $pageId => $data ) {
-						if( isset( $images[$pageId] ) ) {
-							$image = $images[$pageId][0];
-							$data['imgUrl'] = $image['url'];
+			$pageCounters = array();
+			for( $i = 0; $i < count( $allPages); $i++ ) {
+				foreach( $pagesPerCategory as $categoryPages ) {
+					if( in_array($allPages[$i], $categoryPages) ) {
+						if( isset($pageCounters[$allPages[$i]]) ) {
+							$pageCounters[$allPages[$i]]++;
 						}
 						else {
-							// no images, get a text snippet
-							$data['text'] = $this->getArticleSnippet( $pageId );
+							$pageCounters[$allPages[$i]] = 1;
 						}
-						$this->pages[ $pageId ] = $data;
 					}
 				}
-				else {
-					// ImageServing not enabled, just get text snippets for all articles
-					foreach( $this->pages as $pageId => $data ) {
-						$data['text'] = $this->getArticleSnippet( $pageId );
-						$this->pages[ $pageId ] = $data;
-					}
-				}
-
 			}
-		} // if categories
+			arsort($pageCounters);
 
+			$count = 0;
+			foreach ( array_keys( $pageCounters ) as $pageId ) {
+				$title = Title::newFromId( $pageId );
+				if(!empty($title) && $title->exists() && $count < $limit) {
+					$this->pages[ $title->getArticleID() ] = array( 'url' => $title->getFullUrl(), 'title' => $title->getPrefixedText() );
+				}
+				$count++;
+			}
+
+			if( class_exists('imageServing') ) {
+				// ImageServing extension enabled, get images
+				$imageServing = new imageServing( array_keys( $this->pages ), 200, array( 'w' => 2, 'h' => 1 ) );
+				$images = $imageServing->getImages(1); // get just one image per article
+
+				// TMP: always remove last article to get a text snippeting working example
+				$images = array_slice($images, 0, $limit-1, true);
+
+				foreach( $this->pages as $pageId => $data ) {
+					if( isset( $images[$pageId] ) ) {
+						$image = $images[$pageId][0];
+						$data['imgUrl'] = $image['url'];
+					}
+					else {
+						// no images, get a text snippet
+						$data['text'] = $this->getArticleSnippet( $pageId );
+					}
+					$this->pages[ $pageId ] = $data;
+				}
+			}
+			else {
+				// ImageServing not enabled, just get text snippets for all articles
+				foreach( $this->pages as $pageId => $data ) {
+					$data['text'] = $this->getArticleSnippet( $pageId );
+					$this->pages[ $pageId ] = $data;
+				}
+			}
+		} // if $categories
+
+		wfProfileOut( __METHOD__ );
 		return $this->pages;
+	}
+
+	/**
+	 * get all pages for given category
+	 * @param string $category category name
+	 * @return array list of page ids
+	 */
+	private function getPagesForCategory( $category ) {
+		global $wgMemc;
+		wfProfileIn( __METHOD__ );
+
+		$cacheKey = wfMemcKey( __METHOD__, $category );
+		$cache = $wgMemc->get( $cacheKey );
+		if( is_array($cache) ) {
+			wfProfileOut( __METHOD__ );
+			return $cache;
+		}
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$tables = array( "categorylinks" );
+		$joinSql = $this->getPageJoinSql( $dbr, $tables );
+
+		$pages = array();
+		$res = $dbr->select(
+			$tables,
+			array( "cl_from AS page_id" ),
+			array( "cl_to" => $category ),
+			__METHOD__,
+			array(),
+			$joinSql
+		);
+
+		while( $row = $dbr->fetchObject($res) ) {
+			$pages[] = $row->page_id;
+		}
+
+		$wgMemc->set( $cacheKey, $pages, ( $this->categoryCacheTTL * 3600 ) );
+
+		wfProfileOut( __METHOD__ );
+		return $pages;
+	}
+
+	private function getPageJoinSql( $dbr, &$tables ) {
+		global $wgContentNamespaces;
+		wfProfileIn( __METHOD__ );
+
+		if(count($wgContentNamespaces) > 0) {
+			$joinSql = array( "page" =>
+				array(
+					"JOIN",
+					implode(
+						" AND ",
+						array(
+							"page_id = cl_from",
+							( count($wgContentNamespaces) == 1)
+								? "page_namespace = " . intval(reset($wgContentNamespaces))
+								: "page_namespace in ( " . $dbr->makeList( $wgContentNamespaces ) . " )",
+						)
+					)
+				)
+			);
+			$tables[] = "page";
+		} else {
+			$joinSql = array();
+		}
+
+		wfProfileOut( __METHOD__ );
+		return $joinSql;
+	}
+
+	/**
+	 * get categories sorted by rank
+	 * @param array $categories
+	 */
+	private function getCategoriesByRank( Array $categories ) {
+		$categoryRank = $this->getCategoryRank();
+
+		$results = array();
+		foreach( $categories as $category ) {
+			if( isset($categoryRank[$category]) ) {
+				$results[$categoryRank[$category]] = $category;
+			}
+		}
+
+		ksort( $results );
+		return count($results) ? array_values( $results ) : $categories;
+	}
+
+	/**
+	 * get category rank, based on number of articles assigned
+	 * @return array
+	 */
+	private function getCategoryRank() {
+		global $wgMemc;
+		wfProfileIn( __METHOD__ );
+
+		$cacheKey = wfMemcKey( __METHOD__ );
+		$cache = $wgMemc->get( $cacheKey );
+		if( is_array($cache) ) {
+			wfProfileOut( __METHOD__ );
+			return $cache;
+		}
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$tables = array( "categorylinks" );
+		$joinSql = $this->getPageJoinSql( $dbr, $tables );
+
+		$res = $dbr->select(
+			$tables,
+			array( "cl_to", "COUNT(cl_to) AS count" ),
+			array(),
+			__METHOD__,
+			array(
+				"GROUP BY" => "cl_to",
+				"HAVING" => "count > 1",
+				"ORDER BY" => "count DESC"
+			),
+			$joinSql
+		);
+		$results = array();
+
+		$rank = 1;
+		while($row = $dbr->fetchObject($res)) {
+			$results[$row->cl_to] = $rank;
+			$rank++;
+		}
+
+		$wgMemc->set( $cacheKey, $results, ( $this->categoryRankCacheTTL * 3600 ) );
+
+		wfProfileOut( __METHOD__ );
+		return $results;
 	}
 
 	/**
