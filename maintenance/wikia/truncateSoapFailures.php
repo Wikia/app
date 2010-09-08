e819bdd58e2ec14a16745b189800e05d67122e24
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-08 23:25:12 +0000

    Added maintenance script to clear the lw_soap_failures table.  It should be run approx once every 10 days via cron.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26353 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/truncateSoapFailures.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Purge the SOAPfailures from Special:Soapfailures on LyricWiki.
+ *
+ * This script is meant to be run approximately every 10 days.
+ *
+ * @author Sean Colombo
+ */
+
+require_once('../commandLine.inc');
+
+$TABLE_NAME = "lw_soap_failures";
+if (isset($options['help'])) {
+	die( "This script will completely truncate the $TABLE_NAME table for LyricWiki.  It should be run every 10 days via cron to keep the table small enough that it can be used quickly.  It can be run any time needed (but it helps us to have a few days of data in there)." );
+}
+
+$dbw = wfGetDB(DB_MASTER, array(), 'lyricwiki');
+$res = $dbw->query("TRUNCATE $TABLE_NAME");
+if(!$res){
+	print "ERROR WHILE TRYING TO TRUNCATE $TALBE_NAME!!!\n";
+}
