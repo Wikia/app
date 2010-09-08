8ff94d3c794834cacfcf1afbddba595eac75e85b
hyun <hyun@wikia-inc.com>
2010-09-08 23:49:41 +0000

    TD WIP
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26355 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/js/ThemeDesignerPreview.js
+++ extensions/wikia/ThemeDesigner/js/ThemeDesignerPreview.js
@@ -1,1 +1,13 @@
-var a1v1 = 0;
+$(function() {
+	$("#WikiaArticle .thumbinner")
+		.first()
+		.attr("style", "width:302px")
+		.find("a")
+		.first()
+		.html(
+			'<img width="300" src="/extensions/wikia/ThemeDesigner/images/aquarium.jpg">'
+		);	
+		
+	$("#WikiaArticle .thumbinner").append('<div class="picture-attribution"><img width="16" height="16" class="avatar" src="/extensions/wikia/ThemeDesigner/images/td-avatar.jpg">Added by <a>FunnyBunny</a></div>');
+	
+});
--- extensions/wikia/ThemeDesigner/templates/ThemeDesigner_Preview.php
+++ extensions/wikia/ThemeDesigner/templates/ThemeDesigner_Preview.php
@@ -1,16 +1,33 @@
-<?= ThemeDesignerHelper::parseText("[[File:FrankOz-blackwhite.jpg|thumb|300px]]");?>
+<?= ThemeDesignerHelper::parseText("[[File:Aquarium.jpg|thumb|Quisque pellentesque vestibulum ullamcorper.]]
+Lorem ipsum dolor sit amet, [[consectetur]] adipiscing elit. Nunc molestie velit a ante [[fringilla vitae]] euismod velit gravida. [[Curabitur]] eu sem et justo sodales [[consequat quis]] vel justo. Vestibulum ante ipsum primis in faucibus orci [[luctus et ultrices]] posuere cubilia Curae. Quisque vitae lectus odio, quis porta orci. Etiam mattis volutpat enim, id posuere eros faucibus id. [[Integer vehicula libero]] vitae est ornare a pellentesque mauris pharetra. Nam eget lorem vestibulum nisi [[commodo suscipit]] non sit amet justo. Fusce imperdiet lacus sed [[turpis ornare]] pretium. ''Maecenas'' [[ante neque]], imperdiet sed accumsan id, gravida a sem. Morbi eu nisl quis nunc tincidunt condimentum euismod sit amet est.
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus velit non sapien vestibulum eu laoreet elit semper. Aliquam dictum volutpat lobortis. In vel ante metus, ac pretium eros. Nulla lectus quam, varius eget venenatis in, ornare ut lacus. Curabitur sed hendrerit eros. Donec faucibus lobortis justo tincidunt tincidunt. Aliquam gravida magna nec quam malesuada sodales. Donec semper fermentum ipsum, eu fermentum magna imperdiet vel. Proin sed eros eu ante suscipit varius. Maecenas hendrerit faucibus adipiscing. Duis id leo ante. Curabitur ultricies euismod leo fermentum fringilla.
-</p>
 
-<p>
-Nulla facilisi. Aenean non facilisis lacus. Fusce nunc ante, imperdiet vel aliquet tincidunt, vulputate in quam. Proin scelerisque commodo leo nec porttitor. Ut lorem risus, accumsan et pulvinar lobortis, molestie sed urna. Ut consequat, elit et semper hendrerit, risus leo porttitor eros, at euismod libero erat ac magna. Fusce eu eros elit, dignissim porttitor velit. Pellentesque consequat arcu ac urna varius aliquam. Fusce ipsum ante, semper sit amet hendrerit eget, hendrerit in nibh. Ut quis metus quis neque condimentum eleifend ut a nunc. Nulla vehicula augue et sem laoreet pulvinar condimentum mauris ultricies. Quisque luctus nunc egestas sem egestas lacinia. Nam felis nisi, suscipit vel interdum ac, volutpat egestas elit. Vivamus fringilla eleifend metus non bibendum. Quisque aliquet mi quis nunc tincidunt tincidunt.
-</p>
+==Donec dapibus==
 
-<p>
-Phasellus convallis dolor at erat congue a bibendum enim adipiscing. Vestibulum et ultricies nulla. Vestibulum in enim quam. Proin ante ipsum, porttitor id feugiat non, ornare a purus. In est velit, iaculis eget hendrerit id, rhoncus ac purus. Nullam arcu erat, egestas eu vestibulum a, mollis nec mauris. Aenean sit amet metus et lectus mattis cursus. Nunc accumsan placerat libero nec scelerisque. Pellentesque sit amet odio nec quam venenatis iaculis nec dapibus velit. Morbi eget nulla sed mauris fermentum placerat. </p>
-</p>
+''[[Sed porttitor ipsum]]'' quis nisi vehicula vel [[auctor magna]] aliquet. Sed eu quam eget arcu [[varius]] pulvinar. Vestibulum aliquam [[blandit turpis]] a porta. Sed feugiat tempor tristique. Duis iaculis arcu non ligula tincidunt dapibus. Vestibulum gravida [[volutpat purus]] sit amet laoreet. In tincidunt sapien sit amet metus ullamcorper sodales sed dignissim lacus. [[Fusce]] interdum tincidunt sodales. Nunc in metus et nisi scelerisque dapibus.
 
-<p>
-<?= ThemeDesignerHelper::parseText("''wiki'' '''text'''"); ?>
-</p>
+* Quisque nec tellus
+
+* Auctor rutrum quam
+
+* Ut pretium tristique varius
+
+Nulla felis tellus, [[tempus]] a eleifend vel, vulputate elementum augue. [[Aliquam erat]] volutpat. Morbi ut arcu nec nunc suscipit venenatis vitae non tortor. [[Pellentesque]] posuere tortor sed mauris aliquam eget [[facilisis tellus]] imperdiet. Integer odio massa, eleifend gravida porta a, hendrerit non orci. [[Donec quis tellus]] vel purus consectetur posuere eu vitae urna. Phasellus sit amet augue ante. Aenean vulputate [[mattis]] sapien a sagittis.
+
+Cras [[hendrerit tellus]] ac enim hendrerit mattis. [[Praesent justo]] nulla, consequat et pharetra a, aliquet eu erat. Suspendisse commodo hendrerit dui vel vulputate. Sed [[sit amet]] orci ligula.
+
+Phasellus [[semper enim eu massa]] ornare convallis. Suspendisse quis elit sapien. Integer lacinia [[ultrices tortor]], nec rhoncus [[metus accumsan]] ut. Aliquam [[nec erat vitae]] diam semper eleifend. Morbi condimentum [[lacinia]] felis at rhoncus. Integer [[imperdiet ultricies]] lobortis. Curabitur [[aliquam lectus]] vel orci suscipit mollis. Integer faucibus [[laoreet]] pharetra. Aliquam at erat [[venenatis lacus]] rhoncus semper. Praesent fringilla ligula et quam ullamcorper id viverra est pretium.
+
+==Praesent consequat==
+
+Lorem ipsum dolor sit amet, [[consectetur]] adipiscing elit. Nunc molestie velit a ante [[fringilla vitae]] euismod velit gravida. [[Curabitur]] eu sem et justo sodales [[consequat quis]] vel justo. Vestibulum ante ipsum primis in faucibus orci [[luctus et ultrices]] posuere cubilia Curae. Quisque vitae lectus odio, quis porta orci. Etiam mattis volutpat enim, id posuere eros faucibus id. [[Integer vehicula libero]] vitae est ornare a pellentesque mauris pharetra. Nam eget lorem vestibulum nisi [[commodo suscipit]] non sit amet justo. Fusce imperdiet lacus sed [[turpis ornare]] pretium. ''Maecenas'' [[ante neque]], imperdiet sed accumsan id, gravida a sem. Morbi eu nisl quis nunc tincidunt condimentum euismod sit amet est.
+
+Nulla felis tellus, [[tempus]] a eleifend vel, vulputate elementum augue. [[Aliquam erat]] volutpat. Morbi ut arcu nec nunc suscipit venenatis vitae non tortor. [[Pellentesque]] posuere tortor sed mauris aliquam eget [[facilisis tellus]] imperdiet. Integer odio massa, eleifend gravida porta a, hendrerit non orci. [[Donec quis tellus]] vel purus consectetur posuere eu vitae urna. Phasellus sit amet augue ante. Aenean vulputate [[mattis]] sapien a sagittis.
+
+Cras [[hendrerit tellus]] ac enim hendrerit mattis. [[Praesent justo]] nulla, consequat et pharetra a, aliquet eu erat. Suspendisse commodo hendrerit dui vel vulputate. Sed [[sit amet]] orci ligula.
+
+Phasellus [[semper enim eu massa]] ornare convallis. Suspendisse quis elit sapien. Integer lacinia [[ultrices tortor]], nec rhoncus [[metus accumsan]] ut. Aliquam [[nec erat vitae]] diam semper eleifend. Morbi condimentum [[lacinia]] felis at rhoncus. Integer [[imperdiet ultricies]] lobortis. Curabitur [[aliquam lectus]] vel orci suscipit mollis. Integer faucibus [[laoreet]] pharetra. Aliquam at erat [[venenatis lacus]] rhoncus semper. Praesent fringilla ligula et quam ullamcorper id viverra est pretium.
+
+
+[[Category:Etiam condimentums]]
+") ?>
