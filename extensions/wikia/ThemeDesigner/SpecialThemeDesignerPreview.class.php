eb94385eb68bafb94bee15c8bcb900f8b75b6868
inez <inez@wikia-inc.com>
2010-09-08 09:40:40 +0000

    * Make added special page display some example content
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26305 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/SpecialThemeDesignerPreview.class.php
+++ extensions/wikia/ThemeDesigner/SpecialThemeDesignerPreview.class.php
@@ -8,6 +8,16 @@ class SpecialThemeDesignerPreview extends UnlistedSpecialPage {
 
 	public function execute() {
 		wfProfileIn( __METHOD__ );
+		global $wgOut;
+
+		$this->setHeaders();
+
+		$wgOut->setPageTitle('Example Page Title');
+
+		$wgOut->addHtml("<b>Some</b> <i>html</i>");
+
+		$wgOut->addWikiText("''And some'' '''wikitext'''");
+
 		wfProfileOut( __METHOD__ );
 	}
 }
