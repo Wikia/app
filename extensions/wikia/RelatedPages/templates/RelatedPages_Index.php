78da6d49c1da1992e2a6a3b4de33ca483543cd6d
bernhard <bernhard@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-09 21:18:21 +0000

    related pages frontend
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26396 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedPages/templates/RelatedPages_Index.php
+++ extensions/wikia/RelatedPages/templates/RelatedPages_Index.php
@@ -1,19 +1,21 @@
 <?php if( !$skipRendering ) { ?>
-	<section class="RelatedPagesModule">
+	<div class="RelatedPagesModule">
 		<h1><?= wfMsg('wikiarelatedpages-heading') ?></h1>
 		<ul>
 		<?php foreach($pages as $page) { ?>
 			<li>
+				<section class="content">
 				<?php if( isset( $page['imgUrl'] ) ) { ?>
 					<img src="<?= $page['imgUrl']; ?>" />
 				<?php } else { ?>
-					<div class="articleSnippet">"<?= $page['text']; ?>"</div>
+					<div class="articleSnippet"><p><?= $page['text']; ?></p></div>
 				<?php } ?>
+				</section>
 				<p>
 					<a href="<?= $page['url']; ?>"><?= $page['wrappedTitle'] ?></a>
 				</p>
 			</li>
 		<?php } ?>
 		</ul>
-	</section>
+	</div>
 <?php } // !skipRendering ?>
