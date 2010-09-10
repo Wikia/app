dbbca9550ffe7eb35f0048176aba063d419badd1
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-10 09:36:52 +0000

    fix recentchanges table after comment migration
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26413 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/fixes/fixblogcommentsrc.php
@@ -0,0 +1,87 @@
+<?php
+
+ini_set( "include_path", dirname(__FILE__)."/../../" );
+require( "commandLine.inc" );
+
+if (isset($options['help'])) {
+	die( "fix blog comments" );
+}
+
+$help = isset($options['help']);
+$dry = isset($options['dry']) ? $options['dry'] : "";
+
+$method = 'fixBlogCommentsRC';
+
+function fixAllBlogCommentsRC( $dry ) {
+	global $method, $wgExternalDatawareDB, $wgCityId;
+
+	# select rc_title from recentchanges where rc_namespace = 501 and ( rc_title not rlike '.+[0-9]+$' or rc_title not like '%@comment%' );
+
+	$dbw = wfGetDB( DB_MASTER );
+	$res = $dbw->select(
+		array( 'recentchanges' ),
+		array( 'rc_id, rc_title, rc_namespace'),
+		array(
+			'rc_namespace' => NS_BLOG_ARTICLE_TALK,
+			"( rc_title not rlike '.+[0-9]+$' or rc_title not like '%@comment%' )"
+		),
+		$method
+	);
+
+	$pages = array();
+	while( $row = $dbw->fetchRow($res) ) {
+		$pages[] = $row;
+	}
+	$dbw->freeResult( $res );
+
+	print sprintf("Found %0d pages \n", count( $pages ) );
+
+	if( !empty($pages) ) {
+		foreach ( $pages as $row ) {
+			print "parse " . $row['page_title'] . "\n";
+
+			$parts = ArticleComment::explode( $row[ 'rc_title' ] );
+			if( $parts['blog'] == 1 && count( $parts['partsOriginal'] ) > 0 ) {
+				$parts['parsed'] = array();
+				foreach ( $parts['partsOriginal'] as $id => $title ) {
+					$parts['parsed'][$id] = sprintf('%s-%s', '@comment', $title );
+				}
+
+				$newTitle = sprintf('%s/%s', $parts['title'], implode("/", $parts['parsed']) );
+
+				if( 1 ) {
+					printf(
+						"update recentchanges set rc_title = '%s' where rc_id = %d and rc_namespace = %d\n",
+						$newTitle,
+						$row['rc_id'],
+						NS_BLOG_ARTICLE_TALK
+					);
+				}
+				else {
+					$dbw->update(
+						'page',
+						array( 'page_title' => $newTitle ),
+						array( 'page_title' => $row['page_title'], 'page_namespace' => NS_BLOG_ARTICLE_TALK ),
+						$method
+					);
+				}
+			}
+		}
+	}
+}
+
+
+if ( $help ) {
+	echo <<<TEXT
+Usage:
+    php fixblogcomments.php --help
+    php fixblogcomments.php --parse=CITY_ID --dry
+
+    --help         : This help message
+    --dry		   : generate SQL commands (do not update in database)
+TEXT;
+	exit(0);
+}
+else {
+	fixAllBlogCommentsRC( $dry );
+}
