859586d798c6d1b81e835dd7a147f6f8cabdd5f6
sean <sean@wikia-inc.com>
2010-09-10 00:11:08 +0000

    Removed over-indentation.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26402 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SASS/wikia_sass.rb
+++ extensions/wikia/SASS/wikia_sass.rb
@@ -1,27 +1,27 @@
 require 'sass'
 
 module WikiaFunctions
-        def get_command_line_param(paramName, defaultResult="")
-                assert_type paramName, :String
-                retVal = defaultResult.to_s
+	def get_command_line_param(paramName, defaultResult="")
+		assert_type paramName, :String
+		retVal = defaultResult.to_s
 
-                # Look through all of the arguments given to the SASS command-line
-                ARGV.each do |arg|
-                        # If it has an '=' in it and there is something to each side, it is one of our key/value pairs
-                        if arg =~ /.=./
-                                pair = arg.split(/=/)
-                                if(pair[0] == paramName.value)
-                                        # Found correct param-name.  Use the first value if it exists, default to the passed-in default.
-                                        retVal = pair[1] || defaultResult
-                                end
-                        end
-                end
-				begin
-					Sass::Script::Parser.parse(retVal, 0, 0)
-				rescue
-					Sass::Script::String.new(retVal)
+		# Look through all of the arguments given to the SASS command-line
+		ARGV.each do |arg|
+				# If it has an '=' in it and there is something to each side, it is one of our key/value pairs
+				if arg =~ /.=./
+						pair = arg.split(/=/)
+						if(pair[0] == paramName.value)
+								# Found correct param-name.  Use the first value if it exists, default to the passed-in default.
+								retVal = pair[1] || defaultResult
+						end
 				end
-        end
+		end
+		begin
+			Sass::Script::Parser.parse(retVal, 0, 0)
+		rescue
+			Sass::Script::String.new(retVal)
+		end
+	end
 end
 
 module Sass::Script::Functions
