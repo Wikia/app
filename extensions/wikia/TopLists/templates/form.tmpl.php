6a2bcd7e11d305285d9daf4f9c413ddd7eff4c0a
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-10 22:07:00 +0000

    * work on the update dialog
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26445 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/TopLists/templates/form.tmpl.php
+++ extensions/wikia/TopLists/templates/form.tmpl.php
@@ -6,10 +6,13 @@ global $wgBlankImgUrl;
 		<div class="frame">
 			<div class="wrapper"><?= wfMsg( 'toplists-editor-image-browser-tooltip' ) ;?></div>
 		</div>
+
 		<a class="editsection wikia-chiclet-button" title="<?= wfMsg( 'toplists-editor-image-browser-tooltip' ); ?>" rel="nofollow">
 			<img alt="<?= wfMsg( 'toplists-editor-image-browser-tooltip' ); ?>" src="<?= wfBlankImgUrl() ;?>">
 		</a>
+
 		<input type="hidden" name="selected_picture_name" value="<?= $selectedPictureName ;?>" />
+
 		<? if ( !empty( $errors[ 'selected_picture_name' ] ) ) :?>
 			<? foreach( $errors[ 'selected_picture_name' ] as $errorMessage ) :?>
 				<p class="error"><?= $errorMessage ;?></p>
@@ -20,7 +23,8 @@ global $wgBlankImgUrl;
 	<div class="InputSet">
 		<label for="list_name"><?= wfMsg( 'toplists-editor-title-label' ) ;?></label>
 		<? if ( $mode == 'create' ) :?>
-			<input type="text" name="list_name" placeholder="<?= wfMsg( 'toplists-editor-title-placeholder' ) ;?>" value="<?= $listName ;?>"<?= ( !empty( $errors[ 'list_name' ] ) ) ? ' class="error"' : null ;?> />
+			<input type="text" name="list_name" placeholder="<?= wfMsg( 'toplists-editor-title-placeholder' ) ;?>"
+				value="<?= $listName ;?>"<?= ( !empty( $errors[ 'list_name' ] ) ) ? ' class="error"' : null ;?> />
 			<? if ( !empty( $errors[ 'list_name' ] ) ) :?>
 				<? foreach( $errors[ 'list_name' ] as $errorMessage ) :?>
 					<p class="error"><?= $errorMessage ;?></p>
@@ -30,9 +34,18 @@ global $wgBlankImgUrl;
 			<big><strong><?= $listName ;?></strong></big>
 		<? endif ;?>
 	</div>
+
 	<div class="InputSet AutoCompleteWrapper">
-		<label for="related_article_name"><?= wfMsg( 'toplists-editor-related-article-label' ) ;?> <small>(<?= wfMsg( 'toplists-editor-related-article-optional-label' ) ;?>)</small></label>
-		<input type="text" name="related_article_name" placeholder="<?= wfMsg( 'toplists-editor-related-article-placeholder' ) ;?>" autocomplete="off" value="<?= $relatedArticleName ;?>"<?= ( !empty( $errors[ 'related_article_name' ] ) ) ? ' class="error"' : null ;?> />
+		<label for="related_article_name">
+			<?= wfMsg( 'toplists-editor-related-article-label' ) ;?>
+			&nbsp;
+			<small>(<?= wfMsg( 'toplists-editor-related-article-optional-label' ) ;?>)</small>
+		</label>
+
+		<input type="text" name="related_article_name" placeholder="<?= wfMsg( 'toplists-editor-related-article-placeholder' ) ;?>"
+			autocomplete="off" value="<?= $relatedArticleName ;?>"
+			<?= ( !empty( $errors[ 'related_article_name' ] ) ) ? ' class="error"' : null ;?> />
+
 		<? if ( !empty( $errors[ 'related_article_name' ] ) ) :?>
 			<? foreach( $errors[ 'related_article_name' ] as $errorMessage ) :?>
 				<p class="error"><?= $errorMessage ;?></p>
@@ -49,21 +62,28 @@ global $wgBlankImgUrl;
 				<? endif ;?>
 				
 				<div class="ItemNumber">#<?= ( $item['type'] != 'template' ) ? $position : null ;?></div>
+
 				<div class="ItemName">
-					<input type="text" name="items_names[]" value="<?= $item['value'] ;?>"<?= ( $item['type'] == 'template' ) ? ' disabled' : null ;?><?= ( !empty( $errors[ "item_{$position}" ] ) ) ? ' class="error"' : null ;?> />
+					<input type="text" name="items_names[]" value="<?= $item['value'] ;?>"
+						<?= ( $item['type'] == 'template' ) ? ' disabled' : null ;?>
+						<?= ( !empty( $errors[ "item_{$position}" ] ) ) ? ' class="error"' : null ;?> />
 				</div>
+
 				<div class="ItemRemove">
 					<a title="<?= wfMsg( 'toplists-editor-remove-item-tooltip' ) ;?>" rel="nofollow">
 						<img alt="<?= wfMsg( 'toplists-editor-remove-item-tooltip' ) ;?>" src="<?= wfBlankImgUrl() ;?>">
 					</a>
 				</div>
+
 				<? if ( $isDraggable ) :?>
 					<div class="ItemDrag">
 						<a title="<?= wfMsg( 'toplists-editor-drag-item-tooltip' ) ;?>" rel="nofollow">
-							<img alt="<?= wfMsg( 'toplists-editor-drag-item-tooltip' ) ;?>" src="<?= wfBlankImgUrl() ;?>">
+							<img alt="<?= wfMsg( 'toplists-editor-drag-item-tooltip' ) ;?>"
+							     src="<?= wfBlankImgUrl() ;?>">
 						</a>
 					</div>
 				<? endif ;?>
+
 				<? if ( !empty( $errors[ "item_{$position}" ] ) ) :?>
 					<? foreach( $errors[ "item_{$position}" ] as $errorMessage ) :?>
 						<p class="error"><?=$errorMessage ;?></p>
@@ -72,16 +92,19 @@ global $wgBlankImgUrl;
 			</li>
 		<? endforeach ;?>
 	</ul>
+
 	<div class="AddControls">
 		<a id="toplist-add-item" class="wikia-chiclet-button" title="<?= wfMsg( 'toplists-editor-add-item-tooltip' ) ;?>" rel="nofollow">
 			<img alt="<?= wfMsg( 'toplists-editor-add-item-tooltip' ) ;?>" src="<?= wfBlankImgUrl() ;?>">
 		</a>
 		<label><?= wfMsg( 'toplists-editor-add-item-label' ) ;?></label>
 	</div>
+
 	<div class="FormButtons">
 		<? if ( $mode == 'update' ) :?>
 			<a class="wikia-button secondary" href="<?= $listUrl ;?>"><?= wfMsg( "toplists-cancel-button" ) ;?></a>
 		<? endif ;?>
+		
 		<input type="Submit" value="<?= wfMsg( "toplists-{$mode}-button" ) ;?>"/>
 	</div>
 </form>
