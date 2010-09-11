460491ef9d2eccf7a9baf8ff94067bc14027457e
sean <sean@wikia-inc.com>
2010-09-11 20:00:21 +0000

    I'm not on the VPN so every time I change sublime, I'm going to commit. Sorry for the trac-spam!
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26482 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Sublime/testbed.php
+++ extensions/wikia/Sublime/testbed.php
@@ -47,22 +47,73 @@ require( dirname(__FILE__) . '/../../../includes/WebStart.php' );
 		Sublime, isn't it?
 	</article>
 	<br/>
+	<button id='makeContentEditable' onclick='makeContentEditable()'>Make editable</button> &nbsp;&nbsp;&nbsp;&nbsp;
 	<button id='submitContentToWikia' onclick='submitSublime()'>Send it!</button>
-	
+
+	<div id='loginCredentials'>
+		<form id='loginForm'>
+			Login credentials:<br/>
+			<input type='text' id='loginName' value='Sean Colombo'/> Username<br/>
+			<input type='password' id='loginPass'/> Password</br>
+			<button onclick='sublimeLoginWrapper()'>Log in</button>
+		</form>
+	</div>
 
 	<!-- Javascript at the bottom - don't anger Artur! -->
 	<script type="text/javascript" src="/skins/common/jquery/jquery-1.4.2.js?1284232976"></script>
 	<script type="text/javascript" src="/skins/common/jquery/jquery.json-1.3.js?1284232976"></script>
 	<script type="text/javascript" src="/skins/common/jquery/jquery.wikia.js?1284232976"></script>
 	<script type='text/javascript'>
-		function submitSublime(){
-			var sublimeContent = $('#sendMeToWikia').html();
-			alert(sublimeContent);
-		}
-	
 		// Setup for Mediawiki.js
 		var wgScriptPath = "http://sean.wikia-dev.com"; // this should be the endpoint.
 	</script>
 	<script type='text/javascript' src='/extensions/wikia/JavascriptAPI/Mediawiki.js'/>
+	
+	<script type='text/javascript'>
+// TODO: Put the parts that we'll actually use in the extention into object-oriented JS or just a closure instead of a bunch of random functions like this.
+// TODO: Put the parts that we'll actually use in the extention into object-oriented JS or just a closure instead of a bunch of random functions like this.
+
+// PROBABLY JUST CODE FOR THIS PAGE //
+
+		function submitSublime(){
+			var sublimeContent = $('#sendMeToWikia').html();
+			alert(sublimeContent);
+		}
+		function makeContentEditable(){
+		}
+		function sublimeLoginWrapper(){
+			var wikiUsername = $('loginName').val();
+			var wikiPass = $('loginPass').val();
+			return sublimeLogin(wikiUsername, wikiPass);
+		}
+		
+		
+		
+// PROBABLY TO BE PUT INTO EXTENSION (WITH MODIFICATIONS) //
+
+		// Login the user.		
+		function sublimeLogin(wikiUsername, wikiPass){
+			try { 
+				Mediawiki.updateStatus("Logging in...");
+				Mediawiki.login(wikiUsername, wikiPass, loginWorked, apiFailed);
+			} catch (e) {
+				Mediawiki.updateStatus( "Error logging in:" + Mediawiki.print_r(e));	
+			}
+		}
+		function loginWorked (){
+			Mediawiki.updateStatus("Login successful");
+			$("#loginForm").fadeOut();
+			$("#logoutForm").fadeIn();
+		}
+
+		function apiFailed(reqObj, msg, error){
+				Mediawiki.waitingDone();
+			if (typeof msg == "undefined" && typeof reqObj == "string"){
+				msg = reqObj;
+			} else if (typeof msg == "object"){
+						msg = Mediawiki.print_r(msg);
+			}
+			Mediawiki.updateStatus("ERROR from API: " + msg, true);
+		}
 </body>
 </html>
