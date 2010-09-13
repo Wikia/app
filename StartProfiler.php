de560f88bed795464a851aac034858d1a06e1309
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-13 10:33:45 +0000

    we're not using UDP or Stomp profilers at the moment
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26534 e846bd91-c042-42a7-a218-4b65e2c1904d

--- StartProfiler.php
+++ StartProfiler.php
@@ -1,7 +1,5 @@
 <?php
 
-$rand_mod_10 = mt_rand(0, 0x7fffffff) % 10;
-
 if( !empty( $_GET['forceprofile'] ) ) {
 	require_once( dirname(__FILE__).'/includes/ProfilerSimpleText.php' );
 	$wgProfiler = new ProfilerSimpleText;
@@ -9,13 +7,6 @@ if( !empty( $_GET['forceprofile'] ) ) {
 } elseif( !empty( $_GET['forcetrace'] ) ) {
 	require_once( dirname(__FILE__).'/includes/ProfilerSimpleTrace.php' );
 	$wgProfiler = new ProfilerSimpleTrace;
-} elseif( $rand_mod_10 == 1 ) {
-	require_once( dirname(__FILE__).'/includes/ProfilerSimpleStomp.php' );
-	$wgProfiler = new ProfilerSimpleStomp;
-} elseif( $rand_mod_10 == 2 ) {
-	require_once( dirname(__FILE__).'/includes/ProfilerSimpleUDP.php' );
-	$wgProfiler = new ProfilerSimpleUDP;
-	$wgProfiler->setProfileId( 'wikia' );
 } else {
 	require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
 }
