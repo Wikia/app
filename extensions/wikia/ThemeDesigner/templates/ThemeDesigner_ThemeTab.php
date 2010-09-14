cc769d756fcc3dc78eb580ca7e83527f3a1e31f9
inez <inez@wikia-inc.com>
2010-09-14 14:39:20 +0000

    * Very important refactoring for Theme Designer JS
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26614 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/templates/ThemeDesigner_ThemeTab.php
+++ extensions/wikia/ThemeDesigner/templates/ThemeDesigner_ThemeTab.php
@@ -2,46 +2,16 @@
 	<img src="<?= $wgStylePath ?>/common/blank.gif" class="previous chevron disabled">
 	<div class="slider">
 		<ul>
-			<li data-theme="oasis">
-				<label>Oasis</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/oasis_preview.jpg">
+		<?php
+		foreach($wgOasisThemes as $k => $v) {
+		?>
+			<li data-theme="<?= $k ?>">
+				<label><?= ucfirst($k) ?></label>
+				<img src="<?= $wgStylePath ?>/oasis/images/themes/<?= $k ?>_preview.jpg">
 			</li>
-			<li data-theme="sapphire">
-				<label>Sapphire</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/sapphire_preview.jpg">
-			</li>
-			<li data-theme="jade">
-				<label>Jade</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/jade_preview.jpg">
-			</li>
-			<li data-theme="sky">
-				<label>Sky</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/sky_preview.jpg">
-			</li>
-			<li data-theme="moonlight">
-				<label>Moonlight</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/moonlight_preview.jpg">
-			</li>
-			<li data-theme="obsession">
-				<label>Obsession</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/obsession_preview.jpg">
-			</li>
-			<li data-theme="carbon">
-				<label>Carbon</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/carbon_preview.jpg">
-			</li>
-			<li data-theme="beach">
-				<label>Beach</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/beach_preview.jpg">
-			</li>
-			<li data-theme="dragstrip">
-				<label>Dragstrip</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/dragstrip_preview.jpg">
-			</li>
-			<li data-theme="aliencrate">
-				<label>Alien Crate</label>
-				<img src="<?= $wgStylePath ?>/oasis/images/themes/aliencrate_preview.jpg">
-			</li>
+		<?php
+		}
+		?>
 		</ul>
 	</div>
 	<img src="<?= $wgStylePath ?>/common/blank.gif" class="next chevron">
