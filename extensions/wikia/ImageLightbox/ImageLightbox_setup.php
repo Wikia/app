d0da2dcedfc59fac941a54c3ec163b669cbcbc5a
macbre <macbre@wikia-inc.com>
2010-09-14 12:54:23 +0000

    ImageLightbox: render image as img, not CSS background
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26601 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageLightbox/ImageLightbox_setup.php
+++ extensions/wikia/ImageLightbox/ImageLightbox_setup.php
@@ -1,7 +1,7 @@
 <?php
 $wgExtensionCredits['other'][] = array(
 	'name' => 'Image Lightbox',
-	'version' => '1.13',
+	'version' => '1.14',
 	'description' => 'Add lightbox preview for gallery images',
 	'author' => 'Maciej Brencz',
 );
