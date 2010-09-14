b13a7232f87d380c5d1ab0d00a9fa8ab0b3dc002
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-14 15:19:21 +0000

    * Cache buste for AdEngine
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26619 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdEngine.php
+++ extensions/wikia/AdEngine/AdEngine.php
@@ -60,7 +60,7 @@ abstract class AdProviderIframeFiller {
 
 class AdEngine {
 
-	const cacheKeyVersion = "2.02a";
+	const cacheKeyVersion = "2.03a";
 	const cacheTimeout = 1800;
         const lazyLoadAdClass = 'LazyLoadAd';
         const fillIframeFunctionPrefix = 'fillIframe_';
