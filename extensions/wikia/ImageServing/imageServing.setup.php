b0e8c1f4e7402f2dab46ceb43201933bbeb752b0
jakub <jakub@wikia-inc.com>
2010-09-15 11:14:13 +0000

    RSS Feeds for Widgetbox - fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26676 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageServing/imageServing.setup.php
+++ extensions/wikia/ImageServing/imageServing.setup.php
@@ -25,7 +25,7 @@ if (isset($wgHooks['BeforeParserrenderImageGallery'])) {
 }
 
 /* Adds imageCrop api to lists */
-if ( !empty( $wgEnableWidgetBoxFeed ) ) {
+if ( !empty( $wgEnableWidgetBoxFeedExt ) ) {
 	global $wgAPIModules;
 	$wgAutoloadClasses[ "WikiaApiCroppedImage"         ] = "{$IP}/extensions/wikia/WikiaApi/WikiaApiCroppedImage.php";
 	$wgAPIModules[ "imagecrop" ] = "WikiaApiCroppedImage";
