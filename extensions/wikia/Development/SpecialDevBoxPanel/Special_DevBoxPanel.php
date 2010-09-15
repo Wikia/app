1b40ff27f57c5d31704c4747b667cc4c644a21b7
garth <garth@wikia-inc.com>
2010-09-15 22:47:22 +0000

    Fix weird merge
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26731 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Development/SpecialDevBoxPanel/Special_DevBoxPanel.php
+++ extensions/wikia/Development/SpecialDevBoxPanel/Special_DevBoxPanel.php
@@ -277,33 +277,6 @@ function getHtmlForSvnTool() {
 	return $tmpl->execute('svn-tool');
 }
 
-	$perms = fileperms($base_path);
-	if (!($perms & 0x0010)) {
-		$errors[] = "Incorrect file permissions. To fix, run:<br><code>sudo chmod -R g+w $base_path</code>";
-	}
-
-	$tmpl = new EasyTemplate( dirname( __FILE__ ) . "/templates/" );
-	$data = shell_exec("svn info ".dirname(dirname(dirname(dirname(dirname( __FILE__ ))))));
-
-	foreach (split("\n", $data) as $line) {
-		if (empty($line)) continue;
-		list($k, $v) = split(": ", $line);
-		$info{$k} = $v;
-	}
-
-	$data = shell_exec("svn list https://svn.wikia-code.com/wikia/branches/");
-	$branches = array_map(create_function('$v', 'return rtrim($v, "/");'), split("\n", $data));
-
-	$tmpl->set_vars(array(
-						"svnUrl"     => $info{'URL'},
-						"svnUpdated" => $info{'Last Changed Date'},
-						"branches"   => array_merge(array('trunk'), $branches),
-						"action"     => $wgTitle->getLocalUrl(),
-						"errors"     => $errors,
-						));
-	return $tmpl->execute('svn-tool');
-}
-
 /**
  * A tool which shows what databases are installed
  * locally as well as which are available from
