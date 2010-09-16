cffc81c75314f8e3b59dd75a7f21e35addd3299c
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-16 09:51:36 +0000

    * ImageServing: final implementation of generic thumbnailing method + minor code improvements
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26757 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageServing/imageServing.class.php
+++ extensions/wikia/ImageServing/imageServing.class.php
@@ -18,7 +18,7 @@ class imageServing{
 	 * @param $width \int image width
 	 * @param $width \int
 	 */
-	function __construct($articles, $width = 100, $proportion = array("w" => 1, "h" => 1), $db = null){
+	function __construct($articles = null, $width = 100, $proportion = array("w" => 1, "h" => 1), $db = null){
 
 		if( is_array( $articles ) ) {
 			foreach($articles as $article){
@@ -49,143 +49,166 @@ class imageServing{
 
 	public function getImages( $n = 5, $article_lp = 0 ) {
 		global $wgMemc;
-		$articles = $this->articles;
+		
 		wfProfileIn( __METHOD__ );
-		$cache_return = array();
-		
-		foreach ( $articles as $key => $value ) {
-			$mcKey = wfMemcKey( "imageserving", $this->width, $n, $this->proportion["w"], $this->proportion["h"], $value );
-			$mcOut = $wgMemc->get( $mcKey, null );
-
-			if($mcOut != null) {
-				unset( $articles[ $key ] );
-				$cache_return[ $value ] = $mcOut;
-			}
-		}
-
-		if( count( $articles ) < 1 ) {
-			return $cache_return;
-		}
-
-		if( $this->db == null ) {
-			$db = wfGetDB( DB_SLAVE, array() );
-		} else {
-			$db = $this->db;
-		}
-
-		$image_list = array();
-		$images_name = array();
-		
-		$res = $db->select(
-			array( 'page_wikia_props' ),
-			array(
-				'page_id',
-				'props'
-			),
-			array(
-				'page_id in(' . implode( ",", $articles ) . ')',
-				"propname = 'imageOrder' or propname =  0"
-			),
-			__METHOD__
-		);
-
-
-
-		/* build list of images to get info about it */
-		while ($row =  $db->fetchRow( $res ) ) {
-			$props = unserialize( $row['props'] );
-			foreach( $props as $key => $value ) {
-				if( empty($image_list[$value][$row['page_id']]) ) {
-					if( empty($image_list[$value]) ) {
-						$images_name[] = $db->addQuotes( $value );
-					}
-					$image_list[$value][$row['page_id']] = $key;
-				}
-			}
-		}
-		
-		if ( count( $image_list ) == 0 ) {
-			wfProfileOut( __METHOD__ );
-			return $cache_return;
-		}
-
-		$res = $db->select(
-	            array( 'imagelinks LEFT JOIN image on il_to = img_name ' ),
-	            array(	'count(*) cnt',
-	            		'il_to',
-	            		'img_width',
-	            		'img_height',
-	            		'img_minor_mime'
-	            	 ),
-				array(),
-	           	__METHOD__,
-	            array(
-	            	"GROUP BY" => "il_to",
-	            	"HAVING" => implode(' and ',array(
-	            			"il_to in(".implode( ",", $images_name ).")",
-	            			"cnt < ".$this->maxCount,
-	            			"img_height > ".$this->minSize,
-	            			"img_width > ".$this->minSize
-	            		))
-	            )
-		);
-
-		$db_out = array();
-
-		while ($row =  $db->fetchRow( $res ) ) {
-			if(!in_array($row['img_minor_mime'], array( "svg+xml","svg"))) {
-				$db_out[$row['il_to']] = $row;
-			}
-		}
-
-		if (count($db_out) == 0) {
-			wfProfileOut(__METHOD__);
-			return $cache_return;
-		}
 
 		$out = array();
+		$articles = $this->articles;
+		
+		if( !empty( $articles ) ) {
+			$cache_return = array();
 
-		foreach( $image_list as $key => $value  ) {
-			if( isset($db_out[ $key ]) ) {
-				foreach($value as $key2 => $value2) {
-					if (empty($out[$key2]) || count($out[$key2]) < $n) {
-						$out[$key2][] = array(
-							"name" => $key,
-							"url" => $this->getUrl($key, $db_out[$key]['img_width'], $db_out[$key]['img_height']));
+			foreach ( $articles as $key => $value ) {
+				$mcOut = $wgMemc->get( $this->_makeKey( $value, $n ), null );
+
+				if($mcOut != null) {
+					unset( $articles[ $key ] );
+					$cache_return[ $value ] = $mcOut;
+				}
+			}
+
+			if( count( $articles ) < 1 ) {
+				return $cache_return;
+			}
+
+			if( $this->db == null ) {
+				$db = wfGetDB( DB_SLAVE, array() );
+			} else {
+				$db = $this->db;
+			}
+
+			$image_list = array();
+			$images_name = array();
+
+			$res = $db->select(
+				array( 'page_wikia_props' ),
+				array(
+					'page_id',
+					'props'
+				),
+				array(
+					'page_id in(' . implode( ",", $articles ) . ')',
+					"propname = 'imageOrder' or propname =  0"
+				),
+				__METHOD__
+			);
+
+
+
+			/* build list of images to get info about it */
+			while ($row =  $db->fetchRow( $res ) ) {
+				$props = unserialize( $row['props'] );
+				foreach( $props as $key => $value ) {
+					if( empty($image_list[$value][$row['page_id']]) ) {
+						if( empty($image_list[$value]) ) {
+							$images_name[] = $db->addQuotes( $value );
+						}
+						$image_list[$value][$row['page_id']] = $key;
 					}
 				}
 			}
-		}
 
-		foreach ($out as $key => $value) {
-			$mcKey = wfMemcKey("imageserving", $this->width, $n, $this->proportion["w"], $this->proportion["h"], $key);
-			$wgMemc->set($mcKey, $value, 60*60);
+			if ( count( $image_list ) == 0 ) {
+				wfProfileOut( __METHOD__ );
+				return $cache_return;
+			}
+
+			$res = $db->select(
+			    array( 'imagelinks LEFT JOIN image on il_to = img_name ' ),
+			    array(	'count(*) cnt',
+					'il_to',
+					'img_width',
+					'img_height',
+					'img_minor_mime'
+				 ),
+					array(),
+				__METHOD__,
+			    array(
+				"GROUP BY" => "il_to",
+				"HAVING" => implode(' and ',array(
+						"il_to in(".implode( ",", $images_name ).")",
+						"cnt < ".$this->maxCount,
+						"img_height > ".$this->minSize,
+						"img_width > ".$this->minSize
+					))
+			    )
+			);
+
+			$db_out = array();
+
+			while ($row =  $db->fetchRow( $res ) ) {
+				if(!in_array($row['img_minor_mime'], array( "svg+xml","svg"))) {
+					$db_out[$row['il_to']] = $row;
+				}
+			}
+
+			if (count($db_out) == 0) {
+				wfProfileOut(__METHOD__);
+				return $cache_return;
+			}
+
+			foreach( $image_list as $key => $value  ) {
+				if( isset($db_out[ $key ]) ) {
+					foreach($value as $key2 => $value2) {
+						if (empty($out[$key2]) || count($out[$key2]) < $n) {
+							$out[$key2][] = array(
+								"name" => $key,
+								"url" => $this->getUrl($key, $db_out[$key]['img_width'], $db_out[$key]['img_height']));
+						}
+					}
+				}
+			}
+
+			foreach ($out as $key => $value) {
+				$wgMemc->set( $this->_makeKey( $key, $n ), $value, 60*60 );
+			}
+
+			wfProfileOut(__METHOD__);
+			return $out + $cache_return;
 		}
 
 		wfProfileOut(__METHOD__);
-		return $out + $cache_return;
+		return $out;
 	}
 
 	/**
-	 * Supplies the URL to an image thumbnail of the desired size
+	 *  fetches an array with thumbnails and titles for the supplied files
 	 *
 	 * @author Federico "Lox" Lucignano
 	 *
-	 * @param string $fileName
-	 * @param int $width
-	 * @param int $height
-	 * @return Array an array containing the url to the thumbnail image and the title of the image article
+	 * @param Array $fileNames a list of file names to fetch thumbnails for
+	 * @return Array an array containing the url to the thumbnail image and the title associated to the file
 	 */
-	public function getThumbnail( $fileName, $width = 0, $height = 0) {
-		$title = Title::newFromText( $fileName, NS_FILE );
-		if ( $img = wfFindFile( $title ) ) {
-			return array(
-				'name' => $title->getText(),
-				'url' => wfReplaceImageServer( $img->getThumbUrl( $this->getCut( $width, $height ) . "-" . $img->getName() ) )
-			);
+	public function getThumbnails( $fileNames = null ) {
+		wfProfileIn( __METHOD__ );
+
+		global $wgMemc;
+		$ret = array();
+		
+		if( !empty( $fileNames ) ) {
+			foreach ( $fileNames as $fileName ) {
+				$title = Title::newFromText( $fileName, NS_FILE );
+				$mcKey = $this->_makeKey( uniqid('asd') );
+				$mcOut = $wgMemc->get( $mcKey, null );
+				
+				if( $mcOut != null ) {
+					$ret[ $fileName ] = $mcOut;
+				} elseif ( $img = wfFindFile( $title ) ) {
+					$imageInfo = getimagesize( $img->getPath() );
+					
+					$ret[ $fileName ] =  array(
+						'name' => $title->getText(),
+						'url' => wfReplaceImageServer( $img->getThumbUrl( $this->getCut( $imageInfo[ 0 ], $imageInfo[ 1 ]) . "-" . $img->getName() ) )
+					);
+
+					$wgMemc->set( $mcKey, $ret[ $fileName ], 60*60 );
+				}
+			}
 		}
 
-		return null;
+		wfProfileOut( __METHOD__ );
+		
+		return $ret;
 	}
 	
 	/**
@@ -200,13 +223,24 @@ class imageServing{
 	 * @return  \string url for image
 	 */
 
-	private function getUrl($name, $width = 0, $height = 0) {
-		$file_title = Title::newFromText($name ,NS_FILE );
+	private function getUrl( $name, $width = 0, $height = 0 ) {
+		$file_title = Title::newFromText( $name ,NS_FILE );
 		$img = wfFindFile( $file_title  );
-		if($img == null) {
+
+		if( $img == null ) {
 			return "";
 		}
-		return wfReplaceImageServer($img->getThumbUrl( $this->getCut($width, $height)."-".$img->getName()));
+		
+		return wfReplaceImageServer( $img->getThumbUrl( $this->getCut( $width, $height ) . "-" . $img->getName() ) );
+	}
+
+	/**
+	 * Generates a memcache key based on the supplied value
+	 *
+	 * @author Federico "Lox" Lucignano
+	 */
+	private function _makeKey( $key, $n = 1 ) {
+		return wfMemcKey("imageserving", $this->width, $n, $this->proportion["w"], $this->proportion["h"], $key);
 	}
 
 	/**
@@ -219,7 +253,7 @@ class imageServing{
 	 * @return \string prefix for thumb image
 	 */
 
-	public function getCut($width, $height) {
+	public function getCut( $width, $height ) {
 		$pHeight = round(($width)*($this->proportion['h']/$this->proportion['w']));
 
 		if($pHeight >= $height) {
