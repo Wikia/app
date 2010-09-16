f2df5fe7f72c6966b823698d121ce284b848a80d
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-16 10:39:02 +0000

    ModuleDataTest: add unit test for anon
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26766 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ tests/ModuleDataAnonTest.php
@@ -0,0 +1,45 @@
+<?php
+// initialize skin only once
+
+global $wgTitle, $wgUser, $wgForceSkin, $wgOut;
+
+$wgForceSkin = 'oasis';
+$wgTitle = Title::newMainPage();
+
+wfSuppressWarnings();
+ob_start();
+
+$wgOut->setCategoryLinks(array('foo' => 1, 'bar' => 2));
+
+$skin = $wgUser->getSkin();
+$skin->outputPage(new OutputPage());
+
+wfClearOutputBuffers();
+wfRestoreWarnings();
+
+class ModuleDataAnonTest extends PHPUnit_Framework_TestCase {
+
+	// TODO: use it when we will update phpunit to v3.4+
+	public static function setUpBeforeClass() {}
+
+	function testAccountNavigationModule() {
+		global $wgUser;
+		$userName = $wgUser->getName();
+
+		$moduleData = Module::get('AccountNavigation')->getData();
+
+		// there should be no avatar or profile link
+		$this->assertTrue(is_null($moduleData['profileLink']));
+		$this->assertTrue(is_null($moduleData['profileAvatar']));
+		$this->assertTrue(is_null($moduleData['dropdown']));
+
+		// log in / register links
+		$this->assertRegExp('/Special:Signup(.*)type=login/', $moduleData['links'][0]);
+		$this->assertRegExp('/Special:Signup(.*)type=signup/', $moduleData['links'][1]);
+
+		// user data
+		$this->assertTrue($moduleData['isAnon']);
+		$this->assertEquals($userName, $moduleData['username']);
+	}
+
+}
