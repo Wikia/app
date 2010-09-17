3c5bb1201a6f33eefc3872b4a4ec1bd9245151d2
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-17 13:56:03 +0000

    * TopLists: changes to the list template
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26873 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/TopLists/templates/list.tmpl.php
+++ extensions/wikia/TopLists/templates/list.tmpl.php
@@ -1,15 +1,16 @@
 <div id="toplists-list-body">
 	<?php if( !empty($relatedImage) ): ?>
-		<div class="ListPicture">
-			<a href="<?= $relatedUrl?>" class="image">
-				<img src="<?= $relatedImage['url']; ?>" alt="<?= $relatedImage['name']; ?>" title="<?= $relatedImage['name']; ?>" />
-			</a>
-			<? if($relatedTitle instanceof Title) :?>
-				<p>
-					<?= wfMsg( 'toplists-list-related-to' ); ?> <a href="<?= $relatedTitle->getLocalUrl(); ?>"><?= $relatedTitle->getText(); ?></a>
-				</p>
-			<? endif ;?>
-		</div>
+		[[File:<?= $relatedImage[ 'name' ] ;?>|thumb]]
+		<?/* if($relatedTitle instanceof Title) :?>
+			<div class="thumbcaption">
+				<div class="magnify">
+					<a href="/wiki/File:Images234234234234.jpg" class="internal" title="Enlarge">
+						<img src="http://images1.wikia.nocookie.net/__cb21710/common/skins/common/blank.gif" class="sprite details" width="16" height="16" alt="">
+					</a>
+				</div>
+				<?= wfMsg( 'toplists-list-related-to' ); ?> <a href="<?= $relatedTitle->getLocalUrl(); ?>"><?= $relatedTitle->getText(); ?></a>
+			</p>
+		<? endif ;*/?>
 	<?php elseif($relatedTitle instanceof Title): ?>
 		<div class="ListRelatedArticle">
 			<?= wfMsg( 'toplists-list-related-to' ); ?> <a href="<?= $relatedTitle->getLocalUrl(); ?>"><?= $relatedTitle->getText(); ?></a>
@@ -23,7 +24,7 @@
 		<li>
 			<div class="ItemNumber">
 				#<?= ( ++$index ) ;?>
-				<a href="#" class="wikia-button item-vote-button" id="<?= $item->getTitleText() ;?>" ><?= wfMsg( 'toplists-list-vote-up' ) ;?></a>
+				<div class="button item-vote-button" id="<?= $item->getTitleText() ;?>"><?= wfMsg( 'toplists-list-vote-up' ) ;?></div>
 			</div>
 			<div class="ItemContent">
 				<?= $item->getArticle()->getContent() ;?>
