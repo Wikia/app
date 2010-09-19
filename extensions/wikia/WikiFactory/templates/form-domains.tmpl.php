ea0206f67fe2ab2efbe91ab29be077bb64c90f96
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-19 00:18:04 +0000

    add hover text on domain "links", fix whitespace
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26904 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/templates/form-domains.tmpl.php
+++ extensions/wikia/WikiFactory/templates/form-domains.tmpl.php
@@ -1,6 +1,6 @@
 <!-- s:<?= __FILE__ ?> -->
 <h2>
-    Domain info &amp; setup
+	Domain info &amp; setup
 </h2>
 <div id="wiki-factory-domains">
 <?php
@@ -10,28 +10,28 @@
 ?>
 
 	<ul>
-        <li>
-            Domain in city_list is
-            <strong>
-                <a href="<?= $wiki->city_url ?>" target="_blank">
+		<li>
+			Domain in city_list is
+			<strong>
+				<a href="<?= $wiki->city_url ?>" target="_blank">
 					<?php echo $wiki->city_url ?>
 				</a>
-            </strong>
-        </li>
-        <li>
-            Wiki primary domain <strong>$wgServer</strong> is
-            <?php
-                $server = WikiFactory::getVarValueByName( "wgServer", $wiki->city_id );
-                echo empty( $server )
+			</strong>
+		</li>
+		<li>
+			Wiki primary domain <strong>$wgServer</strong> is
+			<?php
+				$server = WikiFactory::getVarValueByName( "wgServer", $wiki->city_id );
+				echo empty( $server )
 					? "not set"
 					: "set to <strong><a href=\"{$server}\" target=\"_blank\">{$server}</a></strong>";
-            ?>
-            <div>
+			?>
+			<div>
 				Changing value of <strong>$wgServer</strong> will change domain in city_list as well!
 			</div>
-        </li>
-        <!-- s:enabling/disabling/redirecting -->
-        <li>
+		</li>
+		<!-- s:enabling/disabling/redirecting -->
+		<li>
 			<form action="<?php echo $title->getFullUrl() ?>" method="post">
 				<input type="hidden" name="wpAction" value="status" />
 				Change Wikia status to:
@@ -39,38 +39,38 @@
 				<?php
 					foreach ( $statuses as $key => $value):
 				?>
-				    <option value="<?php echo $key ?>" <?php echo ( $key == $wiki->city_public ) ? 'selected="selected"' : "" ?> ><?php echo $value?></option>
+					<option value="<?php echo $key ?>" <?php echo ( $key == $wiki->city_public ) ? 'selected="selected"' : "" ?> ><?php echo $value?></option>
 				<?php
 					endforeach;
 				?>
 				</select>
 				<input type="submit" name="wk-status-submit" value="Confirm change" />
 			</form>
-        </li>
-        <!-- e:enabling/disabling/redirecting -->
-            <!-- s:configuring domains -->
-            <li>
-                Wiki Factory is configured for handling domains:
-                <ol id="wk-domain-ol">
-                    <?php
+		</li>
+		<!-- e:enabling/disabling/redirecting -->
+			<!-- s:configuring domains -->
+			<li>
+				Wiki Factory is configured for handling domains:
+				<ol id="wk-domain-ol">
+					<?php
 					foreach( $domains as $key => $domain): ?>
-                    <li id="wk-domain-li-<?= $key ?>"><?= $domain ?>
-                        <a id="wk-domain-li-<?= $key ?>change" href="#">[change]</a>&nbsp;
-                        <a id="wk-domain-li-<?= $key ?>remove" href="#">[remove]</a>&nbsp;
-                        <a id="wk-domain-li-<?= $key ?>setmain" href="#">[set main]</a>
-                        <script type="text/javascript">
-                        /*<![CDATA[*/
-                        	$Factory.Domain.listEvents("<?= $domain?>", <?= $key ?>);
-                        /*]]>*/
-                        </script>
-                    </li>
-                    <?php endforeach ?>
-                </ol>
-                <div>
-                    <input type="text" size="24" maxlength="255" id="wk-domain-add" name="wk-domain-add" />
-                    <input type="button" id="wk-domain-add-submit" value="Add new domain" />
-                </div>
-            </li>
+					<li id="wk-domain-li-<?= $key ?>"><?= $domain ?>
+						<a id="wk-domain-li-<?= $key ?>change" href="#" title="<?php echo wfMsg('wikifactory-domain-edit'); ?>">[change]</a>&nbsp;
+						<a id="wk-domain-li-<?= $key ?>remove" href="#" title="<?php echo wfMsg('wikifactory-domain-remove'); ?>">[remove]</a>&nbsp;
+						<a id="wk-domain-li-<?= $key ?>setmain" href="#" title="<?php echo wfMsg('wikifactory-domain-setmain'); ?>">[set main]</a>
+						<script type="text/javascript">
+						/*<![CDATA[*/
+							$Factory.Domain.listEvents("<?= $domain?>", <?= $key ?>);
+						/*]]>*/
+						</script>
+					</li>
+					<?php endforeach ?>
+				</ol>
+				<div>
+					<input type="text" size="24" maxlength="255" id="wk-domain-add" name="wk-domain-add" />
+					<input type="button" id="wk-domain-add-submit" value="Add new domain" />
+				</div>
+			</li>
 	</ul>
 </div>
 <!-- e:<?= __FILE__ ?> -->
