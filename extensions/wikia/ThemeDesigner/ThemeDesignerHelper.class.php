0a9c865dd70ea96c1613ab89eafdb3d604edf099
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-20 14:54:57 +0000

    * Move ThemeDesigner at the end of MyTools
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26947 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/ThemeDesignerHelper.class.php
+++ extensions/wikia/ThemeDesigner/ThemeDesignerHelper.class.php
@@ -2,20 +2,6 @@
 
 class ThemeDesignerHelper {
 
-	/**
-	 * Add Special:ThemeDesigner to MyTools menu
-	 */
-	public static function addToMyTools( &$tools ) {
-		global $wgUser;
-
-		if ( $wgUser->isAllowed( 'themedesigner' ) ) {
-			wfLoadExtensionMessages( 'ThemeDesigner' );
-			$tools[] = 'ThemeDesigner';
-		}
-
-		return true;
-	}
-
 	public static function parseText($text = "") {
 		global $wgTitle;
 		$tmpParser = new Parser();
