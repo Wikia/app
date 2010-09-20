19c14ab7f7e45bc871d8d2ebbb2b2331f4c29ce5
sean <sean@wikia-inc.com>
2010-09-20 21:21:34 +0000

    Attempt to move Wikia.css after all other headscripts.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26960 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/OasisModule.class.php
+++ skins/oasis/modules/OasisModule.class.php
@@ -67,6 +67,9 @@ class OasisModule extends Module {
 					"\"><!-- site js --></script>");
 		}
 
+		// Since modules add headscripts late in the execution and certain MediaWiki overrides need to be AFTER those files, we add them here.
+		$wgOut->addStyle( Skin::makeNSUrl( 'Wikia.css', $query, NS_MEDIAWIKI ) );
+
 		// We re-process the wgOut scripts and links here so modules can add to the arrays inside their execute method
 		$this->headscripts = $wgOut->getScript();
 		$this->csslinks = $wgOut->buildCssLinks();
