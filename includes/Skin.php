f6b769f267fcf58138f7fc532f9792f82ae132e1
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-20 21:21:34 +0000

    Attempt to move Wikia.css after all other headscripts.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26960 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/Skin.php
+++ includes/Skin.php
@@ -627,7 +627,8 @@ END;
 			if( empty($this->themename) || $this->themename == 'custom' ) {
 				$skinname = $this->getSkinName();
 				if($skinname == 'oasis') {
-					$out->addStyle( self::makeNSUrl( 'Wikia.css', $query, NS_MEDIAWIKI ) );
+					// Moved into OasisModule.class.php so that this file is AFTER other headscripts.
+					//$out->addStyle( self::makeNSUrl( 'Wikia.css', $query, NS_MEDIAWIKI ) );
 				} else {
 					$out->addStyle( self::makeNSUrl( 'Common.css', $query, NS_MEDIAWIKI ) );
 					$out->addStyle( self::makeNSUrl( $skinname . '.css', $query, NS_MEDIAWIKI ) );
