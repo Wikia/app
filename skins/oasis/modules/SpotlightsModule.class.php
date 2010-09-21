19d9ebf22cc4512032b60af1d0b039266cd15eb8
inez <inez@wikia-inc.com>
2010-09-21 09:37:09 +0000

    * Minor refactor
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@26981 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/SpotlightsModule.class.php
+++ skins/oasis/modules/SpotlightsModule.class.php
@@ -2,15 +2,11 @@
 class SpotlightsModule extends Module {
 
 	var $wgBlankImgUrl;
-	
+
 	var $wgSingleH1;
 
 	public function executeIndex() {
-		wfProfileIn(__METHOD__);
 
-		global $wgSingleH1;
-
-		wfProfileOut(__METHOD__);
 	}
 
 }
