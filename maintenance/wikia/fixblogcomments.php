25c4027fff91c2791dc691baefc87a276f871bba
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-23 10:11:21 +0000

    final version of the script
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27185 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/fixblogcomments.php
+++ maintenance/wikia/fixblogcomments.php
@@ -12,10 +12,49 @@ $userName = isset( $options['user'] ) ? $options['user'] : 'Maintenance script';
 $help = isset($options['help']);
 $wikia = isset($options['wikia']) ? $options['wikia'] : "";
 $dry = isset($options['dry']) ? $options['dry'] : "";
+$debug = isset($options['debug']) ? $options['debug'] : "";
 
 $method = 'fixBlogComments';
 
-echo "dry = $dry \n";
+function __parse($comment) {
+	$count = 0;
+	$titleTextStripped = str_replace('@comment-', '', $comment, $count);
+	$partsOriginal = explode('/', $comment);
+	$partsStripped = explode('/', $titleTextStripped);
+
+	//count != 0 for article and == 0 for blog
+	if ($count) {
+		$title = implode('/', array_splice($partsOriginal, 0, -$count));
+		array_splice($partsStripped, 0, -$count);
+	} else {
+		$title = implode('/', array_splice($partsOriginal, 0, 2));
+		array_splice($partsStripped, 0, 2);
+	}
+
+	$result = array(
+		'blog' => 1,
+		'title' => $title,
+		'partsOriginal' => $partsOriginal,
+		'partsStripped' => $partsStripped
+	);
+	return $result;
+}
+
+function __get_page_id($title) {
+	$dbw = wfGetDB( DB_SLAVE );
+	$row = $dbw->selectRow(
+		array( 'page' ),
+		array( 'page_id'),
+		array(
+			'page_namespace' 	=> NS_BLOG_ARTICLE,
+			'page_title'		=> $title
+		),
+		$method
+	);	
+	
+	$page_id = ( isset($row) && is_object($row) ) ? $row->page_id : 0;
+	return $page_id;
+}
 
 function fixAllBlogComments( $dry ) {
 	global $method, $wgExternalDatawareDB, $wgCityId, $wgDBname;
@@ -38,17 +77,25 @@ function fixAllBlogComments( $dry ) {
 	}
 	$dbw->freeResult( $res );
 
-	print sprintf("Found %s: %0d pages \n", $wgDBname, count($pages));
+	if ( $debug ) 
+		print sprintf("Found %s (%d): %0d pages \n", $wgDBname, $wgCityId, count($pages));
 
 	if( !empty($pages) ) {
 		foreach ( $pages as $row ) {
-			print "parse " . $row['page_title'] . "\n";
+			if ( $debug ) 
+				print "parse " . $row['page_title'] . "\n";
 
-			$parts = ArticleComment::explode($row['page_title']);
-			if ( $parts['blog'] == 1 && count($parts['partsOriginal']) > 0 ) {
+			$parts = __parse($row['page_title']);
+			
+			# check blog exists
+			$blog_page_id = __get_page_id( $parts['title'] );
+			
+			# get page id of blog comment
+			$row['page_id'] = __get_page_id( $row['page_title'] );
+			
+			if ( $parts['blog'] == 1 && count($parts['partsOriginal']) > 0 && $blog_page_id > 0 ) {
 				$parts['parsed'] = array();
 				foreach ( $parts['partsOriginal'] as $id => $title ) {
-#					list ($user, $date) = explode( '-', $title );
 					$parts['parsed'][$id] = sprintf('%s-%s', '@comment', $title );
 				}
 
@@ -106,21 +153,23 @@ function fixAllBlogComments( $dry ) {
 				}
 
 				# update dataware
-				if ( $dry ) {
-					 printf( "update pages set page_title = '%s' where page_id = %d and page_wikia_id = %d\n",
-						$newTitle,
-						$row['page_id'],
-						$wgCityId
-					);
-				}
-				else {
-					$dbext = wfGetDB( DB_MASTER, array(), $wgExternalDatawareDB );
-					$dbext->update(
-						'pages',
-						array( 'page_title' => $newTitle ),
-						array( 'page_id' => $row['page_id'], "page_wikia_id" => $wgCityId ),
-						$method
-					);
+				if ( $row['page_id'] > 0 ) {
+					if ( $dry ) {
+						 printf( "update pages set page_title = '%s' where page_id = %d and page_wikia_id = %d\n",
+							$newTitle,
+							$row['page_id'],
+							$wgCityId
+						);
+					}
+					else {
+						$dbext = wfGetDB( DB_MASTER, array(), $wgExternalDatawareDB );
+						$dbext->update(
+							'pages',
+							array( 'page_title' => $newTitle ),
+							array( 'page_id' => $row['page_id'], "page_wikia_id" => $wgCityId ),
+							$method
+						);
+					}
 				}
 				
 				if( $dry ) {
@@ -159,12 +208,12 @@ TEXT;
 else {
      global $wgCityId;
 	# set wgDisableBlogComments
-	WikiFactory::setVarByName( "wgDisableBlogComments", $wgCityId, true ) ;
-	WikiFactory::clearCache( $wgCityId );
+	#WikiFactory::setVarByName( "wgDisableBlogComments", $wgCityId, true ) ;
+	#WikiFactory::clearCache( $wgCityId );
 
 	fixAllBlogComments( $dry );
 
-	WikiFactory::removeVarByName( "wgDisableBlogComments", $wgCityId );
-	WikiFactory::setVarByName( "wgUseNewBlogComments", $wgCityId, true ) ;
-	WikiFactory::clearCache( $wgCityId );
+	#WikiFactory::removeVarByName( "wgDisableBlogComments", $wgCityId );
+	#WikiFactory::setVarByName( "wgUseNewBlogComments", $wgCityId, true ) ;
+	#WikiFactory::clearCache( $wgCityId );
 }
