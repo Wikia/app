8798a8f92c5e9e35266eb5285eabd178463ab6fe
uberfuzzy <uberfuzzy@wikia-inc.com>
2010-09-25 19:35:52 +0000

    stop display at non-allowed skin (#68331)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27327 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WidgetSpecialPage/WidgetsSpecialPage.i18n.php
+++ extensions/wikia/WidgetSpecialPage/WidgetsSpecialPage.i18n.php
@@ -3,7 +3,7 @@
 $messages = array();
 
 $messages['en'] = array(
-	'widgets-specialpage-info' => 'Widgets work best with one of the new skins, eg. [{{SERVER}}/index.php?title=Special:Widgets&useskin=monaco Monaco].
+	'widgets-specialpage-info' => 'Widgets work with the skin [{{SERVER}}/index.php?title=Special:Widgets&useskin=monaco Monaco].
 
 Please change [[Special:Preferences#prefsection-1|your preferences]] to use this tool.',
 	'widgets-specialpage-try-dashboard' => 'This page contains a list of all available widgets. To see your widgets at work go to [[Special:WidgetDashboard]].',
