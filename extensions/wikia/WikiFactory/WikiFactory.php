8e3464137f97e9586b52d7caef99131960550784
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-26 13:46:27 +0000

    disabling the namespace "tied" function for now, less needed with RelatedVars functionality, and soon the new NS manager interface
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27331 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/WikiFactory.php
+++ extensions/wikia/WikiFactory/WikiFactory.php
@@ -1623,7 +1623,7 @@ class WikiFactory {
 	 */
 	static public function getTiedVariables( $cv_name ) {
 		$tied = array(
-			"wgExtraNamespacesLocal|wgContentNamespaces|wgNamespacesWithSubpagesLocal|wgNamespacesToBeSearchedDefault"
+			#"wgExtraNamespacesLocal|wgContentNamespaces|wgNamespacesWithSubpagesLocal|wgNamespacesToBeSearchedDefault"
 		);
 		foreach( $tied as $group ) {
 			$pattern = "/\b{$cv_name}\b/";
