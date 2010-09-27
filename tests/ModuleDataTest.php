fa2cc2da8cddd046f4bb52e665edcb934ad86b68
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-27 13:05:48 +0000

    PageHeader: test case for ThemeDesigner preview page
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27356 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/ModuleDataTest.php
+++ tests/ModuleDataTest.php
@@ -592,6 +592,15 @@ class ModuleDataTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('', $moduleData['title']);
 		$this->assertEquals('', $moduleData['subtitle']);
 
+		// ThemeDesigner (preview page)
+		$wgTitle = Title::newFromText('ThemeDesignerPreview', NS_SPECIAL);
+
+		$moduleData = Module::get('PageHeader')->getData();
+
+		$this->assertType('array', $moduleData['revisions']);
+		$this->assertType('array', $moduleData['categories']);
+		$this->assertType('int', $moduleData['comments']);
+
 		// edit page
 		$wgRequest->setVal('action', 'edit');
 		$wgTitle = Title::newFromText('Foo', NS_TALK);
