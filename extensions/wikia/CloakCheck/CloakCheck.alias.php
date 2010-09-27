36919c5f4ae7514fb2d43d2a2550d2e799296eae
siebrand <siebrand@wikia-inc.com>
2010-09-27 12:16:32 +0000

    * add i18n for special page name * fix i18n issue with hard coded colon in field label * updated indentation * message tweaks
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27352 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/CloakCheck/CloakCheck.alias.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Aliases for special pages.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'CloakCheck' => array( 'CloakCheck' ),
+);
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
--- extensions/wikia/CloakCheck/CloakCheck.php
+++ extensions/wikia/CloakCheck/CloakCheck.php
@@ -7,14 +7,15 @@ if (!defined('MEDIAWIKI')) {
 $dir = dirname(__FILE__);
 
 $wgExtensionCredits['specialpage'][] = array(
-       'name' => 'IRC CloakCheck',
-       'author' => 'Chris \'Uberfuzzy\' Stafford', 
-	   'version' => '1.0 (fall 2010)',
-       'description' => 'cloakcheck-desc'
-       );
+	'name' => 'IRC CloakCheck',
+	'author' => 'Chris \'Uberfuzzy\' Stafford',
+	'version' => '1.0 (fall 2010)',
+	'description' => 'cloakcheck-desc'
+);
 
 $wgAutoloadClasses['CloakCheck'] = $dir . '/CloakCheck.body.php'; # Tell MediaWiki to load the extension body.
 $wgExtensionMessagesFiles['CloakCheck'] = $dir . '/CloakCheck.i18n.php';
+$wgExtensionAliasesFiles['CloakCheck'] = $dir . '/CloakCheck.alias.php';
 $wgSpecialPages['CloakCheck'] = 'CloakCheck'; # Let MediaWiki know about your new special page.
 
 $wgSpecialPageGroups['CloakCheck'] = 'users';
