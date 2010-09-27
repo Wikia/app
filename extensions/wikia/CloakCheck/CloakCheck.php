78323e1c73d3e48264131b8ce517625a80fbb710
uberfuzzy <uberfuzzy@wikia-inc.com>
2010-09-27 01:52:21 +0000

    all-in-one quick form for checking if you (or a username) qualify for a Wikia IRC cloak
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27335 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/CloakCheck/CloakCheck.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined('MEDIAWIKI')) {
+	echo "This is a MediaWiki extension.\n";
+	exit(1);
+}
+
+$dir = dirname(__FILE__);
+
+$wgExtensionCredits['specialpage'][] = array(
+       'name' => 'IRC CloakCheck',
+       'author' => 'Chris \'Uberfuzzy\' Stafford', 
+	   'version' => '1.0 (fall 2010)',
+       'description' => 'cloakcheck-desc'
+       );
+
+$wgAutoloadClasses['CloakCheck'] = $dir . '/CloakCheck.body.php'; # Tell MediaWiki to load the extension body.
+$wgExtensionMessagesFiles['CloakCheck'] = $dir . '/CloakCheck.i18n.php';
+$wgSpecialPages['CloakCheck'] = 'CloakCheck'; # Let MediaWiki know about your new special page.
+
+$wgSpecialPageGroups['CloakCheck'] = 'users';
