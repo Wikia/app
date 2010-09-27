4bd1fb4ef80c77f673b3ccb0d3404af87bd8f6a4
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-09-27 14:53:43 +0000

    StaticChute: refs RT #68286)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27373 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaticChute/wfReplaceCdnStylePathInCss.php
+++ extensions/wikia/StaticChute/wfReplaceCdnStylePathInCss.php
@@ -6,6 +6,10 @@
  * so that it doesn't break this multiline comment that you are reading), modify the URL to start with the actual wgCdnStylePath.
  * This can't be in the line itself (as in the .sql setup files) because that's not valid in CSS to have a comment inside a line.
  *
+ * Macbre: don't modify paths NOT starting with "/" - they already have domains. Compare:
+ *  - /skins/oasis/images/themes/beach.png
+ *  - http://images.wikia.com/muppet/images/e/e7/Oasis-background
+ *
  * This allows us to have images relative to the docroot in the CSS which will work on devboxes, but also do replacements in production
  * so that the resource will come from the CDN.
  *
@@ -20,7 +24,7 @@ function wfReplaceCdnStylePathInCss($css, $cdnStylePath=''){
 	}
 
 	if(strpos($css, "wgCdnStylePath") !== false){ // faster to skip the regex in most cases
-		$css = preg_replace("/([\(][\"']?)([^\n]*?)\s*\/\*\s*[\\\$]?wgCdnStylePath\s*\*\//is", '\\1'.$cdnStylePath.'\\2', $css);
+		$css = preg_replace("/([\(][\"']?)(\/[^\n]*?)\s*\/\*\s*[\\\$]?wgCdnStylePath\s*\*\//is", '\\1'.$cdnStylePath.'\\2', $css);
 	}
 	return $css;
 } // end wfReplaceCdnStylePathInCss()
