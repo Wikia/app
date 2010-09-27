e0fbcec8d78ab8fc1e33341091607e2acd98de6c
inez <inez@wikia-inc.com>
2010-09-27 13:38:21 +0000

    * Add three hours caching layer for GlobalNavigation
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27364 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/GlobalHeaderModule.class.php
+++ skins/oasis/modules/GlobalHeaderModule.class.php
@@ -5,17 +5,14 @@ class GlobalHeaderModule extends Module {
 	var $wgBlankImgUrl;
 	var $wgStylePath;
 	var $wgCdnStylePath;
+	var $wgSingleH1;
 
 	var $centralUrl;
 	var $createWikiUrl;
 	var $menuNodes;
-	
-	var $wgSingleH1;
-	
+
 	public function executeIndex() {
-		global $wgLangToCentralMap, $wgContLang, $wgCityId, $wgUser, $wgLang, $wgCdnStylePath, $wgMemc, $wgSingleH1;
-		
-		$this->wgCdnStylePath = $wgCdnStylePath;
+		global $wgLangToCentralMap, $wgContLang, $wgCityId, $wgLang;
 
 		// generate link to central wiki for language of this wiki
 		$this->centralUrl = Wikia::langToSomethingMap($wgLangToCentralMap, $wgContLang->getCode(), 'http://www.wikia.com/Wikia');
@@ -25,6 +22,23 @@ class GlobalHeaderModule extends Module {
 		$userlang = $userlang == 'en' ? '' : "?uselang=$userlang";
 
 		$this->createWikiUrl = "http://www.wikia.com/Special:CreateWiki{$userlang}";
+
+		// global navigation menu
+		$category = WikiFactory::getCategory($wgCityId);
+
+		$messageName = 'shared-Globalnavigation-'.$category->cat_id;
+
+		if($category === false || wfEmptyMsg($messageName, $text = wfMsg($messageName))) {
+			$messageName = 'shared-Globalnavigation';
+		}
+
+		$service = new NavigationService();
+		$this->menuNodes = $service->parseMessage($messageName, array(3, 4, 5), 60*60*3 /* 3 hours */);
+
+
+
+/*
+
 
 		$mKey = wfMemcKey('mOasisGlobalHeaderNodes', $userlang);
 		$this->menuNodes = $wgMemc->get($mKey);
@@ -39,5 +53,7 @@ class GlobalHeaderModule extends Module {
 			$wgMemc->set($mKey, $this->menuNodes, 86400);
 			// TODO: is there an event we can hook for invalidation?
 		}
+*/
+
 	}
 }
