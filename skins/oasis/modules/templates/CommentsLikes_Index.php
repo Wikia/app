fb832d06238d2d708b1c9c1dc8c835b9a07e8ee2
macbre <macbre@wikia-inc.com>
2010-09-30 08:55:43 +0000

    Comments & Likes: show old comments button on pages without like feature
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27549 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/CommentsLikes_Index.php
+++ skins/oasis/modules/templates/CommentsLikes_Index.php
@@ -1,28 +1,24 @@
 <?php
-	if (is_numeric($comments) || $showLike) {
+	// show both comments and FB likes (new design)
+	if (is_numeric($comments) && $showLike) {
 ?>
 <ul class="commentslikes">
-<?php
-		// render comments button
-		if (is_numeric($comments)) {
-?>
-	<li>
+	<li class="comments">
 		<span class="commentsbubble"><?= $formattedComments ?></span>
 		<a href="<?= htmlspecialchars($commentsLink) ?>"data-id="comment" title="<?= htmlspecialchars($commentsTooltip) ?>"<?= $commentsAccesskey ?>><?= wfMsgExt('oasis-page-header-comments', array('parsemag'), $comments) ?></a>
 	</li>
-<?php
-		}
-
-		// render FB likes button
-		if ($showLike) {
-?>
-	<li>
+	<li class="likes">
 		<fb:like layout="button_count" width="50" show_faces="false" ref="<?= $likeRef ?>" href="<?= htmlspecialchars($likeHref) ?>"></fb:like>
 	</li>
-<?php
-		}
-?>
 </ul>
+<?php
+	}
+	// show just comments (old design)
+	else if (is_numeric($comments)) {
+?>
+	<div class="commentslikes">
+		<a href="<?= htmlspecialchars($commentsLink) ?>" class="wikia-chiclet-button" data-id="comment" title="<?= htmlspecialchars($commentsTooltip) ?>"<?= $commentsAccesskey ?>><img class="osprite icon-article-like" src="<?= $wgBlankImgUrl ?>" height="10" width="10"></a> <?= $comments ?>
+	</div>
 <?php
 	}
 ?>
