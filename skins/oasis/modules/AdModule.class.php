81ccff356b70cb8802f044d9ece8f7b751a3af67
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-01 09:27:03 +0000

    * Fix typo in AdModule
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27603 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/AdModule.class.php
+++ skins/oasis/modules/AdModule.class.php
@@ -19,7 +19,7 @@ class AdModule extends Module {
 			// main page
 			self::$config['HOME_TOP_LEADERBOARD'] = true;
 			self::$config['INVISIBLE_1'] = true;
-			self::$config['INVISIBLE_2 '] = true;
+			self::$config['INVISIBLE_2'] = true;
 			if(ArticleAdLogic::isLongArticle(self::getSkinTemplateObj()->data['bodytext'])) {
 				// long page
 				self::$config['PREFOOTER_LEFT_BOXAD'] = true;
@@ -37,7 +37,7 @@ class AdModule extends Module {
 				self::$config['TOP_LEADERBOARD'] = true;
 				self::$config['TOP_RIGHT_BOXAD'] = true;
 				self::$config['INVISIBLE_1'] = true;
-				self::$config['INVISIBLE_2 '] = true;
+				self::$config['INVISIBLE_2'] = true;
 				if(ArticleAdLogic::isLongArticle(self::getSkinTemplateObj()->data['bodytext'])) {
 					// long content page
 					self::$config['LEFT_SKYSCRAPER_2'] = true;
