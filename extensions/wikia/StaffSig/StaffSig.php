2b57bdadc1e2e7c1883259846684d6330855fa2b
uberfuzzy <uberfuzzy@wikia-inc.com>
2010-10-01 21:06:37 +0000

    run staffsig image through cdn (#69127)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27651 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StaffSig/StaffSig.php
+++ extensions/wikia/StaffSig/StaffSig.php
@@ -17,10 +17,14 @@ function wfSigSetup(&$parser) {
 
 function wfMakeSignature( $contents, $attributes, $parser ) {
 	global $wgStylePath;
-	return '<a href="http://community.wikia.com/wiki/Project:Staff" title="This user is a member of Wikia staff" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
+	$url = 'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png';
+	$url = wfReplaceImageServer($url);
+	return '<a href="http://community.wikia.com/wiki/Project:Staff" title="This user is a member of Wikia staff" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\''.$url.'\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
 }
 
 function wfMakeHSignature( $contents, $attributes, $parser ) {
 	global $wgStylePath;
-	return '<a href="http://community.wikia.com/wiki/Help:Helper_Group" title="This user is a Wikia Helper" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
+	$url = 'http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png';
+	$url = wfReplaceImageServer($url);
+	return '<a href="http://community.wikia.com/wiki/Help:Helper_Group" title="This user is a Wikia Helper" class="staffSigLink"><img src="'.$wgStylePath.'/common/dot.gif" style="background-image: url(\''.$url.'\')" alt="@Wikia" class="staffSig" width="53" height="12" /></a>';
 }
