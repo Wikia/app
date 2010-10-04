66b800479e800d8687fe0615de153f419e9f9083
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-04 11:56:31 +0000

    RelatedPages: log stats via JS
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27679 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedPages/js/RelatedPages.js
+++ extensions/wikia/RelatedPages/js/RelatedPages.js
@@ -3,39 +3,69 @@ $(function() {
 });
 
 RelatedPages = {
+	sections: false,
+
 	log: function(msg) {
 		$().log(msg, 'RelatedPages');
 	},
+	report: function(data) {
+		if (/macbre/.test(window.wgServer)) {
+			data = data || {};
+			data.city = window.wgID;
+			data.page = window.wgPageName;
+			data.sections = this.sections.length;
+
+			$.get('/stats.php', data);
+		}
+	},
 	init: function() {
+		var start = (new Date()).getTime();
+
 		this.log('init');
 
 		var content = $('#WikiaArticle');
 		var module = $('.RelatedPagesModule');
 
+		// after which section module can be added
+		var addAfter = 3;
+
 		// get 2nd level headings
-		var sections = content.find('.mw-headline').parent().filter('h2');
+		this.sections = content.find('.mw-headline').parent().filter('h2');
 
 		// remove headings with floating element next to it (starting from fourth)
-		var filteredSections = sections.slice(3).filter(function() {
+		var filteredSections = this.sections.slice(addAfter).filter(function() {
 			var heading = $(this);
 			return heading.width() > 650;
 		});
 
-		this.log(sections);	this.log(filteredSections);	this.log(module);
+		this.log('found ' + this.sections.length + ' section(s)');
+		//this.log(this.sections); this.log(filteredSections); this.log(module);
 
+		// section found - move Related Pages module before it
 		if (filteredSections.exists()) {
 			var section = filteredSections.first();
-			var sectionId = sections.index(section) + 1;
+			var sectionId = this.sections.index(section) + 1;
 
 			this.log('moving before #' + sectionId + ' section (' + section.children().first().html() + ')');
 
 			module.insertBefore(section);
+
+			this.report({status: 'moved', sectionId: sectionId});
 		}
-		else if (sections.exists()) {
+		// sections found, but none without collision
+		else if (this.sections.length > addAfter) {
 			this.log('collision detected');
+
+			this.report({status: 'collision'});
 		}
+		// not enough sections found
 		else {
 			this.log('article is too short');
+
+			this.report({status: 'too_short'});
 		}
+
+		var time = (new Date()).getTime() - start;
+		this.log('done in ' + time + ' ms');
 	}
 }
