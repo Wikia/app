824b978c05cfd23b6f04f7b068c4340ac91e2b63
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-05 15:06:13 +0000

    Blogs: properly clean trimmed HTML (RT #69661)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27751 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/blog-page.tmpl.php
+++ extensions/wikia/Blogs/templates/blog-page.tmpl.php
@@ -1,11 +1,12 @@
 <!-- s:<?= __FILE__ ?> -->
 <div class="wk_blogs_panel" style="<?=(isset($aOptions['style']))?$aOptions['style']:''?>">
-	<div class="wk_blogs_title color1"><div><?= ( !empty($aOptions['title']) ) ? $aOptions['title'] : "" ?></div>
-	<div class="wk_blogs_title_refresh"><?=$skin->makeLinkObj($wgTitle, "<img src=\"".wfBlankImgUrl()."\" border=\"0\" class=\"sprite refresh\" />", "action=purge")?></div>
-</div>
-<div class="wk_blogs_body">
+	<div class="wk_blogs_title color1">
+		<div><?= ( !empty($aOptions['title']) ) ? $aOptions['title'] : "" ?></div>
+		<div class="wk_blogs_title_refresh"><?=$skin->makeLinkObj($wgTitle, "<img src=\"".wfBlankImgUrl()."\" border=\"0\" class=\"sprite refresh\" />", "action=purge")?></div>
+	</div>
+	<div class="wk_blogs_body">
 <? if (!empty($aRows)) { ?>
-<ul class="list" style="list-style-image:none;list-style-position:outside;list-style-type:none;margin:0.3em 0 0 0.5em">
+		<ul class="list" style="list-style-image:none;list-style-position:outside;list-style-type:none;margin:0.3em 0 0 0.5em">
 <?
 foreach ($aRows as $pageId => $aRow) {
 	$oTitle = Title::newFromText($aRow['title'], $aRow['namespace']);
@@ -18,8 +19,8 @@ foreach ($aRows as $pageId => $aRow) {
 		$isCommenting = $aRow['props']['commenting'];
 	}
 ?>
-<li style="line-height:normal;margin:0;padding:0;">
-<div class="wk_blogs_link"><a href="<?=$oTitle->getLocalUrl()?>"><?=BlogTemplateClass::getSubpageText($oTitle)?></a></div>
+			<li style="line-height:normal;margin:0;padding:0;">
+				<div class="wk_blogs_link"><a href="<?=$oTitle->getLocalUrl()?>"><?=BlogTemplateClass::getSubpageText($oTitle)?></a></div>
 <?
 /* s: TIMESTAMP */
 	if ( !empty($aOptions['timestamp']) ) {
@@ -29,7 +30,7 @@ foreach ($aRows as $pageId => $aRow) {
 			$sUserLinks = $aUserLinks['userpage']." (".$aUserLinks['talkpage']."|".$aUserLinks['contribs'].")";
 		}
 ?>
-<div class="wk_blogs_details"><span class="wk_blogs_date"><?=$wgLang->date($aRow['rev_timestamp'], true)?></span><span class="wk_blogs_author"><?=$sUserLinks?></span></div>
+				<div class="wk_blogs_details"><span class="wk_blogs_date"><?=$wgLang->date($aRow['rev_timestamp'], true)?></span><span class="wk_blogs_author"><?=$sUserLinks?></span></div>
 <?
 	}
 /* e: TIMESTAMP */
@@ -37,20 +38,20 @@ foreach ($aRows as $pageId => $aRow) {
 	if ( !empty($aOptions['summary']) ) {
 		global $wgBlankImgUrl;
 ?>
-<div class="wk_blogs_summary"><?= $aRow['text'] ?></div>
-<div class="wk_blogs_comments"><?php if (!empty($isCommenting)) : ?><?php $commentTitle = clone $oTitle; $commentTitle->setFragment("#comments"); ?><span class="blog-comment"><img src="<?php print $wgBlankImgUrl; ?>" class="sprite talk" /><?=$skin->link($commentTitle, wfMsg('blog-nbrcomments', intval($aRow['comments'])), array('rel' => 'nofollow'))?></span> | <?php endif ?><?=$skin->link($oTitle, wfMsg('blog-continuereading'), array('rel' => 'nofollow'))?></div>
+				<div class="wk_blogs_summary"><?= $aRow['text'] ?></div>
+				<div class="wk_blogs_comments"><?php if (!empty($isCommenting)) : ?><?php $commentTitle = clone $oTitle; $commentTitle->setFragment("#comments"); ?><span class="blog-comment"><img src="<?php print $wgBlankImgUrl; ?>" class="sprite talk" /><?=$skin->link($commentTitle, wfMsg('blog-nbrcomments', intval($aRow['comments'])), array('rel' => 'nofollow'))?></span> | <?php endif ?><?=$skin->link($oTitle, wfMsg('blog-continuereading'), array('rel' => 'nofollow'))?></div>
 <?
 	}
 /* e: SUMMARY */
 ?>
-</li>
+			</li>
 <?
 } /* foreach */
 ?>
-</ul>
+		</ul>
 <?
 } /* if (!empty($aRows)) */
 ?>
-</div>
+	</div>
 </div>
 <!-- e:<?= __FILE__ ?> -->
