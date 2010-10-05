992dc383da51c4ef25aec1e8cc5b76c413bbab31
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-05 00:53:24 +0000

    new themes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27721 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/templates/ThemeDesigner_Picker.php
+++ extensions/wikia/ThemeDesigner/templates/ThemeDesigner_Picker.php
@@ -12,7 +12,43 @@
 		<h1><?= wfMsg('themedesigner-pick-an-image') ?></h1>
 		<ul class="swatches">
 			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/pattern_dots_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/pattern_dots.png">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/pattern_flower_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/pattern_flower.png">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/pattern_steel_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/pattern_steel.png">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/pattern_straightdots_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/pattern_straightdots.png">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/opulence_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/opulence.png">
+			</li>
+			<li>
 				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/oasis_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/oasis.png">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/babygirl_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/babygirl.jpg">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/carbon_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/carbon.png">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/rockgarden_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/rockgarden.jpg">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/creamsicle_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/creamsicle.jpg">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/plated_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/plated.jpg">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/police_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/police.jpg">
+			</li>
+			<li>
+				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/aliencrate_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/aliencrate.jpg">
 			</li>
 			<li>
 				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/sapphire_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/sapphire.png">
@@ -24,19 +60,10 @@
 				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/moonlight_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/moonlight.jpg">
 			</li>
 			<li>
-				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/opulence_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/opulence.png">
-			</li>
-			<li>
-				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/carbon_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/carbon.png">
-			</li>
-			<li>
 				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/beach_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/beach.png">
 			</li>
 			<li>
 				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/dragstrip_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/dragstrip.jpg">
-			</li>
-			<li>
-				<img src="<?= $wgExtensionsPath ?>/wikia/ThemeDesigner/images/aliencrate_swatch.jpg" data-image="<?= $wgCdnStylePath ?>/skins/oasis/images/themes/aliencrate.jpg">
 			</li>
 			<li class="no-image">
 				<?= wfMsg('themedesigner-dont-use-a-background') ?>
