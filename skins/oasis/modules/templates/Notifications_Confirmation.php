a00c0f9cd5bfa13ca93c3f1dcbb6d8fc3a4bcb99
christian <christian@wikia-inc.com>
2010-10-06 18:43:47 +0000

    fixing RT 69907 and RT 69909
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27870 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/Notifications_Confirmation.php
+++ skins/oasis/modules/templates/Notifications_Confirmation.php
@@ -2,7 +2,7 @@
 	if ($confirmation != '') {
 ?>
 <div class="WikiaConfirmation<?= $confirmationClass ?>">
-	<p class="plainlinks"><img src="<?= $wgBlankImgUrl ?>" class="accept-icon"><?= $confirmation ?></p>
+	<p class="plainlinks"><img src="<?= $wgBlankImgUrl ?>" class="sprite ok"> <?= $confirmation ?></p>
 </div>
 <?php
 	}
