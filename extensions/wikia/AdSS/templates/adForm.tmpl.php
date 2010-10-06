1f3ed17b9545c33e6a6d37874444f8074715d974
emil <emil@wikia-inc.com>
2010-10-06 21:32:43 +0000

    Sponsor Links V2, the next iteration
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27883 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdSS/templates/adForm.tmpl.php
+++ extensions/wikia/AdSS/templates/adForm.tmpl.php
@@ -1,62 +1,96 @@
+<?php echo $adForm->error( 'wpType' ); ?>
 <section class="SponsoredLinkForm">
 	<form method="post" action="<?php echo $action; ?>">
 		<input name="wpToken" type="hidden" value="<?php echo $token; ?>" />
-		<input name="wpType" type="hidden" value="site" />
+		<input name="wpType" id="wpType" type="hidden" value="<?php echo $adForm->output( 'wpType' ); ?>" />
 
 		<fieldset>
-			<label for="wpWiki">Which Wiki would you like to advertise on?</label>
-			<input type="text" id="wpWiki" name="wpWiki" size="30" />
-		</fieldset>
-
-
-		<fieldset>
-			<legend>Pick a plan:</legend>
+			<legend><?php echo wfMsgHtml( 'adss-form-pick-plan' ); ?></legend>
 
 			<section class="box item-1">
-				<h3>Buy a share of the links across a wiki</h3>
-
-				<p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-				<p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-				<p>Lorem ipsum lorem ipsum lorem</p>
-
-				<input class="wikia-button" type="submit" name="wpSelect1" value="Select" />
+				<h3><?php echo wfMsgHtml( 'adss-form-site-plan-header' ); ?></h3>
+				<?php echo wfMsgWikiHtml( 'adss-form-site-plan-description' ); ?>
+				<?php echo wfMsgWikiHtml( 'adss-form-site-plan-price', $adForm->formatPrice( $sitePricing ) ); ?>
+				<input class="wikia-button" type="button" id="wpSelectSite" name="wpSelect" value="<?php echo wfMsgHtml( 'adss-button-select' ); ?>" />
 			</section>
 
 			<section class="box item-2">
-				<h3>Buy a link on an individual page</h3>
-
-				<p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-				<p>Lorem ipsum lorem ipsum lorem</p>
-
-				<input class="wikia-button" type="submit" name="wpSelect1" value="Select" />
+				<h3><?php echo wfMsgHtml( 'adss-form-page-plan-header' ); ?></h3>
+				<?php echo wfMsgWikiHtml( 'adss-form-page-plan-description' ); ?>
+				<?php echo wfMsgWikiHtml( 'adss-form-page-plan-price', $adForm->formatPrice( $pagePricing ) ); ?>
+				<input class="wikia-button" type="button" id="wpSelectPage" name="wpSelect" value="<?php echo wfMsgHtml( 'adss-button-select' ); ?>" />
 			</section>
 		</fieldset>
 
 		<fieldset class="box item-1">
-			<legend>Design your advert</legend>
+			<legend><?php echo wfMsgHtml( 'adss-form-header' ); ?></legend>
+
+			<div>
+			<label for="wpPage"><?php echo wfMsgHtml( 'adss-form-page' ); ?></label>
+			<?php echo $adForm->error( 'wpPage' ); ?>
+			<input type="text" name="wpPage" id="wpPage" value="<?php $adForm->output( 'wpPage' ); ?>" />
+			</div>
 
 			<label for="wpUrl"><?php echo wfMsgHtml( 'adss-form-url' ); ?></label>
-			<?php $ad->error( 'wpUrl' ); ?>http://<input type="text" name="wpUrl" size="30" value="<?php $ad->output( 'wpUrl' ); ?>" />
+			<?php echo $adForm->error( 'wpUrl' ); ?>
+			http:// <input type="text" name="wpUrl" id="wpUrl" value="<?php $adForm->output( 'wpUrl' ); ?>" />
 
 			<label for="wpText"><?php echo wfMsgHtml( 'adss-form-linktext' ); ?></label>
-			<?php $ad->error( 'wpText' ); ?><input type="text" name="wpText" size="30" value="<?php $ad->output( 'wpText' ); ?>" />
+			<?php echo $adForm->error( 'wpText' ); ?>
+			<input type="text" name="wpText" id="wpText" value="<?php $adForm->output( 'wpText' ); ?>" />
 
 			<label for="wpDesc"><?php echo wfMsgHtml( 'adss-form-additionaltext' ); ?></label>
-			<?php $ad->error( 'wpDesc' ); ?><textarea name="wpDesc" cols="30"><?php $ad->output( 'wpDesc' ); ?></textarea>
-
-			<label><?php echo wfMsgHtml( 'adss-form-price' ); ?></label>
-			<td id="wpPrice"><?php echo $ad->formatPrice( $priceConf ); ?>
+			<?php echo $adForm->error( 'wpDesc' ); ?>
+			<textarea name="wpDesc" id="wpDesc"><?php $adForm->output( 'wpDesc' ); ?></textarea>
 
 			<label for="wpEmail"><?php echo wfMsgHtml( 'adss-form-email' ); ?></label>
-			<?php $ad->error( 'wpEmail' ); ?><input type="text" name="wpEmail" value="<?php $ad->output( 'wpEmail' ); ?>" />
+			<?php echo $adForm->error( 'wpEmail' ); ?>
+			<input type="text" name="wpEmail" value="<?php $adForm->output( 'wpEmail' ); ?>" />
 
 			<input class="wikia-button" type="submit" name="wpSubmit" value="<?php echo $submit; ?>" />
 		</fieldset>
 
 		<fieldset class="box item-2">
-			<legend>Preview</legend>
-
-			<h2>External Sponsor Links</h2>
+			<legend><?php echo wfMsgHtml( 'adss-preview-header' ); ?></legend>
+			<?php echo wfMsgWikiHtml( 'adss-ad-header' ); ?>
+			<div class="sponsormsg">
+			<ul>
+			  <?php echo $ad->render(); ?>
+			</ul>
+			</div>
 		</fieldset>
 	</form>
 </section>
+
+<script type="text/javascript">/*<![CDATA[*/
+$(function() {
+	if( $("#wpType").val() == "page" ) {
+		$("section.item-1").css({opacity:0.5});
+	}
+	if( $("#wpType").val() == "site" ) {
+		$("section.item-2").css({opacity:0.5});
+		$("#wpPage").parent().hide();
+	}
+} );
+$("#wpSelectSite").click( function() {
+	$("#wpType").val("site");
+	$("section.item-1").animate({opacity:1});
+	$("section.item-2").animate({opacity:0.5});
+	$("#wpPage").parent().hide("slow");
+} );
+$("#wpSelectPage").click( function() {
+	$("#wpType").val("page");
+	$("section.item-1").animate({opacity:0.5});
+	$("section.item-2").animate({opacity:1});
+	$("#wpPage").parent().show("slow");
+} );
+$("#wpUrl").keyup( function() {
+	$("div.sponsormsg > ul > li > a").attr( "href", "http://"+$("#wpUrl").val() );
+} );
+$("#wpText").keyup( function() {
+	$("div.sponsormsg > ul > li > a").html( $("#wpText").val() );
+} );
+$("#wpDesc").keyup( function() {
+	$("div.sponsormsg > ul > li > p").html( $("#wpDesc").val() );
+} );
+</script>
