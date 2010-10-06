a26be66fc91a62e4c6ce5fddc1735830dc5ce957
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-06 04:57:12 +0000

    i18n message
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27794 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CorporatePage/CorporatePage.i18n.php
+++ extensions/wikia/CorporatePage/CorporatePage.i18n.php
@@ -8,6 +8,7 @@ $messages['en'] = array(
 	'corporatepage-hide-confirm' => "Are you sure ???",
 	'corporatepage-hide-error' =>  "There was some error !!!",
 	'corporatepage-hide-success' =>  "Okay, this will be removed.",
+	'corporatepage-staff-blog-header' => 'Recent Company Posts',
 	'corporatepage-facebook-link' => 'http://www.facebook.com/wikia',
 	'corporatepage-wikia-blog-link' => 'http://community.wikia.com/wiki/Blog:Wikia_Staff_Blog',
 	'corporatepage-twitter-link' => 'http://www.twitter.com/wikia'
