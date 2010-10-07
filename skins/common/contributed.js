82c63fe63c63047db5b58c03d34b823658b4dcf2
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-07 23:16:29 +0000

    importScriptPage fix (#68406)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27938 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/contributed.js
+++ skins/common/contributed.js
@@ -1,20 +1,4 @@
 // http://www.wikia.com/wiki/User:Dantman/global.js
 // RT#9031
 
-function importScriptPage( page, server ) {
-	var url = '/index.php?title=' + encodeURIComponent(page.replace(/ /g,'_')).replace('%2F','/').replace('%3A',':') + '&action=raw&ctype=text/javascript';
-	if( typeof server == "string" ) {
-		if( server.indexOf( '://' ) == -1 ) url = 'http://' + server + '.wikia.com' + url;
-		else url = server + url;
-	}
-	return importScriptURI(url);
-}
-
-function importStylesheetPage( page, server ) {
-	var url = '/index.php?title=' + encodeURIComponent(page.replace(/ /g,'_')).replace('%2F','/').replace('%3A',':') + '&action=raw&ctype=text/css';
-	if( typeof server == "string" ) {
-		if( server.indexOf( '://' ) == -1 ) url = 'http://' + server + '.wikia.com' + url;
-		else url = server + url;
-	}
-	return importStylesheetURI(url);
-}
+//moved these to wikibits, so they dont keep getting forgotten
