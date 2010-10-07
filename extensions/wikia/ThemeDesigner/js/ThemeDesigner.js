651d167ddde0c5f5a4df55fac66412303c389dd9
christian <christian@wikia-inc.com>
2010-10-07 22:13:35 +0000

    trimming user-submitted strings to prevent broken urls
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27935 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/js/ThemeDesigner.js
+++ extensions/wikia/ThemeDesigner/js/ThemeDesigner.js
@@ -240,7 +240,7 @@ var ThemeDesigner = {
 			$("#ColorNameForm").submit(function(event) {
 				event.preventDefault();
 
-				var color = $("#color-name").val()
+				var color = $.trim($("#color-name").val());
 
 				// was anything submitted?
 				if (color == "" || color == $("#color-name").attr("placeholder")) {
