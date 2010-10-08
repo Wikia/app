30a1455a52344cc397b9c7811d0d5f969f0554ec
hyun <hyun@wikia-inc.com>
2010-10-08 23:44:59 +0000

    RT:69912 Layout madness.  Wraps and formats itself as a single liner when overflowing.  Removed line breaks so there's no formatting markup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28005 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/PagesOnWiki_Index.php
+++ skins/oasis/modules/templates/PagesOnWiki_Index.php
@@ -4,6 +4,6 @@
 	<? } ?>
 	<?= View::specialPageLink('CreatePage', 'button-createpage', 'wikia-button createpage', 'blank.gif', 'oasis-create-page', 'sprite new'); ?>
 	<details class="tally">
-		<?= wfMsgExt('oasis-total-articles-mainpage', array( 'parsemag' ), $total ) ?>
+		<?= wfMsgExt('oasis-total-articles-mainpage', array( 'parsemag' ), $total, ($total < 100000 ? 'fixedwidth' : '') ) ?>
 	</details>
 </section>
