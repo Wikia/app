340046dcfaad9a9b73190c5e2903e7805cb593c5
macbre <macbre@wikia-inc.com>
2010-10-08 11:26:40 +0000

    Oasis: History dropdown -- don't list a time longer than 2 weeks ago (RT #70400)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@27963 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/PageHeader.js
+++ skins/oasis/js/PageHeader.js
@@ -14,14 +14,18 @@ var PageHeader = {
 		PageHeader.history
 			.one("mouseover", PageHeader.loadAvatars)
 			.hover(PageHeader.historyMouseover, PageHeader.historyMouseout);
-		$("abbr.timeago").timeago();
+
+		// RT #70400: show .. ago only for edits made 2 weeks ago or later
+		$("abbr.timeago").
+			data('maxdiff', 14 * 86400 * 1000).
+			timeago();
 	},
 
 	historyMouseover: function(event) {
-		
+
 		//stop the mouseout timer
 		clearTimeout(PageHeader.mouseoutTimer);
-		
+
 		//delay before showing menu
 		PageHeader.mouseoverTimer = setTimeout(function() {
 			$(event.currentTarget).addClass("hover");
@@ -29,18 +33,18 @@ var PageHeader = {
 			$.tracker.byStr('pageheader/history/open');
 		}, PageHeader.settings.mouseoverDelay);
 	},
-	
+
 	historyMouseout: function(event) {
-	
+
 		//stop the mouseover timer
 		clearTimeout(PageHeader.mouseoverTimer);
-		
+
 		//delay before hiding menu
 		PageHeader.mouseoutTimer = setTimeout(function() {
 			$(event.currentTarget).removeClass("hover");
 		}, PageHeader.settings.mouseoutDelay);
 	},
-	
+
 	loadAvatars: function() {
 		$(".avatar", PageHeader.history).each(function() {
 			$(this).attr("src", $(this).attr("data-realUrl"));
