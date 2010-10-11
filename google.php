a1908e46b3b19146a37bed36513ceb5ed144cb5a
eloy <eloy@wikia-inc.com>
2010-10-11 13:06:25 +0000

    switch off google veryfication, rt#71352
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28027 e846bd91-c042-42a7-a218-4b65e2c1904d

--- google.php
+++ google.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Cache-Control: no-cache");
+header("HTTP/1.0 404 Not Found");
+exit(0);
+
+/**
 if (preg_match("/^\/google([0-9a-f]{16}).html$/", $_SERVER["REQUEST_URI"], $matches)) {
 	header("Cache-Control: public, max-age=3600", true);
 	echo "google-site-verification: google{$matches[1]}.html";
@@ -7,3 +12,4 @@ if (preg_match("/^\/google([0-9a-f]{16}).html$/", $_SERVER["REQUEST_URI"], $matc
 	header("Cache-Control: no-cache");
 	header("HTTP/1.0 404 Not Found");
 }
+**/
