16bdd8fd05631f7af62b4c18f1890035aa0fee7f
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-11 14:05:08 +0000

    missing space between parameters
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28035 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/CommentsLikes_Index.php
+++ skins/oasis/modules/templates/CommentsLikes_Index.php
@@ -5,7 +5,7 @@
 <ul class="commentslikes">
 	<li class="comments">
 		<span class="commentsbubble"><?= $formattedComments ?></span>
-		<a href="<?= htmlspecialchars($commentsLink) ?>"data-id="comment" title="<?= htmlspecialchars($commentsTooltip) ?>"<?= $commentsAccesskey ?>><?= wfMsgExt($commentsEnabled ? 'oasis-page-header-comments' : 'oasis-page-header-talk', array('parsemag'), $comments) ?></a>
+		<a href="<?= htmlspecialchars($commentsLink) ?>" data-id="comment" title="<?= htmlspecialchars($commentsTooltip) ?>"<?= $commentsAccesskey ?>><?= wfMsgExt($commentsEnabled ? 'oasis-page-header-comments' : 'oasis-page-header-talk', array('parsemag'), $comments) ?></a>
 	</li>
 	<li class="likes">
 		<fb:like layout="button_count" width="50" colorscheme="<?= $likeTheme ?>" ref="<?= $likeRef ?>" href="<?= htmlspecialchars($likeHref) ?>"></fb:like>
