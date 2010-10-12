1077c6ad29f628ee7d28b40998bc44e1e5c232ff
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-12 13:56:42 +0000

    Oasis: remove not needed JS code from buttons.js
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28103 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/buttons.js
+++ skins/oasis/js/buttons.js
@@ -7,30 +7,30 @@ var WikiaButtons = {
 	settings: {
 		delay: 350
 	},
-	
+
 	init: function() {
 		//Variables
 		WikiaButtons.menuButtons = $(".wikia-menu-button");
 		WikiaButtons.timer = null;
-		
+
 		//Events
 		WikiaButtons.menuButtons
 			.one("mouseover", WikiaButtons.setup)
 			.hover(WikiaButtons.mouseover, WikiaButtons.mouseout)
 			.click(WikiaButtons.click);
 	},
-	
+
 	setup: function() {
 		//This function is run only once per button to detect arrow starting position and set menu min-width.
-		
+
 		var buttonWidth = $(this).outerWidth();
-		
+
 		//Store where the arrow begins (arrow section of menu buttons is 20px wide)
 		$(this).data("arrowStart", buttonWidth - 20);
-		
+
 		//Store where the button begins
 		$(this).data("buttonStart", $(this).offset().left);
-		
+
 		//Set min-width of menu, minus 4 for borders
 		$(this).find("ul").css("min-width", buttonWidth - 14);
 	},
@@ -41,17 +41,17 @@ var WikiaButtons = {
 			$(this).toggleClass("active");
 		} else {
 			//Mouse is not on arrow. Go to first anchor's href.
-			document.location = $(this).find("a").first().attr("href");
+			//document.location = $(this).find("a").first().attr("href");
 		}
 	},
-	
+
 	mouseover: function() {
-	
+
 		//Stop mouseout timer if running
 		clearTimeout(WikiaButtons.timer);
-		
+
 	},
-	
+
 	mouseout: function(event) {
 
 		//Delay before hiding menu
@@ -60,5 +60,5 @@ var WikiaButtons = {
 		}, WikiaButtons.settings.delay);
 
 	}
-	
+
 };
