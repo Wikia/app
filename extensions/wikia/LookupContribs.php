aecd34d6a6401089c73a9cebe76e246cd207ba5d
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-12 12:02:19 +0000

    new LookupContribs
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28099 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/LookupContribs.php
+++ extensions/wikia/LookupContribs.php
@@ -1,12 +1,8 @@
 <?php
-	define ('LOOKUPCONTRIBS_HELP','This special page can be used to display user\'s contributions on multiple wikias. For displaying the list, give username below.') ;
+define ('LOOKUPCONTRIBS_HELP','This special page can be used to display user\'s contributions on multiple wikias. For displaying the list, give username below.') ;
+define ('LOOKUPCONTRIBS_PATH', '/') ;
+/* cache? no cache? */
+define ('LOOKUPCONTRIBS_NO_CACHE', 'false') ;
 
-	define ('LOOKUPCONTRIBS_PATH', '/') ;
-
-	/* cache? no cache? */
-	define ('LOOKUPCONTRIBS_NO_CACHE', 'false') ;
-
-	require_once ($IP.LOOKUPCONTRIBS_PATH."extensions/wikia/LookupContribs/SpecialLookupContribs.php") ;
-	require_once ($IP.LOOKUPCONTRIBS_PATH."extensions/wikia/LookupContribs/LookupContribsCore.php") ;
-
-?>
+require_once ($IP.LOOKUPCONTRIBS_PATH."extensions/wikia/LookupContribs/SpecialLookupContribs.php") ;
+require_once ($IP.LOOKUPCONTRIBS_PATH."extensions/wikia/LookupContribs/SpecialLookupContribs_hooks.php") ;
