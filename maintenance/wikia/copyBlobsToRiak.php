2ec6c862ce9f06caf3284fb8428b77aee00216d5
eloy <eloy@wikia-inc.com>
2010-10-15 10:05:02 +0000

    preliminary version
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28301 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/copyBlobsToRiak.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @package MediaWiki
+ * @addtopackage maintenance
+ * @author eloy@wikia
+ *
+ * change user_options entry for skin from monaco to oasis
+ *
+ */
+
+$optionsWithArgs = array( 'page-from', 'page-to' );
+
+$dbr = wfGetDB( DB_SLAVE );
+$ext = new ExternalStoreRiak;
+
+
+$sth = $dbr->query( "SELECT * FROM revision r1 FORCE INDEX (PRIMARY), text t2 WHERE old_id = rev_text_id" );
+while( $row = $dbr->fetchObject( $sth ) ) {
+	/**
+	 * get only external revisions
+	 */
+	if( strpos( $row->old_flags, "external" ) !== false ) {
+		$text = ExternalStore::fetchFromURL( $row->old_text );
+	}
+	$key = sprintf( "%d:%d:%d", $wgCityId, $row->rev_page_id, $row->rev_id );
+	echo "Moving from db to riak with key $key\n";
+}
