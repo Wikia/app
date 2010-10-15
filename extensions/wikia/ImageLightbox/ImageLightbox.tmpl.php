b6fc881789153e043a310439f3eeec9ff5848b44
macbre <macbre@wikia-inc.com>
2010-10-15 07:45:53 +0000

    Oasis: revert r28230 (RT #72571)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28290 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageLightbox/ImageLightbox.tmpl.php
+++ extensions/wikia/ImageLightbox/ImageLightbox.tmpl.php
@@ -2,8 +2,8 @@
 	<div style="line-height: <?= $wrapperHeight ?>px">
 		<img alt="<?= htmlspecialchars($name) ?>" height="<?= $thumbHeight ?>" width="<?= $thumbWidth ?>" src="<?= $thumbUrl ?>" style="vertical-align: middle" />
 	</div>
-	<div id="lightbox-caption" class="neutral clearfix" style="line-height: 24px; padding: 8px">
+	<div id="lightbox-caption" class="neutral clearfix" style="padding: 8px">
 		<a id="lightbox-link" href="<?= htmlspecialchars($href) ?>" title="<?= wfMsg('lightbox_details_tooltip') ?>" style="float: right"><img class="sprite details" width="16" height="16" src="<?= $wgBlankImgUrl ?>" alt="" /></a>
-		<div id="lightbox-caption-content" style="margin-right: 25px; text-align: left"></div>
+		<div id="lightbox-caption-content" style="line-height: 18px; margin-right: 25px; text-align: left"></div>
 	</div>
 </div>
