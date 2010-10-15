4c591f4a2dc7d0517cc4f540a53d9cc8c95cbdb8
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-15 11:51:20 +0000

    * This is a special case of special page and name for it has to be defined in global messages to be available in MyTools
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28308 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaNewFiles/SpecialNewFiles.i18n.php
+++ extensions/wikia/WikiaNewFiles/SpecialNewFiles.i18n.php
@@ -9,7 +9,6 @@ $messages = array();
 
 $messages['en'] = array(
 	'wikianewfiles-desc'    => 'Extends a [[Special:NewFiles|special page]] to override some of the header formatting',
-	'wikianewfiles-title'   => 'New photos on this wiki',
 	'wikianewfiles-uploadby' => 'by $1',
 	'wikianewfiles-postedin' => 'Posted&nbsp;in',
 	'wikianewfiles-more'     => 'more...',
