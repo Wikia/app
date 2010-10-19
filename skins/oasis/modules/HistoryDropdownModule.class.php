7d0a5d5c8ecdec86458b2d9b8b850110c8ecd661
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-19 15:50:32 +0000

    Oasis: Move history dropdown to AJAX call (RT #74319)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28588 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/HistoryDropdownModule.class.php
+++ skins/oasis/modules/HistoryDropdownModule.class.php
@@ -25,4 +25,25 @@ class HistoryDropdownModule extends Module {
 
 		wfProfileOut(__METHOD__);
 	}
+
+	/**
+	 * Fetch history dropdown HTML for five previous edits (before the current revision)
+	 */
+	public function executePreviousEdits() {
+		global $wgTitle;
+
+		// use service to get data
+		$service = new PageStatsService($wgTitle->getArticleId());
+
+		$this->revisions = $service->getPreviousEdits();
+
+		if (is_array($this->revisions)) {
+			foreach($this->revisions as &$revision) {
+				if (isset($revision['user'])) {
+					$revision['avatarUrl'] = AvatarService::getAvatarUrl($revision['user']);
+					$revision['link'] = AvatarService::renderLink($revision['user']);
+				}
+			}
+		}
+	}
 }
