7ba87b3f73ef4fc6291d1f2fe353dfe9c04acb80
macbre <macbre@wikia-inc.com>
2010-10-19 12:41:21 +0000

    WikiHeaderModule: remove unused method
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28563 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/WikiHeaderModule.class.php
+++ skins/oasis/modules/WikiHeaderModule.class.php
@@ -18,37 +18,6 @@ class WikiHeaderModule extends Module {
 
 	var $wgSingleH1;
 
-	/**
-	 * @author: Inez Korczyński
-	 */
-	private function parseMonacoSidebarToOasisNavigation($text) {
-		$lines = explode("\n", $text);
-		$skip = true;
-		$firstDone = false;
-		$new_lines = array();
-
-		foreach($lines as $line) {
-			if($skip) {
-				if(trim($line) != '' && $line{0} == '*') {
-					$depth = strrpos($line, '*') + 1;
-					if($depth == 1) {
-						if(!$firstDone) {
-							$firstDone = true;
-						} else {
-							$skip = false;
-						}
-					}
-				}
-			}
-
-			if(!$skip) {
-				$new_lines[] = $line;
-			}
-		}
-
-		return join("\n", $new_lines);
-	}
-
 	public function executeIndex() {
 		global $wgOut, $wgCityId, $wgUser, $wgMemc;
 
