f47a0c379ba1ab9bef1f779909da907325d0d0ca
jakub <jakub@wikia-inc.com>
2010-10-19 09:42:13 +0000

    RelatedVideo
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28530 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ skins/oasis/modules/templates/RelatedVideo_Index.php
@@ -0,0 +1,8 @@
+<section class="WikiaRelatedVideoModule">
+	<h1><?=wfMsg('oasis-related-video-header'); ?></h1>
+	<div class="WikiaRelatedVideoContent">
+		<script type="text/javascript" src="http://pshared.5min.com/Scripts/ThumbSeed2.js?sid=203&amp;textLocation=1&amp;thumbnailSize=1&amp;width=246&amp;height=152&amp;headerTextColor=%23000000&amp;textFGColor=%23FFFFFF&amp;textFGColor_MO=%23FFFFFF&amp;textBGColor=%23000000&amp;textBGColor_MO=%23000000&amp;fallback=234&amp;categories=<?=$categories; ?>&amp;ht=_self&amp;displayHeader=false&amp;fallbackType=&amp;directURL=<?=$server; ?>/wiki/Special:RelatedVideo/[VIDEOID]#RelatedVideoTitle"></script>
+		<a class="more"><?=wfMsg('oasis-related-video-by-5min'); ?></a>
+	</div>
+</section>
+
