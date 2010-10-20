c6a1f421f6a8a948ebdbe38fa508013dc5be3db2
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-20 11:02:30 +0000

    * Use real spotlights from OpenX
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28694 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/Spotlights_Index.php
+++ skins/oasis/modules/templates/Spotlights_Index.php
@@ -35,13 +35,16 @@
 		<img src="<?= $wgBlankImgUrl ?>" class="banner-corner-left" height="0" width="0">
 		<img src="<?= $wgBlankImgUrl ?>" class="banner-corner-right chevron" height="0" width="0">
 	</header>
-	<ul>
-		<? for ($i = 0; $i < 3; $i++) {?>
-		<li>
-			<a href="#" class="highlight"><img height="95" width="95" src="/skins/oasis/images/placeholder1.png"></a>
-			<em>The Sims Wiki</em>
-			<p>Are you ready for the vamps in The Sims Late Night and such and such?</p>
-			<a href="#" class="seemore"><?= wfMsg('oasis-more') ?></a>
+	<ul<? if (!empty($adGroupName)) { ?> id="<?= $adGroupName ?>"<? if ($useLazyLoadAdClass) { ?> class="<?= AdEngine::lazyLoadAdClass ?><?= $mode == 'RAIL' ? ($wgEnableSpotlightsV2_Rail ? ' SPOTLIGHT_RAIL' : '') : ($wgEnableSpotlightsV2_Footer ? ' SPOTLIGHT_FOOTER' : '') ?>"<? } } ?>>
+		<?= AdEngine::getInstance()->getLazyLoadableAdGroup($adGroupName, $adslots) ?>
+		<? for ($i=0; $i<$n_adslots; $i++) { ?>
+		<li class="WikiaSpotlight item-<?= $i+1 ?>" id="<?= $adslots[$i]?>">
+			<?
+			if ( empty( $forceContent[$i] ) ) {
+				echo AdEngine::getInstance()->getAd( $adslots[$i] );
+			} else {
+				echo $forceContent[$i];
+			}  ?>
 		</li>
 		<? } ?>
 	</ul>
