1ad49fb1e60e5262a2a86c081145507e432df00f
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-20 11:08:35 +0000

    * UserProfilePages: TopWikis right rail module minor changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28695 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Blogs/Blogs.namespaces.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Blogs extension namespaces file
+ */
+
+$namespaces = array();
+
+$namespaces['en'] = array(
+	NS_BLOG_ARTICLE => 'User_blog',
+	NS_BLOG_ARTICLE_TALK => 'User_blog_comment',
+	NS_BLOG_LISTING => 'Blog',
+	NS_BLOG_LISTING_TALK => 'Blog_talk'
+);
+
+$namespaces['de'] = array(
+	NS_BLOG_ARTICLE => 'Benutzer_Blog',
+	NS_BLOG_ARTICLE_TALK => 'Benutzer_Blog_Kommentare',
+	NS_BLOG_LISTING => 'Blog',
+	NS_BLOG_LISTING_TALK => 'Blog_Diskussion'
+);
+
+$namespaces['ru'] = array(
+	NS_BLOG_ARTICLE => 'Блог_участника',
+	NS_BLOG_ARTICLE_TALK => 'Комментарий_блога_участника',
+	NS_BLOG_LISTING => 'Блог',
+	NS_BLOG_LISTING_TALK => 'Обсуждение_блога'
+);
+
+$namespaces['es'] = array(
+	NS_BLOG_ARTICLE => 'Usuario_Blog',
+	NS_BLOG_ARTICLE_TALK => 'Usuario_Blog_Comentario',
+	NS_BLOG_LISTING => 'Blog',
+	NS_BLOG_LISTING_TALK => 'Blog_Discusión'
+);
+
+$namespaces['no'] = array(
+	NS_BLOG_ARTICLE => 'Brukerblogg',
+	NS_BLOG_ARTICLE_TALK => 'Brukerbloggkommentar',
+	NS_BLOG_LISTING => 'Blogg',
+	NS_BLOG_LISTING_TALK => 'Bloggdiskusjon'
+);
+
+$namespaces['nn'] = array(
+	NS_BLOG_ARTICLE => 'Brukerblogg',
+	NS_BLOG_ARTICLE_TALK => 'Brukerbloggkommentar',
+	NS_BLOG_LISTING => 'Blogg',
+	NS_BLOG_LISTING_TALK => 'Bloggdiskusjon'
+);
+?>
