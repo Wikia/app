2c060810cb5fa2f6498cab98caadb3eb9d252a10
hyun <hyun@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-20 01:22:03 +0000

    Spotlights V2 work
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28662 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/Spotlights_Index.php
+++ skins/oasis/modules/templates/Spotlights_Index.php
@@ -1,4 +1,5 @@
 <? if (!$wgNoExternals) { ?>
+<? if ($mode == 'FOOTER') { ?>
 <section<? if (!empty($sectionId)) { ?> id="<?= $sectionId ?>"<? } ?>>
 	<header>
 		<? if ($wgSingleH1) { ?>
@@ -6,12 +7,7 @@
 		<? } else { ?>
 		<h1><?= wfMsg($titleMsg) ?></h1>
 		<? } ?>
-		<? if ($mode=='RAIL') { ?>
-		<img src="<?= $wgBlankImgUrl; ?>" class="banner-corner-left" height="0" width="0">
-		<img src="<?= $wgBlankImgUrl; ?>" class="banner-corner-right" height="0" width="0">
-		<? } elseif ($mode=='FOOTER') { ?>
 		<?= wfRenderModule('RandomWiki') ?>
-		<? } ?>
 	</header>
 	<ul<? if (!empty($adGroupName)) { ?> id="<?= $adGroupName ?>"<? if ($useLazyLoadAdClass) { ?> class="<?= AdEngine::lazyLoadAdClass ?><?= $mode == 'RAIL' ? ($wgEnableSpotlightsV2_Rail ? ' SPOTLIGHT_RAIL' : '') : ($wgEnableSpotlightsV2_Footer ? ' SPOTLIGHT_FOOTER' : '') ?>"<? } } ?>>
 		<?= AdEngine::getInstance()->getLazyLoadableAdGroup($adGroupName, $adslots) ?>
@@ -27,4 +23,29 @@
 		<? } ?>
 	</ul>
 </section>
-<? } ?>
+<? } else if ($mode == 'RAIL') { ?>
+
+<section <?= (!empty($sectionId) ? 'id="'.$sectionId.'"' : '') ?>>
+	<header>
+		<? if ($wgSingleH1) { ?>
+			<div class="headline-div"><?= wfMsg($titleMsg) ?><img class="sprite logo" src="<?= $wgBlankImgUrl ?>" height="15" width="61"></div>
+		<? } else { ?>
+			<h1><?= wfMsg($titleMsg) ?><img src="<?= $wgBlankImgUrl ?>" height="15" width="61" class="sprite logo"></h1>
+		<? } ?>
+		<img src="<?= $wgBlankImgUrl ?>" class="banner-corner-left" height="0" width="0">
+		<img src="<?= $wgBlankImgUrl ?>" class="banner-corner-right chevron" height="0" width="0">
+	</header>
+	<ul>
+		<? for ($i = 0; $i < 3; $i++) {?>
+		<li>
+			<a href="#" class="highlight"><img height="95" width="95" src="/skins/oasis/images/placeholder1.png"></a>
+			<em>The Sims Wiki</em>
+			<p>Are you ready for the vamps in The Sims Late Night and such and such?</p>
+			<a href="#" class="seemore"><?= wfMsg('oasis-more') ?></a>
+		</li>
+		<? } ?>
+	</ul>
+	<?= wfRenderModule('RandomWiki') ?>
+</section>
+
+<? } } /* end !wgNoExternals */ ?>
