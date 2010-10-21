87c581cf73caddad3584c636933788ac1000bcf2
macbre <macbre@wikia-inc.com>
2010-10-21 16:16:34 +0000

    Oasis: move link tags from $headscripts to $csslinks (fix for SMW)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28789 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/OasisModule.class.php
+++ skins/oasis/modules/OasisModule.class.php
@@ -314,6 +314,8 @@ EOF;
 		// macbre: this is minified version of /skins/monaco/js/WikiaScriptLoader.js using Google Closure
 		$this->wikiaScriptLoader = "\n\n\t" . '<script type="text/javascript">/*<![CDATA[*/var WikiaScriptLoader=function(){var b=navigator.userAgent.toLowerCase();this.useDOMInjection=b.indexOf("opera")!=-1||b.indexOf("firefox")!=-1;this.isIE=b.indexOf("opera")==-1&&b.indexOf("msie")!=-1;this.headNode=document.getElementsByTagName("HEAD")[0]}; WikiaScriptLoader.prototype={loadScript:function(b,c){this.useDOMInjection?this.loadScriptDOMInjection(b,c):this.loadScriptDocumentWrite(b,c)},loadScriptDOMInjection:function(b,c){var a=document.createElement("script");a.type="text/javascript";a.src=b;var d=function(){a.onloadDone=true;typeof c=="function"&&c()};a.onloadDone=false;a.onload=d;a.onreadystatechange=function(){a.readyState=="loaded"&&!a.onloadDone&&d()};this.headNode.appendChild(a)},loadScriptDocumentWrite:function(b,c){document.write(\'<script src="\'+ b+\'" type="text/javascript"><\/script>\');b=function(){typeof c=="function"&&c()};typeof c=="function"&&this.addHandler(window,"load",b)},loadScriptAjax:function(b,c){var a=this,d=this.getXHRObject();d.onreadystatechange=function(){if(d.readyState==4){var e=d.responseText;if(a.isIE)eval(e);else{var f=document.createElement("script");f.type="text/javascript";f.text=e;a.headNode.appendChild(f)}typeof c=="function"&&c()}};d.open("GET",b,true);d.send("")},loadCSS:function(b,c){var a=document.createElement("link"); a.rel="stylesheet";a.type="text/css";a.media=c||"";a.href=b;this.headNode.appendChild(a)},addHandler:function(b,c,a){if(window.addEventListener)window.addEventListener(c,a,false);else window.attachEvent&&window.attachEvent("on"+c,a)},getXHRObject:function(){var b=false;try{b=new XMLHttpRequest}catch(c){for(var a=["Msxml2.XMLHTTP.6.0","Msxml2.XMLHTTP.3.0","Msxml2.XMLHTTP","Microsoft.XMLHTTP"],d=a.length,e=0;e<d;e++){try{b=new ActiveXObject(a[e])}catch(f){continue}break}}return b}};window.wsl=new WikiaScriptLoader;/*]]>*/</script>';
 
+		wfProfileIn(__METHOD__ . '::regexp');
+
 		// get JS files from <script> tags returned by StaticChute
 		// TODO: get StaticChute package (and other JS files to be loaded) here
 		$jsReferences = array();
@@ -330,6 +332,15 @@ EOF;
 			$jsReferences[] = str_replace('&amp;', '&', $scriptSrc[1]);
 			$headscripts = str_replace($scriptSrc[0], '', $headscripts);
 		}
+
+		// move <link> tags from headscripts to csslinks (fix SMW issue)
+		preg_match_all("#<link ([^>]+)>#", $headscripts, $matches, PREG_SET_ORDER);
+		foreach($matches as $linkTag) {
+			$this->csslinks .= "\n\t" . trim($linkTag[0]);
+			$headscripts = str_replace($linkTag[0], '', $headscripts);
+		}
+
+		wfProfileOut(__METHOD__ . '::regexp');
 
 		// generate code to load JS files
 		$jsReferences = Wikia::json_encode($jsReferences);
