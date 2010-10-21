51aa246ec9b295752f002d5a3330e0d6a67b4b59
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-21 12:25:31 +0000

    EnableOasis: added prompt and force
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28773 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/EnableOasis.php
+++ maintenance/wikia/EnableOasis.php
@@ -16,6 +16,25 @@ include( '../commandLine.inc' );
 
 $list = file( $argv[0] );
 
+// exceptions taken from https://staff.wikia-inc.com/wiki/Oasis/Release_plan#D._Site_wide_launch_plan
+$exceptions = array(
+	'blind.wikia.com',
+	'necyklopedie.wikia.com',
+	'beidipedia.wikia.com',
+	'desencyclopedie.wikia.com',
+	'inciclopedia.wikia',
+	'nonsensopedia.wikia',
+	'absurdopedia.wikia.com',
+	'necyklopedia.wikia.com',
+	'zh.uncyclopedia.wikia',
+	'nonciclopedia.wikia',
+	'eincyclopedia.wikia',
+	'tolololpedia.wikia.com',
+	'neciklopedio.wikia',
+	'de.uncyclopedia.org',
+	'uncyclopedia.wikia.com',
+);
+
 if ( empty( $list ) ) {
 	echo "ERROR: List file empty or not readable. Please provide a line-by-line list of wikis.\n";
 	echo "USAGE: SERVER_ID=177 php EnableOasis.php /path/to/file --conf /usr/wikia/conf/current/wiki.factory/LocalSettings.php\n";
@@ -30,6 +49,23 @@ function sanitizeUrl( $url ) {
 	return $url;
 }
 
+function parseInput( $input ) {
+	$positive = array( 'y', 'Y', 'Yes', 'YES' );
+	$negative = array( 'n', 'no', 'No', 'NO' );
+
+	$input = trim( $input );
+
+	if ( in_array( $input, $positive ) ) {
+		return true;
+	}
+
+	if ( in_array( $input, $negative ) ) {
+		return false;
+	}
+
+	return null;
+}
+
 foreach ( $list as $wiki ) {
 	$wiki = sanitizeUrl( $wiki );
 
@@ -37,27 +73,59 @@ foreach ( $list as $wiki ) {
 	$id = WikiFactory::DomainToID( $wiki );
 
 	if ( empty( $id ) ) {
-		echo "ERROR: $wiki not found in WikiFactory!\n";
+		echo "$wiki: ERROR (not found in WikiFactory)\n";
 		continue;
 	}
 
 	// get URL, in case the one given is not the main one
-	$url = WikiFactory::getVarByName( 'wgServer', $id );
+	$oUrl = WikiFactory::getVarByName( 'wgServer', $id );
 
-	if ( empty( $url ) ) {
+	if ( empty( $oUrl ) ) {
 		// should never happen, but...
-		echo "ERROR: failed to get URL for ID $id; something's wrong.";
+		echo "$wiki: ERROR (failed to get URL for ID $id; something's wrong)";
 		continue;
 	}
 
-	$url = sanitizeUrl( $url );
+	$domain = sanitizeUrl( $oUrl->cv_value );
+
+	$currentSkin = WikiFactory::getVarByName( 'wgDefaultSkin', $id );
+
+	$input = '';
+
+	if ( isset( $options['force'] ) ) {
+		if ( in_array( $currentSkin, array( 'monobook', 'uncyclopedia', 'oasis' ) ) ) {
+			echo "$wiki: CAUTION! Current skin is $currentSkin!\n";
+		}
+
+		if ( in_array( $domain, $exceptions ) ) {
+			echo "$wiki: CAUTION! This wiki is listed as an exception in the Rallout Plan!\n";
+		}
+
+		$response = null;
+		// repeat until we get a valid response
+		while ( is_null( $response ) ) {
+			echo "$wiki: Are you sure you want to switch to Oasis? [yes/no] ";
+			$input = fgets( STDIN );
+			$response = praseInput( $input );
+		}
+
+		if ( !$response ) {
+			// user answered no
+			echo "$wiki: SKIPPING (because you said so)\n";
+			continue;
+		} else {
+			echo "$wiki: PROCEEDING\n";
+		}
+	}
 
 	WikiFactory::setVarByName( 'wgDefaultSkin', $id, 'oasis' );
 
 	WikiFactory::clearCache( $id );
 
 	// purge varnishes
-	exec( "pdsh -g all_varnish varnishadm -T :6082 'purge req.http.host == \"" . $url . "\"'" );
+	exec( "pdsh -g all_varnish varnishadm -T :6082 'purge req.http.host == \"" . $domain . "\"'" );
 
-	echo "PROCESSED WIKI $url with ID = $id\n";
+	echo "$wiki: PROCESSING COMPLETED\n";
+
+	echo "\n";
 }
