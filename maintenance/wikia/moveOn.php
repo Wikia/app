196a7c39b18763f66697ef5c1d86653a449a3ea3
moli <moli@wikia-inc.com>
2010-10-21 14:31:31 +0000

    task to move pages + maintance script
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28781 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/moveOn.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * Maintenance script to move a batch of pages
+ *
+ * @file
+ * @ingroup Maintenance
+ *
+ * USAGE: php moveOn.php [-u <user>] [-r <reason>] [-ot <old title>] [-on <old title namespace>] [-nt <new title>] [-nn <new title namespace>] [-redirect] [-watch]
+ *  -movetalk - move talk page if possible
+ *  -movesub  - move subpages
+ *  -redirect - leave a redirect behind
+ *  -watch - add page to the watchlist
+ */
+
+$oldCwd = getcwd();
+$optionsWithArgs = array( 'u', 'ot', 'on', 'nt', 'nn', 'redirect', 'watch', 'r' );
+
+require_once( 'commandLine.inc' );
+
+chdir( $oldCwd );
+
+# Options processing
+
+$filename = 'php://stdin';
+$user = 'Move page script';
+$oldtitle = $newtitle = $reason = '';
+$oldnamespace = $newnamespace = 0;
+$redirect = $watch = false;
+$interval = 0;
+
+if ( isset( $options['u'] ) )  $user = $options['u'];
+if ( isset( $options['r'] ) )  $reason = $options['r'];
+if ( isset( $options['ot'] ) ) $oldtitle = $options['ot'];
+if ( isset( $options['on'] ) ) $oldnamespace = $options['on'];
+if ( isset( $options['nt'] ) ) $newtitle = $options['nt'];
+if ( isset( $options['nn'] ) ) $newnamespace = $options['nn'];
+if ( isset( $options['redirect'] ) ) $redirect = true;
+if ( isset( $options['watch'] ) )    $watch = true;
+
+$wgUser = User::newFromName( $user );
+
+# Setup complete, now start
+
+$file = fopen( $filename, 'r' );
+if ( !$file ) {
+	print "Unable to read file, exiting\n";
+	exit;
+}
+
+$dbw = wfGetDB( DB_MASTER );
+
+# check titles
+$oOldFile = Title::newFromText( $oldtitle, $oldnamespace );
+$oNewFile = Title::newFromText( $newtitle, $newnamespace );
+if ( is_null( $oOldFile ) ) {
+	print "Invalid old title for: " . $oldtitle . " ns: " . $oldnamespace . " \n";
+	exit();
+}
+
+if ( is_null( $oNewFile ) ) {
+	print "Invalid new title for: " . $newtitle . " ns: " . $newnamespace . " \n";
+	exit();
+}
+
+# Check user rights
+$permErrors = $oOldTitle->getUserPermissionsErrors( 'move', $wgUser );
+if ( !empty( $permErrors ) ) {
+	print "User " . $wgUser->getName() . " doesn't have permission for move page: " . implode(", ", $permErrors) . "\n";
+	exit();
+}
+
+if ( $wgUser->isAllowed( 'suppressredirect' ) ) {
+	$createRedirect = $redirect;
+} else {
+	$createRedirect = true;
+}
+
+print "move: " . $oOldTitle->getPrefixedText() . " to " . $oNewTitle->getPrefixedText() . " \n";
+
+$dbw->begin();
+
+$err = $oOldTitle->moveTo( $oNewTitle, true, $reason, $createRedirect );
+if ( $err !== true ) {
+	$dbw->rollback();
+	$msg = array_shift( $err[0] );
+	print "\nMove failed: " . wfMsg( $msg, $err[0] );
+	exit;
+}
+
+$dbw->immediateCommit();
+
+print "Add/remove watching \n";
+
+if( $watch ) {
+	$wgUser->addWatch( $oOldTitle );
+	$wgUser->addWatch( $oNewTitle );
+} else {
+	$wgUser->removeWatch( $oOldTitle );
+	$wgUser->removeWatch( $oNewTitle );
+}
+
+print "\nDone\n\n";
+
+
+
+
