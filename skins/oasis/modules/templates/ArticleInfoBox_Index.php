56db3b30ef164ec9b48cacf3c269321f3ed605f0
bernhard <bernhard@wikia-inc.com>
2010-10-22 00:17:42 +0000

    infobox update
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28818 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/ArticleInfoBox_Index.php
+++ skins/oasis/modules/templates/ArticleInfoBox_Index.php
@@ -1,86 +1,80 @@
 <section class="InfoBox">
 	<table>
-		<thead>
-			<tr>
-				<th colspan="2">This is the header</th>
-			</tr>
-			
-			<tr>
-				<td class="infobox-image" colspan="2">
-					<img src="http://images.bernhard.wikia-dev.com/wikiaglobal/images/a/a9/Example.jpg" />
-				</td>
-			</tr>
-			
-			<tr>
-				<td class="infobox-caption" colspan="2">Title</td>
-			</tr>			
-			
-		</thead>
+		<tr>
+			<th class="header" colspan="2">This is the header</th>
+		</tr>
 		
-		<tbody>
-			<tr>
-				<th class="infobox-subhead" colspan="2">Subhead #1</th>
-			</tr>
-			
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>			
-		</tbody>	
+		<tr>
+			<td class="infobox-image" colspan="2">
+				<img src="http://images.bernhard.wikia-dev.com/wikiaglobal/images/a/a9/Example.jpg" />
+			</td>
+		</tr>
 		
-		<tbody>	
-			<tr>
-				<th class="infobox-subhead" colspan="2">Subhead #2</th>
-			</tr>
+		<tr>
+			<td class="infobox-caption" colspan="2">Title</td>
+		</tr>			
 			
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>
+		
+		<tr class="section">
+			<th class="infobox-subhead" colspan="2">Subhead #1</th>
+		</tr>
+		
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>				
+		
+		
+		<tr class="section">
+			<th class="infobox-subhead" colspan="2">Subhead #2</th>
+		</tr>
+		
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>
 
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>			
-			
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>
-		</tbody>
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>			
 		
-			<tbody>	
-			<tr>
-				<th class="infobox-subhead" colspan="2">Subhead #3 Info Info Info Info Info Info Info Info  Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info</th>
-			</tr>
-			
-			<tr>
-				<th>Title</th>
-				<td> Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info</td>
-			</tr>
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>
 
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>			
-			
-			<tr>
-				<th>Title</th>
-				<td>Info</td>
-			</tr>
-		</tbody>
+		
+		<tr class="section">
+			<th class="infobox-subhead" colspan="2">Subhead #3 Info Info Info Info Info Info Info Info  Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info</th>
+		</tr>
+		
+		<tr>
+			<th>Title</th>
+			<td> Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info Info</td>
+		</tr>
+
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>			
+		
+		<tr>
+			<th>Title</th>
+			<td>Info</td>
+		</tr>
 		
 	</table>
 </section>
