667aa41b4c6a52ab66e4f7a3a64c7babc47b9c68
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-26 18:44:30 +0000

    trunk: minor change to use isBlogPost() helper
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@28998 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/AdModule.class.php
+++ skins/oasis/modules/AdModule.class.php
@@ -79,7 +79,7 @@ class AdModule extends Module {
 					self::$config['PREFOOTER_LEFT_BOXAD'] = true;
 					self::$config['PREFOOTER_RIGHT_BOXAD'] = true;
 				}
-			} else if(  defined ( 'NS_BLOG_ARTICLE' ) && $namespace == NS_BLOG_ARTICLE) {
+			} else if( BodyModule::isBlogPost() ) {
 				self::$config['TOP_RIGHT_BOXAD'] = true;
 			}
 		}
