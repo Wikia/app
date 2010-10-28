51e118b671c40251ff4f20d12d412c48cc2d6cd1
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-28 18:44:36 +0000

    RT#68520 fixed Special:CreateBlogListing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29142 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/js/categoryCloud.js
+++ extensions/wikia/Blogs/js/categoryCloud.js
@@ -1,37 +1,37 @@
-YAHOO.namespace("Wikia.CreateBlogPost");
+/**
+ * Utility functions for managing a tag cloud of categories for the CreateBlogListing SpecialPage
+ */
 
-var YD = YAHOO.util.Dom;
-var YWC = YAHOO.Wikia.CreateBlogPost;
+var CreateBlogPost = {};
 
-YWC.foundCategories = [] ;
+CreateBlogPost.foundCategories = [] ;
 
-YWC.onclickCategoryFn = function (cat, id, categoryTextarea, cloudId) {
+CreateBlogPost.onclickCategoryFn = function (cat, id, categoryTextarea, cloudId) {
 	return function () {
-		cloudRemove (cloudId, categoryTextarea, escape(cat), id) ;
+		cloudToggle (cloudId, categoryTextarea, escape(cat), id) ;
 		return false ;
 	}
 }
 
-YWC.CheckCategoryCloud = function (cloudId, categoryTextarea, categoryCloudSection) {
-	var cat_textarea = YD.get (categoryTextarea) ;
+CreateBlogPost.CheckCategoryCloud = function (cloudId, categoryTextarea, categoryCloudSection) {
+	var cat_textarea = $(categoryTextarea);
 	if (!cat_textarea) {
 		return ;
 	}
 
-	var cat_full_section = YD.get (categoryCloudSection) ;
+	var cat_full_section = $(categoryCloudSection) ;
 
-	var cloud_num = (cat_full_section.childNodes.length - 1) / 2 ;
+	var cloud_num = cat_full_section.children().length ;
 	var n_cat_count = cloud_num ;
 	var text_categories = new Array () ;
 	for (i=0;i<cloud_num;i++) {
-		var cloud_id = 'cloud' + cloudId + '_' + i ;
-		var found_category = YD.get (cloud_id).innerHTML ;
+		var cloud_id = '#cloud' + cloudId + '_' + i ;
+		var found_category = $(cloud_id).text() ;
 		if (found_category) {
-			YWC.foundCategories[i] = found_category ;
+			CreateBlogPost.foundCategories[i] = found_category ;
 		}
 	}
-
-	var categories = cat_textarea.value ;
+	var categories = cat_textarea.val() ;
 	if ('' == categories) {
 		return ;
 	}
@@ -43,27 +43,23 @@ YWC.CheckCategoryCloud = function (cloudId, categoryTextarea, categoryCloudSecti
 
 	for (i=0; i<text_categories.length;i++) {
 		var c_found = false ;
-		for (j in YWC.foundCategories) {
+		for (j in CreateBlogPost.foundCategories) {
 			var core_cat = text_categories[i].replace (/\|.*/,'') ;
-			if (YWC.foundCategories[j] == core_cat) {
-				this_button = YD.get ('cloud' + cloudId + '_' + j) ;
-				var actual_cloud = YWC.foundCategories[j] ;
-				var cl_num = j ;
-
-				this_button.onclick = YWC.onclickCategoryFn (text_categories[i],j,categoryTextarea,cloudId) ;
-				this_button.style.color = "#419636" ;
+			if (CreateBlogPost.foundCategories[j] == core_cat) {
+				this_button = $('#cloud' + cloudId + '_' + j) ;
+				this_button.css('color', "#419636") ;
 				c_found = true ;
 				break ;
 			}
 		}
-		if (!c_found) {
+		if (!c_found && core_cat != '') {
 			var n_cat = document.createElement ('a') ;
 			var s_cat = document.createElement ('span') ;
 			n_cat_count++ ;
 			var cat_num = n_cat_count - 1 ;
 			n_cat.setAttribute ('id','cloud' + cloudId + '_' + cat_num) ;
 			n_cat.setAttribute ('href','#') ;
-			n_cat.onclick = YWC.onclickCategoryFn (text_categories[i], cat_num, categoryTextarea, cloudId) ;
+			n_cat.onclick = CreateBlogPost.onclickCategoryFn(text_categories[i], cat_num, categoryTextarea, cloudId) ;
 			n_cat.style.color = '#419636' ;
 			n_cat.style.fontSize = '10pt' ;
 			s_cat.setAttribute ('id','tag' + cloudId + '_' + n_cat_count) ;
@@ -72,47 +68,30 @@ YWC.CheckCategoryCloud = function (cloudId, categoryTextarea, categoryCloudSecti
 			n_cat.appendChild (t_cat) ;
 			s_cat.appendChild (n_cat) ;
 			s_cat.appendChild (space) ;
-			cat_full_section.appendChild (s_cat) ;
+			cat_full_section.append (s_cat) ;
 		}
 	}
 }
 
-function cloudAdd(cloudId, categoryTextarea, category, num) {
-	category_text = YD.get (categoryTextarea) ;
-	if (category_text.value == '') {
-		category_text.value += unescape (category) ;
-	} else {
-		category_text.value += '|' + unescape (category) ;
-	}
-	this_button = document.getElementById('cloud' + cloudId + '_' + num);
-	this_button.onclick = function() {
-		eval("cloudRemove('" + cloudId + "', '" + categoryTextarea + "', '" + category + "'," + num + ")");
-		return false;
-	}
-	this_button.style["color"] = "#419636";
-	return false;
-};
 
 function cloudInputAdd (cloudId, categoryTextarea, categoryCloudSection, categoryInput) {
-	category_input = YD.get (categoryInput) ;
-	category_text = YD.get (categoryTextarea) ;
-	var category = 	category_input.value ;
-	if ('' != category_input.value) {
-		if (category_text.value == '') {
-			category_text.value += unescape (category) ;
+	var category_text = $(categoryTextarea).val() ;
+	var category = 	$(categoryInput).val() ;
+
+	if ('' != category) {
+		if ($(categoryTextarea).val() == '') {
+			$(categoryTextarea).val( category_text += unescape (category) );
 		} else {
-			category_text.value += '|' + unescape (category) ;
+			$(categoryTextarea).val( category_text += '|' + unescape (category)) ;
 		}
-		category_input.value = '' ;
+		$(categoryInput).val('') ;
 		var c_found = false ;
 		var core_cat = category.replace (/\|.*/,'') ;
-		for (j in YWC.foundCategories) {
-			if (YWC.foundCategories[j] == core_cat) {
-				this_button = YD.get ('cloud' + cloudId + '_' + j) ;
-				var actual_cloud = YWC.foundCategories[j] ;
-				var cl_num = j ;
-				this_button.onclick = YWC.onclickCategoryFn (core_cat,j,categoryTextarea,cloudId) ;
-				this_button.style.color = "#419636" ;
+		for (j in CreateBlogPost.foundCategories) {
+
+			if (CreateBlogPost.foundCategories[j] == core_cat) {
+				this_button = $('#cloud' + cloudId + '_' + j) ;
+				this_button.css ('color' , "#419636");
 				c_found = true ;
 				break ;
 			}
@@ -120,13 +99,12 @@ function cloudInputAdd (cloudId, categoryTextarea, categoryCloudSection, categor
 		if (!c_found) {
 			var n_cat = document.createElement ('a') ;
 			var s_cat = document.createElement ('span') ;
-			n_cat_count = YWC.foundCategories.length ;
+			n_cat_count = CreateBlogPost.foundCategories.length ;
 
-			var cat_full_section = YD.get (categoryCloudSection) ;
 			var cat_num = n_cat_count ;
 			n_cat.setAttribute ('id','cloud' + cloudId + '_' + cat_num) ;
 			n_cat.setAttribute ('href','#') ;
-			n_cat.onclick = YWC.onclickCategoryFn (core_cat, cat_num, categoryTextarea, cloudId) ;
+			n_cat.onclick = CreateBlogPost.onclickCategoryFn(core_cat, cat_num, categoryTextarea, cloudId) ;
 			n_cat.style.color = '#419636' ;
 			n_cat.style.fontSize = '10pt' ;
 			s_cat.setAttribute ('id','tag' + cloudId + '_' + cat_num) ;
@@ -135,29 +113,35 @@ function cloudInputAdd (cloudId, categoryTextarea, categoryCloudSection, categor
 			n_cat.appendChild (t_cat) ;
 			s_cat.appendChild (n_cat) ;
 			s_cat.appendChild (space) ;
-			cat_full_section.appendChild (s_cat) ;
-			YWC.foundCategories [n_cat_count] = core_cat  ;
+			$(categoryCloudSection).append (s_cat) ;
+			CreateBlogPost.foundCategories [n_cat_count] = core_cat ;
 		}
 	}
 }
 
-function cloudRemove(cloudId, categoryTextarea, category, num) {
-	category_text = YD.get (categoryTextarea) ;
-	this_pos = category_text.value.indexOf (unescape (category)) ;
+function cloudToggle(cloudId, categoryTextarea, category, num) {
+	var category_text = $(categoryTextarea).val() ;
+	this_button = $('#cloud' + cloudId + '_' + num);
+
+	// if cloud is already selected, unselect it
+	this_pos = category_text.indexOf (unescape (category)) ;
 	if (this_pos != -1) {
-		category_text.value = category_text.value.substr(0, this_pos-1) + category_text.value.substr(this_pos + unescape (category).length);
+		$(categoryTextarea).val( category_text.substr(0, this_pos-1) +category_text.substr(this_pos + unescape (category).length) );
+		this_button.css ("color", "");
+	} else {
+		// select the cloud tag
+		if (category_text == '') {
+			$(categoryTextarea).val( category_text += unescape (category)) ;
+		} else {
+			$(categoryTextarea).val( category_text += '|' + unescape (category)) ;
+		}
+		this_button.css ("color", "#419636");
 	}
-	this_button = document.getElementById('cloud' + cloudId + '_' + num);
-	this_button.onclick = function() {
-		eval("cloudAdd('" + cloudId + "', '" + categoryTextarea + "', '" + category + "'," + num + ")");
-		return false
-	};
-	this_button.style["color"] = "";
 	return false;
 };
 
 function cloudBuild(o) {
-	var categories = o.value;
+	var categories = o.val();
 	new_text = '';
 	categories = categories.split("|");
 	for (i=0; i < categories.length; i++) {
--- extensions/wikia/Blogs/templates/createPostCategoryCloud.tmpl.php
+++ extensions/wikia/Blogs/templates/createPostCategoryCloud.tmpl.php
@@ -6,19 +6,21 @@
 	<?php if (!empty($cloud->tags)) : ?>
 		<?php $xnum = 0; foreach( $cloud->tags as $xname => $xtag): ?>
 			<span id="tag<?=$cloudNo;?>_<?=$xnum?>" style="font-size:<?=$xtag['size']?>pt">
-			<a href="#" id="cloud<?=$cloudNo;?>_<?=$xnum?>" onclick="cloudAdd(<?=$cloudNo;?>, 'wpCategoryTextarea<?=$cloudNo;?>', escape ('<?=$xname?>'), <?=$xnum?>); return false;"><?=$xname?></a>
+			<a href="#" id="cloud<?=$cloudNo;?>_<?=$xnum?>" onclick="cloudToggle(<?=$cloudNo;?>, '#wpCategoryTextarea<?=$cloudNo;?>', escape ('<?=$xname?>'), <?=$xnum?>); return false;"><?=$xname?></a>
 			</span>
 		<?php $xnum++; endforeach; ?>
 	<?php endif ?>	
 	</div>
 	<textarea accesskey="," name="wpCategoryTextarea<?=$cloudNo;?>" id="wpCategoryTextarea<?=$cloudNo;?>" class="wpCategoryTextarea" rows='3' cols='<?=$cols?>'><?=$textCategories?></textarea>
 	<?=wfMsg('create-blog-categories-textinput')?>&nbsp;<input type="text" name="wpCategoryInput<?=$cloudNo;?>" class="wpCategoryInput" id="wpCategoryInput<?=$cloudNo;?>" value="" />
-	<input type="button" name="wpCategoryButton" id="wpCategoryButton" class="button color1" value="<?=wfMsg('create-blog-categories-submit')?>" onclick="cloudInputAdd(<?=$cloudNo;?>, 'wpCategoryTextarea<?=$cloudNo;?>', 'categoryCloudSection<?=$cloudNo;?>', 'wpCategoryInput<?=$cloudNo;?>'); return false ;" />
+	<input type="button" name="wpCategoryButton" id="wpCategoryButton" class="button color1" value="<?=wfMsg('create-blog-categories-submit')?>" onclick="cloudInputAdd(<?=$cloudNo;?>, '#wpCategoryTextarea<?=$cloudNo;?>', '#categoryCloudSection<?=$cloudNo;?>', '#wpCategoryInput<?=$cloudNo;?>'); return false ;" />
 </div>
 
 <script type="text/javascript">
 /*<![CDATA[*/
-	YWC.CheckCategoryCloud(<?=$cloudNo;?>, 'wpCategoryTextarea<?=$cloudNo;?>', 'categoryCloudSection<?=$cloudNo;?>');
+	$(document).ready( function () {
+		CreateBlogPost.CheckCategoryCloud(<?=$cloudNo;?>, '#wpCategoryTextarea<?=$cloudNo;?>', '#categoryCloudSection<?=$cloudNo;?>');
+	});
 /*]]>*/
 </script>
 <!-- e:<?= __FILE__ ?> -->
