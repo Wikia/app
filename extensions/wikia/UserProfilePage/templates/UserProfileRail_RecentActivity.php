787eb2ad7a115c844c02a707416568aeb9d9953c
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-10-28 10:26:07 +0000

    * UserProfilePages: small improvements
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29102 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserProfilePage/templates/UserProfileRail_RecentActivity.php
+++ extensions/wikia/UserProfilePage/templates/UserProfileRail_RecentActivity.php
@@ -1,19 +1,25 @@
 <section class="UserProfileRailModule_RecentActivity WikiaActivityModule">
 	<h1 class="activity-heading"><?= wfMsg( 'userprofilepage-recent-activity-title', $userName, $wikiName ) ;?></h1>
+	<ul class="activity_feed">
+		<?if ( count($activityFeed) ) :?>
+
+				<? foreach( $activityFeed as $row ) :?>
+					<li>
+						<img src="<?= wfBlankImgUrl() ?>" class="sprite <?= $row['changeicon'] ?>" height="20" width="20">
+						<em><?= $row['changemessage'] ?></em>
+						<details><?= $row['time_ago'] ?></details>
+					</li>
+				<? endforeach ;?>
+		<? else :?>
+			<li>
+				<img src="<?= wfBlankImgUrl() ?>" class="sprite ok" height="20" width="20">
+				<em><?= wfMsg( 'userprofilepage-recent-activity-default', $userName ) ;?></em>
+				<details><?= $userRegistrationDate ?></details>
+			</li>
+		<? endif ;?>
+	</ul>
+
 	<?if ( count($activityFeed) ) :?>
-		<ul class="activity_feed">
-			<? foreach( $activityFeed as $row ) :?>
-				<li>
-					<img src="<?= wfBlankImgUrl() ?>" class="sprite <?= $row['changeicon'] ?>" height="20" width="20">
-					<em><?= $row['changemessage'] ?></em>
-					<details><?= $row['time_ago'] ?></details>
-				</li>
-			<? endforeach ;?>
-		</ul>
-
 		<a class="more view-all" href="<?= $specialContribsLink ;?>"><?= wfMsg( 'userprofilepage-top-recent-activity-see-more' ); ?> &gt;</a>
-
-	<? else :?>
-		<?= wfMsg( 'userprofilepage-recent-activity-default', $userName ) ;?>
 	<? endif ;?>
 </section>
