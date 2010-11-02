1b4100a781a8514fa9b5ce50c6b6e7cf579e8227
marooned <marooned@wikia-inc.com>
2010-11-02 16:43:16 +0000

    generic script to set option(s) for user(s)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29296 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/setUserOptions.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * setUserOptions
+ * generic script to set option(s) for user(s)
+ *
+ * @package MediaWiki
+ * @subpackage Maintanance
+ *
+ * @author: Marooned <marooned at wikia-inc.com>
+ *
+ */
+
+//variables
+$limitUsers = '';	//WHERE in SQL
+$preferences = array('adoptionmails' => '1');	//pairs key=value
+///
+
+ini_set('include_path', dirname(__FILE__) . '/..' );
+require_once('commandLine.inc');
+
+if (isset($options['help'])) {
+	die( "Usage: php setUserOptions.php [--quiet] [--force]
+
+		  --help     you are reading it right now
+		  --force    overwrite existing option
+		  --quiet    do not print anything to output\n\n");
+}
+
+$dbr = WikiFactory::db(DB_SLAVE);
+$res = $dbr->select(
+	'user',
+	'user_id',
+	$limitUsers,
+	__METHOD__
+);
+
+$force = isset($options['force']);
+$quiet = isset($options['quiet']);
+$count = 0;
+
+while ($row = $dbr->fetchObject($res)) {
+	$user = User::newFromId($row->user_id);
+	if ($user) {
+		$optionsChanged = 0;
+		foreach ($preferences as $key => $val) {
+			if ($user->getOption($key, null) === null || $force) {
+				$user->setOption($key, $val);
+				$optionsChanged++;
+				if (!$quiet) {
+					echo "Setting preference '$key' to '$val' for user {$user->getName()} (id:{$user->getId()}).\n";
+				}
+			}
+		}
+		if ($optionsChanged) {
+			$user->saveSettings();
+			$user->invalidateCache();
+			$count++;
+		}
+	}
+}
+
+if (!$quiet) {
+	echo "Updated options for $count users.\n";
+}
