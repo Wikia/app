5be948bcfa66d9e2a26bae5f44f4cc27a0f28f44
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-11-03 11:07:43 +0000

    * UserProfilePage: Js/SCSS fixes and optimizations
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29332 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserProfilePage/js/UserProfilePage.js
+++ extensions/wikia/UserProfilePage/js/UserProfilePage.js
@@ -5,18 +5,29 @@ $(function() {
 var UserProfilePage = {
 	_topWikisWrapper: null,
 	_topPagesWrapper: null,
+	_hiddenWikisWrapper: null,
+	_hiddenTopPagesWrapper: null,
 	_aboutSection: null,
 	
 	_init: function() {
 		UserProfilePage._topWikisWrapper = $('#profile-top-wikis-body');
 		UserProfilePage._topPagesWrapper = $('#profile-top-pages-body');
 		UserProfilePage._aboutSection = $('#profile-content').find('.user-about-section');
+		UserProfilePage._hiddenWikisWrapper = $('#profile-top-wikis-hidden');
+		UserProfilePage._hiddenTopPagesWrapper = $('#profile-top-pages-hidden');
 		UserProfilePage.attachEvents();
 		UserProfilePage.enrichAboutSection();
 		$().log(UserProfilePage._aboutSection);
 	},
 	
 	doAction: function(name, type, value) {
+		$().log({
+			'action': 'ajax',
+			'rs': 'UserProfilePageHelper::doAction',
+			'name': name,
+			'type': type,
+			'value': value
+		});
 		$.getJSON(wgScript,
 				{
 					'action': 'ajax',
@@ -26,6 +37,7 @@ var UserProfilePage = {
 					'value': value
 				},
 				function(response) {
+					$().log(response);
 					if(response.result === true) {
 						if( response.type == 'page' ) {
 							UserProfilePage._topPagesWrapper.replaceWith( response.html );
@@ -41,32 +53,47 @@ var UserProfilePage = {
 	},
 
 	attachEvents: function() {
+		//handle click outside hidden elements popup menu
 		$('#profile-top-wikis-hidden').find('a.more').click(function(){
-			if(!$('#profile-top-wikis-hidden').hasClass('user-profile-box')){
-				$('#profile-top-wikis-hidden').addClass('user-profile-box');
+			if(!UserProfilePage._hiddenWikisWrapper.hasClass('user-profile-box')){
+				UserProfilePage._hiddenWikisWrapper.addClass('user-profile-box');
 
 				$('body').unbind('click.UserProfilePage_boxClose').bind('click.boxClose', function(event) {
 					if (!$(event.target).closest('#profile-top-wikis-hidden').length) {
-						$('#profile-top-wikis-hidden').removeClass('user-profile-box');
+						UserProfilePage._hiddenWikisWrapper.removeClass('user-profile-box');
 					};
 				});
 			}
 		});
 
+		//handle click outside hidden elements popup menu
 		$('#profile-top-pages-hidden').find('a.more').click(function(){
-			if(!$('#profile-top-pages-hidden').hasClass('user-profile-box')){
-				$('#profile-top-pages-hidden').addClass('user-profile-box');
+			if(!UserProfilePage._hiddenTopPagesWrapper.hasClass('user-profile-box')){
+				UserProfilePage._hiddenTopPagesWrapper.addClass('user-profile-box');
 
 				$('body').unbind('click.UserProfilePage_boxClose').bind('click.boxClose', function(event) {
 					if (!$(event.target).closest('#profile-top-pages-hidden').length) {
-						$('#profile-top-pages-hidden').removeClass('user-profile-box');
+						UserProfilePage._hiddenTopPagesWrapper.removeClass('user-profile-box');
 					};
 				});
 			}
 		});
 
-		/*UserProfilePage._topPagesWrapper.find('.HideButton').bind('click', function() {UserProfilePage.doAction( 'hide', 'page', this.title );} );
-		UserProfilePage._topPagesWrapper.find('.UnhideButton').bind('click', function() {UserProfilePage.doAction( 'unhide', 'page', this.title );} );*/
+		UserProfilePage._topWikisWrapper.find('.HideButton').bind('click', function(){
+			UserProfilePage.doAction('hide', 'wiki', $(this).attr('data-id' ));
+		});
+
+		UserProfilePage._topWikisWrapper.find('.UnhideButton').bind('click', function(){
+			UserProfilePage.doAction('unhide', 'wiki', $(this).attr('data-id' ));
+		});
+
+		UserProfilePage._topPagesWrapper.find('.HideButton').bind('click', function(){
+			UserProfilePage.doAction('hide', 'page', $(this).attr('data-id' ));
+		});
+
+		UserProfilePage._topPagesWrapper.find('.UnhideButton').bind('click', function(){
+			UserProfilePage.doAction('unhide', 'page', $(this).attr('data-id' ));
+		});
 	},
 
 	enrichAboutSection: function(){
@@ -74,8 +101,6 @@ var UserProfilePage = {
 		
 		section.find('.answer,.question').each(function(){
 			var elm = $(this);
-
-			//elm.html($('<img src="' + section.attr('data-' + ((index % 2 != 0) ? 'generic' : 'user') + '-avatar') + '"><div class="' + ((index % 2 != 0) ? 'answer' : 'question') + '">' + elm.html() + '</div>'));
 			elm.parent().prepend($('<img src="' + section.attr('data-' + ((elm.hasClass('question')) ? 'generic' : 'user') + '-avatar') + '">'));
 		});
 	}
