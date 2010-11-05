ae8e91939c23976feced6171b24e54f03324eac4
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-11-05 12:17:40 +0000

    ech...
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29517 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdSS/templates/admin/reports.tmpl.php
+++ extensions/wikia/AdSS/templates/admin/reports.tmpl.php
@@ -1,4 +1,4 @@
-<img style="float:right" width="<?php echo $w; ?>" height="<?php echo $h; ?>" src="http://chart.apis.google.com/chart?chxt=x,y&chbh=a&chs=<?php echo $w; ?>x<?php echo $h; ?>&cht=bvg&chco=A2C180&chd=t:<?php echo $d; ?>&chds=0,$maxY&chm=N*cUSD0*,000000,0,-1,12&chxr=1,0,<?php echo $maxY; ?>&chxl=0:|<?php echo $xl0; ?>|&chtt=Daily+Income+(last+7+days)" />
+<img style="float:right" width="<?php echo $w; ?>" height="<?php echo $h; ?>" src="http://chart.apis.google.com/chart?chxt=x,y&chbh=a&chs=<?php echo $w; ?>x<?php echo $h; ?>&cht=bvg&chco=A2C180&chd=t:<?php echo $d; ?>&chds=0,<?php echo $maxY; ?>&chm=N*cUSD0*,000000,0,-1,12&chxr=1,0,<?php echo $maxY; ?>&chxl=0:|<?php echo $xl0; ?>|&chtt=Daily+Income+(last+7+days)" />
 <table border="1" class="TablePager">
   <thead>
     <tr><th>User</th><th>Balance</th><th>Last billed</th></tr>
