5e3fc39af2401dc33d7a91c74d8caad7c5e4ea77
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-11-07 23:10:52 +0000

    i18n/L10n tweaks.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29561 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ContentFeeds/ContentFeeds.i18n.php
+++ extensions/wikia/ContentFeeds/ContentFeeds.i18n.php
@@ -1,13 +1,14 @@
 <?php
+
 $messages = array();
 
 $messages['en'] = array(
-	'contentfeeds-desc' => 'Provides a rich and up to date information through various tags or "feeds"',
-	'contentfeeds-newimages-rss-title' => 'RSS Feed for Special:Newimages',
+	'contentfeeds-desc' => 'Provides rich and up to date information through various tags or "feeds"',
+	'contentfeeds-newimages-rss-title' => 'RSS Feed for {{#Special:NewFiles}}',
 	'contentfeeds-newimages-rss-desc' => 'Feed from $1',
-	'contentfeeds-firstfewarticles-tag-header' => 'Create your first few articles!',
+	'contentfeeds-firstfewarticles-tag-header' => 'Create your first few pages!',
 	'contentfeeds-firstfewarticles-tag-title' => 'Title',
 	'contentfeeds-firstfewarticles-tag-next' => 'Next',
-	'contentfeeds-firstfewarticles-tag-empty-title-error' => 'Error: Article title is required',
-	'contentfeeds-firstfewarticles-tag-empty-body-error' => 'Error: Article body is required'
+	'contentfeeds-firstfewarticles-tag-empty-title-error' => 'Error: Page title is required',
+	'contentfeeds-firstfewarticles-tag-empty-body-error' => 'Error: Page body is required'
 );
