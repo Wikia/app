887f4a3069ee5163934e24036cfd49eb0f53e972
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-11-08 21:30:00 +0000

    Localise special page names.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29624 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialSignup/Signup.i18n.php
+++ extensions/wikia/SpecialSignup/Signup.i18n.php
@@ -3,6 +3,6 @@
 $messages = array();
 
 $messages['en'] = array(
-	'signup-desc' => 'Adds alias [[Special:Signup]] for [[Special:UserLogin/signup]]',
+	'signup-desc' => 'Adds alias [[{{#Special:Signup}}]] for [[{{#Special:UserLogin}}/signup]]',
 	'signup' => 'Signup',
 );
