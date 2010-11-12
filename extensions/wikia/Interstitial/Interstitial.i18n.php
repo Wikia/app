890f0be2aef42a1747d04876fae9c27b68a2a038
bernhard <bernhard@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-11-12 21:42:57 +0000

    adding exitstital
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29776 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Interstitial/Interstitial.i18n.php
+++ extensions/wikia/Interstitial/Interstitial.i18n.php
@@ -12,12 +12,17 @@ $messages['en'] = array(
 	// If we were displaying interstitials but there is no campaign code, this would be an egregious error.
 	// An extremely friendly message is probably much better than a blank interstitial.  At least we get to tell them
 	// how we feel for X seconds.
-	"interstitial-default-campaign-code"                         => "Wikia Loves You!",
-	"interstitial-skip-ad"                                       => "Skip this ad",
+	"interstitial-default-campaign-code"		=> "Wikia Loves You!",
+	"interstitial-skip-ad"						=> "Skip this ad",
 
-	"interstitial-already-logged-in-no-link"                     => "You are already logged in and there is no destination set.",
-	"interstitial-disabled-no-link"                              => "There is no destination set and interstitials are not enabled on this wiki.",
-	"interstitial-link-away"                                     => "There is nothing to see here!<br/><br/>Would you like to go to the [[Main Page]] or perhaps a [[Special:Random|random page]]?",
+	"interstitial-already-logged-in-no-link"	=> "You are already logged in and there is no destination set.",
+	"interstitial-disabled-no-link"				=> "There is no destination set and interstitials are not enabled on this wiki.",
+	"interstitial-link-away"					=> "There is nothing to see here!<br/><br/>Would you like to go to the [[Main Page]] or perhaps a [[Special:Random|random page]]?",
+	
+	// oasis Exitstitial
+	"exitstitial-title" => "Leaving ",
+	"exitstitial-register" => "Register or Login to skip ads.",
+	"exitstitial-button" => "Skip This Ad"
 );
 
 ?>
