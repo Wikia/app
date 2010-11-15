966b478da01bf557bad51f809f088146707aa4f2
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-11-15 17:04:35 +0000

    transform origname before comparing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29837 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Our404Handler/404handler.pl
+++ extensions/wikia/Our404Handler/404handler.pl
@@ -328,6 +328,7 @@ while( $request->Accept() >= 0 || $test ) {
 		# create false positives (it's not perfect though )
 		#
 		my $origname = pop @{ [ split( "/" , $original ) ] };
+		$origname =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg if $use_http;
 		if( index( $last, $origname ) == -1 ) {
 			$last_status = 404;
 			say STDERR "$origname not found in $last" if $debug;
