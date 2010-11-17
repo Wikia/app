c7ecb4da0a8eade956c0023e128d0eb7d09d6314
jakub <jakub@wikia-inc.com>
2010-11-17 11:57:05 +0000

    SponsorshipDashboard
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@29953 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SponsorshipDashboard/SponsorshipDashboard.i18n.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Internationalisation file for SponsorshipDashboard extension.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/**
+ * Prepare extension messages
+ *
+ * @return array
+ */
+$messages['en'] = array(
+	'serie-1' => 'Total registered editors in current month (all namespaces)',
+	'serie-2' => 'Total registered editors in current month (content namespaces)',
+	'serie-3' => 'Number of registered editors who edited less than 5 times in current month (content namespaces)',
+	'serie-4' => 'Number of registered editors who edited more than 5 times in current month (content namespaces)',
+	'serie-5' => ' Number of registered editors who edited more than 100 times in current month (content namespaces)',
+	'serie-6' => 'Number of all content namespaces articles',
+	'serie-7' => 'New articles per day in current month',
+	'serie-8' => 'Number of edits content namespaces articles',
+	'serie-9' => 'Total number of links to images in content namespaces articles',
+	'serie-10' => 'Total number of uploaded images',
+	'serie-11' => 'Total number of embeded video on content namespaces articles',
+	'serie-12' => 'Total number of uploaded videos',
+	'not-allowed' => "Sorry. You don't have permission to view this page ",
+	'pv-ranking' => "ranked by pageviews:",
+	'show' => 'Show:',
+	'hub-position' => '<strong>$1</strong> on <strong>$2</strong> hub',
+    	'no-hub-position' => 'No data about page position - sorry',
+
+);
+
