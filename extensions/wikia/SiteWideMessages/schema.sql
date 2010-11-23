4b68a0ab89a81e10c38c4f3f5faa9c4c88f1598a
tor <tor@wikia-inc.com>
2010-11-23 15:36:39 +0000

    fixed comments in SWM schema
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@30229 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SiteWideMessages/schema.sql
+++ extensions/wikia/SiteWideMessages/schema.sql
@@ -1,4 +1,4 @@
-# Message textdata
+-- Message textdata
 CREATE TABLE IF NOT EXISTS `messages_text`
 (
 	`msg_id`              int (7)  unsigned   NOT NULL    auto_increment,
@@ -8,24 +8,24 @@ CREATE TABLE IF NOT EXISTS `messages_text`
 	`msg_removed`         tinyint             NOT NULL    default 0,
 	`msg_expire`          datetime,
 	`msg_date`            timestamp           NOT NULL    default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-	`msg_recipient_name`  varchar (255),				#only for displaying in the history
-	`msg_group_name`      varchar (255),				#only for displaying in the history
-	`msg_wiki_name`       varchar (255),				#only for displaying in the history
+	`msg_recipient_name`  varchar (255), -- only for displaying in the history
+	`msg_group_name`      varchar (255), -- only for displaying in the history
+	`msg_wiki_name`       varchar (255), -- only for displaying in the history
 	`msg_hub_id`          int (9),
 	`msg_lang`            varchar (255),
 	PRIMARY KEY (`msg_id`)
 );
-#msg_mode: 0 = all users, 1 = selected users
+-- msg_mode: 0 = all users, 1 = selected users
 
-# Messages metadata
+-- Messages metadata
 CREATE TABLE IF NOT EXISTS `messages_status`
 (
 	`msg_wiki_id`      int (9),
-	`msg_recipient_id` int (10) unsigned,				#NULL -> message to ALL users
+	`msg_recipient_id` int (10) unsigned, -- NULL -> message to ALL users
 	`msg_id`           int (7)  unsigned   NOT NULL,
 	`msg_status`       tinyint             NOT NULL,
 	`msg_date`         timestamp           NOT NULL    default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
 	PRIMARY KEY (`msg_recipient_id`, `msg_id`),
 	KEY `msg_id` (`msg_id`)
 );
-#msg_status: 0 = unseen, 1 = seen, 2 = dismissed
+-- msg_status: 0 = unseen, 1 = seen, 2 = dismissed
