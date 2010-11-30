1956befa2e63690d2f75e3340e167a39647697bc
sean <sean@wikia-inc.com>
2010-11-30 23:00:41 +0000

    Re-wrote SpecialDevCookie to be a bit more generic and handle other cookies.  Started with just adding the Staging cookie.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@30415 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialDevCookie/SpecialDevCookie.php
+++ extensions/wikia/SpecialDevCookie/SpecialDevCookie.php
@@ -1,32 +1,34 @@
 <?php
 
 /**
- * switch cookie used for development
+ * Switch cookie used for development and/or staging servers.
  */
 
 $wgSpecialPages['DevCookie'] = 'SpecialDevCookie';
 $wgDevCookieName = "-wikia-development";
+$wgStagingCookieName = "-wikia-staging-1";
 $wgGroupPermissions['*']['devcookie'] = false;
 $wgGroupPermissions['staff']['devcookie'] = true;
 
 class SpecialDevCookie extends UnlistedSpecialPage {
 
-	private
-		$mTitle,
-		$mCookie,
-		$mCookieName;
+	private	$mTitle;
+	private $mCookieValues;
 
 	public function __construct() {
-		global $wgDevCookieName;
+		global $wgDevCookieName, $wgStagingCookieName;
 
 		parent::__construct( 'DevCookie', 'devcookie' );
-
-		$this->mCookieName = $wgDevCookieName;
-		$this->mCookie = null;
+		
+		$this->mCookieValues = array(
+			$wgDevCookieName => null,
+			$wgStagingCookieName => null
+		);
 	}
 
 	public function execute( $subpage ) {
-		global $wgOut, $wgUser, $wgRequest, $wgMessageCache, $wgCookiePrefix;
+		global $wgOut, $wgUser, $wgMessageCache;
+		global $wgDevCookieName, $wgStagingCookieName;
 
 		$wgMessageCache->addMessages( array( 'devcookie' => 'DevCookie' ), 'en' );
 
@@ -39,46 +41,85 @@ class SpecialDevCookie extends UnlistedSpecialPage {
 			$this->displayRestrictionError();
 			return;
 		}
+		
+		// Output some style since these classes aren't styled in css: {successbox, errorbox}
+		$wgOut->addHTML("<style type='text/css'>
+		.successbox{
+			padding:5px;
+			border: 1px #080 solid;
+			background-color: #afa;
+			color: #000;
+		}
+		.errorbox{
+			padding:5px;
+			border: 1px #800 solid;
+			background-color: #faa;
+			color: #000;
+		}
+		</style>");
 
+		// Dev cookie
+		$wgOut->addHTML('<h1>Dev cookie</h1>');
+		$this->handlePostForCookie($wgDevCookieName);
+		$this->showSelectBox($wgDevCookieName);
+		
+		$wgOut->addHTML('<br/><br/>');
+
+		// Staging cookie
+		$wgOut->addHTML('<h1>Staging cookie</h1>');
+		$wgOut->addHTML('<p>To use the staging server (code that will be released to production soon), set this cookie to <em>true</em></p>');
+		$this->handlePostForCookie($wgStagingCookieName);
+		$this->showSelectBox($wgStagingCookieName);
+
+		wfProfileOut( __METHOD__ );
+	}
+	
+	/**
+	 * Given a cookie name, handles any posts to change its value, and displays
+	 * the current value of the cookie.
+	 */
+	private function handlePostForCookie($cookieName){
 		/**
 		 * if posted change cookie value
 		 */
-		if( $wgRequest->wasPosted() ) {
+		 global $wgRequest, $wgOut, $wgCookiePrefix;
+		if( $wgRequest->wasPosted() && ($cookieName == $wgRequest->getVal( 'cookieName' )) ) {
 			$val = $wgRequest->getVal( 'cookie', 0 );
 			if( $val == 1 ) {
 				$wgOut->addHTML( Wikia::successbox( 'cookie set' ) );
-				$wgRequest->response()->setcookie( $this->mCookieName, '1' );
-				$this->mCookie = '1';
+				$wgRequest->response()->setcookie( $cookieName, '1' );
+				$this->mCookieValues[$cookieName] = '1';
 			}
 			elseif( $val == 0 ) {
 				$wgOut->addHTML( Wikia::successbox( 'cookie set' ) );
-				$wgRequest->response()->setcookie( $this->mCookieName, '0' );
-				$this->mCookie = '0';
+				$wgRequest->response()->setcookie( $cookieName, '0' );
+				$this->mCookieValues[$cookieName] = '0';
 			}
 			else {
 				$wgOut->addHTML( Wikia::successbox( 'cookie removed' ) );
-				$wgRequest->response()->setcookie( $this->mCookieName, '-1', 1 );
-				$this->mCookie = null;
+				$wgRequest->response()->setcookie( $cookieName, '-1', 1 );
+				$this->mCookieValues[$cookieName] = null;
 			}
-
-		}
-		else {
-			/**
-			 * show current value of cookie
-			 */
-			if ( isset( $_COOKIE[ $wgCookiePrefix . $this->mCookieName ] ) ) {
-				$this->mCookie = $_COOKIE[ $wgCookiePrefix . $this->mCookieName ];
+		} else {
+			// get current value of cookie
+			if ( isset( $_COOKIE[ $wgCookiePrefix . $cookieName ] ) ) {
+				$this->mCookieValues[$cookieName] = $_COOKIE[ $wgCookiePrefix . $cookieName ];
 			}
 		}
 		$wgOut->addHTML(
 			"Current value of the dev cookie: <em>" .
-			( is_null($this->mCookie) ? 'not set' : ($this->mCookie ? 'true' : 'false') ) . '</em>'
+			( is_null($this->mCookieValues[$cookieName]) ? 'not set' : ($this->mCookieValues[$cookieName] ? 'true' : 'false') ) . '</em>'
 		);
+	} // end handlePostForCookie()
 
-		/**
-		 * show input chooser
-		 */
+	/**
+	 * Given a cookie-name, show a select box for changing the value of that cookie (currently
+	 * only supports 1/0 cookies w/the option of being removed completely).
+	 */
+	private function showSelectBox($cookieName){
+		global $wgOut;
 		$wgOut->addHTML( Xml::openElement( 'form', array( 'action' => $this->mTitle->getFullURL(), 'method' => 'post' ) ) );
+		$wgOut->addHTML( Xml::hidden( 'cookieName' , $cookieName ) );
 		$select = new XMLSelect( 'cookie', 'cookie' );
 		$select->addOption( 'Set cookie to true', 1 );
 		//$select->addOption( 'Set cookie to false', 0 );
@@ -86,7 +127,5 @@ class SpecialDevCookie extends UnlistedSpecialPage {
 		$wgOut->addHTML( $select->getHTML() );
 		$wgOut->addHTML( Xml::submitButton( 'submit' ) );
 		$wgOut->addHTML( Xml::closeElement( 'form' ) );
-
-		wfProfileOut( __METHOD__ );
 	}
 }
