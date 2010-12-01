b2ee6bbfb1de8896deaf0b276a83b78d66e94168
emil <emil@wikia-inc.com>
2010-12-01 17:22:53 +0000

    adss: print due accounts in red
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@30451 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdSS/templates/admin/reports.tmpl.php
+++ extensions/wikia/AdSS/templates/admin/reports.tmpl.php
@@ -1,3 +1,4 @@
+<?php global $wgAdSSBillingThreshold; ?>
 <img style="float:right" width="<?php echo $w; ?>" height="<?php echo $h; ?>" src="http://chart.apis.google.com/chart?chxt=x,y&chbh=a&chs=<?php echo $w; ?>x<?php echo $h; ?>&cht=bvg&chco=A2C180&chd=t:<?php echo $d; ?>&chds=0,<?php echo $maxY; ?>&chm=N*cUSD0*,000000,0,-1,12&chxr=1,0,<?php echo $maxY; ?>&chxl=0:|<?php echo $xl0; ?>|&chtt=Daily+Income+(last+7+days)" />
 <table border="1" class="TablePager">
   <thead>
@@ -5,7 +6,19 @@
   </thead>
   <tbody>
     <?php foreach( $lines as $l ): ?>
-    <tr><td><?php echo $l['user']; ?></td><td style="text-align: right"><?php echo $l['amount']; ?> USD</td><td><?php echo $l['lastBilled']; ?></td></tr>
+    <tr>
+      <td><?php echo $l['user']; ?></td>
+      <?php if( $l['amount'] > $wgAdSSBillingThreshold ): ?>
+      <td style="text-align: right; color: red;"><?php echo $l['amount']; ?> USD</td>
+      <?php else: ?>
+      <td style="text-align: right"><?php echo $l['amount']; ?> USD</td>
+      <?php endif; ?>
+      <?php if( strtotime( '+1 month', wfTimestampOrNull( TS_UNIX, $l['lastBilled'] )) < time() ): ?>
+      <td style="color: red;"><?php echo $l['lastBilled']; ?></td>
+      <?php else: ?>
+      <td><?php echo $l['lastBilled']; ?></td>
+      <?php endif; ?>
+    </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
