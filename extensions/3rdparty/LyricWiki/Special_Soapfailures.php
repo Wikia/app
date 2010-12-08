b4c8107bfaf9aeb94c466b7e2ef0d112e55f46c3
sean <sean@wikia-inc.com>
2010-12-08 00:03:27 +0000

    RT#133899: Updated Special:Soapfailures to have a "Fixed" button and to memcache just the data, not the output.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@30896 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/LyricWiki/Special_Soapfailures.php
+++ extensions/3rdparty/LyricWiki/Special_Soapfailures.php
@@ -51,7 +51,10 @@ function wfSoapFailures(){
 	global $wgRequest, $wgUser, $wgMemc;
 	
 	$MAX_RESULTS = 100;
-	$CACHE_KEY = "LW_SOAP_FAILURES";
+	$CACHE_KEY_PREFIX = "LW_SOAP_FAILURES";
+	$CACHE_KEY_DATA = wfMemcKey($CACHE_KEY_PREFIX, "data");
+	$CACHE_KEY_TIME = wfMemcKey($CACHE_KEY_PREFIX, "cachedOn");
+	$CACHE_KEY_STATS = wfMemcKey($CACHE_KEY_PREFIX, "stats");
 
 	wfLoadExtensionMessages('SpecialSoapFailures');
 	$wgOut->setPageTitle(wfMsg('soapfailures'));
@@ -107,7 +110,9 @@ function wfSoapFailures(){
 			}
 			print "<br/>Clearing the cache... ";
 			
-			$wgMemc->delete($CACHE_KEY); // purge the entry from memcached
+			$wgMemc->delete($CACHE_KEY_DATA); // purge the entry from memcached
+			$wgMemc->delete($CACHE_KEY_TIME);
+			$wgMemc->delete($CACHE_KEY_STATS);
 
 			print "<div style='background-color:#cfc'>The song was retrieved successfully and ";
 			print "was removed from the failed requests list.";
@@ -117,78 +122,72 @@ function wfSoapFailures(){
 		print "<br/>Back to <a href='$wgScriptPath/Special:Soapfailures'>SOAP Failures</a>\n";
 		exit; // wiki system throws database-connection errors if the page is allowed to display itself.
 	} else {
-		$msg = "";
+		$wgOut->addHTML("<style type='text/css'>
+			table.soapfailures{
+				border-collapse:collapse;
+			}
+			.soapfailures tr.odd{background-color:#eef}
+			.soapfailures td, .soapfailures th{
+				border:1px solid;
+				cell-padding:0px;
+				cell-spacing:0px;
+				vertical-align:top;
+				padding:5px;
+			}</style>\n");
 
 		// Allow the cache to be manually cleared.
+		$msg = "";
 		if(isset($_GET['cache']) && $_GET['cache']=="clear"){
 			$msg.= "Forced clearing of the cache...\n";
-			$wgMemc->delete($CACHE_KEY); // purge the entry from memcached
+			$wgMemc->delete($CACHE_KEY_DATA); // purge the entry from memcached
+			$wgMemc->delete($CACHE_KEY_TIME);
+			$wgMemc->delete($CACHE_KEY_STATS);
 			unset($_GET['cache']);
 			$_SERVER['REQUEST_URI'] = str_replace("?cache=clear", "", $_SERVER['REQUEST_URI']);
 			$_SERVER['REQUEST_URI'] = str_replace("&cache=clear", "", $_SERVER['REQUEST_URI']);
 		}
 
-		$content = $wgMemc->get($CACHE_KEY);
-		if(!$content){
-			ob_start();
+		$msg = ($msg==""?"":"<pre>$msg</pre>");
+		$wgOut->addWikiText($msg);
+		
+		// Form for clearing a fixed song.
+		$wgOut->addHTML(wfMsg('soapfailures-mark-as-fixed') . "
+						<form method='post'>
+							".wfMsg('soapfailures-artist')." <input type='text' name='artist'/><br/>
+							".wfMsg('soapfailures-song')." <input type='text' name='song'/><br/>
+							<input type='submit' name='fixed' value='".wfMsg('soapfailures-fixed')."'/>
+						</form><br/>");
 
+		$data = $wgMemc->get($CACHE_KEY_DATA);
+		$cachedOn = $wgMemc->get($CACHE_KEY_TIME);
+		$statsHtml = $wgMemc->get($CACHE_KEY_STATS);
+		if(!$data){
 			$db = &wfGetDB(DB_SLAVE)->getProperty('mConn');
-
-			print wfMsg('soapfailures-intro');
-
-			print "This page is cached every 2 hours - \n";
-			print "last cached: <strong>".date('m/d/Y \a\t g:ia')."</strong>\n";
 			$queryString = "SELECT * FROM lw_soap_failures ORDER BY numRequests DESC LIMIT $MAX_RESULTS";
-			$totFailures = 0;
 			if($result = mysql_query($queryString,$db)){
+				$data = array();
 				if(($numRows = mysql_num_rows($result)) && ($numRows > 0)){
-					print "<table class='soapfailures'>\n";
-					print "<tr><th nowrap='nowrap'>Requests</th><th>Artist</th><th>Song</th><th>Titles looked for</th><th>Fixed</th></tr>\n";
-					$REQUEST_URI = $_SERVER['REQUEST_URI'];
 					for($cnt=0; $cnt<$numRows; $cnt++){
-						$artist = mysql_result($result, $cnt, "request_artist");
-						$song = mysql_result($result, $cnt, "request_song");
-						$numRequests = mysql_result($result, $cnt, "numRequests");
-						$lookedFor = mysql_result($result, $cnt, "lookedFor");
-						$lookedFor = formatLookedFor($lookedFor);
-						$totFailures += $numRequests;
-						print utf8_encode("<tr".((($cnt%2)!=0)?" class='odd'":"")."><td>$numRequests</td><td>[[$artist]]</td><td>[[$artist:$song|$song]]");
-						$delim = "&amp;";
-						$prefix = "";
-
-						// If the short-url is in the REQUEST_URI, make sure to add the index.php?title= prefix to it.
-						if(strpos($REQUEST_URI, "index.php?title=") === false){
-							$prefix = "/index.php?title=";
-							
-							// If we're adding the index.php ourselves, but the request still started with a slash, remove it because that would break the request if it came after the "title="
-							if(substr($REQUEST_URI,0,1) == "/"){
-								$REQUEST_URI = substr($REQUEST_URI, 1);
-							}
-						}
-						print "</td>";
-						print "<td>$lookedFor</td>";
-						print "<td>";
-						// TODO: NEED TO REWRITE THIS CODE TO PUT JUST THE DATA IN MEMCACHED AND THEN TO USE wgOut->addWikiText OR wgOut->addHTML PER LINE.
-						/*
-							print "<form action='' method='POST'>\n";
-								print "<input type='hidden' name='artist' value='$artist'/>\n";
-								print "<input type='hidden' name='song' value='$song'/>\n";
-								print "<input type='submit' name='fixed' value='".wfMsg('soapfailures-fixed')."'/>\n";
-							print "</form>\n";
-						*/
-						print "</td>";
-						print "</tr>\n";
+						$row = array();
+						$row['artist'] = mysql_result($result, $cnt, "request_artist");
+						$row['song'] = mysql_result($result, $cnt, "request_song");
+						$row['numRequests'] = mysql_result($result, $cnt, "numRequests");
+						$row['lookedFor'] = mysql_result($result, $cnt, "lookedFor");
+						$row['lookedFor'] = formatLookedFor($row['lookedFor']);
 					}
-					print "</table>\n";
-					print "<br/>Total of <strong>$totFailures</strong> requests in the top $MAX_RESULTS.  This number will increase slightly over time, but we should fight to keep it as low as possible!";
-				} else {
-					print "<em>No results found.</em>\n";
+					$data[] = $row;
 				}
 			} else {
-				print "<br/><br/><strong>Error: with query</strong><br/><em>$queryString</em><br/><strong>Error message: </strong>".mysql_error($db);
+				$wgOut->addHTML("<br/><br/><strong>Error: with query</strong><br/><em>$queryString</em><br/><strong>Error message: </strong>".mysql_error($db));
 			}
 
+			$cachedOn = date('m/d/Y \a\t g:ia');
+		}
+
+		// Stats HTML is just an unimportant feature, hackily storing HTML instead of the data - FIXME: It's BAD to cache output rather than data.
+		if(!$statsHtml){
 			// Display some hit-rate stats.
+			ob_start();
 			include "soap_stats.php"; // for tracking success/failure
 			print "<br/><br/><br/><table border='1px' cellpadding='5px'>\n";
 			print "\t<tr><th>".wfMsg('soapfailures-stats-timeperiod')."</th><th>".wfMsg('soapfailures-stats-numfound')."</th><th>".wfMsg('soapfailures-stats-numnotfound')."</th><th>&nbsp;</th></tr>\n";
@@ -202,33 +201,71 @@ function wfSoapFailures(){
 			$stats = lw_soapStats_getStats(LW_TERM_MONTHLY);
 			print "\t<tr><td>".wfMsg('soapfailures-stats-period-thismonth')."</td><td>{$stats[LW_API_FOUND]}</td><td>{$stats[LW_API_NOT_FOUND]}</td><td>{$stats[LW_API_PERCENT_FOUND]}%</td></tr>\n";
 			print "</table>\n";
-
-			$content = ob_get_clean();
-			$wgMemc->set($CACHE_KEY, $content, strtotime("+2 hour"));
+			$statsHtml = ob_get_clean();
 		}
-		$msg = ($msg==""?"":"<pre>$msg</pre>");
-		$wgOut->addHTML("<style type='text/css'>
-			table.soapfailures{
-				border-collapse:collapse;
-			}
-			.soapfailures tr.odd{background-color:#eef}
-			.soapfailures td, .soapfailures th{
-				border:1px solid;
-				cell-padding:0px;
-				cell-spacing:0px;
-				vertical-align:top;
-				padding:5px;
-			}</style>\n");
-		
-		// Form for clearing a fixed song.
-		$wgOut->addHTML(wfMsg('soapfailures-mark-as-fixed') . "
-						<form method='post'>
-							".wfMsg('soapfailures-artist')." <input type='text' name='artist'/><br/>
-							".wfMsg('soapfailures-song')." <input type='text' name='song'/><br/>
-							<input type='submit' name='fixed' value='".wfMsg('soapfailures-fixed')."'/>
-						</form><br/>");
 
-		$wgOut->addWikiText("$msg$content");
+		if($data){
+			$wgOut->addWikiText(wfMsg('soapfailures-intro'));
+
+			$wgOut->addHTML("This page is cached every 2 hours - \n"); // TODO: i18n
+			$wgOut->addHTML("last cached: <strong>$cachedOn</strong>\n"); // TODO: i18n
+			$totFailures = 0;
+			if(!empty($data)){
+				$wgOut->addHTML("<table class='soapfailures'>\n");
+				$wgOut->addHTML("<tr><th nowrap='nowrap'>Requests</th><th>Artist</th><th>Song</th><th>Titles looked for</th><th>Fixed</th></tr>\n");
+				$REQUEST_URI = $_SERVER['REQUEST_URI'];
+				$rowIndex=0;
+				foreach($data as $row){
+					$artist = $row['artist'];
+					$song = $row['song'];
+					$numRequests = $row['numRequests'];
+					$lookedFor = $row['lookedFor'];
+					$totFailures += $numRequests;
+					$wgOut->addHTML(utf8_encode("<tr".((($rowIndex%2)!=0)?" class='odd'":"")."><td>$numRequests</td><td>"));
+					$wgOut->addWikiText("[[$artist]]");
+					$wgOut->addHTML("</td><td>");
+					$wgOut->addWikiText("[[$artist:$song|$song]]");
+					$delim = "&amp;";
+					$prefix = "";
+
+					// If the short-url is in the REQUEST_URI, make sure to add the index.php?title= prefix to it.
+					if(strpos($REQUEST_URI, "index.php?title=") === false){
+						$prefix = "/index.php?title=";
+
+						// If we're adding the index.php ourselves, but the request still started with a slash, remove it because that would break the request if it came after the "title="
+						if(substr($REQUEST_URI,0,1) == "/"){
+							$REQUEST_URI = substr($REQUEST_URI, 1);
+						}
+					}
+					$wgOut->addHTML("</td><td>");
+					$wgOut->addWikiText("$lookedFor");
+					$wgOut->addHTML("</td><td>");
+					$wgOut->addHTML("<form action='' method='POST'>
+							<input type='hidden' name='artist' value='$artist'/>
+							<input type='hidden' name='song' value='$song'/>
+							<input type='submit' name='fixed' value='".wfMsg('soapfailures-fixed')."'/>
+						</form>\n");
+					$wgOut->addHTML("</td>");
+					$wgOut->addHTML("</tr>\n");
+					
+					$rowIndex++;
+				}
+				$wgOut->addHTML("</table>\n");
+				$wgOut->addHTML("<br/>Total of <strong>$totFailures</strong> requests in the top $MAX_RESULTS.  This number will increase slightly over time, but we should fight to keep it as low as possible!");
+			} else {
+				$wgOut->addHTML("<em>No results found.</em>\n");
+			}
+
+			if(!empty($data)){
+				$wgMemc->set($CACHE_KEY_TIME, $cachedOn, strtotime("+2 hour"));
+				$wgMemc->set($CACHE_KEY_STATS, $statsHtml, strtotime("+2 hour"));
+				
+				// We use CACHE_KEY_DATA to determine when all of these keys have expired, so it should expire a few microseconds after the other two (that's why it's below the other set()s).
+				$wgMemc->set($CACHE_KEY_DATA, $data, strtotime("+2 hour"));
+			}
+		}
+
+		$wgOut->addHTML($statsHtml);
 	}
 }
 
