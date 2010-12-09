dc280f7584c308510b14a907637cc66045b29945
macbre <macbre@wikia-inc.com>
2010-12-09 15:13:16 +0000

    RTE: use default path to LocalSettings.php when minify.sh is run without any parameters
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31053 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RTE/minify.sh
+++ extensions/wikia/RTE/minify.sh
@@ -2,4 +2,9 @@
 # This shell script is used to concatenate and minify JS and CSS files (CK core and RTE code)
 svn up
 
-SERVER_ID=177 php minify.php $*
+if [ -n "$1" ]
+then
+	SERVER_ID=177 php minify.php $*
+else
+	SERVER_ID=177 php minify.php --conf=/usr/wikia/docroot/wiki.factory/LocalSettings.php
+fi
