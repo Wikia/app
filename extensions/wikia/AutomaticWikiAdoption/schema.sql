97e7d5ec645a7cafbd535e84a336913b11051d83
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-12-10 13:06:32 +0000

    change table - will be used in PaidWikis
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31107 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutomaticWikiAdoption/schema.sql
+++ extensions/wikia/AutomaticWikiAdoption/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `user_flags` (
   `user_id` int(10) NOT NULL,
 -- type - which extension this record is for
   `type` tinyint(3) unsigned NOT NULL DEFAULT '0',
--- timestamp
-  `timestamp` datetime NOT NULL,
+-- data
+  `data` varchar(255) NOT NULL,
   PRIMARY KEY (`city_id`,`user_id`,`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
