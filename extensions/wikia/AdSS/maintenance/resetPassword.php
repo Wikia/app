c98ac4a62cd6bd9ec96cb05f86e18048373bdd95
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-12-12 16:48:31 +0000

    adss: a script to reset a password
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31184 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AdSS/maintenance/resetPassword.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @package MediaWiki
+ * @addtopackage maintenance
+ */
+
+$optionsWithArgs = array( 'uid' );
+ini_set( "include_path", dirname(__FILE__)."/../../../../maintenance/" );
+require_once( "commandLine.inc" );
+
+if( isset( $options['uid'] ) ) {
+	$uid = intval( $options['uid'] );
+	if( $uid > 0 ) {
+		$user = AdSS_User::newFromId( $uid );
+		$password = AdSS_User::randomPassword();
+		$user->newpassword = $user->cryptPassword( $password );
+		$user->save();
+
+		$to = new MailAddress( $user->email );
+		$from = new MailAddress( $wgPasswordSender );
+		$subject = '[AdSS] Your new password';
+		$url = SpecialPage::getTitleFor( 'AdSS/manager' )->getFullURL();
+		$body = "Hi,
+
+Your password has been reset. With the password shown below, and the
+email address we sent this to as your username, you can login and
+review your ads, buy new ads and cancel an ad if it starts getting
+stale.
+
+URL: $url
+Username: {$user->email}
+Password: $password
+
+Thanks!
+
+The Wikia Team
+";
+		UserMailer::send( $to, $from, $subject, $body );
+		echo "password reset!\n";
+	} else {
+		echo "wrong uid value!\n";
+		exit( 1 );
+	}
+} else {
+	echo "Usage: resetPassword.php --uid <user id>\n";
+	exit( 1 );
+}
