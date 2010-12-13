58d8edfc2ef89217874601382ee7da0d58cd1d14
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-12-13 11:04:55 +0000

    adss: escape html in the preview so people know what to expect
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31195 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdSS/templates/adForm.tmpl.php
+++ extensions/wikia/AdSS/templates/adForm.tmpl.php
@@ -151,10 +151,10 @@ $("#wpUrl").keyup( function() {
 	$("div.sponsormsg-preview > ul > li > a").attr( "href", "http://"+$("#wpUrl").val() );
 } );
 $("#wpText").keyup( function() {
-	$("div.sponsormsg-preview > ul > li > a").html( $("#wpText").val() );
+	$("div.sponsormsg-preview > ul > li > a").text( $("#wpText").val() );
 } );
 $("#wpDesc").keyup( function() {
-	$("div.sponsormsg-preview > ul > li > p").html( $("#wpDesc").val() );
+	$("div.sponsormsg-preview > ul > li > p").text( $("#wpDesc").val() );
 } );
 /*]]>*/
 </script>
