b76e688d4863b1ab34772dc64b49b421f05690bb
will <will@wikia-inc.com>
2010-12-13 22:42:18 +0000

    remove categories from ComScore branding on hubs pages in RT #135581
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31251 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/CorporateFooter_Index.php
+++ skins/oasis/modules/templates/CorporateFooter_Index.php
@@ -5,7 +5,9 @@
 			<div class="wordmark">
 				<img src="<?= $wgBlankImgUrl; ?>" class="sprite logo">
 			</div>
+			<?php if ($hub->cat_id != 4) { ?>
 			<div class="hub"><a href="http://www.wikia.com/<?=$hub->cat_name?>"><?=$hub->cat_name?></a></div>
+			<?php } ?>
 		</div>
 		<ul>
 <?php 
