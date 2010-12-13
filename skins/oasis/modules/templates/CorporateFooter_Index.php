3d7d2aa122cd79470e99641e336c736dab454b0b
will <will@wikia-inc.com>
2010-12-13 22:54:33 +0000

    add comments to r31251
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31252 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/CorporateFooter_Index.php
+++ skins/oasis/modules/templates/CorporateFooter_Index.php
@@ -5,7 +5,7 @@
 			<div class="wordmark">
 				<img src="<?= $wgBlankImgUrl; ?>" class="sprite logo">
 			</div>
-			<?php if ($hub->cat_id != 4) { ?>
+			<?php if ($hub->cat_id != 4) { // 4: Corporate ?>
 			<div class="hub"><a href="http://www.wikia.com/<?=$hub->cat_name?>"><?=$hub->cat_name?></a></div>
 			<?php } ?>
 		</div>
