db829f0a3bae55d45f78964fced9095fdaa92db4
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-12-14 09:47:04 +0000

    Oasis: load YUI on every edit page and revert r31241
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31262 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/EditPageModule.class.php
+++ skins/oasis/modules/EditPageModule.class.php
@@ -8,12 +8,13 @@
 class EditPageModule extends Module {
 
 	/**
-	 * Disables edit form when in read-only mode (RT #85688)
+	 * Disables edit form when in read-only mode (RT #85688) and loads YUI on edit pages
 	 */
 	public static function onAlternateEdit($editPage) {
-		global $wgOut, $wgTitle;
+		global $wgOut, $wgTitle, $wgJsMimeType;
 		wfProfileIn(__METHOD__);
 
+		// disable edit form when in read-only mode
 		if (wfReadOnly()) {
 			$wgOut->setPageTitle(wfMsg('editing', $wgTitle->getPrefixedText()));
 			$wgOut->addHtml(
@@ -25,6 +26,14 @@ class EditPageModule extends Module {
 			wfProfileOut(__METHOD__);
 			return false;
 		}
+
+		// macbre: load YUI on edit page (it's always loaded using $.loadYUI)
+		// PLB has problems with $.loadYUI not working correctly in Firefox (callback is fired to early)
+		$StaticChute = new StaticChute('js');
+		$StaticChute->useLocalChuteUrl();
+		$yui = $StaticChute->getChuteUrlForPackage('yui');
+
+		$wgOut->addScript("<script type=\"{$wgJsMimeType}\" src=\"{$yui}\"></script>");
 
 		wfProfileOut(__METHOD__);
 		return true;
