6c23ba5b44a6e4b81de9827a9c7d9c2b31235aea
tor <tor@wikia-inc.com>
2010-12-16 15:26:39 +0000

    removed leftovers from Widgets in Monaco
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31481 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -270,15 +270,12 @@ $wgAjaxExportList[] = "searchSuggest";
 include_once( "$IP/extensions/ExtensionFunctions.php" );
 include_once( "$IP/extensions/wikia/AjaxFunctions.php" );
 include_once( "$IP/extensions/wikia/DataProvider/DataProvider.php" );
-include_once( "$IP/extensions/wikia/WidgetFramework/WidgetFramework.php" );
-include_once( "$IP/extensions/wikia/SpecialWidgetDashboard/SpecialWidgetDashboard_setup.php" );
 include_once( "$IP/extensions/wikia/StaffSig/StaffSig.php" );
 include_once( "$IP/extensions/wikia/SpecialSignup/SpecialSignup.php" );
 include_once( "$IP/extensions/wikia/MergeFiles/MergeFiles.php" );
 include_once( "$IP/extensions/wikia/TagCloud/TagCloudClass.php" );
 include_once( "$IP/extensions/wikia/MostPopularCategories/SpecialMostPopularCategories.php" );
 include_once( "$IP/extensions/wikia/MostPopularArticles/SpecialMostPopularArticles.php" );
-include_once( "$IP/extensions/wikia/WidgetSpecialPage/WidgetsSpecialPage.php" );
 include_once( "$IP/extensions/wikia/Scribe/SpecialScribeEvents.php" );
 
 /**
