9d31c6d2d5b6671491acf2b74bba80120d2e9206
tor <tor@wikia-inc.com>
2010-12-17 00:54:17 +0000

    dropped Special:MostPopularArticles (rt#139479)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31501 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -277,7 +277,6 @@ include_once( "$IP/extensions/wikia/SpecialSignup/SpecialSignup.php" );
 include_once( "$IP/extensions/wikia/MergeFiles/MergeFiles.php" );
 include_once( "$IP/extensions/wikia/TagCloud/TagCloudClass.php" );
 include_once( "$IP/extensions/wikia/MostPopularCategories/SpecialMostPopularCategories.php" );
-include_once( "$IP/extensions/wikia/MostPopularArticles/SpecialMostPopularArticles.php" );
 include_once( "$IP/extensions/wikia/WidgetSpecialPage/WidgetsSpecialPage.php" );
 include_once( "$IP/extensions/wikia/Scribe/SpecialScribeEvents.php" );
 
