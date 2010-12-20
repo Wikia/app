e196e5b160e74a28d46c0ab50ff2b5e6eed58b07
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-12-20 11:11:27 +0000

    ArticleCategoriesModule: render category box if list of categories is not empty
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31578 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/ArticleCategoriesModule.class.php
+++ skins/oasis/modules/ArticleCategoriesModule.class.php
@@ -7,9 +7,14 @@
 
 class ArticleCategoriesModule extends Module {
 	var $catlinks;
-	
+
 	public function executeIndex() {
 		wfProfileIn(__METHOD__);
+
+		// MW1.16 always returns non empty $catlinks
+		if (strpos($this->catlinks, ' catlinks-allhidden\'></div>') !== false) {
+			$this->catlinks = '';
+		}
 
 		wfProfileOut(__METHOD__);
 	}
