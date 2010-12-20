e3f257768365c5262c135d13447da93ce6187a71
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-12-20 23:54:53 +0000

    trunk: fixing corporate footer links
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31618 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/CorporateFooter_Index.php
+++ skins/oasis/modules/templates/CorporateFooter_Index.php
@@ -14,7 +14,13 @@
 foreach ($footer_links as $link) {
 		?>
 			<li>
+			<?php
+			// biz logic like this should be in the module but the copyright link an exception RT#74478
+			if ($link["text"] == 'GFDL' || (strpos($link["text"], 'LICENSE') > 0) ) {
+				echo $wgUser->getSkin()->getCopyright();
+			} else {?>
 				<a href="<?= $link["href"]; ?>"><?= $link["text"]; ?></a>
+			<?php } ?>
 			</li>
 	<?php
 }
