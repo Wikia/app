b75fce5ead8fce984d0123fd631bdc9ed6ec1b2c
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-12-21 14:42:35 +0000

    RT#140926: WikiBuilder test fails
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31649 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/WikiBuilderTest.java
+++ tests/acceptance/com/wikia/selenium/tests/WikiBuilderTest.java
@@ -14,18 +14,26 @@ public class WikiBuilderTest extends BaseTest {
 	public void testWikiBuilderTest() throws Exception {
 		String rand = Long.toString(Math.abs(new Random().nextLong()), 36).toUpperCase();
 		loginAsStaff();
-		session().open("/wiki/Special:WikiBuilder");
+		session().open("index.php?title=Special:WikiBuilder");
 		waitForElement("//textarea[@id='Description']");
 		session().type("Description", "abc123");
 		session().click("//input[@value='Save Intro']");
+
 		waitForElementVisible("ThemeTab");
 		session().click("//section[@id='ThemeTab']/div/ul/li[2]/img");
 		session().click("//input[@value='Save Theme']");
+
+		waitForElementVisible("//section[@id='WikiBuilder']/div/div[@class='step3']");
 		session().type("//input[@type='text']", rand);
-		session().click("//input[@value='Done']");
-		session().open("/");
+		session().click("//input[@value='Save Pages']");
+
+		waitForElementVisible("//section[@id='WikiBuilder']/div/div[@class='step4']");
+		session().click("//input[@value='Continue to your wiki']");
+
+		session().waitForPageToLoad(TIMEOUT);
 		waitForElement("//div[@id='WikiaArticle']");
 		assertTrue(session().isTextPresent("abc123"));
+
 		session().type("search", rand);
 		session().click("//input[@type='submit']");
 		waitForElement("//header/h1");
