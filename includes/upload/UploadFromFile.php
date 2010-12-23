a18aae8c23223514d8eaab35ad25fc4edd937722
inez <inez@wikia-inc.com>
2010-12-23 15:01:20 +0000

    * Delete SpecialMiniUpload extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31759 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/upload/UploadFromFile.php
+++ includes/upload/UploadFromFile.php
@@ -9,15 +9,17 @@
  */
 class UploadFromFile extends UploadBase {
 
+	const FILE_FIELD_NAME = 'wpUploadFile';
+	const DEST_FIELD_NAME = 'wpDestFile';
 
-	function initializeFromRequest( &$request ) {
-		$desiredDestName = $request->getText( 'wpDestFile' );
+	function initializeFromRequest( &$request, $fileField = UploadFromFile::FILE_FIELD_NAME, $destField = UploadFromFile::DEST_FIELD_NAME ) {
+		$desiredDestName = $request->getText( $destField );
 		if( !$desiredDestName )
-			$desiredDestName = $request->getFileName( 'wpUploadFile' );
+			$desiredDestName = $request->getFileName( $fileField );
 		return $this->initializePathInfo(
 			$desiredDestName,
-			$request->getFileTempName( 'wpUploadFile' ),
-			$request->getFileSize( 'wpUploadFile' )
+			$request->getFileTempName( $fileField ),
+			$request->getFileSize( $fileField )
 		);
 	}
 	/**
