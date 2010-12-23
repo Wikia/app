570010a83155a2b260a9fcfa9b19a836a146e128
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2010-12-23 12:08:53 +0000

    Yahoo_IM: update to v1.1 (2007-11-06)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31753 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/Yahoo_IM/Yahoo_IM.php
+++ extensions/3rdparty/Yahoo_IM/Yahoo_IM.php
@@ -1,83 +1,97 @@
 <?php
 
-// WikiMedia Yahoo Extension Ver 1.0 (http://meta.wikimedia.org/wiki/Yahoo_extension)
+// MediaWiki Yahoo Extension Ver 1.1 (http://wwww.mediawiki.org/wiki/Extension:Yahoo)
 
-// set up WikiMedia to react to the "<yahoo>" tag
+// Extension credits that show up on Special:Version
+$wgExtensionCredits['parserhook'][] = array(
+	'name' => 'Yahoo',
+	'author' => 'Guy Taylor',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:Yahoo',
+	'description' => 'Render Yahoo Button showing users online status.'
+);
+
+// set up MediaWiki to react to the "<yahoo>" tag
 $wgExtensionFunctions[] = "wfYahoo";
- 
+
 function wfYahoo() {
-        global $wgParser;
-        $wgParser->setHook( "yahoo", "RenderYahoo" );
+	global $wgParser;
+	$wgParser->setHook( "yahoo", "RenderYahoo" );
 }
- 
- 
+
+
 // the function that reacts to "<yahoo>"
- 
+
 function RenderYahoo( $input, $argv ) {
-    
-// set your defaults for the style and action (addfriend, call or chat) (0, 1, 2, 3 and 4)
 
-        $style_default = "2" ;
-        $action_default = "chat" ;
-    
-// the varibles are: <skype style="$argv['style']" action="$argv['action']">$input</skpye>
+	// set your defaults for the style and action (addfriend, call or sendim) (0, 1, 2, 3 and 4)
 
-// test to see if the optinal elements of the tags are set and supported. if not set them to the defaults
+	$style_default = "2" ;
+	$action_default = "sendim" ;
 
-        if( isset( $argv['style'] ) ){
-                $style = $argv['style'] ;
-                if( !($style == "0" OR $style == "1" OR $style == "2" OR $style == "3" OR $style == "4" ) ){
-                        $style = $style_default ;
-                }
-        } else {
-                $style = $style_default ;
-        }
-     
-        if( isset( $argv['action'] ) ){
-                $action = $argv['action'] ;
-                if( !($action == "addfriend" OR $action == "chat" OR $action == "call") ){
-                        $action = $action_default ;
-                }
-        } else {
-                $action = $action_default ;
-        }
-        
-// set the url to the image and the style of the image
-        switch( $style ){
+	// escape $input to prevent XSS
+	$input = htmlspecialchars($input) ;
 
-                case "0":    
-                        $image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=0" ' ;
-                        $image .= ' style="border: none; width: 12px; height: 12px;" alt="My status" />' ;
-                break;
-                
-                case "1":    
-                        $image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=1" ' ;
-                        $image .= ' style="border: none; width: 64px; height: 16px;" alt="My status" />' ;
-                break;
-                
-                case "2":    
-                        $image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=2" ' ;
-                        $image .= ' style="border: none; width: 125px; height: 25px;" alt="My status" />' ;
-                break;
-                
-                case "3":    
-                        $image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=3" ' ;
-                        $image .= ' style="border: none; width: 86px; height: 16px;" alt="My status" />' ;
-                break;
-                
-                case "4":
-                        $image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=4" ' ;
-                        $image .= ' style="border: none; width: 12px; height: 12px;" alt="My status" />' ;                  
-                break;
+	// the varibles are: <yahoo style="$argv['style']" action="$argv['action']">$input</yahoo>
 
-     }
-     
-// start the rendering the html outupt
-     $output  = '<!-- MediaWiki extension http://meta.wikimedia.org/w/index.php?title=Yahoo_extension -->';
-     $output .= '<a href="ymsgr:'.$input.'?'.$action.'">'.$image.'</a>';
-     $output .= '<!-- end of Yahoo button -->';
+	// test to see if the optinal elements of the tags are set and supported. if not set them to the defaults
 
-// send the output to MediaWiki
-     return $output;
+	if( isset( $argv['style'] ) ){
+		$style = $argv['style'] ;
+		if( !($style == "0" OR $style == "1" OR $style == "2" OR $style == "3" OR $style == "4" OR $style == "5" ) ){
+			$style = $style_default ;
+		}
+	} else {
+		$style = $style_default ;
+	}
+
+	if( isset( $argv['action'] ) ){
+		$action = $argv['action'] ;
+		if( !($action == "addfriend" OR $action == "sendim" OR $action == "call") ){
+			$action = $action_default ;
+		}
+	} else {
+		$action = $action_default ;
+	}
+
+	// set the url to the image and the style of the image
+	switch( $style ){
+
+		case "0":
+			$image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=0" ' ;
+			$image .= ' style="border: none; width: 12px; height: 12px;" alt="My status" />' ;
+			break;
+
+		case "1":
+			$image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=1" ' ;
+			$image .= ' style="border: none; width: 64px; height: 16px;" alt="My status" />' ;
+			break;
+
+		case "2":
+			$image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=2" ' ;
+			$image .= ' style="border: none; width: 125px; height: 25px;" alt="My status" />' ;
+			break;
+
+		case "3":
+			$image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=3" ' ;
+			$image .= ' style="border: none; width: 86px; height: 16px;" alt="My status" />' ;
+			break;
+
+		case "4":
+			$image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=4" ' ;
+			$image .= ' style="border: none; width: 12px; height: 12px;" alt="My status" />' ;
+			break;
+		case "5":
+			$image = '<img src="http://opi.yahoo.com/online?u='.$input.'&m=g&t=5" ' ;
+			$image .= ' style="border: none; width: 12px; height: 12px;" alt="My status" />' ;
+			break;
+
+	}
+
+	// start the rendering the html outupt
+	$output  = '<!-- MediaWiki extension http://www.mediawiki.org/wiki/Extension:Yahoo -->';
+	$output .= '<a href="ymsgr:'.$action.'?'.$input.'">'.$image.'</a>';
+	$output .= '<!-- end of Yahoo button -->';
+
+	// send the output to MediaWiki
+	return $output;
 }
-?>
