12c9e9a227bb9bc817e6446416c347dcfa0de24b
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-03 10:20:45 +0000

    not used
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31776 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AnswersSudoAd.i18n.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$messages = array();
-
-$messages["en"] = array(
-	"asa-leaderboard" => "leaderboard with ASA::HOMEPAGE_NEW_QUESTIONS ASA::HOMEPAGE_RECENTLY_ANSWERED_QUESTIONS",
-	"asa-leaderboard-list" => "http://image1.jpeg http://image2.jpeg",
-	"asa-leaderboard-image" => '$1',
-);
