614337f49518279542bbef9a5e5f28a1cc3aef53
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-03 09:10:13 +0000

    logger for withcity backend script
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31775 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Logger/SpecialLogger.i18n.php
@@ -0,0 +1,13 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'logger' => 'Backend logger',
+	'loggertitle'  => 'Backend logger', 
+	'loggerselectlogname' => 'Select log name:',
+	'loggerrecordswithpercent' => '\'\'\'$1\'\'\' records (\'\'\'$2\'\'\' percent finished)',
+	'loggerrecords' => '$1 records',
+	'loggergo' => 'Show',
+	'loggernoresult' => 'No results found',
+);
