389abb44603b181b035ec54b8ac3bbb65a8f7f43
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-04 00:36:51 +0000

    trunk: RT#143286 cache popular blog posts html
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31809 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/PopularBlogPostsModule.class.php
+++ skins/oasis/modules/PopularBlogPostsModule.class.php
@@ -5,25 +5,30 @@ class PopularBlogPostsModule extends Module {
 
 	public function executeIndex() {
 		wfProfileIn(__METHOD__);
-		global $wgParser;
+		global $wgParser, $wgMemc, $wgLang;
 
-		$input = "	<title>" .wfMsg('oasis-popular-blogs-title') ."</title>
-					<type>box</type>
-					<order>date</order>";
+		$mcKey = wfSharedMemcKey( "OasisPopularBlogPosts", $wgLang->getCode() );
+		$this->body = $wgMemc->get($mcKey);
+		if (empty ($this->body)) {
+			$input = "	<title>" .wfMsg('oasis-popular-blogs-title') ."</title>
+						<type>box</type>
+						<order>date</order>";
 
-		$time = date('Ymd', strtotime("-1 week")) . '000000'; // 7 days ago
-//		$time = '20091212000000';  // testing
-		$params = array (
-			    "summary" => true,
-				"paging" => false,
-				"timestamp" => $time,
-				"count" => 50,
-				"displaycount" => 4,
-				"order" => "comments"
-//				"style" => "add additionalClass if necessary"
-		);
+			$time = date('Ymd', strtotime("-1 week")) . '000000'; // 7 days ago
+//			$time = '20091212000000';  // use this value for testing if there are no recent posts
+			$params = array (
+					"summary" => true,
+					"paging" => false,
+					"timestamp" => $time,
+					"count" => 50,
+					"displaycount" => 4,
+					"order" => "comments"
+	//				"style" => "add additionalClass if necessary"
+			);
 
-		$this->body = BlogTemplateClass::parseTag($input, $params, $wgParser);
+			$this->body = BlogTemplateClass::parseTag($input, $params, $wgParser);
+			$wgMemc->set ($mcKey, $this->body, 60*60);  // cache for 1 hour
+		}
 
 		wfProfileOut(__METHOD__);
 	}
