baa54a6408304a84621b736f0aebcdea7c7e330b
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-06 21:24:38 +0000

    Update wgExtensionCredits: * no array needed when this is only one author * obsolete description removed * update key for extension description to convention
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31935 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/TimeAgoMessaging/TimeAgoMessaging.i18n.php
+++ extensions/wikia/TimeAgoMessaging/TimeAgoMessaging.i18n.php
@@ -3,7 +3,7 @@
 $messages = array();
 
 $messages['en'] = array(
-	'timeago-desc' => 'Provides i18n for jquery.timeago.js plugin',
+	'timeagomessaging-desc' => 'Provides i18n for jquery.timeago.js plugin',
 	'timeago-day' => '{{PLURAL:$1|a day|$1 days}} ago',
 	'timeago-hour' => '{{PLURAL:$1|an hour|$1 hours}} ago',
 	'timeago-minute' => '{{PLURAL:$1|a minute|$1 minutes}} ago',
@@ -11,7 +11,7 @@ $messages['en'] = array(
 );
 
 $messages['pl'] = array(
-	'timeago-desc' => 'Umozliwia lokalizowanie wtyczki jquery.timeago.js',
+	'timeagomessaging-desc' => 'Umozliwia lokalizowanie wtyczki jquery.timeago.js',
 	'timeago-day' => '{{PLURAL:$1|dzień|$1 dni}} temu',
 	'timeago-hour' => '{{PLURAL:$1|godzinę|$1 godziny|$1 godzin}} temu',
 	'timeago-minute' => '{{PLURAL:$1|minutę|$1 minuty|$1 minut}} temu',
