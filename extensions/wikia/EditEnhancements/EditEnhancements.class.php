9473494bd280b7a30e5714c866af4dade95e7857
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-06 00:43:06 +0000

    EditEnhancements rewrite - entirely JS driven for MW 1.16
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31920 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditEnhancements/EditEnhancements.class.php
+++ extensions/wikia/EditEnhancements/EditEnhancements.class.php
@@ -2,9 +2,6 @@
 
 class EditEnhancements {
 
-	private $buttons;
-	private $checkboxes;
-	private $summary;
 	private $action;
 	private $undo;
 	private $tmpl;
@@ -14,81 +11,27 @@ class EditEnhancements {
 
 		$this->action = $action;
 		$this->undo = intval($wgRequest->getVal('undo', 0)) != 0;
-		$wgHooks['EditPageSummaryBox'][] = array(&$this, 'summaryBox');
+
+		if($this->action == 'edit' && !$this->undo) {
+			$wgHooks['GetHTMLAfterBody'][] = array(&$this, 'editPageJS');
+		} else if ($this->action == 'submit' || $this->undo) {
+			$wgHooks['GetHTMLAfterBody'][] = array(&$this, 'previewJS');
+		}
+
 		$this->tmpl = new EasyTemplate( dirname( __FILE__ ) . '/templates/' );
 	}
 
-	public function summaryBox($summary) {
-		global $wgUser, $wgHooks;
-
-		$valid_skins = array('SkinMonaco', 'SkinAnswers', 'SkinOasis');
-		if(in_array(get_class($wgUser->getSkin()), $valid_skins)) {
-			$wgHooks['EditPage::showEditForm:checkboxes'][] = array(&$this, 'showCheckboxes');
-			if($this->action == 'edit' && !$this->undo) {
-				$wgHooks['GetHTMLAfterBody'][] = array(&$this, 'editPageJS');
-			} else if ($this->action == 'submit' || $this->undo) {
-				$wgHooks['GetHTMLAfterBody'][] = array(&$this, 'previewJS');
-			}
-			$wgHooks['EditForm::MultiEdit:Form'][] = array(&$this, 'showToolbar');
-			$wgHooks['EditPageBeforeEditButtons'][] = array(&$this, 'showButtons');
-			$this->summary = $summary;
-			$summary = '<div>';
-		}
-		return true;
-	}
-
 	public function editPageJS($skin, &$html) {
-		// moved into /js/editEnhancements.js
-		if ($skin->skinname = 'oasis') {
-			global $wgOut, $wgExtensionsPath, $wgStyleVersion, $wgJsMimeType;
+		global $wgOut, $wgExtensionsPath, $wgStyleVersion, $wgJsMimeType;
+		$wgOut->addScript("<script type=\"{$wgJsMimeType}\" src=\"{$wgExtensionsPath}/wikia/EditEnhancements/js/EditEnhancements.js?{$wgStyleVersion}\"></script>");
 
-			$wgOut->addScript("<script type=\"{$wgJsMimeType}\" src=\"{$wgExtensionsPath}/wikia/EditEnhancements/js/EditEnhancements.js?{$wgStyleVersion}\"></script>");
-		}
-		else {
-			$html .= $this->tmpl->render('EditEnhancementsJS');
-		}
 		return true;
 	}
 
 	public function previewJS($skin, &$html) {
-		$html .= $this->tmpl->render('EditEnhancementsPreviewJS');
-		return true;
-	}
-
-	public function showToolbar($a, $b, $c, $d) {
-		global $wgOut, $wgUser;
-
-		$this->tmpl->set_vars(array(
-			'buttons'    => $this->buttons,
-			'checkboxes' => $this->checkboxes,
-			'summary'    => $this->summary,
-			'action'     => $this->action,
-			'undo'       => $this->undo,
-			'arrowTitle' => wfMsg('edit-enhancements-scroll-down-arrow'),
-			'skinname'   => get_class($wgUser->getSkin()),
-		));
-
-		$wgOut->addHTML($this->tmpl->render('EditEnhancementsToolbar'));
-
-		return true;
-	}
-
-	function showButtons($EditPage, &$buttons) {
-		$this->buttons = $buttons;
-		if(isset($this->buttons['diff'])) {
-			unset( $this->buttons['diff'] );
-			$buttons = array('diff' => $buttons['diff'] );
-		} else {
-			$buttons = array();
-		}
-		return true;
-	}
-
-	function showCheckboxes($EditPage, &$checkboxes) {
-		$this->checkboxes = $checkboxes;
-
-		// Change it to hide
-		$checkboxes['minor'] = $checkboxes['watch'] = '';
+		global $wgOut, $wgExtensionsPath, $wgStyleVersion, $wgJsMimeType;
+		$wgOut->addScript("<script type=\"{$wgJsMimeType}\" src=\"{$wgExtensionsPath}/wikia/EditEnhancements/js/EditEnhancementsPreview.js?{$wgStyleVersion}\"></script>");
+		
 		return true;
 	}
 }
--- extensions/wikia/EditEnhancements/js/EditEnhancements.js
+++ extensions/wikia/EditEnhancements/js/EditEnhancements.js
@@ -1,61 +1,117 @@
-EditEnhancements = function() {
-	this.calculate = function() {
-		var textbox = false;
+$(function() {
+	EditEnhancements.init();
+});
 
-		if ($('#wpTextbox1___Frame').exists()) { // FCK-editor
+var EditEnhancements = {
+
+	// Variables used in various EditEnhancements functions
+	settings: {
+		loopCount: 0  // Counter for loop() function
+	},
+
+
+	/**
+	 *  Init funciton is run once ondomready
+	 */
+	init: function() {	
+		/* Repostion elements in layout */
+		$('<div id="edit_enhancements_toolbar"><ul>').insertBefore("#editpage-copywarn");
+		$("#wpSummaryLabel, #wpSummary, #wpSave, #wpPreview").each(function() {
+			$(this).wrap("<li>").parent().appendTo("#edit_enhancements_toolbar ul");
+		});
+		// Checkboxes are done outside of each loop because it must appear last, but isn't last in original layout.
+		$(".editCheckboxes").removeClass().wrap('<li class="editCheckboxes">').parent().appendTo("#edit_enhancements_toolbar ul");
+		
+		// Bind event handlers
+		$(window).bind({
+			'resize': EditEnhancements.calculate,
+			'rteready': EditEnhancements.calculate,
+			'load': EditEnhancements.loop
+		});
+	
+		// Enables jump to top of the editor after editor has been loaded
+		if (skin == 'oasis') {
+			EditEnhancements.jumpToEditor();
+		}
+	},
+		
+
+	/**
+	 *  Adjusts the height of the editor so that the Save button is visible without scrolling.
+	 */
+	calculate: function() {	
+		/* Adjust editor height */
+		// Declare variables
+		var toolbar; 				// Editor toolbar with formatting controls
+		var textbox; 				// Editing area
+		var textboxOffset; 	// jQuery offset calculation of editing area
+		var viewportHeight;	// Height of the browser viewport
+		var heightElements; // Array of elements that add height to the interface
+		var height;					// The height sum of the elements that add height to the interface
+		var targetHeight;		// Height to set the editing area
+
+		
+		// Set variables
+		toolbar = $('#cke_contents_wpTextbox1');
+		
+		textbox = false; 
+		if ($('#wpTextbox1___Frame').exists()) { 
+			// FCK-editor
 			textbox = $('#wpTextbox1___Frame');
-		}
-		else if ( $('#cke_contents_wpTextbox1').exists()) { // CKeditor
+		} else if ( $('#cke_contents_wpTextbox1').exists()) { 
+			// CKeditor
 			textbox = $('#cke_contents_wpTextbox1');
+		} else { 
+			// MediaWiki editor
+			textbox = $('#wpTextbox1');
 		}
-		else  { // MW editor
-			 textbox = $('#wpTextbox1');
-		}
-
-		var toolbar = $('#cke_contents_wpTextbox1');
-
-		var textboxOffset = textbox.offset();
-
+		
+		textboxOffset = textbox.offset();
 		// textbox not fully initialized (RT #79980)
 		if (!textboxOffset) {
 			return;
 		}
-
-		var textareaTop = Math.floor(textboxOffset.top);
-
-		var viewportHeight = $(window).height();
-		var viewportHeight = window.innerHeight ? window.innerHeight : $(window).height(); // Opera 9.5
-
-		var stuff = [
-		//'#blogInfo', '.blogPostForm',	// @todo blog stuff elements
-		'#toolbar', // mediaWiki
-		'.cke_top','#csMainContainer','#edit_enhancements_toolbar'];
-
-		var height = 0;
-		$.each(stuff, function(index, item) {
+		
+		viewportHeight = $(window).height();
+		//TODO: FIGURE OUT IF WE CARE ABOUT OPERA 9.5
+		viewportHeight = window.innerHeight ? window.innerHeight : $(window).height(); // Opera 9.5
+		
+		// Create array of elements that add height to the interface
+		// TODO: ADD BLOG ELEMENTS: '#blogInfo' AND '.blogPostForm'
+		heightElements = ['#toolbar', '.cke_top', '#csMainContainer', '#edit_enhancements_toolbar'];
+		height = 0;
+		$.each(heightElements, function(index, item) {
 			var node = $(item);
 			if (node.exists()) {
 				height += node.outerHeight();
 			}
 		});
 
-		var targetHeight = viewportHeight - height;
-
-		// limit the height-changes to be within <300, 800>
+		// Calculate target height by subtracting height of elements in heightElements array from the viewport height
+		targetHeight = viewportHeight - height;
+		// Height should be at least 200 and at most 800.
 		targetHeight = Math.min(Math.max(targetHeight, 200), 800);
 
-		//set the textbox height
-		textbox.height(targetHeight);
+		// Set the textbox height
+		textbox.height(targetHeight - 25); // 25 accounts for some slop.. i know, it sucks.
 
-		// rescale summary box
-		var items = toolbar.find('li');
-		var summaryBox = $('#wpSummaryEnhanced');
-		var itemsWidth = 0;
+
+		/* Adjust summary field width */
+		/*
+		// Declare variables
+		var items;							// Items within the toolbar
+		var summaryBox;					// The summary text field
+		var itemsWidth;					// Width of items in the toolbar
+		var targetSummaryWidth; // New width to set summary field
+
+		items = toolbar.find('li');
+		summaryBox = $('#wpSummaryEnhanced');
+		itemsWidth = 0;
 
 		items.each(function() {
 			var item = $(this);
 
-			// on Oasis "minor edit" checkbox is below summary field
+			// on Oasis "minor edit" checkbox is below summary field - skip minor checkbox
 			if (window.skin == 'oasis' && item.hasClass('minor')) {
 				return false;
 			}
@@ -64,77 +120,48 @@ EditEnhancements = function() {
 		});
 		itemsWidth -= summaryBox.innerWidth();
 
-		var newSummaryWidth =  (toolbar.innerWidth() - 10) - itemsWidth - 10;
+		targetSummaryWidth =  toolbar.innerWidth() - itemsWidth - 20;
 
-		// limit the width of summary box to be within <200, 500>
-		newSummaryWidth = Math.min(Math.max(newSummaryWidth, 200), 500);
+		// Width should be at least 200 and at most 500.
+		targetSummaryWidth = Math.min(Math.max(targetSummaryWidth, 200), 500);
 		// Bernhard: buggy at the summary box width!
-		//summaryBox.width(newSummaryWidth);
-
-		// inform RTE that its editarea has been resized (RT#36994)
+		//summaryBox.width(targetSummaryWidth);
+		*/
+		
+		// Inform RTE that its editarea has been resized (RT#36994)
 		if (typeof window.RTE !='undefined' && typeof window.RTE.instance == 'object') {
 			RTE.instance.fire('resize');
 		}
-	}
-
-	this.calculate();
-}
-
-
-EditEnhancementsLoad = function() {
-	var EditEnhancementsLoop = function() {
-		if (count == 3) {
-			clearInterval(interval);
+	},
+	
+	
+	/**
+	 *  Runs the EditEnhancements.calculate() function 3 times.
+	 *  Late loading of images can cause calculation to not be correct the first time.
+	 *  This is called once onload.
+	 */
+	loop: function() {
+		if (EditEnhancements.settings.loopCount != 3) {
+			EditEnhancements.calculate();
+			EditEnhancements.settings.loopCount++;
+			setTimeout(arguments.callee, 500);
+		}
+	},
+	
+	/**
+	 *  Scrolls page to the editor
+	 */
+	jumpToEditor: function() {
+		if ($('.cke_editor').exists()) {
+			// CKEditor enabled
+			$('html, body').animate({ scrollTop: $('.cke_editor').eq(0).offset().top }, 1);
+		} else if ($('#toolbar').exists() && $('#toolbar').is(':visible') == true) {
+			// MediaWiki editor enabled
+			$('html, body').animate({ scrollTop: $('#toolbar').eq(0).offset().top }, 1);
 		} else {
-			EditEnhancements();
-			count++;
+			// Editor not found yet. Wait and try again.
+			setTimeout(arguments.callee, 100);
 		}
 	}
-	var count = 0;
 
-	// change the id of summary box, so changing its size works in IE
-	$('#wpSummary').attr('id', 'wpSummaryEnhanced');
-
-	var interval = setInterval(EditEnhancementsLoop, 500);
-
-	// scoll to editor top on load
-
-	// modifications for Oasis
-	if (window.skin == 'oasis') {
-		var toolbar = $('#edit_enhancements_toolbar');
-		var toolbarItems = toolbar.children().children('li');
-
-		// fix left margin of "minor" checkbox
-		toolbarItems.filter('.minor').
-			css('marginLeft', $('#wpSummaryLabel').width() + 5);
-	}
-}
-
-$(window).bind({
-	'load': EditEnhancementsLoad,
-	'resize': EditEnhancements,
-	'rteready': EditEnhancements
-});
-
-
-/** enables jump to top of the editor after editor has been loaded **/
-$(document).ready(function() {
-	if (skin == 'oasis') {
-		jumpToEditor();
-	}
-});
-
-
-jumpToEditor = function() {
-	// CKEditor enabled
-	if ($('.cke_editor').exists()) {
-		$('html, body').animate({ scrollTop: $('.cke_editor').eq(0).offset().top }, 1);
-	}
-	// mediaWiki Editor enabled
-	else if ($('#toolbar').exists() && $('#toolbar').is(':visible') == true) {
-		$('html, body').animate({ scrollTop: $('#toolbar').eq(0).offset().top }, 1);
-	}
-	else {
-		var checkForFinalEditorLoad = window.setTimeout('jumpToEditor()', 100);
-	}
-}
+};
