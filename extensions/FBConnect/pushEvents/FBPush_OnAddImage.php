498cc0d39daca03303ca73411035851e8311d957
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-07 16:21:43 +0000

    fix for BugId: 398
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@31963 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/FBConnect/pushEvents/FBPush_OnAddImage.php
+++ extensions/FBConnect/pushEvents/FBPush_OnAddImage.php
@@ -17,55 +17,60 @@ class FBPush_OnAddImage extends FBConnectPushEvent {
 		wfProfileIn(__METHOD__);
 
 		wfLoadExtensionMessages('FBPush_OnAddImage');
-		$wgHooks['ArticleSaveComplete'][] = 'FBPush_OnAddImage::onArticleSaveComplete'; 	
-		$wgHooks['UploadComplete'][] = 'FBPush_OnAddImage::onUploadComplet';   
+		$wgHooks['ArticleSaveComplete'][] = 'FBPush_OnAddImage::onArticleSaveComplete';
+		$wgHooks['UploadComplete'][] = 'FBPush_OnAddImage::onUploadComplet';
 		wfProfileOut(__METHOD__);
 	}
 
 	public function loadMsg() {
 		wfProfileIn(__METHOD__);
-				
+
 		wfLoadExtensionMessages('FBPush_OnAddImage');
-		
+
 		wfProfileOut(__METHOD__);
 	}
-	
-	public static function onArticleSaveComplete(&$article, &$user, $text, $summary,$flag, $fake1, $fake2, &$flags, $revision, &$status, $baseRevId){ 
+
+	public static function onArticleSaveComplete(&$article, &$user, $text, $summary,$flag, $fake1, $fake2, &$flags, $revision, &$status, $baseRevId){
 		if( $article->getTitle()->getNamespace() != NS_FILE ) {
 			return true;
 		}
 		$img = wfFindFile( $article->getTitle()->getText() );
 		if (!empty($img) && ($img->media_type == 'BITMAP') ) {
-			FBPush_OnAddImage::uploadNews( $img, $img->title->getText(), $img->title->getFullUrl("?ref=fbfeed&fbtype=addimage")) ;	
+			FBPush_OnAddImage::uploadNews( $img, $img->title->getText(), $img->title->getFullUrl("?ref=fbfeed&fbtype=addimage")) ;
 		}
 		return true;
 	}
-	
+
     public static function  onUploadComplet(&$image) {
         global $wgServer, $wgSitename;
-        if ($image->mLocalFile->media_type == 'BITMAP' ) {
-        	FBPush_OnAddImage::uploadNews( $image->mLocalFile, $image->mLocalFile->getTitle(), $image->mLocalFile->getTitle()->getFullUrl( "?ref=fbfeed&fbtype=addimage" ) );
+
+		/**
+		 * $image->mLocalFile is protected
+		 */
+		$localFile = $image->getLocalFile();
+        if ($localFile->media_type == 'BITMAP' ) {
+        	FBPush_OnAddImage::uploadNews( $localFile, $localFile->getTitle(), $localFile->getTitle()->getFullUrl( "?ref=fbfeed&fbtype=addimage" ) );
         }
         return true;
     }
-	
+
 	public static function uploadNews($image, $name, $url) {
 		global $wgSitename;
-		
-		$is = new imageServing(array(), 90);		
+
+		$is = new imageServing(array(), 90);
 		$thumb_url = $is->getThumbnails(array($image));
-		$thumb_url = array_pop($thumb_url); 
+		$thumb_url = array_pop($thumb_url);
 		$thumb_url = $thumb_url['url'];
-		
+
 		$params = array(
 			'$IMGNAME' => $name,
-			'$ARTICLE_URL' =>$url, //inside use  
+			'$ARTICLE_URL' =>$url, //inside use
 			'$WIKINAME' => $wgSitename,
 			'$IMG_URL' => $url,
 			'$EVENTIMG' => $thumb_url
 		);
-		
-		self::pushEvent(self::$messageName, $params, __CLASS__ );	
+
+		self::pushEvent(self::$messageName, $params, __CLASS__ );
 	}
-	
+
 }
