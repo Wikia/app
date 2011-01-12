c2ae4bea53f4e58f4de0b03968da31e97b0a676b
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-12 01:59:41 +0000

    RT 144481 - fixing misplacement of topic field for user_talk ns
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32100 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditEnhancements/js/EditEnhancements.js
+++ extensions/wikia/EditEnhancements/js/EditEnhancements.js
@@ -16,7 +16,15 @@ var EditEnhancements = {
 	init: function() {	
 		/* Repostion elements in layout */
 		$('<div id="edit_enhancements_toolbar"><ul>').insertBefore("#editpage-copywarn");
-		$("#wpSummaryLabel, #wpSummary, #wpSave, #wpPreview").each(function() {
+		
+		/* List of elements to reposition */
+		var elements = ["#wpSave", "#wpPreview"];
+		// NS_3 is User_Talk and shouldn't have the summary field and label repositioned.
+		if (wgNamespaceNumber != 3) {
+			elements.unshift("#wpSummaryLabel", "#wpSummary");
+		}
+		
+		$(elements.toString()).each(function() {
 			$(this).wrap("<li>").parent().appendTo("#edit_enhancements_toolbar ul");
 		});
 		// Checkboxes are done outside of each loop because it must appear last, but isn't last in original layout.
