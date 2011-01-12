b74c2235b87d95985272b502bde364a3408ecc59
siebrand <siebrand@wikia-inc.com>
2011-01-12 14:52:54 +0000

    Tweak messages.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32119 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiPayment/WikiPayment.i18n.php
+++ extensions/wikia/WikiPayment/WikiPayment.i18n.php
@@ -6,8 +6,8 @@ $messages['en'] = array(
 	'wikipayment-desc' => 'Allows payment per wiki to disable ads.',
 	'wikipayment-paypal-redirect' => 'Redirecting to PayPal, please wait..',
 	'wikipayment-click-here' => '',
-	'wikipayment-paypal-error' => "Paypal payment error (code: $1)",
-	'wikipayment-paypal-return-ok' => 'Paypal payment successful',
-	'wikipayment-paypal-return-cancel' => 'Paypal payment canceled',
+	'wikipayment-paypal-error' => "PayPal payment error (code: $1)",
+	'wikipayment-paypal-return-ok' => 'PayPal payment successful',
+	'wikipayment-paypal-return-cancel' => 'PayPal payment canceled',
 	'wikipayment-paypal-profile-name' => "Paid Wiki"
 );
