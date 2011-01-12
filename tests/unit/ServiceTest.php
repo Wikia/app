d8bb137d39ac69a990ebbfaf6247401798879666
macbre <macbre@wikia-inc.com>
2011-01-12 11:38:06 +0000

    CategoriesService: add unit test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32111 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/unit/ServiceTest.php
+++ tests/unit/ServiceTest.php
@@ -113,4 +113,24 @@ class ServiceTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($edits+1, $stats['edits']);
 	}
 
+	function testCategoriesService() {
+		global $wgBiggestCategoriesBlacklist;
+
+		// blacklist to be used by this unit test
+		$wgBiggestCategoriesBlacklist = array('stub', 'foo');
+
+		$service = new CategoriesService();
+
+		$this->assertTrue($service->isCategoryBlacklisted('foo'));
+		$this->assertTrue($service->isCategoryBlacklisted('BARFOO'));
+		$this->assertTrue($service->isCategoryBlacklisted('Stubs'));
+		$this->assertFalse($service->isCategoryBlacklisted('test'));
+		$this->assertFalse($service->isCategoryBlacklisted('stu'));
+
+		// test filtering helper method
+		$categories = array('Characters', 'Stubs', 'Footest', 'testfoo', 'bar', 'test');
+
+		$this->assertEquals(array('Characters', 'bar', 'test'), CategoriesService::filterOutBlacklistedCategories($categories));
+	}
+
 }
