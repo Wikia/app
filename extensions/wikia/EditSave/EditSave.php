5feaa165dd1c420d3616988896660fe4bf168327
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-13 23:45:42 +0000

    New extension exploring localStorage to prevent lost edits - should not be enabled yet
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32198 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/EditSave/EditSave.php
@@ -0,0 +1,13 @@
+<?
+$wgHooks['GetHTMLAfterBody'][] = 'editPageJS';
+
+function editPageJS() {
+	global $wgOut, $wgExtensionsPath, $wgStyleVersion, $wgJsMimeType, $wgRequest;
+	$action = $wgRequest->getVal('action', null);
+	
+	if ($action == 'edit') {
+		$wgOut->addScript("<script type=\"{$wgJsMimeType}\" src=\"{$wgExtensionsPath}/wikia/EditSave/js/EditSave.js?{$wgStyleVersion}\"></script>");
+	}
+
+	return true;
+}
