0d59515011df08bdbb606437974703c8f392f159
adi <adi@wikia-inc.com>
2011-01-18 13:59:37 +0000

    refactored for using MWAL
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32305 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaLabs/WikiaLabs.body.php
+++ extensions/wikia/WikiaLabs/WikiaLabs.body.php
@@ -2,22 +2,23 @@
 
 class WikiaLabs extends SpecialPage {
 
-	private $mpa = null;
+	//private $mpa = null;
+	protected $app = null;
 
 	function __construct() {
+		$this->app = WF::build( 'App' );
 		parent::__construct( 'WikiaLabs', 'WikiaLabs' );
 	}
 
 	function execute($article_id = null, $limit = "", $offset = "", $show = true) {
-		global $wgRequest, $wgOut, $wgTitle, $wgUser;
-		$wgOut->addStyle( wfGetSassUrl( 'extensions/wikia/WikiaLabs/css/wikialabs.scss' )  );
+
+		$this->app->getGlobal('wgOut')->addStyle( $this->app->runFunction( 'wfGetSassUrl' , 'extensions/wikia/WikiaLabs/css/wikialabs.scss' ) );
 
 
-		$oTmpl = new EasyTemplate( dirname( __FILE__ ) . "/templates/" );
-		$oTmpl->set_vars( array(
-		));
+		$oTmpl = WF::build( 'EasyTemplate', array( dirname( __FILE__ ) . "/templates/" ) );
+		$oTmpl->set_vars( array() );
 
-		$wgOut->addHTML( $oTmpl->render("wikialabs-main") );
+		$this->app->getGlobal('wgOut')->addHTML( $oTmpl->render("wikialabs-main") );
 
 		return ;
 	}
