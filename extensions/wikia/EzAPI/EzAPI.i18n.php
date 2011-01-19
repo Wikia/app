13505c4ac632264425c2575f2195b12cfe8d6b9c
siebrand <siebrand@wikia-inc.com>
2011-01-19 10:23:15 +0000

    * remove obsolete translations * update svn props
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32341 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EzAPI/EzAPI.i18n.php
+++ extensions/wikia/EzAPI/EzAPI.i18n.php
@@ -7,27 +7,5 @@ $messages['en'] = array(
 );
 
 $messages['qqq']= array(
-	'ezapi-desc' => 'message has changed, the extension has been repurposed'
+	'ezapi-desc' => '{{desc}}'
 );
-
-/** Spanish (Español)
- * @author Bola
- */
-$messages['es'] = array(
-	'ezapi-desc' => 'API para aplicaciones móviles',
-);
-
-/** Macedonian (Македонски)
- * @author Bjankuloski06
- */
-$messages['mk'] = array(
-	'ezapi-desc' => 'API за мобилни програми',
-);
-
-/** Dutch (Nederlands)
- * @author Siebrand
- */
-$messages['nl'] = array(
-	'ezapi-desc' => 'API voor mobiele applicaties',
-);
-
