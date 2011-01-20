8ee0f447d8216de856b8f000eaab6dd469c9dec0
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-20 17:42:22 +0000

    WikiaGameGuides: more work, caching will come in a second moment
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32423 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaGameGuides/WikiaGameGuides.i18n.php
+++ extensions/wikia/WikiaGameGuides/WikiaGameGuides.i18n.php
@@ -3,5 +3,18 @@
 $messages = array();
 
 $messages['en'] = array(
-	'wikiagameguides-desc' => 'Wikia Game Guides mobile app modules'
+	'wikiagameguides-desc' => 'Wikia Game Guides mobile app modules',
+	'wikiagameguides-tab-1' => 'Tab 1',
+	'wikiagameguides-tab-2' => 'Tab 2',
+	'wikiagameguides-tab-3' => 'Tab 3',
+	'wikiagameguides-tab-more' => 'More',
+	'wikiagameguides-tab-more-content' => ''
+);
+
+$messages['qqq'] = array(
+	'wikiagameguides-tab-1' => 'Please keep it really short 1~8 chars',
+	'wikiagameguides-tab-2' => 'Please keep it really short 1~8 chars',
+	'wikiagameguides-tab-3' => 'Please keep it really short 1~8 chars',
+	'wikiagameguides-tab-more' => 'Please keep it really short 1~8 chars',
+	'wikiagameguides-tab-more-content' => 'Must be empty'
 );
--- extensions/wikia/WikiaGameGuides/ezapi/WikiaGameGuidesEzApiModule.class.php
+++ extensions/wikia/WikiaGameGuides/ezapi/WikiaGameGuidesEzApiModule.class.php
@@ -7,33 +7,59 @@
 class WikiaGameGuidesEzApiModule extends EzApiModuleBase {
 	const WF_WIKI_RECOMMEND_VAR = 'wgWikiaGameGuidesRecommend';
 	
+	function __construct( WebRequest $request ) {
+		global $wgDevelEnvironment;
+		
+		if( !$wgDevelEnvironment ) {
+			$this->setRequiresPost( true ); //only POST requests allowed
+		}
+		
+		parent::__construct( $request );
+	}
+	
 	/*
 	 * Returns a list of recommended wikis with some data from Oasis' ThemeSettings
 	 * 
 	 * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
 	 */
 	public function listWikis(){
-		global $wgDevelEnvironment;
 		wfProfileIn( __METHOD__ );
 		
-		if( !$wgDevelEnvironment ) {
-			$this->requiresPost(); //only POST requests allowed
-		}
-		wfLoadExtensionMessages( 'WikiaGameGuides' );
+		$ret = $this->getWikisList();
+		
 		$this->setContentType( EzApiContentTypes::JSON );
-		$this->setResponseContent( Wikia::json_encode( $this->getWikisData() ) );
+		$this->setResponseContent( Wikia::json_encode( $ret ) );
 		
 		wfProfileOut( __METHOD__ );
+		return $ret;
 	}
 	
+	/*
+	 * Returns a collection of data for the current wiki to use in the
+	 * per-wiki screen of the application
+	 * 
+	 * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
+	 */
+	public function listWikiContents(){
+		wfProfileIn( __METHOD__ );
+		
+		$ret = $this->getWikiContents();
+		
+		$this->setContentType( EzApiContentTypes::JSON );
+		$this->setResponseContent( Wikia::json_encode( $this->getWikiContents() ) );
+		
+		wfProfileOut( __METHOD__ );
+		return $ret;
+	}
 	
 	/*
 	 * Gets a list of recommended wikis through WikiFactory
 	 * 
 	 * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
 	 */
-	protected function getWikisData(){
+	private function getWikisList(){
 		wfProfileIn( __METHOD__ );
+		
 		$ret = Array();
 		
 		$wikiFactoryRecommendVar = WikiFactory::getVarByName( self::WF_WIKI_RECOMMEND_VAR, null );
@@ -49,11 +75,11 @@ class WikiaGameGuidesEzApiModule extends EzApiModuleBase {
 				$wikiThemeSettings = WikiFactory::getVarValueByName( 'wgOasisThemeSettings', $wikiId);
 				
 				$ret[] = Array(
-					'name' => ( !empty( $wikiThemeSettings[ 'wordmark-text' ] ) ) ? $wikiThemeSettings[ 'wordmark-text' ] : $wikiName,
-					'color' => ( !empty( $wikiThemeSettings[ 'wordmark-color' ] ) ) ? $wikiThemeSettings[ 'wordmark-color' ] : '#0049C6',
-					'backgroundColor' => ( !empty( $wikiThemeSettings[ 'color-page' ] ) ) ? $wikiThemeSettings[ 'color-page' ] : '#FFFFFF',
-					'baseUrl' => $wikiUrl,
-					'logoUrl'=> ( !empty( $wikiThemeSettings[ 'wordmark-image-url' ] ) ) ? $wikiThemeSettings[ 'wordmark-image-url' ] : null
+					'wikiName' => ( !empty( $wikiThemeSettings[ 'wordmark-text' ] ) ) ? $wikiThemeSettings[ 'wordmark-text' ] : $wikiName,
+					'wordmarkColor' => ( !empty( $wikiThemeSettings[ 'wordmark-color' ] ) ) ? $wikiThemeSettings[ 'wordmark-color' ] : '#0049C6',
+					'wordmarkBackgroundColor' => ( !empty( $wikiThemeSettings[ 'color-page' ] ) ) ? $wikiThemeSettings[ 'color-page' ] : '#FFFFFF',
+					'wikiUrl' => $wikiUrl,
+					'wordmarkUrl'=> ( !empty( $wikiThemeSettings[ 'wordmark-image-url' ] ) ) ? $wikiThemeSettings[ 'wordmark-image-url' ] : null
 					//,'data' => var_dump( $wikiThemeSettings, true )//debug only
 				);
 			}
@@ -64,5 +90,111 @@ class WikiaGameGuidesEzApiModule extends EzApiModuleBase {
 		
 		wfProfileOut( __METHOD__ );
 		return $ret;
+	}
+	
+	/*
+	 * Returns a structure representing application-related content on for the current wiki
+	 * 
+	 * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
+	 */
+	private function getWikiContents(){
+		wfProfileIn( __METHOD__ );
+		
+		$ret = Array();
+		
+		foreach ( $this->getTabsLabels() as $tab ) {
+			$ret[] = $this->getCategoryInfo( $tab );
+		}
+		
+		$ret[] = $this->getMoreCategoriesInfo();
+		
+		return $ret;
+		
+		wfProfileOut( __METHOD__ );
+	}
+	
+	/*
+	 * Gets the values of the tab messages which are names for real categories, except the last one
+	 * 
+	 * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
+	 */
+	private function getTabsLabels(){
+		wfLoadExtensionMessages( 'WikiaGameGuides' );
+		
+		return array(
+			wfMsgForContent( 'wikiagameguides-tab-1' ),
+			wfMsgForContent( 'wikiagameguides-tab-2' ),
+			wfMsgForContent( 'wikiagameguides-tab-3' ),
+		);
+	}
+	
+	/*
+	 * Gets data for the per-category tabs
+	 * 
+	 * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
+	 */
+	private function getCategoryInfo( $categoryName ) {
+		$categoryName = trim( $categoryName );
+		$category = Category::newFromName( $categoryName );
+		
+		$ret = Array(
+			'name' => $categoryName,
+			'items' => Array()
+		);
+		
+		if ( $category ) {
+			$ret[ 'name' ] = $category->getTitle()->getText();
+			$titles = $category->getMembers();
+			
+			foreach( $titles as $title ) {
+				$ret[ 'items' ][] = Array(
+					'name' => $title->getText(),
+					//TODO: replace temporary solution to reach the App skin
+					'url' => $title->getLocalUrl( array( 'useskin' => 'wikiaapp' ) )
+				);
+			}
+		}
+		
+		return $ret;
+	}
+	
+	/*
+	 * Gets data for the "More" tab
+	 * 
+	 * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
+	 */
+	private function getMoreCategoriesInfo(){
+		wfLoadExtensionMessages( 'WikiaGameGuides' );
+		
+		$ret = Array(
+			'name' => wfMsgForContent( 'wikiagameguides-tab-more' ),
+			'items' => Array()
+		);
+		
+		$categories = array_filter(
+			explode( "\n", wfMsgForContent( 'wikiagameguides-tab-more-content' ) ),
+			array( __CLASS__, 'verifyElement')
+		);
+		
+		foreach ( $categories as $categoryName ) {
+			$category = Category::newFromName( $categoryName );
+			
+			if ( $category ) {
+				$title = $category->getTitle();
+				
+				$ret[ 'items' ][] = Array(
+					'name' => $title->getText(),
+					//TODO: replace temporary solution to reach the App skin
+					'url' => $title->getLocalUrl( array( 'useskin' => 'wikiaapp' ) )
+				);
+			}
+		}
+		
+		return $ret;
+	}
+	
+	public static function verifyElement( $elem ){
+		$elem = trim( $elem );
+		return !empty( $elem );
 	}
 }
