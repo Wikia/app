d23ce3bb727d32de88fa0dcf3fd2fad3e30d4629
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-21 16:32:17 +0000

    WikiaLabs: Fogbugz Service class added
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32455 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ includes/wikia/services/FogbugzService.class.php
@@ -0,0 +1,160 @@
+<?php
+
+class FogbugzService extends Service {
+
+	/**
+	 * curl wrapper
+	 * @var Curl
+	 */
+	protected $curl;
+	protected $login;
+	protected $passwd;
+	protected $params = array();
+	protected $token = null;
+	protected $HTTPProxy = null;
+
+	public function __construct( $url, $login = '', $passwd = '', $HTTPProxy = null ) {
+		$this->setCurl( WF::build( 'Curl', array( 'url' => $url ) ) );
+		$this->setLogin( $login );
+		$this->setPasswd( $passwd );
+		$this->setHTTPProxy( $HTTPProxy );
+	}
+
+	public function setCurl(Curl $curl) {
+		$this->curl = $curl;
+	}
+
+	public function setLogin($value) {
+		$this->login = $value;
+	}
+
+	public function setPasswd($value) {
+		$this->passwd = $value;
+	}
+
+	public function setHTTPProxy($value) {
+		$this->HTTPProxy = $value;
+	}
+
+	private function getCurlOptions() {
+		$curlOptions = array();
+		if( !empty( $this->HTTPProxy ) ) {
+			$curlOptions[CURLOPT_PROXY] = $this->HTTPProxy;
+		}
+
+		$curlOptions[CURLOPT_TIMEOUT] = 30;
+		$curlOptions[CURLOPT_RETURNTRANSFER] = 1;
+		$curlOptions[CURLOPT_POSTFIELDS] = $this->getFormattedParams();
+
+		return $curlOptions;
+	}
+
+	/**
+	 * login to Fogbugz API
+	 * @return FogbugzService
+	 */
+	public function logon() {
+		$this->resetParams();
+		$this->setParam( 'cmd', 'logon' );
+		$this->setParam( 'email', $this->login );
+		$this->setParam( 'password', $this->passwd );
+
+		$this->curl->setopt_array( $this->getCurlOptions() );
+		$xml = $this->curl->exec();
+
+		if( !empty( $xml ) ) {
+			$dom = new DOMDocument;
+			$dom->loadXML( $xml );
+
+			$error = $dom->getElementsByTagName( 'error' )->item(0);
+			if( $error instanceof DOMElement ) {
+				throw new WikiaException("Fogbugz login failed");
+			}
+			else {
+				$this->token = $dom->getElementsByTagName( 'token' )->item(0)->nodeValue;
+			}
+		}
+		else {
+			throw new WikiaException("Fogbugz connection failed");
+		}
+		return $this;
+	}
+
+	/**
+	 * logout from Fogbugz API
+	 * @return FogbugzService
+	 */
+	public function logoff() {
+		$this->resetParams();
+		$this->setParam( 'cmd', 'logoff' );
+		$this->setParam( 'token', $this->token );
+
+		$this->curl->setopt_array( $this->getCurlOptions() );
+		$xml = $this->curl->exec();
+
+		if( !empty( $xml ) ) {
+			$this->token = null;
+			return $this;
+		}
+		else {
+			throw new WikiaException("Fogbugz connection failed");
+		}
+	}
+
+	/**
+	 * get list of areas
+	 * @param int $projectId project id
+	 * @return FogbugzService
+	 */
+	public function getAreas( $projectId = null) {
+		$results = array();
+		if( !empty($this->token) ) {
+			$this->resetParams();
+			$this->setParam( 'cmd', 'listAreas' );
+			$this->setParam( 'token', $this->token );
+
+			$this->curl->setopt_array( $this->getCurlOptions() );
+
+			$xml = $this->curl->exec();
+			if( !empty( $xml ) ) {
+				$dom = new DOMDocument;
+				$dom->loadXML( $xml );
+				$areas = $dom->getElementsByTagName( 'area' );
+				foreach( $areas as $area ) {
+					$areaProjectId = $area->getElementsByTagName( 'ixProject' )->item(0)->nodeValue;
+					if( ( $projectId == null ) || ( $projectId == $areaProjectId ) ) {
+						$results[] = array(
+							'id' => $area->getElementsByTagName( 'ixArea' )->item(0)->nodeValue,
+							'name' => $area->getElementsByTagName( 'sArea' )->item(0)->nodeValue,
+							'projectId' => $areaProjectId
+						);
+					}
+				}
+			}
+			else {
+				throw new WikiaException("Fogbugz connection failed");
+			}
+		}
+		return $results;
+	}
+
+	protected function setParam( $name, $value ) {
+		$this->params[$name] = $value;
+	}
+
+	protected function resetParams() {
+		$this->params = array();
+	}
+
+	private function getFormattedParams() {
+		$params = '';
+		foreach( $this->params as $k => $v ) {
+			if( $params != '' ) {
+				$params .= '&';
+			}
+			$params .= $k . '=' . $v;
+		}
+		return $params;
+	}
+
+}
