ec85ee1ce4c73bfcb4b5eaa2c8f639bb2fe2cb59
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-21 16:36:30 +0000

    WikiaLabs: Fogbugz connection added
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32456 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaLabs/WikiaLabs.class.php
+++ extensions/wikia/WikiaLabs/WikiaLabs.class.php
@@ -2,50 +2,57 @@
 
 class WikiaLabs {
 	function __construct() {
-		$this->app = WF::build( 'App' );	
+		$this->app = WF::build( 'App' );
 		$this->request = $this->app->getGlobal('wgRequest');
 	}
-	
+
 	function onGetRailModuleSpecialPageList(&$railModuleList) {
 		if($this->app->getGlobal('wgTitle')->isSpecial('WikiaLabs')) {
 			$railModuleList['1500'] = array('Search', 'Index', null);
 			$railModuleList['1400'] = array('WikiaLabs', 'Staff', null);
 			$railModuleList['1450'] = array('WikiaLabs', 'Graduates', null);
 		}
-				
+
 		return true;
 	}
 
 	function getProjectModal() {
+		$app = WF::build( 'App' );
 		$response =  WF::build( 'AjaxResponse' );
 		$oTmpl = WF::build( 'EasyTemplate', array( dirname( __FILE__ ) . "/templates/" ) );
-				
 		$wikiaLabsProject = WF::build( 'WikiaLabsProject' );
 
-		$oTmpl->set_vars( array( 
+		$fogbugzAPIConfig = $app->getGlobal( 'wgFogbugzAPIConfig' );
+		$fogbugzService = WF::build( 'FogbugzService', array( 'url' => $fogbugzAPIConfig['apiUrl'] ) );
+		$fogbugzService->setLogin( $fogbugzAPIConfig['username'] );
+		$fogbugzService->setPasswd( $fogbugzAPIConfig['password'] );
+		$fogbugzService->setHTTPProxy( $app->getGlobal( 'wgHTTPProxy' ) );
+
+		$oTmpl->set_vars( array(
 			'status' => $wikiaLabsProject->getStatusDict(),
-			'extensions' => $wikiaLabsProject->getExtensionsDict()
+			'extensions' => $wikiaLabsProject->getExtensionsDict(),
+			'areas' => $fogbugzService->getAreas()
 		));
-		
+
 		$oTmpl->render("wikialabs-addproject");
-		
+
 		$response->addText( $oTmpl->render("wikialabs-addproject") );
 		return $response;
-	}	
-	
+	}
+
 	function saveProject() {
 		global $wgRequest;
 		//TODO: need to refactore remove global
-		
+
 		$response = new AjaxResponse();
 
 		$out = array();
-		
+
 		$project = $wgRequest->getArray('project');
-		
+
 		$project['enablewarning'] = isset($project['enablewarning']);
 		$project['graduates'] = isset($project['graduates']);
-		
+
 		$validateResult = self::validateProjectForm($project);
 		if( !$validateResult->isValid($project) ) {
 			$out['status'] = "error";
@@ -53,30 +60,30 @@ class WikiaLabs {
 			$errors = $validateResult->getErrors();
 			foreach($errors as  $val1) {
 				foreach($val1 as $val2) {
-					$out['errors'][] = $val2; 
-				} 	
+					$out['errors'][] = $val2;
+				}
 			}
 			$response->addText(json_encode($out));
 			return $response;
 		} else {
-			
+
 			if( isset($project['id']) && isInt($project['id'])) {
-				$wlp = WF::build( 'WikiaLabsProject', array($project['id']) );	
+				$wlp = WF::build( 'WikiaLabsProject', array($project['id']) );
 			} else {
 				$wlp = WF::build( 'WikiaLabsProject', array() );
 			}
-			
+
 			$wlp->setName($project['name']);
 			$wlp->setFogbugzProject($project['area']);
-			
-			
+
+
 			if(!empty($project['prjscreen'])) {
 				$file_title = Title::newFromText( $project['prjscreen'], NS_FILE );
 				$img = wfFindFile( $file_title  );
 			}
-			
-			//TODO: 
-			$data = array( 
+
+			//TODO:
+			$data = array(
 				'description' => $project['description'],
 				'link' => $project['link'],
 				'prjscreen' => $project['prjscreen'],
@@ -84,13 +91,13 @@ class WikiaLabs {
 				'warning' =>  $project['warning'],
 				'enablewarning' => $project['enablewarning']
 			);
-			
+
 			$wlp->setData($data);
 			$wlp->setGraduated( $project['graduates'] );
-			$wlp->setActive($project['status'] == 1); 
+			$wlp->setActive($project['status'] == 1);
 			$wlp->setExtension($project['extension']);
 			$wlp->update();
-			
+
 			$out['status'] = 'ok';
 			$response->addText(json_encode($out));
 			return $response;
@@ -98,13 +105,13 @@ class WikiaLabs {
 	}
 
 	public function validateProjectForm($values = array()) {
-		//TODO: laod array form fog bugz 
+		//TODO: laod array form fog bugz
 		$wikiaLabsProject = WF::build( 'WikiaLabsProject' );
-		
+
 		$areas = array(1,2,3,4,5);
 		$status = array_keys($wikiaLabsProject->getStatusDict());
 		$projects = $wikiaLabsProject->getExtensionsDict();
-		
+
 		//TODO: translate msg
 		$mulitvalidator = new WikiaValidatorArray(array(
 			'validators'  => array(
@@ -117,15 +124,15 @@ class WikiaLabs {
 				'area' =>  new WikiaValidatorInArray(array("allowed" => $areas)),
 				'extension' => new WikiaValidatorInArray(array("allowed" => $projects)),
 		)));
-		
+
 		return $mulitvalidator;
 	}
-	
-	
+
+
 	public function getUrlImageAjax() {
 		global $wgRequest;
 		$name = $wgRequest->getVal("name", "");
-		
+
 		if(!empty($name)) {
 			$file_title = Title::newFromText( $name, NS_FILE );
 			$img = wfFindFile( $file_title  );
@@ -140,11 +147,11 @@ class WikiaLabs {
 		$response->addText(json_encode(array("status" => "ok",
 				"url" => wfReplaceImageServer( $img->getThumbUrl( $img->thumbName( array( 'width' => 150 ) ) ) ),
 		)));
-		
+
 		return $response;
 	}
-	
-	
+
+
 }
 
 
