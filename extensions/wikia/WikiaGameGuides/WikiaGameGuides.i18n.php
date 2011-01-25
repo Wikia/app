dc9b91d98bcd80d540b5cca846598aa564ae377e
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-25 13:08:47 +0000

    Documentation tweaks.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32533 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaGameGuides/WikiaGameGuides.i18n.php
+++ extensions/wikia/WikiaGameGuides/WikiaGameGuides.i18n.php
@@ -8,13 +8,13 @@ $messages['en'] = array(
 	'wikiagameguides-tab-2' => 'Tab 2',
 	'wikiagameguides-tab-3' => 'Tab 3',
 	'wikiagameguides-tab-more' => 'More',
-	'wikiagameguides-tab-more-content' => ''
+	'wikiagameguides-tab-more-content' => '' // Do not translate this to other languages.
 );
 
 $messages['qqq'] = array(
-	'wikiagameguides-tab-1' => 'Please keep it really short 1~8 chars',
-	'wikiagameguides-tab-2' => 'Please keep it really short 1~8 chars',
-	'wikiagameguides-tab-3' => 'Please keep it really short 1~8 chars',
-	'wikiagameguides-tab-more' => 'Please keep it really short 1~8 chars',
-	'wikiagameguides-tab-more-content' => 'Must be empty'
+	'wikiagameguides-tab-1' => 'Tab text. Best keep it really short. Suggesting 1 to 8 characters.',
+	'wikiagameguides-tab-2' => 'Tab text. Best keep it really short. Suggesting 1 to 8 characters.',
+	'wikiagameguides-tab-3' => 'Tab text. Best keep it really short. Suggesting 1 to 8 characters.',
+	'wikiagameguides-tab-more' => 'Tab text. Best keep it really short. Suggesting 1 to 8 characters.',
+	'wikiagameguides-tab-more-content' => 'Must remain empty'
 );
