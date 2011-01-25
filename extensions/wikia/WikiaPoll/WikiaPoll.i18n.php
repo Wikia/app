c1323a329968e197fcde753ea3d785aa8c4c9478
siebrand <siebrand@wikia-inc.com>
2011-01-25 13:28:56 +0000

    * remove another in-message use of formatnum and move it to code. * document all messages
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32538 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaPoll/WikiaPoll.i18n.php
+++ extensions/wikia/WikiaPoll/WikiaPoll.i18n.php
@@ -4,9 +4,21 @@ $messages = array();
 
 $messages['en'] = array(
 	'wikiapoll-desc' => 'Provides an easy way to create and manage polls',
-	'wikiapoll-people-voted' => '{{formatnum:$1}} {{PLURAL:$1|person|people}} voted',
+	'wikiapoll-people-voted' => '{{PLURAL:$1|$1 person|$1 people}} voted',
 	'wikiapoll-question' => '$1?',
 	'wikiapoll-thanks-for-vote' => 'Thanks!',
 	'wikiapoll-vote' => 'Vote!',
 	'wikiapoll-votes' => '{{PLURAL:$1|$1 vote|$1 votes}}',
+);
+
+$messages['qqq'] = array(
+	'wikiapoll-desc' => '{{desc}}',
+	'wikiapoll-people-voted' => 'Parameters:
+* $1 is the number of users that have voted.',
+	'wikiapoll-question' => 'Used as a title. Parameter:
+* $1 is the poll question without a question mark.',
+	'wikiapoll-thanks-for-vote' => 'Thank you to voting user.',
+	'wikiapoll-vote' => 'Button text to vote in a poll.',
+	'wikiapoll-votes' => 'Paramters:
+* $1 is the number of votes cast.',
 );
