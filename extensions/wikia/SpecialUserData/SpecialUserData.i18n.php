9680328002f113d2f8dd8b96789be20ae6d56753
marooned <marooned@wikia-inc.com>
2011-01-26 17:20:42 +0000

    New Special Page - used mainly for debugging UTF-8 user names
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32639 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialUserData/SpecialUserData.i18n.php
@@ -0,0 +1,22 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'userdata' => 'Get user data',
+	'userdata-userid-label' => 'Provide user ID:',
+	'userdata-submit' => 'Get data',
+
+	'userdata-desc' => 'Provides an interface for changing wguserdata per wiki.',
+	'userdata-header' => 'Increase wguserdata',
+	'userdata-value' => 'Current value of wguserdata is "$1". Press the button to set it to current timestamp.',
+	'userdata-updated' => 'wguserdata updated to "$1".',
+	'userdata-not-updated' => 'Failed to update wguserdata.',
+	'userdata-wf-reason' => 'Value updated via Special:userdata',
+	'userdata-no-wf' => 'WikiFactory is not enabled on this wiki. Unable to alter wguserdata.',
+);
+
+$messages['qqq'] = array(
+	'userdata-desc' => '{{desc}}',
+	'userdata-wf-reason' => 'Log message for administrators only. Do not change "Special:userdata"',
+);
