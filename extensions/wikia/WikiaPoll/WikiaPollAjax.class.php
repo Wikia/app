3adf17e1c50ab87a5d00eee00dd48ff36036261c
owen <owen@wikia-inc.com>
2011-01-27 01:05:37 +0000

    trunk: poll wip
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32676 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaPoll/WikiaPollAjax.class.php
+++ extensions/wikia/WikiaPoll/WikiaPollAjax.class.php
@@ -3,22 +3,145 @@
 class WikiaPollAjax {
 
 	/**
-	 * Register vote and return refreshed poll's HTML
+	 * Create a new poll
+	 * @param wgRequest question
+	 * @param wgRequest answer   (expects PHP array style in the form <input name=answer[]>)
+		// Page Content should be of a different style so we have to translate
+		//  *question 1\n
+		//  *question 2\n
 	 */
-	static public function vote($poll) {
+
+	static public function create() {
+		global $wgRequest, $wgUser;
+		wfProfileIn(__METHOD__);
+
+		$title = $wgRequest->getVal ('question');
+		$answers = $wgRequest->getArray ('answer');  // array
+
+		$title_object = Title::newFromText ($title, NS_WIKIA_POLL) ;
+
+		if (is_object ($title_object) && $title_object->exists() ) {
+			// TODO msg this
+			$res = array (
+				'success' => false,
+				'error' => "A poll with that name already exists."
+				);
+		} else {
+
+			$content = "";
+			foreach ($answers as $answer) {
+				$content .= "*$answer\n";
+			}
+
+			$article = new Article($title_object, NS_WIKIA_POLL);
+			$status = $article->doEdit($content, 'Poll Created', EDIT_NEW, false, $wgUser);
+			$title_object = $article->getTitle();
+
+			// fixme: check status object
+			$res = array (
+				'success' => true,
+				'pollId' => $article->getID(),
+				'url'  => $title_object->getLocalUrl() ,
+				'text' => $title_object->getPrefixedText()
+				);
+		}
+		wfProfileOut(__METHOD__);
+		return $res;
+	}
+
+	/**
+	 * Update the contents of an existing poll
+	 * @param wgRequest pollId
+	 */
+
+	static public function update() {
+		global $wgRequest;
+		wfProfileIn(__METHOD__);
+
+		$id = $wgRequest->getInt ('pollId');
+		$answers = $wgRequest->getArray ('answer');  // array
+
+		$poll = WikiaPoll::newFromId($pollId);
+		if (!empty($poll) && $poll->exists()) {
+
+			$content = "";
+			foreach ($answers as $answer) {
+				$content .= "*$answer\n";
+			}
+
+			$article = Article::newFromID($id);
+			$status = $article->doEdit($content, 'Poll Updated', EDIT_UPDATE, false, $wgUser);
+
+			// Fixme: check status object
+			$res = array (
+				'success' => true,
+				'pollId' => $article->getID(),
+				'url'  => $title_object->getLocalUrl() ,
+				'text' => $title_object->getPrefixedText()
+				);
+		}
+
+		return $res;
+		wfProfileOut(__METHOD__);
+	}
+
+	/**
+	 * check for poll existing
+	 * 
+	 * @param wgRequest pollId
+	 * @return array {exists, url, text}
+	 */
+
+	static public function exists() {
+		global $wgRequest ;
+		wfProfileIn(__METHOD__);
+
+		$res = array (
+				'exists' => false ,
+		);
+		$id = $wgRequest->getInt ('pollId', 0);
+		if ($id != 0) {
+			$article_object = Article::newFromID ($id) ;
+			$title_object = $article_object->getTitle();
+		}
+
+		if (is_object ($title_object) && $title_object->exists() ) {
+			$res = array (
+				'exists' => true,
+				'url'  => $title_object->getLocalUrl() ,
+				'text' => $title_object->getPrefixedText()
+			);
+		} 
+		wfProfileOut(__METHOD__);
+		return $res;
+	}
+
+	/**
+	 * Register vote and return refreshed poll's HTML
+	 *
+	 * @param wgRequest pollId
+	 * @return array {html}
+	 */
+	static public function vote() {
 		global $wgRequest, $wgTitle;
 		wfProfileIn(__METHOD__);
 
-		// register vote
-		$answer = $wgRequest->getInt('answer');
-		$poll->vote($answer);
+		$pollId = $wgRequest->getInt('pollId');
+		$poll = WikiaPoll::newFromId($pollId);
+		$html = '';
+		if (!empty($poll) && $poll->exists()) {
 
-		// render new poll
-		if ($wgTitle->getNamespace() == NS_WIKIA_POLL) {
-			$html = $poll->render();
-		}
-		else {
-			$html = $poll->renderEmbedded();
+			// register vote
+			$answer = $wgRequest->getInt('answer');
+			$poll->vote($answer);
+
+			// render new poll
+			if ($wgTitle->getNamespace() == NS_WIKIA_POLL) {
+				$html = $poll->render();
+			}
+			else {
+				$html = $poll->renderEmbedded();
+			}
 		}
 
 		$ret = array(
@@ -31,13 +154,19 @@ class WikiaPollAjax {
 
 	/**
 	 * Checks if current user has voted
+	 * @param wgRequest pollId
+	 * @return array {hasVoted}
 	 */
-	static public function hasVoted($poll) {
+	static public function hasVoted() {
 		wfProfileIn(__METHOD__);
 
-		$ret = array(
-			'hasVoted' => $poll->hasVoted(),
-		);
+		$pollId = $wgRequest->getInt('pollId');
+		$poll = WikiaPoll::newFromId($pollId);
+		$ret = array();
+
+		if (!empty($poll) && $poll->exists()) {
+			$ret['hasVoted'] = $poll->hasVoted();
+		}
 
 		wfProfileOut(__METHOD__);
 		return $ret;
