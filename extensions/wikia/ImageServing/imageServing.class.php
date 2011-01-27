275365fe9813722352732956485a47e916b27c4d
owen <owen@wikia-inc.com>
2011-01-27 03:51:32 +0000

    trunk: reapplied performance tweak
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32687 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageServing/imageServing.class.php
+++ extensions/wikia/ImageServing/imageServing.class.php
@@ -60,6 +60,7 @@ class imageServing{
 
 			foreach ( $articles as $key => $value ) {
 				$mcOut = $wgMemc->get( $this->_makeKey( $value, $n ), null );
+				$mcOut = null;
 				if($mcOut != null) {
 					unset( $articles[ $key ] );
 					$cache_return[ $value ] = $mcOut;
@@ -115,15 +116,20 @@ class imageServing{
 			$db_out = array();
 			if ( !empty($images_name) ) {
 				foreach ( $images_name as $img_name ) {
-					$oRow = $db->selectRow(
-						"( SELECT il_to  FROM `imagelinks`  WHERE il_to = " . $db->addQuotes($img_name) . "  LIMIT " . ($this->maxCount + 1) . " ) as t1 ",
-						array( 'count(il_to) as cnt' ),
-						array( ),
-						__METHOD__						
+					$result = $db->select(
+						array( 'imagelinks' ),
+						array( 'il_from' ),
+						array(
+							'il_to' => $img_name
+						),
+						__METHOD__,
+						array ('LIMIT' => ($this->maxCount + 1))
 					);
-					
+
 					if ( $oRow->cnt > $this->maxCount ) continue;
 						
+					# skip images which are too popular
+					if ($result->numRows() > $this->maxCount ) continue;
 					# check image table 
 					$oRowImg = $db->selectRow(
 						array( 'image' ),
@@ -141,7 +147,7 @@ class imageServing{
 					if ( $oRowImg->img_height > $this->minSize && $oRowImg->img_width > $this->minSize ) {
 						if ( !in_array( $oRowImg->img_minor_mime, array( "svg+xml","svg") ) ) {
 							$db_out[ $oRowImg->img_name ] = array(
-								'cnt'            => $oRow->cnt,
+								'cnt'            => $result->numRows(),
 								'il_to'          => $oRowImg->img_name,
 								'img_width'      => $oRowImg->img_width,
 								'img_height'     => $oRowImg->img_height,
