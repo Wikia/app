5c0f7b99abb4b0ae8c499bd2acd240029012bcbb
wladek <wladek@wikia-inc.com>
2011-01-28 17:48:10 +0000

    added missing part of Special:JavascriptTestRunner
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32799 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/JavascriptTestRunner/js/JavascriptTestRunner.js
@@ -0,0 +1,190 @@
+(function() {
+	
+	var JTR = {
+		frameworks: {},
+		filters: {},
+		outputs: {},
+		TestRunner: null
+	};
+	
+	/**
+	 * TestRunner - main testing class
+	 */
+	var TestRunner = function( filters, outputs ) {
+		this.configure(filters,outputs);
+	}
+	TestRunner.prototype.configure = function( filters, outputs ) {
+		this.filters = filters;
+		this.outputs = outputs;
+	}
+	TestRunner.prototype.run = function( frameworkName, testSuites ) {
+		var framework = new JTR.frameworks[frameworkName];
+		var args = Array.prototype.slice.call(arguments,1);
+		var data = framework.run.apply(framework,args);
+		this.dispatch(data);
+	}
+	TestRunner.prototype.dispatch = function( data ) {
+		for (var i=0;i<this.filters.length;i++) {
+			data = this.filters[i].filter(data);
+		}
+		for (var i=0;i<this.outputs.length;i++) {
+			this.outputs[i].handle(data);
+		}
+	}
+	JTR.TestRunner = TestRunner;
+	
+	
+	/**
+	 * CruiseControl xml report generator
+	 */
+	var ccXmlFilter = function() {}
+	ccXmlFilter.prototype.filter = function( data ) {
+		// convert json data to xml...
+		return xml;
+	}
+	JTR.filters.ccXml = ccXmlFilter;
+	
+	
+	/**
+	 * Firebug/console output
+	 */
+	var consoleOutput = function() {}
+	consoleOutput.prototype.handle = function( data ) {
+		console.log('JTR: testSuite result = ',data);
+	}
+	JTR.outputs.console = consoleOutput;
+	
+	/**
+	 * Selenium output
+	 */
+	var seleniumOutput = function() {}
+	seleniumOutput.prototype.handle = function( data ) {
+		
+	}
+	JTR.outputs.selenium = seleniumOutput;
+	
+	
+	/**
+	 * jsUnity framwork adapter
+	 */
+	var jsUnityFramework = function() {}
+	jsUnityFramework.prototype.run = function() {
+		var args = Array.prototype.slice.call(arguments,0);
+		
+		this.data = {
+			suites: {},
+			errors: 0
+		};
+		var oldLogger = jsUnity.logger;
+		jsUnity.logger = this;
+		jsUnity.run.apply(jsUnity,args);
+		jsUnity.logger = oldLogger;
+		
+		return this.data;
+	}
+	jsUnityFramework.prototype.findName = function( o, name ) {
+		if (!name) name = 'unnamed';
+		if (typeof o[name] == 'undefined')
+			return name;
+		for (var i=1;i<5000;i++) {
+			if (typeof o[name+'_'+i] == 'undefined') {
+				return name+'_'+i;
+			}
+		}
+		return 'unnamed';
+	}
+	jsUnityFramework.prototype.log = function( action, name, error ) {
+		switch(action) {
+		case 'suite-error':
+			this.data.errors++;
+			break;
+		case 'suite-start':
+			this.suiteName = this.findName(this.data.suites,name);
+			this.suiteStarted = new Data();
+			this.data.suites[name] = {
+				time: 0;
+				tests: {}
+			};
+			break;
+		case 'suite-end':
+			var suite = this.data.suites[this.suiteName];
+			suite.time = new Date() - this.suiteStarted; 
+			break;
+		case 'test-start':
+			var suite = this.data.suites[this.suiteName];
+			this.testName = this.findName(suite.tests,name);
+			this.testStarted = new Date();
+			suite.tests[this.testName] = {
+				time: 0,
+				status: 'unknown',
+				info: ''
+			};
+			break;
+		case 'test-success':
+			var test = this.data.suites[this.suiteName].tests[this.testName];
+			test.time = new Date() - this.testStarted;
+			test.status = 'success';
+			break;
+		case 'test-failure':
+			var test = this.data.suites[this.suiteName].tests[this.testName];
+			test.time = new Date() - this.testStarted;
+			test.status = 'failure';
+			test.info = "" + error;
+			break;
+		}
+	}
+	JTR.frameworks.jsUnity = jsUnityFramework;
+	
+	
+	/**
+	 * Mediawiki extension handler
+	 */
+	JTR.run = function() {
+		var filterNames = window.jtr_filters;
+		var outputNames = window.jtr_outputs;
+		var frameworkName = window.jtr_framework;
+		var testSuite = window.jtr_testsuite;
+		
+		/*
+		if (!outputNames || outputNames.length == 0) {
+			throw new Exception("No output specified for test suite");
+		}
+		*/
+		if (!frameworkName) {
+			throw new Exception("No framework name specified for test suite");
+		}
+		if (!testSuite) {
+			throw new Exception("No tests specified as a test suite");
+		}
+		
+		var filters = [], outputs = [];
+		for (var i in filterNames) {
+			if (typeof JTR.filters[filterNames[i]] != 'function') {
+				throw new Exception("Filter \""+filterNames[i]+"\" does not exist");
+			}
+			filters.push(new JTR.filters[filterNames[i]]);
+		}
+		for (var i in outputNames) {
+			if (typeof JTR.outputs[outputNames[i]] != 'function') {
+				throw new Exception("Filter \""+outputNames[i]+"\" does not exist");
+			}
+			outputs.push(new JTR.outputs[outputNames[i]]);
+		}
+		
+		var jtr = new JTR.TestRunner();
+		jtr.configure(filters,outputs);
+		jtr.run(frameworkName,testSuite);
+	}
+	JTR.autorun = function() {
+		if ( !window.document || window.document.readyState != "complete" ) {
+			setTimeout(JTR.autorun,500);
+			return;
+		}
+		JTR.run();
+	}
+	
+	
+	// Share UTI with the rest of world...
+	window.WikiaJavascriptTestRunner = JTR;
+
+})();
