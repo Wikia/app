17dd67fde7980ade1bcfcaf85dc2c8bb2edfd34c
wladek <wladek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-01-28 17:46:51 +0000

    added Special:JavascriptTestRunner
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32798 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/JavascriptTestRunner/js/tests/DummyTest.js
@@ -0,0 +1,16 @@
+var test = {
+	setUp: function () {
+		console.log('test: setUp()');
+	},
+    tearDown: function () {
+		console.log('test: tearDown()');
+	},
+	test1: function () {
+		console.log('test: test1()');
+	},
+	test2: function () {
+		console.log('test: test2()');
+	}
+}
+window.jtr_framework = "jsUnity";
+window.jtr_testsuite = test;
