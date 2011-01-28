c2f6173971d660613a7d18684ec027413536ff0a
wladek <wladek@wikia-inc.com>
2011-01-28 17:46:51 +0000

    added Special:JavascriptTestRunner
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32798 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/JavascriptTestRunner/JavascriptTestRunner.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Javascript unit test runner and integration with wikia's stack Selenium + CruiseControl
+ * 
+ * @ingroup Wikia
+ *
+ * @author Władysław Bodzek <wladek@wikia-inc.com>
+ * @copyright Copyright © 2011, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+if(!defined('MEDIAWIKI')) die("This is MediaWiki extension and cannot be used standalone.");
+
+$wgExtensionCredits['specialpage'][] = array(
+	'path'           => __FILE__,
+	'name'           => 'JavascriptUnitTest',
+	'author'         => array( 'Władysław Bodzek' ),
+	'url'            => '',
+	'description'    => 'Javascript test runner',
+	'descriptionmsg' => 'javascripttestrunner-desc',
+);
+
+$dir = dirname(__FILE__) . '/';
+
+
+//special pages
+$wgSpecialPages['JavascriptTestRunner'] = 'SpecialJavascriptTestRunner';
+$wgSpecialPageGroups['JavascriptTestRunner'] = 'wikia';
+
+//rights
+$wgAvailableRights[] = 'javascripttestrunner';
+$wgGroupPermissions['*']['javascripttestrunner'] = false;
+$wgGroupPermissions['staff']['javascripttestrunner'] = true;
+
+//internationalization files
+$wgExtensionMessagesFiles['JavascriptTestRunner'] = $dir . 'JavascriptTestRunner.i18n.php';
+//$wgExtensionAliasesFiles['JavascriptTestRunner'] = $dir . 'JavascriptTestRunner.alias.php';
+
+//classes
+$wgAutoloadClasses['SpecialJavascriptTestRunner'] = dirname( __FILE__ ) . '/SpecialJavascriptTestRunner.class.php';
+
