0f1936284a0ed6f6577b7609178ce367e677b890
jakub <jakub@wikia-inc.com>
2011-01-31 15:29:54 +0000

    BugId:1823
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32837 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SponsorshipDashboard/tests/SponsorshipDashboardServiceTest.php
+++ extensions/wikia/SponsorshipDashboard/tests/SponsorshipDashboardServiceTest.php
@@ -9,8 +9,8 @@ class SponsorshipDashboardServiceTest extends PHPUnit_Framework_TestCase {
 
 	private $app;
 
-	const SDS_SERVICE_TEST_SERVER_ID = 177;
-	const SDS_SERVICE_TEST_SERVER_URL = 'http://community.wikia.com';
+	const SDS_SERVICE_TEST_SERVER_ID = 673;
+	const SDS_SERVICE_TEST_SERVER_URL = 'http://simpsons.wikia.com';
 
 	protected function setUp() {
 		
@@ -22,6 +22,7 @@ class SponsorshipDashboardServiceTest extends PHPUnit_Framework_TestCase {
 	protected function tearDown() {
 
 		WF::setInstance( 'App', $this->app );
+		WF::reset( 'gapi' );
 	}
 
 	/**
@@ -30,16 +31,16 @@ class SponsorshipDashboardServiceTest extends PHPUnit_Framework_TestCase {
 	public function testLoadDataWithoutCache( $method ){
 
 		$app = $this->getMock( 'WikiaApp' );
-		$app->expects( $this->any() )
-		    ->method( 'getGlobal' )
-		    ->will( $this->returnCallback( array( $this, 'getGlobalsForLoadData' )));
+		$app	->expects( $this->any() )
+			->method( 'getGlobal' )
+			->will( $this->returnCallback( array( $this, 'getGlobalsForLoadData' )));
 
 		WF::setInstance( 'App', $app );
 
 		$SDS = $this->getMock('SponsorshipDashboardService', array( 'getFromCache' ));
-		$SDS->expects($this->any())
-		     ->method( 'getFromCache' )
-		     ->will( $this->returnValue( false ) );
+		$SDS	->expects($this->any())
+			->method( 'getFromCache' )
+			->will( $this->returnValue( false ) );
 
 		$return = $SDS->$method();
 
@@ -85,7 +86,7 @@ class SponsorshipDashboardServiceTest extends PHPUnit_Framework_TestCase {
 			case 'wgServer':
 				return self::SDS_SERVICE_TEST_SERVER_URL;
 			break;
-			case 'wgCityId': 
+			case 'wgCityId':
 				return self::SDS_SERVICE_TEST_SERVER_ID;
 			break;
 			default: $GLOBALS[$globalVar];
@@ -107,4 +108,116 @@ class SponsorshipDashboardServiceTest extends PHPUnit_Framework_TestCase {
 		    array( 'loadContentData' )
 		);
 	}
+
+	public function testGADailyDataEmpty(){
+
+		$app = $this->getMock( 'WikiaApp' );
+
+		WF::setInstance( 'App', $app );
+
+		$SDS = $this->getMock( 'SponsorshipDashboardService', array( 'getFromCache' ) );
+
+		$gapi = $this->getMock( 'gapi' , array(), array( 'email', 'pass', 'token' ));
+		$gapi	->expects( $this->any() )
+			->method( 'requestReportData' )
+			->will( $this->returnValue( false ) );
+
+		$gapi	->expects( $this->any() )
+			->method( 'getResults' )
+			->will( $this->returnValue( false ) );
+
+		WF::setInstance( 'gapi', $gapi );
+
+		$SDS->expects( $this->any() )
+			->method( 'getFromCache' )
+			->will( $this->returnValue( false ) );
+		
+		$return = $SDS->getDailyCityPageviewsFromGA( self::SDS_SERVICE_TEST_SERVER_URL, self::SDS_SERVICE_TEST_SERVER_ID, 'c', 0, true );
+		$this->assertEmpty( $return );
+		
+	}
+
+	public function testGADailyDataPositiveAfterRetries(){
+
+		$app = $this->getMock( 'WikiaApp' );
+
+		WF::setInstance( 'App', $app );
+
+		$SDS = $this->getMock( 'SponsorshipDashboardService', array( 'getFromCache' ) );
+
+		$fakeGapiResponse = array();
+		$fakeGapiObject = new fakeGapiResult;
+		$fakeGapiResponse[] = $fakeGapiObject;
+		$fakeGapiResponse[] = $fakeGapiObject;
+		$fakeGapiResponse[] = $fakeGapiObject;
+		$fakeGapiResponse[] = $fakeGapiObject;
+
+		$gapi = $this->getMock( 'gapi' , array(), array( 'email', 'pass', 'token' ));
+		$gapi	->expects( $this->any() )
+			->method( 'requestReportData' )
+			->will( $this->returnValue( false ) );
+
+		$gapi	->expects( $this->any() )
+			->method( 'getResults' )
+			->will( $this->onConsecutiveCalls(  
+				$this->throwException( new Exception('falseTestException') ),
+				$this->throwException( new Exception('falseTestException') ),
+				$this->throwException( new Exception('falseTestException') ),
+				$fakeGapiResponse ) );
+
+		WF::setInstance( 'gapi', $gapi );
+
+		$SDS->expects( $this->any() )
+			->method( 'getFromCache' )
+			->will( $this->returnValue( false ) );
+
+		$return = $SDS->getDailyCityPageviewsFromGA( self::SDS_SERVICE_TEST_SERVER_URL, self::SDS_SERVICE_TEST_SERVER_ID, 'c', 0, true );
+		$this->assertNotEmpty( $return );
+
+	}
+
+	public function testGADailyDataException(){
+
+		$app = $this->getMock( 'WikiaApp' );
+
+		WF::setInstance( 'App', $app );
+
+		$SDS = $this->getMock( 'SponsorshipDashboardService', array( 'getFromCache' ) );
+
+		$gapi = $this->getMock( 'gapi' , array(), array( 'email', 'pass', 'token' ));
+		$gapi	->expects( $this->any() )
+			->method( 'requestReportData' )
+			->will( $this->returnValue( false ) );
+		
+		$gapi	->expects( $this->any() )
+			->method( 'getResults' )
+			->will( $this->throwException( new Exception('falseTestException') ) );
+
+		WF::setInstance( 'gapi', $gapi );
+
+		$SDS->expects( $this->any() )
+			->method( 'getFromCache' )
+			->will( $this->returnValue( false ) );
+
+		$return = $SDS->getDailyCityPageviewsFromGA( self::SDS_SERVICE_TEST_SERVER_URL, self::SDS_SERVICE_TEST_SERVER_ID, 'c', 0, true );
+		$this->assertEmpty( $return );
+
+	}
+}
+
+class fakeGapiResult {
+
+	public function getMonth(){
+		return rand( 1, 12 );
+	}
+	public function getYear(){
+		return rand( 1999, date('Y') );
+	}
+	public function getDay(){
+		return rand( 1, 29 );
+	}
+	public function getPageviews(){
+		return rand( 0, 999999 );
+	}
+
 }
