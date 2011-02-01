39ff47c0034f58a109b806d5f12f490f255e7d55
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-01 17:18:36 +0000

    schema update hook
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32908 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/PageLayoutBuilder/PageLayoutBuilderHelper.class.php
+++ extensions/wikia/PageLayoutBuilder/PageLayoutBuilderHelper.class.php
@@ -6,7 +6,7 @@ class PageLayoutBuilderHelper {
 	 * @params Title
 	 * @return Parser
 	 */
-	
+
 	public static function rteIsCustom($name, $params, $frame, $wikitextIdx) {
 		global $wgPLBwidgets;
 		if (isset($wgPLBwidgets[$name])) {
@@ -14,8 +14,8 @@ class PageLayoutBuilderHelper {
 		}
 		return true;
 	}
-	
-	
+
+
 	static public function wikiFactoryChanged( $cv_name, $city_id, $value ) {
 		global $IP;
 		Wikia::log( __METHOD__, $city_id, "{$cv_name} = {$value}" );
@@ -23,16 +23,27 @@ class PageLayoutBuilderHelper {
 		if( $cv_name != 'wgEnablePageLayoutBuilder' ){
 			return true;
 		}
-		
+
 		$dbr = wfGetDB( DB_MASTER, array(), WikiFactory::IDtoDB($city_id) );
-	
+
 		if( !$dbr->tableExists( "plb_field" ) ){
 			$dbr->sourceFile( "$IP/extensions/wikia/PageLayoutBuilder/sql/plb_field.sql" );
 		}
-		
+
 		if( !$dbr->tableExists( "plb_page" ) ){
 			$dbr->sourceFile( "$IP/extensions/wikia/PageLayoutBuilder/sql/plb_page.sql" );
 		}
 		return true;
 	}
+
+	/**
+	 * create table with update.php
+	 */
+	static public function schemaUpdate() {
+		global $wgExtNewTables;
+		$wgExtNewTables[] = array( "plb_field", dirname(__FILE__) . "/sql/plb_field.sql" );
+		$wgExtNewTables[] = array( "plb_page", dirname(__FILE__) . "/sql/plb_page.sql" );
+		return true;
+	}
+
 }
