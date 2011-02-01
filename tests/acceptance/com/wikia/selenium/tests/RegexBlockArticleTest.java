91d701f43fb3b00e0e097a2a5c155ed0e75afe85
wojtek <wojtek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-01 16:34:30 +0000

    selenium tests fixes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32901 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/RegexBlockArticleTest.java
+++ tests/acceptance/com/wikia/selenium/tests/RegexBlockArticleTest.java
@@ -1,5 +1,6 @@
 package com.wikia.selenium.tests;
 
+import com.thoughtworks.selenium.SeleniumException;
 import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.session;
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
@@ -12,45 +13,65 @@ import java.util.Date;
 
 public class RegexBlockArticleTest extends BaseTest {
 
+	private boolean isRegexBlockEnabled() {
+		boolean isRegexBlockEnabled = true;
+		try {
+			session().open("index.php?title=Special:RegexBlock");
+		} catch (SeleniumException se) {
+			if (se.getMessage().contains("Response_Code = 404")) {
+				isRegexBlockEnabled = false;
+			}
+		}
+		return isRegexBlockEnabled;
+	}
+	
 	@Test(groups={"CI","uncyclopedia"})
 	public void testRegexBlockArticleSave() throws Exception {
 		loginAsRegular();
-		String content = (new Date()).toString();
-		editArticle("Project:WikiaBotAutomatedTest",
+		if (isRegexBlockEnabled()) {
+			String content = (new Date()).toString();
+			editArticle("Project:WikiaBotAutomatedTest",
 				"Regex Block Test. Tested: " + content + " \n");
-		assertFalse(session().isElementPresent("wpSave"));
+			assertFalse(session().isElementPresent("wpSave"));
+		}
 	}
 
 	@Test(groups={"CI","uncyclopedia"})
 	public void testRegexBlockArticleSaveUnmatching() throws Exception {
 		login();
-		String content = (new Date()).toString();
-		editArticle("Project:WikiaBotAutomatedTest",
+		if (isRegexBlockEnabled()) {
+			String content = (new Date()).toString();
+			editArticle("Project:WikiaBotAutomatedTest",
 				"Regex Block Test. Tested: " + content + " \n");
-		assertTrue(session().isElementPresent("wpSave"));
+			assertTrue(session().isElementPresent("wpSave"));
+		}
 	}
 
 	@Test(groups={"CI","uncyclopedia"})
 	public void testRegexBlockArticleAddFreshMatch() throws Exception {
 		loginAsStaff();
-		session().open("index.php?title=Special:RegexBlock");
-		session().type("wpRegexBlockedAddress", getTestConfig().getString("ci.user.wikiabot.username"));
-		session().check("wpRegexBlockedExact");
-		clickAndWait("wpRegexBlockedSubmit");
+		if (isRegexBlockEnabled()) {
+			session().open("index.php?title=Special:RegexBlock");
+			session().type("wpRegexBlockedAddress", getTestConfig().getString("ci.user.wikiabot.username"));
+			session().check("wpRegexBlockedExact");
+			clickAndWait("wpRegexBlockedSubmit");
 
-		login();
-		session().open("index.php?title=Project:WikiaBotAutomatedTest");
-		clickAndWait("//a[@data-id='edit']");
-		assertTrue(session().isElementPresent("wpSave"));
+			login();
+			session().open("index.php?title=Project:WikiaBotAutomatedTest");
+			clickAndWait("//a[@data-id='edit']");
+			assertTrue(session().isElementPresent("wpSave"));
+		}
 	}
 
 	@Test(groups={"CI","uncyclopedia"})
 	public void testRegexBlockArticleClearFreshMatch() throws Exception {
 		loginAsStaff();
-		String content = (new Date()).toString();
-		// session().open("index.php?title=Special:SpamRegex&action=delete&limit=50&offset=0&text=Sonoelementariakillus");
-		editArticle("Project:WikiaBotAutomatedTest",
+		if (isRegexBlockEnabled()) {
+			String content = (new Date()).toString();
+			// session().open("index.php?title=Special:SpamRegex&action=delete&limit=50&offset=0&text=Sonoelementariakillus");
+			editArticle("Project:WikiaBotAutomatedTest",
 				"Regex Block Test. Tested: " + content + " \n");
-		assertFalse(session().isElementPresent("spamprotected"));
+			assertFalse(session().isElementPresent("spamprotected"));
+		}
 	}
 }
