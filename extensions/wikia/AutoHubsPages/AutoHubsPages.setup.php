418bab63e9ddf05c0b5aaf763cd3698a8035b8a9
will <will@wikia-inc.com>
2011-02-01 23:00:33 +0000

    BugzID: 2152
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@32929 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutoHubsPages/AutoHubsPages.setup.php
+++ extensions/wikia/AutoHubsPages/AutoHubsPages.setup.php
@@ -33,43 +33,13 @@ $wgAjaxExportList[] = 'AutoHubsPagesHelper::hideFeed';
 //@TODO remove wfAdProviderDARTFirstChunkForHubs, duplicated in /extensions/wikia/AdEngine/AdConfig.js
 $wgHooks["AdProviderDARTFirstChunk"][] = "wfAdProviderDARTFirstChunkForHubs";
 function wfAdProviderDARTFirstChunkForHubs($first_chunk) {
-	global $wgTitle, $wgHubsPages;
+	global $wgTitle;
 
 	if( !AutoHubsPagesHelper::isHubsPage( $wgTitle ) ) {
 		return true;
 	}
 
-	// ANY CHANGE TO THESE HUBS MUST BE REPLICATED IN /extensions/wikia/AdEngine/AdConfig.js
-	switch ($wgTitle->getUserCaseDBKey()) {
-		case 'Entertainment':
-		case 'Movie':
-		case 'TV':
-		case 'Music':
-		case 'Animation':
-		case 'Anime':
-		case 'Sci-Fi':
-		case 'Horror':
-			$site = 'ent';
-			break;
-		case 'Gaming':
-		case 'PC_Games':
-		case 'Xbox_360_Games':
-		case 'PS3_Games':
-		case 'Wii_Games':
-		case 'Handheld_Games':
-		case 'Casual_Games':
-		case 'Mobile_Games':
-			$site = 'gaming';
-			break;
-		case 'Lifestyle':
-		case 'Recipes':
-			$site = 'life';
-			break;
-		default:
-			$site = 'wikia';
-	}
-
-	$first_chunk = 'wka.'.$site.'/_'.AutoHubsPagesHelper::getHubNameFromTitle($wgTitle).'/hub';
+	$first_chunk = 'wka.'.AutoHubsPagesHelper::getSiteForHub($wgTitle).'/_'.AutoHubsPagesHelper::getHubNameFromTitle($wgTitle).'/hub';
 
 	return true;
 }
