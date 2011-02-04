fee23d7f102b6ed11ed126ebc87501d823d787c4
marooned <marooned@wikia-inc.com>
2011-02-04 18:47:35 +0000

    new test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33082 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ tests/acceptance/com/wikia/selenium/tests/ScavengerHuntTest.java
@@ -0,0 +1,37 @@
+package com.wikia.selenium.tests;
+
+import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.session;
+import static org.testng.AssertJUnit.*;
+
+import org.testng.annotations.Test;
+
+public class MainPageTest extends BaseTest {
+	@Test(groups={"oasis", "CI"})
+	public void testAddGame() throws Exception {
+		DateFormat df = new SimpleDateFormat("yyyyMMDDHHmm");
+		String date = df.format(new Date());
+
+		session().open("index.php?title=Special:ScavengerHunt");
+		session().waitForPageToLoad(this.getTimeout());
+
+		// add button
+		assertTrue(session().isElementPresent("//form[@class='scavenger-form']//input[@name='add']"));
+		session().click("//form[@class='scavenger-form']//input[@name='add']");
+		session().waitForPageToLoad(this.getTimeout());
+
+//		waitForElement("//input[@id='landing']", this.getTimeout());
+		session().type("landing", "WikiaScavengerHuntStartingPage");
+		session().type("//textarea[@name='startingClue']", "starting clue " + date);
+		session().type("//input[@name='pageTitle[]']", "page title " + date);
+		session().type("//input[@name='hiddenImage[]']", "http://hidden.image." + date);
+		session().type("//input[@name='clueImage[]']", "http://clue.image." + date);
+		session().type("//textarea[@name='clue[]']", "clue " + date);
+		session().type("//textarea[@name='entryForm']", "entry form " + date);
+		session().type("//textarea[@name='finalQuestion']", "final question " + date);
+		session().type("//textarea[@name='goodbyeMsg']", "goodbye message " + date);
+		session().click("//form[@class='scavenger-form']//input[@name='save']");
+		session().waitForPageToLoad(this.getTimeout());
+
+		//check on the list if game has been created
+	}
+}
