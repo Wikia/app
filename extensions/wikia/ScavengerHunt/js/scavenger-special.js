606e089ff4efd86e0bea6299c5596de2418d5f70
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-04 16:35:36 +0000

    ScavengerHunt: work in progress: Create Add Hunt form (BugId: 2176)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33074 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ScavengerHunt/js/scavenger-special.js
+++ extensions/wikia/ScavengerHunt/js/scavenger-special.js
@@ -1,3 +1,33 @@
-$(function() {
-	$('#landing').focus();
-});
+var SpecialScavengerHunt = {
+	entry: null,
+
+	init: function() {
+		SpecialScavengerHunt.entry = $('.scavenger-entry').clone();
+		SpecialScavengerHunt.entry.find('input').val('');
+		$('#landing').focus();
+		$('.scavenger-form').delegate('.scavenger-page-title', 'blur', SpecialScavengerHunt.onPageTitleBlur);
+	},
+
+	// console logging
+	log: function(msg) {
+		$().log(msg, 'SpecialScavengerHunt');
+	},
+
+	onPageTitleBlur: function(e) {
+		var titles = $('.scavenger-page-title');
+		var count = titles.length;
+		SpecialScavengerHunt.log('count [1]:' + count);
+		titles.each(function(i,e) {
+			if ($(this).val()) {
+				count--;
+			}
+		});
+		SpecialScavengerHunt.log('count [2]:' + count);
+
+		if (!count) {
+			$('.scavenger-entry').last().after(SpecialScavengerHunt.entry.clone())
+		}
+	},
+};
+
+$(SpecialScavengerHunt.init);
