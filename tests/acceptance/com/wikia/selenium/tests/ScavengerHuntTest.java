e76715f470d2a98c295ac95682bcc5b15810f6fb
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-07 18:25:47 +0000

    ScavengerHunt: work in progress (BugId: 2252)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33142 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/ScavengerHuntTest.java
+++ tests/acceptance/com/wikia/selenium/tests/ScavengerHuntTest.java
@@ -25,30 +25,51 @@ import org.testng.annotations.Test;
 public class ScavengerHuntTest extends BaseTest {
 //	@Test(groups={"oasis", "CI"})
 	public void testAddGame() throws Exception {
+		// prepare some data
 		DateFormat df = new SimpleDateFormat("yyyyMMDDHHmm");
 		String date = df.format(new Date());
+		String gameName = "WikiaScavengerHuntGame " + date;
+		String landing = "Main Page";	//this title MUST exist
+		String startingClue = "starting clue " + date;
+		String pageTitle = "Main Page";	//this title MUST exist
+		String hiddenImage = "http://hidden.image." + date;
+		String clueImage = "http://clue.image." + date;
+		String clue = "clue " + date;
+		String entryForm = "entry form " + date;
+		String finalQuestion = "final question " + date;
+		String goodbyeMsg = "goodbye message " + date;
 
+		// go to the list
 		session().open("index.php?title=Special:ScavengerHunt");
 		session().waitForPageToLoad(this.getTimeout());
 
-		// add button
-		assertTrue(session().isElementPresent("//form[@class='scavenger-form']//input[@name='add']"));
-		session().click("//form[@class='scavenger-form']//input[@name='add']");
+		// click `add` button
+		assertTrue(session().isElementPresent("//form[@class='scavenger-form']//input[@class='scavenger-add']"));
+		session().click("//form[@class='scavenger-form']//input[@class='scavenger-add']");
 		session().waitForPageToLoad(this.getTimeout());
 
-//		waitForElement("//input[@id='landing']", this.getTimeout());
-		session().type("landing", "WikiaScavengerHuntStartingPage");
-		session().type("//textarea[@name='startingClue']", "starting clue " + date);
-		session().type("//input[@name='pageTitle[]']", "page title " + date);
-		session().type("//input[@name='hiddenImage[]']", "http://hidden.image." + date);
-		session().type("//input[@name='clueImage[]']", "http://clue.image." + date);
-		session().type("//textarea[@name='clue[]']", "clue " + date);
-		session().type("//textarea[@name='entryForm']", "entry form " + date);
-		session().type("//textarea[@name='finalQuestion']", "final question " + date);
-		session().type("//textarea[@name='goodbyeMsg']", "goodbye message " + date);
+		// fill in the form
+		session().type("gameName", gameName);
+		session().type("//input[@name='landing']", landing);
+		session().type("//textarea[@name='startingClue']", startingClue);
+		session().type("//input[@name='pageTitle[]']", pageTitle);
+		session().type("//input[@name='hiddenImage[]']", hiddenImage);
+		session().type("//input[@name='clueImage[]']", clueImage);
+		session().type("//textarea[@name='clue[]']", clue);
+		session().type("//textarea[@name='entryForm']", entryForm);
+		session().type("//textarea[@name='finalQuestion']", finalQuestion);
+		session().type("//textarea[@name='goodbyeMsg']", goodbyeMsg);
 		session().click("//form[@class='scavenger-form']//input[@name='save']");
 		session().waitForPageToLoad(this.getTimeout());
 
-		//check on the list if game has been created
+		//TODO: check on the list if game has been created
+
+		// go to edit form (TODO: click from the list)
+		session().open("index.php?title=Special:ScavengerHunt/edit/5");
+		session().waitForPageToLoad(this.getTimeout());
+		//TODO: FIX: getText is returning nothing
+		assertEquals(gameName, session().getText("gameName"));
+		assertEquals(landing, session().getText("//input[@name='landing']"));
+		//TODO: add other fields
 	}
 }
