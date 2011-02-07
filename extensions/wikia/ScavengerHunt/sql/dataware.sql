9ee3eca730153c9fae3a04ad6329b7559d40fa24
marooned <marooned@wikia-inc.com>
2011-02-07 16:24:50 +0000

    ScavengerHunt: work in progress
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33137 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ScavengerHunt/sql/dataware.sql
+++ extensions/wikia/ScavengerHunt/sql/dataware.sql
@@ -1,10 +1,5 @@
 -- DATABASE: DATAWARE
 
---TODO: add tables to dataware
---1) (game data) game ID, game data
---2) (game connection to articles) many-to-many: game ID <--> article ID
---3) (game results) user name, user ID (for logged in), e-mail, exrta data
-
 CREATE TABLE /*$wgDBprefix*/scavenger_hunt_games (
 	`game_id` int(10) NOT NULL auto_increment,
 	`wiki_id` int(10) NOT NULL,
@@ -23,4 +18,3 @@ CREATE TABLE /*$wgDBprefix*/scavenger_hunt_entries (
 	`entry_answer` varchar(255),
     PRIMARY KEY ( `entry_id` )
 ) ENGINE=InnoDB;
-
