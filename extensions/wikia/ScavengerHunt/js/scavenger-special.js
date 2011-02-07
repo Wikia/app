f7484003fc278ebf39dd1e7ef3becf95ee85d2a0
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-07 17:38:04 +0000

    ScavengerHunt: work in progress (BugId: 2247)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33141 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ScavengerHunt/js/scavenger-special.js
+++ extensions/wikia/ScavengerHunt/js/scavenger-special.js
@@ -2,10 +2,11 @@ var SpecialScavengerHunt = {
 	entry: null,
 
 	init: function() {
-		SpecialScavengerHunt.entry = $('.scavenger-entry').clone();
+		SpecialScavengerHunt.entry = $('.scavenger-entry').eq(0).clone();
 		SpecialScavengerHunt.entry.find('input').val('');
 		$('#gameName').focus();
 		$('.scavenger-form').delegate('.scavenger-page-title', 'blur', SpecialScavengerHunt.onPageTitleBlur);
+		$('input[name=delete]').bind('click.sumbit', SpecialScavengerHunt.onDeleteClick);
 	},
 
 	// console logging
@@ -16,18 +17,28 @@ var SpecialScavengerHunt = {
 	onPageTitleBlur: function(e) {
 		var titles = $('.scavenger-page-title');
 		var count = titles.length;
-		SpecialScavengerHunt.log('count [1]:' + count);
 		titles.each(function(i,e) {
 			if ($(this).val()) {
 				count--;
 			}
 		});
-		SpecialScavengerHunt.log('count [2]:' + count);
 
 		if (!count) {
 			$('.scavenger-entry').last().after(SpecialScavengerHunt.entry.clone())
 		}
 	},
+
+	onDeleteClick: function(e) {
+		e.preventDefault();
+		var button = $(this);
+		$.confirm({
+			//TODO: add i18n if this extension will be used outside staff group
+			content: 'Are you sure to delete this game?',
+			onOk: function() {
+				button.unbind('.sumbit').click();
+			}
+		});
+	}
 };
 
 $(SpecialScavengerHunt.init);
