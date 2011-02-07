82ce2d81b96ed704922369d142612339a9cb4b5f
sean <sean@wikia-inc.com>
2011-02-07 00:38:11 +0000

    Fixed a random FIXME that was in the code.  Changed the prefix of the messages in Special:Contact so that it wouldn't conflict w/messages in core (therefore we should be able to add it to TWN now).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33094 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialContact/SpecialContact.i18n.php
+++ extensions/wikia/SpecialContact/SpecialContact.i18n.php
@@ -3,7 +3,7 @@
  * Internationalisation file for SpecialContact extension.
  *
  * @addtogroup Extensions
- * @todo FIXME: Cannot add support for L10n in Translate because of key conflicts with MediaWiki SVN extension ContactPage.
+ * @todo FIXME: <s>Cannot add support for L10n in Translate because of key conflicts with MediaWiki SVN extension ContactPage.</s> Fixed. Is it ready for TWN now?
  */
 
 $messages = array();
@@ -18,39 +18,39 @@ $messages = array(
 	'en' => array(
 		/* special page */
 		'contact' => 'Contact Wikia',
-		'contactwikia' => 'Contact Wikia',
-		'contactpagetitle' => 'Contact Wikia',
+		'specialcontact-wikia' => 'Contact Wikia',
+		'specialcontact-pagetitle' => 'Contact Wikia',
 
 		/* form */
-		'contactintro' => 'You can contact [[w:project:Staff|Wikia Staff]] using this form. Admins of this wiki can be found [[Special:ListAdmins|here]].
+		'specialcontact-intro' => 'You can contact [[w:project:Staff|Wikia Staff]] using this form. Admins of this wiki can be found [[Special:ListAdmins|here]].
 
 Additional information on how to report problems to Wikia can be found [[w:project:Report_a_problem|here]],<br/>
 or you can post on [[w:Forum:Index|Wikia Community Forums]] for user support.
 
 If you prefer to use regular email or have attachments, you can contact us at [mailto:community@wikia.com community@wikia.com]',
-		'contactusername' => 'Username',
-		'contactwikiname' => 'Which wiki',
-		'contactrealname' => 'Your name',
-		'contactyourmail' => 'Email',
-		'contactproblem' => 'Subject',
-		'contactproblemdesc' => 'Message',
-		'contactmail' => 'Send to Wikia',
-		'contactfilledin' => 'This information has been filled in from your account preferences',
-		'contactccme' => 'Send me a copy of this message',
-		'contactccdisabled' => 'DISABLED: Please validate your email address to use this function',
-		'contactnotyou' => 'Not you?',
-		'contactcaptchainfo' => 'Please enter the text in the image.',
-		'contactcaptchatitle' => 'Blurry Word',
-		'contactformtitle' => 'Contact Wikia Support Staff',
+		'specialcontact-username' => 'Username',
+		'specialcontact-wikiname' => 'Which wiki',
+		'specialcontact-realname' => 'Your name',
+		'specialcontact-yourmail' => 'Email',
+		'specialcontact-problem' => 'Subject',
+		'specialcontact-problemdesc' => 'Message',
+		'specialcontact-mail' => 'Send to Wikia',
+		'specialcontact-filledin' => 'This information has been filled in from your account preferences',
+		'specialcontact-ccme' => 'Send me a copy of this message',
+		'specialcontact-ccdisabled' => 'DISABLED: Please validate your email address to use this function',
+		'specialcontact-notyou' => 'Not you?',
+		'specialcontact-captchainfo' => 'Please enter the text in the image.',
+		'specialcontact-captchatitle' => 'Blurry Word',
+		'specialcontact-formtitle' => 'Contact Wikia Support Staff',
 		/* errors */
-		'contactnomessage' => 'Please fill in a message',
-		'contactcaptchafail' =>	'Incorrect or missing confirmation code.', 
+		'specialcontact-nomessage' => 'Please fill in a message',
+		'specialcontact-captchafail' =>	'Incorrect or missing confirmation code.', 
 		/* email */
-		'contactmailsub' => 'Wikia Contact Mail',
-		'contactmailsubcc' => 'Copy of Wikia Contact Mail',
-		'contactccheader' => 'This is a copy of your message that was sent to Wikia Support',
+		'specialcontact-mailsub' => 'Wikia Contact Mail',
+		'specialcontact-mailsubcc' => 'Copy of Wikia Contact Mail',
+		'specialcontact-ccheader' => 'This is a copy of your message that was sent to Wikia Support',
 
 		/* after */
-		'contactsubmitcomplete' => 'Thank you for contacting Wikia.',
+		'specialcontact-submitcomplete' => 'Thank you for contacting Wikia.',
 	)
 );
