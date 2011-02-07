c1534a676ead2dcfc05e92c38131b793bf0296b9
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-07 16:18:05 +0000

    it is group not database
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33135 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/SiteStats.php
+++ includes/SiteStats.php
@@ -271,7 +271,7 @@ class SiteStatsInit {
 	 * @param $useMaster bool Whether to use the master db
 	 */
 	public function __construct( $useMaster = false ) {
-		$this->db = wfGetDB( $useMaster ? DB_MASTER : DB_SLAVE, array(), 'vslow' );
+		$this->db = wfGetDB( $useMaster ? DB_MASTER : DB_SLAVE, 'vslow' );
 	}
 
 	/**
