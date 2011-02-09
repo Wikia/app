50f1edc20c38212af64a12c7e86a42dbb2536f4f
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-09 13:10:53 +0000

    ScavengerHunt: fixed indentation
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33237 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ScavengerHunt/data/ScavengerHuntEntries.class.php
+++ extensions/wikia/ScavengerHunt/data/ScavengerHuntEntries.class.php
@@ -1,78 +1,76 @@
 <?php
+class ScavengerHuntEntries {
 
-	class ScavengerHuntEntries {
+	const ENTRIES_TABLE_NAME = "scavenger_hunt_entries";
 
-		const ENTRIES_TABLE_NAME = "scavenger_hunt_entries";
+	protected $app = null;
 
-		protected $app = null;
-
-		public function __construct( WikiaApp $app ) {
-			$this->app = $app;
-		}
-
-		/**
-		 * @return ScavengerHuntEntry
-		 */
-		public function newEntry() {
-			$entry = WF::build('ScavengerHuntEntry');
-			$entry->setEntries($this);
-			return $entry;
-		}
-
-		public function save( ScavengerHuntEntry $entry ) {
-			$fields = array(
-				'game_id' => $entry->getGameId(),
-				'user_id' => $entry->getUserId(),
-				'entry_name' => $entry->getName(),
-				'entry_email' => $entry->getEmail(),
-				'entry_answer' => $entry->getAnswer(),
-			);
-
-			$db = $this->getDb(DB_MASTER);
-			$db->insert(
-				self::ENTRIES_TABLE_NAME,
-				$fields,
-				__METHOD__
-			);
-			$db->commit();
-			return true;
-		}
-
-		public function findAllByGameId( $gameId ) {
-			$set = $db->select(
-				array( self::ENTRIES_TABLE_NAME ),
-				array( 'game_id', 'user_id', 'entry_name', 'entry_email', 'entry_answer' ),
-				array( 'game_id' => (int)$gameId ),
-				__METHOD__,
-				$options
-			);
-
-			$entries = array();
-			while( $row = $res->fetchObject( $res ) ) {
-				$entries[] = $this->newEntryFromRow($row);
-			}
-
-			return $entries;
-		}
-
-		/**
-		 * get db handler
-		 * @return DatabaseBase
-		 */
-		protected function getDb( $type = DB_SLAVE ) {
-			return $this->app->runFunction( 'wfGetDB', $type, array(), $this->app->getGlobal( 'wgExternalDatawareDB' ) );
-		}
-
-		protected function newEntryFromRow( $row ) {
-			$entry = $this->newEntry();
-
-			$entry->setGameId($row->game_id);
-			$entry->setUserId($row->user_id);
-			$entry->setName($row->entry_name);
-			$entry->setEmail($row->entry_email);
-			$entry->setAnswer($row->entry_answer);
-
-			return $entry;
-		}
-
+	public function __construct( WikiaApp $app ) {
+		$this->app = $app;
 	}
+
+	/**
+	 * @return ScavengerHuntEntry
+	 */
+	public function newEntry() {
+		$entry = WF::build('ScavengerHuntEntry');
+		$entry->setEntries($this);
+		return $entry;
+	}
+
+	public function save( ScavengerHuntEntry $entry ) {
+		$fields = array(
+			'game_id' => $entry->getGameId(),
+			'user_id' => $entry->getUserId(),
+			'entry_name' => $entry->getName(),
+			'entry_email' => $entry->getEmail(),
+			'entry_answer' => $entry->getAnswer(),
+		);
+
+		$db = $this->getDb(DB_MASTER);
+		$db->insert(
+			self::ENTRIES_TABLE_NAME,
+			$fields,
+			__METHOD__
+		);
+		$db->commit();
+		return true;
+	}
+
+	public function findAllByGameId( $gameId ) {
+		$set = $db->select(
+			array( self::ENTRIES_TABLE_NAME ),
+			array( 'game_id', 'user_id', 'entry_name', 'entry_email', 'entry_answer' ),
+			array( 'game_id' => (int)$gameId ),
+			__METHOD__,
+			$options
+		);
+
+		$entries = array();
+		while( $row = $res->fetchObject( $res ) ) {
+			$entries[] = $this->newEntryFromRow($row);
+		}
+
+		return $entries;
+	}
+
+	/**
+	 * get db handler
+	 * @return DatabaseBase
+	 */
+	protected function getDb( $type = DB_SLAVE ) {
+		return $this->app->runFunction( 'wfGetDB', $type, array(), $this->app->getGlobal( 'wgExternalDatawareDB' ) );
+	}
+
+	protected function newEntryFromRow( $row ) {
+		$entry = $this->newEntry();
+
+		$entry->setGameId($row->game_id);
+		$entry->setUserId($row->user_id);
+		$entry->setName($row->entry_name);
+		$entry->setEmail($row->entry_email);
+		$entry->setAnswer($row->entry_answer);
+
+		return $entry;
+	}
+}
--- extensions/wikia/ScavengerHunt/data/ScavengerHuntEntry.class.php
+++ extensions/wikia/ScavengerHunt/data/ScavengerHuntEntry.class.php
@@ -1,51 +1,49 @@
 <?php
+class ScavengerHuntEntry {
 
-	class ScavengerHuntEntry {
+	protected $gameId = 0;
+	protected $userId = 0;
+	protected $name = '';
+	protected $email = '';
+	protected $answer = '';
 
-		protected $gameId = 0;
-		protected $userId = 0;
-		protected $name = '';
-		protected $email = '';
-		protected $answer = '';
-
-		public function getGameId() {
-			return $this->gameId;
-		}
-
-		public function setGameId( $gameId ) {
-			$this->gameId = $gameId;
-		}
-
-		public function getUserId() {
-			return $this->userId;
-		}
-
-		public function setUserId( $userId ) {
-			$this->userId = $userId;
-		}
-
-		public function getName() {
-			return $this->name;
-		}
-
-		public function setName( $name ) {
-			$this->name = $name;
-		}
-
-		public function getEmail() {
-			return $this->email;
-		}
-
-		public function setEmail( $email ) {
-			$this->email = $email;
-		}
-
-		public function getAnswer() {
-			return $this->answer;
-		}
-
-		public function setAnswer( $answer ) {
-			$this->answer = $answer;
-		}
-
+	public function getGameId() {
+		return $this->gameId;
 	}
+
+	public function setGameId( $gameId ) {
+		$this->gameId = $gameId;
+	}
+
+	public function getUserId() {
+		return $this->userId;
+	}
+
+	public function setUserId( $userId ) {
+		$this->userId = $userId;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function setName( $name ) {
+		$this->name = $name;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function setEmail( $email ) {
+		$this->email = $email;
+	}
+
+	public function getAnswer() {
+		return $this->answer;
+	}
+
+	public function setAnswer( $answer ) {
+		$this->answer = $answer;
+	}
+}
