d102da483944c93f2be5e0052298120377389bb8
eloy <eloy@wikia-inc.com>
2011-02-11 10:08:08 +0000

    add ashburn to devcookie
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33350 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialDatacenter/SpecialDatacenter.php
+++ extensions/wikia/SpecialDatacenter/SpecialDatacenter.php
@@ -36,14 +36,19 @@ class SpecialDatacenter extends UnlistedSpecialPage {
 		if( $wgRequest->wasPosted() ) {
 			$val = $wgRequest->getVal( "iowacookie", 0 );
 			if( $val == 2 ) {
-				$wgOut->addHTML( Wikia::successbox( "cookie set" ) );
+				$wgOut->addHTML( Wikia::successbox( "cookie set to sjc" ) );
 				$wgRequest->response()->setcookie( $this->mCookieName, "sjc" );
 				$this->mCookie = "sjc";
 			}
 			elseif( $val == 1 ) {
-				$wgOut->addHTML( Wikia::successbox( "cookie set" ) );
+				$wgOut->addHTML( Wikia::successbox( "cookie set to iowa" ) );
 				$wgRequest->response()->setcookie( $this->mCookieName, "iowa" );
 				$this->mCookie = "iowa";
+			}
+			elseif( $val == 3 ) {
+				$wgOut->addHTML( Wikia::successbox( "cookie set to ash" ) );
+				$wgRequest->response()->setcookie( $this->mCookieName, "ash" );
+				$this->mCookie = "ash";
 			}
 			else {
 				$wgOut->addHTML( Wikia::successbox( "cookie removed" ) );
@@ -72,6 +77,7 @@ class SpecialDatacenter extends UnlistedSpecialPage {
 		$select = new XMLSelect( "iowacookie", "iowacookie" );
 		$select->addOption( "Switch to San Jose", 2 );
 		$select->addOption( "Switch to Iowa",     1 );
+		$select->addOption( "Switch to Ashburn",  3 );
 		$select->addOption( "Remove preferences", 0 );
 		$wgOut->addHTML( $select->getHTML() );
 		$wgOut->addHTML( Xml::submitButton( "submit" ) );
