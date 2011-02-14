a7d28064815efffd143c3f5bde0ad06b9e09da06
siebrand <siebrand@wikia-inc.com>
2011-02-14 08:39:07 +0000

    Normalise i18n file.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33405 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialContact/SpecialContact.i18n.php
+++ extensions/wikia/SpecialContact/SpecialContact.i18n.php
@@ -3,54 +3,44 @@
  * Internationalisation file for SpecialContact extension.
  *
  * @addtogroup Extensions
- * @todo FIXME: <s>Cannot add support for L10n in Translate because of key conflicts with MediaWiki SVN extension ContactPage.</s> Fixed. Is it ready for TWN now?
  */
 
 $messages = array();
 
-/**
- * Prepare extension messages
- *
- * @return array
- */
-$messages = array(
+$messages['en'] = array(
+	/* special page */
+	'contact' => 'Contact Wikia',
+	'specialcontact-wikia' => 'Contact Wikia',
+	'specialcontact-pagetitle' => 'Contact Wikia',
 
-	'en' => array(
-		/* special page */
-		'contact' => 'Contact Wikia',
-		'specialcontact-wikia' => 'Contact Wikia',
-		'specialcontact-pagetitle' => 'Contact Wikia',
+	/* form */
+	'specialcontact-intro' => 'You can contact [[w:project:Staff|Wikia Staff]] using this form. Admins of this wiki can be found [[Special:ListAdmins|here]].
 
-		/* form */
-		'specialcontact-intro' => 'You can contact [[w:project:Staff|Wikia Staff]] using this form. Admins of this wiki can be found [[Special:ListAdmins|here]].
+	Additional information on how to report problems to Wikia can be found [[w:project:Report_a_problem|here]], or you can post on [[w:Forum:Index|Wikia Community Forums]] for user support.
 
-Additional information on how to report problems to Wikia can be found [[w:project:Report_a_problem|here]],<br/>
-or you can post on [[w:Forum:Index|Wikia Community Forums]] for user support.
+If you prefer to use regular e-mail or have attachments, you can contact us at [mailto:community@wikia.com community@wikia.com]',
+	'specialcontact-username' => 'Username',
+	'specialcontact-wikiname' => 'Which wiki',
+	'specialcontact-realname' => 'Your name',
+	'specialcontact-yourmail' => 'E-mail address',
+	'specialcontact-problem' => 'Subject',
+	'specialcontact-problemdesc' => 'Message',
+	'specialcontact-mail' => 'Send to Wikia',
+	'specialcontact-filledin' => 'This information has been filled in from your account preferences',
+	'specialcontact-ccme' => 'Send me a copy of this message',
+	'specialcontact-ccdisabled' => 'DISABLED: Please validate your e-mail address to use this function',
+	'specialcontact-notyou' => 'Not you?',
+	'specialcontact-captchainfo' => 'Please enter the text in the image.',
+	'specialcontact-captchatitle' => 'Blurry Word',
+	'specialcontact-formtitle' => 'Contact Wikia Support Staff',
+	/* errors */
+	'specialcontact-nomessage' => 'Please fill in a message',
+	'specialcontact-captchafail' =>	'Incorrect or missing confirmation code.',
+	/* email */
+	'specialcontact-mailsub' => 'Wikia Contact Mail',
+	'specialcontact-mailsubcc' => 'Copy of Wikia Contact Mail',
+	'specialcontact-ccheader' => 'This is a copy of your message that was sent to Wikia Support',
 
-If you prefer to use regular email or have attachments, you can contact us at [mailto:community@wikia.com community@wikia.com]',
-		'specialcontact-username' => 'Username',
-		'specialcontact-wikiname' => 'Which wiki',
-		'specialcontact-realname' => 'Your name',
-		'specialcontact-yourmail' => 'Email',
-		'specialcontact-problem' => 'Subject',
-		'specialcontact-problemdesc' => 'Message',
-		'specialcontact-mail' => 'Send to Wikia',
-		'specialcontact-filledin' => 'This information has been filled in from your account preferences',
-		'specialcontact-ccme' => 'Send me a copy of this message',
-		'specialcontact-ccdisabled' => 'DISABLED: Please validate your email address to use this function',
-		'specialcontact-notyou' => 'Not you?',
-		'specialcontact-captchainfo' => 'Please enter the text in the image.',
-		'specialcontact-captchatitle' => 'Blurry Word',
-		'specialcontact-formtitle' => 'Contact Wikia Support Staff',
-		/* errors */
-		'specialcontact-nomessage' => 'Please fill in a message',
-		'specialcontact-captchafail' =>	'Incorrect or missing confirmation code.', 
-		/* email */
-		'specialcontact-mailsub' => 'Wikia Contact Mail',
-		'specialcontact-mailsubcc' => 'Copy of Wikia Contact Mail',
-		'specialcontact-ccheader' => 'This is a copy of your message that was sent to Wikia Support',
-
-		/* after */
-		'specialcontact-submitcomplete' => 'Thank you for contacting Wikia.',
-	)
+	/* after */
+	'specialcontact-submitcomplete' => 'Thank you for contacting Wikia.',
 );
