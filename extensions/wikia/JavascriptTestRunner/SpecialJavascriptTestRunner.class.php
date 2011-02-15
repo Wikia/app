87c8c8132a1fd9ba46b07471e9ef1047b74d07ca
wladek <wladek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-15 19:28:15 +0000

    add support for generating cruise-control xml in javascript test runner
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33456 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/JavascriptTestRunner/SpecialJavascriptTestRunner.class.php
+++ extensions/wikia/JavascriptTestRunner/SpecialJavascriptTestRunner.class.php
@@ -1,11 +1,11 @@
 <?php
 
 class SpecialJavascriptTestRunner extends SpecialPage {
-	
+
 	function __construct() {
 		parent::__construct( 'JavascriptTestRunner', 'javascripttestrunner', false );
 	}
-	
+
 	static public function createJavascriptList( $string ) {
 		$items = explode(',',$string);
 		foreach ($items as $k => $v) {
@@ -20,24 +20,25 @@ class SpecialJavascriptTestRunner extends SpecialPage {
 
 	function execute() {
 		global $wgRequest, $wgOut, $wgTitle, $wgUser, $wgServer, $wgScriptPath;
-		
+
 		$this->setHeaders();
-		
+
 		if( !$wgUser->isAllowed( 'javascripttestrunner' ) ) {
 			$wgOut->permissionRequired( 'javascripttestrunner' );
 			return;
 		}
-		
+
 		$testSuiteFile = $wgRequest->getVal('test','');
 		$autoRun = $wgRequest->getVal('autorun',true);
-		
+
 		if ( !empty($testSuiteFile) ) {
 			$basePath = $wgServer . $wgScriptPath;
 			$wgOut->addScript("<script type=\"text/javascript\" src=\"{$basePath}/extensions/wikia/JavascriptTestRunner/js/jsunity-0.6.js\"></script>");
+			$wgOut->addScript("<script type=\"text/javascript\" src=\"{$basePath}/extensions/wikia/JavascriptTestRunner/js/Xml.js\"></script>");
 			$wgOut->addScript("<script type=\"text/javascript\" src=\"{$basePath}/extensions/wikia/JavascriptTestRunner/js/JavascriptTestRunner.js\"></script>");
 			$wgOut->addScript("<script type=\"text/javascript\" src=\"{$basePath}/{$testSuiteFile}\"></script>");
-			
-			
+
+
 			$script = "\n";
 			$filters = $wgRequest->getVal('filter','');
 			$script .= "window.jtr_filters = ".self::createJavascriptList($filters).";\n";
@@ -45,11 +46,11 @@ class SpecialJavascriptTestRunner extends SpecialPage {
 			$script .= "window.jtr_outputs = ".self::createJavascriptList($outputs).";\n";
 			$script .= "window.WikiaJavascriptTestRunner.autorun();\n";
 			$wgOut->addInlineScript($script);
-			
+
 		} else {
 			$wgOut->addHTML("No test given to run.");
 		}
-		
+
     }
-    
+
 }
