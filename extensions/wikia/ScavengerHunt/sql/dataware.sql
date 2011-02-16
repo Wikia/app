74ad1870f2aae9411367395e6c98a9cc9684a37c
wladek <wladek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-16 20:46:00 +0000

    fixed sql create script for Scavenger Hunt
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33546 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ScavengerHunt/sql/dataware.sql
+++ extensions/wikia/ScavengerHunt/sql/dataware.sql
@@ -1,6 +1,6 @@
 -- DATABASE: DATAWARE
 
-CREATE TABLE /*$wgDBprefix*/scavenger_hunt_games (
+CREATE TABLE scavenger_hunt_games (
 	`game_id` int(10) NOT NULL auto_increment,
 	`wiki_id` int(10) NOT NULL,
 	`game_name` varchar(255) NOT NULL,
@@ -8,8 +8,10 @@ CREATE TABLE /*$wgDBprefix*/scavenger_hunt_games (
 	`game_data` blob,
     PRIMARY KEY ( `game_id` )
 ) ENGINE=InnoDB;
+CREATE INDEX scavenger_hunt_games_for_list 
+	ON scavenger_hunt_games(wiki_id);
 
-CREATE TABLE /*$wgDBprefix*/scavenger_hunt_entries (
+CREATE TABLE scavenger_hunt_entries (
 	`entry_id` int(10) NOT NULL auto_increment,
 	`game_id` int(10) NOT NULL,
 	`user_id` int(10),
@@ -18,3 +20,5 @@ CREATE TABLE /*$wgDBprefix*/scavenger_hunt_entries (
 	`entry_answer` varchar(255),
     PRIMARY KEY ( `entry_id` )
 ) ENGINE=InnoDB;
+CREATE INDEX scavenger_hunt_entries_by_game
+	ON scavenger_hunt_entries(game_id,entry_id);
