e7cb721f3eb04f24329b8a11bb38bb4495c82be8
macbre <macbre@wikia-inc.com>
2011-02-16 11:19:15 +0000

    MinifyService: add benchmarking code and Closure to compare with YUI Compressor
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33504 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/MinifyServiceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * This script is used for benchmarking different CSS/JS minifiers
+ *
+ * @author macbre
+ */
+
+error_reporting(E_ALL);
+require_once(dirname(__FILE__)."/../commandLine.inc");
+
+$staticChute = new StaticChute('js');
+
+// get JS/CSS code from StaticChute
+$package = 'oasis_anon_article_js';
+$files = $staticChute->getFileList(array('packages' => $package));
+$count = count($files);
+
+echo "Got {$count} files from '{$package}' package\n\n";
+
+// concat those files
+$code = MinifyService::mergeFiles($files);
+$size = strlen($code);
+
+echo 'Original code size: ' . round($size / 1024, 2) . " kB\n";
+
+// run the minifier and measure the time
+$time = wfTime();
+$minified = MinifyService::minifyJS($code);
+$time = wfTime() - $time;
+
+$minifiedSize = strlen($minified);
+
+// run gzip
+$gzippedSize = strlen(gzcompress($minified));
+
+// results
+echo 'Merged code size:   ' . round($minifiedSize / 1024, 2). " kB\n";
+echo 'Gzipped code size:  ' . round($gzippedSize / 1024, 2). " kB\n";
+
+echo "\nMinified in " . round($time * 1000) . " ms\n\n";
+
+$ratio = round($size / $minifiedSize, 2);
+$ratioGzipped = round($size / $gzippedSize, 2);
+
+echo "Compression ratio:        {$ratio}x\n";
+echo "Compression ratio (gzip): {$ratioGzipped}x\n";
+
+// debug
+/**
+file_put_contents('code.js', $code);
+file_put_contents('code-min.js', $minified);
+**/
