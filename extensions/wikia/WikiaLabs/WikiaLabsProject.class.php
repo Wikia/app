dd58699adb7b44eafb12f2dafabafee86247bfe0
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-17 12:12:35 +0000

    rating as number of votes (BugId: 1945)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33574 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaLabs/WikiaLabsProject.class.php
+++ extensions/wikia/WikiaLabs/WikiaLabsProject.class.php
@@ -404,7 +404,9 @@ class WikiaLabsProject {
 			}
 			$this->updateCachedRating( $userId, $rating );
 
-			$row = $this->getDb( DB_MASTER )->selectRow( 'wikia_labs_project_rating', array( 'SUM(wlpra_value)/COUNT(*) AS rating' ), array( 'wlpra_wlpr_id' => $this->getId() ) );
+			//FB: #1945
+			//$row = $this->getDb( DB_MASTER )->selectRow( 'wikia_labs_project_rating', array( 'SUM(wlpra_value)/COUNT(*) AS rating' ), array( 'wlpra_wlpr_id' => $this->getId() ) );
+			$row = $this->getDb( DB_MASTER )->selectRow( 'wikia_labs_project_rating', array( 'COUNT(*) AS rating' ), array( 'wlpra_wlpr_id' => $this->getId() ) );
 
 			$this->setRating( $row->rating );
 			$this->update();
