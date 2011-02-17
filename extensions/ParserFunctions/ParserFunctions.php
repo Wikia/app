cc952c9095ad9a6287fe4eb722fded12c00da053
tor <tor@wikia-inc.com>
2011-02-17 18:20:23 +0000

    upgraded ParserFunctions to newest version FogBugz:2242
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33613 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/ParserFunctions/ParserFunctions.php
+++ extensions/ParserFunctions/ParserFunctions.php
@@ -4,8 +4,9 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
+
 /**
- * CONFIGURATION
+ * CONFIGURATION 
  * These variables may be overridden in LocalSettings.php after you include the
  * extension file.
  */
@@ -19,12 +20,12 @@ $wgPFStringLengthLimit = 1000;
 /**
  * Enable string functions.
  *
- * Set this to true if you want your users to be able to implement their own
- * parsers in the ugliest, most inefficient programming language known to man:
+ * Set this to true if you want your users to be able to implement their own 
+ * parsers in the ugliest, most inefficient programming language known to man: 
  * MediaWiki wikitext with ParserFunctions.
  *
  * WARNING: enabling this may have an adverse impact on the sanity of your users.
- * An alternative, saner solution for embedding complex text processing in
+ * An alternative, saner solution for embedding complex text processing in 
  * MediaWiki templates can be found at: http://www.mediawiki.org/wiki/Extension:Lua
  */
 $wgPFEnableStringFunctions = false;
@@ -34,18 +35,18 @@ $wgExtensionFunctions[] = 'wfSetupParserFunctions';
 $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'ParserFunctions',
-	'version' => '1.4.0',
+	'version' => '1.3.0',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:ParserFunctions',
-	'author' => array( 'Tim Starling', 'Robert Rohde', 'Ross McClure', 'Juraj Simlovic' ),
+	'author' => array('Tim Starling', 'Robert Rohde', 'Ross McClure', 'Juraj Simlovic'),
+	'description' => 'Enhance parser with logical functions',
 	'descriptionmsg' => 'pfunc_desc',
 );
 
-$wgAutoloadClasses['ExtParserFunctions'] = dirname( __FILE__ ) . '/ParserFunctions_body.php';
-$wgExtensionMessagesFiles['ParserFunctions'] = dirname( __FILE__ ) . '/ParserFunctions.i18n.php';
-$wgExtensionMessagesFiles['ParserFunctionsMagic'] = dirname( __FILE__ ) . '/ParserFunctions.i18n.magic.php';
+$wgAutoloadClasses['ExtParserFunctions'] = dirname(__FILE__).'/ParserFunctions_body.php';
+$wgExtensionMessagesFiles['ParserFunctions'] = dirname(__FILE__) . '/ParserFunctions.i18n.php';
+$wgExtensionMessagesFiles['ParserFunctionsMagic'] = dirname(__FILE__) . '/ParserFunctions.i18n.magic.php';
 
 $wgParserTestFiles[] = dirname( __FILE__ ) . "/funcsParserTests.txt";
-$wgParserTestFiles[] = dirname( __FILE__ ) . "/stringFunctionTests.txt";
 
 function wfSetupParserFunctions() {
 	global $wgPFHookStub, $wgHooks;
@@ -64,7 +65,7 @@ function wfSetupParserFunctions() {
 class ParserFunctions_HookStub {
 	var $realObj;
 
-	function registerParser( $parser ) {
+	function registerParser( &$parser ) {
 		global $wgPFEnableStringFunctions;
 
 		if ( defined( get_class( $parser ) . '::SFH_OBJECT_ARGS' ) ) {
@@ -90,23 +91,22 @@ class ParserFunctions_HookStub {
 		$parser->setFunctionHook( 'rel2abs', array( &$this, 'rel2abs' ) );
 		$parser->setFunctionHook( 'titleparts', array( &$this, 'titleparts' ) );
 
-		// String Functions
+		//String Functions
 		if ( $wgPFEnableStringFunctions ) {
-			$parser->setFunctionHook( 'len',       array( &$this, 'runLen'       ) );
-			$parser->setFunctionHook( 'pos',       array( &$this, 'runPos'       ) );
-			$parser->setFunctionHook( 'rpos',      array( &$this, 'runRPos'      ) );
-			$parser->setFunctionHook( 'sub',       array( &$this, 'runSub'       ) );
-			$parser->setFunctionHook( 'count',     array( &$this, 'runCount'     ) );
-			$parser->setFunctionHook( 'replace',   array( &$this, 'runReplace'   ) );
-			$parser->setFunctionHook( 'explode',   array( &$this, 'runExplode'   ) );
-			$parser->setFunctionHook( 'urldecode', array( &$this, 'runUrlDecode' ) );
+			$parser->setFunctionHook( 'len',      array(&$this, 'runLen'      ));
+			$parser->setFunctionHook( 'pos',      array(&$this, 'runPos'      ));
+			$parser->setFunctionHook( 'rpos',     array(&$this, 'runRPos'     ));
+			$parser->setFunctionHook( 'sub',      array(&$this, 'runSub'      ));
+			$parser->setFunctionHook( 'count',    array(&$this, 'runCount'    ));
+			$parser->setFunctionHook( 'replace',  array(&$this, 'runReplace'  ));
+			$parser->setFunctionHook( 'explode',  array(&$this, 'runExplode'  ));
 		}
 
 		return true;
 	}
 
 	/** Defer ParserClearState */
-	function clearState( $parser ) {
+	function clearState( &$parser ) {
 		if ( !is_null( $this->realObj ) ) {
 			$this->realObj->clearState( $parser );
 		}
