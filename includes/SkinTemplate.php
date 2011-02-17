Wikia/app

commit 26819e2c24e7d3477198786ac387f1067f0df240

fixed new talk notification post-1.16 BugzId:2064
git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33582 e846bd91-c042-42a7-a218-4b65e2c1904d

includes/SkinTemplate.php
@@ -344,7 +344,10 @@ class SkinTemplate extends Skin {
 		$newtalks = $wgUser->getNewMessageLinks();
 		$ntl = '';
 
-		if( count( $newtalks ) == 1 && $newtalks[0]['wiki'] === wfWikiID() ) {
+		/* Wikia change begin - @author: tor */
+		global $wgSitename;
+		if( count( $newtalks ) == 1 && $newtalks[0]['wiki'] === $wgSitename ) {
+		/* Wikia change end */
 			$usertitle = $this->mUser->getUserPage();
 			$usertalktitle = $usertitle->getTalkPage();
 

commit 359a086bc501c398f258a28352b61a9557f416ae

fixed regression in r33582 -- this needs cleanup :)
git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33587 e846bd91-c042-42a7-a218-4b65e2c1904d

includes/SkinTemplate.php
@@ -367,6 +367,7 @@ class SkinTemplate extends Skin {
 					)
 				);
 
+				/* Wikia change begin - @author: tor
 				$newmessagesdifflink = $this->link(
 					$usertalktitle,
 					wfMsgHtml( 'newmessagesdifflink' ),
@@ -380,6 +381,7 @@ class SkinTemplate extends Skin {
 					$newmessageslink,
 					$newmessagesdifflink
 				);
+				  Wikia change end */
 				# Disable Cache
 				$out->setSquidMaxage( 0 );
 			}