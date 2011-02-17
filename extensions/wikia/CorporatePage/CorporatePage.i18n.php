4c8d93febbbdb9103e8f9bfab0d042f9237819af
siebrand <siebrand@wikia-inc.com>
2011-02-17 10:05:35 +0000

    Remove superfluous whitespace.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33566 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CorporatePage/CorporatePage.i18n.php
+++ extensions/wikia/CorporatePage/CorporatePage.i18n.php
@@ -13,11 +13,9 @@ $messages['en'] = array(
 	'corporatepage-facebook-link' => 'http://www.facebook.com/wikia',
 	'corporatepage-wikia-blog-link' => 'http://community.wikia.com/wiki/Blog:Wikia_Staff_Blog',
 	'corporatepage-twitter-link' => 'http://www.twitter.com/wikia',
-	
+
 	'corporatepage-homecontent-headline' => 'Get Started Today',
 	'corporatepage-homecontent-intro' => 'Create a wiki about your favorite topic and begin collaborating with people who love what you love.',
 	'corporatepage-homecontent-button' => 'Start a wiki now',
 	'corportaepage-sales-support-edit' => 'Edit this element >>'
-
-
 );
