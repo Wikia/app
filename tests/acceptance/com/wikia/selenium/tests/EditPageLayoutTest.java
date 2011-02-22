3284792204de423ae1fbfc9a20b69c10f7bf0a93
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-22 11:12:33 +0000

    RTE reskin: more asserts in selenium test (BugId:2797)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33732 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/EditPageLayoutTest.java
+++ tests/acceptance/com/wikia/selenium/tests/EditPageLayoutTest.java
@@ -2,7 +2,7 @@ package com.wikia.selenium.tests;
 
 import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.session;
 import static org.testng.AssertJUnit.assertTrue;
-import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertEquals;
 
 import org.testng.annotations.Test;
 
@@ -20,13 +20,16 @@ public class EditPageLayoutTest extends BaseTest {
 		// get title of edit page
 		String title = session().getEval("window.wgPageName");
 
+		assertEquals(session().getEval("window.wgIsEditPage"), "true");
+
 		assertTrue(session().isElementPresent("//form[@id='editform']"));
 		assertTrue(session().isElementPresent("//*[@id='cke_wpTextbox1']"));
 
+		assertTrue(session().isElementPresent("//span[contains(@class,'wordmark')]"));
 		assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1[contains(text(), 'Editing')]"));
 		//assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1[contains(text(), '" + title + "')]"));
 
-		assertTrue(session().isElementPresent("//td[@id='cke_top_wpTextbox1']"));
+		assertTrue(session().isElementPresent("//td[@id='cke_toolbar_wpTextbox1']"));
 		assertTrue(session().isElementPresent("//td[@id='cke_rail_wpTextbox1']"));
 		assertTrue(session().isElementPresent("//td[@id='cke_contents_wpTextbox1']"));
 	}
