f2fc135065c1f45740ef81550226554912b289c0
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-22 12:47:05 +0000

    CorporateFooterTest: update the test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33735 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/CorporateFooterTest.java
+++ tests/acceptance/com/wikia/selenium/tests/CorporateFooterTest.java
@@ -7,14 +7,24 @@ import org.testng.annotations.Test;
 
 public class CorporateFooterTest extends BaseTest {
 	@Test(groups={"oasis", "CI"})
-	public void testLatestPhotos() throws Exception {
-		loginAsRegular();
+	public void testFooterLinks() throws Exception {
 		session().open("/");
 		session().waitForPageToLoad(this.getTimeout());
-	
+
 		// Coporate Footer Module
 		assertTrue(session().isElementPresent("//footer[@class='CorporateFooter']"));
-		// checking for a random link
-		assertTrue(session().isElementPresent("//footer[@class='CorporateFooter']/nav/ul/li[4]"));
+
+		// checking for "Random Wiki" link
+		assertTrue(session().isElementPresent("//footer[@id='WikiaFooter']//a[@id='WikiaRandomWiki']"));
+
+		// checking for spotlights
+		assertTrue(session().isElementPresent("//footer[@class='WikiaFooter']//ul[@id='SPOTLIGHT_FOOTER']/li"));
+		assertTrue(session().isElementPresent("//footer[@class='WikiaFooter']//ul[@id='SPOTLIGHT_FOOTER']/li/div[contains(@id,'beacon_')]"));
+
+		// checking for hub branding
+		assertTrue(session().isElementPresent("//footer[@class='CorporateFooter']/nav/div[@class='WikiaHubBranding']"));
+
+		// checking for corporate nav
+		assertTrue(session().isElementPresent("//footer[@class='CorporateFooter']/nav/ul"));
 	}
 }
