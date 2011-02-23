bdf912a90edfef27dafd485693454f0f56d4d473
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-23 17:00:39 +0000

    spaces!! *gulp*
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33797 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AutomaticWikiAdoption/maintenance/AutomaticWikiAdoptionGatherDataMapper.php
+++ extensions/wikia/AutomaticWikiAdoption/maintenance/AutomaticWikiAdoptionGatherDataMapper.php
@@ -34,18 +34,18 @@ class AutomaticWikiAdoptionGatherDataMapper {
 		//get wikis with pages < 1000 and admins not active in last 14 days but wit at least 1 edit
 		//194785 = ID of wiki created on 2010-12-14 so it will work for wikis created after this project has been deployed
 		$res = $dbrStats->query(
-			'select e1.wiki_id, sum(e1.edits) as sum_edits from specials.events_local_users e1' .
-			'where e1.wiki_id > 194785' .
-			'group by e1.wiki_id' .
+			'select e1.wiki_id, sum(e1.edits) as sum_edits from specials.events_local_users e1 ' .
+			'where e1.wiki_id > 194785 ' .
+			'group by e1.wiki_id ' .
 			'having sum_edits < 1000 and (' .
-			'select count(0) from specials.events_local_users e2' .
-			'where e1.wiki_id = e2.wiki_id and' .
-			'all_groups like "%sysop%" and' .
-			'editdate > now() - interval 14 day' .
+			'select count(0) from specials.events_local_users e2 ' .
+			'where e1.wiki_id = e2.wiki_id and ' .
+			'all_groups like "%sysop%" and ' .
+			'editdate > now() - interval 14 day ' .
 			') = 0 and (' .
-			'select count(*) from specials.events_local_users e3' .
-			'where e1.wiki_id = e3.wiki_id and' .
-			'all_groups like "%sysop%" and' .
+			'select count(*) from specials.events_local_users e3 ' .
+			'where e1.wiki_id = e3.wiki_id and ' .
+			'all_groups like "%sysop%" and ' .
 			'edits > 0' .
 			') > 0',
 			__METHOD__
