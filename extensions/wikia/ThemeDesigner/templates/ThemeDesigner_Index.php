e34fb7601d184fd98f0083041f8701455f3cbe33
marooned <marooned@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-02-24 13:45:59 +0000

    wgServer is not the right way to build URL to an article
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@33855 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/templates/ThemeDesigner_Index.php
+++ extensions/wikia/ThemeDesigner/templates/ThemeDesigner_Index.php
@@ -83,7 +83,7 @@
 
 	<?= wfRenderModule('ThemeDesigner', 'Picker') ?>
 
-	<iframe frameborder=0 id="PreviewFrame" class="PreviewFrame" src="<?= $wgServer ?>/wiki/Special:ThemeDesignerPreview"></iframe>
+	<iframe frameborder=0 id="PreviewFrame" class="PreviewFrame" src="<?= str_replace('$1', 'Special:ThemeDesignerPreview', $wgArticlePath) ?>"></iframe>
 
 <?= $analytics ?>
 
