346605b9380f957b1dc03e0699adf0a75bb2bec8
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-01 15:33:10 +0000

    WF:: -> F::
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34099 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/DummyExtension/DummyExtension.php
+++ extensions/wikia/DummyExtension/DummyExtension.php
@@ -11,23 +11,23 @@ $dir = dirname(__FILE__) . '/';
 /**
  * classes
  */
-WF::build('App')->registerClass('DummyExtension', $dir . 'DummyExtension.class.php');
-WF::build('App')->registerClass('DummyExtensionSpecialPageController', $dir . 'DummyExtensionSpecialPageController.class.php');
+F::build('App')->registerClass('DummyExtension', $dir . 'DummyExtension.class.php');
+F::build('App')->registerClass('DummyExtensionSpecialPageController', $dir . 'DummyExtensionSpecialPageController.class.php');
 
 /**
  * hooks
  */
-WF::build('App')->registerHook('OutputPageBeforeHTML', 'DummyExtension', 'onOutputPageBeforeHTML', array( 'foo' => 1, 'bar' => null ) );
+F::build('App')->registerHook('OutputPageBeforeHTML', 'DummyExtension', 'onOutputPageBeforeHTML', array( 'foo' => 1, 'bar' => null ) );
 
 /**
  * controllers
  */
-WF::build('App')->registerClass('DummyExtensionController', $dir . 'DummyExtensionController.class.php');
+F::build('App')->registerClass('DummyExtensionController', $dir . 'DummyExtensionController.class.php');
 
 /**
  * special pages
  */
-WF::build('App')->registerSpecialPage('DummyExtension', 'DummyExtensionSpecialPageController');
+F::build('App')->registerSpecialPage('DummyExtension', 'DummyExtensionSpecialPageController');
 
 /**
  * setup functions
