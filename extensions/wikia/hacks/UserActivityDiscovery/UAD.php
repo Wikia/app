4ce411b188262a22e2e214dd47adae036ef1f9ae
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-04 14:01:58 +0000

    UAD: work in progress
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34241 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/hacks/UserActivityDiscovery/UAD.php
+++ extensions/wikia/hacks/UserActivityDiscovery/UAD.php
@@ -11,7 +11,9 @@ $app = F::build('App');
 $app->registerClass('UAD', $dir . 'UAD.class.php');
 $app->registerClass('UADController', $dir . 'UADController.class.php');
 
+F::addClassConstructor( 'UAD', array( 'app' => $app ) );
 F::addClassConstructor( 'UADController', array( 'app' => $app, 'uad' => F::build( 'UAD' ) ) );
+
 
 /**
  * hooks
