5528c272903abd12db45b6c24336da0ffb34837a
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-04 16:16:27 +0000

    added DE namespaces bugid:3352
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34245 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/TopLists/TopLists.namespaces.php
+++ extensions/wikia/TopLists/TopLists.namespaces.php
@@ -9,3 +9,8 @@ $namespaces['en'] = array(
 	NS_TOPLIST => 'Top_10_list',
 	NS_TOPLIST_TALK => 'Top_10_list_talk'
 );
+
+$namespaces['de'] = array(
+	NS_TOPLIST => 'Top_10-Liste',
+	NS_TOPLIST_TALK => 'Top_10-Liste_Diskussion',
+);
