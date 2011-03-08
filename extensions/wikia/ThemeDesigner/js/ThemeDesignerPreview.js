c843c907b2d581425777798dc2ee922912efbaf9
marooned <marooned@wikia-inc.com>
2011-03-08 11:32:24 +0000

    DO NOT create paths starting with slash!
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34327 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/js/ThemeDesignerPreview.js
+++ extensions/wikia/ThemeDesigner/js/ThemeDesignerPreview.js
@@ -10,10 +10,10 @@ var ThemeDesignerPreview = {
 			.find("a")
 			.first()
 			.html(
-				'<img width="300" src="/extensions/wikia/ThemeDesigner/images/aquarium.jpg">'
+				'<img width="300" src="' + wgExtensionsPath + '/wikia/ThemeDesigner/images/aquarium.jpg">'
 			);	
 			
-		$("#WikiaArticle .thumbinner").append('<div class="picture-attribution"><img width="16" height="16" class="avatar" src="/extensions/wikia/ThemeDesigner/images/td-avatar.jpg">Added by <a>FunnyBunny</a></div>');
+		$("#WikiaArticle .thumbinner").append('<div class="picture-attribution"><img width="16" height="16" class="avatar" src="' + wgExtensionsPath + '/wikia/ThemeDesigner/images/td-avatar.jpg">Added by <a>FunnyBunny</a></div>');
 		$("a.new").removeClass("new");
 		
 		//no floating footer on preview
