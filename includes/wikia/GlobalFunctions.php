48688d38fd270893d92524da55240f5cc30dd505
inez <inez@wikia-inc.com>
2011-03-14 23:02:39 +0000

    * Add startsWith and endsWith functions
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34606 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/GlobalFunctions.php
+++ includes/wikia/GlobalFunctions.php
@@ -1442,3 +1442,17 @@ function wfDBReadOnlyFailed( ) {
 	$wgOut->clearHTML();
 	exit;
 }
+
+function startsWith($haystack, $needle, $case = true) {
+	if($case){
+		return (strcmp(substr($haystack, 0, strlen($needle)),$needle)===0);
+	}
+	return (strcasecmp(substr($haystack, 0, strlen($needle)),$needle)===0);
+}
+
+function endsWith($haystack, $needle, $case = true) {
+	if($case){
+		return (strcmp(substr($haystack, strlen($haystack) - strlen($needle)),$needle)===0);
+	}
+	return (strcasecmp(substr($haystack, strlen($haystack) - strlen($needle)),$needle)===0);
+}
