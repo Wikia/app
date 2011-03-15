aa6a2d7e889589398ecfabe96514ad788edb6c86
tomek <tomek@wikia-inc.com>
2011-03-15 20:19:15 +0000

    validators example code
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34653 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WVE/templates/simple.tmpl.php
@@ -0,0 +1,10 @@
+<form method="post" >
+	Name: <br>
+	<input name="form[name]" /> <br>
+	age: <br>
+	<input name="form[age]" /> <br>
+	email: <br>
+	<input name="form[mail]" /> <br>
+	
+	<input type="submit"">
+</form>
