bd33fbd0026aaabff43daa709d2cb7cae0f7dee1
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-17 15:59:18 +0000

    Add extension credits with translatable description.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34808 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat/Chat.i18n.php
+++ extensions/wikia/Chat/Chat.i18n.php
@@ -3,14 +3,15 @@
 $messages = array();
 
 $messages['en'] = array(
-	'chat-rail-buttontext' => "Join chat!",
-	'chat-no-login' => "You must be logged in to chat",
-	'chat-no-login-text' => "Please login to chat.",
-	'chat-default-topic' => "Welcome to the $1 chat",
+	'chat-desc' => '[[Special:Chat|Live chat]]',
+	'chat-rail-buttontext' => 'Join chat!',
+	'chat-no-login' => 'You must be logged in to chat',
+	'chat-no-login-text' => 'Please login to chat.',
+	'chat-default-topic' => 'Welcome to the $1 chat',
 
-	'chat-you-are-banned' => "Sorry, you are banned from chat",
-	'chat-you-are-banned-text' => "You have been banned from the chat.
-If you think this was a mistake or would like to be reconsidered, please contact an administrator.",
-	'chat-kick-log-reason' => "Kick/banned from the chat for this wiki by $1.
-Please contact them for more info."
+	'chat-you-are-banned' => 'Sorry, you are banned from chat',
+	'chat-you-are-banned-text' => 'You have been banned from the chat.
+If you think this was a mistake or would like to be reconsidered, please contact an administrator.',
+	'chat-kick-log-reason' => 'Kick/banned from the chat for this wiki by $1.
+Please contact them for more info.',
 );
