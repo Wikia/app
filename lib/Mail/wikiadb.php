24921c7b8a41c6bcf1b62c41b75b9662ae7f4761
owen <owen@wikia-inc.com>
2011-03-21 22:31:54 +0000

    trunk: BugID:3836 fixing mail headers
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34885 e846bd91-c042-42a7-a218-4b65e2c1904d

--- lib/Mail/wikiadb.php
+++ lib/Mail/wikiadb.php
@@ -23,18 +23,10 @@ class Mail_wikiadb extends Mail {
 		global $wgCityId;
 		$wgCityId = ($wgCityId == null?0:$wgCityId); // fake city-id for contractor/staff.
 		$dbw = wfGetDb(DB_MASTER, array(), self::$MAIL_DB_NAME);
+		$dbw->begin();
 		foreach ($recipients as $recipient) {
 
 			// TODO: SHOULD WE FILTER BASED ON BLOCKS / SPAMS HERE?  FOR NOW WE WILL LET SENDGRID HANDLE THAT.
-
-			// Add a header for the server-name (helps us route where SendGrid will send bounces).
-			if((strlen($textHeaders) > 0) && (substr($textHeaders, -1) != "\n")){
-				$textHeaders .= "\n";
-			}
-			if(!empty($_SERVER)) {
-				$textHeaders .= "X-ServerName: ".$_SERVER['SERVER_NAME'];
-			}
-
 			$dbw->insert(
 				self::$MAIL_TABLE_NAME,
 				array(
@@ -47,14 +39,10 @@ class Mail_wikiadb extends Mail {
 				)
 			);
 			
-			if(!empty($_SERVER)) {
-				$textHeaders .= "\n";
-			}
-
 			// Add postback token so that we can verify that any postback actually comes from SendGrid.
 			$emailId = $dbw->insertId();
 			$postbackToken = wfGetEmailPostbackToken($emailId, $recipient);
-			$textHeaders .= "X-CallbackToken: ".$postbackToken;
+			$textHeaders .= $this->sep . "X-CallbackToken: ".$postbackToken;
 			$dbw->update(
 				self::$MAIL_TABLE_NAME,
 				array( /* SET */'hdr' => $textHeaders ),
