09e236bcc394c7e6f6e95180e73c94bb5df88db5
wojtek <wojtek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-24 22:07:18 +0000

    create new wiki test fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@34997 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreateNewWiki/tests/CreateNewWikiModuleTest.php
+++ extensions/wikia/CreateNewWiki/tests/CreateNewWikiModuleTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . '/../CreateNewWiki_setup.php';
+require_once dirname(__FILE__) . '/../CreateNewWikiModule.class.php';
 
 class CreateNewWikiModuleTest extends PHPUnit_Framework_TestCase {
 	
