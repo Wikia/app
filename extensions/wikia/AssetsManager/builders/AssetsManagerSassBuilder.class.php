8b80dfa268c48dcf3045c5e73f54b939defc1a5e
inez <inez@wikia-inc.com>
2011-03-25 17:02:55 +0000

    * Use
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35028 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/builders/AssetsManagerSassBuilder.class.php
+++ extensions/wikia/AssetsManager/builders/AssetsManagerSassBuilder.class.php
@@ -26,13 +26,13 @@ class AssetsManagerSassBuilder extends AssetsManagerBaseBuilder {
 	}
 	
 	private function sassProcessing() {
-		global $IP;
+		global $IP, $wgSassExacutable;
 
 		$tempDir = sys_get_temp_dir();
 		$tempOutFile = tempnam($tempDir, 'Sass');
 		$params = urldecode(http_build_query($this->mParams, '', ' '));
 		
-		$cmd = "/var/lib/gems/1.8/bin/sass {$IP}/{$this->mOid} {$tempOutFile} --cache-location {$tempDir} --style compact -r {$IP}/extensions/wikia/SASS/wikia_sass.rb {$params}";
+		$cmd = "{$wgSassExacutable} {$IP}/{$this->mOid} {$tempOutFile} --cache-location {$tempDir} --style compact -r {$IP}/extensions/wikia/SASS/wikia_sass.rb {$params}";
 		$escapedCmd = escapeshellcmd($cmd);
 
 		if(shell_exec($escapedCmd) != '') {
