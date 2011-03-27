6fb18129032b169cba4157fefaa701fd8892f6f2
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-27 21:43:32 +0000

    reapply an ooooold patch
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35048 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/CheckUser/CheckUser_body.php
+++ extensions/CheckUser/CheckUser_body.php
@@ -156,8 +156,9 @@ class CheckUser extends SpecialPage {
 		}
 		# Compile our nice form
 		# User box length should fit things like "2001:0db8:85a3:08d3:1319:8a2e:0370:7344/100/xff"
+		$wgOut->addWikiText( wfMsg( 'checkuser-summary' ) );
 		if( $wgUser->isAllowed( 'checkuser-log' ) ) {
-			$wgOut->addWikiText( wfMsg( 'checkuser-summary' ) . 
+			$wgOut->addWikiText(
 				"\n\n[[" . $this->getLogSubpageTitle()->getPrefixedText() .
 					'|' . wfMsg( 'checkuser-showlog' ) . ']]'
 			);
