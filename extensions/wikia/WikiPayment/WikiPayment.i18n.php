cfec765eaa65e04ef0b287d30e99a9bfba0bc26b
tor <tor@wikia-inc.com>
2011-03-28 22:52:07 +0000

    corrected per TWN request
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35067 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiPayment/WikiPayment.i18n.php
+++ extensions/wikia/WikiPayment/WikiPayment.i18n.php
@@ -4,7 +4,7 @@ $messages = array();
 
 $messages['en'] = array(
 	'wikipayment-desc' => 'Allows payment per wiki to disable ads.',
-	'wikipayment-paypal-redirect' => 'Redirecting to PayPal, please wait..',
+	'wikipayment-paypal-redirect' => 'Redirecting to PayPal, please wait&hellip;',
 	'wikipayment-click-here' => '',
 	'wikipayment-paypal-error' => "PayPal payment error (code: $1)",
 	'wikipayment-paypal-return-ok' => 'PayPal payment successful',
