464cb59587d0c5602f788045f7b7b713c0bfa1b9
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-28 23:06:32 +0000

    * AssetsManager config for Oasis
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35070 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -4,3 +4,66 @@ $config = array();
 $config['rte'] = array(
 	'#function_AssetsConfig::getRTEAssets',
 );
+
+$config['oasis_jquery'] = array(
+	'//skins/common/jquery/jquery-1.5.1.js',
+	'//skins/common/jquery/jquery.json-1.3.js',
+	'//skins/common/jquery/jquery.getcss.js',
+	'//skins/common/jquery/jquery.wikia.js',
+	'//skins/common/jquery/jquery.cookies.2.1.0.js',
+	'//skins/common/jquery/jquery.timeago.js',
+	'//skins/oasis/js/tables.js',
+	'//skins/oasis/js/common.js',
+);
+
+$config['oasis_shared_js'] = array(
+	'#group_oasis_jquery',
+	'//skins/common/wikibits.js',
+	'//skins/common/mwsuggest.js',
+	'//skins/oasis/js/tracker.js',
+	'//skins/common/jquery/jquery.wikia.modal.js',
+	'//skins/common/jquery/jquery.wikia.tracker.js',
+	'//skins/oasis/js/hoverMenu.js',
+	'//skins/oasis/js/PageHeader.js',
+	'//skins/oasis/js/Search.js',
+	'//skins/oasis/js/WikiaFooter.js',
+	'//skins/oasis/js/buttons.js',
+	'//skins/oasis/js/WikiHeader.js',
+	'//skins/oasis/js/LatestPhotos.js',
+	'//skins/oasis/js/Interlang.js',
+	'//extensions/wikia/ShareFeature/js/ShareFeature.js',
+	'//extensions/wikia/ArticleComments/js/ArticleComments.js',
+	'//extensions/wikia/RelatedPages/js/RelatedPages.js',
+	'//skins/oasis/js/WikiaNotifications.js',
+	'//skins/oasis/js/Spotlights.js',
+	'//skins/common/ajax.js',
+	'//extensions/wikia/CreatePage/js/CreatePage.js',
+	'//extensions/wikia/ImageLightbox/ImageLightbox.js',
+	'//extensions/wikia/WikiaPhotoGallery/js/WikiaPhotoGallery.view.js',
+	'//extensions/wikia/AjaxLogin/AjaxLoginBindings.js',
+	'//extensions/FBConnect/fbconnect.js',
+	'//extensions/wikia/AdEngine/AdConfig.js',
+	'//extensions/wikia/AdEngine/AdEngine.js',
+	'//extensions/wikia/AdEngine/AdProviderOpenX.js',
+	'//extensions/wikia/AdEngine/LazyLoadAds.js',
+	'//extensions/wikia/AdEngine/ghost/gw-2010.10.4/lib/gw.js',
+	'//extensions/wikia/Geo/geo.js',
+	'//extensions/wikia/QuantcastSegments/qcs.js',
+	'//extensions/wikia/AdEngine/liftium/Liftium.js',
+	'//extensions/wikia/AdEngine/liftium/Wikia.js',
+	'//extensions/wikia/AdEngine/AdDriver.js',
+	'//extensions/wikia/AdSS/adss.js',
+	'//extensions/wikia/PageLayoutBuilder/js/view.js',
+	'//skins/oasis/js/GlobalModal.js'
+);
+
+$config['oasis_anon_js'] = array(
+	'#group_oasis_shared_js',
+	'//skins/oasis/js/LatestActivity.js',
+	'//extensions/wikia/Interstitial/Exitstitial.js',
+);
+
+$config['oasis_user_js'] = array(
+	'#group_oasis_shared_js',
+	'//skins/common/ajaxwatch.js',
+);
