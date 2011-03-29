ef8e29bb5942f3613926d5fdc3795fad66cef26a
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-29 18:26:52 +0000

    * Proper fixes for BugzId 3984
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35105 e846bd91-c042-42a7-a218-4b65e2c1904d

--- wikia.php
+++ wikia.php
@@ -2,7 +2,7 @@
 // Initialise common MW code
 require ( dirname( __FILE__ ) . '/includes/WebStart.php' );
 
-$response = WF::build( 'App' )->dispatch();
+$response = F::build( 'App' )->dispatch();
 
 $response->sendHeaders();
 $response->printText();
