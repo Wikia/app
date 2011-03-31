1dd8f830945ac22f39f8bab79f78e3d52f614d05
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-03-31 01:15:49 +0000

    script to add tags to existing wikis BugzId:4111
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35176 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/addLangTag.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package MediaWiki
+ * @addtopackage maintenance
+ * @author eloy@wikia
+ * @author tor@wikia
+ *
+ * add language tag to all wikis
+ *
+ */
+
+ini_set( "include_path", dirname(__FILE__)."/.." );
+require_once( "commandLine.inc" );
+
+ini_set( "include_path", dirname(__FILE__)."/.." );
+require_once( "commandLine.inc" );
+
+$dbr = WikiFactory::db( DB_SLAVE );
+$sth = $dbr->select(
+	array( "city_list" ),
+	array( "*" ),
+	array( "city_public" => 1, "city_lang != 'en'" ),
+	__METHOD__
+);
+
+while( $row = $dbr->fetchObject( $sth ) ) {
+	$lang = $row->city_lang;
+	switch ( $lang ) {
+		case 'zh-tw':
+		case 'zh-hk':
+			$lang = 'zh';
+			break;
+		case 'pt-br':
+			$lang = 'pt';
+			break;
+	}
+
+	/**
+	 * set this as tag
+	 */
+	$tags = new WikiFactoryTags( $row->city_id );
+	$tags->addTagsByName( $lang );
+
+	Wikia::log( "AddLangTag", false, "$hub added as tag in {$row->city_id}" );
+}
