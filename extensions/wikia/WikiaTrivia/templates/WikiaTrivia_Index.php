088d4eaede0064435544898ba2a709d9bf828858
hyun <hyun@wikia-inc.com>
2011-03-31 01:42:32 +0000

    WikiaTrivia: WIP
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35182 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaTrivia/templates/WikiaTrivia_Index.php
+++ extensions/wikia/WikiaTrivia/templates/WikiaTrivia_Index.php
@@ -1,4 +1,8 @@
 <div class="trivia">
+	<div class="info-panel">
+		<div class="score-pane">Score: <span class="score">0</span></div>
+		<div class="timer">Time Left <span class="clock"></span></div>
+	</div>
 	<ol class="question-sets">
 		<li class="question-set">
 			<div class="question-ui">
@@ -12,20 +16,20 @@
 					<li>I dunno lol</li>
 				</ul>
 			</div>
-			<div class="answer-ui">
-				5000 wood
+			<div class="next-button">
+				Next Question
 			</div>
 		</li>
 		<li class="question-set">
 			<div class="question-ui">
 				<p class="question">
-					How many wood could a woodchuck chuck if a woodchuck could chuck wood?
+					She sells seashells over by the seashore?
 				</p>
 				<ul class="choices">
-					<li>1 wood</li>
-					<li>100 wood</li>
-					<li>5000 wood</li>
-					<li>I dunno lol</li>
+					<li>lol wut</li>
+					<li>that's not a question!</li>
+					<li>blah blah</li>
+					<li>42</li>
 				</ul>
 			</div>
 			<div class="answer-ui">
@@ -33,4 +37,9 @@
 			</div>
 		</li>
 	</ol>
+	<div class="answer-feedback star"></div>
+	<div class="answer-feedback wrong"></div>
 </div>
+<audio id="sound-menu-hover" src="/extensions/wikia/WikiaTrivia/sounds/button-34.mp3" preload="auto"></audio>
+<audio id="sound-answer-correct" src="/extensions/wikia/WikiaTrivia/sounds/magic-chime-01.mp3" preload="auto"></audio>
+<audio id="sound-answer-wrong" src="/extensions/wikia/WikiaTrivia/sounds/clank.mp3" preload="auto"></audio>
