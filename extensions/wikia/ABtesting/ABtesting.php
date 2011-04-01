f32c784c26055c37373f3d21ee09ae6da582685b
christian <christian@wikia-inc.com>
2011-04-01 19:13:22 +0000

    added new AB test for relocating history menu item - FB4073
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35285 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ABtesting/ABtesting.php
+++ extensions/wikia/ABtesting/ABtesting.php
@@ -14,18 +14,20 @@ function getABtestJSandCSS() {
 	global $wgOut, $wgExtensionsPath, $wgUser, $wgABTests;
 	
 	// For testing
-	//$wgABTests[] = 'editbutton1';
+	$wgABTests[] = 'noHistoryBar';
 
 	if (!isset($wgABTests)) {
 		return true;
 	}
 
 	// If a user logs in add them back to a special group so they are not part of the test
+	/*
 	if ($wgUser->isLoggedIn()) {
 		$expire = time() + 12*60*60;  // 12 hours
 		setrawcookie("wikia-ab", 'v5/name=loggedin/UA=UA-19473076-29/', $expire, "/", 'wikia.com');
 		return true;
 	}
+	*/
 	
 	if (in_array('editbutton1', $wgABTests)) {
 		$wgOut->addStyle(F::app()->getAssetsManager()->getSassCommonURL('/extensions/wikia/ABtesting/css/editbutton1.scss'));
@@ -49,6 +51,11 @@ function getABtestJSandCSS() {
 	if (in_array('editbutton5', $wgABTests)) {
 		$wgOut->addStyle(F::app()->getAssetsManager()->getSassCommonURL('/extensions/wikia/ABtesting/css/editbutton5.scss'));
 		$wgOut->addScript('<script src="'.$wgExtensionsPath.'/wikia/ABtesting/js/editbutton5.js"></script>');
+	}
+
+	if (in_array('noHistoryBar', $wgABTests)) {
+		$wgOut->addStyle(F::app()->getAssetsManager()->getSassCommonURL('/extensions/wikia/ABtesting/css/noHistoryBar.scss'));
+		$wgOut->addScript('<script src="'.$wgExtensionsPath.'/wikia/ABtesting/js/noHistoryBar.js"></script>');
 	}
 
 	return true;
