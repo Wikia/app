6ac0f2704b156dcb7ef2c67fcee26cc0145e7090
macbre <macbre@wikia-inc.com>
2011-04-01 15:25:04 +0000

    Update selenium tests for RTE reskin (BugId:3493)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35279 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/EditPageLayoutTest.java
+++ tests/acceptance/com/wikia/selenium/tests/EditPageLayoutTest.java
@@ -29,27 +29,31 @@ public class EditPageLayoutTest extends BaseTest {
 		assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1[contains(text(), 'Editing')]"));
 		//assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1[contains(text(), '" + title + "')]"));
 
-		assertTrue(session().isElementPresent("//td[@id='cke_toolbar_wpTextbox1']"));
-		assertTrue(session().isElementPresent("//td[@id='cke_rail_wpTextbox1']"));
-		assertTrue(session().isElementPresent("//td[@id='cke_space_wpTextbox1']"));
-		assertTrue(session().isElementPresent("//td[@id='cke_contents_wpTextbox1']"));
+		// spaces
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageEditorWrapper']"));
+		assertTrue(session().isElementPresent("//nav[@id='EditPageTabs']"));
 
 		// toolbars
-		assertTrue(session().isElementPresent("//td[@id='cke_toolbar_wpTextbox1']/div[contains(@class, 'cke_toolbar_format')]"));
-		assertTrue(session().isElementPresent("//td[@id='cke_toolbar_wpTextbox1']/div[contains(@class, 'cke_toolbar_templates')]"));
-		assertTrue(session().isElementPresent("//td[@id='cke_toolbar_wpTextbox1']/div[contains(@class, 'cke_toolbar_insert')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_toolbar_source')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_toolbar_format')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_toolbar_format_expanded')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_toolbar_expand')]"));
 
-		assertTrue(session().isElementPresent("//td[@id='cke_toolbar_wpTextbox1']/div[contains(@class, 'cke_buttons')]"));
-		assertTrue(session().isElementPresent("//td[@id='cke_toolbar_wpTextbox1']//span[contains(@class, 'cke_button_bold')]"));
-		assertTrue(session().isElementPresent("//td[@id='cke_toolbar_wpTextbox1']/div[contains(@class, 'cke_toolbar_insert')]/span[contains(@class,'cke_button_big')]"));
+		// buttons
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_buttons')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']//span[contains(@class, 'cke_button_bold')]"));
 
 		// right rail modules
-		assertTrue(session().isElementPresent("//td[@id='cke_rail_wpTextbox1']/div[contains(@class, 'cke_module_page_controls')]"));
-		assertTrue(session().isElementPresent("//td[@id='cke_rail_wpTextbox1']/div[contains(@class, 'cke_module_rail_insert')]"));
-		assertTrue(session().isElementPresent("//td[@id='cke_rail_wpTextbox1']/div[contains(@class, 'cke_module_rail_templates')]"));
-		assertTrue(session().isElementPresent("//td[@id='cke_rail_wpTextbox1']/div[contains(@class, 'cke_module_license')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_page_controls')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_insert')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_categories')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_templates')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_license')]"));
 
-		assertTrue(session().isElementPresent("//td[@id='cke_rail_wpTextbox1']/div[contains(@class, 'cke_module_page_controls')]//textarea[@id='wpSummary']"));
-		assertTrue(session().isElementPresent("//td[@id='cke_rail_wpTextbox1']/div[contains(@class, 'cke_module_page_controls')]//input[@id='wpSave']"));
+		// page controls
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_page_controls')]//textarea[@id='wpSummary']"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_page_controls')]//input[@id='wpSave']"));
 	}
 }
