5d9c6edeb45b5647a407b76a1e9f4c2fb1abfcb3
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-01 02:22:53 +0000

    before i fix the code, fix the formatting, so i have clean place to revert to
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35253 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WhatIsMyIP/whatismyip_body.php
+++ extensions/wikia/WhatIsMyIP/whatismyip_body.php
@@ -1,30 +1,31 @@
 <?php
 
-  if (!defined('MEDIAWIKI')) 
-	{
+if (!defined('MEDIAWIKI')) 
+{
 	echo "This is MediaWiki extension named WhatIsMyIP.\n";
 	exit(1) ;
+}
+
+global $wgMessageCache, $wgOut;
+$wgMessageCache->addMessage( "whatismyip", "What is my IP" );
+$wgMessageCache->addMessage( "whatismyip_out", "Your IP: $1" );
+
+class WhatIsMyIP extends SpecialPage 
+{
+	function  __construct() {
+		parent::__construct('WhatIsMyIP' /*class*/);
+	} 
+
+	function WhatIsMyIP() 
+	{
+		SpecialPage::SpecialPage( 'WhatIsMyIP', 'whatismyip' );
 	}
 
-  global $wgMessageCache, $wgOut;
-  $wgMessageCache->addMessage( "whatismyip", "What is my IP" );
-  $wgMessageCache->addMessage( "whatismyip_out", "Your IP: $1" );
-  class WhatIsMyIP extends SpecialPage 
-  {
-   function  __construct() {
-   parent::__construct('WhatIsMyIP' /*class*/);
-	} 
- 
-   function WhatIsMyIP() 
-    {
-    SpecialPage::SpecialPage( 'WhatIsMyIP', 'whatismyip' );
-    }
-   function execute () 
-    {
-    global $wgMessageCache, $wgOut;
-    $wgOut->SetPageTitle(wfMsg('whatismyip'));
-    $ip = wfGetIP();
-    $wgOut->addWikiMsg( 'whatismyip_out', $ip );
-
-    }
-  }
+	function execute () 
+	{
+		global $wgMessageCache, $wgOut;
+		$wgOut->SetPageTitle(wfMsg('whatismyip'));
+		$ip = wfGetIP();
+		$wgOut->addWikiMsg( 'whatismyip_out', $ip );
+	}
+}
