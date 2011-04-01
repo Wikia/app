3b3a87dcf8bb721b3b3d73d3899aed88be509fa2
inez <inez@wikia-inc.com>
2011-04-01 06:13:17 +0000

    * Merge changes from prod release
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35256 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/AssetsConfig.class.php
+++ extensions/wikia/AssetsManager/AssetsConfig.class.php
@@ -120,9 +120,9 @@ class AssetsConfig {
 				// reference to a function that returns array of URIs
 				$assets = array_merge($assets, call_user_func(substr($item, 10), $combine, $minify, $params));
 
-			} else if(substr($item, 0, 7) == 'http://' || substr($item, 0, 8) == 'https://') {
+			} else if(Http::isValidURI($item)) {
 
-				// reference to remote file (only http, not https)
+				// reference to remote file (http and https)
 				$assets[] = $item;
 
 			}
