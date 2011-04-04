40230f74ecec36fcf0dbdfc40f8f71c2869c308c
garth <garth@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-04 21:30:59 +0000

    Added JS class to handle calling one dot for events.
    BugID: 3766, 3767
    
    
    
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35361 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaStats/js/WikiaStats.js
@@ -0,0 +1,57 @@
+var OneDot = {
+	ajaxUrl: wgServer + wgScript + '?action=ajax',
+	baseUrl: wgOneDotURL,
+	cookieName: wgOneDotCookie,
+	uuid: '',
+	requesting: 0,
+
+	init: function() {
+		if (OneDot.uuid) return;
+		if (OneDot.requesting) return;
+
+		OneDot.uuid = $.cookies.get(OneDot.cookieName);
+		if (OneDot.uuid) {
+			console.log("Found uuid " + OneDot.uuid);	
+		} else {
+			console.log("Requesting new uuid");
+			OneDot.requestUUID();
+		}
+	},
+
+	requestUUID: function () {
+		OneDot.requesting = 1;
+		$.get(OneDot.ajaxUrl, { rs: "WikiaUUID::getUUID" },
+			  function(data) {
+				if (data.error !== undefined) {
+					// NOK
+				} else {
+					// OK
+					console.log("Assigned new uuid " + data.uuid);
+					$.cookies.set(OneDot.cookieName, data.uuid);
+					OneDot.uuid = data.uuid;
+				}
+				OneDot.requesting = 0;
+
+				return true;
+			  },
+			  "json"
+		);
+	},
+
+	track: function(event, version) {
+		OneDot.init();
+	
+		url = OneDot.baseUrl + "&cb="+(new Date).valueOf();
+
+		if (typeof document.referrer != "undefined") url = url + "&r="+escape(document.referrer);
+		if (OneDot.uuid) url = url +  "&uuid=" + OneDot.uuid;
+		if (event) url = url + "&event=" + event;
+		if (version) url = url + "&version=" + version;
+
+		console.log("OneDot track: " + url);
+
+		$.get(url);
+				
+		return true;
+	}
+}
