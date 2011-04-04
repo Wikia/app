aa223bc4f876f41cde1bda014a81832b0a31b0e4
garth <garth@wikia-inc.com>
2011-04-04 21:27:56 +0000

    An extension to create universally unique IDs for OneDot.
    BugID: 3766
    
    
    
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35360 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaUUID/WikiaUUID.i18n.php
@@ -0,0 +1,12 @@
+ <?php
+ /**
+  * Internationalisation file for the WikiaUUID extension.
+  *
+  * @addtogroup Languages
+  */
+ 
+ $messages = array();
+ 
+ $messages['en'] = array(
+ 	'wikia-uuid-desc'     => 'Wikia UUID generator',
+ );
