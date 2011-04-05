c57775ad70d8bd74b9de8680914c4169b99254c9
uberfuzzy <uberfuzzy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-05 00:30:43 +0000

    finally fix the gCal extension. allow width/height params, remove default CSC-NZ title, all overriding title, update url to MW page
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35376 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/googleCalendar/googleCalendar.php
+++ extensions/3rdparty/googleCalendar/googleCalendar.php
@@ -7,14 +7,18 @@
 #   from url http://calendar.google.com/calendarplay?docid=6444586097901795775
 #   <googlecalendar>6444586097901795775</googlecalendar>
 #
+# Ex:
+#   <googlecalendar width="X" height="Y" title="Z">
+#   omitting width/height out/blank/0 will fallback to hardcoded defaults from original
+#   omitting title will use the calendar's title
 # Enjoy !
 
 $wgExtensionFunctions[] = 'wfGoogleCalendar';
 $wgExtensionCredits['parserhook'][] = array(
         'name' => 'Google Calendar',
         'description' => 'Display Google Calendar',
-        'author' => 'Kasper Souren',
-        'url' => 'http://wiki.couchsurfing.com/en/Google_Calendar_MediaWiki_plugin'
+        'author' => 'Kasper Souren, C. \'Uberfuzzy\' Stafford',
+        'url' => 'http://www.mediawiki.org/wiki/Extension:GoogleCalendar'
 );
 
 function wfGoogleCalendar() {
@@ -23,22 +27,33 @@ function wfGoogleCalendar() {
 }
 
 # The callback function for converting the input text to HTML output
-function renderGoogleCalendar($input) {
-        $input = htmlspecialchars($input);
-        //$input = "6444586097901795775"
-        $width = 425;
-        $height = 350;
+function renderGoogleCalendar( $contents, $attributes, $parser ) {
+        $contents = htmlspecialchars($contents);
 
-        $output = '<iframe src="http://www.google.com/calendar/embed?src=g43t277um81su1tjvgvah609qk%40group.calendar.google.com&pvttk=db8cdde758254dbe5a511723cac5da46&title=CSC-NZ&chrome=NAVIGATION&height=600&epr=4" style=" border-width:0 " width="480" frameborder="0" height="600"></iframe>';
-        $output = '<iframe src="http://www.google.com/calendar/embed?src='.$input.'&title=CSC-NZ&chrome=NAVIGATION&height=600&epr=4" style=" border-width:0 " width="480" frameborder="0" height="600"></iframe>';
+		if( empty($attributes['width']) || 0 == preg_match("/^[0-9%]*$/", $attributes['width']) ) {
+			$width = 425;
+		}
+		else {
+			$width = htmlspecialchars($attributes['width']);
+		}
 
-        /*
+		if( empty($attributes['height']) || 0 == preg_match("/^[0-9%]*$/", $attributes['height']) ) {
+			$height = 350;
+		}
+		else {
+			$height = htmlspecialchars($attributes['height']);
+		}
 
-        $output = '<embed style="width:'.$width.'px; height:'.$height.'px;" '
-                .'id="CalendarPlayback" type="application/x-shockwave-flash" '
-                .'src="http://calendar.google.com/googleplayer.swf?docId='
-                .$input.'"> </embed>';
-        */
+		if( empty($attributes['title']) || 0 == preg_match("/^[a-zA-Z0-9- ]*$/", $attributes['title']) ) {
+			$title = '';
+		}
+		else {
+			$title = htmlspecialchars($attributes['title']);
+		}
+
+		
+        $output = '<iframe src="http://www.google.com/calendar/embed?src='.$contents.'&title='.$title.'&chrome=NAVIGATION&height='.$height.'&epr=4" style=" border-width:0 " width="'.$width.'" frameborder="0" height="'.$height.'"></iframe>';
+
         return $output;
 }
 ?>
