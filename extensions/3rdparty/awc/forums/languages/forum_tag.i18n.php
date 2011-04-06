8d4baaf861a3ed0c93aabffb527c5957a6685a79
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-06 00:50:26 +0000

    removed forums, not needed anymore
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35465 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/awc/forums/languages/forum_tag.i18n.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-
-
-$messages['en']['awcsf_thread_title'] = 'Thread Title';
-$messages['en']['awcsf_word_replies'] = 'Replies';
-$messages['en']['awcsf_views'] = 'Views';
-$messages['en']['awcsf_last_action'] = 'Last Action';
-$messages['en']['awcsf_word_last'] = 'Last';
