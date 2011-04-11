b7726a3142ae9a57c93eef727357b1487158d987
garth <garth@wikia-inc.com>
2011-04-11 22:07:43 +0000

    Removed the UUID stuff as it is implemented in the varnishes.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35759 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaStats/js/WikiaStats.js
+++ extensions/wikia/WikiaStats/js/WikiaStats.js
@@ -1,50 +1,11 @@
 var OneDot = {
 	ajaxUrl: wgServer + wgScript + '?action=ajax',
 	baseUrl: wgOneDotURL,
-	cookieName: wgOneDotCookie,
-	uuid: '',
-	requesting: 0,
-
-	init: function() {
-		if (OneDot.uuid) return;
-		if (OneDot.requesting) return;
-
-		OneDot.uuid = $.cookies.get(OneDot.cookieName);
-		if (OneDot.uuid) {
-			$().log("Found uuid " + OneDot.uuid);
-		} else {
-			$().log("Requesting new uuid");
-			OneDot.requestUUID();
-		}
-	},
-
-	requestUUID: function () {
-		OneDot.requesting = 1;
-		$.get(OneDot.ajaxUrl, { rs: "WikiaUUID::getUUID" },
-			  function(data) {
-				if (data.error !== undefined) {
-					// NOK
-				} else {
-					// OK
-					$().log("Assigned new uuid " + data.uuid);
-					$.cookies.set(OneDot.cookieName, data.uuid);
-					OneDot.uuid = data.uuid;
-				}
-				OneDot.requesting = 0;
-
-				return true;
-			},
-			"json"
-		);
-	},
 
 	track: function(event, version) {
-		OneDot.init();
-
 		var url = OneDot.baseUrl + "&cb="+(new Date).valueOf();
 
 		if (typeof document.referrer != "undefined") url = url + "&r="+escape(document.referrer);
-		if (OneDot.uuid) url = url +  "&uuid=" + OneDot.uuid;
 		if (event) url = url + "&event=" + event;
 		if (version) url = url + "&version=" + version;
 
