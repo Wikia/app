fd1dfc17b276e8f98748e6e0a69119add1bd2859
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-14 16:39:37 +0000

    * Re-enable minifier for oasis_shared_js, rte and css
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35914 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/builders/AssetsManagerBaseBuilder.class.php
+++ extensions/wikia/AssetsManager/builders/AssetsManagerBaseBuilder.class.php
@@ -22,6 +22,24 @@ class AssetsManagerBaseBuilder {
 	}
 
 	public function getContent() {
+		if(!empty($this->mContent)) {
+			$start = microtime(true);
+
+			if($this->mOid == 'oasis_shared_js' || $this->mOid == 'rte') {
+				$newContent = $this->minifyJS($this->mContent, true);
+			} else if($this->mContentType == AssetsManager::TYPE_CSS) {
+				$newContent = $this->minifyCSS($this->mContent);
+			} else if($this->mContentType == AssetsManager::TYPE_JS) {
+				//$newContent = $this->minifyJS($this->mContent);
+			}
+
+			$stop = microtime(true);
+		}
+
+		if(!empty($newContent)) {
+			$this->mContent = '/* Minify took '.($stop-$start)." */\n\n".$newContent;
+		}
+
 		return $this->mContent;
 	}
 
@@ -37,4 +55,31 @@ class AssetsManagerBaseBuilder {
 	public function getContentType() {
 		return $this->mContentType;
 	}
+
+	private function minifyJS($content, $useYUI = false) {
+		global $IP;
+
+		$tempInFile = tempnam(sys_get_temp_dir(), 'AMIn');
+		file_put_contents($tempInFile, $content);
+
+		if($useYUI) {
+			$tempOutFile = tempnam(sys_get_temp_dir(), 'AMOut');
+			shell_exec("nice -n 15 java -jar {$IP}/lib/yuicompressor-2.4.2.jar --type js -o {$tempOutFile} {$tempInFile}");
+			$out = file_get_contents($tempOutFile);
+			unlink($tempOutFile);
+		} else {
+			$jsmin = dirname(__FILE__) . '/../../StaticChute/jsmin';
+			$out = shell_exec("cat $tempInFile | $jsmin");
+		}
+
+		unlink($tempInFile);
+
+		return $out;
+	}
+
+	private function minifyCSS($content) {
+		require_once dirname(__FILE__) . '/../../StaticChute/Minify_CSS_Compressor.php';
+		return Minify_CSS_Compressor::process($content);
+	}
+
 }
