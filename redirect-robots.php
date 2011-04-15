72258a69aa3aaab445b2100e6a384800c18ae281
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-15 13:16:15 +0000

    Block heavy bot activity from 213.186.122.3, BugId: 2970
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35977 e846bd91-c042-42a7-a218-4b65e2c1904d

--- redirect-robots.php
+++ redirect-robots.php
@@ -96,6 +96,9 @@ Disallow: /
 User-agent: Download Ninja
 Disallow: /
 
+User-agent: sitebot
+Disallow: /
+
 #
 # Sorry, wget in its recursive mode is a frequent problem.
 # Please read the man page and use it properly; there is a
