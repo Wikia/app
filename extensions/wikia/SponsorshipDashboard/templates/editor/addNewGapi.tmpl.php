8e4210251ee242cf3027972a5f46a42aba06af9a
jakub <jakub@wikia-inc.com>
2011-04-15 22:52:56 +0000

    dump removal
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@35999 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SponsorshipDashboard/templates/editor/addNewGapi.tmpl.php
+++ extensions/wikia/SponsorshipDashboard/templates/editor/addNewGapi.tmpl.php
@@ -6,7 +6,6 @@
 	<a class="wikia-button sd-source-remove" style="float:right; margin-top:3px"> <img src="<?=f::app()->getGlobal('wgBlankImgUrl'); ?>" height="0" width="0" class="sprite close"> <?=wfMsg('sponsorship-dashboard-source-discard');?></a>
 	<div class="sd-source-title"> <?=wfMsg('sponsorship-dashboard-source-datasource');?> <b>#1</b>: <?=wfMsg('sponsorship-dashboard-source-GoogleAnalytics');?></div>
 	<div style="font-weight:bold"> <?=wfMsg('sponsorship-dashboard-source-Metrics');?> </div>
-	<? var_dump( $data ); ?>
 	<ul><?
 		$metrics = explode(',', $data[SponsorshipDashboardSourceGapi::SD_PARAMS_GAPI_METRICS] );
 		$metricsNames = unserialize( $data[ SponsorshipDashboardSourceGapi::SD_PARAMS_GAPI_METRICS_NAMES ] );
