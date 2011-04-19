0f369f7f04ae954aa8be425a67ed3a3938e3950d
emil <emil@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-19 12:38:06 +0000

    a new rail module that shows recent articles from a specified answers wiki
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36062 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/LatestQuestions/LatestQuestions.js
@@ -0,0 +1,25 @@
+$(window).load(function() {
+    LatestQuestions.init();
+});
+
+var LatestQuestions = {
+    init: function() {
+        $.getScript(wgAnswersServer + wgAnswersScript + '?action=ajax&rs=moduleProxy&moduleName=LatestActivity&actionName=Index&outputType=data&callback=LatestQuestions.onLoad');
+    },
+    onLoad: function(data) {
+	if(data.changeList.length > 0) {
+            var html = "<section class='LatestQuestionsModule module'>";
+	    html += "<h1 class='activity-heading'>"+wgLatestQuestionsHeader+"</h1>";
+	    html += "<ul>";
+            for(var i=0; i<data.changeList.length; i++) {
+		html += "<li><img src='"+wgBlankImgUrl+"' class='sprite "+data.changeList[i].changeicon+"' height='20' width='20'><em>"+data.changeList[i].page_href+"</em><details>"+data.changeList[i].changemessage+"</details></li>";
+            }
+	    html += "</ul>";
+	    html += "<a href='/wiki/Special:WikiActivity' title='Special:WikiActivity' class='more'>"+wgOasisMoreMsg+"</a>";
+	    html += "</section>";
+	    $("#WikiaRail").append(html).find("section.LatestQuestionsModule a").attr("href", function(i, val) {
+                return wgAnswersServer + val;
+            });
+        }
+    }
+}
