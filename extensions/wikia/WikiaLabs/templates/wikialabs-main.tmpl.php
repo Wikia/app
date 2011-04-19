ef6580f8ecad8c0f709f36d58f12d054bbb02434
jakub <jakub@wikia-inc.com>
2011-04-19 14:29:41 +0000

    BugId:4599
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36069 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaLabs/templates/wikialabs-main.tmpl.php
+++ extensions/wikia/WikiaLabs/templates/wikialabs-main.tmpl.php
@@ -10,14 +10,14 @@
 		<li>
 			<img class="appScreen" src="<?php echo $data['prjscreenurl'] ?>">
 			<span class='details' >
-			 	<span class="prjname" ><?php echo $value->getName(); ?><span class="data"><?php echo $contLang->date($value->getReleaseDateMW()); ?></span> </span>
+			 	<h1 class="prjname" ><?php echo $value->getName(); ?></h2>
+				<span class="data"><?php echo $contLang->date($value->getReleaseDateMW()); ?></span>
 			 	<?php echo $data['description']; ?>
 			</span>
 			<span class='buttons'>
 				<?php if($isAdmin): ?>
 					<span data-id="<?php echo $value->getId(); ?>" class='slider <?php echo $value->isEnabled($cityId) ? "on":""; ?>'>
-						<span class='button  <?php echo $value->isEnabled($cityId) ? "on":""; ?>'>
-						</span>
+						<span class='button  <?php echo $value->isEnabled($cityId) ? "on":""; ?>'></span>
 						<span class="textoff  <?php echo $value->isEnabled($cityId) ? "on":""; ?>"><?php echo wfMsg('wikialabs-list-project-inactive'); ?></span>
 						<span class="texton  <?php echo $value->isEnabled($cityId) ? "on":""; ?>" ><?php echo wfMsg('wikialabs-list-project-active'); ?></span>
 						<?php if($data['enablewarning']): ?>
@@ -50,9 +50,9 @@
 						<?php endif; ?>
 					<?php endfor; ?>
 					<span class="ratings" >
-	    				<?php echo $value->getRating(); ?><br>
-	    				<?php echo wfMsg('wikialabs-list-project-ratings'); ?>
-	    			</span>
+						<?php echo $value->getRating(); ?><br>
+						<?php echo wfMsg('wikialabs-list-project-ratings'); ?>
+					</span>
 				</span>
 			</span>
 		</li>
@@ -64,15 +64,14 @@
 		<div class='errorBox' >
 			<div id="wpError"></div>
 		</div>
-		<span class="title" ><?php echo wfMsg( 'wikialabs-feedback-title' ); ?></span>
-		<span class="project" ><?php echo wfMsg( 'wikialabs-feedback-rating' ); ?></span>
-
+		<h2 class="title" ><?php echo wfMsg( 'wikialabs-feedback-title' ); ?></h2>
+		<span class="comments" ><?php echo wfMsg( 'wikialabs-feedback-comments' ); ?></span>
 		<span class="stars" >
 			<?php for($i = 1; $i < 6; $i ++): ?>
 				<img data-index="<?php echo $i; ?>" src="<?= wfBlankImgUrl() ;?>"/>
 			<?php endfor; ?>
 		</span>
-		<span class="comments" ><?php echo wfMsg( 'wikialabs-feedback-comments' ); ?></span>
+		<span class="project" ><?php echo wfMsg( 'wikialabs-feedback-rating' ); ?></span>
 		<textarea name="feedbacktext" class="feedbacktext" ></textarea>
 		<button class="okbutton" ><?php echo wfMsg('wikialabs-feedback-submit'); ?></button>
 	</form>
