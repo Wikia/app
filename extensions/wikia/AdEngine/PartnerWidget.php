41b34af89efa269311eb43dea1231831c71b58ef
will <will@wikia-inc.com>
2011-04-21 18:40:47 +0000

    Bug ID 4244: place ebay widget on Muppet
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36225 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/PartnerWidget.php
+++ extensions/wikia/AdEngine/PartnerWidget.php
@@ -5,95 +5,142 @@ $wgHooks["MakeGlobalVariablesScript"][] = "wfPartnerWidgetJSVars";
 function wfPartnerWidgetJSVars($vars) {
 	global $wgEnablePartnerWidget;
 	if ($wgEnablePartnerWidget) {
-		$vars['partnerKeywords'] = getPartnerWidgetKeywords();
+		$vars['partnerKeywords'] = PartnerWidget::getPartnerWidgetKeywords();
 	}
 
 	return true;
 }
 
-function getPartnerWidgetKeywords() {
-	global $wgTitle, $wgDBname, $wgOut;
-	
-	$keywords = null;
+class PartnerWidget {
+	public static function getPartnerWidgetKeywords() {
+		global $wgTitle, $wgDBname, $wgOut;
 
-	switch ($wgDBname) {
-		case 'hotwheels':
-			$HOTWHEELS = 'Hot Wheels';
-			$LIST_OF = 'List of ';
-			$CATEGORY_PREFIX_HOTWHEELSBY = 'Hot Wheels by';
-			$CATEGORY_PREFIX_HOTWHEELSBY = 'Hot Wheels by';
+		$keywords = null;
 
-			$keywords = $wgTitle->getText();
+		switch ($wgDBname) {
+			case 'hotwheels':
+				$HOTWHEELS = 'Hot Wheels';
+				$LIST_OF = 'List of ';
+				$CATEGORY_PREFIX_HOTWHEELSBY = 'Hot Wheels by';
+				$CATEGORY_PREFIX_HOTWHEELSBY = 'Hot Wheels by';
 
-			if (stripos($keywords, $LIST_OF) === 0) {
-				$keywords = substr($keywords, strlen($LIST_OF));
-			}
-			else {
-				$hotwheelsbyPos = stripos($keywords, $CATEGORY_PREFIX_HOTWHEELSBY);
-				if ($hotwheelsbyPos !== FALSE) {
-					$keywords = substr($keywords, $hotwheelsbyPos+strlen($CATEGORY_PREFIX_HOTWHEELSBY));
+				$keywords = $wgTitle->getText();
+
+				if (stripos($keywords, $LIST_OF) === 0) {
+					$keywords = substr($keywords, strlen($LIST_OF));
 				}
-			}
-
-			if (stripos($keywords, $HOTWHEELS) === FALSE) {
-				$keywords = $HOTWHEELS . ' ' . $keywords;
-			}
-
-			break;
-		case 'ipod': // apple.wikia.com
-			$cats = $wgOut->getCategories();
-			foreach ($cats as $cat) {
-				switch (strtolower($cat)) {
-					case 'apple hardware':
-					case 'apple lineup':
-					case 'macintosh ii':
-					case 'macintosh lc':
-					case 'mac models':
-					case 'modular macs':
-					case 'old stuff':
-					case 'portable computers':
-					case 'portable macs':
-					case 'powerbook g4':
-					case 'power macintosh':
-					case 'power macintosh g3':
-					case 'power macintosh g4':
-						$keywords = $wgTitle->getText();
-						break 2; // break out of foreach
-					default:
+				else {
+					$hotwheelsbyPos = stripos($keywords, $CATEGORY_PREFIX_HOTWHEELSBY);
+					if ($hotwheelsbyPos !== FALSE) {
+						$keywords = substr($keywords, $hotwheelsbyPos+strlen($CATEGORY_PREFIX_HOTWHEELSBY));
+					}
 				}
-			}
-			break;
-		//case 'vintagepatterns':
-			//if ($wgTitle->getNamespace() == NS_CATEGORY) {
-				//$keywords = $wgTitle->getText();
-			//}
-			//elseif (in_array('Vintage Sewing Patterns', $wgOut->getCategories())) {
-				//$keywords = $wgTitle->getText();
-				// strip trailing "A", "B", etc.
-				//if (preg_match('/ [A-Za-z]$/', $keywords)) {
-					//$keywords = substr($keywords, 0, strlen($keywords)-2);
-				//}
-			//}
-			//break;
-		case 'yugioh':
-			$CATEGORY_WHITELIST_INDICATOR = 'card';
-			$STRIP_SUBSTRING_FIRST_CHAR = '(';
 
-			// show widget only on articles in whitelisted categories
-			$cats = $wgOut->getCategories();
-			foreach ($cats as $cat) {
-				if (stripos($cat, $CATEGORY_WHITELIST_INDICATOR) !== FALSE) {
-					$keywords = $wgDBname . ' ' . $wgTitle->getText();
-					// strip certain text from end of keywords
-					$stripSubstrPos = strpos($keywords, $STRIP_SUBSTRING_FIRST_CHAR);
-					if ($stripSubstrPos !== FALSE) {
-						$keywords = trim(substr($keywords, 0, $stripSubstrPos));
+				if (stripos($keywords, $HOTWHEELS) === FALSE) {
+					$keywords = $HOTWHEELS . ' ' . $keywords;
+				}
+
+				break;
+			case 'ipod': // apple.wikia.com
+				$cats = $wgOut->getCategories();
+				foreach ($cats as $cat) {
+					switch (strtolower($cat)) {
+						case 'apple hardware':
+						case 'apple lineup':
+						case 'macintosh ii':
+						case 'macintosh lc':
+						case 'mac models':
+						case 'modular macs':
+						case 'old stuff':
+						case 'portable computers':
+						case 'portable macs':
+						case 'powerbook g4':
+						case 'power macintosh':
+						case 'power macintosh g3':
+						case 'power macintosh g4':
+							$keywords = $wgTitle->getText();
+							break 2; // break out of foreach
+						default:
 					}
 				}
 				break;
-			}
-			break;
+			case 'muppet':
+				$cats = $wgOut->getCategories();
+				foreach ($cats as $cat) {
+					$lowercaseCat = strtolower($cat);
+					switch ($lowercaseCat) {
+						case 'muppet characters':
+						case 'sesame street characters':
+							$keywords = $wgTitle->getText();
+							$keywords = self::stripParentheticalSubstring($keywords);
+							break;
+						default:
+							if (stripos($lowercaseCat, 'albums') !== FALSE
+							|| stripos($lowercaseCat, 'books') !== FALSE
+							|| stripos($lowercaseCat, 'buttons') !== FALSE
+							|| stripos($lowercaseCat, 'clothing') !== FALSE
+							|| stripos($lowercaseCat, 'collectibles') !== FALSE
+							|| stripos($lowercaseCat, 'figures') !== FALSE
+							|| stripos($lowercaseCat, 'paper products') !== FALSE
+							|| stripos($lowercaseCat, 'pins') !== FALSE
+							|| stripos($lowercaseCat, 'plush') !== FALSE
+							|| stripos($lowercaseCat, 'puzzles') !== FALSE
+							|| stripos($lowercaseCat, 'supplies') !== FALSE
+							|| stripos($lowercaseCat, 'toys') !== FALSE
+							|| stripos($lowercaseCat, 'video games') !== FALSE
+							|| stripos($lowercaseCat, 'watches') !== FALSE
+							) {
+								$keywords = $wgTitle->getText();
+								$keywords = self::stripParentheticalSubstring($keywords);
+								break;
+							}
+					}
+				}
+				break;
+			//case 'vintagepatterns':
+				//if ($wgTitle->getNamespace() == NS_CATEGORY) {
+					//$keywords = $wgTitle->getText();
+				//}
+				//elseif (in_array('Vintage Sewing Patterns', $wgOut->getCategories())) {
+					//$keywords = $wgTitle->getText();
+					// strip trailing "A", "B", etc.
+					//if (preg_match('/ [A-Za-z]$/', $keywords)) {
+						//$keywords = substr($keywords, 0, strlen($keywords)-2);
+					//}
+				//}
+				//break;
+			case 'yugioh':
+				$CATEGORY_WHITELIST_INDICATOR = 'card';
+
+				// show widget only on articles in whitelisted categories
+				$cats = $wgOut->getCategories();
+				foreach ($cats as $cat) {
+					if (stripos($cat, $CATEGORY_WHITELIST_INDICATOR) !== FALSE) {
+						$keywords = $wgDBname . ' ' . $wgTitle->getText();
+						$keywords = self::stripParentheticalSubstring($keywords);
+					}
+					break;
+				}
+				break;
+		}
+
+		return $keywords;
 	}
 
-	return $keywords;
+	/**
+	 * If input string has parentheses and anything afterward, then strips
+	 * whitespace around that string. For example, if input is
+	 * "Title (subtitle) description", result is "Title".
+	 * @param string $str
+	 * @return string
+	 */
+	private static function stripParentheticalSubstring($str) {
+		$STRIP_SUBSTRING_FIRST_CHAR = '(';
+		$stripSubstrPos = strpos($str, $STRIP_SUBSTRING_FIRST_CHAR);
+		if ($stripSubstrPos !== FALSE) {
+			$str = trim(substr($str, 0, $stripSubstrPos));
+		}
+
+		return $str;
+	}
 }
