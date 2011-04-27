a0cff1f9b6521d74d6ebea133ef0789aae23ad61
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-27 01:08:41 +0000

    trunk: removed obsolete code from DummyExt
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36362 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/DummyExtension/DummyExtensionController.class.php
+++ extensions/wikia/DummyExtension/DummyExtensionController.class.php
@@ -2,11 +2,6 @@
 
 class DummyExtensionController extends WikiaController {
 
-	public function __construct() {
-		$this->allowedRequests[ 'helloWorld' ] = array( 'html', 'json' );
-		$this->allowedRequests[ 'errorTest' ] = array( 'html', 'json' );
-	}
-
 	public function helloWorld() {
 		if($this->request->getVal('param') == 1 ) {
 			$this->response->setTemplatePath( dirname( __FILE__ ) . '/templates/someTemplate.php' );
