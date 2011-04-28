6b569b9da269b8cc2b2e6f8e51daeb20078fe6d2
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-28 12:54:26 +0000

    WikiaApp: added a facade for profiling functions
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36442 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/WikiaApp.class.php
+++ includes/wikia/WikiaApp.class.php
@@ -298,6 +298,22 @@ class WikiaApp {
 		
 		return $this->getDispatcher()->dispatch( $this, $request );
 	}
+	
+	/**
+	 * simple wfProfileIn wrapper
+	 * @see wfProfileIn
+	 */
+	public function profileIn( $method ){
+		$this->runFunction( 'wfProfileIn', $method );
+	}
+	
+	/**
+	 * simple wfProfileOut wrapper
+	 * @see wfProfileOut
+	 */
+	public function profileOut( $method ){
+		$this->runFunction( 'wfProfileOut', $method );
+	}
 
 	/**
 	 * get view Object for given controller and method (previously wfRenderPartial)
