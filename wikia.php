7ed16f18b4bc9ee77d7dbb45fd3165da66feec59
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-04-29 11:41:44 +0000

    Framework++: missed one function rename in wikia.php
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36523 e846bd91-c042-42a7-a218-4b65e2c1904d

--- wikia.php
+++ wikia.php
@@ -2,7 +2,7 @@
 // Initialise common MW code
 require ( dirname( __FILE__ ) . '/includes/WebStart.php' );
 
-$response = F::app()->dispatch( null, null, null, false );
+$response = F::app()->sendRequest( null, null, null, false );
 
 $response->sendHeaders();
 $response->render();
