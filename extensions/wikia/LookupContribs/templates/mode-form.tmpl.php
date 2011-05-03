018215099ab728be42fe48ceaa9521fd371a9aca
uberfuzzy <uberfuzzy@wikia-inc.com>
2011-05-03 19:56:03 +0000

    fix for double quotes in names (BugId:3601)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36582 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/LookupContribs/templates/mode-form.tmpl.php
+++ extensions/wikia/LookupContribs/templates/mode-form.tmpl.php
@@ -131,7 +131,7 @@ $(document).ready(function() {
 <form method="post" action="<?=$action?>" id="lc-form">
 <div class="lc_filter">
 	<span class="lc_filter lc_first"><?= wfMsg('lookupcontribsselectuser') ?></span>
-	<span class="lc_filter"><input type="text" name="target" id="lc_search" size="50" value="<?=$username?>"></span>
+	<input type="text" name="target" id="lc_search" size="50" value="<?= htmlentities($username); ?>"></span>
 	<span class="lc_filter"><input type="button" value="<?=wfMsg('lookupcontribsgo')?>" id="lc-showuser" onclick="submit();"></span>
 </div>
 </form>
