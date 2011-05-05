a7642c65a83d9e82fc770b000698309c6800750c
macbre <macbre@wikia-inc.com>
2011-05-05 09:33:22 +0000

    WikiActivity has lost Followed Pages and See all activity links (BugId:5125) - revert r36436
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36643 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/SpecialThemeDesignerPreview.class.php
+++ extensions/wikia/ThemeDesigner/SpecialThemeDesignerPreview.class.php
@@ -9,11 +9,11 @@ class SpecialThemeDesignerPreview extends UnlistedSpecialPage {
 	public function execute() {
 		wfProfileIn( __METHOD__ );
 		global $wgOut, $wgExtensionsPath, $wgCacheBuster;
-
+		
 		$wgOut->allowClickjacking();
 
 		$this->setHeaders();
-
+		
 		$wgOut->setPageTitle('Example Page Title');
 
 		$wgOut->addScript('<script src="'. $wgExtensionsPath .'/wikia/ThemeDesigner/js/ThemeDesignerPreview.js?'. $wgCacheBuster .'"></script>');
@@ -38,29 +38,27 @@ class SpecialThemeDesignerPreview extends UnlistedSpecialPage {
 	 *
 	 * @author macbre
 	 */
-	static function modifyHeaderData(&$controller, &$params) {
+	static function modifyHeaderData(&$moduleObject, &$params) {
 		global $wgExtensionsPath;
 		wfProfileIn(__METHOD__);
 
 		// fake static data for ThemeDesignerPreview
-		$response = $controller->getResponse();
-
-		$response->setVal('revisions', array(
+		$moduleObject->revisions = array(
 			'current' => array(
 				'user' => 'foo',
 				'avatarUrl' => "{$wgExtensionsPath}/wikia/ThemeDesigner/images/td-avatar.jpg",
 				'link' => '<a>FunnyBunny</a>',
 				'timestamp' => ''
 			),
-		));
+		);
 
-		$response->setVal('action', array("text" => "Edit this page"));
-		$response->setVal('actionImage', '');
-		$response->setVal('actionName', 'edit');
-		$response->setVal('categories',array("<a>More Sample</a>", "<a>Others</a>"));
-		$response->setVal('comments', 23);
-		$response->setVal('dropdown',  array('foo', 'bar'));
-		$response->setVal('pageSubtitle', false);
+		$moduleObject->categories = array("<a>More Sample</a>", "<a>Others</a>");
+		$moduleObject->comments = 23;
+		$moduleObject->pageSubtitle = false;
+		$moduleObject->action = array("text" => "Edit this page");
+		$moduleObject->actionImage = '';
+		$moduleObject->actionName = 'edit';
+		$moduleObject->dropdown = array('foo', 'bar');
 
 		wfProfileOut(__METHOD__);
 		return true;
