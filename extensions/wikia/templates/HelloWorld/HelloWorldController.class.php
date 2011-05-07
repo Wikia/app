a6fa060cda7f91ba82061de6bde29247d10cab24
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-05-07 13:45:19 +0000

    added image
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36742 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/templates/HelloWorld/HelloWorldController.class.php
+++ extensions/wikia/templates/HelloWorld/HelloWorldController.class.php
@@ -11,7 +11,7 @@ class HelloWorldController extends WikiaController {
 	 * This function simply exists to render the index template
 	 */
 	public function index() {
-			
+		$this->setVal( 'html5logo', $this->wg->ExtensionsPath . '/wikia/templates/HelloWorld/images/html5logo.png');
 	}
 
 }
--- extensions/wikia/templates/HelloWorld/templates/HelloWorld_index.php
+++ extensions/wikia/templates/HelloWorld/templates/HelloWorld_index.php
@@ -1,1 +1,4 @@
-<b>some bold text</b>
+<div>
+	<b>Wanna see the HTML5 logo?</b>
+	<img src="<?= $html5logo ?>">
+</div>
