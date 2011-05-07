632819dcb234ad8fdf9afecef89e3c4e20bd0740
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-05-07 13:35:38 +0000

    added aajx
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36741 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/templates/HelloWorld/HelloWorldController.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This is an example use of controller
+ * @author Christian
+ *
+ */
+class HelloWorldController extends WikiaController {
+
+	/**
+	 * This function simply exists to render the index template
+	 */
+	public function index() {
+			
+	}
+
+}
