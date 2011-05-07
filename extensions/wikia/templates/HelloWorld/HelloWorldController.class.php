0aa34713f006fd2f6dd71a285b26e74100706531
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-05-07 13:46:26 +0000

    added comments
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36743 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/templates/HelloWorld/HelloWorldController.class.php
+++ extensions/wikia/templates/HelloWorld/HelloWorldController.class.php
@@ -11,6 +11,8 @@ class HelloWorldController extends WikiaController {
 	 * This function simply exists to render the index template
 	 */
 	public function index() {
+		// This is how a variable is set that can be used in the template.
+		// It will be accessible as $html5logo in the template.
 		$this->setVal( 'html5logo', $this->wg->ExtensionsPath . '/wikia/templates/HelloWorld/images/html5logo.png');
 	}
 
