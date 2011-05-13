24dc83faa274c3f583cd928bc371e0da441001f6
macbre <macbre@wikia-inc.com>
2011-05-13 09:22:56 +0000

    JSLint: example code works
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36905 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/jslint.php
+++ tests/jslint.php
@@ -3,7 +3,22 @@
 /**
  * JS Lint metics tool for CruiseControl
  *
+ * This script is called using the following command:
+ *
+ * SERVER_ID=156 jslint.php
+ *	 --log-junit ${project.dir}/build/logs/jslint.xml
+ *	 --conf ${project.dir}/config/LocalSettings.php
+ *
  * @author Maciej Brencz
  */
+
+require_once('../maintenance/commandLine.inc');
+
+$file = "{$IP}/extensions/wikia/VideoEmbedTool/js/VET.js";
+#$file = "{$IP}/skins/oasis/js/LatestPhotos.js";
+
+exec("java -jar {$IP}/lib/compiler.jar --debug --warning_level DEFAULT --js_output_file /dev/null --js {$file} 2>&1", $out, $res);
+
+var_dump($out);
 
 exit(0);
