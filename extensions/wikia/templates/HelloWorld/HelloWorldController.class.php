0125598cbfd6958df28629bb5c5dc7a92c0f552a
owen <owen@wikia-inc.com>
2011-05-13 09:03:53 +0000

    trunk: cleanup and comments on HelloWorld
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36903 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/templates/HelloWorld/HelloWorldController.class.php
+++ extensions/wikia/templates/HelloWorld/HelloWorldController.class.php
@@ -8,7 +8,10 @@
 class HelloWorldController extends WikiaController {
 
 	/**
-	 * This function simply exists to render the index template
+	 * @brief This function simply exists to render the index template
+	 * @details Returns an array containing 'title' and 'url' keys, given a wiki ID
+	 * 
+	 * @responseParam string html5logo Path to image
 	 */
 	public function index() {
 		// This is how a variable is set that can be used in the template.
--- extensions/wikia/templates/HelloWorld/HelloWorldSpecialController.class.php
+++ extensions/wikia/templates/HelloWorld/HelloWorldSpecialController.class.php
@@ -19,15 +19,18 @@ class HelloWorldSpecialController extends WikiaSpecialPageController {
 		parent::__construct( 'HelloWorld', '', false );
 	}
 
+	// Controllers can all have an optional init method
 	public function init() {
 		$this->businessLogic = F::build( 'HelloWorld', array( $this->app->wg->Title ) );
 	}
 
 	/**
-	 * this is default method, which in this example just redirects to Hello method
+	 * @brief this is default method, which in this example just redirects to Hello method
+	 * @details No parameters
+	 * 
 	 */
 	public function index() {
-
+		$this->wg->Out->setPageTitle("Page Title");
 		$this->wg->Out->setPageTitle($this->wf->msg('helloworld-specialpage-title'));
 		$this->wg->Out->addStyle(AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/templates/HelloWorld/css/HelloWorld_Oasis.scss'));
 		$this->wg->Out->addScriptFile($this->wg->ExtensionsPath . '/wikia/templates/HelloWorld/js/HelloWorld.js');
@@ -36,7 +39,8 @@ class HelloWorldSpecialController extends WikiaSpecialPageController {
 	}
 
 	/**
-	 * Hello method
+	 * @brief Hello method
+	 * @details Hello method
 	 *
 	 * @requestParam int $wikiId
 	 * @responseParam string $header
@@ -51,15 +55,12 @@ class HelloWorldSpecialController extends WikiaSpecialPageController {
 		// setting response data
 		$this->setVal( 'header', $this->wf->msg('helloworld-hello-msg') );
 		$this->setVal( 'wikiData', $this->businessLogic->getWikiData( $wikiId ) );
+		$this->setVal( 'controllerData', $this->controllerData );
 
 		// example of setting SpecialPage::mIncluding
 		$this->mIncluding = true;
 
 		$this->wf->profileOut( __METHOD__ );
-	}
-
-	public function getSomeControllerData() {
-		$this->setVal( 'controllerData', $this->controllerData );
 	}
 
 }
