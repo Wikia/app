767cbdf47933d5d7144b6db1cd127452212ba088
sean <sean@wikia-inc.com>
2011-05-16 21:35:51 +0000

    BugzId: 5149 - Fix caching of Chat Entry point. Not really tested because it relates to Varnish caching which isn't on devboxes.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36953 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat/js/ChatRailModule.js
+++ extensions/wikia/Chat/js/ChatRailModule.js
@@ -5,6 +5,9 @@
 	
 	function lazyLoadChatModule() {
 		// Modules are cached for a bit, so we add the username to the URL so it ends up being cached on a per-user basis (so that we don't get the wrong user-lang or avatar).
-		$(".ChatModule").load(wgServer + wgScript + '?action=ajax&rs=moduleProxy&moduleName=ChatRail&actionName=Contents&outputType=html&username=' + encodeURIComponent(wgUserName));
+		// Since caching of modules is not ironed out yet (currently I think they all cache for 24 hours), make a cb based on the minute:
+		var currentTime = new Date();
+		var minuteTimestamp = currentTime.getFullYear() + currentTime.getMonth() + currentTime.getDate() + currentTime.getHours() + currentTime.getMinutes();
+		$(".ChatModule").load(wgServer + wgScript + '?action=ajax&rs=moduleProxy&moduleName=ChatRail&actionName=Contents&outputType=html&username=' + encodeURIComponent(wgUserName) + '&cb=' + minuteTimestamp);
 	}
 })();
