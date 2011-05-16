bcd3cbbf4004fb628ff8605f1b884f49e352bb09
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-05-16 18:07:53 +0000

    BugzId: 5149 - make the entry-point cached on a per-user basis.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@36950 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat/js/ChatRailModule.js
+++ extensions/wikia/Chat/js/ChatRailModule.js
@@ -4,6 +4,7 @@
 	});
 	
 	function lazyLoadChatModule() {
-		$(".ChatModule").load(wgServer + wgScript + '?action=ajax&rs=moduleProxy&moduleName=ChatRail&actionName=Contents&outputType=html');
+		// Modules are cached for a bit, so we add the username to the URL so it ends up being cached on a per-user basis (so that we don't get the wrong user-lang or avatar).
+		$(".ChatModule").load(wgServer + wgScript + '?action=ajax&rs=moduleProxy&moduleName=ChatRail&actionName=Contents&outputType=html&username=' + encodeURIComponent(wgUserName));
 	}
 })();
