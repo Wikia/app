042a4ed77ebf08420e33f61ffed5d3570daad10d
jakub <jakub@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-05-23 14:33:33 +0000

    avoid warning messages for incompleat printf
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@37194 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Paginator/templates/paginator.tmpl.php
+++ extensions/wikia/Paginator/templates/paginator.tmpl.php
@@ -5,15 +5,15 @@
 			foreach($pages as $page ){
 			?>
 				<? if ( ( $i === 1) && ( $page < $currentPage ) ){
-					?><li><a href="<? printf($url, $currentPage - 1); ?>" data-back="true" data-page="<?=$currentPage - 1; ?>" class="paginator-page special paginator-prev"><?=wfMsg('paginator-back'); ?></a></li><?;
+					?><li><a href="<?=str_replace( '%s', ( $currentPage - 1 ), $url); ?>" data-back="true" data-page="<?=$currentPage - 1; ?>" class="paginator-page special paginator-prev"><?=wfMsg('paginator-back'); ?></a></li><?;
 				} ?>
 				<? if ( $page === '' ){
 					echo '<li><a class="paginator-spacer">...</a></li>';
 				} else {
-					?><li><a href="<? printf($url, $page); ?>" <? if( $currentPage > $page ) echo 'data-back="true"'; ?>  data-page="<?=$page; ?>" class="paginator-page<? if ( $page == $currentPage ) echo ' active'; ?>" ><?=$page; ?></a></li><?
+					?><li><a href="<?=str_replace( '%s', $page, $url); ?>" <? if( $currentPage > $page ) echo 'data-back="true"'; ?>  data-page="<?=$page; ?>" class="paginator-page<? if ( $page == $currentPage ) echo ' active'; ?>" ><?=$page; ?></a></li><?
 				} ?>
 				<? if ( ( $i === count($pages)) && ( $page > $currentPage ) ){
-					?><li><a href="<? printf($url, $currentPage + 1); ?>" data-page="<?=$currentPage + 1; ?>" class="paginator-page special no-border paginator-next"><?=wfMsg('paginator-next'); ?></a></li><?;
+					?><li><a href="<?=str_replace( '%s', ( $currentPage + 1 ), $url); ?>" data-page="<?=$currentPage + 1; ?>" class="paginator-page special no-border paginator-next"><?=wfMsg('paginator-next'); ?></a></li><?;
 				} ?>
 			<?
 			$i++;
