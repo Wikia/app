d547737a750e6ad46d4bd3af497c95c15288f7c7
hyun <hyun@wikia-inc.com>
2011-05-24 17:29:39 +0000

    bugId::6003 removed test that tests non-existant feature
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@37288 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaQuiz/tests/WikiaQuizTest.php
+++ extensions/wikia/WikiaQuiz/tests/WikiaQuizTest.php
@@ -11,27 +11,5 @@ class WikiaQuizTest extends PHPUnit_Framework_TestCase {
 		F::unsetInstance('Category');
 	}
 
-	public function testNewFromName() {
-		$cat = $this->getMock('Category', array('getName', 'getId'), array(), '', false);
-		$cat->expects($this->any())
-		    ->method('getName')
-			->will($this->returnValue('Quiz_foobar'));
-		$cat->expects($this->any())
-		    ->method('getId')
-			->will($this->returnValue(5));
-
-		F::setInstance('Category', $cat);
-
-		$this->markTestIncomplete('Fatal error: Call to undefined method WikiaQuiz::newFromName()');
-		$quiz = WikiaQuiz::newFromName('foobar');
-		$this->assertInstanceOf('WikiaQuiz', $quiz);
-		$this->assertEquals('foobar', $quiz->getName());
-
-		F::unsetInstance('Category');
-		F::setInstance('Category', null);
-		$quiz = WikiaQuiz::newFromName('foobar2');
-		$this->assertFalse($quiz);
-
-	}
 }
 
