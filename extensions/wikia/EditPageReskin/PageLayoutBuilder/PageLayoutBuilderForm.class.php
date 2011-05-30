cbc78aa05d488faaf26ca0cdc093fcc930e2f147
wladek <wladek@wikia-inc.com>
2011-05-30 17:07:37 +0000

    small fix with PLB and paths
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@37530 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditPageReskin/PageLayoutBuilder/PageLayoutBuilderForm.class.php
+++ extensions/wikia/EditPageReskin/PageLayoutBuilder/PageLayoutBuilderForm.class.php
@@ -3,32 +3,32 @@
 class PageLayoutBuilderForm extends SpecialCustomEditPage {
 	protected $formValues = array();
 	protected $fullScreen = false;
-	protected $parser = null; 
+	protected $parser = null;
 	protected $tagValues = false;
-	
+
 	public function __construct() {
 		parent::__construct( 'PageLayoutBuilderForm', 'PageLayoutBuilderForm' );
 	}
-	
+
 	public function renderHeader($par) {
 		$errors = $this->mEditPage->getNotices();
-	
+
 		$oTmpl = new EasyTemplate( dirname( __FILE__ ) . "/templates/" );
 		$oTmpl->set_vars(array(
 			"errors" => $errors,
 			"iserror" => !empty($errors)
 		));
-		
+
 		$this->out->addHTML( $oTmpl->render( "create-article-header" ) );
-		
+
 		$this->forceUserToProvideTitle( 'plb-special-form-article-name' );
 	}
-	
-		
+
+
 	public function renderFooter($par) {
 		$oTmpl = new EasyTemplate( dirname( __FILE__ ) . "/templates/" );
 		$this->out->addHTML( $oTmpl->render( "create-article-footer" ) );
-		
+
 		$rightsText = $this->app->getGlobal('wgRightsText');
 		if ( $rightsText ) {
 			$copywarnMsg = array( 'copyrightwarning',
@@ -55,8 +55,8 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 		htmlspecialchars( wfMsg( 'edithelp' ) ).'</a> ';
 
 		$this->out->addHTML( $cancel.$edithelp );
-	}	
-	
+	}
+
 	public function showOwnTextbox() {
 		$parser = $this->getParser();
 		$text = $parser->preParseForm( $this->layoutArticle->getContent() );
@@ -64,7 +64,7 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 		$parserOptions->setEditSection(false);
 		$parserOut = $parser->parse($text , $this->layoutTitle, $parserOptions);
 		$this->formHTML = $parserOut->getText();
-		
+
 		$oTmpl = new EasyTemplate( dirname( __FILE__ ) . "/templates/" );
 		$oTmpl->set_vars(array(
 			"data" => $this->formValues,
@@ -73,15 +73,15 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 			"iserror" => !empty($this->formErrors),
 			"editmode" => false,//$this->formValues['pageId'] > 0,
 			"layout" => $this->formHTML,
-			
-			//"plbId" => $this->formValues['plbId'], 
+
+			//"plbId" => $this->formValues['plbId'],
 			"isdelete" => false//PageLayoutBuilderModel::layoutIsDelete($this->formValues['plbId']),
 		));
-		
+
 		$this->out->addHTML( $oTmpl->render( "create-article" ) );
 		return true;
 	}
-	
+
 	protected function getParser(){
 		if ($this->parser != null) {
 			return $this->parser;
@@ -91,7 +91,7 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 		$this->parser = $parser;
 		return $parser;
 	}
-	
+
 	/**
 	 * Perform additional checks when saving an article
 	 */
@@ -100,7 +100,7 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 		$formErrors = array();
 		$formErrorsList = array();
 		$formValues = array();
-		
+
 		if(isset($this->elements)) {
 			foreach($this->elements as $key => $value) {
 				$reqVal = $this->request->getVal('plb_'.$key, '');
@@ -115,33 +115,33 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 					}
 				}
 			}
-		} 
-	
+		}
+
 		if(count($formErrors) > 0) {
 			foreach($formErrors as $val) {
 				$this->addEditNotice(  $val );
 			}
 		}
-		
+
 		$this->getParser()->forceFormValue($formValues);
 		$this->getParser()->forceFormErrors($formErrorsList);
-		
+
 		if($this->mode == self::MODE_NEW || $this->mode == self::MODE_NEW_SETUP) {
 			if ( $this->titleStatus == self::STATUS_ALREADY_EXISTS ) {
 				$this->addEditNotice( wfMsg( 'plb-special-form-already-exists' ) );
 			}
-	
+
 			if ( $this->titleStatus == self::STATUS_EMPTY ) {
 				$this->addEditNotice( wfMsg( 'plb-special-form-title-empty' ) );
-				
+
 			}
-			
+
 			if ( $this->titleStatus == self::STATUS_INVALID ) {
 				$this->addEditNotice( wfMsg( 'plb-special-form-invalid-title-error' ) );
 			}
 		}
 	}
-	
+
 	public function getWikitextFromRequest() {
 		$this->tagValues = array();
 		$this->formValues = array();
@@ -153,31 +153,31 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 			$this->formValue[$key] = $reqVal;
 			$this->tagValues['val_'.$key] = $reqVal;
 		}
-		
+
 		if( !empty( $this->pageId ) ) {
 			$pageTitle = Title::newFromID($this->pageId);
 			$oldValues = $parser->loadForm($pageTitle, $this->layoutTitle->getArticleId() );
-			
+
 			foreach($oldValues as $key => $oldValue) {
 				if(!isset($tagValues['val_'.$key])){
 					$tagValues['val_'.$key] = $oldValue;
 				}
 			}
 		}
-		
-		$attribs = $this->tagValues + array("layout_id" => $this->layoutTitle->getArticleID(), 'cswikitext' => $this->request->getVal('csWikitext', '') );	
+
+		$attribs = $this->tagValues + array("layout_id" => $this->layoutTitle->getArticleID(), 'cswikitext' => $this->request->getVal('csWikitext', '') );
 		$mwText = Xml::element("plb_layout", $attribs, '', false);
-		
+
 		if (PageLayoutBuilderModel::layoutIsDelete($this->plbId)) {
 			$preParser = new PageLayoutBuilderParser();
 			$fakeTitle = new FakeTitle();
 			$parserOut = $preParser->preParserArticle($mwText, $fakeTitle );
 			return $parserOut->getText();
 		}
-		
+
 		return $mwText;
 	}
-	
+
 	public function execute( $par ) {
 		if( wfReadOnly() ) {
 			$this->user->readOnlyPage();
@@ -187,47 +187,47 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 		if( $this->user->isBlocked() ) {
 			$this->out->blockedPage();
 			return;
-		}	
+		}
 
 		$staticChute = new StaticChute('js');
 		$staticChute->useLocalChuteUrl();
 		$this->out->addScript($staticChute->getChuteHtmlForPackage('yui'));
-		
-		$this->out->addStyle( AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/EditPageReskin/PageLayoutBuilder/css/form.scss')); 
+
+		$this->out->addStyle( AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/EditPageReskin/PageLayoutBuilder/css/form.scss'));
 		$this->out->addStyle( AssetsManager::getInstance()->getSassCommonURL('skins/oasis/css/core/_EditPage.scss'));
-		
+
 		$js = array(
 			'/wikia/PageLayoutBuilder/widget/allWidgets.js',
 			'/wikia/EditPageLayout/js/editor/WikiaEditor.js',
-			'/wikia/EditPageLayout/js/editor/PageControls.js',
-			'/wikia/PageLayoutBuilder/js/form.js',
+			'/wikia/EditPageLayout/js/plugins/PageControls.js',
+			'/wikia/EditPageReskin/PageLayoutBuilder/js/form.js',
 			'/wikia/EditPageLayout/js/loaders/LayoutBuilderFormEditorLoader.js'
 		);
-		
+
 		foreach( $js as $val ) {
-			$this->out->addScript("<script type=\"text/javascript\" src=\"".$this->app->getGlobal('wgExtensionsPath'). $val . '?' .$this->app->getGlobal('wgStyleVersion')."\"></script>"); 			
+			$this->out->addScript("<script type=\"text/javascript\" src=\"".$this->app->getGlobal('wgExtensionsPath'). $val . '?' .$this->app->getGlobal('wgStyleVersion')."\"></script>");
 		}
 
-		//close every thing in form 
+		//close every thing in form
 		$this->out->addHTML(Xml::element("form", array("method" => "POST", "action" => $this->request->getFullRequestURL()."&action=submit"), '', true));
 		parent::execute( $par );
 		$this->out->addHTML('</form>');
 	}
-	
+
 	protected function initializeTitle($postedTitle) {
 		$this->plbId = (int) $this->getField('plbId',''); // layout ID
 		$this->layoutTitle = Title::newFromID($this->plbId);
-		
+
 		if($this->plbId == 0 || (!$this->layoutTitle instanceof  Title) || $this->layoutTitle->getNamespace() !=  NS_PLB_LAYOUT ) {
 			$this->executeIsNolayout();
 			return false;
 		}
-		
+
 		$this->layoutArticle = new Article($this->layoutTitle);
-		
+
 		parent::initializeTitle($postedTitle);
 	}
-	
+
 	protected function afterArticleInitialize($mode, $title, $article) {
 		$this->formValues = array();
 		$parser = $this->getParser();
@@ -236,20 +236,20 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 				$this->out->permissionRequired( 'edit' );
 				return false;
 			}
-			
+
 			if( $this->plbId != PageLayoutBuilderModel::articleIsFromPLB( $this->getEditedTitle()->getArticleId() ) ) {
 				$this->executeIsNoArtile();
 			}
-			
+
 			$loadedValues = $parser->loadFormFromText($this->getEditPage()->getContent(), $article->getId(), $this->plbId );
-		}		
+		}
 		if(!empty($loadedValues['cswikitext'])) {
 			CategorySelect::SelectCategoryAPIgetData($loadedValues['cswikitext'], true);
 		}
-		
+
 		return true;
 	}
-	
+
 	public function getPageTitle() {
 		if ($this->getEditedArticle()->exists()) {
 			return wfMsg( 'plb-special-form-edit-article', array("$1" => $this->getEditedTitle()->getText() ));
@@ -266,8 +266,8 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 	function executeIsNoArtile() {
 		$this->out->showErrorPage( 'plb-special-no-article', 'plb-special-no-article-body', array(wfGetReturntoParam()));
 	}
-	
-	
+
+
 	static public function blockCategorySelect(){
 		global $wgTitle;
 		if( self::articleIsFromPLBFull($wgTitle->getArticleID(), "") ) {
@@ -275,8 +275,8 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 		}
 		return true;
 	}
-	
-	
+
+
 
 	public static function articleIsFromPLBFull($id, $text) {
 		//TODO: cache it by revison
@@ -289,7 +289,7 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 		}
 		return false;
 	}
-	
+
 	function afterSave( $status ) {
 		switch( $status ) {
 			case EditPage::AS_SUCCESS_UPDATE:
@@ -309,7 +309,7 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 				break;
 		}
 	}
-	
+
 	/**
 	 * Hook entry
 	 *
@@ -327,17 +327,17 @@ class PageLayoutBuilderForm extends SpecialCustomEditPage {
 			$undo = $wgRequest->getVal('undo', '');
 			$layout_id = PageLayoutBuilderModel::articleIsFromPLB( $oEditPage->mTitle->getArticleID() );
 			$oSpecialPageTitle = Title::newFromText('LayoutBuilderForm', NS_SPECIAL);
-			
+
 			$url = "plbId=" . $layout_id . "&pageId=".$oEditPage->mTitle->getArticleId();
-			
+
 			if(!empty($undoafter)) {
-				$url .= "&undoafter=".$undoafter; 
+				$url .= "&undoafter=".$undoafter;
 			}
-			
+
 			if(!empty($undo)) {
-				$url .= "&undo=".$undo;				
-			}			
-			
+				$url .= "&undo=".$undo;
+			}
+
 			$wgOut->redirect($oSpecialPageTitle->getFullUrl($url));
 		}
 
