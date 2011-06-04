2e3fa348edcdd719c9c00ad462cd10e6b10e4f0e
hyun <hyun@wikia-inc.com>
2011-06-04 00:05:20 +0000

    FounderEmails: refactoring before I regret it later
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@37743 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/FounderEmails/FounderEmailsModule.class.php
+++ extensions/wikia/FounderEmails/FounderEmailsModule.class.php
@@ -15,8 +15,7 @@ class FounderEmailsModule extends Module {
 					'$UNIQUEVIEWS' => '6')
 			);
 		} else if(!empty($type)) {
-			$executionType = $type == 'complete-digest' ? 'CompleteDigest' : 'GeneralUpdate';
-			$this->previewBody = wfRenderModule("FounderEmails", $executionType, 
+			$this->previewBody = wfRenderModule("FounderEmails", 'GeneralUpdate', 
 				array('type' => $type, 
 					'language' => 'en', 
 					'$PAGEURL' => 'http://www.wikia.com',
@@ -59,18 +58,19 @@ class FounderEmailsModule extends Module {
 	 * 
 	 */
 	public function executeGeneralUpdate($params) {
-		$type = $params['type'];
+		$this->type = $params['type'];
 		$this->language = $params['language'];
-		$this->greeting = wfMsgForContent('founderemails-email-'.$type.'-greeting');
-		$this->headline = wfMsgForContent('founderemails-email-'.$type.'-headline');
-		$this->content = wfMsgForContent('founderemails-email-'.$type.'-content');
-		$this->signature = wfMsgForContent('founderemails-email-'.$type.'-signature');
-		$this->button = wfMsgForContent('founderemails-email-'.$type.'-button');
-		//$this->buttonUrl = 'http://www.wikia.com';
+		$this->greeting = wfMsgForContent('founderemails-email-'.$this->type.'-greeting');
+		$this->headline = wfMsgForContent('founderemails-email-'.$this->type.'-headline');
+		$this->signature = wfMsgForContent('founderemails-email-'.$this->type.'-signature');
+		$this->button = wfMsgForContent('founderemails-email-'.$this->type.'-button');
+		if ($this->type != 'complete-digest') {
+			$this->content = wfMsgForContent('founderemails-email-'.$this->type.'-content');
+		}
 		if (isset($params['$PAGEURL'])) {
 			$this->buttonUrl = $params['$PAGEURL'];
 		}
-		switch($type) {
+		switch($this->type) {
 			case 'user-registered':
 				$this->buttonUrl = $params['$USERTALKPAGEURL'];
 				break;
@@ -86,23 +86,16 @@ class FounderEmailsModule extends Module {
 			case 'views-digest':
 				break;
 			case 'complete-digest':
+				$this->heading1 = wfMsgForContent('founderemails-email-complete-digest-content-heading1');
+				$this->content1 = wfMsgForContent('founderemails-email-complete-digest-content1');
+				$this->heading2 = wfMsgForContent('founderemails-email-complete-digest-content-heading2');
+				$this->content2 = wfMsgForContent('founderemails-email-complete-digest-content2');
+				$this->heading3 = wfMsgForContent('founderemails-email-complete-digest-content-heading3');
+				$this->content3 = wfMsgForContent('founderemails-email-complete-digest-content3');
 				break;
 			default:
 				break;
 		}
 	}
-	
-	public function executeCompleteDigest($params) {
-		$this->greeting = wfMsgForContent('founderemails-email-complete-digest-greeting');
-		$this->headline = wfMsgForContent('founderemails-email-complete-digest-headline');
-		$this->heading1 = wfMsgForContent('founderemails-email-complete-digest-content-heading1');
-		$this->content1 = wfMsgForContent('founderemails-email-complete-digest-content1');
-		$this->heading2 = wfMsgForContent('founderemails-email-complete-digest-content-heading2');
-		$this->content2 = wfMsgForContent('founderemails-email-complete-digest-content2');
-		$this->heading3 = wfMsgForContent('founderemails-email-complete-digest-content-heading3');
-		$this->content3 = wfMsgForContent('founderemails-email-complete-digest-content3');
-		$this->signature = wfMsgForContent('founderemails-email-complete-digest-signature');
-		$this->button = wfMsgForContent('founderemails-email-complete-digest-button');
-		$this->buttonUrl = $params['$PAGEURL'];
-	}
+
 }
--- extensions/wikia/FounderEmails/templates/FounderEmails_GeneralUpdate.php
+++ extensions/wikia/FounderEmails/templates/FounderEmails_GeneralUpdate.php
@@ -36,7 +36,32 @@
 <td style="color:#2c85d5;font-size:24px"><?= $headline ?></td>
 </tr>
 <tr>
-<td style="color:#3a3a3a;font-size:14px;line-height:20px"><?= $content ?></td>
+<td style="color:#3a3a3a;font-size:14px;line-height:20px">
+	<? if ($type == 'complete-digest') { ?>
+		<table cellpadding="0" cellspacing="0" width="100%">
+			<tr>
+				<td style="color:#2c85d5;font-size:17px;font-weight:bold"><?= $heading1 ?></td>
+			</tr>
+			<tr>
+				<td style="color:#3a3a3a;font-size:14px;line-height:20px"><?= $content1 ?><br><br></td>
+			</tr>
+			<tr>
+				<td style="color:#2c85d5;font-size:17px;font-weight:bold"><?= $heading2 ?></td>
+			</tr>
+			<tr>
+				<td style="color:#3a3a3a;font-size:14px;line-height:20px"><?= $content2 ?><br><br></td>
+			</tr>
+			<tr>
+				<td style="color:#2c85d5;font-size:17px;font-weight:bold"><?= $heading3 ?></td>
+			</tr>
+			<tr>
+				<td style="color:#3a3a3a;font-size:14px;line-height:20px"><?= $content3 ?></td>
+			</tr>
+		</table>
+	<? } else { ?>
+		<?= $content ?>
+	<? } ?>
+</td>
 </tr>
 <? if (!empty($buttonUrl)) { ?>
 <tr>
