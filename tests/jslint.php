b409ffddd266e67c3ba704bf269de737fc60d5fd
macbre <macbre@wikia-inc.com>
2011-06-06 14:18:26 +0000

    JSLint: cover all JS files in our repo (1900+ files)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@37769 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/jslint.php
+++ tests/jslint.php
@@ -10,6 +10,8 @@
  *	 --conf ${project.dir}/config/LocalSettings.php
  * 	 --output ${project.dir}/build/jslint/report.html
  *
+ * Beware! It's still a hack :)
+ *
  * @author Maciej Brencz
  */
 
@@ -21,6 +23,8 @@ function jslint($file) {
 
 	$warnings = array();
 
+	// TODO: try nodejs jslint.js
+	// @see https://github.com/jquery/jquery/blob/master/Makefile
 	exec("java -jar {$IP}/lib/compiler.jar --debug --warning_level DEFAULT --compilation_level WHITESPACE_ONLY --js_output_file /dev/null --js {$file} 2>&1", $out, $res);
 	#var_dump($out);
 
@@ -37,13 +41,43 @@ function jslint($file) {
 	return $warnings;
 }
 
+/**
+ * find files matching a pattern
+ * using PHP "glob" function and recursion
+ *
+ * @see http://www.redips.net/php/find-files-with-php/
+ *
+ * @return array containing all pattern-matched files
+ *
+ * @param string $dir     - directory to start with
+ * @param string $pattern - pattern to glob for
+ */
+function find($dir, $pattern){
+	// escape any character in a string that might be used to trick
+	// a shell command into executing arbitrary commands
+	$dir = escapeshellcmd($dir);
+	// get a list of all matching files in the current directory
+	$files = glob("$dir/$pattern");
+	// find a list of all directories in the current directory
+	// directories beginning with a dot are also included
+	foreach (glob("$dir/{.[^.]*,*}", GLOB_BRACE|GLOB_ONLYDIR) as $sub_dir){
+	    $arr   = find($sub_dir, $pattern);  // resursive call
+	    $files = array_merge($files, $arr); // merge array with files from subdirectory
+	}
+	// return all found files
+    return $files;
+}
+
 $outputFile = isset($options['output']) ? $options['output'] : '/tmp/jslint.html';
 
 echo "JS lint report will be generated in {$outputFile}...\n";
 
-$files = glob("{$IP}/skins/common/*.js");
-#$files = glob("{$IP}/skins/common/jquery/*.js");
-#$files = array("{$IP}/extensions/wikia/VideoEmbedTool/js/VET.js");
+$files = find($IP, '*.js');
+//$files = find("{$IP}/skins/", '*.js');
+//$files = find("{$IP}/skins/common/jquery", '*.js');
+//$files = find("{$IP}/extensions/wikia/", '*.js');
+
+var_dump($files); die();
 
 $warnings = array();
 
