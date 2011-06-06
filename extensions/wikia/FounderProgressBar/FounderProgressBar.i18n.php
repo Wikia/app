e786f85dccbf9f5e6a87eb1e177b270a29f67508
owen <owen@wikia-inc.com>
2011-06-06 22:36:51 +0000

    trunk: FounderProgressBar skeleton
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@37807 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/FounderProgressBar/FounderProgressBar.i18n.php
@@ -0,0 +1,7 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'founderprogressbar-credits' => 'A task hub and guide for new wiki founders',
+);
---
+++ extensions/wikia/FounderProgressBar/FounderProgressBar.setup.php
@@ -0,0 +1,10 @@
+<?php
+$wgExtensionCredits['other'][] = array(
+	'path'           => __FILE__,
+	'name'           => 'FounderProgressBar',
+	'author'         => 'Wikia',
+	'descriptionmsg' => 'founderprogressbar-credits',
+);
+
+$dir = dirname(__FILE__).'/';
+
