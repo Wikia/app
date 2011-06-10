c6494f906fea58916b0454f91ae57db64cf8e118
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-06-10 01:12:54 +0000

    trunk: fixed WikiAdoption test bugid:6688
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38000 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AutomaticWikiAdoption/tests/AutomaticWikiAdoptionGatherDataTest.php
@@ -0,0 +1,39 @@
+<?php
+global $wgAutoloadClasses, $IP;
+$wgAutoloadClasses['AutomaticWikiAdoptionGatherData']  =  $IP.'/extensions/wikia/AutomaticWikiAdoption/maintenance/AutomaticWikiAdoptionGatherData.php';
+
+class AutomaticWikiAdoptionGatherDataTest extends PHPUnit_Framework_TestCase {
+	private $dataMapper;
+
+	/**
+	 * Script can do 3 actions, this tests all three
+	 * @dataProvider maintenanceScriptDataProvider
+	 */
+	
+	function testMaintenanceScript($action, $id, $days, $jobArgs) {
+		
+		$wikiData = array(
+					'recentEdit' => strtotime('-' . $days . ' days'),
+					'admins' => array(1,2,3)
+				);
+		
+		$mock = $this->getMock('AutomaticWikiAdoptionGatherData', array('getRecentAdminEdits', $action));
+		$mock->expects($this->once())
+				->method('getRecentAdminEdits')
+				->will($this->returnValue(array($id => $wikiData)));
+		
+		$mock->expects($this->once())
+				->method($action) //'setAdoptionFlag' or 'sendEmail'
+				->with($this->anything(), $this->equalTo($jobArgs), $this->equalTo($id), $this->equalTo($wikiData));
+		
+		$mock->run(array('quiet'=>1));
+	}
+	
+	function maintenanceScriptDataProvider() {
+		return array(
+			array('sendMail', 4, 16, array('mailType' => 'first')),
+			array('sendMail', 4, 29, array('mailType' => 'second')),
+			array('setAdoptionFlag', 4, 31, array())
+		);
+	}
+}
