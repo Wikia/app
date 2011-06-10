cba4854b3ee3484a1e6caabf9ac35be2b87ed1b2
macbre <macbre@wikia-inc.com>
2011-06-10 11:08:57 +0000

    JSLint found an error in UserInterviewAjax.js (BugId:6763)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38008 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserInterview/js/UserInterviewAjax.js
+++ extensions/wikia/UserInterview/js/UserInterviewAjax.js
@@ -1,21 +1,16 @@
-$(function() {
-	UserInterviewHeader.init();
-});
-
-
 var UserInterviewHeader = {
 	init: function() {
 		UserInterviewHeader.attachEventListeners();
 		UserInterviewHeader.addTrackclicking();
 	},
-	
+
 	attachEventListeners: function() {
 		$('a.toggle-user-interview').click(function() {
 			 $("#user-interview-form").slideToggle(400, function () {
              });
 			return false;
 		});
-		
+
 		$('#user-interview-form').submit(function() {
 			var data = $('#user-interview-form').serialize();
 
@@ -31,19 +26,23 @@ var UserInterviewHeader = {
 					$("#user-interview-form").slideToggle(200, function () {
 						window.location.reload();
 					});
-				
+
 				}
 				if (res.error) {
 					alert(res.error);
 				}
 			});
-	
+
 			return false;
 		});
 	},
-	
+
 	addTrackclicking: function() {
 		$('.toggle-user-interview').trackClick('userinterview/togglebutton');
 		$('#UserInterviewSave').trackClick('userinterview/saveinterviewbutton');
-	},
+	}
 }
+
+$(function() {
+	UserInterviewHeader.init();
+});
