054ba1e6e24296352d7363852243612cc16f1889
hyun <hyun@wikia-inc.com>
2011-06-14 22:37:32 +0000

    ControlCenter: work in progress
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38122 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ControlCenter/js/ControlCenter.js
+++ extensions/wikia/ControlCenter/js/ControlCenter.js
@@ -32,11 +32,16 @@ var ControlCenter = {
 		ControlCenter.ui.showSection('contentarea');
 		
 		// load content
+		var control = $(this).data('control');
+		if(control) {
+			ControlCenter.contentload['load'+control]();
+		}
 	},
 	ui: {
 		resetAll: function() {
 			ControlCenter.ui.deselectAllTabs();
 			ControlCenter.ui.hideAllSections();
+			ControlCenter.section.contentarea.html('Loading...');	//i18n this later
 		},
 		hideAllSections: function() {
 			for(var s in ControlCenter.section) {
@@ -54,8 +59,28 @@ var ControlCenter = {
 		}
 	},
 	contentload: {
-		loadUserList: function() {
-			
+		loadPage: function(page, callback) {
+			ControlCenter.section.contentarea.load(wgScriptPath + '/wikia.php', {
+				controller: 'ControlCenterSpecialPage',
+				method: 'GetSpecialPage',
+				page: page
+			}, function(response){
+				if(callback && typeof callback == 'function') {
+					callback();
+				}
+			});
+		},
+		loadListUsers: function() {
+			var sassUrl = $.getSassCommonURL('/extensions/wikia/Listusers/css/table.scss');
+			$.getResources([wgScriptPath + '/extensions/wikia/Listusers/js/jquery.dataTables.min.js',
+				wgScriptPath + '/extensions/wikia/Listusers/css/table.css',
+				sassUrl
+			], function() {
+				ControlCenter.contentload.loadPage('ListUsers');
+			});
+		},
+		loadUserRights: function() {
+			ControlCenter.contentload.loadPage('UserRightsPage');
 		}
 	}
 };
