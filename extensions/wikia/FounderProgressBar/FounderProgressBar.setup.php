9116667fd827be5fa22f3dfc7508b84256a8962c
owen <owen@wikia-inc.com>
2011-06-14 20:02:10 +0000

    trunk: initial progress bar skeleton
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38114 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/FounderProgressBar/FounderProgressBar.setup.php
+++ extensions/wikia/FounderProgressBar/FounderProgressBar.setup.php
@@ -8,3 +8,24 @@ $wgExtensionCredits['other'][] = array(
 
 $dir = dirname(__FILE__).'/';
 
+//Autoload
+$wgAutoloadClasses[ 'FounderProgressBarController' ] = $dir . '/FounderProgressBarController.class.php';
+$wgAutoloadClasses[ 'FounderProgressBarHooks' ] = $dir . '/FounderProgressBarHooks.class.php';
+
+// I18N
+$wgExtensionMessagesFiles['FounderProgressBarController'] = $dir . '/FounderProgressBarController.i18n.php';
+
+// SETUP
+$wgExtensionFunctions[] = 'FounderProgressBar_setup';
+
+
+function FounderProgressBar_setup () {
+	wfProfileIn(__METHOD__);
+	$app = F::app();
+	
+	// Register hooks for founder events
+	$app->registerHook("ArticleSaveComplete", "FounderProgressBarHooks", "onArticleSave");
+	$app->registerHook("ImageUploadComplete", "FounderProgressBarHooks", "onImageUploadComplete");
+
+	wfProfileOut(__METHOD__);	
+}
