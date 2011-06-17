937c17e44ed4fb30e18329792b84afdce715e9d6
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-06-17 14:41:45 +0000

    test configuration
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38251 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/SemanticMediaWikiTrunk/Wikia_Settings.php
+++ extensions/SemanticMediaWikiTrunk/Wikia_Settings.php
@@ -9,7 +9,19 @@
  */
 
 $smwgDefaultStore = "SMWSparqlStore";
-$smwgSparqlDatabase = 'SMWSparqlDatabase4Store'; 
-$smwgSparqlQueryEndpoint = 'http://localhost:9000/sparql/';
-$smwgSparqlUpdateEndpoint = 'http://localhost:9000/update/';
-$smwgSparqlDataEndpoint = 'http://localhost:9000/data/';
+
+switch( $wgCityId ) {
+
+	// familypedia2/allegrograph
+	case 232959:
+		$smwgSparqlQueryEndpoint = 'http://smw:smw@localhost:10035/repositories/smw/';
+		$smwgSparqlUpdateEndpoint = 'http://smw:smw@localhost:10035/repositories/smw/';
+		$smwgSparqlDataEndpoint = false;
+		break;
+
+	default:
+		$smwgSparqlDatabase = 'SMWSparqlDatabase4Store';
+		$smwgSparqlQueryEndpoint = 'http://localhost:9000/sparql/';
+		$smwgSparqlUpdateEndpoint = 'http://localhost:9000/update/';
+		$smwgSparqlDataEndpoint = 'http://localhost:9000/data/';
+}
