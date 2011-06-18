b27eb228fc3df8abefb54855bd258054aadc75ea
hyun <hyun@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-06-18 00:55:06 +0000

    FounderProgressBar: work in progress
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38268 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/FounderProgressBar/templates/FounderProgressBar_widget.php
+++ extensions/wikia/FounderProgressBar/templates/FounderProgressBar_widget.php
@@ -1,14 +1,59 @@
+<section id="FounderProgressList" class="FounderProgressList" style="display:none;">
+	<header>
+		<h1>Glee Wiki's Tasks</h1>
+		<p class="task-description">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nunc mi. Maecenas elit velit, tempus quis pharetra a, fringilla blandit neque.
+		</p>
+	</header>
+	<ul class="tasks">
+		<li class="task">
+			<div class="task-label">
+				Tasks
+			</div>
+			<div class="task-group">
+				<? for ($j = 0; $j < 3; $j++) { ?>
+					<ul class="activities">
+						<? for ($i = 0; $i < 10; $i++) { ?>
+							<li class="activity">
+								<div class="activity-label">Activity is number <?= $i ?></div>
+								<div class="activity-description">
+									<div class="description">
+										<h4>Activity whatever</h4>
+										Description of this activity.  Description of this activity.  Description of this activity.
+									</div>
+									<div class="actions">
+										<button>Do something</button>
+									</div>
+								</div>
+							</li>
+						<? } ?>
+					</ul>
+				<? } ?>
+			</div>
+		</li>
+		<li class="task">
+			<div class="task-label">
+				Skipped Tasks
+			</div>
+		</li>
+		<li class="task">
+			<div class="task-label">
+				Bonus Tasks
+			</div>
+		</li>
+	</ul>
+</section>
 <section id="FounderProgressWidget">
 	<h1>Glee Wiki's Progress</h1>
 	<section class="preview">
 		<canvas id="FounderProgressBar" class="founder-progress-bar" height="95" width="95">
 		</canvas>
 		<div class="numeric-progress">
-			<span class="score">20</span><span class="percentage">%</span>
+			<span class="score">68</span><span class="percentage">%</span>
 		</div>
 		<header>
 			<h1><?= wfMsg('founderprogressbar-progress-label') ?></h1>
-			<a href="#" class="list-toggle"><?= wfMsg('founderprogressbar-progress-see-full-list') ?></a>
+			<a href="#" class="list-toggle" id="FounderProgressListToggle"><?= wfMsg('founderprogressbar-progress-see-full-list') ?></a>
 		</header>
 		<ul class="activities">
 			<li class="activity active">
