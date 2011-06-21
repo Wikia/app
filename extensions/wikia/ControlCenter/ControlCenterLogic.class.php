35d354409747e75788b8792131a1199d600c6067
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-06-21 21:57:32 +0000

    trunk: Founder Progress Bar wip
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38359 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ControlCenter/ControlCenterLogic.class.php
+++ extensions/wikia/ControlCenter/ControlCenterLogic.class.php
@@ -11,7 +11,12 @@ class ControlCenterLogic {
 	 * @param type $title Title of page we are on
 	 * @return type boolean 
 	 */
-	public static function displayControlCenter($title) {
+	public static function displayControlCenter($app, $title) {
+		
+		// Control center is only for logged in plus a list of groups
+		// FIXME: make this a right and add it to those groups instead
+		if (!$app->wg->User->isLoggedIn()) return false;
+		if (count(array_intersect(array('sysop', 'staff', 'bureaucrat'), $app->wg->User->getGroups())) == 0) return false;
 		
 		$pageList = array ( "ControlCenter", "UserRights", "ListUsers", "RecentChanges", "WikiaLabs", "Categories", "MultipleUpload", "SponsorshipDashboard");
 //		print_pre($title->getDBKey());
--- extensions/wikia/FounderProgressBar/founder_progress_bar.sql
+++ extensions/wikia/FounderProgressBar/founder_progress_bar.sql
@@ -1,9 +1,9 @@
-CREATE TABLE founder_progress_bar (
+CREATE TABLE founder_progress_bar_tasks (
         `wiki_id` int(10) NOT NULL,
-        `action_id` int(10) NOT NULL,
-        `action_count` int(10) DEFAULT '0',
-		`action_completed` tinyint(1) NOT NULL DEFAULT '0',
-        `action_skipped` tinyint(1) NOT NULL DEFAULT '0',
-		`action_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-    PRIMARY KEY ( `game_id` )
+        `task_id` int(10) NOT NULL,
+        `task_count` int(10) DEFAULT '0',
+		`task_completed` tinyint(1) NOT NULL DEFAULT '0',
+        `task_skipped` tinyint(1) NOT NULL DEFAULT '0',
+		`task_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ( `wiki_id`, `task_id` )
 ) ENGINE=InnoDB;
