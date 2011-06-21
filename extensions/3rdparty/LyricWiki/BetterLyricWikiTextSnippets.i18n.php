b2c7a26e7b5f2fd717061076296337785097fb69
sean <sean@wikia-inc.com>
2011-06-21 20:39:01 +0000

    BugzId 7301: Make the ArticleService::getTextSnippet() be awesome for LyricWiki.  Instead of random header garbage, it returns a snippet of the actual lyrics.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38354 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/3rdparty/LyricWiki/BetterLyricWikiTextSnippets.i18n.php
@@ -0,0 +1,7 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'lw-snippets-desc' => "This extension makes more useful text snippets be returned by the Wikia ArticleService's getTextSnippets() function when used on LyricWiki.",
+);
