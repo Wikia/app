7b895ea715c0d27ee021e21281e523bc54d3ea42
will <will@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-06-22 22:01:16 +0000

    Bug ID 7307: suppress ads during login flow
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38429 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/common.js
+++ skins/oasis/js/common.js
@@ -1,24 +1,22 @@
 /**
- * Get all Oasis's ads as jQuery collection (and cache the results)
- *
- * @author macbre
- */
-jQuery.getAds = function() {
-	if (!jQuery.adsNodes) {
-		jQuery.adsNodes = $("[id$='TOP_LEADERBOARD'], [id$='TOP_RIGHT_BOXAD'], .wikia-ad").children();
-	}
-
-	return jQuery.adsNodes;
-};
-
-/**
  * "Safely" hide all ads
  *
  * @author macbre
  * @author Hyun Lim
+ * @author wlee
  */
 jQuery.hideAds = function() {
-	jQuery.getAds().css('margin-left', '-9999px');
+	var slots = { 'CORP_TOP_LEADERBOARD': 1, 'CORP_TOP_RIGHT_BOXAD': 1, 'HOME_TOP_LEADERBOARD': 1, 'HOME_TOP_RIGHT_BOXAD': 1, 'HOME_TOP_RIGHT_BUTTON': 1, 'TEST_HOME_TOP_RIGHT_BOXAD': 1, 'TEST_TOP_RIGHT_BOXAD': 1, 'TOP_LEADERBOARD': 1, 'TOP_RIGHT_BOXAD': 1, 'TOP_RIGHT_BUTTON': 1 }
+	var tags = { 'div': 1, 'iframe': 1, 'img': 1 }
+	var styleContent = '';
+	for (var i in slots) {
+		for (var j in tags) {
+			styleContent += ' #' + i + ' ' + j + ',';
+		}
+	}
+	styleContent = styleContent.substr(0, styleContent.length-1); // remove trailing ','
+
+	$('<style type="text/css" class="wikia-ad-hidden">' + styleContent + ' { margin-left:-9999px; } </style>').appendTo('head');
 }
 
 /**
@@ -26,7 +24,8 @@ jQuery.hideAds = function() {
  *
  * @author macbre
  * @author Hyun Lim
+ * @author wlee
  */
 jQuery.showAds = function() {
-	jQuery.getAds().css('margin-left', 'auto');
+	$('head .wikia-ad-hidden').remove();
 }
