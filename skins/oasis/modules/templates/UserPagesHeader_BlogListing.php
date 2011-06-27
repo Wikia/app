33fbecd4f8065e1981a3652a28bb9c2a70d69e2b
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-06-27 15:44:10 +0000

    blog listing pages will now have proper titles and subtitles BugzID:7544
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38555 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/UserPagesHeader_BlogListing.php
+++ skins/oasis/modules/templates/UserPagesHeader_BlogListing.php
@@ -1,14 +1,16 @@
 <div id="WikiaUserPagesHeader" class="WikiaUserPagesHeader WikiaBlogListingHeader">
-	<h1><?= htmlspecialchars($title) ?></h1>
-
 <?php
 	// render edit button / dropdown menu
 	if (!empty($actionButton)) {
-		echo wfRenderModule('MenuButton', 'Index', array(
-			'action' => $actionButton,
-			'image' => MenuButtonModule::BLOG_ICON,
-			'name' => 'createblogpost',
-		));
+	echo wfRenderModule('MenuButton', 'Index', array(
+				'action' => $actionButton,
+				'image' => MenuButtonModule::BLOG_ICON,
+				'name' => 'createblogpost',
+				));
 	}
 ?>
+
+	<h1><?= htmlspecialchars($title) ?></h1>
+	<h2><?= htmlspecialchars($subtitle) ?></h2>
+
 </div>
