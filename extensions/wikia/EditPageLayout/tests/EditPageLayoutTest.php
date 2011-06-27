14f5a990b2c2d946f5992a836e19d44c517f06bd
macbre <macbre@wikia-inc.com>
2011-06-27 16:16:16 +0000

    Unit tests for EditPageLayout/Special CustomEditPage (php) - BugId:3910
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38557 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditPageLayout/tests/EditPageLayoutTest.php
+++ extensions/wikia/EditPageLayout/tests/EditPageLayoutTest.php
@@ -4,20 +4,34 @@ wfLoadAllExtensions();
 
 class EditPageLayoutTest extends PHPUnit_Framework_TestCase {
 
-	public function testMainPageEdit() {
-		$title = WF::build('Title', array(), 'newMainPage');
+	private function editPageFactory(Title $title) {
 		$article = WF::build('Article', array($title));
 
+		return WF::build('EditPageLayout', array($article));
+	}
+
+	public function testMainPageEdit() {
 		// setup edit page object
-		$editPage = WF::build('EditPageLayout', array($article));
+		$title = WF::build('Title', array(), 'newMainPage');
+		$editPage = $this->editPageFactory($title);
 
 		// it should extend MW core class
 		$this->assertInstanceOf('EditPage', $editPage);
 
-		$this->assertEquals($article, $editPage->getArticle());
+		$this->assertEquals($title->getArticleID(), $editPage->getArticle()->getID());
 		$this->assertEquals($title, $editPage->getEditedTitle());
 
-		$formAction = str_replace('=edit', '=submit', $title->getEditURL());
+		$formAction = str_replace('action=edit', 'action=submit', $title->getEditURL());
 		$this->assertEquals($formAction, $editPage->getFormAction());
+	}
+
+	public function testCustomFormHandler() {
+		$title = WF::build('Title', array('Foo'), 'newFromText');
+		$editPage = $this->editPageFactory($title);
+
+		$customHandler = WF::build('Title', array('Special:CustomHandler'), 'newFromText');
+
+		$editPage->setCustomFormHandler($customHandler);
+		$this->assertEquals($customHandler, $editPage->getCustomFormHandler());
 	}
 }
