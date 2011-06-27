3dd5d415b5296555a5b9b6a763ee32b8b129477e
garth <garth@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-06-27 21:34:10 +0000

    Simplify the onedot code
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38591 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaStats/WikiaWebStats.php
+++ extensions/wikia/WikiaStats/WikiaWebStats.php
@@ -6,26 +6,17 @@ if(!defined('MEDIAWIKI')) {
 $extensions_dir = dirname(dirname(__FILE__));
 
 $wgHooks['SkinAfterBottomScripts'][] = 'wfWikiaWebStatsScript';
-$wgHooks['MakeGlobalVariablesScript'][] = 'wfAddOneDotGlobals';
 
 function wfWikiaWebStatsScript($this, $bottomScriptText) {
-	global $wgEnableOneDotPlus;
+	global $wgCityId, $wgDotDisplay, $wgReadOnly;
 
-	if ($wgEnableOneDotPlus) {
-		ajaxOneDot($bottomScriptText);
-	} else {
-		imageOneDot($bottomScriptText);
+	if ( !empty($wgCityId) && !empty($wgDotDisplay) && empty($wgReadOnly) ) {
+		$url = getOneDotURL();
+
+		$bottomScriptText .= "<script type=\"text/javascript\" src=\"$url\"></script>";
+		$bottomScriptText .= '<noscript><img src="'.$url.'" width="1" height="1" border="0" alt="" /></noscript>';
+		$bottomScriptText .= "\n";
 	}
-	return true;
-}
-
-function wfAddOneDotGlobals ($vars) {
-	global $wgEnableOneDotPlus;
-
-	if ($wgEnableOneDotPlus) {
-		$vars['wgOneDotURL']    = getOneDotURL();
-	}
-
 	return true;
 }
 
@@ -49,27 +40,4 @@ function getOneDotURL () {
 	}
 
 	return $wgOneDotURL;
-}
-
-function ajaxOneDot (&$bottomScriptText) {
-	global $wgCityId, $wgDotDisplay, $wgReadOnly, $wgJsMimeType, $wgStyleVersion, $wgExtensionsPath, $wgOut;
-
-	if ( !empty($wgCityId) && !empty($wgDotDisplay) && empty($wgReadOnly) ) {
-		$url = getOneDotURL();
-
-		$bottomScriptText .= "<script type=\"text/javascript\" src=\"$url\"></script>";
-		$bottomScriptText .= '<noscript><img src="'.$url.'" width="1" height="1" border="0" alt="" /></noscript>';
-		$bottomScriptText .= "\n";
-	}
-}
-
-function imageOneDot (&$bottomScriptText) {
-	global $wgUser, $wgArticle, $wgTitle, $wgCityId, $wgDBname, $wgDBcluster, $wgDotDisplay, $wgAdServerTest, $wgReadOnly, $wgContLanguageCode;
-
-	if ( !empty($wgCityId) && !empty($wgDotDisplay) && empty($wgReadOnly) ) {
-		$url = 'http://a.wikia-beacon.com/__onedot?c='.$wgCityId.'&amp;lc='.$wgContLanguageCode.'&amp;lid='.WikiFactory::LangCodeToId($wgContLanguageCode).'&amp;x='.$wgDBname.'&amp;y='.$wgDBcluster.'&amp;u='.$wgUser->getID().'&amp;a='.(is_object($wgArticle) ? $wgArticle->getID() : null).'&amp;n='.$wgTitle->getNamespace().(!empty($wgAdServerTest) ? '&amp;db_test=1' : '');
-		$bottomScriptText .= '<script type="text/javascript">/*<![CDATA[*/document.write("<img src=\"'.$url.'"+((typeof document.referrer != "undefined") ? "&amp;r="+escape(document.referrer) : "")+"&amp;cb="+(new Date).valueOf()+"\" width=\"1\" height=\"1\" border=\"0\" alt=\"\" />");/*]]>*/</script><noscript><img src="'.$url.'" width="1" height="1" border="0" alt="" /></noscript>';
-		$bottomScriptText .= "\n";
-	}
-	return true;
 }
