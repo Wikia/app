9dfcfddea31b223ff9e1219d610d89f3398dfa59
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-06-28 10:10:10 +0000

    Unit tests for EditPageLayout/Special CustomEditPage (php) - BugId:3910
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38623 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditPageLayout/tests/EditPageLayoutTest.php
+++ extensions/wikia/EditPageLayout/tests/EditPageLayoutTest.php
@@ -34,5 +34,90 @@ class EditPageLayoutTest extends PHPUnit_Framework_TestCase {
 		$editPage->setCustomFormHandler($customHandler);
 		$this->assertEquals($customHandler, $editPage->getCustomFormHandler());
 		$this->assertEquals($customHandler->getLocalURL('action=submit'), $editPage->getFormAction());
+		$this->assertEquals($title, $editPage->getEditedTitle());
+	}
+
+	public function testAddingFields() {
+		$title = WF::build('Title', array('Foo'), 'newFromText');
+		$editPage = $this->editPageFactory($title);
+
+		// test custom checkboxes
+		$editPage->addCustomCheckbox('foo', 'label', true /* $checked */);
+		$editPage->addCustomCheckbox('bar', 'test', false /* $checked */);
+
+		$this->assertEquals(array(
+			array(
+				'name' => 'foo',
+				'label' => 'label',
+				'checked' => true,
+			),
+			array(
+				'name' => 'bar',
+				'label' => 'test',
+				'checked' => false,
+			),
+		), $editPage->getCustomCheckboxes());
+
+		// test hidden fields
+		$editPage->addHiddenField(array(
+			'type' => 'checkbox',
+			'name' => 'foo',
+			'value' => true,
+			'label' => 'label',
+		));
+		$editPage->addHiddenField(array(
+			'type' => 'text',
+			'name' => 'bar',
+			'value' => 'summary',
+			'required' => false,
+		));
+		$editPage->addHiddenField(array(
+			'type' => 'textarea',
+			'name' => 'field123',
+			'value' => 'qwerty',
+			'required' => true,
+		));
+		$editPage->addHiddenField(array(
+			'type' => 'hidden',
+			'name' => 'test',
+			'value' => 'hidden value',
+		));
+
+		$this->assertEquals(
+			'<fieldset id="EditPageHiddenFields">'.
+				'<label>label<input type="checkbox" name="foo" value="1" data-required="" checked="checked" /></label>'.
+				'<input type="text" name="bar" value="summary" data-required="" />'.
+				'<textarea name="field123" data-required="1">qwerty</textarea>'.
+				'<input type="hidden" name="test" value="hidden value" data-required="" />'.
+			'</fieldset>',
+			$editPage->renderHiddenFields());
+
+		// summary box
+		$this->assertStringStartsWith('<textarea id="wpSummary" name="wpSummary" placeholder=', $editPage->renderSummaryBox());
+	}
+
+	public function testPreloadText() {
+		$title = WF::build('Title', array('NewArticle'), 'newFromText');
+		$editPage = $this->editPageFactory($title);
+
+		$preload = 'Preload - testing, testing...';
+		$editPage->setPreloadedText($preload);
+		$editPage->showEditForm();
+
+		$this->assertEquals($preload, $editPage->textbox1);
+	}
+
+	public function testEditNotices() {
+		$title = WF::build('Title', array('NewArticle'), 'newFromText');
+		$editPage = $this->editPageFactory($title);
+
+		$editPage->addEditNotice('<div>foo bar</div>');
+		$editPage->addEditNotice('<div>second notice</div>');
+
+		$this->assertEquals(array(
+			'foo bar',
+			'second notice'
+		), $editPage->getNotices());
+		$this->assertEquals('<div>foo bar</div><div>second notice</div>', $editPage->getNoticesHtml());
 	}
 }
