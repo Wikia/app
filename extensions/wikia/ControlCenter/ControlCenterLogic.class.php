83e298d6d146922484e09944e97a5d1546c43e2e
hyun <hyun@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-02 01:03:26 +0000

    ControlCenter: adding authentication back in
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38822 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ControlCenter/ControlCenterLogic.class.php
+++ extensions/wikia/ControlCenter/ControlCenterLogic.class.php
@@ -15,10 +15,10 @@ class ControlCenterLogic {
 		
 		// Control center is only for logged in plus a list of groups
 		// FIXME: make this a right and add it to those groups instead
-		//if (!$app->wg->User->isLoggedIn()) return false;
-		//if (count(array_intersect(array('sysop', 'staff', 'bureaucrat'), $app->wg->User->getGroups())) == 0) return false;
+		if (!$app->wg->User->isLoggedIn()) return false;
+		if (count(array_intersect(array('sysop', 'staff', 'bureaucrat'), $app->wg->User->getGroups())) == 0) return false;
 		
-		$pageList = array ( "ControlCenter", "UserRights", "ListUsers", "RecentChanges", "WikiaLabs", "Categories", "MultipleUpload", "SponsorshipDashboard");
+		$pageList = array ( "ControlCenter", "UserRights", "ListUsers", "RecentChanges", "Categories", "MultipleUpload", "SponsorshipDashboard");
 //		print_pre($title->getDBKey());
 		if ($title && $title->isSpecialPage() && in_array($title->getDBKey(), $pageList)) {
 			return true;
