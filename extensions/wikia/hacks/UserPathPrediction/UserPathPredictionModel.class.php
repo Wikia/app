54f770411a8a1e28031d5c5767ad4c21bfcc5837
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-05 18:16:23 +0000

    UserPathPrediction: code improvements
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@38894 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/hacks/UserPathPrediction/UserPathPredictionModel.class.php
+++ extensions/wikia/hacks/UserPathPrediction/UserPathPredictionModel.class.php
@@ -3,15 +3,12 @@ class UserPathPredictionModel {
 
 	const S3_ARCHIVE = "s3://one_dot_archive/";
 	const SAVE_PATH = "/tmp/UserPathPredictionRawData/";
-	const SPLIT_DIR = "/tmp/UserPathPredictionParsedData/";
+	const PARSED_DATA_PATH = "/tmp/UserPathPredictionParsedData/";
 	
 	private $files;
+	private $excludePaths = array( ".", ".." );
 	
-	function __construct() {
-		
-	}
-	
-
+	function __construct() {}
 
 	private function createDir( $folder ) {
 		if( !is_dir( $folder )) {
@@ -20,39 +17,47 @@ class UserPathPredictionModel {
 	}
 	
 	private function removeDir($dir) {
-		if (is_dir($dir)) {
-			$objects = scandir($dir);
-			foreach ($objects as $object) {
-				if ($object != "." && $object != "..") {
-					if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+		if ( is_dir( $dir ) ) {
+			$objects = scandir( $dir );
+			
+			foreach ( $objects as $object ) {
+				if ( !in_array( $object, $this->$excludePaths ) ) {
+					$path = "{$dir}/{$object}";
+					
+					if ( filetype( $path ) == 'dir' ) {
+						$this->removeDir( $path );
+					} else {
+						unlink( $path );
+					}
 				}
 			}
-		reset($objects);
-		rmdir($dir);
+			
+			reset( $objects );
+			rmdir( $dir );
 		}
 	} 
 	
 	function gets3Data( $timestr ) {
-		//$this->removeDir(self::SAVE_PATH);
-		//$s3directory = self::S3_ARCHIVE . "{$timestr}/";
-		//$this->createDir( self::SAVE_PATH );
-		//echo shell_exec( "s3cmd get --recursive {$s3directory} " . self::SAVE_PATH );
+		$this->removeDir(self::SAVE_PATH);
+		$s3directory = self::S3_ARCHIVE . "{$timestr}/";
+		$this->createDir( self::SAVE_PATH );
+		echo shell_exec( "s3cmd get --recursive {$s3directory} " . self::SAVE_PATH );
 		
 		$tmpFiles = scandir( self::SAVE_PATH );
 		
-		if(count( $tmpFiles )  > 2 ) {
-			$this->files = array_slice( $tmpFiles, 2 );	
+		if ( count( $tmpFiles )  > 2 ) {
+			$this->files = array_slice( $tmpFiles, 2 );
 			return true;
 		}
 		
 		return false;
 	}
 	
-	function getContent() {
-		$read = next($this->files);
+	function getDataFilePath() {
+		$read = next( $this->files );
 
 		if($read === false) {
-			reset($this->files);
+			reset( $this->files );
 			return false;
 		} 
 		
@@ -60,36 +65,36 @@ class UserPathPredictionModel {
 	}
 	
 	function saveParsedData( $domain, $data ) {
-		if( !is_dir( self::SPLIT_DIR )) {
-				$this->createDir( self::SPLIT_DIR );
+		if( !is_dir( self::PARSED_DATA_PATH )) {
+				$this->createDir( self::PARSED_DATA_PATH );
 		}
 		
-		file_put_contents( self::SPLIT_DIR . $domain, serialize( $data ) . "\n", FILE_APPEND );
+		file_put_contents( self::PARSED_DATA_PATH . $domain, serialize( $data ) . "\n", FILE_APPEND );
 	}
 	
 	function getWikis() {
+		//TODO: get data from WF
 		return array(
-					"490" => array(
-						"db_name" => "wowwiki",
-						"domain_name" => "wowwiki.com"
-					),
-					"10150" => array(
-						"db_name" => "dragonage",
-						"domain_name" => "dragonage.wikia.com"
-					),
-					"3125" => array(
-						"db_name" => "callofduty",
-						"domain_name" => "callofduty.wikia.com"
-					)
+			"490" => array(
+				"db_name" => "wowwiki",
+				"domain_name" => "wowwiki.com"
+			),
+			"10150" => array(
+				"db_name" => "dragonage",
+				"domain_name" => "dragonage.wikia.com"
+			),
+			"3125" => array(
+				"db_name" => "callofduty",
+				"domain_name" => "callofduty.wikia.com"
+			)
 		);
-					
 	}
 	
-	function resetParsedData() {
-		$this->removeDir(self::SPLIT_DIR);
+	function cleanParsedDataFolder() {
+		$this->removeDir( self::PARSED_DATA_PATH );
 	}
 	
 	function cleanSourceFolder() {
-		//$this->removeDir( self::SAVE_PATH );
+		$this->removeDir( self::SAVE_PATH );
 	}
 }
--- extensions/wikia/hacks/UserPathPrediction/UserPathPredictionService.class.php
+++ extensions/wikia/hacks/UserPathPrediction/UserPathPredictionService.class.php
@@ -6,15 +6,14 @@
  */
 
 class UserPathPredictionService extends WikiaService {
-
-	//VARS
-	private $wikis, $yesterday, $model;
+	private $wikis;
+	private $model;
 
 	public function init() {
-		$this->logPath = "/tmp/" . __CLASS__;
-		//$yesterday = date("Ymd", strtotime("-1 day"));
-		$this->yesterday = "20110504";	
 		$this->model = new UserPathPredictionModel();
+		$this->logPath = "/tmp/" . __CLASS__;
+		
+		unlink( $this->logPath );
 	}
 
 	private function log( $msg ) {
@@ -26,31 +25,35 @@ class UserPathPredictionService extends WikiaService {
 	//change colons to %3A in URL
 	private function fixURL( $url ) {
 		return str_replace( ":", "%3A", $url );
-	} //end of colons to %3A
+	}
 
 	//Parse - extracts needed data from downloaded onedot files
 	function processOneDotData() {
-						
-					//$this->model->cleanSourceFolder();
-					//return true;
-		//TODO: move to model
-		$this->log( "Starting parsing part\n" );
+		$this->model->cleanSourceFolder();
 		
-		$this->wikis = $this->model->getWikis();
-	
+		if ( empty ( $this->wikis ) ) {
+			$this->wikis = $this->model->getWikis();
+		}
 		
-		if( $this->model->gets3Data( $this->yesterday ) ) {	
-
-			$this->model->resetParsedData();
+		$strDate = date( "Ymd", strtotime( "-1 day" ) );//"20110504";
+		
+		$this->log( "Fetching OneDot data for {$strDate}...\n" );
+		
+		if( $this->model->gets3Data( $strDate ) ) {
+			$this->log( "Done.\n" );
 			
-			while( ( $src = $this->model->getContent() ) !== false ) {
-				
+			$this->model->cleanParsedDataFolder();
+			
+			while( ( $src = $this->model->getDataFilePath() ) !== false ) {
 				$fileHandle = fopen( $src , "r" );
+				$skip = false;
+				$result = array();
+				
+				$this->log( "Processing: {$src}...\n" );
 				
 				while( !feof( $fileHandle ) ) {
-					$skip = false;
 					$wholeLine = explode( "&", fgets( $fileHandle ));
-					$result = array();
+					
 					foreach( $wholeLine as $param ) {
 						$parameters = explode( "=", $param );
 						$value = $parameters[1];
@@ -66,32 +69,32 @@ class UserPathPredictionService extends WikiaService {
 								$result["lv"] = $value;
 								break;
 							case "event":
-									$skip = true;									
+								//we take into consideration only pure pageviews, no events tracking requests
+								//this avoids duplicated data popping up and screw the stats
+								$skip = true;
 								break;
 						}
 						
-						if($skip == true) {
+						if ( $skip == true ) {
 							break;
 						}
 					}
 					
-					if($skip == true) {
-							continue;
+					if ($skip == true ) {
+						continue;
 					}
 					
-					if( !empty( $result["r"] ) ) {
-						foreach( $this->model->getWikis() as $wiki ) {
+					if ( !empty( $result["r"] ) ) {
+						foreach ( $this->model->getWikis() as $wiki ) {
 							$hasWikiPrefix = strpos( $wiki["domain_name"], '.wikia.com' );
-							$mainURL = ( $hasWikiPrefix ? 
-								$this->fixURL('http://' . $wiki["domain_name"] . '/') : 
-								$this->fixURL('http://www.' . $wiki["domain_name"] . '/') );
-								
-							if( strpos( $result["r"], $mainURL ) === 0 ) {
-								if( $hasWikiPrefix) {
-									$articleName = str_ireplace( $mainURL . 'wiki/', '', $result["r"] );
-								} else {
+							$mainURL = ( $hasWikiPrefix ) ? 
+								$this->fixURL( "http://{$wiki['domain_name']}/" ) : 
+								$this->fixURL( "http://www{$wiki['domain_name']}/" );
+							
+							if ( strpos( $result["r"], $mainURL ) === 0 ) {
+								$articleName = ( $hasWikiPrefix ) ?
+									str_ireplace( $mainURL . 'wiki/', '', $result["r"] ) :
 									$articleName = str_ireplace( $mainURL, '', $result["r"] );
-								}
 								
 								if( !empty( $articleName ) ) {
 									$result["r"] = $articleName;
@@ -101,9 +104,13 @@ class UserPathPredictionService extends WikiaService {
 						}
 					}
 				}
+				
 				fclose( $fileHandle );
-				$this->log( "File " . $src . " has been processed\n" );
+				
+				$this->log( "Done.\n" );
 			}
+			
+			$this->log( "Cleaning up...\n" );
 			$this->model->cleanSourceFolder();
 		}
 	}
