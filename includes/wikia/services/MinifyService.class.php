f45dd5ece7ee0deb70aac16533129317614f7ce4
marcin <marcin@wikia-inc.com>
2011-07-07 10:15:59 +0000

    Editor refactoring: Use AssetsManager package for loading CKeditors wikia skin (bugid:7834)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39009 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ includes/wikia/services/MinifyService.class.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * This service simplifies JS and CSS minification
+ *
+ * Currently this class is only used by RTE's minify.sh script
+ *
+ * @author macbre
+ */
+
+class MinifyService extends Service {
+
+	const RESULT_OK = 0;
+
+	/**
+	 * Store given content in temporary file and return its name
+	 */
+	private static function storeInTempFile($content) {
+		$tmpName = tempnam(sys_get_temp_dir(), 'minify');
+		file_put_contents($tmpName, $content);
+
+		return $tmpName;
+	}
+
+	/**
+	 * Removes given temporary file
+	 */
+	private static function removeTempFile($tmpName) {
+		unlink($tmpName);
+	}
+
+	/**
+	 * Merges content of provided list of files into single string
+	 */
+	public static function mergeFiles($files) {
+		wfProfileIn(__METHOD__);
+		$content = '';
+
+		foreach($files as $file) {
+			if (file_exists($file)) {
+				$content .= file_get_contents($file) . "\n";
+			}
+			else {
+				wfDebug(__METHOD__ . ": file {$file} does not exist!\n");
+			}
+		}
+
+		// remove BOM UTF-8 marker
+		$content = str_replace("\xEF\xBB\xBF", '', $content);
+
+		wfProfileOut(__METHOD__);
+		return $content;
+	}
+
+	/**
+	 * Return minified version of given JavaScript code (or false in case of an error)
+	 */
+	public static function minifyJS($code) {
+		global $IP;
+		wfProfileIn(__METHOD__);
+
+		wfDebug(__METHOD__ . ": starting...\n");
+
+		// store code in temporary file
+		$tmpName = self::storeInTempFile($code);
+
+		// YUI compressor
+		//exec("java -jar {$IP}/lib/yuicompressor-2.4.2.jar {$tmpName} --charset utf-8 --type js", $out, $res);
+
+		// Google Closure
+		#exec("java -jar {$IP}/lib/compiler.jar --js {$tmpName} --compilation_level SIMPLE_OPTIMIZATIONS --charset UTF-8 --warning_level QUIET", $out, $res);
+		exec("java -jar {$IP}/lib/compiler.jar --js {$tmpName} --compilation_level SIMPLE_OPTIMIZATIONS --charset UTF-8", $out, $res);
+
+		// concat lines from the output
+		$code = implode("\n", $out);
+
+		// clean up
+		if ($res != self::RESULT_OK) {
+			self::removeTempFile($tmpName);
+		}
+
+		wfDebug(__METHOD__ . ": done!\n");
+
+		wfProfileOut(__METHOD__);
+		return ($res == self::RESULT_OK) ? $code : false;
+	}
+
+	/**
+	 * Return minified version of given CSS (or false in case of an error)
+	 */
+	public static function minifyCSS($code) {
+		global $IP;
+		wfProfileIn(__METHOD__);
+
+		wfDebug(__METHOD__ . ": starting...\n");
+
+		// store code in temporary file
+		$tmpName = self::storeInTempFile($code);
+
+		// minify (output is written to stdout)
+		exec("java -jar {$IP}/lib/yuicompressor-2.4.2.jar {$tmpName} --charset utf-8 --type css", $out, $res);
+
+		// concat lines from the output
+		$code = implode("\n", $out);
+
+		// clean up
+		self::removeTempFile($tmpName);
+
+		wfDebug(__METHOD__ . ": done!\n");
+
+		wfProfileOut(__METHOD__);
+		return ($res == self::RESULT_OK) ? $code : false;
+	}
+}
