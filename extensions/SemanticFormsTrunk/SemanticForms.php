115d7f59e7ff7da2731cd28e2e23e1dd8e7e9ebe
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-07 11:57:49 +0000

    sync with upstream
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39016 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/SemanticFormsTrunk/SemanticForms.php
+++ extensions/SemanticFormsTrunk/SemanticForms.php
@@ -36,7 +36,7 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'SF_VERSION', '2.2-alpha' );
+define( 'SF_VERSION', '2.2' );
 
 $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'specialpage'][] = array(
 	'path' => __FILE__,
