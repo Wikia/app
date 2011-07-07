7ac79110a5475def7bccc5b7508b1521675de0ef
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-07 18:41:26 +0000

    trunk: rename ControlCenter to AdminDashboard pt 2
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39039 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ControlCenter/templates/ControlCenterSpecialPage_chromedArticleHeader.php
+++
@@ -1,9 +0,0 @@
-<div id="ControlCenterDrawer" class="ControlCenterDrawer">
-	<span class="arrow"></span>
-</div>
-<nav class="ControlCenterNavigation">
-	<a href="<?= $backLink ?>"><?= wfMsg('controlcenter-back-to-dashboard') ?></a>
-</nav>
-<div class="ControlCenterGeneralHeader ControlCenterArticleHeader">
-	<h1><?= $headerText ?></h1>
-</div>
