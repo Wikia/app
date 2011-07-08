5c818049b577150d9a99d61a3ab6d2d2eb5a859b
will <will@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-08 17:07:33 +0000

    Bug ID 8131
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39095 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaQuiz/templates/WikiaQuiz_ArticleIndex.php
+++ extensions/wikia/WikiaQuiz/templates/WikiaQuiz_ArticleIndex.php
@@ -1,6 +1,6 @@
 <? if (!empty($quizElement)) { ?>
 <section class="WikiaQuizArticle" data-id="<?= $quizElement->getId() ?>">
-<p>(in the quiz <a href="<?=$data['quizUrl']?>"><?= htmlentities($data['quiz']) ?></a>)</p>
+<p>(in the quiz <a href="<?=$data['quizUrl']?>"><?= $data['quiz'] ?></a>)</p>
 <? if ($data['image']) { ?>
 <img src="<?= $data['image'] ?>" />
 <? } ?>
@@ -12,14 +12,14 @@
 		<? if ($answer['image']) { ?>
 		<img src="<?= $answer['image'] ?>" />
 		<? } ?>
-		<p><?=htmlentities($answer['text']) ?></p>
+		<p><?=$answer['text'] ?></p>
 	</li>
 <?	} ?>
 </ul>
 <? } ?>
 <? if ($data['explanation']) { ?>
 <h3>Explanation</h3>
-<p><?= htmlentities($data['explanation']) ?></p>
+<p><?= $data['explanation'] ?></p>
 <? } ?>
 </section>
 <? } ?>
