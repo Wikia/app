78bb691ebf1323e17fd0f76d6e7398aadfaaae32
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-12 00:32:14 +0000

    BugzId 8373: add SpecialCategoryIntersection which will be a usable demo of the categoryintersection API functionality (and a partial tutorial in itself).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39173 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialCategoryIntersection/SpecialCategoryIntersection.i18n.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Internationalisation file for Special:CategoryIntersection extension.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'categoryintersection' => 'Category Intersection Demo',
+	'categoryintersection-desc' => 'A front-end for the "categoryintersection" API function to show what it can use and assist in understanding it for actual use via the MediaWiki API.',
+	'categoryintersection-header' => '',
+	'categoryintersection-form-title' => 'Try It Out',
+	'categoryintersection-form-submit' => 'Find Matches',
+	'categoryintersection-and' => ' - and - ',
+	'categoryintersection-limit' => 'Limit',
+);
+
+/** Message documentation (Message documentation)
+ *
+ */
+$messages['qqq'] = array(
+	'categoryintersection-desc' => '{{desc}}',
+	'categoryintersection-and' => 'Displayed between consecutive boxes for categories.  Separates the categories visually but shows that adding more of them is a logical-and operation (ie: articles will have to be in ALL of the categories to be in the result set.'
+);
