8e88afd81e096972edb088e4bd2aa2502508ac2f
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-13 09:19:31 +0000

    move event from special to stats
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39244 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/sql/events.sql
+++ maintenance/wikia/sql/events.sql
@@ -4,8 +4,8 @@ COMMENT 'City list table with categories and number of Wikis per day'
 DO
 BEGIN
 START TRANSACTION;
-DELETE FROM specials.city_list_count;
-INSERT INTO specials.city_list_count SELECT ifnull(date(city_created), '0000-00-00') as city_created, count(city_id) as count_created FROM wikicities.city_list GROUP BY 1;
+DELETE FROM stats.city_list_count;
+INSERT INTO stats.city_list_count SELECT ifnull(date(city_created), '0000-00-00') as city_created, count(city_id) as count_created FROM wikicities.city_list GROUP BY 1;
 COMMIT;
 
 START TRANSACTION;
@@ -26,7 +26,7 @@ ifnull(c1.count_created, 0),
 cc.cat_name
 FROM wikicities.city_list c2
 LEFT JOIN stats.city_list_cats c3 on c3.city_id = c2.city_id
-LEFT JOIN specials.city_list_count c1 on c1.city_created = date(c2.city_created)
+LEFT JOIN stats.city_list_count c1 on c1.city_created = date(c2.city_created)
 LEFT JOIN wikicities.city_cat_mapping ccm ON (c2.city_id = ccm.city_id)
 LEFT JOIN wikicities.city_cats cc ON ccm.cat_id =cc.cat_id
 where c2.city_id > 0 and c3.city_dbname is null
