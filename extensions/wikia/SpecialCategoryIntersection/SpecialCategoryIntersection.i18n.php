6c33ad68d2f7eec6c5a7c362fbedc2029c3ce6df
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-13 00:44:47 +0000

    Basic functionality and some examples working on SpecialCategoryIntersection.  Still needs some polish.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39236 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialCategoryIntersection/SpecialCategoryIntersection.i18n.php
+++ extensions/wikia/SpecialCategoryIntersection/SpecialCategoryIntersection.i18n.php
@@ -8,13 +8,19 @@
 $messages = array();
 
 $messages['en'] = array(
-	'categoryintersection' => 'Category Intersection Demo',
+	'categoryintersection' => 'Category Intersection API Demo',
 	'categoryintersection-desc' => 'A front-end for the "categoryintersection" API function to show what it can use and assist in understanding it for actual use via the MediaWiki API.',
-	'categoryintersection-header' => '',
+	'categoryintersection-header-title' => 'About',
+	'categoryintersection-header-body' => '',
 	'categoryintersection-form-title' => 'Try It Out',
 	'categoryintersection-form-submit' => 'Find Matches',
 	'categoryintersection-and' => ' - and - ',
 	'categoryintersection-limit' => 'Limit',
+	'categoryintersection-results-title' => 'Results',
+	'categoryintersection-noresults' => 'No results. Please check that the category names are correct if you think there should be results.',
+	'categoryintersection-query-used' => 'API Query Used:',
+	'categoryintersection-footer-title' => 'Examples',
+	'categoryintersection-footer-body' => 'Here are some example queries, just to show a few of the types of things that can be done with this API. There are numerous other cool possibilies though... be creative!',
 );
 
 /** Message documentation (Message documentation)
