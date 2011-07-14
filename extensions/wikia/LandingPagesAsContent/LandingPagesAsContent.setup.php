61b6cfaaf8c5328b8339cc55ae0f4304485d1e2a
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-14 18:00:16 +0000

    LandingPages: BugzId:7973,7874,7875,7876
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39347 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/LandingPagesAsContent/LandingPagesAsContent.setup.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Landing Pages as content extension
+ *
+ * @author Federico "Lox" Lucignano <federico(at)wikia-inc.com>
+ * @author Łukasz 'TOR' Garczewski <tor(at)wikia-inc.com>
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "This file is part of MediaWiki, it is not a valid entry point.\n";
+	exit( 1 );
+}
+
+$app = F::app();
+$dir = dirname( __FILE__ );
+
+/**
+ * info
+ */
+$app->wg->append(
+	'wgExtensionCredits',
+	array(
+		"name" => "Landing Pages as content",
+		"description" => "Landing Pages",
+		"descriptionmsg" => "landingpagesascontent-desc",
+		"url" => "http://help.wikia.com/wiki/Help:LendingPagesAsContent",
+		"author" => array(
+			'Federico "Lox" Lucignano <federico(at)wikia-inc.com>',
+			'Łukasz \'TOR\' Garczewski <tor(at)wikia-inc.com>'
+		)
+	),
+	'other'
+);
+
+/**
+ * services
+ */
+$app->wg->set( 'wgAutoloadClasses', "{$dir}/LandingPagesParser.class.php", 'LandingPagesParser' );
+
+/**
+ * message files
+ */
+$app->wg->set( 'wgExtensionMessagesFiles', "{$dir}/LandingPagesAsContent.i18n.php", 'LandingPagesAsContent' );
+
+/**
+ * wikitext magic words
+ */
+define('__NORAIL__', '__NORAIL__');
+define('__NONAV__', '__NONAV__');
+define('__NOHEADER__', '__NOHEADER__');
+
+/**
+ * hooks
+ */
+$app->registerHook( 'LanguageGetMagic', 'LandingPagesParser', 'onLanguageGetMagicHook' );
+$app->registerHook( 'InternalParseBeforeLinks', 'LandingPagesParser', 'onInternalParseBeforeLinksHook' );
+$app->registerHook( 'ArticleFromTitle', 'LandingPagesParser', 'onArticleFromTitle' );
