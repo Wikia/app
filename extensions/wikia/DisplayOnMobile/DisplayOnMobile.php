1d9bd3e34ae933c62706c19e8de072caff333fe7
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-14 13:41:27 +0000

    new extension to finetune display on mobile skins
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39328 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/DisplayOnMobile/DisplayOnMobile.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * DisplayOnMobile
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
+ * @date 2011-07-14
+ * @copyright Copyright © 2011 Łukasz Garczewski, Wikia Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 3.0 or later
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+        echo "This is a MediaWiki extension named DisplayOnMobile.\n";
+        exit( 1 );
+}
+
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['parserhook'][] = array(
+        'name' => 'DisplayOnMobile',
+        'version' => '1.0',
+        'author' => "[http://www.wikia.com/wiki/User:TOR Łukasz 'TOR' Garczewski]",
+);
+
+$wgHooks['ParserFirstCallInit'][] = 'wfSigSetup';
+
+function efDisplayOnMobileInit(&$parser) {
+        $parser->setHook( 'mobile', 'efOnMobileDisplay' );
+        $parser->setHook( 'nomobile', 'efOnMobileHide' );
+        return true;
+}
+
+function efOnMobileDisplay( $contents, $attributes, $parser ) {
+	$wgUser;
+
+	$skin = $wgUser->getSkin();
+
+	if ( $skin->getSkinName() == 'wikiaphone' ) {
+		return $contents;
+	} else {
+		return '';
+	}
+}
+
+function efOnMobileHide( $contents, $attributes, $parser ) {
+	global $wgUser;
+
+	$skin = $wgUser->getSkin();
+
+	if ( $skin->getSkinName() == 'wikiaphone' ) {
+		return '';
+	} else {
+		return $contents;
+	}
+}
