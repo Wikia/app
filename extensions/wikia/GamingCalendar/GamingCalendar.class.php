3c53638f022f77c7da5fd0379273602a23473341
michal <michal@wikia-inc.com>
2011-07-14 13:57:20 +0000

    BugId:7872 - SSW Gaming Calendar, the basics of the extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39330 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/GamingCalendar/GamingCalendar.class.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @brief A notification module for gaming wikis that allows users to quickly review a snapshot of upcoming game releases.
+ * @author Michał Roszka <michal@wikia-inc.com>
+ */
+class GamingCalendar {
+
+}
---
+++ extensions/wikia/GamingCalendar/GamingCalendar.setup.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Gaming Calendar
+ * 
+ * A notification module for gaming wikis that allows users to quickly review a snapshot of upcoming game releases.
+ * 
+ * @author Michał Roszka <michal@wikia-inc.com>
+ */
+$wgExtensionCredits['other'][] = array(
+    'name'        => 'Gaming Calendar',
+    'description' => 'A notification module for gaming wikis that allows users to quickly review a snapshot of upcoming game releases.',
+    'version'     => '1.0',
+    'author'      => array( 'Michał Roszka' )
+);
+
+$dir = dirname( __FILE__ );
+
+$wgAutoloadClasses['GamingCalendar'] = "$dir/GamingCalendar.class.php";
+$wgExtensionMessagesFiles['GamingCalendar'] = "$dir/GamingCalendar.i18n.php";
