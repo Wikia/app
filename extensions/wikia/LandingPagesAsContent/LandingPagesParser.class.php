177bc3896e078e133fb55c9d04ae622ffdc3e13b
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-15 17:52:51 +0000

    LandingPages: Added cache layer + minor changes (BugzId:7873)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39386 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/LandingPagesAsContent/LandingPagesParser.class.php
+++ extensions/wikia/LandingPagesAsContent/LandingPagesParser.class.php
@@ -6,11 +6,7 @@
  */
 
 class LandingPagesParser {
-	private $config = array(
-		__NORAIL__ => 'wgSuppressRail',
-		__NONAV__ => 'wgSuppressWikiHeader',
-		__NOHEADER__ => 'wgSuppressPageHeader'
-	);
+	const CACHE_DURATION = 86400;//24h
 	
 	private $app;
 	private $switches;
@@ -18,7 +14,8 @@ class LandingPagesParser {
 	
 	function __construct(){
 		$this->app = F::app();
-		$this->magicWords = array_keys( $this->config );
+		$this->magicWords = array_keys( $this->app->wg->LandingPagesAsContentMagicWords );
+		
 		
 		//singleton
 		F::setInstance( __CLASS__, $this );
@@ -43,25 +40,57 @@ class LandingPagesParser {
 	}
 	
 	public function onArticleFromTitle( &$title, &$article ) {
-		$article = F::build( 'Article', array( $title ) );
-		$this->switches = array();
-		
-		foreach ( $this->magicWords as $wordID ) {
-			$magicWord = MagicWord::get( $wordID );
-			$this->switches[$wordID] = ( 0 < $magicWord->match( $article->getRawText() ) );
+		if( $title->exists() ){
+			$key = $this->generateCacheKey( $title->getArticleId() );
+			$this->switches = $this->app->wg->memc->get( $key );
+			
+			if ( empty( $this->switches ) ) {
+				$article = F::build( 'Article', array( $title ) );
+				$this->switches = array();
+				
+				foreach ( $this->magicWords as $wordID ) {
+					$magicWord = MagicWord::get( $wordID );
+					$this->switches[$wordID] = ( 0 < $magicWord->match( $article->getRawText() ) );
+				}
+				
+				$this->app->wg->memc->set( $key, $this->switches, self::CACHE_DURATION );
+			}
+			
+			$this->process();
 		}
-		
-		$this->process();
 		
 		return true;
 	}
 	
-	public function process(){
+	public function onArticlePurge( &$article ) {
+		$this->purgeCache( $article->getID() );
+		
+		return true;
+	}
+	
+	public function onArticleSaveComplete( &$article, &$user, $text, $summary,
+		$minoredit, $watchthis, $sectionanchor, &$flags, $revision, &$status,
+		$baseRevId, &$redirect ) {
+ 		$this->purgeCache( $article->getID() );
+		
+		return true;
+	}
+	
+	private function process(){
 		//TODO: skip in case action=edit?
 		foreach ( $this->switches as $wordID => $value ) {
-			$this->app->wg->set( $this->config[$wordID], $value );
+			$this->app->wg->set( $this->app->wg->LandingPagesAsContentMagicWords[$wordID], $value );
 		}
 		
 		$this->switches = null;
+	}
+	
+	private function purgeCache( $articleID ){
+		$key = $this->generateCacheKey( $articleID );
+		$this->app->wg->memc->delete( $key );
+	}
+	
+	private function generateCacheKey( $articleID ) {
+		return $this->app->wf->memcKey( 'LandingPagesAsContent', $articleID );
 	}
 }
