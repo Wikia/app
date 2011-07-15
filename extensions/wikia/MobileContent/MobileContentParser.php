62181bc6238878c775accef9e047c1cf2fe31da7
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-15 15:14:07 +0000

    renamed and nirvanized MobileContent extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39376 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/MobileContent/MobileContentParser.php
@@ -0,0 +1,33 @@
+<?php
+
+class MobileContentParser {
+	public static function onParserFirstCallInit( &$parser ) {
+		$parser->setHook( 'mobile', 'MobileContentParser::displayContent' );
+		$parser->setHook( 'nomobile', 'MobileContentParser::hideContent' );
+		return true;
+	}
+
+	public static function displayContent( $contents, $attributes, $parser ) {
+		$app = F::build('App');
+		$skin = $app->getGlobal( 'wgUser' )->getSkin();
+		$wgMobileSkins = $app->getGlobal( 'wgMobileSkins' );
+
+		if ( in_array( $skin->getSkinName(), $wgMobileSkins ) ) {
+			return $contents;
+		} else {
+			return '';
+		} 
+	}
+
+	public static function hideContent( $contents, $attributes, $parser ) {
+		$app = F::build('App');
+		$skin = $app->getGlobal( 'wgUser' )->getSkin();
+		$wgMobileSkins = $app->getGlobal( 'wgMobileSkins' );
+
+		if ( in_array( $skin->getSkinName(), $wgMobileSkins ) ) {
+			return '';
+		} else {
+			return $contents;
+		}
+	}
+}
