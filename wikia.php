53e68fa128b15ab82278289d66e302c6e91234d4
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-18 22:59:37 +0000

    trunk: wikia.php initialize skin and close transactions
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39433 e846bd91-c042-42a7-a218-4b65e2c1904d

--- wikia.php
+++ wikia.php
@@ -3,7 +3,18 @@
 require ( dirname( __FILE__ ) . '/includes/WebStart.php' );
 
 $app = F::app();
+
+// initialize skin via stub user object created in WebStart->Setup.php
+if ($app->wg->Request->getVal("skin", false)) {
+	$app->wg->User->getSkin();
+}
+
 $response = $app->sendRequest( null, null, null, false );
 
+// commit any open transactions just in case the controller forgot to
+if ($app->wg->Request->wasPosted()) {
+	$factory = wfGetLBFactory();
+	$factory->commitMasterChanges();  // commits only if writes were done on connection
+}
 $response->sendHeaders();
 $response->render();
