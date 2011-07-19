6d498ea0d9783e5c5b5e0eb8bbc58a3b298d5b07
christian <christian@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-19 18:58:34 +0000

    modal work
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39484 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GamingCalendar/js/GamingCalendar.js
+++ extensions/wikia/GamingCalendar/js/GamingCalendar.js
@@ -1,3 +1,7 @@
+$(function() {
+	GamingCalendar.init();
+});
+
 var GamingCalendar = {
     
     init: function() {
@@ -6,23 +10,8 @@ var GamingCalendar = {
     
     showCalendar: function(e) {
         e.preventDefault();
-        // AJAX request to load the data
-        var html = $.ajax({
-            url: "/wikia.php?controller=GamingCalendar&method=getModalLayout&format=html",
-            async: false
-        }).responseText;
-        // process the data
-        // put the data into a modal window
-        $.showModal( '', html );
-        // perform additional actions
-    }
-    
+      	$.get('/wikia.php?controller=GamingCalendar&method=getModalLayout&format=html', function(html) {
+			$(html).makeModal({width: 500, height: 400});
+		});
+    }    
 }
-
-wgAfterContentAndJS.push(
-    function() {
-        $('document').ready(function() {
-            GamingCalendar.init();
-        });
-    }
-);
