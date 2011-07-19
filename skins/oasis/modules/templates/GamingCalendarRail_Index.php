bc33c5522f7ae50649bd1710c8d998cf084974a2
michal <michal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-19 16:08:23 +0000

    BugId:8342 - SSW Gaming Calendar, CSS fixes for the rail module
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39469 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/GamingCalendarRail_Index.php
+++ skins/oasis/modules/templates/GamingCalendarRail_Index.php
@@ -1,8 +1,8 @@
 <section class="GamingCalendarModule">
-    <h1 class="gamingcalendar-module-heading">GAME CALENDAR</h1>
-    <div class="gamingcalendar-module-calendar">
-        <div class="gamingcalendar-module-calendar-month">July</div>
-        <div class="gamingcalendar-module-calendar-day"><span>14</span></div>
+    <h1>GAME CALENDAR</h1>
+    <div class="calendar">
+        <div class="month">July</div>
+        <div class="day"><span>14</span></div>
     </div>
     <img src="http://images4.wikia.nocookie.net/__spotlights/images/b0822292901836dabc186d0979b1f91f.jpg" width="" height="" alt="" />
     <h2>Uncharted</h2>
