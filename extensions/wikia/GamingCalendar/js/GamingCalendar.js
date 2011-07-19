bc2fc31a80799d3dc6eda9d8c1bf5fd9b21aa0b7
michal <michal@wikia-inc.com>
2011-07-19 21:05:33 +0000

    SSW GamingCalendar, rail module, cont.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39490 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GamingCalendar/js/GamingCalendar.js
+++ extensions/wikia/GamingCalendar/js/GamingCalendar.js
@@ -3,9 +3,28 @@ $(function() {
 });
 
 var GamingCalendar = {
-    
+
     init: function() {
-        $('.GamingCalendarModule a.more').bind( 'click', GamingCalendar.showCalendar );
+	$.get( '/wikia.php?controller=GamingCalendar&method=getEntries&format=json&startDate=1310947200&endDate=1311465600', function(data) {
+            // we pick "randomly" entry #0
+            data.entries[0].extended = true;
+            // store for future use
+            window.GamingCalendarData = data;
+            item = data.entries[0];
+            // extract date
+            // split title by : and do h2 and h3
+            // systems to comma separated
+            // open the calendar link
+            // but right now just a shortcut
+            html = '<div class="calendar"><div class="month">July</div><div class="day"><span>14</span></div></div>';
+            html += '<img src="http://images4.wikia.nocookie.net/__spotlights/images/b0822292901836dabc186d0979b1f91f.jpg" width="" height="" alt="" />';
+            html += '<h2>Prince of Persia</h2>';
+            html += '<h3>How to Become a Hacker</h3>';
+            html += '<p>PS3, paper and pencil</p>';
+            html += '<a href="#" class="more">Open&nbsp;Calendar&nbsp;&gt;</a>';
+            $('.GamingCalendarModule').append(html);
+            $('.GamingCalendarModule a.more').bind( 'click', GamingCalendar.showCalendar );
+        });
     },
     
     showCalendar: function(e) {
