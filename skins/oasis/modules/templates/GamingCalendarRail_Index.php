e8b95606db4e5fcc3f5388b868c4533c58946cd6
michal <michal@wikia-inc.com>
2011-07-19 12:35:07 +0000

    BugId:8342 - SSW Gaming Calendar, RailModule, basic styling.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39453 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/GamingCalendarRail_Index.php
+++ skins/oasis/modules/templates/GamingCalendarRail_Index.php
@@ -1,4 +1,12 @@
-<section class="GamingCalendarModule module">
-    <h1 class="gamingcalendar-module-heading"><?= wfMsg( 'gamingcalendar-heading' ); ?></h1>
-    <?= $contents; ?>
+<section class="GamingCalendarModule">
+    <h1 class="gamingcalendar-module-heading">GAME CALENDAR</h1>
+    <div class="gamingcalendar-module-calendar">
+        <div class="gamingcalendar-module-calendar-month">July</div>
+        <div class="gamingcalendar-module-calendar-day"><span>14</span></div>
+    </div>
+    <img src="http://images4.wikia.nocookie.net/__spotlights/images/b0822292901836dabc186d0979b1f91f.jpg" width="" height="" alt="" />
+    <h2>Uncharted</h2>
+    <h3>Drake's Deception</h3>
+    <p>PS3</p>
+    <a href="#" class="more">Open&nbsp;Calendar&nbsp;&gt;</a>
 </section>
