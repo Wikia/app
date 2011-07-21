87cb1a9f7aef45ca369e93dac55358b3862dcb96
moli <moli@wikia-inc.com>
2011-07-21 13:32:33 +0000

    copy user to cluster
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39611 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/copyUser.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set( "include_path", dirname(__FILE__)."/.." );
+require_once( 'commandLine.inc' );
+
+$userid = isset( $options['u'] ) ? $options['u'] : 0;
+
+if ( $userid > 0 ) {
+	die ( 'invalid user ID' );
+}
+
+$mExtUser = ExternalUser::newFromId( $userid );
+echo print_r( $mExtUser, true);
+if ( is_object( $mExtUser ) && ( 0 != $mExtUser->getId() ) ) {
+	$mExtUser->linkToLocal( $mExtUser->getId() );
+}
+
+?>
