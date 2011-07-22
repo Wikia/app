3eb6271f500e2633b5f576f1ad0ee55a9d1f8fc1
marcin <marcin@wikia-inc.com>
2011-07-22 10:52:05 +0000

    Smurfs Sweeps Landing Page (bugid:8788)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39675 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/LandingPageSmurfs/templates/main.tmpl.php
+++ extensions/wikia/LandingPageSmurfs/templates/main.tmpl.php
@@ -1,6 +1,6 @@
 <section id="LandingPageSmurfs" class="LandingPageSmurfs<?= ucfirst($langCode) ?>">
 	<a href="#" class="smurfs-link-enter"><?= wfMsg('landingpagesmurfs-enternow'); ?></a>
 	<a href="<?= wfMsg('landingpagesmurfs-facebook-url'); ?>" class="smurfs-link-fb">facebook</a>
-	<?= str_replace('<a', '<a class="smurfs-link-wiki"', wfMsgExt('landingpagesmurfs-wikia-site-link', array('parseinline'))); ?>"
+	<?= str_replace('<a', '<a class="smurfs-link-wiki"', wfMsgExt('landingpagesmurfs-wikia-site-link', array('parseinline'))); ?>
 	<a href="<?= wfMsg('landingpagesmurfs-site-url'); ?>" class="smurfs-link-trailer">trailer</a>
 </section>
