ee2cbcfa9df71868e1c0702081969cce135b8e34
macbre <macbre@wikia-inc.com>
2011-07-22 10:01:53 +0000

    Smurfs Landing Page: messages updated (BugId:8788)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39668 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/LandingPageSmurfs/LandingPageSmurfs.i18n.php
+++ extensions/wikia/LandingPageSmurfs/LandingPageSmurfs.i18n.php
@@ -4,12 +4,24 @@ $messages = array();
 
 $messages['en'] = array(
 	'landingpagesmurfs' => 'The Smurfs in 3D',
+	'landingpagesmurfs-enternow' => 'Enter Now',
+	'landingpagesmurfs-wikia-site-link' => '[http://www.smurfs.wikia.com Visit the Smurfs Wiki]',
+	'landingpagesmurfs-site-url' => 'http://www.sonypictures.co.uk/movies/thesmurfs/',
+	'landingpagesmurfs-facebook-url' => 'http://www.facebook.com/SmurfMovieUK',
 );
 
 $messages['fr'] = array(
 	'landingpagesmurfs' => 'Les Schtroumpfs en 3D',
+	'landingpagesmurfs-enternow' => 'Valider',
+	'landingpagesmurfs-wikia-site-link' => '[http://www.smurfs.wikia.com Visite le Wiki Schtroumpf]',
+	'landingpagesmurfs-site-url' => 'http://www.lesschtroumpfs-lefilm.fr/',
+	'landingpagesmurfs-facebook-url' => 'http://www.facebook.com/Les.Schtroumpfs.LeFilm',
 );
 
 $messages['nl'] = array(
 	'landingpagesmurfs' => 'De Smurfen ook in 3D',
+	'landingpagesmurfs-enternow' => 'Doe nu mee',
+	'landingpagesmurfs-wikia-site-link' => '[http://www.smurfs.wikia.com Bezoek de Smurfen Wikia]',
+	'landingpagesmurfs-site-url' => 'http://www.desmurfen-film.nl',
+	'landingpagesmurfs-facebook-url' => 'http://www.facebook.com/desmurfen',
 );
