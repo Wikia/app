eb474bd783bbbe976969263518e67a60ce711b8f
macbre <macbre@wikia-inc.com>
2011-07-22 14:50:57 +0000

    Editor reskin: no error message when trying to create layout/page without name (BugId:6723) - CreatePage.js cleanup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39688 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreatePage/js/CreatePage.js
+++ extensions/wikia/CreatePage/js/CreatePage.js
@@ -1,54 +1,43 @@
-var CreatePage = {};
-var CreatePageEnabled = false;
+var CreatePage = {
+	pageLayout: null,
+	options: {},
+	loading: false,
 
-CreatePage.pageLayout = null;
-CreatePage.options = {};
+	checkTitle: function(title, enterWasHit) {
+		$.getJSON(wgScript, {
+			action: 'ajax',
+			rs: 'wfCreatePageAjaxCheckTitle',
+			title: title
+		},
+		function(response) {
+			if(response.result == 'ok') {
+				var action = (enterWasHit) ? 'enter' : 'create';
 
-CreatePage.checkTitle = function( title, enterWasHit ) {
-	$.getJSON(wgScript,
-			{
-				'action':'ajax',
-				'rs':'wfCreatePageAjaxCheckTitle',
-				'title':title
-			},
-			function(response) {
-				if(response.result == 'ok') {
-					var action = (enterWasHit) ? 'enter' : 'create';
-
-					CreatePage.track(action + '/' + CreatePage.options[CreatePage.pageLayout]['trackingId']);
-					location.href = CreatePage.options[CreatePage.pageLayout]['submitUrl'].replace('$1', encodeURIComponent( title ));
-				}
-				else {
-					CreatePage.displayError(response.msg);
-				}
+				CreatePage.track(action + '/' + CreatePage.options[CreatePage.pageLayout]['trackingId']);
+				location.href = CreatePage.options[CreatePage.pageLayout]['submitUrl'].replace('$1', encodeURIComponent( title ));
 			}
-		);
-};
+			else {
+				CreatePage.displayError(response.msg);
+			}
+		});
+	},
 
-CreatePage.openDialog = function(e, titleText) {
-	// BugId:4941
-	if (!!window.WikiaEnableNewCreatepage === false) {
-		// create page popouts are disabled - follow the link
-		return;
-	}
+	openDialog: function(e, titleText) {
+		// BugId:4941
+		if ((!!window.WikiaEnableNewCreatepage) === false) {
+			// create page popouts are disabled - follow the link
+			return;
+		}
 
-	if(e) {
 		e.preventDefault();
-	}
-	if( false == CreatePageEnabled ) {
-		CreatePageEnabled = true;
-		jQuery.ajax({
-			async: false,
-			type: "POST",
-			url: wgScript + '?action=ajax&rs=wfCreatePageAjaxGetDialog',
-			data: null,
-			success: function(data) {
-				$().log(data);
+		if( false === CreatePage.loading ) {
+			CreatePage.loading = true;
+			$.getJSON(wgScript + '?action=ajax&rs=wfCreatePageAjaxGetDialog', null, function(data) {
 				$.showModal(data.title, data.html, {
 					width: data.width,
 					id: 'CreatePageDialog',
 					callback: function() {
-						CreatePageEnabled = false;
+						CreatePage.loading = false;
 						CreatePage.track( 'open' );
 
 						for(var name in CreatePage.options){
@@ -75,111 +64,118 @@ CreatePage.openDialog = function(e, titleText) {
 					onClose: function() {
 						CreatePage.track( 'close' );
 					}
-				})
-			},
-			dataType: 'json'
-		});
-
-	}
-};
-
-CreatePage.submitDialog = function( enterWasHit ) {
-	CreatePage.checkTitle( $('#wpCreatePageDialogTitle').val(), enterWasHit );
-};
-
-CreatePage.displayError = function( errorMsg ) {
-	var box = $( '#CreatePageDialogTitleErrorMsg' );
-	box.html( '<span id="createPageErrorMsg">' + errorMsg + '</span>' );
-	box.removeClass('hiddenStructure');
-};
-
-CreatePage.setPageLayout = function( layout ) {
-	CreatePage.pageLayout = layout;
-	var idToken = layout.charAt(0).toUpperCase() + layout.substring(1);
-
-	$('#CreatePageDialog' + idToken).attr( 'checked', 'checked' );
-	$('#CreatePageDialogChoices').children('li').removeClass( 'accent' );
-	$('#CreatePageDialog' + idToken + 'Container').addClass( 'accent' );
-	CreatePage.track(CreatePage.options[layout]['trackingId']);
-};
-
-CreatePage.track = function( str ) {
-	WET.byStr('CreatePage/' + str);
-};
-
-CreatePage.getTitleFromUrl = function( url ) {
-	var vars = [], hash;
-	var hashes = url.slice(url.indexOf('?') + 1).split('&');
-	for(var i = 0; i < hashes.length; i++)
-	{
-		hash = hashes[i].split('=');
-		vars.push(hash[0]);
-		vars[hash[0]] = hash[1];
-	}
-	return vars['title'].replace(/_/g, ' ');
-};
-
-CreatePage.redLinkClick = function(e, titleText) {
-	title = titleText.split(':');
-	isContentNamespace = false;
-	if( window.ContentNamespacesText && (title.length > 1) ) {
-		for(var i in window.ContentNamespacesText) {
-			if(title[0] == window.ContentNamespacesText[i]) {
-				isContentNamespace = true;
-			}
+				});
+			});
 		}
-	}
-	else {
-		isContentNamespace = true;
-	}
+	},
 
-	if( isContentNamespace ) {
-		CreatePage.openDialog(e, titleText );
-	}
-	else {
-		return false;
-	}
-};
+	submitDialog: function( enterWasHit ) {
+		CreatePage.checkTitle( $('#wpCreatePageDialogTitle').val(), enterWasHit );
+	},
 
-$(function() {
-	if( window.WikiaEnableNewCreatepage ) {
-		$().log('init', 'CreatePage');
+	displayError: function( errorMsg ) {
+		var box = $( '#CreatePageDialogTitleErrorMsg' );
+		box.html( '<span id="createPageErrorMsg">' + errorMsg + '</span>' );
+		box.removeClass('hiddenStructure');
+	},
 
-		if( !window.WikiaDisableDynamicLinkCreatePagePopup ) {
-			$( '#dynamic-links-write-article-link, #dynamic-links-write-article-icon' ).click( function(e) {CreatePage.openDialog(e, null);});
-			$('.noarticletext a').click( function(e) {CreatePage.openDialog(e, wgPageName); return false; });
+	setPageLayout: function( layout ) {
+		CreatePage.pageLayout = layout;
+		var idToken = layout.charAt(0).toUpperCase() + layout.substring(1);
+
+		$('#CreatePageDialog' + idToken).attr( 'checked', 'checked' );
+		$('#CreatePageDialogChoices').children('li').removeClass( 'accent' );
+		$('#CreatePageDialog' + idToken + 'Container').addClass( 'accent' );
+		CreatePage.track(CreatePage.options[layout]['trackingId']);
+	},
+
+	track: function( str ) {
+		WET.byStr('CreatePage/' + str);
+	},
+
+	getTitleFromUrl: function( url ) {
+		var vars = [],
+			i,
+			hash,
+			hashes = url.slice(url.indexOf('?') + 1).split('&');
+
+		for(i = 0; i < hashes.length; i++) {
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
 		}
 
-		// CreatePage chicklet (Oasis)
-		$('.createpage').click(CreatePage.openDialog);
+		return vars['title'].replace(/_/g, ' ');
+	},
 
-		// macbre: RT #38478
-		var addRecipeTab = $('#add_recipe_tab');
-		if (addRecipeTab.exists()) {
-			var addRecipeLink = addRecipeTab.find('a');
+	redLinkClick: function(e, titleText) {
+		var title = titleText.split(':'),
+			isContentNamespace = false;
 
-			// only show popup if this tab really points to CreatePage
-			if (addRecipeLink.attr('href').match(/CreatePage$/)) {
-				addRecipeLink.click(CreatePage.openDialog);
-			}
-		}
-
-		$("a.new").bind('click', function(e) {CreatePage.redLinkClick(e, CreatePage.getTitleFromUrl(this.href))} );
-		$(".createboxButton").bind('click', function(e) {
-			var form = $(e.target).parent();
-
-			// make sure we're inside createbox and not inputbox (RT #40959)
-			if(form.attr('class') == 'createboxForm') {
-				var field = form.children('.createboxInput');
-				var preloadField = form.children("input[name='preload']");
-
-				if((typeof preloadField.val() == undefined) || (preloadField.val() == '')) {
-					CreatePage.openDialog(e, field.val());
-				}
-				else {
-					return true;
+		if( window.ContentNamespacesText && (title.length > 1) ) {
+			for(var i in window.ContentNamespacesText) {
+				if(title[0] == window.ContentNamespacesText[i]) {
+					isContentNamespace = true;
 				}
 			}
-		});
+		}
+		else {
+			isContentNamespace = true;
+		}
+
+		if( isContentNamespace ) {
+			CreatePage.openDialog(e, titleText );
+		}
+		else {
+			return false;
+		}
+	},
+
+	init: function() {
+		if( window.WikiaEnableNewCreatepage ) {
+			$().log('init', 'CreatePage');
+
+			if( !window.WikiaDisableDynamicLinkCreatePagePopup ) {
+				$( '#dynamic-links-write-article-link, #dynamic-links-write-article-icon' ).click( function(e) {CreatePage.openDialog(e, null);});
+				$('.noarticletext a').click( function(e) {CreatePage.openDialog(e, wgPageName); return false; });
+			}
+
+			// CreatePage chicklet (Oasis)
+			$('.createpage').click(CreatePage.openDialog);
+
+			// macbre: RT #38478
+			var addRecipeTab = $('#add_recipe_tab');
+			if (addRecipeTab.exists()) {
+				var addRecipeLink = addRecipeTab.find('a');
+
+				// only show popup if this tab really points to CreatePage
+				if (addRecipeLink.attr('href').match(/CreatePage$/)) {
+					addRecipeLink.click(CreatePage.openDialog);
+				}
+			}
+
+			$("a.new").bind('click', function(e) {
+				CreatePage.redLinkClick(e, CreatePage.getTitleFromUrl(this.href));
+			});
+
+			$(".createboxButton").bind('click', function(e) {
+				var form = $(e.target).parent();
+
+				// make sure we're inside createbox and not inputbox (RT #40959)
+				if(form.attr('class') == 'createboxForm') {
+					var field = form.children('.createboxInput');
+					var preloadField = form.children("input[name='preload']");
+
+					if((typeof preloadField.val() == undefined) || (preloadField.val() == '')) {
+						CreatePage.openDialog(e, field.val());
+					}
+					else {
+						return true;
+					}
+				}
+			});
+		}
 	}
-});
+};
+
+$(CreatePage.init);
