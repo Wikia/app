7d1036f1cbd0646cf5d2261d98a019043b7d7c06
marcin <marcin@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-26 16:23:56 +0000

    Editor reskin: Fix/add access keys for new layout (e.g. Save button) (bugid:5667)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39852 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditPageReskin/RTE/js/plugins/accesskey/plugin.js
+++ extensions/wikia/EditPageReskin/RTE/js/plugins/accesskey/plugin.js
@@ -1,17 +1,19 @@
 CKEDITOR.plugins.add('rte-accesskey',
 {
 	init: function(editor) {
-		if (CKEDITOR.env.ie) {
-			var accessibleElements = [];
-			$('*[accesskey]').each(function(ev) {
-				accessibleElements[$(this).attr('accesskey').toUpperCase()] = $(this);
-			});
-			RTE.getEditor().bind('keydown', function(e) {
-				var key = String.fromCharCode(e.keyCode).toUpperCase();
-				if (e.altKey && accessibleElements[key]) {
-					accessibleElements[key].click();
-				}
-			});
-		}
+		editor.on('wysiwygModeReady', function() {
+			if (CKEDITOR.env.ie) {
+				var accessibleElements = [];
+				$('*[accesskey]').each(function(ev) {
+					accessibleElements[$(this).attr('accesskey').toUpperCase()] = $(this);
+				});
+				RTE.getEditor().bind('keydown', function(e) {
+					var key = String.fromCharCode(e.keyCode).toUpperCase();
+					if (e.altKey && accessibleElements[key]) {
+						accessibleElements[key].click();
+					}
+				});
+			}
+		});
 	}
 });
