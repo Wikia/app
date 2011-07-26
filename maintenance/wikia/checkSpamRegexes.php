d809aa5667892bd05d9228557bb62ae8f148db82
moli <moli@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-26 12:03:56 +0000

    code review
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39824 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/checkSpamRegexes.php
+++ maintenance/wikia/checkSpamRegexes.php
@@ -15,46 +15,63 @@ if ( !isset($title) ) {
 
 $dbr = wfGetDB(DB_SLAVE, 'cron', $wgExternalSharedDB);
 
-echo "read all Wikis \n";
-$where = ( $wikia ) ? array('city_dbname' => $wikia ) : array('city_public' => 1);
-$data = $dbr->select(
-	array('city_list'),
-	array('city_id', 'city_dbname'),
-	$where,
-	__METHOD__
-);
-$pages = array();
-while ($row = $dbr->fetchObject($data)) {
-	$wikis[] = array( 'id' => $row->city_id, 'name' => $row->city_dbname );
-}
-$dbr->FreeResult($data);
-
-if ( !empty($wikis) ) {
-	foreach ( $wikis as $wiki ) {
-		$db = wfGetDB(DB_SLAVE, 'cron', $wiki['name']);
-		if ( $db ) {
-			echo "check Wikia: {$wiki['name']} \n";
-			$row = $db->selectRow('page', array('page_id'), array('page_title' => $title, 'page_namespace' => NS_MEDIAWIKI) );
-			if ( $row && $row->page_id ) {
-				echo "Found page: $title ({$row->page_id}) \n";
-				$oGTitle = GlobalTitle::newFromId( $row->page_id, $wiki['id'] );
-				if ( $oGTitle ) {
-					$text = $oGTitle->getText();
-					$regexes = SpamRegexBatch::buildSafeRegexes( $text );
-					echo "Found " . count($regexes) . " regexes \n";
-					$loop = 0;
-					foreach ($regex as $id => $regex) {
-						$m = array();
-						if (preg_match($regex, strtolower($title->getText()), $m)) {
-							echo "wikia: " . $wiki['name'] . " - ok ( $loop )\n";
-						}
-						$loop++;
-					}				
+if ( !empty( $wikia ) ) {
+	$oTitle = Title::makeTitle( NS_MEDIAWIKI, $title );
+	if ( is_object( $oTitle ) ) {
+		$oArticle = new Article( $oTitle );
+		$content = $oArticle->getContent();
+		if ( is_object( $oArticle ) && !empty( $content ) ) {
+			echo "article = " . print_r( $oArticle, true );
+			echo "content = " . $oArticle->getContent() . " \n";
+			$regexes = SpamRegexBatch::buildSafeRegexes( $content );
+			echo "Found " . count($regexes) . " regexes \n";
+			$loop = 0;
+			foreach ($regex as $id => $regex) {
+				$m = array();
+				if (preg_match($regex, strtolower($title->getText()), $m)) {
+					echo "wikia: " . $wgDBName . " - ok ( $loop )\n";
 				}
+				$loop++;
 			}
+		} else {
+			echo "No text found \n";
+		}
+	} else {
+		echo "Page not found \n";
+	}
+} else {
+	echo "read all Wikis \n";
+	$where = ( $wikia ) ? array('city_dbname' => $wikia ) : array('city_public' => 1);
+	$data = $dbr->select(
+		array('city_list'),
+		array('city_id', 'city_dbname'),
+		$where,
+		__METHOD__
+	);
+	$pages = array();
+	while ($row = $dbr->fetchObject($data)) {
+		$wikis[] = array( 'id' => $row->city_id, 'name' => $row->city_dbname );
+	}
+	$dbr->FreeResult($data);
+
+	if ( !empty($wikis) ) {
+		foreach ( $wikis as $wiki ) {
+			$sCommand  = "SERVER_ID={$wiki['id']} php $IP/maintenance/wikia/checkSpamRegexes.php ";
+			$sCommand .= "--title " . $title . " ";
+			$sCommand .= "--wikia " . $wiki['name'] . " ";
+			$sCommand .= "--conf $wgWikiaLocalSettingsPath";
+
+			$log = wfShellExec( $sCommand, $retval );
+			if ($retval) {
+				echo "Error code returned: $retval, Error was: $log \n";
+			}
+			else {
+				echo "Done: $log \n";
+			}				
 		}
 	}
 }
-
+		
 $time = microtime(true) - $time_start;
 echo "\n-- Execution time: $time seconds\n";
+exit(0);
