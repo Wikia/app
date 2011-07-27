ae5d507b7aa57cef13e09b5ac92a893c1d96889a
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-27 19:13:12 +0000

    Merge back a fix from production to WikiaApiQueryEventsData.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39955 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaApi/WikiaApiQueryEventsData.php
+++ extensions/wikia/WikiaApi/WikiaApiQueryEventsData.php
@@ -788,33 +788,35 @@ class WikiaApiQueryEventsData extends ApiQueryBase {
 		/* use SimpleHTMLDom extensions here ( include in lib/simplehtmldom ) */
 		$html = str_get_html( $this->mContent );
 		
-		$metricsTypes = $this->_get_metrics_types();
-			
-		foreach ( $metricsTypes as $id => $name ) {
-			$res = 0;
-			switch ( $id ) {
-				case 1: /* mainpage */
-					$res = $this->_is_main_page();
-					break;
-				case 2: /* gallery */
-					$gallery = count($html->find("gallery"));
-					$slider = count($html->find("gallery[type=slider]"));
-					$slideshow = count($html->find("gallery[type=slideshow]"));
-					$res = $gallery - $slider - $slideshow;
-					if ( $res < 0 ) $res = 0;
-					break;
-				case 3: /* slider */
-					$res = count($html->find("gallery[type=slider]"));
-					break;
-				case 4: /* slideshow */
-					$res = count($html->find("gallery[type=slideshow]"));
-					break;
-				default:
-					break;
-			}
-			
-			if ( $res > 0 ) {
-				$details[] = array( 'id' => $id, 'name' => $name, 'count' => $res );
+		if(is_object($html)){
+			$metricsTypes = $this->_get_metrics_types();
+				
+			foreach ( $metricsTypes as $id => $name ) {
+				$res = 0;
+				switch ( $id ) {
+					case 1: /* mainpage */
+						$res = $this->_is_main_page();
+						break;
+					case 2: /* gallery */
+						$gallery = count($html->find("gallery"));
+						$slider = count($html->find("gallery[type=slider]"));
+						$slideshow = count($html->find("gallery[type=slideshow]"));
+						$res = $gallery - $slider - $slideshow;
+						if ( $res < 0 ) $res = 0;
+						break;
+					case 3: /* slider */
+						$res = count($html->find("gallery[type=slider]"));
+						break;
+					case 4: /* slideshow */
+						$res = count($html->find("gallery[type=slideshow]"));
+						break;
+					default:
+						break;
+				}
+				
+				if ( $res > 0 ) {
+					$details[] = array( 'id' => $id, 'name' => $name, 'count' => $res );
+				}
 			}
 		}
 
