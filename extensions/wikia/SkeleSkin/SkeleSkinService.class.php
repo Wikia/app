b99da1ab700ae18e41bb6868f81ceda1950c8759
jolek <jolek@wikia-inc.com>
2011-07-27 16:36:38 +0000

    SkeleSkin: Small improvments (BugId:9071)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39934 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SkeleSkin/SkeleSkinService.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class SkeleSkinService extends WikiaService {
+	
+	public function index() {
+
+		$response = $this->sendRequest( 'SkeleSkinWikiHeaderService', 'index' )->toString();
+
+		return false;
+	}
+}
---
+++ extensions/wikia/SkeleSkin/templates/SkeleSkinWikiHeaderService_index.php
@@ -0,0 +1,3 @@
+<body onload="setTimeout(function() { window.scrollTo(0, 1) }, 100);">
+<h1><?php echo $wordmarkText; ?></h1>
+</body>
