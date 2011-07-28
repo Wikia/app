cc8f728a0594e81e105ee07a4026c0c647ebf411
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-28 19:57:31 +0000

    SkeleSkin: js refactor
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40019 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SkeleSkin/templates/SkeleSkinBodyService_index.php
+++ extensions/wikia/SkeleSkin/templates/SkeleSkinBodyService_index.php
@@ -1,6 +1,6 @@
 <h1><?= $wf->MsgExt( 'skeleskin-page-header', array( 'parseinline' ), $headerText ); ?></h1>
 <?= $afterBodyContent ?>
 <section id="WikiaPage">
-		<article id="WikiaMainContent"><?= $bodyContent ?></article>
-		<footer id="WikiaMainContentFooter"><?= $afterContentHookText ;?></footer>
+	<article id="WikiaMainContent"><?= $bodyContent ?></article>
+	<footer id="WikiaMainContentFooter"><?= $afterContentHookText ;?></footer>
 </section>
