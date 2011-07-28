2b0cfa7fbf8981a57105bd5f313deeb8c0dde189
nandy <nandy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-07-28 13:54:56 +0000

    removed interview part from the user profile page v3 template till we finish it; litte changes in javascript file and messages file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@39993 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserProfilePageV3/templates/UserProfilePage_index.php
+++ extensions/wikia/UserProfilePageV3/templates/UserProfilePage_index.php
@@ -1,19 +1,3 @@
-<div id="UPPInterviewBox">
-	<h2><?= wfMsg( 'userprofilepage-interview-section-title' ); ?></h2>
-	<?php if($isUserPageOwner): ?>
-		<button id="UPPAnswerQuestions">answer questions</button>
-	<?php endif; ?>
-	<?php foreach( $questions as $question ): ?>
-		<div>
-			<em><?=$question->getBody();?></em>
-		</div>
-		<div>
-			<?=$question->getAnswerBody(); ?>
-		</div>
-	<?php endforeach; ?>
-</div>
-
 <div id="UPPStuffBox">
-	<h2><?= wfMsg( 'userprofilepage-stuff-section-title' ); ?></h2>
 	<p><?= $stuffSectionBody ?></p>
 </div>
