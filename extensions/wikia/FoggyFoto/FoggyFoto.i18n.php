1390652e53e7cd3eca12325ab12e39a25d4ed842
sean <sean@wikia-inc.com>
2011-07-30 01:03:46 +0000

    BugzId 8876: Added FoggyFoto extension which is the early code for the canvas game which will power the FoggyFoto mobile app. Currently just displays a front image over an obscured back-image which can be revealed in chunks of a configurable size.  Scales and centers the images to look optimal on the canvas-size regardless of the size of the image.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40089 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/FoggyFoto/FoggyFoto.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Internationalisation file for Special:FoggyFoto extension / game.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'foggyfoto' => 'Foggy Foto game',
+	'foggyfoto-desc' => 'This extension creates a page where the Foggy Foto game can be played in HTML5 + Canvas. It will be accessible via Nirvana\s APIs, not as a normal page.',
+);
+
+/** Message documentation (Message documentation)
+ *
+ */
+$messages['qqq'] = array(
+	
+);
---
+++ extensions/wikia/FoggyFoto/FoggyFoto.setup.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Sean Colombo
+ *
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) die("This is a MediaWiki extension.");
+
+$wgExtensionMessagesFiles['FoggyFoto'] = dirname( __FILE__ ) . '/SpecialFoggyFoto.i18n.php';
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'FoggyFoto',
+	'url' => 'http://www.wikia.com/Mobile', // TODO: Update with a link to a landing page for the SPECIFIC mobile app once that is available (eg: /Mobile/FoggyFoto)
+	'author' => '[http://www.seancolombo.com Sean Colombo]',
+	'descriptionmsg' => 'foggyfoto-desc',
+	'version' => '0.1',
+);
+
+
+$dir = dirname(__FILE__) . '/';
+$app = F::app();
+
+$app->registerClass('FoggyFotoController', $dir . 'FoggyFotoController.class.php');
+$app->registerExtensionMessageFile('FoggyFoto', $dir . 'FoggyFoto.i18n.php');
