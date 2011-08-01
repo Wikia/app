999b2fd475408c72e2364e49ef3df3245616463f
sean <sean@wikia-inc.com>
2011-08-01 18:55:25 +0000

    FoggyFoto: Added click-handling to flip individual tiles, made rendering optionally be per-tile for speed, and added a class-level debug switch so that all of the console.log's can be turned off at once.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40143 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/FoggyFoto/js/FoggyFoto.js
+++ extensions/wikia/FoggyFoto/js/FoggyFoto.js
@@ -16,18 +16,19 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
 	 *   then center the image vertically (crops off the top/bottom as needed).
 	 * - If the image aspect ratio is less than or equal to the screen aspect ratio, scale the width of the image to
 	 *   fit, then center the image horizontally (cropping some off both sides as needed).
-	 *
-	 * TODO: Create a function to allow debugging to be enabled/disabled across the whole class.
 	 */
 	FoggyFoto.FlipBoard = function(){
 		var self = this;
-		
+		this.debug = false; // whether to log a whole bunch of info to console.log
+
 		this.canvas = null;
 		this.context = null;
 
 		// URLs of the images
 		this.frontImageSrc = 'http://images4.wikia.nocookie.net/__cb20110304030006/lyricwiki/images/b/b7/Deadmau5_-_It_Sounds_Like.jpg'; // this shows up immediately
 		this.backImageSrc = "http://images1.wikia.nocookie.net/__cb20110606201926/lyricwiki/images/3/39/Deadmau5_-_Project_56.jpg"; // this is the one that's obscured
+		this.frontImage = null;
+		this.backImage = null;
 
 		this.width = 480;
 		this.height = 320;
@@ -47,19 +48,20 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
 		 * After setting the dimensions of the flip board, this should be called once to set up the isBackShowing matrix
 		 * to the right size.
 		 *
-		 * Also, calculates the _tileWidth and _tileHeight values.
+		 * Calculates the _tileWidth and _tileHeight values, sets up clickhandling, loads the front and back images.
+		 *
+		 * Takes in a callback function which will be called after both of the images are loaded.
 		 */
-		this.init = function(){
+		this.init = function(callback){
 			self.canvas = document.getElementById("foggyCanvas");
-			if( self.canvas.getContext )
-			{
+			if( self.canvas.getContext ){
 				self.context = self.canvas.getContext("2d");
 			}
 
 			// Calculate the size of the tiles in pixels.
 			self._tileWidth = (self.width / self.numCols);
 			self._tileHeight = (self.height / self.numRows);
-		
+
 			// Initialize the matrix of what is revealed.
 			for(var row=0; row < self.numRows; row++){
 				for(var col=0; col < self.numCols; col++){
@@ -70,14 +72,35 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
 					self.isBackShowing[row][col] = 0; // back is completely obscured by default
 				}
 			}
+
+			// Load the images before continuing.
+			self.frontImage = new Image();
+			self.frontImage.src = self.frontImageSrc;
+			self.frontImage.onload = function(){
+				self.backImage = new Image();
+				self.backImage.src = self.backImageSrc;
+				self.backImage.onload = function(){
+					// Attach click-handler.
+					self.canvas.addEventListener('click', self.handleClick, false);
+					
+					// We're done loading things, call the callback.
+					if(typeof callback == "function"){
+						callback();
+					}
+				};
+			};
 		};
-		
+
 		/**
 		 * Toggles the display of the tile at the given row and column.
 		 */
 		this.flip = function(row, col){
 			if(typeof self.isBackShowing[row] != "undefined"){
 				if(typeof self.isBackShowing[row][col] != "undefined"){
+				
+					// TODO: Animation for the flip.
+					// TODO: Animation for the flip.
+				
 					self.isBackShowing[row][col] = (self.isBackShowing[row][col] === 0 ? 1 : 0);
 				}
 			}
@@ -114,51 +137,61 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
 				for(var col=0; col < self.numCols; col++){
 					rowStr += "" + self.isBackShowing[row][col] + " ";
 				}
-				console.log(rowStr + "\n");
+				self.log(rowStr + "\n");
 			}
 		};
 
 		/**
-		 * Draws the images to the board, taking into account which tiles are revelaed
+		 * Draws the entire image to the board, taking into account which tiles are revelaed
 		 * according to isBackShowing.
 		 */
 		this.drawBoard = function(){
 			// Display the unaltered image first.
-			var frontImage = new Image();
-			frontImage.src = self.frontImageSrc;
-			frontImage.onload = function(){
-				// Draw the front image at the top-left of the canvas... scaled & centered as needed.
+			
+			// Draw the front image at the top-left of the canvas... scaled & centered as needed.
 //				var fsf = self._getScalingFactor(frontImage); // scaling factor for the front image (Front Scaling Factor == fsf)
 //				var frontOffsetX = ( Math.abs((frontImage.width * fsf) - self.canvas.width) / 2 );
 //				var frontOffsetY = ( Math.abs((frontImage.height * fsf) - self.canvas.height) / 2 );
 //				self._drawImage(frontImage, frontOffsetX, frontOffsetY, (frontImage.width * fsf), (frontImage.height * fsf), 0, 0, self.canvas.width, self.canvas.height);
-				self._drawImageAutoScaled(frontImage, 0, 0, self.canvas.width, self.canvas.height, 0, 0, self.canvas.width, self.canvas.height);
+			self._drawImageAutoScaled(self.frontImage, 0, 0, self.canvas.width, self.canvas.height, 0, 0, self.canvas.width, self.canvas.height);
 
-				// Once the front image is loaded, load the back image and display it only in spots that are flipped.
-				var backImage = new Image();
-				backImage.src = self.backImageSrc;
-				backImage.onload = function(){
-					for(var row=0; row < self.numRows; row++){
-						for(var col=0; col < self.numCols; col++){
-							if((self.isBackShowing[row]) && (self.isBackShowing[row][col] === 1)){
-								console.log("== SHOWING TILE AT (" + row + ", " + col + ") ==");
-								var sourceX = (self._tileWidth * col);
-								var sourceY = (self._tileHeight * row);
-								var sourceWidth = self._tileWidth;
-								var sourceHeight = self._tileHeight;
-								var destWidth = sourceWidth;
-								var destHeight = sourceHeight;
-								var destX = sourceX;
-								var destY = sourceY;
-
-								self._drawImageAutoScaled(backImage, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
-							}
-						}
+			// Display the back-image only in spots that are flipped.
+			for(var row=0; row < self.numRows; row++){
+				for(var col=0; col < self.numCols; col++){
+					// Since we displayed the whole front image, we can skip updating this tile unless it is showing the back tile.
+					if((self.isBackShowing[row]) && (self.isBackShowing[row][col] === 1)){
+						this.drawTile(row, col);
 					}
-				};			
+				}
 			}
 		};
-		
+
+		/**
+		 * Updates the display of a single tile (much faster than redrawing the whole board when one tile changes).
+		 */
+		this.drawTile = function(row, col){
+			var imageToUse;
+			self.log("== SHOWING TILE AT (" + row + ", " + col + ") ==");
+			if((self.isBackShowing[row]) && (self.isBackShowing[row][col] === 1)){
+				imageToUse = self.backImage;
+				self.log("[showing back image for this tile]");
+			} else {
+				imageToUse = self.frontImage;
+				self.log("[showing front image for this tile]");
+			}
+
+			var sourceX = (self._tileWidth * col);
+			var sourceY = (self._tileHeight * row);
+			var sourceWidth = self._tileWidth;
+			var sourceHeight = self._tileHeight;
+			var destWidth = sourceWidth;
+			var destHeight = sourceHeight;
+			var destX = sourceX;
+			var destY = sourceY;
+
+			self._drawImageAutoScaled(imageToUse, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+		};
+
 		/**
 		 * Uses the scaling factor to draw the given image to the canvas.  Takes care of converting the image object so that it acts as though it is the size it
 		 * would be if all dimensions were multiplied by the scaling factor and the image was centered so the center of the image and the centere of the screen align.
@@ -169,21 +202,19 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
 		 */
 		this._drawImageAutoScaled = function(imageObject, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight){
 			var scalingFactor = self._getScalingFactor(imageObject);
-//			var offsetX = ( Math.abs((imageObject.width * scalingFactor) - self.canvas.width) / 2 );
-//			var offsetY = ( Math.abs((imageObject.height * scalingFactor) - self.canvas.height) / 2 );
 			var offsetX = ( Math.abs(imageObject.width - (self.canvas.width / scalingFactor)) / 2 );
 			var offsetY = ( Math.abs(imageObject.height - (self.canvas.height / scalingFactor)) / 2 );
-			console.log("Scaling factor: " + scalingFactor);
-			console.log("\tOffset X: " + offsetX);
-			console.log("\tOffset Y: " + offsetY);
+			self.log("Scaling factor: " + scalingFactor);
+			self.log("\tOffset X: " + offsetX);
+			self.log("\tOffset Y: " + offsetY);
 			
 			// Scale the width and height to act like they were the size of the screen all along (so if the screen is 500px wide and sourceWidth starts
 			// at 250px, we'll end up passing in half of the actual width of the scaled source image).
 			var scaledSourceWidth = Math.round(sourceWidth / scalingFactor);
 			var scaledSourceHeight = Math.round(sourceHeight / scalingFactor);
 			
-			var scaledX = (offsetX + (sourceX / scalingFactor));
-			var scaledY = (offsetY + (sourceY / scalingFactor));
+			var scaledX = Math.floor(offsetX + (sourceX / scalingFactor));
+			var scaledY = Math.floor(offsetY + (sourceY / scalingFactor));
 
 			self._drawImage(imageObject, scaledX, scaledY, scaledSourceWidth, scaledSourceHeight, destX, destY, destWidth, destHeight);
 		};
@@ -194,13 +225,13 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
 		this._drawImage = function(imageObject, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight){
 			var debug = true;
 			if(debug){
-				console.log("Drawing " + imageObject.src);
-				console.log("\tsourceX,Y: (" + sourceX + ", " + sourceY + ")\n");
-				console.log("\tsourceW: "+ sourceWidth +"\n");
-				console.log("\tsourceH: "+ sourceHeight +"\n");
-				console.log("\tdestX,Y: (" + destX + ", " + destY + ")\n");
-				console.log("\tdestW: "+ destWidth +"\n");
-				console.log("\tdestH: "+ destHeight +"\n");
+				self.log("Drawing " + imageObject.src);
+				self.log("\tsourceX,Y: (" + sourceX + ", " + sourceY + ")\n");
+				self.log("\tsourceW: "+ sourceWidth +"\n");
+				self.log("\tsourceH: "+ sourceHeight +"\n");
+				self.log("\tdestX,Y: (" + destX + ", " + destY + ")\n");
+				self.log("\tdestW: "+ destWidth +"\n");
+				self.log("\tdestH: "+ destHeight +"\n");
 			}
 			self.context.drawImage(imageObject, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 		};
@@ -211,21 +242,21 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
 		 * distortion (maintain the same aspect ratio).
 		 */
 		this._getScalingFactor = function(imgObject){
-		//	console.log("_getScalingFactor()");
+		//	self.log("_getScalingFactor()");
 
 			var scalingFactor = 1;
 
 			var imgW = imgObject.width;
 			var imgH = imgObject.height;
 			var imgAspectRatio = (imgW / imgH);
-		//	console.log("\tIMAGE WIDTH:  " + imgW);
-		//	console.log("\tIMAGE HEIGHT: " + imgW);
-		//	console.log("\tASPECT RATIO: " + imgAspectRatio);
+		//	self.log("\tIMAGE WIDTH:  " + imgW);
+		//	self.log("\tIMAGE HEIGHT: " + imgW);
+		//	self.log("\tASPECT RATIO: " + imgAspectRatio);
 
 			var screenAspectRatio = (self.canvas.width / self.canvas.height);
-		//	console.log("\tSCREEN WIDTH:  " + self.canvas.width);
-		//	console.log("\tSCREEN HEIGHT: " + self.canvas.height);
-		//	console.log("\tASPECT RATIO: " + screenAspectRatio);
+		//	self.log("\tSCREEN WIDTH:  " + self.canvas.width);
+		//	self.log("\tSCREEN HEIGHT: " + self.canvas.height);
+		//	self.log("\tASPECT RATIO: " + screenAspectRatio);
 			if(imgAspectRatio > screenAspectRatio){
 				// Scale the height to fit the screen exactly (width may overflow a bit so that the left & right get cropped off a bit).
 				scalingFactor = (self.canvas.height / imgH);
@@ -234,12 +265,82 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
 				scalingFactor = (self.canvas.width / imgW);
 			}
 
-		//	console.log("\t= SCALING FACTOR: " + scalingFactor);
+		//	self.log("\t= SCALING FACTOR: " + scalingFactor);
 			return scalingFactor;
 		};
+
+		/**
+		 * Click-handler which recieves all clicks on the canvas.
+		 */
+		this.handleClick = function(e){
+			var tile = self._getTileByClick(e);
+
+			self.log("Click on tile: (" + tile.row + ", " + tile.col + ")");
+
+			self.flip( tile.row, tile.col );
+			self.drawTile( tile.row, tile.col ); // TODO: Should this be part of the flip/show functions by default?
+		};
+		
+		/**
+		 * Given a click event, calculates what cell on the board the click occurred in.
+		 *
+		 * Cross-platform trickery courtesy of:
+		 * http://answers.oreilly.com/topic/1929-how-to-use-the-canvas-and-draw-elements-in-html5/
+		 *
+		 * @return a FoggyFoto.FlipBoard.Tile (which is just a wrapper for the coordinates)
+		 */
+		this._getTileByClick = function(e){
+			var x;
+			var y;
+			if (e.pageX || e.pageY) {
+			  x = e.pageX;
+			  y = e.pageY;
+			}
+			else {
+			  x = e.clientX + document.body.scrollLeft +
+				   document.documentElement.scrollLeft;
+			  y = e.clientY + document.body.scrollTop +
+				   document.documentElement.scrollTop;
+			}
+			
+			x -= self.canvas.offsetLeft;
+			y -= self.canvas.offsetTop;
+			
+			var tile = new FoggyFoto.FlipBoard.Tile();
+			tile.row = Math.floor(y / self._tileWidth);
+			tile.col = Math.floor(x / self._tileHeight);
+
+			return tile;
+		};
+		
+		/**
+		 * Simple wrapper for console.log to allow us to turn on/off debugging for this
+		 * whole class at once.
+		 */
+		this.log = function(msg){
+			if(self.debug){
+				console.log(msg);
+			}
+		}
+	};
+	
+	/**
+	 * Currently just a wrapper for the row and column coordinates of a tile.  The coordinates are
+	 * relative to the tiles not to pixels.  For example, if we had a FlipBoard with 6 columns and
+	 * 4 rows of Tiles and the tiles were 80 pixels squared, this class would be used to hold a value
+	 * between (0,0) and (5,3).
+	 *
+	 * This is primarily used as  a convenience-type for passing coordinates around & isn't intended to
+	 * balloon into holding a bunch of functionality (since Tiles themselves are more of a locational
+	 * concept at the moment and not strictly-speaking objects - the front and back of the tile do not
+	 * belong to the tile, but rather are calculated at display-time given the location of the tile and
+	 * whether the front image or back image is supposed to be displayed).
+	 */
+	FoggyFoto.FlipBoard.Tile = function(){
+		this.row = -1;
+		this.col = -1;
 	};
 }
-
 
 
 
@@ -251,23 +352,23 @@ if (typeof FoggyFoto.FlipBoard === 'undefined') {
  */
 function initGame()
 {
-	console.log("Started...");
+	//console.log("Started...");
 	var flipBoard = new FoggyFoto.FlipBoard();
-	flipBoard.init();
+	flipBoard.init(function(){
+		/*flipBoard.show(0,0);
+		//flipBoard.show(0,1);
+		flipBoard.show(0,2);
+		//flipBoard.show(1,0);
+		flipBoard.show(1,1);
+		//flipBoard.show(1,2);
+		flipBoard.show(2,0);
+		//flipBoard.show(2,1);
+		flipBoard.show(2,2);
+		flipBoard.show(3,3);
+		flipBoard.show(4,1);*/
 
-	flipBoard.show(0,0);
-	//flipBoard.show(0,1);
-	flipBoard.show(0,2);
-	//flipBoard.show(1,0);
-	flipBoard.show(1,1);
-	//flipBoard.show(1,2);
-	flipBoard.show(2,0);
-	//flipBoard.show(2,1);
-	flipBoard.show(2,2);
-	flipBoard.show(3,3);
-	flipBoard.show(4,1);
-
-	flipBoard.logState();
-	flipBoard.drawBoard();
+		flipBoard.logState();
+		flipBoard.drawBoard();
+	});
 
 } // end initGame()
