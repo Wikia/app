8ae176327e4a2672928d0d9d231c2bb9b4dcbf72
hyun <hyun@wikia-inc.com>
2011-08-03 22:17:54 +0000

    fixed misspelling
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40287 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CorporatePage/modules/templates/CorporateSite_TopHubUsers.php
+++ extensions/wikia/CorporatePage/modules/templates/CorporateSite_TopHubUsers.php
@@ -1,32 +1,32 @@
-	<section id="hub-top-contributors"<?php echo $hidetopeditors ?> class="module">
-		<h1><?php echo wfMsg('hub-topusers-header', $data['title']) ?></h1>
-		<?php
-			if( $data['is_menager'] ) {
-				if( isset($data['var_feeds']['topeditors'] )) {
-		?>
-		<span class="toggleContainer" id="topeditors">[<a class="toggleFeed" href="#"><?php echo wfMsg('hide') ?></a>]</span>
-		<?php
-				} else {
-		?>
-		<span class="toggleContainer" id="topeditors">[<a class="toggleFeed" href="#"><?php echo wfMsg('unhide') ?></a>]</span>
-		<?php
-				}
+<section id="hub-top-contributors"<?php echo $hidetopeditors ?> class="module">
+	<h1>
+		<?php echo wfMsg('hub-topusers-header', $data['title']) ?>
+	</h1>
+	<?php
+		if( $data['is_manager'] ) {
+			if( isset($data['var_feeds']['topeditors'] )) {
+	?>
+	<span class="toggleContainer" id="topeditors">[<a class="toggleFeed" href="#"><?php echo wfMsg('hide') ?></a>]</span>
+	<?php
 			} else {
-
+	?>
+	<span class="toggleContainer" id="topeditors">[<a class="toggleFeed" href="#"><?php echo wfMsg('unhide') ?></a>]</span>
+	<?php
 			}
-		?>
+		}
+	?>
 
-		<p><?php echo wfMsg('hub-contributors-info') ?></p>
+	<p><?php echo wfMsg('hub-contributors-info') ?></p>
 
-		<ul>
-			<?php foreach( $data['topEditors'] as $value ): ?>
-			<li>
-				<?php if (isset($value['avatar'])) echo $value['avatar']; ?>
-				<span class="topuser-info h2"><a href="<?php echo empty($value['userpage']) ? '#':$value['userpage'] ?>"><?php echo $value['username'];	?></a></span>
-				<span class="userEditPoints clearfix"><nobr  class="txt"><?php
-					global $wgLang;
-					echo wfMsgExt('hub-topusers-editpoints', 'parsemag', $wgLang->formatNum( $value['all_count'] ) ) ?></nobr></span>
-			</li>
-			<?php endforeach; ?>
-		</ul>
-	</section><!-- END: #hub-blogs -->
+	<ul>
+		<?php foreach( $data['topEditors'] as $value ): ?>
+		<li>
+			<?php if (isset($value['avatar'])) echo $value['avatar']; ?>
+			<span class="topuser-info h2"><a href="<?php echo empty($value['userpage']) ? '#':$value['userpage'] ?>"><?php echo $value['username'];	?></a></span>
+			<span class="userEditPoints clearfix"><nobr  class="txt"><?php
+				global $wgLang;
+				echo wfMsgExt('hub-topusers-editpoints', 'parsemag', $wgLang->formatNum( $value['all_count'] ) ) ?></nobr></span>
+		</li>
+		<?php endforeach; ?>
+	</ul>
+</section><!-- END: #hub-blogs -->
--- extensions/wikia/CorporatePage/modules/templates/CorporateSite_TopHubWikis.php
+++ extensions/wikia/CorporatePage/modules/templates/CorporateSite_TopHubWikis.php
@@ -2,7 +2,7 @@
 		<h1><?php echo wfMsg('hub-featured', $data['title']) ?></h1>
 		<?php
 			global $wgStylePath;
-			if( $data['is_menager'] ) {
+			if( $data['is_manager'] ) {
 				if( isset($data['var_feeds']['topwikis'] )) {
 		?>
 		<span class="toggleContainer" id="topwikis">[<a class="toggleFeed" href="#"><?php echo wfMsg('hide') ?></a>]</span>
@@ -28,7 +28,7 @@
 					<div class="top-wiki-data">
 						<h2><a href="<?php echo $value['city_url'] ?>" class="top-wiki-link"><?php echo $value['city_title'] ?></a></h2>
 						<p><?php echo $value['city_description'] ?></p>
-						<?php if( $data['is_menager']): ?>
+						<?php if( $data['is_manager']): ?>
 							<?php if(isset($value['hide'])): ?>
 								<a class="wikia-page-link head-hide-link" href="/index.php?action=ajax&rs=AutoHubsPagesHelper::hideFeed&type=city&tag_id=<?php echo $data['tag_id'] ?>&city_id=<?php echo $value['city_id'] ?>&dir=delete"><?php echo wfMsg('hub-show-feed'); ?></a>
 							<?php else: ?>
