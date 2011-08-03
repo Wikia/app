0f3265ad35fc88d0af7ee2681b635578ffd8d473
macbre <macbre@wikia-inc.com>
2011-08-03 10:54:59 +0000

    Selenium: update main reskin test (BugId:9320)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40253 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/EditPageLayoutTest.java
+++ tests/acceptance/com/wikia/selenium/tests/EditPageLayoutTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 public class EditPageLayoutTest extends BaseTest {
 
-	@Test(groups={"broken", "CI"})
+	@Test(groups={"CI", "reskin"})
 	public void testEditPageLayout() throws Exception {
 		// go to random article
 		session().open("index.php?title=Special:Random");
@@ -20,13 +20,13 @@ public class EditPageLayoutTest extends BaseTest {
 		// get title of edit page
 		String title = session().getEval("window.wgPageName");
 
-		assertEquals(session().getEval("window.wgIsEditPage"), "true");
+		assertEquals("true", session().getEval("window.wgIsEditPage"));
 
 		assertTrue(session().isElementPresent("//form[@id='editform']"));
 		assertTrue(session().isElementPresent("//*[@id='cke_wpTextbox1']"));
 
 		assertTrue(session().isElementPresent("//span[contains(@class,'wordmark')]"));
-		assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1[contains(text(), 'Editing')]"));
+		assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h2[contains(text(), 'Editing')]"));
 		//assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1[contains(text(), '" + title + "')]"));
 
 		// spaces
@@ -36,21 +36,21 @@ public class EditPageLayoutTest extends BaseTest {
 		assertTrue(session().isElementPresent("//nav[@id='EditPageTabs']"));
 
 		// toolbars
-		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_toolbar_source')]"));
-		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_toolbar_format')]"));
-		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_toolbar_format_expanded')]"));
-		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_toolbar_expand')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/*[contains(@class, 'cke_toolbar_source')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/*[contains(@class, 'cke_toolbar_format')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/*[contains(@class, 'cke_toolbar_format_expanded')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/*[contains(@class, 'cke_toolbar_expand')]"));
 
 		// buttons
-		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/div[contains(@class, 'cke_buttons')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']/*[contains(@class, 'cke_buttons')]"));
 		assertTrue(session().isElementPresent("//div[@id='EditPageToolbar']//span[contains(@class, 'cke_button_bold')]"));
 
 		// right rail modules
-		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_page_controls')]"));
-		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_insert')]"));
-		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_categories')]"));
-		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_templates')]"));
-		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_license')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']//div[contains(@class, 'module_page_controls')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']//div[contains(@class, 'module_insert')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']//div[contains(@class, 'module_categories')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']//div[contains(@class, 'module_templates')]"));
+		assertTrue(session().isElementPresent("//div[@id='EditPageRail']//div[contains(@class, 'module_license')]"));
 
 		// page controls
 		assertTrue(session().isElementPresent("//div[@id='EditPageRail']/div[contains(@class, 'module_page_controls')]//textarea[@id='wpSummary']"));
