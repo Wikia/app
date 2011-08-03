ed2c8730d35459e655a7b2b7371a0226182ef41b
marcin <marcin@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-03 14:36:32 +0000

    Merge in the communitys LinkSuggest which uses jQuery instead of YUI (bugid:9430)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40272 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RTE/js/plugins/linksuggest/plugin.js
+++ extensions/wikia/RTE/js/plugins/linksuggest/plugin.js
@@ -1,25 +1,45 @@
 CKEDITOR.plugins.add('rte-linksuggest',
 {
+	dataSource: false,
 	init: function(editor) {
+		this.initWikiaLinkSuggest(editor);
+		this.initMediaWikiLinkSuggest(editor);
+	},
+
+	initWikiaLinkSuggest: function(editor) {
+		// check whether LinkSuggest extension is initialized
+		if (typeof window.LS_PrepareTextarea != 'function') {
+			return;
+		}
+
+		// setup data source for link suggest
+		this.dataSource = new window.YAHOO.widget.DS_XHR(window.wgServer + window.wgScriptPath, ["\n"]);
+		this.dataSource.responseType = window.YAHOO.widget.DS_XHR.TYPE_FLAT;
+		this.dataSource.scriptQueryAppend = 'action=ajax&rs=getLinkSuggest';
+
 		// setup events
 		var self = this;
-		editor.on('instanceReady', function(ev) {
-			self.setupLinkSuggest();
-		});
+
 		editor.on('mode', function(ev) {
-			self.setupLinkSuggest();
+			// only run init when in source mode
+			if (editor.mode != 'source') {
+				return;
+			}
+			// get source mode textarea
+			var textarea = $(editor.textarea.$);
+			textarea.attr('id', 'RTEtextarea');
+			window.LS_PrepareTextarea('RTEtextarea', self.dataSource);
 		});
 	},
 
-	// get source mode textarea and setup link suggest
-	setupLinkSuggest: function() {
-		// only run init when in source mode
-		if (RTE.instance.mode != 'source') {
-			return;
+	initMediaWikiLinkSuggest: function(editor) {
+		if (typeof $.fn.linksuggest == 'function') {
+			editor.on('mode', function(ev) {
+				var textarea = $(editor.textarea.$);
+				textarea.attr('id', 'RTEtextarea');
+					$(textarea).css( 'font-family', 'monospace' ).linksuggest();
+			});
 		}
-		// get source mode textarea
-		var textarea = $(RTE.instance.textarea.$);
-		textarea.attr('id', 'RTEtextarea');
-		$(RTE.instance.textarea.$).css( 'font-family', 'monospace' ).linksuggest();
 	}
+
 });
