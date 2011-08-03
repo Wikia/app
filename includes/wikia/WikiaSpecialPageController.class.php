d43b617ecb1852dc6a86298faac3d7c27c215c7f
owen <owen@wikia-inc.com>
2011-08-03 22:58:41 +0000

    trunk: fixed SpecialPageController getter/setter
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40291 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/WikiaSpecialPageController.class.php
+++ includes/wikia/WikiaSpecialPageController.class.php
@@ -20,7 +20,8 @@ class WikiaSpecialPageController extends WikiaController {
 		$this->app = F::app();
 		$out = $this->app->wg->Out;
 		$response = $this->sendRequest( get_class( $this ), null, array( 'par' => $par /* to be compatibile with MW core */ ), false );
-
+		$this->response = $response;
+		
 		if( $response->getFormat() == WikiaResponse::FORMAT_HTML ) {
 			try {
 				$out->addHTML( $response->toString() );
@@ -42,22 +43,25 @@ class WikiaSpecialPageController extends WikiaController {
 		return call_user_func_array( array( $this->specialPage, $method ), $args );
 	}
 
-	/* FIXME: I don't think is really used, remove it?
-	//  __get and __set are more useful pointing to the request/response object
+	// Override WikiaBaseController magic getter and setter because this is a wrapper for a SpecialPage object
+	// Check special page properties first, then Controller properties, then request properties
 	public function __get( $propertyName ) {
-		if (property_exists($this->specialPage->$propertyName))
+		if (property_exists($this->specialPage, $propertyName))
 			return $this->specialPage->$propertyName;
+		else if (property_exists($this, $propertyName))
+			return $this->$propertyName;
 		else 
-			return $this->request->getVal($propertyName);
-	} 
+			return $this->response->getVal($propertyName);
+	}
 
 	// Allow magic setting of template variables so we don't have to do $this->response->setVal
+	// Check special page properties first, then Controller properties, then request properties
 	public function __set($propertyName, $value) {
-		if (property_exists($this->specialPage->$propertyName)) {
+		if (property_exists($this->specialPage, $propertyName))
 			$this->specialPage->$propertyName = $value;
-		} else {
+		else if (property_exists($this, $propertyName))
+			$this->$propertyName = $value;
+		else 
 			$this->response->setVal( $propertyName, $value );
-		}
 	}
-	 */	
 }
