7efaccaa50828b4c160cbf5aafd642c2138e3aad
sean <sean@wikia-inc.com>
2011-08-04 21:33:52 +0000

    FoggyFoto: since canvas was slow on actual mobile devices, trying a version using normal HTML/CSS with CSS3 transitions for animation.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40402 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/FoggyFoto/FoggyFotoController.class.php
+++ extensions/wikia/FoggyFoto/FoggyFotoController.class.php
@@ -36,6 +36,32 @@ class FoggyFotoController extends WikiaController {
 		$this->gameJs = $wgExtensionsPath."/wikia/FoggyFoto/js/FoggyFoto.js?$wgStyleVersion";
 
 		wfProfileOut( __METHOD__ );
-	} // end execute()
+	} // end getMain()
+	
+	/**
+	 * Since the canvas approach was slow on mobile devices, this method was
+	 * created to return a quick experimental version which uses CSS3 masking
+	 * instead of canvas.
+	 */
+	public function getMasked(){
+		global $wgOut, $wgExtensionsPath, $wgStyleVersion;
+		wfProfileIn( __METHOD__ );
+
+		wfLoadExtensionMessages( 'FoggyFoto' );
+
+		$this->boardWidth = $this->getVal('width', 480);
+		$this->boardHeight = $this->getVal('height', 320);
+		$this->numRows = 4;
+		$this->numCols = 6;
+		$this->tileWidth = ($this->boardWidth / $this->numCols);
+		$this->tileHeight = ($this->boardHeight / $this->numRows);
+
+		$this->frontImageSrc = 'http://sean.wikia-dev.com/extensions/wikia/FoggyFoto/front.png'; // this shows up immediately
+		$this->backImageSrc = 'http://images1.wikia.nocookie.net/__cb20100113214904/glee/images/3/3f/Kurtmercedes.jpg'; // this is the one that's obscured
+
+		$this->gameJs = $wgExtensionsPath."/wikia/FoggyFoto/js/FoggyFotoByDivs.js?$wgStyleVersion";
+
+		wfProfileOut( __METHOD__ );
+	} // end getMasked()
 
 } // end class SpecialFoggyFoto
