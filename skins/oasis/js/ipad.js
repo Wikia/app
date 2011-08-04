fc33dcd7bb1891aa6f5b1b56c0178157761a2a71
jolek <jolek@wikia-inc.com>
2011-08-04 18:23:58 +0000

    Oasis for iPad: further developmnents (BugId:9514)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40391 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/ipad.js
+++ skins/oasis/js/ipad.js
@@ -1,31 +1,9 @@
 iPadImprovements = {
 	
-	WikiaMainContentWidth: 800,
-	
-	handleZoom: function() {
-		if( Orientation.getOrientation() == "portrait" ) {
-			$("meta[name=viewport]").attr('content', 'width=device-width');
-			WikiaMainContentWidth = $( '#WikiaMainContent' ).width();
-			$( '#WikiaMainContent' ).width( Orientation.getWidth() - 50 );
-			
-			$( '#WikiaRail' ).hide();
-		} else {
-			$("meta[name=viewport]").attr('content', 'width=device-width');
-			$( '#WikiaMainContent' ).width( WikiaMainContentWidth );
-			$( '#WikiaRail' ).show();
-		}
-	},
-	
 	init: function() {
+		$("meta[name=viewport]").attr('content', 'width=1200, maximum-scale=1.0');
 
-		iPadImprovements.handleZoom();
-		Orientation.bindEventListener( iPadImprovements.handleZoom );
-	
-		//handle  scroll to #WikiaMainContent
-		wikiaMainContentOffset = $('#WikiaMainContent' ).offset();
-		window.scrollTo( wikiaMainContentOffset.left , wikiaMainContentOffset.top );
-	
-		//global nav fix first click opens nav second goes to hub
+		//global nav fix: first click opens nav, second redirects to a hub
 		$( "#WikiaHeader #GlobalNavigation > li > a" ).live( 'click', function( event ) {
 			if ( !$( this ).next().hasClass( 'show' ) ) {
 				event.preventDefault();
@@ -33,14 +11,15 @@ iPadImprovements = {
 			}
 		});
 		
+		//wikia nav fix: first click opens nav, second redirects to a desired page
 		$('#WikiHeader nav > ul > li > a').live('click', function() {
 			if ( $( this ).next( '.subnav' ).css( 'display' ) != 'block' ) {
 				event.preventDefault();
 				$( this ).next().css( 'display', 'block' );
 			}
 		});
-		}
 	}
+	
 }
 
 $(document).ready(function() {
