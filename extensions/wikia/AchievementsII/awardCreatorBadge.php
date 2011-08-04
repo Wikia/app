c786c26e0c80b561f3a108ed1ff22aad1bac09c3
uberfuzzy <uberfuzzy@wikia-inc.com>
2011-08-04 16:54:18 +0000

    fix syntax error reported in unit test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40377 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AchievementsII/awardCreatorBadge.php
+++ extensions/wikia/AchievementsII/awardCreatorBadge.php
@@ -10,19 +10,19 @@ $user = User::newFromName( $username );
 
 if ( is_null( $user ) ) {
 	echo "Not a valid user. Aborting.\n";
-	exit 1;
+	exit(1);
 }
 
 $user->load();
 
 if ( $user->getId() == 0 ) {
 	echo "User does not exist. Aborting.\n";
-	exit 1;
+	exit(1);
 }
 
 if ( !class_exists( 'AchAwardingService' ) ) {
 	echo "Achievements not enabled. Aborting.\n";
-	exit 1;
+	exit(1);
 }
 
 $achService = new AchAwardingService();
