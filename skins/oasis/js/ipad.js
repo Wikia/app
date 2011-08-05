d46489d23597f9d679d538b8a0a676341a3951c3
jolek <jolek@wikia-inc.com>
2011-08-05 11:26:03 +0000

    Oasis for iPad:  onload page zoom to content on portrait orientation (BugId:9515)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40428 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/ipad.js
+++ skins/oasis/js/ipad.js
@@ -1,8 +1,24 @@
+$(document).ready(function() {
+	iPadImprovements.init();
+});
+
 iPadImprovements = {
+	handleOrientation: function() {
+		if ( Orientation.getOrientation() == 'portrait' ) {
+			$("meta[name=viewport]").attr( 'content', 'width=1024, initial-scale=1.09, minimum-scale=0.75, maximum-scale=3.0' );
+		} else {
+			$("meta[name=viewport]").attr( 'content', 'width=1024, initial-scale=1.0, minimum-scale=1.0, maximum-scale=3.0' );
+		}
+	},
 	
 	init: function() {
-		$("meta[name=viewport]").attr('content', 'width=1200, maximum-scale=1.0');
-
+		//onload add proper viewport
+		iPadImprovements.handleOrientation();
+		
+		//onload scroll to main content
+		wikiaMainContent = $('#WikiaMainContent').offset();
+		window.scrollTo( 0, wikiaMainContent.top );
+		
 		//global nav fix: first click opens nav, second redirects to a hub
 		$( "#WikiaHeader #GlobalNavigation > li > a" ).live( 'click', function( event ) {
 			if ( !$( this ).next().hasClass( 'show' ) ) {
@@ -13,15 +29,14 @@ iPadImprovements = {
 		
 		//wikia nav fix: first click opens nav, second redirects to a desired page
 		$('#WikiHeader nav > ul > li > a').live('click', function() {
-			if ( $( this ).next( '.subnav' ).css( 'display' ) != 'block' ) {
-				event.preventDefault();
-				$( this ).next().css( 'display', 'block' );
+			subnav = $( this ).next( 'ul.subnav' );
+			if ( $( subnav ).length ) {
+				if ( $( subnav ).css( 'display' ) != 'block' ) {
+					event.preventDefault();
+					$( subnav ).css( 'display', 'block' );
+				}
 			}
 		});
 	}
 	
 }
-
-$(document).ready(function() {
-	iPadImprovements.init();
-});
