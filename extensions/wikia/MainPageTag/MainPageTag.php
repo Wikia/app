76346fee4c43b940755f2dfda481828959192fde
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-05 23:51:10 +0000

    MainPageTag: Remove inline styles from parser output (BugzId:9321)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40459 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MainPageTag/MainPageTag.php
+++ extensions/wikia/MainPageTag/MainPageTag.php
@@ -40,7 +40,7 @@ function wfMainPageTag_rcs( $input, $args, $parser ) {
 	if(!$wfMainPageTag_lcs_called) {
 		$wfMainPageTag_rcs_called = true;
 	}
-	$html = '<div class="main-page-tag-rcs" style="position: relative; width: 300px; float: right; clear: right;"><div>';
+	$html = '<div class="main-page-tag-rcs"><div>';
 	return $html;
 }
 
@@ -55,19 +55,18 @@ function wfMainPageTag_lcs( $input, $args, $parser ) {
 	global $wfMainPageTag_rcs_called, $wfMainPageTag_lcs_called;
 	$wfMainPageTag_lcs_called = true;
 
-	if(!isset($args['gutter'])) {
+	if ( !isset( $args['gutter'] ) ) {
 		$args['gutter'] = '10';
 	}
 
 	$args['gutter'] = str_replace('px', '', $args['gutter']);
-	
-	$tagClass = 'class="main-page-tag-lcs"';
-	
-	if($wfMainPageTag_rcs_called) {
-		$html = '<div ' . $tagClass . ' style="overflow: hidden; height: 1%; padding-right: '. $args['gutter'] .'px"><div>';
+	$html = '<div class="main-page-tag-lcs ';
+
+	if ( $wfMainPageTag_rcs_called ) {
+		$html .= 'main-page-tag-lcs-collapsed" style="padding-right: '. $args['gutter'] .'px"><div>';
 	} else {
 		$gutter = 300 + $args['gutter'];
-		$html = '<div ' . $tagClass . ' style="float: left; margin-right: -'. $gutter .'px; width: 100%; position: relative;"><div style="margin-right: '. $gutter .'px;">';
+		$html .= 'main-page-tag-lcs-exploded" style="margin-right: -'. $gutter .'px; "><div style="margin-right: '. $gutter .'px;">';
 	}
 	return $html;
 }
