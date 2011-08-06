3861c6e2f5330c4438c89e4c31e6e8e2814d0036
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-06 00:22:36 +0000

    SearchDigest almost done
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40462 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchDigest/SearchDigest.class.php
+++ extensions/wikia/SearchDigest/SearchDigest.class.php
@@ -1,22 +1,83 @@
 <?php
 
-class SpecialSearchDigest extends IndexPager {
+class SpecialSearchDigest extends SpecialPage {
+	const MODE_CSV = 'csv';
+
 	function __construct() {
-		global $wgStatsDB;
+		parent::__construct( 'SearchDigest' );	
+	}
+
+	function execute( $mode ) {
+		global $wgOut;
+
+		$wgOut->setPageTitle( wfMsg( 'searchdigest-title' ) );	
+
+		switch ( $mode ) {
+			case self::MODE_CSV:
+				$pager = new SearchDigestCSVPager();
+
+				// display all results
+				$pager->setLimit( $pager->getNumRows() + 1 );
+
+				$csv = $pager->getBody();
+
+				$wgOut->setArticleBodyOnly( true );
+
+				header("Content-Description: File Transfer");
+
+				//Use the switch-generated Content-Type
+				header("Content-Type: text/plain");
+				header("Content-Disposition: attachment; filename=\"search-digest.csv\"");
+
+				//Force the download
+				header("Content-Transfer-Encoding: utf-8");
+				header("Content-Length: ".strlen($csv));
+
+				$wgOut->addHTML( $csv );
+
+				break;
+			default:
+				$pager = new SearchDigestPager();
+
+				$downloadUrl = Title::newFromText( $this->mName . '/' . self::MODE_CSV, NS_SPECIAL )->getLocalUrl();
+				$wgOut->addHTML( Xml::element( 
+						'a',
+						array(
+							'href' => $downloadUrl,
+							'class' => 'wikia-button'
+						),
+						wfMsg( 'searchdigest-download-csv' )
+					)
+				);
+
+				$html = $pager->getNavigationBar() . $pager->getBody() . $pager->getNavigationBar();
+
+				$wgOut->addHTML( $html );
+		}
+	}
+}
+
+class SearchDigestPager extends ReverseChronologicalPager {
+	function __construct() {
+		global $wgStatsDB, $wgDevelEnvironment;
 
 		parent::__construct( 'SearchDigest' );
-		$this->mDb = wfGetDB( DB_SLAVE, array(), $wgStatsDB );
 
-		$this->mDB->selectDB( 'specials' );
+		if ( $wgDevelEnvironment ) {
+			$this->mDb = wfGetDB( DB_SLAVE );
+		} else {
+			$this->mDb = wfGetDB( DB_SLAVE, array(), $wgStatsDB );
+			$this->mDb->selectDB( 'specials' );
+		}
 
 		// create a Linker object so we don't have to create it every time in formatRow
 		$this->linker = new Linker;
 	}
 
 	function formatRow( $row ) {
-		$link = $this->linker->makeLinkObj( Title::newFromName( $row->sd_query ) );
+		$link = $this->linker->makeLinkObj( Title::newFromText( $row->sd_query ) );
 
-		return $link . ' (' . $row->sd_misses . ')';
+		return Xml::tags( 'li', null, $link . ' (' . $row->sd_misses . ')' );
 	}
 
 	function getQueryInfo() {
@@ -26,10 +87,29 @@ class SpecialSearchDigest extends IndexPager {
 			'tables' => array( 'searchdigest' ),
 			'fields' => array( 'sd_query', 'sd_misses' ),
 			'conds' => array( 'sd_wiki' => $wgCityId ),
+			'options' => array( 'ORDER_BY sd_misses DESC' ),
 		);
 	}
 
 	function getIndexField() {
 		return 'sd_misses';
+	}
+
+	function getStartBody() {
+		return '<ul>';
+	}
+
+	function getEndBody() {
+		return '</ul>';
+	}	
+}
+
+class SearchDigestCSVPager extends SearchDigestPager {
+	function getStartBody() { return ''; }
+
+	function getEndBody() { return ''; }
+
+	function formatRow( $row ) {
+		return $row->sd_query . "," . $row->sd_misses . "\n";
 	}
 }
---
+++ extensions/wikia/SearchDigest/SearchDigest.i18n.php
@@ -0,0 +1,8 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'searchdigest-title' => 'Search Digest',
+	'searchdigest-download-csv' => 'Download as CSV',
+);
