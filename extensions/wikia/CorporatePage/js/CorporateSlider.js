894d6f61428e2a267b8878a8ec71928104f80b37
macbre <macbre@wikia-inc.com>
2011-08-09 15:43:24 +0000

    Slider at wikia.com main page occasionally appears broken (BugId:9600) + cleanup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40575 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CorporatePage/js/CorporateSlider.js
+++ extensions/wikia/CorporatePage/js/CorporateSlider.js
@@ -24,10 +24,6 @@ $(function() {
 	initHideLinks();
 });
 
-$(window).load(function() {
-	spotlightSlider_setup(blockArticle);
-});
-
 function trackTag( str ) {
 	WET.byStr( 'hub/' + str );
 };
@@ -54,9 +50,9 @@ function trackContainer ( ev ) {
 		}
 	}
 
-		if( obj.parent().parent().hasClass( 'blog-jump' )  ) {
-			trackTag( 'blog/comments' );
-		}
+	if( obj.parent().parent().hasClass( 'blog-jump' )  ) {
+		trackTag( 'blog/comments' );
+	}
 }
 
 function makeWikiaButtons() {
@@ -117,63 +113,6 @@ function autoHubToggle(e) {
 	);
 }
 
-function spotlightSlider_setup() {
-	//timer for automatic spotlight slideshow
-	var spotlightSlider_timer;
-	//random integer, 0-3
-	var random = 0; //Math.floor(Math.random() * 4);
-
-	var size = 620;
-	if (wgIsMainpage == true) {
-		size = 943;
-	}
-
-	//move spotlights
-	$(".spotlight-slider-image").each(function() {
-		$(this).css("left", parseInt($(this).css("left")) - (size * random));
-	});
-
-	//select nav
-	$("#spotlight-slider-" + random).find(".nav").addClass("selected");
-
-	//show description
-	$("#spotlight-slider-" + random).find(".description").show();
-
-	//bind events
-	$("#spotlight-slider .nav").click(function() {
-		if($("#spotlight-slider .spotlight-slider-image").queue().length == 0) {
-			clearInterval(spotlightSlider_timer);
-			spotlightSlider_scroll($(this));
-		}
-	});
-
-	spotlightSlider_timer = setInterval(spotlightSlider_slideshow, 7000);
-}
-
-function spotlightSlider_slideshow() {
-	var current = $("#spotlight-slider .selected").parent().prevAll().length;
-	var next = (current == $("#spotlight-slider .nav").length - 1) ? 0 : current + 1;
-	spotlightSlider_scroll($("#spotlight-slider-" + next).find(".nav"));
-}
-
-function spotlightSlider_scroll(nav) {
-	//setup variables
-	var thumb_index = nav.parent().prevAll().length;
-	var scroll_by = parseInt(nav.parent().find(".spotlight-slider-image").css("left"));
-	//set "selected" class
-	$("#spotlight-slider .nav").removeClass("selected");
-	nav.addClass("selected");
-	//hide description
-	$("#spotlight-slider .description").clearQueue().hide();
-	//scroll
-
-	$("#spotlight-slider .spotlight-slider-image").animate({
-		left: "-=" + scroll_by
-	}, function() {
-		$("#spotlight-slider-" + thumb_index).find(".description").fadeIn();
-	});
-}
-
 function wikiaSearch_setup() {
 	var placeholder = $("#wikia-search-form legend").text();
 	$("#wikia-search").focus(function() {
@@ -198,3 +137,71 @@ function initHideLinks() {
 		return false;
 	});
 }
+
+/**
+ * Main page slider
+ */
+var spotlightSlider_timer,
+	SLIDER_INTERVAL = 7000;
+
+function spotlightSlider_setup() {
+	var firstFrame = $("#spotlight-slider-0");
+
+	//select nav
+	firstFrame.find(".nav").addClass("selected");
+
+	//show description
+	firstFrame.find(".description").show();
+
+	//bind events
+	$("#spotlight-slider .nav").click(function() {
+		if($("#spotlight-slider .spotlight-slider-image").queue().length == 0) {
+			clearTimeout(window.spotlightSlider_timer);
+			spotlightSlider_scroll($(this));
+		}
+	});
+
+	window.spotlightSlider_timer = setTimeout(spotlightSlider_slideshow, SLIDER_INTERVAL);
+}
+
+function spotlightSlider_slideshow() {
+	var current = $("#spotlight-slider .selected").parent().prevAll().length,
+		length = $("#spotlight-slider .nav").length,
+		next = (current+1) % length;
+
+	spotlightSlider_scroll($("#spotlight-slider-" + next).find(".nav"), function() {
+		// queue the next animation frame when the current one is finished
+		window.spotlightSlider_timer = setTimeout(spotlightSlider_slideshow, SLIDER_INTERVAL);
+	});
+}
+
+function spotlightSlider_scroll(nav, callback /* function to fire when animation is done */) {
+	//setup variables
+	var thumb_index = nav.parent().prevAll().length;
+	var scroll_by = parseInt(nav.parent().find(".spotlight-slider-image").css("left"));
+	//set "selected" class
+	$("#spotlight-slider .nav").removeClass("selected");
+	nav.addClass("selected");
+	//hide description
+	$("#spotlight-slider .description").clearQueue().hide();
+
+	//scroll
+	var images = $("#spotlight-slider .spotlight-slider-image"),
+		completeCount = images.length;
+
+	images.animate({
+		left: "-=" + scroll_by
+	},
+	function() {
+		$("#spotlight-slider-" + thumb_index).find(".description").fadeIn();
+
+		// call the callback only once (when all animation is done, not per node)
+		if(--completeCount == 0) {
+			if (typeof callback == 'function') {
+				callback();
+			}
+		}
+	});
+}
+
+$(spotlightSlider_setup);
