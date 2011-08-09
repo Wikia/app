4eb9280f869c79615899f0b18450ff6efc4babcd
jolek <jolek@wikia-inc.com>
2011-08-09 12:29:04 +0000

    orientation.js plugin moved
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40563 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/isIPad.js
+++ skins/oasis/js/isIPad.js
@@ -1,3 +1,3 @@
 if( navigator.platform.indexOf("iPad") != -1 ) {
-	$.getResources( [ $.getSassCommonURL( '/skins/oasis/css/ipad.scss' ), '/skins/common/generic/orientation.js' ,'/skins/oasis/js/ipad.js' ] );
+	$.getResources( [ $.getSassCommonURL( '/skins/oasis/css/ipad.scss' ), '/skins/common/zepto/orientation.js' ,'/skins/oasis/js/ipad.js' ] );
 }
