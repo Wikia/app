4a93cb1a851fb0d1ed1a8938ead5c84d872f1d73
wojtek <wojtek@wikia-inc.com>
2011-08-10 18:03:13 +0000

    seleniu 2 tests verification
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40666 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/SharedHelpImagesTest.java
+++ tests/acceptance/com/wikia/selenium/tests/SharedHelpImagesTest.java
@@ -20,17 +20,15 @@ import static org.testng.AssertJUnit.assertTrue;
 
 public class SharedHelpImagesTest extends BaseTest {
 
-	@Test(groups={"CI"})
+	@Test(groups={"CI", "verified"})
 	public void testSharedHelpImages() throws Exception {
 
 		try {
 			// this page can return 404 if there's no local content
-			session().open("index.php?title=Help:YouTube_extension");
+			openAndWait("index.php?title=Help:YouTube_extension");
 		} catch(SeleniumException se) {
 			assertTrue(se.getMessage().contains("Response_Code = 404"));
 		};
-
-		session().waitForPageToLoad(this.getTimeout());
 
 		// basic check to see if shared help is displayed
 		assertTrue( session().isElementPresent("//div[@class='sharedHelp']") );
