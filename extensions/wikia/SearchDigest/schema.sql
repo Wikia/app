3ec7ca2236d5cf8001e1c18d446aad93fb4260b9
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-11 00:33:33 +0000

    missing quote
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40685 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchDigest/schema.sql
+++ extensions/wikia/SearchDigest/schema.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS `searchdigest` (
 	`sd_misses` int(9) unsigned NOT NULL,
 	`sd_lastseen` DATE,
 	PRIMARY KEY `sd_wikiquery` (`sd_wiki`, `sd_query`),
-	KEY `sd_wikimisses` (`sd_wiki`, `sd_misses)
+	KEY `sd_wikimisses` (`sd_wiki`, `sd_misses`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
