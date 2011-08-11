4991b184ffc922a4c410ef23e255ee827e7d2903
jakub <jakub@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-11 13:20:16 +0000

    BugId:8093
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40722 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ScavengerHunt/tests/ScavengerHuntGetFoundIndexesTest.php
@@ -0,0 +1,85 @@
+<?php
+require_once dirname(__FILE__) . '/../ScavengerHunt_setup.php';
+require_once dirname(__FILE__) . '/../tests/ScavengerHuntTest.php';
+wfLoadAllExtensions();
+
+class ScavengerHuntGetFoundIndexesTest extends ScavengerHuntTest {
+
+	public function conditions(){
+
+		return array(
+			array( false, array( 0 => self::WRONG_ARTICLE_NAME ), array( self::MOCK_TEXT ), array() ),
+			array( false, array( 0 => self::MOCK_TEXT ), array( self::MOCK_TEXT ), array() ),
+			array( true, array( 0 => self::MOCK_TEXT, 1 => self::WRONG_ARTICLE_NAME ), array( self::MOCK_TEXT ), array( 0 ) ),
+			array( true, array( 0 => self::MOCK_TEXT, 1 => self::WRONG_ARTICLE_NAME ), array( self::WRONG_ARTICLE_NAME ), array( 1 ) ),
+			array( true, array( 0 => self::MOCK_TEXT, 1 => self::WRONG_ARTICLE_NAME ), array( self::MOCK_TEXT, self::WRONG_ARTICLE_NAME ), array( 0, 1 ) ),
+			array( true, array( 0 => self::MOCK_TEXT ), array( self::WRONG_ARTICLE_NAME ), array() ),
+			array( true, array( 0 => self::MOCK_TEXT ), array(), array() ),
+			array( true, array(), array( self::MOCK_TEXT ), array() ),
+			array( true, array(), array(), array() )
+		);
+	}
+	
+	/**
+	 * @dataProvider conditions
+	 */
+
+	public function testGetFoundIndexes( $gameExists, $articlesList, $foundList, $expectedResult ) {
+		
+
+		// mocking game object
+		$articlesIdentifiers = array();
+		foreach( $articlesList as $art ) {
+			$articlesIdentifiers[] = ScavengerHuntGame::makeIdentifier( self::LANDING_WIKI_ID, $art );
+		}
+
+		$mockedGame = $this->getMock( 'ScavengerHuntGame', array('getArticleIdentifier'), array( $this->app ) );
+		$mockedGame
+			->expects( $this->any() )
+			->method( 'getArticleIdentifier' )
+			->will(  $this->returnValue( $articlesIdentifiers ) );
+
+		$this->mockClass( 'ScavengerHuntGame', $mockedGame );
+
+		$game = $this->getFakeGame();
+
+		// mocking scavenger hunt and passing game to the hunt
+		$scavengerHunt = $this->getMock( 'ScavengerHunt', array( 'getDataFromCache' ) );
+		$scavengerHunt->setGame( $gameExists ? $game : null );
+
+		$cachedIdentifiers = array();
+		foreach( $foundList as $art ) {
+			$cachedIdentifiers[] = $game->makeIdentifier( self::LANDING_WIKI_ID, $art );
+		}
+
+		$scavengerHunt
+			->expects( $this->any() )
+			->method( 'getDataFromCache' )
+			->will(  
+				$this->returnValue(
+					array(
+						'gameId' => $scavengerHunt->getHuntId(),
+						ScavengerHunt::VISITED_ART_KEY => $cachedIdentifiers
+					)
+				)
+			);
+
+		// mocking user
+		$mockedUser = $this->getMock( 'User', array('isAnon') );
+		$mockedUser
+			->expects( $this->any() )
+			->method( 'isAnon' )
+			->will(  $this->returnValue( self::MOCK_USER_NAME ) );
+
+		$this->mockClass( 'User', $mockedUser );
+
+		// setting cookie values
+		$_COOKIE[ $scavengerHunt->getHuntIdKey() ] = ( $gameExists ) ? self::MOCK_GAME_ID : 0;
+
+		// test and assertions
+		$result = $scavengerHunt->getFoundIndexes();
+		$scavengerHunt->resetCache();
+		
+		$this->assertEquals( $expectedResult, $result );
+	}
+}
