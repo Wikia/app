920b626bbad4a99cb3e7cc03851841616d1ee182
macbre <macbre@wikia-inc.com>
2011-08-11 11:51:44 +0000

    Visual editor stats: clicks per session (BugId:2947)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40708 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditPageReskin/RTE/js/plugins/track/plugin.js
+++ extensions/wikia/EditPageReskin/RTE/js/plugins/track/plugin.js
@@ -1,103 +1,71 @@
 CKEDITOR.plugins.add('rte-track',
 {
-	init: function(editor) {
-		var self = this;
+	stats: {},
 
-		// track style applies
-		editor.on('wysiwygModeReady', function() {
-			editor.document.removeListener(self.trackApplyStyle);
-			editor.document.on('applyStyle', self.trackApplyStyle);
-		});
+	// increase stats entry value (default by 1)
+	statsInc: function(key, val) {
+		val = parseInt(val) || 1;
 
-		// track context menu
-		editor.on('contextMenuOnOpen', self.trackContextMenuOpen);
-		editor.on('contextMenuOnClick', self.trackContextMenuItem);
-
-		// track list creation
-		editor.on('listCreated', self.trackListCreated);
-
-		// toolbar tracking
-
-		// tracking for CK toolbar buttons
-		editor.on('buttonClick', function(ev) {
-			var buttonClicked = ev.data.button;
-			//RTE.log(buttonClicked);
-
-			RTE.track('toolbar', buttonClicked.command);
-		});
-
-		// tracking for CK toolbar rich combos (dropdowns)
-		editor.on('panelShow', function(ev) {
-			var me = ev.data.me;
-			//RTE.log(me);
-
-			var id = me.trackingName || me.className.split('_').pop();
-
-			// track combo panel open
-			RTE.track('toolbar', id + 'Menu', 'open');
-		});
-		editor.on('panelClick', function(ev) {
-			var me = ev.data.me;
-			var value = ev.data.value;
-			//RTE.log([me, value]);
-
-			var id = me.trackingName || me.className.split('_').pop();
-
-			// for templates dropdown
-			if (id == 'template') {
-				if (value == '--other--') {
-					value = 'other';
-				}
-				else {
-					var panelItems = me._.items;
-					var idx = 0;
-
-					// iterate thru panel items and find index chosen template
-					for (tpl in panelItems) {
-						idx++;
-						if (tpl == value) {
-							value = idx;
-							break;
-						}
-					}
-				}
-			}
-
-			// track combo panel open
-			RTE.track('toolbar', id + 'Menu', value);
-		});
-
-		// tracking for MW toolbar buttons
-		editor.on('instanceReady', function() {
-			$('#mw-toolbar').bind('click', function(ev) {
-				var target = $(ev.target).filter('img');
-				//RTE.log(target);
-
-				if (!target.exists()) {
-					return;
-				}
-
-				var id = target.attr('id').split('-').pop();
-				RTE.track('source', id);
-			});
-		});
-	},
-
-	trackApplyStyle: function(ev) {
-		var style = ev.data.style;
-		var remove = ev.data.remove;
-
-		// only track when style is applied
-		if (style && !remove) {
-			RTE.track('visualMode', 'applyFormat', style);
+		if (this.stats[key]) {
+			this.stats[key] += val;
+		}
+		else {
+			this.stats[key] = val;
 		}
 	},
 
-	trackContextMenuOpen: function(ev) {
-		RTE.track('visualMode', 'contextMenu', 'open');
+	// get value of given stats entry
+	statsGet: function(key) {
+		return this.stats[key] || 0;
 	},
 
-	trackContextMenuItem: function(ev) {
+	// pass tracking events to RTE.track
+	track: function() {
+		RTE.track.apply(RTE, Array.prototype.slice.call(arguments));
+	},
+
+	init: function(editor) {
+		// track style applies
+		editor.on('wysiwygModeReady', function() {
+			editor.document.removeListener(this.onApplyStyle);
+			editor.document.on('applyStyle', this.onApplyStyle, this);
+		}, this /* scope */);
+
+		// track context menu
+		editor.on('contextMenuOnOpen', this.onContextMenuOpen, this /* scope */);
+		editor.on('contextMenuOnClick', this.onContextMenuItem, this);
+
+		// track list creation
+		editor.on('listCreated', this.onListCreated, this);
+
+		// tracking for CK toolbar buttons
+		editor.on('buttonClick', this.onButtonClick, this);
+
+		// insert media / templates
+		editor.on('afterCommandExec', this.onAfterCommandExec, this);
+
+		// tracking for CK toolbar rich combos (dropdowns)
+		editor.on('panelShow', this.onPanelShow, this);
+		editor.on('panelClick', this.onPanelClick, this);
+
+		editor.on('submit', this.onSubmit, this);
+	},
+
+	onApplyStyle: function(ev) {
+		var style = ev.data.style,
+			remove = ev.data.remove;
+
+		// only track when style is applied
+		if (style && !remove) {
+			this.track('visualMode', 'applyFormat', style);
+		}
+	},
+
+	onContextMenuOpen: function(ev) {
+		this.track('visualMode', 'contextMenu', 'open');
+	},
+
+	onContextMenuItem: function(ev) {
 		var name = ev.data.item.command;
 
 		// group events which name starts with...
@@ -107,18 +75,58 @@ CKEDITOR.plugins.add('rte-track',
 			var group = groups[g];
 
 			if (name.indexOf(group) == 0) {
-				RTE.track('visualMode', 'contextMenu', 'action', group, name);
+				this.track('visualMode', 'contextMenu', 'action', group, name);
 				return;
 			}
 		}
 
-		RTE.track('visualMode', 'contextMenu', 'action', name);
+		this.track('visualMode', 'contextMenu', 'action', name);
 	},
 
-	trackListCreated: function(ev) {
-		var listNode = ev.data.listNode;
-		var listType = (listNode.getName() == 'ul') ? 'unorderedList' : 'orderedList';
+	onListCreated: function(ev) {
+		var listNode = ev.data.listNode,
+			listType = (listNode.getName() == 'ul') ? 'unorderedList' : 'orderedList';
 
-		RTE.track('format', listType);
+		this.track('format', listType);
+	},
+
+	onButtonClick: function(ev) {
+		var buttonClicked = ev.data.button;
+
+		this.track('toolbar', buttonClicked.command);
+		this.statsInc('toolbarButtonsClicked');
+	},
+
+	onAfterCommandExec: function(ev) {
+		var commandName = ev.data.name || '';
+
+		if(commandName.substring(0, 3) == 'add') {
+			this.statsInc('addButtonsClicked');
+		}
+	},
+
+	onPanelShow: function(ev) {
+		var me = ev.data.me,
+			id = me.trackingName || me.className.split('_').pop();
+
+		// track combo panel open
+		this.track('toolbar', id + 'Menu', 'open');
+	},
+
+	onPanelClick: function(ev) {
+		var me = ev.data.me,
+			value = ev.data.value,
+			id = me.trackingName || me.className.split('_').pop();
+
+		this.track('toolbar', id + 'Menu', value);
+	},
+
+	onSubmit: function() {
+		// count number of click on toolbar and "Add media" buttons (BugId:2947)
+		var buttonsClicked = this.statsGet('toolbarButtonsClicked') + this.statsGet('addButtonsClicked');
+
+		if (buttonsClicked > 0) {
+			this.track('buttonsClicked', buttonsClicked);
+		}
 	}
 });
