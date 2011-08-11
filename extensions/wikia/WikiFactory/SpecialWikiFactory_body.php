240b0f394a7ca946d897ff202e7a786fe224497a
uberfuzzy <uberfuzzy@wikia-inc.com>
2011-08-11 22:07:14 +0000

    ugly fix for BugId:9937, making doUpdateHubs clear the key that WikiFactory::getCategory uses (todo: consolidate everyone to use one class for hubs (and make sure it uses memc))
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40764 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFactory/SpecialWikiFactory_body.php
+++ extensions/wikia/WikiFactory/SpecialWikiFactory_body.php
@@ -460,6 +460,12 @@ class WikiFactoryPage extends SpecialPage {
 			$hub = WikiFactoryHub::getInstance();
 			$hub->setCategory( $this->mWiki->city_id, $cat_id, $reason );
 			$categories = $hub->getCategories();
+			
+			// ugly fast fix for fb#9937 (until all the hub management is cleaned up)
+			global $wgMemc;
+			$key = sprintf("%s:%d", 'WikiFactory::getCategory', intval($this->mWiki->city_id));
+			$wgMemc->delete( $key );
+			
 			return Wikia::successbox( "Hub is now set to: ". $categories[ $cat_id ]['name'] );
 		}
 		else {
