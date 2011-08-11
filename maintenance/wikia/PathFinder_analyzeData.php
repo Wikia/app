55c2a9f68f5c050f9f2989b4b0781881d9328ff8
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-11 12:45:27 +0000

    UserPathPrediction: last step of migrating to PathFinder
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40711 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/PathFinder_analyzeData.php
+++ maintenance/wikia/PathFinder_analyzeData.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * This script downloads the data required for UserPathPrediction, parses and analyzes it; the resulting dat is stored in DB in a summary table
+ * This script downloads the data required for PathFinder, parses and analyzes it; the resulting data is stored in DB in a summary table
  *
  * @package MediaWiki
- * @subpackage Maintanance
+ * @subpackage Maintenance
  *
  * @author: Federico 'Lox' Lucignano <federico(at)wikia-inc.com>
  * @author: Jakub Olek <bukaj.kelo(at)gmail.com>
  *
- * @usage: SERVER_ID=177 php UserPathPrediction_analyzeData.php --conf /usr/wikia/conf/current/wiki.factory/LocalSettings.php --aconf /usr/wikia/conf/current/AdminSettings.php
+ * @usage: SERVER_ID=177 php PathFinder_analyzeData.php --conf /usr/wikia/conf/current/wiki.factory/LocalSettings.php --aconf /usr/wikia/conf/current/AdminSettings.php
  */
 
 ini_set( 'include_path', dirname( __FILE__ ) . '/../' );
@@ -27,7 +27,7 @@ global $IP;
 
 if ( isset( $options['help'] ) ) {
 	echo(
-		"Usage: SERVER_ID=177 php UserPathPrediction_initData.php " .
+		"Usage: SERVER_ID=177 php PathFinder_initData.php " .
 		"--conf /usr/wikia/conf/current/wiki.factory/LocalSettings.php " .
 		"--aconf /usr/wikia/conf/current/AdminSettings.php " .
 		"--date=20110623 " .
@@ -40,17 +40,17 @@ if ( isset( $options['help'] ) ) {
 $date = ( !empty( $options['date'] ) ) ? $options['date'] : date( "Ymd", strtotime( "-1 day" ) );//"20110504"
 $s3ConfigFile = ( !empty( $options['s3conf'] ) ) ? $options['s3conf'] : null;
 
-require_once( "$IP/extensions/wikia/hacks/UserPathPrediction/UserPathPrediction.setup.php" );
+require_once( "$IP/extensions/wikia/hacks/PathFinder/PathFinder.setup.php" );
 
 $app = F::app();
 $wikis;
 
-$app->sendRequest( 'UserPathPredictionLogService', 'log', array( 'msg' => 'Start' ) );
+$app->sendRequest( 'PathFinderLogService', 'log', array( 'msg' => 'Start' ) );
 echo( "Initializing data analysis for User Path Prediction.\n\n" );
 
 try{
 	echo( "Downloading and parsing data from {$date}, this could take a while...\n\n");
-	$app->sendRequest( 'UserPathPredictionService', 'extractOneDotData', array( 'date' => $date, 'backendParams' => array( 's3ConfigFile' => $s3ConfigFile ) ) );
+	$app->sendRequest( 'PathFinderService', 'extractOneDotData', array( 'date' => $date, 'backendParams' => array( 's3ConfigFile' => $s3ConfigFile ) ) );
 	echo( 'Done.');
 } catch (WikiaException $e) {
 	echo $e;
