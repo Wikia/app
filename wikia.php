13a2ceb86c2d82a83f7fd605fcd375b058fe755d
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-12 17:38:24 +0000

    Nirvana: global switch to enable/disavle API (BugzId:9971)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40811 e846bd91-c042-42a7-a218-4b65e2c1904d

--- wikia.php
+++ wikia.php
@@ -2,15 +2,19 @@
 // Initialise common MW code
 require ( dirname( __FILE__ ) . '/includes/WebStart.php' );
 
-$app = F::app();
-
-// initialize skin if requested
-$app->initSkin( (bool) $app->wg->Request->getVal( "skin", false ) );
-
-$response = $app->sendRequest( null, null, null, false );
-
-// commit any open transactions just in case the controller forgot to
-$app->commit();
-
-$response->sendHeaders();
-$response->render();
+if ( !empty( $wgEnableNirvanaAPI ) ){
+	$app = F::app();
+	
+	// initialize skin if requested
+	$app->initSkin( (bool) $app->wg->Request->getVal( "skin", false ) );
+	
+	$response = $app->sendRequest( null, null, null, false );
+	
+	// commit any open transactions just in case the controller forgot to
+	$app->commit();
+	
+	$response->sendHeaders();
+	$response->render();
+} else {
+	header( "HTTP/1.0 503 Service Unavailable", true, 503 );
+}
