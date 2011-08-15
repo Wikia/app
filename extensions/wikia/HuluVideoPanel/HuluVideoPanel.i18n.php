922dcac9be24ab990032acbfac7f174087c90d3e
siebrand <siebrand@wikia-inc.com>
2011-08-15 14:49:42 +0000

    Normalise i18n file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40859 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/HuluVideoPanel/HuluVideoPanel.i18n.php
+++ extensions/wikia/HuluVideoPanel/HuluVideoPanel.i18n.php
@@ -1,13 +1,12 @@
 <?php
 /**
-* Internationalisation file for the HuluVideoPanel extension.
-*
-* @addtogroup Languages
-*/
+ * Internationalisation file for the HuluVideoPanel extension.
+ *
+ * @addtogroup Languages
+ */
 
 $messages = array();
 
 $messages['en'] = array(
 	'huluvideopanel-title' => '{{SITENAME}} Hulu Videos'
 );
-
