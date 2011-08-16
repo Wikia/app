f85e20a0bb665d0fb7e0439bd265894451feb804
liz <liz@wikia-inc.com>
2011-08-16 21:50:09 +0000

    re-adding click tracking
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40915 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserProfilePageV3/js/UserProfilePage.js
+++ extensions/wikia/UserProfilePageV3/js/UserProfilePage.js
@@ -229,11 +229,13 @@ var UserProfilePage = {
 
 		$('.favorite-wikis .delete').live('click', function() {
 			UserProfilePage.hideFavWiki($(this).closest('li').data('wiki-id'));
+			UserProfilePage.track('edit/lightbox/top_wiki_hide');		
 		});
 		
 		modal.find('.favorite-wikis-refresh').click(function(event) {
 			event.preventDefault();
-			UserProfilePage.refreshFavWikis()
+			UserProfilePage.refreshFavWikis();
+			UserProfilePage.track('edit/lightbox/top_wiki_refresh');		
 		});
 		
 		var formFields = modal.find('input[type="text"], select');
@@ -250,6 +252,7 @@ var UserProfilePage = {
 		$('#facebookPage a').click(function(event) {
 			event.preventDefault();
 			window.open($(this).attr('href'));
+			UserProfilePage.track('edit/lightbox/feed_preferences');
 		});
 	},
 	
