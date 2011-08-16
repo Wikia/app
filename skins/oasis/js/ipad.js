e82cdad337835fb735150cdd2557bd65e802174f
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-16 16:28:32 +0000

    Oasis for iPad: fixes for iPad / all but new gallery and slideshow
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40899 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/ipad.js
+++ skins/oasis/js/ipad.js
@@ -6,9 +6,9 @@ iPadImprovements = {
 	
 	setViewPort: function() {
 		if ( Orientation.getOrientation() == 'portrait' ) {
-			$("meta[name=viewport]").attr( 'content', 'width=1024, initial-scale=1.09, maximum-scale=2.5' );
+			$( 'meta[name=viewport]' ).attr( 'content', 'width=1024, initial-scale=1.09' );
 		} else {
-			$("meta[name=viewport]").attr( 'content', 'width=1024, initial-scale=1.0, maximum-scale=2.5' );
+			$( 'meta[name=viewport]' ).attr( 'content', 'width=1024, initial-scale=1.0' );
 		}
 	},
 	
@@ -18,11 +18,11 @@ iPadImprovements = {
 		iPadImprovements.setViewPort();
 		
 		//onload scroll to main content
-		wikiaMainContent = $('#WikiaMainContent').offset();
+		wikiaMainContent = $( '#WikiaMainContent' ).offset();
 		window.scrollTo( 0, wikiaMainContent.top );
 
 		//global nav fix: first click opens nav,  second redirects to a hub
-		$( "#WikiaHeader #GlobalNavigation > li > a" ).live( 'click', function( event ) {
+		$( '#WikiaHeader #GlobalNavigation > li > a' ).live( 'click', function( event ) {
 			if ( !$( this ).next().hasClass( 'show' ) ) {
 				$( this ).next().addClass( 'show' );
 				event.preventDefault();
@@ -30,7 +30,7 @@ iPadImprovements = {
 		});
 
 		//wiki nav fix: first click opens nav, second redirects to a desired page
-		$('#WikiHeader nav > ul > li > a').live('click', function() {
+		$( '#WikiHeader nav > ul > li > a' ).live( 'click', function() {
 			subnav = $( this ).next( 'ul.subnav' );
 			if ( $( subnav ).length ) {
 				if ( $( subnav ).css( 'display' ) != 'block' ) {
