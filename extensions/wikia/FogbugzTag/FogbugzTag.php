96f20868490f563c5fee37e4773f79bcef70762c
piotrp <piotrp@wikia-inc.com>
2011-08-18 11:15:26 +0000

    Updated basic description of extension in setup file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@40988 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/FogbugzTag/FogbugzTag.php
+++ extensions/wikia/FogbugzTag/FogbugzTag.php
@@ -3,37 +3,20 @@ if (!defined('MEDIAWIKI'))
 {
 	exit(1);
 }
-$wgExtensionCredits['specialpage'][] = array(
-	'name' => 'FogbugzTagSupport',
-	'author' => 'Pepe',
-	'url' => 'www.jakasstrona.pl',
-	'description' => "Domyslne info",
-	'descriptionmsg' => 'myextension-desc',
-	'version' => '0.1.0',
+$wgExtensionCredits['parserhook'][] = array(
+	'name' => 'FogbugzTag',
+	'author' => 'Pepe & Rychu',
+	'description' => "FogbugzTag allows to use tags <fogbugz_tck id=\"your_case_id\"></fogbugz_tck> ".
+					 "in the articles on Internal Wiki",
+	'version' => '1.0',
 );
 
 $dir = dirname(__FILE__) . '/';
 
-//$wgAutoloadClasses['SpecialMyExtension'] = $dir . 'SpecialMyExtension.php'; // no special page, no need for that
-
-//$wgAutoloadClasses['EmoticonsParser'] = $dir . 'EmoticonsParser.class.php';
-
 $wgAutoloadClasses['FogbugzTag'] = $dir . 'FogbugzTag.class.php';
 $wgAutoloadClasses['FogbugzContainer'] = $dir . 'FogbugzContainer.class.php';
-//set hooks for emotikons parser:
-//$wgHooks['ParserFirstCallInit'][] = 'EmoticonsParser::onEmoticonsParserInit';
-//$wgHooks['RTEUseDefaultPlaceholder'][] = 'EmoticonsParser::onRTEUseDefPlaceholder';
 
 $wgHooks['ParserFirstCallInit'][] = 'FogbugzTag::onFogbugzTagInit';
-
-//$wgHooks['RTEUseDefaultPlaceholder'][] = 'FogbugzParser::onRTEUseDefPlacehldr';
-//$wgHooks['ParserBeforeInternalParse'][] = 'EmoticonsParser::renderTag';
-
-
-//$wgExtensionMessagesFiles['FogbugzTagSupport'] = $dir . 'FogbugzTagSupport.i18n.php';
-//$wgSpecialPages['FogbugzTagSupport'] = 'SpecialMyExtension';
-
-
 
 $wgAjaxExportList[] = 'FogbugzTag::getFogbugzServiceResponse';
 
