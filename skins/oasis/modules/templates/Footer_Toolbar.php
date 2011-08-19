98b1894fcc503f76bec6931e10140ad8064242fe
christian <christian@wikia-inc.com>
2011-08-19 23:43:51 +0000

    BugId: 10274 Toolbar no long obscured by browser status bar
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41082 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/Footer_Toolbar.php
+++ skins/oasis/modules/templates/Footer_Toolbar.php
@@ -1,6 +1,6 @@
 <?= wfRenderModule( 'Footer', 'Menu', array( 'format' => 'html', 'items' => $toolbar )); ?>
 <li class="menu overflow-menu" style="display:none">
 	<span class="arrow-icon-ctr"><span class="arrow-icon arrow-icon-top"></span><span class="arrow-icon arrow-icon-bottom"></span></span>
-	<a href="#"><?= wfMsg('oasis-toolbar-more') ?></a>
+	<span href="#"><?= wfMsg('oasis-toolbar-more') ?></span>
 	<ul class="tools-menu"></ul>
 </li>
