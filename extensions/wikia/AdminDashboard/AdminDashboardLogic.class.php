719f6629f963bb7bb5e1571c0df6de581e68d48e
jakub <jakub@wikia-inc.com>
2011-08-19 09:47:34 +0000

    Adding SponsorshipDashboard and Crunchyroll to blacklist
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41049 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdminDashboard/AdminDashboardLogic.class.php
+++ extensions/wikia/AdminDashboard/AdminDashboardLogic.class.php
@@ -5,7 +5,7 @@
  */
 
 class AdminDashboardLogic {
-	
+
 	public static function isGeneralApp($appName) {
 		$generalApps = array(
 			'Categories' => true,
@@ -29,7 +29,7 @@ class AdminDashboardLogic {
 	/**
 	 * @brief Helper function which determines whether to display the Admin Dashboard Chrome in the Oasis Skin
 	 * @param type $title Title of page we are on
-	 * @return type boolean 
+	 * @return type boolean
 	 */
 	public static function displayAdminDashboard($app, $title) {
 		// Admin Dashboard is only for logged in plus a list of groups
@@ -37,7 +37,7 @@ class AdminDashboardLogic {
 		if (!$app->wg->User->isAllowed( 'admindashboard' )) return false;
 		if ($title && $title->isSpecialPage()) {
 			$exclusionList = array(
-				"Connect", 
+				"Connect",
 				"Contact",
 				"Contributions",
 				"CreateBlogPage",
@@ -80,10 +80,12 @@ class AdminDashboardLogic {
 				"WikiaLabs",
 				"WikiStats",
 				"AdSS",
+				"SponsorshipDashboard",
+				"Crunchyroll"
 			);
 			return (!in_array($title->getDBKey(), $exclusionList));
 		}
 		return false;
 	}
-	
+
 }
