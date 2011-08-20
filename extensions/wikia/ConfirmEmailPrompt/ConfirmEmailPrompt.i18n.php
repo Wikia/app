44d3ce887d5fcc6b497f03147eb37c6c67912a51
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-20 00:36:35 +0000

    extension written at Community All Hands 2012 brainstorm
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41084 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ConfirmEmailPrompt/ConfirmEmailPrompt.i18n.php
@@ -0,0 +1,8 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'confirmemailprompt-desc' => 'Displays a warning/error message if user has unconfirmed e-mail.',
+	'confirmemailprompt-error' => 'Is this your e-mail: <strong>$1</strong>? [[$2|Confirm]] or [[$3|change it]].',
+);
