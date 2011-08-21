72c33937aaca80739b5b26c7e1404bbfa4b557b5
siebrand <siebrand@wikia-inc.com>
2011-08-21 11:41:09 +0000

    Update messages and add message documentation.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41091 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/FoggyFoto/FoggyFoto.i18n.php
+++ extensions/wikia/FoggyFoto/FoggyFoto.i18n.php
@@ -9,9 +9,9 @@ $messages = array();
 
 $messages['en'] = array(
 	'foggyfoto' => 'Foggy Foto game',
-	'foggyfoto-desc' => 'This extension creates a page where the Foggy Foto game can be played in HTML5 + Canvas. It will be accessible via Nirvana\s APIs, not as a normal page.',
-	'foggyfoto-score' => 'SCORE: <span>$1</span>',
-	'foggyfoto-progress' => 'PHOTOS: <span>$1</span>',
+	'foggyfoto-desc' => 'Creates a page where the Foggy Foto game can be played in HTML5 + Canvas. It will be accessible via Nirvana\'s APIs',
+	'foggyfoto-score' => 'Score: <span>$1</span>',
+	'foggyfoto-progress' => 'Photos: <span>$1</span>',
 	'foggyfoto-progress-numbers' => '$1/$2',
 );
 
@@ -19,5 +19,11 @@ $messages['en'] = array(
  *
  */
 $messages['qqq'] = array(
-	'foggyfoto-progress-numbers' => 'This is the format of the numbers that will be substituted into the $1 portion of foggyfoto-progress. The $1 here is what number photo the player is on (starting with 1) and $2 is the total number of photos in a round of the game.',
+	'foggyfoto-progress' => 'Parameters:
+* $1 is replaced with {{msg-wikia|foggyfoto-progress-numbers}}.',
+	'foggyfoto-progress-numbers' => 'This is the format of the numbers that will be substituted into the "$1" portion of {{msg-wikia|foggyfoto-progress}}. Parameters:
+* $1 is what number photo the player is on (starting with 1)
+* $2 is the total number of photos in a round of the game.',
+	'foggyfoto' => 'Special page name for "Foggy Foto" game.',
+	'foggyfoto-desc' => '{{desc}}',
 );
