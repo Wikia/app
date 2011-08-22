5b4de2d75ac67c4d22324857e0ddad4654e508c7
owen <owen@wikia-inc.com>
2011-08-22 21:24:19 +0000

    trunk: WikiFeatures skeleton
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41142 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiFeatures/WikiFeatures.setup.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * WikiFeatures
+ *
+ * @author Hyun Lim, Owen Davis
+ *
+ */
+$dir = dirname(__FILE__) . '/';
+$app = F::app();
+//classes
+$app->registerClass('WikiFeaturesSpecialController', $dir . 'WikiFeaturesSpecialController.class.php');
+
+// i18n mapping
+$wgExtensionMessagesFiles['WikiFeatures'] = $dir . 'WikiFeatures.i18n.php';
+
+// special pages
+$app->registerSpecialPage('WikiFeatures', 'WikiFeaturesSpecialController');
+
+$wgAvailableRights[] = 'wikifeatures';
+
+$wgGroupPermissions['*']['wikifeatures'] = false;
+$wgGroupPermissions['staff']['wikifeatures'] = true;
+$wgGroupPermissions['sysop']['wikifeatures'] = true;
+$wgGroupPermissions['bureaucrat']['wikifeatures'] = true;
+
+$wgRevokePermissions['vstf']['wikifeatures'] = true;
