d041833cc78e6ed1734d21f1b71d6f1129b314a1
wojtek <wojtek@wikia-inc.com>
2011-08-22 14:35:04 +0000

    selenium 2 verification
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41121 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/BlogArticleReskinTest.java
+++ tests/acceptance/com/wikia/selenium/tests/BlogArticleReskinTest.java
@@ -23,7 +23,7 @@ public class BlogArticleReskinTest extends EditPageBaseTest {
 	String postPage;
 	String postTitle;
 
-	@Test(groups={"CI", "envProduction", "reskin"})
+	@Test(groups={"CI", "verified", "envProduction", "reskin"})
 	public void testEnsureLoggedInUserCanCreateBlogPosts() throws Exception {
 		this.postTitle = "BlogPostPrettyLongTestTitle" + Integer.toString(new Random().nextInt(9999));
 		this.postPage = "User_blog:" + getTestConfig().getString("ci.user.wikiabot.username") + "/" + this.postTitle;
@@ -31,33 +31,32 @@ public class BlogArticleReskinTest extends EditPageBaseTest {
 		this.content = "test blog post --~~~~";
 
 		login();
-		session().open("index.php?title=Special:CreateBlogPage");
-		session().waitForPageToLoad(this.getTimeout());
+		openAndWait("index.php?title=Special:CreateBlogPage");
 
 		// test modal with required fields
 		assertTrue(session().isVisible("//section[@id='HiddenFieldsDialog']//input[@name='wpTitle']"));
 
 		// click "Ok" without providing any data - modal should not be hidden
 		session().click("//section[@id='HiddenFieldsDialog']//*[@id='ok']");
-		assertTrue(session().isVisible("//section[@id='HiddenFieldsDialog']"));
+		waitForElementVisible("//section[@id='HiddenFieldsDialog']");
 
 		// provide title
 		session().type("wpTitle", this.postTitle + "foo");
 		session().click("//section[@id='HiddenFieldsDialog']//*[@id='ok']");
-		assertFalse(session().isElementPresent("//section[@id='HiddenFieldsDialog']"));
+		waitForElementNotPresent("//section[@id='HiddenFieldsDialog']");
 
 		// header should be updated with new title
-		assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1/a[contains(@title, '" + this.postTitle + "foo')]"));
+		waitForElement("//header[@id='EditPageHeader']//h1/a[contains(@title, '" + this.postTitle + "foo')]");
 
 		// edit title
 		session().click("//a[@id='EditPageTitle']");
-		assertTrue(session().isVisible("//section[@id='HiddenFieldsDialog']"));
+		waitForElementVisible("//section[@id='HiddenFieldsDialog']");
 		session().type("wpTitle", this.postTitle);
 		session().click("//section[@id='HiddenFieldsDialog']//*[@id='ok']");
-		assertFalse(session().isElementPresent("//section[@id='HiddenFieldsDialog']"));
+		waitForElementNotPresent("//section[@id='HiddenFieldsDialog']");
 
 		// header should be updated with new title
-		assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1/a[contains(@title, '" + this.postTitle + "')]"));
+		waitForElement("//header[@id='EditPageHeader']//h1/a[contains(@title, '" + this.postTitle + "')]");
 
 		// edit article content (don't save yet)
 		doEdit(this.content);
@@ -76,12 +75,12 @@ public class BlogArticleReskinTest extends EditPageBaseTest {
 		assertTrue(session().isTextPresent("test blog post --"));
 	}
 
-	@Test(groups={"CI", "envProduction", "reskin"},dependsOnMethods={"testEnsureLoggedInUserCanCreateBlogPosts"})
+	@Test(groups={"CI", "verified", "envProduction", "reskin"},dependsOnMethods={"testEnsureLoggedInUserCanCreateBlogPosts"})
 	public void testEnsureUserCanPostCommentsOnBlogPosts() throws Exception {
 		loginAsBot();
 
 		// comment newly created blog post
-		session().open("index.php?title=" + this.postPage);
+		openAndWait("index.php?title=" + this.postPage);
 		assertTrue(session().isTextPresent("test blog post --"));
 
 		String comment = "comment test";
@@ -92,13 +91,13 @@ public class BlogArticleReskinTest extends EditPageBaseTest {
 		assertTrue(session().isTextPresent(comment));
 	}
 
-	@Test(groups={"CI", "envProduction", "reskin"},dependsOnMethods={"testEnsureUserCanPostCommentsOnBlogPosts"})
+	@Test(groups={"CI", "verified", "envProduction", "reskin"},dependsOnMethods={"testEnsureUserCanPostCommentsOnBlogPosts"})
 	public void testEditExistingBlogPost() throws Exception {
 		// login as staff user so we can remove blog post when the test is done
 		loginAsStaff();
 
 		// edit existing blog post
-		session().open("index.php?action=edit&title=" + this.postPage);
+		openAndWait("index.php?action=edit&title=" + this.postPage);
 
 		// header should contain blog post title
 		assertTrue(session().isElementPresent("//header[@id='EditPageHeader']//h1/a[contains(@title, '" + this.postTitle + "')]"));
@@ -112,18 +111,17 @@ public class BlogArticleReskinTest extends EditPageBaseTest {
 		// change the content
 		doEdit("blog post updated --~~~~");
 		clickAndWait("wpSave");
-		assertTrue(session().isTextPresent("blog post updated --"));
+		waitForTextPresent("blog post updated --");
 
 		// cleanup
-		session().open("index.php?title=" + this.postPage);
-		doDelete("Author request", "Cleanup after BlogArticle test");
+		openAndWait("index.php?title=" + this.postPage);
+		doDelete("label=regexp:.*Author request", "Cleanup after BlogArticle test");
 	}
 
-	@Test(groups={"CI", "envProduction", "reskin"})
+	@Test(groups={"CI", "verified", "envProduction", "reskin"})
 	public void testEnsureCreateBlogPageIsNotAvailableForAnons() throws Exception {
-		session().open("index.php?action=edit&useeditor=wysiwyg&title=Special:CreateBlogPage");
-		session().waitForPageToLoad(this.getTimeout());
+		openAndWait("index.php?action=edit&useeditor=wysiwyg&title=Special:CreateBlogPage");
 
-		assertTrue(session().isTextPresent("Not logged in"));
+		waitForTextPresent("Not logged in");
 	}
 }
--- tests/acceptance/com/wikia/selenium/tests/BlogArticleTest.java
+++ tests/acceptance/com/wikia/selenium/tests/BlogArticleTest.java
@@ -28,7 +28,7 @@ public class BlogArticleTest extends BaseTest {
 		return this.title;
 	}
 
-	@Test(groups={"CI", "verified"})
+	@Test(groups={"CI", "verified", "deprecated"})
 	public void testEnsureLoggedInUserCanCreateBlogPosts() throws Exception {
 		login();
 		openAndWait("index.php?title=Special:CreateBlogPage");
@@ -45,7 +45,7 @@ public class BlogArticleTest extends BaseTest {
 		logout();
 	}
 	
-	@Test(groups={"CI", "verified"},dependsOnMethods={"testEnsureLoggedInUserCanCreateBlogPosts"})
+	@Test(groups={"CI", "verified", "deprecated"},dependsOnMethods={"testEnsureLoggedInUserCanCreateBlogPosts"})
 	public void testEnsureUserCanPostCommentsOnBlogPosts() throws Exception {
 		loginAsRegular();
 		openAndWait( "index.php?title=User_blog:" + getTestConfig().getString("ci.user.wikiabot.username") + "/" + this.getTitle().replace( " ", "_" ) );
