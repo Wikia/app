eb81572030713dc522ea2f78bbda4407e738b85b
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-22 23:02:09 +0000

    WikiaMobile: Restore of templates
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41145 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileBodyService_index.php
@@ -0,0 +1,6 @@
+<section id="WikiaPage">
+	<?= $pageHeaderContent ;?>
+	<article id="WikiaMainContent"><?= $bodyContent ;?></article>
+	<footer id="WikiaMainContentFooter"><?= $afterContentHookText ;?></footer>
+	<aside id="WikiaAfterBodyContent"><?= $afterBodyContent ;?></aside>
+</section>
---
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileLeftPaneService_index.php
@@ -0,0 +1,3 @@
+<div id="leftPane">
+	<?= $leftPaneContent ?>
+</div>
