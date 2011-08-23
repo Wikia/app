057cbe61182ccfbb0685f65f468d675b668ceff9
hyun <hyun@wikia-inc.com>
2011-08-23 01:19:19 +0000

    WikiFeatures: work in progress
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41153 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFeatures/js/WikiFeatures.js
+++ extensions/wikia/WikiFeatures/js/WikiFeatures.js
@@ -1,3 +1,13 @@
-var AdminDashboard = {
-
+var WikiFeatures = {
+	init: function() {
+		WikiFeatures.sliders = $('#WikiFeatures .slider');
+		WikiFeatures.sliders.find('.button').click(e) {
+			var el = $(this).closest('.slider');
+			el.toggleClass('on');
+		}
+	}
 };
+
+$(function() {
+	WikiFeatures.init();
+});
