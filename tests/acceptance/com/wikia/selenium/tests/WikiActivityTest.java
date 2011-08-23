3294df3867ae3cdf943f68b22a7c7da6399cd9cb
annahojan <annahojan@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-23 14:02:59 +0000

    wiki activity selenium test enhancements
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41175 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/WikiActivityTest.java
+++ tests/acceptance/com/wikia/selenium/tests/WikiActivityTest.java
@@ -1,11 +1,19 @@
 //@author Sean Colombo - based on MyHomeTest by Macbre
 package com.wikia.selenium.tests;
 
+import java.util.Date;
+
 import org.testng.annotations.Test;
 import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.session;
-import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.*;
 
 public class WikiActivityTest extends BaseTest {
+	private static String WIKIACTIVITY_ARTICLE_ONE   = "Wiki Activity Article One";
+	private static String WIKIACTIVITY_ARTICLE_TWO   = "Wiki Activity Article Two";
+	private static String WIKIACTIVITY_ARTICLE_THREE = "Wiki Activity Article Three";
+	private static String WIKIACTIVITY_ARTICLE_FOUR  = "Wiki Activity Article Four";
+	private static String WIKIACTIVITY_ARTICLE_FIVE  = "Wiki Activity Article Five";
+	private static String WIKIACTIVITY_ARTICLE_SIX   = "Wiki Activity Article Six";
 
 	@Test(groups={"CI", "verified"})
 	public void testEnsuresThatWikiActivityButtonLeadsToProperSpecialPage() throws Exception {
@@ -19,9 +27,6 @@ public class WikiActivityTest extends BaseTest {
 
 		// check what page you land on
 		assertTrue(session().getLocation().contains("/wiki/Special:WikiActivity"));
-
-		// check presence of myhome feed (still has this name in WikiActivity)
-		assertTrue(session().isElementPresent("myhome-activityfeed"));
 	}
 
 	@Test(groups={"CI", "verified"})
@@ -33,5 +38,115 @@ public class WikiActivityTest extends BaseTest {
 
 		// check presence of link to ActivityFeed for anons
 		assertTrue(session().isElementPresent("//section[contains(@class,'WikiaActivityModule')]/a[@class='more']"));
+		
+		String content = "Lorem ipsum " + (new Date()).toString();
+		
+		editArticle(WIKIACTIVITY_ARTICLE_ONE, content);
+		editArticle(WIKIACTIVITY_ARTICLE_TWO, content);
+		editArticle(WIKIACTIVITY_ARTICLE_THREE, content);
+		editArticle(WIKIACTIVITY_ARTICLE_FOUR, content);
+
+		session().open("index.php?title=Special:Random");
+		session().waitForPageToLoad(this.getTimeout());
+		waitForElement("//section[contains(@class,'WikiaActivityModule')]");
+		waitForTextPresent(WIKIACTIVITY_ARTICLE_FOUR);
+		
+		assertEquals(WIKIACTIVITY_ARTICLE_FOUR, session().getText("//section[contains(@class,'WikiaActivityModule')]//li[1]//a"));
+		assertEquals(WIKIACTIVITY_ARTICLE_THREE, session().getText("//section[contains(@class,'WikiaActivityModule')]//li[2]//a"));
+		assertEquals(WIKIACTIVITY_ARTICLE_TWO, session().getText("//section[contains(@class,'WikiaActivityModule')]//li[3]//a"));
+		assertEquals(WIKIACTIVITY_ARTICLE_ONE, session().getText("//section[contains(@class,'WikiaActivityModule')]//li[4]//a"));
+		
+		editArticle(WIKIACTIVITY_ARTICLE_FIVE, content);
+
+		session().open("index.php?title=Special:Random");
+		session().waitForPageToLoad(this.getTimeout());
+		waitForElement("//section[contains(@class,'WikiaActivityModule')]");
+		waitForTextPresent(WIKIACTIVITY_ARTICLE_FIVE);
+		
+		assertEquals(WIKIACTIVITY_ARTICLE_FIVE, session().getText("//section[contains(@class,'WikiaActivityModule')]//li[1]//a"));
+		assertEquals(WIKIACTIVITY_ARTICLE_FOUR, session().getText("//section[contains(@class,'WikiaActivityModule')]//li[2]//a"));
+		assertEquals(WIKIACTIVITY_ARTICLE_THREE, session().getText("//section[contains(@class,'WikiaActivityModule')]//li[3]//a"));
+		assertEquals(WIKIACTIVITY_ARTICLE_TWO, session().getText("//section[contains(@class,'WikiaActivityModule')]//li[4]//a"));
+		assertFalse(session().isElementPresent("//section[contains(@class,'WikiaActivityModule')]//li[5]"));
+	}
+
+	@Test(groups={"oasis","CI"})
+	public void testEditingArticleByUser() throws Exception{
+		
+		session().open("index.php?title=Special:Random");
+		String content = "Lorem ipsum " + (new Date()).toString();
+		editArticle(WIKIACTIVITY_ARTICLE_ONE, content);
+	    session().open("index.php?title=Special:Random");
+		assertTrue(session().isElementPresent("//a[@data-id='wikiactivity']"));
+		session().click("//a[@data-id='wikiactivity']");
+		waitForElement("//ul[@id='myhome-activityfeed']");
+        assertTrue(session().getLocation().contains("/wiki/Special:WikiActivity"));
+		assertEquals(WIKIACTIVITY_ARTICLE_ONE, session().getText("//ul[@id='myhome-activityfeed']/li[1]//a"));
+	}
+	
+	@Test(groups={"oasis","CI"})
+	public void testEditingArticleByAnonymousUserAndLoggedUser() throws Exception{
+		
+		session().open("index.php?title=Special:Random");
+		String content = "Lorem ipsum " + (new Date()).toString();
+		editArticle(WIKIACTIVITY_ARTICLE_ONE, content);
+	
+		content = "dolor " + (new Date()).toString();
+		loginAsRegular();
+		editArticle(WIKIACTIVITY_ARTICLE_ONE, content);
+		
+		session().open("index.php?title=Special:Random");
+		assertTrue(session().isElementPresent("//a[@data-id='wikiactivity']"));
+		session().click("//a[@data-id='wikiactivity']");
+		waitForElement("//ul[@id='myhome-activityfeed']");
+        assertTrue(session().getLocation().contains("/wiki/Special:WikiActivity"));
+        
+		assertEquals(WIKIACTIVITY_ARTICLE_ONE, session().getText("//ul[@id='myhome-activityfeed']/li[1]//a"));
+		assertEquals(WIKIACTIVITY_ARTICLE_ONE, session().getText("//ul[@id='myhome-activityfeed']/li[2]//a"));
+	}
+	
+ 	@Test(groups={"oasis","CI"})
+	public void testFollowingPagesFiltres() throws Exception{
+	 		
+		openAndWait("index.php?title=Special:Random");
+		String content = "Lorem ipsum " + (new Date()).toString();
+		loginAsRegular();
+	    session().click("link=My preferences");
+		waitForElement("//ul[@id='preftoc']");
+		session().click("//ul[@id='preftoc']/li[6]/a");
+		
+		if(!session().isChecked("//input[@id='mw-input-watchdefault']"))
+			{
+				session().check("//input[@id='mw-input-watchdefault']");
+			}
+		clickAndWait("//input[@id='prefcontrol']");
+		
+		editArticle(WIKIACTIVITY_ARTICLE_SIX, content);
+		
+		openAndWait("index.php?title=Special:Random");
+		clickAndWait("//a[@data-id='wikiactivity']");
+		waitForElement("//ul[@id='myhome-activityfeed']");
+        assertTrue(session().getLocation().contains("/wiki/Special:WikiActivity"));
+		clickAndWait("//header[@id='WikiaPageHeader']/h2/nav//li[@class='watchlist']/a");
+		waitForElement("//input[@id='wikiactivity-default-view-switch']");
+		assertEquals(WIKIACTIVITY_ARTICLE_SIX, session().getText("//ul[@id='myhome-activityfeed']/li[1]//a"));
+        
+
+		session().click("link=My preferences");
+		waitForElement("//ul[@id='preftoc']");
+		session().click("//ul[@id='preftoc']/li[6]/a");
+		if(session().isChecked("//input[@id='mw-input-watchdefault']"))
+			{
+				session().uncheck("//input[@id='mw-input-watchdefault']");
+			}
+		clickAndWait("//input[@id='prefcontrol']");
+		content = "dolor " + (new Date()).toString();
+		editArticle(WIKIACTIVITY_ARTICLE_FOUR, content);
+		clickAndWait("//a[@data-id='wikiactivity']");
+		waitForElement("//ul[@id='myhome-activityfeed']");
+        assertTrue(session().getLocation().contains("/wiki/Special:WikiActivity"));
+		clickAndWait("//header[@id='WikiaPageHeader']/h2/nav//li[@class='watchlist']/a");
+		waitForElement("//input[@id='wikiactivity-default-view-switch']");
+		assertFalse(session().getText("//ul[@id='myhome-activityfeed']/li[1]//a").equals(WIKIACTIVITY_ARTICLE_FOUR));
 	}
 }
