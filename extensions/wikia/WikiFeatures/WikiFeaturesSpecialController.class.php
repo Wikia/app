a1dea785f83dedd760fb619bf413690d2ccc83db
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-23 00:14:29 +0000

    trunk: initial enable/disable method for WikiFeatures
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41149 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFeatures/WikiFeaturesSpecialController.class.php
+++ extensions/wikia/WikiFeatures/WikiFeaturesSpecialController.class.php
@@ -20,5 +20,61 @@ class WikiFeaturesSpecialController extends WikiaSpecialPageController {
 	public function index() {
 		
 	}
+
+	// TODO: flag this as an internal dispatch function only
+	public function beforeEnableDisable() {
+		
+		$caller = $this->getVal("caller");
+		$this->error = false;
+		if(!$this->wg->User->isAllowed( 'wikifeatures' )) {
+			$this->error = true;
+			$this->errorMsg = $this->wf->Msg('wikifeatures-permission-error');
+			return;
+		}
+
+		// TODO: lots of checking to make sure this extension is in our list of features, etc
+		// For now, just check for null
+		$feature = $this->getVal('feature', null);
+		if (!$feature) {
+			$this->error = true;
+			$this->errorMsg = $this->wf->Msg('wikifeatures-missing-parameter-error');
+			return;
+		}
+		// tells the caller the feature is ok/validated
+		$this->feature = $feature;
+
+		// Full message string contains enabled/disabled string
+		$logMsg = F::app()->wf->msgForContent( "wikifeatures-log-$caller-extension", $feature );
+		$log = WF::build( 'LogPage', array( 'wikifeatures' ) );
+        $log->addEntry( 'wikifeatures', SpecialPage::getTitleFor( 'WikiFeatures'), $logMsg, array() );
+		
+	}
 	
+	public function enable() {
+
+		// do universal validation stuff first
+		$response = $this->sendSelfRequest('beforeEnableDisable', array('caller' => 'enable'));
+		if ($response->getVal('error') == false) {
+			$feature = $response->getVal('feature');
+			WikiFactory::setVarByName ($feature, $this->wg->CityId, true, "WikiFeatures" );
+		} else {
+			$this->errorMsg = $response->getVal('errorMsg');
+		}
+	}
+	
+	public function disable() {
+
+		// do universal validation stuff first
+		$response = $this->sendSelfRequest('beforeEnableDisable', array('caller' =>'disable'));
+
+		if ($response->getVal('error') == false) {
+			$feature = $response->getVal('feature');
+			WikiFactory::setVarByName ($feature, $this->wg->CityId, true, "WikiFeatures" );
+		} else {
+			$this->errorMsg = $response->getVal('errorMsg');
+		}
+		
+	}
+	
+		
 }
