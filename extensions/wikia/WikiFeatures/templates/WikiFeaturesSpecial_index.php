630d9a129ea2960a58ec4a160032a7102c627899
hyun <hyun@wikia-inc.com>
2011-08-24 00:21:39 +0000

    WikiFeatures - work in progress
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41202 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFeatures/templates/WikiFeaturesSpecial_index.php
+++ extensions/wikia/WikiFeatures/templates/WikiFeaturesSpecial_index.php
@@ -1,4 +1,4 @@
-<div class="WikiFeatures" id="WikiFeatures">
+<section class="WikiFeatures" id="WikiFeatures">
 	<h2>
 		<?= wfMsg('wikifeatures-heading') ?>
 	</h2>
@@ -9,14 +9,6 @@
 	<ul class="features">
 		<li class="feature">
 			<img class="representation" height="100" width="150" src="http://placekitten.com/150/100" >
-			<div class="details">
-				<h3>
-					Visual Category Pages <span class="date">May 23, 2011</span>
-				</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis massa sit amet dui blandit pharetra. Ut urna odio, iaculis eget aliquam sed, volutpat eu felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam adipiscing adipiscing turpis, id lacinia massa lacinia eget.
-				</p>
-			</div>
 			<div class="actions">
 				<span class="slider">
 					<span class="button"></span>
@@ -25,6 +17,81 @@
 					<span class="loading"></span>
 				</span>
 			</div>
+			<div class="details">
+				<h3>
+					Visual Category Pages <span class="date">May 23, 2011</span>
+				</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis massa sit amet dui blandit pharetra. Ut urna odio, iaculis eget aliquam sed, volutpat eu felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam adipiscing adipiscing turpis, id lacinia massa lacinia eget.
+				</p>
+			</div>
+		</li>
+		<li class="feature">
+			<img class="representation" height="100" width="150" src="http://placekitten.com/150/100" >
+			<div class="actions">
+				<span class="slider">
+					<span class="button"></span>
+					<span class="textoff">Off</span>
+					<span class="texton">On</span>
+					<span class="loading"></span>
+				</span>
+			</div>
+			<div class="details">
+				<h3>
+					Visual Category Pages <span class="date">May 23, 2011</span>
+				</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis massa sit amet dui blandit pharetra. Ut urna odio, iaculis eget aliquam sed, volutpat eu felis.
+				</p>
+			</div>
 		</li>
 	</ul>
-</div>
+	
+	<h2>
+		<?= wfMsg('wikifeatures-labs-heading') ?>
+	</h2>
+	<p>
+		<?= wfMsg('wikifeatures-labs-creative') ?>
+	</p>
+	
+	<ul class="features">
+		<li class="feature">
+			<img class="representation" height="100" width="150" src="http://placekitten.com/150/100" >
+			<div class="actions">
+				<span class="slider">
+					<span class="button"></span>
+					<span class="textoff">Off</span>
+					<span class="texton">On</span>
+					<span class="loading"></span>
+				</span>
+			</div>
+			<div class="details">
+				<h3>
+					Visual Category Pages <span class="date">May 23, 2011</span>
+				</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis massa sit amet dui blandit pharetra. Ut urna odio, iaculis eget aliquam sed, volutpat eu felis.
+				</p>
+			</div>
+		</li>
+		<li class="feature">
+			<img class="representation" height="100" width="150" src="http://placekitten.com/150/100" >
+			<div class="actions">
+				<span class="slider">
+					<span class="button"></span>
+					<span class="textoff">Off</span>
+					<span class="texton">On</span>
+					<span class="loading"></span>
+				</span>
+			</div>
+			<div class="details">
+				<h3>
+					Visual Category Pages <span class="date">May 23, 2011</span>
+				</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis massa sit amet dui blandit pharetra. Ut urna odio, iaculis eget aliquam sed, volutpat eu felis.
+				</p>
+			</div>
+		</li>
+	</ul>
+</section>
