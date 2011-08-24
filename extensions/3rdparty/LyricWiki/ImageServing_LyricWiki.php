a10806f7a48f3647f945f6db91369e7b731f662d
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-24 05:54:26 +0000

    Started experimenting with the values that ImageServing has in it when it calls its new hook. BugzId 876
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41210 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/LyricWiki/ImageServing_LyricWiki.php
+++ extensions/3rdparty/LyricWiki/ImageServing_LyricWiki.php
@@ -31,4 +31,27 @@ $wgHooks['ImageServing::fallbackOnNoResults'] = 'lw_ImageServingFallback';
  */
 function lw_ImageServingFallback(&$imageServing, $n, &$out){
 	
+	// TODO: Make this work for Gracenote pages too? (they don't have their own images and would have to fall back to the NS_MAIN pages with the same titles).
+
+	$gnArticles = $imageServing->articlesByNS[ NS_GRACENOTE ];
+	if(!empty($gnArticles)){
+		foreach($gnArticles as $gnArticleId => $gnArticleData){
+			$title = $gnArticleData['title'];
+			
+print "TRYING TO FIND FALLBACK IMAGE FOR GRACENOTE PAGE: \"$title\"<br/>\n";
+		}
+	}
+
+	$articles = $imageServing->articlesByNS[ NS_MAIN ];
+	if(!empty($articles)){
+		foreach($articles as $articleId => $articleData){
+			$title = $articleData['title'];
+			
+print "TRYING TO FIND FALLBACK IMAGE FOR PAGE: \"$title\"<br/>\n";
+		}
+	}
+	
+exit;
+
+	return true; // because this is a hook
 } // end lw_ImageServingFallback()
