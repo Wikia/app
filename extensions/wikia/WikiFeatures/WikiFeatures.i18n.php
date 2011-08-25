15672f3635893daf977d63d61feb241a8a834dae
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-25 14:05:23 +0000

    Normalise i18n file. Message tweaks.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41297 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFeatures/WikiFeatures.i18n.php
+++ extensions/wikia/WikiFeatures/WikiFeatures.i18n.php
@@ -9,8 +9,11 @@ $messages = array();
 
 $messages['en'] = array(
 	'wikifeatures-heading' => 'Features',
-	'wikifeatures-creative' => "Welcome to Wikia Features.  Below, you'll find tools you can use to customize your wiki by activating any of the features.  Play around a little - you can always turn off the tools you don't want.",
+	'wikifeatures-creative' => "Welcome to Wikia Features.
+Below, you will find tools you can use to customize your wiki by activating any of the features.
+Play around a little - you can always turn off the tools you do not want.",
 	'wikifeatures-labs-heading' => 'Wikia Labs',
-	'wikifeatures-labs-creative' => "Wikia Labs is where you'll find some ideas we're playing with.  We know they still have their kinks, and we would love to receive your feedback on the good, the bad, and the downright quirky behavior of these products.",
+	'wikifeatures-labs-creative' => "Wikia Labs is where you will find some ideas we are playing with.
+We know they still have their kinks, and we would love to receive your feedback on the good, the bad, and the downright quirky behavior of these products.",
 	'wikifeatures-feedback' => 'Give feedback',
 );
