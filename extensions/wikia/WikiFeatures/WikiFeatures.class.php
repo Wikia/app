df78c568a9b466a2680652aad7d1840ddcc2438a
saipetch <saipetch@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-25 20:49:39 +0000

    bugid 10449: WikiFeatures - use model.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41309 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiFeatures/WikiFeatures.class.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Wiki Features Model
+ * @author Hyun
+ * @author Owen
+ * @author Saipetch
+ */
+class WikiFeatures extends WikiaModel {
+	
+	/**
+	 * @desc get a list of regular features
+	 * @return array $list 
+	 */
+	public function getFeatureNormal() {
+		$list = array();
+		if (is_array($this->wg->WikiFeatures['normal'])) {
+			foreach ($this->wg->WikiFeatures['normal'] as $feature) {
+				$list[] = array(
+					'name' => $feature, 
+					'enabled' => $this->app->getGlobal($feature)
+				);
+			}
+		}
+		return $list;
+	}
+	
+	/**
+	 * @desc get a list of labs features
+	 * @return array $list 
+	 */
+	public function getFeatureLabs() {
+		$list = array();
+		if (is_array($this->wg->WikiFeatures['labs'])) {
+			foreach ($this->wg->WikiFeatures['labs'] as $feature) {
+				$list[] = array(
+					'name' => $feature, 
+					'enabled' => $this->app->getGlobal($feature), 
+					'rating' => $this->getRating($feature), 
+					'active' => $this->getNumActiveWikis($feature),
+				);
+			}
+		}
+		return $list;
+	}
+	
+	/**
+	 * @desc get number of active wikis for specified feature
+	 * @param string $feature
+	 * @return int $num 
+	 */
+	protected function getNumActiveWikis($feature) {
+		$this->wf->ProfileIn( __METHOD__ );
+		
+		$memKey = $this->getMemcKeyNumActiveWikis($feature);
+		$num = $this->wg->Memc->get($memKey);
+		if (is_null($num)) {
+			$db = $this->wf->GetDB(DB_SLAVE, array(), $this->wg->ExternalSharedDB);
+
+			$result = $db->selectRow(
+				array('city_variables_pool', 'city_variables'),
+				'count(distinct cv_city_id) as cnt',
+				array('cv_name' => $feature),
+				__METHOD__,
+				array(),
+				array(
+					'city_variables' => array(
+						'LEFT JOIN',
+						array('cv_id=cv_variable_id', 'cv_value' => serialize(true))
+					)
+				)
+			);
+
+			$num = ($result) ? intval($result->cnt) : 0 ;
+			$this->wg->Memc->set($memKey, $num, 3600*24);
+		}
+		
+		$this->wf->ProfileOut( __METHOD__ );
+		
+		return $num;
+	}
+	
+	/**
+	 * @desc get memcache key of the number of active wikis for specified feature
+	 * @param string $feature
+	 * @return string 
+	 */
+	public function getMemcKeyNumActiveWikis($feature) {
+		return $this->wf->SharedMemcKey('wikifeatures', 'active_wikis', $feature);
+	}
+	
+	protected function getRating($feature) {
+		$rating = 0;
+		
+		return $rating;
+	}
+	
+}
--- extensions/wikia/WikiFeatures/WikiFeaturesSpecialController.class.php
+++ extensions/wikia/WikiFeatures/WikiFeaturesSpecialController.class.php
@@ -21,6 +21,10 @@ class WikiFeaturesSpecialController extends WikiaSpecialPageController {
 		$this->response->addAsset('extensions/wikia/WikiFeatures/css/WikiFeatures.scss');
 		$this->response->addAsset('extensions/wikia/WikiFeatures/js/WikiFeatures.js');
 		//$this->wg->Out->addScriptFile($this->wg->ExtensionsPath . '/wikia/WikiFeatures/js/WikiFeatures.js');
+		
+		$wikifeatures = new WikiFeatures();
+		$this->normal = $wikifeatures->getFeatureNormal();
+		$this->labs = $wikifeatures->getFeatureLabs();
 	}
 
 	/**
@@ -56,115 +60,10 @@ class WikiFeaturesSpecialController extends WikiaSpecialPageController {
 		WikiFactory::setVarByName($feature, $this->wg->CityId, $enabled, "WikiFeatures");
 		
 		// clear cache for active wikis
-		$this->wg->Memc->delete($this->getMemcKeyNumActiveWikis($feature));
+		$wikifeatures = new WikiFeatures();
+		$this->wg->Memc->delete($wikifeatures->getMemcKeyNumActiveWikis($feature));
 			
 		$this->setVal('result', 'OK');
-	}
-	
-	/**
-	 * @desc get a list of features
-	 * @requestParam string $type [normal/labs]
-	 * @responseParam array $list
-	 */
-	public function getFeatureList() {
-		$type = $this->getVal('type', null);
-		
-		$this->wf->ProfileIn( __METHOD__ );
-		
-		$list = array();
-		if (array_key_exists($type, $this->wg->WikiFeatures)) {
-			if ($type=='labs') {
-				$list = $this->getFeatureLabs($this->wg->WikiFeatures[$type]);
-			} else {
-				$list = $this->getFeatureNormal($this->wg->WikiFeatures[$type]);
-			}
-		}
-		$this->wf->ProfileOut( __METHOD__ );
-		
-		$this->setVal('list', $list);
-	}
-	
-	/**
-	 * @desc get a list of regular features
-	 * @param string $features
-	 * @return array $list 
-	 */
-	protected function getFeatureNormal($features) {
-		foreach ($features as $feature) {
-			$list[] = array(
-				'name' => $feature, 
-				'enabled' => $this->app->getGlobal($feature)
-			);
-		}
-		return $list;
-	}
-	
-	/**
-	 * @desc get a list of labs features
-	 * @param string $features
-	 * @return array $list 
-	 */
-	protected function getFeatureLabs($features) {
-		foreach ($features as $feature) {
-			$list[] = array(
-				'name' => $feature, 
-				'enabled' => $this->app->getGlobal($feature), 
-				'rating' => $this->getRating($feature), 
-				'active' => $this->getNumActiveWikis($feature),
-			);
-		}
-		return $list;
-	}
-	
-	/**
-	 * @desc get number of active wikis for specified feature
-	 * @param string $feature
-	 * @return int $num 
-	 */
-	protected function getNumActiveWikis($feature) {
-		$this->wf->ProfileIn( __METHOD__ );
-		
-		$memKey = $this->getMemcKeyNumActiveWikis($feature);
-		$num = $this->wg->Memc->get($memKey);
-		if (is_null($num)) {
-			$db = $this->wf->GetDB(DB_SLAVE, array(), $this->wg->ExternalSharedDB);
-
-			$result = $db->selectRow(
-				array('city_variables_pool', 'city_variables'),
-				'count(distinct cv_city_id) as cnt',
-				array('cv_name' => $feature),
-				__METHOD__,
-				array(),
-				array(
-					'city_variables' => array(
-						'LEFT JOIN',
-						array('cv_id=cv_variable_id', 'cv_value' => serialize(true))
-					)
-				)
-			);
-
-			$num = ($result) ? $result->cnt : 0 ;
-			$this->wg->Memc->set($memKey, $num, 3600*24);
-		}
-		
-		$this->wf->ProfileOut( __METHOD__ );
-		
-		return $num;
-	}
-	
-	/**
-	 * @desc get memcache key of the number of active wikis for specified feature
-	 * @param string $feature
-	 * @return string 
-	 */
-	protected function getMemcKeyNumActiveWikis($feature) {
-		return $this->wf->SharedMemcKey('wikifeatures', 'active_wikis', $feature);
-	}
-	
-	protected function getRating($feature) {
-		$rating = 0;
-		
-		return $rating;
 	}
 	
 }
