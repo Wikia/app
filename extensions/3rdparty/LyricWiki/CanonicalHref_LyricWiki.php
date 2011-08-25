4edffd2deb2af86df27a97a69fa9ced749a4acf5
sean <sean@wikia-inc.com>
2011-08-25 21:20:26 +0000

    BugzId 10559: Customize the canonical href in the header so that Gracenote namespaced pages will use the canonical URL of the main-namespace equivalent page if such a page exists.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41311 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/3rdparty/LyricWiki/CanonicalHref_LyricWiki.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author Sean Colombo
+ *
+ * This extension hooks into Wikia's CanonicalHref extension to extend it for LyricWiki specific functionality. Namely,
+ * that Gracenote pages should have the same 'canonical' tag as their community page (if there is a corresponding community page).
+ *
+ * This prevents search engines from penalizing us for duplicate content, and also makes sure that search results will tend to
+ * show the community (editable) page rather than the Gracenote (locked) page.
+ */
+ 
+if ( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'CanonicalHref tweaks for LyricWiki',
+	'version' => '1.0',
+	'author' => '[http://seancolombo.com Sean Colombo]',
+	//'url' => 'http://lyrics.wikia.com/User:Sean_Colombo',
+	'descriptionmsg' => 'canonicalhref-lw-desc',
+);
+
+$dir = dirname( __FILE__ );
+$wgExtensionMessagesFiles['CanonicalHref_LyricWiki'] = $dir . '/CanonicalHref_LyricWiki.i18n.php';
+
+$wgHooks['WikiaCanonicalHref'][] = 'lw_CanonicalHref';
+
+/**
+ * This is called by ImageServing if no representative image is found for a page.
+ * If this is a song page, will first check the album page, then the artist page for a good image.
+ * If this is an album page, will check the artist page for a good image.
+ * If this is an artist page, there is no good fallback, so 'out' will be returned unmodified.
+ */
+function lw_CanonicalHref( &$canonicalUrl ){
+	wfProfileIn( __METHOD__ );
+	global $wgTitle;
+
+	// Only process if we're in NS_GRACENOTE
+	if(defined('NS_GRACENOTE') && ($wgTitle->getNamespace() == NS_GRACENOTE)){
+		// Check if a community version of the page exists.
+		$nonPrefixedTitleText = $wgTitle->getText();
+		$mainNsTitle = Title::newFromText( $nonPrefixedTitleText, NS_MAIN );
+		if($mainNsTitle->exists()){
+			// There is an equivalent in the main namespace, so modify the canonicalUrl to be the main namespace page's fullUrl.
+			$canonicalUrl = $mainNsTitle->getFullURL();
+		}
+	}
+
+	wfProfileOut( __METHOD__ );
+	return true; // because this is a hook
+} // end lw_CanonicalHref()
