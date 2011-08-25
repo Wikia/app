2a69f3130cf47de2234a33bf0574d2be77114db8
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-25 00:27:02 +0000

    trunk: adding WikiaModel and WikiaObject to Nirvana
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41278 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ includes/wikia/WikiaModel.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Nirvana Framework - Model class
+ * 
+ * This is a minimalist class which defines $app $wf and $wg plus some DB helpers
+ * Use this for anything that is NOT a controller but still needs framework vars
+ * If you do NOT need DB access consider extending WikiaObject instead
+ *
+ * @ingroup nirvana
+ *
+ * @author Owen Davis <owen(at)wikia-inc.com>
+ */
+abstract class WikiaModel extends WikiaObject {
+
+	/* Handy helper functions for getting a database connection */
+	
+	public function getDB( $db_type = DB_SLAVE ) {
+	   return $this->wf->GetDB( $db_type );
+	}
+
+	public function getSharedDB( $db_type = DB_SLAVE ) {
+	   return $this->wf->GetDB( $db_type, array(), $this->wg->ExternalSharedDB );
+	}
+	
+	public function getDatawareDB( $db_type = DB_SLAVE ) {
+		return $this->wf->GetDB( $db_type, array(), $this->wg->ExternalDatawareDB );
+	}
+	
+	public function getStatsDB( $db_type = DB_SLAVE ) {
+		return $this->wf->GetDB( $db_type, array(), $this->wg->StatsDB );
+	}
+}
