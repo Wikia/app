bf4f9394f7286db2f9ac22e9c8fa8bc6fb4115ff
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-30 10:42:38 +0000

    added small tag extension by Grunny BugzId:10568
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41413 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SharedHelp/CentralHelpSearch.i18n.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Messages file for the CentralHelpSearch extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+/**
+ * Get all extension messages
+ *
+ * @return array
+ */
+$messages = array();
+
+$messages['en'] = array(
+	'centralhelpsearch-desc'        => 'Allow inclusion of a form to search Community Central Help',
+	'centralhelpsearch-placeholder' => 'Search Help&hellip;'
+);
