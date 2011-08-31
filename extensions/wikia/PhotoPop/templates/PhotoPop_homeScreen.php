63161d574a3e0c31dbd26d494a7e3267e2979134
sean <sean@wikia-inc.com>
2011-08-31 07:35:02 +0000

    PhotoPop: Refactored the homescreen to have its CSS separated out (since that was doable and is cleaner code and is more cacheable). PhotoPop: BugzId: 10616 - Added the home button to the game screen.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41461 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/PhotoPop/templates/PhotoPop_homeScreen.php
+++ extensions/wikia/PhotoPop/templates/PhotoPop_homeScreen.php
@@ -3,52 +3,13 @@
 		<title><?= wfMsg('photopop-title-tag-homescreen') ?></title>
 		<meta name="viewport" content = "width=device-width, initial-scale = 1, minimum-scale = 1, maximum-scale = 1, user-scalable = no" />
 		<style>
-		html {
-			margin:0px;
-			padding:0px;
-		}
-		body {
-			margin:0px;
-			padding:0px;
-			background:#333;
-		}
 		#wrapper{
 			width: <?= $boardWidth ?>px;
 			height: <?= $boardHeight ?>px;
-			overflow:hidden;
 			background: #00396D url(<?= $BG_IMAGE ?>);
-			position:absolute;
-			left:0px;
-			top:0px;
-		}
-		
-		#logoWrapper{
-			margin-top:5px;
-			width:100%;
-			text-align:center;
-background-color:blue;
-		}
-		#photopop_logo{
-			margin:auto;
-			text-align:right;
-background-color:green;
-		}
-		
-		#buttonWrapper{
-			bottom:0px;
-			width:100%;
-background-color:orange;
-		}
-		#buttonWrapper div{
-			padding:5px;
-		}
-		#button_scores, #button_tutorial{
-			float:left;
-		}
-		#button_volume{
-			float:right;
 		}
 		</style>
+		<?= $cssLink ?>
 	</head>
 	<body>
 		<div id='wrapper'>
@@ -58,16 +19,21 @@ background-color:orange;
 					<img src='<?= $POWERED_BY_LOGO ?>'/>
 				</div>
 			</div>
+
+			<div id='playWrapper'>
+				<a href='<?= $playButtonUrl ?>'><img src='<?= $buttonSrc_play ?>'/></a>
+			</div>
 			
 			<div id='buttonWrapper'>
 				<div id='button_scores'>
 					<img src='<?= $buttonSrc_scores ?>'/>
 				</div>
 				<div id='button_tutorial'>
-					<img src='<?= $buttonSrc_tutorial ?>'/>
+					<a href='<?= $tutorialButtonUrl ?>'><img src='<?= $buttonSrc_tutorial ?>'/></a>
 				</div>
 				<div id='button_volume'>
-					<img src='<?= $buttonSrc_volumeOn ?>'/>
+					<img class='on' src='<?= $buttonSrc_volumeOn ?>'/>
+					<img class='off' src='<?= $buttonSrc_volumeOff ?>'/>
 				</div>
 			</div>
 		</div>
---
+++ extensions/wikia/PhotoPop/templates/PhotoPop_tutorialScreen.php
@@ -0,0 +1,5 @@
+Tutorial screen will go here</br>
+
+Should just wrap a stripped-down version of the playGame template so that the tutorial doesn't have
+to be constantly updated any time the game changes... after the stripped down version (no external JS, etc.) is
+shown, then we just need to add extra code for the dialog box w/the button on it.
