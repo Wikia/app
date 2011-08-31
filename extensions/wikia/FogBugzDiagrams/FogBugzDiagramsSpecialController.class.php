43971ba7a6d2066be550df4b318b8fe9dbe3a942
pawelrychly <pawelrychly@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-31 09:58:06 +0000

    merge FogBugzDiagrams
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41471 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/FogBugzDiagrams/FogBugzDiagramsSpecialController.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Special site showing informations from FogBugz in Diagrams
+ * @author Pawe³ Rych³y
+ * @author Piotr Paw³owski ( Pepe )
+ * @brief Special Page based on project writen by wojtek@wikia-inc.com
+ */
+class FogBugzDiagramsSpecialController extends WikiaSpecialPageController {
+
+	public function __construct() {
+	    // standard SpecialPage constructor call
+		parent::__construct( 'FogBugzDiagrams', '', false );
+	}
+
+
+	
+	
+	/**
+	 * The index page of FogBugzDiagrams Extension
+	 */
+	public function index() {
+		$this->wf->profileIn( __METHOD__ );    	
+		$this->response->addAsset( "./skins/common/jquery/jquery.flot.js" );
+		$this->response->addAsset( "./skins/common/jquery/jquery.flot.threshold.js" );
+		$this->response->addAsset( "./skins/common/jquery/jquery.flot.stack.js" );
+	 	$this->response->addAsset( "./extensions/wikia/FogBugzDiagrams/js/scripts.js" );
+		$this->response->addAsset( "./extensions/wikia/FogBugzDiagrams/css/cssFile.css" );
+		
+		$report= new FogBugzReport();
+		$data_send = $report->getPreparedData();
+		$this->response->setData( $data_send );
+		
+		$this->wg->Out->setPageTitle( $this->wf->msg( 'fog-bugz-diagrams-special-page' ) );
+		$this->wf->profileOut( __METHOD__ );
+	} 
+
+} 
+    	
+    	
