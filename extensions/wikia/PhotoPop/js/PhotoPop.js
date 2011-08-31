67ca39987d758048c1199f30dbe159b013381776
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-08-31 20:59:03 +0000

    PhotoPop: BugzId 10835 - JSLint and other cleanup.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41508 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/PhotoPop/js/PhotoPop.js
+++ extensions/wikia/PhotoPop/js/PhotoPop.js
@@ -1,63 +1,7 @@
 /** IMPLEMENTATION USING HTML/CSS (DIVS FOR TILES) **/
 
 if (typeof PhotoPop === 'undefined') {
-	/**
-	 * This class controls the flow of the entire PhotoPop game.  The actual gameplay is inside of 
-	 * the PhotoPop.FlipBoard class.
-	 */
-	PhotoPop = function(){
-		var self = this;
-		this.debug = true; // whether to log a whole bunch of info to console.log
-
-
-		
-		
-		/**
-		 * Simple wrapper for console.log to allow us to turn on/off debugging for this
-		 * whole class at once.
-		 */
-		this.log = function(msg){
-			if(self.debug){
-				console.log(msg);
-			}
-		};	
-	};
-}
-
-
-
-if (typeof PhotoPop.GameConfigs === 'undefined') {
-	/**
-	 * This class stores a configuration for a single game type. For example, to play the game on TrueBlood with
-	 * category Characters, you would have one GameConfig object, but to play on Glee with category Characters or
-	 * TrueBlood with category Episodes would be another.
-	 */
-	PhotoPop.GameConfigs = function(wikiDomain, category, iconSrc){
-		var self = this;
-		this._wikiDomain = wikiDomain; // eg: "lyrics.wikia.com"
-		this._category = category; // eg: "Category:Albums released in 1984";
-		this._iconSrc = iconSrc; // the URL of the square icon for the selector-screen.
-	};
-
-	/**
-	 * Loads all possible game configurations to present the player with a choice for which to play.
-	 */
-	PhotoPop.getConfigs = function(){
-		var configs = [];
-
-		// Hard-coded games. Eventually this will just be a fallback in case the MediaWiki message containing the data doesn't load.
-		configs.push( new PhotoPop.GameConfigs("trueblood.wikia.com", "Category:Characters", "") );
-		configs.push( new PhotoPop.GameConfigs("glee.wikia.com", "Category:Characters", "") );
-		configs.push( new PhotoPop.GameConfigs("lyrics.wikia.com", "Category:Album", "") );
-		configs.push( new PhotoPop.GameConfigs("muppet.wikia.com", "Category:The_Muppets_Characters", "") );
-		configs.push( new PhotoPop.GameConfigs("dexter.wikia.com", "Category:Characters", "") );
-		configs.push( new PhotoPop.GameConfigs("futurama.wikia.com", "Category:Characters", "") );
-		configs.push( new PhotoPop.GameConfigs("twilight.wikia.com", "Category:Characters", "") );
-		//configs.push( new PhotoPop.GameConfigs("gameofthrones.wikia.com", "Category:Characters", "") );
-		//configs.push( new PhotoPop.GameConfigs("ben10.wikia.com", "Category:Episodes", "") );
-
-		return configs;
-	};
+	PhotoPop = {};
 }
 
 if (typeof PhotoPop.SoundManager === 'undefined') {
@@ -70,11 +14,11 @@ if (typeof PhotoPop.SoundManager === 'undefined') {
 		this.debug = true;
 
 		this.soundFiles = {
-			'click': '/extensions/wikia/PhotoPop/audio/FingerPlop4.wav',
-			'right': '/extensions/wikia/PhotoPop/audio/Human-Applause-MediumCrowd03.wav',
-			'wrong': '/extensions/wikia/PhotoPop/audio/tubePop.wav',
-			'timeLow': '/extensions/wikia/PhotoPop/audio/CountdowntoBlastOff.wav',
-			'timeUp': '/extensions/wikia/PhotoPop/audio/Sad-Trombone.wav',
+			'click': wgExtensionsPath + '/wikia/PhotoPop/audio/FingerPlop4.wav',
+			'right': wgExtensionsPath + '/wikia/PhotoPop/audio/Human-Applause-MediumCrowd03.wav',
+			'wrong': wgExtensionsPath + '/wikia/PhotoPop/audio/tubePop.wav',
+			'timeLow': wgExtensionsPath + '/wikia/PhotoPop/audio/CountdowntoBlastOff.wav',
+			'timeUp': wgExtensionsPath + '/wikia/PhotoPop/audio/Sad-Trombone.wav'
 		};
 		
 		// Caches actual Audio objects.
@@ -127,9 +71,9 @@ if (typeof PhotoPop.SoundManager === 'undefined') {
 
 // Once the page is loaded, start the game.
 $(document).ready(function(){
-	console.log("Starting PhotoPop game...");
+	//console.log("Starting PhotoPop game...");
 	var flipBoard = new PhotoPop.FlipBoard();
 	flipBoard.init(function(){
-		console.log("Game has started!");
+		//console.log("Game has started!");
 	});
 });
