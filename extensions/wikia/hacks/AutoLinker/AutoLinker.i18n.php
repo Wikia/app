0a03dc566b6bd7dc3903a0993d7e7ad83adb6998
macbre <macbre@wikia-inc.com>
2011-09-02 10:29:32 +0000

    Autolinker: performance improvement - PHP provides regexp for JS layer
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41571 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/hacks/AutoLinker/AutoLinker.i18n.php
+++ extensions/wikia/hacks/AutoLinker/AutoLinker.i18n.php
@@ -6,5 +6,5 @@ $messages['en'] = array(
 	'wikia-editor-modules-autolinker-title' => 'Auto linker tool',
 	'wikia-editor-modules-autolinker-loading' => 'Fetching list of pages to link to...',
 	'wikia-editor-modules-autolinker-button' => 'Perform auto linking',
-	'wikia-editor-modules-autolinker-completed' => '$1 link(s) has been added to this article',
+	'wikia-editor-modules-autolinker-completed' => '$1 link(s) have been added to this article: $2',
 );
