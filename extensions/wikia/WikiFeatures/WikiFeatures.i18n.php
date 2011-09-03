971c9dc99b597df0592f387a3ffb24f9ab89ab88
siebrand <siebrand@wikia-inc.com>
2011-09-03 22:09:26 +0000

    Tweak 'wikifeatures-feature-description-wgEnableTopListsExt'
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41624 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFeatures/WikiFeatures.i18n.php
+++ extensions/wikia/WikiFeatures/WikiFeatures.i18n.php
@@ -34,7 +34,7 @@ We know they still have their kinks, and we would love to receive your feedback 
 	'wikifeatures-feedback-comment-label' => 'Comment',
 	// feature message list
 	'wikifeatures-feature-heading-wgEnableTopListsExt' => 'Top 10 Lists',
-	'wikifeatures-feature-description-wgEnableTopListsExt' => 'Top 10 Lists allow anyone on your wiki to create a votable list. They are a good way to get your junior editors to interact with the wiki. Create a new list at Special:CreateTopList',
+	'wikifeatures-feature-description-wgEnableTopListsExt' => 'Top 10 Lists allow anyone on your wiki to create a votable list. They are a good way to get your junior editors to interact with the wiki. [[Special:CreateTopList|Create a new list]]!',
 	'wikifeatures-feature-heading-wgEnableAchievementsExt' => 'Achievements',
 	'wikifeatures-feature-description-wgEnableAchievementsExt' => 'Achievements will help your wiki to grow by incentivizing editors to contribute a variety of content. You can create your own challenges that are unique to your wiki, and even upload your own images for the badges.',
 	'wikifeatures-feature-heading-wgEnableArticleCommentsExt' => 'Article Comments',
