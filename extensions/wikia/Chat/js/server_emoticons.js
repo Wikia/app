cc74ce742ecb9c9ca6f99b25092f5eaeb9229249
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-05 02:12:10 +0000

    Emoticon debugging code.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41632 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat/js/server_emoticons.js
+++ extensions/wikia/Chat/js/server_emoticons.js
@@ -62,7 +62,7 @@ if(typeof EmoticonMapping === 'undefined'){
 			"http://images.wikia.com/lyricwiki/images/6/67/Smile001.gif": [":)", ":-)", "(smile)"],
 			"http://images3.wikia.nocookie.net/__cb20100822133322/lyricwiki/images/d/d8/Sad.png": [":(", ":-(", ":|"],
 		};
-		
+
 		/**
 		 * Convert our specific wikitext format into the hash of emoticons settings.  Overwrites all
 		 * of the existing settings (instead of merging).
@@ -73,15 +73,19 @@ if(typeof EmoticonMapping === 'undefined'){
 			var emoticonArray = wikiText.split("\n");
 			var currentKey = '';
 
+			// TODO: FIXME: Rewrite this to use regexes so that we don't require the space after the asterisks (because that's not needed in normal wikitext).
 			// Loop through array, construct object
+console.log("Loading emoticon mapping...");
 			for(var i=0; i<emoticonArray.length; i++) {
 				if (emoticonArray[i].indexOf('* ') == 0) {
 					var url = emoticonArray[i].substr(2);
 					self._settings[url] = [];
 					currentKey = url;
+console.log("  " + url + "...\n");
 				} else if (emoticonArray[i].indexOf('** ') == 0) {
 					var glyph = emoticonArray[i].substr(3);
 					self._settings[currentKey].push(glyph);
+console.log("       " + glyph + "\n");
 				}
 			}
 		
@@ -120,6 +124,122 @@ if(typeof EmoticonMapping === 'undefined'){
 			}
 			
 			return self._regexes;
+		};
+		
+		/**
+		 * Loads a reasonable default of emoticons.
+		 *
+		 * TODO: Replace with loading Community Wiki's "MediaWiki:Emoticons" at startup and using that as the default
+		 * until the client load's their appropriate wikitext.
+		 */
+		this.loadDefault = function(){
+			self.loadFromWikiText('* http://images2.wikia.nocookie.net/__cb20110904035827/central/images/7/79/Emoticon_angry.png\
+** (angry)\
+** >:O\
+** >:-O\
+* http://images1.wikia.nocookie.net/__cb20110904035827/central/images/a/a3/Emoticon_argh.png\
+** (argh)\
+* http://images3.wikia.nocookie.net/__cb20110904035827/central/images/e/ec/Emoticon_BA.png\
+** (ba)\
+* http://images2.wikia.nocookie.net/__cb20110904035827/central/images/7/76/Emoticon_batman.png\
+** (batman)\
+* http://images1.wikia.nocookie.net/__cb20110904035828/central/images/e/e2/Emoticon_blush.png\
+** (blush)\
+** :]\
+** :-]\
+* http://images2.wikia.nocookie.net/__cb20110904040557/central/images/e/ed/Emoticon_books.png\
+** (books)\
+* http://images4.wikia.nocookie.net/__cb20110904035828/central/images/c/cd/Emoticon_confused.png\
+** (confused)\
+** :S\
+** :-S\
+* http://images2.wikia.nocookie.net/__cb20110904035828/central/images/2/28/Emoticon_content.png\
+** (content)\
+* http://images4.wikia.nocookie.net/__cb20110904035828/central/images/a/a2/Emoticon_cool.png\
+** (cool)\
+** B)\
+** B-)\
+* http://images4.wikia.nocookie.net/__cb20110904035828/central/images/1/16/Emoticon_crying.png\
+** (crying)\
+** ;-(\
+** ;(\
+** :\'(\
+* http://images2.wikia.nocookie.net/__cb20110904040556/central/images/7/7d/Emoticon_fingers_crossed.png\
+** (fingers crossed)\
+** (yn)\
+* http://images1.wikia.nocookie.net/__cb20110904040557/central/images/0/07/Emoticon_frustrated.png\
+** (frustrated)\
+** >:-/\
+** >:/\
+* http://images3.wikia.nocookie.net/__cb20110904040557/central/images/7/78/Emoticon_ghost.png\
+** (ghost)\
+** (swayze)\
+* http://images1.wikia.nocookie.net/central/images/3/31/Emoticon_happy.png\
+** (happy)\
+** :-)\
+** :)\
+* http://images2.wikia.nocookie.net/__cb20110904040557/central/images/1/1e/Emoticon_heart.png\
+** (heart)\
+** (h)\
+** <3\
+* http://images1.wikia.nocookie.net/__cb20110904040557/central/images/7/7b/Emoticon_hmm.png\
+** (hmm)\
+* http://images1.wikia.nocookie.net/__cb20110904040557/central/images/b/b5/Emoticon_indifferent.png\
+** (indifferent)\
+** :/\
+** :-/\
+* http://images1.wikia.nocookie.net/__cb20110904040558/central/images/a/ac/Emoticon_laughing.png\
+** (laughing)\
+** :D\
+** :-D\
+* http://images2.wikia.nocookie.net/__cb20110904041805/central/images/c/c1/Emoticon_mario.png\
+** (mario)\
+* http://images3.wikia.nocookie.net/__cb20110904041806/central/images/4/43/Emoticon_moon.png\
+** (moon)\
+* http://images3.wikia.nocookie.net/__cb20110904041806/central/images/1/1d/Emoticon_ninja.png\
+** (ninja)\
+* http://images3.wikia.nocookie.net/__cb20110904041806/central/images/9/92/Emoticon_nintendo.png\
+** (nintendo)\
+* http://images2.wikia.nocookie.net/__cb20110904041806/central/images/4/40/Emoticon_no.png\
+** (no)\
+** (n)\
+* http://images3.wikia.nocookie.net/__cb20110904041806/central/images/2/2d/Emoticon_owl.png\
+** (owl)\
+* http://images1.wikia.nocookie.net/__cb20110904041806/central/images/c/c2/Emoticon_pacmen.png\
+** (pacmen)\
+** (pacman)\
+** (redghost)\
+* http://images1.wikia.nocookie.net/__cb20110904041806/central/images/5/52/Emoticon_peace.png\
+** (peace)\
+* http://images3.wikia.nocookie.net/__cb20110904041806/central/images/7/74/Emoticon_pirate.png\
+** (pirate)\
+* http://images1.wikia.nocookie.net/__cb20110904041806/central/images/8/8a/Emoticon_sad.png\
+** (sad)\
+** :(\
+** :-(\
+* http://images1.wikia.nocookie.net/__cb20110904041912/central/images/c/c2/Emoticon_silly.png\
+** (silly)\
+** :P\
+** :-P\
+* http://images4.wikia.nocookie.net/__cb20110904041912/central/images/a/a9/Emoticon_stop.png\
+** (stop)\
+* http://images2.wikia.nocookie.net/__cb20110904041913/central/images/a/a2/Emoticon_unamused.png\
+** (unamused)\
+** :|\
+** :-|\
+* http://images1.wikia.nocookie.net/__cb20110904041913/central/images/d/dc/Emoticon_walter.png\
+** (walter)\
+* http://images1.wikia.nocookie.net/__cb20110904041913/central/images/d/dc/Emoticon_walter.png\
+** (wikia)\
+** (w)\
+* http://images1.wikia.nocookie.net/__cb20110904041913/central/images/8/87/Emoticon_wink.png\
+** (wink)\
+** ;)\
+** ;-)\
+* http://images2.wikia.nocookie.net/__cb20110904041913/central/images/1/1c/Emoticon_yes.png\
+** (yes)\
+** (y)\
+');
 		};
 	};
 }
