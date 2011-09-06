9a2bc4cdb6b670bd6401baebb2b299262f28d199
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-06 07:02:25 +0000

    BugzId 11047 - Making Chat rail module behave better during failover.  Did the following during read-only: - TECH FIX: Made all API calls to Node be skipped in wgReadOnly because it's possible the read-only is due to a complete network failure in the primary datacenter (kinda weird assumption, but we don't have any other wiki-vars to express this specific type of problem). This fix prevents any sort of hanging while waiting for responses from a server we're not going to be able to reach. - INTERFACE FIX: Made the entry-point not display the chatters and the chat button during failover, but rather it displays an i18n message that explains that Chat is not available in read-only mode.
    Keep in mind that chat MAY work in read-only depending on the failure since it doesn't need to write to the production databases for anything such as promotions and kick-bans. By disabling the entry-point we're not advertising it as working... if it works & users go to it via the URL, then that's bonus for them (but they're playing in the Wild West w/no kickbans ;) ).  We may change our mind on whether we want to completely disable it, but I think our hardcore users will appreciate being able to chat during read-only if they are lucky enough that it still works during the particular failure.
    
    NOTE: More testing of this pending... may be some followup commits.
    
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41662 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat/ChatRailModule.class.php
+++ extensions/wikia/Chat/ChatRailModule.class.php
@@ -40,61 +40,68 @@ class ChatRailModule extends Module {
 	 * Render module contents - loaded via ajax only for freshness
 	 */
 	public function executeContents(){
-		global $wgUser, $wgSitename, $wgOut, $wgExtensionsPath, $wgContLang;
+		global $wgUser, $wgSitename, $wgOut, $wgExtensionsPath, $wgContLang, $wgReadOnly;
 		wfProfileIn( __METHOD__ );
-
-		// Main variables
-		$this->linkToSpecialChat = SpecialPage::getTitleFor("Chat")->escapeLocalUrl();
-		$this->windowFeatures = $this->getWindowFeatures();
-		$this->chatHeadline = wfMsg('chat-headline', $wgSitename);
-		$this->isLoggedIn = $wgUser->isLoggedIn();
-		if($wgUser->isLoggedIn()){
-			$this->profileAvatar = AvatarService::renderAvatar($wgUser->getName(), ChatRailModule::AVATAR_SIZE);
-		} else {
-			$this->profileAvatar = "";
-		}
-
-		// List of other people in chat
-		$this->totalInRoom = 0;
-		$roomName = $roomTopic = ""; // just needed for pass-by-reference... will be ignored
-		$roomId = NodeApiClient::getDefaultRoomId($roomName, $roomTopic); // get id of default chat for the wiki
 		
-		// Gets array of users currently in chat to populate rail module and user stats menus
-		$chatters = NodeApiClient::getChatters($roomId, ChatRailModule::MAX_CHATTERS, $this->totalInRoom);
-		for ($i = 0; $i < count($chatters); $i++) {
-			// avatar
-			$chatters[$i]['avatarUrl'] = AvatarService::getAvatarUrl($chatters[$i]['username'], ChatRailModule::AVATAR_SIZE);
-			
-			// get stats for edit count and member since
-			$user = User::newFromName($chatters[$i]['username']);
-			if(is_object($user)){
-				$userStatsService = new UserStatsService($user->getId());
-				$stats = $userStatsService->getStats();
-
-				// edit count
-				$chatters[$i]['editCount'] = $wgContLang->formatNum((int) $stats['edits']);
-				
-				// member since
-				$chatters[$i]['showSince'] = $chatters[$i]['editCount'] != 0;
-				$chatters[$i]['since'] = date("M Y", strtotime($stats['date']));
-				
-				// profile page
-				$chatters[$i]['profileUrl'] = Title::makeTitle( NS_USER, $chatters[$i]['username'] )->getFullURL();
-				
-				// contribs page
-				$chatters[$i]['contribsUrl'] = SpecialPage::getTitleFor( 'Contributions', $chatters[$i]['username'] )->getFullURL();
+		// Since there is no chat in the backup datacenter yet, if we're in read-only, disable the entry-point.
+		// Depending on the actual failure, chat may or may not work, but the user would have to get there via URL which
+		// is essentially saying that they accept some risk since they're going somewhere our interface doesn't direct them to.
+		// If it's just, eg: a database problem, then they may get lucky and be able to use most of the chat features (kickbanning wouldn't work, etc.).
+		$this->wgReadOnly = $wgReadOnly;
+		if(empty($wgReadOnly)){
+			// Main variables
+			$this->linkToSpecialChat = SpecialPage::getTitleFor("Chat")->escapeLocalUrl();
+			$this->windowFeatures = $this->getWindowFeatures();
+			$this->chatHeadline = wfMsg('chat-headline', $wgSitename);
+			$this->isLoggedIn = $wgUser->isLoggedIn();
+			if($wgUser->isLoggedIn()){
+				$this->profileAvatar = AvatarService::renderAvatar($wgUser->getName(), ChatRailModule::AVATAR_SIZE);
+			} else {
+				$this->profileAvatar = "";
 			}
-		}
-		$this->chatters = $chatters;
 
-		// Button setup
-		$this->buttonIconUrl = $wgExtensionsPath .'/wikia/Chat/images/chat_icon.png';
-		if ( !empty($this->totalInRoom) ) {
-			$this->buttonText = wfMsg('chat-join-the-chat');
-		} else {
-			$this->buttonText = wfMsg('chat-start-a-chat');		
+			// List of other people in chat
+			$this->totalInRoom = 0;
+			$roomName = $roomTopic = ""; // just needed for pass-by-reference... will be ignored
+			$roomId = NodeApiClient::getDefaultRoomId($roomName, $roomTopic); // get id of default chat for the wiki
+			
+			// Gets array of users currently in chat to populate rail module and user stats menus
+			$chatters = NodeApiClient::getChatters($roomId, ChatRailModule::MAX_CHATTERS, $this->totalInRoom);
+			for ($i = 0; $i < count($chatters); $i++) {
+				// avatar
+				$chatters[$i]['avatarUrl'] = AvatarService::getAvatarUrl($chatters[$i]['username'], ChatRailModule::AVATAR_SIZE);
+				
+				// get stats for edit count and member since
+				$user = User::newFromName($chatters[$i]['username']);
+				if(is_object($user)){
+					$userStatsService = new UserStatsService($user->getId());
+					$stats = $userStatsService->getStats();
+
+					// edit count
+					$chatters[$i]['editCount'] = $wgContLang->formatNum((int) $stats['edits']);
+					
+					// member since
+					$chatters[$i]['showSince'] = $chatters[$i]['editCount'] != 0;
+					$chatters[$i]['since'] = date("M Y", strtotime($stats['date']));
+					
+					// profile page
+					$chatters[$i]['profileUrl'] = Title::makeTitle( NS_USER, $chatters[$i]['username'] )->getFullURL();
+					
+					// contribs page
+					$chatters[$i]['contribsUrl'] = SpecialPage::getTitleFor( 'Contributions', $chatters[$i]['username'] )->getFullURL();
+				}
+			}
+			$this->chatters = $chatters;
+
+			// Button setup
+			$this->buttonIconUrl = $wgExtensionsPath .'/wikia/Chat/images/chat_icon.png';
+			if ( !empty($this->totalInRoom) ) {
+				$this->buttonText = wfMsg('chat-join-the-chat');
+			} else {
+				$this->buttonText = wfMsg('chat-start-a-chat');		
+			}
+			$this->chatClickAction = "window.open('{$this->linkToSpecialChat}', 'wikiachat', '{$this->windowFeatures}')";
 		}
-		$this->chatClickAction = "window.open('{$this->linkToSpecialChat}', 'wikiachat', '{$this->windowFeatures}')";
 
 		wfProfileOut( __METHOD__ );
 	}
--- extensions/wikia/Chat/templates/ChatRail_Contents.php
+++ extensions/wikia/Chat/templates/ChatRail_Contents.php
@@ -1,58 +1,66 @@
 <div class="chat-live"><?= wfMsg('chat-live') ?></div>
 <h1><?= $chatHeadline ?></h1>
+<?php
 
-<? //print_r($chatters) ?>
+if(!empty($wgReadOnly)){
+	// For a variety of reasons, we don't display the entry point when in read-only mode.  See BugzId 11047 for more info.
+	print "<em>".wfMsg('chat-read-only')."</em>";
+} else { ?>
+	<? //print_r($chatters) ?>
 
-<? if ( !empty($totalInRoom) ) { ?>
-<div class="chat-whos-here">
-	<h2><?= wfMsg('chat-whos-here', $totalInRoom) ?></h2>
-	<?php if(!empty($chatters)){ ?>
-	<ul>
-		<? foreach($chatters as $chatter) { ?>
-			<li>
-				<img src='<?= $chatter['avatarUrl'] ?>' class='avatar'/>
-				<div class="UserStatsMenu">
-					<div class="info">
-						<img src="<?= $chatter['avatarUrl'] ?>">
-						<span class="username"><?= $chatter['username'] ?></span>
-						<span class="edits"><?= wfMsg('chat-edit-count', $chatter['editCount']) ?></span>
-						<?php if($chatter['showSince']): ?>
-							<span class="since"><?= wfMsg('chat-member-since', $chatter['since']) ?></span>
-						<?php endif; ?>
-					</div>
-					<ul class="actions">
-						<li class="profile"><a href="<?= $chatter['profileUrl'] ?>">User Profile</a></li>
-						<li class="contribs"><a href="<?= $chatter['contribsUrl'] ?>">Contributions</a></li>
-					</ul>
-				</div>				
-			</li>
-		<? } ?>
-	</ul>
-	<?php } ?>
-</div>
-<? } ?>
+	<? if ( !empty($totalInRoom) ) { ?>
+	<div class="chat-whos-here">
+		<h2><?= wfMsg('chat-whos-here', $totalInRoom) ?></h2>
+		<?php if(!empty($chatters)){ ?>
+		<ul>
+			<? foreach($chatters as $chatter) { ?>
+				<li>
+					<img src='<?= $chatter['avatarUrl'] ?>' class='avatar'/>
+					<div class="UserStatsMenu">
+						<div class="info">
+							<img src="<?= $chatter['avatarUrl'] ?>">
+							<span class="username"><?= $chatter['username'] ?></span>
+							<span class="edits"><?= wfMsg('chat-edit-count', $chatter['editCount']) ?></span>
+							<?php if($chatter['showSince']): ?>
+								<span class="since"><?= wfMsg('chat-member-since', $chatter['since']) ?></span>
+							<?php endif; ?>
+						</div>
+						<ul class="actions">
+							<li class="profile"><a href="<?= $chatter['profileUrl'] ?>">User Profile</a></li>
+							<li class="contribs"><a href="<?= $chatter['contribsUrl'] ?>">Contributions</a></li>
+						</ul>
+					</div>				
+				</li>
+			<? } ?>
+		</ul>
+		<?php } ?>
+	</div>
+	<? } ?>
 
-<div class="chat-join">
-	<?= $profileAvatar ?>
-	<button onclick="onChatButtonClick()"<?= ($isLoggedIn?"":" class='loginToChat'"); ?>>
-		<img src="<?= $buttonIconUrl ?>">
-		<?= $buttonText ?>
-	</button>
-</div>
+	<div class="chat-join">
+		<?= $profileAvatar ?>
+		<button onclick="onChatButtonClick()"<?= ($isLoggedIn?"":" class='loginToChat'"); ?>>
+			<img src="<?= $buttonIconUrl ?>">
+			<?= $buttonText ?>
+		</button>
+	</div>
 
-<script>
-	function onChatButtonClick(){
-		var isLoggedIn = <?= ($isLoggedIn ? "true" : "false") ?>;
-		var message = 'protected'; // gives the 'login required to perform this action' message at the top of the login box
-		if(isLoggedIn){
-			<?= $chatClickAction ?>
-		} else {
-			showComboAjaxForPlaceHolder(false, "", function() {
-				AjaxLogin.doSuccess = function() {
-					$('.modalWrapper').children().not('.close').not('.modalContent').not('h1').remove();
-					$('.modalContent').load(wgServer + wgScript + '?action=ajax&rs=moduleProxy&moduleName=ChatRail&actionName=AnonLoginSuccess&outputType=html');
-				}
-			}, false, message); // show the 'login required for this action' message.
-		}
-	} // end onChatButtonClick()
-</script>
+	<script>
+		function onChatButtonClick(){
+			var isLoggedIn = <?= ($isLoggedIn ? "true" : "false") ?>;
+			var message = 'protected'; // gives the 'login required to perform this action' message at the top of the login box
+			if(isLoggedIn){
+				<?= $chatClickAction ?>
+			} else {
+				showComboAjaxForPlaceHolder(false, "", function() {
+					AjaxLogin.doSuccess = function() {
+						$('.modalWrapper').children().not('.close').not('.modalContent').not('h1').remove();
+						$('.modalContent').load(wgServer + wgScript + '?action=ajax&rs=moduleProxy&moduleName=ChatRail&actionName=AnonLoginSuccess&outputType=html');
+					}
+				}, false, message); // show the 'login required for this action' message.
+			}
+		} // end onChatButtonClick()
+	</script>
+<?php
+}
+?>
