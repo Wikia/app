e098b39b47c78836f139db0db7a15feb0dd946da
siebrand <siebrand@wikia-inc.com>
2011-09-09 23:07:27 +0000

    Fix break.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41846 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserRenameTool/SpecialRenameuser.i18n.php
+++ extensions/wikia/UserRenameTool/SpecialRenameuser.i18n.php
@@ -11,7 +11,7 @@ $messages['en'] = array(
 	'userrenametool' => "Change a user's name",
 	'renameuser' => 'Rename user',
 	'userrenametool-warning' => '<strong>Please read the following information carefully</strong>:<p>Before renaming a user, please make sure <strong>all the information is correct</strong>, and ensure that <strong>the user knows it may take some time to complete</strong>.
-<br/>Please be aware that due to some external factors the first part of the process <strong>could result in a blank page</strong>, that doesn\'t mean the process won\'t be completed correctly.</p><p>You can track the progress of the process through [[Special:Stafflog|Staff log]], also <strong>the system will send an email to you as the whole rename procedure will be completed</strong>.</p>',
+<br />Please be aware that due to some external factors the first part of the process <strong>could result in a blank page</strong>, that doesn\'t mean the process won\'t be completed correctly.</p><p>You can track the progress of the process through [[Special:Stafflog|Staff log]], also <strong>the system will send an email to you as the whole rename procedure will be completed</strong>.</p>',
 	'userrenametool-desc' => 'Adds a [[Special:UserRenameTool|special page]] to rename a user (need \'\'renameuser\'\' right) and process all the related data',
 	'userrenametool-old' => 'Current username:',
 	'userrenametool-new' => 'New username:',
