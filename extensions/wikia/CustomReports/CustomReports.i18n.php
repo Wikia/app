234fd7b8f397fc0766a214f5f294203a773c2d5d
saipetch <saipetch@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-12 21:23:11 +0000

    bugid 7154: add special page (CustomReports) - show data for founder emails, new wikis per day, all emails.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@41879 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/CustomReports/CustomReports.i18n.php
@@ -0,0 +1,36 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'report-xaxis' => 'Date',
+	
+	'report-name-new_wikis' => 'New Wikis by Day',
+	'report-yaxis-new_wikis' => 'Wikis',
+	
+	'report-name-founderemails' => 'Founder Emails',
+	'report-name-founderemails_sent' => 'Founder Emails: Emails Sent by Day',
+	'report-yaxis-founderemails_sent' => 'Emails Sent',
+	'report-name-founderemails_opens' => 'Founder Emails: Emails Opened by Day',
+	'report-yaxis-founderemails_opens' => 'Emails Opened',
+	'report-name-founderemails_clicks' => 'Founder Emails: Emails Clicked by Day',
+	'report-yaxis-founderemails_clicks' => 'Clicks',
+	'report-name-founderemails_clicks_per_sent' => 'Founder Emails: Click Rate by Day',
+	'report-yaxis-founderemails_clicks_per_sent' => 'Clicks/Sent (%25)',
+	'report-name-founderemails_opens_per_sent' => 'Founder Emails: Open Rate by Day',
+	'report-yaxis-founderemails_opens_per_sent' => 'Opens/Sent (%25)',
+	
+	'report-name-allemails' => 'All Emails',
+	'report-name-allemails_sent' => 'All Emails: Emails Sent by Day',
+	'report-yaxis-allemails_sent' => 'Emails Sent',
+	'report-name-allemails_opens' => 'All Emails: Emails Opened by Day',
+	'report-yaxis-allemails_opens' => 'Emails Opened',
+	'report-name-allemails_clicks' => 'All Emails: Emails Clicked by Day',
+	'report-yaxis-allemails_clicks' => 'Clicks',
+	'report-name-allemails_clicks_per_sent' => 'All Emails: Click Rate by Day',
+	'report-yaxis-allemails_clicks_per_sent' => 'Clicks/Sent (%25)',
+	'report-name-allemails_opens_per_sent' => 'All Emails: Open Rate by Day',
+	'report-yaxis-allemails_opens_per_sent' => 'Opens/Sent (%25)',
+	
+);
+
