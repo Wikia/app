4a961b1112c74c943070adf300dfc9e905e5ab94
hyun <hyun@wikia-inc.com>
2011-09-14 22:19:59 +0000

    WikiFeatures - WikiaLabs redirect to WikiFeatures
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42021 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFeatures/WikiFeatures.setup.php
+++ extensions/wikia/WikiFeatures/WikiFeatures.setup.php
@@ -10,12 +10,14 @@ $app = F::app();
 //classes
 $app->registerClass('WikiFeaturesSpecialController', $dir . 'WikiFeaturesSpecialController.class.php');
 $app->registerClass('WikiFeaturesHelper', $dir . 'WikiFeaturesHelper.class.php');
+$app->registerClass('WikiaLabsSpecialController', $dir . 'WikiaLabsSpecialController.class.php');
 
 // i18n mapping
 $wgExtensionMessagesFiles['WikiFeatures'] = $dir . 'WikiFeatures.i18n.php';
 
 // special pages
 $app->registerSpecialPage('WikiFeatures', 'WikiFeaturesSpecialController');
+$app->registerSpecialPage('WikiaLabs', 'WikiaLabsSpecialController');
 
 $wgAvailableRights[] = 'wikifeatures';
 
