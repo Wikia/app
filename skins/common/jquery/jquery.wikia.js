aff3ae61bba4ed9f3cab2c071ab7b925e3214b9a
will <will@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-15 09:21:54 +0000

    simulate cookies in JS Local Storage
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42068 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/jquery/jquery.wikia.js
+++ skins/common/jquery/jquery.wikia.js
@@ -884,3 +884,52 @@ $(function() {
 		}
 	}
 });
+
+/*****************************************************************************
+ ******** LOCAL STORAGE WITH EXPIRATIONS (cookie simulation)   ***************
+ ******** NOTE: the simulation is not a true cookie. Unlike    ***************
+ ******** real cookies, the simulation requires an expiration. ***************
+ *****************************************************************************/
+
+$.cookieStorage = new $.store();
+$.cookieStorage.get = function(key) {
+	var value = this.driver.get( key );
+	if (!value) {
+		return;
+	}
+	cookie = this.driver.encodes ? value : this.unserialize( value );
+	value = null; 
+	d = new Date();
+	if (cookie.expires >= d.getTime()) {
+		value = cookie.value;
+	}
+	else {
+		this.del(key);
+	}
+	return value;
+};
+
+// Set cookie. Expires logic borrowed from a jquery plugin. 
+// NOTE: expires is either a date object, or a number of *milli*seconds until the cookie expires 
+$.cookieStorage.set = function(key, value, expires) {
+	var d;
+	if (typeof expires == 'number' || (typeof expires == 'object' && expires.toUTCString)) {
+		if (typeof expires == 'number') {
+			d = new Date();
+			d.setTime(d.getTime() + (expires));
+		} else {
+			d = expires;
+		}
+	}
+	else {
+		return;
+	}
+	var cookie = { value: value, expires: d.getTime() };
+	this.driver.set( key, this.driver.encodes ? cookie : this.serialize( cookie ) );
+
+	return value;
+};
+
+/*****************************************************************************
+ ******** END LOCAL STORAGE WITH EXPIRATIONS *********************************
+ *****************************************************************************/
