0075fa402919e7ebc842eadb6f9dceb69134f4d4
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-18 03:43:27 +0000

    Initial commit of ApiExplorer.  It is in MediaWiki style instead of Nirvana style because it has no specific needs that aren't portable (and seems like the type of thing the community would commit-back to for us).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42203 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ApiExplorer/SpecialApiExplorer.i18n.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Internationalisation file for extension ApiExplorer.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/** English
+ * @author Sean Colombo
+ */
+$messages['en'] = array(
+	'apiexplorer' => 'API Explorer',
+	'apiexplorer-desc' => 'Page for interactively exploring the documentation of the live version of the MediaWiki API running on this installation.',
+);
+
+/** Message documentation (Message documentation)
+ */
+$messages['qqq'] = array(
+);
