37f2b99f9972cb2dddb2a7d89d1bfae594a4f4c5
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-18 04:03:59 +0000

    ApiExplorer: fix the name of the i18n file, added the css file and created the initial skeleton of the JS app.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42204 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ApiExplorer/ApiExplorer.i18n.php
+++ extensions/wikia/ApiExplorer/ApiExplorer.i18n.php
@@ -13,9 +13,14 @@ $messages = array();
 $messages['en'] = array(
 	'apiexplorer' => 'API Explorer',
 	'apiexplorer-desc' => 'Page for interactively exploring the documentation of the live version of the MediaWiki API running on this installation.',
+	'apiexplorer-intro' => 'This page shows documentation for the current wiki\'s API. Please click on a function below to see more info about it.',
+	
+	'apiexplorer-loading' => 'Loading functions...',
 );
 
 /** Message documentation (Message documentation)
  */
 $messages['qqq'] = array(
+	'apiexplorer-desc' => "Description of the extension (for Special:Version, etc.).",
+	'apiexplorer-intro' => "The intro at the top of the page to acquiant new users with what the page does."
 );
--- extensions/wikia/ApiExplorer/apiExplorer.js
+++ extensions/wikia/ApiExplorer/apiExplorer.js
@@ -1,3 +1,36 @@
+/**
+ * @author Sean Colombo
+ * @date 20110917
+ *
+ */
 
+$().log("== API Explorer ==");
 
-console.log("Loaded API Explorer Javascript :)");
+if(typeof ApiExplorer == "undefined"){
+	ApiExplorer = function(){
+		var self = this;
+
+		/**
+		 * Called on document ready to make the first call to the API to build the list of available functions.
+		 */
+		this.run = function(){
+			$().log("Starting API Explorer...");
+			
+			// Show the loading indicator until the API request has returned & the new page-content is set-up.
+			$('#apEx_loading').show();
+
+			// TODO: MAKE THE REQUEST TO THE API FOR THE LIST OF FUNCTIONS.
+			// TODO: MAKE THE REQUEST TO THE API FOR THE LIST OF FUNCTIONS.
+		
+			// TODO: Add the list of functions to the DOM.
+			// TODO: Add the list of functions to the DOM.
+			//$('#apEx_loading').hide();
+		
+		};
+	};
+}
+
+$(document).ready(function(){
+	var apEx = new ApiExplorer();
+	apEx.run();
+});
