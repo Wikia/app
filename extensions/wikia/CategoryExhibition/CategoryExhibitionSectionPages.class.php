4305e8e683c0cd2c67ec61df5708654f0bac19ac
jakub <jakub@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-20 17:41:17 +0000

    BugId:3515
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42295 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CategoryExhibition/CategoryExhibitionSectionPages.class.php
+++ extensions/wikia/CategoryExhibition/CategoryExhibitionSectionPages.class.php
@@ -11,7 +11,7 @@ class CategoryExhibitionSectionPages extends CategoryExhibitionSection {
 	public function getSectionHTML(){
 		global $wgCategoryExhibitionPagesSectionRows, $wgContentNamespaces;
 		$this->loadPaginationVars();
-		$oTmpl = $this->getTemplateForNameSpace( $wgContentNamespaces, $wgCategoryExhibitionPagesSectionRows * 4 );
+		$oTmpl = $this->getTemplateForNameSpace( array( 500, NS_FILE, NS_VIDEO, NS_CATEGORY ), $wgCategoryExhibitionPagesSectionRows * 4, true );
 		return $this->executeTemplate( $oTmpl );
 	}
 
@@ -21,7 +21,11 @@ class CategoryExhibitionSectionPages extends CategoryExhibitionSection {
 		$this->isFromAjax = true;
 		$this->paginatorPosition = $paginatorPosition;
 		$this->sUrl = $sUrl;
-		$oTmpl = $this->getTemplateForNameSpace( $wgContentNamespaces, $wgCategoryExhibitionPagesSectionRows * 4 );
+		$oTmpl = $this->getTemplateForNameSpace( array( 500, NS_FILE, NS_VIDEO, NS_CATEGORY ), $wgCategoryExhibitionPagesSectionRows * 4, true );
 		return $this->executeTemplate( $oTmpl );
+	}
+
+	protected function getTitleForElement( $oTitle ){
+		return $oTitle->getPrefixedText();
 	}
 }
