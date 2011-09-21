b310bb37da68c45e9350097da535276843875fb6
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-21 13:15:46 +0000

    folder fill
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42331 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/hacks/PhotoPop/shared/modules/templates.js
@@ -0,0 +1,45 @@
+var exports = exports || {};
+
+define.call(exports, {
+
+	mainPage: "<div id='wrapper'>\
+			<div id='logoWrapper'>\
+				<div id='photopop_logo'>\
+					<img src='{{#image}}PHOTOPOP_LOGO{{/image}}'/><br/>\
+					<img src='{{#image}}POWERED_BY_LOGO{{/image}}'/>\
+				</div>\
+			</div>\
+			<div id='playWrapper'>\
+				<a href='{{#url}}playButtonUrl{{/url}}'><img src='{{#image}}buttonSrc_play{{/image}}'/></a>\
+			</div>\
+			<div id='buttonWrapper'>\
+				<div id='button_scores'>\
+					<img src='{{#image}}buttonSrc_scores{{/image}}'/>\
+				</div>\
+			<div id='button_tutorial'>\
+				<a href='{{#url}}tutorialButtonUrl{{/url}}'><img src='{{#image}}buttonSrc_tutorial{{/image}}'/></a>\
+			</div>\
+			<div id='button_volume'>\
+				<img class='on' src='{{#image}}buttonSrc_volumeOn{{/image}}'/>\
+				<img class='off' src='{{#image}}buttonSrc_volumeOff{{/image}}'/>\
+			</div>\
+		</div>\
+	</div>",
+	
+	selectorScreen: "<div id='sliderWrapper'>\
+				<div id='closeButton'>\
+					<a href='{{backHomeUrl}}'><img src='{{#image}}buttonSrc{{/image}}'/></a>\
+				</div>\
+				<div class='sliderContent' data-scroll='x'>\
+					{{#games}}\
+						<div class='gameIcon' data-gameurl='{{gameUrl}}'>\
+							<img src ='{{#image}}gameicon_{{name}}{{/image}}'><br/>\
+							<div class='gameName'>\
+								{{gameName}}\
+							</div>\
+						</div>\
+					{{/games}}\
+				</div>\
+			</div>"
+
+});
