31bb95c006b4c844fc9e7c6430621d97cc92c25d
macbre <macbre@wikia-inc.com>
2011-09-26 22:36:48 +0000

    WikiNav: update and optimize sprite.png in trunk
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42475 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/wgCacheBuster.php
+++ includes/wikia/wgCacheBuster.php
@@ -3,6 +3,4 @@
  * This file contains just the wgCacheBuster so that it can be included from
  * various code-paths, some of which don't load the rest of the MediaWiki stack.
  */
-
-
-$wgCacheBuster = '42448';
+$wgCacheBuster = '42474';
