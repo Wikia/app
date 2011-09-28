8fcaa84396177feaaf88280444167d3d7af8c00f
jolek <jolek@wikia-inc.com>
2011-09-28 17:55:12 +0000

    PhotoPop: further improvements
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42606 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/hacks/PhotoPop/PhotoPopAppCacheController.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * PhotoPop AppCache manifest generator
+ * 
+ * @authore Federico "Lox" Lucignano <federico(at)wikia-inc.com>
+ * $author Jakub Olek
+ */
+class PhotoPopAppCacheController extends WikiaController {
+	const CONTENT_TYPE = 'text/cache-manifest';
+	const MANIFEST_VARNISH_CACHE_TIME = 86400;//24h
+	
+	public function serveManifest(){
+		if ( $this->response->getFormat() == WikiaResponse::FORMAT_HTML ) {
+			$this->response->setContentType( self::CONTENT_TYPE );
+			$this->response->setCacheValidity( self::MANIFEST_VARNISH_CACHE_TIME, self::MANIFEST_VARNISH_CACHE_TIME, array( WikiaResponse::CACHE_TARGET_VARNISH ) );
+		}
+		
+		$files = array();
+		$basePath = "extensions/wikia/hacks/PhotoPop/";
+		$dirs = array('shared/audio', 'shared/images');
+		
+		foreach( $dirs as $dir ) {
+			if ( $handle = opendir( $basePath . $dir ) ) {
+				while ( false !== ( $file = readdir( $handle ) ) ) {
+					if( $file != "." && $file != ".." ) {
+						array_push( $files, $dir . "/" . $file);
+					}
+				}
+				closedir( $handle );
+			}
+		}
+		
+		
+		$this->cacheVersion = $this->wg->CacheBuster;//$this->wg->StyleVersion;
+		$this->cacheFiles = implode("\n",$files);
+		$this->freshFiles = '*';
+	}
+}
