88bdb54122c4c69f0acab1fd861110ab7f7fcf99
saipetch <saipetch@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-29 19:57:42 +0000

    bugid 11787: update setUserOptions script to allow to set user prepferences for founders.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42676 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/setUserOptions.php
+++ maintenance/wikia/setUserOptions.php
@@ -10,38 +10,69 @@
  *
  */
 
-//variables
-$limitUsers = '';	//WHERE in SQL
-$preferences = array('adoptionmails' => '1');	//pairs key=value
-///
-$optionsWithArgs = array( "from" );
+$optionsWithArgs = array( "from", "to" );
 ini_set('include_path', dirname(__FILE__) . '/..' );
 require_once('commandLine.inc');
+	
+// get preferences
+$default_keys = array('conf', 'help', 'force', 'quiet', 'from', 'to', 'founder');
+$preferences = array();	//pairs key=value
+foreach($options as $key => $value) {
+	if (!in_array($key, $default_keys))
+		$preferences[$key] = $value;
+}
 
-if (isset($options['help'])) {
-	die( "Usage: php setUserOptions.php [--quiet] [--force]
+if (isset($options['help']) || empty($preferences)) {
+	die( "Usage: php setUserOptions.php --adoptionmails=1 --founderemails-joins-%=1 [--from=12345] [--to=12345] [--founderemail] [--quiet] [--force]
 
+		  --founder  get unique founders from active wikis
+		  --from     get data from user_id if not set founder or city_id if founder is set
+		  --to       get data to user_id if not set founder or city_id if founder is set
 		  --help     you are reading it right now
 		  --force    overwrite existing option
 		  --quiet    do not print anything to output\n\n");
 }
 
-
+$founder = isset($options['founder']);
 $force = isset($options['force']);
 $quiet = isset($options['quiet']);
 $from  = $options['from'];
+$to  = $options['to'];
 
-if(  $from ) {
-  $limitUsers = array( "user_id >= $from" );
+if (!$quiet)
+	echo 'Preferences: '.var_export($preferences, TRUE)."\n";
+
+$limitUsers = array();	//WHERE in SQL
+if ($founder) {
+	$dbr = WikiFactory::db(DB_SLAVE);
+	$limitUsers = array("city_founding_user != 0", "city_founding_user is not null", "city_public = 1");
+	if ($from)
+		$limitUsers[] = "city_id >= $from";
+	if ($to)
+		$limitUsers[] = "city_id <= $to";
+	
+	$res = $dbr->select(
+		'city_list',
+		array('city_id', 'city_founding_user as user_id'),
+		$limitUsers,
+		__METHOD__
+	);
+} else {
+	$dbr = wfGetDB( DB_SLAVE );
+	if ($from)
+		$limitUsers[] = "user_id >= $from";
+	if ($to)
+		$limitUsers[] = "user_id <= $to";
+	if (empty($limitUsers))
+		$limitUsers = '';
+	
+	$res = $dbr->select(
+		'user',
+		'user_id',
+		$limitUsers,
+		__METHOD__
+	);
 }
-
-$dbr = WikiFactory::db(DB_SLAVE);
-$res = $dbr->select(
-	'user',
-	'user_id',
-	$limitUsers,
-	__METHOD__
-);
 
 $count = 0;
 
@@ -51,11 +82,14 @@ while ($row = $dbr->fetchObject($res)) {
 	if ($user) {
 		$optionsChanged = 0;
 		foreach ($preferences as $key => $val) {
-			if ($force || $user->getOption($key, null) === null) {
+			if (isset($row->city_id))
+				$key = str_replace('%', $row->city_id, $key);
+			$old_val = $user->getOption($key, null);
+			if ($force || $old_val === null) {
 				$user->setOption($key, $val);
 				$optionsChanged++;
 				if (!$quiet) {
-					echo "Setting preference '$key' to '$val' for user {$user->getName()} (id:{$user->getId()}).\n";
+					echo "Setting preference '$key' from '$old_val' to '$val' for user {$user->getName()} (id:{$user->getId()}).\n";
 				}
 			}
 		}
