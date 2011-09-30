5fd1e04947a329754b92373f5b830eda77ada473
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-09-30 22:10:38 +0000

    trunk: removing EditPageReskin
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42771 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditPageReskin/PageLayoutBuilder/templates/toolbox.tmpl.php
+++
@@ -1,9 +0,0 @@
-<?php global $wgBlankImgUrl; ?>
-
-<div class="plb-widgets" style="display: none;">
-	<ul class='plb-widget-list'>
-	</ul>
-</div>
-<div class="plb-widgets-tutorial" style="display: none;">
-	<?php echo wfMsg('plb-editor-left-sidebar-tutorial'); ?>
-</div>
