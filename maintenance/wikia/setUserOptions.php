443046519882e9156a886c841be031b97b0cf73e
saipetch <saipetch@wikia-inc.com>
2011-09-30 17:35:33 +0000

    bugid 11787: update setUserOptions script - add clear user cache option.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42748 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/setUserOptions.php
+++ maintenance/wikia/setUserOptions.php
@@ -14,18 +14,21 @@ $optionsWithArgs = array( "from", "to" );
 ini_set('include_path', dirname(__FILE__) . '/..' );
 require_once('commandLine.inc');
 	
+$cache = isset($options['cache']);
+
 // get preferences
-$default_keys = array('conf', 'help', 'force', 'quiet', 'from', 'to', 'founder');
+$default_keys = array('conf', 'help', 'force', 'quiet', 'from', 'to', 'founder', 'cache');
 $preferences = array();	//pairs key=value
 foreach($options as $key => $value) {
 	if (!in_array($key, $default_keys))
 		$preferences[$key] = $value;
 }
 
-if (isset($options['help']) || empty($preferences)) {
-	die( "Usage: php setUserOptions.php --adoptionmails=1 --founderemails-joins-%=1 [--from=12345] [--to=12345] [--founderemail] [--quiet] [--force]
+if (isset($options['help']) || (empty($preferences) && !$cache)) {
+	die( "Usage: php setUserOptions.php --adoptionmails=1 --founderemails-joins-%=1 [--from=12345] [--to=12345] [--founder] [--cache] [--quiet] [--force]
 
 		  --founder  get unique founders from active wikis
+		  --cache    clear user cache only
 		  --from     get data from user_id if not set founder or city_id if founder is set
 		  --to       get data to user_id if not set founder or city_id if founder is set
 		  --help     you are reading it right now
@@ -39,26 +42,31 @@ $quiet = isset($options['quiet']);
 $from  = $options['from'];
 $to  = $options['to'];
 
-if (!$quiet)
+if (!$quiet && !empty($preferences))
 	echo 'Preferences: '.var_export($preferences, TRUE)."\n";
 
 $limitUsers = array();	//WHERE in SQL
+$dbr = WikiFactory::db(DB_SLAVE);
 if ($founder) {
-	$dbr = WikiFactory::db(DB_SLAVE);
 	$limitUsers = array("city_founding_user != 0", "city_founding_user is not null", "city_public = 1");
 	if ($from)
 		$limitUsers[] = "city_id >= $from";
 	if ($to)
 		$limitUsers[] = "city_id <= $to";
 	
+	// SELECT in SQL
+	if($cache)
+		$selectUsers = 'distinct city_founding_user as user_id';
+	else
+		$selectUsers = array('city_id', 'city_founding_user as user_id');
+		
 	$res = $dbr->select(
 		'city_list',
-		array('city_id', 'city_founding_user as user_id'),
+		$selectUsers,
 		$limitUsers,
 		__METHOD__
 	);
 } else {
-	$dbr = wfGetDB( DB_SLAVE );
 	if ($from)
 		$limitUsers[] = "user_id >= $from";
 	if ($to)
@@ -67,7 +75,7 @@ if ($founder) {
 		$limitUsers = '';
 	
 	$res = $dbr->select(
-		'user',
+		'`user`',
 		'user_id',
 		$limitUsers,
 		__METHOD__
@@ -80,28 +88,36 @@ $count = 0;
 while ($row = $dbr->fetchObject($res)) {
 	$user = User::newFromId($row->user_id);
 	if ($user) {
-		$optionsChanged = 0;
-		foreach ($preferences as $key => $val) {
-			if (isset($row->city_id))
-				$key = str_replace('%', $row->city_id, $key);
-			$old_val = $user->getOption($key, null);
-			if ($force || $old_val === null) {
-				$user->setOption($key, $val);
-				$optionsChanged++;
-				if (!$quiet) {
-					echo "Setting preference '$key' from '$old_val' to '$val' for user {$user->getName()} (id:{$user->getId()}).\n";
-				}
-			}
-		}
-		if ($optionsChanged) {
-			$user->saveSettings();
+		if ($cache) {
 			$user->invalidateCache();
 			$count++;
+			if (!$quiet) {
+				echo "Clear cache for user {$user->getName()} (id:{$user->getId()}).\n";
+			}
+		} else {
+			$optionsChanged = 0;
+			foreach ($preferences as $key => $val) {
+				if (isset($row->city_id))
+					$key = str_replace('%', $row->city_id, $key);
+				$old_val = $user->getOption($key, null);
+				if ($force || $old_val === null) {
+					$user->setOption($key, $val);
+					$optionsChanged++;
+					if (!$quiet) {
+						echo "Setting preference '$key' from '$old_val' to '$val' for user {$user->getName()} (id:{$user->getId()}).\n";
+					}
+				}
+			}
+			if ($optionsChanged) {
+				$user->saveSettings();
+				$user->invalidateCache();
+				$count++;
+			}
 		}
 	}
 	wfWaitForSlaves( 5 );
 }
 
 if (!$quiet) {
-	echo "Updated options for $count users.\n";
+	echo "Updated $count users.\n";
 }
