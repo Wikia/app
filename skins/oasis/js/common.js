ccd46ba0a5de8810fe0949ea6d5c317c4ae71aca
macbre <macbre@wikia-inc.com>
2011-10-03 22:57:21 +0000

    JSLint found an issue in oasis/js/common.js (BugId:10060)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42913 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/common.js
+++ skins/oasis/js/common.js
@@ -11,12 +11,12 @@ jQuery.hideAds = function() {
 	 * The design team has requested that we avoid hiding and showing ads: FB 9676.
 	 * I have tested a problematic remnant ad network, and 10 wikis with premium campaigns
 	 * and have not reproduced z-index issues.
-	 * 
+	 *
 	 * For now, I'm going to prevent the hiding/showing of ads.
 	 * Community team will send all bug reports to me.
 	 *
 	 * -Christian
-	 */
+	 *
 	return;
 
 	var slots = { 'CORP_TOP_LEADERBOARD': 1, 'CORP_TOP_RIGHT_BOXAD': 1, 'HOME_TOP_LEADERBOARD': 1, 'HOME_TOP_RIGHT_BOXAD': 1, 'HOME_TOP_RIGHT_BUTTON': 1, 'TEST_HOME_TOP_RIGHT_BOXAD': 1, 'TEST_TOP_RIGHT_BOXAD': 1, 'TOP_BUTTON': 1, 'TOP_LEADERBOARD': 1, 'TOP_RIGHT_BOXAD': 1, 'TOP_RIGHT_BUTTON': 1 }
@@ -30,6 +30,7 @@ jQuery.hideAds = function() {
 	styleContent = styleContent.substr(0, styleContent.length-1); // remove trailing ','
 
 	$('<style type="text/css" class="wikia-ad-hidden">' + styleContent + ' { margin-left:-9999px; } </style>').appendTo('head');
+	*/
 };
 
 /**
@@ -41,7 +42,9 @@ jQuery.hideAds = function() {
  */
 jQuery.showAds = function() {
 	// Disabled. See comment in hideAds().
+	/*
 	return;
-	
+
 	$('head .wikia-ad-hidden').remove();
+	*/
 };
