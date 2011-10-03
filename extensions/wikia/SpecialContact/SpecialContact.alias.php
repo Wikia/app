d1efc1893aec2d3bc9a062a915d701cd546e93ab
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-03 18:45:26 +0000

    oops, removed to soon, this is still live on non-EN wikis
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@42891 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialContact/SpecialContact.alias.php
@@ -0,0 +1,17 @@
+<?php
+$aliases = array();
+ 
+/** English */
+$aliases['en'] = array(
+    'Contact' => array(  'Contact', 'ContactForm', 'ContactWikia',  ),
+);
+ 
+/** German */
+$aliases['de'] = array(
+    'Contact' => array(  'Kontakt',  ),
+);
+
+/** Spanish */
+$aliases['es'] = array(
+    'Contact' => array(  'Contactar',  ),
+);
