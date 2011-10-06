9e2fbbd10d7d22e064654df08ea4840640bfcf74
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-06 16:21:16 +0000

    PhotoPop: Working tutorial, local highscore, initHome. Amazing stuff!
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43169 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/hacks/PhotoPop/js/main.js
+++ extensions/wikia/hacks/PhotoPop/js/main.js
@@ -18,6 +18,8 @@
 			gameLoader = new data.XDomainLoader(),
 			selectedGame,
 			wrapper,
+			g,
+			Game = gameLogic.Game,
 			muteButton,
 			view = {
 				image: function() {
@@ -31,13 +33,24 @@
 					}
 				}
 			},
-			
 			gameScreenRender = function(event, round){
-				var imgPath = (round.gameId == 'tutorial') ? imageServer.getAsset(round.data.image) : imageServer.getPicture(round.data.image);
-				view.path = imgPath;
 				wrapper.innerHTML = Mustache.to_html(templates.gameScreen, view);
+				if(g.getId() == 'tutorial')
+				g.openModal({
+					name: 'intro',
+					html: "Tap the screen to take a peek of the mystery image underneath.",
+					fade: true,
+					clickThrough: false,
+					closeOnClick: true
+				});
+				
+				if(!localStorage['highScore_' + g.getId()]) localStorage['highScore_' + g.getId()] = 0;
+				document.getElementById('highScore').getElementsByTagName('span')[0].innerHTML = localStorage['highScore_' + g.getId()];
 			},
-			
+			changeImg = function(gameId, image) {
+				var imgPath = (gameId == 'tutorial') ? imageServer.getAsset(image) : imageServer.getPicture(image);
+				document.getElementById('bgPic').getElementsByTagName('img')[0].src = imgPath;	
+			},
 			loadSelectedGame = function(){
 				if(typeof selectedGame != 'undefined'){
 					gameLoader.load('http://' +
@@ -83,6 +96,7 @@
 			}
 			
 			function initHomeScreen(){
+				wrapper.innerHTML += Mustache.to_html(templates.selectorScreen, view);
 				setTimeout(
 					function(){
 						document.getElementById('sliderWrapper').style.bottom = 0;
@@ -95,13 +109,196 @@
 					},
 					2000
 				);
-			}
+				
+				muteButton = document.getElementById('button_volume');
+				muteButton.onclick = function(){
+					soundServer.play('pop');
+					
+					var imgs = this.getElementsByTagName('img');
+					
+					if(!soundServer.getMute()) {
+						soundServer.setMute(true);
+						imgs[0].style.display = "none";
+						imgs[1].style.display = "block";
+					} else {
+						soundServer.setMute(false);
+						imgs[1].style.display = "none";
+						imgs[0].style.display = "block";
+					}
+				}
+			};
+			//event handlers
+			var modalOpened = function(event, options) {
+				if(g.getId() == 'tutorial') {
+					g.pause();
+					g._tutorialSteps[options.name] = 1;
+				}
+			},
+			modalClosed = function(event, options) {
+			},
+			answerClicked = function(event, options) {
+				g.resume();
+				if(options.li.id != g._correctAnswer) {
+					this.fire('wrongAnswerClicked', {li: options.li});
+				} else {
+					this.fire('rightAnswerClicked', {li: options.li});
+				}
+			},
+			wrongAnswerClicked = function(event, options) {
+				var li = options.li;
+				if(!li.clicked) {
+					li.className = Game.INCORRECT_CLASS_NAME;
+					li.clicked = true;
+					soundServer.play('wrongAnswer');
+		
+					g._roundPoints.deductPoints(g._wrongAnswerPointDeduction);
+					g.updateScoreBar();
+					g.updateHudScore();
+				}
+			},
+			rightAnswerClicked = function(event, options) {
+				if(g.getId() == 'tutorial') {
+					g.openModal({
+						name: 'continue',
+						html: 'After the answer is revealed tap the "next" button to continue on to a new image.',
+						fade: true,
+						clickThrough: false,
+						closeOnClick: true});
+				}
+				
+				g._roundIsOver = true;
+				g._timer = null;
+	
+				// Record this as a correct answer (for the stats at the end of the game).
+				g._numCorrect++;
+	
+				// Move the points from the round-score to the total score.
+				g._totalPoints.addPoints(g._roundPoints.getPoints());
+				g._roundPoints.setPoints(0);
+				// Play sound.
+				soundServer.play('win');
+	
+				// Collapse answer-drawer.
+				g.hideAnswerDrawer();
+				
+				// Reveal all tiles.
+				g.revealAll();
+				g.hideScoreBar();
+				g.showContinue('Excellent! It\'s ' + options.li.innerHTML);
+			},
+			answerDrawerButtonClicked = function(event, options) {
+				var button = options.button,
+				buttonClassList = button.classList,
+				imgs = button.getElementsByTagName('img');
+				
+				g.resume();
+				
+				if (buttonClassList.contains('closed')) {
+					imgs[0].style.opacity = 0;
+					imgs[1].style.opacity = 1;
+					buttonClassList.remove('closed');
+					document.getElementById('answerDrawer').style.right = 0;
+					if(g.getId() == 'tutorial') {
+						g.openModal({
+							name: 'drawer',
+							html: 'The fewer peek you take, the fewer guesses you make, and the less time you take, the bigger your score!',
+							fade: true,
+							clickThrough: false,
+							fontSize: 'x-large',
+							closeOnClick: true});
+					}
+				} else {
+					imgs[1].style.opacity = 0;
+					imgs[0].style.opacity = 1;
+					buttonClassList.add('closed');
+					document.getElementById('answerDrawer').style.right = -225;
+				}
+			},
+			answerDrawerHidden = function() {
+				var answerList = document.getElementById('answerList'),
+				answerListLength = answerList.length,
+				answerButton = document.getElementById('answerButton');
+				
+				answerButton.classList.add('closed');
+				answerButton.getElementsByTagName('img')[1].style.opacity = 0;
+				answerButton.getElementsByTagName('img')[0].style.opacity = 1;
+			},
+			continueClicked = function() {
+				g.nextRound();
+			},
+			endGame = function() {
+				g.showEndGameScreen();
+				if (localStorage['highScore_' + g.getId()] < g._totalPoints.getPoints()) {
+					document.getElementById('highScore').getElementsByTagName('span')[0].innerHTML = g._totalPoints.getPoints();
+					localStorage['highScore_' + g.getId()] = g._totalPoints.getPoints();
+						g.openModal({
+							name: 'highscore',
+							html: 'New highscore: ' + g._totalPoints.getPoints() + ' !',
+							fade: true,
+							clickThrough: false,
+							closeOnClick: true});
+				}
+			},
+			goHome = function() {
+				this.fire('initHomeScreen');	
+			},
+			maskDisplayed = function(event , options) {
+				changeImg(g.getId(), options.image);
+				g.updateHudProgress();
+			},
+			scoreBarHidden = function() {
+				var scoreBarStyle = document.getElementById('scoreBar').style;
+				scoreBarStyle.height = g._barWrapperHeight;
+				scoreBarStyle.backgroundColor = 'rgba(137, 196, 64, 0.9)';
+			},
+			tileClicked = function(event, tile) {
+				tile = tile.tile;
+				
+				if(!tile.clicked) {
+					g.resume();
+					soundServer.play('pop');
+					tile.clicked = true;
+					tile.style.opacity = 0;
+					if( g.getId() == "tutorial" ) {
+						g.openModal({
+							name: 'tile',
+							html:'Tap the "answer" button to make your guess.',
+							fade: true,
+							clickThrough: false,
+							closeOnClick: true,
+							triangle: 'right'});
+					}
+
+				}
+			},
+			timeIsUp = function(event, options){
+				soundServer.play('fail');
+				g.roundIsOver = true;
+				g.revealAll();
+				g.hideAnswerDrawer();
+				g.hideScoreBar();
+				g.showTimeUp();
+				//g.updateHudScore();
+				setTimeout(function() {
+					g.hideTimeUp();
+					g.showContinue('Wrong! It\'s ' + options.correct);
+				}, Game.TIME_UP_NOTIFICATION_DURATION_MILLIS);
+			},
+			timerEvent = function() {
+				g._roundPoints.deductPoints(g._timerPointDeduction);
+				g.updateScoreBar();
+				g.updateHudScore();
+			},
+			timeIsLow = function() {
+				soundServer.play('timeLow')
+			},
+			//end of event handlers
+			elm = (isApp) ? document.body : document.getElementById('PhotoPopWrapper');
 			
 			function onDataError(event, resp){
 				alert('Error loading ' + resp.url + ': ' + resp.error.toString());
 			}
-			
-			//init audio and graphics
+					
 			imageServer.init(config.images);
 			soundServer.init(config.sounds);
 			
@@ -133,38 +330,36 @@
 			
 			elm.innerHTML = Mustache.to_html(templates.mainPage, view);
 			
-			wrapper = document.getElementById('wrapper'),
-			wrapper.innerHTML += Mustache.to_html(templates.selectorScreen, view);
-			
-			muteButton = document.getElementById('button_volume');
-			muteButton.onclick = function(){
-				soundServer.play('pop');
-				
-				var imgs = this.getElementsByTagName('img');
-				
-				if(!soundServer.getMute()) {
-					soundServer.setMute(true);
-					imgs[0].style.display = "none";
-					imgs[1].style.display = "block";
-				} else {
-					soundServer.setMute(false);
-					imgs[1].style.display = "none";
-					imgs[0].style.display = "block";
-				}
-			}
+			wrapper = document.getElementById('wrapper');			
 			
 			if(startTutorial){
-				var g = new gameLogic.Game({
+				g = new Game({
 					id: 'tutorial',
 					data: config.tutorial,
 					watermark: imageServer.getAsset('watermark_dexter')
 				});
 				
-				g.addEventListener('roundStart', gameScreenRender);
-				g.addEventListener('playSound', function(event, sound) {
-					soundServer.play(sound.name);
-				});
-				g.play();			
+				g.addEventListener('renderGameScreen', gameScreenRender);
+				g.addEventListener('initHomeScreen', initHomeScreen);
+			
+				//register event listeners
+				g.addEventListener('goHome', goHome);
+				g.addEventListener('timeIsUp', timeIsUp);
+				g.addEventListener('modalOpened', modalOpened);
+				g.addEventListener('modalClosed', modalClosed);
+				g.addEventListener('answerClicked', answerClicked);
+				g.addEventListener('wrongAnswerClicked', wrongAnswerClicked);
+				g.addEventListener('rightAnswerClicked', rightAnswerClicked);
+				g.addEventListener('answerDrawerButtonClicked', answerDrawerButtonClicked);
+				g.addEventListener('continueClicked', continueClicked);
+				g.addEventListener('maskDisplayed', maskDisplayed);
+				g.addEventListener('scoreBarHidden',scoreBarHidden);
+				g.addEventListener('answerDrawerHidden', answerDrawerHidden);
+				g.addEventListener('tileClicked', tileClicked);
+				g.addEventListener('timerEvent', timerEvent);
+				g.addEventListener('timeIsLow', timeIsLow);
+				g.addEventListener('endGame', endGame);
+				g.prepareGame();			
 			}else{
 				initHomeScreen();
 			}
