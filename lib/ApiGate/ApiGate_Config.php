2cd70ed47bd62129a0d046c37283440a3a60c244
sean <sean@wikia-inc.com>
2011-10-06 00:44:58 +0000

    Some initial structure of the ApiGate system as roughly-outlined in the whiteboard (pic included).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43128 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ lib/ApiGate/ApiGate_Config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Sean Colombo
+ * @date 20111005
+ *
+ * This is the configuration file for API Gate.
+ */
+
+
+/** HEADERS **/
+	// Expected & understood by Fastly (a CDN that has worked with API Gate) to indicate that this result requires a valid API key in the request.
+	$APIGATE_HEADER_REQUIRES_API = "X-Requires-ApiKey";
+
+	// Recommended that each app override this for clarity.  Example value (follows a widely used convention): "X-Wikia-API-Key".
+	$APIGATE_HEADER_API_KEY = "X-ApiGate-API-Key";
+/** /HEADERS **/
+
+
+/** HTTP STATUS CODES **/
+	$APIGATE_HTTP_STATUS_OK = 200;
+	$APIGATE_HTTP_STATUS_FAILED_AUTH = 401;
+	$APIGATE_HTTP_STATUS_RATE_LIMITED = 509; // 509 means "Bandwidth exceeded" which is the closest match.
+	//$APIGATE_HTTP_STATUS_TEAPOT = 418; // unused, for now ;)
+/** /HTTP STATUS CODES **/
+
