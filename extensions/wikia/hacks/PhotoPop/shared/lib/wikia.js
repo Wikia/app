22bff23a34075f9b677ccf0493ea1d6cc5eec03e
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-07 10:11:35 +0000

    PhotoPop: Wikia namespace and platform recognition
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43208 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/hacks/PhotoPop/shared/lib/wikia.js
@@ -0,0 +1,82 @@
+/**
+ * @namespace Wikia
+ */
+var Wikia = {};
+
+(function(ns){
+	/**
+	 * @private
+	 */
+	
+	var ua = navigator.userAgent,
+	isApp = (typeof Titanium != 'undefined'),
+	isIPad = /iPad/i.test(ua),
+	isIPhone = /iPhone/i.test(ua),
+	isIPod = /iPod/i.test(ua),
+	isAndroid = /Android/i.test(ua),
+	isDesktop = !isApp && !(isIPad || isIPhone || isIPod || isAndroid);
+	
+	/**
+	 * @public
+	 */		
+	
+	ns.Platform = {
+		is: function(){
+			var result = true, result2, x, y;
+			
+			for(x = 0, y = arguments.length; x < y; x++){
+				switch(arguments[x]){
+					case 'app':
+						result2 = isApp;
+						break;
+					case 'web':
+						result2 =  !isApp;
+						break;
+					case 'desktop':
+						result2 =  isDesktop;
+						break;
+					case 'mobile':
+						result2 =  !isDesktop;
+						break;
+					case 'ios':
+						result2 =  (isIPad || isIPhone || isIPod);
+						break;
+					case 'ipad':
+						result2 =  isIPad;
+						break;
+					case 'ipod':
+						result2 =  isIPod;
+						break;
+					case 'iphone':
+						result2 =  isIPhone;
+						break;
+					case 'android':
+						result2 = isAndroid;
+						break;
+					default:
+						result2 = false;
+						break;
+				}
+				
+				result = result && result2;
+			}
+			
+			return result;
+		}
+	};
+	
+	/**
+	 * XPlatform and platform-specific fixes
+	 */
+	
+	if(Wikia.Platform.is('app', 'ios')){
+		window.requireJsGetUrl = function(url){
+			return 'app://Resources/' + url;
+		};
+	}
+	
+	//crossplatform class list implementation
+	HTMLElement.prototype.getClasses = function(){
+		return (this.classList) ? this.classList : this.className.split(' ');
+	};
+})(Wikia);
