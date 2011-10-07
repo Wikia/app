20cc4f78668ca1ded22cf4420f116b49768f2f11
sean <sean@wikia-inc.com>
2011-10-07 18:25:39 +0000

    ApiExplorer: Merge back changes from core & testing some additional refactorings (fix FIXMEs and add alias file).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43239 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/ApiExplorer/SpecialApiExplorer.alias.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Aliases for Special:404
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'ApiExplorer' => array( 'ApiExplorer' ),
+);
+
+
+
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
