7efc2034b57fbe2a4291b186318554a30af876c8
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-07 18:25:39 +0000

    ApiExplorer: Merge back changes from core & testing some additional refactorings (fix FIXMEs and add alias file).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43239 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/ApiExplorer/ApiExplorer.i18n.php
+++ extensions/ApiExplorer/ApiExplorer.i18n.php
@@ -12,15 +12,74 @@ $messages = array();
  */
 $messages['en'] = array(
 	'apiexplorer' => 'API Explorer',
-	'apiexplorer-desc' => 'Page for interactively exploring the documentation of the live version of the MediaWiki API running on this installation.',
-	'apiexplorer-intro' => 'This page shows documentation for the current wiki\'s API. Please click on a function below to see more info about what parameters can be used in each call.  For the general MediaWiki API documentation, see $1',
-	
+	'apiexplorer-desc' => 'Page for interactively exploring the documentation of the MediaWiki API',
+	'apiexplorer-intro' => "This page shows documentation for the current wiki's API. Please click on a function below to see more info about what parameters can be used in each call. Also see [http://www.mediawiki.org/wiki/API:Main_page the general MediaWiki API documentation].",
+
 	'apiexplorer-loading' => 'Loading functions...',
 );
 
 /** Message documentation (Message documentation)
+ * @author Sean Colombo
  */
 $messages['qqq'] = array(
-	'apiexplorer-desc' => "Description of the extension (for Special:Version, etc.).",
-	'apiexplorer-intro' => "The intro at the top of the page to acquiant new users with what the page does."
+	'apiexplorer-desc' => 'Description of the extension (for Special:Version, etc.).',
+	'apiexplorer-intro' => 'The intro at the top of the page to acquiant new users with what the page does.',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'apiexplorer' => 'API-Explorer',
+	'apiexplorer-desc' => 'Ergänzt eine Spezialseite, mit der das interaktive Nutzen der Dokumentation zur API der MediaWiki-Installation möglich ist',
+	'apiexplorer-intro' => 'Diese Seite zeigt die Dokumention zur API dieses Wikis. Bitte unten auf eine der Funktionen klicken, um mehr Informationen zu den mit jedem Aufruf verfügbaren Parametern zu erhalten. Eine allgemeine Dokumentation zur API von MediaWiki ist auf der folgenden Webseite vorhanden: $1',
+	'apiexplorer-loading' => 'Funktionen werden geladen …',
+);
+
+/** French (Français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'apiexplorer' => 'API Explorer',
+	'apiexplorer-desc' => "Page pour explorer de façon interactive la documentation de la version courante de l'API MediaWiki tournant sur cette installation.",
+	'apiexplorer-intro' => "Cette page affiche la documentation pour l'API du wiki actuel. Merci de cliquer sur une fonction ci-dessous pour voir plus d'information sur les paramètres qui peuvent être utilisés dans chaque appel. Pour la documentation générale de l'API MediaWiki, voyez $1",
+	'apiexplorer-loading' => 'Chargement des fonctions...',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'apiexplorer' => 'Explorador API',
+	'apiexplorer-desc' => 'Páxina para explorar de xeito interactivo a documentación da versión actual do API de MediaWiki que emprega esta instalación.',
+	'apiexplorer-intro' => 'Esta páxina mostra a documentación para o API do wiki actual. Prema nunha das funcións para ollar máis información sobre os parámetros que se poden usar ??en cada chamada. Se o que quere é unha documentación xeral do API de MediaWiki, bótelle unha ollada a $1',
+	'apiexplorer-loading' => 'Cargando as funcións...',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'apiexplorer' => 'Explorator API',
+	'apiexplorer-desc' => 'Pagina pro explorar interactivemente le documentation del version del API de MediaWiki que es active in iste installation.',
+	'apiexplorer-intro' => 'Iste pagina monstra documentation pro le API del wiki actual. Clicca sur un function hic infra pro vider plus information super le parametros que pote esser usate in cata appello. Pro le documentation general del API de MediaWiki, vide $1',
+	'apiexplorer-loading' => 'Carga functiones...',
+);
+
+/** Macedonian (??????????)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'apiexplorer' => 'API-??????????',
+	'apiexplorer-desc' => '???????? ?? ???????????? ??????????? ?? ??????????????? ?? ????????? ??????? ??????? ?? API-???????? ?? ?????????? ??? ?????? ?? ???? ???????????.',
+	'apiexplorer-intro' => '???? ???????? ???????? ????????????? ?? API-???????? ?? ????????? ????. ???????? ?? ???????? ?????? ?? ?? ??????? ?????? ?????????? ?? ??? ??? ????????? ????? ?? ?? ????????? ?? ????? ?????. ????? ????????????? ?? API-???????? ?? ?????????? ?? ??????? ?? $1',
+	'apiexplorer-loading' => '???????? ???????...',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'apiexplorer-loading' => 'Bezig met het laden van functies...',
+);
+
--- extensions/ApiExplorer/SpecialApiExplorer.php
+++ extensions/ApiExplorer/SpecialApiExplorer.php
@@ -12,98 +12,93 @@
  * handle all functions.
  */
 
-if (!defined('MEDIAWIKI')) die();
+if ( !defined( 'MEDIAWIKI' ) ) die();
 
-$wgExtensionFunctions[] = 'wfSpecialApiExplorer';
+$wgSpecialPages[ "ApiExplorer" ] = "SpecialApiExplorer";
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'API Explorer',
-	//'url' => 'http://www.mediawiki.org/wiki/Extension:ApiExplorer', // TODO: Put into MediaWiki Extensions repo
-	'description' => 'Extension for interactively viewing live API documentation', // TODO: Update this if we have forms for each function.
+	'url' => 'http://www.mediawiki.org/wiki/Extension:ApiExplorer',
 	'descriptionmsg' => 'apiexplorer-desc',
 	'author' => '[http://seancolombo.com Sean Colombo]'
 );
 
-$dir = dirname(__FILE__) . '/';
+$dir = dirname( __FILE__ ) . '/';
 $wgExtensionMessagesFiles['ApiExplorer'] = $dir . 'ApiExplorer.i18n.php';
+$wgExtensionAliasesFiles['ApiExplorer'] = $dir . '/ApiExplorer.alias.php';
 
-function wfSpecialApiExplorer () {
-	global $IP;
-	require_once "$IP/includes/SpecialPage.php";
-	class SpecialApiExplorer extends SpecialPage {
-		/**
-		 * Constructor
-		 */
-		function SpecialApiExplorer() {
-			SpecialPage::SpecialPage( 'ApiExplorer' );
-			$this->includable( false );
-		}
-
-		/**
-		 * Main function of the special page. Basically serves as a wrapper which loads
-		 * the main functionality (which is in Javascript).
-		 *
-		 * @param $par - parameters for SpecialPage. Ignored.
-		 */
-		function execute( $par = null ) {
-			global $wgOut, $wgExtensionsPath, $wgCityId, $wgStyleVersion;
-			wfProfileIn( __METHOD__ );
-
-			wfLoadExtensionMessages( "AutoCreateWiki" ); // TODO: This isn't needed anymore, even in Wikia code (which is 2 versions back at the moment), is it?
-
-			// TODO: Make this work for ResourceLoader (Wikia isn't using RL yet at the time of this writing).
-			// Wikia has the cachebuster in the wgExtensionPath (we rewrite that in varnish because many proxies won't cache things that have "?" in the URL), but other MediaWikis need the style-version in the querystring.
-			$cbSuffix = ( isset($wgCityId) ? "?{$wgStyleVersion}" : "" );
-			$wgOut->addScript( "<script type=\"text/javascript\" src=\"{$wgExtensionsPath}/wikia/JavascriptAPI/Mediawiki.js{$cbSuffix}\"></script>" );
-			$wgOut->addScript( "<script type=\"text/javascript\" src=\"{$wgExtensionsPath}/ApiExplorer/apiExplorer.js{$cbSuffix}\"></script>" );
-			$wgOut->addScript( "<link rel=\"stylesheet\" type=\"text/css\" href=\"{$wgExtensionsPath}/ApiExplorer/apiExplorer.css{$cbSuffix}\" />" );
-
-			ob_start();
-				$buttonHeight = 15;
-				$collapseSrc = "$wgExtensionsPath/ApiExplorer/collapse.png$cbSuffix";
-				$expandSrc = "$wgExtensionsPath/ApiExplorer/collapse.png$cbSuffix"; 
-				?><style>
-					.collapsible h2 span, .collapsible h3 span{
-						width:<?= $buttonHeight ?>px;
-						height:1em;
-						float:right;
-						display:inline-block;
-
-						background-repeat:no-repeat;
-						background-position:right center;
-						background-image: url(<?= "$wgExtensionsPath/ApiExplorer/collapse.png$cbSuffix"; ?>);
-					}
-					.collapsed h2 span, .collapsed h3 span{
-						background-image: url(<?= "$wgExtensionsPath/ApiExplorer/expand.png$cbSuffix"; ?>);
-					}
-				</style>
-				<div id='apEx_intro'>
-					<?= wfMsg('apiexplorer-intro', "<a href='http://www.mediawiki.org/wiki/API:Main_page'>http://www.mediawiki.org/wiki/API:Main_page</a>") ?>
-				</div>
-				<div id='apEx_loading'><?= wfMsg('apiexplorer-loading') ?></div>
-				<div id='apEx'>
-					<?php
-					$params = array("modules", "querymodules", "formatmodules");
-					foreach($params as $param){
-						?><div class='<?= $param ?> collapsible collapsed paramName' data-param-name='<?= $param ?>'>
-							<h2 class='name'><span class='toggleIcon'></span></h2>
-							<div class='paramContent'>
-								<div class='description'></div>
-								<dl>
-									<!-- Filled by a call to the API -->
-								</dl>
-							</div>
-						</div><?php
-					} ?>
-				</div>
-			<?php
-			$outHtml = ob_get_clean();
-
-			$this->setHeaders();
-			$wgOut->addHTML( $outHtml );
-			wfProfileOut( __METHOD__ );
-		} // end execute()
+/**
+ * @ingroup SpecialPage
+ */
+class SpecialApiExplorer extends SpecialPage {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		SpecialPage::SpecialPage( 'ApiExplorer' );
+		$this->includable( false );
 	}
 
-	SpecialPage::addPage( new SpecialApiExplorer );
-} // end wfSpecialApiExplorer()
+	/**
+	 * Main function of the special page. Basically serves as a wrapper which loads
+	 * the main functionality (which is in Javascript).
+	 *
+	 * @param $par - parameters for SpecialPage. Ignored.
+	 */
+	public function execute( $par = null ) {
+		global $wgOut, $wgExtensionsPath, $wgCityId, $wgStyleVersion;
+		wfProfileIn( __METHOD__ );
+
+		// TODO: Make this work for ResourceLoader (Wikia isn't using RL yet at the time of this writing).
+		// Wikia has the cachebuster in the wgExtensionPath (we rewrite that in varnish because many proxies won't cache things that have "?" in the URL), but other MediaWikis need the style-version in the querystring.
+		$cbSuffix = ( isset( $wgCityId ) ? "?{$wgStyleVersion}" : "" );
+		$wgOut->addScript( "<script type=\"text/javascript\" src=\"{$wgExtensionsPath}/wikia/JavascriptAPI/Mediawiki.js{$cbSuffix}\"></script>" );
+		$wgOut->addScript( "<script type=\"text/javascript\" src=\"{$wgExtensionsPath}/ApiExplorer/apiExplorer.js{$cbSuffix}\"></script>" );
+		$wgOut->addScript( "<link rel=\"stylesheet\" type=\"text/css\" href=\"{$wgExtensionsPath}/ApiExplorer/apiExplorer.css{$cbSuffix}\" />" );
+
+		ob_start();
+			$buttonHeight = 15;
+			$collapseSrc = "$wgExtensionsPath/ApiExplorer/collapse.png$cbSuffix";
+			$expandSrc = "$wgExtensionsPath/ApiExplorer/collapse.png$cbSuffix";
+			?><style>
+				.collapsible h2 span, .collapsible h3 span{
+					width:<?= $buttonHeight ?>px;
+					height:1em;
+					float:right;
+					display:inline-block;
+
+					background-repeat:no-repeat;
+					background-position:right center;
+					background-image: url(<?= "$wgExtensionsPath/ApiExplorer/collapse.png$cbSuffix"; ?>);
+				}
+				.collapsed h2 span, .collapsed h3 span{
+					background-image: url(<?= "$wgExtensionsPath/ApiExplorer/expand.png$cbSuffix"; ?>);
+				}
+			</style>
+			<div id='apEx_intro'>
+				<?= wfMsg('apiexplorer-intro') ?>
+			</div>
+			<div id='apEx_loading'><?= wfMsg('apiexplorer-loading') ?></div>
+			<div id='apEx'>
+				<?php
+				$params = array( "modules", "querymodules", "formatmodules" );
+				foreach ( $params as $param ) {
+					?><div class='<?= $param ?> collapsible collapsed paramName' data-param-name='<?= $param ?>'>
+						<h2 class='name'><span class='toggleIcon'></span></h2>
+						<div class='paramContent'>
+							<div class='description'></div>
+							<dl>
+								<!-- Filled by a call to the API -->
+							</dl>
+						</div>
+					</div><?php
+				} ?>
+			</div>
+		<?php
+		$outHtml = ob_get_clean();
+
+		$this->setHeaders();
+		$wgOut->addHTML( $outHtml );
+		wfProfileOut( __METHOD__ );
+	} // end execute()
+} // end class SpecialApiExplorer
