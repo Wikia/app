abd6d4d48c93ac15f13e87615c6e29aa31bf836d
marcin <marcin@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-11 22:28:50 +0000

    WikiNav2: error message when a wiki nav menu item is too long (bugid:13034)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43352 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/WikiHeader.js
+++ skins/oasis/js/WikiHeader.js
@@ -236,6 +236,7 @@ var WikiHeaderV2 = {
 					menu.css('visibility', 'hidden').show();
 				}
 
+/*				
 				// loop through each menu item and remove it if doesn't fit into the first row
 				items.each(function() {
 					var item = $(this),
@@ -250,6 +251,7 @@ var WikiHeaderV2 = {
 							itemsRemoved++;
 						}
 				});
+*/
 
 				if (i > 0 ) {
 					menu.css('visibility', 'visible').hide();
@@ -387,6 +389,34 @@ var WikiHeaderV2 = {
 	positionNav: function() {
 		//This runs once. Sets the proper top position of the subnav. Can't be calculated earlier because custom font loading can adjust wiki nav height.
 		WikiHeaderV2.navtop = WikiHeaderV2.nav.height() - 7;
+	},
+	
+	firstMenuValidator: function() {
+		var widthLevelFirst = 0; 
+		$.each($('.ArticlePreview #WikiHeader > nav > ul > li'), function(menuItemKey, menuItem) {
+			$().log($(menuItem).width());
+			widthLevelFirst += $(menuItem).width();
+		});
+		if (widthLevelFirst > 580) {
+			alert($.msg('oasis-navigation-v2-level1-validation')); //TODO: add messages here
+		}
+	},
+	
+	secondMenuValidator: function() {
+		var widthLevelSecond = 0;
+		$.each($('.ArticlePreview #WikiHeader .subnav-2'), function(parentMenuItemKey, parentMenuItem) {
+			$(parentMenuItem).show(1, function() {
+				$.each($(parentMenuItem).children('li'), function(menuItemKey, menuItem) {
+					widthLevelSecond += $(menuItem).width();
+				});
+				if (widthLevelSecond > 720) {
+					alert($.msg('oasis-navigation-v2-level2-validation')); //TODO: add messages here
+				}
+				widthLevelSecond = 0;
+				$(parentMenuItem).hide();
+			});
+		});
+		$('.ArticlePreview #WikiHeader .marked .subnav-2').show();
 	}
 };
 
@@ -406,6 +436,10 @@ $(function() {
 			// don't style wiki nav like article content
 			previewNode.removeClass('WikiaArticle');
 			WikiHeaderV2.init();
+			$.getMessages('Oasis-navigation-v2', function() {
+				WikiHeaderV2.firstMenuValidator();
+				WikiHeaderV2.secondMenuValidator();
+			});
 		});
 	}
 });
