5349d30ecfb7c20b4062b76074654210b4210d4a
sean <sean@wikia-inc.com>
2011-10-12 00:05:42 +0000

    Attempted to integrate i18n into the core ApiGate library in a way that it's just a wrapper of MediaWiki i18n (but could be split out later).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43357 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialApiGate/SpecialApiGate.i18n.php
+++ extensions/wikia/SpecialApiGate/SpecialApiGate.i18n.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * Internationalisation file for Special:CategoryIntersection extension.
+ * Internationalisation file for Special:ApiGate extension.
  *
  * @addtogroup Extensions
  */
+
+// Include the messages from the ApiGate library (they're compatible with MediaWiki).
+global $IP;
+include "$IP/lib/ApiGate/i18n/ApiGate_i18n.strings.php";
+$apiGateMessages = $messages; // cache to merge together later (SpecialApiGate messages will take preference over ApiGate library messages).
 
 $messages = array();
 
@@ -18,3 +23,7 @@ $messages['en'] = array(
 $messages['qqq'] = array(
 	'apigate-desc' => '{{desc}}',
 );
+
+// Merge together the messages.
+// SpecialApiGate messages will take preference over ApiGate library messages.
+$messages = array_merge($apiGateMessages, $messages);
---
+++ lib/ApiGate/i18n/ApiGate_i18n.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author Sean Colombo
+ * @date 20111011
+ *
+ * Internationalization / localization code for ApiGate.
+ *
+ * Currently not really implemented in a stand-alone way.  Just leaning on the MediaWiki
+ * implementation for now.
+ *
+ * TODO: Implement this in a way which allows ApiGate to be standalone but still default to MediaWiki's
+ * i18n system if that's available.
+ */
+
+// Load the actual string files.
+$dir = dirname(__FILE__);
+require "$dir/i18n/ApiGate_i18n.strings.php";
+
+// For now, just wrap MediaWiki's i18n functions.
+function i18n( $msgName ) {
+	wfProfileIn( __METHOD__ );
+
+	if( function_exists( 'wfMsg' ) ) {
+		$retVal = wfMsg( $msgName );
+	} else {
+		$retVal = "<ApiGate - $msgName>";
+	}
+	
+	wfProfileOut( __METHOD__ );
+	return $retVal;
+} // end i18n()
