c04d629ac72e1937cf0332dacd021d272424760d
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-13 14:11:39 +0000

    argh, committed to the wrong branch, merged back from prod 049 BugzId:12801
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43454 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiFeatures/WikiFeatures.alias.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English */
+$aliases['en'] = array(
+	'WikiFeatures' => array( 'WikiFeatures' ),
+);
+
+/** German(Deutsch) */
+$aliases['de'] = array(
+	'WikiFeatures' => array( 'WikiFunktionen' ),
+);
