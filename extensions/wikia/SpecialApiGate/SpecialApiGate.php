d165afc1ebbb9f619b30c0cafeafb8f0c549c28d
sean <sean@wikia-inc.com>
2011-10-13 00:41:12 +0000

    ApiGate: Progress on the registration form and the various design tweaks that we're figuring out as we go.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43437 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialApiGate/SpecialApiGate.php
+++ extensions/wikia/SpecialApiGate/SpecialApiGate.php
@@ -41,13 +41,20 @@ class SpecialApiGate extends SpecialPage {
 	 * @param $subpage Mixed: string if any subpage provided, else null
 	 */
 	public function execute( $subpage ) {
-		global $wgOut, $wgRequest, $IP;
+		global $wgOut, $wgRequest, $IP, $wgUser;
 		wfProfileIn( __METHOD__ );
 
 		include "$IP/lib/ApiGate/ApiGate.php";
 
 		$wgOut->setPagetitle( wfMsg('apigate') );
+		
+		// Box the main content of the text into a left-column so that a custom menu can be put on the right (below).
+		$wgOut->addWikiText( "<mainpage-leftcolumn-start />");
 
+// TODO: Make sure that all subpages (EXCEPT checkKey!) redirect to Api wiki if they're on another wiki (needed for that right-rail template to work & still community editable - including the images on it).
+// TODO: Make sure that all subpages (EXCEPT checkKey!) redirect to Api wiki if they're on another wiki (needed for that right-rail template to work & still community editable - including the images on it).
+
+		$mainSectionHtml = "";
 		$apiKey = $wgRequest->getVal( 'apiKey' );
 		switch($subpage){
 			case $this->SUBPAGE_CHECK_KEY:
@@ -64,13 +71,23 @@ class SpecialApiGate extends SpecialPage {
 
 				break;
 			case $this->SUBPAGE_REGISTER:
-			
+
 				$data = array();
 
-				// TODO: Set up some more vars for registration form (user_id?)
-				// TODO: Set up some more vars for registration form (user_id?)
+				// Users must be logged in to get an API key
+				if ( !$wgUser->isAllowed( 'emailconfirmed' ) ) {
+					if( !$wgUser->isLoggedIn() ){
+						$wgOut->showErrorPage( 'apigate-nologin', 'apigate-nologintext' );
+					} else {
+						$wgOut->permissionRequired( 'emailconfirmed' );
+					}
+				} else {
 
-				$wgOut->addHTML( ApiGate_Dispatcher::renderTemplate( $data, "register" ) );
+					// TODO: Set up some more vars for registration form (user_id?) - also, prepopulate their email addr if they're email-confirmed
+					// TODO: Set up some more vars for registration form (user_id?) - also, prepopulate their email addr if they're email-confirmed
+
+					$mainSectionHtml = ApiGate_Dispatcher::renderTemplate( $data, "register" );
+				}
 
 				break;
 			case $this->SUBPAGE_ALL_KEYS:
@@ -100,6 +117,17 @@ class SpecialApiGate extends SpecialPage {
 
 				break;
 		}
+		$wgOut->addHTML( "<div id='specialApiGateMainSection'><div class='module'>
+			<div style='margin:auto;width:400px;background-color:white'>
+				\n$mainSectionHtml\n
+			</div>
+		</div></div>" );
+
+		// End the main column and add the right-rail.
+		$wgOut->addWikiText("<mainpage-endcolumn />
+							<mainpage-rightcolumn-start />
+							{{MenuRail}}
+							<mainpage-endcolumn />");
 
 		wfProfileOut( __METHOD__ );
 	} // end execute()
