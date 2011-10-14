f061202f21784676d6f5e9b89db731bd62494e9f
will <will@wikia-inc.com>
2011-10-14 14:39:53 +0000

    Bug ID 13246: Wikia Video URLs do not work in the video embed tool
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43518 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoEmbedTool/js/VET.js
+++ extensions/wikia/VideoEmbedTool/js/VET.js
@@ -1071,6 +1071,6 @@ function VET_sendQueryEmbed(query) {
 	}
 	VET_indicator(1, true);
 	YAHOO.util.Connect.abort(VET_asyncTransaction);
-	VET_asyncTransaction = YAHOO.util.Connect.asyncRequest('GET', wgScriptPath + '/index.php?action=ajax&rs=VET&method=insertVideo&' + 'url=' + escape($G('VideoEmbedUrl').value), callback);
+	VET_asyncTransaction = YAHOO.util.Connect.asyncRequest('POST', wgScriptPath + '/index.php', callback, 'action=ajax&rs=VET&method=insertVideo&url=' + escape($G('VideoEmbedUrl').value));
 }
 
