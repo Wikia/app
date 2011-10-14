29aba572c7ac698a821eccede0fb7a278c8b9e4b
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-14 18:49:32 +0000

    ApiGate: some plumbing for processing the registration submission.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43528 e846bd91-c042-42a7-a218-4b65e2c1904d

--- lib/ApiGate/templates/register.php
+++ lib/ApiGate/templates/register.php
@@ -2,12 +2,17 @@
 <h1><?= i18n( 'apigate-register-heading' ) ?></h1>
 <form method="post" action="">
 	<div>
+
+		<!-- TODO: Place for displaying error messages -->
+
+		<input type='hidden' name='formName' value='apiGate_register'/>
 		<?= i18n( 'apigate-register-name' ); ?><br/>
 		<input type='text' name='firstName' placeholder='<?= i18n( 'apigate-register-hint-firstname' ); ?>' value='<?= $firstName ?>' style='width:192px'/>
 		&nbsp;<input type='text' name='lastName' placeholder='<?= i18n( 'apigate-register-hint-lastname' ); ?>' value='<?= $lastName ?>' style='width:192px'/><br/>
 		<?= i18n( 'apigate-register-email' ); ?><br/>
 		<input type='text' name='email_1' placeholder='<?= i18n( 'apigate-register-hint-email' ); ?>' value='<?= $email_1 ?>' style='width:400px'/><br/>
 		<input type='text' name='email_2' placeholder='<?= i18n( 'apigate-register-hint-confirm-email' ); ?>' value='<?= $email_2 ?>' style='width:400px'/><br/>
+		<br/>
 		<input type='submit' value='<?= i18n( 'apigate-register-submit' ) ?>'/>
 	</div>
 </form>
