f70be1e8366813d08766a145acebb05f4f4f13e0
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-14 15:04:10 +0000

    wikia tracking: desperate r43423 reverted
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43520 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/liftium/Liftium.js
+++ extensions/wikia/AdEngine/liftium/Liftium.js
@@ -355,8 +355,6 @@ Liftium.callInjectedIframeAd = function (sizeOrSlot, iframeElement){
 	// this is a(n ugly?) shortcut, the right name would be slotname's parent div
 	var placement = iframeElement.id.replace(/_iframe$/, "");
 	WikiaTracker.track(Liftium.buildTrackUrl([LiftiumOptions.pubid, "slot", sizeOrSlot + "_" + placement]), 'liftium.slot');
-	WikiaTracker.track(Liftium.buildTrackUrl([LiftiumOptions.pubid, "slot", sizeOrSlot + "_" + placement]), 'liftium.slot2');
-	Liftium.trackEvent(Liftium.buildTrackUrl(["slot", sizeOrSlot + "_" + placement]), "UA-17475676-17");
 
 	var t = Liftium.getNextTag(slotname);
 	if (!t) {
