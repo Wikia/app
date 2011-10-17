69429dc3fca0bcaf4295fb72ebf8e51ab6efdf50
nandy <nandy@wikia-inc.com>
2011-10-17 11:55:56 +0000

    removed a typo
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43562 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/BlogsFollowJob.php
+++ extensions/wikia/Blogs/BlogsFollowJob.php
@@ -7,7 +7,7 @@
  * @ingroup JobQueue
  *
  * @copyright Copyright © Krzysztof Krzyżaniak for Wikia Inc.
- * @author Krzysztof Krzyżaniak (eloy) <eloy@wlikia-inc.com>
+ * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
  * @date 2010-07-19
  * @version 1.0
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
