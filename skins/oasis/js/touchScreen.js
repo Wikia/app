71705c305e1036fb9de7f211179a0b5efcde4767
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-18 17:42:03 +0000

    TouchScreens: fix for navs global and wiki (BugId: 12302)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43663 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/touchScreen.js
+++ skins/oasis/js/touchScreen.js
@@ -1,36 +1,44 @@
 $(document).ready(function() {
-	iPadImprovements.init();
+	touchImprovements.init();
 });
 
-iPadImprovements = {
+touchImprovements = {
 	
 	init: function() {
 		
 		//onload scroll to main content
 		var wikiaMainContent = $( '#WikiaMainContent' ).offset();
-		window.scrollTo( 0, wikiaMainContent.top );
+		window.scrollTo( 0, wikiaMainContent.top ),
+		wikiUls = $('#WikiHeader nav > ul > li > ul'),
+		globalUls = $('#GlobalNavigation li > ul');
 
 		//global nav fix: first click opens nav,  second redirects to a hub
 		$( '#GlobalNavigation' ).delegate( 'li > a', 'click', function( event ) {
 			var next = $( this ).next();
-			if ( next.hasClass( 'subnav' ) && !next.hasClass( 'show' ) ) {
+			if ( next.hasClass( 'subnav' ) && !next.hasClass('open') ) {
 				event.preventDefault();
+				globalUls.removeClass('open');
+				next.addClass('open');
 			};
 		});
 
 		//wiki nav fix: first click opens nav, second redirects to a desired page
 		$( '#WikiHeader' ).delegate( 'nav > ul > li > a', 'click', function() {
-			var subnav = $( this ).next( 'ul.subnav' );
-			if ( subnav.length && subnav.css( 'display' ) != 'block') {
+			var subnav = $( this ).next( 'ul' );
+			if ( subnav.length && !subnav.hasClass('open')) {
 				event.preventDefault();
+				wikiUls.removeClass('open');
+				subnav.addClass('open');
 			};
 		});
 		
 		//user menu fix:
 		$( '#AccountNavigation' ).delegate( 'a', 'click', function( event ) {
 			var next = $( this ).next();
-			if ( next.hasClass( 'subnav' ) && !next.hasClass( 'show' ) ) {
+			if ( next.hasClass( 'subnav' ) && !next.hasClass('open') ) {
 				event.preventDefault();
+				$('#GlobalNavigation li > ul').removeClass('open');
+				next.addClass('open');
 			};
 		});
 	}
