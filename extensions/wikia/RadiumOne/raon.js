8477434aedc9e977fc5328fda6757c5a4b2b4a55
will <will@wikia-inc.com>
2011-10-19 12:45:26 +0000

    Bug ID 10111: next version of code after review
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43725 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RadiumOne/raon.js
+++ extensions/wikia/RadiumOne/raon.js
@@ -2,24 +2,20 @@ var RadiumOne = {
 	containerId: 'RadiumOne',
 	cookieName: 'raon',
 	enabledComscoreCats: ['Lifestyle'],
+	pixelUrl: 'http://rs.gwallet.com/r1/pixel/x2073r',
 	
 	init: function() {
 		if (window.wgIntegrateRadiumOne
 		|| $.inArray(window.cscoreCat, RadiumOne.enabledComscoreCats) != -1) {
 			// if no cookie, get pixel (which sets cookie)
 			if (document.cookie.indexOf( RadiumOne.cookieName+'=' ) == -1) {
-				$('#'+RadiumOne.containerId).append('<img src="http://rs.gwallet.com/r1/pixel/x2073r'+Math.round(Math.random()*10000000)+'" width="1" height="1" border="0" alt=""/>');
+				new Image().src = RadiumOne.pixelUrl+Math.round(Math.random()*10000000);
 			}			
 		}
 	},
 	
 	getDARTKeyValue: function () {
-		var value = 0;
-		var cookie = $.cookies.get(RadiumOne.cookieName);
-		if (cookie) {
-			value = cookie;
-			
-		}
+		var value = $.cookies.get(RadiumOne.cookieName) || 0;
 		var kv = 'ro='+value+';';
 		return kv;
 	}
