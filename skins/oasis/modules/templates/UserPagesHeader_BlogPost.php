0ed2a3c287c02407290fab932909ac4b7b1a038c
nandy <nandy@wikia-inc.com>
2011-10-19 14:02:08 +0000

    added header tag to the template; bugId:13703
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43743 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/UserPagesHeader_BlogPost.php
+++ skins/oasis/modules/templates/UserPagesHeader_BlogPost.php
@@ -1,19 +1,21 @@
-<div id="WikiaUserPagesHeader" class="WikiaUserPagesHeader WikiaBlogPostHeader">
-	<?= wfRenderModule('CommentsLikes', 'Index', array('comments' => $comments, 'likes' => $likes)); ?>
-	<?php
-		if (!empty($actionMenu['action'])) {
-			echo wfRenderModule('MenuButton', 'Index', array(
-				'action' => $actionMenu['action'],
-				'dropdown' => $actionMenu['dropdown'],
-				'image' => MenuButtonModule::EDIT_ICON,
-			));
-		}
-	?>
-	<h1><?= htmlspecialchars($title) ?></h1>
-
-	<div class="author-details">
-		<?= $avatar ?>
-		<span class="post-author"><a href="<?= htmlspecialchars($userPage) ?>"><?= htmlspecialchars($userName) ?></a></span>
-		<span><?= $editTimestamp ?></span>
+<header>
+	<div id="WikiaUserPagesHeader" class="WikiaUserPagesHeader WikiaBlogPostHeader">
+		<?= wfRenderModule('CommentsLikes', 'Index', array('comments' => $comments, 'likes' => $likes)); ?>
+		<?php
+			if (!empty($actionMenu['action'])) {
+				echo wfRenderModule('MenuButton', 'Index', array(
+					'action' => $actionMenu['action'],
+					'dropdown' => $actionMenu['dropdown'],
+					'image' => MenuButtonModule::EDIT_ICON,
+				));
+			}
+		?>
+		<h1><?= htmlspecialchars($title) ?></h1>
+	
+		<div class="author-details">
+			<?= $avatar ?>
+			<span class="post-author"><a href="<?= htmlspecialchars($userPage) ?>"><?= htmlspecialchars($userName) ?></a></span>
+			<span><?= $editTimestamp ?></span>
+		</div>
 	</div>
-</div>
+</header>
