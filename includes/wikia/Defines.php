dad92f66eb5405ff22434255f3e3b2ba414ab48b
macbre <macbre@wikia-inc.com>
2011-10-20 14:15:53 +0000

    Places: add wikia_page_props constants (BugId:12723)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43839 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/Defines.php
+++ includes/wikia/Defines.php
@@ -17,15 +17,16 @@ define("DB_DPL", 3);
 define('NS_FORUM', 110);
 define('NS_FORUM_TALK', 111);
 
-
 /*
- * wikia page props type 
+ * wikia page props type
  *
  */
 
-define( "WPP_IMAGE_SERVING", 0 );
-define( "WPP_PLB_PROPS", 1 );
-define( "WPP_PLB_LAYOUT_DELETE", 2);
-define( "WPP_PLB_LAYOUT_NOT_PUBLISH", 3);
-define( "WPP_BLOGS_VOTING", 4);
-define( "WPP_BLOGS_COMMENTING", 5);
+define("WPP_IMAGE_SERVING", 0);
+define("WPP_PLB_PROPS", 1);
+define("WPP_PLB_LAYOUT_DELETE", 2);
+define("WPP_PLB_LAYOUT_NOT_PUBLISH", 3);
+define("WPP_BLOGS_VOTING", 4);
+define("WPP_BLOGS_COMMENTING", 5);
+define("WPP_PLACES_LATITUDE", 6);
+define("WPP_PLACES_LONGITUDE", 7);
