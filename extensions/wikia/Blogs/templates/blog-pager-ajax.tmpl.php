50b614266fa9ea6b7cbfe9045d18f2dd500f7efe
hyun <hyun@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-20 19:18:14 +0000

    bugId::9334 Bloglisting re-styling and new paginator
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43851 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Blogs/templates/blog-pager-ajax.tmpl.php
+++ extensions/wikia/Blogs/templates/blog-pager-ajax.tmpl.php
@@ -1,15 +1,12 @@
 <!-- s:<?= __FILE__ ?> -->
 <? 
-$i = 0; $prevPage = $nextPage = "";
-if ($iPageCount > 1) {
-	if ( $iPage != 0 ) { 
-		$prevPage = '<a href="javascript:wkBlogShowPage(-1);" id="wk-blog-prev-link">&#171; '.wfMsg('blog-newerposts').'</a>';
-	} 
-	if ( ($iPage + 1) != $iPageCount ) {
-		$nextPage = '<a href="javascript:wkBlogShowPage(1);" id="wk-blog-next-link">'.wfMsg('blog-olderposts').' &#187;</a>';
-	}	
+if ($iPageCount > 1) {	
 ?>	
+<?
+	$pages = Paginator::newFromArray( $iTotal, $iCount, 3, false, '', 50 );
+	$pages->setActivePage($iPage);
+	echo $pages->getBarHTML( '', 'BlogPaginator' );
+?>
 <script type="text/javascript" src="<?=$wgExtensionsPath?>/wikia/Blogs/js/BlogsPager.js?<?=$wgStyleVersion?>"></script>
-<div style="text-align:right; padding:2px 5px;"><?=$prevPage?>&nbsp;&nbsp;<strong id="wk-blog-current-page"><?=($iPage+1)?></strong>&nbsp;&nbsp;<?=$nextPage?></div>
 <? } ?>
 <!-- e:<?= __FILE__ ?> -->
