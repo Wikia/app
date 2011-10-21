d157a24749fb156895a8b123c53de10d88ca5c1a
jakub <jakub@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-21 15:52:42 +0000

    Places up
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43885 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Places/templates/Places_placeFromModel.php
+++ extensions/wikia/Places/templates/Places_placeFromModel.php
@@ -2,6 +2,5 @@
 	<a><img src="<?=$url;?>" width="<?=$width;?>" height="<?=$height;?>" data-zoom="<?=$zoom;?>" data-lat="<?=$lat;?>" data-lon="<?=$lon;?>" /></a>
 	<figcaption class="thumbcaption">
 		<span class="geo"><?=$lat;?>; <?=$lon;?></span>
-		<meta name="geo.position" content="<?=$lat;?>;<?=$lon;?>">
 	</figcaption>
 </figure>
