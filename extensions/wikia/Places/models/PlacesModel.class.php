c0b459ab8138659761d0fe458755c8e7d92cd23c
macbre <macbre@wikia-inc.com>
2011-10-24 14:36:05 +0000

    Places: add PlacesModel::getAll() (BugId:12723)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@43952 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Places/models/PlacesModel.class.php
+++ extensions/wikia/Places/models/PlacesModel.class.php
@@ -1,4 +1,78 @@
 <?php
-class PlacesModel {
 
+/**
+ * PlacesModel
+ *
+ * Allows to get array of PlaceModel objects representing geo data of:
+ *  - all articles on a wiki
+ *  - all articles from given categories
+ *  - all articles tagged as being "nearby" given place
+ */
+
+ class PlacesModel {
+
+	const WPP_TABLE = 'page_wikia_props';
+
+	private $app;
+
+	public function __construct() {
+		$this->app = F::app();
+	}
+
+	/**
+	 * Get geodata for all tagged articles on a wiki
+	 *
+	 * @return array set of PlaceModel objects
+	 */
+	public function getAll() {
+		$dbr = $this->getDB();
+		$attrs = array();
+
+		$res = $dbr->select(self::WPP_TABLE, array('page_id', 'propname', 'props'), array(
+			'propname' => array(WPP_PLACES_LATITUDE, WPP_PLACES_LONGITUDE),
+		), __METHOD__);
+
+		while($row = $res->fetchObject()) {
+			$value = (float) $row->props;
+			$pageId = intval($row->page_id);
+
+			$attrs[$pageId]['pageId'] = $pageId;
+
+			switch($row->propname) {
+				case WPP_PLACES_LATITUDE:
+					$attrs[$pageId]['lat'] = $value;
+					break;
+
+				case WPP_PLACES_LONGITUDE:
+					$attrs[$pageId]['lon'] = $value;
+					break;
+			}
+		}
+
+		// we now have lat/lon data in $cords array
+		// now let's create PlaceModel objects
+		$models = array();
+
+		foreach($attrs as $attr) {
+			$models[] = F::build('PlaceModel', array($attr), 'newFromAttributes');
+		}
+
+		return $models;
+	}
+
+	public function getFromCategories($categories) {
+
+	}
+
+	public function getNearby(PlaceModel $center) {
+
+	}
+
+	public function getNearbyByTitle(Title $center) {
+
+	}
+
+	private function getDB($type = DB_SLAVE) {
+		return $this->app->wf->GetDB($type, array(), $this->app->wg->DBname);
+	}
 }
