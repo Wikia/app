17497331c00c0b002d3051746b0385e9f65509ea
will <will@wikia-inc.com>
2011-10-25 22:08:41 +0000

    Bug ID 14000: remove aperture audience data collection js from AssetsManager. remove quantcast segments js from AssetsManager
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44053 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -112,8 +112,6 @@ $config['oasis_extensions_js'] = array(
 		'//extensions/wikia/AdEngine/LazyLoadAds.js',
 		'//extensions/wikia/AdEngine/ghost/gw-11.6.7/lib/gw.min.js',
 		'//extensions/wikia/Geo/geo.js',
-		'//extensions/wikia/QuantcastSegments/qcs.js',
-		'//extensions/wikia/ApertureAudience/Aperture.js',
 		'//extensions/wikia/RadiumOne/raon.js',
 		'//extensions/wikia/AdEngine/liftium/Liftium.js',
 		'//extensions/wikia/AdEngine/liftium/Wikia.js',
