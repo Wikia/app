d29c333bc038458a62eb5267cbf3f7cfb79a4059
marcin <marcin@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-25 09:36:00 +0000

    SSW: Tidy User Preferences: Tab1 (My info) done (bugid:12540)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44013 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserPreferencesV2/UserPreferencesV2.i18n.php
+++ extensions/wikia/UserPreferencesV2/UserPreferencesV2.i18n.php
@@ -3,4 +3,15 @@ $messages = array();
 
 $messages['en'] = array(
 	'prefs-personal' => 'My info',
+	'preverences-v2-username' => 'My username:',
+	'preverences-v2-usergroups' => 'My user groups:',
+	'preverences-v2-gender' => 'My gender:',
+	'preverences-v2-password' => 'My password:',
+	'preverences-v2-oldsig' => 'Existing signature:',
+	'preverences-v2-nickname' => 'Custom signature:',
+	'preverences-v2-fancysig' => 'I want to use wikitext in my signature<br />[[Help:Signature|What are signatures?]]',
+	'prefs-appearance' => 'Appearance',
+	'preverences-v2-date' => 'Date:',
+	'preverences-v2-time' => 'Time:',
+	'preverences-v2-skin' => 'Layout:',
 );
