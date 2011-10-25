2279cfc99c099e02b349a3b8e48fac8726b55a2c
jakub <jakub@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-25 13:00:51 +0000

    Places: another mode for special page
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44021 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Places/PlacesSpecialController.class.php
+++ extensions/wikia/Places/PlacesSpecialController.class.php
@@ -16,15 +16,15 @@ class PlacesSpecialController extends WikiaSpecialPageController {
 	}
 
 	public function index(){
-
 		$this->response->addAsset('extensions/wikia/Places/js/Places.js');
 		$sParam = $this->getPar();
-		$oTitle = F::build( 'Title', array( $sParam ) );
+		$oTitle = F::build( 'Title', array( $sParam ), 'newFromText' );
 		if ( !empty( $oTitle ) && $oTitle->exists() ){
 			if ( $oTitle->getNamespace() == NS_CATEGORY ){
-				$this->placesForCategory();
+				$this->placesForCategory( $oTitle );
 			} else {
-				$this->center = $oTitle;
+				$oMarker = F::build( 'PlaceStorage', array( $oTitle ), 'newFromTitle' )->getModel();
+				$this->center = $oMarker->getForMap();
 				$this->allPlaces();
 			}
 		} else {
@@ -35,14 +35,18 @@ class PlacesSpecialController extends WikiaSpecialPageController {
 		$this->setVal( 'markers', $this->markers );
 	}
 
-	protected function placesForCategory(){
-		$this->markers = array();
+	protected function placesForCategory( $oTitle ){
+		$placesModel = F::build( 'PlacesModel' );
+		$this->prepareMarkers( $placesModel->getFromCategories( $oTitle->getText() ) );
 	}
 
 	protected function allPlaces(){
 		$placesModel = F::build( 'PlacesModel' );
-		$markers = $placesModel->getAll();
-		foreach( $markers as $oMarker ){
+		$this->prepareMarkers( $placesModel->getAll() );
+	}
+
+	protected function prepareMarkers( $aMarkers ) {
+		foreach( $aMarkers as $oMarker ){
 			$aMapParams = $oMarker->getForMap();
 			if ( !empty( $aMapParams ) ){
 				$this->markers[] = $oMarker->getForMap();
