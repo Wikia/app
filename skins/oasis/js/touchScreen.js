343986c6146e09c0e36dd2695a21601fb250f21a
jolek <jolek@wikia-inc.com>
2011-10-25 19:08:18 +0000

    JSLint issues (BugId:13998)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44045 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/touchScreen.js
+++ skins/oasis/js/touchScreen.js
@@ -7,26 +7,27 @@ touchImprovements = {
 	init: function() {
 		
 		//onload scroll to main content
-		var wikiaMainContent = $( '#WikiaMainContent' ).offset();
-		window.scrollTo( 0, wikiaMainContent.top ),
+		var wikiaMainContent = $('#WikiaMainContent').offset(),
 		wikiUls = $('#WikiHeader nav > ul > li > ul'),
 		globalUls = $('#GlobalNavigation li > ul');
+		
+		window.scrollTo(0, wikiaMainContent.top);
 
 		//global nav fix: first click opens nav,  second redirects to a hub
-		$( '#GlobalNavigation' ).delegate( 'li > a', 'click', function( event ) {
-			var next = $( this ).next();
-			if ( !next.hasClass('open') ) {
+		$('#GlobalNavigation').delegate('li > a', 'click', function(event) {
+			var next = $(this).next();
+			if (!next.hasClass('open')) {
 				event.preventDefault();
-				globalUls.removeClass('open');
+				globalUls.removeClass('open'); 
 				next.addClass('open');
 			};
 		});
 		
 
 		//wiki nav fix: first click opens nav, second redirects to a desired page
-		$( '#WikiHeader' ).delegate( 'nav > ul > li > a', 'click', function() {
-			var subnav = $( this ).next( 'ul' );
-			if ( subnav.length && !subnav.hasClass('open')) {
+		$('#WikiHeader').delegate('nav > ul > li > a', 'click', function(event) {
+			var subnav = $(this).next('ul');
+			if (subnav.length && !subnav.hasClass('open')) {
 				event.preventDefault();
 				wikiUls.removeClass('open');
 				subnav.addClass('open');
@@ -34,11 +35,9 @@ touchImprovements = {
 		});
 		
 		//user menu fix:
-		$( '#AccountNavigation' ).delegate( 'li > a[accesskey="."]', 'click', function( event ) {
-			var next = $( this ).next( 'ul.subnav' );
-			alert(next);
-			if ( next && !next.hasClass('open') ) {
-				alert(next);
+		$('#AccountNavigation').delegate('li > a[accesskey="."]', 'click', function(event) {
+			var next = $(this).next('ul.subnav');
+			if (next && !next.hasClass('open')) {
 				event.preventDefault();
 				next.addClass('open');
 			};
