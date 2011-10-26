537c7b1c4b1101931b753ca65a6b6850f9e2168c
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-26 13:11:17 +0000

    Places: render <place> tag "visually" in RTE (BugId:12723)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44076 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Places/templates/Places_placeFromModel.php
+++ extensions/wikia/Places/templates/Places_placeFromModel.php
@@ -1,6 +1,16 @@
+<?php
+	if ($rteData !== false) {
+?>
+	<img<?=$rteData;?>class="thumb t<?=$align;?> src="<?=$url;?>" width="<?=$width;?>" height="<?=$height;?>" />
+<?php
+	} else {
+?>
 <figure class="thumb t<?=$align;?> thumbinner placemap" style="width:<?=$width+2;?>px;">
 	<a><img src="<?=$url;?>" width="<?=$width;?>" height="<?=$height;?>" data-zoom="<?=$zoom;?>" data-lat="<?=$lat;?>" data-lon="<?=$lon;?>" /></a>
 	<figcaption class="thumbcaption">
 		<span class="geo"><?=$lat;?>; <?=$lon;?></span>
 	</figcaption>
 </figure>
+<?php
+	}
+?>
