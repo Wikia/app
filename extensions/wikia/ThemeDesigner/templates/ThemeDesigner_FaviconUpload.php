ca9ee9f14665f91db7eda889264c8796b3c1a276
hyun <hyun@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-10-26 18:38:03 +0000

    bugId::12315 Favicon upload work in progress
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44116 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ThemeDesigner/templates/ThemeDesigner_FaviconUpload.php
+++ extensions/wikia/ThemeDesigner/templates/ThemeDesigner_FaviconUpload.php
@@ -1,1 +1,1 @@
-<?= json_encode(array('faviconImageUrl' => $faviconImageUrl, 'errors' => $errors)) ?>
+<?= json_encode(array('faviconImageUrl' => $faviconImageUrl, 'faviconImageName' => $faviconImageName, 'errors' => $errors)) ?>
