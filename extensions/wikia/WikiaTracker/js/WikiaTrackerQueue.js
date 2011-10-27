ea2319930b6c59350c7647fcbe54cfe7b6d732e1
macbre <macbre@wikia-inc.com>
2011-10-27 11:03:31 +0000

    WikiaTracker: synchronize wtq with beacon id (BugId:14021)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44181 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaTracker/js/WikiaTrackerQueue.js
+++ extensions/wikia/WikiaTracker/js/WikiaTrackerQueue.js
@@ -1,16 +1,40 @@
 /*global WikiaTracker: true */
 var WikiaTrackerQueue = {
 
+	// check beacon_id every second
+	POLL_INTERVAL: 1000,
+	pollIntervalId: false,
+
 	trackFn: false,
 
 	init: function() {
-		var queue = window._wtq || [],
-			item;
+		$().log('init', 'wtq');
 
 		// set tracking function - queued items will be passed there
 		this.trackFn = $.proxy(WikiaTracker.track, WikiaTracker);
 
-		// move queued items to WikiaTracker
+		// check whether beacon_id exists every POLL_INTERVAL ms
+		this.pollIntervalId = setInterval($.proxy(this.pollBeaconId, this), this.POLL_INTERVAL);
+		this.pollBeaconId();
+	},
+
+	pollBeaconId: function() {
+		if (typeof window.beacon_id != 'undefined') {
+			clearInterval(this.pollIntervalId);
+
+			$().log('beacon_id has arrived', 'wtq');
+			this.moveQueue();
+		}
+		else {
+			$().log('beacon_id is not here yet...', 'wtq');
+		}
+	},
+
+	// move queued items to WikiaTracker
+	moveQueue: function() {
+		var queue = window._wtq || [],
+			item;
+
 		while ((item = queue.shift()) !== undefined) {
 			this.pushCallback(item);
 		}
@@ -28,9 +52,6 @@ var WikiaTrackerQueue = {
 
 		this.trackFn.apply(this, item);
 	}
-
 };
 
-$(function() {
-	WikiaTrackerQueue.init();
-});
+WikiaTrackerQueue.init();
