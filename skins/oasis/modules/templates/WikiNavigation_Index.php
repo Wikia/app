e214944c26245437bbd767ebee3f86e77fabe667
macbre <macbre@wikia-inc.com>
2011-11-03 12:57:09 +0000

    add chevrons to second-level navigation (BugId:13667)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44435 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/WikiNavigation_Index.php
+++ skins/oasis/modules/templates/WikiNavigation_Index.php
@@ -1,83 +1,87 @@
 		<ul>
-
 <?php
 $counter = 0;
 $firstChild = true;
 foreach ( array( $wikiaMenuNodes, $wikiMenuNodes ) as $menuNodes )
 if ( is_array($menuNodes) && isset($menuNodes[0]) && $showMenu) {
 	foreach ($menuNodes[0][ NavigationService::CHILDREN ] as $level0) {
-		if ($menuNodes[$level0][ NavigationService::TEXT ]) {
+		$menuNode0 = $menuNodes[$level0];
+
+		if ($menuNode0[ NavigationService::TEXT ]) {
 ?>
 			<li<?php echo ($counter == 0 ) ? ' class="marked"' : '';
 				$counter++;
 			?>>
-				<a<?= empty( $menuNodes[$level0][ NavigationService::SPECIAL ] ) ? '' : ' data-extra="'.$menuNodes[$level0][ NavigationService::SPECIAL ].'"' ?> href="<?= $menuNodes[$level0][ NavigationService::HREF ] ?>"><?= $menuNodes[$level0][ NavigationService::TEXT ] ?></a>
+				<a<?= empty( $menuNode0[ NavigationService::SPECIAL ] ) ? '' : ' data-extra="'.$menuNode0[ NavigationService::SPECIAL ].'"' ?> href="<?= $menuNode0[ NavigationService::HREF ] ?>"><?= $menuNode0[ NavigationService::TEXT ] ?></a>
 <?php
-		if (isset($menuNodes[$level0][ NavigationService::CHILDREN ])) {
+			if (isset($menuNodes[$level0][ NavigationService::CHILDREN ])) {
 ?>
 				<ul class="subnav-2 accent"<? if ( $firstChild ){ echo ' style="display:block"'; $firstChild = false; } ?>>
 <?php
-			foreach ($menuNodes[$level0][ NavigationService::CHILDREN ] as $level1) {
+				foreach ($menuNodes[$level0][ NavigationService::CHILDREN ] as $level1) {
+					$menuNode1 = $menuNodes[$level1];
+					$hasChildNodes = isset($menuNode1[ NavigationService::CHILDREN ]);
 ?>
 					<li>
-						<a class="subnav-2a"<? if($menuNodes[$level1][ NavigationService::TEXT ] == 'Chat'): ?> onclick="$.openPopup('/wiki/Special:Chat','wikiachat','ChatRail',600,600); event.preventDefault();"<? endif; ?><?= empty( $menuNodes[$level1][ NavigationService::SPECIAL ] ) ? '' : ' data-extra="'.$menuNodes[$level1][ NavigationService::SPECIAL ].'"' ?> href="<?= $menuNodes[$level1][ NavigationService::HREF ] ?>" <?php if( strpos($menuNodes[$level1][ NavigationService::HREF ], 'Special:Random') !== false ):?>accesskey="x"<?php endif; ?>><?= $menuNodes[$level1][ NavigationService::TEXT ] ?></a>
+						<a class="subnav-2a"<? if($menuNode1[ NavigationService::TEXT ] == 'Chat'): ?> onclick="$.openPopup('/wiki/Special:Chat','wikiachat','ChatRail',600,600); event.preventDefault();"<? endif; ?><?= empty( $menuNode1[ NavigationService::SPECIAL ] ) ? '' : ' data-extra="'.$menuNode1[ NavigationService::SPECIAL ].'"' ?> href="<?= $menuNode1[ NavigationService::HREF ] ?>" <?php if( strpos($menuNode1[ NavigationService::HREF ], 'Special:Random') !== false ):?>accesskey="x"<?php endif; ?>><?= $menuNode1[ NavigationService::TEXT ] ?><?php if($hasChildNodes):?><img src="<?= wfBlankImgUrl() ?>" class="chevron"><?php endif; ?></a>
 <?php
-				if (isset($menuNodes[$level1][ NavigationService::CHILDREN ])) {
+					if ($hasChildNodes) {
 ?>
 						<ul class="subnav subnav-3">
 <?php
-					foreach ($menuNodes[$level1][ NavigationService::CHILDREN ] as $level2) {
+						foreach ($menuNode1[ NavigationService::CHILDREN ] as $level2) {
+							$menuNode2 = $menuNodes[$level2];
 ?>
 							<li>
-								<a class="subnav-3a"<?= empty( $menuNodes[$level2][ NavigationService::SPECIAL ] ) ? '' : ' data-extra="'.$menuNodes[$level2][ NavigationService::SPECIAL ].'"' ?> href="<?= $menuNodes[$level2][ NavigationService::HREF ] ?>"><?= $menuNodes[$level2][ NavigationService::TEXT ] ?></a>
+								<a class="subnav-3a"<?= empty( $menuNode2[ NavigationService::SPECIAL ] ) ? '' : ' data-extra="'.$menuNode2[ NavigationService::SPECIAL ].'"' ?> href="<?= $menuNode2[ NavigationService::HREF ] ?>"><?= $menuNode2[ NavigationService::TEXT ] ?></a>
 							</li>
 <?php
-					}
+						}
 ?>
 						</ul>
 <?php
-				}
+					}
 ?>
 					</li>
 <?php
-			}
+				}
 ?>
 <?php
-			if (
-				!empty( $wikiaMenuLocalNodes ) &&
-				isset( $wikiaMenuLocalNodes[0] ) &&
-				isset( $wikiaMenuLocalNodes[0][ NavigationService::CHILDREN ] )
-			)
-				foreach ( $wikiaMenuLocalNodes[0][ NavigationService::CHILDREN ] as $level1 ){
+				if (
+					!empty( $wikiaMenuLocalNodes ) &&
+					isset( $wikiaMenuLocalNodes[0] ) &&
+					isset( $wikiaMenuLocalNodes[0][ NavigationService::CHILDREN ] )
+				)
+					foreach ( $wikiaMenuLocalNodes[0][ NavigationService::CHILDREN ] as $level1 ){
 ?>
 					<li>
 						<a class="subnav-2a"<?= empty( $wikiaMenuLocalNodes[$level1][ NavigationService::SPECIAL ] ) ? '' : ' data-extra="'.$wikiaMenuLocalNodes[$level1][ NavigationService::SPECIAL ].'"' ?> href="<?= $wikiaMenuLocalNodes[$level1][ NavigationService::HREF ] ?>">
 							<?= $wikiaMenuLocalNodes[$level1][ NavigationService::TEXT ] ?>
 						</a>
 <?php
-				if (isset($wikiaMenuLocalNodes[$level1][ NavigationService::CHILDREN ])) {
+					if (isset($wikiaMenuLocalNodes[$level1][ NavigationService::CHILDREN ])) {
 ?>
 						<ul class="subnav subnav-3">
 <?php
-					foreach ($wikiaMenuLocalNodes[$level1][ NavigationService::CHILDREN ] as $level2) {
+						foreach ($wikiaMenuLocalNodes[$level1][ NavigationService::CHILDREN ] as $level2) {
 ?>
 							<li>
 								<a class="subnav-3a"<?= empty( $wikiaMenuLocalNodes[$level2][ NavigationService::SPECIAL ] ) ? '' : ' data-extra="'.$wikiaMenuLocalNodes[$level2][ NavigationService::SPECIAL ].'"' ?> href="<?= $wikiaMenuLocalNodes[$level2][ NavigationService::HREF ] ?>"><?= $wikiaMenuLocalNodes[$level2][ NavigationService::TEXT ] ?></a>
 							</li>
 <?php
-					}
+						}
 ?>
 						</ul>
 <?php
-				}
+					}
 ?>
 					</li>
 <?php
-			}
+				}
 ?>
 				</ul>
 <?php
-		}
+			}
 ?>
 			</li>
 <?php
