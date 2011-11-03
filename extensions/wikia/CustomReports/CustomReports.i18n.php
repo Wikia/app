1328a01d8fe925bb00066b130c26a512e0f63743
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-11-03 21:38:32 +0000

    Tweak messages.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44459 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CustomReports/CustomReports.i18n.php
+++ extensions/wikia/CustomReports/CustomReports.i18n.php
@@ -4,33 +4,31 @@ $messages = array();
 
 $messages['en'] = array(
 	'report-xaxis' => 'Date',
-	
-	'report-name-new_wikis' => 'New Wikis by Day',
-	'report-yaxis-new_wikis' => 'Wikis',
-	
-	'report-name-founderemails' => 'Founder Emails',
-	'report-name-founderemails_sent' => 'Founder Emails: Emails Sent by Day',
-	'report-yaxis-founderemails_sent' => 'Emails Sent',
-	'report-name-founderemails_opens' => 'Founder Emails: Emails Opened by Day',
-	'report-yaxis-founderemails_opens' => 'Emails Opened',
-	'report-name-founderemails_clicks' => 'Founder Emails: Emails Clicked by Day',
-	'report-yaxis-founderemails_clicks' => 'Clicks',
-	'report-name-founderemails_clicks_per_sent' => 'Founder Emails: Click Rate by Day',
-	'report-yaxis-founderemails_clicks_per_sent' => 'Clicks/Sent (%25)',
-	'report-name-founderemails_opens_per_sent' => 'Founder Emails: Open Rate by Day',
-	'report-yaxis-founderemails_opens_per_sent' => 'Opens/Sent (%25)',
-	
-	'report-name-allemails' => 'All Emails',
-	'report-name-allemails_sent' => 'All Emails: Emails Sent by Day',
-	'report-yaxis-allemails_sent' => 'Emails Sent',
-	'report-name-allemails_opens' => 'All Emails: Emails Opened by Day',
-	'report-yaxis-allemails_opens' => 'Emails Opened',
-	'report-name-allemails_clicks' => 'All Emails: Emails Clicked by Day',
-	'report-yaxis-allemails_clicks' => 'Clicks',
-	'report-name-allemails_clicks_per_sent' => 'All Emails: Click Rate by Day',
-	'report-yaxis-allemails_clicks_per_sent' => 'Clicks/Sent (%25)',
-	'report-name-allemails_opens_per_sent' => 'All Emails: Open Rate by Day',
-	'report-yaxis-allemails_opens_per_sent' => 'Opens/Sent (%25)',
-	
-);
 
+	'report-name-new_wikis' => 'New wikis by day',
+	'report-yaxis-new_wikis' => 'Wikis',
+
+	'report-name-founderemails' => 'Founder Emails',
+	'report-name-founderemails_sent' => 'Founder Emails: E-mails sent by day',
+	'report-yaxis-founderemails_sent' => 'E-mails sent',
+	'report-name-founderemails_opens' => 'Founder Emails: E-mails opened by day',
+	'report-yaxis-founderemails_opens' => 'E-mails opened',
+	'report-name-founderemails_clicks' => 'Founder Emails: E-mails clicked by day',
+	'report-yaxis-founderemails_clicks' => 'Clicks',
+	'report-name-founderemails_clicks_per_sent' => 'Founder Emails: Click rate by day',
+	'report-yaxis-founderemails_clicks_per_sent' => 'Clicks/Sent (%25)',
+	'report-name-founderemails_opens_per_sent' => 'Founder Emails: Open rate by day',
+	'report-yaxis-founderemails_opens_per_sent' => 'Opens/sent (%25)',
+
+	'report-name-allemails' => 'All e-mails',
+	'report-name-allemails_sent' => 'All e-mails: E-mails sent by day',
+	'report-yaxis-allemails_sent' => 'E-mails Sent',
+	'report-name-allemails_opens' => 'All e-mails: E-mails opened by day',
+	'report-yaxis-allemails_opens' => 'E-mails opened',
+	'report-name-allemails_clicks' => 'All e-mails: E-mails clicked by day',
+	'report-yaxis-allemails_clicks' => 'Clicks',
+	'report-name-allemails_clicks_per_sent' => 'All e-mails: Click rate by day',
+	'report-yaxis-allemails_clicks_per_sent' => 'Clicks/sent (%25)',
+	'report-name-allemails_opens_per_sent' => 'All e-mails: Open rate by day',
+	'report-yaxis-allemails_opens_per_sent' => 'Opens/sent (%25)',
+);
