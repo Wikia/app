e6276063bf50ce00d975078ec20ba7fb8f9f5e60
siebrand <siebrand@wikia-inc.com>
2011-11-03 21:42:04 +0000

    Tweak messages.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44463 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialCategoryIntersection/SpecialCategoryIntersection.i18n.php
+++ extensions/wikia/SpecialCategoryIntersection/SpecialCategoryIntersection.i18n.php
@@ -8,20 +8,20 @@
 $messages = array();
 
 $messages['en'] = array(
-	'categoryintersection' => 'Category Intersection API Demo',
-	'categoryintersection-desc' => 'A front-end for the "categoryintersection" API function to show what it can use and assist in understanding it for actual use via the MediaWiki API.',
+	'categoryintersection' => 'Category Intersection API demonstration',
+	'categoryintersection-desc' => 'A front-end for the "categoryintersection" API function to show what it can use and assist in understanding it for actual use via the MediaWiki API',
 	'categoryintersection-header-title' => 'About',
 	'categoryintersection-header-body' => 'This page is designed to show the types of things that the CategoryIntersection API function can do and make it easy to learn how to use it.  For more information please see: $1',
 	'categoryintersection-docs-linktext' => 'Category Intersection MediaWiki API documentation',
-	'categoryintersection-form-title' => 'Try It Out',
-	'categoryintersection-form-submit' => 'Find Matches',
-	'categoryintersection-and' => ' - and - ',
+	'categoryintersection-form-title' => 'Try it out',
+	'categoryintersection-form-submit' => 'Find matches',
+	'categoryintersection-and' => '- and -',
 	'categoryintersection-limit' => 'Limit',
 	'categoryintersection-results-title' => 'Results',
 	'categoryintersection-noresults' => 'No results. Please check that the category names are correct if you think there should be results.',
-	'categoryintersection-query-used' => 'API Query Used:',
+	'categoryintersection-query-used' => 'API query used:',
 	'categoryintersection-footer-title' => 'Examples',
-	'categoryintersection-footer-body' => 'Here are some example queries, just to show a few of the types of things that can be done with this API. There are numerous other cool possibilies though... be creative!',
+	'categoryintersection-footer-body' => 'Here are some example queries, just to show a few of the types of things that can be done with this API. There are numerous other cool possibilies though... Be creative!',
 	'categoryintersection-summary' => 'Searched for categories: $1; limit: $2; number of results returned: $3',
 );
 
@@ -30,5 +30,5 @@ $messages['en'] = array(
  */
 $messages['qqq'] = array(
 	'categoryintersection-desc' => '{{desc}}',
-	'categoryintersection-and' => 'Displayed between consecutive boxes for categories.  Separates the categories visually but shows that adding more of them is a logical-and operation (ie: articles will have to be in ALL of the categories to be in the result set.'
+	'categoryintersection-and' => 'Displayed between consecutive boxes for categories. Separates the categories visually but shows that adding more of them is a logical-and operation (ie: articles will have to be in ALL of the categories to be in the result set.'
 );
