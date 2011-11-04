5c14972854c4b2a1f829107200cc957e53f24a92
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-11-04 10:53:58 +0000

    get function from mediawiki trunk
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44495 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/SiteStats.php
+++ includes/SiteStats.php
@@ -142,7 +142,7 @@ class SiteStats {
 			$hit = $wgMemc->get( $key );
 			if ( !$hit ) {
 				$dbr = wfGetDB( DB_SLAVE, 'vslow' );
-				$hit = $dbr->selectField( 'job', 'COUNT(*)', '', __METHOD__ );
+				$hit = $dbr->estimateRowCount( 'job' );
 				/* Zero rows still do single row read for row that doesn't exist, but people are annoyed by that */
 				if ($hit == 1) {
 					$hit = 0;
@@ -221,16 +221,16 @@ class SiteStatsUpdate {
 		$dbw = wfGetDB( DB_MASTER );
 
 		$updates = '';
-		
+
 		# fix (bugId: 10928)
-		# if ( ss_good_article == 0 ) 
+		# if ( ss_good_article == 0 )
 		# update site_stats set ss_good_articles = ss_good_articles - 1 where ss_row_id = 1;
-		# result is: 18446744073709551615 
-		$oRow = $dbw->selectRow( 'site_stats', '*', false, __METHOD__ );		
+		# result is: 18446744073709551615
+		$oRow = $dbw->selectRow( 'site_stats', '*', false, __METHOD__ );
 		$this->mViews = ( isset( $oRow->ss_total_views ) && ( $this->mViews + $oRow->ss_total_views ) < 0 ) ? 0 : $this->mViews;
 		$this->mEdits = ( isset( $oRow->ss_total_edits ) && ( $this->mEdits + $oRow->ss_total_edits ) < 0 ) ? 0 : $this->mEdits;
 		$this->mGood = ( isset( $oRow->ss_good_articles ) && ( $this->mGood + $oRow->ss_good_articles ) < 0 ) ? 0 : $this->mGood;
-		
+
 		$this->appendUpdate( $updates, 'ss_total_views', $this->mViews );
 		$this->appendUpdate( $updates, 'ss_total_edits', $this->mEdits );
 		$this->appendUpdate( $updates, 'ss_good_articles', $this->mGood );
