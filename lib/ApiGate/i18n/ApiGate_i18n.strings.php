7a125b76f7740155105448c6aa9d3c7678709859
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-11-08 02:20:53 +0000

    ApiGate: BugzId: 11428 - initial framework of the key-info form.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44638 e846bd91-c042-42a7-a218-4b65e2c1904d

--- lib/ApiGate/i18n/ApiGate_i18n.strings.php
+++ lib/ApiGate/i18n/ApiGate_i18n.strings.php
@@ -15,9 +15,11 @@ $messages['en'] = array(
 	'apigate' => 'API Gate',
 	'apigate-mysql-error' => 'There was a db error with query<br/>\"$1\"<br/><br/>Error was: \"$2\"',
 	
+	// Intro module on the main API Gate landing page
 	'apigate-intro-header' => 'Welcome to the API Control Panel, $1!',
 	'apigate-intro' => 'You will always be able to access this page by logging into your Wikia account and selecting "API Control Panel" in the user drop-down menu on the top, right corner of any page on Wikia or by going to api.wikia.com/Special:ApiGate.',
 
+	// Registration module
 	'apigate-register-title' => 'Get an API Key',
 	'apigate-register-heading' => 'Get an API Key',
 	'apigate-register-name' => 'Name*',
@@ -33,14 +35,28 @@ $messages['en'] = array(
 	'apigate-register-error-email-doesnt-match' => 'Email addresses entered do not match.',
 	'apigate-register-error-mysql_error' => 'Error trying to save new API key to the database.',
 	
+	// For the module which lists all keys for a user
 	'apigate-userkeys-intro' => 'Click on your keys below to view usage statistics for each key or edit your contact information (in case something goes wrong).',
 	'apigate-userkeys-footer' => 'If you need another key and want to track it in this account, please <a href="$1">create another key</a> while you are logged in to this account.<br/><br/>If you have any questions regarding our APIs, please contact us at $2. We would be happy to help out.',
 	'apigate-userkeys-disabled' => 'disabled',
+	
+	// Info page for an individual API key
+	'apigate-keyinfo-apiKey' => 'API Key: <strong>$1</strong>',
+	'apigate-keyinfo-status' => 'Status: $1',
+	'apigate-keyinfo-status-enabled' => 'Active',
+	'apigate-keyinfo-status-disabled' => 'Disabled',
+	'apigate-keyinfo-nickname' => 'Key Nickname',
+	'apigate-keyinfo-hint-nickname' => 'Name of application',
+	'apigate-keyinfo-name' => 'Name',
+	'apigate-keyinfo-email' => 'Email',
+	'apigate-keyinfo-submit' => 'Update',
 
+	// Admin-links module
 	'apigate-adminlinks-header' => 'Administrator Links',
 	'apigate-adminlinks-viewkeys' => 'View table of each individual key',
 	'apigate-adminlinks-viewaggregate' => 'View aggregate stats',
-	
+
+	// Module which lists all keys in the system (visible to Admins)
 	'apigate-listkeys-th-keyname' => 'Key Nickname',
 	'apigate-listkeys-th-apikey' => 'API Key',
 	'apigate-listkeys-th-username' => 'Username',
--- lib/ApiGate/templates/key.php
+++ lib/ApiGate/templates/key.php
@@ -1,20 +1,14 @@
 <div class='sub_module'>
-	TODO: Key info form for <?= $apiKeyObject->getApiKey() ?>
 	<?php
 		// TODO: Create the i18n strings
-		
-		
+		// TODO: Create the i18n strings
+
 		// TODO: Highlight the nickname & apiKey if disabled
 		$keyClass = "";
 		if( !$apiKeyObject->isEnabled() ){
-			$keyClass = "class='disabled'";
+			$keyClass = " class='disabled'";
 		}
 		// TODO: Highlight the nickname & apiKey if disabled
-		
-		// TODO: If disabled, show the 'reason' value (make ApiGate_ApiKey lazy-load that so it just shows up through an accessor).
-		// TODO: If disabled, show the 'reason' value (make ApiGate_ApiKey lazy-load that so it just shows up through an accessor).
-		
-		
 	?>
 	<form method="post" action="">
 		<div>
@@ -25,16 +19,31 @@
 				}
 			?>
 			<?= i18n( 'apigate-keyinfo-nickname' ); ?><br/>
-			<input type='text' name='nickName' value='<?= $apiKeyObject->getNickName() ?>'/>
-			<?= i18n( 'apigate-keyinfo-apiKey' ); ?>
-			<strong><?= i18n( 'apigate-keyinfo-apiKey' ); ?></strong><br/>
+			<input type='text' name='nickName' placeholder='<?= i18n('apigate-keyinfo-hint-nickname') ?>' value='<?= $apiKeyObject->getNickName() ?>' style='font-size:1.5em'/><br/>
+			<br/>
+			<span<?= $keyClass ?>><?= i18n( 'apigate-keyinfo-apiKey', $apiKeyObject->getApiKey() ); ?></span><br/>
+			<?php
+				if($apiKeyObject->isEnabled()){
+					$statusClass = "enabled";
+					$statusMsg = i18n('apigate-keyinfo-status-enabled');
+				} else {
+					$statusClass = "disabled";
+					$statusMsg = i18n('apigate-keyinfo-status-disabled');
+				}
+				$statusHtml = "<span class='status $statusClass'>$statusMsg</span>";
+				print i18n( 'apigate-keyinfo-status', $statusHtml );
+				
+				// TODO: If disabled, show the 'reason' value (make ApiGate_ApiKey lazy-load that so it just shows up through an accessor).
+				// TODO: If disabled, show the 'reason' value (make ApiGate_ApiKey lazy-load that so it just shows up through an accessor).
+			?><br/>
+			<br/>
 			<?= i18n( 'apigate-keyinfo-name' ); ?><br/>
-			<input type='text' name='firstName' value='<?= $firstName ?>' style='width:192px'/>
-			&nbsp;<input type='text' name='lastName' value='<?= $lastName ?>' style='width:192px'/><br/>
+			<input type='text' name='firstName' value='<?= $apiKeyObject->getFirstName() ?>' style='width:192px'/>
+			&nbsp;<input type='text' name='lastName' value='<?= $apiKeyObject->getLastName() ?>' style='width:192px'/><br/>
 			<br/>
 			<?= i18n( 'apigate-keyinfo-email' ); ?><br/>
-			<input type='text' name='email_1' value='<?= $email_1 ?>' style='width:400px'/><br/>
-			<input type='text' name='email_2' value='<?= $email_2 ?>' style='width:400px'/><br/>
+			<input type='text' name='email_1' value='<?= $apiKeyObject->getEmail() ?>' style='width:400px'/><br/>
+			<input type='text' name='email_2' value='<?= $apiKeyObject->getEmail() ?>' style='width:400px'/><br/>
 			<br/>
 			<input type='submit' value='<?= i18n( 'apigate-keyinfo-submit' ) ?>'/><br/>
 		</div>
