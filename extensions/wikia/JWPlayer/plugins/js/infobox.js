c7021516f65b7398c32144cd328494b3f8f9213e
will <will@wikia-inc.com>
2011-11-09 00:27:22 +0000

    Bug ID 15082: assorted JW Player skin changes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44696 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/JWPlayer/plugins/js/infobox.js
+++ extensions/wikia/JWPlayer/plugins/js/infobox.js
@@ -4,27 +4,32 @@
 	});
 
 	var template = function(player, config, div) {
-		function setup(evt) {			
-			var container = $(div);
-			container.addClass('jwplayer-infobox');
-			var background = $('<div class="jwplayer-infobox-background"></div>');
-			var content = $('<div class="jwplayer-infobox-content"></div>');
-			container.append(background).append(content);
-			content.html(config.title);
+		function setup(evt) {		
+			if (player.config.width >= 330) {
+				var container = $(div);
+				container.addClass('jwplayer-infobox');
+				var background = $('<div class="jwplayer-infobox-background"></div>');
+				var contentClasses = '';
+				if (player.config.width < 660) {
+					contentClasses += ' small';
+				}
+				var content = $('<div class="jwplayer-infobox-content'+contentClasses+'"></div>');
+				container.append(background).append(content);
+				content.html(config.title);
 
-			// bind player play control to clicks on div
-			container.click(function() {
-				player.play();
-			});
-			
-			// bind div display to controlbar display
-			player.getPlugin('controlbar').onShow(function(args) {
-				container.fadeIn(200);
-			});
-			player.getPlugin('controlbar').onHide(function(args) {
-				container.fadeOut(200);
-			});
+				// bind player play control to clicks on div
+				container.click(function() {
+					player.play();
+				});
 
+				// bind div display to controlbar display
+				player.getPlugin('controlbar').onShow(function(args) {
+					container.fadeIn(200);
+				});
+				player.getPlugin('controlbar').onHide(function(args) {
+					container.fadeOut(200);
+				});
+			}
 		};
 		player.onReady(setup);
 		this.resize = function(width, height) {};
