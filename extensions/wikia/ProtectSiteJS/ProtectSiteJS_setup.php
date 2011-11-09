1c50f68e8944cc7ae1039a44e61aaf07ae3464a3
inez <inez@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-11-09 01:02:56 +0000

    * Extension to prevent edits to articles named *.js by people not in staff group
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@44708 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ProtectSiteJS/ProtectSiteJS_setup.php
@@ -0,0 +1,19 @@
+<?php
+// @author: Inez
+
+$wgHooks['AlternateEdit'][] = 'ProtectSiteJS_handler';
+$wgHooks['ArticleSave'][] = 'ProtectSiteJS_handler';
+$wgHooks['EditPage::attemptSave'][] = 'ProtectSiteJS_handler';
+function ProtectSiteJS_handler() {
+	global $wgTitle, $wgUser, $wgOut;
+	if ( strtoupper( substr( $wgTitle->getText(), -3 ) ) === '.JS' ) {
+		$groups = $wgUser->getEffectiveGroups();
+		if( !in_array( 'staff', $groups ) ) {
+			$wgOut->addHTML( '<div class="errorbox" style="width:92%;">' );
+			$wgOut->addWikiMsg( 'actionthrottledtext' );
+			$wgOut->addHTML( '</div>' );
+			return false;
+		}
+	}
+	return true;
+}
