fafe09fa59f168bcdb2c546dcc7e4ff5ff3fb325
garth <garth@wikia-inc.com>
2011-11-16 23:23:19 +0000

    Remove some unused code, normalize the URL to include the event type at the beginning
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45029 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/jquery/jquery.wikia.tracker.js
+++ skins/common/jquery/jquery.wikia.tracker.js
@@ -14,7 +14,6 @@ jQuery.internalTrack = function(event, data) {
 	
 	// Set up params object - this should stay in sync with /extensions/wikia/Track/Track.php
 	var params = {
-		'type': event,
 		'c': wgCityId,
 		'x': wgDB,
 		'a': wgArticleId,
@@ -28,7 +27,7 @@ jQuery.internalTrack = function(event, data) {
 	$.extend(params, data);
 
 	// Make request	
-	$.get('http://a.wikia-beacon.com/__track/special', params);
+	$.get('http://a.wikia-beacon.com/__track/special/' + event, params);
 }
 
 
