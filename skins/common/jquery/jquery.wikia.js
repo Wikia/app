41bcee6dcf3bca145150aab2b3e75a49ee9c7a89
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-11-21 13:06:23 +0000

    jquery.wikia.js: add support for Array.indexOf in IE8
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45173 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/jquery/jquery.wikia.js
+++ skins/common/jquery/jquery.wikia.js
@@ -1192,7 +1192,7 @@ jQuery.nirvana.postJson = function(controller, method, data, callback, onErrorCa
 
 jQuery.nirvana.ajaxJson = function(controller, method, data, callback, onErrorCallback, requestType) {
 
-		// data parameter can be omitted
+	// data parameter can be omitted
 	if ( typeof data == 'function' ) {
 		callback = data;
 		data = {};
@@ -1229,6 +1229,20 @@ jQuery.openPopup = function(url, name, moduleName, width, height) {
 				);
 			}
 		}, false, message); // show the 'login required for this action' message.
+	}
+}
+
+// add Array.indexOf function in IE8
+// @see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf
+if (typeof [].indexOf == 'undefined') {
+	Array.prototype.indexOf = function(val, fromIndex) {
+		fromIndex = fromIndex || 0;
+		for (var i = fromIndex, m = this.length; i < m; i++) {
+			if (this[i] === val) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
 
