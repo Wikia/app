c6006a8ca1eab62c43ecb4e7111d2d94f7ec6f46
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-11-21 16:26:00 +0000

    WikiaMobile: output cleanup (BugzId:15927,15465)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45196 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaMobile/WikiaMobileHooks.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * WikiaMobile Hooks handlers
+ *
+ * @author Federico "Lox" Lucignano <federico(at)wikia-inc.com>
+ */
+class WikiaMobileHooks extends WikiaObject{
+	public function onOutputPageParserOutput( &$out, $parserOutput ){
+		//cleanup page output from unwanted stuff
+		if ( get_class( $this->wg->User->getSkin() ) == 'SkinWikiamobile' ) {
+			$text = $parserOutput->getText();
+			
+			//remove inline styling
+			$text = reg_replace('/style=(\'|")[^"\']*(\'|")/im', '', $text);
+			
+			//remove image sizes
+			//$text = reg_replace('/(width|height)=(\'|")[^"\']*(\'|")/im', '', $text);
+			
+			$parserOutput->setText( $text );
+		}
+		
+		return true;
+	}
+}
