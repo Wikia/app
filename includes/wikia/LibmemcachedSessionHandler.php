7cec88e258a0be8539f1b7193c14ef26e56484ff
wladek <wladek@wikia-inc.com>
2011-11-22 14:02:55 +0000

    work in progress on libmemcached client (BugId: 14979)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45249 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/LibmemcachedSessionHandler.php
+++ includes/wikia/LibmemcachedSessionHandler.php
@@ -38,34 +38,17 @@ class LibmemcachedSessionHandler {
 	}
 
 	static public function read( $id ) {
-		$app = F::app();
-		try {
-			wfDebugLog( "session", __METHOD__ . ": reading $id from storage\n", true );
-			$data = $app->wg->redis->get( self::key( $id ) );
-		} catch (Exception $e) {
-			self::error($e);
-		}
+		$data = self::getMemcached()->get(self::key($id));
+		if (!is_string($data)) $data = ''; // according to docs string must be returned
 		return $data;
 	}
 
 	static public function write( $id, $data ) {
-		$app = F::app();
-		try {
-			wfDebugLog( "session", __METHOD__ . ": stored with key {$id}\n", true );
-			$app->wg->redis->setex( self::key( $id ), self::EXPIRE, $data );
-		} catch (Exception $e) {
-			self::error($e);
-		}
+		return self::getMemcached()->set(self::key($id),$data,self::EXPIRE);
 	}
 
 	static public function destroy( $id ) {
-		$app = F::app();
-
-		try {
-			$app->wg->redis->del( self::key($id) );
-		} catch (Exception $e) {
-			self::error($e);
-		}
+		return self::getMemcached()->delete(self::key($id));
 	}
 
 	static public function gc( $maxlifetime ) {
@@ -79,40 +62,14 @@ class LibmemcachedSessionHandler {
 			
 			$memcached = new LibmemcachedBagOStuff(array(
 				'servers' => $wgSessionMemCachedServers,
+				'persistent' => $wgMemCachedPersistent,
 			));
-			/*
-			$memcached = empty( $wgMemCachedPersistent )
-				? new Memcached()
-				: new Memcached( 'sessions-'.md5(serialize($wgSessionMemCachedServers)) );
-			$memcached->setOption(Memcached::OPT_LIBKETAMA_COMPATIBLE,true);
-			// the line above sets the next 2 options automatically
-			//$memcached->setOption(Memcached::OPT_HASH,Memcached::HASH_MD5);
-			//$memcached->setOption(Memcached::OPT_DISTRIBUTION,Memcached::DISTRIBUTION_CONSISTENT);
-//			$memcached->setOption(Memcached::OPT_SERIALIZER,Memcached::SERIALIZER_IGBINARY);
-			$memcached->setOption(Memcached::OPT_COMPRESSION,true);
-//			$memcached->setOption(Memcached::OPT_BINARY_PROTOCOL,$this->binary);
-			$memcached->setOption(Memcached::OPT_CONNECT_TIMEOUT,10);
-			$memcached->setOption(Memcached::OPT_SERVER_FAILURE_LIMIT,2);
-			$memcached->setOption(Memcached::OPT_SEND_TIMEOUT,intval($wgMemCachedTimeout/1000));
-			$memcached->setOption(Memcached::OPT_RECV_TIMEOUT,intval($wgMemCachedTimeout/1000));
-			$memcached->setOption(Memcached::OPT_POLL_TIMEOUT,intval($wgMemCachedTimeout/1000));
-			*/
 		}
 		return $memcached;
 	}
-	static public function error( $exception = null ) {
-		$message = ($exception instanceof Exception) ? $exception->getMessage() : "Unknown error";
-		$stacktrace = ($exception instanceof Exception) ? $exception->getTraceAsString() : "";
-
-		// XXX: put log to better place
-		trigger_error("Could not connect to Redis: {$message}");
-		// just die with 500
-		header("HTTP/1.0 500 Internal Server Error");
-		die("Could not reach Redis ({$message})");
-	}
 }
 
-if( $wgSessionsInRedis ) {
+if( $wgSessionsInLibmemcached ) {
 	register_shutdown_function( 'session_write_close' );
 	session_set_save_handler(
 		array( "LibmemcachedSessionHandler", "open" ),
