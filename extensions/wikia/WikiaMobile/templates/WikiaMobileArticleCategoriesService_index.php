30a2fd22b73ebd5f3d2d6aca55f0e8008876b420
jolek <jolek@wikia-inc.com>
2011-11-25 16:12:09 +0000

    MobileSkin: lots of improvements for navigation, categories, read more, modal
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45420 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/templates/WikiaMobileArticleCategoriesService_index.php
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileArticleCategoriesService_index.php
@@ -1,6 +1,6 @@
 <? if ( !empty( $categoryLinks ) ) :?>
 <nav id="WikiaArticleCategories">
-	<h1><?= $wf->Msg( 'wikiamobile-article-categories' ); ?></h1>
+	<h1><?= $wf->Msg( 'wikiamobile-article-categories' ); ?><span class="chevron"></span></h1>
 	<?= $categoryLinks ?>
 </nav>
 <? endif ;?>
