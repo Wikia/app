30238ff76b2f4a05fcf9e3a0de7a66806e3b91f0
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-11-29 15:31:21 +0000

    RadiumOne: remove jQuery dependency (BugzId:16608)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45504 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RadiumOne/raon.js
+++ extensions/wikia/RadiumOne/raon.js
@@ -1,29 +1,78 @@
-var RadiumOne = {
-	containerId: 'RadiumOne',
-	cookieName: 'raon',
-	enabledComscoreCats: ['Lifestyle', 'Entertainment'],
-	pixelUrl: 'http://rs.gwallet.com/r1/pixel/x2073r',
+var RadiumOne = {};
+
+(function(ns){
+	var indexOf = Array.prototype.indexOf;
 	
-	init: function() {
+	function inArray(elem, array){
+		if(indexOf)
+			return indexOf.call(array, elem);
+
+		for(var i = 0, length = array.length; i < length; i++){
+			if(array[i] === elem)
+				return i;
+		}
+
+		return -1;
+	}
+	
+	function fetchCookieVal(offset) {
+		var endstr = document.cookie.indexOf(";", offset);
+
+		if(endstr == -1)
+			endstr = document.cookie.length;
+
+		return unescape(document.cookie.substring(offset, endstr));
+	}
+	
+	function getCookie(name){
+		var arg = name + "=",
+		alen = arg.length,
+		clen = document.cookie.length,
+		i = 0;
+
+		while(i < clen){
+			var j = i + alen;
+			if(document.cookie.substring(i, j) == arg)
+				return fetchCookieVal(j);
+
+			i = document.cookie.indexOf(" ", i) + 1;
+
+			if(i == 0)
+				break; 
+		}
+
+		return null;
+	}
+	
+	ns.containerId = 'RadiumOne';
+	ns.cookieName = 'raon';
+	ns.enabledComscoreCats = ['Lifestyle', 'Entertainment'];
+	ns.pixelUrl = 'http://rs.gwallet.com/r1/pixel/x2073r';
+	
+	ns.init = function(){
 		if (window.wgIntegrateRadiumOne
-		|| $.inArray(window.cscoreCat, RadiumOne.enabledComscoreCats) != -1) {
+		|| inArray(window.cscoreCat, RadiumOne.enabledComscoreCats) != -1) {
 			// if no cookie, get pixel (which sets cookie)
-			if ($.cookies.get( RadiumOne.cookieName ) === null) {
+			if (getCookie( RadiumOne.cookieName ) === null) {
 				new Image().src = RadiumOne.pixelUrl+Math.round(Math.random()*10000000);
 			}			
 		}
-	},
+	};
 	
-	getDARTKeyValue: function () {
-		var kv = '';
-		if (window.wgIntegrateRadiumOne
-		|| $.inArray(window.cscoreCat, RadiumOne.enabledComscoreCats) != -1) {
-			var value = $.cookies.get(RadiumOne.cookieName) || 0;
+	ns.getDARTKeyValue = function(){
+		var kv = '',
+		value;
+
+		if(
+			window.wgIntegrateRadiumOne ||
+			inArray(window.cscoreCat, RadiumOne.enabledComscoreCats) != -1
+		) {
+			value = getCookie(RadiumOne.cookieName) || 0;
 			kv = 'ro='+value+';';
 		}
+
 		return kv;
-	}
-	
-};
+	};
+})(RadiumOne);
 
 RadiumOne.init();
