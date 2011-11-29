7c93bbe5c863af6b856ccd281fcfc5e6b8387b3d
pbablok <pbablok@wikia-inc.com>
2011-11-29 20:09:00 +0000

    unify article comments 4
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45544 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WallArticleComment/modules/templates/ArticleComments_RCHeaderBlock.php
+++
@@ -1,8 +0,0 @@
-<table cellpadding="0" cellspacing="0" border="0" style="background: none">
-<tr>
-	<td valign="top" style="white-space: nowrap">
-		<tt><span id='mw-rc-openarrow-<?=$inx?>' class='mw-changeslist-expanded changes-list-entry' style='visibility:hidden'><a href='#' onclick='toggleVisibility(<?=$inx?>); return false' title='<?=htmlspecialchars( wfMsg( 'rc-enhanced-expand' ) )?>' class='mw-arr-r'><img src="/skins/common/images/Arr_r.png" width="12" height="12" alt="+" title="<?=htmlspecialchars( wfMsg( 'rc-enhanced-expand' ) )?>" /></a></span><span id='mw-rc-closearrow-<?=$inx?>' class='mw-changeslist-hidden changes-list-entry' style='display:none'><a href='#' onclick='toggleVisibility(<?=$inx?>); return false' title='<?=htmlspecialchars( wfMsg( 'rc-enhanced-hide' ) )?>' class='mw-arr-d'><img src="/skins/common/images/Arr_d.png" width="12" height="12" alt="-" title="<?=htmlspecialchars( wfMsg( 'rc-enhanced-hide' ) )?>" /></a></span>&nbsp;<span class="newpage">&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;</tt>
-	</td>
-	<td style="padding-left:2px"><?=$hdrtitle?>&nbsp;(<?=$cntChanges?>) . . [<?= implode("; ", $users) ?>]</td>
-</tr>
-</table>
