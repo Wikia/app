e90f43da6c49dbf97e934955f449278e2d209fdf
jolek <jolek@wikia-inc.com>
2011-11-30 12:27:47 +0000

    WikiaMobile: a bit better styling for slideshow
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45599 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaPhotoGallery/templates/renderMobileSlideshow.tmpl.php
+++ extensions/wikia/WikiaPhotoGallery/templates/renderMobileSlideshow.tmpl.php
@@ -2,15 +2,14 @@
 		<?php
 		echo "data-image-count=".count($images);
 		foreach ( $images as $key => $val) {
-			echo " data-slideshow-image-id-".$key."=".$val;
+			echo " data-slideshow-image-id-{$key}={$val}";
 		}
 		?>
 	>
-
-	<img class=placeholderImg src="<?=$images[0]?>">
+	<img class=placeholderImg src=<?=$images[0]?>>
 	<footer>
 		<div>
-			<img src="<?= $magnifyClipSrc ?>">
+			<img src=<?= $magnifyClipSrc ?>>
 			<span><?= $counterValue ?></span>
 		</div>
 	</footer>
