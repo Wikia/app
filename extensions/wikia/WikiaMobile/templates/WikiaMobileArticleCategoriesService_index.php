96fc22ba4fa4b82c0c605d2ac8e2896a4d6526e0
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-05 12:12:25 +0000

    WikiaMobile:  better solution to handle collapsible sections
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45768 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/templates/WikiaMobileArticleCategoriesService_index.php
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileArticleCategoriesService_index.php
@@ -1,6 +1,6 @@
 <? if ( !empty( $categoryLinks ) ) :?>
 <nav id="WikiaArticleCategories">
-	<h1><?= $wf->Msg( 'wikiamobile-article-categories' ); ?><span class="chevron"></span></h1>
+	<h1 class="collapsible-section"><?= $wf->Msg( 'wikiamobile-article-categories' ); ?><span class="chevron"></span></h1>
 	<?= $categoryLinks ?>
 </nav>
 <? endif ;?>
