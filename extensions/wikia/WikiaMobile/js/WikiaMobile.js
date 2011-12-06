240919b2b0ad39fe69fd3a8694d1b9e2aaedbfa5
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-06 14:55:53 +0000

    WikiaMobile: JS re-organization
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45826 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaMobile/js/WikiaMobile.js
@@ -0,0 +1,164 @@
+var WikiaMobile = WikiaMobile || (function() {
+	/** @private **/
+	
+	var allImages = [],
+	deviceWidth = ($.os.ios) ? 268 : 320,
+	deviceHeight = ($.os.ios) ? 416 : 533,
+	realWidth = (window.orientation == 0 || window.orientation == 180) ? 320 : 480,
+	//TODO: finalize the following line and update all references to it (also in extensions)
+	clickEvent = ('ontouchstart' in window) ? 'tap' : 'click';
+
+	function getImages(){
+		return allImages;
+	}
+
+	//slide up the addressbar on webkit mobile browsers for maximum reading area
+	//setTimeout is necessary to make it work on ios...
+	function hideURLBar(){
+		setTimeout(function(){
+		  	if(!pageYOffset)
+				window.scrollTo(0, 1);
+		}, 1);
+	}
+
+	function handleTables(){
+		$('table').not('table table').each(function() {
+			if(this.offsetWidth > realWidth)
+				$(this).addClass('tooBigTable');
+		});
+	}
+
+	function processImages() {
+		var number = 0,
+		image;
+		
+		$('.infobox .image').each(function(){
+			allImages.push($(this).data('number', number++).attr('href'));
+		});
+
+		$('figure').each(function(){
+			var self = $(this);
+			allImages.push([
+				self.find('.image').data('number', number++).attr('href'),
+				self.find('.thumbcaption').html()
+			]);
+		});
+
+		$('.wikia-slideshow').each(function(){
+			var slideshow = $(this),
+			length = slideshow.data('number', number++).data('image-count');
+
+			for(var i = 0; i < length; i++) {
+				allImages.push(slideshow.data('slideshow-image-id-' + i));
+			}
+		});
+
+		if(allImages.length <= 1) $('body').addClass('justOneImage');
+	}
+
+	function getDeviceResolution(){
+		return [deviceWidth, deviceHeight];
+	}
+
+	function imgModal(number, href, caption){
+		$.openModal({
+			html: '<div class="changeImageButton" id="previousImage"><div class="changeImageChevron"></div></div><div class="fullScreenImage" data-number='+
+				number +
+				' style=background-image:url("'+
+				href +
+				'")></div><div class="changeImageButton" id="nextImage"><div class="changeImageChevron"></div></div>',
+			toHide: '.changeImageButton',
+			caption: caption
+		});
+	}
+
+	function getClickEvent(){
+		return clickEvent;
+	}
+
+	//init
+	$(function(){
+		var body = $(document.body),
+		navigationWordMark = $('#navigationWordMark'),
+		navigationSearch = $('#navigationSearch'),
+		searchToggle = $('#searchToggle'),
+		searchInput = $('#searchInput'),
+		wikiaAdPlace = $('#WikiaAdPlace');
+
+		hideURLBar();
+		handleTables();
+		processImages();
+
+		//TODO: optimize selectors caching for this file
+		body.delegate( '#WikiaMainContent > h2, #WikiaPage .collapsible-section', clickEvent, function() {
+			$(this).toggleClass('open').next().toggleClass('open');
+		});
+
+		$('.infobox img').bind(clickEvent, function(event) {
+			event.preventDefault();
+			
+			var thumb = $(this),
+				image = thumb.parents('.image');
+			imgModal(image.data('number'), image.attr('href'));
+		});
+
+		$('figure').bind(clickEvent, function(event) {
+			event.preventDefault();
+			
+			var thumb = $(this),
+				image = thumb.children('.image').first();
+			imgModal(image.data('number'), image.attr('href'), thumb.children('.thumbcaption').html());
+		});
+
+		$('.wikia-slideshow').bind(clickEvent, function(event){
+			event.preventDefault();
+			
+			var slideshow = $(this);
+			imgModal(slideshow.data('number'), slideshow.find('img').attr('src'), "Slideshow image #1");
+		});
+
+		$('#searchToggle').bind(clickEvent, function(event){
+			var self = $(this);
+			
+			if(self.hasClass('open')){
+				navigationWordMark.show();
+				navigationSearch.hide().removeClass('open');
+				self.removeClass('open');
+				searchInput.val('');
+			}else{
+				navigationWordMark.hide();
+				navigationSearch.show().addClass('open');
+				self.addClass('open');
+			}
+		});
+
+		$('#WikiaPage').bind(clickEvent, function(event) {
+			navigationWordMark.show();
+			navigationSearch.hide().removeClass('open');
+			searchToggle.removeClass('open');
+			searchInput.val('');
+		});
+
+		$('.tooBigTable').bind(clickEvent, function(event) {
+			event.preventDefault();
+			
+			$.openModal({
+				addClass: 'wideTable',
+				html: this.outerHTML
+			});
+		});
+
+		$('#fullSiteSwitch').bind('click', function(event){
+			event.preventDefault();
+			
+			Wikia.CookieCutter.set('mobilefullsite', 'true');
+			location.reload();
+		});
+	});
+
+	return {
+		getImages: getImages,
+		getDeviceResolution: getDeviceResolution,
+		getClickEvent: getClickEvent
+	}
+})();
