370f710d31ebe0402c893751d8320bcf7b967f77
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-06 16:10:11 +0000

    WikiaMobile: rework footer markup (BugzId:16500)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45838 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/templates/WikiaMobileArticleCategoriesService_index.php
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileArticleCategoriesService_index.php
@@ -1,6 +1,6 @@
 <? if ( !empty( $categoryLinks ) ) :?>
-<nav id="WikiaArticleCategories">
+<section id="WikiaArticleCategories">
 	<h1 class="collapsible-section"><?= $wf->Msg( 'wikiamobile-article-categories' ); ?><span class="chevron"></span></h1>
 	<?= $categoryLinks ?>
-</nav>
+</section>
 <? endif ;?>
