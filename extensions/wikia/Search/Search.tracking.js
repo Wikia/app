2b7d682b1379c0a231eda7c7fa2b94d6564510a1
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-08 13:03:48 +0000

    search click tracking fix for monobook skin ( BugId: #17162)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45957 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Search/Search.tracking.js
+++ extensions/wikia/Search/Search.tracking.js
@@ -1,20 +1,27 @@
 //result links click tracking
-$(function() {
-	$('.mw-search-result-title').bind('click', function(event) {
-		event.preventDefault();
-
-		var callback = function() {
-			$().log('Result click tracked, redirecting ..');
-			window.location.href = element.attr('href');
-		}
-
-		var element = $(this);
-		$.tracker.byStr('search/result/click/pos/'+element.attr('data-pos'));
-		$.internalTrack('search_click', {
-			'pos': element.attr('data-pos'),
-			'sterm': element.attr('data-sterm'),
-			'stype': element.attr('data-stype'),
-			'rver': element.attr('data-rver')
-		}, callback, callback );
-	});
-});
+var SearchClickTracking = {
+	init: function() {
+		$(function() {
+			$('.mw-search-result-title').bind('click', function(event) {
+				if( typeof $.tracker == 'undefined' ) {
+					return true;
+				}
+				else {
+					event.preventDefault();
+					var element = $(this);
+					var callback = function() {
+						$().log('Result click tracked, redirecting ..');
+						window.location.href = element.attr('href');
+					}
+					$.tracker.byStr('search/result/click/pos/'+element.attr('data-pos'));
+					$.internalTrack('search_click', {
+						'pos': element.attr('data-pos'),
+						'sterm': element.attr('data-sterm'),
+						'stype': element.attr('data-stype'),
+						'rver': element.attr('data-rver')
+					}, callback, callback );
+				}
+			});
+		});
+	}
+};
