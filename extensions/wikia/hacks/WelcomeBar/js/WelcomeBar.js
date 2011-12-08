f9028a97f069f710df0f51f4a48850705780babc
macbre <macbre@wikia-inc.com>
2011-12-08 14:47:19 +0000

    WelcomeBar: first working version
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45963 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/hacks/WelcomeBar/js/WelcomeBar.js
@@ -0,0 +1,44 @@
+var WelcomeBar = {
+	STORAGE_TIMESTAMP: 'welcome-bar-timestamp',
+	SHOW_DELAY: 7 * 24 * 3600,
+
+	bar: false,
+
+	show: function() {
+		var content = $.msg('welcome-bar-message', '<a href="' + wgServer + '">' + wgSiteName + '</a>') +
+			'<img class="close" src="' + stylepath + '/oasis/images/icon_close.png" width="15" height="15">';
+
+		this.bar = $('<div>', {id: 'WelcomeBar'}).
+			html(content).
+			appendTo('#WikiaHeader').
+			slideDown(1000);
+
+		this.bar.children('.close').bind('click', $.proxy(this.close, this));
+	},
+
+	close: function() {
+		this.bar.slideUp(1000);
+
+		// wait at least SHOW_DELAY before showing the toolbar the next time
+		$.storage.set(this.STORAGE_TIMESTAMP, this.now());
+	},
+
+	now: function() {
+		return Math.round(new Date().getTime() / 1000);
+	},
+
+	checkShow: function() {
+		var lastTimeClosed = $.storage.get(this.STORAGE_TIMESTAMP);
+		return (lastTimeClosed === null) || (this.now() - lastTimeClosed > this.SHOW_DELAY);
+	},
+
+	init: function() {
+		if (this.checkShow()) {
+			$.getResources([
+					function(cb) {$.getMessagesForContent('WelcomeBar', cb)},
+					function(cb) {$.getCSS($.getSassCommonURL('extensions/wikia/hacks/WelcomeBar/css/WelcomeBar.scss'), cb)}
+				],
+				$.proxy(this.show, this));
+		}
+	}
+}
