853eae1e7f744954ab1f5250f5f22b6c2b17ac88
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-08 16:32:21 +0000

    Places: WikiaMobile adaptation
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@45978 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Places/templates/Places_placeFromModel.php
+++ extensions/wikia/Places/templates/Places_placeFromModel.php
@@ -6,7 +6,7 @@
 	} else {
 ?>
 <figure class="thumb t<?=$align;?> thumbinner placemap" style="width:<?=$width+2;?>px;">
-	<a><img src="<?=$url;?>" width="<?=$width;?>" height="<?=$height;?>" data-zoom="<?=$zoom;?>" data-lat="<?=$lat;?>" data-lon="<?=$lon;?>" /></a>
+	<a><img class="thumbimage" src="<?=$url;?>" width="<?=$width;?>" height="<?=$height;?>" data-zoom="<?=$zoom;?>" data-lat="<?=$lat;?>" data-lon="<?=$lon;?>" /></a>
 	<figcaption class="thumbcaption">
 		<span class="geo"><?=$lat;?>; <?=$lon;?></span>
 	</figcaption>
