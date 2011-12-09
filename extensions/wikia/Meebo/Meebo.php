252cf5856e1cd3167c40cd0c1e9866f7b7f89f7d
will <will@wikia-inc.com>
2011-12-09 17:30:46 +0000

    Bug ID 17129: enable meebo toolbar on all english language wikis
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46031 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Meebo/Meebo.php
+++ extensions/wikia/Meebo/Meebo.php
@@ -10,26 +10,19 @@ $wgHooks['MakeGlobalVariablesScript'][] = 'Meebo::onMakeGlobalVariablesScript';
 class Meebo {
 	private static $PROHIBITED_DBNAMES = array('fallout', 'answers');
 	public static function onMakeGlobalVariablesScript( &$vars ) {
-		global $wgEnableMeeboExt, $wgContLang, $wgDBname;
+		global $wgEnableMeeboExt, $wgDBname;
 		wfProfileIn( __METHOD__ );
 
 		global $wgRequest, $wgNoExternals, $wgUser;
 		$wgNoExternals = $wgRequest->getBool('noexternals', $wgNoExternals);
 
-		if ($wgEnableMeeboExt) {
-			if (!$wgNoExternals) {
-				if ($wgContLang->getCode() == 'en') {
-					if ($wgUser->isAnon()) {
-						if ($wgUser->getSkin()->getSkinName() == 'oasis') {
-							if ($wgRequest->getText( 'action', 'view' ) == 'view') {
-								if (array_search($wgDBname, self::$PROHIBITED_DBNAMES) === FALSE) {
-									$vars['wgEnableMeeboExt'] = true;
-								}
-							}
-						}
-					}
-				}
-			}
+		if ($wgEnableMeeboExt
+		&& !$wgNoExternals
+		&& $wgUser->isAnon()
+		&& $wgUser->getSkin()->getSkinName() == 'oasis'
+		&& $wgRequest->getText( 'action', 'view' ) == 'view'
+		&& array_search($wgDBname, self::$PROHIBITED_DBNAMES) === FALSE) {
+			$vars['wgEnableMeeboExt'] = true;
 		}
 
 		wfProfileOut( __METHOD__ );
