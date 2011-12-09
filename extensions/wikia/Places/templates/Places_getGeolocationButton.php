a86b4c5d3ea15bdc86654a5c2aa9e59fdcfe7e52
jakub <jakub@wikia-inc.com>
2011-12-09 15:43:12 +0000

    places
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46026 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Places/templates/Places_getGeolocationButton.php
+++ extensions/wikia/Places/templates/Places_getGeolocationButton.php
@@ -1,2 +1,6 @@
-<button id="geotagButton" class="secondary"><?= $wf->Msg( 'places-geolocation-button-label' ) ;?></button>
+<figure class="thumb tright thumbinner" style="width:302px;">
+	<div style="padding:5px;">
+		<button id="geotagButton" style="margin:50px 0px"><?= $wf->Msg( 'places-geolocation-button-label' ) ;?></button>
+	</div>
+</figure>
 <?= $jsSnippet ;?>
