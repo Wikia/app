059804f73affa9f6adba705a6a4b0e46b1179736
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-09 14:47:11 +0000

    WikiaMobile: css tweak + JSMessages
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46019 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -200,6 +200,7 @@ $config['wikiamobile_js_body'] = array(
 		'//skins/common/zepto/zepto.wikiamobile.js',
 		'//skins/common/zepto/zepto.modal.wikiamobile.js',
 		'//extensions/wikia/WikiaMobile/js/WikiaMobile.js',
+		'//extensions/wikia/JSMessages/js/JSMessages.wikiamobile.js',
 		'//extensions/wikia/JSSnippets/js/JSSnippets.wikiamobile.js',
 		'//extensions/wikia/WikiaTracker/js/WikiaLogger.js',
 		'//extensions/wikia/WikiaTracker/js/WikiaTracker_config.js',
