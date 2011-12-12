ba0aad745567a8fec0bcd3455637a3e371a85c35
macbre <macbre@wikia-inc.com>
2011-12-12 15:15:51 +0000

    Places: simplify the HTML
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46050 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Places/templates/PlacesSpecial_getMapSnippet.php
+++ extensions/wikia/Places/templates/PlacesSpecial_getMapSnippet.php
@@ -1,6 +1,6 @@
-<table style="width:200px; height:<?=( isset( $imgUrl ) && !empty( $imgUrl ) ) ? '170px' : '60px'; ?>"><tr><td>
-<?php if( isset( $imgUrl ) && !empty( $imgUrl ) ) { ?>
-	<a href='<?= $url; ?>'><img src='<?= $imgUrl; ?>' style='display:block; width:200px; height:100px' /></a>
+<a href="<?= htmlspecialchars($url) ?>">
+<?php if( !empty( $imgUrl ) ) { ?>
+	<img src="<?= htmlspecialchars($imgUrl) ?>" width="200" height="100" style="display: block">
 <?php } ?>
-<a href='<?= $url; ?>' class='more'><?= $title ?></a>
-</td></tr></table>
+<?= $title ?>
+</a>
