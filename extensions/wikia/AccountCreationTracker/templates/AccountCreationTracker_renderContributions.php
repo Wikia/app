4d21c37eed5da44b434528f6d3da3dd367d8684f
tor <tor@wikia-inc.com>
2011-12-14 15:53:22 +0000

    ACT: contributions display
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46121 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AccountCreationTracker/templates/AccountCreationTracker_renderContributions.php
+++ extensions/wikia/AccountCreationTracker/templates/AccountCreationTracker_renderContributions.php
@@ -4,13 +4,8 @@
 		<th>page_id</th>
 		<th>rev_id</th>
 		<th>user_id</th>
-		<th>page_ns</th>
-		<th>is_content</th>
-		<th>is_redirect</th>
-		<th>ip</th>
 		<th>rev_timestamp</th>
 		<th>event_type</th>
-		<th>event_date</th>
 	</thead>
 	<tbody>
 	<? foreach( $contributions as $key=>$row ): ?>
@@ -19,13 +14,8 @@
 		<td><?= $row['page_id'] ?></td>
 		<td><?= $row['rev_id'] ?></td>
 		<td><?= $row['user_id'] ?></td>
-		<td><?= $row['page_ns'] ?></td>
-		<td><?= $row['is_content'] ?></td>
-		<td><?= $row['is_redirect'] ?></td>
-		<td><?= $row['ip'] ?></td>
 		<td><?= $row['rev_timestamp'] ?></td>
 		<td><?= $row['event_type'] ?></td>
-		<td><?= $row['event_date'] ?></td>
 	</tr>
 	<? endforeach; ?>
 	</tbody>
