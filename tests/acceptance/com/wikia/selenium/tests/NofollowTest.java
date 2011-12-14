f31b567d19a062d042dcc89471439d911431346a
wojtek <wojtek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-14 17:11:17 +0000

    selenium tests verification and fixes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46131 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/NofollowTest.java
+++ tests/acceptance/com/wikia/selenium/tests/NofollowTest.java
@@ -15,7 +15,7 @@ import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStor
 import static org.testng.AssertJUnit.assertEquals;
 
 public class NofollowTest extends BaseTest {
-	@Test(groups={"CI"})
+	@Test(groups={"CI", "verified"})
 	public void testNofollow() throws Exception {
 		login();
 		String content = (new Date()).toString();
