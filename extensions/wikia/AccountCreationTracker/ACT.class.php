95d78271134bb6f1add94e957d33b1eca751f968
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-14 21:56:10 +0000

    ACT: new right
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46144 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AccountCreationTracker/ACT.class.php
+++ extensions/wikia/AccountCreationTracker/ACT.class.php
@@ -129,13 +129,17 @@ class AccountCreationTracker extends WikiaObject {
 
 	public function getWikisCreatedByUsers( Array $users ) {
 		$wikis = array();
+		$userIds = array();
+
+		foreach ( $users as $userDetails ) {
+			$userIds[] = $userDetails['user']->getId();
+		}
 
 		$dbr = $this->wf->getDB( DB_SLAVE, array(), $this->wg->ExternalSharedDB );
-
 		$res = $dbr->select( 
 			'city_list',
 			'city_id',
-			array( 'city_founding_user' => $dbr->makeList( $users, LIST_OR ) )
+			array( $dbr->makeList( array( 'city_founding_user' => $userIds), LIST_OR ) )
 		);
 
 		while ( $row = $dbr->fetchObject( $res ) ) {
