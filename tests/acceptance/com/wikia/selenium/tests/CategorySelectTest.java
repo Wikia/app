c1faa565a522976e10ab4d74a870bce6eaaa6301
wojtek <wojtek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-15 16:56:45 +0000

    selenium test fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46170 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/CategorySelectTest.java
+++ tests/acceptance/com/wikia/selenium/tests/CategorySelectTest.java
@@ -2,75 +2,63 @@ package com.wikia.selenium.tests;
 
 import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.session;
 import static org.testng.AssertJUnit.assertEquals;
+import java.awt.event.KeyEvent;
 
 import org.testng.annotations.Test;
 import java.util.Random;
 
 public class CategorySelectTest extends BaseTest {
 
-	@Test(groups={"CI"})
+	@Test(groups={"CI", "verified"})
 	public void testAddCategory() throws Exception {
 		loginAsStaff();
 		Random randomGenerator = new Random();
 		int randomInt = randomGenerator.nextInt(666);
-		// System.out.println("Starting test...");
 		editArticle("WikiaAutomatedTest" + randomInt,
 				"Wikia automated test for CategorySelect\n[[Category:Wikia tests]]");
 
 		// Add new category on view page
-		// System.out.println("Clicking on add-category...");
 		waitForElement("link=Add category");
 		session().click("link=Add category");
-		waitForElement("//*[@id=\"csCategoryInput\"]");
+		waitForElement("csCategoryInput");
 
-		// System.out.println("Typing a category in...");
 		session().type("csCategoryInput", "other category");
-		session().keyPress("csCategoryInput", "\\13");
+		session().getEval("var e = window.jQuery.Event('keypress'); e.keyCode = 13; window.$('#csCategoryInput').trigger(e)");
+		// proper but not working in chrome way of doing the above
+		//session().focus("csCategoryInput");
+		//session().keyPress("csCategoryInput", Integer.toString(KeyEvent.VK_ENTER));
+		session().keyPressNative(Integer.toString(KeyEvent.VK_ENTER));
+		waitForElement("//div[@id='csItemsContainer']/a[1]/img");
 		session().click("//div[@id='csItemsContainer']/a[1]/img");
 		waitForElement("csInfoboxCategory");
 
-		// System.out.println("Typing in the sorting dialog...");
 		session().type("csInfoboxCategory", "Other Category");
 		session().type("csInfoboxSortKey", "Other Category sortkey");
-		// System.out.println("Saving...");
 		session().click("//a[@id='sortDialogSave']");
 		session().click("csSave");
 		// Check present of category added via AJAX
 		waitForElement("link=Other Category");
 
-		// System.out.println("Switching to wikitext version...");
-		session().click(isOasis() ? "//a[@data-id='edit']" : "ca-edit");
-		session().waitForPageToLoad(this.getTimeout());
-		session().click("csSwitchView");
+		clickAndWait(isOasis() ? "//a[@data-id='edit']" : "ca-edit");
+		session().click("//a[contains(@class, 'cke_button_ModeSource')]");
+		waitForElement("csWikitext");
 		waitForElementVisible("csWikitext");
 
-		// System.out.println("Typing in wikitext version...");
-		session()
-				.type("csWikitext",
-						"[[Category:Other Category|Other Category sortkey]]\n[[Category:Wikia tests]]");
-		// System.out.println("Clicking to switch view...");
-		session().click("csSwitchView");
-		// System.out.println("Checking to make sure the text field is gone...");
-		waitForElementNotVisible("csWikitext", 15);
+		session().type("csWikitext", "[[Category:Other Category|Other Category sortkey]]\n[[Category:Wikia tests]]");
+		session().click("//a[contains(@class, 'cke_button_ModeWysiwyg')]");
+		waitForElementNotVisible("csWikitext");
+		waitForElement("csItemsContainerDiv");
+		waitForElementVisible("csItemsContainerDiv");
 
-		// System.out.println("Making sure categorizations are present...");
-		// System.out.println("Making sure the gui-view categorization held...");
-		assertEquals("Other Category", session().getText("//div[@id='csItemsContainer']/a[1]"));
-		// System.out.println("Making sure categorization from wiki-text view is present...");
-		assertEquals("Wikia tests", session().getText("//div[@id='csItemsContainer']/a[2]"));
-		session().click("//div[@id='csItemsContainer']/a[2]/img/");
+		assertEquals("Other Category", session().getText("//div[@id='csItemsContainerDiv']//li[1]/span"));
+		assertEquals("Wikia tests", session().getText("//div[@id='csItemsContainerDiv']//li[2]/span"));
+		session().click("//div[@id='csItemsContainerDiv']//li[2]/img[contains(@class, 'delete')]");
 
-		// System.out.println("Testing that users can still add categories right in the wikitext of the article...");
 		doEdit("Wikia automated test for CategorySelect\n[[Category:Other category]] added via textbox");
-		session().click("wpPreview");
-		session().waitForPageToLoad(this.getTimeout());
-		session().click("//div[@id='csItemsContainer']/a[1]/img/");
-		session().click("wpSave");
-		session().waitForPageToLoad(this.getTimeout());
+		clickAndWait("wpSave");
 		assertEquals("Other Category", session().getText("link=Other Category"));
 
 		// TODO: Test to make sure that deleting one tag by clicking an X deletes exactly that one tag (and not more, if there are more). See rt#45033
-
 		doDelete("label=regexp:.*Author request", "Clean up after test");
 	}
 }
