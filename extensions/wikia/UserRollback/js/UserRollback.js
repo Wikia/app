922854c2249b9358390dd6656defe543c9d69044
wladek <wladek@wikia-inc.com>
2011-12-15 13:58:02 +0000

    imported UserRollback extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46161 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/UserRollback/js/UserRollback.js
@@ -0,0 +1,19 @@
+$(function() {
+	UserRollback.init();
+})
+
+var UserRollback = {
+	
+	init: function() {
+
+		// Bind click to button
+		/*
+		$('#HelloWorldAjax button').click(function() {
+			$.get( wgScriptPath + '/wikia.php?controller=HelloWorld&method=index&format=html', function(data) {
+				$('#HelloWorldAjax').append(data);
+			});
+		});
+		*/
+	}
+	
+};
