2736842d5bb329c5df7442a0fad0433d760d4c3c
nandy <nandy@wikia-inc.com>
2011-12-23 15:39:49 +0000

    merged small stuff week project to trunk; bugId:16208
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46328 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaRSS/js/WikiaRss.js
@@ -0,0 +1,27 @@
+var WikiaRss = WikiaRss || (function(){
+	/** @private **/
+	
+	/** @public **/
+	return {
+		init: function(options) {
+			$.nirvana.sendRequest({
+				controller: 'WikiaRssExternalController',
+				method: 'getRssFeeds',
+				format: 'json',
+				data: {
+					options: options,
+				},
+				callback: function(data) {
+					if( data.status == true ) {
+						$('.wikiaRssPlaceholder[data-id="' + options.id + '"]').html(data.html);
+					} else {
+						$('.wikiaRssPlaceholder[data-id="' + options.id + '"]').html(data.error);
+					}
+				},
+				onErrorCallback: function(jqXHR, textStatus, errorThrown) {
+					$('.wikiaRssPlaceholder[data-id="' + options.id + '"]').html(options.ajaxErrorMsg);
+				}
+			});
+		}
+	};
+})();
