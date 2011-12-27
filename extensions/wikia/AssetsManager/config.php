c8a76946d375073a99dda06b9307d2d2b54ef307
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2011-12-27 19:13:40 +0000

    WikiaMobile: fixes for tables implementation
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46344 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -193,7 +193,6 @@ $config['wikiamobile_js_body'] = array(
 		'//extensions/wikia/WikiaTracker/js/WikiaTracker_config.js',
 		'//extensions/wikia/WikiaTracker/js/WikiaTracker.js',
 		'//extensions/wikia/WikiaTracker/js/WikiaTrackerQueue.js',
-		'//extensions/wikia/WikiaMobile/js/WMWideTables.js',
 		'//extensions/wikia/WikiaMobile/js/WikiaMobile.js',
 		'//extensions/wikia/JSMessages/js/JSMessages.wikiamobile.js',
 		'//extensions/wikia/JSSnippets/js/JSSnippets.wikiamobile.js'
