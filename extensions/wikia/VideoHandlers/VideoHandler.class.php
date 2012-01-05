791aeba624f3365784a1ec4e560bfbd72bfc3c8f
jakub <jakub@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-05 12:59:10 +0000

    Extra comment
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46469 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoHandlers/VideoHandler.class.php
+++ extensions/wikia/VideoHandlers/VideoHandler.class.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Handler layer between specyfic video handler and the rest of BitmapHandlers
+ * Used mainly for identyfication of Video hanlders
+ *
+ * In future common handler logic will be migrated here
+ * If you are using public video handler specyfic function write them down here
+ * 
+ */
+
 class VideoHandler extends BitmapHandler
 {
 	function getEmbed(){
