eadf655b1c70f6d5e118abd4f8b2ae6e70b0f96a
liz <liz@wikia-inc.com>
2012-01-09 21:32:12 +0000

    (BugId:16836) hiding tab buttons until editor is initialized fully
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46554 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/EditPageLayout/js/modules/ModeSwitch.js
+++ extensions/wikia/EditPageLayout/js/modules/ModeSwitch.js
@@ -10,7 +10,21 @@
 		items: [
 			'ModeSource',
 			'ModeWysiwyg'
-		]
+		],
+		
+		init: function() {
+			// (BugId:16836) 
+			var editor = this.editor;
+			// Tabs are in place - hide them. 
+			editor.on('editboxReady', function() {
+				editor.getSpace('tabs').hide();
+			});
+			// Editor is loaded - show tabs. 
+			editor.on('editorReady', function() {
+				editor.getSpace('tabs').show();				
+			})
+
+		}
 
 	});
 
