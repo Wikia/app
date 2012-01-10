b7d806759cf59e15e50eceb03f08488106512748
will <will@wikia-inc.com>
2012-01-10 18:48:05 +0000

    YouTube API wrapper
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46587 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoHandlers/apiwrappers/YoutubeApiWrapper.class.php
+++ extensions/wikia/VideoHandlers/apiwrappers/YoutubeApiWrapper.class.php
@@ -1,116 +1,194 @@
 <?php
 
-class YoutubeApiWrapper extends ApiWrapper{
+class YoutubeApiWrapper extends ApiWrapper {
 
-//	private static $interfaceObject;
+	private $interfaceObj = null;
 
-	private $videoId;
-//	private $videoEntry;
-	
+	protected static $API_URL = 'http://gdata.youtube.com/feeds/api/videos/$1?v=2&alt=json';
+	private static $CACHE_KEY = 'youtubeapi';
+	private static $CACHE_EXPIRY = 86400;
+
 	public function __construct($videoId) {
-		$this->videoId = $videoId;
+		parent::__construct($videoId);
+		$this->getInterfaceObject();
+	}
+	
+	public function getTitle() {
+		return $this->getVideoTitle();
+	}
+	
+	public function getDescription() {
+		//@todo might want to add category and keywords
+		return $this->getVideoDescription();
+	}
+	
+	public function getThumbnailUrl() {
+		$lowresUrl = '';
+		$hiresUrl = '';
+		
+		$thumbnailDatas = $this->getVideoThumbnails();
+		foreach ($thumbnailDatas as $thumbnailData) {
+			switch ($thumbnailData['yt$name']) {
+				case 'default':
+					$lowresUrl = $thumbnailData['url'];
+					break;
+				case 'hqdefault':
+					$hiresUrl = $thumbnailData['url'];
+					break;
+			}
+		}
+		
+		return !empty($hiresUrl) ? $hiresUrl : $lowresUrl;
 	}
 
-//	private static function getInterfaceObject() {
-//		global $wgHTTPProxy;
-//		
-//		if (!isset(self::$interfaceObject)) {
-//			$httpProxySettings = explode(':', $wgHTTPProxy);
-//			$config = array(
-//			    'adapter'    => 'Zend_Http_Client_Adapter_Proxy',
-//			    'proxy_host' => $httpProxySettings[0],
-//			    'proxy_port' => $httpProxySettings[1]
-//			);
-//			$proxiedHttpClient = new Zend_Http_Client(null, $config);
-//			self::$interfaceObject = new Zend_Gdata_YouTube($proxiedHttpClient);
-//		}
-//		
-//		return self::$interfaceObject;
-//	}
-//	
-//	private function getVideoEntry() {
-//		if (!isset($this->videoEntry)) {
-//			$this->videoEntry = self::getInterfaceObject()->getVideoEntry($this->videoId);
-//		}
-//		
-//		return $this->videoEntry;
-//	}
-//	
-//	/**
-//	 *
-//	 * @return type string
-//	 */
-//	public function getVideoTitle() {
-//		return $this->getVideoEntry()->getVideoTitle();
-//	}
-//	
-//	/**
-//	 *
-//	 * @return type string
-//	 */
-//	public function getVideoDescription() {
-//		return $this->getVideoEntry()->getVideoDescription();
-//	}
-//	
-//	/**
-//	 *
-//	 * @return type array
-//	 */
-//	public function getVideoTags() {
-//		return $this->getVideoEntry()->getVideoTags();
-//	}
-//	
-//	/**
-//	 *
-//	 * @return type string
-//	 */
-//	public function getVideoCategory() {
-//		return $this->getVideoEntry()->getVideoCategory();
-//	}
-//	
-//	/**
-//	 *
-//	 * @return type 
-//	 */
-//	public function getVideoUploaded() {
-//		return $this->getVideoEntry()->getMediaGroup()->getUploaded();
-//	}
-//	
-//	/**
-//	 * Video duration, in seconds
-//	 * @return type int
-//	 */
-//	public function getVideoDuration() {
-//		return $this->getVideoEntry()->getVideoDuration();		
-//	}
-//	
-//	public function getDuration() {
-//		return $this->getVideoEntry()->getMediaGroup()->getDuration();
-//	}
-//	
-//	/**
-//	 * returns array of thumbnail data. Thumbnails taken from different 
-//	 * points of video. Elements: time, height, width, url
-//	 * @return type array
-//	 */
-//	public function getVideoThumbnails() {
-//		return $this->getVideoEntry()->getVideoThumbnails();
-//	}
-//	
-//	public function getVideoState() {
-//		return $this->getVideoEntry()->getVideoState();
-//	}
-//	
-//	public function getVideoDeveloperTags() {
-//		return $this->getVideoEntry()->getVideoDeveloperTags();
-//	}
-//	public function isVideoPrivate() {
-//		return $this->getVideoEntry()->isVideoPrivate();
-//	}
-//	public function getVideoRatingInfo() {
-//		return $this->getVideoEntry()->getVideoRatingInfo();
-//	}
-//	public function getDOM() {
-//		return $this->getVideoEntry()->getDOM();
-//	}
+	public function getMetadata() {
+		$metadata = array();
+		
+		$metadata['videoId'] = $this->videoId;
+		$metadata['published'] = $this->getVideoPublished();
+		$metadata['category'] = $this->getVideoCategory();
+		$metadata['canEmbed'] = $this->canEmbed();
+		$metadata['hd'] = $this->isHdAvailable();
+		$metadata['keywords'] = $this->getVideoKeywords();
+		$metadata['duration'] = $this->getVideoDuration();
+		
+		return $metadata;
+	}
+
+	/**
+	 * Connect to YouTube's API and retrieve a data structure containing the response
+	 * @return type 
+	 */
+	protected function getInterfaceObject() {
+		global $wgMemc;
+		
+		if ($this->interfaceObj == null) {
+			$apiUrl = str_replace('$1', $this->videoId, self::$API_URL);
+			$memcKey = wfMemcKey( self::$CACHE_KEY, $apiUrl ); 
+			$response = $wgMemc->get( $memcKey );
+			if (empty($response)) {
+				$response = @Http::get($apiUrl);
+				$wgMemc->set( $memcKey, $response, self::$CACHE_EXPIRY );				
+			}
+			$this->interfaceObj = json_decode($response, true);
+		}
+		
+		return $this->interfaceObj;
+	}
+
+	/**
+	 * Title
+	 * @return string
+	 */
+	protected function getVideoTitle() {
+		if (!empty($this->interfaceObj['entry']['title']['$t'])) {
+			
+			return $this->interfaceObj['entry']['title']['$t'];
+		}
+		
+		return '';
+	}
+	
+	/**
+	 * User-defined description
+	 * @return string
+	 */
+	protected function getVideoDescription() {
+		if (!empty($this->interfaceObj['entry']['media$group']['media$description']['$t'])) {
+			
+			return $this->interfaceObj['entry']['media$group']['media$description']['$t'];
+		}
+		
+		return '';
+	}
+	
+	/**
+	 * User-defined keywords
+	 * @return array
+	 */
+	protected function getVideoKeywords() {
+		if (!empty($this->interfaceObj['entry']['media$group']['media$keywords']['$t'])) {
+			
+			return $this->interfaceObj['entry']['media$group']['media$keywords']['$t'];
+		}
+		
+		return '';
+	}
+	
+	/**
+	 * YouTube category
+	 * @return string
+	 */
+	protected function getVideoCategory() {
+		if (!empty($this->interfaceObj['entry']['media$group']['media$category'][0]['$t'])) {
+			
+			return $this->interfaceObj['entry']['media$group']['media$category'][0]['$t'];
+		}
+		
+		return '';
+	}
+	
+	/**
+	 * Time that this feed entry was created, in UTC
+	 * @return string
+	 */
+	protected function getVideoPublished() {
+		if (!empty($this->interfaceObj['entry']['published']['$t'])) {
+			
+			return $this->interfaceObj['entry']['published']['$t'];
+		}
+		
+		return '';
+	}
+	
+	/**
+	 * Video duration, in seconds
+	 * @return int
+	 */
+	protected function getVideoDuration() {
+		if (!empty($this->interfaceObj['entry']['media$group']['yt$duration']['seconds'])) {
+			
+			return $this->interfaceObj['entry']['media$group']['yt$duration']['seconds'];
+		}
+		
+		return '';
+	}
+	
+	/**
+	 * returns array of thumbnail data. Thumbnails taken from different 
+	 * points of video. Elements: time, height, width, url
+	 * @return array
+	 */
+	protected function getVideoThumbnails() {
+		if (!empty($this->interfaceObj['entry']['media$group']['media$thumbnail'])) {
+			
+			return $this->interfaceObj['entry']['media$group']['media$thumbnail'];
+		}
+		
+		return array();
+	}
+	
+	/**
+	 * Is resolution of 720 or higher available
+	 * @return boolean 
+	 */
+	protected function isHdAvailable() {
+		return isset($this->interfaceObj['entry']['yt$hd']);
+	}
+	
+	/**
+	 * Can video be embedded
+	 * @return boolean
+	 */
+	protected function canEmbed() {
+		if (!empty($this->interfaceObj['entry']['yt$accessControl'])) {
+			foreach ($this->interfaceObj['entry']['yt$accessControl'] as $accessControl) {
+				if ($accessControl['action'] == 'embed') {
+					return $accessControl['permission'] == 'allowed';
+				}
+			}
+		}
+		
+		return true;
+	}
 }
