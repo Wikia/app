4379c7f3812e0fb0b44fd7a586dd01ccda7cba80
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-11 23:21:36 +0000

    added HasCategory extension BugzId:18185
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46652 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/3rdparty/HasCategory/HasCategory.i18n.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Internationalisation for extension HasCategory.
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Matěj Grabovský
+ */
+$messages['en'] = array(
+	'hascategory-desc' => 'Ehances parser with ability to find out whether a page is contained within a category with the <tt><nowiki>{{#ifhascat:}}</nowiki></tt> parser function.',
+);
+
+/** Message documentation (Message documentation)
+ * @author Matěj Grabovský
+ */
+$messages['qqq'] = array(
+	'hascategory-desc' => 'Short description of the extension. Shown in [[Special:Version]].',
+);
+
+/** Czech (Česky)
+ * @author Matěj Grabovský
+ */
+$messages['cs'] = array(
+	'hascategory-desc' => 'Obohacuje parser možností zjistit, zda-li je určitá stránka v určité kategorii pomocí funkce parseru <tt><nowiki>{{#ifhascat:}}</nowiki></tt>.',
+);
