11784fabd82f19f3332253e4723b8968f5087a7a
macbre <macbre@wikia-inc.com>
2012-01-12 09:35:37 +0000

    Chat: JS linitng
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46663 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat/js/server_api.js
+++ extensions/wikia/Chat/js/server_api.js
@@ -9,7 +9,7 @@
 var config = require("./server_config.js"); // our node-chat config
 var http = require("http");
 var urlUtil = require('url');
-var redis = require('redis')
+var redis = require('redis');
 var rc = redis.createClient();
 rc.on('error', function(err) {
     console.log('Error ' + err);
@@ -19,7 +19,7 @@ rc.on('error', function(err) {
 http.createServer(function (req, res) {
 	var reqData = urlUtil.parse(req.url, true);
 	apiDispatcher(req, res, reqData, function(result){
-		resg = res;
+		resg = res; // macbre: is this variable needed?
 		// SUCCESS CALLBACK
 		res.writeHead(200, {'Content-Type': 'application/json'});
 		res.write( JSON.stringify( result ) );
@@ -44,13 +44,13 @@ console.log("API server running on port " + config.API_SERVER_PORT);
 function apiDispatcher(req, res, reqData, successCallback, errorCallback){
 	if(reqData.query.func){
 		var func = reqData.query.func.toLowerCase();
-		
+
 		if(func == "getdefaultroomid"){
 			// TODO: FIXME: ADD SOME TOKEN-VERIFICATION. This would make sure that only the MediaWiki app is creating rooms (it can check permissions, make sure the extraDataString is good, etc.).
 			// TODO: FIXME: ADD SOME TOKEN-VERIFICATION. This would make sure that only the MediaWiki app is creating rooms (it can check permissions, make sure the extraDataString is good, etc.).
-			
+
 			api_getDefaultRoomId(reqData.query.wgCityId, reqData.query.defaultRoomName,
-								 reqData.query.defaultRoomTopic, reqData.query.extraDataString, reqData.query.roomType, reqData.query.roomUsers, 
+								 reqData.query.defaultRoomTopic, reqData.query.extraDataString, reqData.query.roomType, reqData.query.roomUsers,
 								 successCallback, errorCallback);
 
 		} else if(func == "getcityidforroom"){
@@ -87,11 +87,11 @@ function api_getDefaultRoomId(cityId, defaultRoomName, defaultRoomTopic, extraDa
 	var roomId = "";
 	var roomName = "";
 	var roomTopic = "";
-	
+
 	users = typeof(users) == 'undefined' ? []:users.split(',');
-	
+
 	var keyForListOfRooms = config.getKey_listOfRooms(cityId, type, users);
-	
+
 	//console.log("About to get rooms using key: " + keyForListOfRooms);
 	rc.llen(keyForListOfRooms, function(err, numRooms){
 		if (err) {
@@ -118,10 +118,10 @@ function api_getDefaultRoomId(cityId, defaultRoomName, defaultRoomTopic, extraDa
 							// No luck loading the room... create it.
 							api_createChatRoom(cityId, defaultRoomName, defaultRoomTopic, extraDataString, type, users, successCallback, errorCallback);
 						} else {
-						
+
 							console.log("Room data loaded for " + roomData.room_name);
 							//console.log(roomData);
-						
+
 							var result = {
 								'type' : users,
 								'key' :	keyForListOfRooms,
@@ -141,7 +141,7 @@ function api_getDefaultRoomId(cityId, defaultRoomName, defaultRoomTopic, extraDa
 			});
 		} else {
 			console.log("Warning: First room not found even though there were rooms a moment ago for cityId: " + cityId);
-			
+
 			api_createChatRoom(cityId, defaultRoomName, defaultRoomTopic, extraDataString, type, users, successCallback, errorCallback);
 		}
 	});
@@ -217,7 +217,7 @@ function api_createChatRoom(cityId, roomName, roomTopic, extraDataString, type, 
  * Given a roomId, returns the cityId which it has in redis (as JSON).
  */
 function api_getCityIdForRoom(roomId, successCallback, errorCallback){
-	
+
 	rc.hget(config.getKey_room(roomId), 'wgCityId', function(err, cityId){
 		if (err) {
 			var errorMsg = 'Error: while getting wgCityId of room: "'+ roomId + '": ' + err;
@@ -256,9 +256,9 @@ function api_getRoomsList(successCallback, errorCallback){
 	rc.keys(config.getKey_room("*"), function(err, roomKeys){
 		test.push( roomKeys );
 			for(var index in roomKeys){
-				test.push( roomKeys[index] );	
-				rc.hgetall( 'room:13' , function(err, data) {	
-					test.push( data );	
+				test.push( roomKeys[index] );
+				rc.hgetall( 'room:13' , function(err, data) {
+					test.push( data );
 					successCallback( test );
 				});
 		}
@@ -308,7 +308,7 @@ function api_getStats(successCallback, errorCallback){
 	var out = {};
 	out.node_heapTotal = process.memoryUsage().heapTotal;
 	out.node_heapUsed = process.memoryUsage().heapUsed;
-	
+
 	api_getRedisStats(function(data){
 		var redis_pid = data.process_id;
 		out.redis_used_memory = data.used_memory;
@@ -317,30 +317,30 @@ function api_getStats(successCallback, errorCallback){
 		out.redis_uptime_in_days = data.uptime_in_days;
 		out.redis_used_cpu_sys = data.used_cpu_sys;
 		out.redis_used_cpu_user = data.used_cpu_user;
-		out.redis_used_cpu_by_days = (out.redis_used_cpu_sys + out.redis_used_cpu_user)/out.redis_uptime_in_days;  
-			
+		out.redis_used_cpu_by_days = (out.redis_used_cpu_sys + out.redis_used_cpu_user)/out.redis_uptime_in_days;
+
 		rc.hgetall(config.getKey_runtimeStats(), function(err, data){
 			if(err) {
-				errorCallback(err);	
+				errorCallback(err);
 				return ;
 			}
 			out.runtime = (parseInt(new Date().getTime()) - parseInt(data.laststart) + parseInt(data.runtime) );
 			out.avg_runtime = out.runtime/(parseInt(data.startcount) + 1);
 			rc.hget(config.getKey_userCount(), 'count', function(err, data){
 				if(err) {
-					errorCallback(err);	
+					errorCallback(err);
 					return ;
 				}
 				out.usercount = data;
-				
+
 				getCpuPused( process.pid, function(cpu) {
 					out.node_cpup = cpu;
 					getCpuPused( redis_pid, function(cpu) {
 						out.redis_cpup = cpu;
 						successCallback(out);
-					}); 
+					});
 				});
-		
+
 			});
 		});
 
