72c0c1e7af1d49f753e9b440db7fc70cd4f91a01
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-16 13:45:48 +0000

    LiteSemantics: cleanup and more example hook code
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46777 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/hacks/LiteSemantics/LiteSemantics.class.php
+++ extensions/wikia/hacks/LiteSemantics/LiteSemantics.class.php
@@ -1,150 +1,256 @@
 <?php
+/**
+ * Lite Semantics Pull Parser
+ * @author Federico "Lox" Lucignano
+ * @see http://www.codem.com.au/streams/2009/web-development/consuming-xml-fast-with-php-and-xmlreader.html
+ */
 
+ //test http://codepad.org/xKYZa2RQ
+ 
 class LiteSemanticsParser {
+	const DATA_TAG_OPENING = '<data';
+	const DATA_TAG_CLOSING = '</data>';
 
-	static protected $instance = null;
-	protected $sText = '';
-	protected $aData = array();
+	protected $xmlReader = null;
+	protected $dataItems = null;
 
-	protected function __construct() {
-
+	function __construct(){
+		$this->xmlReader = new XMLReader();
+		$this->dataItems = new LiteSemanticsCollection();
 	}
 
-	protected function __clone() {
-
-	}
-
-	static public function getInstance() {
-		if (self::$instance == null) {
-			self::$instance = new LiteSemanticsParser;
-		}
-		return self::$instance;
-	}
-
-	function parse( $sText ){
-		$this->setText( $sText );
-		$this->parseData();
-		return $this->getResults();
-	}
-
-	protected function insertData( $sText, $iOffset ) {
+	public function parse( $document ){
+		$prevIndex = 0;
+		$closingLength = strlen( self::DATA_TAG_CLOSING );
+		$counter = 0;
 		
-		$oData = F::build(
-			'LiteSemanticsData',
-			array(
-				$this->getDataText( $sText ),
-				$iOffset,
-				$this->getDataName( $sText )
-			)
-		);
+		while ( ( $startIndex = strpos( $document, '<data', $prevIndex ) ) !== false ) {
+			$endIndex = strpos( $document, '</data>', $startIndex );
 
-		$this->insertProperties( $oData, $sText );
-
-		if ( $oData->isValid() ){
-			$this->aData[ $iOffset ] = $oData;
-		}
-	}
-
-	protected function insertProperties( $oData, $sDataText ){
-		preg_match_all(
-			'/<prop.*>(.|\n)*<\/.*prop.*>/iU',
-			$sDataText,
-			$aPregResults
-		);
-
-		if ( !isset( $aPregResults[0] ) ){
-			return false;
-		}
-
-		foreach( $aPregResults[0] as $aDataResult ){
-			$oRes = simplexml_load_string( $aDataResult );
-			if (	!isset( $oRes['name'] ) ||
-				!isset( $oRes['name'][0] ) || 
-				empty( $oRes['name'][0] )
-			){
-				break;
+			if ( $endIndex === false ) {
+				throw new LiteSemanticsParserException( $startIndex, substr( $document, $startIndex ) );
+			} else {
+				$endIndex += $closingLength;
 			}
-			$sMeaning = $oRes['name']['0'];
-			$sValue = preg_replace( '/<(\/|)(prop|data)[^>]*>/iU', '', $aDataResult );
-			$oData->addProperty( $sMeaning, $sValue );
+
+			$markup = substr( $document, $startIndex, $endIndex - $startIndex );
+			
+			if (
+				$this->xmlReader->xml( $markup ) &&
+				$this->xmlReader->read() &&
+				$this->xmlReader->name == 'data'
+			) {
+				$data = new LiteSemanticsData( $this->xmlReader->readInnerXML(), $startIndex, $endIndex );
+
+				while ( $this->xmlReader->moveToNextAttribute() ) {
+					$data->setAttribute( $this->xmlReader->name, $this->xmlReader->value );
+				}
+				
+				
+				while (
+					$this->xmlReader->read()
+				){
+					if(
+						$this->xmlReader->nodeType == XMLReader::ELEMENT &&
+						$this->xmlReader->name == 'prop'
+					){//cannot be checked together with read() in a subtree :(
+						$property = new LiteSemanticsProperty( $this->xmlReader->readInnerXML() );//value won't work here as it's embedded in a text node
+						$type = null;
+
+						while ( $this->xmlReader->moveToNextAttribute() ) {
+							echo($this->xmlReader->name);
+							echo('---');
+							if ( $this->xmlReader->name == 'type' ) {
+								$property->setType( $type = $this->xmlReader->value );
+							} else {
+								$property->setAttribute( $this->xmlReader->name, $this->xmlReader->value );
+							}
+						}
+
+						if ( $type !== null && $type !== '' ){
+							$data->setProperty( $type, $property);
+						} else {
+							//TODO: decide if warning, parser exception or specific kind of exception
+						}
+					}
+				}
+
+				$this->dataItems->storeItem( $counter, $data );
+			} else {
+				throw new LiteSemanticsParserException( $startIndex, $markup );
+			}
+
+			$prevIndex = $endIndex;
+			$counter++;
 		}
 	}
 
-	protected function getDataText( $sText ){
-		return preg_replace( '/<(\/|)(data|prop)[^>]*>/iU', '', $sText );
-	}
-
-	protected function getDataName( $sText ){
-		preg_match( '/<data.*(name)=("[^"]*")>/iU', $sText, $result );
-		return isset( $result[2] ) ? $result[2] : false;
-	}
-
-	protected function setText($sText) {
-		$this->sText = $sText;
-	}
-
-	protected function parseData() {
-		preg_match_all(
-			'/<data.*>(.|\n)*<\/.*data.*>/iU',
-			$this->sText,
-			$aPregResults,
-			PREG_OFFSET_CAPTURE
-		);
-
-		if ( !isset( $aPregResults[0] ) ){
-			return false;
-		}
-
-		foreach( $aPregResults[0] as $aDataResult ){
-			$this->insertData( $aDataResult[0], $aDataResult[1] );
-		}
-	}
-
-	protected function getResults() {
-		return $this->aData;
-	}
-
-}
-
-class LiteSemanticsData {
-
-	protected $sText = '';
-	protected $sName = '';
-	protected $aProperties = array();
-	protected $iOffset = false;
-
-	function __construct($sText, $iOffset, $sName = '') {
-		$this->sText = $sText;
-		$this->sName = $sName;
-	}
-
-	function addProperty($sMeaning, $mValue) {
-		$aProperties[] = F::build('LiteSemanticsProperty', array($sMeaning, $mValue));
-	}
-
-	function fitsPattern($oPattern) {
-		if ($oPattern instanceof LiteSemanticsParserPattern) {
-			/* some logic here */
-		}
-		return false;
-	}
-
-	function isValid(){
-		return true;
+	//TODO: implement a LiteSemanticsDocument class as a DTO, which will get data
+	public function getData(){
+		return $this->dataItems;
 	}
 }
 
-class LiteSemanticsProperty {
+class LiteSemanticsEntity {
+	protected $name = null;
+	protected $value = null;
 
-	var $sMeaning = '';
-	var $mValue = '';
+	function __construct( $name, $value = null ){
+		$this->name = $name;
+		$this->value = $value;
+	}
 
-	function __construct($sMeaning, $mValue) {
-		$this->sMeaning = $sMeaning;
-		$this->mValue = $mValue;
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName( $name ){
+		$this->name = $name;
+	}
+
+	public function getValue(){
+		return $this->value;
+	}
+
+	public function setValue( $value ){
+		$this->value = $value;
 	}
 }
 
-class LiteSemanticsParserPattern {
+class LiteSemanticsAttribute extends LiteSemanticsEntity{
+	function __construct( $name, $value = null){
+		parent::__construct( $name, $value );
+	}
+
+	function __toString(){
+		return $this->name . ( $value !== null ) ? '="' . str_replace( '"', '\"', $value ) . '"' : '';
+	}
+}
+
+class LiteSemanticsProperty extends LiteSemanticsEntity{
+	protected $attributes = null;
 	
+	function __construct( $value = null ){
+		parent::__construct( __CLASS__, $value );
+		$this->attributes = new LiteSemanticsCollection();
+	}
+
+	public function getType(){
+		return $this->name;
+	}
+
+	public function setType( $type ){
+		$this->name = $type;
+	}
+
+	public function hasAttributes(){
+		return $this->attributes->count() != 0;
+	}
+
+	public function getAttribute( $name ){
+		return $this->attributes->getItem( $name );
+	}
+
+	public function setAttribute( $name, $value ){
+		$this->attributes->storeItem( $name, new LiteSemanticsAttribute( $name, $value ) );	
+	}
 }
+
+class LiteSemanticsCollection{
+	protected $items = null;
+
+	function __construct(){/* ... */}
+
+	public function exists( $name ){
+		return $this->items !== null && array_key_exists( $name, $this->items );
+	}
+
+	public function storeItem( $name, LiteSemanticsEntity $item ){
+		if( $this->items === null ) {
+			$this->items = array();
+		}
+
+		$this->items[$name] = $item;
+	}
+
+	public function getItem( $name ){
+		return ( $this->exists( $name ) ) ? $this->items[$name] : null;
+	}
+
+	public function removeItem( $name ){
+		if ( $this->exists( $name ) ) {
+			unset( $this->items[$name] );
+		}
+	}
+
+	public function count(){
+		return ( $this->items !== null ) ? count( $this->items ) : 0;
+	}
+
+	public function rewind(){
+		reset( $this->items );
+    }
+
+    public function current(){
+		return current( $this->items );
+    }
+
+    public function key(){
+		return key( $this->items );
+    }
+
+    public function next(){
+		return next( $this->items );
+    }
+
+    public function valid(){
+        $key = key( $this->items );
+		return ( $key !== NULL && $key !== FALSE );
+    }
+}
+
+class LiteSemanticsData extends LiteSemanticsEntity{
+	protected $attributes = null;
+	protected $properties = null;
+	protected $startIndex = null;
+	protected $endIndex = null;
+	
+	function __construct( $content = null, $startIndex = null, $endIndex = null ){
+		parent::__construct( /*uniqid(*/__CLASS__/*)*/, $content );
+		$this->attributes = new LiteSemanticsCollection();
+		$this->properties = new LiteSemanticsCollection();
+		$this->startIndex = $startIndex;
+		$this->endIndex = $endIndex;
+	}
+	
+	public function hasAttributes(){
+		return $this->attributes->count() != 0;
+	}
+
+	public function getAttribute( $name ){
+		return $this->attributes->getItem( $name );
+	}
+
+	public function setAttribute( $name, $value ){
+		$this->attributes->storeItem( $name, ( $value instanceof LiteSemanticsAttribute ) ? $value : new LiteSemanticsAttribute( $name, $value ) );	
+	}
+
+	public function hasProperties(){
+		return $this->properties->count() != 0;
+	}
+
+	public function getProperty( $name ){
+		return $this->properties->getItem( $name );
+	}
+
+	public function setProperty( $name, $value ){
+		$this->properties->storeItem( $name, ( $value instanceof LiteSemanticsProperty ) ? $value : new LiteSemanticsProperty( $name, $value ) );	
+	}
+}
+
+class LiteSemanticsParserException extends Exception{
+	function __construct( $index, $invalidMarkup = null ){
+		parent::__construct( "Invalid Lite Semantics markup at index {$index}" . ( !empty( $invalidMarkup ) ) ? ": <pre>{$invalidMarkup}</pre>" : '' );
+	}
+}
+?>
--- extensions/wikia/hacks/LiteSemantics/LiteSemanticsHooks.class.php
+++ extensions/wikia/hacks/LiteSemantics/LiteSemanticsHooks.class.php
@@ -3,7 +3,13 @@ class LiteSemanticsHooks extends WikiaObject{
 
 	public function onInternalParseBeforeLinks( $oParser, $sText, $mStripState ){
 
-		/* Shazam - parser magic */
+		//TODO: check if semantics parsing should actually happen (only edits/purges)
+
+		$parser = F::build( 'LiteSemanticsParser' )->parse( $sText );
+
+		$semanticsData = $parser->getData();
+
+		//TODO: do something with the data
 
 		return true;
 	}
