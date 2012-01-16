62ebffceb8802ccb9d7292b46858d838ef0f05fc
sean <sean@wikia-inc.com>
2012-01-16 22:04:19 +0000

    BugzId 19038: formatting fixes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46803 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat/js/lib/md5.js
+++ extensions/wikia/Chat/js/lib/md5.js
@@ -2,9 +2,8 @@ var crypto = require('crypto');
 
 var md5 = function (plainText) {
 	var md5Hash = crypto.createHash('md5');
-  //plainText = utf8_encode(plainText); // should be able to skip this line
-  md5Hash.update(plainText);
-  return md5Hash.digest('hex');
+	md5Hash.update(plainText);
+	return md5Hash.digest('hex');
 }
 
 module.exports.md5 = md5;
