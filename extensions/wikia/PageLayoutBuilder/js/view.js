ed4527ccd70e30288cfef0e93daff1c564b8b456
macbre <macbre@wikia-inc.com>
2012-01-16 15:36:02 +0000

    PLB: JS linting
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46783 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/PageLayoutBuilder/js/view.js
+++ extensions/wikia/PageLayoutBuilder/js/view.js
@@ -1,16 +1,16 @@
 $( function(){
 	if($('.position-marker').exists()) {
 	    $('.editsection a').click(function(e) {
-	    	$().log('go to section','PLB')
-	        var target = $(e.target);
-	    	var marker = target.closest('h1,h2,h3').nextAll('.position-marker:first')
+	    	$().log('go to section','PLB');
+	        var target = $(e.target),
+	    		marker = target.closest('h1,h2,h3').nextAll('.position-marker:first');
 	    	if(marker.exists()) {
 		        var id = target.closest('h1,h2,h3').nextAll('.position-marker:first').attr('id');
 		        window.location =  target.attr('href') + '#' + id ;
-		        return false;	
+		        return false;
 	    	} else {
 	    		return true;
-	    	} 
+	    	}
 	    });
-	}	
+	}
 });
