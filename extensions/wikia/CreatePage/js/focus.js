3acef73212a2e4c552b7c40edb9a3c86bdd62c15
macbre <macbre@wikia-inc.com>
2012-01-20 12:20:03 +0000

    CreatePage: JS linting
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46952 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/CreatePage/js/focus.js
+++ extensions/wikia/CreatePage/js/focus.js
@@ -1,21 +1,13 @@
-$(
-function()
-{
+$(function() {
     $("#postTitle").keydown(function (e) {
     	if(e.keyCode == 9){
-    		if (typeof FCK != "undefined" ) {
-    			FCK.Focus();
-    			return false;
-    		}
-    		
     		if (typeof RTE != "undefined" ) {
     			RTE.instance.focus();
     			return false;
     		}
-    		
+
     		$("#wpTextbox1").focus();
     		return false;
-    	};
+    	}
     });
-}
-);
+});
