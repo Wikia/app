6580fe9d1264b60d39ef58a194b0f7cff7bcf571
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-20 13:45:50 +0000

    WikiaMobile: group more than two images in a row in a media stack (BugzId:17543)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46962 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/WikiaMobileHooks.class.php
+++ extensions/wikia/WikiaMobile/WikiaMobileHooks.class.php
@@ -5,12 +5,16 @@
  * @author Federico "Lox" Lucignano <federico(at)wikia-inc.com>
  */
 class WikiaMobileHooks extends WikiaObject{
+	const IMAGE_GROUP_MIN = 2;
+
 	public function onParserAfterTidy( &$parser, &$text ){
 		//cleanup page output from unwanted stuff
 		if ( $parser->getOptions()->getSkin() instanceof SkinWikiaMobile ) {
 			//remove inline styling to avoid weird results and optimize the output size
 			$text = preg_replace('/\s+(style|color|bgcolor|border|align|cellspacing|cellpadding|hspace|vspace)=(\'|")[^"\']*(\'|")/im', '', $text );
-			
+
+			//transform groups of IMAGE_GROUP_MIN images in a row into a media stack
+			$text = preg_replace( '/(\s*<figure[^>]*>(<\/?a|<img|<\/?figcaption|[^<])+<\/figure>\s*){' . self::IMAGE_GROUP_MIN . ',}/im', '<section class="wkImgStk group">$0</section>', $text );
 		}
 
 		return true;
