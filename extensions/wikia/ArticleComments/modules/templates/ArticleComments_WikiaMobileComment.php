3dac48c2da7c9dbcb0cda4128b65b50fd1cb1666
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-20 18:18:09 +0000

    WikWikiaMobile: cleaning up CSS/JS/HTML (BugzId:18852)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46978 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ArticleComments/modules/templates/ArticleComments_WikiaMobileComment.php
+++ extensions/wikia/ArticleComments/modules/templates/ArticleComments_WikiaMobileComment.php
@@ -1,15 +1,15 @@
-<li id="comm<?=$commentId?>" class="comment" data-user="<?=$comment['username']?>">
-	<div class="avatar">
-		<a href="<?= $comment['userurl'] ?>"><?= $comment['avatar'] ?></a>
+<li id=comm<?=$commentId?> class=comment data-user="<?=$comment['username']?>">
+	<div class=avatar>
+		<a href=<?= $comment['userurl'] ?>><?= $comment['avatar'] ?></a>
 	</div>
-	<blockquote class="msg">
-		<div class="by">
+	<blockquote class=msg>
+		<div class=by>
 			<?= $comment['sig'] ;?>
 			<? if( !empty( $comment['isStaff'] ) ) :?>
-			<img class="staff" src="http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png" alt="@wikia"/></span>
+			<img class=staff src=http://images.wikia.com/wikia/images/e/e9/WikiaStaff.png alt=@wikia/></span>
 			<? endif ;?>
 		</div>
-		<div class="txt"><?= $comment['text'] ?></div>
-		<div class="date"><?= $comment['timestamp'] ;?></div>
+		<div class=txt><?= $comment['text'] ?></div>
+		<div class=date><?= $comment['timestamp'] ;?></div>
 	</blockquote>
 </li>
