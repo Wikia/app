3f0447fd6fee537c22d81dbf69cff694735d866c
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-20 18:18:09 +0000

    WikWikiaMobile: cleaning up CSS/JS/HTML (BugzId:18852)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@46978 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ArticleComments/modules/templates/ArticleComments_WikiaMobileIndex.php
+++ extensions/wikia/ArticleComments/modules/templates/ArticleComments_WikiaMobileIndex.php
@@ -1,15 +1,15 @@
-<section id="wkArtCom" data-pages="<?= $pagesCount ;?>">
-	<h1 class="collSec"><?= $wf->MsgExt( 'wikiamobile-article-comments-header', array('parsemag'), $wg->Lang->formatNum( $countCommentsNested ) ) ;?><span class="chev"></span></h1>
-	<div class="comms">
+<section id=wkArtCom data-pages="<?= $pagesCount ;?>">
+	<h1 class=collSec><?= $wf->MsgExt( 'wikiamobile-article-comments-header', array('parsemag'), $wg->Lang->formatNum( $countCommentsNested ) ) ;?><span class=chev></span></h1>
+	<div class=comms>
 		<? if ( $countCommentsNested > 0 ) :?>
 			<? if ( $showMore ) :?>
-				<div id="commPrev"><span class="lbl"><?= $wf->Msg( 'wikiamobile-article-comments-prev' ) ;?></span></div>
+				<div id=commPrev><span class=lbl><?= $wf->Msg( 'wikiamobile-article-comments-prev' ) ;?></span></div>
 			<? endif ;?>
 			
 			<?= wfRenderPartial( 'ArticleComments', 'CommentList', array( 'commentListRaw' => $commentListRaw, 'page' => $page, 'useMaster' => false ) );?>
 		
 			<? if ( $showMore ) :?>
-				<div id="commMore"><span class="lbl"><?= $wf->Msg( 'wikiamobile-article-comments-more' ) ;?></span></div>
+				<div id=commMore><span class=lbl><?= $wf->Msg( 'wikiamobile-article-comments-more' ) ;?></span></div>
 			<? endif ;?>
 		<? else :?>
 			<?= $wf->Msg( 'wikiamobile-article-comments-none' ) ;?>
