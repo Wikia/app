ec0b167acbd80ead1bf184418e47693d3f0e3169
macbre <macbre@wikia-inc.com>
2012-01-23 11:07:00 +0000

    LatestQuestions: JS linting
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47032 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/LatestQuestions/LatestQuestions.js
+++ extensions/wikia/LatestQuestions/LatestQuestions.js
@@ -1,30 +1,34 @@
-$(window).load(function() {
-    LatestQuestions.init();
-});
-
 var LatestQuestions = {
     init: function() {
         $.getScript(wgAnswersServer + wgAnswersScript + '?action=ajax&rs=moduleProxy&moduleName=LatestActivity&actionName=Index&outputType=data&callback=LatestQuestions.onLoad&uselang='+wgUserLanguage);
     },
     onLoad: function(data) {
-	if(data.changeList.length > 0) {
-            var html = "<section class='LatestQuestionsModule module'>";
-	    html += "<h1 class='activity-heading'>"+wgLatestQuestionsHeader+"</h1>";
-	    html += "<ul>";
-            for(var i=0; i<data.changeList.length; i++) {
-		html += "<li><img src='"+wgBlankImgUrl+"' class='sprite "+data.changeList[i].changeicon+"' height='20' width='20'><em>"+data.changeList[i].page_href+"</em><div class=\"edited-by\">"+data.changeList[i].changemessage+"</div></li>";
-            }
-	    html += "</ul>";
-	    html += "<a href='/wiki/Special:WikiActivity' title='Special:WikiActivity' class='more'>"+wgOasisMoreMsg+"</a>";
-	    html += "</section>";
-            if ($.browser.msie) {
-	        $("section.LatestQuestionsModule").empty().append(html);
-            } else {
-	        $("section.LatestQuestionsModule").replaceWith(html);
-            }
-	    $("section.LatestQuestionsModule a").attr("href", function(i, val) {
-                return wgAnswersServer + val;
-            });
-        }
-    }
+		if(data.changeList.length > 0) {
+			var html = "<section class='LatestQuestionsModule module'>" +
+				"<h1 class='activity-heading'>"+wgLatestQuestionsHeader+"</h1>" +
+				"<ul>";
+
+			for(var i=0; i<data.changeList.length; i++) {
+				html += "<li><img src='"+wgBlankImgUrl+"' class='sprite "+data.changeList[i].changeicon+"' height='20' width='20'><em>"+data.changeList[i].page_href+"</em><div class=\"edited-by\">"+data.changeList[i].changemessage+"</div></li>";
+			}
+
+			html += "</ul>";
+			html += "<a href='/wiki/Special:WikiActivity' title='Special:WikiActivity' class='more'>"+wgOasisMoreMsg+"</a>";
+			html += "</section>";
+
+			if ($.browser.msie) {
+				$("section.LatestQuestionsModule").empty().append(html);
+			} else {
+				$("section.LatestQuestionsModule").replaceWith(html);
+		    }
+
+			$("section.LatestQuestionsModule a").attr("href", function(i, val) {
+				return wgAnswersServer + val;
+			});
+		}
+	}
 }
+
+$(window).load(function() {
+    LatestQuestions.init();
+});
