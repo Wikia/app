4abc4cd23d53bc77991ac3825dff5533e57fc188
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-24 12:58:22 +0000

    WikiaMobile: Galleries/Slideshows hang (BugzId:19526)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47102 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaPhotoGallery/templates/renderWikiaMobileSlideshow.tmpl.php
+++ extensions/wikia/WikiaPhotoGallery/templates/renderWikiaMobileSlideshow.tmpl.php
@@ -1,4 +1,12 @@
-<section class=wkImgStk data-img-count=<?=$count;?>
-		<?php foreach ( $images as $key => $val) {
-			echo " data-img-{$key}='{$val[0]}'" . ( $val[1]  ? " data-cap-{$key}='{$val[1]}'" : "");
-		} ?>><img src=<?=$images[0][0]?>><footer><?=$footerText?></footer></section>
+<section class=wkImgStk data-img-count=<?= $count ;?>>
+	<img src=<?= $images[0][0] ;?>>
+	<? if( $count ) :?>
+		<ul>
+		<? foreach ( $images as $val) :?>
+			<li data-img='<?= $val[0];?>'><?= $val[1] ;?></li>
+		<? endforeach ;?>
+		</ul>
+		
+	<? endif ;?>
+	<footer><?= $footerText ;?></footer>
+</section>
