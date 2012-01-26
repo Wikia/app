b307fecc966f4080594c7e0f6ad91ca72f15a5ab
michal <michal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-26 12:57:20 +0000

    Replaced NewWindowLinks static methods with ef* global functions and removed the NewWindowLinks class.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47223 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/NewWindowLinks/NewWindowLinks.php
+++ extensions/wikia/NewWindowLinks/NewWindowLinks.php
@@ -36,9 +36,67 @@ $wgExtensionCredits['parserhook'][] = array(
 );
 
 /**
- * Classes to be loaded.
+ * Extension functions 
  */
-$wgAutoloadClasses['NewWindowLinks'] = __DIR__ . '/NewWindowLinks.class.php';
+
+/**
+ * Creates a HTML link that will open in a new browser window or tab.
+ * 
+ * @param object $parser Parser being used
+ * @param string $target literal URI or a MediaWiki link for linked resource
+ * @param string $label link text
+ * @return string HTML
+ */
+function efParserCreateLink( $parser, $target, $label = null ) {
+    // sanitise the input and set defaults
+    if ( is_null( $label ) ) { $label = $target; }
+    $label = htmlspecialchars( $label, ENT_NOQUOTES, 'UTF-8' );
+    $attributes = array( 'target' => '_blank' );
+
+    // WARNING: the order of the statements below does matter!
+    // 
+    // Also, the Parser::insertStripItem is used to render the HTML inline.
+    // See: http://www.mediawiki.org/wiki/Manual:Parser_functions#Parser_interface
+
+        // As of the 1.18.0 MW release Linker::link and Linker::makeExternalLink are static methods.
+        // Prior to that release the Linker class have to be instantiated in order to use those methods.
+        // See: http://trac.wikia-code.com/browser/wikia/trunk/extensions/wikia/NewWindowLinks/NewWindowLinks.class.php?rev=46348&marks=37-51#L35
+        $oLinker = new Linker();
+
+    // Process (or rule out) external targets (literal URIs)
+    if ( preg_match( $parser->mExtLinkBracketedRegex, "[{$target}]" ) ) {
+        return $parser->insertStripItem( $oLinker->makeExternalLink( $target, $label, true, '', $attributes ) , $parser->mStripState );
+    }
+
+    // The target is not a literal URI.  Create a Title object.
+    $oTitle = Title::newFromText( $target );
+
+    // Process (or rule out) existing local articles.
+    if ( $oTitle->exists() ) {
+        return $parser->insertStripItem( $oLinker->link( $oTitle, $label, $attributes ), $parser->mStripState );
+    }
+
+    // Process (or rule out) interwiki links.
+    if ( true !== $oTitle->isLocal() ) {
+        return $parser->insertStripItem( $oLinker->makeExternalLink( $oTitle->getFullURL(), $label, true, '', $attributes ), $parser->mStripState );
+    }
+
+    // Only non existing local articles remain.
+    return $parser->insertStripItem( $oLinker->link( $oTitle, $label, $attributes ), $parser->mStripState );
+}
+
+/**
+ * Registers a new parser function.
+ * 
+ * @param object $parser Parser being initialised
+ * @return boolean
+ */
+function efHookParserFirstCallInit( &$parser ) {
+    // associate the "NewWindowLink" magic word with the NewWindowLinks::parserCreateLink method.
+    $parser->setFunctionHook( 'NewWindowLink', 'efParserCreateLink' );
+    // return true so the MediaWiki continues to load extensions.
+    return true;
+}
 
 /**
  * Internationalisation: messages and magic words.
@@ -48,4 +106,4 @@ $wgExtensionMessagesFiles['NewWindowLinks'] = __DIR__ . '/NewWindowLinks.i18n.ph
 /**
  * Hooks.
  */
-$wgHooks['ParserFirstCallInit'][]   = 'NewWindowLinks::hookParserFirstCallInit';
+$wgHooks['ParserFirstCallInit'][]   = 'efHookParserFirstCallInit';
