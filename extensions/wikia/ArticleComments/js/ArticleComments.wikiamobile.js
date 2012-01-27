ecc03b82c857b191f873b4f5497b0c341131ba25
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-27 16:22:49 +0000

    WikiaMobile: comments (BugId:19615)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47277 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ArticleComments/js/ArticleComments.wikiamobile.js
+++ extensions/wikia/ArticleComments/js/ArticleComments.wikiamobile.js
@@ -15,7 +15,8 @@ var ArticleComments = ArticleComments || (function(){
 		ajaxUrl = wgServer + "/index.php?action=ajax&rs=ArticleCommentsAjax&method=axGetComments&useskin=" + skin + "&article=" + wgArticleId,
 		clickEvent = WikiaMobile.getClickEvent(),
 		firstPage,
-		commsUl;
+		commsUl,
+		track = WikiaMobile.track;
 
 	function clickHandler(event){
 		event.preventDefault();
@@ -25,8 +26,10 @@ var ArticleComments = ArticleComments || (function(){
 			pageIndex = (forward) ? currentPage + 1 : currentPage - 1,
 			condition = (forward) ? (currentPage < totalPages) : (currentPage > 1);
 
-		if(currentPage === 1 && !firstPage)
-				firstPage = commsUl.html();
+		if(currentPage === 1)
+				firstPage = commsUl[0].cloneNode(true);
+				
+		track(['comment', 'page', (forward)?'next':'previous']);
 
 		if(condition){
 			elm.toggleClass('active');
@@ -55,55 +58,75 @@ var ArticleComments = ArticleComments || (function(){
 			});
 		}
 	}
-	
+
 	function post(ev) {
 		ev.preventDefault();
 
 		var self = $(this),
-		commId = self.find('input').first(),
+		inputs = self.find('input'),
+		commId = inputs.first(),
+		submit = inputs.last(),
 		parentId = (commId.attr('name') === 'wpParentId')?commId.val():undefined,
-		data = {
-			action:'ajax',
-			article:wgArticleId,
-			method:'axPost',
-			rs:'ArticleCommentsAjax',
-			title:wgPageName,
-			wpArticleComment:self.find('textarea').val(),
-			useskin:'wikiamobile'
-		};
-		
-		if(parentId)
-			data.parentId = parentId;
-
-		$.post(wgScript, data, function(resp) {
-			var json = JSON.parse(resp);
-			if(!json.error){
-				self.find('textarea').val('');
-				var reply = $(json.text);
-				
-				if(parentId) {
-					var prev = self.parent().prev();
-
-					if(prev.hasClass('sub-comments')) {
-						prev.append(reply.find('.sub-comments li'));
-					}else{
-						prev.after(reply.find('.sub-comments'));
+		text = self.find('textarea').val();
+		if(text !== '') {
+			var data = {
+				action:'ajax',
+				article:wgArticleId,
+				method:'axPost',
+				rs:'ArticleCommentsAjax',
+				title:wgPageName,
+				wpArticleComment:text,
+				useskin:'wikiamobile'
+			};
+			
+			submit.attr('disabled', true);
+	
+			if(parentId)
+				data.parentId = parentId;
+	
+			$.post(wgScript, data, function(resp) {
+				var json = JSON.parse(resp);
+				if(!json.error){
+					self.find('textarea').val('');
+					var reply = $(json.text);
+					
+					submit.removeAttr('disabled');
+	
+					if(currentPage > 1){
+						commsUl.remove();
+						loadMore.before(firstPage);
+						commsUl = $('#article-comments-ul');
+						currentPage = 1;
+						loadPrev.hide();
 					}
-				}else{
-					commsUl.prepend(reply.find('li, .rpl'));
+	
+					if(parentId) {
+						track('comment/reply/submit');
+						var prev = self.parent().prev();
+	
+						if(prev.hasClass('sub-comments')) {
+							prev.append(reply.find('.sub-comments li'));
+						}else{
+							prev.after(reply.find('.sub-comments'));
+						}
+					}else{
+						track('comment/new/submit');
+						commsUl.prepend(reply.find('li, .rpl'));
+					}
+					wrapper.children('h1')[0].innerHTML = json.counter + '<span class=chev><span>';
 				}
-				wrapper.children('h1').text(json.counter);
-			}
-		});
+			});
+		}
 	}
 
 	//init
 	$(function(){
+		var postComm = document.getElementById('article-comm-form');
+
 		wrapper = $('#wkArtCom');
 		loadMore = $('#commMore');
 		loadPrev = $('#commPrev');
 		commsUl = $('#article-comments-ul');
-		postComm = document.getElementById('article-comm-form');
 		totalPages = parseInt(wrapper.data('pages'));
 
 		if(totalPages > 1 && wgArticleId){
@@ -114,15 +137,20 @@ var ArticleComments = ArticleComments || (function(){
 		wrapper.on(clickEvent, '.rpl', function(){
 			var self = $(this),
 			prev = self.prev(),
-			rplComm = $(postComm.cloneNode(true)).height(35);
+			rplComm = $(postComm.cloneNode(true));
+			
+			track('comment/reply/form/show');
 
 			if(prev.is('ul')) prev = prev.prev();
-			
+
 			rplComm.find('input').first().attr('name', 'wpParentId').val(prev.attr('id')).next().attr('name', 'wpArticleReply');
 
 			self.removeClass().html('').append(rplComm).find('#article-comm').focus();
 		})
-		.on('submit', '.article-comm-form', post);
+		.on('submit', '.article-comm-form', post)
+		.on(clickEvent, '.collSec', function(){
+			track(['comment',($(this).hasClass('open')?'close':'open')]);
+		});
 	});
 
 	/** @public **/
