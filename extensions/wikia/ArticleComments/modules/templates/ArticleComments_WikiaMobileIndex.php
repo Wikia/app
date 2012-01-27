784dc7c7e20801fd8bd5953252fd90b0f09b76a4
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-27 20:24:23 +0000

    WikiaMobile: non-js comment posting (BugzId:19798)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47281 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ArticleComments/modules/templates/ArticleComments_WikiaMobileIndex.php
+++ extensions/wikia/ArticleComments/modules/templates/ArticleComments_WikiaMobileIndex.php
@@ -1,13 +1,7 @@
 <section id=wkArtCom data-pages="<?= $pagesCount ;?>">
 	<h1 class=collSec><?= $wf->MsgExt( 'wikiamobile-article-comments-header', array('parsemag'), $wg->Lang->formatNum( $countCommentsNested ) ) ;?><span class=chev></span></h1>
 	<div id=article-comments>
-		<? if (!$isReadOnly) { ?>
-		<form method=post class=article-comm-form id=article-comm-form>
-			<input type=hidden name=wpArticleId value="<?= $title->getArticleId() ;?>" />
-			<textarea type=text placeholder="<?= $wf->Msg('wikiamobile-article-comments-placeholder') ;?>" name=wpArticleComment id=article-comm></textarea>
-			<input type=submit name=wpArticleSubmit id=article-comm-submit class=wikia-button value="<?= $wf->Msg('wikiamobile-article-comments-post') ;?>" />
-		</form>
-		<? } ?>
+		<? if ( !$isReadOnly ) :?><?= wfRenderPartial( 'ArticleComments', 'WikiaMobileReply', array( 'title' => $title ) ) ;?><? endif ;?>
 		<? if ( $countCommentsNested > 0 ) :?>
 			<? if ( $pagesCount > 1 ) :?>
 				<a id=commPrev class="lbl<?= ( !empty( $prevPage ) ) ? ' pag" href="?page=' . $prevPage . '#article-comments"' : '' ?>"><?= $wf->Msg( 'wikiamobile-article-comments-prev' ) ;?></a>
