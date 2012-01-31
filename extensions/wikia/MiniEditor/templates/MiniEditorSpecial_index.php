93a0b057e303548a31d9d39f65f3de271a91cbec
kflorence <kflorence@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-01-31 23:02:04 +0000

    Cleanup MiniEditor special page
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47403 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MiniEditor/templates/MiniEditorSpecial_index.php
+++ extensions/wikia/MiniEditor/templates/MiniEditorSpecial_index.php
@@ -1,11 +1,14 @@
 <section class="SpecialPageWrapper">
 
-<? if ($wg->enableMiniEditorExt): echo $app->getView( 'MiniEditorController', 'EditorWrapper_Begin', array(
-	'attributes' => array( 'data-min-height' => 200 )
-))->render(); endif; ?>
+<?= $app->getView( 'MiniEditorController', 'EditorWrapper_Begin', array(
+	'attributes' => array(
+		'data-min-height' => 200,
+		'data-max-height' => 400
+	)
+))->render(); ?>
 
 	<textarea id="SpecialMiniEditor" placeholder="<?= wfMsg('minieditor-placeholder-message') ?>"></textarea>
 
-<? if ($wg->enableMiniEditorExt): echo $app->getView( 'MiniEditorController', 'EditorWrapper_End' )->render(); endif; ?>
+<?= $app->getView( 'MiniEditorController', 'EditorWrapper_End' )->render(); ?>
 
 </section>
