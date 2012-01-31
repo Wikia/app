cd7b884c56b2ac62016f5ce94e90170ed2fea397
kflorence <kflorence@wikia-inc.com>
2012-01-31 23:02:04 +0000

    Cleanup MiniEditor special page
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47403 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MiniEditor/MiniEditorSpecialController.class.php
+++ extensions/wikia/MiniEditor/MiniEditorSpecialController.class.php
@@ -1,69 +1,25 @@
 <?php
 
 /**
- * This is an example use of SpecialPage controller
+ * Special:MiniEditor
  * 
- * @author ADi
- *
+ * @author Liz Lee, Kyle Florence
  */
+
 class MiniEditorSpecialController extends WikiaSpecialPageController {
 
-	private $businessLogic = null;
-	private $controllerData = array();
-
 	public function __construct() {
-		$this->controllerData[] = 'foo';
-		$this->controllerData[] = 'bar';
-		$this->controllerData[] = 'baz';
 
-		// standard SpecialPage constructor call
+		// Params: name, restrictions, listed
 		parent::__construct( 'MiniEditor', '', false );
 	}
 
-
-	/**
-	 * @brief this is default method, which in this example just redirects to Hello method
-	 * @details No parameters
-	 *
-	 */
 	public function index() {
-		$this->response->setVal("test","test");
-
-		// Load MiniEditor assets, if enabled
-		if ($this->wg->EnableMiniEditorExt) {
-			$this->sendRequest('MiniEditor', 'loadAssets', array(
-				'additionalAssets' => array(
-					'extensions/wikia/MiniEditor/js/SpecialPage.js',
-					'extensions/wikia/MiniEditor/css/SpecialPage.scss'
-				)
-			));
-		}
-
+		$this->sendRequest('MiniEditor', 'loadAssets', array(
+			'additionalAssets' => array(
+				'extensions/wikia/MiniEditor/js/SpecialPage.js',
+				'extensions/wikia/MiniEditor/css/SpecialPage.scss'
+			)
+		));
 	}
-
-	/**
-	 * @brief Hello method
-	 * @details Hello method
-	 *
-	 * @requestParam int $wikiId
-	 * @responseParam string $header
-	 * @responseParam array $wikiData
-	 */
-	/*public function MiniEditor() {
-		$this->wf->profileIn( __METHOD__ );
-
-		// getting request data
-		$wikiId = $this->getVal( 'wikiId', $this->wg->CityId );
-
-		// setting response data
-		$this->setVal( 'header', $this->wf->msg('helloworld-hello-msg') );
-		$this->setVal( 'wikiData', $this->businessLogic->getWikiData( $wikiId ) );
-		$this->setVal( 'controllerData', $this->controllerData );
-
-		// example of setting SpecialPage::mIncluding
-		$this->mIncluding = true;
-
-		$this->wf->profileOut( __METHOD__ );
-	}*/
-
 }
