bd82f18886e32c825ffe112527c6904d752e4d9c
will <will@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-01 23:22:31 +0000

    move video title sanitize logic to VideoHandlersUploader
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47474 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoHandlers/VideoHandlersUploader.class.php
+++ extensions/wikia/VideoHandlers/VideoHandlersUploader.class.php
@@ -2,6 +2,8 @@
 
 class VideoHandlersUploader {
 	
+	protected static $ILLEGAL_TITLE_CHARS = array( '/', ':', '#' );
+	
 	public static function uploadVideo($provider, $videoId, $undercover=false, &$title) {
 		$apiWrapper = F::build( ucfirst( $provider ) . 'ApiWrapper', array( $videoId ) );
 
@@ -43,19 +45,30 @@ class VideoHandlersUploader {
 		return $result;		
 	}
 
-	private static function sanitizeTitle( $titleText ) {
-		//@todo titles that end with right paren somehow interfere with
-		// file uploader. Figure out the bug so that we can use right
-		// parens again
-		$illegalChars = array( '/', '#', ':', '(', ')', '[', ']', '|', '"' );
-		//$illegalChars = array( '/' );
-		foreach ( $illegalChars as $char ) {
-			$titleText = str_replace( $char, ' ', $titleText );
+	public static function sanitizeTitle( $titleText, $replaceChar=' ' ) {
+		foreach (self::$ILLEGAL_TITLE_CHARS as $illegalChar) {
+			$titleText = str_replace( $illegalChar, $replaceChar, $titleText );
+		}
+		// remove multi _
+		$aTitle = explode( $replaceChar, $titleText );
+		$sTitle = implode ( $replaceChar, array_filter( $aTitle ) );	// array_filter() removes null elements
+		$title = Title::makeTitleSafe( NS_FILE, $sTitle );
+		
+		if ($replaceChar == ' ') {
+			return $title->getText();
 		}
 		
-		$titleText = str_replace( '  ', ' ', $titleText );
-
-		return $titleText;		
+		return $title->getDBkey();
+	}
+	
+	public static function hasForbiddenCharacters( $text ) {
+		foreach (self::$ILLEGAL_TITLE_CHARS as $illegalChar) {
+			if (strpos($text, $illegalChar) !== FALSE) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 }
