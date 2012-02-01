5ff603c5aebe5dce68cfbbfa40a601e26951f68c
kflorence <kflorence@wikia-inc.com>
2012-02-01 01:34:04 +0000

    Add edit, reply and styles to Special:MiniEditor
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47417 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MiniEditor/MiniEditor.i18n.php
+++ extensions/wikia/MiniEditor/MiniEditor.i18n.php
@@ -4,6 +4,10 @@ $messages = array();
 
 $messages['en'] = array(
 	'minieditor-hello-msg' => 'Welcome to the Mini Editor demo',
-	'minieditor-specialpage-title' => 'Special:Mini Editor',
-	'minieditor-placeholder-message' => 'Enter your message here',
+	'minieditor-specialpage-title' => 'Mini Editor Demo Page',
+
+	'minieditor-new-heading' => 'Create a New Message',
+	'minieditor-placeholder-new' => 'Enter your message here',
+	'minieditor-message-edit' => 'This is an existing message',
+	'minieditor-placeholder-reply' => 'Reply to the message here',
 );
--- extensions/wikia/MiniEditor/templates/MiniEditorSpecial_index.php
+++ extensions/wikia/MiniEditor/templates/MiniEditorSpecial_index.php
@@ -1,14 +1,44 @@
 <section class="SpecialPageWrapper">
+	<div id="MiniEditorNew" class="Instance">
+		<h3><?= wfMsg( 'minieditor-new-heading' ) ?></h3>
 
-<?= $app->getView( 'MiniEditorController', 'EditorWrapper_Begin', array(
-	'attributes' => array(
-		'data-min-height' => 200,
-		'data-max-height' => 400
-	)
-))->render(); ?>
+		<?= $app->getView( 'MiniEditorController', 'EditorWrapper_Begin', array(
+			'attributes' => array(
+				'data-min-height' => 200,
+				'data-max-height' => 400
+			)
+		))->render(); ?>
 
-	<textarea id="SpecialMiniEditor" placeholder="<?= wfMsg('minieditor-placeholder-message') ?>"></textarea>
+		<textarea id="UniqueIdTest" class="ElementToReplace" placeholder="<?= wfMsg( 'minieditor-placeholder-new' ) ?>"></textarea>
 
-<?= $app->getView( 'MiniEditorController', 'EditorWrapper_End' )->render(); ?>
+		<?= $app->getView( 'MiniEditorController', 'EditorWrapper_End' )->render(); ?>
+	</div>
 
+	<div id="MiniEditorEdit" class="Instance">
+		<?= $app->getView( 'MiniEditorController', 'EditorWrapper_Begin', array(
+			'attributes' => array(
+				'data-min-height' => 100,
+				'data-max-height' => 400
+			)
+		))->render(); ?>
+
+		<div class="ElementToReplace">
+			<p><span style="font-weight: bold;"><?= wfMsg( 'minieditor-message-edit' ) ?></span></p>
+		</div>
+
+		<?= $app->getView( 'MiniEditorController', 'EditorWrapper_End' )->render(); ?>
+	</div>
+
+	<div id="MiniEditorReply" class="Instance">
+		<?= $app->getView( 'MiniEditorController', 'EditorWrapper_Begin', array(
+			'attributes' => array(
+				'data-min-height' => 100,
+				'data-max-height' => 400
+			)
+		))->render(); ?>
+		
+		<textarea class="ElementToReplace" placeholder="<?= wfMsg( 'minieditor-placeholder-reply' ) ?>"></textarea>
+
+		<?= $app->getView( 'MiniEditorController', 'EditorWrapper_End' )->render(); ?>
+	</div>
 </section>
