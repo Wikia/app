3d291a4f26e6b4f06f356975d2eb3ee9b656d167
jacek <jacek@wikia-inc.com>
2012-02-02 12:08:48 +0000

    autoplay fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47494 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoHandlers/handlers/DailymotionVideoHandler.class.php
+++ extensions/wikia/VideoHandlers/handlers/DailymotionVideoHandler.class.php
@@ -7,8 +7,9 @@ class DailymotionVideoHandler extends VideoHandler {
 	
 	public function getEmbed( $articleId, $width, $autoplay = false, $isAjax = false ) {
 		$height =  $this->getHeight( $width );
+		$autoStartParam = $autoplay ? '1' :  '0';
 		$code = <<<EOT
-<iframe frameborder="0" width="$width" height="$height" src="http://www.dailymotion.com/embed/video/{$this->videoId}" allowfullscreen></iframe>    
+<iframe frameborder="0" width="$width" height="$height" src="http://www.dailymotion.com/embed/video/{$this->videoId}?autoPlay={$autoStartParam}" allowfullscreen></iframe>    
 EOT;
 		return $code;
 	}
