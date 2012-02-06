a173be22020d2193a2588607e6437a50347a03a1
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-06 11:33:00 +0000

    Missing files from merge (WTF SVN?)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47578 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ skins/common/modernizr/feature-detects/positionfixed.wikiamobile.js
@@ -0,0 +1,42 @@
+/**
+ * Position fixed availability for mobile platforms
+ * @see https://github.com/jquery/jquery-mobile/blob/master/js/jquery.mobile.fixedToolbar.js
+ *
+ * @author Federico "Lox" Lucignano <federico(at)wikia-inc.com>
+ */
+Modernizr.addTest('positionfixed', function(){
+	var ua = navigator.userAgent,
+		platform = navigator.platform,
+		// Rendering engine is Webkit, and capture major version
+		wkmatch = ua.match( /AppleWebKit\/([0-9]+)/ ),
+		wkversion = !!wkmatch && wkmatch[ 1 ],
+		ffmatch = ua.match( /Fennec\/([0-9]+)/ ),
+		ffversion = !!ffmatch && ffmatch[ 1 ],
+		operammobilematch = ua.match( /Opera Mobile\/([0-9]+)/ ),
+		omversion = !!operammobilematch && operammobilematch[ 1 ],
+
+		w = window;
+
+	if(
+		// iOS 4.3 and older : Platform is iPhone/Pad/Touch and Webkit version is less than 534 (ios5)
+		( ( platform.indexOf( "iPhone" ) > -1 || platform.indexOf( "iPad" ) > -1  || platform.indexOf( "iPod" ) > -1 ) && wkversion && wkversion < 534 )
+		||
+		// Opera Mini
+		( w.operamini && ({}).toString.call( w.operamini ) === "[object OperaMini]" )
+		||
+		( operammobilematch && omverson < 7458 )
+		||
+		//Android lte 2.1: Platform is Android and Webkit version is less than 533 (Android 2.2)
+		( ua.indexOf( "Android" ) > -1 && wkversion && wkversion < 533 )
+		||
+		// Firefox Mobile before 6.0 - 
+		( ffversion && ffversion < 6 )
+		||
+		// WebOS less than 3
+		( "palmGetResource" in window && wkversion && wkversion < 534 )
+	){
+		return false;
+	}
+
+	return true;
+});
