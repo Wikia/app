602d5cc623c248dfe4f1ae00fa912c1c785479a9
pbablok <pbablok@wikia-inc.com>
2012-02-07 11:06:05 +0000

    Video Postmigration undo
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47656 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/VideoHandlers/sql/video_postmigrate_undo.sql
@@ -0,0 +1,11 @@
+// table for UNDO data, needed to reverse postmigration (article namespace migration)
+
+CREATE TABLE video_postmigrate_undo (
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	wiki_id INT(11) NOT NULL,
+	entry_id VARCHAR(255) NOT NULL,
+	entry_id_field VARCHAR(255) NOT NULL,
+	entry_ns_field VARCHAR(255) NOT NULL,
+	KEY `vpu_wiki_id` (wiki_id),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB;
