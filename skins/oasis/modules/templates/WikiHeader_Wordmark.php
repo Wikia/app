db72fa5c512c9f6e5b57f991b58889b615b685fd
macbre <macbre@wikia-inc.com>
2012-02-07 13:20:10 +0000

    Signup: UAT 2.1.01 on devbox - wiki's wordmark not correctly displayed in Special:UserSignup (BugId:20251)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47661 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/WikiHeader_Wordmark.php
+++ skins/oasis/modules/templates/WikiHeader_Wordmark.php
@@ -1,4 +1,4 @@
-<h1 class="wordmark <?= $wordmarkSize ?> <?= $wordmarkType ?>">
+<h1 class="wordmark <?= $wordmarkSize ?> <?= $wordmarkType ?> <?= $wordmarkFontClass ?>">
 	<a accesskey="z" href="<?= htmlspecialchars($mainPageURL) ?>">
 		<? if (!empty($wordmarkUrl)) { ?>
 			<img src="<?= $wordmarkUrl ?>" alt="<?= htmlspecialchars($wordmarkText) ?>" width="250" height="65">
