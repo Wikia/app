fe97d58115a9afd7a6ddb6a8ea4b800f48a06adc
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-08 14:07:42 +0000

    WikiaMobile: adding some missing profiling calls
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47734 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/WikiaMobileHooks.class.php
+++ extensions/wikia/WikiaMobile/WikiaMobileHooks.class.php
@@ -8,6 +8,8 @@ class WikiaMobileHooks extends WikiaObject{
 	const IMAGE_GROUP_MIN = 2;
 
 	public function onParserAfterTidy( &$parser, &$text ){
+		$this->wf->profileIn( __METHOD__ );
+
 		//cleanup page output from unwanted stuff
 		if ( $parser->getOptions()->getSkin() instanceof SkinWikiaMobile ) {
 			//remove inline styling to avoid weird results and optimize the output size
@@ -17,19 +19,25 @@ class WikiaMobileHooks extends WikiaObject{
 			$text = preg_replace( '/(\s*<figure[^>]*>(<\/?a|<img|<\/?figcaption|[^<])+<\/figure>\s*){' . self::IMAGE_GROUP_MIN . ',}/im', '<section class="wkImgStk grp">$0<footer>' . $this->wf->Msg('wikiaPhotoGallery-slideshow-view-number', '1', '') . '</footer></section>', $text );
 		}
 
+		$this->wf->profileOut( __METHOD__ );
 		return true;
 	}
 
 	public function onParserLimitReport( $parser, &$limitReport ){
+		$this->wf->profileIn( __METHOD__ );
+
 		//strip out some unneeded content to lower the size of the output
 		if ( Wikia::isWikiaMobile() ) {
 			$limitReport = null;
 		}
 
+		$this->wf->profileOut( __METHOD__ );
 		return true;
 	}
 
 	public function onMakeHeadline( $skin, $level, $attribs, $anchor, $text, $link, $legacyAnchor, $ret ){
+		$this->wf->profileIn( __METHOD__ );
+
 		if ( $skin instanceof SkinWikiaMobile ) {
 			//remove bold, italics, underline and anchor tags from section headings (also optimizes output size)
 			$text = preg_replace( '/<\/?(b|u|i|a|em|strong){1}(\s+[^>]*)*>/im', '', $text );
@@ -46,6 +54,7 @@ class WikiaMobileHooks extends WikiaObject{
 			$ret .= "</h{$level}>";
 		}
 
+		$this->wf->profileOut( __METHOD__ );
 		return true;
 	}
 
@@ -59,6 +68,8 @@ class WikiaMobileHooks extends WikiaObject{
 	}
 
 	public function onCategoryPageView( CategoryPage &$categoryPage ) {
+		$this->wf->profileIn( __METHOD__ );
+
 		if ( Wikia::isWikiaMobile() ) {
 			//TODO: remove before release
 			if ( !$this->wg->DevelEnvironment ) {
@@ -71,13 +82,17 @@ class WikiaMobileHooks extends WikiaObject{
 			$this->wg->Out->addHTML( $this->app->renderView( 'WikiaMobileCategoryService', 'categoryExhibition', array( 'categoryPage' => $categoryPage ) ) );
 			$this->wg->Out->addHTML( $this->app->renderView( 'WikiaMobileCategoryService', 'alphabeticalList', array( 'categoryPage' => $categoryPage ) ) );
 
+			$this->wf->profileOut( __METHOD__ );
 			return false;
 		}
 
+		$this->wf->profileOut( __METHOD__ );
 		return true;
 	}
 
 	public function onArticlePurge( Article &$article ) {
+		$this->wf->profileIn( __METHOD__ );
+
 		$title = $article->getTitle();
 
 		if ( $title->getNamespace() == NS_CATEGORY ) {
@@ -85,6 +100,7 @@ class WikiaMobileHooks extends WikiaObject{
 			F::build( 'WikiaMobileCategoryModel' )->purgeItemsCollectionCache( $category->getName() );
 		}
 
+		$this->wf->profileOut( __METHOD__ );
 		return true;
 	}
 }
