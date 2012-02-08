755601438e5bfa4c47b8f3016700b32316d5802b
pbablok <pbablok@wikia-inc.com>
2012-02-08 11:23:15 +0000

    Video Scripts moved
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47716 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/VideoHandlers/videoTest.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Usage $0 WIKI_ID"
+
+SERVER_ID=$1 php videoPremigrate.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php
+SERVER_ID=$1 php videoMigrateData.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php
+SERVER_ID=$1 php videoPostmigrate.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php
