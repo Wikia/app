6a14190c9e8fa4c251ed577bcae42121cfe3fca2
owen <owen@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-08 18:36:51 +0000

    trunk: bugid:17179 logo scaling broken
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47749 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/WikiHeader_Wordmark.php
+++ skins/oasis/modules/templates/WikiHeader_Wordmark.php
@@ -1,7 +1,7 @@
 <h1 class="wordmark <?= $wordmarkSize ?> <?= $wordmarkType ?> <?= $wordmarkFontClass ?>">
 	<a accesskey="z" href="<?= htmlspecialchars($mainPageURL) ?>">
 		<? if (!empty($wordmarkUrl)) { ?>
-			<img src="<?= $wordmarkUrl ?>" alt="<?= htmlspecialchars($wordmarkText) ?>" width="250" height="65">
+			<img src="<?= $wordmarkUrl ?>" alt="<?= htmlspecialchars($wordmarkText) ?>">
 		<? } else { ?>
 			<?= htmlspecialchars($wordmarkText) ?>
 		<? } ?>
