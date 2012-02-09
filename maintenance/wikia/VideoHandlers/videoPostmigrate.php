983b74c3beafa5373ef3473e12ad8aff629ef20c
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-09 17:06:20 +0000

    Video
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47801 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/videoPostmigrate.php
+++ maintenance/wikia/VideoHandlers/videoPostmigrate.php
@@ -8,7 +8,7 @@ $options = array('help');
 @require_once( '../../commandLine.inc' );
 global $IP, $wgCityId, $wgExternalDatawareDB;
 #$IP = '/home/pbablok/video/VideoRefactor/'; // HACK TO RUN ON SANDBOX
-echo( "$IP\n" );
+#echo( "$IP\n" );
 echo( "Postmigration script running for $wgCityId\n" );
 
 if( isset( $options['help'] ) && $options['help'] ) {
@@ -24,10 +24,10 @@ $dbw_dataware = wfGetDB( DB_MASTER, array(), $wgExternalDatawareDB );
 $tables = array(
 	'archive' => array('ns'=>'ar_namespace','id'=>'ar_page_id'),
 	'cu_changes' => array('ns'=>'cuc_namespace','id'=>'cuc_id'),
-	//'hidden' => array('ns'=>'hidden_namespace','id'=>'hidden_page'),
+	'hidden' => array('ns'=>'hidden_namespace','id'=>'hidden_page'),
 	'logging' => array('ns'=>'log_namespace','id'=>'log_id'),
 	'page' => array('ns'=>'page_namespace','id'=>'page_id'),
-	//'pagelinks' => array('ns'=>'pl_namespace','id'=>'pl_from'),
+	'pagelinks' => array('ns'=>'pl_namespace','id'=>'pl_from'),
 	'protected_titles' => array('ns'=>'pt_namespace','id'=>'pt_title'),
 	'recentchanges' => array('ns'=>'rc_namespace','id'=>'rc_id'),
 );
@@ -36,38 +36,64 @@ $tables = array(
 
 foreach( $tables as $tableName => $tableData ) {
 	echo( "Processing $tableName\n" );
+	
+	if( !$dbw->tableExists($tableName) ) {
+		echo "Table does not exist in this database\n";
+		continue;
+	}
+	
+	# get all affected rows (those that are in namespace 400)
 
 	$rows = $dbw->select($tableName, 
 			array( $tableData['id'] ),
 			array( $tableData['ns'] => 400 ),
 			__METHOD__
 	);
-		
+	
+	$rows_preloaded = array();
 	while( $row = $dbw->fetchObject($rows) ) {
-		$idField = $tableData['id'];
-		$id = $row->$idField;
-		echo "  row $idField => $id\n";
-		$dbw->update($tableName,
-			array( $tableData['ns'] => 6 ),
-			array( $tableData['id'] => $id ),
-			__METHOD__
-		);
-
-		$dbw_dataware->insert(
-			'video_postmigrate_undo',
-			array(
-				'wiki_id'		=> $wgCityId,
-				'entry_id'		=> $id,
-				'entry_id_field'=> $tableData['id'],
-				'entry_ns_field'=> $tableData['ns'],
-				'entry_table'	=> $tableName,
-			)
-		);
-			
-		
+		$rows_preloaded[] = $row;
 	}
 
 	$dbw->freeResult($rows);
+	
+		
+	foreach( $rows_preloaded as $row ) {
+		$idField = $tableData['id'];
+		$id = $row->$idField;
+		echo "  row $idField => $id\n";
+		
+		try {
+			# update namespace for specific row
+		
+			$dbw->update($tableName,
+				array( $tableData['ns'] => 6 ),
+				array( $tableData['id'] => $id ),
+				__METHOD__
+			);
+			
+			# log it so that undo operation is possible in the future
+	
+			$dbw_dataware->insert(
+				'video_postmigrate_undo',
+				array(
+					'wiki_id'		=> $wgCityId,
+					'entry_id'		=> $id,
+					'entry_id_field'=> $tableData['id'],
+					'entry_ns_field'=> $tableData['ns'],
+					'entry_table'	=> $tableName,
+				)
+			);
+				
+		} catch(DBError $e) {
+			# this is most likely due to duplication errors
+			# as long as this happens in pagelinks it should be safe to remove them instead
+			# TODO - remove pagelinks if they exist in NS_FILE namespace
+		
+			echo "  failed\n";
+		}		
+	}
+
 }
 
 echo "Done updating tables\n";
