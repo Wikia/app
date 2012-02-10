eb1fc50b15ff88065abd6b994fa1ffddff4a3b17
agas <agas@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-10 17:42:09 +0000

    toolbar selenium test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47871 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/ToolbarTest.java
+++ tests/acceptance/com/wikia/selenium/tests/ToolbarTest.java
@@ -8,18 +8,84 @@ import org.testng.annotations.Test;
 
 public class ToolbarTest extends BaseTest {
 
-	@Test(groups={"CI", "verified"})
+	@Test(groups={"verified"})
 	public void testEnsuresThatToolbarIsNotPresentForAnonymousUsers() throws Exception {
 		openAndWait("/");
 		assertTrue(session().isElementPresent("WikiaFooter"));
-		assertFalse(session().isElementPresent("//div[contains(@class, 'toolbar')]"));
+		assertFalse(session().isElementPresent("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]"));
 	}
 
-	@Test(groups={"CI", "verified"})
-	public void testEnsuresThatToolbarIsPresentForSignedInUsers() throws Exception {
-		login();
+	@Test(groups={"verified"})
+	public void testResetsDefaultsInCustomizedToolbar() throws Exception {
+		
 		openAndWait("/");
-		assertTrue(session().isElementPresent("WikiaFooter"));
-		assertTrue(session().isElementPresent("//div[contains(@class, 'toolbar')]"));
+		login();
+		session().click("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='customize']");
+		waitForElement("//section[@id='MyToolsConfigurationWrapper']");
+		session().click("//section[@id='MyToolsConfigurationWrapper']//a[@href='#']");
+		assertFalse(session().isElementPresent("//section[contains(@class, 'modalContent')]//ul[contains(@class, 'options-list ui-sortable')]//li[@data-caption='Edit']"));
+		session().click("//section[@id='MyToolsConfigurationWrapper']//div[contains(@class, 'buttons')]/input[@type='submit']");
+		logout();
+		
+		loginAsRegular();
+		session().click("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='customize']");
+		waitForElement("//section[@id='MyToolsConfigurationWrapper']");
+		session().click("//section[@id='MyToolsConfigurationWrapper']//a[@href='#']");
+		assertFalse(session().isElementPresent("//section[contains(@class, 'modalContent')]//ul[contains(@class, 'options-list ui-sortable')]//li[@data-caption='Edit']"));
+		session().click("//section[@id='MyToolsConfigurationWrapper']//div[contains(@class, 'buttons')]/input[@type='submit']");
+		logout();
+		
 	}
+	
+	
+	@Test(groups={"verified"},dependsOnMethods={"testResetsDefaultsInCustomizedToolbar"},alwaysRun=false)
+	public void testEnsuresThatSignedInUserCanAddAnItemToCustomizedToolbar() throws Exception {
+		
+		
+		openAndWait("/");	
+		login();
+		assertTrue(session().isElementPresent("WikiaFooter"));
+		assertTrue(session().isElementPresent("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]"));
+		session().click("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='customize']");
+		waitForElement("//section[@id='MyToolsConfigurationWrapper']");
+		session().click("//section[@id='MyToolsConfigurationWrapper']//div[contains(@class, 'popular-toggle toggle-1')]");
+		waitForElementVisible("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'popular-list')]");
+		session().click("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'popular-list')]//a[@data-tool-id='PageAction:Edit']");
+		waitForElement("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'options-list ui-sortable')]//li[@data-tool-id='PageAction:Edit']");
+		assertTrue(session().isVisible("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'options-list ui-sortable')]//li[@data-tool-id='PageAction:Edit']"));
+		session().click("//section[@id='MyToolsConfigurationWrapper']//div[contains(@class, 'buttons')]/input[@type='submit']");
+		waitForElement("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='edit']");
+		assertTrue(session().isVisible("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='edit']"));
+		logout();
+		
+		loginAsRegular();
+		assertFalse(session().isElementPresent("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='edit']"));
+		logout();
+		
+		login();
+		assertTrue(session().isElementPresent("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='edit']"));
+		logout();
+		
+		
+		openAndWait("/");
+		loginAsRegular();
+		openAndWait("wiki/Special:Upload");
+		session().click("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='customize']");
+		waitForElement("//section[@id='MyToolsConfigurationWrapper']");
+		session().click("//section[@id='MyToolsConfigurationWrapper']//div[contains(@class, 'popular-toggle toggle-1')]");
+		waitForElementVisible("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'popular-list')]");
+		session().click("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'popular-list')]//a[@data-tool-id='PageAction:Edit']");
+		waitForElement("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'options-list ui-sortable')]//li[@data-tool-id='PageAction:Edit']");
+		assertTrue(session().isVisible("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'options-list ui-sortable')]//li[@data-tool-id='PageAction:Edit']"));
+		session().click("//section[@id='MyToolsConfigurationWrapper']//div[contains(@class, 'buttons')]/input[@type='submit']");
+		waitForElementNotPresent("//section[@id='MyToolsConfigurationWrapper']");
+		waitForElement("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]");
+		assertFalse(session().isElementPresent("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='edit']"));
+		
+		editArticle("toolbartest", "testujemy toolbar");
+		waitForElement("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]");
+		assertTrue(session().isVisible("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='edit']"));
+		logout();
+	}
+		
 }
