252c50385d5180de51eaadc8c21a7dc2bdfc4df7
moli <moli@wikia-inc.com>
2012-02-14 13:42:04 +0000

    add city_founding_ip
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@47973 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/sql/wikifactory-schema.sql
+++ maintenance/wikia/sql/wikifactory-schema.sql
@@ -72,11 +72,13 @@ CREATE TABLE `city_list` (
   `city_flags` int(10) unsigned NOT NULL default '0',
   `city_cluster` varchar(255) default NULL,
   `city_last_timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `city_founding_ip` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`city_id`),
   KEY `city_dbname_idx` (`city_dbname`),
   KEY `titleidx` (`city_title`),
   KEY `urlidx` (`city_url`),
-  KEY `city_flags` (`city_flags`)
+  KEY `city_flags` (`city_flags`),
+  KEY `city_founding_ip` (`city_founding_ip`)
 ) ENGINE=InnoDB AUTO_INCREMENT=88756 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 SET @saved_cs_client     = @@character_set_client;
