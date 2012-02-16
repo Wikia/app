67991c99dfa756c697a6baa0b39bd4a46c5ad25a
owen <owen@wikia-inc.com>
2012-02-16 05:52:40 +0000

    trunk: final version with debugging and anon safety
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48103 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/ExternalUser_Wikia.php
+++ includes/wikia/ExternalUser_Wikia.php
@@ -68,12 +68,12 @@ class ExternalUser_Wikia extends ExternalUser {
 
 		// Copy safety check from User.php
 		$uid = intval( isset( $_COOKIE["{$wgCookiePrefix}UserID"] ) ? $_COOKIE["{$wgCookiePrefix}UserID"] : 0 );
-		if( isset( $_SESSION['wsUserID'] ) && $uid != $_SESSION['wsUserID'] ) {
+		if( $uid != 0 && isset( $_SESSION['wsUserID'] ) && $uid != $_SESSION['wsUserID'] ) {
 			$wgRequest->response()->setcookie( "UserID", '', time() - 86400 );
 			$wgRequest->response()->setcookie( "UserName", '', time() - 86400 );
 			$wgRequest->response()->setcookie( "_session", '', time() - 86400 );
 			$wgRequest->response()->setcookie( "Token", '', time() - 86400 );
-			wfDebugLog( 'initFromCookie', "Session user ID ({$_SESSION['wsUserID']}) and cookie user ID ($uid) don't match!" );
+			trigger_error("###INEZ### {$_SESSION['wsUserID']}\n", E_USER_WARNING);
 			return false;
 		}
 		
