08e4d8d41a958c108a91b3d966539f742ba0d1e2
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-21 12:59:03 +0000

    Video: fix testing twilight wiki getting old data
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48227 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/testTwilight.sh
+++ maintenance/wikia/VideoHandlers/testTwilight.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd /tmp
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -f twilightsaga
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -c C -f twilightsaga
 php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i twilightsaga.sql.gz
 rm twilightsaga.sql.gz
 
