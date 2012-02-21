261728c24f26bb3567a7ab146221680d0221bcf9
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-21 14:54:38 +0000

    Video: small tweaks to migration process
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48244 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/doAll.sh
+++ maintenance/wikia/VideoHandlers/doAll.sh
@@ -4,6 +4,8 @@ then
 	echo "Usage ./$0 WIKI_ID"
 fi
 
+sudo -u www-data SERVER_ID=$1 php videoReset.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php || exit
+echo ""
 sudo -u www-data SERVER_ID=$1 php videoSanitize.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php || exit
 echo ""
 sudo -u www-data SERVER_ID=$1 php videoPremigrate.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php || exit
--- maintenance/wikia/VideoHandlers/testVideo.sh
+++ maintenance/wikia/VideoHandlers/testVideo.sh
@@ -3,9 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd /tmp
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -f video
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i video.sql.gz
-rm video.sql.gz
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -c D -f video151
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i video151.sql.gz
+rm video151.sql.gz
 
 cd $DIR
 sh doAll.sh 298117 | tee video.log.txt
