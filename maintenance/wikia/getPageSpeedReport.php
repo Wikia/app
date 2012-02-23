fb8a8babeb8cb00b51d7780193f6ae42b69a2aec
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-23 16:09:21 +0000

    Create maintenance script to get PageSpeed report using CLI (BugId:21991)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48380 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/getPageSpeedReport.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @addto maintenance
+ * @author Maciej Brencz (Macbre) <macbre at wikia-inc.com>
+ */
+
+ini_set( "include_path", dirname(__FILE__)."/.." );
+require_once( 'commandLine.inc' );
+
+function printHelp() {
+		echo <<<HELP
+Returns Google PageSpeed report
+
+USAGE: php codelint.php --url=http://foo.bar --score-only
+
+	--url
+		Page to be checked
+
+HELP;
+}
+
+// show help and quit
+if (!isset($options['url'])) {
+	printHelp();
+	die(1);
+}
+
+$service = new PageSpeedAPI();
+$url = $options['url'];
+
+// use GooglePage speed API
+$report = $service->getReportForURL($url);
+
+if (empty($report)) {
+	echo "API request failed!\n";
+	die(1);
+}
+
+// print the report
+$reportUrl = $report['url'];
+
+echo <<<REPORT
+---------------------------------------------------------------------
+PageSpeed report for <$reportUrl>:
+---------------------------------------------------------------------
+
+REPORT;
+
+echo "\nPageSpeed score is: \033[32m{$report['score']}\033[0m\n";
+
+// print key/value details
+echo "\nDetails:\n--------\n";
+
+foreach($report['stats'] as $key => $value) {
+	echo '* ' . sprintf('%-30s: %s', $key, number_format($value)) . "\n";
+}
+
+die(0);
