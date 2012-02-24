63230d74b2f92c420a35a05a7e52a439b14f80a5
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-24 14:33:40 +0000

    WikiaMobile: further work no sharing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48431 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/templates/WikiaMobileSharingService_index.php
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileSharingService_index.php
@@ -32,6 +32,6 @@
 </style>
 <ul class=wkLst>
 <? foreach ( $networks as $n ) :?>
-	<li class=<?= $n->getId() ;?>Shr><a href="<?= $n->getUrl( '$1', ($n->getId() == 'email'? '$3' : '$2' ) )/*param substitution happens in JS*/ ;?>" target=_blank></a></li>
+	<li class=<?= $n->getId() ;?>Shr><a href="<?= ($n->getId() == 'email' ? $n->getUrl( '__1__&subject=__4__', '__3__' ) : $n->getUrl('__1__', '__2__'));/*param substitution happens in JS*/ ;?>" target=_blank>&nbsp;</a></li>
 <? endforeach ;?>
 </ul>
