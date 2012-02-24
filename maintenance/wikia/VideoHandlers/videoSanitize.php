5802bbf9be01dc3faee438d70179fba0745f5f6a
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-24 13:07:28 +0000

    Video: if sanitizer renames video it should make sure that also if they are included in RelatedVideos they are renamed there as well
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48420 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/videoSanitize.php
+++ maintenance/wikia/VideoHandlers/videoSanitize.php
@@ -31,13 +31,46 @@ function title_replacer( $title, $replacement, $fulltext  ) {
 			}
 		}
 	}
-	
-	//$regexp = '/(\\[\\[Video\\:)' . $regexp . '(\\]\\]|\\|[^]]+\\]\\])/';
+
 	$regexp = '/(\\[\\[Video\\:)' . $regexp . '(( *)?#.*?)?'.'(\\]\\]|\\|[^]]+\\]\\])/';
 	
 	$new = preg_replace( $regexp, '$1' . $replacement . '$4', $fulltext );
 	if($new === null) return $fulltext;
 	return $new;	
+}
+
+function title_replacer_rv( $title, $replacement, $fulltext  ) {
+	$symbols = array(
+		array(' ','_','-','+','/'),
+	);
+	$refs = array();
+	foreach( $symbols as $id => $val ) {
+		foreach( $val as $id2 => $symbol ) {
+			$imp = implode('\\',$val);
+			$refs[$symbol] = '[\\' . $imp .']';
+		}
+	}
+
+	$regexp = '';
+
+	$j = mb_strlen($title);
+	for ($k = 0; $k < $j; $k++) {
+		$char = mb_substr($title, $k, 1);
+		if(isset($refs[$char])) {
+			$regexp .= $refs[$char];
+		} else {
+			if(ctype_alnum($char)) {
+				$regexp .= $char;
+			} else {
+				$regexp .= '\\' . $char;
+			}
+		}
+	}
+
+	$regexp = '/(\\*\\ (VM\\:|))' . $regexp . '(\\|)/';
+	$new = preg_replace( $regexp, '$1' . $replacement . '$3', $fulltext );
+	if($new === null) return $fulltext;
+	return $new;
 }
 
 
@@ -211,63 +244,7 @@ foreach ( $aTranslation as $key => $val ) {
 		'where' => array( 'wl_namespace' => NS_LEGACY_VIDEO, 'wl_title' => $strippedOld ),
 		'update' => array( 'wl_title' => $strippedNew )
 	);
-	
-	// Fixing links in article;
-	//echo "SELECT distinct il_from FROM imagelinks WHERE il_to ='{$key}'! /n";
-	$rows = $dbw->query( "SELECT distinct il_from FROM imagelinks WHERE il_to ='".mysql_real_escape_string($key)."'");
-	while( $file = $dbw->fetchObject( $rows ) ) {
-		// var_dump( $key );
-//		echo "ONE! /n";
-		echo "FETCH FROM DB il_from= ".$file->il_from." // ($key)\n";
-		$articleId = $file->il_from;
-		$oTitle = Title::newFromId( $articleId );
-		if ( $oTitle instanceof Title && $oTitle->exists() ){
-			global $wgTitle;
-			// in some cases hooks depend on wgTitle (hook for article edit)
-			// but normally it wouldn't be set for maintenance script
-			$wgTitle = $oTitle;
-//			echo "TWO! /n";
-			$oArticle = new Article ( $oTitle );
-			if ( $oArticle instanceof Article ){
-//				echo "THREE! /n";
-				$sTextAfter = $sText = $oArticle->getContent();
-//				var_dump( $sTextAfter, $key, $val );
-//				var_dump( $oTitle );
-				echo "\n ========== ART:" .$oTitle." =============\n";
 
-				$sTextAfter = title_replacer( substr( $key, 1 ), substr( $val, 1), $sTextAfter  );
-
-				if ( $sTextAfter != $sText ) {
-					echo "ARTICLE WAS CHANGED! \n";
-/*					echo "BEFORE:\n";
-					echo $sText;
-					echo "\n\n\n AFTER:\n";
-					echo $sTextAfter;
-					echo "\n\n----\n\n";*/
-					$status = $oArticle->doEdit( $sTextAfter, 'Fixing broken video names', EDIT_UPDATE, false, $botUser );
-//					var_dump( $status );
-//					if ( is_object( $status ) && !$status->isOK() ){
-//						var_dump( $status );
-//					}
-					//$sanitizeHelper->logVideoTitle($key, $val, 'OK', $oTitle);
-					
-				} else {
-					if (strpos($sText, $key)!==false) {
-						echo "FAIL! \n";
-						echo "ARTICLE:\n";
-						echo $sText;					
-						$sanitizeHelper->logVideoTitle($key, $val, 'FAIL', $oTitle);
-					} else {
-						$sanitizeHelper->logVideoTitle($key, $val, 'UNKNOWN', $oTitle);
-					}
-				}
-			} else {
-				var_dump( $oArticle );
-			}
-		} else {
-			var_dump( $oTitle );
-		}
-	}
 
 	foreach(  $aTablesMove as $table => $actions ){
 		if( $dbw->tableExists($table)) {
@@ -287,6 +264,40 @@ foreach ( $aTranslation as $key => $val ) {
 		}
 	}
 
+	// Fixing links in article;
+	//echo "SELECT distinct il_from FROM imagelinks WHERE il_to ='{$key}'! /n";
+	$rows = $dbw->query( "SELECT distinct il_from FROM imagelinks WHERE il_to ='".mysql_real_escape_string($key)."'");
+	while( $file = $dbw->fetchObject( $rows ) ) {
+		echo "FETCH FROM DB il_from= ".$file->il_from." // ($key)\n";
+		$articleId = $file->il_from;
+		$oTitle = Title::newFromId( $articleId );
+		if ( $oTitle instanceof Title && $oTitle->exists() ){
+			global $wgTitle;
+			// in some cases hooks depend on wgTitle (hook for article edit)
+			// but normally it wouldn't be set for maintenance script
+			$wgTitle = $oTitle;
+			$oArticle = new Article ( $oTitle );
+			if ( $oArticle instanceof Article ){
+				$sTextAfter = $sText = $oArticle->getContent();
+				echo "\n ========== ART:" .$oTitle." =============\n";
+
+				$sTextAfter = title_replacer( substr( $key, 1 ), substr( $val, 1), $sTextAfter  );
+
+				if ( $sTextAfter != $sText ) {
+					echo "ARTICLE WAS CHANGED! \n";
+					$status = $oArticle->doEdit( $sTextAfter, 'Fixing broken video names', EDIT_UPDATE, false, $botUser );
+				} else {
+					$sanitizeHelper->logVideoTitle($key, $val, 'UNKNOWN', $oTitle);
+				}
+			} else {
+				var_dump( $oArticle );
+			}
+		} else {
+			var_dump( $oTitle );
+		}
+	}
+
+
 	// when you sanitize make sure that the original entry in video_premigrate is deleted
 	$dbw_dataware->delete(
 		'video_premigrate',
@@ -298,6 +309,38 @@ foreach ( $aTranslation as $key => $val ) {
 }
 
 echo(": {$rowCount} videos processed.\n");
+
+
+echo "Fixing Related Videos\n";
+$rows = $dbw->query( "SELECT page_id FROM page WHERE page_namespace = 1100" );
+
+while( $page = $dbw->fetchObject( $rows ) ) {
+	global $wgTitle;
+	$oTitleRV = Title::newFromID( $page->page_id );
+	$wgTitle = $oTitleRV;
+	if ( $oTitleRV instanceof Title && $oTitleRV->exists() ){
+		$oArticle = new Article ( $oTitleRV );
+		if ( $oArticle instanceof Article ){
+			$sTextAfter = $sText = $oArticle->getContent();
+			echo "\n ========== ART(RV):" .$oTitle." =============\n";
+
+			foreach ( $aTranslation as $key => $val ) {
+				$sTextAfter = title_replacer_rv( substr( $key, 1 ), substr( $val, 1), $sTextAfter  );
+			}
+
+			if ( $sTextAfter != $sText ) {
+				echo "ARTICLE WAS CHANGED! \n";
+				$status = $oArticle->doEdit( $sTextAfter, 'Fixing broken video names', EDIT_UPDATE, false, $botUser );
+			}
+		} else {
+			var_dump( $oArticle );
+		}
+	}
+
+}
+
+
+
 /*
  * wywala nam sie upload
  * check if new name will be available
