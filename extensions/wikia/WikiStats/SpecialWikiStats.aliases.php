0e8e2e85d30701fccf50e912e5c33e7c321ae166
michal <michal@wikia-inc.com>
2012-02-28 15:40:50 +0000

    BugId:21354 i18n - WikiStats SpecialPage aliases.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48568 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiStats/SpecialWikiStats.aliases.php
@@ -0,0 +1,14 @@
+<?php
+$aliases = array();
+/**
+ * English
+ */
+$aliases['en'] = array(
+    'WikiStats' => array( 'WikiStats' ),
+);
+/**
+ * German
+ */
+$aliases['de'] = array(
+    'WikiStats' => array( 'WikiStatistik', 'WikiStats' ),
+);
