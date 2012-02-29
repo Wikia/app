86fd3a10972c9ddff048b09131172300815b166a
michal <michal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-29 15:35:41 +0000

    BugId:21354 ExtWikiaWikiFeatures i18n.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48637 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFeatures/WikiFeatures.alias.php
+++ extensions/wikia/WikiFeatures/WikiFeatures.alias.php
@@ -13,7 +13,12 @@ $aliases['en'] = array(
 	'WikiFeatures' => array( 'WikiFeatures' ),
 );
 
-/** German(Deutsch) */
+/** German (Deutsch) */
 $aliases['de'] = array(
 	'WikiFeatures' => array( 'WikiFunktionen' ),
+);
+
+/** Polish (Polski) */
+$aliases['pl'] = array(
+	'WikiFeatures' => array( 'Rozszerzenia wiki', 'WikiFeatures' ),
 );
