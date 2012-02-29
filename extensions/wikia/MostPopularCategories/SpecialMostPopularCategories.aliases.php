77d9fab52a4d4c9f0f523da6dbe3089d9ecee10b
michal <michal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-29 13:42:08 +0000

    BugId:21354 ExtWikiaMostPopularCategories i18n.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48620 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MostPopularCategories/SpecialMostPopularCategories.aliases.php
+++ extensions/wikia/MostPopularCategories/SpecialMostPopularCategories.aliases.php
@@ -5,7 +5,11 @@ $aliases = array();
 $aliases['en'] = array(
     'Mostpopularcategories' => array( 'Mostpopularcategories' ),
 );
-
+/** German */
 $aliases['de'] = array(
     'Mostpopularcategories' => array( 'BeliebtesteKategorien', 'Mostpopularcategories' ),
+);
+/** Polish */
+$aliases['pl'] = array(
+	'Mostpopularcategories' => array( 'Najpopularniejsze kategorie', 'Mostpopularcategories' ),
 );
