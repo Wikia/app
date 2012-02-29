575b22812f838833c8cf6fe385480245750e60d5
michal <michal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-02-29 15:41:08 +0000

    BugId:21354 ExtApiExplorer i18n.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48640 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/ApiExplorer/ApiExplorer.alias.php
+++ extensions/ApiExplorer/ApiExplorer.alias.php
@@ -13,8 +13,12 @@ $specialPageAliases['en'] = array(
 	'ApiExplorer' => array( 'ApiExplorer' ),
 );
 
-
-
+/**
+ * Polish (Polski)
+ */
+$specialPageAliases['pl'] = array(
+	'ApiExplorer' => array( 'Eksplorator API', 'ApiExplorer' )
+);
 
 /**
  * For backwards compatibility with MediaWiki 1.15 and earlier.
