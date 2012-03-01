f192f0040be9ba4e8793d87a8db0e5eea1affa79
michal <michal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-01 12:25:12 +0000

    BugId:21354 ExtWikiaMultiTasks i18n.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48728 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/MultiTasks/MultiTasks.aliases.php
@@ -0,0 +1,14 @@
+<?php
+$aliases = array();
+/**
+ * English
+ */
+$aliases['en'] = array(
+	'Multidelete' => array( 'Multidelete' )
+);
+/**
+ * Polish (Polski)
+ */
+$aliases['pl'] = array(
+	'Multidelete' => array( 'Usuwanie w wielu wiki', 'Multidelete' )
+);
