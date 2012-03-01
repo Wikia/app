411853e81df3af458307b3bb73aa5630106805c7
michal <michal@wikia-inc.com>
2012-03-01 12:21:12 +0000

    BugId:21354 ExtWikiaSpecialMultiLookup i18n.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48727 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialMultipleLookup/SpecialMultipleLookup.aliases.php
@@ -0,0 +1,14 @@
+<?php
+$aliases = array();
+/**
+ * English
+ */
+$aliases['en'] = array(
+	'MultiLookup' => array( 'MultiLookup' )
+);
+/**
+ * Polish (Polski)
+ */
+$aliases['pl'] = array(
+	'MultiLookup' => array( 'Wyszukiwanie użytkownika w wielu wiki', 'MultiLookup' )
+);
