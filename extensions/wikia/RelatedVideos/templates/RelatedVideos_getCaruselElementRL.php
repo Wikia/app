6152ec5c274f4def5d908f7c2b63795f1ebdfabf
pbablok <pbablok@wikia-inc.com>
2012-03-01 11:56:11 +0000

    RelatedVideos: bugfix for bugId:20877
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48723 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedVideos/templates/RelatedVideos_getCaruselElementRL.php
+++ extensions/wikia/RelatedVideos/templates/RelatedVideos_getCaruselElementRL.php
@@ -16,7 +16,7 @@
 		<img data-src="<?=$video['thumbnailData']['thumb'];?>" src="<?=( $preloaded ) ? $video['thumbnailData']['thumb'] : wfBlankImgUrl();?>" style="margin-top:<?= floor( ( $elementHeight - $video['thumbnailData']['height'] ) / 2 ); ?>px; height:<?=$video['thumbnailData']['height'];?>px; width:<?=$video['thumbnailData']['width'];?>px;" />
 	</a>
 	<div class="description">
-		<a class="<?= $videoPlay; ?>" href="<?=$video['fullUrl'];?>" data-ref="<?=$video['prefixedUrl'];?>" data-video-name="<?=$video['title'];?>" >
+		<a class="<?= $videoPlay; ?>" href="<?=$video['fullUrl'];?>" data-ref="<?=$video['prefixedUrl'];?>" data-external="<?=$video['external'];?>" data-video-name="<?=$video['title'];?>" >
 		<?=( strlen( $video['title'] ) > $maxDescriptionLength )
 			? substr( $video['title'], 0, $maxDescriptionLength).'&#8230;'
 			: $video['title'];
