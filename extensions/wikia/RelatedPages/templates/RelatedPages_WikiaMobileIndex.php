ab7fd138b3e2f785933ea2b4fa2dd744492268bd
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-02 12:14:04 +0000

    WikiaMobile: avoid CSS reflow in related pages section (BugzId:23123)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48782 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedPages/templates/RelatedPages_WikiaMobileIndex.php
+++ extensions/wikia/RelatedPages/templates/RelatedPages_WikiaMobileIndex.php
@@ -4,7 +4,7 @@
 		<ul class="open wkLst">
 		<? foreach ( $pages as $page ) :?>
 			<? $noImage = empty( $page['imgUrl'] ) ;?>
-			<li><a href="<?= $page['url'] ;?>"<?= ($noImage) ? 'class="noImg"' : '' ;?>><? if( !$noImage ) :?><img src="<?= $page['imgUrl']; ?>"/><? endif ;?><?= $page['title'] ?></a></li>
+			<li><a href="<?= $page['url'] ;?>"<?= ($noImage) ? 'class="noImg"' : '' ;?>><? if( !$noImage ) :?><img src="<?= $page['imgUrl']; ?>" width=100 height=50/><? endif ;?><?= $page['title'] ?></a></li>
 		<? endforeach ;?>
 		</ul>
 	</section>
