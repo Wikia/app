1f0626140649f1f1beb4b1a7c6f0ae114359793e
nandy <nandy@wikia-inc.com>
2012-03-05 18:21:52 +0000

    bugId:23451; change in memc keys for refactored videos and old videos
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@48910 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedVideos/RelatedVideosService.class.php
+++ extensions/wikia/RelatedVideos/RelatedVideosService.class.php
@@ -109,15 +109,16 @@ class RelatedVideosService {
 	}
 
 	public function getMemcKey( $title, $source, $videoWidth, $cityShort ) {
-
+		$isVideoFile = WikiaVideoService::isVideoStoredAsFile() ? 'VR' : 'OV'; //VR - video refactored / OV - old video
+		
 		if( empty( $source ) ){
 			$video = Title::newFromText( $title, NS_VIDEO );
 			if ($video instanceof Title && $video->exists() ) {
-				return F::app()->wf->memcKey( $video->getArticleID(), F::app()->wg->wikiaVideoRepoDBName, $videoWidth, $cityShort, self::memcKeyPrefix, self::memcVersion, self::$width );
+				return F::app()->wf->memcKey( $video->getArticleID(), F::app()->wg->wikiaVideoRepoDBName, $videoWidth, $cityShort, self::memcKeyPrefix, self::memcVersion, self::$width, $isVideoFile );
 			}
 			return '';
 		} else {
-			return F::app()->wf->sharedMemcKey( md5( $title ), F::app()->wg->wikiaVideoRepoDBName, $videoWidth, $cityShort, self::memcKeyPrefix, self::memcVersion, self::$width );
+			return F::app()->wf->sharedMemcKey( md5( $title ), F::app()->wg->wikiaVideoRepoDBName, $videoWidth, $cityShort, self::memcKeyPrefix, self::memcVersion, self::$width, $isVideoFile );
 		}
 	}
 
