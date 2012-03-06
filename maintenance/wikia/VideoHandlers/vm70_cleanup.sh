d5e74f5cbbe26e8aa46fd407bb748324c5194a4d
pbablok <pbablok@wikia-inc.com>
2012-03-06 14:12:04 +0000

    VideoRefactoring: migration plan written as scripts
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49006 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/vm70_cleanup.sh
+++ maintenance/wikia/VideoHandlers/vm70_cleanup.sh
@@ -1,21 +1,20 @@
 #/bin/bash
 TMPFILE=`mktemp /tmp/$0.XXXXXX` || exit 1
 echo "Getting list of wikis..."
-echo "select 
+echo "select
 c.city_id, count(*), c.city_dbname
 from events, wikicities.city_list as c
 where c.city_id = wiki_id and page_ns = 400 and event_type = 2
-group by wiki_id order by count(*) desc
-limit 5;" | slave stats > $TMPFILE
+group by wiki_id order by count(*) desc;" | slave stats > $TMPFILE
 cat $TMPFILE
 cat $TMPFILE | while read line; do
 	cityid=`echo "$line" | cut -f 1`
 	if [ "$cityid" = "city_id" ];
 	then
 		continue
-	fi 
+	fi
 	echo "Processing $line"
-	SERVER_ID=$cityid php videoPremigrate.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php
+	SERVER_ID=$cityid php videoPurgeOld.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php | tee -a logs/${cityid}.purgeold.log
 	echo "\n\n"
 done
 rm -f $TMPFILE
