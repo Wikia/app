292c753eb7a0a6cdfd810609165581b633f711e5
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-07 18:44:42 +0000

    WikiaMobile: partial commit of FB login button (BugzId:23601,23602)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49135 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserLogin/FacebookButtonController.class.php
+++ extensions/wikia/UserLogin/FacebookButtonController.class.php
@@ -11,5 +11,9 @@ class FacebookButtonController extends WikiaController {
 		$this->class = $this->request->getVal('class', '');
 		$this->text = $this->request->getVal('text', '');
 		$this->tooltip = $this->request->getVal('tooltip');
+
+		if ( Wikia::isWikiaMobile() ) {
+			$this->overrideTemplate( 'WikiaMobileIndex' );
+		}
 	}
 }
--- extensions/wikia/UserLogin/templates/UserLoginSpecial_WikiaMobileIndex.php
+++ extensions/wikia/UserLogin/templates/UserLoginSpecial_WikiaMobileIndex.php
@@ -1,4 +1,5 @@
-<section id=wkLgn>
+<div id=wkLgn>
+	<?= $app->renderView('UserLoginSpecial', 'Providers') ;?>
 	<form method=post>
 		<input type=hidden name=loginToken value='<?= $loginToken ?>'>
 		<input type=text name=username placeholder='<?= $wf->Msg('yourname')?>'>
@@ -6,4 +7,4 @@
 		<a href=recovery.php id=wkPwdRcv><?= $wf->Msg('userlogin-forgot-password') ?></a>
 		<input type=submit value='<?= $wf->Msg('login') ?>' class='wkBtn main'>
 	</form>
-</section>
+</div>
