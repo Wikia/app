163cf9f67e004045b6ba8651f69d01457d26b851
jolek <jolek@wikia-inc.com>
2012-03-07 16:16:14 +0000

    WikiaMobile: template for regular login
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49117 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/UserLogin/templates/UserLoginSpecial_WikiaMobileIndex.php
@@ -0,0 +1,9 @@
+<section id=wkLgn>
+	<form method=post>
+		<input type=hidden name=loginToken value='<?= $loginToken ?>'>
+		<input type=text name=username placeholder='<?= $wf->Msg('yourname')?>'>
+		<input type=password name=password placeholder='<?= $wf->Msg('yourpassword') ?>'>
+		<a href=recovery.php id=wkPwdRcv><?= $wf->Msg('userlogin-forgot-password') ?></a>
+		<input type=submit value='<?= $wf->Msg('login') ?>' class='wkBtn main'>
+	</form>
+</section>
