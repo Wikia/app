ea8977d2fb404b112d241f888114d38754e3cb23
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-08 22:23:40 +0000

    WidgetTag: cleanup for BugId:23830
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49256 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WidgetTag/WidgetTag.php
+++ extensions/wikia/WidgetTag/WidgetTag.php
@@ -8,28 +8,20 @@
  * LocalSettings.php file:
  * require_once("$IP/extensions/wikia/WidgetTag/WidgetTag.php");
  */
-
-
 $wgExtensionCredits['parserhook'][] = array(
   'name'	=> 'WidgetTag',
   'url'		=> 'http://help.wikia.com/wiki/Help:WidgetTag',
   'author'	=> 'Maciej Brencz',
   'description'	=> 'Adds &lt;widget&gt; tag for dynamic embedding of Wikia widgets',
-  'version'	=> 0.1
+  'version'	=> 0.2
 );
 
-//Avoid unstubbing $wgParser on setHook() too early on modern (1.12+) MW versions, as per r35980
-if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
-	$wgHooks['ParserFirstCallInit'][] = 'efWidgetTagSetup';
-}
-else {
-	$wgExtensionFunctions[] = 'efWidgetTagSetup';
-}
+$wgHooks['ParserFirstCallInit'][] = 'efWidgetTagSetup';
 
-$wgAutoloadClasses['WidgetTagRenderer'] = dirname(__FILE__) . '/WidgetTag_class.php';
+$wgAutoloadClasses['WidgetTagRenderer'] = dirname(__FILE__) . '/WidgetTagRenderer.class.php';
 
 // setup parser hook
-function efWidgetTagSetup(&$parser) {
+function efWidgetTagSetup(Parser $parser) {
 	global $wgHooks;
 	$parser->setHook( 'widget', 'efWidgetTagRender' );
 	$wgHooks['ParserAfterTidy'][] = 'efWidgetTagReplaceMarkers';
@@ -37,12 +29,12 @@ function efWidgetTagSetup(&$parser) {
 }
 
 function efWidgetTagRender( $input, $args, $parser ) {
-	$widgetTagRenderer = & WidgetTagRenderer::getInstance();
+	$widgetTagRenderer = WidgetTagRenderer::getInstance();
 	return $widgetTagRenderer->renderTag( $input, $args, $parser );
 }
 
 function efWidgetTagReplaceMarkers(&$parser, &$text) {
-	$widgetTagRenderer = & WidgetTagRenderer::getInstance();
+	$widgetTagRenderer = WidgetTagRenderer::getInstance();
 	$text = $widgetTagRenderer->replaceMarkers($text);
 	return true;
 }
