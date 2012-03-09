d81a1cfdd5df4a6202fec1eaddfd696a9ce54e65
owen <owen@wikia-inc.com>
2012-03-09 03:16:03 +0000

    trunk: weird unit test thing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49291 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ScavengerHunt/tests/ScavengerHuntTest.php
+++ extensions/wikia/ScavengerHunt/tests/ScavengerHuntTest.php
@@ -4,9 +4,11 @@ wfLoadAllExtensions();
 
 class ScavengerHuntTest extends WikiaBaseTest {
 
+/*
 	public function tearDown() {
 		F::unsetInstance('GlobalTitle');
 	}
+*/
 
 	public function getFakeSprite() {
 		return array( 'X' => 50, 'Y' => 50, 'X1' => 0, 'Y1' => 0, 'X2' => 10, 'Y2' => 20 );
