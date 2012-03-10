14fbc54f22937916869e311e59a2556c2269870e
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-10 02:12:26 +0000

    ImageReview: more work on UI
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49370 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageReview/templates/ImageReviewSpecial_index.php
+++ extensions/wikia/ImageReview/templates/ImageReviewSpecial_index.php
@@ -1,40 +1,69 @@
-<form action="<?= $submitUrl ?>/submit" method="post">
-	<input type="submit" value="Submit images" />
+<style>
+	#ImageReviewForm table {
+		margin: 20px 0;
+	}
 
-	<table width=="100%">
+	#ImageReviewForm img {
+		display: block;
+		max-height: 250px;
+		max-width: 240px;
+	}
+
+	#ImageReviewForm input:checked {
+		border: solid 3px #FF2020ff;
+	}
+
+	#ImageReviewForm a {
+		float: right;
+	}
+</style>
+
+<form action="<?= $submitUrl ?>/submit" method="post" id="ImageReviewForm">
+	<table width=="980" cellspacing="5">
 		<colspan>
-			<col width="25%">
-			<col width="25%">
-			<col width="25%">
-			<col width="25%">
+			<col width="245">
+			<col width="245">
+			<col width="245">
+			<col width="245">
 		</colspan>
+
+<?php
+	for ($n=0; $n<5; $n++) {
+?>
 		<tr>
 			<td>
 				<label>
-					<input type="checkbox" value="12345">
 					<img src="http://images4.wikia.nocookie.net/__spotlights/images/0654911ff943af82aa614c6a44ded605.png">
+					<input type="checkbox" value="1">
+					<small><a href="#" target="_blank">link</a></small>
 				</label>
 			</td>
 			<td>
 				<label>
-					<input type="checkbox" value="12345">
 					<img src="http://images4.wikia.nocookie.net/__spotlights/images/0654911ff943af82aa614c6a44ded605.png">
+					<input type="checkbox" value="2">
+					<small><a href="#" target="_blank">link</a></small>
 				</label>
 			</td>
 			<td>
 				<label>
-					<input type="checkbox" value="12345">
 					<img src="http://images4.wikia.nocookie.net/__spotlights/images/0654911ff943af82aa614c6a44ded605.png">
+					<input type="checkbox" value="3">
+					<small><a href="#" target="_blank">link</a></small>
 				</label>
 			</td>
 			<td>
 				<label>
-					<input type="checkbox" value="12345">
 					<img src="http://images4.wikia.nocookie.net/__spotlights/images/0654911ff943af82aa614c6a44ded605.png">
+					<input type="checkbox" value="4">
+					<small><a href="#" target="_blank">link</a></small>
 				</label>
 			</td>
 		</tr>
+<?php
+	}
+?>
 	</table>
 
-	<input type="submit" value="Submit images" />
+	<input type="submit" value="Review and get next batch &raquo;" />
 </form>
