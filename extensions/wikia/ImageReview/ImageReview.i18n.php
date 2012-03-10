1ea5af6c0461764d923d832db1c60f68a7cc3034
tor <tor@wikia-inc.com>
2012-03-10 04:52:39 +0000

    added link to terms of use
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49395 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageReview/ImageReview.i18n.php
+++ extensions/wikia/ImageReview/ImageReview.i18n.php
@@ -3,9 +3,9 @@
 $messages = array();
 
 $messages['en'] = array(
-	'imagereview-reason' => "Violation of Wikia's Terms of Use",
+	'imagereview-reason' => "Violation of Wikia's [[wikia:Terms of Use|Terms of Use]]",
 );
 
 $messages['pl'] = array(
-	'imagereview-reason' => "Naruszenie Regulaminu serwisu Wikia",
+	'imagereview-reason' => "Naruszenie [[wikia:Terms of Use|Regulaminu]] serwisu Wikia",
 );
