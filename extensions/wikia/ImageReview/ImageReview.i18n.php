dc1ec014c89ed6bbec24a1e8d9588b469b7015e4
tor <tor@wikia-inc.com>
2012-03-10 04:18:09 +0000

    added PL translation for ImageReview
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49389 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageReview/ImageReview.i18n.php
+++ extensions/wikia/ImageReview/ImageReview.i18n.php
@@ -5,3 +5,7 @@ $messages = array();
 $messages['en'] = array(
 	'imagereview-reason' => "Violation of Wikia's Terms of Use",
 );
+
+$messages['pl'] = array(
+	'imagereview-reason' => "Naruszenie Regulaminu serwisu Wikia",
+);
