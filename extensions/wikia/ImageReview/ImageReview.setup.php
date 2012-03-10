e9e882ac6933a4cba13dd69f4faa862ef36127c6
macbre <macbre@wikia-inc.com>
2012-03-10 01:07:36 +0000

    ImageReview: render Special:ImageReview
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49365 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageReview/ImageReview.setup.php
+++ extensions/wikia/ImageReview/ImageReview.setup.php
@@ -1,2 +1,12 @@
 <?php
 
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'Image Review',
+);
+
+$dir = dirname(__FILE__) . '/';
+$app = F::app();
+
+//classes
+$app->registerClass('ImageReviewSpecialController', $dir . 'ImageReviewSpecialController.class.php');
+$app->registerSpecialPage('ImageReview', 'ImageReviewSpecialController');
