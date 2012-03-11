2b2469b86d2c65a5538956c1300959e58e6fb90c
mech <mech@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-11 04:02:07 +0000

    ImageReview: small fix for posting empty set
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49467 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageReview/ImageReviewSpecialController.class.php
+++ extensions/wikia/ImageReview/ImageReviewSpecialController.class.php
@@ -56,8 +56,9 @@ class ImageReviewSpecialController extends WikiaSpecialPageController {
 						}
 					}
 				}
-
-				$helper->updateImageState( $images );
+				if ( count($images) > 0 ) {
+					$helper->updateImageState( $images );
+				}
 				$ts = null;
 			}
 		}
