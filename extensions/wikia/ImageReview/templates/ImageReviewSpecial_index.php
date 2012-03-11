9e5903ac7339a300b64bca691f695aed6772c504
mech <mech@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-11 04:09:17 +0000

    ImageReview: change from r49466 disappeared somehow during merge
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49470 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageReview/templates/ImageReviewSpecial_index.php
+++ extensions/wikia/ImageReview/templates/ImageReviewSpecial_index.php
@@ -49,7 +49,7 @@ if (is_array($imageList) && count($imageList) > 0):
 				</div>
 				<a href="<?= htmlspecialchars($image['url']) ?>" target="_blank" class="internal sprite details magnify" title="Go to image page"></a>
 
-				<label title="Mark as OK"><input type="radio" name="<?= $id ?>" value="<?= ImageReviewHelper::STATE_APPROVED ?>"<?= ($stateId == ImageReviewHelper::STATE_APPROVED || $stateId == ImageReviewHelper::STATE_IN_REVIEW || $stateId == ImageReviewHelper::STATE_QUESTIONABLE_IN_REVIEW || $stateId == ImageReviewHelper::STATE_UNREVIEWED ? ' checked' :'') ?>>OK</label>
+				<label title="Mark as OK"><input type="radio" name="<?= $id ?>" value="<?= ImageReviewHelper::STATE_APPROVED ?>"<?= ($stateId == ImageReviewHelper::STATE_APPROVED || $stateId == ImageReviewHelper::STATE_IN_REVIEW || $stateId == ImageReviewHelper::STATE_UNREVIEWED ? ' checked' :'') ?>>OK</label>
 				<label title="Delete"><input type="radio" name="<?= $id ?>" value="<?= ImageReviewHelper::STATE_DELETED ?>"<?= ($stateId == ImageReviewHelper::STATE_DELETED ? ' checked' :'') ?>>Del</label>
 				<label title="Questionable"><input type="radio" name="<?= $id ?>" value="<?= ImageReviewHelper::STATE_QUESTIONABLE ?>"<?= ($stateId == ImageReviewHelper::STATE_QUESTIONABLE || $stateId == ImageReviewHelper::STATE_QUESTIONABLE_IN_REVIEW ? ' checked' :'') ?>>Q</label>
 			</td>
