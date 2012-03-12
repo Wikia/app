4747d3f71478253cd67bf057d910c58dc02c8060
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-12 17:21:00 +0000

    VideoRefactoring: correct cluster for icarly
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49522 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/ST_icarly.sh
+++ maintenance/wikia/VideoHandlers/ST_icarly.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd /tmp
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -f icarly
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -c B -f icarly
 php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i icarly.sql.gz
 rm icarly.sql.gz
 
