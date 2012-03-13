ac8ff50fe90fd70512ca06841c71527c6f386b5f
wladek <wladek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-13 17:41:47 +0000

    blocked all calls to WikiaTracker and GA and put raw GA API call to track pageviews (BugId: 20284)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49609 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AnalyticsEngine/AnalyticsProviderGA_Urchin.php
+++ extensions/wikia/AnalyticsEngine/AnalyticsProviderGA_Urchin.php
@@ -13,14 +13,18 @@ class AnalyticsProviderGA_Urchin implements iAnalyticsProvider {
 
 		$script = '';
 
+		$setDomainName = '';
+		/*
 		if(strpos($_SERVER['SCRIPT_URI'], '.wikia.com/') !== false) {
 			$setDomainName = '_gaq.push([\'_setDomainName\', \'.wikia.com\']);';
 		} else {
 			$setDomainName = '';
 		}
+		*/
 
 		// TODO: use asynchronous approach (BugId:20216)
 		// @see http://code.google.com/intl/pl/apis/analytics/docs/tracking/asyncTracking.html
+/*
 		$script .= <<<SCRIPT2
 <script type="text/javascript" src="{$wgProto}://www.google-analytics.com/ga.js"></script>
 <script type="text/javascript">
@@ -31,7 +35,22 @@ urchinTracker = function() {
 };
 </script>
 SCRIPT2;
+*/
+		$script .= <<<SCRIPT2
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30014103-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+SCRIPT2;
 		// Load the OneDot javascript after GA
 		if (class_exists('Track')) {
 			$script .= Track::getViewJS();
@@ -47,7 +66,8 @@ SCRIPT2;
 				break;
 
 			case AnalyticsEngine::EVENT_PAGEVIEW:
-				return '<script type="text/javascript">_wtq.push([\'AnalyticsEngine::EVENT_PAGEVIEW\', \'Wikia.main\']);</script>';
+				return '';
+// 				return '<script type="text/javascript">_wtq.push([\'AnalyticsEngine::EVENT_PAGEVIEW\', \'Wikia.main\']);</script>';
 				#return '<script type="text/javascript">_gaq.push([\'_setAccount\',\'UA-288915-1\']);_gaq.push([\'_setSampleRate\',10]);_gaq.push([\'_setDomainName\',\'.wikia.com\']);_gaq.push([\'_trackPageview\']);</script>'; // BugId:21127
 
 			// oasis is not calling this?!?
@@ -56,7 +76,8 @@ SCRIPT2;
 					return '<!-- Missing category name  for hub tracking event -->';
 				}
 				$hub = "/" . str_replace(' ', '_', $eventDetails['name']);
-				return '<script type="text/javascript">_wtq.push([\'' . addslashes($hub) . '\', \'Wikia.hub\']);</script>';
+				return '';
+// 				return '<script type="text/javascript">_wtq.push([\'' . addslashes($hub) . '\', \'Wikia.hub\']);</script>';
 
 			case 'onewiki':
 				return $this->onewiki($eventDetails[0]);
@@ -78,12 +99,15 @@ SCRIPT2;
 		if (empty($wgGoogleAnalyticsAccount)){
 			return '<!-- No tracking for this wiki -->';
 		} else {
-			return '<script type="text/javascript">_wtq.push([\'AnalyticsEngine::EVENT_PAGEVIEW\', \'' . addslashes($wgGoogleAnalyticsAccount) . '\']);</script>';
+			return '';
+// 			return '<script type="text/javascript">_wtq.push([\'AnalyticsEngine::EVENT_PAGEVIEW\', \'' . addslashes($wgGoogleAnalyticsAccount) . '\']);</script>';
 		}
 	}
 
 	/* Record how long the page took to load  */
 	private function pagetime($skin){
+		return '';
+/*
 		return '<script type="text/javascript">
 if (typeof window.wgNow == "object"){
 	var now = new Date();
@@ -93,15 +117,19 @@ if (typeof window.wgNow == "object"){
 	_wtq.push([slashtime, \'Wikia.pagetime\']);
 }
 </script>';
+*/
 	}
 
 	private function varnishstat() {
+		return '';
+/*
 		return '<script type="text/javascript">
 var varnish_server;
 if (varnish_server = document.cookie.match(/varnish-stat=([^;]+)/)) {
 	_wtq.push([varnish_server[1], \'Wikia.varnish\']);
 }
 </script>';
+*/
 		}
 
 	private function lyrics() {
@@ -121,10 +149,10 @@ if (varnish_server = document.cookie.match(/varnish-stat=([^;]+)/)) {
 		$ns = $wgTitle->getNamespace();
 
 		$out = '';
-		$out .= "<script type=\"text/javascript\">_wtq.push(['/GN2/".$ns."', 'lyrics']);</script>\n";
+// 		$out .= "<script type=\"text/javascript\">_wtq.push(['/GN2/".$ns."', 'lyrics']);</script>\n";
 
 		if (in_array($ns, array(0, 220))) {
-			$out .= "<script type=\"text/javascript\">_wtq.push(['/GN4/".$ns."/".$wgTitle->getArticleID()."', 'lyrics']);</script>\n";
+// 			$out .= "<script type=\"text/javascript\">_wtq.push(['/GN4/".$ns."/".$wgTitle->getArticleID()."', 'lyrics']);</script>\n";
 		}
 
 		return $out;
