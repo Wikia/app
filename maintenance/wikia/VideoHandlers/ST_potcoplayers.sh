5f74599f46091d8d23972f672a234700baccf6a3
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-14 14:15:05 +0000

    VideoRefactoring: proper cluster for potcoplayers
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49693 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/ST_potcoplayers.sh
+++ maintenance/wikia/VideoHandlers/ST_potcoplayers.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd /tmp
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -f potcoplayers
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -c B -f potcoplayers
 php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i potcoplayers.sql.gz
 rm potcoplayers.sql.gz
 
