92ed021603218380364241be5964338e1e17bd51
pbablok <pbablok@wikia-inc.com>
2012-03-14 15:46:33 +0000

    VideoRefactoring: bugfix for poznan wiki
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49714 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/ST_poznan.sh
+++ maintenance/wikia/VideoHandlers/ST_poznan.sh
@@ -3,9 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd /tmp
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -c B -f poznan
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i poznan.sql.gz
-rm poznan.sql.gz
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -c A -f plpoznan
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i plpoznan.sql.gz
+rm plpoznan.sql.gz
 
 cd $DIR
 sudo -u www-data SERVER_ID=5915 php videoReset.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php | tee ST_poznan.log || exit
