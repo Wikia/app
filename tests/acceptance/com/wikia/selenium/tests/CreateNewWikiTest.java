cf3dc264348b7458ef55dc84d52426db61469024
patrick <patrick@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-15 15:03:06 +0000

    Removed verified group from CreateNewWikiTest.createWikiDefaultLanguageForAnonymousIsWikiAndItCanBeChanged
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49815 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/CreateNewWikiTest.java
+++ tests/acceptance/com/wikia/selenium/tests/CreateNewWikiTest.java
@@ -290,7 +290,7 @@ public class CreateNewWikiTest extends BaseTest {
 		
 	//Test Case 003
 	//logged out in german wiki,change language to english in CNW flow,log in as QATestsStaff,verify domain is english
-	@Test(groups={"envProduction", "verified"})
+	@Test(groups={"envProduction"})
 	//fixed by Rodrigo Molinero 12-Mar-12
 	public void createWikiDefaultLanguageForAnonymousIsWikiAndItCanBeChanged() throws Exception {
 		openAndWait("http://de.fallout.wikia.com/wiki/Fallout_Wiki");
