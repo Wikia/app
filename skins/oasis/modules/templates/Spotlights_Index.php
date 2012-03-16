29802a2898b51220fab9c33d535e5888f66178d8
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-16 06:44:44 +0000

    HTML validation: script cannot be a descendant of ul BugzId:24532
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@49892 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/templates/Spotlights_Index.php
+++ skins/oasis/modules/templates/Spotlights_Index.php
@@ -4,8 +4,8 @@
 		<h1><?= wfMsg($titleMsg) ?></h1>
 		<?= wfRenderModule('RandomWiki') ?>
 	</div>
+	<?= AdEngine::getInstance()->getLazyLoadableAdGroup($adGroupName, $adslots) ?>
 	<ul<? if (!empty($adGroupName)) { ?> id="<?= $adGroupName ?>"<? if ($useLazyLoadAdClass) { ?> class="<?= AdEngine::lazyLoadAdClass ?><?= $wgEnableSpotlightsV2_Footer ? ' SPOTLIGHT_FOOTER' : '' ?>"<? } } ?>>
-		<?= AdEngine::getInstance()->getLazyLoadableAdGroup($adGroupName, $adslots) ?>
 		<? for ($i=0; $i<$n_adslots; $i++) { ?>
 		<li class="WikiaSpotlight item-<?= $i+1 ?>" id="<?= ( isset($adslots[$i]) ) ? $adslots[$i] : 'SPOTLIGHT_FOOTER_FORCED_'.$i; ?>">
 			<?
