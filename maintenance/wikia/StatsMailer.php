e0357b3fb0300dc9012fe4fb322f5abdacc441c2
timq <timq@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-19 23:36:26 +0000

    Updating to include all staff blogs
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50019 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/StatsMailer.php
+++ maintenance/wikia/StatsMailer.php
@@ -1,15 +1,64 @@
 <?php
 require_once("commandLine.inc");
 
-$dbr = wfGetDB(DB_MASTER);
+$dbr = wfGetDB(DB_SLAVE);
 $res = $dbr->query("select count(*) as count from watchlist where wl_title = \"Wikia_Staff_Blog\" and wl_namespace = 502");
+ while ($row = $dbr->fetchObject($res)) {
+	$row->count;
+	$output = $row->count;
+}
+	$to = new MailAddress('comteam-l@wikia-inc.com');
+	$from = new MailAddress('timq@wikia-inc.com');
+	$subject = 'Staff Blog Numbers!';
+	$body = "This is a monthly automated report that simply counts the number of followers on the Wikia Staff Blogs on Community Central\n\nBlog:Wikia Staff Blog followers: " . $output;
+	
+$res = $dbr->query("select count(*) as count from watchlist where wl_title = \"Wikia_Technical_Updates\" and wl_namespace = 502");
+ while ($row = $dbr->fetchObject($res)) {
+    	$row->count;
+	$output = $row->count;
+}
+
+$body = $body . "\nBlog:Wikia Technical Updates  followers: " . $output;
+
+$res = $dbr->query("select count(*) as count from watchlist where wl_title = \"Wikia_Community_News\" and wl_namespace = 502");
+ while ($row = $dbr->fetchObject($res)) {
+        $row->count;
+        $output = $row->count;
+}
+
+$body = $body . "\nBlog:Wikia Community News followers: " . $output;
+
+$res = $dbr->query("select count(*) as count from watchlist where wl_title = \"Wikia_Contests_&_Giveaways\" and wl_namespace = 502");
+ while ($row = $dbr->fetchObject($res)) {
+        $row->count;
+        $output = $row->count;
+}
+
+$body = $body . "\nBlog:Wikia Contests & Giveaways followers: " . $output;
+
+
+$res = $dbr->query("select count(*) as count from watchlist where wl_title = \"Wikia_New_Features\" and wl_namespace = 502");
+ while ($row = $dbr->fetchObject($res)) {
+        $row->count;
+        $output = $row->count;
+}
+
+$body = $body . "\nBlog:Wikia New Features followers: " . $output;
+
+
+$res = $dbr->query("select count(*) as count from watchlist where wl_title = \"Wikia_Tips_&_Tricks\" and wl_namespace = 502");
 while ($row = $dbr->fetchObject($res)) {
     $row->count;
-echo $row->count;
 $output = $row->count;
 }
-$to = new MailAddress('timq@wikia-inc.com');
-$from = new MailAddress('timq@wikia-inc.com');
-$subject = 'Hi there!';
-$body = "This is the number of Staff Blog followers: " . $output;
+$body = $body. "\nBlog:Wikia Tips & Tricks followers: " . $output;
+
+$res = $dbr->query("select count(*) as count from watchlist where wl_title = \"Wikia_Founders_&_Admins\" and wl_namespace = 502");
+while ($row = $dbr->fetchObject($res)) {
+    $row->count;
+$output = $row->count;
+}
+$body = $body.  "\n\nBlog:Wikia Founders & Admins followers: " . $output;
+
 UserMailer::send($to, $from, $subject, $body);
+
