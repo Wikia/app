1a8a91bbc178bd29df0a001c2de2c1423780f21c
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-20 20:34:45 +0000

    Re-deleted AdSkin extension which accidentally re-appeared due to a bad-merge.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50090 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdSkin/js/iphone_games.js
+++
@@ -1,6 +0,0 @@
-$("body").click(function(e) {
-	var gutter_width = ($("body").width() - 962) / 2;
-	if (e.pageX < gutter_width && e.pageY > 51 || e.pageX > gutter_width + 962 && e.pageY > 51) {
-		document.location = "http://click.linksynergy.com/fs-bin/stat?id=NcD/XW8Zk6E&offerid=146261&type=3&subid=2253962&tmpid=1826&u1=US_WEB_iPhoneGamesWiki_July09&RD_PARM1=http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewArtist?id=282764297&mt=8&partnerId=30";
-	}
-});
--- extensions/wikia/AdSkin/js/warhammer.js
+++
@@ -1,3 +0,0 @@
-jQuery('#background_strip').click(function() {
-	document.location = 'http://www.warhammeronline.com/trial?WHK33J-U7C8Q-HWPEJ-9QAIJ-4Y8JK';
-});
