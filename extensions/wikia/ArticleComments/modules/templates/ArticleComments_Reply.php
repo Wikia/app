75a4beae3de09cfe68680bbd968a1b647b86ab81
kflorence <kflorence@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-20 21:28:05 +0000

    MiniEditor: fix button handling bugs on ArticleComments (BugId:25151). Also fix edge cases handling for RTE disabled (BugId:24375).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50101 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ArticleComments/modules/templates/ArticleComments_Reply.php
+++ extensions/wikia/ArticleComments/modules/templates/ArticleComments_Reply.php
@@ -19,7 +19,7 @@
 					echo $app->getView( 'MiniEditorController', 'Editor_Footer' )->render(); 
 				endif; ?>
 				<div class="buttons" data-space-type="buttons">
-					<input type="submit" name="wpArticleSubmit" id="article-comm-reply-submit-<?= $commentId ?>" value="<? echo wfMsg('article-comments-post') ?>" />
+					<input type="submit" class="actionButton" name="wpArticleSubmit" id="article-comm-reply-submit-<?= $commentId ?>" value="<? echo wfMsg('article-comments-post') ?>" />
 					<img src="<?= $stylePath ?>/common/images/ajax.gif" class="throbber" />
 				</div>
 			</div>
