78d995d135fc525e18f355fd6fd6daf3a8384db5
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-20 17:13:42 +0000

    VideoRefactoring: fixing scripts
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50069 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/ST_muppets.sh
+++ maintenance/wikia/VideoHandlers/ST_muppets.sh
@@ -3,9 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd /tmp
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -c B -f muppets
-php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i muppets.sql.gz
-rm muppets.sql.gz
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -c A -f muppet
+php /usr/wikia/source/wiki/maintenance/wikia/getDatabase.php -i muppet.sql.gz
+rm muppet.sql.gz
 
 cd $DIR
 sudo -u www-data SERVER_ID=831 php videoReset.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php | tee ST_muppets.log || exit
