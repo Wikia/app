f16d8ee62fc42fa58c1490a7a895383d6c72eeba
macbre <macbre@wikia-inc.com>
2012-03-21 15:30:35 +0000

    Implement AssetsManagerController::getMultiTypePackage (BugId:25107)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50168 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/AssetsManagerController.class.php
+++ extensions/wikia/AssetsManager/AssetsManagerController.class.php
@@ -10,7 +10,21 @@ class AssetsManagerController extends WikiaController {
 
 		// TODO: handle CSS/SASS files
 
-		// TODO: handle JS files / assets manager packages
+		// handle assets manager packages
+		$scripts = $this->request->getVal('scripts');
+		if (!is_null($scripts)) {
+			$scriptPackages = explode(',', $scripts);
+			$scriptsOutput = array();
+
+			foreach($scriptPackages as $package) {
+				$builder = $this->getBuilder('group', $package);
+				if (!is_null($builder)) {
+					 $scriptsOutput[] = $builder->getContent();
+				}
+			}
+
+			$this->response->setVal('scripts', $scriptsOutput);
+		}
 
 		// handle JSmessages
 		$messages = $this->request->getVal('messages');
@@ -26,5 +40,28 @@ class AssetsManagerController extends WikiaController {
 		}
 
 		$this->response->setFormat('json');
+	}
+
+	/**
+	 * Returns instance of AssetsManager builder handling given type of assets
+	 *
+	 * @param string $type assets type ('one', 'group', 'groups', 'sass')
+	 * @param string $oid assets / group name
+	 * @return AssetsManagerBaseBuilder instance of builder
+	 */
+	private function getBuilder($type, $oid) {
+		$type = ucfirst($type);
+		$className = "AssetsManager{$type}Builder";
+
+		if (class_exists($className)) {
+			$request = new WebRequest();
+			$request->setVal('oid', $oid);
+
+			$builder = F::build($className, array($request));
+			return $builder;
+		}
+		else {
+			return null;
+		}
 	}
 }
