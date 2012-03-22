66c81bf3fc27c6430c1c0e5c42f3aa402c891a9a
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-22 13:11:23 +0000

    VideoRefactoring: bugfix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50260 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/videoSanitize.php
+++ maintenance/wikia/VideoHandlers/videoSanitize.php
@@ -108,6 +108,7 @@ if( isset( $options['help'] ) && $options['help'] ) {
 	exit( 0 );
 }
 
+$IP = '/home/release/video_refactoring/trunk'; // HACK TO RUN ON SANDBOX
 require_once( "$IP/extensions/wikia/VideoHandlers/VideoHandlers.setup.php" );
 
 $dbw = wfGetDB( DB_MASTER );
