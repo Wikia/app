89087bf30612614cc7eb9a9d04d6d8f1e2a05ea1
will <will@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-26 17:54:28 +0000

    update video wiki sanitizer
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50422 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/vm15_sanitizeVideoWiki.sh
+++ maintenance/wikia/VideoHandlers/vm15_sanitizeVideoWiki.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -u www-data SERVER_ID=298117 php videoSanitizeVideoWiki.php --conf /usr/wikia/docroot/wiki.factory/LocalSettings.php | tee -a logs/298117.sanitize.log
+/usr/wikia/backend/bin/withcity --maintenance-script='../../../../../home/release/video_refactoring/trunk/maintenance/wikia/VideoHandlers/videoSanitizeVideoWiki.php' --usedb=video151 | tee -a logs/298117.sanitize.log
