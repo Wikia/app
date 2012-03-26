90a8163f66337a32a8e7cef08ee58e9c2d510690
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-26 11:30:25 +0000

    change to DB_SLAVE
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50378 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageReview/ImageReviewHelper.class.php
+++ extensions/wikia/ImageReview/ImageReviewHelper.class.php
@@ -11,7 +11,7 @@ class ImageReviewHelper extends WikiaModel {
 	 * we fetch a few icons from DB, we can skip them
 	 */
 	const LIMIT_IMAGES_FROM_DB = 24;
-	
+
 	const STATE_UNREVIEWED = 0;
 	const STATE_IN_REVIEW = 1;
 	const STATE_APPROVED = 2;
@@ -19,18 +19,18 @@ class ImageReviewHelper extends WikiaModel {
 	const STATE_DELETED = 4;
 	const STATE_QUESTIONABLE = 5;
 	const STATE_QUESTIONABLE_IN_REVIEW = 6;
-	
+
 	const STATE_INVALID_IMAGE = 98;
 	const STATE_ICO_IMAGE = 99;
-	
+
 	const FLAG_SUSPICOUS_USER = 2;
 	const FLAG_SUSPICOUS_WIKI = 4;
 	const FLAG_SKIN_DETECTED = 8;
 
 	static $sortOptions = array(
-                'latest first' => 0, 
-                'by priority and recency' => 1, 
-                'oldest first' => 2, 
+                'latest first' => 0,
+                'by priority and recency' => 1,
+                'oldest first' => 2,
 	);
 
 	const ORDER_LATEST = 0;
@@ -96,7 +96,7 @@ class ImageReviewHelper extends WikiaModel {
 			$statsInsert,
 			__METHOD__
 		);
-		
+
 		// update stats directly in memcache so they look nice without impacting the database
 		$key = wfMemcKey( 'ImageReviewSpecialController', 'ImageReviewHelper::getImageCount', $this->wg->user->getId() );
 		$stats = $this->wg->memc->get($key, null);
@@ -117,7 +117,7 @@ class ImageReviewHelper extends WikiaModel {
 			// Quick hack/fix for stats going negative -- FIXME
 			if ($stats['unreviewed'] < 0)
 				$this->wg->memc->delete( $key );
-		}		
+		}
 
 		if ( !empty( $deletionList ) ) {
 			$task = new ImageReviewTask();
@@ -128,7 +128,7 @@ class ImageReviewHelper extends WikiaModel {
 				TASK_QUEUED
 			);
 		}
-		
+
 		$this->wf->ProfileOut( __METHOD__ );
 	}
 
@@ -181,11 +181,11 @@ class ImageReviewHelper extends WikiaModel {
 	* Note: NOT update image state
 	* @param integer review_end
 	* @return array images
-	*/	
+	*/
 	public function refetchImageListByTimestamp( $timestamp ) {
 		$this->wf->ProfileIn( __METHOD__ );
 
-		$db = $this->wf->GetDB( DB_MASTER, array(), $this->wg->ExternalDatawareDB );
+		$db = $this->wf->GetDB( DB_SLAVE, array(), $this->wg->ExternalDatawareDB );
 
 		// try to re-fetch the previuos set of images
 		// TODO: optimize it, so we don't do it on every request
@@ -225,7 +225,7 @@ class ImageReviewHelper extends WikiaModel {
 
 		return $imageList;
 	}
-	
+
 	/**
 	 * get image list
 	 * @return array imageList
@@ -263,20 +263,20 @@ class ImageReviewHelper extends WikiaModel {
 		}
 
 		$values[] = 'state = '.$newState;
-		
+
 		$where[] = 'image_review.wiki_id=pages.page_wikia_id';
 		$where[] =  'image_review.page_id=pages.page_id';
-		
+
 		$result = $db->select(
 			array( 'image_review', 'pages' ),
 			array( 'image_review.wiki_id, image_review.page_id, image_review.state, image_review.flags, image_review.priority, pages.page_title_lower',  ),
 			$where,
 			__METHOD__,
 			array(
-				'ORDER BY' => $this->getOrder($order), 
+				'ORDER BY' => $this->getOrder($order),
 				'LIMIT' => self::LIMIT_IMAGES_FROM_DB )
 		);
-		
+
 		$rows = array();
 		$updateWhere = array();
 		$iconsWhere = array();
@@ -290,27 +290,27 @@ class ImageReviewHelper extends WikiaModel {
 		}
 
 		$db->freeResult( $result );
-		
+
 		if ( count($updateWhere) > 0) {
 			$db->update(
-				'image_review', 
+				'image_review',
 				$values,
 				array(implode(' OR ', $updateWhere)),
-				__METHOD__				
+				__METHOD__
 			);
 		}
-		
-		
+
+
 		if ( count($iconsWhere) > 0 ) {
 			$db->update(
-				'image_review', 
+				'image_review',
 				array( 'state' => self::STATE_ICO_IMAGE),
 				array(implode(' OR ', $iconsWhere)),
 				__METHOD__
 			);
 		}
 		$db->commit();
-		
+
 		$invalidImages = array();
 		$unusedImages = array();
 		foreach( $rows as $row) {
@@ -334,11 +334,11 @@ class ImageReviewHelper extends WikiaModel {
 				$unusedImages[] = "(wiki_id = {$row->wiki_id} and page_id = {$row->page_id})";
 			}
 		}
-		
+
 		$commit = false;
 		if ( count($invalidImages) > 0 ) {
 			$db->update(
-				'image_review', 
+				'image_review',
 				array( 'state' => self::STATE_INVALID_IMAGE),
 				array(implode(' OR ', $invalidImages)),
 				__METHOD__
@@ -348,7 +348,7 @@ class ImageReviewHelper extends WikiaModel {
 
 		if ( count($unusedImages) > 0 ) {
 			$db->update(
-				'image_review', 
+				'image_review',
 				array( 'reviewer_id = null', 'state' => $state),
 				array(implode(' OR ', $unusedImages)),
 				__METHOD__
@@ -356,14 +356,14 @@ class ImageReviewHelper extends WikiaModel {
 			$commit = true;
 			error_log("ImageReview : returning " . count($unusedImages) . " back to the queue");
 		}
-		
+
 		if ( $commit ) $db->commit();
-		
-		
+
+
 		error_log("ImageReview : fetched new " . count($imageList) . " images");
 
 		$this->wf->ProfileOut( __METHOD__ );
-		
+
 		return $imageList;
 	}
 
@@ -387,7 +387,7 @@ class ImageReviewHelper extends WikiaModel {
 
 		$imageSrc = ( empty($response['image']['imagecrop']) ) ? '' : $response['image']['imagecrop'] ;
 		$imagePage = ( empty($response['imagepage']['imagecrop']) ) ? '' : $response['imagepage']['imagecrop'] ;
-		
+
 		$this->wf->ProfileOut( __METHOD__ );
 
 		return array('src' => $imageSrc, 'page' => $imagePage );
@@ -408,34 +408,34 @@ class ImageReviewHelper extends WikiaModel {
 
 		$this->wf->ProfileOut( __METHOD__ );
 		return $imagePage;
-	}	
+	}
 
-	
+
 	protected function getWhitelistedWikis() {
 		$this->wf->ProfileIn( __METHOD__ );
 
 		$topWikis =  $this->getTopWikis();
 
 		$whitelistedWikis =  $this->getWhitelistedWikisFromWF();
-		
+
 		$out = array_keys( $whitelistedWikis + $topWikis );
 
 		$this->wf->ProfileOut( __METHOD__ );
 
 		return $out;
 	}
-	
+
 	protected function getWhitelistedWikisFromWF() {
 		$this->wf->ProfileIn( __METHOD__ );
 		$key = wfMemcKey( 'ImageReviewSpecialController', __METHOD__ );
 
-		$data = $this->wg->memc->get($key, null); 
-		
+		$data = $this->wg->memc->get($key, null);
+
 		if(!empty($data)) {
 			$this->wf->ProfileOut( __METHOD__ );
 			return $data;
 		}
-		
+
 		$oVariable = WikiFactory::getVarByName( 'wgImageReviewWhitelisted', 177 );
 		$fromWf = WikiFactory::getListOfWikisWithVar($oVariable->cv_variable_id, 'bool', '=' ,true);
 
@@ -443,27 +443,27 @@ class ImageReviewHelper extends WikiaModel {
 		$this->wf->ProfileOut( __METHOD__ );
 		return $fromWf;
 	}
-	
+
 	protected function getTopWikis() {
 		$this->wf->ProfileIn( __METHOD__ );
 		$key = wfMemcKey( 'ImageReviewSpecialController', __METHOD__ );
-		$data = $this->wg->memc->get($key, null); 		
+		$data = $this->wg->memc->get($key, null);
 		if(!empty($data)) {
 			$this->wf->ProfileOut( __METHOD__ );
 			return $data;
 		}
-		
+
 		$db = $this->wf->GetDB(DB_SLAVE, array(), $this->wg->StatsDB);
 		$ids = array();
-		
+
 		if (!$this->wg->DevelEnvironment ) {
 			$result = $db->select(
 				array( 'google_analytics.pageviews' ),
 				array( 'city_id', 'sum(pageviews) as cnt' ),
 				array( 'date > curdate() - interval 31 day' ),
 				__METHOD__,
-				array( 
-					'GROUP BY'=> 'city_id', 
+				array(
+					'GROUP BY'=> 'city_id',
 					'HAVING' => 'cnt > 150000'
 				)
 			);
@@ -472,12 +472,12 @@ class ImageReviewHelper extends WikiaModel {
 				$ids[$row['city_id']] = 1;
 			}
 		}
- 	
+
 		$this->wg->memc->set( $key, $ids, 86400 /* 24h */ );
 		$this->wf->ProfileOut( __METHOD__ );
 		return $ids;
 	}
-	
+
 	public function getImageCount() {
 		$this->wf->ProfileIn( __METHOD__ );
 
@@ -494,7 +494,7 @@ class ImageReviewHelper extends WikiaModel {
 		if(!empty($list)) {
 			$where[] = 'wiki_id not in('.implode(',', $list).')';
 		}
-		
+
 		$where['state'] = self::STATE_QUESTIONABLE;
 
 		// select by reviewer, state and total count with rollup and then pick the data we want out
@@ -505,29 +505,29 @@ class ImageReviewHelper extends WikiaModel {
 			__METHOD__,
 			array()
 		);
-		
+
 		$total = array('reviewer' => 0, 'unreviewed' => 0, 'questionable' => 0);
 		while( $row = $db->fetchObject($result) ) {
 			$total['questionable'] = $row->total;
-			
+
 			// Rollup row with Reviewer total count
 		/*	if ($row->reviewer_id == $reviewer_id && ($row->state > self::STATE_IN_REVIEW)) {
 				$total['reviewer'] += $row->total;
 			}
 			// Rollup row with total unreviewed
 			if ($row->state == self::STATE_UNREVIEWED) {
-				$total['unreviewed'] += $row->total; 
+				$total['unreviewed'] += $row->total;
 			}
 			// Rollup row with total questionable
 			if ($row->state == self::STATE_QUESTIONABLE) {
-				$total['questionable'] += $row->total; 
+				$total['questionable'] += $row->total;
 			} */
 		}
 	//	$total['reviewer'] = $this->wg->Lang->formatNum($total['reviewer']);
 	//	$total['unreviewed'] = $this->wg->Lang->formatNum($total['unreviewed']);
-		$total['questionable'] = $this->wg->Lang->formatNum($total['questionable']);				
+		$total['questionable'] = $this->wg->Lang->formatNum($total['questionable']);
 		$this->wg->memc->set( $key, $total, 3600 /* 1h */ );
-		
+
 		return $total;
 		$this->wf->ProfileOut( __METHOD__ );
 	}
