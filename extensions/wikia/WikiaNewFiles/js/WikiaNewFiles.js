88dd2fce1fdddc6babe99d6f3fd241e610c496ba
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-27 23:44:06 +0000

    JSLint fixes.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50608 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaNewFiles/js/WikiaNewFiles.js
+++ extensions/wikia/WikiaNewFiles/js/WikiaNewFiles.js
@@ -1,22 +1,22 @@
 var WikiaNewFiles = {
 	init: function() {
 		$("#gallery-")
-		.find(".wikia-gallery-item-user").click(function() { WikiaNewFiles.resultTrack($(this), 'username') })
+		.find(".wikia-gallery-item-user").click(function() { WikiaNewFiles.resultTrack($(this), 'username'); })
 		.end()
-		.find(".wikia-gallery-item-posted").click(function() { WikiaNewFiles.resultTrack($(this), 'postedinArticle') })
+		.find(".wikia-gallery-item-posted").click(function() { WikiaNewFiles.resultTrack($(this), 'postedinArticle'); })
 		.end()
-		.find(".wikia-gallery-item-more").click(function() { WikiaNewFiles.resultTrack($(this), 'postedinMore') });
+		.find(".wikia-gallery-item-more").click(function() { WikiaNewFiles.resultTrack($(this), 'postedinMore'); });
 
 		$("#newfiles-nav")
-		.find(".navigation-showbots").click(function() { WikiaNewFiles.navTrack('showbots') })
+		.find(".navigation-showbots").click(function() { WikiaNewFiles.navTrack('showbots'); })
 		.end()
-		.find(".navigation-hidebots").click(function() { WikiaNewFiles.navTrack('hidebots') })
+		.find(".navigation-hidebots").click(function() { WikiaNewFiles.navTrack('hidebots'); })
 		.end()
-		.find(".navigation-older").click(function() { WikiaNewFiles.navTrack('older') })
+		.find(".navigation-older").click(function() { WikiaNewFiles.navTrack('older'); })
 		.end()
-		.find(".navigation-newer").click(function() { WikiaNewFiles.navTrack('newer') })
+		.find(".navigation-newer").click(function() { WikiaNewFiles.navTrack('newer'); })
 		.end()
-		.find(".navigation-filesfrom").click(function() { WikiaNewFiles.navTrack('filesfrom') });
+		.find(".navigation-filesfrom").click(function() { WikiaNewFiles.navTrack('filesfrom'); });
 	},
 
 	resultTrack: function( elem, linkType ) {
@@ -27,7 +27,7 @@ var WikiaNewFiles = {
 
 	navTrack: function ( navType ) {
 		//alert("Tracking '" + '/newFilesGallery/' + navType + "'")
-		$.tracker.byStr('/newFilesGallery/' + navType)
+		$.tracker.byStr('/newFilesGallery/' + navType);
 	}
 };
 
