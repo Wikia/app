2044dbfc4abd35e48e9cf23c9bcc203e3cd41e56
jacek <jacek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-27 12:01:07 +0000

    sanitized article list
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50531 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/sanitizedArticleList.php
+++ maintenance/wikia/VideoHandlers/sanitizedArticleList.php
@@ -4,26 +4,18 @@
  * @usage: SERVER_ID=177 php videoSanitize.php --conf /usr/wikia/conf-current/wiki.factory/LocalSettings.php --aconf /usr/wikia/conf-current/AdminSettings.php
  *  */
 
-echo "\n\n";
-
 ini_set( "include_path", dirname(__FILE__)."/.." );
 //require_once( 'commandLine.inc' );
 
 ini_set( 'display_errors', 'stdout' );
 $options = array('help');
-@require_once( '../../commandLine.inc' );
+require_once( '../../commandLine.inc' );
 require_once ('videoSanitizerMigrationHelper.class.php');
 require_once( 'videolog.class.php' );
 
 global $IP, $wgCityId, $wgDBname, $wgExternalDatawareDB, $wgVideoHandlersVideosMigrated, $wgDBname, $wgStatsDB;
 
 $wgVideoHandlersVideosMigrated = false; // be sure we are working on old files
-
-$devboxuser = exec('hostname');
-$sanitizeHelper = new videoSanitizerMigrationHelper($wgCityId, $wgDBname, $wgExternalDatawareDB);
-//$previouslyProcessed = $sanitizeHelper->getRenamedVideos("new", " 1 ");
-
-
 
 
 if( isset( $options['help'] ) && $options['help'] ) {
@@ -69,18 +61,9 @@ if ( $rowCount ) {
 
 $dbw->freeResult( $rows );
 
-$botUser = User::newFromName( 'WikiaBot' );
-
-$i=0;
-
 $count = count( $aTranslation );
 $current = 0;
 foreach ( $aAllFiles as $key => $fileRow ) {
-
-//	$rows = $dbw->query( "SELECT distinct il_from FROM imagelinks WHERE il_to ='".mysql_real_escape_string($key)."'");
-
-//	while( $file = $dbw->fetchObject( $rows ) ) {
-
 
 		$oTitle = Title::newFromText($fileRow->article_title);
 
@@ -90,12 +73,9 @@ foreach ( $aAllFiles as $key => $fileRow ) {
 			$wikiUrl = WikiFactory::getWikiByID( $fileRow->city_id )->city_url;
 			$allChangesArticleURLs[ str_replace('http://localhost/', $wikiUrl, $oTitle->getFullURL()) ] = $fileRow->sanitized_title;
 		}
-//	}
-
-//	$i++;
 }
 if (count ($allChangesArticleURLs) > 0 ) {
-	echo( "Article List for cityId: $wgCityId  ( $wikiUrl ) \n" );
+
 	foreach( $allChangesArticleURLs as $url => $v ) {
 		echo str_replace("::", ":", "$url -> Video:$v \n");
 	}
