c481a345b881f962e40fdd29ef6f74fff8ac29a7
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-27 14:34:55 +0000

    Weinre web inspector (BugzId:25947,25948)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50554 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Weinre/WeinreHooks.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Hooks handlers for the Weinre web inspector
+ * 
+ * @author Federico "Lox" Lucignano <federico(at)wikia-inc.com>
+ */
+class WeinreHooks extends WikiaObject {
+	public function onSkinAfterBottomScripts( WikiaSkin $sk, &$scripts ) {
+		$weinre = F::build( 'Weinre' );
+
+		if ( $weinre->isEnabled() ) {
+			$host = $weinre->getRequestedHost();
+
+			//allow testing from non-owned test environment or production/staging
+			if ( !empty( $host ) || !empty( $this->wg->develEnvironment ) ) {
+				//this would be filtered on a per-skin basis by AssetManager config
+				foreach ( F::build( 'AssetsManager', array(), 'getInstance' )->getURL( 'weinre_js' ) as $s ) {
+					$scripts .= "<script src=\"{$s}\"></script>";
+				}
+			}
+		}
+
+		return true;
+	}
+}
