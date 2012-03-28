e77dfde199bc9e33922adf56c3546bda7f23e4ae
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-03-28 14:03:43 +0000

    GA custom variables global var scope refactoring
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50659 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AnalyticsEngine/AnalyticsProviderGA_Urchin.php
+++ extensions/wikia/AnalyticsEngine/AnalyticsProviderGA_Urchin.php
@@ -41,8 +41,7 @@ SCRIPT2;
   _gaq.push(['_setAccount', 'UA-30014103-1']);
   _gaq.push(['_setSampleRate', '10']);
 
-  var wiki = 'db=' + (window.wgDBname || 'unknown') + ';hub=' + (window.wgCatId || 'unknown') + ';lang=' + (window.wgContentLanguage || 'unknown');
-  _gaq.push(['_setCustomVar', 1, 'wiki',  wiki, 3]);
+  _gaq.push(['_setCustomVar', 1, 'wiki', 'db=' + (window.wgDBname || 'unknown') + ';hub=' + (window.wgCatId || 'unknown') + ';lang=' + (window.wgContentLanguage || 'unknown'), 3]);
   _gaq.push(['_setCustomVar', 3, 'AB',   'unknown', 3]);
   _gaq.push(['_setCustomVar', 4, 'skin',  window.skin || 'unknown', 3]);
   _gaq.push(['_setCustomVar', 5, 'user', (window.wgUserName == null) ? 'anon' : 'user', 3]);
