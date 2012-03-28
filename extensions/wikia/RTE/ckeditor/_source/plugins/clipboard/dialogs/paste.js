ef9bea665ff944aea6b89deb0e4c99adc490b2bf
liz <liz@wikia-inc.com>
2012-03-28 01:31:11 +0000

    (BugId:23788) removing colored bg from text pasted into the editor
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50617 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RTE/ckeditor/_source/plugins/clipboard/dialogs/paste.js
+++ extensions/wikia/RTE/ckeditor/_source/plugins/clipboard/dialogs/paste.js
@@ -198,6 +198,19 @@ CKEDITOR.dialog.add( 'paste', function( editor )
 								container.setStyle( 'display', 'block' );
 								container.setStyle( 'height', ( iframe.$.offsetHeight + 2 ) + 'px' );
 							}
+							
+							var body = iframe.getFrameDocument().getBody();
+							body.on('paste', function(e) {
+							
+								setTimeout(function() {
+									var content = body.getHtml();
+	
+									editor.fire('pastedRemoveBG', content);
+						 			// TODO: get a return value from editor.fire instead of using RTE.tmpPasted
+						 			body.setHtml(RTE.tmpPasted);								
+									
+								}, 0);
+							});
 						},
 						commit : function( data )
 						{
