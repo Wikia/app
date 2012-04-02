2fbab23d0e53c67addd7f4da35157cf612095732
pbablok <pbablok@wikia-inc.com>
2012-04-02 16:09:22 +0000

    VideoRefactoring: bugfix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50875 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/videoMigrateData.php
+++ maintenance/wikia/VideoHandlers/videoMigrateData.php
@@ -177,7 +177,7 @@ if( $rowCount ) {
 			echo "Using default thumbnail (could not get from url: " . $video->thumbnail_url . " )\n";
 		}
 
-		if( !is_array($res) || $res['status'] != UploadFromUrl::OK || $res['status'] != UploadFromUrl::VERIFICATION_ERROR ) {
+		if( !is_array($res) || ($res['status'] != UploadFromUrl::OK && $res['status'] != UploadFromUrl::VERIFICATION_ERROR) ) {
 			echo "Thumbnail upload fail\n";
 			var_dump( $res );
 			continue;
