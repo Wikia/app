311cb504ca5be6d55499a83e5a4024e8add9a011
timq <timq@wikia-inc.com>
2012-04-03 16:18:39 +0000

    Implementation for BugID:26152
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50967 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Blogs/SpecialMyblog.php
@@ -0,0 +1,20 @@
+<?php
+
+class SpecialMyblog extends UnlistedSpecialPage {
+
+var $mAllowedRedirectParams = array();
+        
+	function __construct() {
+                parent::__construct( 'Myblog' );
+                $this->mAllowedRedirectParams = array( 'action' , 'preload' , 'editintro', 'section' );
+        }
+
+        function getRedirect( $subpage ) {
+                global $wgUser;
+                if ( strval( $subpage ) !== '' ) {
+                        return Title::makeTitle( NS_BLOG_ARTICLE, $wgUser->getName() . '/' . $subpage );
+                } else {
+                        return Title::makeTitle( NS_BLOG_ARTICLE, $wgUser->getName() );
+                }
+        } // end function getRedirect
+     } // end class
