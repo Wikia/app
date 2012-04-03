9fb9c2d64d399f4949a7b102ed305c48c906b32c
mech <mech@wikia-inc.com>
2012-04-03 11:18:32 +0000

    selenium training: toolbar test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@50939 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/com/wikia/selenium/tests/ToolbarTest.java
+++ tests/acceptance/com/wikia/selenium/tests/ToolbarTest.java
@@ -8,6 +8,19 @@ import org.testng.annotations.Test;
 
 public class ToolbarTest extends BaseTest {
 
+	public static String randomArticlePath = "index.php?title=Special:Random";
+	
+	/**
+	 * selenium training test
+	 */ 
+	//@Test
+	public void testEditToolbarEntry() throws Exception {
+		login();
+		openAndWait(randomArticlePath);
+		
+		logout();
+	}
+	
 	@Test(groups={"envProduction","verified"})
 	public void testEnsuresThatToolbarIsNotPresentForAnonymousUsers() throws Exception {
 		//Written by Aga Serowiec 02-Feb-2012
@@ -22,8 +35,9 @@ public class ToolbarTest extends BaseTest {
 		openAndWait("/");
 		login();
 		session().click("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='customize']");
-		waitForElement("//section[@id='MyToolsConfigurationWrapper']");
-		session().click("//section[@id='MyToolsConfigurationWrapper']//a[@href='#']");
+		waitForElement("css=section#MyToolsConfigurationWrapper span.reset-defaults");
+		session().click("css=section#MyToolsConfigurationWrapper span.reset-defaults a");
+
 		assertFalse(session().isElementPresent("//section[contains(@class, 'modalContent')]//ul[contains(@class, 'options-list ui-sortable')]//li[@data-caption='Edit']"));
 		session().click("//section[@id='MyToolsConfigurationWrapper']//div[contains(@class, 'buttons')]/input[@type='submit']");
 		logout();
@@ -46,8 +60,14 @@ public class ToolbarTest extends BaseTest {
 		openAndWait("/");	
 		login();
 		assertTrue(session().isElementPresent("WikiaFooter"));
+		
+		//"ul.tools a.tools-customize"
+		
+		assertTrue(session().isElementPresent("footer#WikiaFooter div.toolbar"));
 		assertTrue(session().isElementPresent("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]"));
-		session().click("//footer[@id='WikiaFooter']//div[contains(@class, 'toolbar')]//a[@data-name='customize']");
+		
+		//footer#WikiaFooter div.toolbar
+		session().click("ul.tools link=Customize");
 		waitForElement("//section[@id='MyToolsConfigurationWrapper']");
 		session().click("//section[@id='MyToolsConfigurationWrapper']//div[contains(@class, 'popular-toggle toggle-1')]");
 		waitForElementVisible("//section[@id='MyToolsConfigurationWrapper']//ul[contains(@class, 'popular-list')]");
