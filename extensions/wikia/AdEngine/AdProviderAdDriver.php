7b9e1f2d451b5f34b6b53f7222904c29803c75dc
will <will@wikia-inc.com>
2012-04-04 21:07:59 +0000

    BugID 26771: leaderboard ads on hubs do not have top padding
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51055 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdProviderAdDriver.php
+++ extensions/wikia/AdEngine/AdProviderAdDriver.php
@@ -20,6 +20,7 @@ class AdProviderAdDriver implements iAdProvider {
 		switch ($slotname) {
 			case 'CORP_TOP_LEADERBOARD':
 			case 'HOME_TOP_LEADERBOARD':
+			case 'HUB_TOP_LEADERBOARD':
 			case 'TOP_LEADERBOARD':
 			case 'CORP_TOP_RIGHT_BOXAD':
 			case 'HOME_TOP_RIGHT_BOXAD':
