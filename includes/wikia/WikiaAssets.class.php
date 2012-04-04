aad73d32375cdb8086cb3d652e0ef15c6eccb0ae
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-04 15:47:52 +0000

    Remove dependency on WikiaAssets / StaticChute (BugId:25943)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51039 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/WikiaAssets.class.php
+++ includes/wikia/WikiaAssets.class.php
@@ -2,92 +2,9 @@
 
 class WikiaAssets {
 
-	/**
-	 * Since this function will return a string (containing comments) even in
-	 * the case of failure.  If the optional 'resultWasEmpty' is provided, it will
-	 * be set to true if the actual request returns an empty string.
-	 */
-	private static function get($url, $resultWasEmpty=false) {
-		global $wgRequest, $wgServer;
-
-		$url = str_replace('&amp;', '&', $url);
-
-		// Re-write local URLs to have the server name at the beginning.
-		if(preg_match("/^https?:\/\//i", $url) == 0){
-			if(stripos($url, '/') === 0) {
-				$url = $wgServer.$url;
-			} else {
-				$url = $wgServer.'/'.$url;
-			}
-		}
-
-		$out = "\n/* Version: not-nginx Call to: {$url} */\n";
-		$content = trim(Http::get($url));
-		if($content == ""){
-			$resultWasEmpty = true;
-		}
-		$out .= $content;
-
-		return $out;
-	}
-
+	// leave it for a while (BugId:25943)
 	public static function combined() {
-		global $wgRequest, $wgStylePath, $wgStyleVersion, $wgUseSiteJs;
-
-		$type = $wgRequest->getVal('type');
-
-		global $wgHttpProxy;
-		$wgHttpProxy = "127.0.0.1:80";
-
-		$contentType = "";
-		$hadAnError = false;
-		if($type == 'PrintCSS') {
-			// TODO: used by Oasis
-			$contentType = "text/css";
-			$out = '';
-			$cb = $wgRequest->getVal('cb');
-			$ref = WikiaAssets::GetPrintCSSReferences($cb);
-			foreach($ref as $reference) {
-				$errorOnThisCall = false;
-				$out .= self::get($reference['url'], $errorOnThisCall);
-				$hadAnError |= $errorOnThisCall;
-			}
-		}
-
-		// If one or more of the files failed... tell varnish/akamai/etc. not to cache this.
-		if($hadAnError){
-			header('HTTP/1.0 503 Temporary error');
-		} else {
-			header("Content-type: $contentType");
-			header('Cache-Control: max-age=2592000, public');
-		}
-
-		echo $out;
+		header('HTTP/1.0 503 Temporary error');
 		exit();
-	}
-
-	private function GetPrintCSSReferences($cb=""){
-		global $wgRequest, $wgSquidMaxage;
-		$cssReferences = array();
-		$siteargs = array(
-			'action' => 'raw',
-			'maxage' => $wgSquidMaxage,
-		);
-		$query = wfArrayToCGI( array(
-			'usemsgcache' => 'yes',
-			'ctype' => 'text/css',
-			'smaxage' => $wgSquidMaxage,
-			'cb' => $cb
-		) + $siteargs );
-
-		$cssReferences[] = array('url' => urldecode(Skin::makeNSUrl('Print.css', $query, NS_MEDIAWIKI)));
-
-		$StaticChute = new StaticChute('css');
-		$StaticChute->useLocalChuteUrl();
-		foreach($StaticChute->config['oasis_css_print'] as $url){
-			$cssReferences[] = array('url' => $url);
-		}
-
-		return $cssReferences;
 	}
 }
