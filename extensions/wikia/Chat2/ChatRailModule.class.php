1cf79b2b67dd4da582ba4295ae6177189dc7f57d
liz <liz@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-09 19:32:51 +0000

    separating assets from Chat to Chat2
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51241 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat2/ChatRailModule.class.php
+++ extensions/wikia/Chat2/ChatRailModule.class.php
@@ -19,9 +19,9 @@ class ChatRailModule extends Module {
 	 * Render placeholder. Content will be ajax-loaded for freshness
 	 */
 	public function executePlaceholder() {
-		//global $wgOut, $wgExtensionsPath;
-		//$wgOut->addStyle(AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/Chat/css/ChatRailModule.scss'));
-		//$this->jsInitializer = F::build('JSSnippets')->addToStack( array( "/extensions/wikia/Chat/js/ChatEntryPoint.js" ), array(), 'ChatEntryPoint.init' );
+		global $wgOut, $wgExtensionsPath;
+		$wgOut->addStyle(AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/Chat/css/ChatRailModule.scss'));
+		$this->jsInitializer = F::build('JSSnippets')->addToStack( array( "/extensions/wikia/Chat/js/ChatEntryPoint.js" ), array(), 'ChatEntryPoint.init' );
 	}
 
 	public function executeAnonLoginSuccess() {
