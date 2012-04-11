6f206e7960413a7ebe7e5598d0a8ae8e1ce88ba6
liz <liz@wikia-inc.com>
2012-04-11 19:24:14 +0000

    (BugId:26764) logic for grouping and debugging
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51394 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchV2/templates/WikiaSearch_result.php
+++ extensions/wikia/SearchV2/templates/WikiaSearch_result.php
@@ -1,30 +1,36 @@
 <!-- single search result -->
-<?php if($result->hasCanonical()): ?>
-	<?=$debug?$pos.'. ':'';?><a href="<?= $result->getUrl(); ?>"><?=$result->getTitle();?></a> (Redirect: <?=$result->getCanonical();?>)
-<?php else: ?>
-	<?=$debug?$pos.'. ':'';?><a href="<?= $result->getUrl(); ?>"><?=$result->getTitle();?></a>
+<?php if(empty($inGroup)): ?>
+	<section class="Result">
 <?php endif; ?>
-
-<?php if(empty($inGroup) && ($result->getVar('cityHost', false) !== false)): ?>
-	| <a href="<?= $result->getVar('cityHost') .'/wiki/Special:Search?search='.urlencode($query).'&fulltext=Search';?>"><?= wfMsg( 'wikiasearch2-search-on-wiki')?></a>
-<?php endif; ?>
-
+<article>
+<header>
+<h1>
+	<?php if($result->hasCanonical()): ?>
+		<?= $debug ? $pos.'. ' : ''; ?><a href="<?= $result->getUrl(); ?>"><?= $result->getTitle(); ?></a> (Redirect: <?= $result->getCanonical(); ?>)
+	<?php else: ?>
+		<?= $debug ? $pos.'. ' : ''; ?><a href="<?= $result->getUrl(); ?>"><?= $result->getTitle(); ?></a>
+	<?php endif; ?>
+</h1>
+</header>
 
 <?php if($debug): ?>
-<br/>
-<?php if(!empty($rank)): ?>
-	<font color="red">WikiRank: <?=$rank;?></font> |
-<?php endif; ?>
-	Score: <?=$result->score?>
+	<i>[<?php if(!empty($rank)): ?><font color="red">WikiRank: <?=$rank;?></font> | <?php endif; ?>Score: <?=$result->score?>]</i>
 <?php endif; ?>
 
-<br />
-<div <?=empty($inGroup)?'class="searchresult"':'';?>>
-	<?= $result->getText(); ?>
-</div>
+<?= $result->getText(); ?>
+
 <?php if(empty($inGroup)): ?>
-	<a href="<?= $result->getUrl(); ?>"><?=$result->getUrl();?></a><br />
+	<nav>
+		<ul>
+			<li><a href="<?= $result->getUrl(); ?>"><?=$result->getUrl();?></a></li>
+			<?php if($result->getVar('cityHost', false) !== false): ?>
+				<li><a href="<?= $result->getVar('cityHost') .'/wiki/Special:Search?search='.urlencode($query).'&fulltext=Search'; ?>"><?= wfMsg( 'wikiasearch2-search-on-wiki') ?></a></li>
+			<?php endif; ?>
+		</ul>
+	</nav>
 <?php endif; ?>
+
+
 
 <?php if($debug): ?>
 	<?php
@@ -47,3 +53,9 @@
 	?>
 	<i>[id: <?=$result->getId();?>, text_relevance: <?=$result->getVar('text_relevance', '?');?>, backlinks: <?=$result->getVar('backlinks', '?');?>, rank: <?= $rankValue; ?>]</i><br />
 <?php endif; //debug ?>
+
+</article>
+
+<?php if(empty($inGroup)): ?>
+	</section>
+<?php endif; ?>
--- extensions/wikia/SearchV2/templates/WikiaSearch_resultSet.php
+++ extensions/wikia/SearchV2/templates/WikiaSearch_resultSet.php
@@ -1,25 +1,25 @@
 <?php if($resultSet->getResultsFound() > 1): ?>
 	<!-- grouped search result-->
-	<section class="Result grouped-result">
+	<section class="Result">
 		<header>
 			<h1><?=$debug?$pos.'. ':'';?><a href="<?=$resultSet->getHeader('cityUrl');?>"><?=$resultSet->getHeader('cityTitle');?></a></h1>
-			<nav>
-				<ul>
-					<li><a href="<?=$resultSet->getHeader('cityUrl');?>"><?=$resultSet->getHeader('cityUrl');?></a></li>
-					<?php if($resultSet->getHeader('cityArticlesNum')): ?>
-						<li><?=$resultSet->getHeader('cityArticlesNum');?> <?= wfMsg( 'wikiasearch2-pages' ); ?></li>
-					<?php endif; ?>
-					<li><a href="<?= $resultSet->getHeader('cityUrl') .'/wiki/Special:Search?search='.urlencode($query).'&fulltext=Search';?>"><?= wfMsg( 'wikiasearch2-search-on-wiki')?></a></li>
-					<?php if ($debug): ?>
-						<li><?=$resultSet->getResultsFound();?> <?= wfMsg( 'wikiasearch2-results' ); ?></li>
-						<?php if($resultSet->getHeader('cityRank')): ?>
-							<li>(1st pos: <?=$resultSet->getHeader('1stResultPos');?>) <font color="red">WikiRank: <?=$resultSet->getHeader('cityRank');?></font></li>
-						<?php endif; ?>
-						<li>Total Score: <?=sprintf('%.3f',  $resultSet->totalScore)?></li>
-					<?php endif;?>
-				</ul>
-			</nav>
 		</header>
+		<nav>
+			<ul>
+				<li><a href="<?=$resultSet->getHeader('cityUrl');?>"><?=$resultSet->getHeader('cityUrl');?></a></li>
+				<?php if($resultSet->getHeader('cityArticlesNum')): ?>
+					<li><?= wfMsg( 'wikiasearch2-pages', $resultSet->getHeader('cityArticlesNum') ); ?></li>
+				<?php endif; ?>
+				<li><a href="<?= $resultSet->getHeader('cityUrl') .'/wiki/Special:Search?search='.urlencode($query).'&fulltext=Search';?>"><?= wfMsg( 'wikiasearch2-search-on-wiki')?></a></li>
+				<?php if ($debug): ?>
+					<li><?=$resultSet->getResultsFound();?> <?= wfMsg( 'wikiasearch2-results' ); ?></li>
+					<?php if($resultSet->getHeader('cityRank')): ?>
+						<li>(1st pos: <?=$resultSet->getHeader('1stResultPos');?>) <font color="red">WikiRank: <?=$resultSet->getHeader('cityRank');?></font></li>
+					<?php endif; ?>
+					<li>Total Score: <?=sprintf('%.3f',  $resultSet->totalScore)?></li>
+				<?php endif;?>
+			</ul>
+		</nav>
 		<?php for($i = 1; $i < 5; $i++): ?>
 			<?php $result = $resultSet->next(); ?>
 			<?php if($result instanceof WikiaSearchResult): ?>
