e90d0b3a2cd6432881c1a4a01cb081f2680212e0
liz <liz@wikia-inc.com>
2012-04-11 23:32:50 +0000

    removing groupResults from pagination urls
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51403 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchV2/templates/WikiaSearch_pagination.php
+++ extensions/wikia/SearchV2/templates/WikiaSearch_pagination.php
@@ -1,19 +1,19 @@
 <?php if($windowLastPage > 1): ?>
 	<div class="pagination">
 		<?php if( $windowFirstPage > 1 ): ?>
-			<a href="<?= $pageTitle->getFullUrl( array( 'query' => $query, 'page' => ($windowFirstPage-1), 'crossWikia' => ( $isInterWiki ? '1' : '0' ), 'rankExpr' => $rankExpr, 'groupResults' => ( $isInterWiki ? '1' : '0' ) ) ); ?>">...</a>
+			<a href="<?= $pageTitle->getFullUrl( array( 'query' => $query, 'page' => ($windowFirstPage-1), 'crossWikia' => ( $isInterWiki ? '1' : '0' ), 'rankExpr' => $rankExpr ) ); ?>">...</a>
 		<?php endif; ?>
 		<?php for( $i = $windowFirstPage; $i <= $windowLastPage; $i++ ): ?>
 		
 			<?php if($i == $currentPage): ?>
 				<?=$i;?>&nbsp;
 			<?php else: ?>
-				<a href="<?= $pageTitle->getFullUrl( array( 'query' => $query, 'page' => $i, 'crossWikia' => ( $isInterWiki ? '1' : '0' ), 'rankExpr' => $rankExpr, 'groupResults' => ( $isInterWiki ? '1' : '0' ) ) ); ?>"><?=$i;?></a>&nbsp;
+				<a href="<?= $pageTitle->getFullUrl( array( 'query' => $query, 'page' => $i, 'crossWikia' => ( $isInterWiki ? '1' : '0' ), 'rankExpr' => $rankExpr ) ); ?>"><?=$i;?></a>&nbsp;
 			<?php endif;?>
 		<?php endfor; ?>
 		
 		<?php if( $windowLastPage < $pagesNum ): ?>
-		<a href="<?= $pageTitle->getFullUrl( array( 'query' => $query, 'page' => $i, 'crossWikia' => ( $isInterWiki ? '1' : '0' ), 'rankExpr' => $rankExpr, 'groupResults' => ( $isInterWiki ? '1' : '0' ) ) ); ?>">...</a>
+		<a href="<?= $pageTitle->getFullUrl( array( 'query' => $query, 'page' => $i, 'crossWikia' => ( $isInterWiki ? '1' : '0' ), 'rankExpr' => $rankExpr ) ); ?>">...</a>
 		<?php endif; ?>
 	</div>
 <?php endif; ?>	
