0c92b5b74ec1be59c766080e65ac5dc5a9d540a4
jacek <jacek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-12 14:04:02 +0000

    list update
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51425 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/unsupportedProvidersArticleList.php
+++ maintenance/wikia/VideoHandlers/unsupportedProvidersArticleList.php
@@ -62,13 +62,23 @@ foreach ( $aAllFiles as $key => $fileRow ) {
 				$wiki = WikiFactory::getWikiByID( $fileRow->wiki_id );
 
 				echo "Videos from: ". $wiki->city_url ." \n";
+				echo "========================================\n\n";
 
 				$dbw = wfGetDB( DB_MASTER, array(), $wiki->city_dbname );
 
 				$dataBase = $fileRow->wiki_id ;
+
+
 			}
 
+			echo $wiki->city_url . "wiki/File:" . $fileRow->img_name ;
+
+
 			$rows = $dbw->query( "SELECT distinct il_from FROM imagelinks WHERE il_to ='".mysql_real_escape_string($fileRow->img_name)."'");
+			$rowCount = $rows->numRows();
+
+			echo " (" . $rowCount ." backlinks) " . ( $rowCount > 0 ) ? ": \n" : "\n";
+
 			while( $file = $dbw->fetchObject( $rows ) ) {
 				$articleId = $file->il_from;
 				$oTitle = GlobalTitle::newFromId($articleId, $fileRow->wiki_id); //::newFromId( $articleId );
