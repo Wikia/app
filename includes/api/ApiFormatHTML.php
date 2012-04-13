11815915a869ef1d4c4ca18a9d35cf7e7a461526
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-13 16:35:33 +0000

    WikiaMobile: work on horizontal scrollable content
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51481 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/api/ApiFormatHTML.php
+++ includes/api/ApiFormatHTML.php
@@ -1,26 +1,11 @@
 <?php
 
 /*
- * Created on Oct 22, 2006
+ * This is simple 'hack' to get HTML response
+ * Created for a Hackathon to be able to call Wikia API through MW API
  *
- * API for MediaWiki 1.8+
+ * @author Jakub Olek
  *
- * Copyright (C) 2008 Roan Kattouw <Firstname>.<Lastname>@home.nl
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- * http://www.gnu.org/copyleft/gpl.html
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
