1dfeacbf9b919a01d90d3d002aecda4b11f587f9
mech <mech@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-18 15:03:02 +0000

    Wikia AllHands 2012: tmp file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51580 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/JavascriptTestRunner/js/phantom.js
+++ extensions/wikia/JavascriptTestRunner/js/phantom.js
@@ -11,6 +11,9 @@ test = system.args[1];
 
 page.onConsoleMessage = function(msg){
 	console.log(msg);
+	if (msg == '!!PHANTOM_EXIT!!') {
+		phantom.exit();
+	}
 };
 
 page.onError  = function(msg){
@@ -27,9 +30,10 @@ console.log(framework[1]);
 var requires = /@test-require-file[\s]+([^\s]+)/g, requiredFiles = [], match;
 
 if (framework[1] == "QUnit") {
-	requiredFiles.push('extensions/wikia/JavascriptTestRunner/js/qunit.js');
+	requiredFiles.push('../extensions/wikia/JavascriptTestRunner/js/qunit.js');
+	requiredFiles.push('../extensions/wikia/JavascriptTestRunner/js/phantom-qunit.js');
 } else if (framework[1] == "jsUnity") {
-	requiredFiles.push('extensions/wikia/JavascriptTestRunner/js/jsunity-0.6.js');
+	requiredFiles.push('../extensions/wikia/JavascriptTestRunner/js/jsunity-0.6.js');
 } else {
 	console.error("You're fucked without a test framework");
 	phantom.exit(-1);
@@ -38,13 +42,30 @@ if (framework[1] == "QUnit") {
 do {
     match = requires.exec(testSource);
     if (match) {
-    	requiredFiles.push(match[1]);
+    	requiredFiles.push('../' + match[1]);
         //console.log(match[1]);
     }
 } while (match != null);
 
+requiredFiles.push(test);
+
 console.log(requiredFiles);
 
+var htmlTemplate = fs.read('test.html');
+
+var header = '';
+for(var i = 0 ; i < requiredFiles.length ; i++) {
+	header += '<script type="text/javascript" src="' + requiredFiles[i] + '"></script>\n';
+}
+htmlTemplate = htmlTemplate.replace( '<!--PHANTOM_SCRIPT_PLACEHOLDER-->', header);
+
+var tempPath = 'phantom_js_test.html';
+
+fs.write(tempPath, htmlTemplate, 'w');
+
+page.open(tempPath);
+
+/*
 var runAll = function(files, runTestsCallback) {
   var no = -1;
   function consumer() {
@@ -58,6 +79,14 @@ var runAll = function(files, runTestsCallback) {
   };
   return consumer;
 }(requiredFiles, function() {
+	page.evaluate(function() {
+		console.log('EVALUATED!');
+		console.log(this);
+		
+		jsUnity.log = function (s) {
+			console.log('jsUnity: ' + s);
+		};
+	});
 	// run the tests here
 	console.log('callback called!!!');
 	page.includeJs( test, function(){
@@ -68,3 +97,4 @@ var runAll = function(files, runTestsCallback) {
 });
 
 page.open('test.html', runAll);
+*/
