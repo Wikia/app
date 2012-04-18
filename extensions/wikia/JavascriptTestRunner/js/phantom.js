747685c83039777f54baa82fb40803ae474ff3f3
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-18 10:28:11 +0000

    Team coding: phantom JS test runner
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51514 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/JavascriptTestRunner/js/phantom.js
@@ -0,0 +1,17 @@
+var page = require('webpage').create(),
+	system = require('system'),
+	test;
+
+if (system.args.length === 2) {
+	console.log('Usage: phantom.js <test file>');
+	phantom.exit();
+}
+
+test = system.args[2];
+
+page.includeJS( test, function(){
+	console.log('runs!');
+	phantom.exit();
+} );
+
+console.log('fuck!');
---
+++ tests/run-js-test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Runs js unit tests using phantomJS
+ *
+ * @author Federico "Lox" Lucignano <federico(at)wikia-inc.com>
+ *
+ * php run-js-test.php --test=path/to/test.js
+ */
+
+function printHelp() {
+		echo <<<HELP
+Runs a javascript unit test
+
+USAGE: php run-js-test.php -t/path/to/test.js
+
+	-t
+		test file to run
+HELP;
+}
+
+$options = getopt( 't:' );
+
+if ( empty( $options['t'] ) ) {
+	printHelp();
+}
+
+$dir = dirname(__FILE__);
+$test = escapeshellcmd( $options['t'] );
+$cmd = "phantomjs {$dir}../extensions/wikia/JSTestRunner/js/phantom.js {$test}";
+
+exec($cmd, $output, $retVal);
+
+// decode the last line
+var_dump( array( 'ret' => $retVal, 'output' => $output ) );
