a6337e1391d56675ae3fb1428dc4d038900c8494
will <will@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-18 14:21:05 +0000

    BugID 28633: wikia video library user
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51570 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoHandlers/VideoHandlers.i18n.php
+++ extensions/wikia/VideoHandlers/VideoHandlers.i18n.php
@@ -14,10 +14,370 @@ $messages['en'] = array(
 	'videohandler-error-video-no-exist' => 'Video specified by title does not exist',
 	'videohandler-unknown-title' => 'Unknown title',
 	'videohandler-video-details' => '$1 (provider: $2)',
+	'videohandler-category' => 'Videos',
 );
 
 $messages['qqq'] = array(
 	'videohandler-video-details' => 'Parameters:
 * $1 is a link to the video on its source page, with the label being the original title of the video
 * $2 is a link to the provider\'s homepage (e.g. [http://youtube.com/ youtube.com])',
+);
+
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'videohandler-category' => "Video's",
+);
+
+/** Azerbaijani (Azərbaycanca)
+ * @author Sortilegus
+ * @author Vago
+ */
+$messages['az'] = array(
+	'videohandler-category' => 'Videolar',
+);
+
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author EugeneZelenko
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'videohandler-category' => 'Відэа',
+);
+
+/** Bulgarian (Български)
+ * @author DCLXVI
+ */
+$messages['bg'] = array(
+	'videohandler-category' => 'Видео',
+);
+
+/** Breton (Brezhoneg)
+ * @author Gwenn-Ael
+ * @author Y-M D
+ */
+$messages['br'] = array(
+	'videohandler-category' => 'Videoioù',
+);
+
+/** Bosnian (Bosanski)
+ * @author Palapa
+ */
+$messages['bs'] = array(
+	'videohandler-category' => 'Videa',
+);
+
+/** Catalan (Català)
+ * @author Gemmaa
+ * @author Paucabot
+ */
+$messages['ca'] = array(
+	'videohandler-category' => 'Vídeos',
+);
+
+/** Czech (Česky)
+ * @author Mr. Richard Bolla
+ */
+$messages['cs'] = array(
+	'videohandler-category' => 'Videa',
+);
+
+/** Welsh (Cymraeg)
+ * @author Lloffiwr
+ */
+$messages['cy'] = array(
+	'videohandler-category' => 'Fideos',
+);
+
+/** German (Deutsch)
+ * @author Inkowik
+ * @author LWChris
+ * @author Tiin
+ */
+$messages['de'] = array(
+	'videohandler-category' => 'Videos',
+);
+
+/** German (formal address) (‪Deutsch (Sie-Form)‬)
+ * @author LWChris
+ */
+$messages['de-formal'] = array(
+	'videohandler-category' => 'Videos',
+);
+
+/** Spanish (Español)
+ * @author Bola
+ * @author Ciencia Al Poder
+ * @author Fitoschido
+ * @author Translationista
+ * @author VegaDark
+ */
+$messages['es'] = array(
+	'videohandler-category' => 'Vídeos',
+);
+
+/** Basque (Euskara)
+ * @author An13sa
+ */
+$messages['eu'] = array(
+	'videohandler-category' => 'Bideoak',
+);
+
+
+
+/** Persian (فارسی) */
+$messages['fa'] = array(
+	'videohandler-category' => 'ویدیو‌ها',
+);
+
+/** Finnish (Suomi)
+ * @author Centerlink
+ * @author Crt
+ */
+$messages['fi'] = array(
+	'videohandler-category' => 'Videot',
+);
+
+/** French (Français)
+ * @author IAlex
+ * @author Wyz
+ */
+$messages['fr'] = array(
+	'videohandler-category' => 'Vidéos',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'videohandler-category' => 'Vídeos',
+);
+
+/** Hebrew (עברית)
+ * @author שומבלע
+ */
+$messages['he'] = array(
+	'videohandler-category' => 'סרטוני וידאו',
+);
+
+/** Hungarian (Magyar)
+ * @author Glanthor Reviol
+ */
+$messages['hu'] = array(
+	'videohandler-category' => 'Videók',
+);
+
+/** Armenian (Հայերեն)
+ * @author Pandukht
+ */
+$messages['hy'] = array(
+	'videohandler-category' => 'Տեսանյութեր',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'videohandler-category' => 'Videos',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author Irwangatot
+ * @author Kenrick95
+ */
+$messages['id'] = array(
+	'videohandler-category' => 'Video',
+);
+
+/** Italian (Italiano)
+ * @author HalphaZ
+ */
+$messages['it'] = array(
+	'videohandler-category' => 'Video',
+);
+
+/** Japanese (日本語)
+ * @author Schu
+ * @author Tommy6
+ */
+$messages['ja'] = array(
+	'videohandler-category' => '動画',
+);
+
+/** Georgian (ქართული)
+ * @author David1010
+ */
+$messages['ka'] = array(
+	'videohandler-category' => 'ვიდეოები',
+);
+
+/** Karachay-Balkar (Къарачай-Малкъар)
+ * @author Къарачайлы
+ */
+$messages['krc'] = array(
+	'videohandler-category' => 'Видеола',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'videohandler-category' => 'Videoen',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'videohandler-category' => 'Видеоснимки',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'videohandler-category' => 'Video',
+);
+
+/** Norwegian Bokmål (‪Norsk (bokmål)‬)
+ * @author Audun
+ * @author Nghtwlkr
+ */
+$messages['nb'] = array(
+	'videohandler-category' => 'Videoer',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'videohandler-category' => "Video's",
+);
+
+/** Punjabi (ਪੰਜਾਬੀ)
+ * @author Aalam
+ */
+$messages['pa'] = array(
+	'videohandler-category' => 'ਵਿਡੀਓ',
+);
+
+/** Polish (Polski)
+ * @author Marcin Łukasz Kiejzik
+ * @author Sovq
+ * @author Sp5uhe
+ * @author Woytecr
+ */
+$messages['pl'] = array(
+	'videohandler-category' => 'Filmy',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'videohandler-category' => 'Video',
+);
+
+/** Pashto (پښتو)
+ * @author Ahmed-Najib-Biabani-Ibrahimkhel
+ */
+$messages['ps'] = array(
+	'videohandler-category' => 'ويډيوګانې',
+);
+
+/** Portuguese (Português)
+ * @author Crazymadlover
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'videohandler-category' => 'Vídeos',
+);
+
+/** Brazilian Portuguese (Português do Brasil)
+ * @author Daemorris
+ */
+$messages['pt-br'] = array(
+	'videohandler-category' => 'Vídeos',
+);
+
+/** Romanian (Română)
+ * @author Stelistcristi
+ */
+$messages['ro'] = array(
+	'videohandler-category' => 'Videoclipuri',
+);
+
+/** Russian (Русский)
+ * @author Eleferen
+ * @author Kuzura
+ * @author Lockal
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'videohandler-category' => 'Видео',
+);
+
+/** Sinhala (සිංහල)
+ * @author තඹරු විජේසේකර
+ */
+$messages['si'] = array(
+	'videohandler-category' => 'වීඩියෝ',
+);
+
+/** Slovenian (Slovenščina)
+ * @author Dbc334
+ */
+$messages['sl'] = array(
+	'videohandler-category' => 'Videoposnetki',
+);
+
+/** Serbian (Cyrillic script) (‪Српски (ћирилица)‬)
+ * @author Charmed94
+ * @author Rancher
+ * @author Verlor
+ * @author Михајло Анђелковић
+ */
+$messages['sr-ec'] = array(
+	'videohandler-category' => 'Видео-снимци',
+);
+
+/** Swedish (Svenska)
+ * @author Tobulos1
+ * @author WikiPhoenix
+ */
+$messages['sv'] = array(
+	'videohandler-category' => 'Videoklipp',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'videohandler-category' => 'Mga bidyo',
+);
+
+/** Turkish (Türkçe)
+ * @author Mert.subay
+ */
+$messages['tr'] = array(
+	'videohandler-category' => 'Videolar',
+);
+
+/** Ukrainian (Українська)
+ * @author A1
+ * @author Prima klasy4na
+ * @author Тест
+ */
+$messages['uk'] = array(
+	'videohandler-category' => 'Відео',
+);
+
+/** Simplified Chinese (‪中文(简体)‬)
+ * @author Hydra
+ * @author Yanmiao liu
+ */
+$messages['zh-hans'] = array(
+	'videohandler-category' => '视频',
 );
--- extensions/wikia/VideoHandlers/feedingesters/VideoFeedIngester.class.php
+++ extensions/wikia/VideoHandlers/feedingesters/VideoFeedIngester.class.php
@@ -64,7 +64,7 @@ abstract class VideoFeedIngester {
 		}
 
 		$categories = $this->generateCategories($data, $addlCategories);
-		$categories[] = VideoFileUploader::VIDEOS_CATEGORY;
+		$categories[] = wfMsgForContent( 'videohandler-category' );
 		$categoryStr = '';
 		foreach ($categories as $categoryName) {
 			$category = Category::newFromName($categoryName);
