53e6993f99c83f8acdb5c039f7af55465431e8a3
garth <garth@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-18 10:48:08 +0000

    First version of service code
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51521 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/services/DataMartService.class.php
+++ includes/wikia/services/DataMartService.class.php
@@ -11,28 +11,74 @@ class DataMartService extends Service {
 	const PERIOD_ID_YEARLY = 5;
 	const PERIOD_ID_15MINS = 15;
 	const PERIOD_ID_60MINS = 60;
-	const PERIOD_ID_ROLLING_7DAYS = 1007;	// every day
-	const PERIOD_ID_ROLLING_28DAYS = 1028;	// every day
+	const PERIOD_ID_ROLLING_7DAYS = 1007;		// every day
+	const PERIOD_ID_ROLLING_28DAYS = 1028;		// every day
 	const PERIOD_ID_ROLLING_24HOURS = 10024;	// every 15 minutes
 
 	
-	protected static function getPageviews() {
+	protected static function getPageviews( $periodId, $startDate, $endDate=null, $wikiId=null ) {
+		$app = F::app(); 
+
+		if ( empty($wikiId) ) {
+			$wikiId = $app->wg->CityId;
+		}
 		
-	}
+		if ( empty($endDate) ) {
+			$endDate = date( 'Y-m-d', strtotime('-1 day') );
+		}
 
-	public static function getPageviewsDaily() {
+		$memKey = $app->wf->MemcKey("pageviews_$periodId_$startDate_$endDate");
+		$pageviews = $app->wg->Memc->get($memKey);
+		if ( !is_array($pageviews) ) {
+
+			$db = $app->wf->GetDB(DB_SLAVE, array(), $app->wg->DatamartDB);
+
+			$result = $db->select(
+					array('rollup_wiki_pageviews'),
+					array('time_id as date, pageviews as cnt'),
+					array('period_id' => $periodId,
+					      'wiki_id'   => $wikiId,
+					      "time_id between '$startDate' and '$endDate'"),
+					__METHOD__
+			);
+
+			while ( $row = $db->fetchObject($result) ) {
+				$pageviews[ $row->date ] = $row->cnt;
+			}
+		}
+
 		return $pageviews;
 	}
 
-	public static function getPageviewsWeekly() {
+	public static function getPageviewsDaily( $startDate, $endDate=null, $wikiId=null ) {
+		$pageviews = $this::getPageviews( self::PERIOD_ID_DAILY, $startDate, $endDate, $wikiId  );
+		return $pageviews;
+	}
+
+	public static function getPageviewsWeekly( $startDate, $endDate=null, $wikiId=null ) {
+		$pageviews = $this::getPageviews( self::PERIOD_ID_WEEKLY, $startDate, $endDate, $wikiId  );
 		return $pageviews;
 	}	
 
-	public static function getPageviews() {
-		return $pageviews;
-	}	
-
-	public static function getPageviewsWeekly() {
+	public static function getPageviewsMonthly( $startDate, $endDate=null, $wikiId=null ) {
+		$pageviews = $this::getPageviews( self::PERIOD_ID_MONTHLY, $startDate, $endDate, $wikiId  );
 		return $pageviews;
 	}	
 }
+
+
+
+			$db = $this->wf->GetDB(DB_SLAVE, array(), $this->wg->StatsDB);
+
+echo "####### HERE?? ##########\n";
+			$today = date( 'Ymd', strtotime('-1 day') );
+			$week = date( 'Ymd', strtotime('-7 day') );
+
+			$oRes = $db->select(
+					array('rollup_wiki_pageviews'),
+					array('time_id as date, pageviews as cnt'),
+					array('period_id' => 1,
+					      'wiki_id'   => $cityID,
+					      "time_id between '$week' and '$today'"),
+					__METHOD__
+			);
