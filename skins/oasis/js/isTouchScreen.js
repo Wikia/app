6fbb199c72f668693d00a0fa19a75ab29f150a4b
macbre <macbre@wikia-inc.com>
2012-04-19 11:52:09 +0000

    isTouchScreen: no need to use wgStyleVersion as the URL suffix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51600 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/isTouchScreen.js
+++ skins/oasis/js/isTouchScreen.js
@@ -1,3 +1,6 @@
 if ( $().isTouchscreen() ) {
-	$.getResources( [ $.getSassCommonURL(  '/skins/oasis/css/touchScreen.scss' ), stylepath + '/oasis/js/touchScreen.js?' + wgStyleVersion ] );
+	$.getResources([
+		$.getSassCommonURL('/skins/oasis/css/touchScreen.scss'),
+		stylepath + '/oasis/js/touchScreen.js'
+	]);
 }
