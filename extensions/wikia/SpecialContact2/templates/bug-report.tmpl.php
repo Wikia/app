Wikia/app

commit 93829af3904146e3496db0c170876affb391d423

Fixing style and presentation of S:C
git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51607 e846bd91-c042-42a7-a218-4b65e2c1904d

extensions/wikia/SpecialContact2/templates/bug-report.tmpl.php
@@ -68,9 +68,22 @@ if( !$isLoggedIn && (isset($captchaForm)) ) {
 }
 ?>
 
+<?php
+
+if( $isLoggedIn && $hasEmail ) {
+	//is user, has email, but is verified?
+	if( $hasEmailConf ) {
+		//yes!
+		print "<input tabindex='" . ($tabindex++) . "' type='checkbox' name=\"wgCC\" value=\"1\" />" . wfMsg('specialcontact-ccme');
+	}
+	else
+	{
+		//not
+		print "<s><i>" . wfMsg('specialcontact-ccme') . "</i></s><br/> ". wfMsgExt('specialcontact-ccdisabled', array('parse') ) ."";
+	}
+}
+?>
 <input type="submit" value="<?= wfMsg( 'specialcontact-mail' ) ?>" />
 
 <input type="hidden" id="wpBrowser" name="wpBrowser" value="<?php echo $_SERVER['HTTP_USER_AGENT']; ?>" />
 </form>
-
-<p><?= wfMsgExt( 'specialcontact-noform-footer', array( 'parse' ) ) ?></p>

extensions/wikia/SpecialContact2/templates/account-issue.tmpl.php
@@ -60,4 +60,3 @@ if( !$isLoggedIn && (isset($captchaForm)) ) {
 <input type="hidden" id="wpBrowser" name="wpBrowser" value="<?php echo $_SERVER['HTTP_USER_AGENT']; ?>" />
 </form>
 
-<p><?= wfMsgExt( 'specialcontact-noform-footer', array( 'parse' ) ) ?></p>

extensions/wikia/SpecialContact2/templates/close-account.tmpl.php
@@ -31,4 +31,3 @@ echo wfMsgExt( 'specialcontact-intro-close-account', array( 'parse' ) );
 <input type="hidden" id="wpBrowser" name="wpBrowser" value="<?php echo $_SERVER['HTTP_USER_AGENT']; ?>" />
 </form>
 
-<p><?= wfMsgExt( 'specialcontact-noform-footer', array( 'parse' ) ) ?></p>

extensions/wikia/SpecialContact2/templates/form.tmpl.php
@@ -9,43 +9,25 @@ if ( !empty($err) ) {
 <?php print $intro; ?>
 </div>
 <hr/>
-<form name="contactform" id="contactform" method="post" action="<?php echo $form_action; ?>"  enctype="multipart/form-data">
-<p class="contactformcaption"><?php echo wfMsg( 'specialcontact-wikiname' ); ?></p>
-<?php
-	if( $unlockURL ) :
-		print "<input ". $eclass['wpContactWikiName'] ." tabindex='" . ($tabindex++) . "' type='text' name=\"wpContactWikiName\" value=\"{$wgServer}\" size='40' />";
-	else :
-		print "{$wgServer} <input type=\"hidden\" name=\"wpContactWikiName\" value=\"{$wgServer}\" />";
-	endif;
-?>
+<form id="contactform" method="post" action="" enctype="multipart/form-data">
+<input name="wpEmail" type="hidden" value="<?= $encEmail ?>" />
+<input name="wpUserName" type="hidden" value="<?= $encName ?>" />
 
-<p class="contactformcaption"><?php echo wfMsg( 'specialcontact-username' ); ?></p>
-<?php
-	if( empty($user_readonly) ) :
-		print "<input ". $eclass['wpUserName'] ."  tabindex='" . ($tabindex++) . "' type='text' name=\"wpUserName\" value=\"{$encName}\" size='40' />";
-	else :
-		print "{$encName} <input type=\"hidden\" name=\"wpUserName\" value=\"{$encName}\" />".
-				" &nbsp;<span style=\"\" id='contact-not-me'><i><a href=\"{$logoutURL}\">(". wfMsg( 'specialcontact-notyou', $encName ) .")</a></i></span>";
-	endif;
+<?php if ( $isLoggedIn ) {
+	echo wfMsgExt( 'specialcontact-logged-in-as', array( 'parse' ), $encName );
+	echo wfMsgExt( 'specialcontact-mail-on-file', array( 'parse' ), $encEmail );
+} else {
 ?>
+<p>
+<label for="wpUserName"><?= wfMsg( 'specialcontact-username' ) ?></label>
+<input name="wpUserName" value="<?= $encName ?>" />
+</p>
 
-<p class="contactformcaption"><?php echo wfMsg( 'specialcontact-realname' ); ?></p>
-<?php
-	if( empty($name_readonly) ) :
-		print "<input ". $eclass['wpContactRealName'] ." tabindex='" . ($tabindex++) . "' type='text' name=\"wpContactRealName\" value=\"{$encRealName}\" size='40' />";
-	else :
-		print "{$encRealName} <input  type=\"hidden\" name=\"wpContactRealName\" value=\"{$encRealName}\" />";
-	endif;
-?>
-
-<p class="contactformcaption"><?php echo wfMsg( 'specialcontact-yourmail' ); ?></p>
-<?php
-	if( empty($mail_readonly) ) :
-		print "<input ". $eclass['wpEmail'] ." tabindex='" . ($tabindex++) . "' type='text' name=\"wpEmail\" value=\"{$encEmail}\" size='40' />";
-	else :
-		print "{$encEmail} <input  type=\"hidden\" name=\"wpEmail\" value=\"{$encEmail}\" />";
-	endif;
-?>
+<p>
+<label for="wpEmail"><?= wfMsg( 'specialcontact-yourmail' ) ?></label>
+<input name="wpEmail" value="<?= $encEmail ?>" />
+</p>
+<?php } ?>
 
 <p class="contactformcaption"><?php echo wfMsg( 'specialcontact-problem' ); ?></p>
 <?php

extensions/wikia/SpecialContact2/templates/rename-account.tmpl.php
@@ -36,4 +36,3 @@ echo wfMsgExt( 'specialcontact-intro-rename-account', array( 'parse' ) );
 <input type="hidden" id="wpBrowser" name="wpBrowser" value="<?php echo $_SERVER['HTTP_USER_AGENT']; ?>" />
 </form>
 
-<p><?= wfMsgExt( 'specialcontact-noform-footer', array( 'parse' ) ) ?></p>

commit 609c92418e089b9bd03ab66176bc2b885657cd7f

fix undefined variable notice caused by r51607, plus moving CC me message below submit button to match other forms
git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51682 e846bd91-c042-42a7-a218-4b65e2c1904d

extensions/wikia/SpecialContact2/templates/bug-report.tmpl.php
@@ -68,22 +68,20 @@ if( !$isLoggedIn && (isset($captchaForm)) ) {
 }
 ?>
 
-<?php
+<p><input type="submit" value="<?= wfMsg( 'specialcontact-mail' ) ?>" /></p>
 
+<?php
 if( $isLoggedIn && $hasEmail ) {
 	//is user, has email, but is verified?
-	if( $hasEmailConf ) {
+	if ( $hasEmailConf ) {
 		//yes!
-		print "<input tabindex='" . ($tabindex++) . "' type='checkbox' name=\"wgCC\" value=\"1\" />" . wfMsg('specialcontact-ccme');
-	}
-	else
-	{
+		print "<input type=\"checkbox\" name=\"wgCC\" value=\"1\" />" . wfMsg('specialcontact-ccme');
+	} else {
 		//not
 		print "<s><i>" . wfMsg('specialcontact-ccme') . "</i></s><br/> ". wfMsgExt('specialcontact-ccdisabled', array('parse') ) ."";
 	}
 }
 ?>
-<input type="submit" value="<?= wfMsg( 'specialcontact-mail' ) ?>" />
 
 <input type="hidden" id="wpBrowser" name="wpBrowser" value="<?php echo $_SERVER['HTTP_USER_AGENT']; ?>" />
 </form>