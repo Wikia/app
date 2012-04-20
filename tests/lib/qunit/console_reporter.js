c6c12934922a95df70d121d04013fab7c68afb72
mech <mech@wikia-inc.com>
2012-04-20 11:04:47 +0000

    JS unit testing
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51645 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/lib/qunit/console_reporter.js
+++ tests/lib/qunit/console_reporter.js
@@ -5,22 +5,43 @@
  */
 QUnit.testStart = function(data){
 	console.log('Test running: ', data.module, '::', data.name);
+	console.log(JSON.stringify({
+		command:'startTest',
+		name: data.name
+	}));
+    QUnit.reporter_messages = [];
+    QUnit.reporter_assertions = 0;
+
 };
 
 QUnit.testDone = function(data){
 	console.log('Test completed', data.module, '::', data.name, '(', data.failed, 'failed and', data.passed,'passed on', data.total, 'total)' );
+		
+	console.log(JSON.stringify({
+		command:'stopTest',
+		status: data.failed == 0 ? 'SUCCESS' : 'FAILURE',
+		assertions: QUnit.reporter_assertions,
+		messages: QUnit.reporter_messages.join('\n')
+	}));
 };
 
 QUnit.log = function(data){
+    QUnit.reporter_assertions++;
+    if (!data.result) QUnit.reporter_messages.push(data.message);
 	console.log('Assert:', data.message, 'is', data.result);
 };
 
 QUnit.moduleStart = function(data){
 	console.log('Module running:', data.name);
+	console.log(JSON.stringify({
+		command:'startSuite',
+		name: data.name
+	}));
 };
 
 QUnit.moduleDone = function(data){
 	console.log('Module completed', data.name, '(', data.failed, 'failed and', data.passed,'passed on', data.total, 'total)' );
+	console.log(JSON.stringify({command:'stopSuite'}));
 };
 
 QUnit.begin = function(){
@@ -29,5 +50,5 @@ QUnit.begin = function(){
 
 QUnit.done = function(data){
 	console.log('End -', data.failed, 'failed and', data.passed,'passed on', data.total, 'total in', data.runtime, 'milliseconds');
-	console.log('!!PHANTOM_EXIT!!');
+	console.log(JSON.stringify({command:'PHANTOM_EXIT'}));
 };
