d338613234322f3c18febe0f5f5774d6737fbc0c
will <will@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-23 21:11:07 +0000

    BugID 28736: remove movieclips as source of premium videos to ingest
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51732 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoHandlers/VideoHandlers.setup.php
+++ extensions/wikia/VideoHandlers/VideoHandlers.setup.php
@@ -148,7 +148,6 @@ $wgMediaHandlers['video/youtube'] = 'YoutubeVideoHandler';
  * Feed ingesters
  */
 $app->registerClass('VideoFeedIngester', $dir . '/feedingesters/VideoFeedIngester.class.php');
-$app->registerClass('MovieclipsFeedIngester', $dir . '/feedingesters/MovieclipsFeedIngester.class.php');
 $app->registerClass('RealgravityFeedIngester', $dir . '/feedingesters/RealgravityFeedIngester.class.php');
 $app->registerClass('ScreenplayFeedIngester', $dir . '/feedingesters/ScreenplayFeedIngester.class.php');
 
