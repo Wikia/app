68d482eb16e0ff84fdafd579eeb11d30cf4d4f68
pbablok <pbablok@wikia-inc.com>
2012-04-24 16:19:13 +0000

    VideoRefactoring: scripts update
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51769 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/vm66_migrate_missed_wikis.sh
+++ maintenance/wikia/VideoHandlers/vm66_migrate_missed_wikis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#scriptpath="../../../../../home/release/video_refactoring/trunk/maintenance/wikia/VideoHandlers"
-scriptpath="../../../../../usr/wikia/mac/maintenance/wikia/VideoHandlers"
+scriptpath="../../../../../home/release/video_refactoring/trunk/maintenance/wikia/VideoHandlers"
+#scriptpath="../../../../../usr/wikia/mac/maintenance/wikia/VideoHandlers"
 
 echo "11551	11551	azumanga
 22850	22850	casemod
@@ -12,10 +12,10 @@ echo "11551	11551	azumanga
 	echo "Processing $line ($cityid, $dbname)"
 
 	#/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoReset.php" --usedb=$dbname
-	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoSanitize.php" --usedb=$dbname | tee -a logs/${cityid}.sanitize.log
-	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoPremigrate.php" --usedb=$dbname | tee -a logs/${cityid}.premigrate.log
-	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoMigrateData.php" --usedb=$dbname | tee -a logs/${cityid}.migratedata.log
-	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoPostmigrate.php" --usedb=$dbname | tee -a logs/${cityid}.postmigrate.log
+	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoSanitize.php" --usedb=$dbname | tee -a logs/${cityid}.sanitize.log || die
+	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoPremigrate.php" --usedb=$dbname | tee -a logs/${cityid}.premigrate.log || die
+	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoMigrateData.php" --usedb=$dbname | tee -a logs/${cityid}.migratedata.log || die
+	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoPostmigrate.php" --usedb=$dbname | tee -a logs/${cityid}.postmigrate.log || die
 
 	echo "---"
 done
