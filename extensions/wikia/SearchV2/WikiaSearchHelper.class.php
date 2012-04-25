521cf30753f97a0979848a53c11bcca9ac1e0c7b
liz <liz@wikia-inc.com>
2012-04-25 22:17:46 +0000

    (BugId:29262) Adding commas to numbers using wgLang->formatNum
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51842 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchV2/WikiaSearchHelper.class.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-class WikiaSearchHelper {
-	
-	public function formatNumber($num, $decimals=0) {
-		return number_format($num, $decimals, wfMsg('wikiasearch2-decimal-point'), wfMsg('wikiasearch2-thousands-separator'));
-	}
-}
