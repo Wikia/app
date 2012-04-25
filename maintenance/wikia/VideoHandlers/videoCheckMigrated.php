2e85d061626f252840eb648d017d9c3a92baa1a8
pbablok <pbablok@wikia-inc.com>
2012-04-25 16:14:35 +0000

    VideoRefactoring: check migration status of all wikis
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51833 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/VideoHandlers/videoCheckMigrated.php
@@ -0,0 +1,43 @@
+<?php
+ini_set( 'display_errors', 'stdout' );
+$options = array('help');
+
+require_once( '../../commandLine.inc' );
+
+global $IP, $wgCityId, $wgExternalDatawareDB;
+
+#$IP = '/home/pbablok/video/VideoRefactor/'; // HACK TO RUN ON SANDBOX
+echo( "Check migration for $wgCityId\n" );
+
+$dbr = wfGetDB( DB_SLAVE );
+$dbw_dataware = wfGetDB( DB_MASTER, array(), $wgExternalDatawareDB );
+
+$rows = $dbr->select('image',
+	array( 'img_name' ),
+	array( "img_name LIKE ':%'" ),
+	__METHOD__
+);
+
+$rowCount = $rows->numRows();
+
+$dbr->freeResult($rows);
+
+echo(": {$rowCount} old videos found\n");
+
+$v = WikiFactory::getVarByName('wgVideoHandlersVideosMigrated', $wgCityId);
+
+//if($rowCount && empty($v)) {
+if(true) {
+	$dbw_dataware->insert(
+		'video_notmigrated',
+		array(
+			'wiki_id' => $wgCityId,
+			'wiki_name' => WikiFactory::getWikiByID($wgCityId)->city_url,
+			'video_count' => $rowCount
+		)
+	);
+	echo("$wgCityId has videos but was not migrated!\n");
+}
+
+
+?>
