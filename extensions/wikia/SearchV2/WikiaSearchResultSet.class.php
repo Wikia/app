6d9af8526053dba63ebae31a568ba432db768264
adi <adi@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-25 16:37:36 +0000

    SearchV2: 1) code cleanup 2) profiling added
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51836 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchV2/WikiaSearchResultSet.class.php
+++ extensions/wikia/SearchV2/WikiaSearchResultSet.class.php
@@ -31,9 +31,12 @@ class WikiaSearchResultSet implements Iterator {
 	 * @param array $results list of WikiaResult or WikiaResultSet (for result grouping) objects
 	 */
 	public function setResults(Array $results) {
+		wfProfileIn(__METHOD__);
+
 		foreach($results as $result) {
 			$this->addResult($result);
 		}
+		wfProfileOut(__METHOD__);
 	}
 
 	public function setResultsFound($value) {
@@ -183,6 +186,8 @@ class WikiaSearchResultSet implements Iterator {
 	 * @param WikiaSearchResultSet $resultSet
 	 */
 	public function merge(WikiaSearchResultSet $resultSet) {
+		wfProfileIn(__METHOD__);
+
 		foreach($resultSet as $result) {
 			$this->addResult($result);
 		}
@@ -192,6 +197,8 @@ class WikiaSearchResultSet implements Iterator {
 		if($resultSet->isComplete()) {
 			$this->markAsComplete();
 		}
+
+		wfProfileOut(__METHOD__);
 	}
 
 	public function __sleep() {
