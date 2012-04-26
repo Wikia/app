f8d7ea529f6ca86972e4cd889d3e06ebb7999f34
liz <liz@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-26 21:18:55 +0000

    (BugId:29268) adding tab styles to search options
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51898 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Paginator/templates/paginator.tmpl.php
+++ extensions/wikia/Paginator/templates/paginator.tmpl.php
@@ -1,23 +1,21 @@
 <div  class="wikia-paginator">
-	<div>
-		<ul>
-		<?	$i = 1;
-			foreach($pages as $page ){
-			?>
-				<? if ( ( $i === 1) && ( $page < $currentPage ) ){
-					?><li><a href="<?=str_replace( '%s', ( $currentPage - 1 ), $url); ?>" data-back="true" data-page="<?=$currentPage - 1; ?>" class="button secondary paginator-prev"></a></li><?;
-				} ?>
-				<? if ( $page === '' ){
-					echo '<li><span class="paginator-spacer">...</span></li>';
-				} else {
-					?><li><a href="<?=str_replace( '%s', $page, $url); ?>" <? if( $currentPage > $page ) echo 'data-back="true"'; ?>  data-page="<?=$page; ?>" class="paginator-page<? if ( $page == $currentPage ) echo ' active'; ?>" ><?=$page; ?></a></li><?
-				} ?>
-				<? if ( ( $i === count($pages)) && ( $page > $currentPage ) ){
-					?><li><a href="<?=str_replace( '%s', ( $currentPage + 1 ), $url); ?>" data-page="<?=$currentPage + 1; ?>" class="button secondary paginator-next"></a></li><?;
-				} ?>
-			<?
-			$i++;
+	<ul>
+	<?	$i = 1;
+		foreach($pages as $page ){
+		?>
+			<? if ( ( $i === 1) && ( $page < $currentPage ) ){
+				?><li><a href="<?=str_replace( '%s', ( $currentPage - 1 ), $url); ?>" data-back="true" data-page="<?=$currentPage - 1; ?>" class="button secondary paginator-prev"></a></li><?;
 			} ?>
-		</ul>
-	</div>
+			<? if ( $page === '' ){
+				echo '<li><span class="paginator-spacer">...</span></li>';
+			} else {
+				?><li><a href="<?=str_replace( '%s', $page, $url); ?>" <? if( $currentPage > $page ) echo 'data-back="true"'; ?>  data-page="<?=$page; ?>" class="paginator-page<? if ( $page == $currentPage ) echo ' active'; ?>" ><?=$page; ?></a></li><?
+			} ?>
+			<? if ( ( $i === count($pages)) && ( $page > $currentPage ) ){
+				?><li><a href="<?=str_replace( '%s', ( $currentPage + 1 ), $url); ?>" data-page="<?=$currentPage + 1; ?>" class="button secondary paginator-next"></a></li><?;
+			} ?>
+		<?
+		$i++;
+		} ?>
+	</ul>
 </div>
