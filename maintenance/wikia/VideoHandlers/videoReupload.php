8958b72e6c3158ff0b8e33ec5e97f05fb1e75ad2
jacek <jacek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-26 14:01:38 +0000

    video reupload maintenance script
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51872 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/videoReupload.php
+++ maintenance/wikia/VideoHandlers/videoReupload.php
@@ -8,49 +8,16 @@ ini_set( 'display_errors', 'stdout' );
 $options = array('help');
 
 require_once( '../../commandLine.inc' );
-require_once( 'premigrate.class.php' );
-require_once( 'videolog.class.php' );
-
-function adjustThumbnailToVideoRatio( $upload, $ratio ){
-	if(empty($ratio)) {
-		$ratio = 16/9;
-	}
-
-	$data = file_get_contents( $upload->getTempPath() );
-	$src = imagecreatefromstring( $data );
-
-	$orgWidth = $upload->mFileProps['width'];
-	$orgHeight = $upload->mFileProps['height'];
-	$finalWidth = $upload->mFileProps['width'];
-	$finalHeight = $finalWidth / $ratio;
-
-	$dest = imagecreatetruecolor ( $finalWidth, $finalHeight );
-	imagecopy( $dest, $src, 0, 0, 0, ( $orgHeight - $finalHeight ) / 2 , $finalWidth, $finalHeight );
-
-	$sTmpPath = $upload->getTempPath();
-	switch ( $upload->mFileProps['minor_mime'] ) {
-		case 'jpeg':	imagejpeg( $dest, $sTmpPath ); break;
-		case 'gif':	imagegif ( $dest, $sTmpPath ); break;
-		case 'png':	imagepng ( $dest, $sTmpPath ); break;
-	}
-
-	imagedestroy( $src );
-	imagedestroy( $dest );
-
-}
 
 global $wgCityId, $wgExternalDatawareDB;
 
 echo( "Video Migration script running for $wgCityId\n" );
-videoLog( 'reupload', 'START', '');
+
 
 if( isset( $options['help'] ) && $options['help'] ) {
 	echo( "Usage: php VideoReupload.php\n" );
 	exit( 0 );
 }
-
-$defaultThumbnailUrl = 'http://community.wikia.com/extensions/wikia/VideoHandlers/images/BrokenVideo_Icon_Large.png'; // TODO: CHANGEME
-
 
 //@include( "$IP/extensions/wikia/VideoHandlers/VideoHandlers.setup.php" );
 
@@ -63,7 +30,14 @@ echo( "Loading list of videos to process\n" );
 
 $rows = $dbw->select( 'image',
 		array( 'img_name', 'img_metadata', 'img_major_mime', 'img_minor_mime' ),
-		array( " img_major_mime='video' AND img_name NOT LIKE ':%' AND img_minor_mime='viddler' AND img_name='Danny_Jacobs_Interview' "),
+		array( " img_major_mime='video'
+			AND img_name NOT LIKE ':%'
+			AND (
+				img_minor_mime IN ('viddler','gametrailers','sevenload','southparkstudios')
+				OR
+				(img_width=0 AND img_height=0)
+			    )
+			 "),
 		__METHOD__
 );
 
@@ -90,15 +64,14 @@ if( $rowCount ) {
 
 		$provider = $videoRow->img_minor_mime;
 		$apiWrapperName = ucfirst( $videoRow->img_minor_mime ) . 'ApiWrapper';
-		//$videoId = is_subclass_of($apiWrapperName, 'PseudoApiWrapper') ? $videoRow->img_name : $metadata['videoId'];
 		$videoId = $metadata['videoId'];
 		$result = null;
 		$status = "fail";
 
+		echo "reuploading: [from:".$provider." videoId:$videoId] - ".$videoRow->img_name."\n";
+		echo "title: " . $title . "\n";
+
 		try {
-			echo "reuploading: [from:".$provider." videoId:$videoId] - ".$videoRow->img_name."\n";
-			echo "title: " . $title . "\n";
-			//$result = VideoFileUploader::uploadVideo($provider, $videoId, $title, null, true);
 			$oUploader = new VideoFileUploader();
 			$oUploader->setProvider( $provider );
 			$oUploader->setVideoId( $videoId );
@@ -107,36 +80,11 @@ if( $rowCount ) {
 			$oUploader->hideAction();
 			$oUploader->upload( $title );
 
-			//var_dump($result);
-
 		} catch( Exception $e ) {
+
 			echo ("ERROR FETCHING DATA: [{$videoRow->img_minor_mime}]". $videoRow->img_name . "\n");
 			echo $e->getMessage() ."\n";
 		}
-
-
-		/*
-		$apiWrapper = null;
-		try {
-			$apiWrapperName = ucfirst( $videoRow->img_minor_mime ) . 'ApiWrapper';
-
-			if(is_subclass_of($apiWrapperName, 'PseudoApiWrapper')) {
-				$apiWrapper = F::build( $apiWrapperName, array( $videoRow->img_name ) );
-			} else {
-				$apiWrapper = F::build( $apiWrapperName, array( $metadata['videoId'] ) );
-			}
-
-			$meta = $apiWrapper->getMetadata();
-			$thumbanilUrl = $apiWrapper->getThumbnailUrl();
-
-			echo "Thumbnail: {$thumbanilUrl} \n";
-		}
-		catch( Exception $e ) {
-			echo ("ERROR FETCHING DATA: [{$videoRow->img_minor_mime}]". $videoRow->img_name . "\n");
-			break;
-		}
-		*/
-
 
 		echo("\n ========================================== \n");
 	}
@@ -148,9 +96,6 @@ $dbw_dataware->freeResult($rows);
 echo(": {$rowCount} videos processed.\n");
 
 echo "Done\n";
-
-videoLog( 'reupload', 'STOP', "");
-
 
 wfWaitForSlaves( 2 );
 ?>
