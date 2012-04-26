2c4c181b67634a203752875a36debbe5a08bab8c
liz <liz@wikia-inc.com>
2012-04-26 19:38:17 +0000

    (BugId:19669) Updating paginator to use UISG pagination styles and (BugId:29265) pagination styles for search v2
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51896 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Paginator/templates/paginator.tmpl.php
+++ extensions/wikia/Paginator/templates/paginator.tmpl.php
@@ -5,15 +5,15 @@
 			foreach($pages as $page ){
 			?>
 				<? if ( ( $i === 1) && ( $page < $currentPage ) ){
-					?><li><a href="<?=str_replace( '%s', ( $currentPage - 1 ), $url); ?>" data-back="true" data-page="<?=$currentPage - 1; ?>" class="paginator-page special paginator-prev"><?=wfMsg('paginator-back'); ?></a></li><?;
+					?><li><a href="<?=str_replace( '%s', ( $currentPage - 1 ), $url); ?>" data-back="true" data-page="<?=$currentPage - 1; ?>" class="button secondary paginator-prev"></a></li><?;
 				} ?>
 				<? if ( $page === '' ){
-					echo '<li><a class="paginator-spacer">...</a></li>';
+					echo '<li><span class="paginator-spacer">...</span></li>';
 				} else {
 					?><li><a href="<?=str_replace( '%s', $page, $url); ?>" <? if( $currentPage > $page ) echo 'data-back="true"'; ?>  data-page="<?=$page; ?>" class="paginator-page<? if ( $page == $currentPage ) echo ' active'; ?>" ><?=$page; ?></a></li><?
 				} ?>
 				<? if ( ( $i === count($pages)) && ( $page > $currentPage ) ){
-					?><li><a href="<?=str_replace( '%s', ( $currentPage + 1 ), $url); ?>" data-page="<?=$currentPage + 1; ?>" class="paginator-page special no-border paginator-next"><?=wfMsg('paginator-next'); ?></a></li><?;
+					?><li><a href="<?=str_replace( '%s', ( $currentPage + 1 ), $url); ?>" data-page="<?=$currentPage + 1; ?>" class="button secondary paginator-next"></a></li><?;
 				} ?>
 			<?
 			$i++;
--- extensions/wikia/SearchV2/templates/WikiaSearch_pagination.php
+++ extensions/wikia/SearchV2/templates/WikiaSearch_pagination.php
@@ -20,21 +20,23 @@
 		}
 	}
 ?>
-	<div class="pagination">
-		<?php if( $windowFirstPage > 1 ): ?>
-			<a href="<?= $pageTitle->getFullUrl( array_merge( array( 'query' => $query, 'page' => ($windowFirstPage-1) ), $extraParams ) ); ?>">...</a>
-		<?php endif; ?>
-		<?php for( $i = $windowFirstPage; $i <= $windowLastPage; $i++ ): ?>
-		
-			<?php if($i == $currentPage): ?>
-				<?=$i;?>&nbsp;
-			<?php else: ?>
-				<a href="<?= $pageTitle->getFullUrl( array_merge( array( 'query' => $query, 'page' => $i ), $extraParams ) ); ?>"><?=$i;?></a>&nbsp;
-			<?php endif;?>
-		<?php endfor; ?>
-		
-		<?php if( $windowLastPage < $pagesNum ): ?>
-			<a href="<?= $pageTitle->getFullUrl( array_merge( array( 'query' => $query, 'page' => $i ), $extraParams ) ); ?>">...</a>
-		<?php endif; ?>
+	<div class="wikia-paginator">
+		<ul>
+			<?php if( $currentPage > 1 ): ?>
+				<li><a class="paginator-prev button secondary" href="<?= $pageTitle->getFullUrl( array_merge( array( 'query' => $query, 'page' => ($currentPage-1) ), $extraParams ) ); ?>"></a></li>
+			<?php endif; ?>
+			<?php for( $i = $windowFirstPage; $i <= $windowLastPage; $i++ ): ?>
+			
+				<?php if($i == $currentPage): ?>
+					<li><span class="active paginator-page"><?=$i;?></span></li>
+				<?php else: ?>
+					<li><a class="paginator-page" href="<?= $pageTitle->getFullUrl( array_merge( array( 'query' => $query, 'page' => $i ), $extraParams ) ); ?>"><?=$i;?></a></li>
+				<?php endif;?>
+			<?php endfor; ?>
+			
+			<?php if( $windowLastPage < $pagesNum ): ?>
+				<li><a class="paginator-next button secondary" href="<?= $pageTitle->getFullUrl( array_merge( array( 'query' => $query, 'page' => ($currentPage+1) ), $extraParams ) ); ?>"></a></li>
+			<?php endif; ?>
+		</ul>
 	</div>
 <?php endif; ?>	
