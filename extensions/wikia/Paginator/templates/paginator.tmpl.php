cc42bf8b06968c55dcd780d8699fb0a040b055d2
liz <liz@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-04-27 17:43:58 +0000

    fixing hard coded messages in pagination
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@51950 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Paginator/templates/paginator.tmpl.php
+++ extensions/wikia/Paginator/templates/paginator.tmpl.php
@@ -4,7 +4,7 @@
 		foreach($pages as $page ){
 		?>
 			<? if ( ( $i === 1) && ( $page < $currentPage ) ){
-				?><li><a href="<?=str_replace( '%s', ( $currentPage - 1 ), $url); ?>" data-back="true" data-page="<?=$currentPage - 1; ?>" class="button secondary paginator-prev"><span>Previous</span></a></li><?;
+				?><li><a href="<?=str_replace( '%s', ( $currentPage - 1 ), $url); ?>" data-back="true" data-page="<?=$currentPage - 1; ?>" class="button secondary paginator-prev"><span><?=wfMsg('paginator-back')?></span></a></li><?;
 			} ?>
 			<? if ( $page === '' ){
 				echo '<li><span class="paginator-spacer">...</span></li>';
@@ -12,7 +12,7 @@
 				?><li><a href="<?=str_replace( '%s', $page, $url); ?>" <? if( $currentPage > $page ) echo 'data-back="true"'; ?>  data-page="<?=$page; ?>" class="paginator-page<? if ( $page == $currentPage ) echo ' active'; ?>" ><?=$page; ?></a></li><?
 			} ?>
 			<? if ( ( $i === count($pages)) && ( $page > $currentPage ) ){
-				?><li><a href="<?=str_replace( '%s', ( $currentPage + 1 ), $url); ?>" data-page="<?=$currentPage + 1; ?>" class="button secondary paginator-next"><span>Next</span></a></li><?;
+				?><li><a href="<?=str_replace( '%s', ( $currentPage + 1 ), $url); ?>" data-page="<?=$currentPage + 1; ?>" class="button secondary paginator-next"><span><?=wfMsg('paginator-next')?></span></a></li><?;
 			} ?>
 		<?
 		$i++;
