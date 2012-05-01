7a410a2465e19a2723502bd6047d3f56b78e3449
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-01 17:45:35 +0000

    Removing previous A/B Testing extension (the one for edit buttons, history dropdown, etc.) to replace with the upcoming general-purpose framework that will use the data warehouse for the backend.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52024 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ABtesting/js/editbutton2.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	var button = $("#WikiaPageHeader").find(".wikia-button");
-	var icon = button.find(".sprite").detach();
-	
-	button.before(icon).show();
-})();
--- extensions/wikia/ABtesting/js/editbutton4.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var action = '?action=history';
-	if (document.location.search) {
-		action = '&action=history';
-	}
-	$("#WikiaPageHeader").find("details").before('<a href="' + document.location.href + action + '" class="historyLink">History</a>');
-
-	
-})();
