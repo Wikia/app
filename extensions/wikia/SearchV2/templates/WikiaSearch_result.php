77747d6d887db4f101e2bf6c6e5c3914fe8fb0ee
liz <liz@wikia-inc.com>
2012-05-01 01:28:30 +0000

    (BugId:29797) Adding images to search results
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52018 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchV2/templates/WikiaSearch_result.php
+++ extensions/wikia/SearchV2/templates/WikiaSearch_result.php
@@ -2,34 +2,42 @@
 <?php if(empty($inGroup)): ?>
 	<section class="Result">
 <?php endif; ?>
+
 <article>
-<header>
-<h1>
-	<?php $title = ( empty($inGroup) && $isInterWiki ) ? str_replace('$1', $result->getTitle(), $result->getVar('wikititle')) : $result->getTitle(); ?>
-	<?= $debug ? $pos.'. ' : ''; ?><a href="<?= $result->getUrl(); ?>"><?= $title ?></a>
-</h1>
-</header>
 
-<?php if($debug): ?>
-	<i>[<?php if(!empty($rank)): ?><font color="red">WikiRank: <?=$rank;?></font> | <?php endif; ?>Score: <?=$result->score?>]</i>
-<?php endif; ?>
-
-<?= $result->getText(); ?>
-
-<?php if(empty($inGroup)): ?>
-	<nav>
-		<ul>
-			<li><a href="<?= $result->getUrl(); ?>"><?=$result->getUrl();?></a></li>
-			<?php if($isInterWiki): ?>
-				<li><a href="<?= $result->getVar('cityHost') .'/wiki/Special:Search?search='.urlencode($query).'&fulltext=Search'; ?>"><?= wfMsg( 'wikiasearch2-search-on-wiki') ?></a></li>
-			<?php endif; ?>
-		</ul>
-	</nav>
-<?php endif; ?>
-
-<?php if($debug): ?>
-	<i>[id: <?=$result->getId();?>, text_relevance: <?=$result->getVar('text_relevance', '?');?>, backlinks: <?=$result->getVar('backlinks', '?');?>]</i><br />
-<?php endif; //debug ?>
+	<?php if($result->getThumbnail() != null): ?>
+		<figure>
+			<?= $result->getThumbnail()->toHtml(); ?>
+		</figure>
+	<?php endif; ?>
+	
+	<header>
+	<h1>
+		<?php $title = ( empty($inGroup) && $isInterWiki ) ? str_replace('$1', $result->getTitle(), $result->getVar('wikititle')) : $result->getTitle(); ?>
+		<?= $debug ? $pos.'. ' : ''; ?><a href="<?= $result->getUrl(); ?>"><?= $title ?></a>
+	</h1>
+	</header>
+	
+	<?php if($debug): ?>
+		<i>[<?php if(!empty($rank)): ?><font color="red">WikiRank: <?=$rank;?></font> | <?php endif; ?>Score: <?=$result->score?>]</i>
+	<?php endif; ?>
+	
+	<?= $result->getText(); ?>
+	
+	<?php if(empty($inGroup)): ?>
+		<nav>
+			<ul>
+				<li><a href="<?= $result->getUrl(); ?>"><?=$result->getUrl();?></a></li>
+				<?php if($isInterWiki): ?>
+					<li><a href="<?= $result->getVar('cityHost') .'/wiki/Special:Search?search='.urlencode($query).'&fulltext=Search'; ?>"><?= wfMsg( 'wikiasearch2-search-on-wiki') ?></a></li>
+				<?php endif; ?>
+			</ul>
+		</nav>
+	<?php endif; ?>
+	
+	<?php if($debug): ?>
+		<i>[id: <?=$result->getId();?>, text_relevance: <?=$result->getVar('text_relevance', '?');?>, backlinks: <?=$result->getVar('backlinks', '?');?>]</i><br />
+	<?php endif; //debug ?>
 
 </article>
 
