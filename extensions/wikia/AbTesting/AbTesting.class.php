71a8cbdf160736043587951a84b1b4e8039e097f
sean <sean@wikia-inc.com>
2012-05-02 00:21:02 +0000

    Made code to embed a minified version of getTestGroup() into the head scripts of every page. Currently just a stub. BugzId 29832
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52050 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AbTesting/AbTesting.class.php
+++ extensions/wikia/AbTesting/AbTesting.class.php
@@ -15,11 +15,37 @@ class AbTesting {
 	 * @return boolean return true to tell other functions on the same hook to continue executing
 	 */
 	public static function onSkinGetHeadScripts($scripts) {
+		wfProfileIn( __METHOD__ );
 
 		// TODO: EMBED THE EXPERIMENT CONFIG IN HERE (use memcached for the generated json string).
 		$scripts .= "\n\n<!-- A/B TESTING! -->\n";
 //		$scripts .= Html::inlineScript("var wgNow = new Date();");
 
+		// NOTE: This is embedded instead of being an extra request because it needs to be done this early on the page (and external blocking-requests are time-consuming).
+		$scripts .= "\n\n<!-- A/B Testing getTestGroup() -->\n";
+		$scripts .= Html::inlineScript( AbTesting::getJsFunction() )."\n";
+
+		wfProfileOut( __METHOD__ );
 		return true;
 	}
+	
+	/**
+	 * Returns the javascript for the getTestGroup() function as a string.
+	 */
+	public static function getJsFunction(){
+		ob_start();
+		
+// TODO: WRITE THE BODY OF THE FUNCTION!
+		?>function getTestGroup(){
+			
+		}
+		<?php
+		$jsString = ob_get_clean();
+
+		// We're embedding this in every page, so minify it.
+		$jsString = AssetsManagerBaseBuilder::minifyJs( $jsString );
+		
+		return $jsString;
+	} // end getJsFunction()
+
 }
