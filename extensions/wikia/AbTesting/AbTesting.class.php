7033d8c19864c1edcb29245c4fff5c45b7c39799
sean <sean@wikia-inc.com>
2012-05-02 00:44:05 +0000

    Started the code to protect against accidental calls to getTestGroup() before beacon is set. BugzId 29835
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52052 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AbTesting/AbTesting.class.php
+++ extensions/wikia/AbTesting/AbTesting.class.php
@@ -11,6 +11,9 @@ class AbTesting {
 	/**
 	 * Add inline JS in <head> section
 	 *
+	 * NOTE: This is embedded instead of being an extra request because it needs to be done this early
+	 * on the page (and external blocking-requests are time-consuming).
+	 *
 	 * @param string $scripts inline JS scripts
 	 * @return boolean return true to tell other functions on the same hook to continue executing
 	 */
@@ -20,7 +23,6 @@ class AbTesting {
 		// Config for experiments and treatment groups.
 		$scripts .= "\n\n<!-- A/B Testing code -->\n";
 
-		// NOTE: This is embedded instead of being an extra request because it needs to be done this early on the page (and external blocking-requests are time-consuming).
 		$js = AbTesting::getJsExperimentConfig() . "\n" . AbTesting::getJsFunction();
 
 		$scripts .= Html::inlineScript( $js )."\n";
@@ -50,7 +52,19 @@ class AbTesting {
 		
 // TODO: WRITE THE BODY OF THE FUNCTION!
 		?>function getTestGroup(){
-			
+			var treatmentGroup = "";
+			if(typeof beacon_id == "undefined"){
+				if (typeof console != 'undefined') {
+					console.log("DO NOT CALL getTestGroup() BEFORE THE BEACON/PAGE-VIEW CALL! Experiment is broken (will fall back to control group).");
+				}
+				
+				// TODO: SET IT TO USE THE CONTROL GROUP.
+				
+			}
+
+			// TODO: LOAD THE CORRECT GROUP (or control if there is no beacon_id).
+
+			return treatmentGroup;
 		}
 		<?php
 		$jsString = ob_get_clean();
