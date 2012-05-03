d091780ab8a87ef7c67ebab0c18bc379896bda1f
liz <liz@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-03 17:17:45 +0000

    (BugId:30024) duplicating ImageLightbox extension into Lightbox
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52142 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Lightbox/Lightbox_setup.php
+++ extensions/wikia/Lightbox/Lightbox_setup.php
@@ -1,37 +1,45 @@
 <?php
- 
-/**
-* Lightbox
-*
-* Lightboxes display media in articles and various modules throught the site. 
-*
-* @author Liz Lee <liz@wikia-inc.com>
-* @date 2012-05-02
-* @copyright Copyright (C) 2012 Wikia Inc.
-* @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
-* @package MediaWiki
-*
-*/
-
-// Extension credits
 $wgExtensionCredits['other'][] = array(
 	'name' => 'Lightbox',
 	'version' => '1.0',
-	'url' => 'http://www.wikia.com/wiki/c:help:Help:Lightbox',
-	'author' => array( 'Liz Lee', 'Hyun Lim' ),
-	'descriptionmsg' => 'lightbox-desc',
+	'description' => 'Add lightbox preview for images within article',
+	'author' => array('Maciej Brencz', '[http://www.wikia.com/wiki/User:Marooned Maciej Błaszkowski (Marooned)]', 'Liz Lee', 'Hyun Lim'),
 );
 
+// register extension class
 $dir = dirname(__FILE__);
-
-// Interface code
-include("$dir/Lightbox.php");
-
-// autoloaded classes
 $wgAutoloadClasses['Lightbox'] = "$dir/Lightbox.class.php";
+
+// hooks
+$wgHooks['MakeGlobalVariablesScript'][] = 'Lightbox::addJSVariable';
 
 // i18n
 $wgExtensionMessagesFiles['Lightbox'] = $dir.'/Lightbox.i18n.php';
 
-// hooks
-$wgHooks['MakeGlobalVariablesScript'][] = 'Lightbox::addJSVariable';
+// Ajax dispatcher
+$wgAjaxExportList[] = 'LightboxAjax';
+function LightboxAjax() {
+	global $wgRequest;
+	$method = $wgRequest->getVal('method', false);
+
+	if (method_exists('Lightbox', $method)) {
+		wfProfileIn(__METHOD__);
+
+		$data = Lightbox::$method();
+
+		if (is_array($data)) {
+			// send array as JSON
+			$json = Wikia::json_encode($data);
+			$response = new AjaxResponse($json);
+			$response->setContentType('application/json; charset=utf-8');
+		}
+		else {
+			// send text as text/html
+			$response = new AjaxResponse($data);
+			$response->setContentType('text/html; charset=utf-8');
+		}
+
+		wfProfileOut(__METHOD__);
+		return $response;
+	}
+}
