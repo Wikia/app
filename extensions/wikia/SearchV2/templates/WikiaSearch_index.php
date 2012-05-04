35ad5c5820586543ad2d99497968901b3f856a1c
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-04 15:06:25 +0000

    removed unused messages, corrected capitalization, etc. BugzId:28793
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52212 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchV2/templates/WikiaSearch_index.php
+++ extensions/wikia/SearchV2/templates/WikiaSearch_index.php
@@ -12,12 +12,12 @@
 		<?php endif; ?>
 		<input type="text" name="query" value="<?=$query;?>" />
 
-		<button type="submit" class="wikia-button" id="search-v2-button" value="<?= wfMsg( 'wikiasearch2-search-btn' ); ?>"><img src="<?= $wg->BlankImgUrl ?>" class="sprite search" height="17" width="21"></button>
+		<button type="submit" class="wikia-button" id="search-v2-button" value="<?= wfMsg( 'searchbutton' ); ?>"><img src="<?= $wg->BlankImgUrl ?>" class="sprite search" height="17" width="21"></button>
 
 		<?php if($debug): ?>
 		<fieldset>
 			<label><input type="checkbox" name="crossWikia" value="1" <?= ( $isInterWiki ? 'checked' : '' ); ?>/> <?= wfMsg( 'wikiasearch2-search-all-wikia' ); ?></label>
-			<label><input type="checkbox" name="debug" value="1" <?= ( $debug ? 'checked' : '' ); ?>/> <?= wfMsg( 'wikiasearch2-debug-mode' ); ?></label>
+			<label><input type="checkbox" name="debug" value="1" <?= ( $debug ? 'checked' : '' ); ?>/> Debug mode</label>
 			
 			<?php if($isInterWiki) : ?>
 				<?php echo $app->renderView('WikiaSearchController', 'boostSettings'); ?>
