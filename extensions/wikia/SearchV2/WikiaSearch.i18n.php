52e3eabddcdb6167766633497728e4b43b57c1bd
siebrand <siebrand@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-05 08:49:44 +0000

    L10n review for SearchV2.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52250 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SearchV2/WikiaSearch.i18n.php
+++ extensions/wikia/SearchV2/WikiaSearch.i18n.php
@@ -7,17 +7,39 @@ $messages['en'] = array(
 	'wikiasearch2-page-title-no-query-interwiki' => 'Search Wikia',
 	'wikiasearch2-page-title-no-query-intrawiki' => 'Search $1',
 	'wikiasearch2-search-all-wikia' => 'Search all of Wikia',
-	'wikiasearch2-results' => '$1 results',
-	'wikiasearch2-pages' => '$1 pages',
+	'wikiasearch2-results' => '$1 {{PLURAL:$1|result|results}}',
+	'wikiasearch2-pages' => '$1 {{PLURAL:$1|page|pages}}',
 	'wikiasearch2-search-on-wiki' => 'Search within this wiki',
-	'wikiasearch2-results-count' => 'About $1 results for $2',
+	'wikiasearch2-results-count' => 'About $1 {{PLURAL:$1|result|results}} for $2',
 	'wikiasearch2-results-for' => 'Results for $1',
 	'wikiasearch2-global-search-headline' => 'Find wikis on Wikia',
 	'wikiasearch2-wiki-search-headline' => 'Search this wiki',
 	'wikiasearch2-advanced-search' => 'Advanced Search Options',
 );
 
-$messages["de"] = array(
+$message['qqq'] = array(
+	'wikiasearch2-page-title-with-query' => 'Page title for search pages when a search term is entered. Parameters:
+* $1 is the search term
+* $2 is the wiki name.',
+	'wikiasearch2-page-title-no-query-interwiki' => 'Default page title for inter-wiki search pages before a search string is entered.',
+	'wikiasearch2-page-title-no-query-intrawiki' => 'Default page title for intra-wiki search pages before a search string is entered.',
+	'wikiasearch2-search-btn' => 'Submit button value for search form.',
+	'wikiasearch2-search-all-wikia' => 'Form input label to indicate the search should be cross-wiki.',
+	'wikiasearch2-results' => 'Results count. Parameters:
+* $1 is the number of results.',
+	'wikiasearch2-pages' => 'Number of pages in the wiki that contains the search result.',
+	'wikiasearch2-search-on-wiki' => 'Link from inter-wiki search results page to perform the same search on the actual wiki.',
+	'wikiasearch2-results-count' => 'Parameters:
+* $1 is the total count for all search results
+* $2 is the search term',
+	'wikiasearch2-results-for' => 'Parameters:
+* $1 is the search term.',
+	'wikiasearch2-global-search-headline' => 'Label for search form input (inter-wiki search).',
+	'wikiasearch2-wiki-search-headline' => 'Label for search form input (intra-wiki search).',
+	'wikiasearch2-advanced-search' => 'This is the header for the section where you can choose to search by different criteria, for example for advanced searches.',
+);
+
+$messages['de'] = array(
 	'wikiasearch2-page-title-with-query' => 'Suchergebnisse für "$1" - $2',
 	'wikiasearch2-page-title-no-query-interwiki' => 'Durchsuche Wikia',
 	'wikiasearch2-page-title-no-query-intrawiki' => 'Suche nach $1',
@@ -32,7 +54,7 @@ $messages["de"] = array(
 	'wikiasearch2-advanced-search' => 'Erweiterte Such-Optionen',
 );
 
-$messages["pl"] = array(
+$messages['pl'] = array(
 	'wikiasearch2-page-title-with-query' => 'Wyników dla frazy "$1" - $2',
 	'wikiasearch2-page-title-no-query-interwiki' => 'Przeszukaj Wikię',
 	'wikiasearch2-page-title-no-query-intrawiki' => 'Wyszukaj $1',
@@ -47,12 +69,12 @@ $messages["pl"] = array(
 	'wikiasearch2-advanced-search' => 'Zaawansowane Opcje Wyszukiwania',
 );
 
-$messages["fr"] = array(
-        'wikiasearch2-page-title-with-query' => 'Résultats de recherche pour « $1 » - $2',
-        'wikiasearch2-page-title-no-query-interwiki' => 'Rechercher sur Wikia',
-        'wikiasearch2-page-title-no-query-intrawiki' => 'Rechercher $1',
-        'wikiasearch2-search-all-wikia' => 'Rechercher sur tout Wikia',
-        'wikiasearch2-results' => '$1 résultats',
+$messages['fr'] = array(
+	'wikiasearch2-page-title-with-query' => 'Résultats de recherche pour « $1 » - $2',
+	'wikiasearch2-page-title-no-query-interwiki' => 'Rechercher sur Wikia',
+	'wikiasearch2-page-title-no-query-intrawiki' => 'Rechercher $1',
+	'wikiasearch2-search-all-wikia' => 'Rechercher sur tout Wikia',
+	'wikiasearch2-results' => '$1 résultats',
 	'wikiasearch2-pages' => '$1 pages',
 	'wikiasearch2-search-on-wiki' => 'Rechercher sur ce wiki',
 	'wikiasearch2-results-count' => 'Environ $1 résultats pour « $2 »',
@@ -60,21 +82,4 @@ $messages["fr"] = array(
 	'wikiasearch2-global-search-headline' => 'Trouver des wikis sur Wikia',
 	'wikiasearch2-wiki-search-headline' => 'Rechercher sur ce wiki',
 	'wikiasearch2-advanced-search' => 'Options de recherche avancée',
-);
-
-
-$message['qqq'] = array(
-	'wikiasearch2-page-title-with-query' => 'Page title for search pages when a search term is entered. $1 = search term.  $2 = wiki name.',
-	'wikiasearch2-page-title-no-query-interwiki' => 'Default page title for inter-wiki search pages before a search string is entered.',
-	'wikiasearch2-page-title-no-query-intrawiki' => 'Default page title for intra-wiki search pages before a search string is entered.',
-	'wikiasearch2-search-btn' => 'Submit button value for search form.',
-	'wikiasearch2-search-all-wikia' => 'Form input label to indicate the search should be cross-wiki.',
-	'wikiasearch2-results' => 'Results count. $1 is the number of results.',
-	'wikiasearch2-pages' => 'Number of pages in the wiki that contains the search result.',
-	'wikiasearch2-search-on-wiki' => 'Link from inter-wiki search results page to perform the same search on the actual wiki.',
-	'wikiasearch2-results-count' => '$1 is the total count for all search results, $2 is the search term',
-	'wikiasearch2-results-for' => '$1 is the search term',
-	'wikiasearch2-global-search-headline' => 'Label for search form input (inter-wiki search).',
-	'wikiasearch2-wiki-search-headline' => 'Label for search form input (intra-wiki search).',
-	'wikiasearch2-advanced-search' => 'This is the header for the section where you can choose to search by different criteria, i.e. for advanced searches.',
 );
