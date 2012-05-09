717dd9ecbf2c0cc4bbe8678fa04a952298ae7d5a
sean <sean@wikia-inc.com>
2012-05-09 22:45:11 +0000

    Tweaks after testing the tracking-event-spooling. AB Test appears to be working in dev. BugzId 30745.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52463 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaTracker/js/WikiaTracker.js
+++ extensions/wikia/WikiaTracker/js/WikiaTracker.js
@@ -192,14 +192,6 @@ WikiaTracker.AB = function(page) {
 	return true;
 };
 
-// Now that the code is loaded, if there were any tracking events in the spool from before this file loaded, replay them.
-if (typeof wikiaTrackingSpool !== 'undefined') {
-	wikiaTrackingSpool.forEach(function( eventNameAndData ){
-		$.log('Sending previously-spooled tracking event: ', eventNameAndData);
-		$.internalTrack( eventNameAndData[0], eventNameAndData[1] );
-	});
-}
-
 
 /*
 TODO: REMOVE - Old A/B Testing framework.
