a5763a38dd8ac795548aa522797bbc51573479c7
nandy <nandy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-10 17:19:18 +0000

    merged r51783:r52475; added self service advertising splash special page
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52511 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/SpecialSelfServiceAdvertisingSplash/SelfServiceAdvertisingSplash.i18n.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Internationalisation file for the SelfServiceAdvertisingSplash extension.
+ *
+ * @addtogroup Languages
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'ssa-splash-please-enter-your-name' => 'Please enter your name',
+	'ssa-splash-please-enter-your-email-address' => 'Please enter your valid email address',
+	'ssa-splash-message-sent' => 'Thanks for contacting us. Someone will get back to you soon with your coupon code!',
+	'ssa-splash-message-not-sent-please-try-again' => 'Your message could not be delivered. Please try again!',
+	'ssa-splash-h1' => 'Wikia\'s Self-Service Advertising Platform',
+	'ssa-splash-h2' => 'Reach 40 million global people where they create and collaborate on topics<br />ranging from Video Games to Entertainment and Lifestyle',
+	'ssa-splash-h3' => 'CPM based packages begin at $100.<br />The higher the ad package the lower the CPM.',
+	'ssa-splash-leftbox-powerful-title' => 'Powerful marketing solutions',
+	'ssa-splash-leftbox-powerful' => "* Reach your target\n* Promote your content & build community\n* Control your budget",
+	'ssa-splash-leftbox-available-title' => 'Available ad units',
+	'ssa-splash-leftbox-available' => "* '''728x90''' and '''300x250''' (both above the fold)",
+	'ssa-splash-leftbox-targeting-title' => 'Targeting capabilities',
+	'ssa-splash-leftbox-targeting' => "* '''Run of Category:''' Video Games, Entertainment, Lifestyle\n* '''Wiki-specific:''' Individual wikis such as World of Warcraft, Call of Duty, Harry Potter, Marvel, Star Wars, Camerapedia, etc.\n* '''Geo-Targeting:''' Target by Country",
+	'ssa-splash-leftbox-getstarted' => 'Get Started Now!',
+	'ssa-splash-leftbox-selfservice-url' => 'http://www.wikia.com/Advertising/Self_Service',
+	'ssa-splash-rightbox-title' => 'Want to receive a discount coupon?<br />Sign up now.',
+	'ssa-splash-rightbox-name' => 'Name (Required)',
+	'ssa-splash-rightbox-email' => 'Email (Required)',
+	'ssa-splash-rightbox-company' => 'Company',
+	'ssa-splash-rightbox-phone' => 'Phone no. (xxx) xxx-xxxx',
+	'ssa-splash-rightbox-getcoupon' => 'Get Coupon Now!',
+	'ssa-splash-modal-title' => 'Thank You',
+	'ssa-splash-modal-content' => 'Thanks for contacting us. Someone will get back to you soon with your coupon code!',
+	'ssa-splash-modal-ok' => 'Ok'
+);
---
+++ extensions/wikia/SpecialSelfServiceAdvertisingSplash/tests/SelfServiceAdvertisingSplashControllerTest.php
@@ -0,0 +1,93 @@
+<?php
+
+class SelfServiceAdvertisingSplashControllerTest extends WikiaBaseTest {
+	public function setUp() {
+		$this->setupFile = dirname(__FILE__) . '/../SelfServiceAdvertisingSplash.setup.php';
+		parent::setUp();
+	}
+
+	/**
+	 * @dataProvider sendEmailsDataProvider
+	 */
+	public function testSendEmails($request, $expResponse) {
+		$modelMock = $this->getMock('SelfServiceAdvertisingSplashModel', array('executeSendingFunction','getDstEmails'), array());
+		$modelMock->expects($this->any())->method('executeSendingFunction')->will($this->returnValue(true));
+		$modelMock->expects($this->any())->method('getDstEmails')->will($this->returnValue(array('test@example.com')));
+		$this->mockClass('SelfServiceAdvertisingSplashModel', $modelMock);
+
+		/* @var $response WikiaResponse */
+		$response = $this->app->sendRequest('SelfServiceAdvertisingSplashController', 'sendEmails', $request);
+
+		$this->assertEquals($expResponse,$response->getData());
+	}
+
+	public function sendEmailsDataProvider() {
+		return array(
+			// case1: no email nor name
+			array(
+				'request' => array(
+					'email' => null,
+					'name' => null,
+					'company' => null,
+					'telephone' => null
+				),
+				'expResponse' =>
+				array(
+					"validationResult" => false,
+					"validationMessages" => array(
+						'email' => wfMsg('ssa-splash-please-enter-your-email-address'),
+						'name' => wfMsg('ssa-splash-please-enter-your-name')
+					)
+				)
+			),
+			// case2: email, but no name
+			array(
+				'request' => array(
+					'email' => 'noname@example.com',
+					'name' => null,
+					'company' => null,
+					'telephone' => null
+				),
+				'expResponse' =>
+				array(
+					"validationResult" => false,
+					"validationMessages" => array(
+						'name' => wfMsg('ssa-splash-please-enter-your-name')
+					)
+				)
+			),
+			// case3: no email, but name given
+			array(
+				'request' => array(
+					'email' => null,
+					'name' => 'Noemail',
+					'company' => null,
+					'telephone' => null
+				),
+				'expResponse' =>
+				array(
+					"validationResult" => false,
+					"validationMessages" => array(
+						'email' => wfMsg('ssa-splash-please-enter-your-email-address')
+					)
+				)
+			),
+			// case4: both email and name given
+			array(
+				'request' => array(
+					'email' => 'validemail@example.com',
+					'name' => 'Valid Name',
+					'company' => null,
+					'telephone' => null
+				),
+				'expResponse' =>
+				array(
+					"validationResult" => true,
+					"sendResult" => true,
+					"sendMessage" => wfMsg('ssa-splash-message-sent')
+				)
+			)
+		);
+	}
+}
+
