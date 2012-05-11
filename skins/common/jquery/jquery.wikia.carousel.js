a85056d7bdf89439dfd32b50692e3bd9ee8ffb60
liz <liz@wikia-inc.com>
2012-05-11 23:36:32 +0000

    (BugId:30038) Refactoring LatestPhotos carousel into a jQuery plugin that can be used elsewhere
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52649 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ skins/common/jquery/jquery.wikia.carousel.js
@@ -0,0 +1,154 @@
+/* 
+ * Wikia Carousel jQuery plugin
+ * Author Liz Lee, Hyun Lim
+ * 
+ * TODO: Add DOM examples here
+*/
+
+$.fn.carousel = function(options) {
+	// use just the first element from current collection
+	return this.each(function() {
+		var defaults = {
+			transition_speed: 500,
+			itemCount: 3,
+			nextClass: "next",
+			prevClass: "previous"
+		}
+		
+		options = $.extend(defaults, options);
+		
+		var dom = {
+			wrapper: $(this),
+			carousel: $(this).find('.carousel'),
+			container: $(this).children('div:first'),
+			next: $(this).siblings('.' + options.nextClass),
+			previous: $(this).siblings('.' + options.prevClass)
+		}
+
+		var states = {
+			browsing: false,
+			enable_next: true,
+			enable_previous: false,
+			carousel: false
+		} 
+		
+		
+		// TODO: test this to make sure it works as expected
+		// also probably extract this back into LatestPhotos.js
+		attachBlindImages();
+		
+		// Set up click events
+		dom.next.click(nextImage);
+		dom.previous.click(previousImage);
+
+		// on mouseover load the rest of images
+		dom.wrapper.one('mouseover', function() {
+			lazyLoadImages('rest');
+		});
+		
+		function nextImage() {
+			var width = setCarouselWidth();
+	
+			enableBrowsing();
+	
+			if (states.browsing == false && states.enable_next == true) {
+				states.browsing = true;
+				dom.container.animate({
+					left: '-' + width
+				}, options.transition_speed, function() {
+					removeFirstPhotos();
+					states.browsing = false;
+				});
+			}
+			return false;
+
+		}
+
+		function previousImage() {
+			var width = setCarouselWidth();
+	
+			enableBrowsing();
+	
+			if (states.browsing == false && states.enable_previous == true) {
+				states.browsing = true;
+				var images = $('.carousel li').length;
+				for (var i=0; i < 3; i++) {
+					dom.carousel.prepend( dom.carousel.find('li').eq(images -1) ) ;
+				}
+				dom.container.css('left', - width + 'px');
+	
+				dom.container.animate({
+					left:  '0px'
+				}, options.transition_speed, function() {
+					states.browsing = false;
+				});
+			}
+			return false;
+		}
+
+		function enableBrowsing() {
+			var current = dom.carousel.find('li').slice(0, 3).each(function (i) {
+				if ($(this).is('.last-image')) {
+					states.enable_next = false;
+					return false;
+				}
+				else {
+					states.enable_next = true;
+				}
+	
+				if ($(this).is('.first-image')) {
+					states.enable_previous = false;
+					return false;
+				}
+				else {
+					states.enable_previous = true;
+				}
+			});
+		}
+		
+		function lazyLoadImages(limit) {
+			var images = dom.carousel.find('img').filter('[data-src]');
+			$().log('lazy loading rest of images', 'LatestPhotos');
+	
+			var count = 0;
+			images.each(function() {
+				count ++;
+				if (count > limit) { // exit the loop for init image loading.
+					return false;
+				}
+				var image = $(this);
+				image.
+					attr('src', image.attr('data-src')).
+					removeAttr('data-src');
+			});
+		}
+
+		function setCarouselWidth() {
+			var width = dom.carousel.find('li').outerWidth() * options.itemCount + 6;
+			dom.carousel.css('width', width * dom.carousel.find('li').length + 'px'); // all li's in one line
+			return width;
+		}
+
+		function removeFirstPhotos() {
+			var old = dom.carousel.find('li').slice(0,3);
+			dom.container.css('left', '0px');
+			dom.carousel.find('li').slice(0,3).remove();
+			dom.carousel.append(old);
+	
+		}
+
+		function attachBlindImages() {
+			if (dom.carousel.find('li').length == 5) {
+				dom.carousel.append("<li class='blind'></li>");
+			}
+			else if (dom.carousel.find('li').length == 4) {
+				dom.carousel.append("<li class='blind'></li>");
+				dom.carousel.append("<li class='blind'></li>");
+			}
+	
+			dom.carousel.find('li').first().addClass("first-image");
+			dom.carousel.find('li').last().addClass("last-image");
+		}
+		
+	});
+}
