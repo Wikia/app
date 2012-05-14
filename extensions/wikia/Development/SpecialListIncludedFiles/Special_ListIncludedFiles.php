84d03a554392cae3b5dbb270d9f4ef714d57d3da
sean <sean@wikia-inc.com>
2012-05-14 21:44:26 +0000

    Refactored /extensions/wikia/Development/SpecialListIncludedFiles as part of MW 1.19 upgrade subcase BugzId 31017.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52697 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Development/SpecialListIncludedFiles/Special_ListIncludedFiles.php
+++ extensions/wikia/Development/SpecialListIncludedFiles/Special_ListIncludedFiles.php
@@ -1,36 +1,43 @@
 <?php
+/**
+ * Special page which lists all of the PHP files included at the time. Should help track down what's being included
+ * on a wiki to help debug some strange occurrances and to help minimize unnecessary inclusions.
+ *
+ * Mainly intended for dev use... not because it's private, just because it's not too useful to anyone else.
+ */
 
-$wgExtensionFunctions[] = 'wfSetupListIncludedFiles';
 $wgGroupPermissions['staff']['listincludedfilesright'] = true;
 $dir = dirname(__FILE__) . '/';
 $wgExtensionMessagesFiles['listincludedfiles'] = $dir . 'Special_ListIncludedFiles.i18n.php';
 
+require_once($IP . '/includes/SpecialPage.php');
+$wgSpecialPages['ListIncludedFiles'] = 'ListIncludedFiles';
 
-function wfSetupListIncludedFiles(){
-	global $IP;
-	require_once($IP . '/includes/SpecialPage.php');
-	wfLoadExtensionMessages('listincludedfiles');
-	SpecialPage::addPage(new SpecialPage('ListIncludedFiles', 'listincludedfilesright', true, 'wfListIncludedFiles', false));
-}
+class ListIncludedFiles extends SpecialPage{
 
-function wfListIncludedFiles(){
-	global $wgOut;
-	global $wgRequest, $wgUser;
-
-
-	ob_start();
-	
-	$includedFiles = get_included_files();
-	sort($includedFiles);
-
-	print count($includedFiles)." Included files:<ul>\n";
-	foreach($includedFiles as $fileName){
-		print "<li>$fileName</li>\n";
+	public function ListIncludedFiles(){
+		SpecialPage::SpecialPage('ListIncludedFiles');
 	}
-	print "</ul>\n";
 
-	$html = ob_get_clean();
-	$wgOut->addHTML($html);
-}
+	function execute(){
+		global $wgOut;
+		global $wgRequest, $wgUser;
 
-?>
+		wfLoadExtensionMessages('listincludedfiles');
+
+		ob_start();
+		
+		$includedFiles = get_included_files();
+		sort($includedFiles);
+
+		print count($includedFiles)." Included files:<ul>\n";
+		foreach($includedFiles as $fileName){
+			print "<li>$fileName</li>\n";
+		}
+		print "</ul>\n";
+
+		$html = ob_get_clean();
+		$wgOut->addHTML($html);
+	} // end execute()
+
+} // end class ListIncludedFiles
