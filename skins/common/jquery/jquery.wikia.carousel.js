d21735d6ac30e63707bb5d84baf1377a37e9aa86
macbre <macbre@wikia-inc.com>
2012-05-16 09:07:30 +0000

    jquery.wikia.carousel.js: JS linting
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@52870 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/common/jquery/jquery.wikia.carousel.js
+++ skins/common/jquery/jquery.wikia.carousel.js
@@ -1,26 +1,26 @@
-/* 
+/*
  * Wikia Carousel jQuery plugin
  * Author Liz Lee, Hyun Lim
- * 
+ *
  * Sample HTML:
- * 
+ *
  * <span class="next"></span>
  * <span class="previous"></span>
  * <div id="myCarousel">
  * 	  <div>
- * 		  <ul class="carousel"> 
+ * 		  <ul class="carousel">
  * 			<li><img></li>
  * 			<li><img></li>
  * 			<li><img></li>
  * 		  </ul>
  * 	  </div>
  * </div>
- * 
+ *
  * $('#myCarousel').carousel();
- * 
+ *
  */
 
-;(function($, window) {
+(function($, window) {
 	$.fn.carousel = function(options) {
 		var defaults = {
 			transitionSpeed: 500,
@@ -29,13 +29,13 @@
 			activeIndex: 0,
 			nextClass: "next",
 			prevClass: "previous",
-			attachBlindImages: false, 
+			attachBlindImages: false,
 			itemClick: false,
 			trackProgress: false // pass in function for inserting progress data into dom. 
-		}
+		};
 
 		options = $.extend(defaults, options);
-		
+
 		// Any dom elements that will be reused
 		var dom = {
 			wrapper: $(this),
@@ -44,46 +44,46 @@
 			container: $(this).children('div:first'),
 			next: $(this).siblings('.' + options.nextClass),
 			previous: $(this).siblings('.' + options.prevClass)
-		}
+		};
 
 		var constants = {
 			viewPortWidth: $(this).outerWidth(),
 			itemWidth: dom.items.first().outerWidth(true), // item width including margin
 			carouselWidth: 0, // updated on init
 			minLeft: 0 // updated on init
-		}
-		
+		};
+
 		var states = {
 			browsing: false,
 			enable_next: false,
 			enable_previous: false,
 			currIndex: 0, // index of first li shown in viewport
-			left: 0,
-		}
-		
+			left: 0
+		};
+
 		function nextImage() {
 			if (states.browsing == false && states.enable_next == true) {
 				states.browsing = true;
-				
+
 				// calculate a full cycle of new items
 				var left = states.left - constants.viewPortWidth;
-				
+
 				// don't go farther left than last image
 				if(left < constants.minLeft) {
 					var remainder = constants.minLeft - left,
 						itemDiff = options.itemsShown - (remainder / constants.itemWidth);
-					
+
 					states.currIndex = states.currIndex + itemDiff;
-					
+
 					left = constants.minLeft;
-					
+
 				} else {
 					// update current index
 					states.currIndex = states.currIndex + options.itemsShown;
 				}
-				
+
 				states.left = left;
-				
+
 				dom.carousel.animate({
 					left: left
 				}, options.transitionSpeed, function() {
@@ -92,15 +92,15 @@
 				});
 			}
 			return false;
-	
+
 		}
-	
+
 		function previousImage() {
 			if (states.browsing == false && states.enable_previous == true) {
 				states.browsing = true;
 
 				var left = states.left + constants.viewPortWidth;
-				
+
 				// Don't go farther right than the first image
 				if(left > 0) {
 					left = 0;
@@ -108,9 +108,9 @@
 				} else {
 					states.currIndex = states.currIndex - options.itemsShown;
 				}
-				
+
 				states.left = left;
-				
+
 				dom.carousel.animate({
 					left:  left
 				}, options.transitionSpeed, function() {
@@ -120,17 +120,17 @@
 			}
 			return false;
 		}
-		
+
 		function moveToIndex(idx) {
 			// check if index is visible
 			if(!isVisible(idx)) {
-				// if idx is too close to the end, set idx the last possible index. 
+				// if idx is too close to the end, set idx the last possible index.
 				if(idx > (dom.items.length - options.itemsShown)) {
 					idx = dom.items.length - options.itemsShown;
 				}
-				
+
 				states.currIndex = idx;
-								
+
 				var left = constants.itemWidth * idx * -1;
 
 				// Move carouself to currIndex
@@ -139,17 +139,17 @@
 			// Activate/deactivate left/right arrows
 			afterMove();
 		}
-		
+
 		function isVisible(idx) {
 			// returns true if item at given index is inside viewport
 			return idx >= states.currIndex && idx < (states.currIndex + options.itemsShown);
 		}
-		
+
 		function getVisible() {
 			// returns jQuery object of visible items inside viewport
-			return dom.items.slice(states.currIndex, states.currIndex + options.itemsShown)
+			return dom.items.slice(states.currIndex, states.currIndex + options.itemsShown);
 		}
-		
+
 		function setAsActive(idx) {
 			// add an active class to the item that is selected (i.e. to show a larger view of it)
 			dom.items.removeClass('active');
@@ -157,7 +157,7 @@
 
 			moveToIndex(idx);
 		}
-		
+
 		function afterMove() {
 			updateArrows();
 			if(typeof options.trackProgress == 'function') {
@@ -176,22 +176,22 @@
 		}
 		
 		function updateArrows() {
-			
+
 			// If we don't have enough items to fill the viewport, disable both arrows
 			if(dom.items.length <= options.itemsShown) {
 				disableNext();
 				disablePrevious();
 				return;
 			}
-			
+
 			// get css 'left' property without 'px'
 			var left = parseInt(dom.carousel.css('left'));
-			
+
 			// if css 'left' is undefined, set it to 0
 			if(isNaN(left)) {
 				left = 0;
 			}
-			
+
 			if(left == constants.minLeft) {
 				// disable right arrow
 				disableNext();
@@ -206,12 +206,12 @@
 				enableNext();
 			}
 		}
-	
+
 		// on hover, load any images that are invisible and that haven't bee loaded yet
 		function lazyLoadImages(limit) {
 			var images = dom.carousel.find('img').filter('[data-src]');
 			$().log('lazy loading rest of images', 'LatestPhotos');
-	
+
 			var count = 0;
 			images.each(function() {
 				count ++;
@@ -224,7 +224,7 @@
 					removeAttr('data-src');
 			});
 		}
-	
+
 		// run this once on init
 		function setCarouselWidth() {
 			var width = constants.itemWidth * dom.items.length;
@@ -234,12 +234,12 @@
 
 			// get minimum possible css left value of carousel
 			var minLeft = (width - constants.viewPortWidth) * -1;
-			
+
 			// Set constants
 			constants.carouselWidth = width;
 			constants.minLeft = minLeft;
 		}
-	
+
 		// This is mainly used on LatestPhotos
 		function attachBlindImages() {
 			if (dom.carousel.find('li').length == 5) {
@@ -249,11 +249,11 @@
 				dom.carousel.append("<li class='blind'></li>");
 				dom.carousel.append("<li class='blind'></li>");
 			}
-	
+
 			dom.carousel.find('li').first().addClass("first-image");
 			dom.carousel.find('li').last().addClass("last-image");
 		}
-		
+
 		function disableNext() {
 			states.enable_next = false;
 			dom.next.addClass('disabled');
@@ -263,7 +263,7 @@
 			states.enable_next = true;
 			dom.next.removeClass('disabled');
 		}
-		
+
 		function disablePrevious() {
 			states.enable_previous = false;
 			dom.previous.addClass('disabled');
@@ -274,32 +274,32 @@
 		}
 
 		return this.each(function() {
-			
+
 			// if there's an awkward number of thumbnails, add empty <li>'s to fill the space
 			if(typeof options.attachBlindImages == 'function') {
 				attachBlindImages();
 			}
-			
+
 			setCarouselWidth();
-			
+
 			setAsActive(options.activeIndex);
-			
+
 			// Set up click events
 			dom.next.click(nextImage);
 			dom.previous.click(previousImage);
-			
+
 			if(options.itemClick) {
 				dom.carousel.on('click', 'li', function(e) {
 					setAsActive($(this).index());
-					options.itemClick.call(this, e)
+					options.itemClick.call(this, e);
 				});
 			}
-	
+
 			// on mouseover load the rest of images
 			dom.wrapper.parent().one('mouseover', function() {
 				lazyLoadImages('rest');
 			});
-			
+
 		});
 	}
 })(jQuery, this);
