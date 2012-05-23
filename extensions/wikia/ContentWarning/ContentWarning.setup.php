f0123a2a779ad487d0aa00918b69d8b21f29aec0
saipetch <saipetch@wikia-inc.com>
2012-05-23 00:44:06 +0000

    ContentWarning: cleanup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53511 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ContentWarning/ContentWarning.setup.php
+++ extensions/wikia/ContentWarning/ContentWarning.setup.php
@@ -17,12 +17,8 @@ $app = F::app();
 $app->registerClass('ContentWarningController', $dir.'ContentWarningController.class.php');
 $app->registerClass('ContentWarningHooks', $dir . 'ContentWarningHooks.class.php');
 
-
 // i18n mapping
 $app->registerExtensionMessageFile('ContentWarning', $dir.'ContentWarning.i18n.php');
-
-// special pages
-$app->registerSpecialPage('ContentWarning', 'ContentWarningSpecialController');
 
 // Hooks
 $app->registerHook('MakeGlobalVariablesScript', 'ContentWarningHooks', 'onMakeGlobalVariablesScript');
