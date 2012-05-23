d949fff966fb84932c60a44885f98f6a3dc1e5a3
kflorence <kflorence@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-23 23:19:49 +0000

    WikiaStyleGuide: fix DropdownController. Add assets.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53635 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaStyleGuide/DropdownController.class.php
+++ extensions/wikia/WikiaStyleGuide/DropdownController.class.php
@@ -1,11 +1,14 @@
 <?php
 
 /**
- * UISG Multiple Select Dropdown Controller
+ * UISG Dropdown Controller
  * @author Kyle Florence, Saipetch Kongkatong, Tomasz Odrobny
  */
-class MultiSelectDropdownController extends WikiaController {
-	public function index() {
+class DropdownController extends WikiaController {
+	public function multiSelect() {
+		$this->response->addAsset('extensions/wikia/WikiaStyleGuide/js/Dropdown/MultiSelectDropdown.js');
+		$this->response->addAsset('extensions/wikia/WikiaStyleGuide/css/Dropdown/MultiSelectDropdown.scss');
+
 		$this->options = $this->getVal('options', array());
 		$this->selected = $this->getVal('selected', array());
 	}
