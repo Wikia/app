f357c15bdfb799c1ba12d3c80fac4715e9882489
kflorence <kflorence@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-23 22:05:22 +0000

    RecentChanges: cleanup.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53624 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RecentChanges/RecentChangesHooks.class.php
+++ extensions/wikia/RecentChanges/RecentChangesHooks.class.php
@@ -1,39 +1,45 @@
-<?php 
+<?php
 
 class RecentChangesHooks {
-	public static function onGetNamespaceCheckbox( &$html, $selected = '', $all = null, $element_name = 'namespace', $label = null ) {
+	public static function onGetNamespaceCheckbox(&$html, $selected = '', $all = null, $element_name = 'namespace', $label = null) {
 		$app = F::app();
 
-		if ( !($app->wg->Title->isSpecialPage() && $app->wg->Title->getText() =='RecentChanges') )
+		if (!($app->wg->Title->isSpecialPage() && $app->wg->Title->getText() =='RecentChanges')) {
 			return true;
+		}
 
 		$namespaces = $app->wg->ContLang->getFormattedNamespaces();
 
-		$app->wf->RunHooks( 'XmlNamespaceSelectorAfterGetFormattedNamespaces', array(&$namespaces, $selected, $all, $element_name, $label) );
+		$app->wf->RunHooks('XmlNamespaceSelectorAfterGetFormattedNamespaces', array(&$namespaces, $selected, $all, $element_name, $label));
 
 		$options = array();
 
-		if( preg_match( '/^\d+$/', $selected ) ) {
-			$selected = intval( $selected );
+		if (preg_match('/^\d+$/', $selected)) {
+			$selected = intval($selected);
 		}
 
-		if( !is_null( $all ) )
-			$namespaces = array( $all => wfMsg( 'namespacesall' ) ) + $namespaces;
+		if (!is_null($all)) {
+			$namespaces = array($all => wfMsg('namespacesall')) + $namespaces;
+		}
 
-		foreach( $namespaces as $index => $name ) {
-			if( $index < NS_MAIN )
+		foreach ($namespaces as $index => $name) {
+			if ($index < NS_MAIN) {
 				continue;
-			if( $index === 0 )
-				$options[$index] = $app->wf->Msg( 'blanknamespace' );
-			else
+			}
+
+			if ($index === 0) {
+				$options[$index] = $app->wf->Msg('blanknamespace');
+
+			} else {
 				$options[$index] = $name;
+			}
 		}
 
-		$selected = array( $selected );
-		$html = $app->renderView( 'RecentChangesController', 'dropdown', array( 'options' => $options, 'selected' => $selected ) );
+		$selected = array($selected);
+		$html = $app->renderView('RecentChangesController', 'dropdown', array('options' => $options, 'selected' => $selected));
 
-		if ( !is_null( $label ) ) {
-			$html = Xml::label( $label, $element_name ) . '&nbsp;' . $ret;
+		if (!is_null($label)) {
+			$html = Xml::label($label, $element_name) . ' ' . $ret;
 		}
 
 		return true;
--- extensions/wikia/RecentChanges/templates/RecentChanges_dropdown.php
+++ extensions/wikia/RecentChanges/templates/RecentChanges_dropdown.php
@@ -1,7 +1,7 @@
 <ul>
-<? foreach ( $options as $index => $name ) { ?>
-	<li>
-		<input type="checkbox" name="namespace[]" value="<?= $index ?>" <? if ( in_array($index, $selected) ) echo "checked"; ?>><?= $name ?>
-	</li>
-<? } ?>
+	<? foreach( $options as $index => $name ): ?>
+		<li>
+			<input type="checkbox" name="namespace[]" value="<?= $index ?>" <?= in_array($index, $selected) ? 'checked' : '' ?>><?= $name ?>
+		</li>
+	<? endforeach; ?>
 </ul>
