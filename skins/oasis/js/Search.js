7914bd7fb6215d3d7aa0c134e3ed96eb4803508d
robert <robert@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-23 22:11:39 +0000

    make our version of autocomplete forwards-compatible, up deferrequest value to 250
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53625 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/Search.js
+++ skins/oasis/js/Search.js
@@ -71,7 +71,7 @@ var WikiaSearchApp = {
 						window.location.href = wgArticlePath.replace(/\$1/, encodeURIComponent(v.replace(/ /g, '_'))).replace(encodeURIComponent('/'), '/');
 					},
 					appendTo: '#WikiaSearch',
-					deferRequestBy: 250,
+					deferRequestBy: 2000,
 					minLength: 3,
 					maxHeight: 1000,
 					selectedClass: 'selected',
