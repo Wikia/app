73de12e23c2a467949603a9b7f16eb61e6ff1ba3
tomek <tomek@wikia-inc.com>
2012-05-23 21:19:33 +0000

    RecentChanges
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53611 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RecentChanges/js/RecentChanges.js
+++ extensions/wikia/RecentChanges/js/RecentChanges.js
@@ -1,3 +1,34 @@
-var RecentChanges = {
-
-};
+$(function(){
+	var RecentChanges = {
+		init: function(){
+			//TODO: other selector
+			$('.mw-recentchanges-table input[type=submit]').click(RecentChanges.saveFilters);
+					
+		},
+		saveFilters: function(e) {
+			e.preventDefault();
+			$.nirvana.sendRequest({
+				controller: 'RecentChangesController',
+				method: 'saveFilters',
+				data: { 'filters': RecentChanges.getFilter()},
+				type: "POST",
+				format: 'json',
+				callback: function(data) {
+					alert(data);
+				}
+			}); 
+		},
+		
+		getFilter: function() {
+			//TODO:  other selector ?
+			var filters = [];
+			var inputs = $('.mw-recentchanges-table input[type=checkbox]:checked');
+			debugger;
+			for( var i = 0; i < inputs.length; i++ ){
+				filters.push($(inputs[i]).val());
+			}
+			return filters;
+		}
+	};
+	RecentChanges.init();
+});
