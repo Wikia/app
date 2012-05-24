83e149bb062e4603381c9afe338112dc17a1589e
adi <adi@wikia-inc.com>
2012-05-24 15:08:48 +0000

    getVideoMetadata() method added (bugId:32032)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53709 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MediaTool/MediaToolController.class.php
+++ extensions/wikia/MediaTool/MediaToolController.class.php
@@ -5,9 +5,47 @@
  * @author mech
  */
 class MediaToolController extends WikiaController {
-		
+	const RESPONSE_STATUS_OK = 'ok';
+	const RESPONSE_STATUS_ERROR  = 'error';
+
 	public function __construct() {
 		
 	}
-	
+
+	public function getVideoMetadata() {
+		$this->response->setFormat('json');
+
+		$videoUrl = $this->getVal( 'videoUrl' );
+
+		$data = array();
+		if(!empty($videoUrl)) {
+			try {
+				$wrapper = ApiWrapperFactory::getApiWrapper( $videoUrl );
+			}
+			catch(Exception $exception) {
+				// catch any response errors from providers here
+				$data['status'] = self::RESPONSE_STATUS_ERROR;
+				$data['msg'] = wfMsg('mediatool-error-invalid-video');
+
+				$this->response->setData( $data );
+				return;
+			}
+
+			if($wrapper instanceof ApiWrapper) {
+				$data['status'] = self::RESPONSE_STATUS_OK;
+				$data['title'] = $wrapper->getTitle();
+				$data['thumbnailUrl'] = $wrapper->getThumbnailUrl();
+			}
+			else {
+				$data['status'] = self::RESPONSE_STATUS_ERROR;
+				$data['msg'] = wfMsg('mediatool-error-unknown-video-provider');
+			}
+		}
+		else {
+			$data['status'] = self::RESPONSE_STATUS_ERROR;
+			$data['msg'] = wfMsg('mediatool-error-missing-video-url');
+		}
+
+		$this->response->setData( $data );
+	}
 }
