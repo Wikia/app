cbdf24bb0055dea7e3d1e5123524e8dfac77ff35
tomek <tomek@wikia-inc.com>
2012-05-24 17:12:14 +0000

    RecentChangesHooks
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53715 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaStyleGuide/templates/WikiaStyleGuideDropdown_multiSelect.php
+++ extensions/wikia/WikiaStyleGuide/templates/WikiaStyleGuideDropdown_multiSelect.php
@@ -1,7 +1,7 @@
 <ul>
 	<? foreach( $options as $index => $name ): ?>
 		<li>
-			<input type="checkbox" name="namespace[]" value="<?= $index ?>" <?= in_array($index, $selected, true) ? 'checked' : '' ?>><?= $name ?>
+			<input type="checkbox" name="namespace[]" value="<?= $index ?>" <?= !empty($selected[$index]) ? 'checked' : '' ?>><?= $name ?>
 		</li>
 	<? endforeach; ?>
 </ul>
