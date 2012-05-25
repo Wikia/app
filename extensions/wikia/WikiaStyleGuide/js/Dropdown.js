21154be82b77e1e9b2eae22bfd38f055f76aec39
kflorence <kflorence@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-25 01:38:24 +0000

    WikiaStyleGuide: complete base styles for multiple select dropdown. Add skeleton javascript functionality.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53766 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaStyleGuide/js/Dropdown.js
@@ -0,0 +1,13 @@
+(function($) {
+
+$.fn.createDropdown = function(options) {
+	options = $.extend(true, {}, $.fn.createDropdown.options, options);
+
+	return this.each(function() {
+		// todo
+	});
+};
+
+$.fn.createDropdown.options = {};
+
+})(jQuery);
