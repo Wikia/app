cca63b2f704562f8130e86c77f35bfed843e2b5f
hyun <hyun@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-26 00:33:42 +0000

    Lightbox: split into basic loader and actual lightbox
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53858 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -50,7 +50,7 @@ $config['oasis_noads_extensions_js'] = array(
 		'//extensions/wikia/CreatePage/js/CreatePage.js',
 		'//extensions/wikia/ImageLightbox/ImageLightbox.js',
 		'//skins/common/jquery/jquery.md5.js',
-		'//extensions/wikia/Lightbox/js/Lightbox.js',
+		'//extensions/wikia/Lightbox/js/LightboxLoader.js',
 		'//extensions/wikia/AjaxLogin/AjaxLoginBindings.js',
 		'//extensions/wikia/UserLogin/js/UserLoginAjaxForm.js', // TODO: do we need to load this for logged-in?  answer: yes, we allow logged in users to change login without logging out
 		'//extensions/wikia/UserLogin/js/UserSignupAjaxForm.js', // TODO: do we need to load this for logged-in?  answer: yes, we allow logged in users to create account without logging out
