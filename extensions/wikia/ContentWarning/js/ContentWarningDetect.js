316fcc0cd2b56b346d6c3046ce6e5d923fb56c4e
kflorence <kflorence@wikia-inc.com>
2012-05-29 19:50:42 +0000

    ContentWarning: add case for logged in user with cookie.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@53985 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ContentWarning/js/ContentWarningDetect.js
+++ extensions/wikia/ContentWarning/js/ContentWarningDetect.js
@@ -1,1 +1,1 @@
-!(window.wgContentWarningApproved = !window.wgUserName && (document.cookie.indexOf('ContentWarningApproved=1') >= 0)) && (document.body.className += ' ShowContentWarning');
+!(window.wgContentWarningApproved = (document.cookie.indexOf('ContentWarningApproved=1') >= 0)) && (document.body.className += ' ShowContentWarning');
