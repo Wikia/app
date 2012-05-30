d0278ae09f77592e77c12719eaf33ff3b6788564
liz <liz@wikia-inc.com>
2012-05-30 05:21:57 +0000

    (BugId:31537) adding some error message handling to Lightbox template
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@54053 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Lightbox/templates/Lightbox_lightboxModalContentError.php
+++ extensions/wikia/Lightbox/templates/Lightbox_lightboxModalContentError.php
@@ -1,1 +1,7 @@
-Put error related things here.
+<h1><?= wfMsg('lightbox-no-media-error-header') ?></h1>
+
+<p><?= wfMsg('lightbox-no-media-error') ?></p>
+
+<div class="modalToolbar">
+	<button id="close-lightbox"><?= wfMsg('lightbox-no-media-error-close') ?></button>
+</div>
