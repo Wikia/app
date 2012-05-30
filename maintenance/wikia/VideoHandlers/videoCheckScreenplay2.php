f117336b15a9b61b9053295ee4012e1ff9aebdd2
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-05-30 18:58:27 +0000

    Ingestion: remove screenplay videos with illegal characters from video.wikia.com
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@54107 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ maintenance/wikia/VideoHandlers/videoCheckScreenplay2.php
@@ -0,0 +1,41 @@
+<?php
+
+$_SERVER['QUERY_STRING'] = 'VideoCleanup';
+
+ini_set( "include_path", dirname(__FILE__)."/.." );
+
+ini_set( 'display_errors', 'stdout' );
+$options = array('help');
+require_once( '../../commandLine.inc' );
+
+global $IP, $wgCityId, $wgExternalDatawareDB;
+
+
+$wgUser = User::newFromName( 'Wikia Video Library' );
+$dbw = wfGetDB( DB_MASTER );
+
+$rows = $dbw->select('image',
+	'*',
+	array('img_media_type'=>'VIDEO','img_minor_mime'=>'screenplay')
+);
+
+while($row = $dbw->fetchObject($rows)) {
+	$name = $row->img_name;
+	$title = Title::newFromText($name, NS_FILE);
+	$wgTitle = $title;
+	$new_name = VideoFileUploader::sanitizeTitle( $name );
+	$new_title = Title::newFromText($new_name, NS_FILE);
+	if( $new_name != $name ) {
+		$exists = $new_title->exists() ? '+++' : "---";
+		echo "$exists $name is illegal name\n";
+		echo "    Incorrect URL: " . WikiFactory::getLocalEnvURL( $title->getFullURL()) ."\n";
+		if( $new_title->exists() ) {
+			echo "    Correct   URL: " . WikiFactory::getLocalEnvURL( $new_title->getFullURL()) ."\n";
+			// only remove if there is a copy under "correct" name
+			$file = wfFindFile( $name );
+			$file->delete('Duplicated file, Illegal characters in name');
+		}
+	}
+
+
+}
