5c3e59b35ea7dbe3cfee9bafb47bd26caff941d7
tor <tor@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-06-19 00:17:14 +0000

    added proper language support for central redirect (on cancel) BugzId:33712
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@54967 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ContentWarning/ContentWarningController.class.php
+++ extensions/wikia/ContentWarning/ContentWarningController.class.php
@@ -11,10 +11,15 @@ class ContentWarningController extends WikiaController {
 	 */
 	public function index() {
 		$domain = str_replace('http://', '', $this->wg->Server );
-		$this->title = $this->wf->Msg('content-warning-title');
 		$this->body = $this->wf->MsgExt( 'content-warning-body', array('parse'), $domain );
-		$this->btnContinue = $this->wf->Msg( 'content-warning-button-continue' );
-		$this->btnCancel = $this->wf->Msg( 'content-warning-button-cancel' );
+
+		$userLang = $this->wg->Lang->getCode();
+
+		if ( !empty( $this->wg->LangToCentralMap[$userLang] ) ) {
+			$this->btnCancelUrl = $this->wg->LangToCentralMap[$userLang];
+		} else {
+			$this->btnCancelUrl = 'http://www.wikia.com/';
+		}
 	}
 
 	/**
