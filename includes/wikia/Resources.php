b2a310a7b17d0653d7c11eeb1200f726a377eba3
liz <liz@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-02 20:48:16 +0000

    (BugId:35750) trying to fix resource loading on preview
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@55717 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/Resources.php
+++ includes/wikia/Resources.php
@@ -25,11 +25,15 @@ $wgResourceModules['wikia.jquery.ui'] = array(
 
 // libraries
 $wgResourceModules['jquery.aim'] = array(
-	'scripts' => 'resources/wikia/libraries/aim/jquery.aim.js'
+	'scripts' => array(
+		'resources/wikia/libraries/aim/jquery.aim.js',
+	),
 );
 
 $wgResourceModules['jquery.mustache'] = array(
-	'scripts' => 'resources/wikia/libraries/mustache/jquery.mustache.js'
+	'scripts' => array(
+		'resources/wikia/libraries/mustache/jquery.mustache.js',
+	),
 );
 
 // moved here from AssetsManager by wladek
