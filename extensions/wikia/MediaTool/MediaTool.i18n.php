93f1d9cc6b3577550f1d16796c8a823a3528ec5c
adi <adi@wikia-inc.com>
2012-07-05 14:31:14 +0000

    messages updated
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@55916 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MediaTool/MediaTool.i18n.php
+++ extensions/wikia/MediaTool/MediaTool.i18n.php
@@ -12,11 +12,13 @@ $messages['en'] = array(
 	'mediatool-button-cancel' => 'Cancel',
 	'mediatool-button-continue' => 'Continue',
 	'mediatool-button-done' => 'Save',
+	'mediatool-button-add' => 'Add',
 	'mediatool-mediaoptions' => 'Media Options',
 	'mediatool-error-missing-video-url' => 'Missing video url',
 	'mediatool-error-unknown-video-provider' => 'Unknown video provider',
 	'mediatool-error-invalid-video' => 'Invalid video',
-	'mediatool-selected-media-count' => 'Your selected media ($1 Items)'
+	'mediatool-selected-media-count' => 'Your selected media ($1 Items)',
+	'mediatool-addviaurl-label' => 'Add via direct URL'
 );
 
 /** Message documentation (Message documentation)
