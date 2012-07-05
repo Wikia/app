95c153fe1031eb76ccb99de91514fabad1326b1d
michal <michal@wikia-inc.com>
2012-07-05 15:17:09 +0000

    i18n TWN corrections, redo.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@55928 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoHandlers/VideoHandlers.i18n.php
+++ extensions/wikia/VideoHandlers/VideoHandlers.i18n.php
@@ -10,8 +10,7 @@ $messages['en'] = array(
 	'movieclips-videohandler-extension-desc' => 'MovieClips video handler',    
 	'screenplay-videohandler-extension-desc' => 'Screenplay video handler',
 	'youtube-videohandler-extension-desc' => 'YouTube video handler',
-	'videohandler-error-missing-title' => 'Required parameter "title" is missing',
-	'videohandler-error-missing-width' => 'Required parameter "width" is missing',
+	'videohandler-error-missing-parameter' => 'Required parameter "$1" is missing',
 	'videohandler-error-video-no-exist' => 'Video specified by title does not exist',
 	'videohandler-unknown-title' => 'Unknown title',
 	'videohandler-video-details' => '$1 (provider: $2)',
@@ -23,6 +22,8 @@ $messages['qqq'] = array(
 	'videohandler-video-details' => 'Parameters:
 * $1 is a link to the video on its source page, with the label being the original title of the video
 * $2 is a link to the provider\'s homepage (e.g. [http://youtube.com/ youtube.com])',
+	'videohandler-error-missing-parameter' => 'Says, that a certain parameter is missing in the input data.
+* $1 is the exact name of the missing parameter, as the computer would expect.'
 );
 
 /** Afrikaans (Afrikaans)
@@ -98,6 +99,15 @@ $messages['cy'] = array(
  * @author Tiin
  */
 $messages['de'] = array(
+	'videohandler' => 'Video handler',
+	'prototype-videohandler-extension-desc' => 'Prototyp-Video-Steuerung',
+	'movieclips-videohandler-extension-desc' => 'MovieClips-Video-Steuerungsprogramm',
+	'screenplay-videohandler-extension-desc' => 'Screenplay-Video-Steuerungsprogramm',
+	'youtube-videohandler-extension-desc' => 'YouTube-Video-Steuerungsprogramm',
+	'videohandler-error-missing-parameter' => 'Erforderlicher Parameter "$1" fehlt',
+	'videohandler-error-video-no-exist' => 'Video existiert nicht mit dem Titel',
+	'videohandler-unknown-title' => 'Unbekannter Titel',
+	'videohandler-video-details' => '$1 (Provider: $2)',
 	'videohandler-category' => 'Videos',
 );
 
@@ -116,6 +126,15 @@ $messages['de-formal'] = array(
  * @author VegaDark
  */
 $messages['es'] = array(
+	'videohandler' => 'Controlador de vídeo',
+	'prototype-videohandler-extension-desc' => 'Controlador del prototipo de vídeo',
+	'movieclips-videohandler-extension-desc' => 'Controlador de clips de película',
+	'screenplay-videohandler-extension-desc' => 'Controlador de vídeo Screnplay',
+	'youtube-videohandler-extension-desc' => 'Controlador de vídeo de YouTube',
+	'videohandler-error-missing-parameter' => 'Falta el parámetro "$1"',
+	'videohandler-error-video-no-exist' => 'El vídeo especificado por título no existe',
+	'videohandler-unknown-title' => 'Título desconocido',
+	'videohandler-video-details' => '$1 (proveedor: $2)',
 	'videohandler-category' => 'Vídeos',
 );
 
@@ -146,6 +165,15 @@ $messages['fi'] = array(
  * @author Wyz
  */
 $messages['fr'] = array(
+	'videohandler' => 'Gestionnaire de vidéos',
+	'prototype-videohandler-extension-desc' => 'Gestionnaire de vidéos de Prototype',
+	'movieclips-videohandler-extension-desc' => 'Gestionnaire de vidéos de MovieClips',
+	'screenplay-videohandler-extension-desc' => 'Gestionnaire de vidéos de ScreenPlay',
+	'youtube-videohandler-extension-desc' => 'Gestionnaire de vidéos de YouTube',
+	'videohandler-error-missing-parameter' => 'Le paramètre « $1 » obligatoire est manquant',
+	'videohandler-error-video-no-exist' => 'Aucune vidéo n’existe avec ce titre',
+	'videohandler-unknown-title' => 'Titre inconnu',
+	'videohandler-video-details' => '$1 (hébergeur : $2)',
 	'videohandler-category' => 'Vidéos',
 );
 
@@ -153,6 +181,15 @@ $messages['fr'] = array(
  * @author Toliño
  */
 $messages['gl'] = array(
+	'videohandler' => 'Manipulador de vídeos',
+	'prototype-videohandler-extension-desc' => 'Manipulador de vídeos de Prototype',
+	'movieclips-videohandler-extension-desc' => 'Manipulador de vídeos de MovieClips',
+	'screenplay-videohandler-extension-desc' => 'Manipulador de vídeos de Screnplay',
+	'youtube-videohandler-extension-desc' => 'Manipulador de vídeos de YouTube',
+	'videohandler-error-missing-parameter' => 'Falta o parámetro obrigatorio "$1"',
+	'videohandler-error-video-no-exist' => 'Non existe ningún vídeo con ese título',
+	'videohandler-unknown-title' => 'Título descoñecido',
+	'videohandler-video-details' => '$1 (provedor: $2)',
 	'videohandler-category' => 'Vídeos',
 );
 
@@ -232,6 +269,15 @@ $messages['lb'] = array(
  * @author Bjankuloski06
  */
 $messages['mk'] = array(
+	'videohandler' => 'Поставување на видеа',
+	'prototype-videohandler-extension-desc' => 'Прототипен поставувач на видеа',
+	'movieclips-videohandler-extension-desc' => 'Поставувач на видеа од MovieClips',
+	'screenplay-videohandler-extension-desc' => 'Поставувач на видеа од Screnplay',
+	'youtube-videohandler-extension-desc' => 'Поставувач на видеа од YouTube',
+	'videohandler-error-missing-parameter' => 'Недостасува задолжителниот параметар „$1“',
+	'videohandler-error-video-no-exist' => 'Не постои видео со таков наслов',
+	'videohandler-unknown-title' => 'Непознат наслов',
+	'videohandler-video-details' => '$1 (добавувач: $2)',
 	'videohandler-category' => 'Видеоснимки',
 );
 
@@ -239,6 +285,15 @@ $messages['mk'] = array(
  * @author Anakmalaysia
  */
 $messages['ms'] = array(
+	'videohandler' => 'Pengelola video',
+	'prototype-videohandler-extension-desc' => 'Pengelola video prototaip',
+	'movieclips-videohandler-extension-desc' => 'Pengelola video MovieClips',
+	'screenplay-videohandler-extension-desc' => 'Pengelola video ScreenPlay',
+	'youtube-videohandler-extension-desc' => 'Pengelola video YouTube',
+	'videohandler-error-missing-parameter' => 'Parameter wajib "$1" tertinggal',
+	'videohandler-error-video-no-exist' => 'Tiada video dengan tajuk yang dinyatakan',
+	'videohandler-unknown-title' => 'Tajuk tidak diketahui',
+	'videohandler-video-details' => '$1 (penyedia: $2)',
 	'videohandler-category' => 'Video',
 );
 
@@ -254,6 +309,11 @@ $messages['nb'] = array(
  * @author Siebrand
  */
 $messages['nl'] = array(
+	'videohandler' => 'Videoverwerking',
+	'prototype-videohandler-extension-desc' => 'Prototype videoverwerking',
+	'videohandler-error-missing-parameter' => 'Vereiste parameter "$1" ontbreekt',
+	'videohandler-unknown-title' => 'Onbekende titel',
+	'videohandler-video-details' => '$1 (provider: $2)',
 	'videohandler-category' => "Video's",
 );
 
@@ -271,6 +331,15 @@ $messages['pa'] = array(
  * @author Woytecr
  */
 $messages['pl'] = array(
+	'videohandler' => 'Odtwarzacz filmów',
+	'prototype-videohandler-extension-desc' => 'Prototypowy odtwarzacz filmów',
+	'movieclips-videohandler-extension-desc' => 'Odtwarzacz filmów MovieClips',
+	'screenplay-videohandler-extension-desc' => 'Odtwarzacz filmów Screnplay',
+	'youtube-videohandler-extension-desc' => 'Odtwarzacz filmów YouTube',
+	'videohandler-error-missing-parameter' => 'Brak wymaganego parametru "$1"',
+	'videohandler-error-video-no-exist' => 'Film o tym tytule nie istnieje',
+	'videohandler-unknown-title' => 'Nieznany tytuł',
+	'videohandler-video-details' => '$1 (z $2)',
 	'videohandler-category' => 'Filmy',
 );
 
@@ -318,6 +387,15 @@ $messages['ro'] = array(
  * @author Александр Сигачёв
  */
 $messages['ru'] = array(
+	'videohandler' => 'Видео обработчик',
+	'prototype-videohandler-extension-desc' => 'Прототип видео обработчик',
+	'movieclips-videohandler-extension-desc' => 'Видео обработчик клипов',
+	'screenplay-videohandler-extension-desc' => 'Видео обработчик сценария',
+	'youtube-videohandler-extension-desc' => 'YouTube видео обработчик',
+	'videohandler-error-missing-parameter' => 'Отсутствует обязательный параметр «$1»',
+	'videohandler-error-video-no-exist' => 'Видео с данным названием не существует',
+	'videohandler-unknown-title' => 'Неизвестное название',
+	'videohandler-video-details' => '$1 (источник: $2)',
 	'videohandler-category' => 'Видео',
 );
 
