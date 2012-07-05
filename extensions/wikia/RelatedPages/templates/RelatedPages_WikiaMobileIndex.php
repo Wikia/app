1044fe31aed8e3551ab32fd64087ce211c75902b
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-05 18:18:53 +0000

    WikiaMobile: Image optimization of our assets (extensions/wikia)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@55947 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedPages/templates/RelatedPages_WikiaMobileIndex.php
+++ extensions/wikia/RelatedPages/templates/RelatedPages_WikiaMobileIndex.php
@@ -1,10 +1,10 @@
 <? if ( !$skipRendering ) :?>
-	<section id="wkRelPag">
-		<h1 class="collSec open"><?= wfMsgForContent( 'wikiarelatedpages-heading' ) ;?><span class=chev></span></h1>
+	<section id=wkRelPag>
+		<h1 class="collSec addChev open"><?= $wf->MsgForContent( 'wikiarelatedpages-heading' ) ;?></h1>
 		<ul class="wkLst open">
 		<? foreach ( $pages as $page ) :?>
 			<? $noImage = empty( $page['imgUrl'] ) ;?>
-			<li><a href="<?= $page['url'] ;?>"<?= ($noImage) ? 'class="noImg"' : '' ;?>><? if( !$noImage ) :?><img src="<?= $page['imgUrl']; ?>" width=100 height=50/><? endif ;?><?= $page['title'] ?></a></li>
+			<li><a href="<?= $page['url'] ;?>"<?= ($noImage) ? ' class=noImg' : '' ;?>><? if( !$noImage ) :?><img src="<?= $page['imgUrl']; ?>" width=100 height=50 /><? endif ;?><?= $page['title'] ?></a></li>
 		<? endforeach ;?>
 		</ul>
 	</section>
