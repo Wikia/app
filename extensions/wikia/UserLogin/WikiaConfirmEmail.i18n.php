c30ad91ba3aa292f0bd31ce531763c21500327cc
michal <michal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-05 14:43:54 +0000

    i18n TWN corrections, redo.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@55918 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserLogin/WikiaConfirmEmail.i18n.php
+++ extensions/wikia/UserLogin/WikiaConfirmEmail.i18n.php
@@ -10,7 +10,7 @@ $messages = array();
 $messages['en'] = array(
 	'wikiaconfirmemail-error-empty-code'     => 'Empty confirmation code',
 	'wikiaconfirmemail-error-invalid-code'     => 'Invalid confirmation code',
-	'wikiaconfirmemail-error-user-not-match' => 'Oops, this confirmation link doesn\'t match up with the username you entered.  Make sure you\'re entering the right username. If you want to log in with a different account, click <a href="{{fullurl:{{ns:special}}:UserLogin}}">here</a>',
+	'wikiaconfirmemail-error-user-not-match' => 'This confirmation link does no match up with the username you entered. Make sure you are entering the right username. You can also <a href="{{fullurl:{{ns:special}}:UserLogin}}">log in with a different account</a>',
 	'wikiaconfirmemail-login-button' => 'Confirm and log in',
 	'wikiaconfirmemail-heading' => 'Almost there!',
 	'wikiaconfirmemail-subheading' => 'Log in with your username and password so we know that you\'ve received the email.',
