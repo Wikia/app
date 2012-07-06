1f33c620cacb7a5801462ac13e7c788e52395568
marcin <marcin@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-06 13:02:30 +0000

    Unit tests (bugid:36036)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@55998 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialPromote/js/tests/SpecialPromoteTest.js
+++ extensions/wikia/SpecialPromote/js/tests/SpecialPromoteTest.js
@@ -28,3 +28,13 @@ test("removeImage test", function() {
 	ok( s.removeImage({imageIndex: 1}) == !true, 'index, but no type - Should not return true' );
 	ok( s.removeImage({}) !== true, 'no params - should not return true' );
 });
+
+test("modifyImage test", function() {
+	var s = specialPromoteSetup();
+	var wrapperHtml = $('<div class="input-group main-image required"><label>Main Image</label><a href="#" data-image-type="main" class="wikia-button upload-button" title="Add a photo"><img src="" class="sprite photo">Add a photo</a><br class="clear"><div class="large-photo"><div class="modify-remove"><a class="modify" href="#">Modify</a></div><img id="curMainImageName" src="http://images.marcin.wikia-dev.com/__cb65/wikiaglobal/images/thumb/2/2d/Wikia-Visualization-Main.jpg/550px-0%2C550%2C42%2C309-Wikia-Visualization-Main.jpg" data-filename="Wikia-Visualization-Main.jpg" data-image-type="main"></div><span class="explanatory-copy">This is the main image that will represent your wiki on wikia.com. Choose an image that will show people what the wiki is about. You can always change it to keep it current.</span></div>');
+	var wrapperEvent = $.Event('click', {target: wrapperHtml.find('.modify-remove .modify')});
+	ok( s.onChangePhotoClick(wrapperEvent) == true, 'type=main - should return true' );
+	var wrapperHtmlNoType = $('<div class="input-group main-image required"><label>Main Image</label><a href="#" data-image-type="main" class="wikia-button upload-button" title="Add a photo"><img src="" class="sprite photo">Add a photo</a><br class="clear"><div class="large-photo"><div class="modify-remove"><a class="modify" href="#">Modify</a></div><img id="curMainImageName" src="http://images.marcin.wikia-dev.com/__cb65/wikiaglobal/images/thumb/2/2d/Wikia-Visualization-Main.jpg/550px-0%2C550%2C42%2C309-Wikia-Visualization-Main.jpg" data-filename="Wikia-Visualization-Main.jpg"></div><span class="explanatory-copy">This is the main image that will represent your wiki on wikia.com. Choose an image that will show people what the wiki is about. You can always change it to keep it current.</span></div>');
+	var wrapperEventNoType = $.Event('click', {target: wrapperHtmlNoType.find('.modify-remove .modify')});
+	ok( s.onChangePhotoClick(wrapperEventNoType) !== true, 'no type - should not return true' );
+});
