7fa83800d6c6c4bab0c281349f53b91e49a03c78
arturk <arturk@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-11 17:03:27 +0000

    Fixed search wiki link on search page (BugId:36980)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56331 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Search/templates/WikiaSearch_WikiaMobileResultSet.php
+++ extensions/wikia/Search/templates/WikiaSearch_WikiaMobileResultSet.php
@@ -1,21 +1,18 @@
 <?php if( $resultSet->getResultsFound() > 1 ): ?>
 <li class=group>
 <?php
-    $trackingData = 'class=ResultLink data-wid="'.$resultSet->getHeader('cityId').'" data-gpos="'.$pos.'" data-pos=0 data-sterm="'.addslashes($query).'" data-stype="'.( $isInterWiki ? 'inter' : 'intra' ).'" data-rver="'.$relevancyFunctionId.'" data-event=search_click_wiki';
+	$trackingData = 'class=ResultLink data-wid="'.$resultSet->getHeader('cityId').'" data-gpos="'.$pos.'" data-pos=0 data-sterm="'.addslashes($query).'" data-stype="'.( $isInterWiki ? 'inter' : 'intra' ).'" data-rver="'.$relevancyFunctionId.'" data-event=search_click_wiki';
 ?>
-    <p>
-        <a class=groupTitle href="<?= $resultSet->getHeader( 'cityUrl' );?>" <?= $trackingData; ?> ><?= $resultSet->getHeader( 'cityTitle' );?></a>
-        <a class=searchGroup href="<?= $resultSet->getHeader('cityUrl') . SpecialPage::getTitleFor('Search')->getLocalUrl( array(
-			'fulltext' => 'Search',
-			'search' => urlencode( $query )
-		) ) ?>"></a>
-    </p>
-    <a class=url href="<?= $resultSet->getHeader( 'cityUrl' );?>" <?= $trackingData; ?> ><?=$resultSet->getHeader( 'cityUrl' );?></a>
-    <?php for($i = 1; $i < 5; $i++){
+	<p>
+		<a class=groupTitle href="<?= $resultSet->getHeader( 'cityUrl' );?>" <?= $trackingData; ?> ><?= $resultSet->getHeader( 'cityTitle' );?></a>
+		<a class="searchGroup" href="<?= $resultSet->getHeader('cityUrl') .'/wiki/Special:Search?search='.urlencode($query).'&fulltext=Search';?>"></a>
+	</p>
+	<a class=url href="<?= $resultSet->getHeader( 'cityUrl' );?>" <?= $trackingData; ?> ><?=$resultSet->getHeader( 'cityUrl' );?></a>
+	<?php for($i = 1; $i < 5; $i++){
 		$result = $resultSet->next();
-    	if($result instanceof WikiaSearchResult): ?>
-        <div class=groupResults>
-            <?= $app->getView( 'WikiaSearch', 'WikiaMobileResult', array(
+		if($result instanceof WikiaSearchResult): ?>
+		<div class=groupResults>
+			<?= $app->getView( 'WikiaSearch', 'WikiaMobileResult', array(
 				'result' => $result,
 				'gpos' => $pos,
 				'pos' => $i,
@@ -23,12 +20,12 @@
 				'inGroup' => true,
 				'isInterWiki' => $isInterWiki,
 				'relevancyFunctionId' => $relevancyFunctionId ) ); ?>
-        </div>
-        <?php else: break; endif; ?>
-    <?php }; ?>
+		</div>
+		<?php else: break; endif; ?>
+	<?php }; ?>
 </li>
 <?php else: ?>
-    <?= $app->getView( 'WikiaSearch', 'WikiaMobileResult', array(
+	<?= $app->getView( 'WikiaSearch', 'WikiaMobileResult', array(
 		'result' => $resultSet->next(),
 		'gpos' => 0,
 		'pos' => $pos,
