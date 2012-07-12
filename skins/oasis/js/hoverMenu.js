76efbfba58f3c3be7c8edb4590c58a9d5b52b60b
kflorence <kflorence@wikia-inc.com>
2012-07-12 23:22:53 +0000

    Lazy load global navigation on mouseover (BugId:36973).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56471 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/hoverMenu.js
+++ skins/oasis/js/hoverMenu.js
@@ -1,3 +1,5 @@
+(function(window, $) {
+
 var HoverMenu = function(selector) {
 
 	//Settings
@@ -30,27 +32,56 @@ var HoverMenu = function(selector) {
 		this.hideNav();
 		this.showNav($(event.currentTarget).closest("li"));
 	}, this));
+
 	this.menu.find(".subnav a").focus($.proxy(function(event) {
 		this.hideNav();
 		this.showNav($(event.currentTarget).closest(".subnav").closest("li"));
 	}, this));
+
 	//Hide when focus out of first and last anchor
 	this.menu.children("li").first().children("a").focusout($.proxy(this.hideNav, this));
 	this.menu.find(".subnav>li:last-child li:last-child a").focusout($.proxy(this.hideNav, this));
 };
 
+var globalNavigationMenus = [];
 HoverMenu.prototype.mouseover = function(event) {
-
-	var self = this;
+	var li = $(event.currentTarget);
 
 	//Hide all subnavs except for this one
-	this.menu.children("li").children("ul").not($(event.currentTarget).find("ul")).removeClass("show");
+	this.menu.children("li").children("ul").not(li.find("ul")).removeClass("show");
 
 	//Cancel mouseoutTimer
 	clearTimeout(this.mouseoutTimer);
 
+	// Lazy load content for Global Navigation (BugId:36973)
+	var index = li.data('index');
+	if (this.selector == '#GlobalNavigation' && ($.inArray(index, globalNavigationMenus) < 0)) {
+		$.nirvana.sendRequest({
+			controller: 'GlobalHeaderController',
+			method: 'menuItems',
+			format: 'html',
+			type: 'GET',
+			data: {
+				index: index
+			},
+			callback: $.proxy(function(html) {
+				li.find('.subnav').html(html);
+				globalNavigationMenus.push(index);
+				this.handleShowNav(event);
+			}, this)
+		});
+
+	// Everything else: handle show immediately
+	} else {
+		this.handleShowNav(event);
+	}
+};
+
+HoverMenu.prototype.handleShowNav = function(event) {
+	var self = this;
+
+	// Mouse is not coming from within the nav.
 	if ($(event.relatedTarget).closest(this.selector).length == 0) {
-		//Mouse is not coming from within the nav.
 
 		//Delay before showing subnav.
 		this.mouseoverTimer = setTimeout(function() {
@@ -59,32 +90,30 @@ HoverMenu.prototype.mouseover = function(event) {
 		}, this.settings.mouseoverDelay);
 		this.mouseoverTimerRunning = true;
 
+	//Mouse IS coming from within the nav
 	} else {
-		//Mouse IS coming from within the nav
 
 		//Don't show subnavs when quickly moving mouse horizontally through wiki nav
 		if (this.mouseoverTimerRunning) {
 
 			//Stop current timer
 			clearTimeout(this.mouseoverTimer);
-			this.mouseoverTimerRunning = false;
 
 			//Start new timer
 			this.mouseoverTimer = setTimeout(function() {
 				self.showNav(event.currentTarget);
+				self.mouseoverTimerRunning = false;
 			}, this.settings.mouseoverDelay);
 			this.mouseoverTimerRunning = true;
 
+		//Mouseover timer isn't running, so show subnavs immediately
 		} else {
-			//Mouseover timer isn't running, so show subnavs immediately
 			this.showNav(event.currentTarget);
 		}
 	}
-
-};
+}
 
 HoverMenu.prototype.mouseout = function(event) {
-
 	var self = this;
 
 	if ($(event.relatedTarget).closest(this.selector).length == 0) {
@@ -110,7 +139,6 @@ HoverMenu.prototype.mouseout = function(event) {
 		this.hideNav();
 		$.showAds();
 	}
-
 };
 
 HoverMenu.prototype.showNav = function(parent) {
@@ -175,3 +203,9 @@ $(function() {
 		$("#GlobalNavigation .subnav, #WikiHeader .subnav").show();
 	});
 });
+
+// Exports
+window.HoverMenu = HoverMenu;
+window.HoverMenuGlobal = HoverMenuGlobal;
+
+})(this, jQuery);
