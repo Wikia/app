036586c00b2f0f4af569e7b396de86cc12e7e9ea
jacek <jacek@wikia-inc.com>
2012-07-13 12:55:33 +0000

    MediaTool preview - part 9
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56495 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MediaTool/js/VideoPreview.js
+++ extensions/wikia/MediaTool/js/VideoPreview.js
@@ -4,6 +4,16 @@ MediaTool.VideoPreview = $.createClass(Observable,{
 
 	constructor: function() {
 		MediaTool.VideoPreview.superclass.constructor.call(this);
+	},
+
+	thumbnailClickAction: function(target) {
+
+		var a = $(target).parents('a').eq(0);
+		var img = a.find('img');
+		var title = a.attr('data-video-name');
+		if ( typeof ImageLightbox != "undefined" ) {
+			ImageLightbox.displayInlineVideo(img, title);
+		}
 	}
 
 
