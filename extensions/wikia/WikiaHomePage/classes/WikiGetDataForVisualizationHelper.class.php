4f53f78a1ff1caddb55789508da7effbdd65529d
marzjan <marzjan@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-16 10:24:40 +0000

    Adding documentation and some tweaks in Special:Promote
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56555 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaHomePage/classes/WikiGetDataForVisualizationHelper.class.php
+++ extensions/wikia/WikiaHomePage/classes/WikiGetDataForVisualizationHelper.class.php
@@ -14,4 +14,15 @@ class WikiGetDataForVisualizationHelper implements WikiGetDataHelper {
 	public function getImages($wikiId, $langCode, $wikiRow) {
 		return json_decode($wikiRow->city_images);
 	}
+
+	/**
+	 * @param $wikiId int
+	 * @param $langCode String
+	 * @param $imageSource object (city_visualization row)
+	 * @param $currentData array
+	 * @return bool
+	 */
+	public function getMainImage($wikiId, $langCode, $imageSource, $currentData) {
+		return json_decode($imageSource->city_main_image);
+	}
 }
--- extensions/wikia/WikiaHomePage/classes/WikiGetDataHelper.class.php
+++ extensions/wikia/WikiaHomePage/classes/WikiGetDataHelper.class.php
@@ -3,4 +3,5 @@
 interface WikiGetDataHelper {
 	public function getMemcKey($wikiId, $langCode);
 	public function getImages($wikiId, $langCode, $wikiRow);
+	public function getMainImage($wikiId, $langCode, $imageSource, $currentData);
 }
