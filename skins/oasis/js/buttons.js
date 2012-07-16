936da607ff8c737b99f57910d8277b8d7c118f31
macbre <macbre@wikia-inc.com>
2012-07-16 15:08:12 +0000

    buttons.js: cache jQuery selectors
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56568 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/js/buttons.js
+++ skins/oasis/js/buttons.js
@@ -6,11 +6,11 @@ var WikiaButtons = {
 	init: function(limit) {
 		//Variables
 		if(typeof limit == 'object') {
-			WikiaButtons.menuButtons = $(".wikia-menu-button", limit);	
+			WikiaButtons.menuButtons = $(".wikia-menu-button", limit);
 		} else {
 			WikiaButtons.menuButtons = $(".wikia-menu-button");
 		}
-		
+
 		WikiaButtons.timer = null;
 
 		//Events
@@ -18,15 +18,15 @@ var WikiaButtons = {
 	},
 
 	add: function( e, opts ) {
-		 var defaults = {
-		  click: WikiaButtons.click
-		 };
-		 var s = $.extend({},defaults,opts||{});
-		 e.unbind('.wikiabutton')
-		  .one("mouseover.wikiabutton", WikiaButtons.setup)
-		  .bind('mouseenter.wikiabutton',WikiaButtons.mouseover)
-		  .bind('mouseleave.wikiabutton',WikiaButtons.mouseout)
-		  .bind('click.wikiabutton',s.click);
+		var defaults = {
+			click: WikiaButtons.click
+		};
+		var s = $.extend({},defaults,opts||{});
+		e.unbind('.wikiabutton')
+			.one("mouseover.wikiabutton", WikiaButtons.setup)
+			.bind('mouseenter.wikiabutton',WikiaButtons.mouseover)
+			.bind('mouseleave.wikiabutton',WikiaButtons.mouseout)
+			.bind('click.wikiabutton',s.click);
 	},
 
 	setup: function() {
@@ -35,34 +35,28 @@ var WikiaButtons = {
 	},
 
 	click: function(event) {
-		if(!$(this).attr('disabled')){
+		var button = $(this);
+
+		if(!button.attr('disabled')){
 			if(event.target.tagName !== 'A'){
-				var width = $(this).outerWidth();
+				var width = button.outerWidth();
 
-				if(width !== $(this).data('prevWidth')){
-					$(this).data('prevWidth', width);
+				if(width !== button.data('prevWidth')){
+					button.data('prevWidth', width);
 
-					var menu = $(this).data('menu');
+					var menu = button.data('menu');
 					width = width - (parseInt(menu.css('padding-left')) + parseInt(menu.css('padding-right')) + parseInt(menu.css('border-left-width')) + parseInt(menu.css('border-right-width')));
 
 					menu.css("min-width", width);
 				}
 
-				$(this).toggleClass("active");
+				button.toggleClass("active");
 			}
 		}
 		else{
 			event.preventDefault();
 			return false;
 		}
-	},
-
-	clickToggle: function(event) {
-		var m = $('>li>ul',$(this));
-		if (m.length == 0 || $.contains(m[0],event.target)) {
-			return;
-		}
-		$(this).toggleClass("active");
 	},
 
 	mouseover: function() {
@@ -73,11 +67,11 @@ var WikiaButtons = {
 	mouseout: function(event) {
 		//Delay before hiding menu
 		var dalay = $(event.target).closest('.wikia-menu-button').attr('data-delay');
-	
+
 		if((typeof dalay) != 'string') {
 			dalay = WikiaButtons.settings.delay;
-		}		
-		
+		}
+
 		WikiaButtons.timer = setTimeout(function() {
 			$(event.currentTarget).removeClass("active");
 		}, dalay);
