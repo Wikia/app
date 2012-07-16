e522069c91d6f50baa230493a1842344b3b76377
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-16 12:39:13 +0000

    ImageServing inclusion should be moved to DefaultSettings (BugId:8166)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56559 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -380,6 +380,8 @@ include_once( "$IP/extensions/wikia/ShareButtons/ShareButtons.setup.php" );
 include_once( "$IP/extensions/wikia/SpecialUnlockdb/SpecialUnlockdb.setup.php" );
 include_once( "$IP/extensions/wikia/WikiaWantedQueryPage/WikiaWantedQueryPage.setup.php" );
 include_once( "$IP/includes/wikia/Resources.php" );
+include_once( "$IP/extensions/wikia/ImageServing/imageServing.setup.php" );
+include_once( "$IP/extensions/wikia/ImageServing/Test/ImageServingTest.setup.php" );
 
 /**
  * @name $wgSkipSkins
