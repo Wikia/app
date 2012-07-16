046930c9dbf31fe40c2c8f7da5619dc833fb5e2f
kflorence <kflorence@wikia-inc.com>
2012-07-16 22:02:57 +0000

    Fix loading $.tmpl for mini editor.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56592 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -496,6 +496,7 @@ $config['eplrte'] = array(
 $config['mini_editor_js'] = array(
 	'type' => AssetsManager::TYPE_JS,
 	'assets' => array(
+		'//resources/wikia/libraries/jquery/tmpl/jquery.tmpl.js',
 		'//extensions/wikia/EditPageLayout/js/editor/WikiaEditor.js',
 		'//extensions/wikia/EditPageLayout/js/editor/Buttons.js',
 		'//extensions/wikia/EditPageLayout/js/editor/Modules.js',
