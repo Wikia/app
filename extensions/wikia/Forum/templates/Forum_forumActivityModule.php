4e299751ea389bb9e8e5649d18d0e1417fc73dee
hyun <hyun@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-18 18:57:43 +0000

    standardizing right rail module to WikiaActivityModue
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56739 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Forum/templates/Forum_forumActivityModule.php
+++ extensions/wikia/Forum/templates/Forum_forumActivityModule.php
@@ -1,15 +1,17 @@
-<section class="module ForumActivityModule">
+<section class="module WikiaActivityModule ForumActivityModule">
 	<h1><?= wfMsg('forum-activity-module-heading') ?></h1>
 	<ul>
 		<?php foreach($posts as $value): ?>
-		<li class="forum-activity">
+		<li>
 			<?= AvatarService::renderAvatar($value['user']->getName(), 24); ?>
-			<h2>
+			<em>
 				<a href="<?= $value['wall_message']->getMessagePageUrl(); ?>"><?= $value['metatitle']; ?></a>
-			</h2>
-			<?php $user = '<a href="'.$value['user']->getUserPage()->getFullUrl().'">'.$value['display_username'].'</a>'; ?>
-			<?php $time = '<span class="timeago abstimeago" title="'.$value['event_iso'].'" alt="'.$value['event_mw'].'">&nbsp;</span>' ?>
-			<?= wfMsg( $value['is_reply'] ? 'forum-activity-module-posted':'forum-activity-module-started', array($user, $time )); ?>			
+			</em>
+			<div class="edited-by">
+				<?php $user = '<a href="'.$value['user']->getUserPage()->getFullUrl().'">'.$value['display_username'].'</a>'; ?>
+				<?php $time = '<span class="timeago abstimeago" title="'.$value['event_iso'].'" alt="'.$value['event_mw'].'">&nbsp;</span>' ?>
+				<?= wfMsg( $value['is_reply'] ? 'forum-activity-module-posted':'forum-activity-module-started', array($user, $time )); ?>			
+			</div>
 		</li>
 		<?php endforeach; ?>
 	</ul>
--- extensions/wikia/Forum/templates/Forum_forumParticipationModule.php
+++ extensions/wikia/Forum/templates/Forum_forumParticipationModule.php
@@ -1,17 +1,19 @@
-<section class="module ForumParticipationModule">
+<section class="module WikiaActivityModule ForumParticipationModule">
 	<h1><?= wfMsg('forum-participation-module-heading') ?></h1>
 	<ul>
 		<?php foreach($participants as $value): ?>
-			<li class="forum-participant">
+			<li>
 				<?= AvatarService::renderAvatar($value['user']->getName(), 24); ?>
-				<h2>
+				<em>
 					<a href="<?php echo $value['user']->getUserPage()->getFullUrl(); ?>"><?php echo $value['display_username'] ?></a>
 					<?php if($value['user']->getId() == 0): ?>
 						<a href="<?php echo $value['user']->getUserPage()->getFullUrl(); ?>" class="subtle"><?php echo $value['user']->getName();?></a>
 					<?php endif; ?>
-				</h2>
-				<?php $time = '<span class="timeago abstimeago" title="'.$value['event_iso'].'" alt="'.$value['event_mw'].'">&nbsp;</span>' ?>
-				<?= wfMsg($value['is_reply'] ? 'forum-participation-module-posted':'forum-participation-module-started', array($value['wall_message']->getMessagePageUrl(), $time) ); ?>
+				</em>
+				<div class="edited-by">
+					<?php $time = '<span class="timeago abstimeago" title="'.$value['event_iso'].'" alt="'.$value['event_mw'].'">&nbsp;</span>' ?>
+					<?= wfMsg($value['is_reply'] ? 'forum-participation-module-posted':'forum-participation-module-started', array($value['wall_message']->getMessagePageUrl(), $time) ); ?>
+				</div>
 			</li>
 		<?php endforeach; ?>
 	</ul>
