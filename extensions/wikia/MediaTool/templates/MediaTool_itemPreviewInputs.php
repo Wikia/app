1ad008fe8d9dcb915000aa6a9350c98d3a2a3d04
mech <mech@wikia-inc.com>
2012-07-18 13:51:14 +0000

    MediaTool: preview area inputs (bugid:37982)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56710 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/MediaTool/templates/MediaTool_itemPreviewInputs.php
+++ extensions/wikia/MediaTool/templates/MediaTool_itemPreviewInputs.php
@@ -1,4 +1,17 @@
 {{{itemPreview}}}
 <div class="media-item-details">
-	<input name="zzzzzzzzzzz" />
+	<div class="file-details">
+		<h4>File Details</h4>
+		<label>File name</label><br/>
+		<input name="zzzzzzzzzzz" /><br/>
+		<label>File Description</label><br/>
+		<textarea rows="4"></textarea><br/>
+		<input type="checkbox" /><label>Follow this media</label>
+	</div>
+	<div class="article-details">
+		<h4>Article Details</h4>
+		<label>Caption</label><br/>
+		<input name="xxxxxx" />
+	</div>
+
 </div>
