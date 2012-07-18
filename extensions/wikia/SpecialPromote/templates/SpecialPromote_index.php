4c845f2fe8308fe50aac7936eff692ce944c2ce3
nandy <nandy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-18 09:49:10 +0000

    bugId:30990; small change of function name connected with pevious commit
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56692 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialPromote/templates/SpecialPromote_index.php
+++ extensions/wikia/SpecialPromote/templates/SpecialPromote_index.php
@@ -6,7 +6,7 @@
 			</span>
 			<span class="status-arrow"></span>
 		</div>
-		<?= $wikiStatus ? strtr( wfMsgExt('promote-statusbar-approved', array('parseinline')), $approvedParams ) : wfMsg('promote-statusbar-inreview') ?>
+		<?= $wikiStatus ? strtr( wfMsg('promote-statusbar-approved'), $approvedParams ) : wfMsg('promote-statusbar-inreview') ?>
 	</div>
 	<p class='introduction'>
 		<strong><?= wfMsg('promote-introduction-header'); ?></strong><br/>
