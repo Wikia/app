ece82829e7ebb9d01b0697809692a801eea2cf23
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-20 19:31:51 +0000

    WikiaMobile: fix ad positioning on page load (BugzId:38017)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@56906 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/js/ads.js
+++ extensions/wikia/WikiaMobile/js/ads.js
@@ -1,14 +1,17 @@
-/*
- * @define ads
- * module used to handle ads on wikiamobile
+/**
+ * Module used to handle ads on wikiamobile
  * closing it and keeping it on the screen
  *
+ * @define ads
  * @require events
  *
  * @author Jakub Olek
  */
+/*global window, document, define, setTimeout,
+setInterval, clearInterval, Modernizr*/
 
-define('ads', ['events'], function(ev){
+define('ads', ['events'], function (ev) {
+	'use strict';
 
 	var d = document,
 		adSlot,
@@ -16,50 +19,68 @@ define('ads', ['events'], function(ev){
 		w,
 		ftr;
 
-	function moveSlot(plus){
-		if(adSlotStyle){
-			adSlotStyle.top = Math.min((w.pageYOffset + w.innerHeight - 50 + ~~plus), ftr.offsetTop + 150) + 'px';
+	function moveSlot(plus) {
+		if (adSlotStyle) {
+			if (Modernizr.positionfixed) {
+				//position fixed needs to be applied after
+				//a delay to avoid the Ad to jump in the
+				//middle of the screen and then back to
+				//the bottom on iOS 5+ (BugzId:38017)
+				setTimeout(
+					function () {
+						adSlot.className += ' fixed';
+					},
+					1000
+				);
+			} else {
+				adSlotStyle.top = Math.min(
+					(w.pageYOffset + w.innerHeight - 50 + ~~plus),
+					ftr.offsetTop + 150
+				) + 'px';
+			}
 		}
 	}
 
-	function init(){
+	function init() {
 		adSlot = d.getElementById('wkAdPlc');
 
-		if(adSlot){
+		if (adSlot) {
 			adSlotStyle = adSlot.style;
 			w = window;
 			ftr = d.getElementById('wkFtr');
 
 			var close = d.getElementById('wkAdCls'),
 				i = 0,
+				int,
 				click = ev.click,
-				adExist = function(){
-					if(adSlot.childElementCount > 3){
+				adExist = function () {
+					if (adSlot.childElementCount > 3) {
 						close.className = 'show';
 						adSlot.className += ' show';
 
-						close.addEventListener(click, function() {
+						close.addEventListener(click, function () {
 							//track('ad/close');
 							adSlot.className += ' anim';
-							setTimeout(function(){d.body.removeChild(adSlot);},800);
+							setTimeout(function () {
+								d.body.removeChild(adSlot);
+							}, 800);
 							w.removeEventListener('scroll', moveSlot);
 						}, false);
 
-						if(Modernizr.positionfixed){
-							adSlot.className += ' fixed';
-						}else{
+						if (!Modernizr.positionfixed) {
 							w.addEventListener('scroll', moveSlot);
 						}
 						return true;
 					}
+
+					return false;
 				};
 
-			if(!adExist()) {
-
-				var int = setInterval(function() {
-					if(!adExist() && i < 5) {
+			if (!adExist()) {
+				int = setInterval(function () {
+					if (!adExist() && i < 5) {
 						i += 1;
-					}else{
+					} else {
 						d.body.removeChild(adSlot);
 						clearInterval(int);
 					}
@@ -71,5 +92,5 @@ define('ads', ['events'], function(ev){
 	return {
 		init: init,
 		moveSlot: moveSlot
-	}
+	};
 });
--- extensions/wikia/WikiaMobile/js/hide_url_bar.js
+++ extensions/wikia/WikiaMobile/js/hide_url_bar.js
@@ -1,20 +1,28 @@
-/*
- *	@define hideURLBar
- *	hides url bar
+/**
+ * Hides url bar
+ * to ensure that ad is placed correctly
+ * when url bar is being hidden
  *
- * 	@require ads
- * 	to ensure that ad is placed correctly when url bar is being hidden
+ * @define hideURLBar
+ * @require ads
  *
- * 	@author Jakub Olek
+ * @author Jakub Olek
  */
-define('hideURLBar', ['ads'], function(ads){
+/*global window, define, setTimeout*/
+
+define('hideURLBar', ['ads'], function (ads) {
+	'use strict';
+
 	var w = window;
-	return function(){
-		if(w.pageYOffset < 20) {
-			setTimeout(function(){
+
+	return function () {
+		if (w.pageYOffset < 20) {
+			setTimeout(function () {
 				w.scrollTo(0, 1);
-				if(!Modernizr.positionfixed && ads) ads.moveSlot();
-			}, 1);
+				if (ads) {
+					ads.moveSlot();
+				}
+			}, 100);
 		}
-	}
+	};
 });
