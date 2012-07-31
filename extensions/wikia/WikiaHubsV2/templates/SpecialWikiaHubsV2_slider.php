2f66541aeede9a908d33deb066e56224e9617df1
marzjan <sebastianm@wikia-inc.com>
2012-07-31 13:01:32 +0000

    BugID: 43167 - Structured Hubs, Gallery, adding comments, WIP
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57445 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_slider.php
+++ extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_slider.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This needs to be generated in the template, as we have to avoid
+ * assigning html output of gallery in the controller in order
+ * to prevent displaying it in raw/JSON output formats
+ *
+ */
+
 $app = F::app();
 $galleryText = '<gallery type="slider" orientation="mosaic">';
 foreach($images as $image) {
@@ -15,4 +22,5 @@ foreach($images as $image) {
 $galleryText .= "\n</gallery>";
 
 echo $app->wg->parser->parse( $galleryText, $app->wg->title, $app->wg->out->parserOptions(),true )->getText();
+
 ?>
