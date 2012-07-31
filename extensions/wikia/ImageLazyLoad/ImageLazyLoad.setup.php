4d61117a0966f87f7471bd756dc8f50bcc107d6d
pbablok <pbablok@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-07-31 17:07:27 +0000

    Image Lazy Loading - in articles (also lazy loads images for video)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57462 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/ImageLazyLoad/ImageLazyLoad.setup.php
@@ -0,0 +1,13 @@
+<?php
+/* Lazy loading for images inside articles (skips wikiamobile skin)
+ * @author Piotr Bablok <pbablok@wikia-inc.com>
+ */
+
+
+$dir = dirname(__FILE__);
+$wgAutoloadClasses['ImageLazyLoad'] = "$dir/ImageLazyLoad.class.php";
+
+
+$app = F::app();
+$app->registerHook( 'ThumbnailImageHTML', 'ImageLazyLoad', 'onThumbnailImageHTML' );
+$app->registerHook( 'ThumbnailVideoHTML', 'ImageLazyLoad', 'onThumbnailImageHTML' );
