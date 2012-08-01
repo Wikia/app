1651a0851c6cda4a038834da562cd4872ff886a3
marzjan <marzjan@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-01 12:51:24 +0000

    BugID: 43197 - Structured Hubs, layout tweaks
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57531 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_topwikis.php
@@ -0,0 +1,42 @@
+<div class="grid-2 alpha wikiahubs-top-wikis">
+	<h2>
+		<span class="mw-headline" id="Top_Gaming_Wikis">Top Gaming Wikis</span>
+	</h2>
+	<div class="description">
+		Here are the top 10 Video Game wikis based on wiki activity, breadth of content and awesomeness.
+	</div>
+	<div class="top-wikis-content">
+		<ol>
+			<li>
+				<a  class="text" href="http://elderscrolls.wikia.com/?redirect=no">The Elder Scrolls Wiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://masseffect.wikia.com/?redirect=no">Mass Effect Wiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://runescape.wikia.com/?redirect=no">RuneScape Wiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://fallout.wikia.com/?redirect=no">Fallout Wiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://www.wowwiki.com/?redirect=no">WoWWiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://wiki.ffxiclopedia.org/?redirect=no">Final Fantasy XI Wiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://leagueoflegends.wikia.com/?redirect=no">League of Legends Wiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://callofduty.wikia.com/?redirect=no">Call of Duty Wiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://finalfantasy.wikia.com/?redirect=no">Final Fantasy Wiki</a>
+			</li>
+			<li>
+				<a  class="text" href="http://tibia.wikia.com/?redirect=no">Tibia Wiki</a>
+			</li>
+		</ol>
+	</div>
+</div>
