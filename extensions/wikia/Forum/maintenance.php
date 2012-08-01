32717583464bd3598d2d4e029dbba7e949c0525b
saipetch <saipetch@wikia-inc.com>
2012-08-01 20:17:06 +0000

    Forum (bugid 43547): update maintenance script
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57566 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Forum/maintenance.php
+++ extensions/wikia/Forum/maintenance.php
@@ -76,6 +76,8 @@
 
 	// insert data into commnets_index table
 	function insertIntoCommentsIndex( $parentPageId, $articleComment, $parentCommentId = 0, $lastChildCommentId = 0 ) {
+		global $isDryrun;
+
 		$data = array(
 			'parentPageId' => $parentPageId,
 			'commentId' => $articleComment->getTitle()->getArticleID(),
@@ -89,7 +91,10 @@
 
 		$data = array_merge($data, getCommentProperties($articleComment) );
 		$commentsIndex = F::build( 'CommentsIndex', array($data) );
-		$commentsIndex->addToDatabase();
+
+		if ( !$isDryrun ) {
+			$commentsIndex->addToDatabase();
+		}
 
 		return true;
 	}
@@ -100,6 +105,18 @@
 
 	require_once( "commandLine.inc" );
 
+	if ( isset($options['help']) ) {
+		die( "Usage: php maintenance.php [--dry-run] [--forum] [--wall] [--skip_reset] [--from_id=12345] [--to_id=12345] [--insert_data] [--help]
+		--dryrun			dry run
+		--forum				only forum
+		--wall				only wall pages
+		--skip_reset		skip delete data from comments_index table
+		--from_id			from page id
+		--to_id				to page id
+		--insert_data		only insert data to comments_index table
+		--help				you are reading it right now\n\n" );
+	}
+
 	$app = F::app();
 	if ( empty($app->wg->CityId) ) {
 		die( "Error: Invalid wiki id." );
@@ -109,107 +126,154 @@
 		die( "Error: In read only mode." );
 	}
 
+	$isDryrun = ( isset($options['dryrun']) ) ? true : false ;
+	$forumOnly = ( isset($options['forum']) ) ? true : false ;
+	$wallOnly = ( isset($options['wall']) ) ? true : false ;
+	$skipReset = ( isset($options['skip_reset']) ) ? true : false ;
+	$insertDataOnly = ( isset($options['insert_data']) ) ? true : false ;
+	$fromId = ( isset($options['from_id']) ) ? intval($options['from_id']) : null ;
+	$maxId = ( isset($options['to_id']) ) ? intval($options['to_id']) : null ;
+
 	echo "Wiki $wgCityId:\n";
-
-	// create table or patch table schema
-	$commentsIndex = F::build( 'CommentsIndex' );
-	$commentsIndex->patchTableCommentsIndexV1();
-
-	echo "Updated database schema for comment_index table.\n";
 
 	$db = $app->wf->GetDB( DB_MASTER );
 
-	// clear data in the table
-	$db->delete( 'comments_index', '*' );
+	if ( !$isDryrun && !$insertDataOnly ) {
+		// create table or patch table schema
+		$commentsIndex = F::build( 'CommentsIndex' );
+		$commentsIndex->patchTableCommentsIndexV1();
 
-	echo "Removed data from comments_index table.\n";
+		echo "Updated database schema for comment_index table.\n";
 
-	// get min/max comments
-	$row = $db->selectRow(
-		array( 'page' ),
-		array( 'min(page_id) min_id, max(page_id) max_id' ),
-		array(
-			'page.page_title '.$db->buildLike( $db->anyString(), sprintf( "/%s", ARTICLECOMMENT_PREFIX ), $db->anyString() ),
-			'page_latest > 0'	// BugId:22821
-		),
-		__METHOD__
+		// clear data in the table
+		if ( !$skipReset ) {
+			$db->delete( 'comments_index', '*' );
+		}
+
+		echo "Removed data from comments_index table.\n";
+	}
+
+	$cmdOptions = '';
+
+	// set where clauses
+	$sqlWhereBase = array(
+		'page.page_title '.$db->buildLike( $db->anyString(), sprintf( "/%s", ARTICLECOMMENT_PREFIX ), $db->anyString() ),
+		'page_latest > 0'	// BugId:22821
 	);
 
-	$fromId = 0;
-	$maxId = 0;
-	if ($row) {
-		$fromId = intval( $row->min_id );
-		$maxId = intval( $row->max_id );
+	if ( $wallOnly ) {
+		$sqlWhereBase['page_namespace'] = NS_USER_WALL_MESSAGE;
+		$cmdOptions .= ' --wall';
 	}
 
-	if ( $fromId > 0 ) {
-		$fromId = $fromId - 1;
+	if ( $forumOnly ) {
+		$sqlWhereBase['page_namespace'] = NS_WIKIA_FORUM_BOARD_THREAD;
+		$cmdOptions .= ' --forum';
 	}
+
+	// get min/max comments
+	if ( is_null($fromId) || is_null($maxId) ) {
+		$row = $db->selectRow(
+			array( 'page' ),
+			array( 'min(page_id) min_id, max(page_id) max_id' ),
+			$sqlWhereBase,
+			__METHOD__
+		);
+
+		if ( is_null($fromId) ) {
+			$fromId = ( $row ) ? intval( $row->min_id ) : 0 ;
+			if ( $fromId > 0 ) {
+				$fromId = $fromId - 1;
+			}
+		}
+
+		if ( is_null($maxId) ) {
+			$maxId = ( $row ) ? intval( $row->max_id ) : 0 ;
+		}
+	}
+
 
 	$cnt = 0;
 	$failed = 0;
 	$range = 1000;
-	while( $fromId <= $maxId ) {
+	while( $fromId < $maxId ) {
 		$toId = ( ($fromId + $range) < $maxId ) ? $fromId + $range : $maxId ;
 		echo "LOOP: From ID $fromId to ID $toId (Max ID = $maxId).\n";
 
-		// get comment list
-		$result = $db->select(
-			array( 'page' ),
-			array( 'page_id',
-				'page_title',
-				'page_namespace',
-				'page_len',
-				'page_is_redirect',
-				'page_latest'
-			),
-			array(
+		if ( $maxId - $toId >= $range ) {
+			$cmd = 'SERVER_ID='.$app->wg->CityId.' php '.$IP.'/extensions/wikia/Forum/maintenance.php '.
+				'--conf='.$app->wg->WikiaLocalSettingsPath.' --from_id='.$fromId.' --to_id='.$toId.
+				$cmdOptions.' --insert_data > /tmp/forum'.$fromId.'_'.$toId.'.log & ';
+			echo "command: $cmd\n";
+			if ( !$isDryrun ) {
+				$result = wfShellExec( $cmd, $retval );
+				echo "result: $result\n";
+				if ( $retval ) {
+					echo "Error code $retval: $result \n";
+				} else {
+					echo "$result \n";
+				}
+			}
+		} else {
+			$sqlWhere = array(
 				'page_id > '.$fromId,
-				'page_id <= '.$toId,
-				'page.page_title '.$db->buildLike( $db->anyString(), sprintf( "/%s", ARTICLECOMMENT_PREFIX ), $db->anyString() ),
-				'page_latest > 0'	// BugId:22821
-			),
-			__METHOD__,
-			array( 'ORDER BY' => 'page_id ASC' )
-		);
+				'page_id <= '.$toId
+			);
+			$sqlWhere = array_merge( $sqlWhereBase, $sqlWhere );
 
-		$commentList = array();
-		while ( $row = $db->fetchObject( $result ) ) {
-			$commentList[$row->page_id] = $row;
-		}
-		$db->freeResult( $result );
+			// get comment list
+			$result = $db->select(
+				array( 'page' ),
+				array( 'page_id',
+					'page_title',
+					'page_namespace',
+					'page_len',
+					'page_is_redirect',
+					'page_latest'
+				),
+				$sqlWhere,
+				__METHOD__,
+				array( 'ORDER BY' => 'page_id ASC' )
+			);
 
-		// collect and insert data
-		foreach ( $commentList as $comment ) {
-			$cnt++;
-			echo "$cnt [$fromId-$toId]: ID $comment->page_id(NS $comment->page_namespace): $comment->page_title";
-
-			$title = F::build( 'Title' , array( $comment ), 'newFromRow' );
-			$articleComment = F::build( 'ArticleComment', array( $title ), 'newFromTitle' );
-			$articleComment->load();
-
-			// parent page id
-			$parentPage = getParentPage( $articleComment );
-			if ( !$parentPage instanceof ArticleComment ) {
-				echo ".....Parent page NOT found.\n";
-				$failed++;
-				continue;
+			$commentList = array();
+			while ( $row = $db->fetchObject( $result ) ) {
+				$commentList[$row->page_id] = $row;
 			}
-			$parentPageId = $parentPage->getTitle()->getArticleID();
+			$db->freeResult( $result );
 
-			// get parent comment id
-			$parentCommentObj = $articleComment->getTopParentObj();
-			if ( $parentCommentObj instanceof ArticleComment ) { // posts/replies
-				$parentCommentId = $parentCommentObj->getTitle()->getArticleID();
-				$lastChildCommentId = 0;
-			} else { // main comments
-				$parentCommentId = 0;
-				$lastChildCommentId = getLastChildCommentId( $articleComment );
+			// collect and insert data
+			foreach ( $commentList as $comment ) {
+				$cnt++;
+				echo "$cnt [$fromId-$toId]: ID $comment->page_id(NS $comment->page_namespace): $comment->page_title";
+
+				$title = F::build( 'Title' , array( $comment ), 'newFromRow' );
+				$articleComment = F::build( 'ArticleComment', array( $title ), 'newFromTitle' );
+				$articleComment->load();
+
+				// parent page id
+				$parentPage = getParentPage( $articleComment );
+				if ( !$parentPage instanceof ArticleComment ) {
+					echo ".....Parent page NOT found.\n";
+					$failed++;
+					continue;
+				}
+				$parentPageId = $parentPage->getTitle()->getArticleID();
+
+				// get parent comment id
+				$parentCommentObj = $articleComment->getTopParentObj();
+				if ( $parentCommentObj instanceof ArticleComment ) { // posts/replies
+					$parentCommentId = $parentCommentObj->getTitle()->getArticleID();
+					$lastChildCommentId = 0;
+				} else { // main comments
+					$parentCommentId = 0;
+					$lastChildCommentId = getLastChildCommentId( $articleComment );
+				}
+
+				// insert main comment
+				insertIntoCommentsIndex( $parentPageId, $articleComment, $parentCommentId, $lastChildCommentId );
+				echo ".....DONE.\n";
 			}
-
-			// insert main comment
-			insertIntoCommentsIndex( $parentPageId, $articleComment, $parentCommentId, $lastChildCommentId );
-			echo ".....DONE.\n";
 		}
 
 		$fromId += $range;
