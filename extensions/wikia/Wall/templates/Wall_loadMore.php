af5e586d88ecc7b64716a89916889bda918f12e3
tor <tor@wikia-inc.com>
2012-08-01 00:06:23 +0000

    fixed plural support in Wall BugzId:39504
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57508 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Wall/templates/Wall_loadMore.php
+++ extensions/wikia/Wall/templates/Wall_loadMore.php
@@ -1,3 +1,3 @@
 <li class="load-more">
-	<a href="#"><?= wfMsgExt('wall-message-loadmore', array('parsemag'), array('$1' => '<span class="count" >'.$repliesNumber.'</span>')) ?></a>
+	<a href="#"><?= wfMsgExt('wall-message-loadmore', array('parsemag'), array( $repliesNumber )) ?></a>
 </li>
