5fe6f8f5f35030f1c985b72abbef44fe848749f8
garth <garth@wikia-inc.com>
2012-08-03 21:00:55 +0000

    Use sed to strip the unwanted first lines.
    BugID: 34583
    
    
    
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57719 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/vm72_cleanup.sh
+++ maintenance/wikia/VideoHandlers/vm72_cleanup.sh
@@ -6,15 +6,13 @@ TMPFILE=`mktemp /tmp/$0.XXXXXX` || exit 1
 echo "Getting list of wikis..."
 echo "select distinct wiki_id, wiki_dbname from video_migration_log where action_name='POSTMIGRATION' and action_status='STOP'" | /usr/wikia/backend/bin/master dataware > $TMPFILE
 
-cat $TMPFILE | while read line; do
+sed '1,2d' $TMPFILE | while read line; do
 	cityid=`echo "$line" | cut -f 1`
 	dbname=`echo "$line" | cut -f 2`
 
-	if [[ $dbname != "wiki_dbname" ]]; then
-		echo "Processing $line ($cityid, $dbname)"
+	echo "Processing $line ($cityid, $dbname)"
 
-		/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoCleanupImageLinks.php" --usedb=$dbname | tee -a logs/${cityid}.cleanup.log || die
+	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoCleanupImageLinks.php" --usedb=$dbname | tee -a logs/${cityid}.cleanup.log || die
 
-		echo "---"
-	fi
+	echo "---"
 done
