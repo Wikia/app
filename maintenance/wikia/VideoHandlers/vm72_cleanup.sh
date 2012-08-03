8d50e90e5f08328e960cd4c93b570c5cc786049d
garth <garth@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-03 20:54:05 +0000

    Don't process the header line
    BugID: 34583
    
    
    
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57718 e846bd91-c042-42a7-a218-4b65e2c1904d

--- maintenance/wikia/VideoHandlers/vm72_cleanup.sh
+++ maintenance/wikia/VideoHandlers/vm72_cleanup.sh
@@ -4,14 +4,17 @@ scriptpath="../../../../../usr/wikia/mac/maintenance/wikia/VideoHandlers"
 
 TMPFILE=`mktemp /tmp/$0.XXXXXX` || exit 1
 echo "Getting list of wikis..."
-echo "select distinct wiki_id, wiki_id, wiki_dbname from video_migration_log where action_name='POSTMIGRATION' and action_status='STOP'" | /usr/wikia/backend/bin/master dataware > $TMPFILE
-cat $TMPFILE
+echo "select distinct wiki_id, wiki_dbname from video_migration_log where action_name='POSTMIGRATION' and action_status='STOP'" | /usr/wikia/backend/bin/master dataware > $TMPFILE
+
 cat $TMPFILE | while read line; do
 	cityid=`echo "$line" | cut -f 1`
-	dbname=`echo "$line" | cut -f 3`
-	echo "Processing $line ($cityid, $dbname)"
+	dbname=`echo "$line" | cut -f 2`
 
-	/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoCleanupImageLinks.php" --usedb=$dbname | tee -a logs/${cityid}.cleanup.log || die
+	if [[ $dbname != "wiki_dbname" ]]; then
+		echo "Processing $line ($cityid, $dbname)"
 
-	echo "---"
+		/usr/wikia/backend/bin/withcity --maintenance-script="$scriptpath/videoCleanupImageLinks.php" --usedb=$dbname | tee -a logs/${cityid}.cleanup.log || die
+
+		echo "---"
+	fi
 done
