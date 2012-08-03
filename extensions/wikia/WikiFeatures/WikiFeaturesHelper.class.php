df3849c7aa2263a036e03396c0ba6168a0c5e213
saipetch <saipetch@wikia-inc.com>
2012-08-03 20:24:21 +0000

    WikiFeatures: update getNumActiveWikis() - fix memcache
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57715 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiFeatures/WikiFeaturesHelper.class.php
+++ extensions/wikia/WikiFeatures/WikiFeaturesHelper.class.php
@@ -103,7 +103,7 @@ class WikiFeaturesHelper extends WikiaModel {
 		
 		$memKey = $this->getMemcKeyNumActiveWikis($feature);
 		$num = $this->wg->Memc->get($memKey);
-		if (is_null($num)) {
+		if ( $num === false ) {
 			$db = $this->wf->GetDB(DB_SLAVE, array(), $this->wg->ExternalSharedDB);
 
 			$result = $db->selectRow(
