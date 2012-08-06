df8ef4593be7df627ee3763f80834be10164f909
michalnowierski <michalnowierski@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-06 14:05:34 +0000

    Hubs update
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57754 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/Hubs/VideoGamesHub.java
@@ -0,0 +1,24 @@
+package com.wikia.webdriver.pageObjects.PageObject.Hubs;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.wikia.webdriver.pageObjects.PageObject.Hub;
+
+public class VideoGamesHub extends Hub {
+
+
+	
+	
+	public VideoGamesHub(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+
+}
