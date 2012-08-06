eb3aaef1d617636d33bdf0cc314032fe095cdf74
macbre <macbre@wikia-inc.com>
2012-08-06 09:41:33 +0000

    Wall: wfGetIP() is deprecated
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57741 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Wall/VoteHelper.class.php
+++ extensions/wikia/Wall/VoteHelper.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 //TODO: move this to services
 
@@ -8,18 +8,19 @@ class VoteHelper {
 	protected $userIP = '';
 
 	function __construct($user = null, $pageId = 0) {
+		global $wgRequest;
 		wfProfileIn(__METHOD__);
-		$this->userIP = wfGetIP();
+		$this->userIP = $wgRequest->getIP();
 		if($user->isLoggedIn() ) {
 			$this->userId = $user->getId();
 		} else {
 			$this->userId = 0;
 		}
-				
+
 		$this->pageId = $pageId;
 		wfProfileOut(__METHOD__);
 	}
-	
+
 	//TODO: caching ?
 	function getVotersList($from = 0, $count = 50) {
 		wfProfileIn(__METHOD__);
@@ -27,10 +28,10 @@ class VoteHelper {
 		//TODO: abstract the query
 		$res = $dbr->select( array('page_vote', 'ipblocks'),
 			array( 'user_id' ),
-			array( 
+			array(
 				"ifnull(ipb_expiry, 0) != 'infinity'",
 				'ifnull(ipb_expiry, 0) < '.wfTimestamp(TS_MW),
-				'article_id' => $this->pageId,			
+				'article_id' => $this->pageId,
 			),
 			__METHOD__,
 			array(
@@ -40,21 +41,21 @@ class VoteHelper {
 			),
 			array( 'ipblocks'  => array( 'LEFT JOIN', 'user_id = ipb_user' ))
 		);
-   
+
 		$out = array();
-		
+
 		while($row = $res->fetchObject()) {
 			$out[] = $row->user_id;
 		}
-		
+
 		$this->getCache()->delete( $this->getPageCacheKey() );
-		
+
 		$this->getVoteCount();
-		
+
 		wfProfileOut(__METHOD__);
 		return $out;
 	}
-	
+
 	function addVote($score = 1) {
 		wfProfileIn(__METHOD__);
 		if($this->isVoted()) {
@@ -62,20 +63,20 @@ class VoteHelper {
 			return false;
 		}
 		$dbr = wfGetDB( DB_MASTER );
-		
+
 		$values = array();
 		$values['article_id'] = $this->pageId;
-		$values['ip'] = $this->userIP; 
+		$values['ip'] = $this->userIP;
 		$values['user_id'] = $this->userId;
-		
+
 		$values['unique_id'] = md5($this->userIP); //Backward compatibility
 		$values['time'] = wfTimestampNow();
 		$values['vote'] = $score;
-		
+
 		$dbr->insert( 'page_vote',
 			$values,
 		__METHOD__ );
-		
+
 		$cache = $this->getCache();
 		$cache->set($this->getUserCacheKey(), 'Y', 60*60);
 		$this->getVoteCount(DB_MASTER);
@@ -89,7 +90,7 @@ class VoteHelper {
 		$dbr->delete( 'page_vote',
 			$this->getUserWhere(),
 		__METHOD__ );
-		
+
 		$cache = $this->getCache();
 		$cache->set($this->getUserCacheKey(), 'N');
 		$this->getVoteCount(DB_MASTER);
@@ -111,10 +112,10 @@ class VoteHelper {
 		//TODO: abstract the query
 		$row = $dbr->selectRow( array('page_vote', 'ipblocks'),
 			array( 'count(*) as cnt' ),
-			array( 
+			array(
 				"ifnull(ipb_expiry, 0) != 'infinity'",
 				'ifnull(ipb_expiry, 0) < '.wfTimestamp(TS_MW),
-				'article_id' => $this->pageId,			
+				'article_id' => $this->pageId,
 			),
 			__METHOD__,
 			array(
@@ -122,21 +123,21 @@ class VoteHelper {
 			),
 			array( 'ipblocks'  => array( 'LEFT JOIN', 'user_id = ipb_user' ))
 		);
-		
+
 		$cache->set($this->getPageCacheKey(), $row->cnt, 60*60);
 		wfProfileOut(__METHOD__);
-		return (int) $row->cnt; 
+		return (int) $row->cnt;
 	}
-	
+
 	//TODO: move this to background tasks, for now just invalidation of last 100 votes
 	function invalidateUser() {
 		wfProfileIn(__METHOD__);
 		$dbr = wfGetDB(DB_SLAVE );
-		
+
 		$res = $dbr->select( array('page_vote'),
 			array( 'article_id' ),
 			array(
-				'user_id' => $this->userId,			
+				'user_id' => $this->userId,
 			),
 			__METHOD__,
 			array(
@@ -144,23 +145,23 @@ class VoteHelper {
 				'ORDER BY' => 'time desc'
 			)
 		);
-   
+
 		$out = array();
-		
+
 		$cache = $this->getCache();
 		while($row = $res->fetchObject()) {
 			$key = $this->getPageCacheKey($row->article_id);
 			$cache->delete($key);
 		}
-		
+
 		wfProfileOut(__METHOD__);
 		return true;
 	}
-	
+
 	function isVoted($db = DB_SLAVE) {
 		wfProfileIn(__METHOD__);
 		$dbr = wfGetDB( $db );
-		
+
 		$cache = $this->getCache();
 		$cacheVal = $cache->get($this->getUserCacheKey(), "N");
 
@@ -173,29 +174,29 @@ class VoteHelper {
 			array( 'count(*)as cnt' ),
 			$this->getUserWhere(),
 			__METHOD__ );
-		
+
 		$val = ($row->cnt > 0);
-	
+
 		// Y:N solve the problem with defult val
 		$cache->set($this->getUserCacheKey(), $val ? 'Y':'N');
 		wfProfileOut(__METHOD__);
 		return $val;
 	}
-	
+
 	protected function getUserWhere() {
 		wfProfileIn(__METHOD__);
-		$where = array( 
+		$where = array(
 				'article_id' => $this->pageId,
 		);
-		
+
 		if($this->userId == 0) {
 			$where['user_id'] = 0;
-			$where['ip'] = $this->userIP; 
+			$where['ip'] = $this->userIP;
 		} else {
 			$where['user_id'] = $this->userId;
 		}
 		wfProfileOut(__METHOD__);
-		return $where; 
+		return $where;
 	}
 
 	protected function getUserCacheKey() {
@@ -206,13 +207,13 @@ class VoteHelper {
 		} else {
 			wfProfileOut(__METHOD__);
 			return wfMemcKey(__CLASS__, __METHOD__, $this->pageId,  $this->userId, '', 'VER1');
-		}		
+		}
 	}
-	
+
 	protected function getPageCacheKey($page = false) {
 		return wfMemcKey(__CLASS__, __METHOD__, $page ? $page:$this->pageId, 'VER2');
 	}
-	
+
 	private function getCache() {
 		return F::App()->wg->Memc;
 	}
