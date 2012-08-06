11810790e4a412f9444d11b1dc662bae4acd9118
michalnowierski <michalnowierski@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-06 13:39:09 +0000

    VideoGamesHub.java PageObject Creation
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57749 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/HomePageObject.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/HomePageObject.java
@@ -4,10 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import com.wikia.webdriver.pageObjects.PageObject.Hubs.VideoGamesHub;
+
 public class HomePageObject extends BasePageObject{
 
 	@FindBy(className="create-wiki") 
 	public WebElement startWikiButton;
+	@FindBy(css="section.grid-2.videogames a img") 
+	public WebElement OpenVideoGamesHub;
+	
 	
 	public HomePageObject(WebDriver driver) 
 	{
@@ -19,6 +24,12 @@ public class HomePageObject extends BasePageObject{
 	{
 		click(startWikiButton);
 		return new CreateNewWikiPageObject(driver);
+	}
+	
+	public VideoGamesHub OpenVideoGamesHub() {
+	
+		
+		return new VideoGamesHub(driver);		
 	}
 
 }
