fc11c3f7ed5b54a8178b542675326dea1235e7f8
sean <sean@wikia-inc.com>
2012-08-06 21:31:37 +0000

    Updated OpenGraphMeta extension to MW 1.19 version. Part of BugzId 43709.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57784 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/OpenGraphMeta/OpenGraphMeta.magic.php
+++ extensions/OpenGraphMeta/OpenGraphMeta.magic.php
@@ -15,12 +15,27 @@ $magicWords['ar'] = array(
 	'setmainimage' => array( 0, 'ضبط_صورة_رئيسية' ),
 );
 
+/** German (Deutsch) */
+$magicWords['de'] = array(
+	'setmainimage' => array( 0, 'setze_hauptbild' ),
+);
+
+/** Zazaki (Zazaki) */
+$magicWords['diq'] = array(
+	'setmainimage' => array( 0, 'sazêserêresiman' ),
+);
+
 /** Persian (فارسی) */
 $magicWords['fa'] = array(
 	'setmainimage' => array( 0, 'تنظیم_نگاره_اصلی' ),
 );
 
-/** Macedonian (Македонски) */
+/** Korean (한국어) */
+$magicWords['ko'] = array(
+	'setmainimage' => array( 0, '주그림설정' ),
+);
+
+/** Macedonian (македонски) */
 $magicWords['mk'] = array(
 	'setmainimage' => array( 0, 'поставиглавнастраница' ),
 );
@@ -30,17 +45,22 @@ $magicWords['nl'] = array(
 	'setmainimage' => array( 0, 'hoofdafbeeldinginstellen' ),
 );
 
-/** Russian (Русский) */
+/** Russian (русский) */
 $magicWords['ru'] = array(
 	'setmainimage' => array( 0, 'установитьглавноеизображение', 'установить_главное_изображение' ),
 );
 
-/** Serbian (Cyrillic script) (‪Српски (ћирилица)‬) */
+/** Serbian (Cyrillic script) (‪српски (ћирилица)‬) */
 $magicWords['sr-ec'] = array(
 	'setmainimage' => array( 0, 'поставиглавнуслику', 'постави_главну_слику' ),
 );
 
-/** Serbian (Latin script) (‪Srpski (latinica)‬) */
+/** Serbian (Latin script) (‪srpski (latinica)‬) */
 $magicWords['sr-el'] = array(
 	'setmainimage' => array( 0, 'postavi_glavnu_sliku' ),
+);
+
+/** Ukrainian (українська) */
+$magicWords['uk'] = array(
+	'setmainimage' => array( 0, 'встановити_головне_зображення' ),
 );
