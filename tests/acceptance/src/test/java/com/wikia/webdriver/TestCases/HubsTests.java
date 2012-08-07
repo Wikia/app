afc8b44773f40976a4f98bd195d83e21b76a0ff2
michalnowierski <michalnowierski@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-07 15:48:06 +0000

    Hubs creation progress
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57845 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/TestCases/HubsTests.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/TestCases/HubsTests.java
@@ -21,40 +21,40 @@ public class HubsTests {
 		
 		
 		VideoGamesHub VGHub = home.OpenVideoGamesHub();
+		VGHub.verifyURL("http://www.wikia.com/Video_Games");
 		
-		
+		VGHub.verifyWikiaMosaicSliderHasImages();
 		VGHub.ClickOnNewsTab(2);
 		VGHub.ClickOnNewsTab(3);
 		VGHub.ClickOnNewsTab(1);
 		VGHub.RelatedVideosScrollRight();
 		VGHub.RelatedVideosScrollLeft();
-		VGHub.verifyTitle("http://www.wikia.com/Video_Games");
-		
+				
 		home = VGHub.BackToHomePage();
 		
 		
 		LifestyleHub LHub = home.OpenLifestyleHub();
+		LHub.verifyURL("http://www.wikia.com/Lifestyle");
 		
-		
+		LHub.verifyWikiaMosaicSliderHasImages();
 		LHub.ClickOnNewsTab(2);
 		LHub.ClickOnNewsTab(3);
 		LHub.ClickOnNewsTab(1);
 		LHub.RelatedVideosScrollRight();
 		LHub.RelatedVideosScrollLeft();
-		LHub.verifyTitle("http://www.wikia.com/Lifestyle");
+		
 		
 		home = LHub.BackToHomePage();
 		
-		
 		EntertainmentHub EHub = home.OpenEntertainmentHub();
+		EHub.verifyURL("http://www.wikia.com/Entertainment");
 		
-		
+		EHub.verifyWikiaMosaicSliderHasImages();
 		EHub.ClickOnNewsTab(2);
 		EHub.ClickOnNewsTab(3);
 		EHub.ClickOnNewsTab(1);
 		EHub.RelatedVideosScrollRight();
 		EHub.RelatedVideosScrollLeft();
-		EHub.verifyTitle("http://www.wikia.com/Entertainment");
 		driver.close();
 	}
 }
