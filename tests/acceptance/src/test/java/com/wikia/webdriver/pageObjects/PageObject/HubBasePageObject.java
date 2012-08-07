9fa38052d7cad4ce012c273eeaa146a6c5bb1c87
michalnowierski <michalnowierski@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-07 11:03:50 +0000

    adding line:
    PageFactory.initElements(driver, this); 
    
    to classes constructors.
    
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57809 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/HubBasePageObject.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/HubBasePageObject.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 
 import com.wikia.webdriver.pageObjects.PageObject.Hubs.VideoGamesHub;
@@ -21,7 +22,7 @@ public class HubBasePageObject extends BasePageObject{
 		
 	public HubBasePageObject(WebDriver driver) {
 		super(driver);
-		// TODO Auto-generated constructor stub
+		PageFactory.initElements(driver, this);
 	}
 
 	public void ClickOnNewsTab(int TabNumber) {
