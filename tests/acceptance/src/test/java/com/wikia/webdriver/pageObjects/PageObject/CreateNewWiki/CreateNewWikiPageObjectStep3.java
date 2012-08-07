df504dcde0122eb9891a26674fa918b5eabdbb0e
karol <karol@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-07 07:54:10 +0000

    added steps into CreateNewWiki
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57803 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/CreateNewWiki/CreateNewWikiPageObjectStep3.java
@@ -0,0 +1,14 @@
+package com.wikia.webdriver.pageObjects.PageObject.CreateNewWiki;
+
+import org.openqa.selenium.WebDriver;
+
+import com.wikia.webdriver.pageObjects.PageObject.BasePageObject;
+
+public class CreateNewWikiPageObjectStep3 extends BasePageObject{
+
+	public CreateNewWikiPageObjectStep3(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/HomePageObject.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/HomePageObject.java
@@ -1,9 +1,14 @@
 package com.wikia.webdriver.pageObjects.PageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.wikia.webdriver.pageObjects.PageObject.CreateNewWiki.CreateNewWikiPageObjectStep1;
 import com.wikia.webdriver.pageObjects.PageObject.Hubs.VideoGamesHub;
 
 public class HomePageObject extends BasePageObject{
@@ -12,6 +17,14 @@ public class HomePageObject extends BasePageObject{
 	public WebElement startWikiButton;
 	@FindBy(css="section.grid-2.videogames a img") 
 	public WebElement OpenVideoGamesHub;
+	@FindBy(className="ajaxLogin")
+	public WebElement logInAjax;
+	@FindBy (xpath="//div[@class='input-group required   ']/input[@name='username']")
+	public WebElement userNameField;
+	@FindBy (xpath="//div[@class='input-group required   ']/input[@name='password']")
+	public WebElement passwordField;
+	@FindBy (css="input[type='submit']")
+	public WebElement submitButton;
 	
 	
 	public HomePageObject(WebDriver driver) 
@@ -20,10 +33,23 @@ public class HomePageObject extends BasePageObject{
 		driver.get(liveDomain);
 	}
 	
-	public CreateNewWikiPageObject StartAWikia()
+	public CreateNewWikiPageObjectStep1 StartAWikia()
 	{
 		click(startWikiButton);
-		return new CreateNewWikiPageObject(driver);
+		return new CreateNewWikiPageObjectStep1(driver);
+	}
+	
+	public void logIn(String userName, String password)
+	{
+		Actions builder = new Actions(driver);
+		builder.click(logInAjax).build().perform();
+		WebDriverWait wait = new WebDriverWait(driver, timeOut);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='username']")));
+		userNameField.sendKeys(userName);
+		passwordField.sendKeys(password);
+		submitButton.click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[href='/User:"+userName+"']")));
+		
 	}
 	
 	public VideoGamesHub OpenVideoGamesHub() {
