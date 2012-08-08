afcd667575307c92bb9da7a35bfb3c0e80cb9f4f
karol <karol@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-08 08:50:15 +0000

    delete wikia test + fixes
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57877 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/CreateNewWiki/CreateNewWikiPageObjectStep2.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/pageObjects/PageObject/CreateNewWiki/CreateNewWikiPageObjectStep2.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+import com.wikia.webdriver.Logging.PageObjectLogging;
 import com.wikia.webdriver.pageObjects.PageObject.BasePageObject;
 
 public class CreateNewWikiPageObjectStep2 extends BasePageObject{
@@ -26,17 +27,20 @@ public class CreateNewWikiPageObjectStep2 extends BasePageObject{
 	public void describeYourTopic(String description)
 	{
 		descriptionField.sendKeys(description);
+		PageObjectLogging.log("describeYourTopic", "describe your topic populated with: "+description, true);
 	}
 	
 	public void selectCategory(String category)
 	{
 		Select dropList = new Select(wikiCategory);
 		dropList.selectByVisibleText(category);
+		PageObjectLogging.log("selectCategory", "selected "+category+" category", true);
 	}
 	
 	public CreateNewWikiPageObjectStep3 submit()
 	{
 		submitButton.click();
+		PageObjectLogging.log("submit", "Submit button clicked", true);
 		return new CreateNewWikiPageObjectStep3(driver);
 	}
 
