7c5c0b244b9be7bbb399028e1adadde75e50a09b
tomek <tomek@wikia-inc.com>
2012-08-08 21:25:48 +0000

    wall performace
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57951 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Wall/templates/Wall_messageEditButtons.php
@@ -0,0 +1,7 @@
+<div class="edit-buttons sourceview">
+	<button class="wikia-button cancel-edit cancel-source"><?php echo wfMsg('wall-button-done-source'); ?></button>
+</div>
+<div class="edit-buttons edit" data-space-type="buttons">
+	<button class="wikia-button save-edit"><?php echo wfMsg('wall-button-save-changes'); ?></button>
+	<button class="wikia-button cancel-edit secondary"><?php echo wfMsg('wall-button-cancel-changes'); ?></button>
+</div>
