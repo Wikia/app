f27c583372ea4f2baa5e05714e71c259c33c67c1
piotrozga <piotrozga@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-08 14:07:13 +0000

    optimalisation of VisualStats extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57921 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VisualStats/js/VisualStatsPunchcard.js
+++ extensions/wikia/VisualStats/js/VisualStatsPunchcard.js
@@ -62,38 +62,6 @@ var VisualStatsPunchcard = {
         })
     },
 
-   /* setUserData: function(){
-        var dateIndex=1;
-        var self = this;
-        this.scale.domain([0, self.userPunchcardMax]);
-
-        $.each(this.userPunchcardData, function(date, hourset){
-            $.each(hourset, function(hour, value){
-                var object = $("#day" + dateIndex + "hour" + hour);
-                var strDate = "#day" + dateIndex;
-                var strHour = "#hour" + hour;
-                if (value > 0){
-                    self.setRadius(value, object);
-                    object.mouseover(function(){
-                        $(strDate).css('font-weight', 'bold').css('color', self.color).css('font-size', '13px');
-                        $(strHour).css('font-weight', 'bold').css('color', self.color);
-                    })
-                    object.mouseout(function(){
-                        $(strDate).css('font-weight', 'normal').css('color', 'black').css('font-size', '10px');
-                        $(strHour).css('font-weight', 'normal').css('color', 'black');
-                    })
-                    object.attr("title", value);
-                }
-                else{
-                    object.css('display', 'none');
-                    object.mouseout(function(){});
-                    object.mouseover(function(){});
-                    object.removeAttr("title");
-                }
-            })
-            dateIndex++;
-        })
-    },*/
     triggerButtons: function(){
         var self = this;
         if (this.user != "0"){
@@ -116,6 +84,7 @@ var VisualStatsPunchcard = {
             });
         }
     },
+
     setDates: function(){
             var i = 1;
             $.each(this.wikiaPunchcardData, function(date, hourset){
@@ -123,6 +92,7 @@ var VisualStatsPunchcard = {
                 i++;
             });
     },
+
     setRadius: function(count, object){
         var self = this;
         var result = Math.ceil(self.scale(count));
