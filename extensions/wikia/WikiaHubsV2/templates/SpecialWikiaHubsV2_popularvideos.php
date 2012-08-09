09a9254a7dc98b2545a2e7bcc54f9271c4d94846
marcin <marcin@wikia-inc.com>
2012-08-09 14:16:55 +0000

    Story C0060: Templating for Popular Videos (work in progress) (bugid:43634)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57985 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_popularvideos.php
+++ extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_popularvideos.php
@@ -4,31 +4,23 @@
 </div>
 
 <section class="WikiaMediaCarousel">
-	<a href="#" class="button secondary left next">
-		<img src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" class="chevron">
+	<a href="#" class="button secondary right next">
+		<img src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" class="chevron" />
 	</a>
-	<a href="#" class="button secondary right previous">
-		<img src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" class="chevron">
+	<a href="#" class="button secondary left previous">
+		<img src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" class="chevron" />
 	</a>
-	<div id="carouselContainer" class="carouselContainer">
-		<div>
-			<ul class="carousel" style="">
-				<li class="thumbs active">
-					<?= $app->renderView( 'RelatedHubsVideos', 'getCaruselElement', array( 'video' => $videos[0], 'preloaded' => 1 ) ) ?>
-				</li>
-				<li class="thumbs active">
-					<?= $app->renderView( 'RelatedHubsVideos', 'getCaruselElement', array( 'video' => $videos[0], 'preloaded' => 1 ) ) ?>
-				</li>
-				<li class="thumbs active">
-					<?= $app->renderView( 'RelatedHubsVideos', 'getCaruselElement', array( 'video' => $videos[0], 'preloaded' => 1 ) ) ?>
-				</li>
-				<li class="thumbs active">
-					<?= $app->renderView( 'RelatedHubsVideos', 'getCaruselElement', array( 'video' => $videos[0], 'preloaded' => 1 ) ) ?>
-				</li>
-				<li class="thumbs active">
-					<?= $app->renderView( 'RelatedHubsVideos', 'getCaruselElement', array( 'video' => $videos[0], 'preloaded' => 1 ) ) ?>
-				</li>
-			</ul>
+	<? if (is_array($videos)): ?>
+		<div id="carouselContainer" class="carouselContainer">
+			<div>
+				<ul class="carousel" style="">
+					<? foreach($videos as $video): ?>
+						<li class="thumbs active">
+							<?= $app->renderView( 'RelatedHubsVideos', 'getCaruselElement', array( 'video' => $video, 'preloaded' => 1 ) ) ?>
+						</li>
+					<? endforeach; ?>
+				</ul>
+			</div>
 		</div>
-	</div>
+	<? endif; ?>
 </section>
