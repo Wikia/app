0bed1831685ac745058ef7da5dc75d67e227e933
karol <karol@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-09 12:09:07 +0000

    added beforeSuite and afterSuite
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57973 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/Templates/TestTemplate.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/Templates/TestTemplate.java
@@ -1,9 +1,14 @@
 package com.wikia.webdriver.Templates;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import com.wikia.webdriver.Common.CommonUtils;
 import com.wikia.webdriver.DriverProvider.DriverProvider;
 import com.wikia.webdriver.Logging.PageObjectLogging;
 
@@ -11,17 +16,34 @@ public class TestTemplate {
 	
 	public WebDriver driver;
 	
+	@BeforeSuite
+	public void beforeSuite()
+	{
+		CommonUtils.deleteDirectory("."+File.separator+"logs");
+		CommonUtils.createDirectory("."+File.separator+"logs");
+		PageObjectLogging.startLoggingSuite();
+	}
+	
+	@AfterSuite
+	public void afterSuite()
+	{
+		PageObjectLogging.stopLoggingSuite();
+	}
+	
 	@BeforeMethod
 	public void start()
 	{
-		PageObjectLogging.startLogging(getClass().getName().toString());
+		PageObjectLogging.startLoggingMethod(getClass().getName().toString());
 		driver = DriverProvider.getInstance().getWebDriver();
 	}
 	
 	@AfterMethod
 	public void stop()
 	{
+		PageObjectLogging.stopLoggingMethod();
 		driver.close();
 	}
+	
+	
 
 }
