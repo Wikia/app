cf28f0729c6741f5b4e1c37ccaec4ecd6d8ba21d
sean <sean@wikia-inc.com>
2012-08-09 00:44:50 +0000

    Further cleanup (unit-tests-only) for removing UserInterview. BugzId 44247
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@57955 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserProfilePageV3/tests/UserProfilePageTest.php
+++ extensions/wikia/UserProfilePageV3/tests/UserProfilePageTest.php
@@ -1,6 +1,5 @@
 <?php
 require_once dirname(__FILE__) . '/../UserProfilePage.setup.php';
-require_once dirname(__FILE__) . '/../InterviewQuestion.class.php';
 
 class UserProfilePageTest extends WikiaBaseTest {
 	const TEST_WIKI_ID = 111;
@@ -12,78 +11,8 @@ class UserProfilePageTest extends WikiaBaseTest {
 	 * @return UserProfilePage
 	 */
 	protected function getObjectMock( User $user ) {
-		$object = $this->getMock( 'UserProfilePage', array( 'parseInterviewAnswers', 'getInterviewArticle', 'invalidateCache' ), array( F::app(), $user ) );
+		$object = $this->getMock( 'UserProfilePage', array( 'invalidateCache' ), array( F::app(), $user ) );
 		return $object;
-	}
-
-	public function gettingInterviewQuestionsDataProvider() {
-		return array(
-			array( true, false ),
-			array( true, true ),
-			array( false, false ),
-			array( false, true )
-		);
-	}
-
-	/**
-	 * @dataProvider gettingInterviewQuestionsDataProvider
-	 */
-	public function testGettingInterviewQuestions( $answeredOnly, $asArray ) {
-		$answers = array(
-			10 => 'Test Answer 10',
-			20 => 'Test Answer 20',
-			30 => null
-		);
-
-		$question10Mock = $this->getMock( 'InterviewQuestion', array( 'getId' ), array(), '', false );
-		$question10Mock->expects( $this->any() )
-		  ->method( 'getId' )
-		  ->will( $this->returnValue( 10 ));
-		  
-		$question20Mock = $this->getMock( 'InterviewQuestion', array( 'getId' ), array(), '', false );
-		$question20Mock->expects( $this->any() )
-		  ->method( 'getId' )
-		  ->will( $this->returnValue( 20 ));
-
-		$question30Mock = $this->getMock( 'InterviewQuestion', array( 'getId' ), array(), '', false );
-		$question30Mock->expects( $this->any() )
-		  ->method( 'getId' )
-		  ->will( $this->returnValue( 30 ));
-
-		$questions = array(
-			$question10Mock,
-			$question20Mock,
-			$question30Mock
-		);
-
-		$this->mockGlobalFunction( 'msg', self::TEST_CAPTION, count( $questions ) );
-		$this->mockApp();
-
-		$object = $this->getObjectMock( $this->getMock( 'User' ) );
-		$object->expects( $this->once() )
-		  ->method( 'parseInterviewAnswers' )
-		  ->will( $this->returnValue( $answers ) );
-
-		$interviewMock = $this->getMock( 'Interview', array( 'getQuestions' ), array(), '', false );
-		$interviewMock->expects( $this->once() )
-		  ->method( 'getQuestions' )
-		  ->will( $this->returnValue( $questions ) );
-
-		$this->mockClass( 'Interview', $interviewMock );
-
-		$result = $object->getInterviewQuestions( self::TEST_WIKI_ID, $answeredOnly, $asArray );
-
-		$this->assertEquals( ( $answeredOnly ? 2 : 3 ), count( $result ) );
-		foreach( $result as $question ) {
-			if( $asArray ) {
-				$this->assertEquals( self::TEST_CAPTION, $question['caption'] );
-				$this->assertEquals( $answers[$question['id']], $question['answerBody'] );
-			}
-			else {
-				$this->assertEquals( self::TEST_CAPTION, $question->getCaption() );
-				$this->assertEquals( $answers[$question->getId()], $question->getAnswerBody() );
-			}
-		}
 	}
 
 	protected function setUpMobileSkin( $mobileSkin ) {
