ddb4751781a89ad4d11b1bc8f675b5d7caf15a32
marcin <marcin@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-10 13:38:37 +0000

    Story C0060: Templating for Popular Videos (work in progress) (bugid:43634)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58027 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_renderCaruselElement.php
+++ extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_renderCaruselElement.php
@@ -1,9 +1,7 @@
-<div class="item">
-	<a class="video-thumbnail lightbox" data-wiki="<?= $data['wiki']; ?>" data-video-name="<?= $data['video-name']; ?>" data-ref="<?= $data['ref']; ?>" href="<?= $href; ?>">
-		<div class="timer"><?= $duration; ?></div>
-		<div class="playButton"></div>
-		<img class="Wikia-video-thumb" src="<?= $imgUrl; ?>" />
-	</a>
-	<div class="description"><?= $description; ?></div>
-	<div class="info"><?= $info; ?></div>
-</div>
+<a class="video-thumbnail lightbox" data-wiki="<?= $data['wiki']; ?>" data-video-name="<?= $data['video-name']; ?>" data-ref="<?= $data['ref']; ?>" href="<?= $href; ?>">
+	<div class="timer"><?= $duration; ?></div>
+	<div class="playButton"></div>
+	<img class="Wikia-video-thumb" src="<?= $imgUrl; ?>" />
+</a>
+<div class="description"><?= $description; ?></div>
+<div class="info"><?= $info; ?></div>
