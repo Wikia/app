ed24b87c36f671d6b0cb4faad599b8928f93bc0c
marcin <marcin@wikia-inc.com>
2012-08-10 14:57:29 +0000

    Story C0060: Templating for Popular Videos (work in progress) (bugid:43634)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58037 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_index.php
+++ extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_index.php
@@ -57,102 +57,14 @@
 			<?= $app->renderView('SpecialWikiaHubsV2', 'wikitextmodule', array()); ?>
 			<?= $app->renderView('SpecialWikiaHubsV2', 'topwikis', array()); ?>
 		</div>
+		<div class="grid-4 alpha wikiahubs-popular-videos">
 			<?= $app->renderView('SpecialWikiaHubsV2', 'popularvideos', array()); ?>
-			<div class="grid-4 alpha wikiahubs-from-the-community">
-				<h2>
-					<span class="mw-headline" id="From_the_Community_.0A.0A_Get_Promoted">From the Community
-						<button id="suggestArticle" class="wikia-button secondary">Get Promoted </button>
-					</span>
-				</h2>
-				<ul class="wikiahubs-ftc-list">
-					<li class="wikiahubs-ftc-item">
-						<div class="floatleft">
-							<a href="http://assassinscreed.wikia.com/wiki/User_blog:Master_Sima_Yi/Assassinews_07/09_-_Assassin%27s_Creed_film_news"><img alt="Michael-Fassbender-cast-in-Assassins-Creed-Movie.jpg" src="http://images1.wikia.nocookie.net/__cb20120711003839/wikiaglobal/images/5/54/Michael-Fassbender-cast-in-Assassins-Creed-Movie.jpg" width="570" height="300" /></a>
-						</div>
-						<div class="wikiahubs-ftc-title">
-							<p>
-								<a class="text" href="http://assassinscreed.wikia.com/wiki/User_blog:Master_Sima_Yi/Assassinews_07/09_-_Assassin%27s_Creed_film_news">Assassin's Creed Film News</a>
-							</p>
-						</div>
-						<div class="wikiahubs-ftc-subtitle">
-							<p>
-								From <a  class="text" href="http://assassinscreed.wikia.com/wiki/User:Master_Sima_Yi">Master Sima Yi</a> on <a  class="text" href="http://assassinscreed.wikia.com/?redirect=no">assassinscreed.wikia.com</a>
-							</p>
-						</div>
-						<div class="wikiahubs-ftc-creative">
-							<p>
-								<b>Master Sima Yi Says:</b><br />
-							</p>
-							Today, several news sites have reported that actor Michael Fassbender (known for his roles in Inglourious Basterds, Shame, X-Men: First Class and Prometheus) has signed on for the planned Assassin's Creed film.
-						</div>
-					</li>
-					<li class="wikiahubs-ftc-item">
-						<div class="floatleft">
-							<a href="http://assassinscreed.wikia.com/wiki/User_blog:Master_Sima_Yi/Assassinews_07/09_-_Assassin%27s_Creed_film_news"><img alt="Michael-Fassbender-cast-in-Assassins-Creed-Movie.jpg" src="http://images1.wikia.nocookie.net/__cb20120711003839/wikiaglobal/images/5/54/Michael-Fassbender-cast-in-Assassins-Creed-Movie.jpg" width="570" height="300" /></a>
-						</div>
-						<div class="wikiahubs-ftc-title">
-							<p>
-								<a class="text" href="http://assassinscreed.wikia.com/wiki/User_blog:Master_Sima_Yi/Assassinews_07/09_-_Assassin%27s_Creed_film_news">Assassin's Creed Film News</a>
-							</p>
-						</div>
-						<div class="wikiahubs-ftc-subtitle">
-							<p>
-								From <a  class="text" href="http://assassinscreed.wikia.com/wiki/User:Master_Sima_Yi">Master Sima Yi</a> on <a  class="text" href="http://assassinscreed.wikia.com/?redirect=no">assassinscreed.wikia.com</a>
-							</p>
-						</div>
-						<div class="wikiahubs-ftc-creative">
-							<p>
-								<b>Master Sima Yi Says:</b><br />
-							</p>
-							Today, several news sites have reported that actor Michael Fassbender (known for his roles in Inglourious Basterds, Shame, X-Men: First Class and Prometheus) has signed on for the planned Assassin's Creed film.
-						</div>
-					</li>
-					<li class="wikiahubs-ftc-item">
-						<div class="floatleft">
-							<a href="http://assassinscreed.wikia.com/wiki/User_blog:Master_Sima_Yi/Assassinews_07/09_-_Assassin%27s_Creed_film_news"><img alt="Michael-Fassbender-cast-in-Assassins-Creed-Movie.jpg" src="http://images1.wikia.nocookie.net/__cb20120711003839/wikiaglobal/images/5/54/Michael-Fassbender-cast-in-Assassins-Creed-Movie.jpg" width="570" height="300" /></a>
-						</div>
-						<div class="wikiahubs-ftc-title">
-							<p>
-								<a class="text" href="http://assassinscreed.wikia.com/wiki/User_blog:Master_Sima_Yi/Assassinews_07/09_-_Assassin%27s_Creed_film_news">Assassin's Creed Film News</a>
-							</p>
-						</div>
-						<div class="wikiahubs-ftc-subtitle">
-							<p>
-								From <a  class="text" href="http://assassinscreed.wikia.com/wiki/User:Master_Sima_Yi">Master Sima Yi</a> on <a  class="text" href="http://assassinscreed.wikia.com/?redirect=no">assassinscreed.wikia.com</a>
-							</p>
-						</div>
-						<div class="wikiahubs-ftc-creative">
-							<p>
-								<b>Master Sima Yi Says:</b><br />
-							</p>
-							Today, several news sites have reported that actor Michael Fassbender (known for his roles in Inglourious Basterds, Shame, X-Men: First Class and Prometheus) has signed on for the planned Assassin's Creed film.
-						</div>
-					</li>
-					<li class="wikiahubs-ftc-item">
-						<div class="floatleft">
-							<a href="http://assassinscreed.wikia.com/wiki/User_blog:Master_Sima_Yi/Assassinews_07/09_-_Assassin%27s_Creed_film_news"><img alt="Michael-Fassbender-cast-in-Assassins-Creed-Movie.jpg" src="http://images1.wikia.nocookie.net/__cb20120711003839/wikiaglobal/images/5/54/Michael-Fassbender-cast-in-Assassins-Creed-Movie.jpg" width="570" height="300" /></a>
-						</div>
-						<div class="wikiahubs-ftc-title">
-							<p>
-								<a class="text" href="http://assassinscreed.wikia.com/wiki/User_blog:Master_Sima_Yi/Assassinews_07/09_-_Assassin%27s_Creed_film_news">Assassin's Creed Film News</a>
-							</p>
-						</div>
-						<div class="wikiahubs-ftc-subtitle">
-							<p>
-								From <a  class="text" href="http://assassinscreed.wikia.com/wiki/User:Master_Sima_Yi">Master Sima Yi</a> on <a  class="text" href="http://assassinscreed.wikia.com/?redirect=no">assassinscreed.wikia.com</a>
-							</p>
-						</div>
-						<div class="wikiahubs-ftc-creative">
-							<p>
-								<b>Master Sima Yi Says:</b><br />
-							</p>
-							Today, several news sites have reported that actor Michael Fassbender (known for his roles in Inglourious Basterds, Shame, X-Men: First Class and Prometheus) has signed on for the planned Assassin's Creed film.
-						</div>
-					</li>
-				</ul>
-			</div>
+		</div>
+		<div class="grid-4 alpha wikiahubs-from-the-community">
+			<?= $app->renderView('SpecialWikiaHubsV2', 'fromthecommunity', array()); ?>
 		</div>
 	</div>
+</div>
 
 <div class="RelatedVideos RelatedVideosHidden noprint" id="RelatedVideos" data-count="1">
 	<div class="deleteConfirm messageHolder">Are you sure you want to remove this video?</div>
--- extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_popularvideos.php
+++ extensions/wikia/WikiaHubsV2/templates/SpecialWikiaHubsV2_popularvideos.php
@@ -1,29 +1,27 @@
-<div class="grid-4 alpha wikiahubs-popular-videos">
-	<div class="title-wrapper">
-		<h2><?= wfMsg('wikiahubs-suggest-video-header'); ?></h2>
-		<button id="suggest-popularvideos" class="wikia-button secondary">
-			<?= wfMsg('wikiahubs-suggest-video-submit-button'); ?>
-		</button>
-	</div>
-	<section class="WikiaMediaCarousel">
-		<a href="#" class="button secondary right next">
-			<img src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" class="chevron" />
-		</a>
-		<a href="#" class="button secondary left previous">
-			<img src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" class="chevron" />
-		</a>
-		<? if (is_array($videos)): ?>
-		<div id="carouselContainer" class="carouselContainer">
-			<div>
-				<ul class="carousel" style="">
-					<? foreach($videos as $video): ?>
-					<li class="thumbs active">
-						<?= $app->renderView( 'SpecialWikiaHubsV2', 'renderCaruselElement', array('video' => $video) ); ?>
-					</li>
-					<? endforeach; ?>
-				</ul>
-			</div>
-		</div>
-		<? endif; ?>
-	</section>
+<div class="title-wrapper">
+	<h2><?= wfMsg('wikiahubs-suggest-video-header'); ?></h2>
+	<button id="suggest-popularvideos" class="wikia-button secondary">
+		<?= wfMsg('wikiahubs-suggest-video-submit-button'); ?>
+	</button>
 </div>
+<section class="WikiaMediaCarousel">
+	<a href="#" class="button secondary right next">
+		<img src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" class="chevron" />
+	</a>
+	<a href="#" class="button secondary left previous">
+		<img src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" class="chevron" />
+	</a>
+	<? if (is_array($videos)): ?>
+	<div id="carouselContainer" class="carouselContainer">
+		<div>
+			<ul class="carousel" style="">
+				<? foreach($videos as $video): ?>
+				<li class="thumbs active">
+					<?= $app->renderView( 'SpecialWikiaHubsV2', 'renderCaruselElement', array('video' => $video) ); ?>
+				</li>
+				<? endforeach; ?>
+			</ul>
+		</div>
+	</div>
+	<? endif; ?>
+</section>
