66e0bf57bb71a0227d03bb3712f691fa0762b6e7
michalnowierski <michalnowierski@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-13 10:14:53 +0000

    modifying
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58132 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/TestCases/HubsTests.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/TestCases/HubsTests.java
@@ -1,57 +1,55 @@
 package com.wikia.webdriver.TestCases;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 
 import com.wikia.webdriver.Templates.TestTemplate;
 import com.wikia.webdriver.pageObjects.PageObject.HomePageObject;
+import com.wikia.webdriver.pageObjects.PageObject.HubBasePageObject;
 import com.wikia.webdriver.pageObjects.PageObject.Hubs.EntertainmentHubPageObject;
 import com.wikia.webdriver.pageObjects.PageObject.Hubs.LifestyleHubPageObject;
 import com.wikia.webdriver.pageObjects.PageObject.Hubs.VideoGamesHubPageObject;
 
 public class HubsTests extends TestTemplate{
 
-	@Test
-	public void VideoGamesHubTest()
+	
+	
+
+	private static final VideoGamesHubPageObject VGHub = null;
+	private static final LifestyleHubPageObject LHub = null;
+	private static final EntertainmentHubPageObject EHub = null;
+	private static HomePageObject home  = null;
+
+	@DataProvider
+	private static final Object[][] provideHub(){
+		return new Object[][] {
+				{VGHub, "VideoGamesHub", "http://www.wikia.com/Video_Games"},
+				{EHub, "EntertainmentHub", "http://www.wikia.com/Entertainment"},
+				{LHub, "LifestyleHub", "http://www.wikia.com/Lifestyle"}
+		};
+	}
+	
+	@Test(dataProvider = "provideHub")
+	public void VideoGamesHubTest(HubBasePageObject Hub, String HubName, String HubURL)
 	{
 		
-		HomePageObject home = new HomePageObject(driver);
+		
+		home = new HomePageObject(driver);
 		home.openHomePage();
+						
+		Hub = home.OpenHub(HubName);
+		Hub.verifyURL(HubURL);
 		
-		VideoGamesHubPageObject VGHub = home.OpenVideoGamesHub();
-		VGHub.verifyURL("http://www.wikia.com/Video_Games");
-		
-		VGHub.verifyWikiaMosaicSliderHasImages();
-		VGHub.ClickOnNewsTab(2);
-		VGHub.ClickOnNewsTab(3);
-		VGHub.ClickOnNewsTab(1);
-		VGHub.RelatedVideosScrollRight();
-		VGHub.RelatedVideosScrollLeft();
+		Hub.verifyWikiaMosaicSliderHasImages();
+		Hub.ClickOnNewsTab(2);
+		Hub.ClickOnNewsTab(3);
+		Hub.ClickOnNewsTab(1);
+		Hub.RelatedVideosScrollRight();
+		Hub.RelatedVideosScrollLeft();
 				
-		home = VGHub.BackToHomePage();
-		
-		
-		LifestyleHubPageObject LHub = home.OpenLifestyleHub();
-		LHub.verifyURL("http://www.wikia.com/Lifestyle");
-		
-		LHub.verifyWikiaMosaicSliderHasImages();
-		LHub.ClickOnNewsTab(2);
-		LHub.ClickOnNewsTab(3);
-		LHub.ClickOnNewsTab(1);
-		LHub.RelatedVideosScrollRight();
-		LHub.RelatedVideosScrollLeft();
-		
-		
-		home = LHub.BackToHomePage();
-		
-		EntertainmentHubPageObject EHub = home.OpenEntertainmentHub();
-		EHub.verifyURL("http://www.wikia.com/Entertainment");
-		
-		EHub.verifyWikiaMosaicSliderHasImages();
-		EHub.ClickOnNewsTab(2);
-		EHub.ClickOnNewsTab(3);
-		EHub.ClickOnNewsTab(1);
-		EHub.RelatedVideosScrollRight();
-		EHub.RelatedVideosScrollLeft();
+
+		home = Hub.BackToHomePage();
 		
 	}
 }
