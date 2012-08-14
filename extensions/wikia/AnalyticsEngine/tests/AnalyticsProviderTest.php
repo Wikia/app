96cb599e72a9c3812b0aa800b6b68c12dd46f627
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-14 16:44:33 +0000

    full set of data for IVW test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58318 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AnalyticsEngine/tests/AnalyticsProviderTest.php
+++ extensions/wikia/AnalyticsEngine/tests/AnalyticsProviderTest.php
@@ -50,8 +50,37 @@ class AnalyticsProviderTest extends WikiaBaseTest {
 	public static function analyticsDataProvider()
 	{
 		return array(
-			array('http://shaun.wikia.com/wiki/Shaun', 'RC_WIKIA_UGCENT'),
+			array('http://de.wikia.com/Wikia', 'RC_WIKIA_HOME'),
+			
+			array('http://de.wikia.com/Spezial:Kontakt', 'RC_WIKIA_SVCE'),
+			array('http://de.wikia.com/%C3%9Cber_Wikia', 'RC_WIKIA_SVCE'),
+			array('http://de.wikia.com/Presse', 'RC_WIKIA_SVCE'),
+			array('http://de.wikia.com/Stellen', 'RC_WIKIA_SVCE'),
+			array('http://de.wikia.com/Projekt:Datenschutz', 'RC_WIKIA_SVCE'),
+			array('http://de.wikia.com/Spezial:Kontakt', 'RC_WIKIA_SVCE'),
+			array('http://de.wikia.com/Spezial:UserSignup', 'RC_WIKIA_SVCE'),
+ 
 			array('http://de.wikia.com/Videospiele', 'RC_WIKIA_START'),
+			array('http://de.wikia.com/Entertainment', 'RC_WIKIA_START'),
+			array('http://de.wikia.com/Lifestyle', 'RC_WIKIA_START'),
+
+			array('http://de.wikia.com/Mobil', 'RC_WIKIA_MOBIL'),
+			array('http://de.wikia.com/Mobil/LyricWiki', 'RC_WIKIA_MOBIL'),
+			array('http://de.wikia.com/Mobil/GameGuides', 'RC_WIKIA_MOBIL'),
+
+			array('http://de.community.wikia.com/wiki/Admin-Bereich:Hauptseite', 'RC_WIKIA_COMMUNITY'),
+			array('http://de.community.wikia.com/wiki/Community_Deutschland', 'RC_WIKIA_COMMUNITY'),
+			array('http://de.community.wikia.com/wiki/Blog%3AWikia_Deutschland_News', 'RC_WIKIA_COMMUNITY'),
+
+			array('http://de.wikia.com/index.php?search=elder&fulltext=Search', 'RC_WIKIA_SEARCH'),
+
+			array('http://de.elderscrolls.wikia.com/wiki/Elder_Scrolls_Wiki', 'RC_WIKIA_UGC'),
+			array('http://de.swtor.wikia.com/wiki/Star_Wars_-_The_Old_Republic_Wiki', 'RC_WIKIA_UGC'),
+
+			array('http://de.community.wikia.com/wiki/Forum:%C3%9Cbersicht', 'RC_WIKIA_PIN'),
+
+			array('http://shaun.wikia.com/wiki/Shaun', 'RC_WIKIA_UGCENT'),
+			array('http://de.marvel-filme.wikia.com/wiki/Marvel-Filme', 'RC_WIKIA_UGCENT'),
 		);
 	}
 
