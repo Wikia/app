f89571a345c517236c69836b8c5f3faf7c5c73d5
mech <mech@wikia-inc.com>
2012-08-14 13:13:41 +0000

    AnalyticsEngine: new infrastructure test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58289 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AnalyticsEngine/tests/AnalyticsProviderTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Integragion tests for Analytics Engine
+ * User: mech
+ * Date: 8/14/12
+ * Time: 1:33 PM
+ */
+class AnalyticsProviderTest extends WikiaBaseTest {
+	private function get_data($url)
+	{
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+
+	/**
+	 * Check the page source for analytics tracking and return used tag
+	 *
+	 * @param $url page address to fetch
+	 * @return analytics parameter used in this page
+	 *
+	 */
+	private function getAnalyticsProviderTag($url) {
+		$page = $this->get_data($url);
+		if (preg_match('/<img src="([^">]+)" [^>]+ alt="szmtag"/', $page, $m)) {
+			$path = parse_url($m[1], PHP_URL_PATH);
+			return array_pop(explode('/', $path));
+		} else {
+			$this->fail('Cannot find the analytics image on page ' . $url);
+		}
+	}
+
+	/**
+	 * Test the IVW provider
+	 *
+	 * @group Infrastructure
+	 */
+	public function testAnalyticsProviderIVW() {
+		$this->assertEquals($this->getAnalyticsProviderTag('http://shaun.wikia.com/wiki/Shaun'), 'RC_WIKIA_UGCENT');
+		$this->assertEquals($this->getAnalyticsProviderTag('http://de.wikia.com/Videospiele'), 'RC_WIKIA_START');
+	}
+}
+
