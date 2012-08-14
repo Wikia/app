34f5c5dc57074a682d63b1f97d8250a9e65d9c79
kflorence <kflorence@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-14 23:37:28 +0000

    Implement importArticles javascript function (BugId:44180).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58346 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ resources/wikia/wikia.wikibits.js
@@ -0,0 +1,61 @@
+(function( window, $ ) {
+
+var baseUri = mw.config.get( 'wgLoadScript' ) + '?=',
+	defaults = {
+		lang: mw.config.get( 'wgUserLanguage' ),
+		mode: 'articles',
+		skin: mw.config.get( 'skin' )
+	},
+	slice = [].slice;
+
+function log( text ) {
+	return $().log( text, 'importArticle' );
+}
+
+var importArticle = function() {
+	var i, l, module, uri,
+		modules = slice.call(arguments);
+
+	for ( i = 0, l = modules.length; i < l; i++ ) {
+		module = $.extend( {}, defaults, modules[ i ] );
+
+		// Resource loader expects "articles" param
+		module.articles = module.article || module.articles;
+		delete module.article;
+
+		if ( !module.articles || !module.articles.length ) {
+			log( 'Missing required argument: articles' );
+			continue;
+		}
+
+		// Resource loader expects pipe separated article names
+		if ( $.isArray( module.articles ) ) {
+			module.articles = module.articles.join( '|' );
+		}
+
+		// These import methods are in /skins/common/wikibits.js
+		var importMethod;
+		if ( module.type == 'script' ) {
+			importMethod = window.importScriptURI;
+
+		} else if ( module.type == 'style' ) {
+			importMethod = window.importStylesheetURI;
+		}
+
+		if ( !importMethod ) {
+			log( 'Invalid article type: ' + type );
+			continue;
+		}
+
+		// Resource loader expects "only" param instead of "type"
+		module.only = module.type + 's';
+		delete module.type;
+
+		return importMethod( baseUri + $.param( module ) );
+	}
+};
+
+// Exports
+window.importArticle = window.importArticles = importArticle;
+
+})( this, jQuery );
