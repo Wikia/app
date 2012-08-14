543f0cde0984ddb03e8b9d49331af77326a071d8
jacek <jacek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-14 13:09:13 +0000

    Structured Content - get changed article list
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58287 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/StructuredContent/StructuredContent.setup.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * setup
+ */
+
+$app = F::app();
+$dir = dirname( __FILE__ );
+$app->registerClass( 'StructuredContentController', $dir . '/StructuredContentController.class.php' );
+$app->registerClass( 'StructuredContentHelper',	$dir . '/StructuredContentHelper.class.php' );
+
+
+/**
+ * messages
+ */
+$wgExtensionMessagesFiles['StructuredContent'] = "$dir/StructuredContent.i18n.php";
+?>
