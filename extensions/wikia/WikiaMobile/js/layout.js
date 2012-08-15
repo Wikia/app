9b20b3a0702b8e1ad142eb34544f973e2505b73f
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-15 15:15:18 +0000

    WikiaMobile: small change to lazy loading
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58371 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/js/layout.js
+++ extensions/wikia/WikiaMobile/js/layout.js
@@ -5,30 +5,6 @@
  * Layout handling of WikiaMobile
  * ie. Sections, Images, Galleries etc.
  */
-
-(function(w){
-	var onload = function() {
-		require(['lazyload', 'sections'], function(lazyLoad, sections){
-			var processedSections = {};
-
-			lazyLoad(document.getElementsByClassName('noSect'));
-
-			sections.addEventListener('open', function () {
-				var self = this,
-					id = self.getAttribute('data-index');
-
-				if (id && !processedSections[id]) {
-					lazyLoad(self.getElementsByClassName('lazy'));
-
-					processedSections[id] = true;
-				}
-			}, true);
-		});
-	};
-
-	w.addEventListener ? w.addEventListener('load', onload) : w.attachEvent('onload', onload);
-})(window);
-
 define('layout', ['sections', 'media'], function(sections, media) {
 	var images = document.getElementsByClassName('media');
 
