6b2339c636b72b1d18b7e2fe5a1b65c3694505ff
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-16 13:53:57 +0000

    WikiaMobile: Polls:  Dont show what do you thing button for non-js users, styling fixes, fixes for loading assets
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58413 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaPoll/templates/WikiaPoll_WikiaMobileIndex.php
+++ extensions/wikia/WikiaPoll/templates/WikiaPoll_WikiaMobileIndex.php
@@ -1,14 +1,10 @@
 <aside class=WikiaPoll data-id="<?= $poll->getId() ?>"><div class=pollHeader><?= htmlspecialchars( $wf->Msg( 'wikiapoll-question', $poll->getTitle() ) ); ?></div>
 <ul>
-<?php
-foreach( $data['answers'] as $n => $answer ) {
-?>
+<?php foreach( $data['answers'] as $n => $answer ) { ?>
 <li>
 <div class=answer><?= htmlspecialchars( $answer['text'] ) ?></div>
 <div class=bar><div class=percentage style=width:<?= $answer['percentage']; ?>%></div></div><span class=perc><?= $answer['percentage']; ?>%</span>
 </li>
-<?php
-}
-?>
+<?php } ?>
 </ul><button class="wkBtn openPoll"><?= $wf->Msg('wikiamobile-wikiapoll-open-poll'); ?></button>
-<div class=votes><?= $wf->MsgExt( 'wikiapoll-people-voted', array( 'parsemag' ), $wg->Lang->formatNum( $data['votes'] ) ); ?><div></aside>
+<div class=votes><?= $wf->MsgExt( 'wikiapoll-people-voted', array( 'parsemag' ), $wg->Lang->formatNum( $data['votes'] ) ); ?></aside>
