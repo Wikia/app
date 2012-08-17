e055662f06476eb3b8b90fc700d2502a8f9668ce
macbre <macbre@wikia-inc.com>
2012-08-17 11:19:12 +0000

    Scribe: use native json_encode + fix unit tests
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58484 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Scribe/ScribeEventProducer.class.php
+++ extensions/wikia/Scribe/ScribeEventProducer.class.php
@@ -6,13 +6,13 @@ class ScribeEventProducer {
 
 	private $mediaNS = array(NS_IMAGE, NS_FILE);
 
-	const 
+	const
 		EDIT_CATEGORY 		    = 'log_edit',
 		CREATEPAGE_CATEGORY		= 'log_create',
 		UNDELETE_CATEGORY	    = 'log_undelete',
 		DELETE_CATEGORY		    = 'log_delete';
 
-	const 
+	const
 		EDIT_CATEGORY_INT			= 1,
 		CREATEPAGE_CATEGORY_INT		= 2,
 		DELETE_CATEGORY_INT			= 3,
@@ -21,34 +21,34 @@ class ScribeEventProducer {
 	function __construct( WikiaApp $app, $key, $archive = 0 ) {
 		$this->app = $app;
 		switch ( $key ) {
-			case 'edit' 		: 
-				$this->mKey = self::EDIT_CATEGORY; 
+			case 'edit' 		:
+				$this->mKey = self::EDIT_CATEGORY;
 				$this->mEventType = self::EDIT_CATEGORY_INT;
 				break;
-			case 'create' 		: 
-				$this->mKey = self::CREATEPAGE_CATEGORY; 
+			case 'create' 		:
+				$this->mKey = self::CREATEPAGE_CATEGORY;
 				$this->mEventType = self::CREATEPAGE_CATEGORY_INT;
 				break;
-			case 'delete' 		: 
-				$this->mKey = self::DELETE_CATEGORY; 
+			case 'delete' 		:
+				$this->mKey = self::DELETE_CATEGORY;
 				$this->mEventType = self::DELETE_CATEGORY_INT;
 				break;
-			case 'undelete'		: 
-				$this->mKey = self::UNDELETE_CATEGORY; 
+			case 'undelete'		:
+				$this->mKey = self::UNDELETE_CATEGORY;
 				$this->mEventType = self::UNDELETE_CATEGORY_INT;
 				break;
 		}
-		
+
 		$this->setCityId( $this->app->wg->CityId );
 		$this->setServerName( $this->app->wg->Server );
 		$this->setIp( $this->app->wg->Request->getIP() );
 		$this->setHostname( wfHostname() );
 		$this->setBeaconId ( wfGetBeaconId() );
-		$this->setArchive( $archive );	
+		$this->setArchive( $archive );
 		$this->setLanguage();
 		$this->setCategory();
 	}
-	
+
 	public function buildEditPackage( $oPage, $oUser, $oRevision = null, $revision_id = null ) {
 		$this->app->wf->ProfileIn( __METHOD__ );
 
@@ -57,13 +57,13 @@ class ScribeEventProducer {
 			$this->app->wf->ProfileOut( __METHOD__ );
 			return false;
 		}
-		
+
 		if ( !$oUser instanceof User ) {
 			Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid user object" );
 			$this->app->wf->ProfileOut( __METHOD__ );
 			return false;
 		}
-		
+
 		if ( !empty( $oRevision ) ) {
 			if ( !$oRevision instanceof Revision ) {
 				Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid revision object" );
@@ -71,10 +71,10 @@ class ScribeEventProducer {
 				return false;
 			}
 		}
-		
+
 		$revision_id = $page_id = $page_namespace = 0;
 		$oTitle = $oPage->getTitle();
-		
+
 		if ( $oRevision instanceof Revision ) {
 			if ( empty( $revision_id ) ) {
 				$revision_id = $oRevision->getId();
@@ -116,27 +116,27 @@ class ScribeEventProducer {
 		$this->setEventTS($d->format("Y-m-d\TH:i:s.uO"));
 
 		$this->app->wf->ProfileOut( __METHOD__ );
-		
+
 		return true;
 	}
-	
+
 	public function buildRemovePackage ( $oPage, $oUser, $page_id ) {
 		$this->app->wf->ProfileIn( __METHOD__ );
-	
+
 		if ( !is_object( $oPage ) ) {
 			Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid WikiPage object" );
 			$this->app->wf->ProfileOut( __METHOD__ );
 			return false;
 		}
-		
+
 		if ( !$oUser instanceof User ) {
 			Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid user object" );
 			$this->app->wf->ProfileOut( __METHOD__ );
 			return false;
 		}
-			
+
 		$oTitle = $oPage->getTitle();
-                                    
+
 		$table = 'recentchanges';
 		$what = array( 'rc_logid' );
 		$cond = array(
@@ -150,13 +150,13 @@ class ScribeEventProducer {
 		$dbr = wfGetDB( DB_SLAVE );
 		$oRow = $dbr->selectRow( $table, $what, $cond, __METHOD__, $options );
 		if ( !isset( $oRow->rc_logid ) ) {
-			$dbr = wfGetDB( DB_MASTER ); 
+			$dbr = wfGetDB( DB_MASTER );
 			$oRow = $dbr->selectRow( $table, $what, $cond, __METHOD__, $options );
 		}
-		
+
 		$logid = ( !empty( $oRow ) ) ? $oRow->rc_logid : 0;
 
-		if ( $logid ) { 
+		if ( $logid ) {
 			$this->setPageId( $page_id ) ;
 			$this->setPageNamespace( $oTitle->getNamespace() );
 			$this->setPageTitle( $oTitle->getDBkey() );
@@ -167,15 +167,15 @@ class ScribeEventProducer {
 			$this->setIsRedirect( $oTitle->isRedirect() );
 			$this->setRevisionTimestamp( wfTimestamp( TS_DB, $oPage->getTimestamp() ) );
 			$this->setLogId( $logid );
-		}	
+		}
 
 		$this->app->wf->ProfileOut( __METHOD__ );
-		return $logid;    
+		return $logid;
 	}
-	
+
 	public function buildUndeletePackage( $oTitle ) {
 		$this->app->wf->ProfileIn( __METHOD__ );
-			
+
 		if ( !is_object( $oTitle ) ) {
 			Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid title object" );
 			$this->app->wf->ProfileOut( __METHOD__ );
@@ -188,7 +188,7 @@ class ScribeEventProducer {
 			$this->app->wf->ProfileOut( __METHOD__ );
 			return true;
 		}
-		
+
 		$username = $oPage->getUserText();
 		if ( empty( $username ) ) {
 			Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid username" );
@@ -196,21 +196,21 @@ class ScribeEventProducer {
 			return true;
 		}
 
-		$oUser = F::build('User', array( $username ), 'newFromName');		
+		$oUser = F::build('User', array( $username ), 'newFromName');
 		if ( !$oUser instanceof User ) {
 			Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid user object" );
 			$this->app->wf->ProfileOut( __METHOD__ );
 			return true;
 		}
-		
+
 		$this->app->wf->ProfileOut( __METHOD__ );
-		
+
 		return $this->buildEditPackage( $oPage, $oUser );
 	}
-	
+
 	public function buildMovePackage( $oTitle, $oUser, $page_id = null, $redirect_id = null ) {
-		$this->app->wf->ProfileIn( __METHOD__ );		
-		
+		$this->app->wf->ProfileIn( __METHOD__ );
+
 		if ( !$oTitle instanceof Title ) {
 			Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid title object" );
 			$this->app->wf->ProfileOut( __METHOD__ );
@@ -226,15 +226,15 @@ class ScribeEventProducer {
 			Wikia::log( __METHOD__, "error", "Cannot send log using scribe ({$this->app->wg->CityId}): invalid revision object" );
 			$this->app->wf->ProfileOut( __METHOD__ );
 			return true;
-		}	
+		}
 
 		if ( empty( $page_id ) ) {
 			$page_id = $oRevision->getPage();
 		}
-		
+
 		if ( empty( $page_id ) || $page_id < 0 ) {
 			$page_id = $oTitle->getArticleId();
-		}		
+		}
 
 		$oPage = WikiPage::newFromID( $page_id );
 		if ( !$oPage instanceof WikiPage ) {
@@ -244,81 +244,81 @@ class ScribeEventProducer {
 		}
 
 		$this->app->wf->ProfileOut( __METHOD__ );
-		
+
 		return $this->buildEditPackage( $oPage, $oUser, $oRevision );
 	}
-						
-	public function setCityId ( $city_id ) { 
-		$this->mParams['cityId'] = $city_id; 
+
+	public function setCityId ( $city_id ) {
+		$this->mParams['cityId'] = $city_id;
 	}
-	
+
 	public function setServerName ( $server_name ) {
 		$this->mParams['serverName'] = $server_name;
 	}
-	
+
 	public function setHostname ( $hostname ) {
 		$this->mParams['hostname'] = $hostname;
 	}
-	
-	public function setPageId ( $page_id ) { 
-		$this->mParams['pageId'] = $page_id; 
+
+	public function setPageId ( $page_id ) {
+		$this->mParams['pageId'] = $page_id;
 	}
-	
-	public function setPageNamespace ( $page_namespace ) { 
-		$this->mParams['pageNamespace'] = $page_namespace; 
+
+	public function setPageNamespace ( $page_namespace ) {
+		$this->mParams['pageNamespace'] = $page_namespace;
 	}
-	
+
 	public function setPageTitle ( $title ) {
 		$this->mParams['pageTitle'] = $title;
 	}
-	
-	public function setRevisionId ( $revision_id  ) { 
+
+	public function setRevisionId ( $revision_id  ) {
 		$this->mParams['revId'] = $revision_id;
 	}
-	
-	public function setLogId ( $log_id  ) { 
-		$this->mParams['logId'] = $log_id; 
-	}
-	
-	public function setUserId ( $user_id ) { 
-		$this->mParams['userId'] = $user_id; 
-	}
-	
-	public function setUserIsBot ( $user_is_bot ) { 
-		$this->mParams['userIsBot'] = intval( $user_is_bot ); 
-	}
-	
-	public function setIsContent ( $is_content ) { 
-		$this->mParams['isContent'] = intval( $is_content ); 
-	}
-	
-	public function setIsRedirect ( $is_redirect ) { 
-		$this->mParams['isRedirect'] = intval( $is_redirect ); 
-	}
-	
-	public function setIP ( $ip ) { 
-		$this->mParams['userIp'] = $ip; 
-	}
-	
-	public function setRevisionTimestamp ( $revision_timestamp ) { 
-		$this->mParams['revTimestamp'] = $revision_timestamp; 
-	}
-	
-	public function setRevisionSize ( $size ) { 
-		$this->mParams['revSize'] = $size; 
+
+	public function setLogId ( $log_id  ) {
+		$this->mParams['logId'] = $log_id;
 	}
 
-	public function setEventTS ( $ts ) { 
-		$this->mParams['eventTS'] = $ts; 
+	public function setUserId ( $user_id ) {
+		$this->mParams['userId'] = $user_id;
 	}
-	
-	public function setMediaType ( $oTitle ) { 
+
+	public function setUserIsBot ( $user_is_bot ) {
+		$this->mParams['userIsBot'] = intval( $user_is_bot );
+	}
+
+	public function setIsContent ( $is_content ) {
+		$this->mParams['isContent'] = intval( $is_content );
+	}
+
+	public function setIsRedirect ( $is_redirect ) {
+		$this->mParams['isRedirect'] = intval( $is_redirect );
+	}
+
+	public function setIP ( $ip ) {
+		$this->mParams['userIp'] = $ip;
+	}
+
+	public function setRevisionTimestamp ( $revision_timestamp ) {
+		$this->mParams['revTimestamp'] = $revision_timestamp;
+	}
+
+	public function setRevisionSize ( $size ) {
+		$this->mParams['revSize'] = $size;
+	}
+
+	public function setEventTS ( $ts ) {
+		$this->mParams['eventTS'] = $ts;
+	}
+
+	public function setMediaType ( $oTitle ) {
 		$this->app->wf->ProfileIn( __METHOD__ );
 
 		$result = 0;
 		$page_namespace = $oTitle->getNamespace();
 		if ( in_array( $page_namespace, $this->mediaNS ) ) {
-			
+
 			$mediaType = MEDIATYPE_UNKNOWN;
 			$oLocalFile = RepoGroup::singleton()->getLocalRepo()->newFile( $oTitle );
 
@@ -342,30 +342,30 @@ class ScribeEventProducer {
 			}
 		}
 
-		$this->mParams['mediaType'] = $result; 
+		$this->mParams['mediaType'] = $result;
 		$this->app->wf->ProfileOut( __METHOD__ );
 	}
-	
-	public function setImageLinks ( $image_links ) { 
-		$this->mParams['imageLinks'] = $image_links; 
+
+	public function setImageLinks ( $image_links ) {
+		$this->mParams['imageLinks'] = $image_links;
 	}
-	
-	public function setVideoLinks ( $video_links ) { 
-		$this->mParams['videoLinks'] = $video_links; 
+
+	public function setVideoLinks ( $video_links ) {
+		$this->mParams['videoLinks'] = $video_links;
 	}
-	
-	public function setTotalWords ( $total_words ) { 
-		$this->mParams['totalWords'] = $total_words; 
+
+	public function setTotalWords ( $total_words ) {
+		$this->mParams['totalWords'] = $total_words;
 	}
-	
-	public function setArchive ( $archive ) { 
-		$this->mParams['archive'] = intval( $archive ); 
+
+	public function setArchive ( $archive ) {
+		$this->mParams['archive'] = intval( $archive );
 	}
 
 	public function setBeaconId ( $beacon_id ) {
 		$this->mParams['beaconId'] = $beacon_id;
 	}
-	
+
 	public function setLanguage( $lang_code = '' ) {
 		if ( empty( $lang_code ) ) {
 			$lang_code = $this->app->wg->LanguageCode;
@@ -373,15 +373,15 @@ class ScribeEventProducer {
 		$this->mParams['languageId'] = WikiFactory::LangCodeToId($lang_code);
 	}
 
-	
+
 	public function setCategory() {
 		$this->mParams['categoryId'] = WikiFactory::getCategory( $this->app->wg->CityId );
 	}
-	
+
 	public function sendLog() {
 		$this->app->wf->ProfileIn( __METHOD__ );
 		try {
-			$data = F::build('Wikia', array( $this->mParams ), 'json_encode');
+			$data = json_encode($this->mParams);
 			WScribeClient::singleton( $this->mKey )->send( $data );
 		}
 		catch( TException $e ) {
@@ -389,10 +389,10 @@ class ScribeEventProducer {
 		}
 		$this->app->wf->ProfileOut( __METHOD__ );
 	}
-	
+
 	public function setMediaLinks( $oPage ) {
 		wfProfileIn(__METHOD__);
-		
+
 		$links = array( 'image' => 0, 'video' => 0 );
 		if ( isset( $oPage->mPreparedEdit ) && isset( $oPage->mPreparedEdit->output ) ) {
 			$images = $oPage->mPreparedEdit->output->getImages();
@@ -409,25 +409,25 @@ class ScribeEventProducer {
 								default:
 									$links['image']++;
 							}
-						}						
+						}
 					}
 					else {
 						//@todo remove this code after video refactoring
 						if ( substr($iname, 0, 1) == ':' ) {
-							$links['video']++;							
+							$links['video']++;
 						} else {
 							$links['image']++;
-						}						
-					}										
+						}
+					}
 				}
-			}			
+			}
 		}
-		
+
 		$this->setImageLinks( $links['image'] );
 		$this->setVideoLinks( $links['video'] );
-		
+
 		wfProfileOut(__METHOD__);
-		
-		return $links;			
+
+		return $links;
 	}
 }
