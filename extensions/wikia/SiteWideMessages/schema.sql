c640fba136981f85cabbb4b16b64ad2df69a07f7
grunny <grunny@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-21 06:53:35 +0000

    Update SWM table create statement to its current state
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58716 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SiteWideMessages/schema.sql
+++ extensions/wikia/SiteWideMessages/schema.sql
@@ -23,7 +23,7 @@ CREATE index removed_mode_expire_date ON messages_text (msg_removed,msg_mode,msg
 CREATE TABLE IF NOT EXISTS `messages_status`
 (
 	`msg_wiki_id`      int (9),
-	`msg_recipient_id` int (10) unsigned, -- NULL -> message to ALL users
+	`msg_recipient_id` int (10) unsigned   NOT NULL    default 0,
 	`msg_id`           int (7)  unsigned   NOT NULL,
 	`msg_status`       tinyint             NOT NULL,
 	`msg_date`         timestamp           NOT NULL    default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
