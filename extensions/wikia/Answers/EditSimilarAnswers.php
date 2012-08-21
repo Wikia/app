6bbbaf44f4ae6654d486233dcfdfa2baa204d839
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-21 10:52:38 +0000

    HomePageList::related_unanswered_questions is now a static method
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58727 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Answers/EditSimilarAnswers.php
+++ extensions/wikia/Answers/EditSimilarAnswers.php
@@ -2,10 +2,10 @@
 
 $wgHooks["EditSimilar::getSimilarArticles"][] = "wfGetSimilarArticlesOnAnswers";
 
-function wfGetSimilarArticlesOnAnswers($data) {
+function wfGetSimilarArticlesOnAnswers(&$data) {
 		$html = HomePageList::related_unanswered_questions();
 		$html = preg_replace("/^<li>/", "", $html);
-		$html = preg_replace("/<\/li>$/", "", $html);
+		$html = preg_replace("/<\\/li>$/", "", $html);
 		$res  = explode("</li><li>", $html);
 
 		$data = $res;
