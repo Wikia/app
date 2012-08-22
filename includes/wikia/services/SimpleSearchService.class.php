17b7c4c201b938dcfc1bcd346b820ce6cbcdffa1
macbre <macbre@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-22 14:59:29 +0000

    SimpleSearchService: whitespaces
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58840 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/services/SimpleSearchService.class.php
+++ includes/wikia/services/SimpleSearchService.class.php
@@ -1,26 +1,26 @@
 <?php
 /**
  * Simplified API for local and wikia-wide (global) search
- * 
+ *
  * @author Federico "Lox" Lucignano
  * @deprecated
  * @see WikiaSearch
  */
 class SimpleSearchService extends WikiaService {
 	const DEFAULT_LIMIT = 100;
-	
+
 	private $mEnableCrossWikiaSearch;
 
 	public function init() {
 		$this->mEnableCrossWikiaSearch = $this->wg->EnableCrossWikiaSearch;
 	}
-	
+
 	/*
 	 * @todo separate the data fetching code in a model
 	 */
 	private function getResults() {
 		$this->wf->profileIn( __METHOD__ );
-		
+
 		//parameters
 		$key = trim( $this->getVal( 'key' ) );
 		$limit = $this->request->getInt( 'limit', self::DEFAULT_LIMIT );
@@ -29,28 +29,28 @@ class SimpleSearchService extends WikiaService {
 		$showRedirects = $this->request->getBool( 'redirects', true );
 		$urlParams = $this->getVal( 'urlParams', '' );
 		$fullURL = $this->request->getBool( 'fullURL', false );
-		
+
 		$results = Array();
 		$this->setVal( 'key', $key );
-		
+
 		if ( !empty( $key ) ) {
 			$search = F::build( 'SearchEngine', array(), 'create' );
-			
+
 			if ( $limit > 0 ) {
 				$search->setLimitOffset( $limit, $offset );
 			}
-			
+
 			$namespaces = array_merge( $search->namespaces, $namespaces );
 			$search->setNamespaces( $namespaces );
-			
+
 			$search->showRedirects = $showRedirects;
-			
+
 			$key = $search->transformSearchTerm( $key );
 			$rewritten = $search->replacePrefixes( $key );
 			$titleMatches = $search->searchTitle( $rewritten );
 			$textMatches = $search->searchText( $rewritten );
 			$totalCount = 0;
-			
+
 			if ( !( ( $search instanceof SearchErrorReporting ) && $search->getError() ) ) {
 				if ( empty( $this->wg->DisableTextSearch ) ) {
 					// Sometimes the search engine knows there are too many hits
@@ -58,55 +58,55 @@ class SimpleSearchService extends WikiaService {
 						//count number of results
 						$num = ( $titleMatches ? $titleMatches->numRows() : 0 ) +
 							( $textMatches ? $textMatches->numRows() : 0);
-						
+
 						//MW hooks
 						if( $num ) {
 							$this->app->runHook( 'SpecialSearchResults', array( $key, &$titleMatches, &$textMatches ) );
 						} else {
 							$this->app->runHook( 'SpecialSearchNoResults', array( $key ) );
 						}
-						
+
 						$this->setVal( 'count', $num );
-						
+
 						if ( $titleMatches && !is_null( $titleMatches->getTotalHits() ) ) {
 							$totalCount += $titleMatches->getTotalHits();
 						}
-						
+
 						if ( $textMatches && !is_null( $textMatches->getTotalHits() ) ) {
 							$totalCount += $textMatches->getTotalHits();
 						}
-						
+
 						$this->setVal( 'totalCount', $totalCount );
-						
+
 						// did you mean... suggestions
 						if ($textMatches && $textMatches->hasSuggestion() ) {
 							$this->setVal( 'suggestionQuery', $textMatches->getSuggestionQuery() );
 							$this->setVal( 'suggestionSnippet', $textMatches->getSuggestionSnippet() );
 						}
-						
+
 						foreach ( array( 'title', 'text' ) as $set ) {
 							$resultSetVar = "{$set}Matches";
 							$matches = $$resultSetVar;
-							
+
 							if ( $matches ) {
 								if ( $matches->numRows() ) {
 									$this->setVal( "{$set}ResultsInfo", $matches->getInfo() );
 									$results = array();
-									
+
 									while ( $result = $matches->next() ) {
 										if ( !$result->isBrokenTitle() && !$result->isMissingRevision() ) {
 											$title = $result->getTitle();
-											
+
 											$results[] = array(
 												'textForm' => $title->getText(),
 												'urlForm' => ( $fullURL ) ? $title->getFullUrl( $urlParams ) : $title->getLocalUrl( $urlParams )
 											);
 										}
 									}
-									
+
 									$this->setVal( "{$set}Results", $results );
 								}
-								
+
 								$matches->free();
 							}
 						}
@@ -126,15 +126,15 @@ class SimpleSearchService extends WikiaService {
 			$this->wf->profileOut( __METHOD__ );
 			throw new SimpleSearchEmptyKeyException();
 		}
-		
+
 		$this->setVal( 'results', $results );
-		
+
 		$this->wf->profileOut( __METHOD__ );
 	}
-	
+
 	/**
 	 * @brief Runs a local search on the current wiki
-	 * 
+	 *
 	 * @todo Finish documenting
 	 * @see getResults
 	 */
@@ -143,10 +143,10 @@ class SimpleSearchService extends WikiaService {
 		$this->getResults();
 		$this->wg->EnableCrossWikiaSearch = $this->mEnableCrossWikiaSearch;
 	}
-	
+
 	/**
 	 * @brief Runs a wikia-wide (global) search
-	 * 
+	 *
 	 * @todo Finish documenting
 	 * @see getResults
 	 */
