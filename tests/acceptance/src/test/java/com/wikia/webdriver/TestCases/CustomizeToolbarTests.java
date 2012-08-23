6c537b246742b37141bbe9661d1889c8007e1b93
michalnowierski <michalnowierski@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-23 10:35:21 +0000

    1. Updating BasePageObject 2. Adding Tests to CustomizeToolbarTests.java
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58885 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/TestCases/CustomizeToolbarTests.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/TestCases/CustomizeToolbarTests.java
@@ -22,7 +22,7 @@ public class CustomizeToolbarTests extends TestTemplate{
 	private String videoURL2name = "What is love (?) - on piano (Haddway)";
 	
 	
-//	@Test(groups = {"CustomizeToolbar001"}) 
+	@Test(groups = {"CustomizeToolbar001"}) 
 //	https://internal.wikia-inc.com/wiki/QA/Core_Features_and_Testing/Manual_Regression_Tests/Customize_Toolbar_Adding
 	public void CustomizeToolbar001_Adding()
 	{
@@ -33,15 +33,15 @@ public class CustomizeToolbarTests extends TestTemplate{
 		article.customizeToolbar_ClickOnResetDefaults();
 		article.customizeToolbar_TypeIntoFindATool("e");
 		article.customizeToolbar_ClickOnFoundTool("Edit");
-		article.customizeToolbar_VerifyToolOnToolbarList("PageAction:Edit");
+		article.customizeToolbar_VerifyToolOnToolbarList("Edit");
 		article.customizeToolbar_ClickOnSaveButton();
 		article.customizeToolbar_VerifyToolOnToolbar("Edit");
 		
 	}
 	
 	@Test(groups = {"CustomizeToolbar002"}) 
-//	https://internal.wikia-inc.com/wiki/QA/Core_Features_and_Testing/Manual_Regression_Tests/Customize_Toolbar_Adding
-	public void CustomizeToolbar001_Editing()
+//	https://internal.wikia-inc.com/wiki/QA/Core_Features_and_Testing/Manual_Regression_Tests/Customize_Toolbar_Editing
+	public void CustomizeToolbar002_Editing()
 	{
 		WikiBasePageObject wiki = new WikiBasePageObject(driver, Global.DOMAIN);
 		WikiArticlePageObject article = wiki.OpenArticle(wikiArticle);
@@ -50,11 +50,103 @@ public class CustomizeToolbarTests extends TestTemplate{
 		article.customizeToolbar_ClickOnResetDefaults();
 		article.customizeToolbar_TypeIntoFindATool("e");
 		article.customizeToolbar_ClickOnFoundTool("Edit");
-		article.customizeToolbar_VerifyToolOnToolbarList("PageAction:Edit");
-		article.customizeToolbar_ClickOnToolRemoveButton("PageAction:Edit");
-//		article.customizeToolbar_VerifyToolNotOnToolbarList("PageAction:Edit");
+		article.customizeToolbar_VerifyToolOnToolbarList("Edit");
+		article.customizeToolbar_ClickOnToolRenameButton("Edit");
+		article.customizeToolbar_TypeIntoRenameItemDialog("Edit123");
+		article.customizeToolbar_saveInRenameItemDialog();
+		article.customizeToolbar_VerifyToolOnToolbarList("Edit123");
 		article.customizeToolbar_ClickOnSaveButton();
-		article.customizeToolbar_VerifyToolNotOnToolbar("Edit");
+		article.customizeToolbar_VerifyToolOnToolbar("Edit123");
 				
 	}
+	
+//	@Test(groups = {"CustomizeToolbar003"}) 
+//	https://internal.wikia-inc.com/wiki/QA/Core_Features_and_Testing/Manual_Regression_Tests/Customize_Toolbar_Moving
+	public void CustomizeToolbar003_Moving()
+	{
+		WikiBasePageObject wiki = new WikiBasePageObject(driver, Global.DOMAIN);
+		WikiArticlePageObject article = wiki.OpenArticle(wikiArticle);
+		CommonFunctions.logIn(Properties.userName2, Properties.password2);
+		article.customizeToolbar_ClickCustomize();
+		article.customizeToolbar_ClickOnResetDefaults();
+		
+		
+	}
+	
+	@Test(groups = {"CustomizeToolbar004"}) 
+//	https://internal.wikia-inc.com/wiki/QA/Core_Features_and_Testing/Manual_Regression_Tests/Customize_Toolbar_Deleting
+	public void CustomizeToolbar004_Deleteing()
+	{
+		WikiBasePageObject wiki = new WikiBasePageObject(driver, Global.DOMAIN);
+		WikiArticlePageObject article = wiki.OpenArticle(wikiArticle);
+		CommonFunctions.logIn(Properties.userName2, Properties.password2);
+		article.customizeToolbar_ClickCustomize();
+		article.customizeToolbar_ClickOnResetDefaults();
+		article.customizeToolbar_TypeIntoFindATool("e");
+		article.customizeToolbar_ClickOnFoundTool("Edit");
+		article.customizeToolbar_VerifyToolOnToolbarList("Edit");
+		article.customizeToolbar_ClickOnToolRemoveButton("Edit");
+//		article.customizeToolbar_VerifyToolNotOnToolbarList("Edit");
+		article.customizeToolbar_ClickOnSaveButton();
+		article.customizeToolbar_VerifyToolNotOnToolbar("Edit");
+	}
+	
+	@Test(groups = {"CustomizeToolbar005"}) 
+//	https://internal.wikia-inc.com/wiki/QA/Core_Features_and_Testing/Manual_Regression_Tests/Customize_Toolbar_Finding
+	public void CustomizeToolbar005_Finding()
+	{
+		WikiBasePageObject wiki = new WikiBasePageObject(driver, Global.DOMAIN);
+		WikiArticlePageObject article = wiki.OpenArticle(wikiArticle);
+		CommonFunctions.logIn(Properties.userName2, Properties.password2);
+		article.customizeToolbar_ClickCustomize();
+		article.customizeToolbar_ClickOnResetDefaults();
+		article.customizeToolbar_TypeIntoFindATool("Up");
+		article.customizeToolbar_ClickOnFoundTool("Upload photo");
+		article.customizeToolbar_VerifyToolOnToolbarList("Upload photo");
+		article.customizeToolbar_ClickOnSaveButton();
+		article.customizeToolbar_VerifyToolOnToolbar("Upload photo");
+	}
+	
+	@Test(groups = {"CustomizeToolbar006"}) 
+//	https://internal.wikia-inc.com/wiki/QA/Core_Features_and_Testing/Manual_Regression_Tests/Customize_Toolbar_Reset_Defaults
+	public void CustomizeToolbar006_ResetDefaults()
+	{
+		WikiBasePageObject wiki = new WikiBasePageObject(driver, Global.DOMAIN);
+		WikiArticlePageObject article = wiki.OpenArticle(wikiArticle);
+		CommonFunctions.logIn(Properties.userName2, Properties.password2);
+		article.customizeToolbar_ClickCustomize();
+		article.customizeToolbar_ClickOnResetDefaults();
+		article.customizeToolbar_TypeIntoFindATool("Up");
+		article.customizeToolbar_ClickOnFoundTool("Upload photo");
+		article.customizeToolbar_VerifyToolOnToolbarList("Upload photo");
+		article.customizeToolbar_ClickOnSaveButton();
+		article.customizeToolbar_VerifyToolOnToolbar("Upload photo");
+		article.customizeToolbar_ClickCustomize();
+		article.customizeToolbar_ClickOnResetDefaults();
+//		article.customizeToolbar_VerifyToolNotOnToolbarList("Upload photo");
+		article.customizeToolbar_ClickOnSaveButton();
+		article.customizeToolbar_VerifyToolNotOnToolbar("Upload photo");
+	}
+	
+	@Test(groups = {"CustomizeToolbar007"}) 
+//	https://internal.wikia-inc.com/wiki/QA/Core_Features_and_Testing/Manual_Regression_Tests/Customize_Toolbar_Buttons_actions
+	public void CustomizeToolbar007_ButtonsActions()
+	{
+		WikiBasePageObject wiki = new WikiBasePageObject(driver, Global.DOMAIN);
+		WikiArticlePageObject article = wiki.OpenArticle(wikiArticle);
+		CommonFunctions.logIn(Properties.userName2, Properties.password2);
+		article.customizeToolbar_ClickCustomize();
+		article.customizeToolbar_ClickOnResetDefaults();
+		article.customizeToolbar_TypeIntoFindATool("Fo");
+		article.customizeToolbar_ClickOnFoundTool("Follow");
+		article.customizeToolbar_VerifyToolOnToolbarList("Follow");
+		article.customizeToolbar_ClickOnSaveButton();
+		article.customizeToolbar_VerifyToolOnToolbar("Follow");
+		article.customizeToolbar_ClickOnTool("follow");
+		article.customizeToolbar_VerifyToolOnToolbar("Following");
+		article.customizeToolbar_ClickOnTool("follow");
+		article.customizeToolbar_VerifyToolOnToolbar("Follow");
+		
+	}
+	
 }
