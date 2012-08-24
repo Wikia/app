be2da6b2056b6309a0aad63f6d5fb416d3f08d7a
pbablok <pbablok@wikia-inc.com>
2012-08-24 13:30:28 +0000

    Achievements: migration script 1 (adding tables)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58990 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AchievementsII/schema_local.sql
+++ extensions/wikia/AchievementsII/schema_local.sql
@@ -33,14 +33,13 @@ CREATE TABLE IF NOT EXISTS `ach_custom_badges` (
 	`sponsored` TINYINT(1) NOT NULL DEFAULT 0,
 	`badge_tracking_url` VARCHAR(255) DEFAULT NULL,
 	`hover_tracking_url` VARCHAR(255) DEFAULT NULL,
-	`click_tracking_url` VARCHAR(255) DEFAULT NULL;
+	`click_tracking_url` VARCHAR(255) DEFAULT NULL,
 	KEY `id` (`id`),
 	KEY `type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-CREATE TABLE `ach_ranking_snapshots` (
+CREATE TABLE IF NOT EXISTS `ach_ranking_snapshots` (
 	`date` DATETIME NOT NULL ,
 	`data` TEXT NOT NULL
 ) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-
