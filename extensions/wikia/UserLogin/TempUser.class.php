8b0ab71eac7dd52aeda7cb999c160b7abc3eab3a
macbre <macbre@wikia-inc.com>
2012-08-24 14:17:21 +0000

    TempUser: code cleanup (BugId:45406)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58996 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserLogin/TempUser.class.php
+++ extensions/wikia/UserLogin/TempUser.class.php
@@ -7,7 +7,7 @@
  *
  */
 class TempUser extends WikiaModel {
-	
+
 	protected $user_id = 0;
 	protected $user_name = '';
 	protected $user_password = '';
@@ -16,17 +16,17 @@ class TempUser extends WikiaModel {
 	protected $user_birthdate = '';
 	protected $user_source = '';
 	protected $user_wiki_id = 0;
-	
+
 	const DefaultPassword = '';
 	const DefaultEmail = '';
-		
+
 	public function __construct( $data = array() ) {
 		foreach ( $data as $key => $value ) {
 			$this->$key = $value;
 		}
 		parent::__construct();
 	}
-	
+
 	/**
 	 * get temp user's id
 	 * @return string $user_id
@@ -34,7 +34,7 @@ class TempUser extends WikiaModel {
 	public function getId() {
 		return $this->user_id;
 	}
-	
+
 	/**
 	 * get temp user's name
 	 * @return string user_name
@@ -42,7 +42,7 @@ class TempUser extends WikiaModel {
 	public function getName() {
 		return $this->user_name;
 	}
-	
+
 	/**
 	 * get temp user's email
 	 * @return string user_email
@@ -50,7 +50,7 @@ class TempUser extends WikiaModel {
 	public function getEmail() {
 		return $this->user_email;
 	}
-	
+
 	/**
 	 * get temp user's password
 	 * @return string user_password
@@ -58,7 +58,7 @@ class TempUser extends WikiaModel {
 	public function getPassword() {
 		return $this->user_password;
 	}
-	
+
 	/**
 	 * get temp user's source page
 	 * @return string user_source
@@ -66,7 +66,7 @@ class TempUser extends WikiaModel {
 	public function getSource() {
 		return $this->user_source;
 	}
-	
+
 	/**
 	 * get temp user's wiki id
 	 * @return integer user_wiki_id
@@ -74,43 +74,43 @@ class TempUser extends WikiaModel {
 	public function getWikiId() {
 		return $this->user_wiki_id;
 	}
-	
+
 	/**
 	 * set temp user's id
 	 */
 	public function setId( $id ) {
 		$this->user_id = $id;
 	}
-	
+
 	/**
 	 * set temp user's email
 	 */
 	public function setEmail( $email ) {
 		$this->user_email = $email;
 	}
-	
+
 	/**
 	 * set temp user's password
 	 */
 	public function setPassword( $password ) {
 		$this->user_password = $password;
 	}
-	
-	/*
+
+	/**
 	 * get default name for temp user
 	 */
 	public static function getDefaultName( $userId ) {
 		return 'TempUser'.$userId;
 	}
-	
+
 	/**
 	 * create TempUser object from User object
 	 * @param User object $user
-	 * @return TempUser object $tempUser 
+	 * @return TempUser object $tempUser
 	 */
 	public static function createNewFromUser( $user, $source='' ) {
 		global $wgCityId;
-		
+
 		$class = __CLASS__;
 		$wikiId = ( empty($wgCityId) ) ? 0 : $wgCityId;
 		$params = array(
@@ -126,16 +126,16 @@ class TempUser extends WikiaModel {
 		$tempUser = new $class( $params );
 		return $tempUser;
 	}
-	
+
 	/**
-	 * get memcache key for temp user 
+	 * get memcache key for temp user
 	 * @param string $username
-	 * @return string key 
+	 * @return string key
 	 */
 	protected static function getMemKeyTempUser( $username ) {
 		return F::app()->wf->SharedMemcKey( 'userlogin', 'temp_user', md5($username) );
 	}
-	
+
 	/**
 	 * get temp user from name
 	 * @param string $username
@@ -146,7 +146,7 @@ class TempUser extends WikiaModel {
 		$tempUser = false;
 
 		$app->wf->ProfileIn( __METHOD__ );
-		
+
 		$username = User::getCanonicalName( $username, 'valid' );
 		if ( $username != false ) {
 			$memKey = self::getMemKeyTempUser( $username );
@@ -163,17 +163,17 @@ class TempUser extends WikiaModel {
 				if ( $row ) {
 					$rowArr = get_object_vars( $row );
 					$tempUser = F::build( __CLASS__, array($rowArr) );
-				
+
 					$app->wg->Memc->set( $memKey, $tempUser, 60*60*24 );
 				}
 			}
 		}
-		
+
 		$app->wf->ProfileOut( __METHOD__ );
-		
+
 		return $tempUser;
 	}
-	
+
 	/**
 	 * map temp user to user
 	 * @param boolean $resetUserId
@@ -182,16 +182,17 @@ class TempUser extends WikiaModel {
 	 */
 	public function mapTempUserToUser( $resetUserId=true, $user=null ) {
 		if ( !$user ) {
-			$user = F::build( 'User' , array(self::getDefaultName($this->getId())), 'newFromName' );
+			$user = F::build( 'User' , array(self::getDefaultName($this->getId())), 'newFromName' ); /** @var User $user */
 			if ( $user->getId() == 0 ) {
 				$user = F::build( 'User' , array($this->getId()), 'newFromId' );
 				$user->loadFromDatabase();
 			}
 		}
-		
-		if ( $resetUserId )
+
+		if ( $resetUserId ) {
 			$user->mId = 0;
-		
+		}
+
 		$user->mName = $this->user_name;
 		$user->mPassword = $this->user_password;
 		$user->mEmail = $this->user_email;
@@ -200,12 +201,12 @@ class TempUser extends WikiaModel {
 
 		return $user;
 	}
-	
+
 	/**
 	 * save settings for temp user - update username, passwordm email for temp user before saving
-	 * @param User object $user 
+	 * @param User object $user
 	 */
-	public function saveSettingsTempUserToUser( &$user ) {
+	public function saveSettingsTempUserToUser( User &$user ) {
 		if ( $user->mId == 0 ) {
 			$user->mId = $this->getId();
 		}
@@ -214,13 +215,13 @@ class TempUser extends WikiaModel {
 		$user->mEmail = self::DefaultEmail;
 		$user->saveSettings();
 	}
-		
+
 	/**
 	 * add temp user to database
 	 */
 	public function addToDatabase() {
 		$this->wf->ProfileIn( __METHOD__ );
-		
+
 		if ( !$this->wf->ReadOnly() ) {
 			$db = $this->wf->GetDB( DB_MASTER, array(), $this->wg->ExternalSharedDB );
 			$db->insert(
@@ -237,17 +238,17 @@ class TempUser extends WikiaModel {
 				),
 				__METHOD__
 			);
-			$db->commit();
+			$db->commit(__METHOD__);
 		}
 		$this->wf->ProfileOut( __METHOD__ );
 	}
-	
+
 	/**
 	 * remove temp user from database by user id
 	 */
 	public function removeFromDatabase() {
 		$this->wf->ProfileIn( __METHOD__ );
-		
+
 		if ( !$this->wf->ReadOnly() && !empty($this->user_id) ) {
 			$db = $this->wf->GetDB( DB_MASTER, array(), $this->wg->ExternalSharedDB );
 			$db->delete(
@@ -255,8 +256,8 @@ class TempUser extends WikiaModel {
 				array( 'user_id' => $this->user_id),
 				__METHOD__
 			);
-			$db->commit();
-			
+			$db->commit(__METHOD__);
+
 			// clear temp user cache
 			$memKey = self::getMemKeyTempUser( $this->getName() );
 			$this->wg->Memc->delete( $memKey );
@@ -264,47 +265,47 @@ class TempUser extends WikiaModel {
 
 		$this->wf->ProfileOut( __METHOD__ );
 	}
-	
+
 	/**
 	 * update temp user by user id
 	 */
 	public function updateData() {
 		$this->wf->ProfileIn( __METHOD__ );
-		
+
 		if ( !$this->wf->ReadOnly() && !empty($this->user_id) && !empty($this->user_email) ) {
 			$db = $this->wf->GetDB( DB_MASTER, array(), $this->wg->ExternalSharedDB );
 			$db->update(
 				'user_temp',
-				array( 
-					'user_email' => $this->user_email, 
+				array(
+					'user_email' => $this->user_email,
 					'user_password' => $this->user_password,
 				),
 				array( 'user_id' => $this->user_id ),
 				__METHOD__
 			);
-			$db->commit();
+			$db->commit(__METHOD__);
 		}
-		
+
 		// clear temp user cache
 		$memKey = self::getMemKeyTempUser( $this->getName() );
 		$this->wg->Memc->delete( $memKey );
-		
+
 		$this->wf->ProfileOut( __METHOD__ );
 	}
-	
+
 	/**
 	 * activate user
-	 * @param User object $user 
-	 * @return User object $user 
+	 * @param User object $user
+	 * @return User object $user
 	 */
-	public function activateUser( $user ) {
+	public function activateUser( User $user ) {
 		$this->wf->ProfileIn( __METHOD__ );
-		
+
 		// confirm email
 		$user = $this->mapTempUserToUser( false, $user );
 		$user->confirmEmail();
 		$user->saveSettings();
-		
+
 		// send welcome email
 		$emailParams = array(
 			'$USERNAME' => $user->getName(),
@@ -313,32 +314,32 @@ class TempUser extends WikiaModel {
 			'$EXPLOREWIKISURL' => 'http://www.wikia.com',
 		);
 
-		$userLoginHelper = F::build( 'UserLoginHelper' );
+		$userLoginHelper = F::build( 'UserLoginHelper' ); /** @var UserLoginHelper $userLoginHelper */
 		$userLoginHelper->sendEmail( $user, 'WelcomeMail', 'usersignup-welcome-email-subject', 'usersignup-welcome-email-body', $emailParams, 'welcome-email', 'WelcomeMail' );
-		
+
 		// remove temp user
 		$this->removeFromDatabase();
 
 		// Update user count
 		$ssUpdate = new SiteStatsUpdate( 0, 0, 0, 0, 1 );
 		$ssUpdate->doUpdate();
-		
+
 		$this->wf->RunHooks( 'AddNewAccount', array( $user, false ) );
 		$userLoginHelper->addNewUserLogEntry( $user );
-		
+
 		$this->wf->RunHooks( 'ConfirmEmailComplete', array( &$user ) );
-		
+
 		$this->wf->ProfileOut( __METHOD__ );
-		
+
 		return $user;
 	}
-	
+
 	public static function clearTempUserSession() {
 		unset($_SESSION['tempUserId']);
 	}
-	
+
 	public function setTempUserSession() {
 		$_SESSION['tempUserId'] = $this->getId();
 	}
-	
+
 }
