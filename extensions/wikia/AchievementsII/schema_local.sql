0fec4c4b5b16031558587938c41d9b5d7d926eb6
pbablok <pbablok@wikia-inc.com>
2012-08-24 12:44:57 +0000

    Achievements: remove Drop tables from local schema (just in case)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58976 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AchievementsII/schema_local.sql
+++ extensions/wikia/AchievementsII/schema_local.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS `ach_user_score`;
 CREATE TABLE IF NOT EXISTS `ach_user_score` (
 	`user_id` int(5) unsigned NOT NULL,
 	`score` int(10) unsigned NOT NULL,
@@ -6,7 +5,6 @@ CREATE TABLE IF NOT EXISTS `ach_user_score` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-DROP TABLE IF EXISTS `ach_user_counters`;
 CREATE TABLE IF NOT EXISTS `ach_user_counters` (
 	`user_id` int(5) unsigned NOT NULL,
 	`data` text NOT NULL,
@@ -14,7 +12,6 @@ CREATE TABLE IF NOT EXISTS `ach_user_counters` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-DROP TABLE IF EXISTS `ach_user_badges`;
 CREATE TABLE IF NOT EXISTS `ach_user_badges` (
 	`user_id` int(5) unsigned NOT NULL,
 	`badge_type_id` int(10) signed NOT NULL,
@@ -28,7 +25,6 @@ CREATE TABLE IF NOT EXISTS `ach_user_badges` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-DROP TABLE IF EXISTS `ach_custom_badges`;
 CREATE TABLE IF NOT EXISTS `ach_custom_badges` (
 	`id` int(10) signed NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`cat` varchar(255),
@@ -43,7 +39,6 @@ CREATE TABLE IF NOT EXISTS `ach_custom_badges` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-DROP TABLE IF EXISTS `ach_ranking_snapshots`;
 CREATE TABLE `ach_ranking_snapshots` (
 	`date` DATETIME NOT NULL ,
 	`data` TEXT NOT NULL
