d98f9b8f5466ec02739f42181aa53222d8ca1ae9
marzjan <marzjan@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-24 13:53:00 +0000

    BugID: 45369 - Pulling of data for WikiaBar, Failsafe implemented, WIP
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@58991 e846bd91-c042-42a7-a218-4b65e2c1904d

--- skins/oasis/modules/models/wikiabar/WikiaBarDataFailsafeModel.class.php
+++ skins/oasis/modules/models/wikiabar/WikiaBarDataFailsafeModel.class.php
@@ -8,8 +8,107 @@
  */
 
 class WikiaBarDataFailsafeModel extends WikiaBarModelBase {
-	public function getData() {
 
+	/**
+	 * @var array
+	 * Structure should look like:
+	 * [
+	 *        language =>
+	 *             [
+	 *                 varticalid => message,
+	 *                 varticalid => message
+	 *            ],
+	 *        language =>
+	 *             [
+	 *                 varticalid => message,
+	 *                 varticalid => message
+	 *            ]
+	 * ]
+	 */
+	protected $data = array(
+		'en' => array(
+			WikiFactoryHub::CATEGORY_ID_GAMING =>
+			'button-1-class=cup
+button-1-text=Summer Olympics
+button-1-href=http://www.wikia.com
+button-2-class=travel
+button-2-text=Travel
+button-2-href=http://www.wikia.com
+button-3-class=beauty
+button-3-text=Beauty
+button-3-href=http://www.wikia.com
+button-4-class=social
+button-4-text=@WikiaLifestyle
+button-4-href=http://www.wikia.com
+line-1-text=Line one text / Gaming
+line-1-href=http://www.wikia.com
+line-2-text=Line two text / Gaming
+line-2-href=http://www.wikia.com
+line-3-text=Line three text / Gaming
+line-3-href=http://www.wikia.com
+line-4-text=Line four text / Gaming
+line-4-href=http://www.wikia.com
+line-5-text=Line five text / Gaming
+line-5-href=http://www.wikia.com
+',
+			WikiFactoryHub::CATEGORY_ID_ENTERTAINMENT =>
+			'button-1-class=cup
+button-1-text=Movie Trailers
+button-1-href=http://movies.wikia.com/wiki/Moviepedia
+button-2-class=travel
+button-2-text=Fall TV
+button-2-href=http://www.wikia.com/Entertainment/TV_Schedule
+button-3-class=beauty
+button-3-text=New In Entertainment
+button-3-href=http://www.wikia.com/Entertainment
+button-4-class=social
+button-4-text=@WikiaEnt
+button-4-href=https://twitter.com/wikiaent
+line-1-text=Go behind the scenes for the 23rd Bond film.
+line-1-href=http://jamesbond.wikia.com/wiki/Skyfall
+line-2-text=Learn about the mysterious blackout in J.J. Abrams\' Revolution.
+line-2-href=http://revolution.wikia.com/wiki/Revolution_Wiki
+line-3-text=Explore the final adaptation of Twilight: Breaking Dawn
+line-3-href=http://twilightsaga.wikia.com/wiki/Breaking_Dawn
+line-4-text=Look up the Lyrics of your favorite song now!
+line-4-href=http://lyrics.wikia.com/Lyrics_Wiki
+line-5-text=Follow the development process of the new Hobbit trilogy.
+line-5-href=http://lotr.wikia.com/wiki/The_Hobbit_%28films%29
+',
+			WikiFactoryHub::CATEGORY_ID_LIFESTYLE => 'button-1-class=cup
+button-1-text=Summer Olympics
+button-1-href=http://www.wikia.com
+button-2-class=travel
+button-2-text=Travel
+button-2-href=http://www.wikia.com
+button-3-class=beauty
+button-3-text=Beauty
+button-3-href=http://www.wikia.com
+button-4-class=social
+button-4-text=@WikiaLifestyle
+button-4-href=http://www.wikia.com
+line-1-text=Line one text / Lifestyle
+line-1-href=http://www.wikia.com
+line-2-text=Line two text / Lifestyle
+line-2-href=http://www.wikia.com
+line-3-text=Line three text / Lifestyle
+line-3-href=http://www.wikia.com
+line-4-text=Line four text / Lifestyle
+line-4-href=http://www.wikia.com
+line-5-text=Line five text / Lifestyle
+line-5-href=http://www.wikia.com
+'
+		)
+	);
+
+	public function getData() {
+		if (
+			!empty($this->data[$this->getLang()])
+			&& !empty($this->data[$this->getLang()][$this->getVertical()])
+		) {
+			$message = trim($this->data[$this->getLang()][$this->getVertical()]);
+			return $message;
+		}
 	}
 }
 
--- skins/oasis/modules/models/wikiabar/WikiaBarModel.class.php
+++ skins/oasis/modules/models/wikiabar/WikiaBarModel.class.php
@@ -10,16 +10,43 @@
 class WikiaBarModel extends WikiaBarModelBase {
 	const WIKIA_BAR_TYPE_DATA_MODEL = 1;
 	const WIKIA_BAR_TYPE_DATA_FAILSAFE_MODEL = 2;
-	
-	
+
+	protected $requiredFields = array(
+		'button-1-class',
+		'button-1-text',
+		'button-1-href',
+		'button-2-class',
+		'button-2-text',
+		'button-2-href',
+		'button-3-class',
+		'button-3-text',
+		'button-3-href',
+		'button-4-class',
+		'button-4-text',
+		'button-4-href',
+		'line-1-text',
+		'line-1-href',
+		'line-2-text',
+		'line-2-href',
+		'line-3-text',
+		'line-3-href',
+		'line-4-text',
+		'line-4-href',
+		'line-5-text',
+		'line-5-href'
+	);
+
+
 	public function getBarContents() {
 		$message = $this->getRegularMessage();
 		$parseResult = $this->parseBarConfigurationMessage($message);
+		$this->status = true;
 
-		if(!$parseResult) {
-			Wikia::log(__METHOD__, null, 'WikiaBar message ' . implode('',array($this->getVertical(),'/',$this->getLang())) . ' falling back to failsafe');
+		if (!$parseResult) {
+			Wikia::log(__METHOD__, null, 'WikiaBar message ' . implode('', array($this->getVertical(), '/', $this->getLang())) . ' falling back to failsafe');
 			$message = $this->getFailsafeMessage();
 			$parseResult = $this->parseBarConfigurationMessage($message);
+			$this->status = false;
 		}
 
 		return $parseResult;
@@ -51,7 +78,7 @@ class WikiaBarModel extends WikiaBarModelBase {
 	 * @return bool|WikiaBarModelBase
 	 */
 	protected function getModel($modelType) {
-		switch($modelType) {
+		switch ($modelType) {
 			case self::WIKIA_BAR_TYPE_DATA_MODEL:
 				$model = F::build('WikiaBarDataModel');
 				break;
@@ -69,6 +96,39 @@ class WikiaBarModel extends WikiaBarModelBase {
 	 * @return bool|array
 	 */
 	protected function parseBarConfigurationMessage($message) {
+		$data = array();
+		$valid = true;
 
+		if(empty($message)) {
+			$valid = false;
+		}
+
+		if($valid) {
+			$lines = explode("\n", $message);
+		}
+
+		if (!empty($lines)) {
+			foreach ($lines as $line) {
+				if (stripos($line, '=') !== false) {
+					list($key, $val) = explode('=', $line);
+					$val = trim($val);
+					if (empty($val)) {
+						$valid = false;
+					}
+					$data[$key] = $val;
+				}
+			}
+			if(count(array_intersect(array_keys($data),$this->requiredFields)) != count($this->requiredFields)) {
+				$valid = false;
+			}
+		} else {
+			$valid = false;
+		}
+
+		if($valid) {
+			return $data;
+		} else {
+			return $valid;
+		}
 	}
 }
