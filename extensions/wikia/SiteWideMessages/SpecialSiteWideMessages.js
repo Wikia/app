64b42864c3faa5b56e76d5d506b94cc3888474aa
grunny <grunny@wikia-inc.com>
2012-08-26 15:18:49 +0000

    (BugID: 45402) Add click tracking to SiteWideMessages in Monobook
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59032 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SiteWideMessages/SpecialSiteWideMessages.js
+++ extensions/wikia/SiteWideMessages/SpecialSiteWideMessages.js
@@ -6,13 +6,41 @@ $(document).ready(function(){
 			$(this).bind('click', {id: id}, SWMAjaxDismiss);
 		}
 	});
+	$( '.SWM_message' ).each( function() {
+		var msgId = parseInt( $( this ).attr( 'id' ).substr( 4 ) );
+		$( this ).find( 'p a' ).click( function () {
+			var trackObj = {
+				ga_category: 'sitewidemessages',
+				ga_action: WikiaTracker.ACTIONS.CLICK_LINK_TEXT,
+				ga_label: 'swm-link',
+				ga_value: msgId,
+				href: $( this ).attr( 'href' )
+			};
+			WikiaTracker.trackEvent(
+				'trackingevent',
+				trackObj,
+				'internal'
+			);
+		} );
+	} );
 });
 
-function SWMAjaxDismiss(e) {
+function SWMAjaxDismiss( e ) {
 	e.preventDefault();
-	var id = e.data.id;
-	var ajaxUrl = wgServer + wgScript + "?title=" + wgPageName + "&action=ajax&rs=SiteWideMessagesAjaxDismiss&rsargs[]=" + id;
-	var request = $.get(ajaxUrl,function(data){
-		$("#msg_"+id).remove();
-	});
+	var	id = e.data.id,
+		ajaxUrl = wgServer + wgScript + "?title=" + wgPageName + "&action=ajax&rs=SiteWideMessagesAjaxDismiss&rsargs[]=" + id,
+		request = $.get(ajaxUrl,function(data){
+			$("#msg_"+id).remove();
+		}),
+		trackObj = {
+			ga_category: 'sitewidemessages',
+			ga_action: WikiaTracker.ACTIONS.CLICK_LINK_BUTTON,
+			ga_label: 'swm-dismiss',
+			ga_value: id
+		};
+	WikiaTracker.trackEvent(
+		'trackingevent',
+		trackObj,
+		'internal'
+	);
 }
