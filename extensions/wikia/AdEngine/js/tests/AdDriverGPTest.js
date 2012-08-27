f93736b711d89ee4d342ce8342ee84d27798dba1
mech <mech@wikia-inc.com>
2012-08-27 17:54:48 +0000

    simple unit test for AdDriverGP
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59087 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/AdEngine/js/tests/AdDriverGPTest.js
@@ -0,0 +1,25 @@
+/*
+ @test-framework QUnit
+ @test-require-asset resources/wikia/modules/cookies.js
+ @test-require-asset extensions/wikia/AdEngine/AdDriverGP.js
+
+ */
+module('AdDriverGP');
+
+test('slots', function() {
+	window.gpslots = [ [ "a", 1 ], [ "b", 2 ] ];
+
+	var pushedSlots = [];
+
+	AdDriverGP.fillInSlot = function(s) {
+		pushedSlots.push(s);
+	}
+
+	AdDriverGP.init();
+
+	equal( pushedSlots.length, 2 );
+
+	window.gpslots.push( [ "c", 3 ] );
+
+	equal( pushedSlots.length, 3 );
+});
