28019d77bd308e0addd58f26495654397917ca9c
saipetch <saipetch@wikia-inc.com>
2012-08-27 15:10:34 -0700

    Video (bugid 45614): get embed url for sevenload and screenplay.

--- extensions/wikia/VideoHandlers/handlers/ScreenplayVideoHandler.class.php
+++ extensions/wikia/VideoHandlers/handlers/ScreenplayVideoHandler.class.php
@@ -13,23 +13,11 @@ class ScreenplayVideoHandler extends VideoHandler {
 
 	public function getEmbed($articleId, $width, $autoplay=false, $isAjax=false, $postOnload=false) {
 		$height =  $this->getHeight( $width );
-		$metadata = $this->getMetadata(true);
-		if(isset($metadata['streamUrl'])) {
-			$file = $metadata['streamUrl'];
-		} else {
-			$file = $this->getFileUrl(ScreenplayApiWrapper::VIDEO_TYPE, $this->getStandardBitrateCode());
-		}
-
-		if(isset($metadata['streamHdUrl'])) {
-			$hdfile = $metadata['streamHdUrl'];
-		} else {
-			$hdfile = $this->getFileUrl(ScreenplayApiWrapper::VIDEO_TYPE, ScreenplayApiWrapper::HIGHDEF_BITRATE_ID);
-		}
 
 		$app = F::app();
 
 		if ( $app->checkSkin( 'wikiamobile' ) ) {
-			$url = ( ( $this->isHd() ) ? $hdfile : $file );
+			$url = $this->getEmbedUrl();
 			$fileObj = $app->wf->FindFile( $this->getTitle() );
 			$poster = '';
 
@@ -40,6 +28,10 @@ class ScreenplayVideoHandler extends VideoHandler {
 
 			$result = '<video controls width="' . $width . '" height="' . $height . '"' . $poster . '><source src="' . $url . '">' . $app->wf->Msg( 'wikiamobile-unsupported-video-download', $url ) . '</video>';
 		} else {
+			$metadata = $this->getMetadata( true );
+			$file = $this->getStreamUrl( $metadata );
+			$hdfile = $this->getStreamHdUrl( $metadata );
+
 			$jwplayer = new JWPlayer($this->getVideoId());
 			$jwplayer->setArticleId($articleId);
 			$jwplayer->setUrl($file);
@@ -70,6 +62,35 @@ class ScreenplayVideoHandler extends VideoHandler {
 
 		$urlCommonPart = self::$urlTemplate . http_build_query($fileParams);
 		return $urlCommonPart . '&type=' . $type . '&bitrateid=' . $bitrateid;
+	}
+
+	protected function getStreamUrl( $metadata ) {
+		if ( isset($metadata['streamUrl']) ) {
+			$file = $metadata['streamUrl'];
+		} else {
+			$file = $this->getFileUrl( ScreenplayApiWrapper::VIDEO_TYPE, $this->getStandardBitrateCode() );
+		}
+
+		return $file;
+	}
+
+	protected function getStreamHdUrl( $metadata ) {
+		if ( isset($metadata['streamHdUrl']) ) {
+			$hdfile = $metadata['streamHdUrl'];
+		} else {
+			$hdfile = $this->getFileUrl( ScreenplayApiWrapper::VIDEO_TYPE, ScreenplayApiWrapper::HIGHDEF_BITRATE_ID );
+		}
+
+		return $hdfile;
+	}
+
+	public function getEmbedUrl() {
+		$metadata = $this->getMetadata( true );
+		$file = $this->getStreamUrl( $metadata );
+		$hdfile = $this->getStreamHdUrl( $metadata );
+		$url = ( $this->isHd() ) ? $hdfile : $file ;
+
+		return $url;
 	}
 
 	protected function getStandardBitrateCode() {
--- extensions/wikia/VideoHandlers/handlers/SevenloadVideoHandler.class.php
+++ extensions/wikia/VideoHandlers/handlers/SevenloadVideoHandler.class.php
@@ -2,25 +2,24 @@
 
 class SevenloadVideoHandler extends VideoHandler {
 	protected $apiName = 'SevenloadApiWrapper';
-	protected static $urlTemplate = 'http://en.sevenload.com/pl/$1/$2x$3/swf$4';
+	protected static $urlTemplate = 'http://en.sevenload.com/pl/$1';
 	protected static $providerDetailUrlTemplate = 'http://sevenload.com/videos/$1';
 	protected static $providerHomeUrl = 'http://sevenload.com/';
 	
 	public function getEmbed($articleId, $width, $autoplay = false, $isAjax = false, $postOnload=false) {
 		$height = $this->getHeight($width);
-		$url = str_replace('$1', $this->getEmbedVideoId(), static::$urlTemplate);
-		$url = str_replace('$2', $width, $url);
-		$url = str_replace('$3', $height, $url);
-		$url = str_replace('$4', $autoplay ? '/play' : '', $url);
+		$autoplayStr = ( $autoplay ) ? '/play' : '' ;
+		$url = $this->getEmbedUrl();
+		$url .= '/'.$width.'x'.$height.'/swf'.$autoplayStr;
 
 		$html = <<<EOT
 <object type="application/x-shockwave-flash" data="$url" width="$width" height="$height">
 	<param name="allowFullscreen" value="true" />
 	<param name="allowScriptAccess" value="always" />
 	<param name="movie" value="$url" />
-</object>       
+</object>
 EOT;
-		
+
 		return $html;
 	}
 	
