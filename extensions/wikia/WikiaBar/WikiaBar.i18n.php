4ee6097ff1b16aeca571a34a6d145b537fbce0f0
marzjan <sebastianm@wikia-inc.com>
2012-08-29 10:32:28 +0000

    adding todo
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59213 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaBar/WikiaBar.i18n.php
+++ extensions/wikia/WikiaBar/WikiaBar.i18n.php
@@ -1,8 +1,3 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: S
- * Date: 28.08.12
- * Time: 14:45
- * To change this template use File | Settings | File Templates.
- */
+
+// todo: insert local failsafe here
