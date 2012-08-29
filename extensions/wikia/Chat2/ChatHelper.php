4d36a6776f4718d122bb79e67ada0f8c79f10a51
sean <sean@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-29 18:17:15 +0000

    Only include ChatEntryPoint resources in Oasis. This is probably not a permanent fix because we probably want the EntryPoint to work in Monobook (since users can include it as a tag). BugzId 45863.
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59244 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat2/ChatHelper.php
+++ extensions/wikia/Chat2/ChatHelper.php
@@ -168,11 +168,13 @@ class ChatHelper {
 			}
 		}
 
-		// TODO: move these to asset manager when we release chat globally
-		$out->addScriptFile($wgResourceBasePath . '/resources/wikia/libraries/bootstrap/popover.js');
-		$out->addScriptFile($wgExtensionsPath . '/wikia/Chat2/js/ChatEntryPoint.js');
-		$out->addStyle(AssetsManager::getInstance()->getSassCommonURL('resources/wikia/libraries/bootstrap/popover.scss'));
-		$out->addStyle(AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/Chat2/css/ChatEntryPoint.scss'));
+		if ( F::app()->checkSkin( 'oasis' ) ){
+			// TODO: move these to asset manager when we release chat globally
+			$out->addScriptFile($wgResourceBasePath . '/resources/wikia/libraries/bootstrap/popover.js');
+			$out->addScriptFile($wgExtensionsPath . '/wikia/Chat2/js/ChatEntryPoint.js');
+			$out->addStyle(AssetsManager::getInstance()->getSassCommonURL('resources/wikia/libraries/bootstrap/popover.scss'));
+			$out->addStyle(AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/Chat2/css/ChatEntryPoint.scss'));
+		}
 
 		wfProfileOut(__METHOD__);
 		return true;
