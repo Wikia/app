a21553907a0eefebbbc3cd9e8a878e59a1d6e482
karol <karol@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-29 07:58:58 +0000

    driver as parameter in logOut method (experimental) htmlUnit added staff credentials from xml file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59197 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/Common/Properties/Properties.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/Common/Properties/Properties.java
@@ -58,8 +58,8 @@ public class Properties {
 		userNameLong = XMLFunctions.getXMLConfiguration(Global.CONFIG_FILE, "ci.user.long.username");
 		passwordLong = XMLFunctions.getXMLConfiguration(Global.CONFIG_FILE, "ci.user.long.password");
 		
-		userNameStaff = "";
-		passwordStaff = "";
+		userNameStaff = XMLFunctions.getXMLConfiguration(Global.CONFIG_FILE, "ci.user.wikiastaff.username");
+		passwordStaff = XMLFunctions.getXMLConfiguration(Global.CONFIG_FILE, "ci.user.wikiastaff.password");
 		
 		email = XMLFunctions.getXMLConfiguration(Global.CONFIG_FILE, "ci.email.generic.username");
 		emailPassword = XMLFunctions.getXMLConfiguration(Global.CONFIG_FILE, "ci.email.generic.password");
