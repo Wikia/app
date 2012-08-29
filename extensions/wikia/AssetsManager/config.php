d830fe8282096c064ba6f42d8ee31d55c5c18505
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-29 21:49:29 +0000

    ad provider gamepro
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59255 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -58,6 +58,7 @@ $config['oasis_ads_js'] = array(
 
 		'//extensions/wikia/AdEngine/liftium/AdsInContent.js',
 		'//extensions/wikia/AdEngine/AdProviderAdEngine2.js',
+		'//extensions/wikia/AdEngine/AdProviderGamePro.js',
 		'//extensions/wikia/AdEngine/AdProviderEvolve.js',
 		'//extensions/wikia/AdEngine/AdProviderAdDriver2.js',
 		'//extensions/wikia/AdEngine/AdEngine2.js',
