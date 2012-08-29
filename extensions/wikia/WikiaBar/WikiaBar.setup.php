932958eeeea2e026906328346dd47b2fbf6f7005
marcin <marcin@wikia-inc.com>
2012-08-29 10:19:55 +0000

    Define assets (bugid:45677)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59209 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaBar/WikiaBar.setup.php
+++ extensions/wikia/WikiaBar/WikiaBar.setup.php
@@ -7,6 +7,21 @@
  * @author Sebastian Marzjan
  */
 
+$app = F::app();
+$dir = dirname(__FILE__) . '/';
 
-// hooks
-$wgHooks['MessageCacheReplace'][] = 'WikiaBarModel::onMessageCacheReplace';
+/**
+ * classes
+ */
+$app->registerClass('WikiaBarController', $dir . 'WikiaBarController.class.php');
+$app->registerClass('WikiaBarHooks', $dir . 'WikiaBarHooks.class.php');
+$app->registerClass('WikiaBarModel', $dir . 'models/WikiaBarModel.class.php');
+$app->registerClass('WikiaBarModelBase', $dir . 'models/WikiaBarModelBase.class.php');
+$app->registerClass('WikiaBarDataModel', $dir . 'models/WikiaBarDataModel.class.php');
+$app->registerClass('WikiaBarDataFailsafeModel', $dir . 'models/WikiaBarDataFailsafeModel.class.php');
+
+/**
+ * hooks
+ */
+$app->registerHook('WikiaAssetsPackages', 'WikiaBarHooks', 'onWikiaAssetsPackages');
+$app->registerHook('MessageCacheReplace', 'WikiaBarHooks', 'onMessageCacheReplace');
