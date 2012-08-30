e907464a84e1bda7b0f87d335413cc154b5b0b2f
nandy <nandy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-30 17:35:57 +0000

    changes after code review
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59348 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialManageWikiaHome/ManageWikiaHome.i18n.php
+++ extensions/wikia/SpecialManageWikiaHome/ManageWikiaHome.i18n.php
@@ -9,7 +9,6 @@ $messages = array();
 
 $messages['en'] = array(
 	'managewikiahome' => 'Manage Wikia Home',
-	'manage-wikia-home-title' => 'Manage Wikia Home',
 	'manage-wikia-home-wrong-rights' => "You are probably logged-out or you don't have permissions to use this special page.",
 
 	'manage-wikia-home-wikis-in-slots-heading' => 'Slots setup',
