fbb762f773953624197d15e3774e90475532d683
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-08-31 10:23:38 +0000

    wikia log for ad logging
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59372 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdProviderAdEngine2.js
+++ extensions/wikia/AdEngine/AdProviderAdEngine2.js
@@ -1,30 +1,24 @@
 var AdProviderAdEngine2 = my.Class({
 	// core stuff, should be overwritten
-	name:'AdProviderAdEngine2 (error, this should be overwritten)',
+	name:'AdProviderAdEngine2',
 
 	fillInSlot:function (slot) {
-		this.log('fillInSlot', slot);
+		this.log('fillInSlot', 5, slot);
 
 		WikiaTracker.trackAdEvent('liftium.slot2', {'ga_category':'slot2/' + slot[1], 'ga_action':slot[0], 'ga_label':'adengine2'}, 'ga');
 
-		this.log('error, this should be overwritten');
+		this.log('error, AdProviderAdEngine2.fillInSlot method should be overwritten', 1);
 	},
 
 	// private stuff
 	// none
 
 	// helpers, should be inherited
-	debug:true,
+	log:function (msg, level, obj) {
+		Wikia.log(msg, level, this.name);
 
-	log:function (msg, obj) {
-		if (typeof console == 'undefined') return;
-
-		// FIXME WikiaLogger...
-		if (!this.debug) return;
-
-		console.log(this.name + ': ' + msg);
 		if (typeof obj != 'undefined') {
-			console.log(obj);
+			Wikia.log(obj, level, this.name);
 		}
 	}
 });
