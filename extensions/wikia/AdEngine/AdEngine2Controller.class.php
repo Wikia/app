aaadf8f07f1d0e31fa59da6e85af5b978aa46826
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-04 13:02:36 +0000

    global adslots var 1.16 versus 1.19
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59541 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdEngine2Controller.class.php
+++ extensions/wikia/AdEngine/AdEngine2Controller.class.php
@@ -12,7 +12,30 @@ class AdEngine2Controller extends WikiaController {
 	 * @return bool
 	 */
 	public function onWikiaSkinTopScripts(&$vars) {
+		wfProfileIn(__METHOD__);
+
 		$vars['adslots2'] = array();
+
+		wfProfileOut(__METHOD__);
+
+		return true;
+	}
+
+	/**
+	 * register ad-related vars for 1.16
+	 *
+	 * @param $vars
+	 * @return bool
+	 */
+	public function onMakeGlobalVariableScript(&$vars) {
+		wfProfileIn(__METHOD__);
+
+		global $wgVersion;
+		if (version_compare( $wgVersion, '1.16.5', '>' )) return true;
+
+		$vars['adslots2'] = array();
+
+		wfProfileOut(__METHOD__);
 
 		return true;
 	}
