136cd3aa23e4c062e37b4ddec07d21a42770b113
macbre <macbre@wikia-inc.com>
2012-09-04 14:36:37 +0000

    Places: improve structured data markup
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59556 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Places/templates/Places_placeFromModel.php
+++ extensions/wikia/Places/templates/Places_placeFromModel.php
@@ -7,10 +7,10 @@
 	} else {
 		// render map for view mode
 ?>
-<figure class="thumb t<?=$align;?> thumbinner placemap" style="width:<?=$width+2;?>px;" >
+<figure class="thumb t<?=$align;?> thumbinner placemap" style="width:<?=$width+2;?>px;" itemscope itemtype="http://schema.org/Place">
 	<img class="thumbimage" src="<?=$url;?>" width="<?=$width;?>" height="<?=$height;?>" data-categories="<?=$categories?>" data-zoom="<?=$zoom;?>" data-lat="<?=$lat;?>" data-lon="<?=$lon;?>" />
 	<figcaption class="thumbcaption">
-		<span itemprop="geo" itemscope itemtype="http://data-vocabulary.org/Geo"><?= htmlspecialchars($caption) ?>
+		<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates"><?= htmlspecialchars($caption) ?>
 			<meta itemprop="latitude" content="<?= $lat ?>">
 			<meta itemprop="longitude" content="<?= $lon ?>">
 		</span>
