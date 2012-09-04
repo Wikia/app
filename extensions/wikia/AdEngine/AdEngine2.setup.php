c894eb5c213de77abc3bdc4282a2e387c1f6a8f8
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-04 16:42:13 +0000

    macbre hates 1.16
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59579 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdEngine2.setup.php
+++ extensions/wikia/AdEngine/AdEngine2.setup.php
@@ -7,7 +7,3 @@ $app = F::app();
 $app->registerClass('AdEngine2Controller', "$dir/AdEngine2Controller.class.php");
 
 $app->registerHook('WikiaSkinTopScripts', 'AdEngine2Controller', 'onWikiaSkinTopScripts');
-
-if (version_compare( $wgVersion, '1.19', '<' )) {
-	$app->registerHook('MakeGlobalVariablesScript', 'AdEngine2Controller', 'onMakeGlobalVariableScript');
-}
