275d74ff994d28f48c63f83d961e7d9a766be4d7
marcin <marcin@wikia-inc.com>
2012-09-04 23:03:16 +0000

    Templating and CSS (bugid:45362)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59593 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaBar/templates/WikiaBar_Index.php
+++ extensions/wikia/WikiaBar/templates/WikiaBar_Index.php
@@ -1,7 +1,13 @@
 <div class="WikiaBarWrapper">
 	<div class="wikia-bar">
 		<div class="ad"></div>
-		<div class="buttons"></div>
+		<ul class="buttons">
+			<li>temp_button_1</li>
+			<li>temp_button_2</li>
+			<li>temp_button_3</li>
+			<li>temp_button_4</li>
+			<li>temp_button_5</li>
+		</ul>
 		<div class="messages"></div>
 	</div>
 </div>
