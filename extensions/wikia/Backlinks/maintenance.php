2fd6d2840348c15c4499b8195affe249ba68f6e5
robert <robert@wikia-inc.com>
2012-09-06 17:55:45 +0000

    fixed backlinks maintenance script to run regardless of wgEnableBacklinksExt setting
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59710 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Backlinks/maintenance.php
+++ extensions/wikia/Backlinks/maintenance.php
@@ -8,4 +8,9 @@
 ini_set( "include_path", dirname(__FILE__)."/../../../maintenance/" );
 require_once( "commandLine.inc" );
 
+global $wgEnableBacklinksExt;
+$wgEnableBacklinksExt = true;
+
+include("$IP/extensions/wikia/Backlinks/Backlinks.setup.php");
+
 echo Backlinks::initTable()."\n";
