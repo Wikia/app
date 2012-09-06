9a8e921f5423d9e76b17fb4b45c8285b234176d3
marcin <marcin@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-06 21:59:48 +0000

    Templating and CSS (bugid:45362)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59741 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WikiaBar/js/WikiaBar.js
@@ -0,0 +1,15 @@
+var WikiaBar = {
+	init: function() {
+		return true;
+	},
+	getAd: function() {
+		return true;
+	},
+	collapse: function() {
+		return true;
+	}
+};
+
+$(function() {
+	WikiaBar.init();
+});
