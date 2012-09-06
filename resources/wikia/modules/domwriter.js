df2981d4f7db7fee956de53382d40e1cd474bea7
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-06 20:32:17 +0000

    DOM Writer reworked
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59723 e846bd91-c042-42a7-a218-4b65e2c1904d

--- resources/wikia/modules/domwriter.js
+++ resources/wikia/modules/domwriter.js
@@ -1,6 +1,72 @@
 /**
  * DOM Writer: binds document.write calls to a DOM element to preserve the
- * page after DOMReady
+ * content of the page after DOMReady has fired
  *
  * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
  */
+
+/*global define, document*/
+(function (context) {
+	'use strict';
+
+	function domwriter() {
+		function TargetException(element) {
+			this.message = "Target is not a DOM element:" + element;
+		}
+
+		TargetException.prototype.toString = function () {
+			return this.message;
+		};
+
+		/**
+		 * @public
+		 * @param {DOMElement} element The DOM element to bind all the content to write to
+		 * @return {Bool} true for success, false for failure
+		 */
+		function target(element) {
+			if (typeof element === 'object' && typeof element.innerHTML === 'string') {
+				var doc = element.ownerDocument;
+
+				//backup native implementations
+				doc.backupWrite = doc.write;
+				doc.backupWriteLn = doc.writeln;
+
+				doc.write = function (text) {
+					element.innerHTML += text;
+				};
+
+				doc.writeln = function (text) {
+					this.write(text + "\n");
+				};
+			} else {
+				throw new TargetException();
+			}
+		}
+
+		/**
+		 * @public
+		 */
+		function reset() {
+			//restore native implementations
+			doc.write = doc.backupWrite;
+			doc.writeln = doc.backupWriteLn;
+		}
+
+		return {
+			target: target,
+			reset: reset
+		};
+	}
+
+	if (typeof define !== 'undefined' && define.amd) {
+		//AMD
+		define('domwriter', domwriter);
+	} else {
+		//Namespace
+		if (!context.Wikia) {
+			context.Wikia = {};
+		}
+
+		context.Wikia.DOMWriter = domwriter();
+	}
+}(this));
