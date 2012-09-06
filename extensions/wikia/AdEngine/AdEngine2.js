527da114abf32d81f90c459d468b49ebfd53640e
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-06 21:50:01 +0000

    ad engine II refactoring
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59739 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdEngine2.js
+++ extensions/wikia/AdEngine/AdEngine2.js
@@ -1,44 +1,47 @@
-var AdEngine2 = {
-	log:function (msg, level, obj) {
+window.AdEngine2 = window.AdEngine2 || (function (Wikia, AdConfig2, window) {
+	function log(msg, level, obj) {
 		Wikia.log(msg, level, 'AdEngine2');
 
 		if (typeof obj != 'undefined') {
 			Wikia.log(obj, level, 'AdEngine2');
 		}
-	},
+	}
 
-	init:function () {
-		this.log('init', 5);
+	function init() {
+		log('init', 5);
 
-		this.moveQueue();
-	},
+		moveQueue();
+	}
 
-	fillInSlot:function (slot) {
-		this.log('fillInSlot', 5, slot);
+	function fillInSlot(slot) {
+		log('fillInSlot', 5, slot);
 
 		var provider = AdConfig2.getProvider(slot);
-		this.log('calling ' + provider + ' for ' + slot[0], 3);
+		log('calling ' + provider + ' for ' + slot[0], 3);
 		slot[2] = provider;
 		window['adProvider' + provider].fillInSlot(slot);
-	},
+	}
 
 	// based on WikiaTrackerQueue by macbre
-	moveQueue:function () {
-		this.log('moveQueue', 5);
+	function moveQueue() {
+		log('moveQueue', 5);
 
 		var slots = window.adslots2 || [], slot;
-		this.log('queue', 7, slots);
+		log('queue', 7, slots);
 		while ((slot = slots.shift()) !== undefined) {
-			this.fillInSlot(slot);
+			fillInSlot(slot);
 		}
 
-		slots.push = this.proxy(this.fillInSlot, this);
-		this.log('queue moved', 6);
-	},
+		slots.push = proxy(fillInSlot, this);
+		log('queue moved', 6);
+	}
 
-	proxy:function (fn, scope) {
+	function proxy(fn, scope) {
 		return function () {
 			return fn.apply(scope, arguments);
 		}
 	}
-};
+
+	return {init: init};
+
+}(Wikia, AdConfig2, window));
--- extensions/wikia/AdEngine/js/tests/AdEngine2Test.js
+++ extensions/wikia/AdEngine/js/tests/AdEngine2Test.js
@@ -3,6 +3,7 @@
  @test-require-asset resources/wikia/modules/querystring.js
  @test-require-asset resources/wikia/modules/cookies.js
  @test-require-asset resources/wikia/modules/log.js
+ @test-require-asset extensions/wikia/AdEngine/js/tests/AdConfig2.js
  @test-require-asset extensions/wikia/AdEngine/AdEngine2.js
 */
 module('AdEngine2');
@@ -10,7 +11,8 @@ module('AdEngine2');
 test('moveQueue', function() {
 	// setup
 	var slots_done = [];
-	AdEngine2.fillInSlot = function(slot) {
+	adProviderDummy = {};
+	adProviderDummy.fillInSlot = function(slot) {
 		slots_done.push(slot);
 	};
 
@@ -24,4 +26,40 @@ test('moveQueue', function() {
 	window.adslots2.push(['baz']);
 
 	equal(slots_done.length, 3, 'post move');
+});
+
+test('moveQueue empty', function() {
+	// setup
+	var slots_done = [];
+	window.adProviderDummy = {};
+	window.adProviderDummy.fillInSlot = function(slot) {
+		slots_done.push(slot);
+	};
+
+	window.adslots2 = [];
+	window.AdEngine2.init();
+
+	equal(slots_done.length, 0, 'pre move');
+
+	window.adslots2.push(['baz']);
+
+	equal(slots_done.length, 1, 'post move');
+});
+
+test('moveQueue null', function() {
+	// setup
+	var slots_done = [];
+	window.adProviderDummy = {};
+	window.adProviderDummy.fillInSlot = function(slot) {
+		slots_done.push(slot);
+	};
+
+	//window.adslots2 = [];
+	window.AdEngine2.init();
+
+	equal(slots_done.length, 0, 'pre move');
+
+	window.adslots2.push(['baz']);
+
+	equal(slots_done.length, 1, 'post move');
 });
