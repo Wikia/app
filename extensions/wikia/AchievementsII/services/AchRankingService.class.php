727a5bd74fea3fc713fb592db6c560351c8f528d
kflorence <kflorence@wikia-inc.com>
2012-09-07 21:51:38 +0000

    AchievementsII: fix undefined property error (BugId:45280).
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59782 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AchievementsII/services/AchRankingService.class.php
+++ extensions/wikia/AchievementsII/services/AchRankingService.class.php
@@ -2,46 +2,46 @@
 
 class AchRankingService {
 	private $mRecentAwardedUsers;
-	
+
 	function __construct() {
 		$this->mRecentAwardedUsers = null;
 	}
-	
+
 	static public function getRankingCacheKey($limit = null, $compareToSnapshot = null){
 		$params = array( 'Achievements', 'Ranking', 'default' );
-		
+
 		if ( !empty( $limit ) ) {
 			$params[] = "limit{$limit}";
 		}
-		
+
 		if ( $compareToSnapshot == true ) {
 			$params[] = "compareToSnapshot";
 		}
-		
+
 		return call_user_func_array( 'wfMemcKey', $params );
 	}
-	
+
 	public function getUsersRanking($limit = 1000, $compareToSnapshot = false) {
 		wfProfileIn(__METHOD__);
-		
+
 		global $wgMemc;
 		$cacheKey = self::getRankingCacheKey( $limit, $compareToSnapshot );
 		$ranking = $wgMemc->get( $cacheKey );
-		
+
 		if( empty( $ranking ) ) {
 			global $wgCityId, $wgWikiaBotLikeUsers, $wgExternalSharedDB;
 			global $wgEnableAchievementsStoreLocalData;
 
 			$ranking = array();
 			$rules = array('ORDER BY' => 'score desc');
-	
+
 			if($limit > 0)
 				$rules['LIMIT'] = $limit * 2;//bots and blocked users are filtered after the query has been run, let's admit that ratio is 2:1
 
 			$where = array();
 			if(empty($wgEnableAchievementsStoreLocalData)) {
 				$dbr = wfGetDB(DB_SLAVE, array(), $wgExternalSharedDB);
-				$where['wiki_id'] = $this->mCityId;
+				$where['wiki_id'] = $wgCityId;
 			} else {
 				$dbr = wfGetDB(DB_SLAVE);
 			}
@@ -52,32 +52,32 @@ class AchRankingService {
 			$counter = 1;
 			$prevScore = -1;
 			$prevPosition = -1;
-			
+
 			while ( $row = $dbr->fetchObject( $res ) ) {
 				$user = User::newFromId($row->user_id);
-					
+
 				if ( $user && AchAwardingService::canEarnBadges( $user ) ) {
 					// If this user has the same score as previous user, give them the same (lower) rank (RT#67874).
 					if ( $prevScore != $row->score ) {
 						$position = $counter;
 					}
-					
+
 					$ranking[] = new AchRankedUser($user, $row->score, $position, ($rankingSnapshot != null && isset($rankingSnapshot[$user->getId()])) ? $rankingSnapshot[$user->getId()] : null);
-					
+
 					$counter++;
 					$prevScore = $row->score;
 				}
-				
+
 				if ( $limit > 0 && $counter == $limit ) break;
 			}
-			
+
 			$dbr->freeResult($res);
-			
+
 			$wgMemc->set($cacheKey, $ranking, 86400 /* 24h */ );
 		}
-		
+
 		wfProfileOut(__METHOD__);
-		
+
 		return $ranking;
 	}
 
@@ -86,21 +86,21 @@ class AchRankingService {
 		global $wgEnableAchievementsStoreLocalData;
 
 		if ( empty( $user_id ) ) return 0;
-		
+
 		$user = User::newFromId( $user_id );
 		$score = false;
-		
+
 		if ( $user && AchAwardingService::canEarnBadges( $user ) ) {
 			$where = array('user_id' => $user_id);
 			if(empty($wgEnableAchievementsStoreLocalData)) {
 				$dbr = wfGetDB(DB_SLAVE, array(), $wgExternalSharedDB);
-				$where['wiki_id'] = $this->mCityId;
+				$where['wiki_id'] = $wgCityId;
 			} else {
 				$dbr = wfGetDB(DB_SLAVE);
 			}
 			$score = $dbr->selectField('ach_user_score', 'score', $where, __METHOD__);
 		}
-		
+
 		// if no score found return zero
 		return $score ? $score : 0;
 	}
@@ -133,7 +133,7 @@ class AchRankingService {
 
 	function loadFromSnapshot() {
 	    global $wgCityId;
-	    
+
 	    $dbr = WikiFactory::db( DB_SLAVE );
 
 	    $res = $dbr->select('ach_ranking_snapshots', array('data'), array('wiki_id' => $wgCityId));
@@ -173,15 +173,15 @@ class AchRankingService {
 
 		if($daySpan != null)
 			$conds[] = "date >= (CURDATE() - INTERVAL {$daySpan} DAY)";
-		
+
 		if(is_array($blackList))
 			$conds[] = 'badge_type_id NOT IN (' . implode($blackList) . ')';
-		
+
 		if($limit != null)
 			$rules['LIMIT'] = $limit * 2; //bots and blocked users are filtered after the query hs been run, let's admit that ratio is 2:1
-		
+
 		$res = $dbr->select('ach_user_badges', 'user_id, badge_type_id, badge_lap, badge_level, date', $conds, __METHOD__, $rules);
-		
+
 		while(($row = $dbr->fetchObject($res)) && (count($badges) <= $limit)) {
 			$user = User::newFromId($row->user_id);
 
