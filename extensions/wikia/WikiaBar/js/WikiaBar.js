b865c30b47c43f3a58e9221345baf0af7ff23be5
nandy <nandy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-07 00:34:55 +0000

    bugId:45363; loading an ad; WIP;
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59750 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaBar/js/WikiaBar.js
+++ extensions/wikia/WikiaBar/js/WikiaBar.js
@@ -1,14 +1,51 @@
 var WikiaBar = {
+	//WEEBO_BOXAD_NAME: 'WEEBO_BOXAD_1',
+	WEEBO_BOXAD_NAME: 'INCONTENT_BOXAD_1',
+	WEEBO_BOXAD_ANIMATION_TIME: 1000,
+
 	init: function() {
 		$('.WikiaBarWrapper .arrow').click(function (e) {
 			$('.WikiaBarWrapper').addClass('hidden');
 			e.preventDefault();
 		});
+		/*
+		$(this.WEEBO_BOXAD_NAME + '_iframe').ready($.proxy(function() {
+			this.animateWeeboBoxAd();
+		}, this));
+		*/
+		this.getAd();
 		return true;
 	},
+
 	getAd: function() {
+		var weeboBoxAd = $(this.WEEBO_BOXAD_NAME);
+		if( weeboBoxAd.hasClass('wikia-ad') == false ) {
+			LiftiumOptions.placement = this.WEEBO_BOXAD_NAME;
+			Liftium.callInjectedIframeAd("300x250", document.getElementById(this.WEEBO_BOXAD_NAME + "_iframe"));
+			weeboBoxAd.addClass('wikia-ad');
+
+		}
 		return true;
 	},
+
+	animateWeeboBoxAd: function() {
+		$().log('=========================');
+		$().log('=========================');
+
+		var weeboBoxAd = $(this.WEEBO_BOXAD_NAME);
+		var weeboBoxAdHeight = weeboBoxAd.height();
+
+		$().log('Before animation...');
+		$().log('weeboBoxAd: ' + weeboBoxAd);
+		$().log('weeboBoxAdHeight: ' + weeboBoxAdHeight);
+
+		weeboBoxAd.animate({top: '-' + weeboBoxAdHeight + 'px'}, this.WEEBO_BOXAD_ANIMATION_TIME);
+
+		$().log('After animation...');
+		$().log('=========================');
+		$().log('=========================');
+	},
+
 	collapse: function() {
 		return true;
 	}
--- extensions/wikia/WikiaBar/templates/WikiaBar_Index.php
+++ extensions/wikia/WikiaBar/templates/WikiaBar_Index.php
@@ -1,6 +1,8 @@
 <div class="WikiaBarWrapper">
 	<div class="wikia-bar">
-		<div class="ad">Ad slot</div>
+		<div class="ad">
+			<div id="INCONTENT_BOXAD_1" class="noprint" style="height: 250px; width: 300px; position: relative;"><div id="Liftium_300x250_99"><iframe width="300" height="250" id="INCONTENT_BOXAD_1_iframe" class="" noresize="true" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" style="border:none" target="_blank"></iframe></div></div>
+		</div>
 		<a href="#" class="arrow"></a>
 		<div class="message">message</div>
 			<a class="wikiabar-button" href="#">
