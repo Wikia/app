5c8711b89f290fa345f81ae03f2226c81e494226
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-10 12:35:40 +0000

    ad test fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59826 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/js/tests/AdProviderGameProTest.js
+++ extensions/wikia/AdEngine/js/tests/AdProviderGameProTest.js
@@ -1,16 +1,14 @@
 /*
 @test-framework QUnit
-@test-require-asset resources/wikia/libraries/my.class/my.class.js
  @test-require-asset resources/wikia/modules/querystring.js
  @test-require-asset resources/wikia/modules/cookies.js
  @test-require-asset resources/wikia/modules/log.js
- @test-require-asset extensions/wikia/AdEngine/AdProviderAdEngine2.js
+ @test-require-asset extensions/wikia/WikiaTracker/js/WikiaTracker.js
+ @test-require-asset extensions/wikia/AdEngine/ghost/gw-11.6.7/lib/gw.js
 @test-require-asset extensions/wikia/AdEngine/AdProviderGamePro.js
 */
 module('AdProviderGamePro');
 
 test('rebuildKV', function() {
-	var ap = new AdProviderGamePro;
-
-    equal(ap.rebuildKV('egnre=action;egnre=adventure;egnre=drama;egnre=scifi;media=tv'), 'egnre=action,adventure,drama,scifi;media=tv', 'egnre=action;egnre=adventure;egnre=drama;egnre=scifi;media=tv');
+    equal(AdProviderGamePro.rebuildKV('egnre=action;egnre=adventure;egnre=drama;egnre=scifi;media=tv'), 'egnre=action,adventure,drama,scifi;media=tv', 'egnre=action;egnre=adventure;egnre=drama;egnre=scifi;media=tv');
 });
