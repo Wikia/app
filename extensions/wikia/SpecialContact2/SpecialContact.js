6b3a5d6b3b8377aa4c00f3f9ad7ab8500ab65929
grunny <grunny@wikia-inc.com>
2012-09-10 15:37:19 +0000

    (BugID: 47234) Special:Contact throwing JS error
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59846 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SpecialContact2/SpecialContact.js
+++ extensions/wikia/SpecialContact2/SpecialContact.js
@@ -1,28 +1,49 @@
 var SpecialContact = {
 	init: function() {
-		$('.specialcontact-seclink-content-issue').trackClick('wiki/SpecialContact/content-issue');
-		$('.specialcontact-seclink-user-conflict').trackClick('wiki/SpecialContact/user-conflict');
-		$('.specialcontact-seclink-adoption').trackClick('wiki/SpecialContact/adoption');
-		$('.specialcontact-seclink-account-issue').trackClick('wiki/SpecialContact/account-issue');
-		$('.specialcontact-seclink-close-account').trackClick('wiki/SpecialContact/close-account');
-		$('.specialcontact-seclink-rename-account').trackClick('wiki/SpecialContact/rename-account');
-		$('.specialcontact-seclink-blocked').trackClick('wiki/SpecialContact/blocked');
-		$('.specialcontact-seclink-using-wikia').trackClick('wiki/SpecialContact/using-wikia');
-		$('.specialcontact-seclink-feedback').trackClick('wiki/SpecialContact/feedback');
-		$('.specialcontact-seclink-bug').trackClick('wiki/SpecialContact/bug');
-		$('.specialcontact-seclink-bad-ad').trackClick('wiki/SpecialContact/bad-ad');
-		$('.specialcontact-seclink-wiki-name-change').trackClick('wiki/SpecialContact/wiki-name-change');
-		$('.specialcontact-seclink-design').trackClick('wiki/SpecialContact/design');
-		$('.specialcontact-seclink-features').trackClick('wiki/SpecialContact/features');
-		$('.specialcontact-seclink-close-wiki').trackClick('wiki/SpecialContact/close-wiki');
-		$('#SpecialContactFooterPicker a').trackClick('wiki/SpecialContact/footer-picker');
-		$('#SpecialContactFooterNoForm a').trackClick('wiki/SpecialContact/footer-noform');
-		$('#SpecialContactIntroNoForm a').trackClick('wiki/SpecialContact/intro-noform');
-		$('#SpecialContactIntroForm a').trackClick('wiki/SpecialContact/intro-form');
+		$( '#picker_grid' ).find( 'td > a' ).click( function () {
+			var	trackLabel = $( this ).attr( 'class' ).substr( 23 ),
+				trackUrl = $( this ).attr( 'href' );
+			SpecialContact.trackClick( trackLabel, trackUrl );
+		} );
+
+		$( '#SpecialContactFooterPicker a' ).click( function () {
+			var	trackLabel = 'footer-picker',
+				trackUrl = $( this ).attr( 'href' );
+			SpecialContact.trackClick( trackLabel, trackUrl );
+		} );
+		$( '#SpecialContactFooterNoForm a' ).click( function () {
+			var	trackLabel = 'footer-noform',
+				trackUrl = $( this ).attr( 'href' );
+			SpecialContact.trackClick( trackLabel, trackUrl );
+		} );
+		$( '#SpecialContactIntroNoForm a' ).click( function () {
+			var	trackLabel = 'intro-noform',
+				trackUrl = $( this ).attr( 'href' );
+			SpecialContact.trackClick( trackLabel, trackUrl );
+		} );
+		$( '#SpecialContactIntroForm a' ).click( function () {
+			var	trackLabel = 'intro-form',
+				trackUrl = $( this ).attr( 'href' );
+			SpecialContact.trackClick( trackLabel, trackUrl );
+		} );
 
 		$('input[type=file]').change(function() {
 			$(this).closest('p').next().show();
 		});
+	},
+
+	trackClick: function ( trackLabel, trackUrl ) {
+		var trackObj = {
+			ga_category: 'specialcontact',
+			ga_action: WikiaTracker.ACTIONS.CLICK_LINK_TEXT,
+			ga_label: trackLabel,
+			href: trackUrl
+		};
+		WikiaTracker.trackEvent(
+			'trackingevent',
+			trackObj,
+			'internal'
+		);
 	}
 };
 
