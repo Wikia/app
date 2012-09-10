75aa6bd6a513917435cdb44809bf813521435dbe
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-10 12:31:03 +0000

    ad test fix
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59825 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/js/tests/AdProviderEvolveTest.js
+++ extensions/wikia/AdEngine/js/tests/AdProviderEvolveTest.js
@@ -1,25 +1,26 @@
 /*
 @test-framework QUnit
- @test-require-asset resources/wikia/libraries/my.class/my.class.js
  @test-require-asset resources/wikia/modules/querystring.js
  @test-require-asset resources/wikia/modules/cookies.js
  @test-require-asset resources/wikia/modules/log.js
- @test-require-asset extensions/wikia/AdEngine/AdProviderAdEngine2.js
+ @test-require-asset extensions/wikia/WikiaTracker/js/WikiaTracker.js
+ @test-require-asset extensions/wikia/AdEngine/ghost/gw-11.6.7/lib/gw.js
 @test-require-asset extensions/wikia/AdEngine/AdProviderEvolve.js
 */
 module('AdProviderEvolve');
 
 test('sanitizeSlotname', function() {
-	var ap = new AdProviderEvolve;
-
-    equal(ap.sanitizeSlotname('foo'), '', 'foo');
-    equal(ap.sanitizeSlotname('TOP_LEADERBOARD'), 'TOP_LEADERBOARD', 'TOP_LEADERBOARD');
-    equal(ap.sanitizeSlotname('=TOP_LEADERBOARD;ord=1'), 'TOP_LEADERBOARD', '=TOP_LEADERBOARD;ord=1');
+    equal(AdProviderEvolve.sanitizeSlotname('foo'), '', 'foo');
+    equal(AdProviderEvolve.sanitizeSlotname('TOP_LEADERBOARD'), 'TOP_LEADERBOARD', 'TOP_LEADERBOARD');
+    equal(AdProviderEvolve.sanitizeSlotname('=TOP_LEADERBOARD;ord=1'), 'TOP_LEADERBOARD', '=TOP_LEADERBOARD;ord=1');
 });
 
 test('getUrl', function() {
-	var ap = new AdProviderEvolve;
-	ap.ord = 1;
+	var expected = 'http://n4403ad.doubleclick.net/adj/gn.wikia4.com/home;sect=home;mtfInline=true;pos=TOP_LEADERBOARD;sz=728x90;dcopt=ist;type=pop;type=int;tile=1;ord=1234567890?';
+	expected = expected.replace(/;ord=[0-9]+\?$/, '');
 
-	equal(ap.getUrl('TOP_LEADERBOARD', '728x90'), 'http://n4403ad.doubleclick.net/adj/gn.wikia4.com/home;sect=home;mtfInline=true;pos=TOP_LEADERBOARD;sz=728x90;dcopt=ist;type=pop;type=int;tile=1;ord=1?', 'TOP_LEADERBOARD');
+	var actual = AdProviderEvolve.getUrl('TOP_LEADERBOARD', '728x90');
+	actual = actual.replace(/;ord=[0-9]+\?$/, '');
+
+	equal(actual, expected, 'TOP_LEADERBOARD');
 });
