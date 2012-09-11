30b6dc99c96d596629a082d61b02ac14eaa8042c
piotrgabryjeluk <rychu@wikia-inc.com>
2012-09-11 14:34:54 +0000

    Moving dependencies of AdConfig2 to AdEngine2.run.js
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59902 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/js/tests/AdConfig2Test.js
+++ extensions/wikia/AdEngine/js/tests/AdConfig2Test.js
@@ -3,68 +3,128 @@
  @test-require-asset resources/wikia/modules/querystring.js
  @test-require-asset resources/wikia/modules/cookies.js
  @test-require-asset resources/wikia/modules/log.js
- @test-require-asset extensions/wikia/AdEngine/ghost/gw-11.6.7/lib/gw.js
- @test-require-asset extensions/wikia/WikiaTracker/js/WikiaTracker.js
- @test-require-asset extensions/wikia/AdEngine/js/AdProviderEvolve.js
- @test-require-asset extensions/wikia/AdEngine/js/AdProviderGamePro.js
- @test-require-asset extensions/wikia/AdEngine/js/AdProviderAdDriver2.js
  @test-require-asset extensions/wikia/AdEngine/js/AdConfig2.js
 */
 module('AdConfig2');
 
 test('getProvider failsafe', function() {
 	// setup
+	var AdProviderGameProMock = {name: 'GameProMock'}
+		, AdProviderEvolveMock = {name: 'EvolveMock'}
+		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
+		, adConfig;
+
 	window.wgContentLanguage = 'not de';
 	window.testUseCountry = 'error';
 
-	equal(AdConfig2.getProvider(['foo']).name, 'AdDriver2', 'AdDriver2');
+	adConfig = AdConfig2(
+		Wikia.log, Wikia, window,
+		// AdProviders
+		AdProviderGameProMock,
+		AdProviderEvolveMock,
+		AdProviderAdDriver2Mock
+	);
+
+	equal(adConfig.getProvider(['foo']).name, 'AdDriver2Mock', 'AdDriver2Mock');
 });
 
 // TODO may be refactored to AdProviderEvolve
 test('getProvider evolve AU', function() {
 	// setup
+	var AdProviderGameProMock = {name: 'GameProMock'}
+		, AdProviderEvolveMock = {name: 'EvolveMock'}
+		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
+		, adConfig;
+
 	window.testUseCountry = 'AU';
 
-	equal(AdConfig2.getProvider(['TOP_LEADERBOARD']).name, 'Evolve', 'TOP_LEADERBOARD');
-	equal(AdConfig2.getProvider(['TOP_RIGHT_BOXAD']).name, 'Evolve', 'TOP_RIGHT_BOXAD');
-	equal(AdConfig2.getProvider(['LEFT_SKYSCRAPER_2']).name, 'Evolve', 'LEFT_SKYSCRAPER_2');
+	adConfig = AdConfig2(
+		Wikia.log, Wikia, window,
+		// AdProviders
+		AdProviderGameProMock,
+		AdProviderEvolveMock,
+		AdProviderAdDriver2Mock
+	);
 
-	notEqual(AdConfig2.getProvider(['INCONTENT_BOXAD_1']).name, 'Evolve', 'INCONTENT_BOXAD_1');
-	notEqual(AdConfig2.getProvider(['PREFOOTER_LEFT_BOXAD']).name, 'Evolve', 'PREFOOTER_LEFT_BOXAD');
-	notEqual(AdConfig2.getProvider(['PREFOOTER_RIGHT_BOXAD']).name, 'Evolve', 'PREFOOTER_RIGHT_BOXAD');
+	equal(adConfig.getProvider(['TOP_LEADERBOARD']).name, 'EvolveMock', 'TOP_LEADERBOARD');
+	equal(adConfig.getProvider(['TOP_RIGHT_BOXAD']).name, 'EvolveMock', 'TOP_RIGHT_BOXAD');
+	equal(adConfig.getProvider(['LEFT_SKYSCRAPER_2']).name, 'EvolveMock', 'LEFT_SKYSCRAPER_2');
+
+	notEqual(adConfig.getProvider(['INCONTENT_BOXAD_1']).name, 'EvolveMock', 'INCONTENT_BOXAD_1');
+	notEqual(adConfig.getProvider(['PREFOOTER_LEFT_BOXAD']).name, 'EvolveMock', 'PREFOOTER_LEFT_BOXAD');
+	notEqual(adConfig.getProvider(['PREFOOTER_RIGHT_BOXAD']).name, 'EvolveMock', 'PREFOOTER_RIGHT_BOXAD');
 });
 
 // TODO may be refactored to AdProviderEvolve
 test('getProvider evolve not AU', function() {
 	// setup
+	var AdProviderGameProMock = {name: 'GameProMock'}
+		, AdProviderEvolveMock = {name: 'EvolveMock'}
+		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
+		, adConfig;
+
 	window.testUseCountry = 'not AU';
 
-	notEqual(AdConfig2.getProvider(['TOP_LEADERBOARD']).name, 'Evolve', 'TOP_LEADERBOARD');
-	notEqual(AdConfig2.getProvider(['TOP_RIGHT_BOXAD']).name, 'Evolve', 'TOP_RIGHT_BOXAD');
-	notEqual(AdConfig2.getProvider(['LEFT_SKYSCRAPER_2']).name, 'Evolve', 'LEFT_SKYSCRAPER_2');
+	adConfig = AdConfig2(
+		Wikia.log, Wikia, window,
+		// AdProviders
+		AdProviderGameProMock,
+		AdProviderEvolveMock,
+		AdProviderAdDriver2Mock
+	);
+
+	notEqual(adConfig.getProvider(['TOP_LEADERBOARD']).name, 'EvolveMock', 'TOP_LEADERBOARD');
+	notEqual(adConfig.getProvider(['TOP_RIGHT_BOXAD']).name, 'EvolveMock', 'TOP_RIGHT_BOXAD');
+	notEqual(adConfig.getProvider(['LEFT_SKYSCRAPER_2']).name, 'EvolveMock', 'LEFT_SKYSCRAPER_2');
 });
 
 // TODO may be refactored to AdProviderGamePro
 test('getProvider GamePro de', function() {
 	// setup
+	var AdProviderGameProMock = {name: 'GameProMock'}
+		, AdProviderEvolveMock = {name: 'EvolveMock'}
+		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
+		, adConfig;
+
 	window.wgContentLanguage = 'de';
 
-	equal(AdConfig2.getProvider(['TOP_LEADERBOARD']).name, 'GamePro', 'TOP_LEADERBOARD');
-	equal(AdConfig2.getProvider(['TOP_RIGHT_BOXAD']).name, 'GamePro', 'TOP_RIGHT_BOXAD');
-	equal(AdConfig2.getProvider(['LEFT_SKYSCRAPER_2']).name, 'GamePro', 'LEFT_SKYSCRAPER_2');
-	equal(AdConfig2.getProvider(['PREFOOTER_LEFT_BOXAD']).name, 'GamePro', 'PREFOOTER_LEFT_BOXAD');
+	adConfig = AdConfig2(
+		Wikia.log, Wikia, window,
+		// AdProviders
+		AdProviderGameProMock,
+		AdProviderEvolveMock,
+		AdProviderAdDriver2Mock
+	);
 
-	notEqual(AdConfig2.getProvider(['INCONTENT_BOXAD_1']).name, 'GamePro', 'INCONTENT_BOXAD_1');
-	notEqual(AdConfig2.getProvider(['PREFOOTER_RIGHT_BOXAD']).name, 'GamePro', 'PREFOOTER_RIGHT_BOXAD');
+	equal(adConfig.getProvider(['TOP_LEADERBOARD']).name, 'GameProMock', 'TOP_LEADERBOARD');
+	equal(adConfig.getProvider(['TOP_RIGHT_BOXAD']).name, 'GameProMock', 'TOP_RIGHT_BOXAD');
+	equal(adConfig.getProvider(['LEFT_SKYSCRAPER_2']).name, 'GameProMock', 'LEFT_SKYSCRAPER_2');
+	equal(adConfig.getProvider(['PREFOOTER_LEFT_BOXAD']).name, 'GameProMock', 'PREFOOTER_LEFT_BOXAD');
+
+	notEqual(adConfig.getProvider(['INCONTENT_BOXAD_1']).name, 'GameProMock', 'INCONTENT_BOXAD_1');
+	notEqual(adConfig.getProvider(['PREFOOTER_RIGHT_BOXAD']).name, 'GameProMock', 'PREFOOTER_RIGHT_BOXAD');
 });
 
 // TODO may be refactored to AdProviderGamePro
 test('getProvider GamePro not de', function() {
 	// setup
+	var AdProviderGameProMock = {name: 'GameProMock'}
+		, AdProviderEvolveMock = {name: 'EvolveMock'}
+		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
+		, adConfig;
+
 	window.wgContentLanguage = 'not de';
 
-	notEqual(AdConfig2.getProvider(['TOP_LEADERBOARD']).name, 'GamePro', 'TOP_LEADERBOARD');
-	notEqual(AdConfig2.getProvider(['TOP_RIGHT_BOXAD']).name, 'GamePro', 'TOP_RIGHT_BOXAD');
-	notEqual(AdConfig2.getProvider(['LEFT_SKYSCRAPER_2']).name, 'GamePro', 'LEFT_SKYSCRAPER_2');
-	notEqual(AdConfig2.getProvider(['PREFOOTER_LEFT_BOXAD']).name, 'GamePro', 'PREFOOTER_LEFT_BOXAD');
+	adConfig = AdConfig2(
+		Wikia.log, Wikia, window,
+		// AdProviders
+		AdProviderGameProMock,
+		AdProviderEvolveMock,
+		AdProviderAdDriver2Mock
+	);
+
+	notEqual(adConfig.getProvider(['TOP_LEADERBOARD']).name, 'GameProMock', 'TOP_LEADERBOARD');
+	notEqual(adConfig.getProvider(['TOP_RIGHT_BOXAD']).name, 'GameProMock', 'TOP_RIGHT_BOXAD');
+	notEqual(adConfig.getProvider(['LEFT_SKYSCRAPER_2']).name, 'GameProMock', 'LEFT_SKYSCRAPER_2');
+	notEqual(adConfig.getProvider(['PREFOOTER_LEFT_BOXAD']).name, 'GameProMock', 'PREFOOTER_LEFT_BOXAD');
 });
