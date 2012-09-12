6031134fd550affef60fe25e463c4c6cda5732e5
liz <liz@wikia-inc.com>
2012-09-12 01:12:55 +0000

    (BugId:44848) adding 301 redirect from Special:Video to Special:Videos

--- extensions/wikia/SpecialVideos/SpecialVideos.setup.php
+++ extensions/wikia/SpecialVideos/SpecialVideos.setup.php
@@ -21,3 +21,13 @@ $app->registerExtensionMessageFile( 'SpecialVideos', $dir.'SpecialVideos.i18n.ph
 
 // special pages
 $app->registerSpecialPage( 'Videos', 'SpecialVideosSpecialController' );
+
+// Fake special page for 301 redirect (Video->Videos)
+$app->registerSpecialPage('Video', 'Video');
+
+// redirects from Video to Videos
+class Video extends SpecialRedirectToSpecial {
+	function __construct() {
+		parent::__construct( 'Video', 'Videos', false, array() );
+	}
+}
