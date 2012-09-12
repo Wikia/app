29f252f7726879760d7d6c3a139aa61b1912acb9
jacek <jacek@wikia-inc.com>
2012-09-12 10:38:21 +0000

    SEO Tweaks - image and video page title
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59965 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/SEOTweaks/SEOTweaksHooksHelper.class.php
+++ extensions/wikia/SEOTweaks/SEOTweaksHooksHelper.class.php
@@ -4,6 +4,7 @@
  * SEOTweaks Hooks Helper
  * @author mech
  * @author ADi
+ * @author Jacek Jursza <jacek at wikia-inc.com>
  */
 
 class SEOTweaksHooksHelper extends WikiaModel {
@@ -32,6 +33,40 @@ class SEOTweaksHooksHelper extends WikiaModel {
 	public function onArticleFromTitle( &$title, &$article ) {
 		if( !$title->exists() && $title->isDeleted() ) {
 			F::app()->wg->Out->setStatusCode( SEOTweaksHooksHelper::DELETED_PAGES_STATUS_CODE );
+		}
+		return true;
+	}
+
+	/**
+	 * change title tag for Video Page and Image Page
+	 * @author Jacek Jursza
+	 * @param ImagePage $imgPage
+	 * @param $html
+	 * @return bool
+	 */
+	function onImagePageAfterImageLinks( $imgPage, $html ) {
+
+		$file = $imgPage->getDisplayedFile(); /* @var $file LocalRepo */
+		$title = $imgPage->getTitle();  /* @var $title Title */
+		$newTitle = '';
+
+		if ( !empty( $file ) && !empty( $title ) ) {
+
+			if ( WikiaFileHelper::isFileTypeVideo( $file ) ) {
+
+				$newTitle = wfMsg('seotweaks-video') . ' - ' . $title->getBaseText();
+			} else {
+
+				// It's not Video so lets check if it is Image
+				if ( $file instanceof LocalFile && $file->getHandler() instanceof BitmapHandler ) {
+
+					$newTitle = wfMsg('seotweaks-image') . ' - ' . $title->getBaseText();
+				}
+			}
+
+			if ( !empty( $newTitle ) ) {
+				$this->wg->out->setPageTitle( $newTitle );
+			}
 		}
 		return true;
 	}
