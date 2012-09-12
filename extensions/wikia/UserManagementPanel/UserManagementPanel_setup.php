a9b7fab3943e71195fa8ef2f2cbb7e3095fb34ea
macbre <macbre@wikia-inc.com>
2012-09-12 14:31:34 +0000

    UserManagmentPanel: add reference to function definition
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@59979 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/UserManagementPanel/UserManagementPanel_setup.php
+++ extensions/wikia/UserManagementPanel/UserManagementPanel_setup.php
@@ -1,41 +1,34 @@
 <?php
- 
- /**
-  * FooAndBar
-  *
-  * A short description of the FooAndBar extention
-  *
-  * @author Author Name <author-email@wikia-inc.com>
-  * @date 2010-01-09
-  * @copyright Copyright (C) 2010 Wikia Inc.
-  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
-  * @package MediaWiki
-  *
-  */
- 
+
  // Extension credits
  $wgExtensionCredits['other'][] = array(
- 	'name' => 'FooAndBar',
+ 	'name' => 'UserManagementPanel',
  	'author' => array( 'Lucas "TOR" Garczewski', 'Michał Roszka' ),
  );
- 
- $dir = dirname(__FILE__);
- 
- // autoloaded classes
+
+$dir = dirname(__FILE__);
+
+// autoloaded classes
 $app->registerClass('SpecialUserManagementController', $dir . '/SpecialUserManagementController.class.php' );
 $app->registerSpecialPage('UserManagement', 'SpecialUserManagementController');
- 
- // i18n
- $wgExtensionMessagesFiles['UserManagementPanel'] = $dir.'/UserManagementPanel.i18n.php';
- 
- // hooks
- $wgHooks['UserPagesHeaderModuleAfterGetTabs'][] = 'efUserManagementPanelAddTab';
+
+// i18n
+$wgExtensionMessagesFiles['UserManagementPanel'] = $dir.'/UserManagementPanel.i18n.php';
+
+// hooks
+$wgHooks['UserPagesHeaderModuleAfterGetTabs'][] = 'efUserManagementPanelAddTab';
 
 // user rights
 $wgAvailableRights[] = 'usermanagement';
 $wgGroupPermissions['util']['usermanagement'] = true;
 
-function efUserManagementPanelAddTab( $tabs, $namespace, $userName ) {
+/**
+ * @param Array $tabs
+ * @param $namespace
+ * @param $userName
+ * @return bool
+ */
+function efUserManagementPanelAddTab( &$tabs, $namespace, $userName ) {
 	global $wgTitle, $wgUser;
 
 	if ( !$wgUser->isAllowed( 'usermanagement' ) ) {
@@ -43,9 +36,9 @@ function efUserManagementPanelAddTab( $tabs, $namespace, $userName ) {
 	}
 
 	$tabs[] = array(
-			'link' => Wikia::link(SpecialPage::getTitleFor('UserManagement/' . $userName ), wfMsg('usermanagement-tab-label')),
-			'selected' => ($wgTitle->isSpecial( 'UserManagement' )),
-			'data-id' => 'usermanagment',
+		'link' => Wikia::link(SpecialPage::getTitleFor('UserManagement/' . $userName ), wfMsg('usermanagement-tab-label')),
+		'selected' => ($wgTitle->isSpecial( 'UserManagement' )),
+		'data-id' => 'usermanagment',
 	);
 
 	return true;
