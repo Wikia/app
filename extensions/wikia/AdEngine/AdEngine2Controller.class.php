100097daa4b9f9844e4af314846d09e6f5a6893c
piotrgabryjeluk <rychu@wikia-inc.com>
2012-09-13 14:59:02 +0000

    Moving hooks to AdEngine2Controller (BugId:46885)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60061 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdEngine2Controller.class.php
+++ extensions/wikia/AdEngine/AdEngine2Controller.class.php
@@ -4,11 +4,14 @@
  * AdEngine II Controller
  */
 class AdEngine2Controller extends WikiaController {
+	const ASSET_GROUP_CORE = 'oasis_shared_core_js';
+	const ASSET_GROUP_ADENGINE = 'adengine2_js';
 
 	/**
-	 * register ad-related vars on top
+	 * Register ad-related vars on top
 	 *
-	 * @param $vars
+	 * @param array $vars
+	 *
 	 * @return bool
 	 */
 	public function onWikiaSkinTopScripts(&$vars) {
@@ -18,6 +21,55 @@ class AdEngine2Controller extends WikiaController {
 
 		wfProfileOut(__METHOD__);
 
+		return true;
+	}
+
+	/**
+	 * Add ad asset to JavaScripts loaded on bottom (with regular JavaScripts)
+	 *
+	 * @param array $jsAssets
+	 *
+	 * @return bool
+	 */
+	public function addNonBlockingAssetGroup(&$jsAssets) {
+		$coreGroupIndex = array_search(self::ASSET_GROUP_CORE, $jsAssets);
+		if ($coreGroupIndex !== false) {
+			// Adding self::ASSET_GROUP_ADENGINE group after self::ASSET_GROUP_CORE
+			array_splice($jsAssets, $coreGroupIndex + 1, 0, self::ASSET_GROUP_ADENGINE);
+		} else {
+			// Do nothing. oasis_shared_core_js must be present for ads to work
+		}
+		return true;
+	}
+
+	/**
+	 * Add ad assets and oasis_shared_core_js to JavaScripts at the top
+	 *
+	 * @param array $jsAssets
+	 *
+	 * @return bool
+	 */
+	public function addBlockingAssetGroup(&$jsAssets) {
+		$jsAssets[] = self::ASSET_GROUP_CORE;
+		$jsAssets[] = self::ASSET_GROUP_ADENGINE;
+		return true;
+	}
+
+	/**
+	 * Remove oasis_shared_core_js asset (because we added it to the scripts in HEAD)
+	 *
+	 * @param array $jsAssets
+	 *
+	 * @return bool
+	 */
+	public function removeCoreAssetGroup(&$jsAssets) {
+		$coreGroupIndex = array_search(self::ASSET_GROUP_CORE, $jsAssets);
+		if ($coreGroupIndex !== false) {
+			// Removing oasis_shared_core_js asset group
+			array_splice($jsAssets, $coreGroupIndex, 1);
+		} else {
+			// Do nothing. oasis_shared_core_js must be present for ads to work
+		}
 		return true;
 	}
 }
