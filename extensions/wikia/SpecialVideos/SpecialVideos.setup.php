cc70128de42f29825c2eaa71b65feb6e9c210ace
liz <liz@wikia-inc.com>
2012-09-13 22:38:15 +0000

    (BugId:44848) fixing fatal in video redirect code

--- extensions/wikia/SpecialVideos/SpecialVideos.setup.php
+++ extensions/wikia/SpecialVideos/SpecialVideos.setup.php
@@ -23,10 +23,10 @@ $app->registerExtensionMessageFile( 'SpecialVideos', $dir.'SpecialVideos.i18n.ph
 $app->registerSpecialPage( 'Videos', 'SpecialVideosSpecialController' );
 
 // Fake special page for 301 redirect (Video->Videos)
-$app->registerSpecialPage('Video', 'Video');
+$app->registerSpecialPage('Video', 'RedirectToVideos');
 
 // redirects from Video to Videos
-class Video extends SpecialRedirectToSpecial {
+class RedirectToVideos extends SpecialRedirectToSpecial {
 	function __construct() {
 		parent::__construct( 'Video', 'Videos', false, array() );
 	}
