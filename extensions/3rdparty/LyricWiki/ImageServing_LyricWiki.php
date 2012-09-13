c99115c7567dd819930b92ef4e2fdb77339d8158
macbre <macbre@wikia-inc.com>
2012-09-13 13:32:18 +0000

    PHP Fatal error: Cannot access private property ImageServing::$articlesByNS in ImageServing_LyricWiki.php on line 37 (BugId:47722)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60052 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/3rdparty/LyricWiki/ImageServing_LyricWiki.php
+++ extensions/3rdparty/LyricWiki/ImageServing_LyricWiki.php
@@ -5,8 +5,8 @@
  * This extension hooks into Wikia's ImageServing to provide reasonable fallback images
  * on LyricWiki since there is a logical heirarchy of song -> album -> artist on LyricWiki.
  */
- 
- 
+
+
 if ( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
 
 $wgExtensionCredits['other'][] = array(
@@ -29,7 +29,7 @@ $wgHooks['ImageServing::fallbackOnNoResults'][] = 'lw_ImageServingFallback';
  * If this is an album page, will check the artist page for a good image.
  * If this is an artist page, there is no good fallback, so 'out' will be returned unmodified.
  */
-function lw_ImageServingFallback(&$imageServing, $n, &$out){
+function lw_ImageServingFallback(ImageServing $imageServing, $n, &$out){
 	wfProfileIn( __METHOD__ );
 
 	// For Gracenote pages (since they don't have their own images), fall back to the NS_MAIN pages with the same titles.
@@ -49,17 +49,17 @@ function lw_ImageServingFallback(&$imageServing, $n, &$out){
 		$articles = $imageServing->articlesByNS[ NS_MAIN ];
 		foreach($articles as $articleId => $articleData){
 			$title = $articleData['title'];
-			
+
 			// If the title is an album, fall back to the artist... if the title is a song, fall back to the album.
 			$matches = array();
 			if(0 < preg_match("/^(.*?):.*[ _]\([0-9]{4}\)$/", $title, $matches)){
 				$articleTitlesToTry[] = $matches[1];
 			} else {
-			
+
 				// TODO: Fall back to the album if there is one in the wikiText (which would automatically fall back to the artist if the album doesn't have a pic).
 					// NOTE: At the moment, I actually think artist pics might be better than album pics.
 				// TODO: Fall back to the album if there is one in the wikiText (which would automatically fall back to the artist if the album doesn't have a pic).
-				
+
 				// Fall back to the artist if no album could be found in the wikiText.
 				// TODO: if we have the wikitext already & the artist can be found in there, use that instead of a regex on the title (it is more likely to be accurate since the artist name may have colons in it).
 				// TODO: if we have the wikitext already & the artist can be found in there, use that instead of a regex on the title (it is more likely to be accurate since the artist name may have colons in it).
@@ -67,7 +67,7 @@ function lw_ImageServingFallback(&$imageServing, $n, &$out){
 			 		$articleTitlesToTry[] = $matches[1];
 				}
 			}
-			
+
 		}
 	}
 
@@ -80,12 +80,13 @@ function lw_ImageServingFallback(&$imageServing, $n, &$out){
 				$articleId = $title->getArticleID();
 				$article = Article::newFromID( $articleId );
 				if(is_object($article)){
+					/* @var $article WikiPage */
 					// Automatically follow redirects.
 					if($article->isRedirect()){
 						$title = $article->followRedirect();
 						$articleId = $title->getArticleID();
 					}
-					
+
 					// The title str could be converted into an article-id, so add it to the array to be processed.
 					$articleIds[] = $articleId;
 				}
