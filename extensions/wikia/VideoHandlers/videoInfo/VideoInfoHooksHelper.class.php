5c270c8a905351b4abee2511c9c955ca264b0064
saipetch <saipetch@wikia-inc.com>
2012-09-12 18:26:37 -0700

    Video (bugid 45003, 45004, 45010): add hooks for videoInfo

---
+++ extensions/wikia/VideoHandlers/videoInfo/VideoInfoHooksHelper.class.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * VideoInfo Hooks Helper
+ * @author Liz Lee, Saipetch Kongkatong
+ */
+class VideoInfoHooksHelper {
+
+	/**
+	 * Hook: clear cache when file is uploaded
+	 * @param $file LocalFile
+	 * @param $reupload
+	 * @param $hasDescription
+	 * @return true
+	 */
+	public static function onFileUpload( $file, $reupload, $hasDescription ) {
+		$title = $file->getTitle();
+		if ( $title instanceof Title && F::build( 'WikiaFileHelper', array( $file ), 'isFileTypeVideo' ) ) {
+			$mediaService = F::build( 'MediaQueryService' );
+
+			if ( $file->isLocal() ) {
+				$mediaService->clearCacheVideoList();
+			} else {
+				$videoInfoHelper = F::build( 'VideoInfoHelper' );
+				$videoData = $videoInfoHelper->getVideoDataByTitle( $title );
+				if ( !empty($videoData) ) {
+					$videoInfo = F::build( 'VideoInfo', array( $videoData ) );
+					if ( $reupload ) {
+						$videoInfo->reuploadVideo();
+					} else {
+						$videoInfo->addVideo();
+					}
+				}
+
+				$mediaService->clearCachePremiumVideoList();
+			}
+
+			if ( !$reupload ) {
+				$mediaService->clearCacheVideoList();
+				$mediaService->clearCacheTotalVideos();
+			}
+
+			$mediaService->clearCacheSortedVideos();
+		}
+
+		return true;
+	}
+
+	/**
+	 * Hook: clear cache for premium videos
+	 * @param type $article
+	 * @param type $user
+	 * @param type $revision
+	 * @param type $status
+	 * @return true 
+	 */
+	public static function onArticleSaveComplete( $article, $user, $revision, $status ) {
+		$insertedImages = Wikia::getVar( 'imageInserts' );
+		$imageDeletes = Wikia::getVar( 'imageDeletes' );
+
+		$changedImages = $imageDeletes;
+		foreach( $insertedImages as $img ) {
+			$changedImages[ $img['il_to'] ] = true;
+
+			
+		}
+		
+		foreach( $changedImages as $imageDBName => $dummy ) {
+			$title = F::build( 'Title', array( NS_FILE, $imageDBName ), 'makeTitle' );
+			if ( $title instanceof Title ) {
+				$file = wfFindFile( $title );
+				if ( $file instanceof File && $file->exists() && !$file->isLocal()
+					&& F::build( 'WikiaFileHelper', array( $file ), 'isFileTypeVideo' ) ) {
+					$mediaService = F::build( 'MediaQueryService' );
+					$mediaService->clearCachePremiumVideoList();
+					$mediaService->clearCacheVideoList();
+					$mediaService->clearCacheTotalVideos();
+					$mediaService->clearCacheSortedPremiumVideos();
+					break;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Hook: clear cache when file is deleted
+	 * @param $file LocalFile
+	 * @param $oldimage
+	 * @param $article
+	 * @param $user User
+	 * @param $reason
+	 * @return true
+	 */
+	public static function onFileDeleteComplete( &$file, $oldimage, $article, $user, $reason ) {
+		$title = $file->getTitle();
+		if ( $title instanceof Title && F::build( 'WikiaFileHelper', array( $file ), 'isFileTypeVideo' ) ) {
+			$mediaService = F::build( 'MediaQueryService' );
+			if ( $file->isLocal() ) {
+				$mediaService->clearAllCacheVideos();
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Hook: clear cache when file is restored
+	 * @param Title $title
+	 * @param type $versions
+	 * @param User $user
+	 * @param type $comment
+	 * @return true 
+	 */
+	public static function onFileUndeleteComplete( $title, $versions, $user, $comment ) {
+		$mediaService = F::build( 'MediaQueryService' );
+		$mediaService->invalidateAllCacheVideos( $title );
+
+		return true;
+	}
+
+	/**
+	 * Hook: clear cache when file is renamed
+	 * @param type $form
+	 * @param type $oldTitle
+	 * @param type $newTitle
+	 * @return true
+	 */
+	public static function onFileRenameComplete( &$form , &$oldTitle , &$newTitle ) {
+		$mediaService = F::build( 'MediaQueryService' );
+		$mediaService->invalidateAllCacheVideos( $newTitle );
+
+		return true;
+	}
+
+}
