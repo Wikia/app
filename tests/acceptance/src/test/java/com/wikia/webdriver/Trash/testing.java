5c591668d7c8ff1455c0b71b45e4a182fe34f9f2
karol <karol@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-13 11:38:52 +0000

    for testing only
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60028 e846bd91-c042-42a7-a218-4b65e2c1904d

--- tests/acceptance/src/test/java/com/wikia/webdriver/Trash/testing.java
+++ tests/acceptance/src/test/java/com/wikia/webdriver/Trash/testing.java
@@ -26,6 +26,11 @@ public class testing extends TestTemplate{
 //		
 		((JavascriptExecutor)driver).executeScript("$('.Video_Games').mouseenter()");
 		((JavascriptExecutor)driver).executeScript("$('.Video_Games').mouseenter()");
+		((JavascriptExecutor)driver).executeScript("$('.Video_Games').mouseenter()");
+		((JavascriptExecutor)driver).executeScript("$('.Video_Games').mouseenter()");
+		((JavascriptExecutor)driver).executeScript("$('.Video_Games').mouseenter()");
+		((JavascriptExecutor)driver).executeScript("$('.Video_Games').mouseenter()");
+		((JavascriptExecutor)driver).executeScript("$('.Video_Games').mouseenter()");
 
 //		b.executeScript("$('.Video_Games').mouseenter()");
 //		b.executeScript("$('.Video_Games').mouseenter()");
