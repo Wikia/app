f969a097a25b2cc07c56e9f6fa6238088c2b3933
nandy <nandy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-14 01:14:24 +0000

    changed liftium id in wikia bar
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60090 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaBar/templates/WikiaBar_Index.php
+++ extensions/wikia/WikiaBar/templates/WikiaBar_Index.php
@@ -1,7 +1,7 @@
 <div class="WikiaBarWrapper">
 	<div class="wikia-bar">
 		<div class="ad">
-			<div id="WIKIA_BAR_BOXAD_1" class="noprint" style="height: 250px; width: 300px; position: relative;"><div id="Liftium_300x250_99"><iframe width="300" height="250" id="WIKIA_BAR_BOXAD_1_iframe" class="" noresize="true" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" style="border:none" target="_blank"></iframe></div></div>
+			<div id="WIKIA_BAR_BOXAD_1" class="noprint" style="height: 250px; width: 300px; position: relative;"><div id="Liftium_300x250_98"><iframe width="300" height="250" id="WIKIA_BAR_BOXAD_1_iframe" class="" noresize="true" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" style="border:none" target="_blank"></iframe></div></div>
 		</div>
 		<a href="#" class="arrow"></a>
 		<div class="message">message</div>
