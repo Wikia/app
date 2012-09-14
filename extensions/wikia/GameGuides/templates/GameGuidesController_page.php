5bea031efdbea3185b53019aed30653a39e97b54
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-14 16:59:15 +0000

    GameGuides: Special Page visible only by Staff and SysOps
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60162 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GameGuides/templates/GameGuidesController_page.php
+++ extensions/wikia/GameGuides/templates/GameGuidesController_page.php
@@ -2,7 +2,8 @@
 /**
  * @var $title String a title of a page
  * @var $html String content of a page
- */ ?>
+ */
+?>
 <h1 id=pageName><?= $title; ?></h1>
 <section id=wkPage>
 	<div id=mw-content-text>
