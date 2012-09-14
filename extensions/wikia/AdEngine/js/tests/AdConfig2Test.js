3dc613ee41f710ddc1ad9431373a6eda9c393b1f
piotrgabryjeluk <rychu@wikia-inc.com>
2012-09-14 14:33:33 +0000

    Moving AdConfig2.isSlotGamePro to AdProviderGamePro.canHandleSlot (BugId:47746)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60150 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/js/tests/AdConfig2Test.js
+++ extensions/wikia/AdEngine/js/tests/AdConfig2Test.js
@@ -9,7 +9,7 @@ module('AdConfig2');
 
 test('getProvider failsafe', function() {
 	// setup
-	var AdProviderGameProMock = {name: 'GameProMock'}
+	var AdProviderGameProMock = {name: 'GameProMock', canHandleSlot: function() {return false;}}
 		, AdProviderEvolveMock = {name: 'EvolveMock'}
 		, AdProviderEvolveRSMock = {name: 'EvolveRSMock'}
 		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
@@ -37,7 +37,7 @@ test('getProvider failsafe', function() {
 // TODO may be refactored to AdProviderEvolve
 test('getProvider evolve AU', function() {
 	// setup
-	var AdProviderGameProMock = {name: 'GameProMock'}
+	var AdProviderGameProMock = {name: 'GameProMock', canHandleSlot: function() {return false;}}
 		, AdProviderEvolveMock = {name: 'EvolveMock'}
 		, AdProviderEvolveRSMock = {name: 'EvolveRSMock'}
 		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
@@ -70,7 +70,7 @@ test('getProvider evolve AU', function() {
 // TODO may be refactored to AdProviderEvolve
 test('getProvider evolve not AU', function() {
 	// setup
-	var AdProviderGameProMock = {name: 'GameProMock'}
+	var AdProviderGameProMock = {name: 'GameProMock', canHandleSlot: function() {return false;}}
 		, AdProviderEvolveMock = {name: 'EvolveMock'}
 		, AdProviderEvolveRSMock = {name: 'EvolveRSMock'}
 		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
@@ -94,67 +94,4 @@ test('getProvider evolve not AU', function() {
 	notEqual(adConfig.getProvider(['TOP_LEADERBOARD']).name, 'EvolveMock', 'TOP_LEADERBOARD');
 	notEqual(adConfig.getProvider(['TOP_RIGHT_BOXAD']).name, 'EvolveMock', 'TOP_RIGHT_BOXAD');
 	notEqual(adConfig.getProvider(['LEFT_SKYSCRAPER_2']).name, 'EvolveMock', 'LEFT_SKYSCRAPER_2');
-});
-
-// TODO may be refactored to AdProviderGamePro
-test('getProvider GamePro de', function() {
-	// setup
-	var AdProviderGameProMock = {name: 'GameProMock'}
-		, AdProviderEvolveMock = {name: 'EvolveMock'}
-		, AdProviderEvolveRSMock = {name: 'EvolveRSMock'}
-		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
-		, AdProviderAdDriverMock = {name: 'AdDriverMock'}
-		, AdProviderLiftium2Mock = {name: 'Liftium2Mock'}
-		, adConfig;
-
-	window.wgContentLanguage = 'de';
-
-	adConfig = AdConfig2(
-		Wikia.log, Wikia, window,
-		// AdProviders
-		AdProviderGameProMock,
-		AdProviderEvolveMock,
-		AdProviderEvolveRSMock,
-		AdProviderAdDriver2Mock,
-		AdProviderAdDriverMock,
-		AdProviderLiftium2Mock
-	);
-
-	equal(adConfig.getProvider(['TOP_LEADERBOARD']).name, 'GameProMock', 'TOP_LEADERBOARD');
-	equal(adConfig.getProvider(['TOP_RIGHT_BOXAD']).name, 'GameProMock', 'TOP_RIGHT_BOXAD');
-	equal(adConfig.getProvider(['LEFT_SKYSCRAPER_2']).name, 'GameProMock', 'LEFT_SKYSCRAPER_2');
-	equal(adConfig.getProvider(['PREFOOTER_LEFT_BOXAD']).name, 'GameProMock', 'PREFOOTER_LEFT_BOXAD');
-
-	notEqual(adConfig.getProvider(['INCONTENT_BOXAD_1']).name, 'GameProMock', 'INCONTENT_BOXAD_1');
-	notEqual(adConfig.getProvider(['PREFOOTER_RIGHT_BOXAD']).name, 'GameProMock', 'PREFOOTER_RIGHT_BOXAD');
-});
-
-// TODO may be refactored to AdProviderGamePro
-test('getProvider GamePro not de', function() {
-	// setup
-	var AdProviderGameProMock = {name: 'GameProMock'}
-		, AdProviderEvolveMock = {name: 'EvolveMock'}
-		, AdProviderEvolveRSMock = {name: 'EvolveRSMock'}
-		, AdProviderAdDriver2Mock = {name: 'AdDriver2Mock'}
-		, AdProviderAdDriverMock = {name: 'AdDriverMock'}
-		, AdProviderLiftium2Mock = {name: 'Liftium2Mock'}
-		, adConfig;
-
-	window.wgContentLanguage = 'not de';
-
-	adConfig = AdConfig2(
-		Wikia.log, Wikia, window,
-		// AdProviders
-		AdProviderGameProMock,
-		AdProviderEvolveMock,
-		AdProviderEvolveRSMock,
-		AdProviderAdDriver2Mock,
-		AdProviderAdDriverMock,
-		AdProviderLiftium2Mock
-	);
-
-	notEqual(adConfig.getProvider(['TOP_LEADERBOARD']).name, 'GameProMock', 'TOP_LEADERBOARD');
-	notEqual(adConfig.getProvider(['TOP_RIGHT_BOXAD']).name, 'GameProMock', 'TOP_RIGHT_BOXAD');
-	notEqual(adConfig.getProvider(['LEFT_SKYSCRAPER_2']).name, 'GameProMock', 'LEFT_SKYSCRAPER_2');
-	notEqual(adConfig.getProvider(['PREFOOTER_LEFT_BOXAD']).name, 'GameProMock', 'PREFOOTER_LEFT_BOXAD');
 });
