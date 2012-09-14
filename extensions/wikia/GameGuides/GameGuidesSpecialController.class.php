c8b01d75289833b5f1ab174762511052c5926e29
jolek <jolek@wikia-inc.com>
2012-09-14 16:59:15 +0000

    GameGuides: Special Page visible only by Staff and SysOps
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60162 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GameGuides/GameGuidesSpecialController.class.php
+++ extensions/wikia/GameGuides/GameGuidesSpecialController.class.php
@@ -2,10 +2,15 @@
 
 class GameGuidesSpecialController extends WikiaSpecialPageController {
 	public function __construct() {
-		parent::__construct('GameGuidesPreview', '', false /* $listed */);
+		parent::__construct('GameGuidesPreview', '', false);
 	}
 
 	public function index() {
+		if (!$this->wg->User->isAllowed('gameguidespreview')) {
+			$this->displayRestrictionError();
+			return false;  // skip rendering
+		}
+
 		$titleText = $this->getPar();
 
 		$this->setVal( 'url', $this->wg->Server .'/wikia.php?allinone=1&controller=GameGuidesController&method=renderPage&title=' . $titleText );
