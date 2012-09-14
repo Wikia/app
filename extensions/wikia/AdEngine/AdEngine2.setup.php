651b9177323ec689ac3f24d6014e4f53ea5bdc36
piotrgabryjeluk <piotrgabryjeluk@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-14 09:42:19 +0000

    Moving hook logic to AdEngine2Controller (BugId:46885)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60116 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/AdEngine2.setup.php
+++ extensions/wikia/AdEngine/AdEngine2.setup.php
@@ -7,22 +7,5 @@ $app = F::app();
 $app->registerClass('AdEngine2Controller', "$dir/AdEngine2Controller.class.php");
 
 $app->registerHook('WikiaSkinTopScripts', 'AdEngine2Controller', 'onWikiaSkinTopScripts');
-
-// Decide where to load JavaScript files:
-if (!empty($wgLoadAdsInHead)) {
-	/*
-	 * At the top. Here is what we do:
-	 *
-	 * Remove oasis_shared_core_js from OasisSkinAssetGroups
-	 * Add oasis_shared_core_js and adengine2_js to OasisSkinAssetGroupsBlocking
-	 */
-	$app->registerHook('OasisSkinAssetGroupsBlocking', 'AdEngine2Controller', 'addBlockingAssetGroup');
-	$app->registerHook('OasisSkinAssetGroups', 'AdEngine2Controller', 'removeCoreAssetGroup');
-} else {
-	/*
-	 * At the bottom. Here is what we do:
-	 *
-	 * Add adengine2_js just after oasis_shared_core_js in OasisSkinAssetGroupsBlocking
-	 */
-	$app->registerHook('OasisSkinAssetGroups', 'AdEngine2Controller', 'addNonBlockingAssetGroup');
-}
+$app->registerHook('OasisSkinAssetGroupsBlocking', 'AdEngine2Controller', 'onOasisSkinAssetGroupsBlocking');
+$app->registerHook('OasisSkinAssetGroups', 'AdEngine2Controller', 'onOasisSkinAssetGroups');
