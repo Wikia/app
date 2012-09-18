6dc62002a48b0927bd96ca84f5366121b9661b36
eloy <eloy@wikia-inc.com>
2012-09-18 08:51:08 +0000

    create database updater for wikia_page_backlinks table
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60295 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/Backlinks/patch-create-wikia_page_backlinks.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `/*$wgDBprefix*/wikia_page_backlinks` (
+	`source_page_id` UNSIGNED INT NOT NULL,
+	`target_page_id` UNSIGNED INT NOT NULL,
+	`backlink_text` VARBINARY(255),
+	`count` INT,
+	PRIMARY KEY (`source_page_id`, `target_page_id`, `backlink_text`),
+	KEY `wikia_page_backlinks_source_page_id` (`source_page_id`),
+	KEY `wikia_page_backlinks_target_page_id` (`target_page_id`)
+) ENGINE = InnoDB;
