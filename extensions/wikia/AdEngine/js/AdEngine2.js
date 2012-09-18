1ffe8585f8063b725ffb703d84efeee5779b0df2
piotrgabryjeluk <piotrgabryjeluk@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-18 14:42:17 +0000

    AdEngine2 var name change MODULE -> module
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60325 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/js/AdEngine2.js
+++ extensions/wikia/AdEngine/js/AdEngine2.js
@@ -1,33 +1,33 @@
 var AdEngine2 = function(AdConfig2, log, LazyQueue) {
 	'use strict';
 
-	var MODULE = 'AdEngine2'
+	var module = 'AdEngine2'
 		, fillInSlot
 		, run;
 
 	fillInSlot = function(slot) {
-		log('fillInSlot', 5, MODULE);
-		log(slot, 5, MODULE);
+		log('fillInSlot', 5, module);
+		log(slot, 5, module);
 
 		var provider = AdConfig2.getProvider(slot);
-		log('calling ' + provider.name + ' for ' + slot[0], 3, MODULE);
+		log('calling ' + provider.name + ' for ' + slot[0], 3, module);
 
 		provider.fillInSlot(slot);
 	};
 
 	run = function(adslots) {
-		log('run', 5, MODULE);
+		log('run', 5, module);
 
-		log('initial queue', 7, MODULE);
-		log(adslots, 7, MODULE);
+		log('initial queue', 7, module);
+		log(adslots, 7, module);
 
-		log('initializing LazyQueue on adslots', 7, MODULE);
+		log('initializing LazyQueue on adslots', 7, module);
 		LazyQueue.makeQueue(adslots, fillInSlot);
 
-		log('launching queue on adslots', 7, MODULE);
+		log('launching queue on adslots', 7, module);
 		adslots.start();
 
-		log('initial queue handled', 6, MODULE);
+		log('initial queue handled', 6, module);
 	};
 
 	return {run: run};
