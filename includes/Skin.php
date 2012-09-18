c50ac72c547cc4412ba52937809349b31527bbb8
jolek <jolek@wikia-inc.com>
2012-09-18 14:56:15 +0000

    PHP Lint little helper
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60334 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/Skin.php
+++ includes/Skin.php
@@ -18,6 +18,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
  * //Wikia Change - Jakub (Helps PHP Lint)
  * @method get
  * @method set
+ * @method String getTopScripts
  * //Wikia Change End
  */
 abstract class Skin extends ContextSource {
