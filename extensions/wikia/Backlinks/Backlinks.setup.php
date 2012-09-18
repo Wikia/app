6a7a202e8245fd224ea5943e96c28018e2122f6c
eloy <eloy@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-18 08:51:08 +0000

    create database updater for wikia_page_backlinks table
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60295 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Backlinks/Backlinks.setup.php
+++ extensions/wikia/Backlinks/Backlinks.setup.php
@@ -2,9 +2,9 @@
 
 /**
  * Backlinks
- * 
+ *
  * Writes backlink text to a custom database when rendering wikitext
- * 
+ *
  * @author Robert Elwell <robert@wikia-inc.com>
  * @date 2012-09-01
  * @copyright Copyright (C) 2012 Wikia Inc.
@@ -21,27 +21,29 @@ $wgExtensionCredits['other'][] = array(
 	'author' => array('Robert Elwell'),
 	'descriptionmsg' => 'backlinks-desc',
 );
+
 global $wgEnableBacklinksExt;
 if ($wgEnableBacklinksExt) {
 
 	$dir = dirname(__FILE__);
 	$app = F::app();
-	
+
 	// Interface code
 	include("$dir/Backlinks.php");
-	
+
 	$app->registerClass("Backlinks", "$dir/Backlinks.class.php");
-	
+
 	//@todo make a special page controller that lets you view all pointing to a given page
 	/**
 	 * * register class BacklinksController $dir/BacklinksController.class.php
 	 * * register special page Backlinks BacklinksController
 	 */
-	
+
 	// i18n
 	$wgExtensionMessagesFiles['Backlinks'] = $dir.'/Backlinks.i18n.php';
-	
+
 	// hooks
 	$wgHooks['LinkEnd'][] = 'Backlinks::storeBacklinkText';
 	$wgHooks['OutputPageParserOutput'][] = 'Backlinks::updateBacklinkText';
+	$wgHooks['LoadExtensionSchemaUpdates'][] = 'Backlinks::onLoadExtensionSchemaUpdates';
 }
