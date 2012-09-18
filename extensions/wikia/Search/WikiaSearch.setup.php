113a8bd3b77c01a7f43198c0fcfe6ff782053aa0
Robert Elwell <robert@wikia-inc.com>
2012-09-18 21:57:46 +0000

    Created WikiaSearchConfig and added to search setup

--- extensions/wikia/Search/WikiaSearch.setup.php
+++ extensions/wikia/Search/WikiaSearch.setup.php
@@ -10,17 +10,18 @@
 $app = F::app();
 $dir = dirname(__FILE__) . '/';
 
-require_once( $dir . 'SolrPhpClient/Apache/Solr/Service.php' );
+require( 'Solarium/Autoloader.php' );
+Solarium_Autoloader::register();
 
 /**
  * classes
  */
 $app->registerClass('WikiaSearch', $dir . 'WikiaSearch.class.php');
+$app->registerClass('WikiaSearchConfig', $dir . 'WikiaSearchConfig.class.php');
 $app->registerClass('WikiaSearchClient', $dir . 'WikiaSearchClient.class.php');
 $app->registerClass('WikiaSearchController', $dir . 'WikiaSearchController.class.php');
 $app->registerClass('WikiaSearchResult', $dir . 'WikiaSearchResult.class.php');
 $app->registerClass('WikiaSearchResultSet', $dir . 'WikiaSearchResultSet.class.php');
-$app->registerClass('WikiaSolrClient', $dir . 'WikiaSolrClient.class.php');
 $app->registerClass('WikiaSearchAdsController', $dir . 'WikiaSearchAdsController.class.php');
 $app->registerClass('WikiaSearchAjaxController', $dir . 'WikiaSearchAjaxController.class.php');
 
@@ -39,11 +40,15 @@ if (isset($_GET['solrhost']) || isset($_GET['solrport'])) {
      $wgWikiaSearchUseProxy = false;
 }
 
-F::addClassConstructor( 'WikiaSolrClient',
-	array(
-	 'solrHost' => ( !empty( $wgSolrHost ) ? $wgSolrHost : 'localhost' ),
-	 'solrPort' => ( !empty( $wgSolrPort ) ? $wgSolrPort : 8180 )
-	));
+$solariumConfig = array(
+		'adapteroptions'	=> array(
+			'host' => ( !empty( $wgSolrHost ) ? $wgSolrHost : 'localhost'),
+			'port' => ( !empty( $wgSolrPort ) ? $wgSolrPort : 8180 ),
+			'path' => '/solr/',
+		)
+);
+
+F::addClassConstructor( 'WikiaSolrClient', array( 'solariumConfig' => $solariumConfig ) );
 
 
 F::addClassConstructor( 'WikiaSearch', array( 'client' => F::build('WikiaSolrClient') ) );
@@ -65,7 +70,7 @@ $app->registerHook('WikiaMobileAssetsPackages', 'WikiaSearchController', 'onWiki
 
 $wgExtensionCredits['other'][] = array(
 	'name'        => 'Wikia Search',
-	'version'     => '2.0.5',
+	'version'     => '3.0',
 	'author'      => '[http://www.wikia.com/wiki/User:Adi3ek Adrian \'ADi\' Wieczorek], [http://wikia.com/wiki/User:Relwell Robert Elwell]',
 	'descriptionmsg' => 'wikia-search-desc',
 );
