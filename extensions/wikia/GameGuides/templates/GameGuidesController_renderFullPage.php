1697f2b72006eb2e0790b45881aba36ff42a7325
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-18 14:56:19 +0000

    RelatedPages: Further work and improvements
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60335 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GameGuides/templates/GameGuidesController_renderFullPage.php
+++ extensions/wikia/GameGuides/templates/GameGuidesController_renderFullPage.php
@@ -1,11 +1,34 @@
-<DOCTYPE html>
+<?
+/**
+ * @var $globals String
+ * @var $messages String
+ * @var $title String
+ */
+?>
 <html>
 <head>
 <meta name=viewport content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
 <style><?= $css ?></style>
 </head>
 <body>
-<?= $html ?>
+<?= $globals; ?>
+<script>
+    wgMessages = <?= json_encode( $messages ); ?>;
+</script>
+<h2><?= $title ?></h2>
+<section id=wkPage>
+    <div id=mw-content-text>
+		<?= $html; ?>
+    </div>
+</section>
+<div id=wkMdlWrp>
+    <div id=wkMdlTB>
+        <div id=wkMdlTlBar></div>
+        <div id=wkMdlClo class=clsIco></div>
+    </div>
+    <div id=wkMdlCnt></div>
+    <div id=wkMdlFtr></div>
+</div>
 <script><?= $js ?></script>
 </body>
 </html>
