b7c4dea34b8d2cabaa319f46ba4d0b91a5993201
jolek <jolek@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-19 14:41:46 +0000

    GameGuides: Working on API
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60393 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GameGuides/templates/GameGuidesController_renderFullPage.php
+++ extensions/wikia/GameGuides/templates/GameGuidesController_renderFullPage.php
@@ -11,24 +11,7 @@
 <style><?= $css ?></style>
 </head>
 <body>
-<?= $globals; ?>
-<script>
-    wgMessages = <?= json_encode( $messages ); ?>;
-</script>
-<h2><?= $title ?></h2>
-<section id=wkPage>
-    <div id=mw-content-text>
-		<?= $html; ?>
-    </div>
-</section>
-<div id=wkMdlWrp>
-    <div id=wkMdlTB>
-        <div id=wkMdlTlBar></div>
-        <div id=wkMdlClo class=clsIco></div>
-    </div>
-    <div id=wkMdlCnt></div>
-    <div id=wkMdlFtr></div>
-</div>
+<?= $html; ?>
 <script><?= $js ?></script>
 </body>
 </html>
