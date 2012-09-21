0a612103c381642d19de21312a8189891c2cdcf0
mbrusilo <mbrusilo@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-21 14:11:40 +0000

    last change
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60487 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiEvolution/LocalRenderer.sh
+++ extensions/wikia/WikiEvolution/LocalRenderer.sh
@@ -1,14 +1,18 @@
 
 echo "Set Wiki name : "
 read wikiname
-echo "Set file extension : "
-read extension
-echo "Set picture resolution (WIDTHxHEIGHT) : "
-read resolution
 
 
+resolution=1360x720
+seconds_per_day=0.01
+auto_skip_seconds=0.1
+elasticity=0.05
+framerate=25
+bitrate=4000K
+extension=avi
 
-gource -$resolution --log-format custom $wikiname/gource.log --seconds-per-day 0.00001 --auto-skip-seconds 0.0001 --multi-sampling --stop-at-end --elasticity 0.1 -b 000000  --hide filenames,dirnames,progress --user-friction .2 --background-image background_logo.png --logo  $wikiname/wordmark.png --user-image-dir $wikiname/avatars -s 3  --output-ppm-stream - --output-framerate 60 | avconv -y -r 60 -f image2pipe -vcodec ppm -i - -b 8192K $wikiname/$wikiname.$extension
+
+gource -$resolution --log-format custom $wikiname/gource.log --seconds-per-day $seconds_per_day --auto-skip-seconds $auto_skip_seconds --multi-sampling --stop-at-end --elasticity $elasticity -b 000000  --hide filenames,dirnames,progress --user-friction .2 --background-image background_logo.png --logo  $wikiname/wordmark.png --user-image-dir $wikiname/avatars --output-ppm-stream - --output-framerate $framerate | avconv -y -r $framerate -f image2pipe -vcodec ppm -i - -b $bitrate $wikiname/$wikiname.$extension
 
 
 
