8722eb00806d1936d595b2c357c6ad5dd5c7a490
marcin <marcin@wikia-inc.com>
2012-09-26 16:18:43 +0000

    WikiaBar - correct styling for ad slot (bugid:48620)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60644 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaBar/templates/WikiaBar_Anon.php
+++ extensions/wikia/WikiaBar/templates/WikiaBar_Anon.php
@@ -1,6 +1,6 @@
 <div class="ad">
 	<?php //todo: use renderView('Ad', 'index'); instead of line below ?>
-	<div id="WIKIA_BAR_BOXAD_1" class="noprint" style="height: 250px; width: 300px; position: relative;"><div id="Liftium_300x250_98"><iframe width="300" height="250" id="WIKIA_BAR_BOXAD_1_iframe" class="" noresize="true" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" style="border:none" target="_blank"></iframe></div></div>
+	<div id="WIKIA_BAR_BOXAD_1" class="noprint" style="width: 300px; position: relative;"><div id="Liftium_300x250_98"></div></div>
 </div>
 <div class="message<?if (empty($status)): ?> failsafe<? endif; ?>" data-content="<?= htmlentities(json_encode($barContents['messages']), ENT_QUOTES)  ?>"></div>
 <? foreach ($barContents['buttons'] as $idx => $button): ?>
