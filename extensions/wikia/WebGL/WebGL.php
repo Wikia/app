95a662310e8b2cf22804092a7d0b453d4a128232
mech <mech@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-09-27 08:06:24 +0000

    WebGL internship extension
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60685 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/WebGL/WebGL.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * WebGL widget c - implements tag <cube> for showing webGL 3D cube 
+ *
+ * @author Andrzej P.Urbański <andrzej.urbanski(at)wikia-inc.com>
+ */
+
+if(!defined('MEDIAWIKI')) {
+	die();
+}
+
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'Cube',
+	'author' => 'Andrzej P.Urbański',
+	'url' => 'http://www.wikia.com' ,
+	'description' => 'implements tag <cube> for webGL widget of cube 3D object',
+	'descriptionmsg' => 'cube-desc'
+);
+
+$app = F::app();
+$dir = dirname( __FILE__ );
+$app->registerClass('CubeController', $dir . '/CubeController.class.php');
+/**
+ * setup functions
+ */
+$wgExtensionFunctions[] = 'wfCube';
+$wgExtensionMessagesFiles['Cube'] = dirname(__FILE__) . '/Cube.include.php';
+
+function wfCube() {
+	global $wgHooks, $wgAutoloadClasses;
+
+	$dir = dirname(__FILE__) . '/';
+
+	/**
+	 * hooks
+	 */
+	$wgHooks['ParserFirstCallInit'][] = 'wfCubeInitParserHooks';
+	$wgHooks['ParserAfterTidy'][] = 'Cube::replaceMarkers';
+
+	/**
+	 * classes
+	 */
+	$wgAutoloadClasses['Cube'] = $dir . 'Cube.class.php';
+}
+
+function wfCubeInitParserHooks( &$parser ) {
+
+	$parser->setHook('cube','Cube::CubeParserHook');
+
+	return true;
+}
