b8782da71bd5721641a349cad3118301b29b16ea
tor <tor@wikia-inc.com>
2012-09-28 14:36:23 +0000

    removed viedeo transcusions tracking code BugzId:23881
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@60759 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -3946,20 +3946,6 @@ class Parser {
 	function interwikiTransclude( $title, $action ) {
 		global $wgEnableScaryTranscluding;
 
-		# BEGIN WIKIA CHANGE
-		# wlee 2012/3/8 - log interwiki transclusions to video repo for debugging
-		global $wgCityId, $wgTitle, $wgExternalDatawareDB, $wgWikiaVideoInterwikiPrefix;
-		if ($title->getInterwiki() == $wgWikiaVideoInterwikiPrefix) {
-			// log for debugging purposes
-			$dbw_dataware = wfGetDB( DB_MASTER, array(), $wgExternalDatawareDB );
-			$res = $dbw_dataware->query(
-				"INSERT INTO " .
-					"`video_interwiki` (city_id,article_id,video_title) " .
-				"VALUES (". $dbw_dataware->addQuotes($wgCityId). ",". $dbw_dataware->addQuotes($wgTitle->getArticleId()). ",".
-					$dbw_dataware->addQuotes($title->getText()). ") ON DUPLICATE KEY UPDATE last_accessed=NOW()", __METHOD__ );
-		}
-		# END WIKIA CHANGE
-
 		if ( !$wgEnableScaryTranscluding ) {
 			return wfMsgForContent('scarytranscludedisabled');
 		}
