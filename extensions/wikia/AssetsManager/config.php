40e3ac57653a9ab3d02f2570a5e7da28b24cb036
ppiotr <ppiotr@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-10-03 23:03:34 +0000

    ad provider liftium2 dom for wikia bar
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61047 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -91,6 +91,7 @@ $config['oasis_ads_js'] = array(
 		// @requires adengine2 core already loaded
 		// @requires liftium loaded later (TODO FIXME)
 		'//extensions/wikia/AdEngine/js/AdProviderLiftium2.js',
+		'//extensions/wikia/AdEngine/js/AdProviderLiftium2Dom.js',
 		'//extensions/wikia/AdEngine/js/AdConfig2Late.js',
 		'//extensions/wikia/AdEngine/js/AdEngine2.configLateAds.js',
 
