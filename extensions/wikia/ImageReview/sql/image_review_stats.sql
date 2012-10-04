b8c1e2b8c9685559c573b6f4ac314b45ea9f73d2
tor <tor@wikia-inc.com>
2012-10-04 14:30:15 +0000

    added index BugzId:48702
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61091 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/ImageReview/sql/image_review_stats.sql
+++ extensions/wikia/ImageReview/sql/image_review_stats.sql
@@ -5,5 +5,6 @@ CREATE TABLE `image_review_stats` (
   `review_state` int(11) NOT NULL DEFAULT '0',
   `review_end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   KEY `reviewer_idx` (`reviewer_id`),
-  KEY `state_idx` (`reviewer_id`,`review_state`)
+  KEY `state_idx` (`reviewer_id`,`review_state`),
+  KEY `image_idx` (`wiki_id`,`page_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
