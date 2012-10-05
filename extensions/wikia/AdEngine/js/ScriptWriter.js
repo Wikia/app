fe1776d84ec0b6c2542a73ec4bf35d9cc3d34086
piotrgabryjeluk <rychu@wikia-inc.com>
2012-10-05 12:26:05 +0000

    Revert "Devbox test"
    This reverts commit 75fb19409e2a9911c5df017f7dd23c742702f2cf.
    
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61198 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdEngine/js/ScriptWriter.js
+++ extensions/wikia/AdEngine/js/ScriptWriter.js
@@ -15,7 +15,6 @@ var ScriptWriter = function(log, ghostwriter, document) {
 				script: { src: url },
 				done: function() {
 					log('DONE injectScriptByUrl: (' + url + ' to slot: ' + elementId + ')', 5, module);
-					log('ghostwriter.flushloadhandlers()', 1, 'GWFLUSH');
 					ghostwriter.flushloadhandlers();
 					if (typeof(callback) === 'function') {
 						callback();
@@ -34,7 +33,6 @@ var ScriptWriter = function(log, ghostwriter, document) {
 				script: { text: text },
 				done: function() {
 					log('DONE injectScriptByText: (' + text.substr(0, 20) + '... to slot: ' + elementId + ')', 5, module);
-					log('ghostwriter.flushloadhandlers()', 1, 'GWFLUSH');
 					ghostwriter.flushloadhandlers();
 					if (typeof(callback) === 'function') {
 						callback();
@@ -52,7 +50,6 @@ var ScriptWriter = function(log, ghostwriter, document) {
 				insertType: 'append',
 				script: { text: '' },
 				done: function() {
-					log('NO ghostwriter.flushloadhandlers()', 1, 'GWFLUSH');
 					if (typeof(callback) === 'function') {
 						log('Calling callLater now', 7, module);
 						callback();
