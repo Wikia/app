48eaa33449e6f2c53938e8f19cfedd7b1beaaa58
eloy <eloy@wikia-inc.com>
2012-10-05 08:57:41 +0000

    swap places
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61154 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Backlinks/patch-create-wikia_page_backlinks.sql
+++ extensions/wikia/Backlinks/patch-create-wikia_page_backlinks.sql
@@ -1,6 +1,6 @@
 CREATE TABLE /*$wgDBprefix*/wikia_page_backlinks (
-	`source_page_id` UNSIGNED INT NOT NULL,
-	`target_page_id` UNSIGNED INT NOT NULL,
+	`source_page_id` INT UNSIGNED NOT NULL,
+	`target_page_id` INT UNSIGNED NOT NULL,
 	`backlink_text` VARBINARY(255),
 	`count` INT,
 	PRIMARY KEY (`source_page_id`, `target_page_id`, `backlink_text`),
