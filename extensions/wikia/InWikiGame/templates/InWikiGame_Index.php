738b4f34559e10288aafbe6146c71fb44bf53fe2
marzjan <sebastianm@wikia-inc.com>
2012-10-05 15:10:41 +0000

    BugId: 49571 - create InWikiGame extension, WIP
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61216 e846bd91-c042-42a7-a218-4b65e2c1904d

---
+++ extensions/wikia/InWikiGame/templates/InWikiGame_Index.php
@@ -0,0 +1,12 @@
+<div id="InWikiGame" class="InWikiGame">
+
+</div>
+
+<?php
+
+echo F::build('JSSnippets')->addToStack(
+	array('/extensions/wikia/InWikiGame/js/InWikiGame.js'),
+	null,
+	'InWikiGame.init',
+	null
+);
