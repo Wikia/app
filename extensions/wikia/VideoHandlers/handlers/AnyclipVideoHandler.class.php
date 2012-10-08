7096016102fa7091c997ba73b13fb334b82f5b3b
saipetch <saipetch@wikia-inc.com>
2012-10-08 22:38:53 +0000

    Video (bugid 47950): update anyclip embed code
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61322 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/VideoHandlers/handlers/AnyclipVideoHandler.class.php
+++ extensions/wikia/VideoHandlers/handlers/AnyclipVideoHandler.class.php
@@ -16,18 +16,27 @@ class AnyclipVideoHandler extends VideoHandler {
 
 	public function getEmbed( $articleId, $width, $autoplay = false, $isAjax = false, $postOnload = false ) {
 		$height =  $this->getHeight( $width );
-		$autoPlay = ( $autoplay ) ? ', autoPlay:true' : '' ;
 
 		$html = <<<EOT
 <div id="AnyClipPlayer" style="width: {$width}px; height: {$height}px;"></div>
+EOT;
+
+		if ( $autoplay ) {
+			$html .= <<<EOT
 <script type="text/javascript">
 	$.when(
 		$.getScript('http://player.anyclip.com/embed/AnyClipPlayer.js')
 	).done(function() {
-		AnyClipPlayer.load(["#AnyClipPlayer", {clipID:"{$this->videoId}"{$autoPlay}}]);
+		AnyClipPlayer.load(["#AnyClipPlayer", {clipID:"{$this->videoId}", autoPlay:true}]);
 	});
 </script>
 EOT;
+		} else {
+			$html .= <<<EOT
+<script type="text/javascript" src="http://player.anyclip.com/embed/AnyClipPlayer.js"></script>
+<script type="text/javascript">AnyClipPlayer.load(["#AnyClipPlayer", {clipID:"{$this->videoId}"}]);</script>
+EOT;
+		}
 
 		return $html;
 	}
