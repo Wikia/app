2efecf218f5f45084424664b846eb3a47f5ced59
liz <liz@wikia-inc.com>
2012-10-08 17:32:54 +0000

    (BugId:46727) Lazy loading RelatedVideos
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61300 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/RelatedVideos/templates/RelatedVideos_getCaruselElementRL.php
+++
@@ -1,9 +0,0 @@
-<div class="item">
-	<?= $videoThumb ?>
-	<div class="description">
-		<a class="lightbox" href="<?=$video['fullUrl'];?>" data-ref="<?=$video['prefixedUrl'];?>" data-external="<?=$video['external'];?>" data-video-name="<?= $escapedTitle ?>" >
-		<?= $truncatedTitle ?>
-		</a>
-		<span class="video-views"><?= $videoViewsMsg ?></span>
-	</div>
-</div>
