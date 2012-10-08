7fb4bc9227bd9f8e04b5d423733ae0c0001af19a
marcin <marcin@wikia-inc.com>
2012-10-08 15:07:48 +0000

    Javascript for handling lazy-load of ROTMG iframe and tracking (bugid:49550)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61293 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/InWikiGame/js/InWikiGame.js
+++ extensions/wikia/InWikiGame/js/InWikiGame.js
@@ -11,6 +11,29 @@ var InWikiGame = {
 				}
 			);
 			$(this).html(iframe);
+			InWikiGame.trackClick('in-wiki-game', WikiaTracker.ACTIONS.CLICK_LINK_BUTTON, 'game-iframe-appears', null, {});
 		});
+	},
+	//todo: extract class
+	trackClick: function (category, action, label, value, params) {
+		var trackingObj = {
+			ga_category: category,
+			ga_action: action,
+			ga_label: label
+		};
+
+		if (value) {
+			trackingObj['ga_value'] = value;
+		}
+
+		if (params) {
+			$.extend(trackingObj, params);
+		}
+
+		WikiaTracker.trackEvent(
+			'trackingevent',
+			trackingObj,
+			'ga'
+		);
 	}
 }
