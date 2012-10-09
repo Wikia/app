9a59a32c5e7f09a78606103f99fb45c66f5e67da
nandy <nandy@wikia-inc.com>
2012-10-09 14:19:12 +0000

    bugId:49394; reverting previous changes to implement new fix in a better way
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61364 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/AdminDashboard/AdminDashboardController.class.php
+++ extensions/wikia/AdminDashboard/AdminDashboardController.class.php
@@ -18,10 +18,7 @@ class AdminDashboardController extends WikiaController {
 			$this->tab = 'advanced';
 		}
 
-		if( $this->wg->User->isLoggedIn() ) {
-		//fix for fb#49394 -- the page for anons looks better without this stylesheet
-			$this->wg->Out->addStyle(AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/AdminDashboard/css/AdminDashboard.scss'));
-		}
+		$this->wg->Out->addStyle(AssetsManager::getInstance()->getSassCommonURL('extensions/wikia/AdminDashboard/css/AdminDashboard.scss'));
 		
 		$this->wg->Out->addScriptFile($this->wg->ExtensionsPath . '/wikia/AdminDashboard/js/AdminDashboard.js');
 		
