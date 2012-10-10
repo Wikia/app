83486b68a56abb75c6f1266ed43924a413fb42b7
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-10-10 02:49:35 +0000

    reverting r61378
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61442 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaStyleGuide/js/Dropdown.js
+++ extensions/wikia/WikiaStyleGuide/js/Dropdown.js
@@ -44,8 +44,8 @@ Wikia.Dropdown = $.createClass(Observable, {
 			.on('click.' + this.settings.eventNamespace, this.proxy(this.onClick));
 
 		this.getItems()
-			.off('change.' + this.settings.eventNamespace)
-			.on('change.' + this.settings.eventNamespace, this.proxy(this.onChange));
+			.off('click.' + this.settings.eventNamespace)
+			.on('click.' + this.settings.eventNamespace, this.proxy(this.onChange));
 
 		this.$document
 			.off('click.' + this.settings.eventNamespace)
@@ -94,7 +94,7 @@ Wikia.Dropdown = $.createClass(Observable, {
 	},
 
 	getSelectedItems: function() {
-		return this.$dropdown.find(':checked:not(.select-all)');
+		return this.getItems().filter('.selected');
 	},
 
 	isOpen: function() {
