961d6812a078c3bb4c8ee812b3846048c90b34e1
federico <federico@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-10-10 02:47:42 +0000

    Wikis search API completed
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61411 e846bd91-c042-42a7-a218-4b65e2c1904d

--- includes/wikia/api/WikiInfoController.class.php
+++ includes/wikia/api/WikiInfoController.class.php
@@ -6,7 +6,7 @@
  */
 
 class WikiInfoController extends WikiaApiController {
-
+	const MAX_FIND_WIKIS_RESULTS = 250;
 
 	/**
 	 * Get the top wikis by pageviews optionally filtering by vertical (hub) and language
@@ -21,29 +21,32 @@ class WikiInfoController extends WikiaApiController {
 	public function getTopWikis(){
 		$this->wf->profileIn( __METHOD__ );
 
-		$hub = $this->request->getVal( 'hub', null );
-		$limit = $this->request->getInt( 'limit', 10 );
-		$lang = $this->getVal( 'lang', null );
-		$memKey = $this->app->wf->SharedMemcKey( __METHOD__, $lang, $hub, $limit );
+		$hub = trim( $this->request->getVal( 'hub', null ) );
+		$lang = trim( $this->getVal( 'lang', null ) );
+		$limit = $this->request->getInt( 'limit', 25 );
+		$memKey = $this->app->wf->SharedMemcKey( __METHOD__, $hub, $lang, $limit );
 		$results = $this->app->wg->Memc->get( $memKey );
 
 		if ( !is_array( $results ) ) {
 			$results = array();
 			//On devboxes use a 90-days span as DataMart
 			//data is seldomly updated there
-			$wikis = DataMartService::getTopWikisByPageviews( $limit, $lang, $hub, 1, ( $this->wg->DevelEnvironment ) ? 90 : 7 );
+			$wikis = DataMartService::getTopWikisByPageviews( $limit, $lang, $hub, 1 /* only pubic */, ( $this->wg->DevelEnvironment ) ? 90 : 7 );
 
 			foreach ( $wikis as $wikiId => $wiki ) {
-				$result = array();
-				$result['id'] =  $wikiId;
-				$result['hub'] = WikiFactory::getCategory($wikiId)->cat_name;
-
-				//fetching data from WikiFactory
-				$result['language'] = WikiFactory::getVarValueByName( 'wgLanguageCode', $wikiId );
-				$result['name'] = WikiFactory::getVarValueByName( 'wgSitename', $wikiId );
-				$result['topic'] = WikiFactory::getVarValueByName( 'wgWikiTopics', $wikiId );
-				$result['domain'] = str_replace( 'http://', '', WikiFactory::getVarValueByName( 'wgServer', $wikiId ) );
-				$results[] = $result;
+				$results[] = array(
+					'id' => $wikiId,
+					//fetching data from WikiFactory
+					//the table is indexed and values cached separately
+					//so making one query for all of them or many small
+					//separate one doesn't make any big difference while
+					//this respects WF's data abstraction layer
+					'name' => WikiFactory::getVarValueByName( 'wgSitename', $wikiId ),
+					'hub' => ( !empty( $hub ) ) ? $hub : $this->getWikiHubById( $wikiId ),
+					'language' => WikiFactory::getVarValueByName( 'wgLanguageCode', $wikiId ),
+					'topic' => WikiFactory::getVarValueByName( 'wgWikiTopics', $wikiId ),
+					'domain' => $this->getWikiDomainById( $wikiId )
+				);
 			}
 
 			$this->app->wg->Memc->set( $memKey, $results, 86400 /* 24h */ );
@@ -60,5 +63,112 @@ class WikiInfoController extends WikiaApiController {
 		);
 
 		$this->wf->profileOut( __METHOD__ );
+	}
+
+	public function findWikis(){
+		$this->wf->profileIn( __METHOD__ );
+
+		$keyword = trim( $this->request->getVal( 'keyword', '' ) );
+		$hub = trim( $this->request->getVal( 'hub', null ) );
+		$lang = trim( $this->getVal( 'lang', null ) );
+		$limit = $this->request->getInt( 'limit', 25 );
+		$batch = $this->request->getInt( 'batch', 1 );
+		$hubId = null;
+		$wikis = array();
+
+		if ( !empty( $keyword ) ) {
+			if ( !empty( $hub ) ) {
+				//this has it's own memcache layer (24h)
+				$hubData = WikiFactoryHub::getInstance()->getCategoryByName( $hub );
+
+				if ( is_array( $hubData ) ) {
+					$hubId = $hubData['id'];
+				}
+			}
+
+			if ( empty( $hub ) || ( !empty( $hub ) && is_integer( $hubId ) ) ) {
+				$memKey = $this->app->wf->SharedMemcKey( __METHOD__, str_replace( ' ', '_', $keyword ), $hub, $lang );
+				$wikis = $this->app->wg->Memc->get( $memKey );
+
+				if ( !is_array( $wikis ) ) {
+					$db = $this->wf->GetDB( DB_SLAVE, array(), $this->wg->ExternalSharedDB );
+
+					$keyword = mysql_real_escape_string( $keyword );
+					$varId = (int) WikiFactory::getVarByName( 'wgWikiTopics', null )->cv_variable_id;
+
+					$queryParts = array( "SELECT  cl.city_id AS id, cl.city_lang AS lang, cl.city_title AS name ,cv.cv_value AS topic FROM city_list AS cl LEFT JOIN city_variables AS cv ON cl.city_id = cv.cv_city_id AND cv.cv_variable_id = {$varId}" );
+
+					if ( is_integer( $hubId ) ) {
+						$queryParts[] = "LEFT JOIN city_cat_mapping AS ccm ON cl.city_id = ccm.city_id";
+					}
+
+					$queryParts[] = "WHERE cl.city_public = 1";
+
+					if ( !empty( $lang ) ) {
+						$lang = mysql_real_escape_string( $lang );
+						$queryParts[] = "AND cl.city_lang = '{$lang}'";
+					}
+
+					$queryParts[] = "AND (cl.city_title LIKE '%{$keyword}%' OR cv.cv_value LIKE '%{$keyword}%')";
+
+					if ( is_integer( $hubId ) ) {
+						$queryParts[] = "AND ccm.cat_id = {$hubId}";
+					}
+
+					$queryParts[] = 'LIMIT ' . self::MAX_FIND_WIKIS_RESULTS;
+
+
+					//manual query as the DataBase class doesn't allow for LEFT JOIN
+					//which is required in this specific case
+					$rows = $db->query( implode( ' ', $queryParts ), __METHOD__ );
+
+					while ( $row = $db->fetchObject( $rows ) ) {
+						$wikis[] = array(
+							'id' => $row->id,
+							'name' => $row->name,
+							//getting this as a separate query since it would require
+							//a double join, that would complicate the situation with
+							//the query's performances
+							'hub' => ( !empty( $hub ) ) ? $hub : $this->getWikiHubById( $row->id ),
+							'language' => $row->lang,
+							//WF stores strings as serialized data
+							'topic' => ( !empty( $row->topic ) ) ? unserialize( $row->topic ) : null,
+							//getting this as a separate query since it would require
+							//a double join, that would complicate the situation with
+							//the query's performances
+							'domain' => $this->getWikiDomainById( $row->id )
+						);
+					}
+
+					$this->app->wg->Memc->set( $memKey, $wikis, 86400 /* 24h */ );
+				}
+			}
+		}
+
+		foreach ( $this->wf->PaginateArray( $wikis, $limit, $batch ) as $name => $value ) {
+			$this->response->setVal( $name, $value );
+		}
+
+		//store only for 24h to allow new wikis we want to push
+		//to appear in a reasonable amount of time in the search
+		//results
+		$this->response->setCacheValidity(
+			86400 /* 24h */,
+			86400 /* 24h */,
+			array(
+				WikiaResponse::CACHE_TARGET_BROWSER,
+				WikiaResponse::CACHE_TARGET_VARNISH
+			)
+		);
+
+		$this->wf->profileOut( __METHOD__ );
+	}
+
+	private function getWikiDomainById( $wikiId ){
+		return str_replace( 'http://', '', WikiFactory::getVarValueByName( 'wgServer', $wikiId ) );
+	}
+
+	private function getWikiHubById( $wikiId ){
+		return WikiFactory::getCategory( $wikiId )->cat_name;
 	}
 }
