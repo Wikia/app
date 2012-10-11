ceef81a8b2dc2c10c1d888f6dfd25e86518f18ec
jolek <jolek@wikia-inc.com>
2012-10-11 02:37:08 +0000

    GameGuides: adding Photos.getList and handling clicking a link
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61530 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/GameGuides/js/GameGuides.js
+++ extensions/wikia/GameGuides/js/GameGuides.js
@@ -1,26 +1,47 @@
-$(function(){
-	var html = document.documentElement;
+(function(html, w){
+	var server = new RegExp(wgServer + '/(wiki\/)?', 'i');
 
-	require(['querystring'], function(qs){
-		var links = Array.prototype.slice.call(document.getElementsByTagName('a')),
-			l = links.length,
-			link,
-			q;
+	//handling clicking on a link
+	html.addEventListener('click', function(ev){
+		var t = ev.target;
 
-		while(l--){
-			link = links[l];
-			q = new qs(link.href);
-			var path = q.getPath();
-			if(q.getPath().indexOf('/wiki/') > -1 && path.indexOf(':') == -1) {
-				link.href = wgServer + '/wikia.php?controller=GameGuidesController&method=renderFullPage&title=' + path.slice(6);
-				link.className += ' validLink';
-			} else if(link.className.indexOf('image') > -1) {
-				link.className += ' validLink';
-			}
+		if(t.tagName == 'A') {
+			ev.preventDefault();
+			Ponto.invoke(
+				'Linker',
+				'goTo',
+				{
+					link: t.href.replace(server, '')
+				}
+			);
 		}
 	});
 
-	window.changeFontType = function(){
+	//handling grabing all links on a page;
+	function Photos(){
+		this.getList = function(){
+			require(['media'], function(m){
+				var images = m.getImages(),
+					links = [];
+
+				for(var i = 0, l = images.length; i < l; i++){
+					links[i] = images[i].url;
+				}
+
+				return links;
+			});
+		};
+	}
+
+	Ponto.PontoBaseHandler.derive(Photos);
+
+	Photos.getInstance = function(){
+		return new Photos();
+	};
+
+	window.Photos = Photos;
+
+	w.changeFontType = function(){
 		if(html.className.indexOf('serif') > -1) {
 			html.className = html.className.replace(' serif', '');
 		}else{
@@ -28,7 +49,33 @@ $(function(){
 		}
 	};
 
-	window.setFontSize = function(size){
+	w.setFontSize = function(size){
 		html.style.fontSize = Math.max(Math.min(~~size, 200), 50) + '%';
-	}
-});
+	};
+
+	$(function(){
+		var	links = Array.prototype.slice.call(document.getElementsByTagName('a')),
+			l = links.length,
+			link;
+
+		while(link = links[l--]){
+			if(link.href.match(server)) {
+				link.className += ' validLink';
+			}
+		}
+
+		require(['toc'], function(toc){
+			Ponto.invoke(
+				'Article',
+				'date',
+				{
+					data: {
+
+					},
+					toc: toc.getList()
+				}
+			);
+		});
+	});
+})(document.documentElement, this);
+
