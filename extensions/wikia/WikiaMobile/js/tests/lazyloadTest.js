5d2a8bf1baeaf570822a0cd7a60a2fa317a61bae
jolek <jolek@wikia-inc.com>
2012-10-11 02:38:06 +0000

    WikiaMobile: small fix for lazyload test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61546 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiaMobile/js/tests/lazyloadTest.js
+++ extensions/wikia/WikiaMobile/js/tests/lazyloadTest.js
@@ -2,13 +2,26 @@
  @test-framework Jasmine
  @test-require-asset /resources/wikia/libraries/modil/modil.js
  @test-require-asset /resources/wikia/modules/thumbnailer.js
+ @test-require-asset /resources/wikia/modules/querystring.js
+ @test-require-asset /extensions/wikia/WikiaTracker/js/WikiaTracker.js
+ @test-require-asset /extensions/wikia/WikiaMobile/js/track.js
+ @test-require-asset /extensions/wikia/WikiaMobile/js/events.js
+ @test-require-asset /extensions/wikia/WikiaMobile/js/loader.js
+ @test-require-asset /extensions/wikia/WikiaMobile/js/modal.js
+ @test-require-asset /extensions/wikia/WikiaMobile/js/sections.js
+ @test-require-asset /extensions/wikia/WikiaMobile/js/media.js
+ @test-require-asset /extensions/wikia/WikiaMobile/js/layout.js
  @test-require-asset /extensions/wikia/WikiaMobile/js/lazyload.js
  */
 
 /*global describe, it, runs, waitsFor, expect, require, document*/
 describe("Test lazyload module", function () {
 	'use strict';
+
 	var async = new AsyncSpec(this);
+
+	window.Features = {};
+	window.wgStyleVersion = 123;
 
 	async.it('should be defined', function(done){
 		require(['lazyload'], function(lazyload){
