0d8334f36e02cd7ab4c5d3308366b000e12d17b6
marzjan <sebastianm@wikia-inc.com>
2012-10-12 08:31:19 +0000

    BugId: 49364 - In Wiki External Form Extension, WIP
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61587 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/InWikiExternalForm/InWikiExternalFormController.class.php
+++ extensions/wikia/InWikiExternalForm/InWikiExternalFormController.class.php
@@ -8,6 +8,7 @@ class InWikiExternalFormController  extends WikiaSpecialPageController {
 	public function index() {
 		$this->wg->SuppressPageHeader = true;
 		$this->wg->SuppressSpotlights = true;
+		$this->formUrl = WikiFactory::getVarValueByName('wgInWikiExternalFormUrl',$this->wg->cityId);
 		$this->response->addAsset("extensions/wikia/InWikiExternalForm/js/InWikiExternalForm.js");
 	}
 }
