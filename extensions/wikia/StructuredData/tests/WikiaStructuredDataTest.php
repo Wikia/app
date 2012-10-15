7fe2aa56b29211257b8d59cacce6bc9d35e99598
mech <mech@wikia-inc.com>
2012-10-15 12:25:23 +0000

    StructuredData: SDElement unit test
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61692 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StructuredData/tests/WikiaStructuredDataTest.php
+++ extensions/wikia/StructuredData/tests/WikiaStructuredDataTest.php
@@ -4,7 +4,6 @@
  * User: mech
  * Date: 10/15/12
  * Time: 12:29 PM
- * To change this template use File | Settings | File Templates.
  */
 
 class WikiaStructuredDataTest extends WikiaBaseTest {
@@ -86,9 +85,17 @@ class WikiaStructuredDataTest extends WikiaBaseTest {
 
 	private $codCharacterManson = '{"@context":"/contexts/callofduty.jsonld","cod:rank":"Captain","schema:birthDate":"1999-6-3","schema:dateCreated":"2012-08-20T21:47:58Z","schema:dateModified":"2012-08-20T21:47:58Z","schema:description":"Mason was born on June 3, 1933 and raised in Fairbanks, Alaska; the geographical proximity of his home state to Communist Russia brought the threat of the Cold War much closer to his doorstep than most Americans. This was a key contributor to his fervent anti-Communist ideals and eventually led to his enlistment in the USMC. He spent his youth hunting elk and grizzlies in the Alaskan wilderness with his father (a WWII veteran and recipient of the Purple Heart for wounds received during action at the Makin Atoll); as such, he is adept in cold weather environments and is an excellent sniper; at the age of 23 he became the youngest Wimbledon Cup winner in the history of the marksmanship competition.  He was recruited to SAD/SOG at the age of 25 after an exemplary term of service with the Marines. Having served with distinction since joining the Agency, Mason has a long standing friendship with many of his fellow Agency colleagues and shows a particular kinship with operative, Frank Woods. Always itching for action, Mason\'s tendency towards impulsiveness is the one trait that may not have been completely \"trained out of him.\" However, his close friend Woods is well aware of this, and knows exactly when and how to rein him in.","schema:name":"Alex Mason","type":"cod:Character","wikia:characterIn":[{"id":"500f28856e7f187196000001"},{"id":"50258194ac50ed4616000005"},{"id":"50258162ac50ed4616000004"},{"id":"500f1a1b6e7f1827ba000001"}],"wikia:includeWith":[{"id":"500f28856e7f187196000001"},{"id":"500f44556e7f18f7ef000001"}],"wikia:playable":"true","id":"501849c96e7f187909000001"}';
 
+	private function getPropertyByLabel($array, $schemaName) {
+		foreach($array['properties'] as $v) {
+			if ($v['label'] == $schemaName) return $v;
+		}
+		return null;
+	}
+	/**
+	 * Check the basic SDElement features
+	 */
 	public function testSDElement() {
-
-		$apiClient =  $this->getMock( 'StructuredDataAPIClient', array('getTemplate', 'getContext', 'getObject'), array('http://localhost/', '', '') );
+		$apiClient =  $this->getMock( 'StructuredDataAPIClient', array('getTemplate', 'getContext', 'getObject'), array('http://localhost/', 'api/v0/', 'callofduty') );
 		$apiClient->expects( $this->any() )
 			->method( 'getTemplate' )
 			->will( $this->returnValue( json_decode( $this->codCharacterTemplate ) ) );
@@ -107,5 +114,11 @@ class WikiaStructuredDataTest extends WikiaBaseTest {
 		$sdElement = $structuredData->getSDElement( $id );
 		$this->isInstanceOf( 'SDElement', $sdElement );
 		$this->assertEquals( $sdElement->getId(), $id );
+		$this->assertEquals( $sdElement->getType(), 'cod:Character' );
+		$a = $sdElement->toArray();
+		$description = $this->getPropertyByLabel($a, 'description' );
+
+		$this->assertEquals( $description['type'], 'rdf:Literal' );
+		$this->assertEquals( $description['name'], 'schema:description' );
 	}
 }
