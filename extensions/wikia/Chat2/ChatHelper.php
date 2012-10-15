4a70fa0f6965ed23f0a19aad3193bdff58e8bf29
wladek <wladek@wikia-inc.com>
2012-10-15 11:48:06 +0000

    disable ChatEntryPoint on edit pages in Oasis
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61688 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Chat2/ChatHelper.php
+++ extensions/wikia/Chat2/ChatHelper.php
@@ -168,7 +168,8 @@ class ChatHelper {
 			}
 		}
 
-		if ( F::app()->checkSkin( 'oasis' ) ){
+		// don't load ChatEntryPoint on edit pages (perf optimization)
+		if ( F::app()->checkSkin( 'oasis' ) && !BodyController::isEditPage() ){
 			// TODO: move these to asset manager when we release chat globally
 			$out->addScriptFile($wgResourceBasePath . '/resources/wikia/libraries/bootstrap/popover.js');
 			$out->addScriptFile($wgExtensionsPath . '/wikia/Chat2/js/ChatEntryPoint.js');
