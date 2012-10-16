88682a4ec144ae2eacc38d0d96b6ff3c47a2cc0c
macbre <macbre@wikia-inc.com>
2012-10-16 16:55:19 +0000

    WikiEvolution: use /tmp for PPM file
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61795 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiEvolution/LocalRenderer.sh
+++ extensions/wikia/WikiEvolution/LocalRenderer.sh
@@ -10,11 +10,12 @@ elasticity=0.05
 framerate=25
 bitrate=4000K
 extension=avi
+ppm=/tmp/$wikiname.ppm
 
 echo "Running gource..."
-xvfb-run -a -s "-screen 0 1360x768x24" gource -$resolution --log-format custom $wikiname/gource.log --seconds-per-day $seconds_per_day --auto-skip-seconds $auto_skip_seconds --multi-sampling --stop-at-end --elasticity $elasticity -b 000000  --hide filenames,dirnames,progress --user-friction .2 --background-image background_logo.png --logo  $wikiname/wordmark.png --user-image-dir $wikiname/avatars --output-ppm-stream $wikiname/$wikiname.ppm --output-framerate $framerate
+xvfb-run -a -s "-screen 0 1360x768x24" gource -$resolution --log-format custom $wikiname/gource.log --seconds-per-day $seconds_per_day --auto-skip-seconds $auto_skip_seconds --multi-sampling --stop-at-end --elasticity $elasticity -b 000000  --hide filenames,dirnames,progress --user-friction .2 --background-image background_logo.png --logo  $wikiname/wordmark.png --user-image-dir $wikiname/avatars --output-ppm-stream $ppm --output-framerate $framerate
 
 echo "Generating movie..."
-avconv -y -r $framerate -f image2pipe -vcodec ppm -i $wikiname/$wikiname.ppm -b $bitrate $wikiname/$wikiname.$extension
+avconv -y -r $framerate -f image2pipe -vcodec ppm -i $ppm -b $bitrate $wikiname/$wikiname.$extension
 
 echo "Done."
