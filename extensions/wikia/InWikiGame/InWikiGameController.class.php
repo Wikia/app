a7c384ca19749c31fd70f441045a6ce713e0a521
marcin <marcin@wikia-inc.com>
2012-10-16 14:59:04 +0000

    Removing InWikiGame scss file (we dont need it anymore)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61789 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/InWikiGame/InWikiGameController.class.php
+++ extensions/wikia/InWikiGame/InWikiGameController.class.php
@@ -11,7 +11,7 @@ class InWikiGameController extends WikiaController {
 	public function executeIndex() {
 		$this->gameId = $this->getVal('inWikiGameId', 1);
 		$this->jsSnippet = F::build('JSSnippets')->addToStack(
-			array('/extensions/wikia/InWikiGame/js/InWikiGame.js', '/extensions/wikia/InWikiGame/css/InWikiGame.scss'),
+			array('/extensions/wikia/InWikiGame/js/InWikiGame.js'),
 			null,
 			'InWikiGame.init',
 			array(
