03324ac3941e83709629ef5be1b5ee9d6eff42e8
rafal <rafal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-10-17 14:43:38 +0000

    renderer template for ImageObject update
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61883 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StructuredData/templates/renderers/ImageObject.php
+++ extensions/wikia/StructuredData/templates/renderers/ImageObject.php
@@ -1,4 +1,12 @@
-<img src="<?= $object->getProperty('schema:contentURL')->getValue(); ?>" />
 <?php if($context == SD_CONTEXT_SPECIAL): ?>
-	IT'S SPECIAL PAGE!
+	<?php $imgSrc = $object->getPropertyValue('schema:contentURL', false); ?>
+	<figure>
+		<?php if ($imgSrc != false) : ?>
+			<img src="<?= $imgSrc ?>" />
+		<?php endif; ?>
+		<figcaption>Lorem ipsum</figcaption>
+	</figure>
+	<a href="" title="">File Page Link</a>
+<?php else : ?>
+	<img src="<?= $object->getPropertyValue('schema:contentURL', false); ?>" />
 <?php endif; ?>
