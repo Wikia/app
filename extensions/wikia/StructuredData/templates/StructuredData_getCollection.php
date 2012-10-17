39d0d392b277ad229625e9b2449f34ca70e0932c
rafal <rafal@e846bd91-c042-42a7-a218-4b65e2c1904d>
2012-10-17 15:37:15 +0000

    fix url - string in array bug
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61896 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/StructuredData/templates/StructuredData_getCollection.php
+++ extensions/wikia/StructuredData/templates/StructuredData_getCollection.php
@@ -1,9 +1,12 @@
-
 <ul>
 <?php foreach ( $list as $obj ) { ?>
 	<li>
 		<?php if(!empty($obj['url'])): ?>
-			<a href="/wiki/Special:StructuredData/<?=$obj['url']?>">
+			<?php if (is_array($obj['url'])) : ?>
+				<a href="/wiki/Special:StructuredData/<?=$obj['url'][0]?>">
+			<?php else : ?>
+				<a href="/wiki/Special:StructuredData/<?=$obj['url']?>">
+			<? endif; ?>
 		<?php else: ?>
 			<a href="?method=showObject&id=<?=$obj['id']?>">
 		<?php endif; ?>
