4e7679f6104c8c6049820fd971b6fc7de680d6c9
macbre <macbre@wikia-inc.com>
2012-10-18 15:24:36 +0000

    WikiEvolution: use /tmp for output movie
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61954 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/WikiEvolution/LocalRendererDo.sh
+++ extensions/wikia/WikiEvolution/LocalRendererDo.sh
@@ -10,4 +10,4 @@ framerate=25
 bitrate=4000K
 extension=avi
 
-gource -$resolution --log-format custom $wikiname/gource.log --seconds-per-day $seconds_per_day --auto-skip-seconds $auto_skip_seconds --multi-sampling --stop-at-end --elasticity $elasticity -b 000000  --hide filenames,dirnames,progress --user-friction .2 --background-image background_logo.png --logo  $wikiname/wordmark.png --user-image-dir $wikiname/avatars --output-ppm-stream - --output-framerate $framerate | avconv -y -r $framerate -f image2pipe -vcodec ppm -i - -b $bitrate $wikiname/$wikiname.$extension
+gource -$resolution --log-format custom $wikiname/gource.log --seconds-per-day $seconds_per_day --auto-skip-seconds $auto_skip_seconds --multi-sampling --stop-at-end --elasticity $elasticity -b 000000  --hide filenames,dirnames,progress --user-friction .2 --background-image background_logo.png --logo  $wikiname/wordmark.png --user-image-dir $wikiname/avatars --output-ppm-stream - --output-framerate $framerate | avconv -y -r $framerate -f image2pipe -vcodec ppm -i - -b $bitrate /tmp/$wikiname.$extension
