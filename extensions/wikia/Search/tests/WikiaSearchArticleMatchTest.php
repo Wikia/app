a5acbd4aef4a5ca88138a0eb04e9c3fb241d3dff
macbre <macbre@wikia-inc.com>
2012-10-18 12:54:28 +0000

    Trying to @cover not existing method "WikiaSearchArticleMatch::getOriginalArticle" (BugId:51123)
    git-svn-id: svn+ssh://svn.wikia-code.com/svn/wikia/trunk@61947 e846bd91-c042-42a7-a218-4b65e2c1904d

--- extensions/wikia/Search/tests/WikiaSearchArticleMatchTest.php
+++ extensions/wikia/Search/tests/WikiaSearchArticleMatchTest.php
@@ -1,67 +1,67 @@
-<?php 
+<?php
 
 require_once( 'WikiaSearchBaseTest.php' );
 
 class WikiaSearchArticleMatchTest extends WikiaSearchBaseTest {
-	
+
 	/**
 	 * @covers WikiaSearchArticleMatch::__construct
 	 * @covers WikiaSearchArticleMatch::hasRedirect
 	 * @covers WikiaSearchArticleMatch::getArticle
 	 * @covers WikiaSearchArticleMatch::getRedirect
-	 * @covers WikiaSearchArticleMatch::getOriginalArticle
+	 * @covers WikiaSearchArticleMatch::getCanonicalArticle
 	 */
 	public function testArticleMatchCanonical() {
-		
+
 		$mockTitle		= $this->getMock( 'Title' );
 		$mockArticle	= $this->getMock( 'Article', array( 'isRedirect', 'getRedirectTarget' ), array( $mockTitle ) );
-		
+
 		$mockArticle
 			->expects	( $this->any() )
 			->method	( 'isRedirect' )
 			->will		( $this->returnValue( false ) )
 		;
-		
+
 		$articleMatch = F::build( 'WikiaSearchArticleMatch', array( $mockArticle ) );
-		
+
 		$this->assertInstanceOf(
 				'Article',
 				$articleMatch->getArticle(),
 				'WikiaSearchArticleMatch should return the original article, canonical or not, when calling getArticle().'
 		);
-		
+
 		$this->assertEquals(
 				false,
 				$articleMatch->hasRedirect(),
 				'WikiaSearchArticleMatch::hasRedirect should return false if it does not have a redirect.'
 		);
-		
+
 		$this->assertNull(
 				$articleMatch->getRedirect(),
 				'WikiaSearchArticleMatch::getRedirect should return null if the article passed during construction was canonical.'
 		);
-		
+
 		$this->assertEquals(
 				$articleMatch->getCanonicalArticle(),
 				$articleMatch->getArticle(),
 				'A canonical article as the constructor param should be returned as both canonical and main article in WikiaSearchArticleMatch.'
 		);
-		
+
 	}
-	
+
 	/**
 	 * @covers WikiaSearchArticleMatch::__construct
 	 * @covers WikiaSearchArticleMatch::hasRedirect
 	 * @covers WikiaSearchArticleMatch::getArticle
 	 * @covers WikiaSearchArticleMatch::getRedirect
-	 * @covers WikiaSearchArticleMatch::getOriginalArticle
+	 * @covers WikiaSearchArticleMatch::getCanonicalArticle
 	 */
 	public function testArticleMatchRedirect() {
 		$mockTitle		= $this->getMock( 'Title' );
 		$mockArticle	= $this->getMock( 'Article', array( 'isRedirect', 'getRedirectTarget' ), array( $mockTitle ) );
 		$mockRedirect	= $this->getMock( 'Article', array(), array( $mockTitle ) );
 		$this->mockClass( 'Article', $mockRedirect );
-		
+
 		$mockArticle
 			->expects	( $this->any() )
 			->method	( 'isRedirect' )
@@ -72,46 +72,46 @@ class WikiaSearchArticleMatchTest extends WikiaSearchBaseTest {
 			->method	( 'getRedirectTarget' )
 			->will		( $this->returnValue( $mockTitle ) )
 		;
-		
+
 		$articleMatch = F::build( 'WikiaSearchArticleMatch', array( $mockArticle ) );
-		
+
 		$this->assertInstanceOf(
 		        'Article',
 		        $articleMatch->getCanonicalArticle(),
 		        'WikiaSearchArticleMatch should return an article when calling getCanonicalArticle().'
 		);
-		
+
 		$this->assertInstanceOf(
 				'Article',
 				$articleMatch->getArticle(),
-				'WikiaSearchArticleMatch should return the original article when calling getArticle().' 
+				'WikiaSearchArticleMatch should return the original article when calling getArticle().'
 		);
-		
+
 		$this->assertEquals(
 				true,
 				$articleMatch->hasRedirect(),
-				'WikiaSearchArticleMatch::hasRedirect should true if the article passed during construction was a redirect.' 
+				'WikiaSearchArticleMatch::hasRedirect should true if the article passed during construction was a redirect.'
 		);
-		
+
 		$this->assertInstanceOf(
 				'Article',
 				$articleMatch->getRedirect(),
 				'WikiaSearchArticleMatch::getRedirect should return an article if the article match had a redirect.'
 		);
-		
+
 		$this->assertNotEquals(
 				$articleMatch->getArticle(),
 				$articleMatch->getCanonicalArticle(),
 				'A non-canonical original article match should default to the redirect when calling getCanonicalArticle.'
 		);
-		
+
 		$this->assertEquals(
-				$articleMatch->getRedirect(), 
+				$articleMatch->getRedirect(),
 				$articleMatch->getCanonicalArticle(),
 				'A non-canonical original article match should default to the redirect when calling getCanonicalArticle.'
 		);
-		
-		
+
+
 	}
-	
+
 }
