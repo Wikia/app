deceb4f861e4280416b2e4feb9da4dcbbe4e265a
Piotr Gabryjeluk <piotr@wikidot.com>
2012-10-22 22:39:50 +0200

    BugId:31652 - Adding AdEngine's exitstitial infobox

---
+++ extensions/wikia/AdEngine/AdEngine.i18n.php
@@ -0,0 +1,20 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'adengine-exitstitial-title-template' => 'You\'re leaving $1...',
+	'adengine-exitstitial-redirecting' => 'Your browser will redirect you to the page you requested in a few seconds.',
+	'adengine-exitstitial-go-back' => 'Want to go back?',
+	'adengine-exitstitial-button' => 'Skip this Ad',
+);
+
+/**
+ * Message documentation
+ */
+$messages['qqq'] = array(
+	'adengine-exitstitial-title-template' => 'Title of the box showing up when you click on link to outside the wiki (site title is $1)',
+	'adengine-exitstitial-redirecting' => 'Text displayed in the box showing up when you click on link to outside the wiki',
+	'adengine-exitstitial-go-back' => 'Text of the link/button that you can close the box with and get back to the content',
+	'adengine-exitstitial-button' => 'Text of the link/button that you can close the box with and get directly to the link target',
+);
