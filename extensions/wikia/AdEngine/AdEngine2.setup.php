29e7b9197349f18475a9f129038559e7e18b163e
Piotr Gabryjeluk <piotr@wikidot.com>
2012-10-22 22:39:50 +0200

    BugId:31652 - Adding AdEngine's exitstitial infobox (backported from dev)

--- extensions/wikia/AdEngine/AdEngine2.setup.php
+++ extensions/wikia/AdEngine/AdEngine2.setup.php
@@ -1,11 +1,17 @@
 <?php
 
-$dir = dirname(__FILE__);
-
 $app = F::app();
 
-$app->registerClass('AdEngine2Controller', "$dir/AdEngine2Controller.class.php");
+$app->registerClass('AdEngine2Controller', __DIR__ . '/AdEngine2Controller.class.php');
 
 $app->registerHook('WikiaSkinTopScripts', 'AdEngine2Controller', 'onWikiaSkinTopScripts');
 $app->registerHook('OasisSkinAssetGroupsBlocking', 'AdEngine2Controller', 'onOasisSkinAssetGroupsBlocking');
 $app->registerHook('OasisSkinAssetGroups', 'AdEngine2Controller', 'onOasisSkinAssetGroups');
+
+$app->registerExtensionMessageFile('AdEngine', __DIR__ . '/AdEngine.i18n.php');
+
+// TODO: how to do it better?
+$wgExtensionFunctions[] = 'wfAdEngineInitJSMessages';
+function wfAdEngineInitJSMessages() {
+	F::build('JSMessages')->registerPackage('AdEngine', array('adengine-*'));
+}
