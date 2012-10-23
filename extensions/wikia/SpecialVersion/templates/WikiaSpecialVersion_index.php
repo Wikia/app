45f0085f9f5d6b228406da75e69aaffc061bafd9
Robert Elwell <robert@wikia-inc.com>
2012-10-23 21:54:11 +0000

    bugid: 51370 -- implement special:version view based on values passed through controller

---
+++ extensions/wikia/SpecialVersion/templates/WikiaSpecialVersion_index.php
@@ -0,0 +1,9 @@
+<h2 id='mw-version-license'><?=$versionLicense?></h2>
+<div>
+	<?= $copyRightAndAuthorList ?>
+	<?= $versionLicenseInfo ?>
+</div>
+<?= $softwareInformation ?>
+<?= $extensionCredit ?>
+<!-- visited from <?= $ip ?> -->
+<span style='display:none'>visited from <?= $ip ?></span>
