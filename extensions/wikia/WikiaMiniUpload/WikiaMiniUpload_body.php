facf07ccddd827a622b221700bac4e8caeeea79c
saipetch <saipetch@wikia-inc.com>
2012-10-23 10:47:55 -0700

    bugid-55085: update WikiaMiniUpload

--- extensions/wikia/WikiaMiniUpload/WikiaMiniUpload_body.php
+++ extensions/wikia/WikiaMiniUpload/WikiaMiniUpload_body.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * @author Inez Korczyński
- * @author Bartek Łapiński
+ * @author Inez KorczyÅ„ski
+ * @author Bartek Å�apiÅ„ski
  */
 
 class WikiaMiniUpload {
@@ -379,6 +379,7 @@ class WikiaMiniUpload {
 
 		$extraId = $wgRequest->getVal('extraId');
 		$newFile =  true;
+		$file = null;
 
 		if($name !== NULL) {
 			$name = urldecode( $name );
@@ -527,7 +528,10 @@ class WikiaMiniUpload {
 			$title = Title::newFromText($mwname, 6);
 		}
 
-		$file = wfFindFile($title);
+		if ( is_null($file) ) {
+			$file = wfFindFile( $title );
+		}
+
 		if (!is_object($file)) {
 			header('X-screen-type: error');
 			return 'File was not found!';
