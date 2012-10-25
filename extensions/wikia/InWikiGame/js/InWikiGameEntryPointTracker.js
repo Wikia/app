ec2b06576b829c55b8c5bc0c050a16641d1f59e8
nAndy <nandy@wikia-inc.com>
2012-10-25 11:10:47 +0000

    remove a typo in class name, added tracking for main page

--- extensions/wikia/InWikiGame/js/InWikiGameEntryPointTracker.js
+++ extensions/wikia/InWikiGame/js/InWikiGameEntryPointTracker.js
@@ -1,8 +1,9 @@
 var InWikiGameEntryPointTracker = {
 	ENTRY_POINT_STORAGE_KEY: 'IN_WIKI_GAME_ENTRY_POINT',
 	init: function() {
-		$('.InWikIGameRailModule a').click(this.onEntryPointClick);
+		$('.InWikiGameRailModule a').click(this.onEntryPointClick);
 		$('.WikiNav .subnav-2a').click(this.onEntryPointClick);
+		$('.InWikiGameMainPageRailModule').click(this.onEntryPointClick);
 	},
 	onEntryPointClick: function(event) {
 		var target = $(event.target);
@@ -10,6 +11,8 @@ var InWikiGameEntryPointTracker = {
 
 		if( target.hasClass('in-wiki-game-rail-link') ) {
 			localStorageValue = 'railLink';
+		} else if( target.closest('div').hasClass('InWikiGameMainPageRailModule') ) {
+			localStorageValue = 'mainPageRailLink';
 		} else if( target.attr('href') == '/wiki/Play' ) {
 		//TODO: investigate and implement if possible adding custom CSS class in WikiaNav then change the condition above
 			localStorageValue = 'navLink';
