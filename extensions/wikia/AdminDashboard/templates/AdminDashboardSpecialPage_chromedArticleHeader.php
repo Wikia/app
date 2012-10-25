d71e28bee7f4245a1b23e594ae34a11c75c90ddb
Kamil Koterba <kamil@wikia-inc.com>
2012-10-25 19:16:03 +0000

    BugId:12889 Watchlist links to edit in Admin Dashboard

--- extensions/wikia/AdminDashboard/templates/AdminDashboardSpecialPage_chromedArticleHeader.php
+++ extensions/wikia/AdminDashboard/templates/AdminDashboardSpecialPage_chromedArticleHeader.php
@@ -1,3 +1,13 @@
 <div class="AdminDashboardGeneralHeader AdminDashboardArticleHeader">
 	<h1><?= $headerText ?></h1>
 </div>
+<div class="AdminDashboardStub">
+        <? if ( !empty($tagline) ) : ?>
+            <h3 id="siteSub"><?= $tagline ?></h3>
+        <? endif; ?>
+        <? if ( !empty($subtitle) ) : ?>
+            <div id="contentSub">
+                 <?= $subtitle ?>
+            </div>
+        <? endif; ?>
+</div>
