f5a4a33f5afaac28130a69bf91c228a17e6eb5f3
liz <liz@wikia-inc.com>
2012-10-25 00:37:00 +0000

    bugid-25459 clearing mini editor's textarea if the user starts typing before it's all loaded

--- extensions/wikia/EditPageLayout/js/plugins/MiniEditor.js
+++ extensions/wikia/EditPageLayout/js/plugins/MiniEditor.js
@@ -39,7 +39,7 @@
 			this.buttonsWrapper = this.editor.getSpace('buttons');
 
 			if (this.buttonsWrapper.length) {
-				this.buttons = this.buttonsWrapper.find('button');
+				this.buttons = this.buttonsWrapper.find('button, input[submit]');
 			}
 		},
 
@@ -162,6 +162,12 @@
 
 			// Trigger editorActivated
 			this.editor.fire('editorActivated', event);
+
+			var replacedElement = this.editor.getEditorElement();
+			if(replacedElement.is('textarea') && typeof this.editor.ck != "undefined") {
+				replacedElement.val("");
+			}
+
 		},
 
 		editorReset: function() {
