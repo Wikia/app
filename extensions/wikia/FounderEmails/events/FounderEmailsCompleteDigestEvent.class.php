66ce3d2ec9a1acf758b32a59766f8431f76c324e
saipetch <saipetch@wikia-inc.com>
2012-10-26 12:34:01 -0700

    bugid-11808: FounderEmails - send complete digest and views digest emails for i18n wikis

--- extensions/wikia/FounderEmails/events/FounderEmailsCompleteDigestEvent.class.php
+++ extensions/wikia/FounderEmails/events/FounderEmailsCompleteDigestEvent.class.php
@@ -53,22 +53,19 @@ class FounderEmailsCompleteDigestEvent extends FounderEmailsEvent {
 				self::addParamsUser($cityID, $user->getName(), $emailParams);
 
 				$langCode = $user->getOption( 'language' );
-				// Only send digest emails for English users until translation is done
-				if ($langCode == 'en') {
-					$links = array(
-						'$WIKINAME' => $emailParams['$WIKIURL'],
-					);
+				$links = array(
+					'$WIKINAME' => $emailParams['$WIKIURL'],
+				);
 
-					$mailSubject = strtr(wfMsgExt('founderemails-email-complete-digest-subject', array('content')), $emailParams);
-					$mailBody = strtr(wfMsgExt('founderemails-email-complete-digest-body', array('content','parsemag'), $emailParams['$UNIQUEVIEWS'], $emailParams['$USEREDITS'], $emailParams['$USERJOINS']), $emailParams);
-					$mailBodyHTML = F::app()->renderView("FounderEmails", "GeneralUpdate", array_merge($emailParams, array('language' => 'en', 'type' => 'complete-digest')));
-					$mailBodyHTML = strtr($mailBodyHTML, FounderEmails::addLink($emailParams,$links));
-					$mailCategory = FounderEmailsEvent::CATEGORY_COMPLETE_DIGEST.(!empty($langCode) && $langCode == 'en' ? 'EN' : 'INT');
+				$mailSubject = strtr(wfMsgExt('founderemails-email-complete-digest-subject', array('content')), $emailParams);
+				$mailBody = strtr(wfMsgExt('founderemails-email-complete-digest-body', array('content','parsemag'), $emailParams['$UNIQUEVIEWS'], $emailParams['$USEREDITS'], $emailParams['$USERJOINS']), $emailParams);
+				$mailBodyHTML = F::app()->renderView("FounderEmails", "GeneralUpdate", array_merge($emailParams, array('language' => 'en', 'type' => 'complete-digest')));
+				$mailBodyHTML = strtr($mailBodyHTML, FounderEmails::addLink($emailParams,$links));
+				$mailCategory = FounderEmailsEvent::CATEGORY_COMPLETE_DIGEST.(!empty($langCode) && $langCode == 'en' ? 'EN' : 'INT');
 
-					// Only send email if there is some kind of activity to report
-					if ($emailParams['$UNIQUEVIEWS'] > 0 || $emailParams['$USERJOINS'] > 0 || $emailParams['$USEREDITS'] > 0 ) {
-						$founderEmailObj->notifyFounder( $user, $this, $mailSubject, $mailBody, $mailBodyHTML, $cityID, $mailCategory );
-					}
+				// Only send email if there is some kind of activity to report
+				if ($emailParams['$UNIQUEVIEWS'] > 0 || $emailParams['$USERJOINS'] > 0 || $emailParams['$USEREDITS'] > 0 ) {
+					$founderEmailObj->notifyFounder( $user, $this, $mailSubject, $mailBody, $mailBodyHTML, $cityID, $mailCategory );
 				}
 			}
 		}
