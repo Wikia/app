f02c4518c5e3ff02920d7f4bc4496ca4ca1a9824
Piotr Gabryjeluk <rychu@wikia-inc.com>
2012-10-29 20:39:07 +0100

    Fix continued

--- extensions/wikia/AdEngine/js/AdEngine2.js
+++ extensions/wikia/AdEngine/js/AdEngine2.js
@@ -15,7 +15,9 @@ var AdEngine2 = function(log, LazyQueue) {
 			log('fillInSlot', 5, module);
 			log(slot, 5, module);
 
-			var provider = adConfig.getProvider(slot);
+			var slotname = slot[0]
+				, provider = adConfig.getProvider(slot)
+			;
 
 			log('calling ' + provider.name + '.fillInSlot for ' + slotname, 3, module);
 
