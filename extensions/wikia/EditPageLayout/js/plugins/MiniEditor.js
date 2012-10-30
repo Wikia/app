0c5dbc7abdecb03b2ed600ed31324bc5022f71a8
Marcin Maciejewski <marcin@wikia-inc.com>
2012-10-30 15:38:43 +0000

    bugid-51633: Preview - Unable to post message on message wall which contains only photo/video

--- extensions/wikia/EditPageLayout/js/plugins/MiniEditor.js
+++ extensions/wikia/EditPageLayout/js/plugins/MiniEditor.js
@@ -83,6 +83,10 @@
 			this.editor.element.addClass('active');
 		},
 
+		editorButtonsActivate: function() {
+			this.buttons.removeAttr('disabled');
+		},
+
 		editorAfterActivated: function() {
 			this.showButtons();
 			this.showToolbar();
@@ -95,23 +99,13 @@
 
 			this.editor.getEditbox().on(
 				'keyup',
-				$.proxy(function() {
-					this.buttons.removeAttr('disabled');
-				}, this)
+				$.proxy(this.editorButtonsActivate, this)
 			);
 			this.editor.on({
-				mode: $.proxy(function() {
-					this.buttons.removeAttr('disabled');
-				}, this),
-				editorAddImage: $.proxy(function() {
-					this.buttons.removeAttr('disabled');
-				}, this),
-				editorAddVideo: $.proxy(function() {
-					this.buttons.removeAttr('disabled');
-				}, this),
-				editorAddLink: $.proxy(function() {
-					this.buttons.removeAttr('disabled');
-				}, this)
+				mode: $.proxy(this.editorButtonsActivate, this),
+				editorAddImage: $.proxy(this.editorButtonsActivate, this),
+				editorAddVideo: $.proxy(this.editorButtonsActivate, this),
+				editorAddLink: $.proxy(this.editorButtonsActivate, this)
 			});
 		},
 
