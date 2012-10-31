67520fc18cec3518edf8fb4eedf9b5cea660b69e
ArturKlajnerok <arturk@wikia-inc.com>
2012-10-31 21:01:04 +0000

    bugid: 33811 - Refactored achievements badges for ajax pagination

--- skins/oasis/js/Achievements.js
+++ skins/oasis/js/Achievements.js
@@ -1,8 +1,27 @@
 var AchievementsThing = {
 	init: function() {
-		$(".AchievementsModule, .WikiaLatestEarnedBadgesModule").find(".view-all").click(AchievementsThing.seeAllClick);
+		AchievementsThing.page = 0;
+		AchievementsThing.user = $(".AchievementsModule, .WikiaLatestEarnedBadgesModule").find(".data").attr("data-user");
+		AchievementsThing.badgesCount = ~~$(".AchievementsModule, .WikiaLatestEarnedBadgesModule").find(".data").attr("data-badges-count");
+		AchievementsThing.badgesPerPage = ~~$(".AchievementsModule, .WikiaLatestEarnedBadgesModule").find(".data").attr("data-badges-per-page");
+		AchievementsThing.pageCount = Math.floor(AchievementsThing.badgesCount/AchievementsThing.badgesPerPage);
+		AchievementsThing.badgesUl = $(".AchievementsModule, .WikiaLatestEarnedBadgesModule").find(".badges-icons");
+		AchievementsThing.next = $(".AchievementsModule, .WikiaLatestEarnedBadgesModule").find(".badges-next");
+		AchievementsThing.prev = $(".AchievementsModule, .WikiaLatestEarnedBadgesModule").find(".badges-prev");
+
+		if(AchievementsThing.next && AchievementsThing.prev){
+			AchievementsThing.next.click(AchievementsThing.loadBadges);
+			AchievementsThing.prev.click(AchievementsThing.loadBadges);
+		}
 
 		//Show badge description when hovering over the badge
+		AchievementsThing.showBadgesDescription();
+
+		//Track sponsored badges
+		AchievementsThing.trackSponsoredBadges();
+	},
+
+	showBadgesDescription: function(){
 		$('.AchievementsModule, .WikiaLatestEarnedBadgesModule').find('.badges li > img, .badges .sponsored-link').add("#LeaderboardTable .badge-icon").each(function(){
 			var badge = $(this);
 			var html = badge.prevAll(".profile-hover").clone().wrap('<div>').parent().html();
@@ -11,8 +30,10 @@ var AchievementsThing = {
 				placement: 'left'
 			});
 		});
+	},
 
-		$('.AchievementsModule, .WikiaLatestEarnedBadgesModule').find('.sponsored-link img:not(.badges-more)').each(function(){
+	trackSponsoredBadges: function(){
+		$('.AchievementsModule, .WikiaLatestEarnedBadgesModule').find('.sponsored-link img').each(function(){
 			AchievementsThing.trackSponsored($(this).parent().attr('data-badgetrackurl'));
 		});
 	},
@@ -29,23 +50,42 @@ var AchievementsThing = {
 		}
 	},
 
-	seeAllClick: function(event) {
-		if ($(".AchievementsModule .badges-more").is(':visible')) {
-			$(".AchievementsModule").find(".view-all span").text($(this).attr('data-msg-show'));
-		}
-		else {
-			$(".AchievementsModule").find(".view-all span").text($(this).attr('data-msg-hide'));
+	loadBadges: function(event){
+		AchievementsThing.badgesUl.startThrobbing();
 
-			$('.AchievementsModule, .WikiaLatestEarnedBadgesModule').find('.sponsored-link img.badges-more').each(function(){
-				AchievementsThing.trackSponsored($(this).parent().attr('data-badgetrackurl'));
-			});
-		}
+		var element = this;
+		AchievementsThing.page = (element.getAttribute('class') == 'badges-prev') ? AchievementsThing.page - 1 : AchievementsThing.page + 1;
 
-		$(".AchievementsModule .badges-more").toggle();
+		$.nirvana.sendRequest({
+			controller: 'AchievementsController',
+			method: 'Badges',
+			format: 'html',
+			type: 'GET',
+			data: {
+				user: AchievementsThing.user,
+				page: AchievementsThing.page
+			},
+			callback: function(html){
+				AchievementsThing.badgesUl.html(html);
+				AchievementsThing.showBadgesDescription();
+				AchievementsThing.trackSponsoredBadges();
+
+				if(Math.floor(AchievementsThing.page >= AchievementsThing.pageCount)){
+					AchievementsThing.next.hide();
+				} else if (AchievementsThing.page <= 0){
+					AchievementsThing.prev.hide();
+				} else {
+					AchievementsThing.next.show();
+					AchievementsThing.prev.show();
+				}
+
+				AchievementsThing.badgesUl.stopThrobbing();
+			}
+		});
 	}
-
 };
 
 $(function() {
 		AchievementsThing.init();
 });
+
