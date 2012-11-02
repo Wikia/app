42c7c1a592592d2712757c28ba9fdff6ef762adf
Federico "Lox" Lucignano <federico.lox@gmail.com>
2012-11-02 14:56:27 +0100

    outerhtml.js linting

--- resources/wikia/polyfills/outerhtml.js
+++ resources/wikia/polyfills/outerhtml.js
@@ -4,17 +4,23 @@
  * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
  */
 
-if(typeof document.body.outerHTML == 'undefined' && document.body.__defineGetter__ && HTMLElement){
-	HTMLElement.prototype.__defineGetter__("outerHTML",
-		function(){
-			var el = document.createElement('div'),
-			shtml;
+(function (context) {
+	'use strict';
+
+	var doc = context.document,
+		body = doc.body,
+		htmlElm = context.HTMLElement;
+
+	if (htmlElm && !doc.body.outerHTML && body.__defineGetter__) {
+		htmlElm.prototype.__defineGetter__("outerHTML", function () {
+			var el = doc.createElement('div'),
+				shtml;
 
 			el.appendChild(this.cloneNode(true));
 			shtml = el.innerHTML;
 			el = null;
 
 			return shtml;
-		}
-	);
-}
+		});
+	}
+}(this));
