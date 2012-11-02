2b80829da130408b37ead05c64174f9d98721032
Jakub Olek <jakub.olek@wikia-inc.com>
2012-11-02 16:00:49 +0100

    QueryString: use removeVal(key) instead of setVal(key), documentation

--- resources/wikia/modules/querystring.js
+++ resources/wikia/modules/querystring.js
@@ -18,11 +18,18 @@
 		return true;
 	}
 
-	function querystring(){
+	function querystring() {
 		var l = context.location,
 			p = Querystring.prototype,//late binding
 			u;
 
+		/**
+		 * You can pass url to parse to it
+		 * if url will be undefined, it'll grab URL from window.location
+		 *
+		 * @param url an URL to parse
+		 * @constructor
+		 */
 		function Querystring(url){
 			var srh,
 				link,
@@ -76,6 +83,14 @@
 			this.hash = hash;
 		}
 
+		/**
+		 * Function that takes all parameters gathered and cast it to string
+		 *
+		 * @example new Querystring().toString()
+		 * @example new Querystring() + 'some string'
+		 *
+		 * @return {String}
+		 */
 		p.toString = function () {
 			var ret = ((this.protocol) ? this.protocol + '//' : '') + this.link + this.path + (isEmpty(this.cache) ? '' : '?'),
 				attr, val,
@@ -89,28 +104,78 @@
 			return ret + tmpArr.join('&') + ((this.hash) ? '#' + this.hash : '');
 		};
 
+		/**
+		 * Function that returns value for given key from GET parameters
+		 *
+		 * @param name key to get value for
+		 * @param defVal default value to return if given key does not exist
+		 * @return {String}
+		 */
 		p.getVal = function (name, defVal) {
 			return this.cache[name] || defVal;
 		};
 
+
+		/**
+		 * Function to set value for a given key
+		 *
+		 * to remove key=value use removeVal(key)
+		 *
+		 * @param name key to set value for
+		 * @param val value to set
+		 * @return {Querystring}
+		 */
 		p.setVal = function (name, val) {
-			if(val === u){
-				delete this.cache[name];
-			}else{
+			if (name && val) {
 				this.cache[name] = encodeURIComponent(val);
 			}
 			return this;
 		};
 
+		/**
+		 * It can be called with a string as list then it'll remove one param
+		 * if array of keys will be passed it'll remove all of them
+		 *
+		 * @param list string or array of keys to be removed from URL GET parametes
+		 * @return {Querystring}
+		 */
+		p.removeVal = function(list){
+			if (list instanceof Array){
+				for(var i = 0, l = list.length; i < l; i++){
+					delete this.cache[list[i]];
+				}
+			} else {
+				delete this.cache[list];
+			}
+			return this;
+		};
+
+		/**
+		 * @return {String} a hash from URL
+		 */
 		p.getHash = function () {
 			return this.hash;
 		};
 
+		/**
+		 *
+		 * @param h String a hash to set
+		 * @return {Querystring}
+		 */
 		p.setHash = function (h) {
 			this.hash = h;
 			return this;
 		};
 
+		/**
+		 *
+		 * Function that let's you remove hash from URL
+		 * without any parameters will remove any hash
+		 * or you can give a black list of hashes as array or a single black listed hash as string
+		 *
+		 * @param list a string or array
+		 * @return {Querystring}
+		 */
 		p.removeHash = function(list){
 			if (list === u || (list instanceof Array && list.indexOf(this.hash) > -1) || list === this.hash) {
 				this.hash = '';
@@ -118,20 +183,37 @@
 			return this;
 		};
 
+		/**
+		 *
+		 * @return {String} a path part of URL
+		 */
 		p.getPath = function () {
 			return this.path;
 		};
 
+		/**
+		 *
+		 * @param p String a path to set
+		 * @return {Querystring}
+		 */
 		p.setPath = function (p) {
 			this.path = p;
 			return this;
 		};
 
+		/**
+		 * a helper function to add CB value to URL
+		 *
+		 * @return {Querystring}
+		 */
 		p.addCb = function () {
 			this.setVal('cb', Math.ceil(Math.random() * 10001));
 			return this;
 		};
 
+		/**
+		 * This will create an URL and reload a page with it
+		 */
 		p.goTo = function () {
 			l.href = this.toString();
 		};
