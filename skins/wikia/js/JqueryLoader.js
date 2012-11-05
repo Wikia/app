b4723c7d3f5ae6db5ff4f7207d200e73491162c1
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-05 18:38:11 +0100

    BugId: 51901, prevent from not loading jQuery when 'window.jQuery' points to HTML node with id='jQuery'

--- skins/wikia/js/JqueryLoader.js
+++ skins/wikia/js/JqueryLoader.js
@@ -2,7 +2,7 @@
 	var jQueryRequested = false;
 	window.getJqueryUrl = function() {
 		var result = [];
-		if (typeof window.jQuery == 'undefined' && jQueryRequested == false) {
+		if ((typeof window.jQuery === 'undefined' || typeof window.jQuery.fn === 'undefined')  && jQueryRequested == false) {
 			jQueryRequested = true;
 			result = result.concat(window.wgJqueryUrl);
 		}
