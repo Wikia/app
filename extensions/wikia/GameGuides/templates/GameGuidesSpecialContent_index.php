a4c48be6aa6ee6c01a834a6d3c82bb224a5cb4ae
Jakub Olek <jakub.olek@wikia-inc.com>
2012-11-05 16:20:02 +0100

    Add template to turn on/off GG CMT for Admins

--- extensions/wikia/GameGuides/templates/GameGuidesSpecialContent_index.php
+++ extensions/wikia/GameGuides/templates/GameGuidesSpecialContent_index.php
@@ -1,3 +1,28 @@
+<? if ( $staff ) :?>
+<section class=WikiFeatures id=WikiFeatures>
+	<ul class=features>
+		<?=
+			F::app()->getView( 'WikiFeaturesSpecial', 'feature',
+				array(
+					'feature' => array(
+						'name' => 'wgGameGuidesContentForAdmins',
+						'enabled' => $enabled,
+					),
+					'editable' => true
+				)
+			)
+		?>
+	</ul>
+</section>
+<div id=DeactivateDialog class=DeactivateDialog>
+    <h1><?= $wf->Msg('wikifeatures-deactivate-heading') ?></h1>
+    <p><?= $wf->Msg('wikifeatures-deactivate-notification') ?></p>
+    <nav>
+        <button class="cancel secondary"><?= $wf->Msg('wikifeatures-deactivate-cancel-button') ?></button>
+        <button class=confirm><?= $wf->Msg('wikifeatures-deactivate-confirm-button') ?></button>
+    </nav>
+</div>
+<? endif; ?>
 <div id=contentManagmentForm>
 	<div class=header>
 		<div>
