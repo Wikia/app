02f270160b7ba528af64e615655cea14ec1555ca
Federico "Lox" Lucignano <federico.lox@gmail.com>
2012-11-05 04:55:30 +0100

    improved doc template

--- includes/wikia/nirvana/templates/Wikia_help.php
+++ includes/wikia/nirvana/templates/Wikia_help.php
@@ -5,34 +5,22 @@
 	 <dt><h3><?= $method['method']; ?></h3></d3t>
 	 <dd>
 	 	<?= $method['description']; ?>
-	 	<? if ( !empty( $method['request'] ) ) :?>
-			<dl>
-		 		<dt><h4>Request</h4></dt>
-		 		<dd><ul>
-				 <? foreach ( $method['request'] as $item ) :?>
-				 	<li>
-			 		 <? foreach ( $item as $key => $val ) :?>
-					 	<strong><?= $key ;?></strong>: <?= ($key == 'optional') ? (($val) ? 'yes' : 'no' ) : $val ;?><br/>
+	 	<? foreach( array( 'request', 'response' ) as $section ) :?>
+		 	<? if ( !empty( $method[$section] ) ) :?>
+				<dl>
+			 		<dt><h4><?= ucfirst( $section ) ;?></h4></dt>
+			 		<dd><ul>
+					 <? foreach ( $method[$section] as $item ) :?>
+					 	<li>
+				 		 <? foreach ( $item as $key => $val ) :?>
+						 	<strong><?= $key ;?></strong>: <?= ($key == 'optional') ? (($val) ? 'yes' : 'no' ) : $val ;?><br/>
+						 <? endforeach ;?>
+						</li>
 					 <? endforeach ;?>
-					</li>
-				 <? endforeach ;?>
-				</ul></dd>
-			</dl>
-		<?	endif ;?>
-		<? if ( !empty( $method['response'] ) ) :?>
-			<dl>
-		 		<dt><h4>Response</h4></dt>
-		 		<dd><ul>
-				 <? foreach ( $method['request'] as $item ) :?>
-				 	<li>
-			 		 <? foreach ( $item as $key => $val ) :?>
-					 	<strong><?= $key ;?></strong>: <?= ($key == 'optional') ? (($val) ? 'yes' : 'no' ) : $val ;?><br/>
-					 <? endforeach ;?>
-					</li>
-				 <? endforeach ;?>
-				</ul></dd>
-			</dl>
-		<?	endif ;?>
+					</ul></dd>
+				</dl>
+			<?	endif ;?>
+		<? endforeach ;?>
 	 </dd>
  <?php endforeach; ?>
 </dl>
