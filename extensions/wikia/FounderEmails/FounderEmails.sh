ad0027eb10727ee53a6606311dc8321fa0b89459
saipetch <saipetch@wikia-inc.com>
2012-11-06 15:09:54 -0800

    bugid 51358: Founder Emails - update config path

--- extensions/wikia/FounderEmails/FounderEmails.sh
+++ extensions/wikia/FounderEmails/FounderEmails.sh
@@ -3,7 +3,7 @@
 cd /usr/wikia/source/wiki/extensions/wikia/FounderEmails/
 echo -e "$(date)\n"
 ulimit -n 100000
-SERVER_ID=177 php maintenance.php --conf /usr/wikia/conf/current/LocalSettings.php --event=daysPassed
-SERVER_ID=177 php maintenance.php --conf /usr/wikia/conf/current/LocalSettings.php --event=viewsDigest
-SERVER_ID=177 php maintenance.php --conf /usr/wikia/conf/current/LocalSettings.php --event=completeDigest
+SERVER_ID=177 php maintenance.php --conf /usr/wikia/slot1/docroot/LocalSettings.php --event=daysPassed
+SERVER_ID=177 php maintenance.php --conf /usr/wikia/slot1/docroot/LocalSettings.php --event=viewsDigest
+SERVER_ID=177 php maintenance.php --conf /usr/wikia/slot1/docroot/LocalSettings.php --event=completeDigest
 echo -e "\nDone\n"
