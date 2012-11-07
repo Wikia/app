fd891158d2c7934393c2961653766a09f86ab291
jacekjursza <jacek@wikia-inc.com>
2012-11-07 12:55:18 +0100

    SDS template: remove the arrays if the property is not a collection

--- extensions/wikia/StructuredData/templates/renderers/container.php
+++ extensions/wikia/StructuredData/templates/renderers/container.php
@@ -1,8 +1,22 @@
 <?php
 $values = $object->getValues();
-if (empty($values) || $values[0] == null):?>
-	<p class="empty">empty</p>
-<?php else:
+if (empty($values) || $values[0] == null) {
+	/*
+	 *  property is empty
+	 */
+	echo '<p class="empty">empty</p>';
+
+} elseif ( (is_array($values) && count($values) == 1) || !is_string($values) ) {
+	/*
+	 *  if we have single property (not array) or one-element array
+	 *  we should display it as pure string not as <li> element
+	 */
+	echo is_array($values) ? $values[0] : $values;
+
+} else {
+	/*
+	 * property is an array
+	 */
 	echo ($rendererName == '@list') ? '<ol>' : '<ul>';
 	foreach($values as $reference) {
 		echo '<li>';
@@ -17,9 +31,9 @@ if (empty($values) || $values[0] == null):?>
 			echo $reference;
 		}
 		echo '</li>';
-
 	}
 
 	echo ($rendererName == '@list') ? '</ol>' : '</ul>';
-	?>
-<?php endif;?>
+
+}
+?>
