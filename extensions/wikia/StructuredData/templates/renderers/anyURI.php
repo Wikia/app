a932c170382483382a7600a96dfa76ca8bc5bbff
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-08 16:45:47 +0100

    add edit template for URI renderer

--- extensions/wikia/StructuredData/templates/renderers/anyURI.php
+++ extensions/wikia/StructuredData/templates/renderers/anyURI.php
@@ -1,8 +1,14 @@
 <?php 
 	$value = $object->getValue();
 ?>
-<?php if (empty($value)) :?>
-	<p class="empty">empty</p>	
-<?php else : ?>
-	<a href="<?php echo $value; ?>"><?php echo $value; ?></a>
+<?php if($context == SD_CONTEXT_EDITING): ?>
+	<div class="input-group">
+		<input type="text" value="<?=$value;?>" name="<?=$object->getName();?>" />
+	</div>
+<?php else: ?>
+	<?php if (empty($value)) :?>
+		<p class="empty">empty</p>
+	<?php else : ?>
+		<a href="<?php echo $value; ?>"><?php echo $value; ?></a>
+	<?php endif; ?>
 <?php endif; ?>
