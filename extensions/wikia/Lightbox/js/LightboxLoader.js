ea1df2db90be3d4b01d9f8df0fdb3e009dda0dfc
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-09 16:17:21 +0100

    BugId: 51930, fix removing of inline videos flash objects when opening another video in modal

--- extensions/wikia/Lightbox/js/LightboxLoader.js
+++ extensions/wikia/Lightbox/js/LightboxLoader.js
@@ -11,7 +11,6 @@ var LightboxLoader = {
 		share: {},
 		to: 0
 	},
-	inlineVideos: $(),	// jquery array of inline videos
 	inlineVideoLinks: $(),	// jquery array of inline video links
 	lightboxLoading: false,
 	inlineVideoLoading: [],
@@ -255,9 +254,7 @@ var LightboxLoader = {
 			width: targetChildImg.width()
 		}, function(json) {
 			//retrieve DOM reference
-			var videoReference = target.next(),
-				embedCode = json['videoEmbedCode'];
-			
+			var	embedCode = json['videoEmbedCode'];
 			target.hide().after(embedCode);
 
 			// if player script, run it
@@ -267,8 +264,6 @@ var LightboxLoader = {
 
 			// save references for inline video removal later
 			LightboxLoader.inlineVideoLinks = target.add(LightboxLoader.inlineVideoLinks);
-			LightboxLoader.inlineVideos = videoReference.add(LightboxLoader.inlineVideos);
-
 			LightboxTracker.inlineVideoTrackingTimeout = setTimeout(function() {
 				LightboxTracker.track(WikiaTracker.ACTIONS.VIEW, 'video-inline', null, {title:json.title, provider: json.providerName, clickSource: clickSource});
 			}, 1000);
@@ -279,8 +274,9 @@ var LightboxLoader = {
 
 	removeInlineVideos: function() {
 		clearTimeout(LightboxTracker.inlineVideoTrackingTimeout);
-		LightboxLoader.inlineVideos.remove();
-		LightboxLoader.inlineVideoLinks.show();
+		LightboxLoader.inlineVideoLinks.each(function(){
+			$(this).show().next().remove();
+		});
 	},
 
 	getMediaDetail: function(mediaParams, callback) {
