a7c46932cced6e5583115baa49bc8a42df67f863
Adrian Wieczorek <adi@wikia-inc.com>
2012-11-09 12:05:12 +0100

    sitemap_blobs schema fixed

--- extensions/wikia/Sitemap/sitemap_blobs.patch.sql
+++ extensions/wikia/Sitemap/sitemap_blobs.patch.sql
@@ -2,6 +2,6 @@ CREATE TABLE sitemap_blobs (
 	`sbl_ns_id` INT NOT NULL,
 	`sbl_page_id` INT NOT NULL,
 	`sbl_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	`sbl_content` BLOB,
+	`sbl_content` LONGBLOB,
 	PRIMARY KEY (`sbl_ns_id`, `sbl_page_id`)
 ) ENGINE = InnoDB;
