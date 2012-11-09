ca09e1da6e1cc91e093512471c94f0bb2c5c64fb
Piotr Bablok <piotr.bablok@gmail.com>
2012-11-08 16:21:59 -0800

    Achievements: change SQL query to use indexes that result in speeding it up considerably

--- extensions/wikia/AchievementsII/services/AchUserProfileService.class.php
+++ extensions/wikia/AchievementsII/services/AchUserProfileService.class.php
@@ -118,9 +118,11 @@ class AchUserProfileService {
 					AND date = (SELECT max(date) from ach_user_badges a2 where wiki_id = $wgCityId and a1.user_id = a2.user_id) ";
 			} else {
 				$dbr = wfGetDB(DB_SLAVE);
-				$sql = "SELECT user_id, badge_type_id, badge_lap FROM `ach_user_badges` a1
-					WHERE user_id IN (" . implode(',', $user_ids) . ")
-					AND date = (SELECT max(date) from ach_user_badges a2 where a1.user_id = a2.user_id) ";
+				$sql = 'SELECT  a.user_id, a.badge_type_id, a.badge_lap FROM
+				(SELECT user_id, max(date) as max_date from ach_user_badges a2
+					where a2.user_id in (' . implode(','. $user_ids) . ') group by user_id ) sub
+					join ach_user_badges a
+					on a.user_id = sub.user_id AND a.date = sub.max_date';
 			}
 
 			$res = $dbr->query( $sql, __METHOD__ );
