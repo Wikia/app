7b2278593615d7df49077064a0c94ec4b9746203
Kyle Florence <kyle.florence@gmail.com>
2012-11-09 23:18:53 +0000

    ShareButtons: return promise for Facebook button.

--- extensions/wikia/ShareButtons/js/ShareButtonFacebook.js
+++ extensions/wikia/ShareButtons/js/ShareButtonFacebook.js
@@ -7,7 +7,12 @@ if ( ShareButtons ) {
 	ShareButtons.add({
 		dependencies: [ $.loadFacebookAPI ],
 		callback: function() {
-			window.onFBloaded();
+			var dfd = new $.Deferred();
+
+			FB.Event.subscribe( 'xfbml.render', dfd.resolve );
+			FB.XFBML.parse( $( '.fb-like' ).parent( '.shareButton' ).get( 0 ) );
+
+			return dfd.promise();
 		}
 	});
 }
