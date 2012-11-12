6ba952c61796bfd503638fdc57fef03bfb6f50d5
nAndy <nandy@wikia-inc.com>
2012-11-12 08:27:42 +0000

    Revert "changed name of landing page property"
    This reverts commit 751972b09d87be71c9d07abb75d0615b5ccdcc06.

--- extensions/wikia/UserPreferencesV2/UserPreferencesV2.setup.php
+++ extensions/wikia/UserPreferencesV2/UserPreferencesV2.setup.php
@@ -9,24 +9,23 @@ $app = F::app();
 $dir = dirname(__FILE__) . '/';
 
 /**
- * new default user preferences
- */
+* new default user preferences
+*/
 $wgDefaultUserOptions['watchdeletion'] = 1;
 
 /**
- * classes
- */
+* classes
+*/
 $app->registerClass('UserPreferencesV2', $dir . 'UserPreferencesV2.class.php');
 
 /**
- * hooks
- */
+* hooks
+*/
 $app->registerHook('GetPreferences', 'UserPreferencesV2', 'onGetPreferences');
 $app->registerHook('SpecialPreferencesBeforeResetUserOptions', 'UserPreferencesV2', 'onSpecialPreferencesBeforeResetUserOptions');
 $app->registerHook('SpecialPreferencesAfterResetUserOptions', 'UserPreferencesV2', 'onSpecialPreferencesAfterResetUserOptions');
 $app->registerHook('PreferencesTrySetUserEmail', 'UserPreferencesV2', 'onPreferencesTrySetUserEmail');
 $app->registerHook('SavePreferences', 'UserPreferencesV2', 'onSavePreferences');
-$app->registerHook('UserGetDefaultOptions', 'UserPreferencesV2', 'onUserGetDefaultOptions');
 
 /**
  * messages
