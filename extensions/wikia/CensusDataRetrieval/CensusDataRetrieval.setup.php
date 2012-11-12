eabe12f4487613a40c46a94e3674a8ca947bd785
Kamil Koterba <kamil@wikia-inc.com>
2012-11-12 18:09:51 +0000

    BugId:52384 EditPage::attemptSave Hook structure

--- extensions/wikia/CensusDataRetrieval/CensusDataRetrieval.setup.php
+++ extensions/wikia/CensusDataRetrieval/CensusDataRetrieval.setup.php
@@ -3,5 +3,7 @@
 $app = F::app();
 
 $app->registerHook( 'EditFormPreloadText', 'CensusDataRetrieval', 'retrieveFromName' );
+//$app->registerHook( 'ParserBeforeInternalParse', 'CensusDataRetrieval', 'replaceLinks' );
+$app->registerHook( 'EditPage::attemptSave', 'CensusDataRetrieval', 'replaceLinks' );
 $app->registerClass( 'CensusDataRetrieval', __DIR__ . '/CensusDataRetrieval.class.php' );
 $app->registerExtensionMessageFile( 'CensusDataRetrieval', __DIR__ . '/CensusDataRetrieval.i18n.php' );
