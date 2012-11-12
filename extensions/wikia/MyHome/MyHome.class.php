7d1e5c758cd58085000e002f44270811bafa4324
nAndy <nandy@wikia-inc.com>
2012-11-12 08:34:11 +0000

    Revert "added new logic in MyHome::getInitialMainPage() hook"
    This reverts commit 17a273a36c171a6b776558ea0eaa9015d6526618.

--- extensions/wikia/MyHome/MyHome.class.php
+++ extensions/wikia/MyHome/MyHome.class.php
@@ -174,18 +174,12 @@ class MyHome {
 			return true;
 		}
 
-		//user must be logged in and have redirect enabled;
-		//this is not used for Corporate Sites where Wikia Visualization is enabled
-		if( $wgUser->isLoggedIn() && empty($wgEnableWikiaHomePageExt) ) {
-			$value = $wgUser->getOption('myhomedisableredirect');
-			switch($value) {
-				case UserPreferencesV2::MYINFO_REDIRECT_WIKI_ACTIVITY:
-					$title = Title::newFromText('WikiActivity', NS_SPECIAL);
-					break;
-				case UserPreferencesV2::MYINFO_REDIRECT_RECENT_CHANGES:
-					$title = Title::newFromText('RecentChanges', NS_SPECIAL);
-					break;
-			}
+		// user must be logged in and have redirect enabled; this is not used for Corporate Sites where Wikia Visualization
+		// is enabled
+		if ($wgUser->isLoggedIn()
+			&& ($wgUser->getOption('myhomedisableredirect') != true)
+			&& empty($wgEnableWikiaHomePageExt )) {
+			$title = Title::newFromText('WikiActivity', NS_SPECIAL);
 		}
 
 		wfProfileOut(__METHOD__);
