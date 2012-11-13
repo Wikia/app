3bd673406a8619bd6408ae52b1fdf8d07cfb62f1
Jakub Olek <jolek@wikia-inc.com>
2012-11-13 12:51:27 +0100

    BoilerPlate for NavigationApiController

---
+++ includes/wikia/api/NavigationApiController.class.php
@@ -0,0 +1,18 @@
+<?php
+	/**
+	 * Controller to get Wiki Navigation for a wiki
+	 *
+	 * @author Jakub "Student" Olek
+	 */
+
+class NavigationApiController extends WikiaApiController {
+
+	/**
+	 * Fetches wiki navigation
+	 *
+	 * @responseParam array $navigation Wiki Navigation
+	 */
+	public function get(){
+
+	}
+}
