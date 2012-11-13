2f99b3619d585a60b2db71653e078ad1ed15003d
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-13 15:43:57 +0100

    add remove button to object references

--- extensions/wikia/StructuredData/templates/renderers/default.php
+++ extensions/wikia/StructuredData/templates/renderers/default.php
@@ -3,3 +3,7 @@
 	<input type="hidden" name="<?=$params['fieldName'];?>" value="<?=$object->getId();?>" />
 <?php endif; ?>
 <a href="<?=$object->getSpecialPageUrl();?>"><?=htmlspecialchars( $object->getName() );?></a>
+
+<?php if($context == SD_CONTEXT_EDITING): ?>
+	<button class="secondary remove">Remove</button>
+<?php endif; ?>
