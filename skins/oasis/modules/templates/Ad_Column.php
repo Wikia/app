4ce14df959e1e43fb0c2c8c1e608304b74151f38
sebastian-wikia <sebastianm@wikia-inc.com>
2012-11-13 14:26:22 +0000

    Removing conditional for undefined configuration variable

--- skins/oasis/modules/templates/Ad_Column.php
+++ skins/oasis/modules/templates/Ad_Column.php
@@ -1,11 +1,6 @@
 <div class="home-top-right-ads">
 	<?php
-		if (in_array('leaderboard', $wg->ABTests)) {
-			echo $app->renderView('Ad', 'Index', array('slotname' => 'TEST_HOME_TOP_RIGHT_BOXAD'));
-		} else {
-			echo $app->renderView('Ad', 'Index', array('slotname' => 'HOME_TOP_RIGHT_BOXAD'));
-		}
-
+		echo $app->renderView('Ad', 'Index', array('slotname' => 'HOME_TOP_RIGHT_BOXAD'));
 		echo $app->renderView('Ad', 'Index', array('slotname' => 'HOME_TOP_RIGHT_BUTTON'));
 	?>
 </div>
