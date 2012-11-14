e9f7b7d936d2ff2bfa1f0672daf0f9030bf0a389
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-14 18:23:27 +0100

    add templete for boolean values

--- extensions/wikia/StructuredData/templates/renderers/boolean.php
+++ extensions/wikia/StructuredData/templates/renderers/boolean.php
@@ -1,1 +1,13 @@
-BOOOLEAAAAN <input type="checkbox" name="" value="" />
+<?php $value = $object->getValue(); ?>
+<?php if($context == SD_CONTEXT_EDITING): ?>
+	<div class="input-group">
+		<input type="checkbox" name="<?=$object->getName();?>" <?=($value == 'true') ?
+			'checked="checked"' : ''; ?> />
+	</div>
+<?php else: ?>
+	<?php if(empty($value)): ?>
+		<p class="empty">empty</p>
+	<?php else: ?>
+		<?=$value;?>
+	<?php endif; ?>
+<?php endif; ?>
