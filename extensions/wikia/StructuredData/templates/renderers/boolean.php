ee32cf9cd2c340f67a6c5d7b24bf76e7177311f8
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-14 18:41:27 +0100

    chenge checkbox to select for boolean values because of the 'empty value' possibility

--- extensions/wikia/StructuredData/templates/renderers/boolean.php
+++ extensions/wikia/StructuredData/templates/renderers/boolean.php
@@ -1,8 +1,11 @@
 <?php $value = $object->getValue(); ?>
 <?php if($context == SD_CONTEXT_EDITING): ?>
 	<div class="input-group">
-		<input type="checkbox" name="<?=$object->getName();?>" <?=($value == 'true') ?
-			'checked="checked"' : ''; ?> />
+		<select name="<?=$object->getName();?>">
+			<option <?=(empty($value)) ? 'selected="selected"' : ''; ?> >empty</option>
+			<option <?=($value === 'true') ? 'selected="selected"' : ''; ?> value="true">true</option>
+			<option <?=($value === 'false') ? 'selected="selected"' : ''; ?> value="false">false</option>
+		</select>
 	</div>
 <?php else: ?>
 	<?php if(empty($value)): ?>
