c6f578f7866e819872ba256206e67e3c4f10ad14
Przemek Piotrowski (Nef) <ppiotr@wikia-inc.com>
2012-11-14 17:24:10 +0100

    bugid:64193 addriver2 size tracking fixed

--- extensions/wikia/AdEngine/js/AdProviderGamePro.js
+++ extensions/wikia/AdEngine/js/AdProviderGamePro.js
@@ -30,8 +30,8 @@ var AdProviderGamePro = function(wikiaDart, ScriptWriter, WikiaTracker, log, win
 			, slotsize = slot[1] || slotMap[slotname].size
 		;
 
-		slotsize = slotsize.replace(/,.*$/, '');
-		WikiaTracker.trackAdEvent('liftium.slot2', {'ga_category': 'slot2/' + slotsize, 'ga_action': slotname, 'ga_label': 'gamepro'}, 'ga');
+		WikiaTracker.trackAdEvent('liftium.slot2', {'ga_category': 'slot2/' + slotsize.replace(/,.*$/, ''), 'ga_action': slotname, 'ga_label': 'gamepro'}, 'ga');
+
 		ScriptWriter.injectScriptByUrl(slotname, getUrl(slotname));
 	}
 
