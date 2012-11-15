d3c72a1671b8b23527e7fa0ae7b1ed34b5882a34
Damian Jóźwiak <damian@wikia-inc.com>
2012-11-15 11:29:43 +0100

    bugid-64228 | added messages for spanish main page

--- extensions/wikia/WikiaHomePage/WikiaHomePage.i18n.php
+++ extensions/wikia/WikiaHomePage/WikiaHomePage.i18n.php
@@ -131,10 +131,6 @@ __NOEDITSECTION__',
 	'wikiahome-preview-error' => 'Interstitial error',
 	'wikiahome-preview-user-member-since' => "Member since $1",
 
-	'wikiahome-footer-interlang-flag-class' => 'de',
-	'wikiahome-footer-interlang-flag-href' => 'http://de.wikia.com',
-	'wikiahome-footer-interlang-flag-title' => 'Wikia Deutschland',
-
 	'wikiahome-import-script-no-description' => 'No description given',
 
 	'wikiahome-hub-current-day' => '{{CURRENTDAY}} {{CURRENTMONTHNAME}} {{CURRENTYEAR}}',
@@ -251,10 +247,6 @@ $messages['de'] = array(
 	'wikiahome-preview-error' => 'Fehler Zwischenseite',
 	'wikiahome-preview-user-member-since' => "Mitglied seit $1",
 
-	'wikiahome-footer-interlang-flag-class' => 'us',
-	'wikiahome-footer-interlang-flag-href' => 'http://www.wikia.com',
-	'wikiahome-footer-interlang-flag-title' => 'Wikia Inc.',
-
 	//fb#45624
 	'wikiahome-image-auto-uploaded-comment' => 'Automatisch erstelltes Bild für die Nutzung auf http://de.wikia.com – in Kürze dazu mehr Informationen im Wikia-Blog: http://de.community.wikia.com/wiki/Blog:Wikia_Deutschland_News',
 );
@@ -340,55 +332,55 @@ $messages['es'] = array(
 *[http://www.wikia.com/Lifestyle Featured]',
 */
 	'wikiahome-community-column1-heading' => 'Para la comunidad',
-//	'wikiahome-community-column1-link' => 'http://community.wikia.com',
+	'wikiahome-community-column1-link' => 'http://comunidad.wikia.com/wiki/Wikia',
 	'wikiahome-community-column1-creative' => 'Cualquier cosa que necesites para comenzar y hacer crecer tu comunidad en Wikia, desde consejos y herramientas hasta noticias y acceso a personas que te serán de mucha ayuda.',
 	'wikiahome-community-column2-heading' => 'Innovando en Wikia',
-//	'wikiahome-community-column2-link' => 'http://www.wikia.com/Mobile',
+	'wikiahome-community-column2-link' => 'http://es.wikia.com/M%C3%B3viles',
 	'wikiahome-community-column2-creative' => 'Mira como Wikia se abre hueco en el universo de los teléfonos con su nueva web para móviles y aplicaciones como Guías de videojuegos o Letras de canciones de LyricWiki.',
 	'wikiahome-community-column3-heading' => 'Síguenos',
-/*	'wikiahome-community-social-wikia-blog' => 'Wikia Blog',
-	'wikiahome-community-social-wikia-blog-link' => 'http://community.wikia.com/wiki/Blog:Wikia_Staff_Blog',
+	'wikiahome-community-social-wikia-blog' => 'Blog de Wikia',
+	'wikiahome-community-social-wikia-blog-link' => 'http://es.wikia.com/wiki/Blog:Noticias_de_Wikia',
 	'wikiahome-community-social-twitter' => 'Twitter',
-	'wikiahome-community-social-twitter-link' => 'http://twitter.com/#!/wikia',
+	'wikiahome-community-social-twitter-link' => 'https://twitter.com/#!/wikia_es',
 	'wikiahome-community-social-facebook' => 'Facebook',
-	'wikiahome-community-social-facebook-link' => 'http://www.facebook.com/wikia',
-	'wikiahome-community-social-googleplus' => 'Google+',
-	'wikiahome-community-social-googleplus-link' => 'http://gplus.to/wikia',
+	'wikiahome-community-social-facebook-link' => 'http://www.facebook.com/wikia.es',
+//	'wikiahome-community-social-googleplus' => 'Google+',
+//	'wikiahome-community-social-googleplus-link' => 'http://gplus.to/wikia',
 
 	'wikiahome-footer-wikiainc' => '===Wikia, Inc.===
-*[http://www.wikia.com/About_Us About Us]
-*[http://www.wikia.com/Special:Contact Contact Wikia]
-*[http://www.wikia.com/Careers Careers]
-*[http://www.wikia.com/Press Press]
+*[http://es.wikia.com/Sobre_nosotros Sobre nosotros]
+*[http://es.wikia.com/Especial:Contactar Contacta con Wikia]
+*[http://www.wikia.com/Careers Ofertas de empleo]
+*[http://es.wikia.com/Prensa Prensa]
 __NOEDITSECTION__',
-	'wikiahome-footer-get-started-heading' => '===Get Started===
+	'wikiahome-footer-get-started-heading' => '===Comienza===
 __NOEDITSECTION__',
-	'wikiahome-footer-get-started-creative' => 'Find your community,<br />start something epic.',
-	'wikiahome-footer-get-started-button' => 'http://www.wikia.com/Special:CreateNewWiki', // FIXME: why is this in a message?!
-	'wikiahome-footer-follow-us' => '===Follow Us===
-*[http://community.wikia.com/wiki/Blog:Wikia_Staff_Blog Wikia Blog]
-*[http://www.facebook.com/Wikia Facebook]
-*[http://www.twitter.com/Wikia Twitter]
+	'wikiahome-footer-get-started-creative' => 'Encuentra tu comunidad,<br />comienza algo épico.',
+	'wikiahome-footer-get-started-button' => 'http://www.wikia.com/Special:CreateWiki?uselang=es', // FIXME: why is this in a message?!
+	'wikiahome-footer-follow-us' => '===Síguenos===
+*[http://es.wikia.com/wiki/Blog:Noticias_de_Wikia Blog de Wikia]
+*[http://www.facebook.com/wikia.es Facebook]
+*[https://twitter.com/wikia_es Twitter]
 __NOEDITSECTION__',
-	'wikiahome-footer-community' => '===Community===
-*[http://www.community.wikia.com Community Central]
-*[http://community.wikia.com/wiki/Admin_Central:Main_Page Founders & Admins]
-*[http://www.wikia.com/Special:Contact Contact Support]
-*[http://help.wikia.com/wiki/Help:Contents Help]
+	'wikiahome-footer-community' => '===Comunidad===
+*[http://comunidad.wikia.com/wiki/Wikia Comunidad Hispana]
+*[http://comunidad.wikia.com/wiki/Foro:%C3%8Dndice Foro]
+*[http://es.wikia.com/Especial:Contactar Contacta con Wikia]
+*[http://ayuda.wikia.com/wiki/Ayuda:Contenidos Ayuda]
 __NOEDITSECTION__',
-	'wikiahome-footer-everywhere' => '===Wikia Everywhere===
-*[http://www.wikia.com/Mobile Wikia Mobile]
-*[http://www.wikia.com/Mobile/GameGuides Video Game Apps]
-*[http://www.wikia.com/Mobile/PhotoPop Entertainment Apps]
+	'wikiahome-footer-everywhere' => '===Wikia para todos===
+*[http://es.wikia.com/Móviles Wikia Móviles]
+*[http://es.wikia.com/Móviles/GameGuide Ap. Videojuegos]
+*[http://es.wikia.com/Móbiles/PhotoPop Ap. Entretenimiento]
 *[http://api.wikia.com/wiki/Wikia_API_Wiki API]
 __NOEDITSECTION__',
-	'wikiahome-footer-advertise' => '===[http://www.mediakit.wikia.com/ Advertise]===
-*[http://mediakit.wikia.com/Solutions/Audience Audience]
-*[http://mediakit.wikia.com/Solutions Solutions]
-*[http://mediakit.wikia.com/Advertising-Products Ad products]
-*[https://wikia.sl.advertising.com/admin/advertisers/indexPl.jsp Self Service]
+	'wikiahome-footer-advertise' => '===[http://www.mediakit.wikia.com/ Publicidad]===
+*[http://mediakit.wikia.com/Solutions/Audience Audiencia]
+*[http://mediakit.wikia.com/Solutions Soluciones]
+*[http://mediakit.wikia.com/Advertising-Products Anuncios]
+*[https://wikia.sl.advertising.com/admin/advertisers/indexPl.jsp Autoservicio]
 __NOEDITSECTION__',
-
+/*
 	'wikia-home-page-hot' => 'Hot',
 	'wikia-home-page-new' => 'New',
 	'wikia-home-page-official' => 'Official',
@@ -408,10 +400,6 @@ __NOEDITSECTION__',
 	'wikiahome-preview-go-to-wiki-label' => 'Visit Wiki',
 	'wikiahome-preview-error' => 'Interstitial error',
 	'wikiahome-preview-user-member-since' => "Member since $1",
-
-	'wikiahome-footer-interlang-flag-class' => 'de',
-	'wikiahome-footer-interlang-flag-href' => 'http://de.wikia.com',
-	'wikiahome-footer-interlang-flag-title' => 'Wikia Deutschland',
 
 	'wikiahome-import-script-no-description' => 'No description given',
 
@@ -497,9 +485,6 @@ $messages['fr'] = array(
 	'wikiahome-preview-go-to-wiki-label' => 'Visiter le wiki',
 	'wikiahome-preview-error' => 'Erreur interstitielle',
 	'wikiahome-preview-user-member-since' => 'Membre depuis $1',
-	'wikiahome-footer-interlang-flag-class' => 'fr',
-	'wikiahome-footer-interlang-flag-href' => '<a  class="free" href="http://www.wikia.com">http://www.wikia.com</a>',
-	'wikiahome-footer-interlang-flag-title' => 'Wikia Inc.',
 	'wikiahome-import-script-no-description' => 'Aucune description renseignée',
 	'wikiahome-hub-current-day' => '{{CURRENTDAY}} {{CURRENTMONTHNAME}} {{CURRENTYEAR}}',
 	'wikiahome-image-auto-uploaded-comment' => 'Image générée automatiquement pour être utilisée sur <a  class="free" href="http://fr.wikia.com/">http://fr.wikia.com/</a> &#8212; retrouvez plus d\'informations sur le blog dédié&nbsp;: <a  class="free" href="http://communaute.wikia.com/wiki/Blog:Actualit%C3%A9_Wikia">http://communaute.wikia.com/wiki/Blog:Actualit%C3%A9_Wikia</a>',
