29cfa8db41865cde4647c2d88dac5d4658755617
Jakub Olek <jolek@wikia-inc.com>
2012-11-19 14:37:46 +0100

    GameGuides: if string then get the element

--- extensions/wikia/WikiaMobile/js/sections.js
+++ extensions/wikia/WikiaMobile/js/sections.js
@@ -128,6 +128,8 @@ define('sections', ['events', 'JSMessages'], function(ev, msg){
 
 	function toggle(h2, scroll){
 		if(h2){
+			(typeof h2 == 'string') && (h2 = document.getElementById(h2));
+
 			if(isOpen(h2)){
 				close(h2);
 			}else{
