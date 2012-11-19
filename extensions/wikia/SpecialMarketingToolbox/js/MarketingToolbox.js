01e1b6780a044868b1b9c02fce484ebe3d3b9516
Damian Jozwiak <damian@wikia-inc.com>
2012-11-19 16:58:29 +0100

    bugid-64235 | datepicker styles

--- extensions/wikia/SpecialMarketingToolbox/js/MarketingToolbox.js
+++ extensions/wikia/SpecialMarketingToolbox/js/MarketingToolbox.js
@@ -1,12 +1,44 @@
 var MarketingToolbox = function() {};
 
+// TODO mocked data
+var specialDates = {
+	'2012-11-20': 1,
+	'2012-11-12': 2
+};
+
+// TODO
+var tooltipMessages = {};
+tooltipMessages[window.wgMarketingToolboxConstants.DAY_EDITED_NOT_PUBLISHED] = 'In progress / Not published';
+tooltipMessages[window.wgMarketingToolboxConstants.DAY_PUBLISHED] = 'Saved / Published';
+
+
 MarketingToolbox.prototype = {
 	init: function() {
 		$.when(
 			// jQuery UI datepicker plugin
 			mw.loader.use(['jquery.ui.datepicker'])
 		).then(function(getResourcesData) {
-			$("#date-picker").datepicker();
+			$("#date-picker").datepicker({
+				showOtherMonths: true,
+				selectOtherMonths: true,
+				beforeShowDay: function (date) {
+					var tdClassName, tooltip;
+					var theday = date.getFullYear() + '-' +
+						(date.getMonth()+1) + '-' +
+						date.getDate();
+
+					if (theday in specialDates) {
+						if (specialDates[theday] == window.wgMarketingToolboxConstants.DAY_EDITED_NOT_PUBLISHED) {
+							tdClassName = 'inProg';
+						} else if (specialDates[theday] == window.wgMarketingToolboxConstants.DAY_PUBLISHED) {
+							tdClassName = 'published';
+						}
+						tooltip = tooltipMessages[specialDates[theday]];
+					}
+
+					return [true, tdClassName, tooltip];
+				}
+			});
 		});
 	}
 };
