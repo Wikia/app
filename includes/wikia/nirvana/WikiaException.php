8be819d875e4b4996e5949acde98781c3b00a93b
Federico "Lox" Lucignano <federico.lox@gmail.com>
2012-11-20 14:21:16 +0100

    cleaning up WikiaException from legacy PHP code

--- includes/wikia/nirvana/WikiaException.php
+++ includes/wikia/nirvana/WikiaException.php
@@ -1,106 +1,57 @@
 <?php
 
 /**
- * Nirvana Framework - Wikia exception
- *
- * Base class for Wikia codebase exceptions. PHP 5.3 compatible
+ * Base exception class for the Nirvana framework
  *
  * @ingroup nirvana
  *
  * @author Wojciech Szela <wojtek@wikia-inc.com>
+ * @author Federico "Lox" Lucignano <federico@wikia-inc.com>
  * @link http://pl2.php.net/manual/en/class.exception.php
  */
 class WikiaException extends MWException {
 	/**
-	 * Previous exception
-	 *
-	 * @var Exception|null
-	 */
-	private $_previous;
-
-	/**
 	 * Constructor
 	 *
-	 * @link  http://pl2.php.net/manual/en/exception.construct.php
-	 * @param string $message
-	 * @param int $code
-	 * @param Exception $exception
+	 * @param string $message The exception message
+	 * @param int $code The error code
+	 * @param Exception $previous The previous exception in the chain if any
+	 *
+	 * @link  http://www.php.net/manual/en/class.exception.php
+	 * @see MWException
 	 */
 	public function __construct($message = '', $code = 0, Exception $previous = null) {
-		if (version_compare(PHP_VERSION, '5.3.0', '<')) {
-			parent::__construct($message, $code);
-			$this->_previous = $previous;
-		} else {
-			parent::__construct($message, $code, $previous);
-		}
+		parent::__construct( $message, $code, $previous );
 
 		// log more details (macbre)
-		Wikia::logBacktrace(__METHOD__);
+		Wikia::logBacktrace( __METHOD__ );
 	}
 
 	/**
-	 * Simulates getPrevious() introduced in PHP 5.3
+	 * Overrides MWException::report to also write exceptions to error_log
 	 *
-	 * @link   http://pl2.php.net/manual/en/exception.getprevious.php
-	 * @param  string $method
-	 * @param  array $args
-	 * @return mixed
-	 */
-	public function __call($method, array $args) {
-		if ('getprevious' == strtolower($method)) {
-			return $this->_getPrevious();
-		}
-
-		return null;
-	}
-
-	/**
-	 * String representation of the exception
-	 *
-	 * @link http://pl2.php.net/manual/en/exception.tostring.php
-	 * @return string
-	 */
-	public function __toString() {
-		if (version_compare(PHP_VERSION, '5.3.0', '<')) {
-			if (null !== ($e = $this->_getPrevious())) {
-				return $e->__toString() . "\n\nNext " . parent::__toString();
-			}
-		}
-
-		return parent::__toString();
-	}
-
-	/**
-	 * Previous exception getter
-	 *
-	 * To simulate getPrevious() method introduced in PHP 5.3
-	 *
-	 * @return Exception|null
-	 */
-	protected function _getPrevious() {
-		return $this->_previous;
-	}
-
-	/**
-	 * Override MWException report() and write exceptions to error_log
-	 *
-	 * Uncomment the flush() line to override normal MWException headers
-	 * so we can display an error page instead of a 500 error (varnish doesn't like those)
-	 *
-	 * TODO: display a nice walter?
+	 * @see  MWException::report
 	 */
 	function report() {
-		global $wgRequest;
 		$file = $this->getFile();
 		$line = $this->getLine();
 		$message = $this->getMessage();
+		$request = RequestContext::getMain()->getRequest();
 		$url = '[no URL]';
-		if ( isset( $wgRequest ) ) {
-			$url = $wgRequest->getFullRequestURL();
-		}
-		trigger_error("Exception from line $line of $file: $message ($url)", E_USER_ERROR);
 
-		//flush();   // bust the headers_sent check in MWException::report()
+		if ( isset( $request ) ) {
+			$url = $request->getFullRequestURL();
+		}
+
+		trigger_error( "Exception from line {$line} of {$file}: {$message} ({$url})", E_USER_ERROR );
+
+		/*
+		bust the headers_sent check in MWException::report()
+		Uncomment to override normal MWException headers
+		in order to display an error page instead of a 500 error
+		WARNING: Varnish doesn't like those
+		flush();
+		*/
 		parent::report();
 	}
 }
