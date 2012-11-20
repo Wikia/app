a95e34b98102fcad2fb96b7869ba17505402c9ec
Hyun Lim <hyun@wikia-inc.com>
2012-11-20 10:31:54 +0000

    Set default message topic on topic new message

--- extensions/wikia/Forum/js/ForumNewMessageForm.js
+++ extensions/wikia/Forum/js/ForumNewMessageForm.js
@@ -13,9 +13,18 @@ Forum.NewMessageForm = $.createClass(Wall.settings.classBindings.newMessageForm,
 		this.notifyEveryone = this.message.find('.notify-everyone');
 		this.loading = this.message.find('.loadingAjax');
 		this.messageTitle.on('focus', this.proxy(this.messageTitleFocus));
-		this.messageTopic = this.message.find('.message-topic').messageTopic({});
 		this.boardList = $('#BoardList');
 		
+		var topicOptions = {};
+		
+		if(this.boardList.exists()) {
+			this.page = {
+				namespace: $('#Wall').data('board-namespace')
+			};
+			topicOptions['topics'] = [window.wgTitle];
+		}
+		this.messageTopic = this.message.find('.message-topic').messageTopic(topicOptions);
+
 	},
 	afterPost: function(newmsg) {
 		// TODO: this is a hack. We should just be getting the ID back
@@ -29,10 +38,11 @@ Forum.NewMessageForm = $.createClass(Wall.settings.classBindings.newMessageForm,
 	},
 	doPostNewMessage: function(title) {
 		var boardTitle = this.boardList.find('option:selected').val();
-		if(!this.boardList.exists() || (this.boardList.exists() && boardTitle)) {
+		if(!this.boardList.exists()) {
 			Forum.NewMessageForm.superclass.doPostNewMessage.call(this, title);
-		} else {
-			alert('nope');
+		} else if(this.boardList.exists() && boardTitle) {
+			this.page['title'] = boardTitle;
+			Forum.NewMessageForm.superclass.doPostNewMessage.call(this, title);
 		}
 	}
 });
