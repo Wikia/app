6e3e744aa61857ae62b1596587c0360dc7131436
Hyun Lim <hyun@wikia-inc.com>
2012-11-21 00:49:01 +0000

    Made change to Oasis sprite, added dual talk bubble for all article page, and removed obsolete shadow

--- extensions/wikia/Forum/templates/RelatedForumDiscussion_zeroState.php
+++ extensions/wikia/Forum/templates/RelatedForumDiscussion_zeroState.php
@@ -1,4 +1,4 @@
 <p class="forum-zero-state-creative">
-	<img class="sprite talk" src="<?= $wg->BlankImgUrl ?>">
+	<img class="sprite talk-two" src="<?= $wg->BlankImgUrl ?>">
 	<?= $creative ?>
 </p>
