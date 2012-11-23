c32a552e1dc47b79ae939cdb7522d307e8896e8a
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-23 13:18:04 +0100

    add styling to modal form for adding wikitext from article level

--- extensions/wikia/StructuredData/js/StructuredDataInArticle.js
+++ extensions/wikia/StructuredData/js/StructuredDataInArticle.js
@@ -1,37 +1,31 @@
 $('button.add-wikiText-SDObj-from-article').click(function() {
-	$.nirvana.sendRequest({
-		controller: 'StructuredData',
-		method: 'showObject',
-		type: 'GET',
-		format: 'html',
-		data: {
-			type: 'wikia:WikiText',
-			action: 'create'
-		},
-		callback: function(data) {
-			$.showCustomModal(
-				'Add new WikiText Object',
-				//data,
-				'<form class="WikiaForm"><div class="input-group"><label>Name:</label><input type="text" value="" name="schema:name"></div><div class="input-group"><label>WikiText:</label><textarea name="schema:text"></textarea></div></form>',
-				{
-					id: "AddWikiTextSDObject",
-					width: 300,
-					buttons: [
-						{
-							defaultButton:true,
-							message:'Add',
-							handler:function() {
-							}
-						},
-						{
-							message:'Cancel',
-							handler:function() {
-								$('#AddWikiTextSDObject').closeModal();
-							}
+	var modalTitle = 'Add new WikiText Object',
+		modalHtml = '<form class="WikiaForm"><div class="input-group"><label>Name:</label><input type="text" value="" name="schema:name"></div><div class="input-group"><label>WikiText:</label><textarea name="schema:text"></textarea></div></form>',
+		modalId = 'AddWikiTextSDObject',
+		modalWidth = 505;
+	$.getResources([$.getSassCommonURL('extensions/wikia/StructuredData/css/StructuredDataInArticle.scss')]).done(function() {
+		$.showCustomModal(
+			modalTitle,
+			modalHtml,
+			{
+				className: modalId,
+				id: modalId,
+				width: modalWidth,
+				buttons: [
+					{
+						defaultButton:true,
+						message:'Add',
+						handler:function() {
 						}
-					]
-				}
-			);
-		}
+					},
+					{
+						message:'Cancel',
+						handler:function() {
+							$('#AddWikiTextSDObject').closeModal();
+						}
+					}
+				]
+			}
+		);
 	});
 });
