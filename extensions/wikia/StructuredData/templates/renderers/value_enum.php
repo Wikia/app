02391b9f9cf0cc775d354500ad01a4506ae9ac71
mech <mech@wikia-inc.com>
2012-11-26 12:09:28 +0100

    SDS bugid:68469 - fixing enum type

--- extensions/wikia/StructuredData/templates/renderers/value_enum.php
+++ extensions/wikia/StructuredData/templates/renderers/value_enum.php
@@ -7,7 +7,7 @@ if ($context == SD_CONTEXT_EDITING):
 	$currentValue = $object->getValue();
 	foreach ($types['enum'] as $value) {
 		$selected = ($currentValue == $value) ? ' selected="selected"' : '';
-	echo '<option value="' . $value . $selected . '">' . $value . '</option>';
+	echo '<option value="' . $value . '"'.$selected.'>' . $value . '</option>';
 }
 echo '</select>';
 else:
