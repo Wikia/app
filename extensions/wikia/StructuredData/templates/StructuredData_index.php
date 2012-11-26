22b482bdcdc3be0a091f25a42d4f99e59707e3fb
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-26 16:42:46 +0100

    Devide SD Classes into two columns on SP index

--- extensions/wikia/StructuredData/templates/StructuredData_index.php
+++ extensions/wikia/StructuredData/templates/StructuredData_index.php
@@ -1,20 +1,29 @@
-Hello Structured World!
-<ul>
-<?php foreach ( $mainObjects as $objectClass => $objectName ): ?>
-	<li>
-		<a href="?method=getCollection&objectType=<?=$objectClass?>">
-		<?=$objectName;?>
-		</a>
-		<small>( <?=$objectClass ?>)</small>
-	</li>
-<?php endforeach; ?>
-	<li></li>
-<?php foreach ( $advObjects as $objectClass => $objectName ): ?>
-    <li>
-        <a href="?method=getCollection&objectType=<?=$objectClass?>">
-			<?=$objectName;?>
-        </a>
-        <small>( <?=$objectClass ?>)</small>
-    </li>
-<?php endforeach; ?>
-</ul>
+<h1>Hello Structured World!</h1>
+<div class="SDClass-list main">
+	<h3>Main Structure Data Classes</h3>
+	<ul>
+		<?php foreach ( $mainObjects as $objectClass => $objectName ): ?>
+		<li>
+			<a href="?method=getCollection&objectType=<?=$objectClass?>">
+				<?=$objectName;?>
+			</a>
+			<small>( <?=$objectClass ?>)</small>
+		</li>
+		<?php endforeach; ?>
+	</ul>
+</div>
+<div class="SDClass-list">
+	<h3>Secondary Structure Data Classes</h3>
+	<ul class="SDClass-list">
+		<?php foreach ( $advObjects as $objectClass => $objectName ): ?>
+		<li>
+			<a href="?method=getCollection&objectType=<?=$objectClass?>">
+				<?=$objectName;?>
+			</a>
+			<small>( <?=$objectClass ?>)</small>
+		</li>
+		<?php endforeach; ?>
+	</ul>
+</div>
+
+
