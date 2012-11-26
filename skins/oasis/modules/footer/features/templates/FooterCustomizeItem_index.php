276bfa468d58f3af3b439ad0dd5097a1dff8fca7
sebastian-wikia <sebastianm@wikia-inc.com>
2012-11-26 16:33:31 +0000

    Toolbar Refactor small tweaks

--- skins/oasis/modules/footer/features/templates/FooterCustomizeItem_index.php
+++ skins/oasis/modules/footer/features/templates/FooterCustomizeItem_index.php
@@ -1,4 +1,4 @@
 <li>
-    <img height="16" width="16" class="sprite gear" src="<?= $wg->BlankImgUrl; ?>">
+    <img height="16" width="16" class="sprite gear" src="<?= $wg->BlankImgUrl; ?>" />
     <a class="tools-customize" href="#" data-name="customize"><?= wfMsg('oasis-toolbar-customize') ?></a>
 </li>
--- skins/oasis/modules/footer/features/templates/FooterShareItem_index.php
+++ skins/oasis/modules/footer/features/templates/FooterShareItem_index.php
@@ -1,5 +1,5 @@
 <li id="ca-share_feature" class="overflow">
     <a id="control_share_feature"
 	   href="#"
-	   data-name="<?= $data['tracker-name'] ?>"><?= $data['caption'] ?>'</a>
+	   data-name="<?= $data['tracker-name'] ?>"><?= $data['caption'] ?></a>
 </li>
