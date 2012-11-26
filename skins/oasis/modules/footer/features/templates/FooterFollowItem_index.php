80934778503696d5573518041aaebb97835dbfec
sebastian-wikia <sebastianm@wikia-inc.com>
2012-11-26 17:33:06 +0000

    Fix omitted variable reference

--- skins/oasis/modules/footer/features/templates/FooterFollowItem_index.php
+++ skins/oasis/modules/footer/features/templates/FooterFollowItem_index.php
@@ -1,4 +1,6 @@
 <li class="overflow">
-    <a accesskey="w" id="' . $this->rawData['link-id'] . '" href="<?= $data['href'] ?>"
+    <a accesskey="w"
+	   id="<?= $data['link-id'] ?>"
+	   href="<?= $data['href'] ?>"
        data-name="<?= $data['tracker-name'] ?>"><?= $data['caption'] ?></a>
 </li>
