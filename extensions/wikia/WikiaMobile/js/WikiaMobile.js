d9ecb74e4fceafa3e6788e693c506e59c6a71c48
Jakub Olek <jolek@wikia-inc.com>
2012-11-27 14:02:46 +0100

    not needed module

--- extensions/wikia/WikiaMobile/js/WikiaMobile.js
+++ extensions/wikia/WikiaMobile/js/WikiaMobile.js
@@ -29,8 +29,8 @@
 //init
 window.addEventListener('DOMContentLoaded', function () {
 	'use strict';
-	require(['layout', 'querystring', require.optional('topbar'), require.optional('toc'), 'events', require.optional('share'), require.optional('popover'), require.optional('cookies'), 'track'],
-		function (layout, qs, topbar, toc, events, share, popover, cookies, track) {
+	require(['querystring', require.optional('topbar'), require.optional('toc'), 'events', require.optional('share'), require.optional('popover'), require.optional('cookies'), 'track'],
+		function (qs, topbar, toc, events, share, popover, cookies, track) {
 			var d = document,
 				clickEvent = events.click,
 				//add chevrons to elements that need it
