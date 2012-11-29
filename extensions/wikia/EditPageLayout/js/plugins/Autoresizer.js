3282c2281f0ef016048acdf894a1f869360d1b0f
Rafal Leszczynski <rafal@wikia-inc.com>
2012-11-29 12:14:28 +0100

    BugId: 68871, force document to reflow to fix iframe scrolling bug on Safari 6 on OSX 10.8

--- extensions/wikia/EditPageLayout/js/plugins/Autoresizer.js
+++ extensions/wikia/EditPageLayout/js/plugins/Autoresizer.js
@@ -70,6 +70,11 @@
 			this.editboxOffsetHeight = (offsetHeight + footerHeight);
 
 			this.delayedResize();
+
+			// BugId: 68871 force document to reflow (Safari 6 on OSX 10.8 bug - impossible to scroll iframe content)
+			if (CKEDITOR.env.webkit) {
+				this.editbox.css('overflow', 'scroll');
+			}
 		},
 
 		delayedResize: function() {
