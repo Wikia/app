3adc37ac29a8f58fd444ef42263ee862b6a90f5e
Kamil Koterba <kamil@wikia-inc.com>
2012-11-30 23:54:07 +0000

    BugId:45249 added NoTrans so it pastes template code instead of parsed template code

--- extensions/wikia/AchievementsII/services/AchAwardingService.class.php
+++ extensions/wikia/AchievementsII/services/AchAwardingService.class.php
@@ -626,7 +626,7 @@ class AchAwardingService {
 						$origIP = $wgIP;
 						$wgIP = '127.0.0.1';
 
-						$userPageArticle->doEdit( wfMsgForContent('welcome-user-page', $userPageTitle->getText() ), '', $userWikia->isAllowed('bot') ? EDIT_FORCE_BOT : 0, false, $userWikia );
+						$userPageArticle->doEdit( wfMsgForContentNoTrans('welcome-user-page', $userPageTitle->getText() ), '', $userWikia->isAllowed('bot') ? EDIT_FORCE_BOT : 0, false, $userWikia );
 
 						//restore original IP from user session
 						$wgIP = $origIP;
