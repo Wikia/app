23fd93f9060a3091e863ffc12b92a40386d9c798
Marcin Maciejewski <marcin@wikia-inc.com>
2012-11-30 16:03:19 +0000

    bugid-68791: curation tools pulse: Templating and CSS, WikiaForm introduced

--- extensions/wikia/SpecialMarketingToolbox/modules/MarketingToolboxModulePulseService.class.php
+++ extensions/wikia/SpecialMarketingToolbox/modules/MarketingToolboxModulePulseService.class.php
@@ -6,5 +6,106 @@ class MarketingToolboxModulePulseService extends MarketingToolboxModuleService {
 			'halo' => 2
 		);
 	}
+	public function renderEditor($data) {
+		$data['form'] = array(
+			'inputs' => array(
+				array(
+					'type' => 'raw',
+					'output' => '<div class="grid-4 alpha url-and-topic">',
+				),
+				array(
+					'type' => 'text',
+					'name' => 'boardTitle',
+					'isRequired' => true,
+					'label' => wfMsg('marketing-toolbox-hub-module-pulse-wikiurl'),
+					'attributes' => array(
+						'maxlength' => '40'
+					),
+				),
+				array(
+					'type' => 'text',
+					'name' => 'boardDescription',
+					'isRequired' => true,
+					'label' => wfMsg('marketing-toolbox-hub-module-pulse-topic'),
+					'attributes' => array(
+						'maxlength' => '255'
+					),
+				),
+				array(
+					'type' => 'raw',
+					'output' => '</div>',
+				),
+				array(
+					'type' => 'raw',
+					'output' => '<div class="grid-2 alpha">',
+				),
+				array(
+					'type' => 'text',
+					'name' => 'boardTitle',
+					'isRequired' => true,
+					'label' => wfMsg('marketing-toolbox-hub-module-pulse-stat1'),
+					'attributes' => array(
+						'maxlength' => '40'
+					),
+				),
+				array(
+					'type' => 'text',
+					'name' => 'boardDescription',
+					'isRequired' => true,
+					'label' => wfMsg('marketing-toolbox-hub-module-pulse-stat2'),
+					'attributes' => array(
+						'maxlength' => '255'
+					),
+				),
+				array(
+					'type' => 'text',
+					'name' => 'boardDescription',
+					'isRequired' => true,
+					'label' => wfMsg('marketing-toolbox-hub-module-pulse-stat3'),
+					'attributes' => array(
+						'maxlength' => '255'
+					),
+				),
+				array(
+					'type' => 'raw',
+					'output' => '</div><div class="grid-2 alpha">',
+				),
+				array(
+					'type' => 'text',
+					'name' => 'boardTitle',
+					'isRequired' => true,
+					'label' => wfMsg('marketing-toolbox-hub-module-pulse-number1'),
+					'attributes' => array(
+						'maxlength' => '40'
+					),
+				),
+				array(
+					'type' => 'text',
+					'name' => 'boardDescription',
+					'isRequired' => true,
+					'label' => wfMsg('marketing-toolbox-hub-module-pulse-number2'),
+					'attributes' => array(
+						'maxlength' => '255'
+					),
+				),
+				array(
+					'type' => 'text',
+					'name' => 'boardDescription',
+					'isRequired' => true,
+					'label' => wfMsg('marketing-toolbox-hub-module-pulse-number3'),
+					'attributes' => array(
+						'maxlength' => '255'
+					),
+				),
+				array(
+					'type' => 'raw',
+					'output' => '</div>',
+				),
+			),
+			'method' => 'post',
+			'action' => '',
+		);
+		return parent::renderEditor($data);
+	}
 }
 ?>
