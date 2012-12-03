d33b60f312c21b7969289d5eacd161734a8bdef6
Marcin Maciejewski <marcin@wikia-inc.com>
2012-12-03 16:35:30 +0000

    bugid-68795: curation tools pulse: Implement JS validation in a reusable way (template changes)

--- extensions/wikia/SpecialMarketingToolbox/templates/MarketingToolbox_FormField.php
+++ extensions/wikia/SpecialMarketingToolbox/templates/MarketingToolbox_FormField.php
@@ -1,1 +1,6 @@
-form inputText template test
+<label><?= wfMsg('marketing-toolbox-hub-module-pulse-wikiurl'); ?></label>
+<div class="headline-wrapper">
+	<input data-min="50" data-max="200" type="text" name="title" value="" placeholder="">
+	<p class="headline-character-counter"></p>
+	<p class="error error-msg"></p>
+</div>
