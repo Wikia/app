ad2fda2c317f8c8c726b7fb9af5fe4c2f68bba24
Hyun Lim <hyun@wikia-inc.com>
2012-12-04 00:27:06 +0000

    bugId::64185 fixed create new wiki's theme sizing issue

--- extensions/wikia/CreateNewWiki/js/CreateNewWikiSupplemental.js
+++ extensions/wikia/CreateNewWiki/js/CreateNewWikiSupplemental.js
@@ -4,7 +4,7 @@ ThemeDesigner.init = function() {
 };
 ThemeDesigner.set = function(setting, newValue) {
 	ThemeDesigner.settings = themes[newValue];
-	var sassUrl = $.getSassCommonURL('/skins/oasis/css/oasis.scss', ThemeDesigner.settings);
+	var sassUrl = $.getSassCommonURL('/skins/oasis/css/oasis.scss', $.extend(ThemeDesigner.settings, window.applicationThemeSettings));
 	$.getCSS(sassUrl, function(link) {
 		$(ThemeDesigner.link).remove();
 		ThemeDesigner.link = link;
