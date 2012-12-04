39bd4c24520781e17006175cfcd1a950ac4306df
Damian Jozwiak <damian@wikia-inc.com>
2012-12-04 17:09:29 +0100

    bugid-68448 | S:MTb fixed XSS

--- extensions/wikia/SpecialMarketingToolbox/templates/MarketingToolbox_FormField.php
+++ extensions/wikia/SpecialMarketingToolbox/templates/MarketingToolbox_FormField.php
@@ -2,7 +2,7 @@
 	<? if (!empty($inputData['label'])): ?>
 		<label for="<?=$inputData['name']?>"><?=$inputData['label']?></label>
 	<? endif ?>
-	<input type="text" <?=$inputData['attributes']?> name="<?=$inputData['name']?>" value="<?=$inputData['value']?>" />
+	<input type="text" <?=$inputData['attributes']?> name="<?=$inputData['name']?>" value="<?= htmlspecialchars($inputData['value'])?>" />
 
 	<p class="error error-msg"><?=$inputData['errorMessage']?></p>
 </div>
