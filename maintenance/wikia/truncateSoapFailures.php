146fa16f52c2e65817752fe6d43b0544af12008a
moliwikia <moli@wikia-inc.com>
2012-12-04 13:14:09 +0100

    fix minor bugs and added message after finishing script

--- maintenance/wikia/truncateSoapFailures.php
+++ maintenance/wikia/truncateSoapFailures.php
@@ -4,14 +4,14 @@
  *
  * This script is meant to be run approximately every 10 days.
  *
- * @author Sean Colombo
+ * @author Maciej Szumocki
  */
 
 require_once( dirname( __FILE__ ) . '/../Maintenance.php' );
 
 class TruncateSoapFailures extends Maintenance {
-	const TABLE_NAME = "lw_soap_failures";
-	const DB_NAME = "lyricwiki";
+	private $TABLE_NAME = "lw_soap_failures";
+	private $DB_NAME = "lyricwiki";
 	
 	public function __construct() {
 		parent::__construct();
@@ -26,6 +26,8 @@ class TruncateSoapFailures extends Maintenance {
 		$res = $dbw->query("TRUNCATE ".$this->TABLE_NAME);
 		if(!$res) {
 			$this->output("ERROR WHILE TRYING TO TRUNCATE ".$this->DB_NAME.".".$this->TABLE_NAME."\n");
+		} else {
+			$this->output("Table ".$this->DB_NAME.".".$this->TABLE_NAME." has been cleared\n");
 		}
 	}
 
