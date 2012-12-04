f52c882caa1041d6b060bb98245c5f1cf9732ec4
Maciej Szumocki <szumo@wikia-inc.com>
2012-12-04 10:31:50 +0100

    BugId: 52581 truncateSoapFailures maintentance script for LyricWiki

--- maintenance/wikia/truncateSoapFailures.php
+++ maintenance/wikia/truncateSoapFailures.php
@@ -7,16 +7,33 @@
  * @author Sean Colombo
  */
 
-$dir = dirname(__FILE__);
-require_once($dir . "/../commandLine.inc");
+require_once( dirname( __FILE__ ) . '/../Maintenance.php' );
 
-$TABLE_NAME = "lw_soap_failures";
-if (isset($options['help'])) {
-	die( "This script will completely truncate the $TABLE_NAME table for LyricWiki.  It should be run every 10 days via cron to keep the table small enough that it can be used quickly.  It can be run any time needed (but it helps us to have a few days of data in there)." );
+class TruncateSoapFailures extends Maintenance {
+	const TABLE_NAME = "lw_soap_failures";
+	const DB_NAME = "lyricwiki";
+	
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription("This script will completely truncate the ".$this->TABLE_NAME." table for ".$this->DB_NAME,
+				      "It should be run every 10 days via cron to keep the table small enough that it can be ".
+				      "used quickly. It can be run any time needed (but it helps us to have a few days of data ".
+				      "in there).");
+	}
+	
+	public function execute() {
+		$dbw = $this->getDB(DB_MASTER, array(), $this->DB_NAME);
+		$res = $dbw->query("TRUNCATE ".$this->TABLE_NAME);
+		if(!$res) {
+			$this->output("ERROR WHILE TRYING TO TRUNCATE ".$this->DB_NAME.".".$this->TABLE_NAME."\n");
+		}
+	}
+
+	public function getDbType() {
+		return Maintenance::DB_ADMIN;
+	}
 }
 
-$dbw = wfGetDB(DB_MASTER, array(), 'lyricwiki');
-$res = $dbw->query("TRUNCATE $TABLE_NAME");
-if(!$res){
-	print "ERROR WHILE TRYING TO TRUNCATE $TALBE_NAME!!!\n";
-}
+$maintClass = "TruncateSoapFailures";
+require_once( RUN_MAINTENANCE_IF_MAIN );
+?>
