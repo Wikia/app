ae24f99330713c0a3d85dc5a197053ee6ca0a014
mech <mech@wikia-inc.com>
2012-12-06 11:41:59 +0100

    bugid:69036 - reapplying Oasis related changes from bugid:6957

--- skins/oasis/modules/ContentDisplayController.class.php
+++ skins/oasis/modules/ContentDisplayController.class.php
@@ -75,10 +75,20 @@ class ContentDisplayController extends WikiaController {
 			return true;
 		}
 
-		$ret = "<h$level$attribs"
-			. "<span class=\"mw-headline\" id=\"$anchor\">$text</span>"
-			. $link
-			. "</h$level>";
+		if ($skin instanceof SkinOasis) {
+			$levelClass = ($level == '2') ? '-h2' : '';
+			$ret = "<div class=\"headline-wrapper$levelClass\">" .
+				"<h$level$attribs" .
+				" <span class=\"mw-headline\" id=\"$anchor\">$text</span>" .
+				"</h$level>" .
+				$link .
+				'</div>';
+		} else {
+			$ret = "<h$level$attribs"
+				. "<span class=\"mw-headline\" id=\"$anchor\">$text</span>"
+				. $link
+				. "</h$level>";
+		}
 
 		wfProfileOut(__METHOD__);
 		return true;
