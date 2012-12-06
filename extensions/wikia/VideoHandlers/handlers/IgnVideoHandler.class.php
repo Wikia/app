9ae7733cb80864283a919478d4a2fdce85c9e7ba
saipetch <saipetch@wikia-inc.com>
2012-12-06 15:08:02 -0800

    Video (bugid 68737): IGN - update player

--- extensions/wikia/VideoHandlers/handlers/IgnVideoHandler.class.php
+++ extensions/wikia/VideoHandlers/handlers/IgnVideoHandler.class.php
@@ -5,7 +5,7 @@ class IgnVideoHandler extends VideoHandler {
 	protected $apiName = 'IgnApiWrapper';
 	protected static $urlTemplate = '';
 	protected static $providerDetailUrlTemplate = 'http://www.ign.com/watch?v=$1';
-	protected static $providerPlayerUrl = 'http://media.ign.com/ev/prod/embed.swf';
+	protected static $providerPlayerUrl = 'http://widgets.ign.com/video/embed/content.html';
 	protected static $providerHomeUrl = 'http://www.ign.com/';
 	protected static $autoplayParam = "qs_autoplay";
 	protected static $autoplayValue = "true";
@@ -15,15 +15,14 @@ class IgnVideoHandler extends VideoHandler {
 	}
 
 	private function getEmbedNative($width, $autoplay=false) {
-		// YouTube parameters: http://code.google.com/apis/youtube/player_parameters.html
 		$height =  $this->getHeight( $width );
-		$videoUrl = $this->getEmbedUrl();
-		$autoplay = $autoplay ? '?' . self::$autoplayParam . '=' . self::$autoplayValue : '';
-		$playerUrl = self::$providerPlayerUrl;
+		$autoplay = $autoplay ? '&' . self::$autoplayParam . '=' . self::$autoplayValue : '';
+		$url = self::$providerPlayerUrl.'?url='.$this->getEmbedUrl().$autoplay;
 
 		$code = <<<EOT
-		<object id="vid_{$this->videoId}" class="ign-videoplayer" width="$width" height="$height" data="{$playerUrl}" type="application/x-shockwave-flash"><param name="movie" value="{$playerUrl}" /><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="bgcolor" value="#000000" /><param name="flashvars" value="url={$videoUrl}{$autoplay}"/></object>
+<iframe src="{$url}" width="{$width}" height="{$height}" scrolling="no" frameborder="0" allowfullscreen></iframe>
 EOT;
+
 		return $code;
 	}
 
