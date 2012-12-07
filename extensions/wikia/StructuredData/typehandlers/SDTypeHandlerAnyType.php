943486625dadd333aad56c1675f170004c4b9f77
jacekjursza <jacek@wikia-inc.com>
2012-12-07 12:19:31 +0100

    SDS: disable schema:name checking on MediaWiki side

--- extensions/wikia/StructuredData/typehandlers/SDTypeHandlerAnyType.php
+++ extensions/wikia/StructuredData/typehandlers/SDTypeHandlerAnyType.php
@@ -6,10 +6,11 @@ class SDTypeHandlerAnyType extends SDTypeHandler {
 
 	public function handleSaveData( array $data ) {
 
+		/*
 		if ( empty( $data['schema:name'] ) ) {
 			$this->addError( 'schema:name', wfMsg('structureddata-name-can-not-be-empty') );
 		}
-
+		 */
 		return $data;
 	}
 }
