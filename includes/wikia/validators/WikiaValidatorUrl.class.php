7cf27e30fd4b3b18e0f984405235943c185799b0
Damian Jozwiak <damian@wikia-inc.com>
2012-12-07 14:23:10 +0000

    bugid-68795 | S:MTb Url validator passed regex to javascript

--- includes/wikia/validators/WikiaValidatorUrl.class.php
+++ includes/wikia/validators/WikiaValidatorUrl.class.php
@@ -1,9 +1,10 @@
 <?php
 
 	class WikiaValidatorUrl extends WikiaValidatorRegex {
+		const URL_REGEX = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/i';
 
 		protected function config( array $options = array() ) {
-			$this->setOption('pattern', '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/i' );
+			$this->setOption('pattern', self::URL_REGEX);
 		}
 
 	}
