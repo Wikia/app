14fef69a0f726f833e45c6df0954c97cfd2b5287
sebastian-wikia <sebastianm@wikia-inc.com>
2012-12-07 15:11:03 +0000

    Adding unit test

--- includes/wikia/validators/WikiaValidatorUrl.class.php
+++ includes/wikia/validators/WikiaValidatorUrl.class.php
@@ -1,7 +1,7 @@
 <?php
 
 	class WikiaValidatorUrl extends WikiaValidatorRegex {
-		const URL_REGEX = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/i';
+		const URL_REGEX = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-\?#]*)*\/?$/i';
 
 		protected function config( array $options = array() ) {
 			$this->setOption('pattern', self::URL_REGEX);
