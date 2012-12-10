a706805d3d066227042ce31c416e7848e4e99d7d
Federico "Lox" Lucignano <federico.lox@gmail.com>
2012-12-10 17:55:38 +0100

    improved docs, added private scope

--- resources/wikia/libraries/define.mock.js
+++ resources/wikia/libraries/define.mock.js
@@ -1,23 +1,46 @@
 /**
- * AMD define mock to unit test AMD modules.
+ * Mock of the AMD define function meant for unit tests focusing on AMD modules.
+ *
+ * @author  Piotr Gabryjeluk <rychu@wikia-inc.com>
+ *
+ * @example
  *
  * Usage:
  *
- * In JavaScript test include this file before including the tested module.
- * Whatever is defined as AMD module will be available in test via define.getModule.
- * This let's you initialize module multiple times and mock it's dependencies.
+ * In the JavaScript test include this file before including the module to test,
+ * the module definition will then be available throughout the test via define.getModule();
+ * this will re-initialize the module every time it's called in the code, allowing for multiple
+ * instances to be tested in different case inside the same test scenario, while also making mocking
+ * define-time dependencies inline.
  *
- * Don't use modil and this mock at the same time in tests.
+ * WARNING: Don't use any other AMD module loader (such as Require.js and modil.js) at the same time,
+ * as the definitions of the define function will clash.
  *
- * See LazyQueueTest.js or CacheTest.js as examples of how to use this define mock.
+ * @example
+ *
+ * //mockedMod will contain a reference to an isolated instance of the module to test
+ * var mockedMod = define.getModule();
+ *
+ * //mockedMod2 will containa  reference to an isolated instance of the module to test
+ * //passing in mockedDep1 and mockedDep2 as the define-time dependencies
+ * //to match modules definition like: define('myMod', ['dep1', 'dep2'], function (dep1, dep2){ ... });
+ * var mockedMod2 = define.getModule(mockedDep1, mockedDep2);
+ *
+ * @see  LazyQueueTest.js
+ * @see  CacheTest.js
  */
 
-function define(id, deps, def) {
-	var undef;
-	if (def === undef) {
-		def = deps;
-	}
-	define.getModule = def;
-}
+(function (context) {
+	'use strict';
 
-define.amd = true;
+	function define(id, deps, def) {
+		var undef;
+		if (def === undef) {
+			def = deps;
+		}
+		define.getModule = def;
+	}
+
+	define.amd = true;
+	context.define = define;
+}(this));
