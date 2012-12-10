4ce4ca76f552daeb7f2eee85261f25db95a3ae12
Marcin Maciejewski <marcin@wikia-inc.com>
2012-12-10 14:19:53 +0000

    bugid-69139: Investigate pulling data from VET to specialPage

--- extensions/wikia/SpecialMarketingToolbox/js/EditHub.js
+++ extensions/wikia/SpecialMarketingToolbox/js/EditHub.js
@@ -1,11 +1,14 @@
 var EditHub = function() {};
 
+var vet_back,vet_close; //hack for VET, please remove this line after VET refactoring
+
 EditHub.prototype = {
 	form: undefined,
 	wmuDeffered: undefined,
 
 	init: function () {
 		$('.MarketingToolboxMain .wmu-show').click($.proxy(this.wmuInit, this));
+		$('.MarketingToolboxMain .vet-show').click($.proxy(this.vetInit, this));
 
 		this.form = $('#marketing-toolbox-form');
 
@@ -43,6 +46,23 @@ EditHub.prototype = {
 		$(window).bind('WMU_addFromSpecialPage', $.proxy(function(event, wmuData) {
 			this.addImage(wmuData);
 		}, this));
+	},
+
+	vetInit: function(event) {
+		var vet_back,vet_close;
+		$.when(
+			$.loadYUI(),
+			$.getScript(wgResourceBasePath + '/resources/wikia/libraries/mustache/jquery.mustache.js'),
+			$.getResources($.getSassCommonURL('/extensions/wikia/VideoEmbedTool/css/VET.scss')),
+			$.getScript(wgExtensionsPath + '/wikia/WikiaStyleGuide/js/Dropdown.js'),
+			$.getResources($.getSassCommonURL('/extensions/wikia/WikiaStyleGuide/css/Dropdown.scss')),
+			$.getScript(wgExtensionsPath + '/wikia/VideoEmbedTool/js/VET.js')
+		).then(function() {
+			VET_show();
+			$(window).bind('VET_addFromSpecialPage', $.proxy(function(event, vetData) {
+				$().log(vetData);
+			}, this));
+		});
 	},
 
 	addImage: function(wmuData) {
