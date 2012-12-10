c777b81cced20139d0926bbea9c8097bd9df40a4
Piotr Gabryjeluk <rychu@wikia-inc.com>
2012-12-10 20:57:30 +0100

    Update resources/wikia/libraries/define.mock.js
    Documentation updated

--- resources/wikia/libraries/define.mock.js
+++ resources/wikia/libraries/define.mock.js
@@ -7,24 +7,26 @@
  *
  * Usage:
  *
- * In the JavaScript test include this file before including the module to test,
- * the module definition will then be available throughout the test via define.getModule();
- * this will re-initialize the module every time it's called in the code, allowing for multiple
- * instances to be tested in different case inside the same test scenario, while also making mocking
- * define-time dependencies inline.
+ * In the JavaScript test include this file before including the module to test.
+ * The module definition will then be available throughout the test via define.getModule().
+ * This will initialize the module every time the "getModule" is called.
  *
- * WARNING: Don't use any other AMD module loader (such as Require.js and modil.js) at the same time,
- * as the definitions of the define function will clash.
+ * It allows multiple instances of the module to be tested in different cases inside the same
+ * test scenario, and makes mocking define-time dependencies inline.
+ *
+ * WARNING: Don't use any other AMD module loader (such as Require.js and modil.js) at the
+ * same time, as two implementations of the define function will clash (or at least will
+ * makes no sense).
  *
  * @example
  *
- * //mockedMod will contain a reference to an isolated instance of the module to test
- * var mockedMod = define.getModule();
+ * // testedModule will contain a reference to an isolated instance of the module to test
+ * var testedModule = define.getModule();
  *
- * //mockedMod2 will containa  reference to an isolated instance of the module to test
- * //passing in mockedDep1 and mockedDep2 as the define-time dependencies
- * //to match modules definition like: define('myMod', ['dep1', 'dep2'], function (dep1, dep2){ ... });
- * var mockedMod2 = define.getModule(mockedDep1, mockedDep2);
+ * // testedModuleWithMockedDeps will contain a reference to an isolated instance of the module to test
+ * // passing in mockedDep1 and mockedDep2 as the define-time dependencies
+ * // matching module definition like: define('myMod', ['dep1', 'dep2'], function (dep1, dep2) { ... });
+ * var testedModuleWithMockedDeps = define.getModule(mockedDep1, mockedDep2);
  *
  * @see  LazyQueueTest.js
  * @see  CacheTest.js
