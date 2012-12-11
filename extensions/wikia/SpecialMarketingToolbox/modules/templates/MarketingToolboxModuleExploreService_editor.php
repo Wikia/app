842b444e5386aa5d6df68c98debe73d30f78cf19
Marcin Maciejewski <marcin@wikia-inc.com>
2012-12-11 10:47:03 +0000

    bugid-84692: explore module: Templating / CSS

--- extensions/wikia/SpecialMarketingToolbox/modules/templates/MarketingToolboxModuleExploreService_editor.php
+++ extensions/wikia/SpecialMarketingToolbox/modules/templates/MarketingToolboxModuleExploreService_editor.php
@@ -1,59 +1,50 @@
-<div class="grid-4 alpha wide">
-	<?= F::app()->renderView(
-			'MarketingToolbox',
-			'FormField',
-			array('inputData' => $fields['exploreTitle'])
-		);
-	?>
-	<?= F::app()->renderView(
-			'MarketingToolbox',
-			'FormField',
-			array('inputData' => $fields['exploreSectionHeader1'])
-		);
-	?>
-	<?= F::app()->renderView(
+<div class="grid-2 alpha">
+	<?=F::app()->renderView(
 		'MarketingToolbox',
 		'FormField',
-		array('inputData' => $fields['exploreLinkHeader1a'])
-	);
+	array('inputData' => $fields['exploreLinkHeader1a'])
+);
 	?>
-	<?= F::app()->renderView(
+	<?=F::app()->renderView(
+		'MarketingToolbox',
+		'FormField',
+	array('inputData' => $fields['exploreLinkHeader1b'])
+);
+	?>
+	<?=F::app()->renderView(
+	'MarketingToolbox',
+	'FormField',
+	array('inputData' => $fields['exploreLinkHeader1c'])
+);
+	?>
+	<?=F::app()->renderView(
+		'MarketingToolbox',
+		'FormField',
+	array('inputData' => $fields['exploreLinkHeader1d'])
+);
+	?>
+</div>
+
+<div class="grid-2 alpha">
+	<?=F::app()->renderView(
 		'MarketingToolbox',
 		'FormField',
 		array('inputData' => $fields['exploreLinkUrl1a'])
 	);
 	?>
-	<?= F::app()->renderView(
-		'MarketingToolbox',
-		'FormField',
-		array('inputData' => $fields['exploreLinkHeader1b'])
-	);
-	?>
-	<?= F::app()->renderView(
+	<?=F::app()->renderView(
 		'MarketingToolbox',
 		'FormField',
 		array('inputData' => $fields['exploreLinkUrl1b'])
 	);
 	?>
-	<?= F::app()->renderView(
-		'MarketingToolbox',
-		'FormField',
-		array('inputData' => $fields['exploreLinkHeader1c'])
-	);
-	?>
-	<?= F::app()->renderView(
+	<?=F::app()->renderView(
 		'MarketingToolbox',
 		'FormField',
 		array('inputData' => $fields['exploreLinkUrl1c'])
 	);
 	?>
-	<?= F::app()->renderView(
-		'MarketingToolbox',
-		'FormField',
-		array('inputData' => $fields['exploreLinkHeader1d'])
-	);
-	?>
-	<?= F::app()->renderView(
+	<?=F::app()->renderView(
 		'MarketingToolbox',
 		'FormField',
 		array('inputData' => $fields['exploreLinkUrl1d'])
