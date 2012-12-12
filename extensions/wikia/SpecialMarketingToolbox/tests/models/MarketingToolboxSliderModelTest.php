c7d157ca3738845fa6b05d11de1683702dc6930a
Damian Jozwiak <damian@wikia-inc.com>
2012-12-12 15:05:24 +0000

    fixed unit tests

--- extensions/wikia/SpecialMarketingToolbox/tests/models/MarketingToolboxSliderModelTest.php
+++ extensions/wikia/SpecialMarketingToolbox/tests/models/MarketingToolboxSliderModelTest.php
@@ -1,6 +1,15 @@
 <?
 
 class MarketingToolboxSliderModelTest extends WikiaBaseTest {
+	/**
+	 * (non-PHPdoc)
+	 * @see WikiaBaseTest::setUp()
+	 */
+	public function setUp() {
+		$this->setupFile = dirname(__FILE__) . '/../../SpecialMarketingToolbox.setup.php';
+		parent::setUp();
+	}
+
 	public function testGetSlidesCount() {
 		$model = new MarketingToolboxSliderModel();
 
