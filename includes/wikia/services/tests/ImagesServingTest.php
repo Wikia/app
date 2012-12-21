774944b298e1cc3e879934dce91dd976810d7a3a
nAndy <nandy@wikia-inc.com>
2012-12-21 11:13:42 +0000

    unit tests form ImagesServing::calculateScaledImageSizes

--- includes/wikia/services/tests/ImagesServingTest.php
+++ includes/wikia/services/tests/ImagesServingTest.php
@@ -15,21 +15,30 @@ class ImagesServingTest extends WikiaBaseTest {
 	
 	public function calculateScaledImageSizesDataProvider() {
 		$resultSizes = array(
-			array(0, 0),
-			array(0, 0),
-			array(150, 150),
+			array('width' => 0, 'height' => 0),
+			array('width' => 0, 'height' => 0),
+			array('width' => 150, 'height' => 150),
+			array('width' => 180, 'height' => 90),
+			array('width' => 90, 'height' => 180),
+			array('width' => 133, 'height' => 106),
+			array('width' => 29, 'height' => 155),
 		);
+		
 		foreach($resultSizes as $data) {
 			$result = new stdClass();
-			$result->width = $data[0];
-			$result->height = $data[1];
+			$result->width = $data['width'];
+			$result->height = $data['height'];
 			$results[] = $result;
 		}
 		
 		return array(
 			array(0, 0, 0, $results[0]), 
 			array(0, 150, 150, $results[1]),
-			array(150, 150, 150, $results[2])
+			array(150, 150, 150, $results[2]),
+			array(180, 560, 280, $results[3]),
+			array(180, 280, 560, $results[4]),
+			array(133, 600, 480, $results[5]),
+			array(155, 123, 654, $results[6]),
 		);
 	}
 }
