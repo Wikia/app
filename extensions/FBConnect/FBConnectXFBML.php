afbf1a5a9f9976b9f2d6335fa3a04febf15a8131
macbre <maciej.brencz@gmail.com>
2013-01-02 14:51:53 +0100

    Facebook <fb:> tags broken

--- extensions/FBConnect/FBConnectXFBML.php
+++ extensions/FBConnect/FBConnectXFBML.php
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,10 +26,10 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 
 /**
  * Class FBConnectXFBML
- * 
+ *
  * This class allows FBML (Facebook Markup Language, an extension to HTML) to
  * be incorporated into the wiki through XFBML.
- * 
+ *
  * See: <http://wiki.developers.facebook.com/index.php/XFBML>
  */
 class FBConnectXFBML {
@@ -45,7 +45,7 @@ class FBConnectXFBML {
 		switch ($tag) {
 			case '':
 				break; // Error: We shouldn't be here!
-			
+
 			// To implement a custom XFBML tag handler, simply case it here like so
 			#case 'fb:login-button':
 			case 'fb:login-button-perms':
@@ -55,7 +55,7 @@ class FBConnectXFBML {
 			case 'fb:serverfbml':
 				// TODO: Is this safe? Does it respect $fbAllowFacebookImages?
 				$attrs = self::implodeAttrs( $args );
-				return "<fb:serverfbml{$attrs}>$text</fb:serverfbml>";
+				return '<fb:serverfbml' . $attrs . '>' . $text . '</fb:serverfbml>';
 			case 'fb:profile-pic':
 			case 'fb:photo':
 			case 'fb:video':
@@ -73,12 +73,20 @@ class FBConnectXFBML {
 			default:
 				// Allow other tags by default
 				$attrs = self::implodeAttrs( $args );
+
+				// load Facebook's JS API on demand (BugId:68983)
+				if ($parser instanceof Parser) {
+					/* @var $parser Parser */
+					$parser->getOutput()->addModules('ext.facebook.FBXML');
+				}
+
+				/* @var $parser DummyParser */
 				return "<{$tag}{$attrs}>" . $parser->recursiveTagParse( $text ) . "</$tag>";
 		}
 		// Strip the tag entirely
 		return '';
 	}
-	
+
 	/**
 	 * Helper function to create name-value pairs from the list of attributes passed to the
 	 * parser hook.
@@ -95,7 +103,7 @@ class FBConnectXFBML {
 		}
 		return $attrs;
 	}
-	
+
 	/**
 	 * Helper function for parserHook. Originally, all tags were directed to
 	 * that function, but I had no way of knowing which tag provoked the function.
@@ -105,7 +113,7 @@ class FBConnectXFBML {
 		$code = 'return FBConnectXFBML::parserHook($text,$args,$parser,\''.$tag.'\');';
 		return create_function($args, $code);
 	}
-	
+
 	/**
 	 * Returns true if XFBML is enabled (i.e. $fbUseMarkup is not false).
 	 * Defaults to true if $fbUseMarkup is not set.
@@ -114,20 +122,20 @@ class FBConnectXFBML {
 		global $fbUseMarkup;
 		return !isset($fbUseMarkup) || $fbUseMarkup;
 	}
-	
+
 	/**
 	 * Returns the availabe XFBML tags. For now, this is just an array copied from
 	 * <http://wiki.developers.facebook.com/index.php/XFBML> and the second-to-last line in
 	 * <http://static.ak.fbcdn.net/rsrc.php/zAE5U/lpkg/2netpns0/en_US/141/136684/js/connect.js.pkg.php>.
 	 * Eventually, this method can be replaced with one that gathers these tags
 	 * from the internet (e.g. by downloading an xml or js file) in real time.
-	 * 
+	 *
 	 * This is necessary because the Facebook Platform is so new, and major updates
 	 * are being pushed out every week. With such turmoil regarding the available tags
 	 * and the features they offer, our list of tags should not be hardcoded into this file.
-	 * 
+	 *
 	 * But for now... HELP! Where does Firefox pull in the XFBML tags in from??
-	 * 
+	 *
 	 * After switching to the new JavaScript SDK, these will be the only tags
 	 * implemented for a while: <http://github.com/facebook/connect-js/xfbml>.
 	 *
@@ -138,7 +146,7 @@ class FBConnectXFBML {
 			// If XFBML isn't enabled, then don't report any tags
 			return array( );
 		}
-		
+
 		// XFBML tags in the beta version of the Facebook Connect JavaScript SDK
 		// <http://wiki.github.com/facebook/connect-js/xfbml/5>
 		$tags = array('fb:comments',
@@ -183,7 +191,7 @@ class FBConnectXFBML {
 		              #'fb:userlink',
 		              #'fb:video',
 		);
-		
+
 		// Reject discarded tags (that return an empty string) from Special:Version
 		$tempParser = new DummyParser();
 		foreach( $tags as $i => $tag ) {
@@ -200,7 +208,7 @@ class FBConnectXFBML {
 
 /**
  * Class DummyParser
- * 
+ *
  * Allows FBConnectXML::availableTags() to pre-sanatize the list of tags reported to
  * MediaWiki, excluding any tags that result in the tag being replaced by an empty
  * string. Sorry for the confusing summary here, its really late. =)
