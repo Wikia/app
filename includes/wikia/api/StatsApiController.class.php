8cb5d41447e0f168ed455984176b3b98bad54891
Jakub Olek <jolek@wikia-inc.com>
2013-01-03 14:56:33 +0100

    docs for Api discovery

--- includes/wikia/api/StatsApiController.class.php
+++ includes/wikia/api/StatsApiController.class.php
@@ -12,7 +12,14 @@ class StatsApiController extends WikiaApiController {
 	/**
 	 * Get statistical information about the current wiki
 	 *
-	 * @responseParam object stats Stats about wiki edits | articles | pages | users | activeUsers | images | videos | admins
+	 * @responseParam Integer $edits Number of edits on a wiki
+	 * @responseParam Integer $articles Number of real articles on a wiki
+	 * @responseParam Integer $pages Number of all pages on a wiki (eg. File pages, Articles, Category pages ...)
+	 * @responseParam Integer $users Stats Number of all users in a wiki
+	 * @responseParam Integer $activeUsers Number of active users on a wiki
+	 * @responseParam Integer $images Number of all images on a wiki
+	 * @responseParam Integer $videos Number of all videos on a wiki
+	 * @responseParam Integer $admins Number of all admins on a wiki
 	 *
 	 * @example
 	 */
