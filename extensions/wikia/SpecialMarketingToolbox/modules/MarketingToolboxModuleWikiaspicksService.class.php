bd8ec43d9304add105c1c8e662de764bc445aa8d
nAndy <nandy@wikia-inc.com>
2013-01-03 13:57:40 +0000

    added validation rules; bugId:94781

--- extensions/wikia/SpecialMarketingToolbox/modules/MarketingToolboxModuleWikiaspicksService.class.php
+++ extensions/wikia/SpecialMarketingToolbox/modules/MarketingToolboxModuleWikiaspicksService.class.php
@@ -1,7 +1,43 @@
-<?
+<?php
 class MarketingToolboxModuleWikiaspicksService extends MarketingToolboxModuleService {
 	protected function getFormFields() {
-		return array();
+		$fields = array(
+			'fileName' => array(
+				'type' => 'hidden',
+				'attributes' => array(
+					'class' => 'wmu-file-name-input'
+				),
+				'validator' => new WikiaValidatorFileTitle(
+					array(),
+					array('wrong-file' => 'marketing-toolbox-validator-wrong-file')
+				)
+			),
+			'header' => array(
+				'label' => $this->wf->Msg('marketing-toolbox-hub-module-wikiaspicks-header'),
+				'validator' => new WikiaValidatorString(
+					array(
+						'required' => true,
+						'min' => 1
+					),
+					array('too_short' => 'marketing-toolbox-validator-string-short')
+				),
+				'attributes' => array(
+					'class' => 'required'
+				)
+			),
+			'text' => array(
+				'validator' => new WikiaValidatorFileTitle(
+					array(),
+					array('wrong-file' => 'marketing-toolbox-validator-wrong-file')
+				),
+				'type' => 'textarea',
+				'attributes' => array(
+					'class' => 'required',
+					'rows' => 4
+				)
+			)
+		);
+
+		return $fields;
 	}
 }
-?>
