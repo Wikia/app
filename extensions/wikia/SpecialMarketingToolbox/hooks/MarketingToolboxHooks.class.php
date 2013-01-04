e80c87dcba42eb1ece58e824baa1119b512cc3bb
Damian Jozwiak <damian@wikia-inc.com>
2013-01-04 12:18:46 +0100

    bugid:94786 | added vet to S:MTb Featured Video module

--- extensions/wikia/SpecialMarketingToolbox/hooks/MarketingToolboxHooks.class.php
+++ extensions/wikia/SpecialMarketingToolbox/hooks/MarketingToolboxHooks.class.php
@@ -6,6 +6,7 @@ class MarketingToolboxHooks {
 		if (F::app()->wg->title->isSpecial('MarketingToolbox')) {
 			$toolboxModel = new MarketingToolboxModel();
 			$vars['wgMarketingToolboxConstants'] = $toolboxModel->getAvailableStatuses();
+			$vars['wgMarketingToolboxThumbnailSize'] = $toolboxModel->getThumbnailSize();
 			$vars['wgMarketingToolboxUrlRegex'] = trim(WikiaValidatorUrl::URL_REGEX, 'i/');
 			$vars['wgEditHubUrl'] = SpecialPage::getTitleFor('MarketingToolbox','editHub')->getLocalURL();
 		}
