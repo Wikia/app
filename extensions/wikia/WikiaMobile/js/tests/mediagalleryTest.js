2d5e07e74b32344c5d5452c051103cca027977b7
macbre <maciej.brencz@gmail.com>
2013-01-07 11:56:22 +0100

    mediagalleryTest: use define.mock

--- extensions/wikia/WikiaMobile/js/tests/mediagalleryTest.js
+++ extensions/wikia/WikiaMobile/js/tests/mediagalleryTest.js
@@ -1,44 +1,26 @@
 /*
  @test-framework Jasmine
- @test-require-asset /extensions/wikia/WikiaTracker/js/WikiaTracker.js
- @test-require-asset /resources/wikia/libraries/modil/modil.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/Wikia.utils.js
- @test-require-asset /extensions/wikia/JSMessages/js/JSMessages.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/sections.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/layout.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/lazyload.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/pager.js
- @test-require-asset /resources/wikia/modules/thumbnailer.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/throbber.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/track.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/events.js
- @test-require-asset /resources/wikia/modules/window.js
- @test-require-asset /resources/wikia/modules/ajax.js
- @test-require-asset /resources/wikia/modules/nirvana.js
- @test-require-asset /resources/wikia/modules/loader.js
- @test-require-asset /resources/wikia/modules/querystring.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/modal.js
- @test-require-asset /extensions/wikia/WikiaMobile/js/media.js
+ @test-require-asset /resources/wikia/libraries/define.mock.js
  @test-require-asset /extensions/wikia/WikiaMobile/js/mediagallery.js
  */
 
 /*global describe, it, runs, waitsFor, expect, require, document*/
 describe("Media Gallery module", function () {
 	'use strict';
-	var async = new AsyncSpec(this);
 
-	window.wgStyleVersion = 123456789;
-	window.Features = {};
+	var mediaMock = {
+		getImages: function() {},
+	},
+	modalMock = {
+		getWrapper: function() {}
+	},
+	// ['media', 'modal', 'pager', 'thumbnailer', 'lazyload', 'track']
+	mg = define.getModule(mediaMock, modalMock);
 
-	async.it('should be defined', function(done){
-		require(['mediagallery'], function(mg){
+	it('should be defined', function() {
+		expect(mg).toBeDefined();
 
-			expect(mg).toBeDefined();
-
-			expect(typeof mg.init).toBe('function');
-			expect(typeof mg.open).toBe('function');
-
-			done();
-		});
+		expect(typeof mg.init).toBe('function');
+		expect(typeof mg.open).toBe('function');
 	});
 });
