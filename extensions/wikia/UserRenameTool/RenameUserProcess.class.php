8d75f392c10d1adb2621221c8d414d96b9fab5ab
tor <tor@wikia-inc.com>
2013-01-08 15:04:12 +0000

    moved secondary cluster clearing code to ExternalUser_Wikia and applied it to UserRenameTool and EditAccount

--- extensions/wikia/UserRenameTool/RenameUserProcess.class.php
+++ extensions/wikia/UserRenameTool/RenameUserProcess.class.php
@@ -552,25 +552,8 @@ class RenameUserProcess {
 		wfRunHooks($hookName, array($this->mUserId, $this->mOldUsername, $this->mNewUsername));
 
 		// delete the record from all the secondary clusters
-		$clusters = WikiFactory::getSecondaryClusters(); // wikicities with a c1 .. cx cluster suffix.
-
-		foreach ($clusters as $clusterName) {
-			// This is a classic double-check. I do not want to delete the record from the primary cluster.
-			// No, really! I do not.
-			if ( RenameUserHelper::CLUSTER_DEFAULT != $clusterName ) {
-				$memkey = sprintf("extuser:%d:%s", $this->mUserId, $clusterName);
-				$clusterName = 'wikicities_' . $clusterName;
-				$oDB = wfGetDB( DB_MASTER, array(), $clusterName );
-				$oDB->delete( $this->getUserTableName($clusterName), array( 'user_id' => $this->mUserId ) );
-				if ( $oDB->affectedRows() ) {
-					$this->addLog( sprintf( '%s: deleted user data.', $clusterName ) );
-				} else {
-					$this->addLog( sprintf( '%s: nothing to do here.', $clusterName ) );
-				}
-				$oDB->commit();
-				# clear memcache
-				$wgMemc->delete( $memkey );
-			}
+		if ( class_exists( 'ExternalUser_Wikia' ) ) {
+			ExternalUser_Wikia::removeFromSecondaryClusters( $this->mUserId );	
 		}
 
 		// rename the user on the shared cluster
