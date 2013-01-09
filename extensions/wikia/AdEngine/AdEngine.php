80370c7f2392b186369056b963c7aef5bdd7c8a5
Piotr Bablok <piotr.bablok@gmail.com>
2013-01-09 16:44:09 +0100

    AdEngine: changing back code (different solution will be used for NOT loading liftium configuration than removing adengine .js files from load queue)

--- extensions/wikia/AdEngine/AdEngine.php
+++ extensions/wikia/AdEngine/AdEngine.php
@@ -24,11 +24,14 @@ function wfAdEngineInit() {
 		$wgShowAds = false;
 	}
 
-	if ($wgUser->isLoggedIn() && !$wgUser->getOption('showAds')) {
-		$wgShowAds = false;
+	if (empty($wgShowAds)) {
+		$wgEnableAdsInContent = false;
+		$wgEnableAdMeldAPIClient = false;
+		$wgEnableKruxTargeting = false;
 	}
 
-	if (empty($wgShowAds)) {
+	if ($wgUser->isLoggedIn() && !$wgUser->getOption('showAds')) {
+		// Disable right rail ads and AdMeld ads for logged in users not willing to see ads
 		$wgEnableAdsInContent = false;
 		$wgEnableAdMeldAPIClient = false;
 		$wgEnableKruxTargeting = false;
