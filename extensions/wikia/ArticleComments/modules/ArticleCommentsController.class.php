e8bfe84d4b19337baddbd0c201e682c0de8a05b7
Jakub Olek <jolek@wikia-inc.com>
2013-01-09 13:57:28 +0100

    use setValues

--- extensions/wikia/ArticleComments/modules/ArticleCommentsController.class.php
+++ extensions/wikia/ArticleComments/modules/ArticleCommentsController.class.php
@@ -186,25 +186,9 @@ class ArticleCommentsController extends WikiaController {
 			Wikia::log( __METHOD__, false, 'No data, this should not happen.' );
 		}
 
-		// Hm.
-		// TODO: don't pass whole instance of Masthead object for author of current comment
-		// Did I miss something? It doesn't seem to pass the whole Masthead object anywhere... -- Federico
-		$this->avatar = $data['avatar'];
 		$this->ajaxicon = $this->wg->StylePath.'/common/images/ajax.gif';
-		$this->canEdit = $data['canEdit'];
-		$this->isBlocked = $data['isBlocked'];
-		$this->reason = $data['reason'];
-		$this->commentListRaw = $data['commentListRaw'];
-		$this->isAnon = $data['isAnon'];
-		$this->isReadOnly = $data['isReadOnly'];
-		$this->page = $data['page'];
-		$this->pagination = $data['pagination'];
-		$this->countComments = $data['countComments'];
-		$this->countCommentsNested = $data['countCommentsNested'];
-		$this->commentingAllowed = $data['commentingAllowed'];
-		$this->commentsPerPage = $data['commentsPerPage'];
 		$this->pagesCount = ( $data['commentsPerPage'] > 0 ) ? ceil( $data['countComments'] / $data['commentsPerPage'] ) : 0;
-		$this->title = $data['title'];
+		$this->response->setValues( $data );
 
 		wfProfileOut(__METHOD__);
 
