6e36580bdddb6eac950585f27a78f0fe09674b72
sovq <sovq@wikia-inc.com>
2013-01-12 14:17:07 +0100

    BugId:68841

--- extensions/wikia/Wall/Wall.namespaces.php
+++ extensions/wikia/Wall/Wall.namespaces.php
@@ -23,3 +23,27 @@ $namespaces['pl'] = array(
         NS_USER_WALL_MESSAGE_GREETING   => 'Nagłówek_tablicy'
 );
 
+$namespaces['es'] = array(
+        NS_USER_WALL                    => 'Muro',
+        NS_USER_WALL_MESSAGE            => 'Hilo',
+        NS_USER_WALL_MESSAGE_GREETING   => 'Bienvenida_del_Muro'
+);
+
+$namespaces['uk'] = array(
+        NS_USER_WALL                    => 'Стіна_обговорення',
+        NS_USER_WALL_MESSAGE            => 'Тема',
+        NS_USER_WALL_MESSAGE_GREETING   => 'Привітання_на_стіні_обговорення'
+);
+
+$namespaces['it'] = array(
+        NS_USER_WALL                    => 'Bacheca',
+        NS_USER_WALL_MESSAGE            => 'Thread',
+        NS_USER_WALL_MESSAGE_GREETING   => 'Message_Wall_Greeting'
+);
+
+$namespaces['ru'] = array(
+        NS_USER_WALL                    => 'Стена_обсуждения',
+        NS_USER_WALL_MESSAGE            => 'Тема',
+        NS_USER_WALL_MESSAGE_GREETING   => 'Приветствие_стены_обсуждения'
+);
+
