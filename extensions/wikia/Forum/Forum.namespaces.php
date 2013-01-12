fd3ed2e8a4d40d0e7717193a948a4522f119eaba
sovq <sovq@wikia-inc.com>
2013-01-12 13:52:39 +0100

    BugId:68841

--- extensions/wikia/Forum/Forum.namespaces.php
+++ extensions/wikia/Forum/Forum.namespaces.php
@@ -8,3 +8,66 @@ $namespaces['en'] = array(
 	NS_WIKIA_FORUM_TOPIC_BOARD	=> 'Topic',
 	NS_WIKIA_FORUM_BOARD_THREAD	=> 'Board_Thread',
 );
+
+/**
+ * German
+ */
+$namespaces['de'] = array(
+	NS_WIKIA_FORUM_BOARD	=> 'Forum',
+	NS_WIKIA_FORUM_TOPIC_BOARD	=> 'Thema',
+	NS_WIKIA_FORUM_BOARD_THREAD	=> 'Forum-Diskussionsfaden',
+);
+
+/**
+ * Spanish
+ */
+$namespaces['es'] = array(
+	NS_WIKIA_FORUM_BOARD	=> 'Subforo',
+	NS_WIKIA_FORUM_TOPIC_BOARD	=> 'Tema',
+	NS_WIKIA_FORUM_BOARD_THREAD	=> 'Tema_del_foro',
+);
+
+/**
+ * French
+ */
+$namespaces['fr'] = array(
+	NS_WIKIA_FORUM_BOARD	=> 'Sous-forum',
+	NS_WIKIA_FORUM_TOPIC_BOARD	=> 'Sujet',
+	NS_WIKIA_FORUM_BOARD_THREAD	=> 'Fil_forum',
+);
+
+/**
+ * Italian
+ */
+$namespaces['it'] = array(
+	NS_WIKIA_FORUM_BOARD	=> 'Sottoforum',
+	NS_WIKIA_FORUM_TOPIC_BOARD	=> 'Argomento',
+	NS_WIKIA_FORUM_BOARD_THREAD	=> 'Discussione_forum',
+);
+
+/**
+ * Polish
+ */
+$namespaces['pl'] = array(
+	NS_WIKIA_FORUM_BOARD	=> 'Subforum',
+	NS_WIKIA_FORUM_TOPIC_BOARD	=> 'Temat',
+	NS_WIKIA_FORUM_BOARD_THREAD	=> 'Wątek_forum',
+);
+
+/**
+ * Russian
+ */
+$namespaces['ru'] = array(
+	NS_WIKIA_FORUM_BOARD	=> 'Главная тема',
+	NS_WIKIA_FORUM_TOPIC_BOARD	=> 'Topic',
+	NS_WIKIA_FORUM_BOARD_THREAD	=> 'Тема форума',
+);
+
+/**
+ * Ukrainian
+ */
+$namespaces['uk'] = array(
+	NS_WIKIA_FORUM_BOARD	=> 'Головна тема',
+	NS_WIKIA_FORUM_TOPIC_BOARD	=> 'Topic',
+	NS_WIKIA_FORUM_BOARD_THREAD	=> 'Тема форума',
+);
