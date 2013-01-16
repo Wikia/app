9750f6b2fc7721dcefde3a9c393ca39bca6f7aae
nAndy <nandy@wikia-inc.com>
2013-01-16 13:44:56 +0000

    added option to provide additional class/classes to WikiaTooltipIcon component

--- extensions/wikia/WikiaStyleGuide/templates/WikiaStyleGuideTooltipIcon_index.php
+++ extensions/wikia/WikiaStyleGuide/templates/WikiaStyleGuideTooltipIcon_index.php
@@ -1,4 +1,4 @@
 <span class="tooltip-icon-wrapper">
 	<?= $text; ?>
-	<span class="tooltip-icon" rel="tooltip" title="<?= $tooltipIconTitle; ?>"><?= $tooltipIconSign ?></span>
+	<span class="tooltip-icon <?= $class; ?>" rel="tooltip" title="<?= $tooltipIconTitle; ?>"><?= $tooltipIconSign; ?></span>
 </span>
