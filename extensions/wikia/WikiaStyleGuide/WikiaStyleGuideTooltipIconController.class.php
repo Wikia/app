1563f510f2b94d6203f4293abe0043136ebc91ae
nAndy <nandy@wikia-inc.com>
2013-01-16 13:58:22 +0000

    small refactoring: changed the optional parameter's name from class to classes; added doc comments

--- extensions/wikia/WikiaStyleGuide/WikiaStyleGuideTooltipIconController.class.php
+++ extensions/wikia/WikiaStyleGuide/WikiaStyleGuideTooltipIconController.class.php
@@ -1,11 +1,20 @@
 <?php
 class WikiaStyleGuideTooltipIconController extends WikiaController {
 	const DEFAULT_ICON_SIGN = '?';
-	
+
+	/**
+	 * @desc Renders simple markup for a circle with a sign within it
+	 * 
+	 * @responseParam string tooltipIconTitle (required) The text which will fill tooltip's content (or just value of title attribute)
+	 * @responseParam string text (optional) Text before the icon if not given will set empty string
+	 * @responseParam string classes (optional) Additional classes separated with a comma which will be added to the icon <span /> 
+	 * @responseParam string tooltipIconSign (optional) The sign which should be within the icon, by default question mark
+	 */
 	public function index() {
+		$this->tooltipIconTitle = $this->request->getVal('tooltipIconTitle');
+		
 		$this->text = $this->request->getVal('text', '');
-		$this->class = $this->request->getVal('class', '');
-		$this->tooltipIconTitle = $this->request->getVal('tooltipIconTitle', '');
+		$this->classes = $this->request->getVal('classes', '');
 		$this->tooltipIconSign = $this->request->getVal('tooltipIconSign', self::DEFAULT_ICON_SIGN);
 	}
 }
