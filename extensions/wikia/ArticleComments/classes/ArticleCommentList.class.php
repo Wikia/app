2494863686cb98cd960705e66ef24c7f46f4c5e7
Jakub Olek <jolek@wikia-inc.com>
2013-01-21 12:40:25 +0100

    check if title exists and log if don't

--- extensions/wikia/ArticleComments/classes/ArticleCommentList.class.php
+++ extensions/wikia/ArticleComments/classes/ArticleCommentList.class.php
@@ -867,17 +867,31 @@ class ArticleCommentList {
 			if ($parts['title'] != '') {
 				$changeRecentChangesHeader = true;
 
-				$title = Title::newFromText($parts['title']);
-				$namespace = $title->getNamespace();
-				$title = Title::newFromText($title->getText(), MWNamespace::getSubject($namespace));
+				$title = Title::newFromText( $parts['title'] );
 
-				if ( ArticleComment::isBlog() ) {
-					$messageKey = 'article-comments-rc-blog-comments';
+				if ( $title instanceof Title ) {
+					$namespace = $title->getNamespace();
+
+					$text = $title->getText();
+
+					$title = Title::newFromText( $text, MWNamespace::getSubject( $namespace ) );
+
+					if ( $title instanceof Title ) {
+						if ( ArticleComment::isBlog() ) {
+							$messageKey = 'article-comments-rc-blog-comments';
+						} else {
+							$messageKey = 'article-comments-rc-comments';
+						}
+
+						$headerTitle = wfMsgExt($messageKey, array('parseinline'), $title->getPrefixedText());
+					} else {
+						Wikia::log( __METHOD__, '2', 'Title does not exist: ' . $text, true );
+					}
 				} else {
-					$messageKey = 'article-comments-rc-comments';
+					Wikia::log( __METHOD__, '1', 'Title does not exist: ' . $parts['title'], true );
 				}
 
-				$headerTitle = wfMsgExt($messageKey, array('parseinline'), $title->getPrefixedText());
+
 			}
 		}
 
