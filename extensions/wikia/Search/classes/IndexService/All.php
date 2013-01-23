d1fd2a7e0cba1278d5779fd1223552658d3f28b6
Robert Elwell <robert@wikia-inc.com>
2013-01-23 18:12:00 +0000

    Add \Wikia\Search\IndexService\All for bulk reindexing

---
+++ extensions/wikia/Search/classes/IndexService/All.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Class definition for \Wikia\Search\IndexService\All
+ */
+namespace Wikia\Search\IndexService;
+use Wikia\Search\MediaWikiInterface;
+/**
+ * Aggregates all other services into a single request -- good for populating a full index
+ * @author relwell
+ */
+class All extends AbstractService
+{
+	protected $services = array(
+			'DefaultContent'  => null,
+			'BacklinkCount'   => null,
+			'MediaData'       => null,
+			'Metadata'        => null,
+			'Redirects'       => null,
+			'Wam'             => null,
+			'WikiPromoData'   => null,
+			'WikiViews'       => null
+			);
+	
+	/**
+	 * Invokes a bunch of other services' execute functions
+	 * @see \Wikia\Search\IndexService\AbstractService::execute()
+	 * @return array
+	 */
+	public function execute() {
+		if ( $this->currentPageId === null ) {
+			throw new \Exception( "This service requires a page ID to be set." );
+		}
+		$result = array();
+		foreach ( $this->services as $serviceName => $service ) {
+			if ( $service === null ) {
+				$service = Factory::getInstance()->get( $serviceName );
+				$this->services[$serviceName] = $service;
+			}
+			$subResult = $service->setPageId( $this->currentPageId )->execute();
+			if ( is_array( $subResult ) ) {
+    			$result = array_merge( $result, $subResult );
+			}
+		}
+		return $result;
+	}
+	
+}
