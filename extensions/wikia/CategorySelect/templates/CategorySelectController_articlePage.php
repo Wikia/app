928255dfb094b15fd9c27eb0f436cf3e123e4667
Kyle Florence <kflorence@wikia-inc.com>
2013-01-24 21:28:36 +0000

    Restyle article page according to new design specs.

--- extensions/wikia/CategorySelect/templates/CategorySelectController_articlePage.php
+++ extensions/wikia/CategorySelect/templates/CategorySelectController_articlePage.php
@@ -1,23 +1,25 @@
 <nav class="WikiaArticleCategories CategorySelect articlePage<? if ( $showHidden ): ?> showHidden<? endif ?>" id="WikiaArticleCategories">
-	<div class="special-categories"><?= $categoriesLink ?>:</div>
-	<ul class="categories">
-		<? if ( count( $categories ) ): ?>
-			<? foreach( $categories as $index => $category ): ?>
-				<?= $app->renderView( 'CategorySelectController', 'category', array(
-					'index' => $index,
-					'category' => $category
-				)) ?>
-			<? endforeach ?>
-		<? endif ?>
-		<? if ( $userCanEdit ): ?>
-			<li class="add">
-				<button class="wikia-button secondary categoryselect-add" id="CategorySelectAdd" type="button"><?= $wf->Message( 'categoryselect-button-add' ); ?></button>
-				<?= $app->getView( 'CategorySelect', 'input' ) ?>
-			</li>
-		<? endif ?>
-	</ul>
+	<div class="container">
+		<div class="special-categories"><?= $categoriesLink ?>:</div>
+		<ul class="categories">
+			<? if ( count( $categories ) ): ?>
+				<? foreach( $categories as $index => $category ): ?>
+					<?= $app->renderView( 'CategorySelectController', 'category', array(
+						'index' => $index,
+						'category' => $category
+					)) ?>
+				<? endforeach ?>
+			<? endif ?>
+			<? if ( $userCanEdit ): ?>
+				<li class="last">
+					<button class="wikia-button secondary add" id="CategorySelectAdd" type="button"><?= $wf->Message( 'categoryselect-button-add' ); ?></button>
+					<?= $app->getView( 'CategorySelect', 'input' ) ?>
+				</li>
+			<? endif ?>
+		</ul>
+	</div>
 	<div class="toolbar">
-		<button class="wikia-button secondary cancel categoryselect-cancel" id="CategorySelectCancel" type="button"><?= $wf->Message( 'categoryselect-button-cancel' ) ?></button>
-		<button class="wikia-button save categoryselect-save" id="CategorySelectSave" type="button"><?= $wf->Message( 'categoryselect-button-save' ) ?></button>
+		<button class="wikia-button secondary cancel" id="CategorySelectCancel" type="button"><?= $wf->Message( 'categoryselect-button-cancel' ) ?></button>
+		<button class="wikia-button save" id="CategorySelectSave" type="button" disabled="disabled"><?= $wf->Message( 'categoryselect-button-save' ) ?></button>
 	</div>
 </nav>
