ba93c429e27ccde5dff396927fbba9cc9d900ab0
nAndy <nandy@wikia-inc.com>
2013-01-25 14:54:51 +0000

    small changes in image model and ImageService

--- extensions/wikia/SpecialMarketingToolbox/models/MarketingToolboxImageModel.class.php
+++ extensions/wikia/SpecialMarketingToolbox/models/MarketingToolboxImageModel.class.php
@@ -13,8 +13,21 @@ class MarketingToolboxImageModel extends WikiaModel {
 	public function getFileName() {
 		return $this->fileName;
 	}
-	
+
+	/**
+	 * @param integer $destImageWidth
+	 * @return stdClass (simple stdObject with fields: url, width and height)
+	 */
 	public function getImageData($destImageWidth) {
-		return ImagesService::getLocalFileThumbUrlAndSizes($this->getFileName(), $destImageWidth);
+		$tmpData = ImagesService::getLocalFileThumbUrlAndSizes($this->getFileName(), $destImageWidth);
+		
+		$data = new stdClass();
+		$data->url = $tmpData->url;
+		
+		//if the file is not found in repo with equals string with the title that's why intval() here
+		$data->width = intval($tmpData->width);
+		$data->height = intval($tmpData->height);
+		
+		return $data;
 	}
 }
