a8ee10d9e569bdcad37ca9b9cabe78b7bb4b1fc9
Jakub Olek <jolek@wikia-inc.com>
2013-01-25 14:40:19 +0100

    migrate test for cache.js

--- resources/wikia/modules/tests/CacheTest.js
+++ resources/wikia/modules/tests/CacheTest.js
@@ -1,119 +1,124 @@
 /**
- * @test-framework QUnit
+ * @test-framework Jasmine
  * @test-require-asset resources/wikia/libraries/define.mock.js
  * @test-require-asset resources/wikia/modules/cache.js
  */
 
-module("CacheTest");
+describe("CacheTest", function(){
 
-test('Set then get', function() {
-	var wc = define.getModule();
+	it( 'Set then get', function() {
+		var wc = define.getModule();
 
-	wc.set('a1', 'some-value');
-	equal(wc.get('a1'), 'some-value', 'string');
+		wc.set('a1', 'some-value');
+		expect(wc.get('a1')).toEqual('some-value');
 
-	wc.set('a2', {'x': 'y', '123': 456});
-	deepEqual(wc.get('a2'), {'x': 'y', '123': 456}, 'object');
+		wc.set('a2', {'x': 'y', '123': 456});
+		expect(wc.get('a2')).toEqual({'x': 'y', '123': 456});
 
-	wc.set('a3', false);
-	equal(wc.get('a3'), false, 'false');
+		wc.set('a3', false);
+		expect(wc.get('a3')).toEqual(false);
 
-	wc.set('a4', null);
-	equal(wc.get('a4'), null, 'null');
+		wc.set('a4', null);
+		expect(wc.get('a4')).toEqual(null);
 
-	wc.set('a5', 0);
-	equal(wc.get('a5'), 0, '0');
+		wc.set('a5', 0);
+		expect(wc.get('a5')).toEqual(0);
 
-	wc.set('a6', '');
-	equal(wc.get('a6'), '', 'empty string');
+		wc.set('a6', '');
+		expect(wc.get('a6')).toEqual('');
 
-	wc.set('a7', {});
-	deepEqual(wc.get('a7'), {}, 'empty object');
+		wc.set('a7', {});
+		expect(wc.get('a7')).toEqual({});
 
-	wc.set('a8', []);
-	deepEqual(wc.get('a8'), [], 'empty array');
-});
+		wc.set('a8', []);
+		expect(wc.get('a8')).toEqual([]);
+	});
 
-test('Gets from localStorage', function() {
-	var wc = define.getModule();
 
-	localStorage.setItem('wkch_val_b1', JSON.stringify('some-value'));
-	equal(wc.get('b1'), 'some-value', 'string');
+	it('Gets from localStorage', function() {
+		var wc = define.getModule();
 
-	localStorage.setItem('wkch_val_b2', JSON.stringify({'x': 'y', '123': 456}));
-	deepEqual(wc.get('b2'), {'x': 'y', '123': 456}, 'object');
+		localStorage.setItem('wkch_val_b1', JSON.stringify('some-value'));
+		expect(wc.get('b1')).toEqual('some-value');
 
-	localStorage.setItem('wkch_val_b3', JSON.stringify(false));
-	equal(wc.get('b3'), false, 'false');
+		localStorage.setItem('wkch_val_b2', JSON.stringify({'x': 'y', '123': 456}));
+		expect(wc.get('b2')).toEqual({'x': 'y', '123': 456});
 
-	localStorage.setItem('wkch_val_b4', JSON.stringify(null));
-	equal(wc.get('b4'), null, 'null');
+		localStorage.setItem('wkch_val_b3', JSON.stringify(false));
+		expect(wc.get('b3')).toEqual(false);
 
-	localStorage.setItem('wkch_val_b5', JSON.stringify(0));
-	equal(wc.get('b5'), 0, '0');
+		localStorage.setItem('wkch_val_b4', JSON.stringify(null));
+		expect(wc.get('b4')).toEqual(null);
 
-	localStorage.setItem('wkch_val_b6', JSON.stringify(''));
-	equal(wc.get('b6'), '', 'empty string');
+		localStorage.setItem('wkch_val_b5', JSON.stringify(0));
+		expect(wc.get('b5')).toEqual(0);
 
-	localStorage.setItem('wkch_val_b7', JSON.stringify({}));
-	deepEqual(wc.get('b7'), {}, 'empty object');
+		localStorage.setItem('wkch_val_b6', JSON.stringify(''));
+		expect(wc.get('b6')).toEqual('');
 
-	localStorage.setItem('wkch_val_b8', JSON.stringify([]));
-	deepEqual(wc.get('b8'), [], 'empty array');
-});
+		localStorage.setItem('wkch_val_b7', JSON.stringify({}));
+		expect(wc.get('b7')).toEqual({});
 
-test('Sets to localStorage', function() {
-	var wc = define.getModule();
+		localStorage.setItem('wkch_val_b8', JSON.stringify([]));
+		expect(wc.get('b8')).toEqual([]);
+	});
 
-	wc.set('c1', 'some-value');
-	equal(localStorage.getItem('wkch_val_c1'), JSON.stringify('some-value'), 'string');
+	it('Sets to localStorage', function() {
+		var wc = define.getModule();
 
-	wc.set('c2', {'x': 'y', '123': 456});
-	equal(localStorage.getItem('wkch_val_c2'), JSON.stringify({'x': 'y', '123': 456}), 'object');
+		wc.set('c1', 'some-value');
+		expect(localStorage.getItem('wkch_val_c1')).toEqual(JSON.stringify('some-value'));
 
-	wc.set('c3', false);
-	equal(localStorage.getItem('wkch_val_c3'), JSON.stringify(false), 'false');
+		wc.set('c2', {'x': 'y', '123': 456});
+		expect(localStorage.getItem('wkch_val_c2')).toEqual(JSON.stringify({'x': 'y', '123': 456}));
 
-	wc.set('c4', null);
-	equal(localStorage.getItem('wkch_val_c4'), JSON.stringify(null), 'null');
+		wc.set('c3', false);
+		expect(localStorage.getItem('wkch_val_c3')).toEqual(JSON.stringify(false));
 
-	wc.set('c5', 0);
-	equal(localStorage.getItem('wkch_val_c5'), JSON.stringify(0), '0');
+		wc.set('c4', null);
+		expect(localStorage.getItem('wkch_val_c4')).toEqual(JSON.stringify(null));
 
-	wc.set('c6', '');
-	equal(localStorage.getItem('wkch_val_c6'), JSON.stringify(''), 'empty string');
+		wc.set('c5', 0);
+		expect(localStorage.getItem('wkch_val_c5')).toEqual(JSON.stringify(0));
 
-	wc.set('c7', {});
-	equal(localStorage.getItem('wkch_val_c7'), JSON.stringify({}), 'empty object');
+		wc.set('c6', '');
+		expect(localStorage.getItem('wkch_val_c6')).toEqual(JSON.stringify(''));
 
-	wc.set('c8', []);
-	equal(localStorage.getItem('wkch_val_c8'), JSON.stringify([]), 'empty array');
-});
+		wc.set('c7', {});
+		expect(localStorage.getItem('wkch_val_c7')).toEqual(JSON.stringify({}));
 
-test('Get from localStorage then set', function() {
-	var wc = define.getModule();
-	localStorage.setItem('wkch_val_d', JSON.stringify('some-value'));
-	equal(wc.get('d'), 'some-value', 'get from localStorage');
-	wc.set('d', 'new-value');
-	equal(wc.get('d'), 'new-value', 'get after set');
-	equal(localStorage.getItem('wkch_val_d'), JSON.stringify('new-value'), 'set to localStorage');
-});
+		wc.set('c8', []);
+		expect(localStorage.getItem('wkch_val_c8')).toEqual(JSON.stringify([]));
+	});
 
-test('Get returns the value that was set last', function() {
-	var wc = define.getModule();
-	wc.set('e', 'some-value');
-	wc.set('e', 'other-value');
-	equal(wc.get('e'), 'other-value');
-});
+	it('Get from localStorage then set', function() {
+		var wc = define.getModule();
 
-test('Value expires after given TTL', function() {
-	var wc = define.getModule(),
-		fakeNowTimestamp = 8723687632,
-		fakeNow = {getTime: function() {return fakeNowTimestamp;}},
-		anHourLater = {getTime: function() {return fakeNowTimestamp + 3600 * 1000;}},
-		twoHoursLater = {getTime: function() {return fakeNowTimestamp + 2 * 3600 * 1000;}};
+		localStorage.setItem('wkch_val_d', JSON.stringify('some-value'));
+		expect(wc.get('d')).toEqual('some-value');
+		wc.set('d', 'new-value');
+		expect(wc.get('d')).toEqual('new-value');
+		expect(localStorage.getItem('wkch_val_d')).toEqual(JSON.stringify('new-value'));
+	});
 
-	wc.set('f', 'some-value', 3601, fakeNow);
-	equal(wc.get('f', anHourLater), 'some-value');
-	equal(wc.get('f', twoHoursLater), null);
+
+	it('Get returns the value that was set last', function() {
+		var wc = define.getModule();
+		wc.set('e', 'some-value');
+		wc.set('e', 'other-value');
+		expect(wc.get('e')).toEqual('other-value');
+	});
+
+	it('Value expires after given TTL', function() {
+		var wc = define.getModule(),
+			fakeNowTimestamp = 8723687632,
+			fakeNow = {getTime: function() {return fakeNowTimestamp;}},
+			anHourLater = {getTime: function() {return fakeNowTimestamp + 3600 * 1000;}},
+			twoHoursLater = {getTime: function() {return fakeNowTimestamp + 2 * 3600 * 1000;}};
+
+		wc.set('f', 'some-value', 3601, fakeNow);
+		expect(wc.get('f', anHourLater)).toEqual('some-value');
+		expect(wc.get('f', twoHoursLater)).toEqual(null);
+	});
+
 });
