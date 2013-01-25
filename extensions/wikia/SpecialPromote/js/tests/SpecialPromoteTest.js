24a629e227affb975d4903b4d44d7e3f2d1a0fd0
Jakub Olek <jolek@wikia-inc.com>
2013-01-25 15:08:15 +0100

    migrate SpecialPromoteTest

--- extensions/wikia/SpecialPromote/js/tests/SpecialPromoteTest.js
+++ extensions/wikia/SpecialPromote/js/tests/SpecialPromoteTest.js
@@ -1,6 +1,5 @@
 /*
- // Set framework to QUnit
- @test-framework QUnit
+ @test-framework Jasmine
  // Include jQuery and jQuery.wikia and specialPromote
  @test-require-asset /resources/wikia/libraries/modil/modil.js
  @test-require-asset resources/jquery/jquery-1.7.2.js
@@ -22,26 +21,30 @@ function specialPromoteSetup() {
 	return new SpecialPromote();
 }
 
-module("Module A");
-test("removeImage test", function() {
-	var s = specialPromoteSetup();
-	s.current.mainImageName = 'a';
-	s.current.additionalImagesNames = ['a', 'b'];
-	var smallPhotosContainer = $('<div class="small-photos"><div class="small-photos-wrapper"><img src="http://images.marcin.wikia-dev.com/__cb38/muppet/images/thumb/2/28/Wikia-Visualization-Add-2.jpg/180px-0%2C500%2C30%2C308-Wikia-Visualization-Add-2.jpg" class="additionalImage" data-filename="Wikia-Visualization-Add-2.jpg" data-image-index="1" data-image-type="additional"></div><div class="small-photos-wrapper"><img src="http://images.marcin.wikia-dev.com/__cb160/muppet/images/thumb/6/6b/Wikia-Visualization-Add-3.jpg/180px-0%2C550%2C32%2C338-Wikia-Visualization-Add-3.jpg" class="additionalImage" data-filename="Wikia-Visualization-Add-3.jpg" data-image-index="2" data-image-type="additional"></div><div class="small-photos-wrapper"><img src="http://images.marcin.wikia-dev.com/__cb441/muppet/images/thumb/9/96/Wikia-Visualization-Add-4.jpg/180px-0%2C236%2C17%2C148-Wikia-Visualization-Add-4.jpg" class="additionalImage" data-filename="Wikia-Visualization-Add-4.jpg" data-image-index="3" data-image-type="additional"></div></div>');
-	$('body').append(smallPhotosContainer);
-	ok( s.removeImage({uploadType: s.UPLOAD_TYPE_ADDITIONAL, imageIndex: 1}) == true, 'additional, index - Should return true' );
-	ok( s.removeImage({uploadType: s.UPLOAD_TYPE_ADDITIONAL, imageIndex: 0}) == !true, 'additional, index=0 - Should not return true' );
-	ok( s.removeImage({uploadType: s.UPLOAD_TYPE_ADDITIONAL}) == !true, 'additional, no index - Should not return true' );
-	ok( s.removeImage({imageIndex: 1}) == !true, 'index, but no type - Should not return true' );
-	ok( s.removeImage({}) !== true, 'no params - should not return true' );
+describe("Module A", function(){
+	it("removeImage test", function() {
+		var s = specialPromoteSetup();
+		s.current.mainImageName = 'a';
+		s.current.additionalImagesNames = ['a', 'b'];
+		var smallPhotosContainer = $('<div class="small-photos"><div class="small-photos-wrapper"><img src="http://images.marcin.wikia-dev.com/__cb38/muppet/images/thumb/2/28/Wikia-Visualization-Add-2.jpg/180px-0%2C500%2C30%2C308-Wikia-Visualization-Add-2.jpg" class="additionalImage" data-filename="Wikia-Visualization-Add-2.jpg" data-image-index="1" data-image-type="additional"></div><div class="small-photos-wrapper"><img src="http://images.marcin.wikia-dev.com/__cb160/muppet/images/thumb/6/6b/Wikia-Visualization-Add-3.jpg/180px-0%2C550%2C32%2C338-Wikia-Visualization-Add-3.jpg" class="additionalImage" data-filename="Wikia-Visualization-Add-3.jpg" data-image-index="2" data-image-type="additional"></div><div class="small-photos-wrapper"><img src="http://images.marcin.wikia-dev.com/__cb441/muppet/images/thumb/9/96/Wikia-Visualization-Add-4.jpg/180px-0%2C236%2C17%2C148-Wikia-Visualization-Add-4.jpg" class="additionalImage" data-filename="Wikia-Visualization-Add-4.jpg" data-image-index="3" data-image-type="additional"></div></div>');
+		$('body').append(smallPhotosContainer);
+
+		expect( s.removeImage({uploadType: s.UPLOAD_TYPE_ADDITIONAL, imageIndex: 1}), 'additional, index - Should return true' ).toBeTruthy();
+		expect( s.removeImage({uploadType: s.UPLOAD_TYPE_ADDITIONAL, imageIndex: 0}), 'additional, index=0 - Should not return true' ).toBeFalsy();
+		expect( s.removeImage({uploadType: s.UPLOAD_TYPE_ADDITIONAL}) == !true, 'additional, no index - Should not return true' ).toBeTruthy();
+		expect( s.removeImage({imageIndex: 1}), 'index, but no type - Should not return true' ).toBeFalsy();
+		expect( s.removeImage({}), 'no params - should not return true' ).toBeFalsy();
+	});
+
+	it("modifyImage test", function() {
+		var s = specialPromoteSetup();
+		var wrapperHtml = $('<div class="input-group main-image required"><label>Main Image</label><a href="#" data-image-type="main" class="wikia-button upload-button" title="Add a photo"><img src="" class="sprite photo">Add a photo</a><br class="clear"><div class="large-photo"><div class="modify-remove"><a class="modify" href="#">Modify</a></div><img id="curMainImageName" src="http://images.marcin.wikia-dev.com/__cb65/wikiaglobal/images/thumb/2/2d/Wikia-Visualization-Main.jpg/550px-0%2C550%2C42%2C309-Wikia-Visualization-Main.jpg" data-filename="Wikia-Visualization-Main.jpg" data-image-type="main"></div><span class="explanatory-copy">This is the main image that will represent your wiki on wikia.com. Choose an image that will show people what the wiki is about. You can always change it to keep it current.</span></div>');
+		var wrapperEvent = $.Event('click', {target: wrapperHtml.find('.modify-remove .modify')});
+		expect( s.onChangePhotoClick(wrapperEvent)).toBeTruthy();
+
+		var wrapperHtmlNoType = $('<div class="input-group main-image required"><label>Main Image</label><a href="#" data-image-type="main" class="wikia-button upload-button" title="Add a photo"><img src="" class="sprite photo">Add a photo</a><br class="clear"><div class="large-photo"><div class="modify-remove"><a class="modify" href="#">Modify</a></div><img id="curMainImageName" src="http://images.marcin.wikia-dev.com/__cb65/wikiaglobal/images/thumb/2/2d/Wikia-Visualization-Main.jpg/550px-0%2C550%2C42%2C309-Wikia-Visualization-Main.jpg" data-filename="Wikia-Visualization-Main.jpg"></div><span class="explanatory-copy">This is the main image that will represent your wiki on wikia.com. Choose an image that will show people what the wiki is about. You can always change it to keep it current.</span></div>');
+		var wrapperEventNoType = $.Event('click', {target: wrapperHtmlNoType.find('.modify-remove .modify')});
+		expect( s.onChangePhotoClick(wrapperEventNoType), 'no type - should not return true').toBeFalsy();
+	});
 });
 
-test("modifyImage test", function() {
-	var s = specialPromoteSetup();
-	var wrapperHtml = $('<div class="input-group main-image required"><label>Main Image</label><a href="#" data-image-type="main" class="wikia-button upload-button" title="Add a photo"><img src="" class="sprite photo">Add a photo</a><br class="clear"><div class="large-photo"><div class="modify-remove"><a class="modify" href="#">Modify</a></div><img id="curMainImageName" src="http://images.marcin.wikia-dev.com/__cb65/wikiaglobal/images/thumb/2/2d/Wikia-Visualization-Main.jpg/550px-0%2C550%2C42%2C309-Wikia-Visualization-Main.jpg" data-filename="Wikia-Visualization-Main.jpg" data-image-type="main"></div><span class="explanatory-copy">This is the main image that will represent your wiki on wikia.com. Choose an image that will show people what the wiki is about. You can always change it to keep it current.</span></div>');
-	var wrapperEvent = $.Event('click', {target: wrapperHtml.find('.modify-remove .modify')});
-	ok( s.onChangePhotoClick(wrapperEvent) == true, 'type=main - should return true' );
-	var wrapperHtmlNoType = $('<div class="input-group main-image required"><label>Main Image</label><a href="#" data-image-type="main" class="wikia-button upload-button" title="Add a photo"><img src="" class="sprite photo">Add a photo</a><br class="clear"><div class="large-photo"><div class="modify-remove"><a class="modify" href="#">Modify</a></div><img id="curMainImageName" src="http://images.marcin.wikia-dev.com/__cb65/wikiaglobal/images/thumb/2/2d/Wikia-Visualization-Main.jpg/550px-0%2C550%2C42%2C309-Wikia-Visualization-Main.jpg" data-filename="Wikia-Visualization-Main.jpg"></div><span class="explanatory-copy">This is the main image that will represent your wiki on wikia.com. Choose an image that will show people what the wiki is about. You can always change it to keep it current.</span></div>');
-	var wrapperEventNoType = $.Event('click', {target: wrapperHtmlNoType.find('.modify-remove .modify')});
-	ok( s.onChangePhotoClick(wrapperEventNoType) !== true, 'no type - should not return true' );
-});
