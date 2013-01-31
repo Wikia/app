d81f0e254d53a7e67f219921a1c000e076acf794
Przemek Piotrowski (Nef) <ppiotr@wikia-inc.com>
2013-01-31 13:05:41 +0100

    bugid:96496 hubs slot for evolve

--- skins/oasis/modules/AdController.class.php
+++ skins/oasis/modules/AdController.class.php
@@ -118,6 +118,8 @@ class AdController extends WikiaController {
 				self::$config['TEST_TOP_RIGHT_BOXAD'] = true;
 				self::$config['MIDDLE_RIGHT_BOXAD'] = true;
 				self::$config['LEFT_SKYSCRAPER_2'] = true;
+				self::$config['INVISIBLE_1'] = true;
+				self::$config['INVISIBLE_2'] = true;
 				self::$config['PREFOOTER_LEFT_BOXAD'] = true;
 				self::$config['PREFOOTER_RIGHT_BOXAD'] = true;
 				self::$config['TOP_BUTTON'] = true;
