a8c80fd4ee5b854d889d7f99eaac6de425a89934
macbre <maciej.brencz@gmail.com>
2013-02-01 17:04:04 +0100

    Handle "bulk mode" button on Special:Phalanx

--- extensions/wikia/PhalanxII/js/SpecialPhalanx.js
+++ extensions/wikia/PhalanxII/js/SpecialPhalanx.js
@@ -1,25 +1,37 @@
 require(['jquery', 'mw', 'phalanx', 'wikia.log'], function($, mw, phalanx, log) {
+	// edit token is required by Phalanx API
 	phalanx.init(mw.config.get('wgPhalanxToken'));
 
-	$('body').on('click', 'button.unblock', function(ev) {
-		var node = $(this),
-			blockId = parseInt(node.data('id'), 10);
+	$('body').
+		// handle blocks "unblocking" (i.e. removing blocks)
+		on('click', 'button.unblock', function(ev) {
+			var node = $(this),
+				blockId = parseInt(node.data('id'), 10);
 
-		if (!blockId) {
-			return;
-		}
+			if (!blockId) {
+				return;
+			}
 
-		node.attr('disabled', true);
+			node.attr('disabled', true);
 
-		phalanx.unblock(blockId).
-			done(function() {
-				// hide
-				node.closest('li').addClass('removed');
-			}).
-			fail(function() {
-				node.attr('disabled', false);
+			phalanx.unblock(blockId).
+				done(function() {
+					// hide
+					node.closest('li').addClass('removed');
+				}).
+				fail(function() {
+					node.attr('disabled', false);
 
-				log('block #' + blockId + ' not removed', log.levels.error, 'Phalanx');
-			});
-	});
+					log('block #' + blockId + ' not removed', log.levels.error, 'Phalanx');
+				});
+		}).
+
+		// handle "bulk mode" button
+		on('click', '#enterbulk', function(ev) {
+			var singleModeWrapper = $('#singlemode'),
+				bulkModeWrapper = $('#bulkmode');
+
+			singleModeWrapper.slideUp();
+			bulkModeWrapper.slideDown();
+		});
 });
