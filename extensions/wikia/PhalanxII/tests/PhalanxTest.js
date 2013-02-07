6cc64c6795d7c4fa96ff51f4841a602fde09d06a
macbre <maciej.brencz@gmail.com>
2013-02-07 13:50:32 +0100

    Phalanx: JS unit tests completed

--- extensions/wikia/PhalanxII/tests/PhalanxTest.js
+++ extensions/wikia/PhalanxII/tests/PhalanxTest.js
@@ -8,9 +8,11 @@
 describe("Phalanx", function () {
 	'use strict';
 
-	var async = new AsyncSpec(this),
-		TOKEN = "asdfghjkl",
-		nirvanaMock = function() {},
+	var TOKEN = "asdfghjkl",
+		REGEXP = 'foo',
+		BLOCKID = 123,
+		async = new AsyncSpec(this),
+		nirvanaMock = {},
 		phalanx = define.getModule(jQuery, nirvanaMock);
 
 	it('registers AMD module', function() {
@@ -20,23 +22,75 @@ describe("Phalanx", function () {
 		expect(typeof phalanx.unblock).toBe('function');
 	});
 
-	async.it('RegExp is validated', function(done) {
-		var regexp = "foo",
-			nirvanaMock = {
-				postJson: function(controllerName, method, params, callback) {
-					// token and regexp is correctly passed
-					expect(params.regexp).toBe(regexp);
-					expect(params.token).toBe(TOKEN);
+	function mockNirvanaValidate(resp) {
+		return {
+			postJson: function(controllerName, method, params, callback) {
+				// token and regexp is correctly passed
+				expect(params.regexp).toBe(REGEXP);
+				expect(params.token).toBe(TOKEN);
 
-					callback({valid: 1});
-				}
-			},
+				callback(resp);
+			}
+		};
+	}
+
+	async.it('RegExp is validated', function(done) {
+		var nirvanaMock = mockNirvanaValidate({valid: 1}),
 			phalanx = define.getModule(jQuery, nirvanaMock);
 
 		phalanx.init(TOKEN);
 
-		phalanx.validate(regexp).then(function(resp) {
+		phalanx.validate(REGEXP).then(function(resp) {
 			expect(resp).toBe(true);
+			done();
+		});
+	});
+
+	async.it('RegExp is not validated', function(done) {
+		var nirvanaMock = mockNirvanaValidate({valid: 0}),
+			phalanx = define.getModule(jQuery, nirvanaMock);
+
+		phalanx.init(TOKEN);
+
+		phalanx.validate(REGEXP).then(function(resp) {
+			expect(resp).toBe(false);
+			done();
+		});
+	});
+
+	function mockNirvanaUnblock(resp) {
+		return {
+			postJson: function(controllerName, method, params, callback) {
+				// token and block ID is correctly passed
+				expect(params.blockId).toBe(BLOCKID);
+				expect(params.token).toBe(TOKEN);
+
+				callback(resp);
+			}
+		};
+	}
+
+	async.it('block is deleted', function(done) {
+		var regexp = "foo",
+			nirvanaMock = mockNirvanaUnblock({success: true}),
+			phalanx = define.getModule(jQuery, nirvanaMock);
+
+		phalanx.init(TOKEN);
+
+		phalanx.unblock(BLOCKID).then(function(resp) {
+			expect(resp).toBe(true);
+			done();
+		});
+	});
+
+	async.it('block deletion failed', function(done) {
+		var regexp = "foo",
+			nirvanaMock = mockNirvanaUnblock({success: false}),
+			phalanx = define.getModule(jQuery, nirvanaMock);
+
+		phalanx.init(TOKEN);
+
+		phalanx.unblock(BLOCKID).fail(function() {
 			done();
 		});
 	});
