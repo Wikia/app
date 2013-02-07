1de3732577c887584f598fe4e5c338135f8a49c9
nAndy <nandy@wikia-inc.com>
2013-02-07 10:21:42 +0000

    Revert "changed logic a little bit in WikiaStyleGuide so if there are no fields in "submits" group it won't render div.submits"
    This reverts commit 5c9805a56ddab1dc65b8c856751876a66f8b4148.

--- extensions/wikia/WikiaStyleGuide/templates/WikiaStyleGuideForm_index.php
+++ extensions/wikia/WikiaStyleGuide/templates/WikiaStyleGuideForm_index.php
@@ -136,8 +136,8 @@
 		<? endif; ?>
 	</fieldset>
 
-	<? if ( !empty( $form[ 'submits' ] ) ): ?>
-		<div class="submits">
+	<div class="submits">
+		<? if ( !empty( $form[ 'submits' ] ) ): ?>
 			<? foreach( $form[ 'submits' ] as $submit ): ?>
 				<?
 					$submitAttributes = isset( $submit[ 'attributes' ] ) ? $submit[ 'attributes' ] : array();
@@ -146,6 +146,6 @@
 				?>
 				<input type="submit" <?= $submitAttributes ?>>
 			<? endforeach ?>
-		</div>
-	<? endif ?>
+		<? endif ?>
+	</div>
 </form>
