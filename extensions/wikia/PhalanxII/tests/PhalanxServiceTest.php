58775b687e1bba1912d65f0ebe57b9e467614b93
Szumo <szumo@wikia-inc.com>
2013-02-13 15:30:34 +0100

    BLAC-19

--- extensions/wikia/PhalanxII/tests/PhalanxServiceTest.php
+++ extensions/wikia/PhalanxII/tests/PhalanxServiceTest.php
@@ -8,22 +8,22 @@ class PhalanxServiceTest extends WikiaBaseTest {
 	 * setup tests
 	 */
 	public function setUp() {
-
 		$this->setupFile =  dirname(__FILE__) . '/../Phalanx_setup.php';
 		wfDebug( __METHOD__ . ': '  .$this->setupFile );
 		parent::setUp();
-
-		$this->mockGlobalVariable( "wgPhalanxServiceUrl", "http://phalanx" );
-		$this->mockApp();
+		// $this->mockApp(); // not required anymore?
+		$this->checkPhalanxAlive();
 	}
 
-	public function isPhalanxAlive( ) {
+	public function checkPhalanxAlive( ) {
 		error_log( __CLASS__ . '::' . __FUNCTION__ );
 
-
 		$this->service = new PhalanxService();
-		return $this->service->status();
+		if (!$this->service->status()) {
+			throw new Exception("Can't connect to phalanx service");
+		}
 	}
+
 
 	/**
 	 * check for defined methods in service
@@ -38,83 +38,53 @@ class PhalanxServiceTest extends WikiaBaseTest {
 
 	public function testPhalanxServiceCheck() {
 		error_log( __CLASS__ . '::' . __FUNCTION__ );
-		if( $this->isPhalanxAlive() ) {
-			$ret = $this->service->check( "content", "hello world" );
-			$this->assertEquals( $ret, 1 );
+		$ret = $this->service->check( "content", "hello world" );
+		$this->assertEquals( $ret, 1 );
 
-			$ret = $this->service->check( "invalid type", "hello world" );
-			$this->assertEquals( $ret, false );
+		$ret = $this->service->check( "invalid type", "hello world" );
+		$this->assertEquals( $ret, false );
 
-			$ret = $this->service->check( "content", "pornhub.com" );
-			$this->assertEquals( $ret, 0 );
-		}
-		else {
-			$this->markTestSkipped( sprintf( "Can't contact with phalanx service on %s.\n", F::app()->wg->PhalanxServiceUrl ) );
-		}
+		$ret = $this->service->check( "content", "pornhub.com" );
+		$this->assertEquals( $ret, 0 );
 	}
 
+
 	public function testPhalanxServiceReload() {
-//		global $wgDebugLogFile;
-//		$wgDebugLogFile = "php://stdout";
+		$this->markTestSkipped("Forced reload disabled until we have separate phalanx service for tests.\n");
+		return;
 
-		if( $this->isPhalanxAlive() ) {
-			$ret = $this->service->reload();
-			$this->assertEquals( 1, $ret );
+		$ret = $this->service->reload();
+		$this->assertEquals( 1, $ret );
 
-			$ret = $this->service->reload( array( 1, 2, 3 ) );
-			$this->assertEquals( 1, $ret );
+		$ret = $this->service->reload( array( 1, 2, 3 ) );
+		$this->assertEquals( 1, $ret );
 
-		}
-		else {
-			$this->markTestSkipped( sprintf( "Can't contact with phalanx service on %s.\n", F::app()->wg->PhalanxServiceUrl ) );
-		}
 	}
 
 	public function testPhalanxServiceMatch() {
-		if( $this->isPhalanxAlive() ) {
-
-			$ret = $this->service->limit( 1 )->match( "content", "hello" );
-			$this->assertEquals( 0, $ret );
+		$ret = $this->service->limit( 1 )->match( "content", "hello" );
+		$this->assertEquals( 0, $ret );
 
 
-			$ret = $this->service->limit( 1 )->match( "content", "pornhub.com" );
-			$val = is_integer( $ret->id ) && $ret->id > 1;
-			$this->assertEquals( true, $val, "pornhub.com should be matched as spam content" );
-		}
-		else {
-			$this->markTestSkipped( sprintf( "Can't contact with phalanx service on %s.\n", F::app()->wg->PhalanxServiceUrl ) );
-		}
+		$ret = $this->service->limit( 1 )->match( "content", "pornhub.com" );
+		$val = is_integer( $ret->id ) && $ret->id > 1;
+		$this->assertEquals( true, $val, "pornhub.com should be matched as spam content" );
 	}
 
 	public function testPhalanxServiceValidate() {
-		global $wgDebugLogFile;
-	//	$wgDebugLogFile = "php://stdout";
 
-		if( $this->isPhalanxAlive() ) {
-			$ret = $this->service->validate( '^alamakota$' );
-			$this->assertEquals( 1, $ret, "Valid regex" );
+		$ret = $this->service->validate( '^alamakota$' );
+		$this->assertEquals( 1, $ret, "Valid regex" );
 
-			$ret = $this->service->validate( '^alama(((kota$' );
-			$this->assertEquals( 0, $ret, "Invalid regex" );
+		$ret = $this->service->validate( '^alama(((kota$' );
+		$this->assertEquals( 0, $ret, "Invalid regex" );
 
-		}
-		else {
-			$this->markTestSkipped( sprintf( "Can't contact with phalanx service on %s.\n", F::app()->wg->PhalanxServiceUrl ) );
-		}
 	}
 
 	public function testPhalanxServiceStats() {
-		global $wgDebugLogFile;
-	//	$wgDebugLogFile = "php://stdout";
-
-		if( $this->isPhalanxAlive() ) {
-			$ret = $this->service->stats( );
-			// check for known strings
-			$this->assertRegexp( "/email|wiki_creation|summary/", $ret );
-		}
-		else {
-			$this->markTestSkipped( sprintf( "Can't contact with phalanx service on %s.\n", F::app()->wg->PhalanxServiceUrl ) );
-		}
+		$ret = $this->service->stats( );
+		// check for known strings
+		$this->assertRegexp( "/email|wiki_creation|summary/", $ret );
 	}
 
 }
