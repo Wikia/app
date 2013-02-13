e999e60032d3cd38f52cc8913d77195c0f7211f7
nAndy <nandy@wikia-inc.com>
2013-02-13 15:57:37 +0000

    rendered all video's data

--- extensions/wikia/WikiaHubsServices/modules/templates/MarketingToolboxModulePopularvideosService_editor.php
+++ extensions/wikia/WikiaHubsServices/modules/templates/MarketingToolboxModulePopularvideosService_editor.php
@@ -17,43 +17,45 @@
 		</div>
 	</div>
 	
-	<div class="module-box">
-		<h3 class="alternative">2.</h3>
-		<div class="module-right-box grid-4 alpha">
-			<div class="module-input-box">
-				<h4 class="video-title">Star Wars vs. Star Trek</h4>
-				<time class="timeago">2 days ago</time>
-				<a href="#" class="video-url alternative">http://darksiders.wikia.com/wiki/File:Wii_U_-_Darksiders_II_Death_Lives</a>
-				<?= $app->renderView(
-						'MarketingToolbox',
-						'FormField',
-						array(
-							'inputData' => array_merge(
-								$fields['video'],
-								array('index' => 0)
+	<?php foreach($videos as $idx => $video): ?>
+		<div class="module-box">
+			<h3 class="alternative"><?= $video['section-no'] . '.'; ?></h3>
+			<div class="module-right-box grid-4 alpha">
+				<div class="module-input-box">
+					<h4 class="video-title"><?= $video['title']; ?></h4>
+					<time class="timeago"><?= wfTimeFormatAgo($video['timestamp']); ?></time>
+					<a href="<?= $video['fullUrl']; ?>" target="_blank" class="video-url alternative"><?= $video['fullUrl']; ?></a>
+					<?= $app->renderView(
+							'MarketingToolbox',
+							'FormField',
+							array(
+								'inputData' => array_merge(
+									$fields['video'],
+									array('index' => $idx)
+								)
 							)
-						)
-					);
-				?>
-			</div>
-			
-			<div class="module-image-box">
-				<div class="image-placeholder">
-					<?php if( !empty($video['thumb']) ): ?>
-						<img width="<?= $video['thumb']['imageWidth']; ?>" height="<?= $video['thumb']['imageHeight']; ?>" src="<?= $video['thumb']['url']; ?>" />
-					<?php else: ?>
-						<img src="<?= $wg->BlankImgUrl; ?>" />
-					<?php endif ?>
+						);
+					?>
 				</div>
+				
+				<div class="module-image-box">
+					<div class="image-placeholder">
+						<?php if( !empty($video['thumbnailData']) ): ?>
+							<img width="<?= $video['thumbnailData']['width']; ?>" height="<?= $video['thumbnailData']['height']; ?>" src="<?= $video['thumbnailData']['thumb']; ?>" />
+						<?php else: ?>
+							<img src="<?= $wg->BlankImgUrl; ?>" />
+						<?php endif ?>
+					</div>
+				</div>
+	
+				<div class="module-input-box buttons">
+					<input type="button" class="delete" value="<?= $wf->msg('marketing-toolbox-edithub-delete-button'); ?>" />
+					<input class="secondary clear" type="button" value="<?= $wf->msg('marketing-toolbox-edithub-clear-button')?>" />
+				</div>
+				
+				<button class="secondary navigation nav-up"><img class="chevron chevron-up" src="<?= $wg->BlankImgUrl; ?>"></button>
+				<button class="secondary navigation nav-down"><img class="chevron chevron-down" src="<?= $wg->BlankImgUrl; ?>"></button>
 			</div>
-
-			<div class="module-input-box buttons">
-				<input type="button" class="delete" value="<?= $wf->msg('marketing-toolbox-edithub-delete-button'); ?>" />
-				<input class="secondary clear" type="button" value="<?= $wf->msg('marketing-toolbox-edithub-clear-button')?>" />
-			</div>
-			
-			<button class="secondary navigation nav-up"><img class="chevron chevron-up" src="<?= $wg->BlankImgUrl; ?>"></button>
-			<button class="secondary navigation nav-down"><img class="chevron chevron-down" src="<?= $wg->BlankImgUrl; ?>"></span></button>
 		</div>
-	</div>
+	<?php endforeach; ?>
 </div>
