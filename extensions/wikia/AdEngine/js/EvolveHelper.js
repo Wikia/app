0c20996835b887be6042571a7284e1032bc7fa43
Przemek Piotrowski (Nef) <ppiotr@wikia-inc.com>
2013-02-13 11:20:36 -0800

    bugid:97754 hubs tweak for evolve

--- extensions/wikia/AdEngine/js/EvolveHelper.js
+++ extensions/wikia/AdEngine/js/EvolveHelper.js
@@ -16,6 +16,9 @@ var EvolveHelper = function (log, window) {
 			if (window.wgPageName === 'Video_Games') {
 				sect = 'gaming';
 			}
+			if (window.wgPagename === 'Entertainment') {
+				sect = 'entertainment';
+			}
 		} else if (kv.indexOf('movie') !== -1) {
 			sect = 'movies';
 		} else if (kv.indexOf('tv') !== -1) {
