9b2bbe9158090ad230338917ce1c77d7d5d60fb2
nAndy <nandy@wikia-inc.com>
2013-02-13 16:10:01 +0000

    added one more condition in template

--- extensions/wikia/WikiaHubsServices/modules/templates/MarketingToolboxModulePopularvideosService_editor.php
+++ extensions/wikia/WikiaHubsServices/modules/templates/MarketingToolboxModulePopularvideosService_editor.php
@@ -17,46 +17,48 @@
 		</div>
 	</div>
 	
-	<?php foreach($videos as $idx => $video): ?>
-		<div class="module-box">
-			<h3 class="alternative"><?= $video['section-no'] . '.'; ?></h3>
-			<div class="module-right-box grid-4 alpha">
-				<div class="module-input-box">
-					<h4 class="video-title"><?= $video['title']; ?></h4>
-					<time class="timeago"><?= wfTimeFormatAgo($video['timestamp']); ?></time>
-					<a href="<?= $video['fullUrl']; ?>" target="_blank" class="video-url alternative"><?= $video['fullUrl']; ?></a>
-					<?= $app->renderView(
-							'MarketingToolbox',
-							'FormField',
-							array(
-								'inputData' => array_merge(
-									$fields['video'],
-									array('index' => $idx)
-								)
-							)
-						);
-					?>
-				</div>
-				
-				<div class="module-image-box">
-					<div class="image-placeholder video">
+	<?php if( !empty($videos) ): ?>
+		<?php foreach($videos as $idx => $video): ?>
+			<div class="module-box">
+				<h3 class="alternative"><?= $video['section-no'] . '.'; ?></h3>
+				<div class="module-right-box grid-4 alpha">
+					<div class="module-input-box">
+						<h4 class="video-title"><?= $video['title']; ?></h4>
+						<time class="timeago"><?= wfTimeFormatAgo($video['timestamp']); ?></time>
+						<a href="<?= $video['fullUrl']; ?>" target="_blank" class="video-url alternative"><?= $video['fullUrl']; ?></a>
 						<?= $app->renderView(
-								'MarketingToolboxVideos',
-								'index',
-								array('video' => $video)
+								'MarketingToolbox',
+								'FormField',
+								array(
+									'inputData' => array_merge(
+										$fields['video'],
+										array('index' => $idx)
+									)
+								)
 							);
 						?>
 					</div>
+					
+					<div class="module-image-box">
+						<div class="image-placeholder video">
+							<?= $app->renderView(
+									'MarketingToolboxVideos',
+									'index',
+									array('video' => $video)
+								);
+							?>
+						</div>
+					</div>
+					
+					<div class="module-input-box buttons">
+						<input type="button" class="delete" value="<?= $wf->msg('marketing-toolbox-edithub-delete-button'); ?>" />
+						<input class="secondary clear" type="button" value="<?= $wf->msg('marketing-toolbox-edithub-clear-button')?>" />
+					</div>
+					
+					<button class="secondary navigation nav-up"><img class="chevron chevron-up" src="<?= $wg->BlankImgUrl; ?>"></button>
+					<button class="secondary navigation nav-down"><img class="chevron chevron-down" src="<?= $wg->BlankImgUrl; ?>"></button>
 				</div>
-				
-				<div class="module-input-box buttons">
-					<input type="button" class="delete" value="<?= $wf->msg('marketing-toolbox-edithub-delete-button'); ?>" />
-					<input class="secondary clear" type="button" value="<?= $wf->msg('marketing-toolbox-edithub-clear-button')?>" />
-				</div>
-				
-				<button class="secondary navigation nav-up"><img class="chevron chevron-up" src="<?= $wg->BlankImgUrl; ?>"></button>
-				<button class="secondary navigation nav-down"><img class="chevron chevron-down" src="<?= $wg->BlankImgUrl; ?>"></button>
 			</div>
-		</div>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
+	<?php endif; ?>
 </div>
