622341b7a09bd37cbdbf2044a7738647d775be5c
Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
2013-02-14 10:59:32 +0000

    BLAC-47: improve comments

--- includes/wikia/services/TrustedProxyService.class.php
+++ includes/wikia/services/TrustedProxyService.class.php
@@ -2,6 +2,16 @@
 
 class TrustedProxyService extends WikiaObject {
 
+	/**
+	 * check if $ip is in given $cidr range
+	 *
+	 * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
+	 *
+	 * @param string $cidr address range in CIDR notation
+	 * @param string $ip ip string
+	 *
+	 * @return boolean true if in range, false otherwise
+	 */
 	private function inRange( $cidr, $ip  ) {
 		list ( $net, $mask ) = explode ( '/', $cidr );
 		return ( ip2long ($ip) & ~(( 1 << ( 32 - $mask ) ) - 1 ) ) == ip2long ( $net );
@@ -10,9 +20,17 @@ class TrustedProxyService extends WikiaObject {
 	/**
 	 * hook entry
 	 *
+	 * @author Krzysztof Krzyżaniak (eloy) <eloy@wikia-inc.com>
+	 *
+	 * @param string $ip ip address we want to check
+	 * @param bool $trusted should ip be trusted or not
+	 *
 	 * @see ProxyTools.php
+	 *
+	 * @return true always since hook
 	 */
 	public function onIsTrustedProxy( &$ip, &$trusted ) {
+		wfProfileIn( __METHOD__ );
 		$ranges = $this->wg->SquidServersNoPurge;
 		if( is_array( $ranges ) ) {
 			foreach( $ranges as $range ) {
@@ -21,13 +39,16 @@ class TrustedProxyService extends WikiaObject {
 					# stop if match is true
 					#
 					if( $this->inRange( $range, $ip ) === true ) {
+						wfDebug( __METHOD__ . ": $ip is in range $range.\n" );
 						$trusted = true;
+						wfProfileOut( __METHOD__ );
 						return true;
 					}
 				}
 			}
 		}
-
+		wfDebug( __METHOD__ . ": no ranges for $ip.\n" );
+		wfProfileOut( __METHOD__ );
 		return true;
 	}
 }
