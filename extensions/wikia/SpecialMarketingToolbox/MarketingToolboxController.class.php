06a0ac44792d808101877d7d293ecfc3f13fd746
nAndy <nandy@wikia-inc.com>
2013-02-18 15:54:12 +0000

    removed mocked data

--- extensions/wikia/SpecialMarketingToolbox/MarketingToolboxController.class.php
+++ extensions/wikia/SpecialMarketingToolbox/MarketingToolboxController.class.php
@@ -366,7 +366,6 @@ class MarketingToolboxController extends WikiaSpecialPageController {
 		$this->videoFileName = $fileName;
 		$this->videoFileMarkup = $thumb;
 		$this->videoUrl = $url;
-		$this->videoDate = 'September 25, 2012'; //mocked date, please use php magic here ;)
 	}
 
 	// TODO extract this code somewhere
