b76a4a867c686abad0927e8f3b656aae41aa4106
macbre <maciej.brencz@gmail.com>
2013-02-18 16:36:44 +0100

    BugId:51070 - Write a maintenance script removing wikis created by automated tests

--- maintenance/wikia/removeQAWikis.php
+++ maintenance/wikia/removeQAWikis.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * Script that removes QA wikis created by automated tests
+ * Script that marks QA wikis (created by automated tests)
+ * to be removed by /extensions/wikia/WikiFactory/Close/maintenance.php script
  *
  * Use --quiet option when running on cron machines
  *
@@ -12,21 +13,32 @@
 
 require_once( dirname( __FILE__ ) . '/../Maintenance.php' );
 
-/**
- * Maintenance script class
- */
 class RemoveQAWikis extends Maintenance {
 
-	const REASON = 'Removed by RemoveQAWikis maintenance script';
-	const WIKI_PREFIX = 'qa';
+	const REASON = 'Marked for removal by RemoveQAWikis maintenance script';
+	const WIKI_PREFIX = 'qatestwiki';
 
 	/**
 	 * Set script options
 	 */
 	public function __construct() {
 		parent::__construct();
-		$this->addOption( "dry-run", "Do not remove any wikis, just list them" );
-		$this->mDescription = "Remove QA wikis created by automated tests";
+		$this->addOption( "dry-run", "Do not mark any wikis, just list them" );
+		$this->mDescription = 'Mark QA wikis created by automated tests for removal';
+	}
+
+	/**
+	 * Mark given wiki as queued for removal
+	 *
+	 * @param $wikiId integer city ID
+	 * @return bool
+	 */
+	private function markWikiAsClosed($wikiId) {
+		WikiFactory::setFlags( $wikiId, WikiFactory::FLAG_FREE_WIKI_URL | WikiFactory::FLAG_DELETE_DB_IMAGES );
+		$res = WikiFactory::setPublicStatus( WikiFactory::CLOSE_ACTION, $wikiId, self::REASON );
+		WikiFactory::clearCache( $wikiId );
+
+		return $res !== false;
 	}
 
 	public function execute() {
@@ -40,11 +52,13 @@ class RemoveQAWikis extends Maintenance {
 		$res = $dbr->select(
 			'city_list',
 			array(
+				'city_id',
 				'city_dbname',
 				'city_url',
 				'city_factory_timestamp',
 			),
 			array(
+				'city_public' => 1,
 				'city_dbname ' . $dbr->buildLike( self::WIKI_PREFIX, $dbr->anyString() )
 			),
 			__METHOD__
@@ -53,10 +67,19 @@ class RemoveQAWikis extends Maintenance {
 		$this->output("\n\n");
 
 		while($wiki = $res->fetchObject()) {
-			$this->output("* {$wiki->city_dbname} <{$wiki->city_url}>...");
+			$this->output("* {$wiki->city_dbname} <{$wiki->city_url}>... ");
 
-			$this->output(" done\n");
-			//var_dump($wiki);
+			if (!$isDryRun) {
+				if ($this->markWikiAsClosed($wiki->city_id)) {
+					$this->output("done\n");
+				}
+				else {
+					$this->output("err!\n");
+				}
+			}
+			else {
+				$this->output("skipping, dry run\n");
+			}
 		}
 
 		$this->output("\nDone!\n");
