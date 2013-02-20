03c9ee1fb97a100a85b70a6a0f5c5f526ce438a2
Kyle Florence <kflorence@wikia-inc.com>
2013-02-20 15:26:47 -0800

    Update path to tracker.

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -21,7 +21,7 @@ $config['oasis_extensions_js'] = array(
 $config['tracker_js'] = array(
 	'type' => AssetsManager::TYPE_JS,
 	'assets' => array(
-		'//extensions/wikia/WikiaTracker/js/WikiaTracker.js',
+		'//resources/wikia/modules/tracker.js',
 	)
 );
 
