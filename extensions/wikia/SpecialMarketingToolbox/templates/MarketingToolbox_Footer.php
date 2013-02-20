e1e2f95ba99c9e5baab0873024068b2c96b49298
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-02-20 17:04:07 +0000

    BugId-98049: Added "disabled" property to publish button

--- extensions/wikia/SpecialMarketingToolbox/templates/MarketingToolbox_Footer.php
+++ extensions/wikia/SpecialMarketingToolbox/templates/MarketingToolbox_Footer.php
@@ -2,5 +2,5 @@
 
 <footer class="MarketingToolboxFooter">
 	<input class="big secondary" type="submit" value="<?=$wf->msg('marketing-toolbox-footer-button-preview')?>"/>
-	<input class="big" type="submit" value="<?=$wf->msg('marketing-toolbox-footer-button-publish')?>" />
+	<input class="big" type="submit" value="<?=$wf->msg('marketing-toolbox-footer-button-publish')?>" <?= $allModulesSaved ?> />
 </footer>
