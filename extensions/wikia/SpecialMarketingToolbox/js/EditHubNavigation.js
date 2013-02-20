8f4c6f241b318b486a4b8c8fce3861ed792c67df
Damian Jozwiak <damian@wikia-inc.com>
2013-02-20 10:27:11 +0000

    bugid-68510 | S:MTb Popular videos - fixed navigation and video preview
    Conflicts:
    	extensions/wikia/SpecialMarketingToolbox/js/EditHubNavigation.js

--- extensions/wikia/SpecialMarketingToolbox/js/EditHubNavigation.js
+++ extensions/wikia/SpecialMarketingToolbox/js/EditHubNavigation.js
@@ -4,7 +4,7 @@ var ModuleNavigation = function() {
 ModuleNavigation.prototype = {
 	boxes: undefined,
 	wrapper: undefined,
-	switchSelector: 'input:not(:button), textarea, .filename-placeholder, .image-placeholder img.Wikia-video-thumb, .video-title, .video-url-input, .timeago',
+	switchSelector: 'input:not(:button), textarea, .filename-placeholder, .image-placeholder img, .video-title, .video-url-input, .timeago, .timer, div.image-placeholder.video > a',
 
 	init: function () {
 		this.wrapper = $('#marketing-toolbox-form');
@@ -55,7 +55,8 @@ ModuleNavigation.prototype = {
 		if (sourceTagName != dest.nodeName.toLowerCase()) {
 			throw "Switchable type not equals";
 		}
-		var imgAttribsToSwitch = ['src', 'width', 'height'];
+		var imgAttribsToSwitch = ['src', 'width', 'height', 'data-video'];
+		var linkAttribsToSwitch = ['href'];
 
 		source = $(source);
 		dest = $(dest);
@@ -66,15 +67,18 @@ ModuleNavigation.prototype = {
 			case 'h4':
 			case 'time':
 			case 'span':
+			case 'div':
 				tmp = source.text();
 				source.text(dest.text());
 				dest.text(tmp);
 				break;
 			case 'img':
-				for (var i = 0; i < imgAttribsToSwitch.length; i++) {
-					tmp = source.attr(imgAttribsToSwitch[i]);
-					source.attr(imgAttribsToSwitch[i], dest.attr(imgAttribsToSwitch[i]));
-					dest.attr(imgAttribsToSwitch[i], tmp);
+			case 'a':
+				var attribs = (sourceTagName == 'a') ? linkAttribsToSwitch : imgAttribsToSwitch;
+				for (var i = 0; i < attribs.length; i++) {
+					tmp = source.attr(attribs[i]);
+					source.attr(attribs[i], dest.attr(attribs[i]));
+					dest.attr(attribs[i], tmp);
 				}
 				break;
 			case 'textarea':
