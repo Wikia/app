b4418a1849bbaf12e328b8c01b42179d9de2f0e8
Kyle Florence <kflorence@wikia-inc.com>
2013-02-20 11:15:01 -0800

    (Re: @gabrys) Make WikiaTracker into an AMD module
    Conflicts:
    	extensions/wikia/JSVariables/JSVariables.php
    	resources/wikia/Resources.php
    	resources/wikia/modules/tracker.js

--- resources/wikia/Resources.php
+++ resources/wikia/Resources.php
@@ -106,16 +106,13 @@ return array(
 		)
 	),
 	'wikia.tracker' => array(
-<<<<<<< HEAD
-		'scripts' => 'extensions/wikia/WikiaTracker/js/WikiaTracker.js',
-		'dependencies' => 'wikia.log',
-=======
 		'scripts' => 'resources/wikia/modules/tracker.js',
-	),
-
-	'wikia.trackerqueue' => array(
-		'scripts' => 'resources/wikia/modules/trackerqueue.js',
->>>>>>> e194030... WikiaTracker as AMD module
+		'dependencies' => array(
+			'amd',
+			'wikia.window',
+			'wikia.log',
+			'wikia.tracker.stub',
+		)
 	),
 	'wikia.tracker.stub' => array(
 		'scripts' => 'resources/wikia/modules/tracker.stub.js',
