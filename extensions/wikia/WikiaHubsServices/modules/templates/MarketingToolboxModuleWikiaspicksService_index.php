8350e005d4d6b65e382683990c6acc5960365930
Marcin Maciejewski <marcin@wikia-inc.com>
2013-02-20 15:38:31 +0000

    bugid-98047: Hubs V2 - Wikias Picks - Revise and modify CSS to handle sponsored image

--- extensions/wikia/WikiaHubsServices/modules/templates/MarketingToolboxModuleWikiaspicksService_index.php
+++ extensions/wikia/WikiaHubsServices/modules/templates/MarketingToolboxModuleWikiaspicksService_index.php
@@ -1,4 +1,7 @@
-<h2><?= $title ?></h2>
+<h2>
+	<?= $title ?>
+	<img class="floatright sponsored-image" src="<?= $sponsoredImageUrl ?>" alt="<?= $sponsoredImageAlt ?>" />
+</h2>
 <p>
 	<img class="floatright" src="<?= $imageUrl ?>" alt="<?= $imageAlt ?>" />
 	<?= $text ?>
