7e27263f96acaeae17a09da1aac239522e14031e
Hyun Lim <hyun@wikia-inc.com>
2013-02-21 01:36:31 +0000

    bugId::97760 Initial template for File Listing

--- extensions/wikia/VideoHandlers/templates/VideoPage_fileUsage.php
+++ extensions/wikia/VideoHandlers/templates/VideoPage_fileUsage.php
@@ -1,1 +1,19 @@
-Please code over me.  File Usage here
+<section class="page-listings">
+	<h2>Appears in these articles</h2>
+	<ul class="WikiaGrid">
+		<? for($i = 0; $i < 3; $i++) { ?>
+		<li class="page-listing">
+			<div class="grid-1 alpha">
+				<img src="" class="page-listing-image" height="90" width="160">
+			</div>
+			<div class="grid-3">
+				<h3 class="page-listing-title"><a href="">Maecenas non ipsum article name</a></h3>
+				<a href="" class="page-listing-wiki">Wiki Name</a>
+				<p class="page-listing-snippet">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. 
+				</p>
+			</div>
+		</li>
+		<? } ?>
+	</ul>
+</section>
