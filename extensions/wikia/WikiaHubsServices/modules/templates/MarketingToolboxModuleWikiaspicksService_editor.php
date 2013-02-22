cb19d86a20c7230699f00382c19cd4bdb4149f19
nAndy <nandy@wikia-inc.com>
2013-02-22 16:03:36 +0000

    added new message and new field in fields declaration of Wikia's picks module in SMTb

--- extensions/wikia/WikiaHubsServices/modules/templates/MarketingToolboxModuleWikiaspicksService_editor.php
+++ extensions/wikia/WikiaHubsServices/modules/templates/MarketingToolboxModuleWikiaspicksService_editor.php
@@ -8,7 +8,7 @@
 				'imageWidth' => (isset($sponsoredImage->width) ? $sponsoredImage->width : ''),
 				'imageHeight' => (isset($sponsoredImage->height) ? $sponsoredImage->height : ''),
 			)
-		); 
+		);
 	?>
 	<div class="module-box grid-4 alpha">
 		<div class="grid-3 alpha">
@@ -30,6 +30,12 @@
 					'MarketingToolbox',
 					'FormField',
 					array('inputData' => $fields['moduleTitle'])
+				);
+			?>
+			<?= $app->renderView(
+					'MarketingToolbox',
+					'FormField',
+					array('inputData' => $fields['imageLink'])
 				);
 			?>
 			<?= $app->renderView(
