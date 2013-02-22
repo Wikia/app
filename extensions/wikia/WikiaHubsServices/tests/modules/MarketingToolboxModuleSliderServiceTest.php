619b95d75bfe4712613ade400de066efb07c90f3
Damian Jozwiak <damian@wikia-inc.com>
2013-02-22 12:39:39 +0100

    bugid-95783 | S:MTb Slider - bold on slide name

--- extensions/wikia/WikiaHubsServices/tests/modules/MarketingToolboxModuleSliderServiceTest.php
+++ extensions/wikia/WikiaHubsServices/tests/modules/MarketingToolboxModuleSliderServiceTest.php
@@ -74,11 +74,11 @@ class MarketingToolboxModuleSliderServiceTest extends WikiaBaseTest
 			));
 		$expectedText1 = <<<WIKI_TEXT
 <gallery type="slider" orientation="mosaic">
-File:photoName1|alt 1|link=url 1|linktext=l desc 1|shorttext=s desc 1
-File:photoName2|alt 2|link=url 2|linktext=l desc 2|shorttext=s desc 2
-File:photoName3|alt 3|link=url 3|linktext=l desc 3|shorttext=s desc 3
-File:photoName4|alt 4|link=url 4|linktext=l desc 4|shorttext=s desc 4
-File:photoName5|alt 5|link=url 5|linktext=l desc 5|shorttext=s desc 5
+File:photoName1|'''alt 1'''|link=url 1|linktext=l desc 1|shorttext=s desc 1
+File:photoName2|'''alt 2'''|link=url 2|linktext=l desc 2|shorttext=s desc 2
+File:photoName3|'''alt 3'''|link=url 3|linktext=l desc 3|shorttext=s desc 3
+File:photoName4|'''alt 4'''|link=url 4|linktext=l desc 4|shorttext=s desc 4
+File:photoName5|'''alt 5'''|link=url 5|linktext=l desc 5|shorttext=s desc 5
 </gallery>
 WIKI_TEXT;
 
@@ -123,11 +123,11 @@ WIKI_TEXT;
 			));
 		$expectedText2 = <<<WIKI_TEXT
 <gallery type="slider" orientation="mosaic">
-File:other_photo_name1|alt text 1a|link=http://url.com/1|linktext=ultra very long description here 1, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 1
-File:other_photo_name2|alt text 2a|link=http://url.com/2|linktext=ultra very long description here 2, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 2
-File:other_photo_name3|alt text 3a|link=http://url.com/3|linktext=ultra very long description here 3, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 3
-File:other_photo_name4|alt text 4a|link=http://url.com/4|linktext=ultra very long description here 4, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 4
-File:other_photo_name5|alt text 5a|link=http://url.com/5|linktext=ultra very long description here 5, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 5
+File:other_photo_name1|'''alt text 1a'''|link=http://url.com/1|linktext=ultra very long description here 1, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 1
+File:other_photo_name2|'''alt text 2a'''|link=http://url.com/2|linktext=ultra very long description here 2, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 2
+File:other_photo_name3|'''alt text 3a'''|link=http://url.com/3|linktext=ultra very long description here 3, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 3
+File:other_photo_name4|'''alt text 4a'''|link=http://url.com/4|linktext=ultra very long description here 4, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 4
+File:other_photo_name5|'''alt text 5a'''|link=http://url.com/5|linktext=ultra very long description here 5, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris|shorttext=short desc 5
 </gallery>
 WIKI_TEXT;
 
