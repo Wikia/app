63c88ef4bdf6c8c49e1ff1abc396dc454e0b38ee
Damian Jozwiak <damian@wikia-inc.com>
2013-02-22 15:15:09 +0100

    bugid-98052 | S:MTb removed previev button

--- extensions/wikia/SpecialMarketingToolbox/templates/MarketingToolbox_Footer.php
+++ extensions/wikia/SpecialMarketingToolbox/templates/MarketingToolbox_Footer.php
@@ -1,6 +1,5 @@
 <div class="MarketingToolboxFooterGradient"></div>
 
 <footer class="MarketingToolboxFooter">
-	<input class="big secondary" type="submit" value="<?=$wf->msg('marketing-toolbox-footer-button-preview')?>"/>
-	<input class="big" type="submit" value="<?=$wf->msg('marketing-toolbox-footer-button-publish')?>" />
+	<input id="MarketingToolboxPublish" class="big" type="submit" value="<?=$wf->msg('marketing-toolbox-footer-button-publish')?>" <?= $allModulesSaved ?> />
 </footer>
