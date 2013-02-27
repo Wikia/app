a027b99833e3f01a52dbc7c9f4df407a67f46bf1
Damian Jozwiak <damian@wikia-inc.com>
2013-02-27 15:47:37 +0100

    bugid-98239 | Hubs v2 fixed old getPromoted functionality

--- extensions/wikia/WikiaHubsV2/WikiaHubsV2.setup.php
+++ extensions/wikia/WikiaHubsV2/WikiaHubsV2.setup.php
@@ -31,6 +31,8 @@ $app->registerClass('WikiaHubsV2Hooks', $dir . 'hooks/WikiaHubsV2Hooks.php');
 $app->registerClass('WikiaHubsV2Article', $dir . 'models/WikiaHubsV2Article.class.php');
 $app->registerClass('WikiaHubsV2Model', $dir . 'models/WikiaHubsV2Model.class.php');
 
+$app->registerClass('WikiaHubsV2SuggestModel', $dir . 'models/WikiaHubsV2SuggestModel.class.php');
+
 $app->registerClass('WikiaHubsV2Module', $dir . 'models/modules/WikiaHubsV2Module.class.php');
 $app->registerClass('WikiaHubsV2PulseModule', $dir . 'models/modules/WikiaHubsV2PulseModule.class.php');
 $app->registerClass('WikiaHubsV2SliderModule', $dir . 'models/modules/WikiaHubsV2SliderModule.class.php');
