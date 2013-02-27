0e322a92ed7512fb1319987c73268208a095f644
liz <liz@wikia-inc.com>
2013-02-27 01:08:21 +0000

    bugid:98260 fixing special videos lightbox when there's quotes

--- extensions/wikia/SpecialVideos/templates/SpecialVideosSpecial_index.php
+++ extensions/wikia/SpecialVideos/templates/SpecialVideosSpecial_index.php
@@ -30,7 +30,7 @@
 		<?php $alpha = $counter % 3 == 0 ? ' alpha' : ''; ?>
 
 		<div class="grid-2 video-element<?= $alpha ?>" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
-			<a href="<?= $video['fileUrl'] ?>" class="image video" data-video-name="<?= $video['fileTitle'] ?>">
+			<a href="<?= $video['fileUrl'] ?>" class="image video" data-video-name="<?= htmlspecialchars($video['fileTitle']) ?>">
 				<?= $video['videoPlayButton'] ?>
 				<img itemprop="thumbnail" alt="<?= $video['fileTitle'] ?>" src="<?= $video['thumbUrl'] ?>" width="<?= $thumbWidth ?>" height="<?= $thumbHeight ?>" data-video="<?= $video['fileTitle'] ?>" class="Wikia-video-thumb thumbimage">
 				<?= $video['videoOverlay'] ?>
