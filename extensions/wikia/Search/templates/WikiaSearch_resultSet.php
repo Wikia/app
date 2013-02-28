bd261ab0ea54c02871e2bb4380e8392952553a70
Robert Elwell <robert@wikia-inc.com>
2013-02-28 22:35:37 +0000

    removed relevancy function id stuff from templates as not useful

--- extensions/wikia/Search/templates/WikiaSearch_resultSet.php
+++ extensions/wikia/Search/templates/WikiaSearch_resultSet.php
@@ -3,7 +3,7 @@
 	<li class="result">
 
 		<?php
-			$trackingData = 'class="ResultLink" data-wid="'.$resultSet->getHeader('cityId').'" data-gpos="'.$pos.'" data-pos="0" data-sterm="'.addslashes($query).'" data-stype="'.( $isInterWiki ? 'inter' : 'intra' ).'" data-rver="'.$relevancyFunctionId.'" data-event="search_click_wiki"';
+			$trackingData = 'class="ResultLink" data-wid="'.$resultSet->getHeader('cityId').'" data-gpos="'.$pos.'" data-pos="0" data-sterm="'.addslashes($query).'" data-stype="'.( $isInterWiki ? 'inter' : 'intra' ).'" data-rver="6" data-event="search_click_wiki"';
 		?>
 
 		<h3><a href="<?=$resultSet->getHeader('cityUrl');?>" <?=$trackingData;?> ><?=$resultSet->getHeader('cityTitle');?></a></h3>
@@ -18,11 +18,11 @@
 		<?php foreach ( $resultSet as $result ): ?>
 			<?php if ( $result instanceof Wikia\Search\Result || $i++ < 5 ): ?>
 				<div class="grouped-result <?php if($i%2): ?>new-row<?php endif; ?>">
-					<?= $app->getView( 'WikiaSearch', 'result', array( 'result' => $result, 'gpos' => $pos, 'pos' => $i, 'query' => $query, 'inGroup' => true, 'isInterWiki' => $isInterWiki, 'relevancyFunctionId' => $relevancyFunctionId )); ?>
+					<?= $app->getView( 'WikiaSearch', 'result', array( 'result' => $result, 'gpos' => $pos, 'pos' => $i, 'query' => $query, 'inGroup' => true, 'isInterWiki' => $isInterWiki )); ?>
 				</div>
 			<?php else: break; endif; ?>
 		<?php endforeach; ?>
 	</li>
 <?php elseif ($nextResult = $resultSet->next()): ?>
-	<?= $app->getView( 'WikiaSearch', 'result', array( 'result' => $nextResult, 'gpos' => 0, 'pos' => $pos, 'query' => $query, 'rank' =>  $resultSet->getHeader('cityRank'), 'isInterWiki'=> true, 'relevancyFunctionId' => $relevancyFunctionId )); ?>
+	<?= $app->getView( 'WikiaSearch', 'result', array( 'result' => $nextResult, 'gpos' => 0, 'pos' => $pos, 'query' => $query, 'rank' =>  $resultSet->getHeader('cityRank'), 'isInterWiki'=> true )); ?>
 <?php endif; ?>
