bb7c45e27320580f1a0959f7dda7ce5420a05bba
Jakub Olek <jolek@wikia-inc.com>
2013-03-01 13:01:47 +0100

    Fix styling and closing of this section

--- extensions/wikia/RelatedPages/templates/RelatedPages_WikiaMobileIndex.php
+++ extensions/wikia/RelatedPages/templates/RelatedPages_WikiaMobileIndex.php
@@ -1,4 +1,4 @@
 <section id=wkRelPag>
 	<h1 id=wkRelPagHead class="collSec addChev open"><?= $wf->Message( 'wikiarelatedpages-heading' )->inContentLanguage()->text(); ?></h1>
-	<ul class="wkLst open"></ul>
+	<ul class="wkLst artSec open"></ul>
 </section>
