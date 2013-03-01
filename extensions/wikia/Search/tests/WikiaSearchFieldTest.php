c42c62a17216fea570025e155f9bbd3976937cd9
Robert Elwell <robert@wikia-inc.com>
2013-03-01 17:59:55 +0000

    added tests for Wikia\Search\Field namespace

---
+++ extensions/wikia/Search/tests/WikiaSearchFieldTest.php
@@ -0,0 +1,141 @@
+<?php
+
+require_once( 'WikiaSearchBaseTest.php' );
+
+class WikiaSearchFieldTest extends WikiaSearchBaseTest {
+
+	/**
+	 * @covers Wikia\Search\Field\Field::__construct
+	 */
+	public function test__construct() {
+		$field = new Wikia\Search\Field\Field( 'html', 'en' );
+		
+		$reflFieldName = new ReflectionProperty( 'Wikia\Search\Field\Field', 'fieldName' );
+		$reflFieldName->setAccessible( true );
+		$this->assertEquals(
+				'html',
+				$reflFieldName->getValue( $field )
+		);
+		$reflLangCode = new ReflectionProperty( 'Wikia\Search\Field\Field', 'languageCode' );
+		$reflLangCode->setAccessible( true );
+		$this->assertEquals(
+				'en',
+				$reflLangCode->getValue( $field )
+		);
+	}
+	
+	/**
+	 * @covers Wikia\Search\Field\Field::__toString()
+	 */
+	public function test__toString() {
+		$field = new Wikia\Search\Field\Field( 'html', 'en' );
+		$this->assertEquals(
+				'html_en',
+				$field->__toString()
+		);
+		$field = new Wikia\Search\Field\Field( 'html', 'zzzz' );
+		$this->assertEquals(
+				'html',
+				$field->__toString()
+		);
+		$field = new Wikia\Search\Field\Field( 'redirect_titles', 'en' );
+		$this->assertEquals(
+				'redirect_titles_mv_en',
+				$field->__toString()
+		);
+	}
+	
+	/**
+	 * @covers Wikia\Search\Field\FieldExpression::getFieldValueString
+	 * @covers Wikia\Search\Field\FieldExpression::__toString()
+	 */
+	public function testFieldExpression__toString() {
+		$mockFe = $this->getMockBuilder( 'Wikia\Search\Field\FieldExpression' )
+		               ->disableOriginalConstructor()
+		               ->setMethods( array( 'getNegationString', 'getFieldValueString', 'getBoostString' ) )
+		               ->getMock();
+		
+		$mockFe
+		    ->expects( $this->once() )
+		    ->method ( 'getNegationString' )
+		    ->will   ( $this->returnValue( '-' ) )
+		;
+		$mockFe
+		    ->expects( $this->once() )
+		    ->method ( 'getFieldValueString' )
+		    ->will   ( $this->returnValue( '(foo:bar)' ) )
+		;
+		$mockFe
+		    ->expects( $this->once() )
+		    ->method ( 'getBoostString' )
+		    ->will   ( $this->returnValue( '^5' ) )
+		;
+		$this->assertEquals(
+				'-(foo:bar)^5',
+				$mockFe->__toString()
+		);
+	}
+	
+	/**
+	 * @covers Wikia\Search\Field\FieldExpression::getFieldValueString
+	 * @covers Wikia\Search\Field\FieldExpression::getNegationString
+	 * @covers Wikia\Search\Field\FieldExpression::setNegate
+	 */
+	public function testNegation()
+	{
+		$field = new Wikia\Search\Field\Field( 'foo', 'en' );
+		$expr = new Wikia\Search\Field\FieldExpression( $field, array( 'value' => 'bar' ) );
+		
+		$this->assertEquals(
+				"(foo:bar)",
+				$expr->__toString()
+		);
+		$expr->setNegate( true );
+		$this->assertEquals(
+				"-(foo:bar)",
+				$expr->__toString()
+		);
+	}
+	
+	
+	/**
+	 * @covers Wikia\Search\Field\FieldExpression::getFieldValueString
+	 * @covers Wikia\Search\Field\FieldExpression::getBoostString
+	 * @covers Wikia\Search\Field\FieldExpression::setBoost
+	 */
+	public function testBoosting()
+	{
+		$field = new Wikia\Search\Field\Field( 'foo', 'en' );
+		$expr = new Wikia\Search\Field\FieldExpression( $field, array( 'value' => 'bar' ) );
+		
+		$this->assertEquals(
+				"(foo:bar)",
+				$expr->__toString()
+		);
+		$expr->setBoost( 5 );
+		$this->assertEquals(
+				"(foo:bar)^5",
+				$expr->__toString()
+		);
+	}
+	
+	/**
+	 * @covers Wikia\Search\Field\FieldExpression::getFieldValueString
+	 * @covers Wikia\Search\Field\FieldExpression::setValueQuote
+	 */
+	public function testQuoting()
+	{
+		$field = new Wikia\Search\Field\Field( 'foo', 'en' );
+		$expr = new Wikia\Search\Field\FieldExpression( $field, array( 'value' => 'bar' ) );
+		
+		$this->assertEquals(
+				"(foo:bar)",
+				$expr->__toString()
+		);
+		$expr->setValueQuote( '"' );
+		$this->assertEquals(
+				'(foo:"bar")',
+				$expr->__toString()
+		);
+	}
+}
