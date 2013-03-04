be0b1a763e1fd1daa36648a564e49a6e0aee3620
Damian Jozwiak <damian@wikia-inc.com>
2013-03-04 12:21:35 +0100

    bugid-98818 | Hubs v2 - clean in messages

--- extensions/wikia/WikiaHubsV2/templates/WikiaHubsV2Suggest_suggestArticle.php
+++ extensions/wikia/WikiaHubsV2/templates/WikiaHubsV2Suggest_suggestArticle.php
@@ -2,8 +2,4 @@
 	<h1><?= wfMsg('wikiahubs-suggest-article-header') ?></h1>
 
 	<?= F::app()->renderView( 'WikiaStyleGuideForm', 'index', array('form' => $formData) ); ?>
-
-	<div class="close-button">
-		<button class="wikia-button close"><?= wfMsg( 'wikiahubs-button-close' ) ?></button>
-	</div>
 </div>
