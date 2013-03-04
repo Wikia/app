4de25ef8eea1b78c5438c50288af0f265a7ce37a
macbre <maciej.brencz@gmail.com>
2013-03-04 11:58:45 +0100

    BAC-86: Add a hook to check if uploaded image file is corrupted
    Add unit tests

---
+++ includes/wikia/tests/UploadVerifyFileTest.php
@@ -0,0 +1,60 @@
+<?php
+
+class TestUploadClass extends UploadBase {
+	public function initializeFromRequest( &$request ) {}
+}
+
+class UploadVerifyFile extends WikiaBaseTest {
+	/**
+	 * @dataProvider uploadVerifyFileDataProvider
+	 */
+	public function testUploadVerifyFile($mime, $expectedReturnVal, $uploadContent = '') {
+		if ($uploadContent !== '') {
+			$tmpPath = tempnam("/tmp", "UploadTest");
+			file_put_contents($tmpPath, $uploadContent);
+		}
+		else {
+			$tmpPath = false;
+		}
+
+		$titleMock = $this->mockClassWithMethods('Title', array(
+			'getText' => 'Foo'
+		));
+		$uploadMock = $this->mockClassWithMethods('TestUploadClass', array(
+			'getTitle' => $titleMock,
+			'getTempPath' => $tmpPath
+		));
+
+		$status = array();
+		$ret = Wikia::onUploadVerifyFile($uploadMock, $mime, $status);
+
+		$this->assertEquals($expectedReturnVal, $ret);
+		$this->assertEquals($ret ? array() : array('verification-error'), $status);
+	}
+
+	public function uploadVerifyFileDataProvider() {
+		return array(
+			// valid image
+			array(
+				'mime' => 'image/gif',
+				'expectedReturnVal' => true,
+				'uploadContent' => base64_decode('R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D'), // blank 1x1 gif
+			),
+			// invalid image
+			array(
+				'mime' => 'image/gif',
+				'expectedReturnVal' => false,
+				'uploadContent' => 'foobar',
+			),
+			// don't do anything if uploaded file is not a supported image
+			array(
+				'mime' => 'image/svg',
+				'expectedReturnVal' => true,
+			),
+			array(
+				'mime' => 'application/pdf',
+				'expectedReturnVal' => true,
+			)
+		);
+	}
+}
