f3a70bad20449eaa7c5c7bce158c68636d292644
nAndy <nandy@wikia-inc.com>
2013-03-06 07:29:15 +0000

    added ok button to the template

--- extensions/wikia/WikiaHubsV2/templates/WikiaHubsV2Suggest_suggestArticle.php
+++ extensions/wikia/WikiaHubsV2/templates/WikiaHubsV2Suggest_suggestArticle.php
@@ -3,4 +3,7 @@
 
 	<?= F::app()->renderView( 'WikiaStyleGuideForm', 'index', array('form' => $formData) ); ?>
 	<p class="successMessage"></p>
+	<div class="close-button">
+		<button class="wikia-button close"><?= wfMessage('ok')->text(); ?></button>
+	</div>
 </div>
