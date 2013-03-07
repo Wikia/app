448d905c9778995fcaba8da9d14e52fc17e1cd76
saipetch <saipetch@wikia-inc.com>
2013-03-06 18:37:44 -0800

    Video (bugid 98408): Ooyala - update player to v3

--- extensions/wikia/VideoHandlers/handlers/OoyalaVideoHandler.class.php
+++ extensions/wikia/VideoHandlers/handlers/OoyalaVideoHandler.class.php
@@ -2,6 +2,9 @@
 
 class OoyalaVideoHandler extends VideoHandler {
 
+	const OOYALA_PLAYER_ID = '52bc289bedc847e3aa8eb2b347644f68';
+	const OOYALA_PLAYER_ID_AGEGATE = '5b38887edf80466cae0b5edc918b27e8';
+
 	protected $apiName = 'OoyalaApiWrapper';
 	protected static $urlTemplate = 'http://player.ooyala.com/player.swf?embedCode=$1&version=2';
 	protected static $providerDetailUrlTemplate = 'http://video.wikia.com/';
@@ -9,12 +12,18 @@ class OoyalaVideoHandler extends VideoHandler {
 
 	public function getEmbed( $articleId, $width, $autoplay = false, $isAjax = false, $postOnload=false ) {
 		$height = $this->getHeight($width);
-		$autoPlayStr = ( $autoplay ) ? 1 : 0;
-		$containerId = 'ooyalaplayer-'.$this->videoId.'-'.time();
+		//$url = $this->getEmbedUrl();
+		$playerId = 'ooyalaplayer-'.$this->videoId.'-'.time().'-'.intval($isAjax);
+		$jsFile = 'http://player.ooyala.com/v3/'.self::OOYALA_PLAYER_ID;
+
+		$autoPlayStr = ( $autoplay ) ? 'true' : 'false';
 
 		$embed = <<<EOT
-<div id="{$containerId}" style="width:{$width}px;"></div>
-<script type="text/javascript" src="http://player.ooyala.com/player.js?embedCode={$this->videoId}&width={$width}&height={$height}&playerContainerId={$containerId}&autoplay={$autoPlayStr}"></script>
+<script src='{$jsFile}'></script>
+<div id='{$playerId}' style='width:{$width}px;height:{$height}px;'></div>
+<script>
+    OO.ready(function() { OO.Player.create('{$playerId}','{$this->videoId}', { autoplay:{$autoPlayStr} }); });
+</script>
 EOT;
 
 		return $embed;
