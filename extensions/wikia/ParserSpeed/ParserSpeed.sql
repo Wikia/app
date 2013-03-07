b664347cedd9ca3ebdc23dc78aeb028a6dc2bb15
Władysław Bodzek <wladek@wikia-inc.com>
2013-03-07 17:38:24 +0100

    PER-5: added indexes to parser_speed_article table

--- extensions/wikia/ParserSpeed/ParserSpeed.sql
+++ extensions/wikia/ParserSpeed/ParserSpeed.sql
@@ -3,6 +3,8 @@ CREATE TABLE parser_speed_article (
   wiki_id INT NOT NULL,
   article_id INT NOT NULL,
 
+  page_ns INT,
+
   average_time NUMERIC(10,3),
   minimum_time NUMERIC(10,3),
   maximum_time NUMERIC(10,3),
@@ -12,7 +14,13 @@ CREATE TABLE parser_speed_article (
 
   updated TIMESTAMP,
 
-  PRIMARY KEY (wiki_id,article_id)
+  PRIMARY KEY (wiki_id,article_id),
+  KEY namespace (wiki_id,page_ns),
+  KEY avg_time (wiki_id,average_time),
+  KEY min_time (wiki_id,minimum_time),
+  KEY max_time (wiki_id,maximum_time),
+  KEY wikitext_size (wiki_id,wikitext_size),
+  KEY html_size (wiki_id,html_size)
 );
 
 CREATE TABLE parser_speed_article_rollup (
