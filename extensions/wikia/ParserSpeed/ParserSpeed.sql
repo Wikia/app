d559cdc38f3a4ed269f382830ff64bda936a31d8
Władysław Bodzek <wladek@wikia-inc.com>
2013-03-07 14:46:59 +0100

    PER-4: adjusted db schema to the new formula

--- extensions/wikia/ParserSpeed/ParserSpeed.sql
+++ extensions/wikia/ParserSpeed/ParserSpeed.sql
@@ -4,13 +4,13 @@ CREATE TABLE parser_speed_article (
   article_id INT NOT NULL,
 
   average_time NUMERIC(10,3),
-
-  rolling_time_sum NUMERIC(10,3),
-  rolling_time_count INT,
-  rolling_time_previous NUMERIC(10,3),
+  minimum_time NUMERIC(10,3),
+  maximum_time NUMERIC(10,3),
 
   wikitext_size INT,
   html_size INT,
+
+  updated TIMESTAMP,
 
   PRIMARY KEY (wiki_id,article_id)
 );
