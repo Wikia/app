11f319ab69d323ba8e18272a2fff20f30c9049eb
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-03-08 11:17:59 +0000

    bugid-11055 code quality fix

--- extensions/wikia/CreateNewWiki/CreateNewWikiHooks.class.php
+++ extensions/wikia/CreateNewWiki/CreateNewWikiHooks.class.php
@@ -1,16 +1,14 @@
 <?php
 class CreateNewWikiHooks {
 	public static function onBeforePageDisplay() {
-		global $wgOut, $wgRequest;
-
 		$wg = F::app()->wg;
 
-		$wikiWelcome = $wgRequest->getVal('wiki-welcome');
+		$wikiWelcome = $wg->request->getVal('wiki-welcome');
 		$assetsManager = F::build( 'AssetsManager', array(), 'getInstance' );
 
 		if(!empty($wikiWelcome)) {
-			$wgOut->addStyle( $assetsManager->getSassCommonURL( 'extensions/wikia/CreateNewWiki/css/WikiWelcome.scss' ) );
-			$wgOut->addScript( '<script src="' . $wg->ExtensionsPath . '/wikia/CreateNewWiki/js/WikiWelcome.js"></script>' );
+			$wg->out->addStyle( $assetsManager->getSassCommonURL( 'extensions/wikia/CreateNewWiki/css/WikiWelcome.scss' ) );
+			$wg->out->addScript( '<script src="' . $wg->ExtensionsPath . '/wikia/CreateNewWiki/js/WikiWelcome.js"></script>' );
 		}
 		return true;
 	}
