21e79be76a5460866416d171af40b1d7d7c249e2
Robert Elwell <robert@wikia-inc.com>
2013-03-11 21:34:57 +0000

    removed singleton pattern and made memoized properties in mw interface static

--- extensions/wikia/Search/classes/IndexService/Factory.php
+++ extensions/wikia/Search/classes/IndexService/Factory.php
@@ -5,29 +5,12 @@
  */
 namespace Wikia\Search\IndexService;
 /**
- * A singleton instance for instantiating services.
+ * A responsible for instantiating services.
  * This lets us make something else reponsible for dependency injection.
  * @author relwell
  */
 class Factory
 {
-	/**
-	 * Singleton instance
-	 * @var 
-	 */
-	private static $instance;
-	
-	/**
-	 * Return a singleton instance
-	 * @return \Wikia\Search\IndexService\Factory
-	 */
-	public static function getInstance() {
-		if (! isset( self::$instance ) ) {
-			self::$instance = new Factory();
-		}
-		return self::$instance;
-	}
-	
 	/**
 	 * Allows us to instantiate a service anywhere in the app by name only importing a single class
 	 * @param string $terminalClassName
