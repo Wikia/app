3409fb98bf724e115630e60b3063310033467c82
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-03-13 20:13:10 +0000

    Revert "bugid-11055 code quality fix"
    This reverts commit 11f319ab69d323ba8e18272a2fff20f30c9049eb.

--- extensions/wikia/CreateNewWiki/CreateNewWikiHooks.class.php
+++ extensions/wikia/CreateNewWiki/CreateNewWikiHooks.class.php
@@ -1,14 +1,16 @@
 <?php
 class CreateNewWikiHooks {
 	public static function onBeforePageDisplay() {
+		global $wgOut, $wgRequest;
+
 		$wg = F::app()->wg;
 
-		$wikiWelcome = $wg->request->getVal('wiki-welcome');
+		$wikiWelcome = $wgRequest->getVal('wiki-welcome');
 		$assetsManager = F::build( 'AssetsManager', array(), 'getInstance' );
 
 		if(!empty($wikiWelcome)) {
-			$wg->out->addStyle( $assetsManager->getSassCommonURL( 'extensions/wikia/CreateNewWiki/css/WikiWelcome.scss' ) );
-			$wg->out->addScript( '<script src="' . $wg->ExtensionsPath . '/wikia/CreateNewWiki/js/WikiWelcome.js"></script>' );
+			$wgOut->addStyle( $assetsManager->getSassCommonURL( 'extensions/wikia/CreateNewWiki/css/WikiWelcome.scss' ) );
+			$wgOut->addScript( '<script src="' . $wg->ExtensionsPath . '/wikia/CreateNewWiki/js/WikiWelcome.js"></script>' );
 		}
 		return true;
 	}
