183dab5e01376d1b9805a0264c6e395951d90692
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-03-13 20:13:07 +0000

    Revert "bugid-11055 use $out from parameter in handler"
    This reverts commit 20ace27cf1c21ce0ca94e7e1ce12cbdad6c1fd29.

--- extensions/wikia/CreateNewWiki/CreateNewWikiHooks.class.php
+++ extensions/wikia/CreateNewWiki/CreateNewWikiHooks.class.php
@@ -1,14 +1,14 @@
 <?php
 class CreateNewWikiHooks {
-	public static function onBeforePageDisplay($out) {
+	public static function onBeforePageDisplay() {
 		$wg = F::app()->wg;
 
 		$wikiWelcome = $wg->request->getVal('wiki-welcome');
 		$assetsManager = F::build( 'AssetsManager', array(), 'getInstance' );
 
 		if(!empty($wikiWelcome)) {
-			$out->addStyle( $assetsManager->getSassCommonURL( 'extensions/wikia/CreateNewWiki/css/WikiWelcome.scss' ) );
-			$out->addScript( '<script src="' . $wg->ExtensionsPath . '/wikia/CreateNewWiki/js/WikiWelcome.js"></script>' );
+			$wg->out->addStyle( $assetsManager->getSassCommonURL( 'extensions/wikia/CreateNewWiki/css/WikiWelcome.scss' ) );
+			$wg->out->addScript( '<script src="' . $wg->ExtensionsPath . '/wikia/CreateNewWiki/js/WikiWelcome.js"></script>' );
 		}
 		return true;
 	}
