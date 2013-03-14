22159c5474d18dc9c0fcd6350174a1bf485b10c7
nAndy <nandy@wikia-inc.com>
2013-03-14 15:56:14 +0000

    get WAM page name and WAM FAQ page from WF config, use mb_strtolower()

--- extensions/wikia/WAMPage/WAMPageHooks.class.php
+++ extensions/wikia/WAMPage/WAMPageHooks.class.php
@@ -11,12 +11,19 @@ class WAMPageHooks {
 		$app = F::app();
 		$dbKey = null;
 
+		if( !empty($app->wg->WamPageConfig) ) {
+			$config = $app->wg->WamPageConfig;
+			$wamPageName = mb_strtolower( $config['pageName'] );
+			$wamPageFaqPageName = mb_strtolower( $config['faqPageName'] );
+		} else {
+			return true;
+		}
+		
 		if( $title instanceof Title ) {
-			$dbKey = $title->getDBKey();
+			$dbKey = mb_strtolower( $title->getDBKey() );
 		}
 
-		if( !empty($app->wg->EnableWAMPageExt)
-			&& ($dbKey === WAMPageArticle::WAM_PAGE_NAME || $dbKey === WAMPageArticle::WAM_FAQ_PAGE_NAME)
+		if( !empty($app->wg->EnableWAMPageExt) && ( $dbKey === $wamPageName || $dbKey === $wamPageFaqPageName )
 		) {
 			$article = new WAMPageArticle($title);
 		}
