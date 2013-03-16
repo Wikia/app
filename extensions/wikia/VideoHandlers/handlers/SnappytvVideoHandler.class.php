75baf7fad8580a43715ffa2efcfb2cc82a558648
saipetch <saipetch@wikia-inc.com>
2013-03-15 17:26:33 -0700

    Video (bugid 97838): Snappy TV - updated video handler

--- extensions/wikia/VideoHandlers/handlers/SnappytvVideoHandler.class.php
+++ extensions/wikia/VideoHandlers/handlers/SnappytvVideoHandler.class.php
@@ -4,21 +4,28 @@ class SnappytvVideoHandler extends VideoHandler {
 
 	protected $apiName = 'SnappytvApiWrapper';
 	protected static $urlTemplate = 'https://www.snappytv.com/embed_player.swf?id=$1';
-	protected static $providerDetailUrlTemplate = 'http://snpy.tv/$1';
+	protected static $providerDetailUrlTemplate = 'http://www.snappytv.com/snap/$1';
 	protected static $providerHomeUrl = 'http://www.snappytv.com/';
 
 	public function getEmbed( $articleId, $width, $autoplay = false, $isAjax = false, $postOnload = false ) {
 		$height = $this->getHeight( $width );
 		$autoPlayStr = $autoplay ? 1 : 0 ;
-		$url = $this->getEmbedUrl().'&autoplay='.$autoPlayStr;
 
-		$html = <<<EOT
-<object width='$width' height='$height'>
-	<param name='movie' value='$url';></param>
+		if ( F::app()->checkSkin( 'wikiamobile' ) ) {
+			$url = $this->getProviderDetailUrl();
+			$html = <<<EOT
+<iframe src='{$url}?w={$width}&h={$height}' width='{$width}' height='{$height}' frameborder='0' scrolling='no' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+EOT;
+		} else {
+			$url = $this->getEmbedUrl().'&autoplay='.$autoPlayStr;
+			$html = <<<EOT
+<object width='{$width}' height='{$height}'>
+	<param name='movie' value='{$url}'></param>
 	<param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param>
-	<embed src='$url'; type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='$width' height='$height'></embed>
+	<embed src='{$url}' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='{$width}' height='{$height}'></embed>
 </object>
 EOT;
+		}
 		return $html;
 	}
 
@@ -28,14 +35,15 @@ EOT;
 	}
 
 	public function getProviderDetailUrl() {
-		return str_replace( '$1', $this->getAltVideoId(), static::$providerDetailUrlTemplate );
+		return str_replace( '$1', $this->getUniqueName(), static::$providerDetailUrlTemplate );
 	}
 
-	protected function getAltVideoId() {
+	protected function getUniqueName() {
 		$metadata = $this->getMetadata( true );
-		if ( !empty($metadata['altVideoId']) ) {
-			return $metadata['altVideoId'];
+		if ( !empty($metadata['uniqueName']) ) {
+			return $metadata['uniqueName'];
 		}
+
 		return '';
 	}
 
