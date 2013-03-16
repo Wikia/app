baa55d8735a25c5d9b93657dc39e2b29fd7c073c
Garth Webb <gart@wikia-inc.com>
2013-03-15 18:01:45 -0700

    Added more comments

--- extensions/wikia/VideoHandlers/VideoPageController.class.php
+++ extensions/wikia/VideoHandlers/VideoPageController.class.php
@@ -1,23 +1,39 @@
 <?php
 
+/**
+ * Controller for the video file page
+ *
+ * @author garth@wikia-inc.com
+ * @author hyun@wikia-inc.com
+ */
+
 class VideoPageController extends WikiaController {
 
 	/**
-	 *
+	 * Collects data about what articles the current file appears in, either
+	 * locally and globally.
+	 * 
+	 * @requestParam string type (optional) Possible values are 'global' or 'local', defaulting to 'local' if not given.
+	 *                                      Determines whether this collects a local or global file usage data
 	 */
 	public function fileUsage() {
 		$app = F::app();
 
+		// The 'limit' parameter is used by both usage methods we forward to as a way to limit
+		// the number of rows returned.  This is a safeguard against extreme cases
 		$this->setVal('limit', 50);
 
-		$heading = '';
-		$fileList = array();
-		$shortenedSummary = array();
+		$heading = '';               // The message to use for the section header
+		$shortenedSummary = array(); // A subset of the data returned to show immediately
+		$fileList = array();         // The list of other articles to show based on $shortenedSummary
+
 		$type = $this->getVal('type', 'local');
 
+		// Based on $typoe get global or local data
 		if ($type === 'global') {
 			$heading = wfMsg('video-page-global-file-list-header');
 
+			// Forward to the getGlobalUsage method
 			$summary = $app->sendRequest('VideoPageController', 'getGlobalUsage')->getData()['summary'];
 			
 			if (array_key_exists($this->wg->DBname, $summary)) {
@@ -26,7 +42,9 @@ class VideoPageController extends WikiaController {
 			
 			$count = 0;
 			
-			// shorten it to 3
+			// Shorten the list to 3 articles.  We'll flesh out these three with full
+			// details to display now and flesh out the others dynamically from JS when
+			// the user pages forward
 			foreach ($summary as $wiki => $articles) {
 				if ($count < 3) {
 					foreach ($articles as $article) {
@@ -47,21 +65,38 @@ class VideoPageController extends WikiaController {
 			$heading = wfMsg('video-page-file-list-header');
 			$summary = $app->sendRequest('VideoPageController', 'getLocalUsage')->getData()['summary'];
 
+			// Shorten the list down to three articles much like above in global, but
+			// here we also need to make the $shortentedSummary structure uniform to match
+			// the global case
 			if ($summary and count($summary)) {
 				$dbName = $this->wg->DBname;
 				$shortenedSummary = array($dbName => array_slice($summary, 0, 3));
 			}
 		}
 
-		$data = $app->sendRequest( 'VideoPageController', 'fileList', array('summary' => $shortenedSummary, 'type' => $type) )->getData();
+		// Send the $shortenedSummary to fileList to flesh out the details
+		$params = array('summary' => $shortenedSummary, 'type' => $type);
+		$data = $app->sendRequest( 'VideoPageController', 'fileList', $params )->getData();
 		$fileList = empty($data['fileList']) ? array() : $data['fileList'];
 
+		// Set template variables
 		$this->heading = $heading;
 		$this->fileList = $fileList;
 		$this->summary = $summary;
 		$this->type = $type;
 	}
 	
+	/**
+	 * This method takes the minimum data provided in summary (at least an article title
+	 * an article ID and namespace ID) and fills in additional details needed to show
+	 * an image, a snippet and some links to the source article.
+	 *
+	 * @requestParam summary array (required) An array of associative arrays.  This data is
+	 *                                        returned by fileUsage above as $summary
+	 * @requestParam type string (optional) Can be 'global' or 'local'.  Defaults to 'local'.
+	 *                                      Determines whether to grab summary data from the local wiki
+	 *                                      or from an external wiki
+	 */
 	public function fileList() {
 		$summary = $this->getVal('summary', '');
 		$type = $this->getVal('type', '');
@@ -94,6 +129,10 @@ class VideoPageController extends WikiaController {
 		return $wikiData->city_title;
 	}
 
+	/**
+	 * Controller to handle showing pages related to the current file.  Uses
+	 * the RelatedPages extension to render the final HTML
+	 */
 	public function relatedPages() {
 		$this->text = '';
 
@@ -136,6 +175,9 @@ class VideoPageController extends WikiaController {
 		$this->text = F::app()->renderView( 'RelatedPages', 'Index', array( "altTitle" => $title ) );
 	}
 
+	/**
+	 * Controller to display a caption under the video, including provider and views
+	 */
 	public function videoCaption() {
 		global $wgWikiaVideoProviders;
 
@@ -162,9 +204,18 @@ class VideoPageController extends WikiaController {
 		$target = $this->wg->Title->getDBkey();
 		$dbr = wfGetDB( DB_SLAVE );
 
-		# We want to find the first page that has a link to the current file page AND
-		# has at least one category associated with it.  The categor(ies) are how
-		# the RelatedPages extention determines what's related
+		// We want to find the first page that has a link to the current file page AND
+		// has at least one category associated with it.  The categor(ies) are how
+		// the RelatedPages extention determines what's related.  The query looks something
+		// like:
+		// 
+		//     SELECT distinct(page_id) as page_id
+		//       FROM imagelinks, page, categorylinks
+		//      WHERE il_to = 'Scooby_Eats_Scooby_Snacks'
+		//        AND il_from = page_id
+		//        AND page_is_redirect = 0
+		//        AND cl_from = page_id
+		//      LIMIT 1
 		$res = $dbr->select(
 			array( 'imagelinks', 'page', 'categorylinks' ),
 			array( 'distinct(page_id) as page_id' ),
@@ -179,12 +230,18 @@ class VideoPageController extends WikiaController {
 		return empty($info) ? null : $info->page_id;
 	}
 
+	/**
+	 * Figure out what articles on the local wiki are using this file.
+	 */
 	public function getLocalUsage () {
 		$target = $this->getVal('fileTitle', $this->wg->Title->getDBkey());
 		$limit = $this->wg->Request->getInt('limit', 50);
 
 		$dbr = wfGetDB( DB_SLAVE );
 
+		// This SQL is lifted from the core file page code that shows where a file is
+		// used but it couldn't be resued because it included outputing HTML.  It finds
+		// pages that are referenced from imagelinks rows with our current file.
 		$res = $dbr->select(
 			array( 'imagelinks', 'page' ),
 			array( 'page_id', 'page_namespace', 'page_title', 'page_is_redirect', 'il_to' ),
@@ -206,6 +263,9 @@ class VideoPageController extends WikiaController {
 		$this->summary = $summary;
 	}
 
+	/**
+	 * Figure out what articles include this file from any wiki
+	 */
 	public function getGlobalUsage () {
 		$fileTitle = $this->getVal('fileTitle', '');
 		$titleObj = empty($fileTitle) ? $this->wg->Title : Title::newFromText($fileTitle);
@@ -236,6 +296,10 @@ class VideoPageController extends WikiaController {
 		$this->summary = $summary;
 	}
 
+	/**
+	 * Add more detail for local articles to the current $data by forwarding to 
+	 * the ArticleSummaryController
+	 */
 	public function addLocalSummary ( $data ) {
 		return $this->addSummary($data, function ($dbName, $articleIds) {
 			$response = $this->app->sendRequest('ArticleSummaryController', 'blurb', array('ids' => implode(',', $articleIds)));
@@ -243,6 +307,10 @@ class VideoPageController extends WikiaController {
 		});
 	}
 
+	/**
+	 * Add more detail for global articles to the current $data by making HTTP requests
+	 * to the other wiki URLs
+	 */
 	public function addGlobalSummary( $data ) {
 		return $this->addSummary($data, function ($dbName, $articleIds) {
 			$url = WikiFactory::DBtoURL($dbName);
