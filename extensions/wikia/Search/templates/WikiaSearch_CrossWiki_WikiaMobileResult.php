a53bb3da3cf08901ea12e4f3eb7499e2e629a37c
Rafal Leszczynski <rafal@wikia-inc.com>
2013-03-18 13:59:14 +0100

    mobile tracking remove unneeded code

--- extensions/wikia/Search/templates/WikiaSearch_CrossWiki_WikiaMobileResult.php
+++ extensions/wikia/Search/templates/WikiaSearch_CrossWiki_WikiaMobileResult.php
@@ -1,20 +1,12 @@
 <?php if($resultSet->getResultsFound() > 1 ): ?>
 	<li class=group>
-		<?php
-		$trackingData = 'class=ResultLink data-wid="'.$resultSet->getHeader('cityId').
-			'" data-gpos="' . $pos .
-			'" data-pos=0 data-sterm="' . addslashes($query).
-			'" data-stype="' . ( $isInterWiki ? 'inter' : 'intra' ).
-			'" data-rver="' . WikiaSearchController::RVERSION .
-			'" data-event=search_click_wiki';
-		?>
 		<p>
-			<a class=groupTitle href="<?= $resultSet->getHeader( 'host' );?>" <?= $trackingData; ?> ><?=
+			<a class=groupTitle href="<?= $resultSet->getHeader( 'host' );?>"><?=
 				$resultSet->getHeader( 'wikititle' );?></a>
 			<a class=searchGroup href="<?= $resultSet->getHeader('host') .'/wiki/Special:Search?search='.urlencode
 			($query).'&fulltext=Search';?>"></a>
 		</p>
-		<a class=url href="<?= $resultSet->getHeader( 'host' );?>" <?= $trackingData; ?> ><?=$resultSet->getHeader(
+		<a class=url href="<?= $resultSet->getHeader( 'host' );?>"><?=$resultSet->getHeader(
 				'host' );?></a>
 		<span class=desc><?= $resultSet->getHeader('description'); ?></span>
 	</li>
