4ac4e9bee120b4059a77351b1e956cdebb53b82b
liz <liz@wikia-inc.com>
2013-03-20 00:21:22 +0000

    VID-83 and VID-84 - updating video bootstrap so that all json params are optional

--- extensions/wikia/VideoHandlers/js/videoBootstrap.js
+++ extensions/wikia/VideoHandlers/js/videoBootstrap.js
@@ -4,85 +4,51 @@
 	function videoBootstrap(loader) {
 
 		function VideoBootstrap(element, json) {
-			
-//console.log(element);
-//console.log(json);
-console.log(json.scripts);
-			
+			var init = json.init,
+				html = json.html,
+				scripts = json.scripts,
+				jsParams = json.jsParams;
+
 			// insert html
-			element.innerHTML = json.html;
-			/*loader({
-				type: loader.JS,
-				resources: 'http://player.ooyala.com/v3/52bc289bedc847e3aa8eb2b347644f68'
-			})*/
-			
-			//loader('http://player.ooyala.com/v3/52bc289bedc847e3aa8eb2b347644f68')
-			
-			
-			/*loader({
-				type: loader.JS,
-				resources: {
-					scripts: json.scripts[1]
-				}
-				//resources: json.scripts[1]
-			})
-			.done(function(res) {
-console.log(res);
-				var script = res.scripts;
-console.log('done');
-console.log(script);
-				loader.processScript(script);
-
-				loader(json.scripts[1]).done(function(res) {
-
-					require([json.init], function(init) {
-						init(json.jsParams);
-					});
-					
-				});
-			});*/
-			// load the script
-			
-			
-			// Load all JS files with jQuery for now, while we sort out loader()
-			var dfd = [],
-				d = {},
-				i;
-			
-			for(i=0; i<json.scripts.length; i++) {
-				(function(i) {
-					d[i] = $.Deferred(); 
-
-					$.getScript(json.scripts[i]).done(function() {
-						d[i].resolve();
-					});
-					
-					dfd.push(d[i]);
-				})(i);
+			if(html) {
+				element.innerHTML = html;
 			}
 			
-			$.when(dfd).done(function() {
-				// hack for now - not sure why there's a race condition here but we're not going to stick with this code anyway, so I'm not going to worry about it for now (liz)
-				setTimeout(function() {
-					require([json.init], function(init) {
-						init(json.jsParams);
-					});							
+			// load the script (Load all JS files with jQuery for now, while we sort out loader())
+			if(scripts) {
+				var dfd = [],
+					d = {},
+					i;
 				
-				}, 1000)
-			});
-			
-			/*$.getScript(json.scripts[0]).done(function() {
-				$.getScript(json.scripts[1]).done(function() {
-					
-					require([json.init], function(init) {
-						init(json.jsParams);
-					});				
+				for(i=0; i<scripts.length; i++) {
+					(function(i) {
+						d[i] = $.Deferred(); 
+	
+						$.getScript(scripts[i]).done(function() {
+							d[i].resolve();
+						});
+						
+						dfd.push(d[i]);
+					})(i);
+				}
 				
+				$.when(dfd).done(function() {
+					// hack for now - not sure why there's a race condition here but we're not going to stick with this code anyway, so I'm not going to worry about it for now (liz)
+					setTimeout(runInit, 1000)
 				});
-			});*/
+			} else {
+				runInit();
+			}
 			
-			// execute the init function
-				
+			// execute the init function				
+			function runInit() {
+				if(init) {
+					require([init], function(init) {
+						init(jsParams);
+					});			
+				}
+			} 			
+			
 		}
 
 		return VideoBootstrap;
