1295b7db431c45492f70314c5793342246a66922
grunny <grunny@wikia-inc.com>
2013-03-22 03:57:51 +1000

    (BugID: 97023) Error on incorrect interwiki transclusions

--- languages/messages/wikia/MessagesQqq.php
+++ languages/messages/wikia/MessagesQqq.php
@@ -3,5 +3,6 @@
 $messages = array_merge( $messages, array(
 'copyrightpage' => 'w:Wikia:Licensing|Wikia:Licensing',
 'addnewtalksection-link' => 'Tooltip shown when hovering over the "addsection" tab (shown on talk pages).',
+'scarytranscludebadinterwiki' => 'Message displayed when user tries to use a non-transcludable interwiki for scary transclusion.',
 ) );
 
