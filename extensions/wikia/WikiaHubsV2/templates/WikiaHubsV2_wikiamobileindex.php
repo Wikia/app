ee9f3798e2cec5c6445b96809a63597a33330b5d
Damian Jozwiak <damian@wikia-inc.com>
2013-03-22 13:06:57 +0100

    bugid-99704 | Hubs V2 mobile - fixed wikias picks

--- extensions/wikia/WikiaHubsV2/templates/WikiaHubsV2_wikiamobileindex.php
+++ extensions/wikia/WikiaHubsV2/templates/WikiaHubsV2_wikiamobileindex.php
@@ -2,7 +2,8 @@
 	<?= $modules[MarketingToolboxModuleSliderService::MODULE_ID] ?>
 	<?= $modules[MarketingToolboxModuleExploreService::MODULE_ID] ?>
 	<?= $modules[MarketingToolboxModuleFeaturedvideoService::MODULE_ID] ?>
-	<?= $modules[MarketingToolboxModuleWikiaspicksService::MODULE_ID] ?>
+
+	<div class="wikias-picks-content"><?= $modules[MarketingToolboxModuleWikiaspicksService::MODULE_ID] ?></div>
 	<?= $modules[MarketingToolboxModuleFromthecommunityService::MODULE_ID] ?>
 	<?= $modules[MarketingToolboxModulePollsService::MODULE_ID] ?>
 </section>
