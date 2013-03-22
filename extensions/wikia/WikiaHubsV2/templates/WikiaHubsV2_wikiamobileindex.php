da51c957621c6654433396cd25bd9a1a126bbc46
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-03-22 14:59:43 +0000

    BugId-99705: Featured Video removed from hubs v2 mobile version

--- extensions/wikia/WikiaHubsV2/templates/WikiaHubsV2_wikiamobileindex.php
+++ extensions/wikia/WikiaHubsV2/templates/WikiaHubsV2_wikiamobileindex.php
@@ -1,9 +1,7 @@
 <section class="WikiaHubs">
 	<?= $modules[MarketingToolboxModuleSliderService::MODULE_ID] ?>
 	<?= $modules[MarketingToolboxModuleExploreService::MODULE_ID] ?>
-	<?= $modules[MarketingToolboxModuleFeaturedvideoService::MODULE_ID] ?>
-
-	<div class="wikias-picks-content"><?= $modules[MarketingToolboxModuleWikiaspicksService::MODULE_ID] ?></div>
+	<?= $modules[MarketingToolboxModuleWikiaspicksService::MODULE_ID] ?>
 	<?= $modules[MarketingToolboxModuleFromthecommunityService::MODULE_ID] ?>
 	<?= $modules[MarketingToolboxModulePollsService::MODULE_ID] ?>
 </section>
