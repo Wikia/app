a2b581d5d7782b5b3b02dcfa952152568825019f
nAndy <nandy@wikia-inc.com>
2013-03-25 17:29:05 +0000

    Revert "Revert "Merge branch 'DAR-36' into dev""
    This reverts commit 9780f9c1d116a43e43c9cb764b9199bc85039aa2.

--- extensions/wikia/RTE/js/RTE.js
+++ extensions/wikia/RTE/js/RTE.js
@@ -74,8 +74,7 @@
 				'rte-template,' +
 				'rte-temporary-save,' +
 				'rte-toolbar,' +
-				'rte-tools,' +
-				'rte-track',
+				'rte-tools',
 			// TODO: Too buggy. Try to use this after we update to 3.6.2 (BugId:23061)
 			//readOnly: true,
 			resize_enabled: false,
@@ -86,7 +85,6 @@
 		},
 
 		// Unique editor instance Id, set on modeswitch
-		// It is used mostly for paste tracking (detecting where pasted content came from)
 		// See RTE::getInstanceID() for details
 		instanceId: null,
 
