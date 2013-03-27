b0f3007d855c2c2255fc37bd7069ea7ec8f10de9
Marcin Maciejewski <marcin@wikia-inc.com>
2013-03-27 17:52:20 +0000

    bugid-100138 WAM Tabs: Templating&Css

--- extensions/wikia/WAMPage/templates/WAMPageController_index.php
+++ extensions/wikia/WAMPage/templates/WAMPageController_index.php
@@ -1,19 +1,9 @@
 <ul class="wam-tabs">
-	<li>
-		<a href="#">1</a>
-	</li>
-	<li>
-		<a href="#">2</a>
-	</li>
-	<li>
-		<a href="#">3</a>
-	</li>
-	<li>
-		<a href="#">4</a>
-	</li>
-	<li>
-		<a href="#">5</a>
-	</li>
+	<? foreach($tabs as $tab): ?>
+		<li>
+			<a href="<?= $tab['url'] ?>"><?= $tab['name'] ?></a>
+		</li>
+	<? endforeach; ?>
 </ul>
 <div class="wam-header">
 	<div class="wam-cards">
