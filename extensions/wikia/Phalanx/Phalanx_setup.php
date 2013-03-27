35bc5a546f2ed3148a62e574ea01e7d43e194be7
moliwikia <moli@wikia-inc.com>
2013-03-27 14:53:05 +0000

    replace Phalanx class with hook (BAC-134)

--- extensions/wikia/Phalanx/Phalanx_setup.php
+++ extensions/wikia/Phalanx/Phalanx_setup.php
@@ -103,6 +103,9 @@ function efPhalanxInit() {
 	// fb#5311 - prevent a phalanx blocked name from being created.
 	$wgHooks['AbortNewAccount'][] = 'UserBlock::onAbortNewAccount';
 	$wgHooks['cxValidateUserName'][] = 'UserBlock::onValidateUserName';
+	
+	// new PhalanxII hooks
+	$wgHooks['CheckContent'][] = 'ContentBlock::onCheckContent';
 }
 
 
