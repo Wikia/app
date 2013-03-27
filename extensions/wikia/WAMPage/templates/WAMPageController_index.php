c4a4fd213d04d212deb288918798683a5532b6a0
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-03-27 14:33:57 +0000

    BugId-100135: WAM visualization templating & CSS

--- extensions/wikia/WAMPage/templates/WAMPageController_index.php
+++ extensions/wikia/WAMPage/templates/WAMPageController_index.php
@@ -1,28 +1,60 @@
 <div class="wam-header">
-	<h2><?= wfMessage('wampage-header')->text(); ?></h2>
+	<ul class="wam-tabs">
+		<li>
+			<a href="#">1</a>
+		</li>
+		<li>
+			<a href="#">2</a>
+		</li>
+		<li>
+			<a href="#">3</a>
+		</li>
+		<li>
+			<a href="#">4</a>
+		</li>
+		<li>
+			<a href="#">5</a>
+		</li>
+		<li>
+			<a href="#">6</a>
+		</li>
+	</ul>
+
+	<div class="wam-cards">
+		<a href="#" class="wam-card">
+			<figure class="wam-img">
+				<span class="wam-img-info">Lorem ipsum</span>
+			</figure>
+			<div class="wam-score">4.18</div>
+			<span class="wam-vertical">Lifestyle</span>
+		</a>
+        <a href="#" class="wam-card">
+            <figure class="wam-img">
+                <span class="wam-img-info">Lorem ipsum</span>
+            </figure>
+            <div class="wam-score">4.18</div>
+            <span class="wam-vertical">Lifestyle</span>
+        </a>
+    	<a href="#" class="wam-card">
+            <figure class="wam-img">
+                <span class="wam-img-info">Lorem ipsum</span>
+            </figure>
+            <div class="wam-score">4.18</div>
+            <span class="wam-vertical">Lifestyle</span>
+    	</a>
+        <a href="#" class="wam-card">
+            <figure class="wam-img">
+                <span class="wam-img-info">Lorem ipsum</span>
+            </figure>
+            <div class="wam-score">4.18</div>
+            <span class="wam-vertical">Lifestyle</span>
+        </a>
+	</div>
+	
+    <h2><?= wfMessage('wampage-header')->text(); ?></h2>
 </div>
 
-<ul class="wam-tabs">
-	<li>
-		<a href="#">1</a>
-	</li>
-	<li>
-		<a href="#">2</a>
-	</li>
-	<li>
-		<a href="#">3</a>
-	</li>
-	<li>
-		<a href="#">4</a>
-	</li>
-	<li>
-		<a href="#">5</a>
-	</li>
-	<li>
-		<a href="#">6</a>
-	</li>
-</ul>
-
+<div class="wam-progressbar"></div>
 <div class="wam-content">
 	<?= wfMessage(
 		'wampage-content',
