95a214c370af1f80e9db5e06545c698cf1c459a5
Piotr Gabryjeluk <rychu@wikia-inc.com>
2013-03-29 13:12:37 +0100

    ADEN-7 Removing last parts of AdEngine.php
    Conflicts:
    	includes/wikia/GlobalFunctions.php

--- extensions/wikia/AdEngine/AdEngine2Controller.class.php
+++ extensions/wikia/AdEngine/AdEngine2Controller.class.php
@@ -242,6 +242,115 @@ class AdEngine2Controller extends WikiaController {
 	 */
 
 	/**
+	 * Register global JS variables bottom (migrated from wfAdEngineSetupJSVars)
+	 *
+	 * @param array $vars
+	 * @param array $scripts
+	 *
+	 * @return bool
+	 */
+	public function onMakeGlobalVariablesScript(array &$vars) {
+		wfProfileIn(__METHOD__);
+
+		global $wgRequest, $wgNoExternals, $wgEnableAdsInContent, $wgEnableOpenXSPC,
+			   $wgAdDriverCookieLifetime, $wgHighValueCountries,
+			   $wgUser, $wgEnableWikiAnswers, $wgAdDriverUseCookie, $wgAdDriverUseExpiryStorage,
+			   $wgEnableAdMeldAPIClient, $wgEnableAdMeldAPIClientPixels,
+			   $wgLoadAdDriverOnLiftiumInit, $wgOutboundScreenRedirectDelay,
+			   $wgEnableOutboundScreenExt;
+
+		$wgNoExternals = $wgRequest->getBool('noexternals', $wgNoExternals);
+
+		if (!empty($wgNoExternals)) {
+			$vars["wgNoExternals"] = $wgNoExternals;
+		}
+		if (!empty($wgEnableAdsInContent)) {
+			$vars["wgEnableAdsInContent"] = $wgEnableAdsInContent;
+		}
+		if (!empty($wgEnableAdMeldAPIClient)) {
+			$vars["wgEnableAdMeldAPIClient"] = $wgEnableAdMeldAPIClient;
+		}
+		if (!empty($wgEnableAdMeldAPIClientPixels)) {
+			$vars["wgEnableAdMeldAPIClientPixels"] = $wgEnableAdMeldAPIClientPixels;
+		}
+
+		// OpenX SPC (init in AdProviderOpenX.js)
+		if (!empty($wgEnableOpenXSPC)) {
+			$vars["wgEnableOpenXSPC"] = $wgEnableOpenXSPC;
+		}
+
+		// AdDriver
+		$vars['wgAdDriverCookieLifetime'] = $wgAdDriverCookieLifetime;
+		$highValueCountries = WikiFactory::getVarValueByName('wgHighValueCountries', 177);	// community central
+		if (empty($highValueCountries)) {
+			$highValueCountries = $wgHighValueCountries;
+		}
+		$vars['wgHighValueCountries'] = $highValueCountries;
+
+		if (!empty($wgAdDriverUseExpiryStorage)) {
+			$vars["wgAdDriverUseExpiryStorage"] = $wgAdDriverUseExpiryStorage;
+		}
+		if (!empty($wgAdDriverUseCookie)) {
+			$vars["wgAdDriverUseCookie"] = $wgAdDriverUseCookie;
+		}
+		if (!empty($wgLoadAdDriverOnLiftiumInit)) {
+			$vars['wgLoadAdDriverOnLiftiumInit'] = $wgLoadAdDriverOnLiftiumInit;
+		}
+
+		if ($wgUser->getOption('showAds')) {
+			$vars['wgUserShowAds'] = true;
+		}
+
+		// Answers sites
+		if (!empty($wgEnableWikiAnswers)) {
+			$vars['wgEnableWikiAnswers'] = $wgEnableWikiAnswers;
+		}
+
+		if (!empty($wgOutboundScreenRedirectDelay)) {
+			$vars['wgOutboundScreenRedirectDelay'] = $wgOutboundScreenRedirectDelay;
+		}
+		if (!empty($wgEnableOutboundScreenExt)) {
+			$vars['wgEnableOutboundScreenExt'] = $wgEnableOutboundScreenExt;
+		}
+
+		wfProfileOut(__METHOD__);
+		return true;
+	}
+
+	/**
+	 * Register ad-related vars on top (migrated from wfAdEngineSetupTopVars)
+	 *
+	 * @param array $vars
+	 * @param array $scripts
+	 *
+	 * @return bool
+	 */
+	public function onWikiaSkinTopScriptsLegacy(&$vars, &$scripts) {
+		global $wgCityId, $wgEnableKruxTargeting, $wgNoExternals;
+
+		wfProfileIn(__METHOD__);
+
+		// generic type of page: forum/search/article/home/...
+		$vars['wikiaPageType'] = WikiaPageType::getPageType();
+		$vars['wikiaPageIsHub'] = WikiaPageType::isWikiaHub();
+
+		// category/hub
+		$catInfo = HubService::getComscoreCategory($wgCityId);
+		$vars['cscoreCat'] = $catInfo->cat_name;
+
+		// Krux
+		$cat = AdEngine2Controller::getCachedCategory();
+		if (!empty($wgEnableKruxTargeting) && empty($wgNoExternals)) {
+			$vars['wgEnableKruxTargeting'] = $wgEnableKruxTargeting;
+			$vars['wgKruxCategoryId'] = WikiFactoryHub::getInstance()->getKruxId($cat['id']);
+		}
+
+		wfProfileOut(__METHOD__);
+
+		return true;
+	}
+
+	/**
 	 * Register ad-related vars on top
 	 *
 	 * @param array $vars
