42201f516ce6219d1e3e3cb11424779964441da4
Robert Elwell <robert@wikia-inc.com>
2013-04-01 18:35:48 +0000

    update Wikia\Search\Result to allow language-agnostic access of fields, direct control of toArray by requested keys

--- extensions/wikia/Search/classes/Result.php
+++ extensions/wikia/Search/classes/Result.php
@@ -206,13 +206,31 @@ class Result extends ReadWrite {
 	 */
 	public function toArray( $keys ) {
 		$array = array();
-		foreach ( $this->_fields as $key => $value )
-		{
-			if( in_array( $key, $keys ) ) {
-				$array[$key] = $value;
-			}
+		foreach ( $keys as $key ) {
+			$array[$key] = $this[$key];
 		}
 		return $array;
+	}
+	
+	/**
+	 * Allows us to overload parent offsetGet with getTitle(), getText(), etc.
+	 * This is good when using $result->toArray()
+	 * @see Solarium_Document_ReadOnly::offsetGet()
+	 */
+	public function offsetGet( $key ) {
+		$value = null;
+		$nolangKey = array_shift( explode( '_', $key ) );
+		switch ( $nolangKey ) {
+		    case 'title':
+		    	$value = $this->getTitle(); break;
+		    case 'text':
+		    	$value = $this->getText(); break;
+		    case 'videoViews':
+		    	$value = $this->getVideoViews(); break;
+		    default:
+		    	$value = parent::offsetGet( Utilities::field( $nolangKey ) );
+		}
+		return $value;
 	}
 	
 	/**
