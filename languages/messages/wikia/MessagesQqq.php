78b9c8c0606a13dab2e452f6d5428afdc44e1de1
grunny <grunny@wikia-inc.com>
2013-04-03 03:27:41 +1000

    (BugID: 99863) Monobook: Fix missing message
    Monobook: <shared-News_box> sidebar message missing on non-English wikis

--- languages/messages/wikia/MessagesQqq.php
+++ languages/messages/wikia/MessagesQqq.php
@@ -4,5 +4,6 @@ $messages = array_merge( $messages, array(
 'copyrightpage' => 'w:Wikia:Licensing|Wikia:Licensing',
 'addnewtalksection-link' => 'Tooltip shown when hovering over the "addsection" tab (shown on talk pages).',
 'scarytranscludebadinterwiki' => 'Message displayed when user tries to use a non-transcludable interwiki for scary transclusion.',
+'shared-News_box' => 'Message displayed in Wikia portlet in Monobook',
 ) );
 
