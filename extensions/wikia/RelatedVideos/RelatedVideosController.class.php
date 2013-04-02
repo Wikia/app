06e84ed05146f0d2d09424331560073a975af47e
Garth Webb <gart@wikia-inc.com>
2013-04-01 17:59:49 -0700

    VID-142 Do rights the right way and make sure Helpers can delete related videos too.

--- extensions/wikia/RelatedVideos/RelatedVideosController.class.php
+++ extensions/wikia/RelatedVideos/RelatedVideosController.class.php
@@ -183,8 +183,7 @@ class RelatedVideosController extends WikiaController {
 
 			$video['viewsMsg'] = wfMsg('related-videos-video-views', $this->wg->ContLang->formatNum($video['views']));
 
-			$userGroups = $this->wg->User->getEffectiveGroups();
-			$isAdmin = in_array('sysop', $userGroups) || in_array('staff', $userGroups);
+			$isAdmin = $this->wg->User->isAllowed( 'relatedvideosdelete' );
 
 			$this->removeTooltip = $this->wf->Message( 'related-videos-tooltip-remove' );
 			$this->videoThumb = $videoThumb;
