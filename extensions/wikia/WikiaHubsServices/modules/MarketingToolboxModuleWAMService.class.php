81a200c38529b03273a925d8a222b9ee7238655c
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-04-05 10:38:57 +0000

    BugId-100483: Fixed blank WAM on hubs (removed timestamp from WAM services)

--- extensions/wikia/WikiaHubsServices/modules/MarketingToolboxModuleWAMService.class.php
+++ extensions/wikia/WikiaHubsServices/modules/MarketingToolboxModuleWAMService.class.php
@@ -20,14 +20,6 @@ class MarketingToolboxModuleWAMService extends MarketingToolboxModuleNonEditable
 	protected function prepareParameters($params) {
 		$params['limit'] = $this->getModel()->getWamLimitForHubPage();
 
-		if( empty($params['ts']) ) {
-			$params['ts'] = strtotime('00:00 -1 day');
-		}
-
-		if( empty($params['ts_previous_day']) ) {
-			$params['ts_previous_day'] = strtotime('-1 day', $params['ts']);
-		}
-
 		if( empty($params['image_height']) ) {
 			$params['image_height'] = $this->getModel()->getImageHeight();
 		}
