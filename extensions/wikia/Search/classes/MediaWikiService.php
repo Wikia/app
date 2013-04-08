46c4285d71a596fd019f7c3481e2b62ea920fc84
Robert Elwell <robert@wikia-inc.com>
2013-04-08 19:22:25 +0000

    PLA-107 -- added capability to get main page id for a given wiki to mediawikiservice

--- extensions/wikia/Search/classes/MediaWikiService.php
+++ extensions/wikia/Search/classes/MediaWikiService.php
@@ -494,6 +494,15 @@ class MediaWikiService
 	}
 	
 	/**
+	 * Returns the article ID of a main page for the wiki ID passed.
+	 * @param int $wikiId
+	 * @return Ambigous <number, boolean>
+	 */
+	public function getMainPageIdForWikiId( $wikiId ) {
+		return $this->getMainPageTitleForWikiId( $wikiId )->getArticleId();
+	}
+	
+	/**
 	 * Returns text from the main page of a provided wiki.
 	 * @param int $wikiId
 	 * @return string
