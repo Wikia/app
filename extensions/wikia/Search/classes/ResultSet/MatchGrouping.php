5d3836dcd5ce7fecc370bbc45886164e777f0727
Robert Elwell <robert@wikia-inc.com>
2013-04-08 20:59:43 +0000

    PLA-107 added getTopPages method to resultset -- no unit test yet because i need to ascertain it is correct

--- extensions/wikia/Search/classes/ResultSet/MatchGrouping.php
+++ extensions/wikia/Search/classes/ResultSet/MatchGrouping.php
@@ -5,6 +5,7 @@
  *
  */
 namespace Wikia\Search\ResultSet;
+use DataMartService;
 /**
  * This class is used to create a "grouping" based on a wiki match.
  * @author relwell
@@ -12,6 +13,12 @@ namespace Wikia\Search\ResultSet;
  * @subpackage ResultSet
  */
 class MatchGrouping extends Grouping {
+	
+	/**
+	 * Stores top pages.
+	 * @var array
+	 */
+	protected $topPages = [];
 	
 	/**
 	 * Uses DependencyContainer to pre-populate attributes, and then configures stuff.
@@ -28,6 +35,28 @@ class MatchGrouping extends Grouping {
 		$this->results            = new \ArrayIterator( array( $result ) );
 		
 		$this->addHeaders( $result->getFields() );
+	}
+	
+	/**
+	 * Returns the top four articles for this wiki
+	 * @return array of Articles
+	 */
+	public function getTopPages() {
+		if ( empty( $this->topPages ) ) {
+			$wikiId = $this->getHeader( 'wid' );
+			$articles = (new DataMartService)->getTopArticlesByPageview(
+					$wikiId,
+					null,
+					null,
+					false,
+					5 //compensation for Main Page
+					);
+			$mainId = $this->service->getMainPageIdForWikiId( $wikiId );
+			$counter = 0;
+			unset( $articles[$mainId] );
+			$this->topPages = array_slice( array_values( $articles ), 0, 4 );
+		}
+		return $this->topPages;
 	}
 	
 }
