Wikia/app

commit d462dbb54fb18a803759fb5fb248ac9ec19aec9e

WikiaDartMobileHelper rewrite

extensions/wikia/WikiaMobile/js/ads.js
@@ -4,6 +4,7 @@
  *
  * @define ads
  * @require events
+ * @require dartmobilehelper
  * @require domwriter
  * @require cookies
  * @require track
@@ -14,7 +15,7 @@
  */
 
 /*global window, document, define, require, setTimeout, setInterval, clearInterval, Features, AdConfig*/
-define('ads', ['domwriter', 'wikia.cookies', 'track', 'wikia.log', 'wikia.window', 'wikia.utils'], function (dw, ck, track, log, w, $) {
+define('ads', ['domwriter', 'wikia.cookies', 'track', 'wikia.log', 'wikia.window', 'wikia.utils', 'wikia.dartmobilehelper'], function (dw, ck, track, log, w, $, dartHelper) {
 	'use strict';
 
 	var AD_TYPES = {
@@ -35,9 +36,7 @@ define('ads', ['domwriter', 'wikia.cookies', 'track', 'wikia.log', 'wikia.window
 		fixed = false,
 		ftr,
 		inited,
-		positionfixed = w.Features.positionfixed,
-		type,
-		dartHelper = w.WikiaDartMobileHelper(log, w, d);
+		type;
 
 	/**
 	 * @private
@@ -105,7 +104,7 @@ define('ads', ['domwriter', 'wikia.cookies', 'track', 'wikia.log', 'wikia.window
 		if (shouldRequestAd()) {
 			var url = dartHelper.getMobileUrl({
 					slotname: name,
-					positionfixed: (Features.positionfixed ? 'css' : 'js'),
+					size: '5x5',
 					uniqueId: getUniqueId()
 				}),
 				s = d.createElement('script');

extensions/wikia/AdEngine/js/AdLogicPageLevelParams.js
@@ -170,3 +170,10 @@ var AdLogicPageLevelParams = function (
 		getPageLevelParams: getPageLevelParams
 	};
 };
+
+(function (context) {
+	'use strict';
+	if (context.define && context.define.amd) {
+		context.define('wikia.adlogicpageparams', ['wikia.log', 'wikia.window'], AdLogicPageLevelParams);
+	}
+}(this));

extensions/wikia/AdEngine/js/DartUrl.js
@@ -109,3 +109,10 @@ var DartUrl = function () {
 		urlBuilder: urlBuilder
 	};
 };
+
+(function (context) {
+	'use strict';
+	if (context.define && context.define.amd) {
+		context.define('wikia.darturl', DartUrl);
+	}
+}(this));

extensions/wikia/AdEngine/js/WikiaDartHelper.js
@@ -17,9 +17,9 @@ var WikiaDartHelper = function (log, adLogicPageLevelParams, dartUrl) {
 	 *     slotname
 	 *     slotsize
 	 *   OPTIONAL:
-	 *     adType (default: adj, adi, jwplayer, mobile)
+	 *     adType (default: adj, adi, jwplayer)
 	 *     src (default: driver)
-	 *     loc, dcopt, tile, positionfixed
+	 *     loc, dcopt, tile
 	 * }
 	 * @return {String} URL of DART script
 	 */
@@ -27,7 +27,6 @@ var WikiaDartHelper = function (log, adLogicPageLevelParams, dartUrl) {
 		var slotname = params.slotname,
 			size = params.slotsize,
 			adType = params.adType || 'adj',
-			pathPrefix,
 			src = params.src || 'driver',
 			localTile,
 			localOrd = params.ord || ord,
@@ -41,12 +40,6 @@ var WikiaDartHelper = function (log, adLogicPageLevelParams, dartUrl) {
 			adType = 'pfadx';
 		}
 
-		if (adType === 'mobile') {
-			pathPrefix = 'DARTProxy/mobile.handler?k=';
-		}
-
-		pathPrefix = pathPrefix || adType + '/';
-
 		if (params.tile) {
 			localTile = params.tile;
 		} else {
@@ -58,7 +51,7 @@ var WikiaDartHelper = function (log, adLogicPageLevelParams, dartUrl) {
 
 		url = dartUrl.urlBuilder(
 			subdomain + '.doubleclick.net',
-			pathPrefix + 'wka.' + pageParams.s0 + '/' + pageParams.s1 + '/' + pageParams.s2
+			adType + '/' + 'wka.' + pageParams.s0 + '/' + pageParams.s1 + '/' + pageParams.s2
 		);
 
 		// per page params
@@ -76,7 +69,6 @@ var WikiaDartHelper = function (log, adLogicPageLevelParams, dartUrl) {
 				}
 			}
 		}
-		url.addParam('positionfixed', params.positionfixed);
 
 		// global params
 		url.addParam('src', src);
@@ -91,9 +83,7 @@ var WikiaDartHelper = function (log, adLogicPageLevelParams, dartUrl) {
 
 		// sync params
 		url.addParam('tile', localTile);
-		if (!params.omitEndTag) {
-			url.addString('endtag=$;');
-		}
+		url.addString('endtag=$;');
 		url.addString('ord=' + localOrd + '?');
 
 		log(url.toString(), 5, logGroup);

extensions/wikia/AdEngine/js/WikiaDartMobileHelper.js
@@ -1,29 +1,79 @@
-// Relies on AdLogicPageLevelParams, WikiaDartHelper and DartUrl
+// Relies on AdLogicPageLevelParams, DartUrl
 
-var WikiaDartMobileHelper = function (log, window, document) {
+var WikiaDartMobileHelper = function (log, window, adLogicPageLevelParams, dartUrl) {
 	'use strict';
 
-	var dartUrl = DartUrl(),
-		adLogicPageLevelParams = AdLogicPageLevelParams(log, window),
-		wikiaDartHelper = WikiaDartHelper(log, adLogicPageLevelParams, dartUrl);
+	var logGroup = 'WikiaDartMobileHelper',
+		features = window.Features, // TODO: AMD
+		ord = Math.round(Math.random() * 23456787654),
+		tile = 0,
+		categoryStrMaxLength = 300;
+
+	function getMobileUrl(params) {
+		var slotname = params.slotname,
+			pageParams = adLogicPageLevelParams.getPageLevelParams(),
+			url = dartUrl.urlBuilder(
+				'ad.mo.doubleclick.net',
+				'DARTProxy/mobile.handler?k=wka.' + pageParams.s0 + '/' + pageParams.s1 + '/' + pageParams.s2
+			),
+			name,
+			value;
+
+		log(['getMobileUrl', slotname], 5, logGroup);
+
+		tile += 1;
+
+		// per page params
+		for (name in pageParams) {
+			if (pageParams.hasOwnProperty(name)) {
+				value = pageParams[name];
+				if (value) {
+					if (name === 'cat') {
+						url.addParam(name, value, categoryStrMaxLength);
+					} else {
+						url.addParam(name, value);
+					}
+				}
+			}
+		}
+
+		// global params
+		url.addParam('positionfixed', features.positionfixed ? 'css' : 'js');
+		url.addParam('src', 'mobile');
+		url.addString('mtfIFPath=/extensions/wikia/AdEngine/;');
+		url.addParam('mtfInline', 'true');	// http://www.google.com/support/richmedia/bin/answer.py?hl=en&answer=182220
+
+		// per slot params
+		url.addParam('pos', params.slotname);
+		url.addParam('loc', params.loc);
+		url.addParam('dcopt', params.dcopt);
+		url.addParam('sz', params.size);
+
+		// sync params
+		url.addParam('tile', tile);
+		url.addString('ord=' + ord + '?');
+
+		url = url.toString() + '&csit=1&dw=1&u=' + params.uniqueId;
+
+		log(['getMobileUrl', url], 5, logGroup);
+
+		return url;
+	}
 
 	return {
 		/**
 		 * Get URL for a mobile ad
 		 *
-		 * @param params (slotname, positionfixed, uniqueId)
+		 * @param params (slotname, size, uniqueId)
 		 * @return {String} URL of DART script
 		 */
-		getMobileUrl: function (params) {
-			return wikiaDartHelper.getUrl({
-				slotname: params.slotname,
-				positionfixed: params.positionfixed,
-				subdomain: 'ad.mo',
-				adType: 'mobile',
-				src: 'mobile',
-				slotsize: '5x5',
-				omitEndTag: true
-			}) + '&csit=1&dw=1&u=' + params.uniqueId;
-		}
+		getMobileUrl: getMobileUrl
 	};
 };
+
+(function (context) {
+	'use strict';
+	if (context.define && context.define.amd) {
+		context.define('wikia.dartmobilehelper', ['wikia.log', 'wikia.window', 'wikia.adlogicpageparams', 'wikia.darturl'], WikiaDartMobileHelper);
+	}
+}(this));

extensions/wikia/AdEngine/js/spec/WikiaDartMobileHelper.spec.js
@@ -1,14 +1,18 @@
+// TODO: proper UNIT tests for this
+
 describe('WikiaDartMobileHelper', function(){
-	it('getsMobileUrl', function() {
-		var logMock = function() {},
+	it('getMobileUrl', function () {
+		var logMock = function () {},
 			windowMock = {
 				location: {hostname: 'example.org'},
 				cityShort: 'vertical',
 				wgDBname: 'dbname',
-				wgContentLanguage: 'xx'
+				wgContentLanguage: 'xx',
+				Features: {positionfixed: true}
 			},
-			documentMock = {documentElement: {}, body: {}},
-			dartHelper = WikiaDartMobileHelper(logMock, windowMock, documentMock),
+			adLogicPageLevelParams = AdLogicPageLevelParams(logMock, windowMock),
+			dartUrl = DartUrl(),
+			dartHelper = WikiaDartMobileHelper(logMock, windowMock, adLogicPageLevelParams, dartUrl),
 			expected = 'http://ad.mo.doubleclick.net/DARTProxy/mobile.handler?k=wka.vertical/_dbname/article;' +
 				's0=vertical;s1=_dbname;s2=article;' +
 				'dmn=exampleorg;' +
@@ -27,7 +31,7 @@ describe('WikiaDartMobileHelper', function(){
 		expect(
 			dartHelper.getMobileUrl({
 				slotname: 'SLOTNAME_MOBILE',
-				positionfixed: 'css',
+				size: '5x5',
 				uniqueId: 'someuniqid'
 			}).replace(/ord=[0-9]+/, 'ord=XXX')
 		).toBe(expected);

extensions/wikia/AssetsManager/config.php
@@ -525,7 +525,6 @@ $config['wikiamobile_js_ads'] = array(
 		//advertisement "core"
 		'//extensions/wikia/AdEngine/js/AdLogicPageLevelParams.js',
 		'//extensions/wikia/AdEngine/js/DartUrl.js',
-		'//extensions/wikia/AdEngine/js/WikiaDartHelper.js',
 		'//extensions/wikia/AdEngine/js/WikiaDartMobileHelper.js',
 
 		//modules

extensions/wikia/WikiaMobile/js/spec/ads.spec.js
@@ -17,10 +17,8 @@ describe("Ads module", function () {
 			this.parentNode = parentNode;
 			this.className = '';
 		},
+		dartHelper = function(){},
 		window = {
-			WikiaDartMobileHelper: function(){
-				return function(){};
-			},
 			document: {
 				getElementById: function(name){
 					return elements[name] || (elements[name] = new Element());
@@ -31,7 +29,7 @@ describe("Ads module", function () {
 		utils = function(func){
 			func();
 		},
-		ads = modules.ads(domwriter, cookies, track, log, window, utils);
+		ads = modules.ads(domwriter, cookies, track, log, window, utils, dartHelper);
 
 	it("is defined as a module", function () {
 		expect(ads).toBeDefined();

commit 878f5a76d635c66268182408fd8f7d3f1751a914

BugId:100768 - fix mobile footer ad
This fixes a regression introduced by
https://github.com/Wikia/app/commit/d462dbb54fb18a803759fb5fb248ac9ec19aec9e#L6L38

That was generating a JS fatal that stopped the footer logic from
completing and causing the ad not to float.

extensions/wikia/WikiaMobile/js/ads.js
@@ -34,6 +34,7 @@ define('ads', ['domwriter', 'wikia.cookies', 'track', 'wikia.log', 'wikia.window
 		d = w.document,
 		found = false,
 		fixed = false,
+		positionfixed = Features.positionfixed,
 		ftr,
 		inited,
 		type;