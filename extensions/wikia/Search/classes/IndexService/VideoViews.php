e87faed07b6efe4067374c1ca8c2f55b4aedc2e5
Robert Elwell <robert@wikia-inc.com>
2013-04-10 18:44:02 +0000

    implement separate video views service

---
+++ extensions/wikia/Search/classes/IndexService/VideoViews.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Class definition for Wikia\Search\IndexService\VideoViews
+ */
+namespace Wikia\Search\IndexService;
+/**
+ * This class is intended to serve as a single entry point for updating
+ * @author relwell
+ * @package Search
+ * @subpackage IndexService 
+ */
+class VideoViews extends AbstractService
+{
+	/**
+	 * For a video file, overwrites the 'views' field with the video views, extracted by a different service.
+	 * (non-PHPdoc)
+	 * @see \Wikia\Search\IndexService\AbstractService::execute()
+	 * @return array
+	 */
+	public function execute() {
+		$service = $this->getService();
+		$results = [];
+		if ( $service->pageIdIsVideoFile( $this->currentPageId ) ) {
+			$results['views'] = $service->getVideoViewsForPageId( $this->currentPageId );
+		}
+		return $results;
+	}
+}
