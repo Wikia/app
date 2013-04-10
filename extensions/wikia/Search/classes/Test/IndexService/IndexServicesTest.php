49adfa7d03221276be5e0c12941b486cf1565226
Robert Elwell <robert@wikia-inc.com>
2013-04-10 18:44:02 +0000

    implement separate video views service

--- extensions/wikia/Search/classes/Test/IndexService/IndexServicesTest.php
+++ extensions/wikia/Search/classes/Test/IndexService/IndexServicesTest.php
@@ -74,7 +74,7 @@ class IndexServicesTest extends BaseTest
 	/**
 	 * @covers Wikia\Search\IndexService\Metadata::execute
 	 */
-    public function testMetadataExecuteInternalNoPageId() {
+	public function testMetadataExecuteInternalNoPageId() {
 		$service = $this->getMockBuilder( '\Wikia\Search\IndexService\Metadata' )
 		                ->disableOriginalConstructor()
 		                ->setMethods( null )
@@ -100,7 +100,7 @@ class IndexServicesTest extends BaseTest
 	/**
 	 * @covers Wikia\Search\IndexService\Metadata::execute
 	 */
-    public function testMetadataExecuteInternalSuccess() {
+	 public function testMetadataExecuteInternalSuccess() {
 		$service = $this->getMockBuilder( '\Wikia\Search\IndexService\Metadata' )
 		                ->disableOriginalConstructor()
 		                ->setMethods( null )
@@ -132,34 +132,74 @@ class IndexServicesTest extends BaseTest
 				$expected,
 				$service->execute()
 		);
-    }
-    
-    /**
-     * @covers \Wikia\Search\IndexService\Redirects::execute
-     */
-    public function testRedirectsService() {
-    	$mwservice = $this->service->setMethods( array( 'getGlobal', 'getRedirectTitlesForPageId' ) )->getMock();
-    	$mwservice
-    	    ->expects( $this->at( 0 ) )
-    	    ->method ( 'getGlobal' )
-    	    ->with   ( 'AppStripsHtml' )
-    	    ->will   ( $this->returnValue( true ) )
-    	;
-    	$mwservice
-    	    ->expects( $this->at( 1 ) )
-    	    ->method ( 'getRedirectTitlesForPageId' )
-    	    ->with   ( $this->pageId )
-    	    ->will   ( $this->returnValue( array( 'foo', 'bar' ) ) )
-    	;
-    	$service = $this->getMockBuilder( '\Wikia\Search\IndexService\Redirects' )
-    	                ->disableOriginalConstructor()
-    	                ->setMethods( null )
-    	                ->getMock();
-    	$this->injectService( $service, $mwservice );
-    	$service->setPageId( $this->pageId );
-    	$this->assertEquals(
-    			array( \Wikia\Search\Utilities::field( 'redirect_titles' ) => array( 'foo', 'bar' ) ),
-    			$service->execute()
+	}
+	/**
+	 * @covers \Wikia\Search\IndexService\Redirects::execute
+	 */
+	public function testRedirectsService() {
+		$mwservice = $this->service->setMethods( array( 'getGlobal', 'getRedirectTitlesForPageId' ) )->getMock();
+		$mwservice
+		    ->expects( $this->at( 0 ) )
+		        ->method ( 'getGlobal' )
+		        ->with   ( 'AppStripsHtml' )
+		        ->will   ( $this->returnValue( true ) )
+		;
+		$mwservice
+		    ->expects( $this->at( 1 ) )
+		    ->method ( 'getRedirectTitlesForPageId' )
+		    ->with   ( $this->pageId )
+		    ->will   ( $this->returnValue( array( 'foo', 'bar' ) ) )
+		;
+		$service = $this->getMockBuilder( '\Wikia\Search\IndexService\Redirects' )
+		                ->disableOriginalConstructor()
+		                ->setMethods( null )
+		                ->getMock();
+		$this->injectService( $service, $mwservice );
+		$service->setPageId( $this->pageId );
+		$this->assertEquals(
+				array( \Wikia\Search\Utilities::field( 'redirect_titles' ) => array( 'foo', 'bar' ) ),
+				$service->execute()
 		);
-    }
+	}
+	
+	/**
+	 * @covers Wikia\Search\IndexService\VideoViews::execute
+	 */
+	public function testVideoViewsService() {
+		$mwService = $this->service->setMethods( array( 'pageIdIsVideoFile', 'getVideoViewsForPageId' ) )->getMock();
+		$service = $this->getMockBuilder( 'Wikia\Search\IndexService\VideoViews' )
+		                ->setMethods( [ 'getService' ] )
+		                ->getMock();
+		$service
+		    ->expects( $this->any() )
+		    ->method ( 'getService' )
+		    ->will   ( $this->returnValue( $mwService ) )
+		;
+		$mwService
+		    ->expects( $this->at( 0 ) )
+		    ->method ( 'pageIdIsVideoFile' )
+		    ->with   ( $this->pageId )
+		    ->will   ( $this->returnValue( true ) )
+		;
+		$mwService
+		    ->expects( $this->at( 1 ) )
+		    ->method ( 'getVideoViewsForPageId' )
+		    ->with   ( $this->pageId )
+		    ->will   ( $this->returnValue( 1234 ) )
+		;
+		$service->setPageId( $this->pageId );
+		$this->assertEquals(
+				[ 'views' => 1234 ],
+				$service->execute()
+		);
+		$mwService
+		    ->expects( $this->at( 0 ) )
+		    ->method ( 'pageIdIsVideoFile' )
+		    ->with   ( $this->pageId )
+		    ->will   ( $this->returnValue( false ) )
+		;
+		$this->assertEmpty(
+				$service->execute()
+		);
+	}
 }
