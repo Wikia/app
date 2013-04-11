2066800899db93d9d289051182e3a3c0a8749872
Michał 'Mix' Roszka <mix@wikia-inc.com>
2013-04-11 11:25:55 +0200

    BugId:51338 HAWelcome Minor fix.

--- extensions/wikia/HAWelcome/HAWelcome.php
+++ extensions/wikia/HAWelcome/HAWelcome.php
@@ -191,11 +191,11 @@ class HAWelcomeJob extends Job {
             // does not contain the associated Title object. It has to be
             // recreated based on the associated Page object.
             if ( !$oTitle ) {
+				$oTitle = Title::newFromId( $oRevision->getPage(), Title::GAID_FOR_UPDATE );
                 trigger_error( sprintf(
                     '%s Recreated Title for page %d, revision %d, URL %s',
                     __METHOD__, $oRevision->getPage(), $oRevision->getId(), $oTitle->getFullURL()
                 ), E_USER_WARNING );
-                $oTitle = Title::newFromId( $oRevision->getPage(), Title::GAID_FOR_UPDATE );
             }
             /** @type Array Parameters for the job */
             $aParams = array(
