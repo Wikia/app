8d40c2eeda4a9acb3f72c6926b5251791ec5222b
Adam Robak <adamr@wikia-inc.com>
2013-04-12 18:14:34 +0200

    PLA-111 examples for using select, insert, update queries for webmaster_sitemaps table, send alert email example

--- maintenance/wikia/webmaster_sitemap_uploader.php
+++ maintenance/wikia/webmaster_sitemap_uploader.php
@@ -1,10 +1,67 @@
 <?php
 
-require_once( __DIR__.'../commandLine.inc' );
+require_once( __DIR__.'/../commandLine.inc' );
+
+//UserMailer::send(
+//	new MailAddress( 'adamr@wikia-inc.com' ),
+//	new MailAddress( 'testadam@wikia-inc.com' ),
+//	'alert test',
+//	'testing mail alert sending'
+//);
 
 global $wgExternalSharedDB;
 $app = F::app();
 
 $db = $app->wf->getDB( DB_MASTER, array(), $wgExternalSharedDB);
 
-//$db->select( );
+//get last upload date, so we can check for wikis added after that date
+$row = $db->selectRow(
+	'webmaster_sitemaps',
+	array( 'MAX(upload_date)' ),
+	array(),
+	'WebmasterSitemapsUploader'
+);
+//if lastUpload not set, check only today added wikis
+$lastUpload = ( !empty( $row ) ) ? reset( $row ) : date( 'Y-m-d', time() );
+
+//get all wikis that need a sitemap
+//get data from analytics module
+//$id = 3000;
+//save it to db
+//$db->insert(
+//	array( 'webmaster_sitemaps' ),
+//	array( 'wiki_id' => $id ),
+//	'WebmasterSitemapsUploader'
+//);
+
+//select wikis to update
+//TODO: get date from command line?
+//TODO: get certain wiki from command line?
+
+$time = strtotime( '2013-05-01' );
+$formatedDate = date('Ymd', $time);
+
+$where = array(
+	"upload_date IS NULL OR date_format( upload_date, '%Y%m%d' ) <= date'{$formatedDate}'",
+	'ws.wiki_id = cl.city_id'
+);
+
+$result = $db->select(
+	array( 'ws' => 'webmaster_sitemaps', 'cl' => 'city_list' ),
+	array( 'wiki_id', 'city_url' ),
+	$where,
+	'WebmasterSitemapsUploader'
+);
+
+while( $row = $result->fetchRow() ) {
+	//connect to google and try to upload sitemap
+	//if success
+	$db->update(
+		array( 'webmaster_sitemaps' ),
+		array( 'user_id' => $userId, 'upload_date' => 'NOW()' ),
+		array( "wiki_id = {$id}"),
+		'WebmasterSitemapsUploader'
+	);
+	print_r( $row );
+}
+
