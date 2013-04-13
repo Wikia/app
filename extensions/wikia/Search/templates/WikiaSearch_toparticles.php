567a4d12653feaa39438e02ace4aadcc67263bd3
Robert Elwell <robert@wikia-inc.com>
2013-04-13 00:01:17 +0000

    add defensiveness to template

--- extensions/wikia/Search/templates/WikiaSearch_toparticles.php
+++ extensions/wikia/Search/templates/WikiaSearch_toparticles.php
@@ -2,9 +2,11 @@
     <ul>
     <?php foreach ( $resultSet->getTopPages() as $articleId ): ?>
         <?php $article = Article::newFromID( $articleId ); ?>
+        <? if ( $article ): ?>
         <li>
             <a href="<?=$article->getTitle()->getFullUrl()?>"><?=$article->getTitle()->getText()?></a>: <span class="subtle"><?=(new ArticleService( $article ))->getTextSnippet( 60 )?></span>
         </li>
+        <? endif; ?>
     <?php endforeach; ?>
     </ul>
 </div>
