e62ca4a8458a85de8e33e9a599363c5afb7ef9f5
Damian Jozwiak <damian@wikia-inc.com>
2013-04-17 18:44:48 +0200

    CON-22 | Collection setup template

--- extensions/wikia/SpecialManageWikiaHome/templates/ManageWikiaHome_index.php
+++ extensions/wikia/SpecialManageWikiaHome/templates/ManageWikiaHome_index.php
@@ -16,52 +16,97 @@
 
 	<hr />
 
-	<h2 class="heading">
-		<?= wfMsg('manage-wikia-home-wikis-in-slots-heading') ?>
-	</h2>
+	<div class="slots-setup">
+		<h2 class="heading">
+			<?= wfMsg('manage-wikia-home-wikis-in-slots-heading') ?>
+		</h2>
 
-	<?php if( !empty($errorMsg) ): ?>
-	<p class="error">
-		<?= $errorMsg; ?>
-	</p>
-	<?php endif; ?>
+		<?php if( !empty($errorMsg) ): ?>
+		<p class="error">
+			<?= $errorMsg; ?>
+		</p>
+		<?php endif; ?>
 
-	<?php if( !empty($infoMsg) ): ?>
-	<p class="success">
-		<?= $infoMsg; ?>
-	</p>
-	<?php endif; ?>
+		<?php if( !empty($infoMsg) ): ?>
+		<p class="success">
+			<?= $infoMsg; ?>
+		</p>
+		<?php endif; ?>
 
-	<p>
-		<?= wfMsg('manage-wikia-home-wikis-in-slots-total'); ?>
-		<?= $slotsInTotal; ?>
-	</p>
-	
-	<div class="slot-forms">
-		<form id="wikis-in-slots-verticals" class="wikis-in-slots" name="wikis-in-slots" method="post">
-			<h3><?= wfMsg('manage-wikia-home-verticals-proportions'); ?></h3>
-			<p><label for="video-games-amount"><?= wfMsg('hub-Video_Games'); ?></label><input type="text" id="video-games-amount" name="video-games-amount" value="<?= $videoGamesAmount; ?>" /></p>
-			<p><label for="entertainment-amount"><?= wfMsg('hub-Entertainment'); ?></label><input type="text" id="entertainment-amount" name="entertainment-amount" value="<?= $entertainmentAmount; ?>" /></p>
-			<p><label for="lifestyle-amount"><?= wfMsg('hub-Lifestyle'); ?></label><input type="text" id="lifestyle-amount" name="lifestyle-amount" value="<?= $lifestyleAmount; ?>" /></p>
-			<p><input type="hidden" name="visualization-lang" value="<?= $visualizationLang; ?>"></p>
-			<span class="status-msg"></span>
-			<p><input type="submit" value="<?= wfMsg('manage-wikia-home-change-button'); ?>"></p>
+		<p>
+			<?= wfMsg('manage-wikia-home-wikis-in-slots-total'); ?>
+			<?= $slotsInTotal; ?>
+		</p>
+
+		<div class="slot-forms">
+			<form id="wikis-in-slots-verticals" class="wikis-in-slots" name="wikis-in-slots" method="post">
+				<h3><?= wfMsg('manage-wikia-home-verticals-proportions'); ?></h3>
+				<p><label for="video-games-amount"><?= wfMsg('hub-Video_Games'); ?></label><input type="text" id="video-games-amount" name="video-games-amount" value="<?= $videoGamesAmount; ?>" /></p>
+				<p><label for="entertainment-amount"><?= wfMsg('hub-Entertainment'); ?></label><input type="text" id="entertainment-amount" name="entertainment-amount" value="<?= $entertainmentAmount; ?>" /></p>
+				<p><label for="lifestyle-amount"><?= wfMsg('hub-Lifestyle'); ?></label><input type="text" id="lifestyle-amount" name="lifestyle-amount" value="<?= $lifestyleAmount; ?>" /></p>
+				<p><input type="hidden" name="visualization-lang" value="<?= $visualizationLang; ?>"></p>
+				<span class="status-msg"></span>
+				<p><input type="submit" value="<?= wfMsg('manage-wikia-home-change-button'); ?>"></p>
+			</form>
+		</div>
+	</div>
+
+	<div class="collections-setup">
+		<h2 class="heading">
+			<?= wfMsg('manage-wikia-home-collections-setup-header') ?>
+		</h2>
+
+		<form method="post" class="WikiaForm">
+			<div class="collection-module">
+				<div class="input-group">
+					<label for="">Label</label>
+					<input type="text" value="test">
+				</div>
+				<div class="input-group">
+					<label for="">Label</label>
+					<input type="text" value="test">
+				</div>
+			</div>
+			<div class="collection-module">
+				<div class="input-group">
+					<label for="">Label</label>
+					<input type="text" value="test">
+				</div>
+				<div class="input-group">
+					<label for="">Label</label>
+					<input type="text" value="test">
+				</div>
+			</div>
+			<div class="collection-module">
+				<div class="input-group">
+					<label for="">Label</label>
+					<input type="text" value="test">
+				</div>
+				<div class="input-group">
+					<label for="">Label</label>
+					<input type="text" value="test">
+				</div>
+			</div>
+
+			<input type="submit" value="<?= wfMsg('manage-wikia-home-collections-setup-save-button'); ?>" />
 		</form>
 	</div>
 
-	<h2 class="heading">
-		<?= wfMsg('manage-wikia-home-wikis-in-visualization-heading') ?>
-	</h2>
+	<div class="wikis-setup">
+		<h2 class="heading">
+			<?= wfMsg('manage-wikia-home-wikis-in-visualization-heading') ?>
+		</h2>
 
-	<form id="wiki-name-filter" class="wiki-name-filter" name="wiki-name-filter" method="get">
-		<p><?= wfMsg('manage-wikia-home-wiki-name-filter'); ?></p>
-		<p><input type="text" id="wiki-name-filer-input" name="wiki-name-filer-input" value="" /></p>
-	</form>
+		<form id="wiki-name-filter" class="wiki-name-filter" name="wiki-name-filter" method="get">
+			<p><?= wfMsg('manage-wikia-home-wiki-name-filter'); ?></p>
+			<p><input type="text" id="wiki-name-filer-input" name="wiki-name-filer-input" value="" /></p>
+		</form>
 
-	<div id="wikisWithVisualizationList">
-		<?= F::app()->renderView('ManageWikiaHome', 'renderWikiListPage', array(
-			'page' => $currentPage,
-			'visualizationLang' => $visualizationLang,
-		)); ?>
+		<div id="wikisWithVisualizationList">
+			<?= F::app()->renderView('ManageWikiaHome', 'renderWikiListPage', array(
+				'page' => $currentPage,
+				'visualizationLang' => $visualizationLang,
+			)); ?>
+		</div>
 	</div>
 </section>
