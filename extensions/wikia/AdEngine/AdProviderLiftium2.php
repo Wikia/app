eaf20c4ad4168ebecd5b8926c597c9f452a9261e
Piotr Gabryjeluk <rychu@wikia-inc.com>
2013-04-17 17:40:09 +0200

    ADEN-6 Mark legacy (PHP) AdProvider files for deletion

--- extensions/wikia/AdEngine/AdProviderLiftium2.php
+++ extensions/wikia/AdEngine/AdProviderLiftium2.php
@@ -1,5 +1,11 @@
 <?php
 
+if (empty($wgDevelEnvironment)) {
+	error_log('File marked for deletion, but still used: ' . __FILE__);
+} else {
+	die('File marked for deletion, but still used: ' . __FILE__);
+}
+
 class AdProviderLiftium2 extends AdProviderAdEngine2 implements iAdProvider {
 	public $name = 'Liftium2';
 
