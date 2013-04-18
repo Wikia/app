59172feaafe97be05d5f806fcb1b9107998e3f16
macbre <maciej.brencz@gmail.com>
2013-04-18 12:52:45 +0200

    Extra check for valid AMD id in JS tracking code

--- extensions/3rdparty/LyricWiki/LyricFind/js/tracking.js
+++ extensions/3rdparty/LyricWiki/LyricFind/js/tracking.js
@@ -1,6 +1,11 @@
 require(['jquery','LyricFindTracker', 'wikia.log'], function($, tracker, log) {
 	var amgId = parseInt($('#lyric').data('amg-id'), 10);
 
-	tracker(amgId);
-	log('tracking page view', log.levels.info, 'LyricFind');
+	if (amgId) {
+		tracker(amgId);
+		log('tracking page view', log.levels.info, 'LyricFind');
+	}
+	else {
+		log('failed to get AMG id', log.levels.error, 'LyricFind');
+	}
 });
