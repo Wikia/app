091d9c67094d5ea6481a68a49122d12ca696b399
moli@kofeina.net <moli@wikia-inc.com>
2013-04-23 22:18:03 +0200

    added "wiki" parameter to PhalanxStats special page (BAC-221)

--- extensions/wikia/PhalanxII/templates/PhalanxStatsSpecial_index.php
+++ extensions/wikia/PhalanxII/templates/PhalanxStatsSpecial_index.php
@@ -1,6 +1,8 @@
 <?= $table ?>
 
+<? if ( isset( editUrl ) ): ?>
 <a href="<?= htmlspecialchars($editUrl) ?>" class="modify"><?= wfMsg('phalanx-link-modify') ?></a>
+<? endif ?>
 
 <fieldset>
 	<legend><?= wfMsg('phalanx-stats-results') ?></legend>
