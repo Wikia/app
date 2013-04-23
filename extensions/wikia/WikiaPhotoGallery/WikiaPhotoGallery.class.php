69d4cde394f76d7340ad855fd6e2f9fff8dba32e
Damian Jozwiak <damian@wikia-inc.com>
2013-04-23 16:22:39 +0200

    bugid-49417 | Wikia photo gallery css fix for miages without thumb

--- extensions/wikia/WikiaPhotoGallery/WikiaPhotoGallery.class.php
+++ extensions/wikia/WikiaPhotoGallery/WikiaPhotoGallery.class.php
@@ -879,7 +879,7 @@ class WikiaPhotoGallery extends ImageGallery {
 				}
 
 				$linkAttribs = array(
-					'class' => $image['classes'],
+					'class' => empty($image['thumbnail']) ? 'image-no-lightbox' : $image['classes'],
 					'href' => $image['link'],
 					'title' => $image['linkTitle']. (isset($image['bytes'])?' ('.$skin->formatSize($image['bytes']).')':""),
 					'style' => "height:{$image['height']}px; width:{$image['width']}px;"
