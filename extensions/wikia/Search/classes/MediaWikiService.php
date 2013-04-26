6d0d31579d3ecbff81c3eeb34b22cd4f4cc9286b
Robert Elwell <robert@wikia-inc.com>
2013-04-26 17:48:12 +0000

    bugid 102485 -- changed WikiaException to Exception to avoid excessive logging

--- extensions/wikia/Search/classes/MediaWikiService.php
+++ extensions/wikia/Search/classes/MediaWikiService.php
@@ -810,17 +810,17 @@ class MediaWikiService
 	 * Standard interface for this class's services to access a page
 	 * @param int $pageId
 	 * @return Article
-	 * @throws WikiaException
+	 * @throws Exception
 	 */
 	protected function getPageFromPageId( $pageId ) {
 		wfProfileIn( __METHOD__ );
 		if ( isset( self::$pageIdsToArticles[$pageId] ) ) {
 			return self::$pageIdsToArticles[$pageId];
 		}
-	    $page = \Article::newFromID( $pageId );
+		$page = \Article::newFromID( $pageId );
 
 		if( $page === null ) {
-			throw new \WikiaException( 'Invalid Article ID' );
+			throw new \Exception( 'Invalid Article ID' );
 		}
 		if( $page->isRedirect() ) {
 			self::$redirectArticles[$pageId] = $page;
