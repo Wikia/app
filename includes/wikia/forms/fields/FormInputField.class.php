3b4e45500dac958b237ea247422cddfab18bcbeb
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-05-09 11:22:41 +0000

    FormBuilder refactor - code cleanup

--- includes/wikia/forms/fields/FormInputField.class.php
+++ includes/wikia/forms/fields/FormInputField.class.php
@@ -2,10 +2,6 @@
 
 class FormInputField extends FormField
 {
-	public function __construct(WikiaValidator $validator) {
-		parent::__construct($validator);
-	}
-
 	public function render() {
 		//TODO add logic
 	}
