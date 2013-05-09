93a3511154cb1cfd4ab22718066b3ec75a27c0de
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-05-09 16:40:21 +0000

    CON-67: create table joining collections and wikis

---
+++ includes/wikia/models/sql/collections_city_visualization.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `wikia_homepage_collections_city_visualization` (
+	`collection_id` int UNSIGNED NOT NULL,
+	`city_id` int UNSIGNED NOT NULL,
+	PRIMARY KEY (`collection_id`, `city_id`)
+);
