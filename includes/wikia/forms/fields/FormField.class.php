d9832bf7376e1925c6cee39b8cd1274f27579f0a
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-05-09 10:42:27 +0000

    FormBuilder refactor - setter/getter methods

--- includes/wikia/forms/fields/FormField.class.php
+++ includes/wikia/forms/fields/FormField.class.php
@@ -2,15 +2,25 @@
 abstract class FormField {
 	const ATTRIBUTE_NAME_VALUE = 'value';
 
+	protected $properties;
+
 	// TODO check if abstract is required or maybe we can create universal logic for all fields
 	abstract public function render();
 
-	public function setFieldProperty($propertyName, $propertyValue) {
-		// TODO add logic
+	public function setProperty($propertyName, $propertyValue) {
+		$this->properties[$propertyName] = $propertyValue;
 	}
 
-	public function setFieldValue($value) {
-		$this->setFieldProperty(self::ATTRIBUTE_NAME_VALUE, $value);
+	public function setValue($value) {
+		$this->setProperty(self::ATTRIBUTE_NAME_VALUE, $value);
+	}
+
+	public function getProperty($propertyName) {
+		return $this->properties[$propertyName];
+	}
+
+	public function getValue() {
+		return $this->properties[self::ATTRIBUTE_NAME_VALUE];
 	}
 
 	public function filterValue($value) {
