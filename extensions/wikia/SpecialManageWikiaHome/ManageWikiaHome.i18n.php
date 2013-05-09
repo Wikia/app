37d428a9d65316b23bc1953f3e9f1d11e65e02fa
nAndy <nandy@wikia-inc.com>
2013-05-09 13:10:27 +0000

    first part of i18n/l10n changes after review; added two more authors of the code ;)

--- extensions/wikia/SpecialManageWikiaHome/ManageWikiaHome.i18n.php
+++ extensions/wikia/SpecialManageWikiaHome/ManageWikiaHome.i18n.php
@@ -12,13 +12,13 @@ $messages['en'] = array(
 	'manage-wikia-home-wrong-rights' => "You are probably logged-out or you don't have permissions to use this special page.",
 
 	'manage-wikia-home-wikis-in-slots-heading' => 'Slots setup',
-	'manage-wikia-home-wikis-in-slots-total' => 'Total amount of slots:',
+	'manage-wikia-home-wikis-in-slots-total' => 'Total amount of slots: $1',
 	'manage-wikia-home-visualization-wikis' => 'Corporate wikis with visualization:',
 
 	'manage-wikia-home-wikis-in-slots-success' => 'Numbers slots has been changed.',
 	'manage-wikia-home-collections-success' => 'Collections has been saved.',
 
-	'manage-wikia-home-verticals-proportions' => 'Number of wikis for each vertical in WV',
+	'manage-wikia-home-verticals-proportions' => 'Number of wikis per vertical',
 
 	'manage-wikia-home-error-invalid-total-no-of-slots' => "Invalid slots amount in total ($1). Please make sure sum of each hub's slots equals total amount of slots ($2).",
 	'manage-wikia-home-error-negative-slots-number-not-allowed' => 'Negative number of slots is not allowed',
@@ -51,12 +51,12 @@ $messages['en'] = array(
 
 	'manage-wikia-home-collections-enabled-field-label' => 'Enabled',
 	'manage-wikia-home-collections-name-field-label' => 'Collection name',
-	'manage-wikia-home-collections-sponsor-url-field-label' => 'Sponsors url',
+	'manage-wikia-home-collections-sponsor-url-field-label' => 'Sponsor URL',
 	'manage-wikia-home-collections-sponsor-hero-image-field-label' => 'Hero image',
 	'manage-wikia-home-collections-sponsor-image-field-label' => 'Sponsor image',
 
-	'manage-wikia-home-collection-hero-image-tooltip' => 'This image should be 1010px x 650px',
-	'manage-wikia-home-collection-sponsor-image-tooltip' => 'This image should be 330px x 210px',
+	'manage-wikia-home-collection-hero-image-tooltip' => 'This image should be $1px × $2px',
+	'manage-wikia-home-collection-sponsor-image-tooltip' => 'This image should be $1px × $2px',
 	'manage-wikia-home-collection-add-file-button' => 'Add a photo'
 );
 
@@ -64,14 +64,14 @@ $messages['qqq'] = array(
 	'managewikiahome' => 'Page title visible as browser window\'s title; Content of <title /> tag in HTML',
 	'manage-wikia-home-wrong-rights' => "A message displayed to a user if he doesn't have rights to use this special page",
 
-	'manage-wikia-home-wikis-in-slots-heading' => 'Header above slots configuration',
-	'manage-wikia-home-wikis-in-slots-total' => 'A label next to total slots number',
+	'manage-wikia-home-wikis-in-slots-heading' => 'Header of slots\' configuration container; this container have input fields which we use to configure amounts of images per vertical we\'re showing in the visualization on corporate main page',
+	'manage-wikia-home-wikis-in-slots-total' => 'Text describing how many slots are available to use in configuration; $1 = number; amount of slots',
 	'manage-wikia-home-visualization-wikis' => 'A label next to selectbox with a list of wikis where visualization is enabled (corporate wikis)',
 
 	'manage-wikia-home-wikis-in-slots-success' => 'Success information after successful change in slots configuration',
 	'manage-wikia-home-collections-success' => 'Success information after successful change in collections configuration',
 
-	'manage-wikia-home-verticals-proportions' => 'Header above slots configuration\'s input fields',
+	'manage-wikia-home-verticals-proportions' => 'Header above slots configuration\'s input fields; in the form below this header we can configure how many slots are assigned to a vertical; later we fill those slots with a wiki image from a specific vertical',
 
 	'manage-wikia-home-error-invalid-total-no-of-slots' => "An error which is displayed when set numbers in total doesn't equal required number of all slots; first parameter is the sum of entered numbers and second one is the required number",
 	'manage-wikia-home-error-negative-slots-number-not-allowed' => "An error which is displayed when any of set numbers is negative",
@@ -105,11 +105,11 @@ $messages['qqq'] = array(
 	'manage-wikia-home-collections-enabled-field-label' => 'A label before checkbox; the checkbox is to indicate status of a collection (enabled/disabled)',
 	'manage-wikia-home-collections-name-field-label' => 'A label next to a collection\'s name field; in this field you put a collection\'s name',
 	'manage-wikia-home-collections-sponsor-url-field-label' => 'A label next to collection\'s sponsor URL field; in the field you put the sponsor URL address',
-	'manage-wikia-home-collections-sponsor-hero-image-field-label' => 'A label next to collection\'s hero image field; in the field you put an image name',
-	'manage-wikia-home-collections-sponsor-image-field-label' => 'A label next to collection\'s sponsor image field; in the field you put an image name',
+	'manage-wikia-home-collections-sponsor-hero-image-field-label' => 'A label next to collection\'s bigger image field (the big image covers all wikis images on corporate main page for some time then it fades out); in the field you put an image name',
+	'manage-wikia-home-collections-sponsor-image-field-label' => 'A label next to collection\'s smaller image field (the smaller image covers statistic section on corporate main page for a specific wikis\' set); in the field you put an image name',
 
-	'manage-wikia-home-collection-hero-image-tooltip' => 'Image size information under the hero image field',
-	'manage-wikia-home-collection-sponsor-image-tooltip' => 'Image size information under the sponsor image field',
+	'manage-wikia-home-collection-hero-image-tooltip' => 'Image size information under the big image field; the big image covers all wikis images on corporate main page; $1 = width of the image, $2 = its height',
+	'manage-wikia-home-collection-sponsor-image-tooltip' => 'Image size information under the smaller image field; $1 = width of the image, $2 = its height',
 	
-	'manage-wikia-home-collection-add-file-button' => 'A label on buttons which are supposed to open image uploader',
+	'manage-wikia-home-collection-add-file-button' => 'A label on buttons which open the image uploader',
 );
