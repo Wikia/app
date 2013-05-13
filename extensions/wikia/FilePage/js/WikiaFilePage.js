e5ee26ebaf44113f390d8a29a9fd4aefb4d9d72c
liz <liz@wikia-inc.com>
2013-05-13 22:14:01 +0000

    reformating file page js to be wrapped with amd

--- extensions/wikia/FilePage/js/WikiaFilePage.js
+++ extensions/wikia/FilePage/js/WikiaFilePage.js
@@ -1,26 +1,20 @@
 $(function() {
 
-var WikiaFilePage = {
+require(['wikia.videoBootstrap'], function (VideoBootstrap) {
+
 	/*
 	 * Use VideoBootstrap to create a video instance
 	 */
-	initVideo: function() {
-		if(window.playerParams) {
-			require(['wikia.videoBootstrap'], function (VideoBootstrap) {
+	if(window.playerParams) {
+		var element = $('#file'),
+			clickSource = 'filePage',
+			videoInstance = new VideoBootstrap(element[0], window.playerParams, clickSource);
 
-				var element = $('#file'),
-					clickSource = 'filePage',
-					videoInstance = new VideoBootstrap(element[0], window.playerParams, clickSource);
-
-				$(window).on('lightboxOpened', function() {
-					videoInstance.reload(wgTitle, 670, false, clickSource);
-				});
-
-			});
-		}
+		$(window).on('lightboxOpened', function() {
+			videoInstance.reload(wgTitle, 670, false, clickSource);
+		});
 	}
-}
 
-WikiaFilePage.initVideo();
+});
 
 });
