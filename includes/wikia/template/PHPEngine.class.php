af4a32f8690c48855ee930d5bb8183aaf856a1d9
Federico 'Lox' Lucignano <federico@wikia-inc.com>
2013-05-13 17:11:03 +0200

    PHPEngine: avoid template path being overridden by template data after extract has been called

--- includes/wikia/template/PHPEngine.class.php
+++ includes/wikia/template/PHPEngine.class.php
@@ -14,6 +14,13 @@ namespace Wikia\Template;
  */
 
 class PHPEngine extends Engine {
+	/*
+	 * Stores temporaneously the full path to the template file
+	 * using a member of the class shields the value from being
+	 * overridden by the result of calling extract
+	 */
+	private $path;
+
 	/**
 	 * @see Engine::exists() for documentation and usage
 	 */
@@ -32,21 +39,22 @@ class PHPEngine extends Engine {
 	public function render( $template ) {
 		wfProfileIn( __METHOD__ );
 
-		$path = $this->prefix == '' ? $template : $this->prefix . DIRECTORY_SEPARATOR . $template;
+		$this->path = $this->prefix == '' ? $template : $this->prefix . DIRECTORY_SEPARATOR . $template;
 
 		extract( $this->values );
 		ob_start();
 
-		wfProfileIn( __METHOD__ . " - template: {$path}" );
-		( include $path ) or call_user_func( function () use( $template ) {
+		wfProfileIn( __METHOD__ . " - template: {$this->path}" );
+		( include $this->path ) or call_user_func( function () use( $template ) {
 			//avoid failing miserably, match Mustache PHP exception message
 			//doing this avoids having to do an FS lookup every time to check
 			//if the template is missing before invoking include
 			throw new \Exception( "Template \"{$template}\" was not found" );
 		} );
-		wfProfileOut( __METHOD__ . " - template: {$path}" );
+		wfProfileOut( __METHOD__ . " - template: {$this->path}" );
 
 		$contents = ob_get_clean();
+		$this->path = null;
 
 		wfProfileOut( __METHOD__ );
 		return $contents;
