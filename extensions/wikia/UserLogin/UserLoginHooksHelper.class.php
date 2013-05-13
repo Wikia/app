15ee6c53d71bbca97414ce74645df75a9aa68395
tor <tor@wikia-inc.com>
2013-05-13 13:56:49 +0000

    set merketingallowed option correctly by default for new accounts

--- extensions/wikia/UserLogin/UserLoginHooksHelper.class.php
+++ extensions/wikia/UserLogin/UserLoginHooksHelper.class.php
@@ -2,6 +2,13 @@
 
 class UserLoginHooksHelper {
 
+	// set default user options and perform other actions after account creation
+	public static function onAddNewAccount( User $user, $byEmail ) {
+		$user->setOption( 'marketingallowed', 1 );
+
+		return true;
+	}
+
 	// send reconfirmation mail
 	public static function onUserSendReConfirmationMail( &$user, &$result ) {
 		$userLoginHelper = F::build( 'UserLoginHelper' );
