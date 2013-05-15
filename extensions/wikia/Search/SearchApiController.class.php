bf2d6be662edb6b272d01a3acad677cf1780f5b9
Robert Elwell <robert@wikia-inc.com>
2013-05-15 19:30:42 +0000

    updated abstract select searchasapi to allow field mapping

--- extensions/wikia/Search/SearchApiController.class.php
+++ extensions/wikia/Search/SearchApiController.class.php
@@ -38,57 +38,43 @@ class SearchApiController extends WikiaApiController {
 	 * @example &namespaces=14&query=char
 	 */
 	public function getList() {
-		$searchConfig = new Config();
-		$type = $this->request->getVal( 'type', 'articles' );
-		$query = $this->getVal( 'query', null );
-		$rank = $this->request->getVal( 'rank', 'default' );
-		$limit = $this->request->getInt( 'limit', self::ITEMS_PER_BATCH );
-		$batch = $this->request->getVal( 'batch', 1 );
-		$namespaces = $this->request->getArray( 'namespaces', null );
+		$searchConfig = $this->getConfigFromRequest();
+		$this->validateNamespacesForConfig( $searchConfig );
+
+		if (! $searchConfig->getQuery()->hasTerms() ) {
+			throw new NotFoundApiException();
+		}
+		
+		$this->setResponseFromConfig( $searchConfig );
+	}
+	
+	/**
+	 * Sets the response based on values set in config
+	 * @param Wikia\Search\Config $searchConfig
+	 */
+	protected function setResponseFromConfig( Wikia\Search\Config $searchConfig ) {
 		$total = 0;
 		$results = [];
 		$batches = 0;
 		$currentBatch = 0;
 		$next = 0;
+		
+		$limit = $searchConfig->getLimit();
+		
+		$resultSet = (new Factory)->getFromConfig( $searchConfig )->search();
+		
+		$total = $searchConfig->getResultsFound();
 
-		$searchConfig->setQuery( $query )
-		             ->setLimit( $limit )
-		             ->setPage( $batch )
-		             ->setRank( $rank )
-		             ->setVideoSearch( $type == 'videos' )
-		;
+		if ( $total ) {
+			$results = $resultSet->toArray( ['pageid' => 'id', 'title', 'url', 'ns' ] );
+			unset( $results['pageid'] );
 
-		if ( !empty( $namespaces ) ) {
-			foreach ( $namespaces as &$n ) {
-				if ( is_numeric( $n ) ) {
-					$n = (int) $n;
-				} else {
-					throw new InvalidParameterApiException( self::PARAMETER_NAMESPACES );
-				}
-			}
+			$batches = $searchConfig->getNumPages();
+			$currentBatch = $searchConfig->getPage();
+			$next = max( 0, $total - ( $limit * $currentBatch ) );
 
-			$searchConfig->setNamespaces( $namespaces );
-		}
-
-		if ( $searchConfig->getQuery()->hasTerms() ) {
-			$wikiaSearch = (new Factory)->getFromConfig( $searchConfig );
-
-			$resultSet = $wikiaSearch->search( $searchConfig );
-			$total = $searchConfig->getResultsFound();
-
-			if ( $total ) {
-				$results = $resultSet->toArray( ['pageid' => 'id', 'title', 'url', 'ns' ] );
-				unset( $results['pageid'] );
-
-				$batches = $searchConfig->getNumPages();
-				$currentBatch = $searchConfig->getPage();
-				$next = max( 0, $total - ( $limit * $currentBatch ) );
-
-				if ( $next > $limit ) {
-					$next = $limit;
-				}
-			} else {
-				throw new NotFoundApiException();
+			if ( $next > $limit ) {
+				$next = $limit;
 			}
 		}
 
@@ -107,5 +93,41 @@ class SearchApiController extends WikiaApiController {
 				WikiaResponse::CACHE_TARGET_VARNISH
 			)
 		);
+	}
+	
+	/**
+	 * Validates user-provided namespaces.
+	 * @param Wikia\Search\Config $searchConfig
+	 * @throws InvalidParameterApiException
+	 */
+	protected function validateNamespacesForConfig( $searchConfig ) {
+		$namespaces = $this->getRequest()->getArray( 'namespaces', null );
+		if (! empty( $namespaces ) ) {
+			foreach ( $namespaces as &$n ) {
+				if ( is_numeric( $n ) ) {
+					$n = (int) $n;
+				} else {
+					throw new InvalidParameterApiException( self::PARAMETER_NAMESPACES );
+				}
+			}
+
+			$searchConfig->setNamespaces( $namespaces );
+		}
+	}
+	
+	/**
+	 * Inspects request and sets config accordingly.
+	 * @return Wikia\Search\Config
+	 */
+	protected function getConfigFromRequest() {
+		$request = $this->getRequest();
+		$searchConfig = new Wikia\Search\Config;
+		$searchConfig->setQuery( $request->getVal( 'query', null ) )
+		             ->setLimit( $request->getInt( 'limit', self::ITEMS_PER_BATCH ) )
+		             ->setPage( $request->getVal( 'batch', 1 ) )
+		             ->setRank( $request->getVal( 'rank', 'default' ) )
+		             ->setVideoSearch( $request->getVal( 'type', 'articles' ) == 'videos' )
+		;
+		return $searchConfig;
 	}
 }
