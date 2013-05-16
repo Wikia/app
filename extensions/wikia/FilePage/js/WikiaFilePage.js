43b25909f0ff200929ab8ccd551138cecf264648
liz <liz@wikia-inc.com>
2013-05-16 19:05:30 +0000

    requiring window and jquery and adding variable for file page video width for clarity

--- extensions/wikia/FilePage/js/WikiaFilePage.js
+++ extensions/wikia/FilePage/js/WikiaFilePage.js
@@ -1,20 +1,23 @@
-$(function() {
+require(['wikia.videoBootstrap', 'jquery', 'wikia.window'], function (VideoBootstrap, jquery, window) {
 
-require(['wikia.videoBootstrap'], function (VideoBootstrap) {
+	'use strict';
 
 	/*
 	 * Use VideoBootstrap to create a video instance
 	 */
-	if(window.playerParams) {
-		var element = $('#file'),
-			clickSource = 'filePage',
-			videoInstance = new VideoBootstrap(element[0], window.playerParams, clickSource);
+	function initVideo() {
+		if(window.playerParams) {
+			var filePageVideoWidth = 670,
+				element = $('#file'),
+				clickSource = 'filePage',
+				videoInstance = new VideoBootstrap(element[0], window.playerParams, clickSource);
 
-		$(window).on('lightboxOpened', function() {
-			videoInstance.reload(wgTitle, 670, false, clickSource);
-		});
+			$(window).on('lightboxOpened', function() {
+				videoInstance.reload(wgTitle, filePageVideoWidth, false, clickSource);
+			});
+		}
 	}
 
-});
+	$(initVideo);
 
 });
