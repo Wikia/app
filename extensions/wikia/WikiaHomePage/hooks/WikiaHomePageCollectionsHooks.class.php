689a3f1d69f5845d1c52689b2ea261b120ed19a3
Damian Jozwiak <damian@wikia-inc.com>
2013-05-16 13:18:22 +0200

    CON-113 | Falat error fix - Change after Code review

--- extensions/wikia/WikiaHomePage/hooks/WikiaHomePageCollectionsHooks.class.php
+++ extensions/wikia/WikiaHomePage/hooks/WikiaHomePageCollectionsHooks.class.php
@@ -4,8 +4,7 @@
  * 
  * @desc Temporary class which will be removed after we create admin tool to manage collection and we resign from keeping them in WF variable
  */
-$dir = dirname(__FILE__);
-require_once( $dir . '/../../CityVisualization/models/CityVisualization.class.php' );
+require_once( $IP . '/extensions/wikia/CityVisualization/models/CityVisualization.class.php' );
 
 F::app()->registerHook('WikiFactoryChanged', 'WikiaHomePageCollectionsHooks', 'onWikiFactoryVarChanged');
 
