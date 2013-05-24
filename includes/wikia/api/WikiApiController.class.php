73d473d792f5aa8ecb8d30befdae9c143c07dc23
jursza <jacek@wikia-inc.com>
2013-05-24 16:26:12 +0200

    getWikiData polishing

--- includes/wikia/api/WikiApiController.class.php
+++ includes/wikia/api/WikiApiController.class.php
@@ -22,42 +22,40 @@ class WikiApiController extends WikiaApiController {
 	 * @responseParam array $items The list of wikis, each containing: title, url, description, thumbnail, no. of articles, no. of photos, list of top contributors, no. of videos
 	 *
 	 * @example &ids=159,831,3125
-	 * @example &ids=159,831,3125&height=100&width=100
+	 * @example &ids=159,831,3125&width=100
 	 * @example &ids=159,831,3125&height=100&width=100&snippet=25
 	 */
 	public function getWikiData() {
 		$ids = $this->request->getArray( 'ids' );
 		$imageWidth = $this->request->getInt( 'width', 250 );
+		$imageHeight = $this->request->getInt( 'height', null );
 
 		$items = array();
 		foreach ( $ids as $wikiId ) {
 			if ( ( $cached = $this->getFromCacheWiki( $wikiId ) ) !== false ) {
 				//get from cache
-//				F::app()->wg->memc->delete( $this->getMemCacheKey( $wikiId ));
 				$items[] = $cached;
 			} else {
 				//get data providers
 				$wikiObj = WikiFactory::getWikiByID( $wikiId );
 				$service = new WikiService();
 				$wikiStats = $service->getSiteStats( $wikiId );
-				$wikiDesc = $service->getWikiDescription( [ $wikiId ], $imageWidth );
-
-				//missing table on dev-box, its available on production
-//				$topUsers = $service->getTopEditors( $wikiId, 10, true );
+				$wikiDesc = $service->getWikiDescription( [ $wikiId ], $imageWidth, $imageHeight );
+				$topUsers = $service->getTopEditors( $wikiId, 10, true );
 
 				$wikiInfo = array(
 					'wikiId' => (int) $wikiId,
 					'articles' => (int) $wikiStats[ 'articles' ],
 					'images' => (int) $wikiStats[ 'images' ],
 					'videos' => (int) $service->getTotalVideos( $wikiId ),
-//					'topUsers' => get from $topUsers,
+					'topUsers' => array_keys( $topUsers ),
 					'title' => $wikiObj->city_title,
 					'url' => $wikiObj->city_url,
 					'thumbnail' => isset( $wikiDesc[ $wikiId ] ) ? $wikiDesc[ $wikiId ]['image_url'] : '',
 					'description' => isset( $wikiDesc[ $wikiId ] ) ? $wikiDesc[ $wikiId ]['desc'] : '',
 				);
 				//cache data
-//				$this->cacheWikiData( $wikiInfo );
+				$this->cacheWikiData( $wikiInfo );
 				$items[] = $wikiInfo;
 			}
 		}
