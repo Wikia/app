0e2ba2d96ff6586b05f99da0ea11e422ddedc706
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-05-27 15:03:53 +0000

    CON-121: Script for counting edits and contributors on CSS files - add timestamp

--- maintenance/wikia/checkWikiCssEdits.php
+++ maintenance/wikia/checkWikiCssEdits.php
@@ -7,6 +7,8 @@ const CSV_FILE = 'cssEdits.csv';
 
 global $wgCityId, $wgSitename, $wgDBname;
 
+$ts = time() - (180 * 24 * 60 * 60);
+
 $db = wfGetDb(DB_SLAVE, array(), $wgDBname);
 
 $pageId = getCssPageId($db);
@@ -18,10 +20,10 @@ echo "Wiki name: " . $wgSitename . "\n";
 if (!empty($pageId)) {
 	echo "checking number of contributors...\n";
 
-	$numContributors = countContributors($pageId, $db);
+	$numContributors = countContributors($pageId, $ts, $db);
 	echo 'There is ' . $numContributors . ' contributors';
 
-	$numEdits = countCssEdits($pageId, $db);
+	$numEdits = countCssEdits($pageId, $ts, $db);
 	echo 'There is ' . $numEdits . ' edits';
 
 	$cssEditData = array( $wgCityId, $numContributors, $numEdits );
@@ -47,20 +49,28 @@ function getCssPageId($db) {
 	return $row['page_id'];
 }
 
-function countContributors($pageId, $db) {
-	$cond = ['rev_page' => $pageId];
+function countContributors($pageId, $ts, $db) {
+	$conds = [
+			'rev_page' => $pageId
+	];
 
-	$result = $db->select('revision', 'COUNT(DISTINCT(rev_user)) AS contributors', $cond);
+	$conds  = $db->makeList($conds, LIST_AND);
+	$conds .= ' AND rev_timestamp >= ' . $ts;
+
+	$result = $db->select('revision', 'COUNT(DISTINCT(rev_user)) AS contributors', $conds);
 
 	$row = $db->fetchRow($result);
 
 	return $row['contributors'];
 }
 
-function countCssEdits($pageId, $db) {
-	$cond = ['rev_page' => $pageId];
+function countCssEdits($pageId, $ts, $db) {
+	$conds = ['rev_page' => $pageId];
 
-	$result = $db->select('revision', 'COUNT(1) AS edits', $cond);
+	$conds  = $db->makeList($conds, LIST_AND);
+	$conds .= ' AND rev_timestamp >= ' . $ts;
+
+	$result = $db->select('revision', 'COUNT(1) AS edits', $conds);
 
 	$row = $db->fetchRow($result);
 
