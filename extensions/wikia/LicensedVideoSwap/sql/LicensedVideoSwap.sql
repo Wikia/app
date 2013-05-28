27832888fc126b468757d37508966f560e56d0e1
Garth Webb <gart@wikia-inc.com>
2013-05-28 15:55:09 -0700

    VID-243 Added some code to list out unswapped videos.

--- extensions/wikia/LicensedVideoSwap/sql/LicensedVideoSwap.sql
+++ extensions/wikia/LicensedVideoSwap/sql/LicensedVideoSwap.sql
@@ -5,7 +5,7 @@ create table youtube_swap (
   is_swapped    boolean,
   exact_match   boolean,
   user_id       integer,
-  date          datetime on update current_timestamp,
+  date          timestamp DEFAULT current_timestamp,
   index (orig_title, is_swapped, exact_match),
   index (user_id),
   index (swap_title)
