f3e9cbe016ed8141aee00fdc125e77937d6c5e8d
nAndy <nandy@wikia-inc.com>
2013-05-29 16:28:33 +0000

    removed redudant part of code

--- extensions/wikia/SpecialCss/SpecialCssModel.class.php
+++ extensions/wikia/SpecialCss/SpecialCssModel.class.php
@@ -17,18 +17,8 @@ class SpecialCssModel extends WikiaModel {
 	 * @return Return|string
 	 */
 	public function getCssFileContent() {
-		wfProfileIn(__METHOD__);
-		
-		$out = '';
-		$cssTitle = $this->getCssFileTitle();
-
-		if( $cssTitle instanceof Title) {
-			$cssArticle = $this->getCssFileArticle( $cssTitle->getArticleId() );
-			$out = $cssArticle->getContent();
-		}
-
-		wfProfileOut(__METHOD__);
-		return $out;
+		$cssArticle = $this->getCssFileArticle( $this->getCssFileArticleId() );
+		return $cssArticle->getContent();
 	}
 
 	/**
@@ -108,4 +98,3 @@ class SpecialCssModel extends WikiaModel {
 		return SpecialPage::getTitleFor('CSS');
 	}
 }
-
