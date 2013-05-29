0df14e3e3da0205e81c9006bfaf42b2cb735e4e6
saipetch <saipetch@wikia-inc.com>
2013-05-28 17:32:10 -0700

    Video (vid-243): move getUnswappedVideoList() to helper class

--- extensions/wikia/LicensedVideoSwap/LicensedVideoSwapHelper.class.php
+++ extensions/wikia/LicensedVideoSwap/LicensedVideoSwapHelper.class.php
@@ -9,6 +9,71 @@
 class LicensedVideoSwapHelper extends WikiaModel {
 
 	/**
+	 * Gets a list of videos that have not yet been swapped (e.g., no decision to keep or not keep the
+	 * original video has been made)
+	 *
+	 * @param string $sort - The sort order for the video list (options: recent, popular, trend)
+	 * @param int $limit - The number of videos to return
+	 * @param int $page - Which page of video to return
+	 * @return array - An array of video metadata
+	 */
+	public function getUnswappedVideoList ( $sort = 'popular', $limit = 10, $page = 1 ) {
+		wfProfileIn( __METHOD__ );
+
+		$db = $this->wf->GetDB( DB_SLAVE );
+
+		// We want to make sure the video hasn't been removed, is not premium and does not exist
+		// in the video_swap table
+		$sqlWhere = array(
+			'removed' => 0,
+			'premium' => 0,
+			'orig_title IS NULL'
+		);
+
+		// Paging options
+		$sqlOptions = array(
+			'LIMIT'  => $limit,
+			'OFFSET' => ( ( $page - 1 ) * $limit )
+		);
+
+		// Get the right sorting
+		switch ( $sort ) {
+			case 'popular': $sqlOptions['ORDER BY'] = 'views_total DESC';
+							break;
+			case 'trend'  : $sqlOptions['ORDER BY'] = 'views_30day DESC';
+							break;
+			default:        $sqlOptions['ORDER BY'] = 'added_at DESC';
+		}
+
+		// Do the outer join on the video_swap table
+		$joinCond = array( 'video_swap' => array( 'LEFT JOIN', 'video_title = orig_title' ) );
+
+		// Select video info making sure to skip videos that have entries in the video_swap table
+		$result = $db->select(
+			array( 'video_info', 'video_swap' ),
+			array( 'video_title, added_at, added_by' ),
+			$sqlWhere,
+			__METHOD__,
+			$sqlOptions,
+			$joinCond
+		);
+
+		// Build the return array
+		$videoList = array();
+		while( $row = $db->fetchObject($result) ) {
+			$videoList[] = array(
+				'title' => $row->video_title,
+				'addedAt' => $row->added_at,
+				'addedBy' => $row->added_by,
+			);
+		}
+
+		wfProfileOut( __METHOD__ );
+
+		return $videoList;
+	}
+
+	/**
 	 * get sort options
 	 * @return array $options
 	 */
--- extensions/wikia/LicensedVideoSwap/LicensedVideoSwapSpecialController.class.php
+++ extensions/wikia/LicensedVideoSwap/LicensedVideoSwapSpecialController.class.php
@@ -79,7 +79,8 @@ class LicensedVideoSwapSpecialController extends WikiaSpecialPageController {
 		$playButton = WikiaFileHelper::videoPlayButtonOverlay( self::THUMBNAIL_WIDTH, self::THUMBNAIL_HEIGHT );
 
 		// Get the list of videos that haven't been swapped yet
-		$videoList = $this->getUnswappedVideoList( $sort, self::VIDEOS_PER_PAGE, $page );
+		$videoHelper = new LicensedVideoSwapHelper();
+		$videoList = $videoHelper->getUnswappedVideoList( $sort, self::VIDEOS_PER_PAGE, $page );
 
 		// Reuse code from SpecialVideosHelper
 		$helper = new SpecialVideosHelper();
@@ -105,65 +106,6 @@ class LicensedVideoSwapSpecialController extends WikiaSpecialPageController {
 		wfProfileOut( __METHOD__ );
 
 		return $videos;
-	}
-
-	/**
-	 * Gets a list of videos that have not yet been swapped (e.g., no decision to keep or not keep the
-	 * original video has been made)
-	 *
-	 * @param string $sort - The sort order for the video list (options: recent, popular, trend)
-	 * @param int $limit - The number of videos to return
-	 * @param int $page - Which page of video to return
-	 * @return array - An array of video metadata
-	 */
-	private function getUnswappedVideoList ( $sort = 'popular', $limit = 10, $page = 1 ) {
-		wfProfileIn( __METHOD__ );
-
-		$db = $this->wf->GetDB( DB_SLAVE );
-
-		// We want to make sure the video hasn't been removed, is not premium and does not exist
-		// in the video_swap table
-		$sqlWhere = array( 'removed' => 0,
-						   'premium' => 0,
-						   'orig_title IS NULL');
-
-		// Paging options
-		$sqlOptions = array( 'LIMIT'  => $limit,
-							 'OFFSET' => (($page - 1) * $limit));
-
-		// Get the right sorting
-		switch ($sort) {
-			case 'popular': $sqlOptions['ORDER BY'] = 'views_total DESC'; break;
-			case 'trend'  : $sqlOptions['ORDER BY'] = 'views_30day DESC'; break;
-			default:        $sqlOptions['ORDER BY'] = 'added_at DESC';
-		}
-
-		// Do the outer join on the video_swap table
-		$joinCond = array( 'video_swap' => array( 'LEFT JOIN', 'video_title = orig_title' ) );
-
-		// Select video info making sure to skip videos that have entries in the video_swap table
-		$result = $db->select(
-			array( 'video_info', 'video_swap' ),
-			array( 'video_title, added_at, added_by' ),
-			$sqlWhere,
-			__METHOD__,
-			$sqlOptions,
-			$joinCond
-		);
-
-		// Build the return array
-		$videoList = array();
-		while( $row = $db->fetchObject($result) ) {
-			$videoList[] = array(
-				'title' => $row->video_title,
-				'addedAt' => $row->added_at,
-				'addedBy' => $row->added_by,
-			);
-		}
-
-		wfProfileOut( __METHOD__ );
-
-		return $videoList;
 	}
 
 	/**
