bdc0735703582246faa6c0f4b66fe24f785103fd
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-05-29 14:05:00 +0200

    PLA-267 move JsonFormat to extension

---
+++ extensions/wikia/JsonFormat/JsonFormat.setup.php
@@ -0,0 +1,32 @@
+<?php
+
+$dir = dirname(__FILE__);
+
+$wgAutoloadClasses['HtmlToJsonFormatParser'] =                $dir . "/HtmlToJsonFormatParser.php";
+$wgAutoloadClasses['WikitextToHtmlParser'] =                  $dir . "/WikitextToHtmlParser.php";
+$wgAutoloadClasses['WikitextToJsonFormatParserFactory'] =     $dir . "/WikitextToJsonFormatParserFactory.php";
+$wgAutoloadClasses['WikitextToJsonFormatParser'] =            $dir . "/WikitextToJsonFormatParser.php";
+$wgAutoloadClasses['JsonFormatService'] =                     $dir . "/JsonFormatService.php";
+
+$wgAutoloadClasses['JsonFormatContainerNode'] =               $dir . "/nodes/JsonFormatContainerNode.php";
+$wgAutoloadClasses['JsonFormatLinkNode'] =                    $dir . "/nodes/JsonFormatLinkNode.php";
+$wgAutoloadClasses['JsonFormatNode'] =                        $dir . "/nodes/JsonFormatNode.php";
+$wgAutoloadClasses['JsonFormatRootNode'] =                    $dir . "/nodes/JsonFormatRootNode.php";
+$wgAutoloadClasses['JsonFormatSectionNode'] =                 $dir . "/nodes/JsonFormatSectionNode.php";
+$wgAutoloadClasses['JsonFormatTextNode'] =                    $dir . "/nodes/JsonFormatTextNode.php";
+$wgAutoloadClasses['JsonFormatListNode'] =                    $dir . "/nodes/JsonFormatListNode.php";
+$wgAutoloadClasses['JsonFormatListItemNode'] =                $dir . "/nodes/JsonFormatListItemNode.php";
+
+$wgAutoloadClasses['AVisitor'] =                              $dir . "/visitors/AVisitor.php";
+$wgAutoloadClasses['PVisitor'] =                              $dir . "/visitors/PVisitor.php";
+$wgAutoloadClasses['BVisitor'] =                              $dir . "/visitors/BVisitor.php";
+$wgAutoloadClasses['IVisitor'] =                              $dir . "/visitors/IVisitor.php";
+$wgAutoloadClasses['SpanVisitor'] =                           $dir . "/visitors/SpanVisitor.php";
+$wgAutoloadClasses['ListVisitor'] =                           $dir . "/visitors/ListVisitor.php";
+$wgAutoloadClasses['BodyVisitor'] =                           $dir . "/visitors/BodyVisitor.php";
+$wgAutoloadClasses['CompositeVisitor'] =                      $dir . "/visitors/CompositeVisitor.php";
+$wgAutoloadClasses['DOMNodeVisitorBase'] =                    $dir . "/visitors/DOMNodeVisitorBase.php";
+$wgAutoloadClasses['HeaderVisitor'] =                         $dir . "/visitors/HeaderVisitor.php";
+$wgAutoloadClasses['IDOMNodeVisitor'] =                       $dir . "/visitors/IDOMNodeVisitor.php";
+$wgAutoloadClasses['JsonFormatTraversingState'] =             $dir . "/visitors/JsonFormatTraversingState.php";
+$wgAutoloadClasses['TextNodeVisitor'] =                       $dir . "/visitors/TextNodeVisitor.php";
