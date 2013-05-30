bfef91e5fe6a01cf9833e09c4fb67231f5fed351
liz <liz@wikia-inc.com>
2013-05-30 21:55:40 +0000

    using css to hide divs instead of inline styles

--- extensions/wikia/VideoEmbedTool/templates/VideoEmbedTool_modal.php
+++ extensions/wikia/VideoEmbedTool/templates/VideoEmbedTool_modal.php
@@ -5,6 +5,6 @@
 <div id="VideoEmbedBody" class="VideoEmbedBody">
 	<h1 id="VideoEmbedHeader"></h1>
 	<div id="VideoEmbedMain" class="VET_screen VideoEmbedMain"></div>
-	<div id="VideoEmbedDetails" class="VET_screen VideoEmbedDetails" style="display: none;"></div>
-	<div id="VideoEmbedSummary" class="VET_screen VideoEmbedSummary" style="display: none;"></div>
+	<div id="VideoEmbedDetails" class="VET_screen VideoEmbedDetails"></div>
+	<div id="VideoEmbedSummary" class="VET_screen VideoEmbedSummary"></div>
 </div>
