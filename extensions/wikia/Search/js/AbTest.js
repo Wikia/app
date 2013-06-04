df95b12c30fb6635ca4ecdceab7d449e41c78ad4
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-06-04 18:10:36 +0200

    PLA-332 preserve ABTest.* parameters in search

--- extensions/wikia/Search/js/AbTest.js
+++ extensions/wikia/Search/js/AbTest.js
@@ -1,7 +1,7 @@
 jQuery(function($)  {
 	var searchFormsSelector = 'form.WikiaSearch'
-		,searchPaginationLinksSelector = '.Search .wikia-paginator a.paginator-page'
-		,parameterName = 'ab';
+		,searchPaginationLinksSelector = '.wikia-paginator a.paginator-page, .search-tabs a'
+		,abGroupParameterName = 'ab';
 
 	function findSearchRelatedExperiments() {
 		var experiments = Wikia.AbTest.getExperiments();
@@ -18,7 +18,7 @@ jQuery(function($)  {
 	function getSearchRelatedGroupIds() {
 		var searchRelatedExperiments = findSearchRelatedExperiments();
 		var groupIds = [];
-		for( var i = 0; i < experiments.length; i++ ) {
+		for( var i = 0; i < searchRelatedExperiments.length; i++ ) {
 			if ( searchRelatedExperiments[i].group ) {
 				groupIds.push( searchRelatedExperiments[i].group.id );
 			}
@@ -26,18 +26,59 @@ jQuery(function($)  {
 		return groupIds;
 	}
 
-	function addParameterToForms( groupIds ) {
-		$(searchFormsSelector).append('<input type="hidden" name="' + parameterName + '" value="' + groupIds.join(',') + '">');
+	function addParameterToForms( params ) {
+		for( var paramName in params ) {
+			$(searchFormsSelector).append('<input type="hidden" name="' + paramName + '" value="' + params[paramName] + '">');
+		}
 	}
 
-	function addParamterToLinks(groupIds) {
+	function getQueryParameters() {
+		var queryString = window.location.search;
+		var parameters = {};
+
+		queryString.replace(
+			new RegExp( "([^?=&]+)(=([^&]*))?", "g" ),
+			function( $0, $1, $2, $3 ){
+				parameters[ $1 ] = $3;
+			}
+		);
+		return parameters;
+	};
+
+	function getAbTestingParameters() {
+		var parameters = {};
+		var allParameters = getQueryParameters();
+		for( var parameterName in allParameters ) {
+			if( parameterName.indexOf('AbTest.') === 0 ) {
+				parameters[parameterName] = allParameters[parameterName];
+			}
+		}
+		return parameters;
+	}
+
+	function addParameterToLinks( params ) {
 		$(searchPaginationLinksSelector).each(function() {
-			$(this).attr('href', $(this).attr('href') + '&' + parameterName + '=' + groupIds.join(','));
+			var originalLink = $(this).attr('href'),
+				modifiedLink = originalLink;
+			for( var paramName in params ) {
+				var modifiedLink = modifiedLink + '&' + paramName + '=' + params[paramName];
+			}
+			console.log('changing ' + originalLink  + ' to ' + modifiedLink);
+			$(this).attr('href', modifiedLink);
 		});
 	}
 
 	var groups = getSearchRelatedGroupIds();
-	addParameterToForms( groups );
-	addParamterToLinks( groups );
+	var parametersToEnforce = {};
+	if( groups.length ) {
+		parametersToEnforce[abGroupParameterName] = groups.join(',');
+	}
+	var abTestingParameters = getAbTestingParameters();
+	for( var parameterName in abTestingParameters ) {
+		parametersToEnforce[parameterName] = abTestingParameters[parameterName];
+	}
+
+	addParameterToForms( parametersToEnforce );
+	addParameterToLinks( parametersToEnforce );
 });
 
