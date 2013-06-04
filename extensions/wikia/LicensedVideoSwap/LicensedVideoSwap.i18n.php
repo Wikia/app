d63b386b96f7f9669745fe78613356cc7ee3e743
liz <liz@wikia-inc.com>
2013-06-04 22:59:14 +0000

    adding qqq for LVS messages

--- extensions/wikia/LicensedVideoSwap/LicensedVideoSwap.i18n.php
+++ extensions/wikia/LicensedVideoSwap/LicensedVideoSwap.i18n.php
@@ -16,7 +16,6 @@ $messages['en'] = array(
 	'lvs-callout-reason-collaborative' => 'Wikia videos are collaborative and can be used across multiple wikis',
 	'lvs-callout-reason-more' => 'and more... we will be adding more features and ways to easily use and manage Wikia Libary videos. Stay tuned!',
 	'lvs-instructions' => 'This page lists all unlicensed videos on this wiki that we think we have a licensed video match for in the Wikia Video Library. We recommend that you swap out your unlicensed videos for Wikia Video Libary videos, which have been licensed by Wikia for use on your wikis. Please note that the same video may have different video thumbnails so it\'s best to watch the video before you make a decision.',
-
 	'lvs-button-keep' => 'Keep',
 	'lvs-button-swap' => 'Swap',
 	'lvs-more-suggestions' => '$1 more {{PLURAL:$1|suggestion|suggestions}}',
@@ -31,13 +30,8 @@ $messages['qqq'] = array(
 	'lvs-callout-reason-collaborative' => 'This is a bullet point that appears below lvs-callout-header.  This section is temporary and will go away after a certain number of views.',
 	'lvs-callout-reason-more' => 'This is a bullet point that appears below lvs-callout-header.  This section is temporary and will go away after a certain number of views.',
 	'lvs-instructions' => 'This is the text at the top of the Licensed Video Swap special page that explains to the user what this page is all about. The idea is that users can exchange unlicensed videos for videos licensed for use on Wikia.',
-
-
-
-
-	// TODO: QQQ
-	'lvs-button-keep' => 'Keep',
-	'lvs-button-swap' => 'Swap',
-	'lvs-more-suggestions' => '$1 more suggestions',
-	'lvs-best-match-label' => 'Best Match from Wikia Video Library',
+	'lvs-button-keep' => 'This is the text that appears on a button that, when clicked, will keep the non-licensed video as opposed to swapping it out for a licensed video.',
+	'lvs-button-swap' => 'This is the text that appears on a button that, when clicked, will swap out a non-licensed video for a licensed video suggested from the wikia video library.',
+	'lvs-more-suggestions' => 'This text will appear below a video that is a suggestion for a licensed version of a video that already exists on the wiki.  When clicked, this link will reveal more licensed possible replacements for the non-licensed video.',
+	'lvs-best-match-label' => 'This text appears above the licensed video that is considered the best match for replacing a non-licensed video.',
 );
