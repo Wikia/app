af4b1d01de1b967c84a05867a8fcc318dd287334
Kamil Koterba <kamil@wikia-inc.com>
2013-06-05 10:21:45 +0000

    CE-26 Removing debug dumps

--- extensions/wikia/EmailTemplates/EmailTemplatesHooksHelper.class.php
+++ extensions/wikia/EmailTemplates/EmailTemplatesHooksHelper.class.php
@@ -9,21 +9,17 @@ class EmailTemplatesHooksHelper {
 	public function onComposeCommonBodyMail($title, &$keys, &$body, $editor, &$bodyHTML, &$postTransformKeys ) {
 		global $wgLanguageCode;
 		$app = F::app();
-var_dump('onComposeCommonBodyMail');
 		if ( array_key_exists( '$ACTION', $keys) ) {
 			$action = $keys['$ACTION'];
 
 			/* modify bodyHTML for blogpost action */
 			if ( $action == 'blogpost' ) {
 
-				var_dump('onComposeCommonBodyMail blogpost');
 				$msgContentHTML = wfMsgHTMLwithLanguageAndAlternative(
 					'enotif_body' . ( $action == '' ? '' : ( '_' . $action ) ),
 					'enotif_body',
 					$wgLanguageCode
 				);
-				var_dump('msgContentHTML');
-				var_dump($msgContentHTML);
 				$params = array(
 					'language' => $wgLanguageCode,
 					'greeting' => 'Hi, $WATCHINGUSERNAME',
