46bc003193fa35e36b5d3b0ef4778cd8983282dc
liz <liz@wikia-inc.com>
2013-06-05 00:55:07 +0000

    more styles for LVS

--- extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_index.php
+++ extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_index.php
@@ -20,39 +20,41 @@
 <div class="WikiaGrid LVSGrid">
 
 	<? foreach ($videoList as $video) : ?>
-		<div class="grid-3 alpha non-premium">
-			<p>
-				<?php foreach( $video['truncatedList'] as $article ) { ?>
-					<?= $article['titleText'] ?>
-				<?php } ?>
-				&nbsp; <!-- TODO: hack, replace this with message -->
-			</p>
-			<div class="video-container" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
-				<a href="<?= $video['fileUrl'] ?>" class="image video">
-					<?= $video['videoPlayButton'] ?>
-					<img itemprop="thumbnail" alt="<?= $video['fileTitle'] ?>" src="<?= $video['thumbUrl'] ?>" width="<?= $thumbWidth ?>" height="<?= $thumbHeight ?>" data-video-name="<?= htmlspecialchars($video['fileTitle']) ?>" data-video-key="<?= htmlspecialchars(urlencode($video['title'])) ?>" class="Wikia-video-thumb thumbimage">
-					<?= $video['videoOverlay'] ?>
-				</a>
-				<meta itemprop="embedUrl" content="<?= $video['embedUrl'] ?>" />
+		<div class="row">
+			<div class="grid-3 alpha non-premium">
+				<p>
+					<?php foreach( $video['truncatedList'] as $article ) { ?>
+						<?= $article['titleText'] ?>
+					<?php } ?>
+					&nbsp; <!-- TODO: hack, replace this with message -->
+				</p>
+				<div class="video-container" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+					<a href="<?= $video['fileUrl'] ?>" class="image video">
+						<?= $video['videoPlayButton'] ?>
+						<img itemprop="thumbnail" alt="<?= $video['fileTitle'] ?>" src="<?= $video['thumbUrl'] ?>" width="<?= $thumbWidth ?>" height="<?= $thumbHeight ?>" data-video-name="<?= htmlspecialchars($video['fileTitle']) ?>" data-video-key="<?= htmlspecialchars(urlencode($video['title'])) ?>" class="Wikia-video-thumb thumbimage">
+						<?= $video['videoOverlay'] ?>
+					</a>
+					<meta itemprop="embedUrl" content="<?= $video['embedUrl'] ?>" />
+				</div>
+				<button class="secondary"><?= wfMessage('lvs-button-keep') ?></button>
 			</div>
-			<button class="secondary"><?= wfMessage('lvs-button-keep') ?></button>
-		</div>
-		<div class="grid-3 premium">
-			<p><?= wfMessage('lvs-best-match-label')->text() ?></p>
-			<div class="video-container" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
-				<a href="<?= $video['fileUrl'] ?>" class="image video">
-					<?= $video['videoPlayButton'] ?>
-					<img itemprop="thumbnail" alt="<?= $video['fileTitle'] ?>" src="<?= $video['thumbUrl'] ?>" width="<?= $thumbWidth ?>" height="<?= $thumbHeight ?>" data-video-name="<?= htmlspecialchars($video['fileTitle']) ?>" data-video-key="<?= htmlspecialchars(urlencode($video['title'])) ?>" class="Wikia-video-thumb thumbimage">
-					<?= $video['videoOverlay'] ?>
-				</a>
-				<meta itemprop="embedUrl" content="<?= $video['embedUrl'] ?>" />
+			<div class="grid-3 premium">
+				<p><?= wfMessage('lvs-best-match-label')->text() ?></p>
+				<div class="video-container" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+					<a href="<?= $video['fileUrl'] ?>" class="image video">
+						<?= $video['videoPlayButton'] ?>
+						<img itemprop="thumbnail" alt="<?= $video['fileTitle'] ?>" src="<?= $video['thumbUrl'] ?>" width="<?= $thumbWidth ?>" height="<?= $thumbHeight ?>" data-video-name="<?= htmlspecialchars($video['fileTitle']) ?>" data-video-key="<?= htmlspecialchars(urlencode($video['title'])) ?>" class="Wikia-video-thumb thumbimage">
+						<?= $video['videoOverlay'] ?>
+					</a>
+					<meta itemprop="embedUrl" content="<?= $video['embedUrl'] ?>" />
+				</div>
+				<a class="more-link" href="#"><?= wfMessage('lvs-more-suggestions')->numParams(5)->text() ?></a>
+				<!-- TODO: add arrow asset -->
+				<button>(&lt;) <?= wfMessage('lvs-button-swap') ?></button>
 			</div>
-			<a class="more" href="#"><?= wfMessage('lvs-more-suggestions')->numParams(5)->text() ?></a>
-			<!-- TODO: add arrow asset -->
-			<button>(&lt;) <?= wfMessage('lvs-button-swap') ?></button>
-		</div>
-		<div class="more-videos">
-
+			<div class="more-videos">
+	asdf
+			</div>
 		</div>
 	<? endforeach; ?>
 
