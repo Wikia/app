056b52315314e7f0ecec311a635eeb36c8ecc902
Adam Robak <adamr@wikia-inc.com>
2013-06-06 12:36:58 +0200

    PLA-342 get lang code from different location

--- extensions/wikia/Search/SearchApiController.class.php
+++ extensions/wikia/Search/SearchApiController.class.php
@@ -67,7 +67,7 @@ class SearchApiController extends WikiaApiController {
 		foreach( $resultSet->getResults() as $result ) {
 			$items[] = array(
 				'id' => (int) $result->getHeader( 'wid' ),
-				'language' => $result->getHeader( 'lang' ),
+				'language' => $result->getVar( 'lang' ),
 			);
 		}
 
