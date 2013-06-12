a6625477dcf7e693b7249b5231c6566f7e537b3b
Kamil Koterba <kamil@wikia-inc.com>
2013-06-12 09:25:57 +0000

    CE-26 Adding i18n file to EmailTemplates Extension

---
+++ extensions/wikia/EmailTemplates/EmailTemplates.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Internationalisation file for the EmailTemplates extension.
+*
+* @addtogroup Languages
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'read-the-whole-post' => 'Read the whole post'
+);
+
+$messages['pl'] = array(
+	'read-the-whole-post' => 'Przeczytaj cały post'
+);
