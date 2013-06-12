dd43c1375d274450cd79f85842c7c5b88494316a
Rafal Leszczynski <rafal@wikia-inc.com>
2013-06-12 14:39:41 -0700

    Style guide home page content and styling

--- extensions/wikia/SpecialStyleguide/SpecialStyleguideController.class.php
+++ extensions/wikia/SpecialStyleguide/SpecialStyleguideController.class.php
@@ -21,7 +21,7 @@ class SpecialStyleguideController extends WikiaSpecialPageController {
 			(new Wikia\Template\MustacheEngine)
 				->setPrefix( dirname(__FILE__) . '/templates' )
 				->setData( [
-					'body' => $this->getPageContent('home')
+					'body' => $this->getPageContent('home'),
 				])
 				->render('SpecialStyleguide_index.mustache')
 		);
@@ -35,6 +35,53 @@ class SpecialStyleguideController extends WikiaSpecialPageController {
 	public function getPageContent($pagename) {
 		return (new Wikia\Template\MustacheEngine)
 			->setPrefix( dirname(__FILE__) . '/templates' )
+			->setData($this->getPageData($pagename))
 			->render('SpecialStyleguide_' . $pagename . '.mustache');
+	}
+
+	private function getPageData($pagename) {
+		$homePageData = [
+			'sections' => [
+				[
+					'header' => 'Lorem ipsum',
+					'paragraph' => 'Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit
+					sit amet non magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed diam eget risus varius blandit sit amet non magna. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.',
+				],
+				[
+					'header' => 'Lorem ipsum',
+					'paragraph' => 'Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit
+					sit amet non magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed diam eget risus varius blandit sit amet non magna. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.',
+				],
+				[
+					'header' => 'Lorem ipsum',
+					'paragraph' => 'Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit
+					sit amet non magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed diam eget risus varius blandit sit amet non magna. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.',
+					'list' => [
+						[
+							'link' => '',
+							'linkTitle' => 'Lorem Ipsum',
+							'linkName' => 'Lorem Ipsum',
+							'linkTagline' => 'Lorem ipsum dolor'
+						],
+						[
+							'link' => '',
+							'linkTitle' => 'Lorem Ipsum',
+							'linkName' => 'Lorem Ipsum',
+							'linkTagline' => 'Lorem ipsum dolor'
+						],
+						[
+							'link' => '',
+							'linkTitle' => 'Lorem Ipsum',
+							'linkName' => 'Lorem Ipsum',
+							'linkTagline' => 'Lorem ipsum dolor'
+						]
+					]
+				]
+			]
+		];
+
+		if ($pagename == 'home') {
+			return $homePageData;
+		}
 	}
 }
