adf162ee6ae4af053cbb1e2e7833a1f6f3eae448
Robert Elwell <robert@wikia-inc.com>
2013-06-18 00:00:29 +0000

    stubbed out tests needed for service

---
+++ includes/wikia/services/tests/SolrDocumentServiceTest.php
@@ -0,0 +1,46 @@
+<?php
+
+class SolrDocumentServiceTest extends WikiaBaseTest
+{
+	/**
+	 * @covers SolrDocumentService::getDocument
+	 */
+	public function testGetDocumentNoResult() {}
+	
+	/**
+	 * @covers SolrDocumentService::getDocument
+	 */
+	public function testGetDocumentWithResult() {}
+	
+	/**
+	 * @covers SolrDocumentService::setArticleId
+	 * @covers SolrDocumentService::getArticleId
+	 */
+	public function testSetGetArticleId() {}
+	
+	/**
+	 * @covers SolrDocumentService::setWikiId
+	 * @covers SolrDocumentService::getWikiId
+	 */
+	public function testSetGetWikiId() {}
+	
+	/**
+	 * @covers SolrDocumentService::setRequestedFields
+	 */
+	public function testSetRequestedFields() {}
+	
+	/**
+	 * @covers SolrDocumentService::getDocumentId
+	 */
+	public function testGetDocumentId() {}
+	
+	/**
+	 * @covers SolrDocumentService::getConfig
+	 */
+	public function testGetConfig() {}
+	
+	/**
+	 * @covers SolrDocumentService::getFactory
+	 */
+	public function testGetFactory() {}
+}
