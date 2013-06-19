b27820d3ff8e0435c71c029a9bbc9efb76e7a86b
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-06-19 20:04:53 +0000

    DAR-752: Templating & CSS for edit summary and publish menu button

--- extensions/wikia/SpecialCss/SpecialCss.i18n.php
+++ extensions/wikia/SpecialCss/SpecialCss.i18n.php
@@ -8,8 +8,18 @@ $messages['en'] = [
 	'special-css-current-version' => 'Current Version $1',
 
 	'special-css-minor-edit-label' => 'Minor edit',
+
 	'special-css-summary-placeholder' => 'Add a summary of your edit',
+
+	'special-css-edit-summary' => 'Edit summary',
+
 	'special-css-publish-button' => 'Publish',
+
+	'special-css-history-button' => 'History',
+
+	'special-css-compare-button' => 'Compare changes',
+
+	'special-css-delete-button' => 'Delete',
 
 	'special-css-oasis-only' => 'Special:CSS is available only in Wikia skin. If you would like to use this feature please switch your preference to the Wikia skin.',
 
@@ -26,9 +36,19 @@ $messages['qqq'] = [
 
 	'special-css-current-version' => 'Messages describes current version of CSS file; $1 = version name i.e. 1.0.17',
 
-	'special-css-minor-edit-label' => 'Label for minor edit checkbox',
 	'special-css-summary-placeholder' => 'Placeholder text for summary field. Disappears when user starts typing',
+	
+	'special-css-minor-edit-label' => 'Label describes minor edit checkbox input',
+
+	'special-css-edit-summary' => 'Label describes edit summary textarea field',
+	
 	'special-css-publish-button' => 'Text on the publish button; pressing the button saves new content of CSS file',
+
+	'special-css-history-button' => 'Text on the history button from dropdown menu; pressing the button shows editing history of CSS file',
+
+	'special-css-compare-button' => 'Text on the compare changes button from dropdown; pressing the button shows differences of CSS file',
+
+	'special-css-delete-button' => 'Text on the delete button from dropdown; pressing the button deletes CSS file',
 
 	'special-css-oasis-only' => 'Message displayed to users who use skin different than "Wikia Skin" (i.e. Monobook). This feature is enabled only for "Wikia Skin".',
 
--- extensions/wikia/SpecialCss/templates/SpecialCssController_index.php
+++ extensions/wikia/SpecialCss/templates/SpecialCssController_index.php
@@ -3,14 +3,33 @@
 		<pre class="css-editor-container" id="cssEditorContainer"><?= htmlspecialchars($cssContent); ?></pre>
 	</div>
 	<aside class="css-side-bar">
+		<div class="css-edit-box">
+            <label for="minorEdit"><input id="minorEdit" type="checkbox" name="minorEdit"/><?= wfMessage('special-css-minor-edit-label')->plain(); ?></label>
+            <label for="editSummary"><?= wfMessage('special-css-edit-summary')->plain() ?></label>
+			<textarea id="editSummary" name="editSummary"></textarea>
+            <nav class="wikia-menu-button primary">
+				<input class="css-publish-button" type="submit" value="<?= wfMessage('special-css-publish-button') ?>"/>
+                <span class="drop">
+					<img src="<?= $wg->BlankImgUrl ?>" class="chevron">
+					</span>
+					<ul class="WikiaMenuElement">
+						<li>
+							<a href="#" data-id=""><?= wfMessage('special-css-history-button')->plain() ?></a>
+						</li>
+						<li>
+                            <a href="#" data-id=""><?= wfMessage('special-css-compare-button')->plain() ?></a>
+						</li>
+						<li>
+                            <a href="#" data-id=""><?= wfMessage('special-css-delete-button')->plain() ?></a>
+						</li>
+					</ul>
+            </nav>
+		</div>
 		<div class="version-box">
 			<?= wfMessage('special-css-current-version')->rawParams(['1.0.17'])->text(); ?>
 		</div>
 		<a href="#" id="showChanges">show changes</a>
 	</aside>
 	<footer class="css-footer">
-		<label><input type="checkbox" name="minorEdit"/><?= wfMessage('special-css-minor-edit-label')->plain(); ?></label>
-		<textarea name="editSummary" placeholder="<?= wfMessage('special-css-summary-placeholder')->plain(); ?>"></textarea>
-		<input class="big" type="submit" value="<?= wfMessage('special-css-publish-button')->escaped(); ?>" />
 	</footer>
 </form>
