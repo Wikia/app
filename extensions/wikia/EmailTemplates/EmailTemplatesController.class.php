04517197a8b4645919f2d251cba1d77cb19dbf70
Kamil Koterba <kamil@wikia-inc.com>
2013-06-20 11:01:22 +0000

    CE-26 Adding documentation and default values, removing unused vars

--- extensions/wikia/EmailTemplates/EmailTemplatesController.class.php
+++ extensions/wikia/EmailTemplates/EmailTemplatesController.class.php
@@ -10,41 +10,50 @@ class EmailTemplatesController extends WikiaController {
 	/**
 	 * Execute function for generating view of NewBlogPostMail template
 	 * Sets params' values of template
-	 * @param $params
+	 * @param $params array of params described below
+	 * @param $params['content'] string HTML code to be included in main part of template
 	 */
 	public function executeNewBlogPostMail( $params ) {
-		$this->language = $params['language'];
 		$this->content = $params['content'];
 	}
 
 	/**
 	 * Execute function for generating view of PostInfo template
 	 * Sets params' values of template
-	 * @param $params
+	 * @param $params array of params described below
+	 * @param $params['post_url'] string Full link to full article
+	 * @param $params['post_title'] string Title of post
+	 * @param $params['avatar_url'] string Full url to user avatar image ~48px width
+	 * @param $params['username'] string Name of post author
+	 * @param $params['user_page_url'] string Full url to author page
+	 * @param $params['date'] string Date of post publish
+	 * @param $params['short_text'] string Shortened version of text
 	 */
 	public function executePostInfo( $params ) {
 		wfProfileIn( __METHOD__ );
 
-		$this->post_url = $params['post_url'];
-		$this->post_title = $params['post_title'];
-		$this->avatar_url = $params['avatar_url'];
-		$this->username = $params['username'];
-		$this->user_page_url = $params['user_page_url'];
-		$this->date = $params['date'];
-		$this->short_text = $params['short_text'];
+		$this->post_url = array_key_exists( 'post_url', $params ) ? $params['post_url'] : '';
+		$this->post_title = array_key_exists( 'post_title', $params ) ? $params['post_title'] : '';
+		$this->avatar_url = array_key_exists( 'avatar_url', $params ) ? $params['avatar_url'] : AvatarService::renderAvatar('',48);
+		$this->username = array_key_exists( 'username', $params ) ? $params['username'] : wfMessage('oasis-anon-user')->plain();
+		$this->user_page_url = array_key_exists( 'user_page_url', $params ) ? $params['user_page_url'] : '';
+		$this->date = array_key_exists( 'date', $params ) ? $params['date'] : '';
+		$this->short_text = array_key_exists( 'short_text', $params ) ? $params['short_text'] : '';
 		wfProfileOut( __METHOD__ );
 	}
 
 	/**
 	 * Execute function for generating view of Button template
 	 * Sets params' values of template
-	 * @param $params
+	 * @param $params array of params described below
+	 * @param $params['link_url'] string Full link to some destination (mandatory)
+	 * @param $params['link_text'] string Description showed on a button
 	 */
 	public function executeButton( $params ) {
 		wfProfileIn( __METHOD__ );
 
 		$this->link_url = $params['link_url'];
-		$this->link_text = $params['link_text'];
+		$this->link_text = array_key_exists( 'link_text', $params ) ? $params['link_text'] : $this->link_url;
 		wfProfileOut( __METHOD__ );
 	}
 
