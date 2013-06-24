ab7e0ff5b7ecf4e9182e3d6d4bb6629aa74b7f54
nAndy <nandy@wikia-inc.com>
2013-06-24 18:52:09 +0000

    nasty hacks to make GlobalHeader hack working correctly

--- extensions/wikia/SpecialStyleguide/SpecialStyleguideGlobalHeaderControllerOverride.php
+++ extensions/wikia/SpecialStyleguide/SpecialStyleguideGlobalHeaderControllerOverride.php
@@ -12,8 +12,18 @@ class GlobalHeaderController extends WikiaController {
 
 	public function index() {
 		$this->response->setVal( 'centralUrl', $this->getCentralUrl() );
-
-		$this->response->setVal( 'menuNodes', $this->getMenuNodes() );
+		
+		$subpage = mb_strtolower( $this->getFirstTextAfterSlash( $this->wg->Title->getSubpageText() ) );
+		
+		switch( $subpage ) {
+			case 'components':
+				$this->response->setVal( 'menuNodes', $this->getMenuNodes( 4 ) );
+				break;
+			default:
+				$this->response->setVal( 'menuNodes', $this->getMenuNodes() );
+				break;
+		}
+		
 		$this->response->setVal( 'menuNodesHash', ! empty( $this->menuNodes[0] ) ? $this->menuNodes[0]['hash'] : null );
 		$this->response->setVal( 'topNavMenuItems', ! empty( $this->menuNodes[0] ) ? $this->menuNodes[0]['children'] : null );
 	}
@@ -22,17 +32,29 @@ class GlobalHeaderController extends WikiaController {
 	 * Generates array of nodes for styleguide top menu bar
 	 * @return array
 	 */
-	private function getMenuNodes() {
+	private function getMenuNodes($activeElement = 1) {
 		$menuNodes = [
 			0 => [ 'children' => [ 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5 ] ],
-			1 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-home' )->plain(), 'href' => '#', 'specialAttr' => 'active', 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ],
-			2 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-get-started' )->plain(), 'href' => '#', 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ],
-			3 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-base-styles' )->plain(), 'href' => '#', 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ],
-			4 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-components' )->plain(), 'href' => '#', 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ],
-			5 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-design' )->plain(), 'href' => '#', 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ] ];
+			1 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-home' )->plain(), 'href' => $this->getLink(), 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ],
+			2 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-get-started' )->plain(), 'href' => $this->getLink( 'Get started' ), 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ],
+			3 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-base-styles' )->plain(), 'href' => $this->getLink( 'Base Styles' ), 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ],
+			4 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-components' )->plain(), 'href' => $this->getLink( 'Components' ), 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ],
+			5 => [ 'original' => '#', 'text' => wfMessage( 'styleguide-design' )->plain(), 'href' => $this->getLink( 'Design' ), 'specialAttr' => null, 'parentIndex' => 0, 'depth' => 0, 'children' => [ ] ] ];
 
+		$menuNodes[$activeElement]['specialAttr'] = 'active';
+		
 		$menuNodes[0][NavigationModel::HASH] = md5( serialize( $menuNodes ) );
 		return $menuNodes;
+	}
+	
+	private function getFirstTextAfterSlash( $subpageText ) {
+		$supageArr = explode( '/', $subpageText );
+		return ( !empty($supageArr[1]) ) ? $supageArr[1] : '';
+	}
+	
+	private function getLink( $pageName = false ) {
+		$styleguideModel = new SpecialStyleguideDataModel();
+		return $styleguideModel->getStyleguidePageUrl( $pageName );
 	}
 
 	/**
