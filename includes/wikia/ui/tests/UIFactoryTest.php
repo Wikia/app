356a84cdf3683e7c0a4abb4010c35fa305451d2d
Bartosz Bentkowski <bartosz.bentkowski@gmail.com>
2013-06-30 07:29:47 +0000

    unit tests first pass

--- includes/wikia/ui/tests/UIFactoryTest.php
+++ includes/wikia/ui/tests/UIFactoryTest.php
@@ -2,27 +2,51 @@
 
 class UIFactoryTest extends WikiaBaseTest {
 
+	private $instance = null;
+
 	public function setUp() {
 		parent::setUp();
 
 		global $IP;
 
 		include_once $IP.'/includes/wikia/ui/UIFactory.class.php';
+
+		$this->instance = UIFactory::getInstance();
 	}
 
 	public function testInitalizationAndSingleton() {
-		$instanceA = UIFactory::getInstance();
 		$instanceB = UIFactory::getInstance();
 
-		$this->assertEquals($instanceA, $instanceB);
+		$this->assertEquals($this->instance, $instanceB);
 	}
 
-	public function testInitalizingComponents() {
-		$component = UIFactory::getInstance()->init( $this->findOneComponentName() );
+	public function testGettingFileFullPath() {
+		// test private method
+		$method = new ReflectionMethod( 'UIFactory', 'getComponentConfigFileFullPath' );
+		$method->setAccessible(true);
+
+		$fullPath = $method->invoke( $this->instance, 'component' );
+
+		$this->assertEquals($fullPath, '/usr/wikia/source/wiki/resources/wikia/ui_components/component/component_config.json');
 	}
 
-	private function findOneComponentName() {
-		
+	/**
+	 * @dataProvider getSampleComponentConfigJSON
+	 */
+	public function testLoadingComponentsFromString( $stringJSON ) {
+		// test private method
+		$method = new ReflectionMethod( 'UIFactory', 'loadComponentConfigFromString' );
+		$method->setAccessible(true);
+
+		$component = $method->invoke( $this->instance, $stringJSON );
 	}
 
+	public function getSampleComponentConfigJSON() {
+		return [
+			// empty, sample JSON
+			[
+				'json' => '{ "name":"Sample", "desc": "Sample component", "templateValues": { "required": [], "optional": [] }, "dependencies": { "js": [], "css": [] } }'
+			]
+		];
+	}
 }
