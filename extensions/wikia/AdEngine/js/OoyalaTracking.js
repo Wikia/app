05321c0b9e715651b2e014b752e2193f90917fab
lizlux <liz@lizlux.net>
2013-07-01 15:53:05 -0700

    VID-517 adding more comments

--- extensions/wikia/AdEngine/js/OoyalaTracking.js
+++ extensions/wikia/AdEngine/js/OoyalaTracking.js
@@ -2,9 +2,16 @@ require( ['wikia.window', 'wikia.tracker'], function( window, tracker ) {
 	var wikia = window.Wikia = window.Wikia || {};
 
 	/**
-	 * Add ability to tracking video views inside flite
+	 * Hack to add ability to track video views inside flite.
+	 * Used specifically for the 'content-begin' event.
+	 * Jira ADEN-295 and VID-517
+	 *
+	 * Call like this from flite:
+	 * window.Wikia.fliteOoyalaTracker( 'video-identifier' );
+	 *
 	 * @param {String} video Identifier for video, either title or id
 	 */
+
 	wikia.fliteOoyalaTracker = function( video ) {
 		tracker.track({
 			action: 'content-begin',
