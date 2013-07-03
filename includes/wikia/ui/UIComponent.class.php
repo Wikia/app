9f71fbce50a309fe874625a74ffebe8cf9bfc868
marzjan <sebastianm@wikia-inc.com>
2013-07-03 10:42:00 +0000

    DAR-810 Added local caching for Template Engine
    Typo fixes
    Added missing self:: for class constant
    Added local cache for template engine to avoid multiple instantiations
    if the component is rendered more than once.

--- includes/wikia/ui/UIComponent.class.php
+++ includes/wikia/ui/UIComponent.class.php
@@ -1,7 +1,13 @@
 <?php
 class UIComponent {
+
 	const EXCEPTION_MSG_INVALID_TEMPLATE = 'Invalid template';
-	
+
+	/**
+	 * @var $templateEngine \Wikia\Template\Engine
+	 */
+	private $templateEngine;
+
 	/**
 	 * @var Array $templateVarsConfig
 	 */
@@ -16,44 +22,45 @@ class UIComponent {
 	 * @var Array data passed to the template
 	 */
 	private $templateData;
-	
+
 	/**
 	 * @desc Sets template variables from configuration
-	 * 
-	 * @param $templateVarsConfing
+	 *
+	 * @param $templateVarsConfig
 	 */
-	public function setTemplateVarsConfig( $templateVarsConfing ) {
-		$this->templateVarsConfig = $templateVarsConfing;
+	public function setTemplateVarsConfig( $templateVarsConfig ) {
+		$this->templateVarsConfig = $templateVarsConfig;
 	}
 
 	/**
 	 * @desc Validates if required values are set and renders component
-	 * 
+	 *
 	 * @param $params
 	 * @return string
 	 */
 	public function render( $params ) {
-		$this->setTemplate( $params['type'] );		// set template for rendering
-		$this->setValues( $params['params'] );		// set mustache variables
-		$this->validateTemplateVars();				// check if required vars are set
-		
-		return ( new Wikia\Template\MustacheEngine )
+		$this->setTemplate( $params['type'] ); // set template for rendering
+		$this->setValues( $params['params'] ); // set mustache variables
+		$this->validateTemplateVars(); // check if required vars are set
+
+		return $this->getTemplateEngine()
 			->setData( $this->getValues() )
 			->render( $this->getTemplate() );
 	}
 
 	/**
-	 * @param String $template "sub template" name of the component (i.e. a button component can have three 
-	 * "sub templates": button_input.mustache, button_link.mustache and button_button.mustache the part between _ and . 
+	 * @param String $template "sub template" name of the component (i.e. a button component can have three
+	 * "sub templates": button_input.mustache, button_link.mustache and button_button.mustache the part between _ and .
 	 * is a "sub template" name
+	 * @throws Exception
 	 */
 	private function setTemplate( $template ) {
 		$mustacheTplPath = $this->templateVarsConfig['templatePath'] . '_' . $template . '.mustache';
-		
-		if( file_exists( $mustacheTplPath) ) {
+
+		if ( file_exists( $mustacheTplPath ) ) {
 			$this->templatePath = $mustacheTplPath;
 		} else {
-			throw new Exception( EXCEPTION_MSG_INVALID_TEMPLATE );
+			throw new Exception( self::EXCEPTION_MSG_INVALID_TEMPLATE );
 		}
 	}
 
@@ -64,7 +71,7 @@ class UIComponent {
 	public function setValues( Array $varsArray ) {
 		$this->templateData = $varsArray;
 	}
-	
+
 	public function getValues() {
 		return $this->templateData;
 	}
@@ -73,5 +80,16 @@ class UIComponent {
 		// TODO: this method will be implemented during work on DAR-935
 		return true;
 	}
-	
+
+	/**
+	 * @return \Wikia\Template\Engine
+	 */
+	private function getTemplateEngine() {
+		if ( empty( $this->templateEngine ) ) {
+			$this->templateEngine = new Wikia\Template\MustacheEngine;
+		}
+
+		return $this->templateEngine;
+	}
+
 }
