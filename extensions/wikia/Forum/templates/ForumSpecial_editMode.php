1d71a8d73b1d76252ee69dcbe4f4942b634f6035
Bartosz Bentkowski <bartosz.bentkowski@gmail.com>
2013-07-05 11:02:47 +0000

    i18n support

--- extensions/wikia/Forum/templates/ForumSpecial_editMode.php
+++ extensions/wikia/Forum/templates/ForumSpecial_editMode.php
@@ -1,14 +1,16 @@
 <section id="ForumBoardEdit" class="Forum ForumBoardEdit">
 	<header>
 		<button id="CreateNewBoardButton" class="create-new-board-button">
-			<?= wfMsg('forum-admin-create-new-board-label') ?>
+			<?= wfMessage('forum-admin-create-new-board-label')->text() ?>
 		</button>
 		<div class="BreadCrumbs">
-			<a href="<?= $wg->Title->getLocalUrl() ?>"><?= wfMsg('forum-forum-title') ?></a>
+			<a href="<?= $wg->Title->getLocalUrl() ?>"><?= wfMessage('forum-forum-title')->text() ?></a>
 			<span class="separator">&gt;</span>
-			<?= wfMsg('forum-admin-page-breadcrumb') ?>
+			<?= wfMessage('forum-admin-page-breadcrumb')->text() ?>
 		</div>
 	</header>
-	<div class="box">For easier editing, you can see included templates in edit mode.</div>
+	<div>
+		<?= wfMessage('forum-admin-page-editmode-templates-included')->text() ?>
+	</div>
 	<?= $app->renderPartial('ForumSpecial', 'boards', array('boards' => $boards, 'isEditMode' => true ) ) ?>
 </section>
