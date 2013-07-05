6ca8bbfda0798890cc80f18ff0d05555546a009c
Kamil Koterba <kamil@wikia-inc.com>
2013-07-05 08:35:32 +0000

    CE-63 Adding escaping for messages directly passed to html

--- extensions/wikia/CreateNewWiki/templates/CreateNewWiki_Index.php
+++ extensions/wikia/CreateNewWiki/templates/CreateNewWiki_Index.php
@@ -3,36 +3,36 @@
 ?>
 <section id="CreateNewWiki">
 	<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-	<h1><?= wfMessage('cnw-title')->text() ?></h1>
+	<h1><?= wfMessage('cnw-title')->escaped() ?></h1>
 	<ol class="steps">
 		<li id="NameWiki" class="step">
-			<h2><?= wfMessage('cnw-name-wiki-headline')->text() ?></h2>
-			<p class="creative"><?= wfMessage('cnw-name-wiki-creative')->text() ?></p>
+			<h2><?= wfMessage('cnw-name-wiki-headline')->escaped() ?></h2>
+			<p class="creative"><?= wfMessage('cnw-name-wiki-creative')->escaped() ?></p>
 			<form name="label-wiki-form">
-				<label for="wiki-name"><?= wfMessage('cnw-name-wiki-label')->text() ?></label>
+				<label for="wiki-name"><?= wfMessage('cnw-name-wiki-label')->escaped() ?></label>
 				<span class="wiki-name-status-icon status-icon"></span>
-				<input type="text" name="wiki-name" value="<?= empty($params['wikiName']) ? '' : $params['wikiName'] ?>"> <?= wfMessage('cnw-name-wiki-wiki')->text() ?>
+				<input type="text" name="wiki-name" value="<?= empty($params['wikiName']) ? '' : $params['wikiName'] ?>"> <?= wfMessage('cnw-name-wiki-wiki')->escaped() ?>
 				<div class="wiki-name-error error-msg"></div>
-				<label for="wiki-domain" dir="ltr"><?= wfMessage('cnw-name-wiki-domain-label')->text() ?></label>
+				<label for="wiki-domain" dir="ltr"><?= wfMessage('cnw-name-wiki-domain-label')->escaped() ?></label>
 				<div class="wiki-domain-container">
 					<span class="domain-status-icon status-icon"></span>
 					<span class="domain-country"><?= empty($selectedLang) || $selectedLang === 'en' ? '' : $selectedLang.'.' ?></span>
-					<?= wfMessage('cnw-name-wiki-language')->text() ?>
-					<input type="text" name="wiki-domain" value="<?= empty($params['wikiDomain']) ? '' : $params['wikiDomain'] ?>"> <?= wfMessage('cnw-name-wiki-domain')->text() ?>
+					<?= wfMessage('cnw-name-wiki-language')->escaped() ?>
+					<input type="text" name="wiki-domain" value="<?= empty($params['wikiDomain']) ? '' : $params['wikiDomain'] ?>"> <?= wfMessage('cnw-name-wiki-domain')->escaped() ?>
 				</div>
 				<div class="wiki-domain-error error-msg"></div>
 				<div class="language-default">
-					<?= wfMessage('cnw-desc-default-lang', Language::getLanguageName($selectedLang) )->text() ?> - <a href="#" id="ChangeLang"><?= wfMessage('cnw-desc-change-lang')->text() ?></a>
+					<?= wfMessage('cnw-desc-default-lang', Language::getLanguageName($selectedLang) )->escaped() ?> - <a href="#" id="ChangeLang"><?= wfMessage('cnw-desc-change-lang')->escaped() ?></a>
 				</div>
 				<div class="language-choice">
-					<label for="wiki-language"><?= wfMessage('cnw-desc-lang')->text() ?></label>
+					<label for="wiki-language"><?= wfMessage('cnw-desc-lang')->escaped() ?></label>
 					<select name="wiki-language">
 
 					<?php
 		$isSelected = false;
 		if (!empty($aTopLanguages) && is_array($aTopLanguages)) :
 	?>
-					<optgroup label="<?= wfMessage('autocreatewiki-language-top', count($aTopLanguages))->text() ?>">
+					<optgroup label="<?= wfMessage('autocreatewiki-language-top', count($aTopLanguages))->escaped() ?>">
 	<?php foreach ($aTopLanguages as $sLang) :
 			$selected = '';
 			if ( empty($isSelected) && $sLang == $selectedLang ) {
@@ -44,7 +44,7 @@
 	<?php endforeach ?>
 					</optgroup>
 	<?php endif ?>
-					<optgroup label="<?= wfMessage('autocreatewiki-language-all')->text() ?>">
+					<optgroup label="<?= wfMessage('autocreatewiki-language-all')->escaped() ?>">
 	<?php if (!empty($aLanguages) && is_array($aLanguages)) : ?>
 	<?php
 		ksort($aLanguages);
@@ -64,7 +64,7 @@
 				</div>
 				<nav class="next-controls">
 					<span class="submit-error error-msg"></span>
-					<input type="button" value="<?= wfMessage('cnw-next')->text() ?>" class="next">
+					<input type="button" value="<?= wfMessage('cnw-next')->escaped() ?>" class="next">
 				</nav>
 			</form>
 		</li>
@@ -73,29 +73,29 @@
 		if($wg->ComboAjaxLogin) {
 ?>
 		<li id="Auth" class="step">
-			<h2 class="headline login"><?= wfMessage('cnw-auth-headline')->text() ?></h2>
-			<h2 class="headline signup"><?= wfMessage('cnw-auth-headline2')->text() ?></h2>
-			<p class="creative login"><?= wfMessage('cnw-auth-creative')->text() ?></p>
-			<p class="creative signup"><?= wfMessage('cnw-auth-signup-creative')->text() ?></p>
+			<h2 class="headline login"><?= wfMessage('cnw-auth-headline')->escaped() ?></h2>
+			<h2 class="headline signup"><?= wfMessage('cnw-auth-headline2')->escaped() ?></h2>
+			<p class="creative login"><?= wfMessage('cnw-auth-creative')->escaped() ?></p>
+			<p class="creative signup"><?= wfMessage('cnw-auth-signup-creative')->escaped() ?></p>
 			<?= AjaxLoginForm::getTemplateForCombinedForms()->render('ComboAjaxLogin') ?>
 			<p class="signup-msg">
-				<?= wfMessage('cnw-signup-prompt')->text() ?> <a href="#"><?= wfMessage('cnw-call-to-signup')->text() ?></a>
+				<?= wfMessage('cnw-signup-prompt')->escaped() ?> <a href="#"><?= wfMessage('cnw-call-to-signup')->escaped() ?></a>
 			</p>
 			<p class="login-msg">
-				<?= wfMessage('cnw-login-prompt')->text() ?> <a href="#"><?= wfMessage('cnw-call-to-login')->text() ?></a>
+				<?= wfMessage('cnw-login-prompt')->escaped() ?> <a href="#"><?= wfMessage('cnw-call-to-login')->escaped() ?></a>
 				<br>
-				<?php print '<fb:login-button id="fbAjaxSignupConnect" size="large" length="short"'.FBConnect::getPermissionsAttribute().FBConnect::getOnLoginAttribute().'>'.wfMessage('cnw-auth-facebook-signup')->text().'</fb:login-button>'; ?>
+				<?php print '<fb:login-button id="fbAjaxSignupConnect" size="large" length="short"'.FBConnect::getPermissionsAttribute().FBConnect::getOnLoginAttribute().'>'.wfMessage('cnw-auth-facebook-signup')->escaped().'</fb:login-button>'; ?>
 			</p>
 			<div class="facebook login">
-				<span>- <?= wfMessage('cnw-or')->text() ?> -</span>
-				<?php print '<fb:login-button id="fbAjaxLoginConnect" size="large" length="short"'.FBConnect::getPermissionsAttribute().FBConnect::getOnLoginAttribute().'>'.wfMessage('cnw-auth-facebook-login')->text().'</fb:login-button>'; ?>
+				<span>- <?= wfMessage('cnw-or')->escaped() ?> -</span>
+				<?php print '<fb:login-button id="fbAjaxLoginConnect" size="large" length="short"'.FBConnect::getPermissionsAttribute().FBConnect::getOnLoginAttribute().'>'.wfMessage('cnw-auth-facebook-login')->escaped().'</fb:login-button>'; ?>
 			</div>
 			<nav class="back-controls">
-				<input type="button" value="<?= wfMessage('cnw-back')->text() ?>" class="secondary back">
+				<input type="button" value="<?= wfMessage('cnw-back')->escaped() ?>" class="secondary back">
 			</nav>
 			<nav class="next-controls">
-				<input type="button" value="<?= wfMessage('cnw-login')->text() ?>" class="login">
-				<input type="button" value="<?= wfMessage('cnw-signup')->text() ?>" class="signup">
+				<input type="button" value="<?= wfMessage('cnw-login')->escaped() ?>" class="login">
+				<input type="button" value="<?= wfMessage('cnw-signup')->escaped() ?>" class="signup">
 			</nav>
 		</li>
 <?php
@@ -103,17 +103,17 @@
 		else if($wg->EnableUserLoginExt){
 ?>
 	<li id="UserAuth" class="step">
-		<h2 class="headline"><?= wfMessage('cnw-userauth-headline')->text() ?></h2>
-		<p class="creative"><?= wfMessage('cnw-userauth-creative')->text() ?></p>
+		<h2 class="headline"><?= wfMessage('cnw-userauth-headline')->escaped() ?></h2>
+		<p class="creative"><?= wfMessage('cnw-userauth-creative')->escaped() ?></p>
 		<div class="signup-loginmodal"><?= F::app()->sendRequest('UserLoginSpecial', 'modal') ?></div>
 		<div class="signup-marketing">
-			<h3><?= wfMessage('cnw-userauth-marketing-heading')->text() ?></h3>
+			<h3><?= wfMessage('cnw-userauth-marketing-heading')->escaped() ?></h3>
 			<p><?= wfMessage('cnw-userauth-marketing-body')->parse() ?></p>
 			<form method="post" action="<?= $signupUrl ?>" id="SignupRedirect">
 				<input type="hidden" name="returnto" value="">
 				<input type="hidden" name="redirected" value="true">
 				<input type="hidden" name="uselang" value="<?= $params['wikiLanguage'] ?>">
-				<input type="submit" value="<?= wfMessage('cnw-userauth-signup-button')->text() ?>">
+				<input type="submit" value="<?= wfMessage('cnw-userauth-signup-button')->escaped() ?>">
 			</form>
 		</div>
 	</li>
@@ -123,26 +123,26 @@
 ?>
 		<li id="DescWiki" class="step">
 			<h2><?= wfMessage('cnw-desc-headline') ?></h2>
-			<p class="creative"><?= wfMessage('cnw-desc-creative')->text() ?></p>
+			<p class="creative"><?= wfMessage('cnw-desc-creative')->escaped() ?></p>
 			<form name="desc-form">
-				<textarea id="Description" placeholder="<?= wfMessage('cnw-desc-placeholder')->text() ?>"></textarea>
+				<textarea id="Description" placeholder="<?= wfMessage('cnw-desc-placeholder')->escaped() ?>"></textarea>
 				<ol>
 					<li>
-						<?= wfMessage('cnw-desc-tip1')->text() ?>
-						<div class="tip-creative"><?= wfMessage('cnw-desc-tip1-creative')->text() ?></div>
+						<?= wfMessage('cnw-desc-tip1')->escaped() ?>
+						<div class="tip-creative"><?= wfMessage('cnw-desc-tip1-creative')->escaped() ?></div>
 					</li>
 					<li>
-						<?= wfMessage('cnw-desc-tip2')->text() ?>
-						<div class="tip-creative"><?= wfMessage('cnw-desc-tip2-creative')->text() ?></div>
+						<?= wfMessage('cnw-desc-tip2')->escaped() ?>
+						<div class="tip-creative"><?= wfMessage('cnw-desc-tip2-creative')->escaped() ?></div>
 					</li>
 					<li>
-						<?= wfMessage('cnw-desc-tip3')->text() ?>
-						<div class="tip-creative"><?= wfMessage('cnw-desc-tip3-creative')->text() ?></div>
+						<?= wfMessage('cnw-desc-tip3')->escaped() ?>
+						<div class="tip-creative"><?= wfMessage('cnw-desc-tip3-creative')->escaped() ?></div>
 					</li>
 				</ol>
-				<label for="wiki-category"><?= wfMessage('cnw-desc-choose')->text() ?></label>
+				<label for="wiki-category"><?= wfMessage('cnw-desc-choose')->escaped() ?></label>
 				<select name="wiki-category">
-					<option value=""><?= wfMessage('cnw-desc-select-one')->text() ?></option>
+					<option value=""><?= wfMessage('cnw-desc-select-one')->escaped() ?></option>
 	<?php
 		foreach ($aCategories as $iCat => $catData) {
 			$sCatName = $catData["name"];
@@ -153,7 +153,7 @@
 	<?php
 		}
 	?>
-					<option value="3"><?= wfMessage('autocreatewiki-category-other')->text() ?></option>
+					<option value="3"><?= wfMessage('autocreatewiki-category-other')->escaped() ?></option>
 				</select>
 				<div class="checkbox">
 					<input type="checkbox" name="all-ages" value="1">
@@ -161,30 +161,30 @@
 						'WikiaStyleGuideTooltipIcon',
 						'index',
 						array(
-							'text' => wfMessage('cnw-desc-all-ages')->text(),
-							'tooltipIconTitle' => wfMessage('cnw-desc-tip-all-ages')->text(),
+							'text' => wfMessage('cnw-desc-all-ages')->escaped(),
+							'tooltipIconTitle' => wfMessage('cnw-desc-tip-all-ages')->escaped(),
 						)
 					);
 					?>
 				</div>
 
 				<nav class="back-controls">
-					<input type="button" value="<?= wfMessage('cnw-back')->text() ?>" class="secondary back">
+					<input type="button" value="<?= wfMessage('cnw-back')->escaped() ?>" class="secondary back">
 				</nav>
 				<nav class="next-controls">
 					<span class="submit-error error-msg"></span>
-					<input type="button" value="<?= wfMessage('cnw-next')->text() ?>" class="next">
+					<input type="button" value="<?= wfMessage('cnw-next')->escaped() ?>" class="next">
 				</nav>
 			</form>
 		</li>
 		<li id="ThemeWiki" class="step">
-			<h2><?= wfMessage('cnw-theme-headline')->text() ?></h2>
-			<p class="creative"><?= wfMessage('cnw-theme-creative')->text() ?></p>
+			<h2><?= wfMessage('cnw-theme-headline')->escaped() ?></h2>
+			<p class="creative"><?= wfMessage('cnw-theme-creative')->escaped() ?></p>
 			<?= F::app()->renderView('ThemeDesigner', 'ThemeTab') ?>
-			<p class="instruction creative"><?= wfMessage('cnw-theme-instruction')->text() ?></p>
+			<p class="instruction creative"><?= wfMessage('cnw-theme-instruction')->escaped() ?></p>
 			<nav class="next-controls">
 				<span class="submit-error finish-status"></span>
-				<input type="button" value="<?= wfMessage('cnw-next')->text() ?>" class="next" disabled>
+				<input type="button" value="<?= wfMessage('cnw-next')->escaped() ?>" class="next" disabled>
 			</nav>
 		</li>
 	</ol>
@@ -204,13 +204,13 @@
 </section>
 <script>
 	WikiBuilderCfg = {
-		'name-wiki-submit-error':'<?= wfMessage('cnw-name-wiki-submit-error')->text() ?>',
-		'desc-wiki-submit-error':'<?= wfMessage('cnw-desc-wiki-submit-error')->text() ?>',
+		'name-wiki-submit-error':'<?= wfMessage('cnw-name-wiki-submit-error')->escaped() ?>',
+		'desc-wiki-submit-error':'<?= wfMessage('cnw-desc-wiki-submit-error')->escaped() ?>',
 		'currentstep':'<?= $currentStep ?>',
 		'skipwikiaplus':'<?= $skipWikiaPlus ?>',
-		'descriptionplaceholder':'<?= wfMessage('cnw-desc-placeholder')->text() ?>',
-		'cnw-error-general':'<?= wfMessage('cnw-error-general')->text() ?>',
-		'cnw-error-general-heading':'<?= wfMessage('cnw-error-general-heading')->text() ?>',
+		'descriptionplaceholder':'<?= wfMessage('cnw-desc-placeholder')->escaped() ?>',
+		'cnw-error-general':'<?= wfMessage('cnw-error-general')->escaped() ?>',
+		'cnw-error-general-heading':'<?= wfMessage('cnw-error-general-heading')->escaped() ?>',
 		'cnw-keys': <?= json_encode($keys) ?>
 	};
 	var themes = <?= json_encode($wg->OasisThemes) ?>;
