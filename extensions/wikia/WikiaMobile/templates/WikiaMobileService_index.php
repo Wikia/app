c5be0dbd6129bd52c181ff1adcd4fd4c98b1f242
hakubo <bukaj.kelo+github@gmail.com>
2013-07-16 12:29:37 +0200

    WikiaMobile: Since the skin is optimized for smaller screens width in viewport tag is not necessary, this also fixes letterboxing our site on iphone 5....

--- extensions/wikia/WikiaMobile/templates/WikiaMobileService_index.php
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileService_index.php
@@ -34,7 +34,7 @@
 	<?endif; ?>
 	<meta name=HandheldFriendly content=true>
 	<meta name=MobileOptimized content=width>
-	<meta name=viewport content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name=viewport content="initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
 	<meta name=apple-mobile-web-app-capable content=yes>
 	<? if ( !empty( $smartBannerConfig ) ) : ?>
 		<? foreach( $smartBannerConfig['meta'] as $name => $content ) : ?>
