24ef4dae7dfe3fd219cb4ffaeeb2e3778be3886d
Kenneth Kouot <kenneth@wikia-inc.com>
2013-07-22 23:26:12 +0000

    update comments accordingly

--- extensions/wikia/EditPageLayout/js/modules/InsertMiniEditor.js
+++ extensions/wikia/EditPageLayout/js/modules/InsertMiniEditor.js
@@ -7,12 +7,10 @@
 			'InsertImage'
 		];
 
-		// hide add video button if is true and user does not have apppropriate rights
+		// for wgAllVideosAdminOnly
 		if (window.showAddVideoBtn) {
-			// using ES5 Array.indexOf -- current browser support includes ES5 support, but be aware for future bug reports
 			items.splice( 1, 0, "InsertVideo" );
 		}
-
 
 		WE.modules.InsertMiniEditor = $.createClass(WE.modules.Insert, {
 				items: items,
