2048de3bd0c06ff9f550688ba27bc9b60f43744e
jursza <jacek@wikia-inc.com>
2013-07-23 15:29:09 +0200

    adding --multi mode

--- maintenance/wikia/EntitySets/setWikiTopicsInWF.php
+++ maintenance/wikia/EntitySets/setWikiTopicsInWF.php
@@ -13,16 +13,16 @@ ini_set('error_reporting', E_NOTICE);
 require_once( dirname( __FILE__ ) . '/../../../extensions/wikia/PageClassification/EntityAPIClient.class.php' );
 require_once( dirname( __FILE__ ) . '/../../../extensions/wikia/PageClassification/CommonPrefix.class.php' );
 require_once( dirname( __FILE__ ) . '/../../../extensions/wikia/PageClassification/ImportantArticles.class.php' );
+require_once( dirname( __FILE__ ) . '/../../../extensions/wikia/PageClassification/PageClassificationData.class.php' );
 require_once( dirname( __FILE__ ) . '/../../Maintenance.php' );
 require_once( dirname( __FILE__ ) . '/../../../includes/HttpFunctions.php' );
 
 class SetWikiTopicsInWF extends Maintenance {
 
-	const WF_VARIABLE_NAME = 'wgWikiVideoSearchTopics';
+	const WF_VARIABLE_NAME = 'wgWikiVideoSearchTopicsAutomated';
 	protected $wikiDomain = null;
 	protected $apiClient = null;
 	protected $wikiId = 0;
-	protected $wfWikiTopics = null;
 
 	/**
 	 * @var string [add|overwrite|remove]
@@ -30,51 +30,72 @@ class SetWikiTopicsInWF extends Maintenance {
 	protected $mode = "add";
 	protected $modeOptions = array( "add", "overwrite", "remove" );
 
+
+	/**
+	 * @var bool [if true, script apply changes to all categorized wikis]
+	 */
+	protected $multi = false;
+
+
 	function __construct() {
 		$this->apiClient = new EntityAPIClient();
 		$this->wikiId = $_SERVER['SERVER_ID'];
-
 		return parent::__construct();
-	}
-
-	protected function getWfWikiTopics() {
-		if ( $this->wfWikiTopics === null ) {
-			$this->wfWikiTopics = unserialize( WikiFactory::getVarByName( self::WF_VARIABLE_NAME, $this->wikiId )->cv_value );
-			if ( $this->wfWikiTopics === null ) {
-				$this->wfWikiTopics = array();
-			}
-		}
-		return $this->wfWikiTopics;
 	}
 
 	public function execute() {
 		$this->mode = $this->getOption("mode", false);
+
+		// use param --mode=[add|remove|overwrite]
 		if ( !in_array( $this->mode, $this->modeOptions ) ) {
 			die("\n\n * * * Use param --mode=[".implode("|", $this->modeOptions)."] * * * \n\n");
 		}
-		echo "\nExecuting SetWikiTopicsInWF for: " . $this->getWikiDomain() . " | MODE: ".$this->mode."\n";
+
+		// use param --multi=true for running the script for all categorized wikis
+		$this->multi = $this->getOption("multi", false);
+
+		if ( $this->multi === false ) {
+			$this->setWikiTopics( $this->wikiId );
+		} else {
+			$classificationData = new PageClassificationData();
+			$wikilist = $classificationData->getWikiList();
+			foreach ( $wikilist as $item ) {
+				$this->setWikiTopics( $item['wikiId'] );
+			}
+		}
+	}
+
+	protected function getWfWikiTopics( $wikiId ) {
+		$topics =  unserialize( WikiFactory::getVarByName( self::WF_VARIABLE_NAME, $wikiId )->cv_value );
+		if ( !is_array( $topics ) || $topics == false || empty($topics) ) {
+			$topics = array();
+		}
+		return $topics;
+	}
+
+	protected function setWikiTopics( $wikiId ) {
+		echo "\nExecuting SetWikiTopicsInWF for: " . $this->getWikiDBName( $wikiId ) . " | MODE: ".$this->mode."\n";
 
 		if ( $this->mode != "remove" ) {
-			$important = new ImportantArticles( $this->wikiId );
+			$important = new ImportantArticles( $wikiId );
 			$phrases = $important->getImportantPhrasesAsList();
 
 			echo "\nWikiTopics are: " . $phrases . "\n";
 
 			$phrasesArray = explode(", ", $phrases);
-			$phrasesArray[] = 'test';
-			$this->appendTopicsToWF( $phrasesArray );
+			$this->appendTopicsToWF( $phrasesArray, $wikiId );
 
 		} else {
 
-			WikiFactory::setVarByName( self::WF_VARIABLE_NAME, $this->wikiId, array() );
+			WikiFactory::setVarByName( self::WF_VARIABLE_NAME, $wikiId, array() );
 		}
 		echo "\n\nDONE.\n";
 	}
 
-	protected function appendTopicsToWF( $phrases ) {
+	protected function appendTopicsToWF( $phrases, $wikiId ) {
 
 		if ( $this->mode == "add" ) {
-			$wfWikiTopics = $this->getWfWikiTopics();
+			$wfWikiTopics = $this->getWfWikiTopics( $wikiId );
 			foreach ( $phrases as $phrase ) {
 				if ( !in_array( $phrase, $wfWikiTopics ) ) {
 					$wfWikiTopics[] = $phrase;
@@ -87,21 +108,9 @@ class SetWikiTopicsInWF extends Maintenance {
 		}
 	}
 
-
-	public function setWikiDomain( $wikiDomain ) {
-		$this->wikiDomain = $wikiDomain;
+	public function getWikiDBName( $wikiId ) {
+			return WikiFactory::IDtoDB( $wikiId );
 	}
-
-	public function getWikiDomain() {
-		global $wgDBname;
-
-		if ( empty( $this->wikiDomain ) ) {
-			$this->setWikiDomain( WikiFactory::DBtoDomain( $wgDBname ) );
-		}
-		return $this->wikiDomain;
-	}
-
-
 
 }
 
