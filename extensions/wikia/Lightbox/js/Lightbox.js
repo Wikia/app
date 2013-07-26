7ac816c97be2341c6742b5321a10351788bd5009
saipetch <saipetch@wikia-inc.com>
2013-07-26 11:56:13 -0700

    coding convention

--- extensions/wikia/Lightbox/js/Lightbox.js
+++ extensions/wikia/Lightbox/js/Lightbox.js
@@ -653,7 +653,9 @@ var Lightbox = {
 		var overlay = Lightbox.openModal;
 
 		// If an interstitial ad is being shown, do not hideOverlay
-		if (Lightbox.ads.adIsShowing) return;
+		if (Lightbox.ads.adIsShowing) {
+			return;
+		}
 
 		if(!overlay.hasClass('overlay-hidden') && overlay.data('overlayactive')) {
 			clearTimeout(Lightbox.eventTimers.overlay);
