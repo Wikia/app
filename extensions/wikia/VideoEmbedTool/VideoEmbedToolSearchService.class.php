5b7664b38917c8f36a1b4625d97f3595a007e0eb
Robert Elwell <robert@wikia-inc.com>
2013-07-26 20:28:31 +0000

    fix unit test breakage

--- extensions/wikia/VideoEmbedTool/VideoEmbedToolSearchService.class.php
+++ extensions/wikia/VideoEmbedTool/VideoEmbedToolSearchService.class.php
@@ -218,7 +218,7 @@ class VideoEmbedToolSearchService
 			             ->setRank( $this->getRank() );
 			$this->config->setFilterQueryByCode( Wikia\Search\Config::FILTER_VIDEO );
 			if ( $this->getSearchType() == 'premium' ) {
-				$this->config->setWikiId( Wikia\Search\QueryService\Select\Video::VIDEO_WIKI_ID );
+				$this->config->setWikiId( Wikia\Search\QueryService\Select\Dismax\Video::VIDEO_WIKI_ID );
 			}
 			
 		}
