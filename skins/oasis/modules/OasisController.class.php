2941000730450a95c0da08267422bad49eda5bdb
hakubo <bukaj.kelo+github@gmail.com>
2013-07-29 11:38:07 +0200

    MOB-167: Link to Ticket for future reference

--- skins/oasis/modules/OasisController.class.php
+++ skins/oasis/modules/OasisController.class.php
@@ -90,6 +90,7 @@ class OasisController extends WikiaController {
 		wfProfileIn(__METHOD__);
 
 		//Add Smart banner for My Wikia App
+		//See: https://wikia-inc.atlassian.net/browse/MOB-167
 		$wgOut->addHeadItem('My Wikia Smart Banner', '<meta name="apple-itunes-app" content="app-id=623705389">');
 
 		/* set the grid or full width if passed in, otherwise, respect the default */
