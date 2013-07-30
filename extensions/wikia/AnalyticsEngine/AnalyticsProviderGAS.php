f93ae07751fa73b0d705567818ae07a6514b83e6
hakubo <bukaj.kelo+github@gmail.com>
2013-07-30 16:13:24 +0200

    MOB-253: Move analytics to bottom package

--- extensions/wikia/AnalyticsEngine/AnalyticsProviderGAS.php
+++ extensions/wikia/AnalyticsEngine/AnalyticsProviderGAS.php
@@ -26,7 +26,7 @@ class AnalyticsProviderGAS implements iAnalyticsProvider {
 		//should be added unprocessed as per Cardinal Path's request
 		//but screw it, that's an additional single request that adds overhead
 		//and the main experiment is done on Oasis :P
-		$jsHeadPackages[] = 'analytics_gas_js';
+		$jsBodyPackages[] = 'analytics_gas_js';
 		return true;
 	}
 
