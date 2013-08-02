fe4bff980464049f63988cdb4f6c258b4fc66f62
Robert Elwell <robert@wikia-inc.com>
2013-08-02 16:00:10 +0000

    PLA-508 -- put wiki match below result message

--- extensions/wikia/Search/templates/WikiaSearch_index.php
+++ extensions/wikia/Search/templates/WikiaSearch_index.php
@@ -22,9 +22,6 @@
 		<?php echo $tabs; ?>
 
 		<div class="results-wrapper grid-3 alpha">
-			<?php if(!empty($wikiMatch)):?>
-				<?=$wikiMatch?>
-			<?php endif; ?>
 			<?php if(!empty($results)): ?>
 				<?php if( $resultsFound > 0 ): ?>
 					<p class="result-count subtle">
@@ -33,6 +30,9 @@
 						<?php else: ?>
 							<?= wfMsg('wikiasearch2-results-for', '<strong>'.$query.'</strong>'); ?>
 						<?php endif; ?>
+						<?php if(!empty($wikiMatch)):?>
+							<?=$wikiMatch?>
+						<?php endif; ?>			
 						<?php if ( isset($hub) && $hub ) : ?>
 							<?= wfMsg('wikiasearch2-onhub', $hub)?>
 							|
