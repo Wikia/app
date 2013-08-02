d8208e4f8999812276e631cd8d186224650d816a
liz <liz@wikia-inc.com>
2013-08-02 23:39:58 +0000

    VID-648 adding new sprite images and css for updated LVS callout

---
+++ extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_callout.php
@@ -0,0 +1,12 @@
+<section class="lvs-callout">
+	<button class="close wikia-chiclet-button">
+		<img src="<?= $wgStylePath ?>/oasis/images/icon_close.png">
+	</button>
+	<h1><?= wfMessage( 'lvs-callout-header' )->plain() ?></h1>
+	<ul>
+		<li>- <?= wfMessage( 'lvs-callout-reason-licensed' )->parse() ?></li>
+		<li>- <?= wfMessage( 'lvs-callout-reason-quality' )->parse() ?></li>
+		<li>- <?= wfMessage( 'lvs-callout-reason-collaborative' )->parse() ?></li>
+		<li>- <?= wfMessage( 'lvs-callout-reason-more' )->plain() ?></li>
+	</ul>
+</section>
