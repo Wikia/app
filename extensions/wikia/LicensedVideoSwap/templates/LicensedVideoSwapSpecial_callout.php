24bc1ddc869e9454e917a4aca0b88beea223ae69
lizlux <liz@lizlux.net>
2013-08-02 17:20:26 -0700

    VID-648 styles for new version of LVS callout

--- extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_callout.php
+++ extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_callout.php
@@ -2,11 +2,23 @@
 	<button class="close wikia-chiclet-button">
 		<img src="<?= $wgStylePath ?>/oasis/images/icon_close.png">
 	</button>
-	<h1><?= wfMessage( 'lvs-callout-header' )->plain() ?></h1>
+	<p><?= wfMessage( 'lvs-callout-header' )->plain() ?></p>
+
+
 	<ul>
-		<li>- <?= wfMessage( 'lvs-callout-reason-licensed' )->parse() ?></li>
-		<li>- <?= wfMessage( 'lvs-callout-reason-quality' )->parse() ?></li>
-		<li>- <?= wfMessage( 'lvs-callout-reason-collaborative' )->parse() ?></li>
-		<li>- <?= wfMessage( 'lvs-callout-reason-more' )->plain() ?></li>
+		<li>
+			<?= wfMessage( 'lvs-callout-title-licensed' )->parse() ?>
+			<?= wfMessage( 'lvs-callout-reason-licensed' )->parse() ?>
+		</li>
+		<li>
+			<?= wfMessage( 'lvs-callout-title-quality' )->parse() ?>
+			<?= wfMessage( 'lvs-callout-reason-quality' )->parse() ?>
+		</li>
+		<li>
+			<?= wfMessage( 'lvs-callout-title-collaborative' )->parse() ?>
+			<?= wfMessage( 'lvs-callout-reason-collaborative' )->parse() ?>
+		</li>
 	</ul>
+
+	<p><?= wfMessage( 'lvs-callout-reason-more' )->plain() ?></p>
 </section>
