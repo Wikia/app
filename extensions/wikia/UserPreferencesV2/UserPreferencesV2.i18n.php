e42e44ca3e804370263b6aebe3bb4eb5f0eb9ef4
tor <tor@wikia-inc.com>
2013-08-06 16:59:37 +0000

    a bit more verbose qqq msg

--- extensions/wikia/UserPreferencesV2/UserPreferencesV2.i18n.php
+++ extensions/wikia/UserPreferencesV2/UserPreferencesV2.i18n.php
@@ -65,6 +65,6 @@ $messages['en'] = array(
 
 $messages['qqq'] = array(
 	'preferences-v2-redirect-explanation' => '$1 is the URL of the site, e.g. www.wikia.com',
-	'preferences-v2-fancysig' => 'Message text for checkbox for enabling wikitext in signature. Includes localized namespace to Help:Signature as a link',
+	'preferences-v2-fancysig' => 'Message text for checkbox for enabling wikitext in signature. Includes localized message with link to Help:Signature (or equivalent). This link needs to be in the content language (language of the wiki) hence the MediaWiki namespace transclusion.',
 	'preferences-v2-fancysig-help-page-name' => 'The translated version of the namespace based on local wiki language: Help:Signature for wikis in English'
 );
