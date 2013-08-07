1325946344a3c07cb5a481d48dee53716abfcaa5
marzjan <sebastianm@wikia-inc.com>
2013-08-07 14:47:55 +0000

    DAR-1498 Corporate Pages Checks, WIP
    - added method skeletons for first implementation
    - implemented pulling of corporate page content

--- maintenance/wikia/checks/homePagesChecks.php
+++ maintenance/wikia/checks/homePagesChecks.php
@@ -11,31 +11,93 @@
 ini_set( "include_path", dirname( __FILE__ ) . "/../.." );
 require_once( 'commandLine.inc' );
 
-echo date( "Y-m-d H:i:s" );
+echo 'Execution starts ' . date( "Y-m-d H:i:s" );
 echo "\n";
-echo date( "Y-m-d H:i:s" );
-echo "\nScript finished running!\n";
 
 $homePageTest = new CorporateHomePageTest();
 $homePageTest->run_tests();
 
+echo 'Execution ends' . date( "Y-m-d H:i:s" );
+echo "\nScript finished running!\n";
 
 class CorporateHomePageTest {
+
+	/**
+	 * @var CorporateHomePageStatisticsStorage
+	 */
+	private $dataStorage;
+
+	/**
+	 * @var string
+	 */
+	private $currentDate;
+
+
+	public function __construct() {
+		$this->currentDate = date( "Y-m-d" );
+		$this->dataStorage = new CorporateHomePageStatisticsStorage();
+	}
 
 	public function run_tests() {
 		$cityVisualization = new CityVisualization();
 		$corporateWikis = $cityVisualization->getVisualizationWikisIds();
 
 		foreach ( $corporateWikis as $wikiId ) {
-			$statistics = $this->getStatisticsModulesContents( $wikiId );
+			$this->processCorporateWikiStats( $wikiId );
+		}
+	}
 
+	private function processCorporateWikiStats( $wikiId ) {
+		$corpWikiStatistics = $this->getStatisticsModulesContents( $wikiId );
+		foreach ( $corpWikiStatistics as $statName => $statValue ) {
+			$this->dataStorage->setValue( $this->getStatsKey( $this->currentDate, $wikiId, $statName ), $statValue );
 		}
 	}
 
 	private function getStatisticsModulesContents( $wikiId ) {
-		$html = $this->getHomePageContents( $wikiId );
-		$statistics = $this->extractNumbersFromHtml( $html );
+		$homePageHtml = $this->getHomePageContents( $wikiId );
+		$homePageStatistics = $this->extractNumbersFromHtml( $homePageHtml );
+
+		return $homePageStatistics;
 	}
+
+	private function getStatsKey( $date, $wikiId, $statName ) {
+		return implode( '-', [ $date, $wikiId, $statName ] );
+	}
+
+	private function getHomePageContents( $wikiId ) {
+		$url = unserialize( WikiFactory::getVarById( 5, $wikiId )->cv_value );
+
+		echo "Loading $wikiId - ${url}";
+
+		$htmlContents =file_get_contents($url);
+
+		if ( !empty( $htmlContents ) ) {
+			echo " - success\n";
+		} else {
+			echo " - failed\n";
+			$htmlContents = '';
+		}
+
+		return $htmlContents;
+	}
+
+	/**
+	 * Processes HTML and returns associative array
+	 * of stat => value, i.e.
+	 *
+	 * [
+	 *
+	 *
+	 * ]
+	 *
+	 * @param $html
+	 * @return array
+	 */
+	private function extractNumbersFromHtml( $html ) {
+		return [ ];
+	}
+
 }
 
 /**
@@ -52,7 +114,7 @@ class CorporateHomePageStatisticsStorage {
 	 * @param $value
 	 */
 	public function setValue( $key, $value ) {
-
+		echo "setting $value at $key\n";
 	}
 
 	/**
