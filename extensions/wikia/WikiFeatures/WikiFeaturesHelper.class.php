8986d6fed24e378b780a0d938e37a761a5f1dabb
Michał ‘Mix’ Roszka <michal@roszka.pl>
2013-08-07 15:07:53 +0000

    remove FOGBUGZ_CASE_TAG constant

--- extensions/wikia/WikiFeatures/WikiFeaturesHelper.class.php
+++ extensions/wikia/WikiFeatures/WikiFeaturesHelper.class.php
@@ -11,7 +11,6 @@ class WikiFeaturesHelper extends WikiaModel {
 	protected static $instance = NULL;
 	protected $fogbugzService = NULL;
 
-	const FOGBUGZ_CASE_TAG = 'WikiFeaturesFeedback';
 	const FEEDBACK_FREQUENCY = 60;
 
 	/**
