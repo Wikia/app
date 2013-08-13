03c3e4ef13ce328bee3dda92a0f9cce44efd6515
Rafal Leszczynski <rafal@wikia-inc.com>
2013-08-13 15:31:30 +0200

    DAR-1184, add tests to karma

--- tests/karma/js-unit.config.js
+++ tests/karma/js-unit.config.js
@@ -52,6 +52,9 @@ files = [
 	'resources/wikia/modules/stringhelper.js',
 	'resources/wikia/modules/thumbnailer.js',
 	'resources/wikia/modules/uniqueId.js',
+	//UI Repo JS API
+	'resources/wikia/modules/uicomponent.js',
+	'resources/wikia/modules/uifactory.js',
 	'resources/wikia/modules/spec/*.spec.js',
 
 	//Advertisment
@@ -87,7 +90,8 @@ files = [
 	//Search
 	'extensions/wikia/Search/js/SearchAbTest.js',
 	'extensions/wikia/Search/js/SearchAbTest.*.js',
-	'extensions/wikia/Search/js/spec/*.spec.js'
+	'extensions/wikia/Search/js/spec/*.spec.js',
+
 ];
 
 notCovered = [
