db5c16214df29de5c99f86f4f348c80164bec12f
saipetch <saipetch@wikia-inc.com>
2013-08-16 16:47:09 -0700

    Video (vid-576, vid-578): VideoPageTool - update special page controller and add function to get calendar info

--- extensions/wikia/VideoPageTool/VideoPageTool.i18n.php
+++ extensions/wikia/VideoPageTool/VideoPageTool.i18n.php
@@ -9,8 +9,10 @@ $messages = array();
 
 $messages['en'] = array(
 	'videopagetool-page-title' => 'Video Page Tool',
+	'videopagetool-error-permission' => 'You cannot access this tool.',
 );
 
 $messages['qqq'] = array(
 	'videopagetool-page-title' => 'This is the page header/title (h1 tag) that is displayed at the top of the page.',
+	'videopagetool-error-permission' => 'This text appears if user does not have permission to access the tool.',
 );
--- extensions/wikia/VideoPageTool/VideoPageToolSpecialController.class.php
+++ extensions/wikia/VideoPageTool/VideoPageToolSpecialController.class.php
@@ -23,8 +23,12 @@ class VideoPageToolSpecialController extends WikiaSpecialPageController {
 	 * on GET, calendar template will render
 	 * on POST, form template will render
 	 * @requestParam string region (on POST)
+	 * @requestParam string date [yyyy-mm-dd] (on POST)
 	 * @requestParam string section [featured/trending/fan] (on POST)
 	 * @responseParam array regions - list of regions
+	 * @responseParam string region - current region
+	 * @responseParam string result [ok/error]
+	 * @responseParam string msg - result message
 	 */
 	public function index() {
 		if ( !$this->getUser()->isAllowed( 'videopagetool' ) ) {
@@ -39,8 +43,58 @@ class VideoPageToolSpecialController extends WikiaSpecialPageController {
 		// Change the <title> attribute and the <h1> for the page
 		$this->getContext()->getOutput()->setPageTitle( wfMessage( 'videopagetool-page-title' )->plain() );
 
+		$date = $this->getVal( 'date', date( 'Y-M-d' ) );
+		$region = $this->getVal( 'region', 'en' );
+
+		if ( $this->wg->request->wasPosted() ) {
+			$section = $this->getVal( 'section', 'featured' );
+
+			$this->section = $section;
+		} else {
+			$regions = array(
+				'en' => 'English',
+			);
+			$this->regions = $regions;
+
+			$response = $this->sendSelfRequest( 'getCalendarInfo', array( 'region' => $region, 'date' => $date ) );
+			$this->calendarInfo = $response->getVal( 'info', array() );
+			$this->result = $response->getVal( 'result', '' );
+			$this->msg = $response->getVal( 'msg', '' );
+		}
+
+		$this->date = $date;
+		$this->region = $region;
 	}
 
+	/**
+	 * get calendar info
+	 * @requestParam string region
+	 * @requestParam string date [yyyy-mm-dd]
+	 * @responseParam array info [array( date => status ); status = 0 (not published)/ 1 (published)]
+	 * @responseParam string result [ok/error]
+	 * @responseParam string msg - result message
+	 */
+	public function getCalendarInfo() {
+		if ( !$this->wg->User->isAllowed( 'videopagetool' ) ) {
+			$this->result = 'error';
+			$this->msg = wfMessage( 'videopagetool-error-permission' )->plain();
+			return false;
+		}
+
+		$date = $this->getVal( 'date', date( 'Y-M-d' ) );
+		$region = $this->getVal( 'region', 'en' );
+
+		$info = array(
+			'2013-08-08' => 1,
+			'2013-08-12' => 2,
+			'2013-08-14' => 2,
+			'2013-08-15' => 1,
+		);
+
+		$this->result = 'ok';
+		$this->msg = '';
+		$this->info = $info;
+	}
 
 }
 
