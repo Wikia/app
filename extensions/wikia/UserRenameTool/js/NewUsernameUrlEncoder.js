478abc3416f411f215a3b4ffb4bc6fd05aa37bf9
Adam Karminski <adam.karminski@wikia-inc.com>
2013-08-21 15:50:43 +0000

    Modified NewUsernameUrlEncoder. It is now also responsible for some validation of new username. Considering changing file name to NewUsernameValidator.

--- extensions/wikia/UserRenameTool/js/NewUsernameUrlEncoder.js
+++ extensions/wikia/UserRenameTool/js/NewUsernameUrlEncoder.js
@@ -2,21 +2,55 @@ var NewUsernameUrlEncoder = {
 
 	init: function() {
 		this.input = $( "input[name='newusername']" ),
-			this.output = $( "#newUsernameEncoded>strong" );
+		this.output = $( "#newUsernameEncoded>strong" ),
+		this.newUsername = this.input.val(),
 
+		this.warningsRow = $( "tr#mw-warnings-row" ),
+		this.warningsList = { 'characters' : false, 'maxlength' : false }
+
+		NewUsernameUrlEncoder.bindEvent();
+		NewUsernameUrlEncoder.validate();
+	},
+
+	bindEvent: function() {
 		this.input.on( 'input', function() {
-			NewUsernameUrlEncoder.update();
+			NewUsernameUrlEncoder.newUsername = NewUsernameUrlEncoder.input.val();
+			NewUsernameUrlEncoder.validate();
 		});
 	},
 
-	update: function() {
-		var newUsername = this.input.val(),
-			newUsernameEncoded = encodeURIComponent( newUsername );
+	validate: function() {
+		var newUsernameEncoded = encodeURIComponent( this.newUsername );
 		this.output.html( newUsernameEncoded );
+
+		var forbiddenCharacters = /[@\/\xC2\xA0]/i;
+		this.warningsList[ 'characters' ] = Array.isArray ( this.newUsername.match( forbiddenCharacters ) );
+
+		if ( newUsernameEncoded.length > 255 ) {
+			this.warningsList[ 'maxlength' ] = true;
+		} else {
+			this.warningsList[ 'maxlength' ] = false;
+		}
+
+		this.warnings = 0;
+
+		$.each( this.warningsList, function ( key, value ) {
+			if ( value == true ) {
+				++NewUsernameUrlEncoder.warnings;
+				$( 'li#mw-warnings-list-' + key ).show();
+			} else {
+				$( 'li#mw-warnings-list-' + key ).hide();
+			}
+		});
+
+		if ( this.warnings > 0 ) {
+			this.warningsRow.show();
+		} else {
+			this.warningsRow.hide();
+		}
 	}
 }
 
 $(document).ready( function() {
 	NewUsernameUrlEncoder.init();
-	NewUsernameUrlEncoder.update();
 });
