1a01cffcc6a467fd00680295a1291af3eaf4044b
Adam Karminski <adam.karminski@wikia-inc.com>
2013-08-21 15:50:29 +0000

    Added row for displaying warnings.

--- extensions/wikia/UserRenameTool/templates/rename-form.tmpl.php
+++ extensions/wikia/UserRenameTool/templates/rename-form.tmpl.php
@@ -74,9 +74,14 @@
 					</td>
 				</tr>
 			<?endif;?>
-			<tr id="mw-warnings-row">
+			<tr id="mw-warnings-row" style="display: none;">
 				<td class="mw-label"><?= wfMessage( 'userrenametool-warnings' )->inContentLanguage()->escaped(); ?></td>
-				<td class="mw-input"><ul id="mw-warnings-list"></ul></td>
+				<td class="mw-input">
+					<ul id="mw-warnings-list">
+						<li style="display: none;" id="mw-warnings-list-characters"><?= wfMessage( 'userrenametool-warnings-characters' )->inContentLanguage()->escaped(); ?></li>
+						<li style="display: none;" id="mw-warnings-list-maxlength"><?= wfMessage( 'userrenametool-warnings-maxlength' )->inContentLanguage()->escaped(); ?></li>
+					</ul>
+				</td>
 			</tr>
 		</table>
 	</fieldset>
