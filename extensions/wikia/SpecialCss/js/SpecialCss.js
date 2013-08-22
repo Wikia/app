3fe50cec6a87591cecf697bd68cab216e273ed2b
VForge <bartosz.bentkowski@gmail.com>
2013-08-22 10:19:15 +0000

    DAR-1496: Add tracking

--- extensions/wikia/SpecialCss/js/SpecialCss.js
+++ extensions/wikia/SpecialCss/js/SpecialCss.js
@@ -1,4 +1,42 @@
+
+var trackSpecialCssClick = function (action, label, value, params, event) {
+    Wikia.Tracker.track({
+        category: 'special-css',
+        action: action,
+        browserEvent: event,
+        label: label,
+        trackingMethod: 'both',
+        value: value
+    }, params);
+};
+
 $(function() {
+    // impressions
+    Wikia.Tracker.buildTrackingFunction({
+        category: "special-css",
+        trackingMethod: "both",
+        action: Wikia.Tracker.ACTIONS.IMPRESSION
+    });
+
+    // click on update-items and educational modules
+    $("a[data-tracking]").on("click", function(e) {
+        trackSpecialCssClick(Wikia.Tracker.ACTIONS.CLICK, $(e.target).data('tracking'), null, {}, e);
+    });
+    $("input.css-publish-button").on("click", function(e) {
+        trackSpecialCssClick(Wikia.Tracker.ACTIONS.SUBMIT, 'publish', null, {}, e);
+    });
+    // history and show changes
+    $(".wikia-menu-button-submit a").on("click", function(e) {
+        switch ($(e.target).data('id')) {
+            case 0:
+                trackSpecialCssClick(Wikia.Tracker.ACTIONS.OPEN, 'history', null, {}, e);
+                return;
+            case 1:
+                trackSpecialCssClick(Wikia.Tracker.ACTIONS.OPEN, 'changes', null, {}, e);
+                return;
+        }
+    });
+
 	require(['ace/ace'], function(ace) {
 		var disableBeforeUnload = false;
 		var EDITOR_BOTTOM_MARGIN = 10;
