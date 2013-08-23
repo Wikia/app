a9eb573f8155788a75ecdb0767c5050679b76906
Damian Jozwiak <damian@wikia-inc.com>
2013-08-23 10:49:25 +0000

    Revert "DAR-1648 | UI Drawer component"
    This reverts commit 27116f753832721955083fa457a6ba47adb424fc.

--- resources/wikia/ui_components/drawer/drawer.i18n.php
+++
@@ -1,7 +0,0 @@
-<?
-$messages['en'] = [
-	'styleguide-name-drawer' => 'Drawers',
-	'styleguide-description-drawer' => 'Description for Drawer component in Styleguide.',
-	'styleguide-description-drawer-side' => 'Parameter which describes side on which drawer is sticked. Available values: left/right',
-	'styleguide-description-drawer-content' => 'Parameter - Drawer content',
-];
