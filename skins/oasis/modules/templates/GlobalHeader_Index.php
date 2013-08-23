f084a646b89283ee25700ad9dd44ce8226ec6945
Damian Jozwiak <damian@wikia-inc.com>
2013-08-23 17:11:04 +0200

    DAR-1659 |60ro Browse menu entry point hover state

--- skins/oasis/modules/templates/GlobalHeader_Index.php
+++ skins/oasis/modules/templates/GlobalHeader_Index.php
@@ -12,6 +12,7 @@
 			<a href="#" id="BrowseEntry" class="browse-entry">
 				<span class="chevron-container"></span>
 				<span class="text-container"><?= wfMessage('oasis-global-nav-browse-entry-point')->text() ?></span>
+				<span class="hubname-container"><?= wfMsg('hub-'. $category->cat_name)?></span>
 			</a>
 			<nav>
 				<? if ( $displayHeader ): ?>
