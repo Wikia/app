8002e7927d072a5cee0684e5ac0d84eea438985f
moliwikia <moli@wikia-inc.com>
2013-08-27 10:44:25 +0000

    BAC-691: - added class WikiaLog to send errors to private logs - send all errors (with backtrace) to private log if occurs any problem with uploading images.

--- includes/filerepo/backend/FileOp.php
+++ includes/filerepo/backend/FileOp.php
@@ -98,6 +98,7 @@ abstract class FileOp {
 
 		$n = count( $performOps );
 		if ( $n > self::MAX_BATCH_SIZE ) {
+			WikiaLog::init( 'file', true )->send( 'max_batch_size is exceeded (n = ' . $n . ', max = ' . self::MAX_BATCH_SIZE . ')', $performOps );
 			$status->fatal( 'backend-fail-batchsize', $n, self::MAX_BATCH_SIZE );
 			return $status;
 		}
@@ -111,6 +112,7 @@ abstract class FileOp {
 			$subStatus = $fileOp->precheck( $predicates );
 			$status->merge( $subStatus );
 			if ( !$subStatus->isOK() ) { // operation failed?
+				WikiaLog::init( 'file', true )->send( 'attempt batch failed (' . $index . ')', $performOps );
 				$status->success[$index] = false;
 				++$status->failCount;
 				if ( !$ignoreErrors ) {
@@ -148,6 +150,7 @@ abstract class FileOp {
 				for ( $i = ($index + 1); $i < count( $performOps ); $i++ ) {
 					$performOps[$i]->logFailure( 'attempt_aborted' );
 				}
+				WikiaLog::init( 'file', true )->send( 'attempt batch failed (' . $index . ')', $performOps );
 				return $status; // bail out
 			}
 		}
