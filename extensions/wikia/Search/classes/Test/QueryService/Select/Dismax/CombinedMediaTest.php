1d3f2c80fc6cd1eef6d4bcbabefcd2fbb33e24e2
Robert Elwell <robert@wikia-inc.com>
2013-08-27 18:07:49 +0000

    PLA-539 added unit tests to combinedmedia query service

--- extensions/wikia/Search/classes/Test/QueryService/Select/Dismax/CombinedMediaTest.php
+++ extensions/wikia/Search/classes/Test/QueryService/Select/Dismax/CombinedMediaTest.php
@@ -10,13 +10,15 @@ use Wikia\Search\Test\BaseTest, Wikia\Search\Utilities, \Wikia\Search\QueryServi
 class CombinedMediaTest extends BaseTest
 {
 
+	const CLASSNAME = 'Wikia\Search\QueryService\Select\Dismax\CombinedMedia';
+	
 	/**
 	 * @covers Wikia\Search\QueryService\Select\Dismax\CombinedMedia::getQueryClausesString
 	 */
 	public function testGetQueryClausesStringIncludesPhotos() {
-		$qs = $this->getMockBuilder( 'Wikia\Search\QueryService\Select\Dismax\CombinedMedia' )
+		$qs = $this->getMockBuilder( self::CLASSNAME )
 		           ->disableOriginalConstructor()
-		           ->setMethods( [ 'getConfig' ] )
+		           ->setMethods( [ 'getConfig', 'getTopicsAsQuery' ] )
 		           ->getMock();
 		
 		$config = $this->getMock( 'Wikia\Search\Config' );
@@ -25,6 +27,11 @@ class CombinedMediaTest extends BaseTest
 		    ->expects( $this->once() )
 		    ->method ( "getConfig" )
 		    ->will   ( $this->returnValue( $config ) )
+		;
+		$qs
+		    ->expects( $this->once() )
+		    ->method ( 'getTopicsAsQuery' )
+		    ->will   ( $this->returnValue( '"foo" OR "bar"'  ) )
 		;
 		$config
 		    ->expects( $this->once() )
@@ -39,7 +46,7 @@ class CombinedMediaTest extends BaseTest
 		$get = new ReflectionMethod( $qs, 'getQueryClausesString' );
 		$get->setAccessible( true );
 		$this->assertEquals(
-				sprintf( '(+(wid:%d OR wid:123) AND (ns:%d))', Video::VIDEO_WIKI_ID, \NS_FILE ),
+				sprintf( '(+((wid:%d AND ("foo" OR "bar")) OR wid:123) AND (ns:%d))', Video::VIDEO_WIKI_ID, \NS_FILE ),
 				$get->invoke( $qs )
 		);
 	}
@@ -48,9 +55,9 @@ class CombinedMediaTest extends BaseTest
 	 * @covers Wikia\Search\QueryService\Select\Dismax\CombinedMedia::getQueryClausesString
 	 */
 	public function testGetQueryClausesStringNotIncludesPhotos() {
-		$qs = $this->getMockBuilder( 'Wikia\Search\QueryService\Select\Dismax\CombinedMedia' )
+		$qs = $this->getMockBuilder( self::CLASSNAME )
 		           ->disableOriginalConstructor()
-		           ->setMethods( [ 'getConfig' ] )
+		           ->setMethods( [ 'getConfig', 'getTopicsAsQuery' ] )
 		           ->getMock();
 		
 		$config = $this->getMock( 'Wikia\Search\Config' );
@@ -59,6 +66,11 @@ class CombinedMediaTest extends BaseTest
 		    ->expects( $this->once() )
 		    ->method ( "getConfig" )
 		    ->will   ( $this->returnValue( $config ) )
+		;
+		$qs
+		    ->expects( $this->once() )
+		    ->method ( 'getTopicsAsQuery' )
+		    ->will   ( $this->returnValue( '"foo" OR "bar"'  ) )
 		;
 		$config
 		    ->expects( $this->once() )
@@ -73,8 +85,75 @@ class CombinedMediaTest extends BaseTest
 		$get = new ReflectionMethod( $qs, 'getQueryClausesString' );
 		$get->setAccessible( true );
 		$this->assertEquals(
-				sprintf( '(+(wid:%d OR wid:123) AND (ns:%d) AND (is_video:true))', Video::VIDEO_WIKI_ID, \NS_FILE ),
+				sprintf( '(+((wid:%d AND ("foo" OR "bar")) OR wid:123) AND (ns:%d) AND (is_video:true))', Video::VIDEO_WIKI_ID, \NS_FILE ),
 				$get->invoke( $qs )
+		);
+	}
+	
+	/**
+	 * @covers Wikia\Search\QueryService\Select\Dismax\CombinedMedia::getTopicsAsQuery
+	 */
+	public function testGetTopicsAsQueryWithTopics() {
+		$service = $this->getMockBuilder( self::CLASSNAME )
+		                ->disableOriginalConstructor()
+		                ->setMethods( [ 'getService' ] )
+		                ->getMock();
+		
+		$mwService = $this->getMock( 'Wikia\Search\MediaWikiService', [ 'getGlobalWithDefault' ] );
+		
+		$service
+		    ->expects( $this->once() )
+		    ->method ( 'getService' )
+		    ->will   ( $this->returnValue( $mwService ) )
+		;
+		$mwService
+		    ->expects( $this->at( 0 ) )
+		    ->method ( 'getGlobalWithDefault' )
+		    ->with   ( 'WikiVideoSearchTopics', [] )
+		    ->will   ( $this->returnValue( [ 'topic 1', 'topic2' ] ) )
+		;
+		
+		$get = new ReflectionMethod( self::CLASSNAME, 'getTopicsAsQuery' );
+		$get->setAccessible( true );
+		$this->assertEquals(
+				'"topic 1" OR "topic2"',
+				$get->invoke( $service )
+		);
+	}
+	
+	/**
+	 * @covers Wikia\Search\QueryService\Select\Dismax\CombinedMedia::getTopicsAsQuery
+	 */
+	public function testGetTopicsAsQueryNoTopics() {
+		$service = $this->getMockBuilder( self::CLASSNAME )
+		                ->disableOriginalConstructor()
+		                ->setMethods( [ 'getService' ] )
+		                ->getMock();
+		
+		$mwService = $this->getMock( 'Wikia\Search\MediaWikiService', [ 'getGlobal', 'getGlobalWithDefault' ] );
+		
+		$service
+		    ->expects( $this->once() )
+		    ->method ( 'getService' )
+		    ->will   ( $this->returnValue( $mwService ) )
+		;
+		$mwService
+		    ->expects( $this->at( 0 ) )
+		    ->method ( 'getGlobalWithDefault' )
+		    ->with   ( 'WikiVideoSearchTopics', [] )
+		    ->will   ( $this->returnValue( [] ) )
+		;
+		$mwService
+		    ->expects( $this->at( 1 ) )
+		    ->method ( 'getGlobal' )
+		    ->with   ( 'Sitename' )
+		    ->will   ( $this->returnValue( 'Diamond Lane Wiki' ) )
+		;
+		$get = new ReflectionMethod( self::CLASSNAME, 'getTopicsAsQuery' );
+		$get->setAccessible( true );
+		$this->assertEquals(
+				'"diamond lane"',
+				$get->invoke( $service )
 		);
 	}
 	
