a4bfc1dde5b00742222702c30b34459ea0586d98
Damian Jozwiak <damian@wikia-inc.com>
2013-08-27 12:26:25 +0200

    DAR-1658 | Styleguide drawer - fixes after CR

--- resources/wikia/ui_components/drawer/drawer.i18n.php
+++ resources/wikia/ui_components/drawer/drawer.i18n.php
@@ -4,4 +4,5 @@ $messages['en'] = [
 	'styleguide-description-drawer' => 'Description for Drawer component in Styleguide.',
 	'styleguide-description-drawer-side' => 'Parameter which describes side on which drawer is sticked. Available values: left/right',
 	'styleguide-description-drawer-content' => 'Parameter - Drawer content',
+	'styleguide-description-drawer-content' => 'Optional parameter - Drawer HTML container class',
 ];
