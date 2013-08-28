ba860a062dc33cb505af6d7f05d86fea6b98339f
RodriGomez <rodrigo@wikia-inc.com>
2013-08-28 14:43:13 +0200

    PLA-541 PLA-542 PLA-543 PLA-544 PLA-545StatsApiDoc

--- includes/wikia/api/SearchSuggestionsApiController.class.php
+++ includes/wikia/api/SearchSuggestionsApiController.class.php
@@ -1,4 +1,65 @@
 <?php
+
+use Swagger\Annotations as SWG;
+
+/**
+ * @SWG\Resource(
+ *     apiVersion="0.2",
+ *     swaggerVersion="1.1",
+ *     resourcePath="SearchSuggestionsApi",
+ *     basePath="http://muppet.wikia.com"
+ * )
+ */
+
+/**
+ * @SWG\Model(id="SearchSuggestionsPhrases")
+ *     @SWG\Property(
+ *         name="items",
+ *         type="Array",
+ *         description="The list of phrases matching the query",
+ *         items="$ref:SearchSuggestionsItems"
+ *     )
+ * )
+ * @SWG\Model(id="SearchSuggestionsItems")
+ *      @SWG\Property(
+ *         name="title",
+ *         type="string",
+ *         required="true",
+ *         description="Searching article title"
+ *     )
+ * )
+ */
+
+/**
+ *
+ * @SWG\Api(
+ *     path="/wikia.php?controller=SearchSuggestionsApi&method=getList",
+ *     description="Controller to find suggested phrases for chosen query",
+ *     @SWG\Operations(
+ *         @SWG\Operation(
+ *             httpMethod="GET",
+ *             summary="Finds suggested phrases for chosen query",
+ *             nickname="getList",
+ *             responseClass="SearchSuggestionsPhrases",
+ *             @SWG\ErrorResponses(
+ *                 @SWG\ErrorResponse( code="404", reason="Search Suggestions extension not available" )
+ *             ),
+ *             @SWG\Parameters(
+ *                 @SWG\Parameter(
+ *                     name="query",
+ *                     description="Search term for suggestions",
+ *                     paramType="query",
+ *                     required="true",
+ *                     allowMultiple="false",
+ *                     dataType="String",
+ *                     defaultValue=""
+ *                 )
+ *             )
+ *         )
+ *     )
+ * )
+ */
+
 /**
 * Controller to suggest searched phrases
 *
