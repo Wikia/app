acc7516b671b1c6a5d55474c2021f1102fd54610
Adam Robak <adamr@wikia-inc.com>
2013-08-28 14:34:22 +0200

    PLA-543 PLA-542 PLA-544 PLA-545 PLA-541 initial docs in swagger annotation for WikiaHubsApi

--- includes/wikia/api/WikiaHubsApiController.class.php
+++ includes/wikia/api/WikiaHubsApiController.class.php
@@ -1,4 +1,71 @@
 <?php
+
+use Swagger\Annotations as SWG;
+
+/**
+ * @SWG\Resource(
+ *     apiVersion="0.2",
+ *     swaggerVersion="1.1",
+ *     resourcePath="WikiaHubsApi",
+ *     basePath="http://www.wikia.com"
+ * )
+ *
+ * @SWG\Model( id="WikiaHubsResultSet" )
+ * 		@SWG\Property(
+ * 			name="data",
+ * 			type="HubsData",
+ * 			required="true",
+ * 			description="Wrapping object"
+ * 		)
+ *
+ * @SWG\Model( id="HubsData" )
+ * 		@SWG\Property(
+ * 			name="slides",
+ * 			type="Array",
+ * 			items="$ref:SlideItem",
+ * 			required="true",
+ * 			description="Slides collection"
+ * 		)
+ *
+ * @SWG\Model( id="SlideItem" )
+ * 		@SWG\Property(
+ * 			name="photoUrl",
+ * 			type="string",
+ * 			required="true",
+ * 			description="Image URL"
+ * 		)
+ * 		@SWG\Property(
+ * 			name="strapline",
+ * 			type="string",
+ * 			required="true",
+ * 			description="Headline text"
+ * 		)
+ * 		@SWG\Property(
+ * 			name="shortDesc",
+ * 			type="string",
+ * 			required="true",
+ * 			description="Short description"
+ * 		)
+ * 		@SWG\Property(
+ * 			name="longDesc",
+ * 			type="string",
+ * 			required="true",
+ * 			description="Long description"
+ * 		)
+ * 		@SWG\Property(
+ * 			name="url",
+ * 			type="string",
+ * 			required="true",
+ * 			description="URL to page"
+ * 		)
+ * 		@SWG\Property(
+ * 			name="photoName",
+ * 			type="string",
+ * 			required="true",
+ * 			description="Image name"
+ * 		)
+ */
+
 /**
  * Controller to fetch information from WikiaHubs pages
  */
@@ -25,6 +92,61 @@ class WikiaHubsApiController extends WikiaApiController {
 	 * @example
 	 * @example &module=1&vertical=2&ts=1359504000
 	 * @example &module=1&vertical=2&ts=1359504000&lang=de
+	 */
+
+	/**
+	 *
+	 * @SWG\Api(
+	 *     path="/wikia.php?controller=WikiaHubsApi&method=getModuleData",
+	 *     description="Get explore module data from given date and vertical",
+	 *     @SWG\Operations(
+	 *         @SWG\Operation(
+	 *             httpMethod="GET",
+	 *             summary=" Get explore module data from given date and vertical",
+	 *             nickname="getModuleData",
+	 *             responseClass="WikiaHubsResultSet",
+	 *             @SWG\ErrorResponses(
+	 *                 @SWG\ErrorResponse( code="400", reason="Module, vertical or timestamp not valid" )
+	 *             ),
+	 *             @SWG\Parameters(
+	 *                 @SWG\Parameter(
+	 *                     name="module",
+	 *                     description="Module id",
+	 *                     paramType="query",
+	 *                     required="true",
+	 *                     allowMultiple="false",
+	 *                     dataType="int"
+	 *                 ),
+	 *                 @SWG\Parameter(
+	 *                     name="vertical",
+	 *                     description="Vertical id",
+	 *                     paramType="query",
+	 *                     required="true",
+	 *                     allowMultiple="false",
+	 *                     dataType="int"
+	 *                 ),
+	 *                 @SWG\Parameter(
+	 *                     name="ts",
+	 *                     description="Timestamp",
+	 *                     paramType="query",
+	 *                     required="false",
+	 *                     allowMultiple="false",
+	 *                     dataType="int"
+	 *                 ),
+	 *                 @SWG\Parameter(
+	 *                     name="lang",
+	 *                     description="Language",
+	 *                     paramType="query",
+	 *                     required="false",
+	 *                     allowMultiple="false",
+	 *                     dataType="string",
+	 * 					   defaultValue="en"
+	 *                 )
+	 *             )
+	 *         )
+	 *     )
+	 * )
+	 *
 	 */
 	public function getModuleData() {
 		wfProfileIn( __METHOD__ );
