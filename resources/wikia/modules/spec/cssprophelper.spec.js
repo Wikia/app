499f35e356f9afd86193ae74f4bee1fc7f28b7b2
Rafal Leszczynski <rafal@wikia-inc.com>
2013-08-28 13:18:38 +0200

    DAR-712, add unit tests

--- resources/wikia/modules/spec/cssprophelper.spec.js
+++ resources/wikia/modules/spec/cssprophelper.spec.js
@@ -1,7 +1,35 @@
 describe('CSSPropsHelper', function() {
 	'use strict';
 
-	var
+	var document = {
+			documentElement: {
+				style: {
+					webkitTransformOrigin: ''
+				}
+			}
+		},
+		cssProperty = 'transform-origin',
 		cssPropsHelper = modules['wikia.csspropshelper'](document);
+
+	it('registers AMD module', function() {
+		expect(typeof cssPropsHelper).toBe('object');
+		expect(typeof cssPropsHelper.getCSSPropName).toBe('function');
+		expect(typeof cssPropsHelper.convertPropName).toBe('function');
+	});
+
+	it('apply JS camel case style formatting', function() {
+		var formattedProp = 'transformOrigin',
+			prop = cssPropsHelper.convertPropName(cssProperty);
+
+		expect(prop).toBe(formattedProp);
+	});
+
+	it('return prop name supported by current browser', function() {
+		var formattedProp = 'webkitTransformOrigin',
+			prop = cssPropsHelper.getCSSPropName(cssProperty);
+
+		expect(prop).toBe(formattedProp);
+	});
+
 
 });
