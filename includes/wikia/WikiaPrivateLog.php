3e13c015abdb859504fd48cec3503daf2390d5ee
Federico 'Lox' Lucignano <federico@wikia-inc.com>
2013-08-29 18:30:58 +0200

    more improvements for BAC-691

--- includes/wikia/WikiaPrivateLog.php
+++ includes/wikia/WikiaPrivateLog.php
@@ -25,13 +25,11 @@ class WikiaPrivateLog {
 	public static function getChannel( $channelName ) {
 		$canonicalName = is_null( $channelName ) ? '*' : strtoupper( $channelName );
 
-		if ( array_key_exists( $canonicalName, self::$channels ) ) {
-			$channel = self::$channels[$canonicalName];
-		} else {
-			self::$channels[$canonicalName] = $channel = new self( $canonicalName );
+		if ( !array_key_exists( $canonicalName, self::$channels ) ) {
+			self::$channels[$canonicalName] = new self( $canonicalName );
 		}
 
-		return $channel;
+		return self::$channels[$canonicalName];
 	}
 
 	/**
@@ -80,7 +78,7 @@ class WikiaPrivateLog {
 			if ( is_scalar( $arg ) ) {
 				$msg = $arg;
 			} elseif (is_array( $arg ) ) {
-				$msg = json_encode( $this->flattenArray( print_r( $arg, true ) ) );
+				$msg = json_encode( $this->flattenArray( $arg ) );
 			} else {
 				$msg = gettype( $arg );
 			}
@@ -97,22 +95,77 @@ class WikiaPrivateLog {
 
 	/**
 	 * Flattens the textual representation of an array, either multidimensional
-	 * or containing objects (includes the object's fields)
+	 * or containing objects (includes the object's fields), it's meant to allow
+	 * serializing non-serializable structures used in the FileSystem stack.
+	 *
+	 * @example
+	 * $a = [new DateTime(), new DateTime()];
+	 * var_dump($a);
+	 * >   array(2) {
+	 * >       [0]=>
+	 * >       object(DateTime)#1 (3) {
+	 * >            ["date"]=> string(19) "2013-08-29 17:58:42 "
+	 * >            ["timezone_type"]=> int(3)
+	 * >            ["timezone"]=> string(13) "Europe/Warsaw"
+	 *         }
+	 * >       [1]=>
+	 * >       object(DateTime)#2 (3) {
+	 * >            ["date"]=> string(19) "2013-08-29 17:58:42"
+	 * >            ["timezone_type"]=> int(3)
+	 * >            ["timezone"]=> string(13) "Europe/Warsaw"
+	 * >       }
+	 * >   }
+	 *
+	 * After applying flattenArray:
+	 * >   array(8) {
+	 * >       [0]=>
+	 * >       array(1) {
+	 * >           [0]=> string(20) "  DateTime Object ( "
+	 * >       }
+	 * >       [1]=>
+	 * >       array(1) {
+	 * >           ["date"]=> string(22) "  2013-08-29 18:15:44 "
+	 * >       }
+	 * >       [2]=>
+	 * >       array(1) {
+	 * >           ["timezone_type"]=> string(4) "  3 "
+	 * >       }
+	 * >       [3]=>
+	 * >       array(1) {
+	 * >           ["timezone"]=> string(18) "  Europe/Warsaw ) "
+	 * >       }
+	 * >       [4]=>
+	 * >       array(1) {
+	 * >           [1]=> string(20) "  DateTime Object ( "
+	 * >       }
+	 * >       [5]=>
+	 * >       array(1) {
+	 * >           ["date"]=> string(22) "  2013-08-29 18:15:44 "
+	 * >       }
+	 * >       [6]=>
+	 * >       array(1) {
+	 * >           ["timezone_type"]=> string(4) "  3 "
+	 * >       }
+	 * >       [7]=>
+	 * >       array(1) {
+	 * >           ["timezone"]=> string(18) "  Europe/Warsaw ) "
+	 * >       }
+	 * >   }
 	 */
-	private function flattenArray( $text ) {
+	private function flattenArray( Array $data ) {
 		$output = [];
-		$cleanText = preg_replace( '/\s+/', ' ', str_replace( "\n", ' ', $text ) );
-		$array_contents = substr($cleanText, 7, -2);
+		$cleanText = preg_replace( '/\s+/', ' ', str_replace( "\n", ' ', print_r( $data, true ) ) );
+		$array_contents = substr( $cleanText, 7, -2 );
 		$array_contents = str_replace( ['[', ']', '=>'], ['#!#', '#?#', ''], $array_contents );
 		$array_fields = explode( "#!#", $array_contents );
 
 		for( $i = 0; $i < count($array_fields); $i++ ) {
 			if( $i != 0 ) {
 				$bits = explode( '#?#', $array_fields[$i] );
-				if( $bits[0] != '' ) $output[$bits[0]] = $bits[1];
+				if( $bits[0] != '' ) $output[] = [$bits[0] => $bits[1]];
 			}
 		}
 
 		return $output;
-    }
+	}
 }
