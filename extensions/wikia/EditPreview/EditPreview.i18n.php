4258936ecf3ebbf81f047b093dc5dcd04ec6260e
nAndy <nandy@wikia-inc.com>
2013-08-29 08:47:26 +0000

    changes in messages

--- extensions/wikia/EditPreview/EditPreview.i18n.php
+++ extensions/wikia/EditPreview/EditPreview.i18n.php
@@ -1,14 +1,14 @@
 <?php
 $messages['en'] = [
-	'wikia-editor-preview-current-width' => 'current width',
-	'wikia-editor-preview-min-width' => 'minimum width',
-	'wikia-editor-preview-max-width' => 'maximum width',
-	'wikia-editor-preview-type-tooltip' => "To make sure your edits look good on all computers, we've introduced the ability to change browser width in preview!",
+	'wikia-editor-preview-current-width' => 'Current width',
+	'wikia-editor-preview-min-width' => 'Minimum width',
+	'wikia-editor-preview-max-width' => 'Maximum width',
+	'wikia-editor-preview-type-tooltip' => "See how your edits will look on any computer.  Changing this option will show you what this article will look like when it's displayed in a browser at minimum width, maximum width, or on your current display.",
 ];
 
 $messages['qqq'] = [
-	'wikia-editor-preview-current-width' => 'select option - current width',
-	'wikia-editor-preview-min-width' => 'select option - minimum width',
-	'wikia-editor-preview-max-width' => 'select option - maximum width',
-	'wikia-editor-preview-type-tooltip' => 'Tooltip explaining what preview type select does',
+	'wikia-editor-preview-current-width' => 'select option; after choosing it a user see preview of the article in his/her current screen width',
+	'wikia-editor-preview-min-width' => 'select option; after choosing it a user see preview of the article in minimum supported width (1024px)',
+	'wikia-editor-preview-max-width' => 'select option; after choosing it a user see preview of the article in maximum supported width (1600px)',
+	'wikia-editor-preview-type-tooltip' => 'content of a comics bubble displayed after hovering over small icon with a question mark; it explains what preview type select does',
 ];
