81a6b21af48a0441d570b9eef50e4d93dde05316
Adam Robak <adamr@wikia-inc.com>
2013-09-02 14:59:35 +0200

    PLA-543 PLA-542 PLA-544 PLA-545 PLA-541 refactoring

--- includes/wikia/api/swagger/RelatedPagesController.swagger.php
+++ includes/wikia/api/swagger/RelatedPagesController.swagger.php
@@ -4,93 +4,85 @@ use Swagger\Annotations as SWG;
 
 /**
  * @SWG\Resource(
- *     apiVersion="0.2",
- *     swaggerVersion="1.1",
- *     resourcePath="RelatedPagesApi",
- *     basePath="http://muppet.wikia.com"
+ * 	apiVersion="0.2",
+ * 	swaggerVersion="1.1",
+ * 	resourcePath="RelatedPagesApi",
+ * 	basePath="http://muppet.wikia.com"
  * )
- */
-
-/**
- *
+ * 
+ * @SWG\Model( id="RelatedPages" )
+ * 	@SWG\Property(
+ * 		name="items",
+ * 		type="Array",
+ * 		description="Standard container name for element collection (list)",
+ * 		items="$ref:RelatedPage"
+ * 	)
+ * 	@SWG\Property(
+ * 		name="basepath",
+ * 		type="string",
+ * 		description="Common URL prefix for relative URLs"
+ * 	)
+ * 
+ * @SWG\Model( id="RelatedPage" )
+ * 	@SWG\Property(
+ * 		name="url",
+ * 		type="string",
+ * 		required="true",
+ * 		description="The relative URL of the Article. Absolute URL: obtained from combining relative URL with basepath attribute from response."
+ * 	)
+ * 	@SWG\Property(
+ * 		name="title",
+ * 		type="string",
+ * 		required="true",
+ * 		description="Formatted article title"
+ * 	)
+ * 	@SWG\Property(
+ * 		name="id",
+ * 		type="int",
+ * 		required="true",
+ * 		description="An internal identification number for Article"
+ * 	)
+ * 	@SWG\Property(
+ * 		name="imgUrl",
+ * 		type="string",
+ * 		required="true",
+ * 		description="The absolute URL of the image"
+ * 	)
+ * 
  * @SWG\Api(
- *     path="/wikia.php?controller=RelatedPagesApi&method=getList",
- *     description="Controller to fetch related pages for given article ID",
- *     @SWG\Operations(
- *         @SWG\Operation(
- *             httpMethod="GET",
- *             summary="Get pages related to a given article ID",
- *             nickname="getList",
- *             responseClass="RelatedPages",
- *             @SWG\ErrorResponses(
- *                 @SWG\ErrorResponse( code="404", reason="Related Pages extension not available" )
- *             ),
- *             @SWG\Parameters(
- *                 @SWG\Parameter(
- *                     name="ids",
- *                     description="Id of an article to fetch related pages for",
- *                     paramType="query",
- *                     required="true",
- *                     allowMultiple="false",
- *                     dataType="Array",
- *                     defaultValue="50"
- *                 ),
- *                 @SWG\Parameter(
- *                     name="limit",
- *                     description="Limit the number of related pages to return",
- *                     paramType="query",
- *                     required="false",
- *                     allowMultiple="false",
- *                     dataType="int",
- *                     defaultValue="3"
- *                 )
- *             )
- *         )
- *     )
- * )
- */
-
-/**
- * @SWG\Model(
- *     id="RelatedPages",
- *     @SWG\Property(
- *         name="items",
- *         type="Array",
- *         description="Standard container name for element collection (list)",
- *         items="$ref:RelatedPage"
- *     ),
- *     @SWG\Property(
- *         name="basepath",
- *         type="string",
- *         description="Common URL prefix for relative URLs"
- *     )
- * )
- * @SWG\Model(
- *     id="RelatedPage",
- *     @SWG\Property(
- *         name="url",
- *         type="string",
- *         required="true",
- *         description="The relative URL of the Article. Absolute URL: obtained from combining relative URL with basepath attribute from response."
- *  ),
- *      @SWG\Property(
- *         name="title",
- *         type="string",
- *         required="true",
- *         description="Formatted article title"
- *     ),
- *     @SWG\Property(
- *         name="id",
- *         type="int",
- *         required="true",
- *         description="An internal identification number for Article"
- * ),
- *     @SWG\Property(
- *         name="imgUrl",
- *         type="string",
- *         required="true",
- *         description="The absolute URL of the image"
- *     )
+ * 	path="/wikia.php?controller=RelatedPagesApi&method=getList",
+ * 	description="Controller to fetch related pages for given article ID",
+ * 	@SWG\Operations(
+ * 		@SWG\Operation(
+ * 			httpMethod="GET",
+ * 			summary="Get pages related to a given article ID",
+ * 			nickname="getList",
+ * 			responseClass="RelatedPages",
+ * 			@SWG\ErrorResponses(
+ * 				@SWG\ErrorResponse( code="404", reason="Related Pages extension not available" )
+ * 			),
+ * 			@SWG\Parameters(
+ * 				@SWG\Parameter(
+ * 					name="ids",
+ * 					description="Id of an article to fetch related pages for",
+ * 					paramType="query",
+ * 					required="true",
+ * 					allowMultiple="false",
+ * 					dataType="Array",
+ * 					defaultValue="50"
+ * 				),
+ * 				@SWG\Parameter(
+ * 					name="limit",
+ * 					description="Limit the number of related pages to return",
+ * 					paramType="query",
+ * 					required="false",
+ * 					allowMultiple="false",
+ * 					dataType="int",
+ * 					defaultValue="3"
+ * 				)
+ * 			)
+ * 		)
+ * 	)
  * )
  */
 
--- includes/wikia/api/swagger/SearchSuggestionsApiController.swagger.php
+++ includes/wikia/api/swagger/SearchSuggestionsApiController.swagger.php
@@ -4,59 +4,53 @@ use Swagger\Annotations as SWG;
 
 /**
  * @SWG\Resource(
- *     apiVersion="0.2",
- *     swaggerVersion="1.1",
- *     resourcePath="SearchSuggestionsApi",
- *     basePath="http://muppet.wikia.com"
+ * 	apiVersion="0.2",
+ * 	swaggerVersion="1.1",
+ * 	resourcePath="SearchSuggestionsApi",
+ * 	basePath="http://muppet.wikia.com"
  * )
- */
-
-/**
- * @SWG\Model(id="SearchSuggestionsPhrases")
- *     @SWG\Property(
- *         name="items",
- *         type="Array",
- *         description="Standard container name for element collection (list)",
- *         items="$ref:SearchSuggestionsItems"
- *     )
- * )
- * @SWG\Model(id="SearchSuggestionsItems")
- *      @SWG\Property(
- *         name="title",
- *         type="string",
- *         required="true",
- *         description="Searching article title"
- *     )
- * )
- */
-
-/**
- *
+ * 
+ * @SWG\Model( id="SearchSuggestionsPhrases" )
+ * 	@SWG\Property(
+ * 		name="items",
+ * 		type="Array",
+ * 		description="Standard container name for element collection (list)",
+ * 		items="$ref:SearchSuggestionsItems"
+ * 	)
+ * 
+ * @SWG\Model( id="SearchSuggestionsItems" )
+ * 	@SWG\Property(
+ * 		name="title",
+ * 		type="string",
+ * 		required="true",
+ * 		description="Searching article title"
+ * 	)
+ * 
  * @SWG\Api(
- *     path="/wikia.php?controller=SearchSuggestionsApi&method=getList",
- *     description="Controller to find suggested phrases for chosen query",
- *     @SWG\Operations(
- *         @SWG\Operation(
- *             httpMethod="GET",
- *             summary="Finds suggested phrases for chosen query",
- *             nickname="getList",
- *             responseClass="SearchSuggestionsPhrases",
- *             @SWG\ErrorResponses(
- *                 @SWG\ErrorResponse( code="404", reason="Search Suggestions extension not available" )
- *             ),
- *             @SWG\Parameters(
- *                 @SWG\Parameter(
- *                     name="query",
- *                     description="Search term for suggestions",
- *                     paramType="query",
- *                     required="true",
- *                     allowMultiple="false",
- *                     dataType="string",
- *                     defaultValue=""
- *                 )
- *             )
- *         )
- *     )
+ * 	path="/wikia.php?controller=SearchSuggestionsApi&method=getList",
+ * 	description="Controller to find suggested phrases for chosen query",
+ * 	@SWG\Operations(
+ * 		@SWG\Operation(
+ * 			httpMethod="GET",
+ * 			summary="Finds suggested phrases for chosen query",
+ * 			nickname="getList",
+ * 			responseClass="SearchSuggestionsPhrases",
+ * 			@SWG\ErrorResponses(
+ * 				@SWG\ErrorResponse( code="404", reason="Search Suggestions extension not available" )
+ * 			),
+ * 			@SWG\Parameters(
+ * 				@SWG\Parameter(
+ * 					name="query",
+ * 					description="Search term for suggestions",
+ * 					paramType="query",
+ * 					required="true",
+ * 					allowMultiple="false",
+ * 					dataType="String",
+ * 					defaultValue=""
+ * 				)
+ * 			)
+ * 		)
+ * 	)
  * )
  */
 
