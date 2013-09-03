00c26d267b61a2b5779e32e49400df61468c9aec
Garth Webb <gart@wikia-inc.com>
2013-09-03 16:42:47 -0700

    VID-745 Make sure we constrain the location of this module

--- extensions/wikia/TouchStorm/TouchStormHooks.class.php
+++ extensions/wikia/TouchStorm/TouchStormHooks.class.php
@@ -11,11 +11,21 @@ class TouchStormHooks extends WikiaObject {
 		$app = F::App();
 		wfProfileIn(__METHOD__);
 
-		// These positions are set to be between the Photos module and
-		// the Videos module (1303) when logged out and below the Recent Activity module
-		// when logged in (1287)
-		$pos = $app->wg->User->isAnon() ? 1303 : 1287;
-		$modules[$pos] = array('TouchStorm', 'index', null);
+		$curNS = $app->wg->Title->getNamespace();
+
+		// Set the default location to the file page
+		$tsLocation = $app->wg->TouchStormLocation ? $app->wg->TouchStormLocation : 'file';
+
+		// Make sure the TouchStorm location matches the current location
+		if ( (($curNS == NS_FILE) && preg_match('/file/', $tsLocation))    ||
+			 (($curNS == NS_MAIN) && preg_match('/article/', $tsLocation))
+		) {
+			// These positions are set to be between the Photos module and
+			// the Videos module (1303) when logged out and below the Recent Activity module
+			// when logged in (1287)
+			$pos = $app->wg->User->isAnon() ? 1303 : 1287;
+			$modules[$pos] = array('TouchStorm', 'index', null);
+		}
 
 		wfProfileOut(__METHOD__);
 		return true;
