012ec60f401df0bf51377f0e870174e9692fd4f2
Adam Robak <adamr@wikia-inc.com>
2013-09-06 18:27:04 +0200

    PLA-670 removing namespaces from global wiki

--- skins/oasis/modules/SearchController.class.php
+++ skins/oasis/modules/SearchController.class.php
@@ -17,6 +17,11 @@ class SearchController extends WikiaController {
 			$this->noautocomplete = $this->request->getVal( 'noautocomplete' );
 		}
 
+		$this->nonamespaces = $this->wg->request->getVal('nonamespaces');
+		if ( !isset( $this->nonamespaces ) ) {
+			$this->nonamespaces = $this->request->getVal( 'nonamespaces' );
+		}
+
 		$this->fulltext = $this->wg->User->getOption('enableGoSearch') ? 0 : 'Search';
 		$this->placeholder = wfMsg('Tooltip-search', $this->wg->Sitename);
 		$this->isCrossWikiaSearch = $this->wg->request->getCheck('crossWikiaSearch');
