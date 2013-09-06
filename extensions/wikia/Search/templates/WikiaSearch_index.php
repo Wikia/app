743e565c100af720f5c8c1059bd1383a613514fd
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-09-06 13:56:31 +0200

    PLA-539 change video addon module positions again

--- extensions/wikia/Search/templates/WikiaSearch_index.php
+++ extensions/wikia/Search/templates/WikiaSearch_index.php
@@ -52,7 +52,7 @@
 					<?php foreach( $results as $result ): ?>
 						<?php
 							$pos++;
-							if ( ( $pos == 2 || $pos == 6 ) && $mediaData ):
+							if ( ( $pos == 3 || $pos == 7 ) && $mediaData ):
 								echo '<li class="video-addon-results video-addon-results-before-' . $pos . '">' . $app->getView( 'WikiaSearch', 'mediadata', array( 'mediaData' => $mediaData, 'query' => $query ) ) . '</li>';
 							endif;
 							if ( $result['ns'] === 0 ) {
