ac479e922a16358f94359b8f6ba9b59f62aeb0ac
Kyle Florence <kflorence@wikia-inc.com>
2013-09-06 19:05:08 -0700

    VE-290: create unit test base for WikiaBlockImageNode tests.

--- extensions/VisualEditor/wikia/VisualEditor.php
+++ extensions/VisualEditor/wikia/VisualEditor.php
@@ -71,7 +71,8 @@ function Wikia_onResourceLoaderTestModules( array &$testModules, ResourceLoader 
 	$testModules['qunit']['ext.visualEditor.wikiaTest'] = array(
 		'scripts' => array(
 			'test/dm/ve.dm.wikiaExample.js',
-			'test/dm/ve.dm.WikiaConverter.test.js'
+			'test/dm/ve.dm.WikiaConverter.test.js',
+			'test/ce/ve.ce.WikiaBlockImageNode.test.js'
 		),
 		'dependencies' => array(
 			'ext.visualEditor.test',
---
+++ extensions/VisualEditor/wikia/test/ce/ve.ce.WikiaBlockImageNode.test.js
@@ -0,0 +1,71 @@
+/*!
+ * VisualEditor ContentEditable WikiaBlockImageNode tests.
+ */
+
+QUnit.module( 've.ce.WikiaBlockImageNode' );
+
+/* Tests */
+
+QUnit.test( 'Verify rendered content', function( assert ) {
+	var cl,
+		ci,
+		doc,
+		documentModel,
+		documentView,
+		node,
+		surface,
+		surfaceModel,
+		surfaceView,
+		target,
+		ti,
+		test,
+		testCase,
+		testCases,
+		$fixture = $( '#qunit-fixture' ),
+		expectCount = 0,
+		tests = [
+			{
+				name: 'mw:Image/Thumb',
+				html: '<body><figure typeof="mw:Image/Thumb" class="mw-halign-right foobar" data-mw=\'{"attribution":{"username":"Foo","avatar":"Foo.png"}}\'><a href="Foo"><img src="Bar" width="1" height="2" resource="FooBar"></a><figcaption>abc</figcaption></figure></body>',
+				cases: [
+					{
+						msg: 'no changes',
+						html: '<figure class="thumb thumbinner tright ve-ce-branchNode ve-ce-protectedNode" style="width: 3px;" contenteditable="false"><a class="image" href="Foo"><img src="Bar" width="1" height="2"></a><a class="internal sprite details magnify ve-no-shield"></a><figcaption class="thumbcaption ve-ce-branchNode"><p class="ve-ce-generated-wrapper ve-ce-branchNode">abc</p></figcaption><img class="ve-ce-protectedNode-shield" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></figure>'
+					}
+				]
+			}
+		],
+		tl = tests.length;
+
+	for ( ti = 0; ti < tl; ti++ ) {
+		test = tests[ ti ];
+		testCases = test.cases;
+
+		// Set up the document
+		doc = ve.createDocumentFromHtml( test.html );
+		target = new ve.init.sa.Target( $fixture, doc );
+		surface = target.surface;
+		surfaceModel = surface.getModel();
+		surfaceView = surface.getView();
+		documentModel = surfaceModel.getDocument();
+		documentView = surfaceView.getDocument();
+
+		// Get the node
+		node = documentView.documentNode.children[ 0 ];
+
+		for ( ci = 0, cl = testCases.length; ci < cl; ci++ ) {
+			testCase = testCases[ ci ];
+
+			if ( testCase.data !== undefined ) {
+				documentModel.change( ve.dm.Transaction.newFromAttributeChanges( documentModel, node.getOffset(), testCase.data ) );
+			}
+
+			// TODO: figure out a better way to remove debug styles
+			node.$.find( '.ve-ce-generated-wrapper' ).removeAttr( 'style' );
+			assert.equalDomElement( node.$[ 0 ], $( testCase.html )[ 0 ], test.name + ' - ' + testCase.msg );
+			expectCount++;
+		}
+	}
+
+	QUnit.expect( expectCount );
+});
