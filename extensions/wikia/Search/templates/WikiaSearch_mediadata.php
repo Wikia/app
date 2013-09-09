f157b1979ac010c62dbfe2258e13eb52fd328619
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-09-09 14:43:28 +0200

    PLA-539 if there are no video results don't display video addon

--- extensions/wikia/Search/templates/WikiaSearch_mediadata.php
+++ extensions/wikia/Search/templates/WikiaSearch_mediadata.php
@@ -1,6 +1,6 @@
-<h3><a href="<?= $mediaData['videoUrl'] ?>" class="video-addon-seach-video"><?= wfMessage( 'wikiasearch2-video-results', $query ) ?></a></h3>
-<ul>
-	<?php foreach ( $mediaData['items'] as $fileDatum ): ?><li><?= $fileDatum["thumbnail"] ?></li><? endforeach; ?>
-</ul>
-
-
+<?php if ( isset($mediaData['videoUrl']) && sizeof($mediaData['videoUrl']) != 0 ): ?>
+	<h3><a href="<?= $mediaData['videoUrl'] ?>" class="video-addon-seach-video"><?= wfMessage( 'wikiasearch2-video-results', $query ) ?></a></h3>
+	<ul>
+		<?php foreach ( $mediaData['items'] as $fileDatum ): ?><li><?= $fileDatum["thumbnail"] ?></li><? endforeach; ?>
+	</ul>
+<?php endif ?>
