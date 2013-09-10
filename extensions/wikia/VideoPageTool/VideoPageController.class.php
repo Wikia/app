590d72b8b987f7bce5c7f229520cac676e724b6d
Garth Webb <gart@wikia-inc.com>
2013-09-10 10:57:14 -0700

    VID-580 Added comments & fixed controller names

--- extensions/wikia/VideoPageTool/VideoPageController.class.php
+++ extensions/wikia/VideoPageTool/VideoPageController.class.php
@@ -8,11 +8,27 @@ class VideoPageController extends WikiaController {
 
 	}
 
+	/**
+	 * Return display content for any of the supported modules, one of:
+	 *
+	 *  - featured
+	 *  - latest
+	 *  - fan
+	 *  - popular
+	 *
+	 * Example controller request:
+	 *
+	 *   /wikia.php?controller=VideoPageController&method=getModule&moduleName=latest
+	 *
+	 * @requestParam moduleName - The name of the module to display
+	 * @return bool
+	 */
 	public function getModule( ) {
 		$name = $this->getVal('moduleName', '');
-		$handler = $name.'Handler';
+		$handler = 'handle'.ucfirst(strtolower($name));
 
 		if ( method_exists( __CLASS__, $handler ) ) {
+			$this->overrideTemplate( $name );
 			$this->forward( __CLASS__, $handler );
 			return true;
 		} else {
@@ -23,18 +39,30 @@ class VideoPageController extends WikiaController {
 		}
 	}
 
+	/**
+	 * Displays the featured module
+	 */
 	public function handleFeatured() {
 
 	}
 
+	/**
+	 * Displays the latest module
+	 */
 	public function handleLatest() {
 
 	}
 
+	/**
+	 * Displays the fan module
+	 */
 	public function handleFan() {
 
 	}
 
+	/**
+	 * Displays the popular module
+	 */
 	public function handlePopular() {
 
 	}
---
+++ extensions/wikia/VideoPageTool/templates/VideoPageController_latest.php
@@ -0,0 +1,1 @@
+<h1>This is the 'latest' display handler</h1>
