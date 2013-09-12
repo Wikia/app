ebf43d46451ca1fc7d42baf2249d63f2b5ee783a
nAndy <nandy@wikia-inc.com>
2013-09-12 13:36:58 +0000

    initial idea of the component

---
+++ resources/wikia/ui_components/navigation/navigation.i18n.php
@@ -0,0 +1,7 @@
+<?php
+$messages['en'] = [
+	'styleguide-name-navigation' => 'Navigation',
+	'styleguide-description-navigation' => 'Description for Navigation component in Styleguide.',
+	'styleguide-description-navigation-elements' => 'Parameter which is a collection of elements in the navigation; each element has three properties: name, link and class',
+	'styleguide-description-navigation-classes' => 'Optional parameter - Navigation HTML container classes',
+];
