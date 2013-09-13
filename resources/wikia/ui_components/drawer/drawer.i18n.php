60d36ba13fceb8653a852118262255f082610f68
nAndy <nandy@wikia-inc.com>
2013-09-13 17:37:16 +0200

    fixed message for drawer component

--- resources/wikia/ui_components/drawer/drawer.i18n.php
+++ resources/wikia/ui_components/drawer/drawer.i18n.php
@@ -1,8 +1,8 @@
-<?
+<?php
 $messages['en'] = [
 	'styleguide-name-drawer' => 'Drawers',
 	'styleguide-description-drawer' => 'Description for Drawer component in Styleguide.',
 	'styleguide-description-drawer-side' => 'Parameter which describes side on which drawer is sticked. Available values: left/right',
 	'styleguide-description-drawer-content' => 'Parameter - Drawer content',
-	'styleguide-description-drawer-content' => 'Optional parameter - Drawer HTML container class',
+	'styleguide-description-drawer-class' => 'Optional parameter - Drawer HTML container class',
 ];
