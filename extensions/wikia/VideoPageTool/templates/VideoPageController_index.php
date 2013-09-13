e1a632ccebd898fa01365cced77465c69317ddca
Kenneth Kouot <kenneth@wikia-inc.com>
2013-09-13 19:20:50 +0000

    add assets

---
+++ extensions/wikia/VideoPageTool/templates/VideoPageController_index.php
@@ -0,0 +1,11 @@
+<h1>This is a test</h1>
+<h2>This is only a test</h2>
+<section class="branding-partners">
+	<ul>
+		<li class="partner"><a class="partner-link" href=""><img class="partner-logo" src="" alt=""></a></li>
+		<li class="partner"><a class="partner-link" href=""><img class="partner-logo" src="" alt=""></a></li>
+		<li class="partner"><a class="partner-link" href=""><img class="partner-logo" src="" alt=""></a></li>
+		<li class="partner"><a class="partner-link" href=""><img class="partner-logo" src="" alt=""></a></li>
+		<li class="partner"><a class="partner-link" href=""><img class="partner-logo" src="" alt=""></a></li>
+	</ul>
+</section>
