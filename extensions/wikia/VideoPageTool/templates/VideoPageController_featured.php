39b2de9db080a6fa542f7ad2583c94ae323fcd70
lizlux <liz@lizlux.net>
2013-09-13 18:02:29 -0700

    some incorporation of back end into front end for featured video module

--- extensions/wikia/VideoPageTool/templates/VideoPageController_featured.php
+++ extensions/wikia/VideoPageTool/templates/VideoPageController_featured.php
@@ -1,32 +1,19 @@
-<h1>This is the 'featured' display handler</h1>
-<ul>
-<? foreach ( $assets as $featured ): ?>
-	<? $videoData = $featured->getAssetData() ?>
-	<li><?= $featured->getOrder() ?> - <?= $videoData['videoThumb'] ?><?= $videoData['videoTitle'] ?> : <?= $videoData['largeThumbUrl'] ?></li>
-<? endforeach; ?>
-</ul>
-
-<br />
-<br />
-<br />
-<br />
-<br />
 <div id="featured-video" class="featured-video">
+
 	<ul class="featured-video-slider">
-		<li>
-			<img src="http://placekitten.com/1000/500">
-		</li>
-		<li>
-			<img src="http://placekitten.com/1001/500">
-		</li>
-		<li>
-			<img src="http://placekitten.com/1002/500">
-		</li>
-		<li>
-			<img src="http://placekitten.com/1003/500">
-		</li>
-		<li>
-			<img src="http://placekitten.com/1004/500">
-		</li>
+		<? foreach ( $assets as $featured ): ?>
+			<? $videoData = $featured->getAssetData() ?>
+			<li>
+				<img src="<?= $videoData['largeThumbUrl'] ?>">
+			</li>
+		<? endforeach; ?>
 	</ul>
+
+	<ul>
+		<? foreach ( $assets as $featured ): ?>
+			<? $videoData = $featured->getAssetData() ?>
+			<li><?= $featured->getOrder() ?> - <?= $videoData['videoThumb'] ?><?= $videoData['videoTitle'] ?></li>
+		<? endforeach; ?>
+	</ul>
+
 </div>
