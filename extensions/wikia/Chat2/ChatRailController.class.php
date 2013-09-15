3587f651db852f2af78c51ca9ca99133bf15510f
mech <mech@wikia-inc.com>
2013-09-15 23:55:20 +0200

    DAR-1724 Chat entry point - restoring old controller method in order not to break pages cached on varnish

--- extensions/wikia/Chat2/ChatRailController.class.php
+++ extensions/wikia/Chat2/ChatRailController.class.php
@@ -31,6 +31,42 @@ class ChatRailController extends WikiaController {
 		wfProfileOut( __METHOD__ );
 	}
 
+	/**
+	 * Chat entry point - rendered via Ajax or pre-rendered in JS variable
+	 * @todo: backward compatibility method, remove it till Oct 2013
+	 */
+	public function executeContents() {
+		global $wgUser, $wgReadOnly, $wgEnableWallExt;
+		wfProfileIn( __METHOD__ );
+
+		if(empty($wgReadOnly)) {
+			// Main variables
+			$this->profileType = !empty($wgEnableWallExt) ? 'message-wall' : 'talk-page';
+			$this->linkToSpecialChat = SpecialPage::getTitleFor("Chat")->escapeLocalUrl();
+			$this->isLoggedIn = $wgUser->isLoggedIn();
+			$this->profileAvatarUrl = $this->isLoggedIn ? AvatarService::getAvatarUrl($wgUser->getName(), ChatRailController::AVATAR_SIZE) : '';
+
+			// List of other people in chat
+			$this->totalInRoom = 0;
+
+			// Gets array of users currently in chat to populate rail module and user stats menus
+			$this->chatters = ChatEntryPoint::getChatUsersInfo();
+			$this->totalInRoom = count($this->chatters);
+			for($i = 0 ; $i < $this->totalInRoom ; $i++) {
+				global $wgLang;
+				if ($this->chatters[$i]['showSince']) {
+					$this->chatters[$i]['since'] =  $wgLang->getMonthAbbreviation($this->chatters[$i]['since_month']) .
+						' ' . $this->chatters[$i]['since_year'];
+				}
+			}
+		}
+
+		// Cache the entire call in varnish (and browser).
+		$this->response->setCacheValidity(self::CACHE_DURATION, self::CACHE_DURATION, array(WikiaResponse::CACHE_TARGET_BROWSER, WikiaResponse::CACHE_TARGET_VARNISH));
+
+		wfProfileOut( __METHOD__ );
+	}
+
 	public function executeGetUsers() {
 		wfProfileIn( __METHOD__ );
 		$this->users = ChatEntryPoint::getChatUsersInfo();
