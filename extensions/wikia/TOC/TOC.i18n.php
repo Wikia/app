65ad221367bd307b0c0962ec3a5254af039b4374
Rafal Leszczynski <rafal@wikia-inc.com>
2013-09-16 15:41:11 +0200

    DAR-1727, add i18n messages

--- extensions/wikia/TOC/TOC.i18n.php
+++ extensions/wikia/TOC/TOC.i18n.php
@@ -3,8 +3,12 @@
 $messages = [];
 
 $messages['en'] = [
-
+	"toc-header" => "Content",
+	"toc-show" => "show",
+	"toc-hide" => "hide"
 ];
 $messages['qqq'] = [
-
+	"toc-header" => "TOC header",
+	"toc-show" => "Link label to show TOC",
+	"toc-hide" => "Link label to hide TOC"
 ];
--- extensions/wikia/TOC/TOCController.class.php
+++ extensions/wikia/TOC/TOCController.class.php
@@ -3,7 +3,10 @@
 class TOCController extends WikiaController {
 
 	public function index() {
+
+		$this->tocHeader = wfMessage( 'toc-header' )->plain();
+		$this->show = wfMessage( 'toc-show' )->plain();
+		$this->hide = wfMessage( 'toc-hide' )->plain();
 		$this->response->setTemplateEngine(WikiaResponse::TEMPLATE_ENGINE_MUSTACHE);
 	}
-
 }
