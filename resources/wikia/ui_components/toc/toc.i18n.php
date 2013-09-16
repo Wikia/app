19b9fbe8b90915b687facbc5e9948b23c6c1d1ae
Rafal Leszczynski <rafal@wikia-inc.com>
2013-09-16 18:37:19 +0200

    DAR-1602, add toc component

---
+++ resources/wikia/ui_components/toc/toc.i18n.php
@@ -0,0 +1,10 @@
+<?php
+$messages['en'] = [
+	// main component name and description
+	'styleguide-name-toc' => "Table of contents",
+	'styleguide-description-toc' => 'Lorem ipsu, dolor',
+
+	// parameters' documentation messages
+
+
+];
