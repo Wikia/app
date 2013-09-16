e0fce0633a1ff74685a601db8fe2709c7f332761
nAndy <nandy@wikia-inc.com>
2013-09-16 12:52:25 +0000

    updated components messages

--- resources/wikia/ui_components/navigation/navigation.i18n.php
+++ resources/wikia/ui_components/navigation/navigation.i18n.php
@@ -1,7 +1,13 @@
 <?php
 $messages['en'] = [
+	// main component name and description
 	'styleguide-name-navigation' => 'Navigation',
 	'styleguide-description-navigation' => 'Description for Navigation component in Styleguide.',
+
+	// parameters' documentation messages
 	'styleguide-description-navigation-elements' => 'Parameter which is a collection of elements in the navigation; each element has three properties: name, link and class',
 	'styleguide-description-navigation-classes' => 'Optional parameter - Navigation HTML container classes',
+
+	// examples
+	'styleguide-example-description-navigation-unavailable' => "This component's examples are unavailable at this moment",
 ];
