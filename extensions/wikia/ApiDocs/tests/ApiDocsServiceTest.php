7569c4cfd09f62e66068d51dd1186bf31e67c7ad
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-09-17 16:40:01 +0200

    PLA-719 ApiDocsServiceTest

--- extensions/wikia/ApiDocs/tests/ApiDocsServiceTest.php
+++ extensions/wikia/ApiDocs/tests/ApiDocsServiceTest.php
@@ -4,6 +4,12 @@ namespace Wikia\ApiDocs\Services;
 
 
 class ApiDocsServiceTest extends \WikiaBaseTest {
+	protected function setUp() {
+		global $IP;
+		$this->setupFile = "{$IP}/extensions/wikia/ApiDocs/ApiDocs.setup.php";
+
+		parent::setUp();
+	}
 
 	public function testGetDocList() {
 		$swaggerMock = $this->getMockBuilder( '\Swagger\Swagger' )
@@ -11,17 +17,46 @@ class ApiDocsServiceTest extends \WikiaBaseTest {
 
 		$swaggerMock->expects( $this->once() )
 			->method( "getDefaultApiVersion" )
-			->will( $this->returnValue("vfoo") );
+			->will( $this->returnValue("bar_version") );
 
 		$swaggerMock->expects( $this->once() )
 			->method( "getDefaultSwaggerVersion" )
-			->will( $this->returnValue("sversion") );
+			->will( $this->returnValue("foo_version") );
+
+		$api = new \StdClass();
+		$api->description = "bar";
+		$resource = new \StdClass();
+		$resource->resourcePath = "baz";
+		$resource->apis = [ $api ];
 
 		$swaggerMock->expects( $this->once() )
 			->method( "getRegistry" )
-			->will( $this->returnValue("sversion") );
+			->will( $this->returnValue([ $resource ]) );
 
 		$apiDocsService = new ApiDocsService( $swaggerMock, function($foo) { return "path_for_$foo"; } );
-		$apiDocsService->getDocList();
+		$result = $apiDocsService->getDocList();
+
+		$this->assertEquals( "bar_version", $result["apiVersion"] );
+		$this->assertEquals( "foo_version", $result["swaggerVersion"] );
+		$this->assertEquals( 1 , sizeof( $result["apis"] ) );
+		$this->assertEquals( "bar" , $result["apis"][0]["description"] );
+		$this->assertEquals( "baz" , $result["apis"][0]["readableName"] );
+		$this->assertEquals( "path_for_baz" , $result["apis"][0]["path"] );
+	}
+
+	public function testGetDoc() {
+		$swaggerMock = $this->getMockBuilder( '\Swagger\Swagger' )
+			->disableOriginalConstructor()->getMock();
+
+		$swaggerMock->expects( $this->once() )
+			->method( "getResource" )
+			->with( "foo", false, false )
+			->will( $this->returnValue( [ "basePath" => "foo" ] ) );
+
+		$apiDocsService = new ApiDocsService( $swaggerMock, function($foo) {  } );
+
+		$result = $apiDocsService->getDoc( "foo" );
+
+		$this->assertEquals( [ "basePath" => "foo" ], $result );
 	}
 }
