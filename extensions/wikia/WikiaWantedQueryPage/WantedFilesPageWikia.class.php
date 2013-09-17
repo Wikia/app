3bd4aa2d9677383a769f35a21228a90d903dd125
Garth Webb <gart@wikia-inc.com>
2013-09-16 17:28:15 -0700

    VID-492 Ignore known premium video in the WantedFiles page

--- extensions/wikia/WikiaWantedQueryPage/WantedFilesPageWikia.class.php
+++ extensions/wikia/WikiaWantedQueryPage/WantedFilesPageWikia.class.php
@@ -13,6 +13,17 @@ class WantedFilesPageWikia extends WantedFilesPage {
 			$queryInfo['conds'][] = "il_to not in (" . $dbr->makeList($wgExcludedWantedFiles) . ") ";
 		}
 
+		// Make sure the video_info table exists before adding this additional condition
+		$infoHelper = new VideoInfoHelper();
+		if ( $infoHelper->videoInfoExists() ) {
+			// Ignore any missing images that are actually premium video.  Premium video comes from the
+			// video.wikia.com wiki and will not have local image table entries
+			$queryInfo['tables'][] = 'video_info';
+			$queryInfo['join_conds']['video_info'] = array( 'LEFT JOIN',
+															array("il_to = video_title", "premium = 1"));
+			$queryInfo['conds'][] = 'video_title IS NULL';
+		}
+
 		return $queryInfo;
 	}
 }
