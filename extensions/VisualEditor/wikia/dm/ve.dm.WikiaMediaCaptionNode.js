708dcc70c7cf4fcdafb15ba919472225671b1e52
Christian Williams <christian@wikia-inc.com>
2013-09-17 00:30:13 +0000

    Fixing naming

--- extensions/VisualEditor/wikia/dm/ve.dm.WikiaMediaCaptionNode.js
+++ extensions/VisualEditor/wikia/dm/ve.dm.WikiaMediaCaptionNode.js
@@ -11,23 +11,23 @@
  * @param {number} [length] Length of content data in document
  * @param {Object} [element] Reference to element in linear model
  */
-ve.dm.VeWikiaMediaCaptionNode = function VeDmWikiaMediaCaptionNode( length, element ) {
+ve.dm.WikiaMediaCaptionNode = function VeDmWikiaMediaCaptionNode( length, element ) {
 	ve.dm.MWImageCaptionNode.call( this, length, element );
 };
 
 /* Inheritance */
 
-ve.inheritClass( ve.dm.VeWikiaMediaCaptionNode, ve.dm.MWImageCaptionNode );
+ve.inheritClass( ve.dm.WikiaMediaCaptionNode, ve.dm.MWImageCaptionNode );
 
 /* Static Properties */
 
-ve.dm.VeWikiaMediaCaptionNode.static.name = 'wikiaMediaCaption';
+ve.dm.WikiaMediaCaptionNode.static.name = 'wikiaMediaCaption';
 
-ve.dm.VeWikiaMediaCaptionNode.static.parentNodeTypes = [
+ve.dm.WikiaMediaCaptionNode.static.parentNodeTypes = [
 	'wikiaBlockImage',
 	'wikiaBlockVideo'
 ];
 
 /* Registration */
 
-ve.dm.modelRegistry.register( ve.dm.VeWikiaMediaCaptionNode );
+ve.dm.modelRegistry.register( ve.dm.WikiaMediaCaptionNode );
