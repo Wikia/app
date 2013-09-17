9834e37fb41f33c051d3c8bdabea39e07afae75a
nAndy <nandy@wikia-inc.com>
2013-09-17 12:12:06 +0000

    changed logic of showing/hiding parameters' tables

--- extensions/wikia/SpecialStyleguide/js/SpecialStyleguide.js
+++ extensions/wikia/SpecialStyleguide/js/SpecialStyleguide.js
@@ -1,19 +1,33 @@
 var SpecialStyleguide = function() {};
 
 SpecialStyleguide.prototype = {
+	CLASS_HIDDEN: 'hidden',
+	CLASS_SHOWN: 'shown',
+
 	init: function() {
-		$( ".toggleParameters").toggle(
-			function() {
-				var $link = $(this);
-				$link.text( $.msg( 'styleguide-hide-parameters' ) );
-				$link.closest('h4').next( '.styleguide-component-params' ).show();
-			},
-			function() {
-				var $link = $(this);
-				$link.text( $.msg( 'styleguide-show-parameters' ) );
-				$link.closest('h4').next( '.styleguide-component-params' ).hide();
+		$( '.toggleParameters' ).click( $.proxy( function( event ) {
+			event.preventDefault();
+
+			var $link = $( event.target ),
+				$paramsTable = $link.closest('h4').next( '.styleguide-component-params'),
+				previousClass = '',
+				currentClass = '',
+				message = '';
+
+			if( $paramsTable.hasClass( this.CLASS_HIDDEN ) ) {
+				currentClass = this.CLASS_SHOWN;
+				previousClass = this.CLASS_HIDDEN;
+				message = $.msg( 'styleguide-hide-parameters' );
+			} else {
+				currentClass = this.CLASS_HIDDEN;
+				previousClass = this.CLASS_SHOWN;
+				message = $.msg( 'styleguide-show-parameters' );
 			}
-		);
+
+			$link.text( message );
+			$paramsTable.removeClass( previousClass );
+			$paramsTable.addClass( currentClass );
+		}, this) );
 	}
 };
 
