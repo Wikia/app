78fd43425716f9cbc046499eea47ed470b3bef90
Garth Webb <gart@wikia-inc.com>
2013-09-18 16:17:29 -0700

    VID-438 Hide results that actually exist rather than showing them as strikethrough.

--- extensions/wikia/WikiaWantedQueryPage/WantedFilesPageWikia.class.php
+++ extensions/wikia/WikiaWantedQueryPage/WantedFilesPageWikia.class.php
@@ -1,7 +1,24 @@
 <?php
+/**
+ * Class WantedFilesPageWikia
+ *
+ * Wikia specific functionality for the WantedFilesPage
+ *
+ * @author Garth <garth@wikia-inc.com>
+ * @author Liz <liz@wikia-inc.com>
+ * @author Saipetch <saipetch@wikia-inc.com>
+ * @author Kenneth <kenneth@wikia-inc.com>
+ */
 class WantedFilesPageWikia extends WantedFilesPage {
 
-
+	/**
+	 * Returns additional query conditions to use when constructing the wanted files SQL.  In this case we
+	 * are adding:
+	 * - the ability to filter out specific files by file name.
+	 * - a filter to remove premium video results which do not have image table entries
+	 *
+	 * @return array
+	 */
 	public function getQueryInfo() {
 
 		global $wgExcludedWantedFiles;
@@ -25,5 +42,24 @@ class WantedFilesPageWikia extends WantedFilesPage {
 		}
 
 		return $queryInfo;
+	}
+
+	/**
+	 * Override the default formatResult method in the parent.  The difference is that the parent will display
+	 * titles that turn out to pass the 'isKnown' test with a strikethrough whereas we will simply not display
+	 * these results in this version.
+	 * @param Skin $skin - The skin used to display this special page
+	 * @param Result $result - The result line to format
+	 * @return string - The HTML to use for this result line
+	 */
+	public function formatResult( $skin, $result ) {
+		$title = Title::makeTitleSafe( $result->namespace, $result->title );
+		if ( $title instanceof Title ) {
+			if ( $title->isKnown() ) {
+				return '';
+			}
+		}
+
+		return parent::formatResult( $skin, $result );
 	}
 }
