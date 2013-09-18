99e1b6d1cbc166868f7670e2e0aff03b3ba23859
jursza <jacek@wikia-inc.com>
2013-09-18 14:05:59 +0200

    PLA-763 PLA-770 creating LicensedWikisService

---
+++ includes/wikia/services/LicensedWikisService.php
@@ -0,0 +1,17 @@
+<?php
+
+class LicensedWikisService extends WikiaModel {
+
+	public function getLicensedWikis() {
+
+	}
+
+	public function isLicensedById( $wikiId ) {
+
+	}
+
+	public function isLicensedByHostName( $hostName ) {
+
+	}
+
+}
