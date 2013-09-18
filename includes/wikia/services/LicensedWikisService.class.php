0414a1eb23551d5a696c38932644636770250073
jursza <jacek@wikia-inc.com>
2013-09-18 16:05:04 +0200

    PLA-763 PLA-770 LicensedWikisService implementation

--- includes/wikia/services/LicensedWikisService.class.php
+++ includes/wikia/services/LicensedWikisService.class.php
@@ -2,18 +2,46 @@
 
 class LicensedWikisService extends WikiaModel {
 
-	const LICENSE_WIKI_FACTORY_VAR_ID = 253;
+	const LICENSE_WIKI_FACTORY_VAR_ID = 253; /* wgRightsText */
+	const CACHE_KEY_COMMERCIAL_NOT_ALLOWED = 'commercial_use_not_allowed_wikis';
+	const CACHE_VALID_TIME = 86400;
 
+	protected static $wikiList = null;
 
+	/*
+	 * Assuming that if wgRightsText is set, wiki has some commercial restrictions
+	 * @return Array wiki list [ ["id", "url", "host", "db" ], ... ]
+	 */
 	public function getCommercialUseNotAllowedWikis() {
 
-		//WikiFactory::getListOfWikisWithVar( self::LICENSE_WIKI_FACTORY_VAR_ID, "string" );
+		if ( empty( self::$wikiList ) ) {
 
+			$varId = self::LICENSE_WIKI_FACTORY_VAR_ID;
+			self::$wikiList = WikiaDataAccess::cache(
+								wfSharedMemcKey( self::CACHE_KEY_COMMERCIAL_NOT_ALLOWED ),
+								self::CACHE_VALID_TIME,
+								function() use( $varId ) {
+									$wikiList = [];
+									$list =  WikiFactory::getListOfWikisWithVar( $varId, "full", '', '' );
+									foreach ( $list as $wikiId => $val ) {
+										$wikiList[ $wikiId ] = array(
+											"id" => $wikiId,
+											"url" => $val['u'],
+											"host" => rtrim( ltrim( $val['u'], "http://" ), "/"),
+											"db" => $val['d']
+										);
+									}
+									return $wikiList;
+								}
+							);
+		}
+		return self::$wikiList;
 	}
 
 	public function isCommercialUseAllowedById( $wikiId ) {
 
-		if ( $wikiId == 3125 ) {
+		$list = $this->getCommercialUseNotAllowedWikis();
+		if ( isset( $list[ $wikiId ]) ) {
 			return false;
 		}
 		return true;
@@ -21,6 +49,13 @@ class LicensedWikisService extends WikiaModel {
 
 	public function isCommercialUseAllowedByHostName( $hostName ) {
 
+		$list = $this->getCommercialUseNotAllowedWikis();
+		foreach ( $list as $wiki ) {
+			if ( $wiki['host'] == $hostName ) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
