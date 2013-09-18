840f121dd9a47da350b7dda3b5ecdf978da8b673
jursza <jacek@wikia-inc.com>
2013-09-18 14:33:32 +0200

    PLA-763 PLA-770 LicensedWikisService adding to autoloader

--- includes/wikia/services/LicensedWikisService.class.php
+++ includes/wikia/services/LicensedWikisService.class.php
@@ -2,12 +2,21 @@
 
 class LicensedWikisService extends WikiaModel {
 
+	const LICENSE_WIKI_FACTORY_VAR_ID = 253;
+
+
 	public function getCommercialUseNotAllowedWikis() {
+
+		//WikiFactory::getListOfWikisWithVar( self::LICENSE_WIKI_FACTORY_VAR_ID, "string" );
 
 	}
 
 	public function isCommercialUseAllowedById( $wikiId ) {
 
+		if ( $wikiId == 3125 ) {
+			return false;
+		}
+		return true;
 	}
 
 	public function isCommercialUseAllowedByHostName( $hostName ) {
