df2514da482f2e30389ff7f07db9672159dff891
jursza <jacek@wikia-inc.com>
2013-09-19 15:30:25 +0200

    PLA-763 PLA-770 LicensedWikisService tests

--- includes/wikia/tests/LicensedWikisServiceTest.php
+++ includes/wikia/tests/LicensedWikisServiceTest.php
@@ -40,4 +40,50 @@ class LicensedWikisServiceTest extends WikiaBaseTest {
 		$this->assertEquals( true, $licensedWikisService->isCommercialUseAllowedByHostName( "bar-host" ) );
 	}
 
+	public function testIsCommercialUseAllowedForThisWiki() {
+
+		$licensedWikisService = $this->getMock('LicensedWikisService', ['getCommercialUseNotAllowedWikis']);
+		$this->mockGlobalVariable('wgCityId', 159);
+
+		$licensedWikisService->expects( $this->once() )
+			->method('getCommercialUseNotAllowedWikis')
+			->will( $this->returnValue( [ 159 => [	"id"=>159,
+					"url"=>"foo-url",
+					"host"=>"foo-host",
+					"db"=>"foo-db" ] ] )
+			);
+
+		$this->assertEquals( false, $licensedWikisService->isCommercialUseAllowedForThisWiki() );
+	}
+
+	public function wfDataProvider() {
+		return [
+			[ 159, "http://foo-url.com/", "foo title", "foodbname" ],
+			[ 180, "http://foo-url.com", "foo title2", "foodbd" ],
+		];
+	}
+
+	/**
+	 * @dataProvider wfDataProvider
+	 */
+	public function testGetWikisWithVar( $wikiId, $url, $wikiTitle, $dbName) {
+
+		$licensedWikisService = $this->getMock('LicensedWikisService', ['getCommercialUseNotAllowedWikis']);
+
+		$this->mockStaticMethod( 'WikiFactory', 'getListOfWikisWithVar', array( $wikiId =>
+																				array( 'u'=> $url,
+																						"t" => $wikiTitle,
+																						"d" => $dbName ) ) );
+		$class = new ReflectionClass('LicensedWikisService');
+		$method = $class->getMethod('getWikisWithVar');
+		$method->setAccessible(true);
+		$result = $method->invoke( $licensedWikisService );
+
+		$this->assertEquals( $wikiId, $result[ $wikiId ]['id'] );
+		$this->assertEquals( rtrim( ltrim( $url, "http://" ), "/"), $result[ $wikiId ]['host'] );
+		$this->assertEquals( $url, $result[ $wikiId ]['url'] );
+		$this->assertEquals( $dbName, $result[ $wikiId ]['db'] );
+		$this->assertNotEquals( $wikiId, $result[ $wikiId ]['id']*2 );
+	}
+
 }
