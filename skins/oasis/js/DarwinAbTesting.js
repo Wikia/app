c31d400e517950687c776c8d5cb6a002b718fd62
VForge <bartosz.bentkowski@gmail.com>
2013-09-19 13:43:00 +0000

    DAR-1859 | Add impression tracking with sloth.js

--- skins/oasis/js/DarwinAbTesting.js
+++ skins/oasis/js/DarwinAbTesting.js
@@ -1,5 +1,23 @@
+// ABTEST: DAR_RIGHTRAILPOSITION
+
+var hookRightRailImpression = function (label) {
+	require(['sloth'], function(sloth) {
+		sloth( {
+			on: document.getElementById('WikiaRail'),
+			threshold: 100,
+			callback: function(element) {
+				Wikia.Tracker.track({
+					action: Wikia.Tracker.ACTIONS.IMPRESSION,
+					category: 'right-rail-abtesting',
+					label: label,
+					trackingMethod: 'both'
+				}, {});
+			}
+		});
+	});
+};
+
 $(function () {
-	// ABTEST: DAR_RIGHTRAILPOSITION
 	// Determine if we're in experiment DAR_RIGHTRAILPOSITION
 	group = window.Wikia.AbTest
 		? Wikia.AbTest.getGroup( "DAR_RIGHTRAILPOSITION" )
@@ -9,8 +27,10 @@ $(function () {
     case "STATIC":
         // don't let right rail to fall down
         $('html').addClass('keep-rail-on-right');
+	    hookRightRailImpression('static');
         break;
     default:
         // no changes in behaviour
+		hookRightRailImpression('control');
 	}
 });
