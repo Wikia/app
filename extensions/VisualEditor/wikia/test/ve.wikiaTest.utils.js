def02907fd23af78482280019a4b6bed4c13ae55
Kyle Florence <kflorence@wikia-inc.com>
2013-09-19 11:15:48 -0700

    Cleanup and add JSDoc comments to functions.

--- extensions/VisualEditor/wikia/test/ve.wikiaTest.utils.js
+++ extensions/VisualEditor/wikia/test/ve.wikiaTest.utils.js
@@ -7,12 +7,12 @@
 ( function () {
 	var utils = {};
 
-	// TODO: figure out a better way to remove debug styles
-	//
-
 	/**
+	 * Disables debug mode while tests are running.
+	 *
 	 * @method
 	 * @static
+	 * @returns {Object} Setup and teardown methods for module.
 	 */
 	utils.disableDebugModeForTests = function () {
 		var debug;
@@ -28,8 +28,15 @@
 	};
 
 	/**
+	 * Generates a string which describes an objects contents.
+	 * @example 'key1: value1, key2: value2'
+	 *
 	 * @method
 	 * @static
+	 * @param {Object} obj The object to describe.
+	 * @param {String} joinStr The string used to join keys and values.
+	 * @param {String} pairStr The string used to join key/value pairs.
+	 * @returns {String} The object description string.
 	 */
 	utils.getObjectDescription = function ( obj, joinStr, pairStr ) {
 		var key,
@@ -51,17 +58,23 @@
 	};
 
 	/**
+	 * Creates the diff of an object based on value.
+	 * Null values are ignored.
+	 *
 	 * @method
 	 * @static
+	 * @param {Object} first The base object.
+	 * @param {Object} second The object to compare values from.
+	 * @returns {Object} An object containing all the different values.
 	 */
-	utils.getObjectDiff = function ( target, source ) {
+	utils.getObjectDiff = function ( first, second ) {
 		var key,
 			result = {},
 			value;
 
-		for ( key in source ) {
-			value = source[ key ];
-			if ( value !== null && target[ key ] !== value ) {
+		for ( key in second ) {
+			value = second[ key ];
+			if ( value !== null && first[ key ] !== value ) {
 				result[ key ] = value;
 			}
 		}
@@ -70,8 +83,13 @@
 	};
 
 	/**
+	 * Generates all of the permutations from the given data to produce
+	 * a set of test cases to use in unit tests.
+	 *
 	 * @method
 	 * @static
+	 * @param {Object} data An object containing arrays of possible values for each key.
+	 * @returns {Array} An array of test case permutations.
 	 */
 	utils.getTestCases = function ( data ) {
 		var i,
@@ -100,16 +118,12 @@
 	};
 
 	/**
+	 * Uppercase the first letter in a string.
+	 *
 	 * @method
 	 * @static
-	 */
-	utils.stripDebugStyles = function ( obj ) {
-		return $( obj );
-	};
-
-	/**
-	 * @method
-	 * @static
+	 * @param {String} str The string.
+	 * @returns {String} The string with the first letter uppercased.
 	 */
 	utils.ucFirst = function ( str ) {
 		return str.charAt( 0 ).toUpperCase() + str.slice( 1 );
@@ -119,6 +133,17 @@
 
 	utils.media = {};
 
+	/**
+	 * Runs the QUnit tests for generating node views from HTML DOM.
+	 * Should be used inside of a QUnit.test()
+	 *
+	 * @method
+	 * @static
+	 * @param {Object} assert QUnit.assert object
+	 * @param {String} displayType The node's display type: 'block' or 'inline'
+	 * @param {String} rdfaType The node's RDFa type: 'mw:Image' or 'mw:Video'
+	 * @param {Function} callback A function that returns the proper node view from a document node
+	 */
 	utils.media.runHtmlDomToNodeViewTests = function ( assert, displayType, rdfaType, callback ) {
 		var $fixture = $( '#qunit-fixture' ),
 			current,
@@ -159,6 +184,17 @@
 		QUnit.expect( testCases.length );
 	};
 
+	/**
+	 * Runs the QUnit tests for performing transaction changes on node views.
+	 * Should be used inside of a QUnit.test()
+	 *
+	 * @method
+	 * @static
+	 * @param {Object} assert QUnit.assert object
+	 * @param {String} displayType The node's display type: 'block' or 'inline'
+	 * @param {String} rdfaType The node's RDFa type: 'mw:Image' or 'mw:Video'
+	 * @param {Function} callback A function that returns the proper node view from a document node
+	 */
 	utils.media.runNodeViewTransactionTests = function ( assert, displayType, rdfaType, callback ) {
 		var $fixture = $( '#qunit-fixture' ),
 			current,
