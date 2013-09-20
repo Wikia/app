af59c2d518683a671e5105a2c0784e84c0ae6893
Kenneth Kouot <kenneth@wikia-inc.com>
2013-09-20 23:39:56 +0000

    basic styling added

--- extensions/wikia/VideoPageTool/templates/VideoHomePageController_header.php
+++ extensions/wikia/VideoPageTool/templates/VideoHomePageController_header.php
@@ -1,12 +1,16 @@
-<header>
-	<section class="wv-logo"></section>
-	<nav class="discover dropdown">
-		Discover
+<header class="main-header row">
+	<section class="columns med-5 large-12">
+		<a class="wv-logo" href="#"></a>
+	</section>
+	<nav class="columns med-2 large-12 discover dropdown">
+		<span>Discover</span>
 	</nav>
-	<section class="search-container">
-		<form action="">
-			<input type="text" value="search">
-			<button type="submit"></button>
+	<section class="columns med-5 large-12 hide-for-large search-container">
+		<form action="" id="WikiaSearch" class="WikiaSearch">
+			<input type="text" placeholder="Search for Wikia videos">
+			<button class="wikia-button" type="submit">
+				<div class="sprite search" width="21" height="17">
+			</button>
 		</form>
 	</section>
 </header>
