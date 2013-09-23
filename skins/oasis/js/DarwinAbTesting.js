073b8b238e355962be672c35f90eb661c49ba24e
VForge <bartosz.bentkowski@gmail.com>
2013-09-23 11:24:17 +0000

    DAR-1859 | Fire an event immediately, if possible

--- skins/oasis/js/DarwinAbTesting.js
+++ skins/oasis/js/DarwinAbTesting.js
@@ -1,39 +1,61 @@
 // ABTEST: DAR_RIGHTRAILPOSITION
 
+var checkElementVisibility = function (elem) {
+	var top = elem.offsetTop,
+		bottom = (top + elem.offsetHeight),
+		wTop = window.scrollY,
+		wBottom = (window.scrollY + window.innerHeight);
+
+	return wBottom >= top && wTop <= bottom;
+}
+
+var doTrackImpression = function (label) {
+	Wikia.Tracker.track({
+		action: Wikia.Tracker.ACTIONS.IMPRESSION,
+		category: 'abtest-right-rail',
+		label: label,
+		trackingMethod: 'both'
+	}, {});
+}
+
 var hookRightRailImpression = function (label) {
-	require(['sloth'], function(sloth) {
-		sloth( {
-			on: document.getElementById('WikiaRail'),
-			threshold: 100,
-			callback: function(element) {
-				Wikia.Tracker.track({
-					action: Wikia.Tracker.ACTIONS.IMPRESSION,
-					category: 'abtest-right-rail',
-					label: label,
-					trackingMethod: 'both'
-				}, {});
-			}
+	var elem = document.getElementById('WikiaRail');
+
+	// if element is already being visible, we don't need slothjs
+	// and we can count impression right away.
+	if ( checkElementVisibility(elem) ) {
+		doTrackImpression(label);
+	} else {
+		require(['sloth'], function(sloth) {
+			sloth( {
+				on: elem,
+				threshold: 0,
+				callback: function(element) {
+					doTrackImpression(label);
+				}
+			});
 		});
-	});
-};
-
-$(function () {
-	// Determine if we're in experiment DAR_RIGHTRAILPOSITION
-	group = window.Wikia.AbTest
-		? Wikia.AbTest.getGroup( "DAR_RIGHTRAILPOSITION" )
-		: null ;
-
-	switch (group) {
-    case "STATIC":
-        // don't let right rail to fall down
-        $('html').addClass('keep-rail-on-right');
-	    hookRightRailImpression('static');
-        break;
-	case "CONTROL":
-		// it's control group, do nothing (just track)
-		hookRightRailImpression('control');
-		break;
-    default:
-        // don't track
 	}
-});
+}
+
+
+// Determine if we're in experiment DAR_RIGHTRAILPOSITION
+group = window.Wikia.AbTest
+	? Wikia.AbTest.getGroup( "DAR_RIGHTRAILPOSITION" )
+	: null ;
+
+switch (group) {
+case "STATIC":
+	// don't let right rail to fall down
+	document.documentElement.className += " keep-rail-on-right";
+
+	// track
+	hookRightRailImpression('static');
+	break;
+case "CONTROL":
+	// it's control group, do nothing (just track)
+	hookRightRailImpression('control');
+	break;
+default:
+    // don't track
+}
