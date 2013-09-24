f3304780290677325f75c1d1b843af52e61dfb3d
Kyle Florence <kflorence@wikia-inc.com>
2013-09-24 16:47:54 -0700

    Add missing comma.

--- extensions/VisualEditor/wikia/VisualEditor.i18n.php
+++ extensions/VisualEditor/wikia/VisualEditor.i18n.php
@@ -10,6 +10,6 @@ $messages['en'] = array(
 );
 
 $messages['qqq'] = array(
-	'visualeditor-wikiamediainsertbuttontool-label' => 'Call-to-action text rendered in the toolbar button for opening the dialog to add media items to the article'
+	'visualeditor-wikiamediainsertbuttontool-label' => 'Call-to-action text rendered in the toolbar button for opening the dialog to add media items to the article',
 	'visualeditor-wikiapreference-enable' => 'The description next to the checkbox to enable or disable VisualEditor in Preferences',
 );
