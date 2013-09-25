4119482372433eef590116974df8179524678de3
Rafal Leszczynski <rafal@wikia-inc.com>
2013-09-25 16:44:40 +0200

    DAR-712, remove deprecated dependency

--- extensions/wikia/EditPreview/js/preview.js
+++ extensions/wikia/EditPreview/js/preview.js
@@ -4,7 +4,6 @@
 define( 'wikia.preview', [
 	'wikia.window',
 	'wikia.nirvana',
-	'wikia.deferred',
 	'jquery',
 	'wikia.loader',
 	'wikia.mustache',
@@ -14,7 +13,6 @@ define( 'wikia.preview', [
 ], function(
 	window,
 	nirvana,
-	deferred,
 	$,
 	loader,
 	mustache,
