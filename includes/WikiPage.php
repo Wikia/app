7cc8754ab55ca8e8cea6b224e0898263c18a266e
mech <mech@wikia-inc.com>
2013-09-25 16:05:30 +0200

    DAR-120 marking wall-related changes in MW code

--- includes/WikiPage.php
+++ includes/WikiPage.php
@@ -1349,9 +1349,9 @@ class WikiPage extends Page {
 				# creates a window where concurrent edits can cause an ignored edit conflict.
 				$ok = $this->updateRevisionOn( $dbw, $revision, $oldid, $oldIsRedirect );
 
-				// <Wikia>
+				// wikia changes begin
 				wfRunHooks( 'ArticleDoEdit', array( $dbw, $this->mTitle, $revision, $flags ) );
-				// </Wikia>
+				// wikia changes end
 
 				if ( !$ok ) {
 					/* Belated edit conflict! Run away!! */
@@ -1446,9 +1446,9 @@ class WikiPage extends Page {
 			# Update the page record with revision data
 			$this->updateRevisionOn( $dbw, $revision, 0 );
 
-			// <Wikia>
+			// wikia changes begin
 			wfRunHooks( 'ArticleDoEdit', array( &$dbw, $this->mTitle, $revision, $flags ) );
-			// </Wikia>
+			// wikia changes end
 
 			wfRunHooks( 'NewRevisionFromEditComplete', array( $this, $revision, false, $user ) );
 
