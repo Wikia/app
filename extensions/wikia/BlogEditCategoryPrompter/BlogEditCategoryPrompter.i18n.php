2c6574927c1110c6083ef52f943e306d5cbcfc64
nelson <nelson@wikia-inc.com>
2013-09-26 15:23:17 -0700

    added BlogEditCategoryPrompter, which tells users when they're about to publish a blog without any categories (set to be enabled only on community.wikia.com)

---
+++ extensions/wikia/BlogEditCategoryPrompter/BlogEditCategoryPrompter.i18n.php
@@ -0,0 +1,9 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+	'becp-desc' => 'Prompt staff on community site to add categories to their blog posts (if none present)',
+	'becp-prompt' => "You haven't added a category to this blog post. Are you sure you want to continue?",
+	'right-becp_user' => 'Is prompted to add a category to a blog post',
+	'group-becp_user' => 'Blog Edit Category Prompter users',
+);
