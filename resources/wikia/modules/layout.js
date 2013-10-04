7870a9e09b1515e231ae5f2737128e3523a14784
mech <mech@wikia-inc.com>
2013-10-04 12:59:38 +0200

    DAR-2146: create the JS layout module

---
+++ resources/wikia/modules/layout.js
@@ -0,0 +1,15 @@
+/**
+ * Helper module defining layout properties.
+ *
+ * @author: Jacek 'mech' Wozniak <mech(at)wikia-inc.com>
+ */
+
+define('wikia.csspropshelper',  function() {
+	'use strict';
+
+	return {
+		'width': 0,
+		'breakpoint': null
+	};
+
+});
