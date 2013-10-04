bfebbb9a522a3efaf21644d3df95aadcabd497fb
mech <mech@wikia-inc.com>
2013-10-04 16:23:03 +0200

    DAR-2146: defining minimum and maximum article width in the layout module

--- resources/wikia/modules/layout.js
+++ resources/wikia/modules/layout.js
@@ -4,12 +4,35 @@
  * @author: Jacek 'mech' Wozniak <mech(at)wikia-inc.com>
  */
 
-define('wikia.csspropshelper',  function() {
+define('wikia.layout',  function() {
 	'use strict';
 
+	var MIN_ARTICLE_WIDTH = 768,
+		MAX_ARTICLE_WIDTH = 1300;
+
+	/**
+	 * Public API method for getting the maximum width to which an article can be resized
+	 *
+	 * @return {number} Maximum article width in pixels
+	 */
+	function getMinArticleWidth() {
+		return MIN_ARTICLE_WIDTH;
+	}
+
+	/**
+	 * Public API method for getting the minimum width of an article
+	 *
+	 * @return {number} Minimum article width in pixels
+	 */
+	function getMaxArticleWidth() {
+		return MAX_ARTICLE_WIDTH;
+	}
+
+	/** @public **/
+
 	return {
-		'width': 0,
-		'breakpoint': null
-	};
+		getMinArticleWidth: getMinArticleWidth,
+		getMaxArticleWidth: getMaxArticleWidth
+	}
 
 });
