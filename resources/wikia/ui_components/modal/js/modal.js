b57c5898cb7f33d9cae23e41062af9d48199bd85
nAndy <nandy@wikia-inc.com>
2013-10-04 17:21:50 +0000

    extracted blackout to mixin and reused in for modals and drawer

--- resources/wikia/ui_components/modal/js/modal.js
+++ resources/wikia/ui_components/modal/js/modal.js
@@ -1,13 +1,55 @@
-define( 'wikia.ui.modal', [ 'jquery', 'wikia.window' ], function( $, w ) {
+define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 	"use strict";
 
-	function Modal() {
+	var BLACKOUT_ID = 'blackout',
+		$body = $('body');
 
-		if( !( this instanceof Modal ) ) {
-			return new Modal;
+	function Modal() {
+		var that = this;
+
+		this.$element = $( '#' + id );
+		this.$blackout = getBlackout();
+
+		function getBlackout() {
+			var $blackout = $('#blackout');
+
+			if( !$blackout.exists() ) {
+				$blackout = $('<div id="' + BLACKOUT_ID + '" />');
+				$body.append( $blackout );
+			}
+
+			$blackout.click( $.proxy(function() {
+				if( this.isShown() ) {
+					this.hide();
+				}
+			}, that) );
+
+			return $blackout;
 		}
 	}
 
+	Modal.prototype.show = function() {
+		this.$element.addClass( 'shown' );
+		this.$blackout.addClass( 'visible' );
+	};
+
+	Modal.prototype.hide = function() {
+		this.$element.removeClass( 'shown' );
+		this.$blackout.removeClass( 'visible' );
+	};
+
+	Modal.prototype.isShown = function() {
+		return this.$element.hasClass( 'shown' );
+	};
+
+	Modal.prototype.close = function() {
+		$(this).remove();
+	};
+
 	/** Public API */
-	return Modal;
+	return {
+		init: function() {
+			return new Modal();
+		}
+	}
 });
--- resources/wikia/ui_components/modal/modal.i18n.php
+++ resources/wikia/ui_components/modal/modal.i18n.php
@@ -7,6 +7,7 @@ $messages['en'] = [
 	// parameters' documentation messages
 	'styleguide-description-modal-classes' => 'An array of strings; each string is a class name passed to class attribute of modal container',
 	'styleguide-description-modal-title' => 'Title visible on the top of modal',
+	'styleguide-description-modal-close-button' => "If set to true then a close button will be displayed in the modal's header otherwise it won't be rendered; by default it's set to true",
 	'styleguide-description-modal-content' => 'Modal content; it can be a HTML markup',
 	'styleguide-description-modal-alt-link' => "OPTIONAL alternative link data in a modal's footer",
 	'styleguide-description-modal-secondary-button' => "OPTIONAL secondary button in a modal's footer; result of UIComponent->render() method (HTML markup)",
