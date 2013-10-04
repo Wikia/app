ca18a124c4d0288216a130c168331e69fc050c04
Garth Webb <gart@wikia-inc.com>
2013-10-04 13:25:18 -0700

    VID-867 Fix the i18n messages as well to elminate unused messages

--- extensions/wikia/WikiaMiniUpload/WikiaMiniUpload.i18n.php
+++ extensions/wikia/WikiaMiniUpload/WikiaMiniUpload.i18n.php
@@ -76,9 +76,6 @@ $messages['en'] = array(
 	'wmu-file-protected' => 'This image is protected',
 	'wmu-file-error' => 'There was an error with the upload process, sorry. Please try again.',
 	'wmu-file-not-found' => 'File not found',
-	'wmu-error-under-height' => 'Image was less than the minimum height ($1 px)',
-	'wmu-error-under-width' => 'Image was less than the minimum width ($1 px)',
-	'wmu-error-under-width-height' => 'Image was less than the minimum dimensions ($1 x $2 px)',
 	'wmu-error-exact-height' => 'Image height must be exactly $1 px (got $2 px)',
 	'wmu-error-exact-width' => 'Image width must be exactly $1 px (got $2 px)',
 	'wmu-error-exact-width-height' => 'Image width and height must be exactly $1 x $3 px (got $2 x $4 px)',
@@ -119,12 +116,9 @@ $messages['qqq'] = array(
 	'wmu-gallery' => '{{Identical|Gallery}}',
 	'wmu-column' => '{{Identical|Column}}',
 	'wmu-file-not-found' => 'Message displayed when a file cannot be found during the image insert step of this extention.',
-	'wmu-error-under-height' => 'Error shown when the image selected/uploaded was shorter than the required height',
-	'wmu-error-under-width' => 'Error shown when the image selected/uploaded was narrower than the required width',
-	'wmu-error-under-width-height' => 'Error shown when the image selected/uploaded was smaller than the required dimensions',
-	'wmu-error-under-height' => 'Error shown when the image selected/uploaded was not the required height',
-	'wmu-error-under-width' => 'Error shown when the image selected/uploaded was not the required width',
-	'wmu-error-under-width-height' => 'Error shown when the image selected/uploaded was not the required dimensions',
+	'wmu-error-exact-height' => 'Error shown when the image selected/uploaded was not the required height',
+	'wmu-error-exact-width' => 'Error shown when the image selected/uploaded was not the required width',
+	'wmu-error-exact-width-height' => 'Error shown when the image selected/uploaded was not the required dimensions',
 	'wmu-error-bad-aspect-ratio' => 'Error shown when the aspect ratio was incorrect'
 );
 
