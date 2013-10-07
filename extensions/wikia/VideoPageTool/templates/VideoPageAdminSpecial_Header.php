0b4023bd2ff75ccefe627a5643430068a768b0c8
lizlux <liz@lizlux.net>
2013-10-07 15:21:51 -0700

    removing unnecessary semicolons

--- extensions/wikia/VideoPageTool/templates/VideoPageAdminSpecial_Header.php
+++ extensions/wikia/VideoPageTool/templates/VideoPageAdminSpecial_Header.php
@@ -1,6 +1,6 @@
 <header class="VideoPageToolHeader">
 	<div class="VideoPageToolTitle">
-		<h1><a href="<?= $dashboardHref ?>"><?= wfMsg( 'videopagetool-header-dashboard' ); ?></a></h1>
+		<h1><a href="<?= $dashboardHref ?>"><?= wfMsg( 'videopagetool-header-dashboard' ) ?></a></h1>
 		<? if ( !empty( $section ) ): ?>
 			<h2><?= $section ?></h2>
 		<? endif ?>
@@ -16,11 +16,11 @@
 
 	<aside class="right">
 		<? if ( !empty( $lastEditTime ) ): ?>
-		<p><strong><?= wfMsg( 'videopagetool-header-right-last-saved' ); ?></strong> <?= $wg->lang->timeanddate( $lastEditTime, true ) ?></p>
+		<p><strong><?= wfMsg( 'videopagetool-header-right-last-saved' ) ?></strong> <?= $wg->lang->timeanddate( $lastEditTime, true ) ?></p>
 		<? endif ?>
 
 		<? if ( !empty( $lastEditor ) ): ?>
-		<p><strong><?= wfMsg( 'videopagetool-header-right-by' ); ?></strong> <?= $lastEditor ?></p>
+		<p><strong><?= wfMsg( 'videopagetool-header-right-by' ) ?></strong> <?= $lastEditor ?></p>
 		<? endif ?>
 	</aside>
 </header>
