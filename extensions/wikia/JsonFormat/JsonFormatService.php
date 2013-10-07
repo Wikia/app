01cd319c49fd194ddcf5b9ba29695cea313e47c5
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-10-07 18:56:44 +0200

    PLA-793  move jsonFormatSimple to ArticlesApi

--- extensions/wikia/JsonFormat/JsonFormatService.php
+++ extensions/wikia/JsonFormat/JsonFormatService.php
@@ -2,6 +2,9 @@
 
 class JsonFormatService extends \WikiaService {
 
+	const SIMPLE_JSON_SCHEMA_VERSION = 1;
+	const SIMPLE_JSON_CACHE_EXPIRATION = 14400; //4 hour
+
 	private $htmlParser;
 	private $requestContext;
 
@@ -32,12 +35,36 @@ class JsonFormatService extends \WikiaService {
 	}
 
 	/**
-	 * @param Article $article
+	 * @param \Article $article
 	 * @return JsonFormatNode
 	 */
 	public function getJsonFormatForArticle( \Article $article ) {
+		$measurement = \Wikia\Measurements\Time::start(["JsonFormatService", "getJsonFormatForArticle"]);
+
 		$html = $article->getPage()->getParserOutput( \ParserOptions::newFromContext( $this->requestContext))->getText();
 
+		$measurement->stop();
 		return $this->htmlParser->parse( $html );
+	}
+
+	/**
+	 *
+	 */
+	public function getSimpleFormatForArticle( \Article $article ) {
+		$measurement = \Wikia\Measurements\Time::start(["JsonFormatService", "getSimpleFormatForArticle"]);
+
+		$cacheKey = wfMemcKey( "SimpleJson", $article->getPage()->getId(), self::SIMPLE_JSON_SCHEMA_VERSION );
+		$jsonSimple = $this->app->wg->memc->get( $cacheKey );
+		if ( $jsonSimple === false ) {
+			$jsonFormatRootNode = $this->getJsonFormatForArticle( $article );
+
+			$simplifier = new Wikia\JsonFormat\JsonFormatSimplifier;
+			$jsonSimple = $simplifier->getJsonFormat( $jsonFormatRootNode, $article->getTitle()->getText() );
+
+			$this->app->wg->memc->set( $cacheKey, $jsonSimple, self::SIMPLE_JSON_CACHE_EXPIRATION );
+		}
+
+		$measurement->stop();
+		return $jsonSimple;
 	}
 }
