14f1cc38cc85553b84333c00541b07ffb39fc6c5
VForge <bartosz.bentkowski@gmail.com>
2013-10-08 11:45:44 +0000

    DAR-2106 | Remove un-needed code

--- extensions/wikia/CreateNewWiki/js/CreateNewWikiSupplemental.js
+++ extensions/wikia/CreateNewWiki/js/CreateNewWikiSupplemental.js
@@ -10,9 +10,6 @@ ThemeDesigner.set = function(setting, newValue) {
 			'/skins/oasis/css/oasis.scss'
 		],
 		urls = [];
-	if ( wgOasisResponsive ) {
-		paths.push( '/skins/oasis/css/core/responsive.scss' );
-	}
 
 	$.each(paths, function(i, path) {
 		urls.push($.getSassCommonURL(path, $.extend(ThemeDesigner.settings, window.applicationThemeSettings)));
