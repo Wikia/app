0d04894565c70e6f8fac9d06f09497b2154f0a79
lizlux <liz@lizlux.net>
2013-10-07 18:33:34 -0700

    VID-718 qqq

--- resources/wikia/ui_components/alert_badge/alert_badge.i18n.php
+++ resources/wikia/ui_components/alert_badge/alert_badge.i18n.php
@@ -1,10 +1,17 @@
 <?php
 
 $messages['en'] = [
-	// main component name and description
 	'styleguide-name-alert-badge' => 'Alert Badge',
 	'styleguide-description-alert-badge' => 'This badge appears in order to alert a user that there\'s some sort of notification that they should pay attention to.',
 	'styleguide-example-description-alert-badge' => 'Here\'s how the alert badge looks on its own. It will usually be combined with another element though and will be absolutely positioned to the top right of that element.',
 	'styleguide-description-alert-badge-number' => 'The number displayed in the circle',
 	'styleguide-description-alert-badge-id' => 'The css id to be added to the span element',
+];
+
+$messages['qqq'] = [
+	'styleguide-name-alert-badge' => 'The name of the ui component',
+	'styleguide-description-alert-badge' => 'The description of the ui component',
+	'styleguide-example-description-alert-badge' => 'Summary of an example usage of the ui component',
+	'styleguide-description-alert-badge-number' => 'Description of the "number" parameter for docs',
+	'styleguide-description-alert-badge-id' => 'Description of the "id" parameter for docs',
 ];
