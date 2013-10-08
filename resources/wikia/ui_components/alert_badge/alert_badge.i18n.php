97485dc203b9954ccaa4ed520c7e02c2973ce276
lizlux <liz@lizlux.net>
2013-10-07 17:52:21 -0700

    VID-718 adding more code for alert badge ui component

---
+++ resources/wikia/ui_components/alert_badge/alert_badge.i18n.php
@@ -0,0 +1,8 @@
+<?php
+
+$messages['en'] = [
+	// main component name and description
+	'styleguide-name-alert-badge' => 'Alert Badge',
+	'styleguide-description-alert-badge' => 'This badge appears in order to alert a user that there\'s some sort of notification that they should pay attention to.',
+	'styleguide-example-description-alert-badge' => 'Here\'s how the alert badge looks on its own. It will usually be combined with another element though and will be absolutely positioned to the top right of that element.',
+];
