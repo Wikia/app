c9190bc92934438945d0b8ae26c2aab92e26db1e
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-10-08 14:38:53 +0200

    PLA-793 include strike in inline elements

--- extensions/wikia/JsonFormat/HtmlParser.php
+++ extensions/wikia/JsonFormat/HtmlParser.php
@@ -69,7 +69,7 @@ class HtmlParser {
 		$compositeVisitor->addVisitor( new \BVisitor($compositeVisitor, $jsonFormatTraversingState) );
 		$compositeVisitor->addVisitor( new \IVisitor($compositeVisitor, $jsonFormatTraversingState) );
 
-		$compositeVisitor->addVisitor( new \InlineVisitor($compositeVisitor, $jsonFormatTraversingState, ['span', 'center', "strong"]) );
+		$compositeVisitor->addVisitor( new \InlineVisitor($compositeVisitor, $jsonFormatTraversingState, ['span', 'center', "strong", "strike"]) );
 
 
 		return $compositeVisitor;
