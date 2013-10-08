570da1a212991554f772933532db27f6338f5b53
hakubo <bukaj.kelo+github@gmail.com>
2013-10-08 13:37:23 +0200

    MOB-354: Lazy load comments based on scroll position

--- extensions/wikia/ArticleComments/js/ArticleComments_init.wikiamobile.js
+++ extensions/wikia/ArticleComments/js/ArticleComments_init.wikiamobile.js
@@ -7,7 +7,10 @@
  * @author Federico "Lox" Lucignano <federico(at)wikia-inc.com>
  **/
 
-require(['throbber', 'wikia.querystring', 'wikia.loader', 'wikia.nirvana'], function(throbber, qs, loader, nirvana){
+require(['throbber', 'wikia.querystring', 'wikia.loader', 'wikia.nirvana', 'sloth'],
+	function(throbber, qs, loader, nirvana, sloth){
+	'use strict';
+
 	var hash = qs().getHash(),
 		wkArtCom,
 		collSec,
@@ -80,12 +83,18 @@ require(['throbber', 'wikia.querystring', 'wikia.loader', 'wikia.nirvana'], func
 		collSec = wkArtCom.getElementsByClassName('collSec')[0];
 		wkComm = document.getElementById('wkComm');
 
-		if(open){
-			init();
-			collSec.className += ' open';
-			wkComm.className += ' open';
-		}else{
-			$(collSec).one(clickEvent, init);
-		}
+		sloth({
+			on: collSec,
+			threshold: 300,
+			callback: init
+		});
+
+//		if(open){
+//			init();
+//			collSec.className += ' open';
+//			wkComm.className += ' open';
+//		}else{
+//			$(collSec).one(clickEvent, init);
+//		}
 	});
 });
