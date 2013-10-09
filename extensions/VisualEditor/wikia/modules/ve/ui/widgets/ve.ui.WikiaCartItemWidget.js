a2b45e665ef6d2403a12ec9e8af1956472c9ba0d
Inez Korczynski <inez@wikia-inc.com>
2013-10-09 20:13:53 +0000

    Adjust ve.ui.WikiaCartItemWidget to use require in order to load wikia.thumbnailer.

--- extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaCartItemWidget.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaCartItemWidget.js
@@ -2,16 +2,18 @@ ve.ui.WikiaCartItemWidget = function VeUiWikiaCartItemWidget( model, config ) {
 	this.model = model;
 	ve.ui.OptionWidget.call( this, this.model.title, config );
 
-	var imgDimension = 60;
-	var $img = this.$$( '<img>' );
-	$img
-		.attr( {
-			'src': Wikia.Thumbnailer.getThumbURL( model.url, 'image', imgDimension, imgDimension ),
-			'height': imgDimension,
-			'width': imgDimension
-		} )
-		.addClass( 've-ui-wikiaCartImage' );
-	this.$.prepend( $img );
+	var size = 60;
+
+	require( ['wikia.thumbnailer'], ve.bind( function ( thumbnailer ) {
+		this.$$( '<img>' )
+			.attr( {
+				'src': thumbnailer.getThumbURL( this.model.url, 'image', size, size ),
+				'height': size,
+				'width': size
+			} )
+			.addClass( 've-ui-wikiaCartImage' )
+			.prependTo( this.$ );
+	}, this ) );
 };
 
 ve.inheritClass( ve.ui.WikiaCartItemWidget, ve.ui.OptionWidget );
