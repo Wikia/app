a67b3d8a7f0d06f4458a9447eac8a37aeb17a9a4
Christian Williams <christian@wikia-inc.com>
2013-10-09 18:49:16 +0000

    adding dimensions to image tag

--- extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaCartItemWidget.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaCartItemWidget.js
@@ -2,9 +2,14 @@ ve.ui.WikiaCartItemWidget = function VeUiWikiaCartItemWidget( model, config ) {
 	this.model = model;
 	ve.ui.OptionWidget.call( this, this.model.title, config );
 
+	var imgDimension = 60;
 	var $img = this.$$( '<img>' );
 	$img
-		.attr( 'src', Wikia.Thumbnailer.getThumbURL(model.url, 'image', 60, 60) )
+		.attr( {
+			'src': Wikia.Thumbnailer.getThumbURL( model.url, 'image', imgDimension, imgDimension ),
+			'height': imgDimension,
+			'width': imgDimension
+		} )
 		.addClass( 've-ui-wikiaCartImage' );
 	this.$.prepend( $img );
 };
