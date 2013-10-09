1791776fb9ebe148c1f5713190eea1d06f100891
tor <tor@wikia-inc.com>
2013-10-09 00:30:28 +0000

    fixed variable names in Signup controller

--- extensions/wikia/UserLogin/UserSignupSpecialController.class.php
+++ extensions/wikia/UserLogin/UserSignupSpecialController.class.php
@@ -492,16 +492,14 @@ class UserSignupSpecialController extends WikiaSpecialPageController {
 	}
 
 	private function disableCaptchaForAutomatedTests() {
-		global $wgHooks, $wgAutomatedTestsIP;
-
 		//Disable captcha for automated tests
-		if ( in_array( $this->wg->Request->getIP(), $wgAutomatedTestsIPsList ) && $this->wg->Request->getInt( 'nocaptchatest' ) == 1 ) {
+		if ( in_array( $this->wg->Request->getIP(), $this->wg->AutomatedTestsIPsList ) && $this->wg->Request->getInt( 'nocaptchatest' ) == 1 ) {
 			//Switch off global var
 			$this->wg->WikiaEnableConfirmEditExt = false;
 			//Remove hook function
 			$hookArrayKey = array_search( 'ConfirmEditHooks::confirmUserCreate', $this->wg->Hooks['AbortNewAccount'] );
 			if ( $hookArrayKey ) {
-				unset($wgHooks['AbortNewAccount'][$hookArrayKey]);
+				unset($this->wg->Hooks['AbortNewAccount'][$hookArrayKey]);
 			}
 			$this->wg->Out->addJsConfigVars([
 				'wgUserLoginDisableCaptcha' => true
