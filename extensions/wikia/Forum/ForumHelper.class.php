8d24a9efd758560d77b7b77df932cf47ec85025d
mech <mech@wikia-inc.com>
2013-10-09 12:29:28 +0200

    DAR-2351 fixing wall fatals

--- extensions/wikia/Forum/ForumHelper.class.php
+++ extensions/wikia/Forum/ForumHelper.class.php
@@ -9,7 +9,13 @@ class ForumHelper {
 				return true;
 			} else if ($wgTitle->getNamespace() === NS_USER_WALL_MESSAGE) {
 				$mainTitle = Title::newFromId($wgTitle->getText());
-				return in_array($mainTitle->getNamespace(), self::$forumNamespaces);
+				/*
+				 * if we visit the thread using Thread:xxxxx url, $wgTitle's namespace is 1201 for both, wall and forum
+				 * threads. But when we create new title from it using newFromId, we're able to distinguish those two
+				 */
+				if ($mainTitle instanceof Title) {
+					return in_array($mainTitle->getNamespace(), self::$forumNamespaces);
+				}
 			}
 		}
 		return false;
