4b48f8c95896bcccb6c6676af1fdb61fc8a751af
lizlux <liz@lizlux.net>
2013-10-08 17:29:10 -0700

    VID-718 updating styles to allow for larger/wider numbers and updating examples in styleguide

--- resources/wikia/ui_components/alert_badge/alert_badge.i18n.php
+++ resources/wikia/ui_components/alert_badge/alert_badge.i18n.php
@@ -4,6 +4,7 @@ $messages['en'] = [
 	'styleguide-name-alert-badge' => 'Alert Badge',
 	'styleguide-description-alert-badge' => 'This badge appears in order to alert a user that there\'s some sort of notification that they should pay attention to.',
 	'styleguide-example-description-alert-badge' => 'Here\'s how the alert badge looks on its own. It will usually be combined with another element though and will be absolutely positioned to the top right of that element.',
+	'styleguide-example-description-alert-badge-wider' => 'If there\'s more than one digit, the circle just gets wider.',
 	'styleguide-description-alert-badge-number' => 'The number displayed in the circle',
 	'styleguide-description-alert-badge-id' => 'The css id to be added to the span element',
 ];
@@ -12,6 +13,7 @@ $messages['qqq'] = [
 	'styleguide-name-alert-badge' => 'The name of the ui component',
 	'styleguide-description-alert-badge' => 'The description of the ui component',
 	'styleguide-example-description-alert-badge' => 'Summary of an example usage of the ui component',
+	'styleguide-example-description-alert-badge-wider' => 'Summary of an example usage of the ui component',
 	'styleguide-description-alert-badge-number' => 'Description of the "number" parameter for docs',
 	'styleguide-description-alert-badge-id' => 'Description of the "id" parameter for docs',
 ];
