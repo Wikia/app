55207353125a999c66d32bf9fc61da371bbc8551
Krzysztos 'suchy' Suchanek <suchy@wikia-inc.com>
2013-10-09 14:08:41 +0200

    PLA-722 hub rss

--- extensions/wikia/HubRssFeed/tests/HubRssServiceTest.php
+++ extensions/wikia/HubRssFeed/tests/HubRssServiceTest.php
@@ -9,8 +9,7 @@
 
 class HubRssServiceTest extends WikiaBaseTest {
 
-	public function setUp()
-	{
+	public function setUp() {
 		$dir = dirname( __FILE__ ) . '/../';
 		require_once $dir . '../WikiFactory/Hubs/WikiFactoryHub.php';
 		$this->setupFile = $dir . 'HubRssFeed.setup.php';
@@ -18,41 +17,83 @@ class HubRssServiceTest extends WikiaBaseTest {
 	}
 
 	public function testDataToXml() {
-		$service = new HubRssFeedService('en','http://www.google.pl');
 
-		$prop = new ReflectionProperty($service,'descriptions');
-		$prop->setAccessible(true);
-		$val = $prop->getValue($service);
+		$service = new HubRssFeedService('xx', 'yy');
 
-		reset($val);
+		$prop = new ReflectionProperty($service, 'descriptions');
+		$prop->setAccessible( true );
+		$val = $prop->getValue( $service );
 
-		$hub = key($val);
-		$date1 =  strtotime('2010-01-01');
-		$date2 = strtotime('2011-01-01');
+		reset( $val );
 
-		$inputData= [
-			['title' =>'title1', 'img'=>'img1',	'timestamp'=>$date1, 'description'=>'description1' ],//2010-01-01
-			['title' =>'title2', 'img'=>'img2',	'timestamp'=>$date2, 'description'=>'description2' ] //2011-01-01
+		$hub = key( $val );
+		$date1 = strtotime( '2010-01-01' );
+		$date2 = strtotime( '2011-01-01' );
+
+		$inputData = [
+			['title' => 'title1', 'img' => 'img1', 'timestamp' => $date1, 'description' => 'description1'], //2010-01-01
+			['title' => 'title2', 'img' => 'img2', 'timestamp' => $date2, 'description' => 'description2'] //2011-01-01
 		];
 
-		$xml = $service->dataToXml($inputData, $hub);
-		//$xml = simplexml_load_string($xml);
-		//var_dump($xml);
+		$xml = $service->dataToXml( $inputData, $hub );
+
 		$doc = new DOMDocument;
-		$doc->loadXML($xml);
 
-		$this->assertInstanceOf('DOMDocument',$doc);
+		$doc->loadXML( $xml );
 
-		$buildDateItems = $doc->getElementsByTagName('lastBuildDate');
-		$buildDate = $buildDateItems->item(0);
-
-		$this->assertEquals($date2,strtotime($buildDate->textContent));
+		$this->assertInstanceOf( 'DOMDocument', $doc );
 
 		$rss = $doc->firstChild;
 
-		$this->assertEquals('rss',$rss->nodeName);
+		$this->assertEquals( 'rss', $rss->nodeName );
 
+		$xpath = new DOMXpath($doc);
 
+		/* checking for channel node */
+		$this->assertEquals( 1, $xpath->query( '//rss/channel' )->length );
+
+		$this->assertEquals( 1, $xpath->query( '//rss/channel/title[string-length() > 0]' )->length );
+
+		$this->assertEquals( 1, $xpath->query( '//rss/channel/description[string-length() > 0]' )->length );
+
+		$this->assertEquals( 1, $xpath->query( '//rss/channel/language[.="xx"]' )->length );
+
+		$this->assertEquals( 1, $xpath->query( '//rss/channel/link[.="yy"]' )->length );
+
+		/*checking for date*/
+		$pubDate = date( HubRssFeedService::DATE_FORMAT, max( $date1, $date2 ) );
+
+		$res = $xpath->query( '//rss/channel/lastBuildDate[.="' . $pubDate . '"]' );
+		$this->assertEquals( 1, $res->length );
+
+		/* checking for items */
+		$items = $xpath->query( '//rss/channel/item' );
+		$this->assertEquals( 2, $items->length );
+
+		foreach ( $inputData as $key => $data ) {
+			$item = $items->item( $key );
+
+			$this->assertEquals( 1, $xpath->query( 'title[.="' . $data[ 'title' ] . '"]', $item )->length );
+			$this->assertEquals( 1, $xpath->query( 'pubDate[.="' . date( HubRssFeedService::DATE_FORMAT, $data[ 'timestamp' ] ) . '"]', $item )->length );
+			$this->assertEquals( 1, $xpath->query( 'dc:creator[.="Wikia"]', $item )->length );
+
+			$desc = $xpath->query( 'description', $item );
+
+			$descHTML = $desc->item( 0 )->nodeValue;
+
+			$descDoc = new DOMDocument();
+			$descDoc->loadHTML( $descHTML );
+
+			$descBody = $descDoc->getElementsByTagName( 'body' )->item( 0 );
+
+			$this->assertInstanceOf( 'DOMElement', $descBody );
+
+			$descXpath = new DOMXPath($descDoc);
+
+			$this->assertEquals( 1, $descXpath->query( 'img[@src="' . $data[ 'img' ] . '"]', $descBody )->length );
+			$this->assertEquals( 1, $descXpath->query( 'p[.="' . $data[ 'description' ] . '"]', $descBody )->length );
+
+		}
 
 	}
 
