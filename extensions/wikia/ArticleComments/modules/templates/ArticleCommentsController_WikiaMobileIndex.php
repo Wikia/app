0aa5d747cb8188521adeca2cc4aefd117c5fb4b7
hakubo <bukaj.kelo+github@gmail.com>
2013-10-10 17:41:42 +0200

    MOB-354: Update Article Comments template

--- extensions/wikia/ArticleComments/modules/templates/ArticleCommentsController_WikiaMobileIndex.php
+++ extensions/wikia/ArticleComments/modules/templates/ArticleCommentsController_WikiaMobileIndex.php
@@ -1,4 +1,4 @@
 <section id=wkArtCom data-pages="<?= $pagesCount ;?>">
-	<h1 class='collSec addChev'><?= wfMsgExt( 'wikiamobile-article-comments-header', array('parsemag', 'content'), $wg->ContLang->formatNum( $countCommentsNested ) ) ;?><? if ( $requestedPage < 1 ) :?><a id=wkShowCom href="?page=1#wkArtCom"><?= wfMsg( 'wikiamobile-article-comments-show' ) ;?></a><? endif ;?></h1>
+	<h2 class='collSec addChev'><?= wfMessage( 'wikiamobile-article-comments-header')->inContentLanguage()->params( $wg->ContLang->formatNum( $countCommentsNested ) )->parse() ;?><? if ( $requestedPage < 1 ) :?><a id=wkShowCom href="?page=1#wkArtCom"><?= wfMsg( 'wikiamobile-article-comments-show' ) ;?></a><? endif ;?></h2>
 	<div id=wkComm><? if ( $requestedPage > 0 ) :?><?= $app->renderView( 'ArticleCommentsController', 'WikiaMobileCommentsPage', array( 'error' => ( !empty( $error ) ) ? $error : false ) ) ;?><? endif ;?></div>
 </section>
