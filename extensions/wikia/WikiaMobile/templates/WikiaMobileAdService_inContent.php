8b1886c8d4cb7d7f29f0c7cd6abf32dcdccfabf6
hakubo <bukaj.kelo+github@gmail.com>
2013-10-11 16:12:49 +0200

    MOB-498: Add group F with footer in content ad

--- extensions/wikia/WikiaMobile/templates/WikiaMobileAdService_inContent.php
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileAdService_inContent.php
@@ -1,31 +1,44 @@
 <script>window.addEventListener('load', function () {
-if(Wikia.AbTest && ['A', 'C', 'D'].indexOf(Wikia.AbTest.getGroup("WIKIAMOBILEADSLOTS")) != -1){
-		require(['ads', 'sloth', 'jquery', 'JSMessages'], function (ads, sloth, $, msg) {
-			var MIN_ZEROTH_SECTION_LENGTH = 1000,
-				$firstSection = $('.collSec');
+var MIN_ZEROTH_SECTION_LENGTH = 700,
+	MIN_PAGE_LENGTH = 2000;
 
-			if($firstSection.length && $firstSection.offset().top > MIN_ZEROTH_SECTION_LENGTH){
-
-				$firstSection.before('<div id=wkAdInContent />');
-
-				sloth({
-					on: document.getElementById('wkAdInContent'),
-					threshold: 500,
-					callback: function(adWrapper){
-						ads.setupSlot({
-							name: 'MOBILE_IN_CONTENT',
-							size: '300x250',
-							wrapper: adWrapper,
-							init: function(found){
-								if(found) {
-									adWrapper.innerHTML += '<label class="wkAdLabel inContent">' + msg('wikiamobile-ad-label') + '<label>';
-									adWrapper.className = 'show';
-								}
-							}
-						});
+require(['ads', 'sloth', 'jquery', 'JSMessages'], function (ads, sloth, $, msg) {
+	var $firstSection = $('.collSec').first(),
+		$footer = $('#wkMainCntFtr'),
+		firstSectionTop = ($firstSection.length && $firstSection.offset().top) || 0,
+		showInContent = firstSectionTop > MIN_ZEROTH_SECTION_LENGTH,
+		showBeforeFooter = document.height > MIN_PAGE_LENGTH || firstSectionTop < MIN_ZEROTH_SECTION_LENGTH,
+		onEnter = function onEnter(adWrapper){
+			ads.setupSlot({
+				name: 'MOBILE_IN_CONTENT',
+				size: '300x250',
+				wrapper: adWrapper,
+				init: function onInit(found){
+					if(found) {
+						adWrapper.innerHTML += '<label class="wkAdLabel inContent">' + msg('wikiamobile-ad-label') + '<label>';
+						adWrapper.className += ' show';
 					}
-				})
-			}
-		});
-}
+				}
+			});
+		},
+		lazyLoadAd = function(elem){
+			sloth({
+				on: elem,
+				threshold: 500,
+				callback: onEnter
+			})
+		};
+
+	if(showInContent && Wikia.AbTest && ['A', 'C', 'D', 'F'].indexOf(Wikia.AbTest.getGroup("WIKIAMOBILEADSLOTS")) != -1){
+		$firstSection.before('<div id=wkAdInContent class=ad-in-content />');
+		lazyLoadAd(document.getElementById('wkAdInContent'));
+	}
+
+	if(showBeforeFooter && Wikia.AbTest && ['F'].indexOf(Wikia.AbTest.getGroup("WIKIAMOBILEADSLOTS")) != -1){
+		$footer.after('<div id=wkAdBeforeFooter class=ad-in-content />');
+		lazyLoadAd(document.getElementById('wkAdBeforeFooter'));
+	}
+
+	sloth();
+});
 });</script>
