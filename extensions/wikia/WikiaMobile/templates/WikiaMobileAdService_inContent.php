51a18a8d71db61ec7f9a95a1e8f06a441f96373d
hakubo <bukaj.kelo+github@gmail.com>
2013-10-14 11:09:40 +0200

    WikiaMobile: Rename incontent to prefooter (cherry picked from commit 7baa891)

--- extensions/wikia/WikiaMobile/templates/WikiaMobileAdService_inContent.php
+++ extensions/wikia/WikiaMobile/templates/WikiaMobileAdService_inContent.php
@@ -8,35 +8,34 @@ require(['ads', 'sloth', 'jquery', 'JSMessages'], function (ads, sloth, $, msg) 
 		firstSectionTop = ($firstSection.length && $firstSection.offset().top) || 0,
 		showInContent = firstSectionTop > MIN_ZEROTH_SECTION_LENGTH,
 		showBeforeFooter = document.height > MIN_PAGE_LENGTH || firstSectionTop < MIN_ZEROTH_SECTION_LENGTH,
-		onEnter = function onEnter(adWrapper){
-			ads.setupSlot({
-				name: 'MOBILE_IN_CONTENT',
-				size: '300x250',
-				wrapper: adWrapper,
-				init: function onInit(found){
-					if(found) {
-						adWrapper.innerHTML += '<label class="wkAdLabel inContent">' + msg('wikiamobile-ad-label') + '<label>';
-						adWrapper.className += ' show';
-					}
-				}
-			});
-		},
-		lazyLoadAd = function(elem){
+		lazyLoadAd = function(elem, slotName){
 			sloth({
 				on: elem,
 				threshold: 500,
-				callback: onEnter
+				callback: function onEnter(adWrapper){
+					ads.setupSlot({
+						name: slotName,
+						size: '300x250',
+						wrapper: adWrapper,
+						init: function onInit(found){
+							if(found) {
+								adWrapper.innerHTML += '<label class="wkAdLabel inContent">' + msg('wikiamobile-ad-label') + '<label>';
+								adWrapper.className += ' show';
+							}
+						}
+					});
+				}
 			})
 		};
 
 	if(showInContent && Wikia.AbTest && ['A', 'C', 'D', 'F'].indexOf(Wikia.AbTest.getGroup("WIKIAMOBILEADSLOTS")) != -1){
 		$firstSection.before('<div id=wkAdInContent class=ad-in-content />');
-		lazyLoadAd(document.getElementById('wkAdInContent'));
+		lazyLoadAd(document.getElementById('wkAdInContent'), 'MOBILE_IN_CONTENT');
 	}
 
 	if(showBeforeFooter && Wikia.AbTest && ['F'].indexOf(Wikia.AbTest.getGroup("WIKIAMOBILEADSLOTS")) != -1){
 		$footer.after('<div id=wkAdBeforeFooter class=ad-in-content />');
-		lazyLoadAd(document.getElementById('wkAdBeforeFooter'));
+		lazyLoadAd(document.getElementById('wkAdBeforeFooter'), 'MOBILE_PREFOOTER');
 	}
 
 	sloth();
