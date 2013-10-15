180f62b2ca125d3633fd41b37b11045a7c6a0f44
nAndy <nandy@wikia-inc.com>
2013-10-15 10:45:31 +0000

    javascript logic for calculating hight

--- resources/wikia/ui_components/modal/js/modal.js
+++ resources/wikia/ui_components/modal/js/modal.js
@@ -17,6 +17,10 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 			this.hide();
 		}, that ) );
 
+		$(window).bind( 'resize', $.proxy( function () {
+			this.calculateHeight();
+		}, that ) );
+
 		function getBlackout() {
 			var blackoutId = BLACKOUT_CLASS + '_' + id,
 				$blackout = $('#' + blackoutId );
@@ -37,6 +41,8 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 		$html.addClass( 'modal-shown' );
 		this.$element.addClass( 'shown' );
 		this.$blackout.addClass( 'visible' );
+
+		this.calculateHeight();
 	};
 
 	Modal.prototype.hide = function() {
@@ -51,6 +57,25 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 
 	Modal.prototype.close = function() {
 		$(this).remove();
+	};
+
+	Modal.prototype.calculateHeight = function() {
+		var $header = this.$element.find("header"),
+			$section = this.$element.find("section"),
+			$footer = this.$element.find("footer"),
+			currentModalHeight = this.$element.outerHeight() || 0,
+			currentHeaderHeight = $header.outerHeight() || 0,
+			currentSectionHeight = $section.outerHeight() || 0,
+			currentFooterHeight = $footer.outerHeight() || 0,
+			// once the currentSectionHeight is higher than maxSection we're going to change positioning of footer
+			// therefor, we need to multiply it here
+			headerFooterHeightSum = currentHeaderHeight + ( 2 * currentFooterHeight ),
+			maxSectionHeight = currentModalHeight - headerFooterHeightSum;
+
+		if( currentSectionHeight > maxSectionHeight ) {
+			$footer.css( "position", "relative" );
+			$section.height( maxSectionHeight );
+		}
 	};
 
 	/** Public API */
