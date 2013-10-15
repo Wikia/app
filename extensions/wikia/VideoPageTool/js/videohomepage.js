0583be5099f13a4966d6c8a3b73b034f8f00c6c7
Kenneth Kouot <kenneth@wikia-inc.com>
2013-10-15 02:17:04 +0000

    convert videohomepage featured to backbone view, convert ellipses to jquery amd plugin

---
+++ extensions/wikia/VideoPageTool/js/videohomepage.js
@@ -0,0 +1,8 @@
+define( 'wikia.videohomepage', [
+		/* inject AMD'd Backbone here */
+		'models.videohomepage.featured',
+		'views.videohomepage.index',
+		'views.videohomepage.featured',
+		'views.videohomepage.search',
+	], function() {
+});
