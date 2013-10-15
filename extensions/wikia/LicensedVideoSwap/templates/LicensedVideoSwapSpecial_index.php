ac0bd573e184c2f77546a841df56e26ea28118a6
Kenneth Kouot <kenneth@wikia-inc.com>
2013-10-15 22:51:05 +0000

    setup pagination markup

--- extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_index.php
+++ extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_index.php
@@ -26,3 +26,30 @@
 <? else: ?>
 	<p class="lvs-zero-state"><?= wfMessage( 'lvs-zero-state' )->plain() ?></p>
 <? endif; ?>
+<? $currentPage = 1; ?>
+<? $pagesNum = 6; ?>
+
+<div class="wikia-paginator">
+	<ul>
+		<? if( $currentPage > 1 ): ?>
+			<li><a class="paginator-prev button secondary" href=""><span><?= wfMessage('paginator-back')->plain() ?></span></a></li>
+		<? else: ?>
+			<li><span class="paginator-prev disabled"><span><?= wfMessage('paginator-back')->plain() ?></span></span></li>
+		<? endif; ?>
+
+		<!-- Loop through pages, creating a paginator link for each -->
+		<? for( $i = 1; $i <= 6; $i++ ): ?>
+			<? if( $i == $currentPage ): ?>
+				<li><span class="active paginator-page"><?= $i ?></span></li>
+			<? else: ?>
+				<li><a class="paginator-page" href=""><?= $i ?></a></li>
+			<? endif; ?>
+		<? endfor; ?>
+		
+		<? if( $currentPage < $pagesNum ): ?>
+			<li><a class="paginator-next button secondary" href=""><span><?= wfMessage('paginator-next')->plain() ?></span></a></li>
+		<? else: ?>
+			<li><span class="paginator-next disabled"><span><?= wfMessage('paginator-next')->plain() ?></span></span></li>
+		<? endif; ?>
+	</ul>
+</div>
