2a4c1e0ce501db33f3dd13f2ac1f3b492b9ff3e6
saipetch <saipetch@wikia-inc.com>
2013-10-17 10:41:41 -0700

    Video (vid-758): LicensedVideoSwap - update maintenance script

--- maintenance/wikia/VideoHandlers/lvsUpdateStatus.php
+++ maintenance/wikia/VideoHandlers/lvsUpdateStatus.php
@@ -6,131 +6,119 @@
 * @author Saipetch Kongkatong
 */
 
-ini_set( 'include_path', dirname( __FILE__ ).'/../../' );
+class LvsStatus {
 
-require_once( 'commandLine.inc' );
+	public static function lvsUpdateStatus( DatabaseMysql $db, $dbname, $verbose = false, $dryRun = false ) {
+		echo "Wiki: $dbname\n";
 
-if ( isset( $options['help'] ) ) {
-	die( "Usage: php lvsUpdateStatus.php [--help] [--dry-run]
-	--dry-run                   dry run
-	--help                      you are reading it right now\n\n" );
-}
+		$limit = 5000;
+		$total = 0;
+		$kept = 0;
+		$swapped = 0;
+		$swappedExact = 0;
+		$suggestions = 0;
+		$totalAffected = 0;
 
-if ( empty( $wgCityId ) ) {
-	die( "Error: Invalid wiki id.\n" );
-}
+		$statusInfo = WPP_LVS_STATUS_INFO;
+		$statusSuggest = WPP_LVS_SUGGEST;
+		$status = WPP_LVS_STATUS;
 
-$dryRun = isset( $options['dry-run'] );
-
-echo "Wiki: $wgCityId ($wgDBname)\n";
-
-$db = wfGetDB( DB_MASTER );
-
-$limit = 5000;
-$total = 0;
-$kept = 0;
-$swapped = 0;
-$swappedExact = 0;
-$suggestions = 0;
-$totalAffected = 0;
-
-$statusInfo = WPP_LVS_STATUS_INFO;
-$statusSuggest = WPP_LVS_SUGGEST;
-$status = WPP_LVS_STATUS;
-
-$sqls[] = <<<SQL
-		SELECT p1.page_id, p1.props as suggestions,
-			substring(p2.props, locate('"status";i:', p2.props)+11, 1) status
-		FROM page_wikia_props p1
-		LEFT JOIN page_wikia_props p2 ON p1.page_id = p2.page_id AND p2.propname = $statusInfo
-		WHERE p1.propname = $statusSuggest
-		ORDER by p1.page_id
-		LIMIT $limit
+		$sqls[] = <<<SQL
+				SELECT p1.page_id, p1.props as suggestions,
+					substring(p2.props, locate('"status";i:', p2.props)+11, 1) status
+				FROM page_wikia_props p1
+				LEFT JOIN page_wikia_props p2 ON p1.page_id = p2.page_id AND p2.propname = $statusInfo
+				WHERE p1.propname = $statusSuggest
+				ORDER by p1.page_id
+				LIMIT $limit
 SQL;
 
-$sqls[] = <<<SQL
-		SELECT p1.page_id, '' as suggestions,
-			substring(p1.props, locate('"status";i:', p1.props)+11, 1) status
-		FROM page_wikia_props p1
-		LEFT JOIN page_wikia_props p2 ON p1.page_id = p2.page_id AND p2.propname = $statusSuggest
-		WHERE p1.propname = $statusInfo AND p2.page_id is null
-		ORDER by p1.page_id
-		LIMIT $limit
+		$sqls[] = <<<SQL
+				SELECT p1.page_id, '' as suggestions,
+					substring(p1.props, locate('"status";i:', p1.props)+11, 1) status
+				FROM page_wikia_props p1
+				LEFT JOIN page_wikia_props p2 ON p1.page_id = p2.page_id AND p2.propname = $statusSuggest
+				WHERE p1.propname = $statusInfo AND p2.page_id is null
+				ORDER by p1.page_id
+				LIMIT $limit
 SQL;
 
-foreach ( $sqls as $sql ) {
-	echo "SQL: $sql\n";
+		foreach ( $sqls as $sql ) {
+			echo "SQL: $sql\n";
 
-	do {
-		$result = $db->query( $sql, __METHOD__ );
+			do {
+				$result = $db->query( $sql, __METHOD__ );
 
-		$pages = $result->numRows();
-		echo "Total Pages: $pages\n";
+				$pages = $result->numRows();
+				echo "Total Pages: $pages\n";
 
-		$cnt = 1;
-		$total = $total + $pages;
+				$cnt = 1;
+				$total = $total + $pages;
 
-		while ( $row = $db->fetchObject( $result ) ) {
-			$pageId = $row->page_id;
-			echo "\tPage $pageId [$cnt of $pages]: ";
+				while ( $row = $db->fetchObject( $result ) ) {
+					$pageId = $row->page_id;
+					echo "\tPage $pageId [$cnt of $pages]: ";
 
-			$flags = array();
-			$statusList = array();
+					$flags = array();
+					$statusList = array();
 
-			// video with suggestions
-			if( !empty( $row->suggestions ) ) {
-				$statusList[] = "STATUS_SWAPPABLE";
-				$flags[] = LicensedVideoSwapHelper::STATUS_SWAPPABLE;
-				$suggestions++;
-			}
+					// video with suggestions
+					if( !empty( $row->suggestions ) ) {
+						$statusList[] = "STATUS_SWAPPABLE";
+						$flags[] = LicensedVideoSwapHelper::STATUS_SWAPPABLE;
+						$suggestions++;
+					}
 
-			// kept video
-			if ( !empty( $row->status) && $row->status == 1 ) {
-				$statusList[] = "STATUS_KEEP";
-				$flags[] = LicensedVideoSwapHelper::STATUS_KEEP;
-				$kept++;
-			}
+					// kept video
+					if ( !empty( $row->status) && $row->status == 1 ) {
+						$statusList[] = "STATUS_KEEP";
+						$flags[] = LicensedVideoSwapHelper::STATUS_KEEP;
+						$kept++;
+					}
 
-			// swapped video
-			if ( !empty( $row->status) && $row->status == 2 ) {
-				$statusList[] = "STATUS_SWAP";
-				$flags[] = LicensedVideoSwapHelper::STATUS_SWAP;
-				$swapped++;
-			}
+					// swapped video
+					if ( !empty( $row->status) && $row->status == 2 ) {
+						$statusList[] = "STATUS_SWAP";
+						$flags[] = LicensedVideoSwapHelper::STATUS_SWAP;
+						$swapped++;
+					}
 
-			// swapped video with exact match
-			if ( !empty( $row->status) && $row->status == 3 ) {
-				$statusList[] = "STATUS_SWAP";
-				$statusList[] = "STATUS_EXACT ";
-				$flags[] = LicensedVideoSwapHelper::STATUS_SWAP;
-				$flags[] = LicensedVideoSwapHelper::STATUS_EXACT;
-				$swappedExact++;
-			}
+					// swapped video with exact match
+					if ( !empty( $row->status) && $row->status == 3 ) {
+						$statusList[] = "STATUS_SWAP";
+						$statusList[] = "STATUS_EXACT ";
+						$flags[] = LicensedVideoSwapHelper::STATUS_SWAP;
+						$flags[] = LicensedVideoSwapHelper::STATUS_EXACT;
+						$swappedExact++;
+					}
 
-			$props = implode( '|', $flags );
-			echo implode( ', ', $statusList )." ( $props ) .... ";
+					$props = implode( '|', $flags );
+					echo implode( ', ', $statusList )." ( $props ) .... ";
 
-			$sqlInsert = <<<SQL
-				INSERT INTO page_wikia_props (page_id, propname, props)
-				VALUES ($pageId, $status, ($props))
-				ON DUPLICATE KEY UPDATE props = (props | $props)
+					$sqlInsert = <<<SQL
+						INSERT INTO page_wikia_props (page_id, propname, props)
+						VALUES ($pageId, $status, ($props))
+						ON DUPLICATE KEY UPDATE props = (props | $props)
 SQL;
 
-			if ( $dryRun ) {
-				$affected = 1;
-			} else {
-				$db->query( $sqlInsert, __METHOD__ );
-				$affected = $db->affectedRows();
-			}
+					if ( $dryRun ) {
+						$affected = 1;
+					} else {
+						$db->query( $sqlInsert, __METHOD__ );
+						$affected = $db->affectedRows();
+					}
 
-			echo "$affected affected.\n";
-			$totalAffected += $affected;
+					echo "$affected affected.\n";
+					$totalAffected += $affected;
+				}
+
+				$cnt++;
+			} while ( $pages == $limit );
+			echo "\n";
 		}
 
-		$cnt++;
-	} while ( $pages == $limit );
-	echo "\n";
-}
+		echo "$dbname: Total Pages: $total, Kept Videos: $kept, Swapped Videos: $swapped, ";
+		echo "Swapped Videos with Exact Match: $swappedExact, Videos with Suggestions: $suggestions, Affected: $totalAffected\n\n";
+	}
 
-echo "$wgDBname: Total Pages: $total, Kept Videos: $kept, Swapped Videos: $swapped, ";
-echo "Swapped Videos with Exact Match: $swappedExact, Videos with Suggestions: $suggestions, Affected: $totalAffected\n\n";
+}
