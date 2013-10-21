51186d73d4a7ad6accbcc4ab7e7b030feac04c1a
Lukasz Konieczny <lukaszk@wikia-inc.com>
2013-10-21 15:23:19 +0000

    DAR-2446: Replace wfMsg to wfMessage

--- extensions/wikia/ThemeDesigner/templates/ThemeDesigner_CustomizeTab.php
+++ extensions/wikia/ThemeDesigner/templates/ThemeDesigner_CustomizeTab.php
@@ -1,13 +1,13 @@
 <section id="CustomizeTab" class="CustomizeTab">
 	<fieldset>
-		<h1><?= wfMsg('themedesigner-background') ?></h1>
+		<h1><?= wfMessage('themedesigner-background')->plain() ?></h1>
 		<ul>
 			<li>
 				<h2><?= wfMsg('themedesigner-color') ?></h2>
 				<img src="<?= $wg->BlankImgUrl ?>" class="color-body" id="swatch-color-background">
 			</li>
 			<li class="background">
-				<h2><?= wfMsg('themedesigner-graphic') ?></h2>
+				<h2><?= wfMessage('themedesigner-graphic')->plain() ?></h2>
 				<img src="<?= $wg->BlankImgUrl ?>" class="background-image" id="swatch-image-background">
 				<input type="checkbox" id="tile-background"> <label for="tile-background"><?= wfMessage('themedesigner-tile-background')->plain() ?></label>
 				<input type="checkbox" id="fix-background"> <label for="fix-background"><?= wfMessage('themedesigner-fix-background')->plain() ?></label>
@@ -16,24 +16,24 @@
 		</ul>
 	</fieldset>
 	<fieldset class="page">
-		<h1><?= wfMsg('themedesigner-page') ?></h1>
+		<h1><?= wfMessage('themedesigner-page')->plain() ?></h1>
 		<ul>
 			<li>
-				<h2><?= wfMsg('themedesigner-buttons') ?></h2>
+				<h2><?= wfMessage('themedesigner-buttons')->plain() ?></h2>
 				<img src="<?= $wg->BlankImgUrl ?>" class="color-buttons" id="swatch-color-buttons">
 			</li>
 			<li>
-				<h2><?= wfMsg('themedesigner-links') ?></h2>
+				<h2><?= wfMessage('themedesigner-links')->plain() ?></h2>
 				<img src="<?= $wg->BlankImgUrl ?>" class="color-links" id="swatch-color-links">
 			</li>
 			<li>
-				<h2><?= wfMsg('themedesigner-header') ?></h2>
+				<h2><?= wfMessage('themedesigner-header')->plain() ?></h2>
 				<img src="<?= $wg->BlankImgUrl ?>" class="color-header" id="swatch-color-header">
 			</li>
 			<li>
-				<h2><?= wfMsg('themedesigner-color') ?></h2>
+				<h2><?= wfMessage('themedesigner-color')->plain() ?></h2>
 				<img src="<?= $wg->BlankImgUrl ?>" class="color-page" id="swatch-color-page">
-				<h2><?= wfMsg('themedesigner-transparency') ?></h2>
+				<h2><?= wfMessage('themedesigner-transparency')->plain() ?></h2>
 				<div id="OpacitySlider" class="WikiaSlider OpacitySlider"></div>
 			</li>
 		</ul>
