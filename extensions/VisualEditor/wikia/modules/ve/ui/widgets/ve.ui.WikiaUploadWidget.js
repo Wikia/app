24fb067e8ee9d0aba1131f97e52a768c87fae660
Christian Williams <christian@wikia-inc.com>
2013-10-23 06:41:45 +0000

    changes from code review

--- extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaUploadWidget.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaUploadWidget.js
@@ -1,10 +1,25 @@
+/*!
+ * VisualEditor UserInterface WikiaUploadWidget class.
+ */
+
+/* global mw */
+
+/**
+ * @class
+ * @extends ve.ui.Widget
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ */
 ve.ui.WikiaUploadWidget = function VeUiWikiaUploadWidget( config ) {
 	// Parent constructor
 	ve.ui.Widget.call( this, config );
 
-	this.$uploadIcon = this.$$( '<span class="ve-ui-iconedElement-icon ve-ui-icon-upload"></span>' );
+	this.$uploadIcon = this.$$( '<span>' )
+		.addClass( 've-ui-iconedElement-icon ve-ui-icon-upload' );
 
-	this.$uploadLabel = this.$$( '<span>' + ve.msg( 'visualeditor-wikiauploadwidget-label' ) + '</span>' );
+	this.$uploadLabel = this.$$( '<span>' )
+		.text( ve.msg( 'visualeditor-wikiauploadwidget-label' ) );
 
 	this.uploadButton = new ve.ui.ButtonWidget( {
 		'$$': this.$$,
@@ -12,13 +27,15 @@ ve.ui.WikiaUploadWidget = function VeUiWikiaUploadWidget( config ) {
 		'flags': ['constructive']
 	} );
 
+	this.$file = this.$$( '<input>' ).attr( 'type', 'file' );
+
 	// Initialization
 	this.$
 		.addClass( 've-ui-wikiaUploadButtonWidget' )
 		.append( this.$uploadIcon )
 		.append( this.$uploadLabel )
 		.append( this.uploadButton.$ )
-		.append( '<input type="file">' );
+		.append( this.$file );
 };
 
 /* Inheritance */
