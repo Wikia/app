64d2d79cb35259be9c3ceddcd7e299263048c048
lizlux <liz@lizlux.net>
2013-10-23 16:46:18 -0700

    VID-925 adding logging when a file load fails and JSHinting video bootstrap code

--- extensions/wikia/VideoHandlers/js/handlers/Ooyala.js
+++ extensions/wikia/VideoHandlers/js/handlers/Ooyala.js
@@ -6,15 +6,15 @@
  */
 
 /*global define, require*/
-define('wikia.videohandler.ooyala', ['wikia.window', require.optional('ext.wikia.adengine.dartvideohelper'), 'wikia.loader'], function(window, dartVideoHelper, loader) {
+define( 'wikia.videohandler.ooyala', [ 'wikia.window', require.optional( 'ext.wikia.adengine.dartvideohelper' ), 'wikia.loader', 'wikia.log' ], function( window, dartVideoHelper, loader, log ) {
 	'use strict';
 
 	/**
 	 * Set up Ooyala player and tracking events
 	 * @param {Object} params Player params sent from the video handler
-	 * @param {VideoBootstrap} vb Instance of video player
+	 * @param {Constructor} vb Instance of video player
 	 */
-	return function(params, vb) {
+	return function( params, vb ) {
 		var containerId = vb.timeStampId( params.playerId ),
 			started = false,
 			createParams = {
@@ -24,31 +24,31 @@ define('wikia.videohandler.ooyala', ['wikia.window', require.optional('ext.wikia
 				wmode: 'transparent'
 			};
 
-		function onCreate(player) {
+		function onCreate( player ) {
 			var messageBus = player.mb;
 
 			// Player has loaded
-			messageBus.subscribe(window.OO.EVENTS.PLAYER_CREATED, 'tracking', function(eventName, payload) {
-				vb.track('player-load');
+			messageBus.subscribe( window.OO.EVENTS.PLAYER_CREATED, 'tracking', function( eventName, payload ) {
+				vb.track( 'player-load' );
 			});
 
 			// Actual content starts playing (past any ads or age-gates)
-			messageBus.subscribe(window.OO.EVENTS.PLAYING, 'tracking', function() {
+			messageBus.subscribe( window.OO.EVENTS.PLAYING, 'tracking', function() {
 				if ( !started ) {
-					vb.track('content-begin');
+					vb.track( 'content-begin' );
 					started = true;
 				}
 
 			});
 
 			// Ad starts
-			messageBus.subscribe(window.OO.EVENTS.WILL_PLAY_ADS, 'tracking', function(eventName, payload) {
+			messageBus.subscribe( window.OO.EVENTS.WILL_PLAY_ADS, 'tracking', function( eventName, payload ) {
 				vb.track('ad-start');
 			});
 
 			// Ad has been fully watched
-			messageBus.subscribe(window.OO.EVENTS.ADS_PLAYED, 'tracking', function(eventName, payload) {
-				vb.track('ad-finish');
+			messageBus.subscribe( window.OO.EVENTS.ADS_PLAYED, 'tracking', function( eventName, payload ) {
+				vb.track( 'ad-finish' );
 			});
 
 			// Log all events and values (for debugging)
@@ -61,14 +61,25 @@ define('wikia.videohandler.ooyala', ['wikia.window', require.optional('ext.wikia
 
 		createParams.onCreate = onCreate;
 
-		if (window.wgAdVideoTargeting && window.wgShowAds) {
-			if (!dartVideoHelper) {
+		if ( window.wgAdVideoTargeting && window.wgShowAds ) {
+			if ( !dartVideoHelper ) {
 				throw 'ext.wikia.adengine.dartvideohelper is not defined and it should as we need to display ads';
 			}
-			createParams['google-ima-ads-manager'] = {
+			createParams[ 'google-ima-ads-manager' ] = {
 				adTagUrl: dartVideoHelper.getUrl(),
 				showInAdControlBar : true
 			};
+		}
+
+		// log any errors from failed script loading
+		function loadFail( data ) {
+			var message = data.error + ':';
+
+			$.each( data.resources, function() {
+				message += ' ' + this;
+			});
+
+			log( message, log.levels.error, 'VideoBootstrap' );
 		}
 
 		/* Ooyala doesn't support more than one player type (i.e. age-gate and non-age-gate)
@@ -82,15 +93,15 @@ define('wikia.videohandler.ooyala', ['wikia.window', require.optional('ext.wikia
 		/* the second file depends on the first file */
 		loader({
 			type: loader.JS,
-			resources: params.jsFile[0]
+			resources: params.jsFile[ 0 ]
 		}).done(function() {
 			loader({
 				type: loader.JS,
-				resources: params.jsFile[1]
+				resources: params.jsFile[ 1 ]
 			}).done(function() {
-				window.OO.Player.create(containerId, params.videoId, createParams);
-			});
-		});
+				window.OO.Player.create( containerId, params.videoId, createParams );
+			}).fail( loadFail );
+		}).fail( loadFail );
 
 	};
 });
