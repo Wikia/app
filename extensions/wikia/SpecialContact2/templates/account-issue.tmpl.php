c007cca464021388c886804f33e01a48e8d990ce
grunny <mwgrunny@gmail.com>
2013-10-26 01:51:54 +1000

    (CE-460) Add missing CC option to Special:Contact forms
    Add missing CC option to Special:Contact/bad-ad and /account-issue
    forms.

--- extensions/wikia/SpecialContact2/templates/account-issue.tmpl.php
+++ extensions/wikia/SpecialContact2/templates/account-issue.tmpl.php
@@ -1,63 +1,73 @@
 <?php
 if ( !empty($err) ) {
-        print $err;
+	print $err;
 }
 
-echo wfMsgExt( 'specialcontact-intro-account-issue', array( 'parse' ) );
+echo wfMessage( 'specialcontact-intro-account-issue' )->parseAsBlock();
 ?>
 
-<h2><?= wfMsg( 'specialcontact-form-header' ) ?></h2>
+<h2><?= wfMessage( 'specialcontact-form-header' )->escaped() ?></h2>
 
 <form id="contactform" method="post" action="" enctype="multipart/form-data">
 
 <p>
-<label for="wpUserName"><?= wfMsg( 'specialcontact-username' ) ?></label>
+<label for="wpUserName"><?= wfMessage( 'specialcontact-username' )->escaped() ?></label>
 <input name="wpUserName" value="<?= $encName ?>" />
 </p>
 
 <p>
-<label for="wpEmail"><?= wfMsg( 'specialcontact-yourmail' ) ?></label>
+<label for="wpEmail"><?= wfMessage( 'specialcontact-yourmail' )->escaped() ?></label>
 <input name="wpEmail" value="<?= $encEmail ?>" />
 </p>
 
 <p>
-<label for="wpContactWikiName"><?= wfMsg( 'specialcontact-wikiname' ) ?></label>
+<label for="wpContactWikiName"><?= wfMessage( 'specialcontact-wikiname' )->escaped() ?></label>
 <input name="wpContactWikiName" />
 </p>
 
 <p>
-<label for="wpDescription"><?= wfMsg( 'specialcontact-label-account-issue-description' ) ?></label>
+<label for="wpDescription"><?= wfMessage( 'specialcontact-label-account-issue-description' )->escaped() ?></label>
 <textarea name="wpDescription"></textarea>
 </p>
 
 <p>
-<label for="wpScreenshot1"><?= wfMsg( 'specialcontact-label-screenshot' ) ?></label>
+<label for="wpScreenshot1"><?= wfMessage( 'specialcontact-label-screenshot' )->escaped() ?></label>
 <input id="wpScreenshot1" name="wpScreenshot[]" type="file" accept="image/*" />
 </p>
 
 <p class="additionalScreenShot">
-<label for="wpScreenshot2"><?= wfMsg( 'specialcontact-label-additionalscreenshot' ) ?></label>
+<label for="wpScreenshot2"><?= wfMessage( 'specialcontact-label-additionalscreenshot' )->escaped() ?></label>
 <input id="wpScreenshot2" name="wpScreenshot[]" type="file" accept="image/*" />
 </p>
 
 <p class="additionalScreenShot">
-<label for="wpScreenshot3"><?= wfMsg( 'specialcontact-label-additionalscreenshot' ) ?></label>
+<label for="wpScreenshot3"><?= wfMessage( 'specialcontact-label-additionalscreenshot' )->escaped() ?></label>
 <input id="wpScreenshot3" name="wpScreenshot[]" type="file" accept="image/*" />
 </p>
 
 <?php
-if( !$isLoggedIn && (isset($captchaForm)) ) {
-        echo "<div class='captcha'>" .
-        wfMsg( 'specialcontact-captchatitle' ) .
-        $captchaForm .
-        wfMsg( 'specialcontact-captchainfo' ) .
-        "</div>\n";
+if ( !$isLoggedIn && isset( $captchaForm ) ) {
+	echo '<div class="captcha">' .
+	wfMessage( 'specialcontact-captchatitle' )->escaped() .
+	$captchaForm .
+	wfMessage( 'specialcontact-captchainfo' )->escaped() .
+	"</div>\n";
 }
 ?>
 
-<input type="submit" value="<?= wfMsg( 'specialcontact-mail' ) ?>" />
+<p><input type="submit" value="<?= wfMessage( 'specialcontact-mail' )->escaped() ?>" /></p>
 
-<input type="hidden" id="wpBrowser" name="wpBrowser" value="<?php echo $_SERVER['HTTP_USER_AGENT']; ?>" />
+<?php
+if ( $isLoggedIn && $hasEmail ) {
+	if ( $hasEmailConf ) {
+		echo '<input type="checkbox" name="wgCC" value="1" />' . wfMessage( 'specialcontact-ccme' )->escaped();
+	} else {
+		echo '<s><i>' . wfMessage( 'specialcontact-ccme' )->escaped() . '</i></s><br/>' . wfMessage( 'specialcontact-ccdisabled' )->parse();
+	}
+}
+?>
+
+<input type="hidden" id="wpBrowser" name="wpBrowser" value="<?= htmlspecialchars( $_SERVER['HTTP_USER_AGENT'] ); ?>" />
 <input type="hidden" id="wpAbTesting" name="wpAbTesting" value="[unknown]" />
 </form>
 
