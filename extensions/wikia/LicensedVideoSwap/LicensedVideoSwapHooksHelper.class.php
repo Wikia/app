4698ce53b07f1ba6ad0224bbe8a9108520d7d20c
saipetch <saipetch@wikia-inc.com>
2013-10-25 16:50:51 -0700

    Video (vid-982): LicensedVideoSwap - add hook to reset message in global notification when the page is deleted

--- extensions/wikia/LicensedVideoSwap/LicensedVideoSwapHooksHelper.class.php
+++ extensions/wikia/LicensedVideoSwap/LicensedVideoSwapHooksHelper.class.php
@@ -39,4 +39,22 @@ class LicensedVideoSwapHooksHelper {
 
 		return true;
 	}
+
+	/**
+	 * Hook: reset confirmation message in global notification when file page is deleted
+	 * @param Title $title
+	 * @param string $message
+	 * @return true
+	 */
+	public static function onOasisAddPageDeletedConfirmationMessage( &$title, &$message ) {
+		$app = F::app();
+		$controller = $app->wg->Request->getVal( 'controller', '' );
+		$method = $app->wg->Request->getVal( 'method', '' );
+		if ( $title instanceof Title && $title->getNamespace() == NS_FILE && $controller == 'LicensedVideoSwapSpecial' && $method == 'swapVideo' ) {
+			$message = '';
+		}
+
+		return true;
+	}
+
 }
