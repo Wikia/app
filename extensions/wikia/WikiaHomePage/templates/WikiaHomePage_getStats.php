7ff29a5b746ea35c36421c9510b8077fb8309856
marzjan <sebastianm@wikia-inc.com>
2013-10-28 12:21:51 +0000

    DAR-2501 More text() to plain() changes

--- extensions/wikia/WikiaHomePage/templates/WikiaHomePage_getStats.php
+++ extensions/wikia/WikiaHomePage/templates/WikiaHomePage_getStats.php
@@ -14,23 +14,23 @@
 		<strong><?= $visitors; ?></strong>
 	</div>
     <div class="datasection">
-        <h4><?= wfMessage('wikiahome-stats-mobile-percentage')->text(); ?></h4>
+        <h4><?= wfMessage('wikiahome-stats-mobile-percentage')->plain(); ?></h4>
         <strong><?= wfMessage('wikiahome-stats-mobile-percentage-value', $mobilePercentage)->plain(); ?></strong>
     </div>
     <div class="datasection firstrowcell">
-        <h4><?= wfMessage('wikiahome-stats-totalpages')->text(); ?></h4>
+        <h4><?= wfMessage('wikiahome-stats-totalpages')->plain(); ?></h4>
         <strong><?= $totalPages; ?></strong>
     </div>
 	<div class="datasection">
-		<h4><?= wfMessage('wikiahome-stats-edits')->text(); ?></h4>
+		<h4><?= wfMessage('wikiahome-stats-edits')->plain(); ?></h4>
 		<strong><?= $edits; ?></strong>
 	</div>
 	<div class="datasection firstrowcell">
-		<h4><?= wfMessage('wikiahome-stats-communities')->text(); ?></h4>
+		<h4><?= wfMessage('wikiahome-stats-communities')->plain(); ?></h4>
 		<strong><?= $communities; ?></strong>
 	</div>
     <div class="datasection">
-        <h4><?= wfMessage('wikiahome-stats-new-communities')->text(); ?></h4>
+        <h4><?= wfMessage('wikiahome-stats-new-communities')->plain(); ?></h4>
         <strong><?= $newCommunities; ?></strong>
     </div>
 </div>
