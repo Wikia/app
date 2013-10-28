3dacf2e7c1a3d83f5fb6ad1b6e7d8753e1492ed8
marzjan <sebastianm@wikia-inc.com>
2013-10-28 10:51:49 +0000

    DAR-2501 Changing copy on Home Pages
    * Additionally added escaping to WikiaStyleGuideTooltip

--- extensions/wikia/WikiaHomePage/templates/WikiaHomePage_getStats.php
+++ extensions/wikia/WikiaHomePage/templates/WikiaHomePage_getStats.php
@@ -1,12 +1,21 @@
 <h2><?= wfMsg('wikiahome-stats-heading'); ?></h2>
 <div class="statisticdata">
 	<div class="datasection firstrowcell">
-		<h4><?= wfMessage('wikiahome-stats-visitors')->text(); ?></h4>
+		<h4><?=
+				$app->renderView(
+					'WikiaStyleGuideTooltipIconController',
+					'index',
+					[
+						'text' => wfMessage( 'wikiahome-stats-visitors' )->parse(),
+						'tooltipIconTitle' => wfMessage( 'wikiahome-stats-visitors-tooltip' )->text()
+					]
+				);
+			?></h4>
 		<strong><?= $visitors; ?></strong>
 	</div>
     <div class="datasection">
-        <h4><?= wfMessage('wikiahome-stats-mobile-visitors')->text(); ?></h4>
-        <strong><?= $mobileVisitors; ?></strong>
+        <h4><?= wfMessage('wikiahome-stats-mobile-percentage')->text(); ?></h4>
+        <strong><?= wfMessage('wikiahome-stats-mobile-percentage-value', $mobilePercentage)->text(); ?></strong>
     </div>
     <div class="datasection firstrowcell">
         <h4><?= wfMessage('wikiahome-stats-totalpages')->text(); ?></h4>
