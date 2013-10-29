ca95ef61821ded671973e5a4a6a1be4b48fa431d
Michał ‘Mix’ Roszka <mix@if-then-else.pl>
2013-10-29 16:35:27 +0100

    explain the meaning of the S3_MIGRATION constant

--- extensions/wikia/WikiFactory/Dumps/DumpsOnDemand.php
+++ extensions/wikia/WikiFactory/Dumps/DumpsOnDemand.php
@@ -18,6 +18,11 @@ $wgGroupPermissions['autoconfirmed']['dumpsondemand'] = true;
 class DumpsOnDemand {
 
 	const BASEURL = "http://dumps.wikia.net";
+
+    /**
+     * From this moment on we use Amazon S3 storage for the dumps.
+     * All ealier dumps are gone and all data referring to them should be considered invalid.
+     */
 	const S3_MIGRATION = '20131002154415';
 
 	/**
