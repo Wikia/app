17ed13bc2888900a47bd846970444d9cdb0123c8
Adam Robak <adamr@wikia-inc.com>
2013-10-31 15:56:33 +0100

    annotation change

--- extensions/wikia/ApiDocs/ApiDocsController.class.php
+++ extensions/wikia/ApiDocs/ApiDocsController.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Backwards compatibility after name change to DocsApi
+ * Backwards compatibility after name change to DocsApi, to be removed after apache redirects update
  * Class ApiDocsController
  */
 class ApiDocsController extends DocsApiController {
