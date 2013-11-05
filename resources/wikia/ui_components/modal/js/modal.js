9f4641963326051c895c29b67faa14543a07d583
nAndy <nandy@wikia-inc.com>
2013-11-05 14:18:05 +0000

    DAR-2618 added documentation to modal.js

--- resources/wikia/ui_components/modal/js/modal.js
+++ resources/wikia/ui_components/modal/js/modal.js
@@ -6,6 +6,18 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 		INACTIVE_CLASS = 'inactive',
 		destroyOnClose;
 
+	/**
+	 * Initializes a modal
+	 *
+	 * Checks if element with given id exists in DOM and if not creates it
+	 * and appends it to body; adds event handlers for blackout and close button;
+	 * sets flags depending on data- attributes:
+	 * - data-destroy-on-close -- if false value passed the modal will remain in DOM after closing it
+	 *
+	 * @param {String} id
+	 * @param {Object} modalMarkup (optional) jQuery wrapper for existing in DOM modal markup
+	 * @constructor
+	 */
 	function Modal( id, modalMarkup ) {
 		var that = this,
 			jQuerySelector = '#' + id;
@@ -16,6 +28,11 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 			this.$element = $( jQuerySelector );
 		}
 
+		/**
+		 * Wraps with jQuery blackout div, adds click event handler and returns it
+		 *
+		 * @returns {Object} jQuery wrapped blackout markup
+		 */
 		function getBlackout() {
 			var blackoutId = BLACKOUT_CLASS + '_' + id,
 				$blackout = $('#' + blackoutId );
@@ -44,11 +61,17 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 		destroyOnClose = ( typeof(destroyOnClose) === 'undefined' ) ? true : destroyOnClose;
 	}
 
+	/**
+	 * Shows modal; adds shown class to modal and visible class to blackout
+	 */
 	Modal.prototype.show = function() {
 		this.$element.addClass( 'shown' );
 		this.$blackout.addClass( 'visible' );
 	};
 
+	/**
+	 * Closes the modal; removes it from dom or just removes classes - it depends on destroyOnClose flag
+	 */
 	Modal.prototype.close = function() {
 		if( !destroyOnClose ) {
 			this.$element.removeClass( 'shown' );
@@ -59,6 +82,10 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 		}
 	};
 
+	/**
+	 * Disables all modal's buttons, adds inactive class to the modal
+	 * and runs jQuery $.startThrobbing() method on it
+	 */
 	Modal.prototype.deactivate = function() {
 		var dialog = this.$element;
 
@@ -66,6 +93,10 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 		dialog.startThrobbing();
 	};
 
+	/**
+	 * Runs jQuery $.stopThrobbing() on modal, removes inactive class from it and
+	 * sets disabled attribute for all modal's buttons to false
+	 */
 	Modal.prototype.activate = function() {
 		var dialog = this.$element;
 
@@ -74,10 +105,20 @@ define( 'wikia.ui.modal', [ 'jquery' ], function( $ ) {
 			.find( 'button' ).attr( 'disabled', false );
 	};
 
+	/**
+	 * Returns true if modal has shown class and false otherwise
+	 *
+	 * @returns {Boolean}
+	 */
 	Modal.prototype.isShown = function() {
 		return this.$element.hasClass( 'shown' );
 	};
 
+	/**
+	 * Returns true if modal hasn't inactive class and false otherwise
+	 *
+	 * @returns {boolean}
+	 */
 	Modal.prototype.isActive = function() {
 		return !this.$element.hasClass( INACTIVE_CLASS );
 	};
