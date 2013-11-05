f4823ffbe4b01973620a8f0fbbbe26ba10062d85
Michał ‘Mix’ Roszka <michal@roszka.pl>
2013-11-05 11:42:06 +0000

    DAR-2322 fix a null pointer crash

--- extensions/wikia/SpecialPromote/SpecialPromoteHooks.class.php
+++ extensions/wikia/SpecialPromote/SpecialPromoteHooks.class.php
@@ -1,10 +1,24 @@
 <?
 class SpecialPromoteHooks {
+	/**
+	 * Called once a file has been deleted.
+	 *
+	 * @param $file reference to the deleted file
+	 * @param $oldimage in case of the deletion of an old image, the name of the old file
+	 * @param $article in case all revisions of the file have been deleted a reference to the article associated with the file.
+	 * @see http://www.mediawiki.org/wiki/Manual:Hooks/FileDeleteComplete
+	 */
 	public static function onFileDeleteComplete($file, $oldimage, $page) {
 		global $wgCityId, $wgContLang;
-
 		$visualization = new CityVisualization();
-		$visualization->removeImageFromReviewByName($wgCityId, $page->getTitle()->getText(), $wgContLang->getCode());
+		/**
+		 * No $page when deleting an old image ($oldimage).
+		 *
+		 * @see FileDeleteForm::doDelete()
+		 */
+		if ( $page instanceof Page ) {
+			$visualization->removeImageFromReviewByName($wgCityId, $page->getTitle()->getText(), $wgContLang->getCode());
+		}
 		$visualization->purgeWikiPromoteDataCache($wgCityId, $wgContLang->getCode());
 		return true;
 	}
