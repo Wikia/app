6c2ac9d454b2f9e2b926b5999c17d98e1471e29b
Adam Robak <adamr@wikia-inc.com>
2013-11-05 16:44:13 +0100

    merge fix

--- extensions/wikia/Search/classes/Config.php
+++ extensions/wikia/Search/classes/Config.php
@@ -726,20 +726,10 @@ class Config
 		return $this->setQueryService( 'Select\\Dismax\\InterWiki', $apply );
 	}
 
-
 	/**
 	 * Synonym function for backward compatbility
 	 * @param  boolean $apply
 	 * @return Wikia\Search\Config provides fluent interface
-	 */
-	public function setOnWiki( $apply ) {
-		return $this->setQueryService( 'Select\\Dismax\\OnWiki', $apply );
-	}
-
-	/**
-	 * Synonym function for backward compatbility
-	 * @param  boolean $apply
-	 * @return \Wikia\Search\Config provides fluent interface
 	 */
 	public function setOnWiki( $apply ) {
 		return $this->setQueryService( 'Select\\Dismax\\OnWiki', $apply );
