4f388f86a7523ca2f48d6859b9d4ad70949ecafc
Krzysztos 'suchy' Suchanek <suchy@wikia-inc.com>
2013-11-08 12:51:14 +0100

    PLA-833 Solr helper class for update/delete/query

--- extensions/wikia/SolrHelper/classes/Query.class.php
+++ extensions/wikia/SolrHelper/classes/Query.class.php
@@ -11,24 +11,94 @@ namespace Wikia\SolrHelper;
 use \Solarium_Client;
 
 class Query {
-	public static function getByArticleId( $wikiID, $articleId, $fields = [], $config = null ) {
-		if ( !$config ) {
-			//For testing only!
-			$config = [
-				'adapter' => 'Solarium_Client_Adapter_Curl',
-				'adapteroptions' => [
-					'host' => 'search-s10',
-					'port' => 8983,
-					'path' => '/solr/',
-					'core' => 'main'
-				]
-			];
+
+	/**
+	 * @var \Solarium_Client
+	 */
+	protected $client;
+
+
+	public function __construct( $config ) {
+		if ( !$config || !is_array( $config ) ) {
+			throw new \InvalidArgumentException('No config specified');
+		}
+		$this->client = new Solarium_Client($config);
+	}
+
+	public function updateDocuments( $documents ) {
+		if ( !is_array( $documents ) || !is_array( $documents[ 0 ] ) ) {
+			throw new \InvalidArgumentException('Expected format: [ [xxx=>1,yyy=>2} , ... ]');
 		}
 
+		$update = $this->client->createUpdate();
+
+		$list = [];
+		foreach ( $documents as $current ) {
+			$doc = $update->createDocument();
+			foreach ( $current as $fName => $fvalue ) {
+				$doc->setField( $fName, $fvalue );
+			}
+
+			$list[ ] = $doc;
+		}
+
+		$update->addDocuments( $list, true );
+		$update->addCommit();
+		$result = $this->client->update( $update );
+
+		return $result->getStatus();
+	}
+
+	public function deleteDocuments( $wikiID, $from, $to = null ) {
+		$range = false;
+		$wikiID = (int)$wikiID;
+		$to = (int)$to;
+		if ( !$wikiID ) {
+			throw new \InvalidArgumentException('wikiID not specified');
+		}
+
+		if ( $to ) {
+			if ( is_array( $from ) ) {
+				throw new \InvalidArgumentException('Expected format: ID | [A,B,C,D] | FROM, TO ');
+			}
+			$from = (int)$from;
+			$range = true;
+		}
+		else {
+			if ( !is_array( $from ) ) {
+				$from = [(int)$from];
+			}
+		}
+
+		$update = $this->client->createUpdate();
+		$query = '+(wid:' . (int)$wikiID . ') AND ';
+		if ( $range ) {
+			$query .= ' pageid:[' . $from . ' TO ' . $to . ']';
+		}
+		else {
+			$ids = '';
+			foreach ( $from as &$val ) {
+				if ( $ids > '' ) {
+					$ids .= ' OR ';
+				}
+				$ids .= (int)$val;
+			}
+			$query .= ' pageid:(' . $ids . ')';
+
+		}
+
+		$update->addDeleteQuery( $query );
+		$update->addCommit();
+		$result = $this->client->update( $update );
+
+		return $result->getStatus();
+	}
+
+
+	public function getByArticleId( $wikiID, $articleId, $fields = [], $config = null ) {
 		if ( $fields && !is_array( $fields ) ) {
 			$fields = [$fields];
 		}
-
 		if ( $articleId && !is_array( $articleId ) ) {
 			$articleId = [(int)$articleId];
 		}
@@ -41,17 +111,16 @@ class Query {
 			}
 			$ids .= (int)$val;
 		}
-		$query .= ' pageid:(' . $ids . ')';
 
-		$client = new Solarium_Client($config);
-		$queryObj = $client->createSelect();
+		$query .= ' pageid:(' . $ids . ')';
+		$queryObj = $this->client->createSelect();
 
 		if ( !empty($fields) ) {
 			$queryObj->setFields( $fields );
 		}
 
 		$queryObj->setQuery( $query );
-		$results = $client->select( $queryObj );
+		$results = $this->client->select( $queryObj );
 
 		return $results->getDocuments();
 	}
