174dc770160f36d8be09d08e02d809f465d82e9b
Christian Williams <christian@wikia-inc.com>
2013-11-14 01:21:29 +0000

    selector cache

--- extensions/VisualEditor/wikia/modules/ve/init/ve.init.mw.WikiaViewPageTarget.init.js
+++ extensions/VisualEditor/wikia/modules/ve/init/ve.init.mw.WikiaViewPageTarget.init.js
@@ -301,12 +301,13 @@
 			'mouseover click',
 			'a[href*="action=edit"][href*="&redlink"]:not([href*="veaction=edit"])',
 			function () {
-				var href = $( this ).attr( 'href' ),
+				var $element = $( this ),
+					href = $element.attr( 'href' ),
 					articlePath = mw.config.get( 'wgArticlePath' ).replace( '$1', '' ),
 					redlinkArticle = new mw.Uri( href ).path.replace( articlePath, '' );
 
 				if ( init.isAvailable( redlinkArticle ) ) {
-					$( this ).attr( 'href', href.replace( 'action=edit', 'veaction=edit' ) );
+					$element.attr( 'href', href.replace( 'action=edit', 'veaction=edit' ) );
 				}
 			}
 		);
