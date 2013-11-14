b0aef9f90911ef759de4659f72b9b0f6b0592cd3
hakubo <bukaj.kelo+github@gmail.com>
2013-11-13 16:46:16 -0800

    MOB-450: Some cleanup

--- extensions/wikia/WikiaMobile/js/toc.js
+++ extensions/wikia/WikiaMobile/js/toc.js
@@ -4,81 +4,70 @@ require(['sections', 'wikia.window', 'jquery', 'wikia.mustache', 'wikia.toc'],
 	'use strict';
 
 	//private
-	var $document = $(window.document),
+	var BOTTOM = 'bottom',
+		FIXED = 'fixed',
+		DISABLED = 'disabled',
+		OPEN = 'open',
+		$document = $(window.document),
 		$anchors,
 		$openOl,
+		timeout,
 		state,
 		offsetTop = 0,
 		$parent,
 		ol = '<ol class="toc-list level{{level}}">{{#sections}}{{> lis}}{{/sections}}</ol>',
-		lis = '{{#.}}<li {{#sections.length}}class=has-children{{/sections.length}}><a href="#{{id}}">{{name}}{{#firstLevel}}{{#sections.length}}<span class="chevron right"></span>{{/sections.length}}{{/firstLevel}}</a>{{#sections.length}}{{> ol}}{{/sections.length}}</li>{{/.}}',
+		lis = '{{#.}}<li {{#sections.length}}class="has-children{{#firstLevel}} first-children{{/firstLevel}}"{{/sections.length}}><a href="#{{id}}">{{name}}{{#firstLevel}}{{#sections.length}}<span class="chevron right"></span>{{/sections.length}}{{/firstLevel}}</a>{{#sections.length}}{{> ol}}{{/sections.length}}</li>{{/.}}',
 		$toc,
 		$ol,
-		tocData = toc.getData(sections.list, function(header, level){
-			return {
-				id: header.id,
-				name: header.textContent.trim(),
-				level: level,
-				firstLevel: level === 2,
-				sections: []
-			};
-		});
+		inited;
 
 	function toggleLi($li, force){
-		if($li.is('.has-children') && $li.parent().is('.level')) {
-			$li.siblings().removeClass('fixed bottom open');
+		if($li.is('.first-children')) {
+			$li.siblings().removeClass(FIXED + ' ' + BOTTOM + ' ' + OPEN);
 
-			if($li.toggleClass('open', force).hasClass('open')) {
+			if($li.toggleClass(OPEN, force).hasClass(OPEN)) {
 				$openOl = $li.find('ol').first();
-				offsetTop = $openOl[0].offsetTop;
+				offsetTop = Math.round($openOl.position().top);
 				$parent = $openOl.parent();
 
 				handleFixingLiElement();
 			}else {
 				state = null;
-				$li.removeClass('fixed bottom');
+				$li.removeClass(FIXED + ' ' + BOTTOM);
 
 				$openOl = null;
 			}
 
-			$ol.scrollTop($li[0].offsetTop - 45);
+			$ol.scrollTop($li.position().top - 45);
 		}
 	}
 
 	function handleFixingLiElement(){
-		var scrollTop,
-			self = $ol[0];
+		if(!timeout && $openOl) {
+			timeout = setTimeout(function(){
+				var scrollTop = $ol.scrollTop() + 90;
 
-		$ol.off('scroll', handleFixingLiElement);
-
-		scrollTop = self.scrollTop + 90;
-
-		if(state !== 'disabled' && scrollTop < offsetTop) {
-			state = 'disabled';
-			$parent.removeClass('fixed');
-		} else if (scrollTop >= offsetTop) {
-			if(offsetTop + $openOl[0].offsetHeight - scrollTop >= 0) {
-				if(state !== 'fixed') {
-					state = 'fixed';
-					$parent.removeClass('bottom').addClass('fixed');
+				if(state !== DISABLED && scrollTop < offsetTop) {
+					state = DISABLED;
+					$parent.removeClass(FIXED);
+				} else if (scrollTop >= offsetTop) {
+					if(offsetTop + $openOl.height() - scrollTop >= 0) {
+						if(state !== FIXED) {
+							state = FIXED;
+							$parent.removeClass(BOTTOM).addClass(FIXED);
+						}
+					} else if (state !== BOTTOM) {
+						state = BOTTOM;
+						$parent.addClass(BOTTOM);
+					}
 				}
-			} else if (state !== 'bottom') {
-				state = 'bottom';
-				$parent.addClass('bottom');
-			}
+
+				timeout = null;
+			}, 50);
 		}
-
-		setTimeout(function(){
-			$ol.on('scroll', handleFixingLiElement);
-		}, 50);
-
 	}
 
 	$toc = $('#wkTOC')
-		.append(mustache.render(ol, tocData, {
-			ol: ol,
-			lis: lis
-		}))
 		.on('click', 'header', function(){
 			window.scrollTo(0,0);
 		})
@@ -94,11 +83,6 @@ require(['sections', 'wikia.window', 'jquery', 'wikia.mustache', 'wikia.toc'],
 
 			toggleLi($li);
 		});
-
-	$ol = $toc.find('.level')
-		.on('scroll', handleFixingLiElement);
-
-	$anchors = $ol.find('li > a');
 
 	function onSectionChange(event, data, scrollTo){
 		$anchors.filter('.current').removeClass('current');
@@ -118,7 +102,7 @@ require(['sections', 'wikia.window', 'jquery', 'wikia.mustache', 'wikia.toc'],
 
 			if(scrollTo){
 				toggleLi($currentLi, true);
-				$ol.scrollTop($current.parent()[0].offsetTop - 90);
+				$ol.scrollTop($current.parent().position().top - 90);
 			}
 		}
 	}
@@ -128,8 +112,39 @@ require(['sections', 'wikia.window', 'jquery', 'wikia.mustache', 'wikia.toc'],
 		onClose();
 	});
 
+	function init(){
+		if ( !inited ) {
+			var tocData = toc.getData(
+				sections.list,
+				function(header, level){
+					return {
+						id: header.id,
+						name: header.textContent.trim(),
+						level: level,
+						firstLevel: level === 2,
+						sections: []
+					};
+				}
+			);
+
+			$ol = $toc
+				.append(mustache.render(ol, tocData, {
+					ol: ol,
+					lis: lis
+				}))
+				.find('.level')
+				.on('scroll', handleFixingLiElement);
+
+			$anchors = $ol.find('li > a');
+
+			inited = true;
+		}
+	}
+
 	function onOpen() {
 		$document.on('section:changed', onSectionChange);
+
+		init();
 
 		onSectionChange(null, sections.current()[0], true);
 		$.event.trigger('curtain:show');
