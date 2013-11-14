4985142dae5432cd58668eb9c72196979ee3eacb
Kyle Florence <kyle.florence@gmail.com>
2013-11-13 17:40:28 -0800

    VE-161: add error handling and param descriptions.

--- includes/api/wikia/ApiLicenses.php
+++ includes/api/wikia/ApiLicenses.php
@@ -8,7 +8,13 @@ class ApiLicenses extends ApiBase {
 
 	public function execute() {
 		$params = $this->extractRequestParams();
-		$licenses = new Licenses( $params );
+
+		try {
+			$licenses = new Licenses( $params );
+		} catch ( Exception $e ) {
+			$this->dieUsageMsg( 'Invalid name or id' );
+		}
+
 		$selected = isset( $params['default'] ) ? $params['default'] : null;
 		$this->getResult()->addValue( null, $this->getModuleName(), array(
 			'html' => $licenses->getInputHTML( $selected )
@@ -18,6 +24,7 @@ class ApiLicenses extends ApiBase {
 	public function getAllowedParams() {
 		return array(
 			'default' => array (
+				ApiBase::PARAM_DFLT => wfMsg( 'nolicense' ),
 				ApiBase::PARAM_TYPE => 'string',
 				ApiBase::PARAM_REQUIRED => false
 			),
@@ -34,7 +41,15 @@ class ApiLicenses extends ApiBase {
 
 	public function getDescription() {
 		return array (
-			'Get media licenses dropdown HTML.'
+			'Get media license dropdown HTML.'
+		);
+	}
+
+	public function getParamDescription() {
+		return array (
+			'default' => 'The default (selected) value',
+			'id' => 'The value for the "id" attribute',
+			'name' => 'The value for the "name" attribute'
 		);
 	}
 
