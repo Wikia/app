2ad9ec5598c6cb228364969339c2f62af6ab04d2
Adam Robak <adamr@wikia-inc.com>
2013-11-14 11:39:37 +0100

    PLA-894 use correct suggestion box when we have two input boxes on the site

--- extensions/wikia/Suggestions/js/Suggestions.js
+++ extensions/wikia/Suggestions/js/Suggestions.js
@@ -2,9 +2,16 @@ require( [ 'jquery', 'wikia.log', 'SuggestionsView' ], function( $, log, view ) 
 	'use strict';
 	log('New search suggestions loading...', log.levels.info, 'suggestions');
 	$(function() {
+		//get suggestions box
+		//main page
+		var handle = $('#WikiaMainContent ul.search-suggest');
+		//other pages
+		if ( !handle.length ) {
+			handle = $('#WikiaRail ul.search-suggest');
+		}
 		window.Wikia.newSearchSuggestions =  view.init(
 			$('#WikiaSearch input[name="search"]'),
-			$('ul.search-suggest'),
+			handle,
 			window.wgCityId
 		);
 		log('New search suggestions loaded!', log.levels.info, 'suggestions');
