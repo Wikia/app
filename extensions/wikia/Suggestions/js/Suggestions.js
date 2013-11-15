336917587e2ea73987c8f7be85a75bab44d0358a
Adam Robak <adamr@wikia-inc.com>
2013-11-15 11:49:29 +0100

    PLA-894 selector changed to find correct suggest dropdown

--- extensions/wikia/Suggestions/js/Suggestions.js
+++ extensions/wikia/Suggestions/js/Suggestions.js
@@ -2,16 +2,9 @@ require( [ 'jquery', 'wikia.log', 'SuggestionsView' ], function( $, log, view ) 
 	'use strict';
 	log('New search suggestions loading...', log.levels.info, 'suggestions');
 	$(function() {
-		//get suggestions box
-		//main page
-		var handle = $('#WikiaMainContent ul.search-suggest');
-		//other pages
-		if ( !handle.length ) {
-			handle = $('#WikiaRail ul.search-suggest');
-		}
 		window.Wikia.newSearchSuggestions =  view.init(
 			$('#WikiaSearch input[name="search"]'),
-			handle,
+			$('#WikiaSearch').nextAll('ul.search-suggest').first(),
 			window.wgCityId
 		);
 		log('New search suggestions loaded!', log.levels.info, 'suggestions');
