0c223969fe490108f73085d67f6796f264f4a9ae
Damian Jozwiak <damian@wikia-inc.com>
2013-11-18 18:11:32 +0100

    DAR-2720 | white spaces

--- extensions/wikia/TOC/js/tocWikiaArticle.js
+++ extensions/wikia/TOC/js/tocWikiaArticle.js
@@ -1,4 +1,4 @@
-require(['jquery', 'wikia.toc', 'wikia.mustache'], function($, toc, mustache) {
+require( ['jquery', 'wikia.toc', 'wikia.mustache'], function ( $, toc, mustache ) {
 	'use strict';
 
 	var containerHasTOC = {},
@@ -11,9 +11,9 @@ require(['jquery', 'wikia.toc', 'wikia.mustache'], function($, toc, mustache) {
 	 */
 
 	function wrapper() {
-		return function (text, render) {
-			if (text !== '') {
-				return '<ol>' + render(text) + '</ol>';
+		return function ( text, render ) {
+			if ( text !== '' ) {
+				return '<ol>' + render( text ) + '</ol>';
 			} else {
 				return false;
 			}
@@ -29,21 +29,21 @@ require(['jquery', 'wikia.toc', 'wikia.mustache'], function($, toc, mustache) {
 	 *                             or custom TOC single section object.
 	 */
 
-	function createTOCSection(header) {
+	function createTOCSection( header ) {
 
-		header = $(header).children('.mw-headline');
+		header = $( header ).children( '.mw-headline' );
 
-		if (header.length === 0) {
+		if ( header.length === 0 ) {
 			return false;
 		}
 
 		// clone node and remove noscript to exclude it from text
 		header = header.clone();
-		header.find('noscript').remove();
+		header.find( 'noscript' ).remove();
 
 		return {
 			title: header.text(),
-			id: header.attr('id'),
+			id: header.attr( 'id' ),
 			sections: []
 		};
 	}
@@ -57,34 +57,34 @@ require(['jquery', 'wikia.toc', 'wikia.mustache'], function($, toc, mustache) {
 	function loadTemplate() {
 		var dfd = new $.Deferred();
 
-		require(['wikia.loader', 'wikia.cache'], function(loader, cache) {
-			var template = cache.getVersioned(cacheKey);
+		require( ['wikia.loader', 'wikia.cache'], function ( loader, cache ) {
+			var template = cache.getVersioned( cacheKey );
 
-			if(template) {
-				dfd.resolve(template);
+			if ( template ) {
+				dfd.resolve( template );
 			} else {
-				require(['wikia.throbber'], function(throbber) {
-					var toc = $('#toc');
+				require( ['wikia.throbber'], function ( throbber ) {
+					var toc = $( '#toc' );
 
-					throbber.show(toc);
+					throbber.show( toc );
 
-					loader({
+					loader( {
 						type: loader.MULTI,
 						resources: {
 							mustache: 'extensions/wikia/TOC/templates/TOC_articleContent.mustache'
 						}
-					}).done(function(data) {
-						template = data.mustache[0];
+					} ).done( function ( data ) {
+							template = data.mustache[0];
 
-						dfd.resolve(template);
+							dfd.resolve( template );
 
-						cache.setVersioned(cacheKey, template, 604800); //7days
+							cache.setVersioned( cacheKey, template, 604800 ); //7days
 
-						throbber.remove(toc);
-					});
-				});
+							throbber.remove( toc );
+						} );
+				} );
 			}
-		});
+		} );
 
 		return dfd.promise();
 	}
@@ -96,19 +96,19 @@ require(['jquery', 'wikia.toc', 'wikia.mustache'], function($, toc, mustache) {
 	 *                           which gives a context to either article or preview in the editor.
 	 */
 
-	function renderTOC($target) {
-		var $container = $target.parents('#toc').children('ol'),
-			$contentContainer = getContentContainer($target),
-			$headers = $contentContainer.find('h2, h3, h4, h5'),
-			data = toc.getData($headers, createTOCSection);
+	function renderTOC( $target ) {
+		var $container = $target.parents( '#toc' ).children( 'ol' ),
+			$contentContainer = getContentContainer( $target ),
+			$headers = $contentContainer.find( 'h2, h3, h4, h5' ),
+			data = toc.getData( $headers, createTOCSection );
 
 		data.wrapper = wrapper;
 
-		loadTemplate().done(function(template) {
-			$container.append(mustache.render(template, data));
+		loadTemplate().done( function ( template ) {
+			$container.append( mustache.render( template, data ) );
 
-			setHasTOC($target, true);
-		});
+			setHasTOC( $target, true );
+		} );
 	}
 
 	/**
@@ -117,11 +117,11 @@ require(['jquery', 'wikia.toc', 'wikia.mustache'], function($, toc, mustache) {
 	 * @param {?Number} isHidden - accepts 'null' or 1
 	 */
 
-	function setTOCCookie(isHidden) {
-		$.cookie('mw_hidetoc', isHidden, {
+	function setTOCCookie( isHidden ) {
+		$.cookie( 'mw_hidetoc', isHidden, {
 			expires: 30,
 			path: '/'
-		});
+		} );
 	}
 
 	/**
@@ -131,25 +131,25 @@ require(['jquery', 'wikia.toc', 'wikia.mustache'], function($, toc, mustache) {
 	 *                           which gives a context to either article or preview in the editor.
 	 */
 
-	function showHideTOC($target) {
-		var tocWrapper = $target.parents('#toc'),
+	function showHideTOC( $target ) {
+		var tocWrapper = $target.parents( '#toc' ),
 			targetLabel,
 			tocCookie;
 
-		tocWrapper.toggleClass('show');
+		tocWrapper.toggleClass( 'show' );
 
-		if (tocWrapper.hasClass('show')) {
-			targetLabel = $target.data('hide');
+		if ( tocWrapper.hasClass( 'show' ) ) {
+			targetLabel = $target.data( 'hide' );
 			tocCookie = null;
 		} else {
-			targetLabel = $target.data('show');
+			targetLabel = $target.data( 'show' );
 			tocCookie = 1;
 		}
 
-		$target.text(targetLabel);
-		$target.attr('title', targetLabel);
+		$target.text( targetLabel );
+		$target.attr( 'title', targetLabel );
 
-		setTOCCookie(tocCookie);
+		setTOCCookie( tocCookie );
 	}
 
 	/**
@@ -161,67 +161,67 @@ require(['jquery', 'wikia.toc', 'wikia.mustache'], function($, toc, mustache) {
 	 */
 
 	function isNewTOC() {
-		return $('#toc').is('nav');
+		return $( '#toc' ).is( 'nav' );
 	}
 
 	/**
 	 * Initialized the TOC after an article has been loaded
 	 */
 	function initTOC() {
-		var $showLink = $('#togglelink');
-		if (!hasTOC($showLink)) {
-			renderTOC($showLink);
+		var $showLink = $( '#togglelink' );
+		if ( !hasTOC( $showLink ) ) {
+			renderTOC( $showLink );
 		}
-		showHideTOC($showLink);
+		showHideTOC( $showLink );
 	}
 
-	function hasTOC($target) {
-		var containerIdentifier = getContainerIndetifier($target);
+	function hasTOC( $target ) {
+		var containerIdentifier = getContainerIndetifier( $target );
 
 		return typeof containerHasTOC[containerIdentifier] !== 'undefined' && containerHasTOC[containerIdentifier];
 	}
 
-	function setHasTOC($target, value) {
-		var containerIdentifier = getContainerIndetifier($target);
+	function setHasTOC( $target, value ) {
+		var containerIdentifier = getContainerIndetifier( $target );
 		containerHasTOC[containerIdentifier] = value;
 	}
 
-	function getContentContainer($target) {
+	function getContentContainer( $target ) {
 		// if tabviewer is on site use content from one tab only
 		return $target.parents( '.tabBody, #mw-content-text' ).first();
 	}
 
-	function getContainerIndetifier($target) {
-		return getContentContainer($target ).data('tab-body') || 'main';
+	function getContainerIndetifier( $target ) {
+		return getContentContainer( $target ).data( 'tab-body' ) || 'main';
 	}
 
-	$(function() {
+	$( function () {
 		/** Attach events */
-		$('body').on('click', '#togglelink', function(event) {
+		$( 'body' ).on( 'click', '#togglelink', function ( event ) {
 			event.preventDefault();
 
-			if(isNewTOC()) {
-				var $target = $(event.target);
+			if ( isNewTOC() ) {
+				var $target = $( event.target );
 
-				if (!hasTOC($target)) {
-					renderTOC($target);
+				if ( !hasTOC( $target ) ) {
+					renderTOC( $target );
 				}
 
-				showHideTOC($target);
+				showHideTOC( $target );
 			}
-		});
+		} );
 
 		// reset containerHasTOC flags for each time preview modal is opened
-		$(window).on('EditPageAfterRenderPreview', function() {
+		$( window ).on( 'EditPageAfterRenderPreview', function () {
 			containerHasTOC = {};
-			if (isNewTOC() && window.wgUserName !== null) {
+			if ( isNewTOC() && window.wgUserName !== null ) {
 				initTOC();
 			}
-		});
+		} );
 
 		/** Auto expand TOC in article for logged-in users with hideTOC cookie set to 'null'  */
-		if (isNewTOC() && window.wgUserName !== null && $.cookie('mw_hidetoc') === null) {
+		if ( isNewTOC() && window.wgUserName !== null && $.cookie( 'mw_hidetoc' ) === null ) {
 			initTOC();
 		}
-	});
-});
+	} );
+} );
