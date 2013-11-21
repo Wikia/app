464883bda2125dcc00bbff8bcb02ffceb88a99de
James Sutterfield <james@wikia-inc.com>
2013-11-20 16:21:48 -0800

    Changed text() calls to plain() calls

--- extensions/wikia/TOC/TOCController.class.php
+++ extensions/wikia/TOC/TOCController.class.php
@@ -4,9 +4,9 @@ class TOCController extends WikiaController {
 
 	public function index() {
 
-		$this->tocHeader = wfMessage( 'toc' )->inContentLanguage()->text();
-		$this->show = wfMessage( 'showtoc' )->inContentLanguage()->text();
-		$this->hide = wfMessage( 'hidetoc' )->inContentLanguage()->text();
+		$this->tocHeader = wfMessage( 'toc' )->inContentLanguage()->plain();
+		$this->show = wfMessage( 'showtoc' )->inContentLanguage()->plain();
+		$this->hide = wfMessage( 'hidetoc' )->inContentLanguage()->plain();
 		$this->response->setTemplateEngine(WikiaResponse::TEMPLATE_ENGINE_MUSTACHE);
 	}
 }
