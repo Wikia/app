f692fa97257affb2039a4fdc9d094f0b9de59f38
Christian Williams <christian@wikia-inc.com>
2013-11-21 01:38:11 +0000

    short tags

--- extensions/wikia/WikiFeatures/templates/WikiFeaturesSpecial_feature.php
+++ extensions/wikia/WikiFeatures/templates/WikiFeaturesSpecial_feature.php
@@ -1,20 +1,20 @@
 <li class="feature" data-name="<?= $feature['name'] ?>">
 	<h3>
 		<?= wfMsg('wikifeatures-feature-heading-'.$feature['name']) ?>
-		<? if (isset($feature['active'])) { ?>
-		<span class="active-on">
-			<?= wfMsg('wikifeatures-active-on', $feature['active']) ?>
-		</span>
-		<? } ?>
+		<? if (isset($feature['active'])): ?>
+			<span class="active-on">
+				<?= wfMsg('wikifeatures-active-on', $feature['active']) ?>
+			</span>
+		<? endif; ?>
 	</h3>
 
 	<div class="representation<?= !empty($feature['new']) ? ' promotion' : '' ?>">
 		<div class="feature-image-wrapper">
 			<img src="<?= $wg->ExtensionsPath ?>/wikia/WikiFeatures/images/<?= $feature['name'] ?>.png" >
 		</div>
-		<? if(!empty($feature['new'])) { ?>
+		<? if (!empty($feature['new'])): ?>
 			<span class="promo-text"><?= wfMsg('wikifeatures-promotion-new') ?></span>
-		<? } ?>
+		<? endif; ?>
 	</div>
 	<div class="details">
 		<p>
@@ -22,19 +22,19 @@
 		</p>
 	</div>
 	<div class="actions">
-		<? if ($editable && isset($feature['enabled'])) { ?>
+		<? if ($editable && isset($feature['enabled'])): ?>
 			<span class="slider<?= $feature['enabled'] ? ' on' : '' ?>">
 				<span class="button"></span>
 				<span class="textoff"><?= wfMsg('wikifeatures-toggle-inactive') ?></span>
 				<span class="texton"><?= wfMsg('wikifeatures-toggle-active') ?></span>
 				<span class="loading"></span>
 			</span>
-		<? } ?>
-		<? if (isset($feature['active'])) { ?>
+		<? endif; ?>
+		<? if (isset($feature['active'])): ?>
 			<button class="secondary feedback">
 				<img height="10" width="10" src="<?= $wg->ExtensionsPath ?>/wikia/WikiFeatures/images/star-inactive.png">
 				<?php echo wfMsg('wikifeatures-feedback'); ?>
 			</button>
-		<? } ?>
+		<? endif; ?>
 	</div>
 </li>
