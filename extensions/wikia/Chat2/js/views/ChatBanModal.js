1c72c81e0e66ae1fbb37cc7bec8c005bdb01a93e
aquilax <evgeniy@wikia-inc.com>
2013-11-21 16:40:28 +0000

    Converted Chat Ban Modal

--- extensions/wikia/Chat2/js/views/ChatBanModal.js
+++ extensions/wikia/Chat2/js/views/ChatBanModal.js
@@ -1,9 +1,9 @@
-var ChatBanModal = function(title, okCallback, options) {
+var ChatBanModal = function( title, okCallback, options ) {
 	'use strict';
 
 	var data = {};
 
-	if (options) {
+	if ( options ) {
 		data = {
 			userId: options.userId || ''
 		};
@@ -12,77 +12,74 @@ var ChatBanModal = function(title, okCallback, options) {
 	// TODO: Remove isChangeBan - back end will check for this
 	$.get( window.wgScript + '?action=ajax&rs=ChatAjax&method=BanModal', data, function( data ) {
 		require( [ 'wikia.ui.factory' ], function( uiFactory ) {
-			uiFactory.init( [ 'button', 'modal' ] ).then( function( uiButton, uiModal ) {
-				var primaryBtnMsg = data.isChangeBan ?
-						$.msg('chat-ban-modal-button-change-ban') : $.msg('chat-ban-modal-button-ok'),
-					modalPrimaryBtn = uiButton.render( {
-						'type': 'link',
-						'vars': {
-							'id': 'ok',
-							'href': '#',
-							'classes': [ 'normal', 'primary' ],
-							'value': primaryBtnMsg,
-							'title': primaryBtnMsg
-						}
-					} ),
-					secondaryBtnMsg = $.msg( 'chat-ban-modal-button-cancel' ),
-					modalSecondaryBtn = uiButton.render( {
-						'type': 'link',
-						'vars': {
-							'id': 'cancel',
-							'href': '#',
-							'classes': [ 'normal', 'secondary' ],
-							'value': secondaryBtnMsg,
-							'title': secondaryBtnMsg
-						}
-					}),
-					modalId = 'ChatBanModal',
-					banModal = uiModal.render( {
+			uiFactory.init( [ 'modal' ] ).then( function( uiModal ) {
+				var banModalConfig = {
 						type: 'default',
 						vars: {
-							id: modalId,
+							id: 'ChatBanModal',
 							size: 'small',
 							content: data.template,
 							title: title,
-							closeText: $.msg( 'close' ),
-							primaryBtn: modalPrimaryBtn,
-							secondBtn: modalSecondaryBtn
+							buttons: [
+								{
+									vars: {
+										value: data.isChangeBan ?
+											$.msg( 'chat-ban-modal-button-change-ban' ) :
+											$.msg( 'chat-ban-modal-button-ok' ),
+										classes: [ 'normal', 'primary' ],
+										data: [
+											{
+												key: 'event',
+												value: 'ok'
+											}
+										]
+									}
+								},
+								{
+									'vars': {
+										'value': $.msg( 'chat-ban-modal-button-cancel' ),
+										data: [
+											{
+												key: 'event',
+												value: 'close'
+											}
+										]
+									}
+
+								}
+							]
 						}
-					} );
+					};
 
-				require( [ 'wikia.ui.modal' ], function( modal ) {
-					banModal = modal.init( modalId, banModal );
+				uiModal.createComponent( banModalConfig, function( banModal ) {
 
-					banModal.$element.find( '#cancel' ).click( function( event ) {
-						event.preventDefault();
-						banModal.close();
-					} );
+					var reasonInput = banModal.$element.find( 'input[name=reason]' );
 
-					var banModalOkBtn = banModal.$element.find( '#ok'),
-						reasonInput = banModal.$element.find( 'input[name=reason]' );
-
-					reasonInput.placeholder().keydown( function( e ) {
-						if( e.which === 13 ) {
-							// Submit when 'enter' key is pressed (BugId:28101).
-							e.preventDefault();
-							banModalOkBtn.click();
-						}
-					} );
-
-					banModalOkBtn.click( function( event ) {
-						event.preventDefault();
-
+					function banUser() {
 						var reason = reasonInput.val(),
 							expires = banModal.$element.find( 'select[name=expires]' ).val();
 
 						okCallback( expires, reason );
 
 						banModal.close();
-					} );
+					}
+
+					reasonInput.placeholder().keydown(function( e ) {
+						if( e.which === 13 ) {
+							// Submit when 'enter' key is pressed (BugId:28101).
+							e.preventDefault();
+							banUser();
+						}
+					});
+
+					banModal.bind( 'ok', function ( event ) {
+						event.preventDefault();
+						banUser();
+					});
 
 					banModal.show();
-				} );
-			} );
-		} );
-	} );
+				});
+			});
+		});
+	});
 };
