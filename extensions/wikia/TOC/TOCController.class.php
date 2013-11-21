ce74552d95133a8a0d6cd81eb9e5e807056ac9b5
James Sutterfield <james@wikia-inc.com>
2013-11-20 16:02:08 -0800

    TOC should be in content language, not user language

--- extensions/wikia/TOC/TOCController.class.php
+++ extensions/wikia/TOC/TOCController.class.php
@@ -4,9 +4,9 @@ class TOCController extends WikiaController {
 
 	public function index() {
 
-		$this->tocHeader = wfMessage( 'toc' )->plain();
-		$this->show = wfMessage( 'showtoc' )->plain();
-		$this->hide = wfMessage( 'hidetoc' )->plain();
+		$this->tocHeader = wfMessage( 'toc' )->inContentLanguage()->text();
+		$this->show = wfMessage( 'showtoc' )->inContentLanguage()->text();
+		$this->hide = wfMessage( 'hidetoc' )->inContentLanguage()->text();
 		$this->response->setTemplateEngine(WikiaResponse::TEMPLATE_ENGINE_MUSTACHE);
 	}
 }
