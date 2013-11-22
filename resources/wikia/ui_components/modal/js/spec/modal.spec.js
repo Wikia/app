6d0ed090fbf25d55d584ef9b631195a2ab48889b
mech <mech@wikia-inc.com>
2013-11-22 15:22:27 +0100

    DAR-2604: modal - tests for multiple listeners

--- resources/wikia/ui_components/modal/js/spec/modal.spec.js
+++ resources/wikia/ui_components/modal/js/spec/modal.spec.js
@@ -38,7 +38,7 @@ describe( 'Modal events', function() {
 	it( 'triggers the proper event listener', function() {
 		var listeners = {
 			onFoo : function () { },
-			onBar : function () { },
+			onBar : function () { }
 		};
 		spyOn( listeners, 'onFoo' );
 		spyOn( listeners, 'onBar' );
@@ -51,6 +51,42 @@ describe( 'Modal events', function() {
 		expect( listeners.onFoo ).toHaveBeenCalled();
 		expect( listeners.onFoo.calls.length ).toEqual( 2 );
 		expect( listeners.onBar ).not.toHaveBeenCalled();
+	} );
+
+	it( 'triggers all event listeners', function() {
+		var listeners = {
+			onFoo1 : function () { },
+			onFoo2 : function () { }
+		};
+		spyOn( listeners, 'onFoo1' );
+		spyOn( listeners, 'onFoo2' );
+
+		modal.bind( 'foo', listeners.onFoo1 );
+		modal.bind( 'foo', listeners.onFoo2 );
+		modal.trigger( 'foo' );
+
+		expect( listeners.onFoo1 ).toHaveBeenCalled();
+		expect( listeners.onFoo1.calls.length ).toEqual( 1 );
+		expect( listeners.onFoo2 ).toHaveBeenCalled();
+		expect( listeners.onFoo2.calls.length ).toEqual( 1 );
+	} );
+
+	it( 'triggers event listeners in order', function() {
+		var array = [],
+			listeners = {
+			onFoo1 : function () {
+				array.push( 'foo1' );
+			},
+			onFoo2 : function () {
+				array.push( 'foo2' );
+			}
+		};
+
+		modal.bind( 'foo', listeners.onFoo1 );
+		modal.bind( 'foo', listeners.onFoo2 );
+		modal.trigger( 'foo' );
+
+		expect( array ).toEqual( [ 'foo1', 'foo2' ] );
 	} );
 
 });
