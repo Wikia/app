fe461a394dcf7445dbaff6c82230ce9fb1049d1b
mech <mech@wikia-inc.com>
2013-11-22 15:15:06 +0100

    DAR-2604: modal - basic tests for triggering events

--- resources/wikia/ui_components/modal/js/spec/modal.spec.js
+++ resources/wikia/ui_components/modal/js/spec/modal.spec.js
@@ -7,7 +7,7 @@ describe( 'Modal module', function() {
 	it( 'registers AMD module', function() {
 		expect( modal ).toBeDefined();
 		expect( typeof modal ).toBe( 'object' );
-	});
+	} );
 
 });
 
@@ -15,10 +15,42 @@ describe( 'Modal events', function() {
 	'use strict';
 
 	var browserDetect = {},
+		module = modules[ 'wikia.ui.modal' ]( jQuery, window, browserDetect),
 		modal = null;
 
 	beforeEach( function() {
-		modal = modules[ 'wikia.ui.modal' ]( jQuery, window, browserDetect );
-	});
+		modal = module.init( 'test' );
+	} );
+
+	it( 'triggers the event listener exactly once', function() {
+		var listeners = {
+			onFoo : function () { }
+		};
+		spyOn( listeners, 'onFoo' );
+
+		modal.bind( 'foo', listeners.onFoo );
+		modal.trigger( 'foo' );
+
+		expect( listeners.onFoo ).toHaveBeenCalled();
+		expect( listeners.onFoo.calls.length ).toEqual( 1 );
+	} );
+
+	it( 'triggers the proper event listener', function() {
+		var listeners = {
+			onFoo : function () { },
+			onBar : function () { },
+		};
+		spyOn( listeners, 'onFoo' );
+		spyOn( listeners, 'onBar' );
+
+		modal.bind( 'foo', listeners.onFoo );
+		modal.bind( 'bar', listeners.onBar );
+		modal.trigger( 'foo' );
+		modal.trigger( 'foo' );
+
+		expect( listeners.onFoo ).toHaveBeenCalled();
+		expect( listeners.onFoo.calls.length ).toEqual( 2 );
+		expect( listeners.onBar ).not.toHaveBeenCalled();
+	} );
 
 });
