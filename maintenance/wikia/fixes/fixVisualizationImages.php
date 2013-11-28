a5b92e9c594fcdbad4f2e0af3807ac8527b17ea1
Damian Jóźwiak <damian@wikia-inc.com>
2013-11-28 15:00:03 +0000

    DAR-2540 | CV lost images recovery maintenance script

--- maintenance/wikia/fixes/fixVisualizationImages.php
+++ maintenance/wikia/fixes/fixVisualizationImages.php
@@ -1,4 +1,4 @@
-<?php
+<?php                                                        
 require_once( dirname(__FILE__) . '/../../Maintenance.php' );
 
 class FixWikiMainImage extends Maintenance {
@@ -15,10 +15,25 @@ class FixWikiMainImage extends Maintenance {
 			$wikiLocalImage = $this->getCVImage($wiki['city_id']);
 			if (!empty($wikiLocalImage)) {
 				$dbname = WikiFactory::IDtoDB($wiki['city_id']);
-				$corpWikiImageName = $this->getCorpWikiImageName($wikiLocalImage->image_name, $dbname);
-				$corpWikiId = $corporateModel->getCorporateWikiIdByLang($wiki['city_lang_code']);
-				$fileTitle = GlobalTitle::newFromText($corpWikiImageName, NS_FILE, $corpWikiId);
-				var_dump($fileTitle->getFullURL(), $fileTitle->exists());
+				try {
+				    $corpWikiId = $corporateModel->getCorporateWikiIdByLang($wiki['city_lang_code']);
+				}
+				catch (Exception $e) {
+				    var_dump($wiki['city_id'], $wiki['city_lang_code']);
+				}
+
+				$t = GlobalTitle::newFromText('Wikia-Visualization-Main.png', NS_FILE, $wiki['city_id']);
+				
+				$task = new PromoteImageReviewTask();
+				
+				var_dump($t->getArticleID(), $wiki['city_main_image'], $corpWikiId, $wiki['city_id']);
+				
+				$res = $task->uploadSingleImage($t->getArticleID(), 'Wikia-Visualization-Main.png', $corpWikiId, $wiki['city_id']);
+				if ($res['status'] == 0 && !empty($res['name'])) {
+				var_dump($res);
+				    $this->updateVisualizationMainImageName($wiki['city_id'], $res['name']);
+				}
+				
 			}
 		}
 	}
@@ -31,7 +46,10 @@ class FixWikiMainImage extends Maintenance {
 		$result = $dbr->select(
 			array( CityVisualization::CITY_VISUALIZATION_TABLE_NAME ),
 			array( '*' ),
-			array( 'city_main_image' => null ),
+			array(
+			    'city_main_image' => null,
+			    'city_lang_code' => F::app()->wg->ContLang->getCode() 
+			),
 			__METHOD__
 		);
 
@@ -49,11 +67,25 @@ class FixWikiMainImage extends Maintenance {
 			array( '*' ),
 			array(
 				'city_id' => $wikiId,
-				'image_index' => 0
+				'image_index' => 0,
+				'image_review_status' => ImageReviewStatuses::STATE_APPROVED
 			),
 			__METHOD__
 		);
 		return $result;
+	}
+
+	protected function updateVisualizationMainImageName($wikiId, $imageName) {
+		global $wgExternalSharedDB;
+		$db = wfGetDB( DB_MASTER, array(), $wgExternalSharedDB );
+		var_dump($wikiId, $imageName);
+		$db->update(
+		    CityVisualization::CITY_VISUALIZATION_TABLE_NAME, 
+		    ['city_main_image' => $imageName], 
+		    ['city_id' => $wikiId, 'city_lang_code' => F::app()->wg->ContLang->getCode()], 
+		    __METHOD__
+		);
+		$db->commit();
 	}
 
 	protected function getCorpWikiImageName($destinationName, $wikiDBname) {
@@ -65,6 +97,5 @@ class FixWikiMainImage extends Maintenance {
 		return implode('', $destinationFileNameArr).'.'.$destinationFileExt;
 	}
 }
-
 $maintClass = "FixWikiMainImage";
 require_once( DO_MAINTENANCE );
