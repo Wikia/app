b0a732c311baf6d545fc847e0a7f9eabb4b05e6b
Kenneth Kouot <kenneth@wikia-inc.com>
2013-12-03 22:27:04 +0000

    fix removal of cart items

--- extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaCartWidget.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaCartWidget.js
@@ -18,7 +18,7 @@ ve.ui.WikiaCartWidget.prototype.onAdd = function ( items, index ) {
 ve.ui.WikiaCartWidget.prototype.onRemove = function ( items ) {
 	var i, widgetItem, widgetItems = [];
 	for ( i = 0; i < items.length; i++ ) {
-		widgetItem = this.getItemFromData( items[i].title );
+		widgetItem = this.getItemFromData( items[i].id || items[i].title );
 		if ( widgetItem ) {
 			widgetItems.push( widgetItem );
 		}
