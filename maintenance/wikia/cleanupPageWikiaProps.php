f9668a41c01bbeff7df717c4645c32653daa633a
James Sutterfield <james@wikia-inc.com>
2013-12-05 16:51:42 -0800

    Changes following code review

--- maintenance/wikia/cleanupPageWikiaProps.php
+++ maintenance/wikia/cleanupPageWikiaProps.php
@@ -14,24 +14,21 @@
 
 class CleanupPageWikiaProps {
 
-	const STATUS_SWAPPABLE = 8;
-	const WPP_LVS_SUGGEST = 19;
-	const WPP_LVS_STATUS = 22;
-
-
-	public static function run ( $db, $dbname, $verbose = false, $test = false ) {
+	public static function run ( $db, $test = false, $verbose = false, $params ) {
+		$dbname = $params['dbname'];
+		$helper = new LicensedVideoSwapHelper();
 
 		// Get all pages which have a status record with the swappable
 		// bit turned on, but do not have a corresponding suggestions record
 		$sql = "SELECT page_id
 				FROM page_wikia_props
-				WHERE propname = WPP_LVS_STATUS
-				AND props & STATUS_SWAPPABLE != 0
+				WHERE propname = " . WPP_LVS_STATUS . "
+				AND props & " . LicensedVideoSwapHelper::STATUS_SWAPPABLE . " != 0
 				AND page_id not in (
 					SELECT page_id
 					FROM page_wikia_props
-					WHERE propname = WPP_LVS_SUGGEST)";
-		$result = $db->query($sql);
+					WHERE propname = " . WPP_LVS_SUGGEST . ")";
+		$result = $db->query( $sql );
 
 		$pagesWithoutSuggestions = array();
 		while ( $row = $db->fetchObject( $result ) ) {
@@ -39,29 +36,25 @@ class CleanupPageWikiaProps {
 		}
 
 		// Turn off the swappable bit in the props column for any page_ids found in the previous query
-		foreach ( $pagesWithoutSuggestions as $page )   {
-			$sql = "UPDATE page_wikia_props
-					SET props=props & ~STATUS_SWAPPABLE
-					WHERE page_id = $page
-					AND propname = WPP_LVS_STATUS";
-			$db->query( $sql );
-
-			if ($verbose) {
-				echo "Found status record in $dbname without suggestion record. Turning off swappable bit for page_id: $page\n";
-				print_tmp("Turn off bit. Database: $dbname -- Page_id: $page\n");
+		foreach ( $pagesWithoutSuggestions as $page_id )   {
+			if ( !$test ) {
+				$helper->clearFlag( $page_id, $helper::STATUS_SWAPPABLE );
+			}
+			if ( $verbose ) {
+				echo "Found status record in $dbname without suggestion record. Turning off swappable bit for page_id: $page_id\n";
 			}
 		}
 
 		// Get all pages which have a suggestion record, but do not have a status record with the swappable bit turned on
 		$sql = "SELECT page_id
 				FROM page_wikia_props
-				WHERE propname = WPP_LVS_SUGGEST
+				WHERE propname = " . WPP_LVS_SUGGEST . "
 				AND page_id not in (
 					SELECT page_id
 					FROM page_wikia_props
-					WHERE propname = WPP_LVS_STATUS
-					AND props & STATUS_SWAPPABLE != 0)";
-		$result = $db->query($sql);
+					WHERE propname = " . WPP_LVS_STATUS . "
+					AND props & " . LicensedVideoSwapHelper::STATUS_SWAPPABLE . " != 0)";
+		$result = $db->query( $sql );
 
 		$suggestionsWithoutSwappableBit = array();
 		while ( $row = $db->fetchObject( $result ) ) {
@@ -69,36 +62,49 @@ class CleanupPageWikiaProps {
 		}
 
 		// Turn on the swappable bit in the props column for any page_ids found in the previous query
-		foreach ( $suggestionsWithoutSwappableBit as $suggestion )   {
+		foreach ( $suggestionsWithoutSwappableBit as $page_id )   {
 			// First make sure that a status record actually exists for this video
-			$result = $db->query( "SELECT page_id from page_wikia_props where page_id = $suggestion and propname = WPP_LVS_STATUS" );
+			$result = $db->query( "SELECT page_id from page_wikia_props where page_id = $page_id and propname = " . WPP_LVS_STATUS );
 			// if not, create it first
-			if (! $db->fetchObject( $result )) {
-				$db->query( "INSERT INTO page_wikia_props (page_id, propname, props) values ($suggestion, WPP_LVS_STATUS, 0)" );
+			if ( !$db->fetchObject( $result ) ) {
+				if ( !$test ) {
+					$db->query( "INSERT INTO page_wikia_props (page_id, propname, props) values ($page_id, " . WPP_LVS_STATUS . ", 0)" );
+				}
 			}
-
-			// Turn on swappable bit
-			$db->query( "UPDATE page_wikia_props SET props=props | STATUS_SWAPPABLE WHERE page_id = $suggestion AND propname = WPP_LVS_STATUS" );
+			if ( !$test ) {
+				$helper->setPageStatusSwappable( $page_id );
+			}
 			if ( $verbose ) {
-				echo "Suggestion record found in $dbname without swappable bit turned on in status record. Turning on swappable for $suggestion\n";
-				print_tmp("Turn on bit. Database: $dbname -- Page_id: $suggestion\n");
+				echo "Suggestion record found in $dbname without swappable bit turned on in status record. Turning on swappable for $page_id\n";
 			}
 		}
 
 		// Finally, make sure that all pages listed in the page_wikia_props table actually exist in the page table.
 		// If they do not, delete them.
-		$result = $db->query("SELECT page_id FROM page_wikia_props WHERE page_id NOT IN (SELECT page_id FROM page)");
+		$result = $db->query( "SELECT pp.page_id FROM page_wikia_props pp LEFT JOIN page p ON pp.page_id=p.page_id WHERE p.page_id IS NULL" );
 
-		$deletePages = array();
+		$page_ids = array();
 		while ( $row = $db->fetchObject( $result ) ) {
-			$deletePages[] = $row->page_id;
+			$page_ids[] = $row->page_id;
+			// Send MySQL pages to be deleted in batches of 100
+			if ( count($page_ids) == 100 ) {
+				if ( $verbose ) {
+					echo "Deleted pages found in $dbname. Deleting " . count($page_ids) . " records from page_wikia_props\n";
+				}
+				if ( !$test ) {
+					$db->query( "DELETE FROM page_wikia_props WHERE IN (" . implode(",", $page_ids) . ")" );
+				}
+				$page_ids = array();
+			}
 		}
 
-		foreach ( $deletePages as $page ) {
-			$db->query( "DELETE FROM page_wikia_props WHERE page_id = $page" );
+		// Make sure to delete the last batch since it's probably less than 100.
+		if ( count($page_ids) ) {
 			if ( $verbose ) {
-				echo "Deleted page found in $dbname. Deleting records with page_id $page from page_wikia_props\n";
-				print_tmp("Deleted page found in $dbname. Deleting records with page_id $page from page_wikia_props");
+				echo "Deleted pages found in $dbname. Deleting " . count($page_ids) . " records from page_wikia_props\n";
+			}
+			if ( !$test ) {
+				$db->query( "DELETE FROM page_wikia_props WHERE IN (" . implode(",", $page_ids) . ")" );
 			}
 		}
 
