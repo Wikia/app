5d746660fccf36e4eabdf6e0fc1a19e60e3f0ec1
lizlux <liz@lizlux.net>
2013-12-06 17:52:21 -0800

    updating whitespace

--- extensions/wikia/ImagePlaceholder/js/MediaPlaceholder.js
+++ extensions/wikia/ImagePlaceholder/js/MediaPlaceholder.js
@@ -7,127 +7,126 @@
 
 (function( $, window ) {
 
-'use strict';
+	'use strict';
 
-// Make global so JSSnippets can pick it up
-window.MediaPlaceholder = {
+	// Make global so JSSnippets can pick it up
+	window.MediaPlaceholder = {
 
-	init: function() {
+		init: function() {
 
-		// Don't run more than once
-		if ( this.loaded ) {
-			return;
-		}
+			// Don't run more than once
+			if ( this.loaded ) {
+				return;
+			}
 
-		this.loaded = true;
-		this.imageLoaded = false;
-		this.WikiaArticle = $( '#WikiaArticle' );
+			this.loaded = true;
+			this.imageLoaded = false;
+			this.WikiaArticle = $( '#WikiaArticle' );
 
-		// Don't allow editing on history or diff pages
-		if ( $.getUrlVar( 'diff' ) || $.getUrlVar( 'oldid' ) ) {
-			return;
-		}
+			// Don't allow editing on history or diff pages
+			if ( $.getUrlVar( 'diff' ) || $.getUrlVar( 'oldid' ) ) {
+				return;
+			}
 
-		this.setupVideoPlaceholders();
-		this.setupImagePlaceholders();
-	},
+			this.setupVideoPlaceholders();
+			this.setupImagePlaceholders();
+		},
 
-	setupImagePlaceholders: function() {
-		var self = this;
+		setupImagePlaceholders: function() {
+			var self = this;
 
-		// Handle clicks on image placeholders (video placeholders are handled differently)
-		this.WikiaArticle.on( 'click', '.wikiaImagePlaceholder a', function( e ) {
-			var $this = $( this ),
-				props = self.getProps( $this );
+			// Handle clicks on image placeholders (video placeholders are handled differently)
+			this.WikiaArticle.on( 'click', '.wikiaImagePlaceholder a', function( e ) {
+				var $this = $( this ),
+					props = self.getProps( $this );
 
-			e.preventDefault();
+				e.preventDefault();
 
-			// Provide immediate feedback once button is clicked
-			$this.startThrobbing();
+				// Provide immediate feedback once button is clicked
+				$this.startThrobbing();
 
-			if ( !self.imageLoaded ) {
-				// open WMU
-				$.when(
-					$.getResources( [
-						$.loadYUI,
-						$.loadJQueryAIM,
-						$.getSassCommonURL( 'extensions/wikia/WikiaMiniUpload/css/WMU.scss' ),
-						window.wgResourceBasePath + '/extensions/wikia/WikiaMiniUpload/js/WMU.js'
-					] )
-				).done( function() {
-					self.imageLoaded = true;
+				if ( !self.imageLoaded ) {
+					// open WMU
+					$.when(
+						$.getResources( [
+							$.loadYUI,
+							$.loadJQueryAIM,
+							$.getSassCommonURL( 'extensions/wikia/WikiaMiniUpload/css/WMU.scss' ),
+							window.wgResourceBasePath + '/extensions/wikia/WikiaMiniUpload/js/WMU.js'
+						] )
+					).done( function() {
+						self.imageLoaded = true;
+						$this.stopThrobbing();
+						window.WMU_show( // jshint ignore:line
+							window.event, -2, props.placeholderIndex, props.align, props.thumb,
+							props.width, props.caption, props.link
+						);
+					} );
+				} else {
 					$this.stopThrobbing();
 					window.WMU_show( // jshint ignore:line
-						window.event, -2, props.placeholderIndex, props.align, props.thumb,
-						props.width, props.caption, props.link
+						window.event, -2, props.placeholderIndex, props.align, props.thumb, props.width,
+						props.caption, props.link
 					);
-				} );
-			} else {
-				$this.stopThrobbing();
-				window.WMU_show( // jshint ignore:line
-					window.event, -2, props.placeholderIndex, props.align, props.thumb, props.width,
-					props.caption, props.link
-				);
-			}
-		} );
-	},
-	setupVideoPlaceholders: function() {
-		var self = this;
-
-		self.WikiaArticle.find( '.wikiaVideoPlaceholder a' ).each( function() {
-			var $this = $( this ),
-				props = self.getProps( $this );
-
-			$this.addVideoButton( {
-				embedPresets: props,
-				insertFinalVideoParams: [
-					'placeholder=1',
-					'box=' + props.placeholderIndex,
-					'article=' + encodeURIComponent( window.wgTitle ),
-					'ns=' + window.wgNamespaceNumber
-				],
-				callbackAfterEmbed: $.proxy( self.videoEmbedCallback, self )
+				}
 			} );
-		} );
-	},
+		},
+		setupVideoPlaceholders: function() {
+			var self = this;
 
-	videoEmbedCallback: function( embedData ) {
-		var placeholders = this.WikiaArticle.find( '.wikiaVideoPlaceholder a' ),
-			// get placeholder to turn into a video thumbnail
-			toUpdate = placeholders.filter( '[data-id=' + embedData.placeholderIndex + ']' ),
-			// get thumbnail code from hidden div in success modal
-			html = $( '#VideoEmbedCode' ).html();
+			self.WikiaArticle.find( '.wikiaVideoPlaceholder a' ).each( function() {
+				var $this = $( this ),
+					props = self.getProps( $this );
 
-		toUpdate.closest( '.wikiaVideoPlaceholder' ).replaceWith( html );
+				$this.addVideoButton( {
+					embedPresets: props,
+					insertFinalVideoParams: [
+						'placeholder=1',
+						'box=' + props.placeholderIndex,
+						'article=' + encodeURIComponent( window.wgTitle ),
+						'ns=' + window.wgNamespaceNumber
+					],
+					callbackAfterEmbed: $.proxy( self.videoEmbedCallback, self )
+				} );
+			} );
+		},
 
-		// update data id so we can match DOM placeholders to parsed wikitext placeholders
-		placeholders.each( function() {
-			var $this = $( this ),
-				id = $this.attr( 'data-id' );
+		videoEmbedCallback: function( embedData ) {
+			var placeholders = this.WikiaArticle.find( '.wikiaVideoPlaceholder a' ),
+				// get placeholder to turn into a video thumbnail
+				toUpdate = placeholders.filter( '[data-id=' + embedData.placeholderIndex + ']' ),
+				// get thumbnail code from hidden div in success modal
+				html = $( '#VideoEmbedCode' ).html();
 
-			if ( id > embedData.placeholderIndex ) {
-				$this.attr( 'data-id', id - 1 );
-			}
-		} );
+			toUpdate.closest( '.wikiaVideoPlaceholder' ).replaceWith( html );
 
-		// purge cache of article so video will show up on reload
-		$.post( window.wgScript, {title: window.wgPageName, action: 'purge'} );
+			// update data id so we can match DOM placeholders to parsed wikitext placeholders
+			placeholders.each( function() {
+				var $this = $( this ),
+					id = $this.attr( 'data-id' );
 
-		// re-bind events
-		this.setupVideoPlaceholders();
-	},
+				if ( id > embedData.placeholderIndex ) {
+					$this.attr( 'data-id', id - 1 );
+				}
+			} );
 
-	// Get data for WMU / VET from placeholder element
-	getProps: function( elem ) {
-		return {
-			placeholderIndex: elem.attr( 'data-id' ),
-			align: elem.attr( 'data-align' ),
-			width: elem.attr( 'data-width' ),
-			thumb: elem.attr( 'data-thumb' ),
-			link: elem.attr( 'data-link' ),
-			caption: elem.attr( 'data-caption' )
-		};
-	}
-};
+			// purge cache of article so video will show up on reload
+			$.post( window.wgScript, {title: window.wgPageName, action: 'purge'} );
 
+			// re-bind events
+			this.setupVideoPlaceholders();
+		},
+
+		// Get data for WMU / VET from placeholder element
+		getProps: function( elem ) {
+			return {
+				placeholderIndex: elem.attr( 'data-id' ),
+				align: elem.attr( 'data-align' ),
+				width: elem.attr( 'data-width' ),
+				thumb: elem.attr( 'data-thumb' ),
+				link: elem.attr( 'data-link' ),
+				caption: elem.attr( 'data-caption' )
+			};
+		}
+	};
 })( jQuery, window );
