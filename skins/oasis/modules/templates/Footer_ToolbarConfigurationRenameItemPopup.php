72dfa8510a26eb614dd79accb58f9e90fbdbe8cd
Rafal Leszczynski <rafal@wikia-inc.com>
2013-12-11 17:01:41 +0100

    DAR-2557, apply new styles to rename item modal

--- skins/oasis/modules/templates/Footer_ToolbarConfigurationRenameItemPopup.php
+++ skins/oasis/modules/templates/Footer_ToolbarConfigurationRenameItemPopup.php
@@ -1,3 +1,7 @@
 <div class="toolbar-edit-rename-item">
-	<input type="text" class="input-box" value="" />
+	<form class="WikiaForm">
+		<div class="input-group">
+			<input type="text" class="input-box" value="" />
+		</div>
+	</form>
 </div>
