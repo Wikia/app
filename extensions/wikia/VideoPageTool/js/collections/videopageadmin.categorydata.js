c118eb037da2be2e24dc43b2ff75213405f3f3e5
Kenneth Kouot <kenneth@wikia-inc.com>
2014-01-07 21:04:33 +0000

    update category collection to maintain proper lifecycle events

--- extensions/wikia/VideoPageTool/js/collections/videopageadmin.categorydata.js
+++ extensions/wikia/VideoPageTool/js/collections/videopageadmin.categorydata.js
@@ -5,12 +5,18 @@ define( 'collections.videopageadmin.categorydata', [
 		var CategoryCollection = Backbone.Collection.extend({
 				url: '/wikia.php',
 				initialize: function() {
+					_.bindAll( this, 'setCategory', 'fetch' );
 					this.controller = 'VideoPageAdminSpecial';
 					this.method = 'getCategoryData';
 					this.format = 'json';
 					this.categoryName = null;
+					this.response = null;
 				},
 				fetch: function() {
+					/*
+					 * override fetch with our decorated version,
+					 * must specify custom parse method to properly reset collection
+					 */
 					return Backbone.Collection.prototype.fetch.call( this, {
 						data: {
 							controller: this.controller,
@@ -20,7 +26,17 @@ define( 'collections.videopageadmin.categorydata', [
 						}
 					});
 				},
-				setCategory: function( name, doFetch ) {
+				parse: function( resp ) {
+					// cache the response
+					this.response = resp;
+					this.reset( this.response.data );
+				},
+				/**
+				 * @method
+				 * @description sets the category as a property on the collection then performs fetch
+				 * @param { string } name name of the category
+				 */
+				setCategory: function( name ) {
 					if ( typeof name === 'string' ) {
 						this.categoryName = name;
 						this.fetch();
--- extensions/wikia/VideoPageTool/js/views/videopageadmin.categorypreview.js
+++ extensions/wikia/VideoPageTool/js/views/videopageadmin.categorypreview.js
@@ -10,11 +10,15 @@ define( 'views.videopageadmin.categorypreview', [
 	var CategoryPreviewView = Backbone.View.extend({
 		initialize: function() {
 			_.bindAll( this, 'render' );
-			// this.collection.on( 'reset', this.render );
+			console.log( this.collection );
+			this.collection.on( 'reset', this.render );
 		},
 		events: {
 		},
 		render: function() {
+			this.collection.each(function( categoryItem ) {
+				console.log( categoryItem );
+			});
 			return this;
 		}
 	});
