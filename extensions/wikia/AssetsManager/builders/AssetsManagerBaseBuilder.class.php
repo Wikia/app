58b39b03bc21a5698e1508dd01338c675be50f20
macbre <maciej.brencz@gmail.com>
2014-01-08 10:23:52 +0100

    SASS builder: improve minification
    Use CSSMin::minify instead of Minify_CSS_Compressor::process which is up to x100 faster

--- extensions/wikia/AssetsManager/builders/AssetsManagerBaseBuilder.class.php
+++ extensions/wikia/AssetsManager/builders/AssetsManagerBaseBuilder.class.php
@@ -122,9 +122,15 @@ class AssetsManagerBaseBuilder {
 	}
 
 	private function minifyCSS($content) {
+		$minifyTimeStart = microtime(true);
+
 		wfProfileIn(__METHOD__);
-		$out = Minify_CSS_Compressor::process($content);
+		$out = CSSMin::minify($content);
 		wfProfileOut(__METHOD__);
+
+		\Wikia::log('sass-minify-WIKIA', false,
+			sprintf('%s: took %.2f ms', $this->mOid, ((microtime(true) - $minifyTimeStart) * 1000)), true /* $force */);
+
 		return $out;
 	}
 }
