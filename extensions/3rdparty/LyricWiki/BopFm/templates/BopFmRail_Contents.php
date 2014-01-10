a2ade75837ffbc0de23ed541f3efc8a42e8e0680
Sean Colombo <sean.colombo@gmail.com>
2014-01-09 22:28:52 -0500

    Small fix to get the bop.fm widget to fit nicely in our rail. Removed the "bop.fm" rail header (it didn't really add much & it wasted a bunch of above-the-fold space).

--- extensions/3rdparty/LyricWiki/BopFm/templates/BopFmRail_Contents.php
+++ extensions/3rdparty/LyricWiki/BopFm/templates/BopFmRail_Contents.php
@@ -1,13 +1,10 @@
 <section class="module">
 	<div class="bopfm-contents">
-		<h1 class="bopfm-headline">
-			bop.fm
-		</h1>
 		<?php
 			// A bunch of data to pass to the widget
 			// TODO: ONCE THIS IS WORKING, MOVE THESE TO THE CONTROLLER... THAT'S WHERE VARS SHOULD BE SET.
 			$id = "bopfm_1"; // TODO: Add a unique id? What should it be... the article-id? amg-id?
-			$width = "298"; // this gets passed to bop.fm so that they know how wide to render their widget
+			$width = "100%"; // this gets passed to bop.fm so that they know how wide to render their widget
 			//$artist = "{{ARTIST}}";
 			//$songName = "{{SONG}}";
 			
