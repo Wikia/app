1bcf96c0c279dbfd54f1ce91ce46df87de3400c0
Sean Colombo <sean.colombo@gmail.com>
2014-01-09 19:12:18 -0500

    Added a copy of bop.fm's javascript file (we're going to host it for now). Small changes related to licensing changes: mostly style and comments.

--- extensions/3rdparty/LyricWiki/BopFm/templates/BopFmRail_Contents.php
+++ extensions/3rdparty/LyricWiki/BopFm/templates/BopFmRail_Contents.php
@@ -6,13 +6,14 @@
 		<?php
 			// A bunch of data to pass to the widget
 			// TODO: ONCE THIS IS WORKING, MOVE THESE TO THE CONTROLLER... THAT'S WHERE VARS SHOULD BE SET.
-			$id = 1; // TODO: Add a unique id? What should it be... the article-id? amg-id?
+			$id = "bopfm_1"; // TODO: Add a unique id? What should it be... the article-id? amg-id?
 			$width = "298"; // this gets passed to bop.fm so that they know how wide to render their widget
 			//$artist = "{{ARTIST}}";
 			//$songName = "{{SONG}}";
 			
 			$artist="Cake";$songName="Dime"; // TODO: REMOVE
-		?><a id="<?= $id ?>" data-width="<?= $width ?>" data-bop-link href="http://www.bop.fm/embed/<?= $artist ?>/<?= $songName ?>/">
+			$pageUrl = urlencode($wg->Title->getFullURL());
+		?><a id="<?= $id ?>" data-width="<?= $width ?>" data-bop-link href="http://www.bop.fm/embed/<?= $artist ?>/<?= $songName ?>/<?= $pageUrl ?>">
 			<?= $artist ?> - <?= $songName ?>
 		</a>
 		<script async src="http://assets.bop.fm/embed.js"></script>
