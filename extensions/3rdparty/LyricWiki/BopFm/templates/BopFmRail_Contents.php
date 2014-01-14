fd761b063e4272e6d8fe2319f616bb427a67d2d9
Sean Colombo <sean.colombo@gmail.com>
2014-01-14 14:49:00 -0500

    Reworked the bop.fm widget to be page-specific, and to only show up on song pages (in the main namespace).

--- extensions/3rdparty/LyricWiki/BopFm/templates/BopFmRail_Contents.php
+++ extensions/3rdparty/LyricWiki/BopFm/templates/BopFmRail_Contents.php
@@ -1,15 +1,11 @@
 <section class="module">
 	<div class="bopfm-contents">
 		<?php
-			// A bunch of data to pass to the widget
-			// TODO: ONCE THIS IS WORKING, MOVE THESE TO THE CONTROLLER... THAT'S WHERE VARS SHOULD BE SET.
-			$id = "bopfm_1"; // TODO: Add a unique id? What should it be... the article-id? amg-id?
-			$width = "100%"; // this gets passed to bop.fm so that they know how wide to render their widget
-			//$artist = "{{ARTIST}}";
-			//$songName = "{{SONG}}";
-			
-			$artist="Cake";$songName="Dime"; // TODO: REMOVE
-			$pageUrl = urlencode($wg->Title->getFullURL());
+		
+		// Some display-specific constants.
+		$id = "bopfm_1";
+		$width = "100%"; // this gets passed to bop.fm so that they know how wide to render their widget
+		
 		?><a id="<?= $id ?>" data-width="<?= $width ?>" data-bop-link href="http://www.bop.fm/embed/<?= $artist ?>/<?= $songName ?>">
 			<?= $artist ?> - <?= $songName ?>
 		</a><?php
