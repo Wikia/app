dcd806a0e244eed41eb1ed11dd97c0b74556f0f2
Adam Robak <adamr@wikia-inc.com>
2014-01-21 10:22:19 +0100

    PLA-1069 remove suggestions from assets manager

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -1571,21 +1571,3 @@ $config['api_docs_scss'] = array(
 		'//extensions/wikia/ApiDocs/css/ApiDocs.scss',
 	)
 );
-
-$config['suggestions_js'] = array(
-	'type' => AssetsManager::TYPE_JS,
-	'assets' => array(
-		'//extensions/wikia/Suggestions/js/SuggestionsMatcher.js',
-		'//extensions/wikia/Suggestions/js/SuggestionsClient.js',
-		'//extensions/wikia/Suggestions/js/SuggestionsViewModel.js',
-		'//extensions/wikia/Suggestions/js/SuggestionsView.js',
-		'//extensions/wikia/Suggestions/js/Suggestions.js',
-	)
-);
-
-$config['suggestions_scss'] = array(
-	'type' => AssetsManager::TYPE_SCSS,
-	'assets' => array(
-		'//extensions/wikia/Suggestions/css/Suggestions.scss',
-	)
-);
