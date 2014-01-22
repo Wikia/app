cc9d47b03ba704264efc282a7557589b1e2c49e4
lizlux <liz@lizlux.net>
2014-01-21 16:40:56 -0800

    comments

--- extensions/wikia/VideoPageTool/js/admin/views/index.js
+++ extensions/wikia/VideoPageTool/js/admin/views/index.js
@@ -1,3 +1,9 @@
+/**
+ * @description This is a replacement 'router' or 'controller' file used to instantiate Backbone views.
+ * Since we aren't using Backbone's router ATM due to time limitations and this just being an exploratory
+ * exercise, we'll just inspect window.location to decide which modules to instantiate.
+ * Note that the default section is featured videos.
+ */
 require( [
 	'wikia.querystring',
 	'videopageadmin.views.featured',
