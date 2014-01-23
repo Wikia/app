1f785eadd03ad81166731bcf2f72f756523d291c
James Sutterfield <james@wikia-inc.com>
2014-01-22 17:59:26 -0800

    Added getVideosByCategory method, converted query to fluentSQL

--- extensions/wikia/VideosModule/VideosModuleController.class.php
+++ extensions/wikia/VideosModule/VideosModuleController.class.php
@@ -2,26 +2,37 @@
 
 class VideosModuleController extends WikiaController {
 
-	const VIDEO_LIMIT = 20;
+	const VIDEO_LIMIT = 40;
 
 	/**
 	 * VideosModule page
 	 *
 	 */
 	public function executeIndex() {
+
+		wfProfileIn( __METHOD__ );
+
 		$articleId = $this->request->getVal( 'articleId', 0 );
 		$helper = new VideosModuleHelper();
 		$suggestedVideos = $helper->getSuggestedVideos( $articleId );
+		$suggestedVideosCount = $suggestedVideos['returnedVideoCount'];
 
 		$this->videos = $suggestedVideos['items'];
 
-		if ( $suggestedVideos['totalSuggestedVideos'] <= self::VIDEO_LIMIT ) {
+		if ( $suggestedVideosCount <= self::VIDEO_LIMIT ) {
 			global $wgCityId;
-			$category = WikiFactory::getCategory( $wgCityId );
+			$numVideosNeeded = self::VIDEO_LIMIT - $suggestedVideosCount;
+			$categoryName = WikiFactory::getCategory( $wgCityId )->cat_name;
+			$categoryTitle = Title::newFromText( $categoryName, NS_CATEGORY );
+			$categoryVideos = $helper->getVideosByCategory( $categoryTitle, $numVideosNeeded );
+			if ( $categoryVideos ) {
+				shuffle( $categoryVideos );
+				$this->videos[] = $categoryVideos;
+			}
 		}
 
-		$this->category = $category;
-
 		$this->hello_world = "Hello world!";
+
+		wfProfileOut( __METHOD__ );
 	}
 }
--- extensions/wikia/VideosModule/VideosModuleHelper.class.php
+++ extensions/wikia/VideosModule/VideosModuleHelper.class.php
@@ -4,6 +4,9 @@ class VideosModuleHelper {
 
 	const SEARCH_START = 0;
 	const SEARCH_LIMIT = 20;
+	const THUMBNAIL_CATEGORY_WIDTH = 263;
+	const THUMBNAIL_CATEGORY_HEIGHT = 148;
+	const CACHE_TTL_CATEGORY_DATA = 3600;
 
 	/*
 	 *   Example of use:
@@ -15,17 +18,73 @@ class VideosModuleHelper {
 	 *   articleId 	 - the suggestions should be related to this article
 	 */
 	public function getSuggestedVideos( $articleId ) {
+
+		wfProfileIn( __METHOD__ );
+
 		$service = new VideoEmbedToolSearchService();
 		$service->setStart( self::SEARCH_START )->setLimit( self::SEARCH_LIMIT );
 		$response = $service->getSuggestionsForArticleId( $articleId );
-
-		print_tmp("response: \n", $response);
-
-		$result = array(
-			'totalSuggestedVideos' => count( $response['items'] ),
-			'items' => $response['items']
+		$videos = array(
+			'items' => $response['items'],
+			'returnedVideoCount' => count( $response['items'] )
 		);
 
-		return $result;
+		return $videos;
+
+		wfProfileOut( __METHOD__ );
+
+	}
+
+	/**
+	 * Get videos tagged with the category given by parameter $categoryTitle (limit = 100)
+	 * @param Title $categoryTitle
+	 * @param int $numVideos
+	 * @param array $thumbOptions
+	 * @return array An array of video data where each array element has the structure:
+	 *   [ title => 'Video Title',
+	 *     url   => 'http://url.to.video',
+	 *     thumb => '<thumbnail_html_snippet>'
+	 */
+	public function getVideosByCategory( $categoryTitle, $numVideosNeeded, $thumbOptions = array() ) {
+
+		wfProfileIn( __METHOD__ );
+
+		$dbKey = $categoryTitle->getDBkey();
+		$db = wfGetDB( DB_SLAVE );
+
+		// Why does this have to be treated as an expression?
+		$videos = ( new WikiaSQL() )
+			->SELECT( 'page_id, page_title' )
+			->FROM( 'page' )
+			->LEFT_JOIN( 'video_info' )
+			->ON( 'page_title', 'video_title' )
+			->INNER_JOIN( 'categorylinks' )
+			->ON( 'cl_from', 'page_id' )
+			->WHERE( 'cl_to' )->EQUAL_TO( $dbKey )
+			->AND_( 'page_namespace' )->EQUAL_TO( NS_FILE )
+			->ORDER_BY( ['views_7day', 'page_title'] )
+			->LIMIT( $numVideosNeeded )
+			->cache( self::CACHE_TTL_CATEGORY_DATA )
+			->run( $db, function ( $result ) use ( $thumbOptions ) {
+				$videos = array();
+				while ( $row = $result->fetchObject( $result ) ) {
+					$title = $row->page_title;
+					$file = WikiaFileHelper::getVideoFileFromTitle( $title );
+					if ( !empty( $file ) ) {
+						$thumb = $file->transform( array( 'width' => self::THUMBNAIL_CATEGORY_WIDTH, 'height' => self::THUMBNAIL_CATEGORY_HEIGHT ) );
+						$videoThumb = $thumb->toHtml( $thumbOptions );
+						$videos[] = array(
+							'title' => $title->getText(),
+							'url'   => $title->getFullURL(),
+							'thumb' => $videoThumb,
+						);
+					}
+				}
+				return $videos;
+			});
+
+		wfProfileOut( __METHOD__ );
+
+		return $videos;
 	}
 }
