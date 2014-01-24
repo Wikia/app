5c7651f296848c9a480df50f0056284a48b81051
Christian Williams <christian@wikia-inc.com>
2014-01-24 07:25:23 +0000

    Adding WAM controller

--- extensions/wikia/EditorSurvey/EditorSurvey.setup.php
+++ extensions/wikia/EditorSurvey/EditorSurvey.setup.php
@@ -4,6 +4,7 @@ $dir = dirname(__FILE__) . '/';
 
 // classes
 $wgAutoloadClasses['EditorSurveyController'] =  $dir . 'EditorSurveyController.class.php';
+$wgAutoloadClasses['WAMApiController'] = $dir . '../WAM/controllers/api/WAMApiController.class.php';
 
 // i18n
 $wgExtensionMessagesFiles['EditorSurvey'] = $dir . 'EditorSurvey.i18n.php';
