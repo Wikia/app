fe2573a22db0e146f7f5a911dc6ec97767392069
zlayatapka <sergey@wikia-inc.com>
2014-01-24 14:41:33 +0100

    CE-397: DPL Needs Upgrade

--- extensions/DynamicPageList/DynamicPageList.php
+++ extensions/DynamicPageList/DynamicPageList.php
@@ -1,24 +1,25 @@
 <?php
-/*
+
+/* 
    This is the main entry point for DPL installations
    --------------------------------------------------
-
+   
 	In your LocalSettings.php you write:
-
+   
         require_once( "$IP/extensions/DynamicPageList/DynamicPageList.php" );
-
+		
 	DPL has many features. If you do not want to enable all of them
 	you can define the "Level of Functional Richness" you want to offer to your users.
 	Make the following call in your LocalSettings.php after the require_once() statement:
-
+	
         ExtDynamicPageList::setFunctionalRichness(  <n>  );
-
-	where <n> is a number between 0 and 4.
-
-	If your wiki has been using Extension:Intersection you must UNINSTALL that
+		
+	where <n> is a number between 0 and 4.  
+		
+	If your wiki has been using Extension:Intersection you must UNINSTALL that 
 	extension before you can use DynamicPageList (DPL).
 
-	DPL is downward compatible with Extension:Intersection. It registers
+	DPL is downward compatible with Extension:Intersection. It registers 
 	the tag <DynamicPageList> and behaves exactly like ExtensionIntersection
 	(without any additional functionality).
 
@@ -30,9 +31,9 @@
 	DPL in parallel to that extension before you replace Extension:Intersection by DPL.
 	In this case you must require_once("DynamicPageListMigration.php") instead of the current file
 	in your LocalSettings.php.
-
+	  
 	Some functions of DPL are quite useful but if abused (by error or bad intention) they may put severe load
-	on your server / database. For wikis up to 10.000 pages this is normally not a problem,
+	on your server / database. For wikis up to 10.000 pages this is normally not a problem, 
 	but with larger wikis some care is advisable.
 
 	By default the RichnessLevel is set to 4 (= activate whole set of functions).
@@ -47,57 +48,37 @@
 	           revision level
 	-  level=4 adds a few additional features which are useful for private websites (like batch updates)
                but should not be made available on huge public websites.
-
+				
 */
-
-if( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This is not a valid entry point to MediaWiki.' );
-}
 
 // we register the tag <dpl> and function #dpl
 // we also register the tag <DynamicPageList> because DPL is downward compatible with Extension:Intersection
 // This means that your LocalSettings.php MUST NO LONGER include Extension:Intersection;
 
-$wgHooks['ParserFirstCallInit'][] = 'ExtDynamicPageList::setupDPL';
+if( !defined( 'MEDIAWIKI' ) ) {
+    die( 'This is not a valid entry point to MediaWiki.' );
+}
+
+$wgExtensionFunctions[]        = array( 'ExtDynamicPageList', 'setupDPL' );
+$wgHooks['LanguageGetMagic'][] = 'ExtDynamicPageList__languageGetMagic';
 
 $wgExtensionMessagesFiles['DynamicPageList'] =  dirname( __FILE__ ) . '/DynamicPageList.i18n.php';
-$wgExtensionMessagesFiles['DynamicPageListMagic'] =  dirname( __FILE__ ) . '/DynamicPageList.i18n.magic.php';
 
-$DPLVersion = '1.9.0';
+$DPLVersion = '2.02';
 
 $wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'DynamicPageList',
-	'author' => '[http://de.wikipedia.org/wiki/Benutzer:Algorithmix Gero Scholz]',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:DynamicPageList_(third-party)',
-	'descriptionmsg' => 'dpl-desc',
-	'version' => $DPLVersion
-);
-
-// Wikia change start
-$wgHooks['PageRenderingHash'][] = 'dynamicPageList_onPageRenderingHash';
-
-function dynamicPageList_onPageRenderingHash( &$hash ) {
-	global $wgRequest;
-
-	$offset = $wgRequest->getVal( 'DPL_offset' );
-	$count = $wgRequest->getVal( 'DPL_count' );
-
-	if ( $offset !== null ) {
-		$hash .= "!DPL_offset={$offset}";
-	}
-
-	if ( $count !== null ) {
-		$hash .= "!DPL_count={$count}";
-	}
-
-	return true;
-}
-// Wikia change end
+	'path' 				=> __FILE__,
+	'name' 				=> 'DynamicPageList (third party)',
+	'author' 			=>  '[http://de.wikipedia.org/wiki/Benutzer:Algorithmix Gero Scholz]',
+	'url' 				=> 'https://www.mediawiki.org/wiki/Extension:DynamicPageList_(third-party)',
+	'descriptionmsg' 	=> 'dpl-desc',
+  	'version' 			=> $DPLVersion
+  );
 
 require_once( 'DPLSetup.php' );
 
 ExtDynamicPageList::$DPLVersion = $DPLVersion;
 
 // use full functionality by default
-ExtDynamicPageList::setFunctionalRichness( 4 );
+ExtDynamicPageList::setFunctionalRichness(4);
+
