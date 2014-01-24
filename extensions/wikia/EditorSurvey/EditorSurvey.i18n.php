71895e0db7c1b233763614f8baa1267ca1bb42b9
Christian Williams <christian@wikia-inc.com>
2014-01-24 18:45:55 +0000

    remove quotation

--- extensions/wikia/EditorSurvey/EditorSurvey.i18n.php
+++ extensions/wikia/EditorSurvey/EditorSurvey.i18n.php
@@ -4,6 +4,6 @@ $messages = array();
 
 $messages['en'] = array(
 	'editorsurvey-heading' => 'Take our survey!',
-	'editorsurvey-success' => 'Thanks for your edit!" Could you please take this short one-minute survey to tell us about your editing experience?',
+	'editorsurvey-success' => 'Thanks for your edit! Could you please take this short one-minute survey to tell us about your editing experience?',
 	'editorsurvey-fail' => 'We notice you didn\'t save your edit. Could you please take this short one-minute survey to tell us why?',
 );
