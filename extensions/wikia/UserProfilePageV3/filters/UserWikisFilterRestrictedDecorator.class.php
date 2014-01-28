c744d29c56f4528a8792f4943ee4299cb6a26184
nAndy <nandy@wikia-inc.com>
2014-01-28 15:50:10 +0000

    refactored UserWikisFilterRestrictedDecorator and added a unit test for its getFiltered() method

--- extensions/wikia/UserProfilePageV3/filters/UserWikisFilterRestrictedDecorator.class.php
+++ extensions/wikia/UserProfilePageV3/filters/UserWikisFilterRestrictedDecorator.class.php
@@ -1,25 +1,33 @@
 <?php
 class UserWikisFilterRestrictedDecorator extends UserWikisFilterDecorator {
-	private $privateWikis;
 	private $filter;
 
 	public function __construct( UserWikisFilter $filter ) {
 		$this->filter = $filter;
-		$this->privateWikis = UserProfilePageHelper::getRestrictedWikisIds();
 	}
 
 	public function getFiltered() {
 		$filtered = $this->filter->getFiltered();
+		$privateWikis = $this->getRestrictedWikis();
 
 		foreach( $filtered as $key => $wikiData ) {
 			$wikiId = (int) $wikiData['id'];
 
-			if( in_array( $wikiId, $this->privateWikis ) ) {
+			if( in_array( $wikiId, $privateWikis ) ) {
 				unset( $filtered[$key] );
 			}
 		}
 
 		return $filtered;
+	}
+
+	/**
+	 * @desc Helper method which can be mocked in unit tests
+	 *
+	 * @return Array
+	 */
+	public function getRestrictedWikis() {
+		return UserProfilePageHelper::getRestrictedWikisIds();
 	}
 
 }
