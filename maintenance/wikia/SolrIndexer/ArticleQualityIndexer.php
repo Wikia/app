7d6afb8530cc87c4716288ec23e112713a1a0921
Adam Robak <adamr@wikia-inc.com>
2014-01-28 14:11:06 +0100

    Indexer using rest on

--- maintenance/wikia/SolrIndexer/ArticleQualityIndexer.php
+++ maintenance/wikia/SolrIndexer/ArticleQualityIndexer.php
@@ -4,19 +4,53 @@ require_once( dirname( __FILE__ ) . '/../../Maintenance.php' );
 
 class ArticleQualityIndexer extends Maintenance {
 
-	const ARRAY_SIZE = 100;
+	const ARRAY_SIZE = 1000;
 
 	private $wikiId;
+	private $domain;
 	private $client;
 	private $curlClient;
-	private $service;
 	private $CONFIG = [
-			'adapteroptions' => [
-				'host' => 'search-s11',
-				'port' => 8983,
-				'path' => '/solr/',
-				'core' => 'main'
-			]
+		'adapter' => 'Solarium_Client_Adapter_Curl',
+		'adapteroptions' => [
+			'host' => 'search-s11',
+			'port' => 8983,
+			'path' => '/solr/',
+			'core' => 'main'
+		]
+	];
+
+	private $ips = [
+		'10.12.68.119',
+		'10.12.68.113',
+		'10.12.66.117',
+		'10.12.62.121',
+		'10.12.60.109',
+		'10.12.68.115',
+		'10.12.66.120',
+		'10.12.68.112',
+		'10.12.60.111',
+		'10.12.60.101',
+		'10.12.64.118',
+		'10.12.68.114',
+		'10.12.62.103',
+		'10.12.66.101',
+		'10.12.62.120',
+		'10.12.66.124',
+		'10.12.64.117',
+		'10.12.66.114',
+		'10.12.60.100',
+		'10.12.64.119',
+		'10.12.64.121',
+		'10.12.66.116',
+		'10.12.62.122',
+		'10.12.66.122',
+		'10.12.64.101',
+		'10.12.64.123',
+		'10.12.60.103',
+		'10.12.64.116',
+		'10.12.62.101',
+		'10.12.62.123'
 	];
 	/**
 	 * Do the actual work. All child classes will need to implement this
@@ -26,12 +60,18 @@ class ArticleQualityIndexer extends Maintenance {
 			die( 1 );
 		}
 		$this->wikiId = $_ENV['SERVER_ID'];
+		$this->domain = WikiFactory::getDomains( $this->wikiId )[0];
+
+		if ( empty( $this->domain ) ) {
+			die;
+		}
 
 		$ids = $this->getIds();
 		$batches = array_chunk( $ids, self::ARRAY_SIZE );
+		$b = count( $batches );
 		foreach( $batches as $key => $batch ) {
-			echo "\nPrepare data for ids: ";
-			$data = $this->prepareData( $batch );
+			echo "\nPrepare data for batch: $key out of $b\n";
+			$data = $this->prepareData( $batch, $key + 1, $b );
 			echo "\nData prepared!\n";
 			$this->setInSolr( $data );
 			echo "Data stored!";
@@ -39,18 +79,37 @@ class ArticleQualityIndexer extends Maintenance {
 	}
 
 	//[{ "id" : "831_72005", "article_quality_i" : {"set":25}}]
-	protected function prepareData( $ids ) {
+	protected function prepareData( $ids, $batch = 0, $total = 0 ) {
 		$result = [];
-		$qs = $this->getArticleService();
-		foreach( $ids as $id ) {
-			echo $id." ";
-			$qs->setArticleById( $id );
-			$result[] = [
-				'id' => $this->wikiId.'_'.$id,
-				'article_quality_i' => [ 'set' => $qs->getArticleQuality() ]
-			];
+		foreach( $ids as $key => $id ) {
+			$start = microtime( true );
+			$res = $this->getArticleQuality( $id );
+			echo '[' . $this->wikiId . ']' . $batch . " out of " . $total . " current: " . $key . " " . ( microtime( true ) - $start );
+			if ( isset( $res->contents[0]->article_quality_i ) ) {
+				$result[] = $res->contents[0];
+			}
+			echo "\n";
 		}
 		return $result;
+	}
+
+	protected function getArticleQuality( $id ) {
+		$url = $this->getIP() . '/wikia.php?controller=WikiaSearchIndexer&method=get&service=ArticleQuality&ids=' . $id;
+              echo $this->domain . " :" . $url." ";
+		$ch = curl_init( $url );
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Host: '.$this->domain
+		));
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		$res = curl_exec( $ch );
+              echo $res."\n";
+		curl_close( $ch );
+		return json_decode( $res );
+	}
+
+	protected function getIP() {
+		return $this->ips[rand(0,count($this->ips)-1)];
 	}
 
 	protected function setInSolr( $data ) {
@@ -65,7 +124,7 @@ class ArticleQualityIndexer extends Maintenance {
 		$client = $this->getSolrConnection();
 		$select = $client->createSelect();
 		$select->setFields( [ 'pageid' ] );
-		$select->setQuery( '+(wid:'.$this->wikiId.') AND +(ns:0)' );
+		$select->setQuery( '+(wid:'.$this->wikiId.') AND +(ns:0) AND -(article_quality_i:[0 TO *])' );
 
 		$select->setRows( 1000000 );
 
@@ -77,15 +136,6 @@ class ArticleQualityIndexer extends Maintenance {
 		return $ids;
 	}
 
-	/**
-	 * @return ArticleQualityV1Service
-	 */
-	protected function getArticleService() {
-		if ( !isset( $this->service ) ) {
-			$this->service = new ArticleQualityService();
-		}
-		return $this->service;
-	}
 
 	protected function getCurlConnection() {
 		if ( !isset( $this->curlClient ) ) {
@@ -111,12 +161,13 @@ class ArticleQualityIndexer extends Maintenance {
 	}
 
 	protected function getMasterSolrUrl() {
-		global $wgSolrMaster;
-		if ( isset( $wgSolrMaster ) ) {
-			$url = 'http://' . $wgSolrMaster . ':8983/solr/update';
-			return $url;
-		}
-		die(1);
+		return 'http://search-s11:8983/solr/update';
+//              global $wgSolrMaster;
+//              if ( isset( $wgSolrMaster ) ) {
+//                      $url = 'http://' . $wgSolrMaster . ':8983/solr/update';
+//                      return $url;
+//              }
+//              die(1);
 	}
 }
 
