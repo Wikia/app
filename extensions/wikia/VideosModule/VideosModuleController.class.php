7d2a9c0cd513023d952fa10db61de802de046ff5
James Sutterfield <james@wikia-inc.com>
2014-01-30 11:54:33 -0800

    VID-1082: Return helpful error messages, don\'t exepction.

--- extensions/wikia/VideosModule/VideosModuleController.class.php
+++ extensions/wikia/VideosModule/VideosModuleController.class.php
@@ -4,6 +4,10 @@ class VideosModuleController extends WikiaController {
 
 	/**
 	 * VideosModule page
+	 * @requestParam int articleId
+	 * @responseParam string $result [ok/error]
+	 * @responseParam string $msg - result message
+	 * @responseParam array $videos - list of videos
 	 */
 	public function executeIndex() {
 
@@ -12,12 +16,16 @@ class VideosModuleController extends WikiaController {
 		$articleId = $this->request->getVal( 'articleId', 0 );
 
 		if ( !$articleId ) {
+			$this->result = 'error';
+			$this->msg = wfMessage( 'videosmodule-error-no-articleId' )->plain();
 			wfProfileOut(__METHOD__);
-			throw new Exception( "Please include the articleID." );
+			return;
 		}
 
 		$helper = new VideosModuleHelper();
 		$this->videos = $helper->getVideos( $articleId  );
+		$this->result = "ok";
+		$this->msg = '';
 
 		wfProfileOut( __METHOD__ );
 	}
