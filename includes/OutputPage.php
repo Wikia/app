6e17200ae69aa01e676c4331b6f3c5ff2962e3d9
macbre <maciej.brencz@gmail.com>
2014-01-31 17:09:03 +0100

    OutputPage: add setCacheOK mmethod

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -694,6 +694,18 @@ class OutputPage extends ContextSource {
 		# Not modified
 		# Give a 304 response code and disable body output
 		wfDebug( __METHOD__ . ": NOT MODIFIED, $info\n", false );
+		$this->setCacheOK();
+		return true;
+	}
+
+	/**
+	 * Sets the response of this request to 304 not modified
+	 * Adds required headers and disables body output
+	 *
+	 * @author macbre / BAC-1521
+	 * @see https://gerrit.wikimedia.org/r/#/c/60440/7/includes/OutputPage.php
+	 */
+	private function setCacheOK() {
 		ini_set( 'zlib.output_compression', 0 );
 		$this->getRequest()->response()->header( "HTTP/1.1 304 Not Modified" );
 		$this->sendCacheControl();
