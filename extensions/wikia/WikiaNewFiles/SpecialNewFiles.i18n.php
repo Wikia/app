60241277afb47f4eb46694b7eab26cf31396d364
saipetch <saipetch@wikia-inc.com>
2014-01-30 17:58:52 -0800

    Video (vid-1294): NewFiles - add qqq messages

--- extensions/wikia/WikiaNewFiles/SpecialNewFiles.i18n.php
+++ extensions/wikia/WikiaNewFiles/SpecialNewFiles.i18n.php
@@ -8,8 +8,15 @@
 $messages = array();
 
 $messages['en'] = array(
-	'wikianewfiles-desc'    => 'Extends a [[Special:NewFiles|special page]] to override some of the header formatting',
+	'wikianewfiles-desc'     => 'Extends a [[Special:NewFiles|special page]] to override some of the header formatting',
 	'wikianewfiles-uploadby' => 'by $1',
 	'wikianewfiles-postedin' => 'Posted&nbsp;in',
 	'wikianewfiles-more'     => 'more...',
+);
+
+$messages['qqq'] = array(
+	'wikianewfiles-desc'     => 'This is the description of the extension',
+	'wikianewfiles-uploadby' => 'Text displayed below a video to indicate which user uploaded the video',
+	'wikianewfiles-postedin' => 'Text displayed below a video to indicate which articles the video is posted in.',
+	'wikianewfiles-more'     => 'Text displayed when there are more articles the video is posted in.',
 );
