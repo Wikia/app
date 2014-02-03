6b00e0a49fea5357efde94c27b9ccf17fb0f26a5
hakubo <bukaj.kelo+github@gmail.com>
2014-02-03 18:17:56 +0100

    MOB-755: finish toc integreation tests

--- extensions/wikia/WikiaMobile/js/spec/toc.spec.js
+++ extensions/wikia/WikiaMobile/js/spec/toc.spec.js
@@ -9,49 +9,19 @@ describe( "toc module", function () {
 		},
 		window = {
 			document: {
-				body: getBody( "<div id=wkTOC class=side-menu><header>TOC</header><img id=wkTOCHandle src=''></div>" )
+				body: getBody( "<div id=wkTOC class=side-menu><header>TOCHEADER</header><img id=wkTOCHandle src=''></div>" ),
+				getElementById: function(id){
+					return window.document.body.querySelector( '#' + id );
+				}
 			},
 			Features: {
 				positionfixed: true,
 				overflow: true
 			}
 		},
-		jQueryObject = {
-			on: function (event, func) {
-				if ( event == 'click' && typeof func == 'function' ) {
-					var event = {
-						stopPropagation: function(){}
-					};
-
-
-
-					func(event);
-				}
-
-				return jQueryObject;
-			},
-			hide: function () {
-
-			},
-			append: function( html ){
-				window.document.body.innerHTML += html;
-
-				return jQueryObject;
-			},
-			find: function(){
-				return jQueryObject;
-			},
-			hasClass: function(){},
-			removeClass: function(){},
-			addClass: function(){}
-		},
-		jQuery = function ( selector ) {
-			jQueryObject.selector = selector;
-			return jQueryObject;
-		},
 		mustache = {
 			render: function(){
-				return 'TEMPLATE';
+				return '<ol class="toc-list level"><li class="has-children first-children"><a href="#Biography">Biography<span class="chevron right"></span></a><ol class="toc-list level1"><li class="has-children"><a href="#Season_One">Season One</a><ol class="toc-list level2"><li><a href="#Pilot">Pilot</a></li></ol></ol></ol>';
 			}
 		},
 		wikiaToc = {
@@ -83,31 +53,91 @@ describe( "toc module", function () {
 			event: function(){}
 		};
 
-	jQuery.event = {
-		trigger: function(){}
-	};
-
 	it( 'should init itself', function () {
 
-		spyOn( jQueryObject, 'on' ).andCallThrough();
+		spyOn( jQuery.fn, 'on' ).andCallThrough();
 
 		modules['require,sections,wikia.window,jquery,wikia.mustache,wikia.toc,track']( sections, window, jQuery, mustache, wikiaToc, track );
 
-		expect( jQueryObject.on.calls.length ).toEqual( 5 );
-		expect( jQueryObject.on.calls[0].args[0] ).toEqual( 'curtain:hidden' );
-		expect( jQueryObject.on.calls[0].args[1] ).toEqual( jasmine.any( Function ) );
+		expect( jQuery.fn.on.calls.length ).toEqual( 2 );
+		expect( jQuery.fn.on.calls[0].args[0] ).toEqual( 'curtain:hidden' );
+		expect( jQuery.fn.on.calls[0].args[1] ).toEqual( jasmine.any( Function ) );
 
-		expect( jQueryObject.on.calls[1].args[0] ).toEqual( 'click' );
-		expect( jQueryObject.on.calls[1].args[1] ).toEqual( jasmine.any( Function ) );
+		expect( jQuery.fn.on.calls[1].args[0] ).toEqual( 'click' );
+		expect( jQuery.fn.on.calls[1].args[1] ).toEqual( jasmine.any( Function ) );
 	} );
 
-	it( 'should add itself to DOM on open', function () {
-		var tocHandle = window.document.body.querySelector( '#wkTOCHandle' );
+	it( 'should add itself to DOM and add active class on open', function () {
+		var tocHandle = window.document.getElementById( 'wkTOCHandle' );
 
-		expect(window.document.body.innerHTML.indexOf('TEMPLATE') ).toEqual(-1)
+		expect(window.document.body.innerHTML.indexOf('<ol class="toc-list level">') ).toEqual(-1);
 
-		fireEvent( 'click', tocHandle );
+		$( tocHandle ).trigger( 'click' );
 
-		expect(window.document.body.innerHTML.indexOf('TEMPLATE') ).not.toEqual(-1)
+		expect( window.document.getElementById( 'wkTOC' ).className ).toContain('active');
+		expect( window.document.body.innerHTML.indexOf('<ol class="toc-list level">') ).not.toEqual(-1);
+	} );
+
+	it( 'should remove active class on close', function(){
+		var tocHandle = window.document.getElementById( 'wkTOCHandle' ),
+			toc = window.document.getElementById( 'wkTOC' );
+
+		expect( toc.className ).toContain( 'active' );
+
+		$( tocHandle ).trigger( 'click' );
+
+		expect( toc.className ).not.toContain( 'active' );
+	} );
+
+	it( 'should track events', function(){
+		var tocHandle = window.document.getElementById( 'wkTOCHandle' ),
+			toc = window.document.getElementById( 'wkTOC' );
+
+		spyOn( track, 'event' );
+
+		$( tocHandle ).trigger( 'click' );
+		$( tocHandle ).trigger( 'click' );
+
+		expect( track.event ).toHaveBeenCalledWith( 'newtoc', undefined, { label : 'open' }  );
+		expect( track.event ).toHaveBeenCalledWith(  'newtoc', undefined, { label : 'close' } );
+	} );
+
+	it( 'should mark current active section', function(){
+		var tocHandle = window.document.getElementById( 'wkTOCHandle' ),
+			$toc = $( window.document.getElementById( 'wkTOC' ) );
+
+		$( tocHandle ).trigger( 'click' );
+
+		expect( $toc.find('.current' ).length ).toEqual( 0 );
+
+		$( window.document ).trigger( 'section:changed', {
+			id: 'Biography'
+		} );
+
+		expect( $toc.find('.current' ).length ).toEqual( 1 );
+		expect( $toc.find('.current' ).eq( 0 ).html()  ).toContain( 'Biography' );
+
+		$( window.document ).trigger( 'section:changed', {
+			id: 'Season_One'
+		} );
+
+		expect( $toc.find('.current' ).length ).toEqual( 2 );
+		expect( $toc.find('.current' ).eq( 0 ).html()  ).toContain( 'Biography' );
+		expect( $toc.find('.current' ).eq( 1 ).html()  ).toEqual( 'Season One' );
+
+		$( window.document ).trigger( 'section:changed', {
+			id: 'Pilot'
+		} );
+
+		expect( $toc.find('.current' ).length ).toEqual( 2 );
+		expect( $toc.find('.current' ).eq( 0 ).html()  ).toContain( 'Biography' );
+		expect( $toc.find('.current' ).eq( 1 ).html()  ).toEqual( 'Pilot' );
+
+		$( window.document ).trigger( 'section:changed', {
+			id: 'Biography'
+		} );
+
+		expect( $toc.find('.current' ).length ).toEqual( 1 );
+		expect( $toc.find('.current' ).eq( 0 ).html()  ).toContain( 'Biography' );
 	} );
 } );
