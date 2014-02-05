85f22e36be36c80f6dd8711b85d241dd26a7a04e
lizlux <liz@lizlux.net>
2014-02-04 17:56:52 -0800

    VID-1156 styles for title thumbnails

--- extensions/wikia/VideosModule/scripts/templates.mustache.js
+++ extensions/wikia/VideosModule/scripts/templates.mustache.js
@@ -1,4 +1,4 @@
 define( 'videosmodule.templates.mustache', [], function() { 'use strict'; return {
-    "bottomModule" : '<section class="videos-module"><h1>{{title}}</h1><ul class="small-block-grid-3">{{{thumbnails}}}</ul></section>',
+    "bottomModule" : '<section class="videos-module"><h1>{{title}}</h1><ul class="small-block-grid-3 large-block-grid-4">{{{thumbnails}}}</ul></section>',
     "done": "true"
   }; });
