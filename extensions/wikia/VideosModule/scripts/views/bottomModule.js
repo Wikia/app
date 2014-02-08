329b2ff3bc3b3cd27e87cf15d4222b46ae16e373
Kenneth Kouot <kenneth@wikia-inc.com>
2014-02-08 00:56:06 +0000

    VID-1268 Set up AB test case scenarios (sans tracking)

--- extensions/wikia/VideosModule/scripts/views/bottomModule.js
+++ extensions/wikia/VideosModule/scripts/views/bottomModule.js
@@ -2,9 +2,80 @@ define( 'videosmodule.views.bottommodule', [
 	'sloth',
 	'thumbnails.views.titlethumbnail',
 	'wikia.mustache',
-    'videosmodule.templates.mustache'
+	'videosmodule.templates.mustache'
 ], function( sloth, TitleThumbnailView, Mustache, templates ) {
 	'use strict';
+
+	// AB Test Code
+	var testParams,
+			testGroup,
+			groups,
+			groupParams;
+
+	testParams = window.Wikia.AbTest;
+	testParams.getGroup = function() {
+		return 'GROUP_E';
+	};
+	testGroup = testParams ? testParams.getGroup( 'VIDEOS_MODULE_BOTTOM' ) : null;
+
+	/*
+	 * AB Test Info:
+	 * Position 1: Below Read More
+	 * Position 2: Above Read More
+	 */
+
+	groups = {
+		// Article, Above Read More, 2 Rows
+		'GROUP_A': {
+			verticalOnly: false,
+			position: 2,
+			rows: 2
+		},
+		// Vertical, Above Read More, 2 Rows
+		'GROUP_B': {
+			verticalOnly: true,
+			position: 2,
+			rows: 2
+		},
+		// Article, Below Read More, 2 Rows
+		'GROUP_C': {
+			verticalOnly: false,
+			position: 1,
+			rows: 2
+		},
+		// Article, Above Read More, 2 Rows
+		'GROUP_D': {
+			verticalOnly: false,
+			position: 2,
+			rows: 2
+		},
+		// Article, Above Read More, 1 Row
+		'GROUP_E': {
+			verticalOnly: false,
+			position: 2,
+			rows: 1
+		},
+		// Article, Above Read More, 1 Row
+		'GROUP_F': {
+			verticalOnly: true,
+			position: 2,
+			rows: 1
+		},
+		// Article, Below Read More, 1 Row
+		'GROUP_G': {
+			verticalOnly: false,
+			position: 1,
+			rows: 1
+		},
+		// Article, Above Read More, 1 Row
+		'GROUP_H': {
+			verticalOnly: false,
+			position: 2,
+			rows: 1
+		},
+	};
+
+	groupParams = groups[ testGroup || 'GROUP_G' ];
 
 	function VideoModule( options ) {
 		this.el = options.el;
@@ -19,7 +90,11 @@ define( 'videosmodule.views.bottommodule', [
 	}
 
 	VideoModule.prototype.init = function() {
-		this.model.fetch();
+		if ( !groupParams ) {
+			// Add tracking for GROUP_I, Control Group
+			return false;
+		}
+		this.model.fetch( groupParams.verticalOnly );
 		// Sloth is a lazy loading service that waits till an element is visisble to load more content
 		sloth( {
 			on: this.el,
@@ -30,14 +105,17 @@ define( 'videosmodule.views.bottommodule', [
 
 	VideoModule.prototype.render = function() {
 		$.when( this.model.fetch() )
-			.done( $.proxy( this.renderWithData, this ) );
+		.done( $.proxy( this.renderWithData, this ) );
 	};
 
 	VideoModule.prototype.renderWithData = function() {
 		var i, out,
-			videos = this.model.data.videos,
-			len = videos.length,
-			thumbHtml = '';
+				videos = this.model.data.videos,
+				len = videos.length,
+				thumbHtml = '';
+
+		// AB test set rows shown
+		videos = videos.slice( 0, groupParams.rows > 1 ? 8 : 4 );
 
 		for ( i = 0; i < len; i++ ) {
 			thumbHtml += new TitleThumbnailView( videos[i], { el: 'li' } ).render().el.outerHTML;
@@ -45,7 +123,12 @@ define( 'videosmodule.views.bottommodule', [
 
 		// TODO: hard coded title
 		out = Mustache.render( templates.bottomModule, { title: 'Must Watch Videos', thumbnails: thumbHtml } );
-		this.$el.append( out );
+		if ( groupParams.position === 1 ) {
+			this.$el.append( out );
+		} else {
+			$( out ).insertBefore( this.$el.find( '.RelatedPagesModule' ) );
+		}
+		this.$el.find( '.videos-module' ).addClass( groupParams.rows > 1 ? 'rows-2' : 'rows-1' );
 	};
 
 	return VideoModule;
