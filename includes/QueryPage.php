995c88701fbad5810863d985df6ebe00534d4e74
Onei <mdowdell244@gmail.com>
2014-02-09 11:07:04 +0000

    Fix indentation on Special:LongPages and Special:ShortPages for high offset lists

--- includes/QueryPage.php
+++ includes/QueryPage.php
@@ -603,8 +603,16 @@ abstract class QueryPage extends SpecialPage {
 	 * @param $offset
 	 * @return string
 	 */
-	function openList( $offset ) {
-		return "\n<ol start='" . ( $offset + 1 ) . "' class='special'>\n";
+	function openList( $offset, $num ) {
+		// begin Wikia change
+		// @author Cqm
+		// VOLDEV-30
+		// handle ordered lists above 1000
+		$indent = strlen ( strval( $offset + 1 + $num ) );
+
+		// add value of $indent to be used as a hook for css
+		return "\n<ol start='" . ( $offset + 1 ) . "' class='special' data-specialindent='" . $indent . "'>\n";
+		// end Wikia change
 	}
 
 	/**
