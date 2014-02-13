a276c044126e25e3ec9282c013967c175b844f4b
zlayatapka <sergey@wikia-inc.com>
2014-02-13 19:12:20 +0100

    ADEN-908 RH+ Null Implementation
    Conflicts:
    
    	extensions/wikia/AdEngine/js/AdProviderGptMobile.js
    	extensions/wikia/WikiaMobile/js/ad_slots2.js

--- extensions/wikia/AdEngine/js/AdProviderGptMobile.js
+++ extensions/wikia/AdEngine/js/AdProviderGptMobile.js
@@ -24,7 +24,7 @@ define(
 				hop({method: 'hop'}, 'Null');
 			}
 
-			wikiaGpt.pushAd(slotname, success, hopToNull, 'mobile');
+			wikiaGpt.pushAd(slotname, success, (window.wgEnableRHonMobile ? hop : hopToNull), 'mobile');
 			wikiaGpt.flushAds();
 		}
 
