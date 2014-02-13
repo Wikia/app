16fc0515d1e3139e01844fc50f0c8b29f959a02b
saipetch <saipetch@wikia-inc.com>
2014-02-12 17:33:57 -0800

    Video (VID-1294): NewFiles - use wfMessage() function

--- extensions/wikia/WikiaNewFiles/SpecialNewFiles.i18n.php
+++ extensions/wikia/WikiaNewFiles/SpecialNewFiles.i18n.php
@@ -8,15 +8,15 @@
 $messages = array();
 
 $messages['en'] = array(
-	'wikianewfiles-desc'     => 'Extends a [[Special:NewFiles|special page]] to override some of the header formatting',
+	'wikianewfiles-desc' => 'Extends a [[Special:NewFiles|special page]] to override some of the header formatting',
 	'wikianewfiles-uploadby' => 'by $1',
-	'wikianewfiles-postedin' => 'Posted&nbsp;in',
-	'wikianewfiles-more'     => 'more...',
+	'wikianewfiles-postedin' => 'Posted in',
+	'wikianewfiles-more' => 'more...',
 );
 
 $messages['qqq'] = array(
-	'wikianewfiles-desc'     => 'This is the description of the extension',
-	'wikianewfiles-uploadby' => 'Text displayed below a video to indicate which user uploaded the video',
-	'wikianewfiles-postedin' => 'Text displayed below a video to indicate which articles the video is posted in.',
-	'wikianewfiles-more'     => 'Text displayed when there are more articles the video is posted in.',
+	'wikianewfiles-desc' => 'This is the description of the extension',
+	'wikianewfiles-uploadby' => 'Text displayed below a file to indicate which user uploaded the file',
+	'wikianewfiles-postedin' => 'Text displayed below a file to indicate which articles the file is posted in.',
+	'wikianewfiles-more' => 'Text displayed when there are more articles the file is posted in.',
 );
