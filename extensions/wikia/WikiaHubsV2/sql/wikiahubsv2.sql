fba80d611f386e259e4a13b942fd5a5f3959f202
Lukasz Konieczny <lukaszk@wikia-inc.com>
2014-02-13 11:38:43 +0000

    CON-548: Added city_id field to wikia_hub_modules and primary_key

--- extensions/wikia/WikiaHubsV2/sql/wikiahubsv2.sql
+++ extensions/wikia/WikiaHubsV2/sql/wikiahubsv2.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `wikia_hub_modules` (
-  `city_id` int NOT NULL,
+  `city_id` int NOT NULL DEFAULT 0,
   `lang_code` varchar(8) NOT NULL,
   `vertical_id` tinyint NOT NULL, /* in sync with comscore id (2-gaming, 3-entertainment, 9-lifestyle) */
   `hub_date` date  NOT NULL,
@@ -8,6 +8,5 @@ CREATE TABLE `wikia_hub_modules` (
   `module_status` tinyint NOT NULL DEFAULT 1, /* in sync with CT statuses: 1-saved, 2-published */
   `last_editor_id` int(11),
   `last_edit_timestamp` timestamp,
-  PRIMARY KEY ( `lang_code`, `vertical_id`, `hub_date`, `module_id` )
+  PRIMARY KEY ( `lang_code`, `vertical_id`, `hub_date`, `city_id`, `module_id` )
 ) ENGINE=InnoDB;
-CREATE INDEX `wikia_hub_index` ON `wikia_hub_modules` (`city_id`, `hub_date`, `module_id`);
