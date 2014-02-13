ce5d66cc6baf909bb775354744fec9f6938669ff
hakubo <bukaj.kelo+github@gmail.com>
2014-02-13 15:54:29 +0100

    MOB-832: Base AB test

--- extensions/wikia/WikiaMobile/js/relatedPages.js
+++ extensions/wikia/WikiaMobile/js/relatedPages.js
@@ -1,4 +1,4 @@
-require( [ 'wikia.window', 'wikia.nirvana', 'jquery', 'wikia.thumbnailer', 'lazyload', 'sloth' ], function( window, nirvana, $, thumbnailer, lazyload, sloth ) {
+require( [ 'wikia.window', 'wikia.nirvana', 'jquery', 'wikia.thumbnailer', 'lazyload', 'sloth', 'JSMessages' ], function( window, nirvana, $, thumbnailer, lazyload, sloth, msg ) {
 	var sectionsList = $.makeArray(
 			window.document.querySelectorAll('#mw-content-text h2[id]:not(:first-of-type):not(:last-of-type)' )
 		).filter( function( section ){
@@ -7,30 +7,37 @@ require( [ 'wikia.window', 'wikia.nirvana', 'jquery', 'wikia.thumbnailer', 'lazy
 		} ),
 		sectionsLength = sectionsList.length,
 		articleId = window.wgArticleId,
-		header = 'Related Article',
-		template = function(item){
+		testGroup = 'A',// && 'B' && 'C' && 'D',
+		largeStyle = (['A', 'B'].indexOf( testGroup ) > -1),
+		width = largeStyle ? 280 : 136,
+		height = largeStyle ? 158 : 76,
+		template = function( item ){
+			var header = (['A', 'C'].indexOf( testGroup ) > -1) ? msg( 'wikiamobile-related-article' ) : msg( 'wikiamobile-people-also-read' );
+
 			return '<a href="'+ item.url +'" class="related-article"><div class="header">' +
 				header +
 				'</div><img class="lazy link" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="' +
-				(item.imgUrl ? thumbnailer.getThumbURL(item.imgUrl, 'image', 280, 158) : '!!!') +
-				'" width="280px" height="158px"><div class="title">' +
+				(item.imgUrl ? thumbnailer.getThumbURL(item.imgUrl, 'image', width, height) : '!!!') +
+				'" width="' + width+'px" height="' + height + 'px"><div class="title">' +
 				item.title +
 				'</div></div>'
 		};
 
-	nirvana.getJson(
-		'RelatedPagesApi',
-		'getList',
-		{
-			ids: [ articleId ],
-			limit: sectionsLength
-		}
+	$.when(
+		nirvana.getJson(
+			'RelatedPagesApi',
+			'getList',
+			{
+				ids: [ articleId ],
+				limit: sectionsLength
+			}
+		),
+		msg.get( 'RelatedPagesInContent' )
 	).done( function( data ){
-		var items = data.items[articleId],
+		var items = data[0].items[articleId],
 			l = Math.min( sectionsLength, items.length ),
 			i = 0;
-			console.log(items);
-			console.log(l);
+
 			for(; i < l; i++) {
 				sectionsList[i].insertAdjacentHTML('beforebegin', template(items[i]));
 			}
