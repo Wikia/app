004e45e06d4fdb3379a536b7e20c152c0bbcfa7a
hakubo <bukaj.kelo+github@gmail.com>
2014-02-14 15:03:03 +0100

    MOB-833: trending articles refactor

--- extensions/wikia/WikiaMobile/WikiaMobileBodyService.class.php
+++ extensions/wikia/WikiaMobile/WikiaMobileBodyService.class.php
@@ -66,48 +66,12 @@ class WikiaMobileBodyService extends WikiaService {
 			)
 		);
 
-		//Render Trending Articles "on the fly"
-		$trendingArticlesTmpl = $this->sendSelfRequest( 'trendingArticles' )->toString();
+		//Render Trending Articles
+		$trendingArticles = $this->sendRequest( 'WikiaMobileTrendingArticlesService', 'index' )->toString();
 
-		$this->response->setVal( 'trendingArticles', $trendingArticlesTmpl );
+		$this->response->setVal( 'trendingArticles', $trendingArticles );
 		$this->response->setVal( 'afterBodyContent', $afterBodyHtml );
 		$this->response->setVal( 'afterContentHookText', $afterContentHookText );
 
-	}
-
-	public function trendingArticles(){
-		$this->response->setTemplateEngine( WikiaResponse::TEMPLATE_ENGINE_MUSTACHE );
-
-		$maxTrendingArticles = 6; //max number of trending articles to show
-		$imgHeight = 72;
-		$imgWidth = 136;
-
-		//fetch Trending Articles
-		try {
-			$trendingArticlesFromApi = F::app()->sendRequest( 'ArticlesApi', 'getTop' )->getData();
-		}
-		catch ( Exception $e ) {
-			//ToDo -> get rid of mock and make it a blank array
-			$trendingArticlesFromApi = new myJsonMock();
-		}
-
-		//load data from response to template
-		$current = 0;
-		$trendingArticles = [];
-		while ( $current < $maxTrendingArticles && !empty( $trendingArticlesFromApi->items[$current] ) ){
-			$currentItem = $trendingArticlesFromApi->items[$current];
-			$trendingArticles[$current] = [
-				'pageUrl' => $currentItem->url,
-				'pageTitle' => $currentItem->title,
-				'imgUrl' => $currentItem->thumbnail,
-				'imgWidth' => $imgWidth,
-				'imgHeight' => $imgHeight
-			];
-			$current++;
-		}
-
-		$this->response->setVal( 'trendingArticles', $trendingArticles );
-		$this->response->setVal( 'blankImg', $this->wg->blankImgUrl );
-		$this->response->setVal( 'trendingArticlesHeading', wfMessage( 'wikiamobile-trending-articles-heading' ) );
 	}
 }
