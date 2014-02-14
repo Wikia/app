027af4df72da73c3a712b310586325502c2d562a
bkoval <brt.kowal@gmail.com>
2014-02-14 12:30:50 +0100

    Parse TrendingArticles from API in php

--- extensions/wikia/WikiaMobile/WikiaMobileBodyService.class.php
+++ extensions/wikia/WikiaMobile/WikiaMobileBodyService.class.php
@@ -5,6 +5,33 @@
  * @author Jakub Olek <bukaj.kelo(at)gmail.com>
  * @authore Federico "Lox" Lucignano <federico(at)wikia-inc.com>
  */
+//quick mock of json to test api ToDo -> remove it before merging!!!!!!!
+class json{
+	public $id;
+	public $title;
+	public $url;
+	public $ns;
+	public $thumbnail;
+	public $originalDimensions;
+	public function __construct(){
+		$this->id = 234;
+		$this->title = 'Rachel Berry';
+		$this->url = '/Rachel_Berry';
+		$this->ns = 0;
+		$this->thumbnail = 'http://static3.wikia.nocookie.net/__cb20140208202628/glee/images/thumb/c/cc/Tumblr_n0p1zdllkm1qe476yo1_500.jpg/200px-0%2C501%2C0%2C500-Tumblr_n0p1zdllkm1qe476yo1_500.jpg';
+		$this->originalDimensions = Array('width' => 136, 'height' => 76);
+	}
+}
+
+class myJsonMock{
+	public $items;
+	public function __construct(){
+		$items1 = new json;
+		$items2 = new json;
+		$this->items = array($items1, $items2);
+	}
+}
+
 class WikiaMobileBodyService extends WikiaService {
 	public function index() {
 		$bodyContent = $this->request->getVal( 'bodyText', '' );
@@ -39,7 +66,47 @@ class WikiaMobileBodyService extends WikiaService {
 			)
 		);
 
+		//Render Trending Articles "on the fly"
+		$trendingArticlesTmpl = $this->sendSelfRequest( 'trendingArticles' )->toString();
+
+		$this->response->setVal( 'trendingArticles', $trendingArticlesTmpl );
 		$this->response->setVal( 'afterBodyContent', $afterBodyHtml );
 		$this->response->setVal( 'afterContentHookText', $afterContentHookText );
+
+	}
+
+	public function trendingArticles(){
+		$this->response->setTemplateEngine( WikiaResponse::TEMPLATE_ENGINE_MUSTACHE );
+
+		$maxTrendingArticles = 6; //max number of trending articles to show
+		$imgHeight = 72;
+		$imgWidth = 136;
+
+		//fetch Trending Articles
+		try{
+			$trendingArticlesFromApi = F::app()->sendRequest( 'ArticlesApi', 'getTop' )->getData();
+		}
+		catch( Exception $e ){
+			$trendingArticlesFromApi = new myJsonMock();
+		}
+
+		//load data from response to template
+		$current = 0;
+		$trendingArticles = [];
+		while( $current < $maxTrendingArticles && !empty( $trendingArticlesFromApi->items[$current] ) ){
+			$currentItem = $trendingArticlesFromApi->items[$current];
+			$trendingArticles[$current] = [
+				'pageUrl' => $currentItem->url,
+				'pageTitle' => $currentItem->title,
+				'imgUrl' => $currentItem->thumbnail,
+				'imgWidth' => $imgWidth,
+				'imgHeight' => $imgHeight
+			];
+			$current++;
+		}
+
+		$this->response->setVal( 'trendingArticles', $trendingArticles );
+		$this->response->setVal( 'blankImg', $this->wg->blankImgUrl );
+		$this->response->setVal( 'trendingArticlesHeading', wfMessage( 'wikiamobile-trending-articles-heading' ) );
 	}
 }
