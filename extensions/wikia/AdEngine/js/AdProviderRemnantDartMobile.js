b4c58d56889794839d2f4eace388a03bec6098cb
zlayatapka <sergey@wikia-inc.com>
2014-02-21 12:28:44 +0100

    RH+ working implementation
    Conflicts:
    
    	extensions/wikia/WikiaMobile/js/ad_slots2.js

--- extensions/wikia/AdEngine/js/AdProviderRemnantDartMobile.js
+++ extensions/wikia/AdEngine/js/AdProviderRemnantDartMobile.js
@@ -1,19 +1,48 @@
 /*global define*/
-define('ext.wikia.adengine.provider.remnantdartmobile', ['wikia.log', 'ext.wikia.adengine.slottweaker'], function (log, slotTweaker) {
+define('ext.wikia.adengine.provider.remnantdartmobile', ['wikia.log', 'ext.wikia.adengine.slottweaker', 'ext.wikia.adengine.gpthelper'], function (log, slotTweaker, wikiaGpt) {
 	'use strict';
 
-	var logGroup = 'AdProviderDartRemnantMobile';
+	var logGroup = 'AdProviderDartRemnantMobile',
 
-	function canHandleSlot() {
-		return true;
+		slotMap = {
+			MOBILE_TOP_LEADERBOARD: {size: '320x50'},
+			MOBILE_IN_CONTENT: {size: '300x250'},
+			MOBILE_PREFOOTER: {size: '300x250'}
+		};
+
+	wikiaGpt.init(slotMap, 'rh_mobile');
+
+	function canHandleSlot(slotname) {
+		return !!slotMap[slotname];
 	}
 
 	function fillInSlot(slotname, success, hop) {
 		log(['fillInSlot', slotname], 5, logGroup);
 
-		// just hop to null
-		hop({method: 'hop'}, 'Null');
+		wikiaGpt.pushAd(slotname,
+			function () { // Success
+				var slot = document.getElementById( slotname ),
+					$iframe = $( slot ).find( 'iframe' ).contents();
 
+				if (
+					$iframe.find( 'body *:not(script)' ).length === 0 ||
+						$iframe.find( 'body img' ).width() <= 1
+					) {
+					log( 'Slot seems to be empty: ' + slotname, 5, logGroup );
+					slotTweaker.hide(slotname);
+					slotTweaker.hideSelfServeUrl(slotname);
+				}
+			},
+			function () { // Hop
+
+				log(slotname + ' was not filled by DART', 'info', logGroup);
+
+				slotTweaker.hide(slotname);
+				slotTweaker.hideSelfServeUrl(slotname);
+
+				success();
+			}, true);
+		wikiaGpt.flushAds();
 	}
 
 	return {
