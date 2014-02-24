0c593e756cadf425425dc39c3ba5a2e70da2b172
Krzysztof 'suchy' Suchanek <suchy@wikia-inc.com>
2014-02-24 14:03:42 +0100

    PLA-1192 fix

--- includes/wikia/api/swagger/ArticlesApiController.swagger.php
+++ includes/wikia/api/swagger/ArticlesApiController.swagger.php
@@ -697,8 +697,6 @@ use Swagger\Annotations as SWG;
  * 			@SWG\ErrorResponses(
  * 				@SWG\ErrorResponse( code="400", reason="Invalid parameter or category" ),
  * 				@SWG\ErrorResponse( code="404", reason="Results not found" )
- * 			),
- * 			@SWG\Parameters(
  * 			)
  * 		)
  * 	)
@@ -716,8 +714,6 @@ use Swagger\Annotations as SWG;
  * 			@SWG\ErrorResponses(
  * 				@SWG\ErrorResponse( code="400", reason="Invalid parameter or category" ),
  * 				@SWG\ErrorResponse( code="404", reason="Results not found" )
- * 			),
- * 			@SWG\Parameters(
  * 			)
  * 		)
  * 	)
