5691d92bfe7a5aad12bd3ed2e651aafa69dbba7b
nAndy <nandy@wikia-inc.com>
2014-02-25 17:48:45 +0000

    moved option parameters to const, added more placeholders

--- maintenance/wikia/lyrics/LyricsWikiCrawler.php
+++ maintenance/wikia/lyrics/LyricsWikiCrawler.php
@@ -3,17 +3,48 @@
 require_once( dirname(__FILE__) . '/../../Maintenance.php' );
 
 class LyricsWikiCrawler extends Maintenance {
-	const DB_NAME = '';
-	const DB_TABLE = '';
+	const OPTION_ARTICLE_ID = 'articleId';
+	const OPTION_ARTICLE_ALL = 'all';
+
+	private $articleId = 0;
 
 	public function __construct() {
 		parent::__construct();
-		$this->addOption( 'articleId', 'Article ID which we will get data from' );
+		$this->addOption( self::OPTION_ARTICLE_ID, 'Article ID which we will get data from' );
+		$this->addOption( self::OPTION_ARTICLE_ALL, 'If passed it pulls all articles on lyrics.wikia.com; otherwise it pulls edits from yesterday' );
 		$this->mDescription = "Crawls through LyricWiki, pulls data from its articles and puts it to our database";
 	}
 
 	public function execute() {
-		$this->output( 'Done.' );
+		if( $this->hasOption( self::OPTION_ARTICLE_ALL ) ) {
+			$this->doScrapeAllArticles();
+		} else if( ( $articleId = intval( $this->getOption( self::OPTION_ARTICLE_ID, 0 ) ) ) && $articleId > 0 ) {
+			$this->setArticleId( $articleId );
+			$this->doScrapeArticle();
+		} else {
+			$this->doScrapeArticlesFromYesterday();
+		}
+	}
+
+	public function doScrapeAllArticles() {
+		$this->output( 'Scraping all articles...' );
+	}
+
+	public function doScrapeArticle() {
+		$this->output( 'Scraping article #' . $this->getArticleId() );
+	}
+
+	public function doScrapeArticlesFromYesterday() {
+		$yesterday = date( "Y-m-d", strtotime( '-1 day' ) );
+		$this->output( 'Scraping articles from ' . $yesterday );
+	}
+
+	public function setArticleId( $articleId ) {
+		$this->articleId = $articleId;
+	}
+
+	public function getArticleId() {
+		return $this->articleId;
 	}
 
 }
