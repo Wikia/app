f70b834d7f3ff38231e0613f8ae46b0f8c1932e1
Krzysztof 'suchy' Suchanek <suchy@wikia-inc.com>
2014-02-25 09:57:47 +0100

    PLA-1192 Service

--- includes/wikia/api/services/PrivilegedApiService.php
+++ includes/wikia/api/services/PrivilegedApiService.php
@@ -7,13 +7,30 @@ class PrivilegedApiService {
 		]
 	];
 
-	public function isPrivileged( $controller, $action )
-	{
-		//if(isset($this->privileged[]))
+	protected function isPrivileged( $controller, $action ){
+
+		if(isset($this->privileged[$controller])){
+			$actions = $this->privileged[$controller];
+			if(is_array($actions))
+			{
+				return isset($actions[$action]) && $actions[$action] === true;
+			}else{
+				return $actions === true;
+			}
+		}
+		return false;
 	}
 
-	public function checkPrivileged()
-	{
+	public function canUse($controller, $action){
+		global $wgPrivileged;
 
+	}
+
+	public function checkUse($controller, $action )
+	{
+		if(! $this->canUse($controller, $action))
+		{
+			throw new NotFoundApiException();
+		}
 	}
 }
