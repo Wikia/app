4f2fb4672daa2a92738a7e202973175878f11fba
Artur Dwornik <artur.dwornik@wikia-inc.com>
2014-02-26 16:06:44 +0100

    PLA-1240 sanitize query string

--- extensions/wikia/VideoEmbedTool/VideoEmbedToolSearchService.class.php
+++ extensions/wikia/VideoEmbedTool/VideoEmbedToolSearchService.class.php
@@ -111,6 +111,7 @@ class VideoEmbedToolSearchService
 	public function getSuggestedVideosByArticleId( $articleId ) {
 		$this->setSuggestionQueryByArticleId( $articleId );
 		$query = $this->getSuggestionQuery();
+		$query =  (new \Wikia\Search\Query\Select($query))->getSanitizedQuery();
 		$expectedFields = $this->getExpectedFields();
 		$config = $this->getConfig()->setWikiId( Wikia\Search\QueryService\Select\Dismax\Video::VIDEO_WIKI_ID )
 		                            ->setQuery( $query )
