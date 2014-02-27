64b214cb2279a1b416f27012d7084229fd3dc1c7
bkoval <brt.kowal@gmail.com>
2014-02-27 14:42:39 +0100

    Adding isDefined method and completing jsdoc for sections methods

--- extensions/wikia/WikiaMobile/js/sections.js
+++ extensions/wikia/WikiaMobile/js/sections.js
@@ -11,7 +11,7 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 	var d = window.document,
 		$wkPage = $( '#wkPage' ),
 		h2s = $( 'h2[id]', $wkPage ).toArray(),
-		sections = getHeaders(),
+		sections = [],
 		lastSection,
 		escapeRegExp = /[()\.\+]/g,
 		offset = 5;
@@ -26,9 +26,11 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 			'h2[id]:not( [id=""] ), h3[id]:not( [id=""] ), h4[id]:not( [id=""] )' ) );
 	}
 
+	sections = getHeaders();
+
 	/**
 	 * @desc Function that lets you scroll viewport to a given section
-	 * @param header - a string representation of a header
+	 * @param {String} header
 	 * @returns {undefined|true} - status code if scroll actually happened
 	 */
 	function scrollTo ( header ) {
@@ -46,7 +48,7 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 
 	/**
 	 * @desc Finds and returns a current section
-	 * @returns jQuery object - a current section
+	 * @returns {Object} - a current section
 	 */
 	function current () {
 		var top = window.scrollY,
@@ -66,9 +68,9 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 
 	/**
 	 * @desc Check if intro is longer than 700px
-	 * @param section - number of section or header element to measure
-	 * @param minHeight - height of intro to compare against
-	 * @returns boolean
+	 * @param {Number} section - number of section or header element to measure
+	 * @param {Number} minHeight - height of intro to compare against
+	 * @returns {Boolean}
 	 */
 	function isSectionLongerThan ( section, minHeight ) {
 		var currentSection,
@@ -100,9 +102,9 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 	}
 
 	/**
-	 * @desc If possible, get section under which the ad can be placed (700px down)
-	 * @param distFromTop - an int value representing given height in document
-	 * @returns jQuery object or null
+	 * @desc If possible, get section at given distance from top
+	 * @param {Number} distFromTop - an int value representing given height in document
+	 * @returns {Object|null}
 	 */
 	function getElementAt ( distFromTop ) {
 		var currentElement = $( '#mw-content-text' ).children().first(),
@@ -116,8 +118,8 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 		return currentElement;
 	}
 
-		/**
-	 * @desc Function that fires at most every 200ms while scrolling\
+	/**
+	 * @desc Function that fires at most every 200ms while scrolling
 	 * @triggers section:changed with a current section refernece and its id
 	 */
 	function onScroll () {
@@ -139,14 +141,35 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 		}, 200 );
 	}
 
-	function getId( section ){
+	/**
+	 * @desc Gets an index of the given H2 headline
+	 * @param {Object} section
+	 * @returns {Number}
+	 */
+	function getId ( section ) {
 		return h2s.indexOf( section );
 	}
 
-	function getNext( section ) {
+	/**
+	 * @desc Gets next H2 in an article
+	 * @param {Object|Number} section - headline or number representing it's index
+	 * @returns {Object}
+	 */
+	function getNext ( section ) {
 		section = typeof section === 'number' ? section : getId( section );
 
 		return h2s[section + 1];
+	}
+
+	/**
+	 * @desc Checks if the given article Section (only H2-leveled sections) exists
+	 * @param {Number} number of the article's section getting checked
+	 * @returns {Boolean}
+	 */
+	function isDefined ( section ) {
+		//first h2 has index 0 in the nodeList
+		var headline = h2s[section-1];
+		return !!headline && headline.parentElement === d.getElementById( 'mw-content-text' );
 	}
 
 	window.addEventListener( 'scroll', onScroll );
@@ -162,6 +185,7 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 		getElementAt: getElementAt,
 		getId: getId,
 		getNext: getNext,
+		isDefined: isDefined,
 		scrollTo: scrollTo,
 		current: current
 	};
