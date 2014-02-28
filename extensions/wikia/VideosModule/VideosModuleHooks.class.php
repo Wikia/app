633e8df75b4d532dd060c600fe6db9553912be8e
Garth Webb <garth@wikia-inc.com>
2014-02-27 16:18:07 -0800

    VID-1426 Withhold the VideosModule on a few special pages

--- extensions/wikia/VideosModule/VideosModuleHooks.class.php
+++ extensions/wikia/VideosModule/VideosModuleHooks.class.php
@@ -85,7 +85,13 @@ class VideosModuleHooks {
 	static public function canShowVideosModule() {
 		$wg = F::app()->wg;
 		$showableNameSpaces = array_merge( $wg->ContentNamespaces, [ NS_FILE ] );
-		if ( in_array( $wg->Title->getNamespace(), $showableNameSpaces ) ) {
+
+		if ( in_array( $wg->Title->getNamespace(), $showableNameSpaces ) &&
+			 in_array( $wg->request->getVal( 'action' ), [ 'view', null ] ) &&
+			 $wg->request->getVal( 'diff' ) === null &&
+			 !WikiaPageType::isCorporatePage() &&
+			 $wg->Title->exists()
+		) {
 			return true;
 		}
 		return false;
