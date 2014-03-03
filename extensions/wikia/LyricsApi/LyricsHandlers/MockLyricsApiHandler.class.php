202e2e94f1fa5225b6c3660b0427c06e230fbcea
aquilax <evgeniy@wikia-inc.com>
2014-03-03 15:42:40 +0000

    Added songs

--- extensions/wikia/LyricsApi/LyricsHandlers/MockLyricsApiHandler.class.php
+++ extensions/wikia/LyricsApi/LyricsHandlers/MockLyricsApiHandler.class.php
@@ -8,6 +8,7 @@
 
 class MockLyricsApiHandler extends AbstractLyricsApiHandler {
 	const API_ENTRY_POINT = 'wikia.php';
+	const API_CONTROLLER_NAME = 'LyricsApi';
 
 	function buildUrl( $params ) {
 		global $wgServer;
@@ -19,6 +20,10 @@ class MockLyricsApiHandler extends AbstractLyricsApiHandler {
 				'?',
 				http_build_query( $params )
 			]);
+	}
+
+	function getImage( $image ) {
+		return 'http://placekitten.com/' . rand(128, 1024) . '/' . rand(128, 1024) . '/';
 	}
 
 	public function getArtist( $artist ) {
@@ -50,50 +55,67 @@ class MockLyricsApiHandler extends AbstractLyricsApiHandler {
 
 	public function getAlbum( $artistName, $albumName ) {
 		$album = new StdClass();
-
 		$album->name = $albumName;
-		$album->image = $albumName . '.jpg';
+		$album->image = $this->getImage( $albumName . '.jpg' );
 		$album->year = '2000';
 		$album->length = '6:66';
 		$album->genres = ['Hard', 'Heavy'];
+		$album->itunes = 'ALBUMITUNES';
 
 		$artist = new StdClass();
 		$artist->name = $artistName;
-		$artist->image = $artistName . '.jpg';
+		$artist->image = $this->getImage( $artistName . '.jpg' );
 		$artist->url = $this->buildUrl([
-			'controller' => 'LyricsApiController',
+			'controller' => self::API_CONTROLLER_NAME,
 			'method' => 'getArtist',
 			LyricsApiController::PARAM_ARTIST => $artistName
 		]);
-		$artist->itunes = 'ARTIST';
 		$album->artist = $artist;
 
 		$album->songs = [];
-		$song = new StdClass();
-		$song->name =  $albumName . ' song 1';
-		$song->url = $this->buildUrl([
-			'controller' => 'LyricsApiController',
-			'method' => 'getSong',
-			LyricsApiController::PARAM_ARTIST => $artistName,
-			LyricsApiController::PARAM_ALBUM => $albumName,
-			LyricsApiController::PARAM_SONG => $song->name
-		]);
-		$album->songs[] = $song;
-
-		$song = new StdClass();
-		$song->name =  $albumName . ' song 2';
-		$song->url = $this->buildUrl([
-			'controller' => 'LyricsApiController',
-			'method' => 'getSong',
-			'artist' => $artistName,
-			'album' => $albumName,
-			'song' => $song->name
-		]);
-		$album->songs[] = $song;
+		for($i = 0; $i < 13; $i++) {
+			$song = new StdClass();
+			$song->name =  sprintf('%s song %d', $albumName, $i);
+			$song->url = $this->buildUrl([
+				'controller' => self::API_CONTROLLER_NAME,
+				'method' => 'getSong',
+				LyricsApiController::PARAM_ARTIST => $artistName,
+				LyricsApiController::PARAM_ALBUM => $albumName,
+				LyricsApiController::PARAM_SONG => $song->name
+			]);
+			$album->songs[] = $song;
+		}
 		return $album;
 	}
 
-	public function getSong( $artist, $album, $song ) {}
+	public function getSong( $artistName, $albumName, $songName ) {
+		$song = new StdClass();
+		$song->name = $songName;
+		$song->lyrics = str_repeat('Lorem ipsum opusum'.PHP_EOL, 10);
+		$song->itunes = 'SONGITUNES';
+
+		$artist = new StdClass();
+		$artist->name = $artistName;
+		$artist->image = $this->getImage( $artistName . '.jpg' );
+		$artist->url = $this->buildUrl([
+			'controller' => self::API_CONTROLLER_NAME,
+			'method' => 'getArtist',
+			LyricsApiController::PARAM_ARTIST => $artistName
+		]);
+		$song->artist = $artist;
+
+		$album = new StdClass();
+		$album->name = $albumName;
+		$album->image = $this->getImage( $albumName . '.jpg' );
+		$album->url = $this->buildUrl([
+			'controller' => self::API_CONTROLLER_NAME,
+			'method' => 'getAlbum',
+			LyricsApiController::PARAM_ARTIST => $artistName,
+			LyricsApiController::PARAM_ALBUM => $albumName
+		]);
+		$song->album = $album;
+		return $song;
+	}
 	public function searchArtist( $query ) {}
 	public function searchSong( $query ) {}
 	public function searchLyrics( $query ) {}
