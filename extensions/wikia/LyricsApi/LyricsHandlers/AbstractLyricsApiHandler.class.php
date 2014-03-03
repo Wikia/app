60ffc9b9906ca4ba4da0c0e0434892c1ec0c75c9
aquilax <evgeniy@wikia-inc.com>
2014-03-03 13:19:07 +0000

    Started Lyrics API extension

---
+++ extensions/wikia/LyricsApi/LyricsHandlers/AbstractLyricsApiHandler.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aquilax
+ * Date: 3/3/14
+ * Time: 2:08 PM
+ */
+
+
+abstract class AbstractLyricsApiHandler {
+
+	abstract public function getArtist( $artist );
+	abstract public function getAlbum( $artist, $album );
+	abstract public function getSong( $artist, $album, $song );
+	abstract public function searchArtist( $query );
+	abstract public function searchSong( $query );
+	abstract public function searchLyrics( $query );
+
+} 
