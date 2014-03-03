132423d05a8dfe19ce71f29177be888de7bc3e1b
Krzysztof 'suchy' Suchanek <suchy@wikia-inc.com>
2014-03-03 15:16:16 +0100

    PLA-1249 evaluating article type

--- includes/wikia/services/ArticleTypeService.class.php
+++ includes/wikia/services/ArticleTypeService.class.php
@@ -1,21 +1,7 @@
 <?php
 
 
-class ArticleTypeService {
-	const KNOWLEDGE_DB = "dataknowledge";
-
-	/**
-	 * @var DatabaseBase
-	 */
-	private $knowledgeDbConnection = null;
-
-	function __construct( DatabaseBase $knowledgeDbConnection = null ) {
-		if ( $knowledgeDbConnection == null ) {
-			$knowledgeDbConnection = wfGetDB( DB_SLAVE, array(), self::KNOWLEDGE_DB);
-		}
-
-		$this->knowledgeDbConnection = $knowledgeDbConnection;
-	}
+class ArticleTypeService  /*extends AbstractService*/ {
 
 	/**
 	 * Returns article type for given (wikiId, pageId) pair
@@ -24,21 +10,23 @@ class ArticleTypeService {
 	 * @return string|null
 	 */
 	public function getArticleType( $wikiId, $pageId ) {
-		return (new WikiaSQL())->SELECT("a.type")
-			->FROM("article_types")->AS_("a")
-				->LEFT_OUTER_JOIN("wiki_types")->AS_("w")->ON("a.wiki_id = w.wiki_id")
-			->WHERE("(w.type IS NULL OR w.type = 'canonical')")
-				->AND_("a.wiki_id")->EQUAL_TO( $wikiId )
-				->AND_("a.page_id")->EQUAL_TO( $pageId )
-			->cache( 60 * 60 )
-			->run( $this->knowledgeDbConnection, function( ResultWrapper $result ) {
-				$row = $result->fetchObject( $result );
+		//curl -X POST -H "Content-Type: application/json" "http://dev-arturd:8080/holmes/classifications/" --data "{\"title\": \"John Price\", \"wikiText\": \"asdfasfd\"}"
 
-				if ( $row && isset( $row->type ) ) {
-					return $row->type;
-				}
 
-				return null;
-			});
+		$art = Article::newFromID(50);
+		var_dump($art->getTitle()->getText());
+		var_dump($art->getPage()->getRawText());
+		$ch= curl_init();
+		curl_setopt($ch,CURLOPT_URL, 'http://dev-arturd:8080/holmes/classifications/');
+		cur_setopt($ch,CURLOPT_CONNECTTIMEOUT, 1);
+		curl_setopt($ch,CURLOPT_POST, 1);
+		curl_setopt($ch,CURLOPT_POSTFIELDS, "{\"title\": \"John Price\", \"wikiText\": \"asdfasfd\"}");
+		curl_setopt($ch,CURLOPT_HTTPHEADER,[ 'Content-Type: application/json' ] );
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($ch);
+		var_dump($result);
+//close connection
+		curl_close($ch);
 	}
 }
