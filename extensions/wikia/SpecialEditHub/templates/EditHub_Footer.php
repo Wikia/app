c03e5d839ec1e7d68623f02f68661b2a57f39c42
Damian Jozwiak <damian@wikia-inc.com>
2014-03-03 17:58:14 +0100

    CON-707 | S:EditHub fixed publish button position

--- extensions/wikia/SpecialEditHub/templates/EditHub_Footer.php
+++ extensions/wikia/SpecialEditHub/templates/EditHub_Footer.php
@@ -1,5 +1,5 @@
 <div class="EditHubFooterGradient"></div>
 
-<footer class="EditHubboxFooter">
+<footer class="EditHubFooter">
 	<input id="EditHubPublish" class="big" type="submit" value="<?=wfMessage('edit-hub-footer-button-publish')->escaped()?>" <?= $allModulesSaved ?> />
 </footer>
