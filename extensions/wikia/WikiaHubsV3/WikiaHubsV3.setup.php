3f6b0a49246a8acb28a61ab24345b7b75db569f1
Damian Jozwiak <damian@wikia-inc.com>
2014-03-07 16:05:43 +0100

    CON-552 | HubsV3 moved social icons to page header [WIP]

--- extensions/wikia/WikiaHubsV3/WikiaHubsV3.setup.php
+++ extensions/wikia/WikiaHubsV3/WikiaHubsV3.setup.php
@@ -37,8 +37,6 @@ $wgAutoloadClasses['WikiaHubsParserHelper'] =  $dir . 'WikiaHubsParserHelper.cla
 
 // i18n mapping
 $wgExtensionMessagesFiles['WikiaHubsV3'] = $dir . 'WikiaHubsV3.i18n.php';
-// TODO: We need this to support both hub versions. Remove after hubs v3 release @WikiaHubsV2Remove tag
-$wgExtensionMessagesFiles['WikiaHubsV2'] = $dir . '../WikiaHubsV2/WikiaHubsV2.i18n.php';
 
 // hooks
 $wgHooks['ArticleFromTitle'][] = 'WikiaHubsV3Hooks::onArticleFromTitle';
--- skins/oasis/modules/templates/PageHeader_Hubs.php
+++ skins/oasis/modules/templates/PageHeader_Hubs.php
@@ -1,13 +1,30 @@
 <header id="WikiaPageHeader" class="WikiaPageHeader">
-    <h1><?= !empty($displaytitle) ? htmlspecialchars($title) : '' ?></h1>
+	<?php if ($showSearchBox) : ?>
+		<section id="WikiaSearchHeader" class="WikiaSearchHeader">
+			<?=  F::app()->renderView('Search', 'Index', array('searchFormId' => 'WikiaSearch')) ?>
+		</section>
+	<?php endif ?>
+
 	<?php if (!is_null($tallyMsg)) : ?>
 		<div class="tally">
 			<?= $tallyMsg ?>
         </div>
 	<?php endif ?>
+
+	<div class="social-links">
+		<? $fbMsg = wfMessage('wikiahubs-v3-social-facebook-link-' . $canonicalVerticalName)->inContentLanguage()->text(); ?>
+		<? if (!empty($fbMsg)): ?>
+			<a href="<?= $fbMsg ?>"><img src="<?= $wg->BlankImgUrl; ?>" class="facebook" /></a>
+		<? endif ?>
+		<? $twMsg =  wfMessage('wikiahubs-v3-social-twitter-link-' . $canonicalVerticalName)->inContentLanguage()->text()?>
+		<? if (!empty($twMsg)): ?>
+			<a href="<?= $twMsg ?>"><img src="<?= $wg->BlankImgUrl; ?>" class="twitter" /></a>
+		<? endif ?>
+		<? $gplusMsg =  wfMessage('wikiahubs-v3-social-googleplus-link-' . $canonicalVerticalName)->inContentLanguage()->text()?>
+		<? if (!empty($gplusMsg)): ?>
+			<a href="<?= $gplusMsg ?>"><img src="<?= $wg->BlankImgUrl; ?>" class="gplus" /></a>
+		<? endif ?>
+	</div>
+
+	<h1><?= !empty($displaytitle) ? htmlspecialchars($title) : '' ?></h1>
 </header>
-<?php if ($showSearchBox) : ?>
-<section id="WikiaSearchHeader" class="WikiaSearchHeader">
-	<?=  F::app()->renderView('Search', 'Index', array('searchFormId' => 'WikiaSearch')) ?>
-</section>
-<?php endif ?>
