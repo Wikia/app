de9d8326249fcf6f362f848b70489520c5d41982
zlayatapka <sergey@wikia-inc.com>
2014-03-10 17:35:21 +0100

    Revert "Proper Liftium replace"
    This reverts commit 584603041ddbff62a39a05747451c1ccc1f88adc.

--- extensions/wikia/AdEngine/js/AdProviderRemnantDart.js
+++ extensions/wikia/AdEngine/js/AdProviderRemnantDart.js
@@ -1,74 +1,94 @@
 /*exported AdProviderRemnantDart*/
-var AdProviderRemnantDart = function ( adTracker, log, slotTweaker, wikiaGpt ) {
-    'use strict';
+var AdProviderRemnantDart = function (adTracker, log, slotTweaker, wikiaGpt) {
+	'use strict';
 
-    var logGroup = 'AdProviderRemnantDart',
+	var logGroup = 'AdProviderRemnantDart',
 
-        slotMap = {
-            'EXIT_STITIAL_BOXAD_1': {'size': '300x250'},
-            'HOME_TOP_LEADERBOARD': {'size': '728x90', 'tile': 2, 'loc': 'top', 'dcopt': 'ist'},
-            'HOME_TOP_RIGHT_BOXAD': {'size': '300x250', 'tile': 1, 'loc': 'top'},
-            'INCONTENT_BOXAD_1': {'size': '300x250'},
-            'INVISIBLE_1': {'size': '0x0', 'useGw': true},
-            'INVISIBLE_2': {'size': '0x0', 'useGw': true},
-            'LEFT_SKYSCRAPER_2': {'size': '160x600', 'tile': 3, 'loc': 'middle'},
-            'LEFT_SKYSCRAPER_3': {'size': '160x600', 'tile': 6, 'loc': 'footer'},
-            'TEST_TOP_RIGHT_BOXAD': {'size': '300x250', 'tile': 1, 'loc': 'top'},
-            'TEST_HOME_TOP_RIGHT_BOXAD': {'size': '300x250', 'tile': 1, 'loc': 'top'},
-            'TOP_BUTTON_WIDE': {'size': '292x90', 'tile': 3, 'loc': 'top'},
+		slotMap = {
+			'EXIT_STITIAL_BOXAD_1': {'size': '300x250'},
+			'HOME_TOP_LEADERBOARD': {'size': '728x90', 'tile': 2, 'loc': 'top', 'dcopt': 'ist'},
+			'HOME_TOP_RIGHT_BOXAD': {'size': '300x250', 'tile': 1, 'loc': 'top'},
+			'INCONTENT_BOXAD_1': {'size': '300x250'},
+			'INVISIBLE_1': {'size': '0x0', 'useGw': true},
+			'INVISIBLE_2': {'size': '0x0', 'useGw': true},
+			'LEFT_SKYSCRAPER_2': {'size': '160x600', 'tile': 3, 'loc': 'middle'},
+			'LEFT_SKYSCRAPER_3': {'size': '160x600', 'tile': 6, 'loc': 'footer'},
+			'TEST_TOP_RIGHT_BOXAD': {'size': '300x250', 'tile': 1, 'loc': 'top'},
+			'TEST_HOME_TOP_RIGHT_BOXAD': {'size': '300x250', 'tile': 1, 'loc': 'top'},
+			'TOP_BUTTON_WIDE': {'size': '292x90', 'tile': 3, 'loc': 'top'},
 
-            'TOP_LEADERBOARD': {'size': '728x90', 'tile': 2, 'loc': 'top', 'dcopt': 'ist'},
-            'TOP_RIGHT_BOXAD': {'size': '300x250', 'tile': 1, 'loc': 'top'},
-            'PREFOOTER_LEFT_BOXAD': {'size': '300x250', 'tile': 7, 'loc': 'footer'},
-            'PREFOOTER_RIGHT_BOXAD': {'size': '300x250', 'tile': 8, 'loc': 'footer'},
-            'WIKIA_BAR_BOXAD_1': {'size': '300x250', 'tile': 4, 'loc': 'bottom'}
-        };
+			'TOP_LEADERBOARD': {'size': '728x90', 'tile': 2, 'loc': 'top', 'dcopt': 'ist'},
+			'TOP_RIGHT_BOXAD': {'size': '300x250', 'tile': 1, 'loc': 'top'},
+			'PREFOOTER_LEFT_BOXAD': {'size': '300x250', 'tile': 7, 'loc': 'footer'},
+			'PREFOOTER_RIGHT_BOXAD': {'size': '300x250', 'tile': 8, 'loc': 'footer'},
+			'WIKIA_BAR_BOXAD_1': {'size': '300x250', 'tile': 4, 'loc': 'bottom'}
+		};
 
-    wikiaGpt.init( slotMap, 'rh' );
+	wikiaGpt.init(slotMap, 'rh');
 
-    function canHandleSlot( slotname ) {
+	function isAmznOnPage( slotname, slot ) {
+		log( ['isAmznOnPage', slotname], 'debug', logGroup );
 
-        if ( !slotMap[slotname] ) {
-            return false;
-        }
+		var matches, i;
 
-        return true;
-    }
+		if ( !window.amzn_targs || !slot.size ) {
+			return false;
+		}
 
-    function fillInSlot( slotname, success ) {
+		matches = window.amzn_targs.match( /\d+x\d+/g );
 
-        log( ['fillInSlot', slotname], 5, logGroup );
+		for ( i = 0; i < matches.length; i++ ) {
+			if ( slot.size.indexOf( matches[i].toLowerCase() ) !== -1 ) {
+				return true;
+			}
+		}
 
-        var slotTracker = adTracker.trackSlot( 'addriver2', slotname );
+		return false;
+	}
 
-        slotTracker.init();
+	function canHandleSlot(slotname) {
 
-        wikiaGpt.pushAd(
-            slotname,
-            function () { // Success
-                slotTweaker.removeDefaultHeight( slotname );
-                slotTweaker.removeTopButtonIfNeeded( slotname );
-                slotTweaker.adjustLeaderboardSize( slotname );
+		if (!slotMap[slotname]) {
+			return false;
+		}
 
-                success();
-            },
-            function () { // Hop
-                log( slotname + ' was not filled by DART', 'info', logGroup );
+		return isAmznOnPage(slotname, slotMap[slotname]);
+	}
 
-                slotTweaker.hide( slotname );
-                slotTweaker.hideSelfServeUrl( slotname );
+	function fillInSlot(slotname, success) {
 
-                success();
-            },
-            true
-        );
+		log(['fillInSlot', slotname], 5, logGroup);
 
-        wikiaGpt.flushAds();
-    }
+		var slotTracker = adTracker.trackSlot('addriver2', slotname);
 
-    return {
-        name: 'RemnantDart',
-        canHandleSlot: canHandleSlot,
-        fillInSlot: fillInSlot
-    };
+		slotTracker.init();
+
+		wikiaGpt.pushAd(
+			slotname,
+			function () { // Success
+				slotTweaker.removeDefaultHeight(slotname);
+				slotTweaker.removeTopButtonIfNeeded(slotname);
+				slotTweaker.adjustLeaderboardSize(slotname);
+
+				success();
+			},
+			function () { // Hop
+				log(slotname + ' was not filled by DART', 'info', logGroup);
+
+				slotTweaker.hide(slotname);
+				slotTweaker.hideSelfServeUrl(slotname);
+
+				success();
+			},
+			true
+		);
+
+		wikiaGpt.flushAds();
+	}
+
+	return {
+		name: 'RemnantDart',
+		canHandleSlot: canHandleSlot,
+		fillInSlot: fillInSlot
+	};
 };
