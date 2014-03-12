da4e0dc5533f2edf61f4857e312bb1acd48fd622
Onei <mdowdell244@gmail.com>
2014-03-12 19:37:08 +0000

    VOLDEV-53 - Add alias file for new Special:ListUsers redirects

---
+++ extensions/wikia/Listusers/SpecialListUsers.alias.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Aliases for Special:ListUsers redirects
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAlisases['en'] = array(
+	'Listvstf' => array( 'Listvstf' ),
+	'Liststaff' => array( 'Liststaff' ),
+	'Listhelpers' => array( 'Listhelpers' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases[ 'de' ] = array(
+	'Listvstf' => array( 'Vstf' ),
+	'Liststaff' => array( 'Wikia-Mitarbeiter' ),
+	'Listhelpers' => array( 'Wikia-Helfer' ),
+);
+
+/** French (Français) */
+$specialPageAlisases['fr'] = array(
+	'Listvstf' => array( 'Listevstf', 'Liste_vstf' ),
+	'Liststaff' => array( 'Listestaff', 'Liste_staff' ),
+	'Listhelpers' => array( 'Listedesassistants', 'Liste_des_assistants' ),
+);
+
+/** Italian (Italiano) */
+$specialPageAlisases['it'] = array(
+	'Listvstf' => array( 'Vstf', 'Elencovstf' ),
+	'Liststaff' => array( 'Staff', 'Elencostaff' ),
+	'Listhelpers' => array( 'Helper', 'Elencohelper' ),
+);
+
+/** Polish (Polski) */
+$specialPageAlisases['pl'] = array(
+	'Listvstf' => array( 'Vstf' ),
+	'Liststaff' => array( 'Staffowie' ),
+	'Listhelpers' => array( 'Helperzy' ),
+);
