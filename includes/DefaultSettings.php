c07754b46f9c4d3b8c5bb2c224d70dc343098911
grunny <mwgrunny@gmail.com>
2014-03-13 15:46:56 +1000

    (CE-689) Apply patch for MW 1.19.13

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.19.12';
+$wgVersion = '1.19.13';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
