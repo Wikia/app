2091e04a86d7b15528856dd83d4e50820b2315e3
Matt Klucsarits <mattk@wikia-inc.com>
2014-03-21 00:01:49 +0000

    VE-742 Script to migrate user editor preference

---
+++ maintenance/wikia/migrateEditorPreference.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Migrates users' RTE setting to the new editor preference
+ *
+ * @author mklucsarits
+ */
+ini_set( "include_path", dirname(__FILE__)."/../" );
+require_once( "commandLine.inc" );
+
+$dbr = wfGetDB( DB_MASTER, array(), $wgExternalSharedDB );
+//$dbr = wfGetDB( DB_MASTER );
+
+$dbName = $dbr->getDBname();
+echo "Database name: $dbName\n";
+if ( !in_array( $dbName, array( 'wikicities', 'wikicities_c1' ) ) ) {
+	exit( "*** Database name not in list of acceptable databases for this script. Aborting.\n" );
+}
+
+// Only users who don't have enablerichtext set should appear in this table
+$result = $dbr->select( 'user_properties', 'count(up_user)', "up_property = 'enablerichtext' and up_value != '' && up_value != '0'" );
+if ( (int)$dbr->fetchRow( $result )[0] > 0 ) {
+	exit( "*** Non-null or non-zero values found in table user_properties for 'enablerichtext'. Aborting.\n" );
+}
+
+$result = $dbr->select( 'user_properties', 'up_user, up_property', "up_property = 'enablerichtext'" );
+$userCount = 0;
+$start = time();
+
+while ( $row = $dbr->fetchObject( $result ) ) {
+	$user = User::newFromId( $row->up_user );
+	$output = 'User ID: '.$row->up_user;
+
+	// User's editor preference will be set to "Source"
+	$user->setOption( 'defaulteditor', 1 );
+	$output .= ' --> Setting defaulteditor to 1 (Source)';
+
+	$user->saveSettings();
+	echo "$output\n";
+	$userCount++;
+}
+
+$end = time();
+$delta = $end - $start;
+echo "\nDone -- $userCount users updated, $delta seconds elapsed.\n";
+
+$dbr->freeResult( $result );
