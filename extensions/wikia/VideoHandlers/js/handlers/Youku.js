092a02f7d28e6d418839f72e3f965e275910c7a7
James Sutterfield <james@wikia-inc.com>
2014-03-21 16:17:51 -0700

    VID-1296 Update Youku js file

--- extensions/wikia/VideoHandlers/js/handlers/Youku.js
+++ extensions/wikia/VideoHandlers/js/handlers/Youku.js
@@ -1,3 +1,8 @@
+/**
+ * NOTE: This file currently isn't being used, we're embedding videos with iframes for the
+ * time being. The plan is to switch over to the javascript player in the future so we can
+ * more accurately track playing events. We can use this file when we do make that switch.
+ */
 define('wikia.videohandler.youku', function() {
     'use strict';
 
@@ -8,7 +13,7 @@ define('wikia.videohandler.youku', function() {
      */
     return function(params, vb) {
         var player = new YKU.Player('youkuplayer',{
-            styleid: '0',
+            styleid: params.playerColor,
             client_id: params.client_id,
             vid: params.vid,
             autoplay: params.autoplay
