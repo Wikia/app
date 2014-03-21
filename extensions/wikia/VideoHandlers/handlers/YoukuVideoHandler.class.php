18f63eb4714d2c178e50467cd05916105ab01fa7
James Sutterfield <james@wikia-inc.com>
2014-03-20 20:29:40 -0700

    VID-1296 Refactor YoukuVideoHandler a bit

--- extensions/wikia/VideoHandlers/handlers/YoukuVideoHandler.class.php
+++ extensions/wikia/VideoHandlers/handlers/YoukuVideoHandler.class.php
@@ -3,34 +3,31 @@
 class YoukuVideoHandler extends VideoHandler {
 
 	protected $apiName = 'YoukuApiWrapper';
-	protected static $urlTemplate = 'http://www.youtube.com/embed/$1';
+	protected static $urlTemplate = 'http://player.youku.com/player.php/sid/$1/v.swf';
 	protected static $providerDetailUrlTemplate = "http://v.youku.com/v_show/id_$1.html";
 	protected static $providerHomeUrl = 'http://www.youku.com/';
 
 	public function getEmbed($articleId, $width, $autoplay=false, $isAjax=false, $postOnload=false) {
 
 		$height =  $this->getHeight( $width );
-		$apiKey = F::app()->wg->YoukuConfig['AppKey'];
-		$jsFile = 'http://player.youku.com/jsapi';
-
+		$youKuConfig = F::app()->wg->YoukuConfig;
 		$html = <<< EOF
 <div id="youkuplayer" style="width: {$width}px; height: {$height}px"></div>
 EOF;
-
 
 		return array(
 			'html'	=> $html,
 			'width' => $width,
 			'height' => $height,
 			'scripts' => array(
-				$jsFile,
+				'http://player.youku.com/jsapi',
 				"extensions/wikia/VideoHandlers/js/handlers/Youku.js"
 			),
 			'jsParams' => array(
-				'styleid'	=> '0',
-				'client_id'	=> F::app()->wg->YoukuConfig['AppKey'],
+				'styleid'	=> $youKuConfig['playerColor'],
+				'client_id'	=> $youKuConfig['AppKey'],
 				'vid'		=> $this->videoId,
-				'autoplay'	=> true,
+				'autoplay'	=> $autoplay,
 			),
 			'init' => 'wikia.videohandler.youku',
 		);
