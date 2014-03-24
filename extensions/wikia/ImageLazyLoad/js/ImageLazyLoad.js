3d8e4f506af7b9a500c68edf5b544334a3dd94d3
macbre <maciej.brencz@gmail.com>
2014-03-24 12:31:51 +0100

    Handle wgEnableWebPSupportStats + JS linting

--- extensions/wikia/ImageLazyLoad/js/ImageLazyLoad.js
+++ extensions/wikia/ImageLazyLoad/js/ImageLazyLoad.js
@@ -1,36 +1,33 @@
+/*global syslogReport: true */
 /* Lazy loading for images inside articles (skips wikiamobile)
  * @author Piotr Bablok <pbablok@wikia-inc.com>
  */
 require( [ 'jquery', 'wikia.log', 'wikia.browserDetect', 'wikia.window' ], function( $, log, browserDetect, w ) {
 	'use strict';
 
-	var browserSupportsWebP;
+	var browserSupportsWebP,
+		ImgLzy;
 
 	function checkWebPSupport() {
-		// WebP is turned off on this wiki
-		if ( w.wgEnableWebPThumbnails !== true ) {
-			return;
-		}
-
 		log('checking WebP support...',  log.levels.info, 'ImgLzy');
 
 		// @see http://stackoverflow.com/a/5573422
 		var webP = new Image();
 		webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-		webP.onload = webP.onerror = $.proxy(function () {
+		webP.onload = webP.onerror = function () {
 			browserSupportsWebP = webP.height === 2;
 
 			log('has support for WebP: ' + (browserSupportsWebP ? 'yes' : 'no'),  log.levels.info, 'ImgLzy');
 
-			// report browsers support stats to Kibana
-			if (typeof syslogReport === 'function') {
+			// report WebP support stats to Kibana
+			if ( w.wgEnableWebPSupportStats === true && typeof syslogReport === 'function' ) {
 				syslogReport(log.levels.info, 'webp', {'webp-support': browserSupportsWebP ? 'yes' : 'no'});
 			}
-		}, this);
+		};
 	}
 
 	// it's a global, it should be a global
-	var ImgLzy = {
+	ImgLzy = {
 		cache: [],
 		timestats: 0,
 
@@ -56,9 +53,9 @@ require( [ 'jquery', 'wikia.log', 'wikia.browserDetect', 'wikia.window' ], funct
 			return e.offset().top;
 		},
 
-		// rewrite the URL to request WebP thumbnails (if supported)
+		// rewrite the URL to request WebP thumbnails (if enabled on this wiki and supported by the browser)
 		rewriteURLForWebP: function(src) {
-			if ( browserSupportsWebP && src.indexOf( '/images/thumb/' ) > 0 ) {
+			if ( w.wgEnableWebPThumbnails === true && browserSupportsWebP && src.indexOf( '/images/thumb/' ) > 0 ) {
 				src = src.replace( /\.[^\./]+$/, '.webp' );
 			}
 			return src;
@@ -72,7 +69,7 @@ require( [ 'jquery', 'wikia.log', 'wikia.browserDetect', 'wikia.window' ], funct
 					relativeTo = $( '.scroller' ).find( this ),
 					topCalc, top;
 
-				if ( relativeTo.length != 0 ) {
+				if ( relativeTo.length !== 0 ) {
 					relativeTo = relativeTo.parents( '.scroller' );
 					topCalc = self.relativeTop;
 				} else {
@@ -132,11 +129,10 @@ require( [ 'jquery', 'wikia.log', 'wikia.browserDetect', 'wikia.window' ], funct
 				image.src = this.rewriteURLForWebP( dataSrc );
 			}
 			$img.removeClass( 'lzy' ).removeClass( 'lzyPlcHld' );
-
 		},
 
 		parentVisible: function( item ) {
-			if ( item.parent[0] == window ) {
+			if ( item.parent[0] === window ) {
 				return true;
 			}
 
@@ -172,7 +168,7 @@ require( [ 'jquery', 'wikia.log', 'wikia.browserDetect', 'wikia.window' ], funct
 
 				cacheItem.parent.data( 'lastScrollTop', lastScrollTop );
 				visible = (scrollTop < cacheItem.top && scrollBottom > cacheItem.top) ||
-					(scrollTop < cacheItem.bottom && scrollBottom > cacheItem.bottom)
+					(scrollTop < cacheItem.bottom && scrollBottom > cacheItem.bottom);
 
 				if ( visible && this.parentVisible( cacheItem ) ) {
 					cacheItem.jq.addClass( 'lzyTrns' );
