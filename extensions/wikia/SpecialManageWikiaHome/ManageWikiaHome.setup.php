e97e80967eb58fcff9be7fab32a3e32454153d46
Lukasz Konieczny <lukaszk@wikia-inc.com>
2014-03-27 10:55:45 +0000

    CON-751: Added hubs slots form to setup file

--- extensions/wikia/SpecialManageWikiaHome/ManageWikiaHome.setup.php
+++ extensions/wikia/SpecialManageWikiaHome/ManageWikiaHome.setup.php
@@ -32,6 +32,7 @@ $wgAutoloadClasses['SpecialManageWikiaHomeModel'] = $dir . '/models/SpecialManag
 // forms
 $wgAutoloadClasses['CollectionsForm'] = $dir.'/forms/CollectionsForm.class.php';
 $wgAutoloadClasses['StatsForm'] = $dir.'/forms/StatsForm.class.php';
+$wgAutoloadClasses['HubsForm'] = $dir.'/forms/HubsSlotsForm.class.php';
 
 //special page
 $wgSpecialPages['ManageWikiaHome'] = 'ManageWikiaHomeController';
