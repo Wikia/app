9640aa3af30bd0bf5a4612f80804de87c96dcd8c
VForge <bartosz.bentkowski@gmail.com>
2014-04-01 14:09:57 +0000

    CON-804 | Beta version

--- skins/oasis/js/GlobalBurger.js
+++ skins/oasis/js/GlobalBurger.js
@@ -1,4 +1,7 @@
-require([ 'jquery', 'wikia.ui.factory', 'wikia.nirvana' ], function( $, uiFactory, nirvana ) {
+require([ 'jquery', 'wikia.ui.factory', 'wikia.nirvana' ], function ($, uiFactory, nirvana) {
+	'use strict';
+
+
 	var menuPromise = nirvana.sendRequest({
 			controller: 'GlobalHeaderController',
 			method: 'getGlobalMenuItems',
@@ -8,27 +11,81 @@ require([ 'jquery', 'wikia.ui.factory', 'wikia.nirvana' ], function( $, uiFactor
 				cb: Date.now()
 			}
 		}),
-		drawerPromise = uiFactory.init( [ 'drawer' ] );
+		drawerPromise = uiFactory.init([ 'drawer' ]),
+		buildSubMenus = function (menuItemsObj) {
+			var subMenus = [];
 
-	$.when(menuPromise, drawerPromise).done( function(menuXhr, uiDrawer) {
+			$.each(menuItemsObj, function () {
+				var subMenu = [],
+					extraClassName = this.specialAttr;
+
+				$.each(this.children, function () {
+					subMenu.push('<ul class="drawerSubmenu ' + extraClassName + '">');
+					subMenu.push('<li><header>' + this.text + '</header></li>');
+
+					$.each(this.children, function () {
+						subMenu.push('<li class="' + this.specialAttr + '" ><a href="' + this.href + '">' + this.text + '</a></li>');
+					});
+
+					subMenu.push('</ul>');
+				});
+				subMenus.push(subMenu.join(''));
+			});
+
+			return subMenus;
+		},
+		buildMainMenu = function (menuItemsObj) {
+			var menu = [];
+
+			$.each(menuItemsObj, function (i) {
+				menu.push('<li class="' + this.specialAttr + '" data-id="' + i + '"><a href="' + this.href + '">' + this.text + '</a></li>');
+			});
+
+			return '<ul id="drawerGlobalNavigation">' + menu.join('') + '</ul>';
+		};
+
+	$.when(menuPromise, drawerPromise).done(function (menuXhr, uiDrawer) {
 		var menuItems = menuXhr[0],
+			mainMenu = buildMainMenu( menuItems ),
+			subMenus = buildSubMenus( menuItems ),
 			drawerConfig = {
 				vars: {
 					//style: 'fixed',
 					closeText: 'Close',
 					side: 'left',
-					content: 'Lorem'
+					content: mainMenu
 				}
 			};
 
-			uiDrawer.createComponent(drawerConfig, function(drawer) {
-				$('.WikiaLogo').on('click', function(e) {
-					e.preventDefault();
+		$('#GlobalNavigation').remove();
 
-					console.log(menuItems);
+		uiDrawer.createComponent(drawerConfig, function (drawer) {
+			var delay = 300,
+				timer;
 
-					drawer.open();
-				});
+			window.GlobalNavigationDrawer = drawer;
+
+			$('#drawerGlobalNavigation li').hover(function (e) {
+				var $self = $(this),
+					id = $self.data('id');
+
+				timer = setTimeout(function () {
+					if (!$self.hasClass('active')) {
+						$('#drawerGlobalNavigation').find('li').removeClass('active');
+						$self.addClass('active');
+
+						drawer.swipeSub(subMenus[id]);
+					}
+				}, delay);
+			}, function (e) {
+				clearTimeout(timer);
 			});
-	} );
-} );
+
+			$('.WikiaLogo').on('click', function (e) {
+				e.preventDefault();
+
+				drawer.open();
+			});
+		});
+	});
+});
