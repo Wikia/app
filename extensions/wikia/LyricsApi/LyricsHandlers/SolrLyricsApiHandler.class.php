5bb3fa333699dc358ff3c3ae6086ab396da2ce02
nAndy <nandy@wikia-inc.com>
2014-04-02 07:35:50 +0000

    MOB-1323: let's check if at least file page exists for given image name

--- extensions/wikia/LyricsApi/LyricsHandlers/SolrLyricsApiHandler.class.php
+++ extensions/wikia/LyricsApi/LyricsHandlers/SolrLyricsApiHandler.class.php
@@ -66,9 +66,14 @@ class SolrLyricsApiHandler extends AbstractLyricsApiHandler {
 	private function appendImages( $obj, $image ) {
 		// Quick fix for wrong data in templates (i.e. metallica.jpg instead of Metallica.jpg)
 		$image = ucfirst( $image );
-		$obj->small_image = $this->getImage( $image );
-		$obj->medium_image = $this->getImage( $image, self::IMG_WIDTH_MEDIUM, self::IMG_HEIGHT_MEDIUM );
-		$obj->large_image = $this->getImage( $image, self::IMG_WIDTH_LARGE, self::IMG_HEIGHT_LARGE );
+
+		// MOB-1323 - file provided in artist template but not existing in MW
+		$file = Title::newFromText( $image, NS_FILE );
+		if( $file->exists() ) {
+			$obj->small_image = $this->getImage( $image );
+			$obj->medium_image = $this->getImage( $image, self::IMG_WIDTH_MEDIUM, self::IMG_HEIGHT_MEDIUM );
+			$obj->large_image = $this->getImage( $image, self::IMG_WIDTH_LARGE, self::IMG_HEIGHT_LARGE );
+		}
 	}
 
 	/**
