410a23e22ea36e10a4018c5304c03406c9f15f20
VForge <bartosz.bentkowski@gmail.com>
2014-04-03 09:45:45 +0000

    CON-846 | Fix Qualaroo _kiq

--- extensions/wikia/Qualaroo/scripts/Qualaroo.js
+++ extensions/wikia/Qualaroo/scripts/Qualaroo.js
@@ -1,10 +1,12 @@
 ( function ( window ) {
 	'use strict';
-	var _kiq = _kiq || [];
+	var _kiq = [];
+
 	setTimeout(function(){
 		var d = document, f = d.getElementsByTagName('script')[0], s = d.createElement('script'); s.type = 'text/javascript';
 		s.async = true; s.src = window.wgQualarooUrl; f.parentNode.insertBefore(s, f);
 	}, 1);
+
 	if (window.wgUser) {
 		_kiq.push(['identify', window.wgUser]);
 	}
@@ -12,7 +14,9 @@
 		'userLanguage': window.wgUserLanguage,
 		'contentLanguage': window.wgContentLanguage,
 		'pageType': window.wikiaPageType,
-		'isCorporatePage': window.wikiaPageIsCorporate ? 'Yes' : 'No',
+		'isCorporatePage': (window.wikiaPageIsCorporate ? 'Yes' : 'No'),
 		'verticalName': window.verticalName
 	}]);
+
+	window._kiq = _kiq;
 })( window );
