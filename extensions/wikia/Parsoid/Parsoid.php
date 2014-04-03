79fc7bcb9f27860688aa61a42bcf03c695a52bc6
Inez Korczynski <inez@wikia-inc.com>
2014-04-03 23:01:13 +0000

    Initial import of ParsoidCacheUpdateJob

---
+++ extensions/wikia/Parsoid/Parsoid.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Basic cache invalidation for Parsoid
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "Parsoid extension\n";
+	exit( 1 );
+}
+
+class ParsoidSetup {
+
+	public static function setup() {
+		global $wgAutoloadClasses, $wgJobClasses, $wgParsoidCacheServers;
+		$dir = __DIR__;
+
+		$wgAutoloadClasses['ParsoidHooks'] = "$dir/Parsoid.hooks.php";
+		$wgAutoloadClasses['ParsoidCacheUpdateJob'] = "$dir/ParsoidCacheUpdateJob.php";
+		$wgAutoloadClasses['CurlMultiClient'] = "$dir/CurlMultiClient.php";
+
+		$wgJobClasses['parsoidJob'] = 'ParsoidCacheUpdateJob';
+
+		self::registerHooks();
+
+		$wgParsoidCacheServers = array( 'http://parsoid-cache' );
+	}
+
+	protected static function registerHooks() {
+		global $wgHooks;
+
+		# Article edit/create
+		$wgHooks['ArticleEditUpdates'][] = 'ParsoidHooks::onArticleEditUpdates';
+		# Article delete/restore
+		$wgHooks['ArticleDeleteComplete'][] = 'ParsoidHooks::onArticleDeleteComplete';
+		$wgHooks['ArticleUndelete'][] = 'ParsoidHooks::onArticleUndelete';
+		# Revision delete/restore
+		$wgHooks['ArticleRevisionVisibilitySet'][] = 'ParsoidHooks::onArticleRevisionVisibilitySet';
+		# Article move
+		$wgHooks['TitleMoveComplete'][] = 'ParsoidHooks::onTitleMoveComplete';
+		# File upload
+		$wgHooks['FileUpload'][] = 'ParsoidHooks::onFileUpload';
+	}
+
+}
+
+ParsoidSetup::setup();
---
+++ extensions/wikia/Parsoid/ParsoidCacheUpdateJob.php
@@ -0,0 +1,110 @@
+<?php
+
+class ParsoidCacheUpdateJob extends Job {
+
+	var $type, $table, $start, $end;
+
+	function __construct( $title, $params, $id = 0 ) {
+		parent::__construct( 'ParsoidCacheUpdateJob' . $params['type'], $title, $params, $id );
+		$this->type = $params['type'];
+		if ( isset( $params['table'] ) ) {
+			$this->table = $params['table'];
+		}
+		if ( isset( $params['start'] ) ) {
+			$this->start = $params['start'];
+		}
+		if ( isset( $params['end'] ) ) {
+			$this->end = $params['end'];
+		}
+	}
+
+	public function run() {
+		global $wgUpdateRowsPerJob;
+
+		if ( $this->type === 'invalidate' ) {
+			$titles = $this->title->getBacklinkCache()->getLinks( $this->table, $this->start, $this->end );
+			$this->invalidateTitles( $titles );
+		} else if ( $this->type === 'OnDependencyChange' ) {
+			$cache = $this->title->getBacklinkCache();
+			$batches = $cache->partition( $this->table, $wgUpdateRowsPerJob );
+			$jobs = array();
+			foreach ( $batches as $batch ) {
+				 list( $start, $end ) = $batch;
+				 $jobs[] = new ParsoidCacheUpdateJob( $this->title, array (
+				 	'type' => 'invalidate',
+				 	'table' => $this->table,
+				 	'start' => $start,
+				 	'end' => $end
+				 ) );
+			}
+			Job::batchInsert( $jobs );
+		} else if ( $this->type === 'OnEdit' ) {
+			$this->invalidateTitle( $this->title );
+		}
+	}
+
+	protected function getParsoidURL( Title $title, $server, $prev = false ) {
+		global $wgParsoidWikiPrefix, $wgVisualEditorParsoidURL;
+
+		$oldid = $prev ?
+			$title->getPreviousRevisionID( $title->getLatestRevID() ) :
+			$title->getLatestRevID();
+
+		return $wgVisualEditorParsoidURL . '/' . wfExpandUrl( wfScript( 'api' ) ) . '/' .
+			wfUrlencode( $title->getPrefixedDBkey() ) . '?oldid=' . $oldid;
+	}
+
+	protected function checkCurlResults( $results ) {
+		foreach( $results as $k => $result ) {
+			if ($results[$k]['error'] != null) {
+				$this->setLastError($results[$k]['error']);
+				return false;
+			}
+		}
+		return true;
+	}
+
+	protected function invalidateTitles( $titles ) {
+	}
+
+	protected function invalidateTitle( Title $title ) {
+		global $wgParsoidCacheServers;
+
+		# First request the new version
+		$parsoidInfo = array();
+		$parsoidInfo['cacheID'] = $title->getPreviousRevisionID( $title->getLatestRevID() );
+		$parsoidInfo['changedTitle'] = $this->title->getPrefixedDBkey();
+
+		$requests = array();
+		foreach ( $wgParsoidCacheServers as $server ) {
+			$requests[] = array(
+				'url'     => $this->getParsoidURL( $title, $server ),
+				'headers' => array(
+					'X-Parsoid: ' . json_encode( $parsoidInfo ),
+					// Force implicit cache refresh similar to
+					// https://www.varnish-cache.org/trac/wiki/VCLExampleEnableForceRefresh
+					'Cache-control: no-cache'
+				)
+			);
+		}
+		print_pre($requests);
+		$this->checkCurlResults( CurlMultiClient::request( $requests ) );
+
+		# And now purge the previous revision so that we make efficient use of
+		# the Varnish cache space without relying on LRU. Since the URL
+		# differs we can't use implicit refresh.
+		$requests = array();
+		foreach ( $wgParsoidCacheServers as $server ) {
+			// @TODO: this triggers a getPreviousRevisionID() query per server
+			$requests[] = array(
+				'url' => $this->getParsoidURL( $title, $server, true )
+			);
+		}
+		$options = CurlMultiClient::getDefaultOptions();
+		$options[CURLOPT_CUSTOMREQUEST] = "PURGE";
+		print_pre($requests);
+		$this->checkCurlResults( CurlMultiClient::request( $requests, $options ) );
+		return $this->getLastError() == null;
+	}
+
+}
