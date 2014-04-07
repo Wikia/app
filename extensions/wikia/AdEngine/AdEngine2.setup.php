784d613b549955edb4ac459368a55e30c9a04f69
Piotr Gabryjeluk <rychu@wikia-inc.com>
2014-04-07 08:48:50 +0200

    ADEN-433 Separate file for Exitstitial ads

--- extensions/wikia/AdEngine/AdEngine2.setup.php
+++ extensions/wikia/AdEngine/AdEngine2.setup.php
@@ -1,22 +1,27 @@
 <?php
 
+// Autoload
 $wgAutoloadClasses['AdServer'] =  __DIR__ . '/AdServer.php';
 $wgAutoloadClasses['AdEngine2Controller'] =  __DIR__ . '/AdEngine2Controller.class.php';
+$wgAutoloadClasses['AdEngine2ExitstitialHooks'] =  __DIR__ . '/AdEngine2ExitstitialHooks.class.php';
 $wgAutoloadClasses['AdEngine2Hooks'] =  __DIR__ . '/AdEngine2Hooks.class.php';
 $wgAutoloadClasses['AdEngine2Service'] =  __DIR__ . '/AdEngine2Service.class.php';
 $wgAutoloadClasses['ResourceLoaderAdEngineSevenOneMediaModule'] = __DIR__ . '/ResourceLoaderAdEngineSevenOneMediaModule.php';
 
+// Hooks for AdEngine2
 $wgHooks['AfterInitialize'][] = 'AdEngine2Hooks::onAfterInitialize';
 $wgHooks['WikiaSkinTopScripts'][] = 'AdEngine2Hooks::onWikiaSkinTopScripts';
 $wgHooks['MakeGlobalVariablesScript'][] = 'AdEngine2Hooks::onMakeGlobalVariablesScript';
 $wgHooks['OasisSkinAssetGroupsBlocking'][] = 'AdEngine2Hooks::onOasisSkinAssetGroupsBlocking';
 $wgHooks['WikiaSkinTopModules'][] = 'AdEngine2Hooks::onWikiaSkinTopModules';
 $wgHooks['OasisSkinAssetGroups'][] = 'AdEngine2Hooks::onOasisSkinAssetGroups';
-$wgHooks['LinkerMakeExternalLink'][] = 'AdEngine2Hooks::onLinkerMakeExternalLink';
-$wgHooks['LinkEnd'][] = 'AdEngine2Hooks::onLinkEnd';
 
+// Hooks for Exitstitial ads
+$wgHooks['LinkerMakeExternalLink'][] = 'AdEngine2ExitstitialHooks::onLinkerMakeExternalLink';
+$wgHooks['LinkEnd'][] = 'AdEngine2ExitstitialHooks::onLinkEnd';
+
+// i18n
 $wgExtensionMessagesFiles['AdEngine'] = __DIR__ . '/AdEngine.i18n.php';
-
 $wgExtensionFunctions[] = function() {
 	JSMessages::registerPackage('AdEngine', array('adengine-*'));
 };
--- extensions/wikia/AdEngine/AdEngine2Hooks.class.php
+++ extensions/wikia/AdEngine/AdEngine2Hooks.class.php
@@ -28,7 +28,7 @@ class AdEngine2Hooks {
 
 
 	/**
-	 * Register global JS variables bottom (migrated from wfAdEngineSetupJSVars)
+	 * Register global JS variables bottom
 	 *
 	 * @param array $vars
 	 *
@@ -94,7 +94,7 @@ class AdEngine2Hooks {
 	}
 
 	/**
-	 * Register ad-related vars on top (migrated from wfAdEngineSetupTopVars)
+	 * Register ad-related vars on top
 	 *
 	 * @param array $vars
 	 * @param array $scripts
@@ -218,108 +218,4 @@ class AdEngine2Hooks {
 		}
 		return true;
 	}
-
-
-	/**
-	 * Deal with external interwiki links: add exitstitial class to them if needed
-	 *
-	 * @param $skin
-	 * @param $target
-	 * @param $options
-	 * @param $text
-	 * @param $attribs
-	 * @param $ret
-	 *
-	 * @return bool
-	 */
-	static public function onLinkEnd($skin, Title $target, array $options, &$text, array &$attribs, &$ret) {
-		if ($target->isExternal()) {
-			static::handleExternalLink($attribs['href'], $attribs);
-		}
-		return true;
-	}
-
-	/**
-	 * Deal with external links: add exitstitial class to them if needed
-	 *
-	 * @param $url
-	 * @param $text
-	 * @param $link
-	 * @param $attribs
-	 *
-	 * @return bool
-	 */
-	static function onLinkerMakeExternalLink(&$url, &$text, &$link, &$attribs) {
-		static::handleExternalLink($url, $attribs);
-		return true;
-	}
-
-	/**
-	 * Add exitstitial class to the external links pointing to not-whitelisted domains
-	 * if $wgEnableOutboundScreenExt is set, user is anonymous, not in editor, etc
-	 *
-	 * @param $url
-	 * @param $attribs
-	 *
-	 * @return null
-	 */
-	static private function handleExternalLink($url, &$attribs) {
-		global $wgEnableOutboundScreenExt, $wgRTEParserEnabled, $wgTitle, $wgUser;
-
-		if (!$wgEnableOutboundScreenExt
-			|| $wgRTEParserEnabled    // skip logic when in FCK
-			|| empty($wgTitle)        // setup functions can call MakeExternalLink before wgTitle is set RT#144229
-			|| $wgUser->isLoggedIn()  // logged in users have no exit stitial ads
-			|| strpos($url, 'http://') !== 0
-		) {
-			return;
-		}
-
-		foreach (static::getExitstitialUrlsWhiteList() as $whiteListedUrl) {
-			if (preg_match('/' . preg_quote($whiteListedUrl) . '/i', $url)) {
-				return;
-			}
-		}
-
-		if (isset($attribs['class'])) {
-			$attribs['class'] .= ' exitstitial';
-		}
-	}
-
-
-	static private function getExitstitialUrlsWhiteList() {
-
-		global $wgDevelEnvironment, $wgCityId;
-
-		static $whiteList = null;
-
-		if (is_array($whiteList)) {
-			return $whiteList;
-		}
-
-		$whiteList = [];
-		$whiteListContent = wfMsgExt('outbound-screen-whitelist', array('language' => 'en'));
-
-		if (!empty($whiteListContent)) {
-			$lines = explode("\n", $whiteListContent);
-			foreach($lines as $line) {
-				if(strpos($line, '* ') === 0 ) {
-					$whiteList[] = trim($line, '* ');
-				}
-			}
-		}
-
-		$wikiDomains = WikiFactory::getDomains($wgCityId);
-		if ($wikiDomains !== false) {
-			$whiteList = array_merge($wikiDomains, $whiteList);
-		}
-
-		// Devboxes run on different domains than just what is in WikiFactory.
-		if ($wgDevelEnvironment && !empty($_SERVER['SERVER_NAME'])) {
-			array_unshift($whiteList, $_SERVER['SERVER_NAME']);
-		}
-
-		return $whiteList;
-	}
-
 }
