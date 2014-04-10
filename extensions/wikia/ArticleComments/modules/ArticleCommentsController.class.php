e4242edf6f029e50ef6aa874c85b4d500be99177
Owen Davis <owen@wikia-inc.com>
2014-04-10 20:11:44 +0000

    Revert "Merge pull request #3292 from Wikia/CONN-438"
    This caused CONN-480

--- extensions/wikia/ArticleComments/modules/ArticleCommentsController.class.php
+++ extensions/wikia/ArticleComments/modules/ArticleCommentsController.class.php
@@ -45,12 +45,9 @@ class ArticleCommentsController extends WikiaController {
 				}
 			}
 
-            $this->page = $this->wg->request->getVal( 'page', 1 );
-            $this->isLoadingOnDemand = ArticleComment::isLoadingOnDemand();
-            $this->isMiniEditorEnabled = ArticleComment::isMiniEditorEnabled();
-
-            // store avatar related stuff in main template so it could be added to lazy loaded comments which could be cached
-            $this->avatarUrl = AvatarService::getAvatarUrl(F::app()->wg->User->getName(), AvatarService::AVATAR_SIZE_MEDIUM);
+			$this->page = $this->wg->request->getVal( 'page', 1 );
+			$this->isLoadingOnDemand = ArticleComment::isLoadingOnDemand();
+			$this->isMiniEditorEnabled = ArticleComment::isMiniEditorEnabled();
 
 			if ( $this->isLoadingOnDemand ) {
 				$this->response->setJsVar( 'wgArticleCommentsLoadOnDemand', true );
@@ -106,9 +103,10 @@ class ArticleCommentsController extends WikiaController {
 		// Uncomment this when surrogate key purging works
 		//$this->wg->Out->tagWithSurrogateKeys( ArticleComment::getSurrogateKey($articleId) );
 
-		// Set cache for a day
+		// When lazy loading this request it shouldn't be cached in the browser
 		if ( !empty( $this->wg->ArticleCommentsLoadOnDemand ) ) {
-			$this->response->setCacheValidity(WikiaResponse::CACHE_STANDARD, WikiaResponse::CACHE_DISABLED);
+			$this->response->setCachePolicy( WikiaResponse::CACHE_PRIVATE );
+			$this->response->setCacheValidity( WikiaResponse::CACHE_DISABLED );
 		}
 
 		wfProfileOut( __METHOD__ );
--- extensions/wikia/ArticleComments/modules/templates/ArticleComments_Index.php
+++ extensions/wikia/ArticleComments/modules/templates/ArticleComments_Index.php
@@ -1,8 +1,7 @@
 <? if ( $isMiniEditorEnabled ): ?>
 	<?= $app->renderView( 'MiniEditorController', 'Setup' ) ?>
 <? endif ?>
-<section id="WikiaArticleComments" class="WikiaArticleComments noprint" data-page="<?= $page ?>"
-         data-avatar-url="<?= htmlentities($avatarUrl) ?>">
+<section id="WikiaArticleComments" class="WikiaArticleComments noprint" data-page="<?= $page ?>">
 	<? if ( empty( $isLoadingOnDemand ) ): ?>
 		<?= $app->renderView( 'ArticleCommentsController', 'Content' ) ?>
 	<? endif ?>
