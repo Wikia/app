cd86bfbee76fc8218974d65e4b9612f197b95740
lizlux <liz@lizlux.net>
2014-04-17 18:11:16 -0700

    VID-1308 reducing requests for thumbnails scss

--- extensions/wikia/Thumbnails/ThumbnailVideoHooks.class.php
+++ extensions/wikia/Thumbnails/ThumbnailVideoHooks.class.php
@@ -7,14 +7,13 @@ class ThumbnailVideoHooks {
 	 * @param $skin
 	 * @return bool
 	 */
-	static public function onBeforePageDisplay( $out, $skin ) {
+	public static function onBeforePageDisplay( $out, $skin ) {
 		wfProfileIn(__METHOD__);
 
 		if ( F::app()->checkSkin( 'monobook', $skin ) ) {
 			// not used on mobileskin
 			// part of oasis skin so not needed there
 			$out->addStyle( AssetsManager::getInstance()->getSassCommonURL( 'skins/oasis/css/core/thumbnails.scss' ) );
-			$out->addStyle( AssetsManager::getInstance()->getSassCommonURL( 'skins/oasis/css/core/thumbnails-video.scss' ) );
 		}
 
 		wfProfileOut(__METHOD__);
