ce90cccc61fdd26d88c17b1369f162966df593e5
Garth Webb <garth@wikia-inc.com>
2014-04-17 17:16:06 -0700

    VID-952 Added additional docs and cleaned up code slightly

--- extensions/wikia/VideoEmbedTool/VideoEmbedToolController.class.php
+++ extensions/wikia/VideoEmbedTool/VideoEmbedToolController.class.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Class VideoEmbedToolController
+ */
 class VideoEmbedToolController extends WikiaController {
 
 	const VIDEO_THUMB_DEFAULT_WIDTH = 160;
@@ -10,28 +13,36 @@ class VideoEmbedToolController extends WikiaController {
 	}
 
 	/**
-	 *   Example of use:
+	 * Looks for videos related to the current article
+	 *
+	 * Example of use:
 	 *
 	 *   http://harrypotter.jacek.wikia-dev.com/wikia.php?controller=VideoEmbedTool&method=getSuggestedVideos&svStart=0&svSize=5&articleId=15&format=json
 	 *
-	 *   @requestParam svStart - offset
-	 *   @requestParam svSize - limit
-	 *   @requestParam videoWidth - thumbnail width
-	 *   @requestParam videoHeight - thumbnail height
-	 *   @requestParam articleId - the suggestions should be related to this article
+	 * @requestParam svStart - offset
+	 * @requestParam svSize - limit
+	 * @requestParam trimTitle - Whether to trim the video title returned (boolean 1 or 0)
+	 * @requestParam articleId - the suggestions should be related to this article
+	 *
+	 * @responseParam searchQuery
+	 * @responseParam caption - Video caption
+	 * @responseParam totalItemCount - Number of items available
+	 * @responseParam nextStartFrom - The index for the next page of videos
+	 * @responseParam currentSetItemCount - Number of items returned in the current page
+	 * @responseParam items - Array of video suggestions
+	 * @responseParam addMessage - A translated 'Add Video' message
 	 */
 	public function getSuggestedVideos() {
 		if ( $this->wg->VETEnableSuggestions != true ) {
 			// Return empty set if wgVETEnableSuggestions is not enabled
-			$result = array(
+			$result = [
 				'caption' => wfMessage( 'vet-suggestions' )->plain(),
 				'totalItemCount' => 0,
 				'currentSetItemCount' => 0,
-				'items' => array()
-			);
+				'items' => []
+			];
 			$this->response->setData( $result );
-		}
-		else {
+		} else {
 			$request = $this->getRequest();
 			$service = new VideoEmbedToolSearchService();
 			$service->setStart( $request->getInt( 'svStart', 0 ) )
@@ -39,7 +50,7 @@ class VideoEmbedToolController extends WikiaController {
 			        ->setTrimTitle( $this->request->getInt( 'trimTitle', 0 ) );
 			$response = $service->getSuggestionsForArticleId( $this->request->getInt('articleId', 0 ) );
 
-			$result = array(
+			$result = [
 					'searchQuery' => $service->getSuggestionQuery(),
 					'caption' => wfMessage( 'vet-suggestions' )->plain(),
 					'totalItemCount' => $response['totalItemCount'],
@@ -47,12 +58,34 @@ class VideoEmbedToolController extends WikiaController {
 					'currentSetItemCount' => count($response['items']),
 					'items' => $response['items'],
 					'addMessage' => wfMessage('vet-add-from-preview')->plain()
-			);
+			];
 
 			$this->response->setData( $result );
 		}
 	}
 
+	/**
+	 * Looks for videos related to a search phrase
+	 *
+	 * Example of use:
+	 *
+	 *   http://glee.wikia.com/wikia.php?controller=VideoEmbedTool&method=search&format=json&order=default&phrase=frog&svSize=20&svStart=0&type=premium
+	 *
+	 * @requestParam phrase - The search phrase
+	 * @requestParam type - Whether to search the local wiki ('local') or the video wiki ('premium')
+	 * @requestParam svStart - offset
+	 * @requestParam svSize - limit
+	 * @requestParam order - How to order the results
+	 * @requestParam trimTitle - Whether to trim the video title returned (boolean 1 or 0)
+	 *
+	 * @responseParam searchQuery - The 'phrase' parameter passed in
+	 * @responseParam caption - Video caption
+	 * @responseParam totalItemCount - Number of items available
+	 * @responseParam nextStartFrom - The index for the next page of videos
+	 * @responseParam currentSetItemCount - Number of items returned in the current page
+	 * @responseParam items - Array of video suggestions
+	 * @responseParam addMessage - A translated 'Add Video' message
+	 */
 	public function search() {
 		$request = $this->getRequest();
 		$phrase = $request->getVal( 'phrase' );
@@ -68,7 +101,7 @@ class VideoEmbedToolController extends WikiaController {
 
 		// Grep help: can be either vet-search-results-WVL or vet-search-results-local
 		$captionKey = 'vet-search-results-'.($searchType == 'premium' ? 'WVL' : 'local');
-		$result = array (
+		$result = [
 			'searchQuery' => $phrase,
 			'caption' => wfMessage( $captionKey, $response['totalItemCount'], $phrase )->text(),
 			'totalItemCount' => $response['totalItemCount'],
@@ -76,14 +109,19 @@ class VideoEmbedToolController extends WikiaController {
 			'currentSetItemCount' => count( $response['items'] ),
 			'items' => $response['items'],
 			'addMessage' => wfMessage('vet-add-from-preview')->plain()
-		);
+		];
 
 		$this->response->setData( $result );
 	}
 
-
+	/**
+	 * Get the embed code for the given video title
+	 *
+	 * @requestParam fileTitle - The video title to get an embed code for
+	 * @requestParam width - The desired width (default 460)
+	 * @requestParam height - The desired height (default 250)
+	 */
 	public function getEmbedCode() {
-
 		$fileTitle = $this->request->getVal('fileTitle', '');
 		$fileTitle = urldecode($fileTitle);
 		$title = Title::newFromText($fileTitle, NS_FILE);
@@ -91,10 +129,10 @@ class VideoEmbedToolController extends WikiaController {
 			return;
 		}
 
-		$config = array(
+		$config = [
 			'contextWidth' => $this->request->getVal('width', 460),
 			'maxHeight' => $this->request->getVal('height', 250)
-		);
+		];
 
 		$data = WikiaFileHelper::getMediaDetail( $title, $config );
 
@@ -116,7 +154,7 @@ class VideoEmbedToolController extends WikiaController {
 		$vHelper = new VideoHandlerHelper();
 		$status = $vHelper->setVideoDescription($title, $description);
 
-		if ($status) {
+		if ( $status ) {
 			$this->status = 'success';
 		} else {
 			$this->status = 'fail';
