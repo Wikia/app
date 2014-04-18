bdb7f7dd3964022b7708a84394a21ef951edb381
aquilax <evgeniy@wikia-inc.com>
2014-04-18 13:44:00 +0000

    Added alphabetical attribution

--- extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMaps.setup.php
+++ extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMaps.setup.php
@@ -5,7 +5,10 @@ $wgExtensionCredits[ 'specialpage' ][] = [
 	'name' => 'Wikia Interactive Maps',
 	'author' => [
 		'Andrzej "nAndy" Łukaszewski',
+		'Bart(łomey) K.',
 		'Evgeniy "aquilax" Vasilev',
+		'Jakub "Student" Olek',
+		'Rafał Leszczyński',
 	],
 	'description' => 'Create your own maps with point of interest or add your own point of interest into a real world map',
 	'version' => 0.1
