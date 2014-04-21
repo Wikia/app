7e100ee0e6b50910f77955a4b4adf3c3d82e2d9f
lizlux <liz@lizlux.net>
2014-04-21 13:58:24 -0700

    VID-1344 removing a/b test code for videos module and enabling it on rail

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -1712,8 +1712,6 @@ $config['videos_module_js'] = [
 		'//extensions/wikia/Thumbnails/scripts/views/titleThumbnail.js',
 
 		'//extensions/wikia/VideosModule/scripts/templates.mustache.js',
-		'//extensions/wikia/VideosModule/scripts/models/abTestBottom.js',
-		'//extensions/wikia/VideosModule/scripts/models/abTestRail.js',
 		'//extensions/wikia/VideosModule/scripts/models/videos.js',
 		'//extensions/wikia/VideosModule/scripts/views/titleThumbnail.js',
 		'//extensions/wikia/VideosModule/scripts/views/bottomModule.js',
