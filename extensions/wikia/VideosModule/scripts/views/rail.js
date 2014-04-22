c15ffa1564d6592cecb0af65e3cc2da6551c1a41
James Sutterfield <james@wikia-inc.com>
2014-04-22 16:33:58 -0700

    VID-1585 Display a staff pick video in VM if present

--- extensions/wikia/VideosModule/scripts/views/rail.js
+++ extensions/wikia/VideosModule/scripts/views/rail.js
@@ -47,11 +47,13 @@ define('videosmodule.views.rail', [
 	VideoModule.prototype.render = function () {
 		var i,
 			videos = this.model.data.videos,
+			staffPickVideos = this.model.data.staff_videos,
 			len = videos.length,
 			thumbHtml = [],
 			self = this,
 			$imagesLoaded = $.Deferred(),
-			imgCount = 0;
+			imgCount = 0,
+			indexForStaffPick;
 
 		// If no videos are returned from the server, don't render anything
 		if (!len) {
@@ -63,6 +65,15 @@ define('videosmodule.views.rail', [
 				true
 			);
 			return;
+		}
+
+		this.shuffle(videos);
+		// If we have any staff pick videos, shuffle that list and display the first one
+		// in a random position in the Videos Module.
+		if (staffPickVideos.length) {
+			this.shuffle(staffPickVideos);
+			indexForStaffPick = Math.floor(Math.random() * this.numVids);
+			videos[indexForStaffPick] = staffPickVideos[0];
 		}
 
 		for (i = 0; i < this.numVids; i++) {
@@ -91,6 +102,23 @@ define('videosmodule.views.rail', [
 			});
 
 		track();
+	};
+
+	/**
+	 * Randomize array element order in-place.
+	 * Using Fisher-Yates shuffle algorithm.
+	 * Slightly adapted from http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+	 */
+	VideoModule.prototype.shuffle = function(array) {
+		var i, j, temp;
+
+		for (i = array.length - 1; i > 0; i--) {
+			j = Math.floor(Math.random() * (i + 1));
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		return array;
 	};
 
 	return VideoModule;
