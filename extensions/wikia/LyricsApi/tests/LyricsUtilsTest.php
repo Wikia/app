4b3c5cfb2ebf9f40159ee6d038e588d75ff637f7
Evgeniy Vasilev <aquilax@gmail.com>
2014-04-23 13:14:00 +0200

    Backported from dev Merge pull request #3441 from Wikia/LYR-133
    LYR-133: Fixed iTunes link/highlights; Added itunes for artist

--- extensions/wikia/LyricsApi/tests/LyricsUtilsTest.php
+++ extensions/wikia/LyricsApi/tests/LyricsUtilsTest.php
@@ -47,8 +47,8 @@ class LyricsUtilsTest extends WikiaBaseTest {
 	 *
 	 * @dataProvider generateITunesUrlDataProvider
 	 */
-	public function testGenerateITunesURL( $message, $field, $affToken, $expected ) {
-		$this->assertEquals( $expected, LyricsUtils::generateITunesUrl( $field, $affToken), $message );
+	public function testGenerateITunesURL( $message, $field, $linkType, $affToken, $expected ) {
+		$this->assertEquals( $expected, LyricsUtils::generateITunesUrl( $field, $linkType, $affToken), $message );
 	}
 
 	public function generateITunesUrlDataProvider() {
@@ -56,21 +56,66 @@ class LyricsUtilsTest extends WikiaBaseTest {
 			[
 				'empty field',
 				'',
+				LyricsUtils::TYPE_SONG,
 				'0',
 				false
 			],
 			[
 				'use default country',
 				'id292721355?i=292721406',
+				LyricsUtils::TYPE_SONG,
 				'30',
 				'http://itunes.apple.com/us/album/id292721355?i=292721406&at=30'
 			],
 			[
-				'use cusotom country',
+				'use custom country',
 				'id251299901?i=251299941&cc=gb',
+				LyricsUtils::TYPE_SONG,
 				'31',
 				'http://itunes.apple.com/gb/album/id251299901?i=251299941&at=31'
 			],
+			[
+				'wrong song link',
+				'23204023',
+				LyricsUtils::TYPE_SONG,
+				'32',
+				'http://itunes.apple.com/us/album/id23204023?at=32'
+			],
+			[
+				'artist link',
+				'23203991',
+				LyricsUtils::TYPE_ARTIST,
+				'33',
+				'http://itunes.apple.com/us/artist/id23203991?at=33'
+			],
+			[
+				'album link',
+				'id532966186',
+				LyricsUtils::TYPE_ALBUM,
+				'34',
+				'http://itunes.apple.com/us/album/id532966186?at=34'
+			],
+			[
+				'wrong album link',
+				'532966186',
+				LyricsUtils::TYPE_ALBUM,
+				'35',
+				'http://itunes.apple.com/us/album/id532966186?at=35'
+			],
+			[
+				'Finnish song',
+				'id255733668?i=255733670&cc=fi',
+				LyricsUtils::TYPE_SONG,
+				'36',
+				'http://itunes.apple.com/fi/album/id255733668?i=255733670&at=36'
+			],
+			[
+				'Wrong id finnish album',
+				'387912205&cc=fi',
+				LyricsUtils::TYPE_ALBUM,
+				'37',
+				'http://itunes.apple.com/fi/album/id387912205?at=37'
+			]
 		];
 	}
 }
