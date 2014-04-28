cc7316332eb2764bbe47b18ea0895d1377c9d245
Adam Robak <adamr@wikia-inc.com>
2014-04-28 14:26:22 +0200

    PLA-1354 added quality param handling

--- includes/wikia/api/MoviesApiController.class.php
+++ includes/wikia/api/MoviesApiController.class.php
@@ -13,8 +13,9 @@ class MoviesApiController extends WikiaApiController {
 	public function getMovie() {
 		$movieName = $this->getRequiredParam( 'movieName' );
 		$lang = $this->getRequest()->getVal( 'lang', static::LANG_SETTING );
+		$quality = $this->getRequest()->getVal( 'minArticleQuality', static::DEFAULT_MOVIE_QUALITY );
 
-		$result = $this->findMovie( $movieName, $lang );
+		$result = $this->findMovie( $movieName, $lang, $quality );
 
 		$response = $this->getResponse();
 		$response->setValues( $result );
@@ -22,7 +23,7 @@ class MoviesApiController extends WikiaApiController {
 		$response->setCacheValidity(self::RESPONSE_CACHE_VALIDITY);
 	}
 
-	protected function findMovie( $movieName, $lang, $minQuality = self::DEFAULT_MOVIE_QUALITY ) {
+	protected function findMovie( $movieName, $lang, $minQuality ) {
 		$tvs = $this->getTvSearchService();
 
 		$result = $tvs->queryMovie( $movieName, $lang, $tvs::MOVIE_TYPE, null, $minQuality );
