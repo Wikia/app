a89bba15c99723856a1687c1e68823d92342c9ac
nAndy <nandy@wikia-inc.com>
2014-04-29 10:53:20 +0000

    added docs and changed the code to be more readable

--- extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMapsHooks.class.php
+++ extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMapsHooks.class.php
@@ -1,11 +1,23 @@
 <?php
 class WikiaInteractiveMapsHooks {
 
+	/**
+	 * @desc Adds the JS asset to the bottom scripts
+	 *
+	 * @param $skin
+	 * @param String $text
+	 *
+	 * @return bool
+	 */
 	public static function onSkinAfterBottomScripts( $skin, &$text ) {
 		global $wgEnableWikiaInteractiveMaps, $wgExtensionsPath;
 
 		if( !empty( $wgEnableWikiaInteractiveMaps ) ) {
-			$text .= '<script src="' . $wgExtensionsPath . '/wikia/WikiaInteractiveMaps/js/WikiaInteractiveMaps.js" />';
+			$text .= sprintf(
+				'<script src="%s/%s"></script>',
+				$wgExtensionsPath,
+				'wikia/WikiaInteractiveMaps/js/WikiaInteractiveMaps.js'
+			);
 		}
 
 		return true;
