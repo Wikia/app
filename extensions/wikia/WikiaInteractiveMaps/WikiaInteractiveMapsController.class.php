3d0807c672c0c2a6798e62861ea7fff147149651
aquilax <evgeniy@wikia-inc.com>
2014-04-30 11:30:54 +0000

    Added single map page

--- extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMapsController.class.php
+++ extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMapsController.class.php
@@ -5,6 +5,8 @@
  */
 class WikiaInteractiveMapsController extends WikiaSpecialPageController {
 
+	const MAP_HEIGHT = 600;
+
 	/**
 	 * @desc Special page constructor
 	 *
@@ -26,6 +28,14 @@ class WikiaInteractiveMapsController extends WikiaSpecialPageController {
 		$this->wg->SuppressPageHeader = true;
 		$this->wg->out->setHTMLTitle( wfMessage( 'wikia-interactive-maps-title' )->escaped() );
 
+		if ( is_numeric( $this->getPar() ) ) {
+			$this->forward('WikiaInteractiveMaps', 'map' );
+		} else {
+			$this->forward('WikiaInteractiveMaps', 'main' );
+		}
+	}
+
+	public function main() {
 		$mapsModel = new WikiaMaps( $this->wg->IntMapConfig );
 		$params = [
 			'city_id' => $this->app->wg->CityId
@@ -35,6 +45,10 @@ class WikiaInteractiveMapsController extends WikiaSpecialPageController {
 
 		$this->setVal( 'maps', $maps );
 		$this->setVal( 'hasMaps', !empty( $maps ) );
+
+		$url = $this->getContext()->getTitle()->getFullURL();
+
+		$this->setVal( 'base_url', $url );
 		$messages = [
 			'wikia-interactive-maps-title' => wfMessage( 'wikia-interactive-maps-title' ),
 			'wikia-interactive-maps-create-a-map' => wfMessage( 'wikia-interactive-maps-create-a-map' ),
@@ -43,6 +57,36 @@ class WikiaInteractiveMapsController extends WikiaSpecialPageController {
 		$this->setVal( 'messages', $messages );
 
 		$this->response->addAsset( 'extensions/wikia/WikiaInteractiveMaps/css/WikiaInteractiveMaps.scss' );
+		$this->response->setTemplateEngine( WikiaResponse::TEMPLATE_ENGINE_MUSTACHE );
+	}
+
+	public function map() {
+		$mapId = (int)$this->getPar();
+		$zoom = $this->request->getInt('zoom', 0);
+		$lat = $this->request->getInt('lat', 0);
+		$lon = $this->request->getInt('lon', 0);
+
+		$mapsModel = new WikiaMaps( $this->wg->IntMapConfig );
+		$map = $mapsModel->cachedRequest(
+			'getMapByIdFromApi',
+			[ 'id' => $mapId ]
+		);
+		if ( isset( $map->title ) ) {
+			$this->wg->out->setHTMLTitle( $map->title );
+
+			$url = $mapsModel->getMapRenderUrl( [ implode( '/', [ $mapId, $zoom, $lat, $lon ] ) ] );
+
+			$this->setVal( 'title', $map->title );
+			$this->setVal( 'map_found', true );
+			$this->setVal( 'url', $url );
+			$this->setVal( 'height', self::MAP_HEIGHT );
+		} else {
+			$this->setVal( 'map_found', false );
+			$this->setVal( 'title', wfMessage( 'error' ) );
+			$this->setVal( 'messages', [
+				'wikia-interactive-maps-map-not-found-error' => wfMessage( 'wikia-interactive-maps-map-not-found-error' )
+			] );
+		}
 		$this->response->setTemplateEngine( WikiaResponse::TEMPLATE_ENGINE_MUSTACHE );
 	}
 
