cbd54cce351280bbd8e51869049d6b78ce0903ff
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-04-30 12:45:26 +0200

    fix wikia terms

--- extensions/wikia/UserLogin/templates/UserSignupSpecial_WikiaMobileSubmit.php
+++ extensions/wikia/UserLogin/templates/UserSignupSpecial_WikiaMobileSubmit.php
@@ -2,6 +2,6 @@
 <p class="wikia_terms">
 	<?= wfMessage('prefs-help-terms')->parse() ?>
 </p>
-<p class="wikia-terms">
+<p class="wikia_terms">
 	<?= wfMessage('wikiamobile-go-to-login')->parse() ?>
 </p>
