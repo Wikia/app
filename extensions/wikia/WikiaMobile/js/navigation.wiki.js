8d24289254e8802ed58eec87ba3549ddf2eea3ad
lizlux <liz@lizlux.net>
2014-05-06 15:22:03 -0700

    VID-1645 js-beautify

--- extensions/wikia/WikiaMobile/js/navigation.wiki.js
+++ extensions/wikia/WikiaMobile/js/navigation.wiki.js
@@ -1,5 +1,5 @@
-require( ['wikia.window', 'wikia.nirvana', 'track', 'wikia.cache'],
-	function ( window, nirvana, track, cache ) {
+require(['wikia.window', 'wikia.nirvana', 'track', 'wikia.cache'],
+	function (window, nirvana, track, cache) {
 		'use strict';
 
 		var d = window.document,
@@ -10,133 +10,133 @@ require( ['wikia.window', 'wikia.nirvana', 'track', 'wikia.cache'],
 			lvl2Link,
 			ANIMATION_TIME = 250;
 
-		$( d ).on( 'nav:open',function () {
-			trackLevel( 1 );
-		} ).on( 'topbar:close nav:close', function () {
-			if ( $wkNavMenu ) {
-				$wkNavMenu.removeClass( 'cur2 cur3' );
+		$(d).on('nav:open', function () {
+			trackLevel(1);
+		}).on('topbar:close nav:close', function () {
+			if ($wkNavMenu) {
+				$wkNavMenu.removeClass('cur2 cur3');
 			}
-		} );
+		});
 
-		function trackLevel ( level ) {
-			track.event( 'wikinav', track.CLICK, {
+		function trackLevel(level) {
+			track.event('wikinav', track.CLICK, {
 				label: 'level-' + level
-			} );
+			});
 		}
 
-		function setup ( navHtml ) {
-			if ( navHtml ) {
-				$wkNavMenu = $( '#wkNav' ).html( navHtml ).find( '#wkNavMenu' );
+		function setup(navHtml) {
+			if (navHtml) {
+				$wkNavMenu = $('#wkNav').html(navHtml).find('#wkNavMenu');
 
-				$wikiNavHeader = $wkNavMenu.find( 'header' );
-				$wikiNavH1 = $wikiNavHeader.find( 'h1' ).removeClass();
-				$wikiNavLink = $( d.getElementById( 'wkNavLink' ) );
+				$wikiNavHeader = $wkNavMenu.find('header');
+				$wikiNavH1 = $wikiNavHeader.find('h1').removeClass();
+				$wikiNavLink = $(d.getElementById('wkNavLink'));
 
 				//add chevrons to all elements that have child lists
-				$wkNavMenu.find( 'ul ul' ).parent().addClass( 'cld' );
+				$wkNavMenu.find('ul ul').parent().addClass('cld');
 
-				$wkNavMenu.on( 'click', 'li',function ( event ) {
-					var $this = $( this ),
+				$wkNavMenu.on('click', 'li', function (event) {
+					var $this = $(this),
 						$element = $this.children().first(),
-						hasChildren = $this.hasClass( 'cld' ),
-						href = $element.attr( 'href' ),
+						hasChildren = $this.hasClass('cld'),
+						href = $element.attr('href'),
 						$ul;
 
 					event.stopPropagation();
 
 					// track clicks on special videos nav link
-					if ( $element.hasClass( 'video-link' ) ) {
-						track.event( 'wikinav', track.CLICK, {
+					if ($element.hasClass('video-link')) {
+						track.event('wikinav', track.CLICK, {
 							label: 'special-videos'
 						});
 					}
 
-					if ( hasChildren ) {
-						$ul = $this.find( 'ul' ).first().addClass( 'cur' );
+					if (hasChildren) {
+						$ul = $this.find('ul').first().addClass('cur');
 
-						handleHeaderLink( href );
+						handleHeaderLink(href);
 
-						if ( $wkNavMenu.hasClass( 'cur2' ) ) {
-							trackLevel( 3 );
+						if ($wkNavMenu.hasClass('cur2')) {
+							trackLevel(3);
 
-							$wkNavMenu.addClass( 'cur3' );
-							$wikiNavH1.addClass( 'animNext' );
+							$wkNavMenu.addClass('cur3');
+							$wikiNavH1.addClass('animNext');
 
-							window.setTimeout( function () {
-								$ul.addClass( 'anim-done' );
-								$wikiNavLink.text( $element.text() );
-							}, ANIMATION_TIME );
+							window.setTimeout(function () {
+								$ul.addClass('anim-done');
+								$wikiNavLink.text($element.text());
+							}, ANIMATION_TIME);
 
 						} else {
-							trackLevel( 2 );
+							trackLevel(2);
 
-							$wikiNavLink.text( $element.text() );
+							$wikiNavLink.text($element.text());
 							lvl2Link = href;
 
-							$wkNavMenu.addClass( 'cur2' );
-							$wikiNavH1.removeClass().addClass( 'anim' );
+							$wkNavMenu.addClass('cur2');
+							$wikiNavH1.removeClass().addClass('anim');
 
-							window.setTimeout( function () {
-								$ul.addClass( 'anim-done' );
-							}, ANIMATION_TIME );
+							window.setTimeout(function () {
+								$ul.addClass('anim-done');
+							}, ANIMATION_TIME);
 						}
 					}
-				} ).on( 'click', '#wkNavBack', function () {
-					if ( $wkNavMenu.hasClass( 'cur3' ) ) {
-						trackLevel( 2 );
+				}).on('click', '#wkNavBack', function () {
+					if ($wkNavMenu.hasClass('cur3')) {
+						trackLevel(2);
 
-						$wikiNavH1.removeClass().addClass( 'animBack' );
+						$wikiNavH1.removeClass().addClass('animBack');
 
-						handleHeaderLink( lvl2Link );
+						handleHeaderLink(lvl2Link);
 
-						$wkNavMenu.removeClass( 'cur3' );
+						$wkNavMenu.removeClass('cur3');
 
-						window.setTimeout( function () {
-							$wikiNavLink.text( $wkNavMenu.find( '.lvl2.cur' ).prev().text() );
-						}, ANIMATION_TIME );
+						window.setTimeout(function () {
+							$wikiNavLink.text($wkNavMenu.find('.lvl2.cur').prev().text());
+						}, ANIMATION_TIME);
 
-						window.setTimeout( function () {
-							$wkNavMenu.find( '.lvl3.cur' ).removeClass( 'cur anim-done' );
+						window.setTimeout(function () {
+							$wkNavMenu.find('.lvl3.cur').removeClass('cur anim-done');
 							$wikiNavH1.removeClass();
-						}, ANIMATION_TIME * 2 );
+						}, ANIMATION_TIME * 2);
 					} else {
-						trackLevel( 1 );
+						trackLevel(1);
 
-						$wkNavMenu.removeClass( 'cur2' );
-						$wikiNavH1.removeClass().addClass( 'animBack' );
+						$wkNavMenu.removeClass('cur2');
+						$wikiNavH1.removeClass().addClass('animBack');
 
-						window.setTimeout( function () {
-							$wkNavMenu.find( '.lvl2.cur' ).removeClass( 'cur anim-done' );
-						}, ANIMATION_TIME * 2 );
+						window.setTimeout(function () {
+							$wkNavMenu.find('.lvl2.cur').removeClass('cur anim-done');
+						}, ANIMATION_TIME * 2);
 					}
-				} );
+				});
 
-				$wikiNavLink.on( 'click', function () {
-					track.event( 'wikinav', track.CLICK, {
-						label: 'header-' + $wkNavMenu[0].className.slice( 3 )
-					} );
-				} );
+				$wikiNavLink.on('click', function () {
+					track.event('wikinav', track.CLICK, {
+						label: 'header-' + $wkNavMenu[0].className.slice(3)
+					});
+				});
 			}
 		}
 
-		function handleHeaderLink ( link ) {
-			if ( link ) {
-				$wikiNavLink.attr( 'href', link ).show();
+		function handleHeaderLink(link) {
+			if (link) {
+				$wikiNavLink.attr('href', link).show();
 			} else {
 				$wikiNavLink.hide();
 			}
 		}
 
-		$( function () {
+		$(function () {
 			var CACHE_KEY = 'wkNavigation';
 
-			setTimeout( function () {
-				var navHtml = cache.getVersioned( CACHE_KEY );
+			setTimeout(function () {
+				var navHtml = cache.getVersioned(CACHE_KEY);
 
-				if ( navHtml ) {
-					setup( navHtml );
+				if (navHtml) {
+					setup(navHtml);
 				} else {
-					nirvana.sendRequest( {
+					nirvana.sendRequest({
 						type: 'GET',
 						controller: 'WikiaMobileController',
 						method: 'getNavigation',
@@ -144,13 +144,13 @@ require( ['wikia.window', 'wikia.nirvana', 'track', 'wikia.cache'],
 						data: {
 							lang: window.wgUserLanguage
 						}
-					} ).done( function ( navHtml ) {
-						cache.setVersioned( CACHE_KEY, navHtml, 10800 );
+					}).done(function (navHtml) {
+						cache.setVersioned(CACHE_KEY, navHtml, 10800);
 
-						setup( navHtml );
-					} );
+						setup(navHtml);
+					});
 				}
-			}, 200 ); //do this on load but give browser some time to finish
-		} );
+			}, 200); //do this on load but give browser some time to finish
+		});
 	}
 );
