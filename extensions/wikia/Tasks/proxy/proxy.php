203ced8863d398a5bcd32afb5e6b70d21914f0f0
nelson <nelson@wikia-inc.com>
2014-05-06 13:29:37 -0700

    escaping shell args

--- extensions/wikia/Tasks/proxy/proxy.php
+++ extensions/wikia/Tasks/proxy/proxy.php
@@ -3,10 +3,10 @@ set_time_limit(0);
 
 $script = dirname(__FILE__).'/../../../../maintenance/wikia/task_runner.php';
 
-$wikiId = $_POST['wiki_id'];
+$wikiId = escapeshellarg($_POST['wiki_id']);
 $list = $_POST['task_list'];
 $order = $_POST['call_order'];
-$createdBy = $_POST['created_by'];
+$createdBy = escapeshellarg($_POST['created_by']);
 
 $command = "SERVER_ID=$wikiId php $script --task_list=".json_encode($list)." --call_order=".json_encode($order)." --created_by=$createdBy";
 
