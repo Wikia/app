3561b9e7e1fe9d1a96d7ca4d896ccc302637567b
VForge <bartosz.bentkowski@gmail.com>
2014-05-08 14:07:00 +0200

    CON-1073 | First release

--- skins/oasis/js/WikiHeaderAB.js
+++ skins/oasis/js/WikiHeaderAB.js
@@ -1,41 +1,43 @@
-$(function() {
+$(function () {
 	'use strict';
 	var navigation;
-	navigation = $( '#WikiHeader' ).detach();
-	navigation.find( '.buttons' ).hide();
-	navigation.find( '.WikiHeaderSearch' ).hide();
-	navigation.removeClass( 'WikiHeader' ).addClass( 'WikiHeaderV2' );
-	navigation.find( '.WikiNav' ).removeClass( 'WikiNav' ).addClass( 'WikiNavV2' );
-	navigation.find( '.accent' ).removeClass( 'accent' );
-	navigation.find( '.marked' ).removeClass( 'marked' );
-	navigation.find( '.chevron' ).remove();
+	navigation = $('#WikiHeader').detach();
+	navigation.find('.buttons').hide();
+	navigation.find('.WikiHeaderSearch').hide();
+	navigation.removeClass('WikiHeader').addClass('WikiHeaderV2');
+	navigation.find('.WikiNav').removeClass('WikiNav').addClass('WikiNavV2');
+	navigation.find('.accent').removeClass('accent');
+	navigation.find('.marked').removeClass('marked');
+	navigation.find('.chevron').remove();
 
-	navigation.find( '> nav' ).unbind();
+	navigation.find('> nav').unbind();
 
-	navigation.find( '.nav-item' ).each(function() {
-		var elem = $( this ),
-			seeAll = elem.find( '> a' ).clone(),
-			elem2 = elem.find( '.subnav-2'),
-			size = elem2.first().children().length;
+	navigation.find('.nav-item').each(function () {
+		var elem = $(this),
+			seeAll = elem.find('> a').clone(),
+			elem2 = elem.find('.subnav-2'),
+			length = elem.find('.subnav-2 > li').length,
+			sizeClass = '';
 
-			if (size === 3) {
-				size = 'submenu-wide';
-			} else if (size > 3) {
-				size = 'submenu-full';
-			} else {
-				size = '';
-			}
-			elem2.wrap('<section class="submenu ' + size + '"></section>');
-			elem2.append($('<div class="clearfix"></div>'));
-			// TODO i18n
-			elem2.append( seeAll.text( 'See all in ' + seeAll.text() ).wrap('<section class="see-all"></section>').parent() );
+		if (length === 3) {
+			sizeClass = 'submenu-wide';
+		} else if (length > 3) {
+			sizeClass = 'submenu-full';
+		} else {
+			sizeClass = '';
+		}
+		elem2.wrap('<section class="submenu ' + sizeClass + '"></section>');
+		elem2.append($('<div class="clearfix"></div>'));
+		// TODO i18n
+		elem2.append(seeAll.text('See all in ' + seeAll.text()).wrap('<section class="see-all"></section>').parent());
 	});
 
 	navigation.children().wrapAll('<section class="local-navigation-container"></section>');
 
-	navigation.insertAfter( '#WikiaHeader' );
+	navigation.insertAfter('#WikiaHeader');
 
-	$('.nav-item').click(function(e){
+	// temp
+	$('.nav-item').click(function (e) {
 		e.preventDefault();
 
 		$(this).toggleClass('active');
