da31bada1816f5b463e333e62f74ef53fd4fdbdf
macbre <maciej.brencz@gmail.com>
2014-05-12 17:43:36 +0200

    InstantGlobals: initial commit

---
+++ extensions/wikia/InstantGlobals/InstantGlobals.setup.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This extension emits WikiFactory variables as JS globals.
+ *
+ * Variables are exposed via ResourceLoader module with a short CDN caching time, allowing us to make critial config
+ * changes with no need to wait 24h for CDN cache to invalidate.
+ *
+ * @author macbre
+ */
+
+/**
+ * info
+ */
+$wgExtensionCredits['other'][] = [
+	'name' => 'InstantGlobals',
+	'author' => 'Maciej Brencz',
+	'version' => '1.0'
+];
+
+/**
+ * classes
+ */
+$dir = __DIR__;
+
+$wgAutoloadClasses['InstantGlobalsHooks'] = "{$dir}/InstantGlobalsHooks.class.php";
+$wgAutoloadClasses['InstantGlobalsModule'] = "{$dir}/InstantGlobalsModule.class.php";
+
+/**
+ * hooks
+ */
+$wgHooks['WikiaSkinTopShortTTLModules'][] =  'InstantGlobalsHooks::onWikiaSkinTopShortTTLModules';
+
+/**
+ * register Resource Loader module
+ */
+$wgResourceModules['wikia.ext.instantglobals'] = [
+	'class' => 'InstantGlobalsModule',
+];
