5719b627d8a15f1afa6cb64eae5e8e48bb43ef22
lizlux <liz@lizlux.net>
2014-05-13 18:40:03 -0700

    js-beautify

--- extensions/wikia/EditPageLayout/js/extras/Buttons.js
+++ extensions/wikia/EditPageLayout/js/extras/Buttons.js
@@ -1,12 +1,12 @@
-/* global WikiaEditor, WMU_show, WikiaPhotoGallery, insertTags, skin, wmu_imagebutton */
-(function(window, $) {
+/* global WikiaEditor, WMU_show, WikiaPhotoGallery, insertTags, wmu_imagebutton */
+(function (window, $) {
 	'use strict';
 
 	var buttons = window.wgEditorExtraButtons,
 		extensionsPath = window.wgExtensionsPath,
 		mediawikiButtons = window.mwCustomEditButtons;
 
-	if ( !buttons ) {
+	if (!buttons) {
 		buttons = window.wgEditorExtraButtons = {};
 	}
 
@@ -22,27 +22,27 @@
 	 * Editor right rail buttons
 	 */
 
-	buttons['InsertImage'] = {
+	buttons.InsertImage = {
 		type: 'button',
 		labelId: 'wikia-editor-media-image',
 		titleId: 'wikia-editor-media-image-tooltip',
 		className: 'RTEImageButton',
 		forceLogin: true,
-		clicksource: function() {
-			WikiaEditor.load( 'WikiaMiniUpload' ).done(function() {
+		clicksource: function () {
+			WikiaEditor.load('WikiaMiniUpload').done(function () {
 				WMU_show({}); // jshint ignore:line
 			});
 		},
 		ckcommand: 'addimage'
 	};
 
-	buttons['InsertGallery'] = {
+	buttons.InsertGallery = {
 		type: 'button',
 		labelId: 'wikia-editor-media-gallery',
 		titleId: 'wikia-editor-media-gallery-tooltip',
 		className: 'RTEGalleryButton',
 		forceLogin: true,
-		clicksource: function() {
+		clicksource: function () {
 			WikiaPhotoGallery.showEditor({
 				from: 'source',
 				type: WikiaPhotoGallery.TYPE_GALLERY
@@ -58,7 +58,7 @@
 		titleId: 'wikia-editor-media-slideshow-tooltip',
 		className: 'RTESlideshowButton',
 		forceLogin: true,
-		clicksource: function() {
+		clicksource: function () {
 			WikiaPhotoGallery.showEditor({
 				from: 'source',
 				type: WikiaPhotoGallery.TYPE_SLIDESHOW
@@ -74,7 +74,7 @@
 		titleId: 'wikia-editor-media-slider-tooltip',
 		className: 'RTESliderButton',
 		forceLogin: true,
-		clicksource: function() {
+		clicksource: function () {
 			WikiaPhotoGallery.showEditor({
 				from: 'source',
 				type: WikiaPhotoGallery.TYPE_SLIDER
@@ -84,15 +84,15 @@
 		precondition: checkGallery
 	};
 
-	if( window.wgEnableVideoToolExt ) {
+	if (window.wgEnableVideoToolExt) {
 		buttons.InsertVideo = {
 			type: 'button',
 			labelId: 'wikia-editor-media-video',
 			titleId: 'wikia-editor-media-video-tooltip',
 			className: 'RTEVideoButton',
 			forceLogin: true,
-			clicksource: function() {
-				WikiaEditor.load( 'VideoEmbedTool' ).done(function() {
+			clicksource: function () {
+				WikiaEditor.load('VideoEmbedTool').done(function () {
 					window.vetWikiaEditor({
 						target: {
 							id: 'mw-editbutton-vet'
@@ -109,8 +109,8 @@
 		labelId: 'wikia-editor-source-bold',
 		titleId: 'wikia-editor-source-bold-tooltip',
 		className: 'cke_button_bold',
-		clicksource: function() {
-			insertTags( "'''", "'''", 'Bold text', getTextarea());
+		clicksource: function () {
+			insertTags("'''", "'''", 'Bold text', getTextarea());
 		}
 	};
 
@@ -119,8 +119,8 @@
 		labelId: 'wikia-editor-source-italic',
 		titleId: 'wikia-editor-source-italic-tooltip',
 		className: 'cke_button_italic',
-		clicksource: function() {
-			insertTags( "''", "''", 'Italic text', getTextarea());
+		clicksource: function () {
+			insertTags("''", "''", 'Italic text', getTextarea());
 		}
 	};
 
@@ -129,8 +129,8 @@
 		labelId: 'wikia-editor-source-link',
 		titleId: 'wikia-editor-source-link-tooltip',
 		className: 'cke_button_link',
-		clicksource: function() {
-			insertTags( '[[', ']]', 'Link title', getTextarea());
+		clicksource: function () {
+			insertTags('[[', ']]', 'Link title', getTextarea());
 		}
 	};
 
@@ -138,26 +138,26 @@
 	 * Mediawiki toolbar buttons (Oasis skin only)
 	 */
 
-	if ( skin === 'oasis' && mediawikiButtons ) {
-		if ( window.wgEnableWikiaMiniUploadExt ) {
+	if (window.skin === 'oasis' && mediawikiButtons) {
+		if (window.wgEnableWikiaMiniUploadExt) {
 			mediawikiButtons.push({
 				imageFile: extensionsPath + '/wikia/WikiaMiniUpload/images/button_wmu.png',
 				speedTip: wmu_imagebutton, // jshint ignore:line
 				imageId: 'mw-editbutton-wmu',
-				onclick: function( event ) {
-					WikiaEditor.load( 'WikiaMiniUpload' ).done(function() {
-						WMU_show( event ); // jshint ignore:line
+				onclick: function (event) {
+					WikiaEditor.load('WikiaMiniUpload').done(function () {
+						WMU_show(event); // jshint ignore:line
 					});
 				}
 			});
 		}
 
-		if ( window.wgEnableWikiaPhotoGalleryExt ) {
+		if (window.wgEnableWikiaPhotoGalleryExt) {
 			mediawikiButtons.push({
 				imageFile: extensionsPath + '/wikia/WikiaPhotoGallery/images/gallery_add.png',
 				speedTip: window.WikiaPhotoGalleryAddGallery,
 				imageId: 'mw-editbutton-wpg',
-				onclick: function() {
+				onclick: function () {
 					WikiaPhotoGallery.showEditor({
 						from: 'source'
 					});
@@ -166,14 +166,14 @@
 		}
 
 		// Check to see if wiki allows embedding videos, as well as checking user permissions
-		if ( window.wgEnableVideoToolExt && window.showAddVideoBtn) {
+		if (window.wgEnableVideoToolExt && window.showAddVideoBtn) {
 			mediawikiButtons.push({
 				imageFile: extensionsPath + '/wikia/VideoEmbedTool/images/button_vet.png',
 				speedTip: $.msg('vet-imagebutton'),
 				imageId: 'mw-editbutton-vet',
-				onclick: function( event ) {
-					WikiaEditor.load( 'VideoEmbedTool' ).done(function() {
-						window.vetWikiaEditor( event );
+				onclick: function (event) {
+					WikiaEditor.load('VideoEmbedTool').done(function () {
+						window.vetWikiaEditor(event);
 					});
 				}
 			});
