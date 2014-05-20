2c47400eddd33bd7243f7562a58af3b609f1103e
Piotr Gabryjeluk <rychu@wikia-inc.com>
2014-05-20 11:36:01 +0200

    InstantGlobals: allow overriding the values by a wg variable (useful for devbox)

--- extensions/wikia/InstantGlobals/InstantGlobalsModule.class.php
+++ extensions/wikia/InstantGlobals/InstantGlobalsModule.class.php
@@ -14,14 +14,27 @@ class InstantGlobalsModule extends ResourceLoaderModule {
 	 * @return object key / value list variables
 	 */
 	private function getVariablesValues() {
+		global $wgInstantGlobalsOverride;
+
+		if (!empty($wgInstantGlobalsOverride) && is_array($wgInstantGlobalsOverride)) {
+			$override = $wgInstantGlobalsOverride;
+		} else {
+			$override = [];
+		}
+
 		$ret = [];
 
 		$variables = [];
 
-		wfRunHooks( 'InstantGlobalsGetVariables', [ &$variables ] );
+		wfRunHooks('InstantGlobalsGetVariables', [&$variables]);
 
 		foreach($variables as $name) {
-			$value = WikiFactory::getVarValueByName($name, Wikia::COMMUNITY_WIKI_ID);
+			// Use the value on community but override with the $wgInstantGlobalsOverride
+			if (array_key_exists($override, $name)) {
+				$value = $override[$name];
+			} else {
+				$value = WikiFactory::getVarValueByName($name, Wikia::COMMUNITY_WIKI_ID);
+			}
 
 			// don't emit "falsy" values
 			if (!empty($value)) {
