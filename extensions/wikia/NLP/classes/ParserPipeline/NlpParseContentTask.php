c27c0dd68e37d78fa9a11b94a7cf07b565497123
Robert Elwell <robert@wikia-inc.com>
2014-05-21 17:27:06 +0000

    override method exists check

--- extensions/wikia/NLP/classes/ParserPipeline/NlpParseContentTask.php
+++ extensions/wikia/NLP/classes/ParserPipeline/NlpParseContentTask.php
@@ -23,4 +23,20 @@ class NlpParseContentTask extends BaseTask
 		$this->document_id = sprintf( '%d_%d', $title->getArticleId(), $wgCityId );
 	}
 
+	/**
+	 * set this task to call a method in this class. the first argument to this method should be the method to execute,
+	 * and subsequent arguments are arguments passed to that method. Example: call('add', 2, 3) would call the method
+	 * add with 2 and 3 as parameters. we're adding this to the current class to override the method existing herein.
+	 *
+	 * @return array [$this, order in which this call should be made]
+	 */
+	public function call(/** method, arg1, arg2, ...argN */) {
+		$args = func_get_args();
+		$method = array_shift($args);
+
+		$this->calls []= [$method, $args];
+
+		return [$this, count($this->calls) - 1];
+	}
+
 }
