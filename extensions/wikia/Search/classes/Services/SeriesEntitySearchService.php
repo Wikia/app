94453f4db264b5db35595231285a8bf8248c0f87
Adam Robak <adamr@wikia-inc.com>
2014-05-23 21:10:00 +0200

    xwiki score

--- extensions/wikia/Search/classes/Services/SeriesEntitySearchService.php
+++ extensions/wikia/Search/classes/Services/SeriesEntitySearchService.php
@@ -6,8 +6,8 @@ class SeriesEntitySearchService extends EntitySearchService {
 
 	const XWIKI_CORE = 'xwiki';
 	const WIKI_LIMIT = 1;
-	const MINIMAL_WIKIA_ARTICLES = 30;
-	const MINIMAL_WIKIA_SCORE = 2;
+	const MINIMAL_WIKIA_ARTICLES = 20;
+	const MINIMAL_WIKIA_SCORE = 0.75;
 	const DEFAULT_SLOP = 1;
 
 	private static $EXCLUDED_WIKIS = [ '*fanon.wikia.com', '*answers.wikia.com' ];
