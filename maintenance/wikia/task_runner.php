87560902f7b2afeb9a510de0585afd5f35768f9f
Scott Rabin <srabin@wikia-inc.com>
2014-05-28 23:02:33 +0000

    PLATFORM-75 Use GuzzleHttp for REST calls
    An easy-to-use/robust HTTP client will be useful at the platform moves
    to a service oriented architecture. Use GuzzleHttp for the Flower
    long-task pingback.

--- maintenance/wikia/task_runner.php
+++ maintenance/wikia/task_runner.php
@@ -12,30 +12,20 @@ require_once(__DIR__."/../commandLine.inc");
 $runner = new TaskRunner($options['task_id'], $options['task_list'], $options['call_order'], $options['created_by']);
 
 ob_start();
-
 $runner->run();
 $result = $runner->format();
 
 // for long-running tasks that end up timing out, we need to notify flower that
 // the task actually did complete successfully
-$postData = array(
-	'kwargs' => array(
-		'state' => $result->status,
-		'completed' => time(),
-	),
-);
-if ($result->status == 'success') {
-	$postData['kwargs']['result'] = $result->retval;
-} else {
-	$postData['kwargs']['result'] = $result->reason;
-}
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "{$wgFlowerUrl}/api/task/status/{$options['task_id']}");
-curl_setopt($ch, CURLOPT_POST, count($postData));
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
-
-curl_exec($ch);
-curl_close($ch);
+GuzzleHttp\post("{$wgFlowerUrl}/api/task/status/{$options['task_id']}", [
+	'body' => json_encode([
+		'kwargs' => [
+			'completed' => time(),
+			'state' => $result->status,
+			'result' => ($result->status == 'success' ? $result->retval : $result->reason),
+		],
+	]),
+]);
 ob_end_clean();
 
 // if the runner completes in time, this will report back to celery immediately
