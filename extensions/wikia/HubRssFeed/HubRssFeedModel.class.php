074ba8ae859cbc9ecb2cd3c2ddde9130e5731412
Krzysztof 'suchy' Suchanek <suchy@wikia-inc.com>
2014-05-28 10:56:47 +0200

    PLA-1522 added fake sorting when more than one result with the same timestamp

--- extensions/wikia/HubRssFeed/HubRssFeedModel.class.php
+++ extensions/wikia/HubRssFeed/HubRssFeedModel.class.php
@@ -132,8 +132,21 @@ class HubRssFeedModel extends WikiaModel {
 		}
 
 		array_multisort($timestamps, SORT_DESC, $currentData);
+		$currentData = $this->fakeOrderItems($currentData);
 
+		return $currentData;
+	}
 
+	protected function fakeOrderItems( $currentData ){
+		$prevTimestamp = 0;
+		foreach($currentData as &$item){
+			if($prevTimestamp === $item['timestamp']){
+				$item['timestamp'] --;
+			}elseif($prevTimestamp > 0 && $item['timestamp'] > $prevTimestamp){
+				$item['timestamp'] = $prevTimestamp - 1;
+			}
+			$prevTimestamp = $item['timestamp'];
+		}
 		return $currentData;
 	}
 
@@ -199,7 +212,7 @@ class HubRssFeedModel extends WikiaModel {
 
 		array_multisort($timestamps, SORT_DESC, $currentData);
 
-
+		$currentData = $this->fakeOrderItems($currentData);
 		return $currentData;
 
 	}
