64b8194315669065b6fd538b1c6ee61dff1d63a9
nelson <nelson@wikia-inc.com>
2014-05-30 16:01:23 -0700

    removing guzzles for realsies

--- lib/composer/autoload.php
+++ lib/composer/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd7af4d449b53f0515795877540669137::getLoader();
+return ComposerAutoloaderInit3b6ad7b24506fb776e4fcc1977a97cdc::getLoader();
