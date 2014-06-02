497a309862043a852116051c58979811f754cd64
nelson <nelson@wikia-inc.com>
2014-06-02 15:58:12 -0700

    change to use the same instantiation method

--- maintenance/wikia/fixes/fixVisualizationImage.php
+++ maintenance/wikia/fixes/fixVisualizationImage.php
@@ -54,9 +54,11 @@ class FixVisualizationImage extends Maintenance {
 					if ($file && $file->isMissing()) {
 
 						$t = GlobalTitle::newFromText("Wikia-Visualization-Add-$imageIndex.png", NS_FILE, $row['city_id']);
-						$task = TaskRunner::isModern('PromoteImageReviewTask') ?
-							new \Wikia\Tasks\Tasks\PromoteImageReviewTask() :
-							new PromoteImageReviewTask();
+						if (TaskRunner::isModern('PromoteImageReviewTask')) {
+							$task = new \Wikia\Tasks\Tasks\PromoteImageReviewTask();
+						} else {
+							$task = new PromoteImageReviewTask();
+						}
 
 						var_dump($t->getArticleID(), "Wikia-Visualization-Add-$imageIndex.png", $app->wg->cityId, $row['city_id']);
 						$res = $task->uploadSingleImage($t->getArticleID(), "Wikia-Visualization-Add-$imageIndex.png", $app->wg->cityId, $row['city_id']);
