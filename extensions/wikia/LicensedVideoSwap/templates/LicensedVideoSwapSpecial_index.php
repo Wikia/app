d03acac9b51ade64cbd7a244311e52f282216399
lizlux <liz@lizlux.net>
2014-06-03 15:55:45 -0700

    VID-1743 hiding LVS to non-staff for now

--- extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_index.php
+++ extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_index.php
@@ -1,28 +1,50 @@
-<? global $wgStylePath ?>
+<?
+/**
+ * @var $wgUser
+ * @var $pagination
+ * @var $app
+ * @var $videoList
+ * @var $totalVideos
+ * @var $thumbWidth
+ * @var $thumbHeight
+ */
 
-<? if ( !empty( $videoList ) ): ?>
-	<?= $app->renderPartial( 'LicensedVideoSwapSpecialController', 'callout' ) ?>
-<? endif; ?>
+?>
 
-<div class="lvs-instructions">
-	<h2><?= wfMessage( 'lvs-instructions-header' )->parse() ?></h2>
-	<p><?= wfMessage( 'lvs-instructions' )->parse() ?></p>
-</div>
+<h2><?= wfMessage( 'lvs-maintenance-header' )->plain() ?></h2>
+<p><?= wfMessage( 'lvs-maintenance-body' )->plain() ?></p>
 
-<section class="lvs-match-stats">
-	<div class="count"><?= $totalVideos ?></div>
-	<div class="description"><?= wfMessage( 'lvs-match-stats-description' )->plain() ?></div>
-</section>
 
-<? if ( !empty( $videoList ) ): ?>
+<?
+	// Temporary rollout of new LVS thumbnails per VID-1743
+	$isStaff = in_array( 'staff', $app->wg->User->getEffectiveGroups() );
+?>
 
-	<div class="WikiaGrid LVSGrid" id="LVSGrid">
+<? if ( $isStaff ): ?>
+	<? if ( !empty( $videoList ) ): ?>
+		<?= $app->renderPartial( 'LicensedVideoSwapSpecialController', 'callout' ) ?>
+	<? endif; ?>
 
-		<?= $app->renderPartial( 'LicensedVideoSwapSpecial', 'row', array( 'videoList' => $videoList, 'thumbWidth' => $thumbWidth, 'thumbHeight' => $thumbHeight ) ) ?>
-		<?= $pagination ?>
-
+	<div class="lvs-instructions">
+		<h2><?= wfMessage( 'lvs-instructions-header' )->parse() ?></h2>
+		<p><?= wfMessage( 'lvs-instructions' )->parse() ?></p>
 	</div>
 
-<? else: ?>
-	<p class="lvs-zero-state"><?= wfMessage( 'lvs-zero-state' )->plain() ?></p>
+	<section class="lvs-match-stats">
+		<div class="count"><?= $totalVideos ?></div>
+		<div class="description"><?= wfMessage( 'lvs-match-stats-description' )->plain() ?></div>
+	</section>
+
+	<? if ( !empty( $videoList ) ): ?>
+
+		<div class="WikiaGrid LVSGrid" id="LVSGrid">
+
+			<?= $app->renderPartial( 'LicensedVideoSwapSpecial', 'row', array( 'videoList' => $videoList, 'thumbWidth' => $thumbWidth, 'thumbHeight' => $thumbHeight ) ) ?>
+			<?= $pagination ?>
+
+		</div>
+
+	<? else: ?>
+		<p class="lvs-zero-state"><?= wfMessage( 'lvs-zero-state' )->plain() ?></p>
+	<? endif; ?>
 <? endif; ?>
