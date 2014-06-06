29947293c7c6f51e7e1cd009c3b30682fd4ddf83
bkoval <brt.kowal@gmail.com>
2014-06-06 15:40:29 +0200

    Add js for delete map modal

--- extensions/wikia/WikiaInteractiveMaps/js/intMapDeleteMap.js
+++ extensions/wikia/WikiaInteractiveMaps/js/intMapDeleteMap.js
@@ -1,3 +1,85 @@
-/**
- * Created by brt on 06.06.2014.
- */
+define('wikia.intMaps.deleteMap', ['jquery'], function($) {
+	var modal,
+		modalConfig = {
+			vars: {
+				id: 'intMapsDeleteMapModal',
+				size: 'small',
+				content: $.msg('wikia-interactive-maps-delete-map-prompt'),
+				title: $.msg('wikia-interactive-maps-delete-map-title'),
+				buttons: [
+					{
+						vars: {
+							value: 'Delete',
+							data: [
+								{
+									key: 'event',
+									value: 'delete'
+								}
+							]
+						}
+					},
+					{
+						vars: {
+							value: $.msg('wikia-interactive-maps-delete-map-confirm-button'),
+							classes: ['normal', 'primary'],
+							data: [
+								{
+									key: 'event',
+									value: 'close'
+								}
+							]
+						}
+					}
+				]
+			}
+		},
+		$deleteMapButton = $('#intMapsDeleteMap'),
+		$mapId = $('iframe[name=wikia-interactive-map]').data('mapid'),
+		url = 'wikia.php?controller=WikiaInteractiveMaps&method=deleteMap';
+
+	/**
+	 * @Desc Creates a POST form with map ID in the payload and submits it
+	 * to WikiaInteractiveMaps controller (method deleteMap)
+	 */
+	function deleteMap() {
+		event.preventDefault();
+		modal.deactivate();
+		var $form = $('<form method="post" action="'+ url + '"></form>');
+		$('<input type="hidden" name="mapId">')
+			.val($mapId)
+			.appendTo($form);
+		$form.submit();
+	}
+
+	function triggerDeleteMapModal() {
+		if (window.wgUserName === null) {
+			window.UserLoginModal.show({
+				origin: 'wikia-int-map-create-map',
+				callback: function() {
+					loadModal();
+				}
+			});
+		} else {
+			loadModal();
+		}
+	}
+
+	/**
+	 * Opens modal with a prompt to confirm map deletion
+	 */
+	function init() {
+		require( [ 'wikia.ui.factory' ], function( uiFactory ) {
+			uiFactory.init( [ 'modal' ] ).then( function( uiModal ) {
+				uiModal.createComponent( modalConfig, function( _modal ) {
+					modal = _modal;
+					modal.bind('delete', deleteMap);
+					modal.show();
+				});
+			});
+		});
+	}
+
+	return {
+		init: init
+	}
+});
