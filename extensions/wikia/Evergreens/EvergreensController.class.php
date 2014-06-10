bd2dc9576e0e59412a900f4224db6036ea526091
Michał ‘Mix’ Roszka <michal@roszka.pl>
2014-06-10 09:38:56 +0000

    wip

--- extensions/wikia/Evergreens/EvergreensController.class.php
+++ extensions/wikia/Evergreens/EvergreensController.class.php
@@ -5,15 +5,31 @@ class EvergreensController extends WikiaController {
 
 	public function log() {
 		wfProfileIn( __METHOD__ );
-//		if ( $this->request->wasPosted ) {
-			$sData = $this->request->getVal( 'data', null );
-			if ( $sData ) {
-				\Wikia\Logger\WikiaLogger::instance()->alert(
-					'Evergreens: stale page cache detected',
-					[ $sData ]
-				);
+		if ( $this->request->wasPosted() ) {
+			$sHeaderInfo = $this->request->getVal( 'headerInfo', null );
+			if ( $sHeaderInfo && is_string( $sHeaderInfo ) ) {
+				$sHash = sha1( $sHeaderInfo );
+				$oHeaderInfo = json_decode( $sHeaderInfo );
+
+				if (is_array($oHeaderInfo->response[0]->responseHeaders)
+					&& is_array($oHeaderInfo->request[0]->requestHeaders) )
+				{
+
+					\Wikia\Logger\WikiaLogger::instance()->info(
+						'Evergreens: stale page cache detected',
+						[
+							'hash' => $sHash,
+							'request' => $oHeaderInfo->request[0]->requestHeaders,
+							'response' => $oHeaderInfo->response[0]->responseHeaders
+						]
+					);
+				}
+
+				$this->response->setVal( 'hash', $sHash );
 			}
-//		}
+		}
+		$this->response->setCacheValidity( WikiaResponse::CACHE_DISABLED );
+		$this->response->setFormat( 'json' );
 		wfProfileOut( __METHOD__ );
 	}
 
