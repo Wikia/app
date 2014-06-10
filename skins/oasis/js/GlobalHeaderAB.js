869c3bafcd03764814b9962c40eb0f67703ca30c
Damian Jozwiak <damian@wikia-inc.com>
2014-06-10 12:38:35 +0200

    CON-1016 | Global header User menu

---
+++ skins/oasis/js/GlobalHeaderAB.js
@@ -0,0 +1,19 @@
+$(function(){
+	var $accountNavigation = $( '#AccountNavigation' ),
+		$avatar = $accountNavigation.find( 'li:first .avatar' ),
+		avatarSize = 36;
+
+	$('.WikiaHeader').addClass('v3');
+	$( '#AccountNavigation > li:first > a' ).contents().filter(function() { return this.nodeType === 3; }).wrap( '<span class="login-text">' );
+	$accountNavigation.find( '.login-text' ).hide();
+	$( '#WallNotifications' ).hide();
+
+	if ( $avatar.length > 0 ) {
+		$avatar.attr( 'src', $avatar.attr( 'src' ).replace( '/20px-', '/' + avatarSize + 'px-' ) )
+			.attr( 'height', avatarSize)
+			.attr( 'width', avatarSize );
+	}
+
+	$accountNavigation.find('.subnav' ).find( '.new' ).removeClass( 'new' );
+	$accountNavigation.find( '.ajaxRegister' ).wrap( '<div class="ajaxRegisterContainer"></div>' ).parent().prependTo( '#UserLoginDropdown' );
+});
