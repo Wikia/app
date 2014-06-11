0d0f4c0188c6c58f425cc7bee44bcf6130324f06
aquilax <evgeniy@wikia-inc.com>
2014-06-11 14:44:24 +0000

    Removed C/P

--- extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMapsHooks.class.php
+++ extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMapsHooks.class.php
@@ -36,35 +36,4 @@ class WikiaInteractiveMapsHooks {
 		return !empty( $wgEnableWikiaInteractiveMaps ) && $wgTitle->isSpecial( 'InteractiveMaps' );
 	}
 
-	public static function formatLogEntry( $type, $action, $title, $forUI, $params, $filterWikilinks ) {
-		global $wgLang;
-
-		if(empty($params[0])){
-			return "";
-		}
-
-		$endon = "";
-		if(!empty($params[3])) {
-			$endon = $wgLang->timeanddate( wfTimestamp( TS_MW, $params[3] ), true );
-		}
-
-		$skin = RequestContext::getMain()->getSkin();
-		$id =  $params[1];
-		$revert = "(" . "<a class='chat-change-ban' data-user-id='{$params[1]}' href='#'>" . wfMsg( 'chat-ban-log-change-ban-link') . "</a>" . ")";
-		if ( !$filterWikilinks ) { // Plaintext? Used for IRC messages (BugID: 44249)
-			$targetUser = User::newFromId( $id );
-			$link = "[[User:{$targetUser->getName()}]]";
-		} else {
-			$link = $skin->userLink( $id, $title->getText() )
-				.$skin->userToolLinks( $id, $title->getText(), false );
-		}
-
-		$time = "";
-		if(!empty($params[2])) {
-			$time = $params[2];
-		}
-
-		return wfMsg('chat-'.$action.'-log-entry', $link, $time, $endon );
-	}
-
 }
