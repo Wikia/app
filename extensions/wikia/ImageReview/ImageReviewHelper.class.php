0d7cfb6b11c8458ead8333b9932c4476d4117d73
nelson <nelson@wikia-inc.com>
2014-06-11 16:06:45 -0700

    first pass at ImageReviewTask

--- extensions/wikia/ImageReview/ImageReviewHelper.class.php
+++ extensions/wikia/ImageReview/ImageReviewHelper.class.php
@@ -109,14 +109,21 @@ class ImageReviewHelper extends ImageReviewHelperBase {
 		}
 
 		if ( !empty( $deletionList ) ) {
-			$task = new ImageReviewTask();
-			$task->createTask(
-				array(
-					'page_list' => $deletionList,
-				),
-				TASK_QUEUED,
-				BatchTask::PRIORITY_HIGH
-			);
+			if (TaskRunner::isModern('ImageReviewTask')) {
+				$task = new \Wikia\Tasks\Tasks\ImageReviewTask();
+				$task->call('delete', $deletionList);
+				$task->prioritize();
+				$task->queue();
+			} else {
+				$task = new ImageReviewTask();
+				$task->createTask(
+					[
+						'page_list' => $deletionList,
+					],
+					TASK_QUEUED,
+					BatchTask::PRIORITY_HIGH
+				);
+			}
 		}
 
 		wfProfileOut( __METHOD__ );
---
+++ extensions/wikia/ImageReview/ImageReviewTask.class.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * ImageReviewTask
+ *
+ * <insert description here>
+ *
+ * @author Nelson Monterroso <nelson@wikia-inc.com>
+ */
+
+namespace Wikia\Tasks\Tasks;
+
+class ImageReviewTask extends BaseTask {
+	public function delete($pageList, $suppress=false) {
+		$user = \User::newFromId($this->createdBy);
+		$userName = $user->getName();
+		$articlesDeleted = 0;
+
+		foreach ($pageList as $imageData) {
+			list($wikiId, $imageId) = $imageData;
+
+			if (!\WikiFactory::isPublic($wikiId)) {
+				$this->notice('wiki has been disabled', ['wiki_id' => $wikiId]);
+				continue;
+			}
+
+			$dbname = \WikiFactory::getWikisByID($wikiId);
+			if (!$dbname) {
+				$this->warning('did not find database', ['wiki_id' => $wikiId]);
+				continue;
+			}
+
+			$cityUrl = \WikiFactory::getVarValueByName('wgServer', $wikiId);
+			if (empty($cityUrl)) {
+				$this->warning('could not determine city url', ['wiki_id' => $wikiId]);
+				continue;
+			}
+
+			$cityLang = \WikiFactory::getVarValueByName('wgLanguageCode', $wikiId);
+			$reason = wfMsgExt('imagereview-reason', ['language' => $cityLang]);
+
+			$command = "perl /usr/wikia/backend/bin/run_maintenance --id={$wikiId} --script=wikia/deleteOn.php".
+				' --'.
+				' -u '.escapeshellarg($userName).
+				' --id '.$imageId;
+
+			if ($reason) {
+				$command .= ' -r '.escapeshellarg($reason);
+			}
+			if ($suppress) {
+				$command .= ' -s';
+			}
+
+			$title = wfShellExec($command, $retval);
+
+			if ($retval !== 0) {
+				$this->error('article deletion error', [
+					'city_url' => $cityUrl,
+					'retval' => $retval,
+					'error' => $title,
+				]);
+				continue;
+			} else {
+				$cityPath = \WikiFactory::getVarValueByName('wgScript', $wikiId);
+				$escapedTitle = wfEscapeWikiText($title);
+
+				$this->info('removed image', [
+					'link' => "{$cityUrl}{$cityPath}?title={$escapedTitle}",
+					'title' => $escapedTitle,
+				]);
+			}
+
+			++$articlesDeleted;
+		}
+
+		if ($articlesDeleted != count($pageList)) {
+			$this->sendNotification();
+		}
+	}
+
+	public function sendNotification() {
+		global $wgFlowerUrl;
+
+		$subject = "ImageReview deletion failed #{$this->taskId}";
+		$body = "{$wgFlowerUrl}/task/{$this->taskId}";
+		$recipients = [new \MailAddress('tor@wikia-inc.com'), new \MailAddress('sannse@wikia-inc.com')];
+		$from = $recipients[0];
+
+
+		\UserMailer::send($recipients, $from, $subject, $body);
+	}
+} 
