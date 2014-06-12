f6664ae4bc69c2d19b4880141068d7e4c40bcb81
Christian Williams <christian@wikia-inc.com>
2014-06-12 01:17:30 +0000

    VE-518: Insert content properly when saving

--- extensions/VisualEditor/wikia/modules/ve/init/ve.init.mw.WikiaViewPageTarget.js
+++ extensions/VisualEditor/wikia/modules/ve/init/ve.init.mw.WikiaViewPageTarget.js
@@ -224,3 +224,38 @@ ve.init.mw.WikiaViewPageTarget.prototype.maybeShowDialogs = function () {
 		}
 	}
 };
+
+/**
+ * @inheritdoc
+ */
+ve.init.mw.ViewPageTarget.prototype.replacePageContent = function ( html, categoriesHtml ) {
+	var $articleContent, $insertTarget,
+		$mwContentText = $( '#mw-content-text' );
+		$content = $( $.parseHTML( html ) );
+
+	if ( mw.config.get( 'wgNamespaceNumber' ) === 14 ) {
+		//Category
+		$mwContentText.children().filter( function( index ) {
+			var $this = $( this );
+			return !(
+				// Category form
+				$this.hasClass( 'category-gallery-form' ) ||
+				// Category thumbs
+				$this.hasClass( 'category-gallery' ) ||
+				// Category exhibition
+				$this.is( '#mw-pages' ) ||
+				// Category list
+				$this.children( '#mw-pages' ).length
+			);
+		} ).remove();
+
+		$insertTarget = window.CategoryExhibition ? '#mw-pages' : '.category-gallery';
+		$content.insertBefore( $insertTarget );
+		$articleContent = $mwContentText;
+	} else {
+		$articleContent = $mwContentText.empty().append( $content );
+	}
+
+	mw.hook( 'wikipage.content' ).fire( $articleContent );
+	$( '#catlinks' ).replaceWith( categoriesHtml );
+};
