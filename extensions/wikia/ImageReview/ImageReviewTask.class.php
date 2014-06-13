df1d2a083664532852f2cd31bbf5eccf3ce2bc0d
nelson <nelson@wikia-inc.com>
2014-06-12 17:09:12 -0700

    STYLING!

--- extensions/wikia/ImageReview/ImageReviewTask.class.php
+++ extensions/wikia/ImageReview/ImageReviewTask.class.php
@@ -8,71 +8,71 @@
 namespace Wikia\Tasks\Tasks;
 
 class ImageReviewTask extends BaseTask {
-	public function delete($pageList, $suppress=false) {
+	public function delete( $pageList, $suppress = false ) {
 		global $IP;
 
-		$user = \User::newFromId($this->createdBy);
+		$user = \User::newFromId( $this->createdBy );
 		$userName = $user->getName();
 		$articlesDeleted = 0;
 
-		foreach ($pageList as $imageData) {
-			list($wikiId, $imageId) = $imageData;
+		foreach ( $pageList as $imageData ) {
+			list( $wikiId, $imageId ) = $imageData;
 
-			if (!\WikiFactory::isPublic($wikiId)) {
-				$this->notice('wiki has been disabled', ['wiki_id' => $wikiId]);
+			if ( !\WikiFactory::isPublic( $wikiId ) ) {
+				$this->notice( 'wiki has been disabled', ['wiki_id' => $wikiId] );
 				continue;
 			}
 
-			$dbname = \WikiFactory::getWikisByID($wikiId);
-			if (!$dbname) {
-				$this->warning('did not find database', ['wiki_id' => $wikiId]);
+			$dbname = \WikiFactory::getWikisByID( $wikiId );
+			if ( !$dbname ) {
+				$this->warning( 'did not find database', ['wiki_id' => $wikiId] );
 				continue;
 			}
 
-			$cityUrl = \WikiFactory::getVarValueByName('wgServer', $wikiId);
-			if (empty($cityUrl)) {
-				$this->warning('could not determine city url', ['wiki_id' => $wikiId]);
+			$cityUrl = \WikiFactory::getVarValueByName( 'wgServer', $wikiId );
+			if ( empty( $cityUrl ) ) {
+				$this->warning( 'could not determine city url', ['wiki_id' => $wikiId] );
 				continue;
 			}
 
-			$cityLang = \WikiFactory::getVarValueByName('wgLanguageCode', $wikiId);
-			$reason = wfMsgExt('imagereview-reason', ['language' => $cityLang]);
+			$cityLang = \WikiFactory::getVarValueByName( 'wgLanguageCode', $wikiId );
+			$reason = wfMsgExt( 'imagereview-reason', ['language' => $cityLang] );
 
-			$command = "SERVER_ID={$wikiId} php {$IP}/maintenance/wikia/deleteOn.php".
-				' -u '.escapeshellarg($userName).
-				' --id '.$imageId;
+			$command = "SERVER_ID={$wikiId} php {$IP}/maintenance/wikia/deleteOn.php" .
+				' -u ' . escapeshellarg( $userName ) .
+				' --id ' . $imageId;
 
-			if ($reason) {
-				$command .= ' -r '.escapeshellarg($reason);
+			if ( $reason ) {
+				$command .= ' -r ' . escapeshellarg( $reason );
 			}
-			if ($suppress) {
+			if ( $suppress ) {
 				$command .= ' -s';
 			}
 
-			$title = wfShellExec($command, $exitStatus);
+			$title = wfShellExec( $command, $exitStatus );
 
-			if ($exitStatus !== 0) {
-				$this->error('article deletion error', [
+			if ( $exitStatus !== 0 ) {
+				$this->error( 'article deletion error', [
 					'city_url' => $cityUrl,
 					'exit_status' => $exitStatus,
 					'error' => $title,
-				]);
+				] );
 
 				continue;
 			}
 
-			$cityPath = \WikiFactory::getVarValueByName('wgScript', $wikiId);
-			$escapedTitle = wfEscapeWikiText($title);
+			$cityPath = \WikiFactory::getVarValueByName( 'wgScript', $wikiId );
+			$escapedTitle = wfEscapeWikiText( $title );
 
-			$this->info('removed image', [
+			$this->info( 'removed image', [
 				'link' => "{$cityUrl}{$cityPath}?title={$escapedTitle}",
 				'title' => $escapedTitle,
-			]);
+			] );
 
 			++$articlesDeleted;
 		}
 
-		if ($articlesDeleted != count($pageList)) {
+		if ( $articlesDeleted != count( $pageList ) ) {
 			$this->sendNotification();
 		}
 	}
@@ -82,9 +82,9 @@ class ImageReviewTask extends BaseTask {
 
 		$subject = "ImageReview deletion failed #{$this->taskId}";
 		$body = "{$wgFlowerUrl}/task/{$this->taskId}";
-		$recipients = [new \MailAddress('tor@wikia-inc.com'), new \MailAddress('sannse@wikia-inc.com')];
+		$recipients = [new \MailAddress( 'tor@wikia-inc.com' ), new \MailAddress( 'sannse@wikia-inc.com' )];
 		$from = $recipients[0];
 
-		\UserMailer::send($recipients, $from, $subject, $body);
+		\UserMailer::send( $recipients, $from, $subject, $body );
 	}
 } 
