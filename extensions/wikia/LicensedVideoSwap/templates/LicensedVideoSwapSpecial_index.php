68781053619c73fba59d5a1cd954bdf18a76951b
lizlux <liz@lizlux.net>
2014-06-13 15:47:45 -0700

    VID-1746 reenabling LVS after script ran

--- extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_index.php
+++ extensions/wikia/LicensedVideoSwap/templates/LicensedVideoSwapSpecial_index.php
@@ -11,40 +11,29 @@
 
 ?>
 
-<h2><?= wfMessage( 'lvs-maintenance-header' )->plain() ?></h2>
-<p><?= wfMessage( 'lvs-maintenance-body' )->plain() ?></p>
+<? if ( !empty( $videoList ) ): ?>
+	<?= $app->renderPartial( 'LicensedVideoSwapSpecialController', 'callout' ) ?>
+<? endif; ?>
 
+<div class="lvs-instructions">
+	<h2><?= wfMessage( 'lvs-instructions-header' )->parse() ?></h2>
+	<p><?= wfMessage( 'lvs-instructions' )->parse() ?></p>
+</div>
 
-<?
-	// Temporary rollout of new LVS thumbnails per VID-1743
-	$isStaff = in_array( 'staff', $app->wg->User->getEffectiveGroups() );
-?>
+<section class="lvs-match-stats">
+	<div class="count"><?= $totalVideos ?></div>
+	<div class="description"><?= wfMessage( 'lvs-match-stats-description' )->plain() ?></div>
+</section>
 
-<? if ( $isStaff ): ?>
-	<? if ( !empty( $videoList ) ): ?>
-		<?= $app->renderPartial( 'LicensedVideoSwapSpecialController', 'callout' ) ?>
-	<? endif; ?>
+<? if ( !empty( $videoList ) ): ?>
 
-	<div class="lvs-instructions">
-		<h2><?= wfMessage( 'lvs-instructions-header' )->parse() ?></h2>
-		<p><?= wfMessage( 'lvs-instructions' )->parse() ?></p>
+	<div class="WikiaGrid LVSGrid" id="LVSGrid">
+
+		<?= $app->renderPartial( 'LicensedVideoSwapSpecial', 'row', array( 'videoList' => $videoList, 'thumbWidth' => $thumbWidth, 'thumbHeight' => $thumbHeight ) ) ?>
+		<?= $pagination ?>
+
 	</div>
 
-	<section class="lvs-match-stats">
-		<div class="count"><?= $totalVideos ?></div>
-		<div class="description"><?= wfMessage( 'lvs-match-stats-description' )->plain() ?></div>
-	</section>
-
-	<? if ( !empty( $videoList ) ): ?>
-
-		<div class="WikiaGrid LVSGrid" id="LVSGrid">
-
-			<?= $app->renderPartial( 'LicensedVideoSwapSpecial', 'row', array( 'videoList' => $videoList, 'thumbWidth' => $thumbWidth, 'thumbHeight' => $thumbHeight ) ) ?>
-			<?= $pagination ?>
-
-		</div>
-
-	<? else: ?>
-		<p class="lvs-zero-state"><?= wfMessage( 'lvs-zero-state' )->plain() ?></p>
-	<? endif; ?>
+<? else: ?>
+	<p class="lvs-zero-state"><?= wfMessage( 'lvs-zero-state' )->plain() ?></p>
 <? endif; ?>
