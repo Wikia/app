a5d854cf465da4f0380eb228e637c7e11a77e123
Adam Robak <adamr@wikia-inc.com>
2014-06-23 16:11:26 +0200

    added option for changing delay

--- maintenance/wikia/Indexer/ReindexEventGenerator.php
+++ maintenance/wikia/Indexer/ReindexEventGenerator.php
@@ -6,8 +6,15 @@ class ReindexEventGenerator extends IndexerWorkerBase {
 
 	const WG_CONTENT_NAMESPACES_KEY = 'wgContentNamespaces';
 	const DELAY = 1000; //1m = 1sec
+	private $delay;
+
+	public function __construct() {
+		parent::__construct();
+		$this->addOption('delay', 'Delay in micro seconds between messages', false, true);
+	}
 
 	protected function preprocess() {
+		$this->delay = $this->getOption( 'delay', static::DELAY );
 		$db = wfGetDB( DB_SLAVE );
 		$namespaces = WikiFactory::getVarValueByName( self::WG_CONTENT_NAMESPACES_KEY, $this->city_id );
 		( new WikiaSQL() )->SELECT('page_id')->FROM('page')->WHERE('page_namespace')->IN($namespaces)
@@ -16,7 +23,7 @@ class ReindexEventGenerator extends IndexerWorkerBase {
 				$msg->cityId = $this->city_id;
 				$msg->pageId = $row->page_id;
 				$this->publish('article.index', $msg);
-				usleep(static::DELAY);
+				usleep($this->delay);
 			});
 	}
 }
