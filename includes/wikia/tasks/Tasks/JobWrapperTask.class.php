0be9f5b297723ed1428b9b37b05349329d92ebb8
nelson <nelson@wikia-inc.com>
2014-06-30 16:17:12 -0700

    adding simple wrapper for core mediawiki jos

---
+++ includes/wikia/tasks/Tasks/JobWrapperTask.class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * JobWrapperTask
+ *
+ * Wrapper for core MediaWiki Job classes. This is useful so we can upgrade extensions without needing to re-migrate
+ * its job changes to the new job queue.
+ *
+ * @author Nelson Monterroso <nelson@wikia-inc.com>
+ */
+
+namespace Wikia\Tasks\Tasks;
+
+
+class JobWrapperTask extends BaseTask {
+	protected $command;
+	protected $params;
+
+	public function call() {
+		list($command, $title, $this->params) = func_get_args();
+		$this->title($title);
+
+		return parent::call('wrap', $command);
+	}
+
+	public function wrap($command) {
+		$job = \Job::factory($command, $this->title, $this->params);
+		return $job->run();
+	}
+}
