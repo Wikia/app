9f06c25b6a87f76eec45c458f03abf0208bcd6a9
nelson <nelson@wikia-inc.com>
2014-06-30 14:18:00 -0700

    url encode/decode title name
    (cherry picked from commit 94845b2b90e403acc95a26b4f8666c592c3df541)

--- includes/wikia/tasks/Tasks/BaseTask.class.php
+++ includes/wikia/tasks/Tasks/BaseTask.class.php
@@ -55,7 +55,7 @@ abstract class BaseTask {
 			return;
 		}
 
-		$this->title = \Title::makeTitleSafe($this->titleParams['namespace'], $this->titleParams['dbKey']);
+		$this->title = \Title::makeTitleSafe($this->titleParams['namespace'], urldecode($this->titleParams['dbKey']));
 		if ( $this->title == null ) {
 			throw new \Exception( "unable to instantiate title with id {$this->titleParams['dbKey']}" );
 		}
@@ -235,7 +235,7 @@ abstract class BaseTask {
 	public function title(\Title $title) {
 		$this->titleParams = [
 			'namespace' => $title->getNamespace(),
-			'dbKey' => $title->getDBkey()
+			'dbKey' => urlencode($title->getDBkey())
 		];
 
 		return $this;
