53552d633f0a10c2da43fd99d0c007aae50993b9
Adam Karmiński <adamk@wikia-inc.com>
2014-06-30 11:35:20 +0000

    Organizing View and expand its access to all keys of a field

--- extensions/wikia/InfoboxBuilder/includes/lua/InfoboxBuilderView.lua
+++ extensions/wikia/InfoboxBuilder/includes/lua/InfoboxBuilderView.lua
@@ -18,161 +18,147 @@ function InfoboxBuilderView.render( input, vars )
 	    local table = Infobox:tag('table')
 	          table:attr('cellspacing', '0')
 	               :attr('cellpadding', '0')
+	               -- :addClass('infobox')
 	 
 	    for index, field in ipairs( fields ) do
 	 
-	      if     field.Type == "Image" then
-	        table:node( InfoboxBuilderView.addRowImage( field.Label, field.Value ) )
+	  	if field.Type == "Line" then
+	    	if not HF.isempty( field.Value ) then
+	      		table:node( InfoboxBuilderView.addRowLine( field ) )
+	    	end
+	    
+		elseif field.Type == "Header" then
+		    if input.sections[index] == "On" then
+		    	table:node( InfoboxBuilderView.addRowHeader( field ) )
+		    end
+		    
+	    elseif field.Type == "Title" then
+	      table:node( InfoboxBuilderView.addRowTitle( field ) )
+	    
+	    elseif field.Type == "MainImage" then
+	      table:node( InfoboxBuilderView.addRowMainImage( field ) )
 
-	      elseif field.Type == "MainImage" then
-	        table:node( InfoboxBuilderView.addRowMainImage( field.Label, field.Value ) )
+	    elseif field.Type == "Image" then
+	      table:node( InfoboxBuilderView.addRowImage( field ) )
 
-	      elseif field.Type == "Title" then
-	        table:node( InfoboxBuilderView.addRowTitle( field.Label, field.Value ) )
-
-	      elseif field.Type == "Header" then
-	        if input.sections[index] == "On" then
-	          table:node( InfoboxBuilderView.addRowHeader( field.Label, field.Value ) )
-	        end
-
-	      elseif field.Type == "Line" then
-	        if not HF.isempty( field.Value ) then
-	          table:node( InfoboxBuilderView.addRowLine( field.Label, field.Value ) )
-	        end
-
-	      elseif field.Type == "Footer" then
-	        table:node( InfoboxBuilderView.addRowFooter( field.Label, field.Value ) )
+	    elseif field.Type == "Footer" then
+	      table:node( InfoboxBuilderView.addRowFooter( field ) )
 
         elseif field.Type == "Split" then
-          table:node( InfoboxBuilderView.addRowSplit( field.Label, field.Value ) )
+          table:node( InfoboxBuilderView.addRowSplit( field ) )
         
         elseif field.Type == "Custom" then
-          table:node( InfoboxBuilderView.addRowCustom( field.Label, field.Value ) )
+          table:node( InfoboxBuilderView.addRowCustom( field ) )
 
 	      end
 	 
 	    end
   
-  output = tostring( Infobox )
+	output = tostring( Infobox )
   
 	return output
 
 end
 
-function InfoboxBuilderView.addRowCustom( label, value )
-  local node = mw.html.create( label )
-        node:addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxCustom' )
-            :wikitext( value )
-  return node
-end
-
-function InfoboxBuilderView.addRowMainImage( label, value )
-  local node = mw.html.create('')
-  
-  local row1 = node:tag('tr')
-    row1:tag('td')
-        :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxMainImage' )
-        :attr('colspan', '2')
-        :wikitext( value ) 
-
-  if InfoboxBuilderView.vars.MainImageCaption == "On" then
-    local row2 = node:tag('tr')
-      row2:tag('td')
-          :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxMainImageCaption' )
-          :attr('colspan', '2')
-          :wikitext( label )
-  end
-
-  return node
-end
-
-function InfoboxBuilderView.addRowImage( label, value )
-  local node = mw.html.create('')
-  
-  local row1 = node:tag('tr')
-    row1:tag('td')
-        :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxImage' )
-        :attr('colspan', '2')
-        :wikitext( value ) 
-
-  local row2 = node:tag('tr')
-    row2:tag('td')
-        :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxImageCaption' )
-        :attr('colspan', '2')
-        :wikitext( label )
-
-  return node
-end
- 
-function InfoboxBuilderView.addRowTitle( label, value )
-  local row = mw.html.create('tr')
-    row:tag('td')
-       :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxTitle' )
-       :attr('colspan', '2')
-       :wikitext( value )
-  return row
-end
-
-function InfoboxBuilderView.addRowFooter( label, value )
-  local row = mw.html.create('tr')
-    row:tag('td')
-       :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxFooter' )
-       :attr('colspan', '2')
-       :wikitext( value )
-  return row
-end
- 
-function InfoboxBuilderView.addRowHeader( label, value )
- 
-    local row = mw.html.create('tr')
-      row:tag('td')
-         :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxHeader' )
-         :attr('colspan', '2') 
-         :wikitext( value )
-    return row
- 
-end
-
-function InfoboxBuilderView.addRowLink( label, value )
- 
-    local row = mw.html.create('tr')
-      row:tag('td')
-         :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxLink' )
-         :attr('colspan', '2') 
-         :wikitext( value )
-    return row
- 
-end
-
-function InfoboxBuilderView.addRowLine( label, value )
+function InfoboxBuilderView.addRowLine( field )
     
     local row = mw.html.create('tr')
       row:addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxLine' )
+      if not HF.isempty( field.CssClass ) then
+        row:addClass( field.CssClass )
+      end
       row:tag('td')
          :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxLineLeft' )
-         :wikitext( label )
+         :wikitext( field.Label )
          :done()
 
-    if string.len( value ) > tonumber( InfoboxBuilderView.vars.ToggleContentLongerThan ) then
+    if string.len( field.Value ) > tonumber( InfoboxBuilderView.vars.ToggleContentLongerThan ) then
       local cell = row:tag('td')
       cell:addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxLineRight' )
         cell:tag('div')
             :addClass('mw-collapsible mw-collapsed ' .. InfoboxBuilderView.vars.Theme .. 'InfoboxToggleContent')
-            :wikitext( value )
+            :wikitext( field.Value )
             :done()
       row:done()
     else
       row:tag('td')
          :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxLineRight' )
-         :wikitext( value )
+         :wikitext( field.Value )
          :done()
     end
 
     return row
  
 end
+ 
+function InfoboxBuilderView.addRowHeader( field )
+ 
+    local row = mw.html.create('tr')
+      row:tag('td')
+         :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxHeader' )
+         :attr('colspan', '2') 
+         :wikitext( field.Value )
+    return row
+ 
+end
+ 
+function InfoboxBuilderView.addRowTitle( field )
+  local row = mw.html.create('tr')
+    row:tag('td')
+       :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxTitle' )
+       :attr('colspan', '2')
+       :wikitext( field.Value )
+  return row
+end
 
-function InfoboxBuilderView.addRowSplit( label, value )
+function InfoboxBuilderView.addRowMainImage( field )
+  local node = mw.html.create('')
+  
+  local row1 = node:tag('tr')
+    row1:tag('td')
+        :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxMainImage' )
+        :attr('colspan', '2')
+        :wikitext( field.Value ) 
+
+  if InfoboxBuilderView.vars.MainImageCaption == "On" then
+    local row2 = node:tag('tr')
+      row2:tag('td')
+          :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxMainImageCaption' )
+          :attr('colspan', '2')
+          :wikitext( field.Label )
+  end
+
+  return node
+end
+
+function InfoboxBuilderView.addRowImage( field )
+  local node = mw.html.create('')
+  
+  local row1 = node:tag('tr')
+    row1:tag('td')
+        :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxImage' )
+        :attr('colspan', '2')
+        :wikitext( field.Value ) 
+
+  local row2 = node:tag('tr')
+    row2:tag('td')
+        :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxImageCaption' )
+        :attr('colspan', '2')
+        :wikitext( field.Label )
+
+  return node
+end
+
+function InfoboxBuilderView.addRowFooter( field )
+  local row = mw.html.create('tr')
+    row:tag('td')
+       :addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxFooter' )
+       :attr('colspan', '2')
+       :wikitext( field.Value )
+  return row
+end
+
+function InfoboxBuilderView.addRowSplit( field )
 
   local node = mw.html.create('')
   
@@ -186,20 +172,20 @@ function InfoboxBuilderView.addRowSplit( label, value )
   local valueRight = mw.html.create('td')
     valueRight:addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxSplitValueRight' )
 
-  if type( label ) == "string" then
-    labelLeft:wikitext( label )
+  if type( field.Label ) == "string" then
+    labelLeft:wikitext( field.Label )
   end
 
-  if type( value ) == "string" then
-    valueLeft:wikitext( value )
-  elseif type( value ) == "table" then
-    if not HF.isempty( value[1] ) then
-      labelLeft:wikitext( label[1] )
-      valueLeft:wikitext( value[1] )
+  if type( field.Value ) == "string" then
+    valueLeft:wikitext( field.Value )
+  elseif type( field.Value ) == "table" then
+    if not HF.isempty( field.Value[1] ) then
+      labelLeft:wikitext( field.Label[1] )
+      valueLeft:wikitext( field.Value[1] )
     end
-    if not HF.isempty( value[2] ) then
-      labelRight:wikitext( label[2] )
-      valueRight:wikitext( value[2] )
+    if not HF.isempty( field.Value[2] ) then
+      labelRight:wikitext( field.Label[2] )
+      valueRight:wikitext( field.Value[2] )
     end
   end
 
@@ -215,6 +201,13 @@ function InfoboxBuilderView.addRowSplit( label, value )
 
 end
 
+function InfoboxBuilderView.addRowCustom( field )
+  local node = mw.html.create( field.Label )
+        node:addClass( InfoboxBuilderView.vars.Theme .. 'InfoboxCustom' )
+            :wikitext( field.Value )
+  return node
+end
+
 php = mw_interface
 mw_interface = nil
 
@@ -222,6 +215,6 @@ mw_interface = nil
 mw = mw or {}
 mw.InfoboxBuilderView = InfoboxBuilderView
 
-package.loaded['mw.InfoboxBuilderView'] = InfoboxBuilderView
+package.loaded['mw.InfoboxBuilderView'] = InfoboxBuilderViewf
 
 return InfoboxBuilderView
