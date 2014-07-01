1da370440089c1a51c5875970ce51e4a6af6cbb4
Władysław Bodzek <wladek@wikia-inc.com>
2014-07-01 15:41:14 +0200

    used stylize.php

--- includes/wikia/transactiontracer/TransactionTracer.php
+++ includes/wikia/transactiontracer/TransactionTracer.php
@@ -5,7 +5,7 @@
 
 
 class TransactionTracer {
-	//Transaction names
+	// Transaction names
 	const TRANSACTION_PAGE_MAIN = 'page/main';
 	const TRANSACTION_PAGE_FILE = 'page/file';
 	const TRANSACTION_PAGE_MESSAGE_WALL = 'page/message_wall';
@@ -18,14 +18,14 @@ class TransactionTracer {
 	const TRANSACTION_AJAX = 'api/ajax';
 	const TRANSACTION_API = 'api/api';
 
-	//Parameters
+	// Parameters
 	const PARAM_LOGGED_IN = 'logged_in';
 	const PARAM_PARSER_CACHE_USED = 'parser_cache_used';
 	const PARAM_SIZE_CATEGORY = 'size_category';
 	const PARAM_ACTION = 'action';
 	const PARAM_SKIN = 'skin';
 	const PARAM_VERSION = 'version';
-	const PARAM_VIEW_TYPE = 'view_type'; //For Category only
+	const PARAM_VIEW_TYPE = 'view_type'; // For Category only
 	const PARAM_CONTROLLER = 'controller';
 	const PARAM_METHOD = 'method';
 	const PARAM_FUNCTION = 'function';
@@ -34,7 +34,7 @@ class TransactionTracer {
 
 	const PSEUDO_PARAM_TYPE = 'transaction';
 
-	//Definition of different size categories
+	// Definition of different size categories
 	const SIZE_CATEGORY_SIMPLE = 'simple';
 	const SIZE_CATEGORY_AVERAGE = 'average';
 	const SIZE_CATEGORY_COMPLEX = 'complex';
@@ -93,8 +93,8 @@ class TransactionTracer {
 	 *
 	 * @param $transactionType String: Name of the current transaction - should be one of the defines TYPE_XXX
 	 */
-	public static function setType($transactionType) {
-		wfDebug(__CLASS__.": transaction type set - ".$transactionType."\n");
+	public static function setType( $transactionType ) {
+		wfDebug( __CLASS__ . ": transaction type set - " . $transactionType . "\n" );
 		self::$type = $transactionType;
 
 		self::updateName();
@@ -106,12 +106,12 @@ class TransactionTracer {
 	 * @param $parameterKey String: Name of the parameter. The key should be on of the defines PARAM_XXX
 	 * @param $parameterValue String: Value of the parameter
 	 */
-	public static function setAttribute($parameterKey, $parameterValue) {
+	public static function setAttribute( $parameterKey, $parameterValue ) {
 		if ( $parameterValue === null ) {
 			return;
 		}
 
-		wfDebug(__CLASS__.": parameter set - key: ".$parameterKey.", value: ".$parameterValue."\n");
+		wfDebug( __CLASS__ . ": parameter set - key: " . $parameterKey . ", value: " . $parameterValue . "\n" );
 		self::$attributes[$parameterKey] = $parameterValue;
 
 		if ( function_exists( 'newrelic_add_custom_parameter' ) ) {
@@ -133,19 +133,19 @@ class TransactionTracer {
 				// article in main namespace
 				case self::TRANSACTION_PAGE_MAIN:
 					// action is set
-					if ( isset($attributes[self::PARAM_ACTION]) ) {
-						$transactionName .= sprintf("/%s", $attributes[self::PARAM_ACTION]);
+					if ( isset( $attributes[self::PARAM_ACTION] ) ) {
+						$transactionName .= sprintf( "/%s", $attributes[self::PARAM_ACTION] );
 						// action: view
 						if ( $attributes[self::PARAM_ACTION] === self::ACTION_VIEW ) {
 							// skin and parser_cached_used flag
-							if ( isset($attributes[self::PARAM_SKIN]) && isset($attributes[self::PARAM_PARSER_CACHE_USED]) ) {
-								$transactionName .= sprintf("/%s/%s",
+							if ( isset( $attributes[self::PARAM_SKIN] ) && isset( $attributes[self::PARAM_PARSER_CACHE_USED] ) ) {
+								$transactionName .= sprintf( "/%s/%s",
 									$attributes[self::PARAM_SKIN],
 									$attributes[self::PARAM_PARSER_CACHE_USED] ? 'no_parser' : 'parser' );
 								// page size
-								if ( $attributes[self::PARAM_PARSER_CACHE_USED] === false && isset($attributes[self::PARAM_SIZE_CATEGORY]) ) {
-									$transactionName .= sprintf("/%s",
-										$attributes[self::PARAM_SIZE_CATEGORY]);
+								if ( $attributes[self::PARAM_PARSER_CACHE_USED] === false && isset( $attributes[self::PARAM_SIZE_CATEGORY] ) ) {
+									$transactionName .= sprintf( "/%s",
+										$attributes[self::PARAM_SIZE_CATEGORY] );
 								}
 							}
 						}
@@ -154,11 +154,11 @@ class TransactionTracer {
 				// special page
 				case self::TRANSACTION_SPECIAL_PAGE:
 					// special page name was reported
-					if ( isset($attributes[self::PARAM_SPECIAL_PAGE_NAME]) ) {
+					if ( isset( $attributes[self::PARAM_SPECIAL_PAGE_NAME] ) ) {
 						$specialPage = $attributes[self::PARAM_SPECIAL_PAGE_NAME];
 						// it is an important special page
-						if ( in_array($specialPage, self::$IMPORTANT_SPECIAL_PAGES) ) {
-							$transactionName .= sprintf("/%s", $specialPage );
+						if ( in_array( $specialPage, self::$IMPORTANT_SPECIAL_PAGES ) ) {
+							$transactionName .= sprintf( "/%s", $specialPage );
 						} else {
 							$transactionName .= "/other";
 						}
@@ -167,11 +167,11 @@ class TransactionTracer {
 				// nirvana call
 				case self::TRANSACTION_NIRVANA:
 					// controller was reported
-					if ( isset($attributes[self::PARAM_CONTROLLER]) ) {
+					if ( isset( $attributes[self::PARAM_CONTROLLER] ) ) {
 						$controller = $attributes[self::PARAM_CONTROLLER];
 						// it is an important controller
-						if ( in_array($controller, self::$IMPORTANT_CONTROLLERS) ) {
-							$transactionName .= sprintf("/%s", $controller );
+						if ( in_array( $controller, self::$IMPORTANT_CONTROLLERS ) ) {
+							$transactionName .= sprintf( "/%s", $controller );
 						} else {
 							$transactionName .= "/other";
 						}
@@ -180,11 +180,11 @@ class TransactionTracer {
 				// ajax call
 				case self::TRANSACTION_AJAX:
 					// controller was reported
-					if ( isset($attributes[self::PARAM_FUNCTION]) ) {
+					if ( isset( $attributes[self::PARAM_FUNCTION] ) ) {
 						$function = $attributes[self::PARAM_FUNCTION];
 						// it is an important controller
-						if ( in_array($function, self::$IMPORTANT_AJAX_FUNCTIONS) ) {
-							$transactionName .= sprintf("/%s", $function );
+						if ( in_array( $function, self::$IMPORTANT_AJAX_FUNCTIONS ) ) {
+							$transactionName .= sprintf( "/%s", $function );
 						} else {
 							$transactionName .= "/other";
 						}
@@ -193,11 +193,11 @@ class TransactionTracer {
 				// api.php call
 				case self::TRANSACTION_API:
 					// controller was reported
-					if ( isset($attributes[self::PARAM_API_ACTION]) ) {
+					if ( isset( $attributes[self::PARAM_API_ACTION] ) ) {
 						$action = $attributes[self::PARAM_API_ACTION];
 						// it is an important controller
-						if ( in_array($action, self::$IMPORTANT_API_CALLS) ) {
-							$transactionName .= sprintf("/%s", $action );
+						if ( in_array( $action, self::$IMPORTANT_API_CALLS ) ) {
+							$transactionName .= sprintf( "/%s", $action );
 						} else {
 							$transactionName .= "/other";
 						}
@@ -206,7 +206,7 @@ class TransactionTracer {
 			}
 
 			if ( $transactionName !== self::$name ) {
-				wfDebug(__CLASS__.": transaction name updated to: \"".$transactionName."\"\n");
+				wfDebug( __CLASS__ . ": transaction name updated to: \"" . $transactionName . "\"\n" );
 				self::$name = $transactionName;
 				if ( function_exists( 'newrelic_name_transaction' ) ) {
 					newrelic_name_transaction( $transactionName );
@@ -225,7 +225,7 @@ class TransactionTracer {
 
 	public static function getContext() {
 		return array_merge(
-			(self::$type !== null) ? array( self::PSEUDO_PARAM_TYPE => self::$type ) : array(),
+			( self::$type !== null ) ? array( self::PSEUDO_PARAM_TYPE => self::$type ) : array(),
 			self::$attributes
 		);
 	}
@@ -236,7 +236,7 @@ class TransactionTracer {
 		$expFuncCount = $parserOutput->getPerformanceStats( 'expFuncCount' );
 		$nodeCount = $parserOutput->getPerformanceStats( 'nodeCount' );
 
-		if ( !is_numeric($wikitextSize) || !is_numeric($htmlSize) || !is_numeric($expFuncCount) || !is_numeric($nodeCount) ) {
+		if ( !is_numeric( $wikitextSize ) || !is_numeric( $htmlSize ) || !is_numeric( $expFuncCount ) || !is_numeric( $nodeCount ) ) {
 			return true;
 		}
 
@@ -247,7 +247,7 @@ class TransactionTracer {
 				TransactionTracer::SIZE_CATEGORY_AVERAGE :
 			// else
 				TransactionTracer::SIZE_CATEGORY_COMPLEX
-			));
+			) );
 
 		return true;
 	}
