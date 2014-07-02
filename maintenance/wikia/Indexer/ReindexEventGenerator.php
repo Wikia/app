15982c1b54cfc37764b42f76024894288e13265a
Adam Robak <adamr@wikia-inc.com>
2014-07-02 14:54:47 +0200

    PLA-1579 distribution of event changed based on smw extension being turned on

--- maintenance/wikia/Indexer/ReindexEventGenerator.php
+++ maintenance/wikia/Indexer/ReindexEventGenerator.php
@@ -5,8 +5,10 @@ require_once( dirname( __FILE__ ) . '/IndexerWorkerBase.php' );
 class ReindexEventGenerator extends IndexerWorkerBase {
 
 	const WG_CONTENT_NAMESPACES_KEY = 'wgContentNamespaces';
+	const WG_ENABLE_SMW_KEY = 'wgEnableSemanticMediaWikiExt';
 	const DELAY = 1000; //1m = 1sec
 	private $delay;
+	private $key;
 
 	public function __construct() {
 		parent::__construct();
@@ -22,9 +24,17 @@ class ReindexEventGenerator extends IndexerWorkerBase {
 				$msg = new stdClass();
 				$msg->cityId = $this->city_id;
 				$msg->pageId = $row->page_id;
-				$this->publish('article.index', $msg);
+				$this->publish($this->getRoutingPublishKey(), $msg);
 				usleep($this->delay);
 			});
+	}
+
+	private function getRoutingPublishKey() {
+		if ( !isset( $this->key ) ) {
+			$smw = WikiFactory::getVarValueByName( self::WG_ENABLE_SMW_KEY, $this->city_id );
+			$this->key = $smw ? 'article.prod.index' : 'article.index';
+		}
+		return $this->key;
 	}
 }
 
