0682e0333128bb6d076f39719fc9d5b20b38747b
Adam Robak <adamr@wikia-inc.com>
2014-07-02 15:10:29 +0200

    PLA-1579 routing key generation changed

--- maintenance/wikia/Indexer/ReindexEventGenerator.php
+++ maintenance/wikia/Indexer/ReindexEventGenerator.php
@@ -19,22 +19,16 @@ class ReindexEventGenerator extends IndexerWorkerBase {
 		$this->delay = $this->getOption( 'delay', static::DELAY );
 		$db = wfGetDB( DB_SLAVE );
 		$namespaces = WikiFactory::getVarValueByName( self::WG_CONTENT_NAMESPACES_KEY, $this->city_id );
+		$smw = WikiFactory::getVarValueByName( self::WG_ENABLE_SMW_KEY, $this->city_id );
+		$this->key = $smw ? 'article.prod.index' : 'article.index';
 		( new WikiaSQL() )->SELECT('page_id')->FROM('page')->WHERE('page_namespace')->IN($namespaces)
 			->runLoop($db, function (&$d, $row) {
 				$msg = new stdClass();
 				$msg->cityId = $this->city_id;
 				$msg->pageId = $row->page_id;
-				$this->publish($this->getRoutingPublishKey(), $msg);
+				$this->publish($this->key, $msg);
 				usleep($this->delay);
 			});
-	}
-
-	private function getRoutingPublishKey() {
-		if ( !isset( $this->key ) ) {
-			$smw = WikiFactory::getVarValueByName( self::WG_ENABLE_SMW_KEY, $this->city_id );
-			$this->key = $smw ? 'article.prod.index' : 'article.index';
-		}
-		return $this->key;
 	}
 }
 
