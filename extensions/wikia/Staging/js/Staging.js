b47405ec84544e0d71be9f81d262a9cf466974f2
Damian Jozwiak <damian@wikia-inc.com>
2014-07-03 18:58:01 +0200

    CON-1243 | Hacks for stagings - kosherization

--- extensions/wikia/Staging/js/Staging.js
+++ extensions/wikia/Staging/js/Staging.js
@@ -2,14 +2,15 @@ if ( window.wgStagingEnvironment ) {
 	(function(){
 		'use strict';
 
-		var stagingEnvName = window.location.hostname.split( '.' )[0];
+		var stagingEnvName = window.location.hostname.split( '.' )[0],
+			stagingURLPart = '://' + stagingEnvName + '.';
 
 		$('a').each(function() {
 			var $this = $( this ),
 				href = $this.attr( 'href' );
 
-			if ( href.indexOf( '://' ) > -1 && href.indexOf( stagingEnvName ) === -1 ) {
-				$this.attr( 'href', href.replace( '://', '://' + stagingEnvName + '.' ) );
+			if ( href.indexOf( '://' ) > -1 && href.indexOf( stagingURLPart ) === -1 ) {
+				$this.attr( 'href', href.replace( '://', stagingURLPart ) );
 			}
 		});
 	})();
