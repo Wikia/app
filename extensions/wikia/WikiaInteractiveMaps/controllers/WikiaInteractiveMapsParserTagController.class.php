d0d7da26d5326d13824deffd87f2f1939a0c2173
bkoval <brt.kowal@gmail.com>
2014-07-07 18:06:26 +0200

    Use proper extension name

--- extensions/wikia/WikiaInteractiveMaps/controllers/WikiaInteractiveMapsParserTagController.class.php
+++ extensions/wikia/WikiaInteractiveMaps/controllers/WikiaInteractiveMapsParserTagController.class.php
@@ -95,7 +95,7 @@ class WikiaInteractiveMapsParserTagController extends WikiaController {
 			$params->map->imagePlaceholder = $this->wg->BlankImgUrl;
 			$params->map->mobile = true;
 			$params->map->href =
-				WikiaInteractiveMapsController::getSpecialUrl('InteractiveMaps') .
+				WikiaInteractiveMapsController::getSpecialUrl('Maps') .
 				'/' . $params->map->id;
 		} else {
 			$params->map->image = $this->mapsModel->createCroppedThumb( $params->map->image, self::DEFAULT_WIDTH, self::DEFAULT_HEIGHT );
