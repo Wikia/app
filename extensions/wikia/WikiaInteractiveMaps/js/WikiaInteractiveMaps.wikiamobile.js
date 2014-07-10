a1e3159607f2e79bbb09a30b92dbc4f7b8091de6
Rafal Leszczynski <rafal@wikia-inc.com>
2014-07-10 17:37:37 +0200

    remove console.log

--- extensions/wikia/WikiaInteractiveMaps/js/WikiaInteractiveMaps.wikiamobile.js
+++ extensions/wikia/WikiaInteractiveMaps/js/WikiaInteractiveMaps.wikiamobile.js
@@ -1,8 +1,6 @@
 require(['wikia.window', 'ponto'], function(w, ponto) {
 	'use strict';
 
-	console.log('client', Date.now());
-
 	var iframe =  w.document.getElementsByName('wikia-interactive-map')[0];
 
 	if (iframe) {
