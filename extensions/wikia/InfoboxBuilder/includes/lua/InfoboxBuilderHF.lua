dab3cd96098d3959ba57e9c23f30571e42110890
adamkarminski <adamkarminski@gmail.com>
2014-07-10 15:18:56 +0000

    Helper functions clean-up

--- extensions/wikia/InfoboxBuilder/includes/lua/InfoboxBuilderHF.lua
+++ extensions/wikia/InfoboxBuilder/includes/lua/InfoboxBuilderHF.lua
@@ -7,14 +7,6 @@ function HF.explode( sep, text )
   return fields
 end
 
-function HF.trim(s)
-  if type(s) == "string" then
-    return (s:gsub("^%s*(.-)%s*$", "%1"))
-  else
-    return false
-  end
-end
-
 function HF.isempty(s)
   local result = false
   if type(s) == "nil" then
@@ -38,83 +30,6 @@ end
 function HF.round(num, idp)
   local mult = 10^(idp or 0)
   return math.floor(num * mult + 0.5) / mult
-end
-
-function HF.print_r( t, name, indent )
-  local cart     -- a container
-  local autoref  -- for self references
- 
-  --[[ counts the number of elements in a table
-  local function tablecount(t)
-     local n = 0
-     for _, _ in pairs(t) do n = n+1 end
-     return n
-  end
-  ]]
-  -- (RiciLake) returns true if the table is empty
-  local function isemptytable(t) return next(t) == nil end
- 
-  local function basicSerialize (o)
-     local so = tostring(o)
-     if type(o) == "function" then
-        local info = debug.getinfo(o, "S")
-        -- info.name is nil because o is not a calling level
-        if info.what == "C" then
-           return string.format("%q", so .. ", C function")
-        else 
-           -- the information is defined through lines
-           return string.format("%q", so .. ", defined in (" ..
-               info.linedefined .. "-" .. info.lastlinedefined ..
-               ")" .. info.source)
-        end
-     elseif type(o) == "number" or type(o) == "boolean" then
-        return so
-     else
-        return string.format("%q", so)
-     end
-  end
- 
-  local function addtocart (value, name, indent, saved, field)
-     indent = indent or ""
-     saved = saved or {}
-     field = field or name
- 
-     cart = cart .. indent .. field
- 
-     if type(value) ~= "table" then
-        cart = cart .. " = " .. basicSerialize(value) .. ";\n"
-     else
-        if saved[value] then
-           cart = cart .. " = {}; -- " .. saved[value] 
-                       .. " (self reference)\n"
-           autoref = autoref ..  name .. " = " .. saved[value] .. ";\n"
-        else
-           saved[value] = name
-           --if tablecount(value) == 0 then
-           if isemptytable(value) then
-              cart = cart .. " = {};\n"
-           else
-              cart = cart .. " = {\n"
-              for k, v in pairs(value) do
-                 k = basicSerialize(k)
-                 local fname = string.format("%s[%s]", name, k)
-                 field = string.format("[%s]", k)
-                 -- three spaces between levels
-                 addtocart(v, fname, indent .. "   ", saved, field)
-              end
-              cart = cart .. indent .. "};\n"
-           end
-        end
-     end
-  end
- 
-  name = name or "__unnamed__"
-  if type(t) ~= "table" then
-     return name .. " = " .. basicSerialize(t)
-  end
-  cart, autoref = "", ""
-  addtocart(t, name, indent)
-  return cart .. autoref
 end
 
 php = mw_interface
