b7611d1d801746dd1b734e1c15e8bc67728c31e2
Mateusz Warkocki <mateusz.warkocki@wikia-inc.com>
2014-07-14 11:13:57 +0200

    CON-1282 | styling update #1 after CR

--- skins/oasis/js/TopModuleAB.js
+++ skins/oasis/js/TopModuleAB.js
@@ -62,15 +62,15 @@
 							$response.find( '.top-wiki-main.header-test-1' )
 								.css( 'margin', '0 0 10px 0' );
 							$response.find( '.top-wiki-article' )
-								.css( 'float', 'left' ).css( 'margin', '0 0 10px 0' );
+								.css( { 'float': 'left', 'margin': '0 0 10px 0' } );
 							$response.find( '.top-wiki-article-thumbnail' )
-								.css( 'float', 'left' ).css( 'margin', '0 10px 0 0' );
+								.css( { 'float': 'left', 'margin': '0 10px 0 0' } );
 							$response.find( '.top-wiki-article-text' )
-								.css( 'float', 'left' ).css( 'font-size', '15px' ).css( 'width', '175px' );
+								.css( { 'float': 'left', 'font-size': '15px', 'width': '175px' } );
 							$response.find( '.top-wiki-article.hot-article' )
 								.css( 'width', '270px' );
 							$response.find( '.top-wiki-article.hot-article' ).find ( '.top-wiki-article-text' )
-								.css( 'margin', '10px 0' ).css( 'width', '270px' );
+								.css( { 'margin': '10px 0', 'width': '270px' } );
 
 							$topArticlesModule.append( $response ).insertBefore( $wikiaActivityModule );
 
