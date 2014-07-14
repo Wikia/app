e16e1fb385340edd0e81d7d99a4bb5162f5f8bd4
Władysław Bodzek <wladek@wikia-inc.com>
2014-07-14 12:49:02 +0200

    cleaned up ProfilerSimpleUDP.php

--- includes/profiler/ProfilerSimpleUDP.php
+++ includes/profiler/ProfilerSimpleUDP.php
@@ -10,11 +10,8 @@
  * @ingroup Profiler
  */
 class ProfilerSimpleUDP extends ProfilerSimple {
-
-	const SCRIBE_KEY = 'mwprofiler_data';
-
 	public function logData() {
-		global $wgUDPProfilerHost, $wgUDPProfilerPort, $wgProfilerSendViaScribe;
+		global $wgUDPProfilerHost, $wgUDPProfilerPort;
 
 		$this->close();
 
@@ -29,11 +26,6 @@ class ProfilerSimpleUDP extends ProfilerSimple {
 		}
 
 		$profilerId = $this->getProfileID();
-
-		if ( !empty($wgProfilerSendViaScribe) ) {
-			$this->logDataScribe( $profilerId );
-			return;
-		}
 
 		$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 		$plength = 0;
@@ -60,55 +52,4 @@ class ProfilerSimpleUDP extends ProfilerSimple {
 		}
 		socket_sendto( $sock, $packet, $plength, 0x100, $wgUDPProfilerHost, $wgUDPProfilerPort );
 	}
-
-	public function logDataScribe( $profilerId ) {
-		global $wgProfilerMinimumTime;
-
-		if ( !is_callable( array( 'WScribeClient', 'singleton' ) ) ) {
-			if ( function_exists( 'wfIncrStats' ) ) {
-				wfIncrStats('mwprofiler-scribe-not-available');
-			}
-			return false;
-		}
-
-		$entries = array();
-		foreach ( $this->mCollated as $name => $pfdata ) {
-			if( !isset($pfdata['count'])
-				|| !isset( $pfdata['cpu'] )
-				|| !isset( $pfdata['cpu_sq'] )
-				|| !isset( $pfdata['real'] )
-				|| !isset( $pfdata['real_sq'] ) ) {
-				continue;
-			}
-			if ( $pfdata['real'] < $wgProfilerMinimumTime ) {
-				continue;
-			}
-			$entries[$name] = array( $pfdata['count'],
-				round($pfdata['cpu'],6),
-				round($pfdata['cpu_sq'],6),
-				round($pfdata['real'],6),
-				round($pfdata['real_sq'],6),
-			);
-		}
-
-		$data = array(
-			'engine' => 'mwprofiler',
-			'profile' => $profilerId,
-			'entries' => $entries,
-		);
-		if ( is_callable( array( 'Transaction', 'getAll' ) ) ) {
-			$data['context'] = Transaction::getAll();
-		}
-
-		$data = json_encode( $data );
-
-		try {
-			WScribeClient::singleton( self::SCRIBE_KEY )->send( $data );
-		} catch ( TException $e ) {
-			Wikia::log( __METHOD__, 'scribeClient exception', $e->getMessage() );
-		}
-
-		return true;
-	}
-
 }
