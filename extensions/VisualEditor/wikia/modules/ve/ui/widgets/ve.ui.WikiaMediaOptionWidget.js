c97b5729408c43c4e54e87ae4b90a707b18bd85a
Inez Korczynski <inez@wikia-inc.com>
2014-07-15 19:43:34 +0000

    VE-1340: Minor code formatting adjustments

--- extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaMediaOptionWidget.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaMediaOptionWidget.js
@@ -84,7 +84,7 @@ ve.ui.WikiaMediaOptionWidget.prototype.loadThumbnail = function () {
 		// FIXME: Using Thumbnailer should not require this trick of adding "thumb"
 		if ( src.indexOf( '/thumb/') === -1 ) {
 			src = src.split( '/' );
-			src.splice( src.length - 2 , 0 , 'thumb' );
+			src.splice( src.length - 2, 0, 'thumb' );
 			src = src.join( '/' );
 		}
 		this.image.src = src;
