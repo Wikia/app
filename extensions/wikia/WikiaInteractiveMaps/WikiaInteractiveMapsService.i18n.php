d759945ad3ab1f87af7a35e6226d50bc05af22b3
aquilax <evgeniy@wikia-inc.com>
2014-07-17 13:37:45 +0000

    Udated service string translations

--- extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMapsService.i18n.php
+++ extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMapsService.i18n.php
@@ -19,8 +19,7 @@ $messages[ 'en' ] = [
 	'wikia-interactive-maps-create-marker-tooltip' => 'Draw a marker',
 	'wikia-interactive-maps-create-marker-cancel' => 'Cancel',
 	'wikia-interactive-maps-default-poi-category' => 'Other',
-	'wikia-interactive-maps-created-by' =>  'Created by the <b>$1</b> community',
-	'wikia-interactive-maps-edit-on-wikia' =>  'Edit on Wikia',
+	'wikia-interactive-maps-edit-map' =>  'Edit Map',
 ];
 
 
@@ -38,6 +37,5 @@ $messages[ 'qqq' ] = [
 	'wikia-interactive-maps-create-marker-tooltip' => 'Create POI hint',
 	'wikia-interactive-maps-create-marker-cancel' => 'Cancel shown when user clicks on create POI',
 	'wikia-interactive-maps-default-poi-category' => 'Name of the default category for POI',
-	'wikia-interactive-maps-created-by' =>  'Text shown at the attribution footer which is displayed when the map is embedded',
-	'wikia-interactive-maps-edit-on-wikia' =>  'Text for the edit link on the attribution footer which is displayed when the map is embedded',
+	'wikia-interactive-maps-edit-map' =>  'Text for the edit link on the attribution footer which is displayed when the map is embedded',
 ];
