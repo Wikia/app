24915a171157bd42e8135102cc0f20f1cb707340
Bogna Knychala <bogna.ka@gmail.com>
2014-07-21 14:49:51 +0200

    CON-1395 make flags links titles changeable via messages

--- extensions/wikia/WikiaHomePage/WikiaHomePageController.class.php
+++ extensions/wikia/WikiaHomePage/WikiaHomePageController.class.php
@@ -294,7 +294,7 @@ class WikiaHomePageController extends WikiaController {
 					'class' => $lang,
 					'href' => $wiki['url'],
 					'text' => '',
-					'title' => $wiki['wikiTitle']
+					'title' => wfMessage('wikiahome-footer-link-title-' . $lang)->text()
 				);
 			}
 		}
