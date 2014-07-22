60df5e8f4c379fb25c34c2484b3daa74f062d34d
Matt Klucsarits <mattk@wikia-inc.com>
2014-07-22 23:08:04 +0000

    VE-1224 Track more Parsoid errors in Kibana

--- extensions/VisualEditor/wikia/modules/ve/ui/dialogs/ve.ui.WikiaSourceModeDialog.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/dialogs/ve.ui.WikiaSourceModeDialog.js
@@ -199,6 +199,11 @@ ve.ui.WikiaSourceModeDialog.prototype.onParseError = function ( ) {
 		'label': 'dialog-source-parse',
 		'value': ve.now() - this.timings.parseStart
 	} );
+	if ( window.veTrack ) {
+		veTrack( {
+			action: 'parsoid-parsewt-error'
+		} );
+	}
 	// TODO: error handling?
 };
 
