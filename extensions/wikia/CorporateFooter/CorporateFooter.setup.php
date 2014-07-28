5d81ab306af72460c2b8c587156767b387490c3c
Warkot <mateusz.warkocki@wikia-inc.com>
2014-07-28 18:10:31 +0200

    CON-1396 | setup file aded

---
+++ extensions/wikia/CorporateFooter/CorporateFooter.setup.php
@@ -0,0 +1,17 @@
+<?php
+
+$dir = dirname(__FILE__) . '/';
+
+$wgExtensionCredits['specialpage'][] = array(
+		'name' => 'CorporateFooter',
+		'author' => 'Mateusz "Warkot" Warkocki',
+		'description' => 'CorporateFooter',
+		'version' => 1.0
+		);
+
+$wgAutoloadClasses['CorporateFooterController'] =  $dir . 'CorporateFooterController.class.php';
+$wgAutoloadClasses['CorporateFooterHooks'] =  $dir . 'hooks/CorporateFooterHooks.class.php';
+
+$wgHooks['SkinCopyrightFooter'][] = 'CorporateFooterHooks::onSkinCopyrightFooter';
+
+$wgExtensionMessagesFiles['CorporateFooter'] = $dir . 'CorporateFooter.i18n.php';
