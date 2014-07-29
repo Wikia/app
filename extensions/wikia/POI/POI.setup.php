f349290ec34df526038f478de13db9bcea2754da
Yurii Lahodiuk <yurii@wikia-inc.com>
2014-07-29 20:20:29 +0200

    Removed redundant line of code

--- extensions/wikia/POI/POI.setup.php
+++ extensions/wikia/POI/POI.setup.php
@@ -3,6 +3,5 @@
 $dir = dirname(__FILE__) . '/';
 
 //classes
-$wgAutoloadClasses['QuestDetailsController'] =  $dir . 'QuestDetailsApiController.class.php';
 $wgAutoloadClasses['POIApiController'] =  $dir . "POIApiController.class.php";
 $wgAutoloadClasses['QuestDetailsApiController'] = $dir . 'QuestDetailsApiController.class.php';
