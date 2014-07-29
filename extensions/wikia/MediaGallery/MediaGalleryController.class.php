7b36d39f2dffaee6b780c5729280eb2287e68db7
lizlux <liz@lizlux.net>
2014-07-29 16:42:15 -0700

    VID-1854 setting up new MediaGallery extension and routing there when enabled

---
+++ extensions/wikia/MediaGallery/MediaGalleryController.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class MediaGalleryController extends WikiaController {
+	const DEFAULT_TEMPLATE_ENGINE = WikiaResponse::TEMPLATE_ENGINE_MUSTACHE;
+
+	public function gallery() {
+		$items = $this->getVal('items');
+		$media = [];
+
+		foreach ( $items as $item ) {
+			$file = wfFindFile( $item['title'] );
+
+			if ( !$file instanceof File ) {
+				continue; // todo: possible add error state
+			}
+			$thumb = $file->transform(['width'=>500, 'height'=>500]);
+			$media[] = [
+				'src' => wfReplaceImageServer( $thumb->getUrl(), $file->getTimestamp() ), // todo: do we need wfReplaceImageServer?
+			];
+		}
+
+		$this->media = $media;
+	}
+
+}
