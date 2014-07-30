456544c50dcb090f9dcb6e78324ab48068b02a2d
Yurii Lahodiuk <yurii@wikia-inc.com>
2014-07-30 18:46:55 +0200

    Refactoring

--- extensions/wikia/POI/QuestDetailsSearchService.class.php
+++ extensions/wikia/POI/QuestDetailsSearchService.class.php
@@ -52,36 +52,52 @@ class QuestDetailsSearchService extends EntitySearchService {
 	protected function getMetadata( &$item ) {
 		$metadata = [ ];
 
-		$metadataKeys = [];
+		$PREFIX_METADATA = 'metadata_';
+		$LENGTH_PREFIX_METADATA = strlen( $PREFIX_METADATA );
+
+		$SUFFIX_S = '_s';
+		$LENGTH_SUFFIX_S = strlen( $SUFFIX_S );
+
+		$SUFFIX_SS = '_ss';
+		$LENGTH_SUFFIX_SS = strlen( $SUFFIX_SS );
+
+		$SUFFIX_SR = '_sr';
+		$LENGTH_SUFFIX_SR = strlen( $SUFFIX_SR );
+
+		$metadataKeys = [ ];
 		foreach ( $item as $key => $value ) {
-			if ( $this->startsWith( $key, 'metadata_' ) ) {
-				$metadataKeys[] = $key;
+			if ( $this->startsWith( $key, $PREFIX_METADATA ) ) {
+				$metadataKeys[ ] = $key;
 			}
 		}
 
-		foreach($metadataKeys as $key) {
-			$value = $item[$key];
-			if ( $this->endsWith( $key, '_s' ) ) {
+		foreach ( $metadataKeys as $key ) {
+			$value = $item[ $key ];
+			
+			if ( $this->endsWith( $key, $SUFFIX_S ) ) {
 
-				$metadataKey = substr( $key, strlen( 'metadata_' ), strlen( $key ) - strlen( 'metadata_' ) - strlen( '_s' ) );
+				$metadataKey =
+					substr( $key, $LENGTH_PREFIX_METADATA, strlen( $key ) - $LENGTH_PREFIX_METADATA - $LENGTH_SUFFIX_S );
 				$metadata[ $metadataKey ] = $value;
 
-			} else if ( $this->endsWith( $key, '_ss' ) ) {
+			} else if ( $this->endsWith( $key, $SUFFIX_SS ) ) {
 
-				$metadataKey = substr( $key, strlen( 'metadata_' ), strlen( $key ) - strlen( 'metadata_' ) - strlen( '_ss' ) );
+				$metadataKey =
+					substr( $key, $LENGTH_PREFIX_METADATA, strlen( $key ) - $LENGTH_PREFIX_METADATA - $LENGTH_SUFFIX_SS );
 				$metadata[ $metadataKey ] = $value;
 
-			} else if ( $this->endsWith( $key, '_sr' ) ) {
+			} else if ( $this->endsWith( $key, $SUFFIX_SR ) ) {
 
-				$metadataKey = substr( $key, strlen( 'metadata_' ), strlen( $key ) - strlen( 'metadata_' ) - strlen( '_sr' ) );
+				$metadataKey =
+					substr( $key, $LENGTH_PREFIX_METADATA, strlen( $key ) - $LENGTH_PREFIX_METADATA - $LENGTH_SUFFIX_SR );
 
-				$parts = preg_split("/[\s,]+/", $value);
-				$x = $parts[0];
-				$y = $parts[1];
+				$parts = preg_split( "/[\s,]+/", $value );
+				$x = $parts[ 0 ];
+				$y = $parts[ 1 ];
 
-				$metadata[$metadataKey] = [
-					'location_x' => floatval($x),
-					'location_y' => floatval($y),
+				$metadata[ $metadataKey ] = [
+					'location_x' => floatval( $x ),
+					'location_y' => floatval( $y ),
 				];
 			}
 		}
