49e86210a33d3b89dc64be947799de2a73482cda
jacekjursza <jacek@wikia-inc.com>
2014-07-30 17:03:26 +0200

    ArticleMetadataModel

---
+++ extensions/wikia/POI/ArticleMetadataModel.php
@@ -0,0 +1,123 @@
+<?php
+
+class ArticleMetadataModel {
+
+	const quest_id = "quest_id";
+	const fingerprints = "fingerprints";
+	const map_region = "map_region";
+
+	const article_prop_name = WPP_PALANTIR_METADATA;
+
+	protected $articleId = 0;
+	protected $articleTitle = null;
+
+	protected $metadata = [
+		self::quest_id => "",
+		self::map_region => "",
+		self::fingerprints => [],
+	];
+
+	public function __construct( $articleId ) {
+		$this->articleId = (int) $articleId;
+		$this->articleTitle = Title::newFromID( $this->articleId );
+		if ( is_null( $this->articleTitle ) ) {
+			return null;
+		}
+		$this->load();
+	}
+
+
+	/**
+	 * Creates new object if article exists
+	 * @param $title string
+	 * @return ArticleMetadataModel|null
+	 */
+	public static function newFromString( $title ) {
+		$titleObject = Title::newFromText( $title );
+		if ( $titleObject && $titleObject->exists() ) {
+			return new self( $titleObject->getArticleId() );
+		}
+		return null;
+	}
+
+
+	/**
+	 * Load data from database - page_wikia_props
+	 * from prop name defined in self::article_prop_name
+	 */
+	protected function load() {
+		$data = wfGetWikiaPageProp( self::article_prop_name, $this->articleId );
+		foreach ( $this->metadata as $meta_key => $meta_val ) {
+			if ( isset( $data[$meta_key] ) && !empty( $data[$meta_key] ) ) {
+				$this->metadata[$meta_key] = $data[$meta_key];
+			}
+		}
+	}
+
+
+	/**
+	 * Save the metadata to DB ( page_wikia_props )
+	 * to prop name defined in self::article_prop_name
+	 */
+	public function save() {
+		wfSetWikiaPageProp( self::article_prop_name, $this->articleId, $this->getMetadata() );
+	}
+
+	/**
+	 * Set metadata field - you need to manually call save() method in order to save it in the DB
+	 * @param $fieldName
+	 * @param $value
+	 * @throws NotValidPOIMetadataFieldException
+	 */
+	public function setField( $fieldName, $value ) {
+		if ( !isset($this->metadata[ $fieldName ] ) ) {
+			throw new NotValidPOIMetadataFieldException( $fieldName );
+		}
+
+		if ( is_array( $this->metadata[ $fieldName ] ) && !is_array( $value ) ) {
+			throw new FingerprintsNotArrayException('fingerprints should be an array (or use addFingerprint method)');
+		} else {
+			$this->metadata[ $fieldName ] = $value;
+		}
+	}
+
+	public function addFingerprint( $name ) {
+		if ( !in_array($this->metadata[ self::fingerprints ], $name) ) {
+			$this->metadata[ self::fingerprints ][] = $name;
+		}
+	}
+
+	public function removeFingerprint( $name ) {
+		foreach ( $this->metadata[ self::fingerprints ] as $i => $fp ) {
+			if ( $fp == $name ) {
+				unset( $this->metadata[self::fingerprints][$i] );
+			}
+		}
+	}
+
+	public function clearAllFingerprints() {
+		$this->metadata[ self::fingerprints ] = [];
+	}
+
+	/**
+	 * Get single field from metadata
+	 * @param $fieldName
+	 * @return mixed
+	 */
+	public function getField( $fieldName ) {
+		return $this->metadata[ $fieldName ];
+	}
+
+	/**
+	 * Get whole metadata as array
+	 * @return array
+	 */
+	public function getMetadata() {
+		return $this->metadata;
+	}
+
+}
+
+//exceptions
+class NotValidPOIMetadataFieldException extends WikiaException { }
+class FingerprintsNotArrayException extends WikiaException { }
--- extensions/wikia/POI/POI.setup.php
+++ extensions/wikia/POI/POI.setup.php
@@ -5,3 +5,7 @@ $dir = dirname(__FILE__) . '/';
 //classes
 $wgAutoloadClasses['POIApiController'] =  $dir . "POIApiController.class.php";
 $wgAutoloadClasses['QuestDetailsApiController'] = $dir . 'QuestDetailsApiController.class.php';
+$wgAutoloadClasses['ArticleMetadataModel'] = $dir . 'ArticleMetadataModel.php';
+$wgAutoloadClasses['ArticleMetadataService'] = $dir . 'ArticleMetadataService.php';
+
+
