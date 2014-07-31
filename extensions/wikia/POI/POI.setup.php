b8475ca63f68f48fd3574a2aa8bcbaba52bb2995
Yurii Lahodiuk <yurii@wikia-inc.com>
2014-07-31 12:27:28 +0200

    Resolved conflicts after merge

--- extensions/wikia/POI/POI.setup.php
+++ extensions/wikia/POI/POI.setup.php
@@ -1,6 +1,7 @@
 <?php
 
 $dir = dirname(__FILE__) . '/';
+$searchDir = dirname(dirname(__FILE__)) . '/Search/classes/Services/';
 
 //classes
 $wgAutoloadClasses['POIApiController'] =  $dir . "POIApiController.class.php";
@@ -9,6 +10,7 @@ $wgAutoloadClasses['QuestDetailsApiController'] = $dir . 'QuestDetailsApiControl
 $wgAutoloadClasses['MetaCSVService'] = $dir . 'MetaCSVService.class.php';
 $wgAutoloadClasses['ArticleMetadataModel'] = $dir . 'ArticleMetadataModel.php';
 $wgAutoloadClasses['ArticleMetadataService'] = $dir . 'ArticleMetadataService.php';
-
+$wgAutoloadClasses['EntitySearchService'] = $searchDir . "EntitySearchService.php";
+$wgAutoloadClasses['QuestDetailsSearchService'] = $dir . "QuestDetailsSearchService.class.php";
 
 $wgSpecialPages['Metadata']		= 'MetadataSpecialController';
