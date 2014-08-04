7abf81955b6712dba7f9897dbd73457637210e40
James Ogura <jogura@wikia-inc.com>
2014-08-04 15:56:02 -0400

    More CR cleanup (formatting)

--- extensions/wikia/MonetizationModule/scripts/MonetizationModuleTopScript.js
+++ extensions/wikia/MonetizationModule/scripts/MonetizationModuleTopScript.js
@@ -15,14 +15,26 @@
 
     window.isFromSearch = function () {
         var ref = document.referrer;
-        if (ref.indexOf('https://www.google.com/') == 0 || (ref.indexOf('google.') != -1 && ref.indexOf('mail.google.com') == -1 && ref.indexOf('url?q=') == -1 && ref.indexOf('q=') != -1)) return true;
-        if (ref.indexOf('bing.com') != -1 && ref.indexOf('q=') != -1) return true;
-        if (ref.indexOf('yahoo.com') != -1 && ref.indexOf('p=') != -1) return true;
-        if (ref.indexOf('ask.com') != -1 && ref.indexOf('q=') != -1) return true;
-        if (ref.indexOf('aol.com') != -1 && ref.indexOf('q=') != -1) return true;
-        if (ref.indexOf('baidu.com') != -1 && ref.indexOf('wd=') != -1) return true;
-        if (ref.indexOf('yandex.com') != -1 && ref.indexOf('text=') != -1) return true;
-        if (document.cookie.replace(/(?:(?:^|.*;\s*)fromsearch\s*\=\s*([^;]*).*$)|^.*$/, "$1") == "1") return true;
+        if (document.cookie.replace(/(?:(?:^|.*;\s*)fromsearch\s*\=\s*([^;]*).*$)|^.*$/, "$1") == "1") {
+            return true;
+        } else if (ref.indexOf('https://www.google.com/') == 0 || (ref.indexOf('google.') != -1 &&
+                ref.indexOf('mail.google.com') == -1 &&
+                ref.indexOf('url?q=') == -1 &&
+                ref.indexOf('q=') != -1)) {
+            return true;
+        } else if (ref.indexOf('bing.com') != -1 && ref.indexOf('q=') != -1) {
+            return true;
+        } else if (ref.indexOf('yahoo.com') != -1 && ref.indexOf('p=') != -1) {
+            return true;
+        } else if (ref.indexOf('ask.com') != -1 && ref.indexOf('q=') != -1) {
+            return true;
+        } else if (ref.indexOf('aol.com') != -1 && ref.indexOf('q=') != -1) {
+            return true;
+        } else if (ref.indexOf('baidu.com') != -1 && ref.indexOf('wd=') != -1) {
+            return true;
+        } else if (ref.indexOf('yandex.com') != -1 && ref.indexOf('text=') != -1) {
+            return true;
+        }
         return false;
     };
 
