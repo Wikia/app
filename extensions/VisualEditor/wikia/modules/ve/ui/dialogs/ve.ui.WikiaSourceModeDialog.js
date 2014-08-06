9187cb5822058344d7b1f32ea41dfc3dd99f2287
Matt Klucsarits <mattk@wikia-inc.com>
2014-08-06 18:00:04 +0000

    VE-1074 Move LinkSuggest setup to dialog initialization

--- extensions/VisualEditor/wikia/modules/ve/ui/dialogs/ve.ui.WikiaSourceModeDialog.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/dialogs/ve.ui.WikiaSourceModeDialog.js
@@ -64,6 +64,15 @@ ve.ui.WikiaSourceModeDialog.prototype.initialize = function () {
 	this.$body.append( this.sourceModeTextarea.$element );
 	this.$foot.append( this.$helpLink, this.applyButton.$element );
 	this.frame.$content.addClass( 've-ui-wikiaSourceModeDialog-content' );
+	// Add class to iframe body that is required for linksuggest styling
+	this.$( 'body' ).addClass( 'skin-oasis' );
+	// Initialize linksuggest on the dialog textarea
+	mw.loader.using(
+		'ext.wikia.LinkSuggest',
+		ve.bind( function () {
+			this.sourceModeTextarea.$input.linksuggest();
+		}, this )
+	);
 };
 
 /**
@@ -84,15 +93,6 @@ ve.ui.WikiaSourceModeDialog.prototype.getSetupProcess = function ( data ) {
 			this.target.serialize(
 				ve.dm.converter.getDomFromModel( doc, false ),
 				ve.bind( this.onSerialize, this )
-			);
-			// Add class to iframe body that is required for linksuggest styling
-			this.$( 'body' ).addClass( 'skin-oasis' );
-			// Initialize linksuggest on the dialog textarea
-			mw.loader.using(
-				'ext.wikia.LinkSuggest',
-				ve.bind( function () {
-					this.sourceModeTextarea.$input.linksuggest();
-				}, this )
 			);
 		}, this );
 };
