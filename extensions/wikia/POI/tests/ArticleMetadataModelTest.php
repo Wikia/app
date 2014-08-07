17b3319693976546a266847a99d81412dd9f3cb2
Pawel Chojnacki <pawel@chojnacki.ws>
2014-08-07 22:38:34 +0200

    mark test as integration because it uses Database

--- extensions/wikia/POI/tests/ArticleMetadataModelTest.php
+++ extensions/wikia/POI/tests/ArticleMetadataModelTest.php
@@ -59,12 +59,12 @@ class ArticleMetadataModelTest extends WikiaBaseTest {
 	}
 
 	/**
+	 * @group Integration
 	 * @expectedException TitleNotFoundException
 	 * @covers ArticleMetadataModel::newFromString
 	 */
 	public function testIndexNotFound() {
 		ArticleMetadataModel::newFromString("bnvghcfrt6t7y8uhgftdr567");
-
 	}
 
 
