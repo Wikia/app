ee5c63edfa8615495c8d741bf8bfd5009965ebb0
Michał ‘Mix’ Roszka <mix@wikia-inc.com>
2014-08-07 10:45:51 +0200

    initial implementation

---
+++ extensions/wikia/CheckpointCharlie/CheckpointCharlie.i18n.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file CheckpointCharlie.i18n.php
+ * @brief I18n for the CheckpointCharlie MediaWiki extension.
+ * @author Michał ‘Mix’ Roszka <mix@wikia-inc.com>
+ * @date Thursday, 7 August 2014 (created)
+ */
+$messages = array();
+/**
+ * Message documentation.
+ */
+
+$messages['qqq'] = array(
+    'checkpointcharlie-desc' => 'A brief plain text description of the extension used on the Special:Version page.',
+    'checkpointcharlie-title' => 'The title of the error page.',
+    'checkpointcharlie-content' => 'The content of the error page.',
+);
+
+/**
+ * English.
+ */
+$messages['en'] = array(
+    'checkpointcharlie-desc' => 'Whitelist IP addresses allowed access to special pages.',
+    'checkpointcharlie-title' => 'Page not available',
+    'checkpointcharlie-content' => 'Due to security concerns, this page is currently unavailable. Feel free to check back shortly. Sorry for the trouble and thanks for your patience!',
+);
+
