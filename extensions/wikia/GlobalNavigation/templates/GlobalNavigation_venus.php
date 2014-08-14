a09ad7e414f1320c8383e726d19a6f23b47ff1fd
Damian Jozwiak <damian@wikia-inc.com>
2014-08-14 17:14:57 +0200

    CON-1448 | Global Header Logo

--- extensions/wikia/GlobalNavigation/templates/GlobalNavigation_venus.php
+++ extensions/wikia/GlobalNavigation/templates/GlobalNavigation_venus.php
@@ -1,13 +1,13 @@
 <nav class="global-navigation">
 	<div class="page-width">
 		<div class="global-navigation-item">
-			<a href="<?= htmlspecialchars($centralUrl) ?>" rel="nofollow"><img src="<?= $wg->BlankImgUrl ?>" class="logo" height="23" width="91" alt="Wikia">Test</a>
+			<a href="<?= htmlspecialchars($centralUrl) ?>" class="global-navigation-link wikia-logo" rel="nofollow"><img src="<?= $wg->BlankImgUrl ?>" height="24" width="91" alt="Wikia"></a>
 		</div>
 		<div class="global-navigation-item">
 			Search goes here
 		</div>
 		<div class="global-navigation-item">
-			<a href="<?= htmlspecialchars($createWikiUrl) ?>" class="start-wikia"><?= wfMessage('global-navigation-create-wiki')->text(); ?></a>
+			<a href="<?= htmlspecialchars($createWikiUrl) ?>" class="global-navigation-link start-wikia"><?= wfMessage('global-navigation-create-wiki')->text(); ?></a>
 		</div>
 		<div class="global-navigation-item">
 			user login
