b4a757c7fd0ab6469813c5e09e07e9c05d8d64f2
lizlux <liz@lizlux.net>
2014-08-15 16:02:02 -0700

    VID-1854 setting up JS for multiple instances

--- extensions/wikia/MediaGallery/scripts/controllers/index.js
+++ extensions/wikia/MediaGallery/scripts/controllers/index.js
@@ -2,11 +2,17 @@ require(['mediaGallery.toggler'], function (Toggler) {
 	'use strict';
 	$(function () {
 		var maxImagesShown = 8,
-			toggler = new Toggler({
-				$el: $('.media-gallery-wrapper')
+			$galleries = $('.media-gallery-wrapper'),
+			togglers = [];
+
+		$galleries.each(function () {
+			var toggler = new Toggler({
+				$el: $(this)
 			});
-		if (toggler.$media.length > maxImagesShown) {
-			toggler.init();
-		}
+			if (toggler.$media.length > maxImagesShown) {
+				toggler.init();
+				togglers.push(toggler);
+			}
+		});
 	});
 });
