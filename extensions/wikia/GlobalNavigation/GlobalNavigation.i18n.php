34e5c548150660038d4007ce1429978c1085f590
Damian Jozwiak <damian@wikia-inc.com>
2014-08-18 17:23:08 +0200

    CON-1448 | Global Header i18n

--- extensions/wikia/GlobalNavigation/GlobalNavigation.i18n.php
+++ extensions/wikia/GlobalNavigation/GlobalNavigation.i18n.php
@@ -4,4 +4,7 @@ $messages = [];
 $messages['en'] = [
 	'global-navigation-create-wiki' => 'Start a wikia',
 ];
-// TODO qqs and other languages
+
+$messages['qqq'] = [
+	'global-navigation-create-wiki' => 'Label on create new wikia button on Global Navigation - top bar',
+];
