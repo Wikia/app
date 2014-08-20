0928387e58fdfa91b46fb694c88b606e25219130
Bogna Knychala <bogna.ka@gmail.com>
2014-08-20 12:32:48 +0200

    CON-1445 implement search in global nav

--- extensions/wikia/GlobalNavigation/templates/GlobalNavigation_index.php
+++ extensions/wikia/GlobalNavigation/templates/GlobalNavigation_index.php
@@ -3,8 +3,8 @@
 		<div class="global-navigation-item">
 			<a href="<?= htmlspecialchars($centralUrl) ?>" class="global-navigation-link wikia-logo" rel="nofollow"><img src="<?= $wg->BlankImgUrl ?>" height="24" width="91" alt="Wikia"></a>
 		</div>
-		<div class="global-navigation-item">
-			Search goes here
+		<div class="global-navigation-item search-container">
+			<?= $app->renderView('GlobalNavigation', 'searchVenus'); ?>
 		</div>
 		<div class="global-navigation-item">
 			<a href="<?= htmlspecialchars($createWikiUrl) ?>" class="global-navigation-link start-wikia"><span><?= wfMessage('global-navigation-create-wiki')->text(); ?></span></a>
