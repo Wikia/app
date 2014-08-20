396b4e7ecbf1fb9a663ecbf3afd5dae56c41b1a4
Kamil Koterba <kamil@wikia-inc.com>
2014-08-20 14:52:11 +0200

    CE-933 Adding SignupConfirmEmailComplete hook on FB user creation

--- extensions/wikia/UserLogin/UserLoginFacebookForm.class.php
+++ extensions/wikia/UserLogin/UserLoginFacebookForm.class.php
@@ -50,6 +50,7 @@ class UserLoginFacebookForm extends UserLoginForm {
 
 		if ($user instanceof User) {
 			$user->confirmEmail();
+			wfRunHooks( 'SignupConfirmEmailComplete', array( $user ) );
 			$this->connectWithFacebook($user);
 			$user->saveSettings();
 
