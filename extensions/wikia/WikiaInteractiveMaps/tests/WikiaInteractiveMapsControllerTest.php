eb07543515b58383f1cf2ae28c345a4ef9a0cd74
Igor Rogatty <igor@wikia-inc.com>
2014-08-22 10:29:17 +0200

    More readable tests

--- extensions/wikia/WikiaInteractiveMaps/tests/WikiaInteractiveMapsControllerTest.php
+++ extensions/wikia/WikiaInteractiveMaps/tests/WikiaInteractiveMapsControllerTest.php
@@ -13,36 +13,39 @@ class WikiaInteractiveMapsControllerTest extends WikiaBaseTest {
 
 	public function testRedirectIfForeignWiki_not_foreign() {
 		$wikiaInteractiveMapsControllerMock = $this->getWikiaInteractiveMapsControllerMock();
+
+		$wikiaInteractiveMapsControllerMock->wg->CityId = self::WIKI_CITY_ID;
+
+		$outputPageMock = $this->getMock( 'OutputPage', [ 'redirect' ], [], '', false );
+		$outputPageMock->expects( $this->never() )
+			->method( 'redirect' );
+		$wikiaInteractiveMapsControllerMock->wg->out = $outputPageMock;
+
 		$wikiaInteractiveMapsControllerMock->redirectIfForeignWiki( self::WIKI_CITY_ID, 1 );
 	}
 
 	public function testRedirectIfForeignWiki_foreign() {
 		$wikiaInteractiveMapsControllerMock = $this->getWikiaInteractiveMapsControllerMock( true );
+
+		$wikiaInteractiveMapsControllerMock->wg->CityId = self::WIKI_CITY_ID;
+
+		$outputPageMock = $this->getMock( 'OutputPage', [ 'redirect' ], [], '', false );
+		$outputPageMock->expects( $this->once() )
+			->method( 'redirect' );
+		$wikiaInteractiveMapsControllerMock->wg->out = $outputPageMock;
+
 		$wikiaInteractiveMapsControllerMock->redirectIfForeignWiki( self::WIKI_FOREIGN_CITY_ID, 1 );
 	}
 
 	/**
-	 * Returns WikiaInteractiveMapsController mock
-	 *
-	 * @param bool $expectRedirection
 	 * @return PHPUnit_Framework_MockObject_MockObject|WikiaInteractiveMapsController
 	 */
-	private function getWikiaInteractiveMapsControllerMock( $expectRedirection = false ) {
+	private function getWikiaInteractiveMapsControllerMock() {
 		$mock = $this->getMock( 'WikiaInteractiveMapsController', [ 'getWikiPageUrl' ], [], '', false );
 
 		$mock->expects( $this->any() )
 			->method( 'getWikiPageUrl' )
 			->will( $this->returnValue( self::WIKI_URL ) );
-
-		//wg
-		$mock->wg->CityId = self::WIKI_CITY_ID;
-
-		$outputPageMock = $this->getMock( 'OutputPage', [ 'redirect' ], [], '', false );
-		if ( $expectRedirection ) {
-			$outputPageMock->expects( $this->once() )
-				->method( 'redirect' );
-		}
-		$mock->wg->out = $outputPageMock;
 
 		return $mock;
 	}
