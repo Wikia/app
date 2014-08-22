46974b546bc985733cd1b59efc8e3efa6219677b
lizlux <liz@lizlux.net>
2014-08-21 18:13:31 -0700

    VID-1939 tweaks to captions code

--- extensions/wikia/MediaGallery/scripts/views/media.js
+++ extensions/wikia/MediaGallery/scripts/views/media.js
@@ -4,6 +4,11 @@ define('mediaGallery.media', [], function () {
 	function Media(options) {
 		this.$el = options.$el;
 		this.$caption = this.$el.find('.caption');
+
+		// cache some css values that don't change
+		this.mediaPadding = parseFloat(this.$el.css('padding-bottom'));
+		this.captionPadding = parseFloat(this.$caption.css('padding-top'));
+		this.marginTop = parseFloat(this.$caption.css('margin-top'));
 	}
 
 	Media.prototype.init = function () {
@@ -22,9 +27,8 @@ define('mediaGallery.media', [], function () {
 	 * Note: it's hard to do this animation with css b/c the new top margin value is unknown
 	 */
 	Media.prototype.captionHover = function () {
-		var mediaPadding = parseFloat(this.$el.css('padding-bottom')),
+		var self = this,
 			mediaHeight = this.$el.height(),
-			captionPadding = parseFloat(this.$caption.css('padding-top')),
 			contentHeight,
 			newMarginTop;
 
@@ -32,17 +36,29 @@ define('mediaGallery.media', [], function () {
 		this.$caption.addClass('hovered');
 
 		// calculate height of content plus padding of it's container
-		contentHeight = this.$caption.find('.inner').outerHeight() + (2 * captionPadding);
+		contentHeight = this.$caption.find('.inner').outerHeight() + (2 * this.captionPadding);
 
-		// get new negative top margin but limit it to 100% of it's container; adjust for container padding.
-		newMarginTop = -1 * (Math.min(mediaHeight, contentHeight) + mediaPadding);
+		// get new negative top margin but limit it to 100% of it's container
+		// then adjust for container padding
+		newMarginTop = -1 * (Math.min(mediaHeight, contentHeight) + this.mediaPadding);
 
+		// handle small rounding inacuracies to avoid blips on hover of short captions
+		if (Math.abs(Math.abs(this.marginTop) - Math.abs(newMarginTop)) < 2) {
+			return;
+		}
+
+		// do the animation
 		this.$caption.css('marginTop', newMarginTop);
+
+		// add a scrollbar to long captions
+		if (contentHeight > mediaHeight) {
+			self.$caption.addClass('scroll');
+		}
 	};
 
 	Media.prototype.captionHoverOut = function () {
 		this.$caption
-			.removeClass('hovered')
+			.removeClass('hovered scroll')
 			.removeAttr('style');
 	};
 
