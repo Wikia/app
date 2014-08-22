84d4740b8a752dd3ae507e9d72301c5d4a4ce34d
Adam Robak <adamr@wikia-inc.com>
2014-08-22 16:10:49 +0200

    PLA-1790 added mising test

--- extensions/wikia/Search/classes/Test/Services/NearbyPOISearchServiceTest.php
+++ extensions/wikia/Search/classes/Test/Services/NearbyPOISearchServiceTest.php
@@ -34,4 +34,16 @@ class NearbyPOISearchServiceTest extends SearchServiceBaseTest {
 	protected function getMockResponse() {
 		return '{"responseHeader":{"status":0,"QTime":1,"params":{"fl":"id,metadata*","sort":"score desc","q":"metadata_map_location_sr:*","wt":"json","rows":"122"}},"response":{"numFound":2,"start":0,"docs":[{"id":"831_155836","metadata_fingerprint_ids_ss":["amazing","great_job","best"],"metadata_quest_id_s":"very_good","metadata_map_location_sr":"1.11244,-1.21412","metadata_map_region_s":"Map_Region_1"},{"id":"831_8938","metadata_fingerprint_ids_ss":["amazing","great_job","best"],"metadata_quest_id_s":"very_good","metadata_map_location_sr":"1.11244,1.11412","metadata_map_region_s":"Map_Region_1"}]}}';
 	}
+
+	protected function getMockRequest() {
+		$mockQuery = new \Solarium_Query_Select();
+
+		$mockQuery->setQuery( '({!geofilt score=distance sfield=metadata_map_location_sr pt=0,0 d=300})' );
+		$mockQuery->setRows( 200 );
+
+		$mockQuery->setFields( [ 'id', 'metadata_*', 'score'] );
+		$mockQuery->addSort( 'score', 'asc' );
+
+		return $mockQuery;
+	}
 }
