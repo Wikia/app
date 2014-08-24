f5b034a20ddbafe0004e9fe381ff403eb26a65c1
rafal.kalinski <kalina@wikia-inc.com>
2014-08-24 16:19:40 +0200

    CON-1444 | use of menu-aim module for GlobalNav (with lazy loading)

--- extensions/wikia/GlobalNavigation/js/GlobalNavigationHubsMenu.js
+++ extensions/wikia/GlobalNavigation/js/GlobalNavigationHubsMenu.js
@@ -1,4 +1,40 @@
 (function(){
-	// TODO
-	console.log('menuAim code goes here');
+	var $hubLinks;
+
+	$hubLinks = $('.hub-links');
+
+	/**
+	 * menuAim is a method from an external module to handle dropdown menus with very good user experience
+	 * @see https://github.com/Wikia/js-menu-aim
+	 */
+	menuAim(
+		document.querySelector('.hubs'), {
+		rowSelector: 'nav',
+		tolerance: 85,
+		activate: activateSubmenu,
+		deactivate: deactivateSubmenu
+	});
+
+	function activateSubmenu (row) {
+		var subMenuSelector, vertical;
+
+		vertical = $(row).addClass('active').data('vertical');
+		subMenuSelector = '.' + vertical + '-links';
+
+		if (window.lazyLoad === undefined) {
+			throw("There isn't lazyLoad object loaded!");
+		}
+
+		if (!lazyLoad.isMenuWorking()) {
+			lazyLoad.getMenuItems(subMenuSelector);
+		}
+		else {
+			$(subMenuSelector, $hubLinks).addClass('active');
+		}
+	}
+
+	function deactivateSubmenu (row) {
+		$('.hub-links > section').add(row).removeClass('active');
+	}
+
 })();
