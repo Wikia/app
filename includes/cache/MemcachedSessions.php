4f4697cbf3a3f3251c94ec0a91327fef93aac57d
Michał ‘Mix’ Roszka <mix@wikia-inc.com>
2014-08-26 11:19:31 +0200

    porting PLATFORM-308 related changes

--- includes/cache/MemcachedSessions.php
+++ includes/cache/MemcachedSessions.php
@@ -29,6 +29,10 @@ function memsess_key( $id ) {
  * @return Boolean: success
  */
 function memsess_open( $save_path, $session_name ) {
+	/** Wikia change - begin - PLATFORM-308 */
+	global $wgSessionDebugData;
+	$wgSessionDebugData[] = [ 'event' => 'open', 'save_path' => $save_path, 'session_name' => $session_name ];
+	/** Wikia change - end */
 	return true;
 }
 
@@ -39,6 +43,10 @@ function memsess_open( $save_path, $session_name ) {
  * @return Boolean: success
  */
 function memsess_close() {
+	/** Wikia change - begin - PLATFORM-308 */
+	global $wgSessionDebugData;
+	$wgSessionDebugData[] = [ 'event' => 'close' ];
+	/** Wikia change - end */
 	return true;
 }
 
@@ -49,8 +57,26 @@ function memsess_close() {
  * @return Mixed: session data
  */
 function memsess_read( $id ) {
+	/** Wikia change - begin - PLATFORM-308 */
+	global $wgSessionDebugData;
+
+	try {
+		if ( empty( $id ) ) {
+			throw new Exception();
+		}
+	} catch ( Exception $e ) {
+		$wgSessionDebugData[] = [ 'event' => 'read', 'id' => 'empty', 'backtrace' => json_encode( $e->getTrace() ) ];
+		memsess_destroy( $id );
+		return true;
+	}
+	/** Wikia change - end */
+
 	$memc =& getMemc();
 	$data = $memc->get( memsess_key( $id ) );
+
+	/** Wikia change - begin - PLATFORM-308 */
+	$wgSessionDebugData[] = [ 'event' => 'read', 'id' => $id ];
+	/** Wikia change - end */
 
 	if( ! $data ) return '';
 	return $data;
@@ -64,8 +90,26 @@ function memsess_read( $id ) {
  * @return Boolean: success
  */
 function memsess_write( $id, $data ) {
+	/** Wikia change - begin - PLATFORM-308 */
+	global $wgSessionDebugData;
+
+	try {
+		if ( empty( $id ) ) {
+			throw new Exception();
+		}
+	} catch ( Exception $e ) {
+		$wgSessionDebugData[] = [ 'event' => 'write', 'id' => 'empty', 'backtrace' => json_encode( $e->getTrace() ) ];
+		memsess_destroy( $id );
+		return true;
+	}
+	/** Wikia change - end */
+
 	$memc =& getMemc();
 	$memc->set( memsess_key( $id ), $data, 3600 );
+
+	/** Wikia change - begin - PLATFORM-308 */
+	$wgSessionDebugData[] = [ 'event' => 'write', 'id' => $id, 'data' => $data ];
+	/** Wikia change - end */
 
 	return true;
 }
@@ -79,6 +123,11 @@ function memsess_write( $id, $data ) {
 function memsess_destroy( $id ) {
 	$memc =& getMemc();
 	$memc->delete( memsess_key( $id ) );
+
+	/** Wikia change - begin - PLATFORM-308 */
+	global $wgSessionDebugData;
+	$wgSessionDebugData[] = [ 'event' => 'destroy', 'id' => $id ];
+	/** Wikia change - end */
 
 	return true;
 }
@@ -95,7 +144,29 @@ function memsess_gc( $maxlifetime ) {
 }
 
 function memsess_write_close() {
+	/** Wikia change - begin - PLATFORM-308 */
+	global $wgSessionDebugData, $wgRequest, $wgUser;
+	$wgSessionDebugData[] = [ 'event' => 'write_close-begin' ];
+	/** Wikia change - end */
 	session_write_close();
+	/** Wikia change - begin - PLATFORM-308 */
+	$wgSessionDebugData[] = [ 'event' => 'write_close-end' ];
+	$sBrowser = isset( $_SERVER['HTTP_USER_AGENT'] )? $_SERVER['HTTP_USER_AGENT'] : 'unknown';
+	$sCookie = isset( $_COOKIE['wikicities_session'] )? $_COOKIE['wikicities_session'] : 'empty';
+	\Wikia\Logger\WikiaLogger::instance()->debug(
+		'PLATFORM-308',
+		[
+			'data'       => $wgSessionDebugData,
+			'ip'         => IP::sanitizeIP( $wgRequest->getIP() ),
+			'user_id'    => $wgUser->getId(),
+			'user_name'  => $wgUser->getName(),
+			'session_id' => session_id(),
+			'user_agent' => $sBrowser,
+			'cookie'     => $sCookie
+
+		]
+	);
+	/** Wikia change - end */
 }
 
 /* Wikia */
@@ -120,4 +191,3 @@ function &getMemc() {
 		return $wgMemc;
 	}
 }
-
