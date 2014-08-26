68f1005f9d9646691b5b763c54d9a066ca331868
Yurii Lahodiuk <yurii@wikia-inc.com>
2014-08-26 15:47:52 +0200

    Comment

--- includes/wikia/api/ArticlesApiController.class.php
+++ includes/wikia/api/ArticlesApiController.class.php
@@ -1075,11 +1075,11 @@ class ArticlesApiController extends WikiaApiController {
 	 * that items, which urls are in array $links - will be moved to the beginning of array.
 	 *
 	 * Example:
-	 * 1) Assume that $popular = [ [ url: '1'], [ url: '2'], [ url: '3'], [ url: '4'], [ url: '5'], [ url: '6'] ]
+	 * 1) Assume that $popular = [ [ url => '1'], [ url => '2'], [ url => '3'], [ url => '4'], [ url => '5'], [ url => '6'] ]
 	 * 2) Assume that $links = [ '2', '4', '5', '100', '200' ]
 	 *
 	 * This method will returns following array:
-	 * [ [ url: '2'], [ url: '4'], [ url: '5'], [ url: '1'], [ url: '3'], [ url: '6'] ]
+	 * [ [ url => '2'], [ url => '4'], [ url => '5'], [ url => '1'], [ url => '3'], [ url => '6'] ]
 	 *
 	 * @param popular - array of objects, which contains field 'url'
 	 * @param links - array of strings
