16ca3c84e30c4c18dfb3d8f3de1c11e6dc4b583e
nAndy <nandy@wikia-inc.com>
2014-08-27 13:29:04 +0000

    unit tests refactoring

--- extensions/wikia/WikiaInteractiveMaps/tests/WikiaInteractiveMapsPoiControllerTest.php
+++ extensions/wikia/WikiaInteractiveMaps/tests/WikiaInteractiveMapsPoiControllerTest.php
@@ -1,6 +1,10 @@
 <?php
 class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 
+	const USER_TYPE_LOGGED_IN = 'logged-in';
+	const USER_TYPE_LOGGED_OUT = 'logged-out';
+	const USER_TYPE_BLOCKED = 'blocked';
+
 	public function setUp() {
 		global $IP;
 		$this->setupFile = "$IP/extensions/wikia/WikiaInteractiveMaps/WikiaInteractiveMaps.setup.php";
@@ -8,10 +12,7 @@ class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 	}
 
 	public function testEditPoiCategories_throws_permission_error_when_anon() {
-		$userMock = $this->getUserMock();
-		$userMock->expects( $this->once() )
-			->method( 'isLoggedIn' )
-			->willReturn( false );
+		$userMock = $this->getUserMock( self::USER_TYPE_LOGGED_OUT );
 
 		$controllerMock = $this->getWikiaInteractiveMapsPoiControllertMock();
 		$controllerMock->wg->User = $userMock;
@@ -21,13 +22,7 @@ class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 	}
 
 	public function testEditPoiCategories_throws_permission_error_when_user_blocked() {
-		$userMock = $this->getUserMock();
-		$userMock->expects( $this->once() )
-			->method( 'isLoggedIn' )
-			->willReturn( true );
-		$userMock->expects( $this->once() )
-			->method( 'isBlocked' )
-			->willReturn( true );
+		$userMock = $this->getUserMock( self::USER_TYPE_BLOCKED );
 
 		$controllerMock = $this->getWikiaInteractiveMapsPoiControllertMock();
 		$controllerMock->wg->User = $userMock;
@@ -37,19 +32,12 @@ class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 	}
 
 	public function testEditPoi_throws_permission_error_when_anon() {
-		$userMock = $this->getUserMock();
-
-		$userMock->expects( $this->once() )
-			->method( 'isLoggedIn' )
-			->willReturn( false );
+		$userMock = $this->getUserMock( self::USER_TYPE_LOGGED_OUT );
 
 		$controllerMock = $this->getWikiaInteractiveMapsPoiControllertMock();
 		$controllerMock->expects( $this->any() )
 			->method( 'getData' )
 			->willReturn( 'Mocked Data.' );
-		$controllerMock->expects( $this->any() )
-			->method( 'isValidArticleTitle' )
-			->willReturn( true );
 
 		$controllerMock->wg->User = $userMock;
 
@@ -58,22 +46,12 @@ class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 	}
 
 	public function testEditPoi_throws_permission_error_when_blocked() {
-		$userMock = $this->getUserMock();
-
-		$userMock->expects( $this->once() )
-			->method( 'isLoggedIn' )
-			->willReturn( true );
-		$userMock->expects( $this->once() )
-			->method( 'isBlocked' )
-			->willReturn( true );
+		$userMock = $this->getUserMock( self::USER_TYPE_BLOCKED );
 
 		$controllerMock = $this->getWikiaInteractiveMapsPoiControllertMock();
 		$controllerMock->expects( $this->any() )
 			->method( 'getData' )
 			->willReturn( 'Mocked Data.' );
-		$controllerMock->expects( $this->any() )
-			->method( 'isValidArticleTitle' )
-			->willReturn( true );
 
 		$controllerMock->wg->User = $userMock;
 
@@ -82,14 +60,7 @@ class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 	}
 
 	public function testDeletePoi_throws_permission_error_when_anon() {
-		$userMock = $this->getUserMock();
-
-		$userMock->expects( $this->once() )
-			->method( 'isLoggedIn' )
-			->willReturn( true );
-		$userMock->expects( $this->once() )
-			->method( 'isBlocked' )
-			->willReturn( true );
+		$userMock = $this->getUserMock( self::USER_TYPE_BLOCKED );
 
 		$controllerMock = $this->getWikiaInteractiveMapsPoiControllertMock();
 		$controllerMock->expects( $this->any() )
@@ -104,11 +75,7 @@ class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 	}
 
 	public function testDeletePoi_throws_permission_error_when_blocked() {
-		$userMock = $this->getUserMock();
-
-		$userMock->expects( $this->once() )
-			->method( 'isLoggedIn' )
-			->willReturn( false );
+		$userMock = $this->getUserMock( self::USER_TYPE_LOGGED_OUT );
 
 		$controllerMock = $this->getWikiaInteractiveMapsPoiControllertMock();
 		$controllerMock->expects( $this->any() )
@@ -137,16 +104,41 @@ class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 			] )
 			->disableOriginalConstructor()
 			->getMock();
+
+		$controllerMock->expects( $this->any() )
+			->method( 'isValidArticleTitle' )
+			->willReturn( true );
+
 		$controllerMock->request = $requestMock;
 
 		return $controllerMock;
 	}
 
-	private function getUserMock() {
-		return $this->getMockBuilder( 'User' )
+	private function getUserMock( $type = 'default' ) {
+		$userMock = $this->getMockBuilder( 'User' )
 			->setMethods( [ 'getName', 'isLoggedIn', 'isBlocked' ] )
 			->disableOriginalConstructor()
 			->getMock();
+
+		switch( $type ) {
+			case self::USER_TYPE_LOGGED_OUT:
+				$userMock->expects( $this->once() )
+					->method( 'isLoggedIn' )
+					->willReturn( false );
+				break;
+			case self::USER_TYPE_BLOCKED:
+				$userMock->expects( $this->once() )
+					->method( 'isBlocked' )
+					->willReturn( true );
+				// no break for purpose - if user is blocked she's logged-in as well
+			case self::USER_TYPE_LOGGED_IN:
+				$userMock->expects( $this->once() )
+					->method( 'isLoggedIn' )
+					->willReturn( true );
+				break;
+		}
+
+		return $userMock;
 	}
 
 }
