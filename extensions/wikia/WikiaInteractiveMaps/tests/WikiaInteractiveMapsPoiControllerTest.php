f6fb21baebcc50184085422b1dcf98de190cb628
nAndy <nandy@wikia-inc.com>
2014-08-27 13:15:46 +0000

    added two more tests which are failing; skipped them

--- extensions/wikia/WikiaInteractiveMaps/tests/WikiaInteractiveMapsPoiControllerTest.php
+++ extensions/wikia/WikiaInteractiveMaps/tests/WikiaInteractiveMapsPoiControllerTest.php
@@ -57,7 +57,57 @@ class WikiaInteractiveMapsPoiControllerTest extends WikiaBaseTest {
 		$controllerMock->editPoi();
 	}
 
+	public function testEditPoi_throws_permission_error_when_blocked() {
+		$this->markTestSkipped();
+
+		$userMock = $this->getUserMock();
+
+		$userMock->expects( $this->once() )
+			->method( 'isLoggedIn' )
+			->willReturn( true );
+		$userMock->expects( $this->once() )
+			->method( 'isBlocked' )
+			->willReturn( true );
+
+		$controllerMock = $this->getWikiaInteractiveMapsPoiControllertMock();
+		$controllerMock->expects( $this->any() )
+			->method( 'getData' )
+			->willReturn( 'Mocked Data.' );
+		$controllerMock->expects( $this->any() )
+			->method( 'isValidArticleTitle' )
+			->willReturn( true );
+
+		$controllerMock->wg->User = $userMock;
+
+		$this->setExpectedException( 'PermissionsException' );
+		$controllerMock->editPoi();
+	}
+
 	public function testDeletePoi_throws_permission_error_when_anon() {
+		$this->markTestSkipped();
+		
+		$userMock = $this->getUserMock();
+
+		$userMock->expects( $this->once() )
+			->method( 'isLoggedIn' )
+			->willReturn( true );
+		$userMock->expects( $this->once() )
+			->method( 'isBlocked' )
+			->willReturn( true );
+
+		$controllerMock = $this->getWikiaInteractiveMapsPoiControllertMock();
+		$controllerMock->expects( $this->any() )
+			->method( 'getData' )
+			->with( 'poiId' )
+			->willReturn( 1 );
+
+		$controllerMock->wg->User = $userMock;
+
+		$this->setExpectedException( 'PermissionsException' );
+		$controllerMock->deletePoi();
+	}
+
+	public function testDeletePoi_throws_permission_error_when_blocked() {
 		$userMock = $this->getUserMock();
 
 		$userMock->expects( $this->once() )
