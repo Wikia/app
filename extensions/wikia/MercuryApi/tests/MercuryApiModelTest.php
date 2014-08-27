56a5987c9a5aaf43dcbc662aaf4c67cee14254dd
aquilax <evgeniy@wikia-inc.com>
2014-08-27 12:15:16 +0000

    Added unit test

---
+++ extensions/wikia/MercuryApi/tests/MercuryApiModelTest.php
@@ -0,0 +1,107 @@
+<?php
+class MercuryApiModelTest extends WikiaBaseTest {
+
+	public function setUp() {
+		global $IP;
+		$this->setupFile = "$IP/extensions/wikia/MercuryApi/MercuryApi.setup.php";
+		parent::setUp();
+	}
+
+	/**
+	 * Setup global variables used to generate Ads context
+	 */
+	private function setupGlobals () {
+		global $wgLoadAdsInHead, $wgAdEngineDisableLateQueue, $wgLoadLateAdsAfterPageLoad,
+		$wgAdDriverTrackState, $wgEnableWikiaHubsV3Ext, $wgAdDriverSevenOneMediaOverrideSub2Site,
+		$wgWikiDirectedAtChildren, $wgAdDriverForceLiftiumAd, $wgAdDriverForceDirectGptAd, $wgAdDriverUseSevenOneMedia,
+		$wgWikiDirectedAtChildrenByStaff, $wgAdDriverUseEbay;
+
+		$wgLoadAdsInHead = true;
+		$wgAdEngineDisableLateQueue = true;
+		$wgLoadLateAdsAfterPageLoad = true;
+		$wgAdDriverTrackState = true;
+		$wgEnableWikiaHubsV3Ext = true;
+		$wgAdDriverSevenOneMediaOverrideSub2Site = true;
+		$wgWikiDirectedAtChildren = true;
+		$wgAdDriverForceLiftiumAd = true;
+		$wgAdDriverForceDirectGptAd = true;
+		$wgAdDriverUseSevenOneMedia = false;
+		$wgWikiDirectedAtChildrenByStaff = true;
+		$wgAdDriverUseEbay = true;
+	}
+
+	/**
+	 * Test getAdsContext
+	 *
+	 * @covers MercuryApi::getAdsContext
+	 */
+	public function testGetAdsContext() {
+		$this->setupGlobals();
+		$mercuryApi = new MercuryApi();
+
+		$language = $this->getMock( 'Language' );
+		$language->method( 'getCode' )
+			->willReturn( 'en' );
+
+		$title = $this->getMock( 'Title' );
+		$title->method( 'getPageLanguage' )
+			->willReturn( $language );
+		$title->method( 'getArticleId' )
+			->willReturn( 10 );
+
+		$wGReg = $this->getMock( 'WikiaGlobalRegistry' );
+
+		$articleCategories = [
+			[
+				'title' => 'title 1'
+			]
+		];
+		$expected = [
+			'opts' => [
+				'adsInHead' => true,
+				'disableLateQueue' => true,
+				'lateAdsAfterPageLoad' => true,
+				'pageType' => 'corporate',
+				'showAds' => true,
+				'usePostScribe' => null,
+				'trackSlotState' => true,
+			],
+			'targeting' => [
+				'enableKruxTargeting' => true,
+				'kruxCategoryId' => 'Hi0kPhMT',
+		        'pageArticleId' => 10,
+		        'pageCategories' => [
+					'title 1'
+				],
+        		'pageIsArticle' => true,
+		        'pageIsHub' => true,
+		        'pageName' => null,
+		        'pageType' => 'article',
+		        'sevenOneMediaSub2Site' => true,
+		        'skin' => 'mercury',
+		        'wikiCategory' => 'ent',
+		        'wikiCustomKeyValues' => 'egnre=action;egnre=adventure;egnre=drama;egnre=scifi;media=tv;wtpx=815;wtpx=696;wtpx=408;wtpx=911;',
+		        'wikiDbName' => null,
+		        'wikiDirectedAtChildren' => true,
+		        'wikiLanguage' => 'en',
+		        'wikiVertical' => 'Wikia',
+			],
+			'providers' => [
+				'ebay' => true,
+				'sevenOneMedia' => null,
+		        'sevenOneMediaCombinedUrl' => null,
+		        'remnantGptMobile' => null,
+			],
+			'slots' => [
+				'bottomLeaderboardImpressionCapping' => null,
+			],
+			'forceProviders' => [
+				'directGpt' => true,
+				'liftium' => true,
+			],
+ 		];
+		$result = $mercuryApi->getAdsContext( $title, $wGReg, $articleCategories );
+		$this->assertEquals( $expected, $result );
+	}
+
+}
