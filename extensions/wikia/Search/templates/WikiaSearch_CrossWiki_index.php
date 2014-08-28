5bd71b7e8a12c226824fb62a2c074e9af16b4b7f
VForge <bartosz.bentkowski@gmail.com>
2014-08-28 16:10:31 +0200

    CON-1599 | Fixes

--- extensions/wikia/Search/templates/WikiaSearch_CrossWiki_index.php
+++ extensions/wikia/Search/templates/WikiaSearch_CrossWiki_index.php
@@ -1,55 +1,54 @@
-<section class="Search 'this-wiki WikiaGrid clearfix">
-
+<section class="Search all-wikia WikiaGrid clearfix">
 	<div class="results-wrapper">
+		<?php if (!empty($results)): ?>
+			<?php if ($resultsFound > 0): ?>
 
-		<?php if(!empty($results)): ?>
-		<?php if( $resultsFound > 0 ): ?>
+				<p class="result-count subtle">
+					<?php if (empty($isOneResultsPageOnly)): ?>
+						<?= wfMsg('wikiasearch2-results-count', $resultsFoundTruncated, '<strong>' . $query . '</strong>'); ?>
+					<?php else: ?>
+						<?= wfMsg('wikiasearch2-results-for', '<strong>' . $query . '</strong>'); ?>
+					<?php endif; ?>
+					<?php if (isset($hub) && $hub) : ?>
+						<?= wfMsg('wikiasearch2-onhub', $hub) ?>
+						|
+						<a
+							href="<?= preg_replace('/&hub=[^&]+/', '', $_SERVER['REQUEST_URI']) ?>"><?= wfMsg('wikiasearch2-search-all-wikia') ?></a>
+					<?php endif ?>
+				</p>
 
-			<p class="result-count subtle">
-				<?php if( empty( $isOneResultsPageOnly ) ): ?>
-				<?= wfMsg('wikiasearch2-results-count', $resultsFoundTruncated, '<strong>'.$query.'</strong>'); ?>
-				<?php else: ?>
-				<?= wfMsg('wikiasearch2-results-for', '<strong>'.$query.'</strong>'); ?>
-				<?php endif; ?>
-				<?php if ( isset($hub) && $hub ) : ?>
-				<?= wfMsg('wikiasearch2-onhub', $hub)?>
-				|
-				<a href="<?=preg_replace('/&hub=[^&]+/', '', $_SERVER['REQUEST_URI'])?>"><?= wfMsg('wikiasearch2-search-all-wikia') ?></a>
-				<?php endif ?>
-			</p>
-
-			<? if ($results->getQuery() && $query != $results->getQuery()) : ?>
-				<p><?= wfMsg( 'wikiasearch2-spellcheck', $query, $results->getQuery() ) ?></p>
+				<? if ($results->getQuery() && $query != $results->getQuery()) : ?>
+					<p><?= wfMsg('wikiasearch2-spellcheck', $query, $results->getQuery()) ?></p>
 				<? endif; ?>
 
-			<? if ( !$hasArticleMatch && $isMonobook ): ?>
-				<?=wfMsgExt('searchmenu-new', array('parse'), $query);?>
+				<? if (!$hasArticleMatch && $isMonobook): ?>
+					<?= wfMsgExt('searchmenu-new', array('parse'), $query); ?>
 				<? endif; ?>
 
-			<ul class="Results inter-wiki">
-				<?
-				$pos = 0;
-				foreach( $results as $result ) {
-					$pos++;
-					echo $app->getView( 'WikiaSearch', 'CrossWiki_result', array(
-						'result' => $result,
-						'pos' => $pos + (($currentPage - 1) * $resultsPerPage),
-						'query' => $query,
-						'hub' => $hub,
-						'corporateWikiId' => $corporateWikiId,
-						'wgExtensionsPath' => $wgExtensionsPath
-					));
-				}
-				?>
-			</ul>
+				<ul class="Results inter-wiki">
+					<?
+					$pos = 0;
+					foreach ($results as $result) {
+						$pos++;
+						echo $app->getView('WikiaSearch', 'CrossWiki_result', array(
+							'result' => $result,
+							'pos' => $pos + (($currentPage - 1) * $resultsPerPage),
+							'query' => $query,
+							'hub' => $hub,
+							'corporateWikiId' => $corporateWikiId,
+							'wgExtensionsPath' => $wgExtensionsPath
+						));
+					}
+					?>
+				</ul>
 
-			<?= $paginationLinks; ?>
+				<?= $paginationLinks; ?>
 
 			<?php else: ?>
-			<? if ( !$hasArticleMatch && $isMonobook ): ?>
-				<?=wfMsgExt('searchmenu-new', array('parse'), $query);?>
+				<? if (!$hasArticleMatch && $isMonobook): ?>
+					<?= wfMsgExt('searchmenu-new', array('parse'), $query); ?>
 				<? endif; ?>
-			<p class="no-result"><i><?=wfMsg('wikiasearch2-noresults')?></i></p>
+				<p class="no-result"><i><?= wfMsg('wikiasearch2-noresults') ?></i></p>
 			<?php endif; ?>
 		<?php endif; ?>
 
