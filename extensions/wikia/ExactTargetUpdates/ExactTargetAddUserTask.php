1327c35c56286ccccf80ed20cdf802335d1da2ba
Kamil Koterba <kamil.koterba@gmail.com>
2014-09-01 17:23:46 +0200

    CE-933 Changing methods order

--- extensions/wikia/ExactTargetUpdates/ExactTargetAddUserTask.php
+++ extensions/wikia/ExactTargetUpdates/ExactTargetAddUserTask.php
@@ -16,6 +16,41 @@ class ExactTargetAddUserTask extends BaseTask {
 	}
 
 	/**
+	 * Creates Subscriber object in ExactTarget by API request
+	 * @param String $sUserEmail new subscriber email address
+	 */
+	public function createSubscriber( $sUserEmail ) {
+		global $wgExactTargetApiConfig;
+		$wsdl = $wgExactTargetApiConfig[ 'wsdl' ];
+
+		try {
+			/* Create the Soap Client */
+			$oClient = new ExactTargetSoapClient( $wsdl, array( 'trace'=>1 ) );
+			$oClient->username = $wgExactTargetApiConfig[ 'username' ];
+			$oClient->password = $wgExactTargetApiConfig[ 'password' ];
+
+			/* ExactTarget_Subscriber */
+			$oSubscriber = new ExactTarget_Subscriber();
+			$oSubscriber->SubscriberKey = $sUserEmail;
+			$oSubscriber->EmailAddress = $sUserEmail;
+
+			/* Create the subscriber */
+			$oSoapVar = new SoapVar( $oSubscriber, SOAP_ENC_OBJECT, 'Subscriber', 'http://exacttarget.com/wsdl/partnerAPI' );
+			$oRequest = new ExactTarget_CreateRequest();
+			$oRequest->Options = NULL;
+			$oRequest->Objects = array( $oSoapVar );
+			$oClient->Create( $oRequest );
+
+			/* Log response */
+			$this->info( $oClient->__getLastResponse() );
+
+		} catch ( SoapFault $e ) {
+			/* Log error */
+			$this->error( 'SoapFault:' . $e->getMessage() . 'ErrorCode: ' . $e->getCode() );
+		}
+	}
+
+	/**
 	 * Creates DataExtension object in ExactTarget by API request that reflects Wikia user table
 	 * @param Array $aUserData Selected fields from Wikia user table
 	 */
@@ -53,21 +88,6 @@ class ExactTargetAddUserTask extends BaseTask {
 	}
 
 	/**
-	 * Returns ExactTarget_APIProperty object
-	 * This object can be used as ExactTarget_DataExtensionObject property
-	 * It stores key-value pair
-	 * @param String $key Property name
-	 * @param String $value Propert yvalue
-	 * @return ExactTarget_APIProperty
-	 */
-	private function  prepareApiProperty( $key, $value ) {
-		$apiProperty = new ExactTarget_APIProperty();
-		$apiProperty->Name = $key;
-		$apiProperty->Value = $value;
-		return $apiProperty;
-	}
-
-	/**
 	 * Creates DataExtension object in ExactTarget by API request that reflects Wikia user_properties table
 	 * @param Integer $iUserId User ID
 	 * @param Array $aUserProperties key-value array ['property_name'=>'property_value']
@@ -88,23 +108,6 @@ class ExactTargetAddUserTask extends BaseTask {
 			/* Log error */
 			$this->error( 'SoapFault:' . $e->getMessage() . 'ErrorCode: ' . $e->getCode() );
 		}
-	}
-
-	public function getClient() {
-		global $wgExactTargetApiConfig;
-		$wsdl = $wgExactTargetApiConfig[ 'wsdl' ];
-		$oClient = new ExactTargetSoapClient( $wsdl, array( 'trace'=>1 ) );
-		$oClient->username = $wgExactTargetApiConfig[ 'username' ];
-		$oClient->password = $wgExactTargetApiConfig[ 'password' ];
-	}
-
-	public function prepareRequest( $iUserId, $aUserProperties ) {
-		$aSoapVars = $this->prepareUserPropertiesSoapVars( $iUserId, $aUserProperties );
-
-		$oRequest = new ExactTarget_CreateRequest();
-		$oRequest->Options = NULL;
-		$oRequest->Objects = $aSoapVars;
-		return $oRequest;
 	}
 
 	protected function prepareUserPropertiesSoapVars( $iUserId, $aUserProperties ) {
@@ -138,43 +141,40 @@ class ExactTargetAddUserTask extends BaseTask {
 		return $aDE;
 	}
 
+	public function getClient() {
+		global $wgExactTargetApiConfig;
+		$wsdl = $wgExactTargetApiConfig[ 'wsdl' ];
+		$oClient = new ExactTargetSoapClient( $wsdl, array( 'trace'=>1 ) );
+		$oClient->username = $wgExactTargetApiConfig[ 'username' ];
+		$oClient->password = $wgExactTargetApiConfig[ 'password' ];
+	}
+
+	public function prepareRequest( $iUserId, $aUserProperties ) {
+		$aSoapVars = $this->prepareUserPropertiesSoapVars( $iUserId, $aUserProperties );
+
+		$oRequest = new ExactTarget_CreateRequest();
+		$oRequest->Options = NULL;
+		$oRequest->Objects = $aSoapVars;
+		return $oRequest;
+	}
+
 	protected function wrapDataExtensionObjectToSoapVar( $DE ) {
 		return new SoapVar( $DE, SOAP_ENC_OBJECT, 'DataExtensionObject', 'http://exacttarget.com/wsdl/partnerAPI' );
 	}
 
 	/**
-	 * Creates Subscriber object in ExactTarget by API request
-	 * @param String $sUserEmail new subscriber email address
+	 * Returns ExactTarget_APIProperty object
+	 * This object can be used as ExactTarget_DataExtensionObject property
+	 * It stores key-value pair
+	 * @param String $key Property name
+	 * @param String $value Propert yvalue
+	 * @return ExactTarget_APIProperty
 	 */
-	public function createSubscriber( $sUserEmail ) {
-		global $wgExactTargetApiConfig;
-		$wsdl = $wgExactTargetApiConfig[ 'wsdl' ];
-
-		try {
-			/* Create the Soap Client */
-			$oClient = new ExactTargetSoapClient( $wsdl, array( 'trace'=>1 ) );
-			$oClient->username = $wgExactTargetApiConfig[ 'username' ];
-			$oClient->password = $wgExactTargetApiConfig[ 'password' ];
-
-			/* ExactTarget_Subscriber */
-			$oSubscriber = new ExactTarget_Subscriber();
-			$oSubscriber->SubscriberKey = $sUserEmail;
-			$oSubscriber->EmailAddress = $sUserEmail;
-
-			/* Create the subscriber */
-			$oSoapVar = new SoapVar( $oSubscriber, SOAP_ENC_OBJECT, 'Subscriber', 'http://exacttarget.com/wsdl/partnerAPI' );
-			$oRequest = new ExactTarget_CreateRequest();
-			$oRequest->Options = NULL;
-			$oRequest->Objects = array( $oSoapVar );
-			$oClient->Create( $oRequest );
-
-			/* Log response */
-			$this->info( $oClient->__getLastResponse() );
-
-		} catch ( SoapFault $e ) {
-			/* Log error */
-			$this->error( 'SoapFault:' . $e->getMessage() . 'ErrorCode: ' . $e->getCode() );
-		}
+	private function  prepareApiProperty( $key, $value ) {
+		$apiProperty = new ExactTarget_APIProperty();
+		$apiProperty->Name = $key;
+		$apiProperty->Value = $value;
+		return $apiProperty;
 	}
 
 	protected function getLoggerContext() {
