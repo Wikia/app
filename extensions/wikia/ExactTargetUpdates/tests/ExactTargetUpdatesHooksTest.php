7b837ee61d8498f74141731b90b014d9172b5468
Kamil Koterba <kamil@wikia-inc.com>
2014-09-01 12:24:22 +0200

    CE-933 Adding testPrepareRequestShouldReturnRequestObject and testCallApiToCreateUserPropertiesDataExtensionShouldInvokeCreateMethodOnceWithRequestParam tests

--- extensions/wikia/ExactTargetUpdates/tests/ExactTargetUpdatesHooksTest.php
+++ extensions/wikia/ExactTargetUpdates/tests/ExactTargetUpdatesHooksTest.php
@@ -1,5 +1,7 @@
 <?php
 
+include __DIR__ . "/../lib/exacttarget_soap_client.php";
+
 class ExactTargetUpdatesHooksTest extends WikiaBaseTest {
 
 	public function testPrepareParams() {
@@ -9,7 +11,7 @@ class ExactTargetUpdatesHooksTest extends WikiaBaseTest {
 				'getRegistration', 'getEditCount', 'getOptions', 'getTouched') )
 			->getMock();
 
-		$userParams = [
+		$aUserParams = [
 			'user_id' => 12345,
 			'user_name' => 'Test User Name',
 			'user_real_name' => 'Some Real Name',
@@ -24,56 +26,60 @@ class ExactTargetUpdatesHooksTest extends WikiaBaseTest {
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getId' )
-			->will   ( $this->returnValue( $userParams['user_id'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_id'] ) );
 
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getName' )
-			->will   ( $this->returnValue( $userParams['user_name'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_name'] ) );
 
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getRealName' )
-			->will   ( $this->returnValue( $userParams['user_real_name'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_real_name'] ) );
 
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getEmail' )
-			->will   ( $this->returnValue( $userParams['user_email'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_email'] ) );
 
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getEmailAuthenticationTimestamp' )
-			->will   ( $this->returnValue( $userParams['user_email_authenticated'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_email_authenticated'] ) );
 
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getRegistration' )
-			->will   ( $this->returnValue( $userParams['user_registration'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_registration'] ) );
 
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getEditCount' )
-			->will   ( $this->returnValue( $userParams['user_editcount'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_editcount'] ) );
 
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getOptions' )
-			->will   ( $this->returnValue( $userParams['user_options'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_options'] ) );
 
 		$userMock
 			->expects( $this->once() )
 			->method ( 'getTouched' )
-			->will   ( $this->returnValue( $userParams['user_touched'] ) );
+			->will   ( $this->returnValue( $aUserParams['user_touched'] ) );
 
-		$userParamsResult = ExactTargetUpdatesHooks::prepareParams( $userMock );
+		/* Get mock object of tested class ExactTargetUpdatesHooks without mocking any methods */
+		$exactTargetUpdatesHooksMock = $this->getMock( 'ExactTargetUpdatesHooks', null );
 
-		$this->assertEquals($userParamsResult, $userParams);
+		/* Run test */
+		/* @var ExactTargetUpdatesHooks $exactTargetUpdatesHooksMock (mock of ExactTargetUpdatesHooks) */
+		$aUserParamsResult = $exactTargetUpdatesHooksMock->prepareParams( $userMock );
+
+		$this->assertEquals( $aUserParamsResult, $aUserParams );
 
 	}
 
 	function testTaskNotCreatedOnDev() {
-
 		/* Define environment constants if not defined yet */
 		if ( !defined( 'WIKIA_ENV_DEV' ) ) {
 			define( WIKIA_ENV_DEV, 'test-dev') ;
@@ -99,7 +105,7 @@ class ExactTargetUpdatesHooksTest extends WikiaBaseTest {
 			->expects( $this->never() )
 			->method( 'queue' );
 
-		/* Mock tested class ExactTargetUpdatesHooks */
+		/* Get mock object of tested class ExactTargetUpdatesHooks */
 		$exactTargetUpdatesHooksMock = $this->getMock( 'ExactTargetUpdatesHooks', [ 'prepareParams' ] );
 		$exactTargetUpdatesHooksMock
 			->expects( $this->never() )
@@ -108,5 +114,117 @@ class ExactTargetUpdatesHooksTest extends WikiaBaseTest {
 		/* Run test */
 		/* @var ExactTargetUpdatesHooks $exactTargetUpdatesHooksMock (mock of ExactTargetUpdatesHooks) */
 		$exactTargetUpdatesHooksMock->addTheAddUserTask( $userMock, $mockAddUserTask );
+	}
+
+	function testPrepareRequestShouldReturnRequestObject() {
+		/* Params to compare */
+		$iUserId = 12345;
+		$aUserProperties = [
+			'property1' => 'value1',
+			'property2' => 'value2'
+		];
+
+		/* prepare request mock */
+		$aSoapVars = [];
+		foreach ( $aUserProperties as $sProperty => $sValue ) {
+
+			$DE = new ExactTarget_DataExtensionObject();
+			$DE->CustomerKey = 'user_properties';
+
+			$apiPropertyUser = new ExactTarget_APIProperty();
+			$apiPropertyUser->Name = 'up_user';
+			$apiPropertyUser->Value = $iUserId;
+
+			$apiPropertyProperty = new ExactTarget_APIProperty();
+			$apiPropertyProperty->Name = 'up_property';
+			$apiPropertyProperty->Value = $sProperty;
+
+			$apiPropertyValue = new ExactTarget_APIProperty();
+			$apiPropertyValue->Name = 'up_value';
+			$apiPropertyValue->Value = $sValue;
+
+			$apiProperties = [ $apiPropertyUser, $apiPropertyProperty, $apiPropertyValue ];
+
+			$DE->Properties = $apiProperties;
+
+			$soapVar = new SoapVar( $DE, SOAP_ENC_OBJECT, 'DataExtensionObject', "http://exacttarget.com/wsdl/partnerAPI" );
+			$aSoapVars[] = $soapVar;
+		}
+
+		$oRequestExpected = new ExactTarget_CreateRequest();
+
+		$oRequestExpected->Options = NULL;
+		$oRequestExpected->Objects = $aSoapVars;
+
+		/* Mock tested class */
+		$mockAddUserTask = $this->getMock( 'ExactTargetAddUserTask', [ 'getClient' ] );
+
+		/* Run test */
+		$oRequestActual = $mockAddUserTask->prepareRequest( $iUserId, $aUserProperties );
+		$this->assertEquals( $oRequestExpected, $oRequestActual );
+	}
+
+	function testCallApiToCreateUserPropertiesDataExtensionShouldInvokeCreateMethodOnceWithRequestParam() {
+		/* Params to compare */
+		$iUserId = 12345;
+		$aUserProperties = [
+			'property1' => 'value1',
+			'property2' => 'value2'
+		];
+
+		/* Prepare request object */
+		$aSoapVars = [];
+		foreach ( $aUserProperties as $sProperty => $sValue ) {
+
+			$DE = new ExactTarget_DataExtensionObject();
+			$DE->CustomerKey = 'user_properties';
+
+			$apiPropertyUser = new ExactTarget_APIProperty();
+			$apiPropertyUser->Name = 'up_user';
+			$apiPropertyUser->Value = $iUserId;
+
+			$apiPropertyProperty = new ExactTarget_APIProperty();
+			$apiPropertyProperty->Name = 'up_property';
+			$apiPropertyProperty->Value = $sProperty;
+
+			$apiPropertyValue = new ExactTarget_APIProperty();
+			$apiPropertyValue->Name = 'up_value';
+			$apiPropertyValue->Value = $sValue;
+
+			$apiProperties = [ $apiPropertyUser, $apiPropertyProperty, $apiPropertyValue ];
+
+			$DE->Properties = $apiProperties;
+
+			$soapVar = new SoapVar( $DE, SOAP_ENC_OBJECT, 'DataExtensionObject', "http://exacttarget.com/wsdl/partnerAPI" );
+			$aSoapVars[] = $soapVar;
+		}
+
+		$oRequest = new ExactTarget_CreateRequest();
+
+		$oRequest->Options = NULL;
+		$oRequest->Objects = $aSoapVars;
+
+		$soapClient = $this->getMockBuilder( 'ExactTargetSoapClient' )
+			->disableOriginalConstructor()
+			->setMethods( [ 'Create' ] )
+			->getMock();
+		$soapClient
+			->expects( $this->once() )
+			->method( 'Create' )
+			->with( $oRequest );
+
+		/* Mock tested class */
+		$mockAddUserTask = $this->getMock( 'ExactTargetAddUserTask', ['prepareRequest', 'getClient'] );
+		$mockAddUserTask
+			->expects( $this->once() )
+			->method( 'prepareRequest' )
+			->will( $this->returnValue( $oRequest ) );
+		$mockAddUserTask
+			->expects( $this->once() )
+			->method( 'getClient' )
+			->will( $this->returnValue( $soapClient ) );
+
+		/* Run test */
+		$mockAddUserTask->callApiToCreateUserPropertiesDataExtension( $iUserId, $aUserProperties );
 	}
 }
