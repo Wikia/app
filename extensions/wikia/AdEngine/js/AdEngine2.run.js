d060bfa58ee4ea7a68ec0a02d2eb340d5b095b02
Piotr Gabryjeluk <rychu@wikia-inc.com>
2014-09-03 17:53:29 +0200

    Don't disable SevenOne Media and Evolve if Liftium is disabled

--- extensions/wikia/AdEngine/js/AdEngine2.run.js
+++ extensions/wikia/AdEngine/js/AdEngine2.run.js
@@ -112,7 +112,12 @@ require([
 	}
 
 	if (adContext.getContext().opts.disableLateQueue) {
-		log('skipping late queue - wgAdEngineDisableLateQueue set to true', 1, module);
+		log('Skipping late queue - wgAdEngineDisableLateQueue set to true', 1, module);
+	} else {
+		if (instantGlobals.wgSitewideDisableLiftium) {
+			log('Liftium disabled by wgSitewideDisableLiftium - running AdEngine_loadLateAds now', 1, module);
+			window.AdEngine_loadLateAds();
+		}
 	}
 });
 
