58e88715624548b7b6cd1e4cd3a7e727fdf017b9
Yurii Lahodiuk <yurii@wikia-inc.com>
2014-09-03 15:29:03 +0200

    Draft implementation of parsing of <tabview>

--- extensions/wikia/JsonFormat/visitors/DivContainingHeadersVisitor.php
+++ extensions/wikia/JsonFormat/visitors/DivContainingHeadersVisitor.php
@@ -12,8 +12,18 @@ class DivContainingHeadersVisitor extends DOMNodeVisitorBase {
 	public function canVisit( DOMNode $currentNode ) {
 		/** @var DOMElement $currentNode */
 
-		return DomHelper::isElement( $currentNode, 'div' ) &&
-			\DomHelper::hasDescendantHeader( $currentNode );
+		if (DomHelper::isElement( $currentNode, 'div' ) ) {
+			if( \DomHelper::hasDescendantHeader( $currentNode ) ) {
+				return true;
+			}
+
+			if( strpos( $currentNode->getAttribute('id'), 'flytab' ) !== false ) {
+				return true;
+			}
+
+			return false;
+		}
+		return false;
 	}
 
 	/**
@@ -23,6 +33,47 @@ class DivContainingHeadersVisitor extends DOMNodeVisitorBase {
 		/** @var DOMElement $currentNode */
 		DomHelper::verifyDomElementArgument( $currentNode, "currentNode" );
 
+		if( strpos( $currentNode->getAttribute('id'), 'flytab' ) !== false ) {
+			$xpath = new DOMXPath( $currentNode->ownerDocument );
+			$tabUrls = $xpath->query(".//@href", $currentNode);
+			$htmlParser = new Wikia\JsonFormat\HtmlParser();
+			foreach( $tabUrls as $url ) {
+				$url = preg_replace( '/^\/wiki\/(.+?)\?.*$/i', '$1', $url->value );
+				$title = Title::newFromURL( $url );
+				$article = Article::newFromTitle( $title, RequestContext::getMain() );
+				$article->getContent();
+				$html = $article->getPage()->getParserOutput( \ParserOptions::newFromContext( new RequestContext() ) )->getText();
+				$jsonArticle = $htmlParser->parse( $html );
+
+				$tabSection = new JsonFormatSectionNode( 1, $title->getText() );
+				foreach( $jsonArticle->getChildren() as $child ) {
+					$tabSection->addChild( $child );
+				}
+
+				$level = 0;
+				if( $this->getJsonFormatBuilder()->getCurrentContainer()->getType() === 'section' ) {
+					$level = $this->getJsonFormatBuilder()->getCurrentContainer()->getLevel();
+				}
+				if( $level > 1 ) {
+					$this->addLevel( $tabSection, $level - 1 );
+				}
+
+				$this->getJsonFormatBuilder()->add( $tabSection );
+			}
+			return;
+		}
+
 		$this->iterate( $currentNode->childNodes );
+	}
+
+	protected function addLevel( $node, $level ) {
+		if( property_exists( $node, 'level' ) && method_exists( $node, 'getLevel' ) && method_exists( $node, 'setLevel' ) ) {
+			$node->setLevel( $node->getLevel() + $level );
+		}
+		if( method_exists( $node, 'getChildren' ) ) {
+			foreach( $node->getChildren() as $child ) {
+				$this->addLevel( $child, $level );
+			}
+		}
 	}
 }
