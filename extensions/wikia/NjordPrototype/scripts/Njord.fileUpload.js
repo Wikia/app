83976812743f6acde849a5104b5cbf4cd09d13ed
Adam Robak <adamr@wikia-inc.com>
2014-09-05 16:27:48 +0200

    ignore hero image on drag and drop

--- extensions/wikia/NjordPrototype/scripts/Njord.fileUpload.js
+++ extensions/wikia/NjordPrototype/scripts/Njord.fileUpload.js
@@ -21,11 +21,26 @@
 		if (e.dataTransfer.files.length) {
 			//if file is uploaded
 			fd.append('file', e.dataTransfer.files[0]);
+			sendForm(fd);
 		} else if (e.dataTransfer.getData('text/html')) {
 			//if url
 			var $img = $(e.dataTransfer.getData('text/html'));
-			fd.append('url', $img.attr('src'));
+			if(e.target.src !== $img.attr('src')) {
+				fd.append('url', $img.attr('src'));
+				sendForm(fd);
+			}
 		}
+	});
+
+	$heroModuleUpload.find('.upload-btn').on('click', function () {
+		if ($heroModuleInput[0].files.length) {
+			var fd = new FormData();
+			fd.append('file', $heroModuleInput[0].files[0]);
+			sendForm(fd);
+		}
+	});
+
+	function sendForm(formdata) {
 		var client = new XMLHttpRequest();
 		client.open('POST', '/wikia.php?controller=Njord&method=upload', true);
 		client.onreadystatechange = function () {
@@ -35,23 +50,6 @@
 				$heroModule.trigger('change', [data.url, data.filename]);
 			}
 		};
-		client.send(fd);
-	});
-
-	$heroModuleUpload.find('.upload-btn').on('click', function() {
-		if($heroModuleInput[0].files.length){
-			var fd = new FormData();
-			fd.append('file', $heroModuleInput[0].files[0]);
-			var client = new XMLHttpRequest();
-			client.open('POST', '/wikia.php?controller=Njord&method=upload', true);
-			client.onreadystatechange = function () {
-				if (client.readyState === 4 && client.status === 200) {
-					var data = JSON.parse(client.responseText);
-					$heroModuleImage.attr('src', data.url);
-					$heroModule.trigger('change', [data.url, data.filename]);
-				}
-			};
-			client.send(fd);
-		}
-	});
+		client.send(formdata);
+	}
 })(window, jQuery);
