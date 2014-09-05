bff4cfb31c9750339c08b5addb68002c2cc249b8
Matt Klucsarits <mattk@wikia-inc.com>
2014-09-05 21:43:08 +0000

    VE-525 / VE-1495 Address code review comments

--- extensions/VisualEditor/wikia/modules/ve/ui/dialogs/ve.ui.WikiaTemplateInsertDialog.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/dialogs/ve.ui.WikiaTemplateInsertDialog.js
@@ -1,24 +1,17 @@
 /*
- * VisualEditor user interface WikiaTemplateDialog class.
- *
- * @copyright 2011-2014 VisualEditor Team and others; see AUTHORS.txt
- * @license The MIT License (MIT); see LICENSE.txt
+ * VisualEditor user interface WikiaTemplateInsertDialog class.
  */
 
 /**
  * Dialog for inserting templates.
  *
  * @class
- * @abstract
- * @extends ve.ui.MWTemplateDialog
+ * @extends ve.ui.Dialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface Surface dialog is for
  * @param {Object} [config] Configuration options
  */
 ve.ui.WikiaTemplateInsertDialog = function VeUiWikiaTemplateInsertDialog( config ) {
-	// TODO: configured width needed?
-
 	// Parent constructor
 	ve.ui.WikiaTemplateInsertDialog.super.call( this, config );
 };
@@ -45,31 +38,12 @@ ve.ui.WikiaTemplateInsertDialog.prototype.initialize = function () {
 	ve.ui.WikiaTemplateInsertDialog.super.prototype.initialize.call( this );
 
 	// Properties
-	this.searchInput = new OO.ui.TextInputWidget( {
-		'$': this.$,
-		'icon': 'search',
-		'type': 'search',
-		'placeholder': ve.msg( 'wikia-visualeditor-dialog-wikiatemplateinsert-search' )
-	} );
-	this.$search = this.$( '<div>' )
-		.addClass( 've-ui-wikiaTemplateInsertDialog-search' )
-		.append( this.searchInput.$element );
 	this.select = new OO.ui.SelectWidget( { '$': this.$ } );
 
 	// Events
-	this.searchInput.on( 'change', ve.bind( this.onSearchInputChange, this ) );
 
 	// Initialization
 	this.frame.$content.addClass( 've-ui-wikiaTemplateInsertDialog' );
-	this.searchInput.$input.attr( 'tabindex', 1 );
-	this.$body.append( this.$search );
-};
-
-/*
- * Handle change of value in search input
- */
-ve.ui.WikiaTemplateInsertDialog.prototype.onSearchInputChange = function () {
-	// TODO
 };
 
 /* Registration */
