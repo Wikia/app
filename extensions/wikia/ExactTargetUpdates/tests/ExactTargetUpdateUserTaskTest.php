ff0b68535b1a3a1b08ca520b262aed8c2a001a0b
Kamil Koterba <kamil@wikia-inc.com>
2014-09-10 21:03:26 +0200

    CE-1019 test for setting Keys property

--- extensions/wikia/ExactTargetUpdates/tests/ExactTargetUpdateUserTaskTest.php
+++ extensions/wikia/ExactTargetUpdates/tests/ExactTargetUpdateUserTaskTest.php
@@ -33,4 +33,56 @@ class ExactTargetUpdateUserTaskTest extends WikiaBaseTest {
 		/* @var ExactTargetUpdateUserTask $mockUpdateUserTask */
 		$mockUpdateUserTask->updateUserPropertiesDataExtension( $iUserId, $aUserProperties, $soapClient );
 	}
+
+	/**
+	 * prepareUserPropertiesDataExtensionObjectsForUpdate should set Keys property of ExactTarget_DataExtensionObject
+	 * to define API query filter for update
+	 */
+	function testShouldSetKeysProperty() {
+		$iUserId = 12345;
+		$aUserProperties = [
+			'property1' => 'value1',
+			'property2' => 'value2',
+		];
+
+		/* Create new DataExtensionObject */
+		$aDataExtensionExpected = new ExactTarget_DataExtensionObject();
+		$aDataExtensionExpected->CustomerKey = 'user_properties';
+
+		$keys = [];
+		$properties = [];
+
+		$apiProperty = new ExactTarget_APIProperty();
+		$apiProperty->Name = 'up_user';
+		$apiProperty->Value = $iUserId;
+		$keys[] = $apiProperty;
+
+		$apiProperty = new ExactTarget_APIProperty();
+		$apiProperty->Name = 'up_property';
+		$apiProperty->Value = 'property1';// Property name taken from $aUserProperties above
+		$keys[] = $apiProperty;
+
+
+		$apiProperty = new ExactTarget_APIProperty();
+		$apiProperty->Name = 'up_value';
+		$apiProperty->Value = 'value1';// Value taken from $aUserProperties above
+		$properties[] = $apiProperty;
+
+		$aDataExtensionExpected->Keys = $keys;
+		$aDataExtensionExpected->Properties = $properties;// Value taken from $aUserProperties above
+
+		/* @var ExactTargetUpdateUserTask $mockUpdateUserTask */
+		$mockUpdateUserTask = $this->getMockBuilder( 'ExactTargetUpdateUserTask' )
+			->disableOriginalConstructor()
+			->setMethods( NULL )
+			->getMock();
+
+		/* Run tested method */
+		$aDataExtensionActual = $mockUpdateUserTask->prepareUserPropertiesDataExtensionObjectsForUpdate( $iUserId, $aUserProperties );
+
+		/* Check assertions */
+		$this->assertEquals( sizeof( $aDataExtensionActual ), 2 );
+		$this->assertEquals( $aDataExtensionActual[ 0 ], $aDataExtensionExpected );
+	}
+
 }
