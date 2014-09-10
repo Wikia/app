bd9d474d6a0b3673f538933999eeac67d8bb5ca9
Kamil Koterba <kamil@wikia-inc.com>
2014-09-10 19:23:41 +0200

    CE-933 Fixing update user tests

--- extensions/wikia/ExactTargetUpdates/tests/ExactTargetUpdateUserTaskTest.php
+++ extensions/wikia/ExactTargetUpdates/tests/ExactTargetUpdateUserTaskTest.php
@@ -1,17 +1,13 @@
 <?php
 
 require_once __DIR__ . '/../lib/exacttarget_soap_client.php';
-var_dump('class ExactTargetUpdateUserTaskTest loaded');
 
 class ExactTargetUpdateUserTaskTest extends WikiaBaseTest {
 
 	function testShouldInvokeUpdateMethodWithProperParam() {
 		/* Params to compare */
 		$iUserId = 12345;
-		$aUserProperties = [
-			'property1' => 'value1',
-			'property2' => 'value2'
-		];
+		$aUserProperties = [];
 
 		$oRequest = new ExactTarget_CreateRequest();
 
@@ -33,13 +29,8 @@ class ExactTargetUpdateUserTaskTest extends WikiaBaseTest {
 			->expects( $this->once() )
 			->method( 'wrapUpdateRequest' )
 			->will( $this->returnValue( $oRequest ) );
-		$mockUpdateUserTask
-			->expects( $this->once() )
-			->method( 'getClient' )
-			->will( $this->returnValue( $soapClient ) );
 
-		$mockUpdateUserTask->initClient();
 		/* @var ExactTargetUpdateUserTask $mockUpdateUserTask */
-		$mockUpdateUserTask->updateUserPropertiesDataExtension( $iUserId, $aUserProperties );
+		$mockUpdateUserTask->updateUserPropertiesDataExtension( $iUserId, $aUserProperties, $soapClient );
 	}
 }
