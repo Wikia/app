25e3ff277a966672db16384c4864a207af5c3bd0
Warkot <mateusz.warkocki@wikia-inc.com>
2014-09-10 19:57:15 +0200

    CON-1446 | interaction differentiation between UserLoginDropdown and UserMenu closing

--- extensions/wikia/UserLogin/js/UserLoginDropdown.globalNavigation.js
+++ extensions/wikia/UserLogin/js/UserLoginDropdown.globalNavigation.js
@@ -1,28 +1,27 @@
 /* global UserLoginFacebook:true, UserLoginAjaxForm:true */
-require( ['jquery'], function( $ ){
+require(['jquery'], function($){
 	'use strict';
-	var $entryPoint, $transparentOut, loginAjaxForm = false;
+	var $entryPoint, $userLoginDropdown, $transparentOut, loginAjaxForm = false, delayedHoverOptions;
 
 	function openMenu() {
-		$entryPoint.addClass( 'active' );
-		$transparentOut.addClass( 'visible' );
+		$entryPoint.addClass('active');
+		$transparentOut.addClass('visible');
 
 		if (!loginAjaxForm) {
 			loginAjaxForm = new UserLoginAjaxForm($entryPoint);
-			UserLoginFacebook.init( UserLoginFacebook.origins.DROPDOWN );
+			UserLoginFacebook.init(UserLoginFacebook.origins.DROPDOWN);
 		}
 	}
 
 	function closeMenu() {
-		$entryPoint.removeClass( 'active' );
-		$transparentOut.removeClass( 'visible' );
+		$entryPoint.removeClass('active');
+		$transparentOut.removeClass('visible');
 	}
 
-	$entryPoint = $('#AccountNavigation');
-
-	$transparentOut = $('<div class="transparent-out transparent-out-user-login"/>').appendTo('body');
+	$transparentOut = $('<div class="transparent-out transparent-out-user-login-and-account-navigation"/>').appendTo('body');
 	$transparentOut.click(closeMenu);
 
+	$entryPoint = $('#AccountNavigation');
 	$entryPoint.on('click', '.ajaxLogin', function(ev) {
 		ev.preventDefault();
 		ev.stopPropagation(); // BugId:16984
@@ -33,13 +32,16 @@ require( ['jquery'], function( $ ){
 		}
 	});
 
-	if ( !window.touchstart ) {
+	$userLoginDropdown = $('#UserLoginDropdown');
+
+	if (!window.touchstart) {
 		window.delayedHover(
-			$entryPoint.get( 0 ),
+			$entryPoint.get(0),
 			{
 				checkInterval: 100,
 				maxActivationDistance: 20,
-				onActivate: openMenu
+				onActivate: openMenu,
+				onDeactivate: ($userLoginDropdown.length ? Function.prototype : closeMenu)
 			}
 		);
 	}
