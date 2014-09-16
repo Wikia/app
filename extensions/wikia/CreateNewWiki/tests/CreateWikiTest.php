d692575ce3f14f65942ef2a7585630b0fa909b3d
Michał 'Mix' Roszka <mix@wikia-inc.com>
2014-09-16 13:04:35 +0000

    added unit tests

--- extensions/wikia/CreateNewWiki/tests/CreateWikiTest.php
+++ extensions/wikia/CreateNewWiki/tests/CreateWikiTest.php
@@ -13,9 +13,22 @@ class CreateWikiTest extends WikiaBaseTest {
 	public function setUp() {
 		$this->setupFile = __DIR__ . '/../CreateNewWiki_setup.php';
 		parent::setUp();
+		$this->mockGlobalVariable( 'wgMemc', new EmptyBagOStuff() );
 	}
 
+	public function testWgUploadDirectoryExistsAssertion() {
+		$this->setExpectedException('\Wikia\Util\AssertionException');
+		$this->mockStaticMethod( 'WikiFactory', 'getVarIdByName', null);
+		CreateWiki::wgUploadDirectoryExists( 'whatever-input' );
+	}
+	
 	public function testWgUploadDirectoryExists() {
-		$this->assertTrue();
+		$this->mockStaticMethod( 'WikiFactory', 'getVarIdByName', 17 );
+
+		$this->mockStaticMethod( 'WikiFactory', 'getCityIDsFromVarValue', [] );
+		$this->assertFalse( CreateWiki::wgUploadDirectoryExists( 'a-directory-that-does-not-exist' ) );
+
+		$this->mockStaticMethod( 'WikiFactory', 'getCityIDsFromVarValue', [ 1, 2, 3, 5, 8, 12 ] );
+		$this->assertTrue( CreateWiki::wgUploadDirectoryExists( 'a-directory-that-does-exist' ) );
 	}
 }
