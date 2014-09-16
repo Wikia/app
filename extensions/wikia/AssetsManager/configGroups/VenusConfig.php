9a1e125d6755d8a8575a65ff41a229c49a481b72
Bogna Knychala <bogna.ka@gmail.com>
2014-09-16 23:49:30 +0200

    CON-1689 fix facebook login on Venus

--- extensions/wikia/AssetsManager/configGroups/VenusConfig.php
+++ extensions/wikia/AssetsManager/configGroups/VenusConfig.php
@@ -106,13 +106,19 @@ $VenusConfig[ 'global_navigation_js' ] = [
 		'//extensions/wikia/GlobalNavigation/js/SearchSuggestions.js',
 		'//skins/shared/js/transparent-out.js',
 		'//extensions/wikia/UserLogin/js/UserLoginDropdown.globalNavigation.js',
+		'//extensions/wikia/UserLogin/js/UserLoginAjaxForm.js',
+		'//extensions/wikia/WikiaStyleGuide/js/Form.js',
+		'//resources/wikia/libraries/bootstrap/tooltip.js',
+		'//resources/wikia/libraries/jquery/getResources/jquery.wikia.getResources.js',
+		'//resources/wikia/libraries/jquery/loadLibrary/jquery.wikia.loadLibrary.js',
 	]
 ];
 
 $VenusConfig[ 'global_navigation_facebook_login_js' ] = [
 	'type' => AssetsManager::TYPE_JS,
-	'skin' => [ 'venus '],
+	'skin' => [ 'venus'],
 	'assets' => [
+		'//extensions/FBConnect/fbconnect.js',
 		'//extensions/wikia/UserLogin/js/UserLoginFacebook.js',
 		'//extensions/wikia/UserLogin/js/UserLoginFacebookForm.js',
 	]
