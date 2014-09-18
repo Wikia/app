1f93c59bd1219d3cbd3f05aad428c58c0407b2d3
Bogna Knychala <bogna.ka@gmail.com>
2014-09-18 23:11:06 +0200

    CON-1763 add tracking to search and search suggestions

--- extensions/wikia/GlobalNavigation/js/GlobalNavigationTracking.js
+++ extensions/wikia/GlobalNavigation/js/GlobalNavigationTracking.js
@@ -1,52 +1,120 @@
 $(function ($) {
 	'use strict';
-	var track, $globalNavigation;
+	var track, $globalNavigation, $globalNavigationSearch, searchSuggestionsShowed;
 
 	$globalNavigation = $('#globalNavigation');
+	$globalNavigationSearch = $globalNavigation.find('#searchForm');
+	searchSuggestionsShowed = false;
 
-	track = Wikia.Tracker.buildTrackingFunction({
-		action: Wikia.Tracker.ACTIONS.CLICK,
-		category: 'top-nav',
-		trackingMethod: 'ga'
-	});
+	function globalNavigationClickTrackingHandler(event) {
+		var $element;
 
-	function clickTrackingHandler(ev) {
-		var element = $(ev.target);
-		debugger;
-		if (element.closest('.wikia-logo').length > 0) { // wikia logo
+		//Track only primary mouse button click
+		if (event.type === 'mousedown' && event.which !== 1) {
+			return;
+		}
+
+		track = Wikia.Tracker.buildTrackingFunction({
+			action: Wikia.Tracker.ACTIONS.CLICK,
+			category: 'top-nav',
+			trackingMethod: 'ga'
+		});
+
+		$element = $(event.target);
+		if ($element.closest('.wikia-logo').length > 0) { // wikia logo
 			track({
 				label: 'wikia-logo'
 			});
-		} else if (element.closest('.hub-links').length > 0 || element.closest('.hub-list').length > 0) { // hub link
+		} else if ($element.closest('.hub-links').length > 0 || $element.closest('.hub-list').length > 0) { // hub link
 			track({
 				label: 'hub-item'
 			});
-		} else if (element.data('id') === 'logout') {
+		} else if ($element.data('id') === 'logout') {
 			track({
 				label: 'user-menu-logout'
 			});
-		} else if (element.data('id') === 'help') {
+		} else if ($element.data('id') === 'help') {
 			track({
 				label: 'user-menu-help'
 			});
-		} else if (element.hasClass('message-wall')) {
+		} else if ($element.hasClass('message-wall')) {
 			track({
 				label: 'user-menu-message-wall'
 			});
-		} else if (element.closest('.start-wikia').length > 0) { //start a wikia button
+		} else if ($element.closest('.start-wikia').length > 0) { //start a wikia button
 			track({
 				label: 'start-wiki'
 			});
-		} else if (element.hasClass('login-button')) { // log in button
+		} else if ($element.hasClass('login-button')) { // log in button
 			track({
 				label: 'login'
 			});
-		} else if (element.closest('.ajaxLogin').attr('accesskey') === '.') { //user menu profile
+		} else if ($element.closest('.ajaxLogin').attr('accesskey') === '.') { //user menu profile
 			track({
 				label: 'user-menu-profile'
 			});
 		}
 	}
 
-	$globalNavigation.on('mousedown touchstart', clickTrackingHandler);
+	function searchSuggestionsClickTrackingHandler(event) {
+		if (event.type === 'mousedown' && event.which !== 1) {
+			return;
+		}
+
+		trackSearch({
+			browserEvent: event,
+			label: 'search-suggest'
+		});
+	}
+
+	function searchSubmitButtonClickTrackingHandler(event) {
+		if (event.which === 1 && event.clientX > 0) {
+			var label = !searchSuggestionsShowed ? 'search-button' : 'search-after-suggest-button';
+			trackSearch({
+				label: label
+			});
+		}
+	}
+
+	function searchSubmitEnterPressTrackingHandler(event) {
+		if (event.which === 13 && $(this).is(':focus') ) {
+			var label = !searchSuggestionsShowed ? 'search-enter' : 'search-after-suggest-enter';
+			trackSearch({
+				label: label
+			});
+		}
+	}
+
+	function searchSuggestionsEnterPressOnSuggestionsTrackingHandler() {
+		trackSearch({
+			label: 'search-suggest-enter'
+		});
+	}
+
+	function searchSuggestionsShowedTrackingHandler() {
+		searchSuggestionsShowed = true;
+
+		trackSearch({
+			action: Wikia.Tracker.ACTIONS.VIEW,
+			label: 'search-suggest-show'
+		});
+	}
+
+	function trackSearch(data) {
+		track = Wikia.Tracker.buildTrackingFunction({
+			category: 'search',
+			trackingMethod: 'both'
+		});
+		data.action = data.action || Wikia.Tracker.ACTIONS.CLICK;
+
+		track(data);
+	}
+
+	$globalNavigation.on('mousedown touchstart', globalNavigationClickTrackingHandler);
+	$globalNavigationSearch
+		.on('mousedown touchstart', '.autocomplete', searchSuggestionsClickTrackingHandler)
+		.on('mousedown touchstart', '.search-submit', searchSubmitButtonClickTrackingHandler)
+		.on('keypress', '[name=search]', searchSubmitEnterPressTrackingHandler)
+		.on('suggestEnter', searchSuggestionsEnterPressOnSuggestionsTrackingHandler)
+		.one('suggestShow', searchSuggestionsShowedTrackingHandler);
 });
