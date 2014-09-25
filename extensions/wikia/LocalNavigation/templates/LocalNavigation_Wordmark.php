f8cbcd5b9b57bc7f5307045231d036ad6a45be4d
Bogna Knychala <bogna.ka@gmail.com>
2014-09-25 21:33:38 +0200

    CON-1706 cr fixes

--- extensions/wikia/LocalNavigation/templates/LocalNavigation_Wordmark.php
+++ extensions/wikia/LocalNavigation/templates/LocalNavigation_Wordmark.php
@@ -1,6 +1,6 @@
 <header class="wordmark-container">
 	<a class="wordmark" accesskey="z" href="<?= htmlspecialchars( $mainPageURL ) ?>">
-		<? if (!empty($wordmarkUrl)) { ?>
+		<? if ( !empty( $wordmarkUrl ) ) { ?>
 			<img alt="<?= htmlspecialchars( $wordmarkText ) ?>" src="<?= $wordmarkUrl ?>">
 		<? } else { ?>
 			<span class="wordmark-text <?= $wordmarkFontSize ?>"><?= htmlspecialchars( $wordmarkText ) ?></span>
