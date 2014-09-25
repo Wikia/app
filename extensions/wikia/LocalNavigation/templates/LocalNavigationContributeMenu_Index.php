9c3860bb4f489ac2aaeb6169b9c9f2cb0b76ee5c
Warkot <mateusz.warkocki@wikia-inc.com>
2014-09-24 18:00:17 -0700

    CON-1709 | second iteration (rtl/ltr to be moved to separate class)

--- extensions/wikia/LocalNavigation/templates/LocalNavigationContributeMenu_Index.php
+++ extensions/wikia/LocalNavigation/templates/LocalNavigationContributeMenu_Index.php
@@ -1,4 +1,4 @@
-<div class="contribute">
+<div class="contribute-wrapper">
 	<div class="contribute-button"><?= wfMessage('oasis-button-contribute-tooltip')->text() ?></div>
 	<ul class="dropdown">
 <?php
