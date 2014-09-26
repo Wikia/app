91333345c468c78ed6c6a6ea44db1d052bb953bf
lizlux <liz@lizlux.net>
2014-09-25 17:58:10 -0700

    VID-2013 adding RUM profiling to VideosModule request. Also adjusting bucky js

--- extensions/wikia/Bucky/js/bucky_init.js
+++ extensions/wikia/Bucky/js/bucky_init.js
@@ -1,9 +1,19 @@
-$(function () {
-	var config = $.extend({}, window.wgBuckyConfig, {context: window.wgTransactionContext});
-	Bucky.setOptions(config);
-	$(window).load(function () {
-		setTimeout(function () {
-			Bucky.sendPagePerformance(false);
-		}, 0);
+/* global Bucky */
+(function (context) {
+	'use strict';
+
+	if (context.define && context.define.amd) {
+		context.define('bucky', [], Bucky);
+	}
+
+	$(function () {
+		var config = $.extend({}, window.wgBuckyConfig, {context: window.wgTransactionContext});
+		Bucky.setOptions(config);
+		$(window).load(function () {
+			setTimeout(function () {
+				Bucky.sendPagePerformance(false);
+			}, 0);
+		});
 	});
-});
+
+})(window);
