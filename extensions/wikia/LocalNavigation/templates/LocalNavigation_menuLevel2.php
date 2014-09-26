ab923b6349337e448ec785ba4c78bb53e3d6c776
VForge <bartosz.bentkowski@gmail.com>
2014-09-26 16:46:42 -0700

    Fix chrome

--- extensions/wikia/LocalNavigation/templates/LocalNavigation_menuLevel2.php
+++ extensions/wikia/LocalNavigation/templates/LocalNavigation_menuLevel2.php
@@ -2,14 +2,11 @@
 	<? foreach ($nodes as $i2 => $node2): ?>
 		<li class="local-nav-entry">
 			<a href="<?= $node2['href']; ?>" data-content="<?= $node2['text']; ?>"
-				<?if (!empty($node2['children'])): ?>
-					class="has-more"
-				<? endif ?>
-			>
-			<span><?= $node2['text']; ?></span>
 			<?if (!empty($node2['children'])): ?>
-				<span class="more"></span>
+				class="has-more"
 			<? endif ?>
+			>
+				<span><?= $node2['text']; ?></span>
 			</a>
 			<? if ( $node2['children'] ): ?>
 				<?= $app->renderView('LocalNavigation', 'menuLevel3', ['nodes' => $node2['children']]); ?>
