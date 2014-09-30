ff28b5059cfa6ed9772da035835314d40a8103d1
Lukasz Konieczny <lukaszk@wikia-inc.com>
2014-09-30 19:39:03 +0000

    CON-1707: Cache local nav widths

--- extensions/wikia/LocalNavigation/scripts/LocalNavigationMenu.js
+++ extensions/wikia/LocalNavigation/scripts/LocalNavigationMenu.js
@@ -1,10 +1,59 @@
 (function($) {
 	'use strict';
-	var $entryPoints, $localNavStart, $localNav;
+	var $entryPoints, $localNavStart, $localNav, $window,
+		windowWidth, localNavCache = [];
 
 	$localNav = $('#localNavigation');
 	$entryPoints = $localNav.find('.local-nav-entry');
 	$localNavStart = $localNav.find('.first');
+	$window = $(window);
+	windowWidth = $window.width();
+
+	function init(){
+		var secondLvlNav, secondLvlNavWidth = 0, secondLvlNavOffset = 0,
+			thirdLvlWidth = 0, thirdLvlMaxWidth = 0;
+
+		$('> li', $localNavStart).each(function(i){
+			secondLvlNav = $('> ul', this);
+			secondLvlNavWidth = secondLvlNav.outerWidth();
+			secondLvlNavOffset = secondLvlNav.offset().left;
+
+			$('> li', secondLvlNav).each(function(){
+				thirdLvlWidth = $('> ul', this).outerWidth();
+				if ( thirdLvlWidth > thirdLvlMaxWidth ) {
+					thirdLvlMaxWidth = thirdLvlWidth;
+				}
+			});
+
+			localNavCache.push({
+				width: secondLvlNavWidth + secondLvlNavOffset + thirdLvlMaxWidth,
+				menuElement: secondLvlNav
+			});
+
+			if ( secondLvlNavWidth + secondLvlNavOffset + thirdLvlMaxWidth > windowWidth ) {
+				secondLvlNav.addClass('right');
+			} else {
+				secondLvlNav.removeClass('right');
+			}
+		});
+	}
+
+	function recalculateSwap() {
+		var i, arrayLength = localNavCache.length;
+		windowWidth = $window.width();
+
+		if ( arrayLength ) {
+			for (i = 0; i < arrayLength; i++ ) {
+				if ( localNavCache[i].width > windowWidth ) {
+					localNavCache[i].menuElement.addClass('right');
+				} else {
+					localNavCache[i].menuElement.removeClass('right');
+				}
+			}
+		} else {
+			init();
+		}
+	}
 
 	function openMenu() {
 		var $target;
@@ -19,6 +68,12 @@
 
 		$target.removeClass( 'active' );
 	}
+
+	$(window).load(function(){
+		init();
+	});
+
+	$(window).resize(recalculateSwap);
 
 	window.menuAim(
 		$localNavStart.get( 0 ), {
