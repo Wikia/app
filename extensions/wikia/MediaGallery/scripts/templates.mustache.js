1b077ccb0073fcdd90f8c5b06fc93377036f8022
lizlux <liz@lizlux.net>
2014-10-01 13:09:28 -0700

    VID-2016 fixing issue with line breaks inside captions

--- extensions/wikia/MediaGallery/scripts/templates.mustache.js
+++ extensions/wikia/MediaGallery/scripts/templates.mustache.js
@@ -1,6 +1,6 @@
 define( 'mediaGallery.templates.mustache', [], function() { 'use strict'; return {
     "MediaGallery_gallery" : '<div class="media-gallery-wrapper count-{{count}}" data-visible-count="{{visibleCount}}"><script>window.Wikia = window.Wikia || {};Wikia.mediaGalleryData = Wikia.mediaGalleryData || [];Wikia.mediaGalleryData.push({{{json}}});</script><button class="add-image">{{addImageButton}}</button><noscript>{{#media}}<a href="{{linkHref}}"><img src="{{thumbUrl}}" alt="{{{caption}}}"></a>{{/media}}</noscript></div>',
-    "MediaGallery_media" : '{{{thumbHtml}}}{{#caption}}<div class="caption"><div class="inner">{{{caption}}}</div></div>{{/caption}}',
+    "MediaGallery_media" : '{{{thumbHtml}}}{{#caption}}<div class="media-gallery-caption"><div class="inner">{{{caption}}}</div></div>{{/caption}}',
     "MediaGallery_showMore" : '<div class="more"><button class="primary show">{{showMore}}</button><button class="secondary hide hidden">{{showLess}}</button></div>',
     "done": "true"
   }; });
