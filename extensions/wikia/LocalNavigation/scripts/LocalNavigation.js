452964f59ae23d5264a67fbad1a081f834ebcfdd
Warkot <mateusz.warkocki@wikia-inc.com>
2014-09-30 18:19:30 -0700

    CON-1709 | WIP: delayed hover binded, needs transparent out for closing on touch

--- extensions/wikia/LocalNavigation/scripts/LocalNavigation.js
+++ extensions/wikia/LocalNavigation/scripts/LocalNavigation.js
@@ -2,13 +2,31 @@
 	'use strict';
 
 	var entryPoint = window.document.getElementById('contributeEntryPoint'),
-		dropdown = window.document.getElementById('contributeDropdown');
+		dropdown = window.document.getElementById('contributeDropdown'),
+		openMenu,
+		closeMenu;
 
-	entryPoint.addEventListener('mouseover', function() {
+	openMenu = function() {
 		dropdown.classList.add('active');
-	});
+	};
 
-	entryPoint.addEventListener('mouseout', function() {
+	closeMenu = function() {
 		dropdown.classList.remove('active');
-	});
+	};
+
+	if ( !window.ontouchstart ) {
+		window.delayedHover(
+			entryPoint,
+			{
+				checkInterval: 100,
+				maxActivationDistance: 20,
+				onActivate: openMenu,
+				onDeactivate: closeMenu,
+				activateOnClick: true
+			}
+		);
+	} else {
+		entryPoint.click(openMenu);
+	}
+
 })(window);
