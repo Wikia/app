33f030328f7d866252950914a8366ae2e70e4415
nAndy <nandy@wikia-inc.com>
2014-10-03 15:41:58 +0000

    simple unit test for WikiaInteractiveMapsBaseController::isUserAllowed()

--- extensions/wikia/WikiaInteractiveMaps/tests/WikiaInteractiveMapsBaseControllerTest.php
+++ extensions/wikia/WikiaInteractiveMaps/tests/WikiaInteractiveMapsBaseControllerTest.php
@@ -23,6 +23,52 @@ class WikiaInteractiveMapsBaseControllerTest extends WikiaBaseTest {
 	}
 
 	/**
+	 * @dataProvider isUserAllowedDataProvider
+	 */
+	public function testIsUserAllowed( $testCaseDesc, $isLoggedInMock, $isBlockedMock, $expected) {
+		$userMock = $this->getMockBuilder('User')
+			->disableOriginalConstructor()
+			->setMethods( ['isLoggedIn', 'isBlocked'] )
+			->getMock();
+
+		$userMock->expects($this->once())
+			->method('isLoggedIn')
+			->willReturn($isLoggedInMock);
+
+		$userMock->expects($this->any())
+			->method('isBlocked')
+			->willReturn($isBlockedMock);
+
+		$this->mockGlobalVariable('wgUser', $userMock);
+
+		$controller = new WikiaInteractiveMapsBaseController();
+		$this->assertEquals($expected, $controller->isUserAllowed(), $testCaseDesc);
+	}
+
+	public function isUserAllowedDataProvider() {
+		return [
+			[
+				'logged-in BUT not blocked user',
+				'isLoggedInMock' => true,
+				'isBlockedMock' => false,
+				'expected' => true,
+			],
+			[
+				'logged-in AND not blocked user',
+				'isLoggedInMock' => true,
+				'isBlockedMock' => true,
+				'expected' => false,
+			],
+			[
+				'not logged-in user',
+				'isLoggedInMock' => false,
+				'isBlockedMock' => false,
+				'expected' => false,
+			],
+		];
+	}
+
+	/**
 	 * Helper method returns different WikiaUploadStashFile mocks
 	 *
 	 * @param string $version
