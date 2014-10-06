2c383c9ca74f13f15c6dcd3c704da23ad454a634
V <bartosz.bentkowski@gmail.com>
2014-10-06 12:23:04 +0200

    CON-1707 | Remove tag selectors

--- extensions/wikia/LocalNavigation/templates/LocalNavigation_menuLevel3.php
+++ extensions/wikia/LocalNavigation/templates/LocalNavigation_menuLevel3.php
@@ -1,6 +1,6 @@
 <ul class="dropdown third-level-menu">
 	<? foreach ($nodes as $i3 => $node3): ?>
-		<li>
+		<li class="third-level-row">
 			<a href="<?= $node3['href']; ?>" data-content="<?= $node3['textEscaped']; ?>" <?= $node3['canonicalAttr']; ?>>
 				<span><?= $node3['textEscaped']; ?></span>
 			</a>
@@ -9,7 +9,7 @@
 	<? if ( !empty( $more ) ):
 		$label = wfMessage('local-navigation-more-of', $more['text'])->escaped();
 		?>
-		<li>
+		<li class="third-level-row">
 			<a href="<?= $more['href']; ?>" data-content="<?= $label; ?>">
 				<span><?= $label; ?></span>
 			</a>
