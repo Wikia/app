a82a35be58ece1f2fc14eac94f602ebd5f7911ee
V <bartosz.bentkowski@gmail.com>
2014-10-07 18:03:48 +0200

    CON-1872 | Fix

--- extensions/wikia/LocalNavigation/scripts/LocalNavigationContribute.js
+++ extensions/wikia/LocalNavigation/scripts/LocalNavigationContribute.js
@@ -1,26 +1,29 @@
-(function(window) {
+(function(window, $) {
 	'use strict';
 
-	var entryPoint = window.document.getElementById('contributeEntryPoint'), openMenu, closeMenu;
+	$(function(){
+		var entryPoint = window.document.getElementById('contributeEntryPoint'),
+			openMenu,
+			closeMenu;
 
-	openMenu = function() {
-		entryPoint.classList.add('active');
-	};
+		openMenu = function() {
+			entryPoint.classList.add('active');
+		};
 
-	closeMenu = function() {
-		entryPoint.classList.remove('active');
-	};
+		closeMenu = function() {
+			entryPoint.classList.remove('active');
+		};
 
-	if ( !window.ontouchstart ) {
-		window.delayedHover(
-			entryPoint,
-			{
-				onActivate: openMenu,
-				onDeactivate: closeMenu
-			}
-		);
-	} else {
-		entryPoint.click(openMenu);
-	}
-
-})(window);
+		if ( !window.ontouchstart ) {
+			window.delayedHover(
+				entryPoint,
+				{
+					onActivate: openMenu,
+					onDeactivate: closeMenu
+				}
+			);
+		} else {
+			entryPoint.click(openMenu);
+		}
+	});
+})(window, jQuery);
