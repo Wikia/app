a50c425278dd12bd3b6a278b7778f61e6b93e872
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-10-08 16:59:52 +0200

    remove profiling that does not tell you much

--- extensions/wikia/MercuryApi/MercuryApiController.class.php
+++ extensions/wikia/MercuryApi/MercuryApiController.class.php
@@ -20,17 +20,12 @@ class MercuryApiController extends WikiaController {
 	 * @desc Returns smart banner config that is stored in WF
 	 */
 	private function getSmartBannerConfig() {
-		wfProfileIn( __METHOD__ );
-
 		if ( !empty( $this->wg->EnableWikiaMobileSmartBanner )
 			&& !empty( $this->wg->WikiaMobileSmartBannerConfig )
 		) {
-			wfProfileOut( __METHOD__ );
 			return $this->wg->WikiaMobileSmartBannerConfig;
 		}
 
-
-		wfProfileOut( __METHOD__ );
 		return null;
 	}
 
