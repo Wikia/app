f86364969a983702f31238c00f28ad6df601e221
V <bartosz.bentkowski@gmail.com>
2014-10-08 14:42:41 +0200

    CON-1876 | BUG: Contribute button on hubs pages

--- extensions/wikia/LocalNavigation/templates/LocalNavigation_Index.php
+++ extensions/wikia/LocalNavigation/templates/LocalNavigation_Index.php
@@ -1,5 +1,7 @@
 <nav id="localNavigation" class="local-navigation">
 	<?= $app->renderView( 'LocalNavigation', 'Wordmark') ?>
 	<?= $app->renderView( 'LocalNavigation', 'menu' ); ?>
-	<?= $app->renderView( 'LocalNavigationContributeMenu', 'Index' ) ?>
+	<? if ( !empty( $wg->HideNavigationHeaders ) ): ?>
+		<?= $app->renderView( 'LocalNavigationContributeMenu', 'Index' ) ?>
+	<? endif; ?>
 </nav>
