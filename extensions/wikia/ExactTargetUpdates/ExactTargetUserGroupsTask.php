3655ec276cd2c8d664955f7a1f6764150a4e14ff
Kamil Koterba <kamil@wikia-inc.com>
2014-10-10 14:00:43 +0200

    CE-1019 Adding task for removing user group

--- extensions/wikia/ExactTargetUpdates/ExactTargetUserGroupsTask.php
+++ extensions/wikia/ExactTargetUpdates/ExactTargetUserGroupsTask.php
@@ -7,7 +7,7 @@ class ExactTargetUserGroupsTask extends ExactTargetBaseTask {
 	 * @param int $iUserId
 	 * @param string $sGroup name of added group
 	 */
-	public function addUserGroup( $iUserId, $sGroup ) {
+	public function addGroup( $iUserId, $sGroup ) {
 
 		$oClient = $this->getClient();
 		/* Create user_groups DataExtension for provided group and user */
@@ -27,6 +27,30 @@ class ExactTargetUserGroupsTask extends ExactTargetBaseTask {
 		}
 	}
 
+	/**
+	 * Task for creating all necessary objects in ExactTarget related to newly created user
+	 * @param int $iUserId Selected fields from Wikia user table
+	 * @param string $sUserGroup Array of Wikia user gobal properties
+	 */
+	public function removeGroup( $iUserId, $sGroup ) {
+		/* @var ExactTargetSoapClient $oClient */
+		$oClient = $this->getClient();
+		/* Create User DataExtension with new email */
+		try {
+			$oDE = $this->prepareUserGroupDataExtensionObjectForRemove( $iUserId, $sGroup );
+			$oSoapVar = $this->wrapToSoapVar( $oDE );
+			$oRequest = $this->wrapDeleteRequest( [ $oSoapVar ] );
+
+			/* Send API request */
+			$oClient->Delete( $oRequest );
+
+			/* Log response */
+			$this->info( $oClient->__getLastResponse() );
+		} catch ( SoapFault $e ) {
+			/* Log error */
+			$this->error( 'SoapFault:' . $e->getMessage() . 'ErrorCode: ' . $e->getCode() );
+		}
+	}
 	/**
 	 * Prepares proper ExactTarget_DataExtensionObject for creating user_group record in ExactTarget
 	 * @param int $iUserId
@@ -48,5 +72,33 @@ class ExactTargetUserGroupsTask extends ExactTargetBaseTask {
 		$oDE->Properties = $apiProperties;
 		return $oDE;
 	}
+
+	/**
+	 * Prepares proper ExactTarget_DataExtensionObject object
+	 * for sending remove request
+	 * @param int $iUserId id of user to be removed
+	 * @return ExactTarget_DataExtensionObject
+	 */
+	public function prepareUserGroupDataExtensionObjectForRemove( $iUserId, $sGroup ) {
+
+		/* Create new DataExtensionObject that reflects user table data */
+		$oDE = new ExactTarget_DataExtensionObject();
+		/* CustomerKey is a key that indicates Wikia table reflected by DataExtension */
+		$oDE->CustomerKey = 'user_groups';
+
+		/* Prepare query keys */
+		$oDE->Keys = [
+			$this->wrapApiProperty( 'ug_user',  $iUserId ),
+			$this->wrapApiProperty( 'ug_group',  $sGroup )
+		];
+
+		return $oDE;
+	}
+
+
+
+
+
+
 
 }
--- extensions/wikia/ExactTargetUpdates/hooks/ExactTargetUpdatesUserGroups.hooks.php
+++ extensions/wikia/ExactTargetUpdates/hooks/ExactTargetUpdatesUserGroups.hooks.php
@@ -3,28 +3,57 @@
 class ExactTargetUpdatesUserGroupsHooks {
 
 	/**
-	 * Runs a method for adding addUserGroup task to job queue
-	 * Function executed on UserAddGroup hook
+	 * Runs a method that adds a task to job queue for adding user group
+	 * Function executed on AfterUserAddGlobalGroup hook
 	 * @param User $user
+	 * @param string $sGroup
 	 * @return bool
 	 */
-	public static function onUserAddGroup( User $user, $sGroup ) {
+	public static function onAfterUserAddGlobalGroup( User $user, $sGroup ) {
 		$thisInstance = new ExactTargetUpdatesUserGroupsHooks();
-		$thisInstance->addTheAddUserGroupTask( $user, $sGroup, new ExactTargetUserGroupsTask() );
+		$thisInstance->addTheUserAddGroupTask( $user, $sGroup, new ExactTargetUserGroupsTask() );
 		return true;
 	}
 
 	/**
-	 * Adds Task to job queue that updates a user or adds a user if one doesn't exist
+	 * Runs a method for adding a user group remove task to job queue
+	 * Function executed on Aftergit stUserRemoveGlobalGroup hook
+	 * @param User $user
+	 * @param string $sGroup
+	 * @return bool
+	 */
+	public static function onAfterUserRemoveGlobalGroup( User $user, $sGroup ) {
+		$thisInstance = new ExactTargetUpdatesUserGroupsHooks();
+		$thisInstance->addTheUserRemoveGroupTask( $user, $sGroup, new ExactTargetUserGroupsTask() );
+		return true;
+	}
+
+	/**
+	 * Adds Task to job queue that adds user group to ExactTarget
 	 * @param User $oUser
 	 * @param string $sGroup
 	 * @param ExactTargetUserGroupsTask $task
 	 */
-	public function addTheAddUserGroupTask( User $oUser, $sGroup, ExactTargetUserGroupsTask $task ) {
+	public function addTheUserAddGroupTask( User $oUser, $sGroup, ExactTargetUserGroupsTask $task ) {
 		global $wgWikiaEnvironment;
 		/* Don't add task when on dev or internal */
 		if ($wgWikiaEnvironment != WIKIA_ENV_DEV && $wgWikiaEnvironment != WIKIA_ENV_INTERNAL) {
-			$task->call( 'addUserGroup', $oUser->getId(), $sGroup );
+			$task->call( 'addGroup', $oUser->getId(), $sGroup );
+			$task->queue();
+		}
+	}
+
+	/**
+	 * Adds Task to job queue that removes user group from ExactTarget
+	 * @param User $oUser
+	 * @param string $sGroup
+	 * @param ExactTargetUserGroupsTask $task
+	 */
+	public function addTheUserRemoveGroupTask( User $oUser, $sGroup, ExactTargetUserGroupsTask $task ) {
+		global $wgWikiaEnvironment;
+		/* Don't add task when on dev or internal */
+		if ($wgWikiaEnvironment != WIKIA_ENV_DEV && $wgWikiaEnvironment != WIKIA_ENV_INTERNAL) {
+			$task->call( 'removeGroup', $oUser->getId(), $sGroup );
 			$task->queue();
 		}
 	}
