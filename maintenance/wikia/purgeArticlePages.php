757c3b423a802c51ccfc2ca1a1d85d5adbdff707
saipetch <saipetch@wikia-inc.com>
2014-10-10 17:53:17 -0700

    Monetization (MON-237): MonetizationModule - updated comment

--- maintenance/wikia/purgeArticlePages.php
+++ maintenance/wikia/purgeArticlePages.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-* Maintenance script to purge article pages for the wiki
+* Maintenance script to clear varnish cache for article pages for the wiki
 * This is one time use script
 * @author Saipetch Kongkatong
 */
