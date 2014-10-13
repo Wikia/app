c9793d246d912c5c24229fb98bddad04691a0009
V <bartosz.bentkowski@gmail.com>
2014-10-13 17:39:56 +0200

    CON-1809 | Some changes after CR

--- extensions/wikia/GlobalNavigation/js/GlobalNavigation.js
+++ extensions/wikia/GlobalNavigation/js/GlobalNavigation.js
@@ -1,39 +1,49 @@
-$(function() {
+
+
+(function($) {
 	'use strict';
+	var offset, init, scrollTo, handleLinkTo, pushIntoHistory;
 
-	// fix for anchored elements
-	var fromTop = $('#globalNavigation').height() + 10; /* 10px of additional padding */
+	offset = 80; // height of #globalNavigation plus some additional space
 
-	/**
-	 * Check a href for an anchor. If exists, and in document, scroll to it.
-	 * If href argument ommited, assumes context (this) is HTML Element,
-	 * which will be the case when invoked by jQuery after an event
-	 *
-	 * based on this solution: http://jsfiddle.net/ianclark001/aShQL/
-	 */
-	function scroll_if_anchor(href) {
-		href = typeof(href) === 'string' ? href : $(this).attr('href');
+	init = function() {
+		handleLinkTo(window.location.hash);
 
-		// If our Href points to a valid, non-empty anchor, and is on the same page (e.g. #foo)
-		// Legacy jQuery and IE7 may have issues: http://stackoverflow.com/q/1593174
+		$('body').on('click', 'a', function(event){
+			if (handleLinkTo(this.getAttribute('href'))) {
+				event.preventDefault();
+			}
+		});
+	};
+
+	scrollTo = function(targetOffset) {
+		window.scrollTo(0, parseInt(targetOffset, 10));
+	};
+
+	handleLinkTo = function (href) {
 		if(href.indexOf("#") === 0) {
 			var $target = $(href);
 
-			// Older browser without pushState might flicker here, as they momentarily
-			// jump to the wrong position (IE < 10)
-			if($target.length) {
-				$('html, body').animate({ scrollTop: $target.offset().top - fromTop });
-				if(history && "pushState" in history) {
-					history.pushState({}, document.title, window.location.pathname + href);
-					return false;
+			if ($target.length) {
+				//scrollTo($target.offset().top - offset);
+				$('html, body').animate({ scrollTop: $target.offset().top - offset });
+				if (pushIntoHistory({}, document.title, window.location.pathname + href)) {
+					return true;
 				}
 			}
 		}
-	}
+		return false;
+	};
 
-	// When our page loads, check to see if it contains and anchor
-	scroll_if_anchor(window.location.hash);
+	pushIntoHistory = function(state, title, url) {
+		if(history && "pushState" in history) {
+			history.pushState(state, title, url);
+			return true;
+		}
+		return false;
+	};
 
-	// Intercept all anchor clicks
-	$('body').on('click', 'a', scroll_if_anchor);
-});
+	$(function(){
+		init();
+	});
+}(jQuery));
