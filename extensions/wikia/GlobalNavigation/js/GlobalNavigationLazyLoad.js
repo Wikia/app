2ef1dfdc1c73377bfae16368392856dea715101f
Rafal Leszczynski <rafal@wikia-inc.com>
2014-10-14 13:50:17 +0200

    refactoring and code cleanup

--- extensions/wikia/GlobalNavigation/js/GlobalNavigationLazyLoad.js
+++ extensions/wikia/GlobalNavigation/js/GlobalNavigationLazyLoad.js
@@ -1,78 +1,76 @@
 define(
-	'wikia.globalnavigation.lazyload',
-	['jquery', 'wikia.nirvana', 'wikia.querystring'],
-	function lazyLoad( $, nirvana, Querystring ) {
+	'wikia.globalnavigation.lazyload', ['jquery', 'wikia.nirvana', 'wikia.querystring'],
+	function lazyLoad($, nirvana, Querystring) {
 		'use strict';
 
 		var menuLoading = false,
-			menuLoaded = false,
-			getHubLinks,
-			getMenuItemsDone,
-			errorHandler,
-			isMenuWorking,
-			subMenuSelector;
+			menuLoaded = false;
 
 		/**
 		 * @desc callback to handle request that come back with success (Creation of submenus)
 		 * @param {Object} menuItems JSON object with all submenu for Global Nav data
 		 */
-		getMenuItemsDone = function( menuItems ) {
-			var $sections = $( $.parseHTML( menuItems ) ).removeClass( 'active' ),
-				$hubs = $( '#hubs' ),
-				$verticals = $( '> .hub-list', $hubs ),
-				$hubLinks = $( '> .hub-links', $hubs );
+		function getMenuItemsDone(menuItems) {
+			var $sections = $($.parseHTML(menuItems)).removeClass('active'),
+				$hubs = $('#hubs'),
+				$verticals = $('> .hub-list', $hubs),
+				$hubLinks = $('> .hub-links', $hubs),
+				subMenuSelector = '.' + $('.active', $verticals).data('vertical') + '-links';
 
-			$( '> .active', $hubLinks ).removeClass( 'active' );
+			$('> .active', $hubLinks).removeClass('active');
 
-			subMenuSelector = '.' + $( '.active', $verticals ).data( 'vertical' ) + '-links';
-
-			$hubLinks.append( $sections );
-			$hubLinks.find( subMenuSelector ).addClass( 'active' );
+			$hubLinks.append($sections);
+			$hubLinks.find(subMenuSelector).addClass('active');
 
 			menuLoading = false;
 			menuLoaded = true;
-		};
+		}
 
 		/**
-		 * Callback to handle request when there is some error...
+		 * @desc Callback to handle request when there is some error...
 		 */
-		errorHandler = function() {
+		function errorHandler() {
 			menuLoading = false;
 			menuLoaded = false;
-		};
+		}
 
-		getHubLinks = function() {
+		/**
+		 * @desc checks if menu is loaded or is being loaded
+		 * @returns {Boolean}
+		 */
+		function isMenuWorking() {
+			return (menuLoading || menuLoaded);
+		}
+
+		/**
+		 * @desc loads hub menu sections
+		 */
+		function getHubLinks() {
 			var lang,
 				data = {};
 
-			if (menuLoaded || menuLoading) {
+			if (isMenuWorking()) {
 				return;
 			}
 
 			menuLoading = true;
 
-			lang = new Querystring().getVal( 'uselang' );
-			if ( lang ) {
+			lang = new Querystring().getVal('uselang');
+			if (lang) {
 				data.uselang = lang;
 			}
 
-			$.when(
-				nirvana.sendRequest({
-					controller: 'GlobalNavigationController',
-					method: 'lazyLoadHubsMenu',
-					format: 'html',
-					type: 'GET',
-					data: data
-				})
-			).then(
+			nirvana.sendRequest({
+				controller: 'GlobalNavigationController',
+				method: 'lazyLoadHubsMenu',
+				format: 'html',
+				type: 'GET',
+				data: data
+			}).then(
 				getMenuItemsDone,
 				errorHandler
 			);
-		};
-
-		isMenuWorking = function() {
-			return ( menuLoading || menuLoaded );
-		};
+		}
 
 		return {
 			getHubLinks: getHubLinks,
