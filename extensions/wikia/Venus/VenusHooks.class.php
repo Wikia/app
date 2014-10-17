39c725a6bc96103c43404c3bc0e72af977233dc5
Lukasz Konieczny <lukaszk@wikia-inc.com>
2014-10-17 14:02:49 +0000

    CON-1787: Extract infobox from first article section

--- extensions/wikia/Venus/VenusHooks.class.php
+++ extensions/wikia/Venus/VenusHooks.class.php
@@ -2,14 +2,27 @@
 
 class VenusHooks {
 
+	/**
+	 * Check if infobox (div element or table which has class containing 'infobox' string)
+	 * exists in first article section, and extract it from this section
+	 *
+	 * @param $parser Parser
+	 * @param $section number of section in article text
+	 * @param $content section text
+	 * @param $showEditLinks should add edit link
+	 * @return bool
+	 */
 	static public function onParserSectionCreate( $parser, $section, &$content, $showEditLinks ) {
-		if ( $parser->mIsMainParse && $section == 0 ) {
+		if ( $parser->mIsMainParse && $section == 0 && stripos($content, 'infobox') ) {
 			$infoboxes = '';
 			$dom = DOMDocument::loadHTML($content);
 			$finder = new DOMXPath($dom);
-			$nodes = $finder->query("//*[contains(@class, 'infobox')]");
+			$nodes = $finder->query("(//div|//table)[contains(translate(@class, 'INFOBOX', 'infobox'), 'infobox')]");
 
 			foreach( $nodes as $node ) {
+				if ($node->hasAttribute('style')) {
+					$node->removeAttribute('style');
+				}
 				$infoboxes .= $dom->saveHTML($node);
 				$node->parentNode->removeChild($node);
 			}
@@ -22,4 +35,4 @@ class VenusHooks {
 		return true;
 	}
 
-} 
+}
