19b560bf4e51c51d04906c421fa6619df4be4383
Piotr Gabryjeluk <rychu@wikia-inc.com>
2014-10-17 15:33:08 +0200

    Other unit test fix

--- extensions/wikia/AdEngine/js/spec/AdTracker.spec.js
+++ extensions/wikia/AdEngine/js/spec/AdTracker.spec.js
@@ -172,7 +172,7 @@ describe('ext.wikia.adEngine.adTracker', function () {
 			ga_category: 'ad/timing/test/event',
 			ga_action: 'abc=def;xyz=123',
 			ga_label: '0.5-1',
-			ga_value: 888,
+			ga_value: jasmine.any(Number),
 			trackingMethod: 'ad'
 		});
 	});
