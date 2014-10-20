342cf94b3b2cb19a74ce4d8039d78c5e07f3c6be
Kamil Koterba <kamil@wikia-inc.com>
2014-10-20 11:13:46 +0200

    CE-1019 adding new author to extension credits

--- extensions/wikia/ExactTargetUpdates/ExactTargetUpdates.setup.php
+++ extensions/wikia/ExactTargetUpdates/ExactTargetUpdates.setup.php
@@ -33,7 +33,8 @@ $wgExtensionCredits['other'][] = array(
 	'descriptionmsg'    => 'exacttarget-updates-description',
 	'version'           => '0.1',
 	'author'            => array(
-		"Kamil Koterba <kamil@wikia-inc.com>"
+		'Kamil Koterba <kamil@wikia-inc.com>',
+		'Adam Karminski <adamk@wikia-inc.com>',
 	),
 	'url'               => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/ExactTargetUpdates/'
 );
