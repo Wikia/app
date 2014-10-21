d49be831fc29633f4b997a674c8d88ed69fe2f3b
Kamil Koterba <kamil@wikia-inc.com>
2014-10-21 10:34:15 +0200

    CE-1019 Removing more unused methods

--- extensions/wikia/ExactTargetUpdates/tasks/ExactTargetUserTaskHelper.php
+++ extensions/wikia/ExactTargetUpdates/tasks/ExactTargetUserTaskHelper.php
@@ -71,24 +71,6 @@ class ExactTargetUserTaskHelper {
 	}
 
 	/**
-	 * Prepares DataExtension params for prepareApiCreateParams method
-	 * @param int $iUserId
-	 * @param array $aUserProperties array of property name => value pairs
-	 * @return array
-	 */
-	public function prepareUserPropertiesCreateParams( $iUserId, array $aUserProperties ) {
-		$aDataExtensionsParams = [];
-		foreach ( $aUserProperties as $sProperty => $sValue ) {
-			$aDataExtensionsParams[] = [
-				'up_user' => $iUserId,
-				'up_property' => $sProperty,
-				'up_value' => $sValue
-			];
-		}
-		return $aDataExtensionsParams;
-	}
-
-	/**
 	 * Prepares array of params for ExactTarget API for creating DataExtension objects for user table
 	 * @param int $iUserId User id
 	 * @param array $aUserProperties user_properties key value array
@@ -139,40 +121,6 @@ class ExactTargetUserTaskHelper {
 				'Keys' => [
 					'up_user' => $iUserId,
 					'up_property' => $sProperty
-				]
-			];
-		}
-		return $aApiParams;
-	}
-
-	/**
-	 * Returns array of params for ExactTarget API for creating DataExtension objects
-	 * @param array $aDataExtensionsParams
-	 * @param string $sCustomerKey
-	 * @return array
-	 * e.g result params for creating two DataExtenision objects
-	 * [
-	 *   [
-	 *     'DataExtension' => [
-	 *       'CustomerKey' => 'user_properties',
-	 *       'Properties' => ['key'='value']
-	 *     ]
-	 *   ]
-	 *   [
-	 *     'DataExtension' => [
-	 *       'CustomerKey' => 'user_properties',
-	 *       'Properties' => ['key'='value']
-	 *     ]
-	 *   ]
-	 * ]
-	 */
-	public function prepareApiCreateParams( $aDataExtensionsParams, $sCustomerKey ) {
-		$aApiParams = [];
-		foreach ( $aDataExtensionsParams as $aProperties ) {
-			$aApiParams[] = [
-				'DataExtension' => [
-					'CustomerKey' => $sCustomerKey,
-					'Properties' => $aProperties
 				]
 			];
 		}
