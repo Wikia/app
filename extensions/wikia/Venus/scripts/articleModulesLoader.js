5632695b0ea6fc2633d8e21b206847c899d6e7dc
Bogna Knychala <bogna.ka@gmail.com>
2014-10-21 13:43:37 +0200

    CON-NewArticle fix tables on tablets

--- extensions/wikia/Venus/scripts/articleModulesLoader.js
+++ extensions/wikia/Venus/scripts/articleModulesLoader.js
@@ -3,7 +3,8 @@ require([
 	'jquery', 'venus.lightboxLoader', 'scrollableTables', 'wikia.window'
 ], function($, lightboxLoader, scrollableTables, win) {
 	'use strict';
-	var doc = win.document;
+	var doc = win.document,
+		$win = $(win);
 
 	/** Look for all tables on article and add or remove scrollbar if needed */
 	function scanTables() {
@@ -21,8 +22,8 @@ require([
 	//scan for tables in article and if table is too wide add scrollbar
 	scanTables();
 
-	$(win)
-		.on('resize', $.debounce(50, scanTables))
-		// wikiaTabClicked event is triggered when user switches between different tabs in article
-		.on('wikiaTabClicked', scanTables);
+		$win
+			.on('resize', $.throttle(100, scanTables))
+			// wikiaTabClicked event is triggered when user switches between different tabs in article
+			.on('wikiaTabClicked', scanTables);
 });
