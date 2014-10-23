392250bc2809092c0613d98b2dc05e52f6fab106
V <bartosz.bentkowski@gmail.com>
2014-10-23 11:33:49 +0200

    CON-1809 | Fix race condition

--- extensions/wikia/GlobalNavigation/scripts/GlobalNavigationScrollToLink.js
+++ extensions/wikia/GlobalNavigation/scripts/GlobalNavigationScrollToLink.js
@@ -6,19 +6,18 @@ require(
 		var offset = 0;
 
 		/**
-		 * @desc handler for clicked links
+		 * @desc handler for hashchange event
 		 * @param {Event} event
 		 */
-		function scrollToLinkHandler(event) {
-			if (scrollToLink.handleScrollTo(event.target.getAttribute('href'), offset)) {
+		function hashChangeHandler(event) {
+			if (scrollToLink.handleScrollTo(win.location.hash, offset)) {
 				// prevent only if we managed to scroll to desired ID
 				event.preventDefault();
 			}
 		}
 
 		/**
-		 * @desc Initialize: deal with hash passed as part of URL, bind handler for clicks on link
-		 * elements inside BODY
+		 * @desc Initialize: deal with hash passed as part of URL, bind handler for hash change event
 		 */
 		function initScrollToLink() {
 			// offset is negative - we want scroll BEFORE element's top offset
@@ -27,12 +26,12 @@ require(
 
 			scrollToLink.disableBrowserJump();
 
-			// setTimeout is needed here because of Chrome which sometimes scrolls to top on end of page load
+			// setTimeout after 1 second is needed here because of Chrome which sometimes scrolls to top on end of DOM load
 			win.setTimeout(function() {
 				scrollToLink.handleScrollTo(win.location.hash, offset);
-			});
+			}, 1000);
 
-			$('body').on('click', 'a', scrollToLinkHandler);
+			$(win).on('hashchange', hashChangeHandler);
 		}
 
 		// bind to DOMReady
