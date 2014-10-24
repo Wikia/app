c26de1da332289df70a9d0ddba62bb5bb0c0fa20
Bogna Knychala <bogna.ka@gmail.com>
2014-10-24 15:16:30 +0200

    CON-1814 fix issue with fixed elements on ipads

--- extensions/wikia/GlobalNavigation/js/GlobalNavigationSearch.js
+++ extensions/wikia/GlobalNavigation/js/GlobalNavigationSearch.js
@@ -1,46 +1,87 @@
-(function($) {
+require(['jquery', 'wikia.browserDetect', 'wikia.window'], function ($, browserDetect, win) {
 	'use strict';
-	$(function() {
+	var $selectElement, $chevron, cachedScrollY, $searchInput, $globalNav;
+
+	/**
+	 * Look up elements in global navigation's search form and set options for it
+	 */
+	function setFormOptions() {
 		var $inputResultLang = $('#searchInputResultLang'),
 			$formElement = $('#searchForm'),
-			$selectElement = $('#searchSelect'),
-			$searchInput = $('#searchInput'),
 			$searchLabel = $('#searchLabelInline'),
-			$chevron = $('#searchFormChevron');
+			$selectedOption;
+
+		$selectElement = $('#searchSelect');
+		$chevron = $('#searchFormChevron');
+		$searchInput = $('#searchInput');
+		$globalNav = $('#globalNavigation');
 
 		if (!$selectElement) {
 			return;
 		}
 
-		function setFormOptions() {
-			var $selectedOption;
-
-			$selectedOption = $selectElement.find('option:selected');
-			$searchLabel.text($selectedOption.text());
-			$formElement.attr('action', $selectedOption.attr('data-search-url'));
-			if ($selectedOption.val() === 'global') {
-				$inputResultLang.prop('disabled', false);
-				if ($searchInput.data('autocomplete')) {
-					$searchInput.data('autocomplete').disable();
-				}
-			} else {
-				$inputResultLang.prop('disabled', true);
-				if ($searchInput.data('autocomplete')) {
-					$searchInput.data('autocomplete').enable();
-				}
+		$selectedOption = $selectElement.find('option:selected');
+		$searchLabel.text($selectedOption.text());
+		$formElement.attr('action', $selectedOption.attr('data-search-url'));
+		if ($selectedOption.val() === 'global') {
+			$inputResultLang.prop('disabled', false);
+			if ($searchInput.data('autocomplete')) {
+				$searchInput.data('autocomplete').disable();
+			}
+		} else {
+			$inputResultLang.prop('disabled', true);
+			if ($searchInput.data('autocomplete')) {
+				$searchInput.data('autocomplete').enable();
 			}
 		}
+	}
+
+	/**
+	 * Scroll window to top
+	 */
+	function scrollToTop() {
+		cachedScrollY = win.scrollY;
+		setTimeout(function () {
+			win.scrollTo(win.scrollX, 0);
+			$globalNav.addClass('position-static');
+		}, 5);
+	}
+
+	/**
+	 * Restore scrollY to position cached inside cachedScrollY var
+	 */
+	function restoreScrollY() {
+		win.scrollTo(win.scrollX, cachedScrollY);
+		$globalNav.removeClass('position-static');
+	}
+
+	$(function () {
+		var isPositionFixedSupported = browserDetect.isPositionFixedSupported();
 
 		setFormOptions();
 
-		$selectElement.on('change keyup keydown', function() {
+		$selectElement.on('change keyup keydown', function () {
 			setFormOptions();
 		});
-		$selectElement.on('focus', function() {
+
+		$selectElement.on('focus', function () {
 			$chevron.addClass('dark');
+			if (browserDetect.isPositionFixedSupported()) {
+				scrollToTop();
+			}
 		});
-		$selectElement.on('blur', function() {
+
+		$selectElement.on('blur', function () {
 			$chevron.removeClass('dark');
+			if (browserDetect.isPositionFixedSupported()) {
+				restoreScrollY();
+			}
 		});
+
+		if (!isPositionFixedSupported) {
+			$searchInput
+				.on('focus', scrollToTop)
+				.on('blur', restoreScrollY);
+		}
 	});
-}(jQuery));
+});
---
+++ resources/wikia/modules/spec/browserDetect.spec.js
@@ -0,0 +1,39 @@
+/* global describe, modules, expect, it */
+describe('browserDetect', function () {
+	'use strict';
+
+	var winMock = {
+			navigator: {
+				userAgent: ''
+			}
+		},
+		browserDetectModule;
+
+	it('Check if position fixed is not supported for iPad with iOS7', function () {
+		winMock.navigator.userAgent =
+			'Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53';
+		browserDetectModule = modules['wikia.browserDetect'](winMock);
+		expect(browserDetectModule.isPositionFixedSupported()).toBe(false);
+	});
+
+	it('Check if position fixed is not supported for iPad with iOS6', function () {
+		winMock.navigator.userAgent =
+			'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53';
+		browserDetectModule = modules['wikia.browserDetect'](winMock);
+		expect(browserDetectModule.isPositionFixedSupported()).toBe(false);
+	});
+
+	it('Check if position fixed is supported for iPad with iOS8', function () {
+		winMock.navigator.userAgent =
+			'Mozilla/5.0 (iPad; CPU OS 8_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53';
+		browserDetectModule = modules['wikia.browserDetect'](winMock);
+		expect(browserDetectModule.isPositionFixedSupported()).toBe(true);
+	});
+
+	it('Check if position fixed is supported for Android device', function () {
+		winMock.navigator.userAgent =
+			'Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36';
+		browserDetectModule = modules['wikia.browserDetect'](winMock);
+		expect(browserDetectModule.isPositionFixedSupported()).toBe(true);
+	});
+});
