6d36495f04dc25594b98f2e8fc14dcbc4903d495
James Sutterfield <james@wikia-inc.com>
2014-10-27 15:47:16 -0700

    VID-2110 update unit test descriptions

--- extensions/wikia/Lightbox/scripts/spec/lightbox.spec.js
+++ extensions/wikia/Lightbox/scripts/spec/lightbox.spec.js
@@ -4,11 +4,11 @@ describe('Lightbox', function () {
 	var windowMock = {},
 		historyApi = modules['wikia.history'](windowMock);
 
-	it('has access to the history.js, a module it depends on', function () {
+	it('has access to history.js, a module Lightbox depends on', function () {
 		expect(historyApi).toBeDefined();
 		expect(typeof historyApi).toBe('object');
 	});
-	it('history.js has replaceState defined, a function it depends on', function () {
+	it('history.js has replaceState defined, a function Lightbox depends on', function () {
 		expect(historyApi.replaceState).toBeDefined();
 		expect(typeof historyApi.replaceState).toBe('function');
 	});
