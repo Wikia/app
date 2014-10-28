a5099d61b6de997825c8e9cd0eae759cc6aa3940
Kamil Koterba <kamil@wikia-inc.com>
2014-10-28 11:54:51 +0100

    CE-1105 Adding testShouldUpdate test

--- extensions/wikia/ExactTargetUpdates/tests/ExactTargetUserTasksAdderHooksTest.php
+++ extensions/wikia/ExactTargetUpdates/tests/ExactTargetUserTasksAdderHooksTest.php
@@ -2,10 +2,7 @@
 
 class ExactTargetUserTasksAdderHooksTest extends WikiaBaseTest {
 
-	public function testPrepareUserParams() {
-		//pass
-		return;
-
+	public function _testPrepareUserParams() {
 		$userMock = $this->getMockBuilder( 'User' )
 			->disableOriginalConstructor()
 			->setMethods( [ 'getId', 'getName', 'getRealName', 'getEmail', 'getEmailAuthenticationTimestamp',
@@ -108,7 +105,32 @@ class ExactTargetUserTasksAdderHooksTest extends WikiaBaseTest {
 		$this->assertEquals( $aUserPropertiesParamsActual, $aUserPropertiesParamsExpected );
 	}
 
+	/**
+	 * Determine whether update should be performed on specific environment
+	 * @dataProvider shouldUpdateProvider
+	 * @param string $sProduction
+	 * @param bool $bDevelopmentMode
+	 * @param bool $bExpectsShouldUpdate
+	 */
+	function testShouldUpdate( $sEnvironment, $bDevelopmentMode, $bExpectsShouldUpdate ) {
+		/* Mock global vars */
+		$this->mockGlobalVariable( 'wgWikiaEnvironment', $sEnvironment );
+		$this->mockGlobalVariable( 'wgExactTargetDevelopmentMode', $bDevelopmentMode );
+		/* Get mock object of tested class */
+		$exactTargetUpdatesHooksMock = $this->getMockBuilder( 'ExactTargetSetupHooks' )
+			->setMethods( null )
+			->getMock();
 
+		/* Use reflection and set accessibility of method as some of tested methods may be private */
+		$method = new ReflectionMethod( 'ExactTargetSetupHooks', 'shouldUpdate' );
+		$method->setAccessible( true );
+
+		/* Run tested method */
+		$bActualResult = $method->invoke( $exactTargetUpdatesHooksMock );
+
+		$this->assertEquals( $bActualResult, $bExpectsShouldUpdate );
+
+	}
 	/**
 	 * Test that task should be created or shouldn't be created on specific environment
 	 * @dataProvider taskNotCreatedOnDevAnyProvider
@@ -152,6 +174,39 @@ class ExactTargetUserTasksAdderHooksTest extends WikiaBaseTest {
 		array_unshift( $aParams, $exactTargetUpdatesHooksMock );
 		/* Run tested method */
 		call_user_func_array ( [$method, 'invoke'], $aParams );
+	}
+
+
+	/**
+	 * DATA PROVIDERS
+	 */
+
+
+	function shouldUpdateProvider() {
+		/* Define environment constants if not defined yet */
+		if ( !defined( 'WIKIA_ENV_PROD' ) ) {
+			define( WIKIA_ENV_PROD, 'test-prod') ;
+		}
+		if ( !defined( 'WIKIA_ENV_DEV' ) ) {
+			define( WIKIA_ENV_DEV, 'test-dev') ;
+		}
+		if ( !defined( 'WIKIA_ENV_INTERNAL' ) ) {
+			define( WIKIA_ENV_INTERNAL, 'test-internal' );
+		}
+
+		$bDevelopmentModeEnabled = true;
+		$bDevelopmentModeDisabled = false;
+		$bExpectsShouldUpdate = true;
+		$bExpectsShouldNotUpdate = false;
+
+		return [
+			[ WIKIA_ENV_PROD, $bDevelopmentModeDisabled, $bExpectsShouldUpdate ],
+			[ WIKIA_ENV_PROD, $bDevelopmentModeEnabled, $bExpectsShouldUpdate ],
+			[ WIKIA_ENV_DEV, $bDevelopmentModeDisabled, $bExpectsShouldNotUpdate ],
+			[ WIKIA_ENV_DEV, $bDevelopmentModeEnabled, $bExpectsShouldUpdate ],
+			[ WIKIA_ENV_INTERNAL, $bDevelopmentModeDisabled, $bExpectsShouldNotUpdate ],
+			[ WIKIA_ENV_INTERNAL, $bDevelopmentModeEnabled, $bExpectsShouldUpdate ],
+		];
 	}
 
 	function taskNotCreatedOnDevAnyProvider() {
