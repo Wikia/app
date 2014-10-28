46f5331eb7d1f3e42523e984aee43782b30d686f
Rafal Leszczynski <rafal@wikia-inc.com>
2014-10-28 15:41:47 +0100

    create basic logic for dropdown nav

--- resources/wikia/modules/dropdownnav.js
+++ resources/wikia/modules/dropdownnav.js
@@ -1,5 +1,82 @@
 'use strict';
 
-define('wikia.dropdownNavigation', function () {
+define(
+	'wikia.dropdownNavigation',
+	['j.query', 'mustache', 'wikia.dropdownNavigation.template'],
+	function ($, mustache, template) {
 
-});
+		/**
+		 * @desc validates dropdown naviagation options
+		 * @param {Object} params
+		 */
+		function validateParams(params) {
+			if (!Array.isArray(params.data) || params.data.length < 1) {
+				throw new Error('"data" param must be non empty array');
+			}
+			if (typeof params.trigger !== 'string' || params.trigger.length < 1) {
+				throw new Error('"trigger" param must be a valid jQuery selector');
+			}
+		}
+
+		/**
+		 * @desc creates new instance of Dropdown Navigation
+		 * @param {Object} options - configuration options
+		 * @returns {Object} - if called without `new` returns new instance of DropdownNavigation
+		 * @constructor
+		 */
+		function DropdownNavigation(options) {
+			validateParams(options);
+
+			if (!(this instanceof DropdownNavigation)) {
+				return new DropdownNavigation(options);
+			}
+
+			var params = {
+					id: 'wikiaDropdownNav' + Math.random(),
+					maxHeight: 400,
+					posX: 'top',
+					posY: 'right',
+					scrollX: true
+				},
+				$trigger = $(options.trigger),
+				$dropdown;
+
+			$.extend(params, options);
+			renderDropdown(params);
+			attachHandlers();
+
+			/**
+			 * @desc adds dropdown to DOM and caches selectors
+			 */
+			function renderDropdown(params) {
+				$trigger.after(mustache(template, params));
+				$dropdown = $(params.id);
+			}
+
+			/**
+			 * @desc shows dropdown
+			 */
+			function show() {
+				$dropdown.addClass('active');
+			}
+
+			/**
+			 * @desc hides dropdown
+			 */
+			function hide() {
+				$dropdown.addClass('active');
+			}
+
+			/**
+			 * @desc attaches event handlers
+			 */
+			function attachHandlers() {
+				$('body')
+					.on('click', params.trigger, show)
+					.on('click', params.id + 'a', hide);
+			}
+		}
+
+		return DropdownNavigation;
+	}
+);
