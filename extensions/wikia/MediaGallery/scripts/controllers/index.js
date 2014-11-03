6011ecead399206e15252f618e701aa861e3c6eb
lizlux <liz@lizlux.net>
2014-11-03 15:15:07 -0800

    VID-2089 using mw.hook to handle events for loading new galleries

--- extensions/wikia/MediaGallery/scripts/controllers/index.js
+++ extensions/wikia/MediaGallery/scripts/controllers/index.js
@@ -3,18 +3,6 @@ require([
 ], function (GalleriesController) {
 	'use strict';
 
-	/**
-	 * Convenience function for initializing the gallery elements.
-	 */
-	function newGallery() {
-		new GalleriesController().init();
-	}
-
-	// Galleries must be initialized:
-	// In preview dialog
-	$(window).on('EditPageAfterRenderPreview', newGallery);
-	// After VisualEditor
-	mw.hook('postEdit').add(newGallery);
-	// On page load
-	$(newGallery);
+	// Galleries must be initialized whenerver article content is loaded
+	mw.hook('wikipage.content').add(GalleriesController().init);
 });
