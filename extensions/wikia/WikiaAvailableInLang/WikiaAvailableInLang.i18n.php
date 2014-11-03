590dc13ee63290c10bc8a9eef52ce410725adced
adamkarminski <adamkarminski@gmail.com>
2014-11-03 13:58:32 +0000

    CE-1103 Add a i18n file

---
+++ extensions/wikia/WikiaAvailableInLang/WikiaAvailableInLang.i18n.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *  Extension message file
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'exacttarget-updates-description' => "Displays a notification if a wikia is available in a user's native language. The check is based on Geo cookie and a browser's language.",
+);
+
+/**
+ * Message documentation
+ */
+$messages['qqq'] = array(
+	'exacttarget-updates-description' => '{{desc}}',
+);
+
+/** Polish (polski)
+ */
+$messages['pl'] = array(
+	'exacttarget-updates-description' => 'Wyświetla powiadomienie, jeżeli dana wikia dostępna jest w ojczystym języku użytkownika. Sprawdzenie dotyczy ciasteczka Geo oraz języka przeglądarki.',
+);
