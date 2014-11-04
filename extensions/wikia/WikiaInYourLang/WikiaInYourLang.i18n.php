7ffdbd61a450d44ded6571423227248ef0103898
adamkarminski <adamkarminski@gmail.com>
2014-11-04 19:20:18 +0000

    CE-1103 Add messages to display

--- extensions/wikia/WikiaInYourLang/WikiaInYourLang.i18n.php
+++ extensions/wikia/WikiaInYourLang/WikiaInYourLang.i18n.php
@@ -5,20 +5,27 @@
 
 $messages = array();
 
-$messages['en'] = array(
-	'wikia-in-your-lang-description' => "Displays a notification if a wikia is available in a user's native language. The check is based on Geo cookie and a browser's language.",
-);
-
 /**
  * Message documentation
  */
 $messages['qqq'] = array(
 	'wikia-in-your-lang-description' => '{{desc}}',
+	'wikia-in-your-lang-available' => 'A message to show if a wikia is available in a user\'s native language.',
 );
 
 /**
- * Polish (polski)
+ * English (en)
  */
-$messages['pl'] = array(
-	'wikia-in-your-lang-description' => 'Wyświetla powiadomienie, jeżeli dana wikia dostępna jest w ojczystym języku użytkownika. Sprawdzenie dotyczy ciasteczka Geo oraz języka przeglądarki.',
+$messages['en'] = array(
+	'wikia-in-your-lang-description' => 'Displays a notification if a wikia is available in a user\'s native language. The check is based on Geo cookie and a browser\'s language.',
+	'wikia-in-your-lang-available' => 'The $1 wikia is also available in English. Check it out! $2',
+);
+
+/**
+ * Japanese (ja)
+ */
+
+$messages['ja'] = array(
+	'wikia-in-your-lang-description' => 'Displays a notification if a wikia is available in a user\'s native language. The check is based on Geo cookie and a browser\'s language.',
+	'wikia-in-your-lang-available' => 'このコミュニティには日本語版 $1 もあります。そちらもチェックしてどんどん編集もしてみよう！ $2',
 );
