44b8fdc03a2665f377e77bef607e18d2b1f7a30b
V <bartosz.bentkowski@gmail.com>
2014-11-04 11:24:12 +0100

    CON-2050 | It's probably more common to use that variable, so we're gonna use this right now

--- includes/wikia/resourceloader/ResourceLoaderHooks.class.php
+++ includes/wikia/resourceloader/ResourceLoaderHooks.class.php
@@ -184,9 +184,9 @@ class ResourceLoaderHooks {
 			$pages = Wikia::renameArrayKeys($pages,$mapping);
 		}
 
-		// Wikia doesn't include Mediawiki:Common.css in Oasis (even more doesn't in Venus)
+		// Wikia doesn't include Mediawiki:Common.css in Oasis/Venus
 		// lower-case skin name is returned by getSkin()
-		if ( ( $skinName == 'oasis' && empty( $wgOasisLoadCommonCSS ) ) || $skinName == 'venus') {
+		if ( in_array($skinName, ['oasis', 'venus']) && empty( $wgOasisLoadCommonCSS ) ) {
 			unset($pages['MediaWiki:Common.css']);
 		}
 
