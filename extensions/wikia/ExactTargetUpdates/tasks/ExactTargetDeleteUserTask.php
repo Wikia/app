8cc9ff23071d243b19a2d63a1cf330f2af023c33
Kamil Koterba <kamil@wikia-inc.com>
2014-11-04 06:48:15 +0100

    CE-1019 Fix method name

--- extensions/wikia/ExactTargetUpdates/tasks/ExactTargetDeleteUserTask.php
+++ extensions/wikia/ExactTargetUpdates/tasks/ExactTargetDeleteUserTask.php
@@ -55,7 +55,7 @@ class ExactTargetDeleteUserTask extends BaseTask {
 	 * that reflects Wikia entry from user_properties table
 	 * @param int $iUserId
 	 */
-	public function deleteUserPropertiesData( $iUserId ) {
+	public function deleteUserProperties( $iUserId ) {
 		$oHelper = $this->getHelper();
 		$aApiParams = $oHelper->prepareUserPropertiesDeleteParams( $iUserId );
 		$oApiDataExtension = $this->getApiDataExtension();
