7fe3fca4fc283911681f1c7f883883a5513b63a2
V <bartosz.bentkowski@gmail.com>
2014-11-05 15:55:14 +0100

    CON-1654 | Handle opening new windows

--- extensions/wikia/ArticleNavigation/scripts/articleNavigation.js
+++ extensions/wikia/ArticleNavigation/scripts/articleNavigation.js
@@ -1,4 +1,4 @@
-require(['wikia.document', 'wikia.stickyElement', 'venus.variables'], function(doc, stickyElement, v) {
+require(['wikia.window', 'wikia.document', 'wikia.stickyElement', 'venus.variables'], function(win, doc, stickyElement, v) {
 	'use strict';
 
 	var navigationElement = doc.getElementsByClassName('article-navigation')[0],
@@ -6,6 +6,25 @@ require(['wikia.document', 'wikia.stickyElement', 'venus.variables'], function(d
 		globalNavigationHeight = doc.getElementsByClassName('global-navigation')[0].offsetHeight,
 		additionalTopOffset = 100;
 
+	// Sticky navigation
 	stickyElement.spawn().init(navigationElement, boundBoxElement, globalNavigationHeight + additionalTopOffset,
 		v.breakpointSmallMin);
+
+	//Share link events
+	[].forEach.call(doc.getElementsByClassName('share-link'), function(element) {
+		console.log(element);
+
+		element.addEventListener('click', function(event) {
+			var url = element.getAttribute('href'),
+				title = element.getAttribute('title'),
+				h = (window.innerHeight / 2 | 0),
+				w = (window.innerWidth / 2 | 0);
+
+			win.open(url, title, 'width=' + w + ',height=' + h);
+
+			event.stopPropagation();
+			event.preventDefault();
+			return false;
+		});
+	});
 });
