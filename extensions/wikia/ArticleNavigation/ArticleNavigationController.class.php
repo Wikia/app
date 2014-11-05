0ef7d28ec5feebf0787a643eeafb51e53c7e044b
V <bartosz.bentkowski@gmail.com>
2014-11-05 14:27:33 +0100

    CON-1654 | Basic icons

--- extensions/wikia/ArticleNavigation/ArticleNavigationController.class.php
+++ extensions/wikia/ArticleNavigation/ArticleNavigationController.class.php
@@ -5,6 +5,24 @@ class ArticleNavigationController extends WikiaController {
 	public function index() {
 		Wikia::addAssetsToOutput( 'article_navigation_scss' );
 		Wikia::addAssetsToOutput( 'article_navigation_js' );
-		$this->response->setTemplateEngine( WikiaResponse::TEMPLATE_ENGINE_MUSTACHE );
+
+		$this->shareData = self::shareData();
+	}
+
+	public static function shareData() {
+		global $wgArticleNavigationShareServices;
+
+		$services = $wgArticleNavigationShareServices;
+		$location = 'http://' . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+
+		for ($i = sizeof( $services ) - 1; $i >= 0 ; $i--) {
+			$services[$i]['full_url'] = str_replace( '$1', urlencode( $location ), $services[$i]['url'] );
+			$services[$i]['name_cased'] = ucfirst( $services[$i]['name'] );
+			if ( !array_key_exists( 'title', $services[$i] ) ) {
+				$services[$i]['title'] = $services[$i]['name_cased'];
+			}
+		}
+
+		return $services;
 	}
 }
---
+++ extensions/wikia/ArticleNavigation/templates/ArticleNavigation_index.php
@@ -0,0 +1,25 @@
+<nav class="article-navigation multiple-share">
+	<ul>
+		<li>
+			<span class="nav-icon toc" id="articleNavToc">&#xF000;</span>
+		</li>
+		<li>
+			<span class="nav-icon edit" id="articleNavEdit">&#xF001;</span>
+		</li>
+		<li>
+			<span class="nav-icon settings" id="articleNavSettings">&#xF002;</span>
+		</li>
+		<? if ( false ): // disable this for now ?>
+		<li data-share-type="single">
+			<span class="nav-icon share" id="articleNavShare">&#xF006;</span>
+		</li>
+		<? endif; ?>
+		<? foreach ($shareData as $share): ?>
+			<li data-share-type="multiple">
+				<span class="nav-icon share-<?= $share['name'] ?>" id="articleNavShare<?= $share['name_cased'] ?>">
+					<a href="<?= $share['full_url'] ?>" target="_blank" title="<?= $share['title'] ?>"><?= $share['icon'] ?></a>
+				</span>
+			</li>
+		<? endforeach; ?>
+	</ul>
+</nav>
