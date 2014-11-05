05968561999f05d663a5b446cf7d70e6afdb508e
adamkarminski <adamkarminski@gmail.com>
2014-11-05 16:41:09 +0000

    CE-1103 Use short array notation and put Enlish in the front

--- extensions/wikia/WikiaInYourLang/WikiaInYourLang.i18n.php
+++ extensions/wikia/WikiaInYourLang/WikiaInYourLang.i18n.php
@@ -6,24 +6,24 @@
 $messages = array();
 
 /**
- * Message documentation
- */
-$messages['qqq'] = array(
-	'wikia-in-your-lang-description' => '{{desc}}',
-	'wikia-in-your-lang-available' => 'A message to show when a wikia is available in a user\'s native language.',
-);
-
-/**
  * English (en)
  */
-$messages['en'] = array(
+$messages['en'] = [
 	'wikia-in-your-lang-description' => 'Displays a notification if a wikia is available in a user\'s native language. The check is based on Geo cookie and a browser\'s language.',
 	'wikia-in-your-lang-available' => 'The $1 wikia is also available in English. Check it out! $2',
-);
+];
+
+/**
+ * Message documentation
+ */
+$messages['qqq'] = [
+	'wikia-in-your-lang-description' => '{{desc}}',
+	'wikia-in-your-lang-available' => 'A message to show when a wikia is available in a user\'s native language.',
+];
 
 /**
  * Japanese (ja)
  */
-$messages['ja'] = array(
+$messages['ja'] = [
 	'wikia-in-your-lang-available' => 'このコミュニティには日本語版 $1 もあります。そちらもチェックしてどんどん編集もしてみよう！ $2',
-);
+];
