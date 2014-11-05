a20b2559ade02ba6bac35d242a2dcce7388d297c
lizlux <liz@lizlux.net>
2014-11-05 11:10:39 -0800

    UC-54 reverting code that somehow made its way into my branch

--- extensions/wikia/MediaGallery/scripts/controllers/index.js
+++ extensions/wikia/MediaGallery/scripts/controllers/index.js
@@ -10,11 +10,7 @@ require([
 		new GalleriesController().init();
 	}
 
-	// Galleries must be initialized:
-	// In preview dialog
+	// Galleries must be initialized on page-load and on preview dialog
 	$(window).on('EditPageAfterRenderPreview', newGallery);
-	// After VisualEditor
-	mw.hook('postEdit').add(newGallery);
-	// On page load
 	$(newGallery);
 });
