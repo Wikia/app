b986c58c86cd3c0d8a0ca8b00a3d2a14885951c7
James Sutterfield <james@wikia-inc.com>
2014-11-06 11:01:07 -0800

    Add documentation

--- extensions/wikia/Lightbox/scripts/spec/integration/lightbox.spec.js
+++ extensions/wikia/Lightbox/scripts/spec/integration/lightbox.spec.js
@@ -25,6 +25,10 @@ describe('Lightbox', function () {
 
 		it('adds a parameter to the URL when updateUrlState is called with clear option set to false', function () {
 			lightbox.updateUrlState(false);
+			// Inside of lightbox.updateUrlState there is a require() call. This require call is a mocked version
+			// found inside of define.mock.js which doesn't actually execute the callback, but instead stores it
+			// in window.modules['require,wikia.history']. This allows that callback to use mocked dependencies.
+			// We don't actually want to mock the dependencies, so pass in the actual history module to that callback.
 			window.modules['require,wikia.history'](history);
 			expect(window.location.href.split('?').length).toBe(2);
 			expect(window.location.href.split('?')[1]).toBe('file=' + lightbox.current.key);
