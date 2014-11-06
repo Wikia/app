02e9f5e5690cc9758d8abc83705756091fdb4396
V <bartosz.bentkowski@gmail.com>
2014-11-06 11:58:52 +0100

    CON-1654 | New font version

--- extensions/wikia/ArticleNavigation/templates/ArticleNavigation_index.php
+++ extensions/wikia/ArticleNavigation/templates/ArticleNavigation_index.php
@@ -1,17 +1,17 @@
 <nav class="article-navigation multiple-share">
 	<ul>
 		<li>
-			<span class="nav-icon toc" id="articleNavToc">&#xF000;</span>
+			<span class="nav-icon toc" id="articleNavToc">&#xF001;</span>
 		</li>
 		<li>
-			<span class="nav-icon edit" id="articleNavEdit">&#xF001;</span>
+			<span class="nav-icon edit" id="articleNavEdit">&#xF002;</span>
 		</li>
 		<li>
-			<span class="nav-icon settings" id="articleNavSettings">&#xF002;</span>
+			<span class="nav-icon settings" id="articleNavSettings">&#xF003;</span>
 		</li>
 		<? if ( false ): // disable this for now ?>
 		<li data-share-type="single">
-			<span class="nav-icon share" id="articleNavShare">&#xF006;</span>
+			<span class="nav-icon share" id="articleNavShare">&#xF004;</span>
 		</li>
 		<? endif; ?>
 		<? foreach ($shareData as $share): ?>
