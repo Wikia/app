f223bdb820b0f7ff2a5064f635d07052f65f17de
adamkarminski <adamkarminski@gmail.com>
2014-11-06 10:36:29 +0000

    CE-1103 Add missing param docstring

--- extensions/wikia/WikiaInYourLang/WikiaInYourLang.hooks.php
+++ extensions/wikia/WikiaInYourLang/WikiaInYourLang.hooks.php
@@ -10,6 +10,7 @@ class WikiaInYourLangHooks {
 
 	/**
 	 * Add JS module to the output
+	 * @param  OutputPage object  An output object passed from a hook
 	 * @return {bool} true
 	 */
 	public static function onBeforePageDisplay( \OutputPage $out ) {
