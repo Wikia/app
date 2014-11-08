2c76b1b2f70d5364798ffc4b21e975d33b6e2e06
lizlux <liz@lizlux.net>
2014-11-07 17:47:36 -0800

    UC-85 reverting fbconnect js

--- extensions/FBConnect/fbconnect.js
+++ extensions/FBConnect/fbconnect.js
@@ -44,11 +44,11 @@
  */
 window.onFBloaded = function() {
 	var fbAppId = window.fbAppId,
-		// macbre: fix IE issue (RT #140425)
-		// @see http://threebrothers.org/brendan/blog/facebook-connect-ie-fb_xd_fragment-iframe/
+	// macbre: fix IE issue (RT #140425)
+	// @see http://threebrothers.org/brendan/blog/facebook-connect-ie-fb_xd_fragment-iframe/
 		channelUrl = window.location.protocol + '//' +
-			window.location.host + window.wgScriptPath +
-			'/channel.php?lang=' + encodeURIComponent(window.fbScriptLangCode);
+		             window.location.host + window.wgScriptPath +
+		             '/channel.php?lang=' + encodeURIComponent(window.fbScriptLangCode);
 
 	if (window.fbAppInit) {
 		return;
@@ -79,87 +79,86 @@ window.onFBloaded = function() {
  * jQuery code to be run when the DOM is ready to be manhandled.
  */
 $(function() {
-//	var wpCancelClicked = false;
+	var wpCancelClicked = false;
 
-//	// Add a pretty logo to Facebook links
-//	$('#pt-fbconnect,#pt-fblink,#pt-fbconvert').addClass('mw-fblink');
-//
-//	// Add the logout behavior to the "Logout of Facebook" button
-//	$('#pt-fblogout').click(function() {
-//		// TODO: Where did the fancy DHTML window go? Maybe consider jQuery Alert Dialogs:
-//		// http://abeautifulsite.net/2008/12/jquery-alert-dialogs/
-//		var logout = confirm("You are logging out of both this site and Facebook.");
-//		if (logout) {
-//			fbLogout(function () {
-//				window.location = window.fbLogoutURL;
-//			});
-//		}
-//	});
+	// Add a pretty logo to Facebook links
+	$('#pt-fbconnect,#pt-fblink,#pt-fbconvert').addClass('mw-fblink');
+
+	// Add the logout behavior to the "Logout of Facebook" button
+	$('#pt-fblogout').click(function() {
+		// TODO: Where did the fancy DHTML window go? Maybe consider jQuery Alert Dialogs:
+		// http://abeautifulsite.net/2008/12/jquery-alert-dialogs/
+		var logout = confirm("You are logging out of both this site and Facebook.");
+		if (logout) {
+			fbLogout(function () {
+				window.location = window.fbLogoutURL;
+			});
+		}
+	});
 
 
-//	$('#wpCancel').click(function(){
-//		if (!wpCancelClicked) {
-//			var logout = confirm($.msg('fbconnect-logout-confirm'));
-//			if (logout) {
-//				wpCancelClicked = true;
-//				window.FB.getLoginStatus(function(response){
-//					if (response.status === 'connected' ) {
-//						fbLogout(function (){
-//							$('#wpCancel').click();
-//						});
-//					} else {
-//						$('#wpCancel').click();
-//					}
-//				});
-//			}
-//			return false;
-//		} else {
-//			return true;
-//		}
-//	});
+	$('#wpCancel').click(function(){
+		if (!wpCancelClicked) {
+			var logout = confirm($.msg('fbconnect-logout-confirm'));
+			if (logout) {
+				wpCancelClicked = true;
+				window.FB.getLoginStatus(function(response){
+					if (response.status === 'connected' ) {
+						fbLogout(function (){
+							$('#wpCancel').click();
+						});
+					} else {
+						$('#wpCancel').click();
+					}
+				});
+			}
+			return false;
+		} else {
+			return true;
+		}
+	});
 
 	//window.fbAsyncInit ();
-//	$("#fbconnect a").on('click', function(ev){
-//		alert('clicked');
-//		loginByFBConnect();
-//		ev.preventDefault();
-//	});
+	$("#fbconnect a").on('click', function(ev){
+		loginByFBConnect();
+		ev.preventDefault();
+	});
 
-//	if( $.getUrlVar( 'ref' ) === 'fbfeed' ) {
-//		var suffix = '',
-//		type = $.getUrlVar( 'fbtype' );
-//		if( type !== '' ) {
-//			suffix = '/' + type;
-//		}
-//	}
+	if( $.getUrlVar( 'ref' ) === 'fbfeed' ) {
+		var suffix = '',
+			type = $.getUrlVar( 'fbtype' );
+		if( type !== '' ) {
+			suffix = '/' + type;
+		}
+	}
 
 
 	//Checks for visibility of Facebook Like button's hover panel and hides ads accordingly to prevent z-index problems
 	// Christian: (BugId 7297)
-//	if (skin == 'oasis') {
-//		var timer = null,
-//			mouseIn = false,
-//			FBbutton = $('#WikiaPageHeader .likes :first-child');
-//
-//		var poll = function() {
-//			if (FBbutton.children('span').filter(':visible').length > 1) {
-//			} else {
-//				if (!mouseIn) {
-//					clearInterval(timer);
-//				}
-//			}
-//		};
-//
-//		$('#WikiaPageHeader .likes').hover(function() {
-//			mouseIn = true;
-//			timer = setInterval(poll, 250);
-//		}, function() {
-//			mouseIn = false;
-//			if (FBbutton.children('span').filter(':visible').length < 2) {
-//				clearInterval(timer);
-//			}
-//		});
-//	}
+	if (skin == 'oasis') {
+		var timer = null,
+			mouseIn = false,
+			FBbutton = $('#WikiaPageHeader .likes :first-child');
+
+		var poll = function() {
+			if (FBbutton.children('span').filter(':visible').length > 1) {
+			} else {
+				if (!mouseIn) {
+					clearInterval(timer);
+				}
+			}
+		};
+
+		$('#WikiaPageHeader .likes').hover(function() {
+			mouseIn = true;
+			timer = setInterval(poll, 250);
+		}, function() {
+			mouseIn = false;
+			if (FBbutton.children('span').filter(':visible').length < 2) {
+				clearInterval(timer);
+			}
+		});
+	}
 });
 
 /**
@@ -168,124 +167,110 @@ $(function() {
  * This will redirect to Special:Connect with the returnto variables configured properly.
  */
 function sendToConnectOnLogin(){
-	alert('sendToConnectOnLogin');
 	sendToConnectOnLoginForSpecificForm("");
 }
 // Allows optional specification of a form to force on Special:Connect (such as ChooseName, ConnectExisting, or Convert).
 function sendToConnectOnLoginForSpecificForm(formName){
 	FB.getLoginStatus(function(response) {
 		if(formName != ""){
-			formName = "/" + formName;
+			formName = "/"+formName;
 		}
-
-		var destUrl = wgServer +
-			wgScript +
-			"?title=Special:Connect" +
-			formName +
-			"&returnto=" +
-			encodeURIComponent(window.fbReturnToTitle || wgPageName) +
-			"&returntoquery=" +
-			encodeURIComponent(window.wgPageQuery || '');
+		var destUrl = wgServer + wgScript + "?title=Special:Connect" + formName + "&returnto=" + encodeURIComponent(window.fbReturnToTitle || wgPageName) + "&returntoquery=" + encodeURIComponent(window.wgPageQuery || '');
 
 		if (formName == "/ConnectExisting") {
 			window.location.href = destUrl;
 			return;
 		}
 		$('#fbConnectModalWrapper').remove();
-		$.postJSON(
-			window.wgScript +
-				'?action=ajax&rs=SpecialConnect::checkCreateAccount&cb=' +
-				wgStyleVersion,
-			function(data) {
-				if(data.status == "ok") {
+		$.postJSON(window.wgScript + '?action=ajax&rs=SpecialConnect::checkCreateAccount&cb='+wgStyleVersion, function(data) {
+			if(data.status == "ok") {
 
-					// Wikia - UC-18
-					window.Wikia.Tracker.track({
-						category: 'force-login-modal',
-						trackingMethod: 'both',
-						action: window.Wikia.Tracker.ACTIONS.SUCCESS,
-						label: 'facebook-login'
-					});
-					// Wikia end
+				// Wikia - UC-18
+				window.Wikia.Tracker.track({
+					category: 'force-login-modal',
+					trackingMethod: 'both',
+					action: window.Wikia.Tracker.ACTIONS.SUCCESS,
+					label: 'facebook-login'
+				});
+				// Wikia end
 
-					location.reload();
-				} else {
-					window.location.href = destUrl;
-				}
+				location.reload();
+			} else {
+				window.location.href = destUrl;
 			}
-		);
+		});
 	});
 }
 
 
-//function openFbLogin() {
-///*
-//	if (!isFbApiInit()) {
-//		setTimeout(openFbLogin,300);
-//		return true;
-//	}
-//*/
-//	FB.login(sendToConnectOnLogin);
-//}
+function openFbLogin() {
+	/*
+	 if (!isFbApiInit()) {
+	 setTimeout(openFbLogin,300);
+	 return true;
+	 }
+	 */
+	FB.login(sendToConnectOnLogin);
+}
 
 /**
  * only for user header button
  */
-//function loginByFBConnect() {
-///*
-//	if (!isFbApiInit()) {
-//		window.fbAsyncInit();
-//	}
-//*/
-//	openFbLogin();
-//	return false;
-//}
+function loginByFBConnect() {
+	/*
+	 if (!isFbApiInit()) {
+	 window.fbAsyncInit();
+	 }
+	 */
+	openFbLogin();
+	return false;
+}
 
-//function fixXFBML(id) {
-//	// BugId:19603 (IE8 specific fix)
-//	if (typeof $.browser.msie != 'undefined' && typeof $.browser.version != 'undefined' && $.browser.version && $.browser.version.substring(0, $.browser.version.indexOf('.')) < 9) {
-//		GlobalTriggers.bind('fbinit', function() {
-//			var node = $('#' + id).parent(),
-//				html = node.html();
-//
-//			// FB JS adds 'fb' namespace support in IE, regenerate FBML tags
-//			node.html('').append(html);
-//
-//			// force button to be rendered again
-//			FB.XFBML.parse(node.get(0));
-//		});
-//	}
-//}
+function fixXFBML(id) {
+	// BugId:19603 (IE8 specific fix)
+	if (typeof $.browser.msie != 'undefined' && typeof $.browser.version != 'undefined' && $.browser.version && $.browser.version.substring(0, $.browser.version.indexOf('.')) < 9) {
+		GlobalTriggers.bind('fbinit', function() {
+			var node = $('#' + id).parent(),
+				html = node.html();
 
-///**
-// * Wrapper for Facebook Logout
-// * @see UC-18
-// * @param callback
-// */
-//function fbLogout (callback) {
-//	'use strict';
-//	if (!window.FB) {
-//		return;
-//	}
-//
-//	window.FB.logout(function () {
-//		window.Wikia.Tracker.track({
-//			category: 'user-sign-up',
-//			trackingMethod: 'both',
-//			action: window.Wikia.Tracker.ACTIONS.SUCCESS,
-//			label: 'facebook-logout'
-//		});
-//		if (callback && typeof callback === 'function') {
-//			callback();
-//		}
-//	});
-//}
+			// FB JS adds 'fb' namespace support in IE, regenerate FBML tags
+			node.html('').append(html);
 
-//// BugId:19767 - fix FBconnect button on Special:Connect
-//$(function() {
-//	if (window.wgCanonicalSpecialPageName === 'Connect') {
-//		$.loadFacebookAPI( function() {
-//			fixXFBML('fbSpecialConnect');
-//		});
-//	}
-//});
+			// force button to be rendered again
+			FB.XFBML.parse(node.get(0));
+		});
+	}
+}
+
+/**
+ * Wrapper for Facebook Logout
+ * @see UC-18
+ * @param callback
+ */
+function fbLogout (callback) {
+	'use strict';
+	if (!window.FB) {
+		return;
+	}
+
+	window.FB.logout(function () {
+		window.Wikia.Tracker.track({
+			category: 'user-sign-up',
+			trackingMethod: 'both',
+			action: window.Wikia.Tracker.ACTIONS.SUCCESS,
+			label: 'facebook-logout'
+		});
+		if (callback && typeof callback === 'function') {
+			callback();
+		}
+	});
+}
+
+// BugId:19767 - fix FBconnect button on Special:Connect
+$(function() {
+	if (window.wgCanonicalSpecialPageName === 'Connect') {
+		$.loadFacebookAPI( function() {
+			fixXFBML('fbSpecialConnect');
+		});
+	}
+});
