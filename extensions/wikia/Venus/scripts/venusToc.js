f3e2336a1f1bc485624e47658682fa86ce453d49
Rafal Leszczynski <rafal@wikia-inc.com>
2014-11-09 16:30:28 +0100

    nested dropdown support with styling

--- extensions/wikia/Venus/scripts/venusToc.js
+++ extensions/wikia/Venus/scripts/venusToc.js
@@ -1,8 +1,8 @@
 /* global define */
 define(
 	'wikia.venusToc',
-	['wikia.document', 'wikia.toc', 'wikia.dropdownNavigation'],
-	function (doc, tocModule, DropdownNavigation) {
+	['jquery', 'wikia.window', 'wikia.toc', 'wikia.dropdownNavigation'],
+	function ($, win, tocModule, DropdownNavigation) {
 		'use strict';
 
 		var articleWrapperId = 'mw-content-text',
@@ -10,7 +10,16 @@ define(
 			headers = [
 				'h2',
 				'h3'
-			];
+			],
+			delayHoverParams = {
+				onActivate: show,
+				onDeactivate: hide,
+				activateOnClick: false
+			},
+			tocDropdown,
+			// cached jQuery Selectors
+			$triggerButton,
+			$parent;
 
 		/**
 		 * @desc creates single toc data object
@@ -40,14 +49,30 @@ define(
 		 * @desc gets data model for TOC
 		 * @param {Array} headers - array of headers levels (example ['h2, 'h3'])
 		 * @param {string} containerId - id of headers container
+		 * @returns {Array}
 		 */
 		function getTocData(headers, containerId) {
 			var headersSelector = headers.join(','),
-				headersCollection = doc
+				headersCollection = win.document
 					.getElementById(containerId)
 					.querySelectorAll(headersSelector);
 
-			return tocModule.getData(headersCollection, createSection, filterHeader);
+			return tocModule.getData(headersCollection, createSection, filterHeader).sections;
+		}
+
+		/**
+		 * @desc shows dropdown
+		 */
+		function show() {
+			$parent.addClass('active');
+		}
+
+		/**
+		 * @desc hides dropdown
+		 */
+		function hide() {
+			tocDropdown.resetUI();
+			$parent.removeClass('active');
 		}
 
 		/**
@@ -55,10 +80,16 @@ define(
 		 * @param {String} id -  id of the trigger element
 		 */
 		function init(id) {
-			return new DropdownNavigation({
-				data: getTocData(headers, articleWrapperId).sections,
+			var options = {
+				sections: getTocData(headers, articleWrapperId),
 				trigger: id
-			});
+			};
+
+			tocDropdown = new DropdownNavigation(options);
+			$triggerButton = $('#' + id);
+			$parent = $triggerButton.parent();
+
+			win.delayedHover($parent[0], delayHoverParams);
 		}
 
 		return {
--- resources/wikia/ui_components/dropdown_navigation/js/dropdownNavigation.js
+++ resources/wikia/ui_components/dropdown_navigation/js/dropdownNavigation.js
@@ -1,24 +1,17 @@
-'use strict';
-
 define(
 	'wikia.dropdownNavigation',
-	['jquery', 'wikia.window', 'wikia.document', 'wikia.mustache', 'wikia.dropdownNavigation.templates'],
-	function ($, win, doc, mustache, dropdownTemplates) {
-		// used for setting unique id for each dropdown added to the page
-		var dropdownIndex = 0;
+	[
+		'jquery',
+		'wikia.window',
+		'wikia.dropdownNavigation.utils',
+		'wikia.mustache',
+		'wikia.dropdownNavigation.templates'
+	],
+	function ($, win, utils, mustache, templates) {
+		'use strict';
 
-		/**
-		 * @desc validates dropdown naviagation options
-		 * @param {Object} params
-		 */
-		function validateParams(params) {
-			if (!Array.isArray(params.data) || params.data.length < 1) {
-				throw new Error('"data" param must be non empty array');
-			}
-			if (typeof params.trigger !== 'string' || params.trigger.length < 1) {
-				throw new Error('"trigger" param must be a valid jQuery selector');
-			}
-		}
+		// used for generating unique ID for each dropdown rendered on a page
+		var dropdownIndex = 0;
 
 		/**
 		 * @desc creates new instance of Dropdown Navigation
@@ -27,58 +20,62 @@ define(
 		 * @constructor
 		 */
 		function DropdownNavigation(options) {
-			validateParams(options);
 
 			if (!(this instanceof DropdownNavigation)) {
 				return new DropdownNavigation(options);
 			}
 
+			utils.validateParams(options);
+
 			var self = this,
 				dropdownId = 'wikiaDropdownNav' + dropdownIndex++,
-				dropdownParams = {
+				params = {
 					activeClass: 'active',
+					$container: null,
 					id: dropdownId,
-					maxHeight: 400,
-					scrollX: true
+					maxHeight: 400
 				},
-				delayHoverParams = {
-					onActivate: show,
-					onDeactivate: hide,
-					activateOnClick: false
+				menuAIMParams = {
+					activate: showSecondLevelList,
+					deactivate: hideSecondLevelList,
+					tolerance: 85
 				},
 
 				// cached DOM elements
-				$trigger,
-				$container,
-				$dropdown;
+				$sectionsWrapper,
+				$sections,
+				$subsections;
 
 			/**
-			 * @desc adds dropdown to DOM and returns jQuery selector for it
-			 * @returns {jQuery} - jQuery selector for dropdown element
+			 * @desc sets dropdown UI to initial state
 			 */
-			function render() {
-				return $(mustache.render(dropdownTemplates.dropdown_navigation, dropdownParams)).appendTo($container);
+			this.resetUI = function () {
+				this.menuAim.resetActiveRow();
+				$sections
+					.add($subsections)
+					.removeClass(params.activeClass);
+
+				$sectionsWrapper.scrollTop(0);
+			};
+
+			/**
+			 * @desc shows subsection
+			 * @param {Element} row - dropdown "<li>" HTML element
+			 */
+			function showSecondLevelList(row) {
+				$('#' + row.getAttribute('data-id'))
+					.add(row)
+					.addClass(params.activeClass);
 			}
 
 			/**
-			 * @desc shows dropdown
+			 * @desc hides subsection
+			 * @param {Element} row - dropdwon "<li>" HTML element
 			 */
-			function show() {
-				$dropdown.addClass(dropdownParams.activeClass);
-			}
-
-			/**
-			 * @desc hides dropdown
-			 */
-			function hide() {
-				$dropdown.removeClass(dropdownParams.activeClass);
-			}
-
-			/**
-			 * @desc sets dropdown positions
-			 */
-			function setPosition() {
-				$dropdown.css('left', $trigger.outerWidth());
+			function hideSecondLevelList(row) {
+				$('#' + row.getAttribute('data-id'))
+					.add(row)
+					.removeClass(params.activeClass);
 			}
 
 			/**
@@ -86,19 +83,28 @@ define(
 			 * @param {Object} options - configuration options
 			 */
 			function init(options) {
-				$.extend(dropdownParams, options);
+				var html, $dropdownTrigger, $parent, $dropdownWrapper;
 
-				$trigger = $('#' + options.trigger);
-				$container = $trigger.closest('li');
-				$dropdown = render();
+				$.extend(params, options);
+				utils.createSubsectionData(params);
 
-				setPosition();
+				html = mustache.render(templates.dropdown, params, templates);
 
-				// initialize UX enhancements
-				self.delayedHover = win.delayedHover($container[0], delayHoverParams);
+				$dropdownTrigger = $('#' + params.trigger);
+				$parent = params.$container || $dropdownTrigger.closest('li');
+				$dropdownWrapper = $(html).appendTo($parent);
+
+				$sectionsWrapper = $dropdownWrapper.children('ul');
+				$sections = $sectionsWrapper.children();
+				$subsections = $dropdownWrapper.find('div > ul');
+
+				utils.setPosition($dropdownWrapper, $dropdownTrigger);
+
+				// initialize dropdown UX enhancements
+				self.menuAim = win.menuAim($sectionsWrapper[0], menuAIMParams);
 			}
 
-			// initialize dropdown component
+			// initialize component
 			init(options);
 		}
 
