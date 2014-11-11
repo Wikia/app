b3bc9ebdd306c4e276ed79055475a9f1099afe42
Inez Korczynski <inez@wikia-inc.com>
2014-11-11 18:05:27 +0000

    VE-1639: Send AJAX request from ve.ui.WikiaSingleMediaQueryWidget and emit requestMediaDone when results fetched

--- extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaSingleMediaQueryWidget.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaSingleMediaQueryWidget.js
@@ -25,8 +25,14 @@ ve.ui.WikiaSingleMediaQueryWidget = function VeUiWikiaSingleMediaQueryWidget( co
 		'placeholder': config.placeholder,
 		'value': config.value
 	} );
+	this.requestMediaCallback = ve.bind( this.requestMedia, this );
+	this.request = null;
+	this.value = null;
+	this.timeout = null;
+	this.batch = null;
 
 	// Events
+	this.input.connect( this, { 'change': 'onInputChange' } );
 
 	// Initialization
 	this.$element
@@ -51,3 +57,78 @@ OO.inheritClass( ve.ui.WikiaSingleMediaQueryWidget, OO.ui.Widget );
  */
 
 /* Methods */
+
+/**
+ * Handle query input changes.
+ *
+ * @method
+ */
+ve.ui.WikiaSingleMediaQueryWidget.prototype.onInputChange = function () {
+	if ( this.request ) {
+		this.request.abort();
+	}
+	clearTimeout( this.timeout );
+	this.batch = 1;
+	this.value = this.input.getValue();
+	if ( this.value.trim().length !== 0 ) {
+		this.timeout = setTimeout( this.requestMediaCallback, 100 );
+	}
+};
+
+/**
+ * @method
+ */
+ve.ui.WikiaSingleMediaQueryWidget.prototype.requestMedia = function () {
+	this.input.pushPending();
+	this.request = $.ajax( {
+		'url': mw.util.wikiScript( 'api' ),
+		'data': {
+			'format': 'json',
+			'action': 'apimediasearch',
+			'query': this.value,
+			'type': 'photo',
+			'mixed': false,
+			'batch': this.batch,
+			'limit': 16
+		}
+	} )
+		.always( ve.bind( this.onRequestMediaAlways, this ) )
+		.done( ve.bind( this.onRequestMediaDone, this ) );
+};
+
+/**
+ * Handle media request promise.always
+ *
+ * @method
+ */
+ve.ui.WikiaSingleMediaQueryWidget.prototype.onRequestMediaAlways = function () {
+	this.request = null;
+	this.input.popPending();
+};
+
+/**
+ * Handle media request promise.done
+ *
+ * @method
+ * @param {Object} data The response Object from the server.
+ * @fires requestMediaDone
+ */
+ve.ui.WikiaSingleMediaQueryWidget.prototype.onRequestMediaDone = function ( data ) {
+	var items;
+	if ( !data.response || !data.response.results ) {
+		return;
+	}
+	items = data.response.results.photo.items;
+	this.batch++;
+	this.emit( 'requestMediaDone', items );
+};
+
+/**
+ * Get the query input.
+ *
+ * @method
+ * @returns {OO.ui.TextInputWidget} Query input
+ */
+ve.ui.WikiaSingleMediaQueryWidget.prototype.getInput = function () {
+	return this.input;
+};
