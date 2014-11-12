3f9b5c20221f1878a6324c8c994e89b485716912
Rafal Leszczynski <rafal@wikia-inc.com>
2014-11-12 11:50:31 +0100

    add unit test for bulding data model

--- resources/wikia/ui_components/dropdown_navigation/js/spec/dropdownNavigation.utils.spec.js
+++ resources/wikia/ui_components/dropdown_navigation/js/spec/dropdownNavigation.utils.spec.js
@@ -1,16 +1,7 @@
 describe('dropdownNavigation utils', function () {
 	'use strict';
 
-	var utils = modules['wikia.dropdownNavigation']();
-
-	/**
-	 * @desc validation helper
-	 * @param {Object} params
-	 * @throws {Error} - when validation fail
-	 */
-	function validateHelper(params) {
-		utils.validateParams(params);
-	}
+	var utils = modules['wikia.dropdownNavigation.utils']();
 
 	it('throws error when params validation fail', function () {
 		var cases = [
@@ -29,13 +20,125 @@ describe('dropdownNavigation utils', function () {
 			},
 			{
 				sections: [1],
-				trigger: '#',
+				trigger: '',
 				errorMessage: '"trigger" param must be a valid jQuery selector'
 			}
 		];
 
 		cases.forEach(function (params) {
-			expect(validateHelper).toThrow(params.errorMessage);
+			expect(function () {
+				return utils.validateParams(params);
+			}).toThrow(params.errorMessage);
 		});
+	});
+
+	it('create subsection data based on sections children', function () {
+		var cases = [
+			{
+				input: {
+					id: 1,
+					sections: [
+						{
+							sections: []
+						}
+					]
+				},
+				output: {
+					id: 1,
+					sections: [
+						{
+							sections: []
+						}
+					],
+					subsections: []
+				}
+			},
+			{
+				input: {
+					id: 2,
+					sections: [
+						{
+							sections: []
+						},
+						{
+							sections: [1, 2, 3]
+						}
+					]
+				},
+				output: {
+					id: 2,
+					sections: [
+						{
+							sections: []
+						},
+						{
+							referenceId: '2dropdownSection-1',
+							sections: [1, 2, 3]
+						}
+					],
+					subsections: [
+						{
+							referenceId: '2dropdownSection-1',
+							sections: [1, 2, 3]
+						}
+					]
+				}
+			},
+			{
+				input: {
+					id: 3,
+					sections: [
+						{
+							sections: []
+						},
+						{
+							referenceId: '3dropdownSection-1',
+							sections: [1, 2, 3]
+						},
+						{
+							referenceId: '3dropdownSection-2',
+							sections: [4, 5, 6]
+						},
+						{
+							sections: []
+						}
+					]
+				},
+				output: {
+					id: 3,
+					sections: [
+						{
+							sections: []
+						},
+						{
+							referenceId: '3dropdownSection-1',
+							sections: [1, 2, 3]
+						},
+						{
+							referenceId: '3dropdownSection-2',
+							sections: [4, 5, 6]
+						},
+						{
+							sections: []
+						}
+					],
+					subsections: [
+						{
+							referenceId: '3dropdownSection-1',
+							sections: [1, 2, 3]
+						},
+						{
+							referenceId: '3dropdownSection-2',
+							sections: [4, 5, 6]
+						}
+					]
+				}
+			}
+		];
+
+		cases.forEach(function (data) {
+			expect(utils.createSubsectionData(data.input)).toEqual(data.output);
+		});
+
 	});
 });
