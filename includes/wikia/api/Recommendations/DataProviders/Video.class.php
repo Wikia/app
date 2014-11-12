5a7d2d7b023da89608fdc804fa8c81aea6021025
Lukasz Konieczny <lukaszk@wikia-inc.com>
2014-11-12 17:55:38 +0000

    CON-1797: Get video data

--- includes/wikia/api/Recommendations/DataProviders/Video.class.php
+++ includes/wikia/api/Recommendations/DataProviders/Video.class.php
@@ -2,17 +2,41 @@
 namespace Wikia\Api\Recommendations\DataProviders;
 
 class Video implements IDataProvider {
+	const VIDEO_TYPE = 'video';
+	const VIDEO_SOURCE = 'VideoRecommendations';
+
 	public function get( $articleId, $limit ) {
-		// TODO
-		return [
-			[
-				'type' => 'video',
-				'title' => 'Example title',
-				'url' => 'http://muppet.wikia.com/wiki/Kermit',
-				'description' => 'example desciption',
-				'media' => [], // TODO
-				'source' => 'VideoRecommendations'
-			]
-		];
+		$app = \F::app();
+
+		$response = $app->sendRequest( 'VideosModuleController', 'index', [
+			'limit' => $limit
+		]);
+
+		$videos = $response->getData();
+
+		$recommendations = $this->prepareData( $videos['videos'] );
+
+		return $recommendations;
+	}
+
+	protected function prepareData( $videos ) {
+		$data = [];
+
+		foreach ( $videos as $video ) {
+			$data[] = [
+				'type' => self::VIDEO_TYPE,
+				'title' => $video[ 'title' ],
+				'url' => $video[ 'url' ],
+				'description' => '',
+				'source' => self::VIDEO_SOURCE,
+				'media' => [
+					'thumbUrl' => $video[ 'thumbUrl' ],
+					'duration' => $video[ 'duration' ],
+					'videoKey' => $video[ 'videoKey' ]
+				]
+			];
+		}
+
+		return $data;
 	}
 }
