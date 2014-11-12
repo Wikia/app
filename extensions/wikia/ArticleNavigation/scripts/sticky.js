e25735d1e6a9d17bb40192c89ce10fab00713295
V <bartosz.bentkowski@gmail.com>
2014-11-12 17:13:55 +0100

    CON-2085 | Fix for infoboxes hacks

--- extensions/wikia/ArticleNavigation/scripts/sticky.js
+++ extensions/wikia/ArticleNavigation/scripts/sticky.js
@@ -8,7 +8,27 @@ require([
 		globalNavigationHeight = doc.getElementsByClassName('global-navigation')[0].offsetHeight,
 		additionalTopOffset = 100;
 
+	// this function is needed for additional margin for screens >= 1024px
+	// (because header is getting float: left on medium and higher breakpoints)
+	function adjustPosition(value, typ) {
+		switch(typ) {
+			case 'topScrollLimit':
+			case 'topSticked':
+				if (window.matchMedia("(min-width: 1024px)").matches) {
+					return value + $('#WikiaArticle').find('> header').outerHeight(true);
+				}
+			// fall-through on purpose!
+			default:
+				return value;
+		}
+	}
+
 	// Sticky navigation
-	stickyElement.spawn().init(navigationElement, boundBoxElement, globalNavigationHeight + additionalTopOffset,
-		v.breakpointSmallMin);
+	stickyElement.spawn().init({
+		sourceElement: navigationElement,
+		alignToElement: boundBoxElement,
+		topFixed: globalNavigationHeight + additionalTopOffset,
+		minWidth: v.breakpointSmallMin,
+		adjustFunc: adjustPosition
+	});
 });
