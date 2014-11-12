585ca778d0c4d36729a6a7b8949299c2a85b9e22
V <bartosz.bentkowski@gmail.com>
2014-11-12 18:12:55 +0100

    Remove tracking

--- extensions/wikia/ArticleNavigation/scripts/editActions.js
+++ extensions/wikia/ArticleNavigation/scripts/editActions.js
@@ -23,39 +23,6 @@ require([
 		});
 
 		$('#editActionsDropdown').on('mousedown touchstart', 'a', function (e) {
-			var label,
-				el = e.target,
-				name = $(el).data('name');
-
-			// Primary mouse button only
-			if (e.which !== 1) {
-				return;
-			}
-
-			switch (name) {
-				case 'customize':
-				case 'follow':
-				case 'history':
-				case 'whatlinkshere':
-					label = name;
-					break;
-				default:
-					label = 'custom';
-					break;
-			}
-
-			//add anon prefix for not logged in users
-			if (!win.wgUserName) {
-				label = 'anon-' + label;
-			}
-
-			if (label !== undefined) {
-				track({
-					browserEvent: e,
-					category: 'toolbar',
-					label: label
-				});
-			}
 		});
 	}
 });
