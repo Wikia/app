0249bf3b5bc5d30a07b6d818d7aa5b724053a9b1
Kamil Koterba <kamil@wikia-inc.com>
2014-11-12 13:09:47 +0100

    CON-1780 Removing unnecessary row class and changing footer elements display method to inline

--- extensions/wikia/GlobalFooter/templates/GlobalFooter_indexVenus.php
+++ extensions/wikia/GlobalFooter/templates/GlobalFooter_indexVenus.php
@@ -1,4 +1,4 @@
-<footer class="global-footer vertical-<?= $verticalShort ?> row">
+<footer class="global-footer vertical-<?= $verticalShort ?>">
 	<nav>
 		<a href="<?= htmlspecialchars( $centralUrl ) ?>" class="footer-wikia-logo" rel="nofollow">
 			<img src="<?= $wg->BlankImgUrl ?>" height="37" width="134" alt="<?= wfMessage('venus-wikia')->escaped() ?>" title="<?= wfMessage('venus-wikia')->escaped() ?>">
