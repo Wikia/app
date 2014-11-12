b90bddd516012a95ca8a720d46c1737ef213f261
Kamil Koterba <kamil@wikia-inc.com>
2014-11-12 16:25:27 +0100

    CON-1780 Removing nav tag from footer

--- extensions/wikia/GlobalFooter/templates/GlobalFooter_indexVenus.php
+++ extensions/wikia/GlobalFooter/templates/GlobalFooter_indexVenus.php
@@ -1,23 +1,21 @@
 <footer class="global-footer vertical-<?= $verticalShort ?>">
-	<nav>
-		<a href="<?= htmlspecialchars( $centralUrl ) ?>" class="wikia-logo" rel="nofollow">
-			<img src="<?= $wg->BlankImgUrl ?>" height="37" width="134" alt="<?= wfMessage('venus-wikia')->escaped() ?>" title="<?= wfMessage('venus-wikia')->escaped() ?>">
-		</a>
-		<ul class="footer-links">
-			<?php
-			foreach ($footerLinks as $link):
-				?>
-				<li>
-					<?php
-					if ($link['isLicense']) {
-						echo $copyright;
-					} else {?>
-						<a<?= ( !empty( $link[ 'id' ] ) ) ? " id=\"{$link[ 'id' ]}\"" : '' ;?> href="<?= $link["href"]; ?>"<?= ( !empty( $link[ 'nofollow' ] ) ) ? ' rel="nofollow"' : '' ;?>><?= $link["text"]; ?></a>
-					<?php } ?>
-				</li>
-			<?php
-			endforeach;
+	<a href="<?= htmlspecialchars( $centralUrl ) ?>" class="wikia-logo" rel="nofollow">
+		<img src="<?= $wg->BlankImgUrl ?>" height="37" width="134" alt="<?= wfMessage('venus-wikia')->escaped() ?>" title="<?= wfMessage('venus-wikia')->escaped() ?>">
+	</a>
+	<ul class="footer-links">
+		<?php
+		foreach ($footerLinks as $link):
 			?>
-		</ul>
-	</nav>
+			<li>
+				<?php
+				if ($link['isLicense']) {
+					echo $copyright;
+				} else {?>
+					<a<?= ( !empty( $link[ 'id' ] ) ) ? " id=\"{$link[ 'id' ]}\"" : '' ;?> href="<?= $link["href"]; ?>"<?= ( !empty( $link[ 'nofollow' ] ) ) ? ' rel="nofollow"' : '' ;?>><?= $link["text"]; ?></a>
+				<?php } ?>
+			</li>
+		<?php
+		endforeach;
+		?>
+	</ul>
 </footer>
