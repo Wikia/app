b54ba02a8ec55e1e46423eddfab41c9656de7b09
macbre <maciej.brencz@gmail.com>
2014-11-12 14:28:53 +0100

    RecommendationsApi: use "id" param + use "items" root element

--- includes/wikia/api/RecommendationsApiController.class.php
+++ includes/wikia/api/RecommendationsApiController.class.php
@@ -5,10 +5,16 @@ use Wikia\Api\Recommendations\Api;
 class RecommendationsApiController extends WikiaApiController {
 
 	public function getArticle() {
-		$articleId = $this->request->getInt( 'articleId' );
+		$articleId = $this->request->getInt( 'id' );
 		$limit = $this->request->getInt( 'limit', 9 );
 
+		// validate parameters
+		if ( $articleId === 0 ) {
+			// TODO: message
+			throw new BadRequestApiException();
+		}
+
 		$data = ( new Api )->get( $articleId, $limit );
-		$this->setResponseData( $data, null, WikiaResponse::CACHE_STANDARD );
+		$this->setResponseData( [ 'items' => $data ], null, WikiaResponse::CACHE_STANDARD );
 	}
 }
