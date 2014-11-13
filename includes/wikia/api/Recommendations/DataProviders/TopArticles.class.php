33859a324a121fb192f8606ca5b0a98bf2cfbd45
Damian Jozwiak <damian@wikia-inc.com>
2014-11-13 13:14:14 +0100

    CON-2088 | Content Recommendations API | Top Articles

--- includes/wikia/api/Recommendations/DataProviders/TopArticles.class.php
+++ includes/wikia/api/Recommendations/DataProviders/TopArticles.class.php
@@ -2,26 +2,85 @@
 namespace Wikia\Api\Recommendations\DataProviders;
 
 class TopArticles implements IDataProvider {
+	const RECOMMENDATION_ENGINE = 'Top articles';
+	const RECOMMENDATION_TYPE = 'article';
+
 	public function get( $articleId, $limit ) {
-		global $wgCityId, $wgContLang;
+		$hubName = $this->getHubName();
+		$lang = $this->getContentLang();
+
+		// TODO cache
+		$topArticles = $this->getTopArticles( $hubName, $lang, $limit );
+
+		return $this->getArticlesInfo( $topArticles );
+	}
+
+	protected function getHubName() {
+		global $wgCityId;
 
 		// Yes, we need this old category name because rollups don't use new verticals
-		$hubName = \WikiFactoryHub::getInstance()->getCategoryName( $wgCityId );
+		return \WikiFactoryHub::getInstance()->getCategoryName( $wgCityId );
+	}
+
+	protected function getContentLang() {
+		global $wgContLang;
+
+		return $wgContLang->getCode();
+	}
+
+	protected function getTopArticles( $hubName, $lang, $limit ) {
+		$topArticles = [];
 
 		$results = \DataMartService::getTopCrossWikiArticlesByPageview(
 			$hubName,
-			[$wgContLang->getCode()],
+			[$lang],
 			null,
 			$limit
 		);
 
-		foreach ( $results as $wikiSet ) {
+		$articlesCount = ceil( $limit / count($results) );
 
+		foreach ( $results as $wikiResult ) {
+			for ( $i = 0; $i < $articlesCount; $i++ ) {
+				$topArticles[] = [
+					'wikiId' => $wikiResult['wiki']['id'],
+					'articleId' => $wikiResult['articles'][$i]
+				];
+			}
 		}
-		// TODO slice correct number of elems
 
+		return array_slice( $topArticles, 0, $limit );
+	}
 
-		// TODO
-		return [[],[],[]];
+	protected function getArticlesInfo( $topArticles ) {
+		$out = [];
+		$articleService = new \ArticleService();
+
+		foreach ( $topArticles as $topArticleInfo ) {
+			$articleId = $topArticleInfo['articleId'];
+			$title = \GlobalTitle::newFromId( $articleId, $topArticleInfo['wikiId'] );
+
+			$imageServing = new \ImageServing( [$articleId], 400, array( 'w' => 16, 'h' => 9 ) );
+			$images = $imageServing->getImages( 1 );
+
+			$articleService->setArticleById( $articleId );
+
+			$articleInfoExpanded = [
+				'type' => self::RECOMMENDATION_TYPE,
+				'title' => $title->getText(),
+				'url' => $title->getFullURL(),
+				'description' => $articleService->getTextSnippet(),
+				'media' => [],
+				'source' => self::RECOMMENDATION_ENGINE
+			];
+
+			if ( !empty( $images[$articleId] ) ) {
+				$articleInfoExpanded['media']['url'] = $images[$articleId]['url'];
+			}
+
+			$out[] = $articleInfoExpanded;
+		}
+
+		return $out;
 	}
 }
