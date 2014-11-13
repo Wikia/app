976dc4474224bd41b62c189aa37a9f5fbeeede2e
Pawel Chojnacki <pawel@chojnacki.ws>
2014-11-13 13:40:59 +0100

    clean old tests

--- extensions/wikia/CuratedContent/tests/CuratedContentApiTest.php
+++ extensions/wikia/CuratedContent/tests/CuratedContentApiTest.php
@@ -7,39 +7,4 @@ class CuratedContentApiTest extends WikiaBaseTest {
 		$this->setupFile = "{$IP}/extensions/wikia/CuratedContent/CuratedContent.setup.php";
 		parent::setUp();
 	}
-
-
-	public function testSimpleSaveCuratedContent() {
-	}
-
-	public function testSimpleLoadCuratedContent() {
-
-	}
-
-	public function testGetEmptyCuratedContent() {
-	}
-
-	public function testGetNoTagCuratedContent() {
-	}
-
-	public function testGetTagCuratedContentSet1() {
-
-	}
-
-	public function testGetTagCuratedContentSet2() {
-
-	}
-
-	public function testSaveCuratedContentSet1() {
-	}
-
-	public function testSaveCuratedContentSet2() {
-	}
-
-	public function testFailSaveCuratedContentSet1() {
-	}
-
-	public function testFailSaveCuratedContentSet2() {
-	}
-
 }
