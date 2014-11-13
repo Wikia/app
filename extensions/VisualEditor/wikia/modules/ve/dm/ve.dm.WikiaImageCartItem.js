240212e5cf71009b014ed46ba8bc20b765a9925c
Inez Korczynski <inez@wikia-inc.com>
2014-11-13 22:22:20 +0000

    VE-1651: Added documentation for ve.dm.WikiaImageCartItem

--- extensions/VisualEditor/wikia/modules/ve/dm/ve.dm.WikiaImageCartItem.js
+++ extensions/VisualEditor/wikia/modules/ve/dm/ve.dm.WikiaImageCartItem.js
@@ -1,8 +1,23 @@
+/*!
+ * VisualEditor DataModel WikiaImageCartItem class.
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * DataModel WikiaImageCartItem.
+ *
+ * @class
+ * @constructor
+ */
 ve.dm.WikiaImageCartItem = function VeDmWikiaImageCartItem( title, url ) {
 	this.title = title;
 	this.url = url;
 };
 
+/**
+ * @method
+ * @returns {String} Id string
+ */
 ve.dm.WikiaImageCartItem.prototype.getId = function () {
 	return this.title;
 };
