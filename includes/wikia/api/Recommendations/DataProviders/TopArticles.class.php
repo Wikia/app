fcc6e39d507a042ea4cdb940ff22ca92b2d789b7
Damian Jozwiak <damian@wikia-inc.com>
2014-11-13 10:05:31 +0100

    CON-2088 | Top Articles WIP

--- includes/wikia/api/Recommendations/DataProviders/TopArticles.class.php
+++ includes/wikia/api/Recommendations/DataProviders/TopArticles.class.php
@@ -3,9 +3,24 @@ namespace Wikia\Api\Recommendations\DataProviders;
 
 class TopArticles implements IDataProvider {
 	public function get( $articleId, $limit ) {
+		global $wgCityId, $wgContLang;
 
-		$results = \DataMartService::getTopCrossWikiArticlesByPageview( 'games', ['en'], null, 10 );
-		var_dump($results);die;
+		// Yes, we need this old category name because rollups don't use new verticals
+		$hubName = \WikiFactoryHub::getInstance()->getCategoryName( $wgCityId );
+
+		$results = \DataMartService::getTopCrossWikiArticlesByPageview(
+			$hubName,
+			[$wgContLang->getCode()],
+			null,
+			$limit
+		);
+
+		foreach ( $results as $wikiSet ) {
+
+		}
+		// TODO slice correct number of elems
+
+
 		// TODO
 		return [[],[],[]];
 	}
