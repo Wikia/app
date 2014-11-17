bdd6a36f1680e000c3074b6d679aa0b0c95b41db
Damian Jozwiak <damian@wikia-inc.com>
2014-11-17 18:06:36 +0100

    CON-2104 | Content recommendations frontend WIP

--- extensions/wikia/Recommendations/scripts/recommendations.js
+++ extensions/wikia/Recommendations/scripts/recommendations.js
@@ -4,6 +4,9 @@ define(
 	function(loader, win, mustache, msg) {
 		'use strict';
 
+		var controllerName = 'RecommendationsApi',
+			methodName = 'getArticle';
+
 		/**
 		 * Load recommendations template
 		 *
@@ -18,26 +21,47 @@ define(
 					styles: 'extensions/wikia/Recommendations/styles/recommendations.scss',
 					messages: 'Recommendations',
 					templates: [{
-						controller: 'RecommendationsApi',
-						method: 'getArticle',
+						controller: controllerName,
+						method: methodName,
 						params: {
 							id: win.wgArticleId
 						}
 					}]
 				}
 			}).done(function (res) {
-				var data;
+				var data, i, slot = {}, slots = [], slotsData;
 
 				loader.processStyle(res.styles);
 				//loader.processScript(res.scripts);
 				console.log(res);
 
 				if (typeof(callback) === 'function') {
+					slotsData = JSON.parse(res.templates[controllerName + '_' + methodName]).items;
+					// TODO shuffle
+
+					for (i=0; i < slotsData.length; i++) { // TODO length
+						slot = {
+							'title': slotsData[i].title,
+							'url': slotsData[i].url,
+							'description': slotsData[i].description
+							// TODO image
+						};
+
+						if (i === 0) {
+							slot.slotType = 'big';
+						} else {
+							slot.slotType = 'small';
+						}
+
+						slots.push(slot);
+					}
+					console.log(slots);
 
 					data = {
-						header: msg('recommendations-header')
+						header: msg('recommendations-header'),
+						slots: slots
 					};
-					callback(mustache.render(res.mustache[0], data)); // TODO
+					callback(mustache.render(res.mustache[0], data));
 				}
 			});
 		}
