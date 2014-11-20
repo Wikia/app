7aac0ea39dd47184a659b5da05ccc0f4c2851c51
lizlux <liz@lizlux.net>
2014-11-20 15:54:37 -0800

    UC-115 removing unused code

--- extensions/wikia/UserLogin/FacebookSignupController.class.php
+++ extensions/wikia/UserLogin/FacebookSignupController.class.php
@@ -110,8 +110,6 @@ class FacebookSignupController extends WikiaController {
 		switch ($signupResponse['result']) {
 			case 'ok':
 				$this->result = 'ok';
-				// TODO: check if this is ever used
-				$this->location = $signupResponse['userPage'];
 				break;
 
 			case 'error':
