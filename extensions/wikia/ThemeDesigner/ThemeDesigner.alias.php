2af0c5dd2d2b1297dab6caee2462a785a58cdcd5
Onei <mdowdell244@gmail.com>
2014-11-21 15:32:42 +0000

    +translations for ThemeDesignerPreview

--- extensions/wikia/ThemeDesigner/ThemeDesigner.alias.php
+++ extensions/wikia/ThemeDesigner/ThemeDesigner.alias.php
@@ -12,27 +12,29 @@ $specialPageAliases = array();
  * English (English)
  */
 $specialPageAliases['en'] = array(
-	'ThemeDesigner' => array( 'ThemeDesigner' ),
-	'ThemeDesignerPreview' => array( 'ThemeDesignerPreview' ),
+	'ThemeDesigner'		=> array( 'ThemeDesigner' ),
+	'ThemeDesignerPreview'	=> array( 'ThemeDesignerPreview' ),
 );
 
 /**
  * Korean (한국어)
  */
 $specialPageAliases['ko'] = array(
-	'ThemeDesigner' => array( '테마디자이너' ),
+	'ThemeDesigner'		=> array( '테마디자이너' ),
+	'ThemeDesignerPreview'	=> array( '테마디자이너미리보기' ),
 );
 
 /**
  * Polish (Polski)
  */
 $specialPageAliases['pl'] = array(
-	'ThemeDesigner' => array( 'Kreator motywu' ),
+	'ThemeDesigner'		=> array( 'Kreator motywu' ),
 );
 
 /**
  * Vietnamese (Tiếng Việt)
  */
 $specialPageAliases['vi'] = array(
-	'ThemeDesigner' => array( 'Thiết_kế_chủ_đề' ),
+	'ThemeDesigner'		=> array( 'Thiết_kế_chủ_đề' ),
+	'ThemeDesignerPreview'	=> array( 'Mẫu_chủ_đề' ),
 );
