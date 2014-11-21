3be2f05c7404f6a58905a5c0754ea571f6fdf078
adamkarminski <adamkarminski@gmail.com>
2014-11-21 16:06:08 +0100

    CE-1125 Separate hooks from helper

--- extensions/wikia/ImageReview/ImageReview.setup.php
+++ extensions/wikia/ImageReview/ImageReview.setup.php
@@ -31,11 +31,12 @@ $wgAutoloadClasses['Wikia\\Tasks\\Tasks\\ImageReviewTask'] = "{$dir}ImageReviewT
 $wgAutoloadClasses['ImageReviewSpecialController'] =  $dir . 'ImageReviewSpecialController.class.php';
 $wgAutoloadClasses['ImageReviewHelperBase'] =  $dir . 'ImageReviewHelperBase.class.php';
 $wgAutoloadClasses['ImageReviewHelper'] =  $dir . 'ImageReviewHelper.class.php';
+$wgAutoloadClasses['ImageReviewHooks'] =  $dir . 'ImageReview.hooks.php';
 
 $wgSpecialPages['ImageReview'] = 'ImageReviewSpecialController';
 
-// hooks
-$wgHooks['WikiFactoryPublicStatusChange'][] = 'ImageReviewHelper::onWikiFactoryPublicStatusChange' ;
+// hooks setup
+$wgExtensionFunctions[] = 'ImageReviewHooks::setupHooks()';
 
 // rights
 $wgAvailableRights[] = 'imagereview';
--- extensions/wikia/ImageReview/ImageReviewHelper.class.php
+++ extensions/wikia/ImageReview/ImageReviewHelper.class.php
@@ -682,48 +682,4 @@ class ImageReviewHelper extends ImageReviewHelperBase {
 		wfProfileOut( __METHOD__ );
 		return $reviewers;
 	}
-
-	public static function onWikiFactoryPublicStatusChange( $city_public, $city_id, $reason ) {
-		global $wgExternalDatawareDB;
-
-		if ( $city_public == 0 || $city_public == -1 ) {
-			// the wiki was disabled, mark all unreviewed images as deleted
-
-			$newState = ImageReviewStatuses::STATE_WIKI_DISABLED;
-			$statesToUpdate = array(
-				ImageReviewStatuses::STATE_UNREVIEWED,
-				ImageReviewStatuses::STATE_REJECTED,
-				ImageReviewStatuses::STATE_QUESTIONABLE,
-				ImageReviewStatuses::STATE_QUESTIONABLE_IN_REVIEW,
-				ImageReviewStatuses::STATE_REJECTED_IN_REVIEW,
-				ImageReviewStatuses::STATE_IN_REVIEW,
-			);
-		} elseif ( $city_public == 1 ) {
-			// the wiki was re-enabled, put all images back into the queue as unreviewed
-
-			$newState = ImageReviewStatuses::STATE_UNREVIEWED;
-			$statesToUpdate = array(
-				ImageReviewStatuses::STATE_WIKI_DISABLED,
-			);
-		} else {
-			// the state change doesn't affect images, we don't need to do anything here
-			return true;
-		}
-
-		$dbw = wfGetDB( DB_MASTER, array(), $wgExternalDatawareDB );
-
-		$dbw->update(
-			'image_review',
-			array(
-				'state' => $newState,
-			),
-			array(
-				'wiki_id' => $city_id,
-				'state' => $dbw->makeList( $statesToUpdate ),
-			)
-		);
-
-		return true;
-	}
 }
-
