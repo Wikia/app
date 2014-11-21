35ca96612eb00b9eefb842db0e48dee7b2809f59
aquilax <evgeniy@wikia-inc.com>
2014-11-21 12:26:22 +0000

    Handle ParserOutput not returning ParserOutput

---
+++ extensions/wikia/ArticleAsJson/ArticleAsJsonParserException.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class ArticleAsJsonParserException extends WikiaHttpException {
+	protected $code = 503;
+	protected $message = 'Service Unavailable';
+
+	public function __construct($details) {
+		$this->details = $details;
+	}
+} 
