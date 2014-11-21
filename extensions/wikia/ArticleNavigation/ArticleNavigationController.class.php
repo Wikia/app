fb29d3f85eb0d579ab579c4f501d9fbc78c1ecbf
V <bartosz.bentkowski@gmail.com>
2014-11-21 14:55:58 +0100

    CON-2060 | Add new services

--- extensions/wikia/ArticleNavigation/ArticleNavigationController.class.php
+++ extensions/wikia/ArticleNavigation/ArticleNavigationController.class.php
@@ -99,9 +99,11 @@ class ArticleNavigationController extends WikiaController {
 		$protocol = ( !empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443 ) ? 'https://' : 'http://';
 		$location = $protocol . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 		$services = [];
+		$lang = $this->helper->getUserLanguageCode( $this->request );
+
 
 		foreach ($wgArticleNavigationShareServices as $service ) {
-			if ( array_key_exists( 'url', $service ) && array_key_exists( 'name', $service ) ) {
+			if ( $this->helper->isValidShareService($service, $lang ) ) {
 				$service['full_url'] = str_replace( '$1', urlencode( $location ), $service['url'] );
 				$service['name_cased'] = ucfirst( $service['name'] );
 
--- extensions/wikia/ArticleNavigation/ArticleNavigationHelper.class.php
+++ extensions/wikia/ArticleNavigation/ArticleNavigationHelper.class.php
@@ -2,6 +2,38 @@
 
 class ArticleNavigationHelper {
 
+	public function getUserLanguageCode( $request ) {
+		static $language = null;
+
+		if ( $language === null ) {
+			global $wgLang;
+
+			$language  = $request->getVal( 'lang' );
+
+			if ( !$language ) {
+				$language = $wgLang->getCode();
+			}
+		}
+		return $language;
+	}
+
+	public function isValidShareService( $service, $lang ) {
+
+		// filter through include list, default ot true
+		if ( array_key_exists( 'languages:include', $service ) && is_array( $service['languages:include'] ) ) {
+			$allowedInLanguage = in_array( $lang, $service['languages:include']);
+		} else {
+			$allowedInLanguage = true;
+		}
+
+		// filter through exclude list
+		if ( array_key_exists( 'languages:exclude', $service ) && is_array( $service['languages:exclude'] ) ) {
+			$allowedInLanguage = $allowedInLanguage && !in_array( $lang, $service['languages:exclude']);
+		}
+
+		return $allowedInLanguage && array_key_exists( 'url', $service ) && array_key_exists( 'name', $service );
+	}
+
 	public function onOverwriteTOC( &$title, &$toc ) {
 		if ( F::app()->checkSkin( 'venus' ) ) {
 			$toc = '';
