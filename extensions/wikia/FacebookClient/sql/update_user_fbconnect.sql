b97539386897b7bfa3d84db11c201b10ead4a39b
Garth Webb <garth@wikia-inc.com>
2014-11-24 16:08:22 -0800

    UC-138 Add numbering to steps

--- extensions/wikia/FacebookClient/sql/update_user_fbconnect.sql
+++ extensions/wikia/FacebookClient/sql/update_user_fbconnect.sql
@@ -1,8 +1,8 @@
--- Add columns, about 14s in dev (844966 rows each)
+-- 1) Add columns, about 14s in dev (844966 rows each)
 ALTER TABLE user_fbconnect ADD COLUMN user_fb_app_id bigint(20) DEFAULT 0;
 ALTER TABLE user_fbconnect ADD COLUMN user_fb_biz_token varchar(255) DEFAULT '';
 
--- Take care of dup wikia user IDs, about 3s (126 rows)
+-- 2) Take care of dup wikia user IDs, about 3s (126 rows)
 UPDATE
         user_fbconnect AS orig,
         (SELECT user_id, min(time) AS min_time, count(*)
@@ -11,9 +11,9 @@ SET user_fb_app_id = 1
 WHERE orig.user_id = dup.user_id AND
       orig.time = dup.min_time;
 
--- Add constraints, about 7s
+-- 3) Add constraints, about 7s
 ALTER TABLE user_fbconnect ADD UNIQUE KEY (user_id, user_fb_app_id);
 ALTER TABLE user_fbconnect ADD UNIQUE KEY (user_fbid, user_fb_app_id);
 
--- Remove old constraint about 12s (844966 rows)
+-- 4) Remove old constraint about 12s (844966 rows)
 ALTER TABLE user_fbconnect DROP PRIMARY KEY;
