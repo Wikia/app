f7ca4cad17207a7b50acd6c8ca9b99f47c4c6906
Garth Webb <garth@wikia-inc.com>
2014-11-25 12:04:52 -0800

    UC-138 Make the alters faster

--- extensions/wikia/FacebookClient/sql/update_user_fbconnect.sql
+++ extensions/wikia/FacebookClient/sql/update_user_fbconnect.sql
@@ -1,6 +1,7 @@
 -- 1) Add columns, about 14s in dev (844966 rows each)
-ALTER TABLE user_fbconnect ADD COLUMN user_fb_app_id bigint(20) DEFAULT 0;
-ALTER TABLE user_fbconnect ADD COLUMN user_fb_biz_token varchar(255) DEFAULT '';
+ALTER TABLE user_fbconnect
+    ADD COLUMN user_fb_app_id bigint(20) DEFAULT 0,
+    ADD COLUMN user_fb_biz_token varchar(255) DEFAULT '';
 
 -- 2) Take care of dup wikia user IDs, about 3s (126 rows)
 UPDATE
@@ -12,8 +13,9 @@ WHERE orig.user_id = dup.user_id AND
       orig.time = dup.min_time;
 
 -- 3) Add constraints, about 7s
-ALTER TABLE user_fbconnect ADD UNIQUE KEY (user_id, user_fb_app_id);
-ALTER TABLE user_fbconnect ADD UNIQUE KEY (user_fbid, user_fb_app_id);
+ALTER TABLE user_fbconnect
+    ADD UNIQUE KEY (user_id, user_fb_app_id),
+    ADD UNIQUE KEY (user_fbid, user_fb_app_id);
 
 -- 4) Remove old constraint about 12s (844966 rows)
 ALTER TABLE user_fbconnect DROP PRIMARY KEY;
