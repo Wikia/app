0a8672c5da1fb8e7d4339ad8402448d172837b59
adamkarminski <adamkarminski@gmail.com>
2014-11-26 11:43:52 +0100

    CE-1125 Log an action performed on every image

--- extensions/wikia/Scribe/ScribeEventProducer.class.php
+++ extensions/wikia/Scribe/ScribeEventProducer.class.php
@@ -441,18 +441,38 @@ class ScribeEventProducer {
 			2 => MEDIATYPE_DRAWING,
 		];
 
-		if ( $this->mParams['pageNamespace'] == 6
-			&& $this->mParams['isRedirect'] == 0
+		if ( in_array( $this->mParams['pageNamespace'], $this->mediaNS )
 			&& isset( $aAllowedTypes[ $this->mParams['mediaType'] ] )
-			&& $this->mParams['isLocalFile'] == 1
-			&& $this->mParams['isTop200'] == 0
 		) {
-			$bIsImageForReview = true;
-		} else {
-			$bIsImageForReview = false;
-		}
+			if ( $this->mParams['isRedirect'] == 1 ) {
+				$sLogMessage = "The page is a redirect";
+				$bIsImageForReview = false;
+			} elseif ( $this->mParams['isLocalFile'] == 0 ) {
+				$sLogMessage = "The file is from an external repo";
+				$bIsImageForReview = false;
+			} elseif ( $this->mParams['isTop200'] == 1 ) {
+				$sLogMessage = "The was uploaded to one of the Top200 wikias";
+				$bIsImageForReview = false;
+			} else {
+				$sLogMessage = "The image was sent for a review";
+				$bIsImageForReview = true;
+			}
 
-		$this->mParams['isImageForReview'] = intval( $bIsImageForReview );
+			$this->sendImageReviewLog( $sLogMessage );
+			$this->mParams['isImageForReview'] = intval( $bIsImageForReview );
+		}
+	}
+
+	/**
+	 * Sends a unified ImageReviewLog message
+	 * @param  string $sLogMessage  A log message
+	 * @return void
+	 */
+	private function sendImageReviewLog( $sLogMessage ) {
+		\Wikia\Logger\WikiaLogger::instance()->info( "ImageReviewLog: {$sLogMessage}", [
+			'method' => __METHOD__,
+			'params' => $this->mParams,
+		] );
 	}
 
 	public function sendLog() {
