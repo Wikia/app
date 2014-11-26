1e20c19008f7ed58cc266fe70e12290f05574ac4
Armon <armon@wikia-inc.com>
2014-11-25 18:45:06 -0800

    hack up the form for existing user login with fb

--- extensions/wikia/UserLogin/templates/FacebookSignup_modal.php
+++ extensions/wikia/UserLogin/templates/FacebookSignup_modal.php
@@ -65,15 +65,54 @@
 	];
 
 	echo F::app()->renderView( 'WikiaStyleGuideForm', 'index', ['form' => $form] );
+
+	// A temporary hacky dup template, just to make sign in works, follows!
+	$loginForm = [
+		'inputs' => [
+			[
+				'type' => 'text',
+				'name' => 'login_username',
+				'isRequired' => true,
+				'label' => wfMessage( 'yourname' )->escaped(),
+			],
+			[
+				'type' => 'password',
+				'name' => 'login_password',
+				'isRequired' => true,
+				'label' => wfMessage( 'yourpassword' )->escaped(),
+			],
+			[
+				'type' => 'hidden',
+				'name' => 'loginToken',
+				'value' => Sanitizer::encodeAttribute( $loginToken ),
+			],
+			[
+				'type' => 'hidden',
+				'name' => 'returntourl',
+				'value' => Sanitizer::encodeAttribute( $returnToUrl ),
+			],
+			[
+				'type' => 'nirvanaview',
+				'controller' => 'UserSignupSpecial',
+				'view' => 'submit',
+				'class' => 'submit-pane error modal-pane',
+				'params' => [
+					'createAccountButtonLabel' => strtoupper(
+						wfMessage( 'userlogin-login-heading' )->escaped()
+					)
+				],
+			],
+		],
+		'method' => 'post',
+	];
+
 ?>
 		</section>
 
 		<section class="UserLoginFacebookRight">
 			<h1><?= wfMessage( 'usersignup-facebook-have-an-account-heading' )->escaped() ?></h1>
 			<p><?= wfMessage( 'usersignup-facebook-have-an-account' )->escaped() ?></p>
-			<a class="wikia-button" href="<?= htmlspecialchars( $connectUrl )  . '?' . $queryString ?>">
-				<?= wfMessage( 'login' )->escaped() ?>
-			</a>
+			<?= F::app()->renderView( 'WikiaStyleGuideForm', 'index', ['form' => $loginForm] ); ?>
 		</section>
 	</section>
 </div>
