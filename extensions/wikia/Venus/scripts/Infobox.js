cf5d46ed114d1e0ccf7177037a05bf1ceb85c743
Lukasz Konieczny <lukaszk@wikia-inc.com>
2014-11-28 13:59:59 +0000

    CON-2175: Clear left element only if is not floated right

--- extensions/wikia/Venus/scripts/Infobox.js
+++ extensions/wikia/Venus/scripts/Infobox.js
@@ -8,20 +8,11 @@ require(['venus.infobox', 'wikia.document', 'wikia.window'], function(infoboxMod
 	function init() {
 		var infobox = infoboxWrapper.firstChild,
 			articleContent = d.getElementById('mw-content-text'),
-			seeMoreButton,
-			nextElement = infoboxContainer.nextElementSibling,
-			isNextElementFloated;
+			seeMoreButton;
 
 		articleContent.classList.add('clear-none');
-		isNextElementFloated = (window.getComputedStyle(nextElement).getPropertyValue('float') === 'right' ||
-			nextElement.style.float === 'right');
-
-		if (isNextElementFloated) {
-			nextElement.classList.add('clear-both');
-		} else {
-			nextElement.classList.add('clear-left');
-		}
-
+		infoboxContainer.nextElementSibling.classList.add('clear-left');
+		
 		if(infoboxModule.isInfoboxCollapsible(infoboxWrapper)) {
 			infoboxModule.collapseInfobox(infoboxWrapper);
 
