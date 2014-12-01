78fc374bd6526c3db143819759eb408c307ef0e4
Onei <mdowdell244@gmail.com>
2014-12-01 23:55:31 +0000

    update RSHighscores

---
+++ extensions/3rdparty/RSHighscores/RSHighscores.i18n.magic.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for Extension RSHiscores
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$magicWords = array();
+
+/**
+ * English (English)
+ */
+$magicWords['en'] = array(
+	'hs' => array( 0, 'hs' ),
+);
