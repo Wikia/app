cc4a15d20b71cfe327c720283d7b10aa69d2741f
Kamil Koterba <kamil@wikia-inc.com>
2014-12-02 10:47:50 +0100

    CON-2230 Wrap infobox when there are some pixels to wrap - avoid wrapping small amounts of pixels

--- extensions/wikia/Venus/scripts/modules/infobox.module.js
+++ extensions/wikia/Venus/scripts/modules/infobox.module.js
@@ -1,7 +1,7 @@
 define('venus.infobox', ['wikia.document', 'wikia.window'], function(d, w) {
 	'use strict';
 
-	var maxInfoboxHeight = 700,
+	var maxInfoboxHeight = 800,
 		infoboxCollapsedClass = 'collapsed-infobox';
 
 	/**
