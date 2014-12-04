e0b8466d152665372db8e75b8975bb0832fbc313
macbre <maciej.brencz@gmail.com>
2014-12-04 16:59:21 +0100

    ArticleNavigation.i18n: use short arrays notations

--- extensions/wikia/ArticleNavigation/ArticleNavigation.i18n.php
+++ extensions/wikia/ArticleNavigation/ArticleNavigation.i18n.php
@@ -1,24 +1,24 @@
 <?php
 
-$messages = array();
+$messages = [];
 
-$messages['en'] = array(
+$messages['en'] = [
 	'article-navigation-icon-title-toc' => 'Table of Contents',
 	'article-navigation-icon-title-edit' => 'Edit',
 	'article-navigation-icon-title-tools' => 'Tools',
 	'article-navigation-icon-title-share' => 'Share',
-);
+];
 
-$messages['pl'] = array(
+$messages['pl'] = [
 	'article-navigation-icon-title-toc' => 'Spis treści',
 	'article-navigation-icon-title-edit' => 'Edycja',
 	'article-navigation-icon-title-tools' => 'Narzędzia',
 	'article-navigation-icon-title-share' => 'Udostępnij',
-);
+];
 
-$messages['qqq'] = array(
+$messages['qqq'] = [
 	'article-navigation-icon-title-toc' => 'Title for "Table of Contents" icon in article navigation',
 	'article-navigation-icon-title-edit' => 'Title for "Edit" icon in article navigation',
 	'article-navigation-icon-title-tools' => 'Title for "Tools" icon in article navigation',
 	'article-navigation-icon-title-share' => 'Title for "Share" icon in article navigation',
-);
+];
