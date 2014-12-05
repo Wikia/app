a9f871b2b5880a7626bd5ae2e7f0eb19c2f59190
Lukasz Konieczny <lukaszk@wikia-inc.com>
2014-12-05 14:06:51 +0000

    CON-2093: Comment added

--- extensions/wikia/GlobalNavigation/GlobalNavigationHooks.class.php
+++ extensions/wikia/GlobalNavigation/GlobalNavigationHooks.class.php
@@ -5,6 +5,8 @@ class GlobalNavigationHooks {
 	static public function onOutputPageParserOutput( OutputPage &$out, ParserOutput $parseroutput ) {
 		global $wgEnableMWSuggest;
 
+
+		// Global Navigation is always enabled on Venus, so we should disable MW suggest
 		if ( F::app()->checkSkin( 'venus' ) ) {
 			$wgEnableMWSuggest = false;
 		}
