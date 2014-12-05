4466a534d31ddf28b13e471b634090a793ded459
Adam Robak <adamr@wikia-inc.com>
2014-12-05 13:39:11 +0100

    qqq expanded

--- extensions/wikia/NjordPrototype/Njord.i18n.php
+++ extensions/wikia/NjordPrototype/Njord.i18n.php
@@ -18,17 +18,17 @@ $messages['en'] = array(
 
 $messages['qqq'] = array(
 	//template messages
-	'hero-image-drop-here' => 'call to action message displayed as overlay on hero image module during drag action',
-	'hero-image-add-image' => 'call to action message link displayed when hero image not set, after click will open upload image window',
-	'hero-image-update-image' => 'call to action message link displayed when hero image is set, after click will open upload image window',
-	'hero-image-dd-image' => 'information message displayed on hero module about drag & drop function being enabled',
-	'hero-image-position-image' => 'call to action message, displayed on hero module after image upload is completed and reposition is enabled',
-	'hero-image-discard-btn' => 'hero module discard button label',
-	'hero-image-publish-btn' => 'hero module publish button label',
-	'hero-image-default-title' => 'hero module default main title, displayed when title not provided',
-	'hero-image-save-image' => 'message displayed after image upload, together with publish/discard buttons, describes upload image future usage',
-	'hero-image-default-description' => 'hero module default description, displayed when description not provided',
+	'hero-image-drop-here' => 'Call to action message displayed as overlay on hero image module during drag action. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-add-image' => 'Call to action message link displayed when hero image not set, after click will open upload image window. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-update-image' => 'Call to action message link displayed when hero image is set, after click will open upload image window. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-dd-image' => 'Information message displayed on hero image module about drag & drop function being enabled. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-position-image' => 'Call to action message, displayed on hero image module after image upload is completed and reposition is enabled. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-discard-btn' => 'Hero image module discard button label. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-publish-btn' => 'Hero image module publish button label. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-default-title' => 'Hero image module default main title, displayed when title not provided. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-save-image' => 'Message displayed after image upload, together with publish/discard buttons, describes upload image future usage. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-default-description' => 'Hero image module default description, displayed when description not provided. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
 	//js messages
-	'hero-image-error' => 'hero module error modal title',
-	'hero-image-unknown-error' => 'hero module unknown error message displayed in modal',
+	'hero-image-error' => 'Hero image module error modal title. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
+	'hero-image-unknown-error' => 'Hero image module unknown error message displayed in modal. Hero image is a part of main page above the textual content, representing the topic of the wiki.',
 );
