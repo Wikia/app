23f2e8ca1ce32b64425176d70c8d0a2e5d29534c
adamkarminski <adamkarminski@gmail.com>
2014-12-08 14:27:24 +0100

    CE-1162 Typo fix

--- extensions/wikia/WAMPage/WAMPage.i18n.php
+++ extensions/wikia/WAMPage/WAMPage.i18n.php
@@ -23,7 +23,7 @@ $messages['en'] = array(
 	'wampage-title' => 'Top Gainers',
 	'wampage-header-faq' => 'Faq',
 	'wampage-header-wam' => 'WAM Score',
-	'wampage-subheader-wam' => 'is an indicator of a wikia\'s health nad vitality.',
+	'wampage-subheader-wam' => 'is an indicator of a wikia\'s health and vitality.',
 	'wampage-content' => 'The WAM is calculated daily for the top 5,000 Wikia sites and includes an overall and vertical rank as well as a score from 0 to 100. The WAM rankings are sensitive to real-world events and change frequently, so be sure to check back regularly to see the latest trends.',
 	'wampage-content-faq-link' => '[[$1|Frequently Asked Questions]]',
 	'wampage-faq' => '__NOTOC____NOEDITSECTION__===How is the WAM calculated?===
