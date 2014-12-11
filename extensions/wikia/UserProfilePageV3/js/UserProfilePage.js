81517a5a98b6973477b73f3eec4dcbea4c93b685
James Sutterfield <james@wikia-inc.com>
2014-12-10 17:32:30 -0800

    UC-152 remove hacky html entity decoding

--- extensions/wikia/UserProfilePageV3/js/UserProfilePage.js
+++ extensions/wikia/UserProfilePageV3/js/UserProfilePage.js
@@ -286,7 +286,6 @@ var UserProfilePage = {
 			},
 			onComplete: function (response) {
 				try {
-					response = String(response).replace(/&amp;/g, '&');
 					response = JSON.parse(response);
 					var avatarImg = $modal.find('img.avatar');
 					if (response.result.success === true) {
