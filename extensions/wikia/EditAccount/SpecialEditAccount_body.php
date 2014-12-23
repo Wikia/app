4e26a9fdfd17555f58904d345b59fb9bf8625648
lizlux <liz@lizlux.net>
2014-12-23 10:28:49 -0800

    UC-188 removing references to wgEnableFacebookConnectExt

--- extensions/wikia/EditAccount/SpecialEditAccount_body.php
+++ extensions/wikia/EditAccount/SpecialEditAccount_body.php
@@ -368,7 +368,6 @@ class EditAccount extends SpecialPage {
 	 * @return boolean               true on success, false on failure
 	 */
 	public static function closeAccount( $user = '', $changeReason = '', &$mStatusMsg = '', &$mStatusMsg2 = '', $keepEmail = true ) {
-		global $wgEnableFacebookConnectExt;
 		if ( empty( $user ) ) {
 			throw new Exception( 'User object is invalid.' );
 		}
@@ -395,11 +394,11 @@ class EditAccount extends SpecialPage {
 			}
 		}
 
-		# close account and invalidate cache + cluster data
+		// close account and invalidate cache + cluster data
 		Wikia::invalidateUser( $user, true, $keepEmail, true );
-		if ( !empty( $wgEnableFacebookConnectExt ) ) {
-			self::disconnectFBConnect( $user );
-		}
+
+		// if they are connected from facebook, disconnect them
+		self::disconnectFromFacebook( $user );
 
 		if ( $user->getEmail() == '' ) {
 			$title = Title::newFromText( 'EditAccount', NS_SPECIAL );
@@ -421,20 +420,14 @@ class EditAccount extends SpecialPage {
 	}
 
 	/**
-	 * Disconnect Facebook account from Wikia account
+	 * Make sure a wikia user account is disconnected from their facebook account.
 	 *
-	 * @param  User   $user The user account to disconnect
+	 * @param  User $user The user account to disconnect
 	 * @return void
 	 */
-	public static function disconnectFBConnect( User $user ) {
-		global $wgEnableFacebookClientExt;
-		if ( !empty( $wgEnableFacebookClientExt ) ) {
+	public static function disconnectFromFacebook( User $user ) {
+		if ( !empty( F::app()->wg->EnableFacebookClientExt ) ) {
 			FacebookMapModel::deleteFromWikiaID( $user->getId() );
-		} else {
-			$fbIds = FBConnectDB::getFacebookIDs( $user );
-			if ( !empty( $fbIds ) ) {
-				FBConnectDB::removeFacebookID( $user );
-			}
 		}
 	}
 
