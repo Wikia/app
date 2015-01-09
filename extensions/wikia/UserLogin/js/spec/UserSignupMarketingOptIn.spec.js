83f425eab6a661710f9193d8781cc5ffb1963c17
lizlux <liz@lizlux.net>
2015-01-08 17:48:35 -0800

    UC-197 setting up tests for marketing opt in

--- extensions/wikia/UserLogin/js/spec/UserSignupMarketingOptIn.spec.js
+++ extensions/wikia/UserLogin/js/spec/UserSignupMarketingOptIn.spec.js
@@ -1,18 +1,39 @@
 describe('UserLogin Marketing Opt In', function () {
 	'use strict';
 
-	var geo,
-		optIn,
-		wikiaForm;
-
-	beforeEach(function () {
-		geo = modules['wikia.geo']();
-		optIn = modules['usersignup.marketingOptIn'](geo);
-		wikiaForm = {
+	var optIn,
+		geoMock = {
+			getCountryCode: $.noop,
+			getContinentCode: $.noop
+		},
+		wikiaFormMock = {
 			inputs: {
-
+				wpMarketingOptIn: {
+					length: 1
+				}
 			}
-		}
+		};
+
+	it ('should throw an exception', function () {
+		wikiaFormMock.inputs.wpMarketingOptIn.length = 0;
+		optIn = modules['usersignup.marketingOptIn'](geoMock);
+		expect(optIn.init(wikiaFormMock)).toThrow();
+	});
+
+	it ('should check the box in the England', function () {
+
+	});
+
+	it ('should not check the box in Canada', function () {
+
+	});
+
+	it ('should hide the box in the US', function () {
+
+	});
+
+	it ('should not hide the box in England', function () {
+
 	});
 
 
