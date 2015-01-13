624565c4533c6dc472abef5e5105395a0b9c51cf
adamkarminski <adamkarminski@gmail.com>
2015-01-13 19:00:32 +0100

    CE-1195 Add logging of successful and failed autofollow

--- maintenance/wikia/cronjobs/AutoFollow.php
+++ maintenance/wikia/cronjobs/AutoFollow.php
@@ -3,18 +3,13 @@
 require_once( dirname(__FILE__) . '/../../commandLine.inc' );
 
 global $wgCityId;
-
 $contentLanguage = WikiFactory::getVarValueByName( 'wgLanguageCode', $wgCityId );
 
-echo "Starting procedure for a wikia in {$contentLanguage}...";
+$dbr = wfGetDB( DB_SLAVE, array(), $wgExternalSharedDB );
+$range = date( 'Ymd', strtotime( '-30 days' ) ) . '000000';
+$res = $dbr->select( '`user`', 'user_id', array( 'user_registration >= ' . $range ) );
 
 $alreadyWatchedKey = 'autowatched-already';
-
-$dbr = wfGetDB( DB_SLAVE, array(), $wgExternalSharedDB );
-
-$range = date( 'Ymd', strtotime( '-30 days' ) ) . '000000';
-
-$res = $dbr->select( '`user`', 'user_id', array( 'user_registration >= ' . $range ) );
 
 $titlesToWatch = [];
 $textsToWatch = [
@@ -32,44 +27,33 @@ $textsToWatch = [
 	'es' => ['Blog:Comunidad'],
 ];
 
-$reason = '';
-
 while ( $row = $dbr->fetchRow( $res ) ) {
-	if ( !empty( $reason ) ) {
-		echo "User not added to watchlist: {$reason}\n";
-	}
 
 	$user = User::newFromId( $row['user_id'] );
 
 	if ( !is_object( $user ) ) {
-		$reason = "Cannot create an object from ID {$row['user_id']}\n";
 		continue;
 	}
 
 	if ( $user->getOption( $alreadyWatchedKey ) == 1 ) {
-		$reason = "User {$row['user_id']} already on watchlist\n";
 		continue;
 	}
 
 	// Include dialects like pt-br
 	$userLanguage = substr( $user->getOption( 'language' ), 0, 2 );
 	if ( $userLanguage != $contentLanguage ) {
-		$reason = "There is no blog in {$userLanguage} for user {$row['user_id']}\n";
 		continue;
 	}
 
 	if ( $user->getOption( 'marketingallowed' ) != 1 ) {
-		$reason = "User {$row['user_id']} does not allow marketing\n";
 		continue;
 	}
 
 	if ( !$user->isEmailConfirmed() ) {
-		$reason = "User {$row['user_id']} hasn't confirmed his email\n";
 		continue;
 	}
 
 	if ( $user->getEditCount() == 0 ) {
-		$reason = "User {$row['user_id']} has 0 edits\n";
 		continue;
 	}
 
@@ -78,13 +62,20 @@ while ( $row = $dbr->fetchRow( $res ) ) {
 	}
 
 	foreach ( $titlesToWatch as $title ) {
+		$logParams = [
+			'user_id' => $row['user_id'],
+			'user_name' => $user->getName(),
+			'user_lang' => $userLanguage,
+			'title' => $title->getText(),
+		];
 		if ( $title instanceof Title ) {
-			echo "Watching {$title->getText()} in {$contentLanguage} as {$user->getName()}...\n";
 			WatchAction::doWatch( $title, $user );
+			\Wikia\Logger\WikiaLogger::instance()->info( "AutoFollow: User {$user->getName()} added to watchlist of {$title->getText()}.", $logParams );
+		} else {
+			// Log error to check for typos etc. Can be deleted when tested in production enviroment.
+			\Wikia\Logger\WikiaLogger::instance()->error( "AutoFollow: Invalid article name in {$userLanguage}", $logParams );
 		}
 	}
-
-	$reason = '';
 
 	$user->setOption( $alreadyWatchedKey, 1 );
 	$user->saveSettings();
