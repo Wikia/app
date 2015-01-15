df04a4eaf313a36924f6700755ae9b10f9c5d5a6
Armon <armon@wikia-inc.com>
2015-01-15 12:40:01 -0800

    UC-148 Call User method for updating email

--- includes/wikia/tasks/Tasks/FacebookTask.class.php
+++ includes/wikia/tasks/Tasks/FacebookTask.class.php
@@ -36,29 +36,29 @@ class FacebookTask extends BaseTask {
 			return false;
 		}
 
-		$status = ( bool ) $this->updateUserEmail( $userMap->getWikiaUserId(), $email );
+		$this->updateUserEmail( $userMap->getWikiaUserId(), $email );
 		$this->info( 'Facebook user email update complete', [
 			'title' => __METHOD__,
 			'userid' => $userId,
 			'facebookid' => $fbUserId,
 			'email' => $email,
-			'status' => ( $status ? 'success' : 'fail' ),
 		] );
 
-		return $status;
+		return true;
 	}
 
+	/**
+	 * Update user email to the one given
+	 *
+	 * @param int $userId
+	 * @param string $email
+	 */
 	protected function updateUserEmail( $userId, $email ) {
-		$app = \F::app();
-
-		$dbw = wfGetDB( DB_MASTER, null, $app->wg->ExternalSharedDB );
-		( new \WikiaSQL() )
-			->UPDATE( 'user' )
-			->SET( 'user_email', $email )
-			->WHERE( 'user_id' )->EQUAL_TO( $userId )
-			->run( $dbw );
-
-		return $dbw->affectedRows();
+		/** @var \User $user */
+		$user = \User::newFromId( $userId );
+		$user->setEmail( $email );
+		$user->confirmEmail();
+		$user->saveSettings();
 	}
 
 }
