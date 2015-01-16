166d909dbe1fe8eadf2c1685b4ea0882b6fb6dd3
Damian Jozwiak <damian@wikia-inc.com>
2015-01-16 10:12:25 +0100

    CON-2384 | Vertical in footer - brackets removed

--- extensions/wikia/GlobalFooter/templates/GlobalFooter_index.php
+++ extensions/wikia/GlobalFooter/templates/GlobalFooter_index.php
@@ -4,7 +4,7 @@
 			<a class="wikia-logo" href="<?=Sanitizer::encodeAttribute( $logoLink ); ?>">
 				<img src="<?= $wg->BlankImgUrl; ?>">
 				<?php if( !$isCorporate && !empty( $verticalShort ) ): ?>
-					<span>[ <?=$verticalNameMessage->escaped(); ?> ]</span>
+					<span><?=$verticalNameMessage->escaped(); ?></span>
 				<?php endif; ?>
 			</a>
 		</div>
