fb2fdf54793cebb56bdb3ca39fb11dec36f1bf54
James Sutterfield <james@wikia-inc.com>
2015-01-19 12:02:01 -0800

    UC-153 remove deprecated profiling calls

--- extensions/wikia/GlobalWatchlist/GlobalWatchlist.hooks.php
+++ extensions/wikia/GlobalWatchlist/GlobalWatchlist.hooks.php
@@ -7,8 +7,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 
 class GlobalWatchlistHook {
 	
-	public static function getPreferences( /*User*/ $user, /*Array*/ &$defaultPreferences ) {
-		wfProfileIn( __METHOD__ );		
+	public static function getPreferences( &$defaultPreferences ) {
 
 		$defaultPreferences['watchlistdigest'] = array(
 			'type' => 'toggle',
@@ -22,8 +21,6 @@ class GlobalWatchlistHook {
 			'section' => 'watchlist/advancedwatchlist',
 		);
 		
-		wfProfileOut( __METHOD__ );		
-
 		return true;
 	}
 	
@@ -32,35 +29,28 @@ class GlobalWatchlistHook {
 	 * @param $oWatchItem WatchedItem: object
 	 * @return bool (always true)
 	 */
-	static public function addGlobalWatch ( /*WatchedItem*/ $oWatchItem ) {
+	static public function addGlobalWatch ( $oWatchItem ) {
 		global $wgEnableScribeReport, $wgCityId;
 
-		wfProfileIn( __METHOD__ );		
-
 		if ( empty($wgEnableScribeReport) ) {
-			wfProfileOut( __METHOD__ );		
 			return true;
 		}
 		
 		if ( !$oWatchItem instanceof WatchedItem ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
 		
 		if ( $oWatchItem->id == 0 ) {
-			wfProfileOut( __METHOD__ );
 			return true;			
 		}
 		
 		$oTitle = Title::makeTitle( $oWatchItem->ns, $oWatchItem->ti );
 		if ( !is_object( $oTitle ) ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
 		
 		$oRevision = Revision::newFromTitle( $oTitle );
 		if ( !is_object( $oRevision ) ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
 				
@@ -88,7 +78,6 @@ class GlobalWatchlistHook {
 			}
 		}
 
-		wfProfileOut( __METHOD__ );
 		return true;
 	}
 	
@@ -98,40 +87,34 @@ class GlobalWatchlistHook {
 	 * @param $success Boolean: removed successfully
 	 * @return bool (always true)
 	 */		
-	static public function removeGlobalWatch( /*WatchedItem*/ $oWatchItem, /*boolean*/ $success ) {
+	static public function removeGlobalWatch( $oWatchItem, $success ) {
 		global $wgEnableScribeReport, $wgCityId;
 
-		wfProfileIn( __METHOD__ );		
-
 		if ( empty($wgEnableScribeReport) ) {
-			wfProfileOut( __METHOD__ );		
 			return true;
 		}
-		
+
 		if ( !$oWatchItem instanceof WatchedItem ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
-		
+
 		if ( !$success ) {
 			/* some errors when update in local watchlist table */
-			wfProfileOut( __METHOD__ );
-			return true;			
+			return true;
 		}
-		
+
 		if ( $oWatchItem->id == 0 ) {
-			wfProfileOut( __METHOD__ );
-			return true;				
+			return true;
 		}
-		
+
 		foreach ( array ( MWNamespace::getSubject( $oWatchItem->ns ), MWNamespace::getTalk( $oWatchItem->ns ) ) as $ns ) {
 			$params = array (
 				'wl_user' => $oWatchItem->id,
 				'wl_namespace' => $ns,
 				'wl_title' => $oWatchItem->ti,
-				'wl_wikia' => $wgCityId				
+				'wl_wikia' => $wgCityId
 			);
-			
+
 			try {
 				$message = array(
 					'method' => 'removeWatch',
@@ -144,63 +127,51 @@ class GlobalWatchlistHook {
 				Wikia::log( __METHOD__, 'scribeClient exception', $e->getMessage() );
 			}
 		}
-		
-		wfProfileOut( __METHOD__ );
+
 		return true;
 	}
-	
+
 	/**
 	 * Hook function calls when watch was updated in database
 	 * @param $oWatchItem WatchedItem: object
 	 * @param $user Array or Integer: array of user IDs or user ID
 	 * @param $timestamp Datetime or null
 	 * @return bool (always true)
-	 */	
-	static public function updateGlobalWatch( /*WatchedItem*/ $oWatchItem, /*Mixed*/ $user, /*Mixed*/ $timestamp ) {
+	 */
+	static public function updateGlobalWatch( $oWatchItem, $user, $timestamp ) {
 		global $wgEnableScribeReport, $wgCityId;
 
-		wfProfileIn( __METHOD__ );		
-
 		if ( empty($wgEnableScribeReport) ) {
-			wfProfileOut( __METHOD__ );		
 			return true;
 		}
-		
+
 		if ( !$oWatchItem instanceof WatchedItem ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
-		
+
 		if ( empty($user) ) {
-			wfProfileOut( __METHOD__ );
-			return true;			
+			return true;
 		}
 
 		$oTitle = Title::makeTitle( $oWatchItem->ns, $oWatchItem->ti );
 		if ( !is_object( $oTitle ) ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
-		
+
 		$oRevision = Revision::newFromTitle( $oTitle );
 		if ( !is_object( $oRevision ) ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
-					
+
 		if ( !is_array($user) ) {
 			$user = array( $user );
 		}
-		
+
 		$rev_id = $oRevision->getId();
 		$rev_timestamp = $oRevision->getTimestamp();
-						
+
 		foreach ( $user as $user_id ) {
-			
-			if ( empty( $user_id ) ) {
-				continue;
-			}
-			
+
 			$params = array (
 				'update' => array (
 					'wl_notificationtimestamp' => $timestamp,
@@ -214,7 +185,7 @@ class GlobalWatchlistHook {
 				),
 				'wl_wikia' => $wgCityId
 			);
-			
+
 			try {
 				$message = array(
 					'method' => 'updateWatch',
@@ -227,11 +198,10 @@ class GlobalWatchlistHook {
 				Wikia::log( __METHOD__, 'scribeClient exception', $e->getMessage() );
 			}
 		}
-		
-		wfProfileOut( __METHOD__ );
+
 		return true;
 	}
-	
+
 	/**
 	 * Hook function to replace watch records in database
 	 * @param $oldTitle Title
@@ -243,60 +213,54 @@ class GlobalWatchlistHook {
 	 *   'wl_title' => string
 	 * );
 	 * @return bool (always true)
-	 */	
-	static public function replaceGlobalWatch( /*Title*/ $oldTitle, /*Title*/ $newTitle, /*Array*/ $rows ) {
+	 */
+	static public function replaceGlobalWatch( $oldTitle, $newTitle, $rows ) {
 		global $wgEnableScribeReport, $wgCityId;
 
-		wfProfileIn( __METHOD__ );		
-
 		if ( empty($wgEnableScribeReport) ) {
-			wfProfileOut( __METHOD__ );		
 			return true;
 		}
-		
+
 		if ( !$oldTitle instanceof Title ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
-		
+
 		if ( !$newTitle instanceof Title ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
-		
+
 		if ( !is_array($rows) ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
-		
+
 		foreach ( $rows as $row ) {
 			if ( empty($row['wl_user']) ) {
 				continue;
 			}
-			
+
 			$oTitle = Title::makeTitle( $row['wl_namespace'], $row['wl_title'] );
 			if ( !is_object( $oTitle ) ) {
 				continue;
 			}
-			
+
 			$oRevision = Revision::newFromTitle( $oTitle );
 			if ( !is_object( $oRevision ) ) {
 				continue;
 			}
-			
+
 			$row['wl_revision'] = $oRevision->getId();
-			$row['wl_rev_timestamp'] = $oRevision->getTimestamp();			
-			
+			$row['wl_rev_timestamp'] = $oRevision->getTimestamp();
+
 			$params = array (
 				'update' => $row,
 				'where' => array(
 					'wl_title' => $oldTitle->getDBkey(),
 					'wl_namespace' => $oldTitle->getNamespace(),
-					'wl_user' => $row['wl_user']		
+					'wl_user' => $row['wl_user']
 				),
 				'wl_wikia' => $wgCityId
 			);
-			
+
 			try {
 				$message = array(
 					'method' => 'updateWatch',
@@ -309,42 +273,35 @@ class GlobalWatchlistHook {
 				Wikia::log( __METHOD__, 'scribeClient exception', $e->getMessage() );
 			}
 		}
-		
-		wfProfileOut( __METHOD__ );
-				
+
 		return true;
 	}
-	
+
 	/**
 	 * Hook function to delete all watches for User
 	 * @param $oUser User: object
 	 * @return bool (always true)
-	 */		
-	static public function clearGlobalWatch( /*User*/ $oUser) {
+	 */
+	static public function clearGlobalWatch( $oUser) {
 		global $wgEnableScribeReport, $wgCityId;
 
-		wfProfileIn( __METHOD__ );		
-
 		if ( empty($wgEnableScribeReport) ) {
-			wfProfileOut( __METHOD__ );		
 			return true;
 		}
-		
+
 		if ( !$oUser instanceof User ) {
-			wfProfileOut( __METHOD__ );
 			return true;
 		}
 
 		$user_id = $oUser->getId();
-		
+
 		if ( empty( $user_id ) ) {
-			wfProfileOut( __METHOD__ );
-			return true;			
+			return true;
 		}
 
-		$params = array( 
+		$params = array(
 			'wl_user' => $user_id,
-			'wl_wikia' => $wgCityId			
+			'wl_wikia' => $wgCityId
 		);
 
 		try {
@@ -358,27 +315,21 @@ class GlobalWatchlistHook {
 		catch( Exception $e ) {
 			Wikia::log( __METHOD__, 'scribeClient exception', $e->getMessage() );
 		}
-		
-		wfProfileOut( __METHOD__ );
-				
+
 		return true;
 	}
-	
+
 
 	/**
 	 * Hook function to reset all watches for User
 	 * @param $user_id Integer: User ID
 	 * @return bool (always true)
-	 */		
-	static public function resetGlobalWatch( /*Integer*/ $user_id ) {
-		global $wgEnableScribeReport, $wgCityId;
-
-		wfProfileIn( __METHOD__ );		
+	 */
+	static public function resetGlobalWatch( $user_id ) {
 
 		$oUser = User::newFromId( $user_id );
 		$result = self::clearGlobalWatch( $oUser );
 
-		wfProfileOut(__METHOD__);
 
 		return $result;
 	}	
