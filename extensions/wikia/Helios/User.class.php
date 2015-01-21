b858c38f8d695a176fe5f74ffcd7c25fbfe4cf1c
Michał 'Mix' Roszka <mix@wikia-inc.com>
2015-01-21 12:35:44 +0000

    implement the hook method

--- extensions/wikia/Helios/User.class.php
+++ extensions/wikia/Helios/User.class.php
@@ -49,14 +49,32 @@ class User {
 	 * Sets &$bResult to true and returns false if the authentication succeeds.
 	 *
 	 * @param string &$sHash string of the password hash (from the database)
-	 * @param string &$password string of the plaintext password the user entered
-	 * @param integer &$userId integer of the user's ID or Boolean false if the user ID was not supplied
-	 * @param boolean &$result on false returned, this value will be checked to determine if the password was valid
+	 * @param string &$sPassword string of the plaintext password the user entered
+	 * @param integer &$iUserId integer of the user's ID or Boolean false if the user ID was not supplied
+	 * @param boolean &$bResult on false returned, this value will be checked to determine if the password was valid
 	 *
 	 * @return boolean false if the authentication has been done, true otherwise (yeah, I know...)
 	 */
 	public static function comparePasswords( &$sHash, &$sPassword, &$iUserId, &$bResult )
 	{
+		// Get the user's name from the request context.
+		$sUserName= \RequestContext->getMain()->getRequest()->getText( 'wpName' );
+
+		global $wgHeliosBaseUri, $wgHeliosClientId, $wgHeliosClientSecret;
+		$oHelios = new Client( $wgHeliosBaseUri, $wgHeliosClientId, $wgHeliosClientSecret );
+
+		// Authenticate with username and password.
+		try {
+			$oLogin = $oHelios->login( $sUserName, $sPassword );
+			$bResult = !empty( $oLogin->access_token );
+		}
+
+		catch ( \Wikia\Helios\ClientException $e ) {
+			\Wikia\Logger\WikiaLogger::instance()->error( __METHOD__, [ 'exception' => $e ] );
+			return true;
+		}
+
+		return false;
 	}
 
 }
