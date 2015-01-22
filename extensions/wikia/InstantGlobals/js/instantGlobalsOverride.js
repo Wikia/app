f5421965907abd65b1589304d524ff2066061412
bkoval <brt.kowal@gmail.com>
2015-01-22 13:26:11 +0100

    Explanation comment and changing whitespacing

--- extensions/wikia/InstantGlobals/js/instantGlobalsOverride.js
+++ extensions/wikia/InstantGlobals/js/instantGlobalsOverride.js
@@ -6,6 +6,7 @@
 	'use strict';
 	window.location.search.replace(
 		/InstantGlobals\.(\w+)=(\d)/g,
+		//Applying all the InstantGlobals.*=[0|1] params
 		function ($0, $1, $2) {
 			Wikia.InstantGlobals[$1] = $2 > 0;
 		}
