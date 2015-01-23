07e078039429b9b3dfe87052ea3bfd8f6e0557f7
bkoval <brt.kowal@gmail.com>
2015-01-23 14:04:58 +0100

    Trimming js file

--- extensions/wikia/InstantGlobals/js/instantGlobalsOverride.js
+++ extensions/wikia/InstantGlobals/js/instantGlobalsOverride.js
@@ -1,14 +1,11 @@
 /**
- * Module for overriding boolean InstantGlobals
+ * Method for overriding boolean InstantGlobals
  * based on querystring params
  */
-(function () {
-	'use strict';
-	window.location.search.replace(
-		/InstantGlobals\.(\w+)=(\d)/g,
-		//Applying all the InstantGlobals.*=[0|1] params
-		function ($0, $1, $2) {
-			Wikia.InstantGlobals[$1] = $2 > 0;
-		}
-	);
-})();
+location.search.replace(
+	/InstantGlobals\.(\w+)=(\d)/g,
+	//Applying all the InstantGlobals.*=[0|1] params
+	function (a, b, c) {
+		Wikia.InstantGlobals[b] = c > 0;
+	}
+);
