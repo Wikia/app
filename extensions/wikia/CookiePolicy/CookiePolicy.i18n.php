323cd0c673fc0f502f4bda01c751fd39046c466d
lizlux <liz@lizlux.net>
2015-01-29 14:34:29 -0800

    SOC-198 fixing url hash for privacy policy link

--- extensions/wikia/CookiePolicy/CookiePolicy.i18n.php
+++ extensions/wikia/CookiePolicy/CookiePolicy.i18n.php
@@ -4,7 +4,7 @@ $messages = [];
 
 $messages['en'] = [
 	'cookie-policy-description' => "In the EU, there is a law that users be notified of a web site's cookie usage upon arriving there for the first time. This extension allows Wikia to be in compliance with this law.",
-	'cookie-policy-notification-message' => 'Wikia uses cookies to give you the best experience on our websites. By continuing to use Wikia, we assume that you accept our use of cookies. [[homepage:Privacy_Policy| Read more about cookies]]',
+	'cookie-policy-notification-message' => 'Wikia uses cookies to give you the best experience on our websites. By continuing to use Wikia, we assume that you accept our use of cookies. [[homepage:Privacy_Policy#Cookies| Read more about cookies]]',
 ];
 
 $messages['qqq'] = [
