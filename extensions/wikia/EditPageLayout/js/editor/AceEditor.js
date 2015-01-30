9a98a3c85488c585bcd7ea88054896ce4a119fc0
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-01-30 16:04:51 +0000

    CE-syntax-highlighting: Add Ace editor for edit page AMD module

---
+++ extensions/wikia/EditPageLayout/js/editor/AceEditor.js
@@ -0,0 +1,126 @@
+define('wikia.editpage.ace.editor', ['wikia.ace.editor'], function(ace){
+	'use strict';
+
+	var inputAttr = {
+		name: 'wpTextbox1',
+		id: 'wpTextbox1'
+	},
+	beforeInit = function(){
+		$('.loading-indicator').remove();
+		$( '#wpSave' ).removeAttr( 'disabled' );
+	};
+
+	function init() {
+		initConfig();
+		beforeInit();
+		ace.init( 'editarea', inputAttr );
+		initOptions();
+		ace.setTheme( 'solarized_light');
+		ace.setMode( window.aceMode );
+
+		initSubmit();
+		initDiffModal();
+	}
+
+	function initConfig() {
+		var config = {
+				workerPath: window.aceScriptsPath
+			};
+
+		ace.setConfig( config );
+	}
+
+	function initOptions() {
+		var options = {
+			showPrintMargin: false
+		};
+
+		ace.setOptions( options );
+	}
+
+	function initSubmit() {
+		$( '#editform' ).submit(function(e) {
+			var $form = $( this ),
+				hiddenInput = ace.getInput().val( ace.getContent() );
+
+			$form.append( hiddenInput );
+			$form.submit();
+
+			e.preventDefault();
+		});
+	}
+
+	function initDiffModal() {
+		var previewModalConfig = {
+				vars: {
+					id: 'EditPageDialog',
+					title: $.msg( 'editpagelayout-pageControls-changes' ),
+					content: '<div class="ArticlePreview modalContent"><div class="ArticlePreviewInner">' +
+						'</div></div>',
+					size: 'large'
+				}
+			},
+			modalCallback = function(previewModal) {
+				previewModal.deactivate();
+
+				previewModal.$content.bind( 'click', function( event ) {
+					var target = $( event.target );
+					target.closest( 'a' ).not( '[href^="#"]' ).attr( 'target', '_blank' );
+				});
+
+				prepareDiffContent(previewModal, ace.getContent());
+
+				previewModal.show();
+			};
+
+		$('#wpDiff').click(function(){
+			ace.showDiff(previewModalConfig, $.proxy(modalCallback, self));
+		});
+	}
+
+	// send AJAX request
+	function ajax( method, params, callback, skin ) {
+		var url = window.wgEditPageHandler.replace( '$1', encodeURIComponent( window.wgEditedTitle ) );
+
+		params = $.extend({
+			page: window.wgEditPageClass ? window.wgEditPageClass : "",
+			method: method,
+			mode: 'ace'
+		}, params );
+
+		if ( skin ) {
+			url += '&type=full&skin=' + encodeURIComponent( skin );
+		}
+
+		return jQuery.post( url, params, function ( data ) {
+			if ( typeof callback === 'function' ) {
+				callback( data );
+			}
+		}, 'json' );
+	}
+
+	function prepareDiffContent(previewModal, content) {
+		var section = $.getUrlVar( 'section' ) || 0,
+			extraData = {
+				content: content,
+				section: parseInt( section, 10 )
+			};
+
+		$.when(
+			// get wikitext diff
+			ajax( 'diff' , extraData ),
+
+			// load CSS for diff
+			window.mw.loader.use( 'mediawiki.action.history.diff' )
+		).done(function( ajaxData ) {
+			var data = ajaxData[ 0 ],
+				html = '<h1 class="pagetitle">' + window.wgEditedTitle + '</h1>' + data.html;
+			previewModal.$content.find( '.ArticlePreview .ArticlePreviewInner' ).html( html );
+			previewModal.activate();
+		});
+	}
+
+	return {
+		init: init
+	};
+});
