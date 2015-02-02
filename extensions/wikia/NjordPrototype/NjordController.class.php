01a0e8d9d875306491835f7095e73a4f47c74cbe
Rafal Leszczynski <rafal@wikia-inc.com>
2015-02-02 18:01:18 +0100

    jQuery UI test

--- extensions/wikia/NjordPrototype/NjordController.class.php
+++ extensions/wikia/NjordPrototype/NjordController.class.php
@@ -58,7 +58,9 @@ class NjordController extends WikiaController {
 		$this->wg->SupressPageTitle = true;
 
 		$this->wg->out->addStyle( AssetsManager::getInstance()->getSassCommonURL( 'extensions/wikia/NjordPrototype/css/Njord.scss' ) );
-		$this->wg->Out->addScriptFile( $this->wg->ExtensionsPath . '/wikia/NjordPrototype/scripts/jquery-ui-1.10.4.js' );
+		$this->wg->Out->addScriptFile( $this->wg->ExtensionsPath . '/wikia/NjordPrototype/scripts/jquery-ui-1.10.4
+.custom.js' );
+		$this->wg->Out->addScriptFile( $this->wg->ExtensionsPath . '/wikia/NjordPrototype/scripts/jquery.caret.js' );
 		$this->wg->Out->addScriptFile( $this->wg->ExtensionsPath . '/wikia/NjordPrototype/scripts/jquery.caret.js' );
 		$this->wg->Out->addScriptFile( $this->wg->ExtensionsPath . '/wikia/NjordPrototype/scripts/Njord.js' );
 //		$this->wg->out->addStyle( AssetsManager::getInstance()->getSassCommonURL( 'extensions/wikia/NjordPrototype/css/Mom.scss' ) );
