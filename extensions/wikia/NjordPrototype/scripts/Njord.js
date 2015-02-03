8ea7616a204b431b511cccfb9c3cd665b859cde2
Rafal Leszczynski <rafal@wikia-inc.com>
2015-02-03 14:36:55 +0100

    fix wordmark position

--- extensions/wikia/NjordPrototype/scripts/Njord.js
+++ extensions/wikia/NjordPrototype/scripts/Njord.js
@@ -321,6 +321,7 @@
 		onAfterSendForm = function (data) {
 			if (data.isOk) {
 				$heroModuleImage.bind('load', function () {
+					$wordmark.css('top', '-174px');
 					States.setState($imageElement, 'upload-state');
 					States.setState($imageSaveElement, 'upload-state');
 					$heroModule.trigger('enableDragging');
@@ -340,7 +341,6 @@
 		},
 		sendForm = function (formdata) {
 			$heroModule.startThrobbing();
-			$wordmark.css('top', '-174px');
 			$.nirvana.sendRequest({
 				controller: 'NjordController',
 				method: 'upload',
