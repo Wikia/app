3053a9a2954441d275a80e9cbe2b785674a167f8
Rafal Leszczynski <rafal@wikia-inc.com>
2015-02-03 17:47:42 +0100

    testing global notifications

--- extensions/wikia/NjordPrototype/scripts/Njord.js
+++ extensions/wikia/NjordPrototype/scripts/Njord.js
@@ -335,7 +335,7 @@
 
 			} else {
 				trackMom(imageLoadedFailLabel, trackerActionError);
-				window.GlobalNotification.show(data.errMessage, 'error', $('.WikiaPageContentWrapper'));
+				window.GlobalNotification.show(data.errMessage, 'error');
 				$heroModule.stopThrobbing();
 			}
 		},
