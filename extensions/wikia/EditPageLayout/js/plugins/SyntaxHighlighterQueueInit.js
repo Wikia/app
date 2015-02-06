2b3b69d1b5fb5a58af324e5b2622f2d6aafb6c58
Kamil Koterba <kamil@wikia-inc.com>
2015-02-06 10:45:27 +0100

    CE-1245 Coding conventions minor adjustments

--- extensions/wikia/EditPageLayout/js/plugins/SyntaxHighlighterQueueInit.js
+++ extensions/wikia/EditPageLayout/js/plugins/SyntaxHighlighterQueueInit.js
@@ -5,7 +5,7 @@
  */
 
 (function(window, $) {
-	"use strict";
+	'use strict';
 
 	var WE = window.WikiaEditor = window.WikiaEditor || (new Observable());
 
@@ -14,7 +14,7 @@
 		init: function () {
 
 			this.editor.on('editorReady', function () {
-				if (this.mode == 'source') {
+				if (this.mode === 'source') {
 					var textarea = this.getEditbox()[0];
 					require(['WikiTextSyntaxHighlighter'], function (WikiTextSyntaxHighlighter) {
 						WikiTextSyntaxHighlighter.init(textarea);
