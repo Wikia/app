87fa81b28ce479595b2e8f916ad0dacf6902b8e5
lizlux <liz@lizlux.net>
2015-02-09 11:48:38 -0800

    SOC-338 coding conventions for new bind polyfill

--- resources/wikia/polyfills/bind.js
+++ resources/wikia/polyfills/bind.js
@@ -3,29 +3,27 @@
  * Should only need to use this for unit tests, as all Wikia's supported browsers support bind.
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Polyfill
  */
-if (!Function.prototype.bind) {
-	Function.prototype.bind = function(oThis) {
-		'use strict';
+Function.prototype.bind = Function.prototype.bind || function (oThis) {
+	'use strict';
 
-		if (typeof this !== 'function') {
-			// closest thing possible to the ECMAScript 5
-			// internal IsCallable function
-			throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-		}
+	if (typeof this !== 'function') {
+		// closest thing possible to the ECMAScript 5
+		// internal IsCallable function
+		throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+	}
 
-		var aArgs   = Array.prototype.slice.call(arguments, 1),
-			fToBind = this,
-			fNOP    = function() {},
-			fBound  = function() {
-				return fToBind.apply(this instanceof fNOP && oThis
-						? this
-						: oThis,
-					aArgs.concat(Array.prototype.slice.call(arguments)));
-			};
+	var aArgs = Array.prototype.slice.call(arguments, 1),
+		self = this,
+		NoOp = function () {},
+		fBound = function () {
+			return self.apply(
+				this instanceof NoOp && oThis ? this : oThis,
+				aArgs.concat(Array.prototype.slice.call(arguments))
+			);
+		};
 
-		fNOP.prototype = this.prototype;
-		fBound.prototype = new fNOP();
+	NoOp.prototype = this.prototype;
+	fBound.prototype = new NoOp();
 
-		return fBound;
-	};
-}
+	return fBound;
+};
