6698451d245de620252a8fe3d57fedb6feeea2a2
Rafal Leszczynski <rafal@wikia-inc.com>
2015-02-10 16:11:53 +0100

    add placeholders for empty fields

--- extensions/wikia/NjordPrototype/templates/Njord_Summary.php
+++ extensions/wikia/NjordPrototype/templates/Njord_Summary.php
@@ -6,7 +6,7 @@
 <header class="MainPageHeroHeader no-edit-state">
 	<h1 class="title-wrap sg-title <?php if ( isset( $wikiData->title ) ) : ?>filled-state<? else : ?>zero-state<?php endif; ?>">
 		<div class="edit-box">
-			<div class="hero-title" contenteditable="true"><?= htmlspecialchars( $wikiData->title ) ?></div>
+			<div class="hero-title" placeholder="<?= wfMessage('hero-image-default-title')->escaped(); ?>" contenteditable="true"><?= htmlspecialchars( $wikiData->title ) ?></div>
 		</div>
 		<span class="title-text"><?= htmlspecialchars( $wikiData->title ) ?></span>
 		<span class="title-default-text"><?= wfMessage('hero-image-default-title')->escaped(); ?></span>
@@ -16,7 +16,9 @@
 	</h1>
 	<div class="hero-description <?php if ( isset( $wikiData->description ) ) : ?>filled-state<? else : ?>zero-state<?php endif; ?>">
 		<div class="edit-box">
-			<span class="edited-text sg-main" contenteditable="true"><?= htmlspecialchars( $wikiData->description ) ?></span>
+			<span class="edited-text sg-main" placeholder="<?= wfMessage('hero-image-default-description')->escaped();
+			?>" contenteditable="true"><?= htmlspecialchars(
+					$wikiData->description ) ?></span>
 			<div class="btn-bar">
 				<button class="new-btn discard-btn sg-sub"><?= wfMessage('hero-image-discard-btn')->escaped(); ?></button>
 				<button class="new-btn save-btn sg-sub"><?= wfMessage('hero-image-publish-btn')->escaped(); ?></button>
