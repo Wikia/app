3daf0c622226946c7f9c6617a378a78718ebaa38
grunny <mwgrunny@gmail.com>
2015-02-12 03:38:56 +1000

    Some more escaping for good measure

--- extensions/wikia/Forum/templates/Forum_forumRelatedThreads.php
+++ extensions/wikia/Forum/templates/Forum_forumRelatedThreads.php
@@ -6,13 +6,13 @@
 			<li>
 				<?= AvatarService::renderAvatar($message['message']->getUser()->getName(), 20); ?>
 				<em>
-					<a href="<?= $message['message']->getMessagePageUrl(); ?>" title="<?= Sanitizer::encodeAttribute( $message['message']->getMetaTitle() ); ?>"><?= htmlspecialchars( $message['message']->getMetaTitle() ); ?></a>
+					<a href="<?= Sanitizer::encodeAttribute( $message['message']->getMessagePageUrl() ); ?>" title="<?= Sanitizer::encodeAttribute( $message['message']->getMetaTitle() ); ?>"><?= htmlspecialchars( $message['message']->getMetaTitle() ); ?></a>
 				</em>
 				<div class="edited-by">
 
 					<?php $messageLast = empty($message['reply']) ? $message['message'] : $message['reply'];  ?>
 
-					<?php $user = '<a href="' . $messageLast->getUser()->getUserPage()->getFullUrl() . '">' . htmlspecialchars( $messageLast->getUser()->getName() ) . '</a>'; ?>
+					<?php $user = '<a href="' . Sanitizer::encodeAttribute( $messageLast->getUser()->getUserPage()->getFullUrl() ) . '">' . htmlspecialchars( $messageLast->getUser()->getName() ) . '</a>'; ?>
 					<?php $time = '<span class="timeago abstimeago" title="' . $messageLast->getCreateTime() . '" alt="' . $messageLast->getCreateTime(TS_MW) . '">&nbsp;</span>' ?>
 					<?= wfMessage( !empty($message['reply']) ? 'forum-activity-module-posted' : 'forum-activity-module-started' )->rawParams( $user, $time )->escaped(); ?>
 				</div>
