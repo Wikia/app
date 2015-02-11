d94336c454d0d4a6ebbe00d15a70be5cd63f819f
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-02-11 13:00:39 +0000

    CE-1255: Wikitext syntax highlighting for dark themes and some cleanup

--- extensions/wikia/EditPageLayout/js/plugins/WikitextSyntaxHighlighterQueueInit.js
+++ extensions/wikia/EditPageLayout/js/plugins/WikitextSyntaxHighlighterQueueInit.js
@@ -12,15 +12,66 @@ require(['wikia.window', 'jquery'], function(window, $) {
 	WE.plugins.syntaxhighlighterqueueinit = $.createClass(WE.plugin, {
 
 		init: function () {
+			this.editor.on('mode', this.proxy(this.initSyntaxHighlighting));
+			this.initSyntaxHighlighting();
+		},
 
-			this.editor.on('editorReady', function () {
-				if (this.mode === 'source') {
-					var textarea = this.getEditbox()[0];
-					require(['WikiTextSyntaxHighlighter'], function (WikiTextSyntaxHighlighter) {
-						WikiTextSyntaxHighlighter.init(textarea);
-					});
-				}
-			});
+		initConfig: function () {
+			var config;
+
+			if (window.wgIsDarkTheme) {
+				config = this.initDarkThemeColors();
+			} else {
+				config = this.initLightThemeColors();
+			}
+
+			return config;
+		},
+
+		initSyntaxHighlighting: function () {
+			if (this.editor.mode === 'source') {
+				var textarea = this.editor.getEditbox()[0],
+					config = this.initConfig();
+				require(['WikiTextSyntaxHighlighter'], function (WikiTextSyntaxHighlighter) {
+					WikiTextSyntaxHighlighter.init(textarea, config);
+				});
+			}
+		},
+
+		initDarkThemeColors: function() {
+			return {
+				boldOrItalicColor: '#6c71c4',
+				commentColor: '#dc322f',
+				entityColor: '#859900',
+				externalLinkColor: '#2aa198',
+				headingColor: '#6c71c4',
+				hrColor: '#6c71c4',
+				listOrIndentColor: '#dc322f',
+				parameterColor: '#cb4b16',
+				signatureColor: '#cb4b16',
+				tagColor: '#d33682',
+				tableColor: '#b58900',
+				templateColor: '#b58900',
+				wikilinkColor: '#268bd2'
+			};
+		},
+
+		initLightThemeColors: function() {
+			return {
+				boldOrItalicColor: '#e8e9ff',
+				commentColor: '#ffb8b6',
+				entityColor: '#c5cf86',
+				externalLinkColor: '#97d8d2',
+				headingColor: '#e8e9ff',
+				hrColor: '#e8e9ff',
+				listOrIndentColor: '#ffb8b6',
+				parameterColor: '#ffb692',
+				signatureColor: '#ffb692',
+				tagColor: '#ffc0e8',
+				tableColor: '#ecddb1',
+				templateColor: '#ecddb1',
+				wikilinkColor: '#b0e8ff'
+			};
 		}
 	});
 });
