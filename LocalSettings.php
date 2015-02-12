68f388d2a290bdd8dfe31f7f751ea2204a3f7d67
Owen Davis <owen@wikia-inc.com>
2015-02-11 17:35:08 -0800

    use __DIR__ because of unit tests

--- LocalSettings.php
+++ LocalSettings.php
@@ -4,7 +4,7 @@
 # We duplicate this code because some entry points do not go through webstart or maintenance
 $IP = getenv( 'MW_INSTALL_PATH' );
 if ( $IP === false ) {
-	$IP = realpath( '.' );
+	$IP = realpath( __DIR__ );
 }
 
 require __DIR__.'/../config/LocalSettings.php';
