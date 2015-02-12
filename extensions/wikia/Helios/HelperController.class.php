344272d33578a644630631be2a8ca4fce1cdb84a
Michał 'Mix' Roszka <mix@wikia-inc.com>
2015-02-12 11:13:52 +0000

    implement entry points exposing AntiSpoof functionality
    * \Wikia\Helios\HelperController::checkAntiSpoof() - verify whether the name is legal for a new account
    
    * \Wikia\Helios\HelperController::updateAntiSpoof() - update records once a new account has been created

---
+++ extensions/wikia/Helios/HelperController.class.php
@@ -0,0 +1,50 @@
+<?php
+namespace Wikia\Helios;
+
+/**
+ * A helper controller to provide end points exposing MediaWiki functionality to Helios.
+ */
+class HelperController extends \WikiaController
+{
+	/**
+	 * AntiSpoof: verify whether the name is legal for a new account.
+	 *
+	 * @see extensions/AntiSpoof
+	 */
+	public function checkAntiSpoof()
+	{
+		$this->response->setFormat( 'json' );
+		$this->response->setCacheValidity( \WikiaResponse::CACHE_DISABLED );
+
+		$sName = $this->getVal( 'username' );
+
+		// Allow the given user name if the AntiSpoof extension is disabled.
+		if ( empty( $this->wg->EnableAntiSpoofExt ) ) {
+			return $this->response->setVal( 'allow', true );
+		}
+
+		$oSpoofUser = new \SpoofUser( $sName );
+		$aConflicts = $oSpoofUser->getConflicts();
+
+		// Allow the given user name if it is legal and does not conflict with other names.
+		return $this->response->setVal( 'allow', $oSpoofUser->isLegal() && ! $oSpoofUser->getConflicts() );
+	}
+
+	/**
+	 * AntiSpoof: update records once a new account has been created.
+	 *
+	 * @see extensions/AntiSpoof
+	 */
+	public function updateAntiSpoof()
+	{
+		$this->response->setFormat( 'json' );
+		$this->response->setCacheValidity( \WikiaResponse::CACHE_DISABLED );
+
+		$sName = $this->getVal( 'username' );
+
+		if ( !empty( $this->wg->EnableAntiSpoofExt ) ) {
+			$oSpoofUser = new \SpoofUser( $sName );
+			$oSpoofUser->record();
+		}
+	}
+}
