8815fd6a9f3c444638ef57dd552608e1dac1292e
Michał 'Mix' Roszka <mix@wikia-inc.com>
2015-02-12 13:08:44 +0000

    implement entry point exposing confirmation emails sending
    * \Wikia\Helios\HelperController::sendConfirmationEmail() - send a confirmation email a new account has been created

--- extensions/wikia/Helios/HelperController.class.php
+++ extensions/wikia/Helios/HelperController.class.php
@@ -55,4 +55,70 @@ class HelperController extends \WikiaController
 			$oSpoofUser->record();
 		}
 	}
+
+	/**
+	 * UserLogin: send a confirmation email a new account has been created
+	 */
+	public function sendConfirmationEmail()
+	{
+		$this->response->setFormat( 'json' );
+		$this->response->setCacheValidity( \WikiaResponse::CACHE_DISABLED );
+		$this->response->setVal( 'success', false );
+
+		if ( $this->getVal( 'secret' ) != $this->wg->TheSchwartzSecretToken ) {
+			$this->response->setVal( 'message', 'invalid secret' );
+			return;
+		}
+
+		if ( ! $this->wg->EmailAuthentication ) {
+			$this->response->setVal( 'message', 'email authentication is not required' );
+			return;
+		}
+
+		$sName = $this->getVal( 'username' );
+
+		$this->wf->WaitForSlaves( $this->wg->ExternalSharedDB );
+		$oUser = \User::newFromName( $sName );
+
+		if ( ! $oUser instanceof \User ) {
+			$this->response->setVal( 'message', 'unable to create a \User object from name' );
+			return;
+		}
+
+		if ( ! $oUser->getId() ) {
+			$this->response->setVal( 'message', 'no such user' );
+			return;
+		}
+
+		if ( $oUser->isEmailConfirmed() ) {
+			$this->response->setVal( 'message', 'already confirmed' );
+			return;
+		}
+
+		$sMemKey = $this->getMemKeyConfirmationEmailsSent( $oUser->getId() );
+		$iEmailSent = intval( $this->wg->Memc->get( $sMemKey ) );
+
+		if ( $oUser->isEmailConfirmationPending() &&
+			strtotime( $oUser->mEmailTokenExpires ) - strtotime( '+6 days' ) > 0 &&
+			$iEmailSent >= \UserLoginHelper::LIMIT_EMAILS_SENT
+		) {
+			$this->response->setVal( 'message', 'confirmation emails limit reached' );
+			return;
+		}
+
+		if ( ! Sanitizer::validateEmail( $user->getEmail() ) ) {
+			$this->response->setVal( 'message', 'invalid email' );
+			return;
+		}
+
+		$sTemplate = $this->app->renderView( 'UserLogin', 'GeneralMail', [ 'language' => $oUser->getOption( 'language' ), 'type' => 'confirmation-email' ] );
+		$oResponse = $oUser->sendConfirmationMail( false, 'ConfirmationMail', 'usersignup-confirmation-email', true, $sTemplate );
+
+		if ( ! $oResponse->isGood() ) {
+			$this->response->setVal( 'message', 'could not send an email message' );
+			return;
+		}
+
+		$this->response->setVal( 'success', true );
+	}
 }
