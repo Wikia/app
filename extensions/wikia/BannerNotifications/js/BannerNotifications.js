f0d72ba943975c958ac33d0d3a5d8c2086c79b52
bkoval <brt.kowal@gmail.com>
2015-02-13 14:31:07 +0100

    AMD module and big cleanups in JS for BannerNotifications

--- extensions/wikia/BannerNotifications/js/BannerNotifications.js
+++ extensions/wikia/BannerNotifications/js/BannerNotifications.js
@@ -1,195 +1,217 @@
 /*
  * Handles the color-coded notification messages that generally appear
- * at the top the screen or inside a modal.
+ * at the top of the screen or inside a modal.
  * Use like:
  * BannerNotifications.show('Some success message', 'confirm')
  * BannerNotifications.show('Some error message', 'error', $('.myDiv'), 3000)
  */
-var BannerNotifications = {
+define('BannerNotifications', ['jquery', 'wikia.window'], function ($, window) {
+	'use strict';
 
-	defaultTimeout: 10000,
-	/**
-	 * All the possible class names and colors this module supports
-	 */
-	options: {
-		'notify': 'blue',
-		'confirm': 'green',
-		'error': 'red',
-		'warn': 'yellow'
-	},
+	var defaultTimeout = 10000,
+		types = {
+			'notify': 'blue',
+			'confirm': 'green',
+			'error': 'red',
+			'warn': 'yellow'
+		},
+		dom,
+		pageContainer,
+		wikiaHeader,
+		headerHeight,
+		modal,
+		isModal;
+
+	function createMarkup(id, content, type) {
+		return $('<div id="' + id + '" class="global-notification">' +
+			'<button class="close wikia-chiclet-button">' +
+			'<img src="' + window.stylepath + '/oasis/images/icon_close.png">' +
+			'</button><div class="msg">' + content + '</div></div>')
+			.addClass(type)
+			.hide();
+	}
+
+	function addToDOM(markup, $elementBefore) {
+		// allow notification wrapper element to be passed by extension
+		if ($elementBefore instanceof jQuery) {
+			$elementBefore.prepend(markup);
+
+			// handle modal implementations
+		} else if (isModal) {
+			modal.prepend(markup);
+
+			// handle non-modal implementation
+		} else {
+			$(pageContainer).prepend(markup);
+		}
+
+		markup.fadeIn('slow');
+	}
 
 	/**
 	 * Called once to instantiate this feature
 	 */
-	init: function () {
-		'use strict';
+	function init() {
+		setUpClose();
 
-		// If there's already a banner notification on page load, set up JS
-		BannerNotifications.dom = $('.global-notification');
-		if (BannerNotifications.dom.length) {
-			this.setUpClose();
-		}
-
-		this.pageContainer = $('.WikiaPageContentWrapper');
-		this.pageContainerElem = this.pageContainer[0];
-		this.wikiaHeader = $('#globalNavigation');
-		this.headerHeight = this.wikiaHeader.height();
-	},
-
-	/**
-	 * Build the notification DOM element and attach it to the DOM
-	 * @param {object} [element] Element to prepend the notification to
-	 * @param {boolean} [isModal] Whether or not a modal is present and visible on the page
-	 */
-	createDom: function (element, isModal) {
-		'use strict';
-
-		// create and store dom
-		if (!BannerNotifications.dom.length) {
-			BannerNotifications.dom = $('<div class="global-notification">' +
-				'<button class="close wikia-chiclet-button">' +
-				'<img src="' + window.stylepath + '/oasis/images/icon_close.png">' +
-				'</button><div class="msg"></div></div>')
-				.hide();
-			BannerNotifications.setUpClose();
-		}
-
-		// allow notification wrapper element to be passed by extension
-		if (element instanceof jQuery) {
-			element.prepend(BannerNotifications.dom).show();
-
-		// handle modal implementations
-		} else if (isModal) {
-			BannerNotifications.modal.prepend(BannerNotifications.dom);
-
-		// handle non-modal implementation
-		} else {
-			this.pageContainer.prepend(BannerNotifications.dom);
-		}
-
-		BannerNotifications.msg = BannerNotifications.dom.find('.msg');
-	},
+		pageContainer = $('.WikiaPageContentWrapper')[0];
+		wikiaHeader = $('#globalNavigation');
+		headerHeight = wikiaHeader.height();
+	}
 
 	/**
 	 * Main entry point for this feature - shows the notification
+	 * @param {string} id - unique id used for DOM markup of the notification
 	 * @param {string} content - message to be displayed
 	 * @param {string} type - See BannerNotifications.options for supported types
-	 * @param {jQuery} [element] Element to prepend notification to
+	 * @param {jQuery} [$element] Element to prepend notification to
 	 * @param {number} [timeout] Optional time (in ms) after which notification will disappear.
 	 */
-	show: function (content, type, element, timeout) {
-		'use strict';
+	function show(id, content, type, $element, timeout) {
 
-		var isModal,
-			classes = Object.keys(BannerNotifications.options).join(' ');
-		BannerNotifications.content = content;
+		validate(id, content);
+		isModal = isModalShown();
 
-		function callback() {
-			isModal = BannerNotifications.isModal();
-
-			// Modal notifications have no close button so set a timeout
-			if (isModal && typeof timeout !== 'number') {
-				timeout = BannerNotifications.defaultTimeout;
-			}
-
-			BannerNotifications.createDom(element, isModal);
-			BannerNotifications.msg.html(BannerNotifications.content);
-			BannerNotifications.dom.removeClass(classes).addClass(type);
-
-			// Share scroll event with WikiaFooterApp's toolbar floating (BugId:33365)
-			if (window.WikiaFooterApp) {
-				window.WikiaFooterApp.addScrollEvent();
-			}
-
-			BannerNotifications.dom.fadeIn('slow');
-
-			// Close notification after specified amount of time
-			if (typeof timeout === 'number') {
-				setTimeout(function () {
-					BannerNotifications.hide();
-				}, timeout);
-			}
+		// Modal notifications have no close button so set a timeout
+		if (isModal && typeof timeout !== 'number') {
+			timeout = defaultTimeout;
 		}
 
-		// close any existing notifications before adding a new one
-		BannerNotifications.hide(callback);
-	},
+		addToDOM(
+			createMarkup(id, content, type),
+			$element
+		);
 
-	/**
-	 * Hides the notification and executes an optional callback
-	 * @param {function} [callback]
-	 */
-	hide: function (callback) {
-		'use strict';
-		if (!BannerNotifications.dom) {
-			return;
+		// Share scroll event with WikiaFooterApp's toolbar floating (BugId:33365)
+		if (window.WikiaFooterApp) {
+			window.WikiaFooterApp.addScrollEvent();
 		}
-		if (BannerNotifications.dom.length) {
-			BannerNotifications.dom.animate({
-				'height': 0,
-				'padding': 0,
-				'opacity': 0
-			}, 400, function () {
-				BannerNotifications.dom.remove();
-				BannerNotifications.dom = [];
-				if (typeof callback === 'function') {
-					callback();
-				}
-			});
-		} else {
-			if (typeof callback === 'function') {
-				callback();
-			}
+
+		// Close notification after specified amount of time
+		if (typeof timeout === 'number') {
+			setTimeout(function () {
+				hide(id);
+			}, timeout);
 		}
-	},
+	}
 
 	/**
 	 * Determine if a modal is present and visible so we can apply the notification to the modal instead of the page.
 	 * @returns {boolean}
 	 */
-	isModal: function () {
-		'use strict';
-
+	function isModalShown () {
 		// handle all types of modals since the begining of time!
-		BannerNotifications.modal = $('.modalWrapper, .yui-panel, .modal');
+		modal = $('.modalWrapper, .yui-panel, .modal');
 
 		// returns false if there's no modal or it's hidden
-		return BannerNotifications.modal.is(':visible');
-	},
+		return modal.is(':visible');
+	}
+
+	/**
+	 * Removes notification element from DOM and executes callback
+	 * @param {jQuery} $elements - elements to be removed from DOM
+	 * @param {function} [callback]
+	 */
+	function removeFromDOM ($elements, callback) {
+		if ($elements.length) {
+			$elements.animate({
+				'height': 0,
+				'padding': 0,
+				'opacity': 0
+			}, 400, function () {
+				$elements.remove();
+				if (typeof callback === 'function') {
+					callback();
+				}
+			});
+		}
+	}
+
+	/**
+	 * Hides the notification and executes an optional callback
+	 * @param {String} id - ID of the notification element to hide
+	 * @param {Function} [callback] - Optional callback to be triggered after the action
+	 */
+	function hide (id, callback) {
+		var $element = $('#' + id);
+
+		if ($element.length) {
+			removeFromDOM($element, callback);
+		} else {
+			if (typeof callback === 'function') {
+				callback();
+			}
+		}
+	}
+
+	function validate(id, content) {
+		if (!id || document.getElementById(id)) {
+			throw new Error('Either empty or already existing notification ID supplied');
+		} else if (!content) {
+			throw new Error('Empty notification content supplied');
+		}
+	}
+
+	function hideAll (callback) {
+		removeFromDOM($('.global-notification'), callback);
+	}
 
 	/**
 	 * Bind close event to close button
 	 */
-	setUpClose: function () {
-		'use strict';
-		BannerNotifications.dom.find('.close').on('click', BannerNotifications.hide);
-	},
+	function setUpClose () {
+		$(document.body).on('click', '.close', onCloseClicked);
+	}
+
+	/**
+	 * Handles click event on the 'close' button
+	 * @param {Event} event
+	 */
+	function onCloseClicked (event) {
+		removeFromDOM(
+			$(event.target).closest('.global-notification')
+		);
+		event.stopPropagation();
+	}
 
 	/**
 	 * Pin the notification to the top of the screen when scrolled down the page.
 	 * Shares the scroll event with WikiaFooter.js
 	 */
-	onScroll: function () {
-		'use strict';
-
+	function onScroll () {
 		var containerTop;
 
-		if (!BannerNotifications.dom || !BannerNotifications.dom.length) {
+		if (!dom || !dom.length) {
 			return;
 		}
 
 		// get the position of the wrapper element relative to the top of the viewport
-		containerTop = BannerNotifications.pageContainerElem.getBoundingClientRect().top;
+		containerTop = pageContainer.getBoundingClientRect().top;
 
-		if (containerTop < BannerNotifications.headerHeight) {
-			BannerNotifications.dom.addClass('float');
+		if (containerTop < headerHeight) {
+			dom.addClass('float');
 		} else {
-			BannerNotifications.dom.removeClass('float');
+			dom.removeClass('float');
 		}
 	}
-};
+
+	//Attachment to window is for legacy reasons
+	window.BannerNotifications = {
+		init: init,
+		onScroll: onScroll,
+		show: show,
+		hide: hide,
+		hideAll: hideAll,
+		isModal: isModalShown,
+		types: types
+	};
+
+	return window.BannerNotifications;
+});
 
 $(function () {
 	'use strict';
-	BannerNotifications.init();
+	require(['BannerNotifications'], function (bannerNotifications) {
+		bannerNotifications.init();
+	});
 });
