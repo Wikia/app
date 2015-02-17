9f9037501d72f4edef11caaed7fe72ede43e08d9
Michał Roszka <michal@roszka.pl>
2015-02-18 00:38:23 +0100

    improve code readability

--- includes/wikia/ExternalUser_Wikia.php
+++ includes/wikia/ExternalUser_Wikia.php
@@ -216,16 +216,17 @@ class ExternalUser_Wikia extends ExternalUser {
 		// Detect discrepancies between Helios and MediaWiki results.
 		if ( $wgEnableHeliosExt && (  $bHeliosResult != $bMediaWikiResult ) ) {
 			
-			$sMWHashFirst = substr( $this->getPassword(), 0, 3 ); // The first three bytes.
-			$sMWHashLast  = substr( $this->getPassword(),   -3 ); // The last three bytes.
+			$sMWHashFirst  = substr( $this->getPassword(), 0, 3 ); // The first three bytes.
+			$sMWHashLast   = substr( $this->getPassword(),   -3 ); // The last three bytes.
+			$sMWHashLength = strlen( $this->getPassword() );       // The byte-length
 
 			\Wikia\Helios\User::debugLogin( $sPassword, __METHOD__ );
 			\Wikia\Logger\WikiaLogger::instance()->error(
 				'HELIOS_LOGIN',
-				[ 'helios' => $bHeliosResult, 'mediawiki' => $bMediaWikiResult,
-				 'user_id' => $this->getId(), 'username' => $this->getName(),
-				 'mw_hash_first' => $sMWHashFirst, 'mw_hash_last' => $sMWHashLast,
-				 'mw_hash_length' => strlen( $this->getPassword() ) ]
+				[ 'helios'         => $bHeliosResult, 'mediawiki'    => $bMediaWikiResult,
+				  'user_id'        => $this->getId(), 'username'     => $this->getName(),
+				  'mw_hash_first'  => $sMWHashFirst,  'mw_hash_last' => $sMWHashLast,
+				  'mw_hash_length' => $sMWHashLength  ]
 			);
 		}
 
