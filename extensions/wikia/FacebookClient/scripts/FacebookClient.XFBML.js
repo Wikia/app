09854b69f48e0f953a3d65edf21e30eab9958805
James Sutterfield <james@wikia-inc.com>
2015-02-18 08:20:12 -0800

    SOC-261 merge in loadFacebookSDK fix and update xfbml loading logic

--- extensions/wikia/FacebookClient/scripts/FacebookClient.XFBML.js
+++ extensions/wikia/FacebookClient/scripts/FacebookClient.XFBML.js
@@ -1,35 +1,23 @@
 /* global FB */
 
 /**
- * Render FB XML tags emebedded into wikitext
+ * Render XFBML tags emebedded into wikitext
  */
-$(function () {
+mw.hook('wikipage.content').add(function ($content) {
 	'use strict';
 
-	// load api on page load and on demand
-	mw.hook('wikipage.content').add(function ($content) {
+	if ((xfbmlTagsOnPage())) {
+		$.loadFacebookSDK().done(function () {
+			renderXFBMLTags();
+		});
+	}
 
-		if ((xfbmlTagsOnPage())) {
-			if (facebookSDKNotLoaded()) {
-				// XFBML tags rendered automatically when SDK loads
-				$.loadFacebookSDK();
-			} else {
-				renderXFBMLTags();
-			}
-		}
+	function xfbmlTagsOnPage() {
+		var numOfXFBMLTags = $content.find('[data-type="xfbml-tag"]').length;
+		return numOfXFBMLTags > 0;
+	}
 
-		function xfbmlTagsOnPage() {
-			var numOfXFBMLTags = $content.find('[data-type="xfbml-tag"]').length;
-			return numOfXFBMLTags > 0;
-		}
-
-		function facebookSDKNotLoaded() {
-			return typeof FB === 'undefined';
-		}
-
-		function renderXFBMLTags() {
-			FB.XFBML.parse($content[0]);
-		}
-
-	});
+	function renderXFBMLTags() {
+		FB.XFBML.parse($content[0]);
+	}
 });
