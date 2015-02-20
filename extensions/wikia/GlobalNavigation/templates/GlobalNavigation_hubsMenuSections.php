c2f8e08b3e10053c122d52513cc1990f65d624df
V <bartosz.bentkowski@gmail.com>
2015-02-20 13:23:29 +0100

    CONCF-139 | Fix non-existing fields throwing PHP errors

--- extensions/wikia/GlobalNavigation/templates/GlobalNavigation_hubsMenuSections.php
+++ extensions/wikia/GlobalNavigation/templates/GlobalNavigation_hubsMenuSections.php
@@ -1,16 +1,20 @@
 <? foreach( $menuSections as $sections ): ?>
 	<div class="hub-menu-section <?= $sections['specialAttr'] ?>-links active">
 		<div class="hub-menu-columns">
-			<? foreach( $sections['children'] as $category ): ?>
-				<strong><?= $category['text'] ?></strong>
-				<ul>
-					<? foreach( $category['children'] as $node ): ?>
-						<li>
-							<a href="<?= $node['href'] ?>"><?= $node['text'] ?></a>
-						</li>
-					<? endforeach; ?>
-				</ul>
-			<? endforeach; ?>
+			<? if ( array_key_exists( 'children', $sections ) ) : ?>
+				<? foreach( $sections['children'] as $category ): ?>
+					<strong><?= $category['text'] ?></strong>
+					<? if ( array_key_exists( 'children', $category ) ) : ?>
+						<ul>
+							<? foreach( $category['children'] as $node ): ?>
+								<li>
+									<a href="<?= $node['href'] ?>"><?= $node['text'] ?></a>
+								</li>
+							<? endforeach; ?>
+						</ul>
+					<? endif; ?>
+				<? endforeach; ?>
+			<? endif; ?>
 		</div>
 		<a class="more" href="<?= $sections['href']; ?>"><?= wfMessage('global-navigation-hubs-menu-more-of', $sections['text'])->parse(); ?></a>
 	</div>
