24ecbd55aa848b3cc2b49499cec4e339518b07c0
adamkarminski <adamkarminski@gmail.com>
2015-02-20 16:41:33 +0100

    CE-1310 Move Source setup to SourceDatabase

--- extensions/wikia/SpecialPageViews/SpecialPageViewsController.class.php
+++ extensions/wikia/SpecialPageViews/SpecialPageViewsController.class.php
@@ -4,6 +4,8 @@ class SpecialPageViewsController extends WikiaSpecialPageController {
 
 	const SPECIALPAGE_NAME = 'PageViews';
 
+	private $startDate, $endDate;
+
 	function __construct() {
 		parent::__construct( self::SPECIALPAGE_NAME );
 	}
@@ -12,14 +14,10 @@ class SpecialPageViewsController extends WikiaSpecialPageController {
 		wfProfileIn( __METHOD__ );
 		$this->setHeaders();
 
+		$this->setInitialDates();
+
 		$oOutput = ( new SpecialPageViewsOutput );
-
-		$endDate = new DateTime( date( 'Y-m-d' ) );
-		$endDate->sub( new DateInterval( 'P1D' ) );
-		$startDate = clone $endDate;
-		$startDate->sub( new DateInterval( 'P1M' ) );
-
-		$oOutput->set( $this->getReport( $startDate, $endDate ) );
+		$oOutput->set( $this->getReport() );
 		$sReturnChart = $oOutput->getHTML();
 		F::app()->wg->out->addHTML( $sReturnChart );
 
@@ -32,56 +30,52 @@ class SpecialPageViewsController extends WikiaSpecialPageController {
 		$oOut->setPageTitle( wfMessage( 'special-pageviews-title' )->escaped() );
 	}
 
-	private function getReport( $startDate, $endDate ) {
+	private function getReport() {
 		$oReport = ( new SponsorshipDashboardReport );
 		$oReport->name = wfMsg( 'special-pageviews-report-title' );
 		$oReport->frequency = SponsorshipDashboardDateProvider::SD_FREQUENCY_DAY;
-		$oReport->tmpSource = $this->getSource( $startDate, $endDate );
-		$oReport->setLastDateUnits( $startDate->diff( $endDate )->days );
+		$oReport->tmpSource = $this->getSource();
+		$oReport->setLastDateUnits( $this->startDate->diff( $this->endDate )->days );
 		$oReport->acceptSource();
 		$oReport->setId( 1 );
 		$oReport->lockSources();
 		return $oReport;
 	}
 
-	private function getSource( $startDate, $endDate ) {
-		$oSource = new SpecialPageViewsSourceDatabase( 'pageviews' );
-		$oSource->serieName = wfMsg( 'special-pageviews-report-serie' );
-		$oSource->setDatabase( wfGetDB( DB_SLAVE, array(), F::app()->wg->DWStatsDB ) );
-
-		$sql = "SELECT pageviews, DATE( time_id ) as pv_date FROM rollup_wiki_pageviews WHERE period_id='1' AND time_id >= '%s' AND time_id <= '%s' AND wiki_id = " . F::app()->wg->CityId . ";";
-		$oSource->setQuery( $sql );
-		$oSource->setStartDate( $startDate->format( 'Y-m-d' ) );
-		$oSource->setEndDate( $endDate->format( 'Y-m-d' ) );
-
-		return $oSource;
-	}
-
 	public function getData() {
 		$inStartDate = $this->getVal( 'startDate', date( 'Y-m-d' ) );
 		$inEndDate = $this->getVal( 'endDate', date( 'Y-m-d' ) );
-		$startDate = DateTime::createFromFormat( 'Y-m-d', $inStartDate );
-		$endDate = DateTime::createFromFormat( 'Y-m-d', $inEndDate );
+		$this->startDate = DateTime::createFromFormat( 'Y-m-d', $inStartDate );
+		$this->endDate = DateTime::createFromFormat( 'Y-m-d', $inEndDate );
 
 		unset( $inStartDate, $inEndDate );
 
-		if ( !$startDate || !$endDate ) {
-			$endDate = new DateTime( date( 'Y-m-d' ) );
-			$endDate->sub( new DateInterval( 'P1D' ) );
-			$startDate = clone $endDate;
-			$startDate->sub( new DateInterval( 'P1M' ) );
+		if ( !$this->startDate || !$this->endDate ) {
+			$this->setInitialDates();
 		}
 
-		$oOutput = (new SpecialPageViewsOutput);
-		$oOutput->set( $this->getReport( $startDate, $endDate ) );
+		$oOutput = ( new SpecialPageViewsOutput );
+		$oOutput->set( $this->getReport( $this->startDate, $this->endDate ) );
 
 		$mOut = $oOutput->getRaw();
-
 		$this->setVal('chartId', $mOut->chartId );
 		$this->setVal('datasets', $mOut->datasets );
 		$this->setVal('fullTicks', $mOut->fullTicks );
 		$this->setVal('hiddenSeries', $mOut->hiddenSeries );
 		$this->setVal('monthly', $mOut->monthly );
 		$this->setVal('ticks', $mOut->ticks );
+	}
+
+	private function getSource() {
+		$oSource = new SpecialPageViewsSourceDatabase( 'pageviews' );
+		$oSource->setupSource( $this->startDate, $this->endDate );
+		return $oSource;
+	}
+
+	private function setInitialDates() {
+		$this->endDate = new DateTime( date( 'Y-m-d' ) );
+		$this->endDate->sub( new DateInterval( 'P1D' ) );
+		$this->startDate = clone $this->endDate;
+		$this->startDate->sub( new DateInterval( 'P1M' ) );
 	}
 }
--- extensions/wikia/SpecialPageViews/SpecialPageViewsSourceDatabase.class.php
+++ extensions/wikia/SpecialPageViews/SpecialPageViewsSourceDatabase.class.php
@@ -13,7 +13,15 @@ class SpecialPageViewsSourceDatabase extends SponsorshipDashboardSourceDatabase 
 		$this->loadData();
 	}
 
+	public function setupSource( $startDate, $endDate ) {
+		$this->serieName = wfMessage( 'special-pageviews-report-serie' )->escaped();
+		$this->setDatabase( wfGetDB( DB_SLAVE, array(), F::app()->wg->DWStatsDB ) );
 
+		$sql = "SELECT pageviews, DATE( time_id ) as pv_date FROM rollup_wiki_pageviews WHERE period_id='1' AND time_id >= '%s' AND time_id <= '%s' AND wiki_id = " . F::app()->wg->CityId . ";";
+		$this->setQuery( $sql );
+		$this->setStartDate( $startDate->format( 'Y-m-d' ) );
+		$this->setEndDate( $endDate->format( 'Y-m-d' ) );
+	}
 
 	protected function getResults() {
 
