f005a2e015e9bbfb42f0b270c2b4c974bc2d447f
Diana <dianafa@o2.pl>
2015-02-23 08:53:57 +0000

    window renamed to win, typos

--- skins/oasis/js/ArticleLengthAbTesting.js
+++ skins/oasis/js/ArticleLengthAbTesting.js
@@ -1,28 +1,27 @@
-require(['jquery', 'wikia.window', 'wikia.tracker'], function($, window, tracker) {	
+require(['jquery', 'wikia.window', 'wikia.tracker'], function($, win, tracker) {	
 	'use strict';
 
 	var WIDTH_SAMPLING_RATIO = 1,
 		ARTICLE_LENGTH_THRESHOLD = 800,
 		SCALE = 100;
 		
-	if (Math.random() * 100 < WIDTH_SAMPLING_RATIO && window.wgIsArticle)  {
+	if (Math.random() * 100 < WIDTH_SAMPLING_RATIO && win.wgIsArticle)  {
 		var labelPrefix,
 			trackFunction,
 			$article = $('.WikiaArticle'),
-			windowWidth = Math.floor($(window).width() / SCALE);
+			windowWidth = Math.floor($(win).width() / SCALE);
 			
 		trackFunction = tracker.buildTrackingFunction({
 			action: tracker.ACTIONS.IMPRESSION,
 			category: 'articleContentLengthTest',
 			trackingMethod: 'ga'
 		});
-
 		if ($article.height() > ARTICLE_LENGTH_THRESHOLD) {
 			labelPrefix = 'long-';
 		} else {
 			labelPrefix = 'short-';
-		};
-		tracker.track({
+		}
+		trackFunction({
 			label: labelPrefix + windowWidth
 		})
 	}	
