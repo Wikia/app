43f654ebc7908ce36a9a681f075d682d0552f54f
sebastian-wikia <sebastianm@wikia-inc.com>
2015-02-25 10:07:58 +0100

    Merge

--- extensions/wikia/MercuryApi/MercuryApiController.class.php
+++ extensions/wikia/MercuryApi/MercuryApiController.class.php
@@ -9,6 +9,8 @@ class MercuryApiController extends WikiaController {
 	const NUMBER_CONTRIBUTORS = 5;
 	const DEFAULT_PAGE = 1;
 
+	const WIKI_VARIABLES_CACHE_TTL = 60;
+	
 	private $mercuryApi = null;
 
 	public function __construct() {
@@ -256,6 +258,9 @@ class MercuryApiController extends WikiaController {
 
 		$this->response->setVal( 'data', $wikiVariables );
 		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
+
+		//cache wikiVariables for 1 minute
+		$this->response->setCacheValidity( self:: WIKI_VARIABLES_CACHE_TTL );
 	}
 
 	/**
--- skins/oasis/modules/templates/BodyContentOnly_NavOnly.php
+++ skins/oasis/modules/templates/BodyContentOnly_NavOnly.php
@@ -1,2 +1,1 @@
-<?= $app->renderView('GlobalHeader', 'Index') ?>
 <?= $bodytext ?>
