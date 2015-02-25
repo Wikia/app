d7420f10cca7b798c1dc3ed9b0444528841afb70
James Sutterfield <james@wikia-inc.com>
2015-02-25 15:17:20 -0800

    SOC-230 CSS updates

--- extensions/wikia/Captcha/scripts/Captcha.js
+++ extensions/wikia/Captcha/scripts/Captcha.js
@@ -61,5 +61,7 @@ $(function () {
 		}
 	};
 
+	// Temporarily load captcha library on every page until we have on demand loading set up.
+	// Remove with https://wikia-inc.atlassian.net/browse/SOC-288
 	Captcha.init();
 });
