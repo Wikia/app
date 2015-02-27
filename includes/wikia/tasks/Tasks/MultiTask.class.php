664d2a1daba4593c69c827fe38b72654695325cc
macbre <maciej.brencz@gmail.com>
2015-02-27 15:37:46 +0100

    Stylize MultiTask.class.php

--- includes/wikia/tasks/Tasks/MultiTask.class.php
+++ includes/wikia/tasks/Tasks/MultiTask.class.php
@@ -16,22 +16,22 @@ class MultiTask extends BaseTask {
 	 */
 	public function move( $params ) {
 		/** @var \Title $oldTitle */
-		list( , $impersonatedUsername, $oldTitle) = $this->parseCommon($params);
-		$newTitle = isset($params['newpage']) ? \Title::newFromText($params['newpage']) : null;
+		list( , $impersonatedUsername, $oldTitle ) = $this->parseCommon( $params );
+		$newTitle = isset( $params['newpage'] ) ? \Title::newFromText( $params['newpage'] ) : null;
 
-		if (!is_object($oldTitle) || !is_object($newTitle)) {
-			$this->error('move page request invalid, terminating', [
+		if ( !is_object( $oldTitle ) || !is_object( $newTitle ) ) {
+			$this->error( 'move page request invalid, terminating', [
 				'old_page' => $params['page'],
 				'new_page' => $params['newpage'],
-			]);
+			] );
 
 			return false;
 		}
 
 		$oldNamespace = $oldTitle->getNamespace();
-		$oldTitleText = str_replace(' ', '_', $oldTitle->getText());
+		$oldTitleText = str_replace( ' ', '_', $oldTitle->getText() );
 		$newNamespace = $newTitle->getNamespace();
-		$newTitleText = str_replace(' ', '_', $newTitle->getText());
+		$newTitleText = str_replace( ' ', '_', $newTitle->getText() );
 
 		$commandParams = [ // each param has an extra "-" because the script reads it this way
 			'-u' => $impersonatedUsername,
@@ -41,15 +41,15 @@ class MultiTask extends BaseTask {
 			'-nn' => $newNamespace,
 		];
 
-		if (isset($params['reason'])) {
+		if ( isset( $params['reason'] ) ) {
 			$commandParams['-r'] = $params['reason'];
 		}
 
-		if (isset($params['redirect'])) {
+		if ( isset( $params['redirect'] ) ) {
 			$commandParams['-redirect'] = 1;
 		}
 
-		if (isset($params['watch'])) {
+		if ( isset( $params['watch'] ) ) {
 			$commandParams['-watch'] = 1;
 		}
 
@@ -62,7 +62,7 @@ class MultiTask extends BaseTask {
 			$commandParams
 		);
 
-		return array_sum($result);
+		return array_sum( $result );
 	}
 
 	/**
@@ -72,32 +72,32 @@ class MultiTask extends BaseTask {
 	public function delete( $params ) {
 		/** @var \Title $page */
 		/** @var \User $createdBy */
-		list($createdBy, $impersonatedUsername, $page) = $this->parseCommon($params);
+		list( $createdBy, $impersonatedUsername, $page ) = $this->parseCommon( $params );
 
-		$this->info('deleting page', [
+		$this->info( 'deleting page', [
 			'user' => $createdBy->getName(),
 			'impersonating' => $impersonatedUsername,
 			'title' => $params['page'],
-		]);
+		] );
 
-		if (!is_object($page)) {
-			$this->error('page is invalid, terminating task', [
+		if ( !is_object( $page ) ) {
+			$this->error( 'page is invalid, terminating task', [
 				'title' => $params['page'],
-			]);
+			] );
 
 			return false;
 		}
 
 		$commandParams = [
-			't' => str_replace(' ', '_', $page->getPrefixedText()),
+			't' => str_replace( ' ', '_', $page->getPrefixedText() ),
 			'u' => $impersonatedUsername,
 		];
 
-		if (isset($params['suppress']) && $params['suppress'] === true) {
+		if ( isset( $params['suppress'] ) && $params['suppress'] === true ) {
 			$commandParams['s'] = null;
 		}
 
-		if (isset($params['reason'])) {
+		if ( isset( $params['reason'] ) ) {
 			$commandParams['r'] = $params['reason'];
 		}
 
@@ -116,7 +116,7 @@ class MultiTask extends BaseTask {
 			$commandParams
 		);
 
-		return array_sum($result);
+		return array_sum( $result );
 	}
 
 	/**
@@ -125,33 +125,33 @@ class MultiTask extends BaseTask {
 	 */
 	public function edit( $params ) {
 		/** @var \Title $page */
-		list( , $impersonatedUsername, $page) = $this->parseCommon($params);
+		list( , $impersonatedUsername, $page ) = $this->parseCommon( $params );
 
-		if (!is_object($page)) {
-			$this->error('page is invalid, terminating task', [
+		if ( !is_object( $page ) ) {
+			$this->error( 'page is invalid, terminating task', [
 				'title' => $params['page'],
-			]);
+			] );
 
 			return false;
 		}
 
 		$commandParams = [
-			't' => str_replace(' ', '_', $page->getText()),
+			't' => str_replace( ' ', '_', $page->getText() ),
 			'n' => $page->getNamespace(),
 			'x' => $params['text'],
 		];
 
-		if (!empty($impersonatedUsername)) {
+		if ( !empty( $impersonatedUsername ) ) {
 			$commandParams['u'] = $impersonatedUsername;
 		}
 
-		if (!empty($params['summary'])) {
+		if ( !empty( $params['summary'] ) ) {
 			$commandParams['s'] = $params['summary'];
 		}
 
-		$optionsSwitches = ['m','b','a','-no-rc', '-newonly'];
-		for ($i = 0; $i < count($params['flags']); ++$i) {
-			if ($params['flags'][$i]) {
+		$optionsSwitches = ['m', 'b', 'a', '-no-rc', '-newonly'];
+		for ( $i = 0; $i < count( $params['flags'] ); ++$i ) {
+			if ( $params['flags'][$i] ) {
 				$commandParams[$optionsSwitches[$i]] = null;
 			}
 		}
@@ -165,16 +165,16 @@ class MultiTask extends BaseTask {
 			$commandParams
 		);
 
-		return array_sum($result);
+		return array_sum( $result );
 	}
 
-	private function parseCommon($params) {
-		$impersonatedName = isset($params['user']) ? $params['user'] : 'Maintenance script';
-		$createdBy = \User::newFromId($this->createdBy);
-		$impersonatedUser = \User::newFromName($impersonatedName);
-		$page = isset($params['page']) ? \Title::newFromText($params['page']) : null;
+	private function parseCommon( $params ) {
+		$impersonatedName = isset( $params['user'] ) ? $params['user'] : 'Maintenance script';
+		$createdBy = \User::newFromId( $this->createdBy );
+		$impersonatedUser = \User::newFromName( $impersonatedName );
+		$page = isset( $params['page'] ) ? \Title::newFromText( $params['page'] ) : null;
 
-		if (!is_object($impersonatedUser)) {
+		if ( !is_object( $impersonatedUser ) ) {
 			$impersonatedName = '';
 		}
 
@@ -194,7 +194,7 @@ class MultiTask extends BaseTask {
 	 * @param \Title $title
 	 * @return array
 	 */
-	private function getWikisWherePageExists(\Title $title) {
+	private function getWikisWherePageExists( \Title $title ) {
 		global $wgExternalDatawareDB;
 
 		$dbr = wfGetDB( DB_SLAVE, [], $wgExternalDatawareDB );
@@ -209,7 +209,7 @@ class MultiTask extends BaseTask {
 		);
 
 		$wikis = [];
-		foreach($res as $row) {
+		foreach ( $res as $row ) {
 			/* @var mixed $row */
 			$wikis[] = intval( $row->wiki_id );
 		}
@@ -227,16 +227,16 @@ class MultiTask extends BaseTask {
 	 * @return bool|mixed
 	 * @throws \Exception
 	 */
-	private function runOnWikis($wikiInputRaw, $lang, $cat, $wikisId, $action, $commandParams) {
+	private function runOnWikis( $wikiInputRaw, $lang, $cat, $wikisId, $action, $commandParams ) {
 		global $wgExternalSharedDB ;
-		$db = wfGetDB (DB_SLAVE, array(), $wgExternalSharedDB);
+		$db = wfGetDB ( DB_SLAVE, array(), $wgExternalSharedDB );
 
 		$wikiDomains = [];
-		foreach (explode("\n", $wikiInputRaw) as $wikiLine) {
-			foreach (explode(',', $wikiLine) as $wikiLinePart) {
-				$wikiLinePart = trim($wikiLinePart);
-				if (!empty($wikiLinePart)) {
-					$wikiDomains []= $wikiLinePart;
+		foreach ( explode( "\n", $wikiInputRaw ) as $wikiLine ) {
+			foreach ( explode( ',', $wikiLine ) as $wikiLinePart ) {
+				$wikiLinePart = trim( $wikiLinePart );
+				if ( !empty( $wikiLinePart ) ) {
+					$wikiDomains [] = $wikiLinePart;
 				}
 			}
 		}
@@ -246,11 +246,11 @@ class MultiTask extends BaseTask {
 		}
 
 		$this->info( sprintf( 'Running "%s" action of %d wiki(s)', $action, count( $wikisId ) ), [
-			'wikis' => join(', ', $wikisId)
+			'wikis' => join( ', ', $wikisId )
 		] );
 
 		// Get a count of articles in a category.  Give at least a very small cache TTL
-		$query = (new \WikiaSQL())->cacheGlobal( 60 )
+		$query = ( new \WikiaSQL() )->cacheGlobal( 60 )
 			->SELECT( 'city_list.city_id', 'city_list.city_dbname', 'city_list.city_url' )
 			->FROM( 'city_list' )
 			->WHERE( 'city_public' )->EQUAL_TO( 1 );
@@ -259,19 +259,19 @@ class MultiTask extends BaseTask {
 			$query->AND_( 'city_list.city_lang' )->EQUAL_TO( $lang );
 		}
 
-		if (!empty($wikiId)) {
-			$query->AND_('city_list.city_id')->IN($wikisId);
+		if ( !empty( $wikiId ) ) {
+			$query->AND_( 'city_list.city_id' )->IN( $wikisId );
 		}
 
-		if (!empty($cat)) {
-			$cat = intval($cat);
+		if ( !empty( $cat ) ) {
+			$cat = intval( $cat );
 			$query->AND_( 'city_cat_mapping.cat_id' )->EQUAL_TO( $cat )
-				->JOIN('city_cat_mapping')->ON('city_cat_mapping.city_id', 'city_list.city_id');
+				->JOIN( 'city_cat_mapping' )->ON( 'city_cat_mapping.city_id', 'city_list.city_id' );
 		}
 
-		if (!empty($wikiDomains)) {
-			$query->AND_('city_domains.city_domain')->IN($wikiDomains)
-				->JOIN('city_domains')->ON('city_domains.city_id', 'city_list.city_id');
+		if ( !empty( $wikiDomains ) ) {
+			$query->AND_( 'city_domains.city_domain' )->IN( $wikiDomains )
+				->JOIN( 'city_domains' )->ON( 'city_domains.city_id', 'city_list.city_id' );
 		}
 
 		$result = $query->runLoop( $db, function( &$result, $row ) use ( $action, $commandParams ) {
@@ -283,42 +283,42 @@ class MultiTask extends BaseTask {
 
 		return $result;
 	}
-	
+
 	private function makeCommand( $row, $action, $params ) {
 		global $IP;
 
-		$command = "SERVER_ID=". $row->city_id ." php $IP/maintenance/wikia/{$action}On.php";
+		$command = "SERVER_ID=" . $row->city_id . " php $IP/maintenance/wikia/{$action}On.php";
 
 		foreach ( $params as $opt => $value ) {
 			$command .= " -{$opt} ";
-			if ($value !== null) {
-				$command .= escapeshellarg($value);
+			if ( $value !== null ) {
+				$command .= escapeshellarg( $value );
 			}
 		}
-		
+
 		return $command;
 	}
 
 	private function runCommand( $row, $action, $params ) {
 		$command = $this->makeCommand( $row, $action, $params );
 		$response = wfShellExec( $command, $result );
-	
+
 		if ( $result !== 0 ) {
 			$res = false;
-			$this->error('Multi task error!', [
+			$this->error( 'Multi task error!', [
 				'action' => $action,
 				'server' => $row->city_server,
 				'exitStatus' => $result,
 				'error' => $response,
-			]);
+			] );
 		}
 		else {
 			$res = true;
-			$this->info('Multi task complete!', [
-				'link' => "{$row->city_server}{$row->city_script}?title=".wfEscapeWikiText($response),
-			]);
+			$this->info( 'Multi task complete!', [
+				'link' => "{$row->city_server}{$row->city_script}?title=" . wfEscapeWikiText( $response ),
+			] );
 		}
-		
+
 		return $res;
 	}
 }
