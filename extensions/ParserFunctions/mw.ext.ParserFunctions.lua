38d897864e10d0829c4e1a0e8d34d07e40b77d50
grunny <mwgrunny@gmail.com>
2015-03-01 00:01:43 +1000

    Spaces to tabs in Lua module

--- extensions/ParserFunctions/mw.ext.ParserFunctions.lua
+++ extensions/ParserFunctions/mw.ext.ParserFunctions.lua
@@ -2,21 +2,21 @@ local ParserFunctions = {}
 local php
 
 function ParserFunctions.expr( expression )
-   return php.expr( expression )
+	return php.expr( expression )
 end
 
 function ParserFunctions.setupInterface( options )
-   -- Boilerplate
-   ParserFunctions.setupInterface = nil
-   php = mw_interface
-   mw_interface = nil
+	-- Boilerplate
+	ParserFunctions.setupInterface = nil
+	php = mw_interface
+	mw_interface = nil
 
-   -- Register this library in the "mw" global
-   mw = mw or {}
-   mw.ext = mw.ext or {}
-   mw.ext.ParserFunctions = ParserFunctions
+	-- Register this library in the "mw" global
+	mw = mw or {}
+	mw.ext = mw.ext or {}
+	mw.ext.ParserFunctions = ParserFunctions
 
-   package.loaded['mw.ext.ParserFunctions'] = ParserFunctions
+	package.loaded['mw.ext.ParserFunctions'] = ParserFunctions
 end
 
 return ParserFunctions
