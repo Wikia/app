79fe0c90079df52e1e546831d8c33ced4fc57d58
adamkarminski <adamkarminski@gmail.com>
2015-03-03 16:15:20 +0100

    CE-1310 Use Fluent SQL

--- extensions/wikia/SpecialPageViews/SpecialPageViewsSourceDatabase.class.php
+++ extensions/wikia/SpecialPageViews/SpecialPageViewsSourceDatabase.class.php
@@ -18,25 +18,27 @@ class SpecialPageViewsSourceDatabase extends SponsorshipDashboardSourceDatabase 
 		$this->serieName = wfMessage( 'special-pageviews-report-serie' )->escaped();
 		$this->setDatabase( wfGetDB( DB_SLAVE, array(), F::app()->wg->DWStatsDB ) );
 
-		$sql = "SELECT pageviews, DATE( time_id ) as pv_date FROM rollup_wiki_pageviews WHERE period_id='1' AND time_id >= '%s' AND time_id <= '%s' AND wiki_id = " . F::app()->wg->CityId . ";";
-		$this->setQuery( $sql );
 		$this->setStartDate( $startDate->format( 'Y-m-d' ) );
 		$this->setEndDate( $endDate->format( 'Y-m-d' ) );
 	}
 
 	protected function getResults() {
 
-		$sql = sprintf( $this->sQuery, $this->startDate, $this->endDate );
-
 		$dbr = $this->getDatabase();
-		$res = $dbr->query( $sql, __METHOD__ );
 
-		while ( $row = $res->fetchObject( $res ) ) {
-			$sDate = $row->pv_date;
-			$sDate = $this->frequency->formatDateByString( $sDate );
-			$this->dataAll[ $sDate ][ 'date' ] = $sDate;
-			$this->dataAll[ $sDate ][ 'a'.md5( $this->serieName ) ] = $row->pageviews;
-		}
+		$aDataContainer = ( new WikiaSQL() )
+			->SELECT( 'pageviews', 'time_id' )
+			->FROM( 'rollup_wiki_pageviews' )
+			->WHERE( 'period_id' )->EQUAL_TO( DataMartService::PERIOD_ID_DAILY )
+			->AND_( 'time_id' )->BETWEEN( $this->startDate, $this->endDate )
+			->AND_( 'wiki_id' )->EQUAL_TO( F::app()->wg->CityId )
+			->runLoop( $dbr, function( &$aDataContainer, $oRow ) {
+				$oDateTime = new DateTime( $oRow->time_id );
+				$sDate = $oDateTime->format( 'Y-m-d' );
+				$sDate = $this->frequency->formatDateByString( $sDate );
+				$this->dataAll[ $sDate ][ 'date' ] = $sDate;
+				$this->dataAll[ $sDate ][ 'a'.md5( $this->serieName ) ] = $oRow->pageviews;
+			} );
 
 		$this->dataTitles[ 'a'.md5( $this->serieName ) ] = $this->serieName;
 	}
