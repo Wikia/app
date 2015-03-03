c24ede04412ecd7c575a52170487c5732f53f6be
nAndy <nandy@wikia-inc.com>
2015-03-03 12:41:42 +0000

    ADEN-1770 Simple unit test for extendPageTargeting()

--- extensions/wikia/AdEngine/js/spec/LookupServices.spec.js
+++ extensions/wikia/AdEngine/js/spec/LookupServices.spec.js
@@ -2,20 +2,20 @@ describe('Method ext.wikia.adEngine.lookupServices', function () {
 	'use strict';
 
 	var logMock = function () { return; },
-		winMock = {},
-		rtpMock = {
-			trackState: function () { return; },
-			wasCalled: function () { return true; },
-			getConfig: function () {
-				return { slotname: ['TOP_LEADERBOARD'] };
-			},
-			getTier: function () {
-				return 6;
-			}
-		};
+		winMock = {};
 
 	it('extends slot targeting for Rubicon', function () {
-		var lookup = modules['ext.wikia.adEngine.lookupServices'](
+		var rtpMock = {
+				trackState: function () { return; },
+				wasCalled: function () { return true; },
+				getConfig: function () {
+					return { slotname: ['TOP_LEADERBOARD'] };
+				},
+				getTier: function () {
+					return 6;
+				}
+			},
+			lookup = modules['ext.wikia.adEngine.lookupServices'](
 				logMock,
 				winMock,
 				rtpMock
@@ -27,25 +27,29 @@ describe('Method ext.wikia.adEngine.lookupServices', function () {
 		expect(slotTargetingMock).toEqual(expectedSlotTargeting);
 	});
 
-	xit('extends page targeting for Amazon', function () {
-		var amazonMatch = modules['ext.wikia.adEngine.amazonMatch']({}, {}, {}, {}),
+	it('extends page targeting for Amazon', function () {
+		var amazonMatchMock = {
+				trackState: function () { return; },
+				wasCalled: function () { return true; },
+				getPageParams: function () {
+					return {
+						amznslots: ['a7x9p5', 'a7x9p11', 'a1x6p6', 'a1x6p5', 'a3x2p9', 'a1x6p14', 'a7x9p6', 'a3x2p14']
+					};
+				},
+				filterSlots: function () {
+					return ['a1x6p5', 'a3x2p9', 'a7x9p5'];
+				}
+			},
 			lookup = modules['ext.wikia.adEngine.lookupServices'](
 				logMock,
 				winMock,
 				undefined,
-				amazonMatch
+				amazonMatchMock
 			),
 			pageTargetingMock = {},
 			expectedPageTargeting = {
 				amznslots: ['a1x6p5', 'a3x2p9', 'a7x9p5']
 			};
-
-		spyOn(amazonMatch, 'trackState').stub();
-		spyOn(amazonMatch, 'wasCalled').returnValue(true);
-		spyOn(amazonMatch, 'getPageParams').returnValue({
-			amznslots: ['a7x9p5', 'a7x9p11', 'a1x6p6', 'a1x6p5', 'a3x2p9', 'a1x6p14', 'a7x9p6', 'a3x2p14']
-		});
-		spyOn(amazonMatch, 'filterSlots').andCallThrough();
 
 		lookup.extendPageTargeting(pageTargetingMock);
 		expect(pageTargetingMock).toEqual(expectedPageTargeting);
