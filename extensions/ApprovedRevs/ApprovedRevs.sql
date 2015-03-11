8c6cbec3cc52e7a41f2913ce7fd892608a2322be
Władysław Bodzek <wladek@wikia-inc.com>
2015-03-11 00:24:07 +0000

    Remove unused extensions/ApprovedRevs

--- extensions/ApprovedRevs/ApprovedRevs.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE /*_*/approved_revs (
-	page_id int default NULL,
-	rev_id int default NULL
-) /*$wgDBTableOptions*/;
-
-CREATE UNIQUE INDEX approved_revs_page_id ON /*_*/approved_revs (page_id);
