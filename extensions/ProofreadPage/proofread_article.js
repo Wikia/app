c6275f74e078a3265e69afc50772f38b23437863
Władysław Bodzek <wladek@wikia-inc.com>
2015-03-11 00:27:24 +0000

    Remove unused extensions/ProofreadPage

--- extensions/ProofreadPage/proofread_article.js
+++
@@ -1,8 +0,0 @@
-// Author : ThomasV - License : GPL
-
-/* add backlink to index page */
-function pr_add_source() {
-	$( '#ca-nstab-main' ).after( '<li id="ca-proofread-source"><span>' + proofreadpage_source_href + '</span></li>' );
-}
-
-jQuery( pr_add_source );
