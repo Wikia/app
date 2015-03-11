3f4430b3bbbf2e48252761aebb607039fd9837e7
Władysław Bodzek <wladek@wikia-inc.com>
2015-03-11 00:24:38 +0000

    Remove unused extensions/CommunityHiring

--- extensions/CommunityHiring/tables.sql
+++
@@ -1,8 +0,0 @@
--- Adds tables for CommunityHiring
-CREATE TABLE /*_*/community_hiring_application (
-	ch_id int unsigned auto_increment,
-	ch_data LONGBLOB,
-	ch_timestamp varbinary(14),
-	ch_ip varbinary(64),
-	primary key (ch_id)
-);
