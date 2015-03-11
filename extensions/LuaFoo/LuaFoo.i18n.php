bac0e4337f8fa1ebe862978327758935db85bb78
Władysław Bodzek <wladek@wikia-inc.com>
2015-03-11 00:26:28 +0000

    Remove unused extensions/LuaFoo

--- extensions/LuaFoo/LuaFoo.i18n.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$messages = array();
-$messages['en'] = array(
-	'luafoo-luatranslation' => 'Translate template to Lua',
-	'luafoo-convert-title' => 'Template to convert to Lua:',
-	'luafoo-convert-submit' => 'Convert',
-);
