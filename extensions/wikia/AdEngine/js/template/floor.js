7624d081bc5641c6f2f151e1f21664c17eeb79ad
Piotr Gabryjeluk <rychu@wikia-inc.com>
2015-03-11 16:37:26 +0100

    ADEN-1401 Extending comment for floor.js show()

--- extensions/wikia/AdEngine/js/template/floor.js
+++ extensions/wikia/AdEngine/js/template/floor.js
@@ -16,9 +16,21 @@ define('ext.wikia.adEngine.template.floor', [
 			'</div>';
 
 	/**
-	 * Show the footer ad
+	 * Show the floor ad.
+	 *
+	 * You need to supply the HTML to put to the floor ad container and the desired dimensions
+	 * of the container. The semi-transparent bar will be always 90px high, so the ad can stick out
+	 * a little bit (for example if the desired container height is 100px).
+	 *
+	 * If you supply the onClick callback the code fires when you click within the iframe.
+	 * Note the standard event bubbling applies, so it's possible some element within the iframe
+	 * stops the event propagation. Flash will stop propagation for sure.
 	 *
 	 * @param {Object} params
+	 * @param {string} params.code HTML code to put into floor container
+	 * @param {number} params.width width of the ad to put into floor container
+	 * @param {number} params.height width of the ad to put into floor container
+	 * @param {number} [params.onClick] function to call when floor iframe is clicked
 	 */
 	function show(params) {
 		log(['show', params], 'debug', logGroup);
