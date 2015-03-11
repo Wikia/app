99c07512e52770f6dc5a8453b91a9ac08b13e4be
Władysław Bodzek <wladek@wikia-inc.com>
2015-03-11 00:26:20 +0000

    Remove unused extensions/LiveTranslate

--- extensions/LiveTranslate/sql/LT_addTMField.sql
+++
@@ -1,6 +0,0 @@
--- Adds the memory_id field to the live_translate table, introduced in version 0.4.
--- Licence: GNU GPL v3+
--- Author: Jeroen De Dauw < jeroendedauw@gmail.com >
-
-ALTER TABLE /*_*/live_translate ADD COLUMN memory_id INT(4) unsigned NOT NULL;
-UPDATE /*_*/live_translate SET memory_id = 1;
