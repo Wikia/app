c4e37efb21f29b2b260d8dece39864e2dfcd4ee8
Władysław Bodzek <wladek@wikia-inc.com>
2015-03-11 00:29:23 +0000

    Remove unused extensions/Wikidata

--- extensions/Wikidata/Database scripts/Incremental/32 - rename user group.sql
+++
@@ -1,2 +0,0 @@
-UPDATE user_groups SET ug_group='wikidata-omega' where ug_group='wikidata';
-INSERT INTO `script_log` (`time`, `script_name`, `comment`) VALUES (NOW(), '32 - rename user group.sql', 'standardize group names for multiple data sets');
