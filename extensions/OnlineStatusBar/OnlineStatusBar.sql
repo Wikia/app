6fed5574e83743353db8f444e968e4b988b8af48
Władysław Bodzek <wladek@wikia-inc.com>
2015-03-11 00:26:57 +0000

    Remove unused extensions/OnlineStatusBar

--- extensions/OnlineStatusBar/OnlineStatusBar.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE /*$wgDBprefix*/online_status (
-	`username` varchar(255) NOT NULL default '',
-	`timestamp` binary(14) NOT NULL default '19700101000000',
-	PRIMARY KEY USING HASH (`username`)
-) ENGINE=MEMORY;
-
