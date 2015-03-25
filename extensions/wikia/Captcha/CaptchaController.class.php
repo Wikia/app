427f80834653cda45c6c1db573c625dd4cc493f5
Damian Jozwiak <damian@wikia-inc.com>
2015-03-25 21:43:10 +0100

    SOC-525 | Cleanup in CaptchaController

--- extensions/wikia/Captcha/CaptchaController.class.php
+++ extensions/wikia/Captcha/CaptchaController.class.php
@@ -5,18 +5,12 @@
  */
 class CaptchaController extends WikiaController {
 
-	const DEFAULT_TEMPLATE_ENGINE = WikiaResponse::TEMPLATE_ENGINE_MUSTACHE;
-
 	/**
 	 * Displays a captcha image. This is used exclusively by FancyCaptcha.
 	 */
 	public function showImage() {
 		$fancyCaptcha = new \Captcha\Module\FancyCaptcha();
-		if ( !$fancyCaptcha->showImage() ) {
-			$this->response->setData( [
-				'error' => wfMessage( 'captcha-no-image' )->escaped(),
-			] );
-		}
+		$fancyCaptcha->showImage();
 	}
 
 	/**
@@ -28,12 +22,5 @@ class CaptchaController extends WikiaController {
 		$this->response->setData(
 			[ 'form' => $fancyCaptcha->getForm() ]
 		);
-	}
-
-	/**
-	 * Display information about how this captcha works
-	 */
-	public function showHelp() {
-		\Captcha\Factory\Module::getInstance()->showHelp();
 	}
 }
