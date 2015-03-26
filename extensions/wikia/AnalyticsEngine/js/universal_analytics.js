5929b9e93993ba978ad67ff4018796d1f4158331
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-03-26 13:26:02 +0100

    Fixed superlous parameter conversion in guaTrackEvent().

--- extensions/wikia/AnalyticsEngine/js/universal_analytics.js
+++ extensions/wikia/AnalyticsEngine/js/universal_analytics.js
@@ -416,9 +416,6 @@
      */
     window.guaTrackEvent = function (category, action, opt_label, opt_value, opt_noninteractive) {
         var args = Array.prototype.slice.call(arguments);
-        if (args.length === 5) {
-            args[4] = {'nonInteraction': args[4]};
-        }
 
         args.unshift('send', 'event');
         try {
