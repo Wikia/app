e559409e6b0718fced530451d1ed9ae7f8e29c1b
Bogna Knychala <bogna.ka@gmail.com>
2015-03-26 11:57:51 +0100

    CONCF-189 add line social network

--- extensions/wikia/PageShare/PageShareHelper.class.php
+++ extensions/wikia/PageShare/PageShareHelper.class.php
@@ -1,36 +1,20 @@
 <?php
-class IconFileNotFoundException extends WikiaBaseException {}
-
 class PageShareHelper {
 	private static function readIcon( $fileName ) {
 		$fullName = realpath( __DIR__ . '/icons/' . $fileName . '.svg' );
 		if ( is_readable( $fullName ) ) {
 			return file_get_contents( $fullName );
 		} else {
-			throw new IconFileNotFoundException("Icon not found inside PageShare: icons/{$fileName}.svg");
+			return false;
 		}
 	}
 
 	public static function getIcon( $service ) {
-		switch ($service) {
-			case 'facebook':
-				return self::readIcon('facebook');
-			case 'googleplus':
-				return self::readIcon('googleplus');
-			case 'twitter':
-				return self::readIcon('twitter');
-			case 'reddit':
-				return self::readIcon('reddit');
-			case 'tumblr':
-				return self::readIcon('tumblr');
-			case 'vkontakte':
-				return self::readIcon('vkontakte');
-			case 'qzone':
-				return self::readIcon('qzone');
-			default:
-				// default share icon
-				return self::readIcon('share');
+		$icon = self::readIcon( $service );
+		if ( !$icon ) {
+			$icon = self::readIcon( 'share' );
 		}
+		return $icon;
 	}
 
 	public static function isValidShareService( $service, $lang ) {
