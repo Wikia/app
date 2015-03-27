7168c9918cff143f6f5ba22ec444c0c1c443a63e
nAndy <nandy@wikia-inc.com>
2015-03-27 20:29:05 +0100

    aden-1834: added possibility to add more custom classes to an iframe

--- resources/wikia/modules/iframeWriter.js
+++ resources/wikia/modules/iframeWriter.js
@@ -17,21 +17,23 @@ define('wikia.iframeWriter', [
 	 * @param {string} params.code - code to put into the iframe
 	 * @param {number} params.width - desired width of the iframe
 	 * @param {number} params.height - desired height of the iframe
-	 * @param {string} [params.class] - custom class of the iframe
+	 * @param {array} [params.classes] - custom classes of the iframe
 	 */
 	function getIframe(params) {
 		log(['getIframe', params], 'debug', logGroup);
 
 		var code = iframeHeader + iframeStyle + params.code + iframeFooter,
 			iframe = doc.createElement('iframe');
-
+		
 		iframe.frameborder = 'no';
 		iframe.scrolling = 'no';
 		iframe.width = params.width;
 		iframe.height = params.height;
 
-		if (params.class && iframe.classList) {
-			iframe.classList.add(params.class);
+		if (params.classes && iframe.classList) {
+			Object.keys(params.classes).forEach(function (k) {
+				iframe.classList.add(params.classes[k]);
+			});
 		}
 
 		iframe.onload = function () {
