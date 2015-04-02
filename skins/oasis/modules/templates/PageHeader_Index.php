0b47bd0cc727e55c1317cc35f3a121ce27e10830
Warkot <mateusz.warkocki@wikia-inc.com>
2015-04-02 15:42:20 +0200

    CONCF-189 | Spaces! Spaces everywhere!

--- skins/oasis/modules/templates/PageHeader_Index.php
+++ skins/oasis/modules/templates/PageHeader_Index.php
@@ -1,66 +1,67 @@
 <?php
-	if ( !empty( $wg->EnableMonetizationModuleExt ) && !empty( $monetizationModules[MonetizationModuleHelper::SLOT_TYPE_ABOVE_TITLE] ) ) {
-		echo $monetizationModules[MonetizationModuleHelper::SLOT_TYPE_ABOVE_TITLE];
-	}
+if ( !empty( $wg->EnableMonetizationModuleExt ) && !empty( $monetizationModules[ MonetizationModuleHelper::SLOT_TYPE_ABOVE_TITLE ] ) ) {
+	echo $monetizationModules[ MonetizationModuleHelper::SLOT_TYPE_ABOVE_TITLE ];
+}
 
-	if ( ( !empty( $wg->EnableNjordExt ) && WikiaPageType::isMainPage() ) ) {
-		// edit button with actions dropdown
-		if (!empty($action)) {
-			echo F::app()->renderView('MenuButton', 'Index', array('action' => $action, 'image' => $actionImage, 'dropdown' => $dropdown, 'name' => $actionName));
-		}
-	} else {
-?>
-		<header id="WikiaPageHeader" class="WikiaPageHeader wikia-page-header">
-			<div class="header-container">
-				<div class="header-column header-title">
-					<h1><?= !empty($displaytitle) ? $title : htmlspecialchars($title) ?></h1>
-					<?php if (!empty($pageSubtitle)): ?>
-						<h2><?= $pageSubtitle ?></h2>
-					<? endif; if (!empty($subtitle)): ?>
-						<div class="subtitle"><?= $subtitle ?></div>
-					<? endif; ?>
-				</div>
-				<div class="header-column header-tally">
-					<?php if ( !empty( $wg->EnablePageShareExt ) && ( !empty( $wg->EnablePageShareWorldwide ) || ($wg->Lang->getCode() == 'en'))) {
-						echo F::app()->renderView('PageShare', 'index');
-					} ?>
-					<? if (!is_null($tallyMsg)): ?>
-						<div class="tally"><?= $tallyMsg ?></div>
-					<? endif; ?>
-				</div>
+if ( ( !empty( $wg->EnableNjordExt ) && WikiaPageType::isMainPage() ) ) {
+	// edit button with actions dropdown
+	if ( !empty( $action ) ) {
+		echo F::app()->renderView( 'MenuButton', 'Index', array( 'action' => $action, 'image' => $actionImage, 'dropdown' => $dropdown, 'name' => $actionName ) );
+	}
+} else {
+	?>
+	<header id="WikiaPageHeader" class="WikiaPageHeader wikia-page-header">
+		<div class="header-container">
+			<div class="header-column header-title">
+				<h1><?= !empty( $displaytitle ) ? $title : htmlspecialchars( $title ) ?></h1>
+				<?php if ( !empty( $pageSubtitle ) ): ?>
+					<h2><?= $pageSubtitle ?></h2>
+				<? endif;
+				if ( !empty( $subtitle ) ): ?>
+					<div class="subtitle"><?= $subtitle ?></div>
+				<? endif; ?>
 			</div>
+			<div class="header-column header-tally">
+				<?php if ( !empty( $wg->EnablePageShareExt ) && ( !empty( $wg->EnablePageShareWorldwide ) || ( $wg->Lang->getCode() == 'en' ) ) ) {
+					echo F::app()->renderView( 'PageShare', 'index' );
+				} ?>
+				<? if ( !is_null( $tallyMsg ) ): ?>
+					<div class="tally"><?= $tallyMsg ?></div>
+				<? endif; ?>
+			</div>
+		</div>
 		<?php
 		// edit button with actions dropdown
-		if (!empty($action)) {
-			echo F::app()->renderView('MenuButton', 'Index', array('action' => $action, 'image' => $actionImage, 'dropdown' => $dropdown, 'name' => $actionName));
+		if ( !empty( $action ) ) {
+			echo F::app()->renderView( 'MenuButton', 'Index', array( 'action' => $action, 'image' => $actionImage, 'dropdown' => $dropdown, 'name' => $actionName ) );
 		}
 
 		// TODO: use PageHeaderIndexExtraButtons hook for these buttons
 		// "Add a photo" button
-		if (!empty($isNewFiles) && !empty($wg->EnableUploads)) {
-			echo Wikia::specialPageLink('Upload', 'oasis-add-photo', 'wikia-button upphotos', 'blank.gif', 'oasis-add-photo-to-wiki', 'sprite photo');
+		if ( !empty( $isNewFiles ) && !empty( $wg->EnableUploads ) ) {
+			echo Wikia::specialPageLink( 'Upload', 'oasis-add-photo', 'wikia-button upphotos', 'blank.gif', 'oasis-add-photo-to-wiki', 'sprite photo' );
 		}
 
 		// "Add a video" button
-		if (!empty($isSpecialVideos) && !empty($wg->EnableUploads) && $showAddVideoBtn): ?>
-			<a class="button addVideo" href="#" rel="tooltip" title="<?= wfMsg('related-videos-tooltip-add'); ?>">
-				<img src="<?= wfBlankImgUrl(); ?>" class="sprite addRelatedVideo"/> <?= wfMsg('videos-add-video') ?>
+		if ( !empty( $isSpecialVideos ) && !empty( $wg->EnableUploads ) && $showAddVideoBtn ): ?>
+			<a class="button addVideo" href="#" rel="tooltip" title="<?= wfMsg( 'related-videos-tooltip-add' ); ?>">
+				<img src="<?= wfBlankImgUrl(); ?>" class="sprite addRelatedVideo"/> <?= wfMsg( 'videos-add-video' ) ?>
 			</a>
 		<? endif;
 
 		// comments & like button
-		if (!$isWallEnabled) {
-			echo F::app()->renderView('CommentsLikes', 'Index', array('comments' => $comments));
+		if ( !$isWallEnabled ) {
+			echo F::app()->renderView( 'CommentsLikes', 'Index', array( 'comments' => $comments ) );
 		}
 
-		foreach ($extraButtons as $button) {
+		foreach ( $extraButtons as $button ) {
 			echo $button;
 		}
 		?>
 	</header>
 	<?php
-	if (!empty($wg->EnableMonetizationModuleExt) && !empty($monetizationModules[MonetizationModuleHelper::SLOT_TYPE_BELOW_TITLE])) {
-		echo $monetizationModules[MonetizationModuleHelper::SLOT_TYPE_BELOW_TITLE];
+	if ( !empty( $wg->EnableMonetizationModuleExt ) && !empty( $monetizationModules[ MonetizationModuleHelper::SLOT_TYPE_BELOW_TITLE ] ) ) {
+		echo $monetizationModules[ MonetizationModuleHelper::SLOT_TYPE_BELOW_TITLE ];
 	}
 }
 ?>
