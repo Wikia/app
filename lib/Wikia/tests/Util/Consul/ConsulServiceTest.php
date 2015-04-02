90e23e77ba4da0c1164818983e042968879e0851
jacek <jacek@wikia-inc.com>
2015-04-02 14:13:33 +0200

    reformat

--- lib/Wikia/tests/Util/Consul/ConsulServiceTest.php
+++ lib/Wikia/tests/Util/Consul/ConsulServiceTest.php
@@ -5,8 +5,8 @@ class ConsulServiceTest extends PHPUnit_Framework_TestCase {
 
 	public function testGetConsulServiceName() {
 
-		$consulConfig = new ConsulConfig("sjc", "testing", "testtag");
-		$this->assertTrue( $consulConfig->getConsulServiceName() == 'testtag.testing.service.sjc.consul');
+		$consulConfig = new ConsulConfig( "sjc", "testing", "testtag" );
+		$this->assertTrue( $consulConfig->getConsulServiceName() == 'testtag.testing.service.sjc.consul' );
 	}
 
 	/**
@@ -15,32 +15,27 @@ class ConsulServiceTest extends PHPUnit_Framework_TestCase {
 	 */
 	public function testResolve( $doResolveResponse ) {
 
-		$consulConfig = new ConsulConfig("sjc", "infobox", "testing");
-		$consulService = $this->getMockBuilder('\Wikia\Util\Consul\ConsulService')
-			->setConstructorArgs([$consulConfig])
-			->setMethods(['doResolve'])
+		$consulConfig = new ConsulConfig( "sjc", "infobox", "testing" );
+		$consulService = $this->getMockBuilder( '\Wikia\Util\Consul\ConsulService' )
+			->setConstructorArgs( [ $consulConfig ] )
+			->setMethods( [ 'doResolve' ] )
 			->getMock();
 
 		$consulService->expects( $this->any() )
-			->method('doResolve')
-			->will( $this->returnValue(	$doResolveResponse ) );
+			->method( 'doResolve' )
+			->will( $this->returnValue( $doResolveResponse ) );
 
 		$resolved = $consulService->resolve();
 
-		if ( empty($doResolveResponse) ) {
+		if ( empty( $doResolveResponse ) ) {
 			$this->assertTrue( $resolved === false );
 		} else {
-			$this->assertTrue( $resolved['host'] == "test-host" );
-			$this->assertTrue( $resolved['port'] == 123 );
+			$this->assertTrue( $resolved[ 'host' ] == "test-host" );
+			$this->assertTrue( $resolved[ 'port' ] == 123 );
 		}
 	}
 
 	public function sampleDoResolveResponse() {
-		return [
-			[ [['target' => 'test-host', 'port'=>123]] ],
-			[ [] ],
-			[ false ],
-			[ null ]
-		];
+		return [ [ [ [ 'target' => 'test-host', 'port' => 123 ] ] ], [ [ ] ], [ false ], [ null ] ];
 	}
 }
