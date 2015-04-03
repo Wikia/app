fb3178c76707f744745583f3211b53ab4b0d20f3
Bogna Knychala <bogna.ka@gmail.com>
2015-04-03 16:11:51 +0200

    CONCF-414 fix white spaces

--- extensions/wikia/PageShare/PageShareHooks.class.php
+++ extensions/wikia/PageShare/PageShareHooks.class.php
@@ -7,12 +7,12 @@ class PageShareHooks {
 	 * This hook should be removed when we decide to enable PageShare globally.
 	 * More or less the same time when $wgEnablePageShareWorldwide is going to be set to true by default or removed
 	 */
-	public static function onBeforePageDisplay(OutputPage &$out, Skin &$skin) {
+	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 		global $wgEnablePageShareExt, $wgEnablePageShareWorldwide;
 
 		$lang = PageShareHelper::getLangForPageShare();
 
-		if ( empty($wgEnablePageShareWorldwide) && strtolower($lang) != PageShareHelper::SHARE_DEFAULT_LANGUAGE ) {
+		if ( empty( $wgEnablePageShareWorldwide ) && strtolower( $lang ) != PageShareHelper::SHARE_DEFAULT_LANGUAGE ) {
 			$wgEnablePageShareExt = false;
 		}
 
