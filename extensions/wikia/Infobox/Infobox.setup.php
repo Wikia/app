c4dcb5790bc0056bbe43ce323973169a6e27083d
Adam Robak <adamr@wikia-inc.com>
2015-04-08 14:44:43 +0200

    remove unused directory

--- extensions/wikia/Infobox/Infobox.setup.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$dir = dirname( __FILE__ );
-
-$wgAutoloadClasses[ 'InfoboxHooks' ] = $dir . '/InfoboxHooks.class.php';
-
-$wgHooks[ 'ParserFirstCallInit' ][ ] = 'InfoboxHooks::onParserFirstCallInit';
