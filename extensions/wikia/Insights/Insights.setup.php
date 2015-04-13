a4aacdde2a694c1e688e14f7dee46a2d28f992fd
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-04-13 17:25:40 +0000

    CE-1594: Insights ext setup

---
+++ extensions/wikia/Insights/Insights.setup.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Insights
+ *
+ * @author Łukasz Konieczny *
+ */
+
+$dir = dirname(__FILE__) . '/';
+
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'Insights',
+	'descriptionmsg' => 'insights-desc',
+	'authors' => array(
+		'Łukasz Konieczny',
+	),
+	'version' => 1.0,
+	'url' => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/Insights'
+);
+
+//classes
+$wgAutoloadClasses['InsightsController'] = $dir . 'InsightsController.class.php';
+
+// hooks
+
+//special page
+$wgSpecialPages['Insights'] = 'InsightsController';
+$wgSpecialPageGroups['EditHub'] = 'wikia';
+
+//message files
+$wgExtensionMessagesFiles['Insights'] = $dir . 'Insights.i18n.php';
+
