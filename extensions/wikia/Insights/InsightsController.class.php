c64fd3c8d8438f2aa05d11b330b97513bb43e87f
adamkarminski <adamkarminski@gmail.com>
2015-04-14 17:40:49 -0700

    CE-1606 Basic list front-end

--- extensions/wikia/Insights/InsightsController.class.php
+++ extensions/wikia/Insights/InsightsController.class.php
@@ -10,16 +10,29 @@ class InsightsController extends WikiaSpecialPageController {
 	public function index() {
 		wfProfileIn(__METHOD__);
 
-		$this->page = $this->getInsightDataProvider( $this->getPar() );
+		$this->key = $this->getPar();
+		$this->page = $this->getInsightDataProvider( $this->key );
 		$this->wg->Out->setPageTitle( wfMessage( 'insights' )->escaped() );
 
 		$this->response->addAsset('/extensions/wikia/Insights/styles/insights.scss');
 
-		if ( !empty( $this->page ) ) {
-			$this->categoryList();
+		if ( !empty( $this->key ) ) {
+			$this->renderSubpage();
 		}
 
 		wfProfileOut(__METHOD__);
+	}
+
+	public function renderSubpage() {
+		$model = new QueryPagesModel( $this->page, $this->wg->CityId );
+
+		$this->iconUrl = 'https://pbs.twimg.com/profile_images/583867858683166720/netMDLKF.png';
+		$this->subtitle = wfMessage( 'insights-list-' . $this->key . '-subtitle' )->parse();
+		$this->description = wfMessage( 'insights-list-' . $this->key . '-description' )->parse();
+		$this->list = $model->getList();
+		$this->offset = 0;
+
+		$this->overrideTemplate( 'subpageList' );
 	}
 
 	public function categoryList() {
