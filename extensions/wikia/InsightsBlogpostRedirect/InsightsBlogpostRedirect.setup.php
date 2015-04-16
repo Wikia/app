04e4d238e56103f201186c1bd4467e4fd77148ec
Kamil Koterba <kamil@wikia-inc.com>
2015-04-16 12:57:30 -0700

    CE-1626 Adding extension that places redirect on Special:Insights to community blogpost

---
+++ extensions/wikia/InsightsBlogpostRedirect/InsightsBlogpostRedirect.setup.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * InsightsBlogpostRedirect
+ *
+ * @author Kamil Koterba <kamil@wikia-inc.com>
+ */
+
+$dir = dirname(__FILE__) . '/';
+
+$wgExtensionCredits['specialpage'][] = [
+	'name' => 'InsightsBlogpostRedirect',
+	'descriptionmsg' => 'insightsblogpostredirect-desc',
+	'authors' => [
+		'Kamil Koterba <kamil@wikia-inc.com>',
+	],
+	'version' => 1.0,
+	'url' => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/InsightsBlogpostRedirect'
+];
+
+//classes
+$wgAutoloadClasses['InsightsBlogpostRedirectController'] = $dir . 'InsightsBlogpostRedirectController.class.php';
+
+//special page
+$wgSpecialPages['Insights'] = 'InsightsBlogpostRedirectController';
+$wgSpecialPageGroups['EditHub'] = 'wikia';
+
+//message files
+$wgExtensionMessagesFiles['InsightsBlogpostRedirect'] = $dir . 'InsightsBlogpostRedirect.i18n.php';
