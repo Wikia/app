842e4f824615b2fbe64536be37d16893a65b71b6
nAndy <nandy@wikia-inc.com>
2015-04-17 14:13:41 -0700

    ADEN-1676 CR changes: the right size for the slot is 1x1

--- extensions/wikia/AdEngine/js/provider/directGptMobile.js
+++ extensions/wikia/AdEngine/js/provider/directGptMobile.js
@@ -9,7 +9,7 @@ define('ext.wikia.adEngine.provider.directGptMobile', [
 		'DirectGptMobile',
 		'mobile',
 		{
-			INVISIBLE_HIGH_IMPACT:      {size: '320x50,320x100'},
+			INVISIBLE_HIGH_IMPACT:      {size: '1x1'},
 			MOBILE_TOP_LEADERBOARD:     {size: '320x50,320x100,1x1'},
 			MOBILE_IN_CONTENT:          {size: '300x250,1x1'},
 			MOBILE_PREFOOTER:           {size: '300x250,1x1'}
