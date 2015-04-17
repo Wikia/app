865726f560a0c124364f3c4a62fa5554a10ba046
nAndy <nandy@wikia-inc.com>
2015-04-17 16:41:14 -0700

    ADEN-1947 inject code on the top of an article

--- extensions/wikia/PaidAssetDrop/js/PaidAssetDrop.desktop.js
+++ extensions/wikia/PaidAssetDrop/js/PaidAssetDrop.desktop.js
@@ -1,9 +1,33 @@
 define('ext.wikia.PaidAssetDrop', [
+	'jquery',
 	'wikia.log'
-], function (log) {
+], function ($, log) {
 	'use strict';
 
-	var logGroup = 'ext.wikia.PaidAssetDrop';
+	var logGroup = 'ext.wikia.PaidAssetDrop',
+		articleContentId = '#mw-content-text';
 
 	log('Paid Asset Drop enabled', 'info', logGroup);
+
+	function isTodayValid() {
+		//TODO: change me/finish me!
+		return true;
+	}
+
+	function injectPad() {
+		$(articleContentId).prepend('<h1>PAD test</h1>');
+	}
+
+	return {
+		isTodayValid: isTodayValid,
+		injectPAD: injectPad
+	};
+});
+
+require(['ext.wikia.PaidAssetDrop'], function(pad) {
+	'use strict';
+
+	if (pad.isTodayValid()) {
+		pad.injectPAD();
+	}
 });
