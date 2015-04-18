260df706eb9366ec92bf060a377b50c74aa578c2
nAndy <nandy@wikia-inc.com>
2015-04-17 18:01:43 -0700

    ADEN-1947 fetch the desktop pad and display

--- extensions/wikia/PaidAssetDrop/js/PaidAssetDrop.desktop.js
+++ extensions/wikia/PaidAssetDrop/js/PaidAssetDrop.desktop.js
@@ -1,21 +1,66 @@
 define('ext.wikia.PaidAssetDrop', [
 	'jquery',
-	'wikia.log'
-], function ($, log) {
+	'wikia.log',
+	'wikia.window'
+], function ($, log, win) {
 	'use strict';
 
 	var logGroup = 'ext.wikia.PaidAssetDrop',
-		articleContentId = '#mw-content-text';
+		articleContentId = '#mw-content-text',
+		assetArticleName = 'MediaWiki:PAD_desktop.html',
+		apiEntryPoint = 'api.php?action=query&prop=revisions&rvlimit=1&rvprop=content&format=json&titles=';
 
-	log('Paid Asset Drop enabled', 'info', logGroup);
+	log('Paid Asset Drop enabled', 'debug', logGroup);
 
 	function isTodayValid() {
 		//TODO: change me/finish me!
 		return true;
 	}
 
+	function fetchPadContent(response) {
+		var page, revision;
+
+		if (response.query && response.query.pages && Object.keys) {
+			page = response.query.pages[Object.keys(response.query.pages)[0]];
+
+			log('Found page', 'debug', logGroup);
+			log(page, 'debug', logGroup);
+
+			if (page.revisions) {
+				revision = page.revisions.pop();
+
+				log('Found revision', 'debug', logGroup);
+				log(revision, 'debug', logGroup);
+
+				if (revision['*'] ) {
+					return revision['*'];
+				} else {
+					log('Could not find revision[*]', 'debug', logGroup);
+					return null;
+				}
+			} else {
+				log('Could not find page revisions', 'debug', logGroup);
+				return null;
+			}
+		}
+
+		log('Could not find paid asset page', 'debug', logGroup);
+		return null;
+	}
+
 	function injectPad() {
-		$(articleContentId).prepend('<h1>PAD test</h1>');
+		var url = [win.wgServer, '/', apiEntryPoint, assetArticleName].join(''),
+			padContent;
+
+		log('Sending request to: ' + url, 'debug', logGroup);
+
+		$.get(url).then(function (response) {
+			padContent = fetchPadContent(response);
+
+			if (padContent !== null) {
+				$(articleContentId).prepend(padContent);
+			}
+		});
 	}
 
 	return {
@@ -24,10 +69,13 @@ define('ext.wikia.PaidAssetDrop', [
 	};
 });
 
-require(['ext.wikia.PaidAssetDrop'], function(pad) {
+require([
+	'ext.wikia.PaidAssetDrop',
+	'wikia.window'
+], function(pad, win) {
 	'use strict';
 
-	if (pad.isTodayValid()) {
+	if (pad.isTodayValid() && win.wgEnableAPI) {
 		pad.injectPAD();
 	}
 });
