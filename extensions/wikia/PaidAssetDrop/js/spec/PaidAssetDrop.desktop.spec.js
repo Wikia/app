47d479ded93ca21410edb510c3c3874b4cb2d228
nAndy <nandy@wikia-inc.com>
2015-04-20 17:23:37 -0700

    ADEN-1947 renamed global variable

--- extensions/wikia/PaidAssetDrop/js/spec/PaidAssetDrop.desktop.spec.js
+++ extensions/wikia/PaidAssetDrop/js/spec/PaidAssetDrop.desktop.spec.js
@@ -16,63 +16,63 @@ describe('ext.wikia.PaidAssetDrop', function () {
 		win: {}
 	};
 
-	it('today is not valid when wgPaidAssetDrop is not set', function () {
+	it('today is not valid when wgPADConfig is not set', function () {
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 
-	it('today is not valid when wgPaidAssetDrop start day is not set', function () {
-		mocks.win.wgPaidAssetDrop = [];
+	it('today is not valid when wgPADConfig start day is not set', function () {
+		mocks.win.wgPADConfig = [];
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 
-	it('today is not valid when wgPaidAssetDrop start day is not set', function () {
-		mocks.win.wgPaidAssetDrop = ['2015-04-12'];
+	it('today is not valid when wgPADConfig start day is not set', function () {
+		mocks.win.wgPADConfig = ['2015-04-12'];
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 
 	it('today is not valid when it is before start date', function () {
-		mocks.win.wgPaidAssetDrop = ['2015-04-14', '2015-04-21'];
+		mocks.win.wgPADConfig = ['2015-04-14', '2015-04-21'];
 		jasmine.clock().mockDate(new Date('2015-04-13'));
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 
 	it('today is not valid when it is after end date', function () {
-		mocks.win.wgPaidAssetDrop = ['2015-04-14', '2015-04-21'];
+		mocks.win.wgPADConfig = ['2015-04-14', '2015-04-21'];
 		jasmine.clock().mockDate(new Date('2015-04-22'));
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 
 	it('today is valid', function () {
-		mocks.win.wgPaidAssetDrop = ['2015-04-14', '2015-04-21'];
+		mocks.win.wgPADConfig = ['2015-04-14', '2015-04-21'];
 		jasmine.clock().mockDate(new Date('2015-04-20'));
 		expect(getModule().isTodayValid()).toEqual(true);
 	});
 
 	it('today is not valid when it is before start date (case with time)', function () {
-		mocks.win.wgPaidAssetDrop = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
+		mocks.win.wgPADConfig = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
 		jasmine.clock().mockDate(new Date('2015-04-14T10:00:00Z'));
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 
 	it('today is not valid when it is after end date (case with time)', function () {
-		mocks.win.wgPaidAssetDrop = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
+		mocks.win.wgPADConfig = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
 		jasmine.clock().mockDate(new Date('2015-04-14T22:00:00Z'));
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 
 	it('today is valid (case with time)', function () {
-		mocks.win.wgPaidAssetDrop = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
+		mocks.win.wgPADConfig = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
 		jasmine.clock().mockDate(new Date('2015-04-14T16:00:00Z'));
 		expect(getModule().isTodayValid()).toEqual(true);
 	});
 
 	it('today is invalid when both dates are invalid', function () {
-		mocks.win.wgPaidAssetDrop = ['foo', 'bar'];
+		mocks.win.wgPADConfig = ['foo', 'bar'];
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 
 	it('today is invalid when end date is invalid', function () {
-		mocks.win.wgPaidAssetDrop = ['2015-04-20', 'bar'];
+		mocks.win.wgPADConfig = ['2015-04-20', 'bar'];
 		expect(getModule().isTodayValid()).toEqual(false);
 	});
 });
