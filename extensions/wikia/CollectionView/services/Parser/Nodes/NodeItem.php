3d96340f1d5c9e3e4780c8aa9d877bec9295ae42
Igor Rogatty <igor@wikia-inc.com>
2015-04-21 14:58:20 +0200

    Resolve article name to local URL

--- extensions/wikia/CollectionView/services/Parser/Nodes/NodeItem.php
+++ extensions/wikia/CollectionView/services/Parser/Nodes/NodeItem.php
@@ -7,7 +7,7 @@ class NodeItem extends Node {
 		$node = [];
 		$node['description'] = $this->parseWithExternalParser((string) ( $this->xmlNode->{'description'} ), false);
 		$node['img'] = $this->resolveImageUrl( $this->getXmlAttribute( $this->xmlNode, 'img' ) );
-		$node['link'] = $this->getXmlAttribute( $this->xmlNode, 'link' );
+		$node['link'] = $this->resolveArticleUrl( $this->getXmlAttribute( $this->xmlNode, 'link' ) );
 		return $node;
 	}
 
@@ -19,6 +19,14 @@ class NodeItem extends Node {
 		$title = \Title::newFromText( $filename, NS_FILE );
 		if ( $title && $title->exists() ) {
 			return \WikiaFileHelper::getFileFromTitle($title)->getUrlGenerator()->url();
+		}
+		return '';
+	}
+
+	protected function resolveArticleUrl( $titleStr ) {
+		$title = \Title::newFromText( $titleStr );
+		if ( $title && $title->exists() ) {
+			return $title->getLocalURL();
 		}
 		return '';
 	}
