f9bd9d4228a37e6107a46674d76561a702e76f7d
nAndy <nandy@wikia-inc.com>
2015-04-20 23:28:01 -0700

    ADEN-1947 display PAD only on pages in main namespace

--- extensions/wikia/PaidAssetDrop/js/run/desktop.run.js
+++ extensions/wikia/PaidAssetDrop/js/run/desktop.run.js
@@ -1,14 +1,21 @@
 require([
 	'ext.wikia.paidAssetDrop.paidAssetDrop',
+	'wikia.log',
 	'wikia.window'
 ], function(
 	pad,
+	log,
 	win
 ) {
 	'use strict';
 
 	// Everything starts after content and JS
 	win.wgAfterContentAndJS.push(function () {
+		if (win.wgNamespaceNumber !== 0) {
+			log('PAD: Not a main namespace', 'debug', 'ext.wikia.paidAssetDrop.paidAssetDrop');
+			return;
+		}
+
 		if (pad.isNowValid() && win.wgEnableAPI) {
 			pad.injectPAD();
 		}
