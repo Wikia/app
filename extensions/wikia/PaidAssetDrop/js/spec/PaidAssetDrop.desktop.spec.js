7428b5be48394257ad6ca921ab9878373f012e69
nAndy <nandy@wikia-inc.com>
2015-04-21 10:00:41 -0700

    ADEN-1947 added one more unit test and changed descriptions of tests

--- extensions/wikia/PaidAssetDrop/js/spec/PaidAssetDrop.desktop.spec.js
+++ extensions/wikia/PaidAssetDrop/js/spec/PaidAssetDrop.desktop.spec.js
@@ -9,85 +9,83 @@ describe('ext.wikia.paidAssetDrop.paidAssetDrop', function () {
 	var mocks = {
 		jquery: {},
 		log: noop,
-		querystring: function () {
-			return {
-				getVal: noop
-			};
+		qs: {
+			getVal: noop
+		},
+		Querystring: function () {
+			return mocks.qs;
 		},
 		win: {}
 	};
 
 	function getModule() {
-		return modules['ext.wikia.paidAssetDrop.paidAssetDrop'](mocks.jquery, mocks.log, mocks.querystring, mocks.win);
+		return modules['ext.wikia.paidAssetDrop.paidAssetDrop'](mocks.jquery, mocks.log, mocks.Querystring, mocks.win);
 	}
 
-	it('today is not valid when wgPaidAssetDropConfig is not set', function () {
+	it('now is not valid when wgPaidAssetDropConfig is not set', function () {
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
-	it('today is not valid when wgPaidAssetDropConfig start day is not set', function () {
+	it('now is not valid when wgPaidAssetDropConfig start day is not set', function () {
 		mocks.win.wgPaidAssetDropConfig = [];
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
-	it('today is not valid when wgPaidAssetDropConfig start day is not set', function () {
+	it('now is not valid when wgPaidAssetDropConfig start day is not set', function () {
 		mocks.win.wgPaidAssetDropConfig = ['2015-04-12'];
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
-	it('today is not valid when it is before start date', function () {
+	it('now is not valid when it is before start date', function () {
 		mocks.win.wgPaidAssetDropConfig = ['2015-04-14', '2015-04-21'];
 		jasmine.clock().mockDate(new Date('2015-04-13'));
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
-	it('today is not valid when it is after end date', function () {
+	it('now is not valid when it is after end date', function () {
 		mocks.win.wgPaidAssetDropConfig = ['2015-04-14', '2015-04-21'];
 		jasmine.clock().mockDate(new Date('2015-04-22'));
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
-	it('today is valid', function () {
+	it('now is valid', function () {
 		mocks.win.wgPaidAssetDropConfig = ['2015-04-14', '2015-04-21'];
 		jasmine.clock().mockDate(new Date('2015-04-20'));
 		expect(getModule().isNowValid()).toEqual(true);
 	});
 
-	it('today is not valid when it is before start date (case with time)', function () {
+	it('now is not valid when it is before start date (case with time)', function () {
 		mocks.win.wgPaidAssetDropConfig = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
 		jasmine.clock().mockDate(new Date('2015-04-14T10:00:00Z'));
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
-	it('today is not valid when it is after end date (case with time)', function () {
+	it('now is not valid when it is after end date (case with time)', function () {
 		mocks.win.wgPaidAssetDropConfig = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
 		jasmine.clock().mockDate(new Date('2015-04-14T22:00:00Z'));
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
-	it('today is valid (case with time)', function () {
+	it('now is valid (case with time)', function () {
 		mocks.win.wgPaidAssetDropConfig = ['2015-04-14T12:00:00Z', '2015-04-14T20:00:00Z'];
 		jasmine.clock().mockDate(new Date('2015-04-14T16:00:00Z'));
 		expect(getModule().isNowValid()).toEqual(true);
 	});
 
-	it('today is invalid when both dates are invalid', function () {
+	it('now is invalid when both dates are invalid', function () {
 		mocks.win.wgPaidAssetDropConfig = ['foo', 'bar'];
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
-	it('today is invalid when end date is invalid', function () {
+	it('now is invalid when end date is invalid', function () {
 		mocks.win.wgPaidAssetDropConfig = ['2015-04-20', 'bar'];
 		expect(getModule().isNowValid()).toEqual(false);
 	});
 
 	it('URL param', function () {
 		mocks.win.wgPaidAssetDropConfig = false;
-		spyOn(mocks, 'querystring').and.returnValue({
-			getVal: function () {
-				return 1;
-			}
-		});
+		spyOn(mocks.qs, 'getVal').and.returnValue(1);
 		expect(getModule().isNowValid()).toEqual(true);
 	});
 });
+
