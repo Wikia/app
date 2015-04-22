22acefc29600e31b21abf9e93d50e2f8ac04fc5c
marzjan <sebastianm@wikia-inc.com>
2015-04-22 13:04:57 +0000

    Fix typo, fix behavior for empty infobox

--- extensions/wikia/PortableInfobox/tests/PortableInfoboxRenderServiceTest.php
+++ extensions/wikia/PortableInfobox/tests/PortableInfoboxRenderServiceTest.php
@@ -22,7 +22,9 @@ class PortableInfoboxRenderServiceTest extends PHPUnit_Framework_TestCase {
 		$actualDOM = new DOMDocument('1.0');
 		$expectedDOM = new DOMDocument('1.0');
 		$actualDOM->formatOutput = true;
+		$actualDOM->preserveWhiteSpace = false;
 		$expectedDOM->formatOutput = true;
+		$expectedDOM->preserveWhiteSpace = false;
 		$actualDOM->loadXML($actualOutput);
 		$expectedDOM->loadXML($expectedOutput);
 
@@ -33,9 +35,64 @@ class PortableInfoboxRenderServiceTest extends PHPUnit_Framework_TestCase {
 		return [
 			[
 				'input' => [],
-				'output' => '<aside class="portable-infobox"></aside>',
+				'output' => '',
 				'description' => 'Empty data should yield no infobox markup'
 			],
+			[
+				'input' => [
+					[
+						'type' => 'title',
+						'data' => [
+							'value' => 'Test Title'
+						],
+						'isEmpty' => false
+					],
+					[
+						'type' => 'image',
+						'data' => [
+							'alt' => 'image alt',
+							'value' => 'http://image.jpg'
+						],
+						'isEmpty' => false
+					],
+					[
+						'type' => 'pair',
+						'data' => [
+							'label' => 'test label',
+							'value' => 'test value'
+						],
+						'isEmpty' => false
+					]
+				],
+				'output' => '<aside class="portable-infobox"><div class="portable-infobox-item item-type-title portable-infobox-item-margins"><h2 class="portable-infobox-title">Test Title</h2></div><div class="portable-infobox-item item-type-image no-margins"><figure><img class="portable-infobox-image" alt="image alt" data-url="http://image.jpg"/></figure></div><div class="portable-infobox-item item-type-key-val portable-infobox-item-margins"><h3 class="portable-infobox-item-label portable-infobox-header-font">test label</h3><div class="portable-infobox-item-value">test value</div></div></aside>',
+				'description' => 'Simple infobox with title, image and key-value pair'
+			],
+			[
+				'input' => [
+					[
+						'type' => 'title',
+						'data' => [
+							'value' => 'Test Title'
+						],
+						'isEmpty' => false
+					],
+					[
+						'type' => 'image',
+						'data' => [],
+						'isEmpty' => true
+					],
+					[
+						'type' => 'pair',
+						'data' => [
+							'label' => 'test label',
+							'value' => 'test value'
+						],
+						'isEmpty' => false
+					]
+				],
+				'output' => '<aside class="portable-infobox"><div class="portable-infobox-item item-type-title portable-infobox-item-margins"><h2 class="portable-infobox-title">Test Title</h2></div><div class="portable-infobox-item item-type-key-val portable-infobox-item-margins"><h3 class="portable-infobox-item-label portable-infobox-header-font">test label</h3><div class="portable-infobox-item-value">test value</div></div></aside>',
+				'description' => 'Simple infobox with title, empty image and key-value pair'
+			]
 		];
 	}
 }
