9db8a5d6d38a069497a3487062ee35c8c6a9eca4
Garth Webb <garth@wikia-inc.com>
2015-04-21 17:31:36 -0700

    SOC-628 Cache the creation of GlobalFile objects

--- extensions/wikia/Email/EmailImageHelper.class.php
+++ extensions/wikia/Email/EmailImageHelper.class.php
@@ -8,6 +8,10 @@ namespace Email;
  * @package Email
  */
 class ImageHelper {
+
+	// Cache icon information for a day
+	const ICON_CACHE_TTL = 86400;
+
 	static public $icons = [
 		'Wikia',
 		'Comics',
@@ -30,19 +34,38 @@ class ImageHelper {
 		$info = [];
 
 		foreach ( self::$icons as $name ) {
+			$fileInfo = self::getFileInfo( $name );
+			$info[$name] = $fileInfo;
+		}
+
+		return $info;
+	}
+
+	protected static function getFileInfo( $name ) {
+		$key = self::getFileKey( $name );
+		$memc = \F::app()->wg->Memc;
+
+		$info = $memc->get( $key );
+		if ( empty( $info ) ) {
 			$file   = \GlobalFile::newFromText( $name . '.gif', \Wikia::NEWSLETTER_WIKI_ID );
 			$width  = $file->getWidth();
 			$height = $file->getHeight();
 			$url    = $file->getUrlGenerator()->url();
 
-			$info[$name] = [
-				'name' => $name,
-				'url' => $url,
+			$info = [
+				'name'   => $name,
+				'url'    => $url,
 				'height' => $height,
-				'width' => $width,
+				'width'  => $width,
 			];
+
+			$memc->set( $key, $info, self::ICON_CACHE_TTL );
 		}
 
 		return $info;
+	}
+
+	protected static function getFileKey( $name ) {
+		return wfSharedMemcKey( 'Email', 'ImageHelper', 'icon', $name );
 	}
 }
