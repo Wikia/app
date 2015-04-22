59776a5c21281886720a027456ef8a99f613ded3
Sebastian Marzjan <sebastianm@wikia-inc.com>
2015-04-22 13:03:24 +0200

    DAT-2700 Adding first tests to RenderService for Portable Infobox

--- extensions/wikia/PortableInfobox/tests/PortableInfoboxRenderServiceTest.php
+++ extensions/wikia/PortableInfobox/tests/PortableInfoboxRenderServiceTest.php
@@ -4,56 +4,38 @@ class PortableInfoboxRenderServiceTest extends PHPUnit_Framework_TestCase {
 	private $infoboxRenderService;
 
 	public function setUp() {
-		require_once( dirname(__FILE__) . '/../services/PortableInfoboxRenderService.class.php');
+		parent::setUp();
+		require_once( dirname( __FILE__ ) . '/../PortableInfobox.setup.php' );
+
 		$this->infoboxRenderService = new PortableInfoboxRenderService();
 	}
 
 	/**
 	 * @param $input
-	 * @param $output
+	 * @param $expectedOutput
+	 * @param $description
 	 * @dataProvider testRenderInfoboxDataProvider
 	 */
-	public function testRenderInfobox( $input, $output ) {
+	public function testRenderInfobox( $input, $expectedOutput, $description ) {
+		$actualOutput = $this->infoboxRenderService->renderInfobox( $input );
 
-		$realOutput = $this->infoboxRenderService->renderInfobox( $input );
-		$dom = DOMDocument::loadHTML($realOutput);
-		$xpath = new DOMXPath($dom);
+		$actualDOM = new DOMDocument('1.0');
+		$expectedDOM = new DOMDocument('1.0');
+		$actualDOM->formatOutput = true;
+		$expectedDOM->formatOutput = true;
+		$actualDOM->loadXML($actualOutput);
+		$expectedDOM->loadXML($expectedOutput);
 
-		$h2 = '//aside[@class="portable-infobox"]/div[@class="item-type-title"]/h2[text()="Test Title"]';
-		$nodes = $xpath->query($h2);
-
-		$this->assertTrue(count($nodes) == 1);
-
-
+		$this->assertEquals( $expectedDOM->saveXML(), $actualDOM->saveXML(), $description );
 	}
 
 	public function testRenderInfoboxDataProvider() {
 		return [
 			[
-				'input' => [
-					[
-						'type' => 'title',
-						'data' => [
-							'value' => 'Test Title'
-						]
-					],
-					[
-						'type' => 'image',
-						'data' => [
-							'alt' => 'image alt',
-							'value' => 'http://image.jpg'
-						]
-					],
-					[
-						'type' => 'keyVal',
-						'data' => [
-							'label' => 'test label',
-							'value' => 'test value'
-						]
-					]
-				],
-				'output' => '<aside class="portable-infobox"><div class="infobox-item item-type-title"><h2 class=”infobox-title”>Test Title</h2></div><div class="infobox-item item-type-image"><figure><img alt="image alt" data-url="http://image.jpg" /></figure></div><div class="infobox-item item-type-key-val"><h3 class=”infobox-item-label”>test label</h3><div class=”infobox-item-value”>test value</div></div></aside>'
-			]
+				'input' => [],
+				'output' => '<aside class="portable-infobox"></aside>',
+				'description' => 'Empty data should yield no infobox markup'
+			],
 		];
 	}
 }
