4f250cdb27777480caaaafc7aba8cf1789c03208
Matt Klucsarits <mattk@wikia-inc.com>
2015-04-24 22:27:07 +0000

    SOC-683 Use unset() directly on global var

--- extensions/wikia/UserLogin/UserSignupSpecialController.class.php
+++ extensions/wikia/UserLogin/UserSignupSpecialController.class.php
@@ -652,10 +652,12 @@ class UserSignupSpecialController extends WikiaSpecialPageController {
 	 * Disables User Signup Captcha for automated tests, mobile skin, and sites such as internal
 	 */
 	private function disableCaptcha() {
+		global $wgHooks;
+
 		// Remove hook function
-		$hookArrayKey = array_search( 'Captcha\Hooks::confirmUserCreate', $this->wg->Hooks['AbortNewAccount'] );
+		$hookArrayKey = array_search( 'Captcha\Hooks::confirmUserCreate', $wgHooks['AbortNewAccount'] );
 		if ( $hookArrayKey !== false ) {
-			unset( $this->wg->Hooks['AbortNewAccount'][$hookArrayKey] );
+			unset( $wgHooks['AbortNewAccount'][$hookArrayKey] );
 		}
 		$this->wg->Out->addJsConfigVars( [
 			'wgUserSignupDisableCaptcha' => true
