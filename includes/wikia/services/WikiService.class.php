e25c6b4e6820dfcb9c88eef8a734d7acd20a1e09
macbre <maciej.brencz@gmail.com>
2015-04-24 10:57:51 +0200

    WikiService::getTopEditors - improve DB profiling

--- includes/wikia/services/WikiService.class.php
+++ includes/wikia/services/WikiService.class.php
@@ -222,11 +222,12 @@ class WikiService extends WikiaModel {
 		wfProfileIn( __METHOD__ );
 
 		$wikiId = ( empty($wikiId) ) ? $this->wg->CityId : $wikiId ;
+		$fname = __METHOD__;
 
 		$topEditors = WikiaDataAccess::cache(
 			wfSharedMemcKey( 'wiki_top_editors', $wikiId, $excludeBots ),
 			static::TOPUSER_CACHE_VALID,
-			function() use ( $wikiId, $excludeBots ) {
+			function() use ( $wikiId, $excludeBots, $fname ) {
 				$topEditors = array();
 
 				$db = wfGetDB( DB_SLAVE, array(), 'specials' );
@@ -235,7 +236,7 @@ class WikiService extends WikiaModel {
 					array( 'events_local_users' ),
 					array( 'user_id', 'edits', 'all_groups' ),
 					array( 'wiki_id' => $wikiId, 'edits != 0' ),
-					__METHOD__,
+					$fname,
 					array( 'ORDER BY' => 'edits desc', 'LIMIT' => static::TOPUSER_LIMIT )
 				);
 
