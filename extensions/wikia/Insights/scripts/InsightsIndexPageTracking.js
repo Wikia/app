c11289e920b41e90cf057695d348acedc0e57ff3
Kamil Koterba <kamil@wikia-inc.com>
2015-04-28 17:03:46 +0200

    CE-1623 Use on function instead binding many times within each

--- extensions/wikia/Insights/scripts/InsightsIndexPageTracking.js
+++ extensions/wikia/Insights/scripts/InsightsIndexPageTracking.js
@@ -2,23 +2,30 @@ require(['wikia.tracker'],
 	function (tracker) {
 		'use strict';
 
-		function onLinkClick() {
+		function linkTrack(e) {
 			/* Track a click on an insights type link */
 			var trackingParams = {
 				trackingMethod: 'both',
 				category: 'insights-index',
 				action: tracker.ACTIONS.CLICK_LINK_TEXT,
-				label: $(this).parent().data('type')
+				label: $(e.currentTarget).parent().data('type')
 			};
 			tracker.track(trackingParams);
 		}
 
+		function onKeydownTrack(e) {
+			if(e.keyCode === 13) {// Enter keycode
+				linkTrack(e);
+			}
+		}
+
 		function init() {
-			/* Setup click events on all links within special insights container section */
+			/* Setup click events on all links within special insights container section
+			 * Bind mousedown and keydown to invoke tracking logs earlier than regular click to avoid loosing log
+			 * when reload happens quicker than tracking log */
 			var $insightsContainer = $('.insights-container-landing');
-			$insightsContainer.find('a').each(function(){
-				$(this).click(onLinkClick);
-			});
+			$insightsContainer.on('mousedown', 'a', linkTrack);
+			$insightsContainer.on('keydown', 'a', onKeydownTrack);
 		}
 
 		$(init);
