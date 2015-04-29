5d6335931f0c27717ed2a9a5348c78bbf29aeeb3
macbre <maciej.brencz@gmail.com>
2015-04-29 16:19:21 +0200

    LinksUpdate::queueRefreshTasks - remove legacy jobs no-op

--- includes/LinksUpdate.php
+++ includes/LinksUpdate.php
@@ -278,7 +278,6 @@ class LinksUpdate {
 
 	private function queueRefreshTasks( $batches ) {
 		global $wgCityId;
-		$legacyJobs = array();
 
 		foreach ( $batches as $batch ) {
 			list( $start, $end ) = $batch;
@@ -287,10 +286,6 @@ class LinksUpdate {
 			$task->wikiId( $wgCityId );
 			$task->call( 'refreshTemplateLinks', $start, $end );
 			$task->queue();
-		}
-
-		if ( !empty( $legacyJobs ) ) {
-			Job::batchInsert( $legacyJobs );
 		}
 
 	}
