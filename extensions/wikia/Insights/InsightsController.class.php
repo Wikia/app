4586cf982b884ed07388518e2fb084d155bdd48f
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-04-30 15:00:47 +0000

    CE-1657: Remove typo

--- extensions/wikia/Insights/InsightsController.class.php
+++ extensions/wikia/Insights/InsightsController.class.php
@@ -142,7 +142,7 @@ class InsightsController extends WikiaSpecialPageController {
 	}
 
 	/**
-	 * Get params for notification template shown when user fix all issues in given insight type	 *
+	 * Get params for notification template shown when user fix all issues in given insight type
 	 */
 	private function getCongratulationsNotificationParams() {
 		$params = $this->getInsightLinkParams();
