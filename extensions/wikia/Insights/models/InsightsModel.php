09eb03637c33e7a60acb2af1a3585d7d3c0215f7
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-05-05 11:32:11 +0000

    CE-1660: Add pagination

--- extensions/wikia/Insights/models/InsightsModel.php
+++ extensions/wikia/Insights/models/InsightsModel.php
@@ -1,15 +1,39 @@
 <?php
 
 abstract class InsightsModel {
-	const INSIGHTS_FLOW_URL_PARAM = 'insights';
+	const
+		INSIGHTS_FLOW_URL_PARAM = 'insights',
+		INSIGHTS_LIST_MAX_LIMIT = 10;
 
 	public $subpage;
+
+	protected
+		$offset = 1,
+		$limit = 10,
+		$total = 0,
+		$page = 1;
 
 	abstract public function getContent( $params );
 	abstract public function getViewData();
 	abstract public function getTemplate();
 
 	abstract function prepareData( $res );
+
+	public function getTotalResultsNum() {
+		return $this->total;
+	}
+
+	public function getLimitResultsNum() {
+		return $this->limit;
+	}
+
+	public function getOffset() {
+		return $this->offset;
+	}
+
+	public function getPage() {
+		return $this->page;
+	}
 
 	public function getInsightParam() {
 		$type = $this->getInsightType();
