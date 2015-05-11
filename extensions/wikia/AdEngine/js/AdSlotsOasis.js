faf8146e005871c34728d1166b05572cbaf097f6
nAndy <nandy@wikia-inc.com>
2015-05-11 17:31:24 +0200

    ADEN-1716 added logging to AdSlotsOasis.js

--- extensions/wikia/AdEngine/js/AdSlotsOasis.js
+++ extensions/wikia/AdEngine/js/AdSlotsOasis.js
@@ -3,18 +3,23 @@ require([
 	'ext.wikia.adEngine.adContext',
 	'ext.wikia.adEngine.adSlotsInContent',
 	'jquery',
-	'wikia.document'
-], function (adContext, adSlotsInContent, $, doc) {
+	'wikia.document',
+	'wikia.abTest',
+	'wikia.log'
+], function (adContext, adSlotsInContent, $, doc, abTest, log) {
 	'use strict';
+
+	var logGroup = 'AdSlotOasis.js',
+		context = adContext.getContext();
 
 	function init() {
 		var elementsBeforeSlots = $(adSlotsInContent.selector).get(),
 			maxSlots = 1;
+
+		log('init()', 'debug', logGroup);
 		elementsBeforeSlots.unshift(null);
 		adSlotsInContent.init(elementsBeforeSlots, maxSlots);
 	}
-
-	var context = adContext.getContext();
 
 	// Don't start those slots on no_ads, corporate, home etc
 	if (context.opts.pageType === 'all_ads') {
