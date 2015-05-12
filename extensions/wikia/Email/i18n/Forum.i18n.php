86b64396d8256952a8f4d442e1443144a16d877e
Damian Jozwiak <damian@wikia-inc.com>
2015-05-12 14:02:31 -0700

    SOC-611 | Forum email QQQs

--- extensions/wikia/Email/i18n/Forum.i18n.php
+++ extensions/wikia/Email/i18n/Forum.i18n.php
@@ -1,9 +1,16 @@
 <?php
 $messages = array();
 
-$messages['en'] = array(
+$messages['en'] = [
 	'emailext-forum-subject' => 'There is a new discussion on $1 on {{SITENAME}}.',
 	'emailext-forum-salutation' => 'Hi $1,',
 	'emailext-forum-summary' => "'''There is a new discussion on [$1 $2] on [{{SERVER}} {{SITENAME}}].'''",
 	'emailext-forum-button-label' => 'See the discussion'
-);
+];
+
+$messages['qqq'] = [
+	'emailext-forum-subject' => 'Subject for email that is fired when new thread is created. $1 -> Forum board name where thread was created.',
+	'emailext-forum-salutation' => "Email greeting. $1 is the recipient's username.",
+	'emailext-forum-summary' => "Message to the user that new forum thread was created. $1 -> thread url, $2 -> thread name",
+	'emailext-forum-button-label' => 'Text for button that, when clicked, navigates to the new forum thread.'
+];
