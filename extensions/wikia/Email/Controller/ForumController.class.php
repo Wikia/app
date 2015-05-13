6c375e97f237bf6fcbbe2342330316b1ca12618c
rafal.kalinski <kalina@wikia-inc.com>
2015-05-13 13:00:58 -0700

    SOC-457 ForumReplyController moved to ForumController module

--- extensions/wikia/Email/Controller/ForumController.class.php
+++ extensions/wikia/Email/Controller/ForumController.class.php
@@ -71,7 +71,7 @@ class ForumController extends EmailController {
 	}
 
 	protected function getButtonText() {
-		return wfMessage( 'emailext-forum-button-label')
+		return wfMessage( 'emailext-forum-button-label' )
 			->inLanguage( $this->targetLang )
 			->parse();
 	}
@@ -104,6 +104,52 @@ class ForumController extends EmailController {
 				->inLanguage( $this->targetLang )
 				->parse()
 		];
+
 		return array_merge( $footerMessages, parent::getFooterMessages() );
+	}
+}
+
+class ForumReplyController extends ForumController {
+	protected function getButtonLink() {
+		return $this->titleUrl;
+	}
+
+	protected function getViewAllLink() {
+		return $this->titleUrl;
+	}
+
+	protected function getButtonLabelKey () {
+		return 'emailext-forum-reply-link-label';
+	}
+
+	protected function getSummary() {
+		return wfMessage( $this->getSummaryKey(), $this->titleText, $this->titleUrl )
+			->inLanguage( $this->targetLang )
+			->parse();
+	}
+
+	protected function getSubjectKey() {
+		return 'emailext-forum-reply-summary';
+	}
+
+	protected function getSummaryKey() {
+		return 'emailext-forum-reply-summary';
+	}
+
+	protected function getViewAllKey(){
+		return 'emailext-forum-reply-view-all';
+	}
+
+	protected function getFooterMessages() {
+		$unfollowUrl = $this->board->getFullURL( [
+			'action' => 'unwatch'
+		] );
+
+		$footerMessages = [
+			wfMessage( 'emailext-forum-reply-unfollow-text', $unfollowUrl, $this->titleText )
+				->inLanguage( $this->targetLang )
+				->parse()
+		];
+		return array_merge( $footerMessages, EmailController::getFooterMessages() );
 	}
 }
--- extensions/wikia/Email/i18n/Forum.i18n.php
+++ extensions/wikia/Email/i18n/Forum.i18n.php
@@ -5,12 +5,18 @@ $messages['en'] = [
 	'emailext-forum-subject' => 'There is a new discussion on $1 on {{SITENAME}}.',
 	'emailext-forum-salutation' => 'Hi $1,',
 	'emailext-forum-summary' => "'''There is a new discussion on [$1 $2] on [{{SERVER}} {{SITENAME}}].'''",
-	'emailext-forum-button-label' => 'See the discussion'
+	'emailext-forum-button-label' => 'See the discussion',
+	'emailext-forum-reply-summary' => "'''There are new replies to [$2 $1] on [{{SERVER}} {{SITENAME}}].'''",
+	'emailext-forum-reply-link-label' => 'Read the reply',
+	'emailext-forum-reply-view-all' => '[$1 See the entire discussion.]'
 ];
 
 $messages['qqq'] = [
 	'emailext-forum-subject' => 'Subject for email that is fired when new thread is created. $1 -> Forum board name where thread was created.',
 	'emailext-forum-salutation' => "Email greeting. $1 is the recipient's username.",
 	'emailext-forum-summary' => "Message to the user that new forum thread was created. $1 -> thread url, $2 -> thread name",
-	'emailext-forum-button-label' => 'Text for button that, when clicked, navigates to the new forum thread.'
+	'emailext-forum-button-label' => 'Text for button that, when clicked, navigates to the new forum thread.',
+	'emailext-forum-reply-summary' => 'Information about the new replies in thread. $1 is the thread subject.',
+    'emailext-forum-reply-link-label' => 'Link to the post, permalink.',
+    'emailext-forum-reply-view-all' => 'Link to the thread page.'
 ];
