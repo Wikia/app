9bee6806d8022a9f2be41f08a7047319c855eb23
saipetch <saipetch@wikia-inc.com>
2015-05-12 18:19:28 -0700

    Monetization (MON-375): added event tracking for monetization service

--- extensions/wikia/AdEngine/js/MonetizationServiceHelper.js
+++ extensions/wikia/AdEngine/js/MonetizationServiceHelper.js
@@ -1,13 +1,20 @@
 define('ext.wikia.adEngine.monetizationsServiceHelper', [
 	'jquery',
 	'wikia.cache',
+	'wikia.geo',
 	'wikia.loader',
 	'wikia.scriptwriter',
+	'wikia.tracker',
 	'wikia.window',
-], function ($, cache, loader, scriptWriter, window) {
+], function ($, cache, geo, loader, scriptWriter, tracker, window) {
 	'use strict';
 
-	var isEndOfContent = false;
+	var isEndOfContent = false,
+		track = tracker.buildTrackingFunction({
+			trackingMethod: 'internal',
+			category: 'monetization-module',
+			geo: geo.getCountryCode()
+		});
 
 	/**
 	 * Loads all assets for monetization ads
@@ -67,6 +74,11 @@ define('ext.wikia.adEngine.monetizationsServiceHelper', [
 	 */
 	function injectContent(slot, content, success) {
 		scriptWriter.injectHtml(slot, content, function () {
+			var $container = $('#' + slot).find('.monetization-module');
+
+			trackImpression($container);
+			trackClickEvent($container);
+
 			success();
 		});
 	}
@@ -93,6 +105,78 @@ define('ext.wikia.adEngine.monetizationsServiceHelper', [
 		}
 
 		window.adslots2.push(slot);
+	}
+
+	function trackImpression($container) {
+		// track impression for each placement
+		$container.each(function () {
+			var $this = $(this),
+				value = $this.children().children().length,	// check if the ad is blocked
+				monType = $this.attr('data-mon-type'),
+				slot = $this.attr('data-mon-slot'),
+				label = 'monetization-' + monType + '-' + slot;
+
+			track({
+				action: 'module-impression',
+				label: label,
+				value: value,
+				type: monType,
+				slot: slot
+			});
+
+			// track impression for each product
+			if (monType === 'ecommerce' || monType === 'amazon_video') {
+				$this.find('.affiliate').each(function (idx, element) {
+					var $element = $(element);
+					track({
+						label: $element.attr('data-mon-ptag'),
+						action: 'product-impression-' + label,
+						value: idx,
+						type: monType,
+						slot: slot,
+						pid: $element.attr('data-mon-pid')
+					});
+				});
+			}
+		});
+	}
+
+	function trackClickEvent($container) {
+		var elements = [
+			'.module-title',
+			'.product-thumb',
+			'.product-name',
+			'.product-price',
+			'.amazon-logo',
+			'.product-btn'
+		];
+
+		$container.on('click', elements.join(', '), function () {
+			var $this = $(this),
+				$module = $this.closest('.monetization-module'),
+				$products = $module.find('.affiliate'),
+				monType = $module.attr('data-mon-type'),
+				slot = $module.attr('data-mon-slot'),
+				elementName = $this.attr('class'),
+				productUrl = $this.attr('href') || $this.find('a').attr('href'),
+				$product = $products.first();
+
+			if (monType !== 'amazon_video' && elementName !== 'module-title') {
+				$product = $this.parent();
+			}
+
+			track({
+				action: tracker.ACTIONS.CLICK + '-monetization-' + monType + '-' + slot + '-' + elementName,
+				label: $product.attr('data-mon-ptag'),
+				value: $products.index($product),
+				type: monType,
+				slot: slot,
+				title: $product.attr('data-mon-pname'),
+				pid: $product.attr('data-mon-pid'),
+				element: elementName,
+				url: productUrl
+			});
+		});
 	}
 
 	return {
