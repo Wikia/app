1190ad4950175abb882d101e3dd888dcabf12fca
marzjan <sebastianm@wikia-inc.com>
2015-05-13 08:22:43 +0000

    Restore GamestarApiWrapper intergration test after external provider fixed the issue

--- extensions/wikia/VideoHandlers/tests/GamestarApiWrapperTest.php
+++ extensions/wikia/VideoHandlers/tests/GamestarApiWrapperTest.php
@@ -34,9 +34,6 @@ class GamestarApiWrapperTest extends WikiaBaseTest {
 	 * @group Infrastructure
 	 */
 	public function testgetDataFromValidHtmlResponse() {
-
-		$this->markTestSkipped( "This test is broken. The flag will be removed automatically once https://github.com/Wikia/app/pull/6156 is merged." );
-
 		// setup
 		$this->setUpMock();
 
