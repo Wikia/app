04ec21587f7b9866c99fbeaa94d6f0c3eeddae94
Inez Korczynski <inez@wikia-inc.com>
2015-05-14 21:05:21 +0000

    CONCF-547: Adjust code manipulating page header to recent changes done to page header

--- extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaFocusWidget.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/widgets/ve.ui.WikiaFocusWidget.js
@@ -33,7 +33,8 @@ ve.ui.WikiaFocusWidget = function VeUiWikiaFocusWidget( surface ) {
 	this.$body = this.$( this.getElementDocument() ).find( 'body:first' );
 	this.$window = this.$( this.getElementWindow() );
 	this.$surface = surface.$element;
-	this.$pageHeaderElements = this.$( '#WikiaPageHeader' ).children( ':not( h1 )' );
+	this.$pageHeaderElements = this.$( '#WikiaPageHeader' ).children( ':not( .header-container  )' ).add( '.header-tally' );
+
 	this.$navBackground = this.$( '.WikiNav .navbackground' );
 	this.$localNavigation = this.$( '#localNavigation' );
 	this.$wikiaBarWrapper = this.$( '#WikiaBarWrapper' );
@@ -285,7 +286,8 @@ ve.ui.WikiaFocusWidget.prototype.hideDistractions = function () {
 		mw.config.get( 'WikiaBar' ).hide();
 	}
 	// Visibility property - problem with edit button opening when setting display property
-	this.$pageHeaderElements.css( 'visibility', 'hidden' );
+	//this.$pageHeaderElements.css( 'visibility', 'hidden' );
+	this.$pageHeaderElements.hide();
 	this.$wikiaAds
 		.each( function () {
 			var $ad = $( this );
@@ -306,7 +308,8 @@ ve.ui.WikiaFocusWidget.prototype.showDistractions = function () {
 	if ( this.showWikiaBar ) {
 		mw.config.get( 'WikiaBar' ).show();
 	}
-	this.$pageHeaderElements.css( 'visibility', 'visible' );
+	//this.$pageHeaderElements.css( 'visibility', 'visible' );
+	this.$pageHeaderElements.show();
 	this.$wikiaAds
 		.css( {
 			height: 'auto',
