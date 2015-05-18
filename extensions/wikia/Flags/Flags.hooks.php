cc85baa027805758acde15c8f4d034fe83496f79
adamkarminski <adamkarminski@gmail.com>
2015-05-18 16:47:24 +0200

    CE-1771 Exclude Flags from VE API calls

--- extensions/wikia/Flags/Flags.hooks.php
+++ extensions/wikia/Flags/Flags.hooks.php
@@ -4,21 +4,40 @@ namespace Flags;
 
 class Hooks {
 
+	/**
+	 * Hooks into the internalParse() process and injects a wikitext
+	 * with notices for the given page.
+	 * @param \Parser $parser
+	 * @param $text
+	 * @param $stripState
+	 * @return bool
+	 */
 	public static function onParserBeforeInternalParse( \Parser $parser, &$text, &$stripState ) {
-		$mwf = \MagicWord::get( 'flags' );
-		$isMagicWordMatch = $mwf->match( $text );
-		if ( $isMagicWordMatch || !$parser->mFlagsParsed ) {
-			$helper = new \FlagsHelper();
-			$addText = $helper->getFlagsForPageWikitext( $parser->getTitle()->getArticleID() );
-			if ( $addText !== null ) {
-				if ( $isMagicWordMatch ) {
-					$text = $mwf->replace( $addText, $text );
-				} else {
-					$text = $addText . $text;
+		global $wgRequest;
+
+		/**
+		 * Don't check for flags if:
+		 * - a user is on an edit page
+		 * - the request is from VE
+		 */
+		if ( !\WikiaPageType::isEditPage() && !( $wgRequest->getVal( 'action' ) == 'visualeditor' ) ) {
+			$mwf = \MagicWord::get('flags');
+			$isMagicWordMatch = $mwf->match( $text );
+
+			if ( $isMagicWordMatch || ! $parser->mFlagsParsed ) {
+				$helper = new \FlagsHelper();
+				$addText = $helper->getFlagsForPageWikitext( $parser->getTitle()->getArticleID() );
+				if ( $addText !== null ) {
+					if ( $isMagicWordMatch ) {
+						$text = $mwf->replace( $addText, $text );
+					} else {
+						$text = $addText . $text;
+					}
 				}
+				$parser->mFlagsParsed = true;
 			}
-			$parser->mFlagsParsed = true;
 		}
+
 		return true;
 	}
 }
