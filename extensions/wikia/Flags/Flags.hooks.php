a2f0bda901678dc82f5f23bfa31f1d316a292086
Kamil Koterba <kamil@wikia-inc.com>
2015-05-18 15:55:45 +0200

    CE-1706 Enable adding actions to edit dropdown; adding flags action

--- extensions/wikia/Flags/Flags.hooks.php
+++ extensions/wikia/Flags/Flags.hooks.php
@@ -2,6 +2,9 @@
 namespace Flags;
 
 class FlagsHooks {
+
+	const FLAGS_DROPDOWN_ACTION = 'flags';
+
 	public static function onBeforePageDisplay( \OutputPage &$out, \Skin &$skin ) {
 		$out->addScriptFile('/extensions/wikia/Flags/scripts/FlagsModal.js');
 		return true;
@@ -16,11 +19,21 @@ class FlagsHooks {
 	 * @return bool true
 	 */
 	public static function onSkinTemplateNavigation( &$skin, &$links ) {
-		$links['views']['flags'] = [
+		$links['views'][self::FLAGS_DROPDOWN_ACTION] = [
 			'href' => '#',
 			'text' => 'Flags',
 			'class' => 'flags-access-class',
 		];
+		return true;
+	}
+
+	/**
+	 * Adds flags action to dropdown actions to enable displaying flags item on edit dropdown
+	 * @param array $actions
+	 * @return bool true
+	 */
+	public static function onDropdownActions( array &$actions ) {
+		$actions[] = self::FLAGS_DROPDOWN_ACTION;
 		return true;
 	}
 }
