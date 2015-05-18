36de73a8b5adc993307dfa5acb9e0a503fef10f4
adamkarminski <adamkarminski@gmail.com>
2015-05-18 17:09:14 +0200

    CE-1771 Simplify hooks

--- extensions/wikia/Flags/Flags.hooks.php
+++ extensions/wikia/Flags/Flags.hooks.php
@@ -17,25 +17,27 @@ class Hooks {
 
 		/**
 		 * Don't check for flags if:
+		 * - you've already checked
 		 * - a user is on an edit page
 		 * - the request is from VE
 		 */
-		if ( !\WikiaPageType::isEditPage() && !( $wgRequest->getVal( 'action' ) == 'visualeditor' ) ) {
+		if ( !$parser->mFlagsParsed
+			&& !\WikiaPageType::isEditPage()
+			&& !( $wgRequest->getVal( 'action' ) == 'visualeditor' )
+		) {
 			$mwf = \MagicWord::get('flags');
 			$isMagicWordMatch = $mwf->match( $text );
 
-			if ( $isMagicWordMatch || ! $parser->mFlagsParsed ) {
-				$helper = new \FlagsHelper();
-				$addText = $helper->getFlagsForPageWikitext( $parser->getTitle()->getArticleID() );
-				if ( $addText !== null ) {
-					if ( $isMagicWordMatch ) {
-						$text = $mwf->replace( $addText, $text );
-					} else {
-						$text = $addText . $text;
-					}
+			$helper = new \FlagsHelper();
+			$addText = $helper->getFlagsForPageWikitext( $parser->getTitle()->getArticleID() );
+			if ( $addText !== null ) {
+				if ( $isMagicWordMatch ) {
+					$text = $mwf->replace( $addText, $text );
+				} else {
+					$text = $addText . $text;
 				}
-				$parser->mFlagsParsed = true;
 			}
+			$parser->mFlagsParsed = true;
 		}
 
 		return true;
