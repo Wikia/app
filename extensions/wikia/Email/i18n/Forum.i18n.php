0bfbff0b9e0ac151896b83afa1c06da9615f9894
Damian Jozwiak <damian@wikia-inc.com>
2015-05-19 00:39:37 +0000

    SOC-611 | Forum email - updated translations

--- extensions/wikia/Email/i18n/Forum.i18n.php
+++ extensions/wikia/Email/i18n/Forum.i18n.php
@@ -1,14 +1,92 @@
 <?php
 $messages = array();
 
-$messages['en'] = [
+$messages['en'] = array(
 	'emailext-forum-subject' => 'There is a new discussion on $1 on {{SITENAME}}.',
 	'emailext-forum-summary' => "'''There is a new discussion on [$1 $2] on [{{SERVER}} {{SITENAME}}].'''",
-	'emailext-forum-button-label' => 'See the discussion'
-];
+	'emailext-forum-button-label' => 'See the discussion',
+);
 
-$messages['qqq'] = [
+$messages['qqq'] = array(
 	'emailext-forum-subject' => 'Subject for email that is fired when new thread is created. $1 -> Forum board name where thread was created.',
-	'emailext-forum-summary' => "Message to the user that new forum thread was created. $1 -> thread url, $2 -> thread name",
-	'emailext-forum-button-label' => 'Text for button that, when clicked, navigates to the new forum thread.'
-];
+	'emailext-forum-summary' => 'Message to the user that new forum thread was created. $1 -> thread url, $2 -> thread name',
+	'emailext-forum-button-label' => 'Text for button that, when clicked, navigates to the new forum thread.',
+);
+
+$messages['de'] = array(
+	'emailext-forum-subject' => 'Im Forum $1 auf der Seite {{SITENAME}} wurde eine neue Diskussion gestartet.',
+	'emailext-forum-salutation' => 'Hallo $1,',
+	'emailext-forum-summary' => "'''Im Forum [$1 $2] auf der Seite [{{SERVER}} {{SITENAME}}] wurde eine neue Diskussion gestartet.'''",
+	'emailext-forum-button-label' => 'Diskussion anzeigen',
+);
+
+$messages['es'] = array(
+	'emailext-forum-subject' => 'Hay una nueva discusión sobre $1 en {{SITENAME}}.',
+	'emailext-forum-salutation' => 'Hola $1,',
+	'emailext-forum-summary' => "'''Hay una nueva discusión sobre [$1 $2] en [{{SERVER}} {{SITENAME}}].'''",
+	'emailext-forum-button-label' => 'Ver la discusión',
+);
+
+$messages['fr'] = array(
+	'emailext-forum-subject' => 'Une nouvelle discussion a été ajoutée à $1 sur {{SITENAME}}.',
+	'emailext-forum-salutation' => 'Bonjour $1,',
+	'emailext-forum-summary' => "'''Une nouvelle discussion a été ajoutée à [$1 $2] sur [{{SERVER}} {{SITENAME}}].'''",
+	'emailext-forum-button-label' => 'Voir la discussion',
+);
+
+$messages['it'] = array(
+	'emailext-forum-subject' => "C'è una nuova discussione in $1 della {{SITENAME}}.",
+	'emailext-forum-salutation' => 'Ciao, $1.',
+	'emailext-forum-summary' => "'''C'è una nuova discussione in [$1 $2] della [{{SERVER}} {{SITENAME}}].'''",
+	'emailext-forum-button-label' => 'Vedi la discussione',
+);
+
+$messages['ja'] = array(
+	'emailext-forum-subject' => '{{SITENAME}}の$1に新しいスレッドが追加されました',
+	'emailext-forum-salutation' => '$1さん',
+	'emailext-forum-summary' => "'''[{{SERVER}} {{SITENAME}}]の[$1 $2]に新しいスレッドが追加されました。'''",
+	'emailext-forum-button-label' => 'スレッドを見る',
+);
+
+$messages['nl'] = array(
+	'emailext-forum-subject' => 'There is a new discussion on $1 on {{SITENAME}}.',
+	'emailext-forum-salutation' => 'Hi $1,',
+	'emailext-forum-summary' => "'''There is a new discussion on [$1 $2] on [{{SERVER}} {{SITENAME}}].'''",
+	'emailext-forum-button-label' => 'See the discussion',
+);
+
+$messages['pl'] = array(
+	'emailext-forum-subject' => 'Na forum $1 na {{SITENAME}} pojawiła się nowa dyskusja.',
+	'emailext-forum-salutation' => 'Cześć $1,',
+	'emailext-forum-summary' => "'''Na forum [$1 $2] na [{{SERVER}} {{SITENAME}}]  pojawiła się nowa dyskusja.'''",
+	'emailext-forum-button-label' => 'Zobacz dyskusję',
+);
+
+$messages['pt'] = array(
+	'emailext-forum-subject' => 'Há uma nova discussão em $1 na {{SITENAME}}.',
+	'emailext-forum-salutation' => 'Olá $1,',
+	'emailext-forum-summary' => "'''Há uma nova discussão em [$1 $2] na [{{SERVER}} {{SITENAME}}].'''",
+	'emailext-forum-button-label' => 'Ver a discussão',
+);
+
+$messages['ru'] = array(
+	'emailext-forum-subject' => 'Новое обсуждение в разделе $1 на {{SITENAME}}',
+	'emailext-forum-salutation' => 'Здравствуйте, $1!',
+	'emailext-forum-summary' => "'''В разделе [$1 $2] на [{{SERVER}} {{SITENAME}}] появилось новое обсуждение.'''",
+	'emailext-forum-button-label' => 'Просмотреть обсуждение',
+);
+
+$messages['zh-hans'] = array(
+	'emailext-forum-subject' => '在{{SITENAME}}网站的$1论坛上，大家正在对一个新的话题进行讨论。',
+	'emailext-forum-salutation' => '尊敬的$1用户，你好,',
+	'emailext-forum-summary' => "'''在{{SERVER}}上的{{SITENAME}}网站的[$1 $2]页面，大家正在对一个新的话题进行讨论。'''",
+	'emailext-forum-button-label' => '查看该讨论',
+);
+
+$messages['zh-tw'] = array(
+	'emailext-forum-subject' => '在{{SITENAME}}網站的$1論壇上,大家正在對一個新的話題進行討論。',
+	'emailext-forum-salutation' => '尊敬的$1用户，你好，',
+	'emailext-forum-summary' => "'''在{{SERVER}}上的{{SITENAME}}網站的[$1 $2]網頁，大家正在對一個新的話題進行討論]。'''",
+	'emailext-forum-button-label' => '查看该討論',
+);
+
