6c1ec439bf68c8491fd123f6787179ee62159e79
Garth Webb <garth@wikia-inc.com>
2015-05-21 11:28:28 -0700

    SOC-815 Remove conditional in return

--- extensions/wikia/WallNotifications/WallNotifications.class.php
+++ extensions/wikia/WallNotifications/WallNotifications.class.php
@@ -46,7 +46,7 @@ class WallNotifications {
 
 		$list = $this->getWikiNotificationList( $userId, $wikiId );
 		if ( empty( $list ) ) {
-			return $list;
+			return [];
 		}
 
 		$read = [];
@@ -151,7 +151,7 @@ class WallNotifications {
 			$memcSync->lockAndSetData( $callback, $callback );
 		}
 
-		return empty( $list ) ? [] : $list;
+		return $list;
 	}
 
 	private function clearWikiNotificationListCache( $userId, $wikiId ) {
