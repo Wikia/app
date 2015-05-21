f57dd9500d131c435512922ea8559897b80edaba
Garth Webb <garth@wikia-inc.com>
2015-05-21 15:48:26 -0700

    SOC-457 Add back the missing English text for i18n key emailext-unfollow-text

--- extensions/wikia/Email/Email.i18n.php
+++ extensions/wikia/Email/Email.i18n.php
@@ -18,6 +18,7 @@ Questions or concerns? Feel free to <a href="http://community.wikia.com/wiki/Spe
 	'emailext-recipient-notice' => 'Email sent to $1 from Wikia',
 	'emailext-update-frequency' => 'To change which emails you receive or their frequency, please visit your [{{fullurl:Special:Preferences|#mw-prefsection-emailv2}} Preferences] page.',
 	'emailext-unsubscribe' => 'To unsubscribe from all Wikia emails, click [$1 here].',
+	'emailext-unfollow-text' => 'No longer interested in receiving these updates? Click [$1 here] to unfollow $2 on {{SITENAME}}.',
 	'emailext-salutation' => 'Hi $1,',
 	'emailext-forumreply-unfollow-text' => 'No longer interested in receiving these updates? Click [$1 here] to unfollow [$2 this] discussion on [{{SERVER}} {{SITENAME}}].'
 );
