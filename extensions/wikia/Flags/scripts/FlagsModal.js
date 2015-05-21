14d7d66e7874dffd0ffe7dfe4e7259ec8e0d6c64
Kamil Koterba <kamil@wikia-inc.com>
2015-05-21 17:00:56 +0200

    CE-1706 Refactoring FlagsModal.js

--- extensions/wikia/Flags/scripts/FlagsModal.js
+++ extensions/wikia/Flags/scripts/FlagsModal.js
@@ -1,106 +1,114 @@
 require(['jquery'], function($) {
 	'use strict';
 
-	var init,
-		modal = null,
-		$flagsEditForm;
-
-	init = function init() {
-		$('#ca-flags').on('click', showModal);
+	/* Modal component configuration */
+	var modalConfig = {
+		vars: {
+			id: 'FlagsModal',
+			size: 'small', // size of the modal
+			content: '', // content
+			title: 'Flags',
+			buttons: [ // buttons in the footer
+				{
+					vars: {
+						value: 'Done',
+						data: [
+							{
+								key: 'event',
+								value: 'done'
+							}
+						]
+					}
+				},
+				{
+					vars: {
+						value: 'Cancel',
+						data: [
+							{
+								key: 'event',
+								value: 'close'
+							}
+						]
+					}
+				}
+			]
+		}
 	};
 
+	function init() {
+		$('#ca-flags').on('click', showModal);
+	}
+
+	/**
+	 * Function for showing modal.
+	 * First function in showing modal process.
+	 * Performs all necessary job to display modal with flags ready to edit
+	 * @param event
+	 */
 	function showModal(event) {
 		event.preventDefault();
-		$.when(getTemplate('modal')).done(function(template) {
-			require(['wikia.ui.factory'], function(uiFactory) {
-
-				//getModalContent();
-				// initialize the modal component
-				uiFactory.init(['modal']).then(function(uiModal) {
-					// modal component configuration
-					var modalConfig = {
-						vars: {
-							id: 'FlagsModal',
-							size: 'small', // size of the modal
-							content: template.content.html, // content
-							title: 'Flags', // title
-							buttons: [  // buttons in the footer
-								{
-									vars: {
-										value: 'Done',
-										data: [
-											{
-												key: 'event',
-												value: 'save'
-											}
-										]
-									}
-								},
-								{
-									vars: {
-										value: 'Cancel',
-										data: [
-											{
-												key: 'event',
-												value: 'close'
-											}
-										]
-									}
-								}
-							]
-						}
-					};
-
-					// create the wrapping JS Object using the modalConfig
-					uiModal.createComponent(modalConfig, function(sampleModal) {
-						modal = sampleModal;
-						$flagsEditForm = $('#flagsEditForm');
-
-						// bind the Save button to this anon. function
-						sampleModal.bind('save', function() {
-							$flagsEditForm.trigger('submit');
-						});
-
-						// show the modal
-						sampleModal.show();
-					});
-				});
-			});
+		Wikia.getMultiTypePackage({
+			templates: [{
+				controller: 'Flags',
+				method: 'modal',
+				params: {
+					'pageId': window.wgArticleId
+				}
+			}],
+			styles: '/extensions/wikia/Flags/styles/Modal.scss',
+			callback: handlePackage
 		});
 	}
 
-
-	function getTemplate( name ) {
-		//var template = cached.templates[ name ];
-		var template = {};
-
-		//if ( !template ) {
-		//	this.error( 'Template "' + name + '" is not defined' );
-		//}
-
-		//return template.content && template || $.Deferred(function( dfd ) {
-		return $.Deferred(function( dfd ) {
-			Wikia.getMultiTypePackage({
-				templates: [{
-					controller: 'Flags',
-					method: 'modal',
-					params: {
-						'wikiId': window.wgCityId,
-						'pageId': window.wgArticleId
-					}
-				}],
-				styles: '/extensions/wikia/Flags/styles/Modal.scss',
-				callback: function( pkg ) {
-					require(['wikia.ui.factory', 'wikia.loader'], function (uiFactory, loader) {
-						loader.processStyle(pkg.styles);
-					});
-					template.content = $.parseJSON(pkg.templates.Flags_modal);
-					dfd.resolve( template );
-				}
-			});
-
-			return dfd.promise();
+	/**
+	 * Handles package provided by getMultiTypePackage from server.
+	 * Loads received styles, adds content to modal and initializes modal component
+	 * One of sub-tasks for getting modal shown
+	 * @param Object pkg
+	 */
+	function handlePackage(pkg) {
+		/* Load styles */
+		require(['wikia.loader'], function (loader) {
+			loader.processStyle(pkg.styles);
 		});
+
+		/* Add content to modal */
+		modalConfig.vars.content = $.parseJSON(
+			pkg.templates.Flags_modal
+		).html;
+
+		require(['wikia.ui.factory'], function (uiFactory) {
+			/* Initialize the modal component */
+			uiFactory.init(['modal']).then(createComponent);
+		});
+	}
+
+	/**
+	 * Creates modal UI component
+	 * One of sub-tasks for getting modal shown
+	 * @param uiModal
+	 */
+	function createComponent(uiModal) {
+		/* Create the wrapping JS Object using the modalConfig */
+		uiModal.createComponent(modalConfig, processInstance);
+	}
+
+	/**
+	 * CreateComponent callback that finally shows modal
+	 * and binds submit action to Done button
+	 * One of sub-tasks for getting modal shown
+	 * @param modalInstance
+	 */
+	function processInstance(modalInstance) {
+		var $flagsEditForm = modalInstance.$element.find('#flagsEditForm');
+
+		/* Submit flags edit form on Done button click */
+		modalInstance.bind('done', function(){
+			$flagsEditForm.trigger('submit');
+		});
+
+		/* Show the modal */
+		modalInstance.show();
 	}
 
 	// Run initialization method on DOM ready
