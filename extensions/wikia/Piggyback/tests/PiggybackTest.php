cee84d9ce39c454d2e609693337c71ebc975f7c2
Michał Roszka <michal@roszka.pl>
2015-05-21 17:09:57 +0000

    add unit tests

---
+++ extensions/wikia/Piggyback/tests/PiggybackTest.php
@@ -0,0 +1,38 @@
+<?php
+class PiggybackTest extends WikiaBaseTest {
+
+	public function setUp()
+	{
+		$this->setupFile =  __DIR__ . '/../Piggyback.php';
+		parent::setUp();
+	}
+
+	public function testEfPiggybackRequestContextOverrideUserOverride()
+	{
+		$originalUser = 'OriginalUser';
+		$newUser = 'NewUser';
+
+		$this->mockStaticMethod( 'PBLoginForm', 'isPiggyback', true );
+		$this->mockStaticMethod( 'User', 'newFromSession', $newUser );
+
+		$testUser = $originalUser;
+		$result = efPiggybackRequestContextOverrideUser( $testUser, null );
+
+		$this->assertEquals( $testUser, $newUser );
+		$this->assertFalse( $result );
+	}
+
+	public function testEfPiggybackRequestContextOverrideUserDoNotOverride()
+	{
+		$originalUser = 'OriginalUser';
+		$newUser = 'NewUser';
+
+		$this->mockStaticMethod( 'PBLoginForm', 'isPiggyback', false );
+
+		$testUser = $originalUser;
+		$result = efPiggybackRequestContextOverrideUser( $testUser, null );
+
+		$this->assertEquals( $testUser, $originalUser );
+		$this->assertTrue( $result );
+	}
+}
