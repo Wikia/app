b6bd4527938a98102caa425ebe26d4e0e6e7839a
Kamil Koterba <kamil@wikia-inc.com>
2015-05-22 21:13:29 +0200

    CE-1777 Process flag_params_names to be sure we show empty fields on modal as well

--- extensions/wikia/Flags/controllers/templates/FlagsController_editForm.php
+++ extensions/wikia/Flags/controllers/templates/FlagsController_editForm.php
@@ -5,8 +5,11 @@
 			<input type="checkbox" name="<?= $inputNamePrefix . ':' . $flag_type_id . ':' . $inputNameCheckbox ?>" <?php if ( $flag['flag_id'] ){ echo 'checked="checked"'; }?>/>
 			<label><?= $flag['flag_name'] ?></label>
 			<a href="<?= $flag['flag_view_url'] ?>" target="_blank">More info</a>
-			<?php foreach ( $flag['params'] as $flagParamName => $flagParamValue ): ?>
-				<input type="text" name="<?= $inputNamePrefix . ':' . $flag_type_id . ':' . $flagParamName ?>" value="<?= $flagParamValue ?>" placeholder="<?= $flagParamName ?>" class="param" />
+			<?php
+			$flagParamsNames = json_decode($flag['flag_params_names']);
+			foreach ( $flagParamsNames as $flagParamName ):
+			?>
+				<input type="text" name="<?= $inputNamePrefix . ':' . $flag_type_id . ':' . $flagParamName ?>" value="<?= isset( $flag['params'][$flagParamName] ) ? $flag['params'][$flagParamName] : '' ?>" placeholder="<?= $flagParamName ?>" class="param" />
 			<?php endforeach; ?>
 		</li>
 		<?php endforeach; ?>
