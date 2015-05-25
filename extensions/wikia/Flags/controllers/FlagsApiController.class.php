70cfbca1f7e504222f976c3149b8bbf5d2e1669a
adamkarminski <adamkarminski@gmail.com>
2015-05-25 15:21:29 +0200

    CE-1857 Reorder methods based on access level and remove unnecessary comments

--- extensions/wikia/Flags/controllers/FlagsApiController.class.php
+++ extensions/wikia/Flags/controllers/FlagsApiController.class.php
@@ -20,50 +20,44 @@ class FlagsApiController extends WikiaApiController {
 		$status = false;
 
 	/**
-	 * Returns a singleton instance of FlagsCache
-	 * @return FlagsCache
-	 */
-	private function getCache() {
-		if ( !isset( $this->cache ) ) {
-			$this->cache = new FlagsCache();
-		}
-
-		return $this->cache;
-	}
-
-	/**
-	 * Assigns a request's parameters to the object's property
-	 * and sets a wiki_id if it hasn't been specified as one
-	 * of the parameters.
-	 */
-	private function getRequestParams() {
-		$this->params = $this->request->getParams();
-		if ( !isset( $this->params['wiki_id'] ) ) {
-			$this->params['wiki_id'] = $this->wg->CityId;
-		}
-	}
-
-	/**
-	 * To prevent CSRF attacks it checks if a request is a POST one
-	 * and if a sent token matches the user's one.
-	 * Calls getRequestParams if the request is valid.
-	 * @return bool
-	 */
-	private function processRequest() {
-		if ( !$this->request->wasPosted()
-			|| !$this->wg->User->matchEditToken( $this->getVal( 'edit_token' ) )
-		) {
-			$this->response->setException( new \Exception( 'Invalid request' ) );
-			return false;
-		}
-
-		$this->getRequestParams();
-		return true;
-	}
-
-	/**
 	 * Article level API
 	 */
+
+	/**
+	 * Retrieves all data for flags assigned to the given page
+	 * with an intent of rendering them. To get all types of flags:
+	 * @see getFlagsForPageForEdit()
+	 *
+	 * @requestParam int wiki_id
+	 * @requestParam int page_id
+	 * @response Array A list of flags with flag_type_id values as indexes.
+	 *  One item contains the following fields:
+	 *	 	int flag_id
+	 * 		int flag_type_id
+	 * 		int wiki_id
+	 *		int page_id
+	 * 		int flag_group
+	 * 		string flag_name
+	 * 		string flag_view
+	 * 		int flag_targeting
+	 * 		string|null flag_params_names
+	 *
+	 * 	@if flag_params_names is not empty
+	 * 		params = [
+	 * 			param_name => param_value
+	 *		]
+	 */
+	public function getFlagsForPage() {
+		$this->getRequestParams();
+
+		if ( !isset( $this->params['page_id'] ) ) {
+			return null;
+		}
+
+		$flagsForPage = $this->getFlagsForPageRawData( $this->params['wiki_id'], $this->params['page_id'] );
+
+		$this->setResponseData( $flagsForPage );
+	}
 
 	/**
 	 * Retrieves all data for all flag types available on the given wikia
@@ -105,113 +99,6 @@ class FlagsApiController extends WikiaApiController {
 		$allFlagTypes = $this->getAllFlagTypes( $this->params['wiki_id'], $this->params['page_id'] );
 
 		$this->setResponseData( $allFlagTypes );
-	}
-
-	/**
-	 * A method that combines data from two models - Flag and FlagTypes.
-	 * The Flag model gives it data on instances of flags assigned to the given page.
-	 * The FlagType model provides data on every type of a flag available on the wikia.
-	 * The union operator used on arrays lets us safely merge these two arrays.
-	 * In case of an index duplication it preserves an item from the left array and adds a new
-	 * item if an index is not set.
-	 *
-	 * @param $wikiId
-	 * @param $pageId
-	 * @return Array An array of all types of flags, with and without instances
-	 */
-	private function getAllFlagTypes( $wikiId, $pageId ) {
-		/**
-		 * 1. Get flags assigned to the page
-		 */
-		$flagsForPage = $this->getFlagsForPageRawData( $wikiId, $pageId );
-
-		/**
-		 * 2. Get all flag types for a wikia
-		 */
-		$flagTypesForWikia = $this->getFlagTypesForWikiaRawData( $wikiId );
-
-		/**
-		 * 3. Return the united arrays if it's possible to merge them (both arrays use
-		 * flag_type_id values as indexes)
-		 */
-		return $flagsForPage + $flagTypesForWikia;
-	}
-
-	/**
-	 * Retrieves all data for flags assigned to the given page
-	 * with an intent of rendering them. To get all types of flags:
-	 * @see getFlagsForPageForEdit()
-	 *
-	 * @requestParam int wiki_id
-	 * @requestParam int page_id
-	 * @response Array A list of flags with flag_type_id values as indexes.
-	 *  One item contains the following fields:
-	 *	 	int flag_id
-	 * 		int flag_type_id
-	 * 		int wiki_id
-	 *		int page_id
-	 * 		int flag_group
-	 * 		string flag_name
-	 * 		string flag_view
-	 * 		int flag_targeting
-	 * 		string|null flag_params_names
-	 *
-	 * 	@if flag_params_names is not empty
-	 * 		params = [
-	 * 			param_name => param_value
-	 *		]
-	 */
-	public function getFlagsForPage() {
-		$this->getRequestParams();
-
-		if ( !isset( $this->params['page_id'] ) ) {
-			return null;
-		}
-
-		$flagsForPage = $this->getFlagsForPageRawData( $this->params['wiki_id'], $this->params['page_id'] );
-
-		$this->setResponseData( $flagsForPage );
-	}
-
-	/**
-	 * Tries to get the data on Flags instances for the given page from cache.
-	 * If it is not cached it gets it from the database and caches it.
-	 * @param $wikiId
-	 * @param $pageId
-	 * @return bool|mixed
-	 */
-	private function getFlagsForPageRawData( $wikiId, $pageId ) {
-		$flagsCache = $this->getCache();
-		$flagsForPage = $flagsCache->getFlagsForPage( $pageId );
-
-		if ( !$flagsForPage ) {
-			$flagModel = new Flag();
-			$flagsForPage = $flagModel->getFlagsForPage( $wikiId, $pageId );
-
-			$flagsCache->setFlagsForPage( $pageId, $flagsForPage );
-		}
-
-		return $flagsForPage;
-	}
-
-	/**
-	 * Tries to get the data on types of flags available for the given wikia from cache.
-	 * If it is not cached it gets it from the database and caches it.
-	 * @param $wikiId
-	 * @return bool|mixed
-	 */
-	private function getFlagTypesForWikiaRawData( $wikiId ) {
-		$flagsCache = $this->getCache();
-
-		$flagTypesForWikia = $flagsCache->getFlagTypesForWikia();
-		if ( !$flagTypesForWikia ) {
-			$flagTypeModel = new FlagType();
-			$flagTypesForWikia = $flagTypeModel->getFlagTypesForWikia( $wikiId );
-
-			$flagsCache->setFlagTypesForWikia( $flagTypesForWikia );
-		}
-
-		return $flagTypesForWikia;
 	}
 
 	/**
@@ -266,6 +153,10 @@ class FlagsApiController extends WikiaApiController {
 		$this->setVal( 'status', $this->status );
 	}
 
+	/**
+	 * Updates flags on the given page using a `flags` array passed as a request parameter.
+	 * @return bool
+	 */
 	public function updateFlagsForPage() {
 		# @TODO: CE-1849 Proper error handling
 		if ( !$this->processRequest() ) {
@@ -332,5 +223,111 @@ class FlagsApiController extends WikiaApiController {
 		}
 
 		$this->setVal( 'status', $this->status );
+	}
+
+	/**
+	 * Private methods
+	 */
+
+	/**
+	 * Returns a singleton instance of FlagsCache
+	 * @return FlagsCache
+	 */
+	private function getCache() {
+		if ( !isset( $this->cache ) ) {
+			$this->cache = new FlagsCache();
+		}
+
+		return $this->cache;
+	}
+
+	/**
+	 * Assigns a request's parameters to the object's property
+	 * and sets a wiki_id if it hasn't been specified as one
+	 * of the parameters.
+	 */
+	private function getRequestParams() {
+		$this->params = $this->request->getParams();
+		if ( !isset( $this->params['wiki_id'] ) ) {
+			$this->params['wiki_id'] = $this->wg->CityId;
+		}
+	}
+
+	/**
+	 * To prevent CSRF attacks it checks if a request is a POST one
+	 * and if a sent token matches the user's one.
+	 * Calls getRequestParams if the request is valid.
+	 * @return bool
+	 */
+	private function processRequest() {
+		if ( !$this->request->wasPosted()
+			|| !$this->wg->User->matchEditToken( $this->getVal( 'edit_token' ) )
+		) {
+			$this->response->setException( new \Exception( 'Invalid request' ) );
+			return false;
+		}
+
+		$this->getRequestParams();
+		return true;
+	}
+
+	/**
+	 * Tries to get the data on Flags instances for the given page from cache.
+	 * If it is not cached it gets it from the database and caches it.
+	 * @param $wikiId
+	 * @param $pageId
+	 * @return bool|mixed
+	 */
+	private function getFlagsForPageRawData( $wikiId, $pageId ) {
+		$flagsCache = $this->getCache();
+		$flagsForPage = $flagsCache->getFlagsForPage( $pageId );
+
+		if ( !$flagsForPage ) {
+			$flagModel = new Flag();
+			$flagsForPage = $flagModel->getFlagsForPage( $wikiId, $pageId );
+
+			$flagsCache->setFlagsForPage( $pageId, $flagsForPage );
+		}
+
+		return $flagsForPage;
+	}
+
+	/**
+	 * Tries to get the data on types of flags available for the given wikia from cache.
+	 * If it is not cached it gets it from the database and caches it.
+	 * @param $wikiId
+	 * @return bool|mixed
+	 */
+	private function getFlagTypesForWikiaRawData( $wikiId ) {
+		$flagsCache = $this->getCache();
+
+		$flagTypesForWikia = $flagsCache->getFlagTypesForWikia();
+		if ( !$flagTypesForWikia ) {
+			$flagTypeModel = new FlagType();
+			$flagTypesForWikia = $flagTypeModel->getFlagTypesForWikia( $wikiId );
+
+			$flagsCache->setFlagTypesForWikia( $flagTypesForWikia );
+		}
+
+		return $flagTypesForWikia;
+	}
+
+	/**
+	 * A method that combines data from two models - Flag and FlagTypes.
+	 * The Flag model gives it data on instances of flags assigned to the given page.
+	 * The FlagType model provides data on every type of a flag available on the wikia.
+	 * The union operator used on arrays lets us safely merge these two arrays.
+	 * In case of an index duplication it preserves an item from the left array and adds a new
+	 * item if an index is not set.
+	 *
+	 * @param $wikiId
+	 * @param $pageId
+	 * @return Array An array of all types of flags, with and without instances
+	 */
+	private function getAllFlagTypes( $wikiId, $pageId ) {
+		$flagsForPage = $this->getFlagsForPageRawData( $wikiId, $pageId );
+		$flagTypesForWikia = $this->getFlagTypesForWikiaRawData( $wikiId );
+
+		return $flagsForPage + $flagTypesForWikia;
 	}
 }
