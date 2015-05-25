21346f7c0234ed2cc8ec4ce95fe40fe641c53378
kamilkoterba <kamil.koterba@gmail.com>
2015-05-25 13:56:05 +0200

    Experimenting with php opening tags' indents

--- extensions/wikia/Flags/controllers/templates/FlagsController_editForm.php
+++ extensions/wikia/Flags/controllers/templates/FlagsController_editForm.php
@@ -9,14 +9,15 @@
 			<a href="<?= Sanitizer::cleanUrl( $flag['flag_view_url'] ) ?>" target="_blank"><?= wfMessage( 'flags-edit-form-more-info' )->escaped() ?></a>
 			<?php
 			$flagParamsNames = json_decode( $flag['flag_params_names'] );
-			if ( !empty( $flagParamsNames ) ): ?>
+			if ( !empty( $flagParamsNames ) ):
+			?>
 				<fieldset class="params">
 				<?php
 				foreach ( $flagParamsNames as $flagParamName => $flagParamDescription ):
 					$flagParamValue = (string)$flag['params'][$flagParamName];
-					?>
+				?>
 					<input type="text" name="<?= Sanitizer::encodeAttribute( "{$prefix}:{$flagParamName}" ) ?>" value="<?= Sanitizer::encodeAttribute( $flagParamValue ) ?>" placeholder="<?= Sanitizer::encodeAttribute( $flagParamDescription ) ?>" class="param">
-					<?php
+				<?php
 				endforeach;
 				?>
 				</fieldset>
