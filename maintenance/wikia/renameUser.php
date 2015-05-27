8e294be90eb252b7dd37cdf5c6500a0d952b9b71
macbre <maciej.brencz@gmail.com>
2015-05-27 11:11:28 +0200

    Add a maintenance script for running user rename process

---
+++ maintenance/wikia/renameUser.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Perform user rename process
+ *
+ * @author Macbre
+ * @ingroup Maintenance
+ */
+
+putenv( 'SERVER_ID=177' ); // run in the context of community wiki
+
+require_once( dirname( __FILE__ ) . '/../Maintenance.php' );
+
+/**
+ * Maintenance script class
+ */
+class RenameUserMaintenance extends Maintenance {
+
+	/**
+	 * Set script options
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		$this->addOption( 'old-username', 'User to rename', true );
+		$this->addOption( 'new-username', 'New name for the user', true );
+		$this->addOption( 'reason', 'The rename reason' );
+
+		$this->mDescription = 'This script performs user rename from the command line';
+	}
+	/**
+	 * Script entry point
+	 */
+	public function execute() {
+		global $wgUser;
+		$wgUser = User::newFromName( 'WikiaBot' );
+
+		// get options
+		$oldName = $this->getOption( 'old-username' );
+		$newName = $this->getOption( 'new-username' );
+		$reason = $this->getOption( 'reason', 'User rename' );
+
+		// set up the logger
+		$logger = Wikia\Logger\WikiaLogger::instance();
+		$logger->pushContext( [
+			'old-name' => $oldName,
+			'new-name' => $newName,
+			'reason'   => $reason
+		] );
+
+		$logger->info( __CLASS__ . ': start' );
+
+		$renameProcess = new RenameUserProcess( $oldName, $newName, true, $reason );
+		$res = $renameProcess->run();
+
+		if ( $res !== true ) {
+			$logger->info( __CLASS__ . ': error', [
+				'errors' => $renameProcess->getErrors(),
+				'warnings' => $renameProcess->getWarnings(),
+			] );
+
+			$this->error( 'RenameUserProcess::run failed for ' . $oldName, 1 );
+		}
+
+		$logger->info( __CLASS__ . ': done' );
+	}
+}
+
+$maintClass = "RenameUserMaintenance";
+require_once( RUN_MAINTENANCE_IF_MAIN );
