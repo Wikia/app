eba7c1321eedd700ad09b2dd25a70d05a64386f4
grunny <mwgrunny@gmail.com>
2015-06-01 17:29:32 +1000

    (CE-1867) Re-add Wikia's additional translations

---
+++ extensions/SemanticDrilldown/languages/SD_LanguagePl.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @ingroup Language
+ * @ingroup SDLanguage
+ */
+
+class SD_LanguagePl extends SD_Language {
+
+	public $m_SpecialProperties = [
+		// category properties
+		SD_SP_HAS_FILTER  => 'Ma filtr',
+		SD_SP_HAS_DRILLDOWN_TITLE  => 'Ma tytuł semantyczny',
+		// filter properties
+		SD_SP_COVERS_PROPERTY  => 'Pokrywa właściwość',
+		SD_SP_GETS_VALUES_FROM_CATEGORY => 'Pobiera wartości z kategorii',
+		SD_SP_REQUIRES_FILTER => 'Wymaga filtra',
+		SD_SP_HAS_LABEL  => 'Ma etykietę',
+		// display properties
+		SD_SP_HAS_DISPLAY_PARAMETERS => 'Ma parametry wyświetlania',
+	];
+
+	public $m_Namespaces = array(
+		SD_NS_FILTER		=> 'Filtr',
+		SD_NS_FILTER_TALK	=> 'Dyskusja_filtru',
+	);
+
+}
