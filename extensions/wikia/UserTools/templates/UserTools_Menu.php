903fafeeb3c4e93ffdd1d44f6e8d2290cf40cb16
Bogna Knychala <bogna.ka@gmail.com>
2015-06-02 19:15:44 +0200

    CONCF-416 remove share user command

--- extensions/wikia/UserTools/templates/UserTools_Menu.php
+++ extensions/wikia/UserTools/templates/UserTools_Menu.php
@@ -1,10 +1,5 @@
 <?php foreach ($items as $item) { ?>
 <?php switch ($item['type']) { ?>
-<?php case 'share': ?>
-<li id="ca-share_feature" class="overflow">
-	<a id="control_share_feature" href="#" data-name="<?= $item['tracker-name']; ?>"><?= htmlspecialchars( $item['caption'] ); ?></a>
-</li>
-<?php     break; ?>
 <?php case 'follow': ?>
 <li class="overflow">
 	<a accesskey="w" id="<?= Sanitizer::encodeAttribute( $item['link-id'] ); ?>" href="<?= Sanitizer::encodeAttribute( $item['href'] ); ?>" data-name="<?= $item['tracker-name']; ?>"><?= htmlspecialchars( $item['caption'] ); ?></a>
