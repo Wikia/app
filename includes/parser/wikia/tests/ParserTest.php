05e132bd1caf43fc1cbbd665fc0209d556578559
idradm <robakadam@gmail.com>
2015-06-03 15:35:03 +0200

    DAT-2866 formatting updated

--- includes/parser/wikia/tests/ParserTest.php
+++ includes/parser/wikia/tests/ParserTest.php
@@ -2,46 +2,47 @@
 
 class ParserTest extends WikiaBaseTest {
 
-    /** @var Parser */
-    protected $parser;
+	/** @var Parser */
+	protected $parser;
 
-    /** @var ParserOptions */
-    protected $options;
+	/** @var ParserOptions */
+	protected $options;
 
-    public function setUp() {
-        parent::setUp();
-        $this->parser = new Parser();
-        $this->options = new ParserOptions();
-    }
+	public function setUp() {
+		parent::setUp();
+		$this->parser = new Parser();
+		$this->options = new ParserOptions();
+	}
 
-    protected function queryDOM( $html, $query ) {
-        $dom = ( new DOMDocument() );
-        $dom->loadHTML( $html );
-        $x = new DOMXPath( $dom );
-        return $x->query( $query );
-    }
+	protected function queryDOM( $html, $query ) {
+		$dom = ( new DOMDocument() );
+		$dom->loadHTML( $html );
+		$x = new DOMXPath( $dom );
 
-    public function testRefParsingWithNoReferenceTag() {
-        $markup = '<ref>test</ref>';
+		return $x->query( $query );
+	}
 
-        $html = $this->parser->parse( $markup, new Title(), $this->options )->getText();
+	public function testRefParsingWithNoReferenceTag() {
+		$markup = '<ref>test</ref>';
 
-        $sups = $this->queryDOM( $html, '//p/sup' );
-        $this->assertEquals( 1, $sups->length );
-        $error = $this->queryDOM( $html, '//strong[contains(@class, \'error\')]' );
-        $this->assertEquals( 1, $error->length );
-    }
+		$html = $this->parser->parse( $markup, new Title(), $this->options )->getText();
+
+		$sups = $this->queryDOM( $html, '//p/sup' );
+		$this->assertEquals( 1, $sups->length );
+		$error = $this->queryDOM( $html, '//strong[contains(@class, \'error\')]' );
+		$this->assertEquals( 1, $error->length );
+	}
 
 
-    public function testPartialParseOptionForRefTag() {
-        $markup = '<ref>test</ref>';
+	public function testPartialParseOptionForRefTag() {
+		$markup = '<ref>test</ref>';
 
-        $this->options->setIsPartialParse( true );
-        $html = $this->parser->parse( $markup, new Title(), $this->options )->getText();
+		$this->options->setIsPartialParse( true );
+		$html = $this->parser->parse( $markup, new Title(), $this->options )->getText();
 
-        $sups = $this->queryDOM( $html, '//p/sup' );
-        $this->assertEquals( 1, $sups->length );
-        $error = $this->queryDOM( $html, '//strong[contains(@class, \'error\')]' );
-        $this->assertEquals( 0, $error->length );
-    }
+		$sups = $this->queryDOM( $html, '//p/sup' );
+		$this->assertEquals( 1, $sups->length );
+		$error = $this->queryDOM( $html, '//strong[contains(@class, \'error\')]' );
+		$this->assertEquals( 0, $error->length );
+	}
 }
