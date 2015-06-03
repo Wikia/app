50daa5c95d182b1b65ffca52b42fa8f2df45b65f
Bogna Knychala <bogna.ka@gmail.com>
2015-06-03 12:41:18 +0200

    CONCF-619 update regex so it matches also youtu.be URLs

--- extensions/wikia/YouTube/YouTube.php
+++ extensions/wikia/YouTube/YouTube.php
@@ -110,7 +110,7 @@ function upgradeYouTubeTag( $editpage, $request ) {
 			$ytid = trim( $nodeValues[0] );
 
 			// Check to see if the whole URL is used
-			$ytid = preg_replace( '/^.*youtube.com\/watch?.*v=([^&]+).*$/', '$1', $ytid );
+			$ytid = preg_replace( '/^.*(youtube.com|youtu.be)\/(watch\?v=)?([^&]+)$/', '$3', $ytid );
 
 			// Parse out the width and height parameters
 			$params = parseSizeParams( $paramText );
