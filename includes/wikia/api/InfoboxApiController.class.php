8f57072734195002b039b32c5e287394841aae18
jacek <jacek@wikia-inc.com>
2015-06-09 12:15:18 +0200

    replace duplicated functionality inside InfoboxApiController

--- includes/wikia/api/InfoboxApiController.class.php
+++ includes/wikia/api/InfoboxApiController.class.php
@@ -5,13 +5,9 @@ class InfoboxApiController extends WikiaApiController {
 	public function getDetails() {
 		$pageid = $this->getRequiredParam( 'pageid' );
 		$title = Title::newFromID( $pageid );
-		$data = [ ];
-		if ( $title && $title->exists() ) {
-			$data = Article::newFromTitle( $title, RequestContext::getMain() )
-				//on empty parser cache this should be regenerated, see WikiPage.php:2996
-				->getParserOutput()
-				->getProperty( PortableInfoboxParserTagController::INFOBOXES_PROPERTY_NAME );
-		}
+
+		$dataService = new PortableInfoboxDataService();
+		$data = $dataService->getInfoboxDataByTitle( $title );
 
 		$this->setResponseData( [ 'items' => $data ] );
 	}
