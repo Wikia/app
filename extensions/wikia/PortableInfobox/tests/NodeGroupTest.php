27998a3d46e63bd554751b7fac77c0c6e983c6d6
dianafa <diana.falkowska@gmail.com>
2015-06-09 19:35:19 +0200

    prototype test

---
+++ extensions/wikia/PortableInfobox/tests/NodeGroupTest.php
@@ -0,0 +1,67 @@
+<?php
+class NodeGroupTest extends WikiaBaseTest {
+	protected function setUp() {
+		$this->setupFile = dirname( __FILE__ ) . '/../PortableInfobox.setup.php';
+		parent::setUp();
+	}
+
+	/**
+	 * @dataProvider testGetDataDataProvider
+	 */
+	public function testGetData( $val1, $val2, $string, $expectedOutput ) {
+		$xml = simplexml_load_string( $string );
+		$node = new Wikia\PortableInfobox\Parser\Nodes\NodeGroup( $xml, [ 'val1' => $val1, 'val2' => $val2 ] );
+		$data = $node->getData();
+		$this->assertTrue( $data['theme'] === $expectedOutput['theme'] );
+	}
+
+	public function testGetDataDataProvider() {
+		return [
+			[
+				'val1' => 'damage',
+				'val2' => 'prize',
+				'string' => '<group theme="inline">
+						<header>Weapon Characteristics</header>
+							<data source="val1">
+								<label>Weapon damage</label>
+							</data>
+					</group>',
+				'expectedOutput' => [
+					'value' => '',
+					'theme' => 'inline'
+				]
+			],
+			[
+				'val1' => 'damage',
+				'val2' => 'prize',
+				'string' => '<group>
+						<header>Weapon Characteristics</header>
+							<data source="val1">
+								<label>Weapon damage</label>
+						</data>
+					</group>',
+				'expectedOutput' => [
+					'value' => '',
+					'theme' => null
+				]
+			],
+			[
+				'val1' => 'damage',
+				'val2' => 'prize',
+				'string' => '<group theme="inline">
+						<header>Weapon Characteristics</header>
+							<data source="val1">
+								<label>Weapon damage</label>
+						</data>
+						<data source="val2">
+								<label>Weapon prize</label>
+						</data>
+					</group>',
+				'expectedOutput' => [
+					'value' => '',
+					'theme' => 'inline'
+				]
+			],
+		];
+	}
+}
