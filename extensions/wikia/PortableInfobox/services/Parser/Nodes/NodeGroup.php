8b6c793deb4e36a0fc310f95332e507b7fec0894
dianafa <diana.falkowska@gmail.com>
2015-06-09 10:16:47 +0200

    getXmlAttr theme

--- extensions/wikia/PortableInfobox/services/Parser/Nodes/NodeGroup.php
+++ extensions/wikia/PortableInfobox/services/Parser/Nodes/NodeGroup.php
@@ -4,13 +4,17 @@ namespace Wikia\PortableInfobox\Parser\Nodes;
 use Wikia\PortableInfobox\Parser\XmlParser;
 
 class NodeGroup extends Node {
+	const DATA_THEME_ATTR_NAME = 'theme';
 
 	public function getData() {
 		$nodeFactory = new XmlParser( $this->infoboxData );
 		if ( $this->externalParser ) {
 			$nodeFactory->setExternalParser( $this->externalParser );
 		}
-		return [ 'value' => $nodeFactory->getDataFromNodes( $this->xmlNode, $this ) ];
+		$theme = $this->getXmlAttribute( $this->xmlNode, self::DATA_THEME_ATTR_NAME );
+		$value = $nodeFactory->getDataFromNodes( $this->xmlNode, $this );
+		//var_dump("theme: ", $theme);
+		return [ 'value' =>  $value, 'theme' => $theme ];
 	}
 
 	public function isEmpty( $data ) {
