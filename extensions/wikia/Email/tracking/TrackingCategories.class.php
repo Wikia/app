fac2ec124768643e8cb9f7e3e3dfadc011c93b0a
James Sutterfield <james@wikia-inc.com>
2015-06-10 12:17:56 -0700

    SOC-887 update variable name to match value

--- extensions/wikia/Email/tracking/TrackingCategories.class.php
+++ extensions/wikia/Email/tracking/TrackingCategories.class.php
@@ -4,5 +4,5 @@ namespace Email\Tracking;
 
 class TrackingCategories {
 	const DEFAULT_CATEGORY = "UserMail";
-	const WALL_NOTIFICATIONS = "WallNotification";
+	const WALL_NOTIFICATION = "WallNotification";
 }
