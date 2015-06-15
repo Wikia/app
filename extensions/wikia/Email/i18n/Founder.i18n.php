ad0eb80abfd84214673f9eaf45008b74165398d3
Garth Webb <garth@wikia-inc.com>
2015-06-15 12:01:23 -0700

    SOC-466 Adding qqq

--- extensions/wikia/Email/i18n/Founder.i18n.php
+++ extensions/wikia/Email/i18n/Founder.i18n.php
@@ -7,7 +7,6 @@ $messages['en'] = array(
 	'emailext-founder-summary' => '[$1 $2] on [{{SERVER}} {{SITENAME}}] has been edited.',
 	'emailext-founder-body' => 'This is [$1 $2]’s first edit on your wikia. Help them feel like they are a part of the community and encourage them to keep that good stuff coming!',
 	'emailext-founder-link-label' => 'Compare Changes',
-	'emailext-founder-view-all' => '[$1 View all comments]',
 	'emailext-founder-footer-article' => "Head over to [$1 $2] to see what's new",
 	'emailext-founder-footer-all-changes' => 'View all changes to [$1 $2]',
 	'emailext-founder-encourage' => 'This is [$1 $2]’s first edit on your wikia. Help them feel like they are a part of the community and encourage them to keep that good stuff coming!',
@@ -16,5 +15,14 @@ $messages['en'] = array(
 );
 
 $messages['qqq'] = array(
-	'emailext-founder-summary' => 'Information about an article comment comment.  $1 is the article URL and $2 is the article title',
+	'emailext-founder-subject' => 'Subject of the email sent to founders on an edit to their wiki.  $1 -> page title, $2 -> author',
+	'emailext-founder-anon-subject' => 'Subject of the email sent to founder on an anonymous edit to their wiki.  $1 -> page title',
+	'emailext-founder-summary' => 'Summary text in the body of the email.  $1 -> page URL, $2 -> page title',
+	'emailext-founder-body' => 'Body text of the email.  $1 -> author profile page URL, $2 -> author name',
+	'emailext-founder-link-label' => 'Button label linking to diff page of changes to the article changed',
+	'emailext-founder-footer-article' => "Footer text with link inviting user to see changes since last edit.  $1 -> diff URL, $2 -> page title",
+	'emailext-founder-footer-all-changes' => 'Foot text with link inviting user to see all changes on the page.  $1 -> history page URL, $2 -> page title',
+	'emailext-founder-encourage' => 'Encouragement text for the founder to support the user who just edited.  $1 -> author profile URL, $2-> author name',
+	'emailext-founder-anon-encourage' => 'Text explaining what an anonymous edit is',
+	'emailext-founder-multi-encourage' => 'Text letting the founder know a contributor has made multiple edits.  $1 -> author profile URL, $2 -> author name',
 );
