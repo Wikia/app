c70a24476e2fa0def75fe1cd3e240687a1469ae0
idradm <robakadam@gmail.com>
2015-06-18 13:48:52 +0200

    merge with dev

--- includes/wikia/api/InfoboxApiController.class.php
+++ includes/wikia/api/InfoboxApiController.class.php
@@ -6,8 +6,7 @@ class InfoboxApiController extends WikiaApiController {
 		$pageid = $this->getRequiredParam( 'pageid' );
 		$title = Title::newFromID( $pageid );
 
-		$dataService = new PortableInfoboxDataService();
-		$data = $dataService->getInfoboxDataByTitle( $title );
+		$data = ( new PortableInfoboxDataService() )->getInfoboxDataByTitle( $title );
 		$output = isset( $data ) && is_array( $data ) ?
 			array_map( function ( $infobox ) {
 				return isset( $infobox[ 'data' ] ) ? $infobox[ 'data' ] : $infobox;
