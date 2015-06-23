4b4ae05dcfd481aa8a051993a53a18110c343a39
tor <tor@wikia-inc.com>
2015-06-23 14:07:41 +0000

    CE-2122 do not display rail module if marked as non-infobox

--- extensions/wikia/TemplateDraft/TemplateDraftHooks.class.php
+++ extensions/wikia/TemplateDraft/TemplateDraftHooks.class.php
@@ -2,10 +2,26 @@
 
 class TemplateDraftHooks {
 
+	/*
+	 * property used in page_props table, construction:
+	 * * tc- - prefix for "template classifiaction"
+	 * * -marked- - signifies classification decision made by human
+	 * * -auto- - signifies classification decision made by AI
+	 * * -infobox - suffix denoting the type of template we identified
+	 *
+	 * @TODO move this someplace sensible, once available
+	 */
+	const TEMPLATE_INFOBOX_PROP = 'tc-marked-infobox';
+
 	public static function onGetRailModuleList( Array &$railModuleList ) {
 		global $wgTitle;
 
-		// TODO: Check page props here
+		// quit early if page was marked by user as not an infobox
+		if ( Wikia::getProps( $wgTitle->getId(), self::TEMPLATE_INFOBOX_PROP ) === 0 ) {
+			return true;
+		}
+
+		// only display rail module on template namespace
 		if ( $wgTitle->getNamespace() === NS_TEMPLATE ) {
 			$railModuleList[1502] = [ 'TemplateDraftModule', Index, null ];
 		}
