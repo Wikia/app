1b1edaf503bc02eed4daac5c79e40263e2388aff
Damon Snyder <drsnyder@gmail.com>
2015-06-23 14:14:43 -0700

    Stylize PHP

--- lib/Wikia/src/Persistence/User/PreferencePersistenceMySQL.php
+++ lib/Wikia/src/Persistence/User/PreferencePersistenceMySQL.php
@@ -13,29 +13,29 @@ class PreferencePersistenceMySQL implements PreferencePersistence {
 	private $master;
 	private $slave;
 
-	function __construct(\DatabaseMysqli $master, \DatabaseMysqli $slave) {
+	function __construct( \DatabaseMysqli $master, \DatabaseMysqli $slave ) {
 		$this->master = $master;
 		$this->slave = $slave;
 	}
 
 	public function save( $userId, array $preferences ) {
-		$tuples = $this->createTuplesFromPreferences($userId, $preferences);
-		return $this->master->upsert(self::USER_PREFERENCE_TABLE, $tuples, [], self::$UPSERT_SET_BLOCK);
+		$tuples = $this->createTuplesFromPreferences( $userId, $preferences );
+		return $this->master->upsert( self::USER_PREFERENCE_TABLE, $tuples, [], self::$UPSERT_SET_BLOCK );
 	}
 
-	public static function createTuplesFromPreferences($userId, array $preferences) {
-		$userId = intval($userId);
-		return array_map(function(Preference $e) use ($userId) {
+	public static function createTuplesFromPreferences( $userId, array $preferences ) {
+		$userId = intval( $userId );
+		return array_map( function( Preference $e ) use ( $userId ) {
 			return [
 				'up_user' =>  $userId,
 				'up_property' => $e->getName(),
 				'up_value' => $e->getValue()
 				];
-		}, $preferences);
+		} , $preferences );
 	}
 
 	public function get( $userId ) {
-		$userId = intval($userId);
+		$userId = intval( $userId );
 		$result = $this->slave->select(
 			self::USER_PREFERENCE_TABLE,
 			'*',
@@ -44,8 +44,8 @@ class PreferencePersistenceMySQL implements PreferencePersistence {
 		);
 
 		$preferences = [];
-		if (is_array($result) && !empty($result)) {
-			$preferences = $this->userPropertiesResultToPreferenceArray($result);
+		if ( is_array( $result ) && !empty( $result ) ) {
+			$preferences = $this->userPropertiesResultToPreferenceArray( $result );
 		}
 
 		return $preferences;
@@ -57,11 +57,11 @@ class PreferencePersistenceMySQL implements PreferencePersistence {
 	 * @param array of ['up_user', 'up_property', 'up_value'] tuples
 	 * @return Preference[]
 	 */
-	public function userPropertiesResultToPreferenceArray(array $result) {
+	public function userPropertiesResultToPreferenceArray( array $result ) {
 		$preferences = [];
-		foreach( $result as $index => $row ) {
-			if (isset($row["up_property"]) && isset($row["up_value"])) {
-				$preferences[] = new Preference($row["up_property"], $row["up_value"]);
+		foreach ( $result as $index => $row ) {
+			if ( isset( $row["up_property"] ) && isset( $row["up_value"] ) ) {
+				$preferences[] = new Preference( $row["up_property"], $row["up_value"] );
 			}
 		}
 
--- lib/Wikia/tests/Persistence/User/PreferencePersistenceMySQLTest.php
+++ lib/Wikia/tests/Persistence/User/PreferencePersistenceMySQLTest.php
@@ -27,71 +27,71 @@ class PreferencePersistenceMySQLTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testGetSuccess() {
-		$this->mysqliMockSlave->expects($this->once())
-			->method('select')
-			->with('user_properties', '*', array( 'up_user' => $this->userId ), $this->anything())
-			->willReturn([
+		$this->mysqliMockSlave->expects( $this->once() )
+			->method( 'select' )
+			->with( 'user_properties', '*', array( 'up_user' => $this->userId ), $this->anything() )
+			->willReturn( [
 			 [ 'up_user' => $this->userId, 'up_property' => $this->testPreference->getName(), 'up_value' => $this->testPreference->getValue() ],
 			 [ 'up_user' => $this->userId, 'up_property' => 'autopatrol', 'up_value' => '0' ],
 			 [ 'up_user' => $this->userId, 'up_property' => 'date', 'up_value' => '1' ],
-			]);
+			] );
 
-		$persistence = new PreferencePersistenceMySQL($this->mysqliMockMaster, $this->mysqliMockSlave);
+		$persistence = new PreferencePersistenceMySQL( $this->mysqliMockMaster, $this->mysqliMockSlave );
 		$preferences = $persistence->get( $this->userId );
 
-		$this->assertTrue( is_array($preferences), "expecting an array" );
-		$this->assertTrue( !empty($preferences), "expecting a non-empty array" );
+		$this->assertTrue( is_array( $preferences ), "expecting an array" );
+		$this->assertTrue( !empty( $preferences ), "expecting a non-empty array" );
 		$this->assertEquals( $preferences[0]->getName(), $this->testPreference->getName(), "expecting the test preference name" );
 		$this->assertEquals( $preferences[0]->getValue(), $this->testPreference->getValue(), "expecting the test preference value" );
 	}
 
 	public function testGetEmpty() {
-		$this->mysqliMockSlave->expects($this->once())
-			->method('select')
-			->with('user_properties', '*', array( 'up_user' => $this->userId ), $this->anything())
-			->willReturn([]);
+		$this->mysqliMockSlave->expects( $this->once() )
+			->method( 'select' )
+			->with( 'user_properties', '*', array( 'up_user' => $this->userId ), $this->anything() )
+			->willReturn( [] );
 
-		$persistence = new PreferencePersistenceMySQL($this->mysqliMockMaster, $this->mysqliMockSlave);
+		$persistence = new PreferencePersistenceMySQL( $this->mysqliMockMaster, $this->mysqliMockSlave );
 		$preferences = $persistence->get( $this->userId );
 
-		$this->assertTrue( is_array($preferences), "expecting an array" );
-		$this->assertTrue( empty($preferences), "expecting an empty array" );
+		$this->assertTrue( is_array( $preferences ), "expecting an array" );
+		$this->assertTrue( empty( $preferences ), "expecting an empty array" );
 	}
 
 	public function testSave() {
 		$preferences = [$this->testPreference];
-		$this->mysqliMockMaster->expects($this->once())
-			->method('upsert')
+		$this->mysqliMockMaster->expects( $this->once() )
+			->method( 'upsert' )
 			->with(
 				PreferencePersistenceMySQL::USER_PREFERENCE_TABLE,
-				PreferencePersistenceMySQL::createTuplesFromPreferences($this->userId, $preferences),
+				PreferencePersistenceMySQL::createTuplesFromPreferences( $this->userId, $preferences ),
 				[],
-			 	PreferencePersistenceMySQL::$UPSERT_SET_BLOCK)
-			->willReturn(true);
+			 	PreferencePersistenceMySQL::$UPSERT_SET_BLOCK )
+			->willReturn( true );
 
-		$persistence = new PreferencePersistenceMySQL($this->mysqliMockMaster, $this->mysqliMockSlave);
+		$persistence = new PreferencePersistenceMySQL( $this->mysqliMockMaster, $this->mysqliMockSlave );
 		$ret = $persistence->save( $this->userId, [$this->testPreference] );
 
 		$this->assertTrue( $ret, "expected true" );
 	}
 
 	public function testCreateTuples() {
-		$input = [ new Preference('name-a', 'value-a'), new Preference('name-b', 'value-b') ];
+		$input = [ new Preference( 'name-a', 'value-a' ), new Preference( 'name-b', 'value-b' ) ];
 
-		$output = PreferencePersistenceMySQL::createTuplesFromPreferences($this->userId, $input);
+		$output = PreferencePersistenceMySQL::createTuplesFromPreferences( $this->userId, $input );
 
-		$this->assertTrue( is_array($output), "failed to create tuple array" );
+		$this->assertTrue( is_array( $output ), "failed to create tuple array" );
 		$this->assertEquals( 1, $output[0]['up_user'], "user failed to match" );
 		$this->assertEquals( 'name-a', $output[0]['up_property'], "name failed to match" );
 		$this->assertEquals( 'value-a', $output[0]['up_value'], "value failed to match" );
 	}
 
 	public function testCreateTuplesEmpty() {
-		$persistence = new PreferencePersistenceMySQL($this->mysqliMockMaster, $this->mysqliMockSlave);
+		$persistence = new PreferencePersistenceMySQL( $this->mysqliMockMaster, $this->mysqliMockSlave );
 		$input = [ ];
-		$output = PreferencePersistenceMySQL::createTuplesFromPreferences($this->userId, $input);
+		$output = PreferencePersistenceMySQL::createTuplesFromPreferences( $this->userId, $input );
 
-		$this->assertTrue( is_array($output), "unexpected output" );
+		$this->assertTrue( is_array( $output ), "unexpected output" );
 		$this->assertEquals( [], $output, "result failed to match" );
 	}
 
