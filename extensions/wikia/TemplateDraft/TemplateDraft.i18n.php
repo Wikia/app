a96d1f2269d0144a350331a6f2cf751c6344674f
adamkarminski <adamkarminski@gmail.com>
2015-06-23 14:02:14 +0200

    CE-1996 Provide basic TemplateDraft setup and HTML scaffolding for the module

---
+++ extensions/wikia/TemplateDraft/TemplateDraft.i18n.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * TemplateDraft message file
+ */
+
+$messages = [];
+
+/**
+ * English (en)
+ */
+$messages['en'] = [
+	'templatedraft-description' => 'TemplateDraft extension',
+	/**
+	 * Right rail module
+	 */
+	'templatedraft-module-title' => 'Migrate this infobox',
+	'templatedraft-module-subtitle' => 'This template does not use the new [[Help:PortableInfoboxes|infobox markup]].',
+	'templatedraft-module-content' => 'We can generate a draft version of the markup from your existing infobox and save it as a sub-page so you can review it and make any needed changes.',
+	'templatedraft-module-button' => 'Generate draft markup',
+	'templatedraft-module-closelink' => 'This is not an infobox',
+];
+
+/**
+ * Documentation (qqq)
+ */
+$messages['qqq'] = [
+	'templatedraft-description' => '{{desc}}',
+	/**
+	 * Right rail module
+	 */
+	'templatedraft-module-title' => 'A title of the TemplateDraft module that appears in the right rail on Template pages.',
+	'templatedraft-module-subtitle' => 'A title of the TemplateDraft module that appears in the right rail on Template pages.',
+	'templatedraft-module-content' => 'A content of the TemplateDraft module that appears in the right rail on Template pages.',
+	'templatedraft-module-button' => 'Generate draft markup',
+];
---
+++ skins/oasis/modules/templates/TemplateDraftModule_Index.php
@@ -0,0 +1,9 @@
+<section class="templatedraft-module module">
+	<h2><?= wfMessage( 'templatedraft-module-title' )->escaped() ?></h2>
+	<p class="templatedraft-module-subtitle"><?= wfMessage( 'templatedraft-module-subtitle' )->parse() ?></p>
+	<p><?= wfMessage( 'templatedraft-module-content' )->escaped() ?></p>
+	<button class="templatedraft-module-button" type="button"><?= wfMessage( 'templatedraft-module-button' )->escaped() ?></button>
+	<p class="templatedraft-module-closelink">
+		<a href="#"><?= wfMessage( 'templatedraft-module-closelink' )->escaped() ?></a>
+	</p>
+</section>
