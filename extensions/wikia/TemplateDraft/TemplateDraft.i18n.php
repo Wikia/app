70cc0748e067acd21f2cd74a48c26dd5e2887511
adamkarminski <adamkarminski@gmail.com>
2015-06-23 14:07:38 +0200

    CE-1996 Fix the i18n documentation

--- extensions/wikia/TemplateDraft/TemplateDraft.i18n.php
+++ extensions/wikia/TemplateDraft/TemplateDraft.i18n.php
@@ -31,5 +31,6 @@ $messages['qqq'] = [
 	'templatedraft-module-title' => 'A title of the TemplateDraft module that appears in the right rail on Template pages.',
 	'templatedraft-module-subtitle' => 'A title of the TemplateDraft module that appears in the right rail on Template pages.',
 	'templatedraft-module-content' => 'A content of the TemplateDraft module that appears in the right rail on Template pages.',
-	'templatedraft-module-button' => 'Generate draft markup',
+	'templatedraft-module-button' => 'A button shown in a right rail module that gets user to a subpage with a draft of a template.',
+	'templatedraft-module-closelink' => 'A link that closes the right rail module if it is not on a template page with an infobox in it.',
 ];
