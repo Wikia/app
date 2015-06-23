4182f2e008f1ea29fe90af6dbdb75b5c38e2f865
lizlux <liz@lizlux.net>
2015-06-23 15:26:42 -0700

    fixing JS error on WikiaMobile

--- extensions/wikia/WikiaMobile/js/sections.js
+++ extensions/wikia/WikiaMobile/js/sections.js
@@ -23,7 +23,7 @@ define( 'sections', ['jquery', 'wikia.window'], function ( $, window ) {
 	function getHeaders(){
 		//we switch nodeList to Array to use filter / forEach type methods
 		return Array.prototype.slice.apply( d.querySelectorAll(
-			'h2[id]:not( [id=""] ), h3[id]:not( [id=""] ), h4[id]:not( [id=""] )' ) );
+			'h2[id]:not([id=""]), h3[id]:not([id=""]), h4[id]:not([id=""])' ) );
 	}
 
 	sections = getHeaders();
