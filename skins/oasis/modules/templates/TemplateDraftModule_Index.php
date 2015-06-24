9a561d4fe906035de0e0e740f32501dfd5276e2e
adamkarminski <adamkarminski@gmail.com>
2015-06-24 12:44:16 +0200

    CE-1996 Fix the Generate draft button

--- skins/oasis/modules/templates/TemplateDraftModule_Index.php
+++ skins/oasis/modules/templates/TemplateDraftModule_Index.php
@@ -2,8 +2,10 @@
 	<h2><?= wfMessage( 'templatedraft-module-title' )->escaped() ?></h2>
 	<p class="templatedraft-module-subtitle"><?= wfMessage( 'templatedraft-module-subtitle' )->parse() ?></p>
 	<p><?= wfMessage( 'templatedraft-module-content' )->escaped() ?></p>
-	<a href="<?= $draftUrl ?>" class="templatedraft-module-button" type="button">
-		<?= wfMessage( 'templatedraft-module-button' )->escaped() ?>
+	<a href="<?= $draftUrl ?>" title="<?= wfMessage( 'templatedraft-module-button' )->escaped() ?>" target="_blank">
+		<button class="templatedraft-module-button" type="button">
+			<?= wfMessage( 'templatedraft-module-button' )->escaped() ?>
+		</button>
 	</a>
 	<p class="templatedraft-module-closelink">
 		<a href="#"><?= wfMessage( 'templatedraft-module-closelink' )->escaped() ?></a>
