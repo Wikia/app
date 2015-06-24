5e915aabdd2c37fd961b1c58b47d4919a86c4c56
nmonterroso <nelson@wikia-inc.com>
2015-06-24 10:58:27 -0700

    separate injector builder logic from injector

--- includes/wikia/injector/InjectorInitializer.php
+++ includes/wikia/injector/InjectorInitializer.php
@@ -5,20 +5,16 @@ use Wikia\DependencyInjection\Injector;
 use Wikia\Persistence\User\PreferencePersistenceModuleMySQL;
 use Wikia\Service\User\PreferenceKeyValueService;
 use Wikia\Service\User\PreferenceService;
+use Wikia\DependencyInjection\InjectorBuilder;
 
 class InjectorInitializer {
 	public static function init(CacheProvider $cacheProvider = null) {
-		static $initialized = false;
-
-		if ($initialized) {
-			return;
-		}
-
-		Injector::getInjector()
-			->withCache($cacheProvider)
-			->bindClass(PreferenceService::class, PreferenceKeyValueService::class)
-			->addModule(self::getPreferencePersistenceModule())
-			->build();
+		Injector::setInjector(
+			(new InjectorBuilder())
+				->withCache($cacheProvider)
+				->bindClass(PreferenceService::class, PreferenceKeyValueService::class)
+				->addModule(self::getPreferencePersistenceModule())
+				->build());
 	}
 
 	private static function getPreferencePersistenceModule() {
