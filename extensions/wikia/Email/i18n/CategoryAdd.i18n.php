043f4305bc5bb5937714576e7d263c8de8add39b
James Sutterfield <james.sutterfield@gmail.com>
2015-06-24 18:40:04 +0000

    SOC-533 add messages

--- extensions/wikia/Email/i18n/CategoryAdd.i18n.php
+++ extensions/wikia/Email/i18n/CategoryAdd.i18n.php
@@ -12,3 +12,70 @@ $messages['qqq'] = array(
 	'emailext-categoryadd-details' => "Line informing the user a new page has been added to a category they're following",
 	'emailext-categoryadd-see-all-pages' => 'Line which links to the category page the user is watching. Call to Action is to see all pages in that category. $1 -> category page name, $2 -> category page url',
 );
+
+$messages['de'] = array(
+	'emailext-categoryadd-subject' => 'Auf {{SITENAME}} wurde eine Seite zu $1 hinzugefügt!',
+	'emailext-categoryadd-details' => 'Einer Kategorie, der du auf {{SITENAME}} folgst, wurde eine Seite hinzugefügt.',
+	'emailext-categoryadd-footer-1' => 'Alle Seiten in [$1 $2] ansehen.',
+);
+
+$messages['es'] = array(
+	'emailext-categoryadd-subject' => 'Se ha añadido una página a $1 en {{SITENAME}}!',
+	'emailext-categoryadd-details' => 'Se ha añadido una página a una categoría que sigues en {{SITENAME}}.',
+	'emailext-categoryadd-footer-1' => 'Ver todas las páginas en [$1 $2].',
+);
+
+$messages['fr'] = array(
+	'emailext-categoryadd-subject' => 'Une page a été ajoutée à $1 sur {{SITENAME}} !',
+	'emailext-categoryadd-details' => 'Une page a été ajoutée à une catégorie que vous suivez sur {{SITENAME}}.',
+	'emailext-categoryadd-footer-1' => 'Afficher toutes les pages de [$1 $2].',
+);
+
+$messages['it'] = array(
+	'emailext-categoryadd-subject' => 'Una pagina è stata aggiunta a $1 in {{SITENAME}}!',
+	'emailext-categoryadd-details' => 'Una pagina è stato aggiunta a una categoria che stai seguendo su {{SITENAME}}.',
+	'emailext-categoryadd-footer-1' => 'Vedi tutte le pagine in [$1 $2].',
+);
+
+$messages['ja'] = array(
+	'emailext-categoryadd-subject' => '{{SITENAME}}の$1にページが追加されました',
+	'emailext-categoryadd-details' => '{{SITENAME}}でフォローしているカテゴリにページが追加されました。',
+	'emailext-categoryadd-footer-1' => '[$1 $2]ですべてのページをご覧ください。',
+);
+
+$messages['nl'] = array(
+	'emailext-categoryadd-subject' => 'A page has been added to $1 on {{SITENAME}}!',
+	'emailext-categoryadd-details' => 'A page has been added to a category you’re following on {{SITENAME}}.',
+	'emailext-categoryadd-footer-1' => 'See all pages in [$1 $2].',
+);
+
+$messages['pl'] = array(
+	'emailext-categoryadd-subject' => 'Dodano stronę do kategorii $1 na {{SITENAME}}!',
+	'emailext-categoryadd-details' => 'Dodano stronę do kategorii, którą obserwujesz na {{SITENAME}}.',
+	'emailext-categoryadd-footer-1' => 'Zobacz wszystkie strony w kategorii [$1 $2].',
+);
+
+$messages['pt'] = array(
+	'emailext-categoryadd-subject' => 'Uma página foi adicionada a $1 na {{SITENAME}}!',
+	'emailext-categoryadd-details' => 'Uma página foi adicionada a uma categoria que você está seguindo na {{SITENAME}}.',
+	'emailext-categoryadd-footer-1' => 'Ver todas as páginas em [$1 $2].',
+);
+
+$messages['ru'] = array(
+	'emailext-categoryadd-subject' => 'На {{SITENAME}} была добавлена страница в категорию «$1»!',
+	'emailext-categoryadd-details' => 'В отслеживаемую вами категорию на {{SITENAME}} была добавлена страница.',
+	'emailext-categoryadd-footer-1' => 'Посмотреть все страницы данной категории — [$1 $2].',
+);
+
+$messages['zh-hans'] = array(
+	'emailext-categoryadd-subject' => '{{SITENAME}}网站的$1页面上添加了一个新的类别！',
+	'emailext-categoryadd-details' => '{{SITENAME}}网站上您关注的类别中添加了一个新的页面。',
+	'emailext-categoryadd-footer-1' => '查看[$1 $2]类的所有页面。',
+);
+
+$messages['zh-tw'] = array(
+	'emailext-categoryadd-subject' => '{{SITENAME}}網路的$1頁面上添加了一個新的類別！',
+	'emailext-categoryadd-details' => '{{SITENAME}}網路上你關注的類別中添加了一個新的頁面。',
+	'emailext-categoryadd-footer-1' => '查看[$1 $2]類的所有頁面。',
+);
+
