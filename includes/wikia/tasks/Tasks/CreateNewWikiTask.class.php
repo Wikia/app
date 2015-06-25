0358c706fdb9cdeaa6fbb84b3744697228087add
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-06-25 11:47:11 +0000

    CE-1934: Uses default staff user

--- includes/wikia/tasks/Tasks/CreateNewWikiTask.class.php
+++ includes/wikia/tasks/Tasks/CreateNewWikiTask.class.php
@@ -147,8 +147,7 @@ class CreateNewWikiTask extends BaseTask {
 				];
 				if ( $sourceTitle->getPrefixedText() !== $targetTitle->getPrefixedText() ) {
 					$saveUser = $wgUser;
-					$wgUser = \Wikia::staffForLang( $wgContLanguageCode );
-					$wgUser = ( $wgUser instanceof \User ) ? $wgUser : \User::newFromName( \CreateWiki::DEFAULT_STAFF );
+					$wgUser = \User::newFromName( \CreateWiki::DEFAULT_STAFF );
 
 					$err = $sourceTitle->moveTo( $targetTitle, false, "SEO" );
 					if ( $err !== true ) {
