0bf1f87c4a1e8d2f9fb82287c758e52d7517dff2
tiffanyperumpail <tperumpail@berkeley.edu>
2015-06-26 18:20:24 -0700

    Changed getOption to getGlobalAttribute for NOT_CONFIRMED_LOGIN_OPTION_NAME constant set to NotConfirmedLogin

--- extensions/wikia/UserLogin/UserLoginSpecialController.class.php
+++ extensions/wikia/UserLogin/UserLoginSpecialController.class.php
@@ -197,7 +197,7 @@ class UserLoginSpecialController extends WikiaSpecialPageController {
 				$this->errParam = $response->getVal( 'errParam', '' );
 
 				// set the language object
-				$code = $this->wg->request->getVal( 'uselang', $this->wg->User->getOption( 'language' ) );
+				$code = $this->wg->request->getVal( 'uselang', $this->wg->User->getGlobalPreference( 'language' ) );
 				$this->wg->Lang = Language::factory( $code );
 
 				if ( $this->result == 'ok' ) {
@@ -428,11 +428,11 @@ class UserLoginSpecialController extends WikiaSpecialPageController {
 		switch ( $loginCase ) {
 			case LoginForm::SUCCESS:
 				// first check if user has confirmed email after sign up
-				if ( $this->wg->User->getOption( self::NOT_CONFIRMED_SIGNUP_OPTION_NAME ) &&
+				if ( $this->wg->User->getGlobalFlag( self::NOT_CONFIRMED_SIGNUP_OPTION_NAME ) &&
 					/*
 					 * Remove when SOC-217 ABTest is finished
 					 */
-					$this->wg->User->getOption( self::NOT_CONFIRMED_LOGIN_OPTION_NAME ) !== self::NOT_CONFIRMED_LOGIN_ALLOWED
+					$this->wg->User->getGlobalAttribute( self::NOT_CONFIRMED_LOGIN_OPTION_NAME ) !== self::NOT_CONFIRMED_LOGIN_ALLOWED
 					/*
 					 * end remove
 					 */
@@ -461,7 +461,7 @@ class UserLoginSpecialController extends WikiaSpecialPageController {
 					wfRunHooks('UserLoginComplete', array(&$this->wg->User, &$injected_html));
 
 					// set rememberpassword option
-					if ( (bool)$loginForm->mRemember != (bool)$this->wg->User->getOption('rememberpassword') ) {
+					if ( (bool)$loginForm->mRemember != (bool)$this->wg->User->getGlobalPreference('rememberpassword') ) {
 						$this->wg->User->setOption( 'rememberpassword', $loginForm->mRemember ? 1 : 0 );
 						$this->wg->User->saveSettings();
 					} else {
@@ -523,7 +523,7 @@ class UserLoginSpecialController extends WikiaSpecialPageController {
 
 				$attemptedUser = User::newFromName( $loginForm->mUsername );
 				if ( !is_null( $attemptedUser ) ) {
-					$disOpt = $attemptedUser->getOption('disabled');
+					$disOpt = $attemptedUser->getGlobalFlag('disabled');
 					if( !empty($disOpt) ||
 						(defined( 'CLOSED_ACCOUNT_FLAG' ) && $attemptedUser->getRealName() == CLOSED_ACCOUNT_FLAG ) ){
 						#either closed account flag was present, override fail message
@@ -609,7 +609,7 @@ class UserLoginSpecialController extends WikiaSpecialPageController {
 				$this->result = 'error';
 				$this->msg = wfMessage('userlogin-error-throttled-mailpassword', round( $this->wg->PasswordReminderResendTime, 3) )->escaped();
 			} else {
-				$emailTextTemplate = $this->app->renderView( "UserLogin", "GeneralMail", array('language' => $user->getOption('language'), 'type' => 'password-email') );
+				$emailTextTemplate = $this->app->renderView( "UserLogin", "GeneralMail", array('language' => $user->getGlobalPreference('language'), 'type' => 'password-email') );
 				$result = $loginForm->mailPasswordInternal( $user, true, 'userlogin-password-email-subject', 'userlogin-password-email-body', $emailTextTemplate );
 				if( !$result->isGood() ) {
 					$this->result = 'error';
