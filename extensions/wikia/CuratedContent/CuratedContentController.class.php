20b1b89b49f3bc66b978808bc285a3ec49731d00
Warkot <mateusz.warkocki@wikia-inc.com>
2015-06-29 23:45:02 +0200

    CONCF-672 | removed check for existance of MercuryApiHooks class

--- extensions/wikia/CuratedContent/CuratedContentController.class.php
+++ extensions/wikia/CuratedContent/CuratedContentController.class.php
@@ -395,11 +395,10 @@ class CuratedContentController extends WikiaController {
 		global $wgServer;
 
 		$content = F::app()->wg->WikiaCuratedContent;
-		$mercuryApiExists = class_exists( 'MercuryApiHooks' );
 
 		( new SquidUpdate( array_unique( array_reduce(
 			$content,
-			function ( $urls, $item ) use ( $wgServer, $mercuryApiExists ) {
+			function ( $urls, $item ) use ( $wgServer ) {
 				if ( $item[ 'title' ] !== '' && empty( $item[ 'featured' ] ) ) {
 					// Purge section URLs using urlencode() (standard for MediaWiki), which uses implements RFC 1738
 					// https://tools.ietf.org/html/rfc1738#section-2.2 - spaces encoded as `+`.
@@ -414,13 +413,11 @@ class CuratedContentController extends WikiaController {
 					// Mercury web app uses this variant - request from Hapi.js to MediaWiki.
 					$urls[ ] = self::getUrl( 'getList' ) . '&section=' . self::encodeURIQueryParam( $item[ 'title' ] );
 					// Mercury web app uses this variant - request from Ember.js to Hapi.js.
-					if ( $mercuryApiExists ) {
-						$urls[ ] =
-							$wgServer .
-							MercuryApiHooks::SERVICE_API_BASE .
-							MercuryApiHooks::SERVICE_API_CURATED_CONTENT .
-							self::encodeURIQueryParam( $item[ 'title' ] );
-					}
+					$urls[ ] =
+						$wgServer .
+						MercuryApiHooks::SERVICE_API_BASE .
+						MercuryApiHooks::SERVICE_API_CURATED_CONTENT .
+						self::encodeURIQueryParam( $item[ 'title' ] );
 				}
 				return $urls;
 			},
