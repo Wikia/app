5cca31d7817a8b165aed432363e6a3b90c196da3
Artur Klajnerok <a.klajnerok@gmail.com>
2015-06-30 11:29:53 -0700

    getUserPropertyValue removed extensions/wikia/EditPageLayout/models/EditorUserPropertiesHandler.class.php

--- extensions/wikia/EditPageLayout/models/EditorUserPropertiesHandler.class.php
+++ extensions/wikia/EditPageLayout/models/EditorUserPropertiesHandler.class.php
@@ -23,21 +23,7 @@ class EditorUserPropertiesHandler extends WikiaUserPropertiesHandlerBase {
 		}
 		return $results;
 	}
-
-	public function getUserPropertyValue($propertyName, $defaultOption = '') {
-		$results = new stdClass();
-		$results->success = false;
-		$this->throwExceptionForAnons();
-
-		if ($propertyName === false) {
-			throw new Exception('Invalid property name', self::EXCEPTION_INVALID_PROPERTY_NAME);
-		}
-
-		$results->success = true;
-		$results->value = $this->wg->User->getOption($propertyName, $defaultOption);
-		return $results;
-	}
-
+    
 	public function getEditorMainPageNoticePropertyForCurrentUser() {
 		return $this->wg->User->getLocalAttribute(MAIN_PAGE_NOTIFICATIONS_HIDDEN_PROP_NAME, $this->app->wg->CityId);
 	}
