e7334ac4427c4b1a2aac5b4cc667c087c4d2762c
James Sutterfield <james.sutterfield@gmail.com>
2015-07-01 17:46:09 +0000

    SOC-534 updated messges, created crowdin conf

--- extensions/wikia/Email/i18n/UserRightsChanged.i18n.php
+++ extensions/wikia/Email/i18n/UserRightsChanged.i18n.php
@@ -3,10 +3,10 @@ $messages = array();
 
 $messages['en'] = array(
 	'emailext-user-rights-changed-subject' => "Your user rights on {{SITENAME}} have been changed.",
-	'emailext-user-rights-changed-button-text' => "FULL SUMMARY",
+	'emailext-user-rights-changed-button-text' => "Full Summary",
 );
 
 $messages['qqq'] = array(
-	'emailext-user-rights-changed-subject' => "TODO ADD THIS",
-	'emailext-user-rights-changed-button-text' => "TODO ADD THIS"
+	'emailext-user-rights-changed-subject' => "Subject for the email a user receives when their rights have been changed (ie, they are give more abilities on Wikia)",
+	'emailext-user-rights-changed-button-text' => "Text for the button link in the email which points to a log of all changes to the user's rights"
 );
