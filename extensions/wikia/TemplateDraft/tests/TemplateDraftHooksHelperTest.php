bc24087e3c84238b667cd3bc5b53f3c6346dac65
tor <tor@wikia-inc.com>
2015-07-02 14:26:09 +0000

    corrected test

--- extensions/wikia/TemplateDraft/tests/TemplateDraftHooksHelperTest.php
+++ extensions/wikia/TemplateDraft/tests/TemplateDraftHooksHelperTest.php
@@ -143,7 +143,7 @@ class TemplateDraftHooksHelperTest extends WikiaBaseTest {
 		 */
 		return [
 			[ true, true, true ],
-			[ true, false, false ],
+			[ true, false, true ],
 			[ false, true, false ],
 			[ false, false, false ],
 		];
