14cc2cdd19bd3da30be4dfbc3f20cb76db6bfedb
James Sutterfield <james@wikia-inc.com>
2015-07-02 16:48:29 -0700

    SOC-876 update restored page emails to use new templates and email extension

---
+++ extensions/wikia/Email/i18n/WatchedPageRestored.i18n.php
@@ -0,0 +1,14 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+	'emailext-watchedpage-article-restored-subject' => '$1 on {{SITENAME}} has been restored by $2.',
+	'emailext-watchedpage-article-restored-summary' => '$2 on {{SITENAME}} has been restored. Check it out!',
+	'emailext-watchedpage-article-restored-button-text' => 'See Article',
+);
+
+$messages['qqq'] = array(
+	'emailext-watchedpage-article-restored-subject' => 'Subject of email a user recieves when an article they are following is restored (gets undeleted). $1 -> article name, $2 -> username of person who restored the article',
+	'emailext-watchedpage-article-restored-summary' => "Line telling the user an article they're following which was deleted has been restored (undeleted). $2 -> article name",
+	'emailext-watchedpage-article-restored-button-text' => 'Text of button link which takes the user to the restored articld',
+);
--- includes/EmailNotification.php
+++ includes/EmailNotification.php
@@ -472,6 +472,8 @@ class EmailNotification {
 			$controller = 'Email\Controller\WatchedPageUnprotected';
 		} elseif ( $this->isArticlePageDeleted() ) {
 			$controller = 'Email\Controller\WatchedPageDeleted';
+		} elseif ( $this->isArticlePageRestored() ) {
+			$controller = 'Email\Controller\WatchedPageRestored';
 		} elseif ( $this->isArticleComment() ) {
 			$controller = 'Email\Controller\ArticleComment';
 		} elseif ( $this->isBlogComment() ) {
@@ -564,6 +566,15 @@ class EmailNotification {
 	 */
 	private function isArticlePageDeleted() {
 		return in_array( $this->action, [ 'delete' ] );
+	}
+
+	/**
+	 * Check if the performed action is page restoration
+	 *
+	 * @return bool
+	 */
+	private function isArticlePageRestored() {
+		return $this->action == "restore";
 	}
 
 	/**
