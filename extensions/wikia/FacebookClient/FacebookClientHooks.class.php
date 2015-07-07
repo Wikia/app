fd21ea3e709c5614a8aee531625107b6cca4616d
Garth Webb <garth@wikia-inc.com>
2015-07-07 13:54:53 -0700

    Use the UserLogout hook and avoid having to check for the wgEnableFacebookExt global directly.

--- extensions/wikia/FacebookClient/FacebookClientHooks.class.php
+++ extensions/wikia/FacebookClient/FacebookClientHooks.class.php
@@ -144,4 +144,9 @@ class FacebookClientHooks {
 
 		return true;
 	}
+
+	public static function onUserLogout( &$user ) {
+		// Clean up any facebook cookies/data
+		FacebookClient::getInstance()->logout();
+	}
 }
