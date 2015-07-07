531dbd85d209ec9fc46ea4c52a2396703b6a5ced
Lucas 'Tor' Garczewski <tor@wikia-inc.com>
2015-07-07 15:36:20 +0200

    Update InsightsUnconvertedInfoboxesModel.php

--- extensions/wikia/Insights/models/InsightsUnconvertedInfoboxesModel.php
+++ extensions/wikia/Insights/models/InsightsUnconvertedInfoboxesModel.php
@@ -43,11 +43,12 @@ class InsightsUnconvertedInfoboxesModel extends InsightsQuerypageModel {
 	}
 
 	public function getAltActionUrl( Title $title ) {
-		if ( !class_exists( 'TemplateConverter' ) ) {
+		$subpage = Title::newFromText( $title->getText() . "/" . wfMessage('templatedraft-subpage')->escaped() , NS_TEMPLATE );
+
+		if ( $subpage === null ) {
+			// something went terribly wrong, quit early
 			return '';
 		}
-
-		$subpage = Title::newFromText( $title->getText() . "/" . wfMessage('templatedraft-subpage')->escaped() , NS_TEMPLATE );
 
 		return $subpage->getFullUrl( [
 			'action' => 'edit',
