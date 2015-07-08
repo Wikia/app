d7d87d89f5cbc89d9d98aa91a6e9db42e4cf88f8
James Sutterfield <james@wikia-inc.com>
2015-07-07 17:39:35 -0700

    SOC-534 Support condition where target user is not changed user

--- extensions/wikia/Email/Controller/UserRightsChangedController.class.php
+++ extensions/wikia/Email/Controller/UserRightsChangedController.class.php
@@ -8,15 +8,20 @@ use \Email\EmailController;
 class UserRightsChangedController extends EmailController {
 
 	private $details;
+	private $changedUser;
 
 	public function initEmail() {
 		$this->details = $this->getVal( 'summary' );
+		$this->changedUser = $this->getVal( 'pageTitle' );
 		$this->assertValidParams();
 	}
 
 	private function assertValidParams() {
 		if ( empty( $this->details ) ) {
 			throw new Check( "'summary' parameter must not be empty" );
+		}
+		if ( empty( $this->changedUser ) ) {
+			throw new Check( "'pageTitle' parameter must not be empty" );
 		}
 	}
 
@@ -26,7 +31,7 @@ class UserRightsChangedController extends EmailController {
 	public function body() {
 		$this->response->setData( [
 			'salutation' => $this->getSalutation(),
-			'summary' => $this->getMessage( "emailext-user-rights-changed-subject" )->parse(),
+			'summary' => $this->getSubject(),
 			'details' => $this->details,
 			'editorProfilePage' => $this->getCurrentProfilePage(),
 			'editorUserName' => $this->getCurrentUserName(),
@@ -37,14 +42,26 @@ class UserRightsChangedController extends EmailController {
 	}
 
 	protected function getSubject() {
-		return $this->getMessage( "emailext-user-rights-changed-subject" )->parse();
+		if ( $this->isTargetUserChangedUser() ) {
+			return $this->getMessage( "emailext-user-rights-changed-subject" )->text();
+		}
+		return $this->getMessage( "emailext-user-rights-changed-subject-follower", $this->changedUser )->text();
 	}
 
+	/**
+	 * Return if the target user (the person receiving the email) is the same user
+	 * who had their rights changed.
+	 *
+	 * @return bool
+	 */
+	private function isTargetUserChangedUser() {
+		return $this->targetUser->getName() == $this->changedUser;
+	}
 
 	private function getButtonLink() {
 		$urlParams = [
 			"type" => "rights",
-			"page" => $this->targetUser->getName()
+			"page" => $this->changedUser
 		];
 		return \SpecialPage::getTitleFor( 'Log' )->getFullURL( $urlParams );
 	}
@@ -58,6 +75,13 @@ class UserRightsChangedController extends EmailController {
 					'value' => "Added user to the bureaucrat group",
 					'label' => 'Summary of Changes',
 					'tooltip' => "Summary of the changes to the user's rights, eg, 'Added user to bureaucrat group'",
+				],
+				[
+					'type' => 'text',
+					'name' => 'pageTitle',
+					'value' => \F::app()->wg->User->getName(),
+					'label' => 'User whose rights were changed',
+					'tooltip' => "User whose rights were changed",
 				],
 			]
 		];
--- extensions/wikia/Email/i18n/UserRightsChanged.i18n.php
+++ extensions/wikia/Email/i18n/UserRightsChanged.i18n.php
@@ -3,10 +3,12 @@ $messages = array();
 
 $messages['en'] = array(
 	'emailext-user-rights-changed-subject' => "Your user rights on {{SITENAME}} have been changed.",
+	'emailext-user-rights-changed-subject-follower' => "The user rights for $1 on {{SITENAME}} have been changed.",
 	'emailext-user-rights-changed-button-text' => "Full Summary",
 );
 
 $messages['qqq'] = array(
 	'emailext-user-rights-changed-subject' => "Subject for the email a user receives when their rights have been changed (ie, they are give more abilities on Wikia)",
+	'emailext-user-rights-changed-subject-follower' => "Subject for the email when the rights of a followed user has been changed (ie, they are give more abilities on Wikia). $1 username of followed user",
 	'emailext-user-rights-changed-button-text' => "Text for the button link in the email which points to a log of all changes to the user's rights"
 );
