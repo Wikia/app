56296fbbc088bae8cb11e59b84c213d459cdf0c8
James Sutterfield <james@wikia-inc.com>
2015-07-09 11:46:51 -0700

    SOC-876 fix typo in qqq

--- extensions/wikia/Email/i18n/WatchedPageRestored.i18n.php
+++ extensions/wikia/Email/i18n/WatchedPageRestored.i18n.php
@@ -10,5 +10,5 @@ $messages['en'] = array(
 $messages['qqq'] = array(
 	'emailext-watchedpage-article-restored-subject' => 'Subject of email a user recieves when an article they are following is restored (gets undeleted). $1 -> article name, $2 -> username of person who restored the article',
 	'emailext-watchedpage-article-restored-summary' => "Line telling the user an article they're following which was deleted has been restored (undeleted). $2 -> article name",
-	'emailext-watchedpage-article-restored-button-text' => 'Text of button link which takes the user to the restored articld',
+	'emailext-watchedpage-article-restored-button-text' => 'Text of button link which takes the user to the restored article',
 );
