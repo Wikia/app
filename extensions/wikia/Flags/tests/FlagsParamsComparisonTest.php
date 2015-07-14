f7ad033935b427139a963f96613fba727a681709
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-07-14 15:08:09 +0000

    CE-1885: Adjust unit tests

--- extensions/wikia/Flags/tests/FlagsParamsComparisonTest.php
+++ extensions/wikia/Flags/tests/FlagsParamsComparisonTest.php
@@ -17,8 +17,10 @@ class FlagsParamsComparisonTest extends WikiaBaseTest {
 
 		$result = $flagsParamsComparison->compareTemplateVariables( $mockTitle, $oldText, $newText, $flagVariables );
 
-		sort( $result );
-		sort( $expected );
+		asort( $result );
+		asort( $result['params'] );
+		asort( $expected );
+		asort( $expected['params'] );
 
 		$this->assertSame( $result, $expected, $msg );
 	}
@@ -43,56 +45,111 @@ class FlagsParamsComparisonTest extends WikiaBaseTest {
 				'template without variables',
 				'template with new {{{newVar}}} variable',
 				[],
-				[ 'newVar' => '' ],
+				[
+					'added' => [ 'newVar' ],
+					'params' => [
+						'newVar' => ''
+					]
+				],
 				'Template with new variable added'
 			],
 			[
 				'template without variables',
 				'template with new {{{newVar}}} two {{{2}}} variables',
 				[],
-				[ 'newVar' => '', 2 => '' ],
+				[
+					'added' => [ 'newVar', 2 ],
+					'params' => [
+						'newVar' => '', 2 => ''
+					]
+				],
 				'Template with two new variables added'
 			],
 			[
 				'template with {{{1}}} variable',
 				'template with removed variable',
 				[ 1 => 'parameter description' ],
-				[],
+				[
+					'removed' => [ 1 ],
+					'params' => []
+				],
 				'Template with all variables removed'
 			],
 			[
 				'template with {{{1}}} and {{{2}}} variables',
 				'template with {{{2}}} one variable removed',
 				[ 1 => 'parameter 1 description', 2 => 'parameter 2 description' ],
-				[ 2 => 'parameter 2 description' ],
+				[
+					'removed' => [ 1 ],
+					'params' => [
+						2 => 'parameter 2 description'
+					]
+				],
 				'Template with one variable removed'
 			],
 			[
 				'template with {{{1}}} and {{{2}}} variables',
 				'template with {{{2}}} one variable removed and one {{{3}}} changed',
 				[ 1 => 'parameter 1 description', 2 => 'parameter 2 description' ],
-				[ 2 => 'parameter 2 description', 3 => '' ],
+				[
+					'added' => [ 3 ],
+					'removed' => [ 1 ],
+					'params' => [
+						2 => 'parameter 2 description', 3 => ''
+					]
+				],
 				'Template with one variable removed and one added'
 			],
 			[
 				'template with {{{1}}} variable',
 				'template with changed {{{2}}} variable',
 				[ 1 => 'parameter description' ],
-				[ 2 => 'parameter description' ],
+				[
+					'changed' => [
+						2 => [
+							'old' => '1',
+							'new' => '2'
+						]
+					],
+					'params' => [
+						2 => 'parameter description'
+					]
+				],
 				'Template with changed variable name'
 			],
 			[
 				'template with {{{1}}} and {{{2}}} and even three {{{third}}} variables',
 				'template with {{{1}}} and even three {{{thirdChanged}}} variables',
 				[ 1 => 'parameter description', 2 => 'second param description', 'third' => 'thirdParam' ],
-				[ 1 => 'parameter description', 'thirdChanged' => 'thirdParam' ],
+				[
+					'changed' => [
+						'thirdChanged' => [
+							'old' => 'third',
+							'new' => 'thirdChanged'
+						]
+					],
+					'removed' => [ 2 ],
+					'params' => [
+						1 => 'parameter description', 'thirdChanged' => 'thirdParam'
+					]
+				],
 				'Template with first variable the same, second removed, third changed'
 			],
 			[
 				'template with {{{1}}} and {{{2}}} and even three {{{third}}} variables',
 				'template with {{{1}}} and {{{4}}} and even three {{{third}}} variables',
 				[ 1 => 'parameter description', 2 => 'second param description', 'third' => 'thirdParam' ],
-				[ 1 => 'parameter description', 4 => 'second param description', 'third' => 'thirdParam' ],
+				[
+					'changed' => [
+						4 => [
+							'old' => '2',
+							'new' => '4'
+						]
+					],
+					'params' => [
+						1 => 'parameter description', 4 => 'second param description', 'third' => 'thirdParam'
+					]
+				],
 				'Template with one variable changed'
 			],
 		];
