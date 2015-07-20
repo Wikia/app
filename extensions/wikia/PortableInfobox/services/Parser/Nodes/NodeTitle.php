f5fa2236eb9a7dcd376acfb8d4194ec2ec23baee
Diana <dianafa@o2.pl>
2015-07-20 11:31:53 +0000

    remove all HTML tags from inforbox title

--- extensions/wikia/PortableInfobox/services/Parser/Nodes/NodeTitle.php
+++ extensions/wikia/PortableInfobox/services/Parser/Nodes/NodeTitle.php
@@ -4,9 +4,24 @@ namespace Wikia\PortableInfobox\Parser\Nodes;
 class NodeTitle extends Node {
 	public function getData() {
 		if ( !isset( $this->data ) ) {
-			$this->data = [ 'value' => $this->getValueWithDefault( $this->xmlNode ) ];
+			$title = $this->sanitizeInfoboxTitle( $this->getValueWithDefault( $this->xmlNode ) );
+
+			$this->data = [ 'value' => $title ];
 		}
 
 		return $this->data;
+	}
+
+	/**
+	 * removes all html tags and images from title
+	 *
+	 * @param $title
+	 * @return string
+     */
+	private function sanitizeInfoboxTitle( $title ) {
+		$title = strip_tags( $title );
+		$title = trim( $title );
+
+		return $title;
 	}
 }
