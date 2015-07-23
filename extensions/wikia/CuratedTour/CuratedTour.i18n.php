2c6a78bb401b6def8fbae5590b5f6e88d0f371c0
adamkarminski <adamkarminski@gmail.com>
2015-07-23 11:35:59 +0200

    Basic setup for the extension

---
+++ extensions/wikia/CuratedTour/CuratedTour.i18n.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Flags message file
+ */
+
+$messages = [];
+
+/**
+ * English (en)
+ */
+$messages['en'] = [
+
+];
+
+/**
+ * Message documentation
+ */
+$messages['qqq'] = [
+
+];
---
+++ extensions/wikia/CuratedTour/CuratedTour.setup.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Wikia Flags Extension
+ *
+ * Have you ever tried to a content of an article but had to scroll through a dozen of crazy
+ * messages? They were saying something about missing sources and references, something about
+ * an article being messy... While these notifications are very useful in general, you don't
+ * want to see them all the time in every context.
+ *
+ * This extension provides a new way of storing and managing of the Flags that allows them
+ * to be portable and behave accordingly to a given context.
+ *
+ * @author Adam Karmiński <adamk@wikia-inc.com>
+ * @copyright (c) 2015 Wikia, Inc.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgExtensionCredits['other'][] = [
+	'name'				=> 'CuratedTour',
+	'version'			=> '1.0',
+	'author'			=> 'Adam Karmiński',
+	'url'               => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/CuratedTour',
+	'descriptionmsg'    => 'curated-tour-desc',
+];
+
+$wgExtensionMessagesFiles['CuratedTour'] = __DIR__ . '/CuratedTour.i18n.php';
