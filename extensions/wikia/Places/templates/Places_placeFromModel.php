2dff9214f9f8bde603fce3c69270fc5ca345c53c
Maciej Brencz <macbre@wikia-inc.com>
2015-07-24 19:40:41 +0200

    Places: make the markup consistent with thumbs

--- extensions/wikia/Places/templates/Places_placeFromModel.php
+++ extensions/wikia/Places/templates/Places_placeFromModel.php
@@ -9,12 +9,10 @@
 ?>
 <figure class="thumb t<?=$align;?> thumbinner placemap" style="width:<?=$width+2;?>px;" itemscope itemtype="http://schema.org/Place">
 	<img class="thumbimage" src="<?=$url;?>" width="<?=$width;?>" height="<?=$height;?>" data-categories="<?=$categories?>" data-zoom="<?=$zoom;?>" data-lat="<?=$lat;?>" data-lon="<?=$lon;?>" />
-	<figcaption class="thumbcaption">
-		<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
-			<span class="caption"><?= $caption ?></span>
-			<meta itemprop="latitude" content="<?= $lat ?>">
-			<meta itemprop="longitude" content="<?= $lon ?>">
-		</span>
+	<figcaption class="thumbcaption" itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+		<p class="caption"><?= $caption ?></p>
+		<meta itemprop="latitude" content="<?= $lat ?>">
+		<meta itemprop="longitude" content="<?= $lon ?>">
 	</figcaption>
 </figure>
 <?php
