20df8d253c586bba56e76dd2b96e20e7bf6b6013
macbre <maciej.brencz@gmail.com>
2015-07-24 13:08:36 +0200

    Wrap a caption in span.caption

--- extensions/wikia/Places/templates/Places_placeFromModel.php
+++ extensions/wikia/Places/templates/Places_placeFromModel.php
@@ -10,7 +10,8 @@
 <figure class="thumb t<?=$align;?> thumbinner placemap" style="width:<?=$width+2;?>px;" itemscope itemtype="http://schema.org/Place">
 	<img class="thumbimage" src="<?=$url;?>" width="<?=$width;?>" height="<?=$height;?>" data-categories="<?=$categories?>" data-zoom="<?=$zoom;?>" data-lat="<?=$lat;?>" data-lon="<?=$lon;?>" />
 	<figcaption class="thumbcaption">
-		<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates"><?= $caption ?>
+		<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+			<span class="caption"><?= $caption ?></span>
 			<meta itemprop="latitude" content="<?= $lat ?>">
 			<meta itemprop="longitude" content="<?= $lon ?>">
 		</span>
