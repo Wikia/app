c5e91542396afd3cc20b29c47e6a33c1643b1639
agleeson <katanka.gleeson@gmail.com>
2015-07-28 22:06:02 +0000

    HG-785 - test

--- extensions/wikia/ArticleAsJson/ArticleAsJson.class.php
+++ extensions/wikia/ArticleAsJson/ArticleAsJson.class.php
@@ -17,7 +17,7 @@ class ArticleAsJson extends WikiaService {
 	const MEDIA_CONTEXT_INFOBOX = 'infobox';
 
 	private static function createMarker( $width = 0, $height = 0, $isGallery = false ){
-		$blankImgUrl = F::app()->wg->blankImgUrl;
+		$blankImgUrl = 'foo';//F::app()->wg->blankImgUrl;
 		$id = count( self::$media ) - 1;
 		$classes = 'article-media' . ($isGallery ? ' gallery' : '');
 		$width = !empty( $width ) ? " width='{$width}'" : '';
