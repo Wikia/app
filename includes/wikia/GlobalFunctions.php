7031236a914ca082b54d01cd3e00497ddc023649
Inez Korczynski <inez@wikia-inc.com>
2015-07-31 20:19:40 +0000

    INT-133: Added unit tests for mb_pathinfo

--- includes/wikia/GlobalFunctions.php
+++ includes/wikia/GlobalFunctions.php
@@ -1618,10 +1618,18 @@ function wfGetUniqueArrayCI( array $arr ) {
  * Like pathinfo but with support for multibyte - copied from http://php.net/manual/en/function.pathinfo.php#107461
  */
 function mb_pathinfo($filepath) {
-	preg_match('%^(.*?)[\\\\/]*(([^/\\\\]*?)(\.([^\.\\\\/]+?)|))[\\\\/\.]*$%im',$filepath,$m);
-	if($m[1]) $ret['dirname']=$m[1];
-	if($m[2]) $ret['basename']=$m[2];
-	if($m[5]) $ret['extension']=$m[5];
-	if($m[3]) $ret['filename']=$m[3];
+	preg_match('%^(.*?)[\\\\/]*(([^/\\\\]*?)(\.([^\.\\\\/]+?)|))[\\\\/\.]*$%im', $filepath, $m);
+	if($m[1]) {
+		$ret['dirname'] = $m[1];
+	}
+	if($m[2]) {
+		$ret['basename'] = $m[2];
+	}
+	if($m[5]) {
+		$ret['extension'] = $m[5];
+	}
+	if($m[3]) {
+		$ret['filename'] = $m[3];
+	}
 	return $ret;
 }
