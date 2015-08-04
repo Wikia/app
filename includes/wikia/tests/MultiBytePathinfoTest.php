7f6a8e2df6f4414418fcfe61ee994ba135b44762
grunny <mwgrunny@gmail.com>
2015-08-04 10:10:20 +1000

    Formatting per conventions

--- includes/wikia/tests/MultiBytePathinfoTest.php
+++ includes/wikia/tests/MultiBytePathinfoTest.php
@@ -7,31 +7,30 @@
 class MultiBytePathInfoTest extends WikiaBaseTest {
 
 	public function testAll() {
-		$this->assertEquals(mb_pathinfo("すどく.txt"), array(
-			"basename" => "すどく.txt",
-			"extension" => "txt",
-			"filename" => "すどく"
-		));
+		$this->assertEquals( mb_pathinfo( 'すどく.txt' ), [
+			'basename' => 'すどく.txt',
+			'extension' => 'txt',
+			'filename' => 'すどく'
+		] );
 
-		$this->assertEquals(mb_pathinfo("sudoku.すどく.txt"), array(
-			"basename" => "sudoku.すどく.txt",
-			"extension" => "txt",
-			"filename" => "sudoku.すどく"
-		));
+		$this->assertEquals( mb_pathinfo( 'sudoku.すどく.txt' ), [
+			'basename' => 'sudoku.すどく.txt',
+			'extension' => 'txt',
+			'filename' => 'sudoku.すどく'
+		] );
 
-		$this->assertEquals(mb_pathinfo("/dir_すどく/すどく.txt"), array(
-			"dirname" => "/dir_すどく",
-			"basename" => "すどく.txt",
-			"extension" => "txt",
-			"filename" => "すどく"
-		));
+		$this->assertEquals( mb_pathinfo( '/dir_すどく/すどく.txt' ), [
+			'dirname' => '/dir_すどく',
+			'basename' => 'すどく.txt',
+			'extension' => 'txt',
+			'filename' => 'すどく'
+		] );
 
-		$this->assertEquals(mb_pathinfo("/dir_すどく/すどく.すどく.txt"), array(
-			"dirname" => "/dir_すどく",
-			"basename" => "すどく.すどく.txt",
-			"extension" => "txt",
-			"filename" => "すどく.すどく"
-		));
-
+		$this->assertEquals( mb_pathinfo( '/dir_すどく/すどく.すどく.txt' ), [
+			'dirname' => '/dir_すどく',
+			'basename' => 'すどく.すどく.txt',
+			'extension' => 'txt',
+			'filename' => 'すどく.すどく'
+		] );
 	}
 }
