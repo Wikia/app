8e90d3b36525f6ac40b12ddc5061a507ed24cc2c
Rebekah Cunningham <rcunningham@wikia-inc.com>
2015-08-06 14:02:09 -0700

    Merge pull request #7957 from Wikia/SOC-1010
    SOC-1010 Use controller names as categories and simplify everything

--- extensions/wikia/Email/Controller/WelcomeController.class.php
+++ extensions/wikia/Email/Controller/WelcomeController.class.php
@@ -3,12 +3,10 @@
 namespace Email\Controller;
 
 use Email\EmailController;
-use Email\Check;
-use Email\Tracking\TrackingCategories;
 use Email\ImageHelper;
 
 class WelcomeController extends EmailController {
-	const TRACKING_CATEGORY = TrackingCategories::WELCOME;
+
 	const MOBILE_URL_IOS = 'https://itunes.apple.com/us/artist/wikia-inc./id422467077';
 	const MOBILE_URL_ANDROID = 'https://play.google.com/store/apps/developer?id=Wikia,+Inc.';
 
