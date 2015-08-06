7e30f776db4461093109852128efa1fef1ea8b53
drets <dmytro.rets@wikia-inc.com>
2015-08-06 09:23:16 +0300

    ADEN-2265 Add slot events handler

---
+++ extensions/wikia/AdEngine/js/slot/scrollHandler.js
@@ -0,0 +1,45 @@
+/*global define*/
+define('ext.wikia.adEngine.slot.scrollHandler', [
+    'wikia.log',
+    'wikia.document',
+    'wikia.window'
+], function (log, doc, win) {
+
+    var logGroup = 'ext.wikia.adEngine.slot.scrollHandler',
+        config = {
+            PREFOOTER_LEFT_BOXAD: false,
+            PREFOOTER_RIGHT_BOXAD: false
+        };
+
+    function init() {
+        win.addEventListener('scroll', function () {
+            for (var slotName in config) {
+                refreshSlot(slotName);
+            }
+        });
+    }
+
+    function refreshSlot(slotName) {
+        var status = isReached(doc.getElementById(slotName));
+        if (!config[slotName] && status) {
+            win.adslots2.push(slotName);
+            log(['refreshSlot', slotName + ' has been refreshed'], 'debug', logGroup);
+            config[slotName] = true;
+        } else if (!status) {
+            config[slotName] = false;
+        }
+    }
+
+    function isReached(el) {
+        return win.innerHeight + win.scrollY >= getTopPos(el);
+    }
+
+    function getTopPos(el) {
+        for (var topPos = 0; el != null; topPos += el.offsetTop, el = el.offsetParent);
+        return topPos;
+    }
+
+    return {
+        init: init
+    };
+});
