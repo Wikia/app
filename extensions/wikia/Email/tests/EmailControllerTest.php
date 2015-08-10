221b916cf29ea022a702170d3f1da42235109e61
Damon Snyder <damon@wikia-inc.com>
2015-08-10 20:44:18 +0000

    Add token tests for assertCanAccessController

--- extensions/wikia/Email/tests/EmailControllerTest.php
+++ extensions/wikia/Email/tests/EmailControllerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Wikia\Util\GlobalStateWrapper;
+
 class EmailController extends \Email\EmailController {
 	public function assertValidFromAddress( $email ) {
 		return parent::assertValidFromAddress( $email );
@@ -25,4 +27,44 @@ class EmailControllerTest extends WikiaBaseTest {
 		$obj = new EmailController;
 		$this->assertTrue( $obj->assertValidFromAddress( 'valid@email.com' ) );
 	}
+
+	function testAssertCanAccessControllerSuccess() {
+		$token = 'a-token';
+		$request = new \WikiaRequest(array('token' => $token));
+		$controller = new \Email\Controller\ForgotPasswordController();
+		$controller->setRequest($request);
+
+		$wrapper = new GlobalStateWrapper( [
+			'wgTheSchwartzSecretToken' => $token
+			] );
+
+		$result = $wrapper->wrap( function() use ( $controller ) {
+			return $controller->assertCanAccessController();
+		});
+
+		$this->assertTrue(!isset($result));
+	}
+
+
+	/**
+	 *
+	 * @@expectedException \Email\Fatal
+	 */
+	function testAssertCanAccessControllerFail() {
+		$token = 'a-token';
+		$request = new \WikiaRequest(array('token' => $token));
+		$controller = new \Email\Controller\ForgotPasswordController();
+		$controller->setRequest($request);
+
+		$wrapper = new GlobalStateWrapper( [
+			'wgTheSchwartzSecretToken' => 'something-different'
+			] );
+
+		$result = $wrapper->wrap( function() use ( $controller ) {
+			return $controller->assertCanAccessController();
+		});
+
+		$this->assertTrue(!isset($result));
+	}
+
 }
