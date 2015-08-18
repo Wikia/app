308e43d6214888f77a900291d105fce1bf42c954
lukaszkonieczny <lukaszk@wikia-inc.com>
2015-08-18 17:18:01 +0200

    Use whitespace instead of tab

--- extensions/wikia/ContentReview/controllers/ContentReviewApiController.class.php
+++ extensions/wikia/ContentReview/controllers/ContentReviewApiController.class.php
@@ -33,7 +33,7 @@ class ContentReviewApiController extends WikiaApiController {
 		}
 
 		$submitUserId = $this->wg->User->getId();
-		if ( !$submitUserId > 0 || !$this->canUserSubmit( $pageId )	) {
+		if ( !$submitUserId > 0 || !$this->canUserSubmit( $pageId ) ) {
 			throw new PermissionsException( 'edit' );
 		}
 
