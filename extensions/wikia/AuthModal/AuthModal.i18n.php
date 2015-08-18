cefe11ccb23077a74d571302b0038383871896c8
Damian Jozwiak <damian@wikia-inc.com>
2015-08-18 14:31:38 +0200

    SOC-601 | Messaging documentation for AuthModal

--- extensions/wikia/AuthModal/AuthModal.i18n.php
+++ extensions/wikia/AuthModal/AuthModal.i18n.php
@@ -10,3 +10,7 @@ $messages = [];
 $messages['en'] = [
 	'authmodal-registration-success' => 'Welcome, $1! Your Wikia account has been successfully created.'
 ];
+
+$messages['qqq'] = [
+	'authmodal-registration-success' => 'Welcome message displayed on Banner Notification. Informs user that account has been successfully created, $1 - username.'
+];
