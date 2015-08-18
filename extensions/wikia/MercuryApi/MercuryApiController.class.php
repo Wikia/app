b9663918cf62a2ec32faae73724ba1aaea03350f
Matt Klucsarits <mattk@wikia-inc.com>
2015-08-18 21:31:52 +0000

    SOC-959 Simplify variable check

--- extensions/wikia/MercuryApi/MercuryApiController.class.php
+++ extensions/wikia/MercuryApi/MercuryApiController.class.php
@@ -309,7 +309,7 @@ class MercuryApiController extends WikiaController {
 		}
 
 		$wikiImages = ( new WikiService() )->getWikiImages( [ $this->wg->CityId ], 500 );
-		if ( is_array( $wikiImages ) && !empty( $wikiImages[$this->wg->CityId] ) ) {
+		if ( !empty( $wikiImages[$this->wg->CityId] ) ) {
 			$wikiVariables['image'] = $wikiImages[$this->wg->CityId];
 		}
 
