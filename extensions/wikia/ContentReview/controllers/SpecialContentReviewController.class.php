079625119819a5a3859cb23b0b0a193e3e9d241e
Mariusz Czeszejko <mariusz@wikia-inc.com>
2015-08-18 13:06:41 +0000

    CE-2473 add prepareReviewData function to controller

--- extensions/wikia/ContentReview/controllers/SpecialContentReviewController.class.php
+++ extensions/wikia/ContentReview/controllers/SpecialContentReviewController.class.php
@@ -3,22 +3,32 @@ use  Wikia\ContentReview\Models\ReviewModel;
 
 class SpecialContentReviewController extends WikiaSpecialPageController {
 
+	public static $status = [
+		ReviewModel::CONTENT_REVIEW_STATUS_UNREVIEWED => 'content-review-status-unreviewed',
+		ReviewModel::CONTENT_REVIEW_STATUS_IN_REVIEW => 'content-review-status-in-review',
+		ReviewModel::CONTENT_REVIEW_STATUS_APPROVED => 'content-review-status-approved',
+		ReviewModel::CONTENT_REVIEW_STATUS_REJECTED => 'content-review-status-rejected'
+	];
+
 	function __construct() {
 		parent::__construct( 'ContentReview', 'ContentReview', true );
 	}
 
 	public function index() {
 		$this->wg->Out->setPageTitle( wfMessage( 'content-review-special-title' )->escaped() );
-
-		$this->reviewStatus = [];
-		$this->status = [];
 		$model = new ReviewModel();
-		$this->reviewStatus = $model->getContentToReviewFromDatabase();
-
-		$this->status[1] = wfMessage( 'content-review-status-unreviewed' );
-		$this->status[2] = wfMessage( 'content-review-status-in-review' );
-		$this->status[3] = wfMessage( 'content-review-status-approved' );
-		$this->status[4] = wfMessage( 'content-review-status-rejected' );
+		$reviews = $model->getContentToReviewFromDatabase();
+		$reviewData = $this->prepareReviewData( $reviews );
+		$this->reviews = $reviews;
+		$this->reviewData = $reviewData;
 	}
 
+	public function prepareReviewData( $reviews ) {
+		$reviewData = [];
+		foreach ( $reviews as $contentReviewId => $content ) {
+			$reviewData['title'][$contentReviewId] = GlobalTitle::newFromID( $content['page_id'], $content['wiki_id'] );
+			$reviewData['user'][$contentReviewId] = User::newFromId( $content['submit_user_id'] );
+		}
+	return $reviewData;
+	}
 }
--- extensions/wikia/ContentReview/controllers/templates/SpecialContentReviewController_index.php
+++ extensions/wikia/ContentReview/controllers/templates/SpecialContentReviewController_index.php
@@ -8,45 +8,44 @@
 <table class="article-table sortable content-review-special-list">
 	<thead>
 	<tr class="content-review-special-list-headers">
-		<th class="content-review-special-list-header-wiki-name"><?= wfMessage( 'content-review-special-list-header-wiki-name' )->escaped() ?></th>
-		<th class="content-review-special-list-header-page-name"><?= wfMessage( 'content-review-special-list-header-page-name' )->escaped() ?></th>
-		<th class="content-review-special-list-header-revison-id"><?= wfMessage( 'content-review-special-list-header-revision-id' )->escaped() ?></th>
-		<th class="content-review-special-list-header-status"><?= wfMessage( 'content-review-special-list-header-status' )->escaped() ?></th>
-		<th class="content-review-special-list-header-submit-user"><?= wfMessage( 'content-review-special-list-header-submit-user' )->escaped() ?></th>
-		<th class="content-review-special-list-header-status"><?= wfMessage( 'content-review-special-list-header-submit-time' )->escaped() ?></th>
-		<th class="content-review-special-list-header-status"><?= wfMessage( 'content-review-special-list-header-reviewer' )->escaped() ?></th>
-		<th class="content-review-special-list-header-status"><?= wfMessage( 'content-review-special-list-header-review-start' )->escaped() ?></th>
-		<th class="content-review-special-list-header-actions"><?= wfMessage( 'content-review-special-list-header-actions' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-wiki-name' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-page-name' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-revision-id' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-status' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-submit-user' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-submit-time' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-reviewer' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-review-start' )->escaped() ?></th>
+		<th><?= wfMessage( 'content-review-special-list-header-actions' )->escaped() ?></th>
 	</tr>
 	</thead>
 	<tbody>
-	<?php if ( !empty( $reviewStatus ) ): ?>
-	<?php foreach ( $reviewStatus as $contentReviewId => $content ): ?>
-
-	<tr class="content-review-special-list-item" id="content-review-special-list-item-<?= $contentReviewId ?>">
-		<?php $title = GlobalTitle::newFromID( $content['page_id'], $content['wiki_id'] );
-		$user = User::newFromId( $content['submit_user_id'] );?>
-		<td class="content-review-special-list-item-wiki-name data-wiki-name="<?= Sanitizer::encodeAttribute( $title ) ?>">
-		<?= Linker::link( $title, htmlspecialchars( $title->getDatabaseName() ), [
-			'class' => 'content-review-special-list-item-mediawiki-link',
-			'target' => '_blank',
-		] ); ?>
-		</td>
-		<td class="content-review-special-list-item-page-name data-page-name="<?= Sanitizer::encodeAttribute( $title->getBaseText() ) ?>"><?= htmlspecialchars( $title->getBaseText() ) ?></td>
-		<td class="content-review-special-list-item-revision-id data-revision-id="<?= Sanitizer::encodeAttribute( $content['revision_id'] ) ?>"><?= htmlspecialchars( $content['revision_id'] ) ?></td>
-		<td class="content-review-special-list-item-status data-review-status="<?= Sanitizer::encodeAttribute( $status[$content['status']] ) ?>"><?= htmlspecialchars( $status[$content['status']] ) ?></td>
-		<td class="content-review-special-list-item-submit-user data-user-name="<?= Sanitizer::encodeAttribute( $user ) ?>"><?= htmlspecialchars( $user ) ?></td>
-		<td class="content-review-special-list-item-submit-time data-review-submit-time="<?= Sanitizer::encodeAttribute( $content['submit_time'] ) ?>"><?= htmlspecialchars( $content['submit_time'] ) ?></td>
-		<td class="content-review-special-list-item-reviewer data-review-reviewer="<?= Sanitizer::encodeAttribute( $content['review_user_id'] ) ?>"><?= htmlspecialchars( $content['review_user_id'] ) ?></td>
-		<td class="content-review-special-list-item-review-start data-review-start="<?= Sanitizer::encodeAttribute( $content['review_start'] ) ?>"><?= htmlspecialchars( $content['review_start'] ) ?></td>
-		<td class="content-review-special-list-item-actions clearfix">
-		<a class="content-review-special-list-item-actions-diff  wikia-button primary" href="#" title="<?= wfMessage( 'content-review-icons-actions-diff' )->escaped() ?>" data-content-review-id="<?= Sanitizer::encodeAttribute( $contentReviewId ) ?>">
+	<?php if ( !empty( $reviews ) ): ?>
+		<?php foreach ( $reviews as $contentReviewId => $content ): ?>
+			<tr class="content-review-special-list-item" id="content-review-special-list-item-<?= $contentReviewId ?>">
+				<td>
+					<?= Linker::link( $reviewData['title'][$contentReviewId], htmlspecialchars( $reviewData['title'][$contentReviewId]->getDatabaseName() ), [
+						'class' => 'content-review-special-list-item-mediawiki-link',
+						'target' => '_blank',
+					] ); ?>
+				</td>
+				<td><?= htmlspecialchars( $reviewData['title'][$contentReviewId]->getBaseText() ) ?></td>
+				<td><?= htmlspecialchars( $content['revision_id'] ) ?></td>
+				<td><?= htmlspecialchars( wfMessage( SpecialContentReviewController::$status[$content['status']] ) ) ?></td>
+				<td><?= htmlspecialchars( $reviewData['user'][$contentReviewId] ) ?></td>
+				<td><?= htmlspecialchars( $content['submit_time'] ) ?></td>
+				<td><?= htmlspecialchars( $content['review_user_id'] ) ?></td>
+				<td><?= htmlspecialchars( $content['review_start'] ) ?></td>
+				<td class="content-review-special-list-item-actions clearfix">
+					<a class="content-review-special-list-item-actions-diff  wikia-button primary" href="#"
+					   title="<?= wfMessage( 'content-review-icons-actions-diff' )->escaped() ?>"
+					   data-content-review-id="<?= Sanitizer::encodeAttribute( $contentReviewId ) ?>">
 				<span class="content-review-icons-special content-review-icons-diff">
 						<?= wfMessage( 'content-review-icons-actions-diff' )->escaped() ?>
 				</span>
-		</a>
-	</tr>
-<?php endforeach; ?>
-<?php endif; ?>
+					</a>
+			</tr>
+		<?php endforeach; ?>
+	<?php endif; ?>
 	</tbody>
 </table>
