ff5202c475a4566a95dadd8fb03389ba1810ba8e
Igor Rogatty <igor@wikia-inc.com>
2015-08-19 17:50:07 +0200

    CONCF-989 | use proper class for getUrl static method

--- extensions/wikia/CuratedContent/CuratedContentHooks.class.php
+++ extensions/wikia/CuratedContent/CuratedContentHooks.class.php
@@ -16,17 +16,17 @@ class CuratedContentHooks {
 					// Purge section URLs using urlencode() (standard for MediaWiki), which uses implements RFC 1738
 					// https://tools.ietf.org/html/rfc1738#section-2.2 - spaces encoded as `+`.
 					// iOS apps use this variant.
-					$urls[] = self::getUrl( 'getList' ) . '&section=' . urlencode( $item['title'] );
+					$urls[] = CuratedContentController::getUrl( 'getList' ) . '&section=' . urlencode( $item['title'] );
 					// Purge section URLs using rawurlencode(), which uses implements RFC 3986
 					// https://tools.ietf.org/html/rfc3986#section-2.1 - spaces encoded as `%20`.
 					// Android apps use this variant.
-					$urls[] = self::getUrl( 'getList' ) . '&section=' . rawurlencode( $item['title'] );
+					$urls[] = CuratedContentController::getUrl( 'getList' ) . '&section=' . rawurlencode( $item['title'] );
 				}
 
 				return $urls;
 			} ,
 			// Purge all sections list getter URL - no additional params
-			[ self::getUrl( 'getList' ), self::getUrl( 'getData' ) ]
+			[ CuratedContentController::getUrl( 'getList' ), CuratedContentController::getUrl( 'getData' ) ]
 		) ) ) )->doUpdate();
 
 		// Purge cache for obsolete (not updated) apps.
