eee600806c222e51bc3c290c3f3362643a5df76e
bkoval <brt.kowal@gmail.com>
2015-08-19 13:38:43 +0200

    Simplifying template

--- extensions/wikia/GlobalNavigation/templates/GlobalNavigationAccountNavigation_index.php
+++ extensions/wikia/GlobalNavigation/templates/GlobalNavigationAccountNavigation_index.php
@@ -1,5 +1,4 @@
 <ul id="AccountNavigation" class="AccountNavigation table-cell <?= $enableNewAuth ? 'newAuth' : '' ?> ">
-	<?php if ($enableNewAuth): ?>
 		<li class="account-navigation-item">
 			<div class="links-container<?php if ($isAnon): ?> anon <?php endif; ?>">
 				<?= $navItemLinkOpeningTag ?>
@@ -7,34 +6,17 @@
 					<?= $profileAvatar ?>
 				</div>
 				</a>
-				<?php if ($isAnon) : ?>
-					<span class="auth-label register"><?= wfMessage( 'global-navigation-register' )->escaped(); ?></span>
-					<span class="auth-label"><?= wfMessage( 'global-navigation-or' )->escaped(); ?></span>
-					<span class="auth-label sign-in"><?= wfMessage( 'global-navigation-sign-in' )->escaped(); ?></span>
-				<?php else: ?>
+				<?php if ($isAnon):
+					if($enableNewAuth): ?>
+						<a class="auth-label register" href="#"><?= wfMessage( 'global-navigation-register' )->escaped(); ?></a>
+						<span class="auth-label"><?= wfMessage( 'global-navigation-or' )->escaped(); ?></span>
+						<a class="auth-label sign-in" href="#"><?= wfMessage( 'global-navigation-sign-in' )->escaped(); ?></a>
+					<?php else: ?>
+						<span class="auth-label sign-in"><?= wfMessage( 'global-navigation-sign-in' )->escaped(); ?></span>
+					<?php endif;
+				else : ?>
 					<img class="chevron" src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D"/>
 				<? endif; ?>
-			</div>
-			<?php if ( !$isAnon ): ?>
-				<ul class="user-menu subnav global-nav-dropdown">
-					<?php foreach( $userDropdown as $link ): ?>
-						<li><?= $link ?></li>
-					<?php endforeach; ?>
-				</ul>
-			<?php endif; ?>
-		</li>
-	<?php else: ?>
-		<li class="account-navigation-item">
-			<div class="links-container<?php if ($isAnon): ?> anon <?php endif; ?>">
-				<?= $navItemLinkOpeningTag ?>
-				<div class="avatar-container<?= $avatarContainerAdditionalClass ?>">
-					<?= $profileAvatar ?>
-				</div>
-				</a>
-				<?php if ($isAnon) : ?>
-					<span class="auth-label sign-in"><?= wfMessage( 'global-navigation-sign-in' )->escaped(); ?></span>
-				<? endif; ?>
-				<img class="chevron" src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D"/>
 			</div>
 			<?php if ( !$isAnon ): ?>
 				<ul class="user-menu subnav global-nav-dropdown">
@@ -46,5 +28,4 @@
 				echo $loginDropdown;
 			endif; ?>
 		</li>
-	<?php endif; ?>
 </ul>
---
+++ extensions/wikia/GlobalNavigation/templates/GlobalNavigation_authLink.php
@@ -0,0 +1,1 @@
+<a class="auth-label register" href="#"><?= wfMessage( 'global-navigation-register' )->escaped(); ?></a>
