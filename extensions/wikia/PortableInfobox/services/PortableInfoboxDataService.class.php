4a754ce0884e19bfb3498ff910960f53850aa921
Diana <diana.falkowska@gmail.com>
2015-08-20 10:30:22 +0200

    fix check

--- extensions/wikia/PortableInfobox/services/PortableInfoboxDataService.class.php
+++ extensions/wikia/PortableInfobox/services/PortableInfoboxDataService.class.php
@@ -74,7 +74,7 @@ class PortableInfoboxDataService {
 			$parserOutput->getProperty( self::INFOBOXES_PROPERTY_NAME )
 			: false;
 
-		if ( !$parsedInfoboxes && $title->getNamespace()) {
+		if ( !$parsedInfoboxes && $title->getNamespace() === 10 ) {
 			$parser = new Parser();
 			$parserOptions = new ParserOptions();
 			$frame = $parser->getPreprocessor()->newFrame();
