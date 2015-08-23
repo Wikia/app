f52c7ce427807996916edafc3c1bf18d2d775d47
Ty <tyler@faceyspacies.com>
2015-08-23 11:53:19 -0500

    Doc functions, cqm cr changes

--- extensions/wikia/TwitterTag/TwitterTag.i18n.php
+++ extensions/wikia/TwitterTag/TwitterTag.i18n.php
@@ -8,3 +8,10 @@ $messages['en'] = [
 	'twittertag-noid' => 'Error: No Twitter Widget ID provided. Please see [[Help:Social media integration]]',
 	'twittertag-alt-text' => 'Tweets by @$1'
 ];
+
+$messages['qqq'] = [
+	'twittertag-desc' => 'The description of the app found on [[Special:Version]]',
+	'twittertag-nouser' => 'The error text displayed when there is no user attribute. In English one, it points to the help page',
+	'twittertag-noid' => 'The error text displayed when there is no id attribute. In English one, it points to the help page',
+	'twittertag-alt-text' => 'The text displayed when the user has no JavaScript or it fails to load the Twitter feed for some other reason.'
+];
--- extensions/wikia/TwitterTag/TwitterTagHooks.class.php
+++ extensions/wikia/TwitterTag/TwitterTagHooks.class.php
@@ -2,37 +2,52 @@
 
 class TwitterTagHooks {
 	
+	/*
+	 * onParserFirstCallInit
+	 *
+	 * Registers the <twitter> tag with the parser and sets its callback
+	 *
+	 * @param $parser - The parser
+	 * @return true
+	 */
 	static public function onParserFirstCallInit( Parser &$parser ) {
 		$parser->setHook( 'twitter', [ __CLASS__, 'parseTwitterTag' ] );
 		return true;
 	}
 	
+	/*
+	 * parseTwitterTag
+	 *
+	 * Parses the twitter tag. Checks to ensure the required attributes are there. 
+	 *   Then constructs the HTML after seeing which attributes are in use. 
+	 *
+	 * @param $input - not used
+	 * @param $args - The attributes to the tag in an assoc array
+	 * @param $parser - not used
+	 * @param $frame - not used
+	 * @return $html - The HTML for the twitter tag
+	 */
 	static public function parseTwitterTag( $input, array $args, Parser $parser, PPFrame $frame ) {
 		if( empty( $args['user'] ) ) {
-			$html = wfMessage("twittertag-nouser")->parse();
-			return "<p class='error'>" . $html . "</p>";
+			$html = wfMessage( "twittertag-nouser" )->parse();
+			return "<strong class='error'>" . $html . "</strong>";
 			
 		} else if ( empty( $args['id'] ) ) {
 			$html = wfMessage( 'twittertag-noid' )->parse();
-			return "<p class='error'>" . $html . "</p>";
+			return "<strong class='error'>" . $html . "</strong>";
 		}
 		
-		$attributes = [];
+		$quickChecks = [
+			'height' => '',
+			'width' => '',
+			'data-chrome' => '',
+		];
+		
+		$attributes = array_intersect_key( $args, $quickChecks );
+		
 		$attributes['class'] = "twitter-timeline";
 		$attributes['href'] = "https://twitter.come/" . $args['user'];
 		$attributes['data-widget-id'] = $args['id'];
-		
-		if( !empty( $args['height'] ) ) {
-			$attributes['height'] = $args['height'];
-		}
-		
-		if( !empty( $args['width'] ) ) {
-			$attributes['width'] = $args['width'];
-		}
-		
-		if( !empty( $args['data-chrome'])) {
-			$attributes['data-chrome'] = $args['data-chrome'];
-		}
 		
 		if( !empty ( $args['limit'] ) )  {
 			$attributes['data-tweet-limit'] = $args['limit'];
@@ -53,7 +68,7 @@ class TwitterTagHooks {
 		}
 
 		$parser->getOutput()->addModules( 'ext.TwitterTag' );
-		
+
 		return $html;
 	}
 }
