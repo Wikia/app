669b95b65ce88b5dfa71f6df6a07a526ea2a1ec0
nAndy <nandy@wikia-inc.com>
2015-08-26 17:13:58 +0000

    ADEN-2321 added integration test to check noindex,nofollow on regular page and showcase.* page

---
+++ extensions/wikia/AdEngine/tests/AdEngine2HooksIntegrationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class AdEngine2HooksIntegrationTest
+ *
+ * @group Integration
+ */
+class AdEngine2HooksIntegrationTest extends WikiaBaseTest {
+	const SHOWCASE_ADTEST_PAGE_LINK = 'http://showcase.adtest.wikia.com/wiki/Wikia_Ad_Testing';
+	const ADTEST_PAGE_LINK = 'http://adtest.wikia.com/wiki/Wikia_Ad_Testing';
+	const NO_INDEX_NO_FOLLOW = '<meta name="robots" content="noindex,nofollow" />';
+	const HTTP_OK = 200;
+
+	public function testRobotsAllowedOnRegularPage() {
+		$response = \Http::get( self::ADTEST_PAGE_LINK, 'default', [ 'noProxy' => true, 'returnInstance' => true ] );
+		$this->assertEquals( self::HTTP_OK, $response->getStatus() );
+		$this->assertNotContains( self::NO_INDEX_NO_FOLLOW, $response->getContent() );
+	}
+
+	public function testRobotsAllowedOnShowcasePage() {
+		$response = \Http::get( self::SHOWCASE_ADTEST_PAGE_LINK, 'default', [ 'noProxy' => true, 'returnInstance' => true ] );
+		$this->assertEquals( self::HTTP_OK, $response->getStatus() );
+		$this->assertContains( self::NO_INDEX_NO_FOLLOW, $response->getContent() );
+	}
+}
