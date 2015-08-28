33b4be0dc42af2e43128b44115461b6780ce739a
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-08-28 10:55:28 +0000

    CE-2525: Remove unnecessary hook

--- extensions/wikia/ContentReview/ContentReview.setup.php
+++ extensions/wikia/ContentReview/ContentReview.setup.php
@@ -54,7 +54,6 @@ $wgHooks['MakeGlobalVariablesScript'][] = 'Wikia\ContentReview\Hooks::onMakeGlob
 $wgHooks['BeforePageDisplay'][] = 'Wikia\ContentReview\Hooks::onBeforePageDisplay';
 $wgHooks['ArticleContentOnDiff'][] = 'Wikia\ContentReview\Hooks::onArticleContentOnDiff';
 $wgHooks['RawPageViewBeforeOutput'][] = 'Wikia\ContentReview\Hooks::onRawPageViewBeforeOutput';
-$wgHooks['UserLogoutComplete'][] = 'Wikia\ContentReview\Hooks::onUserLogoutComplete';
 
 /**
  * Right rail module
