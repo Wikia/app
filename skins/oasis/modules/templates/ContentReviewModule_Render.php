ce52ae81783e44e0fc347db490eef0f677d26b02
adamkarminski <adamkarminski@gmail.com>
2015-08-28 10:00:19 +0000

    Bring back a class attribute

--- skins/oasis/modules/templates/ContentReviewModule_Render.php
+++ skins/oasis/modules/templates/ContentReviewModule_Render.php
@@ -20,11 +20,11 @@
 
 	<div class="content-review-module-test-mode">
 		<? if ( $isTestModeEnabled ) : ?>
-			<button id="content-review-module-disable-test-mode" class="secondary">
+			<button class="content-review-module-disable-test-mode secondary">
 				<?= wfMessage('content-review-module-disable-test-mode')->escaped() ?>
 			</button>
 		<? else: ?>
-			<button id="content-review-module-enable-test-mode" class="secondary">
+			<button class="content-review-module-enable-test-mode secondary">
 				<?= wfMessage('content-review-module-enable-test-mode')->escaped() ?>
 			</button>
 		<? endif ?>
