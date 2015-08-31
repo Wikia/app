db3d07c8a8cca3cab6d6dae7afc903639ff344fc
Garth Webb <garth@wikia-inc.com>
2015-08-31 14:11:51 -0700

    SOC-1156 Response to code review

--- includes/wikia/WikiaMailer.php
+++ includes/wikia/WikiaMailer.php
@@ -181,7 +181,7 @@ class WikiaSendgridMailer {
 		return false;
 	}
 
-	static public function backtrace( $num = 8 ) {
+	static public function backtrace( $depth = 8 ) {
 		$trace = debug_backtrace();
 
 		// Get rid the same 6 calls that always precede this
@@ -191,21 +191,21 @@ class WikiaSendgridMailer {
 		// to this function and increase the amount of context we show
 		if ( !empty( $trace[4]['function'] ) && $trace[5]['function'] != 'wfRunHooks' ) {
 			$removeCallsUpTo = 1;
-			$num += 10;
+			$depth += 10;
 		}
 		$trace = array_slice( $trace, $removeCallsUpTo );
 
 		$formattedTrace = [];
-		$count = $num;
-		foreach ($trace as $frame) {
+		$count = $depth;
+		foreach ( $trace as $frame ) {
 			if ( $count == 0 ) {
 				break;
 			}
 			$count--;
 
-			$file = empty($frame['file']) ? '(no file)' : $frame['file'];
-			$line = empty($frame['line']) ? '(no line)' : $frame['line'];
-			$func = empty($frame['function']) ? '(anonymous)' : $frame['function'];
+			$file = empty( $frame['file'] ) ? '(no file)' : $frame['file'];
+			$line = empty( $frame['line'] ) ? '(no line)' : $frame['line'];
+			$func = empty( $frame['function'] ) ? '(anonymous)' : $frame['function'];
 			$formattedTrace[] = "$file @ $line : $func";
 		}
 
