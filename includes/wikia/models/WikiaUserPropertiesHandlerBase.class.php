03d4ae5ed60bb96e97abae1f2260193c94ed5621
James Sutterfield <james@wikia-inc.com>
2015-08-30 19:12:35 -0700

    SOC-1203 only update the User::(set|get)Attribute calls

--- includes/wikia/models/WikiaUserPropertiesHandlerBase.class.php
+++ includes/wikia/models/WikiaUserPropertiesHandlerBase.class.php
@@ -4,14 +4,14 @@ class WikiaUserPropertiesHandlerBase extends WikiaModel {
 	const EXCEPTION_USER_NOT_LOGGED_IN_CODE = 1;
 	const EXCEPTION_INVALID_PROPERTY_NAME = 2;
 
-	protected function savePreferenceValue($property, $value) {
+	protected function savePropertyValue($property, $value) {
 		$this->throwExceptionForAnons();
 
 		$this->wg->User->setGlobalPreference($property, $value);
 		$this->wg->User->saveSettings();
 	}
 
-	protected function getPreferenceValue($propertyName, $defaultOption = null) {
+	protected function getPropertyValue($propertyName, $defaultOption = null) {
 		$value = $this->wg->User->getGlobalPreference($propertyName);
 		if (!$value) {
 			$value = $defaultOption;
