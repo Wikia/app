ecfe848206f9ebdb123c70c1de34eaac4165d47d
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-09-01 08:18:59 +0000

    CE-2598: Fix typo

--- skins/oasis/modules/ContentReviewModuleController.class.php
+++ skins/oasis/modules/ContentReviewModuleController.class.php
@@ -23,7 +23,7 @@ class ContentReviewModuleController extends WikiaController {
 		Wikia::addAssetsToOutput( 'content_review_module_scss' );
 		JSMessages::enqueuePackage( 'ContentReviewModule', JSMessages::EXTERNAL );
 
-		$this->setVal( 'isTestModeEnabled', (new Helper)->isContentReviewTestModeEnabled() );
+		$this->setVal( 'isTestModeEnabled', ( new Helper() )->isContentReviewTestModeEnabled() );
 
 		/**
 		 * Latest revision status
