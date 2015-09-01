19073919c1e2072b73433dc63bd961d0e83c9c33
Rafal Leszczynski <rafal@wikia-inc.com>
2015-09-01 14:59:46 +0200

    set ponto target for infobox builder UI

--- extensions/wikia/PortableInfobox/js/PortableInfoboxBuilder.js
+++ extensions/wikia/PortableInfobox/js/PortableInfoboxBuilder.js
@@ -1,56 +1,13 @@
 'use strict';
 
-require(['wikia.querystring', 'jquery', 'wikia.ui.factory'], function (queryString, $, uiFactory) {
-	var initBuilderUIqueryStringParam = 'portableInfoboxBuilder',
-		initBuilderUIEventName = 'initPortableInfoboxBuilder',
-		builderUIModalEntryPointId = 'portableInfoboxBuilderModalEntryPoint',
-		builderUIModalEntryPointParams = {
-			vars: {
-				id: builderUIModalEntryPointId,
-				size: 'small',
-				title: $.msg('portable-infobox-builder-entry-point-modal-title'),
-				content: $.msg('portable-infobox-builder-entry-point-modal-message'),
-				buttons: [
-					{
-						vars: {
-							value: $.msg('portable-infobox-builder-entry-point-modal-ok-button'),
-							classes: ['normal', 'primary'],
-							data: [
-								{
-									key: 'event',
-									value: initBuilderUIEventName
-								}
-							]
-						}
-					},
-					{
-						vars: {
-							value: $.msg('portable-infobox-builder-entry-point-modal-cancel-button'),
-							data: [
-								{
-									key: 'event',
-									value: 'close'
-								}
-							]
-						}
-					}
-				]
-			}
-		};
+require(['ponto', 'jquery'], function (ponto, $) {
+	var infoboxBuilderIframe = $('#infoboxBuilderIframe');
 
-	/**
-	 * adds query string param to URL and reloads the page to init Portable Infobox Builder UI
-	 */
-	function initPortableInfoboxBuilder() {
-		queryString()
-			.setVal(initBuilderUIqueryStringParam, true)
-			.goTo();
+	if (infoboxBuilderIframe.length) {
+		ponto.setTarget(
+			Ponto.TARGET_IFRAME,
+			window.location.origin,
+			infoboxBuilderIframe[0].contentWindow
+		);
 	}
-
-	uiFactory.init(['modal']).then(function (modalComponent) {
-		modalComponent.createComponent(builderUIModalEntryPointParams, function (modal) {
-			modal.bind(initBuilderUIEventName, initPortableInfoboxBuilder);
-			modal.show();
-		});
-	});
 });
