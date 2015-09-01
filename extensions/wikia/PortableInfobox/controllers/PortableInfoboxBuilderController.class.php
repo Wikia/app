4ac163a81d31690d0feb7079b1e87b00c057fb39
Rafal Leszczynski <rafal@wikia-inc.com>
2015-09-01 13:32:15 +0200

    remove templates from API response to infobox builder in mercury

--- extensions/wikia/PortableInfobox/controllers/PortableInfoboxBuilderController.class.php
+++ extensions/wikia/PortableInfobox/controllers/PortableInfoboxBuilderController.class.php
@@ -5,15 +5,9 @@ class PortableInfoboxBuilderController extends WikiaController {
 	const INFOBOX_BUILDER_PARAM = 'portableInfoboxBuilder';
 
 	public function getAssets() {
-		$dir = PortableInfoboxRenderService::getTemplatesDir();
-		$result = array_map( function ( $template ) use ( $dir ) {
-			return file_get_contents( "{$dir}/{$template}" );
-		}, PortableInfoboxRenderService::getTemplates() );
-
 		$response = $this->getResponse();
 		$response->setFormat( WikiaResponse::FORMAT_JSON );
 		$response->setVal( "css", AssetsManager::getInstance()->getURL( "portable_infobox_scss" ) );
-		$response->setVal( "templates", $result );
 	}
 
 	public function publish() {
