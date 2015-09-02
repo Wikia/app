0b23197aed4f2827b583f0ed8f8a2f8de19e85ae
Rafal Leszczynski <rafal@wikia-inc.com>
2015-09-02 17:58:55 +0200

    fix spacing

--- extensions/wikia/PortableInfobox/PortableInfobox.setup.php
+++ extensions/wikia/PortableInfobox/PortableInfobox.setup.php
@@ -73,7 +73,7 @@ $wgHooks[ 'BeforePageDisplay' ][] = 'PortableInfoboxHooks::onBeforePageDisplay';
 $wgHooks[ 'ParserAfterTidy' ][] = 'PortableInfoboxParserTagController::replaceInfoboxMarkers';
 $wgHooks[ 'ImageServing::buildAndGetIndex' ][] = 'PortableInfoboxHooks::onImageServingCollectImages';
 $wgHooks[ 'wgQueryPages' ][] = 'PortableInfoboxHooks::onWgQueryPages';
-$wgHooks['AddPortableInfoboxBuilderText'][] = 'PortableInfoboxHooks::onAddPortableInfoboxBuilderText';
+$wgHooks[ 'AddPortableInfoboxBuilderText' ][] = 'PortableInfoboxHooks::onAddPortableInfoboxBuilderText';
 $wgHooks[ 'SkinAfterBottomScripts' ][] = 'PortableInfoboxHooks::onSkinAfterBottomScripts';
 
 // special pages
