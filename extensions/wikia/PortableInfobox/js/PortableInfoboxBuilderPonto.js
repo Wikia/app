692bcbad25065d584fb7f521e9585c1fab02a7da
Rafal Leszczynski <rafal@wikia-inc.com>
2015-09-02 17:09:01 +0200

    add redirect to template method to infobox builder ponto module

--- extensions/wikia/PortableInfobox/js/PortableInfoboxBuilderPonto.js
+++ extensions/wikia/PortableInfobox/js/PortableInfoboxBuilderPonto.js
@@ -2,11 +2,25 @@
 
 define('wikia.infoboxBuilder.ponto', ['wikia.window', 'ponto'], function (window, ponto) {
 	function InfoboxBuilderPonto() {
+		/**
+		 * sends wiki context to infobox builder in mercury
+		 * @returns {{isWikiaContext: boolean, isLoggedIn: boolean}}
+		 */
 		this.isWikiaContext = function () {
 			return {
 				isWikiaContext: true,
 				isLoggedIn: window.wqUserName !== null ? true : false
 			}
+		};
+
+		/**
+		 * redirects to template page
+		 * @param {String} title
+		 * @returns {Boolean}
+		 */
+		this.redirectToTemplatePage = function (title) {
+			window.location = window.location.origin + '/wiki/Template:' + title;
+			return true;
 		}
 	}
 
