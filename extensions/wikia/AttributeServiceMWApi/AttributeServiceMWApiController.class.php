46c75bf0c0013bfd6aa9ad2815147d3791c80c1d
James Sutterfield <james@wikia-inc.com>
2015-09-04 11:34:13 -0700

    SOC-1216 move setErrorResponse calls higher up

--- extensions/wikia/AttributeServiceMWApi/AttributeServiceMWApiController.class.php
+++ extensions/wikia/AttributeServiceMWApi/AttributeServiceMWApiController.class.php
@@ -2,80 +2,80 @@
 
 class AttributeServiceMWApiController extends WikiaController {
 
-    /**
-     * Helper function for the attribute service to clear user MediaWiki.
-     * This is called when a client other than MW updates an attribute
-     * using the attribute service to clear the User cache.
-     */
-    public function purgeUserCache() {
+	/**
+	 * Helper function for the attribute service to clear user MediaWiki.
+	 * This is called when a client other than MW updates an attribute
+	 * using the attribute service to clear the User cache.
+	 */
+	public function purgeUserCache() {
 
-        $userId = $this->request->getVal( 'userId' );
-        $signature = $this->request->getVal( 'signature' );
+		$userId = $this->request->getVal( 'userId' );
+		$signature = $this->request->getVal( 'signature' );
 
-       if ( !$this->assertValidParameters( $userId, $signature ) ) {
-           $this->setErrorResponse( 'invalid parameters: (userId) and (signature) must not be empty',
-               \WikiaResponse::RESPONSE_CODE_BAD_REQUEST );
-           return;
-       }
+	   if ( !$this->assertValidParameters( $userId, $signature ) ) {
+		   $this->setErrorResponse( 'invalid parameters: (userId) and (signature) must not be empty',
+			   \WikiaResponse::RESPONSE_CODE_BAD_REQUEST );
+		   return;
+	   }
 
-        if ( !$this->assertValidSignature( $userId, $signature ) ) {
-            $this->setErrorResponse( 'invalid signature', \WikiaResponse::RESPONSE_CODE_FORBIDDEN );
-            return;
-        }
+		if ( !$this->assertValidSignature( $userId, $signature ) ) {
+			$this->setErrorResponse( 'invalid signature', \WikiaResponse::RESPONSE_CODE_FORBIDDEN );
+			return;
+		}
 
-        User::clearUserCache( $userId );
-        $userIdentityBox = new UserIdentityBox( User::newFromId( $userId ) );
-        $userIdentityBox->clearCache();
-    }
+		User::clearUserCache( $userId );
+		$userIdentityBox = new UserIdentityBox( User::newFromId( $userId ) );
+		$userIdentityBox->clearCache();
+	}
 
-    /**
-     * Asserts the request is properly formed, ie, contains 'userId' and 'signature' in the
-     * post data.
-     *
-     * @param $userId
-     * @param $signature
-     * @return bool
-     */
-    private function assertValidParameters( $userId, $signature ) {
-        if ( empty( $userId )  || empty( $signature ) ) {
-            return false;
-        }
+	/**
+	 * Asserts the request is properly formed, ie, contains 'userId' and 'signature' in the
+	 * post data.
+	 *
+	 * @param $userId
+	 * @param $signature
+	 * @return bool
+	 */
+	private function assertValidParameters( $userId, $signature ) {
+		if ( empty( $userId )  || empty( $signature ) ) {
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Assert the request contains a valid signature
-     *
-     * @param $userId
-     * @param $signature
-     * @return bool
-     */
-    private function assertValidSignature( $userId, $signature ) {
+	/**
+	 * Assert the request contains a valid signature
+	 *
+	 * @param $userId
+	 * @param $signature
+	 * @return bool
+	 */
+	private function assertValidSignature( $userId, $signature ) {
 
-        if ( $signature != $this->getComputedSignature( $userId ) ) {
-            return false;
-        }
+		if ( $signature != $this->getComputedSignature( $userId ) ) {
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Computes a signature using a shared secret.
-     *
-     * @param $userId
-     * @return string
-     */
-    private function getComputedSignature( $userId ) {
-        global $wgTheSchwartzSecretToken;
+	/**
+	 * Computes a signature using a shared secret.
+	 *
+	 * @param $userId
+	 * @return string
+	 */
+	private function getComputedSignature( $userId ) {
+		global $wgTheSchwartzSecretToken;
 
-        $computedSignature = hash_hmac( 'sha256', $userId, $wgTheSchwartzSecretToken, true );
-        return base64_encode( $computedSignature );
-    }
+		$computedSignature = hash_hmac( 'sha256', $userId, $wgTheSchwartzSecretToken, true );
+		return base64_encode( $computedSignature );
+	}
 
-    private function setErrorResponse( $message, $responseCode ) {
-        $this->response->setVal( 'message', $message );
-        $this->response->setCode( $responseCode );
-    }
+	private function setErrorResponse( $message, $responseCode ) {
+		$this->response->setVal( 'message', $message );
+		$this->response->setCode( $responseCode );
+	}
 }
 
