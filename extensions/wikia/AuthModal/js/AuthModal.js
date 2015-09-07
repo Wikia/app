fa9462103d8959e70727ca925bef6254c9b9bcda
rafal.kalinski <kalina@wikia-inc.com>
2015-09-07 15:25:04 +0000

    SOC-1204 | single quotes

--- extensions/wikia/AuthModal/js/AuthModal.js
+++ extensions/wikia/AuthModal/js/AuthModal.js
@@ -26,7 +26,7 @@ define('AuthModal', ['jquery', 'wikia.window'], function ($, window) {
 		});
 
 		$(window.document).keyup(onKeyUp);
-		window.addEventListener("message", function (event) {
+		window.addEventListener('message', function (event) {
 			if (event.data.isUserAuthorized && typeof successAuthCallback === 'function') {
 				close();
 				successAuthCallback();
