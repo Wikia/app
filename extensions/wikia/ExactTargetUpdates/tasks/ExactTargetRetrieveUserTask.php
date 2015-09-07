ae5a7570042bc411ad4dd6c8c234668b20ffb003
kamilkoterba <kamil.koterba@gmail.com>
2015-09-07 15:09:48 +0200

    CE-2537 Remove unnecessary return

--- extensions/wikia/ExactTargetUpdates/tasks/ExactTargetRetrieveUserTask.php
+++ extensions/wikia/ExactTargetUpdates/tasks/ExactTargetRetrieveUserTask.php
@@ -117,7 +117,6 @@ class ExactTargetRetrieveUserTask extends ExactTargetTask {
 				}
 				$aExactTargetUserPropertiesData[$sPropertyName] = $sPropertyValue;
 			}
-			return $aExactTargetUserPropertiesData;
 		}
 
 		return $aExactTargetUserPropertiesData;
