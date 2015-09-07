04da1dfe9b7e8cbbfb7a19a49dd306eb72562d49
idradm <robakadam@gmail.com>
2015-09-07 15:00:00 +0200

    DAT-3153 use of more reliable methods for url spliting and joining

--- extensions/wikia/PortableInfobox/controllers/PortableInfoboxBuilderSpecialController.class.php
+++ extensions/wikia/PortableInfobox/controllers/PortableInfoboxBuilderSpecialController.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class WikiaMapsSpecialController
  * @desc Special:Maps controller
@@ -7,6 +8,8 @@ class PortableInfoboxBuilderSpecialController extends WikiaSpecialPageController
 	const PAGE_NAME = 'PortableInfoboxBuilder';
 	const PAGE_RESTRICTION = 'editinterface';
 	const INFOBOX_BUILDER_MERCURY_ROUTE = 'infoboxBuilder';
+	const PATH_SEPARATOR = '/';
+	const EXPLODE_LIMIT = 2;
 
 	/**
 	 * Special page constructor
@@ -29,15 +32,16 @@ class PortableInfoboxBuilderSpecialController extends WikiaSpecialPageController
 		$this->wg->SuppressPageHeader = true;
 		$this->wg->out->setHTMLTitle( wfMessage( 'portable-infobox-builder-title' )->text() );
 
-		$title = $this->getTemplateTitle( $this->getPar() );
+		// extract base title from path
+		$title = explode( self::PATH_SEPARATOR, $this->getPar(), self::EXPLODE_LIMIT )[0];
 		$noTemplateSet = empty( $title ) ? true : false;
 
 		if ( $noTemplateSet ) {
-			$this->response->setVal('noTemplateSet', true );
-			$this->response->setVal('setTemplateNameCallToAction', wfMessage(
+			$this->response->setVal( 'noTemplateSet', true );
+			$this->response->setVal( 'setTemplateNameCallToAction', wfMessage(
 				'portable-infobox-builder-no-template-title-set' )->text() );
 		} else {
-			$url = $this->wg->server . '/' . self::INFOBOX_BUILDER_MERCURY_ROUTE . '/' . $title;
+			$url = implode( self::PATH_SEPARATOR, [ $this->wg->server, self::INFOBOX_BUILDER_MERCURY_ROUTE, $title ] );
 			$this->response->setVal( 'iframeUrl', $url );
 		}
 
@@ -52,19 +56,8 @@ class PortableInfoboxBuilderSpecialController extends WikiaSpecialPageController
 			'portable-infobox-builder-create-template-entry-point-create-infobox' )->text() );
 		$this->response->setVal( 'createTemplate', wfMessage(
 			'portable-infobox-builder-create-template-entry-point-create-regular-template' )->text() );
-		$this->response->setVal( 'title', $this->request->getVal('title'), '' );
+		$this->response->setVal( 'title', $this->request->getVal( 'title' ), '' );
 
 		$this->response->setTemplateEngine( WikiaResponse::TEMPLATE_ENGINE_MUSTACHE );
-	}
-
-	/**
-	 * @desc return template title base of the URL path
-	 * @param string $par - URL path after Special:PortableInfoboxBuilder/
-	 * @return string
-	 */
-	private function getTemplateTitle( $par ) {
-		$slicePos = strpos( $par, '/' );
-
-		return urlencode( is_int( $slicePos ) ? substr( $par, 0, $slicePos ) : $par );
 	}
 }
