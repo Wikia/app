4fb5676a79116018046eaf114e2f620f21834595
nAndy <nandy@wikia-inc.com>
2015-09-07 15:22:40 +0200

    ADEN-2410 created new WikiaParserTagController to re-use validation part

---
+++ includes/wikia/nirvana/WikiaParserTagController.class.php
@@ -0,0 +1,37 @@
+<?php
+abstract class WikiaParserTagController extends WikiaController {
+
+	/**
+	 * @desc Checks if parameter from parameters array is valid
+	 *
+	 * @param String $paramName name of parameter which should get validated
+	 * @param String|Mixed $paramValue value of the parameter
+	 * @param String $errorMessage reference to a string variable which will get error message if one occurs
+	 *
+	 * @return bool
+	 */
+	protected function isTagParamValid( $paramName, $paramValue, &$errorMessage ) {
+		$isValid = false;
+
+		$validator = $this->buildParamValidator( $paramName );
+		if( !is_null($validator) ) {
+			$isValid = $validator->isValid( $paramValue );
+
+			if( !$isValid ) {
+				$errorMessage = $validator->getError()->getMsg();
+			}
+		}
+
+		return $isValid;
+	}
+
+	/**
+	 * @desc Factory method to create validators for params; returns false if validator can't be created
+	 *
+	 * @param String $paramName
+	 * @return null|WikiaValidator
+	 */
+	protected function buildParamValidator( $paramName ) {
+		return null;
+	}
+}
