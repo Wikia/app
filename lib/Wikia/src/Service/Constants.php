d10a8afb7920799198642c5fec4084aa6a2eb624
Damon Snyder <damon@wikia-inc.com>
2015-09-08 22:15:55 +0000

    Add handling for 204 No Content to helios client

--- lib/Wikia/src/Service/Constants.php
+++ lib/Wikia/src/Service/Constants.php
@@ -7,4 +7,6 @@ class Constants {
 	 * Name of the header used to authenticate user requests to the Helios service.
      */
 	const HELIOS_AUTH_HEADER = "X-Wikia-UserId";
+
+	const HTTP_STATUS_NO_CONTENT = 204;
 }
