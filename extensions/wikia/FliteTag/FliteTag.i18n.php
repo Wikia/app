56a3a17c413cb2f82f209917376daa5055491441
nAndy <nandy@wikia-inc.com>
2015-09-09 18:21:09 +0200

    ADEN-2410 used new validation and added new error messages

--- extensions/wikia/FliteTag/FliteTag.i18n.php
+++ extensions/wikia/FliteTag/FliteTag.i18n.php
@@ -6,8 +6,10 @@ $messages['en'] = [
 	// validation errors
 	'flite-tag-error-no-required-parameters' => 'No required parameters passed. Provide guid, width and height parameters.',
 	'flite-tag-error-invalid-guid' => 'Invalid guid parameter was passed. Provide valid guid or remove this tag from article\'s content.',
-	'flite-tag-error-invalid-size' => 'Invalid size of the flite unit was passed. Make sure you provide width and height parameters with numeric value.',
-	'flite-tag-error-min-size' => 'Invalid size of the flite unit was passed. Make sure width and height are bigger than 1.',
+	'flite-tag-error-invalid-size-width' => 'Invalid width of the flite unit was passed. Make sure you provide width parameter with numeric value.',
+	'flite-tag-error-invalid-size-height' => 'Invalid height of the flite unit was passed. Make sure you provide height parameter with numeric value.',
+	'flite-tag-error-min-size-width' => 'Invalid width of the flite unit was passed. Make sure width is bigger than 1.',
+	'flite-tag-error-min-size-height' => 'Invalid height of the flite unit was passed. Make sure height is bigger than 1.',
 	'flite-tag-error-unknown' => 'Unknown validation error occurred.',
 ];
 
@@ -15,7 +17,9 @@ $messages['qqq'] = [
 	'flitetag-desc' => 'Extension description',
 	'flite-tag-error-no-required-parameters' => 'validation error which appears instead of flite ad unit when none of the parameters were passed',
 	'flite-tag-error-invalid-guid' => 'validation error which appears instead of flite ad unit when invalid guid parameter was passed',
-	'flite-tag-error-invalid-size' => 'validation error which appears instead of flite ad unit when invalid width or height parameters were passed',
-	'flite-tag-error-min-size' => 'validation error which appears instead of flite ad unit when invalid width or height parameters were passed',
+	'flite-tag-error-invalid-size-width' => 'validation error which appears instead of flite ad unit when invalid width parameter was passed',
+	'flite-tag-error-invalid-size-height' => 'validation error which appears instead of flite ad unit when invalid height parameter was passed',
+	'flite-tag-error-min-size-width' => 'validation error which appears instead of flite ad unit when invalid width parameter was passed',
+	'flite-tag-error-min-size-height' => 'validation error which appears instead of flite ad unit when invalid height parameter was passed',
 	'flite-tag-error-unknown' => 'validation error which appears instead of flite ad unit when an unknown error occurred',
 ];
