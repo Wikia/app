99854bb358d3872edd33cd407bb9c98e921d1dd2
Piotr Gabryjeluk <rychu@wikia-inc.com>
2015-09-09 11:21:18 -0700

    SEO-51 Set up CreateNewWiki aliases in alias file
    This change makes this work (lists all the aliases):
    
      $wgContLang->getSpecialPageAliases()['CreateNewWiki'];

---
+++ extensions/wikia/CreateNewWiki/CreateNewWiki.alias.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Aliases for special pages of CreateNewWiki extension.
+ */
+$specialPageAliases = array();
+
+/** English
+ */
+$specialPageAliases['en'] = array(
+	'CreateNewWiki' => array( 'CreateNewWiki', 'CreateWiki' ),
+);
