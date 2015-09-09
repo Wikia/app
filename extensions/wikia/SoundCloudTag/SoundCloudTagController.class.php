5ac964399d245d64a45c932edf5d69a57224935f
V <bartosz.bentkowski@gmail.com>
2015-09-09 16:59:15 +0200

    XW-216 | wrap

--- extensions/wikia/SoundCloudTag/SoundCloudTagController.class.php
+++ extensions/wikia/SoundCloudTag/SoundCloudTagController.class.php
@@ -33,7 +33,8 @@ class SoundCloudTagController extends WikiaParserTagController {
 	 */
 	private static function buildUrl( array $args ) {
 		// basically white-list of attributes
-		$allowedParams = [ 'auto_play', 'buying', 'color', 'download', 'liking', 'sharing', 'show_artwork','show_comments', 'show_playcount', 'show_user', 'start_track', 'url' ];
+		$allowedParams = [ 'auto_play', 'buying', 'color', 'download', 'liking', 'sharing',
+			'show_artwork','show_comments', 'show_playcount', 'show_user', 'start_track', 'url' ];
 		$data = [];
 
 		foreach ( $allowedParams as $name ) {
