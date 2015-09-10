8136076ed17b66a848b2a1cda4aa620616547bcd
Paul Oslund <poslund@wikia-inc.com>
2015-09-10 20:32:02 +0000

    VE-1914: Move default VirtualRestConfig into DefaultSettings.php

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5744,6 +5744,36 @@ $wgDBtestuser = ''; //db user that has permission to create and drop the test da
 $wgDBtestpassword = '';
 
 /**
+ * Global configuration variable for Virtual REST Services.
+ * Parameters for different services are to be declared inside
+ * $wgVirtualRestConfig['modules'], which is to be treated as an associative
+ * array. Global parameters will be merged with service-specific ones. The
+ * result will then be passed to VirtualRESTService::__construct() in the
+ * module.
+ *
+ * Example config for Parsoid:
+ *
+ *   $wgVirtualRestConfig['modules']['parsoid'] = array(
+ *     'url' => 'http://localhost:8000',
+ *     'prefix' => 'enwiki',
+ *     'domain' => 'en.wikipedia.org',
+ *   );
+ *
+ * @var array
+ * @since 1.25
+ */
+$wgVirtualRestConfig = array(
+	'modules' => array(),
+	'global' => array(
+		# Timeout in seconds
+		'timeout' => 360,
+		# 'domain' is set to $wgCanonicalServer in Setup.php
+		'forwardCookies' => false,
+		'HTTPProxy' => null
+	)
+);
+
+/**
  * For really cool vim folding this needs to be at the end:
  * vim: foldmarker=@{,@} foldmethod=marker
  * @}
