b48e35ce450cb4a91b5c1300e98d4599c81f5dd5
Warkot <mateusz.warkocki@wikia-inc.com>
2015-09-11 14:45:19 +0200

    XW-217 | removed unsed i18n messages

--- extensions/wikia/TwitterTag/TwitterTag.i18n.php
+++ extensions/wikia/TwitterTag/TwitterTag.i18n.php
@@ -1,19 +1,13 @@
 <?php
 
-$messages = [];
+$messages = [ ];
 
 $messages['en'] = [
 	'twittertag-desc' => 'Creates the <nowiki><twitter></nowiki> tag',
 	'twittertag-widget-id' => 'Error: No Twitter Widget ID provided. Please see [[Help:Social media integration]]',
-	'twittertag-screen-name-label' => 'Tweets by @$1',
-	'twittertag-user-id-label' => 'Tweets by user with ID: $1',
-	'twittertag-list-label' => 'Tweets from @$1/$2',
 ];
 
 $messages['qqq'] = [
 	'twittertag-desc' => 'The description of the app found on [[Special:Version]]',
 	'twittertag-widget-id' => 'The error text displayed when there is no id attribute. In English one, it points to the help page',
-	'twittertag-screen-name-label' => 'It\'s a label of a link that leads to the Twitter profile using screen name of this profile. The text displayed when the user has no JavaScript or it fails to load the Twitter feed for some other reason.',
-	'twittertag-user-id-label' => 'It\'s a label of a link that leads to the Twitter profile using ID of this profile. The text displayed when the user has no JavaScript or it fails to load the Twitter feed for some other reason.',
-	'twittertag-list-label' => 'It\'s a label of a link that leads to the Twitter list of tweets by predefined users. The text displayed when the user has no JavaScript or it fails to load the Twitter feed for some other reason.',
 ];
