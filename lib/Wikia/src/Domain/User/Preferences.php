fd1e0b2be54fc8b6c2f388c95527a6ca67e460fd
Artur Klajnerok <a.klajnerok@gmail.com>
2015-09-11 13:35:03 -0700

    [SERVICES-739] Added annotations for Preferences

--- lib/Wikia/src/Domain/User/Preferences.php
+++ lib/Wikia/src/Domain/User/Preferences.php
@@ -6,19 +6,31 @@ use Wikia\Util\Assert;
 
 class Preferences {
 
+    /** @var GlobalPreference[] */
     private $globalPreferences;
+    /** @var LocalPreference[] */
     private $localPreferences;
 
+    /**
+     * @param GlobalPreference[] $globalPreferences
+     * @param LocalPreference[] $localPreferences
+     */
     function __construct( $globalPreferences, $localPreferences ) {
 
         $this->globalPreferences = $globalPreferences;
         $this->localPreferences = $localPreferences;
     }
 
+    /**
+     * @return GlobalPreference[]
+     */
     public function getGlobalPreferences() {
         return $this->globalPreferences;
     }
 
+    /**
+     * @return LocalPreference[]
+     */
     public function getLocalPreferences() {
         return $this->localPreferences;
     }
