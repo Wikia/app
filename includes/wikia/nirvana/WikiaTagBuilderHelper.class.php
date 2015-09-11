80c5f4ab7101225aee0ec65b487610908cfc1db6
Bogna Knychala <bogna.ka@gmail.com>
2015-09-11 13:25:35 +0200

    XW-204 make query builder smarter

--- includes/wikia/nirvana/WikiaTagBuilderHelper.class.php
+++ includes/wikia/nirvana/WikiaTagBuilderHelper.class.php
@@ -2,20 +2,23 @@
 class WikiaTagBuilderHelper {
 
 	public function buildTagSourceQueryParams( array $allowedParams, array $userParams ) {
+		$params = [];
 		foreach ( array_keys( $allowedParams ) as $name ) {
-			if ( isset( $userParams[$name] ) ) {
-				$allowedParams[$name] = $userParams[$name];
+			if ( array_key_exists( $name, $userParams ) && !empty( $userParams[$name] ) ) {
+				$params[$name] = $userParams[$name];
+			} else if ( array_key_exists( $name, $allowedParams ) && !empty( $allowedParams[$name] ) ) {
+				$params[$name] = $allowedParams[$name];
 			}
 		}
-		return http_build_query( $allowedParams );
+		return http_build_query( $params );
 	}
 
-	public function buildTagAttributes(array $allowedAttrs, array $userAttrs) {
+	public function buildTagAttributes( array $allowedAttrs, array $userAttrs ) {
 		$attributes = [];
 
 		foreach ( $allowedAttrs as $attributeName ) {
 			if ( isset( $userAttrs[$attributeName] ) ) {
-				if ($attributeName === 'style') {
+				if ( $attributeName === 'style' ) {
 					$attributes['style'] = Sanitizer::checkCss( $userAttrs['style'] );
 				} else {
 					$attributes[$attributeName] = $userAttrs[$attributeName];
