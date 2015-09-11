1c1d401ea99c4cb49dec560e0d54d83fbb521e12
Warkot <mateusz.warkocki@wikia-inc.com>
2015-09-11 16:33:48 +0200

    XW-217 | periods for sentences

--- extensions/wikia/TwitterTag/TwitterTag.i18n.php
+++ extensions/wikia/TwitterTag/TwitterTag.i18n.php
@@ -3,11 +3,11 @@
 $messages = [ ];
 
 $messages['en'] = [
-	'twittertag-desc' => 'Creates the <nowiki><twitter></nowiki> tag',
-	'twittertag-widget-id' => 'Error: No Twitter Widget ID provided. Please see [[Help:Social media integration]]',
+	'twittertag-desc' => 'Creates the <nowiki><twitter></nowiki> tag.',
+	'twittertag-widget-id' => 'Error: No Twitter Widget ID provided. Please see [[Help:Social media integration]].',
 ];
 
 $messages['qqq'] = [
-	'twittertag-desc' => 'The description of the app found on [[Special:Version]]',
-	'twittertag-widget-id' => 'The error text displayed when there is no id attribute. In English one, it points to the help page',
+	'twittertag-desc' => 'The description of the app found on [[Special:Version]].',
+	'twittertag-widget-id' => 'The error text displayed when there is no id attribute. In English one, it points to the help page.',
 ];
