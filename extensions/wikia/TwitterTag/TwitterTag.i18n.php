794ae16fe88976a533a508c60bfcf2e4b9cea99f
V. <vforge@users.noreply.github.com>
2015-09-14 14:43:52 +0200

    XW-217 | Fix i18n

--- extensions/wikia/TwitterTag/TwitterTag.i18n.php
+++ extensions/wikia/TwitterTag/TwitterTag.i18n.php
@@ -3,11 +3,11 @@
 $messages = [ ];
 
 $messages['en'] = [
-	'twittertag-desc' => 'Creates the <nowiki><twitter></nowiki> tag.',
-	'twittertag-widget-id' => 'Error: No Twitter Widget ID provided. Please see [[Help:Social media integration]].',
+	'twitter-tag-desc' => 'Creates the <nowiki><twitter></nowiki> tag.',
+	'twitter-tag-widget-id' => 'Error: No Twitter Widget ID provided. Please see [[Help:Social media integration]].',
 ];
 
 $messages['qqq'] = [
-	'twittertag-desc' => 'The description of the app found on [[Special:Version]].',
-	'twittertag-widget-id' => 'The error text displayed when there is no id attribute. In English one, it points to the help page.',
+	'twitter-tag-desc' => 'The description of the app found on [[Special:Version]].',
+	'twitter-tag-widget-id' => 'The error text displayed when there is no id attribute. In English one, it points to the help page.',
 ];
