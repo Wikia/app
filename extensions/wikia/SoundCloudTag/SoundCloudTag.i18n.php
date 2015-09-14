7acd348579b4186aff824720411fda8d7f706182
V. <vforge@users.noreply.github.com>
2015-09-14 15:11:11 +0200

    XW-204 | Remove not needed i18n

--- extensions/wikia/SoundCloudTag/SoundCloudTag.i18n.php
+++ extensions/wikia/SoundCloudTag/SoundCloudTag.i18n.php
@@ -3,10 +3,8 @@ $messages = [];
 
 $messages['en'] = [
 	'soundcloud-tag-desc' => 'SoundCloud widget integration for the Wikia platform',
-	'soundclouds-tag-could-not-render' => 'Failed to render the SoundCloud widget.',
 ];
 
 $messages['qqq'] = [
 	'soundcloud-tag-desc' => '{{desc}}',
-	'soundcloud-tag-could-not-render' => 'Error message shown when the SoundCloud widget could not be loaded. Only happens if SoundCloud has a problem, or the user disallowed 3rd-party resources.',
 ];
