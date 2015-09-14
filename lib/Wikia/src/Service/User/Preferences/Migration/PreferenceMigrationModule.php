58c2a7a880c24c0d88195709ff3d8f8598c54c07
nmonterroso <nelson@wikia-inc.com>
2015-09-14 15:53:45 -0700

    adding preference scope service to determine which preferences are local and which are global

---
+++ lib/Wikia/src/Service/User/Preferences/Migration/PreferenceMigrationModule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wikia\Service\User\Preferences\Migration;
+
+use Wikia\DependencyInjection\InjectorBuilder;
+use Wikia\DependencyInjection\Module;
+
+class PreferenceMigrationModule implements Module {
+
+	public function configure(InjectorBuilder $builder) {
+		global $wgGlobalUserPreferenceWhiteList, $wgLocalUserPreferenceWhiteList;
+
+		$builder
+			->bind(PreferenceScopeService::GLOBAL_SCOPE_PREFS)->to($wgGlobalUserPreferenceWhiteList)
+			->bind(PreferenceScopeService::LOCAL_SCOPE_PREFS)->to($wgLocalUserPreferenceWhiteList);
+	}
+}
