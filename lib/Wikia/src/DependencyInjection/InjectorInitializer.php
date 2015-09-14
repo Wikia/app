8e8bf039b96b96b653424742605992d0f457dfd6
nmonterroso <nelson@wikia-inc.com>
2015-09-14 14:14:21 -0700

    remove non existant import

--- lib/Wikia/src/DependencyInjection/InjectorInitializer.php
+++ lib/Wikia/src/DependencyInjection/InjectorInitializer.php
@@ -3,7 +3,6 @@
 namespace Wikia\DependencyInjection;
 
 use Doctrine\Common\Cache\CacheProvider;
-use Wikia\Cache\Memcache\MemcacheModule;
 use Wikia\Service\Gateway\ConsulUrlProviderModule;
 use Wikia\Service\User\Preferences\PreferenceModule;
 use Wikia\Service\User\Attributes\AttributesModule;
