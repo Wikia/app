6b8108dd27620899ab3ad66a42bc1ba930bb9839
drets <dmytro.rets@wikia-inc.com>
2015-09-14 15:19:39 +0300

    ADEN-2448 CR fixes: to tabs

--- extensions/wikia/AdEngine/js/slot/scrollHandler.js
+++ extensions/wikia/AdEngine/js/slot/scrollHandler.js
@@ -1,78 +1,78 @@
 /*global define*/
 define('ext.wikia.adEngine.slot.scrollHandler', [
-    'ext.wikia.adEngine.adContext',
-    'ext.wikia.adEngine.adHelper',
-    'wikia.log',
-    'wikia.document',
-    'wikia.window'
+	'ext.wikia.adEngine.adContext',
+	'ext.wikia.adEngine.adHelper',
+	'wikia.log',
+	'wikia.document',
+	'wikia.window'
 ], function (adContext, adHelper, log, doc, win) {
-    'use strict';
+	'use strict';
 
-    var logGroup = 'ext.wikia.adEngine.slot.scrollHandler',
-        isRefreshed = {},
-        reloadedView = {},
-        context = adContext.getContext(),
-        config = context.opts.scrollHandlerConfig ||
-            {
-                PREFOOTER_LEFT_BOXAD: { reloadedViewMax: 1 },
-                PREFOOTER_RIGHT_BOXAD: { reloadedViewMax: 3 }
-            };
+	var logGroup = 'ext.wikia.adEngine.slot.scrollHandler',
+		isRefreshed = {},
+		reloadedView = {},
+		context = adContext.getContext(),
+		config = context.opts.scrollHandlerConfig ||
+			{
+				PREFOOTER_LEFT_BOXAD: {reloadedViewMax: 1},
+				PREFOOTER_RIGHT_BOXAD: {reloadedViewMax: 3}
+			};
 
-    function init() {
-        if (context.opts.enableScrollHandler) {
-            for (var slotName in config) {
-                if (config.hasOwnProperty(slotName)) {
-                    isRefreshed[slotName] = false;
-                    reloadedView[slotName] = 0;
-                }
-            }
-            win.addEventListener('scroll', adHelper.throttle(function () {
-                log('Scroll event listener has been added', 'debug', logGroup);
-                for (var slotName in config) {
-                    if (config.hasOwnProperty(slotName)) {
-                        if (config[slotName].hasOwnProperty('reloadedViewMax') &&
-                            config[slotName].reloadedViewMax >= 0 &&
-                            config[slotName].reloadedViewMax <= reloadedView[slotName]) {
-                            continue;
-                        }
-                        refreshSlot(slotName);
-                    }
-                }
-            }));
-        }
-    }
+	function init() {
+		if (context.opts.enableScrollHandler) {
+			for (var slotName in config) {
+				if (config.hasOwnProperty(slotName)) {
+					isRefreshed[slotName] = false;
+					reloadedView[slotName] = 0;
+				}
+			}
+			win.addEventListener('scroll', adHelper.throttle(function () {
+				log('Scroll event listener has been added', 'debug', logGroup);
+				for (var slotName in config) {
+					if (config.hasOwnProperty(slotName)) {
+						if (config[slotName].hasOwnProperty('reloadedViewMax') &&
+							config[slotName].reloadedViewMax >= 0 &&
+							config[slotName].reloadedViewMax <= reloadedView[slotName]) {
+							continue;
+						}
+						refreshSlot(slotName);
+					}
+				}
+			}));
+		}
+	}
 
-    function refreshSlot(slotName) {
-        var status = isReached(doc.getElementById(slotName));
-        if (!isRefreshed[slotName] && status) {
-            log(['refreshSlot', slotName + ' has been refreshed'], 'debug', logGroup);
-            reloadedView[slotName]++;
-            win.adslots2.push(slotName);
-            isRefreshed[slotName] = true;
-        } else if (!status) {
-            isRefreshed[slotName] = false;
-        }
-    }
+	function refreshSlot(slotName) {
+		var status = isReached(doc.getElementById(slotName));
+		if (!isRefreshed[slotName] && status) {
+			log(['refreshSlot', slotName + ' has been refreshed'], 'debug', logGroup);
+			reloadedView[slotName]++;
+			win.adslots2.push(slotName);
+			isRefreshed[slotName] = true;
+		} else if (!status) {
+			isRefreshed[slotName] = false;
+		}
+	}
 
-    function isReached(el) {
-        return win.innerHeight + win.scrollY >= getTopPos(el);
-    }
+	function isReached(el) {
+		return win.innerHeight + win.scrollY >= getTopPos(el);
+	}
 
-    function getTopPos(el) {
-        for (var topPos = 0; el !== null; topPos += el.offsetTop, el = el.offsetParent);
-        return topPos;
-    }
+	function getTopPos(el) {
+		for (var topPos = 0; el !== null; topPos += el.offsetTop, el = el.offsetParent);
+		return topPos;
+	}
 
-    function getReloadedViewCount(slotName) {
-        if (reloadedView.hasOwnProperty(slotName)) {
-            return reloadedView[slotName];
-        }
+	function getReloadedViewCount(slotName) {
+		if (reloadedView.hasOwnProperty(slotName)) {
+			return reloadedView[slotName];
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    return {
-        init: init,
-        getReloadedViewCount: getReloadedViewCount
-    };
+	return {
+		init: init,
+		getReloadedViewCount: getReloadedViewCount
+	};
 });
--- extensions/wikia/AdEngine/js/spec/slot/scrollHandler.spec.js
+++ extensions/wikia/AdEngine/js/spec/slot/scrollHandler.spec.js
@@ -1,109 +1,109 @@
 /*global describe, it, expect, modules, spyOn, beforeEach*/
 describe('ext.wikia.adEngine.slot.scrollHandler', function () {
-    'use strict';
+	'use strict';
 
-    function noop() {
-    }
+	function noop() {
+	}
 
-    var mocks = {
-        adHelper: {
-            throttle: function (func) {
-                return func;
-            }
-        },
-        log: noop,
+	var mocks = {
+		adHelper: {
+			throttle: function (func) {
+				return func;
+			}
+		},
+		log: noop,
 
-        win: {
-            innerHeight: 1000,
-            adslots2: {
-                push: noop
-            },
-            addEventListener: function (event, callback) {
-                if (event === 'scroll') {
-                    callback();
-                }
-            }
-        },
-        doc: {
-            getElementById: function () {
-                return {
-                    offsetTop: 3000,
-                    offsetParent: null
-                };
-            }
-        }
-    };
+		win: {
+			innerHeight: 1000,
+			adslots2: {
+				push: noop
+			},
+			addEventListener: function (event, callback) {
+				if (event === 'scroll') {
+					callback();
+				}
+			}
+		},
+		doc: {
+			getElementById: function () {
+				return {
+					offsetTop: 3000,
+					offsetParent: null
+				};
+			}
+		}
+	};
 
-    beforeEach(function () {
-        mocks.win.scrollY = 0;
-        setContext(mocks, {reloadedViewMax: -1});
-    });
+	beforeEach(function () {
+		mocks.win.scrollY = 0;
+		setContext(mocks, {reloadedViewMax: -1});
+	});
 
-    function getModule() {
-        return modules['ext.wikia.adEngine.slot.scrollHandler'](
-            mocks.context,
-            mocks.adHelper,
-            mocks.log,
-            mocks.doc,
-            mocks.win
-        );
-    }
+	function getModule() {
+		return modules['ext.wikia.adEngine.slot.scrollHandler'](
+			mocks.context,
+			mocks.adHelper,
+			mocks.log,
+			mocks.doc,
+			mocks.win
+		);
+	}
 
-    it('Prefooters should not be refreshed', function () {
-        shouldNotBeRefreshed({scrollY: 1000});
-    });
+	it('Prefooters should not be refreshed', function () {
+		shouldNotBeRefreshed({scrollY: 1000});
+	});
 
-    it('Prefooter should not be refreshed when reloadedViewMax is 0', function () {
-        shouldNotBeRefreshed({reloadedViewMax: 0});
-    });
+	it('Prefooter should not be refreshed when reloadedViewMax is 0', function () {
+		shouldNotBeRefreshed({reloadedViewMax: 0});
+	});
 
-    it('Prefooter should be refreshed when reloadedViewMax is 1', function () {
-        shouldBeRefreshed({reloadedViewMax: 1});
-    });
+	it('Prefooter should be refreshed when reloadedViewMax is 1', function () {
+		shouldBeRefreshed({reloadedViewMax: 1});
+	});
 
-    it('Prefooter should be refreshed when reloadedViewMax is -1', function () {
-        shouldBeRefreshed({reloadedViewMax: -1});
-    });
+	it('Prefooter should be refreshed when reloadedViewMax is -1', function () {
+		shouldBeRefreshed({reloadedViewMax: -1});
+	});
 
-    it('RV count of unsupported slot equals null', function () {
-        expect(getModule().getReloadedViewCount('TOP_LEADERBOARD')).toBe(null);
-    });
+	it('RV count of unsupported slot equals null', function () {
+		expect(getModule().getReloadedViewCount('TOP_LEADERBOARD')).toBe(null);
+	});
 
-    function shouldBeRefreshed(params) {
-        spyOn(mocks.win.adslots2, 'push');
-        mocks.win.scrollY = params.scrollY || 2000;
-        setContext(mocks, params);
+	function shouldBeRefreshed(params) {
+		spyOn(mocks.win.adslots2, 'push');
+		mocks.win.scrollY = params.scrollY || 2000;
+		setContext(mocks, params);
 
-        var scrollHandler = getModule();
-        scrollHandler.init();
+		var scrollHandler = getModule();
+		scrollHandler.init();
 
-        expect(mocks.win.adslots2.push).toHaveBeenCalled();
-        expect(scrollHandler.getReloadedViewCount('PREFOOTER_LEFT_BOXAD')).toEqual(1);
-    }
+		expect(mocks.win.adslots2.push).toHaveBeenCalled();
+		expect(scrollHandler.getReloadedViewCount('PREFOOTER_LEFT_BOXAD')).toEqual(1);
+	}
 
-    function shouldNotBeRefreshed(params) {
-        spyOn(mocks.win.adslots2, 'push');
-        mocks.win.scrollY = params.scrollY || 2000;
-        setContext(mocks, params);
+	function shouldNotBeRefreshed(params) {
+		spyOn(mocks.win.adslots2, 'push');
+		mocks.win.scrollY = params.scrollY || 2000;
+		setContext(mocks, params);
 
-        var scrollHandler = getModule();
-        scrollHandler.init();
+		var scrollHandler = getModule();
+		scrollHandler.init();
 
-        expect(mocks.win.adslots2.push).not.toHaveBeenCalled();
-        expect(scrollHandler.getReloadedViewCount('PREFOOTER_LEFT_BOXAD')).toEqual(0);
-    }
+		expect(mocks.win.adslots2.push).not.toHaveBeenCalled();
+		expect(scrollHandler.getReloadedViewCount('PREFOOTER_LEFT_BOXAD')).toEqual(0);
+	}
 
-    function setContext(mocks, params) {
-        mocks.context = {
-            getContext: function () {
-                return {
-                    opts: {
-                        enableScrollHandler: true,
-                        scrollHandlerConfig: {PREFOOTER_LEFT_BOXAD: {reloadedViewMax: params.reloadedViewMax}}
-                    }
-                };
-            }
-        };
-    }
+	function setContext(mocks, params) {
+		mocks.context = {
+			getContext: function () {
+				return {
+					opts: {
+						enableScrollHandler: true,
+						scrollHandlerConfig: {PREFOOTER_LEFT_BOXAD: {reloadedViewMax: params.reloadedViewMax}}
+					}
+				};
+			}
+		};
+	}
 
 });
