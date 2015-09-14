b7a28e841ae33d84c95f360ba960d678b73902dc
Igor Rogatty <igor@wikia-inc.com>
2015-09-14 17:45:16 +0200

    XW-200 | make it work in Mercury

--- extensions/wikia/PollSnackTag/PollSnackTagController.class.php
+++ extensions/wikia/PollSnackTag/PollSnackTagController.class.php
@@ -1,8 +1,10 @@
 <?php
-class PollSnackTagController extends WikiaParserTagController {
+class PollSnackTagController extends WikiaController {
 
 	const PARSER_TAG_NAME = 'pollsnack';
+
 	const TAG_SRC = 'http://files.quizsnack.com/iframe/embed.html?';
+
 	const TAG_SOURCE_ALLOWED_PARAMS_WITH_DEFAULTS = [
 		'hash' => '',
 		// Width and height are defaults when generating iframe in PollSnack
@@ -11,10 +13,12 @@ class PollSnackTagController extends WikiaParserTagController {
 		'wmode' => '',
 		'bgcolor' => '',
 	];
+
 	const TAG_ALLOWED_ATTRIBUTES = [
 		'width',
 		'height',
 	];
+
 	const TAG_DEFAULT_ATTRIBUTES = [
 		'data-wikia-widget' => self::PARSER_TAG_NAME,
 		'scrolling' => 'no',
@@ -26,16 +30,14 @@ class PollSnackTagController extends WikiaParserTagController {
 		'height' => '370',
 	];
 
-	private $tagBuildSource;
-	private $tagBuilderHelper;
-	private $iFrameHelper;
+	private $helper;
 	private $validator;
 
 	public function __construct() {
 		parent::__construct();
-		$this->tagBuilderHelper = new WikiaTagBuilderHelper();
+
+		$this->helper = new WikiaIFrameTagBuilderHelper();
 		$this->validator = new PollSnackTagValidator();
-		$this->iFrameHelper = new WikiaIFrameTagBuilderHelper();
 	}
 
 	public static function onParserFirstCallInit( Parser $parser ) {
@@ -45,31 +47,48 @@ class PollSnackTagController extends WikiaParserTagController {
 
 	public function renderTag( $input, array $args, Parser $parser, PPFrame $frame ) {
 		$isValid = $this->validator->validateAttributes( $args );
+
 		if ( !$isValid ) {
 			return '<strong class="error">' . wfMessage( 'pollsnack-tag-could-not-render' )->parse() . '</strong>';
 		}
 
-		$this->tagBuildSource = self::TAG_SRC . $this->tagBuilderHelper->buildTagSourceQueryParams(
-				self::TAG_SOURCE_ALLOWED_PARAMS_WITH_DEFAULTS, $args
+		$queryParamsOverride = null;
+		if ( $this->helper->isMobileSkin() ) {
+			/**
+			 * This is needed because for mobile devices:
+			 * - we want content (external too!) to be responsive
+			 * - PollSnack checks if browser supports Flash and:
+			 *   - if yes then loads <embed> element
+			 *   - if no then loads another iframe (HTML5) and passes parameters from the original URL
+			 *
+			 * Issue with the last item is that if we don't put `width` or `height` in URL
+			 * then the second iframe gets `&width=undefined&height=undefined` and doesn't load at all.
+			 *
+			 * Trying to load the HTML5 iframe directly results in AccessDenied error
+			 * so this is the only working solution I found.
+			 */
+			$queryParamsOverride = [
+				'width' => '',
+				'height' => ''
+			];
+		}
+
+		$sourceUrl = self::TAG_SRC . $this->helper->buildTagSourceQueryParams(
+				self::TAG_SOURCE_ALLOWED_PARAMS_WITH_DEFAULTS, $args, $queryParamsOverride
 			);
 
 		$iFrameHTML = Html::element(
 			'iframe',
-			$this->buildTagAttributes( $args )
+			$this->buildTagAttributes( $sourceUrl, $args )
 		);
 
-		return $this->iFrameHelper->wrapForMobile( $iFrameHTML );
+		return $this->helper->wrapForMobile( $iFrameHTML );
 	}
 
-	private function buildTagAttributes( $args ) {
-		$attributes = $this->tagBuilderHelper->buildTagAttributes( self::TAG_ALLOWED_ATTRIBUTES, $args );
-		$attributes['src'] = $this->tagBuildSource;
+	private function buildTagAttributes( $sourceUrl, $args ) {
+		$attributes = $this->helper->buildTagAttributes( self::TAG_ALLOWED_ATTRIBUTES, $args );
+		$attributes['src'] = $sourceUrl;
 
 		return array_merge( self::TAG_DEFAULT_ATTRIBUTES, $attributes );
-	}
-
-
-	protected function buildParamValidator( $paramName ) {
-		return true;
 	}
 }
