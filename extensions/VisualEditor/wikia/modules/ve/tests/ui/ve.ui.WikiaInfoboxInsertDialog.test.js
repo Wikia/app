4fabee8ea7deb5fa147be91e376d08a48f33dfb8
Diana <dianafa@o2.pl>
2015-09-15 01:25:52 +0000

    tests

--- extensions/VisualEditor/wikia/modules/ve/tests/ui/ve.ui.WikiaInfoboxInsertDialog.test.js
+++ extensions/VisualEditor/wikia/modules/ve/tests/ui/ve.ui.WikiaInfoboxInsertDialog.test.js
@@ -6,16 +6,16 @@ QUnit.module( 've.ui.WikiaInfoboxInsertDialog' );
 
 /* Tests */
 
-QUnit.test( 'sortTemplateTitles', function ( assert ) {
+QUnit.test( 'sortTemplateTitles', function () {
 	var i,
 		result,
+		dialog = new ve.ui.WikiaInfoboxInsertDialog(),
 		testCases = ve.ui.wikiaExample.sortTemplateTitlesCases,
-		len = testCases.length,
-		dialog = new ve.ui.WikiaInfoboxInsertDialog();
+		len = testCases.length;
 
 	QUnit.expect( len );
 	for ( i = 0; i < len; i++ ) {
-		result = dialog.sortTemplateTitles.apply( dialog, [ testCases[i].data ] );
-		assert.equal( result, testCases[i].expected );
+		result = dialog.sortTemplateTitles.apply( dialog, [testCases[i].data] );
+		QUnit.assert.deepEqual( result, testCases[i].expected );
 	}
 } );
