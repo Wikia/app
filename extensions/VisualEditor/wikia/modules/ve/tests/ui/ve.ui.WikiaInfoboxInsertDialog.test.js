26907b342f7e82f8b1dd4500b491dd1d9628c223
Diana <dianafa@o2.pl>
2015-09-15 01:45:36 +0000

    refactor tests

--- extensions/VisualEditor/wikia/modules/ve/tests/ui/ve.ui.WikiaInfoboxInsertDialog.test.js
+++ extensions/VisualEditor/wikia/modules/ve/tests/ui/ve.ui.WikiaInfoboxInsertDialog.test.js
@@ -6,7 +6,7 @@ QUnit.module( 've.ui.WikiaInfoboxInsertDialog' );
 
 /* Tests */
 
-QUnit.test( 'sortTemplateTitles', function () {
+QUnit.test( 'sortTemplateTitles', function ( assert ) {
 	var i,
 		result,
 		dialog = new ve.ui.WikiaInfoboxInsertDialog(),
@@ -14,8 +14,9 @@ QUnit.test( 'sortTemplateTitles', function () {
 		len = testCases.length;
 
 	QUnit.expect( len );
+
 	for ( i = 0; i < len; i++ ) {
 		result = dialog.sortTemplateTitles.apply( dialog, [testCases[i].data] );
-		QUnit.assert.deepEqual( result, testCases[i].expected );
+		assert.deepEqual( result, testCases[i].expected );
 	}
 } );
