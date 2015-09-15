70332eddb4076d8805ac5c63904a29bf004e846e
Igor Rogatty <igor@wikia-inc.com>
2015-09-15 14:34:26 +0200

    XW-297 | respect `required` option in WikiaValidatorRegex

--- includes/wikia/validators/WikiaValidatorRegex.class.php
+++ includes/wikia/validators/WikiaValidatorRegex.class.php
@@ -1,23 +1,29 @@
-<?php 
+<?php
 
 class WikiaValidatorRegex extends WikiaValidator {
-	
-	protected function config( array $options = array() ) {
-		$this->setOption('pattern', '\s*');
+
+	protected function config( array $options = [] ) {
+		$this->setOption( 'pattern', '\s*' );
 	}
-	
-	protected function configMsgs( array $msgs = array() ) {
+
+	protected function configMsgs( array $msgs = [] ) {
+		$this->setMsg( 'empty', 'wikia-validator-regex-empty' );
 		$this->setMsg( 'wrong', 'wikia-validator-regex-wrong' );
 	}
-	
-	public function isValidInternal($value = null) {
-		$pattern = $this->getOption('pattern'); 
-		
-		if( !preg_match($pattern, $value) ) {
+
+	public function isValidInternal( $value = null ) {
+		$pattern = $this->getOption( 'pattern' );
+
+		if ( !preg_match( $pattern, $value ) ) {
 			$this->createError( 'wrong' );
 			return false;
 		}
-		
+
+		if ( $this->getOption( 'required' ) === true && empty( $value ) ) {
+			$this->createError( 'empty' );
+			return false;
+		}
+
 		return true;
 	}
 }
