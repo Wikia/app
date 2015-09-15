0a535a125edb92ef1a4021bb81460c8f3d4386f3
Rafal Leszczynski <rafal@wikia-inc.com>
2015-09-15 13:27:36 +0200

    fix jscs syntax issues

--- extensions/VisualEditor/wikia/modules/ve/tests/ui/ve.ui.wikiaExample.js
+++ extensions/VisualEditor/wikia/modules/ve/tests/ui/ve.ui.wikiaExample.js
@@ -17,46 +17,46 @@ ve.ui.wikiaExample.sortTemplateTitlesCases = [
 	{
 		data: [
 			{
-				label: "Infobox Character",
-				title: "Infobox_Character"
+				label: 'Infobox Character',
+				title: 'Infobox_Character'
 			},
 			{
-				label: "InfoboxCharacter",
-				title: "InfoboxCharacter"
+				label: 'InfoboxCharacter',
+				title: 'InfoboxCharacter'
 			},
 			{
-				label: "Infobox  Character",
-				title: "Infobox__Character"
+				label: 'Infobox  Character',
+				title: 'Infobox__Character'
 			},
 			{
-				label: "Character",
-				title: "Character"
+				label: 'Character',
+				title: 'Character'
 			},
 			{
-				label: "Some Title",
-				title: "Some_Title"
-			},
+				label: 'Some Title',
+				title: 'Some_Title'
+			}
 		],
 		expected: [
 			{
-				"label": "Character",
-				"title": "Character"
+				'label': 'Character',
+				'title': 'Character'
 			},
 			{
-				"label": "InfoboxCharacter",
-				"title": "InfoboxCharacter"
+				'label': 'InfoboxCharacter',
+				'title': 'InfoboxCharacter'
 			},
 			{
-				"label": "Infobox Character",
-				"title": "Infobox_Character"
+				'label': 'Infobox Character',
+				'title': 'Infobox_Character'
 			},
 			{
-				"label": "Infobox  Character",
-				"title": "Infobox__Character"
+				'label': 'Infobox  Character',
+				'title': 'Infobox__Character'
 			},
 			{
-				"label": "Some Title",
-				"title": "Some_Title"
+				'label': 'Some Title',
+				'title': 'Some_Title'
 			}
 		]
 	}
