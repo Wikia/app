70a2de8e76a3f694e9421e9d1387e91eac1e49c9
Łukasz Harasimowicz <dev@harnash.eu>
2015-09-15 17:59:55 +0200

    Added tests and implementation to bypass UserPreferences caching.
    Caching inside UserPreferences class was hiding the problem that not
    all cached values were properly saved into the storage. This change
    allows test UserPreference without it's internal cache which exposes
    a serious bug described in SERVICES-757.

--- lib/Wikia/tests/Service/User/UserPreferencesIntegrationTest.php
+++ lib/Wikia/tests/Service/User/UserPreferencesIntegrationTest.php
@@ -40,16 +40,29 @@ class UserPreferencesIntegrationTest extends PHPUnit_Framework_TestCase {
 		$this->assertGreaterThanOrEqual(0, $testPref, "Invalid preference value - expected integer >= 0");
 	}
 
-	function testSetSingleUserPreference() {
+	function testSetSingleUserPreferenceCached() {
 		// first set the preference to a value different then default
 		$this->getUserPreferences()->set( $this->testUserId, self::TEST_PREFERENCE_NAME, 1);
 		// now let's verify it's value
 		$testPref = $this->getUserPreferences()->get($this->testUserId, self::TEST_PREFERENCE_NAME);
-		$this->assertEquals($testPref, 1, "Could not set user preference '" . self::TEST_PREFERENCE_NAME . "' for user id: '$this->testUserId'");
+		$this->assertEquals($testPref, 1, "Could not set user preference [cached] '" . self::TEST_PREFERENCE_NAME . "' for user id: '$this->testUserId'");
 		// reset value to the default
 		$this->getUserPreferences()->set($this->testUserId, self::TEST_PREFERENCE_NAME, 0);
 		// again verify it's value
 		$testPref = $this->getUserPreferences()->get($this->testUserId, self::TEST_PREFERENCE_NAME);
-		$this->assertEquals($testPref, 0, "Error resetting user preference '" . self::TEST_PREFERENCE_NAME . "' for user id: '$this->testUserId'");
+		$this->assertEquals($testPref, 0, "Error resetting user preference [cached] '" . self::TEST_PREFERENCE_NAME . "' for user id: '$this->testUserId'");
+	}
+
+	function testSetSingleUserPreferenceNotCached() {
+		// first set the preference to a value different then default
+		$this->getUserPreferences()->set( $this->testUserId, self::TEST_PREFERENCE_NAME, 1, true);
+		// now let's verify it's value
+		$testPref = $this->getUserPreferences()->get($this->testUserId, self::TEST_PREFERENCE_NAME, true);
+		$this->assertEquals($testPref, 1, "Could not set user preference [not cached] '" . self::TEST_PREFERENCE_NAME . "' for user id: '$this->testUserId'");
+		// reset value to the default
+		$this->getUserPreferences()->set($this->testUserId, self::TEST_PREFERENCE_NAME, 0, true);
+		// again verify it's value
+		$testPref = $this->getUserPreferences()->get($this->testUserId, self::TEST_PREFERENCE_NAME, true);
+		$this->assertEquals($testPref, 0, "Error resetting user preference [not cached] '" . self::TEST_PREFERENCE_NAME . "' for user id: '$this->testUserId'");
 	}
 }
