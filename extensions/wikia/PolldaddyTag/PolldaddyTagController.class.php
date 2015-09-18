109480a3393d5bea7b3d665b55ba3e104ddbfacf
V <bartosz.bentkowski@gmail.com>
2015-09-18 12:47:21 +0200

    XW-199 | Add warning for the future generations

--- extensions/wikia/PolldaddyTag/PolldaddyTagController.class.php
+++ extensions/wikia/PolldaddyTag/PolldaddyTagController.class.php
@@ -30,6 +30,12 @@ class PolldaddyTagController extends WikiaController {
 			return '<strong class="error">' . wfMessage( 'polldaddy-tag-could-not-render' )->parse() . '</strong>';
 		}
 
+		/**
+		 * Warning: as we're using user provided ID number to construct ID of an element it HAS TO BE
+		 * unique on the page - in other words: including widget for the SECOND time will not have any
+		 * effect - all content will be rendered inside FIRST element, overriding it.
+		 */
+
 		return $this->helper->isMobileSkin() ?
 			Html::element(
 				'a',
