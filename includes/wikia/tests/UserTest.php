55172981073212c87ff4edcb31beb1fa52c667bf
Damon Snyder <damon@wikia-inc.com>
2015-09-21 23:51:53 +0000

    Add testGetGlobalPreferenceWithMockedUserPreferenceService
    - add the scaffolding to inject a mock PreferenceService into the User
    - add testGetGlobalPreferenceWithMockedUserPreferenceService

--- includes/wikia/tests/UserTest.php
+++ includes/wikia/tests/UserTest.php
@@ -1,5 +1,49 @@
 <?php
+
+use Wikia\DependencyInjection\Injector;
+use Wikia\DependencyInjection\InjectorBuilder;
+use Wikia\Service\User\Preferences\PreferenceService;
+use Wikia\Service\User\Attributes\AttributeService;
+use Wikia\Domain\User\Preferences\UserPreferences;
+use Wikia\Service\User\Preferences\Migration\PreferenceScopeService;
+use Wikia\Service\User\Preferences\Migration\PreferenceCorrectionService;
+
+
 class UserTest extends WikiaBaseTest {
+
+	const TEST_USER_ID = 5;
+
+	protected $injector;
+	protected $userPreferenceServiceMock;
+	protected $userAttributeServiceMock;
+
+	protected $testUser;
+
+	public function setUp() {
+		parent::setUp();
+		$this->setupAndInstallUserPreferenceServiceMock();
+
+		$this->testUser = User::newFromId( self::TEST_USER_ID );
+	}
+
+	private function setupAndInstallUserPreferenceServiceMock() {
+		global $wgGlobalUserPreferenceWhiteList, $wgLocalUserPreferenceWhiteList;
+		$this->userPreferenceServiceMock = $this->getMock( 'Wikia\Service\User\Preferences\PreferenceService',
+			['getGlobalPreference', 'getPreferences', 'setPreferences', 'setGlobalPreference', 'deleteGlobalPreference',
+			'getLocalPreference', 'setLocalPreference', 'deleteLocalPreference', 'save', 'getGlobalDefault'] );
+		$this->userAttributeServiceMock = $this->getMock( 'Wikia\Service\User\Attributes\AttributeService' );
+
+
+		$container = ( new InjectorBuilder() )
+			->bind( PreferenceService::class )->to( $this->userPreferenceServiceMock )
+			->bind( AttributeService::class )->to( $this->userAttributeServiceMock )
+			->bind( PreferenceScopeService::GLOBAL_SCOPE_PREFS )->to( $wgGlobalUserPreferenceWhiteList )
+			->bind( PreferenceScopeService::LOCAL_SCOPE_PREFS )->to( $wgLocalUserPreferenceWhiteList )
+			->bind( PreferenceCorrectionService::PREFERENCE_CORRECTION_ENABLED )->to( false )
+			->build();
+		Injector::setInjector( $container );
+	}
+
 
 	/** @dataProvider localOptionNameProvider */
 	public function testCreateLocalOptionName($input, $expected) {
@@ -20,5 +64,27 @@ class UserTest extends WikiaBaseTest {
 				array("foo", 1, "_"), "foo_1",
 			)
 		);
+	}
+
+	public function testGetGlobalPreferenceWithMockedUserPreferenceService() {
+		$preference = 'somepref';
+		$value = 'somevalue';
+
+		$preferences = (new UserPreferences())
+			->setGlobalPreference( $preference, $value );
+
+		$this->userPreferenceServiceMock->expects( $this->once() )
+			->method( 'getGlobalPreference' )
+			->with( $this->testUser->getId(), $preference, null, false )
+			->willReturn( $value );
+
+
+		$this->userPreferenceServiceMock->expects( $this->once() )
+			->method( 'getPreferences' )
+			->with( $this->testUser->getId() )
+			->willReturn( $preferences );
+
+		$this->mockGlobalVariable('wgPreferenceServiceRead', true);
+		$this->assertEquals($value, $this->testUser->getGlobalPreference($preference));
 	}
 }
