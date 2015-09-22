e1be4b8dd9844891a074abb017cced4c8f7020ea
James Sutterfield <james@wikia-inc.com>
2015-09-22 15:11:06 -0700

    Revert "Merge pull request #8329 from Wikia/SOC-1216"
    This reverts commit 61de0fcb433fe078173a68eb4e1d2d7765f20eba, reversing
    changes made to 8ba4aa3a262c65a91146363ebcd71892a7744c65.

--- extensions/wikia/UserProfilePageV3/UserIdentityBox.class.php
+++ extensions/wikia/UserProfilePageV3/UserIdentityBox.class.php
@@ -629,9 +629,4 @@ class UserIdentityBox {
 		return $this->favWikisModel;
 	}
 
-	public function clearCache() {
-		global $wgMemc;
-
-		$wgMemc->delete( $this->getMemcUserIdentityDataKey() );
-	}
 }
