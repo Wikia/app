07a553135f3c5c4e783d8f0205ef7ac1b1e4af41
Mari McMurtrie <mari@wikia-inc.com>
2015-09-23 12:48:18 -0700

    INT-194: Code refined to limit all wikia search to its content language.

--- extensions/wikia/GlobalNavigation/scripts/GoogleSearchAPI.js
+++ extensions/wikia/GlobalNavigation/scripts/GoogleSearchAPI.js
@@ -1,10 +1,11 @@
 (function () {
-	if ( true ) {
+	if ( window.mytest === 'mari' ) {
 		var searchForm = document.getElementById('searchForm'),
 			searchContainer = document.getElementsByClassName('search-form-wrapper'),
             cx = '005745855109319432328:coaj7jf_wgs',//TODO: Hide it
 			gcse = document.createElement('script'),
-			s = document.getElementsByTagName('script')[0];
+			s = document.getElementsByTagName('script')[0],
+			contentLang = window.wgContentLanguage || 'en';
 
 		gcse.type = 'text/javascript';
 		gcse.async = true;
@@ -16,11 +17,17 @@
 			evt.preventDefault();
 			this.searchQuery = $('#searchInput').val();
 
-			//get info of local or global and modify query if it is local search
+			//get info of local or global and modify query scope
 			$selectElement = $('#searchSelect');
 			$selectedOption = $selectElement.find('option:selected');
 			if ($selectedOption.val() === 'local') {
 				this.searchQuery += ' site:' + window.location.hostname;
+			} else {
+				if ( contentLang === 'en') {
+					this.searchQuery += ' site:*.wikia.com';
+				} else {
+					this.searchQuery += ' site:' + contentLang + '.*.wikia.com';
+				}
 			}
 
 			//Invoke google search
