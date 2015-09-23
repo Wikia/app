e3ad28083637fd356b199e527bb23b7740a920f2
nmonterroso <nelson@wikia-inc.com>
2015-09-23 10:32:31 -0700

    adding counters for differences
    (cherry picked from commit e2b3d5bb6366ee7509156cc840430aa0016af678)

--- lib/Wikia/src/Service/User/Preferences/Migration/PreferenceMigrationModule.php
+++ lib/Wikia/src/Service/User/Preferences/Migration/PreferenceMigrationModule.php
@@ -4,10 +4,12 @@ namespace Wikia\Service\User\Preferences\Migration;
 
 use Wikia\DependencyInjection\InjectorBuilder;
 use Wikia\DependencyInjection\Module;
+use Wikia\Util\Statistics\BernoulliTrial;
 
 class PreferenceMigrationModule implements Module {
 
 	const PREFERENCE_CORRECTION_RAMP = 5;
+	const PREFERENCE_CORRECTION_SAMPLE_RATE = 0.2;
 
 	public function configure( InjectorBuilder $builder ) {
 		global $wgGlobalUserPreferenceWhiteList, $wgLocalUserPreferenceWhiteList, $wgCityId;
@@ -16,6 +18,7 @@ class PreferenceMigrationModule implements Module {
 
 		$builder
 			->bind( PreferenceCorrectionService::PREFERENCE_CORRECTION_ENABLED )->to($preferenceCorrectionEnabled)
+			->bind( PreferenceCorrectionService::PREFERENCE_CORRECTION_SAMPLER )->to(new BernoulliTrial(self::PREFERENCE_CORRECTION_SAMPLE_RATE))
 			->bind( PreferenceScopeService::GLOBAL_SCOPE_PREFS )->to( $wgGlobalUserPreferenceWhiteList )
 			->bind( PreferenceScopeService::LOCAL_SCOPE_PREFS )->to( $wgLocalUserPreferenceWhiteList );
 	}
