126e2285d34cd604ee7e4ee79ff8ed79e6feb335
James Sutterfield <james@wikia-inc.com>
2015-09-22 17:03:47 -0700

    Add ticket number to method docs

--- includes/User.php
+++ includes/User.php
@@ -2695,7 +2695,7 @@ class User {
 	 * Returns true if 1.) User is logged in, 2.) The attribute is one used by clients other
 	 * than MW (eg, the avatar service or discussion app), and 3.) the cache for that user is
 	 * not expired. We're testing to see how many requests would make it to the attribute service
-	 * if we cached the attributes used by outside clients for 1 minute.
+	 * if we cached the attributes used by outside clients for 1 minute. See SOC-1315.
 	 * @param $attributeName
 	 * @return bool
 	 */
