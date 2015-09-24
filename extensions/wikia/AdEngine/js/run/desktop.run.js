cae61a0c3432178ea6453b8b853d90ffc08003f8
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2015-09-24 09:55:02 +0200

    ADEN-2406 Renamed SourcePoint detection module

--- extensions/wikia/AdEngine/js/run/desktop.run.js
+++ extensions/wikia/AdEngine/js/run/desktop.run.js
@@ -13,7 +13,7 @@ require([
 	'ext.wikia.adEngine.slot.scrollHandler',
 	'ext.wikia.adEngine.slotTracker',
 	'ext.wikia.adEngine.slotTweaker',
-	'ext.wikia.adEngine.sourcePoint',
+	'ext.wikia.adEngine.sourcePointDetection',
 	'wikia.krux',
 	'wikia.window'
 ], function (
