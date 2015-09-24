55c8d42f71e9c52d869b9e493bb5b8b7884161c9
V <bartosz.bentkowski@gmail.com>
2015-09-24 13:52:06 +0200

    XW-31 | Remove wgOasisBreakpointsDE from the code

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -1766,13 +1766,6 @@ $wgOasisTypography = true;
 $wgOasisBreakpoints = true;
 
 /**
- * Force new breakpoints $wgOasisBreakpoints for German wikis
- * see CONCF-433
- * todo remove when 71M adjusts their styles
- */
-$wgOasisBreakpointsDE = true;
-
-/**
  * Add poweruser to implicit groups
  */
 $wgImplicitGroups[] = 'poweruser';
