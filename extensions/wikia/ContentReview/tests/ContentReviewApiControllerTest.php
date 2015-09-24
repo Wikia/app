0e9d866d5ce4d4baea464c8743f2e27669eb0d70
adamkarminski <adamkarminski@gmail.com>
2015-09-24 18:10:51 +0200

    CE-2679 Add a test for enableTestMode

--- extensions/wikia/ContentReview/tests/ContentReviewApiControllerTest.php
+++ extensions/wikia/ContentReview/tests/ContentReviewApiControllerTest.php
@@ -70,6 +70,82 @@ class ContentReviewApiControllerTest extends WikiaBaseTest {
 		$this->contentReviewApiControllerMock->updateReviewsStatus();
 	}
 
+	/**
+	 * A test for the API method that enable JS test mode.
+	 * @dataProvider enableTestModeDataProvider
+	 * @param array $inputData
+	 * [
+	 * 		'wasPosted' => bool,
+	 * 		'requestToken' => string,
+	 * 		'userEditToken' => string,
+	 * 		'isJsPage' => bool,
+	 * 		'userId' => int,
+	 * 		'userCanEdit' => bool,
+		],
+	 * @param string $expected 'success' or a class name of an expected exception
+	 * @param $message
+	 */
+	public function testEnableTestMode( array $inputData, $expected, $message ) {
+		if ( $expected !== 'success' ) {
+			$this->setExpectedException( $expected );
+		}
+
+		/**
+		 * User Mock
+		 */
+		$userMock = $this->getMock( '\User', [ 'getId', 'getEditToken' ] );
+		$userMock->expects( $this->any() )
+			->method( 'getId' )
+			->willReturn( $inputData['userId'] );
+		$userMock->expects( $this->any() )
+			->method( 'getEditToken' )
+			->willReturn( $inputData['userEditToken'] );
+
+		$this->mockGlobalVariable( 'wgUser', $userMock );
+
+		/**
+		 * Wikia Request Mock
+		 */
+		$requestMock = $this->getMock( '\WikiaRequest', [ 'wasPosted', 'getVal' ], [ [] ] );
+		$requestMock->expects( $this->any() )
+			->method( 'wasPosted' )
+			->willReturn( $inputData['wasPosted'] );
+		$requestMock->expects( $this->any() )
+			->method( 'getVal' )
+			->willReturn( $inputData['requestToken'] );
+		$requestMock->expects( $this->any() )
+			->method( 'getInt' )
+			->willReturn( 0 ); // pageId, does not really matter since the Title is overwritten
+
+		/**
+		 * Title Mock
+		 */
+		$titleMock = $this->getMock( '\Title', [ 'isJsPage', 'userCan' ] );
+		$titleMock->expects( $this->any() )
+			->method( 'isJsPage' )
+			->willReturn( $inputData['isJsPage'] );
+		$titleMock->expects( $this->any() )
+			->method( 'userCan' )
+			->with( 'edit' )
+			->willReturn( $inputData['userCanEdit'] );
+		$this->getStaticMethodMock( '\Title', 'newFromId' )
+			->expects( $this->any() )
+			->method( 'newFromId' )
+			->willReturn( $titleMock );
+
+		/**
+		 * API Controller Mock
+		 */
+		$apiController = $this->getMock( '\ContentReviewApiController', [ 'getHelper', 'makeSuccessResponse', 'setVal' ] );
+		$helperMock = $this->getMock( '\Wikia\ContentReview\Helper', [ 'setContentReviewTestMode' ] );
+		$apiController->expects( $this->any() )
+			->method( 'getHelper' )
+			->willReturn( $helperMock );
+
+		$apiController->setRequest( $requestMock );
+		$apiController->enableTestMode();
+	}
+
 	private function prepareControllerPropertiesMocks( $params ) {
 		/* @var \WikiaRequest $requestMock */
 		$requestMock = $this->getMockBuilder( '\WikiaRequest' )
@@ -222,6 +298,90 @@ class ContentReviewApiControllerTest extends WikiaBaseTest {
 			[ false, false, false, 'BadRequestApiException' ],
 			[ true, false, false, 'BadRequestApiException' ],
 			[ true, true, false, 'PermissionsException' ],
+		];
+	}
+
+	/**
+	 * A data provider for a test of the ContentReview API method - enableTestMode().
+	 * @return array
+	 */
+	public function enableTestModeDataProvider() {
+		$validToken = MWCryptRand::generateHex( 32 );
+		$invalidToken = MWCryptRand::generateHex( 32 );
+
+		return [
+			[
+				[
+					'wasPosted' => false,
+					'requestToken' => $validToken,
+					'userEditToken' => $validToken,
+					'isJsPage' => true,
+					'userId' => 1234,
+					'userCanEdit' => true,
+				],
+				'BadRequestApiException',
+				'The request would be ok if it was POSTed.',
+			],
+			[
+				[
+					'wasPosted' => true,
+					'requestToken' => $validToken,
+					'userEditToken' => $invalidToken,
+					'isJsPage' => true,
+					'userId' => 1234,
+					'userCanEdit' => true,
+				],
+				'BadRequestApiException',
+				'A token sent in the request does not match with the user\'s edit token.',
+			],
+			[
+				[
+					'wasPosted' => true,
+					'requestToken' => $validToken,
+					'userEditToken' => $validToken,
+					'isJsPage' => false,
+					'userId' => 1234,
+					'userCanEdit' => true,
+				],
+				'NotFoundApiException',
+				'A request was sent from a non-JS page.',
+			],
+			[
+				[
+					'wasPosted' => TRUE,
+					'requestToken' => $validToken,
+					'userEditToken' => $validToken,
+					'isJsPage' => true,
+					'userId' => 0,
+					'userCanEdit' => true,
+				],
+				'PermissionsException',
+				'Asking for the user\'s ID returned 0. For some reason.',
+			],
+			[
+				[
+					'wasPosted' => TRUE,
+					'requestToken' => $validToken,
+					'userEditToken' => $validToken,
+					'isJsPage' => true,
+					'userId' => 123,
+					'userCanEdit' => false,
+				],
+				'PermissionsException',
+				'The user does not have permissions to edit the JS page.',
+			],
+			[
+				[
+					'wasPosted' => TRUE,
+					'requestToken' => $validToken,
+					'userEditToken' => $validToken,
+					'isJsPage' => true,
+					'userId' => 123,
+					'userCanEdit' => true,
+				],
+				'success',
+				'Everything is fine, methods to enable the test mode and make a success response should be called once.',
+			],
 		];
 	}
 }
