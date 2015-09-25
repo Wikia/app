befa3cc28a24dd3b9d6271df0a826a7edc445526
Paul Oslund <poslund@wikia-inc.com>
2015-09-25 16:53:54 +0000

    VE-1926: Bring in new i18n files for Visual Editor

--- extensions/VisualEditor/VisualEditor.php
+++ extensions/VisualEditor/VisualEditor.php
@@ -1,5 +1,4 @@
 <?php
-
 // Wikia change begin, @author: Inez Korczyński
 require_once( __DIR__ . '/registration/Processor.php' );
 require_once( __DIR__ . '/registration/ExtensionProcessor.php' );
@@ -7,6 +6,7 @@ require_once( __DIR__ . '/registration/ExtensionRegistry.php' );
 
 ExtensionRegistry::getInstance()->queue( __DIR__ . '/extension.json' );
 
+$wgExtensionMessagesFiles['VisualEditor'] = __DIR__ . '/VisualEditor.i18n.php';
 // Keep i18n globals so mergeMessageFileList.php doesn't break
 $wgMessagesDirs['VisualEditor'] = array(
 	__DIR__ . '/lib/ve/i18n',
--- extensions/VisualEditor/wikia/VisualEditor.php
+++ extensions/VisualEditor/wikia/VisualEditor.php
@@ -1,4 +1,5 @@
 <?php
-
 ExtensionRegistry::getInstance()->queue( __DIR__ . '/extension.json' );
 ExtensionRegistry::getInstance()->loadFromQueue();
+
+$wgExtensionMessagesFiles['VisualEditorWikia'] = __DIR__ . '/VisualEditor.i18n.php';
