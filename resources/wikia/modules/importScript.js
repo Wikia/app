2cb00c2ba6e20c7e972bdc3d113dd3a56c5b8c82
Lukasz Konieczny <lukaszk@wikia-inc.com>
2015-09-25 09:10:56 +0000

    CE-2703: Move import function to separate amd module

---
+++ resources/wikia/modules/importScript.js
@@ -0,0 +1,49 @@
+define('wikia.importScript', ['wikia.importScriptHelper', 'wikia.window'], function(importScriptHelper, window) {
+	'use strict';
+
+	var importWikiaScriptPages;
+
+	/**
+	 * Imports script from provided JS page name
+	 * Page has to be in MediaWiki namespace and has .js extension
+	 *
+	 * Article name can point a page on other wikia (e.g. 'external:otherwikiadatabasename:Pagename.js')
+	 *
+	 * @param {array} articles Names of pages to import without namespace prefix
+	 */
+	importWikiaScriptPages = function(articles) {
+		var articlesToImport = [],
+			articlesFailed = [],
+			scriptName;
+
+		if (!$.isArray(articles)) {
+			articles = [articles];
+		}
+
+		for (var i = 0; i < articles.length; i++) {
+			if (!importScriptHelper.isJsPage(articles[i])) {
+				articlesFailed.push(namespacePrefix + articles[i]);
+				continue;
+			}
+			if (importScriptHelper.isLocal(articles[i])) {
+				articlesToImport.push(importScriptHelper.getNamespacePrefix() + articles[i]);
+			} else if (importScriptHelper.isExternal(articles[i])) {
+				scriptName = importScriptHelper.prepareExternalScript(articles[i]);
+				articlesToImport.push(scriptName);
+			} else {
+				articlesFailed.push(articles[i]);
+			}
+		}
+
+		window.importNotifications.importNotJsFailed(articlesFailed);
+
+		window.importArticles({
+			type: 'script',
+			articles: articlesToImport
+		});
+	};
+
+	return {
+		importWikiaScriptPages: importWikiaScriptPages
+	};
+});
---
+++ resources/wikia/modules/importScriptHelper.js
@@ -0,0 +1,64 @@
+define('wikia.importScriptHelper', function() {
+	'use strict';
+
+	var wikiaDomain = 'wikia.com',
+		namespacePrefix = 'MediaWiki:',
+		externals = {
+			'db': 'external',
+			'domain': 'url'
+		},
+		// Functions
+		isJsPage, isLocal, isExternal, isExternalDb, isExternalDomain,
+		prepareExternalScript, hasWikiaDomain,
+		getNamespacePrefix;
+
+	getNamespacePrefix = function() {
+		return namespacePrefix;
+	};
+
+	isJsPage = function(scriptName) {
+		return scriptName.substr(scriptName.length - 3) === '.js';
+	};
+
+	isLocal = function(scriptName) {
+		return scriptName.indexOf(':') === -1;
+	};
+
+	isExternal = function(scriptName) {
+		var externalParts = scriptName.split(':');
+
+		return scriptName.indexOf(':') !== -1 && externalParts.length === 3
+			&& ( isExternalDb(externalParts[0]) || isExternalDomain(externalParts[0], externalParts[1]) );
+	};
+
+	isExternalDb = function(prefix) {
+		return prefix === externals.db;
+	};
+
+	isExternalDomain = function(prefix, domain) {
+		return prefix === externals.domain && hasWikiaDomain(domain);
+	};
+
+	hasWikiaDomain = function(scriptName) {
+		if (scriptName.indexOf('.') !== -1) {
+			return scriptName.substr(scriptName.length - wikiaDomain.length) === wikiaDomain;
+		}
+		return true;
+	};
+
+	prepareExternalScript = function(scriptName) {
+		var externalParts = scriptName.split(':');
+
+		externalParts.splice(2, 0, namespacePrefix);
+
+		return externalParts.join(':');
+	};
+
+	return {
+		getNamespacePrefix: getNamespacePrefix,
+		isJsPage: isJsPage,
+		isLocal: isLocal,
+		isExternal: isExternal,
+		prepareExternalScript: prepareExternalScript
+	};
+});
