3efa750d7e8c18c332f7fa8f5c0129b786281687
Mari McMurtrie <mari@wikia-inc.com>
2015-09-25 14:36:56 -0700

    Added more comment for clarity

--- extensions/wikia/GlobalNavigation/scripts/GoogleSearchAPI.js
+++ extensions/wikia/GlobalNavigation/scripts/GoogleSearchAPI.js
@@ -1,6 +1,7 @@
 /**
  * We AB test for wikia search (both within and all wikias) in Oasis with Google custom search result.
- * window.wgGoogleSearchTest global variable is set to true for 50% of users who visit ja wikia.
+ * window.wgGoogleSearchTest: a global variable set true in Optimizely for 50% of users who visit ja wikia.
+ * window.wgGoogleSearchParam: Search ID set in Optimizely.
  */
 $(function () {
 	if ( window.wgGoogleSearchTest ) {
