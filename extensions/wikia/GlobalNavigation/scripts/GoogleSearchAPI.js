85932df20a11abfa7acac88815c6b0423c043b89
Mari McMurtrie <mari@wikia-inc.com>
2015-09-28 14:07:52 -0700

    INT-194: Use all JQeury. Wrote a function to get Google JS for search API.

--- extensions/wikia/GlobalNavigation/scripts/GoogleSearchAPI.js
+++ extensions/wikia/GlobalNavigation/scripts/GoogleSearchAPI.js
@@ -4,19 +4,12 @@
  * window.wgGoogleSearchParam: Search ID set in Optimizely.
  */
 $(function () {
+	'use strict';
 	if ( window.wgGoogleSearchTest ) {
-		var searchForm = document.getElementById('searchForm'),
-			searchId = window.wgGoogleSearchParam,
-			scriptElem1 = document.createElement('script'),
-			scriptElem2 = document.getElementsByTagName('script')[0];
+		var searchId = window.wgGoogleSearchParam;
+		$.loadGoogleSearchAPI(searchId);
 
-		scriptElem1.type = 'text/javascript';
-		scriptElem1.async = true;
-		scriptElem1.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-				   '//cse.google.com/cse.js?cx=' + searchId;
-		scriptElem2.parentNode.insertBefore(scriptElem1, scriptElem2);
-
-		searchForm.addEventListener('submit', function (evt) {
+		$('#searchForm').submit( function (evt) {
 			var contentLang = window.wgContentLanguage || 'en',
 				searchQuery = $('#searchInput').val(),
 				$googleInput = $('.gsc-input'),
