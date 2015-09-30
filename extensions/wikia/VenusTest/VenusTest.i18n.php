6dfcd3bf16bd4fa18fe8b744786f04f5bf336f48
V <bartosz.bentkowski@gmail.com>
2015-09-30 14:18:27 +0200

    XW-117 | Remove Venus* dirs

--- extensions/wikia/VenusTest/VenusTest.i18n.php
+++
@@ -1,8 +0,0 @@
-<?php
-$messages['en'] = [
-	'special-venustest-title' => 'Venus Skin Test',
-];
-
-$messages['qqq'] = [
-	'special-venustest-title' => 'Page title visible in the title bar of a browser or on the browser tab',
-];
