8829db29c1236a7d6a809649c4cd71ec6550afb7
Onei <mdowdell244@gmail.com>
2015-09-30 01:51:04 +0100

    minor whitespace fixes

--- skins/oasis/modules/templates/WikiHeader_Index.php
+++ skins/oasis/modules/templates/WikiHeader_Index.php
@@ -1,21 +1,21 @@
 <header id="WikiHeader" class="WikiHeader">
 	<?= $app->renderView( 'WikiHeader', 'Wordmark' ) ?>
-    <nav class="WikiNav">
-    	<? if ( $displayHeader ) { ?>
-    		<?= $seoTestOneH1 ? '<h2>' : '<h1>' ?>
+	<nav class="WikiNav">
+		<? if ( $displayHeader ) { ?>
+			<?= $seoTestOneH1 ? '<h2>' : '<h1>' ?>
 				<?= wfMessage( 'oasis-wiki-navigation', $wordmarkText )->escaped() ?>
 			<?= $seoTestOneH1 ? '</h2>' : '</h1>' ?>
-        <? } ?>
+	<? } ?>
 		<?= $app->renderView( 'WikiNavigation', 'Index' ) ?>
-    </nav>
-    <?php if ( $displayHeaderButtons ) { ?>
+	</nav>
+	<?php if ( $displayHeaderButtons ) { ?>
 		<div class="buttons">
 			<?= $app->renderView( 'ContributeMenu', 'Index' ) ?>
 		</div>
 	<?php } ?>
-    <div class="hiddenLinks">
+	<div class="hiddenLinks">
 		<?= Wikia::specialPageLink( 'Watchlist', 'watchlist', ['accesskey' => 'l'] ) ?>
 		<?= Wikia::specialPageLink( 'Random', 'randompage', ['accesskey' => 'x'] ) ?>
 		<?= Wikia::specialPageLink( 'RecentChanges', 'recentchanges', ['accesskey' => 'r'] ) ?>
-    </div>
+	</div>
 </header>
