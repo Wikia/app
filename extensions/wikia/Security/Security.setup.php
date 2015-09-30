47412dd9571375c2ea59cf889c2d756030366a1f
Maciej Brencz <macbre@wikia-inc.com>
2015-09-30 16:37:50 +0200

    Merge pull request #8645 from Wikia/PLATFORM-1540-assert-that-user-saveSettings-check-edit-token
    PLATFORM-1540 assert that User::saveSettings checks edit token

--- extensions/wikia/Security/Security.setup.php
+++ extensions/wikia/Security/Security.setup.php
@@ -11,10 +11,17 @@
 $wgAutoloadClasses['Wikia\\Security\\CSRFDetector'] = __DIR__ . '/classes/CSRFDetector.class.php';
 $wgAutoloadClasses['Wikia\\Security\\Exception'] = __DIR__ . '/classes/Exception.class.php';
 
-// PLATFORM-1540: detect revision inserts not guarded by user's edit token check
+// set per-request flags
 $wgHooks['UserMatchEditToken'][] = 'Wikia\\Security\\CSRFDetector::onUserMatchEditToken';
+$wgHooks['WebRequestWasPosted'][] = 'Wikia\\Security\\CSRFDetector::onRequestWasPosted';
+$wgHooks['WikiaRequestWasPosted'][] = 'Wikia\\Security\\CSRFDetector::onRequestWasPosted';
+
+// PLATFORM-1540: detect revision inserts not guarded by user's edit token check
 $wgHooks['RevisionInsertComplete'][] = 'Wikia\\Security\\CSRFDetector::onRevisionInsertComplete';
 
 // PLATFORM-1540: detect file uploads (local files and via URL) not guarded by user's edit token check
-$wgHooks['UploadFromUrlReallyFetchFile'][] = 'Wikia\\Security\\CSRFDetector::onUploadFromUrlReallyFetchFile';
 $wgHooks['UploadComplete'][] = 'Wikia\\Security\\CSRFDetector::onUploadComplete';
+$wgHooks['UploadFromUrlReallyFetchFile'][] = 'Wikia\\Security\\CSRFDetector::onUploadFromUrlReallyFetchFile';
+
+// PLATFORM-1540: detect user settings saves not guarded by user's edit token check
+$wgHooks['UserSaveSettings'][] = 'Wikia\\Security\\CSRFDetector::onUserSaveSettings';
