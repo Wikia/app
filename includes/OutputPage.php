2b1bdbad287168cc8ca16c6c5d834540d3ce98b0
V <bartosz.bentkowski@gmail.com>
2015-10-01 17:15:19 +0200

    XW-117 | Remove pushRLModulesToBottom

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -462,21 +462,6 @@ class OutputPage extends ContextSource {
 	 * @return Array of module names
 	 */
 	public function getModules( $filter = false, $position = null, $param = 'mModules' ) {
-		// Wikia change - begin - @author macbre
-		// Load all ResourceLoader modules at the bottom of the page
-		// when the skin has the flag set
-		$skin = $this->getSkin();
-		if ( $skin instanceof WikiaSkin && !empty( $skin->pushRLModulesToBottom ) ) {
-			// when asked for top modules return nothing
-			if ( $position === 'top' ) {
-				return [];
-			}
-
-			// otherwise, return all modules - null means no filtering below
-			$position = null;
-		}
-		// Wikia change - end
-
 		$modules = array_values( array_unique( $this->$param ) );
 		return $filter
 			? $this->filterModules( $modules, $position )
@@ -2897,13 +2882,6 @@ $templates
 	function getScriptsForBottomQueue( $inHead ) {
 		global $wgUseSiteJs, $wgAllowUserJs, $wgEnableContentReviewExt;
 
-		$asyncMWload = true;
-
-		$skin = $this->getSkin();
-		if ( $skin instanceof WikiaSkin && !empty( $skin->pushRLModulesToBottom ) ) {
-			$asyncMWload = false;
-		}
-
 		// Script and Messages "only" requests marked for bottom inclusion
 		// If we're in the <head>, use load() calls rather than <script src="..."> tags
 		// Messages should go first
@@ -2922,7 +2900,7 @@ $templates
 		if ( $modules ) {
 			$scripts .= Html::inlineScript(
 				ResourceLoader::makeLoaderConditionalScript(
-					Xml::encodeJsCall( 'mw.loader.load', array( $modules, null, $asyncMWload ) )
+					Xml::encodeJsCall( 'mw.loader.load', array( $modules, null, true ) )
 				)
 			);
 		}
