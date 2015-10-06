d3ae70728d505f8be9aa6fbce35db259132f5161
Piotr Gabryjeluk <rychu@wikia-inc.com>
2015-10-06 11:13:03 -0700

    SEO-12 Use em dash consistently as a SEO separator
    Some languages use em dash (instead of regular dash) to separate page
    title from wiki title. We're going to using em dash for those languages
    instead of regular dash for the " - Wikia" message as well.

--- languages/messages/wikia/MessagesKsh.php
+++ languages/messages/wikia/MessagesKsh.php
@@ -3,5 +3,6 @@
 $messages = array_merge( $messages, array(
 'copyrightpage' => 'w:Wikia:Licensing|Wikia:Licensing',
 'addnewtalksection-link' => 'Donn hee enne neue Afschnett opmaache.',
+'wikia-pagetitle' => '$1 — Wikia',
 ) );
 
