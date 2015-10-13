1b44feddc420dfa7f13ae002d4de50304e94abcc
nAndy <nandy@wikia-inc.com>
2015-10-13 18:36:38 +0200

    ADEN-2533 removed HTML from the messages

--- extensions/wikia/AdEngine/i18n/AdEngine.i18n.php
+++ extensions/wikia/AdEngine/i18n/AdEngine.i18n.php
@@ -3,10 +3,12 @@ $messages = [];
 
 $messages[ 'en' ] = [
 	'adengine-recovery-message-blocking-welcome' => 'Hey! It looks like you\'re using ad blocking software!',
-	'adengine-recovery-message-blocking-message' => 'Wikia runs ads so we can keep the lights on and so Wikia will always be free to use. We can bring you fun, free, fan-oriented content until my glorious return to Earh if you <strong>add us to your adblock whitelist</strong>. <a class="action-accept">Click</a> my face to refresh after you\'re done!',
+	'adengine-recovery-message-blocking-click' => 'Click',
+	'adengine-recovery-message-blocking-message-a' => 'Wikia runs ads so we can keep the lights on and so Wikia will always be free to use. We can bring you fun, free, fan-oriented content until my glorious return to Earh if you \'\'\'add us to your adblock whitelist\'\'\'. $1 my face to refresh after you\'re done!',
 ];
 
 $messages[ 'qqq' ] = [
-	'adengine-recovery-message-blocking-welcome' => 'A bold title in comic bubble next to an image welcoming a user and saying our system detected she uses ad blocking software',
-	'adengine-recovery-message-blocking-message' => 'More in-depth explanation why using ad blocking software is not good for Wikia. This description is placed just under the welcome message in the comic bubble.',
+	'adengine-recovery-message-blocking-welcome' => 'A bold title in comic bubble next to an image welcoming a user and saying our system detected she uses ad blocking software.',
+	'adengine-recovery-message-blocking-click' => 'A label of link in the last sentence in adengine-recovery-message-blocking-message-a message. This message is used as a parameter in the other, mentioned message.',
+	'adengine-recovery-message-blocking-message-a' => 'More in-depth explanation why using ad blocking software is not good for Wikia. This description is placed just under the welcome message in the comic bubble. $1 is a translated word in form of a link.',
 ];
