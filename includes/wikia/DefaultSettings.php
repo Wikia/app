b9f18ed8c00102f61f62f5b561aa76e866504fde
nmonterroso <nelson@wikia-inc.com>
2015-10-13 10:47:46 -0700

    remove $wgGlobalUserOptions

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -1015,11 +1015,6 @@ $wgAssetsManagerQuery = '/__am/%4$d/%1$s/%3$s/%2$s';
 //$wgAssetsManagerQuery = '/index.php?action=ajax&rs=AssetsManagerEntryPoint&__am&type=%1$s&cb=%4$d&params=%3$s&oid=%2$s';
 
 /**
- * global user_options
- */
-$wgGlobalUserProperties = [];
-
-/**
  * debug level for memcached
  */
 $wgMemCachedDebugLevel = 1;
