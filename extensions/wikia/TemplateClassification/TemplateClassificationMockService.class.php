525a8e5c219ab7b57284779cfd3e84ebf4067d05
adamkarminski <adamkarminski@gmail.com>
2015-10-16 09:21:03 +0200

    CE-2784 Use cache to standarize an output from a service for a given article

--- extensions/wikia/TemplateClassification/TemplateClassificationMockService.class.php
+++ extensions/wikia/TemplateClassification/TemplateClassificationMockService.class.php
@@ -6,10 +6,19 @@
 class TemplateClassificationMockService extends WikiaService {
 
 	/**
+	 * @param int $articleId
 	 * @return string
 	 */
-	public function getTemplateType() {
-		$types = TemplateClassification::$templateTypes;
-		return $types[array_rand( $types )];
+	public function getTemplateType( $articleId ) {
+		/* Use cache to have consistent results for same pages due to randomization */
+		$type = WikiaDataAccess::cache(
+			wfMemcKey( 'template-classification-type-for-page', $articleId ),
+			WikiaResponse::CACHE_STANDARD,
+			function () {
+				$types = TemplateClassification::$templateTypes;
+				return $types[array_rand( $types )];
+			}
+		);
+		return $type;
 	}
 }
