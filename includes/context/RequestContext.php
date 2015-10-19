072bb795664f9fb51f1ff2cef6568c5656c03aab
Damon Snyder <drsnyder@gmail.com>
2015-10-19 15:49:19 -0700

    Update RequestContext.php
    Always sample the authentication method unless the user is null or not logged in.

--- includes/context/RequestContext.php
+++ includes/context/RequestContext.php
@@ -235,7 +235,7 @@ class RequestContext implements IContextSource {
 	 * @param $authSource array
 	 */
 	private function logAuthenticationMethod($user, $authSource) {
-		if ( count( $authSource ) <= 1 || $user === null || !$user->isLoggedIn() ) {
+		if ( $user === null || !$user->isLoggedIn() ) {
 			return;
 		}
 
