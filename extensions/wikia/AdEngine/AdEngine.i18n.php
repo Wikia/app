bfe7435044950e6c179d2149d6fc08ccd16913de
nAndy <nandy@wikia-inc.com>
2015-10-20 15:32:34 +0200

    ADEN-2546 updated messages

--- extensions/wikia/AdEngine/AdEngine.i18n.php
+++ extensions/wikia/AdEngine/AdEngine.i18n.php
@@ -14,6 +14,7 @@ $messages['en'] = array(
 	'adengine-recovery-message-blocking-welcome' => 'Hey! It looks like you\'re using ad blocking software!',
 	'adengine-recovery-message-blocking-click' => 'Click',
 	'adengine-recovery-message-blocking-message-a' => 'Wikia runs ads so we can keep the lights on and so Wikia will always be free to use. We can bring you fun, free, fan-oriented content until my glorious return to Earth if you add us to your adblock whitelist. $1 my face to refresh after you\'re done!',
+	'adengine-recovery-message-blocking-message-b' => 'Wikia runs ads so we can keep the lights on and so Wikia will always be free to use. We can bring you fun, free, fan-oriented content until my glorious return to Earth if you disable your adblock software while on Wikia. $1 my face to refresh after you\'re done!',
 );
 
 /** Message documentation (Message documentation)
@@ -29,8 +30,10 @@ $messages['qqq'] = array(
 	'adengine-desc' => '{{desc}}',
 	'adengine-recovery-message-blocking-welcome' => 'A bold title in comic bubble next to an image welcoming a user and saying our system detected she uses ad blocking software.',
 	'adengine-recovery-message-blocking-click' => 'A label of link in the last sentence in adengine-recovery-message-blocking-message-a message. This message is used as a parameter in the other, mentioned message.',
-	'adengine-recovery-message-blocking-message-a' => 'More in-depth explanation why using ad blocking software is not good for Wikia. This description is placed just under the welcome message in the comic bubble. Parameters:
+	'adengine-recovery-message-blocking-message-a' => 'More in-depth explanation why using ad blocking software is not good for Wikia. This description is placed just under the welcome message in the comic bubble. And for now it is in two similar versions. Parameters:
 * $1 is a translated call-to-action word which will be linked and clickable.',
+	'adengine-recovery-message-blocking-message-b' => 'More in-depth explanation why using ad blocking software is not good for Wikia. This description is placed just under the welcome message in the comic bubble. And for now it is in two similar versions. Parameters:
+* $1 is a translated call-to-action word which will be linked and clickable.'
 );
 
 /** Arabic (العربية)
@@ -177,6 +180,7 @@ $messages['de'] = array(
 	'adengine-recovery-message-blocking-welcome' => 'Hallo! Es sieht so aus als würdest du eine Werbeblocker-Software verwenden!',
 	'adengine-recovery-message-blocking-click' => 'klicke',
 	'adengine-recovery-message-blocking-message-a' => 'Bei Wikia wird Werbung angezeigt, damit wir auf Sendung bleiben können und Wikia auch weiterhin kostenlos angeboten werden kann. Bis zu meiner glorreichen Rückkehr zur Erde findest du bei uns kostenlose, auf unsere Fans zugeschnittene Inhalte, die Spaß machen. Dazu musst du uns nur auf die Whitelist deines Werbeblockers setzen. Wenn du damit fertig bist, $1 zur Aktualisierung einfach auf mein Gesicht!',
+	'adengine-recovery-message-blocking-message-b' => 'Bei Wikia wird Werbung angezeigt, damit wir auf Sendung bleiben können und Wikia auch weiterhin kostenlos angeboten werden kann. Bis zu meiner glorreichen Rückkehr zur Erde findest du bei uns kostenlose, auf unsere Fans zugeschnittene Inhalte, die Spaß machen. Dazu musst du nur deinen Werbeblocker deaktivieren, solange du auf Wikia unterwegs bist. Wenn du damit fertig bist, $1 zur Aktualisierung einfach auf mein Gesicht!'
 );
 
 /** Greek (Ελληνικά)
@@ -203,6 +207,7 @@ $messages['es'] = array(
 	'adengine-recovery-message-blocking-welcome' => '¡Hola!¡Parece que estás utilizando un software para bloquear publicidad!',
 	'adengine-recovery-message-blocking-click' => 'clic',
 	'adengine-recovery-message-blocking-message-a' => 'Wikia muestra publicidad para que podamos mantener las luces encendidas y que Wikia siempre sea libre de pago. Podemos brindar contenido divertido, libre y orientado a los fans hasta mi glorioso retorno a la tierra si es que nos añades a tu lista blanca de bloqueadores de publicidad. ¡Haz $1 en mi cara para refrescar después de que hayas terminado!',
+	'adengine-recovery-message-blocking-message-b' => 'Wikia muestra publicidad para que podamos mantener las luces encendidas y que Wikia siempre sea libre de pago. Podemos brindar contenido divertido, libre y orientado a los fans hasta mi glorioso retorno a la tierra si es que deshabilitas tu software para bloquear publicidad mientras te encuentres en Wikia. ¡Haz $1 en mi cara para refrescar después de que hayas terminado!',
 );
 
 /** Estonian (eesti)
@@ -393,6 +398,7 @@ $messages['ja'] = array(
 	'adengine-recovery-message-blocking-welcome' => '広告ブロック・ソフトウェアをお使いのようです。',
 	'adengine-recovery-message-blocking-click' => '手順が完了したら',
 	'adengine-recovery-message-blocking-message-a' => 'ウィキアでは、ウィキアの運営を持続し、またユーザーの皆さんにいつでも無料でウィキアをご利用いただけるようにするため、広告を掲載しています。広告ブロックのホワイトリストにウィキアを追加していただくと、ファン向けの楽しい無料コンテンツをご提供できます。$1、私の顔をクリックして更新してくださいね！',
+	'adengine-recovery-message-blocking-message-b' => 'ウィキアでは、ウィキアの運営を持続し、またユーザーの皆さんにいつでも無料でウィキアをご利用いただけるようにするため、広告を掲載しています。ウィキアのご利用時は広告ブロック・ソフトウェアを無効にしていただくと、ファン向けの楽しい無料コンテンツをご提供できます。$1、私の顔をクリックして更新してくださいね！',
 );
 
 /** Georgian (ქართული)
@@ -626,7 +632,8 @@ $messages['pl'] = array(
 	'adengine-desc' => 'Silnik reklam Wikii',
 	'adengine-recovery-message-blocking-welcome' => 'Cześć! Wygląda na to, że korzystasz z oprogramowania do blokowania reklam!',
 	'adengine-recovery-message-blocking-click' => 'Kliknij',
-	'adengine-recovery-message-blocking-message-a' => 'Wikia wyświetla reklamy, aby móc działać, dzięki czemu Wikia zawsze pozostanie darmowa. Możemy być Twoim źródłem zabawnych, bezpłatnych treści skierowanych do fanów do czasu mojego chwalebnego powrotu na ziemię, pod warunkiem że \'\'\'dodasz nas do listy wyjątków swojego programu do blokowania reklam\'\'\'. $1 na moją twarz, aby odświeżyć stronę, kiedy będziesz gotowy!',
+	'adengine-recovery-message-blocking-message-a' => 'Wikia wyświetla reklamy, aby móc działać, dzięki czemu Wikia zawsze pozostanie darmowa. Możemy być Twoim źródłem zabawnych, bezpłatnych treści skierowanych do fanów do czasu mojego chwalebnego powrotu na ziemię, pod warunkiem że dodasz nas do listy wyjątków swojego programu do blokowania reklam. $1 na moją twarz, aby odświeżyć stronę, kiedy będziesz gotowy!',
+	'adengine-recovery-message-blocking-message-b' => 'Wikia wyświetla reklamy, aby móc działać, dzięki czemu Wikia zawsze pozostanie darmowa. Możemy być Twoim źródłem zabawnych, bezpłatnych treści skierowanych do fanów do czasu mojego chwalebnego powrotu na ziemię, pod warunkiem że wyłączysz program do blokowania reklam na czas przeglądania Wikii. $1 na moją twarz, aby odświeżyć stronę, kiedy będziesz gotowy!',
 );
 
 /** Pashto (پښتو)
