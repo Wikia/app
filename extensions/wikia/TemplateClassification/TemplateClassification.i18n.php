285b770f26f1ec3eab6d166f38f16479995d8a4e
tor <tor@wikia-inc.com>
2015-10-21 14:23:13 +0000

    added documentation for i18n

--- extensions/wikia/TemplateClassification/TemplateClassification.i18n.php
+++ extensions/wikia/TemplateClassification/TemplateClassification.i18n.php
@@ -33,8 +33,8 @@ $messages['en'] = [
         'template-classification-type-nonarticle' => 'Non-article',
         'template-classification-description-nonarticle' => 'This template is not used on article pages.',
 
-        'template-classification-type-dontknow' => 'Unknown',
-        'template-classification-description-dontknow' => "I don't know.",
+        'template-classification-type-unknown' => 'Unknown',
+        'template-classification-description-unknown' => "I don't know.",
 
 	'template-classification-type-header' => 'Type of the template',
 	'template-classification-edit-modal-save-button-text' => 'Save',
@@ -49,10 +49,33 @@ $messages['qqq'] = [
 	'template-classification-type-infobox' => 'Name of an infobox type',
 	'template-classification-type-navbox' => 'Name of a navbox type',
 	'template-classification-type-quote' => 'Name of a quote type',
+	'template-classification-type-data' => 'Name of a data type',
+	'template-classification-type-design' => 'Name of a design type',
+	'template-classification-type-media' => 'Name of a media type',
+	'template-classification-type-navigation' => 'Name of a navigation type',
+	'template-classification-type-nonarticle' => 'Name of a non-article type',
+	'template-classification-type-notice' => 'Name of a notice type',
+	'template-classification-type-reference' => 'Name of a reference type',
+	'template-classification-type-unknown' => 'Name of an unknown template type',
+
+        'template-classification-description-infobox' => 'Description (additional label) for the infobox type. This is shown in dialog box under the type name to explain what each type is.',
+        'template-classification-description-navbox' => 'Description (additional label) for the navbox type. This is shown in dialog box under the type name to explain what each type is.',
+        'template-classification-description-quote' => 'Description (additional label) for the quote type. This is shown in dialog box under the type name to explain what each type is.',
+        'template-classification-description-data' => 'Description (additional label) for the data type. This is shown in dialog box under the type name to explain what each type is.',
+        'template-classification-description-design' => 'Description (additional label) for the design type. This is shown in dialog box under the type name to explain what each type is.',
+        'template-classification-description-navigation' => 'Description (additional label) for the navigation type. This is shown in dialog box under the type name to explain what each type is.',
+        'template-classification-description-nonarticle' => 'Description (additional label) for the non-article type. This is shown in dialog box under the type name to explain what each type is.',
+        'template-classification-description-unknown' => 'Description (additional label) for the unknown template type. This is shown in dialog box under the type name to explain what each type is.',
+	'template-classification-description-media' => 'Description (additional label) for the media template type. This is shown in dialog box under the type name to explain what each type is.',
+	'template-classification-description-notice' => 'Description (additional label) for the notice template type. This is shown in dialog box under the type name to explain what each type is.',
+	'template-classification-description-reference'  => 'Description (additional label) for the reference template type. This is shown in dialog box under the type name to explain what each type is.',
+
 	'template-classification-type-header' => 'A header of all boxes that display a type of a template.',
 	'template-classification-edit-modal-save-button-text' => 'Text on save button on modal for editing template type',
 	'template-classification-edit-modal-cancel-button-text' => 'Text on cancel button for editing template type',
 	'template-classification-edit-modal-title' => 'Title of modal for editing template type',
+
+	'template-classification-indicator' => 'Shown in page subheader and near classification dialog entry points to indicate which type the template is currently classified as. Takes one of the template-classification-type-* messages as a parameter.',
 ];
 
 $messages['pl'] = [
