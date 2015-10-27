7f0025670849063a78a5d09bda5db00fda8721fe
Kamil Koterba <kamil@wikia-inc.com>
2015-10-27 14:25:27 +0100

    CE-2894 Add support for modal force on publish in ACE editor

--- extensions/wikia/EditPageLayout/js/plugins/TemplateClassificationEditorPlugin.js
+++ extensions/wikia/EditPageLayout/js/plugins/TemplateClassificationEditorPlugin.js
@@ -1,36 +1,30 @@
 /**
- * Editor plugin to force template classification type choose on publish if doesn't exist
+ * Editor plugin to force template classification type choose on publish if doesn't exist and break article submit
  *
  * @author Kamil Koterba <kamil@wikia-inc.com>
  */
 (function (window, $) {
 		'use strict';
 
-		var WE = window.WikiaEditor = window.WikiaEditor || (new window.Observable());
+		var WE = window.WikiaEditor = window.WikiaEditor || (new window.Observable()),
+			templateClassificationForceModal;
 
 		WE.plugins.templateclassificationeditorplugin = $.createClass(WE.plugin, {
 
 			init: function () {
 				this.editor.on('save', this.proxy(this.forceType));
+				require(['TemplateClassificationModalForce'], function forceTemplateClassificationModal(tcForce) {
+					templateClassificationForceModal = tcForce;
+				});
 			},
 
 			forceType: function () {
-				var	$typeField = $('#editform').find('input[name=templateClassificationType]');
-
-				if ($typeField.length > 0) {
-					if ($typeField.val() !== '') {
-						// Type defined
-						return true;
-					}
+				if (templateClassificationForceModal.forceType() === true) {
+					// Break article submit
+					return false;
+				} else {
+					return true;
 				}
-
-				// Type not defined force modal
-				require(['TemplateClassificationInEdit'], function forceTemplateClassificationModal(tc) {
-					tc.open();
-				});
-
-				// Break article submit
-				return false;
 			}
 
 		});
