66645815e9218de35c6090dd9d4a0703abd3b83f
adamkarminski <adamkarminski@gmail.com>
2015-10-27 09:38:43 +0100

    CE-2834 Modify Logger to only accept the ApiException objects

--- extensions/wikia/TemplateClassification/Logger.class.php
+++ extensions/wikia/TemplateClassification/Logger.class.php
@@ -2,6 +2,7 @@
 
 namespace Wikia\TemplateClassification;
 
+use Swagger\Client\ApiException;
 use Wikia\Logger\Loggable;
 
 class Logger {
@@ -11,10 +12,10 @@ class Logger {
 	const EXCEPTION_MESSAGE = 'TemplateClassification client exception';
 
 	/**
-	 * Log an exception on
-	 * @param \Exception $e
+	 * Log exceptions thrown by TemplateClassification service
+	 * @param  $e
 	 */
-	public function exception( \Exception $e ) {
+	public function exception( ApiException $e ) {
 		$context = [
 			'tcExcptnMessage' => $e->getMessage(),
 			'tcExcptnCode' => $e->getCode(),
