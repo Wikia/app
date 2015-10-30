1c44c6a8d74d8f9b04c2c4bb3f088cb9446aacbc
Damon Snyder <damon@wikia-inc.com>
2015-10-30 23:37:15 +0000

    Fix some omissions from the initial commit
    - add the response to the call to set the auth cookie
    - add the clear auth cookie method to the CookieHelper interface

--- lib/Wikia/src/Service/User/Auth/CookieHelper.php
+++ lib/Wikia/src/Service/User/Auth/CookieHelper.php
@@ -7,4 +7,5 @@ use Wikia\HTTP\Response;
 interface CookieHelper {
 	public function setAuthenticationCookieWithUserId( $userId, Response $response );
 	public function setAuthenticationCookieWithToken( $accessToken, Response $response );
+	public function clearAuthenticationCookie( Response $response );
 }
