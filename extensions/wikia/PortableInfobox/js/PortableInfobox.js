208860870a8120e456796531f338b8fd7dd39518
Inez Korczynski <inez@wikia-inc.com>
2015-11-02 22:06:31 +0000

    Fix the typo

--- extensions/wikia/PortableInfobox/js/PortableInfobox.js
+++ extensions/wikia/PortableInfobox/js/PortableInfobox.js
@@ -9,13 +9,13 @@
 			$('.pi-image-collection-tabs').on('click', 'li', function() {
 				var $tab = $(this),
 					$tabs = $tab.parent().children(),
-					$contens = $tab.parent().parent().children(),
+					$contents = $tab.parent().parent().children(),
 					newTabIndex = $tab.index(),
 					oldTabIndex = $tab.parent().find('.current').index();
 				$tabs.eq(oldTabIndex).removeClass('current');
 				$tabs.eq(newTabIndex).addClass('current');
-				$contens.eq(oldTabIndex+1).removeClass('current');
-				$contens.eq(newTabIndex+1).addClass('current');
+				$contents.eq(oldTabIndex+1).removeClass('current');
+				$contents.eq(newTabIndex+1).addClass('current');
 			});
 		}
 	};
