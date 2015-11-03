5b3d5bef0eb5397be504d2c1ff39035eeafc6390
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2015-11-03 12:57:42 -0800

    ADEN-2578 SourcePoint old vars clean up

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -1513,14 +1513,6 @@ $wgAdDriverTurtleCountries = null;
 $wgAdDriverOpenXCountries = null;
 
 /**
- * @name $wgAdDriverSourcePointCountries
- * @TODO ADEN-2578 - cleanup
- * List of countries to call ads through SourcePoint
- * ONLY UPDATE THROUGH WIKI FACTORY ON COMMUNITY - it's an instant global.
- */
-$wgAdDriverSourcePointCountries = null;
-
-/**
  * @name $wgAdDriverSourcePointDetectionCountries
  * List of countries to call SourcePoint detection scripts
  * ONLY UPDATE THROUGH WIKI FACTORY ON COMMUNITY - it's an instant global.
