701faf40984f811e706032f8f47a3f486f4e4017
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2015-11-04 10:58:52 -0800

    ADEN-2484 Create taboola slot module

---
+++ extensions/wikia/AdEngine/js/slot/taboola.js
@@ -0,0 +1,36 @@
+/*global define*/
+define('ext.wikia.adEngine.slot.inContentPlayer', [
+	'ext.wikia.adEngine.adContext',
+	'ext.wikia.adEngine.recovery.helper',
+	'wikia.log',
+	'wikia.window'
+], function (adContext, recoveryHelper, doc, log, win) {
+	'use strict';
+
+	var logGroup = 'ext.wikia.adEngine.slot.taboola',
+		context = adContext.getContext(),
+		slots = [
+			'NATIVE_TABOOLA_ARTICLE',
+			'NATIVE_TABOOLA_RAIL'
+		];
+
+	function init() {
+		if (!context.providers.taboola) {
+			log(['init', 'Taboola provider is disabled'], 'debug', logGroup);
+			return;
+		}
+
+		recoveryHelper.addOnBlockingCallback(function () {
+			slots.forEach(function (slotName) {
+				if (doc.getElementById(slotName)) {
+					log(['init', 'Push slot', slotName], 'debug', logGroup);
+					win.adslots2.push(slotName);
+				}
+			});
+		});
+	}
+
+	return {
+		init: init
+	};
+});
