539cc577ec951835889ff91976c00d82a4a4a02e
Rafal Leszczynski <rafal@wikia-inc.com>
2015-11-04 17:54:42 +0100

    DAT-3456 add tests

---
+++ includes/wikia/parser/tests/TemplateTypesParserTest.php
@@ -0,0 +1,69 @@
+<?php
+
+class TemplateTypesParserTest extends WikiaBaseTest
+{
+	const TEST_TEMPLATE_TEXT = 'test-template-test';
+
+	/**
+	 * @param $enableTemplateTypesParsing
+	 * @param $wgArticleAsJson
+	 *
+	 * @dataProvider shouldNotChangeTemplateParsingDataProvider
+	 */
+	public function testShouldNotChangeTemplateParsing( $enableTemplateTypesParsing, $wgArticleAsJson )
+	{
+		$text = self::TEST_TEMPLATE_TEXT;
+		$title = $this->getMock( 'Title' );
+
+		$this->mockClassWithMethods(
+			'ExternalTemplateTypesProvider',
+			[ 'getTemplateTypeFromTitle' => '' ]
+		);
+
+		$this->mockGlobalVariable( 'wgCityId', '12345' );
+		$this->mockGlobalVariable( 'wgEnableTemplateTypesParsing', $enableTemplateTypesParsing );
+		$this->mockGlobalVariable( 'wgArticleAsJson', $wgArticleAsJson );
+
+
+		TemplateTypesParser::onFetchTemplateAndTitle( $text, $title );
+
+		$this->assertEquals( $text, self::TEST_TEMPLATE_TEXT );
+	}
+
+	public function shouldNotChangeTemplateParsingDataProvider()
+	{
+		return [
+			[
+				false,
+				false
+			],
+			[
+				true,
+				false
+			],
+			[
+				false,
+				true
+			]
+		];
+	}
+
+	public function testShouldRemoveNavboxTemplateText()
+	{
+		$text = self::TEST_TEMPLATE_TEXT;
+		$title = $this->getMock( 'Title' );
+
+		$this->mockClassWithMethods(
+			'ExternalTemplateTypesProvider',
+			[ 'getTemplateTypeFromTitle' => 'navbox' ]
+		);
+
+		$this->mockGlobalVariable( 'wgCityId', '12345' );
+		$this->mockGlobalVariable( 'wgEnableTemplateTypesParsing', true );
+		$this->mockGlobalVariable( 'wgArticleAsJson', true );
+
+		TemplateTypesParser::onFetchTemplateAndTitle( $text, $title );
+
+		$this->assertEquals( $text, '' );
+	}
+}
