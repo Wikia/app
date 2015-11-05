9be86f039b413e8dbc6ac3f3b6b3f7bd805a08af
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-11-05 21:09:21 +0100

    move to loader.js in lyricsbox.js

--- extensions/3rdparty/LyricWiki/LyricFind/js/lyricbox.js
+++ extensions/3rdparty/LyricWiki/LyricFind/js/lyricbox.js
@@ -1,24 +1,28 @@
-require(['jquery'], function($) {
+(function () {
+	'use strict';
+
+	var $ = require('jquery');
+
 	function preventHighlighting(element){
-		if (typeof element.onselectstart !== "undefined") { // IE
-			element.onselectstart=function() {
+		if (typeof element.onselectstart !== 'undefined') { // IE
+			element.onselectstart = function () {
 				return false;
 			};
-		} else if (typeof element.style.MozUserSelect !== "undefined") { // Moz-based (FireFox, etc.)
-			element.style.MozUserSelect="none";
+		} else if (typeof element.style.MozUserSelect !== 'undefined') { // Moz-based (FireFox, etc.)
+			element.style.MozUserSelect = 'none';
 		} else {
-			element.onmousedown=function() {
+			element.onmousedown = function () {
 				return false;
 			};
 		}
-		element.style.cursor = "default";
+		element.style.cursor = 'default';
 	}
 
-	$('.lyricbox').each(function() {
+	$('.lyricbox').each(function () {
 		preventHighlighting(this);
 	});
 
 	$('body').bind('contextmenu', function(ev) {
 		ev.preventDefault();
 	});
-});
+})();
