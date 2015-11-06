3beef071c3ad350d4921651c4511d836eca79946
Warkot <mateusz.warkocki@wikia-inc.com>
2015-11-06 15:31:39 +0100

    XW-515 | move to loader.js in WikiaMaps.wikiamobile.js

--- extensions/wikia/WikiaMaps/js/WikiaMaps.wikiamobile.js
+++ extensions/wikia/WikiaMaps/js/WikiaMaps.wikiamobile.js
@@ -1,19 +1,24 @@
-require([
-	'wikia.window',
-	'wikia.maps.pontoBridge'
-	//FIXME: turning off ads on map because of JS errors - should be fixed in ADEN-1784
-	//require.optional('ext.wikia.adEngine.slot.interactiveMaps')
-], function (w, pontoBridge, mapAds) {
+(function () {
 	'use strict';
 
-	var iframe =  w.document.getElementsByName('wikia-interactive-map')[0],
+	var w = require('wikia.window'),
+		pontoBridge = require('wikia.maps.pontoBridge'),
+		mapAds,
+		iframe = w.document.getElementsByName('wikia-interactive-map')[0],
 		adContainer = w.document.querySelector('.wikia-ad-interactive-map');
 
 	if (iframe) {
 		pontoBridge.init(iframe);
 	}
 
+	try {
+		//FIXME: turning off ads on map because of JS errors - should be fixed in ADEN-1784
+		//mapAds = require('ext.wikia.adEngine.slot.interactiveMaps');
+	} catch (exception) {
+		mapAds = null;
+	}
+
 	if (mapAds) {
 		mapAds.initSlot(adContainer);
 	}
-});
+})();
