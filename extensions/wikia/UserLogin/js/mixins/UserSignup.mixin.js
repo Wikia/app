fef21021082f32fc46317c523068c852c5230643
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-11-06 16:03:48 +0100

    move to loader.js in UserSignup.mixin.js

--- extensions/wikia/UserLogin/js/mixins/UserSignup.mixin.js
+++ extensions/wikia/UserLogin/js/mixins/UserSignup.mixin.js
@@ -1,23 +1,22 @@
 (function () {
 	'use strict';
 
-	var UserSignupMixin = function () {
-		/**
-		 * Handle marketing email opt-in for different locales
-		 */
-		this.initOptIn =  function (wikiaForm) {
-			require(['usersignup.marketingOptIn'], function (optIn) {
+	var optIn = require('usersignup.marketingOptIn'),
+		UserSignupMixin = function () {
+			/**
+			 * Handle marketing email opt-in for different locales
+			 */
+			this.initOptIn = function (wikiaForm) {
 				optIn.init(wikiaForm);
-			});
+			};
+			/**
+			 * Send country code upon signup
+			 */
+			this.setCountryValue = function (wikiaForm) {
+				var country = Wikia.geo.getCountryCode();
+				wikiaForm.inputs.wpRegistrationCountry.val(country);
+			};
 		};
-		/**
-		 * Send country code upon signup
-		 */
-		this.setCountryValue = function (wikiaForm) {
-			var country = Wikia.geo.getCountryCode();
-			wikiaForm.inputs.wpRegistrationCountry.val(country);
-		};
-	};
 
 	window.UserSignupMixin = UserSignupMixin;
 })();
