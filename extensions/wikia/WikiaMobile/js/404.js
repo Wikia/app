bd451b0980b27ade5534d93096ce9a1afc80a105
Warkot <mateusz.warkocki@wikia-inc.com>
2015-11-06 13:04:55 +0100

    XW-515 | move to loader.js in WikiaMobile/js/404.js

--- extensions/wikia/WikiaMobile/js/404.js
+++ extensions/wikia/WikiaMobile/js/404.js
@@ -1,15 +1,14 @@
-$(function(){
-	var link = document.getElementById('wk404');
+$(function () {
+	var link = document.getElementById('wk404'),
+		track = require('track');
 
 	if (link) {
-		require(['track'], function (track) {
-			link.addEventListener('click', function(ev) {
-				track.event('error-page', track.CLICK, {
+		link.addEventListener('click', function (ev) {
+			track.event('error-page', track.CLICK, {
 					label: 'random-image',
 					href: this.href
 				},
 				ev);
-			});
 		});
 	}
 });
