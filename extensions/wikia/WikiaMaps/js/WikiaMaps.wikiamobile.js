20f9ddfd2bf04d0b84bc87278ba0b79d6bf239cb
Warkot <mateusz.warkocki@wikia-inc.com>
2015-11-06 15:46:32 +0100

    XW-515 | move to loader.js in WikiaMaps.wikiamobile.js (try/catch moved to top)

--- extensions/wikia/WikiaMaps/js/WikiaMaps.wikiamobile.js
+++ extensions/wikia/WikiaMaps/js/WikiaMaps.wikiamobile.js
@@ -7,15 +7,15 @@
 		iframe = w.document.getElementsByName('wikia-interactive-map')[0],
 		adContainer = w.document.querySelector('.wikia-ad-interactive-map');
 
-	if (iframe) {
-		pontoBridge.init(iframe);
-	}
-
 	try {
 		//FIXME: turning off ads on map because of JS errors - should be fixed in ADEN-1784
 		//mapAds = require('ext.wikia.adEngine.slot.interactiveMaps');
 	} catch (exception) {
 		mapAds = null;
+	}
+
+	if (iframe) {
+		pontoBridge.init(iframe);
 	}
 
 	if (mapAds) {
