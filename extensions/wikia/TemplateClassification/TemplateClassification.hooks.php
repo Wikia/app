6070c0c976698489d584d707011ae670f2466ad4
adamkarminski <adamkarminski@gmail.com>
2015-11-10 16:51:24 +0100

    CE-2955 Display TC entry point in a unified way

--- extensions/wikia/TemplateClassification/TemplateClassification.hooks.php
+++ extensions/wikia/TemplateClassification/TemplateClassification.hooks.php
@@ -18,8 +18,8 @@ class Hooks {
 		\Hooks::register( 'PageHeaderPageTypePrepared', [ $hooks, 'onPageHeaderPageTypePrepared' ] );
 		\Hooks::register( 'QueryPageUseResultsBeforeRecache', [ $hooks, 'onQueryPageUseResultsBeforeRecache' ] );
 		/* Edit page hooks */
-		\Hooks::register( 'EditPage::showEditForm:fields', [ $hooks, 'onEditPageShowEditFormFields' ] );
 		\Hooks::register( 'ArticleSaveComplete', [ $hooks, 'onArticleSaveComplete' ] );
+		\Hooks::register( 'EditPage::showEditForm:fields', [ $hooks, 'onEditPageShowEditFormFields' ] );
 		\Hooks::register( 'EditPageLayoutExecute', [ $hooks, 'onEditPageLayoutExecute' ] );
 		\Hooks::register( 'EditPageMakeGlobalVariablesScript', [ $hooks, 'onEditPageMakeGlobalVariablesScript' ] );
 	}
@@ -175,8 +175,8 @@ class Hooks {
 		$user = $editPage->getContext()->getUser();
 		$title = $editPage->getContext()->getTitle();
 		if ( ( new Permissions() )->shouldDisplayEntryPoint( $user, $title ) ) {
-			$editPage->addExtraPageControlsHtml(
-				( new View )->renderEditPageEntryPoint( $wgCityId, $title, $user )
+			$editPage->addExtraHeaderHtml(
+				( new View )->renderTemplateType( $wgCityId, $title, $user )
 			);
 		}
 		return true;
