ba8d913d59329885cbd6c5b634eed70c8ba3754e
dianafa <diana.falkowska@gmail.com>
2015-11-23 17:41:45 +0100

    add ContextLinkTemplateTest

--- includes/wikia/parser/templatetypes/tests/TemplateTypesParserTest.php
+++ includes/wikia/parser/templatetypes/tests/TemplateTypesParserTest.php
@@ -101,46 +101,4 @@ class TemplateTypesParserTest extends WikiaBaseTest {
 			]
 		];
 	}
-
-	/**
-	 * @param $contextLinkWikitext
-	 * @param $expectedTemplateWikiext
-	 *
-	 * @dataProvider sanitizeContextLinkWikitextDataProvider
-	 */
-	public function testSanitizeContextLinkWikitext( $contextLinkWikitext, $expectedTemplateWikiext ) {
-		$sanitizedTemplateWikiext = TemplateTypesParser::sanitizeContextLinkWikitext( $contextLinkWikitext );
-
-		$this->assertEquals( $sanitizedTemplateWikiext, $expectedTemplateWikiext );
-	}
-
-	public function sanitizeContextLinkWikitextDataProvider() {
-		return [
-			[
-				'[[:Disciplinary hearing of Harry Potter|Disciplinary hearing of Harry Potter]]',
-				'[[:Disciplinary hearing of Harry Potter|Disciplinary hearing of Harry Potter]]'
-			],
-			[
-				'* [[Let\'s see powerrangers]] - \'\'[[Super Sentai]]\'\' counterpart
-in \'\'[[and some more crazy stuff!]]\'\'.\'\'',
-				'[[Let\'s see powerrangers]] - [[Super Sentai]] counterpart in [[and some more crazy stuff!]].'
-			],
-			[
-				':\'\'Italics [[Foo Bar]] - [[foo|here]]\'\'.',
-				'Italics [[Foo Bar]] - [[foo|here]].',
-			],
-			[
-				'   \'\'\'Bold [[Foo Bar]] - [[foo|here]] with spaces\'\'\'.',
-				'Bold [[Foo Bar]] - [[foo|here]] with spaces.',
-			],
-			[
-				'===Headers [[Foo Bar]]=== - [[foo|here]] in context-links*!.',
-				'Headers [[Foo Bar]] - [[foo|here]] in context-links*!.'
-			],
-			[
-				'===Headers [[Foo Bar]]====>[[foo|here]] in context-links*!.',
-				'Headers [[Foo Bar]]=>[[foo|here]] in context-links*!.'
-			]
-		];
-	}
 }
