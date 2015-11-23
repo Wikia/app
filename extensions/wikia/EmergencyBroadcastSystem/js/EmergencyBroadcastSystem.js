e727225bafcd6cf7dec51ec795550c6e23c5fd9c
Inez Korczynski <inez@wikia-inc.com>
2015-11-23 20:30:21 +0000

    VE-2078: Return false in the click handler so it prevents default - so # does not appear in the URL and there is no "jump"

--- extensions/wikia/EmergencyBroadcastSystem/js/EmergencyBroadcastSystem.js
+++ extensions/wikia/EmergencyBroadcastSystem/js/EmergencyBroadcastSystem.js
@@ -6,6 +6,6 @@
 		$('.ebs-container').hide();
 	}
 
-	$('.ebs-primary-action').click(function(){ handleClick( 1 ); });
-	$('.ebs-secondary-action').click(function(){ handleClick( 0 ); });
+	$('.ebs-primary-action').click(function(){ handleClick( 1 ); return false; });
+	$('.ebs-secondary-action').click(function(){ handleClick( 0 ); return false; });
 })(jQuery);
