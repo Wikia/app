0adf3d6aa51083b7b4bd1a2547544f9571b2a006
Inez Korczynski <inez@wikia-inc.com>
2015-11-23 22:47:11 +0000

    VE-2063: i18n for EBS

---
+++ extensions/wikia/EmergencyBroadcastSystem/EmergencyBroadcastSystem.i18n.php
@@ -0,0 +1,10 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'ebs-heading' => 'Infoboxes on this wiki are not properly rendered on mobile devices',
+	'ebs-content' => 'Because they\'re not portable. You can fix this issue by converting those infoboxes into portable ones.<br />Do you want to do that? [[w:Help:Infoboxes|Learn more]]',
+	'ebs-yes' => 'Yes',
+	'ebs-no' => 'No'	
+);
--- extensions/wikia/EmergencyBroadcastSystem/templates/EmergencyBroadcastSystem_index.php
+++ extensions/wikia/EmergencyBroadcastSystem/templates/EmergencyBroadcastSystem_index.php
@@ -1,11 +1,11 @@
 <section class="ebs-container">
 	<p class="ebs-unread"><?= $nonPortableCount ?></p>
 	<div class="ebs-content">
-		<h3>Infoboxes on this wiki are not properly rendered on mobile devices</h3>
-		<p>Because they're not portable. You can fix this issue by converting those infoboxes into portable ones.<br />Do you want to do that? <a href="#">Learn more</a></p>
+		<h3><?= wfMessage( 'ebs-heading' )->parse() ?></h3>
+		<p><?= wfMessage( 'ebs-content' )->parse() ?></p>
 	</div>
 	<div class="ebs-actions">
-		<a class="ebs-secondary-action" href="<?= $surveyUrl ?>" target="_blank">No</a>
-		<a class="ebs-primary-action" href="#">Yes</a>
+		<a class="ebs-secondary-action" href="<?= $surveyUrl ?>" target="_blank"><?= wfMessage( 'ebs-no' )->parse() ?</a>
+		<a class="ebs-primary-action" href="#"><?= wfMessage( 'ebs-yes' )->parse() ?></a>
 	</div>
 </section>
