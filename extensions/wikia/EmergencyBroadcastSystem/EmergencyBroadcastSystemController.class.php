01246abf121a664ae9ed4744fd688785d780d39e
Paul Oslund <poslund@wikia-inc.com>
2015-11-23 20:35:52 +0000

    VE-2074: Remove switch statement in favour of an associative array

--- extensions/wikia/EmergencyBroadcastSystem/EmergencyBroadcastSystemController.class.php
+++ extensions/wikia/EmergencyBroadcastSystem/EmergencyBroadcastSystemController.class.php
@@ -70,17 +70,18 @@ class EmergencyBroadcastSystemController extends WikiaController {
 		global $wgContLang;
 
 		$context = $this->getContext();
-		$userName = $context->getUser()->getName();
 		$userLanguage = $context->getLanguage();
+		$userName = $context->getUser()->getName();
 
-		$lang = $userLanguage->mCode;
-		$supportedLanguages = ['en', 'de', 'es', 'fr', 'it', 'ja', 'pl', 'pt-br', 'ru', 'zh'];
+		$url = $this->googleFormUrl( $userLanguage->mCode, $userName );
 
-		if ( !in_array( $lang, $supportedLanguages ) ) {
-			$lang = $wgContLang->mCode;
+		if ( $url === null ) {
+			$url = $this->googleFormUrl( $wgContLang->mCode, $userName );
 		}
 
-		$url = $this->googleFormUrl( $lang, $userName);
+		if ( $url === null ) {
+			$url = $this->googleFormUrl( 'en', $userName );
+		}
 
 		return $url;
 	}
@@ -88,45 +89,20 @@ class EmergencyBroadcastSystemController extends WikiaController {
 	protected function googleFormUrl( $language, $userName ) {
 		global $wgServer;
 
-		preg_match( '/\/\/(.*)/', $wgServer, $matches );
-		$domain = $matches[1];
+		$domain = parse_url( $wgServer, PHP_URL_HOST );
+		$forms = [
+			'de' => "https://docs.google.com/forms/d/1Ks_uhxdi5Cb9EiNDup4bc7O6kI3Dl6rjq7AjlfqtX9A/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'en' => "https://docs.google.com/forms/d/18qE5ub8qs8bkrubcN-00JxuLrfnfpJ88MyGhq1x3RpY/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'es' => "https://docs.google.com/forms/d/1MLwwR8t-uoIOBXjjlW7YnBLOOrbH8_19Q3N5Bttqp0Y/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'fr' => "https://docs.google.com/forms/d/1MUAxAhzIU5rQfaPeBAX3sDHCcxA87L74R04D2TftPx8/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'it' => "https://docs.google.com/forms/d/1R4CHKl620CGwPGcMwQMC3VCnQ2v1LCZKf8JanAoKxj8/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'ja' => "https://docs.google.com/forms/d/1Cn1MkdtNn3HF6eaUmqU2VTcvOm9D-KOl3EoIgMNX_Rs/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'pl' => "https://docs.google.com/forms/d/1fbsGFW01P6vJ_vqAH2yH-CM27K-a484k34Ne1Qc6r00/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'pt-br' => "https://docs.google.com/forms/d/1sYpFtiR-vewJegYXO2gfMtvv-wz2Aml4xuVXzSZtssU/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'ru' => "https://docs.google.com/forms/d/1yH_MiPY9GRj9yXYUjuUwu2Z_e2bulTZp5GXAd0NaB-8/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849",
+			'zh' => "https://docs.google.com/forms/d/1vnLsNYhWPa4lghIMyC8Eyu6gK0dZ0N3j4_RP1K3HH28/viewform?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849"
+		];
 
-		switch ( $lang ) {
-			case 'zh':
-				$url = "https://docs.google.com/forms/d/1vnLsNYhWPa4lghIMyC8Eyu6gK0dZ0N3j4_RP1K3HH28/viewform";
-				break;
-			case 'ru':
-				$url = "https://docs.google.com/forms/d/1yH_MiPY9GRj9yXYUjuUwu2Z_e2bulTZp5GXAd0NaB-8/viewform";
-				break;
-			case 'pt-br':
-				$url = "https://docs.google.com/forms/d/1sYpFtiR-vewJegYXO2gfMtvv-wz2Aml4xuVXzSZtssU/viewform";
-				break;
-			case 'pl':
-				$url = "https://docs.google.com/forms/d/1fbsGFW01P6vJ_vqAH2yH-CM27K-a484k34Ne1Qc6r00/viewform";
-				break;
-			case 'ja':
-				$url = "https://docs.google.com/forms/d/1Cn1MkdtNn3HF6eaUmqU2VTcvOm9D-KOl3EoIgMNX_Rs/viewform";
-				break;
-			case 'it':
-				$url = "https://docs.google.com/forms/d/1R4CHKl620CGwPGcMwQMC3VCnQ2v1LCZKf8JanAoKxj8/viewform";
-				break;
-			case 'fr':
-				$url = "https://docs.google.com/forms/d/1MUAxAhzIU5rQfaPeBAX3sDHCcxA87L74R04D2TftPx8/viewform";
-				break;
-			case 'de':
-				$url = "https://docs.google.com/forms/d/1Ks_uhxdi5Cb9EiNDup4bc7O6kI3Dl6rjq7AjlfqtX9A/viewform";
-				break;
-			case 'es':
-				$url = "https://docs.google.com/forms/d/1MLwwR8t-uoIOBXjjlW7YnBLOOrbH8_19Q3N5Bttqp0Y/viewform";
-				break;
-			case 'en':
-			default:
-				$url = "https://docs.google.com/forms/d/18qE5ub8qs8bkrubcN-00JxuLrfnfpJ88MyGhq1x3RpY/viewform";
-				break;
-		}
-
-		$url = $url . "?entry.2019588325={$userName}&entry.1160019288={$domain}&entry.830663849";
-
-		return $url;
+		return $forms[ $language ];
 	}
 }
