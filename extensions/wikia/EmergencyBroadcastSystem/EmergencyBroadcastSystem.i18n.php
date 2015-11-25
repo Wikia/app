fcf02f275fb6f7c544826c4f9230231d68216a70
Paul Oslund <poslund@wikia-inc.com>
2015-11-25 21:23:57 +0000

    VE-2083: Add EBS translation for 'zh'

--- extensions/wikia/EmergencyBroadcastSystem/EmergencyBroadcastSystem.i18n.php
+++ extensions/wikia/EmergencyBroadcastSystem/EmergencyBroadcastSystem.i18n.php
@@ -6,5 +6,12 @@ $messages['en'] = array(
 	'ebs-heading' => 'Infoboxes on this wiki are not properly rendered on mobile devices',
 	'ebs-content' => 'Because they\'re not portable. You can fix this issue by converting those infoboxes into portable ones.<br />Do you want to do that? [[w:Help:Infoboxes|Learn more]]',
 	'ebs-yes' => 'Yes',
-	'ebs-no' => 'No'	
+	'ebs-no' => 'No'
+);
+
+$messages['zh'] = array(
+	'ebs-heading' => '這個社區上的訊息框在手機上顯示不正常。',
+	'ebs-content' => '您的訊息框過時了。 請使用新的方法重新構建，讓它們在行動裝置上得到更好的顯示。[[w:c:zh.Community:Help:訊息框|點擊這裡查看更多。]]',
+	'ebs-yes' => '是',
+	'ebs-no' => '否'
 );
