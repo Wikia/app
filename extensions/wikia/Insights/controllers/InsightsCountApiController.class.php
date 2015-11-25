a4e206a9892ce5663b04bbe40ab5521fec6b38e2
adamkarminski <adamkarminski@gmail.com>
2015-11-25 15:23:49 +0100

    CE-2820 Add docs and clean up

--- extensions/wikia/Insights/controllers/InsightsCountApiController.class.php
+++ extensions/wikia/Insights/controllers/InsightsCountApiController.class.php
@@ -1,7 +1,15 @@
 <?php
+/**
+ * Class InsightsCountApiController
+ * A public API that allows you to retrieve information about number of items on Insights lists.
+ */
 
 class InsightsCountApiController extends WikiaApiController {
 
+	/**
+	 * @requestParam string type A type of an Insights list
+	 * @response int count A number of items on the list
+	 */
 	public function getCount() {
 		$type = $this->request->getVal( 'type' );
 
@@ -10,6 +18,9 @@ class InsightsCountApiController extends WikiaApiController {
 		$this->setVal( 'count', $count );
 	}
 
+	/**
+	 * @response array An array of counts with types of Insights as keys
+	 */
 	public function getAllCounts() {
 		$counts = ( new InsightsCountService() )->getAllCounts();
 
--- extensions/wikia/Insights/services/InsightsCountService.class.php
+++ extensions/wikia/Insights/services/InsightsCountService.class.php
@@ -1,36 +1,59 @@
 <?php
+/**
+ * Class InsightsCountService
+ * Retrieves information on number of items on Insights lists.
+ */
 
 class InsightsCountService extends WikiaService {
 
+	/**
+	 * @var array
+	 * If you do not want to calculate a count for a set of lists - put them in this array.
+	 */
+	public static $excludedFromCount = [
+		InsightsFlagsModel::INSIGHT_TYPE,
+	];
+
+	/**
+	 * Returns a count of items on a given Insights list based on a passed type.
+	 * @param string $type
+	 * @return int
+	 */
 	public function getCount( $type ) {
 		return $this->calculateCount( $type );
 	}
 
+	/**
+	 * Returns an array of counts on a all Insights list except for the excluded ones.
+	 * @return array An array of counts with types as keys
+	 */
 	public function getAllCounts() {
 		$response = [];
 		foreach ( InsightsHelper::getInsightsPages() as $type => $className ) {
+			if ( in_array( $type, self::$excludedFromCount ) ) {
+				continue;
+			}
 			$response[$type] = $this->calculateCount( $type );
 		}
 
 		return $response;
 	}
 
-	private function calculateCount( $type, $className = false ) {
-		if ( $className === false ) {
-			$className = InsightsHelper::getInsightsPages()[$type];
+	/**
+	 * @param string $type A type of an Insights list for which you'd like to calculate a count.
+	 * @return int|bool Returns false if a given class does not exist or the count otherwise.
+	 */
+	private function calculateCount( $type ) {
+		$className = InsightsHelper::getInsightsPages()[$type];
+		if ( !class_exists( $className ) ) {
+			return false;
 		}
 
+		/**
+		 * Use a regular fetchArticleData method from a given class
+		 */
 		$subpageModel = new $className;
 		$subpageModel->initModel( [] );
-		return count($subpageModel->fetchArticlesData());
-	}
-
-	private function getMemcKey( $type ) {
-		return wfMemcKey(
-			InsightsPageModel::INSIGHTS_MEMC_PREFIX,
-			$type,
-			InsightsPageModel::INSIGHTS_MEMC_ARTICLES_KEY,
-			InsightsPageModel::INSIGHTS_MEMC_VERSION
-		);
+		return count( $subpageModel->fetchArticlesData() );
 	}
 }
