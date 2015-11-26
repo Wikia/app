eba97cd48087ca27a570365edb943b5d954e5ffd
Warkot <mateusz.warkocki@wikia-inc.com>
2015-11-26 23:04:00 +0100

    XW-316 | test cases names added; test case parameters packed in associative arrays

--- includes/wikia/services/tests/HubServiceTest.php
+++ includes/wikia/services/tests/HubServiceTest.php
@@ -4,23 +4,14 @@ class HubServiceTest extends WikiaBaseTest {
 
 	/**
 	 * @dataProvider getVerticalNameForComscoreDataProvider
-	 * @param boolean $wgDisableWAMOnHubsMock
-	 * @param boolean $isWikiaHomePageValueMock
-	 * @param boolean $isWikiaHubValueMock
-	 * @param integer $verticalIdMock
+	 * @param (boolean|integer)[] $mocks
 	 * @param string $expectedVerticalName
 	 */
-	public function testGetVerticalNameForComscore(
-		$isWikiaHomePageValueMock,
-		$isWikiaHubValueMock,
-		$wgDisableWAMOnHubsMock,
-		$verticalIdMock,
-		$expectedVerticalName
-	) {
-		$this->mockGlobalVariable( 'wgDisableWAMOnHubs', $wgDisableWAMOnHubsMock );
+	public function testGetVerticalNameForComscore( $mocks, $expectedVerticalName ) {
+		$this->mockGlobalVariable( 'wgDisableWAMOnHubs', $mocks['wgDisableWAMOnHubs'] );
 
-		$this->mockStaticMethod( 'WikiaPageType', 'isWikiaHomePage', $isWikiaHomePageValueMock );
-		$this->mockStaticMethod( 'WikiaPageType', 'isWikiaHub', $isWikiaHubValueMock );
+		$this->mockStaticMethod( 'WikiaPageType', 'isWikiaHomePage', $mocks['isWikiaHomePage'] );
+		$this->mockStaticMethod( 'WikiaPageType', 'isWikiaHub', $mocks['isWikiaHub'] );
 
 		$wikiFactoryHubMock = $this->getMock( 'WikiFactoryHub', [ 'getVerticalId' ] );
 
@@ -28,50 +19,194 @@ class HubServiceTest extends WikiaBaseTest {
 
 		$wikiFactoryHubMock->expects( $this->any() )
 			->method( 'getVerticalId' )
-			->will( $this->returnValue( $verticalIdMock ) );
+			->will( $this->returnValue( $mocks['verticalId'] ) );
 
 		$this->assertEquals( $expectedVerticalName, HubService::getVerticalNameForComscore( 1 ) );
 	}
 
 	public function getVerticalNameForComscoreDataProvider() {
 		return [
-			// actual corporate hompages
-			// e.g. www.wikia.com (on 2015-11-26)
-			[ true, false, false, WikiFactoryHub::VERTICAL_ID_LIFESTYLE, 'fandom' ],
-			// e.g. pl.wikia.com
-			[ true, false, false, WikiFactoryHub::VERTICAL_ID_OTHER, 'fandom' ],
+			//actual corporate homepages
+			'actual corporate hompage — e.g. www.wikia.com (on 2015-11-26)' => [
+				[
+					'isWikiaHomePage' => true,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_LIFESTYLE
+				],
+				'fandom'
+			],
+			'actual corporate hompage — e.g. pl.wikia.com' => [
+				[
+					'isWikiaHomePage' => true,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_OTHER
+				],
+				'fandom'
+			],
 			// hypothetical cases
-			[ true, true, true, WikiFactoryHub::VERTICAL_ID_BOOKS, 'fandom' ],
-			[ true, false, true, WikiFactoryHub::VERTICAL_ID_COMICS, 'fandom' ],
-			[ true, true, false, WikiFactoryHub::VERTICAL_ID_MOVIES, 'fandom' ],
+			'actual cororate hompage — hypothetical case, vertical: books' => [
+				[
+					'isWikiaHomePage' => true,
+					'isWikiaHub' => true,
+					'wgDisableWAMOnHubs' => true,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_BOOKS
+				],
+				'fandom'
+			],
+			'actual cororate hompage — hypothetical case, vertical: comics' => [
+				[
+					'isWikiaHomePage' => true,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => true,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_COMICS
+				],
+				'fandom'
+			],
+			'actual cororate hompage — hypothetical case, vertical: movies' => [
+				[
+					'isWikiaHomePage' => true,
+					'isWikiaHub' => true,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_MOVIES
+				],
+				'fandom'
+			],
 
 			// hub-based corporate homepages
-			// e.g. pt-br.wikia.com
-			[ false, true, true, WikiFactoryHub::VERTICAL_ID_OTHER, 'fandom' ],
-			// hypothetical case
-			[ false, true, true, WikiFactoryHub::VERTICAL_ID_LIFESTYLE, 'fandom' ],
+			'hub-based corporate homepage — e.g. pt-br.wikia.com' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => true,
+					'wgDisableWAMOnHubs' => true,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_OTHER
+				],
+				'fandom'
+			],
+			'hub-based corporate homepage — hypothetical case' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => true,
+					'wgDisableWAMOnHubs' => true,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_LIFESTYLE
+				],
+				'fandom'
+			],
 
 			// corporate hubs
-			// e.g. gryhub.wikia.com
-			[ false, true, false, WikiFactoryHub::VERTICAL_ID_VIDEO_GAMES, 'gaming' ],
-			// e.g. lifestylehub.wikia.com
-			[ false, true, false, WikiFactoryHub::VERTICAL_ID_LIFESTYLE, 'lifestyle' ],
-			// e.g. tvhub.wikia.com
-			[ false, true, false, WikiFactoryHub::VERTICAL_ID_TV, 'entertainment' ],
+			'corporate hub — e.g. gryhub.wikia.com' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => true,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_VIDEO_GAMES
+				],
+				'gaming'
+			],
+			'corporate hub — e.g. lifestylehub.wikia.com' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => true,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_LIFESTYLE
+				],
+				'lifestyle'
+			],
+			'corporate hub — e.g. tvhub.wikia.com' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => true,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_TV
+				],
+				'entertainment'
+			],
 			// hypothetical case
-			[ false, true, false, WikiFactoryHub::VERTICAL_ID_OTHER, 'lifestyle' ],
+			'corporate hub — hypothetical case' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => true,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_OTHER
+				],
+				'lifestyle'
+			],
 
 			// usual wiki(a)s
-			[ false, false, false, WikiFactoryHub::VERTICAL_ID_VIDEO_GAMES, 'gaming' ],
-			[ false, false, false, WikiFactoryHub::VERTICAL_ID_LIFESTYLE, 'lifestyle' ],
-			[ false, false, false, WikiFactoryHub::VERTICAL_ID_OTHER, 'lifestyle' ],
-			[ false, false, false, WikiFactoryHub::VERTICAL_ID_MUSIC, 'entertainment' ],
-
+			'usual wiki(a) — vertical: games' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_VIDEO_GAMES
+				],
+				'gaming'
+			],
+			'usual wiki(a) — vertical: lifestyle ' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_LIFESTYLE
+				],
+				'lifestyle'
+			],
+			'usual wiki(a) — vertical: other' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_OTHER
+				],
+				'lifestyle'
+			],
+			'usual wiki(a) — vertical: music' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => false,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_MUSIC
+				],
+				'entertainment'
+			],
 			// hypothetical cases
-			[ false, false, true, WikiFactoryHub::VERTICAL_ID_VIDEO_GAMES, 'gaming' ],
-			[ false, false, true, WikiFactoryHub::VERTICAL_ID_LIFESTYLE, 'lifestyle' ],
-			[ false, false, true, WikiFactoryHub::VERTICAL_ID_OTHER, 'lifestyle' ],
-			[ false, false, true, WikiFactoryHub::VERTICAL_ID_MUSIC, 'entertainment' ],
+			'usual wiki(a) — hypothetical case, vertical: games' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => true,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_VIDEO_GAMES
+				],
+				'gaming'
+			],
+			'usual wiki(a) — hypothetical case, vertical: lifestyle' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => true,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_LIFESTYLE
+				],
+				'lifestyle'
+			],
+			'usual wiki(a) — hypothetical case, vertical: other' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => true,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_OTHER
+				],
+				'lifestyle'
+			],
+			'usual wiki(a) — hypothetical case, vertical: music' => [
+				[
+					'isWikiaHomePage' => false,
+					'isWikiaHub' => false,
+					'wgDisableWAMOnHubs' => true,
+					'verticalId' => WikiFactoryHub::VERTICAL_ID_MUSIC
+				],
+				'entertainment'
+			],
 		];
 	}
 }
