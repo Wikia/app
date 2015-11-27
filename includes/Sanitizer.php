8a36f772a8da7ed619557f50f96aec2bbe23e944
tor <tor@wikia-inc.com>
2015-11-27 19:04:18 +0000

    added nav and time as supported HTML elements

--- includes/Sanitizer.php
+++ includes/Sanitizer.php
@@ -383,7 +383,7 @@ class Sanitizer {
 				'strike', 'strong', 'tt', 'var', 'div', 'center',
 				'blockquote', 'ol', 'ul', 'dl', 'table', 'caption', 'pre',
 				'ruby', 'rt' , 'rb' , 'rp', 'p', 'span', 'abbr', 'q', 'acronym', 'dfn',
-				'kbd', 'samp'
+				'kbd', 'samp', 'nav', 'time'
 			);
 			$htmlsingle = array(
 				'br', 'hr', 'li', 'dt', 'dd'
