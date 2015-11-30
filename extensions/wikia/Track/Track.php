915ef62617aac6cdbd794e60c9261cebffe963c9
Inez Korczynski <inez@wikia-inc.com>
2015-11-30 19:41:53 +0000

    VE-2094: Log to 3rd party logging solution if something went terribly wrong

--- extensions/wikia/Track/Track.php
+++ extensions/wikia/Track/Track.php
@@ -65,6 +65,7 @@ class Track {
 		window.beacon_id = result[1];
 	} else {
 		// something went terribly wrong
+		document.write('<img src="http://logs-01.loggly.com/inputs/88a88e56-77c6-49cc-af41-6f44f83fe7fe.gif?message=something%20went%20terribly%20wrong"/>');
 	}
 
 	var utma = RegExp("__utma=([0-9\.]+)").exec(document.cookie);
