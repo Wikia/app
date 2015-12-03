2c0e4c72c9d9ed901aca29e73d0cac8f6cf91ac1
adamkarminski <adamkarminski@gmail.com>
2015-12-03 14:52:38 +0100

    CE-3120 Add a unit test for the NavigationTemplate

---
+++ includes/wikia/parser/templatetypes/tests/NavigationTemplateTest.php
@@ -0,0 +1,55 @@
+<?php
+
+class NavigationTemplateTest extends WikiaBaseTest {
+
+	/**
+	 * @param $expectedOutput
+	 * @param $templateText
+	 * @dataProvider getNavigationTemplates
+	 */
+	public function testHideNavigationWithBlockElements( $templateText, $expectedOutput, $message ) {
+		$sanitizedOutput = NavigationTemplate::handle( $templateText );
+
+		$this->assertSame( $expectedOutput, $sanitizedOutput, $message );
+	}
+
+	public function getNavigationTemplates() {
+		return [
+			[
+				'<a>This is a <strong>template</strong> <b>without</b> a <span>block</span> element</a>.',
+				'<a>This is a <strong>template</strong> <b>without</b> a <span>block</span> element</a>.',
+				'A template with a link, formatting tags and a span one should be shown.',
+			],
+			[
+				'<span>This is a template with a div <div>element</div></span>.',
+				'',
+				'A template with a div tag should be hidden.',
+			],
+			[
+				'<span>This is a template with a DIV <DIV>element</DIV></span>.',
+				'',
+				'A template with a DIV (uppercase) tag should be hidden.',
+			],
+			[
+				'<span>This is a template with a table <table>element</table></span>.',
+				'',
+				'A template with a table tag should be hidden.',
+			],
+			[
+				'<span>This is a template with a TABLE <TABLE>element</TABLE></span>.',
+				'',
+				'A template with a TABLE (uppercase) tag should be hidden.',
+			],
+			[
+				'<span>This is a template with a p <p>element</p></span>.',
+				'',
+				'A template with a p tag should be hidden.',
+			],
+			[
+				'<span>This is a template with a P <P>element</P></span>.',
+				'',
+				'A template with a P (uppercase) tag should be hidden.',
+			],
+		];
+	}
+}
