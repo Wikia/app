287ba4a969b0e717750fe131e77b48d1a662890d
Piotr Gabryjeluk <rychu@wikia-inc.com>
2015-12-03 10:19:47 -0800

    Update ImageServingCroppingTest.php

--- extensions/wikia/ImageServing/tests/ImageServingCroppingTest.php
+++ extensions/wikia/ImageServing/tests/ImageServingCroppingTest.php
@@ -4,6 +4,8 @@
  * @group Integration
  * @group MediaFeatures
  * @group Broken
+ *
+ * TODO: Fix the test: https://wikia-inc.atlassian.net/browse/MAIN-6018
  */
 class ImageServingCroppingTest extends WikiaBaseTest {
 
