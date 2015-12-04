346a0e2ef949b3525e0ccf214105027e5c67cb51
dianafa <diana.falkowska@gmail.com>
2015-12-04 14:21:14 +0100

    add InfoIconTemplate

---
+++ includes/wikia/parser/templatetypes/handlers/InfoIconTemplate.class.php
@@ -0,0 +1,30 @@
+<?php
+class InfoIconTemplate {
+	/**
+	 * @desc sanitize infoicon template content, that is remove all non-images
+	 * from it's wikitext. Then, parse them and add to stripState with
+	 * handlerParams[template-type] set to TEMPLATE_INFOICON.
+	 *
+	 * @param string $wikitext
+	 * @param \Parser $parser
+	 * @return string wikitext containing strip markers. Each strip marker represents parsed markup of an infoicon
+	 * @throws \MWException
+	 */
+	public static function handle( $wikitext, Parser $parser ) {
+		global $wgContLang;
+		$images = FileNamespaceSanitizeHelper::getInstance()->getCleanFileMarkersFromWikitext( $wikitext, $wgContLang );
+		$output = '';
+		foreach ( $images as $image ) {
+			$title = Title::newFromText( $image );
+			$file = wfFindFile( $title );
+			$output .= Linker::makeImageLink2(
+				$title,
+				$file,
+				[],
+				[ 'template-type' => AutomaticTemplateTypes::TEMPLATE_INFOICON ]
+			);
+		}
+		$stripMarker = $parser->insertStripItem( $output );
+		return $stripMarker;
+	}
+}
