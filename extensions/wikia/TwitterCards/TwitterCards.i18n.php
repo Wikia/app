8542cc9934cb78dc0314edfd00dd3315dd167fa3
saipetch <saipetch@wikia-inc.com>
2015-12-07 18:12:19 -0800

    Monetization (SEO-157): add TwitterCards extension

---
+++ extensions/wikia/TwitterCards/TwitterCards.i18n.php
@@ -0,0 +1,11 @@
+<?php
+
+$messages = [];
+
+$messages['en'] = [
+	'twittercards-desc' => 'Add meta tags for Twitter Cards',
+];
+
+$messages['qqq'] = [
+	'twittercards-desc' => '{{desc}}',
+];
---
+++ extensions/wikia/TwitterCards/TwitterCards.setup.php
@@ -0,0 +1,22 @@
+<?php
+
+$wgExtensionCredits['twittercards'][] = [
+	'name' => 'TwitterCards',
+	'author' => [
+		"Piotr Gabryjeluk <rychu at wikia-inc.com>",
+		"Saipetch Kongkatong <saipetch at wikia-inc.com>",
+	],
+	'descriptionmsg' => 'twittercards-desc',
+	'url' => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/TwitterCards'
+];
+
+$dir = dirname( __FILE__ ) . '/';
+
+// Classes
+$wgAutoloadClasses['TwitterCardsHooks'] = $dir . 'TwitterCardsHooks.class.php';
+
+// Hooks
+$wgHooks['BeforePageDisplay'][] = 'TwitterCardsHooks::onBeforePageDisplay';
+
+// i18n mapping
+$wgExtensionMessagesFiles['TwitterCards'] = $dir . 'TwitterCards.i18n.php';
