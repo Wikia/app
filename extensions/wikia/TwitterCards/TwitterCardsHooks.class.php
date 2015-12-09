41cb894166b06036154b8deb0872cd1bee9ddc10
saipetch <saipetch@wikia-inc.com>
2015-12-08 17:21:37 -0800

    Monetization (SEO-157): TwitterCards - add meta tags only once

--- extensions/wikia/TwitterCards/TwitterCardsHooks.class.php
+++ extensions/wikia/TwitterCards/TwitterCardsHooks.class.php
@@ -12,11 +12,18 @@ class TwitterCardsHooks {
 	 * @return bool
 	 */
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
+		$app = F::app();
+		if ( $app->wg->TwitterCardsMetaTagsLoaded ) {
+			return true;
+		}
+
 		$twitterCards = new TwitterCards();
 		$meta = $twitterCards->getMeta( $out );
 		foreach( $meta as $name => $value ) {
 			$out->addMeta( $name, $value );
 		}
+
+		$app->wg->TwitterCardsMetaTagsLoaded = true;
 
 		return true;
 	}
