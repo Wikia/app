2c6726ec9d79949d87f46fee5128b86a486dbea1
saipetch <saipetch@wikia-inc.com>
2015-12-08 17:52:56 -0800

    Monetization (SEO-157): TwitterCards - remove functions to get image url

--- extensions/wikia/TwitterCards/TwitterCards.class.php
+++ extensions/wikia/TwitterCards/TwitterCards.class.php
@@ -6,12 +6,6 @@
 class TwitterCards extends WikiaModel {
 
 	const DESCRIPTION_MAX_LENGTH = 200;
-	const IMAGE_CACHE_TTL = 604800; // 7 days
-	const IMAGE_MIN_HEIGHT = 200;
-	const IMAGE_MIN_WIDTH = 200;
-	const IMAGE_PREF_HEIGHT = 315;
-	const IMAGE_PREF_WIDTH = 600;
-	const THUMB_MAX_WIDTH = 2000;
 	const TITLE_MAX_LENGTH = 70;
 
 	/**
@@ -34,12 +28,6 @@ class TwitterCards extends WikiaModel {
 		$description = $this->getDescription( $output );
 		if ( !empty( $description ) ) {
 			$meta['twitter:description'] = $description;
-		}
-
-		// add image url
-		$imageUrl = $this->getImageUrl( $title );
-		if ( !empty( $imageUrl ) ) {
-			$meta['twitter:image'] = $imageUrl;
 		}
 
 		return $meta;
@@ -76,101 +64,6 @@ class TwitterCards extends WikiaModel {
 		}
 
 		return $description;
-	}
-
-	/**
-	 * Get image url
-	 * @param Title $title
-	 * @return string
-	 */
-	protected function getImageUrl( Title $title ) {
-		if ( $title->isMainPage() ) {
-			return wfExpandUrl( $this->wg->Logo );
-		}
-
-		$namespace = $title->getNamespace();
-		if ( $namespace == NS_USER ) {
-			return '';
-		}
-
-		$cacheKey = $this->getMemcKey( $title );
-		$imageUrl = $this->wg->Memc->get( $cacheKey );
-		if ( !is_string( $imageUrl ) ) {
-			if ( $namespace != NS_FILE ) {
-				$title = $this->getFirstArticleImage( $title );
-			}
-
-			$imageUrl = '';
-			if ( !empty( $title ) ) {
-				$file = wfFindFile( $title );
-				if ( !empty( $file ) ) {
-					if ( $file->getWidth() > self::THUMB_MAX_WIDTH ) {
-						$thumb = $file->transform( [ 'width' => self::THUMB_MAX_WIDTH ], 0 );
-						if ( !empty( $thumb ) ) {
-							$imageUrl = $thumb->getUrl();
-						}
-					} else {
-						$imageUrl = $file->getUrl();
-					}
-				}
-			}
-
-			$this->wg->Memc->set( $cacheKey, $imageUrl, IMAGE_CACHE_TTL );
-		}
-
-		return $imageUrl;
-	}
-
-	/**
-	 * Get first image from the article
-	 * @param Title $title
-	 * @return Title|null
-	 */
-	protected function getFirstArticleImage( $title ) {
-		// check for infobox image
-		$driver = 'ImageServingDriverInfoboxImageNS';
-		$retTitle = $this->getFirstImageLargerThan( $title, self::IMAGE_MIN_WIDTH, self::IMAGE_MIN_HEIGHT, $driver);
-		if ( !empty( $retTitle ) ) {
-			return $retTitle;
-		}
-
-		// check for preferred size image
-		$retTitle = $this->getFirstImageLargerThan( $title, self::IMAGE_PREF_WIDTH, self::IMAGE_PREF_HEIGHT );
-		if ( !empty( $retTitle ) ) {
-			return $retTitle;
-		}
-
-		return $this->getFirstImageLargerThan( $title, self::IMAGE_MIN_WIDTH, self::IMAGE_MIN_HEIGHT );
-	}
-
-	/**
-	 * Get first image from the article that matched the criteria
-	 * @param Title $title
-	 * @param int $width
-	 * @param int $height
-	 * @param string $driverName
-	 * @return Title|null
-	 */
-	protected function getFirstImageLargerThan( $title, $width, $height, $driverName = null ) {
-		$imageServing = new ImageServing( [ $title->getArticleID() ], $width, $height );
-		$images = $imageServing->getImages( 1, $driverName );
-		if ( empty( $images ) ) {
-			return null;
-		}
-
-		// used reset instead direct call because we can get hashmap from ImageServing driver.
-		$first = reset( $images );
-		$name = $first[ 0 ][ 'name' ];
-		return Title::newFromText( $name, NS_FILE );
-	}
-
-	/**
-	 * Get Memcache key
-	 * @param Title $title
-	 * @return String
-	 */
-	protected function getMemcKey( $title ) {
-		return wfMemcKey( 'TwitterCardsImage', md5( $title->getDBkey() ) );
 	}
 
 }
