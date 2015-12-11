ebbd3356ba3c0373b6b2c1da9ab487d6f16bc203
Władysław Bodzek <wladek@wikia-inc.com>
2015-12-11 18:36:41 +0100

    Split page actions out of GlobalShortcuts

--- extensions/wikia/GlobalShortcuts/scripts/GlobalShortcuts.js
+++ extensions/wikia/GlobalShortcuts/scripts/GlobalShortcuts.js
@@ -1,85 +1,61 @@
-define('GlobalShortcuts', ['Mousetrap', 'mw'], function (Mousetrap, mw) {
+define('GlobalShortcuts', ['Mousetrap', 'mw', 'PageActions'], function (Mousetrap, mw, PageActions) {
 	'use strict';
 
 	var shortcuts = {
 		// Actions
-		delete: 'd',
-		edit: 'e',
-		flag: 'f',
-		move: 'm',
-		startWikia: 's',
-		classify: 'k',
+		delete: ['d'],
+		edit: ['e'],
+		flag: ['f'],
+		move: ['m'],
+		startWikia: ['s'],
+		classify: ['k'],
 		// Global navigation
-		discussions: 'g d',
-		history: 'g h',
-		insights: 'g i',
-		recentChanges: 'g r',
+		discussions: ['g d'],
+		history: ['g h'],
+		insights: ['g i'],
+		recentChanges: ['g r'],
 		// Local navigation / focus
-		help: '?',
-		search: 'g s',
-		search1: '/'
+		help: ['?'],
+		search: [ 'g s', '/' ]
 	};
-	function Init() {
-		var globalShortcutsConfig = mw.config.get('globalShortcutsConfig');
-		// Help
-		Mousetrap.bind(shortcuts.help, function () {
-			require(['GlobalShortcutsHelp'],function (help) {
-				help.open();
-			});
-		});
-		// Actions
-		Mousetrap.bind(shortcuts.edit, function () {
-			$('#ca-edit')[0].click();
-		});
-		Mousetrap.bind(shortcuts.flag, function () {
-			$('#ca-flags')[0].click();
-		});
-		Mousetrap.bind(shortcuts.delete, function () {
-			$('[accesskey=' + shortcuts.delete + ']')[0].click();
-		});
-		Mousetrap.bind(shortcuts.move, function () {
-			$('[accesskey=' + shortcuts.move + ']')[0].click();
-		});
-		Mousetrap.bind(shortcuts.startWikia, function () {
-			$('[data-id=start-wikia]')[0].click();
-		});
-		// TODO add conditional shortcuts
-		Mousetrap.bind(shortcuts.classify, function () {
-			require(['TemplateClassificationModal'], function shortcutOpenTemplateClassification(tc) {
-				tc.open();
-			});
-		});
 
-		// Local navigation / focus
-		Mousetrap.bind(shortcuts.search, function (e) {
-			e.preventDefault();
-			$('#searchInput')[0].focus();
-		});
-		Mousetrap.bind(shortcuts.search1, function (e) {
-			e.preventDefault();
-			$('#searchInput')[0].focus();
-		});
-
-		// Global navigation
-		Mousetrap.bind(shortcuts.discussions, function () {
-			window.location.href = mw.config.get('location').origin + '/d';
-		});
-		Mousetrap.bind(shortcuts.history, function () {
-			$('[accesskey=h]')[0].click();
-		});
-		Mousetrap.bind(shortcuts.insights, function (e) {
-			e.preventDefault();
-			window.location.href = globalShortcutsConfig.insights;
-		});
-		Mousetrap.bind(shortcuts.recentChanges, function (e) {
-			e.preventDefault();
-			window.location.href = globalShortcutsConfig.recentChanges;
+	function initShortcut( actionId, key ) {
+		Mousetrap.bind(key,function(){
+			PageActions.find(actionId)();
 		});
 	}
-	return new Init();
+
+	function add( actionId, key ) {
+		if (!PageActions.find(actionId)) {
+			throw new Error('Unknown action: ' + actionId);
+		}
+		var current = (actionId in shortcuts) ? shortcuts[actionId] : [];
+		if (current.indexOf(key)==-1) {
+			current.push(key);
+			shortcuts[actionId] = current;
+			initShortcut(actionId,key);
+		}
+	}
+
+	function find(actionId) {
+		return (actionId in shortcuts) ? shortcuts[actionId] : [];
+	}
+
+	function Init() {
+		for (var id in shortcuts){
+			if (shortcuts.hasOwnProperty(id)) {
+				($.isArray(shortcuts[id]) ? shortcuts[id] : [shortcuts[id]]).forEach(function(key){
+					initShortcut(id,key);
+				});
+			}
+		}
+	}
+
+	return {
+		add: add,
+		find: find
+	};
 });
 
-require(['jquery', 'GlobalShortcuts'], function ($, gs) {
-	'use strict';
-	$(gs);
+require(['GlobalShortcuts'], function (gs) {
 });
---
+++ extensions/wikia/GlobalShortcuts/scripts/PageActions.js
@@ -0,0 +1,88 @@
+define('PageActions', ['wikia.mw'], function (mw) {
+	'use strict';
+
+	var globalShortcutsConfig = mw.config.get('globalShortcutsConfig');
+
+	var all = [],
+		byId = {};
+
+	function PageAction( id, caption, action ) {
+		this.id = id;
+		this.caption = caption;
+		this.action = action;
+	}
+
+	function add( id, caption, action ) {
+		var o = new PageAction(id,caption,weight,action);
+		if (id in byId) {
+			throw new Error('Could not register more than one action with the same ID: ' + id);
+		}
+		all.push(o);
+		byId[id] = o;
+	}
+
+	function addMany( o ) {
+		for (var key in o) {
+			if (o.hasOwnProperty(key)) {
+				add(key, o[key][0], o[key][1]);
+			}
+		}
+	}
+
+	function find(id) {
+		return byId[id];
+	}
+
+	// default actions
+	addMany({
+		'delete': ['Delete page', LOW, function(){
+			$('[accesskey=d]')[0].click();
+		}],
+		'edit': ['Edit page', HIGH, function(){
+			$('#ca-edit')[0].click();
+		}],
+		'flag': ['Change page flags', HIGH, function(){
+			$('#ca-flags')[0].click();
+		}],
+		'move': [ 'Move page', function() {
+			$('[accesskey=m]')[0].click();
+		}],
+		'startWikia': [ 'Start a new wikia', function(){
+			$('[data-id=start-wikia]')[0].click();
+		}],
+		'classify': [ 'Classify page', function() {
+			require(['TemplateClassificationModal'], function shortcutOpenTemplateClassification(tc) {
+				tc.open();
+			});
+		}],
+		'discussions': [ 'Open discussions', function() {
+			window.location.href = mw.config.get('location').origin + '/d';
+		}],
+		'history': [ 'Open page history', function() {
+			$('[accesskey=h]')[0].click();
+		}],
+		'insights': [ 'Open Wikia Insights', function() {
+			e.preventDefault();
+			window.location.href = globalShortcutsConfig.insights;
+		}],
+		'recentChanges': [ 'Recent changes list', function(){
+			e.preventDefault();
+			window.location.href = globalShortcutsConfig.recentChanges;
+		}],
+		'search': [ 'Search for a page', function(){
+			e.preventDefault();
+			$('#searchInput')[0].focus();
+		}]
+	});
+
+	return {
+		add: add,
+		all: all,
+		find: find
+	}
+});
+
+require(['jquery', 'GlobalShortcuts'], function ($, gs) {
+	'use strict';
+	$(gs);
+});
