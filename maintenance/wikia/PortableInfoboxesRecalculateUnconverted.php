8846e28509a7a4d5b3ed962e7c107101beb319f6
Kamil Koterba <kamil@wikia-inc.com>
2015-12-18 15:28:30 +0100

    CE-2835 Add recalculate unconverted infoboxes script

---
+++ maintenance/wikia/PortableInfoboxesRecalculateUnconverted.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/../Maintenance.php';
+
+/**
+ * @author Kamil Koterba <kamil@wikia-inc.com>
+ * @ingroup Maintenance
+ */
+class PortableInfoboxesRecalculateUnconverted extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function execute() {
+		global $wgParser;
+		$ui = new InsightsUnconvertedInfoboxesModel();
+		$articles = $ui->fetchArticlesData();
+		foreach ( $articles as $id => $article ) {
+			$article = Article::newFromID($id);
+			$parserOptions = $article->getParserOptions();
+			$title = $article->getTitle();
+			$output = $wgParser->parse( $article->getContent(), $title, $parserOptions );
+			( new LinksUpdate(  $title, $output ) )->doUpdate();
+		}
+		$uip = new UnconvertedInfoboxesPage();
+		$uip->recache();
+		$ui->purgeInsightsCache();
+	}
+}
+
+$maintClass = 'PortableInfoboxesRecalculateUnconverted';
+require_once RUN_MAINTENANCE_IF_MAIN;
