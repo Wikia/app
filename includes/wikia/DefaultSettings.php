96864bf28d39c133a717b8b2f05d7e4a7321eab9
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-01-08 14:54:22 +0100

    ADEN-2789 Add Nielsen provider in AnalyticsEngine

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -1193,6 +1193,12 @@ $wgWikiaHubsFileRepoPath = 'http://corp.wikia.com/';
 $wgWikiaHubsFileRepoDirectory = '/images/c/corp/images';
 
 /**
+ * @name $wgEnableNielsen
+ * Enables Nielsen Digital Content Ratings
+ */
+$wgEnableNielsen = false;
+
+/**
  * @name $wgEnableAmazonMatch
  * Enables AmazonMatch new integration (id=3115)
  */
