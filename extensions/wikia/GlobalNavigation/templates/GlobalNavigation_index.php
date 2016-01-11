6c232300338cafe46fe973799e75b45d7b8de302
Bogna Knychala <bogna.ka@gmail.com>
2016-01-11 14:20:38 +0100

    XW-923 remove hamburger and star a wiki from global nav

--- extensions/wikia/GlobalNavigation/templates/GlobalNavigation_index.php
+++ extensions/wikia/GlobalNavigation/templates/GlobalNavigation_index.php
@@ -1,41 +1,74 @@
-<nav class="global-navigation" id="globalNavigation">
-	<div class="global-navigation-container">
-		<div class="wikia-logo-container table-cell">
-			<a href="<?= htmlspecialchars( $centralUrl ) ?>"
-				class="wikia-logo"
-				rel="nofollow"
-				data-id="wikia-logo">
-				<img src="<?= $wg->BlankImgUrl ?>"
-					height="24"
-					width="91"
-					alt="<?= wfMessage( 'oasis-global-page-header' )->escaped() ?>"
-					title="<?= wfMessage( 'oasis-global-page-header' )->escaped() ?>">
-			</a>
-		</div>
-		<div class="hubs-container table-cell" id="hubsEntryPoint">
-			<?= $app->renderView( 'GlobalNavigation', 'hubsMenu' ) ?>
-		</div>
-		<div class="search-container table-cell">
-			<?= $app->renderView( 'GlobalNavigation', 'searchIndex' ); ?>
-		</div>
-		<div class="account-navigation-container table-cell">
-			<?= $app->renderView( 'GlobalNavigationAccountNavigation', 'index' ) ?>
-		</div>
-		<?php if ( !$isAnon && $notificationsEnabled ): ?>
+<?php if (!$wg->EnableGlobalNav2016): ?>
+	<nav class="global-navigation" id="globalNavigation">
+		<div class="global-navigation-container">
+			<div class="wikia-logo-container table-cell">
+				<a href="<?= htmlspecialchars( $centralUrl ) ?>"
+					class="wikia-logo"
+					rel="nofollow"
+					data-id="wikia-logo">
+					<img src="<?= $wg->BlankImgUrl ?>"
+						height="24"
+						width="91"
+						alt="<?= wfMessage( 'oasis-global-page-header' )->escaped() ?>"
+						title="<?= wfMessage( 'oasis-global-page-header' )->escaped() ?>">
+				</a>
+			</div>
+			<div class="hubs-container table-cell" id="hubsEntryPoint">
+				<?= $app->renderView( 'GlobalNavigation', 'hubsMenu' ) ?>
+	</div>
+	<div class="search-container table-cell">
+		<?= $app->renderView( 'GlobalNavigation', 'searchIndex' ); ?>
+	</div>
+	<div class="account-navigation-container table-cell">
+		<?= $app->renderView( 'GlobalNavigationAccountNavigation', 'index' ) ?>
+	</div>
+	<?php if ( !$isAnon && $notificationsEnabled ): ?>
 		<div class="notifications-container table-cell" id="notificationsEntryPoint">
 			<?= $app->renderView( 'GlobalNavigationWallNotifications', 'Index' ); ?>
 		</div>
-		<?php endif; ?>
-		<div class="start-wikia-container table-cell">
-			<a href="<?= htmlspecialchars( $createWikiUrl ) ?>"
-			   class="start-wikia"
-			   title="<?= wfMessage( 'global-navigation-create-wiki' )->escaped(); ?>"
-			   data-id="start-wikia">
-				<span><?= ( wfMessage( 'global-navigation-create-wiki' )->escaped() ); ?></span>
-			</a>
+	<?php endif; ?>
+	<div class="start-wikia-container table-cell">
+		<a href="<?= htmlspecialchars( $createWikiUrl ) ?>"
+		   class="start-wikia"
+		   title="<?= wfMessage( 'global-navigation-create-wiki' )->escaped(); ?>"
+		   data-id="start-wikia">
+			<span><?= ( wfMessage( 'global-navigation-create-wiki' )->escaped() ); ?></span>
+		</a>
+	</div>
+	<? if ( $isGameStarLogoEnabled ): ?>
+		<a class="gamestar-logo" href="http://gamestar.de/"></a>
+	<? endif; ?>
+	</div>
+	</nav>
+<?php else: ?>
+	<nav class="global-navigation-2016 global-navigation" id = "globalNavigation" >
+		<div class="global-navigation-container" >
+			<div class="wikia-logo-container table-cell" >
+				<a href = "<?= htmlspecialchars( $centralUrl ) ?>"
+					class="wikia-logo"
+					rel = "nofollow"
+					data - id = "wikia-logo" >
+					<img src = "<?= $wg->BlankImgUrl ?>"
+						height = "24"
+						width = "91"
+						alt = "<?= wfMessage( 'oasis-global-page-header' )->escaped() ?>"
+						title = "<?= wfMessage( 'oasis-global-page-header' )->escaped() ?>" >
+				</a >
+			</div >
+		<div class="search-container table-cell">
+			<?= $app->renderView('GlobalNavigation', 'searchIndex'); ?>
 		</div>
-		<? if ( $isGameStarLogoEnabled ): ?>
+		<div class="account-navigation-container table-cell">
+			<?= $app->renderView('GlobalNavigationAccountNavigation', 'index') ?>
+		</div>
+		<?php if (!$isAnon && $notificationsEnabled): ?>
+			<div class="notifications-container table-cell" id="notificationsEntryPoint">
+				<?= $app->renderView('GlobalNavigationWallNotifications', 'Index'); ?>
+			</div>
+		<?php endif; ?>
+		<? if ($isGameStarLogoEnabled): ?>
 			<a class="gamestar-logo" href="http://gamestar.de/"></a>
 		<? endif; ?>
-	</div>
-</nav>
+		</div>
+	</nav>
+<? endif; ?>
