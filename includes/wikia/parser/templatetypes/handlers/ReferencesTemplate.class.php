b80a2b92688a572ea9662c457954570a1bd3df39
Sebastian Marzjan <sebastianm@wikia-inc.com>
2016-01-11 10:58:47 +0100

    WIP

--- includes/wikia/parser/templatetypes/handlers/ReferencesTemplate.class.php
+++ includes/wikia/parser/templatetypes/handlers/ReferencesTemplate.class.php
@@ -2,11 +2,25 @@
 
 class ReferencesTemplate {
 	/**
-	 * @desc return simple <references /> parser tag
-	 *
+	 * @desc return simple <references /> parser tag if the original text contains it
+	 * @param $text string
 	 * @return string
 	 */
-	public static function handle() {
-		return '<references />';
+	public static function handle( $text ) {
+		$error_setting = libxml_use_internal_errors( true );
+
+		$dom = new DOMDocument();
+		$dom->loadHTML( '<body>' . $text . '</body>');
+		$xpath = new DOMXPath($dom);
+		$references = $xpath->query('//references');
+
+		libxml_clear_errors();
+		libxml_use_internal_errors( $error_setting );
+
+		if ( $references->length > 0 ) {
+			return '<references />';
+		}
+
+		return $text;
 	}
 }
