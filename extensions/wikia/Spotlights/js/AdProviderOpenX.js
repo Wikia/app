c03b9b53e8cde722e7b28fa4f425d88ec9d5a616
jacek <jacek@wikia-inc.com>
2016-01-18 17:10:01 +0100

    ADEN-2688 cleanup

--- extensions/wikia/Spotlights/js/AdProviderOpenX.js
+++ extensions/wikia/Spotlights/js/AdProviderOpenX.js
@@ -1,41 +1,55 @@
 var AdProviderOpenX = {
 	url : '',
-	url2 : '',
 	defaultHttpBase: 'http://spotlights.wikia.net/',
 	reviveHttpBase: 'http://spotlights-revive.wikia.net/'
 };
 
-AdProviderOpenX.getUrl2 = function() {
-
-	var httpBase = window.wgEnableReviveSpotlights ? AdProviderOpenX.reviveHttpBase : AdProviderOpenX.defaultHttpBase,
-		url = httpBase + "spc.php";
-		url += "?zones=14|15|16|17|18|19|20|21|22";
-		url += "&source=";
-		url += "&r=" + Math.floor(Math.random()*99999999);
-		url += "&loc=" + escape(window.location);
+AdProviderOpenX.getConfig = function() {
+	var config =  {
+		'zones':'14|15|16|17|18|19|20|21|22',
+		'source': '',
+		'r': Math.floor(Math.random()*99999999),
+		'loc': escape(window.location),
+		'target': '_top',
+		'cb': Math.floor(Math.random()*99999999),
+		'hub': window.wgWikiVertical,
+		'skin_name': window.skin,
+		'cont_lang': window.wgContentLanguage,
+		'user_lang': window.wgUserLanguage,
+		'dbname': wgDBname,
+		'tags': escape(window.wgWikiFactoryTagNames.join(",")),
+		'block': '1'
+	};
 	if (typeof document.referrer !== "undefined") {
-		url += "&referer=" + escape(document.referrer);
+		config.referer = escape(document.referrer);
 	}
-		url += "&target=_top";
-		url += "&cb=" + Math.floor(Math.random()*99999999);
-		url += "&hub=" + window.wgWikiVertical;
-		url += "&skin_name=" + skin;
-		url += "&cont_lang=" + wgContentLanguage;
-		url += "&user_lang=" + wgUserLanguage;
-		// varnish handles this url += "&browser_lang=X-BROWSER";
-		url += "&dbname=" + wgDBname;
-		url += "&tags=" + escape(wgWikiFactoryTagNames.join(","));
-		url += "&block=1";
-		url += (document.charset ? '&charset='+document.charset : (document.characterSet ? '&charset='+document.characterSet : ''));
-
-	AdProviderOpenX.url2 = url;
-
-	return AdProviderOpenX.url2;
+	if (document.charset || document.characterSet) {
+		config.charset = document.charset ? document.charset : document.characterSet;
+	}
+	return config;
 };
+
+AdProviderOpenX.getUrlParamsFromConfig = function(config) {
+	var params = [];
+	for (var key in config) {
+		if (config.hasOwnProperty(key)) {
+			params.push(key + '=' + config[key]);
+		}
+	}
+	return params.join('&');
+};
+
+AdProviderOpenX.getUrl = function() {
+	var httpBase = window.wgEnableReviveSpotlights ? AdProviderOpenX.reviveHttpBase : AdProviderOpenX.defaultHttpBase,
+		url = httpBase + 'spc.php?' + AdProviderOpenX.getUrlParamsFromConfig(AdProviderOpenX.getConfig());
+	AdProviderOpenX.url = url;
+	return AdProviderOpenX.url;
+};
+
 
 if (!window.wgNoExternals && window.wgEnableOpenXSPC && !window.wgIsEditPage && !window.navigator.userAgent.match(/sony_tvs/)) {
 	jQuery(function($) {
-		$.getScript(AdProviderOpenX.getUrl2(), function() {
+		$.getScript(AdProviderOpenX.getUrl(), function() {
 			var lazy = new window.Wikia.LazyLoadAds();
 		});
 	});
