cbf68aaaac124cfd6411fbe9de44694ab4651f99
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-01-18 10:57:08 +0000

    CE-3199: Replace spaces with tabs

--- includes/wikia/services/usercommands/ShortcutsUserCommand.php
+++ includes/wikia/services/usercommands/ShortcutsUserCommand.php
@@ -3,19 +3,19 @@
  * Displays Global Shortcuts in toolbar
  */
 class ShortcutsUserCommand extends UserCommand {
-    
-    protected $overflow = false;
-    protected $defaultRenderType = 'link';
-    
-    protected function buildData() {
+
+	protected $overflow = false;
+	protected $defaultRenderType = 'link';
+
+	protected function buildData() {
 		$loggedIn = F::app()->wg->user->isLoggedIn();
 
-        $this->available = $loggedIn;
-        $this->enabled = $loggedIn;
-        $this->caption = wfMessage( 'global-shortcuts-name' )->escaped();
-    }
-    
-    protected function getTrackerName() {
-        return 'global-shortcuts-help-entry-point';
-    }
+		$this->available = $loggedIn;
+		$this->enabled = $loggedIn;
+		$this->caption = wfMessage( 'global-shortcuts-name' )->escaped();
+	}
+
+	protected function getTrackerName() {
+		return 'global-shortcuts-help-entry-point';
+	}
 }
