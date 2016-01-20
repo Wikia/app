996dfab9a5dfdb1a171c7a4461ccd482918fc010
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-01-20 14:30:59 +0100

    fallback

--- extensions/wikia/Search/classes/Result/ResultHelper.php
+++ extensions/wikia/Search/classes/Result/ResultHelper.php
@@ -17,6 +17,9 @@ class ResultHelper {
 			$imageFileName = (new PromoImage(PromoImage::MAIN))->setCityId($result['id'])->getPathname();
 			$imageURL = ImagesService::getImageSrcByTitle( $result['id'], $imageFileName,
 						WikiaSearchController::CROSS_WIKI_PROMO_THUMBNAIL_WIDTH, WikiaSearchController::CROSS_WIKI_PROMO_THUMBNAIL_HEIGHT );
+			if (! empty( $imageOriginalURL ) ) {
+				$imageURL = ImagesService::overrideThumbnailFormat($imageOriginalURL, ImagesService::EXT_JPG);
+			}
 
 		}//TODO: end
 
