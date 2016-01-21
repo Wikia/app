85a199fee6ad5f316c5ce83b22e771f424e28fbc
Kamil Koterba <kamil@wikia-inc.com>
2016-01-21 10:57:59 +0100

    CE-3206 Pulling first translations

--- extensions/wikia/GlobalShortcuts/GlobalShortcuts.i18n.php
+++ extensions/wikia/GlobalShortcuts/GlobalShortcuts.i18n.php
@@ -1,4 +1,6 @@
 <?php
+$messages = array();
+
 $messages['en'] = array(
 	'global-shortcuts-caption-action-list' => 'Actions explorer',
 	'global-shortcuts-caption-keyboard-shortcuts-help' => 'Keyboard shortcuts help',
@@ -35,4 +37,130 @@ $messages['qqq'] = array(
 	'global-shortcuts-welcome-hint' => 'One time hint shown tooltip above entrypoint to shortcuts help modal.',
 	'template-class-global-shortcuts-caption-classify-page' => 'This shortcut redirects users to the page where they can classify the template; it appears on template page.',
 	'template-class-global-shortcuts-press-to-explore-shortcuts' => '$1 is an html of . key. This sentence appears at the bottom of a keyboard shortcut help and it indicates that whenever user press a . key the actions explorer will appear.',
+);
+
+$messages['es'] = array(
+	'global-shortcuts-caption-action-list' => 'Explorador de acciones',
+	'global-shortcuts-caption-keyboard-shortcuts-help' => 'Ayuda sobre atajos de teclado',
+	'global-shortcuts-caption-open-discussions' => 'Abrir discusiones',
+	'global-shortcuts-caption-search-for-a-page' => 'Buscar una página',
+	'global-shortcuts-caption-start-a-new-wikia' => 'Crear una nueva wikia',
+	'global-shortcuts-category-current-page' => 'Página actual',
+	'global-shortcuts-category-current-wikia' => 'Wikia actual',
+	'global-shortcuts-category-global' => 'Global',
+	'global-shortcuts-category-help' => 'Ayuda',
+	'global-shortcuts-key-or' => 'o',
+	'global-shortcuts-key-then' => 'entonces',
+	'global-shortcuts-title-help-entry-point' => 'Abrir el diálogo de ayuda sobre atajos de teclado (?)',
+	'global-shortcuts-title-keyboard-shortcuts' => 'Atajos de teclado',
+	'template-class-global-shortcuts-caption-classify-page' => 'Clasificar la página',
+	'template-class-global-shortcuts-press-to-explore-shortcuts' => 'Pulse $1 para explorar atajos',
+);
+
+$messages['fr'] = array(
+	'global-shortcuts-caption-action-list' => "Explorateur d'actions",
+	'global-shortcuts-caption-keyboard-shortcuts-help' => 'Aide sur les raccourcis clavier',
+	'global-shortcuts-caption-open-discussions' => 'Discussions',
+	'global-shortcuts-caption-search-for-a-page' => 'Chercher une page',
+	'global-shortcuts-caption-start-a-new-wikia' => 'Créer un wikia',
+	'global-shortcuts-category-current-page' => 'Page en cours',
+	'global-shortcuts-category-current-wikia' => 'Wikia en cours',
+	'global-shortcuts-category-global' => 'Globales',
+	'global-shortcuts-category-help' => 'Aide',
+	'global-shortcuts-key-or' => 'ou',
+	'global-shortcuts-key-then' => 'puis',
+	'global-shortcuts-title-help-entry-point' => "Ouvrir la boîte de dialogue de l'aide sur les raccourcis clavier (?)",
+	'global-shortcuts-title-keyboard-shortcuts' => 'Raccourcis clavier',
+	'template-class-global-shortcuts-caption-classify-page' => 'Classification de la page',
+	'template-class-global-shortcuts-press-to-explore-shortcuts' => 'Appuyer sur $1 pour explorer les raccourcis',
+);
+
+$messages['it'] = array(
+	'global-shortcuts-caption-action-list' => 'Esplora le azioni',
+	'global-shortcuts-caption-keyboard-shortcuts-help' => 'Aiuto con le scorciatoie da tastiera',
+	'global-shortcuts-caption-open-discussions' => 'Vedi i forum',
+	'global-shortcuts-caption-search-for-a-page' => 'Cerca una pagina',
+	'global-shortcuts-caption-start-a-new-wikia' => 'Crea una nuova wikia',
+	'global-shortcuts-category-current-page' => 'Pagina corrente',
+	'global-shortcuts-category-current-wikia' => 'Wikia corrente',
+	'global-shortcuts-category-global' => 'Globale',
+	'global-shortcuts-category-help' => 'Aiuto',
+	'global-shortcuts-key-or' => 'o',
+	'global-shortcuts-key-then' => 'quindi',
+	'global-shortcuts-title-help-entry-point' => "Finestra di dialogo per aprire l'aiuto con le scorciatoie da tastiera (?)",
+	'global-shortcuts-title-keyboard-shortcuts' => 'Scorciatoie da tastiera',
+	'template-class-global-shortcuts-caption-classify-page' => 'Classifica la pagina',
+	'template-class-global-shortcuts-press-to-explore-shortcuts' => 'Premi $1 per esplorare le scorciatoie',
+);
+
+$messages['ja'] = array(
+	'global-shortcuts-caption-action-list' => '操作リスト',
+	'global-shortcuts-caption-keyboard-shortcuts-help' => 'キーボード・ショートカットのヘルプ',
+	'global-shortcuts-caption-open-discussions' => 'トークを開く',
+	'global-shortcuts-caption-search-for-a-page' => 'ページを検索',
+	'global-shortcuts-caption-start-a-new-wikia' => '新しいウィキアを始める',
+	'global-shortcuts-category-current-page' => '現在のページ',
+	'global-shortcuts-category-current-wikia' => '現在のウィキア',
+	'global-shortcuts-category-global' => 'グローバル',
+	'global-shortcuts-category-help' => 'ヘルプ',
+	'global-shortcuts-key-or' => 'または',
+	'global-shortcuts-key-then' => 'に続いて',
+	'global-shortcuts-title-help-entry-point' => 'キーボード・ショートカットのヘルプダイアログを開く（?）',
+	'global-shortcuts-title-keyboard-shortcuts' => 'キーボード・ショートカット',
+	'template-class-global-shortcuts-caption-classify-page' => 'ページを分類',
+	'template-class-global-shortcuts-press-to-explore-shortcuts' => '$1キーを押すとショートカットを探せます',
+);
+
+$messages['pt'] = array(
+	'global-shortcuts-caption-action-list' => 'Explorador de ações',
+	'global-shortcuts-caption-keyboard-shortcuts-help' => 'Ajuda para os atalhos de teclado',
+	'global-shortcuts-caption-open-discussions' => 'Discussões abertas',
+	'global-shortcuts-caption-search-for-a-page' => 'Buscar páginas',
+	'global-shortcuts-caption-start-a-new-wikia' => 'Começar uma wikia nova',
+	'global-shortcuts-category-current-page' => 'Página atual',
+	'global-shortcuts-category-current-wikia' => 'Wikia atual',
+	'global-shortcuts-category-global' => 'Global',
+	'global-shortcuts-category-help' => 'Ajuda',
+	'global-shortcuts-key-or' => 'ou',
+	'global-shortcuts-key-then' => 'em seguida',
+	'global-shortcuts-title-help-entry-point' => 'Abrir o diálogo de ajuda para atalhos de teclado (?)',
+	'global-shortcuts-title-keyboard-shortcuts' => 'Atalhos de teclado',
+	'template-class-global-shortcuts-caption-classify-page' => 'Classificar a página',
+	'template-class-global-shortcuts-press-to-explore-shortcuts' => 'Pressione $1 para explorar atalhos',
+);
+
+$messages['zh-hans'] = array(
+	'global-shortcuts-caption-action-list' => '操作管理器',
+	'global-shortcuts-caption-keyboard-shortcuts-help' => '键盘快捷方式帮助',
+	'global-shortcuts-caption-open-discussions' => '公开讨论',
+	'global-shortcuts-caption-search-for-a-page' => '网页搜索',
+	'global-shortcuts-caption-start-a-new-wikia' => '开始创建维基',
+	'global-shortcuts-category-current-page' => '当前页',
+	'global-shortcuts-category-current-wikia' => '当前Wikia社区',
+	'global-shortcuts-category-global' => '全球',
+	'global-shortcuts-category-help' => '帮助',
+	'global-shortcuts-key-or' => '或',
+	'global-shortcuts-key-then' => '然后',
+	'global-shortcuts-title-help-entry-point' => '打开键盘快捷方式帮助对话框 (?)',
+	'global-shortcuts-title-keyboard-shortcuts' => '键盘快捷键',
+	'template-class-global-shortcuts-caption-classify-page' => '分类页',
+	'template-class-global-shortcuts-press-to-explore-shortcuts' => '点击$1查找快捷方式',
+);
+
+$messages['zh-hant'] = array(
+	'global-shortcuts-caption-action-list' => '操作管理器',
+	'global-shortcuts-caption-keyboard-shortcuts-help' => '鍵盤快速鍵説明',
+	'global-shortcuts-caption-open-discussions' => '公開討論',
+	'global-shortcuts-caption-search-for-a-page' => '網頁搜索',
+	'global-shortcuts-caption-start-a-new-wikia' => '開始創建維基',
+	'global-shortcuts-category-current-page' => '當前頁',
+	'global-shortcuts-category-current-wikia' => '當前Wikia社區',
+	'global-shortcuts-category-global' => '全球',
+	'global-shortcuts-category-help' => '幫助',
+	'global-shortcuts-key-or' => '或',
+	'global-shortcuts-key-then' => '然後',
+	'global-shortcuts-title-help-entry-point' => '打開鍵盤快速鍵幫助對話框 (?)',
+	'global-shortcuts-title-keyboard-shortcuts' => '鍵盤快速鍵',
+	'template-class-global-shortcuts-caption-classify-page' => '分類頁',
+	'template-class-global-shortcuts-press-to-explore-shortcuts' => '按一下$1查找快捷方式',
 );
