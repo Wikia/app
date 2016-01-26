b4611915b9320fe34c10ff2a370f22f56bf736ec
Piotr Gabryjeluk <rychu@wikia-inc.com>
2016-01-26 13:57:20 -0800

    SEO-246 Don't publish any more language links to lilly
    Processing the queue is disabled because of an issue found, so right now
    lilly serves as a read-only database of starwars links only.
    
    This means we don't need to hook to the links processing and post
    anymore interlanguage links to lilly.

--- extensions/wikia/Lilly/Lilly.setup.php
+++ extensions/wikia/Lilly/Lilly.setup.php
@@ -2,9 +2,4 @@
 
 // Autoload
 $wgAutoloadClasses['Lilly'] = __DIR__ . '/Lilly.class.php';
-$wgAutoloadClasses['LillyHooks'] = __DIR__ . '/LillyHooks.class.php';
 $wgAutoloadClasses['LillyValidator'] = __DIR__ . '/LillyValidator.class.php';
-
-// Hooks
-$wgHooks['LinkerMakeExternalLink'][] = 'LillyHooks::onLinkerMakeExternalLink';
-$wgHooks['LinkEnd'][] = 'LillyHooks::onLinkEnd';
