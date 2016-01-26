7dba3c354308021bca4429a1566da9c6de447aa4
RafalWilinski <tho.ornee@gmail.com>
2016-01-26 10:07:35 +0100

    Name change

--- extensions/wikia/ImageReviewMercury/templates/ImageReviewMercury_index.php
+++ extensions/wikia/ImageReviewMercury/templates/ImageReviewMercury_index.php
@@ -1,2 +1,2 @@
-<iframe src="/image-review?useskin=wikiamobile&fullscreen=true" id="CuratedContentToolIframe"' +
+<iframe src="/image-review?useskin=wikiamobile&fullscreen=true" id="ImageReviewToolIframe"' +
 'class="curated-content-tool" name="curated-content-tool" style="height:100vw; width: 100vh;"></iframe>
