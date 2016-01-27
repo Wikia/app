d55024df744061587db9b620795045ed677aaa09
Per Johan Groland <per@akafugu.jp>
2016-01-27 19:16:36 +0900

    INT-322 New Upload Image / Video dialogs. Optimizely A/B test integration

---
+++ extensions/VisualEditor/wikia/modules/ve/init/ve.init.wikia.Optimizely.js
@@ -0,0 +1,32 @@
+/*!
+ * Utility functions for running Optimizely A/B tests with VisualEditor
+ */
+
+/**
+ * Gets Optimizely ID for Toolbar A/B test
+ * @returns {string}
+ */
+ve.init.wikia.getToolbarABTestId = function () {
+	return window.wgDevelEnvironment ? '4721410313' : '4701112678';
+};
+
+/**
+ * @returns {number} The A/B test variant number for the Toolbar A/B test
+ */
+ve.init.wikia.getToolbarABTestVariantNumber = function () {
+	var optimizely = window.optimizely,
+		optimizelyId = ve.init.wikia.getToolbarABTestId();
+
+	if (optimizely && optimizely.variationMap && optimizely.variationMap.hasOwnProperty(optimizelyId)) {
+		return optimizely.variationMap[optimizelyId] || 0;
+	}
+
+	return 0;
+};
+
+/**
+ * Hook for activating experiment on VE load
+ */
+mw.hook( 've.activate' ).add( function () {
+	window.optimizely.push(['activate', ve.init.wikia.getToolbarABTestId()]);
+});
