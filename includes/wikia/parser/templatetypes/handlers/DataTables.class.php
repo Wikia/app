261494ce0532c66531c7241bc6da9496cf33d865
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-01-27 11:53:50 +0100

    comment

--- includes/wikia/parser/templatetypes/handlers/DataTables.class.php
+++ includes/wikia/parser/templatetypes/handlers/DataTables.class.php
@@ -15,7 +15,7 @@ class DataTables {
 		wfProfileIn( __METHOD__ );
 		//check for tables
 		if ( static::shouldBeProcessed() ) {
-			// marks wikitext tables
+			// marks wikitext tables, omits {{{{{|subst:}}} cases by checking if there is only one '{' before '|'
 			if ( preg_match_all( "/^(.*[^\\{])?\\{\\|(.*)/\n", $wikitext, $wikiTables ) ) {
 				for ( $i = 0; $i < count( $wikiTables[ 0 ] ); $i++ ) {
 					$wikitext = static::markTable( $wikitext, $wikiTables[ 0 ][ $i ], $wikiTables[ 2 ][ $i ], '{|' );
