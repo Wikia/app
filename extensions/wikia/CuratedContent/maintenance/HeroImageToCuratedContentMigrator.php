7e9c800be8946f1bf8446a9ed4ebc817a1a46dd4
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-02-01 11:10:57 +0100

    comments

--- extensions/wikia/CuratedContent/maintenance/HeroImageToCuratedContentMigrator.php
+++ extensions/wikia/CuratedContent/maintenance/HeroImageToCuratedContentMigrator.php
@@ -5,6 +5,8 @@
 
 require_once( dirname( __FILE__ ) . '/../../../../maintenance/Maintenance.php' );
 
+//Migrates image and description from hero module to curated content.
+
 class HeroImageToCuratedContentMigrator extends Maintenance {
 
 	function execute() {
@@ -15,11 +17,13 @@ class HeroImageToCuratedContentMigrator extends Maintenance {
 		$wikiData = new WikiDataModel(Title::newMainPage()->getText());
 		$wikiData->getFromProps();
 
+		// migrate description if there is no description in community data
 		if (empty($commData->getCommunityDescription()) && !empty($wikiData->description)) {
 			$curatedData['community_data']['description'] = $wikiData->description;
 		}
 
 		$heroImage = Title::newFromText($wikiData->getImageName(), NS_IMAGE);
+		// migrate image if there is no image in community data
 		if (empty($commData->getCommunityImageId()) && !empty($heroImage)) {
 			$curatedData['community_data']['image_id'] = $heroImage->getArticleID();
 		}
