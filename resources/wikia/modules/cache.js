2d0b1e73fcf00d62354f0e05d8a985139e2dff5e
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2016-02-01 13:47:07 +0100

    XW-1036 | Add explanation and link to tickets

--- resources/wikia/modules/cache.js
+++ resources/wikia/modules/cache.js
@@ -26,6 +26,16 @@
 			// Trying to access storage with cookies disabled can throw
 			// security exceptions in some browsers like Firefox (BugId:94924)
 			try {
+				/**
+				 * Note:
+				 * in some browsers (eg. WebView for Android, Chrome with disabled
+				 * local storage) `windows.localStorage` exists and it's null;
+				 * in those cases simple `localStorage = window.localStorage;` check
+				 * won't throw exception. This will be thrown later, when trying to
+				 * access methods from it.
+				 * https://wikia-inc.atlassian.net/browse/XW-1036
+				 * https://wikia-inc.atlassian.net/browse/XW-1062
+				 */
 				window.localStorage.setItem('foo', 'bar');
 				window.localStorage.getItem('foo');
 
