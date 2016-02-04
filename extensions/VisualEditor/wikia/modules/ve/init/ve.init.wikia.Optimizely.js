20b030fee026a9d3f721312151a8a139d84922d1
Per Johan Groland <per@akafugu.jp>
2016-02-04 13:36:07 +0900

    INT-322 Simplify ve.init.wikia.getToolbarABTestVariantNumber

--- extensions/VisualEditor/wikia/modules/ve/init/ve.init.wikia.Optimizely.js
+++ extensions/VisualEditor/wikia/modules/ve/init/ve.init.wikia.Optimizely.js
@@ -2,18 +2,16 @@
  * Utility functions for running Optimizely A/B tests with VisualEditor
  */
 
-( function () {
-	/**
-	 * @returns {number} The A/B test variant number for the Toolbar A/B test
-	 */
-	ve.init.wikia.getToolbarABTestVariantNumber = function () {
-		var optimizely = window.optimizely,
-			optimizelyId = window.wgDevelEnvironment ? '4721410313' : '4701112678';
+/**
+ * @returns {number} The A/B test variant number for the Toolbar A/B test
+ */
+ve.init.wikia.getToolbarABTestVariantNumber = function () {
+	var optimizely = window.optimizely,
+		optimizelyId = window.wgDevelEnvironment ? '4721410313' : '4701112678';
 
-		if (optimizely && optimizely.variationMap && optimizely.variationMap.hasOwnProperty(optimizelyId)) {
-			return optimizely.variationMap[optimizelyId] || 0;
-		}
+	if (optimizely && optimizely.variationMap && optimizely.variationMap.hasOwnProperty(optimizelyId)) {
+		return optimizely.variationMap[optimizelyId] || 0;
+	}
 
-		return 0;
-	};
-}() );
+	return 0;
+};
