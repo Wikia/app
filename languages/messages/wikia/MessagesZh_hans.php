e7e4fd8ec545f37d7871604c6d2fc06ce84fc93c
adamkarminski <adamk@wikia-inc.com>
2016-02-08 11:30:53 +0100

    CE-3086 Add translations of the read-only message

--- languages/messages/wikia/MessagesZh_hans.php
+++ languages/messages/wikia/MessagesZh_hans.php
@@ -7,4 +7,7 @@ $messages = array_merge( $messages, array(
 
 'group-content-moderator' => '内容版主',
 'group-content-moderator-member' => '内容版主',
+
+	'wikia-read-only-mode' => '很抱歉！当前数据中心不可用，只能显示为备份状态。这意味着您可能无法执行某些操作，例如编辑、上传文件或在线聊天。
++我们正在努力解决这个问题，请您耐心等待！如果您遇到超时浏览，请发送邮件至community@wikia.com与我们联系。',
 ) );
