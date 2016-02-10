e26f9a0cf43ea3d8e80f9e672cf2684363b47989
Paul Oslund <poslund@wikia-inc.com>
2016-02-10 16:37:53 +0000

    RECIRC-41: Render in-content Recirculation widget on correct pages

---
+++ extensions/wikia/Recirculation/js/incontent.js
@@ -0,0 +1,217 @@
+/*global require*/
+require([
+	'jquery',
+	'wikia.window',
+	'wikia.abTest',
+	'wikia.nirvana',
+	'wikia.mustache',
+	'ext.wikia.recirculation.templates.mustache',
+	'ext.wikia.recirculation.tracker'
+], function ($, w, abTest, nirvana, Mustache, templates, tracker) {
+	var $container = $('#mw-content-text');
+
+	function injectInContentWidget($container) {
+		var $links = $container.find('a'),
+			sections = buildSections($container.find('h2')),
+			width,
+			firstSuitableSection,
+			topTitles;
+
+		// If this page doesn't have enough content (either links or sections) we
+		// don't want to show this widget
+		if ($links.length < 8 || sections.length < 3) {
+			return;
+		}
+
+		// The idea is to show links above the first section under an infobox
+		width = $container.outerWidth();
+		firstSuitableSection = sections.find(function(item, index) {
+			return item.width === width;
+		});
+
+		if (!firstSuitableSection) {
+			return;
+		}
+
+		topTitles = findTopTitles($links);
+
+		if (topTitles.length < 3) {
+			return;
+		}
+
+		nirvana.sendRequest({
+			controller: 'ArticlesApi',
+			method: 'getDetails',
+			format: 'json',
+			type: 'get',
+			data: {
+				titles: topTitles.join(','),
+				abstract: 0,
+				width: 50,
+				height: 50
+			},
+			callback: renderArticles(firstSuitableSection)
+		});
+	}
+
+	function renderArticles(section) {
+		var placeholderImage = w.wgExtensionsPath + '/wikia/Recirculation/images/placeholder.png';
+		tracker.trackImpression('in-content');
+
+		return function (response) {
+			var items = [],
+				html;
+
+			$.each(response.items, function(index, item) {
+				item.thumbnail = item.thumbnail || placeholderImage;
+				items.push(item);
+			});
+
+			$html = $(Mustache.render(templates['inContent.client'], {
+				title: $.msg('recirculation-incontent-title'),
+				items: items
+			}));
+
+			section.$start.before($html);
+
+			$html.on('click', 'a', function() {
+				tracker.trackClick('in-content');
+			});
+		}
+	}
+
+	/**
+	 * Calculate the section height before or after injecting an ad to it
+	 *
+	 * @param {Section} section
+	 * @returns {number}
+	 */
+	function getSectionHeight(section) {
+		var $firstElement = section.$ad || section.$firstElement,
+			firstElementOffset = $firstElement.offset(),
+			offsetTop = firstElementOffset && firstElementOffset.top,
+			offsetBottom;
+
+		if (offsetTop === undefined) {
+			return 0;
+		}
+
+		if (section.$end.length) {
+			offsetBottom = section.$end.offset().top;
+		} else {
+			offsetBottom = $container.offset().top + $container.outerHeight(true);
+		}
+
+		return offsetBottom - offsetTop;
+	}
+
+	/**
+	 * Build the sections array
+	 *
+	 * DOM/layout querying: OK
+	 * DOM modification:    forbidden
+	 *
+	 * @param {jQuery} $headers headers dividing the article to sections
+	 * @returns {Section[]}
+	 */
+	function buildSections($headers) {
+		var i,
+			len,
+			sections = [],
+			intro,
+			$start,
+			$end,
+			$firstElement,
+			section;
+
+		for (i = 0, len = $headers.length; i < len + 1; i += 1) {
+			intro = (i === 0);
+			$start = !intro && $headers.eq(i - 1);
+			$end = $headers.eq(i);
+			$firstElement = intro ? $container.children().first() : $start.next();
+			// Elements with display: none don't alter the layout
+			while ($firstElement.css('display') === 'none') {
+				$firstElement = $firstElement.next();
+			}
+			section = {
+				intro: intro,
+				name: intro ? 'TOP' : $start.children().first().text(),
+				nextName: $end.length ? $end.children().first().text() : 'BOTTOM',
+				$start: intro ? undefined : $start,
+				$end: $end,
+				$firstElement: $firstElement,
+				firstElementFloat: $firstElement.css('float')
+			};
+
+			section.height = getSectionHeight(section);
+			section.width = $start && $start.outerWidth();
+
+			sections.push(section);
+		}
+
+		return sections;
+	}
+
+	function findTopTitles($links) {
+		var links = buildLinks($links),
+			titles = getSortedKeys(links),
+			topTitles = [],
+			i = 0;
+
+		while (topTitles.length < 3) {
+			if (titles[i]) {
+				topTitles.push(titles[i]);
+			}
+			i ++;
+		}
+
+		return topTitles;
+	}
+
+	function validLink(element) {
+	    // Not a link to current article
+	    if (element.title === w.wgTitle) {
+	    	return false;
+	    }
+
+	    // This is a pretty heavy handed Regex, and it may only work for EN communities
+	    // but the idea is to not display links to special pages
+	    if (element.title.match(/\S:\S/)) {
+	    	return false;
+	    }
+
+		return true;
+	}
+
+	function buildLinks($links) {
+		var links = [];
+
+		$links.each(function(index, element) {
+			if (validLink(element)) {
+				links[element.title] = links[element.title] || 0;
+				links[element.title] ++;
+			}
+		});
+
+		return links;
+	}
+
+	function getSortedKeys(obj) {
+		var keys = [];
+		for(var key in obj) {
+			keys.push(key);
+		}
+
+		var sortedKeys = keys.sort(function(a,b){
+			return obj[b] - obj[a];
+		});
+
+		return sortedKeys;
+	}
+
+	// if (abTest.inGroup('RECIRCULATION_INCONTENT', 'YES')) {
+		$(document).ready( function() {
+			injectInContentWidget($container);
+		});
+	// }
+});
