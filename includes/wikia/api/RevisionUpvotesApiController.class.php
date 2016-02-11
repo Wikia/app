e901aaa1d1363b02b057b0de3078dd74782ef451
Kamil Koterba <kamil@wikia-inc.com>
2016-02-11 18:29:03 +0100

    CE-3359 adding DB queries to get upvotes data

--- includes/wikia/api/RevisionUpvotesApiController.class.php
+++ includes/wikia/api/RevisionUpvotesApiController.class.php
@@ -28,14 +28,15 @@ class RevisionUpvotesApiController extends WikiaApiController {
 		}
 
 		$upvotes = new RevisionUpvotesService();
-		$upvotesCount = $upvotes->getCount( $wikiId, $revisionId, $userId );
+		$upvotesCount = $upvotes->getCount( $wikiId, $revisionId );
+		$userUpvoted = $upvotes->userUpvoted( $wikiId, $revisionId, $userId );
 
 		$this->setResponseData( [
 			'wiki_id' => $wikiId,
 			'revision_id' => $revisionId,
-			'upvotes_count' => $upvotesCount['count'],
+			'upvotes_count' => $upvotesCount,
 			'user_id' => $userId,
-			'user_upvoted' => $upvotesCount['user_upvoted'],
+			'user_upvoted' => $userUpvoted,
 		] );
 	}
 }
--- includes/wikia/services/RevisionUpvotesService.class.php
+++ includes/wikia/services/RevisionUpvotesService.class.php
@@ -1,20 +1,51 @@
 <?php
 
-/**
- * This is a mock right now
- * TODO implement model CE-3359
- */
 class RevisionUpvotesService {
+
+	/**
+	 * @param int $wikiId
+	 * @param int $revisionId
+	 */
+	public function getCount( $wikiId, $revisionId ) {
+		$db = $this->getDatabaseForRead();
+
+		/* @var ResultWrapper $wrap  */
+		$wrap = ( new \WikiaSQL() )
+			->SELECT( 'count(*)' )->AS_( 'upvotes_count' )
+			->FROM( 'upvote_revisions' )
+			->WHERE( 'wiki_id' )->EQUAL_TO( $wikiId )
+			->AND_( 'revision_id' )->EQUAL_TO( $revisionId )
+			->run( $db );
+
+		return $wrap->current()->upvotes_count;
+	}
 
 	/**
 	 * @param int $wikiId
 	 * @param int $revisionId
 	 * @param int $userId
 	 */
-	public function getCount( $wikiId, $revisionId, $userId = 0 ) {
-		return [
-			'count' => 128,
-			'user_upvoted' => true
-		];
+	public function userUpvoted( $wikiId, $revisionId, $userId ) {
+		$db = $this->getDatabaseForRead();
+
+		/* @var ResultWrapper $wrap  */
+		$wrap = ( new \WikiaSQL() )
+			->SELECT( 'upvote_id' )
+			->FROM( 'upvote_revisions' )
+			->WHERE( 'wiki_id' )->EQUAL_TO( $wikiId )
+			->AND_( 'revision_id' )->EQUAL_TO( $revisionId )
+			->AND_( 'from_user' )->EQUAL_TO( $userId )
+			->LIMIT( 1 )
+			->run( $db );
+
+		return (bool)$wrap->current();
+	}
+
+	/**
+	 * Connects to a database with an intent of performing SELECT queries
+	 * @return \DatabaseBase
+	 */
+	protected function getDatabaseForRead() {
+		return wfGetDB( DB_SLAVE, [], F::app()->wg->UpvotesDB );
 	}
 }
