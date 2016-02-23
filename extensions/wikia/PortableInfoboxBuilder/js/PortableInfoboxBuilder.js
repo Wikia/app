9a7f4e84883f346f49150bf558f7c895f590378b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-02-23 14:47:32 +0100

    basic browser dialog

--- extensions/wikia/PortableInfoboxBuilder/js/PortableInfoboxBuilder.js
+++ extensions/wikia/PortableInfoboxBuilder/js/PortableInfoboxBuilder.js
@@ -3,7 +3,11 @@
 require(['ponto', 'jquery'], function (ponto, $) {
 	var $infoboxBuilderIframe = $('#infoboxBuilderIframe');
 
-	if ($infoboxBuilderIframe.length) {
+    $( window ).on('beforeunload', function() {
+        return "If you leave this page, all your unsaved changes will be lost.";
+    });
+
+    if ($infoboxBuilderIframe.length) {
 		ponto.setTarget(
 			Ponto.TARGET_IFRAME,
 			window.location.origin,
