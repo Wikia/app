2399b6fd2e76c010f008ece0ffb6915b74665e5a
jacek <jacek@wikia-inc.com>
2016-02-25 15:47:43 +0100

    ADEN-2979 sourcepoint cs

---
+++ extensions/wikia/ARecoveryEngine/ARecoveryEngineApiController.class.php
@@ -0,0 +1,17 @@
+<?php
+class ARecoveryEngineApiController extends WikiaController {
+
+	const DEFAULT_TEMPLATE_ENGINE = WikiaResponse::TEMPLATE_ENGINE_MUSTACHE;
+
+	public function getDelivery() {
+
+		$resourceLoader = new ResourceLoaderAdEngineSourcePointCSDelivery();
+		$resourceLoaderContext = new ResourceLoaderContext( new ResourceLoader(), new FauxRequest());
+		$source = $resourceLoader->getScript($resourceLoaderContext);
+
+
+		//TODO: check the caching
+		$this->response->setContentType( 'text/javascript; charset=utf-8' );
+		$this->response->setVal('code', $source);
+	}
+}
