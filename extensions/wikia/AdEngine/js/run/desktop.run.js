3b961c9f9cd9bb1529db73046a1767ed07e66673
jacek <jacek@wikia-inc.com>
2016-02-26 16:21:57 +0100

    ADEN-2979 yet another logging

--- extensions/wikia/AdEngine/js/run/desktop.run.js
+++ extensions/wikia/AdEngine/js/run/desktop.run.js
@@ -84,6 +84,8 @@ require([
 		if (context.opts.sourcePointRecovery && win.ads) {
 			win.ads.runtime.sp.slots = win.ads.runtime.sp.slots || [];
 			recoveryHelper.addOnBlockingCallback(function () {
+				console.log("AD_BLOCKING_CALLBACK");
+				console.log(win.ads.runtime.sp.slots);
 				adEngineRunner.run(adConfigDesktop, win.ads.runtime.sp.slots, 'queue.sp', false);
 			});
 		}
