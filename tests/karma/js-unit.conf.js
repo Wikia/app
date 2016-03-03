05df20007f64e0afb9b269b4d9d999bc6880deb0
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-03 17:06:31 +0100

    DAT-3793, add tests for helper

--- tests/karma/js-unit.conf.js
+++ tests/karma/js-unit.conf.js
@@ -200,7 +200,11 @@ module.exports = function (config) {
 
 			// Recirculation
 			'extensions/wikia/Recirculation/js/*.js',
-			'extensions/wikia/Recirculation/js/spec/**/*.spec.js'
+			'extensions/wikia/Recirculation/js/spec/**/*.spec.js',
+
+			//PortableInfoboxBuilder
+			'extensions/wikia/PortableInfoboxBuilder/js/PortableInfoboxBuilderTemplateClassificationHelper.js',
+			'extensions/wikia/PortableInfoboxBuilder/js/spec/PortableInfoboxBuilderTemplateClassificationHelper.spec.js',
 		]
 	});
 };
