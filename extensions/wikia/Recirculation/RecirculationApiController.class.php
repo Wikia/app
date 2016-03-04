3be2b1e0bad8d54c3c727830518ac889ae3b8068
Paul Oslund <poslund@wikia-inc.com>
2016-03-04 20:31:08 +0000

    RECIRC-66: Add Recirculation API Method for Fandom Data

---
+++ extensions/wikia/Recirculation/RecirculationApiController.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class RecirculationApiController extends WikiaApiController {
+	const ALLOWED_TYPES = ['popular', 'shares', 'recent_popular'];
+
+	public function getFandomPosts() {
+		$type = $this->request->getVal( 'type' );
+		if ( !$type || !in_array( $type, self::ALLOWED_TYPES ) ) {
+			throw new InvalidParameterApiException( 'type' );
+		}
+
+		$fandomDataService = new FandomDataService();
+
+		$posts = $fandomDataService->getPosts( $this->type );
+
+		$this->response->setCacheValidity( WikiaResponse::CACHE_VERY_SHORT );
+		$this->response->setData( [
+			'title' => wfMessage( 'recirculation-fandom-title' )->plain(),
+			'posts' => $posts,
+		] );
+	}
+}
