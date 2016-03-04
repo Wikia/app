573900d87c975f6d8af8d93f8c065852dabe7e6d
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-03-04 12:03:29 +0100

    SUS-281 Restrict array results type in BaseAdapter

--- extensions/wikia/ExactTargetUpdates/client/adapters/BaseAdapter.class.php
+++ extensions/wikia/ExactTargetUpdates/client/adapters/BaseAdapter.class.php
@@ -4,7 +4,7 @@ namespace Wikia\ExactTarget;
 
 abstract class BaseAdapter {
 
-	public function __construct($results ) {
+	public function __construct( array $results ) {
 		foreach ( $results as $result ) {
 			if ( $result->Properties instanceof \stdClass ) {
 				$this->extractResult( $result->Properties->Property );
