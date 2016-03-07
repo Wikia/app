e65434b469f15078e6db0fd4b86499860a84cbd2
jacek <jacek@wikia-inc.com>
2016-03-07 18:21:55 +0100

    ADEN-2983 uncomment CORP_*

--- extensions/wikia/AdEngine/js/provider/directGpt.js
+++ extensions/wikia/AdEngine/js/provider/directGpt.js
@@ -20,7 +20,7 @@ define('ext.wikia.adEngine.provider.directGpt', [
 				size: '728x90,1030x130,1030x65,1030x250,970x365,970x250,970x90,970x66,970x180,980x150',
 				loc: 'top'
 			},
-			//CORP_TOP_RIGHT_BOXAD:       {size: '300x250,300x600,300x1050', loc: 'top'},
+			CORP_TOP_RIGHT_BOXAD:       {size: '300x250,300x600,300x1050', loc: 'top'},
 			EXIT_STITIAL_BOXAD_1:       {size: '300x250,600x400,800x450,550x480', loc: 'exit'},
 			GPT_FLUSH:                  {flushOnly: true},
 			HOME_TOP_LEADERBOARD:       {
