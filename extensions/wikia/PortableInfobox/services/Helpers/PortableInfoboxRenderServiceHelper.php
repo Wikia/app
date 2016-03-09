2c3353541398ce34b938052eb51db57f70ff8f02
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-09 10:23:55 +0100

    DAT-3855, add comment about changing image thumbnail width

--- extensions/wikia/PortableInfobox/services/Helpers/PortableInfoboxRenderServiceHelper.php
+++ extensions/wikia/PortableInfobox/services/Helpers/PortableInfoboxRenderServiceHelper.php
@@ -8,6 +8,7 @@ class PortableInfoboxRenderServiceHelper {
 	const LOGGER_LABEL = 'portable-infobox-render-not-supported-type';
 	//todo: https://wikia-inc.atlassian.net/browse/DAT-3075
 	//todo: figure out what to do when user changes default infobox width via custom theming
+	// changed value from 270 to 300 to support default theme europa width
 	const DESKTOP_THUMBNAIL_WIDTH = 300;
 	const MOBILE_THUMBNAIL_WIDTH = 360;
 	const MINIMAL_HERO_IMG_WIDTH = 300;
