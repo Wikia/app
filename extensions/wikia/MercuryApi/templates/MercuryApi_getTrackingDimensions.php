eb2a7923dcc6848e6187778f3be58045c14fe7c6
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-03-10 12:51:48 +0100

    wrap values in quotes

--- extensions/wikia/MercuryApi/templates/MercuryApi_getTrackingDimensions.php
+++ extensions/wikia/MercuryApi/templates/MercuryApi_getTrackingDimensions.php
@@ -1,4 +1,5 @@
+window.trackingDimensions = [];
 <?php
 foreach( $dimensions as $key => $value ) {
-	echo "window.trackingDimensions[$key] = $value;\n";
+	echo "window.trackingDimensions[$key] = '$value';\n";
 }
