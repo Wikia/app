cf861baa5610f45dfe90d3763c9e18a57740f9bc
macbre <maciej.brencz@gmail.com>
2016-03-11 11:52:11 +0100

    AsyncTaskList: catch all AMQP exceptions

--- includes/wikia/tasks/AsyncTaskList.class.php
+++ includes/wikia/tasks/AsyncTaskList.class.php
@@ -13,7 +13,7 @@ use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Exception\AMQPRuntimeException;
-use PhpAmqpLib\Exception\AMQPTimeoutException;
+use PhpAmqpLib\Exception\AMQPExceptionInterface;
 use Wikia\Logger\WikiaLogger;
 use Wikia\Tasks\Queues\ParsoidPurgePriorityQueue;
 use Wikia\Tasks\Queues\ParsoidPurgeQueue;
@@ -273,8 +273,7 @@ class AsyncTaskList {
 	 *
 	 * @param AMQPChannel $channel channel to publish messages to, if part of a batch
 	 * @return string the task list's id
-	 * @throws \PhpAmqpLib\Exception\AMQPRuntimeException
-	 * @throws \PhpAmqpLib\Exception\AMQPTimeoutException
+	 * @throws AMQPExceptionInterface
 	 */
 	public function queue( AMQPChannel $channel = null ) {
 		global $wgUser;
@@ -321,9 +320,7 @@ class AsyncTaskList {
 				$connection = $this->connection();
 				$channel = $connection->channel();
 				$channel->basic_publish( $message, '', $this->getQueue()->name() );
-			} catch ( AMQPRuntimeException $e ) {
-				$exception = $e;
-			} catch ( AMQPTimeoutException $e ) {
+			} catch ( AMQPExceptionInterface $e ) {
 				$exception = $e;
 			}
 
@@ -360,8 +357,7 @@ class AsyncTaskList {
 
 	/**
 	 * @return AMQPConnection connection to message broker
-	 * @throws AMQPRuntimeException
-	 * @throws AMQPTimeoutException
+	 * @throws AMQPExceptionInterface
 	 */
 	protected function connection() {
 		if ( $this->connection == null ) {
@@ -437,9 +433,7 @@ class AsyncTaskList {
 
 		try {
 			$connection = self::getConnection();
-		} catch ( AMQPRuntimeException $e ) {
-			return $logError( $e );
-		} catch ( AMQPTimeoutException $e ) {
+		} catch ( AMQPExceptionInterface $e ) {
 			return $logError( $e );
 		}
 
@@ -454,9 +448,7 @@ class AsyncTaskList {
 
 		try {
 			$channel->publish_batch();
-		} catch ( AMQPRuntimeException $e ) {
-			$exception = $e;
-		} catch ( AMQPTimeoutException $e ) {
+		} catch ( AMQPExceptionInterface $e ) {
 			$exception = $e;
 		}
 
