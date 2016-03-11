8baf0e2b5da7b915ee4ea1f2d6bfcd574d17b692
Owen Davis <owen@wikia-inc.com>
2016-03-10 16:05:15 -0800

    fix spacing

--- includes/wikia/services/tests/ApiServiceTest.php
+++ includes/wikia/services/tests/ApiServiceTest.php
@@ -33,19 +33,19 @@ class ApiAccessServiceTest extends \WikiaBaseTest {
 		#turn off special case url building inside getHostByDbName
 		$this->mockGlobalVariable("wgDevelEnvironment", false);
 
-		# user data doesn't matter, just make sure the function is called and the data is passed along to Http::get
+		# user data doesn't matter, just make sure loginAsJson is called and the data is passed along to Http::get
 		$fakeUserData = array("curlOptions" => "COOKIE_STUFF");
 		$this->mockStaticMethod("ApiService", "loginAsUser", $fakeUserData );
 
 		$fakeJson = '{"a": "b"}'; // any non-null json formatted data
 		$fakeUrl = "foo.wikia.com/wikia.php?format=json";  // with our fake data, this is the fake url the code builds
-        $mockHttp = $this->getStaticMethodMock( 'Http', 'get' );
-        $mockHttp->expects( $this->once() )
-        		->method('get')
-        		->with($fakeUrl, 'default', $fakeUserData )
-        		->will($this->returnValue($fakeJson));
+		$mockHttp = $this->getStaticMethodMock( 'Http', 'get' );
+		$mockHttp->expects( $this->once() )
+				->method('get')
+				->with($fakeUrl, 'default', $fakeUserData )
+				->will($this->returnValue($fakeJson));
 
-        // test that calling with setUser==true calls loginAsUser and passes the returned data to Http::get as $options
+		// test that calling with setUser==true calls loginAsUser and passes the returned data to Http::get as $options
 		$result = ApiService::foreignCall("foo", array(), "wikia.php", true);
 		$this->assertEquals($result, json_decode($fakeJson, true));
 	}
