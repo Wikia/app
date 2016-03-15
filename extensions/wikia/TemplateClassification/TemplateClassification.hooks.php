4db1a13b35064ca5799548dde12a6e0f72f4a7cb
Diana Falkowska <diana.falkowska@gmail.com>
2016-03-15 16:25:42 +0100

    fix indent

--- extensions/wikia/TemplateClassification/TemplateClassification.hooks.php
+++ extensions/wikia/TemplateClassification/TemplateClassification.hooks.php
@@ -299,8 +299,8 @@ class Hooks {
 		global $wgEnablePortableInfoboxBuilderExt;
 
 		return $wgEnablePortableInfoboxBuilderExt
-		&& $context->getTitle()->getArticleID() === 0
-		&& empty( $types['current'] && $types['new'] )
-		&& !\PortableInfoboxBuilderHelper::isForcedSourceMode( $context->getRequest() );
+			&& $context->getTitle()->getArticleID() === 0
+			&& empty( $types['current'] && $types['new'] )
+			&& !\PortableInfoboxBuilderHelper::isForcedSourceMode( $context->getRequest() );
 	}
 }
