f565bc96db0c13b50216b20905cb43c245b8cadf
adamkarminski <adamk@wikia-inc.com>
2016-03-15 11:51:34 +0100

    CE-3486 Change former int values to strings and make them required

--- includes/wikia/api/UserFeedbackStorageApiController.class.php
+++ includes/wikia/api/UserFeedbackStorageApiController.class.php
@@ -8,10 +8,10 @@ class UserFeedbackStorageApiController extends WikiaApiController {
 	/**
 	 * Saves user feedback to a temporary table in the portable_flags db
 	 *
-	 * @requestParam int wikiId
-	 * @requestParam int pageId
-	 * @requestParam int experimentId
-	 * @requestParam int variationId
+	 * @requestParam int wikiId (required)
+	 * @requestParam string pageTitle (required)
+	 * @requestParam string experimentId (required)
+	 * @requestParam string variationId (required)
 	 * @requestParam int userId
 	 * @requestParam string feedback
 	 * @requestParam int feedback_impression_count
@@ -50,25 +50,30 @@ class UserFeedbackStorageApiController extends WikiaApiController {
 	 */
 	private function getRequestParams( IRequest $request ) {
 		$requestParams = [];
-		$positiveIntRequired = [
+		$requiredParams = [
 			'experimentId' => 'experiment_id',
-			'wikiId' => 'wiki_id',
-			'pageId' => 'page_id',
+			'pageTitle' => 'page_title',
+			'variationId' => 'variation_id',
 		];
 
-		foreach ( $positiveIntRequired as $paramName => $dbFieldName ) {
-			$paramValue = $request->getInt( $paramName );
+		foreach ( $requiredParams as $paramName => $dbFieldName ) {
+			$paramValue = $request->getVal( $paramName );
 
-			if ( $paramValue === 0 ) {
+			if ( empty( $paramValue ) ) {
 				throw new MissingParameterApiException( $paramName );
 			} else {
 				$requestParams[$dbFieldName] = $paramValue;
 			}
 		}
 
+		$wikiId = $request->getInt( 'wikiId' );
+		if ( $wikiId === 0 ) {
+			throw new MissingParameterApiException( 'wikiId' );
+		}
+
 		$requestParams = $requestParams + [
+				'wiki_id' => $wikiId,
 				'user_id' => $this->getContext()->getUser()->getId(),
-				'variation_id' => $request->getInt( 'variationId' ),
 				'feedback' => $request->getVal( 'feedback', '' ),
 				'feedback_impressions_count' => $request->getInt( 'feedbackImpressionsCount' ),
 				'feedback_previous_count' => $request->getInt( 'feedbackPreviousCount' ),
