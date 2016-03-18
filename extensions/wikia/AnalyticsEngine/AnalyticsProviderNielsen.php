d279ca68e3a2d271506bdcea5efe4453dd083706
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-03-18 11:28:20 +0100

    ADEN-3072 Split nielsen to head and body parts

--- extensions/wikia/AnalyticsEngine/AnalyticsProviderNielsen.php
+++ extensions/wikia/AnalyticsEngine/AnalyticsProviderNielsen.php
@@ -3,32 +3,46 @@
 class AnalyticsProviderNielsen implements iAnalyticsProvider {
 
 	private static $apid = 'T26086A07-C7FB-4124-A679-8AC404198BA7';
+	private static $bodyTemplate = 'extensions/wikia/AnalyticsEngine/templates/nielsen.body.mustache';
+	private static $headTemplate = 'extensions/wikia/AnalyticsEngine/templates/nielsen.head.mustache';
 	private static $libraryUrl = 'http://secure-dcr-cert.imrworldwide.com/novms/js/2/ggcmb500.js';
-	private static $template = 'extensions/wikia/AnalyticsEngine/templates/nielsen.mustache';
 
 	function getSetupHtml( $params=array() ) {
 		return null;
 	}
 
-	public function trackEvent($event, $eventDetails=array()){
-		return '';
+	function trackEvent( $event, $eventDetails = array() ) {
+		global $wgCityId, $wgDBname;
+
+		if ( !self::isEnabled() ) {
+			return '<!-- Nielsen is disabled -->';
+		}
+
+		switch ( $event ) {
+			case AnalyticsEngine::EVENT_PAGEVIEW:
+				return \MustacheService::getInstance()->render(
+					self::$bodyTemplate,
+					[
+						'appId' => self::$apid,
+						'section' => HubService::getVerticalNameForComscore( $wgCityId ),
+						'dbName' => $wgDBname
+					]
+				);
+			default:
+				return '<!-- Unsupported event for Nielsen -->';
+		}
 	}
 
 	static function onWikiaSkinTopScripts(&$vars, &$scripts, $skin) {
-		global $wgCityId, $wgDBname;
-
 		if (!self::isEnabled()) {
 			$scripts .= '<!-- Nielsen is disabled -->';
 			return true;
 		}
 
 		$scripts .= \MustacheService::getInstance()->render(
-			self::$template,
+			self::$headTemplate,
 			[
-				'url' => self::$libraryUrl,
-				'appId' => self::$apid,
-				'section' => HubService::getVerticalNameForComscore( $wgCityId ),
-				'dbName' => $wgDBname
+				'url' => self::$libraryUrl
 			]
 		);
 
