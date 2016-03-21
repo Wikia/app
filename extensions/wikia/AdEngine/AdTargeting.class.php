5583814568fa65294fe3dcda8c07b10821c95b04
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-03-21 15:05:29 +0100

    ADEN-3094 Prepare ESRB rating filter

---
+++ extensions/wikia/AdEngine/AdTargeting.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class AdTargeting {
+
+	const ADULTS_ONLY = 'ao';
+	const EARLY_CHILDHOOD = 'ec';
+	const EVERYONE = 'everyone';
+	const EVERYONE_10_PLUS = 'ec10';
+	const MATURE = 'mature';
+	const RATING_PENDING = 'rp';
+	const TEEN = 'teen';
+
+	static private $esrbRating = [
+		self::EARLY_CHILDHOOD => 1,
+		self::EVERYONE => 2,
+		self::EVERYONE_10_PLUS => 3,
+		self::TEEN => 4,
+		self::MATURE => 5,
+		self::ADULTS_ONLY => 6,
+		self::RATING_PENDING => 7
+	];
+
+	static public function getEsrbRating() {
+		global $wgDartCustomKeyValues, $wgWikiDirectedAtChildrenByFounder, $wgWikiDirectedAtChildrenByStaff;
+
+		$directedAtChildren = $wgWikiDirectedAtChildrenByFounder || $wgWikiDirectedAtChildrenByStaff;
+		$rating = $directedAtChildren ? self::EARLY_CHILDHOOD : self::TEEN;
+
+		$pairs = explode(';', $wgDartCustomKeyValues);
+
+		$dartRating = null;
+		foreach ($pairs as $pair) {
+			list($key, $value) = explode('=', $pair);
+			if ($key === 'esrb' && ($dartRating === null || self::$esrbRating[$value] > self::$esrbRating[$dartRating])) {
+				$dartRating = $value;
+			}
+		}
+
+		return $dartRating !== null ? $dartRating : $rating;
+	}
+
+}
---
+++ extensions/wikia/AdEngine/tests/AdTargetingTest.php
@@ -0,0 +1,56 @@
+<?php
+
+class AdTargetingTest extends WikiaBaseTest {
+
+	public function setUp() {
+		global $IP, $wgDartCustomKeyValues, $wgWikiDirectedAtChildrenByFounder, $wgWikiDirectedAtChildrenByStaff;
+		$wgDartCustomKeyValues = null;
+		$wgWikiDirectedAtChildrenByFounder = false;
+		$wgWikiDirectedAtChildrenByStaff = false;
+
+		$this->setupFile = "$IP/extensions/wikia/AdEngine/AdEngine2.setup.php";
+		parent::setUp();
+	}
+
+	public function testWikiNotDirectedAtChildrenAndNoDartParams() {
+		$this->assertEquals(AdTargeting::TEEN, AdTargeting::getEsrbRating());
+	}
+
+	public function testWikiDirectedAtChildrenByFounderAndNoDartParams() {
+		$this->mockGlobalVariable('wgWikiDirectedAtChildrenByFounder', true);
+
+		$this->assertEquals(AdTargeting::EARLY_CHILDHOOD, AdTargeting::getEsrbRating());
+	}
+
+	public function testWikiDirectedAtChildrenByStaffAndNoDartParams() {
+		$this->mockGlobalVariable('wgWikiDirectedAtChildrenByStaff', true);
+
+		$this->assertEquals(AdTargeting::EARLY_CHILDHOOD, AdTargeting::getEsrbRating());
+	}
+
+	public function testWikiNotDirectedAtChildrenAndDartParamsWithoutEsrb() {
+		$this->mockGlobalVariable('wgDartCustomKeyValues', 'key1=value1;key2=value2');
+
+		$this->assertEquals(AdTargeting::TEEN, AdTargeting::getEsrbRating());
+	}
+
+	public function testWikiNotDirectedAtChildrenAndOverrideRatingByDartParams() {
+		$this->mockGlobalVariable('wgDartCustomKeyValues', 'key1=value1;esrb=mature');
+
+		$this->assertEquals(AdTargeting::MATURE, AdTargeting::getEsrbRating());
+	}
+
+	public function testOverrideRatingByDartParams() {
+		$this->mockGlobalVariable('wgDartCustomKeyValues', 'key1=value1;esrb=everyone');
+
+		$this->assertEquals(AdTargeting::EVERYONE, AdTargeting::getEsrbRating());
+	}
+
+	public function testPickBiggestRating() {
+		$this->mockGlobalVariable('wgWikiDirectedAtChildrenByStaff', true);
+		$this->mockGlobalVariable('wgDartCustomKeyValues', 'esrb=mature;key1=value1;esrb=everyone');
+
+		$this->assertEquals(AdTargeting::MATURE, AdTargeting::getEsrbRating());
+	}
+
+}
