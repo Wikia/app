71ede635105f83181105307f4ac918585d38de39
saipetch <saipetch@wikia-inc.com>
2016-03-25 14:28:47 -0700

    SEO (SEO-308): RobotsTxt - add functions to set custom rules and block discussion user pages

--- wikia-robots-txt.php
+++ wikia-robots-txt.php
@@ -102,11 +102,17 @@ if ( !$allowRobots ) {
 	$robots->blockRobot( 'NPBot' );
 	$robots->blockRobot( 'WebReaper' );
 
+	// path
+	$robots->disallowPath( '/d/u/' );
+
 	// Deprecated items, probably we should delete them
 	$robots->disallowPath( '/w/' );
 	$robots->disallowPath( '/trap/' );
 	$robots->disallowPath( '/dbdumps/' );
 	$robots->disallowPath( '/wikistats/' );
+
+	// custom rules
+	$robots->setCustomRules();
 }
 
 foreach ( $robots->getHeaders() as $header ) {
