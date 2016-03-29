97373e2f5e217093b3308f662892b369a75734ba
macbre <maciej.brencz@gmail.com>
2016-03-29 13:12:17 +0200

    MWMemcached: log "too large value" errors as errors

--- includes/objectcache/MemcachedClient.php
+++ includes/objectcache/MemcachedClient.php
@@ -1201,7 +1201,7 @@ class MWMemcached {
 		if ( $len > self::MEMCACHED_ITEM_MAX_SIZE - 2 ) {
 			// default item_max_size is 1mb, 2 characters are reserved for trailing "\r\n"
 			if ( class_exists( 'Wikia\\Logger\\WikiaLogger' ) ) {
-				\Wikia\Logger\WikiaLogger::instance()->debug( 'MemcachedClient: large value' , [
+				\Wikia\Logger\WikiaLogger::instance()->error( 'MemcachedClient: large value' , [
 					'exception' => new Exception(),
 					'key' => $key,
 					'len' => $len,
