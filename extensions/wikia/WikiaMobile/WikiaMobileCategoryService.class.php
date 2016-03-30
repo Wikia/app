58e7f3bc1bd050f0438c80d88f63187c735a11b6
Warkot <mateusz.warkocki@wikia-inc.com>
2016-03-30 19:33:47 +0200

    XW-1279 | dynamic collatorUcaDefaultStringCompare call

--- extensions/wikia/WikiaMobile/WikiaMobileCategoryService.class.php
+++ extensions/wikia/WikiaMobile/WikiaMobileCategoryService.class.php
@@ -106,7 +106,7 @@ class WikiaMobileCategoryService extends WikiaService {
 			$title = $categoryPage->getTitle();
 			$category = Category::newFromTitle( $title );
 			$collections = $this->model->getCollection( $category, $format );
-			uksort( $collections['items'], 'self::collatorUcaDefaultStringCompare' );
+			uksort( $collections['items'], [ $this, 'collatorUcaDefaultStringCompare' ] );
 
 			$this->response->setVal( 'total', $collections['count'] );
 			$this->response->setVal( 'collections', $collections['items'] );
