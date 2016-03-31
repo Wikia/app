b6374b509715c4bf4d13557ecd9ada5e4cb27825
lizlux <liz@lizlux.net>
2016-03-31 12:34:11 +0900

    JPN-360 adding basic modal to community page

--- extensions/wikia/CommunityPage/scripts/ext.communityPage.js
+++ extensions/wikia/CommunityPage/scripts/ext.communityPage.js
@@ -1,7 +1,50 @@
 require([
 	'jquery',
-], function ($) {
+	'wikia.ui.factory'
+], function ($, uiFactory) {
 	'use strict';
 
-	$(function () {});
+	var uiModalInstance;
+
+	function getUiModalInstance() {
+		var $deferred = $.Deferred();
+
+		if ( uiModalInstance ) {
+			$deferred.resolve(uiModalInstance);
+		} else {
+			uiFactory.init( [ 'modal' ]).then(function (uiModal){
+				uiModalInstance = uiModal;
+				$deferred.resolve(uiModalInstance);
+			});
+		}
+
+		return $deferred;
+	}
+
+	function openCommunityModal() {
+		getUiModalInstance().then( function( uiModal ) {
+			var createPageModalConfig = {
+				vars: {
+					id: 'CommunityPageModalDialog',
+					size: 'medium',
+					title: 'foo',
+					content: 'bar',
+					classes: [ 'modalContent' ]
+				}
+			};
+			uiModal.createComponent( createPageModalConfig, function( modal ) {
+				console.log(modal);
+
+				modal.show();
+			});
+		});
+	}
+
+	$(function () {
+		// prefetch UI modal on DOM ready
+		getUiModalInstance();
+
+		// test code to open modal on demand
+		window.openCommModal = openCommunityModal;
+	});
 });
