9b1ebb87fff2eb771de13302e5a9bb3d2e1732ae
Artur Klajnerok <a.klajnerok@gmail.com>
2016-04-01 16:11:36 -0700

    [SUS-374] Moved polish core wikia messages to extension format

--- extensions/GlobalMessages/GlobalMessages.i18n.php
+++ extensions/GlobalMessages/GlobalMessages.i18n.php
@@ -202,7 +202,7 @@ $1',
 	'aboutsite' => 'O {{GRAMMAR:MS.lp|{{SITENAME}}}}',
 	'aboutpage' => 'Project:O {{GRAMMAR:MS.lp|{{SITENAME}}}}',
 	'copyright' => 'Treść udostępniana na licencji $1.',
-	'copyrightpage' => '{{ns:project}}:Prawa_autorskie',
+	'copyrightpage' => 'w:Wikia:Licensing|Wikia:Licensing',
 	'currentevents' => 'Bieżące wydarzenia',
 	'currentevents-url' => 'Project:Aktualności',
 	'disclaimers' => 'Informacje prawne',
@@ -3262,4 +3262,230 @@ W przeciwnym wypadku można użyć prostego formularza poniżej. Komentarz zosta
 	'api-error-unknownerror' => 'Nieznany błąd: „$1”',
 	'api-error-uploaddisabled' => 'Na tej wiki przesyłanie zostało wyłączone.',
 	'api-error-verification-error' => 'Plik może być uszkodzony lub nazwa pliku ma nieprawidłowe rozszerzenie.',
+	'wg_lastwikis' => 'Ostatnio Odwiedzone',
+	'editedrecently' => 'Ostatnio zmieniane przez',
+	'nocontributors' => 'Aktualny artykuł nie posiada autorów',
+	'refreshpage' => 'Zawartość będzie widoczna po odświeżeniu strony',
+	'needhelp' => 'Potrzebna pomoc: Wyedytuj [[MediaWiki:needhelp|ten artykuł]], aby pokazać tu listę artykułów.',
+	'shoutbox' => 'Shout Box',
+	'send' => 'Wyślij',
+	'title' => 'Tytuł',
+	'wt_help_startup' => '|Poznałeś widgety?||Otwórz menu użytkownika i kliknij na "Manage widgets"...',
+	'wt_help_cockpit' => '|Kokpit||Złap miniaturkę widgeta i przeciągnij nad pole widgetów...',
+	'wt_help_sidebar' => '|Pole widgetów||Kliknij "edytuj", aby zmienić ustawienia widgetów. Widgety możesz zamykać klikając na ikonkę x',
+	'wt_countdown_give_date' => 'Proszę podać datę w formacie RRRR-MM-DD GG:MM:SS (np. 2007-03-28 13:56:00) lub RRRR-MM-DD (np. 2007-02-17) lub GG:MM:SS (17:01:00)',
+	'wt_countdown_show_seconds' => 'Pokazuj sekundy',
+	'widgetwikipage' => "Ten ''tekst'' jest '''prostym''' testem widżeta strona wiki. Możesz '''edytować zawartość widżeta''' [[Mediawiki:$1|edytując stronę]] w przestrzeni MediaWiki.",
+	'widget-wikipage-title' => 'Tytuł widżeta',
+	'widget-wikipage-source' => 'Strona źródłowa',
+	'widget-bookmark-empty' => 'Dodawaj swoje ulubione artykuły klikając na powyższą ikonkę',
+	'wt_shoutbox_initial_message' => 'Cześć... witaj na czacie!',
+	'wt_referers_empty_list' => 'Obecnie nie dysponujemy statystykami wymaganymi do utworzenia chmury stron odsyłających. Spróbuj później....',
+	'wt_show_referrers' => 'Pokaż statystyki',
+	'wt_show_period' => 'Wybierz okres statystyk',
+	'widgets' => 'Lista widgetów',
+	'widgets-specialpage-info' => 'Widżety działają najlepiej w jednej z najnowszych skórek, np. [{{SERVER}}/index.php?title=Special:Widgets&useskin=monaco Monaco].
+
+Zmień [[Special:Preferences#prefsection-1|swoje preferencje]], aby używać widżetów.',
+	'wt_click_to_close' => 'Kliknij, aby zamknąć dymek...',
+	'widget-title-activetalkpages' => 'Lista ostatnio edytowanych strony dyskusji',
+	'widget-desc-activetalkpages' => 'Aktywne strony dyskusji',
+	'widget-title-ancientpages' => 'Najstarsze strony',
+	'widget-desc-ancientpages' => 'Lista stron nieedytowanych przez dłuższy czas',
+	'widget-bookmark-title' => 'Zakładki',
+	'widget-desc-bookmark' => 'Zachowaj swoje ulubione strony',
+	'widget-title-contribs' => 'Wkład',
+	'widget-desc-contribs' => 'Pomocna lista ostatnich edycji',
+	'widget-title-editedrecently' => 'Ostatnio zmieniane przez',
+	'widget-desc-editedrecently' => 'Lista ostatnich edytorów tej strony',
+	'widget-title-lastwikis' => 'Ostatnio odwiedzone',
+	'widget-desc-lastwikis' => 'Lista ostatnio odwiedzonych Wikii',
+	'widget-title-mostvisited' => 'Najczęściej odwiedzane',
+	'widget-desc-mostvisited' => 'Lista najczęściej odwiedzanych artykułów',
+	'widget-title-needhelp' => 'Potrzebna pomoc',
+	'widget-desc-needhelp' => 'Wyświetla artykuły wymagające dopracowania',
+	'widget-title-newpages' => 'Najnowsze artykuły',
+	'widget-desc-newpages' => 'Lista najnowszych artykułów na tej wiki',
+	'widget-title-problemreports' => 'Zgłoszone problemy',
+	'widget-desc-problemreports' => 'Lista zgłoszonych problemów',
+	'widget-title-recentchanges' => 'Ostatnie zmiany',
+	'widget-desc-recentchanges' => 'Lista ostatnich zmian',
+	'widget-title-referers' => 'Strony odsyłające',
+	'widget-desc-referers' => 'Chmura stron odsylajacych na ta Wikie',
+	'widget-title-shoutbox' => 'Czat',
+	'widget-desc-shoutbox' => 'Zobacz kto jest online i rozmawiaj ze swoimi znajomymi',
+	'widget-title-slideshow' => 'Pokaz obrazów',
+	'widget-desc-slideshow' => 'Pokaz obrazów z tej wiki',
+	'widget-title-tips' => 'Czy wiesz, że...',
+	'widget-desc-tips' => 'Pokazuje losowe podpowiedzi',
+	'widget-title-topcontent' => 'Najlepsze na wiki',
+	'widget-desc-topcontent' => '4 w 1: Najpopularniejsze, Najczęściej odwiedzane, Ostatnie zmiany, Najwyżej ocenianie',
+	'widget-title-topusers' => 'Najaktywniejsi użytkownicy',
+	'widget-desc-topusers' => 'Lista najaktywniejszych użytkownikow na tej wiki',
+	'widget-title-topvoted' => 'Najwyżej oceniane',
+	'widget-desc-topvoted' => 'Lista najwyżej ocenionych artykułów na tej wiki',
+	'widget-title-watchlist' => 'Obserwowane',
+	'widget-desc-watchlist' => 'Lista obserwowanych stron na tej wiki',
+	'widget-title-wikiamessages' => 'Komunikaty Wikii',
+	'widget-title-wikiapartners' => 'Nasi partnerzy',
+	'widget-desc-wikiapartners' => 'Nasi partnerzy',
+	'widget-title-wikipage' => 'Strona wiki w widżecie',
+	'widget-desc-wikipage' => 'Umieszcza treść podanej strony wewnątrz widżeta',
+	'or' => 'lub',
+	'login_greeting' => 'Witamy w serwisie Wikia, [[User:$1|$1]]!',
+	'create_an_account' => 'Załóż konto',
+	'login_as_another' => 'Zaloguj się jako inny użytkownik',
+	'not_you' => 'Nie ty?',
+	'this_wiki' => 'ta wiki',
+	'home' => 'Strona główna',
+	'forum' => 'Forum',
+	'helpfaq' => 'Pomoc i FAQ',
+	'createpage' => 'Utwórz nowy artykuł',
+	'joinnow' => 'Dołącz teraz',
+	'most_popular_articles' => 'Najpopularniejsze artykuły',
+	'expert_tools' => 'zaawansowane narzędzia',
+	'this_article' => 'Ten artykuł',
+	'this_page' => 'ta strona',
+	'edit_contribute' => 'Edytuj / Rozwiń',
+	'discuss' => 'Dyskutuj',
+	'share_it' => 'Podziel się:',
+	'my_stuff' => 'moje rzeczy',
+	'choose_reason' => 'wybierz powód',
+	'top_five' => 'pierwsza piątka',
+	'most_popular' => 'Najpopularniejsze',
+	'most_visited' => 'Najczęściej odwiedzane',
+	'newly_changed' => 'Ostatnio zmienione',
+	'highest_ratings' => 'Najwyżej oceniane',
+	'most_emailed' => 'Najczęściej przesyłane',
+	'rate_it' => 'Oceń ten artykuł:',
+	'unrate_it' => 'Cofnij ocenę',
+	'use_old_formatting' => 'Przełącz na styl Monobook',
+	'use_new_formatting' => 'Wypróbuj nowe formatowanie',
+	'contris' => 'Edycje',
+	'watchlist_s' => 'Obserwowane',
+	'add_widgets' => 'Dodaj widgety',
+	'cockpit_hide' => 'Ukryj kokpit',
+	'widget_name' => 'Nazwa',
+	'widget_description' => 'Opis',
+	'see_more' => 'Więcej...',
+	'tog-disablelinksuggest' => 'Nie pokazuj sugestii linków',
+	'tog-enablerichtext' => 'Włącz edytor wizualny',
+	'nodiff' => 'Brak różnic',
+	'footer_1' => 'Pomóż $1',
+	'footer_1.5' => 'edytując tę stronę',
+	'footer_5' => '$1 zmienił tę stronę $2',
+	'footer_6' => 'Przejdź do losowego artykułu',
+	'footer_7' => 'Prześlij ten artykuł mailem',
+	'footer_8' => 'Podziel się tym artykułem',
+	'footer_9' => 'Oceń ten artykuł',
+	'addnewtalksection-link' => 'Dodaj nowy wątek.',
+	'rcshowhidelogs' => '$1 logi',
+	'hub-Entertainment' => 'Rozrywka',
+	'hub-Gaming' => 'Gry wideo',
+	'hub-Video_Games' => 'Gry wideo',
+	'hub-Lifestyle' => 'Lifestyle',
+	'hub-Wikia' => 'Wikia',
+	'vertical-tv' => 'TV',
+	'vertical-games' => 'Gry',
+	'vertical-books' => 'Książki',
+	'vertical-comics' => 'Komiksy',
+	'vertical-lifestyle' => 'Lifestyle',
+	'vertical-music' => 'Muzyka',
+	'vertical-movies' => 'Filmy',
+	'shared-Globalnavigation' => "*w:c:pl:Gry|Gry|video-games-vertical-color
+
+**w:c:pl:Gry|RPG
+***w:c:wiedzmin|Wiedźmin
+***w:c:pl.fallout|Fallout
+***w:c:pl.gothic|Gothic
+***w:c:pl.elderscrolls|Elder Scrolls
+***w:c:pl.risen|Risen
+
+**w:c:pl:Gry|Akcja
+***w:c:pl.callofduty|Call of Duty
+***w:c:pl.gta|Grand Theft Auto
+***w:c:pl.saintsrow|Saints Row
+***w:c:pl.assassinscreed|Assassin's Creed
+***w:c:pl.masseffect|Mass Effect
+
+**w:c:pl:Gry|Multiplayer
+***w:c:pl.wow|World of Warcraft
+***w:c:pl.rappelz|Rappelz
+***w:c:pl.leagueoflegends|League of Legends
+***w:c:pl.dota-2|Dota 2
+***w:c:pl.warframe|Warframe
+
+**w:c:pl:Gry|Pozostałe
+***w:c:pl.sims|The Sims
+***w:c:pl.dontstarve|Don't Starve
+***w:c:pl.slodkiflirt|Słodki Flirt
+***w:c:pl.patapon|Patapon
+***w:c:pl.bindingofisaac|Binding of Isaac
+
+*w:c:pl:Rozrywka|Rozrywka|entertainment-vertical-color
+
+**w:c:pl:Rozrywka|Filmy
+***w:c:pl.lotr|Władca Pierścieni
+***w:c:pl.harrypotter|Harry Potter
+***w:c:pl.polskapersopedia|Percy Jackson
+***w:c:pl.twilightsaga|Zmierzch
+***w:c:igrzyskasmierci|Igrzyska Śmierci
+
+**w:c:pl:Rozrywka|Seriale
+***w:c:pl.mlp|My Little Pony
+***w:c:pl.austinially|Austin i Ally
+***w:c:ranczo|Ranczo
+***w:c:pl.pora-na-przygode|Pora na Przygodę!
+***w:c:pl.nadzdolni|Nadzdolni
+
+**w:c:pl:Rozrywka|Anime
+***w:c:pl.naruto|Naruto
+***w:c:pl.bleach|Bleach
+***w:c:pl.dragonball|Dragon Ball
+***w:c:pl.fairytail|Fairy Tail
+***w:c:pl.onepiece|One Piece
+
+**w:c:pl:Rozrywka|Pozostałe
+***w:c:pl.ben10|Ben 10
+***w:c:pl.fineasziferb|Fineasz i Ferb
+***w:c:pl.pzm|Pingwiny z Madagaskaru
+***w:c:pl.slugterra|Slugterra
+***w:c:pl.yugioh|Yu-Gi-Oh!
+
+*w:c:pl:Lifestyle|Lifestyle|lifestyle-vertical-color
+
+**w:c:pl:Lifestyle|Miasta
+***w:c:warszawa|Warszawa
+***w:c:wroclaw|Wrocław
+***w:c:torun|Toruń
+***w:c:poznan|Poznań
+***w:c:krakow|Kraków
+
+**w:c:pl:Lifestyle|Kultura
+***w:c:wiersze|Wiersze
+***w:c:dziedzictwo|Dziedzictwo
+***w:c:pl.bractwoczarnegosztyletu|Bractwo Czarnego Sztyletu
+***w:c:pl.ogienilod|Pieśń Lodu i Ognia
+***w:c:pl.mitologia|Mitologia
+
+**w:c:pl:Lifestyle|Kreatywne
+***w:c:nonsensopedia|Nonsensopedia
+***w:c:pl.creepypasta|Creepypasta
+***w:c:bezsensopedia|Bezsensopedia
+***w:c:totaldramafanfick|Total Drama Polish FanFick
+***w:c:pl.bakuganfanfic|Bakugan Fan Fiction
+
+**w:c:pl:Lifestyle|Zainteresowania
+***w:c:pl.lego|Lego
+***w:c:egipt|Egipt
+***w:c:pl.monsterhigh|Monster High
+***w:c:pl.bionicle|Bionicle
+***w:c:pl.vocaloid|Vocaloid
+",
+	'tog-enableuserjs' => 'Włącz osobisty kod JavaScript ([[Pomoc:Osobiste_CSS_i_JS|Dowiedz się więcej]])',
+	'wikia-pagetitle' => '$1 – Wikia',
+	'group-content-moderator' => 'Moderatorzy treści',
+	'group-content-moderator-member' => 'Moderator treści',
+	'wikia-read-only-mode' => 'Ojej! Nasze główne centrum danych jest w tej chwili niedostępne, a to jest nasza kopia zapasowa. Niestety oznacza to, że możesz nie być w stanie wykonać niektórych czynności takich jak edycja, przesyłanie plików lub korzystanie z czatu.
+ +Pracujemy właśnie nad rozwiązeniem tego problemu i dziękujemy za cierpliwość! Jeśli ta wiadomość jest widoczna przez dłuższy okres czasu, możesz skontaktować się z Wikią poprzez community@wikia.com.',
 );
