f62109bf78b828d49a21291ea084d3b31770e0f3
ArturKlajnerok <a.klajnerok@gmail.com>
2016-04-01 20:31:40 +0000

    [SUS-374] New extensions for global messages

---
+++ extensions/GlobalMessages/GlobalMessages.i18n.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Internationalisation file for extension GlobalMessages.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+$messages = array();
+$messages['en'] = array(
+	'edit' => 'edit',
+);
+
+$messages['pl'] = array(
+        'edit' => 'edytuj',
+);
---
+++ extensions/GlobalMessages/GlobalMessages.php
@@ -0,0 +1,6 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) {
+	exit( 1 );
+}
+
+$wgExtensionMessagesFiles['GlobalMessages'] =  dirname( __FILE__ ) . '/GlobalMessages.i18n.php';
