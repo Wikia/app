2e889f5584edcf6179903384e79d35408eeda513
Rafal Leszczynski <rafal@wikia-inc.com>
2016-04-01 13:03:07 +0200

    CE-3566, render IB in iframe inside modal in VE WIP

--- extensions/VisualEditor/wikia/modules/ve/ui/dialogs/ve.ui.WikiaInfoboxBuilderDialog.js
+++ extensions/VisualEditor/wikia/modules/ve/ui/dialogs/ve.ui.WikiaInfoboxBuilderDialog.js
@@ -11,14 +11,14 @@
  * @constructor
  * @param {Object} [config] Configuration options
  */
-ve.ui.WikiaInfoboxBuilderDialog = function VeUiWikiaInfoboxBuilderDialog( config ) {
-    // Parent constructor
-    ve.ui.WikiaInfoboxBuilderDialog.super.call( this, config );
+ve.ui.WikiaInfoboxBuilderDialog = function VeUiWikiaInfoboxBuilderDialog(config) {
+	// Parent constructor
+	ve.ui.WikiaInfoboxBuilderDialog.super.call(this, config);
 };
 
 /* Inheritance */
 
-OO.inheritClass( ve.ui.WikiaInfoboxBuilderDialog, OO.ui.Dialog );
+OO.inheritClass(ve.ui.WikiaInfoboxBuilderDialog, OO.ui.Dialog);
 
 /* Static Properties */
 
@@ -31,39 +31,59 @@ ve.ui.WikiaInfoboxBuilderDialog.static.size = 'full';
 /**
  * @inheritdoc
  */
-ve.ui.WikiaInfoboxBuilderDialog.prototype.getSetupProcess = function ( data ) {
-    return ve.ui.WikiaInfoboxBuilderDialog.super.prototype.getSetupProcess.call( this, data )
-        .next( function () {
-            this.surface = data.surface;
-        }, this );
+ve.ui.WikiaInfoboxBuilderDialog.prototype.getSetupProcess = function (data) {
+	return ve.ui.WikiaInfoboxBuilderDialog.super.prototype.getSetupProcess.call(this, data)
+		.next(function () {
+			this.surface = data.surface;
+		}, this);
 };
 
 /**
  * @inheritdoc
  */
 ve.ui.WikiaInfoboxBuilderDialog.prototype.getBodyHeight = function () {
-    return 1000;
+	return 1000;
 };
 
 /* Methods */
 
 ve.ui.WikiaInfoboxBuilderDialog.prototype.initialize = function () {
-    // Parent method
-    ve.ui.WikiaInfoboxBuilderDialog.super.prototype.initialize.call( this );
+	var self = this;
 
-    // Content
-    this.content = new OO.ui.PanelLayout( { padded: true, expanded: false } );
-    this.content.$element.append( '<p>A simple dialog window. Press \'Esc\' to close.</p>' );
-    this.$body.append( this.content.$element );
+	// Parent method
+	ve.ui.WikiaInfoboxBuilderDialog.super.prototype.initialize.call(this);
+
+	require(['wikia.loader', 'wikia.mustache', 'wikia.location'], function (loader, mustache, location) {
+		loader({
+			type: loader.MULTI,
+			resources: {
+				mustache: 'extensions/wikia/PortableInfoboxBuilder/templates/PortableInfoboxBuilderSpecialController_builder.mustache',
+				scripts: 'portable_infobox_builder_js'
+			}
+		}).done(function (assets) {
+			var html = mustache.render(assets.mustache[0], {
+				iframeUrl: location.origin + '/infobox-builder/'
+			});
+
+			loader.processScript(assets.scripts);
+
+			// Content
+			self.content = new OO.ui.PanelLayout({padded: true, expanded: false});
+			self.content.$element.append(html);
+			self.$body.append(self.content.$element);
+		});
+	});
+
+
 };
 
 /**
  * @inheritdoc
  */
-ve.ui.WikiaInfoboxBuilderDialog.prototype.getReadyProcess = function ( data ) {
-    return ve.ui.WikiaInfoboxBuilderDialog.super.prototype.getReadyProcess.call( this, data );
+ve.ui.WikiaInfoboxBuilderDialog.prototype.getReadyProcess = function (data) {
+	return ve.ui.WikiaInfoboxBuilderDialog.super.prototype.getReadyProcess.call(this, data);
 };
 
 /* Registration */
 
-ve.ui.windowFactory.register( ve.ui.WikiaInfoboxBuilderDialog );
+ve.ui.windowFactory.register(ve.ui.WikiaInfoboxBuilderDialog);
