34bd666d05d1e2c5834f8f15e03d5ef3a3eaf756
macbre <maciej.brencz@gmail.com>
2016-04-01 15:34:28 +0200

    Revert "Use proxy when sending HTTP requests to production wikis from devboxes"
    This reverts commit d1eac01baaae5a22988f58f6bc07ead505ebc5e8.
    
    Conflicts:
    	extensions/wikia/Development/Configs_DevBox/LocalSettings.php

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -4029,9 +4029,6 @@ class Parser {
 
 		$text = Http::get( $url );
 		if ( !$text ) {
-			# wikia start
-			Wikia::log(__METHOD__, false, "Scary transclusion failed for <{$url}>");
-			# wikia end
 			return wfMsgForContent( 'scarytranscludefailed', $url );
 		}
 
