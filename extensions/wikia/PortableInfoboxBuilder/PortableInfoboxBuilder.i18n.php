04caa13e496d12107bc31cafe2c6bbabf7c9b873
idradm <robakadam@gmail.com>
2016-04-04 14:22:26 +0200

    DAT-3993 handle title conflicts on move action

--- extensions/wikia/PortableInfoboxBuilder/PortableInfoboxBuilder.i18n.php
+++ extensions/wikia/PortableInfoboxBuilder/PortableInfoboxBuilder.i18n.php
@@ -12,6 +12,7 @@ $messages['en'] = array(
 	'portable-infobox-builder-entry-point-modal-cancel-button' => 'Cancel',
 	'portable-infobox-builder-documentation-example-usage' => 'Example usage:',
 	'portable-infobox-builder-documentation-example-param' => 'Example',
+	'portable-infobox-builder-move-message' => 'Infobox Builder: infobox template renamed',
 );
 
 $messages['qqq'] = array(
@@ -25,4 +26,5 @@ $messages['qqq'] = array(
 	'portable-infobox-builder-create-template-entry-point-create-regular-template' => 'Button to create regular template with editor displayed on new template page',
 	'portable-infobox-builder-documentation-example-usage' => 'Information that infobox usage follows',
 	'portable-infobox-builder-documentation-example-param' => 'Literally \'example param value\'',
+	'portable-infobox-builder-move-message' => 'Move message displayed on revision history page',
 );
