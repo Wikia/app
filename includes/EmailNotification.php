dbaa0f416053bed18a3ea8093eff14918f452054
James Sutterfield <james@wikia-inc.com>
2016-04-06 15:19:43 -0700

    SOC-2289 add username as well

--- includes/EmailNotification.php
+++ includes/EmailNotification.php
@@ -440,6 +440,7 @@ class EmailNotification {
 				'summary' => $this->summary,
 				'action' => $this->action,
 				'subject' => $this->subject,
+				'user' => $user->getName(),
 				'exception' => new Exception()
 			] );
 			$this->sendUsingUserMailer( $user );
