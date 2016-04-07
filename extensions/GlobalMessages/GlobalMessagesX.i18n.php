cac9920af48aec8933915da82107789c42ae067d
Artur Klajnerok <a.klajnerok@gmail.com>
2016-04-07 09:41:59 +0200

    [SUS-374] Distributed global messages to multiple files

---
+++ extensions/GlobalMessages/GlobalMessagesX.i18n.php
@@ -0,0 +1,596 @@
+<?php
+/**
+ * Internationalisation file for extension GlobalMessages.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+$messages = array();
+
+$messages['en'] = array(
+	'xml-error-string' => '$1 at line $2, col $3 (byte $4): $5',
+);
+
+$messages['qqq'] = array(
+	'xml-error-string' => ':$1: Some kind of message, perhaps name of the error?
+:$2: line number
+:$3: columm number
+:$4: ?? $this->mByte . $this->mContext
+:$5: error description
+----
+:Example
+Import failed: XML import parse failure at line 1, col 1 (byte 3; "- <mediawiki xml"): Empty document',
+);
+
+$messages['af'] = array(
+	'xml-error-string' => '$1 op reël $2, kolom $3 (greep $4): $5',
+);
+
+$messages['an'] = array(
+	'xml-error-string' => '$1 en a linia $2, col $3 (byte $4): $5',
+);
+
+$messages['ar'] = array(
+	'xml-error-string' => '$1 عند السطر $2، العمود $3 (بايت $4): $5',
+);
+
+$messages['arz'] = array(
+	'xml-error-string' => '$1 عند السطر $2، العمود $3 (بايت $4): $5',
+);
+
+$messages['as'] = array(
+	'xml-error-string' => '$1 $2 শাৰীত, $3  কলামত (byte $4): $5',
+);
+
+$messages['ast'] = array(
+	'xml-error-string' => '$1 na llinia $2, col $3 (byte $4): $5',
+);
+
+$messages['avk'] = array(
+	'xml-error-string' => '$1 moe $2 conha, $3 briz ($4 byte): $5',
+);
+
+$messages['az'] = array(
+	'xml-error-string' => '$1 $2 sətrində, $3 sütünunda ($4 bayt): $5',
+);
+
+$messages['ba'] = array(
+	'xml-error-string' => '$2 юлда, $3 урында ($4 байт) $1: $5',
+);
+
+$messages['bcc'] = array(
+	'xml-error-string' => '$1 ته خط $2, ستون $3 (بایت $4): $5',
+);
+
+$messages['be'] = array(
+	'xml-error-string' => '$1 у радку $2, знак $3 (байт $4): $5',
+);
+
+$messages['be-tarask'] = array(
+	'xml-error-string' => '$1 у радку $2, пазыцыі $3 (байт $4): $5',
+);
+
+$messages['be-x-old'] = array(
+	'xml-error-string' => '$1 у радку $2, пазыцыі $3 (байт $4): $5',
+);
+
+$messages['bg'] = array(
+	'xml-error-string' => '$1 на ред $2, колона $3 (байт $4): $5',
+);
+
+$messages['bh'] = array(
+	'xml-error-string' => '$1 на ред $2, колона $3 (байт $4): $5',
+);
+
+$messages['bjn'] = array(
+	'xml-error-string' => '$1 pintang baris $2, kolom $3 (bita $4): $5',
+);
+
+$messages['bn'] = array(
+	'xml-error-string' => '$1 যে লাইনে $2, কলামে $3 (বাইট $4): $5',
+);
+
+$messages['br'] = array(
+	'xml-error-string' => '$1 war al linenn $2, bann $3 (okted $4) : $5',
+);
+
+$messages['bs'] = array(
+	'xml-error-string' => '$1 na liniji $2, kolona $3 (bajt $4): $5',
+);
+
+$messages['ca'] = array(
+	'xml-error-string' => '$1 a la línia $2, columna $3 (byte $4): $5',
+);
+
+$messages['cs'] = array(
+	'xml-error-string' => '$1 na řádku $2, sloupec $3 (bajt $4): $5',
+);
+
+$messages['cy'] = array(
+	'xml-error-string' => '$1 ar linell $2, col $3 (beit $4): $5',
+);
+
+$messages['da'] = array(
+	'xml-error-string' => '$1 på linje $2, kolonne $3 (byte $4): $5',
+);
+
+$messages['de'] = array(
+	'xml-error-string' => '$1 Zeile $2, Spalte $3, (Byte $4): $5',
+);
+
+$messages['de-weigsbrag'] = array(
+	'xml-error-string' => '$1 Dseil $2, Schbald $3, (Baid $4): $5',
+);
+
+$messages['diq'] = array(
+	'xml-error-string' => '$1 çizgi de $2 col $3 (bit $4): $5',
+);
+
+$messages['dsb'] = array(
+	'xml-error-string' => '$1 smužka $2, słup $3, (Byte $4): $5',
+);
+
+$messages['el'] = array(
+	'xml-error-string' => '$1 στη γραμμή $2, στήλη $3 (byte $4): $5',
+);
+
+$messages['eo'] = array(
+	'xml-error-string' => '$1 ĉe linio $2, kolumno $3 (bitiko $4): $5',
+);
+
+$messages['es'] = array(
+	'xml-error-string' => '$1 en la línea $2, col $3 (byte $4): $5',
+);
+
+$messages['et'] = array(
+	'xml-error-string' => '$1 real $2, tulbas $3 (bait $4): $5',
+);
+
+$messages['eu'] = array(
+	'xml-error-string' => '$1 $2 lerroan, $3 zutabean ($4 byte): $5',
+);
+
+$messages['ext'] = array(
+	'xml-error-string' => '$1 ena línia $2, col $3 (byte $4): $5',
+);
+
+$messages['fa'] = array(
+	'xml-error-string' => '$1 در سطر $2، ستون $3 (بایت $4): $5',
+);
+
+$messages['fi'] = array(
+	'xml-error-string' => '$1 rivillä $2, sarakkeessa $3 (tavu $4): $5',
+);
+
+$messages['fiu-vro'] = array(
+	'xml-error-string' => '$1 rivillä $2, sarakkeessa $3 (tavu $4): $5',
+);
+
+$messages['fr'] = array(
+	'xml-error-string' => '$1 à la ligne $2, colonne $3 (octet $4) : $5',
+);
+
+$messages['frp'] = array(
+	'xml-error-string' => '$1 a la legne $2, colona $3 (octèt $4) : $5',
+);
+
+$messages['fy'] = array(
+	'xml-error-string' => '$1 op regel $2, kolom $3 (byte $4): $5',
+);
+
+$messages['ga'] = array(
+	'xml-error-string' => '$1 ag líne $2, col $3 ($4 bearta): $5',
+);
+
+$messages['gan-hans'] = array(
+	'xml-error-string' => '$1 位到 $2 行，$3 列 （$4字节）：$5',
+);
+
+$messages['gan-hant'] = array(
+	'xml-error-string' => '$1 位到 $2 行，$3 列 （$4字節）：$5',
+);
+
+$messages['gl'] = array(
+	'xml-error-string' => '$1 na liña $2, col $3 (byte $4): $5',
+);
+
+$messages['grc'] = array(
+	'xml-error-string' => '$1 ἐις τὴν $2, στηλ. $3 (δυφίον $4): $5',
+);
+
+$messages['gsw'] = array(
+	'xml-error-string' => '$1 Zeile $2, Spalte $3, (Byte $4): $5',
+);
+
+$messages['gu'] = array(
+	'xml-error-string' => '$1  લીટી ક્ર્માંક $2, સ્તંભ  $3 (બાઇટ  $4): $5',
+);
+
+$messages['he'] = array(
+	'xml-error-string' => '$1 בשורה $2, עמודה $3 (בייט מספר $4): $5',
+);
+
+$messages['hi'] = array(
+	'xml-error-string' => '$1 कतार $2 में, कालम $3 (बाईट $4): $5',
+);
+
+$messages['hif'] = array(
+	'xml-error-string' => '$1 कतार $2 में, कालम $3 (बाईट $4): $5',
+);
+
+$messages['hif-latn'] = array(
+	'xml-error-string' => '$1 line $2 me, col $3 (byte $4): $5',
+);
+
+$messages['hr'] = array(
+	'xml-error-string' => '$1 u retku $2, stupac $3 (bajt $4): $5',
+);
+
+$messages['hsb'] = array(
+	'xml-error-string' => '$1 linka $2, špalta $3, (bajt $4): $5',
+);
+
+$messages['hu'] = array(
+	'xml-error-string' => '$1 a(z) $2. sorban, $3. oszlopban ($4. bájt): $5',
+);
+
+$messages['ia'] = array(
+	'xml-error-string' => '$1 al linea $2, col $3 (byte $4): $5',
+);
+
+$messages['id'] = array(
+	'xml-error-string' => '$1 pada baris $2, kolom $3 (bita $4): $5',
+);
+
+$messages['ie'] = array(
+	'xml-error-string' => '$1 in linea $2, col $3 (byte $4): $5',
+);
+
+$messages['ilo'] = array(
+	'xml-error-string' => '$1 iti linia $2, tukol $3 (byte $4): $5',
+);
+
+$messages['is'] = array(
+	'xml-error-string' => '$1 í línu $2, dálki $3 ($4 bæt): $5',
+);
+
+$messages['it'] = array(
+	'xml-error-string' => '$1 a riga $2, colonna $3 (byte $4): $5',
+);
+
+$messages['iu'] = array(
+	'xml-error-string' => '$1 a riga $2, colonna $3 (byte $4): $5',
+);
+
+$messages['ja'] = array(
+	'xml-error-string' => '$1、$2行の$3文字目（$4バイト目）：$5',
+);
+
+$messages['jv'] = array(
+	'xml-error-string' => '$1 ing baris $2, kolom $3 (bita $4): $5',
+);
+
+$messages['ka'] = array(
+	'xml-error-string' => '$1 ხაზში $2, პოზიციის $3 (ბაიტი $4): $5',
+);
+
+$messages['kk-arab'] = array(
+	'xml-error-string' => '$1 ٴنومىر $2 جولدا, باعان $3 (بايت $4): $5',
+);
+
+$messages['kk-cn'] = array(
+	'xml-error-string' => '$1 ٴنومىر $2 جولدا, باعان $3 (بايت $4): $5',
+);
+
+$messages['kk-cyrl'] = array(
+	'xml-error-string' => '$1 нөмір $2 жолда, баған $3 (байт $4): $5',
+);
+
+$messages['kk-kz'] = array(
+	'xml-error-string' => '$1 нөмір $2 жолда, баған $3 (байт $4): $5',
+);
+
+$messages['kk-latn'] = array(
+	'xml-error-string' => '$1 nömir $2 jolda, bağan $3 (baýt $4): $5',
+);
+
+$messages['kk-tr'] = array(
+	'xml-error-string' => '$1 nömir $2 jolda, bağan $3 (baýt $4): $5',
+);
+
+$messages['km'] = array(
+	'xml-error-string' => '$1 នៅ ជួរដេក $2, ជួរឈរ $3 (បៃ $4) ៖ $5',
+);
+
+$messages['ko'] = array(
+	'xml-error-string' => '$3단 $2줄 (바이트 $4)에서 $1: $5',
+);
+
+$messages['ko-kp'] = array(
+	'xml-error-string' => '$3단 $2줄 (바이트 $4)에서 $1: $5',
+);
+
+$messages['krc'] = array(
+	'xml-error-string' => '$1 тизгинде $2, позицияда $3 (байт $4): $5',
+);
+
+$messages['ksh'] = array(
+	'xml-error-string' => '$1 — en {{PLURAL:$2|eetz|$2-}}te Reih en de {{PLURAL:$3|eetz|$3-}}te Spalde, dat ess_et {{PLURAL:$4|eetz|$4-}}te Byte: $5',
+);
+
+$messages['ku'] = array(
+	'xml-error-string' => '$1 — en {{PLURAL:$2|eetz|$2-}}te Reih en de {{PLURAL:$3|eetz|$3-}}te Spalde, dat ess_et {{PLURAL:$4|eetz|$4-}}te Byte: $5',
+);
+
+$messages['la'] = array(
+	'xml-error-string' => '$1 in linea $2 et columna $3 (octetus $4): $5',
+);
+
+$messages['lb'] = array(
+	'xml-error-string' => '$1 an der Zeil $2, Spalt $3, (Byte $4): $5',
+);
+
+$messages['li'] = array(
+	'xml-error-string' => '$1 op regel $2, kolom $3 (byte $4): $5',
+);
+
+$messages['lt'] = array(
+	'xml-error-string' => '$1 $2 eilutėje, $3 stulpelyje ($4 baitas): $5',
+);
+
+$messages['lv'] = array(
+	'xml-error-string' => '$1 $2. rindā, $3. kolonnā ($4. baits): $5',
+);
+
+$messages['lzh'] = array(
+	'xml-error-string' => '$1 於行$2，欄$3 （$4字節）: $5',
+);
+
+$messages['mai'] = array(
+	'xml-error-string' => '$1 पाँतीपर $2, col $3 (byte $4): $5',
+);
+
+$messages['mdf'] = array(
+	'xml-error-string' => '$1 $2-це киксса, палмань $3 ($4 байтт): $5',
+);
+
+$messages['mg'] = array(
+	'xml-error-string' => "$1 eo amin'ny andininy faha $2, tsanganana faha $3 (oktety $4) : $5",
+);
+
+$messages['mk'] = array(
+	'xml-error-string' => '$1 во ред $2, колона $3 (бајт $4): $5',
+);
+
+$messages['ml'] = array(
+	'xml-error-string' => '$2 വരിയിൽ $1, നിര $3 (ബൈറ്റ് $4): $5',
+);
+
+$messages['mn'] = array(
+	'xml-error-string' => '$2-р мөр, $3-р баганад $1 （байт $4）: $5',
+);
+
+$messages['mr'] = array(
+	'xml-error-string' => '$1 ओळ $2मध्ये , स्तंभ $3 (बाईट $4): $5',
+);
+
+$messages['ms'] = array(
+	'xml-error-string' => '$1 pada baris $2, lajur $3 (bait $4): $5',
+);
+
+$messages['mt'] = array(
+	'xml-error-string' => '$1 fil-linja $2, kol $3 (byte $4): $5',
+);
+
+$messages['nb'] = array(
+	'xml-error-string' => '$1 på linje $2, kolonne $3 (byte: $4): $5',
+);
+
+$messages['nds'] = array(
+	'xml-error-string' => '$1 Reeg $2, Spalt $3, (Byte $4): $5',
+);
+
+$messages['nds-nl'] = array(
+	'xml-error-string' => '$1 op regel $2, kolom $3 (byte $4): $5',
+);
+
+$messages['ne'] = array(
+	'xml-error-string' => 'बाइट %s मा %s फज्जी छ : %s',
+);
+
+$messages['nl'] = array(
+	'xml-error-string' => '$1 op regel $2, kolom $3 (byte $4): $5',
+);
+
+$messages['nn'] = array(
+	'xml-error-string' => '$1 på rad $2, kolonne $3 (byte: $4): $5',
+);
+
+$messages['no'] = array(
+	'xml-error-string' => '$1 på rad $2, kolonne $3 (byte: $4): $5',
+);
+
+$messages['oc'] = array(
+	'xml-error-string' => '$1 a la linha $2, col $3 (octet $4) : $5',
+);
+
+$messages['or'] = array(
+	'xml-error-string' => '$1 $2 ଧାଡ଼ିରେ ଅଛି, $3 ସ୍ତମ୍ଭ ($4 ବାଇଟ): $5',
+);
+
+$messages['pam'] = array(
+	'xml-error-string' => '$1 king gulis a $2, col $3 (byte $4): $5',
+);
+
+$messages['pl'] = array(
+	'xml-error-string' => '$1 linia $2, kolumna $3 (bajt $4): $5',
+);
+
+$messages['pms'] = array(
+	'xml-error-string' => '$1 ant la riga $2, colòna $3 (byte $4): $5',
+);
+
+$messages['pnb'] = array(
+	'xml-error-string' => '$1 لین $2 تے، کالم $3 (بائٹ $4 ):$5',
+);
+
+$messages['prg'] = array(
+	'xml-error-string' => '$1 en līnijai $2, kōl. $3 (bāitan $4): $5',
+);
+
+$messages['pt'] = array(
+	'xml-error-string' => '$1 na linha $2, coluna $3 (byte $4): $5',
+);
+
+$messages['pt-br'] = array(
+	'xml-error-string' => '$1 na linha $2, coluna $3 (byte $4): $5',
+);
+
+$messages['qu'] = array(
+	'xml-error-string' => "$1, $2 siq'ipi, $3 tunupi (byte $4): $5",
+);
+
+$messages['rm'] = array(
+	'xml-error-string' => '$1 sin lingia $2, colonna $3 (byte $4): $5',
+);
+
+$messages['ro'] = array(
+	'xml-error-string' => '$1 la linia $2, col $3 (octet $4): $5',
+);
+
+$messages['roa-tara'] = array(
+	'xml-error-string' => "$1 a 'a linea $2, colonne $3 (byte $4): $5",
+);
+
+$messages['ru'] = array(
+	'xml-error-string' => '$1 в строке $2, позиции $3 (байт $4): $5',
+);
+
+$messages['rue'] = array(
+	'xml-error-string' => '$1 на рядку $2, стовпець $3 (байт $4): $5',
+);
+
+$messages['sah'] = array(
+	'xml-error-string' => '$1 - $2 строка, $3 колонка ($4 байт): $5',
+);
+
+$messages['scn'] = array(
+	'xml-error-string' => '$1 a riga $2, culonna $3 (byte $4): $5',
+);
+
+$messages['sh'] = array(
+	'xml-error-string' => '$1 na liniji $2, kolona $3 (bajt $4): $5',
+);
+
+$messages['si'] = array(
+	'xml-error-string' => '$2 පේළියෙහි, $3 තීරුවෙහි $1 ($4 බයිට්): $5',
+);
+
+$messages['sk'] = array(
+	'xml-error-string' => '$1 na riadku $2, stĺpec $3 (bajt $4): $5',
+);
+
+$messages['sl'] = array(
+	'xml-error-string' => '$1 v vrstici $2, znak $3 (bajt $4): $5',
+);
+
+$messages['sq'] = array(
+	'xml-error-string' => '$1 në vijën $2, kol $3 (bite $4): $5',
+);
+
+$messages['sr'] = array(
+	'xml-error-string' => '$1 në vijën $2, kol $3 (bite $4): $5',
+);
+
+$messages['sr-ec'] = array(
+	'xml-error-string' => '$1 у реду $2, колона $3 (бајт $4): $5',
+);
+
+$messages['sr-el'] = array(
+	'xml-error-string' => '$1 na liniji $2, kolona $3 (bajt $4): $5',
+);
+
+$messages['stq'] = array(
+	'xml-error-string' => '$1 Riege $2, Spalte $3, (Byte $4): $5',
+);
+
+$messages['su'] = array(
+	'xml-error-string' => '$1 dina baris $2, kolom $3 (bit $4): $5',
+);
+
+$messages['sv'] = array(
+	'xml-error-string' => '$1 på rad $2, kolumn $3 (byte $4): $5',
+);
+
+$messages['szl'] = array(
+	'xml-error-string' => '$1 lińa $2, kolůmna $3 (bajt $4): $5',
+);
+
+$messages['ta'] = array(
+	'xml-error-string' => '$1 நிரை $2, நிரல் $3 (பைட் $4): $5',
+);
+
+$messages['te'] = array(
+	'xml-error-string' => '$1 $2వ లైనులో, వరుస $3 ($4వ బైటు): $5',
+);
+
+$messages['tg-cyrl'] = array(
+	'xml-error-string' => '$1 дар сатри $2, сутуни $3 ($4 байт): $5',
+);
+
+$messages['tg-latn'] = array(
+	'xml-error-string' => '$1 dar satri $2, sutuni $3 ($4 bajt): $5',
+);
+
+$messages['th'] = array(
+	'xml-error-string' => '$1 ที่บรรทัด $2 คอลัมน์ $3 (ไบต์ที่ $4): $5',
+);
+
+$messages['tk'] = array(
+	'xml-error-string' => '$3 sütünde, $2 setirde $1 (baýt $4): $5',
+);
+
+$messages['tl'] = array(
+	'xml-error-string' => '$1 sa linya $2, hanay $3 (byte $4): $5',
+);
+
+$messages['tr'] = array(
+	'xml-error-string' => '$2 satırında, $3 sütununda $1 (bayt $4): $5',
+);
+
+$messages['ug-arab'] = array(
+	'xml-error-string' => '$2 قۇردىكى $1، ستون $3 ($4 بايت): $5',
+);
+
+$messages['uk'] = array(
+	'xml-error-string' => '$1 в рядку $2, позиції $3 (байт $4): $5',
+);
+
+$messages['vec'] = array(
+	'xml-error-string' => '$1 a riga $2, colona $3 (byte $4): $5',
+);
+
+$messages['vi'] = array(
+	'xml-error-string' => '$1 tại dòng $2, cột $3 (byte $4): $5',
+);
+
+$messages['vo'] = array(
+	'xml-error-string' => '$1 pö lien: $2, kolum: $3 (jölat: $4): $5',
+);
+
+$messages['yi'] = array(
+	'xml-error-string' => '$1 בײַ שורה $2, זייל $3 (בייט $4): $5',
+);
+
+$messages['yue'] = array(
+	'xml-error-string' => '$1 響行$2，欄$3 ($4 bytes): $5',
+);
+
+$messages['zh-hans'] = array(
+	'xml-error-string' => '$1于行$2，列$3（$4字节）：$5',
+);
+
+$messages['zh-hant'] = array(
+	'xml-error-string' => '$1 於行$2，欄$3 （$4位元組）: $5',
+);
+
