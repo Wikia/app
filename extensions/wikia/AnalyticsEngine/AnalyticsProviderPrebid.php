1889dbe10ca9451e1d4f744f78405132e609fcff
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-04-11 15:54:43 +0200

    ADEN-2937 Prepare new analytics providers (prebid.js and appnexus)

---
+++ extensions/wikia/AnalyticsEngine/AnalyticsProviderPrebid.php
@@ -0,0 +1,30 @@
+<?php
+
+class AnalyticsProviderPrebid implements iAnalyticsProvider {
+	private static $url = '//acdn.adnxs.com/prebid/prebid.js';
+	private static $template = 'extensions/wikia/AnalyticsEngine/templates/prebid.mustache';
+
+	public static function isEnabled() {
+		global $wgEnableAdEngineExt, $wgShowAds, $wgAdDriverUseSevenOneMedia;
+
+		return $wgEnableAdEngineExt &&
+			$wgShowAds &&
+			AdEngine2Service::areAdsShowableOnPage() &&
+			!$wgAdDriverUseSevenOneMedia;
+	}
+
+	public function getSetupHtml( $params = array() ) {
+		if ( self::isEnabled() ) {
+			return \MustacheService::getInstance()->render(
+				self::$template,
+				[
+					'url' => self::$url
+				]
+			);
+		}
+	}
+
+	public function trackEvent( $event, $eventDetails = array() ) {
+		return '';
+	}
+}
