63c11e2fb6efd7a1dbef04593c2003f01c3680c5
dianafa <diana.falkowska@gmail.com>
2016-04-12 12:27:29 +0200

    SUS-336 add ChatEntryPoint.mustache and use it

--- extensions/wikia/Chat2/ChatRailController.class.php
+++ extensions/wikia/Chat2/ChatRailController.class.php
@@ -14,7 +14,8 @@ class ChatRailController extends WikiaController {
 		}
 
 		// As most the markup for this is the same as for the chat parser tag, we're reusing the tag template
-		$this->response->getView()->setTemplatePath( dirname( __FILE__ ) . '/templates/entryPointTag.tmpl.php' );
+		$this->response->getView()->setTemplatePath( dirname( __FILE__ ) . '/templates/ChatEntryPoint.mustache' );
+		$this->response->setTemplateEngine(WikiaResponse::TEMPLATE_ENGINE_MUSTACHE);
 	}
 
 	public function executeAnonLoginSuccess() {
@@ -28,48 +29,6 @@ class ChatRailController extends WikiaController {
 			ChatHelper::info( __METHOD__ . ': Method called - new room' );
 		}
 		$this->linkToSpecialChat = SpecialPage::getTitleFor( "Chat" )->escapeLocalUrl();
-
-		wfProfileOut( __METHOD__ );
-	}
-
-	/**
-	 * Chat entry point - rendered via Ajax or pre-rendered in JS variable
-	 * @todo: backward compatibility method, remove it till Oct 2013
-	 * @todo: Ticket to remove this function: https://wikia-inc.atlassian.net/browse/SOC-638
-	 */
-	public function executeContents() {
-		global $wgUser, $wgReadOnly, $wgEnableWallExt;
-		wfProfileIn( __METHOD__ );
-
-		if ( empty( $wgReadOnly ) ) {
-			// Main variables
-			$this->profileType = !empty( $wgEnableWallExt ) ? 'message-wall' : 'talk-page';
-			$this->linkToSpecialChat = SpecialPage::getTitleFor( "Chat" )->escapeLocalUrl();
-			$this->isLoggedIn = $wgUser->isLoggedIn();
-			$this->profileAvatarUrl = $this->isLoggedIn ? AvatarService::getAvatarUrl( $wgUser->getName(), ChatRailController::AVATAR_SIZE ) : '';
-
-			// List of other people in chat
-			$this->totalInRoom = 0;
-
-			// Gets array of users currently in chat to populate rail module and user stats menus
-			$this->chatters = ChatEntryPoint::getChatUsersInfo();
-			$this->totalInRoom = count( $this->chatters );
-			for ( $i = 0 ; $i < $this->totalInRoom ; $i++ ) {
-				global $wgLang;
-				if ( $this->chatters[$i]['showSince'] ) {
-					$this->chatters[$i]['since'] =  $wgLang->getMonthAbbreviation( $this->chatters[$i]['since_month'] ) .
-						' ' . $this->chatters[$i]['since_year'];
-				}
-			}
-
-			ChatHelper::info( __METHOD__ . ': Method called', [
-				'chatters' => $this->totalInRoom,
-				'loggedIn' => $this->isLoggedIn,
-			] );
-		}
-
-		// Cache the entire call in varnish (and browser).
-		$this->response->setCacheValidity( self::CACHE_DURATION );
 
 		wfProfileOut( __METHOD__ );
 	}
