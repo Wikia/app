89a08c4f88d697f28a492c480ec64e25aa6813da
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-04-14 16:38:24 +0200

    ADEN-2937 Disable appnexus adapter and create matomy adapter

---
+++ extensions/wikia/AdEngine/js/lookup/adapter/matomy.js
@@ -0,0 +1,61 @@
+/*global define*/
+define('ext.wikia.adEngine.lookup.adapter.matomy', function () {
+	'use strict';
+
+	var bidderName = 'appnexus',
+		placementId = '7071682',
+		slots = {
+			oasis: [
+				'TOP_RIGHT_BOXAD',
+				'LEFT_SKYSCRAPER_2',
+				'LEFT_SKYSCRAPER_3',
+				'INCONTENT_BOXAD_1',
+				'PREFOOTER_LEFT_BOXAD',
+				'PREFOOTER_RIGHT_BOXAD'
+			],
+			mercury: [
+				'MOBILE_TOP_LEADERBOARD',
+				'MOBILE_IN_CONTENT',
+				'MOBILE_PREFOOTER'
+			]
+		};
+
+	function isEnabled() {
+		// TODO: enable with wgCountries
+		return true;
+	}
+
+	function prepareAdUnit(slotName) {
+		return {
+			code: slotName,
+			sizes: [300, 250],
+			bids: [
+				{
+					bidder: bidderName,
+					params: {
+						placementId: placementId
+					}
+				}
+			]
+		};
+	}
+
+	function getAdUnits(skin) {
+		var adUnits = [];
+
+		if (!slots[skin]) {
+			return [];
+		}
+
+		slots[skin].forEach(function (slotName) {
+			adUnits.push(prepareAdUnit(slotName));
+		});
+
+		return adUnits;
+	}
+
+	return {
+		getAdUnits: getAdUnits,
+		isEnabled: isEnabled
+	};
+});
