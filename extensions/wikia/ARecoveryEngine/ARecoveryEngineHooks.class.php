4807d89c6dab35e8290ce7e1f383668b30fbcbbc
jacek <jacek@wikia-inc.com>
2016-04-14 16:02:04 +0200

    ADEN-3053 implement content control callback

--- extensions/wikia/ARecoveryEngine/ARecoveryEngineHooks.class.php
+++ extensions/wikia/ARecoveryEngine/ARecoveryEngineHooks.class.php
@@ -11,17 +11,27 @@ class ARecoveryEngineHooks {
 	 * @return bool
 	 */
 	public static function onWikiaSkinTopScripts( &$vars, &$scripts ) {
-
-
+		global $wgServer;
 		$resourceLoader = new ResourceLoaderAdEngineSourcePointCSBootstrap();
-		$resourceLoaderContext = new ResourceLoaderContext( new ResourceLoader(), new FauxRequest());
+		$resourceLoaderContext = new ResourceLoaderContext( new ResourceLoader(), new FauxRequest() );
 		$source = $resourceLoader->getScript($resourceLoaderContext);
-		$source .= <<<EOF
+		$source .= <<<EOT
 
-		spBootstrap('/api/v1/ARecoveryEngine/delivery');
-EOF;
+		window._sp_ = {
+			config: {
+				content_control_callback: function() {
+					var msg = '<h2>Ad blocker interference detected!</h2>';
+					msg += '<h3>If you added a new rule to your ad blocker that interferes with the loading of ads on your pages, ';
+					msg += 'this Interference Screen is the expected behaviour. <br />';
+					msg += 'Removing the rule(s) should load the page as expected.</h3>';
 
+					document.getElementById('WikiaArticle').innerHTML = msg;
+				}
+			}
+		};
+EOT;
 
+		$source .= "\nspBootstrap('{$wgServer}/api/v1/ARecoveryEngine/delivery');";
 		$scripts = '<script>' . $source . '</script>' . $scripts;
 
 		return true;
