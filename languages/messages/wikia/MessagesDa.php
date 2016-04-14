44bfb4a7e41256f272dcf65beff9ec31e20f4fa6
Artur Klajnerok <a.klajnerok@gmail.com>
2016-04-14 11:28:34 +0200

    [SUS-374] Removed all old global wikia specyfic messages files

--- languages/messages/wikia/MessagesDa.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$messages = array_merge( $messages, array(
-'copyrightpage' => 'w:Wikia:Licensing|Wikia:Licensing',
-'addnewtalksection-link' => 'Tilføj en kommentar til denne diskussion.',
-) );
-
