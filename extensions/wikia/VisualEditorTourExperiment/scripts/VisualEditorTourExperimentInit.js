f357756bdd96d071795cf4237376bfbdafc97bd8
Kamil Koterba <kamil@wikia-inc.com>
2016-04-14 16:11:05 +0200

    CE-3603 Add template for popover content

--- extensions/wikia/VisualEditorTourExperiment/scripts/VisualEditorTourExperimentInit.js
+++ extensions/wikia/VisualEditorTourExperiment/scripts/VisualEditorTourExperimentInit.js
@@ -1,26 +1,34 @@
-define('VisualEditorTourExperimentInit', ['wikia.window', 'jquery', 'mw', 'wikia.loader'],
-	function (w, $, mw, loader) {
+define('VisualEditorTourExperimentInit', ['wikia.window', 'jquery', 'mw', 'wikia.loader', 'wikia.mustache'],
+	function (w, $, mw, loader, mustache) {
 		'use strict';
 
 		var tour = [
-			{selector: '#WikiaArticle', placement: 'top', title: 'Write content',
-				description: 'Share your knowledge with the Community! This space is yours: write, fix typos, ' +
-				'add links. Make this article better with each edit'},
+				{selector: '#WikiaArticle', placement: 'top', title: 'Write content',
+					description: 'Share your knowledge with the Community! This space is yours: write, fix typos, ' +
+					'add links. Make this article better with each edit'},
+			];
 
-		];
 		function start() {
-			var element = $(tour[0].selector).popover({
-				placement:tour[0].placement,
-				trigger:'manual',
-				html:true,
-				content:'<div class="ve-tour-experiment">' +
-					'<h2>' + tour[0].title + '</h2>' +
-					'<p>' + tour[0].description + '</p>' +
-					'</div>'
+			loader({
+				type: loader.MULTI,
+				resources: {
+					mustache: 'extensions/wikia/VisualEditorTourExperiment/templates/VisualEditorTourExperiment_content.mustache',
+				}
+			}).done(function (assets) {
+				var content = mustache.render(assets.mustache[0], {
+						title: tour[0].title,
+						description: tour[0].description
+					}),
+					element = $(tour[0].selector).popover({
+						placement:tour[0].placement,
+						trigger:'manual',
+						html:true,
+						content: content
+					});
 
+				element.popover('show');
 			});
 
-			element.popover('show');
 		}
 
 		return {
