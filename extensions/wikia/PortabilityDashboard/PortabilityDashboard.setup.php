5ae4d99bde2ca82dabbaa5422b60a3ccbac1d609
idradm <robakadam@gmail.com>
2016-04-15 09:26:26 +0200

    DAT-4074 update dashboard stats on insights update

--- extensions/wikia/PortabilityDashboard/PortabilityDashboard.setup.php
+++ extensions/wikia/PortabilityDashboard/PortabilityDashboard.setup.php
@@ -3,7 +3,11 @@
 $dir = dirname( __FILE__ ) . '/';
 
 $wgAutoloadClasses[ 'PortabilityDashboardModel' ] = $dir . 'PortabilityDashboardModel.class.php';
+$wgAutoloadClasses[ 'PortabilityDashboardHooks' ] = $dir . 'PortabilityDashboardHooks.class.php';
 $wgAutoloadClasses[ 'SpecialPortabilityDashboardController' ] = $dir . 'SpecialPortabilityDashboardController.php';
 
 $wgSpecialPages[ 'PortabilityDashboard' ] = 'SpecialPortabilityDashboardController';
 $wgSpecialPageGroups[ 'PortabilityDashboard' ] = 'wikia';
+
+$wgHooks[ 'UnconvertedInfoboxesQueryRecached' ][] = 'PortabilityDashboardHooks::updateUnconvertedInfoboxes';
+$wgHooks[ 'TemplatesWithoutTypeQueryRecached' ][] = 'PortabilityDashboardHooks::updateNotClassifiedTemplates';
--- extensions/wikia/PortabilityDashboard/PortabilityDashboardModel.class.php
+++ extensions/wikia/PortabilityDashboard/PortabilityDashboardModel.class.php
@@ -2,6 +2,9 @@
 
 class PortabilityDashboardModel {
 	const PORTABILITY_DASHBOARD_TABLE = 'portability_dashboard';
+	const WIKI_ID_FIELD = 'wiki_id';
+	const CUSTOM_INFOBOXES_FIELD = 'custom_infoboxes';
+	const TYPELESS_FIELD = 'typeless';
 
 	private $connection;
 
@@ -13,9 +16,9 @@ class PortabilityDashboardModel {
 		return ( new WikiaSQL() )
 			->SELECT_ALL()
 			->FROM( self::PORTABILITY_DASHBOARD_TABLE )
-			->run( $this->getConnection(), function ( ResultWrapper $rows ) {
-				$result = [];
-				while( $row = $rows->fetchObject() ) {
+			->run( $this->readDB(), function ( ResultWrapper $rows ) {
+				$result = [ ];
+				while ( $row = $rows->fetchObject() ) {
 					$result[] = [
 						'wiki_id' => $row->wiki_id,
 						'portability' => $row->portability,
@@ -28,12 +31,33 @@ class PortabilityDashboardModel {
 			} );
 	}
 
-	private function getConnection() {
+	public function updateInfoboxesCount( $wikiId, $count ) {
+		( new WikiaSQL() )
+			->INSERT()
+			->INTO( self::PORTABILITY_DASHBOARD_TABLE, [ self::WIKI_ID_FIELD, self::CUSTOM_INFOBOXES_FIELD ] )
+			->VALUES( [ [ (int)$wikiId, (int)$count ] ] )
+			->ON_DUPLICATE_KEY_UPDATE( [ self::CUSTOM_INFOBOXES_FIELD => (int)$count ] )
+			->run( $this->connect( DB_MASTER ) );
+	}
+
+	public function updateTemplatesTypeCount( $wikiId, $count ) {
+		( new WikiaSQL() )
+			->INSERT()
+			->INTO( self::PORTABILITY_DASHBOARD_TABLE, [ self::WIKI_ID_FIELD, self::TYPELESS_FIELD ] )
+			->VALUES( [ [ (int)$wikiId, (int)$count ] ] )
+			->ON_DUPLICATE_KEY_UPDATE( [ self::TYPELESS_FIELD => (int)$count ] )
+			->run( $this->connect( DB_MASTER ) );
+	}
+
+	private function readDB() {
 		if ( !isset( $this->connection ) ) {
-			global $wgExternalDatawareDB;
-			$this->connection = wfGetDB( DB_SLAVE, array(), $wgExternalDatawareDB );
+			$this->connection = $this->connect( DB_SLAVE );
 		}
 		return $this->connection;
 	}
 
+	private function connect( $type = DB_SLAVE ) {
+		global $wgExternalDatawareDB;
+		return wfGetDB( $type, array(), $wgExternalDatawareDB );
+	}
 }
