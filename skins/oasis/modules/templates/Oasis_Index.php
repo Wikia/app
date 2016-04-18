1b2eb9369e225c5695d0531c41dc387aa7e9dee7
nAndy <nandy@wikia-inc.com>
2016-04-18 09:52:27 +0000

    ADEN-3206 added revcontent script to page

--- skins/oasis/modules/templates/Oasis_Index.php
+++ skins/oasis/modules/templates/Oasis_Index.php
@@ -104,6 +104,18 @@
 <?= $bottomScripts ?>
 
 <?= $nielsen ?>
+<div id="rcjsload_cd6557"></div>
+<script type="text/javascript">
+	(function() {
+		var referer="";try{if(referer=document.referrer,"undefined"==typeof referer)throw"undefined"}catch(exception){referer=document.location.href,(""==referer||"undefined"==typeof referer)&&(referer=document.URL)}referer=referer.substr(0,700);
+		var rcel = document.createElement("script");
+		rcel.id = 'rc_' + Math.floor(Math.random() * 1000);
+		rcel.type = 'text/javascript';
+		rcel.src = "http://trends.revcontent.com/serve.js.php?w=30102&t="+rcel.id+"&c="+(new Date()).getTime()+"&width="+(window.outerWidth || document.documentElement.clientWidth)+"&referer="+referer;
+		rcel.async = true;
+		var rcds = document.getElementById("rcjsload_cd6557"); rcds.appendChild(rcel);
+	})();
+</script>
 </body>
 
 <?= wfReportTime() . "\n" ?>
