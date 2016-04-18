fe690d1501f3cf8ab10dda51e306ba97545d1a77
idradm <robakadam@gmail.com>
2016-04-18 13:02:54 +0200

    DAT-4074 add create sql file

---
+++ extensions/wikia/PortabilityDashboard/sql/portability_dashboard.create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `portability_dashboard` (
+  `wiki_id` int(11) NOT NULL,
+  `portability` decimal(5,4) DEFAULT NULL,
+  `infobox_portability` decimal(5,4) DEFAULT NULL,
+  `traffic` int(11) DEFAULT NULL,
+  `migration_impact` int(11) DEFAULT NULL,
+  `typeless` int(11) DEFAULT NULL,
+  `custom_infoboxes` int(11) DEFAULT NULL,
+  PRIMARY KEY (`wiki_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
