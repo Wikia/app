81f7e25ca10d51a5fef649a56d39cad66c9b3ced
Kamil Koterba <kamil@wikia-inc.com>
2016-04-19 12:11:25 +0200

    Revert "CE-3603 Spread user types to variations for ease of distinguishing tracking"
    This reverts commit 0c2d6c7f3bfa50a27678203626a7765a702f82e8.

--- extensions/wikia/VisualEditorTourExperiment/scripts/VisualEditorTourExperimentInit.js
+++ extensions/wikia/VisualEditorTourExperiment/scripts/VisualEditorTourExperimentInit.js
@@ -16,20 +16,24 @@ define('VisualEditorTourExperimentInit',
 		}
 
 		function isEnabled() {
-			return (isNewlyregistered() || isUserwithoutedit()) && !$.cookie('vetourdisabled');
+			return isExperimentVariation() &&
+				(isNewlyregistered() || isUserwithoutedit) &&
+				!$.cookie('vetourdisabled');
 		}
 
 		function trackPublish() {
-			if (isNewlyregistered()) {
-				track({
-					action: tracker.ACTIONS.SUCCESS,
-					label: 'publish-newlyregistered'
-				});
-			} else if (isUserwithoutedit()) {
-				track({
-					action: tracker.ACTIONS.SUCCESS,
-					label: 'publish-userwithoutedit'
-				});
+			if (isExperimentVariation()) {
+				if (isNewlyregistered()) {
+					track({
+						action: tracker.ACTIONS.SUCCESS,
+						label: 'publish-userwithoutedit'
+					});
+				} else if (isUserwithoutedit()) {
+					track({
+						action: tracker.ACTIONS.SUCCESS,
+						label: 'publish-newlyregistered'
+					});
+				}
 			}
 		}
 
@@ -37,12 +41,16 @@ define('VisualEditorTourExperimentInit',
 			$('#ca-ve-edit').popover('destroy');
 		}
 
+		function isExperimentVariation() {
+			return abTest.inGroup('CONTRIB_EXPERIMENTS', 'VE_TOUR');
+		}
+		
 		function isNewlyregistered() {
-			return abTest.inGroup('CONTRIB_EXPERIMENTS', 'VE_TOUR_NEWLYREGISTERED') && $.cookie('newlyregistered');
+			return $.cookie('newlyregistered');
 		}
 		
 		function isUserwithoutedit() {
-			return abTest.inGroup('CONTRIB_EXPERIMENTS', 'VE_TOUR_USERWITHOUTEDIT') && $.cookie('userwithoutedit');
+			return $.cookie('userwithoutedit');
 		}
 
 		return {
