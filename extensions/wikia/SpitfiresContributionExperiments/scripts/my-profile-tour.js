8f260e817d83b5d72a42ebeac9c270ec166c458e
Kamil Koterba <kamil@wikia-inc.com>
2016-04-20 09:42:11 +0200

    CE-3604 Add tracking for steps

--- extensions/wikia/SpitfiresContributionExperiments/scripts/my-profile-tour.js
+++ extensions/wikia/SpitfiresContributionExperiments/scripts/my-profile-tour.js
@@ -1,143 +1,183 @@
-require(['jquery', 'wikia.loader', 'wikia.nirvana', 'wikia.mustache', 'mw'], function($, loader, nirvana, mustache, mw){
-	var modalConfig = {
-			vars: {
-				id: 'MyProfileModal',
-				classes: ['my-profile-modal'],
-				size: 'medium', // size of the modal
-				content: '' // content
-			},
-			confirmCloseModal: function() {
-				if (!saved && answers.length) {
-					saveProfile();
-				} else {
-					if (saved) {
-						window.location.href = wgServer + '/wiki/' + userPage;
+require(['jquery', 'wikia.loader', 'wikia.nirvana', 'wikia.mustache', 'wikia.tracker', 'mw'],
+	function ($, loader, nirvana, mustache, tracker, mw) {
+		'use strict';
+
+		var modalConfig = {
+				vars: {
+					id: 'MyProfileModal',
+					classes: ['my-profile-modal'],
+					size: 'medium', // size of the modal
+					content: '' // content
+				},
+				confirmCloseModal: function () {
+					if (!saved && answers.length) {
+						saveProfile();
 					} else {
-						return true;
+						if (saved) {
+							window.location.href = wgServer + '/wiki/' + userPage;
+						} else {
+							return true;
+						}
 					}
 				}
-			}
-		},
-		modal = null,
-		templates = [],
-		answers = {},
-		saved = false,
-		currentStep = 1,
-		userName = mw.user.name(),
-		userPage = 'User:' + userName.replace(/ /g, '_'),
-		templateData = {
-			userName: userName
-		},
-		lastStepTemplateData = [];
+			},
+			modal = null,
+			templates = [],
+			answers = {},
+			saved = false,
+			currentStep = 1,
+			userName = mw.user.name(),
+			userPage = 'User:' + userName.replace(/ /g, '_'),
+			templateData = {
+				userName: userName
+			},
+			lastStepTemplateData = [],
+			track = tracker.buildTrackingFunction({
+				category: 'my-profile-tour',
+				trackingMethod: 'analytics'
+			});
 
+		$.when(
+			nirvana.sendRequest({
+				controller: 'ContributionExperiments',
+				method: 'getNextPages',
+				type: 'get'
+			}),
+			loader({
+				type: loader.MULTI,
+				resources: {
+					mustache: '/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentModal.mustache,' +
+					'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep1.mustache,' +
+					'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep2.mustache,' +
+					'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep3.mustache,' +
+					'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep4.mustache',
+					styles: '/extensions/wikia/SpitfiresContributionExperiments/styles/my-profile-tour.scss'
+				}
+			})
+		).done(renderModal);
 
-	$.when(
-		nirvana.sendRequest({
-			controller: 'ContributionExperiments',
-			method: 'getNextPages',
-			type: 'get'
-		}),
-		loader({
-			type: loader.MULTI,
-			resources: {
-				mustache: '/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentModal.mustache,' +
-				'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep1.mustache,' +
-				'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep2.mustache,' +
-				'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep3.mustache,' +
-				'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep4.mustache',
-				styles: '/extensions/wikia/SpitfiresContributionExperiments/styles/my-profile-tour.scss'
-			}
-		})
-	).done(renderModal);
+		function renderModal(data, resources) {
+			templates = resources.mustache;
+			lastStepTemplateData = data[0];
 
-	function renderModal(data, resources) {
-		templates = resources.mustache;
-		lastStepTemplateData = data[0];
+			loader.processStyle(resources.styles);
+			modalConfig.vars.content = mustache.render(resources.mustache[0], {});
 
-		loader.processStyle(resources.styles);
-		modalConfig.vars.content = mustache.render(resources.mustache[0], {});
+			require(['wikia.ui.factory'], function (uiFactory) {
+				uiFactory.init(['modal']).then(createModalComponent);
+			});
+		}
 
-		require(['wikia.ui.factory'], function (uiFactory) {
-			uiFactory.init(['modal']).then(createModalComponent);
-		});
-	}
+		function createModalComponent(uiModal) {
+			uiModal.createComponent(modalConfig, processInstance);
+		}
 
-	function createModalComponent(uiModal) {
-		uiModal.createComponent(modalConfig, processInstance);
-	}
+		function processInstance(modalInstance) {
+			var modalContent = modalInstance.$element.find('.my-profile-content');
 
-	function processInstance(modalInstance) {
-		var modalContent = modalInstance.$element.find('.my-profile-content');
+			modal = modalInstance;
 
-		modal = modalInstance;
+			modalInstance.show();
 
-		modalInstance.show();
-
-		modalContent.html(mustache.render(templates[currentStep], templateData));
-
-		$('#MyProfileModal').on('click', '.next-step', function() {
-			addAnswer();
-
-			currentStep++;
-			templateData.answer = answers[currentStep];
-			if (currentStep === 4) {
-				sendProfileData().done(function () {
-					modalContent.html(mustache.render(templates[currentStep], lastStepTemplateData));
-				});
-			} else {
-				modalContent.html(mustache.render(templates[currentStep], templateData));
-			}
-		});
-		$('#MyProfileModal').on('click', '.my-profile-header-go-back', function () {
-			addAnswer();
-			currentStep--;
-			templateData.answer = answers[currentStep];
 			modalContent.html(mustache.render(templates[currentStep], templateData));
-		});
-	}
 
-	function addAnswer() {
-		var answer = $('#MyProfileModal').find('.my-profile-textarea').val().trim();
+			track({
+				action: tracker.ACTIONS.IMPRESSION,
+				label: getLabelPrefix() + 'step-1-first-time'
+			});
 
-		answers[currentStep] = answer;
-	}
+			$('#MyProfileModal').on('click', '.next-step', function () {
+				addAnswer();
 
-	function saveProfile() {
-		sendProfileData().done(function () {
-			modal.trigger('close');
-		});
-	}
+				currentStep++;
+				templateData.answer = answers[currentStep];
+				if (currentStep === 4) {
+					sendProfileData().done(function () {
+						modalContent.html(mustache.render(templates[currentStep], lastStepTemplateData));
+					});
+				} else {
+					modalContent.html(mustache.render(templates[currentStep], templateData));
+				}
+				track({
+					action: tracker.ACTIONS.IMPRESSION,
+					label: getLabelPrefix() + 'go-next-to-step' + currentStep
+				});
+			});
 
-	function sendProfileData() {
-		var dfd = $.Deferred(),
-			formattedAnswers;
+			$('#MyProfileModal').on('click', '.my-profile-header-go-back', function () {
+				addAnswer();
+				currentStep--;
+				templateData.answer = answers[currentStep];
+				modalContent.html(mustache.render(templates[currentStep], templateData));
+				track({
+					action: tracker.ACTIONS.CLICK,
+					label: getLabelPrefix() + 'go-back-to-step' + currentStep
+				});
+			});
+		}
 
-		modal.$element.find('.my-profile-content').startThrobbing();
+		function addAnswer() {
+			var answer = $('#MyProfileModal').find('.my-profile-textarea').val().trim();
 
-		formattedAnswers = Object.keys(answers).map(function (value, index) {
-			switch (index + 1) {
-				case 1: return answer = "My favorite moment in the game:\n" + answers[index + 1]; break;
-				case 2: return answer = "My gaming platforms:\n" + answers[index + 1]; break;
-				case 3: return answer = "About me:\n" + answers[index + 1]; break;
+			answers[currentStep] = answer;
+		}
+
+		function saveProfile() {
+			sendProfileData().done(function () {
+				modal.trigger('close');
+			});
+		}
+
+		function sendProfileData() {
+			var dfd = $.Deferred(),
+				formattedAnswers;
+
+			modal.$element.find('.my-profile-content').startThrobbing();
+
+			formattedAnswers = Object.keys(answers).map(function (value, index) {
+				switch (index + 1) {
+					case 1:
+						return answer = "My favorite moment in the game:\n" + answers[index + 1];
+						break;
+					case 2:
+						return answer = "My gaming platforms:\n" + answers[index + 1];
+						break;
+					case 3:
+						return answer = "About me:\n" + answers[index + 1];
+						break;
+				}
+			});
+
+			$.ajax({
+				type: 'post',
+				url: '/api.php',
+				data: {
+					action: 'edit',
+					title: userPage,
+					text: formattedAnswers.join('\n\n'),
+					token: mw.user.tokens.get('editToken')
+				}
+			}).done(function () {
+				saved = true;
+				track({
+					action: tracker.ACTIONS.SUCCESS,
+					label: getLabelPrefix() + 'user-data-saved'
+				});
+			}).always(function () {
+				modal.$element.find('.my-profile-content').stopThrobbing();
+				dfd.resolve(true);
+			});
+
+			return dfd.promise();
+		}
+
+		function getLabelPrefix() {
+			if ($.cookie('newlyregistered')) {
+				return LABEL_PREFIX_NEW;
+			} else if ($.cookie('userwithoutedit')) {
+				return LABEL_PREFIX_WITHOUTEDIT;
 			}
-		});
-
-		$.ajax({
-			type: 'post',
-			url: '/api.php',
-			data: {
-				action: 'edit',
-				title: userPage,
-				text: formattedAnswers.join('\n\n'),
-				token: mw.user.tokens.get('editToken')
-			}
-		}).done(function(){
-			saved = true;
-		}).always(function(){
-			modal.$element.find('.my-profile-content').stopThrobbing();
-			dfd.resolve(true);
-		});
-
-		return dfd.promise();
+			return '';
+		}
 	}
-});
+);
