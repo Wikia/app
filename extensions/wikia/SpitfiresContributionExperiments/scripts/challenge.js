1cb6411e13eb465d223ed85d047773fe9e3be656
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-04-21 10:59:27 +0000

    CE-3604: Improve loading scripts and set proper cookie name

--- extensions/wikia/SpitfiresContributionExperiments/scripts/challenge.js
+++ extensions/wikia/SpitfiresContributionExperiments/scripts/challenge.js
@@ -1,138 +1,140 @@
-require([
-	'jquery',
-	'mw',
-	'ext.wikia.spitfires.experiments.tracker',
-	'wikia.cache',
-	'wikia.loader',
-	'wikia.mustache'
-], function ($, mw, tracker, cache, loader, mustache) {
-	'use strict';
+window.addEventListeners('load', function () {
+	require([
+		'jquery',
+		'mw',
+		'ext.wikia.spitfires.experiments.tracker',
+		'wikia.cache',
+		'wikia.loader',
+		'wikia.mustache'
+	], function ($, mw, tracker, cache, loader, mustache) {
+		'use strict';
 
-	var experimentName = 'contribution-experiments',
-		experimentId = 5685550912,
-		dismissCookieName = 'contribchallengedismissed';
+		var experimentName = 'contribution-experiments',
+			experimentId = 5685550912,
+			dismissCookieName = 'contribchallengedismissed';
 
-	function init() {
-		if (
-			mw.user.anonymous() ||
-			window.optimizely.variationNamesMap[experimentId] !== 'CHALLENGE-LIST' ||
-			$.cookie(dismissCookieName) ||
-			mw.config.get('wgNamespaceNumber') !== 0
-		) {
-			return;
+		function init() {
+			if (
+				mw.user.anonymous() ||
+				window.optimizely.variationNamesMap[experimentId] !== 'CHALLENGE-LIST' ||
+				$.cookie(dismissCookieName) ||
+				mw.config.get('wgNamespaceNumber') !== 0
+			) {
+				return;
+			}
+
+			$.when(
+				getPopularPages(),
+				loader({
+					type: loader.MULTI,
+					resources: {
+						mustache: '/extensions/wikia/SpitfiresContributionExperiments/templates/Challenge.mustache',
+						styles: '/extensions/wikia/SpitfiresContributionExperiments/styles/challenge.scss'
+					}
+				})
+			).done(addEntryPoint);
 		}
 
-		$.when(
-			getPopularPages(),
-			loader({
-				type: loader.MULTI,
-				resources: {
-					mustache: '/extensions/wikia/SpitfiresContributionExperiments/templates/Challenge.mustache',
-					styles: '/extensions/wikia/SpitfiresContributionExperiments/styles/challenge.scss'
-				}
-			})
-		).done(addEntryPoint);
-	}
+		function addEntryPoint(pageTitles, resources) {
+			var pages = getRandomPagesData(pageTitles, 2),
+				templateData = {
+					userName: mw.user.name(),
+					blankImgUrl: mw.config.get('wgBlankImgUrl'),
+					pages: pages
+				};
 
-	function addEntryPoint(pageTitles, resources) {
-		var pages = getRandomPagesData(pageTitles, 2),
-			templateData = {
-				userName: mw.user.name(),
-				blankImgUrl: mw.config.get('wgBlankImgUrl'),
-				pages: pages
-			};
+			if (!pages.length) {
+				return;
+			}
 
-		if (!pages.length) {
-			return;
+			loader.processStyle(resources.styles);
+
+			$('#WikiaPageHeader').prepend(
+				mustache.render(resources.mustache[0], templateData)
+			).on('click', '.spitfires-challenge-experiment .close-button', function (e) {
+					e.preventDefault();
+					tracker.trackVerboseClick(experimentName, 'dismissed');
+					$.cookie(dismissCookieName, 1, {expires: 30});
+					$(this).parent().remove();
+				}).on('mousedown touchstart', '.spitfires-challenge-experiment-content a', function () {
+					tracker.trackVerboseClick(experimentName, $(this).attr('class'));
+				});
+
+			tracker.trackVerboseImpression(experimentName, 'view');
 		}
 
-		loader.processStyle(resources.styles);
+		function getRandomPagesData(pageTitles, numItems) {
+			var i, index, title,
+				result = [],
+				editParams = {action: 'edit'};
 
-		$('#WikiaPageHeader').prepend(
-			mustache.render(resources.mustache[0], templateData)
-		).on('click', '.spitfires-challenge-experiment .close-button', function (e) {
-			e.preventDefault();
-			tracker.trackVerboseClick(experimentName, 'dismissed');
-			$.cookie(dismissCookieName, 1, { expires: 30 });
-			$(this).parent().remove();
-		}).on('mousedown touchstart', '.spitfires-challenge-experiment-content a', function () {
-			tracker.trackVerboseClick(experimentName, $(this).attr('class'));
-		});
+			if (!pageTitles.length) {
+				return result;
+			}
 
-		tracker.trackVerboseImpression(experimentName, 'view');
-	}
+			if (mw.config.get('wgVisualEditorPreferred')) {
+				editParams = {veaction: 'edit'};
+			}
 
-	function getRandomPagesData(pageTitles, numItems) {
-		var i, index, title,
-			result = [],
-			editParams = { action: 'edit' };
+			if (pageTitles.length < numItems) {
+				numItems = pageTitles.length;
+			}
 
-		if (!pageTitles.length) {
+			for (i = 0; i < numItems; i++) {
+				index = Math.floor(Math.random() * pageTitles.length);
+				title = mw.Title.newFromText(pageTitles[index]);
+				result[result.length] = {
+					pageTitle: title.getPrefixedText(),
+					pageUrl: title.getUrl(),
+					editUrl: title.getUrl(editParams)
+				};
+				pageTitles.splice(index, 1);
+			}
+
 			return result;
 		}
 
-		if (mw.config.get('wgVisualEditorPreferred')) {
-			editParams = { veaction: 'edit' };
-		}
+		function getPopularPages() {
+			var deferred = $.Deferred(),
+				cacheKey = 'contribChallengeExperiment',
+				pageTitles = cache.get(cacheKey) || [];
 
-		if (pageTitles.length < numItems) {
-			numItems = pageTitles.length;
-		}
+			if (pageTitles.length) {
+				deferred.resolve(pageTitles);
+				return deferred.promise();
+			}
 
-		for (i = 0; i < numItems; i++) {
-			index = Math.floor(Math.random() * pageTitles.length);
-			title = mw.Title.newFromText(pageTitles[index]);
-			result[result.length] = {
-				pageTitle: title.getPrefixedText(),
-				pageUrl: title.getUrl(),
-				editUrl: title.getUrl(editParams)
-			};
-			pageTitles.splice(index, 1);
-		}
+			$.getJSON('/api.php', {
+				action: 'query',
+				list: 'querypage',
+				qppage: 'Popularpages',
+				qplimit: 50,
+				format: 'json'
+			}).done(function (data) {
+				var i, results;
+				if (
+					data.query &&
+					data.query.querypage.results &&
+					data.query.querypage.results.length
+				) {
+					results = data.query.querypage.results;
+					for (i = 0; i < results.length; i++) {
+						if (results[i].ns === 0) {
+							pageTitles[pageTitles.length] = results[i].title;
+						}
+					}
+					cache.set(cacheKey, pageTitles, cache.CACHE_STANDARD);
+					deferred.resolve(pageTitles);
+				} else {
+					deferred.reject();
+				}
+			}).fail(function () {
+				deferred.reject();
+			});
 
-		return result;
-	}
-
-	function getPopularPages() {
-		var deferred = $.Deferred(),
-			cacheKey = 'contribChallengeExperiment',
-			pageTitles = cache.get(cacheKey) || [];
-
-		if (pageTitles.length) {
-			deferred.resolve(pageTitles);
 			return deferred.promise();
 		}
 
-		$.getJSON('/api.php', {
-			action: 'query',
-			list: 'querypage',
-			qppage: 'Popularpages',
-			qplimit: 50,
-			format: 'json'
-		}).done(function (data) {
-			var i, results;
-			if (
-				data.query &&
-				data.query.querypage.results &&
-				data.query.querypage.results.length
-			) {
-				results = data.query.querypage.results;
-				for (i = 0; i < results.length; i++) {
-					if (results[i].ns === 0) {
-						pageTitles[pageTitles.length] = results[i].title;
-					}
-				}
-				cache.set(cacheKey, pageTitles, cache.CACHE_STANDARD);
-				deferred.resolve(pageTitles);
-			} else {
-				deferred.reject();
-			}
-		}).fail(function () {
-			deferred.reject();
-		});
-
-		return deferred.promise();
-	}
-
-	$(init);
+		$(init);
+	});
 });
--- extensions/wikia/SpitfiresContributionExperiments/scripts/my-profile-tour.js
+++ extensions/wikia/SpitfiresContributionExperiments/scripts/my-profile-tour.js
@@ -1,169 +1,171 @@
-require(['jquery', 'ext.wikia.spitfires.experiments.tracker', 'wikia.loader', 'wikia.nirvana', 'wikia.mustache', 'mw'],
-	function ($, tracker, loader, nirvana, mustache, mw) {
-		'use strict';
+window.addEventListeners('load', function () {
+	require(['jquery', 'ext.wikia.spitfires.experiments.tracker', 'wikia.loader', 'wikia.nirvana', 'wikia.mustache', 'mw'],
+		function ($, tracker, loader, nirvana, mustache, mw) {
+			'use strict';
 
-		var experimentName = 'contribution-experiments',
-			experimentId = 5685550912,
-			modalConfig = {
-				vars: {
-					id: 'MyProfileModal',
-					classes: ['my-profile-modal'],
-					size: 'medium', // size of the modal
-					content: '' // content
+			var experimentName = 'contribution-experiments',
+				experimentId = 5685550912,
+				modalConfig = {
+					vars: {
+						id: 'MyProfileModal',
+						classes: ['my-profile-modal'],
+						size: 'medium', // size of the modal
+						content: '' // content
+					},
+					confirmCloseModal: function () {
+						if (currentStep < 4) {
+							return confirm('You\'re about to leave and all the data you filled in will be lost. ' +
+								'Remember, once the data is saved in your profile, you can always change it later. ' +
+								'Do you still wish to leave?');
+						} else {
+							window.location.href = mw.config.get('wgServer') + '/wiki/' + userPage;
+						}
+					}
 				},
-				confirmCloseModal: function () {
-					if (currentStep < 4)  {
-						return confirm('You\'re about to leave and all the data you filled in will be lost. ' +
-							'Remember, once the data is saved in your profile, you can always change it later. ' +
-							'Do you still wish to leave?');
-					} else {
-						window.location.href = mw.config.get('wgServer') + '/wiki/' + userPage;
-					}
-				}
-			},
-			modal = null,
-			templates = [],
-			answers = {},
-			seenCookieName = 'myprofiletour-seen',
-			currentStep = 1,
-			userName = mw.user.name(),
-			userPage = 'User:' + userName.replace(/ /g, '_'),
-			templateData = {
-				userName: userName
-			},
-			lastStepTemplateData = [];
+				modal = null,
+				templates = [],
+				answers = {},
+				seenCookieName = 'myprofiletour-seen',
+				currentStep = 1,
+				userName = mw.user.name(),
+				userPage = 'User:' + userName.replace(/ /g, '_'),
+				templateData = {
+					userName: userName
+				},
+				lastStepTemplateData = [];
 
-		function init() {
-			if ($.cookie(seenCookieName) || window.optimizely.variationNamesMap[experimentId] !== 'USER-PROFILE') {
-				return;
+			function init() {
+				if ($.cookie(seenCookieName) || window.optimizely.variationNamesMap[experimentId] !== 'USER-PROFILE') {
+					return;
+				}
+				$.when(
+					nirvana.sendRequest({
+						controller: 'ContributionExperiments',
+						method: 'getNextPages',
+						type: 'get'
+					}),
+					loader({
+						type: loader.MULTI,
+						resources: {
+							mustache: '/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentModal.mustache,' +
+							'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep1.mustache,' +
+							'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep2.mustache,' +
+							'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep3.mustache,' +
+							'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep4.mustache',
+							styles: '/extensions/wikia/SpitfiresContributionExperiments/styles/my-profile-tour.scss'
+						}
+					})
+				).done(renderModal);
 			}
-			$.when(
-				nirvana.sendRequest({
-					controller: 'ContributionExperiments',
-					method: 'getNextPages',
-					type: 'get'
-				}),
-				loader({
-					type: loader.MULTI,
-					resources: {
-						mustache: '/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentModal.mustache,' +
-						'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep1.mustache,' +
-						'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep2.mustache,' +
-						'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep3.mustache,' +
-						'/extensions/wikia/SpitfiresContributionExperiments/templates/MyProfileTourExperimentStep4.mustache',
-						styles: '/extensions/wikia/SpitfiresContributionExperiments/styles/my-profile-tour.scss'
-					}
-				})
-			).done(renderModal);
-		}
 
-		function renderModal(data, resources) {
-			$.cookie(seenCookieName, 1, {expires: 30});
-			templates = resources.mustache;
-			lastStepTemplateData = data[0];
+			function renderModal(data, resources) {
+				$.cookie(seenCookieName, 1, {expires: 30});
+				templates = resources.mustache;
+				lastStepTemplateData = data[0];
 
-			loader.processStyle(resources.styles);
-			modalConfig.vars.content = mustache.render(resources.mustache[0], {});
+				loader.processStyle(resources.styles);
+				modalConfig.vars.content = mustache.render(resources.mustache[0], {});
 
-			require(['wikia.ui.factory'], function (uiFactory) {
-				uiFactory.init(['modal']).then(createModalComponent);
-			});
-		}
+				require(['wikia.ui.factory'], function (uiFactory) {
+					uiFactory.init(['modal']).then(createModalComponent);
+				});
+			}
 
-		function createModalComponent(uiModal) {
-			uiModal.createComponent(modalConfig, processInstance);
-		}
+			function createModalComponent(uiModal) {
+				uiModal.createComponent(modalConfig, processInstance);
+			}
 
-		function processInstance(modalInstance) {
-			var $modalContent = modalInstance.$element.find('.my-profile-content'),
-				$profileModal;
+			function processInstance(modalInstance) {
+				var $modalContent = modalInstance.$element.find('.my-profile-content'),
+					$profileModal;
 
-			modal = modalInstance;
+				modal = modalInstance;
 
-			modalInstance.show();
+				modalInstance.show();
 
-			$modalContent.html(mustache.render(templates[currentStep], templateData));
-
-			tracker.trackVerboseImpression(experimentName, 'step-1-first-time');
-
-			$profileModal = $('#MyProfileModal');
-			$profileModal.on('click', '.next-step', function () {
-				addAnswer();
-
-				currentStep++;
-				templateData.answer = answers[currentStep];
-				if (currentStep === 4) {
-					sendProfileData().done(function () {
-						$modalContent.html(mustache.render(templates[currentStep], lastStepTemplateData));
-					});
-				} else {
-					$modalContent.html(mustache.render(templates[currentStep], templateData));
-				}
-
-				tracker.trackVerboseClick(experimentName, 'go-next-to-step-' + currentStep);
-			});
-
-			$profileModal.on('click', '.my-profile-header-go-back', function () {
-				addAnswer();
-				currentStep--;
-				templateData.answer = answers[currentStep];
 				$modalContent.html(mustache.render(templates[currentStep], templateData));
 
-				tracker.trackVerboseClick(experimentName, 'go-back-to-step-' + currentStep);
-			});
+				tracker.trackVerboseImpression(experimentName, 'step-1-first-time');
 
-			$profileModal.on('mousedown touchstart', '.my-profile-next-pages a', function () {
-				tracker.trackVerboseClick(experimentName, 'last-step-go-to-article');
-			});
-		}
+				$profileModal = $('#MyProfileModal');
+				$profileModal.on('click', '.next-step', function () {
+					addAnswer();
 
-		function addAnswer() {
-			answers[currentStep] = $('#MyProfileModal').find('.my-profile-textarea').val().trim();
-		}
-
-		function sendProfileData() {
-			var dfd = $.Deferred(),
-				formattedAnswers;
-
-			if (Object.keys(answers).length) {
-				modal.$element.find('.my-profile-content').startThrobbing();
-
-				formattedAnswers = Object.keys(answers).map(function (value, index) {
-					switch (index + 1) {
-						case 1:
-							return "===My favorite moment in the game===\n" + answers[index + 1];
-							break;
-						case 2:
-							return "===My gaming platforms===\n" + answers[index + 1];
-							break;
-						case 3:
-							return "===About me===\n" + answers[index + 1];
-							break;
+					currentStep++;
+					templateData.answer = answers[currentStep];
+					if (currentStep === 4) {
+						sendProfileData().done(function () {
+							$modalContent.html(mustache.render(templates[currentStep], lastStepTemplateData));
+						});
+					} else {
+						$modalContent.html(mustache.render(templates[currentStep], templateData));
 					}
+
+					tracker.trackVerboseClick(experimentName, 'go-next-to-step-' + currentStep);
 				});
 
-				$.ajax({
-					type: 'post',
-					url: '/api.php',
-					data: {
-						action: 'edit',
-						title: userPage,
-						text: formattedAnswers.join('\n\n'),
-						token: mw.user.tokens.get('editToken')
-					}
-				}).done(function () {
-					tracker.trackVerboseSuccess(experimentName, 'user-data-saved');
-				}).always(function () {
-					modal.$element.find('.my-profile-content').stopThrobbing();
-					dfd.resolve(true);
+				$profileModal.on('click', '.my-profile-header-go-back', function () {
+					addAnswer();
+					currentStep--;
+					templateData.answer = answers[currentStep];
+					$modalContent.html(mustache.render(templates[currentStep], templateData));
+
+					tracker.trackVerboseClick(experimentName, 'go-back-to-step-' + currentStep);
 				});
-			} else {
-				dfd.resolve(true);
+
+				$profileModal.on('mousedown touchstart', '.my-profile-next-pages a', function () {
+					tracker.trackVerboseClick(experimentName, 'last-step-go-to-article');
+				});
 			}
 
-			return dfd.promise();
-		}
+			function addAnswer() {
+				answers[currentStep] = $('#MyProfileModal').find('.my-profile-textarea').val().trim();
+			}
 
-		init();
-	}
-);
+			function sendProfileData() {
+				var dfd = $.Deferred(),
+					formattedAnswers;
+
+				if (Object.keys(answers).length) {
+					modal.$element.find('.my-profile-content').startThrobbing();
+
+					formattedAnswers = Object.keys(answers).map(function (value, index) {
+						switch (index + 1) {
+							case 1:
+								return "===My favorite moment in the game===\n" + answers[index + 1];
+								break;
+							case 2:
+								return "===My gaming platforms===\n" + answers[index + 1];
+								break;
+							case 3:
+								return "===About me===\n" + answers[index + 1];
+								break;
+						}
+					});
+
+					$.ajax({
+						type: 'post',
+						url: '/api.php',
+						data: {
+							action: 'edit',
+							title: userPage,
+							text: formattedAnswers.join('\n\n'),
+							token: mw.user.tokens.get('editToken')
+						}
+					}).done(function () {
+						tracker.trackVerboseSuccess(experimentName, 'user-data-saved');
+					}).always(function () {
+						modal.$element.find('.my-profile-content').stopThrobbing();
+						dfd.resolve(true);
+					});
+				} else {
+					dfd.resolve(true);
+				}
+
+				return dfd.promise();
+			}
+
+			$(init);
+		}
+	);
+});
