f168f6ff830605b243673ccf50dcfd21bb73af88
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-04-21 10:03:59 +0000

    CE-3604: Change message and getting server name

--- extensions/wikia/SpitfiresContributionExperiments/scripts/my-profile-tour.js
+++ extensions/wikia/SpitfiresContributionExperiments/scripts/my-profile-tour.js
@@ -13,9 +13,11 @@ require(['jquery', 'ext.wikia.spitfires.experiments.tracker', 'wikia.loader', 'w
 				},
 				confirmCloseModal: function () {
 					if (currentStep < 4)  {
-						return confirm("Are you sure to quit this tour?");
+						return confirm('You\'re about to leave and all the data you filled in will be lost. ' +
+							'Remember, once the data is saved in your profile, you can always change it later. ' +
+							'Do you still wish to leave?');
 					} else {
-						window.location.href = wgServer + '/wiki/' + userPage;
+						window.location.href = mw.config.get('wgServer') + '/wiki/' + userPage;
 					}
 				}
 			},
