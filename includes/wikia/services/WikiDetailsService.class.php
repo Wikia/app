b880558f96f5b1b4be7f1c4771ac671f79b3000b
Roger Szczesny <itachi24@o2.pl>
2016-04-25 16:46:27 +0200

    SOC-2222 | Opt

--- includes/wikia/services/WikiDetailsService.class.php
+++ includes/wikia/services/WikiDetailsService.class.php
@@ -280,8 +280,8 @@ class WikiDetailsService extends WikiService {
 		$response = Http::get( "http://$consulUrl/$id/forums/$id", 'default', array( 'noProxy' => true ));
 		if ( $response != false ) {
 			$decodedResponse = json_decode( $response, true );
-			if ( isset( $decodedResponse ) && json_last_error() === JSON_ERROR_NONE ) {
-				return isset( $decodedResponse[ 'threadCount' ] ) ? $decodedResponse[ 'threadCount' ] : null;
+			if ( isset( $decodedResponse[ 'threadCount' ] ) && json_last_error() === JSON_ERROR_NONE ) {
+				return $decodedResponse[ 'threadCount' ];
 			}
 		}
 		return null;
