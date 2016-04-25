044e19249552c307b839017c38050064b7a233b7
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-04-25 13:50:53 +0000

    CE-3659: Use strategy pattern to choose experiment

--- extensions/wikia/PotentialMemberPageExperiments/scripts/entry-point-experiment.js
+++ extensions/wikia/PotentialMemberPageExperiments/scripts/entry-point-experiment.js
@@ -14,61 +14,76 @@ require([
 		track = tracker.buildTrackingFunction({
 			category: 'potential-member-experiment',
 			trackingMethod: 'analytics'
-		});
+		}),
+		experiment,
+		experiments = {
+			TOP: function () {
+				this.type = 'top';
+				this.addEntryPoint = function () {
+					$banner.insertAfter($('#WikiaPageHeader .header-container'));
+				}
+			},
+			IN_ARTICLE: function () {
+				this.type = 'in-article';
+				this.addEntryPoint = function () {
+					var $content = $('.mw-content-text'),
+						headers = $content.children('h2'),
+						$header;
+
+					// Check if there are headers in content
+					if (headers.length >= 2) {
+						$header = headers.eq(0);
+
+						// Check if first header is not first node in content
+						if ($header.prevAll('p').length) {
+							$banner.insertBefore($header);
+						} else {
+							$banner.insertBefore(headers.eq(1));
+						}
+					} else {
+						$content.append($banner);
+					}
+				}
+			}
+		};
 
 	function init() {
 		if ($.cookie(dismissCookieName)) {
 			return;
 		}
 
-		$.when(
-			loader({
-				type: loader.MULTI,
-				resources: {
-					mustache: '/extensions/wikia/PotentialMemberPageExperiments/templates/PMPEntryPoint.mustache',
-					styles: '/extensions/wikia/PotentialMemberPageExperiments/styles/entry-point-experiment.scss'
-				}
-			})
-		).done(addEntryPoint);
+		if (setExperiment()) {
+			$.when(
+				loader({
+					type: loader.MULTI,
+					resources: {
+						mustache: '/extensions/wikia/PotentialMemberPageExperiments/templates/PMPEntryPoint.mustache',
+						styles: '/extensions/wikia/PotentialMemberPageExperiments/styles/entry-point-experiment.scss'
+					}
+				})
+			).done(addEntryPoint);
+		}
+	}
+
+	function setExperiment() {
+		var group = abTest.getGroup(experimentName);
+
+		if (experiments.hasOwnProperty(group)) {
+			experiment = new experiments[group]();
+			return true;
+		}
+
+		return false;
 	}
 
 	function addEntryPoint(resources) {
-		var group = abTest.getGroup(experimentName);
-
 		loader.processStyle(resources.styles);
 
-		$banner = $(mustache.render(template, { bannerType: group.toLowerCase() }))
+		$banner = $(mustache.render(resources.mustache[0], { bannerType: experiment.type }))
 			.on('click', '.pmp-entry-point-button', onEntryPointClick)
 			.on('click', '.pmp-entry-point-close', close);
 
-		switch (group) {
-			case 'TOP': addEntryPointTop(); break;
-			case 'IN_ARTICLE': addEntryPointInArticle(); break;
-		}
-	}
-
-	function addEntryPointTop() {
-		$banner.insertAfter($('#WikiaPageHeader .header-container'));
-	}
-
-	function addEntryPointInArticle() {
-		var $content = $('.mw-content-text'),
-			headers = $content.children('h2'),
-			$header;
-
-		// Check if there are headers in content
-		if (headers.length >= 2) {
-			$header = headers.eq(0);
-
-			// Check if first header is not first node in content
-			if ($header.prevAll('p').length) {
-				$banner.insertBefore($header);
-			} else {
-				$banner.insertBefore(headers.eq(1));
-			}
-		} else {
-			$content.append($banner);
-		}
+		experiment.addEntryPoint();
 	}
 
 	function onEntryPointClick() {
