abd2f397e477090e903f51701dffddd74b2a763b
drets <dmitryrets@gmail.com>
2016-04-28 21:18:05 +0200

    ADEN-3256 Interstitial as out-of-page slot
    * create custom interstitial
    * use out-of-page slot

--- extensions/wikia/AdEngine/js/template/interstitial.js
+++ extensions/wikia/AdEngine/js/template/interstitial.js
@@ -1,33 +1,31 @@
 /*global define*/
-define('ext.wikia.adEngine.template.floorAdhesion', [
-	'ext.wikia.adEngine.adContext',
+define('ext.wikia.adEngine.template.interstitial', [
 	'ext.wikia.adEngine.slotTweaker',
-	'wikia.log',
 	'wikia.document',
-	'wikia.window'
-], function (adContext, slotTweaker, log, doc, win) {
+	'wikia.log',
+], function (slotTweaker, doc, log) {
 	'use strict';
 
-	var logGroup = 'ext.wikia.adEngine.template.floorAdhesion',
+	var logGroup = 'ext.wikia.adEngine.template.interstitial',
 		slotName = 'INVISIBLE_HIGH_IMPACT_2',
 		wrapper = doc.getElementById('InvisibleHighImpactWrapper');
 
 	function show() {
-		var skin = adContext.getContext().targeting.skin;
-
 		wrapper.querySelector('.close').addEventListener('click', function (event) {
 			event.preventDefault();
+
 			wrapper.classList.add('hidden');
+			doc.documentElement.classList.remove('stop-scrolling');
+
+			log('Hide interstitial', 'info', logGroup);
 		});
 
 		slotTweaker.onReady(slotName, function () {
-			if (skin === 'oasis') {
-				win.WikiaBar.hideContainer();
-			}
-
-			wrapper.classList.add('floor-adhesion');
+			wrapper.classList.add('interstitial');
 			wrapper.classList.remove('hidden');
-			log('Show floor adhesion', 'info', logGroup);
+			doc.documentElement.classList.add('stop-scrolling');
+
+			log('Show interstitial', 'info', logGroup);
 		});
 	}
 
