84aa9d80e0cbaeb82d02e52ac241e972f455faeb
Piotr Gabryjeluk <rychu@wikia-inc.com>
2016-04-28 15:27:48 -0700

    SEO-361 Re-use the staff-internal-reindex.php for reindexing regular wikis

--- extensions/wikia/Search/maintenance/reindex-wiki.php
+++ extensions/wikia/Search/maintenance/reindex-wiki.php
@@ -1,43 +1,48 @@
 <?php
 /**
- * Responsible for reindexing on staff and internal, using Indexer class.
+ * Reindex one full wiki using Indexer class.
  * @package MediaWiki
  * @addtopackage maintenance
  */
 
 require_once( "../../../../maintenance/commandLine.inc" );
 
-include("$IP/extensions/wikia/Search/WikiaSearch.setup.php");
+include( "$IP/extensions/wikia/Search/WikiaSearch.setup.php" );
 
 global $wgContentNamespaces, $wgExtraNamespaces;
-$wgUser = User::newFromName('Owen Davis');
+$wgUser = User::newFromName( 'Owen Davis' );
 $wgTitle = Title::newMainPage();
 $c = RequestContext::getMain();
-$c->setUser($wgUser);
-$c->setTitle($wgTitle);
+$c->setUser( $wgUser );
+$c->setTitle( $wgTitle );
 
-$indexer = (new Wikia\Search\Indexer);
+$indexer = ( new Wikia\Search\Indexer );
 
 $dbr = wfGetDB( DB_MASTER );
 
 $namespaces = !empty( $wgExtraNamespaces ) ? array_merge( $wgContentNamespaces, $wgExtraNamespaces ) : $wgContentNamespaces;
 
+if ( !in_array( NS_FILE, $namespaces ) ) {
+	$namespaces[] = NS_FILE;
+}
+
 $select = $dbr->select(
-		'page',
-		'page_id',
-		array( 'page_namespace' => $namespaces )
-		);
+	'page',
+	'page_id',
+	array( 'page_namespace' => $namespaces )
+);
 
 foreach ( $select as $row ) {
 	if ( $row->page_id ) {
-    	$ids[] = $row->page_id;
+		$ids[] = $row->page_id;
 	}
 }
 
-$idCount = count($ids);
+$idCount = count( $ids );
 $sliceCount = 0;
-foreach ( array_chunk( $ids, 10 ) as $idSlice ) {
-	$sliceCount += 10;
+$batchSize = 100;
+foreach ( array_chunk( $ids, $batchSize ) as $idSlice ) {
+	$sliceCount += $batchSize;
 	$indexer->reindexBatch( $idSlice );
 	echo "Reindexed {$sliceCount}/{$idCount} docs\n";
 }
