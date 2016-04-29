1e43f63dfa4e2746a91a552bd977afedbf0980d7
Piotr Gabryjeluk <rychu@wikia-inc.com>
2016-04-29 12:34:36 -0700

    SEO-361 Reformatted the code a bit

--- extensions/wikia/Search/maintenance/reindex-wiki.php
+++ extensions/wikia/Search/maintenance/reindex-wiki.php
@@ -6,38 +6,31 @@
  */
 
 require_once( "../../../../maintenance/commandLine.inc" );
-
 include( "$IP/extensions/wikia/Search/WikiaSearch.setup.php" );
 
-global $wgContentNamespaces, $wgExtraNamespaces;
-$wgUser = User::newFromName( 'Owen Davis' );
-$wgTitle = Title::newMainPage();
-$c = RequestContext::getMain();
-$c->setUser( $wgUser );
-$c->setTitle( $wgTitle );
+// Namespaces to refresh additionally to those in wgContentNamespaces and wgExtraNamespaces
+$additionalNamespaces = [ NS_FILE ];
 
-$indexer = ( new Wikia\Search\Indexer );
-
-$dbr = wfGetDB( DB_MASTER );
-
-$namespaces = !empty( $wgExtraNamespaces ) ? array_merge( $wgContentNamespaces, $wgExtraNamespaces ) : $wgContentNamespaces;
-
-if ( !in_array( NS_FILE, $namespaces ) ) {
-	$namespaces[] = NS_FILE;
+$namespaces = array_merge( $wgContentNamespaces, $additionalNamespaces );
+if ( !empty( $wgExtraNamespaces ) ) {
+	$namespaces = array_merge( $namespaces, $wgExtraNamespaces );
 }
 
+$dbr = wfGetDB( DB_MASTER );
 $select = $dbr->select(
 	'page',
 	'page_id',
-	array( 'page_namespace' => $namespaces )
+	array( 'page_namespace' => array_unique( $namespaces ) )
 );
 
+$ids = [];
 foreach ( $select as $row ) {
 	if ( $row->page_id ) {
 		$ids[] = $row->page_id;
 	}
 }
 
+$indexer = new Wikia\Search\Indexer();
 $idCount = count( $ids );
 $sliceCount = 0;
 $batchSize = 100;
