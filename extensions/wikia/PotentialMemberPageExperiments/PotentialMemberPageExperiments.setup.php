a506273195e62af478d55bca23691ad38459c199
adamkarminski <adamk@wikia-inc.com>
2016-04-29 14:00:44 +0200

    CE-3697 Prepare a Japanese version of the experiment

--- extensions/wikia/PotentialMemberPageExperiments/PotentialMemberPageExperiments.setup.php
+++ extensions/wikia/PotentialMemberPageExperiments/PotentialMemberPageExperiments.setup.php
@@ -5,3 +5,12 @@
  */
 $wgAutoloadClasses['Wikia\PotentialMemberPageExperiments\Hooks'] = __DIR__ . '/Hooks.class.php';
 $wgExtensionFunctions[] = 'Wikia\PotentialMemberPageExperiments\Hooks::register';
+
+/**
+ * Messages
+ */
+$wgExtensionMessagesFiles['PotentialMemberPageExperiment'] = __DIR__ . '/PotentialMemberPageExperiments.i18n.php';
+
+JSMessages::registerPackage( 'PotentialMemberPageEntryPoint', [
+	'pmp-entry-point-*'
+] );
