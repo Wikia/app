0bb2e7b0db40a43f3c98f911e4afae19e8df8e64
bkoval <brt.kowal@gmail.com>
2016-05-02 14:39:51 +0200

    SUS | Wrapping TD overrides into a self invoking function

--- extensions/wikia/CreateNewWiki/js/CreateNewWikiThemeDesignerOverrides.js
+++ extensions/wikia/CreateNewWiki/js/CreateNewWikiThemeDesignerOverrides.js
@@ -1,47 +1,47 @@
 /* global ThemeDesigner, themes */
 
-/**
- * @returns {void}
- */
-ThemeDesigner.init = function () {
+(function () {
 	'use strict';
-	$('#ThemeTab li label').remove();
-};
+	/**
+	 * @returns {void}
+	 */
+	ThemeDesigner.init = function () {
+		$('#ThemeTab li label').remove();
+	};
 
-/**
- * @param {string} setting
- * @param {string} newValue
- *
- * @returns {void}
- */
-ThemeDesigner.set = function (setting, newValue) {
-	'use strict';
+	/**
+	 * @param {string} setting
+	 * @param {string} newValue
+	 *
+	 * @returns {void}
+	 */
+	ThemeDesigner.set = function (setting, newValue) {
+		var sassUrl;
 
-	var sassUrl;
+		// The newValue is either the name of a theme or a single setting.
+		// The latter should be handled as it is in the original method
+		// or the theme settings will be overwritten (CE-456)
+		if ('undefined' === typeof themes[newValue]) {
+			ThemeDesigner.settings[setting] = newValue;
+		} else {
+			ThemeDesigner.settings = themes[newValue];
+		}
 
-	// The newValue is either the name of a theme or a single setting.
-	// The latter should be handled as it is in the original method
-	// or the theme settings will be overwritten (CE-456)
-	if ('undefined' === typeof themes[newValue]) {
-		ThemeDesigner.settings[setting] = newValue;
-	} else {
-		ThemeDesigner.settings = themes[newValue];
-	}
+		sassUrl = $.getSassCommonURL(
+			'/skins/oasis/css/oasis.scss',
+			$.extend(ThemeDesigner.settings, window.applicationThemeSettings)
+		);
 
-	sassUrl = $.getSassCommonURL(
-		'/skins/oasis/css/oasis.scss',
-		$.extend(ThemeDesigner.settings, window.applicationThemeSettings)
-	);
+		$.getCSS(sassUrl, function (link) {
+			$(ThemeDesigner.link).remove();
+			ThemeDesigner.link = link;
+		});
+	};
 
-	$.getCSS(sassUrl, function (link) {
-		$(ThemeDesigner.link).remove();
-		ThemeDesigner.link = link;
-	});
-};
-
-/**
- * Empty stub to prevent ThemeDesigner from saving state
- *
- * @returns {void}
- */
-ThemeDesigner.save = function () {};
+	/**
+	 * Empty stub to prevent ThemeDesigner from saving state
+	 *
+	 * @returns {void}
+	 */
+	ThemeDesigner.save = function () {};
+})();
