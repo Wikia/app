4b8aa452fe681e904695b2c4bc99c9aa76439946
Władysław Bodzek <wladek@wikia-inc.com>
2016-05-04 14:57:35 +0200

    PLATFORM-2178: Fix escaping

--- extensions/wikia/ArticleComments/modules/templates/ArticleComments_Index.php
+++ extensions/wikia/ArticleComments/modules/templates/ArticleComments_Index.php
@@ -1,7 +1,7 @@
 <? if ( $isMiniEditorEnabled ): ?>
 	<?= $app->renderView( 'MiniEditorController', 'Setup' ) ?>
 <? endif ?>
-<section id="WikiaArticleComments" class="WikiaArticleComments noprint" data-page="<?= $page ?>">
+<section id="WikiaArticleComments" class="WikiaArticleComments noprint" data-page="<?= htmlspecialchars($page) ?>">
 	<? if ( empty( $isLoadingOnDemand ) ): ?>
 		<?= $app->renderView( 'ArticleCommentsController', 'Content' ) ?>
 	<? endif ?>
