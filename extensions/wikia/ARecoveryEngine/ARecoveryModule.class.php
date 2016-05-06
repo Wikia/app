9b5d653ba398815fd27bdb7ed2ff34f615b7a19f
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-05-06 16:46:25 +0200

    ADEN-3318 Fix whitespaces

--- extensions/wikia/ARecoveryEngine/ARecoveryModule.class.php
+++ extensions/wikia/ARecoveryEngine/ARecoveryModule.class.php
@@ -1,11 +1,11 @@
 <?php
 
 class ARecoveryModule {
-    /**
-     * Checks whether recovery is enabled (on current wiki)
-     *
-     * @return bool
-     */
+	/**
+	 * Checks whether recovery is enabled (on current wiki)
+	 *
+	 * @return bool
+	 */
 	public static function isEnabled() {
 		global $wgEnableUsingSourcePointProxyForCSS;
 
