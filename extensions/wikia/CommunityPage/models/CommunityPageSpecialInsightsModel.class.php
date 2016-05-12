fb28f1ffe5220081c6cde33c1c1cc0ab9d25d110
Gautam Bajaj <gbajaj@wikia-inc.com>
2016-05-12 15:38:21 +0200

    Added sorting on view full list

--- extensions/wikia/CommunityPage/models/CommunityPageSpecialInsightsModel.class.php
+++ extensions/wikia/CommunityPage/models/CommunityPageSpecialInsightsModel.class.php
@@ -61,7 +61,7 @@ class CommunityPageSpecialInsightsModel {
 		$insightPages['title'] = wfMessage( 'communitypage-' . $type. '-title' )->text();
 		$insightPages['description'] =  wfMessage( 'communitypage-' . $type. '-description' )->text();
 
-		$insightPages['fulllistlink'] = SpecialPage::getTitleFor( 'Insights', $type )->getLocalURL();
+		$insightPages['fulllistlink'] = SpecialPage::getTitleFor( 'Insights', $type )->getLocalURL() . "?sort=" . self::INSIGHTS_MODULE_SORT_TYPE;
 
 		$insightPages = $this->addLastRevision( $insightPages );
 
