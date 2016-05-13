875d22561fb5b195656856b6fa67dcea6f73f9a6
Bogna Knychala <bogna.ka@gmail.com>
2016-05-13 11:41:52 +0200

    SUS-523 | add empty test files for each task

---
+++ extensions/wikia/CreateNewWiki/tests/tasks/CreateDatabaseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class CreateDatabaseTest extends WikiaBaseTest
+{
+
+	public function setUp()
+	{
+		$this->setupFile = dirname(__FILE__) . '/../CreateNewWiki_setup.php';
+		parent::setUp();
+	}
+
+	public function tearDown()
+	{
+		parent::tearDown();
+	}
+}
