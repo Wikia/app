e8069d45882e20c7eb032bde78e3f60f3f7982dc
Bogna Knychala <bogna.ka@gmail.com>
2016-05-17 17:06:32 +0200

    SUS-523 create tests for ImportStarterData

--- extensions/wikia/CreateNewWiki/tasks/ImportStarterData.php
+++ extensions/wikia/CreateNewWiki/tasks/ImportStarterData.php
@@ -11,7 +11,7 @@ class ImportStarterData implements Task {
 	/** @var  TaskContext */
 	private $taskContext;
 
-	public function __construct($taskContext) {
+	public function __construct( $taskContext ) {
 		$this->taskContext = $taskContext;
 	}
 
@@ -21,22 +21,53 @@ class ImportStarterData implements Task {
 
 	public function check() {
 		// php-cli is required for spawning PHP maintenance scripts
-		if( !file_exists( $this->phpBin ) && !is_executable( $this->phpBin ) ) {
-			return TaskResult::createForError( $this->phpBin . " doesn't exist or is not an executable");
+		if ( !$this->canExecute() ) {
+			return TaskResult::createForError( $this->phpBin . " doesn't exist or is not an executable" );
 		} else {
 			return TaskResult::createForSuccess();
 		}
 	}
 
 	public function run() {
-		global $IP;
-
 		// BugId:15644 - I need to pass $this->sDbStarter to CreateWikiLocalJob::changeStarterContributions
 		$starterDatabase = Starters::getStarterByLanguage( $this->taskContext->getLanguage() );
 		$this->taskContext->setStarterDb( $starterDatabase );
 
 		// import a starter database XML dump from DFS
 		$then = microtime( true );
+
+		$retVal = $this->executeShell();
+
+		if ( $retVal > 0 ) {
+			return TaskResult::createForError( 'starter dump import failed', [
+				'starter' => $starterDatabase,
+				'retval' => $retVal
+			] );
+		}
+
+		if ( $retVal > 0 ) {
+			return TaskResult::createForError( 'starter dump import failed', [
+				'starter' => $starterDatabase,
+				'retval' => $retVal
+			] );
+		}
+
+		$took = microtime( true ) - $then;
+		TaskHelper::waitForSlaves( $this->taskContext, __METHOD__ );
+
+		return TaskResult::createForSuccess( [
+			'retval' => $retVal,
+			'starter' => $starterDatabase,
+			'took' => $took
+		] );
+	}
+
+	public function canExecute() {
+		return !file_exists( $this->phpBin ) && !is_executable( $this->phpBin );
+	}
+
+	public function executeShell() {
+		global $IP;
 
 		$cmd = sprintf(
 			"SERVER_ID=%d %s %s/maintenance/importStarter.php",
@@ -46,20 +77,6 @@ class ImportStarterData implements Task {
 		);
 		wfShellExec( $cmd, $retVal );
 
-		if ( $retVal > 0 ) {
-			return TaskResult::createForError( 'starter dump import failed', [
-				'starter' => $starterDatabase,
-				'retval'  => $retVal
-			] );
-		}
-
-		$took = microtime( true ) - $then;
-		TaskHelper::waitForSlaves( $this->taskContext, __METHOD__ );
-
-		return TaskResult::createForSuccess( [
-			'retval'  => $retVal,
-			'starter' => $starterDatabase,
-			'took'    => $took
-		] );
+		return $retVal;
 	}
 }
