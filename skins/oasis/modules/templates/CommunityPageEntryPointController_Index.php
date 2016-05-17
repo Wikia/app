c170eaedde9ba03630fcbd6f4a02d78ea100ab17
Kamil Koterba <kamil@wikia-inc.com>
2016-05-17 11:41:20 +0200

    JPN-538 Break line before (but not after) wiki name when it's long

--- skins/oasis/modules/templates/CommunityPageEntryPointController_Index.php
+++ skins/oasis/modules/templates/CommunityPageEntryPointController_Index.php
@@ -1,11 +1,16 @@
 <section class="community-page-entry-point-module module">
 	<div class="community-page-entry-point-logo"></div>
-	<div class="community-page-entry-point-new">
-		<?= wfMessage( 'communitypage-new' )->escaped() ?>
-	</div>
 	<div class="community-page-entry-point-container">
+		<div class="community-page-entry-point-new">
+			<?= wfMessage( 'communitypage-new' )->escaped() ?>
+		</div>
 		<div class="community-page-entry-point-description">
-			<?= wfMessage( 'communitypage-help-us-grow' )->parse() ?>
+			<?php
+				/* compage-entry-point-nowrap element starts within message and ends after as we want
+ 				 * to break line before site name but not after (JPN-538)
+				 */
+			?>
+			<?= wfMessage( 'communitypage-help-us-grow', $noWrapOpenWithSiteName, $noWrapClose )->parse() ?>
 		</div>
 		<div>
 			<a href="<?= SpecialPage::getTitleFor( 'Community' )->getLocalURL(); ?>" class="community-page-entry-point-button"><?= wfMessage( 'communitypage-entry-button' )->escaped() ?></a>
