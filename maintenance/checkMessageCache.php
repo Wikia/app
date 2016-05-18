bfed5625cad286fac6abfeb1a3275a895ae7c3a5
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2016-05-18 14:05:03 +0200

    maintenance scripts update

--- maintenance/checkMessageCache.php
+++ maintenance/checkMessageCache.php
@@ -1,19 +1,27 @@
 <?php
 
+// Run from /usr/wikia/slot1/current/maintenance with $sudo SERVER_ID=177 php checkMessageCache.php
+
 require_once( dirname( __FILE__ ) . '/commandLine.inc' );
 error_reporting( E_ALL );
 ini_set( 'display_errors', 'On' );
 
-foreach ( Language::getLanguageNames() as $languageCode => $languageName ) {
+$langs = Language::getLanguageNames();
+$langs_count = count($langs);
+$i = 0;
+
+foreach ( $langs as $languageCode => $languageName ) {
     $lang = Language::factory( $languageCode );
     $mc = $lang->getLocalisationCache();
 
+    echo $i++ . '/' . $langs_count. ': ' . $languageCode;
+
     /** @var $reader CdbReader_DBA */
     // production/sandbox
-    $reader = dba_open( '/usr/wikia/slot1/current/cache/messages/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
+    // $reader = dba_open( '/usr/wikia/slot1/current/cache/messages/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
 
     // devbox
-    //$reader = dba_open( '/var/cache/mediawiki/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
+    $reader = dba_open( '/var/cache/mediawiki/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
 
     $firstKey = dba_firstkey( $reader );
     if ( startsWith( $firstKey, 'messages:' ) ) {
--- maintenance/compareCaches.php
+++ maintenance/compareCaches.php
@@ -1,5 +1,7 @@
 <?php
 
+// php compareCaches.php
+
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 
