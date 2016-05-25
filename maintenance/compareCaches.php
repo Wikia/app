ea3b16f20de0127eb9e1013413a73c5882b97012
rafal.kalinski <kalina@wikia-inc.com>
2016-05-25 11:29:58 +0000

    SUS-374-comparator | more readable diff log

--- maintenance/compareCaches.php
+++ maintenance/compareCaches.php
@@ -7,11 +7,12 @@ ini_set('display_errors','On');
 define(MESSAGE_CACHE_DIR, './messageCache');
 
 function checkLang($lang) {
-    $s1 = unserialize(file_get_contents(MESSAGE_CACHE_DIR . '/messageCache-new.dump.' . $lang . '.php'));
-    $s2 = unserialize(file_get_contents(MESSAGE_CACHE_DIR . '/messageCache-orig.dump.' . $lang . '.php'));
+    $s1 = unserialize(file_get_contents(MESSAGE_CACHE_DIR . '/messageCache-orig.dump.' . $lang . '.php'));
+    $s2 = unserialize(file_get_contents(MESSAGE_CACHE_DIR . '/messageCache-new.dump.' . $lang . '.php'));
 
     $k1 = array_keys($s1);
     $k2 = array_keys($s2);
+    $diff_details = [];
 
     if(count(array_diff($k1,$k2)) == 0) {
         echo $lang . ' has all the keys expected' . PHP_EOL;
@@ -20,28 +21,17 @@ function checkLang($lang) {
         foreach(array_intersect($k1,$k2) as $key) {
             if($s1[$key] !== $s2[$key]) {
                 $diffs++;
-                # uncomment to see keys / differing values
-                # print PHP_EOL . $key;
-                # print PHP_EOL. PHP_EOL . $s1[$key] . PHP_EOL . ' vs ' . PHP_EOL . $s2[$key] . PHP_EOL . PHP_EOL;
+
+                $diff_details['DIFFERENCES'][$key] = [$s1[$key], $s2[$key]];
             }
         }
 
         echo $lang . ' has ' . $diffs . ' differing values' . PHP_EOL;
     } else {
         echo $lang . ' has ' . count(array_diff($k1,$k2)) . ' missing keys' . PHP_EOL;
-        if ( count($k2) > count($k1) ) {
-            echo 'OLD is larger' . PHP_EOL;
-        }
-        if ( count($k1) > count($k2) ) {
-            echo 'NEW is larger' . PHP_EOL;
 
-            $a = [];
-
-            foreach(array_diff($k1,$k2) as $k => $v) {
-                $a[] = $v;
-            }
-
-            file_put_contents(MESSAGE_CACHE_DIR . '/diff.'.$lang.'.txt', implode(PHP_EOL, $a));
+        foreach ( array_diff($k1,$k2) as $key ) {
+            $diff_details['MISSING-IN-NEW'][$key] = [$s1[$key], null];
         }
     }
 
@@ -49,13 +39,32 @@ function checkLang($lang) {
         echo $lang . ' has no excess keys' . PHP_EOL;
     } else {
         echo $lang . ' has ' . count(array_diff($k2,$k1)) . ' excess keys' . PHP_EOL;
-        if ( count($k2) > count($k1) ) {
-            echo 'OLD is larger' . PHP_EOL;
-        }
-        if ( count($k1) > count($k2) ) {
-            echo 'NEW is larger' . PHP_EOL;
+
+        foreach ( array_diff($k2,$k1) as $key ) {
+            $diff_details['EXCESS-IN-NEW'][$key] = [null, $s2[$key]];
         }
     }
+
+    // log differences
+
+    $diff_content = [ $lang ];
+    foreach ($diff_details as $section => $diff ) {
+        $diff_content[] = '----------------------'. $section . '----------------------';
+        foreach ($diff as $k => $part) {
+            $diff_content[] = '<<<<<';
+            $diff_content[] = '* ' . $k;
+            if ( isset($part[0]) ) {
+                $diff_content[] = '- ' . $part[0];
+            }
+            $diff_content[] = '----';
+            if ( isset($part[1]) ) {
+                $diff_content[] = '+ ' . $part[1];
+            }
+            $diff_content[] = '>>>>>' . PHP_EOL;
+        }
+    }
+
+    file_put_contents ( MESSAGE_CACHE_DIR . '/diff.'.$lang.'.txt', join ( PHP_EOL, $diff_content ) );
 }
 
 $langCodes = [ 'en', 'pl', 'de', 'es', 'fr', 'it', 'ja', 'nl', 'pt', 'ru', 'zh-hans', 'zh-tw' ];
@@ -64,3 +73,4 @@ foreach($langCodes as $code) {
     echo '========' . PHP_EOL;
     checkLang($code);
 }
+
