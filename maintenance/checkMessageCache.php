f0bc68d63e54dc9a9d6cf1af0e8ae06231fcfa6b
rafal.kalinski <kalina@wikia-inc.com>
2016-05-25 09:16:30 +0000

    SOC-374-comparator | messageCache for old and for new at once

--- maintenance/checkMessageCache.php
+++ maintenance/checkMessageCache.php
@@ -6,42 +6,49 @@ require_once( dirname( __FILE__ ) . '/commandLine.inc' );
 error_reporting( E_ALL );
 ini_set( 'display_errors', 'On' );
 
+define(MESSAGE_CACHE_DIR, './messageCache');
+
 //$langs = Language::getLanguageNames();
 $langs = [ 'en', 'pl', 'de', 'es', 'fr', 'it', 'ja', 'nl', 'pt', 'ru', 'zh-hans', 'zh-tw' ];
 $langs_count = count($langs);
+$localizationsCacheFoldersPostfix = [ 'new', 'orig' ];
 $i = 0;
 
-foreach ( $langs as $languageCode ) {
-    $lang = Language::factory( $languageCode );
-    $mc = $lang->getLocalisationCache();
-
-    echo $i++ . '/' . $langs_count. ': ' . $languageCode . PHP_EOL;
-
-    /** @var $reader CdbReader_DBA */
-    // production/sandbox
-//     $reader = dba_open( '/usr/wikia/slot1/current/cache/messages/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
-
-    // devbox
-//    $reader = dba_open( '/var/cache/mediawiki/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
-    $reader = dba_open( '/tmp/messagecache-new/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
-//    $reader = dba_open( '/tmp/messagecache-orig/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
-
-    $firstKey = dba_firstkey( $reader );
-    if ( startsWith( $firstKey, 'messages:' ) ) {
-        $messages = [ $firstKey => dba_fetch( $firstKey, $reader ) ];
-    } else {
-        $messages = [ ];
-    }
-    while ( $currentKey = dba_nextkey( $reader ) ) {
-        if ( startsWith( $currentKey, 'messages:' ) ) {
-            $messages [$currentKey] = unserialize( dba_fetch( $currentKey, $reader ) );
-        }
-    }
-    ksort($messages);
-
-    //foreach ($messages as $k => $m) echo "\t" . $k . PHP_EOL;
-
-    file_put_contents( './messageCache/messageCache-new.dump.' . $languageCode . '.php', serialize( $messages ) );
-//    file_put_contents( './messageCache/messageCache-orig.dump.' . $languageCode . '.php', serialize( $messages ) );
+if (!is_dir(MESSAGE_CACHE_DIR)) {
+	mkdir(MESSAGE_CACHE_DIR, 0775) or exit ('Was not able to create the result directory');
 }
 
+
+foreach ( $localizationsCacheFoldersPostfix as $cachePostfix ) {
+	echo 'checking \'' . $cachePostfix . '\' message cache' . PHP_EOL;
+
+	foreach ( $langs as $languageCode ) {
+		$lang = Language::factory( $languageCode );
+		$mc = $lang->getLocalisationCache();
+
+		echo $i++ . '/' . $langs_count . ': ' . $languageCode . PHP_EOL;
+
+		/** @var $reader CdbReader_DBA */
+		// production/sandbox
+		//     $reader = dba_open( '/usr/wikia/slot1/current/cache/messages/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
+
+		$reader = dba_open( '/tmp/messagecache-' . $cachePostfix .  '/l10n_cache-' . $languageCode . '.cdb', 'r', 'cdb' );
+
+		$firstKey = dba_firstkey( $reader );
+		if ( startsWith( $firstKey, 'messages:' ) ) {
+			$messages = [ $firstKey => dba_fetch( $firstKey, $reader ) ];
+		} else {
+			$messages = [ ];
+		}
+		while ( $currentKey = dba_nextkey( $reader ) ) {
+			if ( startsWith( $currentKey, 'messages:' ) ) {
+				$messages [$currentKey] = unserialize( dba_fetch( $currentKey, $reader ) );
+			}
+		}
+		ksort( $messages );
+
+		//foreach ($messages as $k => $m) echo "\t" . $k . PHP_EOL;
+
+		file_put_contents( MESSAGE_CACHE_DIR . '/messageCache-' . $cachePostfix .  '.dump.' . $languageCode . '.php', serialize( $messages ) );
+	}
+}
