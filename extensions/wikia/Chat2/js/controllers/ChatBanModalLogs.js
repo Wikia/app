f793bed7e8995fca0bbc438b8cc816a4f921d0f4
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-05-31 10:46:51 +0200

    don't escape title - it is escaped by default in mustache

--- extensions/wikia/Chat2/js/controllers/ChatBanModalLogs.js
+++ extensions/wikia/Chat2/js/controllers/ChatBanModalLogs.js
@@ -1,29 +1,29 @@
-var ChatBanLogController = function() {
-    $('.chat-change-ban').click(function(e) {
-        e.preventDefault();
-		var title = mw.html.escape($.msg('chat-ban-modal-change-ban-heading')),
-            userId = $(this).data('user-id'),
-			okCallback = function(expires, reason) {
-			 	$.post(wgScript + '?action=ajax&rs=ChatAjax&method=blockOrBanChat' ,{
+var ChatBanLogController = function () {
+	$('.chat-change-ban').click(function (event) {
+		var title = $.msg('chat-ban-modal-change-ban-heading'),
+			userId = $(this).data('user-id'),
+			okCallback = function (expires, reason) {
+				$.post(wgScript + '?action=ajax&rs=ChatAjax&method=blockOrBanChat', {
 						userToBanId: userId,
 						time: expires,
 						reason: reason,
 						mode: 'global',
 						token: mw.user.tokens.get('editToken')
 					},
-					$.proxy(function(data) {
+					function () {
 						window.location.reload();
-					})
-			 	);
- 			};
+					}
+				);
+			},
+			chatBanModal = new ChatBanModal(title, okCallback, {'isChangeBan': true, userId: userId});
 
-		var chatBanModal = new ChatBanModal(title, okCallback, {'isChangeBan':true, userId:userId });
-    });
-}
+		event.preventDefault();
+	});
+};
 
 //
 // Bootstrap the app
 //
-$(function() {
-    window.ChatBanLogPage = new ChatBanLogController();
+$(function () {
+	window.ChatBanLogPage = new ChatBanLogController();
 });
