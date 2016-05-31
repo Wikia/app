7b9c8d5283deafa8732a2ce44c271f34b820b7d4
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-05-31 15:41:34 +0200

    ADEN-3400 Add new MOBILE_BOTTOM_LEADERBOARD slot

--- extensions/wikia/AdEngine/js/provider/directGptMobile.js
+++ extensions/wikia/AdEngine/js/provider/directGptMobile.js
@@ -11,6 +11,7 @@ define('ext.wikia.adEngine.provider.directGptMobile', [
 		{
 			INVISIBLE_HIGH_IMPACT:      {size: '1x1'},
 			MOBILE_TOP_LEADERBOARD:     {size: '320x50,320x100,300x250,300x50,1x1'},
+			MOBILE_BOTTOM_LEADERBOARD:  {size: '300x50,300x250,320x50,320x100,320x480'},
 			MOBILE_IN_CONTENT:          {size: '320x50,300x250,300x50,1x1'},
 			MOBILE_IN_CONTENT_EXTRA_1:  {size: '300x250,1x1'},
 			MOBILE_IN_CONTENT_EXTRA_2:  {size: '300x250,1x1'},
