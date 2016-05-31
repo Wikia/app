08f92df57af5a53bda35d343b2e3aff3d36288ee
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-05-31 15:47:52 +0200

    ADEN-3400 Remove 1x1 sizes from mobile providers

--- extensions/wikia/AdEngine/js/provider/directGptMobile.js
+++ extensions/wikia/AdEngine/js/provider/directGptMobile.js
@@ -10,13 +10,13 @@ define('ext.wikia.adEngine.provider.directGptMobile', [
 		'mobile',
 		{
 			INVISIBLE_HIGH_IMPACT:      {size: '1x1'},
-			MOBILE_TOP_LEADERBOARD:     {size: '320x50,320x100,300x250,300x50,1x1'},
+			MOBILE_TOP_LEADERBOARD:     {size: '320x50,320x100,300x250,300x50'},
 			MOBILE_BOTTOM_LEADERBOARD:  {size: '300x50,300x250,320x50,320x100,320x480'},
-			MOBILE_IN_CONTENT:          {size: '320x50,300x250,300x50,1x1'},
-			MOBILE_IN_CONTENT_EXTRA_1:  {size: '300x250,1x1'},
-			MOBILE_IN_CONTENT_EXTRA_2:  {size: '300x250,1x1'},
-			MOBILE_IN_CONTENT_EXTRA_3:  {size: '300x250,1x1'},
-			MOBILE_PREFOOTER:           {size: '320x50,300x250,300x50,1x1'}
+			MOBILE_IN_CONTENT:          {size: '320x50,300x250,300x50'},
+			MOBILE_IN_CONTENT_EXTRA_1:  {size: '300x250'},
+			MOBILE_IN_CONTENT_EXTRA_2:  {size: '300x250'},
+			MOBILE_IN_CONTENT_EXTRA_3:  {size: '300x250'},
+			MOBILE_PREFOOTER:           {size: '320x50,300x250,300x50'}
 		}
 	);
 });
