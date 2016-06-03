f4be7b5921e5195ed6267226c089cfda4d7cd220
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-06-03 09:46:36 +0000

    JPN-559: avoid data duplication

--- extensions/wikia/CommunityPage/CommunityPageSpecialController.class.php
+++ extensions/wikia/CommunityPage/CommunityPageSpecialController.class.php
@@ -37,8 +37,12 @@ class CommunityPageSpecialController extends WikiaSpecialPageController {
 			'adminWelcomeMsg' => $this->msg( 'communitypage-admin-welcome-message' )->text(),
 			'pageListEmptyText' => $this->msg( 'communitypage-page-list-empty' )->plain(),
 			'pageTitle' => $this->msg( 'communitypage-title' )->plain(),
-			'topContributors' => $this->sendRequest( 'CommunityPageSpecialController', 'getTopContributorsData' )
-				->getData(),
+			'topContributors' => $this->sendRequest(
+				'CommunityPageSpecialController',
+				'getTopContributorsData',
+				[ 'limit' => self::TOP_CONTRIBUTORS_MODULE_LIMIT ]
+
+			)->getData(),
 			'topAdminsData' => $this->sendRequest( 'CommunityPageSpecialController', 'getTopAdminsData' )
 				->getData(),
 			'recentlyJoined' => $this->sendRequest( 'CommunityPageSpecialController', 'getRecentlyJoinedData' )
@@ -55,14 +59,17 @@ class CommunityPageSpecialController extends WikiaSpecialPageController {
 	 * @return array
 	 */
 	public function getTopContributorsData() {
+		$limit = $this->request->getInt( 'limit', 0 );
+
 		$currentUserContributionCount = ( new UserStatsService( $this->getUser()->getId() ) )->getEditCountFromWeek();
 		$topContributors = $this->usersModel->getTopContributors();
+		$topContributorsCount = count( $topContributors );
+
+		if ( $limit > 0 ) {
+			$topContributors = array_slice( $topContributors, 0, $limit );
+		}
+
 		$topContributorsDetails = $this->getContributorsDetails( $topContributors );
-		$topContributorsDetailsLimitedForModule = array_slice(
-			$topContributorsDetails,
-			0,
-			self::TOP_CONTRIBUTORS_MODULE_LIMIT
-		);
 
 		$this->response->setData( [
 			'admin' => $this->msg( 'communitypage-admin' )->plain(),
@@ -72,14 +79,13 @@ class CommunityPageSpecialController extends WikiaSpecialPageController {
 				->numParams( $this->wg->Lang->formatNum( $currentUserContributionCount ) )
 				->text(),
 			'noContribsText' => $this->msg( 'communitypage-no-contributions' )->plain(),
-			'contributors' => $topContributorsDetailsLimitedForModule,
-			'allContributors' => $topContributorsDetails,
+			'contributors' => $topContributorsDetails,
 			'userAvatar' => AvatarService::renderAvatar(
 				$this->getUser()->getName(),
 				AvatarService::AVATAR_SIZE_SMALL_PLUS
 			),
 			'userRank' => $this->calculateCurrentUserRank( $currentUserContributionCount , $topContributors ),
-			'weeklyEditorCount' => $this->formatTotalEditorsNumber( count( $topContributors ) ),
+			'weeklyEditorCount' => $this->formatTotalEditorsNumber( $topContributorsCount ),
 			'userContribCount' => $currentUserContributionCount
 		] );
 	}
