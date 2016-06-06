36a6f16454714619fd2ddc4921f2bb35d71526e3
Igor Rogatty <igor@wikia-inc.com>
2016-06-06 17:55:16 +0200

    SUS-563 | more verbose logs, more generic pattern

--- maintenance/wikia/corruptedMainPages.php
+++ maintenance/wikia/corruptedMainPages.php
@@ -2,24 +2,28 @@
 
 require_once( dirname( __FILE__ ) . '/../commandLine.inc' );
 
-global $wgCityId;
-
 function isCorrupted() {
+	global $wgCityId;
+
 	$title = Title::newFromText( 'Main_Page' );
 	if ( empty ($title) || !$title->exists() ) {
+		echo( "\n Title doesn't exist on wiki " . $wgCityId );
 		return false;
 	}
 
 	$mainPage = Article::newFromID( $title->getArticleID() );
 	if ( empty ($mainPage) || !$mainPage->exists() ) {
+		echo( "\n Article doesn't exist on wiki " . $wgCityId );
 		return false;
 	}
 
-	$pattern = '/<hero description="My description" imagename="" cropposition="" \/>/';
+	$pattern = '/<hero .*\/>/Am';
 	$raw = $mainPage->getContent();
 
 	return preg_match($pattern, $raw) === 1;
 }
+
+global $wgCityId;
 
 echo( "\n Processing..." );
 if ( isCorrupted() ) {
