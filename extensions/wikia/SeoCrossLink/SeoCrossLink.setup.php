efbaf203a7a746c400a65c34c0de552af8b0d3fa
Piotr Gabryjeluk <rychu@wikia-inc.com>
2016-06-06 16:02:28 -0700

    SEO-366 Fix setup file

--- extensions/wikia/SeoCrossLink/SeoCrossLink.setup.php
+++ extensions/wikia/SeoCrossLink/SeoCrossLink.setup.php
@@ -2,5 +2,6 @@
 
 // Autoload
 $wgAutoloadClasses['Wikia\SeoCrossLink\CrossLinkHooks'] =  __DIR__ . '/classes/CrossLinkHooks.class.php';
+$wgAutoloadClasses['Wikia\SeoCrossLink\CrossLinkInserter'] =  __DIR__ . '/classes/CrossLinkInserter.class.php';
 
 $wgHooks['OutputPageBeforeHTML'][] = 'Wikia\SeoCrossLink\CrossLinkHooks::onOutputPageBeforeHTML';
