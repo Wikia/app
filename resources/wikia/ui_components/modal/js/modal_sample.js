d0da45e8d69b2eec73f2696fd3282b41bd521a37
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-06-17 10:26:04 +0000

    CE-3837: Introduce new modal size (content-size)

--- resources/wikia/ui_components/modal/js/modal_sample.js
+++ resources/wikia/ui_components/modal/js/modal_sample.js
@@ -158,6 +158,47 @@ require( [ 'jquery', 'wikia.ui.factory' ], function( $, uiFactory ) {
 					} );
 				} );
 			} );
+
+			// opening a content-size modal example
+			$( '#showContentSizeModalExample' ).click( function() {
+				var modalConfig = {
+					type: 'default',
+					vars: {
+						id: 'contentSizeModalExample',
+						size: 'content-size',
+						content: '<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that\'s what you see at a toy store. And you must think you\'re in a toy store, because you\'re here shopping for an infant named Jeb.</p><p>Your bones don\'t break, mine do. That\'s clear. Your cells react to bacteria and viruses differently than mine. You don\'t get sick, I do. That\'s also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We\'re on the same curve, just on opposite ends.</p><p>Your bones don\'t break, mine do. That\'s clear. Your cells react to bacteria and viruses differently than mine. You don\'t get sick, I do. That\'s also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We\'re on the same curve, just on opposite ends.</p><p>My money\'s in that office, right? If she start giving me some bullshit about it ain\'t there, and we got to go someplace else and get it, I\'m gonna shoot you in the head then and there. Then I\'m gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I\'m talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?</p><p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don\'t know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I\'m breaking now. We said we\'d say it was the snow that killed the other two, but it wasn\'t. Nature is lethal but it doesn\'t hold a candle to man.</p>',
+						class: 'styleguide-example-content-size',
+						title: 'Content Size modal example.',
+						closeText: 'Close',
+						buttons: [
+							{
+								vars: {
+									value: 'Submit',
+									classes: 'primary',
+									data: [
+										{
+											key: 'event',
+											value: 'close'
+										}
+									]
+								}
+							},
+							{
+								vars: {
+									value: 'Cancel',
+									data: [
+										{
+											key: 'event',
+											value: 'close'
+										}
+									]
+								}
+							}
+						]
+					}
+				};
+				showModal( modalConfig );
+			} );
 		} );
 	} );
 
