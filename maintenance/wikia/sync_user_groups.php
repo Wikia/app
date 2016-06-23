cb4d0a087e5daea8d18a5852f09fdc8b2ed2aa11
nmonterroso <nelsonmonterroso@gmail.com>
2016-06-23 09:20:23 -0700

    removing command line arg and relying on SERVER_ID env variable

--- maintenance/wikia/sync_user_groups.php
+++ maintenance/wikia/sync_user_groups.php
@@ -9,20 +9,6 @@ require_once(dirname(__FILE__).'/../Maintenance.php');
 
 class SyncWikiUserGroups extends Maintenance {
 
-	public function __construct() {
-		parent::__construct();
-
-		$this->addOption('wiki_id', '', true, true);
-	}
-
-	public function loadParamsAndArgs($self = null, $opts = null, $args = null) {
-		parent::loadParamsAndArgs($self, $opts, $args);
-
-		if (!empty($this->mOptions['wiki_id'])) {
-			putenv("SERVER_ID={$this->mOptions['wiki_id']}");
-		}
-	}
-
 	public function execute() {
 		global $wgCityId;
 
