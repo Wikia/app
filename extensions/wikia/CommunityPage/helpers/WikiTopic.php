2b6151856949562e0b62334f3d1cc58114a5c298
Kamil Koterba <kamil@wikia-inc.com>
2016-06-24 18:58:17 +0200

    CE-3774 Add test for fallback to sitename

--- extensions/wikia/CommunityPage/helpers/WikiTopic.php
+++ extensions/wikia/CommunityPage/helpers/WikiTopic.php
@@ -4,10 +4,10 @@ class WikiTopic {
 	public static function getWikiTopic() {
 		global $wgSitename, $wgWikiTopic;
 
-		return $wgWikiTopic ? $wgWikiTopic : WikiTopic::prepareWikiTopic( $wgSitename );
+		return $wgWikiTopic ? $wgWikiTopic : self::prepareWikiTopic( $wgSitename );
 	}
 
-	public static function prepareWikiTopic( $siteName ) {
+	private static function prepareWikiTopic( $siteName ) {
 		return trim( preg_replace( '/wikia?$/i', '', $siteName ) );
 	}
 }
--- extensions/wikia/CommunityPage/tests/WikiTopicTest.php
+++ extensions/wikia/CommunityPage/tests/WikiTopicTest.php
@@ -6,16 +6,33 @@ class WikiTopicTest extends WikiaBaseTest {
 		$this->setupFile = __DIR__ . '/../CommunityPage.setup.php';
 		parent::setUp();
 	}
+
 	/**
-	 * @dataProvider FormingWikiTopicProvider
+	 * @dataProvider fallbackToSitenameProvider
 	 */
-	public function testFormingWikiTopic( $sitename, $expectedWikiTopic ) {
+	public function testFallbackToSitename( $sitename, $wikiTopic, $expectedWikiTopic ) {
+		$this->mockGlobalVariable( 'wgWikiTopic', $wikiTopic );
+		$this->mockGlobalVariable( 'wgSitename', $sitename );
+		$this->assertEquals( $expectedWikiTopic, WikiTopic::getWikiTopic() );
+	}
+
+	/**
+	 * @dataProvider preparingWikiTopicFromSitenameProvider
+	 */
+	public function testPreparingWikiTopicFromSitename( $sitename, $expectedWikiTopic ) {
 		$this->mockGlobalVariable( 'wgWikiTopic', null );
 		$this->mockGlobalVariable( 'wgSitename', $sitename );
 		$this->assertEquals( $expectedWikiTopic, WikiTopic::getWikiTopic() );
 	}
 
-	public function FormingWikiTopicProvider() {
+	public function fallbackToSitenameProvider() {
+		return [
+			[ 'Wookiepedia', 'Star Wars', 'Star Wars' ],
+			[ 'Wookiepedia', null, 'Wookiepedia' ],
+		];
+	}
+
+	public function preparingWikiTopicFromSitenameProvider() {
 		return [
 			[ 'Muppet', 'Muppet' ],
 			[ 'Muppet Wiki', 'Muppet' ],
