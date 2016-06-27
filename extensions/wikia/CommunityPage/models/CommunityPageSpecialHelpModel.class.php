a27800ec1e2bebe06fa12d83bd890114c7bcb4de
Gautam <gautam1237@gmail.com>
2016-06-27 19:31:26 +0900

    JPN-577 | updated Help module

--- extensions/wikia/CommunityPage/models/CommunityPageSpecialHelpModel.class.php
+++ extensions/wikia/CommunityPage/models/CommunityPageSpecialHelpModel.class.php
@@ -4,14 +4,30 @@ class CommunityPageSpecialHelpModel {
 	public function getData() {
 		return [
 			'title' => wfMessage( 'communitypage-help-module-title' )->plain(),
-			'link' => $this->getHelpPageLink(),
-			'linkText' => wfMessage( 'communitypage-help-module-link-text' )->plain()
+			'editPage' => wfMessage( 'communitypage-help-edit-pages' )->plain(),
+			'addLinks' => wfMessage( 'communitypage-help-add-link' )->plain(),
+			'addNewPage' => wfMessage( 'communitypage-help-add-new-page' )->plain(),
+			'editPageLink' => $this->geteditPageLink(),
+			'addLinksPageLink' => $this->getAddLinksPageLink(),
+			'addNewPageLink' => $this->getAddNewPageLink()
 		];
 	}
-
-	private function getHelpPageLink() {
+	
+	private function geteditPageLink() {
 		return Title::newFromText(
-			wfMessage( 'communitypage-help-module-link-page-name' )->inContentLanguage()->plain(), NS_HELP
+			wfMessage( 'communitypage-help-module-edit-page-name' )->inContentLanguage()->plain(), NS_HELP
+		)->getLocalURL();
+	}
+
+	private function getAddLinksPageLink() {
+		return Title::newFromText(
+			wfMessage( 'communitypage-help-module-add-link-name' )->inContentLanguage()->plain(), NS_HELP
+		)->getLocalURL();
+	}
+
+	private function getAddNewPageLink() {
+		return Title::newFromText(
+			wfMessage( 'communitypage-help-module-new-page-name' )->inContentLanguage()->plain(), NS_HELP
 		)->getLocalURL();
 	}
 }
