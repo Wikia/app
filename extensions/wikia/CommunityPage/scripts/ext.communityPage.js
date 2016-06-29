e797cbedc28540519d9ad7ae9db966821ed39807
Per Johan Groland <per@akafugu.jp>
2016-06-29 17:22:56 +0900

    JPN-576 Removed redundant data-tracking label

--- extensions/wikia/CommunityPage/scripts/ext.communityPage.js
+++ extensions/wikia/CommunityPage/scripts/ext.communityPage.js
@@ -260,7 +260,7 @@ require([
 		$('.community-page-todo-list-module-content').on('mousedown touchstart', 'a', function (event) {
 			track({
 				category: 'community-page-todo-list-module',
-				label: event.delegateTarget.getAttribute('data-tracking'),
+				label: 'community-page-todo-list-module-content',
 			});
 		});
 	}
