67448ca5b704edef323230d9cb899546abb7327d
nAndy <nandy@wikia-inc.com>
2016-06-30 14:03:33 +0000

    ADEN-3509 updated indentation

--- extensions/wikia/AdEngine/templates/AdEngine2_Ad.php
+++ extensions/wikia/AdEngine/templates/AdEngine2_Ad.php
@@ -1,22 +1,22 @@
 <?php if ($showAd): ?>
-<!-- BEGIN SLOTNAME: <?= htmlspecialchars( $slotName ) ?> -->
-<div id="<?= htmlspecialchars( $slotName ) ?>" class="wikia-ad noprint default-height">
+	<!-- BEGIN SLOTNAME: <?= htmlspecialchars( $slotName ) ?> -->
+	<div id="<?= htmlspecialchars( $slotName ) ?>" class="wikia-ad noprint default-height">
 	<? if ($includeLabel): ?>
 		<label class="wikia-ad-label"><?= wfMessage( 'adengine-advertisement' )->escaped() ?></label>
 	<? endif; ?>
-<? if ($addToAdQueue): ?>
-<script>
-	<? if ($onLoad) { ?>
-		wgAfterContentAndJS.push(function () {
-			window.adslots2.push(<?= json_encode([$slotName]) ?>);
-		});
-	<? } else { ?>
-		window.adslots2.push(<?= json_encode([$slotName]) ?>);
-	<? } ?>
-</script>
-<? endif; ?>
-</div>
-<!-- END SLOTNAME: <?= htmlspecialchars($slotName) ?> -->
+	<? if ($addToAdQueue): ?>
+		<script>
+			<? if ($onLoad) { ?>
+				wgAfterContentAndJS.push(function () {
+					window.adslots2.push(<?= json_encode([$slotName]) ?>);
+				});
+			<? } else { ?>
+				window.adslots2.push(<?= json_encode([$slotName]) ?>);
+			<? } ?>
+		</script>
+	<? endif; ?>
+	</div>
+	<!-- END SLOTNAME: <?= htmlspecialchars($slotName) ?> -->
 <?php else: ?>
 <!-- NO AD <?= htmlspecialchars($slotName) ?> (levels: <?= htmlspecialchars(json_encode($pageTypes)) ?>) -->
 <?php endif; ?>
