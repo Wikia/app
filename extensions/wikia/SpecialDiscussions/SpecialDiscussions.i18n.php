a93ecff317d50fcdcde924bebd2958782313a9b0
Armon <armon@wikia-inc.com>
2016-07-11 02:19:25 +0000

    SOC-2203 Translations

--- extensions/wikia/SpecialDiscussions/SpecialDiscussions.i18n.php
+++ extensions/wikia/SpecialDiscussions/SpecialDiscussions.i18n.php
@@ -24,3 +24,136 @@ $messages['qqq'] = array(
 	'discussions-activate-error' => 'An error message telling user Discussion feature was not deactived and that the error has been recorded and that user should try again later.',
 	'action-specialdiscussions' => 'An action that a user has a forbidden access to when opening Special Page',
 );
+
+$messages['de'] = array(
+	'discussions' => 'Diskussionen',
+	'discussions-desc' => 'Community-Diskussionen von dieser Seite aus aktivieren oder aufrufen.',
+	'discussions-pagetitle' => 'Diskussionen-Kontrolle',
+	'discussions-activate' => 'Diskussionen aktivieren',
+	'discussions-active' => 'In dieser Community ist die Diskussionen-Funktion bereits aktiviert.',
+	'discussions-not-active' => 'In dieser Community ist die Diskussionen-Funktion nicht aktiviert.',
+	'discussions-navigate' => 'Weiter zu Diskussionen',
+	'discussions-activate-error' => 'Die Diskussionen-Funktion wurde aufgrund eines Fehlers nicht aktiviert. Der Fehler wurde aufgezeichnet. Bitte versuche es später noch einmal.',
+	'action-specialdiscussions' => 'Spezial:Diskussionen',
+);
+
+$messages['es'] = array(
+	'discussions' => 'Discusiones',
+	'discussions-desc' => 'Activar o acceder a las Discusiones de la comunidad desde esta página.',
+	'discussions-pagetitle' => 'Control de Discusiones',
+	'discussions-activate' => 'Activar Discusiones',
+	'discussions-active' => 'La funcionalidad de Discusiones ya se encuentra activada para esta comunidad.',
+	'discussions-not-active' => 'La funcionalidad de Discusiones no se encuentra activada para esta comunidad.',
+	'discussions-navigate' => 'Desplácese a Discusiones',
+	'discussions-activate-error' => 'La funcionalidad de Discusiones no se activó debido a un error. Se registró el error. Por favor inténtalo más tarde.',
+	'action-specialdiscussions' => 'Especial:Discusiones',
+);
+
+$messages['fr'] = array(
+	'discussions' => 'Discussions',
+	'discussions-desc' => 'Activez ou consultez les discussions de la communauté à partir de cette page.',
+	'discussions-pagetitle' => 'Contrôle des discussions',
+	'discussions-activate' => 'Activer les discussions',
+	'discussions-active' => 'La fonctionnalité Discussions est déjà activée pour cette communauté.',
+	'discussions-not-active' => "La fonctionnalité Discussions n'est pas activée pour cette communauté.",
+	'discussions-navigate' => 'Accéder aux discussions',
+	'discussions-activate-error' => "Une erreur s'est produite lors de l'activation de la fonctionnalité Discussions. L'erreur a été enregistrée. Veuillez réessayer ultérieurement.",
+	'action-specialdiscussions' => 'Spécial:Discussions',
+);
+
+$messages['it'] = array(
+	'discussions' => 'Discussioni',
+	'discussions-desc' => 'Attiva o accedi a Discussioni per la community da questa pagina.',
+	'discussions-pagetitle' => 'Controllo di Discussioni',
+	'discussions-activate' => 'Attiva Discussioni',
+	'discussions-active' => "L'estensione Discussioni è già attiva per questa community.",
+	'discussions-not-active' => "L'estensione Discussioni non è attiva per questa community.",
+	'discussions-navigate' => 'Vai a Discussioni',
+	'discussions-activate-error' => "L'estensione Discussioni non è stata attivata a causa di un errore. L'errore è stato registrato. Riprovare più tardi.",
+	'action-specialdiscussions' => 'Speciale:Discussioni',
+);
+
+$messages['ja'] = array(
+	'discussions' => 'トーク',
+	'discussions-desc' => 'このページからコミュニティのトークにアクセスもしくは機能を有効にします。',
+	'discussions-pagetitle' => 'トークを管理する方法について',
+	'discussions-activate' => 'トーク機能を有効にする',
+	'discussions-active' => 'トーク機能はこちらのコミュニティにおいてすでに有効になっています。',
+	'discussions-not-active' => 'こちらのコミュニティではトーク機能は有効になっていません。',
+	'discussions-navigate' => 'トークに移動',
+	'discussions-activate-error' => 'エラーが発生したため、トーク機能を有効にすることができませんでした。エラーは報告済みです。恐れ入りますが、後ほど再度お試しください。',
+	'action-specialdiscussions' => '特別:トーク',
+);
+
+$messages['nl'] = array(
+	'discussions' => 'Discussions',
+	'discussions-desc' => 'Activate or access community Discussions from this page.',
+	'discussions-pagetitle' => 'Discussions control',
+	'discussions-activate' => 'Activate Discussions',
+	'discussions-active' => 'Discussions feature is already activated for this community.',
+	'discussions-not-active' => 'Discussions feature is not activated for this community.',
+	'discussions-navigate' => 'Navigate to Discussions',
+	'discussions-activate-error' => 'Discussions feature was not activated due to an error. Error was recorded. Please try again later.',
+	'action-specialdiscussions' => 'Special:Discussions',
+);
+
+$messages['pl'] = array(
+	'discussions' => 'Dyskusje',
+	'discussions-desc' => 'Aktywuj lub uzyskaj dostęp do funkcji Dyskusji dla tej społeczności.',
+	'discussions-pagetitle' => 'Panel Dyskusji',
+	'discussions-activate' => 'Aktywuj Dyskusje',
+	'discussions-active' => 'Funkcja Dyskusje jest już aktywna dla tej społeczności.',
+	'discussions-not-active' => 'Funkcja Dyskusje nie jest aktywna dla tej społeczności.',
+	'discussions-navigate' => 'Przejdź do Dyskusji',
+	'discussions-activate-error' => 'Wystąpił błąd przy aktywowaniu funkcji Dyskusje. Błąd został zapisany. Spróbuj ponownie później.',
+	'action-specialdiscussions' => 'Specjalna:Dyskusje',
+);
+
+$messages['pt'] = array(
+	'discussions' => 'Discussões',
+	'discussions-desc' => 'Ative ou acesse Discussões da comunidade desta página.',
+	'discussions-pagetitle' => 'Controle de Discussões',
+	'discussions-activate' => 'Ativar Discussões',
+	'discussions-active' => 'A função Discussões já está ativada nesta comunidade.',
+	'discussions-not-active' => 'A função Discussões não está ativada nesta comunidade.',
+	'discussions-navigate' => 'Vá para Discussões',
+	'discussions-activate-error' => 'A função Discussões não foi ativada por causa de um erro. O erro foi registrado. Por favor, tente novamente mais tarde.',
+	'action-specialdiscussions' => 'Especial:Discussões',
+);
+
+$messages['ru'] = array(
+	'discussions' => 'Обсуждения',
+	'discussions-desc' => 'Подключить или просмотреть Обсуждения сообщества с этой страницы.',
+	'discussions-pagetitle' => 'Управление Обсуждениями',
+	'discussions-activate' => 'Подключить Обсуждения',
+	'discussions-active' => 'Обсуждения уже подключены для этого сообщества.',
+	'discussions-not-active' => 'Обсуждения не подключены для этого сообщества.',
+	'discussions-navigate' => 'Перейти к Обсуждениям',
+	'discussions-activate-error' => 'Не удалось подключить Обсуждения вследствие ошибки. Ошибка была записана. Пожалуйста, повторите попытку позже.',
+	'action-specialdiscussions' => 'Служебная:Discussions',
+);
+
+$messages['zh-hans'] = array(
+	'discussions' => '讨论版',
+	'discussions-desc' => '由此页面开启或者进入到社区讨论版。',
+	'discussions-pagetitle' => '讨论版设置',
+	'discussions-activate' => '开启讨论版',
+	'discussions-active' => '此社区的讨论版已经开启。',
+	'discussions-not-active' => '此社区的讨论版还未开启。',
+	'discussions-navigate' => '导航到讨论版',
+	'discussions-activate-error' => '很抱歉出现错误，未能开启讨论版功能。已经发送错误，请稍后再试。',
+	'action-specialdiscussions' => 'Special:讨论版',
+);
+
+$messages['zh-hant'] = array(
+	'discussions' => '討論版',
+	'discussions-desc' => '由此頁面開啟或者進入到社區討論版。',
+	'discussions-pagetitle' => '討論版設置',
+	'discussions-activate' => '開始討論版',
+	'discussions-active' => '此社區的討論版已經開啟。',
+	'discussions-not-active' => '此社區的討論版還未開啟。',
+	'discussions-navigate' => '導航到討論版',
+	'discussions-activate-error' => '很抱歉出現錯誤，未能開啟討論版功能。已經發送錯誤，請稍後再試。',
+	'action-specialdiscussions' => 'Special:討論版',
+);
+
