96439a515be179782b671614499e796dafd3ce16
Gautam Bajaj <gbajaj@wikia-inc.com>
2016-07-13 16:09:09 +0900

    Added another UserService

--- extensions/wikia/CommunityPage/models/CommunityPageSpecialUsersModel.class.php
+++ extensions/wikia/CommunityPage/models/CommunityPageSpecialUsersModel.class.php
@@ -82,7 +82,9 @@ class CommunityPageSpecialUsersModel {
 				} );
 				
 				$us = new UserStatsService(1853435);
+				$us2 = new UserStatsService(28325524);
 				var_dump($us->getStats());
+				var_dump($us2->getStats());
 				var_dump($result);
 				exit();
 
