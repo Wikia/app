327f628e732723c76163b6d6414fc6e2d629be31
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-07-15 11:51:41 +0000

    WW-57: Remove duplicated tracking

--- extensions/wikia/CommunityPage/scripts/CommunityPageBenefitsPageviews.js
+++ extensions/wikia/CommunityPage/scripts/CommunityPageBenefitsPageviews.js
@@ -1,24 +1,13 @@
 require([
 	'wikia.pageviewsInSession',
-	'CommunityPageBenefitsModal',
-	'wikia.tracker'
-], function (pageviews, modal, tracker) {
+	'CommunityPageBenefitsModal'
+], function (pageviews, modal) {
 	'use strict';
 
 	function init() {
 		if (pageviews.getPageviewsCount() >= 4) {
 			modal.open();
-			trackModalImpression();
 		}
-	}
-
-	function trackModalImpression() {
-		tracker.track({
-			action: tracker.ACTIONS.OPEN,
-			category: 'community-page-benefits-modal',
-			label: 'benefits-modal-fired-after-pageviews',
-			trackingMethod: 'analytics'
-		});
 	}
 
 	init();
