d6b5ab6e9147ae8e8f34e093c625f15e6d3b03b2
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-07-15 15:43:16 +0200

    ADEN-2952 Playwire template

---
+++ extensions/wikia/AdEngine/js/spec/template/playwire.spec.js
@@ -0,0 +1,63 @@
+/*global describe, it, expect, modules, beforeEach, spyOn*/
+describe('ext.wikia.adEngine.template.playwire', function () {
+	'use strict';
+
+	function noop () {}
+
+	var mocks = {
+			log: noop,
+			player: {
+				inject: noop,
+				getConfigUrl: function () {
+					return '//foo.url';
+				}
+			}
+		};
+
+	function getModule() {
+		return modules['ext.wikia.adEngine.template.playwire'](
+			mocks.player,
+			mocks.log
+		);
+	}
+
+	beforeEach(function () {
+		mocks.player.inject = noop;
+	});
+
+	it('Cannot show without container', function () {
+		var template = getModule();
+
+		spyOn(mocks.player, 'inject');
+
+		template.show({});
+
+		expect(mocks.player.inject).not.toHaveBeenCalled();
+		//expect(mocks.parent.appendChild.calls.mostRecent().args[0]['data-config']).toEqual('//fake.url');
+	});
+
+	it('Show without custom config url (should get config url from player)', function () {
+		var template = getModule();
+
+		spyOn(mocks.player, 'inject');
+
+		template.show({
+			container: {}
+		});
+
+		expect(mocks.player.inject.calls.mostRecent().args[0]).toEqual('//foo.url');
+	});
+
+	it('Show with custom config url', function () {
+		var template = getModule();
+
+		spyOn(mocks.player, 'inject');
+
+		template.show({
+			configUrl: '//foo.bar',
+			container: {}
+		});
+
+		expect(mocks.player.inject.calls.mostRecent().args[0]).toEqual('//foo.bar');
+	});
+});
