b35896c693757402d8c7d119b2cdc0d7450ddd8b
Paul Oslund <poslund@wikia-inc.com>
2016-07-15 20:11:30 +0000

    RECIRC-169: Add category group for SDCC

--- extensions/wikia/Recirculation/services/FandomDataService.class.php
+++ extensions/wikia/Recirculation/services/FandomDataService.class.php
@@ -1,10 +1,11 @@
 <?php
 
 class FandomDataService {
-	const API_BASE = 'http://fandom.wikia.com/wp-json/wp/v2/hero_unit';
+	const API_BASE = 'http://fandom.wikia.com/wp-json/wp/v2/';
 
 	const MCACHE_VER = '1.0';
 	const MCACHE_TIME = 900; // 15 minutes
+	const MCACHE_TIME_LONG = 86400; // 24 hours
 
 	const FANDOM_PER_PAGE = 10; // We ask for more than we need in case we need to filter any out
 	const LIMIT = 5;
@@ -14,7 +15,6 @@ class FandomDataService {
 
 	public function __construct( $cityId ) {
 		$this->cityId = $cityId;
-		// These options are specific to content for E3
 		$this->options = [
 			'_embed' => 1,
 			'per_page' => self::FANDOM_PER_PAGE,
@@ -28,13 +28,18 @@ class FandomDataService {
 	 *
 	 * @return an array of posts
 	 */
-	public function getPosts() {
+	public function getPosts( $type, $ignoreTopic = false ) {
 		$wikiFactoryHub = WikiFactoryHub::getInstance();
 		$vertical = $wikiFactoryHub->getWikiVertical( $this->cityId )['short'];
 
-		$memcKey = wfSharedMemcKey( __METHOD__, $vertical, self::MCACHE_VER );
+		if ( $type === 'category' ) {
+			$category = $this->setupCategories( $ignoreTopic );
+		} else {
+			$category = $this->setupVerticalCategory( $vertical );	
+		}
 
-		$this->setupVerticalCategory( $vertical );
+		$memcKey = wfSharedMemcKey( __METHOD__, $type, $category, self::MCACHE_VER );
+
 		$data = WikiaDataAccess::cache(
 			$memcKey,
 			self::MCACHE_TIME,
@@ -44,6 +49,66 @@ class FandomDataService {
 		);
 
 		return $data;
+	}
+
+	private function setupCategories( $ignoreTopic ) {
+		// Hard coding in the SDCC category for now, eventually we will want to pass this in from JS
+		$returnCategory = 4435;
+		$this->options['categories'] = $returnCategory;
+
+		$topicId = $this->getTopicCategoryId();
+
+		if ( !empty( $topicId ) && empty( $ignoreTopic ) ) {
+			$this->options['filter']['cat'] = $topicId;
+			$returnCategory = $topicId;
+		}
+
+		return $returnCategory;
+	}
+
+	/**
+	 * Get the category id from Fandom API
+	 *
+	 * @return integer
+	 */
+	public function getTopicCategoryId() {
+		$tag = WikiFactory::getVarValueByName( 'wgRecirculationParselyCommunityTag', $this->cityId );
+
+		if ( empty( $tag ) ) {
+			return 0;
+		}
+
+		$memcKey = wfSharedMemcKey( __METHOD__, $tag, self::MCACHE_VER );
+
+		$data = WikiaDataAccess::cache(
+			$memcKey,
+			self::MCACHE_TIME_LONG,
+			function() use ( $tag ) {
+				return $this->categoryApiRequest( $tag );
+			}
+		);
+
+		return $data;
+	}
+
+	private function categoryApiRequest( $tag ) {
+		$options = [
+			'search' => $tag,
+			'orderby' => 'count',
+			'order' => 'desc',
+			'per_page' => 1,
+		];
+
+		$url = $url = self::API_BASE . 'categories?' . http_build_query( $options );
+
+		$data = ExternalHttp::get( $url );
+		$data = json_decode( $data, true );
+
+		if ( is_array( $data ) && count( $data ) > 0 ) {
+			return $data[0]['id'];
+		} else {
+			return 0;
+		}
 	}
 
 	/**
@@ -72,7 +137,7 @@ class FandomDataService {
 	 * @return string
 	 */
 	private function buildUrl( $options ) {
-		$url = self::API_BASE . '?' . http_build_query( $options );
+		$url = self::API_BASE . 'hero_unit?' . http_build_query( $options );
 
 		return $url;
 	}
@@ -106,6 +171,8 @@ class FandomDataService {
 		if ( array_key_exists( $vertical, $verticalMap ) ) {
 			$this->options['categories'] = $verticalMap[$vertical];
 		}
+
+		return $verticalMap[$vertical];
 	}
 
 	private function postHasImage( $post ) {
