1d75fc0c6d51f94797b876209abbb1f1e6d5f0a4
TK-999 <tk.999.wikia@gmail.com>
2016-07-18 13:38:47 +0200

    SUS-744: Use Nirvana forwarding in former Ad modules

--- skins/oasis/modules/AdController.class.php
+++ skins/oasis/modules/AdController.class.php
@@ -3,11 +3,7 @@
 class AdController extends WikiaController {
 
 	public function index() {
-		$this->slotName = $this->request->getVal('slotName');
-		$this->pageTypes = $this->request->getVal('pageTypes');
-		$this->includeLabel = $this->request->getVal('includeLabel');
-		$this->onLoad = $this->request->getVal('onLoad');
-		$this->addToAdQueue = $this->request->getVal('addToAdQueue');
+		$this->forward( 'AdEngine2', 'Ad' );
 	}
 
 	public function executeConfig() {
--- skins/oasis/modules/templates/AdEmptyContainer_Index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo F::app()->renderView(
-	'AdEngine2',
-	'AdEmptyContainer',
-	[ 'slotName' => $slotName, 'pageTypes' => $pageTypes ]
-);
