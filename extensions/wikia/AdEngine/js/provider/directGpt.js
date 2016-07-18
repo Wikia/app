54bcfff53f3cbf3cc6d7441ca2f856f3861ca3a1
Bogna Knychala <bogna.ka@gmail.com>
2016-07-18 15:36:51 +0200

    ADEN-3571 | add openX slot before HOP in GPT direct

--- extensions/wikia/AdEngine/js/provider/directGpt.js
+++ extensions/wikia/AdEngine/js/provider/directGpt.js
@@ -59,6 +59,9 @@ define('ext.wikia.adEngine.provider.directGpt', [
 					slotTweaker.adjustLeaderboardSize(slotName);
 				}
 			},
+			beforeHop: function(slotName) {
+				openXHelper.addOpenXSlot(slotName);
+			},
 			sraEnabled: true,
 			recoverableSlots: [
 				'TOP_LEADERBOARD',
