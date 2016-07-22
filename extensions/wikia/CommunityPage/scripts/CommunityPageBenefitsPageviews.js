32a64785b813fabcaf1d2c1ae6f4864e40ce3c49
dianafa <diana.falkowska@gmail.com>
2016-07-22 13:31:26 +0200

    WW-84
    check cookie on front-end

--- extensions/wikia/CommunityPage/scripts/CommunityPageBenefitsPageviews.js
+++ extensions/wikia/CommunityPage/scripts/CommunityPageBenefitsPageviews.js
@@ -1,11 +1,12 @@
 require([
 	'wikia.pageviewsInSession',
-	'CommunityPageBenefitsModal'
-], function (pageviews, modal) {
+	'CommunityPageBenefitsModal',
+	'wikia.cookies'
+], function (pageviews, modal, cookies) {
 	'use strict';
 
 	function init() {
-		if (pageviews.getPageviewsCount() >= 4) {
+		if (!cookies.get('cpBenefitsModalShown') && pageviews.getPageviewsCount() >= 4) {
 			modal.open();
 		}
 	}
