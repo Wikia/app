f1b3c8a5713e0139f2e1e190abb0385df8c66d56
James Sutterfield <james@wikia-inc.com>
2016-07-25 16:41:11 -0700

    SOC-1891 added qq messages

--- extensions/wikia/Email/i18n/Discussion.i18n.php
+++ extensions/wikia/Email/i18n/Discussion.i18n.php
@@ -19,3 +19,22 @@ $messages['en'] = array(
     'emailext-discussion-100-upvote-summary-with-title' => "Your post [$1 $2] on [$3 $4] just reached 100 upvotes!",
     'emailext-discussion-100-upvote-summary' => "Your post on [$1 $2] just reached 100 upvotes!",
 );
+
+$messages['qq'] = array(
+    'emailext-discussion-reply-with-title-subject' => 'Message indicating the user has received a new reply to their discussions post. $1 will be the URL of the post, $2 will be the title of the post, $3 will be the URL of the wiki, and $4 will be the name of the wiki',
+    'emailext-discussion-reply-subject' => "Message indicating the user has received a new reply to their discussions post. $1 will be the URL of the wiki, and $2 will be the name of the wiki. Difference between this one and emailext-discussion-reply-with-title-subject is this one doesn't include the post title",
+    'emailext-discussion-button-text' => 'Full Discussion, which is a CTA on a button which leads to the post with all replies (the entire discussion)',
+    'emailext-discussion-all-discussions' => 'Message at bottom of email which links main discussions page on the wiki. $1 is the URL for discussions, and $2 is the name of the wiki',
+    'emailext-discussion-5-upvote-subject-with-title' => "Message indicating a user's post has reached 5 upvotes. $1 will be title of the post and $2 will be the name of the wiki",
+    'emailext-discussion-5-upvote-subject' => "Same as emailext-discussion-5-upvote-subject-with-title but without the post title. $1 will be the name of the wiki",
+    'emailext-discussion-5-upvote-summary-with-title' => 'Same as emailext-discussion-5-upvote-subject-with-title, but with links added. $1 will be URL of the post, $2 will be the title of the post, $3 will be the URL of the wiki, and $4 will be the name of the wiki',
+    'emailext-discussion-5-upvote-summary' => 'Same as emailext-discussion-5-upvote-subject, but with links added. $1 will be URL of the wiki and $2 will be the name of the wiki',
+    'emailext-discussion-25-upvote-subject-with-title' => "Message indicating a user's post has reached 25 upvotes. Includes idiom 'You're on a roll!' which means they're doing very well. $1 will be title of the post and $2 will be name of the wiki",
+    'emailext-discussion-25-upvote-subject' => "Same as emailext-discussion-25-upvote-subject-with-title but without title of post. $1 will be name of the wiki",
+    'emailext-discussion-25-upvote-summary-with-title' => "Same as emailext-discussion-25-upvote-subject-with-title but with links added. $1 will be URL of post, $2 will be title of post, $3 will be URL of wiki, and $4 will be name of the wiki",
+    'emailext-discussion-25-upvote-summary' =>  "Same as emailext-discussion-25-upvote-summary-with-title, but without title name or title url. $1 will be URL of the wiki and $2 will be name of the wiki",
+    'emailext-discussion-100-upvote-subject-with-title' => "Message indicating the user's post has reached 100 upvotes. $1 will be name of the post and $2 will be name of the wiki",
+    'emailext-discussion-100-upvote-subject' => "Same as emailext-discussion-100-upvote-subject-with-title but without the name of the title. $1 is the name of the wiki",
+    'emailext-discussion-100-upvote-summary-with-title' => "Same as emailext-discussion-100-upvote-subject-with-title but with links. $1 is URL of the post, $2 is title of the post, $3 is URL of the wiki, and $4 is name of the wiki",
+    'emailext-discussion-100-upvote-summary' => "Same as emailext-discussion-100-upvote-subject but with links. $1 is URL of the title and $2 is the name of the wiki",
+);
