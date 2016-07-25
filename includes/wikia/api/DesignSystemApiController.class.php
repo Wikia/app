fce9a416a882dfd00b32e2fd5350f717ba972ec2
Damian Jozwiak <damian@wikia-inc.com>
2016-07-25 14:18:16 +0200

    XW-1602 | DS: GlobalFooter - changes for international Global Footer

--- includes/wikia/api/DesignSystemApiController.class.php
+++ includes/wikia/api/DesignSystemApiController.class.php
@@ -3,12 +3,14 @@
 class DesignSystemApiController extends WikiaApiController {
 	public function getFooter() {
 		$wikiId = $this->request->getInt( 'wikiId' );
+		// TODO default
+		$lang = $this->request->getVal( 'lang', 'en' );
 
 		if ( WikiFactory::IDtoDB( $wikiId ) === false ) {
 			throw new NotFoundApiException( "Unable to find wiki with ID {$wikiId}" );
 		}
 
-		$footerModel = new DesignSystemGlobalFooterModel( $wikiId );
+		$footerModel = new DesignSystemGlobalFooterModel( $wikiId, $lang );
 
 		$this->setResponseData( $footerModel->getData() );
 	}
