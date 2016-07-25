4978a258e6060f8452284662646de480b5d2b355
James Sutterfield <james@wikia-inc.com>
2016-07-25 13:58:34 -0700

    SOC-1891 rename file, add crowdin config

--- extensions/wikia/Email/Email.setup.php
+++ extensions/wikia/Email/Email.setup.php
@@ -104,5 +104,5 @@ $wgExtensionMessagesFiles['SpecialSendEmail'] = $dir . 'i18n/specialSendEmail.i1
 $wgExtensionMessagesFiles['ForgotPassword'] = $dir . 'i18n/ForgotPassword.i18n.php';
 $wgExtensionMessagesFiles['UserRightsChanged'] = $dir . 'i18n/UserRightsChanged.i18n.php';
 $wgExtensionMessagesFiles['EmailUserNameChange'] = $dir . 'i18n/UserNameChange.i18n.php';
-$wgExtensionMessagesFiles['EmailDiscussions'] = $dir . 'i18n/Discussions.i18n.php';
+$wgExtensionMessagesFiles['EmailDiscussions'] = $dir . 'i18n/Discussion.i18n.php';
 
