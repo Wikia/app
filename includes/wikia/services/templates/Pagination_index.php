0802f8e6535996c7dcb1f8529a7f64bfb4019533
macbre <maciej.brencz@gmail.com>
2016-07-26 12:37:45 +0200

    Use Sanitizer::encodeAttribute

--- includes/wikia/services/templates/Pagination_index.php
+++ includes/wikia/services/templates/Pagination_index.php
@@ -1,7 +1,7 @@
-<ul class="Pagination"  <? foreach($data as $key => $val ): ?> data-<?= htmlspecialchars( $key ) ?>="<?php echo htmlentities($val); ?>" <? endforeach; ?> >
+<ul class="Pagination"  <? foreach($data as $key => $val ): ?> data-<?= htmlspecialchars( $key ) ?>="<?= Sanitizer::encodeAttribute($val); ?>" <? endforeach; ?> >
 	<? if( $prev ): ?>
 		<li data-page="<?= $currentPage - 1; ?>"  class="prev">
-			<a href="<?= htmlspecialchars( $url !== '#' ? $url.($currentPage - 1) : $url) ?>"><?= htmlspecialchars( $prevMsg ) ?></a>
+			<a href="<?= Sanitizer::encodeAttribute( $url !== '#' ? $url.($currentPage - 1) : $url) ?>"><?= htmlspecialchars( $prevMsg ) ?></a>
 		</li>
 	<? endif; ?>
 	
@@ -13,14 +13,14 @@
 			<li class="ellipsis"><?= htmlspecialchars( $pages[$i] ) ?></li>
 		<? else: ?>
 			<li data-page="<?= $pages[$i]; ?>" class="<?= $class ?>">
-				<a href="<?= htmlspecialchars( $url !== '#' ? $url.$pages[$i] : $url ) ?>"><?= $pages[$i] ?></a>
+				<a href="<?= Sanitizer::encodeAttribute( $url !== '#' ? $url.$pages[$i] : $url ) ?>"><?= $pages[$i] ?></a>
 			</li>
 		<? endif; ?>
 	<? endfor; ?>
 	
 	<? if( $next ): ?>
 		<li data-page="<?= $currentPage + 1; ?>" class="next">
-			<a href="<?= htmlspecialchars( $url !== '#' ? $url.($currentPage + 1) : $url ) ?>"><?= htmlspecialchars( $nextMsg ) ?></a>
+			<a href="<?= Sanitizer::encodeAttribute( $url !== '#' ? $url.($currentPage + 1) : $url ) ?>"><?= htmlspecialchars( $nextMsg ) ?></a>
 		</li>
 	<? endif; ?>
 </ul>
