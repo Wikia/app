b8cd6e9f7ba012838a5d2a04e7a38f14c40eec90
TK-999 <tk.999.wikia@gmail.com>
2016-07-28 15:21:02 +0200

    SUS-767: Formatted and documented UserCommand.php

--- includes/wikia/services/usercommands/UserCommand.php
+++ includes/wikia/services/usercommands/UserCommand.php
@@ -1,214 +1,356 @@
 <?php
 
-	abstract class UserCommand {
+/**
+ * Base class for the various commands and tools presented in the bottom toolbar of the Oasis skin
+ */
+abstract class UserCommand {
 
-		protected $id = null;
-		protected $type = null;
-		protected $name = null;
-		protected $data = null;
+	/**
+	 * @var string|null $id Unique id of this tool (tool type and tool name separated by a : )
+	 */
+	protected $id = null;
+	/**
+	 * @var string|null $type
+	 * Type of this tool (one of: follow, menu, link, html, customize, devinfo or disabled)
+	 */
+	protected $type = null;
+	/**
+	 * @var string|null $name Name of this tool
+	 */
+	protected $name = null;
+	/**
+	 * @var array|null $data Data presupplied to this object (caption string etc.)
+	 */
+	protected $data = null;
 
-		public function __construct( $id, $data = array() ) {
-			$this->id = $id;
-			list( $this->type, $this->name ) = explode(':',$this->id,2);
-			$this->data = $data;
-		}
+	/**
+	 * UserCommand constructor - determines type and name from id and sets any presupplied data
+	 * @param string $id Unique id of this tool (tool type and tool name separated by a : )
+	 * @param array $data Data presupplied to this object (caption string etc.)
+	 */
+	public function __construct( $id, $data = [] ) {
+		$this->id = $id;
+		list( $this->type, $this->name ) = explode( ':', $this->id, 2 );
+		$this->data = $data;
+	}
 
-		public function getId() {
-			return $this->id;
-		}
+	/**
+	 * Gets the unique id of this tool
+	 * @return null|string unique id of this tool
+	 */
+	public function getId() {
+		return $this->id;
+	}
 
-		public function getInfo() {
-			$defaultCaption = $this->getAbstractCaption();
-			$caption = !empty($this->data['caption']) ? $this->data['caption'] : $defaultCaption;
-			return array(
-				'id' => $this->getId(),
-				'defaultCaption' => $defaultCaption,
-				'caption' => $caption,
-			);
-		}
+	/**
+	 * Gets information about this tool
+	 * @return array array containing the unique id, default caption and actual caption of this tool
+	 */
+	public function getInfo() {
+		$defaultCaption = $this->getAbstractCaption();
+		$caption = !empty( $this->data['caption'] ) ? $this->data['caption'] : $defaultCaption;
+		return [
+			'id'             => $this->getId(),
+			'defaultCaption' => $defaultCaption,
+			'caption'        => $caption,
+		];
+	}
 
-		public function isAvailable() {
-			$this->needData();
-			return $this->available;
-		}
+	/**
+	 * Returns whether the current user is allowed to add this tool to their toolbar
+	 * @return bool Whether the current user is allowed to add this tool to their toolbar
+	 */
+	public function isAvailable() {
+		$this->needData();
+		return $this->available;
+	}
 
-		public function isEnabled() {
-			$this->needData();
-			return $this->enabled;
-		}
+	/**
+	 * Returns whether the current user is allowed to use this tool on this wiki or page
+	 * @return bool Whether the current user is allowed to use this tool on this wiki or page
+	 */
+	public function isEnabled() {
+		$this->needData();
+		return $this->enabled;
+	}
 
-		public function getCaption() {
-			return $this->caption;
-		}
+	/**
+	 * Returns the caption (link text used in <a> element) of this tool
+	 * @return string|null caption (link text) of this tool
+	 */
+	public function getCaption() {
+		return $this->caption;
+	}
 
-		public function getHref() {
-			return $this->href;
-		}
+	/**
+	 * Returns the URL this tool points to
+	 * @return string URL this tool points to
+	 */
+	public function getHref() {
+		return $this->href;
+	}
 
-		protected $overflow = true;
-		protected $defaultRenderType = 'link';
+	protected $overflow = true;
+	/**
+	 * @var string $defaultRenderType Default type to be used by UserToolsController Menu template, if none were specified
+	 */
+	protected $defaultRenderType = 'link';
 
-		protected $available = false;
-		protected $enabled = false;
+	/**
+	 * @var bool $available Whether the current user is allowed to add this tool to their toolbar
+	 */
+	protected $available = false;
+	/**
+	 * @var bool $enabled Whether the current user is allowed to use this tool on this wiki or page
+	 */
+	protected $enabled = false;
 
-		protected $imageSprite = false;
-		protected $imageUrl = false;
+	/**
+	 * @var bool|string $imageSprite Image sprite used by the icon of this tool, if any
+	 */
+	protected $imageSprite = false;
+	/**
+	 * @var bool|string $imageUrl Image URL used by the icon of this tool, if any
+	 */
+	protected $imageUrl = false;
 
 
-		protected $listItemId = '';
-		protected $listItemClass = '';
-		protected $linkId = '';
-		protected $linkClass = '';
-		protected $accessKey = false;
+	/**
+	 * @var string $listItemId HTML id attribute used for rendering this tool (for menu-type)
+	 */
+	protected $listItemId = '';
+	/**
+	 * @var string $listItemClass HTML class attribute used for rendering this tool (for menu-type)
+	 */
+	protected $listItemClass = '';
+	/**
+	 * @var string $linkId HTML id attribute used for the <a> element of this tool
+	 */
+	protected $linkId = '';
+	/**
+	 * @var string $linkClass HTML class attribute used for the <a> element of this tool
+	 */
+	protected $linkClass = '';
+	/**
+	 * @var bool|string Accesskey of the <a> element of this tool, if any
+	 */
+	protected $accessKey = false;
 
-		protected $href = '#';
-		protected $caption = null;
-		protected $description = null;
+	/**
+	 * @var string $href URL of this tool
+	 */
+	protected $href = '#';
+	/**
+	 * @var null|string $caption Link text of this tool
+	 */
+	protected $caption = null;
+	/**
+	 * @var null|string $description Textual description of this tool
+	 */
+	protected $description = null;
 
-		protected $abstractCaption = null;
-		protected $abstractDescription = null;
+	/**
+	 * @var null|string $abstractCaption
+	 */
+	protected $abstractCaption = null;
+	/**
+	 * @var null|string $abstractDescription
+	 */
+	protected $abstractDescription = null;
 
-		protected function getAbstractCaption() {
-			$this->needData();
-			return $this->abstractCaption;
-		}
+	protected function getAbstractCaption() {
+		$this->needData();
+		return $this->abstractCaption;
+	}
 
-		protected function getAbstractDescription() {
-			$this->needData();
-			return $this->description;
-		}
+	protected function getAbstractDescription() {
+		$this->needData();
+		return $this->description;
+	}
 
-		protected function getListItemAttributes() {
-			$attributes = array();
-			if ($this->listItemId) $attributes['id'] = $this->listItemId;
-			$listItemClass = trim($this->listItemClass . ( $this->overflow ? ' overflow' : '' ));
-			if ($listItemClass) $attributes['class'] = $listItemClass;
-			return $attributes;
-		}
+	/**
+	 * Returns the attributes used for the <li> element when outputting this tool (for menu-type)
+	 * @return array key-value array of attributes and their values
+	 */
+	protected function getListItemAttributes() {
+		$attributes = [];
+		if ( $this->listItemId ) $attributes['id'] = $this->listItemId;
+		$listItemClass = trim( $this->listItemClass . ( $this->overflow ? ' overflow' : '' ) );
+		if ( $listItemClass ) $attributes['class'] = $listItemClass;
+		return $attributes;
+	}
 
-		protected function getLinkAttributes() {
-			$attributes = array();
-			$attributes['data-tool-id'] = $this->id;
-			$attributes['data-name'] = $this->getTrackerName();
-			if ($this->href) $attributes['href'] = $this->href;
-			if ($this->linkId) $attributes['id'] = $this->linkId;
-			if ($this->linkClass) $attributes['class'] = $this->linkClass;
-			if ($this->accessKey) $attributes['accesskey'] = $this->accessKey;
-			return $attributes;
-		}
+	/**
+	 * Returns the attributes used for the <a> element when outputting this tool
+	 * @return array key-value array of attributes and their values
+	 */
+	protected function getLinkAttributes() {
+		$attributes = [];
+		$attributes['data-tool-id'] = $this->id;
+		$attributes['data-name'] = $this->getTrackerName();
+		if ( $this->href ) $attributes['href'] = $this->href;
+		if ( $this->linkId ) $attributes['id'] = $this->linkId;
+		if ( $this->linkClass ) $attributes['class'] = $this->linkClass;
+		if ( $this->accessKey ) $attributes['accesskey'] = $this->accessKey;
+		return $attributes;
+	}
 
-		protected function getTrackerName() {
-			return strtolower($this->name);
-		}
+	/**
+	 * Returns the unique name used by tracking functions to identify this tool
+	 * @return string unique name used by tracking functions to identify this tool
+	 */
+	protected function getTrackerName() {
+		return strtolower( $this->name );
+	}
 
-		public function render() {
-			$this->needData();
+	/**
+	 * Returns the formatted HTML (list item+link) for this tool
+	 * @return string HTML list item for this tool
+	 */
+	public function render() {
+		$this->needData();
 
-			if (!$this->available) {
-				return '';
-			}
-
-			$html = '';
-			$html .= Xml::openElement('li',$this->getListItemAttributes());
-
-			$html .= $this->renderIcon();
-
-			if ($this->enabled) {
-				$html .= Xml::element('a',$this->getLinkAttributes(),$this->caption);
-				$html .= $this->renderSubmenu();
-			} else {
-				$spanAttributes = array(
-					'title' => $this->getDisabledMessage(),
-				);
-				$html .= Xml::element('span',$spanAttributes,$this->caption);
-			}
-
-			$html .= Xml::closeElement('li');
-			return $html;
-		}
-
-		public function getRenderData() {
-			$this->needData();
-			if (!$this->available)
-				return false;
-
-			$data = array(
-				'type' => $this->defaultRenderType,
-				'caption' => $this->caption,
-				'tracker-name' => $this->getTrackerName(),
-			);
-			if ($this->enabled) {
-				$data['href'] = $this->href;
-			} else {
-				$data['type'] = 'disabled';
-				$data['error-message'] = $this->getDisabledMessage();
-			}
-
-			return $data;
-		}
-
-		protected function renderIcon() {
+		if ( !$this->available ) {
 			return '';
 		}
 
-		public function renderSubmenu() {
-			return '';
+		$html = '';
+		$html .= Xml::openElement( 'li', $this->getListItemAttributes() );
+
+		$html .= $this->renderIcon();
+
+		if ( $this->enabled ) {
+			$html .= Xml::element( 'a', $this->getLinkAttributes(), $this->caption );
+			$html .= $this->renderSubmenu();
+		} else {
+			$spanAttributes = [
+				'title' => $this->getDisabledMessage(),
+			];
+			$html .= Xml::element( 'span', $spanAttributes, $this->caption );
 		}
 
-		protected function getDisabledMessage() {
-			return wfMsg('oasis-toolbar-for-admins-only');
+		$html .= Xml::closeElement( 'li' );
+		return $html;
+	}
+
+	/**
+	 * Returns information for UserToolsController Menu template to output this tool
+	 * @return array|bool Array of information used by UserToolsController Menu template to output this tool, or false if disabled
+	 */
+	public function getRenderData() {
+		$this->needData();
+		if ( !$this->available )
+			return false;
+
+		$data = [
+			'type'         => $this->defaultRenderType,
+			'caption'      => $this->caption,
+			'tracker-name' => $this->getTrackerName(),
+		];
+		if ( $this->enabled ) {
+			$data['href'] = $this->href;
+		} else {
+			$data['type'] = 'disabled';
+			$data['error-message'] = $this->getDisabledMessage();
 		}
 
+		return $data;
+	}
 
-		protected $dataBuilt = false;
+	/**
+	 * Returns the HTML for the icon of this tool (empty by default, can be overridden by subclasses)
+	 * @return string Icon HTML (default: empty)
+	 */
+	protected function renderIcon() {
+		return '';
+	}
 
-		protected function needData() {
-			if (!$this->dataBuilt) {
-				$this->buildData();
-				$this->abstractCaption = $this->caption;
-				if (!empty($this->data['caption']))
-					$this->caption = $this->data['caption'];
-				$this->dataBuilt = true;
-			}
+	/**
+	 * Returns the HTML for the list if this is a menu-type tool (default: empty)
+	 * @return string Submenu HTML (default: empty)
+	 */
+	public function renderSubmenu() {
+		return '';
+	}
+
+	/**
+	 * Gets the error message text shown as a tooltip if this tool is disabled
+	 * @return String Error message text - not safe for HTML output, needs to be escaped
+	 */
+	protected function getDisabledMessage() {
+		return wfMessage( 'oasis-toolbar-for-admins-only' )->text();
+	}
+
+
+	/**
+	 * @var bool $dataBuilt Whether the data required for rendering this tool has been loaded
+	 */
+	protected $dataBuilt = false;
+
+	/**
+	 * Populates this object with information required for rendering this tool
+	 */
+	protected function needData() {
+		if ( !$this->dataBuilt ) {
+			$this->buildData();
+			$this->abstractCaption = $this->caption;
+			if ( !empty( $this->data['caption'] ) )
+				$this->caption = $this->data['caption'];
+			$this->dataBuilt = true;
 		}
+	}
 
-		abstract protected function buildData();
+	/**
+	 * Actually loads the information required for rendering this tool
+	 */
+	abstract protected function buildData();
 
-		static protected $skinData = null;
+	/**
+	 * @var array|null $skinData Data related to the current page as provided by the skin (content actions, navigation URLs)
+	 */
+	static protected $skinData = null;
 
-		static public function setSkinData( $skinData ) {
-			self::$skinData = $skinData;
-		}
+	/**
+	 * Set the skin data to be used for rendering this tool (content actions and navigation URLs)
+	 * @param array $skinData array with keys content_actions and nav_urls
+	 */
+	static public function setSkinData( $skinData ) {
+		self::$skinData = $skinData;
+	}
 
-		static public function needSkinData() {
-			if (is_null(self::$skinData)) {
-				$skinTemplateObj = F::app()->getSkinTemplateObj();
-				if ( $skinTemplateObj ) {
-					self::$skinData = array(
-						'content_actions' => $skinTemplateObj->get('content_actions'),
-						'nav_urls' => $skinTemplateObj->get('nav_urls'),
-					);
-				} else {
-					/** @var $context RequestContext */
-					$context = RequestContext::getMain();
-					/** @var $skin WikiaSkin */
-					$skin = $context->getSkin();
+	/**
+	 * Loads the content actions and navigation URLs as set by the current skin
+	 */
+	static public function needSkinData() {
+		if ( is_null( self::$skinData ) ) {
+			/** @var WikiaSkinTemplate $skinTemplateObj */
+			$skinTemplateObj = F::app()->getSkinTemplateObj();
+			if ( $skinTemplateObj ) {
+				self::$skinData = [
+					'content_actions' => $skinTemplateObj->get( 'content_actions' ),
+					'nav_urls'        => $skinTemplateObj->get( 'nav_urls' ),
+				];
+			} else {
+				/** @var $context RequestContext */
+				$context = RequestContext::getMain();
+				/** @var $skin WikiaSkin */
+				$skin = $context->getSkin();
 
-					if (!isset($skin->iscontent)) {
-						$title = $skin->getTitle();
+				if ( !isset( $skin->iscontent ) ) {
+					$title = $skin->getTitle();
 
-						if ( in_array( $title->getNamespace(), array( NS_SPECIAL, NS_FILE ) ) ) {
-							$skin->getOutput()->setArticleRelated(false);
-						}
-						$skin->thispage = $title->getPrefixedDBkey();
-						$skin->loggedin = $context->getUser()->isLoggedIn();
+					if ( in_array( $title->getNamespace(), [ NS_SPECIAL, NS_FILE ] ) ) {
+						$skin->getOutput()->setArticleRelated( false );
 					}
-
-					self::$skinData = array(
-						'content_actions' => $skin->buildContentActionUrls( $skin->buildContentNavigationUrls() ),
-						'nav_urls' => $skin->buildNavUrls(),
-					);
+					$skin->thispage = $title->getPrefixedDBkey();
+					$skin->loggedin = $context->getUser()->isLoggedIn();
 				}
+
+				self::$skinData = [
+					'content_actions' => $skin->buildContentActionUrls( $skin->buildContentNavigationUrls() ),
+					'nav_urls'        => $skin->buildNavUrls(),
+				];
 			}
 		}
 	}
+}
