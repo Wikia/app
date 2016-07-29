58812a6126aa3b2556a126f1fe09ff890aa03444
Paul Oslund <poslund@wikia-inc.com>
2016-07-29 16:57:26 +0000

    RECIRC-179: Move waiting for the rail into rail view

--- extensions/wikia/Recirculation/js/experiments/placement/fandomTopic.js
+++ extensions/wikia/Recirculation/js/experiments/placement/fandomTopic.js
@@ -2,11 +2,10 @@
 define('ext.wikia.recirculation.experiments.placement.FANDOM_TOPIC', [
 	'jquery',
 	'wikia.window',
-	'ext.wikia.recirculation.utils',
 	'ext.wikia.recirculation.helpers.fandom',
 	'ext.wikia.recirculation.helpers.curatedContent',
 	'ext.wikia.recirculation.views.rail'
-], function ($, w, utils, FandomHelper, CuratedHelper, RailView) {
+], function ($, w, FandomHelper, CuratedHelper, RailView) {
 
 	function run(experimentName) {
 		var view = RailView(),
@@ -17,7 +16,6 @@ define('ext.wikia.recirculation.experiments.placement.FANDOM_TOPIC', [
 			limit: 5
 		}).loadData()
 			.then(curated.injectContent)
-			.then(utils.waitForRail)
 			.then(view.render)
 			.then(view.setupTracking(experimentName))
 			.then(curated.setupTracking)
