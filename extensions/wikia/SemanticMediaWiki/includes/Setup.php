8734860ccfa0cf97e908f38a05ce4845ad6fdb6d
Sqreek <Mateusz.Holowiecki@wikia-inc.com>
2016-08-02 10:46:45 +0200

    In Smw QueryEngineDatabaseConnectionProvider add wikia change doc.

--- extensions/wikia/SemanticMediaWiki/includes/Setup.php
+++ extensions/wikia/SemanticMediaWiki/includes/Setup.php
@@ -117,7 +117,9 @@ final class Setup {
 		);
 
 		// Connection can be used to redirect queries to another DB cluster
+		// Wikia Change - Start
 		$queryengineConnectionProvider = $mwCollaboratorFactory->newQueryEngineDatabaseConnectionProvider();
+		// Wikia Change - End
 
 		$connectionManager->registerConnectionProvider(
 			'mw.db.queryengine',
