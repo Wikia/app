3535ec005005e101fc46fb6f737335f928725b8b
Dmytro Rets <dmitryrets@gmail.com>
2016-08-03 11:40:14 +0200

    ADEN-3686 Disable detection if recovery is enabled

--- extensions/wikia/AdEngine/js/spec/AdContext.spec.js
+++ extensions/wikia/AdEngine/js/spec/AdContext.spec.js
@@ -53,11 +53,6 @@ describe('AdContext', function () {
 			wikiaCookies: {
 				get: noop
 			},
-			sampler: {
-				sample: function () {
-					return false;
-				}
-			},
 			callback: noop
 		},
 		queryParams = [
@@ -73,7 +68,6 @@ describe('AdContext', function () {
 			mocks.doc,
 			mocks.geo,
 			mocks.instantGlobals,
-			mocks.sampler,
 			mocks.win,
 			mocks.Querystring
 		);
@@ -455,56 +449,25 @@ describe('AdContext', function () {
 		expect(getModule().getContext().opts.sourcePointDetection).toBeFalsy();
 	});
 
-	it('enables detection when url param pagefairdetection is set', function () {
-		spyOn(mocks.querystring, 'getVal').and.callFake(function (param) {
-			return param === 'pagefairdetection' ?  '1' : '0';
-		});
+	it('disables Source Point detection when Source Point recovery is enabled', function () {
+		mocks.instantGlobals = {
+			wgAdDriverSourcePointDetectionCountries: [
+				'CURRENT_COUNTRY',
+				'ZZ'
+			]
+		};
+		mocks.win = {
+			ads: {
+				context: {
+					opts: {
+						sourcePointDetectionUrl: '//blah.blah',
+						sourcePointRecovery: true
+					}
+				}
+			}
+		};
 
-		expect(getModule().getContext().opts.pageFairDetection).toBeTruthy();
-	});
-
-	it('disable detection when noExtenals is set and pagefairdetection is set', function () {
-		spyOn(mocks.querystring, 'getVal').and.callFake(function (param) {
-			var result = ['noexternals', 'pagefairdetection'].indexOf(param) !== -1;
-			return result ? '1' : '0';
-		});
-
-		expect(getModule().getContext().opts.pageFairDetection).toBeFalsy();
-	});
-
-	it('disable PageFair detection for current country on whitelist and not allowed by sampler', function () {
-		spyOn(mocks.sampler, 'sample').and.callFake(function () {
-			return false;
-		});
-
-		mocks.instantGlobals = {wgAdDriverPageFairDetectionCountries: ['CURRENT_COUNTRY', 'ZZ']};
-		expect(getModule().getContext().opts.pageFairDetection).toBeFalsy();
-	});
-
-	it('enable PageFair detection for current country on whitelist and allowed by sampler', function () {
-		spyOn(mocks.sampler, 'sample').and.callFake(function () {
-			return true;
-		});
-		mocks.instantGlobals = {wgAdDriverPageFairDetectionCountries: ['CURRENT_COUNTRY', 'ZZ']};
-		expect(getModule().getContext().opts.pageFairDetection).toBeTruthy();
-	});
-
-	it('disable PageFair detection when current country is not on whitelist and allowed by sampler', function () {
-		spyOn(mocks.sampler, 'sample').and.callFake(function () {
-			return true;
-		});
-		mocks.instantGlobals = {wgAdDriverPageFairDetectionCountries: ['OTHER_COUNTRY', 'ZZ']};
-		expect(getModule().getContext().opts.pageFairDetection).toBeFalsy();
-	});
-
-	it('enables PageFair detection when url param pagefairdetection is set and current country is on whitelist', function () {
-		mocks.instantGlobals = {wgAdDriverPageFairDetectionCountries: ['CURRENT_COUNTRY', 'ZZ']};
-		spyOn(mocks.querystring, 'getVal').and.callFake(function (param) {
-			var result = ['pagefairdetection'].indexOf(param) !== -1;
-			return result ? '1' : '0';
-		});
-
-		expect(getModule().getContext().opts.pageFairDetection).toBeTruthy();
+		expect(getModule().getContext().opts.sourcePointDetection).toBeFalsy();
 	});
 
 	it('enables detection when url param sourcepointdetection is set', function () {
