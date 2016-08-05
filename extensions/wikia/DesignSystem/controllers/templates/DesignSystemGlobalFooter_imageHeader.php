535fc432af4dc972c692d0c7d9bc55e4718a8fb5
Igor Rogatty <igor@wikia-inc.com>
2016-08-05 17:07:31 +0200

    XW-1729 | remove alt attributes as there are no translations for them

--- extensions/wikia/DesignSystem/controllers/templates/DesignSystemGlobalFooter_imageHeader.php
+++ extensions/wikia/DesignSystem/controllers/templates/DesignSystemGlobalFooter_imageHeader.php
@@ -1,9 +1,9 @@
 <h2 class="wds-global-footer__<?= Sanitizer::encodeAttribute( $section ) ?>-header">
 	<?php if ( $model['type'] === 'link-image' ) : ?>
 		<a href="<?= Sanitizer::encodeAttribute( $model['href'] ) ?>">
-			<?= DesignSystemHelper::getSvg( $model['image'], 'wds-global-footer__' . Sanitizer::encodeAttribute( $section ) . '-logo', DesignSystemHelper::renderText( $model['title'] ) ) ?>
+			<?= DesignSystemHelper::getSvg( $model['image'], 'wds-global-footer__' . Sanitizer::encodeAttribute( $section ) . '-logo' ) ?>
 		</a>
 	<?php else : ?>
-		<?= DesignSystemHelper::getSvg( $model['image'], 'wds-global-footer__' . Sanitizer::encodeAttribute( $section ) . '-logo', DesignSystemHelper::renderText( $model['title'] ) ) ?>
+		<?= DesignSystemHelper::getSvg( $model['image'], 'wds-global-footer__' . Sanitizer::encodeAttribute( $section ) . '-logo' ) ?>
 	<?php endif; ?>
 </h2>
