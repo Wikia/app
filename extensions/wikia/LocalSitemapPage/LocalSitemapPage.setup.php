1d373ff203825675fb5157e3a66f4454b01e9b56
Piotr Gabryjeluk <rychu@wikia-inc.com>
2016-08-05 15:38:20 -0700

    SEO-456 Fix link in global footer

--- extensions/wikia/LocalSitemapPage/LocalSitemapPage.setup.php
+++ extensions/wikia/LocalSitemapPage/LocalSitemapPage.setup.php
@@ -2,6 +2,7 @@
 
 // autoload
 $wgAutoloadClasses['LocalSitemapPageArticle'] = __DIR__ . '/LocalSitemapPageArticle.class.php';
+$wgAutoloadClasses['LocalSitemapPageHelper'] = __DIR__ . '/LocalSitemapPageHelper.class.php';
 $wgAutoloadClasses['LocalSitemapPageHooks'] = __DIR__ . '/LocalSitemapPageHooks.class.php';
 $wgAutoloadClasses['LocalSitemapSpecialPage'] = __DIR__ . '/LocalSitemapSpecialPage.class.php';
 
