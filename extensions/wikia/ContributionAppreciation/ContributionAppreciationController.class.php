0066ec4d7d63d47ae7a637a0913c3f555339d6e4
idradm <robakadam@gmail.com>
2016-08-05 13:59:55 +0200

    WW-114 show button only for logged in users

--- extensions/wikia/ContributionAppreciation/ContributionAppreciationController.class.php
+++ extensions/wikia/ContributionAppreciation/ContributionAppreciationController.class.php
@@ -11,20 +11,32 @@ class ContributionAppreciationController extends WikiaController {
 	}
 
 	public static function onDiffHeader( DifferenceEngine $diffPage, $oldRev, Revision $newRev ) {
-		Wikia::addAssetsToOutput( 'contribution_appreciation_js' );
-		$diffPage->getOutput()->addHTML( self::getAppreciationLink( $newRev->getId() ) );
+		global $wgUser;
+
+		if ( $wgUser->isLoggedIn() ) {
+			Wikia::addAssetsToOutput( 'contribution_appreciation_js' );
+			$diffPage->getOutput()->addHTML( self::getAppreciationLink( $newRev->getId() ) );
+		}
 
 		return true;
 	}
 
 	public static function onPageHistoryLineEnding( HistoryPager $pager, $row, &$s, $classes ) {
-		$s .= self::getAppreciationLink( $row->rev_id );
+		global $wgUser;
+
+		if ( $wgUser->isLoggedIn() ) {
+			$s .= self::getAppreciationLink( $row->rev_id );
+		}
 
 		return true;
 	}
 
 	public static function onPageHistoryBeforeList() {
-		Wikia::addAssetsToOutput( 'contribution_appreciation_js' );
+		global $wgUser;
+
+		if ( $wgUser->isLoggedIn() ) {
+			Wikia::addAssetsToOutput( 'contribution_appreciation_js' );
+		}
 
 		return true;
 	}
