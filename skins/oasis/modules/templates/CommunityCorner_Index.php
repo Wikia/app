11660a7a751032d64180318ea42bc7b19bd92ebe
TK-999 <tk.999.wikia@gmail.com>
2016-08-08 12:05:08 +0200

    SUS-838: cleanup

--- skins/oasis/modules/templates/CommunityCorner_Index.php
+++ skins/oasis/modules/templates/CommunityCorner_Index.php
@@ -1,10 +1,12 @@
 <section class="CommunityCornerModule module">
 	<h2 class="dark_text_2"><?= wfMessage( 'myhome-community-corner-header' )->escaped(); ?></h2>
-	<div id="myhome-community-corner-content"><?= wfMessage( 'community-corner' )->escaped(); ?></div>
+	<div id="myhome-community-corner-content"><?= wfMessage( 'community-corner' )->parse(); ?></div>
 	
 <?php if ( $isAdmin ): ?>
 	<div id="myhome-community-corner-edit">
-		<a class="more" href="<?= Skin::makeNSUrl( 'community-corner', 'edit', NS_MEDIAWIKI ); ?>" rel="nofollow"><?= wfMessage( 'oasis-myhome-community-corner-edit' )->escaped(); ?></a>
+		<a class="more" href="<?= Skin::makeNSUrl( 'community-corner', 'edit', NS_MEDIAWIKI ); ?>" rel="nofollow">
+			<?= wfMessage( 'oasis-myhome-community-corner-edit' )->escaped(); ?>
+		</a>
 	</div>
 <?php endif; ?>
 </section>
