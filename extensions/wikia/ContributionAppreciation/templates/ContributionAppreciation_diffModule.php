2cc42d9c10f878f689a4b0a5c2a4b801b3e12c6d
idradm <robakadam@gmail.com>
2016-08-08 16:24:48 +0200

    WW-114 move sanitization to template

--- extensions/wikia/ContributionAppreciation/templates/ContributionAppreciation_diffModule.php
+++ extensions/wikia/ContributionAppreciation/templates/ContributionAppreciation_diffModule.php
@@ -2,5 +2,5 @@
 	<span class="appreciation-cta"><?= wfMessage( 'appreciation-cta-text', $username )->escaped() ?></span>
 	<span class="appreciation-feedback"><?= wfMessage( 'appreciation-feedback-text', $username )->escaped() ?></span>
 	<button class="appreciation-button"
-			data-revision="<?= $revision ?>"><?= wfMessage( 'appreciation-text' )->escaped() ?></button>
+			data-revision="<?= Sanitizer::encodeAttribute( $revision ) ?>"><?= wfMessage( 'appreciation-text' )->escaped() ?></button>
 </div>
