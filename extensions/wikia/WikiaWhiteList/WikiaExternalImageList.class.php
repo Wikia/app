7212ca04b39647f27b6ba4cbbd8db9d7b374a309
TK-999 <tk.999.wikia@gmail.com>
2016-08-10 11:33:55 +0200

    SUS-521: Further CR changes
    - use array_filter
    - use existing community wiki ID from Wikia class instead of creating new constant

--- extensions/wikia/WikiaWhiteList/WikiaExternalImageList.class.php
+++ extensions/wikia/WikiaWhiteList/WikiaExternalImageList.class.php
@@ -5,10 +5,6 @@
  * Hook handlers related to the global external image whitelist
  */
 class WikiaExternalImageList {
-	/**
-	 * @var int COMMUNITY_WIKI_ID Community Central wiki id
-	 */
-	const COMMUNITY_WIKI_ID = 177;
 
 	/**
 	 * @var string GLOBAL_WHITELIST_PAGE Title of the MediaWiki page that serves as the global external image whitelist
@@ -20,27 +16,25 @@ class WikiaExternalImageList {
 	 * Checks the given URL against the global external image whitelist file
 	 * Whitelist entries are treated the same way as MediaWiki:External image whitelist
 	 *
-	 * @param string $url
+	 * @param string $url URL to be matched against the global external image whitelist
 	 * @return bool Whether the URL matches an item in the global external image whitelist
 	 */
 	public static function onOutputMakeExternalImage( &$url ) {
-		$listPage = GlobalTitle::newFromTextCached( static::GLOBAL_WHITELIST_PAGE, NS_MEDIAWIKI, static::COMMUNITY_WIKI_ID );
+		$listPage = GlobalTitle::newFromTextCached( static::GLOBAL_WHITELIST_PAGE, NS_MEDIAWIKI, Wikia::COMMUNITY_WIKI_ID );
 		$list = explode( "\n", $listPage->getContent() ?? '' );
 
 		// Handle whitelist items in the same way as MediaWiki:External image whitelist
 		// Remove blank entries/comments
-		$i = 0;
-		foreach ( $list as $entry ) {
-			if ( strpos( $entry, '#' ) === 0 || $entry === '' ) {
-				unset( $list[$i] );
-			}
-			$i++;
-		}
+		$list = array_filter( $list, function ( $entry ) {
+			return ( strpos( $entry, '#' ) !== 0 && $entry !== '' );
+		} );
 
 		// Combine and evaluate the list as a single regular expression
 		$combinedRegex = implode( '|', $list );
+
+		// Escape slashes
 		$combinedRegex = str_replace( '/', '\\/', $combinedRegex );
 
-		return (bool) preg_match( '/(' . $combinedRegex . ')/i', $url );
+		return preg_match( '/(' . $combinedRegex . ')/i', $url ) === 1;
 	}
 }
