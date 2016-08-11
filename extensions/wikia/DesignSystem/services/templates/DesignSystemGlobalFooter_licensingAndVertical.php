1ec927483ce0c14a29e0930b958de7aac2cc12f0
Warkot <mateusz.warkocki@wikia-inc.com>
2016-08-11 17:57:52 +0200

    XW-1787 | update markup/DS version to 0.2.1 — Full site link ready

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalFooter_licensingAndVertical.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalFooter_licensingAndVertical.php
@@ -1,3 +1,3 @@
 <div class="wds-global-footer__bottom-bar">
-	<div class="wds-global-footer__bottom-bar-row"><?= DesignSystemHelper::renderText( $model['description'] ) ?></div>
+	<div class="wds-global-footer__bottom-bar-row wds-has-padding"><?= DesignSystemHelper::renderText( $model['description'] ) ?></div>
 </div>
