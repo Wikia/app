5c2cf3af73a0683ad2317967dfb42b77e70b961d
nmonterroso <nelsonmonterroso@gmail.com>
2016-08-15 15:00:53 -0700

    adding cleanup script that can be used after generating code

---
+++ lib/Swagger/cleanup.sh
@@ -0,0 +1,4 @@
+# this should be run from lib/Swagger/ AFTER generating code
+
+rm -rf test/ docs/Api/ docs/Model/ LICENSE ../LICENSE ../.swagger-codegen-ignore .travis.yml git_push.sh;
+git co README.md
