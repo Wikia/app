ffde400b986e178e362d276166e80acb40444927
nmonterroso <nelsonmonterroso@gmail.com>
2016-08-15 15:09:16 -0700

    run git co on composer.json and autoload.php as part of cleanup

--- lib/Swagger/cleanup.sh
+++ lib/Swagger/cleanup.sh
@@ -1,4 +1,4 @@
 # this should be run from lib/Swagger/ AFTER generating code
 
 rm -rf test/ docs/Api/ docs/Model/ LICENSE ../LICENSE .travis.yml git_push.sh;
-git co README.md
+git co README.md composer.json autoload.php
