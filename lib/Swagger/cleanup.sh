3698dede84e06cbb56f022e3c13a09193b43a6ec
nmonterroso <nelsonmonterroso@gmail.com>
2016-08-15 15:05:07 -0700

    don't remove swagger codegen ignore

--- lib/Swagger/cleanup.sh
+++ lib/Swagger/cleanup.sh
@@ -1,4 +1,4 @@
 # this should be run from lib/Swagger/ AFTER generating code
 
-rm -rf test/ docs/Api/ docs/Model/ LICENSE ../LICENSE ../.swagger-codegen-ignore .travis.yml git_push.sh;
+rm -rf test/ docs/Api/ docs/Model/ LICENSE ../LICENSE .travis.yml git_push.sh;
 git co README.md
