62819d1e119c74e95452f12e550e290b7e39ade4
TK-999 <tk.999.wikia@gmail.com>
2016-08-16 12:33:21 +0200

    SUS-893: Refactor UserTagsStrategy

---
+++ extensions/wikia/UserProfilePageV3/UserTagsStrategy.class.php
@@ -0,0 +1,220 @@
+<?php
+use \Wikia\DependencyInjection\Injector;
+use \Wikia\Service\User\Permissions\PermissionsService;
+
+/**
+ * Class for handling user "tags" logic in user masthead
+ * @class UserTagsStrategy
+ */
+class UserTagsStrategy extends WikiaObject {
+
+	/** @var int MAX_USER_TAGS Positive even integer indicating at most how many user tags can be shown */
+	const MAX_USER_TAGS = 2;
+
+	/** @var array GLOBAL_GROUPS_RANK Global groups that should be shown in masthead, in order of importance  */
+	const GLOBAL_GROUPS_RANK = [
+		'staff',
+		'helper',
+		'vstf',
+		'voldev',
+		'wikiastars',
+		'vanguard',
+		'council',
+	];
+
+	/** @var array LOCAL_GROUPS_RANK Local groups that should be shown in masthead, in order of importance  */
+	const LOCAL_GROUPS_RANK = [
+		'bureaucrat',
+		'sysop',
+		'threadmoderator',
+		'chatmoderator',
+	];
+
+	/** @var string[] List of explicit global groups this user belongs to */
+	protected $usersGlobalGroups = [];
+
+	/** @var string[] List of explicit local groups this user belongs to */
+	protected $usersLocalGroups = [];
+
+	/** @var array $globalGroupsWithTags Global groups that have an associated user tag */
+	protected $globalGroupsWithTags = [];
+
+	/** @var array $localGroupsWithTags Local groups that have an associated user tag */
+	protected $localGroupsWithTags = [];
+
+	/** @var User $user The user whose masthead we are rendering */
+	protected $user = null;
+
+	/**
+	 * UserTagsStrategy constructor.
+	 * @param User $user The user whose masthead we are rendering
+	 */
+	public function __construct( User $user ) {
+		parent::__construct();
+		/** @var PermissionsService $permissionsService */
+		$permissionsService = Injector::getInjector()->get( PermissionsService::class );
+
+		$this->usersGlobalGroups = $permissionsService->getExplicitGlobalGroups( $user );
+		$this->usersLocalGroups = $permissionsService->getExplicitLocalGroups( $user );
+		$this->globalGroupsWithTags = array_intersect( static::GLOBAL_GROUPS_RANK, $this->usersGlobalGroups );
+		$this->localGroupsWithTags = array_intersect( static::LOCAL_GROUPS_RANK, $this->usersLocalGroups );
+		$this->user = $user;
+	}
+
+	/**
+	 * Returns if the "BLOCKED" tag should be shown
+	 * i.e. if the user is blocked globally or locally, and isn't a staff member
+	 * @return bool Whether to display the tag
+	 */
+	protected function shouldShowBlockedTag() {
+		if ( in_array( 'staff', $this->usersGlobalGroups ) ) {
+			return false;
+		}
+
+		// check if the user is blocked locally, if not, also check if they're blocked globally (via Phalanx)
+		if ( $this->user->isBlocked( true /* use slave DB */, false /* don't log in PhalanxStats */ ) ||
+			$this->user->isBlockedGlobally()
+		) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Returns if the "FOUNDER" tag should be displayed
+	 * i.e. if the user founded the wiki and is still a bureaucrat or admin
+	 * @return boolean
+	 * @author Andrzej 'nAndy' Łukaszewski
+	 */
+	protected function shouldShowFounderTag() {
+		$wiki = WikiFactory::getWikiByID( $this->wg->CityId );
+		if ( intval( $wiki->city_founding_user ) === $this->user->getId() ) {
+			// mech: BugId 18248
+			return !empty( array_intersect( [ 'bureaucrat', 'sysop' ], $this->usersLocalGroups ) );
+		}
+
+		return false;
+	}
+
+	/**
+	 * Returns if the "BANNED FROM CHAT" tag should be displayed
+	 * i.e. if the user is banned from chat
+	 * @return boolean
+	 */
+	protected function shouldShowBannedFromChatTag() {
+		return $this->wg->EnableChat && ( new ChatUser( $this->user ) )->isBanned();
+	}
+
+	/**
+	 * Tries to get at most $requested user tags for this user's global groups
+	 * @param int $requested Display at most this many global tags
+	 * @return array Text of the tags, safe for HTML output
+	 */
+	protected function getGlobalTags( $requested = 1 ) {
+		$tags = [];
+		// Don't try to show too many tags
+		$count = min( $requested, static::MAX_USER_TAGS, count( $this->globalGroupsWithTags ) );
+		for ( $i = 0; $i < $count; $i++ ) {
+			switch ( array_shift( $this->globalGroupsWithTags ) ) {
+				case 'staff':
+					$tags[] = wfMessage( 'user-identity-box-group-staff' )->escaped();
+					break;
+				case 'helper':
+					$tags[] = wfMessage( 'user-identity-box-group-helper' )->escaped();
+					break;
+				case 'vstf':
+					$tags[] = wfMessage( 'user-identity-box-group-vstf' )->escaped();
+					break;
+				case 'voldev':
+					$tags[] = wfMessage( 'user-identity-box-group-voldev' )->escaped();
+					break;
+				case 'wikiastars':
+					$tags[] = wfMessage( 'user-identity-box-group-wikiastars' )->escaped();
+					break;
+				case 'vanguard':
+					$tags[] = wfMessage( 'user-identity-box-group-vanguard' )->escaped();
+					break;
+				case 'council':
+					$tags[] = wfMessage( 'user-identity-box-group-council' )->escaped();
+					break;
+			}
+		}
+
+		return $tags;
+	}
+
+	/**
+	 * Tries to get at most $requested user tags for this user's local groups
+	 * @param int $requested Display at most this many local tags
+	 * @return string Text of the tag, safe for HTML output
+	 */
+	protected function getLocalTags( $requested = 1 ) {
+		$tags = [];
+		// Don't try to show too many tags
+		$count = min( $requested, static::MAX_USER_TAGS, count( $this->localGroupsWithTags ) );
+		for ( $i = 0; $i < $count; $i++ ) {
+			switch ( array_shift( $this->localGroupsWithTags ) ) {
+				case 'bureaucrat':
+					$tags[] = wfMessage( 'user-identity-box-group-bureaucrat' )->escaped();
+					break;
+				case 'sysop':
+					$tags[] = wfMessage( 'user-identity-box-group-sysop' )->escaped();
+					break;
+				case 'threadmoderator':
+					$tags[] = wfMessage( 'user-identity-box-group-threadmoderator' )->escaped();
+					break;
+				case 'chatmoderator':
+					$tags[] = wfMessage( 'user-identity-box-group-chatmoderator' )->escaped();
+					break;
+			}
+		}
+		
+		if ( $this->shouldShowFounderTag() ) {
+			$tags[$count - 1] = wfMessage( 'user-identity-box-founder' )->escaped();
+		}
+
+		if ( $this->shouldShowBannedFromChatTag() ) {
+			// If we have enough tags, overwrite the last one, otherwise append
+			$index = $count === $requested ? $count - 1 : $count;
+			$tags[$index] = wfMessage( 'user-identity-box-banned-from-chat' )->escaped();
+		}
+
+		return $tags;
+	}
+
+	/**
+	 * Returns the user tags that should be displayed
+	 * @return array The list of user tags to be shown
+	 */
+	public function getUserTags() {
+		if ( $this->shouldShowBlockedTag() ) {
+			return [
+				wfMessage( 'user-identity-box-blocked' )->escaped()
+			];
+		}
+
+		$allGlobalTags = count( $this->globalGroupsWithTags );
+		$allLocalTags = count( $this->localGroupsWithTags );
+		// If there are enough local tags, try to distribute global and local tags evenly
+		if ( $allLocalTags >= static::MAX_USER_TAGS / 2 ) {
+			$globalTagCount = static::MAX_USER_TAGS / 2;
+		} else {
+			// If there aren't enough local tags, try to render more global tags
+			$globalTagCount = static::MAX_USER_TAGS - $allLocalTags;
+		}
+
+		// If there are enough global tags, try to distribute global and local tags evenly
+		if ( $allGlobalTags >= static::MAX_USER_TAGS / 2 ) {
+			$localTagCount = static::MAX_USER_TAGS / 2;
+		} else {
+			// If there aren't enough global tags, try to render more local tags
+			$localTagCount = static::MAX_USER_TAGS - $allGlobalTags;
+		}
+
+		return array_merge(
+			$this->getGlobalTags( $globalTagCount ),
+			$this->getLocalTags( $localTagCount )
+		);
+	}
+}
---
+++ extensions/wikia/UserProfilePageV3/tests/UserTagsStrategyTest.php
@@ -0,0 +1,190 @@
+<?php
+use \Wikia\Service\User\Permissions\PermissionsServiceImpl;
+use \Wikia\Service\User\Permissions\PermissionsService;
+use \Wikia\DependencyInjection\Injector;
+use \DI\ContainerBuilder;
+
+/**
+ * @class UserTagsStrategyTest
+ * @covers UserTagsStrategy
+ */
+class UserTagsStrategyTest extends WikiaBaseTest {
+	/** @var Injector $injector Original Injector singleton instance */
+	private static $injector = null;
+
+	/**
+	 * Back up Injector instance before test
+	 */
+	public static function setUpBeforeClass() {
+		static::$injector = Injector::getInjector();
+		parent::setUpBeforeClass();
+	}
+
+	public function setUp() {
+		$this->setupFile = __DIR__ . '/../UserProfilePage.setup.php';
+		parent::setUp();
+
+		$this->mockGlobalVariable( 'wgEnableChat', true );
+	}
+
+	/**
+	 * @param array $globalGroups Array of strings (global groups for this user)
+	 * @param array $localGroups Array of strings (local groups for this user)
+	 * @param bool $isBlocked Whether this user is blocked
+	 * @param bool $isChatBanned Whether this user is banned from chat
+	 * @param bool $isFounder Whether this user is the founder
+	 * @param array $expectedTags Array of strings (expected tags and their content)
+	 * @covers UserTagsStrategy::getUserTags
+	 * @dataProvider getUserTagsDataProvider
+	 */
+	public function testGetUserTags( array $globalGroups, array $localGroups, $isBlocked, $isChatBanned, $isFounder, array $expectedTags ) {
+		$permissionsServiceMock = $this->getMock( PermissionsServiceImpl::class, [ 'getExplicitGlobalGroups' , 'getExplicitLocalGroups' ] );
+		$permissionsServiceMock->expects( $this->any() )
+			->method( 'getExplicitGlobalGroups' )
+			->willReturn( $globalGroups );
+		$permissionsServiceMock->expects( $this->any() )
+			->method( 'getExplicitLocalGroups' )
+			->willReturn( $localGroups );
+		$container = ContainerBuilder::buildDevContainer();
+		$container->set( PermissionsService::class, $permissionsServiceMock );
+		$injector = new Injector( $container );
+		Injector::setInjector( $injector );
+
+		$chatUserMock = $this->getMockBuilder( ChatUser::class )
+			->disableOriginalConstructor()
+			->setMethods( [ 'isBanned' ] )
+			->getMock();
+		$chatUserMock->expects( $this->any() )
+			->method( 'isBanned' )
+			->willReturn( $isChatBanned );
+		$this->mockClass( ChatUser::class, $chatUserMock );
+
+		$userMock = $this->getMock( User::class, [ 'isBlocked', 'getId' ] );
+		$userMock->expects( $this->any() )
+			->method( 'isBlocked' )
+			->willReturn( $isBlocked );
+		$userMock->expects( $this->any() )
+			->method( 'getId' )
+			->willReturn( 1 );
+
+		$this->mockStaticMethod( WikiFactory::class, 'getWikiById', (object)[ 'city_founding_user' => (int) $isFounder ] );
+
+		$strategy = new UserTagsStrategy( $userMock );
+		$this->assertEquals( $expectedTags, $strategy->getUserTags() );
+	}
+
+	public function getUserTagsDataProvider() {
+		return [
+			'normal user' => [ [], [], false, false, false,
+				[]
+			],
+			'blocked user' => [ [], [], true, false, false,
+				[ wfMessage( 'user-identity-box-blocked' )->escaped() ]
+			],
+			'blocked user with other rights' => [ [ 'helper' ], [ 'sysop' ], true, true, true,
+				[ wfMessage( 'user-identity-box-blocked' )->escaped() ]
+			],
+			'blocked staff member' => [ [ 'staff' ], [], true, false, false,
+				[ wfMessage( 'user-identity-box-group-staff' )->escaped() ]
+			],
+			'blocked staff member with other rights' => [ [ 'staff' ], [ 'sysop' ], true, false, false,
+				[
+					wfMessage( 'user-identity-box-group-staff' )->escaped(),
+					wfMessage( 'user-identity-box-group-sysop' )->escaped()
+				]
+			],
+			'staff founder' => [ [ 'staff' ], [ 'bureaucrat', 'sysop' ], false, false, true,
+				[
+					wfMessage( 'user-identity-box-group-staff' )->escaped(),
+					wfMessage( 'user-identity-box-founder' )->escaped()
+				]
+			],
+			'founder with other global rights' => [ [ 'helper', 'vstf' ], [ 'bureaucrat', 'sysop' ], false, false, true,
+				[
+					wfMessage( 'user-identity-box-group-helper' )->escaped(),
+					wfMessage( 'user-identity-box-founder' )->escaped()
+				]
+			],
+			'staff and local admin' => [ [ 'staff' ], [ 'sysop' ], false, false, false,
+				[
+					wfMessage( 'user-identity-box-group-staff' )->escaped(),
+					wfMessage( 'user-identity-box-group-sysop' )->escaped()
+				]
+			],
+			'multiple global rights and local admin' => [ [ 'vstf', 'council' ], [ 'sysop' ], false, false, false,
+				[
+					wfMessage( 'user-identity-box-group-vstf' )->escaped(),
+					wfMessage( 'user-identity-box-group-sysop' )->escaped()
+				]
+			],
+			'local rights only' => [ [], [ 'bureaucrat', 'threadmoderator', 'sysop' ], false, false, false,
+				[
+					wfMessage( 'user-identity-box-group-bureaucrat' )->escaped(),
+					wfMessage( 'user-identity-box-group-sysop' )->escaped()
+				]
+			],
+			'global rights only' => [ [ 'staff', 'vstf' ], [], false, false, false,
+				[
+					wfMessage( 'user-identity-box-group-staff' )->escaped(),
+					wfMessage( 'user-identity-box-group-vstf' )->escaped()
+				]
+			],
+			'founder with no global and multiple local rights' => [ [], [ 'threadmoderator', 'sysop' ], false, false, true,
+				[
+					wfMessage( 'user-identity-box-group-sysop' )->escaped(),
+					wfMessage( 'user-identity-box-founder' )->escaped()
+				]
+			],
+			'banned from chat' => [ [], [], false, true, false,
+				[ wfMessage( 'user-identity-box-banned-from-chat' )->escaped() ]
+			],
+			'banned from chat with global rights and no local rights' => [ [ 'council' ], [], false, true, false,
+				[
+					wfMessage( 'user-identity-box-group-council' )->escaped(),
+					wfMessage( 'user-identity-box-banned-from-chat' )->escaped()
+				]
+			],
+			'banned from chat with local rights and no global rights' => [ [], [ 'threadmoderator' ], false, true, false,
+				[
+					wfMessage( 'user-identity-box-group-threadmoderator' )->escaped(),
+					wfMessage( 'user-identity-box-banned-from-chat' )->escaped()
+				]
+			],
+			'banned from chat with global and local rights' => [ [ 'council' ], [ 'sysop' ], false, true, false,
+				[
+					wfMessage( 'user-identity-box-group-council' )->escaped(),
+					wfMessage( 'user-identity-box-banned-from-chat' )->escaped()
+				]
+			],
+			'desysopped founder' => [ [], [], false, false, true,
+				[]
+			],
+			'desysopped founder with global rights' => [ [ 'staff', 'helper' ], [], false, false, true,
+				[
+					wfMessage( 'user-identity-box-group-staff' )->escaped(),
+					wfMessage( 'user-identity-box-group-helper' )->escaped()
+				]
+			],
+			'desysopped founder with local rights' => [ [], [ 'threadmoderator', 'chatmoderator' ], false, false, true,
+				[
+					wfMessage( 'user-identity-box-group-threadmoderator' )->escaped(),
+					wfMessage( 'user-identity-box-group-chatmoderator' )->escaped()
+				]
+			],
+			'desysopped founder with global and local rights' => [ [ 'vanguard' ], [ 'chatmoderator' ], false, false, true,
+				[
+					wfMessage( 'user-identity-box-group-vanguard' )->escaped(),
+					wfMessage( 'user-identity-box-group-chatmoderator' )->escaped()
+				]
+			],
+		];
+	}
+
+	/**
+	 * Restore original Injector instance
+	 */
+	public static function tearDownAfterClass() {
+		Injector::setInjector( static::$injector );
+		parent::tearDownAfterClass();
+	}
+}
