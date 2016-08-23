e341efbdeb9428d98e829b4f8ceb998b042ad931
Paul Oslund <poslund@wikia-inc.com>
2016-08-23 17:32:28 +0000

    RECIRC-201: Switch to using static instead of self in ParselyDataService

--- extensions/wikia/Recirculation/services/ParselyDataService.class.php
+++ extensions/wikia/Recirculation/services/ParselyDataService.class.php
@@ -33,11 +33,11 @@ class ParselyDataService {
 			return [];
 		}
 
-		$memcKey = wfSharedMemcKey( __METHOD__, $type, $metadata['key'], $count, self::MCACHE_VER );
+		$memcKey = wfSharedMemcKey( __METHOD__, $type, $metadata['key'], $count, static::MCACHE_VER );
 
 		$data = WikiaDataAccess::cache(
 			$memcKey,
-			self::MCACHE_TIME,
+			static::MCACHE_TIME,
 			function() use ( $type, $metadata, $count ) {
 				return $this->apiRequest( $type, $metadata, $count );
 			}
@@ -53,16 +53,16 @@ class ParselyDataService {
 	 */
 	private function apiRequest( $type, $meta, $count ) {
 		$options = [];
-		$options['limit'] = $count + self::PARSELY_API_EXTRA;
+		$options['limit'] = $count + static::PARSELY_API_EXTRA;
 
 		switch ( $type ) {
 			case 'vertical':
-				$options['sort'] = self::PARSELY_API_SORT;
+				$options['sort'] = static::PARSELY_API_SORT;
 				$options['pub_days'] = 30;
 				$endpoint = 'analytics/tag/' . rawurlencode( $meta['tag'] ) . '/detail';
 				break;
 			case 'community':
-				$options['sort'] = self::PARSELY_API_SORT;
+				$options['sort'] = static::PARSELY_API_SORT;
 				$options['pub_days'] = 30;
 				$endpoint = 'analytics/tag/' . rawurlencode( $meta['tag'] ) . '/detail';
 				break;
@@ -71,19 +71,19 @@ class ParselyDataService {
 				$endpoint = 'shares/posts';
 				break;
 			case 'recent_popular':
-				$options['sort'] = self::PARSELY_API_SORT;
-				$options['pub_days'] = self::PARSELY_API_PUB_DAYS;
+				$options['sort'] = static::PARSELY_API_SORT;
+				$options['pub_days'] = static::PARSELY_API_PUB_DAYS;
 				$endpoint = 'analytics/posts';
 				break;
 			case 'popular':
 			default:
-				$options['sort'] = self::PARSELY_API_SORT;
+				$options['sort'] = static::PARSELY_API_SORT;
 				$endpoint = 'analytics/posts';
 				break;
 		}
 
 		$url = $this->buildUrl( $endpoint, $options );
-		$data = ExternalHttp::get( $url, self::PARSELY_API_TIMEOUT );
+		$data = ExternalHttp::get( $url, static::PARSELY_API_TIMEOUT );
 
 		$posts = json_decode( $data, true );
 
@@ -106,12 +106,12 @@ class ParselyDataService {
 		$defaultParams = [
 			'apikey' => $wgParselyApiKey,
 			'secret' => $wgParselyApiSecret,
-			'page' => self::PARSELY_API_PAGE
+			'page' => static::PARSELY_API_PAGE
 		];
 
 		$params = array_merge( $defaultParams, $options );
 
-		$url = self::PARSELY_API_BASE . $endpoint . '?' . http_build_query( $params );
+		$url = static::PARSELY_API_BASE . $endpoint . '?' . http_build_query( $params );
 
 		return $url;
 	}
