4d991a4d9ecde9bf2d44a097743472942601f9cf
Michał Turek <mturek@wikia-inc.com>
2016-08-24 16:51:43 +0200

    SOC-2816 Rename to SpecialDiscussionsNavigation

---
+++ extensions/wikia/SpecialDiscussionsNavigation/SpecialDiscussionsNavigation.setup.php
@@ -0,0 +1,15 @@
+<?php
+$dir = __DIR__ . '/';
+
+$wgExtensionCredits[ 'specialpage' ][] = [
+	'name' => 'SpecialDiscussionsNavigation',
+	'author' => [
+		'Michal "slayful" Turek'
+	],
+	'descriptionmsg' => 'wikia-discussion-desc',
+	'version' => 0.1,
+	'url' => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/SpecialDiscussionsNavigation'
+];
+
+$wgAutoloadClasses[ 'SpecialDiscussionsNavigationController' ] = $dir . 'SpecialDiscussionsNavigationController.class.php';
+$wgSpecialPages[ 'Discussion' ] = 'SpecialDiscussionsNavigationController';
---
+++ extensions/wikia/SpecialDiscussionsNavigation/SpecialDiscussionsNavigationController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class SpecialDiscussionsNavigationController
+ * @desc Special:DiscussionsNavigation controller
+ */
+class SpecialDiscussionsNavigationController extends WikiaSpecialPageController {
+
+	public function __construct() {
+		parent::__construct( 'SpecialDiscussionsNavigation', '', false );
+	}
+
+	public function index() {
+		$this->wg->out->setArticleBodyOnly( true );
+		$this->wg->out->setSquidMaxage( WikiaResponse::CACHE_LONG );
+		$this->getResponse()->redirect( '/d' );
+	}
+
+}
