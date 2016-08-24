3dd577cd9ea86a3f09e4175ffd24bab8f3318ab0
TK-999 <tk.999.wikia@gmail.com>
2016-08-24 17:47:07 +0200

    SUS-270: Improve PhalanxPager documentation

--- extensions/wikia/PhalanxII/classes/PhalanxPager.class.php
+++ extensions/wikia/PhalanxII/classes/PhalanxPager.class.php
@@ -1,6 +1,12 @@
 <?php
 
+/**
+ * @class PhalanxPager
+ * Class handling the output of the block list as returned by Phalanx service
+ * @see https://www.mediawiki.org/wiki/Manual:Pager.php
+ */
 class PhalanxPager extends ReverseChronologicalPager {
+	/** @var WikiaApp $app */
 	protected $app = null;
 	protected $id = 0;
 	private $pInx = '';
@@ -40,7 +46,7 @@ class PhalanxPager extends ReverseChronologicalPager {
 	 *  typeId => true
 	 * )
 	 *
-	 * @return Array
+	 * @return array
 	 */
 	function getSearchFilter() {
 		if ( is_array( $this->mSearchFilter ) ) {
@@ -51,6 +57,10 @@ class PhalanxPager extends ReverseChronologicalPager {
 		}
 	}
 
+	/**
+	 * Get SQL query config to be used as fallback if Phalanx service is unavailable
+	 * @return array Query config that will be passed to database helper functions
+	 */
 	function getQueryInfo() {
 		$query['tables'] = 'phalanx';
 		$query['fields'] = '*';
@@ -75,29 +85,50 @@ class PhalanxPager extends ReverseChronologicalPager {
 		return $query;
 	}
 
+	/**
+	 * Returns the name of the table field used for indexing and sorting if PHP fallback is used
+	 * @return string table field used for indexing and sorting if PHP fallback is used
+	 */
 	function getIndexField() {
 		return 'p_timestamp';
 	}
 
+	/**
+	 * Returns the opening tag for the HTML element wrapping the results
+	 * @return string Opening HTML tag for wrapper
+	 */
 	function getStartBody() {
 		return Html::openElement( 'ul', [ "id" => 'phalanx-block-' . $this->pInx . '-' . $this->id . '-stats' ] );
 	}
 
+	/**
+	 * Returns the closing tag for the HTML element wrapping the results
+	 * @return string Closing HTML tag for wrapper
+	 */
 	function getEndBody() {
 		return Html::closeElement( 'ul' );
 	}
 
+	/**
+	 * Returns the boilerplate content to be displayed if no results are available
+	 * @return string HTML to display if no results are available
+	 */
 	function getEmptyBody() {
 		return Html::element( 'div', [ 'class' => 'error' ], $this->msg( 'phalanx-no-results' )->text() );
 	}
 
+	/**
+	 * Format a single result row for output
+	 * @param stdClass $row Object returned by Phalanx service containing info about this block
+	 * @return string Formatted HTML
+	 */
 	function formatRow( $row ) {
 		// hide e-mail filters
 		if ( ( $row->p_type & Phalanx::TYPE_EMAIL ) && !$this->getUser()->isAllowed( 'phalanxemailblock' ) ) {
 			return '';
 		}
 
-		// SUS-270: Get author from correct field
+		// SUS-270: Get author from correct field (p_authorId instead of p_author_id)
 		if ( isset( $row->p_authorId ) ) {
 			$author = User::newFromId( $row->p_authorId );
 			$authorName = $author->getName();
