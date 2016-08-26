90290efa5002b5560f225fe6e7d40974c1daeaa7
nAndy <nandy@wikia-inc.com>
2016-08-26 08:36:53 +0000

    SUS-912 change the way how the script will work

--- maintenance/wikia/WikiaMaps/imapTags.php
+++ maintenance/wikia/WikiaMaps/imapTags.php
@@ -1,23 +1,48 @@
 <?php
 
 /**
- * Maintenance script to get information about imap tags used in articles on a wiki and to remove them
+ * Maintenance script to remove real-world <imap /> tags from articles
  */
 
 ini_set( "include_path", dirname( __FILE__ )."/../../" );
 
 require_once( "commandLine.inc" );
 
-if ( isset( $options['help'] ) ) {
-	die(
-<<<TXT
-Usage: php imapTags.php [--help] [--dry-run] [--action] [--city_id] [--map_id]
---action		which action you want to proceed with:
-	info		to gather statistics
-	remove		to remove <imaps> tag
+$displayHelp = isset( $options['help'] );
+$dryRun = isset( $options['help'] );
+$cityId = $options['city-id'];
+
+function isValidCityId($cityId) {
+	$cityId = intval($cityId);
+
+	if( $cityId <= 0 ) {
+		return false;
+	}
+
+	return true;
+}
+
+function run( $cityId, $displayHelp, $dryRun ) {
+	if ( $displayHelp ) {
+		die(
+		<<<TXT
+		Usage: php imapTags.php [--help] [--dry-run] [--city-id]
 --dry-run		dry run - prints information to the output but does not modify data
 --help			you are reading it right now
 
 TXT
-	);
+		);
+	}
+
+	if ( !isValidCityId($cityId) ) {
+		die("Invalid city-id. Try again.");
+	}
+
+	if ( $dryRun ) {
+		echo 'mode: dry run' . PHP_EOL;
+	}
+
+	echo 'Done.' . PHP_EOL;
 }
+
+run( $cityId, $displayHelp, $dryRun );
