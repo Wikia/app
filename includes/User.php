c480cff48a291238d2107ab317bdf80bf339a2b4
Kamil <kamil.piotrowski@student.put.poznan.pl>
2016-08-26 14:19:44 +0200

    Update User.php
    indents

--- includes/User.php
+++ includes/User.php
@@ -2167,8 +2167,8 @@ class User {
 				}
 				throw new PasswordError( wfMsgExt( $message, array( 'parsemag' ), $params ) );
 			}
-            //Delete all access tokens
-            $this->removeAllTokens();
+	            	//Delete all access tokens
+	            	$this->removeAllTokens();
 		}
 
 		if( !$wgAuth->setPassword( $this, $str ) ) {
