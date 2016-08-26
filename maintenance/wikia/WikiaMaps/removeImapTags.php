92c72112c22d38889e22f836080f8720ca2e151b
nAndy <nandy@wikia-inc.com>
2016-08-26 08:44:01 +0000

    SUS-912 rename file, add tiles-set-id option and update help message

--- maintenance/wikia/WikiaMaps/removeImapTags.php
+++ maintenance/wikia/WikiaMaps/removeImapTags.php
@@ -26,7 +26,9 @@ function run( $cityId, $displayHelp, $dryRun ) {
 	if ( $displayHelp ) {
 		die(
 		<<<TXT
-		Usage: php imapTags.php [--help] [--dry-run] [--city-id]
+		Usage: php removeImapTags.php [--help] [--dry-run] --city-id --tiles-set-id
+--city-id		wiki id on which the clean up is supposed to take place
+--tiles-set-id		tiles-set-id used by maps which thumbnails are supposed to be removed
 --dry-run		dry run - prints information to the output but does not modify data
 --help			you are reading it right now
 
