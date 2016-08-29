e1425e3da49d892a439d239f484cf7c411cc9306
Per Johan Groland <per@akafugu.jp>
2016-08-29 17:12:13 +0900

    JPN-6ode cleanup

--- extensions/wikia/EmbeddableDiscussions/scripts/sharing.js
+++ extensions/wikia/EmbeddableDiscussions/scripts/sharing.js
@@ -2,8 +2,7 @@
  * Functionality for social network sharing for Embeddable Discussions extension
  */
 define('EmbeddableDiscussionsSharing',
-	['jquery'],
-	function ($) {
+	function () {
 		'use strict';
 
 		var socialNetworks = {
@@ -58,47 +57,28 @@ define('EmbeddableDiscussionsSharing',
 			]
 		};
 
+		var socialNetworkUrls = {
+			'meneame': 'https://www.meneame.net/submit.php?url=URL',
+			'wykop': 'http://www.wykop.pl/dodaj/link/?url=URL&title=TITLE',
+			'nk': 'http://nk.pl/sledzik?shout=URL',
+			'odnoklassniki': 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl=URL',
+			'vkontakte': 'http://vk.com/share.php?url=URL&title=TITLE',
+			'weibo': 'http://service.weibo.com/share/share.php?url=URL&title=TITLE',
+			'tumblr': 'http://www.tumblr.com/share/link?url=URL&name=TITLE',
+			'reddit': 'http://www.reddit.com/submit?url=URL&title=TITLE',
+			'google': 'https://plus.google.com/share?url=URL',
+			'twitter': 'https://twitter.com/share?url=URL',
+			'facebook': 'http://www.facebook.com/sharer/sharer.php?u=URL',
+			'line': 'http://line.me/R/msg/text/?URL TITLE',
+		};
+
 		function getShareUrl(network, sharedUrl, sharedTitle) {
-			switch (network) {
-				case 'meneame':
-					return 'https://www.meneame.net/submit.php' +
-						'?url=' + encodeURIComponent(sharedUrl);
-				case 'wykop':
-					return 'http://www.wykop.pl/dodaj/link/' +
-						'?url=' + encodeURIComponent(sharedUrl) +
-						'&title=' + encodeURIComponent(sharedTitle);
-				case 'nk':
-					return 'http://nk.pl/sledzik?shout=' + encodeURIComponent(sharedUrl);
-				case 'odnoklassniki':
-					return 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1' +
-						'&st._surl=' + encodeURIComponent(sharedUrl);
-				case 'vkontakte':
-					return 'http://vk.com/share.php?url=' + encodeURIComponent(sharedUrl) +
-						'&title=' + encodeURIComponent(sharedTitle);
-				case 'weibo':
-					return 'http://service.weibo.com/share/share.php' +
-						'?url=' + encodeURIComponent(sharedUrl) +
-						'&title=' + encodeURIComponent(sharedTitle);
-				case 'tumblr':
-					return 'http://www.tumblr.com/share/link' +
-						'?url=' + encodeURIComponent(sharedUrl) +
-						'&name=' + encodeURIComponent(sharedTitle);
-				case 'reddit':
-					return 'http://www.reddit.com/submit' +
-						'?url=' + encodeURIComponent(sharedUrl) +
-						'&title=' + encodeURIComponent(sharedTitle);
-				case 'google':
-					return 'https://plus.google.com/share?url=' + encodeURIComponent(sharedUrl);
-				case 'twitter':
-					return 'https://twitter.com/share?url=' + encodeURIComponent(sharedUrl);
-				case 'facebook':
-					return 'http://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(sharedUrl);
-				case 'line':
-					return 'http://line.me/R/msg/text/?' +
-						encodeURIComponent(sharedTitle + ' ' + sharedUrl);
-				default:
-					return '';
+			if (socialNetworkUrls[network]) {
+				return socialNetworkUrls[network].replace('URL', encodeURIComponent(sharedUrl))
+				  .replace('TITLE', encodeURIComponent(sharedTitle));
 			}
+
+			return '';
 		}
 
 		function getNetworks(lang) {
@@ -129,6 +109,6 @@ define('EmbeddableDiscussionsSharing',
 
 		return {
 			getData: getData,
-		}
+		};
 	}
 );
