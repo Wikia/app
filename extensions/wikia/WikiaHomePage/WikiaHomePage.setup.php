09ccf19312a73a36966f4350a5c431cdd8b4d5b5
rafal.kalinski <kalina@wikia-inc.com>
2016-08-31 11:18:32 +0000

    SUS-853 | WikiaHomePage.setup.php - revert changes

--- extensions/wikia/WikiaHomePage/WikiaHomePage.setup.php
+++ extensions/wikia/WikiaHomePage/WikiaHomePage.setup.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * FandomHomePage
+ * WikiaHomePage
  *
  * @author Andrzej 'nAndy' Łukaszewski
  * @author Hyun Lim
@@ -13,36 +13,36 @@
 $dir = dirname(__FILE__) . '/';
 
 $wgExtensionCredits['other'][] = array(
-	'name'				=> 'FandomHomePage',
+	'name'				=> 'WikiaHomePage',
 	'author'			=> array(
-		'Andrzej "nAndy" Łukaszewski', 
-		'Hyun Lim', 
-		'Marcin Maciejewski', 
-		'Saipetch Kongkatong', 
-		'Sebastian Marzjan', 
+		'Andrzej "nAndy" Łukaszewski',
+		'Hyun Lim',
+		'Marcin Maciejewski',
+		'Saipetch Kongkatong',
+		'Sebastian Marzjan',
 		'Damian Jóźwiak'
 	),
 	'descriptionmsg'	=> 'wikiahome-desc',
 	'version'			=> 1.0,
-	'url'               => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/FandomHomePage'
+	'url'               => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/WikiaHomePage'
 );
 
-$wgAutoloadClasses['FandomHomePageController'] = $dir.'FandomHomePageController.class.php';
+$wgAutoloadClasses['WikiaHomePageController'] = $dir.'WikiaHomePageController.class.php';
 $wgAutoloadClasses['WikiaHubsApiController'] = $dir . '../WikiaHubsServices/api/WikiaHubsApiController.class.php';
 
 //i18n mapping
-$wgExtensionMessagesFiles['FandomHomePage'] = $dir.'FandomHomePage.i18n.php';
-JSMessages::registerPackage('FandomHomePage', array('wikia-home-page-*'));
+$wgExtensionMessagesFiles['WikiaHomePage'] = $dir.'WikiaHomePage.i18n.php';
+JSMessages::registerPackage('WikiaHomePage', array('wikia-home-page-*'));
 
 // services
 $wgAutoloadClasses['RedirectService'] = 'includes/wikia/services/RedirectService.class.php';
 
 // hooks
-$wgHooks['GetHTMLAfterBody'][] = 'FandomHomePageController::onGetHTMLAfterBody';
-$wgHooks['OutputPageBeforeHTML'][] = 'FandomHomePageController::onOutputPageBeforeHTML';
-$wgHooks['WikiaMobileAssetsPackages'][] = 'FandomHomePageController::onWikiaMobileAssetsPackages';
-$wgHooks['ArticleCommentCheck'][] = 'FandomHomePageController::onArticleCommentCheck';
-$wgHooks['AfterGlobalHeader'][] = 'FandomHomePageController::onAfterGlobalHeader';
-$wgHooks['BeforePageDisplay'][] = 'FandomHomePageController::onBeforePageDisplay';
-$wgHooks['AfterOasisSettingsInitialized'][] = 'FandomHomePageController::onAfterOasisSettingsInitialized';
-$wgHooks['ArticleFromTitle'][] = 'FandomHomePageController::onArticleFromTitle';
+$wgHooks['GetHTMLAfterBody'][] = 'WikiaHomePageController::onGetHTMLAfterBody';
+$wgHooks['OutputPageBeforeHTML'][] = 'WikiaHomePageController::onOutputPageBeforeHTML';
+$wgHooks['WikiaMobileAssetsPackages'][] = 'WikiaHomePageController::onWikiaMobileAssetsPackages';
+$wgHooks['ArticleCommentCheck'][] = 'WikiaHomePageController::onArticleCommentCheck';
+$wgHooks['AfterGlobalHeader'][] = 'WikiaHomePageController::onAfterGlobalHeader';
+$wgHooks['BeforePageDisplay'][] = 'WikiaHomePageController::onBeforePageDisplay';
+$wgHooks['AfterOasisSettingsInitialized'][] = 'WikiaHomePageController::onAfterOasisSettingsInitialized';
+$wgHooks['ArticleFromTitle'][] = 'WikiaHomePageController::onArticleFromTitle';
