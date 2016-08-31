8416e85fd6f7834431a9ec3b819f1da8cd6bf7fe
TK-999 <tk.999.wikia@gmail.com>
2016-08-31 20:50:33 +0200

    MAIN-7865: Don't render MiniEditor on Forum boards on Monobook

--- extensions/wikia/Forum/templates/Forum_boardNewThread.php
+++ extensions/wikia/Forum/templates/Forum_boardNewThread.php
@@ -14,7 +14,7 @@
 						<span class="board-list-error"><?= wfMessage( 'forum-no-board-selection-error' )->escaped() ?></span>
 					</div>
 				<? endif; ?>
-				<? if ( $wg->EnableMiniEditorExtForWall ): ?>
+				<? if ( $showMiniEditor ): ?>
 					<?= $app->getView( 'MiniEditorController', 'Header', [
 						'attributes' => [
 							'data-min-height' => 200,
@@ -27,11 +27,11 @@
 					<span class="no-title-warning"><?= wfMessage( 'wall-no-title-warning' )->escaped() ?></span>
 				</div>
 				<div class="body-container">
-					<? if ( $wg->EnableMiniEditorExtForWall ): ?>
+					<? if ( $showMiniEditor ): ?>
 						<?= $app->getView( 'MiniEditorController', 'Editor_Header' )->render() ?>
 					<? endif ?>
 					<textarea class="body" data-space-type="editarea" placeholder="<?= $wall_message ?>"></textarea>
-					<? if ( $wg->EnableMiniEditorExtForWall ): ?>
+					<? if ( $showMiniEditor ): ?>
 						<?= $app->getView( 'MiniEditorController', 'Editor_Footer' )->render() ?>
 					<? endif ?>
 					<div class="buttons" data-space-type="buttons">
@@ -45,7 +45,7 @@
 						<button disabled="disabled" class="preview secondary"><?= wfMessage( 'wall-button-to-preview-comment' )->escaped() ?></button>
 					</div>
 				</div>
-				<? if ( $wg->EnableMiniEditorExtForWall ): ?>
+				<? if ( $showMiniEditor ): ?>
 					<?= $app->getView( 'MiniEditorController', 'Footer' )->render() ?>
 				<? endif ?>
 				<?= F::app()->renderPartialCached( 'Wall', 'messageTopic', [ ] ) ?>
