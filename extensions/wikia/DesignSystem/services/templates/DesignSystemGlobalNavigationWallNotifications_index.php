bb20857243f70b84c25bd59a1a0537acce2fc2a7
Igor Rogatty <igor@wikia-inc.com>
2016-08-31 16:20:21 +0200

    XW-1703 | hide notifications counter when it's empty

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigationWallNotifications_index.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigationWallNotifications_index.php
@@ -1,19 +1,18 @@
-<div class="wds-global-navigation__notifications-menu wds-dropdown wds-is-active notifications-entry-point">
+<div id="notificationsEntryPoint" class="wds-global-navigation__notifications-menu wds-global-navigation__content-item-notifications-menu wds-dropdown notifications-container">
 	<div class="wds-dropdown-toggle wds-global-navigation__dropdown-toggle">
 		<div class="bubbles">
-			<div class="wds-global-navigation__notifications-menu-counter notifications-count"></div>
+			<div class="wds-global-navigation__content-item-notifications-menu-counter notifications-count"></div>
 		</div>
-		<?= DesignSystemHelper::getSvg(
-			'wds-icons-bell',
-			'wds-icon wds-icon-small'
-		) ?>
-		<?= DesignSystemHelper::getSvg(
-			'wds-icons-dropdown-tiny',
-			'wds-icon wds-icon-tiny wds-dropdown-toggle-chevron'
-		) ?>
 	</div>
-	<div id="notifications" class="wds-dropdown-content wds-is-right-aligned wds-global-navigation__dropdown notifications-container">
-		<ul id="GlobalNavigationWallNotifications" class="WallNotifications global-nav-dropdown">
-		</ul>
+	<?= DesignSystemHelper::getSvg(
+		'wds-icons-bell',
+		'wds-icon wds-icon-small'
+	) ?>
+	<?= DesignSystemHelper::getSvg(
+		'wds-icons-dropdown-tiny',
+		'wds-icon wds-icon-tiny wds-dropdown-toggle-chevron'
+	) ?>
+	<div id="notifications" class="wds-dropdown-content wds-is-right-aligned wds-global-navigation__dropdown">
+		<ul id="GlobalNavigationWallNotifications" class="WallNotifications global-nav-dropdown"></ul>
 	</div>
 </div>
