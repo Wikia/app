74ecc9928443d83f0eac7db06ac55e8ab47bfcd6
Igor Rogatty <igor@wikia-inc.com>
2016-08-31 15:21:35 +0200

    XW-1703 | use a modified copy of JS

--- extensions/wikia/DesignSystem/services/DesignSystemGlobalNavigationService.class.php
+++ extensions/wikia/DesignSystem/services/DesignSystemGlobalNavigationService.class.php
@@ -3,7 +3,5 @@
 class DesignSystemGlobalNavigationService extends WikiaService {
 	public function index() {
 
-		$this->response->addAsset( 'extensions/wikia/GlobalNavigation/styles/GlobalNavigationNotifications.scss' );
-		$this->response->addAsset( 'wall_notifications_global_navigation_js' );
 	}
 }
--- extensions/wikia/DesignSystem/services/DesignSystemGlobalNavigationWallNotificationsService.class.php
+++ extensions/wikia/DesignSystem/services/DesignSystemGlobalNavigationWallNotificationsService.class.php
@@ -1,5 +1,8 @@
 <?php
 
 class DesignSystemGlobalNavigationWallNotificationsService extends GlobalNavigationWallNotificationsController {
-
+	protected function addAssets() {
+		$this->response->addAsset( 'extensions/wikia/GlobalNavigation/styles/GlobalNavigationNotifications.scss' );
+		$this->response->addAsset( 'extensions/wikia/DesignSystem/scripts/DesignSystemGlobalNavigationNotifications.js' );
+	}
 }
