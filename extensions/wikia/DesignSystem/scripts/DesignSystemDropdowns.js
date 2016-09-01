8ec103ed97d507fabe7ea8f9f01e0c8139fd5627
Damian Jozwiak <damian@wikia-inc.com>
2016-09-01 15:24:01 +0200

    XW-1927 |jquery variable names

--- extensions/wikia/DesignSystem/scripts/DesignSystemDropdowns.js
+++ extensions/wikia/DesignSystem/scripts/DesignSystemDropdowns.js
@@ -2,21 +2,21 @@ $(function ($) {
 	'use strict';
 
 	$('body').on('click', function(event) {
-		var eventTarget = $(event.target),
-			clickedContent = eventTarget.closest('.wds-dropdown__content'),
-			clickedDropdown = eventTarget.closest('.wds-dropdown');
+		var $eventTarget = $(event.target),
+			$clickedContent = $eventTarget.closest('.wds-dropdown__content'),
+			$clickedDropdown = $eventTarget.closest('.wds-dropdown');
 
 		// We don't use toggle as it has disabled pointer events due to bug in IE.
-		if (clickedContent.length === 0) {
-			clickedDropdown.toggleClass('wds-is-active');
+		if ($clickedContent.length === 0) {
+			$clickedDropdown.toggleClass('wds-is-active');
 
-			if (clickedDropdown.hasClass('wds-is-active')) {
-				clickedDropdown.trigger('wds-dropdown-open');
+			if ($clickedDropdown.hasClass('wds-is-active')) {
+				$clickedDropdown.trigger('wds-dropdown-open');
 			} else {
-				clickedDropdown.trigger('wds-dropdown-close');
+				$clickedDropdown.trigger('wds-dropdown-close');
 			}
 		}
 
-		$('.wds-dropdown.wds-is-active').not(clickedDropdown).removeClass('wds-is-active');
+		$('.wds-dropdown.wds-is-active').not($clickedDropdown).removeClass('wds-is-active');
 	});
 });
