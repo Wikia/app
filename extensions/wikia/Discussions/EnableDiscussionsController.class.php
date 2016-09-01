31979badc29ed855d55b1b3fafab3e167c35da02
Michał Turek <mturek@wikia-inc.com>
2016-09-01 16:22:24 +0200

    Revert "SOC-2811 use static instead of self"
    This reverts commit 12d7b5d140ff5a27633f0404e9bd4daa643b55d7.

--- extensions/wikia/Discussions/EnableDiscussionsController.class.php
+++ extensions/wikia/Discussions/EnableDiscussionsController.class.php
@@ -14,15 +14,15 @@ class EnableDiscussionsController extends \WikiaController {
 	public function index() {
 		$this->request->isInternal();
 
-		$siteId = $this->request->getParams()[static::P_SITE_ID];
+		$siteId = $this->request->getParams()[self::P_SITE_ID];
 
 		if ( !is_numeric( $siteId ) || $siteId < 0 ) {
 			throw new Fatal( 'Please provide a valid siteId, provided ' . $siteId );
 		}
 
-		$discussions = $this->setVariable( $siteId, static::ENABLE_DISCUSSIONS, true );
-		$navigation = $this->setVariable( $siteId, static::ENABLE_DISCUSSIONS_NAVIGATION, true );
-		$forum = $this->setVariable( $siteId, static::ENABLE_DISCUSSIONS_NAVIGATION, false );
+		$discussions = $this->setVariable( $siteId, self::ENABLE_DISCUSSIONS, true );
+		$navigation = $this->setVariable( $siteId, self::ENABLE_DISCUSSIONS_NAVIGATION, true );
+		$forum = $this->setVariable( $siteId, self::ENABLE_DISCUSSIONS_NAVIGATION, false );
 
 		$this->response->setBody( json_encode( [
 			'enableDiscussions' => $discussions,
@@ -43,7 +43,7 @@ class EnableDiscussionsController extends \WikiaController {
 	}
 
 	private function setVariable( $siteId, $name, $value ) {
-		$status = WikiFactory::setVarByName( $name, $siteId, $value, static::REASON );
+		$status = WikiFactory::setVarByName( $name, $siteId, $value, self::REASON );
 		if ( $status ) {
 			WikiFactory::clearCache( $siteId );
 		}
