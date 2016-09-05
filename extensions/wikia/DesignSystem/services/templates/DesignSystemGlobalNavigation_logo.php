af93c42499b39fbc102a738f1bfc487cc463f736
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-09-05 12:49:15 +0200

    XW-1957 | use php comments, add alt attribute to svg in logo

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_logo.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_logo.php
@@ -1,19 +1,21 @@
-<!--TODO: Clean up this file - https://wikia-inc.atlassian.net/browse/XW-1966-->
+<? //TODO: Clean up this file - https://wikia-inc.atlassian.net/browse/XW-1966 ?>
 <? if ( !isset( $model['header']['subtitle'] ) ): ?>
-<a href="<?= Sanitizer::encodeAttribute( $model['header']['href'] ); ?>" class="wds-global-navigation__logo">
-	<?= DesignSystemHelper::getSvg(
-		$model['header']['image'],
-		'wds-global-navigation__logo-fandom'
-	) ?>
-</a>
+	<a href="<?= Sanitizer::encodeAttribute( $model['header']['href'] ); ?>" class="wds-global-navigation__logo">
+		<?= DesignSystemHelper::getSvg(
+			$model['header']['image'],
+			'wds-global-navigation__logo-fandom',
+			wfMessage( $model['header']['title']['value'] )->escaped()
+		) ?>
+	</a>
 <? else: ?>
-<a href="<?= Sanitizer::encodeAttribute( $model['header']['href'] ); ?>" class="wds-global-navigation__logo wds-global-navigation__logo-wikia">
-	<?= DesignSystemHelper::getSvg(
-		$model['header']['image'],
-		'wds-global-navigation__logo-wikia-icon'
-	) ?>
-	<span class="wds-global-navigation__logo-wikia-caption"><?= DesignSystemHelper::renderText(
-			$model['header']['subtitle']
-		) ?></span>
-</a>
+	<a href="<?= Sanitizer::encodeAttribute( $model['header']['href'] ); ?>" class="wds-global-navigation__logo wds-global-navigation__logo-wikia">
+		<?= DesignSystemHelper::getSvg(
+			$model['header']['image'],
+			'wds-global-navigation__logo-wikia-icon',
+			wfMessage( $model['header']['title']['key'] )->escaped()
+		) ?>
+		<span class="wds-global-navigation__logo-wikia-caption"><?=
+			DesignSystemHelper::renderText( $model['header']['subtitle'] )
+		?></span>
+	</a>
 <? endif; ?>
