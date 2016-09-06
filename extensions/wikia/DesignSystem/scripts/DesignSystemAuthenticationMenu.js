230d271225ab7bfae761ffe542b56473b2070344
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-09-06 17:33:05 +0200

    XW-1932 | open auth modal on click

--- extensions/wikia/DesignSystem/scripts/DesignSystemAuthenticationMenu.js
+++ extensions/wikia/DesignSystem/scripts/DesignSystemAuthenticationMenu.js
@@ -1,6 +1,36 @@
 $(function ($) {
 	'use strict';
 
-	var signInButton = $('#global-navigation-anon-sign-in'),
-		registerButton = $('#global-navigation-anon-register');
+	var $signInButton = $('#global-navigation-anon-sign-in'),
+		$registerButton = $('#global-navigation-anon-register');
+
+	function onAuthSuccess() {
+		var redirect = this.url.replace(/.*?redirect=([^&]+).*/, '$1');
+		window.location.href = decodeURIComponent(redirect);
+	}
+
+	function openAuthModal(event) {
+		var url = event.currentTarget.href;
+
+		//Prevent opening modal with shift / alt / ctrl / let only left mouse click
+		if (event.which !== 1 || event.shiftKey || event.altKey || event.metaKey || event.ctrlKey) {
+			return;
+		}
+
+		event.preventDefault();
+
+		window.wikiaAuthModal.load({
+			url: url,
+			origin: 'global-nav',
+			onAuthSuccess: onAuthSuccess.bind({url: url})
+		});
+	}
+
+	$signInButton.click(function (event) {
+		return openAuthModal(event);
+	});
+
+	$registerButton.click(function (event) {
+		return openAuthModal(event);
+	});
 });
