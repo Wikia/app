8f83e8016bd9e4bf9bb823846d3a00b5c11c90ae
Warkot <mateusz.warkocki@wikia-inc.com>
2016-09-06 15:33:23 +0200

    XW-1932 | add id param to login and register links

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_linkAuthentication.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_linkAuthentication.php
@@ -1,6 +1,6 @@
 <?php if ( isset( $model['subtitle'] ) ): ?>
 	<div class="wds-global-navigation__account-menu-dropdown-caption"><?= DesignSystemHelper::renderText( $model['subtitle'] ) ?></div>
 <?php endif; ?>
-<a href="<?= Sanitizer::encodeAttribute( $model['href'] ); ?>" class="wds-button wds-is-full-width <?= isset( $model['subtitle'] ) ? 'wds-is-secondary' : ''; ?>">
+<a href="<?= Sanitizer::encodeAttribute( $model['href'] ); ?>" id="<?= Sanitizer::encodeAttribute( $model['title']['key'] ); ?>" class="wds-button wds-is-full-width <?= isset($model['subtitle'] ) ? 'wds-is-secondary' : ''; ?>">
 	<?= DesignSystemHelper::renderText( $model['title'] ) ?>
 </a>
