d46f246febcc0744dd5777464b58dd102ca4dab3
Piotr Gabryjeluk <rychu@wikia-inc.com>
2016-09-06 11:26:53 -0700

    SEO-477 Converted messages in category.tmpl.php to the new API

--- extensions/wikia/CategoryPagination/templates/category.tmpl.php
+++ extensions/wikia/CategoryPagination/templates/category.tmpl.php
@@ -1,30 +1,30 @@
 <? if ( $empty ): ?>
-	<?= wfMsgExt( 'category-empty', array( 'parse' ) ); ?>
+	<?= wfMessage( 'category-empty' )->parse(); ?>
 <? else: ?>
 	<?= $categoryGallery; ?>
 
 	<div lang="<?= htmlspecialchars( $lang->getCode() ); ?> dir="<?= htmlspecialchars( $lang->getDir() ); ?>">
-	<? if ( $content['subcat'] ): ?>
+	<? if ( !empty( $content['subcat'] ) ): ?>
 		<div id="mw-subcategories">
-			<h2><?= wfMsg( 'subcategories' ); ?></h2>
+			<h2><?= wfMessage( 'subcategories' )->parse(); ?></h2>
 			<?= wfMessage( 'category-subcat-count' )->numParams( $numberShown['subcat'], $counts['subcat'] )->parseAsBlock(); ?>
 			<?= $content['subcat']; ?>
 			<?= $paginator['subcat']->getBarHtml(); ?>
 		</div>
 	<? endif; ?>
 
-	<? if ( $content['page'] ): ?>
+	<? if ( !empty( $content['page'] ) ): ?>
 		<div id="mw-pages">
-			<h2><?= wfMsg( 'category_header', $titleEscaped ); ?></h2>
+			<h2><?= wfMessage( 'category_header' )->params( $titleEscaped )->parse(); ?></h2>
 			<?= wfMessage( 'category-article-count' )->numParams( $numberShown['page'], $counts['page'] )->parseAsBlock(); ?>
 			<?= $content['page']; ?>
 			<?= $paginator['page']->getBarHtml(); ?>
 		</div>
 	<? endif; ?>
 
-	<? if ( $content['file'] ): ?>
+	<? if ( !empty( $content['file'] ) ): ?>
 		<div id="mw-category-media">
-			<h2><?= wfMsg( 'category-media-header', $titleEscaped ); ?></h2>
+			<h2><?= wfMessage( 'category-media-header' )->params( $titleEscaped )->parse(); ?></h2>
 			<?= wfMessage( 'category-file-count' )->numParams( $numberShown['file'], $counts['file'] )->parseAsBlock(); ?>
 			<?= $content['file']; ?>
 			<?= $paginator['file']->getBarHtml(); ?>
