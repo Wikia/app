258543487dd0391a7cec4558c6dc23cfae6893c8
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2016-09-06 17:59:55 +0200

    ADEN-3731 Fix indentions

--- extensions/wikia/AnalyticsEngine/AnalyticsProviderUbisoft.php
+++ extensions/wikia/AnalyticsEngine/AnalyticsProviderUbisoft.php
@@ -2,42 +2,42 @@
 
 class AnalyticsProviderUbisoft implements iAnalyticsProvider {
 
-    const TEMPLATE_PATH = 'extensions/wikia/AnalyticsEngine/templates/ubisoft.mustache';
-    const DISABLED_MESSAGE = '<!-- Ubisoft disabled -->';
-    const URL = '//ubistatic2-a.akamaihd.net/worldwide_analytics/tagcommander/wikia/tc_WikiaEMEA_1.js';
+	const TEMPLATE_PATH = 'extensions/wikia/AnalyticsEngine/templates/ubisoft.mustache';
+	const DISABLED_MESSAGE = '<!-- Ubisoft disabled -->';
+	const URL = '//ubistatic2-a.akamaihd.net/worldwide_analytics/tagcommander/wikia/tc_WikiaEMEA_1.js';
 
-    /**
-     * @param array $params
-     * @return null
-     */
-    public function getSetupHtml( $params = array() ) {
-        return null;
-    }
+	/**
+	 * @param array $params
+	 * @return null
+	 */
+	public function getSetupHtml( $params = array() ) {
+		return null;
+	}
 
-    /**
-     * @param $eventName
-     * @param array $eventDetails
-     * @return string
-     */
-    public function trackEvent( $eventName, $eventDetails = array() ) {
-        return self::isEnabled() ? $this->getScript() : self::DISABLED_MESSAGE;
-    }
+	/**
+	 * @param $eventName
+	 * @param array $eventDetails
+	 * @return string
+	 */
+	public function trackEvent( $eventName, $eventDetails = array() ) {
+		return static::isEnabled() ? $this->getScript() : static::DISABLED_MESSAGE;
+	}
 
-    /**
-     * @return bool
-     */
-    public static function isEnabled() {
-        global $wgEnableUbisoft, $wgNoExternals;
+	/**
+	 * @return bool
+	 */
+	public static function isEnabled() {
+		global $wgEnableUbisoft, $wgNoExternals;
 
-        return $wgEnableUbisoft && !$wgNoExternals;
-    }
+		return $wgEnableUbisoft && !$wgNoExternals;
+	}
 
-    /**
-     * @return string
-     */
-    private function getScript() {
-        return \MustacheService::getInstance()->render( self::TEMPLATE_PATH, [
-            'url' => self::URL
-        ] );
-    }
+	/**
+	 * @return string
+	 */
+	private function getScript() {
+		return \MustacheService::getInstance()->render( static::TEMPLATE_PATH, [
+			'url' => static::URL
+		] );
+	}
 }
