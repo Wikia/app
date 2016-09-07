612a2c77c80aee0496c70ec6d4325449e4adc631
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-09-07 13:34:37 +0200

    XW-1932 | show subtitle if subtitle is provided by API

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_linkAuthentication.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_linkAuthentication.php
@@ -1,4 +1,4 @@
-<?php if ( $model['title']['key'] === 'global-navigation-anon-register' ): ?>
+<?php if ( isset( $model['subtitle'] ) ): ?>
 	<div class="wds-global-navigation__account-menu-dropdown-caption"><?= DesignSystemHelper::renderText( $model['subtitle'] ) ?></div>
 <?php endif; ?>
 <a href="<?= Sanitizer::encodeAttribute( $href ); ?>" id="<?= Sanitizer::encodeAttribute( $model['title']['key'] ); ?>" class="wds-button wds-is-full-width <?= $model['title']['key'] === 'global-navigation-anon-register' ? 'wds-is-secondary' : ''; ?>">
