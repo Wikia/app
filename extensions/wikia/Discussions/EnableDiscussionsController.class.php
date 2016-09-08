0b2243a3e3150881295245b75ac455f3fcd63170
Michał Turek <mturek@wikia-inc.com>
2016-09-08 18:31:45 +0200

    SOC-2811 address feedback

--- extensions/wikia/Discussions/EnableDiscussionsController.class.php
+++ extensions/wikia/Discussions/EnableDiscussionsController.class.php
@@ -1,28 +1,29 @@
 <?php
 
-use Email\Fatal;
-
 class EnableDiscussionsController extends \WikiaController {
 
-	const ENABLE_DISCUSSIONS_NAVIGATION = 'wgEnableDiscussionNavigation';
+	const ENABLE_DISCUSSIONS_NAV = 'wgEnableDiscussionNavigation';
 	const ENABLE_DISCUSSIONS = 'wgEnableDiscussion';
 	const ENABLE_FORUMS = 'wgEnableForumExt';
 
 	const P_SITE_ID = 'siteId';
+	const P_ROLLBACK = 'rollback';
 	const REASON = 'Enabling discussions!';
 
 	public function index() {
 		$this->assertCanAccessController();
 
-		$siteId = $this->request->getParams()[self::P_SITE_ID];
+		$siteId = $this->request->getInt( self::P_SITE_ID );
+		$isRollback = $this->request->getBool( self::P_ROLLBACK );
 
-		if ( !is_numeric( $siteId ) || $siteId < 0 ) {
-			throw new Fatal( 'Please provide a valid siteId, provided ' . $siteId );
+		if ( $siteId < 1 ) {
+			throw new InvalidArgumentException( 'Please provide a valid siteId, provided ' .
+			                                    $siteId );
 		}
 
-		$discussions = $this->setVariable( $siteId, self::ENABLE_DISCUSSIONS, true );
-		$navigation = $this->setVariable( $siteId, self::ENABLE_DISCUSSIONS_NAVIGATION, true );
-		$forum = $this->setVariable( $siteId, self::ENABLE_DISCUSSIONS_NAVIGATION, false );
+		$discussions = $this->setVariable( $siteId, self::ENABLE_DISCUSSIONS, !$isRollback );
+		$navigation = $this->setVariable( $siteId, self::ENABLE_DISCUSSIONS_NAV, !$isRollback );
+		$forum = $this->setVariable( $siteId, self::ENABLE_FORUMS, $isRollback );
 
 		$this->response->setBody( json_encode( [
 			'enableDiscussions' => $discussions,
@@ -37,9 +38,9 @@ class EnableDiscussionsController extends \WikiaController {
 	 */
 	public function assertCanAccessController() {
 		if ( !$this->request->isInternal() ) {
-			throw new Fatal( 'Access to this controller is restricted' );
+			throw new ForbiddenException( 'Access to this controller is restricted' );
 		}
-		if ( !$this->wg->request->wasPosted() ) {
+		if ( !$this->request->wasPosted() ) {
 			throw new MethodNotAllowedException( 'Only POST allowed' );
 		}
 	}
