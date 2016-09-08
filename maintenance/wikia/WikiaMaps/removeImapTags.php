fc7b987c0d81bbfd6c9cfb6372701ac5b43b9a20
Dmytro Rets <dmitryrets@gmail.com>
2016-09-08 14:18:39 +0200

    SUS-912 CR fixes 2

--- maintenance/wikia/WikiaMaps/removeImapTags.php
+++ maintenance/wikia/WikiaMaps/removeImapTags.php
@@ -3,33 +3,26 @@
 /**
  * Maintenance script to remove <imap /> tags from articles
  *
- * @usage SERVER_ID=203236 php removeImapTags.php --tiles-set-id=1 --quiet --test
+ * @usage SERVER_ID=203236 php removeImapTags.php --tiles-set-id=1 --quiet --dry-run
  */
 
 require_once( __DIR__ . '/../../Maintenance.php' );
 
-ini_set( "include_path", __DIR__ . "/../../" );
 ini_set('display_errors', 'stderr');
 ini_set('error_reporting', E_ALL);
 
 class RemoveImapTags extends Maintenance {
-	static protected $test = false;
+	static protected $dryRun = false;
 
 	private $maps;
-
-	/**
-	 * @var Array
-	 */
 	private $mapsClientConfig;
+	private $slaveDBConfig;
+
 
 	public function __construct() {
 		parent::__construct();
 		$this->mDescription = "Removes from articles <imap /> tags for given tiles' set id.";
-		$this->addOption( 'test', 'Test mode; make no changes', $required = false, $withArg = false, 't' );
-		$this->addOption( 'maps-db-host', 'WikiaMaps DB host', $required = true, $withArg = true );
-		$this->addOption( 'maps-db-user', 'WikiaMaps DB user', $required = true, $withArg = true );
-		$this->addOption( 'maps-db-pass', 'WikiaMaps DB pass', $required = true, $withArg = true );
-		$this->addOption( 'maps-db-name', 'WikiaMaps DB name', $required = true, $withArg = true );
+		$this->addOption( 'dry-run', 'Dry run; make no changes', $required = false, $withArg = false );
 		$this->addOption(
 			'tiles-set-id',
 			'Specify a tiles\' set id which leads to a group of maps which <imap /> tags should get removed',
@@ -39,8 +32,8 @@ class RemoveImapTags extends Maintenance {
 		);
 	}
 
-	static public function isTest() {
-		return self::$test;
+	static public function isDryRun() {
+		return self::$dryRun;
 	}
 
 	static public function isNaturalNumber( $int ) {
@@ -49,10 +42,10 @@ class RemoveImapTags extends Maintenance {
 
 	public function getMapsIdsUsingTileset( $cityId, $tilesSetId ) {
 		$dbw = new DatabaseMysqli(
-			$this->getOption('maps-db-host'),
-			$this->getOption('maps-db-user'),
-			$this->getOption('maps-db-pass'),
-			$this->getOption('maps-db-name')
+			$this->slaveDBConfig['host'],
+			$this->slaveDBConfig['user'],
+			$this->slaveDBConfig['password'],
+			$this->slaveDBConfig['database']
 		);
 
 		return $dbw->selectFieldValues( 'map', 'id', [
@@ -117,7 +110,7 @@ class RemoveImapTags extends Maintenance {
 			$newContent = str_replace( $stringWithTagToRemove, "", $oldContent );
 
 			$this->output( sprintf( 'Trying to edit article #%d', $articleId ) . PHP_EOL );
-			if( !self::isTest() ) {
+			if( !self::isDryRun() ) {
 				$result = $article->doEdit(
 					$newContent,
 					wfMessage(
@@ -161,14 +154,15 @@ class RemoveImapTags extends Maintenance {
 	public function execute() {
 		$this->app = F::app();
 		$this->mapsClientConfig = $this->app->wg->IntMapConfig;
+		$this->slaveDBConfig = $this->app->wg->IntMapFullConfig['db']['prod']['slave'][0];
 		$this->maps = new WikiaMaps( $this->mapsClientConfig );
 
-		self::$test = $this->hasOption( 'test' );
+		self::$dryRun = $this->hasOption( 'dry-run' );
 
 		$cityId = $this->app->wg->CityId;
 		$tilesSetId = $this->getOption( 'tiles-set-id' );
 
-		if ( self::isTest() ) {
+		if ( self::isDryRun() ) {
 			$this->output( 'Mode: test run' . PHP_EOL );
 		} else {
 			$this->output( 'Mode: normal run' . PHP_EOL );
