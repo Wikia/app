1da35d89a433f58709b8878029930489ea205c89
Bogna Knychala <bogna.ka@gmail.com>
2016-09-12 17:49:29 +0200

    ADEN-3905 | log when wgAdDriverAppNexusBidderPlacementsConfig is not configured

--- extensions/wikia/AdEngine/js/lookup/prebid/adapters/appnexusPlacements.js
+++ extensions/wikia/AdEngine/js/lookup/prebid/adapters/appnexusPlacements.js
@@ -1,13 +1,23 @@
 define('ext.wikia.adEngine.lookup.prebid.adapters.appnexusPlacements', [
 	'ext.wikia.adEngine.utils.adLogicZoneParams',
-	'wikia.instantGlobals'
-], function (zoneParams, instantGlobals) {
+	'wikia.instantGlobals',
+	'wikia.log'
+], function (zoneParams, instantGlobals, log) {
 	'use strict';
 
-	var placementsMap = instantGlobals.wgAdDriverAppNexusBidderPlacementsConfig;
+	var placementsMap = instantGlobals.wgAdDriverAppNexusBidderPlacementsConfig,
+		logGroup = 'ext.wikia.adEngine.lookup.prebid.adapters.appnexusPlacements';
 
 	function getPlacement(skin) {
-		return placementsMap[skin][zoneParams.getVertical()];
+		if (placementsMap && placementsMap[skin] && placementsMap[skin][zoneParams.getVertical()]) {
+			return placementsMap[skin][zoneParams.getVertical()];
+		} else {
+			log([
+					'getPlacement', skin,
+					'wgAdDriverAppNexusBidderPlacementsConfig variable not set or not configured correctly'
+				], 'error', logGroup
+			);
+		}
 	}
 
 	return {
