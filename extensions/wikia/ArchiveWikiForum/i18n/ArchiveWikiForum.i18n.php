6d62ebc0d2e4db95879b52ac2ff593d38b287669
James Sutterfield <james@wikia-inc.com>
2016-09-13 12:56:09 +0200

    SOC-2815 move messages into new extension

---
+++ extensions/wikia/ArchiveWikiForum/i18n/ArchiveWikiForum.i18n.php
@@ -0,0 +1,136 @@
+<?php
+
+$messages = [];
+
+$messages['qqq'] = [
+    'forum-old-notification-message' => 'Notification displayed on old archived forum pages.',
+    'forum-old-notification-navigation-button' => 'Link to the new forums that appears at the top of old archived forum pages.'
+];
+
+$messages['en'] = [
+    'forum-old-notification-message' => 'This Forum has been archived',
+    'forum-old-notification-navigation-button' => 'Visit the new Forums'
+];
+
+$messages['ar'] = [
+    'forum-old-notification-message' => 'لقد تم أرشفة هذا المنتدى',
+    'forum-old-notification-navigation-button' => 'قم بزيارة المنتديات الجديدة'
+];
+
+$messages['br'] = [
+    'forum-old-notification-message' => 'Diellet eo bet ar forom-mañ',
+    'forum-old-notification-navigation-button' => 'Gweladenniñ ar foromoù nevez'
+];
+
+$messages['ca'] = [
+	'forum-old-notification-message' => 'Aquest Fòrum ha estat arxivat',
+	'forum-old-notification-navigation-button' => 'Visita el nou Fòrum',
+];
+
+$messages['cs'] = [
+	'forum-old-notification-navigation-button' => 'Navštivte nová Fóra',
+];
+
+$messages['de'] = [
+	'forum-old-notification-message' => 'Dieses Forum wurde archiviert',
+	'forum-old-notification-navigation-button' => 'Die neuen Foren besuchen'
+];
+
+$messages['es'] = [
+	'forum-old-notification-message' => 'Este foro ha sido archivado',
+	'forum-old-notification-navigation-button' => 'Visita el nuevo foro'
+];
+
+$messages['fi'] = [
+	'forum-old-notification-message' => 'Tämä Foorumi on arkistoitu',
+	'forum-old-notification-navigation-button' => 'Vieraile uusissa Foorumeissa'
+];
+
+$messages['fr'] = [
+	'forum-old-notification-message' => 'Ce forum a été archivé',
+	'forum-old-notification-navigation-button' => 'Visiter le nouveau forum'
+];
+
+$messages['gl'] = [
+	'forum-old-notification-message' => 'Este foro foi arquivado',
+	'forum-old-notification-navigation-button' => 'Visitar o novo foro'
+];
+
+$messages['ja'] = [
+	'forum-old-notification-message' => 'こちらは旧フォーラムのアーカイブです',
+	'forum-old-notification-navigation-button' => '新しいフォーラムにアクセスする'
+];
+
+$messages['ko'] = [
+	'forum-old-notification-message' => '이 포럼은 보관되었습니다',
+	'forum-old-notification-navigation-button' => '새로운 포럼을 방문해보세요'
+];
+
+$messages['lb'] = [
+	'forum-old-notification-message' => 'Dëse Forum gouf archivéiert'
+];
+
+$messages['ms'] = [
+	'forum-old-notification-message' => 'Forum ini telah diarkibkan',
+	'forum-old-notification-navigation-button' => 'Lawati Forum baru'
+];
+
+$messages['nl'] = [
+	'forum-old-notification-message' => 'Dit forum is gearchiveerd',
+	'forum-old-notification-navigation-button' => 'Bezoek de nieuwe fora'
+];
+
+$messages['pl'] = [
+	'forum-old-notification-message' => 'To forum zostało zarchiwizowane',
+	'forum-old-notification-navigation-button' => 'Odwiedź nowe forum'
+];
+
+$messages['pt'] = [
+	'forum-old-notification-message' => 'Este fórum foi arquivado',
+	'forum-old-notification-navigation-button' => 'Visite os novos Fóruns'
+];
+
+$messages['pt-br'] = [
+	'forum-old-notification-message' => 'Este fórum foi arquivado',
+	'forum-old-notification-navigation-button' => 'Visite os novos Fóruns'
+];
+
+$messages['ru'] = [
+	'forum-old-notification-message' => 'Этот форум был заархивирован.',
+	'forum-old-notification-navigation-button' => 'Посетите новые форумы'
+];
+
+$messages['sv'] = [
+	'forum-old-notification-message' => 'Detta forum har arkiverats',
+	'forum-old-notification-navigation-button' => 'Besök de nya forumen'
+];
+
+$messages['te'] = [
+	'forum-old-notification-message' => 'ఈ చర్చాస్థలం భద్రపరచబడింది',
+	'forum-old-notification-navigation-button' => 'కొత్త వేదికలను చూడండి'
+];
+
+$messages['uk'] = [
+	'forum-old-notification-message' => 'Цей форум вже заархівований',
+	'forum-old-notification-navigation-button' => 'Відвідайте нові форуми'
+];
+
+$messages['vi'] = [
+	'forum-old-notification-message' => 'Diễn đàn này đã được lưu trữ',
+	'forum-old-notification-navigation-button' => 'Truy cập vào các diễn đàn mới'
+];
+
+$messages['zh'] = [
+	'forum-old-notification-message' => '这个论坛已经被存档',
+	'forum-old-notification-navigation-button' => '访问新的论坛'
+];
+
+$messages['zh-hans'] = [
+	'forum-old-notification-message' => '本论坛已存档',
+	'forum-old-notification-navigation-button' => '访问新论坛'
+];
+
+$messages['zh-hant'] = [
+	'forum-old-notification-message' => '本論壇已存檔',
+	'forum-old-notification-navigation-button' => '訪問新論壇'
+];
