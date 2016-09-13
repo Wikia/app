7c290f9b5d20b088cb550bb2ca37a8d53a2d47cb
mleczey <mleczey@gmail.com>
2016-09-13 12:44:13 +0200

    SOC-2898 | feat: rebranding for email confirmation emails

--- extensions/wikia/Email/Controller/EmailConfirmationController.class.php
+++ extensions/wikia/Email/Controller/EmailConfirmationController.class.php
@@ -5,7 +5,9 @@ namespace Email\Controller;
 use Email\EmailController;
 use Email\Check;
 
-abstract class AbstractEmailConfirmationController extends EmailController {
+class EmailConfirmationController extends EmailController {
+
+	const TYPE = "ConfirmationMail";
 
 	const LAYOUT_CSS = EmailController::NO_ADDITIONAL_STYLES;
 
@@ -35,6 +37,10 @@ abstract class AbstractEmailConfirmationController extends EmailController {
 		$this->assertUserHasEmail();
 	}
 
+	protected function getSubject() {
+		return $this->getMessage( 'emailext-emailconfirmation-subject' )->text();
+	}
+
 	/**
 	 * @template emailConfirmation
 	 */
@@ -44,17 +50,34 @@ abstract class AbstractEmailConfirmationController extends EmailController {
 			'summary' => $this->getSummary(),
 			'buttonLink' => $this->confirmUrl,
 			'buttonText' => $this->getButtonText(),
-			'contentFooterMessages' => $this->getContentFooterMessages()
+			'contentFooterMessageTop' => $this->getContentFooterMessageTop(),
+			'contentFooterList' => $this->createContentFooterList(),
+			'contentFooterMessagesBottom' => $this->getContentFooterMessagesBottom()
 		] );
 	}
 
-	abstract protected function getSummary();
+	private function getSummary() {
+		return $this->getMessage( 'emailext-emailconfirmation-summary' )->text();
+	}
 
-	protected function getButtonText() {
+	private function getButtonText() {
 		return $this->getMessage( 'emailext-emailconfirmation-button-text' )->text();
 	}
 
-	protected function getContentFooterMessages() {
+	private function getContentFooterMessageTop() {
+		return $this->getMessage( 'emailext-emailconfirmation-footer-1' )->text();
+	}
+
+	private function createContentFooterList() {
+		return [
+			$this->getMessage( 'emailext-emailconfirmation-footer-list-1' )->text(),
+			$this->getMessage( 'emailext-emailconfirmation-footer-list-2' )->text(),
+			$this->getMessage( 'emailext-emailconfirmation-footer-list-3' )->text(),
+			$this->getMessage( 'emailext-emailconfirmation-footer-list-4' )->text(),
+		];
+	}
+
+	private function getContentFooterMessagesBottom() {
 		$commonFooterMessage = $this->getCommonFooterMessages();
 		$emailSpecificFooterMessages = $this->getEmailSpecificFooterMessages();
 		return array_merge( $emailSpecificFooterMessages, $commonFooterMessage );
@@ -66,129 +89,24 @@ abstract class AbstractEmailConfirmationController extends EmailController {
 		];
 	}
 
-	abstract protected function getEmailSpecificFooterMessages();
-
-	protected static function getEmailSpecificFormFields() {
-			$form = [
-				'inputs' => [
-					[
-						'type' => 'hidden',
-						'name' => 'confirmUrl',
-						'value' => "#",
-					],
-				]
-			];
-
-			return $form;
-
-	}
-}
-
-class EmailConfirmationController extends AbstractEmailConfirmationController {
-	const TYPE = "ConfirmationMail";
-
-	protected function getSubject() {
-		return $this->getMessage( 'emailext-emailconfirmation-subject' )->text();
-	}
-
-	protected function getSummary() {
-		return $this->getMessage( 'emailext-emailconfirmation-summary' )->text();
-	}
-
 	protected function getEmailSpecificFooterMessages() {
 		return [
-			$this->getMessage( 'emailext-emailconfirmation-footer-1' )->text(),
 			$this->getMessage( 'emailext-emailconfirmation-footer-2' )->text()
 		];
-
-	}
-}
-
-class EmailConfirmationReminderController extends AbstractEmailConfirmationController {
-
-	protected function getSubject() {
-		return $this->getMessage( 'emailext-emailconfirmation-reminder-subject', $this->getTargetUserName() )->parse();
-	}
-
-	protected function getSummary() {
-		return $this->getMessage( 'emailext-emailconfirmation-reminder-summary' )->text();
-	}
-
-	protected function getEmailSpecificFooterMessages() {
-		return [
-			$this->getMessage( 'emailext-emailconfirmation-reminder-footer-1',
-				$this->getTargetUserName() )->parse()
-		];
-	}
-}
-
-class ConfirmationChangedEmailController extends AbstractEmailConfirmationController {
-
-	private $newEmail;
-
-	public function initEmail() {
-		parent::initEmail();
-
-		$this->newEmail = $this->request->getVal( 'newEmail' );
-		$this->assertValidChangedParams();
-	}
-
-	protected function assertValidChangedParams() {
-		if ( empty( $this->newEmail ) ) {
-			throw new Check( "A value must be passed for parameter 'newEmail'" );
-		}
-	}
-
-	protected function getTargetUserEmail() {
-		return $this->newEmail;
-	}
-
-	protected function getSubject() {
-		return $this->getMessage( 'emailext-emailconfirmation-changed-subject' )->text();
-	}
-
-	protected  function getSummary() {
-		return $this->getMessage( 'emailext-emailconfirmation-changed-summary' )->text();
-	}
-
-	protected function getEmailSpecificFooterMessages() {
-		return [
-			$this->getMessage( 'emailext-emailconfirmation-changed-footer-1' )->text(),
-			$this->getMessage( 'emailext-emailconfirmation-changed-footer-2' )->text(),
-		];
 	}
 
 	protected static function getEmailSpecificFormFields() {
-		$parentForm = parent::getEmailSpecificFormFields();
-
-		$parentForm['inputs'][] = [
-			'type' => 'text',
-			'name' => 'newEmail',
-			'label' => "New Email",
-			'tooltip' => "The user's new email",
+		$form = [
+			'inputs' => [
+				[
+					'type' => 'hidden',
+					'name' => 'confirmUrl',
+					'value' => "#",
+				],
+			]
 		];
 
-		return $parentForm;
-	}
-}
+		return $form;
 
-class ReactivateAccountController extends AbstractEmailConfirmationController {
-
-	protected function getSubject() {
-		return $this->getMessage( 'emailext-reactivate-account-subject' )->text();
-	}
-
-	protected function getSummary() {
-		return $this->getMessage( 'emailext-reactivate-account-summary' )->text();
-	}
-
-	protected function getButtonText() {
-		return $this->getMessage( 'emailext-reactivate-account-button-text' )->text();
-	}
-
-	protected function getEmailSpecificFooterMessages() {
-		return [
-			$this->getMessage( 'emailext-reactivate-account-welcome-back' )->text(),
-		];
 	}
 }
