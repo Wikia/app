c633881fcee43d04c92477838a155c0e36f72746
James Sutterfield <james@wikia-inc.com>
2016-09-13 12:28:07 +0200

    SOC-2815 remove unused file

--- extensions/wikia/Forum/templates/Forum_oldForumInfo.php
+++
@@ -1,4 +0,0 @@
-<section class="archived-forum-notification">
-	<h1><?= wfMessage( 'forum-old-notification-message' )->escaped() ?></h1>
-	<a href="<?= $forumUrl ?>" class="button secondary"><?= wfMessage( 'forum-old-notification-navigation-button' )->escaped() ?></a>
-</section>
