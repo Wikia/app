e82c9feecaa57c1e236e84d0f27ed3bb5ea60496
Warkot <mateusz.warkocki@wikia-inc.com>
2016-09-15 13:19:23 +0200

    XW-2018 | close search suggestions on search deactivation

--- extensions/wikia/DesignSystem/scripts/DesignSystemGlobalNavigationSearch.js
+++ extensions/wikia/DesignSystem/scripts/DesignSystemGlobalNavigationSearch.js
@@ -4,8 +4,10 @@ $(function ($) {
 	var $globalNav = $('.wds-global-navigation'),
 		$searchInput = $globalNav.find('.wds-global-navigation__search-input'),
 		$searchSubmit = $globalNav.find('.wds-global-navigation__search-submit'),
+		$searchInputWrapper = $globalNav.find('.wds-global-navigation__search-input-wrapper'),
 		placeholderText = $searchInput.attr('placeholder'),
-		activeSearchClass = 'wds-search-is-active';
+		activeSearchClass = 'wds-search-is-active',
+		activeSearchSuggestionsCLass = 'wds-is-active';
 
 	function activateSearch() {
 		if (!$globalNav.hasClass(activeSearchClass)) {
@@ -28,6 +30,7 @@ $(function ($) {
 
 	$globalNav.find('.wds-global-navigation__search-close').on('click', function () {
 		$globalNav.removeClass(activeSearchClass);
+		$searchInputWrapper.removeClass(activeSearchSuggestionsCLass);
 		$searchInput.attr('placeholder', placeholderText);
 		$searchSubmit.prop('disabled', true);
 	});
