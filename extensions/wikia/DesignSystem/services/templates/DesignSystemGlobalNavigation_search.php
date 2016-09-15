3d062395032208f8a1749a7880bc88c04ab2740d
Warkot <mateusz.warkocki@wikia-inc.com>
2016-09-15 17:01:16 +0200

    XW-2018 | add autocomplete="off" to search input

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_search.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_search.php
@@ -13,7 +13,9 @@
 					data-suggestions-param-name="<?= $model['module']['suggestions']['param-name'] ?>"
 				<?php endif; ?>
 				data-active-placeholder="<?= DesignSystemHelper::renderText( $model['module']['placeholder-active'] ); ?>"
-				placeholder="<?= DesignSystemHelper::renderText( $model['module']['placeholder-inactive'] ); ?>"/>
+				placeholder="<?= DesignSystemHelper::renderText( $model['module']['placeholder-inactive'] ); ?>"
+				autocomplete="off"
+			/>
 		</label>
 		<button class="wds-button wds-is-text wds-global-navigation__search-close" type="reset" data-tracking-label="global-navigation-search-close">
 			<?= DesignSystemHelper::getSvg(
