4f0991390d343bd78e31ecb0f8dded6d166563ab
Igor Rogatty <igor@wikia-inc.com>
2016-09-17 11:55:29 +0200

    XW | improve fandom stories markup

--- extensions/wikia/Search/templates/WikiaSearch_fandomStories.php
+++ extensions/wikia/Search/templates/WikiaSearch_fandomStories.php
@@ -1,7 +1,15 @@
-<?php foreach ( $stories as $story ) : ?>
-	<?= $story['title'] ?>
-	<?= $story['excerpt'] ?>
-	<?= $story['vertical'] ?>
-	<?= $story['image'] ?>
-	<?= $story['url'] ?>
-<?php endforeach;
+<?php if ( !empty( $stories ) ) : ?>
+	<div class="fandom-stories RailModule">
+	<h2>Related Fandom Stories</h2>
+	<?php foreach ( $stories as $story ) : ?>
+		<div class="fandom-story">
+			<a href="<?= $story['url'] ?>">
+				<h3><?= $story['title'] ?></h3>
+			</a>
+			<?= $story['excerpt'] ?>
+			<div>Vertical: <?= $story['vertical'] ?></div>
+			<img src="<?= $story['image'] ?>">
+		</div>
+	<?php endforeach; ?>
+	</div>
+<?php endif; ?>
