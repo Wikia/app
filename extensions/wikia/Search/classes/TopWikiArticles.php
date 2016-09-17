8d57147bdc6cd164317d18d7fe42a4af241ee266
Damian Jozwiak <damian@wikia-inc.com>
2016-09-17 14:18:06 +0200

    XW | Fix image size for TopArticles

--- extensions/wikia/Search/classes/TopWikiArticles.php
+++ extensions/wikia/Search/classes/TopWikiArticles.php
@@ -13,18 +13,6 @@ class TopWikiArticles {
 	 */
 	const CACHE_TTL = 86400 * 5;
 
-	/**
-	 * Dimensions for hot article image in Top Wiki Articles module
-	 */
-	const HOT_ARTICLE_IMAGE_WIDTH = 300;
-	const HOT_ARTICLE_IMAGE_HEIGHT = 150;
-
-	/**
-	 * Dimensions for hot article image in Top Wiki Articles module in fluid layout
-	 */
-	const HOT_ARTICLE_IMAGE_WIDTH_FLUID = 270;
-	const HOT_ARTICLE_IMAGE_HEIGHT_FLUID = 135;
-
 	static public function getArticlesWithCache( $cityId, $isGridLayoutEnabled ) {
 		$cacheKey = wfMemcKey(
 			__CLASS__,
@@ -63,22 +51,12 @@ class TopWikiArticles {
 			}
 
 			if ( !empty( $ids ) ) {
-				$params = [ 'ids' => implode( ',', $ids ), 'height' => 50, 'width' => 50, 'abstract' => 120 ];
+				$params = [ 'ids' => implode( ',', $ids ), 'height' => 50, 'width' => 90, 'abstract' => 120 ];
 				$detailResponse = $app->sendRequest( 'ArticlesApiController', 'getDetails', $params )->getData();
-				$dimensions = static::getHotArticleImageDimensions();
 
 				foreach ( $detailResponse['items'] as $id => $item ) {
 					if ( !empty( $item['thumbnail'] ) ) {
 						$item['thumbnailSize'] = "small";
-						//get the first one image from imageServing as it needs other size
-						if ( empty( $pages ) ) {
-							$is = new \ImageServing( [ $id ], $dimensions['width'], $dimensions['height'] );
-							$result = $is->getImages( 1 );
-							if ( !empty( $result[$id][0]['url'] ) ) {
-								$item['thumbnail'] = $result[$id][0]['url'];
-								$item['thumbnailSize'] = "large";
-							}
-						}
 						//render date
 						$item['date'] = $wgLang->date( $item['revision']['timestamp'] );
 						$item = \WikiaSearchController::processArticleItem( $item, 75 );
@@ -91,21 +69,5 @@ class TopWikiArticles {
 		}
 
 		return $pages;
-	}
-
-	private static function getHotArticleImageDimensions() {
-		if ( \BodyController::isGridLayoutEnabled() ) {
-			$dimensions = [
-				'width' => self::HOT_ARTICLE_IMAGE_WIDTH,
-				'height' => self::HOT_ARTICLE_IMAGE_HEIGHT
-			];
-		} else {
-			$dimensions = [
-				'width' => self::HOT_ARTICLE_IMAGE_WIDTH_FLUID,
-				'height' => self::HOT_ARTICLE_IMAGE_HEIGHT_FLUID
-			];
-		}
-
-		return $dimensions;
 	}
 }
