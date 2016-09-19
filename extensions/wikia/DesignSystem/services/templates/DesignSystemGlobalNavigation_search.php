fc3f5327522ef26565803c7ef4007a0d965a9c54
Warkot <mateusz.warkocki@wikia-inc.com>
2016-09-19 12:20:00 +0000

    XW-2003 | update templates to use updated icons

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_search.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_search.php
@@ -2,7 +2,7 @@
 	<div id="searchInputWrapper" class="wds-dropdown wds-global-navigation__search-input-wrapper">
 		<label class="wds-global-navigation__search-label">
 			<?= DesignSystemHelper::getSvg(
-				'wds-icons-magnifying-glass',
+				'wds-icons-magnifying-glass-small',
 				'wds-icon wds-icon-small wds-global-navigation__search-label-icon'
 			) ?>
 			<input id="searchInput"
@@ -19,14 +19,14 @@
 		</label>
 		<button class="wds-button wds-is-text wds-global-navigation__search-close" type="reset" data-tracking-label="global-navigation-search-close">
 			<?= DesignSystemHelper::getSvg(
-				'wds-icons-cross',
+				'wds-icons-cross-small',
 				'wds-icon wds-icon-small wds-global-navigation__search-close-icon',
 				wfMessage( 'global-navigation-search-cancel' )->escaped()
 			) ?>
 		</button>
 		<button class="wds-button wds-global-navigation__search-submit" type="submit" data-tracking-label="global-navigation-search-submit">
 			<?= DesignSystemHelper::getSvg(
-				'wds-icons-arrow',
+				'wds-icons-arrow-small',
 				'wds-icon wds-icon-small wds-global-navigation__search-submit-icon'
 			) ?>
 		</button>
