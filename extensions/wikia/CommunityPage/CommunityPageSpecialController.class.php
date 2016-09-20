492e02bd6a88ad069dc43a458cf7f4b631b75afd
Sebastian Marzjan <sebastianm@wikia-inc.com>
2016-09-20 14:29:03 -0700

    WIP: make the discussions widget styled according to the designs

--- extensions/wikia/CommunityPage/CommunityPageSpecialController.class.php
+++ extensions/wikia/CommunityPage/CommunityPageSpecialController.class.php
@@ -56,7 +56,11 @@ class CommunityPageSpecialController extends WikiaSpecialPageController {
 			'defaultModules' => $this->getDefaultModules( $defaultModulesLimit ),
 			'helpModule' => $this->getHelpModuleData(),
 			'communityTodoListModule' => $this->getCommunityTodoListData(),
-			'discussionsModule' => !empty( $wgEnableDiscussions ) ? EmbeddableDiscussionsController::render( null, [] ) : '',
+			'discussionsModule' => !empty( $wgEnableDiscussions ) ? EmbeddableDiscussionsController::render( null, [
+				'columns' => 2,
+				'size' => 6,
+				'avatarSize' => AvatarService::AVATAR_SIZE_SMALL_PLUS
+			] ) : '',
 			'contributorsModuleEnabled' => !$this->wg->CommunityPageDisableTopContributors,
 			'inspectlet' => ( new InspectletService( InspectletService::COMMUNITY_PAGE ) )->getInspectletCode()
 		] );
