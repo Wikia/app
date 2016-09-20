0d1b2afdfba73970169c3d3fbdfe36bf32daab62
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-09-20 14:00:40 -0700

    php doc

--- extensions/wikia/CommunityPage/helpers/LinkHelper.php
+++ extensions/wikia/CommunityPage/helpers/LinkHelper.php
@@ -4,6 +4,15 @@ class LinkHelper {
 	static $WITH_EDIT_MODE = true;
 	static $WITHOUT_EDIT_MODE = false;
 
+	/**
+	 * Returns url to Special:SignUp with redirect to given article if current wiki has disabled anon edits and current
+	 * user is anon. Otherwise link to given article is returned
+	 *
+	 * @param Title $title
+	 * @param $editMode - if $WITH_EDIT_MODE then action=edit or veaction=edit is added to link (depending on which
+	 * editor is primary).
+	 * @return String - the url
+	 */
 	public static function forceLoginLink( Title $title, $editMode ) {
 		global $wgUser, $wgDisableAnonymousEditing;
 
