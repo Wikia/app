e69eb8a3b825c1e474609bd0d7bfd3dcf6c756ae
Dmytro Rets <dmitryrets@gmail.com>
2016-09-22 17:57:35 +0200

    ADEN-3594 Simplify test

--- extensions/wikia/AdEngine/js/spec/slot/adUnitBuilder.spec.js
+++ extensions/wikia/AdEngine/js/spec/slot/adUnitBuilder.spec.js
@@ -2,35 +2,22 @@ describe('ext.wikia.adEngine.slot.adUnitBuilder', function () {
 	'use strict';
 
 	var mocks = {
-			s0: undefined,
-			s1: undefined,
-			s2: undefined,
 			page: {
 				getPageLevelParams: function() {
 					return {
-						s0: mocks.s0,
-						s1: mocks.s1,
-						s2: mocks.s2
+						s0: 'life',
+						s1: '_project43',
+						s2: 'article'
 					};
 				}
 			}
 	};
-
-	beforeEach(function () {
-		mocks.s0 = undefined;
-		mocks.s1 = undefined;
-		mocks.s2 = undefined;
-	});
 
 	function getModule() {
 		return modules['ext.wikia.adEngine.slot.adUnitBuilder'](mocks.page);
 	}
 
 	it('Build ad unit', function () {
-		mocks.s0 = 'life';
-		mocks.s1 = '_project43';
-		mocks.s2 = 'article';
-
 		var vastUrl = getModule().build('TOP_LEADERBOARD', 'playwire');
 
 		expect(vastUrl).toEqual('/5441/wka.life/_project43//article/playwire/TOP_LEADERBOARD');
