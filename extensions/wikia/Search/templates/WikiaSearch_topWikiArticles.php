e362caafb26c216e47caa4e7c25f88d0f90fa06c
Damian Jozwiak <damian@wikia-inc.com>
2016-09-23 14:04:40 +0200

    XW-2101 | Search topArticles - fix XSSes

--- extensions/wikia/Search/templates/WikiaSearch_topWikiArticles.php
+++ extensions/wikia/Search/templates/WikiaSearch_topWikiArticles.php
@@ -7,14 +7,14 @@
 		<div class="side-article result">
 			<div class="side-article-thumbnail">
 				<? if ( isset( $page['thumbnail'] ) ) : ?>
-				<a href="<?=$page['url']?>" class="top-wiki-article-image" data-pos="<?= $counter ?>">
-					<img src="<?= $page['thumbnail'] ?>" />
+				<a href="<?= Sanitizer::encodeAttribute( $page['url'] ) ?>" class="top-wiki-article-image" data-pos="<?= $counter ?>">
+					<img src="<?= Sanitizer::encodeAttribute( $page['thumbnail'] ) ?>" />
 				</a>
 				<? endif; ?>
 			</div>
 			<div class="side-article-text">
-				<b><a href="<?= $page['url'] ?>" class="top-wiki-article-link" data-pos="<?= $counter ?>"><?= $page['title'] ?></a></b><!-- comment to remove whitespace
-				--><span class="side-article-text-synopsis subtle"><?= $page['abstract'] ?></span>
+				<b><a href="<?= Sanitizer::encodeAttribute( $page['url'] ) ?>" class="top-wiki-article-link" data-pos="<?= $counter ?>"><?= htmlspecialchars( $page['title'] ) ?></a></b><!-- comment to remove whitespace
+				--><span class="side-article-text-synopsis subtle"><?= htmlspecialchars( $page['abstract'] ) ?></span>
 			</div>
 		</div>
 
