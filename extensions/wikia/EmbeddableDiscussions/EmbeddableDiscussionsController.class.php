7cb289fbdc135dbc6346edeea4376e8f8eaa7faf
dianafa <diana.falkowska@gmail.com>
2016-09-23 17:05:03 -0700

    draft idea

--- extensions/wikia/EmbeddableDiscussions/EmbeddableDiscussionsController.class.php
+++ extensions/wikia/EmbeddableDiscussions/EmbeddableDiscussionsController.class.php
@@ -42,26 +42,29 @@ class EmbeddableDiscussionsController {
 		return true;
 	}
 
-	/**
-	 * Checks arguments for errors.
-	 * @param $modelData
-	 * @param array $args
-	 * @internal param $errorMessage /Return parameter with the proper error message to show.
-	 * Disregard if return is false.
-	 * @return true if ok, false if error
-	 */
-	private function checkArguments( $modelData, array $args, &$errorMessage ) {
-		// PARAM_MOSTRECENT must be bool
-		if ( isset( $args[ static::PARAM_MOSTRECENT ] ) &&
-			$args[ static::PARAM_MOSTRECENT ] !== 'true' &&
-			$args[ static::PARAM_MOSTRECENT ] !== 'false'
-		) {
-			$errorMessage = wfMessage( 'embeddable-discussions-parameter-error',
-				static::PARAM_MOSTRECENT,
-				wfMessage( 'embeddable-discussions-parameter-error-boolean' )->plain()
-			)->plain();
+	private function processArguments( array $args ) {
+		$parameters = [
+			static::PARAM_MOSTRECENT => false,
+			static::PARAM_SIZE => self::ITEMS_DEFAULT,
+			static::PARAM_COLUMNS => self::COLUMNS_DEFAULT,
+			static::PARAM_CATEGORY => '',
+		];
 
-			return false;
+		// PARAM_MOSTRECENT must be bool
+		if ( isset( $args[ static::PARAM_MOSTRECENT ] ) ) {
+			if ( is_bool ( $args[ static::PARAM_MOSTRECENT ] ) ||
+				$args[ static::PARAM_MOSTRECENT ] === 'true' ||
+				$args[ static::PARAM_MOSTRECENT ] === 'false'
+			) {
+				$parameters[ static::PARAM_MOSTRECENT ] = $args[ static::PARAM_MOSTRECENT ];
+			} else {
+				$errorMessage = wfMessage( 'embeddable-discussions-parameter-error',
+					static::PARAM_MOSTRECENT,
+					wfMessage( 'embeddable-discussions-parameter-error-boolean' )->plain()
+				)->plain();
+
+				$this->renderError( $errorMessage );
+			}
 		}
 
 		// size must be integer in range
@@ -69,16 +72,18 @@ class EmbeddableDiscussionsController {
 			$sizeArg = $args[ static::PARAM_SIZE ];
 			$size = ctype_digit( $sizeArg ) ? intval( $sizeArg ) : $sizeArg;
 
-			if ( !is_int ( $size ) ||
-				$size > self::ITEMS_MAX ||
-				$size < self::ITEMS_MIN
-			) {
+			if ( is_int ( $size ) &&
+				$size > self::ITEMS_MIN &&
+				$size <= self::ITEMS_MAX) {
+
+				$parameters[ static::PARAM_SIZE ] = $size;
+			} else {
 				$errorMessage = wfMessage( 'embeddable-discussions-parameter-error',
 					static::PARAM_SIZE,
 					wfMessage( 'embeddable-discussions-parameter-error-range', self::ITEMS_MIN, self::ITEMS_MAX )->plain()
 				)->plain();
 
-				return false;
+				$this->renderError( $errorMessage );
 			}
 		}
 
@@ -87,19 +92,31 @@ class EmbeddableDiscussionsController {
 			$columnsArg = $args[ static::PARAM_COLUMNS ];
 			$columns = ctype_digit( $columnsArg ) ? intval( $columnsArg ) : $columnsArg;
 
-			if ( !is_int( $columns ) ||
-				$columns > self::COLUMNS_MAX ||
-				$columns < self::COLUMNS_MIN
+			if ( is_int( $columns ) &&
+				$columns > self::COLUMNS_MIN &&
+				$columns <= self::COLUMNS_MAX
 			) {
+				$parameters[ static::PARAM_COLUMNS ] = $columns;
+			} else {
 				$errorMessage = wfMessage( 'embeddable-discussions-parameter-error',
 					static::PARAM_COLUMNS,
 					wfMessage( 'embeddable-discussions-parameter-error-range', self::COLUMNS_MIN, self::COLUMNS_MAX )->plain()
 				)->plain();
 
-				return false;
+				$this->renderError( $errorMessage );
 			}
 		}
 
+		if ( isset( $args[ static::PARAM_CATEGORY ] ) ) {
+				$parameters[ static::PARAM_CATEGORY ] = $args[ static::PARAM_CATEGORY ];
+		}
+
+
+
+		return $parameters;
+	}
+
+	private function checkCategory( $modelData, array $args ) {
 		// category must be a valid category
 		if ( !empty( $modelData[ 'invalidCategory' ] ) ) {
 			$errorMessage = wfMessage( 'embeddable-discussions-parameter-error',
@@ -107,7 +124,7 @@ class EmbeddableDiscussionsController {
 				wfMessage( 'embeddable-discussions-parameter-error-category' )->plain()
 			)->plain();
 
-			return false;
+			return $this->renderError( $errorMessage );
 		}
 
 		return true;
@@ -120,20 +137,15 @@ class EmbeddableDiscussionsController {
 	public function render( array $args ) {
 		global $wgCityId;
 
-		$showLatest = !empty( $args[ static::PARAM_MOSTRECENT ] ) && filter_var( $args[ static::PARAM_MOSTRECENT ], FILTER_VALIDATE_BOOLEAN );
-		$itemCount = !empty( $args[ static::PARAM_SIZE ] ) ? intval( $args[ static::PARAM_SIZE ] ) : self::ITEMS_DEFAULT;
-		$columns = !empty( $args[ static::PARAM_COLUMNS ] ) ? intval( $args[ static::PARAM_COLUMNS ] ) : self::COLUMNS_DEFAULT;
-		$category = !empty( $args[ static::PARAM_CATEGORY ] ) ? $args[ static::PARAM_CATEGORY ] : '';
+		$params = $this->processArguments( $args );
+		$modelData = ( new DiscussionsThreadModel( $wgCityId ) )
+			->getData( $params[ static::PARAM_MOSTRECENT ], $params[ static::PARAM_SIZE ], $params[ static::PARAM_CATEGORY ] );
 
-		$modelData = ( new DiscussionsThreadModel( $wgCityId ) )->getData( $showLatest, $itemCount, $category );
-
-		if ( !$this->checkArguments( $modelData, $args, $errorMessage ) ) {
-			return $this->renderError( $errorMessage );
-		}
+		$this->checkCategory( $args, $modelData );
 
 		return F::app()->checkSkin( 'wikiamobile' ) ?
-			$this->renderMobile( $modelData, $showLatest, $itemCount ) :
-			$this->renderDesktop( $modelData, $showLatest, $category, $columns );
+			$this->renderMobile( $modelData, $params[ static::PARAM_MOSTRECENT ], $params[ static::PARAM_SIZE ] ) :
+			$this->renderDesktop( $modelData, $params[ static::PARAM_MOSTRECENT ], $params[ static::PARAM_CATEGORY ], $params[ static::PARAM_COLUMNS ] );
 	}
 
 	/**
