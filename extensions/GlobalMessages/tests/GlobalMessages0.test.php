ecbad99a77b106334dcfd74f1ca019056d195319
Michał “Mix” Roszka <michal@roszka.pl>
2016-09-26 20:42:49 +0000

    refine the test

--- extensions/GlobalMessages/tests/GlobalMessages0.test.php
+++ extensions/GlobalMessages/tests/GlobalMessages0.test.php
@@ -13,8 +13,6 @@ class GlobalMessages0Test extends Maintenance {
 
 		$this->dataProvider = require '/usr/wikia/source/wiki/extensions/GlobalMessages/tests/GlobalMessages0.data.php';
 
-//		$this->docProvider = require '/usr/wikia/source/wiki/extensions/GlobalMessages/tests/qqq.raw.php';
-
 		global $wgLang;
 		$wgLang = Language::factory( 'en' );
 
@@ -22,33 +20,14 @@ class GlobalMessages0Test extends Maintenance {
 
 		foreach ( $this->dataProvider as $k => $v ) {
 
-/*			if ( isset($this->docProvider[$k]) ) {
-				$doc = $this->docProvider[$k];
-			} else {
-				$doc = 'Missing documentation';
-			}
-
-			$docOut[$k] = $doc;
-*/
 			$msg = wfMessage( $k )->text();
 
 			if ( $msg !== $v ) {
-
-/*				$title = Title::newFromText( ucfirst( $k ), NS_MEDIAWIKI );
-				if ( $title->exists() ) {
-					echo 'MW: ' . $title->getFullURL() . "\n";
-					$article = new Article( $title );
-					$article->doDelete( 'Message redefined in the source code');
-				} else {
-					echo "!=: $k\n";
-				}
-*/
 				echo "$k\n\nACTUAL:\n\n$msg\n\nEXPECTED:\n\n$v\n\n";
 				break;
 			}
 		}
 
-//		var_export( $docOut );
 		$this->output("OK :-)\n");
 	}
 
