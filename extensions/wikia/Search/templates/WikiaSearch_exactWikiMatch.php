0e37efeccb248921a0514d33874cb6073088f086
Damian Jozwiak <damian@wikia-inc.com>
2016-09-26 11:55:36 +0200

    XW-2101 | Escape WikiaSearch_exactWikiMatch template

--- extensions/wikia/Search/templates/WikiaSearch_exactWikiMatch.php
+++ extensions/wikia/Search/templates/WikiaSearch_exactWikiMatch.php
@@ -1,21 +1,21 @@
 <div class="exact-wiki-match">
 	<h1 class="exact-wiki-match__header"><?= wfMessage('wikiasearch3-related-wiki')->escaped(); ?></h1>
 	<div class="exact-wiki-match__result">
-		<a href="<?= $url ?>" title="<?= $title ?>" data-event="image">
-			<img src="<?= $imageURL ?>" alt="<?= $title ?>" class="exact-wiki-match__image" />
+		<a href="<?= Sanitizer::encodeAttribute( $url ) ?>" title="<?= Sanitizer::encodeAttribute( $title ) ?>" data-event="image">
+			<img src="<?= Sanitizer::encodeAttribute( $imageURL ) ?>" alt="<?= Sanitizer::encodeAttribute( $title ) ?>" class="exact-wiki-match__image" />
 		</a>
 		<div class="exact-wiki-match__content">
 			<h1 class="exact-wiki-match__wiki-header">
-				<a href="<?= $url ?>" data-event="title"><?= $title ?></a>
+				<a href="<?= Sanitizer::encodeAttribute( $url ) ?>" data-event="title"><?= Sanitizer::encodeAttribute( $title ) ?></a>
 			</h1>
-			<p class="exact-wiki-match__hub subtle"><?= $hub ?></p>
+			<p class="exact-wiki-match__hub subtle"><?= htmlspecialchars( $hub ) ?></p>
 			<ul class="exact-wiki-match__statistics">
 				<li><?= wfMessage( 'wikiasearch2-pages' )->rawParams( HTML::element( 'div', [], $wg->Lang->shortenNumberDecorator( $pagesCount) ) )->params( $pagesCount )->escaped(); ?></li>
 				<li><?= wfMessage( 'wikiasearch2-images' )->rawParams( HTML::element( 'div', [], $wg->Lang->shortenNumberDecorator( $imagesCount) ) )->params( $imagesCount )->escaped(); ?></li>
 				<li><?= wfMessage( 'wikiasearch2-videos' )->rawParams( HTML::element( 'div', [], $wg->Lang->shortenNumberDecorator( $videosCount) ) )->params( $videosCount )->escaped(); ?></li>
 			</ul>
-			<p class="exact-wiki-match__wiki-description"><?= $description; ?></p>
+			<p class="exact-wiki-match__wiki-description"><?= htmlspecialchars( $description ); ?></p>
 		</div>
 	</div>
-	<a href="<?= $viewMoreWikisLink ?>" data-event="view-more"><?= wfMessage('wikiasearch3-view-more-wikis')->escaped(); ?> <?= DesignSystemHelper::getSvg( 'wds-icons-arrow', 'wds-icon wds-icon-small exact-wiki-match__arrow' ); ?></a>
+	<a href="<?= Sanitizer::encodeAttribute( $viewMoreWikisLink ) ?>" data-event="view-more"><?= wfMessage('wikiasearch3-view-more-wikis')->escaped(); ?> <?= DesignSystemHelper::getSvg( 'wds-icons-arrow', 'wds-icon wds-icon-small exact-wiki-match__arrow' ); ?></a>
 </div>
