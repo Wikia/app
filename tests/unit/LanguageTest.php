96bd4d3642912ce1bfe4dfa24102285d24cda49d
Damian Jozwiak <damian@wikia-inc.com>
2016-09-26 10:46:31 +0200

    XW-2101 | Fix unit tests for Lang::ShortenNumberDecorator

---
+++ tests/unit/LanguageTest.php
@@ -0,0 +1,30 @@
+<?php
+class LanguageTest extends WikiaBaseTest {
+
+	/**
+	 * @dataProvider shortenNumberDecoratorDataProvider
+	 * @group UsingDB
+	 */
+	public function testShortenNumberDecorator($number,$expected) {
+		$this->mockMessage( 'number-shortening-billions', '$1B' );
+		$this->mockMessage( 'number-shortening-millions', '$1M' );
+		$this->mockMessage( 'number-shortening', '$1K' );
+
+		$lang = new Language();
+
+		$result = $lang->shortenNumberDecorator($number);
+		$this->assertEquals($expected, $result);
+	}
+
+	public function shortenNumberDecoratorDataProvider() {
+		return array(
+			array(1234,'1,234'), // note: this only works for EN
+			array(56000,'56K'),
+			array(56710,'56.7K'),
+			array(56756,'56.8K'),
+			array(56900,'56.9K'),
+			array(56990,'57K'),
+			array(123456789,'123.5M')
+		);
+	}
+}
