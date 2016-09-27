51e408116c0cda8fcbae7d6a74865beb30387dbe
sactage <sactage@gmail.com>
2016-09-26 21:56:38 -0400

    SOC-2952: Use SpecialDiscussionsHelper in SpecialDiscussionsController

--- extensions/wikia/SpecialDiscussions/SpecialDiscussionsController.class.php
+++ extensions/wikia/SpecialDiscussions/SpecialDiscussionsController.class.php
@@ -12,25 +12,18 @@ class SpecialDiscussionsController extends WikiaSpecialPageController {
 
 	const HTTP_STATUS_OK = 200;
 	const DISCUSSIONS_ACTION = 'specialdiscussions';
-	const SERVICE_NAME = 'discussion';
-	const TIMEOUT = 5;
 	const EDIT_TOKEN = 'editToken';
-	const SITE_NAME_MAX_LENGTH = 256;
 
 	const DEFAULT_TEMPLATE_ENGINE = \WikiaResponse::TEMPLATE_ENGINE_MUSTACHE;
 
-	private $logger;
-	private $sitesApi;
 	private $siteId;
 	private $siteName;
 
 	public function __construct() {
 		parent::__construct( 'Discussions', '', false );
 
-		$this->sitesApi = $this->getDiscussionSitesApi();
 		$this->siteId = F::app()->wg->CityId;
 		$this->siteName = F::app()->wg->Sitename;
-		$this->logger = Wikia\Logger\WikiaLogger::instance();
 	}
 
 	public function index() {
@@ -41,9 +34,13 @@ class SpecialDiscussionsController extends WikiaSpecialPageController {
 
 		$this->wg->Out->setPageTitle( wfMessage( 'discussions-pagetitle' )->escaped() );
 
-		if ( $this->request->wasPosted() ) {
-			$this->activateDiscussions();
-			WikiFactory::setVarByName( 'wgEnableDiscussions', $this->siteId, true );
+		if ( $this->request->wasPosted() && $this->assertValidPostRequest() ) {
+			if ( !SpecialDiscussionsHelper::activateDiscussions(
+					$this->siteId,
+					F::app()->wg->ContLang->getCode(),
+					$this->siteName ) ) {
+				throw new ErrorPageError( 'unknown-error', 'discussions-activate-error' );
+			}
 		}
 
 		$this->setIndexOutput();
@@ -63,68 +60,15 @@ class SpecialDiscussionsController extends WikiaSpecialPageController {
 		$this->response->setValues(
 			[
 				'discussionsActiveMessage' => wfMessage( 'discussions-active' )->escaped(),
-				'discussionsLink' => $this->getDiscussionsLink(),
+				'discussionsLink' => SpecialDiscussionsHelper::getDiscussionsLink(),
 				'discussionsLinkCaption' => wfMessage( 'discussions-navigate' )->escaped(),
 			]
 		);
 	}
 
 	private function setIndexOutput() {
-		$callMethod = $this->isDiscussionsActive() ? 'discussionsLink' : 'inputForm';
+		$callMethod = SpecialDiscussionsHelper::isDiscussionsActive( $this->siteId ) ? 'discussionsLink' : 'inputForm';
 		$this->response->setVal( 'content', $this->sendSelfRequest( $callMethod ) );
-	}
-
-	private function activateDiscussions() {
-		$this->assertValidPostRequest();
-
-		$siteInput = new \Swagger\Client\Discussion\Models\SiteInput(
-			[
-				'id' => $this->siteId,
-				'name' => substr( $this->siteName, 0, self::SITE_NAME_MAX_LENGTH ),
-				'language_code' => F::app()->wg->ContLang->getCode(),
-			]
-		);
-
-		try {
-			$this->getDiscussionSitesApi()->createSite( $siteInput, F::app()->wg->TheSchwartzSecretToken );
-		} catch ( ApiException $e ) {
-			$this->logger->error(
-				'Creating site caused an error',
-				[
-					'siteId' => $this->siteId,
-					'error' => $e->getMessage(),
-				]
-			);
-			throw new ErrorPageError( 'unknown-error', 'discussions-activate-error' );
-		}
-	}
-
-	private function isDiscussionsActive() {
-		try {
-			$this->getDiscussionSitesApi()->getSite( $this->siteId );
-			return true;
-		} catch ( ApiException $e ) {
-			$this->logger->debug( 'Getting site caused an error',
-				[
-					'siteId' => $this->siteId,
-					'error' => $e->getMessage(),
-				] );
-		}
-		return false;
-	}
-
-	private function getDiscussionsLink() {
-		return "/d/f";
-	}
-
-	private function getDiscussionSitesApi() {
-		/** @var ApiProvider $apiProvider */
-		$apiProvider = Injector::getInjector()->get( ApiProvider::class );
-		/** @var SitesApi $api */
-		$api = $apiProvider->getApi( self::SERVICE_NAME, SitesApi::class );
-		$api->getApiClient()->getConfig()->setCurlTimeout( self::TIMEOUT );
-
-		return $api;
 	}
 
 	private function assertCanAccess() {
