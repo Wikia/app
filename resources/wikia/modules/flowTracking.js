228b4c43ce73cb0c580a4162b02b50d4defe2021
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-10-03 08:15:21 +0000

    WW-379: Extend tracking parameters

--- resources/wikia/modules/flowTracking.js
+++ resources/wikia/modules/flowTracking.js
@@ -1,34 +1,44 @@
-define('wikia.flowTracking', ['wikia.tracker', 'mw'], function (tracker, mw) {
+define('wikia.flowTracking', ['wikia.tracker', 'wikia.window', 'mw', 'jquery'], function (tracker, w, mw, $) {
 
 	var track = tracker.buildTrackingFunction({
 			trackingMethod: 'analytics',
 			category: 'flow-tracking'
-		});
+		}),
+		userAgent = w.navigator.userAgent;
 
 	/**
 	 * Track edit flow beginning.
 	 *
 	 * @param flow name of the flow
+	 * @param extraParams additional parameters to track
 	 */
-	function beginFlow(flow) {
-		track({
-			action: tracker.ACTIONS.FLOW_START,
-			label: flow,
-			flowname: flow
-		});
+	function beginFlow(flow, extraParams) {
+		var params = $.extend({
+				action: tracker.ACTIONS.FLOW_START,
+				label: flow,
+				flowname: flow
+			}, extraParams, userAgent
+		);
+
+		track(params);
 	}
 
 	/**
 	 * Track intermediary flow step
 	 *
 	 * @param flow name of the flow
+	 * @param extraParams additional parameters to track
 	 */
-	function trackFlowStep(flow) {
+	function trackFlowStep(flow, extraParams) {
+		var params = {
+			action: tracker.ACTIONS.FLOW_MID_STEP,
+			label: flow,
+			flowname: flow
+		};
+
 		if (isContentPage()) {
-			track({
-				action: tracker.ACTIONS.FLOW_MID_STEP,
-				label: flow
-			});
+			params = $.extend(params, extraParams, userAgent);
+			track(params);
 		}
 	}
 
