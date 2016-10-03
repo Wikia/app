88fb963b55330e2706e2230c6a0e6a6c5734e1e5
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-10-03 10:40:39 +0200

    WW-341: track start-flow for direct url in CKE

--- extensions/wikia/RTE/js/plugins/flowtracking/plugin.js
+++ extensions/wikia/RTE/js/plugins/flowtracking/plugin.js
@@ -2,8 +2,24 @@ CKEDITOR.plugins.add('rte-flowtracking', {
 
 	init : function( editor ) {
 		GlobalTriggers.bind('rteinit', function() {
-			require(['wikia.flowTracking'], function(flowTrack) {
-				flowTrack.beginFlow('direct-url');
+			require(['wikia.flowTracking', 'wikia.querystring', 'mw'], function(flowTrack, QueryString, mw) {
+
+				// Track only creating articles from namespace 0
+				// IMPORTANT: on Special:CreatePage even after providing article title the namespace is -1
+				if (mw.config.get('wgNamespaceNumber') === 0 && mw.config.get('wgArticleId') === 0) {
+					var qs = new QueryString(window.location.href);
+
+					// 'flow' is the parameter passed in the url if user has started a flow already
+					var flowParam = qs.getVal('flow', false);
+
+					if (!flowParam && !document.referrer) {
+						flowTrack.beginFlow('direct-url');
+					} else {
+						//TODO: track middle step for other flows
+						console.log('flow='+flowParam);
+					}
+				}
+
 			})
 		});
 	}
