a3ac33c9ef02988326f02986478ff41003071192
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-10-04 10:37:52 +0000

    WW-342: Rename editor label

--- extensions/wikia/FlowTracking/scripts/createNewPageFlowsVE.js
+++ extensions/wikia/FlowTracking/scripts/createNewPageFlowsVE.js
@@ -1,7 +1,7 @@
 require(['wikia.flowTracking.createPage', 'mw', 'jquery'], function (flowTrackingCreatePage, mw, $) {
 	function init() {
 		mw.hook('ve.activationComplete').add(function () {
-			flowTrackingCreatePage.trackOnEditPageLoad('ve');
+			flowTrackingCreatePage.trackOnEditPageLoad('visualeditor');
 		});
 	}
 
