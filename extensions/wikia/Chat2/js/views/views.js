4e26df2a1607daa6a772c79e70a159d9bb729be7
jkarminski <jkarminski@wikia-inc.com>
2016-10-04 13:55:03 +0200

    SUS-966 fix inline alerts

--- extensions/wikia/Chat2/js/views/views.js
+++ extensions/wikia/Chat2/js/views/views.js
@@ -109,20 +109,18 @@ var ChatView = Backbone.View.extend({
 
 		// Inline Alerts have may have i18n messages in them. If so (and they don't have 'text' yet), process the message and cache it in 'text'.
 		// This needs to be done before the template processing below so that 'text' will be set by then.
-
-		/*
+		//console.log(this.model.get('text'));
+		console.log(this.model.get('wfMsg'));
 		if(this.model.get('text') == ''){
 			var params = this.model.get('msgParams'),
 				msgId = this.model.get('wfMsg');
-			if (!params || !msgId) {
+			if (!msgId) {
 				return this;
 			}
-			$().log("Found an i18n message with msg name " + msgId + " and params: " + params);
 			params.unshift(msgId);
-			var i18nText = $.msg.apply(null, params);
+			var i18nText = mw.message.apply(msgId, params).text();
 			this.model.set({text: i18nText});
-			$().log("Message translated to: " + i18nText);
-		}*/
+		}
 
 		var msg = this.model.toJSON();
 		// Make a call to process any text for links, unsafe html/js, emoticions, etc.
