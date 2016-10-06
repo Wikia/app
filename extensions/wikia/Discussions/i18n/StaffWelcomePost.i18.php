f750a37159ddf8ac8977adc43ed69f8c7c0a2834
James Sutterfield <james@wikia-inc.com>
2016-10-06 14:41:42 -0700

    SOC-3105 add localization files

---
+++ extensions/wikia/Discussions/i18n/StaffWelcomePost.i18.php
@@ -0,0 +1,20 @@
+<?php
+$messages = [];
+
+$messages['en'] = [
+	'discussions-staff-welcome-post' => "
+Welcome to Discussions!
+
+This is the place to talk about your favorite topic, to share news, theories, ideas, and to connect with others.
+
+To learn more about what you can do here, check out $1
+
+If you're an admin on this community, read more about how you can customize your Discussions and set up guidelines for contributors: $2
+
+Have fun!
+"
+];
+
+$messages['qqq'] = [
+	'discussions-staff-welcome-post' => 'Message posted by a staff account when Discussions is first enabled for a community. $1 -> Link to help page about Discussions, $2 -> Link to help page about tools for Admins',
+];
