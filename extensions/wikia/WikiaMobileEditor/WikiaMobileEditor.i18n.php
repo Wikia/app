46fe4daaba4645931eed89d249842c16af152fac
macbre <maciej.brencz@gmail.com>
2016-10-10 13:31:33 +0000

    Import tag-mobileedit* messages

--- extensions/wikia/WikiaMobileEditor/WikiaMobileEditor.i18n.php
+++ extensions/wikia/WikiaMobileEditor/WikiaMobileEditor.i18n.php
@@ -1,10 +1,10 @@
 <?php
-$messages = array();
+/** Internationalization file for /extensions/wikia/WikiaMobileEditor/WikiaMobileEditor extension. */
+$messages = [];
 
-$messages['en'] = array(
+$messages['en'] = [
 	'tag-mobileedit' => 'Mobile',
 	'tag-mobileedit-description' => 'Edit made using mobile skin',
-
 	'wikiamobileeditor-desc' => 'WikiaMobile Editor enhancements',
 	'wikiamobileeditor-cancel' => 'Cancel',
 	'wikiamobileeditor-publish' => 'Publish',
@@ -12,18 +12,16 @@ $messages['en'] = array(
 	'wikiamobileeditor-summary-placeholder' => 'What did you change (Optional)?',
 	'wikiamobileeditor-licensing' => 'Contributions licensed as',
 	'wikiamobileeditor-keep-editing' => 'Keep Editing',
-
 	'wikiamobileeditor-on-save' => 'Success! Your edit was published.',
 	'wikiamobileeditor-saving' => 'Saving',
 	'wikiamobileeditor-on-new' => 'You are starting a brand new article (section).',
 	'wikiamobileeditor-wrong' => 'Something went wrong.',
-	'wikiamobileeditor-internet' => 'Do you have internet connection ?'
-);
+	'wikiamobileeditor-internet' => 'Do you have internet connection ?',
+];
 
-$messages['qqq'] = array(
+$messages['qqq'] = [
 	'tag-mobileedit' => 'Shown on lists of changes (history, recentchanges, etc.) for each edit made using mobile skin.',
 	'tag-mobileedit-description' => 'Long description of the mobileedit tag.',
-
 	'wikiamobileeditor-desc' => '{{desc}}',
 	'wikiamobileeditor-cancel' => 'Message on mobile edit page that closses edit page and go back to an article',
 	'wikiamobileeditor-publish' => 'message on a button to publish an edit',
@@ -32,10 +30,23 @@ $messages['qqq'] = array(
 	'wikiamobileeditor-licensing' => 'message explaining under what license an edit is being made. visible in preview modal under publish button',
 	'wikiamobileeditor-licensing-type' => 'Licence name',
 	'wikiamobileeditor-keep-editing' => 'message on a button that closes preview modal',
-
 	'wikiamobileeditor-on-success' => 'massage visible in a small toast that indicates that edit is saved',
 	'wikiamobileeditor-on-new' => 'Massage displayed in a toast pop up, explaining that user is creating a new article',
 	'wikiamobileeditor-wrong' => 'Message displaed in a toast, indicating to user that something went wrong with displaying a preview',
-	'wikiamobileeditor-internet' => 'If no Internet connection is detected, this question is displaed next to wikiamobileeditor-wrong message'
-);
+	'wikiamobileeditor-internet' => 'If no Internet connection is detected, this question is displaed next to wikiamobileeditor-wrong message',
+];
+
+$messages['ko'] = [
+	'tag-mobileedit-description' => '모바일 스킨에서의 편집',
+	'tag-mobileedit' => '모바일',
+];
+
+$messages['pl'] = [
+	'tag-mobileedit-description' => 'Edycja wykonana przy użyciu mobilnej skórki',
+];
+
+$messages['ru'] = [
+	'tag-mobileedit-description' => 'Правка сделана из мобильного скина',
+	'tag-mobileedit' => 'С мобильного',
+];
 
