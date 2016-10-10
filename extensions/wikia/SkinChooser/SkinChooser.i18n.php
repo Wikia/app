2dd41b8420b9edf8bceb1ce7b4bc32fdbbcbc7cf
macbre <maciej.brencz@gmail.com>
2016-10-10 09:53:32 +0000

    Import SkinChooser extension messages

--- extensions/wikia/SkinChooser/SkinChooser.i18n.php
+++ extensions/wikia/SkinChooser/SkinChooser.i18n.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * @todo Update to use extension specific key prefix to get this supported
- *       at translatewiki.net. Also add L10n-able extension description.
- */
-
+/** Internationalization file for /extensions/wikia/SkinChooser/SkinChooser extension. */
 $messages = [];
 
 $messages['en'] = [
@@ -13,350 +9,215 @@ $messages['en'] = [
 	'right-setadminskin' => 'Set the wiki\'s default skin',
 ];
 
-/** Arabic (العربية)
- * @author Achraf94
- * @author Malhargan
- */
 $messages['ar'] = [
 	'skinchooser-desc' => 'يسمح للمستخدمين باختيار skins في التفضيلات الخاصة بهم',
 	'new-look' => 'ويكيا',
 	'site-layout' => 'موقع تخطيطات',
 ];
 
-/** Breton (brezhoneg)
- * @author Fohanno
- * @author Y-M D
- */
 $messages['br'] = [
 	'new-look' => 'Wikia',
 ];
 
-/** Catalan (català)
- * @author Marcmpujol
- */
 $messages['ca'] = [
-	'skinchooser-desc' => "Permet als usuaris triar l'aparença del wiki en les seves preferències",
+	'skinchooser-desc' => 'Permet als usuaris triar l\'aparença del wiki en les seves preferències',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Dissenys del lloc',
 ];
 
-/** Czech (čeština)
- * @author Darth Daron
- * @author Dontlietome7
- */
 $messages['cs'] = [
 	'skinchooser-desc' => 'Umnožňuje uživatelům zvolit vzhled v předvolbách',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Rozložení webu',
 ];
 
-/** German (Deutsch)
- * @author LWChris
- * @author Metalhead64
- * @author PtM
- */
 $messages['de'] = [
 	'skinchooser-desc' => 'Ermöglicht Benutzern in ihren Einstellungen Skins zu wählen',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Seitenlayouts',
 ];
 
-/** Spanish (español)
- * @author Bola
- * @author VegaDark
- */
 $messages['es'] = [
 	'skinchooser-desc' => 'Permite a los usuarios escoger la apariencia del wiki en sus preferencias',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Diseños del sitio',
 ];
 
-/** Persian (فارسی)
- * @author Reza1615
- */
 $messages['fa'] = [
 	'new-look' => 'ویکیا',
 ];
 
-/** Finnish (suomi)
- * @author Crt
- * @author Nedergard
- * @author Tm T
- * @author VezonThunder
- */
 $messages['fi'] = [
 	'skinchooser-desc' => 'Mahdollistaa ulkoasun valinnan käyttäjän asetuksissa',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Sivuston ulkoasut',
 ];
 
-/** Faroese (føroyskt)
- * @author EileenSanda
- */
 $messages['fo'] = [
 	'skinchooser-desc' => 'Loyvir brúkarum at velja útsjónd í innstillingum teirra',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Síðuuppsetan',
 ];
 
-/** French (français)
- * @author Gomoko
- * @author Iketsi
- * @author Verdy p
- * @author Wyz
- */
 $messages['fr'] = [
 	'skinchooser-desc' => 'Permet aux utilisateurs de choisir des habillages dans leurs préférences',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Apparences du site',
 ];
 
-/** Galician (galego)
- * @author Toliño
- */
 $messages['gl'] = [
 	'skinchooser-desc' => 'Permite aos usuarios seleccionar a aparencia nas súas preferencias',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Deseño do sitio',
 ];
 
-/** Hungarian (magyar)
- * @author Dj
- * @author TK-999
- */
 $messages['hu'] = [
 	'skinchooser-desc' => 'Lehetővé teszi a felhasználóknak a felület kiválasztását beállításaik között',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Oldalelrendezések',
 ];
 
-/** Interlingua (interlingua)
- * @author McDutchie
- */
 $messages['ia'] = [
 	'skinchooser-desc' => 'Permitte al usatores de seliger apparentias in lor preferentias',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Designos del sito',
 ];
 
-/** Italian (italiano)
- * @author Darth Kule
- * @author Pietrodn
- */
 $messages['it'] = [
 	'skinchooser-desc' => 'Consente agli utenti di scegliere la skin nelle loro preferenze',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Layout del sito',
 ];
 
-/** Japanese (日本語)
- * @author Schu
- * @author Tommy6
- */
 $messages['ja'] = [
 	'skinchooser-desc' => '利用者の個人設定での外装選択を可能にします。',
 	'new-look' => 'ウィキア',
 	'site-layout' => 'サイトの外装',
 ];
 
-/** Kannada (ಕನ್ನಡ)
- * @author VASANTH S.N.
- */
 $messages['kn'] = [
 	'new-look' => 'ವಿಕಿಯಾ',
 ];
 
-/** Korean (한국어)
- * @author Miri-Nae
- */
 $messages['ko'] = [
-	'new-look' => '위키아',
+	'new-look' => '위키아 기본 스킨',
 	'site-layout' => '사이트 배치',
 ];
 
-/** Northern Luri (لوری مینجایی)
- * @author Mogoeilor
- */
 $messages['lrc'] = [
 	'new-look' => 'ويكيا:',
 ];
 
-/** Macedonian (македонски)
- * @author Bjankuloski06
- */
 $messages['mk'] = [
 	'skinchooser-desc' => 'Им овозможува на корисниците да изберат руво во нагодувањата',
 	'new-look' => 'Викија',
 	'site-layout' => 'Распореди на страницата',
 ];
 
-/** Malay (Bahasa Melayu)
- * @author Anakmalaysia
- */
 $messages['ms'] = [
 	'skinchooser-desc' => 'Membolehkan pengguna memilih kulit pada keutamaan mereka',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Tataletak Tapak',
 ];
 
-/** Norwegian Bokmål (norsk bokmål)
- * @author Audun
- */
 $messages['nb'] = [
 	'skinchooser-desc' => 'Lar brukere velge utseende i innstillingene sine',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Sideoppsett',
 ];
 
-/** Dutch (Nederlands)
- * @author Siebrand
- */
 $messages['nl'] = [
 	'skinchooser-desc' => 'Maakt het mogelijk voor gebruikers in hun voorkeuren een vormgeving te kiezen',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Sitevormgevingen',
 ];
 
-/** Occitan (occitan)
- * @author Cedric31
- */
 $messages['oc'] = [
-	'skinchooser-desc' => "Permet als utilizaires de causir d'abilhatges dins lors preferéncias",
+	'skinchooser-desc' => 'Permet als utilizaires de causir d\'abilhatges dins lors preferéncias',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Aparéncias del site',
 ];
 
-/** Polish (polski)
- * @author Cloudissimo
- * @author Sovq
- */
 $messages['pl'] = [
 	'skinchooser-desc' => 'Pozwala użytkownikom wybrać skórkę w swoich preferencjach',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Skórka',
 ];
 
-/** Piedmontese (Piemontèis)
- * @author Borichèt
- * @author Dragonòt
- */
 $messages['pms'] = [
-	'skinchooser-desc' => "A përmët a j'utent ëd serne dle pel ant ij Sò Gust",
+	'skinchooser-desc' => 'A përmët a j\'utent ëd serne dle pel ant ij Sò Gust',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Presentassion dël sit',
 ];
 
-/** Pashto (پښتو)
- * @author Ahmed-Najib-Biabani-Ibrahimkhel
- */
 $messages['ps'] = [
 	'new-look' => 'ويکيا',
 ];
 
-/** Portuguese (português)
- * @author Hamilton Abreu
- * @author SandroHc
- */
 $messages['pt'] = [
 	'skinchooser-desc' => 'Permite que os utilizadores escolham temas nas suas preferências',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Composições do Site',
 ];
 
-/** Brazilian Portuguese (português do Brasil)
- * @author Aristóbulo
- * @author Luckas
- * @author Luckas Blade
- */
 $messages['pt-br'] = [
 	'skinchooser-desc' => 'Permite que os utilizadores escolham temas nas suas preferências',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Composições do Site',
 ];
 
-/** tarandíne (tarandíne)
- * @author Joetaras
- */
 $messages['roa-tara'] = [
-	'skinchooser-desc' => "Permette a l'utinde de scacchià le teme jndr'à sezione Me piace accussì",
+	'skinchooser-desc' => 'Permette a l\'utinde de scacchià le teme jndr\'à sezione Me piace accussì',
 	'new-look' => 'Uicchia',
-	'site-layout' => "Disposiziune d'u site",
+	'site-layout' => 'Disposiziune d\'u site',
 ];
 
-/** Russian (русский)
- * @author DCamer
- * @author Kuzura
- */
 $messages['ru'] = [
 	'skinchooser-desc' => 'Позволяет участникам выбирать оболочки в личных настройках',
 	'new-look' => 'Викия',
 	'site-layout' => 'Оформления сайта',
 ];
 
-/** Serbian (Cyrillic script) (српски (ћирилица)‎)
- * @author Rancher
- */
 $messages['sr-ec'] = [
 	'site-layout' => 'Изглед',
 ];
 
-/** Swedish (svenska)
- * @author Tobulos1
- * @author WikiPhoenix
- */
 $messages['sv'] = [
 	'skinchooser-desc' => 'Tillåter användare att välja utseende i deras inställningar',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Layouter för webbplatsen',
 ];
 
-/** Tagalog (Tagalog)
- * @author AnakngAraw
- */
 $messages['tl'] = [
 	'skinchooser-desc' => 'Nagpapahintulot sa mga tagagamit na pumili ng mga pabalat sa loob ng kanilang mga nais',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Mga Latag ng Pook',
 ];
 
-/** Turkish (Türkçe)
- * @author Sucsuzz
- */
 $messages['tr'] = [
 	'new-look' => 'Wikia',
 ];
 
-/** Ukrainian (українська)
- * @author Ua2004
- */
 $messages['uk'] = [
 	'skinchooser-desc' => 'Дозволяє користувачам вибирати скіни в налаштуваннях',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Оформлення сайту',
 ];
 
-/** Uzbek (oʻzbekcha/ўзбекча)
- * @author Akmalzhon
- */
 $messages['uz'] = [
 	'skinchooser-desc' => 'Foydalanuvchilarga uz skinlarini tanlashga imkon beradi',
 	'new-look' => 'Vikia',
 ];
 
-/** Vietnamese (Tiếng Việt)
- * @author Baonguyen21022003
- */
 $messages['vi'] = [
 	'skinchooser-desc' => 'Cho phép người dùng lựa chọn giao diện trong sở thích của họ',
 	'new-look' => 'Wikia',
 	'site-layout' => 'Bố trí trang web',
 ];
 
-/** Simplified Chinese (中文（简体）‎)
- * @author Dimension
- */
 $messages['zh-hans'] = [
 	'skinchooser-desc' => '用户可依自身偏好选择皮肤',
 	'new-look' => 'Wikia',
 	'site-layout' => '网站布局',
 ];
+
