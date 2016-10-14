8534f37c8892d45fc0cc8cec3da556eb595369eb
macbre <maciej.brencz@gmail.com>
2016-10-14 12:14:32 +0200

    ForumBoardInfo: expect array to provided to the constructor

--- extensions/wikia/Forum/ForumBoardInfo.class.php
+++ extensions/wikia/Forum/ForumBoardInfo.class.php
@@ -15,13 +15,11 @@ class ForumBoardInfo {
 	 * ForumBoardInfo constructor.
 	 * @param array $array ;
 	 */
-	public function __construct( $array = [] ) {
-		if ( is_array( $array ) ) {
-			$this->setLastPost( new ForumPostInfo( $array[ 'lastPost' ] ) );
-			unset( $array[ 'lastPost' ] );
-			foreach ( $array as $key => $val ) {
-				$this->$key = $val;
-			}
+	public function __construct( Array $array = [] ) {
+		$this->setLastPost( new ForumPostInfo( $array[ 'lastPost' ] ) );
+		unset( $array[ 'lastPost' ] );
+		foreach ( $array as $key => $val ) {
+			$this->$key = $val;
 		}
 	}
 
