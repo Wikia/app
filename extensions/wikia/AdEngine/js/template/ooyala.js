61d5161d80a3bd2c4c8c379b69e138c66593909b
Szymon Frąszczak <szymon@wikia-inc.com>
2016-10-18 09:27:25 +0200

    Merge pull request #11644 from Wikia/ADEN-4133
    ADEN-4133 Load Ooyala player from creative template

---
+++ extensions/wikia/AdEngine/js/template/ooyala.js
@@ -0,0 +1,43 @@
+/*global define*/
+define('ext.wikia.adEngine.template.ooyala', [
+	'ext.wikia.adEngine.video.vastBuilder',
+	'wikia.log',
+	'wikia.videoBootstrap'
+], function (vastBuilder, log, VideoBootstrap) {
+	'use strict';
+	var libraryUrl = 'http://player.ooyala.com/v3/52bc289bedc847e3aa8eb2b347644f68?platform=html5-priority',
+		logGroup = 'ext.wikia.adEngine.template.ooyala';
+
+	function show(params) {
+		var aspectRatio = params.width / params.height,
+			playerId = 'ooyala_' + Math.floor((1 + Math.random()) * 0x10000),
+			vb;
+
+		vb = new VideoBootstrap(params.container, {
+			html: '<div id="' + playerId + '" style="width: 100%; height: 100%;"></div>',
+			width: params.width,
+			height: params.height,
+			jsParams: {
+				playerId: playerId,
+				videoId: params.videoId,
+				autoPlay: params.autoPlay || true,
+				tagUrl: params.vastUrl || vastBuilder.build(params.src, params.slotName, aspectRatio),
+				jsFile: [
+					libraryUrl,
+					'extensions/wikia/VideoHandlers/js/handlers/lib/OoyalaAgeGate.js'
+				],
+				onCreate: params.onReady
+			},
+			init: 'wikia.videohandler.ooyala',
+			scripts: [
+				'extensions/wikia/VideoHandlers/js/handlers/Ooyala.js'
+			]
+		});
+
+		log(['show', vb, params], 'debug', logGroup);
+	}
+
+	return {
+		show: show
+	};
+});
