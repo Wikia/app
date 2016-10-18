c13385ace42d877d1117f0d5e419e1f784c13c99
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-10-18 10:35:52 +0000

    WW-440: Add first flow tracking test scenarios

--- extensions/wikia/FlowTracking/scripts/spec/createPageTracking.spec.js
+++ extensions/wikia/FlowTracking/scripts/spec/createPageTracking.spec.js
@@ -1,11 +1,12 @@
 describe('createPageTracking', function () {
 	'use strict';
 
-	function _() {}
-
-	var mocks = {
+	var wgNamespaceNumber = 0,
+		wgArticleId = 0,
+		wgTitle = 'Test',
+		mocks = {
 			window: {
-				location: 'http://www.muppet.wikia.com',
+				location: 'http://www.muppet.wikia.com/wiki/Elmo?action=edit',
 				history: {
 					replaceState: function () {
 					}
@@ -22,11 +23,11 @@ describe('createPageTracking', function () {
 				config: {
 					get: function (value) {
 						if (value === 'wgNamespaceNumber') {
-							return 0;
+							return wgNamespaceNumber;
 						} else if (value === 'wgArticleId') {
-							return 0;
+							return wgArticleId;
 						} else if (value === 'wgTitle') {
-							return 'Test';
+							return wgTitle;
 						}
 					}
 				}
@@ -38,13 +39,86 @@ describe('createPageTracking', function () {
 		},
 		QuerystringModule = modules['wikia.querystring']();
 
-	it('should be defined', function () {
+	beforeEach(function () {
+		wgArticleId = 0;
+		wgNamespaceNumber = 0;
+		mocks.window.location = 'http://www.muppet.wikia.com/wiki/Elmo?action=edit';
+
+		spyOn(mocks.flowTracking, 'beginFlow');
+		spyOn(mocks.flowTracking, 'trackFlowStep');
+	});
+
+	it('should track direct url flow', function () {
+		wgArticleId = 0;
+		wgNamespaceNumber = 0;
+
 		var createPageTrackingModule = modules['ext.wikia.flowTracking.createPageTracking'](
 			mocks.flowTracking, QuerystringModule, mocks.mw, mocks.document, mocks.window
 		);
 
 		createPageTrackingModule.trackOnEditPageLoad('editor');
 
-		expect(createPageTrackingModule).toBeDefined();
+		expect(mocks.flowTracking.beginFlow).toHaveBeenCalledWith('create-page-direct-url', {editor: 'editor'});
+		expect(mocks.flowTracking.trackFlowStep).not.toHaveBeenCalled();
+	});
+
+	it('should track contribute button flow', function () {
+		wgArticleId = 0;
+		wgNamespaceNumber = 0;
+		mocks.window.location = 'http://www.muppet.wikia.com/wiki/Elmo?action=edit&flow=create-page-contribute-button';
+
+		var createPageTrackingModule = modules['ext.wikia.flowTracking.createPageTracking'](
+			mocks.flowTracking, QuerystringModule, mocks.mw, mocks.document, mocks.window
+		);
+
+		createPageTrackingModule.trackOnEditPageLoad('visualeditor');
+
+		expect(mocks.flowTracking.beginFlow).not.toHaveBeenCalledWith();
+		expect(mocks.flowTracking.trackFlowStep).toHaveBeenCalledWith(
+			'create-page-contribute-button', {editor: 'visualeditor'}
+		);
+	});
+
+	it('should not track flow if article exists', function () {
+		wgArticleId = 2;
+		wgNamespaceNumber = 0;
+
+		var createPageTrackingModule = modules['ext.wikia.flowTracking.createPageTracking'](
+			mocks.flowTracking, QuerystringModule, mocks.mw, mocks.document, mocks.window
+		);
+
+		createPageTrackingModule.trackOnEditPageLoad('editor');
+
+		expect(mocks.flowTracking.beginFlow).not.toHaveBeenCalled();
+		expect(mocks.flowTracking.trackFlowStep).not.toHaveBeenCalled();
+	});
+
+	it('should not track flow if namespace is different than main or special', function () {
+		wgArticleId = 0;
+		wgNamespaceNumber = 4;
+
+		var createPageTrackingModule = modules['ext.wikia.flowTracking.createPageTracking'](
+			mocks.flowTracking, QuerystringModule, mocks.mw, mocks.document, mocks.window
+		);
+
+		createPageTrackingModule.trackOnEditPageLoad('editor');
+
+		expect(mocks.flowTracking.beginFlow).not.toHaveBeenCalled();
+		expect(mocks.flowTracking.trackFlowStep).not.toHaveBeenCalled();
+	});
+
+	it('should not track flow if namespace is special but page is different than CreatePage', function () {
+		wgArticleId = 0;
+		wgNamespaceNumber = -1;
+		wgTitle = 'Insights';
+
+		var createPageTrackingModule = modules['ext.wikia.flowTracking.createPageTracking'](
+			mocks.flowTracking, QuerystringModule, mocks.mw, mocks.document, mocks.window
+		);
+
+		createPageTrackingModule.trackOnEditPageLoad('editor');
+
+		expect(mocks.flowTracking.beginFlow).not.toHaveBeenCalled();
+		expect(mocks.flowTracking.trackFlowStep).not.toHaveBeenCalled();
 	});
 });
