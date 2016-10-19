46f209051ffcc957d4f6485c5a76ce55fd0a6be9
jacek <jacek@wikia-inc.com>
2016-10-19 16:08:06 +0200

    ADEN-4126 fix resource loader bug with EventDispatcher

--- extensions/wikia/AdEngine/js/AdDecoratorPageDimensions.js
+++ extensions/wikia/AdEngine/js/AdDecoratorPageDimensions.js
@@ -1,7 +1,7 @@
 /*global define*/
 define('ext.wikia.adEngine.adDecoratorPageDimensions', [
-	'ext.wikia.adEngine.adLogicPageDimensions', 'wikia.log', 'ext.wikia.adEngine.eventDispatcher'
-], function (adLogicPageDimensions, log, eventDispatcher) {
+	'ext.wikia.adEngine.adLogicPageDimensions', 'wikia.log'
+], function (adLogicPageDimensions, log) {
 	'use strict';
 
 	var logGroup = 'ext.wikia.adEngine.adDecoratorPageDimensions';
