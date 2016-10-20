72cca3f5b7b896c2f327ec24fc621ebb5cfc77af
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2016-10-20 12:35:01 +0200

    ADEN-4124 Separate bfaa

---
+++ extensions/wikia/AdEngine/js/template/bfaaDesktop.js
@@ -0,0 +1,110 @@
+/*global define, require*/
+define('ext.wikia.adEngine.template.bfaaDesktop', [
+	'ext.wikia.adEngine.adHelper',
+	'ext.wikia.adEngine.context.uapContext',
+	'ext.wikia.adEngine.provider.btfBlocker',
+	'ext.wikia.adEngine.slotTweaker',
+	'ext.wikia.adEngine.video.uapVideoAd',
+	'wikia.document',
+	'wikia.log',
+	'wikia.window',
+	require.optional('ext.wikia.aRecoveryEngine.recovery.tweaker')
+], function (
+	adHelper,
+	uapContext,
+	btfBlocker,
+	slotTweaker,
+	uapVideoAd,
+	doc,
+	log,
+	win,
+	recoveryTweaker
+) {
+	'use strict';
+
+	var breakPointWidthNotSupported = 767, // SCSS property: $breakpoint-width-not-supported
+		logGroup = 'ext.wikia.adEngine.template.bfaaDesktop',
+		nav,
+		page,
+		unblockedSlots = [
+			'BOTTOM_LEADERBOARD',
+			'INCONTENT_BOXAD_1'
+		],
+		wrapper;
+
+
+	function updateNavBar(height) {
+		var position = win.scrollY || win.pageYOffset;
+
+		log(['updateNavBar', height, position], 'info', logGroup);
+
+		if (doc.body.offsetWidth <= breakPointWidthNotSupported || position <= height) {
+			nav.classList.add('bfaa-pinned');
+		} else {
+			nav.classList.remove('bfaa-pinned');
+		}
+	}
+
+	function runOnReady(iframe, params) {
+		var spotlightFooter = doc.getElementById('SPOTLIGHT_FOOTER'),
+			adContainer = doc.getElementById(params.slotName);
+
+		nav.style.top = '';
+		page.classList.add('bfaa-template');
+
+		log('desktopHandler::show', 'info', logGroup);
+
+		updateNavBar(adContainer.offsetHeight);
+		doc.addEventListener('scroll', adHelper.throttle(function () {
+			updateNavBar(adContainer.offsetHeight);
+		}, 100));
+
+		if (win.WikiaBar) {
+			win.WikiaBar.hideContainer();
+		}
+
+		if (spotlightFooter) {
+			spotlightFooter.parentNode.style.display = 'none';
+		}
+
+		if (recoveryTweaker && recoveryTweaker.isTweakable()) {
+			slotTweaker.removeDefaultHeight(params.slotName);
+			recoveryTweaker.tweakSlot(params.slotName, iframe);
+		}
+
+		if (params.video && params.videoTriggerElement) {
+			slotTweaker.onReady(params.slotName, function(iframe) {
+				var image = iframe.parentNode.parentNode.parentNode,
+					video = uapVideoAd.init(doc.getElementById(params.slotName), image, params.video);
+
+				params.videoTriggerElement.addEventListener('click', function () {
+					uapVideoAd.playAndToggle(video, image);
+				});
+			});
+		}
+	}
+
+	function show(params) {
+		nav = doc.getElementById('globalNavigation');
+		page = doc.getElementsByClassName('WikiaSiteWrapper')[0];
+		wrapper = doc.getElementById('WikiaTopAds');
+
+		log(['show', page, wrapper, params], 'info', logGroup);
+
+		wrapper.style.opacity = '0';
+		slotTweaker.makeResponsive(params.slotName, params.aspectRatio);
+		slotTweaker.onReady(params.slotName, function (iframe) {
+			runOnReady(iframe, params);
+			wrapper.style.opacity = '';
+		});
+
+		log(['show', params.uap], 'info', logGroup);
+
+		uapContext.setUapId(params.uap);
+		unblockedSlots.forEach(btfBlocker.unblock);
+	}
+
+	return {
+		show: show
+	};
+});
