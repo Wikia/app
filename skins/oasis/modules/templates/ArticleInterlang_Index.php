7df79353dd98c4f41ac69a5385a3720051743a73
grunny <mwgrunny@gmail.com>
2016-10-21 20:29:08 +1000

    (PLATFORM-2536) Escape output in ArticleInterlangController and prevent it being called externally

--- skins/oasis/modules/templates/ArticleInterlang_Index.php
+++ skins/oasis/modules/templates/ArticleInterlang_Index.php
@@ -1,9 +1,9 @@
-<?php if ($language_list) : ?>
+<?php if ( $language_list ): ?>
 <nav class="WikiaArticleInterlang">
-	<h3><?= wfMsg('oasis-interlang-languages'); ?> </h3>
+	<h3><?= wfMessage( 'oasis-interlang-languages' )->escaped(); ?> </h3>
 	<ul>
-	<?php foreach ($language_list as $val) : ?>
-		<li><a href="<?= $val["href"] ?>"><?= $val["name"]; ?></a></li>
+	<?php foreach ( $language_list as $val ) : ?>
+		<li><a href="<?= Sanitizer::encodeAttribute( $val['href'] ); ?>"><?= htmlspecialchars( $val['name'] ); ?></a></li>
 	<?php endforeach ?>
 	</ul>
 </nav>
