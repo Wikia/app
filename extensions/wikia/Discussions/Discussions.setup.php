f4242f53de10ca05a22f4185506c8fd685212064
Garth Webb <garth@wikia-inc.com>
2016-10-28 16:09:30 -0700

    SOC-3129 Redirect Special:Forum to /d/f when Discussions are enabled and Forums are disabled.

--- extensions/wikia/Discussions/Discussions.setup.php
+++ extensions/wikia/Discussions/Discussions.setup.php
@@ -25,6 +25,11 @@ $wgAutoloadClasses['StaffWelcomePoster'] = $dir . 'maintenance/StaffWelcomePoste
 // register special page
 $wgSpecialPages['Discussions'] = 'SpecialDiscussionsController';
 
+if ( empty( $wgEnableForumExt ) ) {
+	$wgAutoloadClasses['SpecialForumRedirectController'] = $dir . 'controllers/SpecialForumRedirectController.class.php';
+	$wgSpecialPages['Forum'] = 'SpecialForumRedirectController';
+}
+
 // message files
 $wgExtensionMessagesFiles['SpecialDiscussions'] = $dir . 'i18n/SpecialDiscussions.i18n.php';
 $wgExtensionMessagesFiles['StaffWelcomePost'] = $dir . 'i18n/StaffWelcomePost.i18n.php';
