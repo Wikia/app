756cfc996c151dcc25ff0b88bba024fbb09082d5
Igor Rogatty <igor@wikia-inc.com>
2016-11-03 16:56:35 +0100

    XW-2277 | use wgArticlePath

--- extensions/wikia/PortableInfobox/js/PortableInfoboxMonobook.js
+++ extensions/wikia/PortableInfobox/js/PortableInfoboxMonobook.js
@@ -1,13 +1,14 @@
-(function () {
+(function (window) {
 	'use strict';
 
 	var ImageLink = {
 		init: function ($content) {
 			$content.find('.portable-infobox .pi-image > a.image').on('click', function () {
 				var $anchor = $(this),
-					fileName = $anchor.children('.pi-image-thumbnail').data('image-key');
+					fileName = $anchor.children('.pi-image-thumbnail').data('image-key'),
+					href = window.wgArticlePath.replace(/\$1/, 'File:' + encodeURIComponent(fileName));
 
-				$anchor.attr('href', '/wiki/File:' + encodeURIComponent(fileName));
+				$anchor.attr('href', href);
 			});
 		}
 	};
@@ -15,4 +16,4 @@
 	mw.hook('wikipage.content').add(function ($content) {
 		ImageLink.init($content);
 	});
-})();
+})(window);
