584ed9130cc5514010754def57b145076e359b5c
Damian Jóźwiak <damian@wikia-inc.com>
2016-11-04 10:16:33 +0100

    XW-2279 | Add 'in'

--- extensions/wikia/PortableInfoboxBuilder/PortableInfoboxBuilder.i18n.php
+++ extensions/wikia/PortableInfoboxBuilder/PortableInfoboxBuilder.i18n.php
@@ -11,7 +11,7 @@ $messages['en'] = array(
 
 $messages['qqq'] = array(
 	'infoboxbuilder' => 'Tool name used on special pages list',
-	'portable-infobox-builder-title' => 'Special Page title that is displayed browser toolbar',
+	'portable-infobox-builder-title' => 'Special Page title that is displayed in browser toolbar',
 	'portable-infobox-builder-documentation-example-usage' => 'Information that infobox usage follows',
 	'portable-infobox-builder-documentation-example-param' => 'Literally \'example param value\'',
 	'portable-infobox-builder-move-message' => 'Move message displayed on revision history page',
