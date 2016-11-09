e979e3813c7e3eb41af4b4c27260f805ba6e91c8
Karol Tatala <ktatala@wikia-inc.com>
2016-11-09 15:13:06 +0100

    SOC-3225 | restyling discussion widget

--- extensions/wikia/EmbeddableDiscussions/scripts/ext.embeddableDiscussions.js
+++ extensions/wikia/EmbeddableDiscussions/scripts/ext.embeddableDiscussions.js
@@ -126,19 +126,68 @@ require([
 		}).done(function (data) {
 			var threads = processData(data._embedded.threads, requestData.upvoteRequestUrl);
 
+			var imagesDir = '/extensions/wikia/EmbeddableDiscussions/images/';
+
 			$elem.html(mustache.render(templates.DiscussionThreads, {
 				threads: threads,
 				columnsDetailsClass: columnsDetailsClass,
 				replyText: $.msg('embeddable-discussions-reply'),
+				replyIconSrc: imagesDir + 'reply.svg',
+				replyTinyIconSrc: imagesDir + 'reply-tiny.svg',
 				shareText: $.msg('embeddable-discussions-share'),
+				shareIconSrc: imagesDir + 'share.svg',
 				showAll: $.msg('embeddable-discussions-show-all'),
 				upvoteText: $.msg('embeddable-discussions-upvote'),
+				upvoteIconSrc: imagesDir + 'upvote.svg',
+				upvoteTinyIconSrc: imagesDir + 'upvote-tiny.svg',
 				zeroText: $.msg('embeddable-discussions-zero'),
 				zeroTextDetail: $.msg('embeddable-discussions-zero-detail'),
 			}));
+
+			replaceSvgImages($elem);
+
 		}).fail(function () {
 			throbber.hide($elem);
 			$elem.html($.msg('embeddable-discussions-error-loading'));
+		});
+	}
+
+	/**
+	 * Replace img elements with svg element, it give us possibility to set fill color of svg.
+	 * @param $elem
+	 */
+	function replaceSvgImages($elem) {
+		$elem.find('img.svg').each(function(){
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
+
+			jQuery.get(imgURL, function(data) {
+				// Get the SVG tag, ignore the rest
+				var $svg = jQuery(data).find('svg');
+
+				// Add replaced image's ID to the new SVG
+				if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+				}
+				// Add replaced image's classes to the new SVG
+				if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+
+				// Remove any invalid XML tags as per http://validator.w3.org
+				$svg = $svg.removeAttr('xmlns:a');
+
+				$svg.attr('width', $img.attr('width'));
+				$svg.attr('height', $img.attr('height'));
+				$svg.attr('viewBox', '0 0 ' + $img.attr('width') + ' ' + $img.attr('height'));
+
+				// Replace image with new SVG
+				$img.replaceWith($svg);
+
+			}, 'xml');
+
 		});
 	}
 
