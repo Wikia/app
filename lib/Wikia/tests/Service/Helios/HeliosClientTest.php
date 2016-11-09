3b98745256f23ceab607a4511546b0932e659fa9
mech <mech@themech.net>
2016-11-09 16:29:46 +0100

    Add a testcase for retries.

--- lib/Wikia/tests/Service/Helios/HeliosClientTest.php
+++ lib/Wikia/tests/Service/Helios/HeliosClientTest.php
@@ -34,7 +34,7 @@ class HeliosClientTest extends \WikiaBaseTest {
 			->method( 'getContent' )
 			->willReturn( null );
 
-		$requestMock->status = $this->getMock( 'stdClass', [ 'isOK', 'getErrorsArray' ]);
+		$requestMock->status = $this->getMock( 'stdClass', [ 'isOK', 'getErrorsArray', 'hasMessage' ]);
 
 		$this->mockStaticMethod( '\Http', 'request', $requestMock );
 
@@ -51,10 +51,40 @@ class HeliosClientTest extends \WikiaBaseTest {
 			->method( 'getContent' )
 			->willReturn( '{}' );
 
-		$this->mockStaticMethod( '\Http', 'request', $requestMock );
+		$requestMock->status = $this->getMock( 'stdClass', [ 'isOK', 'getErrorsArray', 'hasMessage' ]);
+		$requestMock->status->expects( $this->any() )->method( 'hasMessage' )->willReturn( false );
+
+		// With no error message, we expect no retries, hence \Http::request should be called only once.
+		$this->getStaticMethodMock( '\Http', 'request' )->expects( $this->once() )
+			->method( 'request' )
+			->willReturn( $requestMock );
 
 		$client = new HeliosClientImpl( 'http://example.com', 'id', 'secret' );
 		$this->assertInternalType( 'object', $client->request( 'resource', [], [], [] ) );
+	}
+
+	public function testRetry()
+	{
+		// To reduce the unit test time, mock the 'sleep' method called between retries.
+		$this->mockGlobalFunction( 'sleep', null);
+
+		$this->mockStaticMethod( '\MWHttpRequest', 'canMakeRequests', true );
+
+		$requestMock = $this->getMock( '\CurlHttpRequest', [ 'execute', 'getContent' ], [ 'http://example.com' ], '', false );
+		$requestMock->expects( $this->once() )
+			->method( 'getContent' )
+			->willReturn( '{}' );
+
+		$requestMock->status = $this->getMock( 'stdClass', [ 'isOK', 'getErrorsArray', 'hasMessage' ]);
+		$requestMock->status->expects( $this->any() )->method( 'hasMessage' )->willReturn( true );
+
+		// Retries are base on status->hasMessage value, so we expect two calls to \Http::request here.
+		$this->getStaticMethodMock( '\Http', 'request' )->expects( $this->exactly( 2 ) )
+			->method( 'request' )
+			->willReturn( $requestMock );
+
+		$client = new HeliosClientImpl( 'http://example.com', 'id', 'secret' );
+		$client->request( 'resource', [], [], [] );
 	}
 
 }
