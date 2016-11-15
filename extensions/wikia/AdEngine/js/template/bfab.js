e4ee0b3dd8ae09f27ca1dc40e42871ef37795c9e
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2016-11-15 16:01:32 +0100

    ADEN-4182 Run video ad in BFAB slot

--- extensions/wikia/AdEngine/js/template/bfab.js
+++ extensions/wikia/AdEngine/js/template/bfab.js
@@ -1,24 +1,64 @@
 /*global define, require*/
 define('ext.wikia.adEngine.template.bfab', [
 	'ext.wikia.adEngine.slotTweaker',
+	'ext.wikia.adEngine.video.videoAdFactory',
 	'wikia.log',
 	'wikia.document',
+	'wikia.window',
 	require.optional('ext.wikia.aRecoveryEngine.recovery.tweaker')
-], function (slotTweaker, log, doc, recoveryTweaker) {
+], function (slotTweaker, videoAd, log, doc, win, recoveryTweaker) {
 	'use strict';
 
 	var logGroup = 'ext.wikia.adEngine.template.bfab',
 		slot = doc.getElementById('BOTTOM_LEADERBOARD') || doc.getElementById('MOBILE_BOTTOM_LEADERBOARD');
 
+	function isVideoAd(params) {
+		return params.videoAspectRatio && params.uap;
+	}
+
+	function calculateVideoSize(element, videoAspectRatio) {
+		var width = parseInt(win.getComputedStyle(element).width.replace('px', ''));
+		return {
+			width: width,
+			height: width / videoAspectRatio
+		};
+	}
+
+	function initializeVideoAd(iframe, params) {
+		var providerContainer = slot.querySelector('div');
+
+		var video = videoAd.create(
+			providerContainer,
+			calculateVideoSize(iframe, params.videoAspectRatio),
+			slot,
+			{
+				src: 'gpt',
+				slotName: params.pos,
+				uap: params.uap,
+				passback: 'vuap'
+			}
+		);
+
+		win.addEventListener('resize', function () {
+			video.updateSize(calculateVideoSize(iframe, params.videoAspectRatio));
+		});
+
+		params.videoTriggerElement.addEventListener('click', video.play.bind(video));
+	}
+
 	function show(params) {
 		slot.classList.add('bfab-template');
 		slotTweaker.makeResponsive(slot.id, params.aspectRatio);
 
-		if (recoveryTweaker && recoveryTweaker.isTweakable()) {
-			slotTweaker.onReady(slot.id, function (iframe) {
+		slotTweaker.onReady(slot.id, function (iframe) {
+			if (recoveryTweaker && recoveryTweaker.isTweakable()) {
 				recoveryTweaker.tweakSlot(slot.id, iframe);
-			});
-		}
+			}
+
+			if (isVideoAd(params)) {
+				initializeVideoAd(iframe, params);
+			}
+		});
 
 		log('show', 'info', logGroup);
 	}
