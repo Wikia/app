a03c3643632bd65632927063ee6dcad132203af1
Bogna Knychala <bogna.ka@gmail.com>
2016-11-17 17:10:17 +0100

    Revert "ADEN-4245 | minify fallback script"
    This reverts commit 1eb7a8e782291c052f3fd917d3c8aa1e0027b294.

--- extensions/wikia/ARecoveryEngine/js/SourcePoint/mms_client.js
+++ extensions/wikia/ARecoveryEngine/js/SourcePoint/mms_client.js
@@ -1,1 +1,71 @@
-!function(){function n(n){var _="";for(var c in o)_+="&"+encodeURIComponent("t["+c+"]")+"="+encodeURIComponent(o[c]);var s="?v=1&account_id="+encodeURIComponent(window._sp_.config.account_id)+"&abp="+encodeURIComponent(n)+"&referrer="+encodeURIComponent(document.referrer)+_+"&cdc=window._sp_.mms._internal.cdc1",e="//"+window._sp_.config.mms_domain+"/mms/get_site_js"+s;if(n)window._sp_.bootstrap(e);else{var m=document.createElement("script");m.src=e,document.head.appendChild(m)}}window._sp_=window._sp_||{},window._sp_.mms=window._sp_.mms||{},window._sp_.mms.cmd=window._sp_.mms.cmd||[],window._sp_.mms._internal=window._sp_.mms._internal||{};var o={};window._sp_.mms.setTargeting=function(n,_){o[n]=_},window._sp_.mms.startMsg=function(){window._sp_.messagingDetection(n)},window._sp_.mms._internal.cdc1=function(){if("function"==typeof window._sp_.config.mms_client_data_callback)try{window._sp_.config.mms_client_data_callback.apply(window._sp_.config.mms_client_data_callback,arguments)}catch(n){console.log("mms_client_data_callback failed",n)}},window._sp_.msg=window._sp_.msg||{},window._sp_.msg.cmd=window._sp_.msg.cmd||[],window._sp_.msg.cmd.push(function(){for(var n=0;n<window._sp_.mms.cmd.length;n++)try{window._sp_.mms.cmd[n]()}catch(o){console.error("Failed to execute cmd",o)}var _=[];_.push=function(n){try{n()}catch(o){console.error("Failed to execute cmd queue",o)}},window._sp_.mms.cmd=_})}();
+(function() {
+
+    window._sp_ = window._sp_ || {};
+   
+    // Init MMS client cmd queue
+    window._sp_.mms = window._sp_.mms || {};
+    window._sp_.mms.cmd = window._sp_.mms.cmd || [];
+    window._sp_.mms._internal = window._sp_.mms._internal || {};
+
+    // Init MMS API
+    var targetingParams = {};
+    window._sp_.mms.setTargeting = function(key, value) {
+        targetingParams[key] = value;
+    };
+
+    window._sp_.mms.startMsg = function() {
+        window._sp_.messagingDetection(loadMessage);
+    };
+
+    window._sp_.mms._internal.cdc1 = function() {
+        if (typeof window._sp_.config.mms_client_data_callback === 'function') {
+            try {
+                window._sp_.config.mms_client_data_callback.apply(
+                    window._sp_.config.mms_client_data_callback,
+                    arguments
+                );
+            } catch(e) {
+                console.log("mms_client_data_callback failed", e);
+            }
+        }
+    };
+
+    window._sp_.msg = window._sp_.msg || {};
+    window._sp_.msg.cmd = window._sp_.msg.cmd || [];
+    window._sp_.msg.cmd.push(function() {
+        for (var i = 0; i < window._sp_.mms.cmd.length; i++) {
+            try {
+                window._sp_.mms.cmd[i]();
+            } catch(e) {
+                console.error('Failed to execute cmd', e);
+            }
+        }
+
+        var queue = [];
+        queue.push = function(cmd) { try { cmd() } catch(e) { console.error('Failed to execute cmd queue', e); } };
+        window._sp_.mms.cmd = queue;
+    });
+
+    // MMS client 
+    function loadMessage(isBlocking) {
+        var targetingQueryParams = '';
+        for (var key in targetingParams) {
+            targetingQueryParams += '&' + encodeURIComponent('t[' + key + ']') + '=' + encodeURIComponent(targetingParams[key]);
+        }
+
+        var params = '?v=1&account_id=' + encodeURIComponent(window._sp_.config.account_id) +
+                        '&abp=' + encodeURIComponent(isBlocking) +
+                        '&referrer=' + encodeURIComponent(document.referrer) +
+                        targetingQueryParams +
+                        '&cdc=window._sp_.mms._internal.cdc1';
+   
+        var msgScriptUrl = '//' + window._sp_.config.mms_domain + '/mms/get_site_js' + params;
+        if (isBlocking) {
+            window._sp_.bootstrap(msgScriptUrl);
+        } else {
+            var script = document.createElement('script');
+            script.src = msgScriptUrl;
+            document.head.appendChild(script);
+        }
+    }
+})();
