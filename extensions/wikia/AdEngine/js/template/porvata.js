e2f7f0326788c2c8bb6b87cfe895d402d3cbbf41
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-11-21 13:59:57 +0100

    ADEN-4232 Update jsdoc

--- extensions/wikia/AdEngine/js/template/porvata.js
+++ extensions/wikia/AdEngine/js/template/porvata.js
@@ -21,8 +21,12 @@ define('ext.wikia.adEngine.template.porvata', [
 
 	/**
 	 * @param {object} params
-	 * @param {object} params.container - DOM element where player should be placed
-	 * @param {object} params.imageContainer - DOM element where is placed default image
+	 * @param {object} params.adContainer - DOM element where player should be placed
+	 * @param {object} params.slotName - Slot name key-value needed for VastUrlBuilder
+	 * @param {object} params.src - SRC key-value needed for VastUrlBuilder
+	 * @param {object} params.width - Player width
+	 * @param {object} params.height - Player height
+	 * @param {string} [params.onReady] - Callback executed once player is ready
 	 * @param {string} [params.vastUrl] - Vast URL (DFP URL with page level targeting will be used if not passed)
 	 */
 	function show(params) {
