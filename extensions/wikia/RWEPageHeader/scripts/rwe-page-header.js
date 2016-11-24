6ad34ce2b3c73f5953850f50db5be2cf0c1ed78b
idradm <robakadam@gmail.com>
2016-11-24 17:58:23 +0100

    WW-569 add tracking to links

--- extensions/wikia/RWEPageHeader/scripts/rwe-page-header.js
+++ extensions/wikia/RWEPageHeader/scripts/rwe-page-header.js
@@ -1,5 +1,13 @@
-require(['jquery'], function ($) {
+require(['wikia.window', 'jquery', 'wikia.tracker'], function (window, $, tracker) {
 	'use strict';
+
+	var track = function (data) {
+		tracker.track(window.Object.assign({
+			action: tracker.ACTIONS.CLICK,
+			category: 'rwe-page-header',
+			trackingMethod: 'analytics'
+		}, data));
+	};
 
 	$(function () {
 		$('.rwe-page-header--dropdown').hover(function () {
@@ -8,6 +16,12 @@ require(['jquery'], function ($) {
 		}, function () {
 			$(this).parent('.rwe-page-header__nav-element')
 				.removeClass('rwe-page-header__nav-element--active');
+		});
+
+		$('.rwe-page-header__nav-link').on('click', function (e) {
+			track({
+				label: $(this).data().tracking
+			});
 		});
 	});
 });
--- extensions/wikia/RWEPageHeader/templates/RWEPageHeader_index.php
+++ extensions/wikia/RWEPageHeader/templates/RWEPageHeader_index.php
@@ -2,7 +2,7 @@
 	<?= $app->renderView( 'WikiHeader', 'Wordmark' ) ?>
 	<ul class="rwe-page-header__nav">
 		<li class="rwe-page-header__nav-element">
-			<a class="rwe-page-header__nav-link rwe-page-header--dropdown" href="/">
+			<a class="rwe-page-header__nav-link rwe-page-header--dropdown" href="/" data-tracking="read">
 				<svg width="18" height="18" viewBox="0 0 18 18" class="wds-icon wds-icon-small rwe-page-header__icon" xmlns="http://www.w3.org/2000/svg">
 					<path fill-rule="evenodd" d="M12.063 2.875c-1.226 0-2.363.35-3.063.962-.7-.612-1.838-.962-3.063-.962C3.663 2.875 2 4.013 2 5.5v8.75c0 .525.35.875.875.875s.875-.35.875-.875c0-.262.787-.875 2.188-.875 1.4 0 2.187.613 2.187.875 0 .525.35.875.875.875s.875-.35.875-.875c0-.262.787-.875 2.188-.875 1.4 0 2.187.613 2.187.875 0 .525.35.875.875.875s.875-.35.875-.875V5.5c0-1.487-1.662-2.625-3.938-2.625zm-6.126 8.75c-.787 0-1.575.175-2.187.438V5.5c0-.263.787-.875 2.188-.875 1.4 0 2.187.612 2.187.875v6.563c-.612-.263-1.4-.438-2.188-.438zm8.313.438c-.613-.263-1.4-.438-2.188-.438-.787 0-1.574.175-2.187.438V5.5c0-.263.787-.875 2.188-.875 1.4 0 2.187.612 2.187.875v6.563z"></path>
 				</svg>
@@ -15,12 +15,12 @@
 		</li>
 
 		<li class="rwe-page-header__nav-element">
-			<a class="rwe-page-header__nav-link rwe-page-header--dropdown" href="/wiki/Special:Community">
+			<a class="rwe-page-header__nav-link rwe-page-header--dropdown" href="/wiki/Special:Community" data-tracking="participate">
 				<?= DesignSystemHelper::renderSvg(
 					'wds-icons-pencil',
 					'wds-icon wds-icon-small rwe-page-header__icon'
 				) ?>
-				<span class="rwe-page-header__nav-text">Contribute</span>
+				<span class="rwe-page-header__nav-text">Participate</span>
 				<?= DesignSystemHelper::renderSvg(
 					'wds-icons-dropdown-tiny',
 					'wds-icon wds-icon-tiny rwe-page-header__chevron'
@@ -29,7 +29,7 @@
 		</li>
 
 		<li class="rwe-page-header__nav-element">
-			<a class="rwe-page-header__nav-link" href="<?= $discussTabLink ?>">
+			<a class="rwe-page-header__nav-link" href="<?= $discussTabLink ?>" data-tracking="discuss">
 				<?= DesignSystemHelper::renderSvg(
 					'wds-icons-reply',
 					'wds-icon wds-icon-small rwe-page-header__icon'
@@ -39,7 +39,7 @@
 		</li>
 
 		<li class="rwe-page-header__nav-element">
-			<a class="rwe-page-header__nav-link" href="/wiki/Special:Search">
+			<a class="rwe-page-header__nav-link" href="/wiki/Special:Search" data-tracking="search">
 				<?= DesignSystemHelper::renderSvg(
 					'wds-icons-magnifying-glass',
 					'wds-icon wds-icon-small rwe-page-header__icon'
