a236c03d4fb0d75afe9ccd1f30093622b6c8f9b6
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-11-25 11:37:03 +0100

    search within nav

--- extensions/wikia/RWEPageHeader/templates/RWEPageHeader_index.php
+++ extensions/wikia/RWEPageHeader/templates/RWEPageHeader_index.php
@@ -37,53 +37,45 @@
 			</a>
 		</li>
 
-		<li class="rwe-page-header-nav__tab">
-			<a class="rwe-page-header-nav__link" href="/wiki/Special:Search" data-tracking="search">
-				<?= DesignSystemHelper::renderSvg(
-					'wds-icons-magnifying-glass',
-					'wds-icon wds-icon-small rwe-page-header-nav__icon'
-				) ?>
-				<span class="rwe-page-header-nav__text">Search</span>
-			</a>
+		<li id="searchFormWrapperRWE" class="rwe-page-header-nav__tab">
+			<form class="wds-global-navigation__search" action="<?= Sanitizer::encodeAttribute( $searchModel['module']['results']['url'] ); ?>">
+				<div id="searchInputWrapperRWE" class="wds-dropdown wds-global-navigation__search-input-wrapper">
+					<label class="wds-global-navigation__search-label">
+						<?= DesignSystemHelper::renderSvg(
+							'wds-icons-magnifying-glass',
+							'wds-icon wds-icon-small wds-global-navigation__search-label-icon'
+						) ?>
+						<input id="searchInputRWE"
+							   type="search"
+							   class="wds-global-navigation__search-input"
+							   name="<?= Sanitizer::encodeAttribute( $searchModel['module']['results']['param-name'] ); ?>"
+
+							   data-suggestions-url="<?= $searchModel['module']['suggestions']['url'] ?>"
+							   data-suggestions-param-name="<?= $searchModel['module']['suggestions']['param-name'] ?>"
+							   data-suggestions-tracking-label="rwe-<?= Sanitizer::encodeAttribute( $searchModel['module']['suggestions']['tracking_label'] ); ?>"
+
+							   data-active-placeholder="<?= DesignSystemHelper::renderText( $searchModel['module']['placeholder-active'] ); ?>"
+							   placeholder="<?= DesignSystemHelper::renderText( $searchModel['module']['placeholder-inactive'] ); ?>"
+							   autocomplete="off"
+						/>
+					</label>
+					<button class="wds-button wds-is-text wds-global-navigation__search-close" type="reset">
+						<?= DesignSystemHelper::renderSvg(
+							'wds-icons-cross',
+							'wds-icon wds-icon-small wds-global-navigation__search-close-icon',
+							wfMessage( 'global-navigation-search-cancel' )->escaped()
+						) ?>
+					</button>
+					<button class="wds-button wds-global-navigation__search-submit" type="submit" data-tracking-label="rwe-<?= Sanitizer::encodeAttribute( $searchModel['module']['results']['tracking_label'] ); ?>">
+						<?= DesignSystemHelper::renderSvg(
+							'wds-icons-arrow',
+							'wds-icon wds-icon-small wds-global-navigation__search-submit-icon'
+						) ?>
+					</button>
+				</div>
+			</form>
 		</li>
 	</ul>
 </div>
 
-<div id="searchFormWrapperRWE">
-	<form class="wds-global-navigation__search" action="<?= Sanitizer::encodeAttribute( $searchModel['module']['results']['url'] ); ?>">
-		<div id="searchInputWrapperRWE" class="wds-dropdown wds-global-navigation__search-input-wrapper">
-			<label class="wds-global-navigation__search-label">
-				<?= DesignSystemHelper::renderSvg(
-					'wds-icons-magnifying-glass',
-					'wds-icon wds-icon-small wds-global-navigation__search-label-icon'
-				) ?>
-				<input id="searchInputRWE"
-					   type="search"
-					   class="wds-global-navigation__search-input"
-					   name="<?= Sanitizer::encodeAttribute( $searchModel['module']['results']['param-name'] ); ?>"
 
-					   data-suggestions-url="<?= $searchModel['module']['suggestions']['url'] ?>"
-					   data-suggestions-param-name="<?= $searchModel['module']['suggestions']['param-name'] ?>"
-					   data-suggestions-tracking-label="rwe-<?= Sanitizer::encodeAttribute( $searchModel['module']['suggestions']['tracking_label'] ); ?>"
-
-					   data-active-placeholder="<?= DesignSystemHelper::renderText( $searchModel['module']['placeholder-active'] ); ?>"
-					   placeholder="<?= DesignSystemHelper::renderText( $searchModel['module']['placeholder-inactive'] ); ?>"
-					   autocomplete="off"
-				/>
-			</label>
-			<button class="wds-button wds-is-text wds-global-navigation__search-close" type="reset">
-				<?= DesignSystemHelper::renderSvg(
-					'wds-icons-cross',
-					'wds-icon wds-icon-small wds-global-navigation__search-close-icon',
-					wfMessage( 'global-navigation-search-cancel' )->escaped()
-				) ?>
-			</button>
-			<button class="wds-button wds-global-navigation__search-submit" type="submit" data-tracking-label="rwe-<?= Sanitizer::encodeAttribute( $searchModel['module']['results']['tracking_label'] ); ?>">
-				<?= DesignSystemHelper::renderSvg(
-					'wds-icons-arrow',
-					'wds-icon wds-icon-small wds-global-navigation__search-submit-icon'
-				) ?>
-			</button>
-		</div>
-	</form>
-</div>
