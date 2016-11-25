437409306336cee8e573112042ac0c7e531c488e
Konrad Pluta <kpluta@wikia-inc.com>
2016-11-25 19:17:34 +0100

    WW-570 frontend wizardry hacks

--- extensions/wikia/RWEPageHeader/scripts/rwe-page-header.js
+++ extensions/wikia/RWEPageHeader/scripts/rwe-page-header.js
@@ -10,26 +10,6 @@ require(['wikia.window', 'jquery', 'wikia.tracker'], function (window, $, tracke
 	};
 
 	$(function () {
-		$('.rwe-page-header-nav__element-dropdown').hover(function () {
-			$(this).addClass('rwe-page-header-nav__element--active');
-			$('.rwe-page-header-nav__dropdown[data-category=' +
-				$(this).children('.rwe-page-header-nav__link').data('tracking') + ']').show();
-		}, function () {
-			$(this).removeClass('rwe-page-header-nav__element--active');
-			$('.rwe-page-header-nav__dropdown[data-category=' +
-				$(this).children('.rwe-page-header-nav__link').data('tracking') + ']').hide();
-		});
-
-		$('.rwe-page-header-nav__dropdown').hover(function () {
-			$('*[data-tracking=' + $(this).data('category') + ']').parent('.rwe-page-header-nav__element')
-				.addClass('rwe-page-header-nav__element--active');
-			$(this).show();
-		}, function () {
-			$('*[data-tracking=' + $(this).data('category') + ']').parent('.rwe-page-header-nav__element')
-				.removeClass('rwe-page-header-nav__element--active');
-			$(this).hide();
-		});
-
 		$('.rwe-page-header-nav__link').on('click', function (e) {
 			track({
 				label: $(this).data().tracking
