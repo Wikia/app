e958bad49c2f65fef0b86f41e3d65232a5fa661b
idradm <robakadam@gmail.com>
2016-11-25 09:47:39 +0100

    WW-569 comment out unused params

--- extensions/wikia/RWEPageHeader/RWEPageHeaderHooks.class.php
+++ extensions/wikia/RWEPageHeader/RWEPageHeaderHooks.class.php
@@ -1,7 +1,7 @@
 <?php
 
 class RWEPageHeaderHooks {
-	public static function onBeforePageDisplay( \OutputPage $out, \Skin $skin ) {
+	public static function onBeforePageDisplay( /*\OutputPage $out, \Skin $skin */ ) {
 		\Wikia::addAssetsToOutput( 'rwe_page_header_scss' );
 		\Wikia::addAssetsToOutput( 'rwe_page_header_js' );
 
