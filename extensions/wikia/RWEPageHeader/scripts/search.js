129cdd7b5ac6dad95b0b120b64a5b158c827da07
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-11-25 09:51:30 +0100

    removing consol log

--- extensions/wikia/RWEPageHeader/scripts/search.js
+++ extensions/wikia/RWEPageHeader/scripts/search.js
@@ -21,7 +21,6 @@ $(function ($) {
 	}
 
 	function deactivateSearch() {
-		// console.log("!!!!!!!!!!!!!!!!!!!!!!!!");
 		$searchSubmit.prop('disabled', true);
 		$globalNav.removeClass(activeSearchClass);
 		$searchInput.attr('placeholder', placeholderText).val('');
