4a52a99d25e839375a5c4cea88239262581e5a01
idradm <robakadam@gmail.com>
2016-11-25 13:38:07 +0100

    WW-573 wordmark styling

--- extensions/wikia/RWEPageHeader/templates/RWEPageHeader_index.php
+++ extensions/wikia/RWEPageHeader/templates/RWEPageHeader_index.php
@@ -1,10 +1,15 @@
 <div class="rwe-page-header">
-	<?= $app->renderView( 'WikiHeader', 'Wordmark' ) ?>
+	<div class="rwe-page-header-wordmark">
+		<div class="rwe-page-haader-wordmark__shadow"></div>
+		<?= $app->renderView( 'WikiHeader', 'Wordmark' ) ?>
+	</div>
 	<ul class="rwe-page-header-nav">
 		<li class="rwe-page-header-nav__tab">
 			<a class="rwe-page-header-nav__link rwe-page-header--dropdown" href="/" data-tracking="read">
-				<svg width="18" height="18" viewBox="0 0 18 18" class="wds-icon wds-icon-small rwe-page-header-nav__icon" xmlns="http://www.w3.org/2000/svg">
-					<path fill-rule="evenodd" d="M12.063 2.875c-1.226 0-2.363.35-3.063.962-.7-.612-1.838-.962-3.063-.962C3.663 2.875 2 4.013 2 5.5v8.75c0 .525.35.875.875.875s.875-.35.875-.875c0-.262.787-.875 2.188-.875 1.4 0 2.187.613 2.187.875 0 .525.35.875.875.875s.875-.35.875-.875c0-.262.787-.875 2.188-.875 1.4 0 2.187.613 2.187.875 0 .525.35.875.875.875s.875-.35.875-.875V5.5c0-1.487-1.662-2.625-3.938-2.625zm-6.126 8.75c-.787 0-1.575.175-2.187.438V5.5c0-.263.787-.875 2.188-.875 1.4 0 2.187.612 2.187.875v6.563c-.612-.263-1.4-.438-2.188-.438zm8.313.438c-.613-.263-1.4-.438-2.188-.438-.787 0-1.574.175-2.187.438V5.5c0-.263.787-.875 2.188-.875 1.4 0 2.187.612 2.187.875v6.563z"></path>
+				<svg width="18" height="18" viewBox="0 0 18 18"
+					 class="wds-icon wds-icon-small rwe-page-header-nav__icon" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd"
+						  d="M12.063 2.875c-1.226 0-2.363.35-3.063.962-.7-.612-1.838-.962-3.063-.962C3.663 2.875 2 4.013 2 5.5v8.75c0 .525.35.875.875.875s.875-.35.875-.875c0-.262.787-.875 2.188-.875 1.4 0 2.187.613 2.187.875 0 .525.35.875.875.875s.875-.35.875-.875c0-.262.787-.875 2.188-.875 1.4 0 2.187.613 2.187.875 0 .525.35.875.875.875s.875-.35.875-.875V5.5c0-1.487-1.662-2.625-3.938-2.625zm-6.126 8.75c-.787 0-1.575.175-2.187.438V5.5c0-.263.787-.875 2.188-.875 1.4 0 2.187.612 2.187.875v6.563c-.612-.263-1.4-.438-2.188-.438zm8.313.438c-.613-.263-1.4-.438-2.188-.438-.787 0-1.574.175-2.187.438V5.5c0-.263.787-.875 2.188-.875 1.4 0 2.187.612 2.187.875v6.563z"></path>
 				</svg>
 				<span class="rwe-page-header-nav__text">Read</span>
 				<?= DesignSystemHelper::renderSvg(
@@ -15,7 +20,8 @@
 		</li>
 
 		<li class="rwe-page-header-nav__tab">
-			<a class="rwe-page-header-nav__link rwe-page-header--dropdown" href="/wiki/Special:Community" data-tracking="create">
+			<a class="rwe-page-header-nav__link rwe-page-header--dropdown" href="/wiki/Special:Community"
+			   data-tracking="create">
 				<?= DesignSystemHelper::renderSvg(
 					'wds-icons-pencil',
 					'wds-icon wds-icon-small rwe-page-header-nav__icon'
