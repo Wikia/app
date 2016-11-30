3a14640391c5514b462f66a2b9b06c924960fd69
jacek <jacek@wikia-inc.com>
2016-11-30 15:17:40 +0100

    ADEN-4246 pv_unique_id

--- extensions/wikia/AdEngine/js/AdInfoTracker.js
+++ extensions/wikia/AdEngine/js/AdInfoTracker.js
@@ -10,6 +10,8 @@ define('ext.wikia.adEngine.adInfoTracker',  [
 
 	var	logGroup = 'ext.wikia.adEngine.adInfoTracker';
 
+	window.adEnginePvUID = window.adEnginePvUID || generateUUID();
+
 	function prepareData(slot, status) {
 		if (!slot.container.firstChild || !slot.container.firstChild.dataset.gptPageParams) {
 			return;
@@ -24,7 +26,9 @@ define('ext.wikia.adEngine.adInfoTracker',  [
 
 		data = {
 			'pv': pageParams.pv,
+			'pv_unique_id': window.adEnginePvUID,
 			'country': pageParams.geo,
+			'time_bucket': (new Date()).getHours(),
 			'slot_size': slotSize && slotSize.length ? slotSize.join('x') : '',
 			'kv_s0': pageParams.s0,
 			'kv_s1': pageParams.s1,
@@ -33,6 +37,11 @@ define('ext.wikia.adEngine.adInfoTracker',  [
 			'kv_pos': slotParams.pos,
 			'kv_wsi': slotParams.wsi,
 			'kv_lang': pageParams.lang,
+			'kv_skin': pageParams.skin,
+			'kv_esrb': pageParams.esrb,
+			'kv_ref': pageParams.ref,
+			'kv_top': pageParams.top,
+			'kv_ah': pageParams.ah,
 			'bidder_won': '',
 			'bidder_1': slotPrices.indexExchange || '',
 			'bidder_2': slotPrices.appnexus || '',
@@ -47,6 +56,13 @@ define('ext.wikia.adEngine.adInfoTracker',  [
 		};
 
 		return data;
+	}
+
+	function generateUUID() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		});
 	}
 
 	function logSlotInfo(data) {
