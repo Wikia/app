7620184a0958aa32b07113f79081b904821d671b
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2016-12-01 17:15:56 +0100

    ADEN-4318 Test getBootstrapCode method

--- extensions/wikia/ARecoveryEngine/tests/ARecoveryModuleTest.php
+++ extensions/wikia/ARecoveryEngine/tests/ARecoveryModuleTest.php
@@ -2,16 +2,16 @@
 
 
 class ARecoveryModuleTest extends WikiaBaseTest {
+	const bootstrapCode = 'BOOTSTRAP_CODE';
 
-	public function getUser( $isLoggedIn ) {
+	public function getUser($isLoggedIn ) {
 		$stubs = $this->getMockBuilder( User::class )->getMock();
 		$stubs->method( 'isLoggedIn' )
 			->willReturn( $isLoggedIn );
 		return $stubs;
 	}
 
-	public function getData()
-	{
+	public function getData() {
 		// User is logged in, SPRecoveryEnabled, SPMMSEnabled, isDisabled (expected value)
 		return [
 			// User is not logged in
@@ -40,5 +40,24 @@ class ARecoveryModuleTest extends WikiaBaseTest {
 		$this->mockGlobalVariable( 'wgAdDriverEnableSourcePointMMS', $MMSEnabled );
 
 		$this->assertEquals( $expected, ARecoveryModule::isDisabled() );
+	}
+
+	/**
+	 * @dataProvider getData
+	 *
+	 * @param $userLoggedIn
+	 * @param $recoveryEnabled
+	 * @param $MMSEnabled
+	 * @param $isDisabled
+	 */
+	public function testSourcePointCode($userLoggedIn, $recoveryEnabled, $MMSEnabled, $isDisabled ) {
+		$this->mockGlobalVariable( 'wgUser', $this->getUser( $userLoggedIn ) );
+		$this->mockGlobalVariable( 'wgAdDriverEnableSourcePointRecovery', $recoveryEnabled );
+		$this->mockGlobalVariable( 'wgAdDriverEnableSourcePointMMS', $MMSEnabled );
+
+		$this->mockStaticMethod(WikiaApp::class, 'sendRequest', self::bootstrapCode);
+
+		$expectedValue = $isDisabled ? ARecoveryModule::disabledMessage : self::bootstrapCode;
+		$this->assertEquals( $expectedValue , ARecoveryModule::getSourcePointBootstrapCode() );
 	}
 }
