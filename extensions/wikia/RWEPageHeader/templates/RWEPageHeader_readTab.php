bdad6ca55bdd9d43bf7fd9edfc1feae6e6ee993b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-12-01 13:58:29 +0100

    sizes

--- extensions/wikia/RWEPageHeader/templates/RWEPageHeader_readTab.php
+++ extensions/wikia/RWEPageHeader/templates/RWEPageHeader_readTab.php
@@ -7,10 +7,12 @@
 					<li class="rwe-page-header-nav__dropdown-item rwe-page-header-nav__dropdown-first-level-item">
 						<div class="rwe-page-header-nav__dropdown-link-wrapper">
 							<a class="rwe-page-header-nav__link" data-tracking="first-level"<? if ( !empty( $menuNode0[ NavigationModel::SPECIAL ] ) ):
-								?> data-extra="<?= $menuNode0[ NavigationModel::SPECIAL ] ?>"<? endif
-							?> href="<?= $menuNode0[ NavigationModel::HREF ]
-							?>"><?= $menuNode0[ NavigationModel::TEXT ] ?><? if ( isset( $menuNodes[ $level0 ][ NavigationModel::CHILDREN ] ) ):
-									?><?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny' ); ?><? endif ?></a>
+									?> data-extra="<?= $menuNode0[ NavigationModel::SPECIAL ] ?>"<? endif
+								?> href="<?= $menuNode0[ NavigationModel::HREF ]
+								?>"><?= $menuNode0[ NavigationModel::TEXT ] ?>
+							</a>
+							<? if ( isset( $menuNodes[ $level0 ][ NavigationModel::CHILDREN ] ) ):
+								?><?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny' ); ?><? endif ?>
 						</div>
 
 						<? if ( isset( $menuNodes[ $level0 ][ NavigationModel::CHILDREN ] ) ): ?>
@@ -23,11 +25,13 @@
 									<li class="rwe-page-header-nav__dropdown-item rwe-page-header-nav__dropdown-second-level-item">
 										<div class="rwe-page-header-nav__dropdown-link-wrapper">
 											<a class="rwe-page-header-nav__link" data-tracking="second-level"<? if ( !empty( $menuNode1[ NavigationModel::SPECIAL ] ) ):
-												?> data-extra="<?= $menuNode1[ NavigationModel::SPECIAL ] ?>"<? endif
-											?> href="<?= $menuNode1[ NavigationModel::HREF ] ?>"<? if ( !empty( $menuNode1[ NavigationModel::CANONICAL_NAME ] ) ):
-												?> data-canonical="<?= strtolower( $menuNode1[ NavigationModel::CANONICAL_NAME ] ) ?>"<? endif
-											?>><?= $menuNode1[ NavigationModel::TEXT ] ?><? if ( $hasChildNodes ):
-													?><?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny' ); ?><? endif ?></a>
+													?> data-extra="<?= $menuNode1[ NavigationModel::SPECIAL ] ?>"<? endif
+												?> href="<?= $menuNode1[ NavigationModel::HREF ] ?>"<? if ( !empty( $menuNode1[ NavigationModel::CANONICAL_NAME ] ) ):
+													?> data-canonical="<?= strtolower( $menuNode1[ NavigationModel::CANONICAL_NAME ] ) ?>"<? endif
+												?>><?= $menuNode1[ NavigationModel::TEXT ] ?>
+											</a>
+											<? if ( $hasChildNodes ):
+												?><?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny' ); ?><? endif ?>
 										</div>
 
 										<? if ( $hasChildNodes ): ?>
