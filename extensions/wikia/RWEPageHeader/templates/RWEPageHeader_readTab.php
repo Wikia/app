2fb40b6bf66e99a3959d15b84205099cee06818b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-12-01 11:05:59 +0100

    enlarge first level menu links, bottom borders for links

--- extensions/wikia/RWEPageHeader/templates/RWEPageHeader_readTab.php
+++ extensions/wikia/RWEPageHeader/templates/RWEPageHeader_readTab.php
@@ -4,12 +4,14 @@
 			<? foreach ( $menuNodes[ 0 ][ NavigationModel::CHILDREN ] as $level0 ): ?>
 				<? $menuNode0 = $menuNodes[ $level0 ] ?>
 				<? if ( $menuNode0[ NavigationModel::TEXT ] ): ?>
-					<li class="rwe-page-header-nav__dropdown-first-level-item">
-						<a class="rwe-page-header-nav__link" data-tracking="first-level"<? if ( !empty( $menuNode0[ NavigationModel::SPECIAL ] ) ):
-							?> data-extra="<?= $menuNode0[ NavigationModel::SPECIAL ] ?>"<? endif
-						?> href="<?= $menuNode0[ NavigationModel::HREF ]
-						?>"><?= $menuNode0[ NavigationModel::TEXT ] ?><? if ( isset( $menuNodes[ $level0 ][ NavigationModel::CHILDREN ] ) ):
-								?><?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny' ); ?><? endif ?></a>
+					<li class="rwe-page-header-nav__dropdown-item rwe-page-header-nav__dropdown-first-level-item">
+						<div class="rwe-page-header-nav__dropdown-link-wrapper">
+							<a class="rwe-page-header-nav__link" data-tracking="first-level"<? if ( !empty( $menuNode0[ NavigationModel::SPECIAL ] ) ):
+								?> data-extra="<?= $menuNode0[ NavigationModel::SPECIAL ] ?>"<? endif
+							?> href="<?= $menuNode0[ NavigationModel::HREF ]
+							?>"><?= $menuNode0[ NavigationModel::TEXT ] ?><? if ( isset( $menuNodes[ $level0 ][ NavigationModel::CHILDREN ] ) ):
+									?><?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny' ); ?><? endif ?></a>
+						</div>
 
 						<? if ( isset( $menuNodes[ $level0 ][ NavigationModel::CHILDREN ] ) ): ?>
 							<ul class="rwe-page-header-nav__dropdown-second-level">
@@ -18,23 +20,27 @@
 									$menuNode1 = $menuNodes[ $level1 ];
 									$hasChildNodes = isset( $menuNode1[ NavigationModel::CHILDREN ] );
 									?>
-									<li class="rwe-page-header-nav__dropdown-second-level-item">
-										<a class="rwe-page-header-nav__link" data-tracking="second-level"<? if ( !empty( $menuNode1[ NavigationModel::SPECIAL ] ) ):
-											?> data-extra="<?= $menuNode1[ NavigationModel::SPECIAL ] ?>"<? endif
-										?> href="<?= $menuNode1[ NavigationModel::HREF ] ?>"<? if ( !empty( $menuNode1[ NavigationModel::CANONICAL_NAME ] ) ):
-											?> data-canonical="<?= strtolower( $menuNode1[ NavigationModel::CANONICAL_NAME ] ) ?>"<? endif
-										?>><?= $menuNode1[ NavigationModel::TEXT ] ?><? if ( $hasChildNodes ):
-												?><?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny' ); ?><? endif ?></a>
+									<li class="rwe-page-header-nav__dropdown-item rwe-page-header-nav__dropdown-second-level-item">
+										<div class="rwe-page-header-nav__dropdown-link-wrapper">
+											<a class="rwe-page-header-nav__link" data-tracking="second-level"<? if ( !empty( $menuNode1[ NavigationModel::SPECIAL ] ) ):
+												?> data-extra="<?= $menuNode1[ NavigationModel::SPECIAL ] ?>"<? endif
+											?> href="<?= $menuNode1[ NavigationModel::HREF ] ?>"<? if ( !empty( $menuNode1[ NavigationModel::CANONICAL_NAME ] ) ):
+												?> data-canonical="<?= strtolower( $menuNode1[ NavigationModel::CANONICAL_NAME ] ) ?>"<? endif
+											?>><?= $menuNode1[ NavigationModel::TEXT ] ?><? if ( $hasChildNodes ):
+													?><?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny' ); ?><? endif ?></a>
+										</div>
 
 										<? if ( $hasChildNodes ): ?>
 											<ul class="rwe-page-header-nav__dropdown-third-level">
 												<? foreach ( $menuNode1[ NavigationModel::CHILDREN ] as $level2 ): ?>
 													<? $menuNode2 = $menuNodes[ $level2 ] ?>
-													<li class="rwe-page-header-nav__dropdown-third-level-item">
-														<a class="rwe-page-header-nav__link" data-tracking="third-level"<? if ( !empty( $menuNode2[ NavigationModel::SPECIAL ] ) ):
-															?> data-extra="<?= $menuNode2[ NavigationModel::SPECIAL ] ?>"<? endif
-														?> href="<?= $menuNode2[ NavigationModel::HREF ]
-														?>"><?= $menuNode2[ NavigationModel::TEXT ] ?></a>
+													<li class="rwe-page-header-nav__dropdown-item rwe-page-header-nav__dropdown-third-level-item">
+														<div class="rwe-page-header-nav__dropdown-link-wrapper">
+															<a class="rwe-page-header-nav__link" data-tracking="third-level"<? if ( !empty( $menuNode2[ NavigationModel::SPECIAL ] ) ):
+																?> data-extra="<?= $menuNode2[ NavigationModel::SPECIAL ] ?>"<? endif
+															?> href="<?= $menuNode2[ NavigationModel::HREF ]
+															?>"><?= $menuNode2[ NavigationModel::TEXT ] ?></a>
+														</div>
 													</li>
 												<? endforeach ?>
 											</ul>
