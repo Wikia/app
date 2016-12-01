b00e91cb58c9ec9789d7246cde59239bd39943c0
Paul Oslund <poslund@wikia-inc.com>
2016-12-01 15:47:45 +0000

    RECIRC-236: Add custom code for Liftigniter to MIX test

--- extensions/wikia/Recirculation/js/experiments/mix.js
+++ extensions/wikia/Recirculation/js/experiments/mix.js
@@ -4,6 +4,7 @@ require([
 	'wikia.log',
 	'ext.wikia.recirculation.utils',
 	'ext.wikia.recirculation.discussions',
+	'ext.wikia.recirculation.helpers.liftigniter',
 	require.optional('videosmodule.controllers.rail')
 ], function (
 	$,
@@ -11,6 +12,7 @@ require([
 	log,
 	utils,
 	discussions,
+	liftigniter,
 	videosModule
 ) {
 	/**
@@ -22,7 +24,7 @@ require([
 	 *  var recircExperiment = [
 	 *  	{
 	 *  		id: 'unique identifier - only used if you want to use data across multiple views',
-	 *  		sorce: 'the id you want to use for data - only used if you want to use data across multiple views',
+	 *  		source: 'the id you want to use for data - only used if you want to use data across multiple views',
 	 *  		placement: 'name of the view',
 	 *  		helper: 'name of the helper',
 	 *  		options: {
@@ -37,8 +39,10 @@ require([
 	var recircExperiment = w.recircExperiment || false,
 		experimentName = 'RECIRCULATION_MIX',
 		logGroup = 'ext.wikia.recirculation.experiments.mix',
-		views = {},
-		saved = {};
+		views = {}, // Each view holds an array of promises used to gather data for that view
+		saved = {}, // Saved data to be used across multiple views
+		completed = [], // An array of promises to keep track of which views have completed rendering
+		liftigniterHelpers = {};
 
 	if (!recircExperiment || w.wgContentLanguage !== 'en') {
 		if (videosModule) {
@@ -58,17 +62,31 @@ require([
 		}
 
 		if (experiment.helper) {
-			var helperString = 'ext.wikia.recirculation.helpers.' + experiment.helper;
-
-			require([helperString], function (helper) {
-				helper(experiment.options).loadData()
+			if (experiment.helper === 'liftigniter') {
+				// Liftigniter works differently than the other helpers in that it doesn't actually do anything until
+				// 'fetch' is called. Because fetch can only be called once per page, we need to make sure to use
+				// `loadData` before it is called at the bottom of this script.
+				liftigniterHelpers[experiment.id] = liftigniter(experiment.options);
+				liftigniterHelpers[experiment.id].loadData()
 					.done(function (data) {
 						deferred.resolve(data);
 					})
 					.fail(function (err) {
 						deferred.reject(err);
 					});
-			});
+			} else {
+				var helperString = 'ext.wikia.recirculation.helpers.' + experiment.helper;
+
+				require([helperString], function (helper) {
+					helper(experiment.options).loadData()
+						.done(function (data) {
+							deferred.resolve(data);
+						})
+						.fail(function (err) {
+							deferred.reject(err);
+						});
+				});
+			}
 		}
 
 		if (experiment.source && saved[experiment.source]) {
@@ -87,12 +105,15 @@ require([
 		views[experiment.placement].push(deferred);
 	});
 
-	$.each(views, function (key, value) {
-		var viewString = 'ext.wikia.recirculation.views.' + key;
+	$.each(views, function (key, promises) {
+		var viewString = 'ext.wikia.recirculation.views.' + key,
+			deferred = $.Deferred();
+
+		completed.push(deferred);
 
 		log('Initializing View: ' + key, 'info', logGroup);
 		require([viewString], function (viewFactory) {
-			$.when.apply($, value)
+			$.when.apply($, promises)
 				.done(function () {
 					var view = viewFactory(),
 						args = Array.prototype.slice.call(arguments),
@@ -118,9 +139,12 @@ require([
 					data.items = utils.ditherResults(data.items, 4);
 
 					view.render(data)
-						.then(view.setupTracking(experimentName));
+						.then(view.setupTracking(experimentName))
+						.then(deferred.resolve);
 				})
-				.fail(handleError(key));
+				.fail(handleError(key), function() {
+					deferred.reject(key);
+				});
 		});
 	});
 
