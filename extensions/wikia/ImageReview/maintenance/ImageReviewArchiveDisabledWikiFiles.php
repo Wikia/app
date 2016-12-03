3a95d75588a6a33e5b4d737b8e60623d04cc165c
James Sutterfield <james@wikia-inc.com>
2016-12-02 18:26:40 -0800

    SUS-541 call the right method on the right object

--- extensions/wikia/ImageReview/maintenance/ImageReviewArchiveDisabledWikiFiles.php
+++ extensions/wikia/ImageReview/maintenance/ImageReviewArchiveDisabledWikiFiles.php
@@ -9,7 +9,7 @@ $res = $dbr->select( 'city_list', 'city_id', array( 'city_public' => 0 ) );
 while ( $row = $dbr->fetchRow( $res ) ) {
 	echo "Archiving images from wiki ID {$row->city_id}...";
 
-	ImageListGetter::onWikiFactoryPublicStatusChange( 0, $row->city_id, '' );
+	ImageReviewHooks::onWikiFactoryPublicStatusChange( 0, $row->city_id, '' );
 
 	echo "[done]\n";
 }
