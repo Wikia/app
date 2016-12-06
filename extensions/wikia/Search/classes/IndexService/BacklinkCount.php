52605a21fc062e5ebb2725260c46b63ec66d2048
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-12-06 10:49:43 +0100

    SUS | Cleanup code in Search/IndexService

--- extensions/wikia/Search/classes/IndexService/BacklinkCount.php
+++ extensions/wikia/Search/classes/IndexService/BacklinkCount.php
@@ -1,26 +1,28 @@
 <?php
 /**
  * Class definition for \Wikia\Search\Indexservice\BacklinkCount
+ *
  * @author relwell
  */
 namespace Wikia\Search\IndexService;
 /**
- * This class provides backlink info for a page  
+ * This class provides backlink info for a page
+ *
  * @author relwell
  * @package Search
  * @subpackage IndexService
  */
-class BacklinkCount extends AbstractService
-{
+class BacklinkCount extends AbstractService {
 	/**
 	 * Returns an array keyed for a solr schema field for backlinks
+	 *
 	 * @see \Wikia\Search\IndexService\AbstractService::execute()
 	 * @return array
 	 */
 	public function execute() {
-		wfProfileIn(__METHOD__);
-		$response = array( 'backlinks' => $this->getService()->getBacklinksCountFromPageId( $this->currentPageId ) );
-		wfProfileOut(__METHOD__);
+
+		$response = [ 'backlinks' => $this->getService()->getBacklinksCountFromPageId( $this->currentPageId ) ];
+
 		return $response;
 	}
 }
--- extensions/wikia/Search/classes/IndexService/BacklinkText.php
+++ extensions/wikia/Search/classes/IndexService/BacklinkText.php
@@ -3,34 +3,35 @@
  * Class definition for Wikia\Search\IndexService\BacklinkText
  */
 namespace Wikia\Search\IndexService;
+
 use Wikia\Search\Config, \Wikia\Search\QueryService\Factory;
+
 /**
- * This class populates a document's backlinks 
+ * This class populates a document's backlinks
  * based on outgoing links pointing to that
  * document ID in other Solr documents.
+ *
  * @author relwell
  */
-class BacklinkText extends AbstractService
-{
-	
+class BacklinkText extends AbstractService {
+
 	/**
 	 * Accesses backlinks. Iterate over a result set of documents with outbound links, and store.
 	 * (non-PHPdoc)
+	 *
 	 * @see \Wikia\Search\IndexService\AbstractService::execute()
 	 */
 	public function execute() {
 		$docIdSeparated = $this->getCurrentDocumentId() . ' |';
 		$config = new Config;
-		$config->setDirectLuceneQuery( true )
-		       ->setQuery( sprintf( 'outbound_links_txt:"%s"', $docIdSeparated ) );
+		$config->setDirectLuceneQuery( true )->setQuery( sprintf( 'outbound_links_txt:"%s"', $docIdSeparated ) );
 		$factory = new Factory;
-		
+
 		$backlinks = [];
 		do {
 			$offset = 0;
 			$limit = 100;
-			$config->setStart( $offset )
-			       ->setLimit( $limit );
+			$config->setStart( $offset )->setLimit( $limit );
 			$resultSet = $factory->getFromConfig( $config )->search();
 			foreach ( $resultSet as $result ) {
 				foreach ( $result['outbound_links_txt'] as $link ) {
@@ -42,7 +43,8 @@ class BacklinkText extends AbstractService
 			$offset = $limit;
 			$limit = $offset + 100;
 		} while ( $config->getResultsFound() > $limit );
+
 		return [ 'backlinks_txt' => $backlinks ];
 	}
-	
+
 }
