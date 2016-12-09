2edc159ec4865eade5245e96aa3a15b73817ad11
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-12-09 11:17:42 +0100

    ADEN-4362 Extract progressBar module

---
+++ extensions/wikia/AdEngine/js/spec/video/player/ui/progressBar.spec.js
@@ -0,0 +1,72 @@
+/*global describe, it, expect, modules, beforeEach*/
+describe('ext.wikia.adEngine.video.player.ui.progressBar', function () {
+	'use strict';
+
+	function noop () {}
+
+	var mocks = {
+			doc: {
+				createElement: function () {
+					return {
+						appendChild: noop,
+						classList: {
+							add: noop
+						},
+						style: {}
+					};
+				}
+			},
+			log: noop,
+			video: {
+				getRemainingTime: function () {
+					return 5;
+				}
+			},
+			videoBroken: {
+				getRemainingTime: noop
+			}
+		},
+		progressBar;
+
+	function getModule() {
+		return modules['ext.wikia.adEngine.video.player.ui.progressBar'](
+			mocks.doc,
+			mocks.log
+		);
+	}
+
+	beforeEach(function () {
+		var progressBarFactory = getModule();
+
+		progressBar = progressBarFactory.create();
+		mocks.log.levels = {};
+	});
+
+	it('Creates progressBar object with correct interface', function () {
+		expect(typeof progressBar.update).toBe('function');
+		expect(typeof progressBar.pause).toBe('function');
+	});
+
+	it('Updates currentTime element with transitionDuration and width when video returns proper time', function () {
+		progressBar.update(mocks.video);
+
+		expect(progressBar.currentTime.style.transitionDuration).toBe('5s');
+		expect(progressBar.currentTime.style.width).toBe('100%');
+	});
+
+	it('Updates currentTime element with width when video has broken time time', function () {
+		progressBar.update(mocks.videoBroken);
+
+		expect(progressBar.currentTime.style.transitionDuration).toBe(undefined);
+		expect(progressBar.currentTime.style.width).toBe('0');
+	});
+
+	it('Pauses currentTime by setting current width', function () {
+		progressBar.currentTime.offsetWidth = 150;
+		progressBar.container.offsetWidth = 200;
+
+		progressBar.pause();
+
+		expect(progressBar.currentTime.style.width).toBe('75%');
+	});
+});
---
+++ extensions/wikia/AdEngine/js/video/player/ui/progressBar.js
@@ -0,0 +1,45 @@
+/*global define*/
+define('ext.wikia.adEngine.video.player.ui.progressBar', [
+	'wikia.document',
+	'wikia.log'
+], function (doc, log) {
+	'use strict';
+
+	var logGroup = 'ext.wikia.adEngine.video.player.ui.progressBar';
+
+	function create() {
+		var progressBar = doc.createElement('div'),
+			currentTime = doc.createElement('div');
+
+		progressBar.classList.add('progress-bar');
+		currentTime.classList.add('current-time');
+
+		progressBar.appendChild(currentTime);
+
+		return {
+			container: progressBar,
+			currentTime: currentTime,
+			pause: function () {
+				var currentStatus = (this.currentTime.offsetWidth / this.container.offsetWidth * 100) + '%';
+
+				this.currentTime.style.width = currentStatus;
+				log(['pause', currentStatus], log.levels.debug, logGroup);
+			},
+			update: function (video) {
+				var remainingTime = video.getRemainingTime();
+
+				if (remainingTime) {
+					this.currentTime.style.transitionDuration = remainingTime + 's';
+					this.currentTime.style.width = '100%';
+				} else {
+					this.currentTime.style.width = '0';
+				}
+				log(['update, remaining time:', remainingTime], log.levels.debug, logGroup);
+			}
+		};
+	}
+
+	return {
+		create: create
+	};
+});
