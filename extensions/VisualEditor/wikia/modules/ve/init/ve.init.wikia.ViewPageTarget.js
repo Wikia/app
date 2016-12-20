8547650cf7d3bdd46948e94d8b0b6eef4a5a397d
Igor Rogatty <igor@wikia-inc.com>
2016-12-20 18:16:25 +0100

    XW-2385 | add `log in` link to VE and refresh edit token on auth success

--- extensions/VisualEditor/wikia/modules/ve/init/ve.init.wikia.ViewPageTarget.js
+++ extensions/VisualEditor/wikia/modules/ve/init/ve.init.wikia.ViewPageTarget.js
@@ -473,7 +473,17 @@ ve.init.wikia.ViewPageTarget.prototype.getAnonWarning = function () {
 	if ( !this.$anonWarning ) {
 		this.$anonWarning = this.$( '<div>' )
 			.addClass( 've-ui-wikia-anon-warning' )
-			.text( ve.msg( 'visualeditor-anon-warning' ) );
+			.text( ve.msg( 'visualeditor-anon-warning' ) )
+			.append(
+				this.$( '<a>' )
+					.text( ve.msg( 'visualeditor-anon-log-in' ) )
+					.on( 'click', function () {
+						window.wikiaAuthModal.load( {
+							forceLogin: true,
+							onAuthSuccess: ve.init.wikia.ViewPageTarget.prototype.onAuthSuccess.bind( this )
+						} );
+					}.bind( this ) )
+			);
 	}
 	return this.$anonWarning;
 };
@@ -482,4 +492,22 @@ ve.init.wikia.ViewPageTarget.prototype.setupAnonWarning = function () {
 	if ( mw.user.anonymous() ) {
 		this.getAnonWarning().insertAfter( this.getToolbar().$element );
 	}
+};
+
+ve.init.wikia.ViewPageTarget.prototype.onAuthSuccess = function () {
+	var getEditTokenUrl = '/api.php?action=query&prop=info&titles=' +
+		window.wgPageName +
+		'&intoken=edit&format=json';
+
+	this.$anonWarning.remove();
+	$.ajax( getEditTokenUrl )
+		.done( function ( response ) {
+			var editToken = response.query.pages[window.wgArticleId].edittoken;
+
+			mw.user.tokens.set( 'editToken', editToken );
+			// TODO
+			// If we want to get the real user name, we need to make two API calls, to whoami and user-attribute service
+			// Do we really want to do this? Do we need it for anything?
+			mw.config.set( 'wgUserName', 'VE_LOGGED_IN' );
+		} );
 };
