ce36707769c8a3074da5befc0dbb98128e7d6470
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-01-12 13:46:34 +0100

    ConfirmEmail | Code Style

--- extensions/wikia/UserLogin/EmailConfirmationController.class.php
+++ extensions/wikia/UserLogin/EmailConfirmationController.class.php
@@ -5,13 +5,13 @@ class EmailConfirmationController extends WikiaController {
 	 * Attempt to confirm the user's email address and show success or failure
 	 */
 	public function postEmailConfirmation() {
-		$user = User::newFromConfirmationCode( $this->wg->request->getVal('token') );
+		$user = User::newFromConfirmationCode( $this->wg->request->getVal( 'token' ) );
 		if ( is_object( $user ) ) {
 			$user->confirmEmail();
 			$user->saveSettings();
 			wfRunHooks( 'ConfirmEmailComplete', array( &$user ) );
 			$this->response->setCode( 200 );
-			$this->response->setVal('username', $user->getName());
+			$this->response->setVal( 'username', $user->getName() );
 		} else {
 			//User with such token not found
 			$this->response->setCode( 404 );
