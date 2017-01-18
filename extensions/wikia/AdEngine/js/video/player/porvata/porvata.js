b310a669faf66b501179f5dfcb1cdd24ebc68a36
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-01-18 10:53:17 +0100

    ADEN-4516 Fix comments

--- extensions/wikia/AdEngine/js/video/player/porvata/porvata.js
+++ extensions/wikia/AdEngine/js/video/player/porvata/porvata.js
@@ -41,7 +41,7 @@ define('ext.wikia.adEngine.video.player.porvata', [
 					// Don't resume when video was paused manually
 					} else if (isVisible && autoPaused) {
 						video.resume();
-						// Pause video once it's out of viewport and set autoPaused to distinguish manual and auto pause
+					// Pause video once it's out of viewport and set autoPaused to distinguish manual and auto pause
 					} else if (!isVisible && video.isPlaying()) {
 						video.pause();
 						autoPaused = true;
