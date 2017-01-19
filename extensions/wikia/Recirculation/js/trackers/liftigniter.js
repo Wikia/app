57658402c351f7afb436cbfc58c06d1d13a3c053
nmonterroso <nmonterroso@users.noreply.github.com>
2017-01-19 09:54:12 -0800

    adding global context object to LI config

--- extensions/wikia/Recirculation/js/trackers/liftigniter.js
+++ extensions/wikia/Recirculation/js/trackers/liftigniter.js
@@ -3,6 +3,40 @@ arguments)};w[v].l=1*new Date();e=d.createElement(s),r=d.getElementsByTagName(s)
 e.src=p+'?ts='+(+new Date()/3600000|0);
 r.parentNode.insertBefore(e,r)})(window,document,'script','//cdn.fandommetrics.com/l9ehhrb6mtv75bp2.js','$p');
 
+function getLiftIgniterGlobalContext() {
+    var context = {};
+    if (window.ads && window.ads.context && window.ads.context.targeting) {
+        var targeting = window.ads.context.targeting;
+
+        context = {
+            _wCategory: targeting.wikiCategory,
+            _wName: targeting.wikiDbName,
+            _wTop: targeting.wikiIsTop1000,
+            _wikiLanguage: targeting.wikiLanguage,
+            _wVert: targeting.wikiVertical,
+            _pType: targeting.pageType,
+            _pName: targeting.pageName
+        };
+
+        if (targeting.wikiCustomKeyValues) {
+            targeting.wikiCustomKeyValues
+                .split(';')
+                .forEach(function(keyVal) {
+                    var parts = keyVal.split('=');
+                    var key = '_'+parts[0];
+                    if (!context[key]) {
+                        context[key] = [parts[1]];
+                    } else {
+                        context[key].push(parts[1]);
+                    }
+                })
+        }
+    }
+
+    return context;
+}
+
+
 $p("init", "l9ehhrb6mtv75bp2", {
     config: {
         sdk: {
@@ -13,7 +47,8 @@ $p("init", "l9ehhrb6mtv75bp2", {
         },
         inventory: {
             inventoryServer: "//api.fandommetrics.com"
-        }
+        },
+        globalCtx: getLiftIgniterGlobalContext()
     }
 });
 $p("send", "pageview");
