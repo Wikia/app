7a7525e22aa3b0f6a032b4cfd163e9464232efee
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-01-23 14:57:49 +0100

    ConfirmEmail | Fixing url for email confirmation by email

--- includes/User.php
+++ includes/User.php
@@ -3883,7 +3883,7 @@ class User implements JsonSerializable {
 		global $wgEnableNewAuthModal;
 
 		if ( $wgEnableNewAuthModal ) {
-			return "/confirm-email?token=$token";
+			return WikiFactory::getLocalEnvURL( "http://www.wikia.com/confirm-email?token=$token" );
 		}
 
 		// Hack to bypass localization of 'Special:'
