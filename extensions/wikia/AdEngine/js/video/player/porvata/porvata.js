a37780478fd67d0dec2b0c47fc77f633f4f25064
mleczey <mleczey@gmail.com>
2017-01-26 10:17:43 +0100

    ADEN-4555 | fix: pause video second time and on, if less than 50% of video is visilbe

--- extensions/wikia/AdEngine/js/video/player/porvata/porvata.js
+++ extensions/wikia/AdEngine/js/video/player/porvata/porvata.js
@@ -11,7 +11,7 @@ define('ext.wikia.adEngine.video.player.porvata', [
 	function inject(params) {
 		var autoPlayed = false,
 			autoPaused = false,
-			viewportListener;
+			viewportListener = null;
 
 		log(['injecting porvata player', params], log.levels.debug, logGroup);
 
@@ -56,10 +56,14 @@ define('ext.wikia.adEngine.video.player.porvata', [
 					video.ima.setAutoPlay(false);
 					if (viewportListener) {
 						viewportObserver.removeListener(viewportListener);
+						viewportListener = null;
 					}
 				});
 				video.addEventListener('start', function () {
 					video.ima.getAdsManager().dispatchEvent('wikiaAdPlay');
+					if (!viewportListener) {
+						viewportListener = viewportObserver.addListener(params.container, inViewportCallback);
+					}
 				});
 				video.addEventListener('resume', function () {
 					video.ima.getAdsManager().dispatchEvent('wikiaAdPlay');
