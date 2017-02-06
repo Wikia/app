51d2aa442ef05490b8a1d46ee1c2e1ac09e25fb2
Weronika Rudnicka <wrudnicka@wikia-inc.com>
2017-02-06 14:40:17 +0100

    XW-2690 | visual adjumstments

--- extensions/wikia/PremiumDesignABTest/PremiumDesignABTest.setup.php
+++ extensions/wikia/PremiumDesignABTest/PremiumDesignABTest.setup.php
@@ -8,9 +8,9 @@ $wgPremiumDesignABTestVariants = [
 	// List of Spells
 	3581 => [
 		'letter' => 'A',
-		'video_title' => 'Top 5 Best Spells in the Wizarding World',
-		'video_time' => '2:36',
-		'video_url' => ''
+		'video_title' => 'Top 10 Harry Potter Spells We Wish Were Real',
+		'video_time' => '10:21',
+		'video_url' => 'https://www.youtube.com/watch?v=EMQtimSyBXI'
 	],
 	// Hogwart Houses
 	967 => [
@@ -22,7 +22,7 @@ $wgPremiumDesignABTestVariants = [
 	//Newton Scamander
 	2425 => [
 		'letter' => 'D',
-		'video_title' => 'Fantastic Beasts and Where to Find Them – A New Hero Featurette',
+		'video_title' => 'Newton Scamander',
 		'video_time' => '2:13',
 		'video_url' => 'https://www.youtube.com/watch?v=Ql9UxdyoL1c'
 	]
