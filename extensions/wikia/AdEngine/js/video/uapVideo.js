fcd54b4aa280334e5234a640864a5e6195d0782f
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-02-16 13:52:11 +0100

    ADEN-4720 Support multiple video trigger areas

--- extensions/wikia/AdEngine/js/video/uapVideo.js
+++ extensions/wikia/AdEngine/js/video/uapVideo.js
@@ -25,7 +25,7 @@ define('ext.wikia.adEngine.video.uapVideo', [
 		// On mercury splitLayoutVideoPosition and videoPlaceholderElement will be empty
 		// because we always display video in the same way there.
 		if (params.splitLayoutVideoPosition && params.videoPlaceholderElement) {
-			width = params.videoPlaceholderElement.width;
+			width = params.videoPlaceholderElement.offsetWidth;
 		}
 
 		return {
@@ -145,15 +145,23 @@ define('ext.wikia.adEngine.video.uapVideo', [
 		}
 
 		return loadedPlayer.then(function (video) {
+			function playTrigger() {
+				var videoSize = getVideoSize(videoContainer, params);
+				video.play(videoSize.width, videoSize.height);
+			}
+
 			win.addEventListener('resize', throttle(function () {
-				var size = getVideoSize(videoContainer, params);
-				video.resize(size.width, size.height);
+				var videoSize = getVideoSize(videoContainer, params);
+				video.resize(videoSize.width, videoSize.height);
 			}));
 
-			params.videoTriggerElement.addEventListener('click', function () {
-				var size = getVideoSize(videoContainer, params);
-				video.play(size.width, size.height);
-			});
+			if (params.videoTriggerElement) {
+				params.videoTriggerElement.addEventListener('click', playTrigger);
+			} else if (params.videoTriggers) {
+				params.videoTriggers.forEach(function (trigger) {
+					trigger.addEventListener('click', playTrigger);
+				});
+			}
 
 			return video;
 		});
@@ -166,7 +174,7 @@ define('ext.wikia.adEngine.video.uapVideo', [
 	 * @returns bool
 	 */
 	function isEnabled(params) {
-		return !!params.videoAspectRatio;
+		return !!params.videoAspectRatio && (params.videoTriggerElement || params.videoTriggers);
 	}
 
 	return {
