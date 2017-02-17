b2bcb2cc3914d9781e52964f0c976af11d7d3414
Karol Tatala <ktatala@wikia-inc.com>
2017-02-17 09:56:42 +0100

    XW-2711 | use escaped on wfMessage instead of htmlspecialchars

--- extensions/wikia/ArticleVideo/ArticleVideoController.class.php
+++ extensions/wikia/ArticleVideo/ArticleVideoController.class.php
@@ -8,6 +8,8 @@ class ArticleVideoController extends WikiaController {
 		$thumbnailPath = $wg->videoMVPArticles[$wg->cityId][$articleId]['thumbnailPath'];
 
 		$this->setVal( 'thumbnailUrl', $wg->extensionsPath . $thumbnailPath );
+
+		// TODO: replace it with DS icon when it's ready (XW-2824)
 		$this->setVal( 'closeIconUrl',
 			$wg->extensionsPath . '/wikia/ArticleVideo/images/close.svg' );
 		$this->setVal( 'videoPlayButtonUrl',
--- extensions/wikia/ArticleVideo/templates/ArticleVideo_index.php
+++ extensions/wikia/ArticleVideo/templates/ArticleVideo_index.php
@@ -3,15 +3,14 @@
 	<div class="video-container">
 		<div class="video-placeholder">
 			<img class="video-thumbnail" src="<?= $thumbnailUrl; ?>">
-			<!-- TODO: replace with DS tiny icon -->
 			<img src="<?= $closeIconUrl; ?>" class="close">
 			<svg class="spinner">
 				<circle cx="24" cy="24" r="22"></circle>
 			</svg>
 			<div class="video-details">
 				<div class="video-details-left">
-					<div class="video-time"><?= htmlspecialchars( wfMessage( 'articlevideo-watch',
-							$videoDetails['time'] ) ); ?></div>
+					<div class="video-time"><?= wfMessage( 'articlevideo-watch',
+							$videoDetails['time'] )->escaped(); ?></div>
 					<div class="video-title"><?= htmlspecialchars( $videoDetails['title'] ); ?></div>
 				</div>
 				<img src="<?= $videoPlayButtonUrl; ?>" class="video-play-button">
