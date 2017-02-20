e8dbd0093111f456fe4f741b091c0025ac50baad
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-02-20 11:44:04 +0100

    SUS-240: Removing unnecessary error message for wall

--- extensions/wikia/Wall/WallBaseController.class.php
+++ extensions/wikia/Wall/WallBaseController.class.php
@@ -172,9 +172,9 @@ class WallBaseController extends WikiaService {
 		$wallMessage = $this->getWallMessage();
 
 		if ( !( $wallMessage instanceof WallMessage ) ) {
-			$this->forward( 'WallBaseController', 'message_error' );
+			//$this->forward( 'WallBaseController', 'message_error' );
 			wfProfileOut( __METHOD__ );
-			return true;
+			return false;
 		}
 
 		$head = '';
@@ -618,13 +618,6 @@ class WallBaseController extends WikiaService {
 		} else {
 			$this->response->setVal( 'userBlocked', false );
 		}
-
-	}
-
-	/**
-	 * Renders Wall_message_error.php template
-	 */
-	public function message_error() {
 
 	}
 
