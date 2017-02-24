2c8c68878a961400a8d08cd2ec9712d816020a69
dianafa <diana.falkowska@gmail.com>
2017-02-24 16:11:54 +0100

    ADEN-4638
    add audienceNetwork adapter

---
+++ extensions/wikia/AdEngine/js/lookup/prebid/adapters/audienceNetwork.js
@@ -0,0 +1,74 @@
+/*global define*/
+define('ext.wikia.adEngine.lookup.prebid.adapters.audienceNetwork',[
+	'ext.wikia.adEngine.context.slotsContext',
+	'wikia.geo',
+	'wikia.instantGlobals',
+	'ext.wikia.adEngine.adContext'
+], function (slotsContext, geo, instantGlobals, adContext) {
+	'use strict';
+
+	var bidderName = 'audienceNetwork',
+	// only first size for each slot will be requested anyways
+		slots = {
+			mercury: {
+				MOBILE_TOP_LEADERBOARD: {
+					sizes: [
+						[320, 50]
+					],
+					placementId: '963689110376230_1245837502161388'
+				},
+				MOBILE_IN_CONTENT: {
+					sizes: [
+						[300, 250],
+						[320, 480]
+					],
+					placementId: '963689110376230_1245838625494609'
+				},
+				MOBILE_PREFOOTER: {
+					sizes: [
+						[300, 250],
+						[320, 50]
+					],
+					placementId: '963689110376230_1245839585494513'
+				}
+			}
+		};
+
+	function isEnabled() {
+		// TODO: add and set and check here instantGlobals.wgAdDriverAudienceNetworkBidderCountries
+		// https://wikia-inc.atlassian.net/browse/ADEN-4638
+		return adContext.getContext().targeting.skin === 'mercury';
+	}
+
+	function getSlots(skin) {
+		return slotsContext.filterSlotMap(slots[skin]);
+	}
+
+	function prepareAdUnit(slotName, config) {
+		return {
+			code: slotName,
+			sizes: config.sizes,
+			bids: [
+				{
+					bidder: bidderName,
+					params: {
+						// todo: remove after testing
+						testMode: true,
+						placementId: config.placementId
+					}
+				}
+			]
+		};
+	}
+
+	function getName() {
+		return bidderName;
+	}
+
+	return {
+		isEnabled: isEnabled,
+		getName: getName,
+		getSlots: getSlots,
+		prepareAdUnit: prepareAdUnit
+	};
+});
