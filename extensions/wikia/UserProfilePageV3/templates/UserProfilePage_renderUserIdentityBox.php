2e3eea0b7cbda414f42860149a94c423a5f93dbd
James Sutterfield <james@wikia-inc.com>
2017-02-23 18:59:01 -0800

    IRIS-3935 convert ids to classes

--- extensions/wikia/UserProfilePageV3/templates/UserProfilePage_renderUserIdentityBox.php
+++ extensions/wikia/UserProfilePageV3/templates/UserProfilePage_renderUserIdentityBox.php
@@ -190,8 +190,8 @@
 					<? endif; ?>
 				<? endif; ?>
 				<? if ( !empty( $user['bio'] ) ): ?>
-					<li id="discussion-bio"><?= wfMessage( 'user-identity-bio' )->rawParams( $user['bio'] )->parse(); ?></li>
-					<div id="discussion-bio-toggle" data-modal-title="<?= wfMessage( 'user-identity-bio-modal-title' )->escaped(); ?>">
+					<li class="discussion-bio"><?= wfMessage( 'user-identity-bio' )->rawParams( $user['bio'] )->parse(); ?></li>
+					<div class="discussion-bio-toggle" data-modal-title="<?= wfMessage( 'user-identity-bio-modal-title' )->escaped(); ?>">
 						<span>
 							[<?= wfMessage( 'user-identity-bio-show-more' )->escaped(); ?>]
 						</span>
