eea4f0ddc795f1c3746a1147bb08ed39e2f952a4
TK-999 <tk.999.wikia@gmail.com>
2017-02-27 19:27:17 +0100

    SUS-1716: Fix broken Wall tests

--- extensions/wikia/Wall/tests/WallMessageTest.php
+++ extensions/wikia/Wall/tests/WallMessageTest.php
@@ -51,7 +51,7 @@ class WallMessageTest extends WikiaBaseTest {
 			->method( 'entriesFromIds' )
 			->with( [ $slaveId ] )
 			->willReturn( [] );
-		$this->mockClass( CommentsIndex::class, $commentsIndexMock, 'singleton' );
+		$this->mockClass( CommentsIndex::class, $commentsIndexMock, 'getInstance' );
 		$this->mockStaticMethodWithCallBack( 'Title', 'newFromId', function( int $id ) use ( $masterData ) {
 			return $masterData[$id] !== null
 				? $this->mockClassWithMethods( 'Title', [ 'exists' => true ] )
