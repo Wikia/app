92aa42121eeb27656efd4e572de9693325e19ef4
Michał Turek <mturek@wikia-inc.com>
2017-03-03 15:29:53 +0100

    IRIS-4085 | The bubble no longer override by message notifications logic

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigationOnSiteNotifications_index.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigationOnSiteNotifications_index.php
@@ -1,19 +1,19 @@
-<div id="notificationsEntryPoint" class="wds-global-navigation__notifications-menu wds-global-navigation__notifications-menu wds-dropdown notifications-container">
-	<div class="wds-dropdown__toggle wds-global-navigation__dropdown-toggle" title="<?=wfMessage( 'global-navigation-notifications-title' )->escaped()?>">
+<div id="onSiteNotificationsEntryPoint"
+     class="wds-global-navigation__notifications-menu wds-global-navigation__notifications-menu wds-dropdown on-site-notifications-container">
+	<div class="wds-dropdown__toggle wds-global-navigation__dropdown-toggle"
+	     title="<?= wfMessage( 'global-navigation-notifications-title' )->escaped() ?>">
 		<div class="bubbles">
-			<div class="wds-global-navigation__notifications-menu-counter notifications-count"></div>
+			<div class="wds-global-navigation__notifications-menu-counter on-site-notifications-count">
+			</div>
 		</div>
-		<?= DesignSystemHelper::renderSvg(
-			'wds-icons-bell',
-			'wds-icon wds-icon-small',
-			wfMessage( 'global-navigation-notifications-title' )->escaped()
-		) ?>
-		<?= DesignSystemHelper::renderSvg(
-			'wds-icons-dropdown-tiny',
-			'wds-icon wds-icon-tiny wds-dropdown__toggle-chevron'
-		) ?>
+		<?= DesignSystemHelper::renderSvg( 'wds-icons-bell', 'wds-icon wds-icon-small',
+			wfMessage( 'global-navigation-notifications-title' )->escaped() ) ?>
+		<?= DesignSystemHelper::renderSvg( 'wds-icons-dropdown-tiny',
+			'wds-icon wds-icon-tiny wds-dropdown__toggle-chevron' ) ?>
 	</div>
-	<div id="on_site_notifications" class="wds-dropdown__content wds-is-right-aligned wds-global-navigation__dropdown-content">
-		<ul id="GlobalNavigationOnSiteNotifications" class="OnSiteNotifications global-nav-dropdown"></ul>
+	<div id="on_site_notifications"
+	     class="wds-dropdown__content wds-is-right-aligned wds-global-navigation__dropdown-content">
+		<ul id="GlobalNavigationOnSiteNotifications"
+		    class="OnSiteNotifications global-nav-dropdown"></ul>
 	</div>
 </div>
