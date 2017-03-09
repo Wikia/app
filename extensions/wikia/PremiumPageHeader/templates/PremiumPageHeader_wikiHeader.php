dd1169488b555b9d949dfb091b9ee9c147ea9314
Karol Tatala <ktatala@wikia-inc.com>
2017-03-09 14:12:43 +0100

    XW-2928 | createpage class added to add new page button

--- extensions/wikia/PremiumPageHeader/templates/PremiumPageHeader_wikiHeader.php
+++ extensions/wikia/PremiumPageHeader/templates/PremiumPageHeader_wikiHeader.php
@@ -8,7 +8,7 @@
 		<div class="pph-tally"><?= $tallyMsg ?></div>
 
 		<a href="<?= $addNewPageHref; ?>"
-		   class="pph-add-new-page">
+		   class="pph-add-new-page createpage">
 			<svg width="14" height="16" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg">
 				<path
 					d="M2.667 10H6v1.333H2.667V10zm0-3.333h8V8h-8V6.667zm0-3.334h8v1.334h-8V3.333zM.667 16H8v-4.667c0-.368.299-.666.667-.666h4.666v-10A.667.667 0 0 0 12.667 0h-12A.667.667 0 0 0 0 .667v14.666c0 .368.299.667.667.667z"/>
