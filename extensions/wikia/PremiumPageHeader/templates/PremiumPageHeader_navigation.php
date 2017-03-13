c76370e9d7ca394740b3d356c326a5278de1f205
idradm <robakadam@gmail.com>
2017-03-13 10:02:15 +0100

    XW-2774 | remove unnecessary icon classes

--- extensions/wikia/PremiumPageHeader/templates/PremiumPageHeader_navigation.php
+++ extensions/wikia/PremiumPageHeader/templates/PremiumPageHeader_navigation.php
@@ -26,7 +26,7 @@
 	<? endforeach; ?>
 	<li class="pph-local-nav-item-l1 pph-local-nav-container pph-local-nav-explore">
 		<a href="#">
-			<svg class="wds-icon wds-icon-small rwe-page-header-nav__icon" width="18" height="16" viewBox="0 0 18 16"
+			<svg class="wds-icon wds-icon-small" width="18" height="16" viewBox="0 0 18 16"
 				 xmlns="http://www.w3.org/2000/svg">
 				<path fill-rule="evenodd"
 					  d="M12.938 0C11.363 0 9.9.45 9 1.237 8.1.45 6.638 0 5.062 0 2.138 0 0 1.462 0 3.375v11.25c0 .675.45 1.125 1.125 1.125s1.125-.45 1.125-1.125c0-.338 1.013-1.125 2.813-1.125 1.8 0 2.812.787 2.812 1.125 0 .675.45 1.125 1.125 1.125s1.125-.45 1.125-1.125c0-.338 1.012-1.125 2.813-1.125 1.8 0 2.812.787 2.812 1.125 0 .675.45 1.125 1.125 1.125S18 15.3 18 14.625V3.375C18 1.462 15.863 0 12.937 0zM5.061 11.25a7.37 7.37 0 0 0-2.812.563V3.374c0-.338 1.013-1.125 2.813-1.125 1.8 0 2.812.787 2.812 1.125v8.438a7.37 7.37 0 0 0-2.813-.563zm10.688.563a7.37 7.37 0 0 0-2.813-.563 7.37 7.37 0 0 0-2.812.563V3.374c0-.338 1.012-1.125 2.813-1.125 1.8 0 2.812.787 2.812 1.125v8.438z"/>
@@ -48,7 +48,7 @@
 		<a href="<?= $discuss['href'] ?>" data-tracking="discuss">
 			<?= DesignSystemHelper::renderSvg(
 				'wds-icons-reply',
-				'wds-icon wds-icon-small pph-local-nav-discuss'
+				'wds-icon wds-icon-small'
 			) ?>
 			<?= $discuss['text'] ?>
 		</a>
