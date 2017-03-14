a0083e12b3879fd60ce120ae3dfa63b9243be185
idradm <robakadam@gmail.com>
2017-03-14 12:28:23 +0100

    XW-2774 | add edit main page button support, refactor page header controller

--- extensions/wikia/PremiumPageHeader/PremiumPageHeaderController.class.php
+++ extensions/wikia/PremiumPageHeader/PremiumPageHeaderController.class.php
@@ -2,6 +2,56 @@
 
 class PremiumPageHeaderController extends WikiaController {
 
+	public function articleHeader() {
+		$skinVars = $this->app->getSkinTemplateObj()->data;
+		$this->content_actions = $skinVars['content_actions'];
+
+		// edit button preparation
+		// action button (edit / view soruce) and dropdown for it
+		$this->prepareActionButton();
+		// dropdown actions
+		$this->dropdown = $this->getDropdownActions();
+
+		// comments/talk button
+		$commentsEnabled = $this->checkArticleComments();
+		$this->commentButtonMsg = $commentsEnabled ? 'oasis-page-header-comments' : 'oasis-page-header-talk';
+		$this->commentsLink = $this->getCommentsLink();
+
+		$this->setVal( 'displaytitle', $skinVars['displaytitle'] );
+		$this->setVal( 'title', $skinVars['title'] );
+
+		$categoryLinks = $this->getContext()->getOutput()->getCategoryLinks();
+		$normalCategoryLinks = $categoryLinks['normal'] ?? [];
+
+		$visibleCategoriesLimit = 4;
+		if ( count( $normalCategoryLinks ) > 4 ) {
+			$visibleCategoriesLimit = 3;
+		}
+		$visibleCategories = array_slice( $normalCategoryLinks, 0, $visibleCategoriesLimit );
+		$moreCategories = array_slice( $normalCategoryLinks, $visibleCategoriesLimit );
+
+		$this->setVal( 'visibleCategoriesLength', count( $visibleCategories ) );
+		$this->setVal( 'visibleCategories', $visibleCategories );
+		$this->setVal( 'moreCategoriesLength', count( $moreCategories ) );
+		$this->setVal( 'moreCategories', $moreCategories );
+		$this->setVal( 'curatedContentButton', $this->getEditMainPageButton() );
+
+		if ( $this->app->getSkinTemplateObj() ) {
+			$this->language_urls = $this->app->getSkinTemplateObj()->data['language_urls'];
+		} else {
+			$this->language_urls = [];
+		}
+		$this->language_list = null;
+		$this->widgetLanguages();
+	}
+
+	public function navigation() {
+		$this->setVal( 'data',
+			( new NavigationModel() )->getLocalNavigationTree( NavigationModel::WIKI_LOCAL_MESSAGE ) );
+		$this->setVal( 'explore', $this->getExplore() );
+		$this->setVal( 'discuss', $this->getDiscuss() );
+	}
+
 	public function wikiHeader() {
 		$themeSettings = new ThemeSettings();
 		$settings = $themeSettings->getSettings();
@@ -13,11 +63,223 @@ class PremiumPageHeaderController extends WikiaController {
 		$this->setVal( 'mainPageURL', Title::newMainPage()->getLocalURL() );
 	}
 
-	public function navigation() {
-		$this->setVal( 'data',
-			( new NavigationModel() )->getLocalNavigationTree( NavigationModel::WIKI_LOCAL_MESSAGE ) );
-		$this->setVal( 'explore', $this->getExplore() );
-		$this->setVal( 'discuss', $this->getDiscuss() );
+	/**
+	 * Use MW core variable to generate action button
+	 * Copied from PageHeaderController.class.php
+	 */
+	private function prepareActionButton() {
+
+		global $wgTitle, $wgUser, $wgRequest;
+
+		wfRunHooks( 'BeforePrepareActionButtons', [ $this, &$this->content_actions ] );
+
+		$isDiff = !is_null( $wgRequest->getVal( 'diff' ) );
+
+		// "Add topic" action
+		if ( isset( $this->content_actions['addsection'] ) ) {
+			// remove on diff pages (RT #72666)
+			if ( $isDiff ) {
+				unset( $this->content_actions['addsection'] );
+			}
+		}
+
+		// action button
+		# print_pre($this->content_actions);
+
+		// handle protected pages (they should have viewsource link and lock icon) - BugId:9494
+		if ( isset( $this->content_actions['viewsource'] ) &&
+			!$wgTitle->isProtected() &&
+			!$wgTitle->isNamespaceProtected( $wgUser ) &&
+			!$wgUser->isLoggedIn() /* VOLDEV-74: logged in users should see the viewsource button, not edit */
+		) {
+			// force login to edit page that is not protected
+			$this->content_actions['edit'] = $this->content_actions['viewsource'];
+			$this->content_actions['edit']['text'] = wfMessage( 'edit' )->text();
+			unset( $this->content_actions['viewsource'] );
+		}
+
+		// If cascade protected, show viewsource button - BugId:VE-89
+		if ( isset( $this->content_actions['edit'] ) && $wgTitle->isCascadeProtected() ) {
+			$this->content_actions['viewsource'] = $this->content_actions['edit'];
+			$this->content_actions['viewsource']['text'] = wfMessage( 'viewsource' )->text();
+			unset( $this->content_actions['edit'] );
+		}
+
+		// "Add topic"
+		if ( isset( $this->content_actions['addsection'] ) ) {
+			$action = $this->content_actions['addsection'];
+			$action['text'] = wfMsg( 'oasis-page-header-add-topic' );
+			$this->action = $action;
+
+			$this->actionImage = MenuButtonController::ADD_ICON;
+			$this->actionName = 'addtopic';
+		}  // ve-edit
+		else {
+			if ( isset( $this->content_actions['ve-edit'] ) && $this->content_actions['ve-edit']['main'] ) {
+				$this->action = $this->content_actions['ve-edit'];
+				$this->actionImage = MenuButtonController::EDIT_ICON;
+				$this->actionName = 've-edit';
+				unset( $this->content_actions['ve-edit'] );
+			} // edit
+			else {
+				if ( isset( $this->content_actions['edit'] ) ) {
+					$this->action = $this->content_actions['edit'];
+					$this->actionImage = MenuButtonController::EDIT_ICON;
+					$this->actionName = 'edit';
+					unset( $this->content_actions['edit'] );
+				} // view source
+				else {
+					if ( isset( $this->content_actions['viewsource'] ) ) {
+						$this->action = $this->content_actions['viewsource'];
+						$this->actionImage = MenuButtonController::LOCK_ICON;
+						$this->actionName = 'source';
+						unset( $this->content_actions['ve-edit'], $this->content_actions['edit'] );
+					}
+				}
+			}
+		}
+
+		# print_pre($this->action); print_pre($this->actionImage); print_pre($this->actionName);
+	}
+
+	/**
+	 * Get content actions for dropdown
+	 */
+	private function getDropdownActions() {
+		$ret = [];
+
+		$editActions = [];
+		if ( isset( $this->content_actions['edit'] ) ) {
+			array_push( $editActions, 'edit' );
+		}
+		if ( isset( $this->content_actions['ve-edit'] ) ) {
+			if ( $this->content_actions['ve-edit']['main'] ) {
+				array_unshift( $editActions, 've-edit' );
+			} else {
+				array_push( $editActions, 've-edit' );
+			}
+		}
+		if ( isset( $this->content_actions['formedit'] ) ) {
+			array_push( $editActions, 'formedit' ); // SUS-533
+		}
+
+		// items to be added to "edit" dropdown
+		$actions = array_merge( $editActions,
+			[ 'history', 'move', 'protect', 'unprotect', 'delete', 'undelete', 'replace-file' ] );
+
+		// Enable to modify actions list on dropdown
+		wfRunHooks( 'PageHeaderDropdownActions', [ &$actions ] );
+
+		foreach ( $actions as $action ) {
+			if ( isset( $this->content_actions[$action] ) ) {
+				$ret[$action] = $this->content_actions[$action];
+			}
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * Copied from CommentsLikesController.class.php
+	 * Are article comments enabled for context title?
+	 */
+	private function checkArticleComments() {
+		global $wgTitle;
+		$this->isArticleComments = class_exists( 'ArticleComment' ) && ArticleCommentInit::ArticleCommentCheckTitle( $wgTitle );
+		return $this->isArticleComments;
+	}
+
+	/**
+	 * Get URL of the page comments button should be linking to
+	 */
+	private function getCommentsLink() {
+		wfProfileIn( __METHOD__ );
+		global $wgTitle, $wgRequest;
+
+		$isHistory = $wgRequest->getVal( 'action' ) == 'history';
+
+		if ( $this->checkArticleComments() ) {
+			// link to article comments section
+			if ( $wgTitle != $wgTitle || $isHistory ) {
+				$commentsLink = $wgTitle->getLocalUrl() . '#WikiaArticleComments';
+			} else {
+				// fix for redirected articles
+				$commentsLink = '#WikiaArticleComments';
+			}
+		} else {
+			// link to talk page
+			if ( $wgTitle->canTalk( $wgTitle->getNamespace() ) ) {
+				$commentsLink = $wgTitle->getTalkPage()->getLocalUrl();
+			} else {
+				// This case shouldn't happen other than Special:ThemeDesignerPreview
+				// We're faking some comments to show a user what an article would look like
+				$commentsLink = '';
+			}
+		}
+
+		wfProfileOut( __METHOD__ );
+		return $commentsLink;
+	}
+
+	private function getEditMainPageButton() {
+		global $wgEnableCuratedContentExt;
+
+		if ( !empty( $wgEnableCuratedContentExt ) && CuratedContentHelper::shouldDisplayToolButton() ) {
+			return [
+				'href' => '/main/edit?useskin=wikiamobile',
+				'text' => wfMessage( 'wikiacuratedcontent-edit-mobile-main-page' )->text(),
+				'id' => 'PremiumPageHeaderCuratedContentTool'
+			];
+		}
+		return [];
+	}
+
+	private function widgetLanguages() {
+		$request_language_urls = $this->request->getVal( 'request_language_urls' );
+		if ( !empty( $request_language_urls ) ) {
+			$this->language_urls = $request_language_urls;
+		}
+		$language_urls = $this->language_urls;
+		$langSortBy = [];
+		// only display the interlang links if there are interlanguage links
+		if ( !empty( $language_urls ) && is_array( $language_urls ) ) {
+			$lang_index = [];
+
+			// language order
+			$langSortBy = [
+				"interwiki-en" => 1,
+				"interwiki-de" => 2,
+				"interwiki-es" => 3,
+				"interwiki-ru" => 4,
+				"interwiki-pl" => 5,
+				"interwiki-fr" => 6,
+				"interwiki-it" => 7,
+				"interwiki-pt" => 8
+			];
+
+			foreach ( $language_urls as $val ) {
+				if ( !in_array( $val['href'], $lang_index ) ) {
+					if ( !isset( $langSortBy[$val["class"]] ) ) {
+						$langSortBy[$val["class"]] = true;
+					}
+
+					$langSortBy[$val["class"]] = [
+						'href' => $val['href'],
+						'name' => $val['text'],
+						'class' => $val['class'],
+					];
+				}
+			}
+			//	ordering the languages
+			foreach ( $langSortBy as $key => $value ) {
+				if ( !is_array( $value ) ) {
+					unset( $langSortBy[$key] );
+				}
+			}
+		}
+		if ( !empty( $langSortBy ) ) {
+			$this->language_list = $langSortBy;
+		}
 	}
 
 	private function getExplore() {
@@ -63,240 +325,5 @@ class PremiumPageHeaderController extends WikiaController {
 			'text' => wfMessage( 'pph-discuss' )->escaped(),
 			'href' => $href
 		];
-	}
-
-	/**
-	 * Get URL of the page comments button should be linking to
-	 */
-	private function getCommentsLink() {
-		wfProfileIn(__METHOD__);
-		global $wgTitle, $wgRequest;
-
-		$isHistory = $wgRequest->getVal('action') == 'history';
-
-		if ($this->checkArticleComments()) {
-			// link to article comments section
-			if ($wgTitle != $wgTitle || $isHistory) {
-				$commentsLink = $wgTitle->getLocalUrl() . '#WikiaArticleComments';
-			}
-			else {
-				// fix for redirected articles
-				$commentsLink = '#WikiaArticleComments';
-			}
-		}
-		else {
-			// link to talk page
-			if ($wgTitle->canTalk($wgTitle->getNamespace())) {
-				$commentsLink = $wgTitle->getTalkPage()->getLocalUrl();
-			} else {
-				// This case shouldn't happen other than Special:ThemeDesignerPreview
-				// We're faking some comments to show a user what an article would look like
-				$commentsLink = '';
-			}
-		}
-
-		wfProfileOut(__METHOD__);
-		return $commentsLink;
-	}
-
-	/**
-	 * Copied from CommentsLikesController.class.php
-	 * Are article comments enabled for context title?
-	 */
-	private function checkArticleComments() {
-		global $wgTitle;
-		$this->isArticleComments = class_exists('ArticleComment') && ArticleCommentInit::ArticleCommentCheckTitle($wgTitle);
-		return $this->isArticleComments;
-	}
-
-	/**
-	 * Get content actions for dropdown
-	 */
-	protected function getDropdownActions() {
-		$ret = [ ];
-
-		$editActions = [ ];
-		if ( isset( $this->content_actions['edit'] ) ) {
-			array_push( $editActions, 'edit' );
-		}
-		if ( isset( $this->content_actions['ve-edit'] ) ) {
-			if ( $this->content_actions['ve-edit']['main'] ) {
-				array_unshift( $editActions, 've-edit' );
-			} else {
-				array_push( $editActions, 've-edit' );
-			}
-		}
-		if ( isset( $this->content_actions['formedit'] ) ) {
-			array_push( $editActions, 'formedit' ); // SUS-533
-		}
-
-		// items to be added to "edit" dropdown
-		$actions = array_merge( $editActions,
-			[ 'history', 'move', 'protect', 'unprotect', 'delete', 'undelete', 'replace-file' ] );
-
-		// Enable to modify actions list on dropdown
-		wfRunHooks( 'PageHeaderDropdownActions', [ &$actions ] );
-
-		foreach ( $actions as $action ) {
-			if ( isset( $this->content_actions[$action] ) ) {
-				$ret[$action] = $this->content_actions[$action];
-			}
-		}
-
-		return $ret;
-	}
-
-	/**
-	 * Use MW core variable to generate action button
-	 * Copied from PageHeaderController.class.php
-	 */
-	protected function prepareActionButton() {
-
-		global $wgTitle, $wgUser, $wgRequest;
-
-		wfRunHooks( 'BeforePrepareActionButtons', [ $this, &$this->content_actions ] );
-
-		$isDiff = !is_null( $wgRequest->getVal( 'diff' ) );
-
-		// "Add topic" action
-		if ( isset( $this->content_actions['addsection'] ) ) {
-			// remove on diff pages (RT #72666)
-			if ( $isDiff ) {
-				unset( $this->content_actions['addsection'] );
-			}
-		}
-
-		// action button
-		# print_pre($this->content_actions);
-
-		// handle protected pages (they should have viewsource link and lock icon) - BugId:9494
-		if ( isset( $this->content_actions['viewsource'] ) &&
-		     !$wgTitle->isProtected() &&
-		     !$wgTitle->isNamespaceProtected( $wgUser ) &&
-		     !$wgUser->isLoggedIn() /* VOLDEV-74: logged in users should see the viewsource button, not edit */
-		) {
-			// force login to edit page that is not protected
-			$this->content_actions['edit'] = $this->content_actions['viewsource'];
-			$this->content_actions['edit']['text'] = wfMessage( 'edit' )->text();
-			unset( $this->content_actions['viewsource'] );
-		}
-
-		// If cascade protected, show viewsource button - BugId:VE-89
-		if ( isset( $this->content_actions['edit'] ) && $wgTitle->isCascadeProtected() ) {
-			$this->content_actions['viewsource'] = $this->content_actions['edit'];
-			$this->content_actions['viewsource']['text'] = wfMessage( 'viewsource' )->text();
-			unset( $this->content_actions['edit'] );
-		}
-
-		// "Add topic"
-		if ( isset( $this->content_actions['addsection'] ) ) {
-			$action = $this->content_actions['addsection'];
-			$action['text'] = wfMsg( 'oasis-page-header-add-topic' );
-			$this->action = $action;
-
-			$this->actionImage = MenuButtonController::ADD_ICON;
-			$this->actionName = 'addtopic';
-		}  // ve-edit
-		else if ( isset( $this->content_actions['ve-edit'] ) && $this->content_actions['ve-edit']['main'] ) {
-			$this->action = $this->content_actions['ve-edit'];
-			$this->actionImage = MenuButtonController::EDIT_ICON;
-			$this->actionName = 've-edit';
-			unset( $this->content_actions['ve-edit'] );
-		} // edit
-		else if ( isset( $this->content_actions['edit'] ) ) {
-			$this->action = $this->content_actions['edit'];
-			$this->actionImage = MenuButtonController::EDIT_ICON;
-			$this->actionName = 'edit';
-			unset( $this->content_actions['edit'] );
-		} // view source
-		else if ( isset( $this->content_actions['viewsource'] ) ) {
-			$this->action = $this->content_actions['viewsource'];
-			$this->actionImage = MenuButtonController::LOCK_ICON;
-			$this->actionName = 'source';
-			unset( $this->content_actions['ve-edit'], $this->content_actions['edit'] );
-		}
-
-		# print_pre($this->action); print_pre($this->actionImage); print_pre($this->actionName);
-	}
-
-	private function widgetLanguages() {
-		$request_language_urls = $this->request->getVal('request_language_urls');
-		if(!empty($request_language_urls)) {
-			$this->language_urls = $request_language_urls;
-		}
-		$language_urls = $this->language_urls;
-		$langSortBy = array();
-		// only display the interlang links if there are interlanguage links
-		if(!empty($language_urls) && is_array($language_urls)) {
-			$lang_index = array();
-
-			// language order
-			$langSortBy = array("interwiki-en" => 1, "interwiki-de" => 2, "interwiki-es" => 3, "interwiki-ru" => 4, "interwiki-pl" => 5, "interwiki-fr" => 6, "interwiki-it" => 7, "interwiki-pt" => 8);
-
-			foreach($language_urls as $val) {
-				if (!in_array($val['href'], $lang_index)) {
-					if (!isset($langSortBy[$val["class"]])) {
-						$langSortBy[$val["class"]] = true;
-					}
-
-					$langSortBy[$val["class"]] = array(
-						'href'  => $val['href'],
-						'name'  => $val['text'],
-						'class'  => $val['class'],
-					);
-				}
-			}
-			//	ordering the languages
-			foreach ($langSortBy as $key => $value) {
-				if (!is_array($value)) {
-					unset($langSortBy[$key]);
-				}
-			}
-		}
-		if (!empty($langSortBy)) {
-			$this->language_list = $langSortBy;
-		}
-	}
-
-	public function articleHeader() {
-		$skinVars = $this->app->getSkinTemplateObj()->data;
-		$this->content_actions = $skinVars['content_actions'];
-
-		// edit button preparation
-		// action button (edit / view soruce) and dropdown for it
-		$this->prepareActionButton();
-		// dropdown actions
-		$this->dropdown = $this->getDropdownActions();
-
-		// comments/talk button
-		$commentsEnabled = $this->checkArticleComments();
-		$this->commentButtonMsg = $commentsEnabled ? 'oasis-page-header-comments' : 'oasis-page-header-talk';
-		$this->commentsLink = $this->getCommentsLink();
-
-		$this->setVal( 'displaytitle', $skinVars['displaytitle'] );
-		$this->setVal( 'title', $skinVars['title'] );
-
-		$categoryLinks = $this->getContext()->getOutput()->getCategoryLinks();
-		$normalCategoryLinks = $categoryLinks['normal'] ?? [];
-
-		$visibleCategoriesLimit = 4;
-		if ( count( $normalCategoryLinks ) > 4 ) {
-			$visibleCategoriesLimit = 3;
-		}
-		$visibleCategories = array_slice( $normalCategoryLinks, 0, $visibleCategoriesLimit );
-		$moreCategories = array_slice( $normalCategoryLinks, $visibleCategoriesLimit );
-
-		$this->setVal( 'visibleCategoriesLength', count($visibleCategories) );
-		$this->setVal( 'visibleCategories', $visibleCategories );
-		$this->setVal( 'moreCategoriesLength', count($moreCategories) );
-		$this->setVal( 'moreCategories', $moreCategories );
-
-		if($this->app->getSkinTemplateObj()) {
-			$this->language_urls = $this->app->getSkinTemplateObj()->data['language_urls'];
-		} else {
-			$this->language_urls = array();
-		}
-		$this->language_list = null;
-		$this->widgetLanguages();
 	}
 }
--- extensions/wikia/PremiumPageHeader/templates/PremiumPageHeader_articleHeader.php
+++ extensions/wikia/PremiumPageHeader/templates/PremiumPageHeader_articleHeader.php
@@ -4,18 +4,18 @@
 			<div class="pph-categories">
 				<span class="pph-categories-in">in:</span>
 				<span class="pph-category-links">
-					<?php foreach($visibleCategories as $i => $category): ?>
-						<?php if ($i === $visibleCategoriesLength - 1 && count($moreCategories) === 0): ?>
+					<?php foreach ( $visibleCategories as $i => $category ): ?>
+						<?php if ( $i === $visibleCategoriesLength - 1 && count( $moreCategories ) === 0 ): ?>
 							<?= $category ?>
 						<?php else: ?>
 							<?= $category ?>,
 						<?php endif; ?>
 					<?php endforeach; ?>
-					<?php if($moreCategoriesLength > 0): ?>
+					<?php if ( $moreCategoriesLength > 0 ): ?>
 						<div class="pph-dropdown-container">
 							<a href="#">and <?= $moreCategoriesLength ?> more</a>
 							<ul class="pph-dropdown">
-								<?php foreach($moreCategories as $category): ?>
+								<?php foreach ( $moreCategories as $category ): ?>
 									<li><?= $category; ?></li>
 								<?php endforeach; ?>
 							</ul>
@@ -27,7 +27,7 @@
 		<h1><?= $title ?></h1>
 	</div>
 	<div class="pph-article-contribution">
-		<?php if(!empty($language_list)): ?>
+		<?php if ( !empty( $language_list ) ): ?>
 			<div class="pph-languages pph-dropdown-container">
 				<span>
 					English<!-- English label hard-coded - can we take this label from somewhere? -->
@@ -38,7 +38,9 @@
 				</span>
 				<ul class="pph-dropdown">
 					<?php foreach ( $language_list as $val ) : ?>
-						<li><a href="<?= Sanitizer::encodeAttribute( $val['href'] ); ?>"><?= htmlspecialchars( $val['name'] ); ?></a></li>
+						<li>
+							<a href="<?= Sanitizer::encodeAttribute( $val['href'] ); ?>"><?= htmlspecialchars( $val['name'] ); ?></a>
+						</li>
 					<?php endforeach ?>
 				</ul>
 			</div>
@@ -46,14 +48,15 @@
 		<div class="pph-contribution-buttons">
 			<div class="pph-button-group">
 				<?php if ( !empty( $action ) ): ?>
-					<a href="<?= empty($action['href']) ? '' : Sanitizer::encodeAttribute( $action['href'] ) ?>" class="pph-button">
+					<a href="<?= empty( $action['href'] ) ? '' : Sanitizer::encodeAttribute( $action['href'] ) ?>"
+					   class="pph-button">
 						<?php if ( $actionImage === MenuButtonController::EDIT_ICON ) { ?>
 							<?= DesignSystemHelper::renderSvg(
 								'wds-icons-pencil',
 								'wds-icon wds-icon-tiny pph-button-icon'
 							) ?>
 						<?php } ?>
-						<?= htmlspecialchars($action['text']) ?>
+						<?= htmlspecialchars( $action['text'] ) ?>
 					</a>
 				<?php endif; ?>
 				<div class="pph-dropdown-container">
@@ -65,7 +68,7 @@
 					</a>
 					<ul class="pph-dropdown">
 						<?php
-						foreach($dropdown as $key => $item) {
+						foreach ( $dropdown as $key => $item ) {
 							// render accesskeys
 							if ( !empty( $item['accesskey'] ) ) {
 								$accesskey =
@@ -74,13 +77,19 @@
 							} else {
 								$accesskey = '';
 							}
-
 							$href = $item['href'] ?? '#';
 							?>
 							<li>
-								<a href="<?= Sanitizer::encodeAttribute( $href ); ?>" <?= $accesskey ?> data-id="<?= Sanitizer::encodeAttribute( $key ); ?>" <?= empty($item['title']) ? '' : ' title="'. Sanitizer::encodeAttribute( $item['title'] ) .'"'; ?> <?= empty($item['id']) ? '' : ' id="'. Sanitizer::encodeAttribute( $item['id'] ) .'"' ?><?= empty($item['class']) ? '' : ' class="'. Sanitizer::encodeAttribute( $item['class'] ) .'"' ?><?= empty($item['attr']) ? '' : ' '.$item['attr'] ?>><?=htmlspecialchars($item['text']) ?></a>
+								<a href="<?= Sanitizer::encodeAttribute( $href ); ?>" <?= $accesskey ?>
+								   data-id="<?= Sanitizer::encodeAttribute( $key ); ?>" <?= empty( $item['title'] ) ? '' : ' title="' . Sanitizer::encodeAttribute( $item['title'] ) . '"'; ?> <?= empty( $item['id'] ) ? '' : ' id="' . Sanitizer::encodeAttribute( $item['id'] ) . '"' ?><?= empty( $item['class'] ) ? '' : ' class="' . Sanitizer::encodeAttribute( $item['class'] ) . '"' ?><?= empty( $item['attr'] ) ? '' : ' ' . $item['attr'] ?>><?= htmlspecialchars( $item['text'] ) ?></a>
 							</li>
 						<?php } ?>
+						<?php if ( !empty( $curatedContentButton ) ) : ?>
+							<li>
+								<a id="<?= $curatedContentButton['id'] ?>"
+								   href="<?= $curatedContentButton['href'] ?>"><?= $curatedContentButton['text'] ?></a>
+							</li>
+						<?php endif; ?>
 					</ul>
 				</div>
 			</div>
@@ -92,13 +101,13 @@
 				<?= wfMessage( $commentButtonMsg )->text(); ?>
 			</a>
 
-			<? if ( Wikia::isContentNamespace() && $wg->Title->exists() && !$app->checkSkin('oasislight') ): ?>
+			<? if ( Wikia::isContentNamespace() && $wg->Title->exists() && !$app->checkSkin( 'oasislight' ) ): ?>
 				<a id="PremiumPageHeaderShareEntryPoint" href="#" class="pph-button pph-button-secondary">
 					<?= DesignSystemHelper::renderSvg(
 						'wds-icons-share-small',
 						'wds-icon wds-icon-tiny pph-button-icon'
 					) ?>
-					<?= wfMessage( 'page-share-entry-point-label' )->escaped()?>
+					<?= wfMessage( 'page-share-entry-point-label' )->escaped() ?>
 				</a>
 			<?php endif; ?>
 		</div>
