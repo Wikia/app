397dcbc9e5df373856bd4b6ff495d8e22446e8fd
Weronika Rudnicka <wrudnicka@wikia-inc.com>
2017-03-23 14:59:42 +0100

    XW-2635 | removed RWEPageHeader extension

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -3179,24 +3179,6 @@ $config['oasislight_bottom_js'] = [
 	],
 ];
 
-$config[ 'rwe_page_header_scss' ] = [
-	'type' => AssetsManager::TYPE_SCSS,
-	'skin' => [ 'oasis' ],
-	'assets' => [
-		'//extensions/wikia/RWEPageHeader/styles/index.scss'
-	]
-];
-
-$config[ 'rwe_page_header_js' ] = [
-	'type' => AssetsManager::TYPE_JS,
-	'skin' => [ 'oasis' ],
-	'assets' => [
-		'//extensions/wikia/RWEPageHeader/scripts/rwe-page-header.js',
-		'//extensions/wikia/RWEPageHeader/scripts/search.js',
-		'//extensions/wikia/RWEPageHeader/scripts/searchSuggestions.js'
-	]
-];
-
 $config['ooyala_scss'] = [
 	'type' => AssetsManager::TYPE_SCSS,
 	'skin' => [ 'oasis' ],
