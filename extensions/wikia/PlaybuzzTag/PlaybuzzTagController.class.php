03bcb1b938bc44bf032bebe587ed351648ba6532
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-03-28 16:42:24 +0200

    XW-3089: Playbuzz widget

---
+++ extensions/wikia/PlaybuzzTag/PlaybuzzTagController.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class PlaybuzzTagController extends WikiaController {
+	const PARSER_TAG_NAME = 'playbuzz';
+	const PLAYBUZZ_SCRIPT_SRC = '//cdn.playbuzz.com/widget/feed.js';
+	const DATA_ITEM_ATTR = 'data-item';
+
+
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setHook( self::PARSER_TAG_NAME, [ new static(), 'renderTag' ] );
+		return true;
+	}
+
+	public function renderTag( $input, array $args, Parser $parser, PPFrame $frame ) {
+		if ( !$this->validateArgs( $args ) ) {
+			return '<strong class="error">' . wfMessage( 'playbuzz-tag-could-not-render' )->escaped() . '</strong>';
+		}
+
+		return Html::element( 'script', [ 'src' => self::PLAYBUZZ_SCRIPT_SRC ] )
+			. Html::element( 'div', [ 'class' => 'pb_feed', 'data-item' => $args[self::DATA_ITEM_ATTR], 'data-version' => '2' ] );
+	}
+
+	private function validateArgs( $args ) {
+		return array_key_exists( self::DATA_ITEM_ATTR, $args );
+	}
+}
