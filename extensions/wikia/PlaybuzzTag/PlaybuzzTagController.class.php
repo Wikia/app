2d70a6e216c3bf181217944a1c3eb471efe9cfc2
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-03-29 10:49:58 +0200

    XW-3089: support for additional attributes

--- extensions/wikia/PlaybuzzTag/PlaybuzzTagController.class.php
+++ extensions/wikia/PlaybuzzTag/PlaybuzzTagController.class.php
@@ -3,7 +3,11 @@
 class PlaybuzzTagController extends WikiaController {
 	const PARSER_TAG_NAME = 'playbuzz';
 	const PLAYBUZZ_SCRIPT_SRC = 'http://cdn.playbuzz.com/widget/feed.js';
+
 	const DATA_ITEM_ATTR = 'data-item';
+	const DATA_COMMENTS_ATTR = 'data-comments';
+	const DATA_SHARES_ATTR = 'data-shares';
+	const DATA_GAME_INFO_ATTR = 'data-game-info';
 
 
 	public static function onParserFirstCallInit( Parser $parser ) {
@@ -17,10 +21,20 @@ class PlaybuzzTagController extends WikiaController {
 		}
 
 		$script = JSSnippets::addToStack(self::PLAYBUZZ_SCRIPT_SRC);
-		return $script . Html::element( 'div', [ 'class' => 'pb_feed', 'data-item' => $args[self::DATA_ITEM_ATTR], 'data-version' => '2' ] );
+		return $script . Html::element( 'div', $this->getAttributes( $args ) );
 	}
 
 	private function validateArgs( $args ) {
 		return array_key_exists( self::DATA_ITEM_ATTR, $args );
+	}
+
+	private function getAttributes( array $args ): array {
+		return [
+			'class' => 'pb_feed',
+			self::DATA_ITEM_ATTR => $args[self::DATA_ITEM_ATTR] ?? 'false',
+			self::DATA_COMMENTS_ATTR => $args[self::DATA_COMMENTS_ATTR] ?? 'false',
+			self::DATA_GAME_INFO_ATTR => $args[self::DATA_GAME_INFO_ATTR] ?? 'false',
+			self::DATA_SHARES_ATTR => $args[self::DATA_SHARES_ATTR] ?? 'false'
+		];
 	}
 }
