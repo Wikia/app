0f6fbeb43d942d8b73e4cdb1a3529ba00f61c245
idradm <robakadam@gmail.com>
2017-03-30 12:34:52 +0200

    XW-3133 | enable tracking only if correct group, separate local nav tracking and tablet support

--- extensions/wikia/PremiumPageHeader/scripts/old-nav-tracking-hack.js
+++ extensions/wikia/PremiumPageHeader/scripts/old-nav-tracking-hack.js
@@ -16,7 +16,7 @@ require(['wikia.window', 'jquery', 'wikia.tracker', 'wikia.abTest'], function (w
 	}
 
 	$(function () {
-		if (abTest.isExperimentActive('PREMIUM_PAGE_HEADER')) {
+		if (abTest.inGroup('PREMIUM_PAGE_HEADER', 'CONTROL')) {
 			if ($oldWikiHeader.is(':visible')) {
 				track({
 					action: tracker.ACTIONS.IMPRESSION,
