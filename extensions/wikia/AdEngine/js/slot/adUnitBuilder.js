9e70471f6b4d2df2adefc2da6f5a8251576f9269
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2017-04-03 17:58:54 +0200

    ADEN-4908 Add new ad unit builder

--- extensions/wikia/AdEngine/js/slot/adUnitBuilder.js
+++ extensions/wikia/AdEngine/js/slot/adUnitBuilder.js
@@ -18,7 +18,35 @@ define('ext.wikia.adEngine.slot.adUnitBuilder', [
 		].join('/');
 	}
 
+	function getDevice(params) {
+		var result = 'unknown';
+
+		switch (params.skin) {
+			case 'oasis':
+				result = 'desktop'; // TODO: recognize desktop
+			break;
+			case 'mercury':
+			case 'mobile-wiki':
+				result = 'smartphone';
+				break;
+		}
+
+		return result;
+	}
+
+	function buildNew(src, slotName, passback) {
+		var params = page.getPageLevelParams(),
+			device = getDevice(params),
+			skin = params.skin,
+			pageType = params.s2,
+			wikiName = params.s1,
+			vertical = params.s0v;
+
+		return '/' + dfpId + '/' + src + '.' + slotName + '/' + device + '/' + skin + '-' + pageType + '/' + wikiName + '-' + vertical + '/' + passback;
+	}
+
 	return {
-		build: build
+		build: build,
+		buildNew: buildNew
 	};
 });
--- extensions/wikia/AdEngine/js/spec/slot/adUnitBuilder.spec.js
+++ extensions/wikia/AdEngine/js/spec/slot/adUnitBuilder.spec.js
@@ -4,11 +4,7 @@ describe('ext.wikia.adEngine.slot.adUnitBuilder', function () {
 	var mocks = {
 		page: {
 			getPageLevelParams: function() {
-				return {
-					s0: 'life',
-					s1: '_project43',
-					s2: 'article'
-				};
+				return {};
 			}
 		}
 	};
@@ -17,9 +13,32 @@ describe('ext.wikia.adEngine.slot.adUnitBuilder', function () {
 		return modules['ext.wikia.adEngine.slot.adUnitBuilder'](mocks.page);
 	}
 
-	it('Build ad unit', function () {
-		var vastUrl = getModule().build('TOP_LEADERBOARD', 'playwire');
+	function mockPageParams(params) {
+		spyOn(mocks.page, 'getPageLevelParams');
+		mocks.page.getPageLevelParams.and.returnValue(params);
+	}
 
-		expect(vastUrl).toEqual('/5441/wka.life/_project43//article/playwire/TOP_LEADERBOARD');
+	it('Build ad unit', function () {
+		mockPageParams({
+			s0: 'life',
+			s1: '_project43',
+			s2: 'article',
+			skin: 'desktop'
+		});
+
+		expect(getModule().build('TOP_LEADERBOARD', 'playwire'))
+			.toEqual('/5441/wka.life/_project43//article/playwire/TOP_LEADERBOARD');
+	});
+
+	it('Should build new ad unit', function () {
+		mockPageParams({
+			's0v': 'gaming',
+			's1': '_godofwar',
+			's2': 'home',
+			'skin': 'mercury',
+		});
+
+		expect(getModule().buildNew('mobile_remnant', 'MOBILE_PREFOOTER', 'Evolve'))
+			.toEqual('/5441/mobile_remnant.MOBILE_PREFOOTER/smartphone/mercury-home/_godofwar-gaming/Evolve');
 	});
 });
