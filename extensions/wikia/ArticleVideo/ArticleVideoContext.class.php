68810a3e9e4f6eca1fb6058875d205b359937195
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-04 11:55:17 +0200

    XW-3111: using WikiaPageType::isActionPage()

--- extensions/wikia/ArticleVideo/ArticleVideoContext.class.php
+++ extensions/wikia/ArticleVideo/ArticleVideoContext.class.php
@@ -14,7 +14,7 @@ class ArticleVideoContext {
 		return $wg->enableArticleFeaturedVideo &&
 			isset( $wg->articleVideoFeaturedVideos[$title] ) &&
 			self::isFeaturedVideosValid( $wg->articleVideoFeaturedVideos[$title] ) &&
-			self::isArticlePage(); // Prevents to show video on ?action=history etc.;
+			!WikiaPageType::isActionPage(); // Prevents to show video on ?action=history etc.;
 	}
 
 	private static function isFeaturedVideosValid( $featuredVideo ) {
@@ -56,10 +56,5 @@ class ArticleVideoContext {
 	 */
 	public static function isRelatedVideoEmbedded( $title ) {
 		return !empty( static::getRelatedVideoData( $title ) );
-	}
-
-
-	private static function isArticlePage() {
-		return F::app()->wg->Request->getVal( 'action', 'view' ) === 'view';
 	}
 }
