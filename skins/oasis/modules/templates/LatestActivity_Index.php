229aee501cbcdaea1b1f46850fee8fdf03a69800
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-04 18:18:35 +0200

    XW-2943: styling RWA module

--- skins/oasis/modules/templates/LatestActivity_Index.php
+++ skins/oasis/modules/templates/LatestActivity_Index.php
@@ -1,13 +1,12 @@
 <section class="module" id="WikiaRecentActivity">
-	<div class="latest-activity-module-header">
-		<?= $activityIcon ?><h2><?= $moduleHeader ?></h2>
-	</div>
+
+	<h2 class="latest-activity-module-header"><?= $activityIcon ?><?= $moduleHeader ?></h2>
 
 	<? if(!empty($changeList)): ?>
 		<ul class="latest-activity-items">
 		<? foreach ($changeList as $item): ?>
-			<li>
-				<em><a href="<?= $item['page_url'] ?>"><?= $item['page_title'] ?></a></em>
+			<li class="latest-activity-item">
+				<div class="page-title"><a href="<?= $item['page_url'] ?>"><?= $item['page_title'] ?></a></div>
 				<div class="edited-by"><a href="<?= $item['user_profile_url'] ?>"><?= $item['user_name'] ?></a> • <?= $item['time_ago'] ?></div>
 			</li>
 		<? endforeach; ?>
