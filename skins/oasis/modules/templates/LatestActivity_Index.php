674253af5c295821ad0029a7d677b547d778c294
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-04 14:50:47 +0200

    XW-2943: cleanup LatestActivityController and LatestActivity_index

--- skins/oasis/modules/templates/LatestActivity_Index.php
+++ skins/oasis/modules/templates/LatestActivity_Index.php
@@ -1,27 +1,17 @@
-<section class="WikiaActivityModule module" id="<?= !empty( $userName ) ? 'WikiaRecentActivityUser' : 'WikiaRecentActivity'; ?>">
+<section class="WikiaActivityModule module" id="WikiaRecentActivity">
+	<!--TODO: activity icon-->
 	<h2 class="activity-heading"><?= $moduleHeader ?></h2>
-	<ul>
-<?php
-	if(!empty($changeList)){
-		foreach ($changeList as $item) {
-?>
-		<li>
-			<img src="<?= $wg->BlankImgUrl ?>" class="sprite <?= $item['changeicon'] ?>" height="20" width="20">
-			<em><?= $item['page_href'] ?></em>
-			<div class="edited-by"><?= $item['changemessage'] ?></div>
-		</li>
-<?php
-		}
-	}
-	elseif(!empty($userName)) {
-		echo wfMsg( 'userprofilepage-recent-activity-default', $userName );
-	}
-?>
-	</ul>
 
-	<? if ( $userName && count($changeList) ) :?>
-		<?= Wikia::specialPageLink('Contributions/' . $userName, 'userprofilepage-top-recent-activity-see-more', 'more') ;?>
-	<? elseif(empty($userName)): ?>
-		<?= Wikia::specialPageLink('WikiActivity', 'oasis-more', 'more') ?>
-	<? endif ;?>
+	<? if(!empty($changeList)): ?>
+		<ul>
+		<? foreach ($changeList as $item): ?>
+			<li>
+				<em><a href="<?= $item['page_url'] ?>"><?= $item['page_title'] ?></a></em>
+				<div class="edited-by"><a href="<?= $item['user_profile_url'] ?>"><?= $item['user_name'] ?></a> • <?= $item['time_ago'] ?></div>
+			</li>
+		<? endforeach; ?>
+		</ul>
+	<? endif; ?>
+
+	<?= Wikia::specialPageLink('WikiActivity', 'oasis-more', 'more') ?>
 </section>
