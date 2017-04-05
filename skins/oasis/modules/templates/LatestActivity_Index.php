6d026fe638a65edcee4b893f33c5f6447cd9fdd9
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-05 17:31:07 +0200

    XW-2943: code formatting

--- skins/oasis/modules/templates/LatestActivity_Index.php
+++ skins/oasis/modules/templates/LatestActivity_Index.php
@@ -2,19 +2,19 @@
 
 	<h2 class="activity-module-header"><?= $activityIcon ?><?= $moduleHeader ?></h2>
 
-	<? if(!empty($changeList)): ?>
+	<? if ( !empty( $changeList ) ): ?>
 		<ul class="activity-items">
-		<? foreach ($changeList as $item): ?>
-			<li class="activity-item">
-				<div class="page-title"><a href="<?= $item['page_url'] ?>"><?= $item['page_title'] ?></a></div>
-				<div class="edit-info">
-					<a class="edit-info-user" href="<?= $item['user_profile_url'] ?>"><?= $item['user_name'] ?></a>
-					<? if( !empty( $item['time_ago'] ) ): ?>
-						<span class="edit-info-time"> • <?= $item['time_ago'] ?></span>
-					<? endif ?>
-				</div>
-			</li>
-		<? endforeach; ?>
+			<? foreach ( $changeList as $item ): ?>
+				<li class="activity-item">
+					<div class="page-title"><a href="<?= $item['page_url'] ?>"><?= $item['page_title'] ?></a></div>
+					<div class="edit-info">
+						<a class="edit-info-user" href="<?= $item['user_profile_url'] ?>"><?= $item['user_name'] ?></a>
+						<? if ( !empty( $item['time_ago'] ) ): ?>
+							<span class="edit-info-time"> • <?= $item['time_ago'] ?></span>
+						<? endif ?>
+					</div>
+				</li>
+			<? endforeach; ?>
 		</ul>
 	<? endif; ?>
 </section>
