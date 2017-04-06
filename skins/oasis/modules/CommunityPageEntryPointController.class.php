4a2cd9badfdd05adf6370ee7937d2723c585518b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-06 15:54:15 +0200

    XW-3140: adding avatars

--- skins/oasis/modules/CommunityPageEntryPointController.class.php
+++ skins/oasis/modules/CommunityPageEntryPointController.class.php
@@ -4,5 +4,16 @@ class CommunityPageEntryPointController extends WikiaController {
 	public function executeIndex( $params ) {
 		Wikia::addAssetsToOutput( 'community_page_entry_point_js' );
 		Wikia::addAssetsToOutput( 'community_page_entry_point_scss' );
+
+		$topContributors = ( new CommunityPageSpecialUsersModel() )->getTopContributors();
+		$avatars = [];
+
+		if ( is_array( $topContributors ) ) {
+			$avatars = array_map( function( $user ) {
+				return AvatarService::getAvatarUrl( $user['userId'] );
+			}, array_slice( $topContributors, 0, 5) );
+		}
+
+		$this->setVal( 'avatars', $avatars );
 	}
 }
--- skins/oasis/modules/templates/CommunityPageEntryPointController_Index.php
+++ skins/oasis/modules/templates/CommunityPageEntryPointController_Index.php
@@ -1,4 +1,9 @@
 <section class="community-page-entry-point-module">
+	<div class="avatars">
+		<? foreach( $avatars as $index => $avatarUrl ): ?>
+			<img class="wds-avatar" style="position: absolute; left:<?= $index * 20 ?>px; z-index: <?= 5 - $index ?>" src="<?= $avatarUrl ?>" />
+		<? endforeach; ?>
+	</div>
 	<div class="content">
 		<div class="description"><?= wfMessage( 'communitypage-help-us-grow' )->parse() ?></div>
 		<a href="<?= SpecialPage::getTitleFor( 'Community' )->getLocalURL(); ?>" class="wds-is-secondary wds-button wds-is-squished"><?= wfMessage( 'communitypage-entry-button' )->escaped() ?></a>
