beba61e3af8ab57ff44c74ac705daf51d6d6e50c
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-06 14:24:03 +0200

    XW-2943: avoid caching lang-specific data

--- skins/oasis/modules/templates/LatestActivity_Index.php
+++ skins/oasis/modules/templates/LatestActivity_Index.php
@@ -6,11 +6,15 @@
 		<ul class="activity-items">
 			<? foreach ( $changeList as $item ): ?>
 				<li class="activity-item">
-					<div class="page-title"><a href="<?= $item['page_url'] ?>" data-tracking="activity-title" ><?= $item['page_title'] ?></a></div>
+					<div class="page-title">
+						<a href="<?= $item['page_url'] ?>" data-tracking="activity-title" ><?= $item['page_title'] ?></a>
+					</div>
 					<div class="edit-info">
-						<a class="edit-info-user" data-tracking="activity-username" href="<?= $item['user_profile_url'] ?>"><?= $item['user_name'] ?></a>
+						<a class="edit-info-user" data-tracking="activity-username" href="<?= $item['user_profile_url'] ?>">
+							<?= User::isIP( $item['user_name'] ) ? wfMessage( 'oasis-anon-user' )->escaped() : htmlspecialchars( $item['user_name'] ) ?>
+						</a>
 						<? if ( !empty( $item['time_ago'] ) ): ?>
-							<span class="edit-info-time"> • <?= $item['time_ago'] ?></span>
+							<span class="edit-info-time"> • <?=  wfTimeFormatAgo( $item['time_ago'] ) ?></span>
 						<? endif ?>
 					</div>
 				</li>
