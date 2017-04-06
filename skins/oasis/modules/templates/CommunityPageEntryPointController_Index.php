f03395502a2d019f2a4d0878263377490bb97159
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-06 14:11:13 +0200

    XW-3140: basic styles

--- skins/oasis/modules/templates/CommunityPageEntryPointController_Index.php
+++ skins/oasis/modules/templates/CommunityPageEntryPointController_Index.php
@@ -1,5 +1,4 @@
-<section class="community-page-entry-point-module module">
-	<div class="community-page-entry-point-logo"></div>
+<section class="community-page-entry-point-module">
 	<div class="community-page-entry-point-description"><?= wfMessage( 'communitypage-help-us-grow' )->parse() ?></div>
-	<a href="<?= SpecialPage::getTitleFor( 'Community' )->getLocalURL(); ?>" class="community-page-entry-point-button"><?= wfMessage( 'communitypage-entry-button' )->escaped() ?></a>
+	<a href="<?= SpecialPage::getTitleFor( 'Community' )->getLocalURL(); ?>" class="wds-is-secondary wds-button wds-is-squished"><?= wfMessage( 'communitypage-entry-button' )->escaped() ?></a>
 </section>
