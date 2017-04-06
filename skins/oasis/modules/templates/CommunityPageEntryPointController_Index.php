e9c1b0d59bf081a2c90b26c4024729114419cd1a
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-06 14:53:10 +0200

    XW-3140: styling community module

--- skins/oasis/modules/templates/CommunityPageEntryPointController_Index.php
+++ skins/oasis/modules/templates/CommunityPageEntryPointController_Index.php
@@ -1,4 +1,6 @@
 <section class="community-page-entry-point-module">
-	<div class="community-page-entry-point-description"><?= wfMessage( 'communitypage-help-us-grow' )->parse() ?></div>
-	<a href="<?= SpecialPage::getTitleFor( 'Community' )->getLocalURL(); ?>" class="wds-is-secondary wds-button wds-is-squished"><?= wfMessage( 'communitypage-entry-button' )->escaped() ?></a>
+	<div class="content">
+		<div class="description"><?= wfMessage( 'communitypage-help-us-grow' )->parse() ?></div>
+		<a href="<?= SpecialPage::getTitleFor( 'Community' )->getLocalURL(); ?>" class="wds-is-secondary wds-button wds-is-squished"><?= wfMessage( 'communitypage-entry-button' )->escaped() ?></a>
+	</div>
 </section>
