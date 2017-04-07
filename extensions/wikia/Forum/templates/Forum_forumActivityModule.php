140ac59bab632af5d6ba5c76212225151f9b0165
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-07 17:51:40 +0200

    XW-3202: basic styles for forum activity and related threads modules

--- extensions/wikia/Forum/templates/Forum_forumActivityModule.php
+++ extensions/wikia/Forum/templates/Forum_forumActivityModule.php
@@ -1,16 +1,15 @@
-<section class="module WikiaActivityModule ForumActivityModule" id="ForumActivityModule">
-	<h2><?= wfMessage( 'forum-activity-module-heading' )->escaped() ?></h2>
-	<ul>
+<section class="module activity-module ForumActivityModule" id="ForumActivityModule">
+	<h2 class="activity-module-header"><?= wfMessage( 'forum-activity-module-heading' )->escaped() ?></h2>
+	<ul class="activity-items">
 		<?php foreach ( $posts as $value ): ?>
-		<li>
+		<li class="activity-item">
 			<?= AvatarService::renderAvatar( $value['user']->getName(), 20 ); ?>
-			<em>
+			<div class="page-title">
 				<a href="<?= $value['wall_message']->getMessagePageUrl( true ); ?>" title="<?= htmlspecialchars( $value['metatitle'] ); ?>"><?= htmlspecialchars( $value['metatitle'] ); ?></a>
-			</em>
-			<div class="edited-by">
-				<?php $user = '<a href="' . $value['user']->getUserPage()->getFullUrl() . '">' . htmlspecialchars( $value['display_username'] ) . '</a>'; ?>
-				<?php $time = '<span class="timeago abstimeago" title="' . $value['event_iso'] . '" alt="' . $value['event_mw'] . '">&nbsp;</span>' ?>
-				<?= wfMessage( $value['is_reply'] ? 'forum-activity-module-posted' : 'forum-activity-module-started' )->rawParams( $user, $time )->escaped(); ?>
+			</div>
+			<div class="edit-info">
+				<a class="edit-info-user" href="<?= $value['user']->getUserPage()->getFullUrl() ?>"><?= htmlspecialchars( $value['display_username'] ) ?></a>
+				<span class="edit-info-time"> • <?= wfTimeFormatAgo( $value['event_iso'] ) ?></span>
 			</div>
 		</li>
 		<?php endforeach; ?>
--- extensions/wikia/Forum/templates/Forum_forumRelatedThreads.php
+++ extensions/wikia/Forum/templates/Forum_forumRelatedThreads.php
@@ -1,36 +1,21 @@
 <?php if ( $showModule ) { ?>
-	<section class="module WikiaActivityModule ForumActivityModule" id="ForumRelatedThreadsModule">
-		<h2><?= wfMessage( 'forum-related-module-heading' )->escaped() ?></h2>
-		<ul>
+	<section class="module activity-module ForumActivityModule" id="ForumRelatedThreadsModule">
+		<h2 class="activity-module-header"><?= wfMessage( 'forum-related-module-heading' )->escaped() ?></h2>
+		<ul class="activity-items">
 			<?php foreach ( $messages as $message ) { ?>
-			<li>
-				<?php
-				$messageLast = empty( $message['reply'] ) ? $message['message'] : $message['reply'];
-				$user = Html::element(
-					'a',
-					[
-						'href' => $messageLast->getUser()->getUserPage()->getFullUrl()
-					],
-					$messageLast->getUser()->getName()
-				);
-				$time = Html::element(
-					'span',
-					[
-						'class' => 'timeago abstimeago',
-						'title' => $messageLast->getCreateTime(),
-						'alt' => $messageLast->getCreateTime( TS_MW )
-					],
-					'&nbsp;'
-				);
-				?>
+			<li class="activity-item">
+				<?php $messageLast = empty( $message['reply'] ) ? $message['message'] : $message['reply']; ?>
 
 				<?= AvatarService::renderAvatar( $messageLast->getUser()->getName(), 20 ); ?>
 
-				<em>
-					<a href="<?= Sanitizer::encodeAttribute( $message['message']->getMessagePageUrl() ); ?>" title="<?= Sanitizer::encodeAttribute( $message['message']->getMetaTitle() ); ?>"><?= htmlspecialchars( $message['message']->getMetaTitle() ); ?></a>
-				</em>
-				<div class="edited-by">
-					<?= wfMessage( !empty( $message['reply'] ) ? 'forum-activity-module-posted' : 'forum-activity-module-started' )->rawParams( $user, $time )->escaped() ?>
+				<div class="page-title">
+					<a href="<?= Sanitizer::encodeAttribute( $message['message']->getMessagePageUrl() ); ?>" title="<?= Sanitizer::encodeAttribute( $message['message']->getMetaTitle() ); ?>">
+						<?= htmlspecialchars( $message['message']->getMetaTitle() ); ?>
+					</a>
+				</div>
+				<div class="edit-info">
+					<a class="edit-info-user" href="<?= $messageLast->getUser()->getUserPage()->getFullUrl() ?>"><?= htmlspecialchars( $messageLast->getUser()->getName() ) ?></a>
+					<span class="edit-info-time"> • <?= wfTimeFormatAgo( $messageLast->getCreateTime() ) ?></span>
 				</div>
 			</li>
 			<?php } ?>
