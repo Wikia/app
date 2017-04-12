9e68170c33803033f045b8539906e883a643828c
TK-999 <tk.999.wikia@gmail.com>
2017-04-12 11:11:49 +0200

    SUS-1990: Eliminate errors from CNW tests

--- extensions/wikia/CreateNewWiki/CreateWikiChecks.php
+++ extensions/wikia/CreateNewWiki/CreateWikiChecks.php
@@ -101,7 +101,6 @@ class CreateWikiChecks {
 			$sName[0] == '-' || $sName[$sNameLength - 1] == '-'
 		) {
 			#-- invalid name
-			Wikia::log( __METHOD__, $sNameLength, "{$sName} {$sName[$sNameLength -1]}" );
 			$message = wfMessage( 'autocreatewiki-bad-name' );
 		} elseif ( in_array( $sName, array_keys( static::getLanguageNames() ) ) ) {
 			#-- invalid name
