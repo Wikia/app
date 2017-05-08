d5aa71f57c2f17efa44c1972e62c329eb864738a
macbre <maciej.brencz@gmail.com>
2017-05-08 15:32:51 +0200

    TemplateDraft.i18n.php: crowdin download

--- extensions/wikia/TemplateDraft/TemplateDraft.i18n.php
+++ extensions/wikia/TemplateDraft/TemplateDraft.i18n.php
@@ -70,18 +70,18 @@ $messages['qqq'] = array(
 $messages['de'] = array(
 	'templatedraft-description' => 'TemplateDraft-Erweiterung',
 	'templatedraft-subpage' => 'Entwurf',
-	'templatedraft-editintro' => 'Bitteschön! Wir haben eine Entwurfsversion deiner Infobox in unserem neuen Format erstellt. Wir speichern sie als Unterseite, damit du sie überprüfen und alle notwendigen Änderungen vornehmen kannst. Schau auch bei $1 und $2 vorbei.',
+	'templatedraft-editintro' => 'Bitteschön! Wir haben eine Entwurfsversion deiner Infobox in unserem neuen Format erstellt. Wir speichern sie als Unterseite, damit du sie überprüfen und alle notwendigen Änderungen vornehmen kannst. Schau auch $1 und $2 an.',
 	'templatedraft-approval-no-page-error' => 'Übernahme fehlgeschlagen, da die Entwurfsseite nicht existiert',
 	'templatedraft-approval-no-templatedraft-error' => 'Diese Seite ist kein Vorlagenentwurf und kann daher nicht übernommen werden.',
 	'templatedraft-approval-success-confirmation' => 'Der Inhalt dieser Vorlage wurde mit der aktualisierten Version aus dem Vorlagenentwurf ersetzt und der Entwurf wurde entfernt.',
-	'templatedraft-approval-summary' => 'Die Infobox wurde mithilfe des [[Hilfe:Umwandlung von Infoboxen|Umwandlungswerkzeug]] aktualisiert.',
+	'templatedraft-approval-summary' => 'Die Infobox wurde mithilfe des [[Hilfe:Umwandlung von Infoboxen|Umwandlungswerkzeugs]] aktualisiert.',
 	'templatedraft-draft-removal-summary' => 'Aufgeräumt. Der Entwurf wurde auf die übergeordnete Vorlagenseite übernommen.',
 	'templatedraft-module-title-create' => 'Diese Infobox umwandeln',
 	'templatedraft-module-subtitle-create' => 'Diese Vorlage verwendet nicht das neue [[Hilfe:Infoboxen|Infobox-Format]].',
 	'templatedraft-module-content-create' => 'Wir können eine Entwurfsversion der Vorlage im neuen Format aus deiner bestehenden Infobox erstellen und diese als Unterseite speichern. Du kannst sie dann überprüfen und alle notwendigen Änderungen vornehmen.',
 	'templatedraft-module-button-create' => 'Entwurf im neuen Format erstellen',
 	'templatedraft-module-button-title-create' => 'Öffnet einen neuen Browsertab mit vorgefülltem Bearbeitungsformular',
-	'templatedraft-module-title-approve' => 'Diese Entwurfsvorlage verschieben',
+	'templatedraft-module-title-approve' => 'Diesen Vorlagenentwurf verschieben',
 	'templatedraft-module-content-approve' => 'Bist du mit diesem Entwurf zufrieden und möchtest ihn übernehmen?',
 	'templatedraft-module-button-approve' => 'Diesen Entwurf übernehmen',
 	'templatedraft-preview-n-docs' => '== Verwendung und Vorschau ==
@@ -96,13 +96,13 @@ um das zu sehen:
 $2
 
 [{{fullurl:{{ns:Template}}:{{PAGENAME}}}}?action=purge Klicke hier, um die obige Vorschau zu aktualisieren]',
-	'templatedraft-module-editintro-please-convert' => 'Wir können eine Entwurfsversion deiner Infobox mit unserem neuen Markup erstellen. Klicke dazu einfach auf $1.',
-	'templatedraft-module-view-parent' => 'Wikitext der übergeordneten Vorlage ansehen',
-	'templatedraft-module-help' => 'Hilfeseite für das Infobox-Format ansehen',
+	'templatedraft-module-editintro-please-convert' => 'Wir können eine Entwurfsversion deiner Infobox in unserem neuen Format erstellen. Klicke dazu einfach auf $1.',
+	'templatedraft-module-view-parent' => 'den Wikitext der übergeordneten Vorlage',
+	'templatedraft-module-help' => 'die Hilfeseite für das Infobox-Format',
 	'templatedraft-invalid-template' => 'Ungültiger Vorlagenname angegeben.',
-	'templatedraft-module-approve-protected' => 'Diese Vorlage ist geschützt. Wende dich bitte an einen [[Special: ListAdmins| Administrator]], damit er diesen Entwurf freischalten kann.',
+	'templatedraft-module-approve-protected' => 'Diese Vorlage ist geschützt. Wende dich bitte an einen [[Special:ListAdmins|Administrator]], damit er diesen Entwurf freischalten kann.',
 	'templatedraft-protect-edit' => 'Du hast keine Berechtigung, die übergeordnete Vorlagenseite zu bearbeiten.',
-	'right-templatedraft' => 'Ermöglicht den Zugriff auf einen Assistenten, der hilft, die nicht plattformunabhängigen Infoboxen zum neuen Markup zu konvertieren',
+	'right-templatedraft' => 'Ermöglicht den Zugriff auf einen Assistenten, der hilft, die nicht portablen Infoboxen ins neue Format umzuwandeln',
 	'templatedraft-preview-example-value' => 'Dies ist ein Test',
 );
 
@@ -113,10 +113,10 @@ $messages['es'] = array(
 	'templatedraft-approval-no-page-error' => 'No se pudo aprobar porque la página de borrador no existe',
 	'templatedraft-approval-no-templatedraft-error' => 'Esta página no es un borrador de plantilla y por lo tanto no se puede aprobar',
 	'templatedraft-approval-success-confirmation' => 'El contenido de esta platilla se cambió con una versión actualizada de una plantilla de borrador y se eliminó el borrador',
-	'templatedraft-approval-summary' => 'El Infobox se actualizó usando la herramienta de migración de [[w:c:es:Ayuda:Infoboxes_Portátiles|infobox portátil]]',
+	'templatedraft-approval-summary' => 'El Infobox se actualizó usando la herramienta de migración de [[w:c:comunidad:Ayuda:Infoboxes|infobox portátil]]',
 	'templatedraft-draft-removal-summary' => 'Limpieza. Borrador aprobado para la página principal.',
 	'templatedraft-module-title-create' => 'Migrar esta infobox',
-	'templatedraft-module-subtitle-create' => 'Esta plantilla no usa el nuevo [[w:c:es:Ayuda:Infoboxes_Portátiles|marcador de infobox]].',
+	'templatedraft-module-subtitle-create' => 'Esta plantilla no usa el nuevo [[w:c:comunidad:Ayuda:Infoboxes|marcador de infobox]].',
 	'templatedraft-module-content-create' => 'Podemos generar un borrador del marcador desde tu infobox existente y guardarlo como subpágina, para que puedas revisarlo y hacer cualquier cambio necesario.',
 	'templatedraft-module-button-create' => 'Generar marcador de borrador',
 	'templatedraft-module-button-title-create' => 'Abrir una nueva pestaña con un formulario de edición previamente llenado',
@@ -134,12 +134,12 @@ para ver esto:
 
 $2
 
-[{{fullurl:{{ns:Template}}:{{PAGENAME}}}}?action=purge Hacer clic aquí para renovar la previsualización anterior]',
+[{{fullurl:{{ns:Template}}:{{PAGENAME}}}}?action=purge Haz clic aquí para renovar la previsualización anterior]',
 	'templatedraft-module-editintro-please-convert' => 'Podemos generar un borrador de tu infobox con nuestro nuevo marcador. Solo haz clic en $1.',
 	'templatedraft-module-view-parent' => 'ver wikitext de la plantilla principal',
 	'templatedraft-module-help' => 'ver la página de ayuda sobre el marcador del infobox',
 	'templatedraft-invalid-template' => 'El nombre de la plantilla es inválido.',
-	'templatedraft-module-approve-protected' => 'Esta plantilla está protegida. Por favor pregunta a un [[Special:ListAdmins|administrador]] que apruebe este borrador.',
+	'templatedraft-module-approve-protected' => 'Esta plantilla está protegida. Por favor pregunta a un [[{{#Special:ListAdmins}}|administrador]] que apruebe este borrador.',
 	'templatedraft-protect-edit' => 'No tienes permiso para editar la página principal.',
 	'right-templatedraft' => 'Permite el acceso a un asistente que ayudará a convertir infoboxes no portátiles al nuevo marcador',
 	'templatedraft-preview-example-value' => 'Esta es una prueba',
@@ -224,9 +224,9 @@ $2
 );
 
 $messages['ja'] = array(
-	'templatedraft-description' => 'TemplateDraft拡張機能',
+	'templatedraft-description' => 'テンプレートドラフト拡張機能',
 	'templatedraft-subpage' => 'ドラフト',
-	'templatedraft-editintro' => '新しいマークアップを使用したインフォボックスのドラフト版を生成しました。サブページとして保存されますので、内容を確認し、必要に応じて変更を加えていただけます。$1または$2もご覧ください。',
+	'templatedraft-editintro' => '新しいマークアップを使用したインフォボックスのドラフト版を作成しました。サブページとして保存されますので、内容を確認し、必要に応じて変更を加えていただけます。$1または$2もご覧ください。',
 	'templatedraft-approval-no-page-error' => 'ドラフトページが存在しないため、採用できませんでした',
 	'templatedraft-approval-no-templatedraft-error' => 'テンプレートのドラフトページ以外でドラフトの採用を行うことはできません',
 	'templatedraft-approval-success-confirmation' => 'このテンプレートのコンテンツをドラフト・テンプレートの更新版と置き換え、ドラフトを削除しました',
@@ -253,7 +253,7 @@ $2
 
 [{{fullurl:{{ns:Template}}:{{PAGENAME}}}}?action=purge ここをクリックすると上のプレビューを更新できます]',
 	'templatedraft-module-editintro-please-convert' => '「$1」をクリックすると、新しいマークアップを使用したインフォボックスのドラフト版を生成できます。',
-	'templatedraft-module-view-parent' => 'もとのテンプレートのウィキテキスト',
+	'templatedraft-module-view-parent' => 'もとのテンプレートのWikiテキスト',
 	'templatedraft-module-help' => 'インフォボックスのマークアップに関するヘルプページを見る',
 	'templatedraft-invalid-template' => '入力したテンプレート名は無効です。',
 	'templatedraft-module-approve-protected' => 'このテンプレートは保護されているようです。このドラフトを承認するには、[[Special:ListAdmins|アドミン]]にお問い合わせください。',
@@ -326,40 +326,38 @@ $2
 
 $messages['pl'] = array(
 	'templatedraft-description' => 'Rozszerzenie TemplateDraft',
-	'templatedraft-subpage' => 'Wersje robocze',
-	'templatedraft-editintro' => 'Proszę bardzo! Wygenerowaliśmy wersję roboczą twojego infoboksu wykorzystując nasze nowe znaczniki. Zapiszemy go jako podstronę, żebyś mógł go przejrzeć i wprowadzić niezbędne zmiany. Może chcesz $1 albo $2.',
-	'templatedraft-approval-no-page-error' => 'Zatwierdzenie nie powiodło się, ponieważ strona wersji roboczej nie istnieje',
-	'templatedraft-approval-no-templatedraft-error' => 'Zatwierdzenie wersji roboczej nie jest możliwe na stronach wersji roboczych, które nie są szablonami',
-	'templatedraft-approval-success-confirmation' => 'Zawartość tego szablonu została zastąpiona uaktualnioną treścią z wersji roboczej szablonu, a wersja robocza została usunięta',
-	'templatedraft-approval-summary' => 'Infoboks został uaktualniony przy użyciu narzędzia do migracji do [[w:c:pl:Pomoc:Przenośne_infoboksy|przenośnych infoboksów]]',
+	'templatedraft-subpage' => 'szkic',
+	'templatedraft-editintro' => 'Proszę bardzo! Wygenerowaliśmy wersję roboczą twojego infoboksu wykorzystując nasze nowe znaczniki. Zapiszemy go jako podstronę, co umożliwi jego przejrzenie i wprowadzenie niezbędnych zmian. Jeśli chcesz, zajrzyj na $1 lub do $2.',
+	'templatedraft-approval-no-page-error' => 'Zatwierdzenie nie powiodło się, ponieważ strona ze szkicem nie istnieje',
+	'templatedraft-approval-no-templatedraft-error' => 'Zatwierdzenie wersji roboczej nie jest możliwe na stronach szkiców, które nie są szablonami',
+	'templatedraft-approval-success-confirmation' => 'Zawartość tego szablonu została zastąpiona zaktualizowaną wersją ze szkicu, a ten został usunięty.',
+	'templatedraft-approval-summary' => 'Infoboks został uaktualniony przy użyciu narzędzia do migracji do [[w:pl:Pomoc:Przenośne_infoboksy|przenośnych infoboksów]]',
 	'templatedraft-draft-removal-summary' => 'Porządki. Projekt zatwierdzony jako strona nadrzędna.',
-	'templatedraft-module-title-create' => 'Dokonaj migracji tego infoboksu',
-	'templatedraft-module-subtitle-create' => 'Ten szablon nie korzysta z nowych [[w:c:pl:Pomoc:Przenośne_infoboksy|znaczników infoboksów]].',
+	'templatedraft-module-title-create' => 'Migracja infoboksu',
+	'templatedraft-module-subtitle-create' => 'Ten szablon nie korzysta z nowych [[w:pl:Pomoc:Przenośne_infoboksy|znaczników infoboksów]].',
 	'templatedraft-module-content-create' => 'Możemy wygenerować wersję roboczą znaczników korzystając z istniejącego infoboksu i zapisać ją jako podstronę, żebyś mógł go przejrzeć i wprowadzić niezbędne zmiany.',
-	'templatedraft-module-button-create' => 'Wygeneruj wersję roboczą znaczników',
+	'templatedraft-module-button-create' => 'Wygeneruj wersję roboczą',
 	'templatedraft-module-button-title-create' => 'Otwórz nową kartę ze wstępnie wypełnionym formularzem edycji',
-	'templatedraft-module-title-approve' => 'Przenieś wersję roboczą szablonu',
+	'templatedraft-module-title-approve' => 'Przenieś ten szkic',
 	'templatedraft-module-content-approve' => 'Projekt spełnia twoje oczekiwania i chcesz, aby stał się gotowym szablonem?',
-	'templatedraft-module-button-approve' => 'Zatwierdź wersję roboczą',
-	'templatedraft-preview-n-docs' => '== Użycie i podgląd ==
-Wpisz:
-
+	'templatedraft-module-button-approve' => 'Zatwierdź szkic',
+	'templatedraft-preview-n-docs' => '== Użycie ==
 <pre>
-$1
-</pre>
+$1</pre>
 
-aby zobaczyć:
+<br clear="both" />
+== Przykład ==
+$2<pre>
+$2</pre>
 
-$2
-
-[{{fullurl:{{ns:Template}}:{{PAGENAME}}}}?action=purge Kliknij tutaj, żeby odświeżyć podgląd widoczny powyżej]',
-	'templatedraft-module-editintro-please-convert' => 'Możemy wygenerować wersję roboczą twojego infoboksu z naszymi nowymi znacznikami, kliknij tutaj $1.',
-	'templatedraft-module-view-parent' => 'zobacz wikitekst rodzica szablonu',
-	'templatedraft-module-help' => 'zobacz stronę pomocy na temat znaczników Infoboksów',
+[{{fullurl:{{FULLPAGENAME}}|action=purge}} Kliknij tutaj, żeby odświeżyć podgląd].',
+	'templatedraft-module-editintro-please-convert' => 'Możemy wygenerować wersję roboczą twojego infoboksu z naszymi nowymi znacznikami. Wystarczy, że klikniesz „$1”.',
+	'templatedraft-module-view-parent' => 'kodu głównego szablonu',
+	'templatedraft-module-help' => 'stronę pomocy na temat składni infoboksów',
 	'templatedraft-invalid-template' => 'Nieprawidłowa nazwa szablonu.',
-	'templatedraft-module-approve-protected' => 'Ten szablon jest chroniony. Poproś [[Special:ListAdmins|Admina]] o zatwierdzenie wersji roboczej.',
+	'templatedraft-module-approve-protected' => 'Ten szablon jest zabezpieczony. Poproś [[Special:ListAdmins|admina]] o zatwierdzenie szkicu.',
 	'templatedraft-protect-edit' => 'Nie masz uprawnień do edytowania strony nadrzędnej.',
-	'right-templatedraft' => 'Udostępnia kreator pozwalający na konwersję nie-przenośnych infoboksów na nową składnię',
+	'right-templatedraft' => 'Udostępnia kreator pozwalający na konwersję nieprzenośnych infoboksów na nową składnię',
 	'templatedraft-preview-example-value' => 'To jest test',
 );
 
@@ -444,20 +442,20 @@ $2
 $messages['zh-hans'] = array(
 	'templatedraft-description' => 'TemplateDraft扩展',
 	'templatedraft-subpage' => '模版草稿',
-	'templatedraft-editintro' => '好啦！我们已经使用新的标记文件为您生成了信息框草稿。我们会将其保存为一个子页面，以便于您查看并进行必要的更改。您可以$1或$2。',
+	'templatedraft-editintro' => '好啦！我们已经使用新的标记文件为您生成了讯息框草稿。我们会将其保存为一个子页面，以便于您查看并进行必要的更改。您可以$1或$2。',
 	'templatedraft-approval-no-page-error' => '由于页面的草稿不存在，审核操作失败',
 	'templatedraft-approval-no-templatedraft-error' => '草稿审核操作不适用于非模版草稿页面',
-	'templatedraft-approval-success-confirmation' => '此模版内容已被模版的草稿更新版替换，原模版草案已被移除',
-	'templatedraft-approval-summary' => '使用[[w:c:zh.community:Help:移動化訊息框|移动化信息框]]迁移工具进行更新',
+	'templatedraft-approval-success-confirmation' => '此模版内容已被模版的草稿更新版替换，原模版草稿已被移除',
+	'templatedraft-approval-summary' => '使用[[w:c:zh.community:Help:PortableInfoboxes|移动化讯息框]]迁移工具进行更新',
 	'templatedraft-draft-removal-summary' => '清理完毕。草稿已被批准为首页面。',
-	'templatedraft-module-title-create' => '迁移此信息框',
-	'templatedraft-module-subtitle-create' => '此模版没有使用新的[[w:c:zh.community:Help:移動化訊息框|移动式信息框]]。',
-	'templatedraft-module-content-create' => '我们可以从现用的信息框生成标记文件的草稿，并保存为子页面，以便于您查看并进行必要的更改。',
+	'templatedraft-module-title-create' => '迁移此讯息框',
+	'templatedraft-module-subtitle-create' => '此模版没有使用新的[[w:c:zh.community:Help:移動化訊息框|移动化讯息框]]。',
+	'templatedraft-module-content-create' => '我们可以从现用的讯息框生成标记文件的草稿，并保存为子页面，以便于您查看并进行必要的更改。',
 	'templatedraft-module-button-create' => '生成标记文件草稿',
 	'templatedraft-module-button-title-create' => '打开一个含预填充编辑表单的新选项卡',
 	'templatedraft-module-title-approve' => '移动此模版的草稿',
 	'templatedraft-module-content-approve' => '对此草案感到满意，并希望将其推广为现用模版？',
-	'templatedraft-module-button-approve' => '批准此模版的草稿',
+	'templatedraft-module-button-approve' => '批准此模版草稿',
 	'templatedraft-preview-n-docs' => '== 使用与预览==
 键入下列信息：
 
@@ -470,33 +468,33 @@ $1
 $2
 
 [{{fullurl:{{ns:Template}}:{{PAGENAME}}}}?action=purge 点击此处刷新上面的预览内容]',
-	'templatedraft-module-editintro-please-convert' => '我们可以用我们的新标记文件为您生成信息框的草稿，只需点击 $1即可。',
+	'templatedraft-module-editintro-please-convert' => '我们可以用我们的新标记文件为您生成讯息框草稿，只需点击$1即可。',
 	'templatedraft-module-view-parent' => '查看主模版的wikitext',
-	'templatedraft-module-help' => '查看信息框标记文件的帮助页面',
+	'templatedraft-module-help' => '查看讯息框标记文件的帮助页面',
 	'templatedraft-invalid-template' => '提供的模版名称无效',
-	'templatedraft-module-approve-protected' => '此模板受到保护。请咨询[[Special:ListAdmins|管理员]]批准这一草案。',
+	'templatedraft-module-approve-protected' => '此模板受到保护。请询问[[Special:ListAdmins|管理员]]批准这一草稿。',
 	'templatedraft-protect-edit' => '您没有权限编辑首页面。',
 	'right-templatedraft' => '允许向导帮助非移动式讯息框转换成一个新的标记文件',
 	'templatedraft-preview-example-value' => '这是一个测试',
 );
 
 $messages['zh-hant'] = array(
-	'templatedraft-description' => 'TemplateDraft的擴展文件',
+	'templatedraft-description' => '模板草稿的擴展功能',
 	'templatedraft-subpage' => '模版草稿',
-	'templatedraft-editintro' => '好啦！ 我們已經使用新的標記檔為您生成了訊息框草稿。 我們會將其保存為一個子頁面，以便於您查看並進行必要的更改。 您可以$1或$2。',
-	'templatedraft-approval-no-page-error' => '由於沒有頁面的草稿，審核錯作失敗',
-	'templatedraft-approval-no-templatedraft-error' => '草稿審核操作不適用於非模版草稿頁面',
-	'templatedraft-approval-success-confirmation' => '草稿内容已經被模版草稿更新版替換，原模版草稿已經被移除',
-	'templatedraft-approval-summary' => '使用[[w:c:zh.community:Help:移動化訊息框|移動化訊息框]]遷移工具進行更新',
-	'templatedraft-draft-removal-summary' => '清理完畢。 草稿已被批准為首頁面。',
-	'templatedraft-module-title-create' => '遷移這個資訊框',
-	'templatedraft-module-subtitle-create' => '這個模版沒有使用新的[[w:c:zh.community:Help:移動化訊息框|移動化訊息框]]。',
-	'templatedraft-module-content-create' => '我們可以從現用的資訊框生成標記文件的草稿，並保存為子頁面，以便于你查看並進行必要的更改。',
+	'templatedraft-editintro' => '好啦！ 我們已經使用新的標記方式為您製作了訊息框草稿。 我們會將其保存為一個子頁面，以便於您查看並進行必要的更改。 您可以$1或$2。',
+	'templatedraft-approval-no-page-error' => '審核操作失敗，因為草稿頁面不存在。',
+	'templatedraft-approval-no-templatedraft-error' => '草稿審核操作不適用於非模板草稿頁面',
+	'templatedraft-approval-success-confirmation' => '模板内容已經由模板草稿更新替換，模板草稿已經被移除',
+	'templatedraft-approval-summary' => '使用[[w:c:zh.community:Help:移動化訊息框|行動化訊息框]]遷移工具進行更新',
+	'templatedraft-draft-removal-summary' => '清理完畢。 草稿已批准而成為主頁面。',
+	'templatedraft-module-title-create' => '變更這個訊息框',
+	'templatedraft-module-subtitle-create' => '這個模板沒有使用新的[[w:c:zh.community:Help:移動化訊息框|行動化訊息框]]。',
+	'templatedraft-module-content-create' => '我們可以從現在用的訊息框生成標記文件的草稿，並保存為子頁面，以便於你查看並進行必要的更改。',
 	'templatedraft-module-button-create' => '生成標記文件草稿',
-	'templatedraft-module-button-title-create' => '打開一個含預填充編輯表單的新標簽',
-	'templatedraft-module-title-approve' => '移動這個模版草稿',
-	'templatedraft-module-content-approve' => '對這個草稿感到滿意，並希望將它推廣為有效模版？',
-	'templatedraft-module-button-approve' => '批准這個模版的草稿',
+	'templatedraft-module-button-title-create' => '開啟一個有預填充編輯表單的新標籤',
+	'templatedraft-module-title-approve' => '移動這個模板草稿',
+	'templatedraft-module-content-approve' => '對這個草稿感到滿意，並希望讓它生效成為主模板？',
+	'templatedraft-module-button-approve' => '批准這個模板的草稿',
 	'templatedraft-preview-n-docs' => '== 使用與預覽 ==
 鍵入下面的資訊：
 
@@ -510,12 +508,12 @@ $2
 
 [{{fullurl:{{ns:Template}}:{{PAGENAME}}}}?action=purge 按一下這裡刷新上面的預覽内容]',
 	'templatedraft-module-editintro-please-convert' => '我們可以用我們的新標記文件為你生成資訊框的草稿，只需按一下$1就可以了。',
-	'templatedraft-module-view-parent' => '查看主模版的wikitext',
+	'templatedraft-module-view-parent' => '查看主模板的wiki文本',
 	'templatedraft-module-help' => '查看資訊框標記文件的幫助頁面',
 	'templatedraft-invalid-template' => '提供的模版名稱無效',
-	'templatedraft-module-approve-protected' => '此模板受到保護。 請諮詢[[Special:ListAdmins|管理員]]批准這一草案。',
-	'templatedraft-protect-edit' => '您沒有許可權編輯首頁面。',
-	'right-templatedraft' => '允許使用將非行動話訊息框轉變為新標記方式的協助精靈',
+	'templatedraft-module-approve-protected' => '此模板受到保護。 請諮詢[[Special:ListAdmins|管理員]]批准這個草稿。',
+	'templatedraft-protect-edit' => '您沒有權限編輯主頁面。',
+	'right-templatedraft' => '允許使用將非行動訊息框轉變為新標記方式的協助精靈',
 	'templatedraft-preview-example-value' => '這是一個測試',
 );
 
