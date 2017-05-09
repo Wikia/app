b1125f516fe8e9abd7d1b72d6a168eefbd716f3a
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-05-09 16:48:27 +0200

    XW-3277: using '!empty' instead of 'isset' to check if attribution information is set

--- extensions/wikia/ArticleVideo/templates/ArticleVideo_attribution.php
+++ extensions/wikia/ArticleVideo/templates/ArticleVideo_attribution.php
@@ -1,4 +1,4 @@
-<? if ( isset( $videoDetails['username'], $videoDetails['userUrl'], $videoDetails['userAvatarUrl'] ) ): ?>
+<? if ( !empty( $videoDetails['username'] ) && !empty( $videoDetails['userUrl'] ) && !empty( $videoDetails['userAvatarUrl'] ) ): ?>
 	<div class="featured-video__attribution-container">
 		<img class="featured-video__attribution-avatar" src="<?= Sanitizer::encodeAttribute( $videoDetails['userAvatarUrl'] ) ?>"/>
 		<a class="featured-video__attribution-username" href="<?= Sanitizer::encodeAttribute( $videoDetails['userUrl'] ) ?>" target="_blank">
