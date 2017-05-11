a4148d81f21ed88d4d9d6144adf2006eabd56aae
James Ogura <jogura@wikia-inc.com>
2017-05-11 09:59:32 -0400

    ST-282 - Update jquery autocomplete component and add additional tracking
    * Note: this is not fully operational or tested
    * Additional work is required:
      * to ensure that old functionality is not lost
      * to get the styling correct (WDS are have not been added to any of the jquery autocomplete widget classes)

--- extensions/wikia/DesignSystem/scripts/DesignSystemGlobalNavigationSearchSuggestions.js
+++ extensions/wikia/DesignSystem/scripts/DesignSystemGlobalNavigationSearchSuggestions.js
@@ -2,34 +2,96 @@ $(function () {
 	'use strict';
 
 	var $searchInput = $('#searchInput'),
-		$searchInputWrapper = $('#searchInputWrapper'),
-		searchSuggestionsUrl = $searchInput.data('suggestions-url');
+		$searchButton = $('.wds-global-navigation__search-submit'),
+		searchSuggestionsUrl = $searchInput.data('suggestions-url'),
+		initialized = false;
 
 	function initSuggestions() {
-		mw.loader.using('jquery.autocomplete').then(function () {
+		if (initialized == false) {
+			initialized = true;
+			window.Wikia.Tracker.track({
+				action: Wikia.Tracker.ACTIONS.CLICK,
+				category: 'search',
+				trackingMethod: 'analytics',
+				label: 'search-bar-click'
+			});
+		}
+
+		mw.loader.using('jquery.ui.autocomplete').then(function () {
 			var autocompleteReEscape = new RegExp('(\\' + ['/', '.', '*', '+', '?', '|', '(', ')',
-					'[', ']', '{', '}', '\\'].join('|\\') + ')', 'g');
+														   '[', ']', '{', '}', '\\'].join('|\\')
+												  + ')', 'g');
+			var beginTyping = false;
+			var showedSuggestion = false;
+
+			$searchButton
+				.click(function () {
+					// Track when a user clicks the search button
+					window.Wikia.Tracker.track({
+						action: Wikia.Tracker.ACTIONS.CLICK,
+						category: 'search',
+						trackingMethod: 'analytics',
+						label: 'search-button-click'
+					});
+				});
 
 			$searchInput
-				.on({
-					suggestShow: function () {
-						$searchInputWrapper.addClass('wds-is-active');
-					},
-					suggestHide: function () {
-						$searchInputWrapper.removeClass('wds-is-active');
+				.keyup(function () {
+					// Track when user begins to type
+					if (beginTyping == false) {
+						beginTyping = true;
+						window.Wikia.Tracker.track({
+							action: Wikia.Tracker.ACTIONS.ENABLE,
+							category: 'search',
+							trackingMethod: 'analytics',
+							label: 'search-button-enable'
+						});
 					}
 				})
 				.autocomplete({
-					serviceUrl: searchSuggestionsUrl,
-					queryParamName: $searchInput.data('suggestions-param-name'),
-					appendTo: '.wds-global-navigation__search-input-wrapper',
-					deferRequestBy: 200,
-					minLength: 3,
-					maxHeight: 1000,
-					onSelect: function (value, data, event) {
-						var valueEncoded = encodeURIComponent(value.replace(/ /g, '_')),
-							// slashes can't be urlencoded because they break routing
-							location = window.wgArticlePath.replace(/\$1/, valueEncoded).replace(encodeURIComponent('/'), '/');
+					source: function (request, response) {
+						$.ajax({
+							url: searchSuggestionsUrl,
+							dataType: "json",
+							data: {
+								query: request.term
+							},
+							success: function (data) {
+								response(data.suggestions);
+							}
+						});
+					},
+					search: function () {
+						// Track when the search suggest dropdown appears
+						if (showedSuggestion == false) {
+							showedSuggestion = true;
+							window.Wikia.Tracker.track({
+								action: Wikia.Tracker.ACTIONS.ENABLE,
+								category: 'search',
+								trackingMethod: 'analytics',
+								label: 'search-suggest-enable'
+							});
+						}
+					},
+					focus: function (event, ui) {
+						$searchInput.val(ui.item.label);
+						return false;
+					},
+					select: function (event, ui) {
+						var valueEncoded = encodeURIComponent(
+							ui.item.label.replace(/ /g, '_'));
+						// slashes can't be urlencoded because they break routing
+						var location = window.wgArticlePath.replace(/\$1/,
+																	valueEncoded).replace(
+							encodeURIComponent('/'), '/');
+
+						// Track when the user selects a suggestion
+						window.Wikia.Tracker.track({
+							action: Wikia.Tracker.ACTIONS.CLICK,
+							category: 'search',
+							trackingMethod: 'analytics',
+							label: 'search-suggest-click'
+						});
 
 						window.Wikia.Tracker.track({
 							eventName: 'search_start_suggest',
@@ -38,7 +100,8 @@ $(function () {
 							trackingMethod: 'internal'
 						});
 
-						// Respect modifier keys to allow opening in a new window (BugId:29401)
+						// Respect modifier keys to allow opening in a new window
+						// (BugId:29401)
 						if (event.button === 1 || event.metaKey || event.ctrlKey) {
 							window.open(location);
 
@@ -46,35 +109,29 @@ $(function () {
 							return false;
 						} else {
 							window.Wikia.Tracker.track({
-								action: Wikia.Tracker.ACTIONS.CLICK,
-								category: 'navigation',
-								trackingMethod: 'analytics',
-								label: $searchInput.data('suggestions-tracking-label')
-							});
+							   action: Wikia.Tracker.ACTIONS.CLICK,
+							   category: 'navigation',
+							   trackingMethod: 'analytics',
+							   label: $searchInput.data(
+								   'suggestions-tracking-label')
+						   });
 
 							window.location.href = location;
 						}
+						return false;
 					},
-					selectedClass: 'wds-is-selected',
-					// always send the request even if previous one returned no suggestions
-					skipBadQueries: true,
-					setPosition: false,
-					suggestionWrapperElement: 'li',
-					fnContainerMarkup: function (mainContainerId, autocompleteElId) {
-						return '<div id="' + mainContainerId +
-							'" class="wds-dropdown__content wds-global-navigation__search-suggestions">' +
-							'<ul id="' + autocompleteElId +
-							'" class="wds-list wds-has-ellipsis wds-is-linked"></ul>' +
-							'</div>';
-					},
-					fnFormatResult: function (value, data, currentValue) {
-						var pattern = '(' + currentValue.replace(autocompleteReEscape, '\\$1') + ')';
-
-						return '<a class="wds-global-navigation__dropdown-link">' +
-							value.replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>') +
-							'</a>';
-					}
-				});
+					minLength: 3,
+					appendTo: '.wds-global-navigation__search-input-wrapper'
+				})
+				.data("autocomplete")._renderItem = function (ul, item) {
+				var currentValue = $searchInput.val();
+				var pattern = '(' + currentValue.replace(autocompleteReEscape, '\\$1') + ')';
+				return $("<li></li>")
+					.data("item.autocomplete", item)
+					.append('<a class="x">' + item.label.replace(new RegExp(pattern, 'gi'),
+																 '<strong>$1<\/strong>') + '</a>')
+					.appendTo(ul);
+			};
 		});
 	}
 
