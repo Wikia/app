f75cdab6ba85ab343d336a84f11e5d8e2fffc751
Karol Tatała <ktatala@wikia-inc.com>
2017-05-16 17:31:21 +0200

    XW-3417 | ad mix on scroll behaviour

--- extensions/wikia/AdMixExperiment/AdMixExperiment.hooks.php
+++ extensions/wikia/AdMixExperiment/AdMixExperiment.hooks.php
@@ -2,7 +2,8 @@
 
 class AdMixExperimentHooks {
 	public static function onBeforePageDisplay( \OutputPage $out, \Skin $skin ) {
-		\Wikia::addAssetsToOutput( 'ad_mix_experiment' );
+		\Wikia::addAssetsToOutput( 'ad_mix_experiment_js' );
+		\Wikia::addAssetsToOutput( 'ad_mix_experiment_scss' );
 
 		return true;
 	}
