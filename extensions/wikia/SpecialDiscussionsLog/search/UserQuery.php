1aa5201394d27732744f6ecb6ce891c343d91640
James Sutterfield <james@wikia-inc.com>
2017-05-24 14:56:39 -0700

    Merge pull request #12978 from Wikia/IRIS-4479
    IRIS-4479 update logstash connection configs and queries

--- extensions/wikia/SpecialDiscussionsLog/search/UserQuery.php
+++ extensions/wikia/SpecialDiscussionsLog/search/UserQuery.php
@@ -12,19 +12,15 @@ class UserQuery implements SearchQuery {
 	static function getQuery( $userId, $paginationSize ) {
 		return <<<JSON_BODY
 {
-	"query": {
-		"filtered": {
-			"query": {
-				"bool": {
-					"should": [{
-						"query_string": {
-							"query":"rawTags:dis_service_* AND user_id:$userId"
-						}
-					}]
-				}
-			}
-		}
-	},
+    "query": {
+        "bool": {
+            "should": [{
+                "query_string": {
+                    "query":"rawTags:dis_service_* AND user_id:$userId"
+                }
+            }]
+        }
+    },
 	"size":$paginationSize,
 	"sort":[{
 		"@timestamp": {
