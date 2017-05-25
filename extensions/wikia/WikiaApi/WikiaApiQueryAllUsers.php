67d6a1ffc4ade20c83715c71ca644a5485b71b25
macbre <maciej.brencz@gmail.com>
2017-05-25 13:03:36 +0200

    WikiaApiQueryAllUsers: run stylize.php

--- extensions/wikia/WikiaApi/WikiaApiQueryAllUsers.php
+++ extensions/wikia/WikiaApi/WikiaApiQueryAllUsers.php
@@ -1,8 +1,8 @@
 <?php
 
-if (!defined('MEDIAWIKI')) {
+if ( !defined( 'MEDIAWIKI' ) ) {
 	// Eclipse helper - will be ignored in production
-	require_once ('ApiQueryBase.php');
+	require_once ( 'ApiQueryBase.php' );
 }
 
 /**
@@ -17,7 +17,7 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 
 	private $mCityId, $mDB;
 
-	public function __construct($query, $moduleName) {
+	public function __construct( $query, $moduleName ) {
 		parent::__construct( $query, $moduleName, 'au' );
 		$this->showError = true;
 	}
@@ -31,15 +31,15 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 
 		wfProfileIn( __METHOD__ );
 
-		$memkey = wfSharedMemcKey( __METHOD__, implode("-", (array) $this->params['group'] ), $this->mCityId );
+		$memkey = wfSharedMemcKey( __METHOD__, implode( "-", (array) $this->params['group'] ), $this->mCityId );
 		$data = $wgMemc->get( $memkey );
 
 		if ( !is_array( $data ) ) {
-			$db = wfGetDB(DB_SLAVE, array(), $wgSpecialsDB);
+			$db = wfGetDB( DB_SLAVE, array(), $wgSpecialsDB );
 
 			// SUS-2038: build condition for all groups mentioned in the URL
 			$groupsCondition = array_map(
-				function($group) use ($db) {
+				function( $group ) use ( $db ) {
 					return "all_groups" . $db->buildLike( $db->anyString(), $group, $db->anyString() );
 				},
 				$this->params['group']
@@ -62,11 +62,11 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 			);
 
 			$data = [];
-			while ($row = $db->fetchObject($oRes)) {
+			while ( $row = $db->fetchObject( $oRes ) ) {
 				$data[$row->user_id] = $row->all_groups;
 			}
-			$db->freeResult($oRes);
-			$wgMemc->set( $memkey , $data, 300 );			
+			$db->freeResult( $oRes );
+			$wgMemc->set( $memkey , $data, 300 );
 			$this->profileDBOut();
 		}
 
@@ -74,57 +74,57 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 		return $data;
 	}
 
-	private function getUserRegistration($user_id) {
+	private function getUserRegistration( $user_id ) {
 		global $wgMemc, $wgExternalSharedDB;
 		wfProfileIn( __METHOD__ );
-				
-		$dbr = wfGetDB(DB_SLAVE, 'stats', $wgExternalSharedDB);
-		$condition = array( 
+
+		$dbr = wfGetDB( DB_SLAVE, 'stats', $wgExternalSharedDB );
+		$condition = array(
 			"user_id" 	=> $user_id,
 		);
 
-		$row = $dbr->selectRow( 
-			"`user`", 
-			array( "user_registration" ), 
+		$row = $dbr->selectRow(
+			"`user`",
+			array( "user_registration" ),
 			$condition,
-			__METHOD__ 
+			__METHOD__
 		);
 
-		wfProfileOut(__METHOD__);
-		return ( is_object($row) ) ? $row->user_registration : null;
+		wfProfileOut( __METHOD__ );
+		return ( is_object( $row ) ) ? $row->user_registration : null;
 	}
 
 	protected function getDB() {
 		return $this->mDB;
 	}
-	
+
 	protected function getSharedDB() {
 		global $wgExternalSharedDB;
-		$this->mDB = wfGetDB(DB_SLAVE, array(), $wgExternalSharedDB);
+		$this->mDB = wfGetDB( DB_SLAVE, array(), $wgExternalSharedDB );
 		return $this->mDB;
 	}
-	
+
 	protected function getSpecialsDB() {
 		global $wgSpecialsDB;
-		$this->mDB = wfGetDB(DB_SLAVE, array(), $wgSpecialsDB);
+		$this->mDB = wfGetDB( DB_SLAVE, array(), $wgSpecialsDB );
 		return $this->mDB;
-	}	
+	}
 
-	public function execute() { 
+	public function execute() {
 		global $wgCityId;
-		
+
 		$this->params = $this->extractRequestParams();
 		$this->mCityId = (int) $wgCityId;
 
 		# prop
 		$prop = $this->params['prop'];
-		if ( !is_null($prop) ) {
-			$prop = array_flip($prop);
+		if ( !is_null( $prop ) ) {
+			$prop = array_flip( $prop );
 		}
-		$this->fld_blockinfo 		= isset($prop['blockinfo']);
-		$this->fld_editcount 		= isset($prop['editcount']);
-		$this->fld_groups 			= isset($prop['groups']);
-		$this->fld_registration 	= isset($prop['registration']);
+		$this->fld_blockinfo 		= isset( $prop['blockinfo'] );
+		$this->fld_editcount 		= isset( $prop['editcount'] );
+		$this->fld_groups 			= isset( $prop['groups'] );
+		$this->fld_registration 	= isset( $prop['registration'] );
 
 		if ( $this->params['local'] ) {
 			$this->local_users();
@@ -137,15 +137,15 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 		$db = $this->getSharedDB();
 
 		$limit = $this->params['limit'];
-		$this->addTables('`user`', 'u1');
+		$this->addTables( '`user`', 'u1' );
 
 		if ( !is_null( $this->params['group'] ) ) {
 			$users = $this->getUsersForGroup();
-			if ( empty($users) ) 
-				$users = array(0);
+			if ( empty( $users ) )
+				$users = array( 0 );
 			$this->addWhere( 'u1.user_id IN (' . $db->makeList( array_keys( $users ) ) . ') ' );
-		} 
-	
+		}
+
 		if ( !is_null( $this->params['from'] ) ) {
 			$this->addWhere( 'u1.user_name >= ' . $db->addQuotes( $this->keyToTitle( $this->params['from'] ) ) );
 		}
@@ -159,19 +159,19 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 		}
 
 		if ( $this->fld_blockinfo ) {
-			$this->addTables('ipblocks');
-			$this->addTables('`user`', 'u2');
-			$u2 = sprintf("%s %s", '`user`', 'u2');
+			$this->addTables( 'ipblocks' );
+			$this->addTables( '`user`', 'u2' );
+			$u2 = sprintf( "%s %s", '`user`', 'u2' );
 			$this->addJoinConds(
 				array(
-					'ipblocks' => array('LEFT JOIN', 'ipb_user=u1.user_id'),
-					$u2 => array('LEFT JOIN', 'ipb_by=u2.user_id')
+					'ipblocks' => array( 'LEFT JOIN', 'ipb_user=u1.user_id' ),
+					$u2 => array( 'LEFT JOIN', 'ipb_by=u2.user_id' )
 				)
 			);
-			$this->addFields(array('ipb_reason', 'u2.user_name blocker_name'));
+			$this->addFields( array( 'ipb_reason', 'u2.user_name blocker_name' ) );
 		}
 
-		$this->addOption('LIMIT', $limit + 1);
+		$this->addOption( 'LIMIT', $limit + 1 );
 
 		$this->addFields  ( 'u1.user_name, u1.user_id' );
 		$this->addFieldsIf( 'u1.user_editcount', $this->fld_editcount );
@@ -179,7 +179,7 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 
 		$this->addOption  ( 'ORDER BY', 'u1.user_name' );
 
-		$res = $this->select(__METHOD__);
+		$res = $this->select( __METHOD__ );
 
 		$data = array ();
 		$count = 0;
@@ -187,60 +187,60 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 		$lastUser = false;
 		$result = $this->getResult();
 
-		while (true) {
+		while ( true ) {
 
-			$row = $db->fetchObject($res);
+			$row = $db->fetchObject( $res );
 			$count++;
 
-			if (!$row || $lastUser !== $row->user_name) {
+			if ( !$row || $lastUser !== $row->user_name ) {
 				// Save the last pass's user data
-				if (is_array($lastUserData)) {
-					$fit = $result->addValue( array('query', $this->getModuleName()), null, $lastUserData );
-					if(!$fit) {
-						$this->setContinueEnumParameter( 'from', $this->keyToTitle($lastUserData['name']) );
+				if ( is_array( $lastUserData ) ) {
+					$fit = $result->addValue( array( 'query', $this->getModuleName() ), null, $lastUserData );
+					if ( !$fit ) {
+						$this->setContinueEnumParameter( 'from', $this->keyToTitle( $lastUserData['name'] ) );
 						break;
 					}
 				}
 
 				// No more rows left
-				if (!$row) {
+				if ( !$row ) {
 					break;
 				}
 
-				if ($count > $limit) {
+				if ( $count > $limit ) {
 					// We've reached the one extra which shows that there are additional pages to be had. Stop here...
-					$this->setContinueEnumParameter('from', $this->keyToTitle($row->user_name));
+					$this->setContinueEnumParameter( 'from', $this->keyToTitle( $row->user_name ) );
 					break;
 				}
 
 				// Record new user's data
 				$lastUser = $row->user_name;
 				$lastUserData = array( 'name' => $lastUser, 'id' => $row->user_id );
-				if ($this->fld_blockinfo) {
+				if ( $this->fld_blockinfo ) {
 					$lastUserData['blockedby'] = $row->blocker_name;
 					$lastUserData['blockreason'] = $row->ipb_reason;
 				}
-				if ($this->fld_editcount) {
-					$lastUserData['editcount'] = intval($row->user_editcount);
+				if ( $this->fld_editcount ) {
+					$lastUserData['editcount'] = intval( $row->user_editcount );
 				}
-				if ($this->fld_registration) {
-					$lastUserData['registration'] = wfTimestamp(TS_ISO_8601, $row->user_registration);
+				if ( $this->fld_registration ) {
+					$lastUserData['registration'] = wfTimestamp( TS_ISO_8601, $row->user_registration );
 				}
 
 				// Add user's group info
 				if ( $this->fld_groups ) {
-					$lastUserData['groups'] = ( !empty( $users ) && array_key_exists($row->user_id, $users) )
+					$lastUserData['groups'] = ( !empty( $users ) && array_key_exists( $row->user_id, $users ) )
 						? explode( ';', $users[$row->user_id] )
 						: array();
-					$result->setIndexedTagName($lastUserData['groups'], 'g');
+					$result->setIndexedTagName( $lastUserData['groups'], 'g' );
 				}
 			}
 
 		}
 
-		$db->freeResult($res);
+		$db->freeResult( $res );
 
-		$result->setIndexedTagName_internal(array('query', $this->getModuleName()), 'u');
+		$result->setIndexedTagName_internal( array( 'query', $this->getModuleName() ), 'u' );
 	}
 
 	private function local_users() {
@@ -251,42 +251,42 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 
 		# prop
 		$prop = $params['prop'];
-		if ( !is_null($prop) ) {
-			$prop = array_flip($prop);
+		if ( !is_null( $prop ) ) {
+			$prop = array_flip( $prop );
 		}
-		
+
 		# table
-		$this->addTables('events_local_users');
-		$this->addWhere( 'wiki_id = ' . intval($wgCityId) );
+		$this->addTables( 'events_local_users' );
+		$this->addWhere( 'wiki_id = ' . intval( $wgCityId ) );
 		$this->addWhere( 'user_is_closed = 0' );
-		
+
 		# limit
 		$limit = $params['limit'];
-		$this->addOption('LIMIT', $limit);		
-				
-		# params		
-		$fld_blockinfo 		= isset($prop['blockinfo']);
-		$fld_editcount 		= isset($prop['editcount']);
-		$fld_groups 		= isset($prop['groups']);
-		$fld_registration 	= isset($prop['registration']);
+		$this->addOption( 'LIMIT', $limit );
 
-		if ( !is_null($params['from']) )
-			$this->addWhere( 'user_name >= ' . $db->addQuotes($this->keyToTitle($params['from'])) );
+		# params
+		$fld_blockinfo 		= isset( $prop['blockinfo'] );
+		$fld_editcount 		= isset( $prop['editcount'] );
+		$fld_groups 		= isset( $prop['groups'] );
+		$fld_registration 	= isset( $prop['registration'] );
 
-		if ( !is_null($params['prefix']) )
+		if ( !is_null( $params['from'] ) )
+			$this->addWhere( 'user_name >= ' . $db->addQuotes( $this->keyToTitle( $params['from'] ) ) );
+
+		if ( !is_null( $params['prefix'] ) )
 			$this->addWhere( 'user_name' . $db->buildLike( $this->keyToTitle( $params['prefix'] ), $db->anyString() ) );
 
-		if ( !is_null($params['group']) ) {
+		if ( !is_null( $params['group'] ) ) {
 			$this->addWhere( 'all_groups' . $db->buildLike( $db->anyString(), $params['group'], $db->anyString() ) );
 		}
 
 		if ( $params['witheditsonly'] )
 			$this->addWhere( 'edits > 0' );
 
-		$this->addFields('user_id, user_name, cnt_groups, edits as user_editcount, all_groups, user_is_blocked');
-		$this->addOption('ORDER BY', 'user_name');
+		$this->addFields( 'user_id, user_name, cnt_groups, edits as user_editcount, all_groups, user_is_blocked' );
+		$this->addOption( 'ORDER BY', 'user_name' );
 
-		$res = $this->select(__METHOD__);
+		$res = $this->select( __METHOD__ );
 
 		$data = array ();
 		$count = 0;
@@ -294,12 +294,12 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 		$lastUser = false;
 		$result = $this->getResult();
 
-		while (true) {
-			$row = $db->fetchObject($res);
+		while ( true ) {
+			$row = $db->fetchObject( $res );
 			$count++;
 
 			if ( !$row || $lastUser !== $row->user_name ) {
-				if ( is_array($lastUserData) ) {
+				if ( is_array( $lastUserData ) ) {
 					$fit = $result->addValue( array( 'query', $this->getModuleName() ), null, $lastUserData );
 					if ( !$fit ) {
 						$this->setContinueEnumParameter( 'from', $this->keyToTitle( $lastUserData['name'] ) );
@@ -308,9 +308,9 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 				}
 
 				// No more rows left
-				if (!$row) break;
+				if ( !$row ) break;
 
-				if ($count > $limit) {
+				if ( $count > $limit ) {
 					$this->setContinueEnumParameter( 'from', $this->keyToTitle( $row->user_name ) );
 					break;
 				}
@@ -318,13 +318,13 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 				// Record new user's data
 				$lastUser = $row->user_name;
 				$lastUserData = array( 'name' => $lastUser );
-				
+
 				# is blocked
-				if ($this->fld_blockinfo) {
+				if ( $this->fld_blockinfo ) {
 					$blocker_name = $block_reason = '';
 					if ( $row->user_is_blocked ) {
-						$oBlock = Block::newFromTarget( User::whoIs($row->user_id), 0 ); 
-						if ( is_object($oBlock) ) {
+						$oBlock = Block::newFromTarget( User::whoIs( $row->user_id ), 0 );
+						if ( is_object( $oBlock ) ) {
 							$blocker_name = $oBlock->getByName();
 							$block_reason = $oBlock->mReason;
 						}
@@ -333,20 +333,20 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 					$lastUserData['blockreason'] = $block_reason;
 				}
 				# edit count
-				if ($this->fld_editcount) {
-					$lastUserData['editcount'] = intval($row->user_editcount);
+				if ( $this->fld_editcount ) {
+					$lastUserData['editcount'] = intval( $row->user_editcount );
 				}
-				if ($this->fld_registration) {		
-					$user_registration = $this->getUserRegistration($row->user_id);
-					$lastUserData['registration'] = wfTimestamp(TS_ISO_8601, $user_registration);
+				if ( $this->fld_registration ) {
+					$user_registration = $this->getUserRegistration( $row->user_id );
+					$lastUserData['registration'] = wfTimestamp( TS_ISO_8601, $user_registration );
 				}
 			}
 
 			// Add user's group info
 			if ( $this->fld_groups && $row->cnt_groups > 0 ) {
 				$use_group = array();
-				$groups = explode(";", $row->all_groups);
-				if ( !empty($groups) ) {
+				$groups = explode( ";", $row->all_groups );
+				if ( !empty( $groups ) ) {
 					foreach ( $groups as $group ) {
 						if ( !in_array( $group, $use_group ) ) {
 							$lastUserData['groups'][] = $group;
@@ -354,15 +354,15 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 						$use_group[] = $group;
 					}
 				}
-						
-				if ( isset($lastUserData['groups']) ) 
-					$result->setIndexedTagName($lastUserData['groups'], 'g');
+
+				if ( isset( $lastUserData['groups'] ) )
+					$result->setIndexedTagName( $lastUserData['groups'], 'g' );
 			}
 		}
 
-		$db->freeResult($res);
+		$db->freeResult( $res );
 
-		$result->setIndexedTagName_internal(array('query', $this->getModuleName()), 'u');
+		$result->setIndexedTagName_internal( array( 'query', $this->getModuleName() ), 'u' );
 	}
 
 	private function getBaseAllowedParams() {
@@ -404,7 +404,7 @@ class WikiaApiQueryAllUsers extends ApiQueryBase {
 			'activeusers' => false,
 		);
 	}
-	
+
 	public function getAllowedParams() {
 		$params = $this->getBaseAllowedParams();
 		$params['local'] = array (
