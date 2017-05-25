64b32a3908ff98dcf897637e3c8b475d88054e6d
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-05-25 09:57:48 +0200

    ADEN-5235 Disaster recovery

--- extensions/wikia/AdEngine/js/provider/directGpt.js
+++ extensions/wikia/AdEngine/js/provider/directGpt.js
@@ -62,9 +62,6 @@ define('ext.wikia.adEngine.provider.directGpt', [
 				'INCONTENT_BOXAD_1',
 				'INCONTENT_PLAYER',
 				'INVISIBLE_HIGH_IMPACT_2'
-			],
-			adMixBtfSlots: [
-				'INCONTENT_BOXAD_1'
 			]
 		}
 	);
