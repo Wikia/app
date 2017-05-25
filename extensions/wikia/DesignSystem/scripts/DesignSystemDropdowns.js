72a1614ac66cfa5255d6c61c9f9d99222efd0848
Igor Rogatty <igor@wikia-inc.com>
2017-05-25 12:51:21 +0200

    XW-3296 | formatting

--- extensions/wikia/DesignSystem/scripts/DesignSystemDropdowns.js
+++ extensions/wikia/DesignSystem/scripts/DesignSystemDropdowns.js
@@ -1,14 +1,19 @@
 $(function ($) {
 	'use strict';
 
+	var className = 'is-clicked';
+
+	// On touch devices, the first click opens dropdown, the second one opens the link if there is any
 	if (window.Wikia.isTouchScreen()) {
-		$('body').on('click touchstart', '.wds-dropdown', function (e) {
-			if (!$(this).hasClass('is-clicked')) {
-				$(this).addClass('is-clicked');
-				e.preventDefault();
-			}
-		}).on('mouseleave', '.wds-dropdown', function () {
-			$(this).removeClass('is-clicked');
-		});
+		$('body')
+			.on('touchstart', '.wds-dropdown', function (e) {
+				if (!$(this).hasClass(className)) {
+					$(this).addClass(className);
+					e.preventDefault();
+				}
+			})
+			.on('mouseleave', '.wds-dropdown', function () {
+				$(this).removeClass(className);
+			});
 	}
 });
