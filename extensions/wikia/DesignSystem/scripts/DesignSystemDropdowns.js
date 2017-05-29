c0370c83ccd3c636e43b04aaed8d99a4de1eec0a
Igor Rogatty <igor@wikia-inc.com>
2017-05-29 12:39:49 +0200

    XW-3300 | fix dropdowns on touch devices

--- extensions/wikia/DesignSystem/scripts/DesignSystemDropdowns.js
+++ extensions/wikia/DesignSystem/scripts/DesignSystemDropdowns.js
@@ -5,14 +5,14 @@ $(function ($) {
 
 	// On touch devices, the first click opens dropdown, the second one opens the link if there is any
 	if (window.Wikia.isTouchScreen()) {
-		$('body')
-			.on('touchstart', '.wds-dropdown', function (event) {
+		$(window.document)
+			.on('click', '.wds-dropdown, .wds-dropdown-level-2', function (event) {
 				if (!$(this).hasClass(className)) {
 					$(this).addClass(className);
 					event.preventDefault();
 				}
 			})
-			.on('mouseleave', '.wds-dropdown', function () {
+			.on('mouseleave', '.wds-dropdown, .wds-dropdown-level-2', function () {
 				$(this).removeClass(className);
 			});
 	}
