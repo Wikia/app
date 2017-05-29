8f2c93c80ed04d292b065c8a2b5eb25688c1c404
Igor Rogatty <igor@wikia-inc.com>
2017-05-29 18:01:42 +0200

    XW-3300 | move local nav to separate template

--- extensions/wikia/CommunityHeader/CommunityHeaderController.class.php
+++ extensions/wikia/CommunityHeader/CommunityHeaderController.class.php
@@ -27,4 +27,8 @@ class CommunityHeaderController extends WikiaController {
 				->getValue() ?? ''
 		);
 	}
+
+	public function localNavigation() {
+		$this->setVal( 'navigation', $this->getVal( 'navigation' ) );
+	}
 }
