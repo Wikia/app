a0edca43f24a61e69e6eaa03a09147988717440e
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-05-29 16:24:25 +0200

    XW-3300: handling empty dropdowns

--- extensions/wikia/CommunityHeader/templates/CommunityHeader_index.php
+++ extensions/wikia/CommunityHeader/templates/CommunityHeader_index.php
@@ -37,41 +37,49 @@
 		<ul class="wds-tabs">
 			<? foreach( $navigation->localNavigation as $firstLevelItem ): ?>
 				<li class="wds-tabs__tab">
-					<div class="wds-dropdown">
-						<div class="wds-tabs__tab-label wds-dropdown__toggle">
-							<a href="<?= $firstLevelItem['href'] ?? '#' ?>">
-								<span><?= $firstLevelItem['textEscaped'] ?></span>
-							</a>
-							<?= DesignSystemHelper::renderSvg('wds-icons-dropdown-tiny', 'wds-icon wds-icon-tiny wds-dropdown__toggle-chevron'); ?>
+					<? if( !empty( $firstLevelItem['children'] ) ): ?>
+						<div class="wds-dropdown">
+							<div class="wds-tabs__tab-label wds-dropdown__toggle">
+								<a href="<?= $firstLevelItem['href'] ?? '#' ?>">
+									<span><?= $firstLevelItem['textEscaped'] ?></span>
+								</a>
+								<?= DesignSystemHelper::renderSvg('wds-icons-dropdown-tiny', 'wds-icon wds-icon-tiny wds-dropdown__toggle-chevron'); ?>
+							</div>
+							<div class="wds-is-not-scrollable wds-dropdown__content">
+								<ul class="wds-list wds-is-linked wds-has-bolded-items">
+									<? foreach( $firstLevelItem['children'] as $index => $secondLevelItem ): ?>
+										<? if( array_key_exists( 'children', $secondLevelItem ) && !empty( $secondLevelItem['children'] ) ): ?>
+											<li class="<?= $index > count($secondLevelItem['children']) - 1 ? 'wds-is-sticked-to-parent ' : '' ?>wds-dropdown-level-2">
+												<a href="<?= $secondLevelItem['href'] ?? '#' ?>" class="wds-dropdown-level-2__toggle">
+													<span><?= $secondLevelItem['textEscaped'] ?></span>
+													<?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny', 'wds-icon wds-icon-tiny wds-dropdown-chevron' ); ?>
+												</a>
+												<div class="wds-is-not-scrollable wds-dropdown-level-2__content">
+													<ul class="wds-list wds-is-linked">
+														<? foreach( $secondLevelItem['children'] as $thirdLevelItem ): ?>
+															<li>
+																<a href="<?= $thirdLevelItem['href'] ?? '#' ?>"><?= $thirdLevelItem['textEscaped'] ?></a>
+															</li>
+														<? endforeach; ?>
+													</ul>
+												</div>
+											</li>
+										<? else : ?>
+											<li>
+												<a href="<?= $secondLevelItem['href'] ?? '#' ?>"><?= $secondLevelItem['textEscaped'] ?></a>
+											</li>
+										<? endif; ?>
+									<? endforeach; ?>
+								</ul>
+							</div>
 						</div>
-						<div class="wds-is-not-scrollable wds-dropdown__content">
-							<ul class="wds-list wds-is-linked wds-has-bolded-items">
-								<? foreach( $firstLevelItem['children'] as $index => $secondLevelItem ): ?>
-									<? if( array_key_exists( 'children', $secondLevelItem ) ): ?>
-										<li class="<?= $index > count($secondLevelItem['children']) - 1 ? 'wds-is-sticked-to-parent ' : '' ?>wds-dropdown-level-2">
-											<a href="<?= $secondLevelItem['href'] ?? '#' ?>" class="wds-dropdown-level-2__toggle">
-												<span><?= $secondLevelItem['textEscaped'] ?></span>
-												<?= DesignSystemHelper::renderSvg( 'wds-icons-menu-control-tiny', 'wds-icon wds-icon-tiny wds-dropdown-chevron' ); ?>
-											</a>
-											<div class="wds-is-not-scrollable wds-dropdown-level-2__content">
-												<ul class="wds-list wds-is-linked">
-													<? foreach( $secondLevelItem['children'] as $thirdLevelItem ): ?>
-														<li>
-															<a href="<?= $thirdLevelItem['href'] ?? '#' ?>"><?= $thirdLevelItem['textEscaped'] ?></a>
-														</li>
-													<? endforeach; ?>
-												</ul>
-											</div>
-										</li>
-									<? else : ?>
-										<li>
-											<a href="<?= $secondLevelItem['href'] ?? '#' ?>"><?= $secondLevelItem['textEscaped'] ?></a>
-										</li>
-									<? endif; ?>
-								<? endforeach; ?>
-							</ul>
-						</div>
+					<? else : ?>
+					<div class="wds-tabs__tab-label">
+						<a href="<?= $firstLevelItem['href'] ?? '#' ?>">
+							<span><?= $firstLevelItem['textEscaped'] ?></span>
+						</a>
 					</div>
+					<? endif; ?>
 				</li>
 			<? endforeach; ?>
 			<li class="wds-tabs__tab">
