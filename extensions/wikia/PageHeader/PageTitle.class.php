38fe929c38062c7d2090b4741e83fb162dabaa0a
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-05-30 13:40:41 +0200

    Fix code style XW-3327

--- extensions/wikia/PageHeader/PageTitle.class.php
+++ extensions/wikia/PageHeader/PageTitle.class.php
@@ -4,82 +4,80 @@ namespace PageHeader;
 
 use \RequestContext;
 
-class PageTitle
-{
+class PageTitle {
 
-    /* @var string */
-    public $title;
+	/* @var string */
+	public $title;
 
-    /* @var string */
-    public $prefix;
+	/* @var string */
+	public $prefix;
 
-    /* @var \WikiaGlobalRegistry */
-    private $wg;
+	/* @var \WikiaGlobalRegistry */
+	private $wg;
 
-    /* @var int */
-    private $namespace;
+	/* @var int */
+	private $namespace;
 
-    const PREFIX_LESS_NAMESPACES = [NS_MEDIAWIKI, NS_TEMPLATE, NS_CATEGORY, NS_FILE];
+	const PREFIX_LESS_NAMESPACES = [ NS_MEDIAWIKI, NS_TEMPLATE, NS_CATEGORY, NS_FILE ];
 
-    /**
-     * @param \WikiaApp $app
-     */
-    public function __construct($app)
-    {
-        $this->wg = $app->wg;
-        $this->MWTitle = RequestContext::getMain()->getTitle();
+	/**
+	 * @param \WikiaApp $app
+	 */
+	public function __construct( $app ) {
+		$this->wg = $app->wg;
+		$this->MWTitle = RequestContext::getMain()->getTitle();
 
-        $this->namespace = $this->MWTitle->getNamespace();
-        $this->title = $this->handleTitle($app);
-        $this->prefix = $this->handlePrefix();
-    }
+		$this->namespace = $this->MWTitle->getNamespace();
+		$this->title = $this->handleTitle($app);
+		$this->prefix = $this->handlePrefix();
+	}
 
-    private function handleTitle(\WikiaApp $app): string {
-        if (\WikiaPageType::isMainPage()) {
-            return $this->titleMainPage();
-        } else if (in_array($this->namespace, \BodyController::getUserPagesNamespaces())) {
-            return $this->titleUserPages();
-        } else if ($this->MWTitle->isTalkPage() || $this->shouldNotDisplayNamespacePrefix($this->namespace)) {
-            return htmlspecialchars( $this->wg->Title->getText() );
-        }
+	private function handleTitle( \WikiaApp $app ): string {
+		if (\WikiaPageType::isMainPage()) {
+			return $this->titleMainPage();
+		} else if (in_array($this->namespace, \BodyController::getUserPagesNamespaces())) {
+			return $this->titleUserPages();
+		} else if ($this->MWTitle->isTalkPage() || $this->shouldNotDisplayNamespacePrefix($this->namespace)) {
+			return htmlspecialchars($this->wg->Title->getText());
+		}
 
-        return $app->getSkinTemplateObj()->data['title'];
-    }
+		return $app->getSkinTemplateObj()->data['title'];
+	}
 
-    private function handlePrefix() {
-        if ($this->MWTitle->isTalkPage()) {
-            return $this->wg->ContLang->getNsText(NS_TALK);
-        }
+	private function handlePrefix() {
+		if ($this->MWTitle->isTalkPage()) {
+			return $this->wg->ContLang->getNsText(NS_TALK);
+		}
 
-        return null;
-    }
+		return null;
+	}
 
 
-    /**
-     * @return bool
-     */
-    private function shouldNotDisplayNamespacePrefix($namespace): bool {
-        return in_array($namespace,
-            array_merge(self::PREFIX_LESS_NAMESPACES, $this->wg->SuppressNamespacePrefix)
-        );
-    }
+	/**
+	 * @return bool
+	 */
+	private function shouldNotDisplayNamespacePrefix( $namespace ): bool {
+		return in_array($namespace,
+			array_merge(self::PREFIX_LESS_NAMESPACES, $this->wg->SuppressNamespacePrefix)
+		);
+	}
 
 
-    /**
-     * @return string
-     */
-    private function titleUserPages() {
-        $title = explode(':', $this->title, 2); // User:Foo/World_Of_Warcraft:_Residers_in_Shadows (BAC-494)
-        if (count($title) >= 2 && $this->wg->Title->getNsText() == str_replace(' ', '_', $title[0])) // in case of error page (showErrorPage) $title is just a string (cannot explode it)
-            $this->title = $title[1];
+	/**
+	 * @return string
+	 */
+	private function titleUserPages() {
+		$title = explode(':', $this->title, 2); // User:Foo/World_Of_Warcraft:_Residers_in_Shadows (BAC-494)
+		if (count($title) >= 2 && $this->wg->Title->getNsText() == str_replace(' ', '_', $title[0])) // in case of error page (showErrorPage) $title is just a string (cannot explode it)
+			$this->title = $title[1];
 
-        return 'UserPage!';
-    }
+		return 'UserPage!';
+	}
 
-    /**
-     * @return string
-     */
-    private function titleMainPage(): string {
-        return wfMessage('oasis-home')->escaped();
-    }
+	/**
+	 * @return string
+	 */
+	private function titleMainPage(): string {
+		return wfMessage('oasis-home')->escaped();
+	}
 }
--- extensions/wikia/PageHeader/templates/PageHeaderController_index.php
+++ extensions/wikia/PageHeader/templates/PageHeaderController_index.php
@@ -1,4 +1,4 @@
 <header class="page-header">
-	<h1><?= !empty( $pageTitle->prefix ) ? '<span>' . $pageTitle->prefix . ':</span>' : '' ?><?= $pageTitle->title ?></h1>
-    <hr>
+	<h1><?= !empty($pageTitle->prefix) ? '<span>' . $pageTitle->prefix . ':</span>' : '' ?><?= $pageTitle->title ?></h1>
+	<hr>
 </header>
