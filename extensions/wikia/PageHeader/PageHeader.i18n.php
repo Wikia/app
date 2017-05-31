bab56158ec091527cacf2292eea8e6b5e72616b0
Igor Rogatty <igor@wikia-inc.com>
2017-05-31 13:20:59 +0200

    XW-3391 | add counter for blog posts

--- extensions/wikia/PageHeader/PageHeader.i18n.php
+++ extensions/wikia/PageHeader/PageHeader.i18n.php
@@ -2,9 +2,11 @@
 $messages = array();
 
 $messages['en'] = array(
-	'page-header-counter-videos' => '{{FORMATNUM:$1}} {{PLURAL:$1|video|videos}}'
+	'page-header-counter-videos' => '{{FORMATNUM:$1}} {{PLURAL:$1|video|videos}}',
+	'page-header-counter-blog-posts' => '{{FORMATNUM:$1}} {{PLURAL:$1|post|posts}}',
 );
 
 $messages['qqq'] = array(
-	'page-header-counter-videos' => 'Number of videos on the wiki'
+	'page-header-counter-videos' => 'Number of videos on the wiki',
+	'page-header-counter-blog-posts' => 'Number of posts on the current blog',
 );
