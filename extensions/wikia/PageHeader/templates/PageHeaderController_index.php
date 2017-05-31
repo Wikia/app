3c08c4463e34b8a6f794811ef5c23162b38adf3c
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-05-31 11:51:41 +0200

    missing space after span XW-3327

--- extensions/wikia/PageHeader/templates/PageHeaderController_index.php
+++ extensions/wikia/PageHeader/templates/PageHeaderController_index.php
@@ -1,4 +1,4 @@
 <header class="page-header">
-	<h1 class="page-header__title"><?= !empty( $pageTitle->prefix ) ? '<span>' . $pageTitle->prefix . ':</span>' : '' ?><?= $pageTitle->title ?></h1>
+	<h1 class="page-header__title"><?= !empty( $pageTitle->prefix ) ? '<span>' . $pageTitle->prefix . ':</span> ' : '' ?><?= $pageTitle->title ?></h1>
 	<hr class="page-header__separator">
 </header>
