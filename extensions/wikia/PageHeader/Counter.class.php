c0e53819971a7583670c796633d14765cd125394
Igor Rogatty <igor@wikia-inc.com>
2017-05-31 14:10:57 +0200

    XW-3391 | add counter for forum threads

--- extensions/wikia/PageHeader/Counter.class.php
+++ extensions/wikia/PageHeader/Counter.class.php
@@ -2,6 +2,9 @@
 
 namespace Wikia\PageHeader;
 
+use Forum;
+use ForumBoard;
+use Title;
 use WikiaApp;
 
 class Counter {
@@ -9,31 +12,49 @@ class Counter {
 		$title = $app->wg->title;
 
 		if ( $title->isSpecial( 'Videos' ) ) {
-			$this->message = $this->getMessageForVideos();
+			$this->message = $this->getMessageForSpecialVideos();
 		} else if ( $title->isSpecial( 'Images' ) ) {
-			$this->message = $this->getMessageForImages( $app );
+			$this->message = $this->getMessageForSpecialImages();
 		} else if ( $title->inNamespace( NS_BLOG_LISTING ) ) {
-			$this->message = $this->getMessageForBlogPosts( $app );
+			$this->message = $this->getMessageForBlogListing( $app );
+		} else if ( $title->isSpecial( 'Forum' ) ) {
+			$this->message = $this->getMessageForForum();
+		} else if ( $title->inNamespace( NS_WIKIA_FORUM_BOARD ) ) {
+			$this->message = $this->getMessageForForumBoard( $title );
 		}
 	}
 
-	private function getMessageForVideos() {
+	private function getMessageForSpecialVideos() {
 		$mediaService = ( new \MediaQueryService );
 		$count = $mediaService->getTotalVideos();
 
 		return wfMessage( 'page-header-counter-videos', $count )->parse();
 	}
 
-	private function getMessageForImages() {
+	private function getMessageForSpecialImages() {
 		$mediaService = ( new \MediaQueryService );
 		$count = $mediaService->getTotalImages();
 
 		return wfMessage( 'page-header-counter-images', $count )->parse();
 	}
 
-	private function getMessageForBlogPosts( WikiaApp $app ) {
+	private function getMessageForBlogListing( WikiaApp $app ) {
 		$count = $app->wg->Parser->getOutput()->getProperty('blogPostCount');
 
 		return wfMessage( 'page-header-counter-blog-posts', $count )->parse();
+	}
+
+	private function getMessageForForum() {
+		$forum = new Forum();
+		$count = $forum->getTotalThreads();
+
+		return wfMessage( 'page-header-counter-forum-threads', $count )->parse();
+	}
+
+	private function getMessageForForumBoard( Title $title ) {
+		$forumBoard = ForumBoard::newFromTitle( $title );
+		$count = $forumBoard->getThreadCount();
+
+		return wfMessage( 'page-header-counter-forum-threads', $count )->parse();
 	}
 }
--- extensions/wikia/PageHeader/PageHeader.i18n.php
+++ extensions/wikia/PageHeader/PageHeader.i18n.php
@@ -5,10 +5,12 @@ $messages['en'] = array(
 	'page-header-counter-videos' => '{{FORMATNUM:$1}} {{PLURAL:$1|video|videos}}',
 	'page-header-counter-images' => '{{FORMATNUM:$1}} {{PLURAL:$1|image|images}}',
 	'page-header-counter-blog-posts' => '{{FORMATNUM:$1}} {{PLURAL:$1|post|posts}}',
+	'page-header-counter-forum-threads' => '{{FORMATNUM:$1}} {{PLURAL:$1|thread|threads}}',
 );
 
 $messages['qqq'] = array(
 	'page-header-counter-videos' => 'Number of videos on the wiki',
 	'page-header-counter-images' => 'Number of images on the wiki',
 	'page-header-counter-blog-posts' => 'Number of posts on the current blog',
+	'page-header-counter-forum-threads' => 'Number of forum threads',
 );
