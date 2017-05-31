eb3c78214c50a066701fa89cc9d3e6d0799623a3
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2017-05-31 17:08:27 +0200

    ADEN-5269 Add MOAT tracking to feature video preroll

--- extensions/wikia/ArticleVideo/scripts/ooyala/google_ima.js
+++ extensions/wikia/ArticleVideo/scripts/ooyala/google_ima.js
@@ -1,3 +1,5 @@
+
+
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 	if (!OO)
 	{
@@ -4356,6 +4358,15 @@
 					}
 					adsSettings.useStyledLinearAds = this.useGoogleAdUI;
 					_IMAAdsManager = adsManagerLoadedEvent.getAdsManager(_playheadTracker, adsSettings);
+
+					var ids = {
+						partnerCode: 'wikiaimajsint377461931603',
+						viewMode: google.ima.ViewMode.NORMAL,
+						slicer1: 'ooyala',
+						slicer2: ''
+					};
+
+					window.initMoatTracking(_IMAAdsManager, ids, _uiContainer);
 
 					// When the ads manager is ready, we are ready to apply css changes to the video element
 					// If the sharedVideoElement is not used, mark it as null before applying css
