73b5e7b0d6356a7d4bcaa67f89922f1a1399b1e9
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-05-31 12:18:30 +0200

    add tests for namespace for templates spaces and arrays

---
+++ includes/wikia/tests/_fixtures/TestATestBTestController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace TestA\TestB;
+use \WikiaController;
+
+global $wgAutoloadClasses;
+$wgAutoloadClasses['TestA\TestB\TestController'] = dirname(__FILE__) . '/TestATestBTestController.php';
+$wgAutoloadClasses['TestA\TestB\AnotherTestController'] = dirname(__FILE__) . '/TestATestBTestController.php';
+
+class TestController extends WikiaController {
+
+	/**
+	 * This method does nothing and is available in json context only
+	 */
+	public function jsonOnly() {
+	}
+
+	public function index() {
+		$this->getResponse()->setVal("wasCalled", "index");
+	}
+
+	public function sendTest() {
+		$this->sendRequest( 'nonExistentController', 'test' );
+	}
+
+	public function forwardTest() {
+		$resetResponse = (bool) $this->getRequest()->getVal( 'resetResponse', false );
+
+		$this->getResponse()->setVal( 'content', true );
+		$this->getResponse()->setVal( 'controller', __CLASS__ );
+
+		// set some data so we can check that resetData works
+		$this->getResponse()->setVal( 'forwardTest', true );
+
+		$this->forward( 'TestA\TestB\AnotherTest', 'hello', $resetResponse);
+	}
+
+	public function overrideTemplateTest(){
+		$this->response->setVal( 'output', $this->request->getVal( 'input' ) );
+		$this->overrideTemplate( $this->request->getVal( 'template' ) );
+	}
+
+	// This is for testing dispatch by skin
+	public function skinRoutingTest() {
+		$this->getResponse()->setVal( 'wasCalled', "skinRouting");
+	}
+
+	// This is for testing dispatch by global var
+	public function globalRoutingTest() {
+		$this->getResponse()->setVal( 'wasCalled', "globalRouting");
+	}
+}
+
+class AnotherTestController extends WikiaController {
+
+	public function index() {
+		$this->skipRendering();
+		// this is for testing dispatch by * (total controller override)
+		$this->getResponse()->setVal( 'wasCalled', 'controllerRouting');
+	}
+
+	public function hello() {
+		$this->getResponse()->setVal( 'controller', __CLASS__ );
+		$this->getResponse()->setVal( 'wasCalled', "hello" );
+		$this->getResponse()->setVal( 'foo', true );
+
+	}
+}
