30641e63ca50566fe562620342291b7ba1736617
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-05-31 11:17:17 +0200

    include PageHeader extension by default XW-3327

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -634,6 +634,7 @@ include_once( "$IP/extensions/wikia/PageShare/PageShare.setup.php" );
 include_once( "$IP/extensions/wikia/PaidAssetDrop/PaidAssetDrop.setup.php" );
 include_once( "$IP/extensions/wikia/CreateNewWiki/CreateNewWiki_global_setup.php" );
 include_once( "$IP/extensions/wikia/Security/Security.setup.php" );
+include_once( "$IP/extensions/wikia/PageHeader/PageHeader.setup.php" );
 
 /**
  * @name $wgSkipSkins
