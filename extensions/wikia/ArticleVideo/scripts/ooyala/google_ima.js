c22f8e3e3faf3544a795651fbc4986656870b7fd
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2017-05-31 17:30:12 +0200

    ADEN-5269 Use exisiting modules to track MOAT

--- extensions/wikia/ArticleVideo/scripts/ooyala/google_ima.js
+++ extensions/wikia/ArticleVideo/scripts/ooyala/google_ima.js
@@ -1,4 +1,4 @@
-
+var globalRequire = require;
 
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 	if (!OO)
@@ -4358,6 +4358,10 @@
 					}
 					adsSettings.useStyledLinearAds = this.useGoogleAdUI;
 					_IMAAdsManager = adsManagerLoadedEvent.getAdsManager(_playheadTracker, adsSettings);
+
+					globalRequire(['ext.wikia.adEngine.video.player.porvata.moatVideoTracker'], function(moatVideoTracker) {
+						moatVideoTracker.init(_IMAAdsManager, _uiContainer, google.ima.ViewMode.NORMAL, 'ooyala', 'featured-video');
+					});
 
 					var ids = {
 						partnerCode: 'wikiaimajsint377461931603',
