8e407ac7baeace0421645266aafc6c075c1263ba
JamesHawking <jakubj91@gmail.com>
2017-06-01 15:29:36 +0200

    XW-3328 | Separeted categories to diffrent file.

--- extensions/wikia/PageHeader/PageHeaderController.class.php
+++ extensions/wikia/PageHeader/PageHeaderController.class.php
@@ -5,8 +5,11 @@ namespace Wikia\PageHeader;
 class PageHeaderController extends \WikiaController {
 
 	public function index() {
-		$this->setVal( 'categories', new Categories( $this->app ) );
 		$this->setVal( 'counter', new Counter( $this->app ) );
 		$this->setVal( 'pageTitle', new PageTitle( $this->app ) );
+	}
+
+	public function categories() {
+		$this->setVal( 'categories', new Categories( $this->app ) );
 	}
 }
