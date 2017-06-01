232b3db513254009c86fe65a6d303498da5309a1
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-06-01 13:59:08 +0200

    XW-3331: links in button and dropdown

--- extensions/wikia/PageHeader/PageHeaderController.class.php
+++ extensions/wikia/PageHeader/PageHeaderController.class.php
@@ -2,6 +2,8 @@
 
 namespace Wikia\PageHeader;
 
+use CuratedContentHelper;
+
 class PageHeaderController extends \WikiaController {
 
 	public function index() {
@@ -14,6 +16,35 @@ class PageHeaderController extends \WikiaController {
 	}
 
 	public function editButton() {
+		global $wgEnableCuratedContentExt;
 
+		$skinVars = $this->app->getSkinTemplateObj()->data;
+		$content_actions = $skinVars['content_actions'];
+
+		$actions = [ 'edit', 've-edit', 'formedit', 'history', 'move', 'protect', 'unprotect', 'delete', 'undelete', 'replace-file' ];
+
+		// Enable to modify actions list on dropdown
+		wfRunHooks( 'PageHeaderDropdownActions', [ &$actions ] );
+
+		$dropdownActions = [];
+		foreach ( $actions as $action ) {
+			if ( isset( $content_actions[$action] ) ) {
+				if( $content_actions[$action]['main'] ?? false ) {
+					$this->setVal( 'buttonAction', $content_actions[$action] );
+				} else {
+					$dropdownActions[$action] = $content_actions[$action];
+				}
+			}
+		}
+
+		if ( !empty( $wgEnableCuratedContentExt ) && CuratedContentHelper::shouldDisplayToolButton() ) {
+			$dropdownActions[] = [
+				'href' => '/main/edit?useskin=wikiamobile',
+				'text' => wfMessage( 'wikiacuratedcontent-edit-mobile-main-page' )->escaped(),
+				'id' => 'CuratedContentTool'
+			];
+		}
+
+		$this->setVal('dropdownActions', $dropdownActions );
 	}
 }
--- extensions/wikia/PageHeader/templates/PageHeaderController_editButton.php
+++ extensions/wikia/PageHeader/templates/PageHeaderController_editButton.php
@@ -1,17 +1,21 @@
 <div class="wds-button-group page-header__edit-button">
-	<button class="wds-is-squished wds-button">
+	<a href="<?= $buttonAction['href'] ?>" class="wds-is-squished wds-button" id="<?= $buttonAction['id'] ?>" accesskey="<?= $buttonAction['accesskey'] ?>">
 		<?= DesignSystemHelper::renderSvg( 'wds-icons-pencil-small', 'wds-icon wds-icon-small' ); ?>
-		<span>Edit</span>
-	</button>
+		<span><?= $buttonAction['text'] ?></span>
+	</a>
 	<div class="wds-dropdown">
 		<div class="wds-button wds-is-squished wds-dropdown__toggle">
 			<?= DesignSystemHelper::renderSvg( 'wds-icons-dropdown-tiny', 'wds-icon wds-icon-tiny wds-dropdown__toggle-chevron' ); ?>
 		</div>
-		<div class="wds-dropdown__content">
-			<ul class="wds-list">
-				<li>List item 1</li>
-				<li>List item 2</li>
-				<li>List item 3</li>
+		<div class="wds-dropdown__content wds-is-not-scrollable">
+			<ul class="wds-list wds-is-linked">
+				<? foreach( $dropdownActions as $action ): ?>
+					<li>
+						<a id="<?= $action['id'] ?>" href="<?= $action['href'] ?>">
+							<?= $action['text'] ?>
+						</a>
+					</li>
+				<? endforeach; ?>
 			</ul>
 		</div>
 	</div>
