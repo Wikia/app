16afe84d8baa15535dde7893b73515abb899d4b0
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-06-05 13:05:31 +0200

    XW-3331: strings for translation

--- extensions/wikia/PageHeader/PageHeader.i18n.php
+++ extensions/wikia/PageHeader/PageHeader.i18n.php
@@ -11,6 +11,10 @@ $messages['en'] = array(
 	'page-header-categories-more-separator' => ', ',
 	'page-header-categories-more' => 'and {{FORMATNUM:$1}} more',
 	'page-header-action-button-talk' => 'Talk ({{FORMATNUM:$1}})',
+	'page-header-action-button-add-topic' => 'Add topic',
+	'page-header-action-button-viewsource' => 'View source',
+	'page-header-action-button-edit-mobile-main-page' => 'Edit mobile main page',
+	'page-header-action-button-edit' => 'Edit',
 );
 
 $messages['qqq'] = array(
@@ -22,5 +26,9 @@ $messages['qqq'] = array(
 	'page-header-categories-in' => 'Label displayed before list of categories that given article belongs to',
 	'page-header-categories-more-separator' => 'Separator between first couple of categories and "more categories" link',
 	'page-header-categories-more' => 'Link to more categories that are hidden by default',
-	'page-header-action-button-talk' => 'Label for link to talk page, number of talk pages in parenthesis'
+	'page-header-action-button-talk' => 'Label for link to talk page, number of talk pages in parenthesis',
+	'page-header-action-button-add-topic' => 'Add topic link in edit dropdown',
+	'page-header-action-button-viewsource' => 'Link to page with source of the article',
+	'page-header-action-button-edit-mobile-main-page' => 'Link to mobile main page editor',
+	'page-header-action-button-edit' => 'Link to edit page'
 );
