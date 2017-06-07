775519fbb0dcbd08593aa5834ee9a6f39e54d5cf
Igor Rogatty <igor@wikia-inc.com>
2017-06-07 13:22:45 +0200

    XW-2761 | formatting

--- extensions/wikia/Forum/ForumHooksHelper.class.php
+++ extensions/wikia/Forum/ForumHooksHelper.class.php
@@ -481,14 +481,21 @@ class ForumHooksHelper {
 	 * @return bool
 	 */
 	public static function onAfterPageHeaderPageSubtitle( &$pageSubtitle, Title $title ): bool {
-		if ( in_array( $title->getNamespace(), [
+		if (
+			in_array( $title->getNamespace(), [
 				NS_WIKIA_FORUM_BOARD,
 				NS_WIKIA_FORUM_BOARD_THREAD,
 				NS_WIKIA_FORUM_TOPIC_BOARD,
-			] ) && RequestContext::getMain()->getRequest()->getVal( 'action' ) !== 'history'
+			] ) &&
+			RequestContext::getMain()->getRequest()->getVal( 'action' ) !== 'history'
 		) {
-			$pageSubtitle =
-				F::app()->renderView( 'Wall', 'brickHeader', [ 'id' => $title->getText() ] );
+			$pageSubtitle = F::app()->renderView(
+				'Wall',
+				'brickHeader',
+				[
+					'id' => $title->getText()
+				]
+			);
 		}
 
 		return true;
