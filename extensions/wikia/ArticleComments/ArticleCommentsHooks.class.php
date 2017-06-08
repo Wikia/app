edea601d4cdce1e6a1c3ce4ad79de64e9b96c5a0
Igor Rogatty <igor@wikia-inc.com>
2017-06-08 15:52:26 +0200

    XW-3332 | escape the message

--- extensions/wikia/ArticleComments/ArticleCommentsHooks.class.php
+++ extensions/wikia/ArticleComments/ArticleCommentsHooks.class.php
@@ -14,13 +14,16 @@ class ArticleCommentsHooks {
 		$comments = $service->getCommentsCount();
 
 		if ( $comments && ArticleCommentInit::ArticleCommentCheckTitle( $title ) ) {
-			$label =
-				wfMessage( 'article-comments-comments' )
+			$label = wfMessage( 'article-comments-comments' )
 					->params( CommentsLikesController::formatCount( $comments ) )
-					->parse();
-			array_unshift( $buttons,
-				new Button( $label, '', ArticleCommentInit::getCommentsLink( $title ),
-					'wds-is-secondary', '' ) );
+					->escaped();
+
+			array_unshift(
+				$buttons,
+				new Button(
+					$label, '', ArticleCommentInit::getCommentsLink( $title ), 'wds-is-secondary', ''
+				)
+			);
 		}
 
 		return true;
