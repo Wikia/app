47523c70f98a33828deeb2df41ddffc529767e03
Garth Webb <garth@wikia-inc.com>
2017-06-09 14:17:42 -0700

    IRIS-4548 Make sure we treat a null wgEnableForumExt as "true", the default value

--- extensions/wikia/Discussions/maintenance/SiteStatus.class.php
+++ extensions/wikia/Discussions/maintenance/SiteStatus.class.php
@@ -262,6 +262,14 @@ class SiteStatus {
 		$this->debug( "Updating wiki variables." );
 
 		$this->forumsEnabled = $this->getVariableValue( 'wgEnableForumExt' );
+
+		// If Forums are null (i.e. not explicitly false) it means there's no value set for
+		// this community in the DB.  The default value for forums is true in CommonExtensions
+		// so apply that default.
+		if ($this->forumsEnabled !== false) {
+			$this->forumsEnabled = true;
+		}
+
 		$this->discussionsEnabled = $this->getVariableValue( 'wgEnableDiscussions' );
 		$this->navigationEnabled = $this->getVariableValue( 'wgEnableDiscussionsNavigation' );
 	}
