5f86dd4e21b852a526bd617f94dcb4c55733c38c
Garth Webb <garth@wikia-inc.com>
2017-06-16 17:06:41 -0700

    Catch errors around missing comments_index table as well as skip out of a method when we can't update

--- extensions/wikia/Discussions/maintenance/SiteStatus.class.php
+++ extensions/wikia/Discussions/maintenance/SiteStatus.class.php
@@ -174,6 +174,9 @@ class SiteStatus {
 			$statement
 				->INSERT( self::TABLE_STATUS )
 				->SET( 'site_id', $this->siteId );
+		} else {
+			// If we don't meet either above conditions, skip writing this record.
+			return;
 		}
 
 		// Conditionally set these date columns only if we've found something
@@ -308,8 +311,12 @@ class SiteStatus {
 	private function findExistingPosts() {
 		$this->debug( "Finding existing posts" );
 
-		$this->findExistingThreadedForumPosts();
-		$this->findExistingWikiForumPosts();
+		try {
+			$this->findExistingThreadedForumPosts();
+			$this->findExistingWikiForumPosts();
+		} catch(\Exception $e) {
+			$this->debug("\tError finding existing posts: " . $e->getMessage() );
+		}
 	}
 
 	private function findExistingThreadedForumPosts() {
