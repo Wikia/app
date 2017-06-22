c13d236c157b3697c2e5841ec6e4a2167e555c79
dianafa <diana.falkowska@gmail.com>
2017-06-22 13:10:48 +0200

    ADEN-5399 insert scripts only on Chrome > 45

--- resources/wikia/modules/browserDetect.js
+++ resources/wikia/modules/browserDetect.js
@@ -46,6 +46,14 @@ define('wikia.browserDetect', ['wikia.window'], function (win) {
 		return userAgent.toLowerCase().indexOf('firefox') > -1;
 	}
 
+    /**
+     * Checks if the browser is Chrome
+     * @returns {Boolean}
+     */
+    function isChrome() {
+        return userAgent.toLowerCase().indexOf('chrome') > -1;
+    }
+
 	/**
 	 * Checks if the site is opened on iPad
 	 * @returns {boolean}
@@ -143,6 +151,7 @@ define('wikia.browserDetect', ['wikia.window'], function (win) {
 		getBrowser: getBrowser,
 		getBrowserVersion: getBrowserVersion,
 		getOS: getOS,
+		isChrome: isChrome,
 		isIE: isIE,
 		isFirefox: isFirefox,
 		isIPad: isIPad,
