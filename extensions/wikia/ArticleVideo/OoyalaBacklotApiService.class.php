93350445d0fccd2bcdcd7c29354a50d8a1ee3641
Igor Rogatty <igor@wikia-inc.com>
2017-06-27 20:05:41 +0200

    XW-3608 | cache the Ooyala API response

--- extensions/wikia/ArticleVideo/OoyalaBacklotApiService.class.php
+++ extensions/wikia/ArticleVideo/OoyalaBacklotApiService.class.php
@@ -12,27 +12,32 @@ class OoyalaBacklotApiService {
 		);
 	}
 
-	//TODO use memcache
 	public function getLabels( string $videoId ): array {
-		$labels = [];
+		return WikiaDataAccess::cache(
+			wfMemcKey( __CLASS__, __METHOD__, $videoId ),
+			WikiaResponse::CACHE_STANDARD,
+			function () use ( $videoId ) {
+				try {
+					$labels = [];
 
-		try {
-			$result = $this->api->get(
-				'assets/' . $videoId,
-				[
-					'include' => 'labels'
-				]
-			);
+					$result = $this->api->get(
+						'assets/' . $videoId,
+						[
+							'include' => 'labels'
+						]
+					);
 
-			if ( !empty( $result->labels ) ) {
-				foreach ( $result->labels as $label ) {
-					$labels[] = $label->full_name;
+					if ( !empty( $result->labels ) ) {
+						foreach ( $result->labels as $label ) {
+							$labels[] = $label->full_name;
+						}
+					}
+				} catch ( OoyalaRequestErrorException $exception ) {
+					// On error, we return an empty array
 				}
-			}
-		} catch ( OoyalaRequestErrorException $exception ) {
-			// On error, we return an empty array
-		}
 
-		return $labels;
+				return $labels;
+			}
+		);
 	}
 }
