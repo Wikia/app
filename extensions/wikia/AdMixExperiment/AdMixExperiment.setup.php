29caf92f89bfed6d49c250fd7e12a141a209d833
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-06-30 13:46:23 +0200

    ADEN-5454 | Remove admix experiment code

--- extensions/wikia/AdMixExperiment/AdMixExperiment.setup.php
+++ extensions/wikia/AdMixExperiment/AdMixExperiment.setup.php
@@ -1,5 +1,2 @@
 <?php
 $wgAutoloadClasses['AdMixExperimentController'] = __DIR__ . '/AdMixExperimentController.class.php';
-$wgAutoloadClasses['AdMixExperimentHooks'] = __DIR__ . '/AdMixExperiment.hooks.php';
-
-$wgHooks['BeforePageDisplay'][] = 'AdMixExperimentHooks::onBeforePageDisplay';
