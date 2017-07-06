df005c70f51073a56f2df8bd8a7c8baf47e3a630
macbre <maciej.brencz@gmail.com>
2017-07-06 13:19:30 +0200

    SUS-2381 | move Bucky include to app

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -634,6 +634,7 @@ include_once( "$IP/extensions/wikia/CreateNewWiki/CreateNewWiki_global_setup.php
 include_once( "$IP/extensions/wikia/Security/Security.setup.php" );
 include_once( "$IP/extensions/wikia/CommunityHeader/CommunityHeader.setup.php" );
 include_once( "$IP/extensions/wikia/PageHeader/PageHeader.setup.php" );
+include_once( "$IP/extensions/wikia/Bucky/Bucky.setup.php" );
 
 /**
  * @name $wgSkipSkins
@@ -1680,12 +1681,6 @@ $wgLyricsItunesAffiliateToken = '';
  * Enables caching of search results on CDN
  */
 $wgEnableSpecialSearchCaching = true;
-
-/**
- * @name wgEnableBuckyExt
- * Enables real user performance reporting via Bucky
- */
-$wgEnableBuckyExt = true;
 
 /**
  * @name wgBuckySampling
