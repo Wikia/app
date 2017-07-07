0ca8594306d89e148b2222cb4b3cfee54c42d594
JamesHawking <jakubj91@gmail.com>
2017-07-07 15:55:03 +0200

    XW-3650 | Move logic into separate method

--- extensions/wikia/SiteWideMessages/SiteWideMessagesController.class.php
+++ extensions/wikia/SiteWideMessages/SiteWideMessagesController.class.php
@@ -5,14 +5,23 @@ class SiteWideMessagesController extends WikiaController {
 	const CACHE_VALIDITY_VARNISH = 10800; // 3 hours
 	const CACHE_VALIDITY_BROWSER = 3600; // 1 hour
 
-	public function getAnonMessages() {
+	private function shouldShowSiteWideMessage() {
 		global $wgEnableArticleFeaturedVideo;
 
-		$articleTitle = Title::newFromID( $this->getVal( 'articleId' ) )->getPrefixedDBkey();
-		$featuredVideoEmbedded = !empty( $wgEnableArticleFeaturedVideo ) &&
-			ArticleVideoContext::isFeaturedVideoEmbedded( $articleTitle );
+		$articleTitle = Title::newFromID( $this->getVal( 'articleId' ) );
+		if ( !is_null( $articleTitle ) ) {
+			$isFeaturedVideoEmbedded = !empty( $wgEnableArticleFeaturedVideo ) &&
+				ArticleVideoContext::isFeaturedVideoEmbedded( $articleTitle->getPrefixedDBkey() );
+		} else {
+			$isFeaturedVideoEmbedded = false;
+		}
 
-		if ( $this->wg->User->isLoggedIn() || $featuredVideoEmbedded ) {
+		return $this->wg->User->isLoggedIn() || $isFeaturedVideoEmbedded;
+	}
+
+	public function getAnonMessages() {
+
+		if ( $this->shouldShowSiteWideMessage() ) {
 			// Don't return anything if this happens
 			$this->skipRendering();
 
