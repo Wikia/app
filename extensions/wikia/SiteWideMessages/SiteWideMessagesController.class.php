52e696010ef002ea023ad89bc5ebf148b3b21be2
JamesHawking <jakubj91@gmail.com>
2017-07-07 15:33:24 +0200

    XW-3650 | Cleanup

--- extensions/wikia/SiteWideMessages/SiteWideMessagesController.class.php
+++ extensions/wikia/SiteWideMessages/SiteWideMessagesController.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class SiteWideMessagesController extends WikiaController  {
+class SiteWideMessagesController extends WikiaController {
 
 	const CACHE_VALIDITY_VARNISH = 10800; // 3 hours
 	const CACHE_VALIDITY_BROWSER = 3600; // 1 hour
@@ -8,12 +8,14 @@ class SiteWideMessagesController extends WikiaController  {
 	public function getAnonMessages() {
 		global $wgEnableArticleFeaturedVideo;
 
-		$articleTitle = Title::newFromID($this->getVal('articleId'))->getPrefixedDBkey();
-		$featuredVideoEmbedded = !empty( $wgEnableArticleFeaturedVideo ) && ArticleVideoContext::isFeaturedVideoEmbedded( $articleTitle );
-		
+		$articleTitle = Title::newFromID( $this->getVal( 'articleId' ) )->getPrefixedDBkey();
+		$featuredVideoEmbedded = !empty( $wgEnableArticleFeaturedVideo ) &&
+			ArticleVideoContext::isFeaturedVideoEmbedded( $articleTitle );
+
 		if ( $this->wg->User->isLoggedIn() || $featuredVideoEmbedded ) {
 			// Don't return anything if this happens
 			$this->skipRendering();
+
 			return;
 		}
 
