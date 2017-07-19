42b15a3c2bf80440de72e64286b2e38c7cad81f9
mech <wozniak.jacek@gmail.com>
2017-07-19 12:01:55 +0200

    SUS-2435: enabling all seo tweaks site-wide

--- extensions/wikia/SEOTweaks/SEOTweaks.setup.php
+++ extensions/wikia/SEOTweaks/SEOTweaks.setup.php
@@ -14,15 +14,12 @@ $wgAutoloadClasses['SEOTweaksHooksHelper'] =  $dir . 'SEOTweaksHooksHelper.class
 
 // hooks
 $wgHooks['ArticleRobotPolicy'][] = 'SEOTweaksHooksHelper::onArticleRobotPolicy';
+$wgHooks['ArticleViewHeader'][] = 'SEOTweaksHooksHelper::onArticleViewHeader';
+$wgHooks['BeforeParserMakeImageLinkObjOptions'][] = 'SEOTweaksHooksHelper::onBeforeParserMakeImageLinkObjOptions';
+$wgHooks['CategoryPageView'][] = 'SEOTweaksHooksHelper::onCategoryPageView';
+$wgHooks['ImagePageAfterImageLinks'][] = 'SEOTweaksHooksHelper::onImagePageAfterImageLinks';
 $wgHooks['OpenGraphMetaHeaders'][] = 'SEOTweaksHooksHelper::onOpenGraphMetaHeaders';
 $wgHooks['ShowMissingArticle'][] = 'SEOTweaksHooksHelper::onShowMissingArticle';
-
-if ( $wgLanguageCode == 'en' ) {
-    $wgHooks['ArticleViewHeader'][] = 'SEOTweaksHooksHelper::onArticleViewHeader';
-    $wgHooks['BeforeParserMakeImageLinkObjOptions'][] = 'SEOTweaksHooksHelper::onBeforeParserMakeImageLinkObjOptions';
-    $wgHooks['CategoryPageView'][] = 'SEOTweaksHooksHelper::onCategoryPageView';
-    $wgHooks['ImagePageAfterImageLinks'][] = 'SEOTweaksHooksHelper::onImagePageAfterImageLinks';
-}
 
 // messages
 $wgExtensionMessagesFiles['SEOTweaks'] = $dir . 'SEOTweaks.i18n.php';
