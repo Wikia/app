16728529a67447877bef4066681e26e4dcbdd5a6
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-07-25 14:07:30 +0200

    XW-3707 | Integrate Plista into ImpactFooter

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -158,6 +158,7 @@ $config['recirculation_js'] = [
 		'//extensions/wikia/Recirculation/js/views/impactFooter.js',
 		'//extensions/wikia/Recirculation/js/recirculation.js',
 		'//extensions/wikia/Recirculation/js/discussions.js',
+		'//extensions/wikia/Recirculation/js/plista.js',
 	],
 ];
 
---
+++ extensions/wikia/Recirculation/js/plista.js
@@ -0,0 +1,39 @@
+define('ext.wikia.recirculation.plista', function () {
+	function shouldFetchPlista(items) {
+		var index = items.findIndex(function (item) {
+			return item.presented_by;
+		});
+
+		return index === -1 && ['AU', 'NZ', 'DE'].indexOf(Geo.getCountryCode()) > -1;
+	}
+
+	function fetchPlista() {
+		return $.getJSON('http://farm.plista.com/recommendation/?publickey=845c651d11cf72a0f766713f&widgetname=api' +
+			'&count=1&adcount=1&image[width]=583&image[height]=328'
+		)
+			.then(function (data) {
+				return data[0];
+			});
+	}
+
+	function mapPlista(plistaData) {
+		return {
+			meta: 'wikia-impactfooter',
+			source: 'plista',
+			thumbnail: plistaData.img,
+			title: plistaData.title,
+			url: plistaData.url,
+			presented_by: 'Plista',
+			isPlista: true
+		}
+	}
+
+	function getPlista() {
+		return fetchPlista().then(mapPlista);
+	}
+
+	return {
+		get: getPlista,
+		shouldFetch: shouldFetchPlista
+	}
+});
