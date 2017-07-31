0e5f691cabd03b7b381bcfc4fc0dcef2490084a5
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-07-31 11:37:59 +0200

    XW-3336 | Include BrightCove Player

--- extensions/wikia/ArticleVideo/templates/ArticleVideo_featured.php
+++ extensions/wikia/ArticleVideo/templates/ArticleVideo_featured.php
@@ -1,26 +1,47 @@
-<div class="featured-video-wrapper">
-	<div id="article-video" class="article-video">
-		<img class="video-thumbnail"
-			 src="<?= Sanitizer::encodeAttribute( $videoDetails['thumbnailUrl'] ); ?>">
-		<div class="video-container">
-			<div class="video-placeholder">
-				<img class="video-thumbnail"
-					 src="<?= Sanitizer::encodeAttribute( $videoDetails['thumbnailUrl'] ); ?>">
-				<img src="<?= $closeIconUrl; ?>" class="close">
-				<svg class="spinner">
-					<circle cx="24" cy="24" r="22"></circle>
-				</svg>
-			</div>
-			<div id="ooyala-article-video" class="ooyala-article-video"></div>
-			<div class="video-details">
-				<div class="video-label"><?= wfMessage( 'articlevideo-watch' )->escaped() ?>
-					<span class="video-time"><?= $videoDetails['duration'] ?></span>
-				</div>
-				<div class="video-title"><?= htmlspecialchars( $videoDetails['title'] ) ?></div>
-				<?= $app->renderPartial( 'ArticleVideo', 'attribution', [ 'videoDetails' => $videoDetails ] ) ?>
-			</div>
-			<?= $app->renderPartial( 'ArticleVideo', 'feedback' ) ?>
-		</div>
-	</div>
-	<?= $app->renderPartial( 'ArticleVideo', 'attribution', [ 'videoDetails' => $videoDetails ] ) ?>
-</div>
+<video id="myPlayerID"
+	   data-embed="default"
+	   data-application-id
+	   class="video-js"
+	   autoplay
+	   controls></video>
+
+
+
+<script>
+	old = define;
+	define = null;
+</script>
+<script src="//players.brightcove.net/5519514651001/Bys4vXDLb_default/index.min.js" id="myScript"></script>
+
+<script>define = old;
+
+
+	myPlayerID.setAttribute('data-account', 5519514651001);
+	myPlayerID.setAttribute('data-player', 'Bys4vXDLb');
+	myPlayerID.setAttribute('data-video-id', 5522176571001);
+	window.bc(document.getElementById("myPlayerID"));
+	myPlayer = videojs("myPlayerID");
+
+	myPlayer.on('loadedmetadata',function(){
+		myPlayer.play();
+	});
+
+	videojs('vjs_video_3_html5_api').ready(function() {
+		var myPlayer = this;
+
+		myPlayer.on('play', function () {
+			console.log('play')
+		});
+
+		myPlayer.on('pause', function () {
+			console.log('pause')
+		});
+	})
+
+</script>
+<style>
+	.video-js {
+		width: 100%;
+		height: 500px;
+	}
+</style>
