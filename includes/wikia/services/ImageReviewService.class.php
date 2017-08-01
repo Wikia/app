51c1c432b6f13febca68a199ddc982553646a74b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-08-01 09:40:12 +0200

    SUS-2455: remove TODO comment

--- includes/wikia/services/ImageReviewService.class.php
+++ includes/wikia/services/ImageReviewService.class.php
@@ -14,7 +14,7 @@ class ImageReviewService {
 		$api = $this->getImageApiClient();
 		$api->getApiClient()
 			->getConfig()
-			->setApiKey('X-Wikia-UserId', $user->getId() ); //TODO: check if this is valid or should we use Access token
+			->setApiKey('X-Wikia-UserId', $user->getId() );
 
 		try {
 			return $api->getImageHistory( "mw-${cityId}-${pageId}-${revisionId}" );
