a15a85441b50ca55092f072973bf3fb1f12cf493
Kacper Olek <kolek@fandom.com>
2017-08-02 14:34:30 +0000

    added rte-toolbar

--- extensions/wikia/RTE2/js/RTE.js
+++ extensions/wikia/RTE2/js/RTE.js
@@ -84,8 +84,8 @@
 				'rte-signature,' +
 				'rte-spellchecker,' +
 				'rte-template,' +
-				'rte-temporary-save',
-				//'rte-toolbar,' +
+				'rte-temporary-save,' +
+				'rte-toolbar',
 				//'rte-tools',
 			// TODO: Too buggy. Try to use this after we update to 3.6.2 (BugId:23061)
 			//readOnly: true,
