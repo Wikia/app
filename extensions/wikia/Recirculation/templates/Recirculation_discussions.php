a70e94aa4f8b16b2f396b3780ed9512378576a32
JamesHawking <jakubj91@gmail.com>
2017-08-07 12:27:09 +0200

    XW-3626| Remove unnecessary param

--- extensions/wikia/Recirculation/templates/Recirculation_discussions.php
+++ extensions/wikia/Recirculation/templates/Recirculation_discussions.php
@@ -1,8 +1,8 @@
 <section class="mcf-card mcf-card-discussions">
 	<header class="mcf-card-discussions__header">
-		<span><?= wfMessage('recirculation-discussions-latest-discussions')->escaped() ?></span>
+		<span><?= wfMessage( 'recirculation-discussions-latest-discussions' )->escaped() ?></span>
 		<a href="<?= $discussionsUrl ?>" class="mcf-card-discussions__link">
-			<?= wfMessage('recirculation-discussion-link-text')->escaped() ?>
+			<?= wfMessage( 'recirculation-discussion-link-text' )->escaped() ?>
 			<?= DesignSystemHelper::renderSvg(
 				'wds-icons-arrow',
 				'wds-icon wds-icon-small mcf-card-discussions__link-icon'
@@ -11,7 +11,7 @@
 	<ul class="mcf-card-discussions__list">
 		<?php foreach ( $posts as $post ): ?>
 			<li class="mcf-card-discussions__item">
-				<a href="<?= AvatarService::getUrl( $post->author, $ns = NS_USER ) ?>" class="mcf-card-discussions__user-info">
+				<a href="<?= AvatarService::getUrl( $post->author ) ?>" class="mcf-card-discussions__user-info">
 					<img class="wds-avatar" src="<?= $post->meta['authorAvatarUrl'] ?>">
 					<span class="mcf-card-discussions__user-subtitle"><?= $post->author ?>
 						• <time class="discussion-timestamp" datetime="<?= $post->pub_date ?>"></time></span>
@@ -20,7 +20,7 @@
 					<div class="mcf-card-discussions__content"><?= $post->title ?></div>
 					<div class="mcf-card-discussions__meta">
 						<div class="mcf-card-discussions__in">
-							<?= wfMessage('recirculation-discussions-in')->escaped() ?>
+							<?= wfMessage( 'recirculation-discussions-in' )->escaped() ?>
 							<?= $post->meta['forumName'] ?></div>
 						<div class="mcf-card-discussions__counters">
 							<?= DesignSystemHelper::renderSvg( 'wds-icons-upvote', 'wds-icon wds-icon-tiny' ) ?>
