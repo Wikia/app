4043d7367ddca5a98cb0b7a9b137c829484443ed
Damian Jóźwiak <kvas-damian@users.noreply.github.com>
2017-08-09 09:49:27 +0200

    ADEN-5643 | Simplify code in adContext

--- extensions/wikia/AdEngine/js/context/adContext.js
+++ extensions/wikia/AdEngine/js/context/adContext.js
@@ -248,11 +248,8 @@ define('ext.wikia.adEngine.adContext', [
 
 		enableAdMixExperiment(context);
 
-		if (geo.isProperGeo(instantGlobals.wgAdDriverRubiconFastlaneCountries) ||
-			geo.isProperGeo(instantGlobals.wgAdDriverRubiconFastlanePrebidCountries)) {
-			context.opts.premiumAdLayoutRubiconFastlaneTagsEnabled = context.opts.premiumAdLayoutEnabled &&
-				geo.isProperGeo(instantGlobals.wgAdDriverPremiumAdLayoutRubiconFastlaneTagsCountries);
-		}
+		context.opts.premiumAdLayoutRubiconFastlaneTagsEnabled = context.opts.premiumAdLayoutEnabled &&
+			geo.isProperGeo(instantGlobals.wgAdDriverPremiumAdLayoutRubiconFastlaneTagsCountries);
 
 		// Export the context back to ads.context
 		// Only used by Lightbox.js, WikiaBar.js and AdsInContext.js
