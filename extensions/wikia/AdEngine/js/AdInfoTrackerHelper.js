ed350ec3866a0097a875efadf43af71688393085
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-08-10 15:38:13 +0200

    ADE-5569 | Add creative_id, creative_size, and viewport_heigh to kikimora tracking

--- extensions/wikia/AdEngine/js/AdInfoTrackerHelper.js
+++ extensions/wikia/AdEngine/js/AdInfoTrackerHelper.js
@@ -69,6 +69,8 @@ define('ext.wikia.adEngine.adInfoTrackerHelper',  [
 			'product_chosen': '',
 			'product_lineitem_id': slotFirstChildData.gptLineItemId || '',
 			'creative_id': slotFirstChildData.gptCreativeId || '',
+			'creative_size': slotFirstChildData.gptCreativeSize || '',
+			'viewport_height': win.innerHeight || 0,
 			'product_label': '',
 			'ad_status': status || 'unknown'
 		};
