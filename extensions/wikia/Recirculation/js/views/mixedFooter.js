8c9fe0b40ab6154ae50e69a42c85828cda48cb77
JamesHawking <jakubj91@gmail.com>
2017-08-16 16:03:36 +0200

    XW-3825 | Seprate function for injecting template

--- extensions/wikia/Recirculation/js/views/mixedFooter.js
+++ extensions/wikia/Recirculation/js/views/mixedFooter.js
@@ -5,17 +5,17 @@ define('ext.wikia.recirculation.views.mixedFooter', [
 	'ext.wikia.recirculation.utils'
 ], function ($, w, tracker, utils) {
 	'use strict';
-
+	var $nsHook = $('.ns-article'),
+		$wikiArticleHook = $('.wiki-article'),
+		newsAndStoriesList,
+		wikiArticlesList,
+		templateList,
+		templates = {};
 
 	function render(data) {
-		console.log(data);
-		var $nsHook = $('.ns-article'),
-			$wikiArticleHook = $('.wiki-article'),
-			newsAndStoriesList = data.nsItems.items,
-			wikiArticlesList = data.wikiItems.items,
-			templateList = getTemplateList(newsAndStoriesList),
-			templates = {};
-
+		newsAndStoriesList = data.nsItems.items;
+		wikiArticlesList = data.wikiItems.items;
+		templateList = getTemplateList(newsAndStoriesList);
 
 		$('.mcf-discussions-placeholder').replaceWith(data.discussions);
 
@@ -24,57 +24,27 @@ define('ext.wikia.recirculation.views.mixedFooter', [
 			templateList.forEach(function (templateName, index) {
 				templates[templateName] = data[index];
 			})})
-			.then(function () {
-				$.each($nsHook, function( index, value ) {
-					var template = templates['client/Recirculation_article.mustache'];
+			.then(injectTemplates);
+	}
+
+	function injectTemplates() {
+		$.each($nsHook, function( index ) {
+			var template = templates['client/Recirculation_article.mustache'];
 
 
-					if (newsAndStoriesList[index].type === 'topic') {
-						template = templates['client/Recirculation_topic.mustache'];
-					} else if (newsAndStoriesList[index].type === 'storyStream') {
-						template = templates['client/Recirculation_storyStream.mustache'];
-					}
+			if (newsAndStoriesList[index].type === 'topic') {
+				template = templates['client/Recirculation_topic.mustache'];
+			} else if (newsAndStoriesList[index].type === 'storyStream') {
+				template = templates['client/Recirculation_storyStream.mustache'];
+			}
 
-					$(this).replaceWith(utils.renderTemplate(template, newsAndStoriesList[index]));
-				});
-				$.each($wikiArticleHook, function( index, value ) {
-					debugger;
-					var template = templates['client/Recirculation_article.mustache'];
+			$(this).replaceWith(utils.renderTemplate(template, newsAndStoriesList[index]));
+		});
+		$.each($wikiArticleHook, function( index ) {
+			var template = templates['client/Recirculation_article.mustache'];
 
-					$(this).replaceWith(utils.renderTemplate(template, wikiArticlesList[index]));
-				});
-			});
-
-
-
-
-		// $.each($nsHook, function( index, value ) {
-		// 	utils.renderTemplateByName('client/Recirculation_article.mustache', value).then(function (template) {
-		// 		value.append(template);
-		// 	});
-		//
-		// });
-
-
-		// $.each(data.ns, function( index, value ) {
-		// 	utils.renderTemplate('client/Recirculation_article.mustache', value).then(function (template) {
-		// 		console.log($('.ns-article')[index]);
-		// 		$nsHook.eq(index).append(template);
-		// 	});
-		// });
-
-		// $.each(data.wikiarticles, function( index, value ) {
-		// 	utils.renderTemplate('client/Recirculation_article.mustache', value).then(function (template) {
-		// 		$wikiArticleHook.eq(index).append(template);
-		// 	});
-		// });
-
-
-		data = {
-			discussion: '',
-			ns: [],
-			wikiarticles: []
-		};
+			$(this).replaceWith(utils.renderTemplate(template, wikiArticlesList[index]));
+		});
 	}
 	
 	function getTemplateList(newsAndStoriesArticles) {
