08cedcc3d6acecfea1317b210cf253ff926c3e41
TK-999 <tk.999.wikia@gmail.com>
2017-08-18 10:02:57 +0200

    MAIN-11066: switch to JS based solution for FB widgets

---
+++ extensions/wikia/FacebookTags/modules/ext.facebookTags.js
@@ -0,0 +1,35 @@
+/* global require, FB */
+
+/**
+ * Render XFBML and Facebook Page Plugin tags emebedded into wikitext
+ */
+require(['jquery', 'mw', 'wikia.loader'], function ($, mw, loader) {
+	'use strict';
+
+	mw.hook('wikipage.content').add(function ($content) {
+		if (facebookTagsOnPage()) {
+			loader({
+				type: loader.LIBRARY,
+				resources: ['facebook']
+			}).done(function () {
+				var appId = mw.config.get('fbAppId');
+
+				window.FB.init({
+					appId: appId,
+					cookie: true,
+					version: 'v2.9'
+				});
+
+				FB.XFBML.parse($content[0]);
+			});
+		}
+
+		// Checks for XFBML tags or Facebook Social Plugin tags
+		function facebookTagsOnPage() {
+			// All fb social plugins have a class which starts with 'fb-'
+			// eg, fb-like, fb-share-button, etc
+			var numOfFacebookTags = $content.find('[data-type="xfbml-tag"], [class^="fb-"]').length;
+			return numOfFacebookTags > 0;
+		}
+	});
+});
