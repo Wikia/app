816b807d3c6a50940661ef05468e485bb3516cf3
JamesHawking <jakubj91@gmail.com>
2017-08-21 16:57:43 +0200

    XW-3832 | Merge from XW-3616

--- extensions/wikia/Recirculation/templates/RecirculationController_Footer.php
+++ extensions/wikia/Recirculation/templates/RecirculationController_Footer.php
@@ -47,11 +47,7 @@
 		</div>
 		<div class="mcf-row">
 			<div class="mcf-card-wiki-placeholder"></div>
-			<?= F::app()->renderPartial(
-				'Recirculation',
-				'exploreWikis',
-				[ 'recirculationExploreWikis' => wfMessage( 'recirculation-explore-wikis' )->escaped() ]
-			); ?>
+			<?= F::app()->renderPartial('Recirculation', 'exploreWikis', [ 'recirculationExploreWikis' => wfMessage( 'recirculation-explore-wikis' )->escaped(), 'items' => $wikiRecommendations ]); ?>
 			<div class="mcf-card-ns-placeholder"></div>
 		</div>
 	</div>
