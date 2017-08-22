58c870f767e421bd208617ecb4b365561162c23c
Szymon Frąszczak <fraszczak.szymon+github@gmail.com>
2017-08-22 10:40:33 +0200

    ADEN-5755 Change variable name

--- extensions/wikia/AdEngine/AdEngine2Hooks.class.php
+++ extensions/wikia/AdEngine/AdEngine2Hooks.class.php
@@ -43,7 +43,7 @@ class AdEngine2Hooks {
 		$vars[] = 'wgAdDriverDelayCountries';
 		$vars[] = 'wgAdDriverDelayTimeout';
 		$vars[] = 'wgAdDriverEvolve2Countries';
-		$vars[] = 'wgAdDriverFeaturedVideoPrerollFrequencyCapping';
+		$vars[] = 'wgAdDriverFeaturedVideoPrerollNextVideoFrequencyCapping';
 		$vars[] = 'wgAdDriverHighImpactSlotCountries';
 		$vars[] = 'wgAdDriverHighImpact2SlotCountries';
 		$vars[] = 'wgAdDriverHiViLeaderboardRabbitCountries';
