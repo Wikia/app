8d63e55680577cfb8d784514a6a77a1a4e8c9cf4
Karol Tatała <ktatala@wikia-inc.com>
2017-08-22 11:43:13 +0200

    XW-3874 | update html5-skin

--- extensions/wikia/ArticleVideo/bower_components/html5-skin/build/html5-skin.js
+++ extensions/wikia/ArticleVideo/bower_components/html5-skin/build/html5-skin.js
@@ -2857,14 +2857,14 @@ var DiscoveryPanel = React.createClass({displayName: "DiscoveryPanel",
   showCountDown: function () {
     if(!document.hidden && !this.state.autoplayCanceled) {
       this.setState({
-        showDiscoveryCountDown: true
+        showDiscoveryCountDown: this.state.willShowDiscoveryCountDown
       });
     }
   },
 
   autoplayCanceled: function () {
     this.setState({
-      autoplayCanceled: this.state.willShowDiscoveryCountDown
+      autoplayCanceled: true
     });
   },
 
@@ -5730,7 +5730,7 @@ OO.plugin("Html5Skin", function (OO, _, $, W) {
 
   if (OO.publicApi && OO.publicApi.VERSION) {
     // This variable gets filled in by the build script
-    OO.publicApi.VERSION.skin = {"releaseVersion": "4.14.8", "rev": "f5a55245260c971bb4975d3f8aae63fa8df51eb1"};
+    OO.publicApi.VERSION.skin = {"releaseVersion": "4.14.8", "rev": "8444db95cfa1d1e203b2efaf43b0839e9ca15fdb"};
   }
 
   // WIKIA CHANGE - START
