f795269fa81da6e8d6a0849f27eee00084f7946e
mech <wozniak.jacek@gmail.com>
2017-08-25 11:42:29 +0200

    AMP-4: emit amp article address in page header

--- extensions/wikia/GoogleAmp/GoogleAmpHelper.class.php
+++ extensions/wikia/GoogleAmp/GoogleAmpHelper.class.php
@@ -1,6 +1,30 @@
 <?php
 
 class GoogleAmpHelper {
+
+    static public function isAmpEnabled ( Title $title): bool {
+        global $wgEnableGoogleAmp, $wgGoogleAmpArticles;
+        return true;
+    }
+
+    static public function getAmpAddress( Title $title ) {
+        global $wgServer, $wgGoogleAmpAddress;
+
+        if ( !self::isAmpEnabled( $title) ) {
+            return null;
+        }
+
+        $wikiServer = WikiFactory::getLocalEnvURL($wgServer, WIKIA_ENV_PROD);
+        $serverRegex = '/^https?:\/\/(.+)\.wikia\.com/';
+        if ( preg_match( $serverRegex, $wikiServer, $groups ) !== 1 ) {
+            return null;
+        }
+        $wikiServer = $groups[1];
+        $article = $title->getPrefixedDBkey();
+
+        return str_replace( '{WIKI}', $wikiServer, str_replace( '{ARTICLE}', $article, $wgGoogleAmpAddress));;
+    }
+
     /**
      * Attempts to recover a URL that was truncated by an external service (e.g. /wiki/Wanted! --> /wiki/Wanted)
      * @param Article $article
@@ -9,20 +33,16 @@ class GoogleAmpHelper {
      * @return bool
      */
     static public function onBeforePageDisplay( OutputPage $out, Skin $skin ): bool {
-
         $title = $out->getTitle();
-        $isMainpage = $title->isMainPage();
-        if ( $isMainpage ) {
-            //$meta["og:type"] = "website";
-            //$meta["og:title"] = $wgSitename;
-        } else {
-            $out->addLink( [
-                'rel' => 'alternate',
-                'href' => 'http://example.com',
-                'hreflang' => 'en-us'
-            ] );
+        if ( $title ) {
+            $ampArticle = self::getAmpAddress( $title );
+            if ( $ampArticle ) {
+                $out->addLink( [
+                    'rel' => 'amphtml',
+                    'href' => $ampArticle
+                ] );
+            }
         }
-
         return true;
     }
 }
