34de0930486ea374670d26e9944a1c42b8a46c50
Maciej Brencz <maciej.brencz@gmail.com>
2017-08-28 11:29:33 +0200

    Merge pull request #13625 from Wikia/SUS-2685-revert-SUS-1393
    Revert "Merge pull request #13580 from Wikia/SUS-1393"

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -724,9 +724,9 @@ $wgAllowTitlesInSVG = false;
  * JPEGs with ImageMagick, and when using the VipsScaler extension.
  *
  * The default is 50 MB if decompressed to RGBA form, which corresponds to
- * 12.5 million pixels or 3500x3500. - changed to 60MP: SUS-1393
+ * 12.5 million pixels or 3500x3500.
  */
-$wgMaxImageArea = 6e7;
+$wgMaxImageArea = 1.25e7;
 /**
  * Force thumbnailing of animated GIFs above this size to a single
  * frame instead of an animated thumbnail.  As of MW 1.17 this limit
