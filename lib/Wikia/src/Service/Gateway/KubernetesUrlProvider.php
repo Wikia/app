1b50bc8b9b35bc6c82a0dd510c7eb8af7ebf387c
Máté Szabó <tk.999.wikia@gmail.com>
2017-08-30 14:16:55 +0200

    SUS-2687: Introduce classes to handle k8s services lookup (#13629)
    * SUS-2687: Introduce classes to handle k8s services lookup
    
    * SUS-2687: Remove unused GatewayBase class
    
    * SUS-2687: try to please Sonar a bit
    
    * SUS-2687: update URL of KubernetesUrlProvider

---
+++ lib/Wikia/src/Service/Gateway/KubernetesUrlProvider.php
@@ -0,0 +1,49 @@
+<?php
+namespace Wikia\Service\Gateway;
+
+use InvalidArgumentException;
+
+class KubernetesUrlProvider implements UrlProvider {
+	const URL_PROVIDER_WIKIA_ENVIRONMENT = 'url_provider_wikia_environment';
+	const URL_PROVIDER_DATACENTER = 'url_provider_datacenter';
+
+	const K8S_URL = "%s.%s.k8s.wikia.net/%s";
+
+	private $env;
+
+	private $dc;
+
+	/**
+	 * @Inject({
+	 *     Wikia\Service\Gateway\KubernetesUrlProvider::URL_PROVIDER_WIKIA_ENVIRONMENT,
+	 *     Wikia\Service\Gateway\KubernetesUrlProvider::URL_PROVIDER_DATACENTER})
+	 * @param string $wikiaEnvironment
+	 * @param string $dataCenter
+	 */
+	public function __construct( string $wikiaEnvironment, string $dataCenter ) {
+		switch ( $wikiaEnvironment ) {
+			case WIKIA_ENV_PROD:
+			case WIKIA_ENV_PREVIEW:
+			case WIKIA_ENV_VERIFY:
+			case WIKIA_ENV_STABLE:
+			case WIKIA_ENV_SANDBOX:
+				$this->env = WIKIA_ENV_PROD;
+				$this->dc = $dataCenter;
+				break;
+			case WIKIA_ENV_STAGING:
+				$this->env = WIKIA_ENV_STAGING;
+				$this->dc = $dataCenter;
+				break;
+			case WIKIA_ENV_DEV:
+				$this->env = WIKIA_ENV_DEV;
+				$this->dc = "$dataCenter-dev";
+				break;
+			default:
+				throw new InvalidArgumentException( "Invalid environment $wikiaEnvironment" );
+		}
+	}
+
+	public function getUrl( $serviceName ) {
+		return sprintf( static::K8S_URL, $this->env, $this->dc, $serviceName );
+	}
+}
