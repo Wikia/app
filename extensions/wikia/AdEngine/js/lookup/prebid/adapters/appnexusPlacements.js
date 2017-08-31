8b4ac4948d4788b3a872633dd27fe71cb1f282a6
Maciej Leśniewski <mlesniewski@wikia-inc.com>
2017-08-31 17:45:05 +0200

    ADEN-5717 Adds PAL AN placements (review fixes)

--- extensions/wikia/AdEngine/js/lookup/prebid/adapters/appnexusPlacements.js
+++ extensions/wikia/AdEngine/js/lookup/prebid/adapters/appnexusPlacements.js
@@ -7,64 +7,61 @@ define('ext.wikia.adEngine.lookup.prebid.adapters.appnexusPlacements', [
 
 	var logGroup = 'ext.wikia.adEngine.lookup.prebid.adapters.appnexusPlacements',
 		placementsMap = {
-			'mercury': {
-				'ent': '9412992',
-				'gaming': '9412993',
-				'life': '9412994',
-				'other': '9412994'
+			mercury: {
+				ent: '9412992',
+				gaming: '9412993',
+				life: '9412994',
+				other: '9412994'
 			},
-			'oasis': {
-				'ent': {
-					'atf': '9412983',
-					'btf': '9412986',
-					'hivi': '9412989'
-				},
-				'gaming': {
-					'atf': '9412984',
-					'btf': '9412987',
-					'hivi': '9412990'
-				},
-				'life': {
-					'atf': '9412985',
-					'btf': '9412988',
-					'hivi': '9412991'
-				},
-				'other': {
-					'atf': '9412985',
-					'btf': '9412988',
-					'hivi': '9412991'
-				},
-				'recovery': {
-					'atf': '11823778',
-					'btf': '11823724',
-					'hivi': '11823799'
-				}
-			}
-		},
-		palPlacementsMap = {
 			oasis: {
-				atf: '3143028',
-				btf: '3143029',
-				hivi: '3142398',
-				other: '3140173'
-			},
-			recovery: {
-				'atf': '11823778',
-				'btf': '11823724',
-				'hivi': '11823799'
+				ent: {
+					atf: '9412983',
+					btf: '9412986',
+					hivi: '9412989'
+				},
+				gaming: {
+					atf: '9412984',
+					btf: '9412987',
+					hivi: '9412990'
+				},
+				life: {
+					atf: '9412985',
+					btf: '9412988',
+					hivi: '9412991'
+				},
+				other: {
+					atf: '9412985',
+					btf: '9412988',
+					hivi: '9412991'
+				},
+				recovery: {
+					atf: '11823778',
+					btf: '11823724',
+					hivi: '11823799'
+				},
+				pal: {
+					atf: '11977073',
+					btf: '11977096',
+					hivi: '11977016',
+					other: '11969927'
+				}
 			}
 		};
 
 	function getPlacement(skin, position, isRecovering) {
 		var context = adContext.getContext(),
-			vertical = isRecovering ? 'recovery' : context.targeting.mappedVerticalName,
+			vertical = context.targeting.mappedVerticalName,
 			skinVertical;
 
 		if (context.premiumAdLayoutEnabled) {
-			skin = isRecovering ? 'recovery' : skin;
+			vertical = 'pal';
+		}
 
-			return palPlacementsMap[skin] && palPlacementsMap[skin][position];
-		} else if (placementsMap && placementsMap[skin] && placementsMap[skin][vertical]) {
+		if (isRecovering) {
+			vertical = 'recovery';
+		}
+
+		if (placementsMap && placementsMap[skin] && placementsMap[skin][vertical]) {
 			skinVertical = placementsMap[skin][vertical];
 
 			return position && skinVertical[position] ?
