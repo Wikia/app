84b3d48d1c88b41440abd2daf666f6cad59d4acf
TK-999 <tk.999.wikia@gmail.com>
2017-09-04 14:40:56 +0000

    SUS-1777: use Title::makeTitle in WallMessage::getArticleTitle

--- extensions/wikia/Wall/WallMessage.class.php
+++ extensions/wikia/Wall/WallMessage.class.php
@@ -590,8 +590,7 @@ class WallMessage {
 		if ( $title->inNamespace( NS_USER_WALL_MESSAGE ) ) {
 			$parentPageText = $title->getBaseText();
 
-			return Title::newFromText( $parentPageText, NS_USER_WALL ) ??
-				   static::emptyTitle();
+			return Title::makeTitle( $parentPageText, NS_USER_WALL );
 		}
 
 		// Forum Threads may have been moved to another board - use comments index as data source
