2758618d9426f7cb309487a6a5c575bea91ccfda
Máté Szabó <tk.999.wikia@gmail.com>
2017-09-06 15:20:01 +0200

    SUS-2545: Improve image lazy-loading (#13669)
    * SUS-2545: Improve image lazy-loading
    
    * SUS-2545: and coffee please :coffee:
    
    * SUS-2545: avoid JS error in tabber
    
    * SUS-2545: handle nested tabbers correctly
    
    * SUS-2545: another take @ nested tabbers
    
    * SUS-2545: dokumentésön++

--- extensions/wikia/ImageLazyLoad/js/ImageLazyLoad.js
+++ extensions/wikia/ImageLazyLoad/js/ImageLazyLoad.js
@@ -1,7 +1,7 @@
 /* Lazy loading for images inside articles (skips wikiamobile)
  * @author Piotr Bablok <pbablok@wikia-inc.com>
  */
-require(['jquery', 'wikia.ImgLzy', 'wikia.browserDetect', 'wikia.window'], function ($, ImgLzy, browserDetect, w) {
+require(['jquery', 'wikia.ImgLzy', 'wikia.browserDetect'], function ($, ImgLzy, browserDetect) {
 	'use strict';
 
 	// expose as a global
@@ -12,11 +12,11 @@ require(['jquery', 'wikia.ImgLzy', 'wikia.browserDetect', 'wikia.window'], funct
 
 		// fix iOS bug - not firing scroll event when after refresh page is opened in the middle of its content
 		if (browserDetect.isIPad()) {
-			w.addEventListener('pageshow', function () {
+			window.addEventListener('pageshow', function () {
 				// Safari iOS doesn't trigger scroll event after page refresh.
 				// This is a hack to manually lazy-load images after browser scroll the page after refreshing.
 				// Should be fixed if we found better solution
-				w.setTimeout($.proxy(ImgLzy.checkAndLoad, ImgLzy), 0);
+				window.setTimeout(ImgLzy.onScroll.bind(ImgLzy), 0);
 			});
 		}
 	});
