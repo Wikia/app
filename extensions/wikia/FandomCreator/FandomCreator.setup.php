3d0c8ed3d39871c8efc4d4d7f1a49fb672e89d0e
nmonterroso <nmonterroso@users.noreply.github.com>
2017-09-19 14:11:14 -0700

    running through MW style

--- extensions/wikia/FandomCreator/FandomCreator.setup.php
+++ extensions/wikia/FandomCreator/FandomCreator.setup.php
@@ -1,19 +1,19 @@
 <?php
-spl_autoload_register(function($class) {
+spl_autoload_register( function( $class ) {
 	$prefix = 'FandomCreator\\';
-	$len = strlen($prefix);
-	if (strncmp($prefix, $class, $len) !== 0) {
+	$len = strlen( $prefix );
+	if ( strncmp( $prefix, $class, $len ) !== 0 ) {
 		return;
 	}
 
-	$file = __DIR__.'/'.str_replace('\\', '/', substr($class, $len)).'.php';
-	if (file_exists($file)) {
+	$file = __DIR__ . '/' . str_replace( '\\', '/', substr( $class, $len ) ) . '.php';
+	if ( file_exists( $file ) ) {
 		require $file;
 	}
-});
+} );
 
-$wgHooks['NavigationApigetDataAfterExecute'][] = function(WikiaDispatchableObject $dispatchable) {
+$wgHooks['NavigationApigetDataAfterExecute'][] = function( WikiaDispatchableObject $dispatchable ) {
 	global $wgMaxLevelOneNavElements, $wgMaxLevelTwoNavElements, $wgMaxLevelThreeNavElements;
-	\FandomCreator\Hooks::onNavigationApiGetData($dispatchable, [$wgMaxLevelOneNavElements, $wgMaxLevelTwoNavElements, $wgMaxLevelThreeNavElements]);
+	\FandomCreator\Hooks::onNavigationApiGetData( $dispatchable, [$wgMaxLevelOneNavElements, $wgMaxLevelTwoNavElements, $wgMaxLevelThreeNavElements] );
 	return true;
 };
--- extensions/wikia/FandomCreator/FandomCreatorApi.php
+++ extensions/wikia/FandomCreator/FandomCreatorApi.php
@@ -18,30 +18,30 @@ class FandomCreatorApi {
 	 * @param string $baseUrl
 	 * @param string $communityId
 	 */
-	public function __construct(string $baseUrl, string $communityId) {
+	public function __construct( string $baseUrl, string $communityId ) {
 		$this->baseUrl = $baseUrl;
 		$this->communityId = $communityId;
 	}
 
 	public function getCommunity() {
-		$response = $this->doApiRequest("{$this->baseUrl}/communities/{$this->communityId}");
+		$response = $this->doApiRequest( "{$this->baseUrl}/communities/{$this->communityId}" );
 	}
 
 	public function getSitemap() {
-		$response = $this->doApiRequest("{$this->baseUrl}/communities/{$this->communityId}/sitemap");
-		if (!$response->status->isOK()) {
+		$response = $this->doApiRequest( "{$this->baseUrl}/communities/{$this->communityId}/sitemap" );
+		if ( !$response->status->isOK() ) {
 			return null;
 		}
 
-		return json_decode($response->getContent());
+		return json_decode( $response->getContent() );
 	}
 
-	private function doApiRequest(string $url):MWHttpRequest {
-		return Http::request('GET', $url, [
+	private function doApiRequest( string $url ): MWHttpRequest {
+		return Http::request( 'GET', $url, [
 				'userAgent' => $_SERVER['HTTP_USER_AGENT'],
 				'noProxy' => true,
 				'returnInstance' => true,
-				'followRedirects'=> true,
-		]);
+				'followRedirects' => true,
+		] );
 	}
 }
