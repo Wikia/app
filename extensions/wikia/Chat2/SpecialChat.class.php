8a86a4bcba551d8e80b018fc1517554f6a48e2b9
Máté Szabó <tk.999.wikia@gmail.com>
2017-09-20 16:42:35 +0200

    SUS-2755: Remove chat debug logging (#13785)

--- extensions/wikia/Chat2/SpecialChat.class.php
+++ extensions/wikia/Chat2/SpecialChat.class.php
@@ -15,16 +15,13 @@ class SpecialChat extends UnlistedSpecialPage {
 		if ( $user->isLoggedIn() ) {
 			// Check that the user is not blocked or banned from chat
 			if ( Chat::canChat( $user ) ) {
-				Chat::info( __METHOD__ . ': Method called - success' );
 				Wikia::setVar( 'OasisEntryControllerName', 'Chat' );
 				$out->addModules( 'ext.Chat2' );
 				Chat::addConnectionLogEntry();
 			} else {
-				Chat::info( __METHOD__ . ': Method called - banned' );
 				$out->showErrorPage( 'chat-you-are-banned', 'chat-you-are-banned-text' );
 			}
 		} else {
-			Chat::info( __METHOD__ . ': Method called - logged out' );
 			$out->showErrorPage( 'chat-no-login', 'chat-no-login-text' );
 		}
 	}
