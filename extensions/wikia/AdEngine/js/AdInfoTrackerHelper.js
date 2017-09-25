bd0f451778f66f744eebd82021e0919b380cc1a8
nAndy <nandy@wikia-inc.com>
2017-09-25 14:38:58 +0000

    ADEN-5836 cleaned kikimora after rubicon bidder old integration

--- extensions/wikia/AdEngine/js/AdInfoTrackerHelper.js
+++ extensions/wikia/AdEngine/js/AdInfoTrackerHelper.js
@@ -61,9 +61,9 @@ define('ext.wikia.adEngine.adInfoTrackerHelper',  [
 			'bidder_won_price': bidderWon ? realSlotPrices[bidderWon] : '',
 			'bidder_1': transformBidderPrice('indexExchange', realSlotPrices, slotPricesIgnoringTimeout),
 			'bidder_2': transformBidderPrice('appnexus', realSlotPrices, slotPricesIgnoringTimeout),
-			'bidder_3': transformBidderPrice('fastlane', realSlotPrices, slotPricesIgnoringTimeout),
+			'bidder_3': 'DEPRECATED',
 			'bidder_4': transformBidderPrice('rubicon', realSlotPrices, slotPricesIgnoringTimeout),
-			'bidder_5': transformBidderPrice('fastlane_private', realSlotPrices, slotPricesIgnoringTimeout),
+			'bidder_5': 'DEPRECATED',
 			'bidder_6': transformBidderPrice('aol', realSlotPrices, slotPricesIgnoringTimeout),
 			'bidder_7': transformBidderPrice('audienceNetwork', realSlotPrices, slotPricesIgnoringTimeout),
 			'bidder_8': transformBidderPrice('veles', realSlotPrices, slotPricesIgnoringTimeout),
@@ -117,16 +117,6 @@ define('ext.wikia.adEngine.adInfoTrackerHelper',  [
 		// In case of a tie with prebid and rubiconFastlane we promote prebid
 		if (slotParams.hb_bidder && highestPriceBidders.indexOf(slotParams.hb_bidder) >= 0) {
 			return slotParams.hb_bidder;
-		}
-
-		if (slotParams.rpfl_7450) {
-			if (highestPriceBidders.indexOf('fastlane') >= 0) {
-				return 'fastlane';
-			}
-
-			if (highestPriceBidders.indexOf('fastlane_private') >= 0) {
-				return 'fastlane_private';
-			}
 		}
 
 		return '';
