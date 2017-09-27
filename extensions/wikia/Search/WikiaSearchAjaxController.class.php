94fa2095caaeebe5af65a7583918011aa0238504
macbre <maciej.brencz@gmail.com>
2017-09-27 13:27:25 +0200

    Search: check WikiaPageType::isCorporatePage() instead of wgEnableWikiaHomePageExt

--- extensions/wikia/Search/WikiaSearchAjaxController.class.php
+++ extensions/wikia/Search/WikiaSearchAjaxController.class.php
@@ -27,10 +27,7 @@ class WikiaSearchAjaxController extends WikiaController {
 		$crossWikia = $this->request->getBool( 'crossWikia' );
 		$hub = false;
 
-		// TODO: refactor to avoid using wgEnableWikiaHomePageExt
-		$isCorporateWiki = !empty( $this->wg->EnableWikiaHomePageExt );
-
-		$isInterWiki = $crossWikia ? true : $isCorporateWiki;
+		$isInterWiki = $crossWikia ? true : WikiaPageType::isCorporatePage();
 
 		$cityId = $isInterWiki ? 0 : $this->wg->CityId;
 
