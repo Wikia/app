1f559a290413231ac8de1d634d7c7d9501b2c3a8
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-10-04 13:58:33 +0200

    XW-3958 | use svg for autoplay toggle

--- extensions/wikia/ArticleVideo/scripts/jwplayer-instant.js
+++ extensions/wikia/ArticleVideo/scripts/jwplayer-instant.js
@@ -8,6 +8,7 @@
 	function handleTabNotActive(willAutoplay) {
 		var hidden,
 			visibilityChange;
+
 		if (typeof document.hidden !== "undefined") {
 			hidden = "hidden";
 			visibilityChange = "visibilitychange";
@@ -53,18 +54,15 @@
 
 		if (true || inAutoplayCountries) {
 			playerInstance.addButton(
-				//This portion is what designates the graphic used for the button
-				"//icons.jwplayer.com/icons/white/download.svg",
-				//This portion determines the text that appears as a tooltip
+				"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" class='wds-icon'>\n" +
+				"<path fill-rule=\"evenodd\" d=\"M23.905 21.84a.775.775 0 0 1-.018.777.802.802 0 0 1-.686.383H.8a.804.804 0 0 1-.688-.383.775.775 0 0 1-.017-.777l11.2-20.458c.28-.51 1.13-.51 1.41 0l11.2 20.458zM11 8.997v6.006c0 .544.448.997 1 .997.556 0 1-.446 1-.997V8.997C13 8.453 12.552 8 12 8c-.556 0-1 .446-1 .997zM11 19c0 .556.448 1 1 1 .556 0 1-.448 1-1 0-.556-.448-1-1-1-.556 0-1 .448-1 1z\"/>\n" +
+				"</svg>",
 				"Toggle Autoplay",
-				//This portion designates the functionality of the button itself
 				function () {
-					//With the below code, we're grabbing the file that's currently playing
 					cookies.set(autoplayCookieName, cookies.get(autoplayCookieName) === '0' ? '1' : '0');
 
 					console.log('autoplay', cookies.get(autoplayCookieName))
 				},
-				//And finally, here we set the unique ID of the button itself.
 				"autoplayToggle"
 			);
 		}
