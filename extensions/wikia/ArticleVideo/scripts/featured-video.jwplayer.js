d4cfde6173778f8b5eced7fec4c4166d5445694f
Karol Tatała <ktatala@wikia-inc.com>
2017-10-09 16:50:47 +0200

    XW-4073 | remove featured video if VisualEditor is loaded

--- extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.js
+++ extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.js
@@ -1,22 +1,30 @@
 require([
-	'wikia.window',
-	'wikia.tracker',
+	'mw',
 	'wikia.articleVideo.featuredVideo.jwplayer.onScroll',
 	'wikia.articleVideo.featuredVideo.jwplayer.videoFeedback',
 	'wikia.articleVideo.featuredVideo.jwplayer.autoplayToggle',
 	'wikia.articleVideo.featuredVideo.jwplayer.instance'
 ], function (
-	window,
-	tracker,
+	mw,
 	jwPlayerOnScroll,
 	jwPlayerVideoFeedback,
 	jwPlayerAutoplayToggle,
 	playerInstance
 ) {
 	var $featuredVideo = $('.featured-video'),
+		$featuredVideoWrapper = $('.featured-video__wrapper'),
 		$playerContainer = $('.featured-video__player-container');
 
 	jwPlayerOnScroll(playerInstance, $featuredVideo, $playerContainer);
 	jwPlayerVideoFeedback(playerInstance);
 	jwPlayerAutoplayToggle(playerInstance);
+
+	// remove player if VisualEditor is loaded
+	mw.hook('ve.activationComplete').add(function() {
+		if(playerInstance) {
+			$featuredVideoWrapper.addClass('is-removed');
+			playerInstance.remove();
+			playerInstance = null;
+		}
+	});
 });
