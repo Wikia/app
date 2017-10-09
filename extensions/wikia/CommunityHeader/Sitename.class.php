2485bda9681fc0c59e10e98e28075f7751dbfaa3
TK-999 <tk.999.wikia@gmail.com>
2017-10-09 17:19:11 +0200

    SUS-2975: Avoid double escaping sitename in header

--- extensions/wikia/CommunityHeader/Sitename.class.php
+++ extensions/wikia/CommunityHeader/Sitename.class.php
@@ -3,6 +3,7 @@
 namespace Wikia\CommunityHeader;
 
 use DesignSystemCommunityHeaderModel;
+use Sanitizer;
 
 class Sitename {
 	public $titleText;
@@ -11,8 +12,11 @@ class Sitename {
 
 	public function __construct( DesignSystemCommunityHeaderModel $model ) {
 		$sitenameData = $model->getSiteNameData();
+		// SUS-2975: Site name is user input, so it comes pre-escaped.
+		// We must decode HTML entities present in the text to avoid double escaping.
+		$siteName = Sanitizer::decodeCharReferences( $sitenameData['title']['value'] );
 
-		$this->titleText = new Label( $sitenameData['title']['value'] );
+		$this->titleText = new Label( $siteName );
 		$this->url = $sitenameData['href'];
 		$this->trackingLabel = $sitenameData['tracking_label'];
 	}
