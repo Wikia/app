25eb3c540d56f580b74debb2088d57fed9894f93
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-10-13 15:01:58 +0200

    ADEN-6046 Track featured video as slot as well

--- extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.ads-tracking.js
+++ extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.ads-tracking.js
@@ -1,6 +1,7 @@
 define('wikia.articleVideo.featuredVideo.adsTracking', [
+	'ext.wikia.adEngine.utils.eventDispatcher',
 	'ext.wikia.adEngine.video.player.jwplayer.jwplayerTracker'
-], function (tracker) {
+], function (eventDispatcher, tracker) {
 	function updateParams(params, currentAd) {
 		var wrapperCreativeId,
 			wrapperId;
@@ -30,11 +31,24 @@ define('wikia.articleVideo.featuredVideo.adsTracking', [
 			params.creativeId = undefined;
 			params.contentType = undefined;
 		});
+
 		player.on('adError', function () {
 			updateParams(params);
 		});
+
 		player.on('adRequest', function (event) {
 			updateParams(params, event.ima && event.ima.ad);
+		});
+
+		player.on('adImpression', function (event) {
+			if (eventDispatcher) {
+				eventDispatcher.dispatch('adengine.video.status', {
+					vastUrl: event.tag,
+					creativeId: params.creativeId,
+					lineItemId: params.lineItemId,
+					status: 'success'
+				});
+			}
 		});
 
 		tracker.register(player, params);
