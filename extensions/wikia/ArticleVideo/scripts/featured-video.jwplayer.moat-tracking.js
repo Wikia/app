8d5386422ced61aa3b7d52a7656dffa79ee1814b
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-10-17 10:26:46 +0200

    ADEN-6045 Sample MOAT tracking

--- extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.moat-tracking.js
+++ extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.moat-tracking.js
@@ -3,14 +3,16 @@ define('wikia.articleVideo.featuredVideo.moatTracking', [
 	'wikia.window'
 ], function (adContext, win) {
 	return function(player) {
-		player.on('adImpression', function (event) {
-			if (win.moatjw) {
-				win.moatjw.add({
-					adImpressionEvent: event,
-					partnerCode: 'wikiajwint101173217941',
-					player: this
-				});
-			}
-		});
+		if (adContext.get('opts.isMoatTrackingForFeaturedVideoEnabled')) {
+			player.on('adImpression', function (event) {
+				if (win.moatjw) {
+					win.moatjw.add({
+						adImpressionEvent: event,
+						partnerCode: 'wikiajwint101173217941',
+						player: this
+					});
+				}
+			});
+		}
 	};
 });
