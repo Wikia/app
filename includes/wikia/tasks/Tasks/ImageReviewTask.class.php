677b229c49d20c130d6a77b14c64a00d9f490013
Michał “Mix” Roszka <mix@mixoteric.net>
2017-10-18 01:31:17 +0000

    restore ImageReviewTask

--- includes/wikia/tasks/Tasks/ImageReviewTask.class.php
+++ includes/wikia/tasks/Tasks/ImageReviewTask.class.php
@@ -111,6 +111,7 @@ class ImageReviewTask extends BaseTask {
 
 		$success = $articlesDeleted == count( $pageList );
 		if ( !$success ) {
+			$this->sendNotification();
 		}
 
 		return $success;
@@ -130,6 +131,70 @@ class ImageReviewTask extends BaseTask {
 		}
 	}
 
+	public function deleteFromQueue( Array $aDeletionList ) {
+		global $wgExternalDatawareDB;
+
+		$oDB = wfGetDB( DB_MASTER, [], $wgExternalDatawareDB );
+
+		foreach ( $aDeletionList as $aRow ) {
+			$aImageData = [
+				'wiki_id' => $aRow['wiki_id'],
+				'page_id' => $aRow['page_id'],
+			];
+
+			$oDB->delete(
+				'image_review',
+				$aImageData,
+				__METHOD__
+			);
+
+			WikiaLogger::instance()->info( 'ImageReviewLog', [
+				'method' => __METHOD__,
+				'message' => 'Image removed from queue',
+				'params' => $aRow,
+			] );
+		}
+	}
+
+	public function addToQueue() {
+		global $wgExternalDatawareDB;
+		$title = $this->getTitle();
+		$wikiId = $this->getWikiId();
+		$latestRevId = $title->getLatestRevID( \Title::GAID_FOR_UPDATE );
+
+		$imageData = [
+			'wiki_id' => $wikiId,
+			'page_id' => $title->getArticleID(),
+			'revision_id' => $latestRevId,
+			'user_id' => wfLocalFile( $title )->getUser( 'id' ),
+			'last_edited' => \Revision::newFromId( $latestRevId, \Revision::READ_LATEST )->getTimestamp(),
+			'top_200' => $this->isTop200( $wikiId ) ? 1 : 0,
+			'state' => \ImageReviewStatuses::STATE_UNREVIEWED,
+		];
+
+		$dbw = wfGetDB( DB_MASTER, [], $wgExternalDatawareDB );
+		$result = $dbw->upsert(
+			'image_review',
+			$imageData,
+			[],
+			[
+				'last_edited = values(last_edited)',
+				'revision_id = values(revision_id)',
+				'state = values(state)',
+				'user_id = values(user_id)',
+			],
+			__METHOD__
+		);
+
+		if ( $result ) {
+			$this->info( 'Image Review - Added uploaded file', $imageData );
+		} else {
+			$this->error( 'Image Review - Failed to add uploaded file', $imageData );
+		}
+
+		return $result;
+	}
+
 	/**
 	 * Invalidates provided file page (use page ID) by bumping page_touched entry in `page` table
 	 * and purging CDN cache.
@@ -145,5 +210,46 @@ class ImageReviewTask extends BaseTask {
 
 		$title->invalidateCache();
 		$title->purgeSquid();
+	}
+
+	private function isTop200( $wikiId ) {
+		global $wgExternalDatawareDB;
+		$dbr = wfGetDB( DB_SLAVE, [], $wgExternalDatawareDB );
+
+		$result = $dbr->selectRow(
+			'image_review',
+			[ 'top_200' ],
+			[
+				'wiki_id' => $wikiId,
+				'top_200' => 1,
+			],
+			__METHOD__
+		);
+
+		return $result !== false;
+	}
+
+	private function sendNotification() {
+		global $wgFlowerUrl;
+
+		$subject = "ImageReview deletion failed #{$this->taskId}";
+		$body = "{$wgFlowerUrl}/task/{$this->taskId}";
+		$recipients = [
+			new \MailAddress( 'tor@wikia-inc.com' ),
+			new \MailAddress( 'adamk@wikia-inc.com' ),
+			new \MailAddress( 'sannse@wikia-inc.com' ),
+		];
+		$from = $recipients[0];
+
+		foreach ( $recipients as $recipient ) {
+			\UserMailer::send( $recipient, $from, $subject, $body );
+		}
+
+		WikiaLogger::instance()->error( "ImageReviewLog", [
+			'method' => __METHOD__,
+			'message' => "Task #{$this->taskId} deleting images did not succeed. Please check.",
+			'taskId' => $this->taskId,
+			'taskUrl' => $body,
+		] );
 	}
 }
