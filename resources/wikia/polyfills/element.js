384133870ba66a41030eca01b730a8c26215414e
Karol Tatała <ktatala@wikia-inc.com>
2017-10-23 23:08:41 +0200

    replace closest polyfill with one from mdn site

--- resources/wikia/polyfills/element.js
+++ resources/wikia/polyfills/element.js
@@ -1,17 +1,22 @@
 (function () {
 
 	// add Element.closest function in IE
-	// @see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf
-	if (!Element.prototype.matches) Element.prototype.matches = Element.prototype.msMatchesSelector;
-	if (!Element.prototype.closest) Element.prototype.closest = function (selector) {
-		var el = this;
-		while (el) {
-			if (el.matches(selector)) {
-				return el;
-			}
-			el = el.parentElement;
-		}
-	};
+	// @see https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+	if (!Element.prototype.matches)
+		Element.prototype.matches = Element.prototype.msMatchesSelector ||
+			Element.prototype.webkitMatchesSelector;
+
+	if (!Element.prototype.closest) {
+		Element.prototype.closest = function (s) {
+			var el = this;
+			if (!document.documentElement.contains(el)) return null;
+			do {
+				if (el.matches(s)) return el;
+				el = el.parentElement;
+			} while (el !== null);
+			return null;
+		};
+	}
 
 	// add NodeList.foreach function in IE
 	if (window.NodeList && !NodeList.prototype.forEach) {
