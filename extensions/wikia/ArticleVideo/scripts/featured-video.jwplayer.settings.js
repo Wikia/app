c7efbd3c9b90d3050cf13705e16ecdc7529a11d7
Karol Tatała <ktatala@wikia-inc.com>
2017-10-23 09:50:39 +0200

    XW-4104 | add some new lines, merge two classList.remove into one

--- extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.settings.js
+++ extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.settings.js
@@ -5,7 +5,6 @@ define('wikia.articleVideo.featuredVideo.jwplayer.plugin.settings', ['wikia.arti
 		this.wikiaSettingsElement = div;
 		this.buttonID = 'wikiaSettings';
 		this.documentClickHandler = this.documentClickHandler.bind(this);
-		var self = this;
 
 		this.addSettingsContent(this.wikiaSettingsElement);
 		this.addButton();
@@ -23,6 +22,7 @@ define('wikia.articleVideo.featuredVideo.jwplayer.plugin.settings', ['wikia.arti
 		var self = this,
 			// wds-icons-gear-small.svg
 			settingsIcon = '<svg xmlns="http://www.w3.org/2000/svg" class="jw-svg-icon jw-svg-icon-wikia-settings" viewBox="0 0 24 24"><path d="M9 7.09a1.909 1.909 0 1 1 0 3.819A1.909 1.909 0 0 1 9 7.09m-4.702-.03a1.07 1.07 0 0 1-.99.667h-.672A.637.637 0 0 0 2 8.364v1.272c0 .352.285.637.636.637h.672c.436 0 .824.264.99.667l.006.013c.167.403.08.864-.229 1.172L3.6 12.6a.636.636 0 0 0 0 .9l.9.9a.636.636 0 0 0 .9 0l.475-.475a1.072 1.072 0 0 1 1.185-.223c.403.166.667.554.667.99v.672c0 .35.285.636.637.636h1.272a.637.637 0 0 0 .637-.636v-.672c0-.436.264-.824.667-.99l.013-.006a1.07 1.07 0 0 1 1.172.229l.475.475a.636.636 0 0 0 .9 0l.9-.9a.636.636 0 0 0 0-.9l-.475-.475a1.072 1.072 0 0 1-.229-1.172l.006-.013a1.07 1.07 0 0 1 .99-.667h.672A.637.637 0 0 0 16 9.636V8.364a.637.637 0 0 0-.636-.637h-.672a1.07 1.07 0 0 1-.996-.68 1.072 1.072 0 0 1 .229-1.172L14.4 5.4a.636.636 0 0 0 0-.9l-.9-.9a.636.636 0 0 0-.9 0l-.475.475c-.308.308-.77.396-1.172.229l-.013-.006a1.07 1.07 0 0 1-.667-.99v-.672A.637.637 0 0 0 9.636 2H8.364a.637.637 0 0 0-.637.636v.672a1.07 1.07 0 0 1-.68.996 1.07 1.07 0 0 1-1.172-.229L5.4 3.6a.636.636 0 0 0-.9 0l-.9.9a.636.636 0 0 0 0 .9l.475.475a1.072 1.072 0 0 1 .223 1.185" fill-rule="evenodd"/></svg>';
+
 		this.player.addButton(settingsIcon, 'Settings', function () {
 			if (!self.wikiaSettingsElement.style.display) {
 				self.open();
@@ -68,8 +68,7 @@ define('wikia.articleVideo.featuredVideo.jwplayer.plugin.settings', ['wikia.arti
 
 	WikiaJWPlayerSettings.prototype.addSettingsContent = function (div) {
 		div.classList.add('wikia-jw-settings');
-		div.classList.remove('jw-reset');
-		div.classList.remove('jw-plugin');
+		div.classList.remove('jw-reset', 'jw-plugin');
 		this.settingsList = this.createSettingsListElement();
 		this.qualityLevelsList = this.createQualityLevelsList();
 
@@ -81,20 +80,24 @@ define('wikia.articleVideo.featuredVideo.jwplayer.plugin.settings', ['wikia.arti
 
 	WikiaJWPlayerSettings.prototype.createSettingsListElement = function () {
 		var settingsList = document.createElement('ul');
+
 		settingsList.classList.add('wikia-jw-settings__list');
 		settingsList.appendChild(this.createQualityButton());
 		settingsList.appendChild(this.createAutoplayToggle());
+
 		return settingsList;
 	};
 
 	WikiaJWPlayerSettings.prototype.createQualityButton = function () {
 		var qualityButton = document.createElement('li'),
 			self = this;
+
 		qualityButton.classList.add('wikia-jw-settings__quality-button');
 		qualityButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="jw-svg-icon jw-svg-icon-quality-100" viewBox="0 0 240 240"><path d="M55,200H35c-3,0-5-2-5-4c0,0,0,0,0-1v-30c0-3,2-5,4-5c0,0,0,0,1,0h20c3,0,5,2,5,4c0,0,0,0,0,1v30C60,198,58,200,55,200L55,200z M110,195v-70c0-3-2-5-4-5c0,0,0,0-1,0H85c-3,0-5,2-5,4c0,0,0,0,0,1v70c0,3,2,5,4,5c0,0,0,0,1,0h20C108,200,110,198,110,195L110,195z M160,195V85c0-3-2-5-4-5c0,0,0,0-1,0h-20c-3,0-5,2-5,4c0,0,0,0,0,1v110c0,3,2,5,4,5c0,0,0,0,1,0h20C158,200,160,198,160,195L160,195z M210,195V45c0-3-2-5-4-5c0,0,0,0-1,0h-20c-3,0-5,2-5,4c0,0,0,0,0,1v150c0,3,2,5,4,5c0,0,0,0,1,0h20C208,200,210,198,210,195L210,195z"></path></svg> Video Quality';
 		qualityButton.addEventListener('click', function () {
 			self.showQualityLevelsList();
 		});
+
 		return qualityButton;
 	};
 
@@ -133,6 +136,7 @@ define('wikia.articleVideo.featuredVideo.jwplayer.plugin.settings', ['wikia.arti
 			isActiveClass = 'is-active',
 			isQualityListEmpty = 'is-quality-list-empty';
 			self = this;
+
 		qualityLevelsList.classList.add('wikia-jw-settings__quality-levels');
 		backButton.classList.add('wikia-jw-settings__back');
 		backButton.innerHTML = '<svg class="wikia-jw-settings__back-icon" width="18" height="18" viewBox="0 0 18 18" data-reactid=".0.0.5.3.1.$quality.0.0.0.0.1.0"><path d="M9 14a.997.997 0 0 1-.707-.293l-7-7a.999.999 0 1 1 1.414-1.414L9 11.586l6.293-6.293a.999.999 0 1 1 1.414 1.414l-7 7A.997.997 0 0 1 9 14" data-reactid=".0.0.5.3.1.$quality.0.0.0.0.1.0.0"></path></svg> Back';
@@ -154,12 +158,14 @@ define('wikia.articleVideo.featuredVideo.jwplayer.plugin.settings', ['wikia.arti
 		playerInstance.on('levelsChanged', function (data) {
 			self.updateCurrentQuality(qualityLevelsList, data.currentQuality, isActiveClass);
 		});
+
 		return qualityLevelsList;
 	};
 
 	WikiaJWPlayerSettings.prototype.updateQualityLevelsList = function (qualityLevelsList, newLevels, isActiveClass, backButton) {
 		var playerInstance = this.player,
 			self = this;
+
 		while (qualityLevelsList.childElementCount > 1) {
 			qualityLevelsList.removeChild(qualityLevelsList.firstChild);
 		}
