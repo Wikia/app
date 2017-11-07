514954519cb44f19cb1f0a47b905aea272ab987b
mech <wozniak.jacek@gmail.com>
2017-11-07 16:24:08 +0100

    PLATFORM-3186: extra https changes for youtube tags

--- maintenance/wikia/VideoHandlers/fixit/upgradeYoutube.php
+++ maintenance/wikia/VideoHandlers/fixit/upgradeYoutube.php
@@ -301,7 +301,7 @@ class UpgradeYoutube extends Maintenance {
 					return $matches[0];
 				}
 
-				if ( preg_match('/^http:\/\//', $ytid) ) {
+				if ( preg_match('/^https?:\/\//', $ytid) ) {
 					$url = trim($ytid);
 				} else {
 					$url = 'https://www.youtube.com/watch?v='.trim($ytid);
