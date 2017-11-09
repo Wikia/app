0424a87336119747abdd8d7c2309ba7b57885533
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-11-09 19:26:13 +0100

    XW-4262 | Load all data from liftigniter service to limit number of requests made to service

--- extensions/wikia/Recirculation/RecirculationHooks.class.php
+++ extensions/wikia/Recirculation/RecirculationHooks.class.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Swagger\Client\Liftigniter\Metadata\Models\Item;
 /**
  * Class RecirculationHooks
  */
@@ -81,7 +82,7 @@ class RecirculationHooks {
 		$isInShowableNamespaces = $title->exists() && $title->inNamespaces( $showableNamespaces );
 
 		if ( $isInShowableNamespaces && !WikiaPageType::isActionPage() &&
-		     !WikiaPageType::isCorporatePage()
+			!WikiaPageType::isCorporatePage()
 		) {
 			return true;
 		} else {
@@ -128,15 +129,42 @@ class RecirculationHooks {
 	}
 
 	/**
+	 * @param String $cityId
+	 * @param String $articleId
+	 * @return null|Item
+	 */
+	private static function getMetaDataForArticle( String $cityId, String $articleId ) {
+		$metaDataFromService = WikiaDataAccess::cache(
+			'liftigniter-metadata',
+			WikiaResponse::CACHE_VERY_SHORT,
+			function() {
+				$metaDataService = new LiftigniterMetadataService();
+
+				return $metaDataService->getLiMetadata();
+			}
+		);
+
+		return current(array_filter( $metaDataFromService, function ( $item ) use ( $cityId, $articleId ) {
+			/** @var Item $item */
+			if ( $item->getProduct() === $cityId && $item->getId() === $articleId ) {
+				return $item;
+			}
+
+			return null;
+		}));
+	}
+
+	/**
 	 * @return array
 	 */
 	private static function getMetaData() {
 		global $wgLanguageCode, $wgCityId, $wgEnableArticleFeaturedVideo;
+
 		$title = RequestContext::getMain()->getTitle();
 		$articleId = $title->getArticleID();
-		$metaDataService = new LiftigniterMetadataService();
-		$metaDataFromService = $metaDataService->getLiMetadataForArticle( $wgCityId, $articleId );
-		$shouldNoIndex = self::shoudlNoIndex( $metaDataFromService );
+
+		$metaDataFromService = self::getMetaDataForArticle( $wgCityId, $articleId );
+		$shouldNoIndex = self::shouldNoIndex( $metaDataFromService );
 		$metaData = [];
 		$metaData['language'] = $wgLanguageCode;
 
@@ -174,17 +202,17 @@ class RecirculationHooks {
 	}
 
 	/**
-	 * @param $metaDataFromService is actual Data returned by Liftigniter Metadata Service
+	 * @param Item $metaDataFromService is actual Data returned by Liftigniter Metadata Service
 	 *
 	 * @return bool
 	 */
-	private static function shoudlNoIndex( $metaDataFromService ) {
+	private static function shouldNoIndex( $metaDataFromService ) {
 		global $wgDisableShowInRecirculation;
 
 		return self::isPrivateOrNotProduction() ||
-		       ( ( self::isNoIndexNamespace() || $wgDisableShowInRecirculation ) &&
-		         empty( $metaDataFromService ) ) ||
-		       RequestContext::getMain()->getRequest()->getVal( 'redirect' ) === 'no';
+			( ( self::isNoIndexNamespace() || $wgDisableShowInRecirculation ) &&
+				empty( $metaDataFromService ) ) ||
+			RequestContext::getMain()->getRequest()->getVal( 'redirect' ) === 'no';
 	}
 
 	/**
--- includes/wikia/services/LiftigniterMetadataService.class.php
+++ includes/wikia/services/LiftigniterMetadataService.class.php
@@ -17,17 +17,18 @@ class LiftigniterMetadataService {
 	 * @param $cityId
 	 * @param $pageId
 	 *
-	 * @return null|Item
+	 * @return null|Item[]
 	 */
-	public function getLiMetadataForArticle( $cityId, $pageId ) {
+	public function getLiMetadata() {
 		$api = $this->getItemsInternalApiClient();
 		$api->getApiClient()
 			->getConfig()
 			->setApiKey(self::INTERNAL_REQUEST_HEADER, '1' );
 
 		$item = null;
+
 		try {
-			list($response, $code) = $api->getItemWithHttpInfo( $cityId, $pageId );
+			list($response, $code) = $api->getAllWithHttpInfo();
 
 			if ( $code == 200 ) {
 				$item = $response;
@@ -35,8 +36,6 @@ class LiftigniterMetadataService {
 		} catch ( ApiException $apiException ) {
 			WikiaLogger::instance()->debug( 'could not fetch data from liftigniter-metadata service', [
 				'exception' => $apiException,
-				'wiki_id' => intval( $cityId ),
-				'page_id' => intval( $pageId ),
 				'status_code' => intval( $apiException->getCode() )
 			] );
 		}
@@ -70,10 +69,8 @@ class LiftigniterMetadataService {
 		// default CURLOPT_TIMEOUT for API client is set to 0 which means no timeout.
 		// Overwriting to minimal value which is 1.
 		// cURL function is allowed to execute not longer than 1 second
-		$api->getApiClient()
+		return $api->getApiClient()
 			->getConfig()
 			->setCurlTimeout(1);
-
-		return $api;
 	}
 }
