62766708fafdbb6211492a95dbe87e8b4418ae18
dianafa <diana.falkowska@gmail.com>
2017-11-13 15:17:29 +0100

    ADEN-6189 add pubmatic tests

---
+++ extensions/wikia/AdEngine/js/spec/lookup/prebid/adapters/pubmatic.spec.js
@@ -0,0 +1,76 @@
+/*global describe, expect, it, modules, spyOn*/
+describe('ext.wikia.adEngine.lookup.prebid.adapters.pubmatic', function () {
+	'use strict';
+
+	var mocks = {
+		instantGlobals: {
+			wgAdDriverPubMaticBidderCountries: ['PL']
+		},
+		geo: {
+			isProperGeo: function() {
+			}
+		},
+		slotsContext: {
+			filterSlotMap: function (map) {
+				return map;
+			}
+		},
+		instartLogic: {
+			isBlocking: function() {
+				return false;
+			}
+		},
+		log: function () {}
+	};
+
+	mocks.log.levels = {};
+
+	function getPubMatic() {
+		return modules['ext.wikia.adEngine.lookup.prebid.adapters.pubmatic'](
+			mocks.slotsContext,
+			mocks.instartLogic,
+			mocks.geo,
+			mocks.instantGlobals,
+			mocks.log
+		);
+	}
+
+	it('isEnabled checks whether user is not blocking ads', function () {
+		var pubmatic = getPubMatic();
+		spyOn(mocks.instartLogic, 'isBlocking').and.returnValue(false);
+		spyOn(mocks.geo, 'isProperGeo').and.returnValue(true);
+
+		expect(pubmatic.isEnabled()).toBeTruthy();
+	});
+
+	it('', function () {
+		var pubmatic = getPubMatic(),
+			isProperGeoSpy = spyOn(mocks.geo, 'isProperGeo');
+
+		pubmatic.isEnabled();
+		expect(isProperGeoSpy).toHaveBeenCalledWith(['PL']);
+	});
+
+	it('isEnabled checks the countries instant global', function () {
+		var pubmatic = getPubMatic();
+		expect(pubmatic.prepareAdUnit('TOP_LEADERBOARD', {
+			sizes: [
+				[728, 90],
+				[970, 250]
+			],
+			id: 123
+		})).toEqual({
+			code: 'TOP_LEADERBOARD',
+			sizes: [[728, 90], [970, 250]],
+			bids: [
+				{
+					bidder: 'pubmatic',
+					params: {
+						adSlot: 123,
+						publisherId: 156260
+					}
+				}
+			]
+		});
+	});
+});
