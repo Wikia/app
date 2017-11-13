bf70cdd1e8fc18254e27c3ba95d03f10d78e05d2
Karol Tatała <ktatala@wikia-inc.com>
2017-11-13 09:43:37 +0100

    XW-4229 | fallback pt, zh language

--- extensions/wikia/ArticleVideo/ArticleVideoController.class.php
+++ extensions/wikia/ArticleVideo/ArticleVideoController.class.php
@@ -1,16 +1,30 @@
 <?php
 
 class ArticleVideoController extends WikiaController {
+
+	private function fallbackLanguage( $lang ) {
+		switch ( $lang ) {
+			case 'zh-hant':
+			case 'zh-tw':
+				return 'zh';
+			case 'pt-br':
+				return 'pt';
+			default:
+				return $lang;
+		}
+	}
+
 	public function featured() {
 		$requestContext = $this->getContext();
 		$title = $requestContext->getTitle()->getPrefixedDBkey();
+		$lang = $requestContext->getLanguage()->getCode();
 
 		$featuredVideoData = ArticleVideoContext::getFeaturedVideoData( $title );
 
 		if ( !empty( $featuredVideoData ) ) {
 			$requestContext->getOutput()->addModules( 'ext.ArticleVideo' );
 
-			$featuredVideoData['lang'] = $requestContext->getLanguage()->getCode();
+			$featuredVideoData['lang'] = $this->fallbackLanguage( $lang );
 
 			$this->setVal( 'videoDetails', $featuredVideoData );
 
