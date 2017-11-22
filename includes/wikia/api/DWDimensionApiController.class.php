9b77a7ffe7a06be2192818720238d96a222b8f8a
jcellary <jaroslaw@wikia-inc.com>
2017-11-22 13:14:44 +0100

    Fixed formatting

--- includes/wikia/api/DWDimensionApiController.class.php
+++ includes/wikia/api/DWDimensionApiController.class.php
@@ -1,42 +1,42 @@
 <?php
 
 class DWDimensionApiController extends WikiaApiController {
-    const LIMIT = 100;
-    const AFTER_DOMAIN = null;
+	const LIMIT = 100;
+	const AFTER_DOMAIN = null;
 
-    private function getSharedDbSlave() {
-        global $wgExternalSharedDB;
-        return wfGetDB( DB_SLAVE, array(), $wgExternalSharedDB );
-    }
+	private function getSharedDbSlave() {
+		global $wgExternalSharedDB;
+		return wfGetDB( DB_SLAVE, array(), $wgExternalSharedDB );
+	}
 
 	public function getWikiDomains()
-    {
-        $limit = $this->getRequest()->getVal( 'limit', static::LIMIT );
-        $afterDomain = $this->getRequest()->getVal( 'after_domain', static::AFTER_DOMAIN );
+	{
+		$limit = $this->getRequest()->getVal( 'limit', static::LIMIT );
+		$afterDomain = $this->getRequest()->getVal( 'after_domain', static::AFTER_DOMAIN );
 
-        $db = $this->getSharedDbSlave();
-        $dbResult = $db->select(
-            ["city_domains"],
-            ["city_id", "city_domain"],
-            isset( $afterDomain ) ? [ "city_domain > ".$afterDomain ] : [],
-            __METHOD__,
-            [
-                "ORDER BY" => "city_domain",
-                "LIMIT" => $limit
-            ]
-        );
-        $result = [];
-        while ($row = $db->fetchObject($dbResult)) {
-            $result[] = [
-                'city_id' => $row->city_id,
-                'city_domain' => $row->city_domain
-            ];
-        }
+		$db = $this->getSharedDbSlave();
+		$dbResult = $db->select(
+			["city_domains"],
+			["city_id", "city_domain"],
+			isset( $afterDomain ) ? [ "city_domain > ".$afterDomain ] : [],
+			__METHOD__,
+			[
+				"ORDER BY" => "city_domain",
+				"LIMIT" => $limit
+			]
+		);
+		$result = [];
+		while ($row = $db->fetchObject($dbResult)) {
+			$result[] = [
+				'city_id' => $row->city_id,
+				'city_domain' => $row->city_domain
+			];
+		}
 
-        $this->setResponseData(
-            $result,
-            null,
-            WikiaResponse::CACHE_DISABLED
-        );
-    }
+		$this->setResponseData(
+			$result,
+			null,
+			WikiaResponse::CACHE_DISABLED
+		);
+	}
 }
