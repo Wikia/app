cb2a31a5003eb70566bc753ce2884b6ee8df4e7b
thetrentus <thetrentus@users.noreply.github.com>
2017-11-22 19:06:41 +0800

    Fixing ii8n messages
    Fixing more ii8n messages

--- extensions/wikia/FirstContributions/FirstContributions.i18n.php
+++ extensions/wikia/FirstContributions/FirstContributions.i18n.php
@@ -1,9 +1,9 @@
 <?php
 
 $messages['en'] = [
-	'neweditors-desc' => 'Implements API module to list first edits of new editors on a wiki',
+	'firstcontributions-desc' => 'Implements API module to list first edits of new editors on a wiki',
 ];
 
 $messages['qqq'] = [
-	'neweditors-desc'=> 'Description message of extension shown to users on Special:Version',
+	'firstcontributions-desc'=> 'Description message of extension shown to users on Special:Version',
 ];
