2a9fb145888ba1575d98b3404472358cdfa10ed1
Kacper Olek <kolek@fandom.com>
2017-11-23 09:52:41 +0000

    Cleanup RTE3,EditPageLayoutX

--- extensions/wikia/EditPageLayoutX/templates/EditPageLayout_wordmark.php
+++
@@ -1,9 +0,0 @@
-<h2 class="wordmark <?= $wordmarkSize ?> <?= $wordmarkType ?> <?= $wordmarkFontClass ?>">
-	<a accesskey="z" href="<?= Sanitizer::encodeAttribute( $mainPageURL ); ?>">
-		<? if (!empty($wordmarkUrl)) { ?>
-			<img src="<?= $wordmarkUrl ?>" alt="<?= Sanitizer::encodeAttribute( $wordmarkText ); ?>" <? if (!empty($wordmarkStyle)) { ?><?= $wordmarkStyle ?><? } ?>>
-		<? } else { ?>
-			<?= htmlspecialchars($wordmarkText) ?>
-		<? } ?>
-	</a>
-</h2>
