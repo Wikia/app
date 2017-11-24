ef5c586ad20d65b9a245c9fe1905006e342cc2b0
Máté Szabó <tk.999.wikia@gmail.com>
2017-11-24 15:00:02 +0100

    IRIS-5184 Remove type hint

--- extensions/wikia/Discussions/DiscussionsHooksHelper.php
+++ extensions/wikia/Discussions/DiscussionsHooksHelper.php
@@ -15,10 +15,10 @@ class DiscussionsHooksHelper {
 	 * IRIS-5184: Exclude outgoing links in Forum content from Special:WantedPages report
 	 * @see WantedPagesPage::getQueryInfo()
 	 *
-	 * @param IContextSource $context
+	 * @param WantedPagesPage $wantedPagesPage
 	 * @param array $queryInfo
 	 */
-	public static function onWantedPagesGetQueryInfo( IContextSource $context, array &$queryInfo ) {
+	public static function onWantedPagesGetQueryInfo( $wantedPagesPage, array &$queryInfo ) {
 		$queryInfo['conds'][] = "pg2.page_namespace != '" . NS_WIKIA_FORUM_BOARD_THREAD . "'";
 	}
 
