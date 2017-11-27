a3291f52f4391062ac8e073c482ba0a178500dbe
Michał “Mix” Roszka <mix@mixoteric.net>
2017-11-27 20:52:55 +0000

    SRE-19 → sanitize usage of $wgDBserver
    At this point $wgDBserver is not yet set properly which renders the fallback
    futile.  The variable is updated later.  It depends on the mode (rw/ro) and
    the cluster (a, b, …) therefore cannot be preset in the config.

--- extensions/wikia/WikiFactory/Loader/WikiFactoryLoader.php
+++ extensions/wikia/WikiFactory/Loader/WikiFactoryLoader.php
@@ -177,8 +177,7 @@ class WikiFactoryLoader {
 	 * getDB
 	 *
 	 * Method for getting database handler. It checks if $wgDBservers is
-	 * available, if yes it will take one of active slaves. If not it fallbacks
-	 * to $wgDBserver
+	 * available, if yes it will take one of active slaves.
 	 *
 	 * @author Krzysztof Krzyżaniak <eloy@wikia-inc.com>
 	 * @access public
@@ -189,7 +188,6 @@ class WikiFactoryLoader {
 	 * @return DatabaseBase database handler
 	 */
 	public function getDB( $type = DB_SLAVE ) {
-		global $wgDBserver, $wgDBuser, $wgDBpassword;
 
 		if( $this->mDBhandler instanceof DatabaseBase ) {
 			return $this->mDBhandler;
@@ -201,15 +199,6 @@ class WikiFactoryLoader {
 		 */
 		$this->mDBhandler = wfGetDB( $type, array(), $this->mDBname );
 		$this->debug( "connecting to {$this->mDBname} via LoadBalancer" );
-
-		/**
-		 * if something goes wrong just fallback to $wgDBserver
-		 */
-		if( !$this->mDBhandler || !$this->mDBhandler->isOpen() ) {
-			error_log( "WikiFactoryLoader[{$this->mCityID}]: fallback to {$wgDBserver}" );
-			$this->mDBhandler = new DatabaseMysqli( $wgDBserver, $wgDBuser, $wgDBpassword, $this->mDBname );
-			$this->debug( "fallback to wgDBserver {$wgDBserver}" );
-		}
 
 		return $this->mDBhandler;
 	}
