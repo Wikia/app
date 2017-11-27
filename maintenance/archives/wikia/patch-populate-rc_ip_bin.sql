f0f188a763b325ed4403bac1bf1e55e67705ab71
TK-999 <tk.999.wikia@gmail.com>
2017-11-27 12:50:23 +0100

    SUS-3079 Remove fields from updater

--- maintenance/archives/wikia/patch-populate-rc_ip_bin.sql
+++ maintenance/archives/wikia/patch-populate-rc_ip_bin.sql
@@ -1,4 +1,4 @@
 -- SUS-3079: Populate rc_ip_bin column
 
 UPDATE /*$wgDBPrefix*/recentchanges
-  SET rc_ip_bin = IFNULL(INET6_ATON(rc_ip), '') WHERE rc_ip_bin = '';
+  SET rc_ip_bin = IFNULL(INET6_ATON(`rc_ip`), '') WHERE rc_ip_bin = '';
