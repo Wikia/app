ce844c90ca87fa1c9c889778e21b42360ba118b9
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-11-29 14:56:59 +0100

    XW-4189 | Video metatags for featured video pages

--- extensions/wikia/ArticleVideo/ArticleVideoController.class.php
+++ extensions/wikia/ArticleVideo/ArticleVideoController.class.php
@@ -15,6 +15,38 @@ class ArticleVideoController extends WikiaController {
 		}
 	}
 
+	private function getIsoTime($colonDelimitedTime) {
+		$segments = explode(':', $colonDelimitedTime);
+		$isoTime = '';
+
+		if (count($segments) > 2) {
+			$isoTime = 'H' . $segments[0] . 'M' . $segments[1] . 'S' . $segments[2];
+		} else if (count($segments) > 1) {
+			$isoTime = 'M' . $segments[0] . 'S' . $segments[1];
+		} else if (count($segments) > 0) {
+			$isoTime = 'S' . $segments[0];
+		}
+
+		return $isoTime;
+	}
+
+	private function getVideoContentUrl($sources) {
+		return $sources[count($sources) - 1];
+	}
+
+	private function getVideoMetaData($videoDetails) {
+		$playlistItem = $videoDetails['playlist'][0];
+
+		return [
+			'name' => $videoDetails['title'],
+			'thumbnailUrl' => $playlistItem['image'],
+			'uploadDate' => date('c', $playlistItem['pubdate']),
+			'duration' => $this->getIsoTime($videoDetails['duration']),
+			'description' => $videoDetails['description'],
+			'contentUrl' => self::getVideoContentUrl($playlistItem['sources'])
+		];
+	}
+
 	public function featured() {
 		$requestContext = $this->getContext();
 		$title = $requestContext->getTitle()->getPrefixedDBkey();
@@ -28,6 +60,7 @@ class ArticleVideoController extends WikiaController {
 			$featuredVideoData['lang'] = $this->fallbackLanguage( $lang );
 
 			$this->setVal( 'videoDetails', $featuredVideoData );
+			$this->setVal( 'videoMetaData', $this->getVideoMetaData($featuredVideoData) );
 
 			$jwPlayerScript =
 				$requestContext->getOutput()->getResourceLoader()->getModule( 'ext.ArticleVideo.jw' )->getScript(
--- extensions/wikia/ArticleVideo/templates/ArticleVideo_featured.php
+++ extensions/wikia/ArticleVideo/templates/ArticleVideo_featured.php
@@ -1,23 +1,30 @@
-<div class="featured-video__wrapper">
-	<div class="featured-video">
-		<div class="featured-video__player-container">
-			<div id="featured-video__player" class="featured-video__player"></div>
-			<div class="featured-video__details">
-				<div class="featured-video__label"><?= wfMessage( 'articlevideo-watch' )->escaped() ?>
-					<span class="featured-video__time"><?= $videoDetails['duration'] ?></span>
+<div itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+	<h2>Video: <span itemprop="<?= htmlspecialchars( $videoMetaData['name'] ) ?>">Title</span></h2>
+	<meta itemprop="duration" content="T1M33S" />
+	<meta itemprop="thumbnailUrl" content="<?= htmlspecialchars( $videoMetaData['thumbnailUrl'] ) ?>" />
+	<meta itemprop="contentURL" content="<?= htmlspecialchars( $videoMetaData['contentUrl'] ) ?>" />
+	<meta itemprop="uploadDate" content="<?= htmlspecialchars( $videoMetaData['uploadDate'] ) ?>" />
+	<div class="featured-video__wrapper">
+		<div class="featured-video">
+			<div class="featured-video__player-container">
+				<div id="featured-video__player" class="featured-video__player"></div>
+				<div class="featured-video__details">
+					<div class="featured-video__label"><?= wfMessage( 'articlevideo-watch' )->escaped() ?>
+						<span class="featured-video__time"><?= htmlspecialchars( $videoDetails['duration'] ) ?></span>
+					</div>
+					<div class="featured-video__title"><?= htmlspecialchars( $videoDetails['title'] ) ?></div>
 				</div>
-				<div
-					class="featured-video__title"><?= htmlspecialchars( $videoDetails['title'] ) ?></div>
+				<?= DesignSystemHelper::renderSvg( 'wds-icons-cross-tiny', 'wds-icon wds-icon-tiny featured-video__close' ) ?>
 			</div>
-			<?= DesignSystemHelper::renderSvg( 'wds-icons-cross-tiny', 'wds-icon wds-icon-tiny featured-video__close' ) ?>
+			<?= $app->renderPartial( 'ArticleVideo', 'feedback' ) ?>
+			<script>
+				define('wikia.articleVideo.featuredVideo.data', function() {
+					return <?= json_encode($videoDetails); ?>;
+				})
+			</script>
+			<script><?= $jwPlayerScript ?></script>
 		</div>
-		<?= $app->renderPartial( 'ArticleVideo', 'feedback' ) ?>
-		<script>
-			define('wikia.articleVideo.featuredVideo.data', function() {
-				return <?= json_encode($videoDetails); ?>;
-			})
-		</script>
-		<script><?= $jwPlayerScript ?></script>
+		<?= $app->renderPartial( 'ArticleVideo', 'attribution', [ 'videoDetails' => $videoDetails ] ) ?>
 	</div>
-	<?= $app->renderPartial( 'ArticleVideo', 'attribution', [ 'videoDetails' => $videoDetails ] ) ?>
+	<span itemprop="description"><?= htmlspecialchars( $videoMetaData['description'] ) ?></span>
 </div>
