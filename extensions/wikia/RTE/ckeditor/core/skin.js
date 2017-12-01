b7544f96b0346df5bf5b007559ebb96cd652a26c
Kacper Olek <kolek@fandom.com>
2017-12-01 12:58:01 +0000

    css -> Sass initial

--- extensions/wikia/RTE/ckeditor/core/skin.js
+++ extensions/wikia/RTE/ckeditor/core/skin.js
@@ -159,7 +159,7 @@
 				}
 			}
 		}
-		return CKEDITOR.getUrl( getConfigPath() + part + '.css' );
+		return $.getSassCommonURL('extensions/wikia/RTE/ckeditor/skins/wikia/' + part + '.scss');//CKEDITOR.getUrl( getConfigPath() + part + '.css' );
 	}
 
 	function loadCss( part, callback ) {
