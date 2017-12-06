164bb1f000de69106336e6e784d4220693c6e9a9
TK-999 <tk.999.wikia@gmail.com>
2017-12-06 15:21:33 +0100

    SUS-3496 JIRA ticket ID

--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -1904,5 +1904,5 @@ require_once "$IP/extensions/wikia/SpecialMultipleLookup/SpecialMultipleLookup.p
 // SUS-3475: Extension to update shared city_list table
 require_once "$IP/extensions/wikia/CityList/CityList.setup.php";
 
-// Extension to update shared dataware.pages table
+// SUS-3496: Extension to update shared dataware.pages table
 require_once "$IP/extensions/wikia/Pages/Pages.setup.php";
