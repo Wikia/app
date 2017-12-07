89a8b71debf8f5b8c2c5f3a9dc4b1cf70a36fc4e
Kacper Olek <kolek@fandom.com>
2017-12-07 14:25:07 +0000

    simplified language generation(inCaseOfUpdate)

--- extensions/wikia/RTE/inCaseOfUpdate/README.txt
+++ extensions/wikia/RTE/inCaseOfUpdate/README.txt
@@ -1,16 +1,13 @@
 PHP scripts that are used to generate proper messages for CKEDITOR.
 
 Instructions:
-	1. Place generate_i18n_from_core.php and scriptCore.js in /ckeditor/lang/. folder.
-	2. Execute 'php generate_i18n_from_core.php and save the output to CK.core.i18n.phpCORE.
-	3. Place it in /RTE/i18n/. 
+
+	Execute ./generate_i18n.sh.
+
+	It will use the files that are in ckeditor/lang/. as well as ckeditor/plugins/.
 	
-	4. Place generate_i18n_from_plugins.php and scriptPlugins.js in /ckeditor/plugins/. folder.
-	5. Execute 'php generate_i18n_from_plugins.php and save the output CK.core.i18n.phpPLUGINS.
-	6. Place it in /RTE/i18n/.
+	If you want to change which plugins will be included in the file, modify the array in generate_i18n_from_plugins.php.
 	
-	7. Place merge_into_one.php in /RTE/i18n/.
-	8. Execute 'php merge-into-one.php'.
-	9. ???????
-	10. PROFIT $$$
-	11. The output is the final generated language file. Save it as CK.core.i18n.php.
+	OUTPUT: RTE/i18n/CK.core.i18n.php
+	
+	Use Wisely.
---
+++ extensions/wikia/RTE/inCaseOfUpdate/generate_i18n.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cp scripts/generate_i18n_from_core.php ../ckeditor/lang/.;
+cp scripts/scriptCore.js ../ckeditor/lang/.;
+
+cp scripts/generate_i18n_from_plugins.php ../ckeditor/plugins/.;
+cp scripts/scriptPlugins.js ../ckeditor/plugins/.;
+
+cp scripts/merge-into-one.php ../i18n/.;
+
+cd ../ckeditor/lang/.;
+printf "Currently at: ";
+printf `pwd`;
+printf "\n";
+
+printf "Processing CORE\n";
+php generate_i18n_from_core.php > CK.core.i18n.phpCORE;
+
+mv CK.core.i18n.phpCORE ../../i18n/.;
+
+rm generate_i18n_from_core.php;
+rm scriptCore.js;
+
+cd ../plugins/.;
+printf "Currently at: ";
+printf `pwd`;
+printf "\n";
+
+printf "Processing PLUGINS\n";
+
+php generate_i18n_from_plugins.php > CK.core.i18n.phpPLUGINS;
+
+mv CK.core.i18n.phpPLUGINS ../../i18n/.;
+
+rm generate_i18n_from_plugins.php;
+rm scriptPlugins.js;
+
+cd ../../i18n/.;
+
+printf "Currently at: ";
+printf `pwd`;
+printf "\n";
+
+printf "Merging\n";
+
+php merge-into-one.php > CK.core.i18n.php;
+
+rm merge-into-one.php;
+rm CK.core.i18n.phpCORE;
+rm CK.core.i18n.phpPLUGINS;
+
+printf "Generated file is in RTE/i18n/.\n";
+printf "Done!\n";
