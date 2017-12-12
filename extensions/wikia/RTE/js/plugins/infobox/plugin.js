3238b59d6b16f0fc73748e5223a51b1c17fe2c9f
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-12-12 16:07:28 +0100

    XW-4356 | Making dialog work

--- extensions/wikia/RTE/js/plugins/infobox/plugin.js
+++ extensions/wikia/RTE/js/plugins/infobox/plugin.js
@@ -16,6 +16,32 @@ require(['jquery', 'wikia.window'], function ($, window) {
 		$.get('/api.php?format=json&action=query&list=allinfoboxes&yselang=' + window.wgContentLanguage)
 			.then(function (data) {
 				console.log(data);
+				window.CKEDITOR.dialog.add( 'infobox-dialog', function( editor ) {
+					return {
+						title: 'Abbreviation Properties',
+						minWidth: 400,
+						minHeight: 200,
+
+						contents: [
+							{
+								id: 'tab-basic',
+								label: 'Basic Settings',
+								elements: [
+									// UI elements of the first tab will be defined here.
+								]
+							},
+							{
+								id: 'tab-adv',
+								label: 'Advanced Settings',
+								elements: [
+									// UI elements of the second tab will be defined here.
+								]
+							}
+						]
+					};
+				});
+
+				RTE.getInstance().openDialog('infobox-dialog');
 			});
 	}
 
