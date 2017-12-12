07290f9ee7c9d9bf1a164a57fd58e7685370372d
Adam Robak <robakadam@gmail.com>
2017-12-12 16:34:00 +0100

    SER-1478 | use chunks approach

--- maintenance/wikia/PandoraCoppaImageReviewCleanUp.php
+++ maintenance/wikia/PandoraCoppaImageReviewCleanUp.php
@@ -12,24 +12,53 @@ class PandoraCoppaImageReviewCleanUp extends Maintenance {
 		$imageReviewDB = wfGetDB( DB_SLAVE, [], $wgSpecialsDB );
 		$wikiDB = wfGetDB( DB_SLAVE );
 
-		$result = $imageReviewDB->select( 'image_review.images_coppa',
-			[ 'wiki_id', 'page_id', 'revision_id' ],
-			[ 'wiki_id = ' . $wgCityId ],
-			'PandoraCoppaImageReviewCleanUp:execute'
-		);
+		$pageList = ( new WikiaSQL() )
+			->SELECT( 'wiki_id', 'page_id', 'revision_id' )
+			->FROM( 'image_review.images_coppa' )
+			->WHERE( 'wiki_id' )->EQUAL_TO( $wgCityId )
+			->runLoop( $imageReviewDB, function ( $row ) {
+				return $row;
+			} );
+
+		$pages = array_combine( array_map( function ( $item ) {
+			return $item['page_id'];
+		}, $pageList ), $pageList );
+
+		$idChunks = array_chunk( $pages, 100, true );
 
 		print_r( "Image review query time: " . ( time() - $start ) . "sec\n" );
-		print_r( "Images to check: " . $result->numRows() . "\n" );
+		print_r( "Images to check: " . count( $pageList ) . "\n" );
+		print_r( "Chunks to check: " . count( $idChunks ) . "\n" );
 		$wikiStart = time();
 		$missingImages = [];
-		while ( $row = $result->fetchRow() ) {
-			$pageTest = $wikiDB->select( 'page', [ 1 ], [ 'page_id = ' . $row['page_id'] ] );
-			$revisionTest = $wikiDB->select( 'revision', [ 1 ], [ 'rev_id = ' . $row['revision_id'] ] );
+		foreach ( $idChunks as $chunk ) {
+			print_r( "Chunk size: " . count( $chunk ) . "\n" );
+			( new WikiaSQL() )
+				->SELECT( 'page_id' )
+				->FROM( 'page' )
+				->WHERE( 'page_id' )
+				->IN( array_map( function ( $row ) {
+					return $row['page_id'];
+				}, $chunk ) )
+				->runLoop( $wikiDB, function ( $res ) use ( $chunk ) {
+					unset( $chunk[$res['page_id']] );
+				} );
+			print_r( "Chunk size after page id: " . count( $chunk ) . "\n" );
+			( new WikiaSQL() )
+				->SELECT( 'rev_page' )
+				->FROM( 'revision' )
+				->WHERE( 'rev_id' )
+				->IN( array_map( function ( $row ) {
+					return $row['revision_id'];
+				}, $chunk ) )
+				->runLoop( $wikiDB, function ( $res ) use ( $chunk ) {
+					unset( $chunk[$res['page_id']] );
+				} );
+			print_r( "Chunk size after rev id: " . count( $chunk ) . "\n" );
 
-			if ( $pageTest->numRows() == 0 && $revisionTest->numRows() == 0 ) {
-				$missingImages[] = $row;
-			}
+			$missingImages = array_merge( $missingImages, $chunk );
 		}
+
 		print_r( "Wiki query time: " . ( time() - $wikiStart ) . "sec\n" );
 
 		print_r( "Broken images number: " . count( $missingImages ) . "\n" );
