a614e1f086eeb6374e13a96d42bb0bd787c564b6
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-12-12 15:22:47 +0100

    XW-4356 | Fetching infobox list on button click

--- extensions/wikia/RTE/js/plugins/infobox/plugin.js
+++ extensions/wikia/RTE/js/plugins/infobox/plugin.js
@@ -1,13 +1,23 @@
-CKEDITOR.plugins.add( 'rte-infobox', {
-	init: function( editor ) {
-
-		editor.addCommand( 'addinfobox', {
-			exec: function( editor ) {
-				console.log("Hello Infobox!");
-			}
+require(['jquery', 'wikia.window'], function ($, window) {
+	function registerPlugin() {
+		window.CKEDITOR.plugins.add( 'rte-infobox', {
+			init: onEditorInit
 		});
-
-
 	}
 
+	function onEditorInit(editor) {
+		editor.addCommand( 'addinfobox', {
+			exec: openInfoboxModal
+		});
+	}
+
+	function openInfoboxModal(editor) {
+		//ToDo unharcode lang
+		$.get('/api.php?format=json&action=query&list=allinfoboxes&yselang=' + window.wgContentLanguage)
+			.then(function (data) {
+				console.log(data);
+			});
+	}
+
+	registerPlugin();
 });
