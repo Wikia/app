9022423e20c16f52b258c87d59675284d8c0d012
Adam Robak <robakadam@gmail.com>
2017-12-12 15:10:07 +0100

    SER-1478 | check if image exists

--- maintenance/wikia/PandoraCoppaImageReviewCleanUp.php
+++ maintenance/wikia/PandoraCoppaImageReviewCleanUp.php
@@ -5,13 +5,28 @@ require_once( dirname( __FILE__ ) . '/../Maintenance.php' );
 class PandoraCoppaImageReviewCleanUp extends Maintenance {
 
 	public function execute() {
-		global $wgSpecialsDB;
+		global $wgSpecialsDB, $wgCityId;
 
-		$db = wfGetDB( DB_SLAVE, [], $wgSpecialsDB );
-		$result = $db->query( "SELECT * FROM image_review.images_coppa LIMIT 1" );
+		$imageReviewDB = wfGetDB( DB_SLAVE, [], $wgSpecialsDB );
+		$wikiDB = wfGetDB( DB_SLAVE );
+
+		$result = $imageReviewDB->select( 'image_review.images_coppa',
+			[ 'wiki_id', 'page_id', 'revision_id' ],
+			[ 'wiki_id = ' . $wgCityId ],
+			'PandoraCoppaImageReviewCleanUp:execute'
+		);
+
+		$missingImages = [];
 		while ( $row = $result->fetchRow() ) {
-			var_dump( $row );
+			$pageTest = $wikiDB->select( 'page', [ 1 ], [ 'page_id = ' . $row['page_id'] ] );
+			$revisionTest = $wikiDB->select( 'revision', [ 1 ], [ 'rev_id = ' . $row['revision_id'] ] );
+
+			if ( $pageTest->numRows() == 0 && $revisionTest->numRows() == 0 ) {
+				$missingImages[] = $row;
+			}
 		}
+
+		print_r( $missingImages );
 	}
 }
 
