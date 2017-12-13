5c310c714729eaa9fcdb476c9693489c44b8a7e2
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2017-12-13 09:53:35 +0100

    ADEN-6236 CR fixes

--- extensions/wikia/AdEngine/src/templates/templates-registry.js
+++ extensions/wikia/AdEngine/src/templates/templates-registry.js
@@ -9,8 +9,8 @@ import { getConfig as getDesktopBfaaConfig } from './big-fancy-ad-above-desktop-
 
 export default class TemplateRegistry {
 	static init(legacyContext, mercuryListener) {
-		const isMobile = legacyContext.get('targeting.skin') !== 'oasis',
-			getBfaaConfig = isMobile ? getMobileBfaaConfig : getDesktopBfaaConfig;
+		const isMobile = legacyContext.get('targeting.skin') !== 'oasis';
+		const getBfaaConfig = isMobile ? getMobileBfaaConfig : getDesktopBfaaConfig;
 
 		TemplateService.register(BigFancyAdAbove, getBfaaConfig(mercuryListener));
 		TemplateService.register(BigFancyAdBelow);
