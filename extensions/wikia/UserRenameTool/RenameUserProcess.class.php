97b8d0cf489494dbfd3b36cbe3da3e9e2ee408d9
macbre <maciej.brencz@gmail.com>
2017-12-15 15:37:17 +0100

    RenameUserProcess: blockUserRenaming method cleanup

--- extensions/wikia/UserRenameTool/RenameUserProcess.class.php
+++ extensions/wikia/UserRenameTool/RenameUserProcess.class.php
@@ -621,7 +621,12 @@ class RenameUserProcess {
 		return !$user->getGlobalFlag( self::USER_ALREADY_RENAMED_FLAG, false ) || $user->isAllowed( 'renameanotheruser' );
 	}
 
-	public static function blockUserRenaming( User $user, bool $flag = true ) {
-		return $user->setGlobalFlag( self::USER_ALREADY_RENAMED_FLAG, $flag );
+	/**
+	 * Mark a user as renamed. This will prevent any further attempts to rename it.
+	 *
+	 * @param User $user
+	 */
+	private static function blockUserRenaming( User $user ) {
+		$user->setGlobalFlag( self::USER_ALREADY_RENAMED_FLAG, true );
 	}
 }
