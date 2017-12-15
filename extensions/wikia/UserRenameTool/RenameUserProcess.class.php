a0cc9e6964c3669dd4e911d14a00a4e36f866b77
macbre <maciej.brencz@gmail.com>
2017-12-15 14:26:42 +0100

    RenameUserProcess: log to ELK + remove not used addLocalLog() methods

--- extensions/wikia/UserRenameTool/RenameUserProcess.class.php
+++ extensions/wikia/UserRenameTool/RenameUserProcess.class.php
@@ -1,8 +1,11 @@
 <?php
 
 use Wikia\Tasks\Tasks\RenameUserPagesTask;
+use Wikia\Logger\Loggable;
 
 class RenameUserProcess {
+	use Loggable;
+
 	const EMAIL_CONTROLLER = \Email\Controller\UserNameChangeController::class;
 
 	const RENAME_TAG = 'renamed_to';
@@ -486,6 +489,13 @@ class RenameUserProcess {
 		return empty( $this->getErrors() );
 	}
 
+	protected function getLoggerContext() {
+		return [
+			'user_rename_from' => $this->getOldUsername(),
+			'user_rename_to' => $this->getNewUsername(),
+		];
+	}
+
 	/**
 	 * Sends the internal log message to the specified destination
 	 *
@@ -500,16 +510,8 @@ class RenameUserProcess {
 		}
 
 		wfDebugLog( __CLASS__, $text );
-	}
 
-	/**
-	 * Adds log message in Special:Log for the current wiki
-	 *
-	 * @param $text string Log message
-	 */
-	public function addLocalLog( $text ) {
-		$log = new LogPage( 'renameuser' );
-		$log->addEntry( 'renameuser', Title::newFromText( $this->mOldUsername, NS_USER ), $text, array(), User::newFromId( $this->mRequestorId ) );
+		$this->info( $text );
 	}
 
 	/**
