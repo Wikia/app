5a8897ac526719c10025cda7eea7ee93f2f10392
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-12-20 15:42:49 +0100

    XW-4380: fix jwplayer tag styles so it behaves properly in CKE

--- extensions/wikia/JWPlayerTag/JWPlayerTagController.class.php
+++ extensions/wikia/JWPlayerTag/JWPlayerTagController.class.php
@@ -80,7 +80,8 @@ class JWPlayerTagController extends WikiaController {
 			self::DATA_ATTRS => json_encode( [
 				'media-id' => $args['media-id'],
 				'element-id' => $elementId,
-			] )
+			] ),
+			self::STYLE_ATTR => 'overflow:hidden'
 		];
 
 		if ( !empty( $width ) && is_numeric( $width ) ) {
