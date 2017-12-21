bca81827d47d0a7538aa338cb1c90a152887f2b6
Karol Tatała <ktatala@wikia-inc.com>
2017-12-21 15:40:38 +0100

    XW-4404 | anon user in content language

--- extensions/wikia/Recirculation/templates/Recirculation_discussions.php
+++ extensions/wikia/Recirculation/templates/Recirculation_discussions.php
@@ -33,7 +33,7 @@
 						     : null, 26 ) ?>">
 					<span class="mcf-card-discussions__user-subtitle"><?= !$post->authorIsAnon
 							? Sanitizer::escapeHtmlAllowEntities( $post->author )
-							: wfMessage( 'oasis-anon-user' )->escaped() ?>
+							: wfMessage( 'oasis-anon-user' )->inContentLanguage()->escaped() ?>
 						• <time class="discussion-timestamp"
 								datetime="<?= Sanitizer::encodeAttribute( $post->pub_date ) ?>"></time></span>
 				</a>
