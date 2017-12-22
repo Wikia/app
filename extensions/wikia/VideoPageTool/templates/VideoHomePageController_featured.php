bb089597078fa78f4c0f1ec2cdbd7ea403488bb2
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-12-22 14:32:17 +0100

    XW-4331: fix inserting svg in php template file

--- extensions/wikia/VideoPageTool/templates/VideoHomePageController_featured.php
+++ extensions/wikia/VideoPageTool/templates/VideoHomePageController_featured.php
@@ -6,9 +6,9 @@
 			<? foreach ( $assets as $videoData ): ?>
 				<li>
 					<div class="slide-image video video-thumbnail xlarge fluid" data-index="<?= $index ?>">
-						'<span class="thumbnail-play-icon-container">'
-							. <?= DesignSystemHelper::renderSvg('wds-player-icon-play', 'thumbnail-play-icon') ?>
-							. '</span>';
+						<span class="thumbnail-play-icon-container">
+							 <?= DesignSystemHelper::renderSvg('wds-player-icon-play', 'thumbnail-play-icon') ?>
+						</span>
 						<img src="<?= $videoData['largeThumbUrl'] ?>">
 						<div class="caption small-4 columns">
 							<span class="title"><?= $videoData['displayTitle'] ?></span>
