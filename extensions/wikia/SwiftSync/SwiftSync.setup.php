fc2f49e3180f229c5042ee8a11b5b7c67b7b1e99
macbre <maciej.brencz@gmail.com>
2018-01-15 11:21:27 +0100

    ImageSync: improve docs

--- extensions/wikia/SwiftSync/SwiftSync.setup.php
+++ extensions/wikia/SwiftSync/SwiftSync.setup.php
@@ -1,7 +1,18 @@
 <?php
 
 /**
- * Swift Sync classes/helpers etc
+ * Swift Sync
+ *
+ * This code synchronizes DFS operations made in our master DC (SJC) with our secondary DC (RES).
+ *
+ * Files on DFS can be:
+ *
+ *  - created (file from SJC is copied over to RES)
+ *  - deleted (delete operation takes place in RES)
+ *  - moved (copy and delete operation takes place locally on RES)
+ *
+ * Each of those operations as "queued" in ImageSyncTask class and at the end of the request
+ * handling they're are all queued as a single task and pushed to Celery / RabbitMQ.
  *
  * @author Maciej Brencz (macbre@wikia-inc.com)
  * @author Piotr Molski (moli@wikia-inc.com)
