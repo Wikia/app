934f051a02196f32680cee1e62db08d224ca080e
Kacper Olek <kolek@fandom.com>
2018-01-17 18:32:06 +0000

    Revert "Test 2"
    This reverts commit 7087624fbee5c64263dc2af86ce76decf96eea4e.

--- extensions/wikia/RTE/RTEReverseParser.class.php
+++ extensions/wikia/RTE/RTEReverseParser.class.php
@@ -419,12 +419,8 @@ class RTEReverseParser {
 				// templates and extension tags which are wrapped in <div> needs to have additional newline at the end
 				case 'double-brackets':
 				case 'ext':
-					if ($node->nodeName === 'div' && !self::isFirstChild($node) ) {
-						if ( self::getEmptyLinesBefore($node) == 0 ) { $out = "{$out}"; }
-						else if ( self::getEmptyLinesBefore($node) == 1 ) { $out = "\n{$out}"; }
-						else {
-							$out = "{$out}";
-						}
+					if ($node->nodeName === 'div' && !self::isFirstChild($node)) {
+						$out = "\n{$out}";
 					}
 					break;
 				case 'comment':
