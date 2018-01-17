18e31e7a993ef485b27fa3d8b2b76e674d3a6ebf
grunny <mwgrunny@gmail.com>
2018-01-18 02:27:47 +1000

    (PLATFORM-3336) Handle redirecting users who have opted in to HTTPS
    Handle redirecting users who have opted in to HTTPS, and also redirect
    users who haven't or are unable to opt in back to HTTP if necessary.

--- extensions/wikia/HTTPSOptIn/HTTPSOptIn.setup.php
+++ extensions/wikia/HTTPSOptIn/HTTPSOptIn.setup.php
@@ -5,3 +5,4 @@ $wgAutoloadClasses['HTTPSOptInHooks'] =  __DIR__ . '/HTTPSOptInHooks.class.php';
 $wgExtensionMessagesFiles['HTTPSOptIn'] = __DIR__ . '/HTTPSOptIn.i18n.php' ;
 
 $wgHooks['GetPreferences'][] = 'HTTPSOptInHooks::onGetPreferences';
+$wgHooks['BeforeInitialize'][] = 'HTTPSOptInHooks::onBeforeInitialize';
--- extensions/wikia/HTTPSOptIn/HTTPSOptInHooks.class.php
+++ extensions/wikia/HTTPSOptIn/HTTPSOptInHooks.class.php
@@ -11,4 +11,32 @@ class HTTPSOptInHooks {
 		}
 		return true;
 	}
+
+	/**
+	 * Handle redirecting users who have opted in to HTTPS, and those
+	 * who haven't back to HTTP if necessary.
+	 *
+	 * @param  Title      $title
+	 * @param             $unused
+	 * @param  OutputPage $output
+	 * @param  User       $user
+	 * @param  WebRequest $request
+	 * @param  MediaWiki  $mediawiki
+	 * @return bool
+	 */
+	public static function onBeforeInitialize( Title $title, $unused, OutputPage $output,
+		User $user, WebRequest $request, MediaWiki $mediawiki
+	): bool {
+		if ( WebRequest::detectProtocol() == 'http' &&
+			$user->isAllowed( 'https-opt-in' ) &&
+			$user->getGlobalPreference( 'https-opt-in', false )
+		) {
+			$output->redirect( preg_replace( '/^http:\/\//', 'https://', $request->getFullRequestURL() ) );
+		} elseif ( WebRequest::detectProtocol() == 'https' &&
+			( !$user->isAllowed( 'https-opt-in' ) || !$user->getGlobalPreference( 'https-opt-in', false ) )
+		) {
+			$output->redirect( preg_replace( '/^https:\/\//', 'http://', $request->getFullRequestURL() ) );
+		}
+		return true;
+	}
 }
