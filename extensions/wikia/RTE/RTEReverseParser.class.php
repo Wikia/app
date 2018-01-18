e7450215bf355fcbf5bc8afd73a6696155237135
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-01-18 11:31:58 +0100

    XW-4523: add newline between block template paragraph when switching to source

--- extensions/wikia/RTE/RTEReverseParser.class.php
+++ extensions/wikia/RTE/RTEReverseParser.class.php
@@ -414,19 +414,21 @@ class RTEReverseParser {
 		$out = $data['wikitext'];
 
 		// extra fixes for different types of placeholders
-		if (isset($data['type'])) {
-			switch($data['type']) {
+		if ( isset( $data['type'] ) ) {
+			switch ( $data['type'] ) {
 				// templates and extension tags which are wrapped in <div> needs to have additional newline at the end
 				case 'double-brackets':
 				case 'ext':
-					if ($node->nodeName === 'div' && !self::isFirstChild($node)) {
+					if ( $node->nodeName === 'div' &&
+						!self::isFirstChild( $node ) &&
+						!self::previousSiblingIs( $node, [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ] ) ) {
 						$out = "\n{$out}";
 					}
 					break;
 				case 'comment':
 					// FIXME: dirty fix for RT #83859
 					// assuming here that comments in wysiyg mode must be placed at the beginning of new line
-					if (self::isChildOf($node->parentNode, 'div')) {
+					if ( self::isChildOf( $node->parentNode, 'div' ) ) {
 						$out = "\n{$out}";
 					}
 					break;
@@ -738,6 +740,10 @@ class RTEReverseParser {
 		}
 		else {
 			$out = "{$textContent}\n";
+		}
+
+		if ( self::previousSiblingIs( $node, 'div' ) ) {
+			$out = "\n{$out}";
 		}
 
 		// if next paragraph has been pasted into CK add extra newline
