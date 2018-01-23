617e3808f233ab850dd3aad7d4acfaa43796ae27
rogatty <igor@wikia-inc.com>
2018-01-23 12:35:39 +0100

    IRIS-5482 | re-add SpecialDiscussionsLog i18n

---
+++ extensions/wikia/SpecialDiscussionsLog/SpecialDiscussionsLog.i18n.php
@@ -0,0 +1,67 @@
+<?php
+$messages = array();
+
+$messages['qqq'] = array(
+	'discussionslog-contributions-link-title' => 'Link to Discussions Log, visible on Special:Contributions',
+	'right-specialdiscussionslog' => 'Description of user right, visible on Special:ListGroupRights',
+);
+
+$messages['en'] = array(
+	'discussionslog-contributions-link-title' => 'Discussion IP history',
+	'right-specialdiscussionslog' => 'View [[Special:DiscussionsLog]]',
+);
+
+$messages['de'] = array(
+	'discussionslog-contributions-link-title' => 'Diskussions-IP-Verlauf',
+	'right-specialdiscussionslog' => 'Das [[Special:DiscussionsLog|Diskussionsprotokoll]] einsehen',
+);
+
+$messages['es'] = array(
+	'discussionslog-contributions-link-title' => 'Historial de IP en discusiones',
+	'right-specialdiscussionslog' => 'Ver [[Special:DiscussionsLog]]',
+);
+
+$messages['fr'] = array(
+	'discussionslog-contributions-link-title' => 'Historique des IP de discussion',
+	'right-specialdiscussionslog' => 'Voir [[Special:DiscussionsLog]]',
+);
+
+$messages['it'] = array(
+	'discussionslog-contributions-link-title' => "Cronologia discussioni dell'IP",
+	'right-specialdiscussionslog' => 'Visualizza [[Special:DiscussionsLog]]',
+);
+
+$messages['ja'] = array(
+	'discussionslog-contributions-link-title' => 'ディスカッションのIP履歴',
+	'right-specialdiscussionslog' => '[[特別:DiscussionsLog|ログ]]を表示',
+);
+
+$messages['nl'] = array(
+	'discussionslog-contributions-link-title' => 'IP-geschiedenis discussie',
+	'right-specialdiscussionslog' => '[[Speciaal:Discussielogboeken]] bekijken',
+);
+
+$messages['pl'] = array(
+	'discussionslog-contributions-link-title' => 'Historia IP dyskusji',
+	'right-specialdiscussionslog' => 'Zobacz [[Specjalna:Rejestr dyskusji]]',
+);
+
+$messages['pt'] = array(
+	'discussionslog-contributions-link-title' => 'Histórico do IP de discussão',
+	'right-specialdiscussionslog' => 'Ver [[Especial:DiscussionsLog]]',
+);
+
+$messages['ru'] = array(
+	'discussionslog-contributions-link-title' => 'История обсуждений с IP-адреса',
+	'right-specialdiscussionslog' => 'Просмотр [[Служебная:DiscussionsLog]]',
+);
+
+$messages['zh-hans'] = array(
+	'discussionslog-contributions-link-title' => '讨论区IP历史',
+	'right-specialdiscussionslog' => '查看[[Special:DiscussionsLog]]',
+);
+
+$messages['zh-hant'] = array(
+	'discussionslog-contributions-link-title' => '討論區IP歷史',
+	'right-specialdiscussionslog' => '查看[[Special:DiscussionsLog]]',
+);
