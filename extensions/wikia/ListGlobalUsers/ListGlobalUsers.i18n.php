0f138f31a6ae9fdd42308ca753fa4a352bc61e99
TK-999 <tk.999.wikia@gmail.com>
2018-01-24 16:46:13 +0000

    SUS-3455 Download translations

--- extensions/wikia/ListGlobalUsers/ListGlobalUsers.i18n.php
+++ extensions/wikia/ListGlobalUsers/ListGlobalUsers.i18n.php
@@ -1,5 +1,4 @@
 <?php
-
 $messages = array();
 
 $messages['en'] = array(
@@ -17,3 +16,92 @@ $messages['qqq'] = array(
 	'listglobalusers-deselect-all' => 'Text for toggle to deselect all user group checkboxes',
 	'listglobalusers-submit' => 'Label for submit button on Special:ListGlobalUsers search form',
 );
+
+$messages['de'] = array(
+	'listglobalusers' => 'Benutzer mit globalen Gruppen anzeigen',
+	'listglobalusers-select-all' => 'Alle auswählen',
+	'listglobalusers-legend' => 'Benutzer anzeigen, die Mitglieder einer oder mehrerer globaler Gruppen sind',
+	'listglobalusers-submit' => 'Liste aktualisieren',
+	'listglobalusers-deselect-all' => 'Auswahl löschen',
+);
+
+$messages['es'] = array(
+	'listglobalusers' => 'Lista de usuarios globales',
+	'listglobalusers-select-all' => 'Selecciona todo',
+	'listglobalusers-legend' => 'Lista a los usuarios que son miembros de uno o más grupos globales',
+	'listglobalusers-submit' => 'Enviar',
+	'listglobalusers-deselect-all' => 'Deselecciona',
+);
+
+$messages['fr'] = array(
+	'listglobalusers' => 'Liste des utilisateurs globaux',
+	'listglobalusers-select-all' => 'Tout sélectionner',
+	'listglobalusers-legend' => "Indiquez les utilisateurs membres d'un ou de plusieurs groupes globaux.",
+	'listglobalusers-submit' => 'Soumettre',
+	'listglobalusers-deselect-all' => 'Désélectionner tout',
+);
+
+$messages['it'] = array(
+	'listglobalusers' => 'Lista degli utenti globali',
+	'listglobalusers-select-all' => 'Seleziona tutto',
+	'listglobalusers-legend' => 'Elenca gli utenti che fanno parte di uno o più gruppi globali.',
+	'listglobalusers-submit' => 'Invia',
+	'listglobalusers-deselect-all' => 'Deseleziona tutto',
+);
+
+$messages['ja'] = array(
+	'listglobalusers' => 'グローバル・ユーザーのリスト',
+	'listglobalusers-select-all' => 'すべて選択',
+	'listglobalusers-legend' => '1つ、または複数のグローバル・グループのメンバーであるユーザーをリストアップ',
+	'listglobalusers-submit' => '表示',
+	'listglobalusers-deselect-all' => 'すべて解除',
+);
+
+$messages['lol'] = array(
+	'listglobalusers' => 'crwdns75636:0crwdne75636:0',
+	'listglobalusers-select-all' => 'crwdns75642:0crwdne75642:0',
+	'listglobalusers-legend' => 'crwdns75637:0crwdne75637:0',
+	'listglobalusers-submit' => 'crwdns75871:0crwdne75871:0',
+	'listglobalusers-deselect-all' => 'crwdns75643:0crwdne75643:0',
+);
+
+$messages['pl'] = array(
+	'listglobalusers' => 'Pokaż listę użytkowników globalnych',
+	'listglobalusers-select-all' => 'Wybierz wszystko',
+	'listglobalusers-legend' => 'Pokaż listę użytkowników, którzy są członkami przynajmniej jednej grupy globalnej',
+	'listglobalusers-submit' => 'Prześlij',
+	'listglobalusers-deselect-all' => 'Odznacz wszystkie',
+);
+
+$messages['pt'] = array(
+	'listglobalusers' => 'Listar usuários globais',
+	'listglobalusers-select-all' => 'Selecionar tudo',
+	'listglobalusers-legend' => 'Listar usuários que são membros de um ou mais grupos globais',
+	'listglobalusers-submit' => 'Enviar',
+	'listglobalusers-deselect-all' => 'Deselect all',
+);
+
+$messages['ru'] = array(
+	'listglobalusers' => 'Список участников с глобальными статусами',
+	'listglobalusers-select-all' => 'Выбрать всё',
+	'listglobalusers-legend' => 'Это список участников, у которых есть один или несколько глобальных статусов',
+	'listglobalusers-submit' => 'Показать',
+	'listglobalusers-deselect-all' => 'Снять выделение',
+);
+
+$messages['zh-hans'] = array(
+	'listglobalusers' => '全站用户列表',
+	'listglobalusers-select-all' => '全选',
+	'listglobalusers-legend' => '选择属于一个或多个全站群组的用户',
+	'listglobalusers-submit' => '提交',
+	'listglobalusers-deselect-all' => '取消全选',
+);
+
+$messages['zh-hant'] = array(
+	'listglobalusers' => '全站用戶列表',
+	'listglobalusers-select-all' => '全選',
+	'listglobalusers-legend' => '列出屬於一個或多個全站群組的用戶',
+	'listglobalusers-submit' => '提交',
+	'listglobalusers-deselect-all' => '取消全部',
+);
+
