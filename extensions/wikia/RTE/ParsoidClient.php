16c255b05f6dd770cd7a70cd9bda4b59fdd2bf91
TK-999 <tk.999.wikia@gmail.com>
2018-01-25 13:01:47 +0100

    base url

--- extensions/wikia/RTE/ParsoidClient.php
+++ extensions/wikia/RTE/ParsoidClient.php
@@ -13,10 +13,9 @@ class ParsoidClient {
 	}
 
 	public function getPageHtml( string $pageTitle, int $revisionId ) {
-		$baseUrl = $this->urlProvider->getUrl( 'parsoid-redux' );
-		$apiUrl = urlencode(wfExpandUrl( wfScript( 'api' ) ));
+		$baseUrl = $this->getParsoidUrl();
 
-		$fullUrl = "http://$baseUrl/$apiUrl/v3/page/html/$pageTitle/$revisionId";
+		$fullUrl = "$baseUrl/v3/page/html/$pageTitle/$revisionId";
 
 		$ret =  Http::get( $fullUrl, 'default', static::DEFAULT_REQUEST_OPTIONS );
 
@@ -24,19 +23,17 @@ class ParsoidClient {
 	}
 
 	public function html2wt( string $html, string $pageTitle, int $revisionId ) {
-		$baseUrl = $this->urlProvider->getUrl( 'parsoid-redux' );
-		$apiUrl = wfExpandUrl( wfScript( 'api' ) );
+		$baseUrl = $this->getParsoidUrl();
 
-		$fullUrl = "$baseUrl/$apiUrl/v3/transform/html/to/wikitext/$pageTitle/$revisionId";
+		$fullUrl = "$baseUrl/v3/transform/html/to/wikitext/$pageTitle/$revisionId";
 
 		return Http::post( $fullUrl, [ 'html' => $html ] );
 	}
 
 	public function wt2html( string $wikitext) {
-		$baseUrl = $this->urlProvider->getUrl( 'parsoid-redux' );
-		$apiUrl = wfExpandUrl( wfScript( 'api' ) );
+		$baseUrl = $this->getParsoidUrl();
 
-		$fullUrl = "http://$baseUrl/$apiUrl/v3/transform/wikitext/to/html";
+		$fullUrl = "$baseUrl/v3/transform/wikitext/to/html";
 
 		$requestOptions = [
 			'postData' => [
@@ -46,5 +43,12 @@ class ParsoidClient {
 		];
 
 		return Http::post( $fullUrl, array_merge( static::DEFAULT_REQUEST_OPTIONS, $requestOptions ) );
+	}
+
+	private function getParsoidUrl(): string {
+		$baseUrl = $this->urlProvider->getUrl( 'parsoid-redux' );
+		$apiUrl = urlencode( wfExpandUrl( wfScript( 'api' ) ) );
+
+		return "http://$baseUrl/$apiUrl";
 	}
 }
