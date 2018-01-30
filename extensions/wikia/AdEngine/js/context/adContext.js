2a35e49630918a0839c27f13c3ca4eb77ae9d768
Maciej Leśniewski <mlesniewski@wikia-inc.com>
2018-01-30 17:01:30 +0100

    ADEN-6493 Updates ad-engine

--- extensions/wikia/AdEngine/js/context/adContext.js
+++ extensions/wikia/AdEngine/js/context/adContext.js
@@ -237,7 +237,7 @@ define('ext.wikia.adEngine.adContext', [
 		return nextElement;
 	}
 
-	setContext(w.ads ? w.ads.context : {});
+	setContext((w.ads && w.ads.context) ? w.ads.context : {});
 
 	return {
 		get: get,
