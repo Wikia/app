cf3a0a20f3b59fe35b19a2666dfe4fd0029c39f9
Kacper Olek <kolek@fandom.com>
2018-02-01 12:34:18 +0100

    fixed creation of double cover overlay

--- extensions/wikia/RTE/ckeditor/plugins/dialog/plugin.js
+++ extensions/wikia/RTE/ckeditor/plugins/dialog/plugin.js
@@ -3185,8 +3185,10 @@ CKEDITOR.DIALOG_STATE_BUSY = 2;
 		openDialog: function( dialogName, callback ) {
 			var dialog = null, dialogDefinitions = CKEDITOR.dialog._.dialogDefinitions[ dialogName ];
 
-			if ( CKEDITOR.dialog._.currentTop === null )
-				showCover( this );
+			//Wikia start
+			//if ( CKEDITOR.dialog._.currentTop === null )
+				//showCover( this );
+			//Wikia end
 
 			// If the dialogDefinition is already loaded, open it immediately.
 			if ( typeof dialogDefinitions == 'function' ) {
