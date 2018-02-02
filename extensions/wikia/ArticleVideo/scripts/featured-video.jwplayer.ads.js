f86ad0d79dc57008599b71bec30e881bcae8b387
Szymon Tomaszewski <stomaszewski@wikia-inc.com>
2018-02-02 20:29:09 +0100

    ADEN-6531 Disable FV ads when slot is disabled

--- extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.ads.js
+++ extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.ads.js
@@ -3,74 +3,19 @@ define('wikia.articleVideo.featuredVideo.ads', [
 	'ext.wikia.adEngine.video.vastUrlBuilder',
 	'ext.wikia.adEngine.slot.service.megaAdUnitBuilder',
 	'ext.wikia.adEngine.slot.service.srcProvider',
+	'ext.wikia.adEngine.video.articleVideoAd',
 	'ext.wikia.adEngine.video.player.jwplayer.adsTracking',
 	'ext.wikia.adEngine.video.vastDebugger',
 	'wikia.log'
-], function (adContext, vastUrlBuilder, megaAdUnitBuilder, srcProvider, adsTracking, vastDebugger, log) {
+], function (adContext, vastUrlBuilder, megaAdUnitBuilder, srcProvider, articleVideoAd, adsTracking, vastDebugger, log) {
 	'use strict';
 
-	var aspectRatio = 640 / 480,
-		baseSrc = adContext.get('targeting.skin') === 'oasis' ? 'gpt' : 'mobile',
-		featuredVideoPassback = 'jwplayer',
+	var baseSrc = adContext.get('targeting.skin') === 'oasis' ? 'gpt' : 'mobile',
 		featuredVideoSlotName = 'FEATURED',
 		featuredVideoSource,
 		logGroup = 'wikia.articleVideo.featuredVideo.ads';
 
-	function calculateRV(depth) {
-		var capping = adContext.get('opts.fvAdsFrequency');
-
-		return (depth < 2 || !capping) ? 1 : (Math.floor((depth - 1) / capping) + 1);
-	}
-
-	function shouldPlayAdOnNextVideo(depth) {
-		var capping = adContext.get('opts.fvAdsFrequency');
-
-		return adContext.get('opts.replayAdsForFV') && capping > 0 && (depth - 1) % capping === 0;
-	}
-
-	function canAdBePlayed(depth) {
-		var isReplay = depth > 1;
-
-		return !isReplay || (isReplay && shouldPlayAdOnNextVideo(depth));
-	}
-
-	function shouldPlayPreroll(videoDepth) {
-		return canAdBePlayed(videoDepth);
-	}
-
-	function shouldPlayMidroll(videoDepth) {
-		return adContext.get('opts.isFVMidrollEnabled') && canAdBePlayed(videoDepth);
-	}
-
-	function shouldPlayPostroll(videoDepth) {
-		return adContext.get('opts.isFVPostrollEnabled') && canAdBePlayed(videoDepth);
-	}
-
-	function buildVastUrl(position, videoDepth, correlator, bidParams) {
-		var options = {
-				correlator: correlator,
-				vpos: position
-			},
-			slotParams = {
-				passback: featuredVideoPassback,
-				pos: featuredVideoSlotName,
-				rv: calculateRV(videoDepth),
-				src: featuredVideoSource
-			};
-
-		if (videoDepth === 1 && bidParams) {
-			Object.keys(bidParams).forEach(function (key) {
-				slotParams[key] = bidParams[key];
-			});
-		}
-		options.adUnit = megaAdUnitBuilder.build(slotParams.pos, slotParams.src);
-
-		log(['buildVastUrl', position, videoDepth, slotParams, options], log.levels.debug, logGroup);
-
-		return vastUrlBuilder.build(aspectRatio, slotParams, options);
-	}
-
-	return function(player, bidParams) {
+	return function (player, bidParams) {
 		var correlator,
 			featuredVideoElement = player && player.getContainer && player.getContainer(),
 			featuredVideoContainer = featuredVideoElement && featuredVideoElement.parentNode,
@@ -99,36 +44,38 @@ define('wikia.articleVideo.featuredVideo.ads', [
 				trackingParams.adProduct = 'featured-video';
 				videoDepth += 1;
 
-				if (shouldPlayPreroll(videoDepth)) {
+				if (articleVideoAd.shouldPlayPreroll(videoDepth)) {
 					trackingParams.adProduct = 'featured-video-preroll';
-					player.playAd(buildVastUrl('preroll', videoDepth, correlator, bidParams));
+					player.playAd(articleVideoAd.buildVastUrl('preroll', videoDepth, correlator, bidParams));
 				}
 				prerollPositionReached = true;
 			});
 
 			player.on('videoMidPoint', function () {
 				log('Midroll position reached', log.levels.info, logGroup);
-				if (shouldPlayMidroll(videoDepth)) {
+				if (articleVideoAd.shouldPlayMidroll(videoDepth)) {
 					trackingParams.adProduct = 'featured-video-midroll';
-					player.playAd(buildVastUrl('midroll', videoDepth, correlator));
+					player.playAd(articleVideoAd.buildVastUrl('midroll', videoDepth, correlator));
 				}
 
 			});
 
 			player.on('beforeComplete', function () {
 				log('Postroll position reached', log.levels.info, logGroup);
-				if (shouldPlayPostroll(videoDepth)) {
+				if (articleVideoAd.shouldPlayPostroll(videoDepth)) {
 					trackingParams.adProduct = 'featured-video-postroll';
-					player.playAd(buildVastUrl('postroll', videoDepth, correlator));
+					player.playAd(articleVideoAd.buildVastUrl('postroll', videoDepth, correlator));
 				}
 			});
 
 			player.on('complete', function () {
 				prerollPositionReached = false;
 			});
+
 			player.on('adRequest', function (event) {
 				vastDebugger.setVastAttributes(featuredVideoContainer, event.tag, 'success', event.ima && event.ima.ad);
 			});
+
 			player.on('adError', function (event) {
 				vastDebugger.setVastAttributes(featuredVideoContainer, event.tag, 'error', event.ima && event.ima.ad);
 			});
