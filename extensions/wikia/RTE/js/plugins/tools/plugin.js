cbaed4f284352426d638d6448e277122d5ac2f81
Kacper Olek <kolek@fandom.com>
2018-02-02 14:41:58 +0000

    Removed old 'fix' for safari

--- extensions/wikia/RTE/js/plugins/tools/plugin.js
+++ extensions/wikia/RTE/js/plugins/tools/plugin.js
@@ -159,15 +159,8 @@ window.RTE.tools = {
 	getEditorScrollOffsets: function() {
 		var scrollLeft, scrollTop;
 
-		if (CKEDITOR.env.webkit) {
-			// RT #46408: use different property for Safari to get scroll offset
-			scrollLeft = RTE.getInstance().document.$.body.scrollLeft;
-			scrollTop = RTE.getInstance().document.$.body.scrollTop;
-		}
-		else {
-			scrollLeft = RTE.getInstance().document.$.documentElement.scrollLeft;
-			scrollTop = RTE.getInstance().document.$.documentElement.scrollTop;
-		}
+		scrollLeft = RTE.getInstance().document.$.documentElement.scrollLeft;
+		scrollTop = RTE.getInstance().document.$.documentElement.scrollTop;
 
 		return {
 			left: scrollLeft,
