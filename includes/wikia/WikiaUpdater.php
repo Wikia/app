6c2d3c05ea652295a7094eea2d8f110b4730d4bb
macbre <maciej.brencz@gmail.com>
2018-02-05 10:05:47 +0100

    WikiaUpdater: fix a broken merge

--- includes/wikia/WikiaUpdater.php
+++ includes/wikia/WikiaUpdater.php
@@ -86,11 +86,9 @@ class WikiaUpdater {
 			$wikia_update[] = array( 'addTable', 'city_list', $dir . 'wf/patch-create-city_list.sql', true );
 			$wikia_update[] = array( 'addTable', 'city_list', $dir . 'wf/patch-create-city_cats.sql', true );
 		} else {
-			$wikia_update[] = array( 'WikiaUpdater::do_drop_table', 'spoofuser' ); // SUS-3590
-		}
-		else {
 			// run these updates on per-wiki databases only
 			$wikia_update[] = array( 'WikiaUpdater::do_drop_table', 'ach_ranking_snapshots' ); // SUS-3592
+			$wikia_update[] = array( 'WikiaUpdater::do_drop_table', 'spoofuser' ); // SUS-3590
 		}
 
 		foreach ( $wikia_update as $update ) {
