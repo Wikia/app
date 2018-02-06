4bccf5d1aa9f796709f2ec69a4b914b89ad6da4c
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-02-06 12:23:35 +0100

    XW-4609: fix regexp

--- extensions/wikia/RTE/RTEParser.class.php
+++ extensions/wikia/RTE/RTEParser.class.php
@@ -35,7 +35,7 @@ class RTEParser extends Parser {
 	function doBlockLevels( $text, $linestart ) {
 		// XW-4380: Make template placeholders in list items render correctly
 		// XW-4609: remove newlines from template's placeholder when used inside list's item to not break list
-		$text = preg_replace_callback('/([\*#;:]<div class="placeholder placeholder-double-brackets"[^>]+>&#x0200B;)\n?(.*?)(&#x0200B;<\/div>)/s', function($matches) {
+		$text = preg_replace_callback('/^([\*#;:]<div class="placeholder placeholder-double-brackets"[^>]+>&#x0200B;)\n?(.*?)(&#x0200B;<\/div>)/ms', function($matches) {
 			return preg_replace('/\\n/', '', $matches[0]);
 		}, str_replace("\r\n", "\n", $text));
 		$text = preg_replace( '/^([\*#;:]<span class="placeholder placeholder-double-brackets"[^>]+>\&#x0200B;)\n+/m', '$1', $text );
