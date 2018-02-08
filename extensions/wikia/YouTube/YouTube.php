30ddc5e949a8a5d3e8d92115653cfd8a429884b6
mech <wozniak.jacek@gmail.com>
2018-02-08 15:57:27 +0100

    PLATFORM-3284: sunset discontinued Gametrailers provider

--- extensions/wikia/YouTube/YouTube.php
+++ extensions/wikia/YouTube/YouTube.php
@@ -218,7 +218,6 @@ function wfYouTube( Parser $parser ): bool {
 	$parser->setHook( 'aovideo', 'embedArchiveOrgVideo' );
 	$parser->setHook( 'aoaudio', 'embedArchiveOrgAudio' );
 	$parser->setHook( 'wegame', 'embedWeGame' );
-	$parser->setHook( 'gtrailer', 'embedGametrailers' );
 	$parser->setHook( 'nicovideo', 'embedNicovideo' );
 	$parser->setHook( 'cgamer', 'embedCrispyGamer' );
 	$parser->setHook( 'longtail', 'embedLongtailVideo' );
@@ -439,51 +438,6 @@ function embedYouTube_url2tgid( $input ) {
 	}
 
 	return array( $tid, $gid );
-}
-
-function embedYouTube_url2gtid( $url ) {
-	$id = $url;
-
-	if ( preg_match( '/^https?:\/\/www\.gametrailers\.com\/player\/(.+)\.html$/', $url, $preg ) ) {
-		$id = $preg[1];
-	} elseif ( preg_match( '/^https?:\/\/www\.gametrailers\.com\/remote_wrap\.php\?mid=(.+)$/', $url, $preg ) ) {
-		$id = $preg[1];
-	}
-
-	preg_match( '/([0-9]+)/', $id, $preg );
-	$id = $preg[1];
-
-	return $id;
-}
-
-function embedGametrailers( $input, $argv, $parser ) {
-	$gtid   = '';
-	$width  = $width_max  = 480;
-	$height = $height_max = 392;
-
-	if ( !empty( $argv['gtid'] ) ) {
-		$gtid = embedYouTube_url2gtid( $argv['gtid'] );
-	} elseif ( !empty( $input ) ) {
-		$gtid = embedYouTube_url2gtid( $input );
-	}
-	if ( !empty( $argv['width'] ) && settype( $argv['width'], 'integer' ) && ( $width_max >= $argv['width'] ) ) {
-		$width = $argv['width'];
-	}
-	if ( !empty( $argv['height'] ) && settype( $argv['height'], 'integer' ) && ( $height_max >= $argv['height'] ) ) {
-		$height = $argv['height'];
-	}
-
-	if ( !empty( $gtid ) ) {
-		WikiaLogger::instance()->info( 'Embedding gtrailer: ' . $gtid, [
-			'method' => __METHOD__,
-			'video_source' => 'gtrailer'
-		] );
-		// www.gametrailers.com does not yet support HTTPS, to handle in (PLATFORM-3284)
-		$url = "http://www.gametrailers.com/remote_wrap.php?mid={$gtid}";
-		// return "<object type=\"application/x-shockwave-flash\" width=\"{$width}\" height=\"{$height}\"><param name=\"movie\" value=\"{$url}\"/></object>";
-		// gametrailers' flash doesn't work on FF with object tag alone )-: weird, yt and gvideo are ok )-: valid xhtml no more )-:
-		return "<object classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\"  codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" id=\"gtembed\" width=\"{$width}\" height=\"{$height}\">	<param name=\"allowScriptAccess\" value=\"sameDomain\" /> 	<param name=\"allowFullScreen\" value=\"true\" /> <param name=\"movie\" value=\"{$url}\"/> <param name=\"quality\" value=\"high\" /> <embed src=\"{$url}\" swLiveConnect=\"true\" name=\"gtembed\" align=\"middle\" allowScriptAccess=\"sameDomain\" allowFullScreen=\"true\" quality=\"high\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\" type=\"application/x-shockwave-flash\" width=\"{$width}\" height=\"{$height}\"></embed> </object>";
-	}
 }
 
 function embedYouTube_url2nvid( $url ) {
