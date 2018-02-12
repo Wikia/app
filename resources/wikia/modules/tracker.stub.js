8df2ace29e6f2b56d9bda1755a3ab50b8c5c7111
Jakub Olek <bukaj.kelo+github@gmail.com>
2018-02-12 10:23:11 +0100

    XW-4155 | comment why window.$ has to stay

--- resources/wikia/modules/tracker.stub.js
+++ resources/wikia/modules/tracker.stub.js
@@ -237,6 +237,9 @@ function veTrack( data ) {
 			defaultData.whichVE = 'unknown';
 		}
 
+		// This file is loaded both in mobile-wiki and oasis
+		// On oasis we do have $ and we need to support IE11
+		// On mobile-wiki we do not have jquery and we don't have to support IE11 so we can safely use Object.assign
 		if (window.$) {
 			finalData = $.extend( {}, defaultData, data );
 		} else {
