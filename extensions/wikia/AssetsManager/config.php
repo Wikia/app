9c24596f0c38f627a0c6b1bd608ce79a245b8cb6
TK-999 <tk.999.wikia@gmail.com>
2018-02-13 17:34:21 +0100

    SUS-3867 | Cleanup after old spotlights stuff

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -208,15 +208,6 @@ $config['recirculation_scss'] = [
 	],
 ];
 
-$config['spotlights_js'] = [
-	'type' => AssetsManager::TYPE_JS,
-	'assets' => [
-		// ads
-		'//extensions/wikia/Spotlights/js/AdProviderOpenX.js',
-		'//extensions/wikia/Spotlights/js/LazyLoadAds.js',
-	],
-];
-
 
 $config['adengine2_top_js'] = [
 	'type' => AssetsManager::TYPE_JS,
--- includes/wikia/DefaultSettings.php
+++ includes/wikia/DefaultSettings.php
@@ -1855,19 +1855,6 @@ $wgEnableHostnameInHtmlTitle = true;
 include_once("$IP/includes/wikia/parser/templatetypes/TemplateTypes.setup.php");
 
 /**
- * @name $wgEnableReviveSpotlights
- * Enables Revive Spotlights
- */
-$wgEnableReviveSpotlights = true;
-
-/**
- * @name $wgReviveSpotlightsCountries
- * Enables Revive Spotlights in these countries (given wgEnableReviveSpotlights is also true).
- * ONLY UPDATE THROUGH WIKI FACTORY ON COMMUNITY - it's an instant global.
- */
-$wgReviveSpotlightsCountries = null;
-
-/**
  * @name $wgDisableImprovedGenderSupport
  *
  * Allow to disable "improved" gender support included in MW 1.18
