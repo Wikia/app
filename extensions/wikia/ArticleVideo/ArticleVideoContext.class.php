f28a73b2e55a2104bab365bd2945abcd5ce29c81
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-02-15 11:50:45 +0100

    XW-4631 | Logging article-video and JW API errors

--- extensions/wikia/ArticleVideo/ArticleVideoContext.class.php
+++ extensions/wikia/ArticleVideo/ArticleVideoContext.class.php
@@ -2,6 +2,9 @@
 
 class ArticleVideoContext {
 
+	const ARTICLE_VIDEO_ERROR_MESSAGE = 'JWPlayer: Could not find mediaId in article-video service';
+	const JWPLAYER_API_ERROR_MESSAGE = 'JWPlayer: Could not find enough playback info in JW API to play the video';
+
 	/**
 	 * Checks if featured video is embedded on given article
 	 *
@@ -36,6 +39,11 @@ class ArticleVideoContext {
 		if ( self::isFeaturedVideoEmbedded( $pageId ) ) {
 			$videoData = [];
 			$videoData['mediaId'] = ArticleVideoService::getFeatureVideoForArticle( $wg->cityId, $pageId );
+			$logger = Wikia\Logger\WikiaLogger::instance();
+
+			if ( empty( $videoData['mediaId'] ) ) {
+				$logger->error( self::ARTICLE_VIDEO_ERROR_MESSAGE );
+			}
 
 			$details = json_decode(
 				Http::get(
@@ -45,7 +53,11 @@ class ArticleVideoContext {
 				true
 			);
 
-			if ( !empty( $details ) && !empty( $videoData['playlist'] ) && empty( $videoData['playlist'][0] ) ) {
+			if ( empty( $details ) || empty( $details['playlist'] ) || empty( $details['playlist'][0] ) ) {
+				$logger->error( self::JWPLAYER_API_ERROR_MESSAGE );
+
+				return [];
+			} else {
 				$videoData = array_merge( $videoData, $details );
 
 				$videoData['duration'] = WikiaFileHelper::formatDuration( $details['playlist'][0]['duration'] );
@@ -57,8 +69,6 @@ class ArticleVideoContext {
 
 				return $videoData;
 			}
-
-			return [];
 		}
 
 		return [];
