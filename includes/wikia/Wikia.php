ac4ea6e52256efd801bea70bbe91a802915b7408
mech <wozniak.jacek@gmail.com>
2018-02-15 13:17:19 +0100

    PLATFORM-3369: export wgServicesExternalDomain to JS

--- includes/wikia/Wikia.php
+++ includes/wikia/Wikia.php
@@ -1868,7 +1868,7 @@ class Wikia {
 	}
 
 	public static function onWikiaSkinTopScripts( &$vars, &$scripts, Skin $skin ) {
-		global $wgWikiDirectedAtChildrenByFounder;
+		global $wgWikiDirectedAtChildrenByFounder, $wgServicesExternalDomain;
 
 		if ( !empty( $wgWikiDirectedAtChildrenByFounder ) ) {
 			$vars['wgWikiDirectedAtChildrenByFounder'] = $wgWikiDirectedAtChildrenByFounder;
@@ -1877,6 +1877,8 @@ class Wikia {
 		if ( self::isUsingSafeJs() ) {
 			$vars['wgUseSiteJs'] = true;
 		}
+
+		vars['wgServicesExternalDomain'] = $wgServicesExternalDomain;
 
 		return true;
 	}
