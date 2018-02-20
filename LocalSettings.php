4e45a46a9312ab5f5a373088bc9fe1ce1ff364f3
Michał “Mix” Roszka <mix@mixoteric.net>
2018-02-20 17:47:38 +0100

    move code from config/LocalSettings.php
    This is so that CommonExtensions import is the last statement
    in config/LocalSettings.php.

--- LocalSettings.php
+++ LocalSettings.php
@@ -2,3 +2,19 @@
 
 $IP = __DIR__;
 require __DIR__.'/../config/LocalSettings.php';
+
+/* @var $wgDBcluster string */
+
+// in some cases $wgMemc is still null at this point, let's initialize it (SUS-2699)
+$wgMemc = wfGetMainCache();
+
+// disabled wikis do not have $wgDBcluster set at this point, skip the following check
+// to allow update.php and other maintenance scripts to process such wikis (SUS-3589)
+if ( is_string( $wgDBcluster ) && WikiFactoryLoader::checkPerClusterReadOnlyFlag( $wgDBcluster ) ) {
+	$wgReadOnly = WikiFactoryLoader::PER_CLUSTER_READ_ONLY_MODE_REASON;
+}
+
+# this has to be fired after extensions - because any extension may add some new permissions (initialized with their default values)
+if ( !empty( $wgGroupPermissionsLocal ) ) {
+	WikiFactoryLoader::LocalToGlobalPermissions( $wgGroupPermissionsLocal );
+}
