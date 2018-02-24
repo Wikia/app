bdd6446dbcc5764868d9311025d7603f748ee2ef
Michał “Mix” Roszka <mix@mixoteric.net>
2018-02-24 14:07:26 +0100

    minor: clean config from non-config stuff

--- LocalSettings.php
+++ LocalSettings.php
@@ -2,8 +2,37 @@
 // IP stands for Installation Path, the top folder of MediaWiki codebase.
 $IP = __DIR__;
 
+// Register some $IP subdirectories in include_path to speed up imports with
+// relative paths.
+ini_set( 'include_path', "$IP:$IP/includes:$IP/languages:$IP/lib/vendor:.:" );
+
 // FANDOM-specific constants.
 require "$IP/includes/wikia/Defines.php";
+
+/**
+ * @global string $wgWikiaLocalSettingsPath
+ * This is used by some maintenance scripts that execute other maintenance
+ * scripts and need to point them to the right LocalSettings.php.
+ * @deprecated Maintenance scripts must not require --conf
+ */
+$wgWikiaLocalSettingsPath = __FILE__;
+// CONFIG_REVISION: retire $wgWikiaLocalSettingsPath completely
+
+/**
+ * @global string $wgWikiaDatacenter
+ * Runtime datacenter discovered from the environment variable.
+ * @deprecated Code portability: do not add more dc-specific code.
+ */
+$wgWikiaDatacenter = getenv( 'WIKIA_DATACENTER' );
+
+/**
+ * @global string $wgWikiaEnvironment
+ * Runtime environment discovered from the environment variable.
+ * @deprecated Code portability: do not add more environment-specific code.
+ */
+$wgWikiaEnvironment = getenv( 'WIKIA_ENVIRONMENT' );
+
+// CONFIG_REVISION: remove $wgWikiaDatacenter and $wgWikiaEnvironment from the global scope and only use it to load configuration
 
 // the rest of the old contents
 require "$IP/../config/LocalSettings.php";
