fb54eaad67026eb458da7bce315abdd3abb7e200
mech <wozniak.jacek@gmail.com>
2018-03-02 14:52:13 +0100

    PLATFORM-3383: code comments, fixes and tests

--- maintenance/wikia/HttpsMigration/migrateUserCss.php
+++ maintenance/wikia/HttpsMigration/migrateUserCss.php
@@ -1,11 +1,14 @@
 <?php
 
 /**
-* Maintenance script to migrate urls included in custom CSS files to https/protocol relative
-* @usage
-* 	# this will migrate assets for wiki with ID 119:
-*   run_maintenance --script='wikia/HttpsMigration/MigrateUserCssToHttps.php --dryRun' --id=119
-*/
+ * Maintenance script to migrate urls included in custom CSS files to https/protocol relative
+ * @usage
+ * 	# this will migrate assets for wiki with ID 119:
+ *  run_maintenance --script='wikia/HttpsMigration/MigrateUserCss.php' --id=119
+ * 	# running on some wikis in dry mode and dumping url changes to a csv file:
+ *  run_maintenance --script='wikia/HttpsMigration/MigrateUserCss.php --dryRun --file migrate_css.csv' --where='city_id < 10000'
+ *
+ */
 
 ini_set( 'display_errors', 'stderr' );
 ini_set( 'error_reporting', E_ALL ^ E_NOTICE );
@@ -20,7 +23,7 @@ use \Wikia\Logger\WikiaLogger;
 class MigrateUserCssToHttps extends Maintenance {
 
 	protected $dryRun  = false;
-	protected $fh;
+	protected $fh;	// handle to the output csv file
 
 	public function __construct() {
 		parent::__construct();
@@ -35,6 +38,9 @@ class MigrateUserCssToHttps extends Maintenance {
 		}
 	}
 
+	/**
+	 * Logs urls value change if the new value is different than the old one.
+	 */
 	public function logUrlChange( $description, $oldValue, $newValue ) {
 		global $wgCityId;
 		if ( $oldValue !== $newValue ) {
@@ -46,61 +52,84 @@ class MigrateUserCssToHttps extends Maintenance {
 		}
 	}
 
+	/**
+	 * Returns css file edit summary
+	 */
 	private function getEditSummary() {
 		return "Applying changes that should make this CSS file HTTPS-ready. If you have any questions or noticed " .
 			"issues related to this edit, please reach out to us using the [[Special:Contact]] page.";
 	}
 
-	private function isHttpLink( $url ) {
+	private function isHttpUrl( $url ) {
 		return startsWith( $url, 'http://' );
 	}
 
-	private function isHttpsLink( $url ) {
+	private function isHttpsUrl( $url ) {
 		return startsWith( $url, 'https://' );
 	}
 
-	private function isProtocolRelative( $url ) {
+	private function isProtocolRelativeUrl( $url ) {
 		return startsWith( $url, '//' );
 	}
 
-	private function isLocalLink($link) {
+	/**
+	 * True if the url doesn't contain the host part.
+	 */
+	private function isLocalUrl($link) {
 
-		if ( $this->isHttpLink( $link ) ||
-			$this->isHttpsLink( $link ) ||
-			$this->isProtocolRelative( $link ) ) {
+		if ( $this->isHttpUrl( $link ) ||
+			$this->isHttpsUrl( $link ) ||
+			$this->isProtocolRelativeUrl( $link ) ) {
 			return false;
 		}
 		return true;
 	}
 
-	private function isWikiaComLink( $url ) {
+	private function isWikiaComSubdomainUrl( $url ) {
 		$host = parse_url( $url, PHP_URL_HOST );
 		return endsWith( $host, '.wikia.com' );
 	}
 
-	private function isWikiaLink( $url ) {
+	/**
+	 * True the the url points outside of our wikia.com and vignette domain.
+	 */
+	private function isThirdPartyUrl( $url ) {
 		$host = parse_url( $url, PHP_URL_HOST );
-		return endsWith( $host, '.wikia.com' ) || endsWith( $host, '.wikia.nocookie.net' );
+		return !endsWith( $host, '.wikia.com' ) && !endsWith( $host, '.wikia.nocookie.net' );
 	}
 
+	/**
+	 * Returns true if the links points to one of many variations of our vignette domains.
+	 * Handles a lot of historical formats that are not used anymore.
+	 */
 	private function isVignetteUrl( $url ) {
 		$host = parse_url( $url, PHP_URL_HOST );
-		return (preg_match( '/^(www\.)?(vignette|images|img|static|slot)\d*\.wikia\.nocookie\.net$/', $host ) ||
+		return (preg_match( '/^(www\.)?(vignette|images|image|img|static|slot)\d*\.wikia\.nocookie\.net$/', $host ) ||
 			preg_match( '/^(www\.)?slot\d*[\.-]images\.wikia\.nocookie\.net$/', $host ) ||
 			$host === 'images.wikia.com' ||
 			$host === 'static.wikia.com');
 	}
 
+	/**
+	 * Replaces http protocl with https. Protocol-relative links are not affected.
+	 */
 	private function upgradeToHttps( $url ) {
-		if ( $this->isHttpLink( $url ) ) {
+		if ( $this->isHttpUrl( $url ) ) {
 			$url = http_build_url( $url, [ 'scheme' => 'https' ] );
 		}
 		return $url;
 	}
 
+	private function forceHttps( $url ) {
+		return http_build_url($url, ['scheme' => 'https']);
+	}
+
 	private $currentHost = null;
 
-	private function isCurrentWikiLink( $url ) {
+	/**
+	 * True if the url point to currently processed wiki.
+	 */
+	private function isCurrentWikiUrl( $url ) {
 		global $wgServer;
 		if ( !$this->currentHost ) {
 			$this->currentHost = parse_url( $wgServer, PHP_URL_HOST );
@@ -108,6 +137,9 @@ class MigrateUserCssToHttps extends Maintenance {
 		return parse_url( $url, PHP_URL_HOST ) === $this->currentHost;
 	}
 
+	/**
+	 * Removes the host part of the url leaving only the path.
+	 */
 	private function stripProtocolAndHost( $url ) {
 		$parse_url = parse_url( $url );
 		return $parse_url['path'] .
@@ -115,18 +147,26 @@ class MigrateUserCssToHttps extends Maintenance {
 			( ( isset( $parse_url['fragment'] ) ) ? '#' . $parse_url['fragment'] : '' );
 	}
 
-	private function fixWikiLink( $url ) {
-		if ( $this->isCurrentWikiLink( $url ) ) {
+	/**
+	 * Fixes wiki urls according to our guidelines:
+	 * - links to current wiki should be local, without protocol or host
+	 * - links to other wiki should be protocol-relative.
+	 */
+	private function fixWikiUrl( $url ) {
+		if ( $this->isCurrentWikiUrl( $url ) ) {
 			$url = $this->stripProtocolAndHost( $url );
 		} else {
-			if ( $this->isWikiaComLink( $url ) && $this->isHttpLink( $url ) ) {
+			if ( $this->isWikiaComSubdomainUrl( $url ) && $this->isHttpUrl( $url ) ) {
 				$url = wfProtocolUrlToRelative( $url );
 			}
 		}
 		return $url;
 	}
 
-	private function upgradeThirdPartyLink( $url ) {
+	/**
+	 * For host that we know for sure that support https, upgrade the url to use it.
+	 */
+	private function upgradeThirdPartyUrl( $url ) {
 		$known_https_hosts = [ 'en.wikipedia.org', 'i.imgur.com', 'upload.wikimedia.org', 'fonts.googleapis.com',
 			'commons.wikimedia.org'];
 		$host = parse_url( $url, PHP_URL_HOST );
@@ -140,30 +180,49 @@ class MigrateUserCssToHttps extends Maintenance {
 		return $url;
 	}
 
-	private function replaceCssLinks( $matches ) {
-		$url = $matches[ 1 ];
-		if ( $this->isLocalLink( $url ) ) {
-			//$this->output( "Skipping local link {$matches[1]}\n" );
-		} elseif ( !$this->isWikiaLink( $url ) ) {
-			if ( $this->isHttpLink( $matches[ 1 ] ) ) {
-				$url = $this->upgradeThirdPartyLink( $url );
+	/**
+	 * Try to make a single url https-ready
+	 */
+	public function fixUrl( $originalUrl ) {
+		$url = $originalUrl;
+
+		if ( $this->isLocalUrl( $url ) ) {
+			//$this->output( "Skipping local link {$url}\n" );
+		} elseif ( $this->isThirdPartyUrl( $url ) ) {
+			if ( $this->isHttpUrl( $url ) ) {
+				$url = $this->upgradeThirdPartyUrl( $url );
 			}
 		} elseif ( $this->isVignetteUrl( $url ) ) {
-			$url = $this->upgradeToHttps( http_build_url( $url, [ 'host' => 'vignette.wikia.nocookie.net' ] ) );
-			$this->logUrlChange( 'Replaced vignette url', $matches[1], $url );
+			$url = $this->forceHttps( http_build_url( $url, [ 'host' => 'vignette.wikia.nocookie.net' ] ) );
+			$this->logUrlChange( 'Replaced vignette url', $originalUrl, $url );
 		} else {
-			if ( $this->isWikiaComLink( $url ) ) {
-				$url = $this->fixWikiLink( $url );
-				$this->logUrlChange( 'Converted wiki url to protocol/host relative', $matches[1], $url );
+			if ( $this->isWikiaComSubdomainUrl( $url ) ) {
+				$url = $this->fixWikiUrl( $url );
+				$this->logUrlChange( 'Converted wiki url to protocol/host relative', $originalUrl, $url );
 			} else {
 				$this->output( "Don't know to handle {$url}\n" );
 			}
 		}
+		return $url;
+	}
+
+	/**
+	 * Tries to fix the url found in CSS code.
+	 * @param $matches regex match object, $matches[0] contain the whole matched text, $matches[1] is the url itself.
+	 * @return matched text with the url upgraded when possible.
+	 */
+	public function makeUrlHttpsComatible( $matches ) {
+		$url = $this->fixUrl( $matches[ 1 ] );
 		$matches[0] = str_replace( $matches[ 1 ], $url, $matches[0] );
 		return $matches[0];
 	}
 
-	private function updateCSSContent($text) {
+	/**
+	 * Process CSS source code upgrading urls to https/protocol-relative.
+	 * @param $text CSS source code
+	 * @return mixed Updated CSS source code
+	 */
+	public function updateCSSContent($text) {
 		$lines = explode("\n", $text);
 		// check if we covered all CSS urls with our regex
 		foreach($lines as $line) {
@@ -176,12 +235,17 @@ class MigrateUserCssToHttps extends Maintenance {
 				}
 			}
 		}
-		$text =  preg_replace_callback( '/url\s*\([\'"]?(.*?)[\'"]?\)/', [ $this, 'replaceCssLinks' ], $text );
-		$text = preg_replace_callback( '/@import\s+[\'"](.*?)[\'"]/', [ $this, 'replaceCssLinks' ], $text );
-		return preg_replace_callback( '/\(src=[\'"](.*?)[\'"]/', [ $this, 'replaceCssLinks' ], $text );
+		// To be on the safe side, only replace urls in image and import statements. This should reduce the number of
+		// changes and prevent us from changing something we didn't mean to change.
+		$text =  preg_replace_callback( '/url\s*\([\'"]?(.*?)[\'"]?\)/i', [ $this, 'makeUrlHttpsComatible' ], $text );
+		$text = preg_replace_callback( '/@import\s+[\'"](.*?)[\'"]/i', [ $this, 'makeUrlHttpsComatible' ], $text );
+		return preg_replace_callback( '/\(src=[\'"](.*?)[\'"]/i', [ $this, 'makeUrlHttpsComatible' ], $text );
 	}
 
-	private function migrateCSS( Title $title ) {
+	/**
+	 * If possible, make the CSS https-ready and save the updated content (if not running in dry mode)
+	 */
+	public function migrateCSS( Title $title ) {
 		$revId = $title->getLatestRevID();
 		$this->currentTitle = $title;
 		$revision = Revision::newFromId( $revId );
@@ -211,7 +275,7 @@ class MigrateUserCssToHttps extends Maintenance {
 	public function execute() {
 		global $wgUser, $wgCityId;
 
-		$wgUser = User::newFromName( Wikia::BOT_USER );
+		$wgUser = User::newFromName( Wikia::BOT_USER ); // Make changes as FANDOMbot
 
 		$this->dryRun = $this->hasOption( 'dryRun' );
 		$fileName = $this->getOption( 'file', false );
---
+++ maintenance/wikia/HttpsMigration/migrateUserCssTest.php
@@ -0,0 +1,190 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require "migrateUserCss.php";
+
+class MigrateUserCssToHttpsTest extends WikiaBaseTest {
+
+	function setUp() {
+		parent::setUp();
+		$this->task = new MigrateUserCssToHttps();
+		$this->mockGlobalVariable( 'wgServer', 'http://mechtest.wikia.com');
+	}
+
+	public function testWikiaComVignetteUrls() {
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/guildwars/es/images//6/64/Favicon.ico',
+			$this->task->fixUrl( 'http://images.wikia.com/guildwars/es/images//6/64/Favicon.ico' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/guildwars/es/images//6/64/Favicon.ico',
+			$this->task->fixUrl( 'http://static.wikia.com/guildwars/es/images//6/64/Favicon.ico' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/guildwars/es/images//6/64/Favicon.ico',
+			$this->task->fixUrl( 'https://static.wikia.com/guildwars/es/images//6/64/Favicon.ico' ) );
+
+	}
+
+	public function testShardedVignetteUrls() {
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png',
+			$this->task->fixUrl( 'http://images1.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png',
+			$this->task->fixUrl( 'http://vignette5.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png',
+			$this->task->fixUrl( 'http://img1.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/monsterhunters/ko/images/c/cc/Infobox_header_overlay.png',
+			$this->task->fixUrl( 'http://image3.wikia.nocookie.net/monsterhunters/ko/images/c/cc/Infobox_header_overlay.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png',
+			$this->task->fixUrl( 'http://static1.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png',
+			$this->task->fixUrl( 'http://slot1.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png' ) );
+	}
+
+	public function testWwwPrefixedVignetteUrls() {
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png',
+			$this->task->fixUrl( 'http://www.images.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/__cb20121002232621/narutofantasyrpg/de/images/e/e1/Benutzer_Icon_Monobook.png',
+			$this->task->fixUrl( 'http://www.images2.wikia.nocookie.net/__cb20121002232621/narutofantasyrpg/de/images/e/e1/Benutzer_Icon_Monobook.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png',
+			$this->task->fixUrl( 'http://www.static.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png' ) );
+	}
+
+	public function testLegacyImagesWikiaVignetteUrls() {
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/__cb100006/common/skins/oasis/images/sprite.png',
+			$this->task->fixUrl( 'http://slot1.images.wikia.nocookie.net/__cb100006/common/skins/oasis/images/sprite.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/__cb1509098293/common/skins/shared/images/sprite.png',
+			$this->task->fixUrl( 'https://slot1-images.wikia.nocookie.net/__cb1509098293/common/skins/shared/images/sprite.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/__cb58376/common/resources/jquery.ui/themes/default/jquery.ui.theme.css',
+			$this->task->fixUrl( 'http://slot2.images.wikia.nocookie.net/__cb58376/common/resources/jquery.ui/themes/default/jquery.ui.theme.css' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/__cb1509098293/common/skins/shared/images/sprite.png',
+			$this->task->fixUrl( 'https://www.slot1-images.wikia.nocookie.net/__cb1509098293/common/skins/shared/images/sprite.png' ) );
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/__cb58376/common/resources/jquery.ui/themes/default/jquery.ui.theme.css',
+			$this->task->fixUrl( 'http://slot2.images.wikia.nocookie.net/__cb58376/common/resources/jquery.ui/themes/default/jquery.ui.theme.css' ) );
+	}
+
+	public function testProtocolRelativeVignetteUrls() {
+
+		$this->assertEquals( 'https://vignette.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png',
+			$this->task->fixUrl( '//www.static.wikia.nocookie.net/djinni/images/6/68/Wikia_logo.png' ) );
+	}
+
+	public function testExternalHttpUrls() {
+		$this->assertEquals( '//muppet.wikia.com/wiki/MediaWiki:aaa.css',
+			$this->task->fixUrl( 'http://muppet.wikia.com/wiki/MediaWiki:aaa.css' ) );
+
+		$this->assertEquals( '//de.starwars.wikia.com/wiki/MediaWiki:aaa.css',
+			$this->task->fixUrl( 'http://de.starwars.wikia.com/wiki/MediaWiki:aaa.css' ) );
+
+		// leave https links intact
+		$this->assertEquals( 'https://muppet.wikia.com/wiki/MediaWiki:aaa.css',
+			$this->task->fixUrl( 'https://muppet.wikia.com/wiki/MediaWiki:aaa.css' ) );
+	}
+
+	public function testLocalHttpUrls() {
+		$this->assertEquals( '/wiki/MediaWiki:aaa.css',
+			$this->task->fixUrl( 'http://mechtest.wikia.com/wiki/MediaWiki:aaa.css' ) );
+
+		$this->assertEquals( '/wiki/MediaWiki:aaa.css',
+			$this->task->fixUrl( 'https://mechtest.wikia.com/wiki/MediaWiki:aaa.css' ) );
+
+		$this->assertEquals( '/wiki/MediaWiki:aaa.css',
+			$this->task->fixUrl( '//mechtest.wikia.com/wiki/MediaWiki:aaa.css' ) );
+	}
+
+	public function testThirdPartyUrls() {
+		// do not touch https links
+		$this->assertEquals( 'https://www.xyz.com/aaa.css',
+			$this->task->fixUrl( 'https://www.xyz.com/aaa.css' ) );
+
+		// do not touch protocol relative links
+		$this->assertEquals( '//fonts.googleapis.com/css?family=Henny+Penny',
+			$this->task->fixUrl( '//fonts.googleapis.com/css?family=Henny+Penny' ) );
+		$this->assertEquals( '//www.xyz.com/aaa.css',
+			$this->task->fixUrl( '//www.xyz.com/aaa.css' ) );
+
+		// do not touch unknown hosts
+		$this->assertEquals( 'http://www.xyz.com/aaa.css',
+			$this->task->fixUrl( 'http://www.xyz.com/aaa.css' ) );
+
+		// upgrade well known third party links
+		$this->assertEquals( 'https://i.imgur.com/PONtpqK.jpg',
+			$this->task->fixUrl( 'http://i.imgur.com/PONtpqK.jpg' ) );
+
+		$this->assertEquals( 'https://fonts.googleapis.com/css?family=Nunito',
+			$this->task->fixUrl( 'http://fonts.googleapis.com/css?family=Nunito' ) );
+
+		$this->assertEquals( 'https://upload.wikimedia.org/wikipedia/en/a/a7/Monobook-globe.png',
+			$this->task->fixUrl( 'http://upload.wikimedia.org/wikipedia/en/a/a7/Monobook-globe.png' ) );
+
+		$this->assertEquals( 'https://en.wikipedia.org/skins/monobook/discussionitem_icon.gif',
+			$this->task->fixUrl( 'http://en.wikipedia.org/skins/monobook/discussionitem_icon.gif' ) );
+	}
+
+	/**
+	 * @desc Tests the main functionality of scrapers (it's being re-used in all types of scrapers)
+	 * @dataProvider getCssUrlExtractorDataProvider
+	 */
+	public function testCssUrlExtractor($css, $match) {
+		$taskMock = $this->getMockBuilder( 'MigrateUserCssToHttps' )
+			->setMethods( [ 'makeUrlHttpsComatible' ] )
+			->getMock();
+
+		$taskMock
+			->expects( is_null( $match ) ? $this->never() : $this->once() )
+			->method( 'makeUrlHttpsComatible' )
+			->with( $match );
+
+		$taskMock->updateCSSContent( $css );
+	}
+
+	public function getCssUrlExtractorDataProvider() {
+		return [
+			# regular url
+			[ 'background: #fff; url(http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg) repeat scroll center top;' ,
+				[ 'url(http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg)', 'http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg' ] ],
+			# space after "url"
+			[ 'background: #fff; url (http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg) repeat scroll center top;' ,
+				[ 'url (http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg)', 'http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg' ] ],
+			# single quotes
+			[ 'background: #fff; url(\'http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg\') repeat scroll center top;' ,
+				[ 'url(\'http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg\')', 'http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg' ] ],
+			# double quotes
+			[ 'background: #fff; url("http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg") repeat scroll center top;' ,
+				[ 'url("http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg")', 'http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg' ] ],
+			# uppercase
+			[ 'background: #fff; URL("http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg") repeat scroll center top;' ,
+				[ 'URL("http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg")', 'http://images.wikia.com/gaia/images/1/1d/Gaiaonline_global_bg.jpg' ] ],
+			# @import
+			[ '@import "http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css";' ,
+				[ '@import "http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css"', 'http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css' ] ],
+			[ '@import \'http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css\';' ,
+				[ '@import \'http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css\'', 'http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css' ] ],
+			[ '@IMPORT \'http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css\';' ,
+				[ '@IMPORT \'http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css\'', 'http://gaia.wikia.com/index.php?title=MediaWiki:Common.css&action=raw&ctype=text/css' ] ],
+			# alpha filter
+			[ '#submit {filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'/images/btn.png\', sizingMethod=\'scale\');}' ,
+				[ '(src=\'/images/btn.png\'', '/images/btn.png' ] ],
+			# urls that should not be altered
+			[ '.speech-bubble-message a[href="http://theuniversim.wikia.com/wiki/Message_Wall:"]:not(.subtle)::after {' , null ],
+			[ '* For more information see <http://dev.wikia.com/wiki/Less>', null ],
+			[ ' bottom (see http://www.fontspring.com/blog/the-new-bulletproof-font-face-syntax)', null ],
+			[ 'a[href="http://ru.bloodborne.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Contributions/Bermoodok"]', null ],
+			[ 'a[href="http://ru.bloodborne.wikia.com/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:Contributions/Bermoodok"]', null ],
+			[ '.listofponies a[href^="http://www.reddit.com/r/listofponies"],', null ],
+		];
+	}
+
+}
