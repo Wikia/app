bbb0cb7723d79fcda719b94887b406a9c7d0f7ed
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-03-23 09:18:08 +0100

    ADEN-6799 Remove wikia dart helper

--- extensions/wikia/AdEngine/AdEngine2Hooks.class.php
+++ extensions/wikia/AdEngine/AdEngine2Hooks.class.php
@@ -155,8 +155,6 @@ class AdEngine2Hooks {
 		// Legacy vars:
 		// Queue for ads registration
 		$vars['adslots2'] = [ ];
-		// Used to hop by DART ads
-		$vars['adDriverLastDARTCallNoAds'] = [ ];
 		// 3rd party code (eg. dart collapse slot template) can force AdDriver2 to respect unusual slot status
 		$vars['adDriver2ForcedStatus'] = [ ];
 
