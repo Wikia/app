91ed1ea7bee7b5de83a8f706be0a495eea0bbcac
Kacper Olek <kolek@fandom.com>
2018-03-28 17:13:02 +0000

    XW-4808 | Finally 'quite' working script

--- extensions/wikia/Recirculation/script.php
+++ extensions/wikia/Recirculation/script.php
@@ -1,6 +1,8 @@
 <?php
 	exec('node parseRecommendations.js');
 	$file = json_decode(file_get_contents('./recommendations.json', true),true);
-	file_put_contents('rec.txt', var_export( $file, true ));
-	$script = 'sed -i "s/array (/[/g" rec.txt && sed -i "s/)/]/g" rec.txt && sed -i "s/[0-9][0-9]* => //g" rec.txt && sed -i "/^\s*$/d" rec.txt';
+	file_put_contents('recommendations.txt', var_export( $file, true ));
+
+	$script = 'sed -i "s/array (/[/g" recommendations.txt && sed -i "s/)/]/g" recommendations.txt && sed -i "s/[0-9][0-9]* => //g" recommendations.txt && sed -i "/^\s*$/d" recommendations.txt';
 	shell_exec($script);
+	shell_exec("rm recommendations.json");
