064c1758c3ae753392ddc66545fb04221097ba23
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-04-05 17:08:22 +0200

    ADEN-6957 Fix tests

--- extensions/wikia/AdEngine/js/spec/provider/gpt/googleTag.spec.js
+++ extensions/wikia/AdEngine/js/spec/provider/gpt/googleTag.spec.js
@@ -14,6 +14,7 @@ describe('ext.wikia.adEngine.provider.gpt.googleTag', function () {
 			},
 			bridge: {
 				slotService: {
+					clearSlot: noop,
 					getBySlotName: noop,
 					hasViewportConflict: noop
 				}
