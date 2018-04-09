5a02b252fe44b8cd4ba443cd6586a318e1a72653
Kacper Olek <kolek@fandom.com>
2018-04-09 17:39:41 +0200

    XW-4645 | button done

--- extensions/wikia/DesignSystem/scripts/DesignSystemGlobalFooter.js
+++ extensions/wikia/DesignSystem/scripts/DesignSystemGlobalFooter.js
@@ -1,3 +1,9 @@
-function footerClick(){
-	document.cookie = 'useskin=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=.wikia-dev.pl';
-}
+
+require(['jquery'],
+	function ($) {
+		$('.mobile-site-link').click(mobileSiteLinkClicked);
+		function mobileSiteLinkClicked(){
+			document.cookie = 'useskin=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=' + wgCookieDomain;
+		}
+	}
+);
--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalFooter_mobileSiteButton.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalFooter_mobileSiteButton.php
@@ -1,7 +1,6 @@
-<div class="wds-global-footer__bottom-bar-row wds-has-border-top global-footer-full-site-link-wrapper">
+<div class="wds-global-footer__bottom-bar-row wds-has-border-top">
 	<a href="<?= Sanitizer::encodeAttribute( $model['href'] ); ?>"
-	   onclick="footerClick()"
-	   class="wds-global-footer__button-link">
+	   class="wds-global-footer__button-link mobile-site-link">
 		<?= DesignSystemHelper::renderText( $model['title'] ) ?>
 	</a>
 </div>
