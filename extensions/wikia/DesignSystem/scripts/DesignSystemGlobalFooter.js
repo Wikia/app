171afc14fe1d9dbfb268d70cbc56cd842e2fc546
Kacper Olek <kolek@fandom.com>
2018-04-09 18:16:50 +0200

    XW-4645 | deleted unnecessary blank lines

--- extensions/wikia/DesignSystem/scripts/DesignSystemGlobalFooter.js
+++ extensions/wikia/DesignSystem/scripts/DesignSystemGlobalFooter.js
@@ -1,12 +1,10 @@
 
 require(['jquery'],
 	function ($) {
-
 		if ( $.cookie('useskin') === 'oasis' ) {
 			$('.mobile-site-link').click(mobileSiteLinkClicked);
 			$('.mobile-site-link').css("display", "inline-block");
 		}
-
 		function mobileSiteLinkClicked(){
 			$.cookie("useskin", null, {path: '/', domain: wgCookieDomain});
 		}
