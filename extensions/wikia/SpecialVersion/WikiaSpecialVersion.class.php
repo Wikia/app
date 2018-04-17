5be95c7f656c42382022c609204f7f8b31c57e05
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2018-04-17 16:05:24 +0200

    in Special:Version remove useless data

--- extensions/wikia/SpecialVersion/WikiaSpecialVersion.class.php
+++ extensions/wikia/SpecialVersion/WikiaSpecialVersion.class.php
@@ -40,15 +40,11 @@ class WikiaSpecialVersion extends SpecialVersion {
 	 * @return array of strings
 	 */
 	public static function getSoftwareList() {
-	    $dbr = wfGetDB( DB_SLAVE );
-
 	    // Put the software in an array of form 'name' => 'version'. All messages should
 	    // be loaded here, so feel free to use wfMsg*() in the 'name'. Raw HTML or wikimarkup
 	    // can be used.
 	    $software = array();
 	    $software['[https://www.mediawiki.org MediaWiki]'] = self::getVersionLinked();
-	    $software['[http://www.php.net/ PHP]'] = phpversion() . " (" . php_sapi_name() . ")";
-	    $software[$dbr->getSoftwareLink()] = $dbr->getServerInfo();
 
 	    // Allow a hook to add/remove items.
 	    Hooks::run( 'SoftwareInfo', array( &$software ) );
