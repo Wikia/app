e02c9d8ab36faf7c82fda9ae9ce337235f648ce7
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-04-19 12:48:46 +0200

    XW-4880: bring back previously removed logic with check for existance of title

--- extensions/wikia/MercuryApi/MercuryApiController.class.php
+++ extensions/wikia/MercuryApi/MercuryApiController.class.php
@@ -386,6 +386,15 @@ class MercuryApiController extends WikiaController {
 						if ( !empty( $featuredVideo ) ) {
 							$data['article']['featuredVideo'] = $featuredVideo;
 						}
+
+						if ( !$title->isContentPage() ) {
+							// Remove the namespace prefix from display title
+							$tempTitle = Title::newFromText( $displayTitle );
+							if ( !empty( $tempTitle ) && $tempTitle->isKnown() ) {
+								$displayTitle = Title::newFromText( $displayTitle )->getText();
+								$data['article']['displayTitle'] = $displayTitle;
+							}
+						}
 					}
 
 					switch ( $data['ns'] ) {
