62ba8b22d1f1ae11343c209e4b1003508f7d60c9
macbre <maciej.brencz@gmail.com>
2018-04-25 14:54:49 +0200

    MathMathJax: pass TeX syntax via HTML attribute when rendering for mobile-skin

--- extensions/Math/MathMathJax.php
+++ extensions/Math/MathMathJax.php
@@ -20,6 +20,10 @@
  */
 class MathMathJax extends MathRenderer {
 	function render() {
+		# No need to render or parse anything more!
+		# New lines are replaced with spaces, which avoids confusing our parser (bugs 23190, 22818)
+		$tex = '$ ' . str_replace( "\n", " ", $this->tex ) . ' $';
+
 		$attributes = [
 			'class' => 'tex',
 			'dir' => 'ltr'
@@ -27,18 +31,19 @@ class MathMathJax extends MathRenderer {
 
 		// Wikia change - mark span tags with an attribute
 		// so that mobile-wiki's widget framework can handle them
-		if ( ( new WikiaTagBuilderHelper() )->isMobileSkin() ) {
+		$isMobileSkin = ( new WikiaTagBuilderHelper() )->isMobileSkin();
+		if ( $isMobileSkin ) {
 			$attributes['data-wikia-widget'] = 'math';
+			$attributes['data-tex'] = $tex;
+			$attributes['class'] = '';
 		}
 
-		# No need to render or parse anything more!
-		# New lines are replaced with spaces, which avoids confusing our parser (bugs 23190, 22818)
 		return Xml::element( 'span',
 			$this->getAttributes(
 				'span',
 				$attributes
 			),
-			'$ ' . str_replace( "\n", " ", $this->tex ) . ' $'
+			$isMobileSkin ? '' : $tex
 		);
 	}
 }
