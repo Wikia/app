ac3ab63389e18e1f8abdfefd0ccf4c03e2fbe005
Damian Sobczak <dsobczak@wikia-inc.com>
2018-05-07 16:11:06 +0200

    ADEN-7095 Add GDPR tracking disabling

--- resources/wikia/modules/instantGlobals.js
+++ resources/wikia/modules/instantGlobals.js
@@ -1,10 +1,47 @@
+/*global define*/
 /**
  * AMD module exporting Wikia.InstantGlobals object
  */
-/*global define*/
-define('wikia.instantGlobals', ['wikia.window'], function(window) {
+define('wikia.instantGlobals', [
+	'wikia.querystring',
+	'wikia.window'
+], function(Querystring, window) {
 	'use strict';
+
+	var qs = new Querystring(),
+		trackingBlacklist = {
+			boolean: [
+				'wgEnableKruxTargeting'
+			],
+			country: [
+				'wgAdDriverKruxCountries',
+				'wgAdDriverKikimoraPlayerTrackingCountries',
+				'wgAdDriverKikimoraTrackingCountries',
+				'wgAdDriverKikimoraViewabilityTrackingCountries'
+			]
+		};
+
+	function isUrlParameterSet(parameter) {
+		return !!parseInt(qs.getVal(parameter, '0'), 10);
+	}
+
+	function filterBlacklist(variables) {
+		trackingBlacklist.boolean.forEach(function (variable) {
+			variables[variable] = false;
+		});
+
+		trackingBlacklist.country.forEach(function (variable) {
+			variables[variable] = [];
+		});
+
+		return variables;
+	}
+
 	if (window.Wikia && window.Wikia.InstantGlobals) {
+		if (isUrlParameterSet('disabletracking')) {
+			return filterBlacklist(window.Wikia.InstantGlobals);
+		}
+
 		return window.Wikia.InstantGlobals;
 	}
 
