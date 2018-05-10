c4be83d1119f1a528f3d81bf5cea087b7c63eba9
Paweł Wójcik <pwojcik@wikia-inc.com>
2018-05-10 11:07:14 +0200

    SUS-4752 remove user data fromw wiki + tests

--- extensions/wikia/Privacy/RemoveUserDataOnWikiTask.php
+++ extensions/wikia/Privacy/RemoveUserDataOnWikiTask.php
@@ -10,23 +10,42 @@ use Wikia\Tasks\Tasks\BaseTask;
  * TODO: how to report the final result?
  */
 class RemoveUserDataOnWikiTask extends BaseTask {
-
-
-	public function removeCheckUserData() {
-		
-	}
-
-	public function removeData( int $userId ) {
+	
+	/**
+	 * Deletes all CheckUser records associated with the given user
+	 *
+	 * @param int $userId
+	 * @throws DBUnexpectedError
+	 */
+	public function removeCheckUserData( int $userId ) {
 		$db = wfGetDB( DB_MASTER );
 		// remove check user data
 		$db->delete( 'cu_changes', ['cuc_user' => $userId], __METHOD__ );
 		$db->delete( 'cu_log', ['cul_target_id' => $userId], __METHOD__ );
-		// anonimize IP in recent changes
+	}
+
+	/**
+	 * Removes the IP address from all RecentChanges records associated with the given user
+	 *
+	 * @param int $userId
+	 */
+	public function removeIpFromRecentChanges( int $userId ) {
+		$db = wfGetDB( DB_MASTER );
 		// TODO: consider removing last octet / 80 bits
 		$db->update( 'recentchanges', ['rc_ip_bin' => ''], ['rc_user' => $userId], __METHOD__ );
-		// anonimize AbuseFilter
+	}
+
+	/**
+	 * Removes abuse filter logs associated with the given user
+	 *
+	 * @param int $userId
+	 */
+	public function removeAbuseFilterData( int $userId ) {
 		global $wgEnableAbuseFilterExtension;
+		$db = wfGetDB( DB_MASTER );
 		if( $wgEnableAbuseFilterExtension ) {
+			$db->update( 'abuse_filter', ['af_user_text' => ''], ['af_user' => $userId], __METHOD__ );
+			$db->update( 'abuse_filter_history', ['afh_user_text' => ''], ['afh_user' => $userId], __METHOD__ );
 			// TODO: consider removing last octet / 80 bits or removing
 			$db->delete( 'abuse_filter_log', ['afl_user' => $userId], __METHOD__ );
 		}
--- extensions/wikia/Privacy/tests/RemoveUserDataOnWikiTaskTest.php
+++ extensions/wikia/Privacy/tests/RemoveUserDataOnWikiTaskTest.php
@@ -5,8 +5,7 @@
  */
 class RemoveUserDataOnWikiTaskTest extends WikiaDatabaseTest {
 
-	const REMOVED_USER_ID = 1;
-	const OTHER_USER_ID = 2;
+	const TEST_USER_ID = 1;
 
 	/**
 	 * Returns the test dataset.
@@ -18,21 +17,57 @@ class RemoveUserDataOnWikiTaskTest extends WikiaDatabaseTest {
 	}
 
 	protected function extraSchemaFiles() {
-		return [__DIR__ . '/../../../AbuseFilter/abusefilter.tables.sqlite.sql', __DIR__ . '/fixtures/cu_changes.sqllite.sql'];
+		return [__DIR__ . '/fixtures/drop_test_tables.sql', __DIR__ . '/../../../AbuseFilter/abusefilter.tables.sqlite.sql', __DIR__ . '/fixtures/check_user.sql'];
 	}
 
 	protected function setUp() {
 		parent::setUp();
 		include __DIR__ . '/../Privacy.setup.php';
+		$this->mockGlobalVariable( 'wgEnableAbuseFilterExtension', true );
 	}
 
 	public function testShouldRemoveCheckUserData() {
-		(new RemoveUserDataOnWikiTask())->removeData( self::REMOVED_USER_ID );
+		(new RemoveUserDataOnWikiTask())->removeCheckUserData( self::TEST_USER_ID );
 		$logPager = new CheckUserLogPager( null, [], null, null );
 		$this->assertEquals( 1,  $logPager->getNumRows() );
 		foreach ( $logPager->getResult() as $row ) {
-			$this->assertNotEquals( self::REMOVED_USER_ID, $row->cul_target_id );
+			$this->assertNotEquals( self::TEST_USER_ID, $row->cul_target_id );
 		}
+	}
+	
+	public function testShouldRemoveIpsFromRecentChanges() {
+		(new RemoveUserDataOnWikiTask())->removeIpFromRecentChanges( self::TEST_USER_ID );
+		$dbr = wfGetDB( DB_SLAVE );
+		$changes = $dbr->select( 'recentchanges', ['rc_user', 'rc_user_text', 'rc_ip_bin'] );
+		foreach ( $changes as $change ) {
+			if ( $change->rc_user == self::TEST_USER_ID ) {
+				$this->assertTrue( empty( $change->rc_ip_bin ) );
+			} else {
+				$this->assertFalse( empty( $change->rc_ip_bin ) );
+			}
+		}
+	}
+
+	public function testShouldRemoveAbuseFilterData() {
+		(new RemoveUserDataOnWikiTask())->removeAbuseFilterData( self::TEST_USER_ID );
+		$dbr = wfGetDB( DB_SLAVE );
+		// check filter author
+		$filters = $dbr->select( 'abuse_filter', ['af_user', 'af_user_text'] );
+		foreach ( $filters as $f ) {
+			if ( $f->af_user == self::TEST_USER_ID ) {
+				$this->assertTrue( empty( $f->af_user_text ) );
+			}
+		}
+		// check author in filter history
+		$history = $dbr->select( 'abuse_filter_history', ['afh_user', 'afh_user_text'] );
+		foreach ( $history as $h ) {
+			if ( $h->afh_user == self::TEST_USER_ID ) {
+				$this->assertTrue( empty( $h->afh_user_text ) );
+			}
+		}
+		// check abuse filter log
+		$log = $dbr->select( 'abuse_filter_log', ['afl_user'], ['afl_user' => self::TEST_USER_ID] );
+		$this->assertEquals( 0, $log->numRows() );
 	}
 	
 }
