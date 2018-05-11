ba1456216c86e1b49459b7c1c8e2b93efc8aa6f4
Paweł Wójcik <pwojcik@wikia-inc.com>
2018-05-11 12:58:33 +0200

    SUS-4752 controller refactoring

--- extensions/wikia/Privacy/RemoveUserDataController.php
+++ extensions/wikia/Privacy/RemoveUserDataController.php
@@ -1,8 +1,9 @@
 <?php
 
-use Wikia\Logger\WikiaLogger;
+use Wikia\Logger\Loggable;
 
 class RemoveUserDataController extends WikiaController {
+	use Loggable;
 
 	const ACCEPTED = 202;
 	const METHOD_NOT_ALLOWED = 405;
@@ -13,43 +14,72 @@ class RemoveUserDataController extends WikiaController {
 
 	public function removeUserData() {
 		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
+
 		if ( !$this->request->wasPosted() ) {
 			$this->response->setCode( self::METHOD_NOT_ALLOWED );
 			return;
 		}
 
 		$userId = $this->getVal( 'userId' );
-		$isDryRun = $this->getVal( 'dryRun' );
 		if ( empty( $userId ) ) {
 			$this->response->setCode( WikiaResponse::RESPONSE_CODE_BAD_REQUEST );
 			return;
 		}
 
-		if ( $isDryRun ) {
-			WikiaLogger::instance()->info( "Will remove user's global data: $userId" );
-		} else {
-			$dataRemover = new UserDataRemover();
-			$dataRemover->removeGlobalData( $userId );
-		}
+		$this->info( "Right to be forgotten request for user $userId", ['userId' => $userId] );
 
-		global $wgSpecialsDB;
-		$specialsDbr = wfGetDB( DB_SLAVE, [], $wgSpecialsDB );
-		$userWikis = $specialsDbr->selectFieldValues( 'events_local_users', 'wiki_id', ['user_id' => $userId], __METHOD__, ['DISTINCT'] );
+		$dataRemover = new UserDataRemover();
+		$dataRemover->removeGlobalData( $userId );
+
+		$this->info( "Global data removed for user $userId", ['userId' => $userId] );
+
+		$userWikis = $this->getUserWikis( $userId );
 		$username = User::whoIs( $userId );
 		$wikiCount = count( $userWikis );
-		if ( $isDryRun ) {
-			WikiaLogger::instance()->info( "Will remove user:$userId:$username local data from $wikiCount wikis" );
-		} else {
-			$removeWikiDataTask = new RemoveUserDataOnWikiTask();
-			$removeWikiDataTask->call( 'removeAllData', $userId,  $username);
-			$removeWikiDataTask->wikiId( $userWikis )->queue();
-		}
+
+		$removeWikiDataTask = new RemoveUserDataOnWikiTask();
+		$removeWikiDataTask->call( 'removeAllData', $userId,  $username);
+		$removeWikiDataTask->wikiId( $userWikis )->queue();
+
+		$this->info( "Wiki data removal queued for user $userId", ['userId' => $userId] );
+
 		$this->response->setCode( self::ACCEPTED );
 		$this->response->setValues( [
 			'userId' => $userId,
 			'username' => $username,
 			'numberOfWikis' => $wikiCount
 		] );
+	}
+
+	/**
+	 * Helper function to assess the scope of removal.
+	 */
+	public function getRemoveDetails() {
+		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
+
+		$userId = $this->getVal( 'userId' );
+		if ( empty( $userId ) ) {
+			$this->response->setCode( WikiaResponse::RESPONSE_CODE_BAD_REQUEST );
+			return;
+		}
+
+		$this->response->setCode( self::ACCEPTED );
+		$this->response->setValues( [
+			'userId' => $userId,
+			'username' => User::whoIs( $userId ),
+			'numberOfWikis' => count( $this->getUserWikis( $userId ) )
+		] );
+	}
+
+	private function getUserWikis( int $userId ) {
+		global $wgSpecialsDB;
+		$specialsDbr = wfGetDB( DB_SLAVE, [], $wgSpecialsDB );
+		return $specialsDbr->selectFieldValues( 'events_local_users', 'wiki_id', ['user_id' => $userId], __METHOD__, ['DISTINCT'] );
+	}
+
+	protected function getLoggerContext() {
+		// make right to be forgotten logs more searchable
+		return ['right_to_be_forgotten' => 1];
 	}
 
 }
