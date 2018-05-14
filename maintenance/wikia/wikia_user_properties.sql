30306c50a5356703aa52e56258a4ba02b46f593f
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2018-05-14 12:39:20 +0200

    SUS-4773 | in wikia_user_properties table fix indexes

--- maintenance/wikia/wikia_user_properties.sql
+++ maintenance/wikia/wikia_user_properties.sql
@@ -6,11 +6,12 @@ CREATE TABLE /*$wgDBprefix*/wikia_user_properties (
   wup_user INT NOT NULL,
 
   -- Multiple key 2, name of property
-  wup_property VARBINARY(255) NULL DEFAULT NULL,
+  wup_property VARBINARY(255) NOT NULL DEFAULT '',
 
   -- Property value
-  wup_value BLOB NULL DEFAULT NULL
+  wup_value BLOB NULL DEFAULT NULL,
+
+  PRIMARY KEY (wup_user, wup_property)
 )/*$wgDBTableOptions*/;
 
-CREATE UNIQUE INDEX wup_user_property_idx ON wikia_user_properties(wup_user, wup_property);
 CREATE INDEX wup_property_idx ON wikia_user_properties(wup_property);
