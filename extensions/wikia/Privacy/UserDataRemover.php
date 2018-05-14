0f37697a6676188d78674f459c51f9e67804d4ab
macbre <maciej.brencz@gmail.com>
2018-05-14 11:34:41 +0200

    UserDataRemover | solve caching issues with old user name -> user ID mapping

--- extensions/wikia/Privacy/UserDataRemover.php
+++ extensions/wikia/Privacy/UserDataRemover.php
@@ -12,16 +12,26 @@ class UserDataRemover {
 				return false;
 			}
 
+			$userName = $user->getName();
+			$newUserName = uniqid( 'Anonymous ' );
+
 			$userIdentityBox = new UserIdentityBox( $user );
 			$userIdentityBox->clearMastheadContents();
 			Wikia::invalidateUser( $user, true, false );
 
 			$dbMaster = wfGetDB( DB_MASTER, [], 'wikicities' );
 
+			// commit changes performed by Wikia::invalidateUser
+			$dbMaster->commit( __METHOD__ );
+			wfWaitForSlaves( $dbMaster->getDBname() );
+
+			// invalidate the old user name -> user ID mapping
+			$this->invalidateUser( $userName );
+
 			$dbMaster->update(
 				'user',
 				[
-					'user_name' => uniqid( 'Anonymous ' ),
+					'user_name' => $newUserName,
 					'user_birthdate' => null,
 				],
 				['user_id' => $userId],
@@ -32,14 +42,38 @@ class UserDataRemover {
 			$dbMaster->delete( 'user_properties', ['up_user' => $userId] );
 
 			// commit early so that cache is properly invalidated
-			$dbMaster->commit();
+			$dbMaster->commit( __METHOD__ );
 			wfWaitForSlaves( $dbMaster->getDBname() );
 
-			$this->info( "Removed user's global data", ['user_id' => $userId] );
+			// invalidate user cache
+			$user = User::newFromId( $userId );
+
+			$this->invalidateUser( $user );
+			$this->invalidateUser( $newUserName );
+
+			$this->info( "Removed user's global data", ['user_id' => $userId, 'new_user_name' => $newUserName] );
 			return true;
 		} catch ( Exception $error ) {
 			$this->error( "Couldn't remove global user data", ['exception' => $error, 'user_id' => $userId] );
 			return false;
+		}
+	}
+
+	/**
+	 * Borrowed from RenameUserProcess class
+	 *
+	 * @param User|string $user
+	 * @throws InvalidArgumentException
+	 */
+	private function invalidateUser( $user ) {
+		if ( is_string( $user ) ) {
+			$user = User::newFromName( $user );
+		} elseif ( !is_object( $user ) ) {
+			throw new InvalidArgumentException(__METHOD__ . ' method should be called with string or User provided');
+		}
+
+		if ( is_object( $user ) ) {
+			$user->invalidateCache();
 		}
 	}
 
