884e47a8dce113b93739dbef997022012b6eac36
mech <wozniak.jacek@gmail.com>
2018-05-16 17:44:16 +0200

    PLATFORM-3487: return csv file on post request

--- extensions/wikia/DownloadYourData/DownloadYourData.i18n.php
+++ extensions/wikia/DownloadYourData/DownloadYourData.i18n.php
@@ -4,6 +4,7 @@ $messages = array();
 $messages['en'] = array(
 	'downloadyourdata' => 'Download Your Data',
 	'downloadyourdata-desc' => 'Allows users to download their data',
-	'downloadyourdata-intro' => 'This page allows you to download a copy of your account data.',
+	'downloadyourdata-intro' => 'The button below allows you to download a copy of your account data.',
 	'downloadyourdata-not-logged-in' => 'You have to be logged in to use this page',
+	'downloadyourdata-button-text' => 'Download data',
 );
--- extensions/wikia/DownloadYourData/DownloadYourDataSpecialController.class.php
+++ extensions/wikia/DownloadYourData/DownloadYourDataSpecialController.class.php
@@ -11,13 +11,42 @@ class DownloadYourDataSpecialController extends WikiaSpecialPageController {
 
 		$this->specialPage->setHeaders();
 
+		$user = $this->getUser();
+
+		if ( $user->isLoggedIn() && $this->request->wasPosted() && $user->matchEditToken( $this->getVal( 'token' ) ) ) {
+			$this->response->setHeader( 'Content-disposition', 'attachment;filename=wikia_account_data.csv' );
+			$this->response->setContentType( 'text/csv' );
+			$this->response->setCode( WikiaResponse::RESPONSE_CODE_OK );
+			$this->response->setFormat( WikiaResponse::FORMAT_RAW );
+			echo "fake csv content";
+			$this->skipRendering();
+
+			wfProfileOut( __METHOD__ );
+
+			return false;
+		}
+
 		$this->response->setTemplateEngine( WikiaResponse::TEMPLATE_ENGINE_MUSTACHE );
 
-		$this->isLoggedIn = $this->getUser()->isLoggedIn();
+		$this->isLoggedIn = $user->isLoggedIn();
+
+		$this->notLoggedInMessage = $this->msg( 'downloadyourdata-not-logged-in' )->escaped();
+		$this->editToken = $user->getEditToken();
+		$this->showForm = true;
+
+		$buttonParams = [
+			'type' => 'button',
+			'vars' => [
+				'type' => 'submit',
+				'classes' => [ 'wikia-button' ],
+				'value' => $this->msg( 'downloadyourdata-button-text' )->text(),
+				'data' => [],
+			]
+		];
+
+		$this->submitButton = \Wikia\UI\Factory::getInstance()->init( 'button' )->render( $buttonParams );
 
 		$this->introText = $this->msg( 'downloadyourdata-intro' )->escaped();
-		$this->notLoggedInMessage = $this->msg( 'downloadyourdata-not-logged-in' )->escaped();
-
 
 		wfProfileOut( __METHOD__ );
 	}
