5fbda660b1c861c64b8f3462fe2ce243023e0a43
nAndy <nandy@wikia-inc.com>
2018-05-17 12:49:48 +0000

    ADEN-7161 remove trackingOptOut from ad context

--- extensions/wikia/AdEngine/js/context/adContext.js
+++ extensions/wikia/AdEngine/js/context/adContext.js
@@ -9,12 +9,11 @@ define('ext.wikia.adEngine.adContext', [
 	'wikia.geo',
 	'wikia.instantGlobals',
 	'wikia.trackingOptIn',
-	'wikia.trackingOptOut',
 	'ext.wikia.adEngine.geo',
 	'ext.wikia.adEngine.utils.sampler',
 	'wikia.window',
 	'wikia.querystring'
-], function (browserDetect, cookies, doc, geo, instantGlobals, trackingOptIn, trackingOptOut, adsGeo, sampler, w, Querystring) {
+], function (browserDetect, cookies, doc, geo, instantGlobals, trackingOptIn, adsGeo, sampler, w, Querystring) {
 	'use strict';
 
 	instantGlobals = instantGlobals || {};
