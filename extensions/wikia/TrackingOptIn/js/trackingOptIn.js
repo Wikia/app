118d414644f1d2fb9fcd1ff61f78033153c01696
nAndy <nandy@wikia-inc.com>
2018-05-17 09:43:49 +0000

    ADEN-7161 add use strict and logging

--- extensions/wikia/TrackingOptIn/js/trackingOptIn.js
+++ extensions/wikia/TrackingOptIn/js/trackingOptIn.js
@@ -1,24 +1,32 @@
 define('wikia.trackingOptIn', [
 	'trackingOptIn',
-	'wikia.lazyqueue'
-], function (trackingOptIn, lazyQueue) {
+	'wikia.lazyqueue',
+	'wikia.log'
+], function (trackingOptIn, lazyQueue, log) {
+	'use strict';
+
 	var optIn = false,
-		userConsentQueue = [];
+		userConsentQueue = [],
+		logGroup = 'ext.wikia.TrackingOptIn';
 
 	lazyQueue.makeQueue(userConsentQueue, function (callback) {
 		callback(optIn);
 	});
 
 	function init() {
+		log('TrackingOptIn init', log.levels.info, logGroup);
+
 		trackingOptIn.main({
 			countriesRequiringPrompt: ['us'], // TODO provide full list of countries
 			country: 'us', // TODO provide geo from cookie
 			onAcceptTracking: function () {
 				optIn = true;
 				userConsentQueue.start();
+				log(['TrackingOptIn:accept queue started', userConsentQueue], log.levels.info, logGroup);
 			},
 			onRejectTracking: function () {
 				userConsentQueue.start();
+				log(['TrackingOptIn:reject queue started', userConsentQueue], log.levels.info, logGroup);
 			}
 		});
 	}
@@ -30,5 +38,5 @@ define('wikia.trackingOptIn', [
 	return {
 		init: init,
 		pushToUserConsentQueue: pushToUserConsentQueue
-	}
+	};
 });
