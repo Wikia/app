a2702c85983460bfac7d2573827bae495ccc887a
macbre <maciej.brencz@gmail.com>
2018-05-18 09:18:43 +0200

    Move ArticleComment::makeCommentTitle to ArticleCommentsTitle

--- extensions/wikia/ArticleComments/classes/ArticleCommentsTitle.class.php
+++ extensions/wikia/ArticleComments/classes/ArticleCommentsTitle.class.php
@@ -21,4 +21,19 @@ class ArticleCommentsTitle {
 		}, $oldTitleText );
 	}
 
+	/**
+	 * Format a page_title for a comment
+	 *
+	 * e.g. Macbre/@comment-(user_ID_or_IP)-20170301152835/@comment-(user_ID_or_IP)-20171103161051
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @param string $now
+	 * @return string
+	 */
+	public static function format( Title $title, User $user, $now = null ): string {
+		return sprintf( '%s/%s%s-%s', $title->getText(), ARTICLECOMMENT_PREFIX,
+			$user->isLoggedIn() ? $user->getId() : $user->getName(), $now ?: wfTimestampNow() );
+	}
+
 }
