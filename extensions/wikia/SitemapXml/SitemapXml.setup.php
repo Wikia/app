f48248224f274bd6e15fa1beeb017e473916d809
mech <wozniak.jacek@gmail.com>
2018-05-21 20:59:20 +0200

    PLATFORM-3462: update extension path

--- extensions/wikia/SitemapXml/SitemapXml.setup.php
+++ extensions/wikia/SitemapXml/SitemapXml.setup.php
@@ -32,7 +32,7 @@ $wgExtensionCredits['specialpage'][] = array(
 	'author' => 'Krzysztof Krzyżaniak',
 	'description' => 'Generate Sitemaps for the wiki on the fly',
 	'description-msg' => 'sitemap-desc',
-	'url' => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/Sitemap'
+	'url' => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/SitemapXml'
 );
 
 /**
