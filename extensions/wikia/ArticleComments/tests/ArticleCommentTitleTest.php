d1dd81801341ba7fc2402f9ef1dd2665b6dc0e1f
macbre <maciej.brencz@gmail.com>
2018-05-21 10:27:47 +0200

    SUS-4766 | trim page_title when migrating comments / wall and forum entries to fit in the database column

--- extensions/wikia/ArticleComments/tests/ArticleCommentTitleTest.php
+++ extensions/wikia/ArticleComments/tests/ArticleCommentTitleTest.php
@@ -48,6 +48,12 @@ class ArticleCommentTitleTest extends WikiaDatabaseTest {
 			'Macbre/Macbre-20170302153001',
 			'Macbre/Macbre-20170302153001',
 		];
+
+		// too long title
+		yield [
+			'FooBar/Thiś_is_ąn_important_announcement._SO_important,_in_fact,_that_if_you_don\'t_read_it_your_brain_will_explode_out_your_ears,_and_that_will_be_the_end_of_you_forever._Until_I_work_out_how_to_sustain_brains_in_glass_jars./@comment-FooBar-20111217083734',
+			'FooBar/Thiś_is_ąn_important_announcement._SO_important,_in_fact,_that_if_you_don\'t_read_it_your_brain_will_explode_out_your_ears,_and_that_will_be_the_end_of_you_forever._Until_I_work_out_how_to_sustain_brains_in_glass_jars./@comment-1234567890-20111217',
+		];
 	}
 
 	/**
@@ -57,10 +63,10 @@ class ArticleCommentTitleTest extends WikiaDatabaseTest {
 	 * @param string $expected
 	 */
 	public function testNormalize(string $oldTitleText, string $expected) {
-		$this->assertEquals(
-			$expected,
-			ArticleCommentsTitle::normalize($oldTitleText)
-		);
+		$normalizedTitle = ArticleCommentsTitle::normalize( $oldTitleText );
+
+		$this->assertLessThanOrEqual( 255, strlen( $normalizedTitle ), 'New page_title should fit the DB column' );
+		$this->assertEquals( $expected, $normalizedTitle );
 	}
 
 	protected function getDataSet() {
