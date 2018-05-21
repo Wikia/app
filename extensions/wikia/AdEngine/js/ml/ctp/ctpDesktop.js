65d57d755fafa5ad602a2101798f6d8f1c24d291
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-05-21 10:47:27 +0200

    ADEWN-7168 Wrap model with CTP FVs

--- extensions/wikia/AdEngine/js/ml/ctp/ctpDesktop.js
+++ extensions/wikia/AdEngine/js/ml/ctp/ctpDesktop.js
@@ -3,16 +3,21 @@ define('ext.wikia.adEngine.ml.ctp.ctpDesktop', [
 	'ext.wikia.adEngine.adContext',
 	'ext.wikia.adEngine.ml.ctp.ctpDesktopData',
 	'ext.wikia.adEngine.ml.modelFactory',
-	'ext.wikia.adEngine.ml.model.linear'
-], function (adContext, data, modelFactory, linearModel) {
+	'ext.wikia.adEngine.ml.model.linear',
+	require.optional('wikia.articleVideo.featuredVideo.autoplay')
+], function (adContext, data, modelFactory, linearModel, featuredVideoAutoplay) {
 	'use strict';
+
+	var isFeaturedVideoClickToPlay = !!adContext.get('targeting.hasFeaturedVideo') &&
+		!!featuredVideoAutoplay &&
+		featuredVideoAutoplay.isAutoplayEnabled();
 
 	return modelFactory.create({
 		inputParser: data,
 		model: linearModel.create(data.getCoefficients(), data.getIntercept()),
 		name: 'ctpdesktop',
 		wgCountriesVariable: 'wgAdDriverCTPDesktopRabbitCountries',
-		enabled: !!adContext.get('targeting.hasFeaturedVideo') && adContext.get('targeting.skin') === 'oasis',
+		enabled: isFeaturedVideoClickToPlay && adContext.get('targeting.skin') === 'oasis',
 		cachePrediction: true
 	});
 });
