c2dc5e4eb96553d07a772f6e8330a0a907684859
Łukasz Harasimowicz <dev@harnash.eu>
2018-06-04 13:42:40 +0200

    PLATFORM-3525: enable HTTPS on single domain wikis for all logged in users

--- extensions/wikia/HTTPSOptIn/HTTPSOptInHooks.class.php
+++ extensions/wikia/HTTPSOptIn/HTTPSOptInHooks.class.php
@@ -13,25 +13,6 @@ class HTTPSOptInHooks {
 		'wikia' => [ 'Community_Central:Licensing' ]
 	];
 
-	// wikis with ID greater than this threshold will get HTTPS on by default for
-	// logged-in users despite the opt-in preference
-	const WIKI_ID_THRESHOLD_WITH_HTTPS_ON = 100000;
-
-	public static function onGetPreferences( User $user, array &$preferences ): bool {
-		global $wgServer, $wgHTTPSForLoggedInUsers, $wgCityId;
-		if ( empty( $wgHTTPSForLoggedInUsers ) &&
-			$wgCityId <= self::WIKI_ID_THRESHOLD_WITH_HTTPS_ON &&
-			wfHttpsAllowedForURL( $wgServer )
-		) {
-			$preferences['https-opt-in'] = [
-				'type' => 'toggle',
-				'label-message' => 'https-opt-in-toggle',
-				'section' => 'under-the-hood/advanced-displayv2',
-			];
-		}
-		return true;
-	}
-
 	public static function onMercuryWikiVariables( array &$wikiVariables ): bool {
 		$basePath = $wikiVariables['basePath'];
 		$user = RequestContext::getMain()->getUser();
@@ -76,14 +57,7 @@ class HTTPSOptInHooks {
 	}
 
 	private static function httpsAllowed( User $user, string $url ): bool {
-		global $wgHTTPSForLoggedInUsers, $wgCityId;
-		return wfHttpsAllowedForURL( $url ) &&
-			$user->isLoggedIn() &&
-			(
-				!empty( $wgHTTPSForLoggedInUsers ) ||
-				$user->getGlobalPreference( 'https-opt-in', false ) ||
-				$wgCityId > self::WIKI_ID_THRESHOLD_WITH_HTTPS_ON
-			);
+		return wfHttpsAllowedForURL( $url ) && $user->isLoggedIn();
 	}
 
 	private static function httpsEnabledTitle( Title $title ): bool {
