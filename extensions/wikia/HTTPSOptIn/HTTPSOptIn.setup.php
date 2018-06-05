36d3d4cbecc62cde58f41d73228b55226e687cbc
Łukasz Harasimowicz <dev@harnash.eu>
2018-06-05 10:40:08 +0200

    PLATFORM-3525: remove i18n files

--- extensions/wikia/HTTPSOptIn/HTTPSOptIn.setup.php
+++ extensions/wikia/HTTPSOptIn/HTTPSOptIn.setup.php
@@ -2,7 +2,5 @@
 
 $wgAutoloadClasses['HTTPSOptInHooks'] =  __DIR__ . '/HTTPSOptInHooks.class.php';
 
-$wgExtensionMessagesFiles['HTTPSOptIn'] = __DIR__ . '/HTTPSOptIn.i18n.php' ;
-
 $wgHooks['BeforeInitialize'][] = 'HTTPSOptInHooks::onBeforeInitialize';
 $wgHooks['MercuryWikiVariables'][] = 'HTTPSOptInHooks::onMercuryWikiVariables';
