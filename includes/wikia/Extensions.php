e3c384276ab1b2268a59e7daea50227903d3ad25
macbre <maciej.brencz@gmail.com>
2018-06-07 10:26:03 +0200

    SUS-3285 | sunset YouTube extension

--- includes/wikia/Extensions.php
+++ includes/wikia/Extensions.php
@@ -242,11 +242,6 @@ include("{$IP}/extensions/Poem/Poem.php");
 #--- 14. AntiSpamInput - sitewide
 include("{$IP}/extensions/wikia/AntiSpamInput/AntiSpamInput.php");
 
-#--- 16. YouTube -- sitewide (with exceptions)
-if (!empty($wgWikiaEnableYouTubeExt)) {
-	include("{$IP}/extensions/wikia/YouTube/YouTube.php");
-}
-
 if ( !empty( $wgEnableCaptchaExt ) ) {
 	include( "$IP/extensions/wikia/Captcha/Captcha.setup.php" );
 }
