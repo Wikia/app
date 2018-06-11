ef0259e06591b2f4555bdc0816f16616d53cc23a
Sir Jenkins <jenkins@fandom.com>
2018-06-11 18:29:18 +0000

    Automatic translations update

--- extensions/wikia/Oasis/Oasis.i18n.php
+++ extensions/wikia/Oasis/Oasis.i18n.php
@@ -463,7 +463,6 @@ $messages['bs'] = array(
 );
 
 $messages['ca'] = array(
-
 	'oasis-activity-header' => 'Activitat Recent',
 	'oasis-add-photo-to-wiki' => 'Afegeix una imatge al wiki',
 	'oasis-add-photo' => 'Afegir una imatge',
@@ -3913,6 +3912,7 @@ $messages['ru'] = array(
 	'oasis-navigation-v2-edit-this-menu' => 'Править меню вики',
 	'oasis-page-header-diff' => '<strong>Изменения:</strong> $1',
 	'oasis-related-video-by-5min' => 'с сайта 5min Video',
+	'oasis-spotlights-footer-title' => 'Также на ФЭНДОМЕ',
 );
 
 $messages['sr-ec'] = array(
@@ -4506,6 +4506,7 @@ $messages['vi'] = array(
 	'oasis-social-instagram' => 'Instagram',
 	'oasis-social-instagram-link' => 'https://www.instagram.com/getfandom/',
 	'oasis-related-video-by-5min' => 'bởi 5min Video',
+	'oasis-spotlights-footer-title' => 'Cũng trên FANDOM',
 );
 
 $messages['zh-hans'] = array(
