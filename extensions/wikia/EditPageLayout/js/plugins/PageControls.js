8e0f367ac17ee1bdf455ca21ed0e27e9474b5680
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2018-06-13 12:35:06 +0200

    SUS-4895 | readd basic call

--- extensions/wikia/EditPageLayout/js/plugins/PageControls.js
+++ extensions/wikia/EditPageLayout/js/plugins/PageControls.js
@@ -102,7 +102,9 @@
 		},
 
 		// handle "Save" button
-		onSave: function () {
+		onSave: function (event) {
+			event.preventDefault();
+
 			if (this.editor.fire('save') === false) {
 				return;
 			}
@@ -111,10 +113,27 @@
 				this.textarea.val('');
 			}
 
-			this.editor.setState(this.editor.states.SAVING);
+			try {
+				if (window.veTrack) {
+					veTrack({
+						action: 'ck-save-button-click',
+						isDirty: (typeof this.editor.plugins.leaveconfirm === 'undefined' || this.editor.plugins.leaveconfirm.isDirty()) ? 'yes' : 'no'
+					});
+				}
 
-			// block "Publish" button
-			$('#wpSave').attr('disabled', true);
+				this.editor.track({
+					action: Wikia.Tracker.ACTIONS.SUBMIT,
+					label: 'publish'
+				});
+			} finally {
+				this.editor.setState(this.editor.states.SAVING);
+
+				this.editform.off('submit');
+				this.editform.submit();
+
+				// block "Publish" button
+				$('#wpSave').attr('disabled', true);
+			}
 		},
 
 		// handle keypressing in "Edit summary" field
