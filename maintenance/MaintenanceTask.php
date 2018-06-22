219b41b6b28bc6444865615c89ddf318b7b70071
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-06-22 12:05:20 +0200

    SUS-5293 Fix script execution

--- maintenance/MaintenanceTask.php
+++ maintenance/MaintenanceTask.php
@@ -1,7 +1,7 @@
 <?php
 
 class MaintenanceTask extends \Wikia\Tasks\Tasks\BaseTask {
-	public function run( $cmd ) {
-		return shell_exec( $cmd['script'] );
+	public function run( $script ) {
+		return shell_exec( $script );
 	}
 }
