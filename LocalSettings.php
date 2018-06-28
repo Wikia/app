310d5f216571aa2afbf0dba2b9be0a3e5a12a438
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2018-06-28 16:48:51 +0200

    SUS-2952 | Apache on Kubernetes

--- LocalSettings.php
+++ LocalSettings.php
@@ -49,10 +49,9 @@ if ( empty( $wgWikiaEnvironment ) ) {
 
 /**
  * Temporary variable for Kubernetes migration
- * If {@code true}, then logs will be sent to the configured socket address formatted as JSON, instead of using syslog.
+ * If {@code true}, then logs will be sent to stdout formatted as JSON, instead of using syslog.
  */
-$wgLoggerLogToSocketOnly = $_ENV['LOG_SOCKET_ONLY'] ?? false;
-$wgLoggerSocketAddress = $_ENV['LOG_SOCKET_ADDRESS'] ?? 'tcp://127.0.0.1:9999';
+$wgLoggerLogToStdOutOnly = $_ENV['LOG_STDOUT_ONLY'] ?? false;
 
 /**
  * Some environments share components (e.g. preview, verify, sandbox and stable
--- lib/Wikia/src/Logger/LoggerFactory.php
+++ lib/Wikia/src/Logger/LoggerFactory.php
@@ -1,19 +1,20 @@
 <?php
 namespace Wikia\Logger;
 
-use Monolog\Handler\SocketHandler;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
 class LoggerFactory {
 
 	/** @var bool $shouldLogToStandardOutput */
-	private $shouldLogToSocket;
+	private $shouldLogToStandardOutput;
 
 	/** @var bool $shouldExcludeDebugLevel */
 	private $shouldExcludeDebugLevel;
 
-	/** @var string $socketAddress */
-	private $socketAddress;
+	/** @var string $mwLogFile */
+	private $mwLogFile;
 
 	/** @var StatusProcessor $statusProcessor */
 	private $statusProcessor;
@@ -28,51 +29,55 @@ class LoggerFactory {
 	 * @return LoggerFactory
 	 */
 	public static function getInstance(): LoggerFactory {
-		if( static::$instance === null ) {
-			global $wgWikiaEnvironment, $wgLoggerLogToSocketOnly, $wgLoggerSocketAddress;
+		if ( static::$instance === null ) {
+			global $wgWikiaEnvironment, $wgLoggerLogToStdOutOnly;
 
-			static::$instance = new self( $wgLoggerLogToSocketOnly, $wgWikiaEnvironment === WIKIA_ENV_PROD, $wgLoggerSocketAddress );
+			static::$instance = new self( $wgLoggerLogToStdOutOnly, $wgWikiaEnvironment === WIKIA_ENV_PROD );
 		}
 
 		return self::$instance;
 	}
 
-	public function __construct( bool $shouldLogToSocket, bool $shouldExcludeDebugLevel, string $socketAddress ) {
-		$this->shouldLogToSocket = $shouldLogToSocket;
+	public function __construct( bool $shouldLogToStandardOutput,
+								 bool $shouldExcludeDebugLevel, string $mwLogFile = '/var/log/mediawiki.log' ) {
+		$this->shouldLogToStandardOutput = $shouldLogToStandardOutput;
 		$this->shouldExcludeDebugLevel = $shouldExcludeDebugLevel;
-		$this->socketAddress = $socketAddress;
+		$this->mwLogFile = $mwLogFile;
 	}
 
 	public function getLogger( string $ident ): Logger {
-		if( isset( $this->loggers[ $ident ] ) ) {
-			return $this->loggers[ $ident ];
+		if ( isset( $this->loggers[$ident] ) ) {
+			return $this->loggers[$ident];
 		}
 
 		$logger = new Logger( $ident );
 
-		if( $this->shouldLogToSocket ) {
-			$handler = new SocketHandler( $this->socketAddress );
+		if ( $this->shouldLogToStandardOutput ) {
+			$stdout = fopen( 'php://stdout', 'w' );
+			$handler = new StreamHandler( $stdout );
+			$handler->setFormatter( new JsonFormatter( JsonFormatter::BATCH_MODE_NEWLINES ) );
+
+			$logger->pushProcessor( new AppNameProcessor( $ident ) );
 		} else {
 			$handler = new SyslogHandler( $ident );
+			$handler->setFormatter( new LogstashFormatter( $ident ) );
 		}
 
-		$handler->setFormatter( new LogstashFormatter( $ident ) );
-
-		if( $this->shouldExcludeDebugLevel ) {
+		if ( $this->shouldExcludeDebugLevel ) {
 			$handler->setLevel( Logger::INFO );
 		}
 
 		$logger->pushHandler( $handler );
 		$logger->pushProcessor( $this->getStatusProcessor() );
 
-		return ( $this->loggers[ $ident ] = $logger );
+		return ( $this->loggers[$ident] = $logger );
 	}
 
 	/**
 	 * @return StatusProcessor
 	 */
 	private function getStatusProcessor(): StatusProcessor {
-		if( $this->statusProcessor === null ) {
+		if ( $this->statusProcessor === null ) {
 			$this->statusProcessor = new StatusProcessor();
 		}
 
