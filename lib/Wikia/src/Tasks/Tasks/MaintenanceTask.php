8f35fdf2cfa443b5230bb2e6b88d8be53ee54128
JamesHawking <jakubj91@gmail.com>
2018-07-03 14:41:55 +0200

    SUS-5462 | Use WikiaLogger to catch exceptions

--- lib/Wikia/src/Tasks/Tasks/MaintenanceTask.php
+++ lib/Wikia/src/Tasks/Tasks/MaintenanceTask.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace Wikia\Tasks\Tasks;
+use Wikia\Logger\WikiaLogger;
 
 class MaintenanceTask extends BaseTask {
 	public function run( $script ) {
-		$exitStatus = 0;
-		wfShellExec($script, $exitStatus);
+		try {
+			$exitStatus = 0;
+			wfShellExec( $script, $exitStatus );
+		} catch (Exception $e) {
+			WikiaLogger::instance()->error( $script.' failed', [
+				'exception' => $e,
+			] );
+		}
 
 		return;
 	}
