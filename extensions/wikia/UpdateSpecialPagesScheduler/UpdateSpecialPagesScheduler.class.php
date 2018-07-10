698d7fece18ebccd13864d8ab4121ad3baf163ee
macbre <maciej.brencz@gmail.com>
2018-07-10 11:44:28 +0200

    UpdateSpecialPagesScheduler - do schedule the task and show ID to the user

--- extensions/wikia/UpdateSpecialPagesScheduler/UpdateSpecialPagesScheduler.class.php
+++ extensions/wikia/UpdateSpecialPagesScheduler/UpdateSpecialPagesScheduler.class.php
@@ -1,9 +1,23 @@
 <?php
 
+use Wikia\Tasks\Tasks\MaintenanceTask;
+use Wikia\Tasks\Queues\ScheduledMaintenanceQueue;
+
 class UpdateSpecialPagesScheduler {
 
 	private static function isAllowed( User $user ) : bool {
 		return $user->isAllowed( 'schedule-update-special-pages' );
+	}
+
+	private static function doSchedule( int $wikiId ) : string {
+		$task = new MaintenanceTask();
+		$task->call( 'run', 'maintenance/updateSpecialPages.php', '--quiet' );
+		$taskId = $task
+			->setQueue(ScheduledMaintenanceQueue::NAME)
+			->wikiId( $wikiId )
+			->queue();
+
+		return $taskId;
 	}
 
 	/**
@@ -24,8 +38,12 @@ class UpdateSpecialPagesScheduler {
 		     $request->getVal('updateSpecialPagesScheduler') &&
 		     $context->getUser()->matchEditToken( $request->getVal( 'editToken' ) ) ) {
 
-			// TODO: schedule a script
-			$text = Wikia::successbox( $page->msg( 'update-special-pages-scheduler-requested' ) ) . $text;
+			global $wgCityId;
+			$taskId = self::doSchedule( $wgCityId );
+
+			$text = Wikia::successbox(
+				$page->msg( 'update-special-pages-scheduler-requested', $taskId )
+			) . $text;
 		}
 
 		$tpl = new EasyTemplate(__DIR__ . '/templates');
