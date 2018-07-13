08e9d8b4cad91a7b510acab4e9f5a61bd88de212
hakubo <bukaj.kelo+github@gmail.com>
2018-07-13 10:14:08 +0200

    update doc, cronjob->cronjobs

--- docker/maintenance/cronjobs-generator.sh
+++ docker/maintenance/cronjobs-generator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-## usage ./cronjob-generator.sh
+## usage ./cronjobs-generator.sh
 ## This should generate all cron jobs that can be fed to kubectl
-## e.g. ./cronjob-generator.sh | kubectl --context context -n environment apply -f -
+## e.g. ./cronjobs-generator.sh | kubectl --context context -n environment apply -f -
 
 SCRIPT_FOLDER=$(dirname $(readlink -f $0))
 JOB_DESCRIPTIONS=(`ls $SCRIPT_FOLDER | grep -v 'example' | grep -v 'cronjob-template.yaml' | grep '\.yaml$'`)
