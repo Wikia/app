de03bbc8cde0a1d9642a1c2da105d5031d420c8f
macbre <maciej.brencz@gmail.com>
2018-07-16 13:21:21 +0200

    SUS-5523 | random sotring did not work for a while
    Let's just stop sorting Insights data for Special:Community and get rid of PHP notice flood:
    
    PHP Notice: Undefined index: random in /usr/wikia/slot1/24088/src/extensions/wikia/InsightsV2/helpers/InsightsSorting.php on line 59

--- extensions/wikia/InsightsV2/services/InsightsService.class.php
+++ extensions/wikia/InsightsV2/services/InsightsService.class.php
@@ -2,7 +2,7 @@
 
 class InsightsService {
 
-	public function getInsightPagesForModel( InsightsModel $model, $size, $sortingType ): array {
+	public function getInsightPagesForModel( InsightsModel $model, $size ): array {
 		$insightData = ( new InsightsContext( $model ) )->fetchData();
 
 		if ( empty( $insightData ) ) {
@@ -11,37 +11,10 @@ class InsightsService {
 
 		$insightCount = count( $insightData );
 
-		if ( empty( $sortingType ) ) {
-			return [
-				'count' => $insightCount,
-				'pages' => array_slice( $insightData, 0, $size )
-			];
-		}
-
-		$sortedInsightArticleIds = ( new InsightsSorting( $model->getConfig() ) )
-			->getSortedData(
-				$insightData,
-				[ 'sort' => $sortingType ]
-			);
-
-		$articleIds = array_slice( $sortedInsightArticleIds, 0, $size );
-
 		return [
 			'count' => $insightCount,
-			'pages' => $this->getArticlesData( $insightData, $articleIds )
+			'pages' => array_slice( $insightData, 0, $size )
 		];
 	}
 
-	/**
-	 * @param array $articles all articles data
-	 * @param array $ids list of article ids we need data for
-	 * @return array
-	 */
-	private function getArticlesData( $articles, $ids ) {
-		$content = [];
-		foreach ( $ids as $id ) {
-			$content[] = $articles[$id];
-		}
-		return $content;
-	}
 }
