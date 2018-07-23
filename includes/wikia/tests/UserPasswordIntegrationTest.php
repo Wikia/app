2fc90a12e4ad1dbaa94a0ea4d1023ad98737c2d9
chessky <adamczewski@gmail.com>
2018-07-23 15:30:59 +0200

    Test test

---
+++ includes/wikia/tests/UserPasswordIntegrationTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: chessky
+ * Date: 23.07.18
+ * Time: 14:02
+ */
+
+class UserPasswordIntegrationTest extends WikiaBaseTest
+{
+	public function testCorrectPassword() {
+		$this->assertFalse( true );
+	}
+//	$dataProvider = [
+//		'correct' => [ 'CTest50', 'q' ],
+//		'wrong' => [ 'CTest50', 'p' ],
+//		'pwchange' => [ 'user3', 'oldpw', 'newpw' ]
+//		];
+//
+//	$u = User::newFromName( $dataProvider['correct'][0] );
+//	$result = $u->checkPassword( $dataProvider['correct'][1] );
+//
+//		if ( ! $result->checkStatus() ) {
+//			// service unavailable
+//		}
+//
+//		if ( ! $result->success() ) {
+//			// authentication failed, but expected to succeed
+//		}
+
+// test passed
+
+}
