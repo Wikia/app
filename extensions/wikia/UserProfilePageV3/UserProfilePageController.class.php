e497eca4b4ea69e0f2c7d95b596c294321ffa08b
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2018-07-25 19:02:43 +0200

    SRE-91 | don't return a 204

--- extensions/wikia/UserProfilePageV3/UserProfilePageController.class.php
+++ extensions/wikia/UserProfilePageV3/UserProfilePageController.class.php
@@ -351,8 +351,6 @@ class UserProfilePageController extends WikiaController {
 		$user->setGlobalAttribute( AVATAR_USER_OPTION_NAME, $avatar );
 		$user->saveAttributes();
 		$user->invalidateCache();
-
-		$this->response->setCode( 204 );
 	}
 
 	/**
