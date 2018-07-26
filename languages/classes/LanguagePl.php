a2764b92e5a122e6662eaa28f8e9ec4aed08ae8d
Nanaki <NanakiPL@users.noreply.github.com>
2018-07-27 01:41:07 +0200

    IRIS-4093 auto-declension for Polish sitenames

--- languages/classes/LanguagePl.php
+++ languages/classes/LanguagePl.php
@@ -39,4 +39,56 @@ class LanguagePl extends Language {
 			return $_;
 		}
 	}
+    
+	/**
+	 * Grammatical transformations, needed for inflected languages
+	 * Invoked by putting {{grammar:case|word}} in a message
+	 *
+	 * @param $word string
+	 * @param $case string
+	 * @return string
+	 */
+    function convertGrammar( $word, $case ) {
+		global $wgGrammarForms;
+		if ( isset( $wgGrammarForms['pl'][$case][$word] ) ) {
+			return $wgGrammarForms['pl'][$case][$word];
+		}
+		
+		# Declension for 'pedia' and 'wikia' in sitenames. It's not perfect but it'll take care of cases that aren't deliberately wonky.
+		if ( !preg_match( "/Wiki$/us", $word ) ) {
+			$ar = array();
+			if ( preg_match_all( '/pedia\b|wikia\b/ius', $word, $ar, PREG_OFFSET_CAPTURE) ) {
+				$offset = end($ar[0])[1]+4;
+				switch ( $case ) {
+					case 'D.lp': # Dopełniacz
+					case 'C.lp': # Celownik
+					case 'MS.lp': # Miejscownik
+						if ( $word[$offset] == 'a' )
+							$word[$offset] = 'i';
+						elseif ( $word[$offset] == 'A' )
+							$word[$offset] = 'I';
+						break;
+					case 'B.lp': # Biernik
+						if ( $word[$offset] == 'a' )
+							$word[$offset] = 'ę';
+						elseif ( $word[$offset] == 'A' )
+							$word[$offset] = 'Ę';
+						break;
+					case 'N.lp':  # Narzędnik
+						if ( $word[$offset] == 'a' )
+							$word[$offset] = 'ą';
+						elseif ( $word[$offset] == 'A' )
+							$word[$offset] = 'Ą';
+						break;
+					case 'W.lp': # Wołacz
+						if ( $word[$offset] == 'a' )
+							$word[$offset] = 'o';
+						elseif ( $word[$offset] == 'A' )
+							$word[$offset] = 'O';
+						break;
+				}
+			}
+		}
+		return $word;
+	}
 }
