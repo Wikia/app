ed76c1fd27bb57c6079488b8ca31d7bee89bdbb7
grunny <mwgrunny@gmail.com>
2018-08-02 16:50:52 +1000

    (PLATFORM-3616) Make external links to other wikis protocol-relative
    Make external links to other wikis protocol-relative for URLs to communities
    that support HTTPS. This means we won't need users who paste URLs to other wikis
    to update them.

--- extensions/wikia/HTTPSSupport/HTTPSSupport.setup.php
+++ extensions/wikia/HTTPSSupport/HTTPSSupport.setup.php
@@ -4,6 +4,7 @@ $wgAutoloadClasses['HTTPSSupportHooks'] =  __DIR__ . '/HTTPSSupportHooks.class.p
 
 $wgHooks['AfterInitialize'][] = 'HTTPSSupportHooks::onAfterInitialize';
 $wgHooks['MercuryWikiVariables'][] = 'HTTPSSupportHooks::onMercuryWikiVariables';
-$wgHooks['outputMakeExternalImage'][] = 'HTTPSSupportHooks::parserUpgradeVignetteUrls';
 $wgHooks['SitemapPageBeforeOutput'][] = 'HTTPSSupportHooks::onSitemapRobotsPageBeforeOutput';
 $wgHooks['WikiaRobotsBeforeOutput'][] = 'HTTPSSupportHooks::onSitemapRobotsPageBeforeOutput';
+$wgHooks['outputMakeExternalImage'][] = 'HTTPSSupportHooks::parserUpgradeVignetteUrls';
+$wgHooks['LinkerMakeExternalLink'][] = 'HTTPSSupportHooks::onLinkerMakeExternalLink';
