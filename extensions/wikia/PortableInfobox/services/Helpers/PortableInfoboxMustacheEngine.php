4f23332a9e9f16fe16ce8622dc6945efdba95a46
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-08-02 10:22:49 +0200

    XW-5118 | Fixing typo

--- extensions/wikia/PortableInfobox/services/Helpers/PortableInfoboxMustacheEngine.php
+++ extensions/wikia/PortableInfobox/services/Helpers/PortableInfoboxMustacheEngine.php
@@ -12,7 +12,7 @@ class PortableInfoboxMustacheEngine {
 		'wrapper' => 'PortableInfoboxWrapper.mustache',
 		'title' => 'PortableInfoboxItemTitle.mustache',
 		'header' => 'PortableInfoboxItemHeader.mustache',
-		'mobile-header' => 'PortableInfoboxItemHeaderMobile.mustache',
+		'header-mobile' => 'PortableInfoboxItemHeaderMobile.mustache',
 		'image' => 'PortableInfoboxItemImage.mustache',
 		'image-mobile' => 'PortableInfoboxItemImageMobile.mustache',
 		'image-mobile-wikiamobile' => 'PortableInfoboxItemImageMobileWikiaMobile.mustache',
