3d0a67bb96ee68f42224838f17f770b76a3f2e41
mech <wozniak.jacek@gmail.com>
2018-08-14 13:14:52 +0200

    PLATFORM-3615: display sitemaps on old domains

--- extensions/wikia/SitemapXml/SitemapHooks.class.php
+++ extensions/wikia/SitemapXml/SitemapHooks.class.php
@@ -12,4 +12,17 @@ class SitemapHooks {
 		return true;
 	}
 
+	public static function onTestCanonicalRedirect( WebRequest $request, Title $title, OutputPage $output ) {
+		// Question 1: do we cancel a redirect for all alternative domain?
+		$output->cancelRedirect( false );
+		if ( $output->isRedirect() ) {
+			// there is still a protocol redirect left, proceed with redirecting
+			return true;
+		}
+		header('X-sitemaps-redirect-cancelled: 1');
+		// TBD: override the wgServer so the sitemap uses currently used address
+		// this needs to be done because WFL used the address from city_url
+		return false;
+	}
+
 }
