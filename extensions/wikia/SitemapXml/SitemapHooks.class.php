a76ca74a9fc23f5650dbbe40d9451459cb1a4100
grunny <mwgrunny@gmail.com>
2018-08-17 21:33:56 +1000

    (PLATFORM-3615) Only cancel the redirect for the HTTP wikia.com version

--- extensions/wikia/SitemapXml/SitemapHooks.class.php
+++ extensions/wikia/SitemapXml/SitemapHooks.class.php
@@ -17,22 +17,37 @@ class SitemapHooks {
 	}
 
 	public static function onTestCanonicalRedirect( WebRequest $request, Title $title, OutputPage $output ) {
-		if ( !self::isSitemapTitle( $title ) ) {
+		global $wgServer, $wgWikiaBaseDomain, $wgFandomBaseDomain;
+
+		if ( !self::isSitemapTitle( $title ) || WebRequest::detectProtocol() !== 'http' ) {
 			return true;
 		}
-		// Question 1: do we cancel a redirect for all alternative domains?
-		$output->cancelRedirect( false );
-		if ( $output->isRedirect() ) {
-			// there is still a protocol redirect left, proceed with redirecting
+
+		$currentUri = \WikiFactoryLoader::getCurrentRequestUri( $_SERVER, true, true );
+		$currentHost = parse_url( $currentUri, PHP_URL_HOST );
+
+		// We only want to deal with plain HTTP versions of the wikia.com domain
+		if ( strpos( $currentHost, ".{$wgWikiaBaseDomain}" ) === false ) {
 			return true;
 		}
+
+		// Only cancel the redirect if we're redirecting to the fandom.com version of the
+		// current URL
+		$targetUrl = wfExpandUrl( $title->getFullURL(), PROTO_CURRENT );
+		$targetHost = parse_url( $targetUrl, PHP_URL_HOST );
+		if ( strpos( $targetHost, ".{$wgFandomBaseDomain}" ) === false ||
+			str_replace( ".{$wgFandomBaseDomain}", ".{$wgWikiaBaseDomain}", $targetHost ) !== $currentHost
+		) {
+			return true;
+		}
+
+		$output->cancelRedirect();
+
 		$request->response()->header( 'X-sitemaps-redirect-cancelled: 1' );
+
 		// Override the wgServer so the sitemap uses host from the current request.
 		// This needs to be done because WFL used the address from city_url
-		global $wgServer;
-		$currentUri = \WikiFactoryLoader::getCurrentRequestUri( $_SERVER, true, true );
-		$parsed = parse_url( $currentUri );
-		$wgServer = $parsed['scheme'] . '://' . $parsed['host'];	// TBD: need to care about language path?
+		$wgServer = "http://{$currentHost}";
 		return false;
 	}
 
