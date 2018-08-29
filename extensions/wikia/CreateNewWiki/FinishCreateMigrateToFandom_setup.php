d9ad57c87dea14a2f11fef00a8116f3dc42658fb
mech <wozniak.jacek@gmail.com>
2018-08-29 17:55:39 +0200

    PLATFORM-3647: remove unused code lines

--- extensions/wikia/CreateNewWiki/FinishCreateMigrateToFandom_setup.php
+++ extensions/wikia/CreateNewWiki/FinishCreateMigrateToFandom_setup.php
@@ -3,8 +3,6 @@
 // PLATFORM-3647 temp hack to be removed after the switch
 // On the first article view, migrate the wiki to fandom.com domain
 
-$dir = dirname(__FILE__).'/';
-
 function wfMigrateWikiToFandomCom($title, $article, $output, $user, WebRequest $request, $wiki) {
 	global $wgCityId, $wgServer, $wgWikiaBaseDomain, $wgFandomBaseDomain, $wgMigrateNewWikiToFandomCom;
 	if ( $wgMigrateNewWikiToFandomCom )	{ // sanity check
