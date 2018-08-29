6e5bedf6e5f5dfa1a0ab794bc3ce35320cd804be
mech <wozniak.jacek@gmail.com>
2018-08-29 13:01:43 +0200

    PLATFORM-3647: use old main page title

--- extensions/wikia/CreateNewWiki/maintenance/setMainPageContent.php
+++ extensions/wikia/CreateNewWiki/maintenance/setMainPageContent.php
@@ -9,19 +9,27 @@ require_once( __DIR__ . '/../../../../maintenance/Maintenance.php' );
 class SetMainPageContent extends Maintenance {
 
 	public function execute() {
-		global $wgSitename, $wgWikiDescription;
+		global $wgWikiDescription;
 
 		$this->output( "SetMainPageContent: Started" );
-		$this->output( "SetMainPageContent: sitename is " . $wgSitename );
 		$this->output( "SetMainPageContent: description is " . $wgWikiDescription );
 		if ( empty( $wgWikiDescription ) ) {
 			$this->output( "SetMainPageContent: Empty wiki description, skipping" );
 			return;
 		}
-		// set description on main page
 
-		$mainTitle = Title::newFromText( $wgSitename );
-		$mainId = $mainTitle->getArticleID();
+		// this runs before the StartPostCreationTasks so main page is available under the old address
+		$mainTitle = Title::newFromText( wfMsgForContent( 'Mainpage' ) );
+		if ( !$mainTitle ) {
+			$mainTitle = Title::newFromText( 'Main_Page' );
+			if ( !$mainTitle ) {
+				$this->error( "SetMainPageContent: Cannot find the main title!" );
+				return;
+			}
+		}
+		$this->output( "SetMainPageContent: mainTitle " . $mainTitle->getText() );
+		$mainId = $mainTitle->getArticleID( Title::GAID_FOR_UPDATE );
+		$this->output( "SetMainPageContent: mainId " . $mainId );
 		$mainArticle = Article::newFromID( $mainId );
 
 		if ( empty( $mainArticle ) ) {
@@ -29,6 +37,7 @@ class SetMainPageContent extends Maintenance {
 			return;
 		}
 
+		// set description on main page
 		$newMainPageText = $this->getClassicMainPage( $mainArticle, $wgWikiDescription );
 		$mainArticle->doEdit( $newMainPageText, '' );
 
@@ -42,7 +51,7 @@ class SetMainPageContent extends Maintenance {
 	 * @return string - main page article wiki text
 	 */
 	private function getClassicMainPage( $mainArticle, $description ) {
-		global $wgParser, $wgSitename, $wgWikiDescription;
+		global $wgParser, $wgSitename;
 
 		$mainPageText = $mainArticle->getRawText();
 		$matches = [];
