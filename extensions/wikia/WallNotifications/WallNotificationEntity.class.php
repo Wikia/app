f748e08202e55819b721c9bdc0b236ac6940d8e7
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-09-03 16:28:42 +0200

    XW-5224: comment

--- extensions/wikia/WallNotifications/WallNotificationEntity.class.php
+++ extensions/wikia/WallNotifications/WallNotificationEntity.class.php
@@ -159,6 +159,8 @@ class WallNotificationEntity {
 			$this->parentTitleDbKey = $response['parentTitleDbKey'];
 			$this->msgText = $response['msgText'];
 			$this->threadTitleFull = $response['threadTitleFull'];
+			// The data property is treated in further processing as an object but HTTP response contains it in form of
+			// array, therefore casting is needed here
 			$this->data = (object) $response['data'];
 
 			return true;
