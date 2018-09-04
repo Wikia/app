a8c546957e033cbb2dafa6a817f2199095c46fc5
rogatty <igor@wikia-inc.com>
2018-09-04 12:42:45 +0200

    SEO-657 | sunset SeoLinkHreflang extension

--- includes/wikia/Extensions.php
+++ includes/wikia/Extensions.php
@@ -1682,10 +1682,6 @@ if ( !empty( $wgEnableRobotsTxtExt ) ) {
 	include( "$IP/extensions/wikia/RobotsTxt/RobotsTxt.setup.php" );
 }
 
-if ( !empty( $wgEnableSeoLinkHreflangExt ) ) {
-	include "$IP/extensions/wikia/SeoLinkHreflang/SeoLinkHreflang.setup.php";
-}
-
 if ( !empty( $wgEnableSitemapPageExt ) ) {
 	include( "$IP/extensions/wikia/SitemapPage/SitemapPage.setup.php" );
 }
