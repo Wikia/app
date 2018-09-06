d02b6a665bf702845173890a9eabd47a4d5e94f8
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-09-06 15:01:58 +0200

    added  to setup

--- extensions/wikia/MarkWikiAsClosed/MarkWikiAsClosed.setup.php
+++ extensions/wikia/MarkWikiAsClosed/MarkWikiAsClosed.setup.php
@@ -1,4 +1,12 @@
 <?php
+$wgExtensionCredits['parserhook'][] = [
+    'name' => 'Mark Wiki As Closed',
+    'author' => [
+        'bpiatek',
+    ],
+    'version' => '1',
+    'url' => 'https://github.com/Wikia/app/tree/dev/extensions/wikia/MarkWikiAsClosed',
+];
 
-// load classes
+// load classes controller
 $wgAutoloadClasses['MarkWikiAsClosedController'] = __DIR__ . '/controllers/MarkWikiAsClosedController.class.php';
