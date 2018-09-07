40504f90c137e7d0bbf9a6e0bfad3298fac7895f
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-09-07 10:00:34 +0200

    test

--- extensions/wikia/MarkWikiAsClosed/controllers/MarkWikiAsClosedController.class.php
+++ extensions/wikia/MarkWikiAsClosed/controllers/MarkWikiAsClosedController.class.php
@@ -9,20 +9,22 @@ class MarkWikiAsClosedController extends WikiaController {
         $this->response->setFormat( WikiaResponse::FORMAT_JSON );
     }
 
-    public function markWikiAsClosed() {
-        $wikiId = $this->request->getVal(self::WIKI_ID);
-        $reason = $this->request->getVal(self::REASON);
+    public static function markWikiAsClosed() {
 
-        if ( empty( $wikiId) || empty($reason)) {
-            throw new BadRequestException();
-        }
-
-        $res = WikiFactory::setPublicStatus( WikiFactory::CLOSE_ACTION, $wikiId, $reason );
-
-        if ( $res === WikiFactory::CLOSE_ACTION ) {
-            WikiFactory::setFlags( $wikiId, WikiFactory::FLAG_FREE_WIKI_URL | WikiFactory::FLAG_CREATE_DB_DUMP | WikiFactory::FLAG_CREATE_IMAGE_ARCHIVE );
-            WikiFactory::clearCache( $wikiId );
-        }
+        \Wikia\Logger\WikiaLogger::instance()->info("++++++++++ TEST");
+//        $wikiId = $this->request->getVal(self::WIKI_ID);
+//        $reason = $this->request->getVal(self::REASON);
+//
+//        if ( empty( $wikiId) || empty($reason)) {
+//            throw new BadRequestException();
+//        }
+//
+//        $res = WikiFactory::setPublicStatus( WikiFactory::CLOSE_ACTION, $wikiId, $reason );
+//
+//        if ( $res === WikiFactory::CLOSE_ACTION ) {
+//            WikiFactory::setFlags( $wikiId, WikiFactory::FLAG_FREE_WIKI_URL | WikiFactory::FLAG_CREATE_DB_DUMP | WikiFactory::FLAG_CREATE_IMAGE_ARCHIVE );
+//            WikiFactory::clearCache( $wikiId );
+//        }
     }
 
     public function allowsExternalRequests() {
