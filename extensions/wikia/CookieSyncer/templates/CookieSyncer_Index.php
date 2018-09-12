3cfb76e6a9782df83eb9c2949d7480b3ad210ad4
Łukasz Harasimowicz <dev@harnash.eu>
2018-09-12 14:51:12 +0200

    PLATFORM-3677: call cookie sync only once

--- extensions/wikia/CookieSyncer/templates/CookieSyncer_Index.php
+++ extensions/wikia/CookieSyncer/templates/CookieSyncer_Index.php
@@ -1,1 +1,9 @@
-<iframe src="<?= $url; ?>" class="cookie-syncer-module-iframe"></iframe>
+<script type="text/javascript">
+	if (window.document.cookie.indexOf('cookiesync_done') === -1) {
+		var iframe = document.createElement('iframe');
+		iframe.style.display = "none";
+		iframe.src = "$url";
+		iframe.classList.add("cookie-syncer-module-iframe");
+		document.body.appendChild(iframe);
+	}
+</script>
