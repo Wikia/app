132cddb1d14bd6fc9dc736ca9853c0b0e686b13c
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-09-12 11:35:16 +0200

    rewrite controller which is now working

--- extensions/wikia/MarkWikiAsClosed/controllers/MarkWikiAsClosedController.class.php
+++ extensions/wikia/MarkWikiAsClosed/controllers/MarkWikiAsClosedController.class.php
@@ -10,19 +10,41 @@ class MarkWikiAsClosedController extends WikiaController {
     const REASON = 'reason';
 
     public function init() {
+        $this->assertCanAccessController();
         $this->response->setFormat(WikiaResponse::FORMAT_JSON);
     }
 
     public function markWikiAsClosed() {
+        try {
+            $this->markWikiClosed();
+        } catch (WikiaBaseException $exception) {
+            // This is normally done in WikiaDispatcher::dispatch(),
+            // but because it's an internal request, we would return 200 and only log error to ELK stack
+            $this->response->setCode( $exception->getCode() );
+            $this->response->setVal(
+                'exception',
+                [
+                    'message' => $exception->getMessage(),
+                    'code' => $exception->getCode()
+                ]
+            );
+        }
+    }
 
+    private function markWikiClosed() {
         $wikiId = $this->request->getVal(self::WIKI_ID);
         $reason = $this->request->getVal(self::REASON);
-
         if (empty($wikiId) || empty($reason)) {
             throw new BadRequestException();
         }
 
-        $res = WikiFactory::setPublicStatus(WikiFactory::CLOSE_ACTION, $wikiId, $reason);
+        try {
+            $res = WikiFactory::setPublicStatus(WikiFactory::CLOSE_ACTION, $wikiId, $reason);
+        } catch (Solarium_Client_HttpException $exception) {
+            // This is horrible hack due to Solar not working on dev
+            $this->info("Solar not working properly");
+        }
+
 
         if ($res === WikiFactory::CLOSE_ACTION) {
             WikiFactory::setFlags($wikiId, WikiFactory::FLAG_FREE_WIKI_URL | WikiFactory::FLAG_CREATE_DB_DUMP | WikiFactory::FLAG_CREATE_IMAGE_ARCHIVE);
@@ -32,8 +54,16 @@ class MarkWikiAsClosedController extends WikiaController {
         }
     }
 
-    public function allowsExternalRequests() {
-        return false;
+    /**
+     * Make sure to only allow authorized POST methods.
+     * @throws WikiaHttpException
+     */
+    public function assertCanAccessController() {
+        if (!$this->request->isInternal()) {
+            throw new ForbiddenException('Access to this controller is restricted');
+        }
+        if (!$this->request->wasPosted()) {
+            throw new MethodNotAllowedException('Only POST allowed');
+        }
     }
-
 }
