a01708369f93ac557b94e4eb92530be300c538da
Łukasz Harasimowicz <dev@harnash.eu>
2018-09-12 15:02:01 +0200

    PLATFORM-3667: fix cookie sync iframe url

--- extensions/wikia/CookieSyncer/templates/CookieSyncer_Index.php
+++ extensions/wikia/CookieSyncer/templates/CookieSyncer_Index.php
@@ -2,7 +2,7 @@
 	if (window.document.cookie.indexOf('cookiesync_done') === -1) {
 		var iframe = document.createElement('iframe');
 		iframe.style.display = "none";
-		iframe.src = "$url";
+		iframe.src = "<?= $url; ?>";
 		iframe.classList.add("cookie-syncer-module-iframe");
 		document.body.appendChild(iframe);
 	}
