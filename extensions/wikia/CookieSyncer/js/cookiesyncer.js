5259cdd8dc2976fe4698d2f605050289873b2955
Łukasz Harasimowicz <dev@harnash.eu>
2018-09-13 14:50:21 +0200

    PLATFORM-3667: removed extra styling to the iframe

--- extensions/wikia/CookieSyncer/js/cookiesyncer.js
+++ extensions/wikia/CookieSyncer/js/cookiesyncer.js
@@ -6,7 +6,6 @@ require([
 
 	if (cookie.get('cookiesync_done') === null && cookie.get('tracking-opt-in-status') !== null) {
 		var iframe = window.document.createElement('iframe');
-		iframe.style.display = "none";
 		iframe.src = mw.config.get('wgCookieSyncerApiUrl');;
 		iframe.classList.add("cookie-syncer-module-iframe");
 		window.document.body.appendChild(iframe);
