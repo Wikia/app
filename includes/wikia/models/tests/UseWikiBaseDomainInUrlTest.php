d8c3019ebf4733d6de083a393093e046f00aade7
grunny <mwgrunny@gmail.com>
2018-09-20 17:48:39 +1000

    Remove unused variable from tests

--- includes/wikia/models/tests/UseWikiBaseDomainInUrlTest.php
+++ includes/wikia/models/tests/UseWikiBaseDomainInUrlTest.php
@@ -1,22 +1,20 @@
 <?php
 
-class UseWikiBaseDomainInUrlTest extends WikiaBaseTest
-{
+class UseWikiBaseDomainInUrlTest extends WikiaBaseTest {
 	/**
 	 * @dataProvider getUrlProvider
 	 *
 	 * @param $server
 	 * @param $devDomain
 	 * @param $url string source url
-	 * @param $cityId mixed Wiki Id
 	 * @param $expectedResult string
 	 */
-	public function testGetHref( $server, $devDomain, $url, $cityId, $expectedResult ) {
-		$model = new DesignSystemGlobalNavigationModelV2(DesignSystemGlobalFooterModel::PRODUCT_WIKIS, 1 );
-		$this->mockGlobalVariable('wgServer', $server);
+	public function testGetHref( $server, $devDomain, $url, $expectedResult ) {
+		$model = new DesignSystemGlobalNavigationModelV2( DesignSystemGlobalFooterModel::PRODUCT_WIKIS, 1 );
+		$this->mockGlobalVariable( 'wgServer', $server );
 		$this->mockGlobalVariable( 'wgDevDomain', $devDomain );
 
-		$result = $model->useWikiBaseDomainInUrl( $url, $cityId );
+		$result = $model->useWikiBaseDomainInUrl( $url );
 
 		$this->unsetGlobals();
 		$this->assertEquals( $expectedResult, $result );
@@ -28,49 +26,37 @@ class UseWikiBaseDomainInUrlTest extends WikiaBaseTest
 				'https://muppet.wikia.com',
 				'',
 				'https://www.wikia.com/signin',
-				null,
 				'https://www.wikia.com/signin'
-
 			],
 			[
 				'https://muppet.fandom.com',
 				'',
 				'https://www.wikia.com/signin',
-				null,
 				'https://www.fandom.com/signin'
-
 			],
 			[
 				'https://muppet.wladek.wikia-dev.pl',
 				'wladek.wikia-dev.pl',
 				'https://www.wikia.com/signin',
-				null,
 				'https://www.wladek.wikia-dev.pl/signin'
-
 			],
 			[
 				'https://muppet.wladek.fandom-dev.pl',
 				'wladek.fandom-dev.pl',
 				'https://www.wikia.com/signin',
-				null,
 				'https://www.wladek.fandom-dev.pl/signin'
-
 			],
 			[
 				'https://muppet.wladek.wikia-dev.us',
 				'wladek.wikia-dev.us',
 				'https://www.wikia.com/signin',
-				null,
 				'https://www.wladek.wikia-dev.us/signin'
-
 			],
 			[
 				'https://muppet.wladek.fandom-dev.us',
 				'wladek.fandom-dev.us',
 				'https://www.wikia.com/signin',
-				null,
 				'https://www.wladek.fandom-dev.us/signin'
-
 			],
 		];
 	}
