70864bc18199432e1294315440d58b8e1381a40b
macbre <maciej.brencz@gmail.com>
2018-09-25 13:48:42 +0200

    WikiFactoryLoader: vary caching on X-Mw-Wiki-Id request header

--- extensions/wikia/WikiFactory/Loader/WikiFactoryLoader.php
+++ extensions/wikia/WikiFactory/Loader/WikiFactoryLoader.php
@@ -70,6 +70,9 @@ class WikiFactoryLoader {
 		if ( !empty( $server['HTTP_X_MW_WIKI_ID'] ) ) {
 			// SUS-5816 | a special HTTP request with wiki ID forced via request header
 			$this->mCityID = (int) $server['HTTP_X_MW_WIKI_ID'];
+
+			// differ CDN caching on X-Mw-Wiki-Id request header value
+			RequestContext::getMain()->getOutput()->addVaryHeader( 'X-Mw-Wiki-Id' );
 		}
 		elseif ( !empty( $server['SERVER_NAME'] ) ) {
 			// normal HTTP request
@@ -469,7 +472,7 @@ class WikiFactoryLoader {
 					$redirectUrl, 301, $redirectedBy );
 			} else {
 				header( 'X-Redirected-By-WF: ' . $redirectedBy );
-				header( 'Vary: Cookie,Accept-Encoding' );
+				header( RequestContext::getMain()->getOutput()->getVaryHeader() );
 
 				global $wgCookiePrefix;
 				$hasAuthCookie = !empty( $_COOKIE[\Wikia\Service\User\Auth\CookieHelper::ACCESS_TOKEN_COOKIE_NAME] ) ||
