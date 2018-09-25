c878053577bfa7375b5d96c938b40efe86814757
mech <wozniak.jacek@gmail.com>
2018-09-25 14:26:32 +0200

    PLATFORM-3705: small code comments

--- extensions/wikia/SafariStorageAccessPoC/js/SafariStorageAccessPoC.js
+++ extensions/wikia/SafariStorageAccessPoC/js/SafariStorageAccessPoC.js
@@ -8,6 +8,7 @@ require([
         iframe.src = mw.config.get('wgSafariPoCUrl');
         window.document.getElementById('WikiaArticle').appendChild(iframe);
 
+        // when the iframe manages to set the access token cookie, reload the page
         window.addEventListener('message',function(event) {
             if (event.data === "poc_auth_worked") {
                 alert('poc_auth_worked event received, will reload the page');
