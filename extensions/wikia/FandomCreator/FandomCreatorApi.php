db8fa80509d66bc1e99a3dd29dccef5a6c6cc002
Josh Rogan <jjjr1122@gmail.com>
2018-09-26 11:30:04 -0700

    Revert "CAKE-3834 | Update resource route"

--- extensions/wikia/FandomCreator/FandomCreatorApi.php
+++ extensions/wikia/FandomCreator/FandomCreatorApi.php
@@ -40,7 +40,7 @@ class FandomCreatorApi {
 	}
 
 	public function getSitemap( $communityId ) {
-		$response = $this->doApiRequest( "{$this->baseUrl}/communities/{$communityId}/navigation" );
+		$response = $this->doApiRequest( "{$this->baseUrl}/communities/{$communityId}/sitemap" );
 		if ( !$response->status->isOK() ) {
 			return null;
 		}
