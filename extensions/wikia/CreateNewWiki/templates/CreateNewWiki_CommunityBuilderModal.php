4d2625490dbd8bb76daff76cce0bd724bf4788e8
nmonterroso <nmonterroso@users.noreply.github.com>
2018-09-28 09:39:52 -0700

    add image

--- extensions/wikia/CreateNewWiki/templates/CreateNewWiki_CommunityBuilderModal.php
+++ extensions/wikia/CreateNewWiki/templates/CreateNewWiki_CommunityBuilderModal.php
@@ -1,5 +1,9 @@
 <div class="optin-modal">
 	<div class="optin-modal__dialog-wrapper">
+		<img
+			src="https://static.wikia.nocookie.net/913679bd-e51c-4e26-a643-22314e6d9253/scale-to-width/185"
+			alt="community builder"
+		/>
 		<p class="optin-modal__copy">
 			Want to create a new community for your favorite anime or TV show? Try out the Community Builder and start building your community today!
 		</p>
