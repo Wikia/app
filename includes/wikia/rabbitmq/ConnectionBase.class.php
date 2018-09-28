381f92d7c54f9604fa3901b72316487739dcb8b5
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2018-09-28 12:34:21 +0200

    SRE-103 | fix whitespace

--- includes/wikia/rabbitmq/ConnectionBase.class.php
+++ includes/wikia/rabbitmq/ConnectionBase.class.php
@@ -44,7 +44,7 @@ class ConnectionBase {
 				$routingKey
 			);
 
-			$channel->wait_for_pending_acks(self::ACK_WAIT_TIMEOUT_SECONDS );
+			$channel->wait_for_pending_acks( self::ACK_WAIT_TIMEOUT_SECONDS );
 		} catch ( AMQPExceptionInterface $e ) {
 			WikiaLogger::instance()->error( __METHOD__, [
 				'exception' => $e,
