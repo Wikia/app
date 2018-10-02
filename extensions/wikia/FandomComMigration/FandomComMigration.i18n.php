43b2a39c2a06e9d881f6b07970a59530ce83f1b3
rogatty <igor@wikia-inc.com>
2018-10-02 13:38:23 +0200

    SEO-669 | update messages

--- extensions/wikia/FandomComMigration/FandomComMigration.i18n.php
+++ extensions/wikia/FandomComMigration/FandomComMigration.i18n.php
@@ -2,8 +2,8 @@
 $messages = array();
 
 $messages['en'] = array(
-	'fandom-com-migration-after' => 'Your community domain has been migrated to fandom.com. Find out more in <a href="https://community.wikia.com/wiki/User_blog:Brandon_Rhea/Wiki_domains_will_be_changing_from_wikia.com_to_fandom.com_in_early_2019?">this FANDOM news blog</a>.',
-	'fandom-com-migration-before' => 'We are migrating your community\'s domain to fandom.com on $1. Find out more in <a href="https://community.wikia.com/wiki/User_blog:Brandon_Rhea/Wiki_domains_will_be_changing_from_wikia.com_to_fandom.com_in_early_2019?">this FANDOM news blog</a>.',
+	'fandom-com-migration-after' => 'Attention: We have migrated your community\'s domain to fandom.com. Find out more in <a href="https://community.wikia.com/wiki/User_blog:Brandon_Rhea/Wiki_domains_will_be_changing_from_wikia.com_to_fandom.com_in_early_2019?">this FANDOM news blog</a>.',
+	'fandom-com-migration-before' => 'Attention: We are migrating your community\'s domain to fandom.com on $1. Find out more in <a href="https://community.wikia.com/wiki/User_blog:Brandon_Rhea/Wiki_domains_will_be_changing_from_wikia.com_to_fandom.com_in_early_2019?">this FANDOM news blog</a>.',
 );
 
 $messages['qqq'] = array(
