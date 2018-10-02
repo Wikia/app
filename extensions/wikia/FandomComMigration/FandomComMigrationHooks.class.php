76ec50b6ad5c72fabe87d12fb23e3638fff4c62f
rogatty <igor@wikia-inc.com>
2018-10-02 15:12:41 +0200

    SEO-669 | parse wikitext before adding it to WikiVariables

--- extensions/wikia/FandomComMigration/FandomComMigrationHooks.class.php
+++ extensions/wikia/FandomComMigration/FandomComMigrationHooks.class.php
@@ -16,12 +16,12 @@ class FandomComMigrationHooks {
 		// Send HTML instead of message key to avoid the issue of non-compatible i18n libs
 		if ( $wgFandomComMigrationDone ) {
 			$wikiVariables['fandomComMigrationNotificationAfter'] =
-				wfMessage( 'fandom-com-migration-after' )->plain();
+				wfMessage( 'fandom-com-migration-after' )->parse();
 		} else if ( !empty( $wgFandomComMigrationDate ) ) {
 			$wikiVariables['fandomComMigrationNotificationBefore'] =
 				wfMessage( 'fandom-com-migration-before' )
 					->params( $wgFandomComMigrationDate )
-					->plain();
+					->parse();
 		}
 
 		return true;
