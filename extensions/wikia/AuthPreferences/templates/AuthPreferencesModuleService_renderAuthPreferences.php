0514ce58a05efeb53c2577338b72664537cc3c37
Maciej Leśniewski <mlesniewski@wikia-inc.com>
2018-10-09 15:34:46 +0200

    IW-778 Removes paragraphs

--- extensions/wikia/AuthPreferences/templates/AuthPreferencesModuleService_renderAuthPreferences.php
+++ extensions/wikia/AuthPreferences/templates/AuthPreferencesModuleService_renderAuthPreferences.php
@@ -1,18 +1,15 @@
 <div class="auth-preferences facebook-state-<?= Sanitizer::encodeAttribute( $state ); ?>">
 	<div id="fbConnectPreferences">
-		<p><?= wfMessage( 'fbconnect-convert' )->escaped(); ?></p>
 		<span id="facebook-connect-button" class="wds-button facebook-button" role="button">
 			<?= wfMessage( 'prefs-fbconnect-prefstext' )->escaped(); ?>
 		</span>
 	</div>
 	<div id="fbDisconnectPreferences">
-		<p><?= wfMessage( 'fbconnect-disconnect-account-link' )->parse(); ?></p>
 		<span id="facebook-disconnect-button" class="wds-button facebook-button" role="button">
 			<?= wfMessage( 'prefs-fbconnect-disconnect-prefstext' )->escaped(); ?>
 		</span>
 	</div>
 	<div id="googleConnectPreferences">
-		<p><?= wfMessage( 'google-convert' )->escaped(); ?></p>
 		<iframe class="google-button" src="<?= $googleConnectAuthUrl; ?>"></iframe>
 	</div>
 </div>
