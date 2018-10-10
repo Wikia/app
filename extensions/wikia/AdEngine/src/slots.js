2e57a3cac4f66863ba4d9b3c192a6ea22ccb74a3
Damian Sobczak <dsobczak@wikia-inc.com>
2018-10-10 09:34:25 +0200

    ADEN-7683 Add stickyAd to defaultTemplates property

--- extensions/wikia/AdEngine/src/slots.js
+++ extensions/wikia/AdEngine/src/slots.js
@@ -14,6 +14,7 @@ export function getSlotsContext(legacyContext, skin) {
 					],
 					options: {},
 					defaultSizes: [[2, 2]],
+					defaultTemplates: ['stickyAd'],
 					targeting: {
 						pos: 'TOP_LEADERBOARD',
 						loc: 'top'
