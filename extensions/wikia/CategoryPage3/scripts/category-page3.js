9c28cb1a9bae945d97f344f5825a1c215cf932c0
rogatty <igor@wikia-inc.com>
2018-10-11 12:11:11 +0200

    SEO-670 | set cookie when category layout selector is clicked

--- extensions/wikia/CategoryPage3/scripts/category-page3.js
+++ extensions/wikia/CategoryPage3/scripts/category-page3.js
@@ -1,1 +1,18 @@
-// TODO handle alphabet shortcuts
+require([
+	'jquery',
+	'wikia.cookies',
+], function (
+	$,
+	cookies
+) {
+	'use strict';
+
+	$(function () {
+		$('.category-layout-selector__item:not(.is-active)').click(function () {
+			var layoutSelected = this.getAttribute('data-category-layout');
+
+			cookies.set('category-page-layout', layoutSelected);
+			window.location.reload();
+		});
+	});
+});
--- extensions/wikia/CategoryPage3/templates/CategoryPage3_top.php
+++ extensions/wikia/CategoryPage3/templates/CategoryPage3_top.php
@@ -1,21 +1,24 @@
 <ul class="category-layout-selector">
 	<?php /** @var string $currentLayout */ ?>
 	<li title="Classic Categories"
-		class="category-layout-selector__item<?= $currentLayout === CategoryPage3::LAYOUT_MEDIAWIKI ? ' is-active' : '' ?>">
+		class="category-layout-selector__item<?= $currentLayout === CategoryPage3::LAYOUT_MEDIAWIKI ? ' is-active' : '' ?>"
+		data-category-layout="<?= CategoryPage3::LAYOUT_MEDIAWIKI ?>">
 		<?= DesignSystemHelper::renderSvg(
 			'wds-icons-bullet-list-small',
 			'category-layout-selector__icon wds-icon-small'
 		) ?>
 	</li>
 	<li title="Category Exhibition"
-		class="category-layout-selector__item<?= $currentLayout === CategoryPage3::LAYOUT_CATEGORY_EXHIBITION ? ' is-active' : '' ?>">
+		class="category-layout-selector__item<?= $currentLayout === CategoryPage3::LAYOUT_CATEGORY_EXHIBITION ? ' is-active' : '' ?>"
+		data-category-layout="<?= CategoryPage3::LAYOUT_CATEGORY_EXHIBITION ?>">
 		<?= DesignSystemHelper::renderSvg(
 			'wds-icons-grid-small',
 			'category-layout-selector__icon wds-icon-small'
 		) ?>
 	</li>
 	<li title="Dynamic Categories"
-		class="category-layout-selector__item<?= $currentLayout === CategoryPage3::LAYOUT_CATEGORY_PAGE3 ? ' is-active' : '' ?>">
+		class="category-layout-selector__item<?= $currentLayout === CategoryPage3::LAYOUT_CATEGORY_PAGE3 ? ' is-active' : '' ?>"
+		data-category-layout="<?= CategoryPage3::LAYOUT_CATEGORY_PAGE3 ?>">
 		<?= DesignSystemHelper::renderSvg(
 			'wds-icons-pages-small',
 			'category-layout-selector__icon wds-icon-small'
