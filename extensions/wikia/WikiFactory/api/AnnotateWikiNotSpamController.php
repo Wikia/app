c0c79b860c83038d15ace3742afbe95d9ae07282
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-10-12 10:42:49 +0200

    add controller to annotate wiki not spam

---
+++ extensions/wikia/WikiFactory/api/AnnotateWikiNotSpamController.php
@@ -0,0 +1,63 @@
+<?php
+
+use Wikia\Logger\Loggable;
+
+class AnnotateWikiNotSpamController {
+
+	use Loggable;
+
+	const WIKI_ID = 'wikiId';
+	const REASON = 'reason';
+
+	public function init() {
+		$this->assertCanAccessController();
+	}
+
+	public function annotateNotSpam() {
+		$context = $this->getContext();
+		$request = $context->getRequest();
+
+		$wikiId = $request->getVal( self::WIKI_ID );
+		$reason = $request->getVal( self::REASON );
+
+		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
+
+		if ( !is_numeric( $wikiId ) || empty( $reason ) ) {
+			// wikiId or reason given: Bad Request
+			$this->response->setCode( 400 );
+			$this->info('no wikiId or reason parameter in request');
+		} else {
+			$res = WikiFactory::setPublicStatus( WikiFactory::PUBLIC_WIKI, $wikiId, $reason );
+
+			if ( $res === WikiFactory::PUBLIC_WIKI ) {
+//				WikiFactory::setFlags( $wikiId,
+//					WikiFactory::FLAG_FREE_WIKI_URL | WikiFactory::FLAG_CREATE_DB_DUMP |
+//					WikiFactory::FLAG_CREATE_IMAGE_ARCHIVE );
+//				WikiFactory::clearCache( $wikiId );
+				$this->response->setCode( 200 );
+
+				return;
+			} else {
+				$this->response->setCode( 500 );
+				$this->info("could not annotate wiki NOT SPAM in MW. Wiki id: " . $wikiId);
+
+				return;
+			}
+		}
+
+	}
+
+
+	/**
+	 * Make sure to only allow authorized POST methods.
+	 * @throws WikiaHttpException
+	 */
+	public function assertCanAccessController() {
+		if ( !$this->getContext()->getRequest()->isWikiaInternalRequest() ) {
+			throw new ForbiddenException( 'Access to this controller is restricted' );
+		}
+		if ( !$this->getContext()->getRequest()->wasPosted() ) {
+			throw new MethodNotAllowedException( 'Only POST allowed' );
+		}
+	}
+}
