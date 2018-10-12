4b036f5d7cbd8ef4180f26445c29dc96c017c847
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-10-12 15:12:27 +0200

    change if check

--- extensions/wikia/WikiFactory/api/AnnotateWikiNotSpamController.php
+++ extensions/wikia/WikiFactory/api/AnnotateWikiNotSpamController.php
@@ -29,7 +29,7 @@ class AnnotateWikiNotSpamController extends WikiaController {
 		} else {
 			$res = WikiFactory::log( WikiFactory::LOG_STATUS, $reason, $wikiId );
 
-			if ( $res === WikiFactory::LOG_STATUS ) {
+			if ( $res ) {
 				$this->response->setCode( 200 );
 
 				return;
