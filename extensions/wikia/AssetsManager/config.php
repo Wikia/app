8ff7ee5a4bd520c662e97ee475da4c183646066b
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2018-10-16 14:08:25 +0200

    ADEN-7595 Change assets order to alphabetical.

--- extensions/wikia/AssetsManager/config.php
+++ extensions/wikia/AssetsManager/config.php
@@ -2361,8 +2361,8 @@ $config['jwplayer_featured_video_ads_js'] = [
 	'skin' => [ 'oasis', 'wikiamobile' ],
 	'assets' => [
 		'//extensions/wikia/AdEngine/js/video/player/jwplayer/jwplayerAdsTracking.js',
-		'//extensions/wikia/ArticleVideo/scripts/featured-video.cookies.js',
 		'//extensions/wikia/AdEngine/js/video/player/jwplayer/jwplayerTracker.js',
+		'//extensions/wikia/ArticleVideo/scripts/featured-video.cookies.js',
 		'//extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.ads.js',
 		'//extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.lagger.js',
 		'//extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.moat-tracking.js',
