bc9140c6a1d16e826aefb2846e17a4bef0122f2a
Karol Tatała <ktatala@wikia-inc.com>
2018-10-19 10:28:20 +0200

    IW-1070 | tracking

--- extensions/wikia/FeedsAndPosts/scripts/index.js
+++ extensions/wikia/FeedsAndPosts/scripts/index.js
@@ -1,9 +1,13 @@
-require(['jquery'], function ($) {
+require(['jquery', 'wikia.tracker'], function ($, tracker) {
 	// Use number of hours passed since Jan. 1, 1970. That way cache is busted at most every hour.
 	var version = Math.floor((new Date()).getTime() / (60 * 60 * 1000));
 	var fpLibrary = '/feeds-and-posts/public/dist/lib.min.js?' + version;
 
-	$('#WikiaRail').one('afterLoad.rail', function() {
+	var track = tracker.buildTrackingFunction({
+		trackingMethod: 'analytics'
+	});
+
+	$('#WikiaRail').one('afterLoad.rail', function () {
 		$.getScript(fpLibrary, function () {
 			var wikiName = $('meta[property="og:site_name"]').prop('content');
 
@@ -12,9 +16,7 @@ require(['jquery'], function ($) {
 			var fpContainer = $('.feed-posts-module').get(0);
 			window.fandomEmbeddedFeeds.default(fpContainer, {
 				communityName: wikiName,
-				track: function () {
-					// TODO
-				}
+				track: track,
 			});
 		});
 	});
