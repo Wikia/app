fa42abd0ce3ebfaf6f530ff79e04361565fbf065
Paweł Wójcik <pwojcik@wikia-inc.com>
2018-10-23 17:21:59 +0200

    Merge pull request #16252 from Wikia/SUS-6081
    SUS-6081 | htmlescape serchFormId in Search_Index.php template

--- skins/oasis/modules/templates/Search_Index.php
+++ skins/oasis/modules/templates/Search_Index.php
@@ -1,14 +1,14 @@
 <?php $searchterm = isset($searchterm) ? $searchterm : ''; ?>
 <?php $searchFormId = isset($searchFormId) ? $searchFormId : 'WikiaSearch'; ?>
 
-<form id="<?= $searchFormId; ?>" class="WikiaSearch<?= empty($noautocomplete) ? '' : ' noautocomplete' ?>" action="<?= $specialSearchUrl; ?>" method="get">
-	<input type="text" name="search" placeholder="<?= $placeholder ?>" autocomplete="off" accesskey="f" value="<?= htmlspecialchars( $searchterm ) ?>">
-	<input type="hidden" name="fulltext" value="<?= $fulltext ?>">
+<form id="<?= Sanitizer::encodeAttribute($searchFormId); ?>" class="WikiaSearch<?= empty($noautocomplete) ? '' : ' noautocomplete' ?>" action="<?= Sanitizer::encodeAttribute($specialSearchUrl); ?>" method="get">
+	<input type="text" name="search" placeholder="<?= Sanitizer::encodeAttribute($placeholder); ?>" autocomplete="off" accesskey="f" value="<?= Sanitizer::encodeAttribute($searchterm) ?>">
+	<input type="hidden" name="fulltext" value="<?= Sanitizer::encodeAttribute($fulltext); ?>">
 	<?php foreach( $searchParams as $name => $value ) : ?>
-		<input type="hidden" name="<?= $name ?>" value="<?= $value ?>">
+		<input type="hidden" name="<?= Sanitizer::encodeAttribute($name); ?>" value="<?= Sanitizer::encodeAttribute($value); ?>">
 	<?php endforeach; ?>
 	<input type="submit">
-	<button class="wikia-button"><img src="<?= $wg->BlankImgUrl ?>" class="sprite search" height="17" width="21"></button>
+	<button class="wikia-button"><img src="<?= Sanitizer::encodeAttribute($wg->BlankImgUrl); ?>" class="sprite search" height="17" width="21"></button>
 </form>
 <?php
 if ((!$wg->WikiaSearchIsDefault) && $wg->Title->isSpecial('Search')) {
