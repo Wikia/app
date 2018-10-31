3359e173109e990cd1bf9db052ab7d907fc31d0a
mech <wozniak.jacek@gmail.com>
2018-10-31 17:45:15 +0100

    Revert "PLATFORM-3761: include LanguageWikisIndex only on empty and closed wikis"
    This reverts commit 3551f0f504401a3567ffc3aca82712ec58b30ccf.

--- extensions/wikia/LanguageWikisIndex/LanguageWikisIndex.setup.php
+++ extensions/wikia/LanguageWikisIndex/LanguageWikisIndex.setup.php
@@ -2,22 +2,17 @@
 /*
  * Special page included on domain root when language path wikis exist without the English wiki.
  */
-
+$wgAutoloadClasses['LanguageWikisIndexController'] = __DIR__ . '/LanguageWikisIndexController.class.php';
 $wgAutoloadClasses['LanguageWikisIndexHooks'] = __DIR__ . '/LanguageWikisIndexHooks.class.php';
+$wgSpecialPages['LanguageWikisIndex'] = 'LanguageWikisIndexController';
 
-// include only on empty or closed wikis
-if ( LanguageWikisIndexHooks::isEmptyDomainWithLanguageWikis() || !WikiFactory::isPublic( $wgCityId ) ) {
+$wgExtensionMessagesFiles['LanguageWikisIndex'] = __DIR__ . '/LanguageWikisIndex.i18n.php';
 
-	$wgAutoloadClasses['LanguageWikisIndexController'] = __DIR__ . '/LanguageWikisIndexController.class.php';
-
-	$wgSpecialPages['LanguageWikisIndex'] = 'LanguageWikisIndexController';
-
-	$wgExtensionMessagesFiles['LanguageWikisIndex'] = __DIR__ . '/LanguageWikisIndex.i18n.php';
-
+if ( LanguageWikisIndexHooks::isEmptyDomainWithLanguageWikis() ) {
 	$wgExtensionFunctions[] = 'LanguageWikisIndexHooks::onExtensionFunctions';
 	$wgHooks['GenerateRobotsRules'][] = 'LanguageWikisIndexHooks::onGenerateRobotsRules';
-
-	$wgHooks['ClosedWikiHandler'][] = 'LanguageWikisIndexHooks::onClosedWikiPage';
-	$wgHooks['GetHTMLBeforeWikiaPage'][] = 'LanguageWikisIndexHooks::onGetHTMLBeforeWikiaPage';
-	$wgHooks['WikiaCanonicalHref'][] = 'LanguageWikisIndexHooks::onWikiaCanonicalHref';
 }
+
+$wgHooks['ClosedWikiHandler'][] = 'LanguageWikisIndexHooks::onClosedWikiPage';
+$wgHooks['GetHTMLBeforeWikiaPage'][] = 'LanguageWikisIndexHooks::onGetHTMLBeforeWikiaPage';
+$wgHooks['WikiaCanonicalHref'][] = 'LanguageWikisIndexHooks::onWikiaCanonicalHref';
