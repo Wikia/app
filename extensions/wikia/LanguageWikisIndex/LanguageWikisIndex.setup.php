3551f0f504401a3567ffc3aca82712ec58b30ccf
mech <wozniak.jacek@gmail.com>
2018-10-31 11:19:56 +0100

    PLATFORM-3761: include LanguageWikisIndex only on empty and closed wikis

--- extensions/wikia/LanguageWikisIndex/LanguageWikisIndex.setup.php
+++ extensions/wikia/LanguageWikisIndex/LanguageWikisIndex.setup.php
@@ -2,17 +2,22 @@
 /*
  * Special page included on domain root when language path wikis exist without the English wiki.
  */
-$wgAutoloadClasses['LanguageWikisIndexController'] = __DIR__ . '/LanguageWikisIndexController.class.php';
+
 $wgAutoloadClasses['LanguageWikisIndexHooks'] = __DIR__ . '/LanguageWikisIndexHooks.class.php';
-$wgSpecialPages['LanguageWikisIndex'] = 'LanguageWikisIndexController';
 
-$wgExtensionMessagesFiles['LanguageWikisIndex'] = __DIR__ . '/LanguageWikisIndex.i18n.php';
+// include only on empty or closed wikis
+if ( LanguageWikisIndexHooks::isEmptyDomainWithLanguageWikis() || !WikiFactory::isPublic( $wgCityId ) ) {
 
-if ( LanguageWikisIndexHooks::isEmptyDomainWithLanguageWikis() ) {
+	$wgAutoloadClasses['LanguageWikisIndexController'] = __DIR__ . '/LanguageWikisIndexController.class.php';
+
+	$wgSpecialPages['LanguageWikisIndex'] = 'LanguageWikisIndexController';
+
+	$wgExtensionMessagesFiles['LanguageWikisIndex'] = __DIR__ . '/LanguageWikisIndex.i18n.php';
+
 	$wgExtensionFunctions[] = 'LanguageWikisIndexHooks::onExtensionFunctions';
 	$wgHooks['GenerateRobotsRules'][] = 'LanguageWikisIndexHooks::onGenerateRobotsRules';
-}
 
-$wgHooks['ClosedWikiHandler'][] = 'LanguageWikisIndexHooks::onClosedWikiPage';
-$wgHooks['GetHTMLBeforeWikiaPage'][] = 'LanguageWikisIndexHooks::onGetHTMLBeforeWikiaPage';
-$wgHooks['WikiaCanonicalHref'][] = 'LanguageWikisIndexHooks::onWikiaCanonicalHref';
+	$wgHooks['ClosedWikiHandler'][] = 'LanguageWikisIndexHooks::onClosedWikiPage';
+	$wgHooks['GetHTMLBeforeWikiaPage'][] = 'LanguageWikisIndexHooks::onGetHTMLBeforeWikiaPage';
+	$wgHooks['WikiaCanonicalHref'][] = 'LanguageWikisIndexHooks::onWikiaCanonicalHref';
+}
