13bbb10a4bcd21b8333139204e42f7a91942a7c6
nAndy <nandy@wikia-inc.com>
2018-11-05 14:20:48 +0000

    ADEN-7773 bump ae3 and set right things in bidder.js

--- extensions/wikia/AdEngine/src/bidders.js
+++ extensions/wikia/AdEngine/src/bidders.js
@@ -405,7 +405,10 @@ export function getBiddersContext(skin) {
 						enabled: false,
 						slots: {
 							FEATURED: {},
-							INCONTENT_PLAYER: {}
+							INCONTENT_PLAYER: {
+								videoAdUnitId: '/5441/wka.life/_project43//article/test/outstream',
+								customParams: 's1=_project43&artid=402&src=test&pos=outstream&passback=wikiaVideo'
+							}
 						}
 					}
 				}
