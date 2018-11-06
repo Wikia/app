0c9eca03e8cbdec62277d8b2f46f5c3c252fcfc4
JamesHawking <jakubj91@gmail.com>
2018-11-06 11:31:04 +0100

    IW-1116 | Remove 'Content-Security-Policy: upgrade-insecure-requests' header

--- includes/wikia/Wikia.php
+++ includes/wikia/Wikia.php
@@ -1201,7 +1201,6 @@ class Wikia {
 	 */
 	static public function outputHTTPSHeaders( WebRequest $request ) {
 		if ( WebRequest::detectProtocol() === 'https' ) {
-			$request->response()->header('Content-Security-Policy: upgrade-insecure-requests' );
 
 			$urlProvider = new \Wikia\Service\Gateway\KubernetesExternalUrlProvider();
 			$request->response()->header("Content-Security-Policy-Report-Only: " .
