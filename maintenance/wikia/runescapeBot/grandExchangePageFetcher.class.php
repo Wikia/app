844b075ff3d42f1907d10fb65aec9752659b7c17
James Sutterfield <james@wikia-inc.com>
2018-11-07 11:09:39 +0100

    IW-1233 add namespace filter based on community

--- maintenance/wikia/runescapeBot/grandExchangePageFetcher.class.php
+++ maintenance/wikia/runescapeBot/grandExchangePageFetcher.class.php
@@ -3,11 +3,17 @@
 class GrandExchangePageFetcher {
 
 	const GRAND_EXCHANGE_CATEGORY_NAME = "Category: Grand Exchange";
-	const EXCHANGE_NAMESPACE = 112;
+
+	const RUNESCAPE_CITY_ID = 304;
+	const OLDSCHOOL_RUNESCAPE_CITY_ID = 691244;
+
+	const RUNESCAPE_NS_FILTER = 112;
+	const OLDSCHOOL_RUNESCAPE_NS_FILTER = 114;
 
 	/**
 	 * @return array
 	 * @throws MWException
+	 * @throws Exception
 	 */
 	public function fetchAllGrandExchangePages() {
 		$pages = [];
@@ -19,7 +25,7 @@ class GrandExchangePageFetcher {
 			'cmprop' => 'title',
 			'cmlimit' => 'max',
 			'cmtitle' => self::GRAND_EXCHANGE_CATEGORY_NAME,
-			'cmnamespace' => self::EXCHANGE_NAMESPACE,
+			'cmnamespace' => $this->getNameSpaceFilter()
 		];
 
 		$hasNext = true;
@@ -47,4 +53,24 @@ class GrandExchangePageFetcher {
 		return preg_replace("/^Exchange:/", "", $pageTitle );
 	}
 
+	/**
+	 * Item pages which need updating are found in different namespaces on the runescape
+	 * and oldschoolrunescape wikis. Determine which namespace we should filter on based
+	 * on which wiki we're running
+	 * @return int
+	 * @throws Exception
+	 */
+	private function getNameSpaceFilter() {
+		global $wgCityId;
+
+		if ( (int) $wgCityId === self::RUNESCAPE_CITY_ID ) {
+			return self::RUNESCAPE_NS_FILTER;
+		}
+
+		if ( (int) $wgCityId === self::OLDSCHOOL_RUNESCAPE_CITY_ID ) {
+			return self::OLDSCHOOL_RUNESCAPE_NS_FILTER;
+		}
+
+		throw new Exception( "this should only be run on a runescape wiki!" );
+	}
 }
