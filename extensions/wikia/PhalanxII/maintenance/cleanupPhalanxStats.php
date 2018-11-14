c384d027cb0b9fc57f15bb04bc698603dc1fb29d
macbre <maciej.brencz@gmail.com>
2018-11-14 13:30:12 +0100

    SUS-6262 | make CleanupPhalanxStats use Maintenance core MediaWiki class + alerting

--- extensions/wikia/PhalanxII/maintenance/cleanupPhalanxStats.php
+++ extensions/wikia/PhalanxII/maintenance/cleanupPhalanxStats.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../../../../maintenance/Maintenance.php';
+
 /**
  * Script that removes specials.phalanx_stats entries that are older than 365 days
  *
@@ -8,25 +10,31 @@
  * @file
  * @ingroup Maintenance
  */
+class CleanupPhalanxStats extends Maintenance {
 
-require_once( __DIR__ . '/../../../../maintenance/commandLine.inc' );
+	function execute() {
 
-$db = wfGetDB( DB_MASTER, [], $wgSpecialsDB );
-$rows = 0;
+		global $wgSpecialsDB;
 
-do {
+		$db = wfGetDB( DB_MASTER, [], $wgSpecialsDB );
+		$rows = 0;
 
-	// delete entries older than 365 days in small batches
-	// ps_timestamp has MediaWiki-formatted timestamps, e.g. 20170101000000
-	$db->query( 'DELETE FROM phalanx_stats WHERE ps_timestamp < NOW() - INTERVAL 365 DAY LIMIT 5000', __FILE__ );
-	$affectedRows = $db->affectedRows();
+		do {
 
-	$rows +=  $affectedRows;
+			// delete entries older than 365 days in small batches
+			// ps_timestamp has MediaWiki-formatted timestamps, e.g. 20170101000000
+			$db->query( 'DELETE FROM phalanx_stats WHERE ps_timestamp < NOW() - INTERVAL 365 DAY LIMIT 5000', __METHOD__ );
+			$affectedRows = $db->affectedRows();
 
-	wfWaitForSlaves( $db->getDBname() );
+			$rows +=  $affectedRows;
 
-	echo '.';
+			wfWaitForSlaves( $db->getDBname() );
 
-} while ( $affectedRows > 0 );
+			echo '.';
 
-\Wikia\Logger\WikiaLogger::instance()->info( "Count of rows dropped from phalanx stats: $rows" );
+		} while ( $affectedRows > 0 );
+
+		// every week we remove ~150k rows
+		\Wikia\Logger\WikiaLogger::instance()->info( "Count of rows dropped from phalanx stats: $rows" );
+	}
+}
