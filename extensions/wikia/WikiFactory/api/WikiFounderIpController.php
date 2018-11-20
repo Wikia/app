90bbf9b3a3d8264c0825e1f26b2532b3ba6b956c
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-11-20 20:59:01 +0100

    write some tests

--- extensions/wikia/WikiFactory/api/WikiFounderIpController.php
+++ extensions/wikia/WikiFactory/api/WikiFounderIpController.php
@@ -2,7 +2,7 @@
 
 use Wikia\Logger\Loggable;
 
-class GetIpController extends WikiaController {
+class WikiFounderIpController extends WikiaController {
 	use Loggable;
 	const PARAMETER_ID = 'id';
 
@@ -14,24 +14,23 @@ class GetIpController extends WikiaController {
 	public function getIp() {
 		$context = $this->getContext();
 		$request = $context->getRequest();
+		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
 
 		$id = $request->getVal( self::PARAMETER_ID);
 		if ( empty( $id ) || !is_numeric($id) ) {
-			$this->response->setCode( 400 );
+			$this->response->setCode( WikiaResponse::RESPONSE_CODE_BAD_REQUEST );
 			return;
 		}
 
 		$wiki = WikiFactory::getWikiByID( $id );
 		if ( empty( $wiki ) ) {
-			$this->response->setCode( 404 );
+			$this->response->setCode( WikiaResponse::RESPONSE_CODE_NOT_FOUND );
 			return;
 		}
 
-		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
 		$ip = inet_ntop($wiki->city_founding_ip_bin);
 
-		$this->response->setBody($ip);
-		return $ip;
+		$this->response->setVal("wiki_ip", $ip);
 	}
 
 	/**
---
+++ extensions/wikia/WikiFactory/tests/WikiFounderIpControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @group Integration
+ */
+class WikiFounderIpControllerTest extends WikiaDatabaseTest {
+	const WIKI_ID = 1;
+
+	/** @var RequestContext $requestContext */
+	private $requestContext;
+
+	/** @var WikiFounderIpController $wikiFounderIpController */
+	private $wikiFounderIpController;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->requestContext = new RequestContext();
+		$this->wikiFounderIpController->setContext($this->requestContext);
+		$this->wikiFounderIpController->setResponse(new WikiaResponse(WikiaResponse::FORMAT_JSON));
+	}
+
+	public function testPostRequestIsRejected() {
+		$this->expectException( MethodNotAllowedException::class );
+
+		$fauxRequest =
+			new FauxRequest( ['wikiId' => static::WIKI_ID] );
+		$fauxRequest->setHeader( \Wikia\Tracer\WikiaTracer::INTERNAL_REQUEST_HEADER_NAME, 1 );
+		$this->requestContext->setRequest( $fauxRequest );
+
+		$this->wikiFounderIpController->init();
+		$this->wikiFounderIpController->getIp();
+	}
+
+	public function testWikiIdIsRequired() {
+		$fauxRequest = new FauxRequest( []);
+		$fauxRequest->setHeader( \Wikia\Tracer\WikiaTracer::INTERNAL_REQUEST_HEADER_NAME, 1 );
+		$this->requestContext->setRequest( $fauxRequest );
+
+		$this->wikiFounderIpController->init();
+		$this->wikiFounderIpController->getIp();
+
+		$this->assertEquals( 400, $this->wikiFounderIpController->getResponse()->getCode() );
+	}
+
+	protected function getDataSet() {
+		return $this->createYamlDataSet( __DIR__ . '/fixtures/ip.yaml' );
+	}
+}
