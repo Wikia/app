8c0897dc5444037ea3df78b78bc24c5829875f07
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-11-20 08:56:07 +0100

    add getipcontroller

--- extensions/wikia/WikiFactory/api/GetIpController.php
+++ extensions/wikia/WikiFactory/api/GetIpController.php
@@ -1,15 +1,37 @@
 <?php
 
+use Wikia\Logger\Loggable;
+
 class GetIpController extends WikiaController {
 	use Loggable;
+	const PARAMETER_ID = 'id';
+
 
 	public function init() {
 		$this->assertCanAccessController();
 	}
 
-	public function annotateNotSpam($wikiId) {
-		$ip = WikiFactory::getIp( $wikiId );
-		$this->info("This is IP of the " + $wikiId + " wiki: " + $ip);
+	public function getIp() {
+		$context = $this->getContext();
+		$request = $context->getRequest();
+
+		$id = $request->getVal( self::PARAMETER_ID);
+		if ( empty( $id ) || !is_numeric($id) ) {
+			$this->response->setCode( 400 );
+			return;
+		}
+
+		$wiki = WikiFactory::getWikiByID( $id );
+		if ( empty( $wiki ) ) {
+			$this->response->setCode( 404 );
+			return;
+		}
+
+		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
+		$ip = inet_ntop($wiki->city_founding_ip_bin);
+
+		$this->response->setBody($ip);
+		return $ip;
 	}
 
 	/**
