41a4340adc52407808dadf12ee8d3d30ab386d29
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-11-21 10:37:32 +0100

    add some logging

--- extensions/wikia/WikiFactory/api/WikiFounderIpController.php
+++ extensions/wikia/WikiFactory/api/WikiFounderIpController.php
@@ -14,17 +14,21 @@ class WikiFounderIpController extends WikiaController {
 	public function getIp() {
 		$context = $this->getContext();
 		$request = $context->getRequest();
-		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
 
 		$id = $request->getVal( self::PARAMETER_ID);
+
+		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
+
 		if ( empty( $id ) || !is_numeric($id) ) {
 			$this->response->setCode( WikiaResponse::RESPONSE_CODE_BAD_REQUEST );
+			$this->info('no wikiId parameter in request');
 			return;
 		}
 
 		$wiki = WikiFactory::getWikiByID( $id );
 		if ( empty( $wiki ) ) {
 			$this->response->setCode( WikiaResponse::RESPONSE_CODE_NOT_FOUND );
+			$this->info("could not get Wiki IP. Wiki id: " . $id);
 			return;
 		}
 
