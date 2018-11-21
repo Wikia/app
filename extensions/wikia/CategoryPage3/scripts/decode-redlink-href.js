c88f3c797e3882103e669fce3403adbc20474647
Martyna Nowicka <martynaanow@gmail.com>
2018-11-21 16:55:53 +0100

    comment

--- extensions/wikia/CategoryPage3/scripts/decode-redlink-href.js
+++ extensions/wikia/CategoryPage3/scripts/decode-redlink-href.js
@@ -7,8 +7,8 @@ require([
 ) {
 	'use strict';
 	$(function () {
+		// Handles middle click, ctrl+click and regular click
 		$('a[data-uncrawlable-url]').on('mousedown', function () {
-
 			var $this = $(this);
 			var url = window.atob($this.attr('data-uncrawlable-url'));
 			$this.attr('href', url);
