4428cdacfb8fc4bb24fc4b23fc86ab83d96607c5
Bartosz Piatek <bpiatek@wikia-inc.com>
2018-11-21 10:10:44 +0100

    add two more tests

--- extensions/wikia/WikiFactory/tests/WikiFounderIpControllerTest.php
+++ extensions/wikia/WikiFactory/tests/WikiFounderIpControllerTest.php
@@ -44,6 +44,29 @@ class WikiFounderIpControllerTest extends WikiaDatabaseTest {
 		$this->assertEquals( 400, $this->wikiFounderIpController->getResponse()->getCode() );
 	}
 
+	public function testInternalHeaderAbsence() {
+		$this->expectException( ForbiddenException::class );
+
+		$this->requestContext->setRequest( new FauxRequest( [ 'id' => static::WIKI_ID ]) );
+
+		$this->wikiFounderIpController->init();
+		$this->wikiFounderIpController->getIp();
+	}
+
+	public function testGetWikiIp() {
+		$fauxRequest = new FauxRequest( [ 'id' => static::WIKI_ID ]);
+		$fauxRequest->setHeader( \Wikia\Tracer\WikiaTracer::INTERNAL_REQUEST_HEADER_NAME, 1 );
+		$this->requestContext->setRequest( $fauxRequest );
+
+		$this->wikiFounderIpController->init();
+		$this->wikiFounderIpController->getIp();
+
+		$this->assertEquals( 200, $this->wikiFounderIpController->getResponse()->getCode() );
+		$this->assertEquals( WikiaResponse::FORMAT_JSON,
+			$this->wikiFounderIpController->getResponse()->getFormat() );
+		$this->assertEquals( '149.6.28.163', $this->wikiFounderIpController->getResponse()->getBody() );
+	}
+
 	protected function getDataSet() {
 		return $this->createYamlDataSet( __DIR__ . '/fixtures/ip.yaml' );
 	}
