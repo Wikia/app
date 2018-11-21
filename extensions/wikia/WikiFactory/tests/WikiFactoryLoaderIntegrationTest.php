48c1da22a0e9afcf09f2050e553c70f816843208
Máté Szabó <2721291+mszabo-wikia@users.noreply.github.com>
2018-11-21 16:08:57 +0100

    document the why

--- extensions/wikia/WikiFactory/tests/WikiFactoryLoaderIntegrationTest.php
+++ extensions/wikia/WikiFactory/tests/WikiFactoryLoaderIntegrationTest.php
@@ -18,6 +18,9 @@ class WikiFactoryLoaderIntegrationTest extends WikiaDatabaseTest {
 		WikiFactory::isUsed( false );
 		$wgExtensionFunctions = [];
 		$this->dbName = $wgDBname;
+		
+		// WikiFactoryLoader has side effects that initialize the global content language with a StubObject
+		// In tests, we must ensure that any previous value for this global is correctly restored
 		$this->oldContentLanguage = $wgContLang;
 	}
 
