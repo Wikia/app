207405a0b0cabfe8bd42b9bc18f78ce13c47c26b
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2018-11-27 01:59:28 +0100

    Log JSON encoding failures when publishing tasks`

--- lib/Wikia/src/Rabbit/DefaultTaskPublisher.php
+++ lib/Wikia/src/Rabbit/DefaultTaskPublisher.php
@@ -69,18 +69,24 @@ class DefaultTaskPublisher implements TaskPublisher {
 				$queue = $task->getQueue()->name();
 				$payload = $task->serialize();
 
-				$message = new AMQPMessage( json_encode( $payload ), [
-					'content_type' => 'application/json',
-					'content-encoding' => 'UTF-8',
-					'immediate' => false,
-					'delivery_mode' => 2, // persistent
-					'app_id' => 'mediawiki',
-					'correlation_id' => WikiaTracer::instance()->getTraceId(),
-				] );
+				$jsonObject = json_encode( $payload );
 
-				$channel->batch_basic_publish( $message, '', $queue );
+				if ( $jsonObject ) {
+					$message = new AMQPMessage( $jsonObject, [
+						'content_type' => 'application/json',
+						'content-encoding' => 'UTF-8',
+						'immediate' => false,
+						'delivery_mode' => 2, // persistent
+						'app_id' => 'mediawiki',
+						'correlation_id' => WikiaTracer::instance()->getTraceId(),
+					] );
 
-				$this->logPublish( $queue, $payload );
+					$channel->batch_basic_publish( $message, '', $queue );
+
+					$this->logPublish( $queue, $payload );
+				} else {
+					$this->handleJsonError( $payload );
+				}
 			}
 
 			$channel->publish_batch();
@@ -111,5 +117,45 @@ class DefaultTaskPublisher implements TaskPublisher {
 			'spawn_task_args' => substr( $argsJson, 0, 3000 ) . ( strlen( $argsJson ) > 3000 ? '...' : '' ),
 			'spawn_task_queue' => $queue,
 		] );
+	}
+
+	private function handleJsonError( array $payload ) {
+		switch ( json_last_error() ) {
+			case JSON_ERROR_NONE:
+				$message = __CLASS__ . ' - No JSON error but empty message found';
+				break;
+			case JSON_ERROR_DEPTH:
+				$message = __CLASS__ . ' - Maximum stack depth exceeded';
+				break;
+			case JSON_ERROR_STATE_MISMATCH:
+				$message = __CLASS__ . ' - Underflow or the modes mismatch';
+				break;
+			case JSON_ERROR_CTRL_CHAR:
+				$message = __CLASS__ . ' - Unexpected control character found';
+				break;
+			case JSON_ERROR_SYNTAX:
+				$message = __CLASS__ . ' - Syntax error, malformed JSON';
+				break;
+			case JSON_ERROR_UTF8:
+				$message = __CLASS__ . ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+				break;
+			default:
+				$message = __CLASS__ . ' - Unknown JSON error';
+				break;
+		}
+
+		$context = [ 'exception' => new \Exception() ];
+
+		// Extract the task class name from message array
+		// Theoretically an AsyncTaskList instance might contain multiple task calls of different classes
+		// however this is never the case in practice.
+		foreach ( $payload['args'] as $arguments ) {
+			foreach ( $arguments['task_list'] as $task ) {
+				$context['task'] = $task['class'];
+				break;
+			}
+		}
+
+		$this->error( $message, $context );
 	}
 }
