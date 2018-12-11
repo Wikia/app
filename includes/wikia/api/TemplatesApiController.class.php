1a77b71a9b99e452c35eeafe3e044bfe8a156d5b
macbre <maciej.brencz@gmail.com>
2018-12-11 13:46:30 +0100

    Add PreprocessorDOMNewTemplateFrame hook and use it in TemplatesApiController

--- includes/wikia/api/TemplatesApiController.class.php
+++ includes/wikia/api/TemplatesApiController.class.php
@@ -2,12 +2,59 @@
 
 class TemplatesApiController extends WikiaApiController {
 
+	private static $TEMPLATES_BLACKLIST = [
+		'!',
+		'!!',
+	];
+	private static $templates = [];
+
+	/**
+	 * Register templates names and parameters as we parse the article
+	 *
+	 * @param PPFrame_DOM $frame
+	 * @param Title $title
+	 * @param DOMElement[] $numberedArgs
+	 * @param DOMElement[] $namedArgs
+	 * @throws \Swagger\Client\ApiException
+	 */
+	static function onPreprocessorDOMNewTemplateFrame(
+		PPFrame_DOM $frame, Title $title, array $numberedArgs, array $namedArgs
+	) {
+		global $wgCityId;
+
+		// skip blacklisted templates, e.g. ! or !!
+		if ( in_array( $title->getText(), self::$TEMPLATES_BLACKLIST ) ) {
+			return;
+		}
+
+		// skip templates without names arguments
+		if ( empty( $namedArgs ) ) {
+			return;
+		}
+
+		// collect template's parameters
+		$parameters = [];
+		foreach($namedArgs as $name => $argData) {
+			$parameters[ $name ] = $argData->textContent;
+		}
+
+		$templateClassification = new TemplateClassificationService();
+
+		self::$templates[] = [
+			'name' => $title->getText(),
+			'id' => $title->getArticleID(),
+			'type' => $templateClassification->getType( $wgCityId, $title->getArticleID() ),
+			'parameters' => $parameters,
+		];
+	}
+
 	/**
 	 * Returns metadata for all templates used on a specified article.
 	 *
 	 * CORE-20: this is an experimental API that can change in the near future
 	 *
-	 * @throws InvalidParameterApiException
+	 * @throws WikiaHttpException
+	 * @throws \Swagger\Client\ApiException
 	 */
 	public function getMetadata() {
 		$pageid = $this->getRequiredParam( 'pageid' );
@@ -17,7 +64,12 @@ class TemplatesApiController extends WikiaApiController {
 			throw new NotFoundApiException();
 		}
 
-		$templates = [];
+		// register a hook that will collect templates metadata
+		self::$templates = [];
+		Hooks::register(
+			'PreprocessorDOMNewTemplateFrame',
+			[ $this, 'onPreprocessorDOMNewTemplateFrame' ]
+		);
 
 		// parse an article to get the list of all templates
 		$parser = new Parser();
@@ -27,26 +79,9 @@ class TemplatesApiController extends WikiaApiController {
 			$title, $opts
 		);
 
-		$templatesInArticle = $out->getTemplates();
-
-		// fetch templates metadata
-		if ( array_key_exists( NS_TEMPLATE, $templatesInArticle ) ) {
-			$templateClassification = new TemplateClassificationService();
-
-			foreach ( $templatesInArticle[NS_TEMPLATE] as $templateTitle => $templateId ) {
-				$templates[] = [
-					'name' => $templateTitle,
-					'id' => $templateId,
-					'type' => $templateClassification->getType( $this->wg->CityId, $templateId )
-				];
-			}
-		}
-
-		# var_dump(__METHOD__, $out->getTemplateIds()); die;
-
 		$response = [
 			'title' => $title->getPrefixedText(),
-			'templates' => $templates,
+			'templates' => self::$templates,
 			'categories' => array_keys( $out->getCategories() )
 		];
 
