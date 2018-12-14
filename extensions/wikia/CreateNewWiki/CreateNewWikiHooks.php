b51e96f8bebf2b7556c3428909dbe91515f56b40
grunny <mwgrunny@gmail.com>
2018-12-14 19:27:48 +1000

    (PLATFORM-3874) Don't set now unneeded wgWikiaBaseDomain as a JS var

--- extensions/wikia/CreateNewWiki/CreateNewWikiHooks.php
+++ extensions/wikia/CreateNewWiki/CreateNewWikiHooks.php
@@ -4,9 +4,8 @@ class CreateNewWikiHooks {
 
 	public static function onMakeGlobalVariablesScript( array &$vars, OutputPage $outputPage ) {
 		if ( $outputPage->getTitle()->isSpecial( 'CreateNewWiki' ) ) {
-			global $wgWikiaBaseDomain, $wgFandomBaseDomain;
+			global $wgFandomBaseDomain;
 
-			$vars['wgWikiaBaseDomain'] = $wgWikiaBaseDomain;
 			$vars['wgFandomBaseDomain'] = $wgFandomBaseDomain;
 		}
 	}
