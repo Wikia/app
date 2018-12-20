a19fd231f95a1c9d76c604a26a3da0628a09ba10
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-12-20 09:49:11 +0100

    ADEN Fix window.Merucry when there are DOM elements with 'Mercury' id on page on oasis

--- extensions/wikia/JSVariables/JSVariables.php
+++ extensions/wikia/JSVariables/JSVariables.php
@@ -31,6 +31,7 @@ function wfJSVariablesTopScripts(Array &$vars, &$scripts) {
 	$vars['wgAfterContentAndJS'] = array();
 	$vars['wgCdnRootUrl'] = $wg->CdnRootUrl;
 	$vars['wgCdnApiUrl'] = $wg->CdnApiUrl;
+	$vars['Mercury'] = null;
 
 	// analytics needs it (from here till the end of the function)
 	$vars['wgDBname'] = $wg->DBname;
