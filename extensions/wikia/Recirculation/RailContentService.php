fe98d7a21e4336198a2a9554914bf638cf32a058
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2019-01-14 11:17:46 +0100

    IW-1509 | Fix thumbnails in Rail recirculation

--- extensions/wikia/Recirculation/RailContentService.php
+++ extensions/wikia/Recirculation/RailContentService.php
@@ -2,9 +2,26 @@
 
 class RailContentService extends WikiaService {
 
+	const RAIL_THUMBNAIL_WIDTH = 94;
+	const RAIL_THUMBNAIL_HEIGHT = 53;
+
 	public function renderRailModule() {
 		$popularPagesService = new PopularPagesService();
-		$articles = $popularPagesService->getPopularPages( 5, 80, 50 );
+		$articles = $popularPagesService->getPopularPages(
+			5,
+			static::RAIL_THUMBNAIL_WIDTH,
+			static::RAIL_THUMBNAIL_HEIGHT
+		);
+
+		foreach ( $articles as $pageId => $article ) {
+			$gen = ( new VignetteUrlToUrlGenerator( $article['thumbnail'] ) )->build();
+
+			$articles[$pageId]['thumbnail'] =
+				$gen->zoomCrop()
+					->width( static::RAIL_THUMBNAIL_WIDTH )
+					->height( static::RAIL_THUMBNAIL_HEIGHT )
+					->url();
+		}
 
 		$this->response->addAsset( 'extensions/wikia/Recirculation/styles/premium-rail.scss' );
 		$this->setVal( 'popularPages', $articles );
