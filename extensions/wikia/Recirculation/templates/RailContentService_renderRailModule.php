635de9e7606c0f78fc39e74112f67aeb9eee69be
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-01-14 10:17:56 +0100

    IW-1468: tracking in rail

--- extensions/wikia/Recirculation/templates/RailContentService_renderRailModule.php
+++ extensions/wikia/Recirculation/templates/RailContentService_renderRailModule.php
@@ -5,7 +5,7 @@
 			<ul class="thumbnails">
 				<?php foreach ( $popularPages as $page ): ?>
 				<li>
-					<a href="<?= Sanitizer::encodeAttribute( $page['url'] ); ?>" title="<?= Sanitizer::encodeAttribute( $page['title'] ); ?>">
+					<a href="<?= Sanitizer::encodeAttribute( $page['url'] ); ?>" title="<?= Sanitizer::encodeAttribute( $page['title'] ); ?>" data-tracking="rail">
 						<img src="<?= Sanitizer::encodeAttribute( $page['thumbnail'] ); ?>" alt="" />
 						<p><?= htmlspecialchars( $page['title'] ); ?></p>
 					</a>
