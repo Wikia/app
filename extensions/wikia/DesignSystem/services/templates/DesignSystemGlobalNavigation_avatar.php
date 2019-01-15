70608be712f91125450ddf08f9d0e25dcebbe3ab
Weronika Rudnicka <wrudnicka@wikina-inc.com>
2019-01-15 08:36:20 +0000

    IW-1501 | updated avatar component

--- extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_avatar.php
+++ extensions/wikia/DesignSystem/services/templates/DesignSystemGlobalNavigation_avatar.php
@@ -1,8 +1,7 @@
-<div class="wds-avatar">
+<div class="wds-avatar<?= !$src ? ' is-default' : ''?>" title="<?= Sanitizer::encodeAttribute( $alt ); ?>">
 	<?php if($src): ?>
 		<img src="<?= Sanitizer::encodeAttribute( $src ); ?>" alt="<?= Sanitizer::encodeAttribute( $alt ); ?>" title="<?= Sanitizer::encodeAttribute( $alt ); ?>" class="wds-avatar__image">
 	<?php else: ?>
-		<div class="wds-avatar__inner-border" alt="<?= Sanitizer::encodeAttribute( $alt ); ?>" title="<?= Sanitizer::encodeAttribute( $alt ); ?>"></div>
 		<?= DesignSystemHelper::renderSvg( 'wds-avatar-icon-user', 'wds-avatar__image',
 			$alt ); ?>
 	<?php endif; ?>
