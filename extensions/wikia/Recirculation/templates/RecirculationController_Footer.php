cb10c12d2a57e62882252359c0debe719ac2e724
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-01-15 08:59:33 +0100

    IW-1466: update tracking labels

--- extensions/wikia/Recirculation/templates/RecirculationController_Footer.php
+++ extensions/wikia/Recirculation/templates/RecirculationController_Footer.php
@@ -18,28 +18,28 @@
 						]
 					); ?>
 				<? else: ?>
-					<div class="mcf-card-ns-placeholder" data-tracking="card-1"></div>
+					<div class="mcf-card-ns-placeholder" data-tracking="footer-card-1"></div>
 				<? endif; ?>
 
 				<? if ( $canShowDiscussions ): ?>
 					<div class="mcf-discussions-placeholder"></div>
 				<? else: ?>
-					<div class="mcf-card-ns-placeholder" data-tracking="card-4"></div>
-					<div class="mcf-card-wiki-placeholder" data-tracking="card-7"></div>
+					<div class="mcf-card-ns-placeholder" data-tracking="footer-card-4"></div>
+					<div class="mcf-card-wiki-placeholder" data-tracking="footer-card-7"></div>
 				<? endif; ?>
-				<div class="mcf-card-ns-placeholder mcf-card-tall" data-tracking="card-10"></div>
-				<div class="mcf-card-wiki-placeholder" data-tracking="card-13"></div>
-				<div class="mcf-card-ns-placeholder" data-tracking="card-16"></div>
-				<div class="mcf-card-wiki-placeholder mcf-card-tall" data-tracking="card-19"></div>
+				<div class="mcf-card-ns-placeholder mcf-card-tall" data-tracking="footer-card-10"></div>
+				<div class="mcf-card-wiki-placeholder" data-tracking="footer-card-13"></div>
+				<div class="mcf-card-ns-placeholder" data-tracking="footer-card-16"></div>
+				<div class="mcf-card-wiki-placeholder mcf-card-tall" data-tracking="footer-card-19"></div>
 			</div>
 
 			<div class="mcf-column">
-				<div class="mcf-card-ns-placeholder mcf-card-sponsored-content" data-tracking="card-2"></div>
-				<div class="mcf-card-ns-placeholder" data-tracking="card-5"></div>
-				<div class="mcf-card-ns-placeholder" data-tracking="card-8"></div>
-				<div class="mcf-card-wiki-placeholder" data-tracking="card-11"></div>
-				<div class="mcf-card-ns-placeholder" data-tracking="card-14"></div>
-				<div class="mcf-card-wiki-placeholder" data-tracking="card-17"></div>
+				<div class="mcf-card-ns-placeholder mcf-card-sponsored-content" data-tracking="footer-card-2"></div>
+				<div class="mcf-card-ns-placeholder" data-tracking="footer-card-5"></div>
+				<div class="mcf-card-ns-placeholder" data-tracking="footer-card-8"></div>
+				<div class="mcf-card-wiki-placeholder" data-tracking="footer-card-11"></div>
+				<div class="mcf-card-ns-placeholder" data-tracking="footer-card-14"></div>
+				<div class="mcf-card-wiki-placeholder" data-tracking="footer-card-17"></div>
 				<?= F::app()->renderPartial(
 					'Recirculation',
 					'exploreWikis',
@@ -53,13 +53,13 @@
 			</div>
 
 			<div class="mcf-column">
-				<div class="mcf-card-wiki-placeholder" data-tracking="card-3"></div>
-				<div class="mcf-card-wiki-placeholder mcf-card-tall" data-tracking="card-6"></div>
-				<div class="mcf-card-wiki-placeholder" data-tracking="card-9"></div>
-				<div class="mcf-card-ns-placeholder" data-tracking="card-12"></div>
-				<div class="mcf-card-wiki-placeholder mcf-card-tall" data-tracking="card-15"></div>
-				<div class="mcf-card-ns-placeholder" data-tracking="card-18"></div>
-				<div class="mcf-card-ns-placeholder" data-tracking="card-21"></div>
+				<div class="mcf-card-wiki-placeholder" data-tracking="footer-card-3"></div>
+				<div class="mcf-card-wiki-placeholder mcf-card-tall" data-tracking="footer-card-6"></div>
+				<div class="mcf-card-wiki-placeholder" data-tracking="footer-card-9"></div>
+				<div class="mcf-card-ns-placeholder" data-tracking="footer-card-12"></div>
+				<div class="mcf-card-wiki-placeholder mcf-card-tall" data-tracking="footer-card-15"></div>
+				<div class="mcf-card-ns-placeholder" data-tracking="footer-card-18"></div>
+				<div class="mcf-card-ns-placeholder" data-tracking="footer-card-21"></div>
 			</div>
 		</div>
 	</div>
