f42546f5c398fd18bb623ad0fda894e746f5d114
Martyna Nowicka <martynaanow@gmail.com>
2019-01-18 11:33:39 +0100

    display redirect=no in redirected from link

--- extensions/wikia/SEOTweaks/SEOTweaksHooksHelper.class.php
+++ extensions/wikia/SEOTweaks/SEOTweaksHooksHelper.class.php
@@ -364,7 +364,15 @@ class SEOTweaksHooksHelper {
 	public static function onDisplayRedirectedFrom( $rdfrom, Article $article): bool {
 		$context = $article->getContext();
 		if ( !$context->getUser()->isAnon() ) {
-			$redir = Linker::makeExternalLink( $rdfrom, $rdfrom );
+			$redirectNamespace = $article->getTitle()->getNamespace();
+			$title = Title::makeTitleSafe( $redirectNamespace, $rdfrom );
+			$article->setRedirectedFrom( $title );
+			$redir = Linker::linkKnown(
+				$article->mRedirectedFrom,
+				null,
+				array(),
+				array( 'redirect' => 'no' )
+			);
 			$context->getOutput()->addSubtitle( wfMessage( 'redirectedfrom' )->rawParams( $redir ) );
 		}
 
