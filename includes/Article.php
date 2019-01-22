e98cf13553c6768d9e5dfbc79f602052c3b2a600
mech <wozniak.jacek@gmail.com>
2019-01-22 18:44:55 +0100

    Revert "fix url and display message for logged in users"
    This reverts commit 7213742415013a16c281902a0a1f4598acc1e7f6.

--- includes/Article.php
+++ includes/Article.php
@@ -951,8 +951,6 @@ class Article extends Page {
 		} elseif ( $rdfrom ) {
 			// This is an externally redirected view, from some other wiki.
 			// If it was reported from a trusted site, supply a backlink.
-			Hooks::run( 'DisplayRedirectedFrom', array( $rdfrom, $wgOut, $this ) );
-
 			if ( $wgRedirectSources && preg_match( $wgRedirectSources, $rdfrom ) ) {
 				$redir = Linker::makeExternalLink( $rdfrom, $rdfrom );
 				$wgOut->addSubtitle( wfMessage( 'redirectedfrom' )->rawParams( $redir ) );
