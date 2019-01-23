76c8d77b5f92a9ea0e607288b5c51f0441895532
Weronika Rudnicka <wrudnicka@wikia-inc.com>
2019-01-23 10:14:23 +0100

    IW-1261 | question-mark icon renamed to question

--- extensions/wikia/CommunityPage/models/CommunityPageSpecialHelpModel.class.php
+++ extensions/wikia/CommunityPage/models/CommunityPageSpecialHelpModel.class.php
@@ -4,7 +4,7 @@ class CommunityPageSpecialHelpModel {
 	public function getData() {
 		return [
 			'title' => wfMessage( 'communitypage-help-module-title' )->text(),
-			'titleIcon' => DesignSystemHelper::renderSvg( 'wds-icons-question-mark' ),
+			'titleIcon' => DesignSystemHelper::renderSvg( 'wds-icons-question' ),
 			'editPage' => wfMessage( 'communitypage-help-edit-page' )->text(),
 			'addLinks' => wfMessage( 'communitypage-help-add-link' )->text(),
 			'addNewPage' => wfMessage( 'communitypage-help-add-new-page' )->text(),
