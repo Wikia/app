8b20f98574cf94d05f8a9e12767c9911ab29c866
JamesHawking <jakubj91@gmail.com>
2019-02-01 11:25:44 +0100

    CORE-79 | Implements draft saving and retrieval.

--- extensions/wikia/EditDraftSaving/js/index.js
+++ extensions/wikia/EditDraftSaving/js/index.js
@@ -1,6 +1,37 @@
+var computeDraftKey = window.wgPageName + '-draft';
+
+function saveDraft(editorType) {
+	try {
+		var draftData = {
+			editor: editorType,
+			mode: RTE.getInstance().mode,
+			draftText: RTE.getInstance().getData()
+		};
+
+		localStorage.setItem(
+			computeDraftKey,
+			JSON.stringify(draftData)
+		);
+
+	} catch (e) {
+		console.log(e);
+	}
+}
+
 CKEDITOR.on('instanceReady', function () {
+	var draftDataJSON = localStorage.getItem(computeDraftKey);
+
+	if (draftDataJSON) {
+		var draftData = JSON.parse(draftDataJSON);
+
+		RTE.getInstance().setMode(
+			draftData.mode,
+			RTE.getInstance().setData(draftData.draftText)
+		);
+	}
+
 	setInterval(function () {
-		console.log(RTE.getInstance().getData());
-	}, 3000);
+		saveDraft('CK');
+	}, 5000);
 });
 
