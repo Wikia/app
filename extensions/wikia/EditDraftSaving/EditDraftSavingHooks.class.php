a74618cfb59290432432d7440e378334535b6760
macbre <maciej.brencz@gmail.com>
2019-02-04 12:10:54 +0100

    wgEnableEditDraftSavingExt: add docs

--- extensions/wikia/EditDraftSaving/EditDraftSavingHooks.class.php
+++ extensions/wikia/EditDraftSaving/EditDraftSavingHooks.class.php
@@ -1,6 +1,13 @@
 <?php
 
 class EditDraftSavingHooks {
+	/**
+	 * This hook is run when edit page is about to be rendered.
+	 *
+	 * Applies to RichTextEditor (RTE) and Mediawiki's source editor
+	 *
+	 * @param EditPage $editPage
+	 */
 	static public function onAlternateEdit(EditPage $editPage) {
 		RequestContext::getMain()->getOutput()->addModules('ext.wikia.EditDraftSaving');
 	}
