648dd1ced3638d6eefc5f2149d9e46dbe23a2c53
abador <przemyslaw@czaus.pl>
2019-02-04 20:42:18 +0100

    cleanup, PLATFORM-3936

--- includes/cache/SquidUpdate.php
+++ includes/cache/SquidUpdate.php
@@ -13,7 +13,6 @@ class SquidUpdate {
 	var $urlArr, $mMaxTitles;
 
 	function __construct( $urlArr = Array(), $maxTitles = false ) {
-		var_dump($urlArr);
 		global $wgMaxSquidPurgeTitles;
 		if ( $maxTitles === false ) {
 			$this->mMaxTitles = $wgMaxSquidPurgeTitles;
