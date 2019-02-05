07ebeeb703b018ae0091e363c4bdca42cb65b976
macbre <maciej.brencz@gmail.com>
2019-02-05 13:43:52 +0100

    CKeditor: proper handling of mode switching when applying saved draft

--- extensions/wikia/EditDraftSaving/js/rte.js
+++ extensions/wikia/EditDraftSaving/js/rte.js
@@ -13,19 +13,26 @@ require(['EditDraftSaving'], function (EditDraftSaving) {
 	}
 
 	CKEDITOR.on('instanceReady', function () {
-		var draftData = EditDraftSaving.readDraft();
+		var draftData = EditDraftSaving.readDraft(),
+			CKinstance = RTE.getInstance();
 
 		// make sure that this draft comes from this editor
 		if (draftData && draftData.editor === EDITOR_TYPE) {
 			var draftText = draftData.draftText;
 
 			// @see https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-setMode
-			RTE.getInstance().setMode(
-				draftData.mode,
-				function() {
-					RTE.getInstance().setData(draftText)
-				}
-			);
+			if (CKinstance.mode !== draftData.mode) {
+				// we need to switch mode to apply a draft in a right one
+				CKinstance.setMode(
+					draftData.mode,
+					function () {
+						CKinstance.setData(draftText);
+					}
+				);
+			} else {
+				// we're in the right mode
+				CKinstance.setData(draftText);
+			}
 
 			EditDraftSaving.onDraftRestore(EDITOR_TYPE);
 		}
