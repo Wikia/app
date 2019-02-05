68e0e63e0e4c68485dbaa7a9255a83276a4683f1
macbre <maciej.brencz@gmail.com>
2019-02-05 13:01:31 +0100

    EditDraftSaving: add i18n strings for Polish and qqq (help for translators)

--- extensions/wikia/EditDraftSaving/EditDraftSaving.i18n.php
+++ extensions/wikia/EditDraftSaving/EditDraftSaving.i18n.php
@@ -4,3 +4,11 @@ $messages = array();
 $messages['en'] = array(
 	'edit-draft-loaded' => 'Something went wrong while you were editing this page last time. Here are your previous changes. Keep up the great work!',
 );
+
+$messages['pl'] = array(
+	'edit-draft-loaded' => 'Coś poszło nie tak podczas próby zapisu Twojej edycji. Oto Twoje zmiany, które udało się nam przywrócić. Możesz kontynuować swoją edycję.',
+);
+
+$messages['qqq'] = array(
+	'edit-draft-loaded' => 'This message pops up when we managed to restore a draft of an edit.',
+);
