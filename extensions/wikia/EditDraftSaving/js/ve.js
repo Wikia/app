6f00c3f5fb8306e3a28e62c5af39a6550862384a
macbre <maciej.brencz@gmail.com>
2019-02-06 10:25:58 +0100

    EditDraftSaving/js/ve.js: make it an AMD module

--- extensions/wikia/EditDraftSaving/js/ve.js
+++ extensions/wikia/EditDraftSaving/js/ve.js
@@ -3,11 +3,11 @@
  *
  * It should be added as a dependency for "ext.visualEditor.wikia.core" Resource Loader module.
  */
-(function(mw) {
+require(['mw'], function(mw) {
 	console.log('EditDraftSaving: hook init');
 
 	mw.hook('ve.activationComplete').add(function () {
 		console.log('EditDraftSaving: ve.activationComplete hook called, VisualEditor is ready');
 		console.log('EditDraftSaving: ', arguments);
 	});
-})(window.mediaWiki);
+});
