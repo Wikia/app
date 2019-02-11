d2814f5a12e598d78bb5f0f34e458d96f540b480
macbre <maciej.brencz@gmail.com>
2019-02-11 09:10:08 +0100

    CORE-104: Update the generic notification copy for the CK editor and Source editor

--- extensions/wikia/EditDraftSaving/EditDraftSaving.i18n.php
+++ extensions/wikia/EditDraftSaving/EditDraftSaving.i18n.php
@@ -2,11 +2,11 @@
 $messages = array();
 
 $messages['en'] = array(
-	'edit-draft-loaded' => 'Something went wrong while you were editing this page last time. Here are your previous changes. Keep up the great work!',
+	'edit-draft-loaded' => 'Here are your unpublished changes. Keep up the great work!',
 );
 
 $messages['pl'] = array(
-	'edit-draft-loaded' => 'Coś poszło nie tak podczas próby zapisu Twojej edycji. Oto Twoje zmiany, które udało się nam przywrócić. Możesz kontynuować swoją edycję.',
+	'edit-draft-loaded' => 'Oto Twoje niezapisane zmiany. Możesz kontynuować swoją edycję.',
 );
 
 $messages['qqq'] = array(
