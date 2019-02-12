7613a9690f2158a7467f1e452ddfa82fcc0ff67a
mszabo-wikia <2721291+mszabo-wikia@users.noreply.github.com>
2019-02-12 13:25:37 +0100

    IW-1598 | indent

--- extensions/wikia/ArchiveWikiForum/ArchiveWikiForumController.class.php
+++ extensions/wikia/ArchiveWikiForum/ArchiveWikiForumController.class.php
@@ -2,30 +2,30 @@
 
 class ArchiveWikiForumController extends WikiaController {
 
-    const ARCHIVED_FORUM_NOTIFICATION = "archivedForumNotification";
+	const ARCHIVED_FORUM_NOTIFICATION = "archivedForumNotification";
 
-    /**
-     * Render HTML for notification to user that old style wiki forums
-     * have been archived on this wiki.
-     */
-    public function archivedForumNotification() {
-        $this->response->addAsset( 'extensions/wikia/ArchiveWikiForum/css/archivedForumNotification.scss' );
+	/**
+	 * Render HTML for notification to user that old style wiki forums
+	 * have been archived on this wiki.
+	 */
+	public function archivedForumNotification() {
+		$this->response->addAsset( 'extensions/wikia/ArchiveWikiForum/css/archivedForumNotification.scss' );
 
-        global $wgEnableForumExt;
-        if ( !empty( $wgEnableForumExt ) ) {
-            $messageKey = 'archive-wiki-forums-button-to-new-forums';
-            $forumTitle = SpecialPage::getTitleFor( 'Forum' );
-            $url = $forumTitle->getLocalURL();
-        } else {
-        	global $wgScriptPath;
+		global $wgEnableForumExt;
+		if ( !empty( $wgEnableForumExt ) ) {
+			$messageKey = 'archive-wiki-forums-button-to-new-forums';
+			$forumTitle = SpecialPage::getTitleFor( 'Forum' );
+			$url = $forumTitle->getLocalURL();
+		} else {
+			global $wgScriptPath;
 
-            $messageKey = 'archive-wiki-forums-button-to-discussions';
-            $url = "$wgScriptPath/d/f";
-        }
+			$messageKey = 'archive-wiki-forums-button-to-discussions';
+			$url = "$wgScriptPath/d/f";
+		}
 
-        $this->response->setValues( [
-            'messageKey' => $messageKey,
-            'url' => $url
-        ] );
-    }
+		$this->response->setValues( [
+			'messageKey' => $messageKey,
+			'url' => $url,
+		] );
+	}
 }
