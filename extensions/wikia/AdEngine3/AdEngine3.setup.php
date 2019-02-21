2cb0daffc7c4a21012091e66fa4081dea88a0bbb
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2019-02-21 09:54:03 +0100

    ADEN-6934 remove test logic

--- extensions/wikia/AdEngine3/AdEngine3.setup.php
+++ extensions/wikia/AdEngine3/AdEngine3.setup.php
@@ -23,16 +23,13 @@ $wgHooks['OasisSkinAssetGroupsBlocking'][] = 'AdEngine3::onOasisSkinAssetGroupsB
 $wgHooks['WikiaSkinTopScripts'][] = 'AdEngine3::onWikiaSkinTopScripts';
 
 // Mirrors logic
-//if ( !empty( $_SERVER['HTTP_FASTLY_FF'] ) && !empty($_SERVER[ 'X-Staging' ]) ) {
-if (true) {
-//	if ( $_SERVER[ 'X-Staging' ] == 'externaltest' ) {
-	if (false) {
+if ( !empty( $_SERVER['HTTP_FASTLY_FF'] ) && !empty($_SERVER[ 'X-Staging' ]) ) {
+	if ( $_SERVER[ 'X-Staging' ] == 'externaltest' ) {
 	// externaltest is a mirror to our production communities where AdOperations test ads campaigns
 		include "$IP/../config/externaltest.php";
 	}
 
-	if (true) {
-//	if ( $_SERVER[ 'X-Staging' ] == 'showcase' ) {
+	if ( $_SERVER[ 'X-Staging' ] == 'showcase' ) {
 	// showcase is a mirror to our production communities where AdOperations target demo ads campaigns and Sales demonstate it to our clients
 		include "$IP/../config/showcase.php";
 	}
