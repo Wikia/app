4be21d5afb0fd7d7b53663275bd501594b22df34
nAndy <nandy@wikia-inc.com>
2019-02-26 13:25:57 +0000

    ADEN-6934 change entry name to a valid one

--- extensions/wikia/AdEngine3/AdHostMirrors.setup.php
+++ extensions/wikia/AdEngine3/AdHostMirrors.setup.php
@@ -1,12 +1,13 @@
 <?php
 // Mirrors logic
-if ( !empty( $_SERVER['HTTP_FASTLY_FF'] ) && !empty($_SERVER[ 'X-Staging' ]) ) {
-	if ( $_SERVER[ 'X-Staging' ] == 'externaltest' ) {
+if ( !empty( $_SERVER['HTTP_FASTLY_FF'] ) && !empty($_SERVER[ 'HTTP_X_STAGING' ]) ) {
+	if ( $_SERVER[ 'HTTP_X_STAGING' ] == 'externaltest' ) {
 		// externaltest is a mirror to our production communities where AdOperations test ads campaigns
 		include "$IP/../config/externaltest.php";
 	}
 
-	if ( $_SERVER[ 'X-Staging' ] == 'showcase' ) {
+	if ( $_SERVER[ 'HTTP_X_STAGING' ] == 'showcase' ) {
+	// ADEN-8318 - find a way to distinguish externaltest.* from showcase.*
 		// showcase is a mirror to our production communities where AdOperations target demo ads campaigns and Sales demonstate it to our clients
 		include "$IP/../config/showcase.php";
 	}
