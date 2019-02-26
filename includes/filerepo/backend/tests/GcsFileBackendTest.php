af74e34c8b93c62916cbe422e8a38e2646e6a83d
Michał Turek <mturek@wikia-inc.com>
2019-02-26 15:52:49 +0100

    SER-3032 | Add a test stub

---
+++ includes/filerepo/backend/tests/GcsFileBackendTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class GcsFileBackendTest extends TestCase {
+	
+	public function testMergeExternalParserOutputVars() {
+		self::fail( "verify this fails" );
+	}
+}
