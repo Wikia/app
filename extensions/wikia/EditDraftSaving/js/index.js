8b5ebb31281cbc3ebeb20ef44c04a243c671a014
Lukasz Adamczewski <adamczewski@gmail.com>
2019-02-28 16:45:53 +0100

    Revert "CORE-109 CORE-110 | discard draft - source editor and CKEditor"

--- extensions/wikia/EditDraftSaving/js/index.js
+++ extensions/wikia/EditDraftSaving/js/index.js
@@ -1,18 +1,13 @@
-define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, logger, tracker) {
+define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function(jquery, logger, tracker) {
 
 	// keep in sync with PHP code in EditDraftSavingHooks.class.php file
 	var EDIT_DRAFT_KEY_HIDDEN_FIELD = 'wpEditDraftKey';
 
 	// get MediaWiki edit form
-	var editForm = $('#editform');
+	var editForm = jquery('#editform');
 
 	// was draft conflict triggered?
 	var inDraftConflict = false;
-
-	// CORE-110 | initial content that will be used when user discards an edit
-	var initialContent;
-
-	var editFormCurrentTime = document.forms["editform"].wpStarttime.value;
 
 	/**
 	 * @param msg {string}
@@ -46,8 +41,6 @@ define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, 
 	 * @param draftData {object}
 	 */
 	function storeDraft(draftData) {
-		draftData.startTime = editFormCurrentTime;
-
 		try {
 			localStorage.setItem(
 				getDraftKey(),
@@ -74,11 +67,6 @@ define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, 
 		}
 	}
 
-	function storeOriginalContent(content) {
-		log('Stored original content: ' + content);
-		initialContent = content;
-	}
-
 	/**
 	 * Take edit draft timestamp and check if we're in edit conflict. Handle it accordingly.
 	 *
@@ -97,14 +85,12 @@ define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, 
 
 			// and compare it with the wpEdittime value
 			if (draftStartTime < wpEdittime) {
-				// console.log(draftStartTime, wpEdittime);
 				// Set wpEdittime to a timestamp that is before the current article revision timestamp.
 				// This will trigger a condition in EditPage line 1320
 				// "# Article exists. Check for edit conflict."
 				editForm.find('[name="wpEdittime"]').val(draftStartTime);
 
 				onDraftConflict(editorType);
-				editFormCurrentTime = wpEdittime;
 			}
 		}
 	}
@@ -123,7 +109,7 @@ define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, 
 			label: 'draft-conflict'
 		});
 
-		$.showModal(window.wgPageName, window.mediaWiki.message('edit-draft-edit-conflict').text());
+		jquery.showModal(window.wgPageName, window.mediaWiki.message('edit-draft-edit-conflict').text());
 
 		inDraftConflict = true;
 	}
@@ -131,7 +117,7 @@ define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, 
 	/**
 	 * Track draft restore and show the modal with a message saying what just happened
 	 */
-	function onDraftRestore(editorType, prependTo, onDismiss, mode) {
+	function onDraftRestore(editorType) {
 		log('Restored a draft for ' + editorType);
 
 		// Wikia.Tracker:  trackingevent editor-ck/impression/draft-loaded/ [analytics track]
@@ -144,44 +130,7 @@ define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, 
 
 		// CORE-84: in case of a conflict, let's only show the conflict notice
 		if (!inDraftConflict) {
-			$(prependTo).prepend(
-			'<div id="draft-restore-message" class="wds-banner-notification__container">' +
-				'<div class="wds-banner-notification wds-message">' +
-					'<div class="wds-banner-notification__icon">' +
-						'<svg class="wds-icon wds-icon-small" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">' +
-							'<path d="M3 11h10.586l-3.293-3.293a.999.999 0 0 1 0-1.414L13.586 3H3v8zm-1 7a1 1 0 0 1-1-1V1a1 1 0 0 1 2 0h13a1.002 1.002 0 0 1 .707 1.707L12.414 7l4.293 4.293A1 1 0 0 1 16 13H3v4a1 1 0 0 1-1 1z" fill-rule="evenodd"/>' +
-						'</svg>' +
-					'</div>' +
-					'<span class="wds-banner-notification__text">' + window.mediaWiki.message('edit-draft-loaded').text() + '</span>' +
-					'<button type="button" id="discard" class="wds-button wds-is-text primary-only">' + window.mediaWiki.message('edit-draft-discard').text().toUpperCase() + '</button>' +
-					'<button type="button" id="keep" class="wds-button wds-is-text primary-only">' + window.mediaWiki.message('edit-draft-keep').text().toUpperCase() + '</button>' +
-				'</div>'  +
-			'</div>'
-			);
-
-			var bar = $('#draft-restore-message');
-
-			if (editorType === 'editor-ck' && mode === 'source') {
-				bar.addClass('ck-source-mode')
-			}
-
-			bar.find('#keep').on('click', function() {
-				bar.hide();
-			});
-
-			bar.find('#discard').on('click', function() {
-				bar.hide();
-
-				log('Dismissing a draft');
-				tracker.track({
-					trackingMethod: 'analytics',
-					action: tracker.ACTIONS.IMPRESSION,
-					category: editorType,
-					label: 'draft-discard'
-				});
-
-				onDismiss(initialContent);
-			});
+			jquery.showModal(window.wgPageName, window.mediaWiki.message('edit-draft-loaded').text());
 		}
 
 		// bind to editform submit event to track successful edits form draft restore
@@ -196,9 +145,9 @@ define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, 
 	}
 
 	// store the draft key in the form as a hidden field
-	$(function () {
+	jquery(function () {
 		editForm.append(
-			$('<input>').
+			jquery('<input>').
 				attr('type', 'hidden').
 				attr('name', EDIT_DRAFT_KEY_HIDDEN_FIELD).
 				attr('value', getDraftKey())
@@ -207,12 +156,14 @@ define('EditDraftSaving', ['jquery', 'wikia.log', 'wikia.tracker'], function($, 
 
 	return {
 		SAVES_INTERVAL: 5000, // in [ms]
+
 		log: log,
+
 		checkDraftConflict: checkDraftConflict,
 		onDraftRestore: onDraftRestore,
+
 		// getDraftKey: getDraftKey,
 		storeDraft: storeDraft,
-		readDraft: readDraft,
-		storeOriginalContent: storeOriginalContent,
+		readDraft: readDraft
 	}
 });
