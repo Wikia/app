16aa9c5d93a793d6e2d5dad88b8a6e3fc743419b
mech <wozniak.jacek@gmail.com>
2019-03-07 17:17:34 +0100

    Move Special:HealthCheck logic to health.php

--- health.php
+++ health.php
@@ -1,3 +1,22 @@
 <?php
 
-echo 'OK';
+$statusCode = 200;
+$statusMsg = 'OK';
+
+if ( file_exists( "/usr/wikia/conf/current/host_disabled" ) ||
+	 file_exists( "/etc/disabled/apache" ) ) {
+	# failure!
+	$statusCode = 503;
+	$statusMsg  = 'Server status is: NOT OK - Disabled';
+}
+
+http_response_code( $statusCode );
+
+header( 'Cache-Control: private, must-revalidate, max-age=0' );
+header( 'Expires: Thu, 01 Jan 1970 00:00:00 GMT' );
+if ( function_exists( 'posix_uname' ) ) {
+	$uname = posix_uname();
+	header( 'X-Served-By: ' . $uname['nodename'] );
+}
+
+echo $statusMsg;
