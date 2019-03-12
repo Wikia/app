aa8713e677bb896cbc759b4b25a355cfc4eba3eb
Michał Kalinowski <mkalinowski@wikia-inc.com>
2019-03-12 14:31:49 +0100

    SER-3059 Support for ignoring source non-existence

--- includes/filerepo/backend/FileBackend.php
+++ includes/filerepo/backend/FileBackend.php
@@ -713,6 +713,10 @@ abstract class FileBackend {
 		$i = strrpos( $path, '.' );
 		return strtolower( $i ? substr( $path, $i + 1 ) : '' );
 	}
+
+	public function ignoresSourceExistence() {
+		return false;
+	}
 }
 
 /**
@@ -1784,9 +1788,5 @@ class FileBackendStoreShardListIterator implements Iterator {
 		} else {
 			return $this->iter->valid();
 		}
-	}
-
-	public function ignoresSourceExistence() {
-		return false;
 	}
 }
