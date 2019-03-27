61c4406eefe0dd137621eb7f76bac14c9f6cda2c
Máté Szabó <mszabo@wikia-inc.com>
2019-03-27 14:12:29 +0100

    IW-1821 | Bugfix

--- extensions/wikia/FeedsAndPosts/FeedsAndPostsController.class.php
+++ extensions/wikia/FeedsAndPosts/FeedsAndPostsController.class.php
@@ -67,9 +67,9 @@ class FeedsAndPostsController extends WikiaApiController {
 		$this->response->setValues( [
 			'title' => $title->getText(),
 			'exists' => $title->exists(),
-			'thumbnail' => $images[0] ?? null,
-			'content_images' => count($images) > 1 ? array_slice($images, 1) : [],
-			'snippet' => ArticleData::getTextSnippet($title),
+			'thumbnail' => null,
+			'content_images' => [],
+			'snippet' => null,
 			'relativeUrl' => '',
 		]);
 	}
