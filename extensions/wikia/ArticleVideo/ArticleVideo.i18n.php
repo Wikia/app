7e593bb92dc6e1ea56c5df7b16141d222afe7d3f
Karol Tatala <ktatala@wikia-inc.com>
2019-04-09 18:28:20 +0000

    download translations

--- extensions/wikia/ArticleVideo/ArticleVideo.i18n.php
+++ extensions/wikia/ArticleVideo/ArticleVideo.i18n.php
@@ -25,18 +25,30 @@ $messages['de'] = array(
 	'articlevideo-watch' => 'ansehen',
 	'articlevideo-feedback-question' => 'Gefällt dir dieses Video?',
 	'articlevideo-attribution-from' => 'von',
+	'right-showfeaturedvideo' => 'Vorgestellte Videos immer anzeigen',
+	'articlevideo-preference-label' => 'Vorgestelltes Video',
+	'articlevideo-preference-option-show-featured-video' => 'Alle Videos anzeigen',
+	'articlevideo-preference-option-disable-featured-video' => 'Nicht alle Videos anzeigen',
 );
 
 $messages['es'] = array(
 	'articlevideo-watch' => 'mirar',
 	'articlevideo-feedback-question' => '¿Te gusta este video?',
 	'articlevideo-attribution-from' => 'de',
+	'right-showfeaturedvideo' => 'Siempre mostrar los videos destacados',
+	'articlevideo-preference-label' => 'Video Destacado',
+	'articlevideo-preference-option-show-featured-video' => 'Mostrar todos los videos',
+	'articlevideo-preference-option-disable-featured-video' => 'No mostrar todos los videos',
 );
 
 $messages['fr'] = array(
 	'articlevideo-watch' => 'regarder',
 	'articlevideo-feedback-question' => 'Aimez-vous cette vidéo ?',
 	'articlevideo-attribution-from' => 'de',
+	'right-showfeaturedvideo' => 'Toujours afficher les vidéos à la une',
+	'articlevideo-preference-label' => 'Vidéo à la une',
+	'articlevideo-preference-option-show-featured-video' => 'Afficher toutes les vidéos',
+	'articlevideo-preference-option-disable-featured-video' => 'Ne pas afficher toutes les vidéos',
 );
 
 $messages['it'] = array(
@@ -53,35 +65,59 @@ $messages['ja'] = array(
 	'articlevideo-watch' => '再生時間',
 	'articlevideo-feedback-question' => 'この動画はいかがでしたか？',
 	'articlevideo-attribution-from' => '作成：',
+	'right-showfeaturedvideo' => 'メインの動画を常に表示する',
+	'articlevideo-preference-label' => 'メインの動画',
+	'articlevideo-preference-option-show-featured-video' => 'すべての動画を表示する',
+	'articlevideo-preference-option-disable-featured-video' => 'すべての動画を表示しない',
 );
 
 $messages['pl'] = array(
 	'articlevideo-watch' => 'obejrzyj',
 	'articlevideo-feedback-question' => 'Czy podoba ci się ten film?',
 	'articlevideo-attribution-from' => 'dodany przez',
+	'right-showfeaturedvideo' => 'Zawsze pokazuj polecane filmy',
+	'articlevideo-preference-label' => 'Polecany film',
+	'articlevideo-preference-option-show-featured-video' => 'Pokaż wszystkie filmy',
+	'articlevideo-preference-option-disable-featured-video' => 'Nie pokazuj wszystkich filmów',
 );
 
 $messages['pt'] = array(
 	'articlevideo-watch' => 'assistir',
 	'articlevideo-feedback-question' => 'Você gosta deste vídeo?',
 	'articlevideo-attribution-from' => 'de',
+	'right-showfeaturedvideo' => 'Sempre mostrar vídeos em destaque',
+	'articlevideo-preference-label' => 'Vídeo em destaque',
+	'articlevideo-preference-option-show-featured-video' => 'Mostrar todos os vídeos',
+	'articlevideo-preference-option-disable-featured-video' => 'Não mostrar todos os vídeos',
 );
 
 $messages['ru'] = array(
 	'articlevideo-watch' => 'длительность',
 	'articlevideo-feedback-question' => 'Вам нравится это видео?',
 	'articlevideo-attribution-from' => 'автор:',
+	'right-showfeaturedvideo' => 'Всегда показывать избранные видео',
+	'articlevideo-preference-label' => 'Избранные видео',
+	'articlevideo-preference-option-show-featured-video' => 'Показывать все видео',
+	'articlevideo-preference-option-disable-featured-video' => 'Не показывать все видео',
 );
 
 $messages['zh-hans'] = array(
 	'articlevideo-watch' => '观看',
 	'articlevideo-feedback-question' => '你喜欢这个视频吗？',
 	'articlevideo-attribution-from' => '来源',
+	'right-showfeaturedvideo' => '始终显示精选视频',
+	'articlevideo-preference-label' => '精选视频',
+	'articlevideo-preference-option-show-featured-video' => '显示所有视频',
+	'articlevideo-preference-option-disable-featured-video' => '不显示所有视频',
 );
 
 $messages['zh-hant'] = array(
 	'articlevideo-watch' => '觀看',
 	'articlevideo-feedback-question' => '你喜歡這個影片嗎？',
 	'articlevideo-attribution-from' => '來源：',
+	'right-showfeaturedvideo' => '始終顯示精選影片',
+	'articlevideo-preference-label' => '精選影片',
+	'articlevideo-preference-option-show-featured-video' => '顯示所有影片',
+	'articlevideo-preference-option-disable-featured-video' => '不顯示所有影片',
 );
 
