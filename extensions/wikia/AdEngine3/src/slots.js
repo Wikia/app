2f14b051b6e87932298f569c31dab64f8dd9302e
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2019-04-11 07:45:18 -0700

    ADEN-8651 Mark slot which should be always bidded

--- extensions/wikia/AdEngine3/src/slots.js
+++ extensions/wikia/AdEngine3/src/slots.js
@@ -272,7 +272,7 @@ export default {
 
 	setupStates() {
 		slotService.setState('hivi_leaderboard', false);
-		slotService.setState('top_leaderboard', false);
+		slotService.setState('top_leaderboard', true);
 		slotService.setState('top_boxad', isTopBoxadApplicable());
 		slotService.setState('incontent_boxad_1', true);
 		slotService.setState('bottom_leaderboard', true);
@@ -327,15 +327,17 @@ export default {
 	setupTopLeaderboard() {
 		if (context.get('custom.hiviLeaderboard')) {
 			slotService.setState('hivi_leaderboard', true);
+			context.set('slots.top_leaderboard.firstCall', false);
 			context.push('state.adStack', { id: 'hivi_leaderboard' });
 
+			slotService.on('hivi_leaderboard', AdSlot.STATUS_SUCCESS, () => {
+				slotService.setState('top_leaderboard', false);
+			});
+
 			slotService.on('hivi_leaderboard', AdSlot.STATUS_COLLAPSE, () => {
-				slotService.setState('top_leaderboard', true);
-				context.set('slots.top_leaderboard.firstCall', false);
 				context.push('state.adStack', { id: 'top_leaderboard' });
 			});
 		} else {
-			slotService.setState('top_leaderboard', true);
 			context.push('state.adStack', { id: 'top_leaderboard' });
 		}
 	},
