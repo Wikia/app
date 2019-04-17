dfa8c52ec8be3ea5d430bd162447917974469f6a
Michał Kalinowski <mkalinowski@wikia-inc.com>
2019-04-17 11:00:27 +0200

    SER-3089 Tests fix

--- extensions/wikia/Privacy/tests/RemoveUserDataOnWikiTaskTest.php
+++ extensions/wikia/Privacy/tests/RemoveUserDataOnWikiTaskTest.php
@@ -26,7 +26,7 @@ class RemoveUserDataOnWikiTaskTest extends WikiaDatabaseTest {
 
 	protected function extraSchemaFiles() {
 		return [
-			__DIR__ . '/fixtures/audit_log.sql'
+			__DIR__ . '/fixtures/audit_log.sql',
 		];
 	}
 
@@ -36,18 +36,20 @@ class RemoveUserDataOnWikiTaskTest extends WikiaDatabaseTest {
 	}
 
 	public function testShouldRemoveUserData() {
-		(new RemoveUserDataOnWikiTask())->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID, self::TEST_USER_ID, self::OLD_TEST_USER_ID );
+		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID,
+			self::TEST_USER_ID );
 		$logPager = new CheckUserLogPager( null, [], null, null );
-		$this->assertEquals( 1,  $logPager->getNumRows() );
+		$this->assertEquals( 1, $logPager->getNumRows() );
 		foreach ( $logPager->getResult() as $row ) {
 			$this->assertNotEquals( self::TEST_USER_ID, $row->cul_target_id );
 		}
 	}
 
 	public function testShouldRemoveIpsFromRecentChanges() {
-		(new RemoveUserDataOnWikiTask())->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID, self::TEST_USER_ID, self::OLD_TEST_USER_ID );
+		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID,
+			self::TEST_USER_ID );
 		$dbr = wfGetDB( DB_SLAVE );
-		$changes = $dbr->select( 'recentchanges', ['rc_user', 'rc_user_text', 'rc_ip_bin'] );
+		$changes = $dbr->select( 'recentchanges', [ 'rc_user', 'rc_user_text', 'rc_ip_bin' ] );
 		foreach ( $changes as $change ) {
 			if ( $change->rc_user == self::TEST_USER_ID ) {
 				$this->assertTrue( empty( $change->rc_ip_bin ) );
@@ -58,41 +60,46 @@ class RemoveUserDataOnWikiTaskTest extends WikiaDatabaseTest {
 	}
 
 	public function testShouldRemoveAbuseFilterData() {
-		(new RemoveUserDataOnWikiTask())->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID, self::TEST_USER_ID, self::OLD_TEST_USER_ID );
+		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID,
+			self::TEST_USER_ID );
 		$dbr = wfGetDB( DB_SLAVE );
 		// check filter author
-		$filters = $dbr->select( 'abuse_filter', ['af_user', 'af_user_text'] );
+		$filters = $dbr->select( 'abuse_filter', [ 'af_user', 'af_user_text' ] );
 		foreach ( $filters as $f ) {
 			if ( $f->af_user == self::TEST_USER_ID ) {
 				$this->assertTrue( empty( $f->af_user_text ) );
 			}
 		}
 		// check author in filter history
-		$history = $dbr->select( 'abuse_filter_history', ['afh_user', 'afh_user_text'] );
+		$history = $dbr->select( 'abuse_filter_history', [ 'afh_user', 'afh_user_text' ] );
 		foreach ( $history as $h ) {
 			if ( $h->afh_user == self::TEST_USER_ID ) {
 				$this->assertTrue( empty( $h->afh_user_text ) );
 			}
 		}
 		// check abuse filter log
-		$log = $dbr->select( 'abuse_filter_log', ['afl_user'], ['afl_user' => self::TEST_USER_ID] );
+		$log =
+			$dbr->select( 'abuse_filter_log', [ 'afl_user' ],
+				[ 'afl_user' => self::TEST_USER_ID ] );
 		$this->assertEquals( 0, $log->numRows() );
 	}
 
 	public function testShouldRemoveUserPageHistoryFromRecentChanges() {
-		(new RemoveUserDataOnWikiTask())->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID, self::TEST_USER_ID, self::OLD_TEST_USER_ID );
+		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID,
+			self::TEST_USER_ID );
 		$dbr = wfGetDB( DB_SLAVE );
-		$changes = $dbr->select( 'recentchanges', ['rc_namespace', 'rc_title'] );
+		$changes = $dbr->select( 'recentchanges', [ 'rc_namespace', 'rc_title' ] );
 		foreach ( $changes as $change ) {
-			$isUserPage = in_array( $change->rc_namespace, RemoveUserDataOnWikiTask::USER_NAMESPACES );
+			$isUserPage =
+				in_array( $change->rc_namespace, RemoveUserDataOnWikiTask::USER_NAMESPACES );
 			$startsWithUsername = strpos( $change->rc_title, self::TEST_USER_DB_KEY ) === 0;
 			$this->assertFalse( $isUserPage && $startsWithUsername );
 		}
 	}
 
 	public function testShouldRemoveUserPages() {
-		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID, self::TEST_USER_ID, self::OLD_TEST_USER_ID,
-			self::OLD_TEST_USER );
+		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID,
+			self::TEST_USER_ID );
 
 		$dbr = wfGetDB( DB_SLAVE );
 
@@ -113,7 +120,8 @@ class RemoveUserDataOnWikiTaskTest extends WikiaDatabaseTest {
 	}
 
 	public function testShouldRemoveActionLogs() {
-		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID, self::TEST_USER_ID, self::OLD_TEST_USER_ID );
+		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID,
+			self::TEST_USER_ID );
 
 		$db = wfGetDB( DB_MASTER );
 
@@ -124,23 +132,25 @@ class RemoveUserDataOnWikiTaskTest extends WikiaDatabaseTest {
 	}
 
 	public function testShouldRemoveWatchlist() {
-		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID, self::TEST_USER_ID, self::OLD_TEST_USER_ID );
+		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID,
+			self::TEST_USER_ID );
 
 		$db = wfGetDB( DB_SLAVE );
 
 		$this->assertEquals( 0,
-			$db->selectField( 'watchlist', 'count(*)', ['wl_user' => self::TEST_USER_ID], __METHOD__ ),
-			'Watchlist is not empty');
+			$db->selectField( 'watchlist', 'count(*)', [ 'wl_user' => self::TEST_USER_ID ],
+				__METHOD__ ), 'Watchlist is not empty' );
 	}
 
 	public function testShouldHaveAuditLog() {
-		(new RemoveUserDataOnWikiTask())->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID, self::TEST_USER_ID, self::OLD_TEST_USER_ID );
+		( new RemoveUserDataOnWikiTask() )->removeUserDataOnCurrentWiki( self::TEST_AUDIT_ID,
+			self::TEST_USER_ID );
 
 		$dbr = wfGetDB( DB_SLAVE );
 
-		$this->assertEquals( 1,
-			$dbr->selectField( 'rtbf_log_details', 'count(*)', [ 'log_id' => self::TEST_AUDIT_ID, 'was_successful' => 1 ],
-				__METHOD__ ), 'No audit log found' );
+		$this->assertEquals( 1, $dbr->selectField( 'rtbf_log_details', 'count(*)',
+			[ 'log_id' => self::TEST_AUDIT_ID, 'was_successful' => 1 ], __METHOD__ ),
+			'No audit log found' );
 	}
 
 }
