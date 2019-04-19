dd8fffc3d78dd7a632a8a2c9556926d51b600aac
Jamie Shepherd <hello@jamie.sh>
2019-04-19 10:46:32 -0700

    Add tracking status

--- extensions/wikia/TriviaQuizzes/js/ext.TriviaQuizzes.js
+++ extensions/wikia/TriviaQuizzes/js/ext.TriviaQuizzes.js
@@ -1,11 +1,21 @@
-require(['jquery', 'content_types_consumption'], function ($, consumption) {
+require(['jquery', 'content_types_consumption', 'wikia.trackingOptIn'], function ($, consumption, trackingOptIn) {
 	'use strict';
 	$(function() {
+		console.log();
+
 		$('.WikiaRail').on('afterLoad.rail', function() {
+			var cityId = window.wgCityId ? window.wgCityId : 0;
+			var dbName = window.wgDBname ? window.wgDBname : null;
+			var user = window.wgUserName ? { id: 123, isGDPRApproved: trackingOptIn.isOptedIn() } : null;
+
+			// https://github.com/Wikia/content-types/tree/master/consumption#usage
 			consumption.default('wikia-trivia-quizzes', {
-				user: {
-					id: 0,
-					isGDPRApproved: true,
+				environment: 'media-wiki',
+				pageType: 'article',
+				user: user,
+				community: {
+					id: cityId,
+					dbName: dbName,
 				},
 			});
 		});
