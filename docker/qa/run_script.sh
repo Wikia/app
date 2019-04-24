4bcc3b83b3f8331932b98b159fc207f8d3b8edb0
abador <przemyslaw@czaus.pl>
2019-04-24 16:42:51 +0200

    Fix run, PLATFORM-3947

--- docker/qa/run_script.sh
+++ docker/qa/run_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd rebuild
-echo Vault token: $VAULT_TOKEN
 python ./bin/recreate_wiki.py --env=sjc  --debug --vault-address=https://active.vault.service.sjc.consul:8200 --vault-token=$VAULT_TOKEN --city-dbname=szlmercurycc --app-base-path='../app'
 python bin/clear_fastly_cache.py --env=sjc --vault-address=https://active.vault.service.sjc.consul:8200 --vault-token=$VAULT_TOKEN --app-base-path='../app'
