3cdcd5116bc0b014f794757a32a66def4a1fa30d
abador <przemyslaw@czaus.pl>
2019-04-26 09:01:57 +0200

    Fix run, PLATFORM-3947

--- docker/qa/run_script.sh
+++ docker/qa/run_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd python /usr/wikia/slot1/current/src/rebuild
-python ./bin/recreate_wiki.py --env=sjc  --debug --vault-address=https://active.vault.service.sjc.consul:8200 --vault-token=$VAULT_TOKEN --city-dbname=szlmercurycc --app-base-path='/usr/wikia/slot1/current/src'
+cd python /usr/wikia/slot1/current/src/rebuild &&\
+python ./bin/recreate_wiki.py --env=sjc  --debug --vault-address=https://active.vault.service.sjc.consul:8200 --vault-token=$VAULT_TOKEN --city-dbname=szlmercurycc --app-base-path='/usr/wikia/slot1/current/src' &&\
 python ./bin/clear_fastly_cache.py --env=sjc --vault-address=https://active.vault.service.sjc.consul:8200 --vault-token=$VAULT_TOKEN --app-base-path='/usr/wikia/slot1/current/src'
