9d7cb806ede092b35f3ee314ab82713957ba43e4
Jamie Shepherd <hello@jamie.sh>
2019-05-06 16:29:56 -0700

    Note about wikitext

--- extensions/wikia/TaxonomyArticleHooks/TaxonomyArticleHooks.class.php
+++ extensions/wikia/TaxonomyArticleHooks/TaxonomyArticleHooks.class.php
@@ -1,28 +1,29 @@
 <?php
 
-use Wikia\Logger\WikiaLogger;
 use Wikia\Rabbit\ConnectionBase;
 
 /**
  * @author Lore team
  *
- * Something
+ * Send plaintext versions of articles to RabbitMQ as they are edited on Fandom
  */
 
 class TaxonomyArticleHooks {
-    const ROUTING_KEY = "taxonomy-jamie-mw-test";
+    const ROUTING_KEY = "taxonomy.article-edits";
 
     protected static $rabbitConnection;
 
-	static public function onArticleEditUpdates(\WikiPage $page, $editInfo, bool $changed) {
+	static public function onArticleEditUpdates(\WikiPage $page, $editInfo ) {
         global $wgCityId;
 
+        // We currently do not need wikitext for the classifier, but it may be useful
+        //  to have in the future. Uncomment this line to include wikitext in the message
         $message = [
             'wikiId' => $wgCityId,
             'articleId' => $page->getId(),
             'articleTitle' => $page->getTitle()->getText(),
-            // 'articleWikitext' => $article->getText(),
-            'articlePlaintext' => html_entity_decode( strip_tags( $editInfo->output->getText()), ENT_COMPAT, 'UTF-8' )
+            // 'articleWikitext' => $page->getText(),
+            'articlePlaintext' => html_entity_decode( strip_tags( $editInfo->output->getText() ), ENT_COMPAT, 'UTF-8' )
         ];
 
         self::getRabbitConnection()->publish( self::ROUTING_KEY, $message );
