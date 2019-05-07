4697fa4b873d5e8aa366a4277214b48239f70946
Jacek Bronski-Jankowski <40768156+jacekbj@users.noreply.github.com>
2019-05-07 17:51:38 +0200

    ADEN-8682 Reuse wgVar. (#16994)

--- extensions/wikia/AdEngine/AdEngine2Hooks.class.php
+++ extensions/wikia/AdEngine/AdEngine2Hooks.class.php
@@ -61,6 +61,7 @@ class AdEngine2Hooks {
 		$vars[] = 'wgAdDriverFVPostrollCountries';
 		$vars[] = 'wgAdDriverHighImpactSlotCountries';
 		$vars[] = 'wgAdDriverHighImpact2SlotCountries';
+		$vars[] = 'wgAdDriverMobileFloorAdhesionCountries';
 		$vars[] = 'wgAdDriverIncontentPlayerRailCountries';
 		$vars[] = 'wgAdDriverIncontentPlayerSlotCountries';
 		$vars[] = 'wgAdDriverIndexExchangeBidderCountries';
