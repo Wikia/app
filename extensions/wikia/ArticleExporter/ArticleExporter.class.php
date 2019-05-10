5ccacac9fd03f9d15bc060d40c084845ee828d03
nmonterroso <nmonterroso@users.noreply.github.com>
2019-05-10 10:29:44 -0700

    hopefully fixes one of the tests, adds locale

--- extensions/wikia/ArticleExporter/ArticleExporter.class.php
+++ extensions/wikia/ArticleExporter/ArticleExporter.class.php
@@ -12,11 +12,12 @@ class ArticleExporter {
         $articles = [];
         foreach ( $ids as $id ) {
             $article = $this->getArticle( $id );
-            $title = Title::newFromID( $id ); // some items not available through parse api
+            $title = $this->loadTitle( $id ); // some items not available through parse api
 
             if ( $article ) {
                 $articles[$id] = [
                     'wikiId' => $cityId,
+					'lang' => $this->getContentLang(),
                     'pageId' => $id,
                     'namespace' => $title->getNamespace(),
                     'revisionId' => strval( $article['parse']['revid'] ),
@@ -43,6 +44,11 @@ class ArticleExporter {
         );
     }
 
+    public function getContentLang() {
+    	global $wgContLanguageCode;
+    	return $wgContLanguageCode;
+	}
+
     // Return a quick plaintext version of the article content. Ideally we want to pull out things
     // like "Edit" and the TOC - but for the first version this should get us 99% of the way there
     public function getPlaintext( $text ) {
@@ -51,6 +57,10 @@ class ArticleExporter {
 
         return str_replace( [ "&lt;", "&gt;" ], "", $text );
     }
+
+    public function loadTitle( $id ) {
+    	return Title::newFromID($id);
+	}
 
     public function getCategories( $rawCategories ) {
         $categories = [];
--- extensions/wikia/ArticleExporter/tests/ArticleExporterTest.php
+++ extensions/wikia/ArticleExporter/tests/ArticleExporterTest.php
@@ -2,78 +2,100 @@
 use PHPUnit\Framework\TestCase;
 
 class ArticleExporterTest extends TestCase {
-    public $articleExporter;
-    public $parseResponse;
+	public $articleExporter;
+	public $parseResponse;
 
-    public function setUp() {
-        parent::setUp();
-        require_once __DIR__ . '/../ArticleExporter.class.php';
+	public function setUp() {
+		parent::setUp();
+		require_once __DIR__ . '/../ArticleExporter.class.php';
 
-        $this->articleExporter = new ArticleExporter();
-        $this->parseResponse = ['parse' => [
-            'title' => 'lorem',
-            'revid' => 123,
-            'text' => ['*' => '<div class=\"lorem\">Lorem ipsum</div>'],
-            'categories' => [['*' => 'Character'], ['*' => 'Cast']],
-            'links' => [['*' => 'Jon Snow'], ['*' => 'Ghost']],
-            'displaytitle' => 'lorem'
-        ]];
-    }
+		$this->articleExporter = new ArticleExporter();
+		$this->parseResponse = ['parse' => [
+			'title' => 'lorem',
+			'revid' => 123,
+			'text' => ['*' => '<div class=\"lorem\">Lorem ipsum</div>'],
+			'categories' => [['*' => 'Character'], ['*' => 'Cast']],
+			'links' => [['*' => 'Jon Snow'], ['*' => 'Ghost']],
+			'displaytitle' => 'lorem'
+		]];
+	}
 
-    public function testSingleArticle() {
-        $mockArticleExporter = $this->getMockBuilder('ArticleExporter')
-            ->setMethods( [ 'getArticle', 'getPageUrl' ] )
-            ->getMock();
+	public function testSingleArticle() {
+		$jonSnow = $this->buildMockTitle( 'https://community.fandom/wiki/Jon_Snow' );
+		$articleExplorer = $this->buildMockArticleExplorer();
 
-        $mockArticleExporter->expects($this->once())
-            ->method('getArticle')
-            ->will($this->returnValue( $this->parseResponse ));
+		$articleExplorer->expects($this->once())
+			->method('getArticle')
+			->will($this->returnValue( $this->parseResponse ));
 
-        $mockArticleExporter->expects($this->once())
-            ->method('getPageUrl')
-            ->will($this->returnValue( 'https://community.fandom/wiki/Jon_Snow' ));
+		$articleExplorer->expects($this->once())
+			->method('loadTitle')
+			->will($this->returnValue( $jonSnow ));
 
-        $articles = $mockArticleExporter->build('1', ['1']);
+		$articles = $articleExplorer->build('1', ['1']);
 
-        $this->assertEquals(1, sizeof($articles));
-    }
+		$this->assertEquals(1, sizeof($articles));
+	}
 
-    public function testMultipleArticles() {
-        $mockArticleExporter = $this->getMockBuilder('ArticleExporter')
-            ->setMethods( [ 'getArticle', 'getPageUrl' ] )
-            ->getMock();
+	public function testMultipleArticles() {
+		$mockArticleExporter = $this->buildMockArticleExplorer();
 
-        $mockArticleExporter->expects($this->exactly(2))
-            ->method('getArticle')
-            ->will($this->returnValue( $this->parseResponse ));
+		$mockArticleExporter->expects($this->exactly(2))
+			->method('getArticle')
+			->will($this->returnValue( $this->parseResponse ));
 
-        $mockArticleExporter->expects($this->exactly(2))
-            ->method('getPageUrl')
-            ->will($this->returnValue( 'https://community.fandom/wiki/Jon_Snow' ));
+		$mockArticleExporter->expects($this->exactly(2))
+			->method('getPageUrl')
+			->will($this->returnValue( 'https://community.fandom/wiki/Jon_Snow' ));
 
-        $articles = $mockArticleExporter->build('1', ['1', '2']);
+		$articles = $mockArticleExporter->build('1', ['1', '2']);
 
-        $this->assertEquals(2, sizeof($articles));
-    }
+		$this->assertEquals(2, sizeof($articles));
+	}
 
-    public function testGetPlaintext() {
-        $plaintext = $this->articleExporter->getPlaintext($this->parseResponse['parse']['text']['*']);
-        $this->assertEquals('Lorem ipsum', $plaintext);
-    }
+	public function testGetPlaintext() {
+		$plaintext = $this->articleExporter->getPlaintext($this->parseResponse['parse']['text']['*']);
+		$this->assertEquals('Lorem ipsum', $plaintext);
+	}
 
-    public function testGetCategories() {
-        $categories = $this->articleExporter->getCategories($this->parseResponse['parse']['categories']);
-        $this->assertEquals(['Character', 'Cast'], $categories);
-    }
+	public function testGetCategories() {
+		$categories = $this->articleExporter->getCategories($this->parseResponse['parse']['categories']);
+		$this->assertEquals(['Character', 'Cast'], $categories);
+	}
 
-    public function testGetPageTitles() {
-        $titles = $this->articleExporter->getPageTitles($this->parseResponse['parse']['links']);
-        $this->assertEquals(['Jon Snow', 'Ghost'], $titles);
-    }
+	public function testGetPageTitles() {
+		$titles = $this->articleExporter->getPageTitles($this->parseResponse['parse']['links']);
+		$this->assertEquals(['Jon Snow', 'Ghost'], $titles);
+	}
 
-    public function testGetUpdated() {
-        $updated = $this->articleExporter->getUpdated(0);
-        $expected = wfTimestamp( TS_ISO_8601, 0);
-        $this->assertEquals($expected, $updated);
-    }
+	public function testGetUpdated() {
+		$updated = $this->articleExporter->getUpdated(0);
+		$expected = wfTimestamp( TS_ISO_8601, 0);
+		$this->assertEquals($expected, $updated);
+	}
+
+	private function buildMockArticleExplorer() {
+		$mockArticleExporter = $this->getMockBuilder('ArticleExporter')
+			->setMethods( [ 'getArticle', 'loadTitle', 'getContentLang' ] )
+			->getMock();
+		$mockArticleExporter->expects($this->atLeastOnce())
+			->method('getContentLang')
+			->will($this->returnValue('en'));
+
+		return $mockArticleExporter;
+	}
+
+	private function buildMockTitle( $url ) {
+		$mockTitle = $this->getMockBuilder('Title')
+			->setMethods([ 'getFullUrl', 'getNamespace' ])
+			->getMock();
+		$mockTitle->expects($this->once())
+			->method('getFullUrl')
+			->will($this->returnValue( 'https://community.fandom/wiki/Jon_Snow' ));
+		$mockTitle->expects($this->once())
+			->method('getNamespace')
+			->will($this->returnValue(NS_MAIN));
+
+		return $mockTitle;
+	}
 }
