cae8370aa83beebda9652e860a4cba95a8f42570
nmonterroso <nmonterroso@users.noreply.github.com>
2019-05-10 10:48:19 -0700

    fixing spacing

--- extensions/wikia/ArticleExporter/ArticleExporter.class.php
+++ extensions/wikia/ArticleExporter/ArticleExporter.class.php
@@ -1,86 +1,86 @@
 <?php
 /**
-* ArticleExporter methods for getting and processing articles
-*
-* @author Lore team
-* @package ArticleExporter
-*/
+ * ArticleExporter methods for getting and processing articles
+ *
+ * @author Lore team
+ * @package ArticleExporter
+ */
 class ArticleExporter {
-    const SPACE_SEQUENCE_REGEXP = "/\s+/";
+	const SPACE_SEQUENCE_REGEXP = "/\s+/";
 
-    public function build($cityId, $ids) {
-        $articles = [];
-        foreach ( $ids as $id ) {
-            $article = $this->getArticle( $id );
-            $title = $this->loadTitle( $id ); // some items not available through parse api
+	public function build($cityId, $ids) {
+		$articles = [];
+		foreach ( $ids as $id ) {
+			$article = $this->getArticle( $id );
+			$title = $this->loadTitle( $id ); // some items not available through parse api
 
-            if ( $article ) {
-                $articles[$id] = [
-                    'wikiId' => $cityId,
+			if ( $article ) {
+				$articles[$id] = [
+					'wikiId' => $cityId,
 					'lang' => $this->getContentLang(),
-                    'pageId' => $id,
-                    'namespace' => $title->getNamespace(),
-                    'revisionId' => strval( $article['parse']['revid'] ),
-                    'title' => $article['parse']['title'],
-                    'url' => $title->getFullURL(),
-                    'plaintextContent' => $this->getPlaintext( $article['parse']['text']['*'] ),
-                    'categories' => $this->getCategories( $article['parse']['categories'] ),
-                    'linkedPageTitles' => $this->getPageTitles( $article['parse']['links'] ),
-                    'updatedUtc' => $this->getUpdated( $article['parse']['revid'] ),
-                ];
-            }
-        }
+					'pageId' => $id,
+					'namespace' => $title->getNamespace(),
+					'revisionId' => strval( $article['parse']['revid'] ),
+					'title' => $article['parse']['title'],
+					'url' => $title->getFullURL(),
+					'plaintextContent' => $this->getPlaintext( $article['parse']['text']['*'] ),
+					'categories' => $this->getCategories( $article['parse']['categories'] ),
+					'linkedPageTitles' => $this->getPageTitles( $article['parse']['links'] ),
+					'updatedUtc' => $this->getUpdated( $article['parse']['revid'] ),
+				];
+			}
+		}
 
-        return $articles;
-    }
-
-    public function getArticle( $pageId ) {
-        return \ApiService::call(
-            [
-                'pageid' => $pageId,
-                'action' => 'parse',
-                'prop' => 'text|revid|categories|links|displaytitle',
-            ]
-        );
-    }
-
-    public function getContentLang() {
-    	global $wgContLanguageCode;
-    	return $wgContLanguageCode;
+		return $articles;
 	}
 
-    // Return a quick plaintext version of the article content. Ideally we want to pull out things
-    // like "Edit" and the TOC - but for the first version this should get us 99% of the way there
-    public function getPlaintext( $text ) {
-        $text = html_entity_decode( strip_tags( $text ), ENT_COMPAT, 'UTF-8' );
-        $text = preg_replace( self::SPACE_SEQUENCE_REGEXP, ' ', $text );
-
-        return str_replace( [ "&lt;", "&gt;" ], "", $text );
-    }
-
-    public function loadTitle( $id ) {
-    	return Title::newFromID($id);
+	public function getArticle( $pageId ) {
+		return \ApiService::call(
+			[
+				'pageid' => $pageId,
+				'action' => 'parse',
+				'prop' => 'text|revid|categories|links|displaytitle',
+			]
+		);
 	}
 
-    public function getCategories( $rawCategories ) {
-        $categories = [];
-        foreach ( $rawCategories as $category ) {
-            array_push( $categories, $category['*'] );
-        }
-        return $categories;
-    }
+	public function getContentLang() {
+		global $wgContLanguageCode;
+		return $wgContLanguageCode;
+	}
 
-    public function getPageTitles( $links ) {
-        $linkedPageTitles = [];
-        foreach ( $links as $link ) {
-            array_push( $linkedPageTitles, $link['*'] );
-        }
-        return $linkedPageTitles;
-    }
+	// Return a quick plaintext version of the article content. Ideally we want to pull out things
+	// like "Edit" and the TOC - but for the first version this should get us 99% of the way there
+	public function getPlaintext( $text ) {
+		$text = html_entity_decode( strip_tags( $text ), ENT_COMPAT, 'UTF-8' );
+		$text = preg_replace( self::SPACE_SEQUENCE_REGEXP, ' ', $text );
 
-    public function getUpdated( $revid ) {
-        $lastRev = \Revision::newFromId( $revid );
-        $timestamp = empty( $lastRev ) ? '' : $lastRev->getTimestamp();
-        return wfTimestamp( TS_ISO_8601, $timestamp ) ;
-    }
+		return str_replace( [ "&lt;", "&gt;" ], "", $text );
+	}
+
+	public function loadTitle( $id ) {
+		return Title::newFromID($id);
+	}
+
+	public function getCategories( $rawCategories ) {
+		$categories = [];
+		foreach ( $rawCategories as $category ) {
+			array_push( $categories, $category['*'] );
+		}
+		return $categories;
+	}
+
+	public function getPageTitles( $links ) {
+		$linkedPageTitles = [];
+		foreach ( $links as $link ) {
+			array_push( $linkedPageTitles, $link['*'] );
+		}
+		return $linkedPageTitles;
+	}
+
+	public function getUpdated( $revid ) {
+		$lastRev = \Revision::newFromId( $revid );
+		$timestamp = empty( $lastRev ) ? '' : $lastRev->getTimestamp();
+		return wfTimestamp( TS_ISO_8601, $timestamp ) ;
+	}
 }
