8cdaf8a3fa4bd782de9eaaea18cfd314aa55e734
abador <przemyslaw@czaus.pl>
2019-05-16 15:16:14 +0200

    Remove wikia.com reset tracking, PLATFORM-4095

--- extensions/wikia/TrackingOptIn/js/ext.wikia.trackingSettingsManager.js
+++ extensions/wikia/TrackingOptIn/js/ext.wikia.trackingSettingsManager.js
@@ -3,17 +3,7 @@ require(['wikia.trackingOptIn', 'mw'], function (trackingOptIn, mw) {
 	'use strict';
 
 	function createTrackingSettingsButton() {
-		var trackingSettingsButton = document.createElement('button');
-
-		trackingSettingsButton.classList.add('privacy-settings-button', 'wds-button');
-
-		trackingSettingsButton.textContent = mw.message('privacy-settings-button-toggle').text();
-		trackingSettingsButton.addEventListener('click', function () {
-			trackingOptIn.reset();
-		});
-
 		var articleContent = document.getElementById('mw-content-text');
-		articleContent.appendChild(trackingSettingsButton);
 
 		var fandomResetCookieForm = document.createElement('form');
 		fandomResetCookieForm.setAttribute('method', 'post');
