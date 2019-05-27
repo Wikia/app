c25867ec068675d40aba52cd00ae2ebcc37a08c8
Katarzyna Basiukajc <bckatarzyna@gmail.com>
2019-05-27 15:39:24 +0200

    ADEN-8666 | A9 and bidders context cleanup (#17081)
    * ADEN-8666 | A9 and bidders context cleanup
    
    * ADEN-8666 Update ad-engine (2f697290e36279f273110a035fd9a4a9b664f045)
    
    * ADEN-8666 Update ad-engine (3f9d9cf1694eea65b8d2b10e032733afb00df852)
    
    * ADEN-8666 Update ad-engine (v31.1.1)

--- extensions/wikia/AdEngine3/src/slots.js
+++ extensions/wikia/AdEngine3/src/slots.js
@@ -66,7 +66,6 @@ export default {
 			},
 			top_leaderboard: {
 				aboveTheFold: true,
-				bidderAlias: 'TOP_LEADERBOARD',
 				firstCall: true,
 				adProduct: 'top_leaderboard',
 				slotNameSuffix: '',
@@ -102,7 +101,6 @@ export default {
 			top_boxad: {
 				adProduct: 'top_boxad',
 				aboveTheFold: true,
-				bidderAlias: 'TOP_RIGHT_BOXAD',
 				slotNameSuffix: '',
 				group: 'MR',
 				options: {},
@@ -137,7 +135,6 @@ export default {
 			},
 			incontent_boxad_1: {
 				adProduct: 'incontent_boxad_1',
-				bidderAlias: 'INCONTENT_BOXAD_1',
 				slotNameSuffix: '',
 				group: 'HiVi',
 				options: {},
@@ -164,7 +161,6 @@ export default {
 			},
 			bottom_leaderboard: {
 				adProduct: 'bottom_leaderboard',
-				bidderAlias: 'BOTTOM_LEADERBOARD',
 				slotNameSuffix: '',
 				group: 'PF',
 				options: {},
@@ -190,7 +186,6 @@ export default {
 				avoidConflictWith: null,
 				autoplay: true,
 				audio: false,
-				bidderAlias: 'INCONTENT_PLAYER',
 				insertBeforeSelector: '#mw-content-text > h2',
 				insertBelowFirstViewport: true,
 				disabled: true,
@@ -234,7 +229,6 @@ export default {
 			},
 			featured: {
 				adProduct: 'featured',
-				bidderAlias: 'FEATURED',
 				slotNameSuffix: '',
 				nonUapSlot: true,
 				group: 'VIDEO',
