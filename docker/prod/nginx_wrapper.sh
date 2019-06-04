54bc86b89c714ae2955835dc090e4bdd43be4dba
Lukasz Adamczewski <adamczewski@gmail.com>
2019-06-04 10:23:04 +0200

    Revert "Platform 4100"

--- docker/prod/nginx_wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkfifo /var/log/nginx/std.log
-
-# envsubst is recommended by the creators of the nginx docker image
-CMD envsubst '${FASTCGI_READ_TIMEOUT}' < /etc/nginx/conf.d/site.conf.template > /etc/nginx/conf.d/default.conf && nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /var/log/nginx/std.log
