9e68831831e7dd4455b2022965bf33eddb15ebf0
Sir Jenkins <jenkins@fandom.com>
2019-06-05 19:46:02 +0000

    Automatic translations update

--- extensions/wikia/TrackingOptIn/TrackingSettingsManager.i18n.php
+++ extensions/wikia/TrackingOptIn/TrackingSettingsManager.i18n.php
@@ -8,56 +8,55 @@ $messages['en'] = array(
 );
 
 $messages['qqq'] = array(
-
 );
 
 $messages['de'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['es'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['fr'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['it'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['ja'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['pl'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['pt'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['ru'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['zh-hans'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
 $messages['zh-hant'] = array(
-
 	'privacy-settings-button-toggle-fandom' => 'Reset your tracking preferences on fandom.com',
+	'privacy-settings-button-toggle' => 'Reset your tracking preferences',
 );
 
