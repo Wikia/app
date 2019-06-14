9adc96ed3ae1ceacee83f9ded93a57bd6efffaef
Michał Kalinowski <mkalinowski@wikia-inc.com>
2019-06-14 10:36:26 +0200

    SER-3285 Include ArticleQuality in the indexing

--- extensions/wikia/Search/classes/IndexService/AllUnifiedSearch.php
+++ extensions/wikia/Search/classes/IndexService/AllUnifiedSearch.php
@@ -27,6 +27,7 @@ class AllUnifiedSearch extends AbstractService {
 		'Wikia\Search\IndexService\Redirects' => null,
 		'Wikia\Search\IndexService\WikiViews' => null,
 		'Wikia\Search\IndexService\VideoViews' => null, // note the order of operations -- AFTER metadata
+		'Wikia\Search\IndexService\ArticleQuality' => null,
 	];
 
 	/**
