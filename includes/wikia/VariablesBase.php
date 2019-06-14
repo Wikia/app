8688d0a6bfabbfdee6c291b9a562b1f626e8417a
grunny <mwgrunny@gmail.com>
2019-06-14 19:06:35 +1000

    Apply host header changes to current release branch

--- includes/wikia/VariablesBase.php
+++ includes/wikia/VariablesBase.php
@@ -8887,6 +8887,12 @@ $wgFandomComMigrationScheduled = false;
 $wgDomainMigrationDisabled = false;
 
 /**
+ * Environment-specific domain mappings to their normalized variants
+ * @var string[] $wgDomainOverrides
+ */
+$wgEnvironmentDomainMappings = [];
+
+/**
  * Whether the community is scheduled to be migrated to a wikia.org domain
  * @var bool $wgWikiaOrgMigrationScheduled
  */
