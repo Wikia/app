980441cfacc8bb083c3fe74c6a35321bd32a377b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-07-04 12:30:12 +0200

    IW-2154: add impression tracking for sponsored items in recirc

--- extensions/wikia/Recirculation/js/views/mixedFooter.js
+++ extensions/wikia/Recirculation/js/views/mixedFooter.js
@@ -101,6 +101,12 @@ define('ext.wikia.recirculation.views.mixedFooter', [
 
 	function setupTracking() {
 		var shouldSendAdditionalData = window.Wikia.AbTest.inGroup('RECOMMENDATION_SERVICE', 'EXPERIMENTAL');
+		var $sponsoredItem = $mixedContentFooter.find("[data-tracking*='sponsored-item']");
+
+		if ($sponsoredItem.length) {
+			console.log($sponsoredItem);
+			tracker.trackImpression($sponsoredItem.attr('href'));
+		}
 
 		tracker.trackImpression('footer');
 
