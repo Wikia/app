922597e0aa4b69763e9248988262051a0c279f75
Michał Chatlas <micha.chatlas@gmail.com>
2019-07-04 14:36:47 +0200

    Fixed permission problems in composer docker

--- docker/devbox/composer/wrapper.sh
+++ docker/devbox/composer/wrapper.sh
@@ -7,9 +7,16 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-adduser --home /app --shell /bin/bash --disabled-login --uid $USER_ID --system docker_user
+adduser --home /usr/wikia/slot1/current/src --shell /bin/bash --disabled-login --uid $USER_ID --system docker_user
 export HOME=/app
 
 set -Eeo pipefail
 
-exec gosu docker_user "$@"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+chown -R docker_user .
+
+gosu docker_user "$@"
+rm composer.phar
