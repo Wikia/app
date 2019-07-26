600ebe5eb21049f8ce1012dcf5f2146bb6acd9bf
Mateusz Sip <mateusz.sip@gmail.com>
2019-07-26 14:37:27 +0200

    Introduce not-so-visible dropdown to enable cross-wiki search

--- extensions/wikia/Search/templates/WikiaSearch_index.php
+++ extensions/wikia/Search/templates/WikiaSearch_index.php
@@ -5,41 +5,35 @@
 				<p class="advanced-link"><a href="#" id="advanced-link"><?= wfMessage( 'searchprofile-advanced' ) ?></a></p>
 			<?php endif ?>
 
-				<p class="grid-1 alpha">
-					<select name="scope">
-						<option
-							value="<?= \Wikia\Search\Config::SCOPE_INTERNAL ?>"
-							<?= $scope === \Wikia\Search\Config::SCOPE_INTERNAL ? 'selected="selected"' : '' ?>
-						>This wiki</option>
-						<option
-							value="<?= \Wikia\Search\Config::SCOPE_CROSS_WIKI ?>"
-							<?= $scope === \Wikia\Search\Config::SCOPE_CROSS_WIKI ? 'selected="selected"' : '' ?>
-						>All wikis</option>
-					</select>
-				</p>
+			<div class="grid-1 alpha">
+				<div class="wds-dropdown">
+					<div class="wds-dropdown__toggle">
+					<span>
+						<?= $scope === \Wikia\Search\Config::SCOPE_INTERNAL
+							? wfMsg( 'wikiasearch2-search-scope-internal' )
+							: wfMsg( 'wikiasearch2-search-scope-crosswiki' ) ?>
+					</span>
+					</div>
+					<div class="wds-dropdown__content">
+						<ul>
+							<?php
+							$scopeChanger = function ( $scope ) {
+								return preg_replace( '/&hub=[^&]+/', '&hub=' . $scope, $_SERVER['REQUEST_URI'] );
+							}
+							?>
+							<li>
+								<a href="<?= $scopeChanger( \Wikia\Search\Config::SCOPE_INTERNAL ) ?>">
+									<?= wfMsg( 'wikiasearch2-search-scope-internal' ) ?></a>
+							</li>
+							<li>
+								<a href="<?= $scopeChanger( \Wikia\Search\Config::SCOPE_CROSS_WIKI ) ?>">
+									<?= wfMsg( 'wikiasearch2-search-scope-crosswiki' ) ?></a>
+							</li>
+						</ul>
+					</div>
+				</div>
 
-<!--			<div class="wds-dropdown">-->
-<!--				<div class="wds-tabs__tab-label wds-dropdown__toggle">-->
-<!--					<span>Scope</span>-->
-<!--					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 12 12" class="wds-icon wds-icon-tiny wds-dropdown__toggle-chevron" id="wds-icons-dropdown-tiny"><defs><path id="dropdown-tiny-a" d="M6.0001895,8.80004571 C5.79538755,8.80004571 5.5905856,8.72164496 5.43458411,8.56564348 L2.23455364,5.365613 C2.00575146,5.13681083 1.93695081,4.79280755 2.06095199,4.4936047 C2.18415316,4.19440185 2.47695595,4 2.80015903,4 L9.20021997,4 C9.52342305,4 9.81542583,4.19440185 9.93942701,4.4936047 C10.0634282,4.79280755 9.99462754,5.13681083 9.76582536,5.365613 L6.56579489,8.56564348 C6.4097934,8.72164496 6.20499145,8.80004571 6.0001895,8.80004571 Z"></path></defs><use fill-rule="evenodd" xlink:href="#dropdown-tiny-a"></use></svg>						</div>-->
-<!--				<div class="wds-is-not-scrollable wds-dropdown__content" style="z-index: 9999999">-->
-<!--					<ul class="wds-list wds-is-linked wds-has-bolded-items">-->
-<!--						<li>-->
-<!--							<a href="/wiki/Special:RandomInCategory/The_Muppets_Characters" data-tracking="custom-level-2">-->
-<!--								Random Muppets Character											</a>-->
-<!--						</li>-->
-<!--						<li>-->
-<!--							<a href="/wiki/Special:RandomInCategory/Sesame_Street_Characters" data-tracking="custom-level-2">-->
-<!--								Random Sesame Street Character											</a>-->
-<!--						</li>-->
-<!--						<li>-->
-<!--							<a href="/wiki/Special:RandomInCategory/Fraggle_Rock_Characters" data-tracking="custom-level-2">-->
-<!--								Random Fraggle Rock Character											</a>-->
-<!--						</li>-->
-<!--					</ul>-->
-<!--				</div>-->
-<!--			</div>-->
-
+			</div>
 
 			<input type="text" name="search" id="search-v2-input" class="search-v2-input" value="<?=$query; ?>" />
 			<input type="hidden" name="fulltext" value="Search" />
