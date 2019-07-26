ccf9d910a39cdeedab0712b0eaa1b1f2fbf09634
abador <przemyslaw@czaus.pl>
2019-07-26 09:26:26 +0200

    Check test fails

--- extensions/wikia/RobotsTxt/tests/RobotsIntegrationTest.php
+++ extensions/wikia/RobotsTxt/tests/RobotsIntegrationTest.php
@@ -49,12 +49,12 @@ class RobotsIntegrationTest extends WikiaBaseTest {
 			[ sprintf($urlPattern, 'Special:AllPages?cb=' . $cb), self::NO_INDEX_NO_FOLLOW ],
 			[ sprintf($urlPattern, 'User:Abador?cb=' . $cb), self::NO_INDEX_NO_FOLLOW ],
 			[ 'https://creepypasta.fandom.com/wiki/User_talk:Abador?cb=' . $cb, self::NO_INDEX_NO_FOLLOW ],
-			//			[ 'https://community.fandom.com/wiki/Help:Contents?cb=' . $cb,
-			//			  self::NO_INDEX_NO_FOLLOW ],
+			[ 'https://community.fandom.com/wiki/Help:Contents?cb=' . $cb,
+			  self::NO_INDEX_NO_FOLLOW ],
 		];
 		$noindexParams = [
 			'action',
-			//			'feed',
+//			'feed',
 			//			'from',
 			'oldid',
 			//			'printable',
