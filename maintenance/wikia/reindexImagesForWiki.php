7a49c30bdd59ee037f72306d2b7bab19990515f7
Martyna Nowicka <martynaanow@gmail.com>
2019-08-06 08:35:51 +0200

    platform-4143|change memcache key prefix

--- maintenance/wikia/reindexImagesForWiki.php
+++ maintenance/wikia/reindexImagesForWiki.php
@@ -7,19 +7,18 @@ class reindexImagesForWiki extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->mDescription = "Run ImageServingHelper::buildAndGetIndex() and clear memory cache for wiki to index images and update page_wikia_props table";
-		$this->addOption( 'wikiId', 'ID of wiki', true, true, 'w' );
 		$this->addOption( 'pageID', 'Id of article', false, true, 'p' );
 	}
 
 	public function execute() {
 
-		$wikiId = $this->getOption( 'wikiId', 0 );
+		global $wgServer;
+
 		$pageID = $this->getOption( 'pageID', 0 );
 
-		$wikiUrl = WikiFactory::cityIDtoUrl( $wikiId );
 		$db = Wikifactory::db( DB_SLAVE );
 
-		$this->output( "\nTable page_wikia_props will be updated for {$wikiUrl} wiki \n\n" );
+		$this->output( "\nTable page_wikia_props will be updated for {$wgServer} wiki \n\n" );
 
 		if ( !$pageID ) {
 
@@ -45,20 +44,10 @@ class reindexImagesForWiki extends Maintenance {
 
 		$article = Article::newFromID( $pageId );
 		ImageServingHelper::buildAndGetIndex( $article );
-		$this->deleteMemoryCacheKey( $pageId, $article );
 		$this->output( "\nTable updated, memory cache keys cleaned for article with id {$pageId}\n\n" );
 
 	}
 
-	private function deleteMemoryCacheKey( $pageId, $article ) {
-
-		global $wgMemc;
-		$baseCacheKey = wfMemcKey( "imageserving-images-data", $pageId, $article->getRevIdFetched( $pageId ) );
-		$wgMemc->delete( $baseCacheKey . ':40:30' );
-		$wgMemc->delete( $baseCacheKey . ':55:55' );
-		$wgMemc->delete( $baseCacheKey . ':161:121.25' );
-
-	}
 }
 
 $maintClass = "reindexImagesForWiki";
