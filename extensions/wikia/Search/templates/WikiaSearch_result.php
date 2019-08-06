f59b3741962183bf1bf9e2c07dda509b6e945521
Michał Kalinowski <mkalinowski@wikia-inc.com>
2019-08-06 18:00:52 +0200

    SER-3421 Fix thumbnails for search results

--- extensions/wikia/Search/templates/WikiaSearch_result.php
+++ extensions/wikia/Search/templates/WikiaSearch_result.php
@@ -1,42 +1,44 @@
 <li class="result">
 	<article>
-	<?php
-	use Wikia\Search\UnifiedSearch\UnifiedSearchPageResultItem;
+		<?php
 
-	/** @var UnifiedSearchPageResultItem $result */
+		use Wikia\Search\UnifiedSearch\UnifiedSearchPageResultItem;
+
+		/** @var UnifiedSearchPageResultItem $result */
 		if ( $result['ns'] == NS_FILE ) {
-			$thumbnailHtml = $result->getThumbnailHtml();
+			$thumbnail = $result['thumbnail'];
 		}
 		?>
-		<?php if(! empty( $thumbnailHtml ) ): ?>
-		<div class="grid-1 alpha"><?= $thumbnailHtml ?></div>
-		<div class="media-text grid-2"> <? // Open media-text div when there's a thumbnail ?>
-	<?php endif; ?>
-	<h1>
-		<?php $title = $result->getText('title'); ?>
-
 		<?php
-			$trackingData = 'class="result-link"'
-				. 'data-pos="'.$pos.'"'
-				. 'data-page-id="' . $result['pageid'] . '"'
-				. ' data-thumbnail="' . !empty($thumbnailHtml) . '"';
+		$trackingData =
+			'class="result-link"' . ' data-pos="' . $pos . '"' . ' data-page-id="' . $result['pageid'] . '"' .
+			' data-thumbnail="' . !empty( $thumbnail ) . '"';
 		?>
+		<?php if ( !empty( $thumbnail ) ): ?>
+		<div class="grid-1 alpha">
+			<a href="<?= $result->getEscapedUrl() ?>" <?= $trackingData; ?>><img src="<?= $thumbnail ?>"/></a>
+		</div>
+		<div class="media-text grid-2"> <? // Open media-text div when there's a thumbnail ?>
+			<?php endif; ?>
+			<h1>
+				<?php $title = $result->getText( 'title' ); ?>
 
-		<a href="<?= $result->getEscapedUrl() ?>" <?=$trackingData;?>><?= $title ?></a>
-	</h1>
-	
-	<?= $result->getText(); ?>
-	
-	<?php if(empty($inGroup)): ?>
-		<ul>
-			<li><a href="<?= $result->getEscapedUrl(); ?>" <?=$trackingData;?> ><?=Language::factory($wg->ContentLanguage)->truncate($result->getTextUrl(), 90);?></a></li>
-		</ul>
-	<?php endif; ?>
-	
-	<?php if(! empty( $thumbnailHtml ) ): ?>
+				<a href="<?= $result->getEscapedUrl() ?>" <?= $trackingData; ?>><?= $title ?></a>
+			</h1>
+
+			<?= $result->getText(); ?>
+
+			<?php if ( empty( $inGroup ) ): ?>
+				<ul>
+					<li>
+						<a href="<?= $result->getEscapedUrl(); ?>" <?= $trackingData; ?> ><?= Language::factory( $wg->ContentLanguage )
+								->truncate( $result->getTextUrl(), 90 ); ?></a></li>
+				</ul>
+			<?php endif; ?>
+
+			<?php if ( !empty( $thumbnail ) ): ?>
 		</div> <? // Close media-text div when there's a thumbnail ?>
 	<?php endif; ?>
 
-</article>
+	</article>
 </li>
-
