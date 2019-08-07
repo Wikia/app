9af25a3d9cb1daad66172861c5f7a269df22d455
macbre <maciej.brencz@gmail.com>
2019-08-07 11:28:12 +0100

    Revert "Redshift::getConnection - use loadtesting Redshift cluster"
    This reverts commit 6d6d93dc968b3d9f12b92d23ec9b16a411702f35.

--- extensions/wikia/Hydralytics/classes/Redshift.class.php
+++ extensions/wikia/Hydralytics/classes/Redshift.class.php
@@ -22,12 +22,10 @@ class Redshift {
 	 * @throws \PDOException
 	 */
 	private static function getConnection() : \PDO {
-		global $wgRedshiftUser, $wgRedshiftPass;
-
-		$redshiftHost = 'loadtesting.cc5bp3zsf3ha.us-east-1.redshift.amazonaws.com:5439';
+		global $wgRedshiftUser, $wgRedshiftPass, $wgRedshiftHost;
 
 		if ( is_null( self::$connection ) ) {
-			$dsn = "pgsql:host={$redshiftHost};dbname=wikianalytics;port=5439";
+			$dsn = "pgsql:host={$wgRedshiftHost};dbname=wikianalytics;port=5439";
 
 			// https://www.php.net/manual/en/pdo.connections.php
 			// https://www.php.net/manual/en/ref.pdo-pgsql.connection.php
@@ -38,14 +36,14 @@ class Redshift {
 			}
 			catch ( \PDOException $e ) {
 				WikiaLogger::instance()->error( __METHOD__, [
-					'host' => $redshiftHost,
+					'host' => $wgRedshiftHost,
 					'exception' => $e,
 				] );
 				throw $e;
 			}
 
 			WikiaLogger::instance()->info( __METHOD__, [
-				'host' => $redshiftHost,
+				'host' => $wgRedshiftHost,
 				'took_sec' => $took,
 			] );
 		}
