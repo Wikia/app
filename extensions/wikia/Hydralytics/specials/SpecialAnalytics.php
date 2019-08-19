8a8b584734d59db827e126e558c67cf5d4cec5ad
Mariusz Strzelecki <mstrzelecki@wikia-inc.com>
2019-08-19 06:11:01 +0000

    DE-4470 | cache version bump

--- extensions/wikia/Hydralytics/specials/SpecialAnalytics.php
+++ extensions/wikia/Hydralytics/specials/SpecialAnalytics.php
@@ -18,7 +18,7 @@ namespace Hydralytics;
 class SpecialAnalytics extends \SpecialPage {
 
 	// bump this one to invalidate the Redshift results cache
-	const CACHE_VERSION = 3.9;
+	const CACHE_VERSION = 4.0;
 
 	/**
 	 * Output HTML
