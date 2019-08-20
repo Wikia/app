d48be7fa7e2a96075ab4df3d20628283820dd25f
Mateusz Sip <mateusz.sip@gmail.com>
2019-08-20 20:34:45 +0200

    Configure disconnect email for apple

---
+++ extensions/wikia/Email/i18n/AppleDisconnect.i18n.php
@@ -0,0 +1,17 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+	'emailext-apple-disconnect-subject' => 'Your FANDOM account is now disconnected from Apple!',
+	'emailext-apple-disconnect-summary' => "It looks like you've just disconnected your FANDOM account from Apple. We've kept all of your history, messages and profile details intact since you're an important part of the FANDOM community.",
+	'emailext-apple-disconnect-intro' => 'You can use the same username, but we may need you to click the link below to set your new password on FANDOM:',
+	'emailext-apple-disconnect-instructions' => 'If you previously set a password on FANDOM, no action is needed. You can ignore this message and continue signing in to FANDOM with your old password. Otherwise, please click the link above to set your new password on FANDOM.',
+);
+
+$messages['qqq'] = array(
+	'emailext-apple-disconnect-subject' => 'Subject line of the Apple disconnect email',
+	'emailext-apple-disconnect-summary' => 'Summary text in the body of the email',
+	'emailext-apple-disconnect-intro' => 'Text shown just above the username and password',
+	'emailext-apple-disconnect-instructions' => 'Instructions for using and resetting the given temporary password',
+);
+
