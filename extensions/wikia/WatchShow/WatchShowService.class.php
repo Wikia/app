5f4e571eb1e92a55e4cd6a159145d9114d7c28ce
Karol Tatała <ktatala@wikia-inc.com>
2019-09-10 09:50:30 +0200

    IW-2514 | image configurable

--- extensions/wikia/WatchShow/WatchShowService.class.php
+++ extensions/wikia/WatchShow/WatchShowService.class.php
@@ -3,13 +3,18 @@
 
 class WatchShowService extends WikiaService {
 	public function index() {
-		global $wgWatchShowURL, $wgWatchShowCTA, $wgWatchShowButtonLabel;
+		global $wgWatchShowURL,
+		       $wgWatchShowCTA,
+		       $wgWatchShowButtonLabel,
+		       $wgWatchShowImageUrl,
+		       $wgScriptPath;
 
 		$this->response->setValues(
 			[
 				'url' => $wgWatchShowURL,
 				'callToAction' => !empty( $wgWatchShowCTA ) ? $wgWatchShowCTA : 'Watch This Show',
 				'buttonLabel' => !empty( $wgWatchShowButtonLabel ) ? $wgWatchShowButtonLabel : 'Watch Now',
+				'imageUrl' => !empty( $wgWatchShowImageUrl ) ? $wgWatchShowImageUrl : "$wgScriptPath/extensions/wikia/WatchShow/images/moviesanywhere.svg",
 			]
 
 		);
