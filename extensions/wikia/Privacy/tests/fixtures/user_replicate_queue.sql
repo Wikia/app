fb6b457f9348b819629c92c5a1f6ad22e45b4d5d
Michał Chatlas <micha.chatlas@gmail.com>
2019-09-23 15:43:18 +0200

    Added if not exists to table creation

--- extensions/wikia/Privacy/tests/fixtures/user_replicate_queue.sql
+++ extensions/wikia/Privacy/tests/fixtures/user_replicate_queue.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `user_replicate_queue` (
+CREATE TABLE IF NOT EXISTS `user_replicate_queue` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(5) unsigned NOT NULL,
   `retries_count` int(11) NOT NULL DEFAULT '0',
