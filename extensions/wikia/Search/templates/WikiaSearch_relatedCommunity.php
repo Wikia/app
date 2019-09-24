8948cab4385d4608c7b41ceda91de2d04954b6a6
Mateusz Sip <mateusz.sip@gmail.com>
2019-09-24 11:18:05 +0200

    Don't use different positions for right-rail thumb and title links

--- extensions/wikia/Search/templates/WikiaSearch_relatedCommunity.php
+++ extensions/wikia/Search/templates/WikiaSearch_relatedCommunity.php
@@ -2,22 +2,20 @@
 	<h1 class="exact-wiki-match__header"><?= wfMessage('wikiasearch3-related-wiki')->escaped(); ?></h1>
 	<div class="exact-wiki-match__result">
 		<?php
-		$trackingData = function (int $pos) use ($community) {
-			return 'data-pos="' . $pos . '"' .
+		$trackingData = 'data-pos="' . 0 . '"' .
 				   ' data-wiki-id="' . $community['id'] . '"' .
 				   ' data-thumbnail="' . isset( $community['thumbnail'] ) . '"';
-		}
 		?>
 		<a href="<?= Sanitizer::encodeAttribute( $community['url'] ) ?>" title="<?= Sanitizer::encodeAttribute( $community['name'] ) ?>"
 		   data-event="image">
 			<img src="<?= Sanitizer::encodeAttribute( $community['thumbnail'] ) ?>" alt="<?= Sanitizer::encodeAttribute(
-				$community['name'] ) ?>" <?= $trackingData(0); ?> data-name="thumbnail"
+				$community['name'] ) ?>" <?= $trackingData; ?> data-name="thumbnail"
 				 class="exact-wiki-match__image" />
 		</a>
 		<div class="exact-wiki-match__content">
 			<h1 class="exact-wiki-match__wiki-header">
 				<a href="<?= Sanitizer::encodeAttribute( $community['url'] ) ?>" data-event="title"
-					<?= $trackingData(1); ?> data-name="<?= Sanitizer::encodeAttribute( $community['name'] ) ?>">
+					<?= $trackingData; ?> data-name="<?= Sanitizer::encodeAttribute( $community['name'] ) ?>">
 					<?= Sanitizer::encodeAttribute( $community['name'] ) ?>
 				</a>
 			</h1>
