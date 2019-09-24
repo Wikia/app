bf602b0adedab23d4a348bca1dc7c5ad9e454512
Tomasz Felczak <tfelczak@wikia-inc.com>
2019-09-24 15:57:02 +0200

    ADEN-9232 Code refactor

--- extensions/wikia/AdEngine3/src/bill-the-lizard-wrapper.js
+++ extensions/wikia/AdEngine3/src/bill-the-lizard-wrapper.js
@@ -1,30 +1,10 @@
 import {
     billTheLizard,
-    BillTheLizard,
-    billTheLizardEvents,
     context,
-    events,
-    eventService,
 } from '@wikia/ad-engine';
 import targeting from './targeting';
-import pageTracker from './tracking/page-tracker';
 
-const NOT_USED_STATUS = 'not_used';
-
-const bidPosKeyVal = 'INCONTENT_BOXAD_1';
-let config = null;
-let garfieldCalled = false;
-let initialValueOfIncontentsCounter = 1;
-let incontentsCounter = initialValueOfIncontentsCounter;
-let defaultStatus = NOT_USED_STATUS;
-
-function getCallId(counter = null) {
-    if (context.get('options.useTopBoxad') && (counter || incontentsCounter) === 0) {
-        return 'top_boxad';
-    }
-
-    return `incontent_boxad_${counter || incontentsCounter}`; //??
-}
+let config = null; //config will be used later
 
 function serializeBids(slotName) {
     return targeting.getBiddersPrices(slotName, false).then(bidderPrices => [
@@ -49,40 +29,8 @@ function serializeBids(slotName) {
     ].join(','));
 }
 
-function getBtlSlotStatus(btlStatus, callId, fallbackStatus) {
-    let slotStatus;
-
-    switch (btlStatus) {
-        case BillTheLizard.ON_TIME: {
-            const prediction = billTheLizard.getPrediction('garfield', callId);
-            const result = prediction ? prediction.result : undefined;
-            slotStatus = `${BillTheLizard.ON_TIME};res=${result};${callId}`;
-            break;
-        }
-        default: {
-            if (fallbackStatus === NOT_USED_STATUS) {                                                                // 2.
-                // we don't use a slot until we got response from Bill
-                return NOT_USED_STATUS;
-            }
-
-            const prevPrediction = billTheLizard.getPreviousPrediction(incontentsCounter, getCallId, 'garfield');
-
-            if (prevPrediction === undefined) {
-                // probably impossible but set in debugging purposes
-                return 'weird_cat';
-            }
-
-            slotStatus = `${BillTheLizard.REUSED};res=${prevPrediction.result};${prevPrediction.callId}`;
-        }
-    }
-
-    return slotStatus;
-}
-
 export const billTheLizardWrapper = {
-    configureBillTheLizard(billTheLizardConfig) {
-
-        defaultStatus = NOT_USED_STATUS;
+    configureBillTheLizard(billTheLizardConfig) { //config will be used later
 
         config = billTheLizardConfig;
 
@@ -92,50 +40,16 @@ export const billTheLizardWrapper = {
             billTheLizard.projectsHandler.enable('garfield');
         }
 
-        // billTheLizard.executor.register('catlapseIncontentBoxad', () => {
-        //     console.log('catlapsed!');
-        // });
-
-        eventService.on(events.AD_SLOT_CREATED, (adSlot) => {
-            if (adSlot.getConfigProperty('cheshireCatSlot')) {
-                const callId = getCallId();
-                // to je tez wazne 1.
-                adSlot.setConfigProperty('btlStatus', getBtlSlotStatus(
-                    billTheLizard.getResponseStatus(callId),
-                    callId,
-                    defaultStatus,
-                ));
-            }
-        });
-
-        eventService.on(billTheLizardEvents.BILL_THE_LIZARD_REQUEST, (event) => {
-            const { query, callId } = event;
-            let propName = 'btl_request';
-            if (callId) {
-                propName = `${propName}_${callId}`;
-            }
-
-            pageTracker.trackProp(propName, query);
-        });
-
-        eventService.on(billTheLizardEvents.BILL_THE_LIZARD_RESPONSE, (event) => {
-            const { response, callId } = event;
-            let propName = 'btl_response';
-            if (callId) {
-                propName = `${propName}_${callId}`;
-                defaultStatus = BillTheLizard.REUSED;
-            }
-            pageTracker.trackProp(propName, response);
+        billTheLizard.executor.register('catlapseIncontentBoxad', () => {
+            console.log('catlapsed!');
         });
     },
 
     callGarfield(callId) {
         serializeBids(callId).then((bids) => {
-            console.log(bids)
             context.set('services.billTheLizard.parameters.garfield', {
                 bids,
             });
-            garfieldCalled = true;
             billTheLizard.call(['garfield'], callId);
         });
     },
