ead10a852ec243b548829076064ab8e9029ea7f2
Damian Bielecki <bielik20@gmail.com>
2019-10-02 10:25:13 +0200

    remove console logs

--- extensions/wikia/AdEngine3/src/slots.js
+++ extensions/wikia/AdEngine3/src/slots.js
@@ -14,12 +14,6 @@ const PAGE_TYPES = {
 function isIncontentBoxadApplicable() {
 	const isSupportedPageType = ['article', 'search'].indexOf(context.get('wiki.targeting.pageType')) !== -1;
 
-	console.log({
-		wgIsContentNamespace: window.wgIsContentNamespace,
-		opts: context.get('wiki.opts.adsInContent'),
-		isCorporate: context.get('wiki.targeting.wikiIsCorporate'),
-	});
-
 	return isSupportedPageType &&
 		window.wgIsContentNamespace &&
 		context.get('wiki.opts.adsInContent') &&
@@ -385,7 +379,6 @@ export default {
 		const isApplicable = isIncontentBoxadApplicable();
 		const parentNode = document.getElementById('WikiaAdInContentPlaceHolder');
 
-		console.log('start', {isApplicable, parentNode});
 		if (!isApplicable || !parentNode) {
 			slotService.setState(slotName, false);
 			return;
