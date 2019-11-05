48882859359599a4c766fe119f75d6771ed808b2
Łukasz Harasimowicz <dev@harnash.eu>
2019-11-05 11:54:23 +0100

    PLATFORM-4289| review comments addressed

--- extensions/wikia/WikiDescription/WikiDescription.setup.php
+++ extensions/wikia/WikiDescription/WikiDescription.setup.php
@@ -1,9 +1,6 @@
 <?php
 
-$dir = dirname(__FILE__) . '/';
-$app = F::app();
+$wgAutoloadClasses['WikiDescription\WikiDescriptionService'] =  __DIR__ . 'WikiDescriptionService.class.php';
 
-$wgAutoloadClasses['WikiDescription\WikiDescriptionService'] =  $dir . 'WikiDescriptionService.class.php';
-
-$wgExtensionMessagesFiles['WikiDescriptionService'] = $dir . 'WikiDescription.i18n.php';
+$wgExtensionMessagesFiles['WikiDescriptionService'] = __DIR__ . 'WikiDescription.i18n.php';
 $wgEditInterfaceWhitelist[] = 'Wiki-description-site-meta';
