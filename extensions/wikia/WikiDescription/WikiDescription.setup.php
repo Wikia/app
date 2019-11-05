321637c196ff7f0a00b17a5a2d2c13c081c77666
Daniel Grunwell <mwgrunny@gmail.com>
2019-11-05 21:04:42 +1000

    Fix include path

--- extensions/wikia/WikiDescription/WikiDescription.setup.php
+++ extensions/wikia/WikiDescription/WikiDescription.setup.php
@@ -2,5 +2,5 @@
 
 $wgAutoloadClasses['WikiDescription\WikiDescriptionService'] =  __DIR__ . '/WikiDescriptionService.class.php';
 
-$wgExtensionMessagesFiles['WikiDescriptionService'] = __DIR__ . 'WikiDescription.i18n.php';
+$wgExtensionMessagesFiles['WikiDescriptionService'] = __DIR__ . '/WikiDescription.i18n.php';
 $wgEditInterfaceWhitelist[] = 'Wiki-description-site-meta';
