95527cb4f4b0190cfb1e6e920b4c04c45c68e599
Chris Schuhmacher <cschuhmacher@fandom.com>
2019-11-08 02:25:22 +0000

    cake-5273-mediawiki | only add margin right to button

--- extensions/wikia/WatchShow/templates/WatchShow_index.php
+++ extensions/wikia/WatchShow/templates/WatchShow_index.php
@@ -2,21 +2,13 @@
 	<h2><?= $callToAction ?></h2>
 	<div class="watch-show__content">
 		<a href="<?= $url ?>" class="wds-button wds-is-secondary" target="_blank" rel="noopener"><?= $buttonLabel ?></a>
-		<div class="watch-show__image-wrapper">
-			<hr class="watch-show__hr">
-			<img src="<?= $imageURL ?>" alt="<?= $callToAction ?>" class="watch-show__image">
-			<hr class="watch-show__hr">
-		</div>
+		<img src="<?= $imageURL ?>" alt="<?= $callToAction ?>" class="watch-show__image">
 	</div>
 </section>
 <section class="rail-module watch-show wds-is-hidden" id="watch-show-rail-module-ca" data-tracking-pixel="<?= $trackingPixelURL ?>">
 	<h2><?= $callToActionCA ?></h2>
 	<div class="watch-show__content">
 		<a href="<?= $url ?>" class="wds-button wds-is-secondary" target="_blank" rel="noopener"><?= $buttonLabelCA ?></a>
-		<div class="watch-show__image-wrapper">
-			<hr class="watch-show__hr">
-			<img src="<?= $imageURL ?>" alt="<?= $callToActionCA ?>" class="watch-show__image">
-			<hr class="watch-show__hr">
-		</div>
+		<img src="<?= $imageURL ?>" alt="<?= $callToActionCA ?>" class="watch-show__image">
 	</div>
 </section>
