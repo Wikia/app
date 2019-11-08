58188062aa7b37ba7942593f9ff17e0d20107154
Chris Schuhmacher <cschuhmacher@fandom.com>
2019-11-08 01:34:28 +0000

    cake-5273-mediawiki | style changes for watch now

--- extensions/wikia/WatchShow/templates/WatchShow_index.php
+++ extensions/wikia/WatchShow/templates/WatchShow_index.php
@@ -2,13 +2,21 @@
 	<h2><?= $callToAction ?></h2>
 	<div class="watch-show__content">
 		<a href="<?= $url ?>" class="wds-button wds-is-secondary" target="_blank" rel="noopener"><?= $buttonLabel ?></a>
-		<img src="<?= $imageURL ?>" alt="<?= $callToAction ?>" class="watch-show__image">
+		<div class="watch-show__image-wrapper">
+			<hr class="watch-show__hr">
+			<img src="<?= $imageURL ?>" alt="<?= $callToAction ?>" class="watch-show__image">
+			<hr class="watch-show__hr">
+		</div>
 	</div>
 </section>
 <section class="rail-module watch-show wds-is-hidden" id="watch-show-rail-module-ca" data-tracking-pixel="<?= $trackingPixelURL ?>">
 	<h2><?= $callToActionCA ?></h2>
 	<div class="watch-show__content">
 		<a href="<?= $url ?>" class="wds-button wds-is-secondary" target="_blank" rel="noopener"><?= $buttonLabelCA ?></a>
-		<img src="<?= $imageURL ?>" alt="<?= $callToActionCA ?>" class="watch-show__image">
+		<div class="watch-show__image-wrapper">
+			<hr class="watch-show__hr">
+			<img src="<?= $imageURL ?>" alt="<?= $callToActionCA ?>" class="watch-show__image">
+			<hr class="watch-show__hr">
+		</div>
 	</div>
 </section>
