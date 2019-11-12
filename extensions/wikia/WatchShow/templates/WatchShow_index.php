f1991aaceee4412ad65270fa7b5b68ee0812fe9e
Chris Schuhmacher <cschuhmacher@fandom.com>
2019-11-12 17:41:28 +0000

    cake-5286 | Add disclaimer message to watch show module

--- extensions/wikia/WatchShow/templates/WatchShow_index.php
+++ extensions/wikia/WatchShow/templates/WatchShow_index.php
@@ -1,4 +1,5 @@
 <section class="rail-module watch-show wds-is-hidden" id="watch-show-rail-module" data-tracking-pixel="<?= $trackingPixelURL ?>">
+	<p class="watch-show__disclaimer">Disclosure: Some of the links below are affiliate links meaning, at no additional cost to you, Fandom will earn a commission if you click through and make a purchase</p>
 	<h2><?= $callToAction ?></h2>
 	<div class="watch-show__content">
 		<a href="<?= $url ?>" class="wds-button wds-is-secondary" target="_blank" rel="noopener"><?= $buttonLabel ?></a>
@@ -6,6 +7,7 @@
 	</div>
 </section>
 <section class="rail-module watch-show wds-is-hidden" id="watch-show-rail-module-ca" data-tracking-pixel="<?= $trackingPixelURL ?>">
+	<p class="watch-show__disclaimer">Disclosure: Some of the links below are affiliate links meaning, at no additional cost to you, Fandom will earn a commission if you click through and make a purchase</p>
 	<h2><?= $callToActionCA ?></h2>
 	<div class="watch-show__content">
 		<a href="<?= $url ?>" class="wds-button wds-is-secondary" target="_blank" rel="noopener"><?= $buttonLabelCA ?></a>
