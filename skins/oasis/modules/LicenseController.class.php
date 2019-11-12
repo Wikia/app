8e1971c3821c2746f6c05e598f1e765d8894575e
Chris Schuhmacher <cschuhmacher@fandom.com>
2019-11-12 19:01:45 +0000

    testing license description

--- skins/oasis/modules/LicenseController.class.php
+++ skins/oasis/modules/LicenseController.class.php
@@ -8,6 +8,8 @@ class LicenseController extends WikiaController {
 		$licenseLink = F::app()->renderPartial('License', 'Link', [ 'licenseUrl' => $wgRightsUrl, 'licenseText' => $wgRightsText ] );
 		$description = wfMessage('license-description')->inContentLanguage()->rawParams( $licenseLink )->escaped();
 
+		d($description);
+
 		$this->setVal( 'licenseDescription', $description );
 	}
 }
