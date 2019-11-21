38853b712ed1d0ca8dac102575d5038aed01e018
Damian Bielecki <bielik20@gmail.com>
2019-11-21 08:47:29 +0100

    cleanup autoplay

--- extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.autoplay.js
+++ extensions/wikia/ArticleVideo/scripts/featured-video.jwplayer.autoplay.js
@@ -1,29 +1,24 @@
 define('wikia.articleVideo.featuredVideo.autoplay', [
-	'wikia.abTest',
 	'wikia.articleVideo.featuredVideo.cookies',
 	require.optional('ext.wikia.adEngine3.api')
-], function (abTest, featuredVideoCookieService, adsApi) {
+], function (featuredVideoCookieService, adsApi) {
 	'use strict';
-	var inFeaturedVideoClickToPlayABTest = abTest.inGroup('FV_CLICK_TO_PLAY', 'CLICK_TO_PLAY');
 
-	function isDisabledByQueenOfHearts() {
+	function isDisabledByAdEngine() {
 		return adsApi && adsApi.isAutoPlayDisabled();
 	}
 
 	return {
 		isAutoplayEnabled: function () {
 			return featuredVideoCookieService.getAutoplay() !== '0' &&
-				!isDisabledByQueenOfHearts() &&
-				!inFeaturedVideoClickToPlayABTest;
+				!isDisabledByAdEngine();
 		},
 		inNextVideoAutoplayEnabled: function () {
 			return true;
 		},
 		isAutoplayToggleShown: function () {
-			return !(
-				isDisabledByQueenOfHearts() ||
-				inFeaturedVideoClickToPlayABTest
-			);
+			return !isDisabledByAdEngine()
+			;
 		}
 	};
 });
