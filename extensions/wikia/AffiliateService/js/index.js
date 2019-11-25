0ee0c28ef5abdea0f0ee421f3a282998d08762fe
Josh <jrogan@wikia-inc.com>
2019-11-25 21:08:00 +0000

    Updates

--- extensions/wikia/AffiliateService/js/index.js
+++ extensions/wikia/AffiliateService/js/index.js
@@ -2,32 +2,5 @@ require(['jquery', 'content_types_consumption', 'wikia.trackingOptIn', 'mw'], fu
 	'use strict';
 	$(function () {
 		console.log('hello world from affiliate service');
-		$('.WikiaRail').on('afterLoad.rail', function () {
-			// do nothing if there's no module
-			if ($('#wikia-trivia-quizzes').length) {
-				// FIXME: For now: if they are IE11, remove the entire module until we have better support
-				if ($.browser.msie) {
-					$('#wikia-recent-activity').remove();
-				} else {
-					// https://github.com/Wikia/content-types/tree/master/consumption#usage
-					consumption.default('wikia-trivia-quizzes', {
-						environment: 'media-wiki',
-						pageType: 'Article',
-						user: {
-							id: mw.config.get('wgUserId') || 0,
-							isGDPRApproved: trackingOptIn.isOptedIn(),
-						},
-						community: {
-							dbName: mw.config.get('wgDBname') || null,
-							id: mw.config.get('wgCityId') || '0',
-						},
-						theme: {
-							linkColor: mw.config.get('wgTriviaQuizzesLinkColor'),
-							primaryColor: mw.config.get('wgTriviaQuizzesPrimaryColor'),
-						},
-					});
-				}
-			}
-		});
 	});
 });
