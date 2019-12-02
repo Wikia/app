95a56b916b75186992a5f683fda13b8fb6a03393
Damian Bielecki <bielik20@gmail.com>
2019-12-02 13:11:22 +0100

    add console.log to build

--- extensions/wikia/AdEngine3/src/startup.js
+++ extensions/wikia/AdEngine3/src/startup.js
@@ -29,6 +29,8 @@ import slots from './slots';
 import videoTracker from './tracking/video-tracking';
 import { track } from "./tracking/tracker";
 import { communicator } from "./communicator";
+import { merge, of } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 const GPT_LIBRARY_URL = '//www.googletagservices.com/tag/js/gpt.js';
 
@@ -70,6 +72,10 @@ export async function setupAdEngine(isOptedIn, geoRequiresConsent) {
 }
 
 async function setupJWPlayer() {
+	of(1).pipe(
+  		mergeMap(() => merge(of('oasis - first'), of('oasis - second')))
+	).subscribe(console.log);
+
 	new JWPlayerManager().manage();
 
 	if (!context.get('state.showAds')) {
