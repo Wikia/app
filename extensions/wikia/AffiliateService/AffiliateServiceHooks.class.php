d31dfea9630fb88499ef15d873a6f094fef93733
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-12-09 23:05:43 +0000

    CAKE-5378 | Enable only on NS0

--- extensions/wikia/AffiliateService/AffiliateServiceHooks.class.php
+++ extensions/wikia/AffiliateService/AffiliateServiceHooks.class.php
@@ -3,14 +3,28 @@
 class AffiliateServiceHooks {
 	public static function onBeforePageDisplay( OutputPage $out ) {
 		wfProfileIn( __METHOD__ );
+
 		$out->addModules( 'ext.wikia.AffiliateService' );
+
 		wfProfileOut( __METHOD__ );
 		return true;
 	}
 
 	public static function onWikiaSkinTopScripts( Array &$vars, &$scripts ) {
 		wfProfileIn( __METHOD__ );
-		$vars['wgAffiliateEnabled'] = Wikia::isDevEnv();
+
+		global $wgTitle;
+		// Proper namespace => Article (NS 0) and not Main Pages
+		$isProperNamespace = is_object( $wgTitle )
+			&& ( !$wgTitle->isMainPage() )
+			&& ( $wgTitle->getNamespace() === NS_MAIN );
+
+		// TODO: Hardocde community IDs here (CAKE-5379)
+		$isProperCommunity = Wikia::isDevEnv();
+
+		// only enable it onNS 0 AND dev env
+		$vars['wgAffiliateEnabled'] = $isProperNamespace && $isProperCommunity;
+
 		wfProfileOut( __METHOD__ );
 		return true;
 	}
