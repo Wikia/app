602c2b2ef5caf8620aa79038bf2ea57c36293271
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-12-11 20:34:32 +0000

    Fix undefined param, remove 2nd check for insertion point

--- extensions/wikia/AffiliateService/js/ext.AffiliateService.js
+++ extensions/wikia/AffiliateService/js/ext.AffiliateService.js
@@ -196,6 +196,7 @@ require([
 
 					if (availableUnits.length > 0) {
 						var unit = availableUnits[0];
+
 						// add unit data to be inserted into template
 						AffiliateService.renderUnitMarkup(unit);
 					} else {
@@ -208,6 +209,20 @@ require([
 		},
 
 		insertAtPointAndTrack: function ($insertionPoint, unit) {
+			// add extra fields
+			var extraTracking = unit.tracking.slice();
+			extraTracking.push({
+				// Y of the insertion point
+				key: 'instertedAtY',
+				val: $insertionPoint.offset().top,
+			});
+
+			var trackingOptions = {
+				campaignId: unit.campaign,
+				categoryId: unit.category,
+				extraTracking: extraTracking,
+			};
+
 			// check if we found good insertion point
 			if ($insertionPoint) {
 				// get html to insert into target location
@@ -215,20 +230,6 @@ require([
 
 				// insert markup
 				var $element = $insertionPoint.prepend(html);
-
-				// add extra fields
-				var extraTracking = unit.tracking.slice();
-				extraTracking.push({
-					// Y of the insertion point
-					key: 'instertedAtY',
-					val: $insertionPoint.offset().top,
-				});
-
-				var trackingOptions = {
-					campaignId: unit.campaign,
-					categoryId: unit.category,
-					extraTracking: extraTracking,
-				};
 
 				// hook onmousedown tracking
 				$element.find('.affiliate-unit__cta').on('mousedown', function (event) {
@@ -340,9 +341,7 @@ require([
 				}
 			});
 
-			if ($insertionPoint !== undefined) {
-				AffiliateService.insertAtPointAndTrack($insertionPoint, unit);
-			}
+			AffiliateService.insertAtPointAndTrack($insertionPoint, unit);
 		},
 
 		// Using mustache to render template and unit info
