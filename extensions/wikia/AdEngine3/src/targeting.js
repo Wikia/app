fc4eed7b4ef4329b1dd66e0f37ef7c0ef453a830
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-12-18 12:13:14 +0100

    Fix wv-fv prefix

--- extensions/wikia/AdEngine3/src/targeting.js
+++ extensions/wikia/AdEngine3/src/targeting.js
@@ -28,9 +28,7 @@ function getAdLayout(adsContext) {
 		// Comparing with false in order to make sure that API already responds with "isDedicatedForArticle" flag
 		if (adsContext.targeting.featuredVideo && adsContext.targeting.featuredVideo.isDedicatedForArticle === false) {
 			layout = `wv-${layout}`;
-		}
-
-		if (adsContext.targeting.hasFeaturedVideo) {
+		} else if (adsContext.targeting.hasFeaturedVideo) {
 			layout = `fv-${layout}`;
 		}
 
