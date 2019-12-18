657fb0ee29f855dbeceb8a133cd59dc28bfb22ac
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-12-18 13:13:17 +0100

    Extend if statement

--- extensions/wikia/AdEngine3/src/targeting.js
+++ extensions/wikia/AdEngine3/src/targeting.js
@@ -26,7 +26,11 @@ function getAdLayout(adsContext) {
 
 	if (layout === 'article') {
 		// Comparing with false in order to make sure that API already responds with "isDedicatedForArticle" flag
-		if (adsContext.targeting.featuredVideo && adsContext.targeting.featuredVideo.isDedicatedForArticle === false) {
+		if (
+			adsContext.targeting.hasFeaturedVideo
+			&& adsContext.targeting.featuredVideo
+			&& adsContext.targeting.featuredVideo.isDedicatedForArticle === false
+		) {
 			layout = `wv-${layout}`;
 		} else if (adsContext.targeting.hasFeaturedVideo) {
 			layout = `fv-${layout}`;
