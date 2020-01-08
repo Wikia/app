2af13a7d053d3e8cd05af33d6b4bf5cdc15899bb
Paulina Gralak <pgralak@wikia-inc.com>
2020-01-08 11:22:25 +0100

    [ADEN-9569] remove Teads

--- extensions/wikia/AdEngine3/src/targeting.js
+++ extensions/wikia/AdEngine3/src/targeting.js
@@ -264,7 +264,6 @@ export default {
 			bidder_19: transformBidderPrice('gumgum'),
 			bidder_20: transformBidderPrice('33across'),
 			bidder_21: transformBidderPrice('triplelift'),
-			bidder_22: transformBidderPrice('teads'),
 			bidder_23: transformBidderPrice('oneVideo'),
 			bidder_24: transformBidderPrice('criteo'),
 		};
