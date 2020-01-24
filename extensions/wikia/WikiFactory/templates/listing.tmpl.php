99afeaad20fbe80637dc2839ef91538350f0c21c
Martyna Nowicka <martynaanow@gmail.com>
2020-01-24 09:25:30 +0100

    PLATFORM-4494 | Use hack to not show slot2 wikis in result list

--- extensions/wikia/WikiFactory/templates/listing.tmpl.php
+++ extensions/wikia/WikiFactory/templates/listing.tmpl.php
@@ -10,17 +10,19 @@ $cnt = 0;
 foreach( $data as $row ):
 ?>
 <div class="wf-list">
-	<h4><a href="<?php echo $title->getFullUrl() . "/{$row->wiki->city_id}" ?>">
-		<?php CityListPager::bold( $row->wiki->city_title, $part )?>
-		<span style="font-size: smaller">language: <?php echo $row->wiki->city_lang ?></span>
-	</a></h4>
-	<?php echo empty( $row->wiki->city_description ) ? "no description" : $row->wiki->city_description ?>
-	<br />
-	<em>Used domains: <?php echo CityListPager::bold( implode ( ", ", (array) $row->domains ), $part ) ?></em>
-	<br />
-	<span style="color: darkgreen;"><?php CityListPager::bold( $row->wiki->city_url, $part ) ?></a></span>
-	 - <a href="<?php echo $title->getFullUrl() . "/{$row->wiki->city_id}" ?>">wikifactory</a>
-	 - <a href="<?php echo $row->wiki->city_url ?>">link</a>
+	<?php if ( $row->wiki->city_path === 'slot1' ): ?>
+		<h4><a href="<?php echo $title->getFullUrl() . "/{$row->wiki->city_id}" ?>">
+			<?php CityListPager::bold( $row->wiki->city_title, $part )?>
+			<span style="font-size: smaller">language: <?php echo $row->wiki->city_lang ?></span>
+		</a></h4>
+		<?php echo empty( $row->wiki->city_description ) ? "no description" : $row->wiki->city_description ?>
+		<br />
+		<em>Used domains: <?php echo CityListPager::bold( implode ( ", ", (array) $row->domains ), $part ) ?></em>
+		<br />
+		<span style="color: darkgreen;"><?php CityListPager::bold( $row->wiki->city_url, $part ) ?></a></span>
+		 - <a href="<?php echo $title->getFullUrl() . "/{$row->wiki->city_id}" ?>">wikifactory</a>
+		 - <a href="<?php echo $row->wiki->city_url ?>">link</a>
+	<?php endif; ?>
 </div>
 <?php
 	 if( $cnt > $limit) break;
