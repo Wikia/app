d61fcf07c6cfe32cc454e3e670b4dd77280b4fdd
Katarzyna Basiukajc <kbasiukajc@wikia-inc.com>
2020-02-14 14:20:30 +0100

    ADEN-9913 | Remove AE3 InstantGlobals class

--- extensions/wikia/AdEngine3/AdEngine3.setup.php
+++ extensions/wikia/AdEngine3/AdEngine3.setup.php
@@ -11,7 +11,6 @@ $wgExtensionCredits['other'][] = [
 $wgAutoloadClasses['AdEngine3'] =  __DIR__ . '/AdEngine3.class.php';
 $wgAutoloadClasses['AdEngine3Controller'] =  __DIR__ . '/AdEngine3Controller.class.php';
 $wgAutoloadClasses['AdEngine3DeciderService'] = __DIR__ . '/AdEngine3DeciderService.class.php';
-$wgAutoloadClasses['AdEngine3InstantGlobals'] =  __DIR__ . '/AdEngine3InstantGlobals.class.php';
 $wgAutoloadClasses['AdEngine3PageTypeService'] = __DIR__ . '/AdEngine3PageTypeService.class.php';
 $wgAutoloadClasses['AdEngine3Service'] =  __DIR__ . '/AdEngine3Service.class.php';
 $wgAutoloadClasses['AdEngine3WikiData'] =  __DIR__ . '/AdEngine3WikiData.class.php';
@@ -22,11 +21,6 @@ $wgHooks['OasisSkinAssetGroupsBlocking'][] = 'AdEngine3::onOasisSkinAssetGroupsB
 $wgHooks['WikiaSkinTopModules'][] = 'AdEngine3::onWikiaSkinTopModules';
 $wgHooks['WikiaSkinTopScripts'][] = 'AdEngine3::onWikiaSkinTopScripts';
 $wgHooks['AfterInitialize'][] = 'AdEngine3::onAfterInitialize';
-
-// Instant Globals
-$wgHooks['InstantGlobalsGetNewsAndStoriesVariables'][] = 'AdEngine3InstantGlobals::onInstantGlobalsGetNewsAndStoriesVariables';
-$wgHooks['InstantGlobalsGetFandomCreatorVariables'][] = 'AdEngine3InstantGlobals::onInstantGlobalsGetFandomCreatorVariables';
-$wgHooks['InstantGlobalsGetVariables'][] = 'AdEngine3InstantGlobals::onInstantGlobalsGetVariables';
 
 // I18n
 $wgExtensionMessagesFiles['AdEngine3'] = __DIR__ . '/AdEngine3.i18n.php';
