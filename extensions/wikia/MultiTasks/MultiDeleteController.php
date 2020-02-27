714c4eccff527941d86fdfd2c132a631b926360a
Martyna Nowicka <martynaanow@gmail.com>
2020-02-27 15:38:39 +0100

    PLATFORM-4509 | Quickly add script to delete pages on wiki

--- extensions/wikia/MultiTasks/MultiDeleteController.php
+++ extensions/wikia/MultiTasks/MultiDeleteController.php
@@ -3,42 +3,97 @@
 declare( strict_types=1 );
 
 use Wikia\Logger\Loggable;
-use User;
 
 final class MultiDeleteController extends WikiaController {
 	use Loggable;
+
+	private const ALL = 'all';
+	private const CURRENT = 'id';
+	private const LANGUAGE = 'lang';
+	private const SELECTED = 'list';
 
 	public function allowsExternalRequests() {
 		return false;
 	}
 
 	public function multiDeletePages() {
+		global $IP;
+
 		$this->response->setFormat( WikiaResponse::FORMAT_JSON );
 		if ( !$this->request->wasPosted() ) {
 			$this->response->setCode( 405 );
 			return;
 		}
+
 		$pagesToDelete = $this->getVal( 'pages' );
 		$userId = $this->getVal( 'user' );
 		$reason = $this->getVal( 'reason' );
-		if ( empty( $pagesToDelete ) || empty( $userId ) ||  empty( $reason ) ) {
+		$firstWikiId = $this->getVal( 'firstWikiId' );
+		$lastWikiId = $this->getVal( 'lastWikiId' );
+		$runOnType = $this->getVal( 'runOnType' );
+		$runOnValue = $this->getVal( 'runOnValue' );
+
+		if (
+			empty( $pagesToDelete ) ||
+			empty( $userId ) ||
+			empty( $reason ) ||
+			empty( $firstWikiId ) ||
+			empty( $lastWikiId ) ||
+			empty( $runOnType ) ||
+			empty( $runOnValue )
+		) {
 			$this->response->setCode( WikiaResponse::RESPONSE_CODE_BAD_REQUEST );
 			return;
 		}
-		$user = User::newFromId( $userId );
 
-		foreach ( $pagesToDelete as $pageName ) {
-			$page = WikiPage::factory( \Title::newFromText( $pageName ) );
+		$wikis = self::getWikisForPageDelete(
+			$this->params['firstWikiId'],
+			$this->params['lastWikiId'],
+			$this->params['runOnType'],
+			$this->params['runOnValue']
+		);
 
-			if ( $page !== null ) {
-				$e = '';
-				$page->doDeleteArticle( $reason, false, 0, true, $e, $user );
-			} else {
-				$this->info( 'Page selected to delete in MultiDelete is missing', [
-					'pageName' => $pageName
-				]);
-			}
+		foreach( $wikis as $wiki ) {
+			$command = implode( ' ', array_merge( [
+				"SERVER_ID={$wiki->city_id}",
+				"php",
+				"{$IP}/extensions/wikia/MultiTasks/maintenance/DeleteWikiPage.php",
+				"--pagesToDelete={$pagesToDelete}",
+				"--reason={$reason}",
+				"--user={$userId}"
+			]) );
+			$status = 0;
+			wfShellExec( $command, $status );
 		}
+
 		$this->response->setCode( WikiaResponse ::RESPONSE_CODE_OK );
+	}
+
+	private function getWikisForPageDelete( int $firstId, int $lastId, string $runOnType, string $runOnValue ) {
+		$dbr = wfGetDB( DB_SLAVE );
+		switch( $runOnType ) {
+			case MultiDeleteController::ALL:
+			case MultiDeleteController::SELECTED:
+			case MultiDeleteController::CURRENT:
+				return $dbr->select(
+					'city_list',
+					'city_id',
+					[
+						'city_public' => '1',
+						'city_id >=' . $firstId,
+						'city_id <=' . $lastId
+					]
+				);
+			case MultiDeleteController::LANGUAGE:
+				return $dbr->select(
+					'city_list',
+					'city_id',
+					[
+						'city_lang' => $runOnValue,
+						'city_id >=' . $firstId,
+						'city_id <=' . $lastId
+					]
+				);
+		}
 	}
 }
