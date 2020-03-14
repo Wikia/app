4a7bba21df6a18be57a5e677b8eda79f03028f1a
Chris Schuhmacher <cschuhmacher@fandom.com>
2020-03-14 17:43:38 +0000

    remove logging

--- extensions/wikia/GoogleTagManager/js/ext.GoogleTagManager.js
+++ extensions/wikia/GoogleTagManager/js/ext.GoogleTagManager.js
@@ -1,8 +1,6 @@
 require(['jquery'], function ($) {
     'use strict';
 
-    console.log('js loading');
-
     var tagManagerNoScript = '<!-- Google Tag Manager (noscript) -->' +
     '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDPTN53"' +
     'height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>' +
