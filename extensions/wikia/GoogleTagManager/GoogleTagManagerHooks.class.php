fbc2bc14cfc684dc0f8c004b3cd036a7c7e41043
Chris Schuhmacher <cschuhmacher@fandom.com>
2020-03-16 20:26:21 +0000

    Add script to variable

--- extensions/wikia/GoogleTagManager/GoogleTagManagerHooks.class.php
+++ extensions/wikia/GoogleTagManager/GoogleTagManagerHooks.class.php
@@ -2,8 +2,9 @@
 
 class GoogleTagManagerHooks {
 	static public function onWikiaSkinTopScripts( &$vars, &$scripts ) {
+		$tagManagerScript = "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MDPTN53');</script><!-- End Google Tag Manager -->";
 
-		$scripts .= "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MDPTN53');</script><!-- End Google Tag Manager -->";
+		$scripts .= $tagManagerScript;
 
 		return true;
 	}
