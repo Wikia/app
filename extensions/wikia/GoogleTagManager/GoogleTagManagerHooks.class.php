037c97e041e315f5e92bbab39ef2ac3c29dd6fb6
Chris Schuhmacher <cschuhmacher@fandom.com>
2020-03-16 05:43:22 +0000

    Add noscript to body

--- extensions/wikia/GoogleTagManager/GoogleTagManagerHooks.class.php
+++ extensions/wikia/GoogleTagManager/GoogleTagManagerHooks.class.php
@@ -2,9 +2,8 @@
 
 class GoogleTagManagerHooks {
 	static public function onWikiaSkinTopScripts( &$vars, &$scripts ) {
-		$inlineScript = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MDPTN53');";
 
-		$scripts .= Html::inlineScript( $inlineScript );
+		$scripts .= "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MDPTN53');</script><!-- End Google Tag Manager -->";
 
 		return true;
 	}
@@ -13,7 +12,10 @@ class GoogleTagManagerHooks {
 		wfProfileIn( __METHOD__ );
 		global $wgEnableGoogleTagManager;
 
+		$tagManagerNoScript = '<!-- Google Tag Manager (noscript) --><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDPTN53" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!-- End Google Tag Manager (noscript) -->';
+
 		$out->addModules( 'ext.wikia.GoogleTagManager' );
+		$out->addHTML( $tagManagerNoScript );
 
 		wfProfileOut( __METHOD__ );
 		return true;
