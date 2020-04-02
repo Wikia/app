039b516a7522b36437ccf8b1b4a876edbbc50c87
pawelstacherzak <pstacherzak@wikia-inc.com>
2020-04-02 11:43:16 +0200

    Wiki ids dump

---
+++ extensions/wikia/Discussions/maintenance/dumpForumWikiIds.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once( __DIR__ . '/../../../../maintenance/Maintenance.php' );
+
+class ForumWikiIds extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addArg( 'out', "Output file for wiki ids", $required = false );
+	}
+
+	/**
+	 * Do the actual work. All child classes will need to implement this
+	 */
+	public function execute() {
+		$outputName = $this->hasOption( 'out' ) ? $this->getArg() : "php://stdout";
+		$fh = fopen( $outputName, 'w' );
+
+		if ( $fh === false ) {
+			$this->error( "Unable to open file " . $outputName, 1 );
+		}
+
+		global $wgExternalSharedDB;
+			$db = wfGetDB( DB_SLAVE, [], $wgExternalSharedDB );
+
+		( new \WikiaSQL() )->SELECT(
+			"city_id"
+		)
+			->FROM( "city_list" )
+			->JOIN( "city_variables" )
+			->ON( 'city_list.city_id', 'city_variables.cv_city_id' )
+			->WHERE( 'cv_variable_id' )
+			->EQUAL_TO( 1195 )
+			->AND_( 'cv_value' )
+			->EQUAL_TO( 'b:1;' )
+			->AND_( 'city_public' )
+			->EQUAL_TO( WikiFactory::PUBLIC_WIKI )
+			->runLoop(
+				$db,
+				function ( &$cities, $row ) use ( $fh ) {
+					fwrite( $fh, $row->city_id . "\n" );
+				}
+			);
+
+		fclose( $fh );
+	}
+}
+
+$maintClass = ForumWikiIds::class;
+require_once( RUN_MAINTENANCE_IF_MAIN );
