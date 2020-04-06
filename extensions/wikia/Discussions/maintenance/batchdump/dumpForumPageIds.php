1d857b511b7eba1fd923756eb9b849d1db1e2966
pawelstacherzak <pstacherzak@wikia-inc.com>
2020-04-06 09:44:14 +0200

    Include missing class

--- extensions/wikia/Discussions/maintenance/batchdump/dumpForumPageIds.php
+++ extensions/wikia/Discussions/maintenance/batchdump/dumpForumPageIds.php
@@ -8,6 +8,7 @@
 error_reporting(E_ALL);
 
 require_once( __DIR__ . '/../../../../../maintenance/Maintenance.php' );
+include_once( __DIR__ . '/../dumpForumData.php' );
 include_once( __DIR__ . '/../ForumDumper.php' );
 include_once( __DIR__ . '/../FollowsFinder.php' );
 include_once( __DIR__ . '/../WallHistoryFinder.php' );
