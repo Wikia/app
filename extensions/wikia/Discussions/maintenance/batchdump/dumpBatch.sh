5a569a5d8f4cddf8301aafb5ac3c45f73b8f649f
pawelstacherzak <pstacherzak@wikia-inc.com>
2020-04-06 10:16:11 +0200

    Iteration check

--- extensions/wikia/Discussions/maintenance/batchdump/dumpBatch.sh
+++ extensions/wikia/Discussions/maintenance/batchdump/dumpBatch.sh
@@ -4,4 +4,16 @@ echo == Dumping page ids
 command="php extensions/wikia/Discussions/maintenance/batchdump/dumpForumPageIds.php $@"
 echo $command;
 eval $command;
-echo == Pages ids dumped: $(eval "wc -l $2")
+
+pagesNumber = $(eval "wc -l $2")
+echo == Pages ids dumped: $pagesNumber
+
+for i in `seq 0 500 $pagesNumber`;
+do
+  if [[ $($i + 500) -gt $pagesNumber ]]
+  then
+    echo "Min: $i max: $($pagesNumber - 1)";
+  else
+    echo "Min: $i max: $($i + 500 - 1)";
+  fi;
+done
