295fe09046dc97b6f017bcc8c2f028f951fd0678
mikolajmeller <mmeller@wikia-inc.com>
2020-06-26 13:21:11 +0200

    clear check user table from ip

--- extensions/wikia/CoppaTool/legacy/IpAnonymizer.php
+++ extensions/wikia/CoppaTool/legacy/IpAnonymizer.php
@@ -17,8 +17,16 @@ class IpAnonymizer {
 		$this->updateTable( $dbw, 'abuse_filter_log', 'afl_ip', $ip );
 		$this->updateTable( $dbw, 'cu_log', 'cul_target_text', $ip );
 		$this->updateTable( $dbw, 'cu_changes', 'cuc_user_text', $ip );
+		$this->updateTable( $dbw, 'cu_changes', 'cuc_ip', $ip );
 		$this->updateTable( $dbw, 'recentchanges', 'rc_title', $ip, [ 'rc_log_type' => [ 'block', 'rights', 'phalanx' ] ] );
 		$this->updateTable( $dbw, 'logging', 'log_title', $ip, [ 'log_type' => [ 'block', 'rights', 'phalanx' ] ] );
+		if ( $dbw->tableExists( 'cu_changes' ) && $dbw->fieldExists( 'cu_changes', 'cuc_ip_hex' ) ) {
+			$dbw->update(
+				'cu_changes',
+				['cuc_ip_hex' => IP::toHex( self::NON_ROUTABLE_IPV6 ) ],
+				[ 'cuc_ip_hex' => IP::toHex( $ip ) ]
+			);
+		}
 	}
 
 	private function updateTable( DatabaseMysqli $dbw, string $table, string $column, string $ip, array $conds = [] ): void {
