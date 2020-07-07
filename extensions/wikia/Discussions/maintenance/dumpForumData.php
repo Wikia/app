b7dfafbc322c137084b00000bea9e44651cb1c74
pawelstacherzak <pstacherzak@wikia-inc.com>
2020-07-07 09:20:38 +0200

    Xdebug hint removed from comment

--- extensions/wikia/Discussions/maintenance/dumpForumData.php
+++ extensions/wikia/Discussions/maintenance/dumpForumData.php
@@ -3,7 +3,7 @@
  * Dumps the Forum data for the site selected by setting the SERVER_ID variable
  *
  * Run like maintenance script with extra xdebug settings:
- * php -d xdebug.max_nesting_level=-1 extensions/wikia/Discussions/maintenance/dumpForumData.php --out importData.sql
+ * php extensions/wikia/Discussions/maintenance/dumpForumData.php --out importData.sql
  *
  * @ingroup Maintenance
  */
