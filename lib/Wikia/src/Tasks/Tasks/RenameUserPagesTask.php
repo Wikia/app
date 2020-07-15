6abcb47ac54e7c50d7579c3918f87c482bd9f181
Michał Turek <mturek@wikia-inc.com>
2020-07-15 13:58:01 +0200

    SER-4115 Allow to run rename on closed wiki
    Add logging for exceptions

--- lib/Wikia/src/Tasks/Tasks/RenameUserPagesTask.php
+++ lib/Wikia/src/Tasks/Tasks/RenameUserPagesTask.php
@@ -51,22 +51,27 @@ class RenameUserPagesTask extends BaseTask {
 	public function renameLocalPagesAndMarkAsDone( int $renameLogId, string $oldUserName, string $newUserName ) {
 		global $wgSpecialsDB, $wgCityId;
 		$marker = [ 'rename_log_id' => $renameLogId ];
-		$this->info( __METHOD__ . 'starting local rename', $marker );
-		$this->renameLocalPages($oldUserName, $newUserName );
-		$this->info( __METHOD__ . 'local rename success', $marker );
-		$dbw = wfGetDB( DB_MASTER, [], $wgSpecialsDB );
-		$dbw->update(
-			'rename_log_details',
-			[
-				'finished' => wfTimestamp( TS_DB ),
-				'was_successful' => true,
-			],
-			[ 'rename_log_id' => $renameLogId, 'wiki_id' => $wgCityId ]
-		);
-		$this->info( __METHOD__ . 'marked local rename success', $marker );
-		ServiceFactory::instance()->ucpTaskFactory()
-			->queue()->attemptToFinishRename( $renameLogId );
-		$this->info( __METHOD__ . 'scheduled attempt to finish rename', $marker );
+		try {
+			$this->info( __METHOD__ . 'starting local rename', $marker );
+			$this->renameLocalPages( $oldUserName, $newUserName );
+			$this->info( __METHOD__ . 'local rename success', $marker );
+			$dbw = wfGetDB( DB_MASTER, [], $wgSpecialsDB );
+			$dbw->update(
+				'rename_log_details',
+				[
+					'finished' => wfTimestamp( TS_DB ),
+					'was_successful' => true,
+				],
+				[ 'rename_log_id' => $renameLogId, 'wiki_id' => $wgCityId ]
+			);
+			$this->info( __METHOD__ . 'marked local rename success', $marker );
+			ServiceFactory::instance()->ucpTaskFactory()
+				->queue()->attemptToFinishRename( $renameLogId );
+			$this->info( __METHOD__ . 'scheduled attempt to finish rename', $marker );
+		} catch (Exception $e) {
+			$this->error( __METHOD__ . 'failed to perform local rename ' . $e->getMessage(), $marker );
+			throw $e;
+		}
 	}
 
 	/**
