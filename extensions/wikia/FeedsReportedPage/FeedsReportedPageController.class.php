2099ee3fcbf1c56c410aee5e96c5d4a5996ae55b
pawelstacherzak <pstacherzak@wikia-inc.com>
2020-07-22 14:54:15 +0200

    Links changed

--- extensions/wikia/FeedsReportedPage/FeedsReportedPageController.class.php
+++ extensions/wikia/FeedsReportedPage/FeedsReportedPageController.class.php
@@ -1,13 +1,20 @@
 <?php
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Uri;
 use Wikia\Factory\ServiceFactory;
+use function GuzzleHttp\Psr7\build_query;
+use function GuzzleHttp\Psr7\parse_query;
 
 class FeedsReportedPageController extends WikiaController {
 
 	private const PAGINATION_QUERY_PARAMS = [ 'limit', 'page', 'pivotId' ];
 
 	private $gateway;
+
+	private $baseDomain;
+
+	private $scriptPath;
 
 	public function __construct() {
 		parent::__construct();
@@ -19,6 +26,9 @@ class FeedsReportedPageController extends WikiaController {
 			$discussionsServiceUrl,
 			$this->wg->CityId
 		);
+
+		$this->baseDomain = $this->wg->Server;
+		$this->scriptPath = $this->wg->ScriptPath;
 	}
 
 	public function init() {
@@ -47,11 +57,41 @@ class FeedsReportedPageController extends WikiaController {
 		$reportedPosts = $this->gateway->getReportedPosts( $pagination, $viewableOnly, $containerType, $user->getId() );
 
 		if ( $reportedPosts ) {
+			$this->mapLinks( $reportedPosts );
 			$this->response->setData( $reportedPosts );
 			$this->response->setCode( WikiaResponse::RESPONSE_CODE_OK );
 		} else {
 			$this->response->setCode( WikiaResponse::RESPONSE_CODE_INTERNAL_SERVER_ERROR );
 		}
+	}
+
+	private function mapLinks( &$reportedPosts ) {
+		foreach ( $reportedPosts['_links'] as $linkName => $target ) {
+
+			if ( array_key_exists( 'href', $target ) ) {
+				$uri = new Uri( $target['href'] );
+				$reportedPosts['_links'][$linkName]['href'] = $this->buildNewLink( $uri );
+			} else {
+				$uri = new Uri( $target[0]['href'] );
+				$reportedPosts['_links'][$linkName][0]['href'] = $this->buildNewLink( $uri );
+			}
+		}
+	}
+
+	private function buildNewLink( Uri $uri ) {
+		$serviceQueryParams = parse_query( $uri->getQuery() );
+		$controllerQueryParams = [
+			'controller' => 'FeedsReportedPageController',
+			'method' => 'getReportedPosts',
+		];
+
+		foreach ( $serviceQueryParams as $paramName => $value ) {
+			$controllerQueryParams[$paramName] = $value;
+		}
+		return (string)$uri->withScheme( 'https' )
+			->withHost( $this->baseDomain )
+			->withPath( $this->scriptPath . '/wikia.php' )
+			->withQuery( build_query( $controllerQueryParams ) );
 	}
 
 	/**
