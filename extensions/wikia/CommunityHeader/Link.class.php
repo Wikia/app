db06801bc2cbe900986150dbf1ccdd55423c6a07
Máté Szabó <mszabo@fandom.com>
2020-09-16 12:52:05 +0200

    Revert "cake-5934 | Add Fandom Store to Explore Dropdown"

--- extensions/wikia/CommunityHeader/Link.class.php
+++ extensions/wikia/CommunityHeader/Link.class.php
@@ -6,12 +6,10 @@ class Link {
 	public $href;
 	public $label;
 	public $tracking;
-	public $items;
 
-	public function __construct( Label $label, string $href, string $tracking = '', array $items = []) {
+	public function __construct( Label $label, string $href, string $tracking = '') {
 		$this->label = $label;
 		$this->href = $href;
 		$this->tracking = $tracking;
-		$this->items = $items;
 	}
 }
