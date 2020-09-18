88ad6f9f293eb2339b16897376e0ca5bb1f5620d
Chris Schuhmacher <cschuhmacher@fandom.com>
2020-09-18 23:37:18 +0000

    cake-5934 | log cached data

--- includes/wikia/models/DesignSystemCommunityHeaderModel.class.php
+++ includes/wikia/models/DesignSystemCommunityHeaderModel.class.php
@@ -506,6 +506,8 @@ class DesignSystemCommunityHeaderModel extends WikiaModel {
 
 		$memcKey = wfSharedMemcKey( DesignSystemApiController::MEMC_PREFIX_FANDOM_STORE, $wgCityId );
 		$cachedStoreData = $wgMemc->get( $memcKey );
+		var_dump( $cachedStoreData );
+		die();
 		return !empty( $cachedStoreData->results ) ? $this->formatFandomStoreData( $cachedStoreData->results ) : null;
 	}
 
