70039fa627fdca8a6321bfc372fa710c91460a02
Martyna Nowicka <martynaanow@gmail.com>
2020-11-25 12:41:37 +0100

    PLATFORM-5211 | Quickly add banners for app wikis

--- extensions/wikia/FandomComMigration/scripts/fandom-com-migration.js
+++ extensions/wikia/FandomComMigration/scripts/fandom-com-migration.js
@@ -15,9 +15,13 @@ require([
 	var wgFandomComMigrationDone = mw.config.get('wgFandomComMigrationDone');
 	var wgFandomComMigrationCustomMessageBefore = mw.config.get('wgFandomComMigrationCustomMessageBefore');
 	var wgFandomComMigrationCustomMessageAfter = mw.config.get('wgFandomComMigrationCustomMessageAfter');
+	var wgDomainMigrationScheduled = mw.config.get('wgDomainMigrationScheduled');
+	var wgDomainMigrationDone = mw.config.get('wgDomainMigrationDone');
 
 	var afterMigrationClosedStorageKey = 'fandom-com-migration-after-closed';
 	var beforeMigrationClosedStorageKey = 'fandom-com-migration-before-closed';
+	var ucpDomainMigrationScheduledMessageKey = 'ucp-migration-banner-fandom-message-scheduled-fandom-wikis';
+	var ucpDomainMigrationDoneMessageKey = 'ucp-migration-banner-fandom-message-complete';
 	var storageTrueValue = '1';
 
 	// Keep it for a year, it's more than enough
@@ -29,6 +33,14 @@ require([
 
 	function shouldShowBeforeMigrationNotification() {
 		return wgFandomComMigrationScheduled && cache.get(beforeMigrationClosedStorageKey) !== storageTrueValue;
+	}
+
+	function shouldShowDomainMigrationScheduledBannerNotification() {
+		return wgDomainMigrationScheduled && cache.get(ucpDomainMigrationScheduledMessageKey) !== storageTrueValue;
+	}
+
+	function shouldShowDomainMigrationDoneBannerNotification() {
+		return wgDomainMigrationDone && cache.get(ucpDomainMigrationDoneMessageKey) !== storageTrueValue;
 	}
 
 	function showAfterMigrationNotification() {
@@ -63,11 +75,50 @@ require([
 		});
 	}
 
+	function showPreDomainMigrationBanner() {
+		mw.loader.using(['ext.fandomComMigration', 'mediawiki.jqueryMsg']).then(function () {
+			var banner = new BannerNotification(
+				mw.message(ucpDomainMigrationScheduledMessageKey).parse(),
+				'warn',
+				null
+			);
+
+			banner.onCloseHandler = function () {
+				cache.set(ucpDomainMigrationScheduledMessageKey, storageTrueValue, localStorageTTL);
+			}
+
+			banner.show();
+		});
+	}
+
+	function showPostDomainMigrationBanner() {
+		mw.loader.using(['ext.fandomComMigration', 'mediawiki.jqueryMsg']).then(function () {
+			var banner = new BannerNotification(
+				mw.message(ucpDomainMigrationDoneMessageKey).parse(),
+				'warn',
+				null
+			);
+
+			banner.onCloseHandler = function () {
+				cache.set(ucpDomainMigrationScheduledMessageKey, storageTrueValue, localStorageTTL);
+			}
+
+			banner.show();
+		});
+	}
+
 	$(function () {
 		if (shouldShowAfterMigrationNotification()) {
 			showAfterMigrationNotification();
 		} else if (shouldShowBeforeMigrationNotification()) {
 			showBeforeMigrationNotification();
+		}
+
+		if (shouldShowDomainMigrationScheduledBannerNotification()) {
+			showPreDomainMigrationBanner();
+		}
+		if (shouldShowDomainMigrationDoneBannerNotification()) {
+			showPostDomainMigrationBanner();
 		}
 	});
 });
