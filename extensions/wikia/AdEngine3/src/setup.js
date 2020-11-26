99b5a0ce20c27668aaea22fad148e0f9be2b3546
Damian Sobczak <dsobczak@wikia-inc.com>
2020-11-26 13:55:46 +0100

    ADEN-10492 Removing bidders.prebid.targeting context key

--- extensions/wikia/AdEngine3/src/setup.js
+++ extensions/wikia/AdEngine3/src/setup.js
@@ -188,16 +188,6 @@ async function setupAdContext(wikiContext, consents) {
 	setupBidders(context, instantConfig);
 
 	if (context.get('bidders.prebid.enabled')) {
-		const s1 = context.get('wiki.targeting.wikiIsTop1000') ? context.get('targeting.s1') : 'not a top1k wiki';
-
-		context.set('bidders.prebid.targeting', {
-			src: ['gpt'],
-			s0: [context.get('targeting.s0') || ''],
-			s1: [s1],
-			s2: [context.get('targeting.s2') || ''],
-			lang: [context.get('targeting.wikiLanguage') || 'en'],
-		});
-
 		if (!instantConfig.get('icPrebidPubmaticOutstream')) {
 			context.remove('bidders.prebid.pubmatic.slots.INCONTENT_PLAYER');
 		}
