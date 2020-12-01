18b5e8c5b76324ed4b127c7c5e310d4003bb714f
grunny <mwgrunny@gmail.com>
2020-12-02 05:09:56 +1000

    Enable the UCP migration banner on old app wikis by default

--- extensions/wikia/FandomComMigration/FandomComMigrationHooks.class.php
+++ extensions/wikia/FandomComMigration/FandomComMigrationHooks.class.php
@@ -3,7 +3,8 @@
 class FandomComMigrationHooks {
 
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
-		if ( static::isEnabled() ) {
+		global $wgDisableUCPMigrationBanner;
+		if ( empty( $wgDisableUCPMigrationBanner ) ) {
 			$out->addModules( 'ext.fandomComMigration' );
 		}
 
@@ -12,7 +13,8 @@ class FandomComMigrationHooks {
 
 	public static function onMercuryWikiVariables( array &$wikiVariables ): bool {
 		global $wgFandomComMigrationDone, $wgFandomComMigrationCustomMessageBefore,
-			   $wgFandomComMigrationCustomMessageAfter, $wgDomainMigrationScheduled, $wgDomainMigrationDone;
+			   $wgFandomComMigrationCustomMessageAfter, $wgDomainMigrationScheduled,
+			   $wgDomainMigrationDone, $wgDisableUCPMigrationBanner;
 
 		if ( $wgDomainMigrationScheduled || $wgDomainMigrationDone ) {
 			$domainMigrationMessageKey = null;
@@ -31,36 +33,17 @@ class FandomComMigrationHooks {
 			}
 		}
 
-		if ( static::isEnabled() ) {
-			$parser = ParserPool::get();
-
-			// Send HTML instead of message key to avoid the issue of non-compatible i18n libs
-			if ( $wgFandomComMigrationDone ) {
-				if ( !empty( $wgFandomComMigrationCustomMessageAfter ) ) {  // customized message
-					$wikiVariables['fandomComMigrationNotificationAfter'] = $parser->parse(
-						$wgFandomComMigrationCustomMessageAfter, new Title(), new ParserOptions() )->getText();
-				} else {
-					$wikiVariables['fandomComMigrationNotificationAfter'] =
-						wfMessage( 'fandom-com-migration-after' )->parse();
-				}
-			} elseif ( static::isMigrationScheduled() ) {
-				if ( !empty( $wgFandomComMigrationCustomMessageBefore ) ) {  // customized message
-					$wikiVariables['fandomComMigrationNotificationBefore'] = $parser->parse(
-						$wgFandomComMigrationCustomMessageBefore, new Title(), new ParserOptions() )->getText();
-				} else {
-					$wikiVariables['fandomComMigrationNotificationBefore'] =
-						wfMessage( 'fandom-ucp-migration-before' )
-							->parse();
-				}
-			}
+		if ( empty( $wgDisableUCPMigrationBanner ) ) {
+			$wikiVariables['fandomComMigrationNotificationBefore'] =
+				wfMessage( 'fandom-ucp-migration-before' )->parse();
 		}
 
 		return true;
 	}
 
 	public static function onOasisSkinAssetGroups( &$jsAssets ) {
-		global $wgDomainMigrationScheduled, $wgDomainMigrationDone;
-		if ( static::isEnabled() || $wgDomainMigrationScheduled || $wgDomainMigrationDone ) {
+		global $wgDomainMigrationScheduled, $wgDomainMigrationDone, $wgDisableUCPMigrationBanner;
+		if ( empty( $wgDisableUCPMigrationBanner ) || $wgDomainMigrationScheduled || $wgDomainMigrationDone ) {
 			$jsAssets[] = 'fandom_com_migration_js';
 		}
 
@@ -68,14 +51,14 @@ class FandomComMigrationHooks {
 	}
 
 	public static function onWikiaSkinTopScripts( &$vars, &$scripts ) {
-		global $wgDomainMigrationScheduled, $wgDomainMigrationDone;
-		if ( static::isEnabled() || $wgDomainMigrationScheduled || $wgDomainMigrationDone ) {
+		global $wgDomainMigrationScheduled, $wgDomainMigrationDone, $wgDisableUCPMigrationBanner;
+		if ( empty( $wgDisableUCPMigrationBanner ) || $wgDomainMigrationScheduled || $wgDomainMigrationDone ) {
 			$parser = ParserPool::get();
 			global $wgFandomComMigrationDone, $wgFandomComMigrationCustomMessageBefore,
 				   $wgFandomComMigrationCustomMessageAfter;
 
-			$vars['wgFandomComMigrationScheduled'] = static::isMigrationScheduled();
-			$vars['wgFandomComMigrationDone'] = $wgFandomComMigrationDone;
+			$vars['wgFandomComMigrationScheduled'] = empty( $wgDisableUCPMigrationBanner );
+			$vars['wgFandomComMigrationDone'] = false;
 			$vars['wgFandomComMigrationCustomMessageBefore'] = $parser->parse(
 				$wgFandomComMigrationCustomMessageBefore, new Title(), new ParserOptions() )->getText();
 			$vars['wgFandomComMigrationCustomMessageAfter'] = $parser->parse(
@@ -85,35 +68,5 @@ class FandomComMigrationHooks {
 		}
 
 		return true;
-	}
-
-	private static function isEnabled() {
-		global $wgDomainChangeDate, $wgFandomComMigrationDone;
-
-		if ( $wgFandomComMigrationDone && !empty( $wgDomainChangeDate ) ) {
-			$migrationDateTime = new DateTime( $wgDomainChangeDate );
-			$weekAgo = ( new DateTime() )->sub( new DateInterval( 'P7D' ) );
-
-			return $migrationDateTime > $weekAgo;
-		}
-
-		return static::isMigrationScheduled();
-	}
-
-	/**
-	 * Check if the "migration scheduled" banner should be displayed.
-	 *
-	 * The banner will be displayed if either $wgFandomComMigrationScheduled is true
-	 * or the following criteria are met:
-	 *     - It is not a fandom.com already wiki
-	 *     - It is not a single subdomain non-English wiki
-	 *     - It is not in the wiki verticals "Other" or "Lifestyle"
-	 *
-	 * @return boolean
-	 */
-	private static function isMigrationScheduled(): bool {
-		global $wgFandomComMigrationScheduled;
-
-		return !empty( $wgFandomComMigrationScheduled );
 	}
 }
