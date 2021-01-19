fae452bb4b66fbd1360c02a728f74dcd6ea4cc19
Damian Sobczak <dsobczak@wikia-inc.com>
2021-01-19 15:06:47 +0100

    ADEN-10709 Adding Verizon bidder

--- extensions/wikia/AdEngine3/src/ad-context.js
+++ extensions/wikia/AdEngine3/src/ad-context.js
@@ -457,6 +457,28 @@ export default {
 					}
 				}
 			},
+			verizon: {
+				enabled: false,
+				dcn: '8a96945901757509a7551e039e180357',
+				slots: {
+					top_leaderboard: {
+						sizes: [[728, 90]],
+						pos: 'top_leaderboard',
+					},
+					top_boxad: {
+						sizes: [[300, 250]],
+						pos: 'top_boxad',
+					},
+					incontent_boxad_1: {
+						sizes: [[300, 250]],
+						pos: 'incontent_boxad_1',
+					},
+					bottom_leaderboard: {
+						sizes: [[728, 90]],
+						pos: 'bottom_leaderboard',
+					},
+				},
+			},
 			wikia: {
 				enabled: false,
 				slots: {
