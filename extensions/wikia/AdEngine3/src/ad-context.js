b36904c20f2c740722b3043f9d6717c9ba4dc443
Damian Sobczak <dsobczak@wikia-inc.com>
2021-02-15 10:48:45 +0100

    ADEN-10788 Removing AOL and OneMobile Prebid bidders

--- extensions/wikia/AdEngine3/src/ad-context.js
+++ extensions/wikia/AdEngine3/src/ad-context.js
@@ -47,29 +47,6 @@ export default {
 				enabled: false,
 				slots: ['incontent_boxad_1']
 			},
-			aol: {
-				enabled: false,
-				network: '9435.1',
-				slots: {
-					top_leaderboard: {
-						sizes: [
-							[728, 90]
-						],
-						placement: '4431497',
-						alias: '4431497',
-						sizeId: '225'
-					},
-					top_boxad: {
-						sizes: [
-							[300, 250],
-							[300, 600]
-						],
-						placement: '4431473',
-						alias: '4431473',
-						sizeId: '170'
-					}
-				}
-			},
 			appnexus: {
 				enabled: false,
 				slots: {
@@ -225,10 +202,6 @@ export default {
 						siteId: 21872987104
 					}
 				}
-			},
-			onemobile: {
-				enabled: false,
-				slots: {}
 			},
 			oneVideo: {
 				enabled: false,
